61a0014c008b3b4a6ee451684e2ac22938d6a970
Robert Nishihara <robertnishihara@gmail.com>
2016-06-11 13:43:57 -0700

    remove old file (#104)

--- test/gen-python-code.sh
+++
@@ -1,4 +0,0 @@
-# For running the python tests
-
-protoc -I ../protos/ --python_out=. --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_python_plugin` ../protos/ray.proto
-protoc -I ../protos/ --python_out=. --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_python_plugin` ../protos/types.proto
