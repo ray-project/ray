c4a6e719ee97eb95af4897d5fc653a8ad1779f78
Robert Nishihara <robertnishihara@gmail.com>
2016-06-24 19:43:24 -0700

    minor: single quote to double (#156)

--- scripts/shell.py
+++ scripts/shell.py
@@ -13,7 +13,7 @@ parser.add_argument("--scheduler-address", default="127.0.0.1:10001", type=str, 
 parser.add_argument("--objstore-address", default="127.0.0.1:20001", type=str, help="the objstore's address")
 parser.add_argument("--worker-address", default="127.0.0.1:30001", type=str, help="the worker's address")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   args = parser.parse_args()
   worker.connect(args.scheduler_address, args.objstore_address, args.worker_address)
 
