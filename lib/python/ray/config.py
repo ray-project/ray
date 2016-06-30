4469eead5bd33a5375156541544eee3fa3a3bb8e
Mehrdad <mehrdadn@users.noreply.github.com>
2016-06-30 16:03:40 -0700

    Use proper temp directory

--- lib/python/ray/config.py
+++ lib/python/ray/config.py
@@ -1,2 +1,5 @@
-LOG_DIRECTORY = "/tmp/raylogs/"
+import os.path
+import sys
+import tempfile
+LOG_DIRECTORY = os.path.join("/tmp" if sys.platform.startswith("darwin") else tempfile.gettempdir(), "raylogs")
 LOG_TIMESTAMP = "{:%Y-%m-%d=%H:%M:%S}"
