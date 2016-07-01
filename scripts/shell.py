508ea9ce755f9c939ce1ff5891c53638ee6a538c
Robert Nishihara <robertnishihara@gmail.com>
2016-06-30 22:57:29 -0700

    fixes to shell.py (#195)

--- scripts/shell.py
+++ scripts/shell.py
@@ -25,10 +25,10 @@ if __name__ == "__main__":
   if args.attach:
     assert args.worker_path is None, "when attaching, no new worker can be started"
     assert args.num_workers is None, "when attaching, no new worker can be started"
-    worker.connect(args.scheduler_address, args.objstore_address, args.worker_address)
+    worker.connect(args.scheduler_address, args.objstore_address, args.worker_address, is_driver=True, mode=ray.SHELL_MODE)
   else:
     services.start_ray_local(num_workers=args.num_workers if not args.num_workers is None else DEFAULT_NUM_WORKERS,
-                             worker_path=args.worker_path if not args.num_workers is None else DEFAULT_WORKER_PATH,
+                             worker_path=args.worker_path if not args.worker_path is None else DEFAULT_WORKER_PATH,
                              driver_mode=ray.SHELL_MODE)
 
   import IPython
