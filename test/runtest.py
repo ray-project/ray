47bbecf4261227a563d9d5aafd83c9a05a6a1b5c
mehrdadn <mehrdadn@users.noreply.github.com>
2016-07-07 03:31:58 +0300

    int and long should be treated similarly (#220)

--- test/runtest.py
+++ test/runtest.py
@@ -12,7 +12,7 @@ import test_functions
 import ray.array.remote as ra
 import ray.array.distributed as da
 
-RAY_TEST_OBJECTS = [[1, "hello", 3.0], 42, "hello world", 42.0,
+RAY_TEST_OBJECTS = [[1, "hello", 3.0], 42, 43L, "hello world", 42.0, 1L << 62,
                     (1.0, "hi"), None, (None, None), ("hello", None),
                     True, False, (True, False),
                     {True: "hello", False: "world"},
