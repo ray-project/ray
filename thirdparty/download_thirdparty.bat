ed9ed1cbc526bdd8bac650d536b0cae7f42334e1
mehrdadn <mehrdadn@users.noreply.github.com>
2016-07-08 19:24:25 +0300

    Apply patches while considering index as well (#231)

--- thirdparty/download_thirdparty.bat
+++ thirdparty/download_thirdparty.bat
@@ -10,11 +10,11 @@
 		git -C "grpc"                                              submodule update --init "third_party/protobuf"
 		git -C "grpc"                                              submodule update --init "third_party/nanopb"
 		git -C "grpc"                                              submodule update --init "third_party/zlib"
-		git -C "grpc"                                              apply "%~dp0patches/grpc-source.patch"
-		git -C "grpc"                                              apply "%~dp0patches/windows/grpc-projects.patch"
-		git -C "grpc/third_party/protobuf"                         apply "%~dp0patches/windows/protobuf-projects.patch"
-		git -C "arrow/cpp/thirdparty/flatbuffers"                  apply "%~dp0patches/windows/flatbuffers-projects.patch"
-		git -C "arrow/cpp/thirdparty/parquet"                      apply "%~dp0patches/windows/parquet-endian.patch"
-		git -C "python"                                            apply "%~dp0patches/windows/python-pyconfig.patch"
+		git -C "grpc"                                              apply --index --3way "%~dp0patches/grpc-source.patch"
+		git -C "grpc"                                              apply --index --3way "%~dp0patches/windows/grpc-projects.patch"
+		git -C "grpc/third_party/protobuf"                         apply --index --3way "%~dp0patches/windows/protobuf-projects.patch"
+		git -C "arrow/cpp/thirdparty/flatbuffers"                  apply --index --3way "%~dp0patches/windows/flatbuffers-projects.patch"
+		git -C "arrow/cpp/thirdparty/parquet"                      apply --index --3way "%~dp0patches/windows/parquet-endian.patch"
+		git -C "python"                                            apply --index --3way "%~dp0patches/windows/python-pyconfig.patch"
 	@PopD
 @EndLocal
