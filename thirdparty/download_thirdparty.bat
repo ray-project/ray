4c3f22eab5f82895786ca4c1f259372a3aee4645
mehrdadn <mehrdadn@users.noreply.github.com>
2016-07-09 06:06:21 +0300

    Remove parquet for Windows -- we don't need it yet (#240)

--- thirdparty/download_thirdparty.bat
+++ thirdparty/download_thirdparty.bat
@@ -4,7 +4,6 @@
 		@If Not Exist "grpc\.git"   git                            clone "https://github.com/grpc/grpc"
 		@If Not Exist "arrow\.git"  git                            clone "https://github.com/pcmoritz/arrow.git" --branch windows_with_submodules --recursive
 		@If Not Exist "arrow\cpp\thirdparty\flatbuffers\.git"  git clone "https://github.com/google/flatbuffers.git" "arrow/cpp/thirdparty/flatbuffers"
-		@If Not Exist "arrow\cpp\thirdparty\parquet\.git"      git clone "https://github.com/apache/parquet-cpp.git" "arrow/cpp/thirdparty/parquet"
 		@If Not Exist "numbuf\.git" git                            clone "https://github.com/amplab/numbuf.git" --branch win
 		@If Not Exist "python\.git" git                            clone "https://github.com/austinsc/python.git"
 		git -C "grpc"                                              submodule update --init "third_party/protobuf"
@@ -14,7 +13,6 @@
 		git -C "grpc"                                              apply --index --3way "%~dp0patches/windows/grpc-projects.patch"
 		git -C "grpc/third_party/protobuf"                         apply --index --3way "%~dp0patches/windows/protobuf-projects.patch"
 		git -C "arrow/cpp/thirdparty/flatbuffers"                  apply --index --3way "%~dp0patches/windows/flatbuffers-projects.patch"
-		git -C "arrow/cpp/thirdparty/parquet"                      apply --index --3way "%~dp0patches/windows/parquet-endian.patch"
 		git -C "python"                                            apply --index --3way "%~dp0patches/windows/python-pyconfig.patch"
 	@PopD
 @EndLocal
