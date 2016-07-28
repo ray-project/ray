1961deeffb34d23902628a6bbe6aeb962b3bcef7
mehrdadn <mehrdadn@users.noreply.github.com>
2016-07-28 13:11:13 -0700

    Update Windows support (#317)

--- thirdparty/download_thirdparty.bat
+++ thirdparty/download_thirdparty.bat
@@ -1,12 +1,9 @@
 @SetLocal
 	@Echo Off
 	@PushD "%~dp0"
-		If Not Exist "arrow\.git"  git clone  --recursive --branch windows_with_submodules "https://github.com/pcmoritz/arrow.git"
-		If Not Exist "python\.git" git clone                                               "https://github.com/austinsc/python.git"
-		git                                                     submodule update --init
-		git -C "arrow"                                          submodule update --init
-		git -C "grpc"                                           submodule update --init "third_party/protobuf"
-		git -C "grpc"                                           submodule update --init "third_party/nanopb"
+		git                                                     submodule update --init --jobs="%NUMBER_OF_PROCESSORS%"
+		git -C "arrow"                                          submodule update --init --jobs="%NUMBER_OF_PROCESSORS%"
+		git -C "grpc"                                           submodule update --init --jobs="%NUMBER_OF_PROCESSORS%" -- "third_party/nanopb" "third_party/protobuf"
 		Call :GitApply "grpc"                                   "%CD%/patches/grpc-source.patch"
 		Call :GitApply "grpc"                                   "%CD%/patches/windows/grpc-projects.patch"
 		Call :GitApply "grpc/third_party/protobuf"              "%CD%/patches/windows/protobuf-projects.patch"
@@ -18,5 +15,5 @@
 
 :GitApply <ChangeToFolder> <Patch>
 	@REM Check if patch already applied by attempting to apply it in reverse; if not, then force-reapply it
-	git -C "%~1" apply --3way "%~2" -R --check 2> NUL || git -C "%~1" reset --hard && git -C "%~1" apply --3way "%~2"
+	git -C "%~1" apply "%~2" -R --check 2> NUL || git -C "%~1" apply "%~2" --3way 2> NUL || git -C "%~1" reset --hard && git -C "%~1" apply "%~2" --3way
 @GoTo :EOF
