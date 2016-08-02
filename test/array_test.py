7a69c10d0bfd4b4f6f72c5e1bfba7f1c07d6067c
Philipp Moritz <pcmoritz@gmail.com>
2016-08-02 16:11:53 -0700

    clean up tests (#340)

--- test/array_test.py
+++ test/array_test.py
@@ -4,6 +4,7 @@ import numpy as np
 import time
 import subprocess32 as subprocess
 import os
+from numpy.testing import assert_equal, assert_almost_equal
 
 import ray.array.remote as ra
 import ray.array.distributed as da
@@ -18,19 +19,19 @@ class RemoteArrayTest(unittest.TestCase):
     # test eye
     object_id = ra.eye.remote(3)
     val = ray.get(object_id)
-    self.assertTrue(np.alltrue(val == np.eye(3)))
+    assert_almost_equal(val, np.eye(3))
 
     # test zeros
     object_id = ra.zeros.remote([3, 4, 5])
     val = ray.get(object_id)
-    self.assertTrue(np.alltrue(val == np.zeros([3, 4, 5])))
+    assert_equal(val, np.zeros([3, 4, 5]))
 
     # test qr - pass by value
     a_val = np.random.normal(size=[10, 11])
     q_id, r_id = ra.linalg.qr.remote(a_val)
     q_val = ray.get(q_id)
     r_val = ray.get(r_id)
-    self.assertTrue(np.allclose(np.dot(q_val, r_val), a_val))
+    assert_almost_equal(np.dot(q_val, r_val), a_val)
 
     # test qr - pass by objectid
     a = ra.random.normal.remote([10, 13])
@@ -38,7 +39,7 @@ class RemoteArrayTest(unittest.TestCase):
     a_val = ray.get(a)
     q_val = ray.get(q_id)
     r_val = ray.get(r_id)
-    self.assertTrue(np.allclose(np.dot(q_val, r_val), a_val))
+    assert_almost_equal(np.dot(q_val, r_val), a_val)
 
     ray.worker.cleanup()
 
@@ -65,7 +66,7 @@ class DistributedArrayTest(unittest.TestCase):
     a = ra.ones.remote([da.BLOCK_SIZE, da.BLOCK_SIZE])
     b = ra.zeros.remote([da.BLOCK_SIZE, da.BLOCK_SIZE])
     x = da.DistArray([2 * da.BLOCK_SIZE, da.BLOCK_SIZE], np.array([[a], [b]]))
-    self.assertTrue(np.alltrue(x.assemble() == np.vstack([np.ones([da.BLOCK_SIZE, da.BLOCK_SIZE]), np.zeros([da.BLOCK_SIZE, da.BLOCK_SIZE])])))
+    assert_equal(x.assemble(), np.vstack([np.ones([da.BLOCK_SIZE, da.BLOCK_SIZE]), np.zeros([da.BLOCK_SIZE, da.BLOCK_SIZE])]))
 
     ray.worker.cleanup()
 
@@ -75,25 +76,25 @@ class DistributedArrayTest(unittest.TestCase):
     ray.init(start_ray_local=True, num_objstores=2, num_workers=10)
 
     x = da.zeros.remote([9, 25, 51], "float")
-    self.assertTrue(np.alltrue(ray.get(da.assemble.remote(x)) == np.zeros([9, 25, 51])))
+    assert_equal(ray.get(da.assemble.remote(x)), np.zeros([9, 25, 51]))
 
     x = da.ones.remote([11, 25, 49], dtype_name="float")
-    self.assertTrue(np.alltrue(ray.get(da.assemble.remote(x)) == np.ones([11, 25, 49])))
+    assert_equal(ray.get(da.assemble.remote(x)), np.ones([11, 25, 49]))
 
     x = da.random.normal.remote([11, 25, 49])
     y = da.copy.remote(x)
-    self.assertTrue(np.alltrue(ray.get(da.assemble.remote(x)) == ray.get(da.assemble.remote(y))))
+    assert_equal(ray.get(da.assemble.remote(x)), ray.get(da.assemble.remote(y)))
 
     x = da.eye.remote(25, dtype_name="float")
-    self.assertTrue(np.alltrue(ray.get(da.assemble.remote(x)) == np.eye(25)))
+    assert_equal(ray.get(da.assemble.remote(x)), np.eye(25))
 
     x = da.random.normal.remote([25, 49])
     y = da.triu.remote(x)
-    self.assertTrue(np.alltrue(ray.get(da.assemble.remote(y)) == np.triu(ray.get(da.assemble.remote(x)))))
+    assert_equal(ray.get(da.assemble.remote(y)), np.triu(ray.get(da.assemble.remote(x))))
 
     x = da.random.normal.remote([25, 49])
     y = da.tril.remote(x)
-    self.assertTrue(np.alltrue(ray.get(da.assemble.remote(y)) == np.tril(ray.get(da.assemble.remote(x)))))
+    assert_equal(ray.get(da.assemble.remote(y)), np.tril(ray.get(da.assemble.remote(x))))
 
     x = da.random.normal.remote([25, 49])
     y = da.random.normal.remote([49, 18])
@@ -101,33 +102,33 @@ class DistributedArrayTest(unittest.TestCase):
     w = da.assemble.remote(z)
     u = da.assemble.remote(x)
     v = da.assemble.remote(y)
-    np.allclose(ray.get(w), np.dot(ray.get(u), ray.get(v)))
-    self.assertTrue(np.allclose(ray.get(w), np.dot(ray.get(u), ray.get(v))))
+    assert_almost_equal(ray.get(w), np.dot(ray.get(u), ray.get(v)))
+    assert_almost_equal(ray.get(w), np.dot(ray.get(u), ray.get(v)))
 
     # test add
     x = da.random.normal.remote([23, 42])
     y = da.random.normal.remote([23, 42])
     z = da.add.remote(x, y)
-    self.assertTrue(np.allclose(ray.get(da.assemble.remote(z)), ray.get(da.assemble.remote(x)) + ray.get(da.assemble.remote(y))))
+    assert_almost_equal(ray.get(da.assemble.remote(z)), ray.get(da.assemble.remote(x)) + ray.get(da.assemble.remote(y)))
 
     # test subtract
     x = da.random.normal.remote([33, 40])
     y = da.random.normal.remote([33, 40])
     z = da.subtract.remote(x, y)
-    self.assertTrue(np.allclose(ray.get(da.assemble.remote(z)), ray.get(da.assemble.remote(x)) - ray.get(da.assemble.remote(y))))
+    assert_almost_equal(ray.get(da.assemble.remote(z)), ray.get(da.assemble.remote(x)) - ray.get(da.assemble.remote(y)))
 
     # test transpose
     x = da.random.normal.remote([234, 432])
     y = da.transpose.remote(x)
-    self.assertTrue(np.alltrue(ray.get(da.assemble.remote(x)).T == ray.get(da.assemble.remote(y))))
+    assert_equal(ray.get(da.assemble.remote(x)).T, ray.get(da.assemble.remote(y)))
 
     # test numpy_to_dist
     x = da.random.normal.remote([23, 45])
     y = da.assemble.remote(x)
     z = da.numpy_to_dist.remote(y)
     w = da.assemble.remote(z)
-    self.assertTrue(np.alltrue(ray.get(da.assemble.remote(x)) == ray.get(da.assemble.remote(z))))
-    self.assertTrue(np.alltrue(ray.get(y) == ray.get(w)))
+    assert_equal(ray.get(da.assemble.remote(x)), ray.get(da.assemble.remote(z)))
+    assert_equal(ray.get(y), ray.get(w))
 
     # test da.tsqr
     for shape in [[123, da.BLOCK_SIZE], [7, da.BLOCK_SIZE], [da.BLOCK_SIZE, da.BLOCK_SIZE], [da.BLOCK_SIZE, 7], [10 * da.BLOCK_SIZE, da.BLOCK_SIZE]]:
@@ -138,9 +139,9 @@ class DistributedArrayTest(unittest.TestCase):
       q_val = ray.get(da.assemble.remote(q))
       r_val = ray.get(r)
       self.assertTrue(r_val.shape == (K, shape[1]))
-      self.assertTrue(np.alltrue(r_val == np.triu(r_val)))
-      self.assertTrue(np.allclose(x_val, np.dot(q_val, r_val)))
-      self.assertTrue(np.allclose(np.dot(q_val.T, q_val), np.eye(K)))
+      assert_equal(r_val, np.triu(r_val))
+      assert_almost_equal(x_val, np.dot(q_val, r_val))
+      assert_almost_equal(np.dot(q_val.T, q_val), np.eye(K))
 
     # test da.linalg.modified_lu
     def test_modified_lu(d1, d2):
@@ -158,9 +159,9 @@ class DistributedArrayTest(unittest.TestCase):
       s_mat = np.zeros((d1, d2))
       for i in range(len(s_val)):
         s_mat[i, i] = s_val[i]
-      self.assertTrue(np.allclose(q_val - s_mat, np.dot(l_val, u_val))) # check that q - s = l * u
-      self.assertTrue(np.alltrue(np.triu(u_val) == u_val)) # check that u is upper triangular
-      self.assertTrue(np.alltrue(np.tril(l_val) == l_val)) # check that l is lower triangular
+      assert_almost_equal(q_val - s_mat, np.dot(l_val, u_val)) # check that q - s = l * u
+      assert_equal(np.triu(u_val), u_val) # check that u is upper triangular
+      assert_equal(np.tril(l_val), l_val) # check that l is lower triangular
 
     for d1, d2 in [(100, 100), (99, 98), (7, 5), (7, 7), (20, 7), (20, 10)]:
       test_modified_lu(d1, d2)
@@ -178,8 +179,8 @@ class DistributedArrayTest(unittest.TestCase):
       tall_eye = np.zeros((d1, min(d1, d2)))
       np.fill_diagonal(tall_eye, 1)
       q = tall_eye - np.dot(y_val, np.dot(t_val, y_top_val.T))
-      self.assertTrue(np.allclose(np.dot(q.T, q), np.eye(min(d1, d2)))) # check that q.T * q = I
-      self.assertTrue(np.allclose(np.dot(q, r_val), a_val)) # check that a = (I - y * t * y_top.T) * r
+      assert_almost_equal(np.dot(q.T, q), np.eye(min(d1, d2))) # check that q.T * q = I
+      assert_almost_equal(np.dot(q, r_val), a_val) # check that a = (I - y * t * y_top.T) * r
 
     for d1, d2 in [(123, da.BLOCK_SIZE), (7, da.BLOCK_SIZE), (da.BLOCK_SIZE, da.BLOCK_SIZE), (da.BLOCK_SIZE, 7), (10 * da.BLOCK_SIZE, da.BLOCK_SIZE)]:
       test_dist_tsqr_hr(d1, d2)
@@ -192,11 +193,11 @@ class DistributedArrayTest(unittest.TestCase):
       a_val = ray.get(da.assemble.remote(a))
       q_val = ray.get(da.assemble.remote(q))
       r_val = ray.get(da.assemble.remote(r))
-      self.assertTrue(q_val.shape == (d1, K))
-      self.assertTrue(r_val.shape == (K, d2))
-      self.assertTrue(np.allclose(np.dot(q_val.T, q_val), np.eye(K)))
-      self.assertTrue(np.alltrue(r_val == np.triu(r_val)))
-      self.assertTrue(np.allclose(a_val, np.dot(q_val, r_val)))
+      self.assertEqual(q_val.shape, (d1, K))
+      self.assertEqual(r_val.shape, (K, d2))
+      assert_almost_equal(np.dot(q_val.T, q_val), np.eye(K))
+      assert_equal(r_val, np.triu(r_val))
+      assert_almost_equal(a_val, np.dot(q_val, r_val))
 
     for d1, d2 in [(123, da.BLOCK_SIZE), (7, da.BLOCK_SIZE), (da.BLOCK_SIZE, da.BLOCK_SIZE), (da.BLOCK_SIZE, 7), (13, 21), (34, 35), (8, 7)]:
       test_dist_qr(d1, d2)
