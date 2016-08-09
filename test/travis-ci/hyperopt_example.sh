4d5f85e77c2053f612b8bcc5c98d306e9f8a56b1
Johann Schleier-Smith <jssmith@gmail.com>
2016-08-08 18:02:52 -0700

    Fix Travis failure detection on Mac + testing enhancements (#358)
    * properly handle failure in continuous integration
    
    * add stack overflow reference

--- test/travis-ci/hyperopt_example.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Hyperparameter optimization test
-# Runs only on Docker under Linux
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-  docker run --shm-size=500m amplab/ray:test-examples bash -c 'source setup-env.sh && cd examples/hyperopt && python driver.py'
-fi
