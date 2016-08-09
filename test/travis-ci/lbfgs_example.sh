94b0a8aa52d4d001b0f335a0f552005ee7c0e696
Johann Schleier-Smith <jssmith@gmail.com>
2016-08-08 18:02:52 -0700

    Fix Travis failure detection on Mac + testing enhancements (#358)
    * properly handle failure in continuous integration
    
    * add stack overflow reference

--- test/travis-ci/lbfgs_example.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# L-BFGS Test
-# Runs only on Docker under Linux
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-  docker run --shm-size=500m amplab/ray:test-examples bash -c 'source setup-env.sh && cd examples/lbfgs && python driver.py'
-fi
