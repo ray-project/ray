c238ae4aa01025761a6c709c812f1e35caabefcd
Philipp Moritz <pcmoritz@gmail.com>
2016-09-18 13:57:27 -0700

    do not re-download and rebuild redis if it already exists (#10)

--- thirdparty/build-redis.sh
+++ thirdparty/build-redis.sh
@@ -1,4 +1,6 @@
-wget http://download.redis.io/releases/redis-3.2.3.tar.gz
-tar xvfz redis-3.2.3.tar.gz
-cd redis-3.2.3
-make
+if [ ! -f redis-3.2.3/src/redis-server ]; then
+  wget http://download.redis.io/releases/redis-3.2.3.tar.gz
+  tar xvfz redis-3.2.3.tar.gz
+  cd redis-3.2.3
+  make
+fi
