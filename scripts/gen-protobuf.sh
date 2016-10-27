dd1bd3b2d708895953f059343b350c0490993434
Robert Nishihara <robertnishihara@gmail.com>
2016-10-26 23:24:40 -0700

    Remove unnecessary files. (#4)

--- scripts/gen-protobuf.sh
+++
@@ -1,7 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# TODO(mehrdad): How would this look in windows, where does the protoc executable go?
-# On Linux, we compile it ourselves, on Windows we might not want to do that (?)
-mkdir -p $DIR/../lib/python/ray/internal/
-$DIR/../thirdparty/grpc/bins/opt/protobuf/protoc -I ../protos/ --python_out=$DIR/../lib/python/ray/internal/ ../protos/graph.proto
-$DIR/../thirdparty/grpc/bins/opt/protobuf/protoc -I ../protos/ --python_out=$DIR/../lib/python/ray/internal/ ../protos/types.proto
