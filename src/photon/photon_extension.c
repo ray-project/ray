2ea619b4d0b422bfaa63df132e8cd3b551038123
Robert Nishihara <robertnishihara@gmail.com>
2016-10-30 22:58:28 -0700

    Fix formatting in photon_extension.c

--- src/photon/photon_extension.c
+++ src/photon/photon_extension.c
@@ -13,7 +13,8 @@ typedef struct {
 } PyPhotonClient;
 // clang-format on
 
-static int PyPhotonClient_init(PyPhotonClient *self, PyObject *args,
+static int PyPhotonClient_init(PyPhotonClient *self,
+                               PyObject *args,
                                PyObject *kwds) {
   char *socket_name;
   if (!PyArg_ParseTuple(args, "s", &socket_name)) {
@@ -24,8 +25,8 @@ static int PyPhotonClient_init(PyPhotonClient *self, PyObject *args,
 }
 
 static void PyPhotonClient_dealloc(PyPhotonClient *self) {
-  free(((PyPhotonClient *)self)->photon_connection);
-  Py_TYPE(self)->tp_free((PyObject *)self);
+  free(((PyPhotonClient *) self)->photon_connection);
+  Py_TYPE(self)->tp_free((PyObject *) self);
 }
 
 static PyObject *PyPhotonClient_submit(PyObject *self, PyObject *args) {
@@ -33,8 +34,8 @@ static PyObject *PyPhotonClient_submit(PyObject *self, PyObject *args) {
   if (!PyArg_ParseTuple(args, "O", &py_task)) {
     return NULL;
   }
-  photon_submit(((PyPhotonClient *)self)->photon_connection,
-                ((PyTask *)py_task)->spec);
+  photon_submit(((PyPhotonClient *) self)->photon_connection,
+                ((PyTask *) py_task)->spec);
   Py_RETURN_NONE;
 }
 
@@ -44,59 +45,59 @@ static PyObject *PyPhotonClient_get_task(PyObject *self) {
   /* Drop the global interpreter lock while we get a task because
    * photon_get_task may block for a long time. */
   Py_BEGIN_ALLOW_THREADS
-  task_spec = photon_get_task(((PyPhotonClient *)self)->photon_connection);
+  task_spec = photon_get_task(((PyPhotonClient *) self)->photon_connection);
   Py_END_ALLOW_THREADS
   return PyTask_make(task_spec);
 }
 // clang-format on
 
 static PyMethodDef PyPhotonClient_methods[] = {
-    {"submit", (PyCFunction)PyPhotonClient_submit, METH_VARARGS,
+    {"submit", (PyCFunction) PyPhotonClient_submit, METH_VARARGS,
      "Submit a task to the local scheduler."},
-    {"get_task", (PyCFunction)PyPhotonClient_get_task, METH_NOARGS,
+    {"get_task", (PyCFunction) PyPhotonClient_get_task, METH_NOARGS,
      "Get a task from the local scheduler."},
     {NULL} /* Sentinel */
 };
 
 static PyTypeObject PyPhotonClientType = {
-    PyObject_HEAD_INIT(NULL) 0,         /* ob_size */
-    "photon.PhotonClient",              /* tp_name */
-    sizeof(PyPhotonClient),             /* tp_basicsize */
-    0,                                  /* tp_itemsize */
-    (destructor)PyPhotonClient_dealloc, /* tp_dealloc */
-    0,                                  /* tp_print */
-    0,                                  /* tp_getattr */
-    0,                                  /* tp_setattr */
-    0,                                  /* tp_compare */
-    0,                                  /* tp_repr */
-    0,                                  /* tp_as_number */
-    0,                                  /* tp_as_sequence */
-    0,                                  /* tp_as_mapping */
-    0,                                  /* tp_hash */
-    0,                                  /* tp_call */
-    0,                                  /* tp_str */
-    0,                                  /* tp_getattro */
-    0,                                  /* tp_setattro */
-    0,                                  /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT,                 /* tp_flags */
-    "PhotonClient object",              /* tp_doc */
-    0,                                  /* tp_traverse */
-    0,                                  /* tp_clear */
-    0,                                  /* tp_richcompare */
-    0,                                  /* tp_weaklistoffset */
-    0,                                  /* tp_iter */
-    0,                                  /* tp_iternext */
-    PyPhotonClient_methods,             /* tp_methods */
-    0,                                  /* tp_members */
-    0,                                  /* tp_getset */
-    0,                                  /* tp_base */
-    0,                                  /* tp_dict */
-    0,                                  /* tp_descr_get */
-    0,                                  /* tp_descr_set */
-    0,                                  /* tp_dictoffset */
-    (initproc)PyPhotonClient_init,      /* tp_init */
-    0,                                  /* tp_alloc */
-    PyType_GenericNew,                  /* tp_new */
+    PyObject_HEAD_INIT(NULL) 0,          /* ob_size */
+    "photon.PhotonClient",               /* tp_name */
+    sizeof(PyPhotonClient),              /* tp_basicsize */
+    0,                                   /* tp_itemsize */
+    (destructor) PyPhotonClient_dealloc, /* tp_dealloc */
+    0,                                   /* tp_print */
+    0,                                   /* tp_getattr */
+    0,                                   /* tp_setattr */
+    0,                                   /* tp_compare */
+    0,                                   /* tp_repr */
+    0,                                   /* tp_as_number */
+    0,                                   /* tp_as_sequence */
+    0,                                   /* tp_as_mapping */
+    0,                                   /* tp_hash */
+    0,                                   /* tp_call */
+    0,                                   /* tp_str */
+    0,                                   /* tp_getattro */
+    0,                                   /* tp_setattro */
+    0,                                   /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT,                  /* tp_flags */
+    "PhotonClient object",               /* tp_doc */
+    0,                                   /* tp_traverse */
+    0,                                   /* tp_clear */
+    0,                                   /* tp_richcompare */
+    0,                                   /* tp_weaklistoffset */
+    0,                                   /* tp_iter */
+    0,                                   /* tp_iternext */
+    PyPhotonClient_methods,              /* tp_methods */
+    0,                                   /* tp_members */
+    0,                                   /* tp_getset */
+    0,                                   /* tp_base */
+    0,                                   /* tp_dict */
+    0,                                   /* tp_descr_get */
+    0,                                   /* tp_descr_set */
+    0,                                   /* tp_dictoffset */
+    (initproc) PyPhotonClient_init,      /* tp_init */
+    0,                                   /* tp_alloc */
+    PyType_GenericNew,                   /* tp_new */
 };
 
 static PyMethodDef photon_methods[] = {
@@ -125,13 +126,13 @@ PyMODINIT_FUNC initlibphoton(void) {
                      "A module for the local scheduler.");
 
   Py_INCREF(&PyTaskType);
-  PyModule_AddObject(m, "Task", (PyObject *)&PyTaskType);
+  PyModule_AddObject(m, "Task", (PyObject *) &PyTaskType);
 
   Py_INCREF(&PyObjectIDType);
-  PyModule_AddObject(m, "ObjectID", (PyObject *)&PyObjectIDType);
+  PyModule_AddObject(m, "ObjectID", (PyObject *) &PyObjectIDType);
 
   Py_INCREF(&PyPhotonClientType);
-  PyModule_AddObject(m, "PhotonClient", (PyObject *)&PyPhotonClientType);
+  PyModule_AddObject(m, "PhotonClient", (PyObject *) &PyPhotonClientType);
 
   char photon_error[] = "photon.error";
   PhotonError = PyErr_NewException(photon_error, NULL, NULL);
