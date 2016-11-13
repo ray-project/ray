6f9326baa2122ab04793dc368f30cb3e2dfa3695
Robert Nishihara <robertnishihara@gmail.com>
2016-11-12 19:37:20 -0800

    Install python package dependencies through setup.py. (#39)
    * Install python package dependencies through setup.py.
    
    * Do pip installs without sudo.
    
    * Clearer instructions for running tests.
    
    * Wording.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -15,7 +15,12 @@ else
   exit 1
 fi
 
-if [[ $platform == "macosx" ]]; then
+if [[ $platform == "linux" ]]; then
+  # These commands must be kept in sync with the installation instructions.
+  sudo apt-get update
+  sudo apt-get install -y cmake build-essential autoconf curl libtool python-dev python-numpy python-pip libboost-all-dev unzip
+  sudo pip install funcsigs colorama psutil redis
+elif [[ $platform == "macosx" ]]; then
   # check that brew is installed
   which -s brew
   if [[ $? != 0 ]]; then
@@ -25,20 +30,11 @@ if [[ $platform == "macosx" ]]; then
     echo "Updating brew."
     brew update
   fi
-fi
-
-if [[ $platform == "linux" ]]; then
   # These commands must be kept in sync with the installation instructions.
-  sudo apt-get update
-  sudo apt-get install -y git cmake build-essential autoconf curl libtool python-dev python-numpy python-pip libboost-all-dev unzip
-  sudo pip install funcsigs colorama psutil redis
-  sudo pip install --upgrade git+git://github.com/cloudpipe/cloudpickle.git@0d225a4695f1f65ae1cbb2e0bbc145e10167cce4  # We use the latest version of cloudpickle because it can serialize named tuples.
-elif [[ $platform == "macosx" ]]; then
-  # These commands must be kept in sync with the installation instructions.
-  brew install git cmake automake autoconf libtool boost
+  brew install cmake automake autoconf libtool boost
   sudo easy_install pip
   sudo pip install numpy funcsigs colorama psutil redis --ignore-installed six
-  sudo pip install --upgrade git+git://github.com/cloudpipe/cloudpickle.git@0d225a4695f1f65ae1cbb2e0bbc145e10167cce4  # We use the latest version of cloudpickle because it can serialize named tuples.
 fi
 
+sudo pip install --upgrade git+git://github.com/cloudpipe/cloudpickle.git@0d225a4695f1f65ae1cbb2e0bbc145e10167cce4  # We use the latest version of cloudpickle because it can serialize named tuples.
 sudo pip install --upgrade --verbose git+git://github.com/ray-project/numbuf.git@d1974afbab9f0f1bcf8af15a8c476d868ad31aff
