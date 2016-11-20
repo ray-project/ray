b97df6d5663f53e2a84c3c5e7306b8b9604bbdeb
Philipp Moritz <pcmoritz@gmail.com>
2016-11-20 13:37:45 -0800

    fix tests

--- src/plasma/test/run_client_tests.sh
+++ src/plasma/test/run_client_tests.sh
@@ -1,12 +1,17 @@
+../common/thirdparty/redis-3.2.3/src/redis-server &
+sleep 1
+# flush the redis server
+../common/thirdparty/redis-3.2.3/src/redis-cli flushall &
+sleep 1
 ./build/plasma_store -s /tmp/store1 &
 ./build/plasma_manager -m /tmp/manager1 -s /tmp/store1 -h 127.0.0.1 -p 11111 -r 127.0.0.1:6379 &
 ./build/plasma_store -s /tmp/store2 &
 ./build/plasma_manager -m /tmp/manager2 -s /tmp/store2 -h 127.0.0.1 -p 22222 -r 127.0.0.1:6379 &
-../common/thirdparty/redis-3.2.3/src/redis-server &
 sleep 1
 ./build/client_tests
-kill %1
-kill %2
 kill %3
+kill %2
+kill %5
 kill %4
+kill %1
 
