1ca0ad43046f61f3528e770051fa79f1dbf2823d
Robert Nishihara <robertnishihara@gmail.com>
2016-11-21 14:59:59 -0800

    Add documentation.

--- src/common/thirdparty/build-redis.sh
+++ src/common/thirdparty/build-redis.sh
@@ -5,6 +5,9 @@ set -e
 
 if [ ! -f redis/src/redis-server ]; then
   redis_vname="redis-3.2.3"
+  # This check is to make sure the tarball has been fully extracted. The only
+  # relevant bit about redis/utils/whatisdoing.sh is that it is one of the last
+  # files in the tarball.
   if [ ! -f redis/utils/whatisdoing.sh ]; then
     mkdir -p "./redis" && wget -O- "http://download.redis.io/releases/$redis_vname.tar.gz" | tar xvz --strip-components=1 -C "./redis"
   fi
