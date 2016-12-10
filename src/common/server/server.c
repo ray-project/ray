b826f08a0d60a18e71d97c4137c51c810531b4a8
Philipp Moritz <pcmoritz@gmail.com>
2016-12-09 16:23:05 -0800

    temp commit

--- src/common/server/server.c
+++ src/common/server/server.c
@@ -1,9 +1,12 @@
 #include "redismodule.h"
 
-int HelloSimple_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+int ObjectTableAdd_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   REDISMODULE_NOT_USED(argv);
   REDISMODULE_NOT_USED(argc);
-  RedisModule_ReplyWithLongLong(ctx,RedisModule_GetSelectedDb(ctx));
+  RedisModuleKey *key;
+  key = RedisModule_OpenKey(ctx,argv[1],REDISMODULE_READ | REDISMODULE_WRITE);
+  RedisModule_CloseKey(key);
+  RedisModule_ReplyWithLongLong(ctx, RedisModule_GetSelectedDb(ctx));
   return REDISMODULE_OK;
 }
 
@@ -13,11 +16,11 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
   REDISMODULE_NOT_USED(argv);
   REDISMODULE_NOT_USED(argc);
 
-  if (RedisModule_Init(ctx, "helloworld", 1, REDISMODULE_APIVER_1) == REDISMODULE_ERR) {
+  if (RedisModule_Init(ctx, "ray", 1, REDISMODULE_APIVER_1) == REDISMODULE_ERR) {
     return REDISMODULE_ERR;
   }
 
-  if (RedisModule_CreateCommand(ctx, "hello.simple", HelloSimple_RedisCommand,"readonly",0,0,0) == REDISMODULE_ERR) {
+  if (RedisModule_CreateCommand(ctx, "ray.object_table_add", ObjectTableAdd_RedisCommand, "readonly", 0, 0, 0) == REDISMODULE_ERR) {
     return REDISMODULE_ERR;
   }
 
