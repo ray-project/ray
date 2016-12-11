f440a3baf6f45adcf75ecb7f2fbd23b2b1ad90ba
Philipp Moritz <pcmoritz@gmail.com>
2016-12-10 22:37:13 -0800

    fix segfault and clean up code

--- src/common/server/server.c
+++ src/common/server/server.c
@@ -8,6 +8,24 @@
 #define OBJECT_TABLE_PREFIX "OT:"
 #define SUBSCRIPTION_TABLE_PREFIX "ST:"
 
+#define CHECK_ERROR(STATUS, MESSAGE)                   \
+  if ((STATUS) == REDISMODULE_ERR) {                   \
+    return RedisModule_ReplyWithError(ctx, (MESSAGE)); \
+  }
+
+
+RedisModuleKey *OpenPrefixedKey(RedisModuleCtx *ctx,
+                                const char *prefix,
+                                RedisModuleString* keyname,
+                                int mode) {
+  size_t length;
+  const char *value = RedisModule_StringPtrLen(keyname, &length);
+  RedisModuleString *prefixed_keyname = RedisModule_CreateStringPrintf(ctx, "%s%*.*s", prefix, length, length, value);
+  RedisModuleKey *key = RedisModule_OpenKey(ctx, prefixed_keyname, mode);
+  RedisModule_FreeString(ctx, prefixed_keyname);
+  return key;
+}
+
 /* The object table has the following format:
  * "obj:(object id)" "hash" (hash of the object)
  * "obj:(object id)" "data_size" (size of the object)
@@ -16,27 +34,21 @@
 
 int ObjectTableLookup_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   REDISMODULE_NOT_USED(argc);
+  RedisModuleKey *key = OpenPrefixedKey(ctx, OBJECT_TABLE_PREFIX, argv[1], REDISMODULE_READ);
 
-  RedisModuleString *object_id = argv[1];
-  size_t object_id_length;
-  const char *object_id_value = RedisModule_StringPtrLen(object_id, &object_id_length);
-  RedisModuleString *object_table_key = RedisModule_CreateStringPrintf(ctx, OBJECT_TABLE_PREFIX "%*.*s", object_id_length, object_id_length, object_id_value);
+  CHECK_ERROR(RedisModule_ZsetFirstInScoreRange(key, REDISMODULE_NEGATIVE_INFINITE, REDISMODULE_POSITIVE_INFINITE, 1, 1),
+              "Unable to initialize zset iterator");
 
-  RedisModuleKey *key = RedisModule_OpenKey(ctx, object_table_key, REDISMODULE_READ);
-
-  int status = RM_ZsetFirstInScoreRange(key, REDISMODULE_NEGATIVE_INFINITE, REDISMODULE_POSITIVE_INFINITE, 1, 1);
-  if (status == REDISMODULE_ERR) {
-    return RedisModule_ReplyWithError(ctx, "Unable to initialize zset iterator");
-  }
-
-  /* TODO(swang): Seg fault when accessing first. */
-  RedisModuleString *first = RM_ZsetRangeCurrentElement(key, NULL);
-  const char *first_str = RedisModule_StringPtrLen(first, NULL);
-  printf("%s", first_str);
-  if (first == NULL) {
-    return RedisModule_ReplyWithError(ctx, "Null");
-  }
-  RedisModule_ReplyWithString(ctx, first);
+  RedisModule_ReplyWithArray(ctx, REDISMODULE_POSTPONED_ARRAY_LEN);
+  int num_results = 0;
+  int has_next;
+  do {
+    RedisModuleString *curr = RedisModule_ZsetRangeCurrentElement(key, NULL);
+    RedisModule_ReplyWithString(ctx, curr);
+    has_next = RedisModule_ZsetRangeNext(key);
+    num_results += 1;
+  } while (has_next);
+  RedisModule_ReplySetArrayLength(ctx, num_results);
 
   return REDISMODULE_OK;
 }
@@ -52,51 +64,26 @@ int ObjectTableAdd_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, i
   RedisModuleString *new_hash = argv[3];
   RedisModuleString *manager = argv[4];
 
-  /*
-  size_t object_id_length;
-  const char *object_id_value = RedisModule_StringPtrLen(object_id, &object_id_length);
-  char *object_key_value = malloc(object_id_length + PREFIX_LENGTH);
-  memcpy(object_key_value, OBJECT_INFO_PREFIX, PREFIX_LENGTH);
-  memcpy(object_key_value + PREFIX_LENGTH, object_id_value, object_id_length);
-  RedisModuleString *object_key = RedisModule_CreateString(ctx, object_key_value, object_id_length + PREFIX_LENGTH);
-  */
-
-  size_t object_id_length;
-  const char *object_id_value = RedisModule_StringPtrLen(object_id, &object_id_length);
-  RedisModuleString *object_info_key = RedisModule_CreateStringPrintf(ctx, OBJECT_INFO_PREFIX "%*.*s", object_id_length, object_id_length, object_id_value);
-  RedisModuleString *object_table_key = RedisModule_CreateStringPrintf(ctx, OBJECT_TABLE_PREFIX "%*.*s", object_id_length, object_id_length, object_id_value);
-
-  /* TODO(pcm): Free the object_info_key and object_table_key. */
-
   RedisModuleKey *key;
-  key = RedisModule_OpenKey(ctx, object_info_key, REDISMODULE_READ | REDISMODULE_WRITE);
+  key = OpenPrefixedKey(ctx, OBJECT_INFO_PREFIX, object_id, REDISMODULE_READ | REDISMODULE_WRITE);
+
   int keytype = RedisModule_KeyType(key);
   /* Check if this object was already registered and if the hashes agree. */
   if (keytype != REDISMODULE_KEYTYPE_EMPTY) {
     RedisModuleString *existing_hash;
     RedisModule_HashGet(key, REDISMODULE_HASH_CFIELDS, "hash", &existing_hash, NULL);
-    size_t existing_hash_length;
-    const char *existing_hash_value = RedisModule_StringPtrLen(existing_hash, &existing_hash_length);
-    size_t new_hash_length;
-    const char *new_hash_value = RedisModule_StringPtrLen(new_hash, &new_hash_length);
-    if (existing_hash_length != new_hash_length || memcmp(existing_hash_value, new_hash_value, new_hash_length) != 0) {
+    if (RedisModule_StringCompare(existing_hash, new_hash) != 0) {
       RedisModule_CloseKey(key);
       return RedisModule_ReplyWithError(ctx, "object with this id already present with different hash");
     }
   }
-  
-  // 
-  // if (keytype == REDISMODULE_KEYTYPE_EMPTY) {
-  //   RedisModule_CloseKey(key);
-  //   return RedisModule_ReplyWithError(ctx, REDISMODULE_ERRORMSG_WRONGTYPE);
-  // }
-  // RedisModule_HashSet(key, REDISMODULE_HASH_NONE, argv[2], argv[3]);
+
   RedisModule_HashSet(key, REDISMODULE_HASH_CFIELDS, "hash", new_hash, NULL);
   RedisModule_HashSet(key, REDISMODULE_HASH_CFIELDS, "data_size", data_size, NULL);
 
   RedisModuleKey *table_key;
-  table_key = RedisModule_OpenKey(ctx, object_table_key, REDISMODULE_READ | REDISMODULE_WRITE);
-  
+  table_key = OpenPrefixedKey(ctx, OBJECT_TABLE_PREFIX, object_id, REDISMODULE_READ | REDISMODULE_WRITE);
+
   /* Sets are not implemented yet, so we use ZSETs instead. */
   RedisModule_ZsetAdd(table_key, 0.0, manager, NULL);
   RedisModule_CloseKey(key);
@@ -105,37 +92,31 @@ int ObjectTableAdd_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, i
   return REDISMODULE_OK;
 }
 
-/* This function will get back to the client when the object appears in the
- * object table */
-int ObjectTableSubscribe_ReplyFunction(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-  return RedisModule_ReplyWithSimpleString(ctx,"Hello!");
-}
-
 int ObjectTableSubscribe_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-  RedisModuleString *object_id = argv[1];
-  size_t object_id_length;
-  const char *object_id_value = RedisModule_StringPtrLen(object_id, &object_id_length);
-  RedisModuleString *object_subscription_key = RedisModule_CreateStringPrintf(ctx, SUBSCRIPTION_TABLE_PREFIX "%*.*s", object_id_length, object_id_length, object_id_value);
-
-  RedisModuleBlockedClient *blocker = RedisModule_BlockClient(ctx, ObjectTableSubscribe_ReplyFunction, NULL, NULL, 1000000000);
-  RedisModuleKey *subscription_key;
-  subscription_key = RedisModule_OpenKey(ctx, object_subscription_key, REDISMODULE_READ | REDISMODULE_WRITE);
-  /* Clean this up. */
-  RedisModuleString *blocker_pointer = RedisModule_CreateStringFromLongLong(ctx, (long long) blocker);
-  /* Sets are not implemented yet, so we use ZSETs instead. */
-  RedisModule_ZsetAdd(subscription_key, 0.0, blocker_pointer, NULL);
-  RedisModule_CloseKey(subscription_key);
+  REDISMODULE_NOT_USED(ctx);
+  REDISMODULE_NOT_USED(argv);
+  REDISMODULE_NOT_USED(argc);
+  return REDISMODULE_OK;
 }
 
 int ObjectInfoSubscribe_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+  REDISMODULE_NOT_USED(ctx);
+  REDISMODULE_NOT_USED(argv);
+  REDISMODULE_NOT_USED(argc);
   return REDISMODULE_OK;
 }
 
 int ResultTableAdd_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+  REDISMODULE_NOT_USED(ctx);
+  REDISMODULE_NOT_USED(argv);
+  REDISMODULE_NOT_USED(argc);
   return REDISMODULE_OK;
 }
 
 int ResultTableLookup_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+  REDISMODULE_NOT_USED(ctx);
+  REDISMODULE_NOT_USED(argv);
+  REDISMODULE_NOT_USED(argc);
   return REDISMODULE_OK;
 }
 
