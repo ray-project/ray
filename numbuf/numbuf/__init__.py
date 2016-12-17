30903f0c19673490a9c64f7f1835701dcbfd1a7e
Robert Nishihara <robertnishihara@gmail.com>
2016-12-16 23:08:53 -0800

    Print helpful error message when libgcc error occurs.

--- numbuf/numbuf/__init__.py
+++ numbuf/numbuf/__init__.py
@@ -2,4 +2,18 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from numbuf.libnumbuf import *
+# See https://github.com/ray-project/ray/issues/131.
+helpful_message = """
+
+If you are using Anaconda, try fixing this problem by running:
+
+  conda install libgcc
+"""
+
+try:
+  from numbuf.libnumbuf import *
+except ImportError as e:
+  if not hasattr(e, "msg") or not isinstance(e.msg, str) or not "GLIBCXX" in e.msg:
+    raise
+  e.msg += helpful_message
+  raise
