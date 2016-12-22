8b83cab786210185cc610b321a2dd07bc28f7beb
Robert Nishihara <robertnishihara@gmail.com>
2016-12-21 23:31:05 -0800

    Minor changes to Python test and factor out some common code.

--- src/global_scheduler/test/test.py
+++ src/global_scheduler/test/test.py
@@ -16,27 +16,21 @@ import unittest
 import global_scheduler
 import photon
 import plasma
+from plasma.utils import random_object_id, generate_metadata, write_to_data_buffer, create_object_with_id, create_object
 
 USE_VALGRIND = False
 PLASMA_STORE_MEMORY = 1000000000
 ID_SIZE = 20
 
-# These constants must match the schedulign state enum in task.h.
+# These constants must match the scheduling state enum in task.h.
 TASK_STATUS_WAITING = 1
 TASK_STATUS_SCHEDULED = 2
 TASK_STATUS_RUNNING = 4
 TASK_STATUS_DONE = 8
 
-# DB_CLIENT_PREFIX is an implementation detail of ray_redis_module.c, so
-# this must be kept in sync with that file.
+# DB_CLIENT_PREFIX is an implementation detail of ray_redis_module.c, so this
+# must be kept in sync with that file.
 DB_CLIENT_PREFIX = "CL:"
-
-# def random_object_id():
-#   return photon.ObjectID(np.random.bytes(ID_SIZE))
-# object_ids = [random_object_id() for i in range(256)]
-
-def random_object_id():
-  return np.random.bytes(ID_SIZE)
 
 def random_task_id():
   return photon.ObjectID(np.random.bytes(ID_SIZE))
@@ -46,36 +40,6 @@ def random_function_id():
 
 def new_port():
   return random.randint(10000, 65535)
-
-def generate_metadata(length):
-  metadata_buffer = bytearray(length)
-  if length > 0:
-    metadata_buffer[0] = random.randint(0, 255)
-    metadata_buffer[-1] = random.randint(0, 255)
-    for _ in range(100):
-      metadata_buffer[random.randint(0, length - 1)] = random.randint(0, 255)
-  return metadata_buffer
-
-def write_to_data_buffer(buff, length):
-  if length > 0:
-    buff[0] = chr(random.randint(0, 255))
-    buff[-1] = chr(random.randint(0, 255))
-    for _ in range(100):
-      buff[random.randint(0, length - 1)] = chr(random.randint(0, 255))
-
-def create_object_with_id(client, object_id, data_size, metadata_size, seal=True):
-  metadata = generate_metadata(metadata_size)
-  memory_buffer = client.create(object_id, data_size, metadata)
-  write_to_data_buffer(memory_buffer, data_size)
-  if seal:
-    client.seal(object_id)
-  return memory_buffer, metadata
-
-def create_object(client, data_size, metadata_size, seal=True):
-  object_id = random_object_id()
-  memory_buffer, metadata = create_object_with_id(client, object_id, data_size, metadata_size, seal=seal)
-  return object_id, memory_buffer, metadata
-
 
 class TestGlobalScheduler(unittest.TestCase):
 
@@ -87,27 +51,24 @@ class TestGlobalScheduler(unittest.TestCase):
     assert os.path.isfile(redis_module)
     node_ip_address = "127.0.0.1"
     redis_port = new_port()
-    #redis_port = 6379 #default_port=6379
     redis_address = "{}:{}".format(node_ip_address, redis_port)
     self.redis_process = subprocess.Popen([redis_path, "--port", str(redis_port), "--loglevel", "warning", "--loadmodule", redis_module])
     time.sleep(0.1)
     # Create a Redis client.
     self.redis_client = redis.StrictRedis(host=node_ip_address, port=redis_port)
-    #time.sleep(0.5)
     # Start the global scheduler.
     self.p1 = global_scheduler.start_global_scheduler(redis_address, use_valgrind=USE_VALGRIND)
-    #time.sleep(0.5)
     # Start the Plasma store.
     plasma_store_name, self.p2 = plasma.start_plasma_store()
     # Start the Plasma manager.
     plasma_manager_name, self.p3, plasma_manager_port = plasma.start_plasma_manager(plasma_store_name, redis_address)
     self.plasma_address = "{}:{}".format(node_ip_address, plasma_manager_port)
-    self.plasmaclient = plasma.PlasmaClient(plasma_store_name, plasma_manager_name)
+    self.plasma_client = plasma.PlasmaClient(plasma_store_name, plasma_manager_name)
     # Start the local scheduler.
     local_scheduler_name, self.p4 = photon.start_local_scheduler(
-        plasma_store_name, 
-        plasma_manager_name=plasma_manager_name, 
-        plasma_address=self.plasma_address, 
+        plasma_store_name,
+        plasma_manager_name=plasma_manager_name,
+        plasma_address=self.plasma_address,
         redis_address=redis_address)
     # Connect to the scheduler.
     self.photon_client = photon.PhotonClient(local_scheduler_name)
@@ -118,7 +79,7 @@ class TestGlobalScheduler(unittest.TestCase):
     self.assertEqual(self.p2.poll(), None)
     self.assertEqual(self.p3.poll(), None)
     self.assertEqual(self.p4.poll(), None)
-    #self.assertEqual(self.redis_process.poll(), None)
+    self.assertEqual(self.redis_process.poll(), None)
 
     # Kill the global scheduler.
     if USE_VALGRIND:
@@ -134,56 +95,57 @@ class TestGlobalScheduler(unittest.TestCase):
     # after we kill the global scheduler.
     self.redis_process.kill()
 
-  def get_plasmamgr_id(self):
-    ''' iterates over all the client table keys, gets the db_client_id for the client
-        with client_type matching plasma_manager. Strips the client table prefix.
-        Returns None if plasma_manager client not found.
-        Note that it returns db_client id for the first encountered plasma manager.
-        TODO(atumanov): write a separate function to get all plasma manager client IDs
-    '''
+  def get_plasma_manager_id(self):
+    """Get the db_client_id with client_type equal to plasma_manager.
+
+    Iterates over all the client table keys, gets the db_client_id for the
+    client with client_type matching plasma_manager. Strips the client table
+    prefix. TODO(atumanov): write a separate function to get all plasma manager
+    client IDs.
+
+    Returns:
+      The db_client_id if one is found and otherwise None.
+    """
     db_client_id = None
 
-    cli_lst = self.redis_client.keys("{}*".format(DB_CLIENT_PREFIX))
-    for client_id in cli_lst:
-        rediscmdstr = b'HGET {} client_type'.format(client_id)
-        response = self.redis_client.hget(client_id, b'client_type')
-        if response == "plasma_manager":
-            db_client_id = client_id
-            break
+    client_list = self.redis_client.keys("{}*".format(DB_CLIENT_PREFIX))
+    for client_id in client_list:
+      rediscmdstr = b"HGET {} client_type".format(client_id)
+      response = self.redis_client.hget(client_id, b"client_type")
+      if response == "plasma_manager":
+        db_client_id = client_id
+        break
 
     return db_client_id
 
-  def test_redisonly_singletask(self):
-    ''' tests global scheduler functionality by interaction with Redis
-        and checking task state transitions in Redis only
-        TODO(atumanov): implement
-    '''
+  def test_redis_only_single_task(self):
+    """
+    Tests global scheduler functionality by interacting with Redis and checking
+    task state transitions in Redis only. TODO(atumanov): implement.
+    """
     # Check precondition for this test:
     # There should be three db clients, the global scheduler, the local
     # scheduler, and the plasma manager.
     self.assertEqual(len(self.redis_client.keys("{}*".format(DB_CLIENT_PREFIX))), 3)
-    db_client_id = self.get_plasmamgr_id()
+    db_client_id = self.get_plasma_manager_id()
     assert(db_client_id != None)
     assert(db_client_id.startswith("CL:"))
-    db_client_id = db_client_id[len('CL:'):] #remove the CL: prefix
+    db_client_id = db_client_id[len("CL:"):] # Remove the CL: prefix.
 
-    #self.redis_client.execute_command("RAY.OBJECT_TABLE_ADD", object_dep, data_size, "hash1", db_client_id)
-
-  def test_integration_singletask(self):
-
+  def test_integration_single_task(self):
     # There should be three db clients, the global scheduler, the local
     # scheduler, and the plasma manager.
     self.assertEqual(len(self.redis_client.keys("{}*".format(DB_CLIENT_PREFIX))), 3)
 
-    num_return_vals = [0,1,2,3,5,10]
+    num_return_vals = [0, 1, 2, 3, 5, 10]
     # There should not be anything else in Redis yet.
     self.assertEqual(len(self.redis_client.keys("*")), 3)
-    #insert the object into Redis
+    # Insert the object into Redis.
     data_size = 0xf1f0
     metadata_size = 0x40
-    object_dep, memory_buffer, metadata = create_object(self.plasmaclient, data_size, metadata_size, seal=True)
+    object_dep, memory_buffer, metadata = create_object(self.plasma_client, data_size, metadata_size, seal=True)
 
-    #sleep before submitting task to photon
+    # Sleep before submitting task to photon.
     time.sleep(0.1)
     # Submit a task to Redis.
     task = photon.Task(random_function_id(), [photon.ObjectID(object_dep)], num_return_vals[0], random_task_id(), 0)
@@ -202,33 +164,32 @@ class TestGlobalScheduler(unittest.TestCase):
         if task_status == TASK_STATUS_SCHEDULED:
           break
         else:
-            print(task_status)
+          print(task_status)
       print("The task has not been scheduled yet, trying again.")
       num_retries -= 1
       time.sleep(1)
 
     if num_retries <= 0 and task_status != TASK_STATUS_SCHEDULED:
-        #failed to submit and schedule a single task -- bail
-        self.tearDown()
-        sys.exit(1)
+      # Failed to submit and schedule a single task -- bail.
+      self.tearDown()
+      sys.exit(1)
 
-
-  def integration_manytasks_helper(self, timesync = True):
+  def integration_many_tasks_helper(self, timesync=True):
     # There should be three db clients, the global scheduler, the local
     # scheduler, and the plasma manager.
     self.assertEqual(len(self.redis_client.keys("{}*".format(DB_CLIENT_PREFIX))), 3)
-    num_return_vals = [0,1,2,3,5,10]
+    num_return_vals = [0, 1, 2, 3, 5, 10]
 
     # Submit a bunch of tasks to Redis.
     num_tasks = 1000
     for _ in range(num_tasks):
-      #create a new object for each task
-      data_size = np.random.randint(1<<20) #upto 1MB
-      metadata_size = np.random.randint(1<<10) #upto 1KB
-      object_dep, memory_buffer, metadata = create_object(self.plasmaclient, data_size, metadata_size, seal=True)
+      # Create a new object for each task.
+      data_size = np.random.randint(1 << 20)
+      metadata_size = np.random.randint(1 << 10)
+      object_dep, memory_buffer, metadata = create_object(self.plasma_client, data_size, metadata_size, seal=True)
       if timesync:
-        #give 10ms for object info handler to fire
-        time.sleep(0.010) #10ms (yields cpu)
+        # Give 10ms for object info handler to fire (long enough to yield CPU).
+        time.sleep(0.010)
       task = photon.Task(random_function_id(), [photon.ObjectID(object_dep)], num_return_vals[0], random_task_id(), 0)
       self.photon_client.submit(task)
     # Check that there are the correct number of tasks in Redis and that they
@@ -238,7 +199,7 @@ class TestGlobalScheduler(unittest.TestCase):
     while num_retries > 0:
       task_entries = self.redis_client.keys("task*")
       self.assertLessEqual(len(task_entries), num_tasks)
-      #first, check if all tasks made it to Redis
+      # First, check if all tasks made it to Redis.
       if len(task_entries) == num_tasks:
         task_contents = [self.redis_client.hgetall(task_entries[i]) for i in range(len(task_entries))]
         task_statuses = [int(contents[b"state"]) for contents in task_contents]
@@ -248,26 +209,27 @@ class TestGlobalScheduler(unittest.TestCase):
         print("tasks in Redis = {}, tasks waiting = {}, tasks scheduled = {}, retries left = {}"
               .format(len(task_entries), num_tasks_waiting, num_tasks_done, num_retries))
         if all([status == TASK_STATUS_SCHEDULED for status in task_statuses]):
-          #we're done : pass
+          # We're done, so pass.
           break
       num_retries -= 1
-      time.sleep(.1)
+      time.sleep(0.1)
 
     if num_tasks_done != num_tasks:
-        # at least one of the tasks failed to schedule
-        self.tearDown()
-        sys.exit(2)
+      # At least one of the tasks failed to schedule.
+      self.tearDown()
+      sys.exit(2)
 
-  def test_integration_maytasks_handlersync(self):
-    self.integration_manytasks_helper(timesync = True)
+  def test_integration_many_tasks_handler_sync(self):
+    self.integration_many_tasks_helper(timesync=True)
 
-  def test_integration_maytasks(self):
-    #more realistic case: should handle out of order object and task notifications
-    self.integration_manytasks_helper(timesync = False)
+  def test_integration_many_tasks(self):
+    # More realistic case: should handle out of order object and task
+    # notifications.
+    self.integration_many_tasks_helper(timesync=False)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
-    # pop the argument so we don't mess with unittest's own argument parser
+    # Pop the argument so we don't mess with unittest's own argument parser.
     arg = sys.argv.pop()
     if arg == "valgrind":
       USE_VALGRIND = True
--- src/plasma/test/test.py
+++ src/plasma/test/test.py
@@ -16,41 +16,10 @@ import time
 import unittest
 
 import plasma
+from plasma.utils import random_object_id, generate_metadata, write_to_data_buffer, create_object_with_id, create_object
 
 USE_VALGRIND = False
 PLASMA_STORE_MEMORY = 1000000000
-
-def random_object_id():
-  return np.random.bytes(20)
-
-def generate_metadata(length):
-  metadata_buffer = bytearray(length)
-  if length > 0:
-    metadata_buffer[0] = random.randint(0, 255)
-    metadata_buffer[-1] = random.randint(0, 255)
-    for _ in range(100):
-      metadata_buffer[random.randint(0, length - 1)] = random.randint(0, 255)
-  return metadata_buffer
-
-def write_to_data_buffer(buff, length):
-  if length > 0:
-    buff[0] = chr(random.randint(0, 255))
-    buff[-1] = chr(random.randint(0, 255))
-    for _ in range(100):
-      buff[random.randint(0, length - 1)] = chr(random.randint(0, 255))
-
-def create_object_with_id(client, object_id, data_size, metadata_size, seal=True):
-  metadata = generate_metadata(metadata_size)
-  memory_buffer = client.create(object_id, data_size, metadata)
-  write_to_data_buffer(memory_buffer, data_size)
-  if seal:
-    client.seal(object_id)
-  return memory_buffer, metadata
-
-def create_object(client, data_size, metadata_size, seal=True):
-  object_id = random_object_id()
-  memory_buffer, metadata = create_object_with_id(client, object_id, data_size, metadata_size, seal=seal)
-  return object_id, memory_buffer, metadata
 
 def assert_get_object_equal(unit_test, client1, client2, object_id, memory_buffer=None, metadata=None):
   if memory_buffer is not None:
