bac4e1f1d546b27c6a0d1ed04fc75e894e8f57a6
Robert Nishihara <robertnishihara@gmail.com>
2016-12-29 23:08:43 -0800

    Only download arrow if not already present.

--- numbuf/thirdparty/download_thirdparty.sh
+++ numbuf/thirdparty/download_thirdparty.sh
@@ -7,6 +7,8 @@ set -e
 
 TP_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
-git clone https://github.com/pcmoritz/arrow.git "$TP_DIR/arrow"
+if [ ! -d $TP_DIR/arrow ]; then
+  git clone https://github.com/pcmoritz/arrow.git "$TP_DIR/arrow"
+fi
 cd "$TP_DIR/arrow"
 git checkout c88bd70c13cf16c07b840623cb466aa98d535be0
