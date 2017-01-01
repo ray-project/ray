8bb87a4f6b71f3a8a9c910332f1a0be041f9848e
Johann Schleier-Smith <jssmith@gmail.com>
2016-12-31 17:21:33 -0800

    updated Docker files (#171)
    * updated Docker files
    
    * single Docker RUN for apt-get installs and cleanup
    
    * stylistic cleanup

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-docker build -t ray-project/ray:devel docker/devel
-docker build -t ray-project/ray:deploy docker/deploy
-docker build -t ray-project/ray:examples docker/examples
+docker build -t ray-project/base-deps docker/base-deps
+
+tar --exclude './docker' -c . > ./docker/deploy/ray.tar
+docker build --no-cache -t ray-project/deploy docker/deploy
+rm ./docker/deploy/ray.tar
+docker build -t ray-project/examples docker/examples
