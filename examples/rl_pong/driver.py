ef02b4fa5febdbecb762d384882d52bc3dad5675
Robert Nishihara <robertnishihara@gmail.com>
2017-01-05 20:30:39 -0800

    Remove unused variable.

--- examples/rl_pong/driver.py
+++ examples/rl_pong/driver.py
@@ -16,7 +16,6 @@ batch_size = 10 # every how many episodes to do a param update?
 learning_rate = 1e-4
 gamma = 0.99 # discount factor for reward
 decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
-resume = False # resume from previous checkpoint?
 
 D = 80 * 80 # input dimensionality: 80x80 grid
 
