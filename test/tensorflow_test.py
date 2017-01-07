0efcaf482527df5ca73e932636f5e8aeb29da40d
Philipp Moritz <pcmoritz@gmail.com>
2017-01-07 01:07:28 -0800

    more tests

--- test/tensorflow_test.py
+++ test/tensorflow_test.py
@@ -17,12 +17,10 @@ class TensorFlowTest(unittest.TestCase):
     w = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
     b = tf.Variable(tf.zeros([1]))
     y = w * x_data + b
-
     loss = tf.reduce_mean(tf.square(y - y_data))
 
-    init = tf.global_variables_initializer()
     sess = tf.Session()
-    sess.run(init)
+    sess.run(tf.global_variables_initializer())
 
     variables = ray.experimental.TensorFlowVariables(loss, sess)
     weights = variables.get_weights()
@@ -32,6 +30,22 @@ class TensorFlowTest(unittest.TestCase):
 
     variables.set_weights(weights)
     self.assertEqual(weights, variables.get_weights())
+
+    w2 = tf.Variable(tf.random_uniform([1], -1.0, 1.0), name="w")
+    b2 = tf.Variable(tf.zeros([1]), name="b")
+    y2 = w2 * x_data + b2
+    loss2 = tf.reduce_mean(tf.square(y2 - y_data))
+
+    sess.run(tf.global_variables_initializer())
+
+    variables2 = ray.experimental.TensorFlowVariables(loss2, sess)
+    weights2 = variables2.get_weights()
+
+    for (name, val) in weights2.items():
+      weights2[name] += 2.0
+
+    variables2.set_weights(weights2)
+    self.assertEqual(weights2, variables2.get_weights())
 
     ray.worker.cleanup()
 
