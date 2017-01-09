e0faa43815518b770fe7353e2b76f4b293978c16
Philipp Moritz <pcmoritz@gmail.com>
2017-01-08 21:26:02 -0800

    bypass python when storing objects into the object store

--- src/plasma/plasma_extension.c
+++ src/plasma/plasma_extension.c
@@ -10,26 +10,7 @@
 PyObject *PlasmaOutOfMemoryError;
 PyObject *PlasmaObjectExistsError;
 
-static int PyObjectToPlasmaConnection(PyObject *object,
-                                      plasma_connection **conn) {
-  if (PyCapsule_IsValid(object, "plasma")) {
-    *conn = (plasma_connection *) PyCapsule_GetPointer(object, "plasma");
-    return 1;
-  } else {
-    PyErr_SetString(PyExc_TypeError, "must be a 'plasma' capsule");
-    return 0;
-  }
-}
-
-static int PyStringToUniqueID(PyObject *object, object_id *object_id) {
-  if (PyBytes_Check(object)) {
-    memcpy(&object_id->id[0], PyBytes_AsString(object), UNIQUE_ID_SIZE);
-    return 1;
-  } else {
-    PyErr_SetString(PyExc_TypeError, "must be a 20 character string");
-    return 0;
-  }
-}
+#include "plasma_extension.h"
 
 PyObject *PyPlasma_connect(PyObject *self, PyObject *args) {
   const char *store_socket_name;
