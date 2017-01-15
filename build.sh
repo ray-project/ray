63d7d2291f64fc396c9535ee7ee6e363ab317fe4
Philipp Moritz <pcmoritz@gmail.com>
2017-01-15 15:12:33 -0800

    add build.sh script

--- build.sh
+++ build.sh
@@ -16,10 +16,14 @@ else
   exit 1
 fi
 
-COMMON_DIR="$ROOT_DIR/src/common"
-PLASMA_DIR="$ROOT_DIR/src/plasma"
-PHOTON_DIR="$ROOT_DIR/src/photon"
-GLOBAL_SCHEDULER_DIR="$ROOT_DIR/src/global_scheduler"
+COMMON_BUILD_DIR="$ROOT_DIR/build/src/common"
+COMMON_SOURCE_DIR="$ROOT_DIR/src/common"
+PLASMA_BUILD_DIR="$ROOT_DIR/build/src/plasma"
+PLASMA_SOURCE_DIR="$ROOT_DIR/src/plasma"
+PHOTON_BUILD_DIR="$ROOT_DIR/build/src/photon"
+PHOTON_SOURCE_DIR="$ROOT_DIR/src/photon"
+GLOBAL_SCHEDULER_BUILD_DIR="$ROOT_DIR/build/src/global_scheduler"
+GLOBAL_SCHEDULER_SOURCE_DIR="$ROOT_DIR/src/global_scheduler"
 
 PYTHON_DIR="$ROOT_DIR/lib/python"
 PYTHON_COMMON_DIR="$PYTHON_DIR/common"
@@ -28,52 +32,26 @@ PYTHON_PHOTON_DIR="$PYTHON_DIR/photon"
 PYTHON_GLOBAL_SCHEDULER_DIR="$PYTHON_DIR/global_scheduler"
 
 # First clean up old build files.
-pushd "$COMMON_DIR"
-  make clean
-popd
-pushd "$PLASMA_DIR"
-  make clean
-popd
-pushd "$PHOTON_DIR"
-  make clean
-popd
-pushd "$GLOBAL_SCHEDULER_DIR"
-  make clean
-popd
+rm -rf "$ROOT_DIR/build"
+mkdir "$ROOT_DIR/build"
 
 # Now build everything.
-pushd "$COMMON_DIR"
+pushd "$ROOT_DIR/build"
+  cmake ..
   make
 popd
-cp "$COMMON_DIR/thirdparty/redis/src/redis-server" "$PYTHON_COMMON_DIR/thirdparty/redis/src/"
-cp "$COMMON_DIR/redis_module/ray_redis_module.so" "$PYTHON_COMMON_DIR/redis_module/ray_redis_module.so"
+cp "$COMMON_BUILD_DIR/thirdparty/redis/src/redis-server" "$PYTHON_COMMON_DIR/thirdparty/redis/src/"
+cp "$COMMON_BUILD_DIR/redis_module/libray_redis_module.so" "$PYTHON_COMMON_DIR/redis_module/ray_redis_module.so" # XXX
 
-pushd "$PLASMA_DIR"
-  make
-  pushd "$PLASMA_DIR/build"
-    cmake ..
-    make install
-  popd
-popd
-cp "$PLASMA_DIR/build/plasma_store" "$PYTHON_PLASMA_DIR/"
-cp "$PLASMA_DIR/build/plasma_manager" "$PYTHON_PLASMA_DIR/"
-cp "$PLASMA_DIR/plasma/plasma.py" "$PYTHON_PLASMA_DIR/"
-cp "$PLASMA_DIR/plasma/utils.py" "$PYTHON_PLASMA_DIR/"
-cp "$PLASMA_DIR/plasma/libplasma.so" "$PYTHON_PLASMA_DIR/"
+cp "$PLASMA_BUILD_DIR/plasma_store" "$PYTHON_PLASMA_DIR/"
+cp "$PLASMA_BUILD_DIR/plasma_manager" "$PYTHON_PLASMA_DIR/"
+cp "$PLASMA_BUILD_DIR/libplasma.so" "$PYTHON_PLASMA_DIR/"
+cp "$PLASMA_SOURCE_DIR/plasma/plasma.py" "$PYTHON_PLASMA_DIR/"
+cp "$PLASMA_SOURCE_DIR/plasma/utils.py" "$PYTHON_PLASMA_DIR/"
 
-pushd "$PHOTON_DIR"
-  make
-  pushd "$PHOTON_DIR/build"
-    cmake ..
-    make install
-  popd
-popd
-cp "$PHOTON_DIR/build/photon_scheduler" "$PYTHON_PHOTON_DIR/build/"
-cp "$PHOTON_DIR/photon/libphoton.so" "$PYTHON_PHOTON_DIR/photon/"
-cp "$PHOTON_DIR/photon/photon_services.py" "$PYTHON_PHOTON_DIR/photon/"
+cp "$PHOTON_BUILD_DIR/photon_scheduler" "$PYTHON_PHOTON_DIR/build/"
+cp "$PHOTON_BUILD_DIR/libphoton.so" "$PYTHON_PHOTON_DIR/photon/"
+cp "$PHOTON_SOURCE_DIR/photon/photon_services.py" "$PYTHON_PHOTON_DIR/photon/"
 
-pushd "$GLOBAL_SCHEDULER_DIR"
-  make
-popd
-cp "$GLOBAL_SCHEDULER_DIR/build/global_scheduler" "$PYTHON_GLOBAL_SCHEDULER_DIR/build/"
-cp "$GLOBAL_SCHEDULER_DIR/lib/python/global_scheduler_services.py" "$PYTHON_GLOBAL_SCHEDULER_DIR/lib/python/"
+cp "$GLOBAL_SCHEDULER_BUILD_DIR/global_scheduler" "$PYTHON_GLOBAL_SCHEDULER_DIR/build/"
+cp "$GLOBAL_SCHEDULER_SOURCE_DIR/lib/python/global_scheduler_services.py" "$PYTHON_GLOBAL_SCHEDULER_DIR/lib/python/"
