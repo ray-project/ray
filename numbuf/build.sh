7bb9da1696c902347eec6414b6f607ee3c7ef2df
Philipp Moritz <pcmoritz@gmail.com>
2017-01-15 15:23:03 -0800

    remove standalone numbuf

--- numbuf/build.sh
+++ numbuf/build.sh
@@ -16,14 +16,6 @@ else
   exit 1
 fi
 
-# Build plasma. We currently have to do this because we compile numbuf with the
-# HAS_PLASMA flag.
-PLASMA_DIR="$ROOT_DIR/../src/plasma"
-pushd "$PLASMA_DIR"
-  make clean
-  make
-popd
-
 mkdir -p "$ROOT_DIR/build"
 pushd "$ROOT_DIR/build"
   cmake -DHAS_PLASMA=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-g" -DCMAKE_CXX_FLAGS="-g" ..
