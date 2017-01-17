d7bd2125ca4996e826a6cee2f26b82772e586774
Philipp Moritz <pcmoritz@gmail.com>
2017-01-16 17:23:01 -0800

    fix

--- python/global_scheduler/__init__.py
+++ python/global_scheduler/__init__.py
@@ -2,4 +2,4 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .lib.python.global_scheduler_services import *
+from .global_scheduler_services import *
