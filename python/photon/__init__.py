c507fc206d6385ea460392bb486a13e1a6bf481e
Philipp Moritz <pcmoritz@gmail.com>
2017-01-16 17:05:32 -0800

    move files

--- python/photon/__init__.py
+++ python/photon/__init__.py
@@ -1,1 +1,6 @@
-from .photon import *
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from core.src.photon.libphoton import *
+from .photon_services import *
--- python/photon/photon/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from core.src.photon.libphoton import *
-from .photon_services import *
