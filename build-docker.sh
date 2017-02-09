09b9a135f2c6764413b15a7746bad9423f8ee12e
Johann Schleier-Smith <jssmith@gmail.com>
2017-02-08 22:58:29 -0800

    build tar using git archive

--- build-docker.sh
+++ build-docker.sh
@@ -2,7 +2,7 @@
 
 docker build -t ray-project/base-deps docker/base-deps
 
-tar --exclude './docker' -c . > ./docker/deploy/ray.tar
+git archive -o ./docker/deploy/ray.tar $(git rev-parse HEAD)
 docker build --no-cache -t ray-project/deploy docker/deploy
 rm ./docker/deploy/ray.tar
 docker build -t ray-project/examples docker/examples
