0aa234fb9c613ee592f54db6c3ec75436aeaa9d0
Robert Nishihara <robertnishihara@gmail.com>
2017-02-09 23:29:43 -0800

    Fix CXX numbuf error message for Anaconda 3.6. (#258)

--- python/numbuf/__init__.py
+++ python/numbuf/__init__.py
@@ -13,10 +13,10 @@ If you are using Anaconda, try fixing this problem by running:
 try:
   from core.src.numbuf.libnumbuf import *
 except ImportError as e:
-  if hasattr(e, "msg") and isinstance(e.msg, str) and "GLIBCXX" in e.msg:
+  if hasattr(e, "msg") and isinstance(e.msg, str) and ("libstdc++" in e.msg or "CXX" in e.msg):
     # This code path should be taken with Python 3.
     e.msg += helpful_message
-  elif hasattr(e, "message") and isinstance(e.message, str) and "GLIBCXX" in e.message:
+  elif hasattr(e, "message") and isinstance(e.message, str) and ("libstdc++" in e.message or "CXX" in e.message):
     # This code path should be taken with Python 2.
     if hasattr(e, "args") and isinstance(e.args, tuple) and len(e.args) == 1 and isinstance(e.args[0], str):
       e.args = (e.args[0] + helpful_message,)
