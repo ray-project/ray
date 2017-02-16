480cfe248c1e574fbf72e9dc40eb957c39db4d0b
Robert Nishihara <robertnishihara@gmail.com>
2017-02-15 21:39:20 -0800

    Minor formatting.

--- src/numbuf/python/test/runtest.py
+++ src/numbuf/python/test/runtest.py
@@ -128,23 +128,23 @@ class SerializationTests(unittest.TestCase):
       result[0][0] = 1
 
   def testArrowLimits(self):
-    # These tests give out of memory errors on Travis and need to be
-    # run on a machine with lots of RAM
+    # These tests give out of memory errors on Travis and need to be run on a
+    # machine with lots of RAM.
     if os.getenv("TRAVIS") is None:
-      l = 2**29 * [1.0]
+      l = 2 ** 29 * [1.0]
       with self.assertRaises(numbuf.numbuf_error):
         self.roundTripTest(l)
       del l
-      l = 2**29 * ["s"]
+      l = 2 ** 29 * ["s"]
       with self.assertRaises(numbuf.numbuf_error):
         self.roundTripTest(l)
       del l
-      l = 2**29 * [["1"], 2, 3, [{"s": 4}]]
+      l = 2 ** 29 * [["1"], 2, 3, [{"s": 4}]]
       with self.assertRaises(numbuf.numbuf_error):
         self.roundTripTest(l)
       del l
       with self.assertRaises(numbuf.numbuf_error):
-        l = 2**29 * [{"s": 1}] + 2**29 * [1.0]
+        l = 2 ** 29 * [{"s": 1}] + 2 ** 29 * [1.0]
         self.roundTripTest(l)
 
 if __name__ == "__main__":
