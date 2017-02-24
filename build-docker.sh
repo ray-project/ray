349244cf28b5735728e2fca079bcf976f2ce261f
Johann Schleier-Smith <jssmith@gmail.com>
2017-02-23 20:52:53 -0800

    add git revision

--- build-docker.sh
+++ build-docker.sh
@@ -2,7 +2,8 @@
 
 docker build -t ray-project/base-deps docker/base-deps
 
+git rev-parse HEAD > ./docker/deploy/git-rev
 git archive -o ./docker/deploy/ray.tar $(git rev-parse HEAD)
 docker build --no-cache -t ray-project/deploy docker/deploy
-rm ./docker/deploy/ray.tar
+rm ./docker/deploy/ray.tar ./docker/deploy/git-rev
 docker build -t ray-project/examples docker/examples
