6a506822df3f784e1fae2e16a615b442ade991ca
Johann Schleier-Smith <jssmith@gmail.com>
2017-02-23 19:25:43 -0800

    update build docker script

--- build-docker.sh
+++ build-docker.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
-docker build -t ray-project/base-deps docker/base-deps
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    --no-cachd)
+    NO_CACHE="--no-cache"
+    ;;
+    --skip-examples)
+    SKIP_EXAMPLES=YES
+    ;;
+    *)
+    echo "Usage: build-docker.sh [ --no-cache ] [ --skip-examples ]"
+    exit 1
+esac
+shift
+done
 
+# Build base dependencies, allow caching
+docker build -t $NO_CACHE ray-project/base-deps docker/base-deps
+
+# Build the current Ray source
 git rev-parse HEAD > ./docker/deploy/git-rev
 git archive -o ./docker/deploy/ray.tar $(git rev-parse HEAD)
 docker build --no-cache -t ray-project/deploy docker/deploy
 rm ./docker/deploy/ray.tar ./docker/deploy/git-rev
-docker build -t ray-project/examples docker/examples
+
+
+if [ ! $SKIP_EXAMPLES ]; then
+    docker build -t $NO_CACHE ray-project/examples docker/examples
+fi
