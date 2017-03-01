8e34e450694a22040ca614107a3ad087d1b8f1e7
Johann Schleier-Smith <jssmith@gmail.com>
2017-02-28 18:57:51 -0800

    Docker Updates (#308)
    * new path for python build
    
    * add flag
    
    * build tar using git archive
    
    * no exit from start_ray.sh
    
    * update Docker instructions
    
    * update build docker script
    
    * add git revision
    
    * fix typo
    
    * bug fixes and clarifications
    
    * mend
    
    * add objectmanager ports to docker instructions
    
    * rewording
    
    * Small updates to documentation.

--- build-docker.sh
+++ build-docker.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-docker build -t ray-project/base-deps docker/base-deps
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    --no-cache)
+    NO_CACHE="--no-cache"
+    ;;
+    --skip-examples)
+    SKIP_EXAMPLES=YES
+    ;;
+    *)
+    echo "Usage: build-docker.sh [ --no-cache ] [ --skip-examples ]"
+    exit 1
+esac
+shift
+done
 
-tar --exclude './docker' -c . > ./docker/deploy/ray.tar
+# Build base dependencies, allow caching
+docker build $NO_CACHE -t ray-project/base-deps docker/base-deps
+
+# Build the current Ray source
+git rev-parse HEAD > ./docker/deploy/git-rev
+git archive -o ./docker/deploy/ray.tar $(git rev-parse HEAD)
 docker build --no-cache -t ray-project/deploy docker/deploy
-rm ./docker/deploy/ray.tar
-docker build -t ray-project/examples docker/examples
+rm ./docker/deploy/ray.tar ./docker/deploy/git-rev
+
+
+if [ ! $SKIP_EXAMPLES ]; then
+    docker build $NO_CACHE -t ray-project/examples docker/examples
+fi
