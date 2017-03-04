68d8522d58c8c6ca7647fdec839297aeae7327b2
Robert Nishihara <robertnishihara@gmail.com>
2017-03-04 10:45:15 -0800

    Update hyperparameter optimization example. (#332)
    * Update hyperparameter optimization example.
    
    * Remove early stopping.

--- examples/hyperopt/hyperopt_simple.py
+++ examples/hyperopt/hyperopt_simple.py
@@ -1,6 +1,3 @@
-# Most of the tensorflow code is adapted from Tensorflow's tutorial on using CNNs to train MNIST
-# https://www.tensorflow.org/versions/r0.9/tutorials/mnist/pros/index.html#build-a-multilayer-convolutional-network
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -12,16 +9,17 @@ import argparse
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-import hyperopt
+import objective
 
 parser = argparse.ArgumentParser(description="Run the hyperparameter optimization example.")
 parser.add_argument("--trials", default=2, type=int, help="The number of random trials to do.")
 parser.add_argument("--steps", default=10, type=int, help="The number of steps of training to do per network.")
+parser.add_argument("--redis-address", default=None, type=str, help="The Redis address of the cluster.")
 
 if __name__ == "__main__":
   args = parser.parse_args()
 
-  ray.init(num_workers=10)
+  ray.init(redis_address=args.redis_address)
 
   # The number of sets of random hyperparameters to try.
   trials = args.trials
@@ -36,31 +34,32 @@ if __name__ == "__main__":
   validation_images = ray.put(mnist.validation.images)
   validation_labels = ray.put(mnist.validation.labels)
 
-  # Keep track of the best parameters and the best accuracy.
-  best_params = None
+  # Keep track of the best hyperparameters and the best accuracy.
+  best_hyperparamemeters = None
   best_accuracy = 0
   # This list holds the object IDs for all of the experiments that we have
   # launched and that have not yet been processed.
   remaining_ids = []
   # This is a dictionary mapping the object ID of an experiment to the
-  # parameters used for that experiment.
-  params_mapping = {}
+  # hyerparameters used for that experiment.
+  hyperparameters_mapping = {}
 
   # A function for generating random hyperparameters.
-  def generate_random_params():
-    learning_rate = 10 ** np.random.uniform(-5, 5)
-    batch_size = np.random.randint(1, 100)
-    dropout = np.random.uniform(0, 1)
-    stddev = 10 ** np.random.uniform(-5, 5)
-    return {"learning_rate": learning_rate, "batch_size": batch_size, "dropout": dropout, "stddev": stddev}
+  def generate_hyperparameters():
+    return {"learning_rate": 10 ** np.random.uniform(-5, 5),
+            "batch_size": np.random.randint(1, 100),
+            "dropout": np.random.uniform(0, 1),
+            "stddev": 10 ** np.random.uniform(-5, 5)}
 
   # Randomly generate some hyperparameters, and launch a task for each set.
   for i in range(trials):
-    params = generate_random_params()
-    accuracy_id = hyperopt.train_cnn_and_compute_accuracy.remote(params, steps, train_images, train_labels, validation_images, validation_labels)
+    hyperparameters = generate_hyperparameters()
+    accuracy_id = objective.train_cnn_and_compute_accuracy.remote(
+        hyperparameters, steps, train_images, train_labels, validation_images,
+        validation_labels)
     remaining_ids.append(accuracy_id)
-    # Keep track of which parameters correspond to this experiment.
-    params_mapping[accuracy_id] = params
+    # Keep track of which hyperparameters correspond to this experiment.
+    hyperparameters_mapping[accuracy_id] = hyperparameters
 
   # Fetch and print the results of the tasks in the order that they complete.
   for i in range(trials):
@@ -68,16 +67,20 @@ if __name__ == "__main__":
     ready_ids, remaining_ids = ray.wait(remaining_ids)
     # Process the output of this task.
     result_id = ready_ids[0]
-    params = params_mapping[result_id]
-    accuracy = ray.get(result_id)
+    hyperparameters = hyperparameters_mapping[result_id]
+    accuracy, _ = ray.get(result_id)
     print("""We achieve accuracy {:.3}% with
         learning_rate: {:.2}
         batch_size: {}
         dropout: {:.2}
         stddev: {:.2}
-      """.format(100 * accuracy, params["learning_rate"], params["batch_size"], params["dropout"], params["stddev"]))
+      """.format(100 * accuracy,
+                 hyperparameters["learning_rate"],
+                 hyperparameters["batch_size"],
+                 hyperparameters["dropout"],
+                 hyperparameters["stddev"]))
     if accuracy > best_accuracy:
-      best_params = params
+      best_hyperparameters = hyperparameters
       best_accuracy = accuracy
 
   # Record the best performing set of hyperparameters.
@@ -86,4 +89,8 @@ if __name__ == "__main__":
         batch_size: {}
         dropout: {:.2}
         stddev: {:.2}
-    """.format(trials, 100 * best_accuracy, best_params["learning_rate"], best_params["batch_size"], best_params["dropout"], best_params["stddev"]))
+    """.format(trials, 100 * best_accuracy,
+               best_hyperparameters["learning_rate"],
+               best_hyperparameters["batch_size"],
+               best_hyperparameters["dropout"],
+               best_hyperparameters["stddev"]))
