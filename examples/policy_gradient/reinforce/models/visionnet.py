555dcf35a2ce96e70b8f4a1cdd19557d0d44f8a9
Philipp Moritz <pcmoritz@gmail.com>
2017-03-07 23:42:44 -0800

    Add policy gradient example. (#344)
    * add policy gradient example
    
    * fix typos
    
    * Minor changes plus some documentation.
    
    * Minor fixes.

---
+++ examples/policy_gradient/reinforce/models/visionnet.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+
+def vision_net(inputs, num_classes=10):
+  conv1 = slim.conv2d(inputs, 16, [8, 8], 4, scope="conv1")
+  conv2 = slim.conv2d(conv1, 32, [4, 4], 2, scope="conv2")
+  fc1 = slim.conv2d(conv2, 512, [10, 10], padding="VALID", scope="fc1")
+  fc2 = slim.conv2d(fc1, num_classes, [1, 1], activation_fn=None, normalizer_fn=None, scope="fc2")
+  return tf.squeeze(fc2, [1, 2])
---
+++ examples/policy_gradient/reinforce/utils.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+
+def flatten(weights, start=0, stop=2):
+  """This methods reshapes all values in a dictionary.
+
+  The indices from start to stop will be flattened into a single index.
+
+  Args:
+    weights: A dictionary mapping keys to numpy arrays.
+    start: The starting index.
+    stop: The ending index.
+  """
+  for key, val in weights.items():
+    new_shape = val.shape[0:start] + (-1,) + val.shape[stop:]
+    weights[key] = val.reshape(new_shape)
+  return weights
+
+def concatenate(weights_list):
+  keys = weights_list[0].keys()
+  result = {}
+  for key in keys:
+    result[key] = np.concatenate([l[key] for l in weights_list])
+  return result
+
+def shuffle(trajectory):
+  permutation = np.random.permutation(trajectory["dones"].shape[0])
+  for key, val in trajectory.items():
+    trajectory[key] = val[permutation][permutation]
+  return trajectory
+
+def iterate(trajectory, batchsize):
+  trajectory = shuffle(trajectory)
+  curr_index = 0
+  # TODO(pcm): This drops some data at the end of the batch.
+  while curr_index + batchsize < trajectory["dones"].shape[0]:
+    batch = dict()
+    for key in trajectory:
+      batch[key] = trajectory[key][curr_index:curr_index+batchsize]
+    curr_index += batchsize
+    yield batch
