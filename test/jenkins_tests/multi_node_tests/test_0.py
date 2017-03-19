7fca2d6ae36a41bb124cace8423a66f971033872
Robert Nishihara <robertnishihara@gmail.com>
2017-03-18 18:43:35 -0700

    Allow test to retry if it fails to schedule on all nodes.

--- test/jenkins_tests/multi_node_tests/test_0.py
+++ test/jenkins_tests/multi_node_tests/test_0.py
@@ -10,8 +10,13 @@ def f():
 
 if __name__ == "__main__":
   ray.init(redis_address=os.environ["RAY_REDIS_ADDRESS"])
-  ip_addresses = ray.get([f.remote() for i in range(1000)])
-  distinct_addresses = set(ip_addresses)
-  counts = [ip_addresses.count(address) for address in distinct_addresses]
-  print("Counts are {}".format(counts))
+  # Check that tasks are scheduled on all nodes.
+  num_attempts = 30
+  for i in range(num_attempts):
+    ip_addresses = ray.get([f.remote() for i in range(1000)])
+    distinct_addresses = set(ip_addresses)
+    counts = [ip_addresses.count(address) for address in distinct_addresses]
+    print("Counts are {}".format(counts))
+    if len(counts) == 5:
+      break
   assert len(counts) == 5
