3d7004dfee58dd285f24b0e0f89dc7ad6883bb1a
Johann Schleier-Smith <jssmith@gmail.com>
2017-03-18 23:44:54 -0700

    Add multinode tests by simulating multiple nodes using Docker. (#378)
    * run test workloads for a Docker cluster
    
    * better manage docker image versions
    
    * Changes to make multinode docker tests work with Python 3.
    
    * option to mount local test directory on head node to speed development
    
    * Attempt to simplify multinode test setup.
    
    * Small change.
    
    * Add in development-mode to run multinode docker tests more easily during development.
    
    * add jenkins test script that links to Docker hash
    
    * Read docker SHA from build_docker.sh and add test that should fail.
    
    * Consolidate implementations and remove duplicate files.
    
    * Allow test to retry if it fails to schedule on all nodes.
    
    * Remove sleep when in docker multinode tests.

---
+++ test/jenkins_tests/run_multi_node_tests.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Cause the script to exit if a single command fails.
+set -e
+
+ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
+
+DOCKER_SHA=$($ROOT_DIR/../../build-docker.sh --output-sha --no-cache --skip-examples)
+echo "Using Docker image" $DOCKER_SHA
+
+python $ROOT_DIR/multi_node_docker_test.py \
+    --docker-image=$DOCKER_SHA \
+    --num-nodes=5 \
+    --test-script=/ray/test/jenkins_tests/multi_node_tests/test_0.py
