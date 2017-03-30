6b489c12a03002ed480f68801d3820f2ae8a6a28
pcmoritz <pcmoritz@gmail.com>
2017-03-30 23:48:39 +0000

    fix test

--- test/jenkins_tests/multi_node_tests/large_memory_test.py
+++ test/jenkins_tests/multi_node_tests/large_memory_test.py
@@ -9,29 +9,35 @@ import ray
 if __name__ == "__main__":
   ray.init()
 
-  A = np.zeros(2 ** 31, dtype="int8")
+  A = np.zeros(2 ** 31 + 1, dtype="int8")
   a = ray.put(A)
   assert_almost_equal(ray.get(a), A)
+  del A
+  del a
 
-  # B = np.zeros(2 ** 33, dtype="int8")
-  # b = ray.put(B)
-  # assert assert_almost_equal(ray.get(b), B)
+  C = {"hello": np.zeros(2 ** 30 + 1),
+       "world": np.ones(2 ** 30 + 1)}
+  c = ray.put(C)
+  assert_almost_equal(ray.get(c)["hello"], C["hello"])
+  assert_almost_equal(ray.get(c)["world"], C["world"])
+  del C
+  del c
 
-  # C = {"hello": np.zeros(2 ** 32 + 1),
-  #      "world": np.ones(2 ** 32 + 1)}
-  # c = ray.put(C)
-  # assert assert_almost_equal(ray.get(c)["hello"], C["hello"])
-  # assert assert_almost_equal(ray.get(c)["world"], C["world"])
+  D = [np.ones(2 ** 30 + 1), 42.0 * np.ones(2 ** 30 + 1)]
+  d = ray.put(D)
+  assert_almost_equal(ray.get(d)[0], D[0])
+  assert_almost_equal(ray.get(d)[1], D[1])
+  del D
+  del d
 
-  # D = [np.ones(2 ** 32 + 1), 42.0 * np.ones(2 ** 32 + 1)]
-  # d = ray.put(D)
-  # assert assert_almost_equal(ray.get(d)[0], D[0])
-  # assert assert_almost_equal(ray.get(d)[1], D[1])
+  E = (2 ** 30 + 1) * ["h"]
+  e = ray.put(E)
+  assert ray.get(e) == E
+  del E
+  del e
 
-  # E = (2 ** 32 + 1) * ["h"]
-  # e = ray.put(E)
-  # assert ray.get(e) == E
-
-  # F = (2 ** 32 + 1) * ("i",)
-  # f = ray.put(F)
-  # assert ray.get(f) == F
+  F = (2 ** 30 + 1) * ("i",)
+  f = ray.put(F)
+  assert ray.get(f) == F
+  del F
+  del f
