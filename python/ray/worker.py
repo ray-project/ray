b20127bb6cdbcea0df930c4a9686c59dea5a45dd
Robert Nishihara <robertnishihara@gmail.com>
2017-04-04 12:27:46 -0700

    Suppress error messages in worker import thread when Redis terminates.

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -1291,33 +1291,38 @@ def import_thread(worker):
         raise Exception("This code should be unreachable.")
       num_imported += 1
 
-  for msg in worker.import_pubsub_client.listen():
-    with worker.lock:
-      if msg["type"] == "psubscribe":
-        continue
-      assert msg["data"] == b"rpush"
-      num_imports = worker.redis_client.llen("Exports")
-      assert num_imports >= num_imported
-      for i in range(num_imported, num_imports):
-        key = worker.redis_client.lindex("Exports", i)
-        if key.startswith(b"RemoteFunction"):
-          with log_span("ray:import_remote_function", worker=worker):
-            fetch_and_register_remote_function(key, worker=worker)
-        elif key.startswith(b"EnvironmentVariables"):
-          with log_span("ray:import_environment_variable", worker=worker):
-            fetch_and_register_environment_variable(key, worker=worker)
-        elif key.startswith(b"FunctionsToRun"):
-          with log_span("ray:import_function_to_run", worker=worker):
-            fetch_and_execute_function_to_run(key, worker=worker)
-        elif key.startswith(b"Actor"):
-          # Only get the actor if the actor ID matches the actor ID of this
-          # worker.
-          actor_id, = worker.redis_client.hmget(key, "actor_id")
-          if worker.actor_id == actor_id:
-            worker.fetch_and_register["Actor"](key, worker)
-        else:
-          raise Exception("This code should be unreachable.")
-        num_imported += 1
+  try:
+    for msg in worker.import_pubsub_client.listen():
+      with worker.lock:
+        if msg["type"] == "psubscribe":
+          continue
+        assert msg["data"] == b"rpush"
+        num_imports = worker.redis_client.llen("Exports")
+        assert num_imports >= num_imported
+        for i in range(num_imported, num_imports):
+          key = worker.redis_client.lindex("Exports", i)
+          if key.startswith(b"RemoteFunction"):
+            with log_span("ray:import_remote_function", worker=worker):
+              fetch_and_register_remote_function(key, worker=worker)
+          elif key.startswith(b"EnvironmentVariables"):
+            with log_span("ray:import_environment_variable", worker=worker):
+              fetch_and_register_environment_variable(key, worker=worker)
+          elif key.startswith(b"FunctionsToRun"):
+            with log_span("ray:import_function_to_run", worker=worker):
+              fetch_and_execute_function_to_run(key, worker=worker)
+          elif key.startswith(b"Actor"):
+            # Only get the actor if the actor ID matches the actor ID of this
+            # worker.
+            actor_id, = worker.redis_client.hmget(key, "actor_id")
+            if worker.actor_id == actor_id:
+              worker.fetch_and_register["Actor"](key, worker)
+          else:
+            raise Exception("This code should be unreachable.")
+          num_imported += 1
+  except redis.ConnectionError:
+    # When Redis terminates the listen call will throw a ConnectionError, which
+    # we catch here.
+    pass
 
 
 def connect(info, object_id_seed=None, mode=WORKER_MODE, worker=global_worker,
