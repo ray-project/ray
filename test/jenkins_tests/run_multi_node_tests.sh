e18f919e337eae19d83f55c02dec63d7a5a46751
Robert Nishihara <robertnishihara@gmail.com>
2017-04-04 22:40:15 -0700

    Increase memory for docker image for jenkins tests.

--- test/jenkins_tests/run_multi_node_tests.sh
+++ test/jenkins_tests/run_multi_node_tests.sh
@@ -16,5 +16,6 @@ python $ROOT_DIR/multi_node_docker_test.py \
 python $ROOT_DIR/multi_node_docker_test.py \
     --docker-image=$DOCKER_SHA \
     --num-nodes=1 \
-    --shm-size=40G \
+    --mem-size=60G \
+    --shm-size=60G \
     --test-script=/ray/test/jenkins_tests/multi_node_tests/large_memory_test.py
