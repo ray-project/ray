eb5f6810ad3df369b31e5901a3345672277c98dc
Philipp Moritz <pcmoritz@gmail.com>
2017-04-15 20:34:46 -0700

    fix arrow origin and remove submodule

--- src/numbuf/thirdparty/download_thirdparty.sh
+++ src/numbuf/thirdparty/download_thirdparty.sh
@@ -8,7 +8,7 @@ set -e
 TP_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
 if [ ! -d $TP_DIR/arrow ]; then
-  git clone https://github.com/pcmoritz/arrow/ "$TP_DIR/arrow"
+  git clone https://github.com/apache/arrow/ "$TP_DIR/arrow"
 fi
 cd $TP_DIR/arrow
-git checkout cf59732a744eea8e3d736f59f65c5bc336331c05
+git checkout 30e03a90718971c2a1d773145fb042d0c2857036
