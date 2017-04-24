7f06d7a4dd9a6dcfbc91ac8f3b9364b1df8973fd
Philipp Moritz <pcmoritz@gmail.com>
2017-04-23 23:57:59 -0700

    fix include order

--- src/plasma/plasma_store.cc
+++ src/plasma/plasma_store.cc
@@ -25,10 +25,10 @@
 #include <limits.h>
 #include <poll.h>
 
+#include <deque>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
-#include <deque>
 
 #include "common.h"
 #include "format/common_generated.h"
