958897c72dcffa301705e664d125cc28c91774c0
Robert Nishihara <robertnishihara@gmail.com>
2017-05-01 12:22:16 -0700

    Initial commit of evolution strategies example.

---
+++ examples/evolution_strategies/evolution_strategies.py
@@ -0,0 +1,308 @@
+# Code in this file is copied and adapted from
+# https://github.com/openai/evolution-strategies-starter.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+from collections import namedtuple
+import gym
+import numpy as np
+import ray
+import time
+
+import optimizers
+import policies
+import tabular_logger as tlogger
+import tf_util
+import utils
+
+
+Config = namedtuple("Config", [
+    "l2coeff", "noise_stdev", "episodes_per_batch", "timesteps_per_batch",
+    "calc_obstat_prob", "eval_prob", "snapshot_freq", "return_proc_mode",
+    "episode_cutoff_mode"
+])
+
+Result = namedtuple("Result", [
+    "noise_inds_n", "returns_n2", "sign_returns_n2", "lengths_n2",
+    "eval_return", "eval_length", "ob_sum", "ob_sumsq", "ob_count"
+])
+
+
+@ray.remote
+def create_shared_noise():
+  """Create a large array of noise to be shared by all workers."""
+  seed = 123
+  count = 250000000
+  noise = np.random.RandomState(seed).randn(count).astype(np.float32)
+  return noise
+
+
+class SharedNoiseTable(object):
+  def __init__(self, noise):
+    self.noise = noise
+    assert self.noise.dtype == np.float32
+
+  def get(self, i, dim):
+    return self.noise[i:i + dim]
+
+  def sample_index(self, stream, dim):
+    return stream.randint(0, len(self.noise) - dim + 1)
+
+
+@ray.actor
+class Worker(object):
+  def __init__(self, config, policy_params, env_name, noise,
+               min_task_runtime=0.2):
+    self.min_task_runtime = min_task_runtime
+    self.config = config
+    self.policy_params = policy_params
+    self.noise = SharedNoiseTable(noise)
+
+    self.env = gym.make(env_name)
+    self.sess = utils.make_session(single_threaded=True)
+    self.policy = policies.MujocoPolicy(self.env.observation_space,
+                                        self.env.action_space,
+                                        **policy_params)
+    tf_util.initialize()
+
+    self.rs = np.random.RandomState()
+
+    assert self.policy.needs_ob_stat == (self.config.calc_obstat_prob != 0)
+
+  def rollout_and_update_ob_stat(self, timestep_limit, task_ob_stat):
+    if (self.policy.needs_ob_stat and self.config.calc_obstat_prob != 0 and
+            self.rs.rand() < self.config.calc_obstat_prob):
+      rollout_rews, rollout_len, obs = self.policy.rollout(
+          self.env, timestep_limit=timestep_limit, save_obs=True,
+          random_stream=self.rs)
+      task_ob_stat.increment(obs.sum(axis=0), np.square(obs).sum(axis=0),
+                             len(obs))
+    else:
+      rollout_rews, rollout_len = self.policy.rollout(
+          self.env, timestep_limit=timestep_limit, random_stream=self.rs)
+    return rollout_rews, rollout_len
+
+  def do_rollouts(self, params, ob_mean, ob_std, timestep_limit):
+    # Set the network weights.
+    self.policy.set_trainable_flat(params)
+
+    if self.policy.needs_ob_stat:
+      self.policy.set_ob_stat(ob_mean, ob_std)
+
+    if self.rs.rand() < self.config.eval_prob:
+      print("In this case, the reference implementation uses no noise in "
+            "order to evaluate the policy. We're ignoring that.")
+
+    noise_inds, returns, sign_returns, lengths = [], [], [], []
+    # We set eps=0 because we're incrementing only.
+    task_ob_stat = utils.RunningStat(self.env.observation_space.shape, eps=0)
+
+    # Perform some rollouts with noise.
+    while (len(noise_inds) == 0 or
+           time.time() - task_tstart < self.min_task_runtime):
+      noise_idx = self.noise.sample_index(self.rs, self.policy.num_params)
+      v = self.config.noise_stdev * self.noise.get(noise_idx,
+                                                   self.policy.num_params)
+
+      # These two sampling steps could be done in parallel on different actors
+      # letting us update twice as frequently.
+      self.policy.set_trainable_flat(params + v)
+      rews_pos, len_pos = self.rollout_and_update_ob_stat(timestep_limit,
+                                                          task_ob_stat)
+
+      self.policy.set_trainable_flat(params - v)
+      rews_neg, len_neg = self.rollout_and_update_ob_stat(timestep_limit,
+                                                          task_ob_stat)
+
+      noise_inds.append(noise_idx)
+      returns.append([rews_pos.sum(), rews_neg.sum()])
+      sign_returns.append([np.sign(rews_pos).sum(), np.sign(rews_neg).sum()])
+      lengths.append([len_pos, len_neg])
+
+      return Result(
+        noise_inds_n=np.array(noise_inds),
+        returns_n2=np.array(returns, dtype=np.float32),
+        sign_returns_n2=np.array(sign_returns, dtype=np.float32),
+        lengths_n2=np.array(lengths, dtype=np.int32),
+        eval_return=None,
+        eval_length=None,
+        ob_sum=(None if task_ob_stat.count == 0 else task_ob_stat.sum),
+        ob_sumsq=(None if task_ob_stat.count == 0 else task_ob_stat.sumsq),
+        ob_count=task_ob_stat.count)
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Train an RL agent on Pong.")
+  parser.add_argument("--num-workers", default=10, type=int,
+                      help=("The number of actors to create in aggregate "
+                            "across the cluster."))
+  parser.add_argument("--env-name", default="Pendulum-v0", type=str,
+                      help="The name of the gym environment to use.")
+  parser.add_argument("--stepsize", default=0.01, type=float,
+                      help="The stepsize to use.")
+  parser.add_argument("--redis-address", default=None, type=str,
+                      help="The Redis address of the cluster.")
+
+  args = parser.parse_args()
+  num_workers = args.num_workers
+  env_name = args.env_name
+  stepsize = args.stepsize
+
+  ray.init(redis_address=args.redis_address,
+           num_workers=(0 if args.redis_address is None else None))
+
+  # Tell Ray to serialize Config and Result objects.
+  ray.register_class(Config)
+  ray.register_class(Result)
+
+  config = Config(l2coeff=0.005,
+                  noise_stdev=0.02,
+                  episodes_per_batch=10000,
+                  timesteps_per_batch=100000,
+                  calc_obstat_prob=0.01,
+                  eval_prob=0.003,
+                  snapshot_freq=20,
+                  return_proc_mode="centered_rank",
+                  episode_cutoff_mode="env_default")
+
+
+  policy_params = {
+    "ac_bins": "continuous:",
+    "ac_noise_std": 0.01,
+    "nonlin_type": "tanh",
+    "hidden_dims": [256, 256],
+    "connection_type": "ff"
+  }
+
+  # Create the shared noise table.
+  print("Creating shared noise table.")
+  noise_id = create_shared_noise.remote()
+  noise = SharedNoiseTable(ray.get(noise_id))
+
+  # Create the actors.
+  print("Creating actors.")
+  workers = [Worker(config, policy_params, env_name, noise_id)
+             for _ in range(num_workers)]
+
+  env = gym.make(env_name)
+  sess = utils.make_session(single_threaded=False)
+  policy = policies.MujocoPolicy(env.observation_space, env.action_space,
+                                 **policy_params)
+  tf_util.initialize()
+  optimizer = optimizers.Adam(policy, stepsize)
+
+  ob_stat = utils.RunningStat(env.observation_space.shape, eps=1e-2)
+
+  #WE CAN PROBABLY REMOVE THESE
+  tslimit, incr_tslimit_threshold, tslimit_incr_ratio = None, None, None
+  adaptive_tslimit = False
+
+  episodes_so_far = 0
+  timesteps_so_far = 0
+  tstart = time.time()
+
+  while True:
+    step_tstart = time.time()
+    theta = policy.get_trainable_flat()
+    assert theta.dtype == np.float32
+
+    theta_id = ray.put(theta)
+    rollout_ids = [worker.do_rollouts(
+                       theta_id,
+                       ob_stat.mean if policy.needs_ob_stat else None,
+                       ob_stat.std if policy.needs_ob_stat else None,
+                       tslimit)
+                   for worker in workers]
+
+    results = ray.get(rollout_ids)
+
+    curr_task_results, eval_rets, eval_lens = [], [], []
+    num_results_skipped = 0
+    num_episodes_popped = 0
+    num_timesteps_popped = 0
+    ob_count_this_batch = 0
+    # Loop over the results
+    for result in results:
+      assert result.eval_length is None, "We aren't doing eval rollouts."
+      assert result.noise_inds_n.ndim == 1
+      assert result.returns_n2.shape == (len(result.noise_inds_n), 2)
+      assert result.lengths_n2.shape == (len(result.noise_inds_n), 2)
+      assert result.returns_n2.dtype == np.float32
+
+      result_num_eps = result.lengths_n2.size
+      result_num_timesteps = result.lengths_n2.sum()
+      episodes_so_far += result_num_eps
+      timesteps_so_far += result_num_timesteps
+
+      curr_task_results.append(result)
+      num_episodes_popped += result_num_eps
+      num_timesteps_popped += result_num_timesteps
+      # Update ob stats.
+      if policy.needs_ob_stat and result.ob_count > 0:
+        ob_stat.increment(result.ob_sum, result.ob_sumsq, result.ob_count)
+        ob_count_this_batch += result.ob_count
+
+    # Assemble the results.
+    noise_inds_n = np.concatenate([r.noise_inds_n for
+                                   r in curr_task_results])
+    returns_n2 = np.concatenate([r.returns_n2 for r in curr_task_results])
+    lengths_n2 = np.concatenate([r.lengths_n2 for r in curr_task_results])
+    assert noise_inds_n.shape[0] == returns_n2.shape[0] == lengths_n2.shape[0]
+    # Process the returns.
+    if config.return_proc_mode == "centered_rank":
+      proc_returns_n2 = utils.compute_centered_ranks(returns_n2)
+    elif config.return_proc_mode == "sign":
+      proc_returns_n2 = np.concatenate([r.signreturns_n2
+                                        for r in curr_task_results])
+    elif config.return_proc_mode == "centered_sign_rank":
+      proc_returns_n2 = utils.compute_centered_ranks(
+          np.concatenate([r.signreturns_n2 for r in curr_task_results]))
+    else:
+      raise NotImplementedError(config.return_proc_mode)
+
+    # Compute and take a step.
+    g, count = utils.batched_weighted_sum(
+        proc_returns_n2[:, 0] - proc_returns_n2[:, 1],
+        (noise.get(idx, policy.num_params) for idx in noise_inds_n),
+        batch_size=500)
+    g /= returns_n2.size
+    assert (g.shape == (policy.num_params,) and g.dtype == np.float32 and
+            count == len(noise_inds_n))
+    update_ratio = optimizer.update(-g + config.l2coeff * theta)
+
+    # Update ob stat (we're never running the policy in the master, but we
+    # might be snapshotting the policy).
+    if policy.needs_ob_stat:
+      policy.set_ob_stat(ob_stat.mean, ob_stat.std)
+
+    # Update the number of steps to take.
+    if (adaptive_tslimit and
+            (lengths_n2 == tslimit).mean() >= incr_tslimit_threshold):
+      old_tslimit = tslimit
+      tslimit = int(tslimit_incr_ratio * tslimit)
+      print("Increased timestep limit from {} to {}"
+            .format(old_tslimit, tslimit))
+
+    step_tend = time.time()
+    tlogger.record_tabular("EpRewMean", returns_n2.mean())
+    tlogger.record_tabular("EpRewStd", returns_n2.std())
+    tlogger.record_tabular("EpLenMean", lengths_n2.mean())
+
+    tlogger.record_tabular("Norm",
+                           float(np.square(policy.get_trainable_flat()).sum()))
+    tlogger.record_tabular("GradNorm", float(np.square(g).sum()))
+    tlogger.record_tabular("UpdateRatio", float(update_ratio))
+
+    tlogger.record_tabular("EpisodesThisIter", lengths_n2.size)
+    tlogger.record_tabular("EpisodesSoFar", episodes_so_far)
+    tlogger.record_tabular("TimestepsThisIter", lengths_n2.sum())
+    tlogger.record_tabular("TimestepsSoFar", timesteps_so_far)
+
+    tlogger.record_tabular("ObCount", ob_count_this_batch)
+
+    tlogger.record_tabular("TimeElapsedThisIter", step_tend - step_tstart)
+    tlogger.record_tabular("TimeElapsed", step_tend - tstart)
+    tlogger.dump_tabular()
---
+++ examples/evolution_strategies/policies.py
@@ -0,0 +1,251 @@
+# Code in this file is copied and adapted from
+# https://github.com/openai/evolution-strategies-starter.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import logging
+import pickle
+
+import h5py
+import numpy as np
+import tensorflow as tf
+
+import tf_util as U
+
+logger = logging.getLogger(__name__)
+
+
+class Policy:
+    def __init__(self, *args, **kwargs):
+        self.args, self.kwargs = args, kwargs
+        self.scope = self._initialize(*args, **kwargs)
+        self.all_variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, self.scope.name)
+
+        self.trainable_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.scope.name)
+        self.num_params = sum(int(np.prod(v.get_shape().as_list())) for v in self.trainable_variables)
+        self._setfromflat = U.SetFromFlat(self.trainable_variables)
+        self._getflat = U.GetFlat(self.trainable_variables)
+
+        logger.info('Trainable variables ({} parameters)'.format(self.num_params))
+        for v in self.trainable_variables:
+            shp = v.get_shape().as_list()
+            logger.info('- {} shape:{} size:{}'.format(v.name, shp, np.prod(shp)))
+        logger.info('All variables')
+        for v in self.all_variables:
+            shp = v.get_shape().as_list()
+            logger.info('- {} shape:{} size:{}'.format(v.name, shp, np.prod(shp)))
+
+        placeholders = [tf.placeholder(v.value().dtype, v.get_shape().as_list()) for v in self.all_variables]
+        self.set_all_vars = U.function(
+            inputs=placeholders,
+            outputs=[],
+            updates=[tf.group(*[v.assign(p) for v, p in zip(self.all_variables, placeholders)])]
+        )
+
+    def _initialize(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def save(self, filename):
+        assert filename.endswith('.h5')
+        with h5py.File(filename, 'w') as f:
+            for v in self.all_variables:
+                f[v.name] = v.eval()
+            # TODO: it would be nice to avoid pickle, but it's convenient to pass Python objects to _initialize
+            # (like Gym spaces or numpy arrays)
+            f.attrs['name'] = type(self).__name__
+            f.attrs['args_and_kwargs'] = np.void(pickle.dumps((self.args, self.kwargs), protocol=-1))
+
+    @classmethod
+    def Load(cls, filename, extra_kwargs=None):
+        with h5py.File(filename, 'r') as f:
+            args, kwargs = pickle.loads(f.attrs['args_and_kwargs'].tostring())
+            if extra_kwargs:
+                kwargs.update(extra_kwargs)
+            policy = cls(*args, **kwargs)
+            policy.set_all_vars(*[f[v.name][...] for v in policy.all_variables])
+        return policy
+
+    # === Rollouts/training ===
+
+    def rollout(self, env, *, render=False, timestep_limit=None, save_obs=False, random_stream=None):
+        """
+        If random_stream is provided, the rollout will take noisy actions with noise drawn from that stream.
+        Otherwise, no action noise will be added.
+        """
+        env_timestep_limit = env.spec.tags.get('wrapper_config.TimeLimit.max_episode_steps')
+        timestep_limit = env_timestep_limit if timestep_limit is None else min(timestep_limit, env_timestep_limit)
+        rews = []
+        t = 0
+        if save_obs:
+            obs = []
+        ob = env.reset()
+        for _ in range(timestep_limit):
+            ac = self.act(ob[None], random_stream=random_stream)[0]
+            if save_obs:
+                obs.append(ob)
+            ob, rew, done, _ = env.step(ac)
+            rews.append(rew)
+            t += 1
+            if render:
+                env.render()
+            if done:
+                break
+        rews = np.array(rews, dtype=np.float32)
+        if save_obs:
+            return rews, t, np.array(obs)
+        return rews, t
+
+    def act(self, ob, random_stream=None):
+        raise NotImplementedError
+
+    def set_trainable_flat(self, x):
+        self._setfromflat(x)
+
+    def get_trainable_flat(self):
+        return self._getflat()
+
+    @property
+    def needs_ob_stat(self):
+        raise NotImplementedError
+
+    def set_ob_stat(self, ob_mean, ob_std):
+        raise NotImplementedError
+
+
+def bins(x, dim, num_bins, name):
+    scores = U.dense(x, dim * num_bins, name, U.normc_initializer(0.01))
+    scores_nab = tf.reshape(scores, [-1, dim, num_bins])
+    return tf.argmax(scores_nab, 2)  # 0 ... num_bins-1
+
+
+class MujocoPolicy(Policy):
+    def _initialize(self, ob_space, ac_space, ac_bins, ac_noise_std, nonlin_type, hidden_dims, connection_type):
+        self.ac_space = ac_space
+        self.ac_bins = ac_bins
+        self.ac_noise_std = ac_noise_std
+        self.hidden_dims = hidden_dims
+        self.connection_type = connection_type
+
+        assert len(ob_space.shape) == len(self.ac_space.shape) == 1
+        assert np.all(np.isfinite(self.ac_space.low)) and np.all(np.isfinite(self.ac_space.high)), \
+            'Action bounds required'
+
+        self.nonlin = {'tanh': tf.tanh, 'relu': tf.nn.relu, 'lrelu': U.lrelu, 'elu': tf.nn.elu}[nonlin_type]
+
+        with tf.variable_scope(type(self).__name__) as scope:
+            # Observation normalization
+            ob_mean = tf.get_variable(
+                'ob_mean', ob_space.shape, tf.float32, tf.constant_initializer(np.nan), trainable=False)
+            ob_std = tf.get_variable(
+                'ob_std', ob_space.shape, tf.float32, tf.constant_initializer(np.nan), trainable=False)
+            in_mean = tf.placeholder(tf.float32, ob_space.shape)
+            in_std = tf.placeholder(tf.float32, ob_space.shape)
+            self._set_ob_mean_std = U.function([in_mean, in_std], [], updates=[
+                tf.assign(ob_mean, in_mean),
+                tf.assign(ob_std, in_std),
+            ])
+
+            # Policy network
+            o = tf.placeholder(tf.float32, [None] + list(ob_space.shape))
+            a = self._make_net(tf.clip_by_value((o - ob_mean) / ob_std, -5.0, 5.0))
+            self._act = U.function([o], a)
+        return scope
+
+    def _make_net(self, o):
+        # Process observation
+        if self.connection_type == 'ff':
+            x = o
+            for ilayer, hd in enumerate(self.hidden_dims):
+                x = self.nonlin(U.dense(x, hd, 'l{}'.format(ilayer), U.normc_initializer(1.0)))
+        else:
+            raise NotImplementedError(self.connection_type)
+
+        # Map to action
+        adim, ahigh, alow = self.ac_space.shape[0], self.ac_space.high, self.ac_space.low
+        assert isinstance(self.ac_bins, str)
+        ac_bin_mode, ac_bin_arg = self.ac_bins.split(':')
+
+        if ac_bin_mode == 'uniform':
+            # Uniformly spaced bins, from ac_space.low to ac_space.high
+            num_ac_bins = int(ac_bin_arg)
+            aidx_na = bins(x, adim, num_ac_bins, 'out')  # 0 ... num_ac_bins-1
+            ac_range_1a = (ahigh - alow)[None, :]
+            a = 1. / (num_ac_bins - 1.) * tf.to_float(aidx_na) * ac_range_1a + alow[None, :]
+
+        elif ac_bin_mode == 'custom':
+            # Custom bins specified as a list of values from -1 to 1
+            # The bins are rescaled to ac_space.low to ac_space.high
+            acvals_k = np.array(list(map(float, ac_bin_arg.split(','))), dtype=np.float32)
+            logger.info('Custom action values: ' + ' '.join('{:.3f}'.format(x) for x in acvals_k))
+            assert acvals_k.ndim == 1 and acvals_k[0] == -1 and acvals_k[-1] == 1
+            acvals_ak = (
+                (ahigh - alow)[:, None] / (acvals_k[-1] - acvals_k[0]) * (acvals_k - acvals_k[0])[None, :]
+                + alow[:, None]
+            )
+
+            aidx_na = bins(x, adim, len(acvals_k), 'out')  # values in [0, k-1]
+            a = tf.gather_nd(
+                acvals_ak,
+                tf.concat([
+                    tf.tile(np.arange(adim)[None, :, None], [tf.shape(aidx_na)[0], 1, 1]),
+                    2,
+                    tf.expand_dims(aidx_na, -1)
+                ])  # (n,a,2)
+            )  # (n,a)
+        elif ac_bin_mode == 'continuous':
+            a = U.dense(x, adim, 'out', U.normc_initializer(0.01))
+        else:
+            raise NotImplementedError(ac_bin_mode)
+
+        return a
+
+    def act(self, ob, random_stream=None):
+        a = self._act(ob)
+        if random_stream is not None and self.ac_noise_std != 0:
+            a += random_stream.randn(*a.shape) * self.ac_noise_std
+        return a
+
+    @property
+    def needs_ob_stat(self):
+        return True
+
+    @property
+    def needs_ref_batch(self):
+        return False
+
+    def set_ob_stat(self, ob_mean, ob_std):
+        self._set_ob_mean_std(ob_mean, ob_std)
+
+    def initialize_from(self, filename, ob_stat=None):
+        """
+        Initializes weights from another policy, which must have the same architecture (variable names),
+        but the weight arrays can be smaller than the current policy.
+        """
+        with h5py.File(filename, 'r') as f:
+            f_var_names = []
+            f.visititems(lambda name, obj: f_var_names.append(name) if isinstance(obj, h5py.Dataset) else None)
+            assert set(v.name for v in self.all_variables) == set(f_var_names), 'Variable names do not match'
+
+            init_vals = []
+            for v in self.all_variables:
+                shp = v.get_shape().as_list()
+                f_shp = f[v.name].shape
+                assert len(shp) == len(f_shp) and all(a >= b for a, b in zip(shp, f_shp)), \
+                    'This policy must have more weights than the policy to load'
+                init_val = v.eval()
+                # ob_mean and ob_std are initialized with nan, so set them manually
+                if 'ob_mean' in v.name:
+                    init_val[:] = 0
+                    init_mean = init_val
+                elif 'ob_std' in v.name:
+                    init_val[:] = 0.001
+                    init_std = init_val
+                # Fill in subarray from the loaded policy
+                init_val[tuple([np.s_[:s] for s in f_shp])] = f[v.name]
+                init_vals.append(init_val)
+            self.set_all_vars(*init_vals)
+
+        if ob_stat is not None:
+            ob_stat.set_from_init(init_mean, init_std, init_count=1e5)
