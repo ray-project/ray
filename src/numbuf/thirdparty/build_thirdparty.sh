220c8a626c35eec0792e5b31ee108c11dbbda0af
Philipp Moritz <pcmoritz@gmail.com>
2017-05-06 17:10:51 -0700

    rebased on arrow change

--- src/numbuf/thirdparty/build_thirdparty.sh
+++ src/numbuf/thirdparty/build_thirdparty.sh
@@ -24,5 +24,5 @@ echo "building arrow"
 cd $TP_DIR/arrow/cpp
 mkdir -p $TP_DIR/arrow/cpp/build
 cd $TP_DIR/arrow/cpp/build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-g -O3 -march=native -mtune=native" -DCMAKE_CXX_FLAGS="-g -O3 -march=native -mtune=native" -DARROW_BUILD_TESTS=OFF -DARROW_BOOST_HEADER_ONLY=ON -DARROW_PYTHON=on ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-g -O3 -march=native -mtune=native" -DCMAKE_CXX_FLAGS="-g -O3 -march=native -mtune=native" -DARROW_BUILD_TESTS=OFF -DARROW_HDFS=OFF -DARROW_PYTHON=on ..
 make VERBOSE=1 -j$PARALLEL
--- src/numbuf/thirdparty/download_thirdparty.sh
+++ src/numbuf/thirdparty/download_thirdparty.sh
@@ -8,7 +8,7 @@ set -e
 TP_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
 if [ ! -d $TP_DIR/arrow ]; then
-  git clone https://github.com/pcmoritz/arrow/ "$TP_DIR/arrow"
+  git clone https://github.com/apache/arrow/ "$TP_DIR/arrow"
 fi
 cd $TP_DIR/arrow
 
