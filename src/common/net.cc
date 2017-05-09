ae92e0f96fb164676f98e340e19fe62bc8e08bb0
Stephanie <swang@cs.berkeley.edu>
2017-05-08 21:44:50 -0700

    Address Robert's comments

--- src/common/net.cc
+++ src/common/net.cc
@@ -22,34 +22,3 @@ bool valid_ip_address(const std::string &ip_address) {
   int result = inet_pton(AF_INET, ip_address.c_str(), &sa.sin_addr);
   return result == 1;
 }
-
-bool parse_ip_addrs_ports(std::string ip_addrs_ports,
-                          std::vector<std::string> &ip_addrs,
-                          std::vector<int> &ports) {
-  std::stringstream stream(ip_addrs_ports);
-  char ch;
-  stream >> ch;  // consume the initial '['.
-  if (ch != '[') {
-    return false;
-  }
-  do {
-    std::string token;
-    std::getline(stream, token, ':');
-    if (!valid_ip_address(token.c_str())) {
-      return false;
-    }
-    ip_addrs.push_back(token);
-    int port;
-    if (!(stream >> port)) {
-      return false;
-    }
-    ports.push_back(port);
-    stream >> ch;
-  } while (ch == ',');
-
-  if (ch != ']') {
-    return false;
-  }
-
-  return true;
-}
