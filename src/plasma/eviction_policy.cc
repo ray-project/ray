f6a1b2b1fb38f2cb4007660e179fd7d43b9adb00
Philipp Moritz <pcmoritz@gmail.com>
2017-05-09 19:45:22 -0700

    fixes

--- src/plasma/eviction_policy.cc
+++ src/plasma/eviction_policy.cc
@@ -1,44 +1,33 @@
 #include "eviction_policy.h"
 
-#include "utlist.h"
-#include "uthash.h"
-
 #include <list>
 #include <unordered_map>
 
 class LRUCache {
  private:
-  // A doubly-linked list containing the items in the cache and
-  // their sizes in LRU order.
-  std::list<std::pair<ObjectID, int64_t>> item_list_;
-  // A hash table mapping the object ID of objects in the cache to its location
-  // in the doubly linked list item_list_.
-  std::unordered_map<ObjectID, decltype(item_list_.begin()), UniqueIDHasher>
-      item_map_;
+  /** A doubly-linked list containing the items in the cache and
+   *  their sizes in LRU order. */
+  typedef std::list<std::pair<ObjectID, int64_t>> ItemList;
+  ItemList item_list_;
+  /** A hash table mapping the object ID of an object in the cache to its
+   *  location in the doubly linked list item_list_. */
+  std::unordered_map<ObjectID, ItemList::iterator, UniqueIDHasher> item_map_;
 
  public:
   LRUCache(){};
 
-  void store(const ObjectID &key, int64_t size) {
+  void add(const ObjectID &key, int64_t size) {
     auto it = item_map_.find(key);
-    if (it == item_map_.end()) {
-      item_list_.push_front(std::make_pair(key, size));
-      item_map_.insert(std::make_pair(key, item_list_.begin()));
-    }
+    CHECK(it == item_map_.end());
+    item_list_.push_front(std::make_pair(key, size));
+    item_map_.insert(std::make_pair(key, item_list_.begin()));
   }
 
   void remove(const ObjectID &key) {
     auto it = item_map_.find(key);
-    if (it != item_map_.end()) {
-      item_list_.erase(it->second);
-      item_map_.erase(it);
-    }
-  }
-
-  void access(const ObjectID &key) {
-    auto it = item_map_.find(key);
     CHECK(it != item_map_.end());
-    item_list_.splice(item_list_.begin(), item_list_, it->second);
+    item_list_.erase(it->second);
+    item_map_.erase(it);
   }
 
   int64_t choose_objects_to_evict(int64_t num_bytes_required,
@@ -103,7 +92,7 @@ void EvictionState_object_created(EvictionState *eviction_state,
                                   PlasmaStoreInfo *plasma_store_info,
                                   ObjectID object_id) {
   ObjectTableEntry *entry = plasma_store_info->objects[object_id];
-  eviction_state->cache.store(
+  eviction_state->cache.add(
       object_id, entry->info.data_size + entry->info.metadata_size);
 }
 
@@ -160,7 +149,7 @@ void EvictionState_end_object_access(EvictionState *eviction_state,
                                      ObjectID **objects_to_evict) {
   ObjectTableEntry *entry = plasma_store_info->objects[object_id];
   /* Add the object to the LRU cache.*/
-  eviction_state->cache.store(
+  eviction_state->cache.add(
       object_id, entry->info.data_size + entry->info.metadata_size);
   *num_objects_to_evict = 0;
   *objects_to_evict = NULL;
