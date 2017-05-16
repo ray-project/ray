f3e7790f4034c6ede72f887224e4b7d649f00e65
William Paul <wapaul1@berkeley.edu>
2017-05-16 21:22:44 +0000

    Changed documentation

--- examples/rl_pong/driver.py
+++ examples/rl_pong/driver.py
@@ -84,10 +84,9 @@ class PongEnv(object):
   def __init__(self):
     # Tell numpy to only use one core. If we don't do this, each actor may try
     # to use all of the cores and the resulting contention may result in no
-    # speedup over the serial version. Note that if numpy is using OpenBLAS,
-    # then you need to set OPENBLAS_NUM_THREADS=1, and you probably need to do
-    # it from the command line (so it happens before numpy is imported).
+    # speedup over the serial version.
     os.environ["MKL_NUM_THREADS"] = "1"
+    os.environ["OPENBLAS_NUM_THREADS"] = "1"
     self.env = gym.make("Pong-v0")
 
   def compute_gradient(self, model):
