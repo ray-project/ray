d2b6870132cd01c90ba02cc33765fff40b90f85c
Robert Nishihara <robertnishihara@gmail.com>
2017-05-24 11:50:25 -0700

    Entirely remove cloudpickle modifications. Just use plain cloudpickle.

--- python/ray/pickling.py
+++
@@ -1,8 +0,0 @@
-# Note that a little bit of code here is taken and slightly modified from the
-# pickler because it was not possible to change its behavior otherwise.
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from cloudpickle import load, loads, dump, dumps
