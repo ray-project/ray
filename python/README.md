9f2d335fd3581db5bba94025964181d7c35c2627
Richard Shin <ricshin@cs.berkeley.edu>
2017-05-26 01:20:37 -0700

    Add build-wheel-manylinux1.sh

---
+++ python/README.md
@@ -0,0 +1,11 @@
+# Building manylinux1 wheels
+
+To cause everything to be rebuilt, this script will delete ALL changes to the
+repository, including both changes to tracked files, and ANY untracked files. 
+
+It will also produce .whl files owned by root.
+
+Inside the root directory (i.e. one level above this python directory), run
+```
+  docker run -v `pwd`:/ray -ti quay.io/xhochy/arrow_manylinux1_x86_64_base:ARROW-1024 -w /ray /ray/python/build-wheel-manylinux1.sh
+```
---
+++ python/build-wheel-manylinux1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cat << EOF > "/usr/bin/nproc"
+#!/bin/bash
+echo 1
+EOF
+chmod +x /usr/bin/nproc
+
+/opt/python/cp35-cp35m/bin/pip install cmake
+ln -s /opt/python/cp35-cp35m/bin/cmake /usr/bin/cmake
+
+/opt/python/cp27-cp27mu/bin/pip install numpy
+mkdir .whl
+for PYTHON in cp27-cp27mu cp33-cp33m cp34-cp34m cp35-cp35m cp36-cp36m; do
+  rm -rf *
+  git checkout *
+  pushd python
+  PATH=/opt/python/${PYTHON}/bin:$PATH /opt/python/${PYTHON}/bin/python setup.py bdist_wheel
+  # In the future, run auditwheel here.
+  mv dist/*.whl ../.whl/
+  popd
+done
