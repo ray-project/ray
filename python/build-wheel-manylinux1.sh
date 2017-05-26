59c815e2154eca067ad8b904143257016bfe61e5
Richard Shin <ricshin@cs.berkeley.edu>
2017-05-26 01:26:41 -0700

    Install correct version of numpy

--- python/build-wheel-manylinux1.sh
+++ python/build-wheel-manylinux1.sh
@@ -9,12 +9,12 @@ chmod +x /usr/bin/nproc
 /opt/python/cp35-cp35m/bin/pip install cmake
 ln -s /opt/python/cp35-cp35m/bin/cmake /usr/bin/cmake
 
-/opt/python/cp27-cp27mu/bin/pip install numpy
 mkdir .whl
 for PYTHON in cp27-cp27mu cp33-cp33m cp34-cp34m cp35-cp35m cp36-cp36m; do
   rm -rf *
   git checkout *
   pushd python
+  /opt/python/${PYTHON}/bin/pip install numpy
   PATH=/opt/python/${PYTHON}/bin:$PATH /opt/python/${PYTHON}/bin/python setup.py bdist_wheel
   # In the future, run auditwheel here.
   mv dist/*.whl ../.whl/
