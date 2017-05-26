fa6974d99bad1cbb63c93c3acf89ebfd3642f471
Richard Shin <ricshin@cs.berkeley.edu>
2017-05-25 20:23:31 -0700

    Switch to cp27mu

--- manylinux1.sh
+++ manylinux1.sh
@@ -9,6 +9,6 @@ chmod +x /usr/bin/nproc
 /opt/python/cp35-cp35m/bin/pip install cmake
 ln -s /opt/python/cp35-cp35m/bin/cmake /usr/bin/cmake
 
-/opt/python/cp27-cp27m/bin/pip install numpy
+/opt/python/cp27-cp27mu/bin/pip install numpy
 cd python
-PYTHONPATH=build/bdist.linux-x86_64/wheel/:$PYTHONPATH PATH=/opt/python/cp27-cp27m/bin:$PATH /opt/python/cp27-cp27m/bin/python setup.py bdist_wheel
+PATH=/opt/python/cp27-cp27mu/bin:$PATH /opt/python/cp27-cp27mu/bin/python setup.py bdist_wheel
