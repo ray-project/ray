f464636dfe8d7012ebfe4a7c23ce91585accadaa
Robert Nishihara <robertnishihara@gmail.com>
2017-05-26 17:33:27 -0700

    Manually copy files in MANIFEST.in to where Python expects them in order to prevent setup.py from having to be run twice.

--- python/setup.py
+++ python/setup.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+import shutil
 import subprocess
 
 from setuptools import setup, find_packages, Extension, Distribution
@@ -11,6 +13,28 @@ import setuptools.command.build_ext as _build_ext
 class build_ext(_build_ext.build_ext):
   def run(self):
     subprocess.check_call(["../build.sh"])
+    # Ideally, these files would already have been included because they're in
+    # the MANIFEST.in, but the MANIFEST.in gets applied at the very beginning
+    # when setup.py runs before these files have been created, so we have to
+    # move the files manually.
+    manifest_file = open("MANIFEST.in")
+    filenames = [line.split(" ")[1].strip()
+                 for line in manifest_file.readlines()]
+    for filename in filenames:
+      self.move_file(filename)
+
+  def move_file(self, filename):
+    # TODO(rkn): This feels very brittle. It may not handle all cases. See
+    # https://github.com/apache/arrow/blob/master/python/setup.py for an
+    # example.
+    source = filename
+    destination = os.path.join(self.build_lib, filename)
+    # Create the target directory if it doesn't already exist.
+    parent_directory = os.path.dirname(destination)
+    if not os.path.exists(parent_directory):
+      os.makedirs(parent_directory)
+    print("Copying {} to {}.".format(source, destination))
+    shutil.copyfile(source, destination)
 
 
 class BinaryDistribution(Distribution):
