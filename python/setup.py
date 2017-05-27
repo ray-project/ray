ef9fb6443ed6df620bce2f6b385fd636d2219fcc
Robert Nishihara <robertnishihara@gmail.com>
2017-05-26 17:15:51 -0700

    Trigger build_ext in setup.py.

--- python/setup.py
+++ python/setup.py
@@ -21,6 +21,8 @@ class BinaryDistribution(Distribution):
 setup(name="ray",
       version="0.1.0",
       packages=find_packages(),
+      # Dummy extension to trigger build_ext
+      ext_modules=[Extension("__dummy__", sources=[])],
       cmdclass={"build_ext": build_ext},
       distclass=BinaryDistribution,
       install_requires=["numpy",
