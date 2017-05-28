609b5c1a4cfc01e01ce3c3965d9e88c1b072e32d
Richard Shin <shin.richard@gmail.com>
2017-05-27 21:35:48 -0700

    Add script to build manylinux1 .whl files (#600)
    * Add manylinux setup
    
    * Switch to cp27mu
    
    * python/MANIFEST.in
    
    * Fix MANIFEST.in
    
    * Add build-wheel-manylinux1.sh
    
    * Update readme
    
    * Install correct version of numpy
    
    * Fix typo in README-manylinux1.md
    
    * Don't install cmake
    
    * Remove commented line from setup.py
    
    * Delete unused manylinux1.sh
    
    * Run setup.py bdist_wheel twice
    
    * Don't use package_data and MANIFEST.in.
    
    * Small aesthetic change.
    
    * Trigger build_ext in setup.py.
    
    * Remove nonexistent file from MANIFEST.in.
    
    * Manually copy files in MANIFEST.in to where Python expects them in order to prevent setup.py from having to be run twice.
    
    * Only run setup.py once when building wheels.
    
    * Aesthetic change to readme.
    
    * Copy generated flatbuffer Python files in build_ext.
    
    * Fix permission denied error by making sure to preserve executableness when copying files.
    
    * Remove unnecessary argument to setup.py.
    
    * Remove MANIFEST.in and move files to include into list in setup.py.
    
    * Fix numpy version when building wheels and replace rm with git clean.

---
+++ python/build-wheel-manylinux1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cat << EOF > "/usr/bin/nproc"
+#!/bin/bash
+echo 1
+EOF
+chmod +x /usr/bin/nproc
+
+mkdir .whl
+for PYTHON in cp27-cp27mu cp33-cp33m cp34-cp34m cp35-cp35m cp36-cp36m; do
+  # The -f flag is passed twice to also run git clean in the arrow subdirectory.
+  # The -d flag removes directories. The -x flag ignores the .gitignore file,
+  # and the -e flag ensures that we don't remove the .whl directory.
+  git clean -f -f -x -d -e .whl
+  pushd python
+    # Fix the numpy version because this will be the oldest numpy version we can
+    # support.
+    /opt/python/${PYTHON}/bin/pip install numpy==1.10.4
+    PATH=/opt/python/${PYTHON}/bin:$PATH /opt/python/${PYTHON}/bin/python setup.py bdist_wheel
+    # In the future, run auditwheel here.
+    mv dist/*.whl ../.whl/
+  popd
+done
