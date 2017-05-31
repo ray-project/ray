0eefe76f63a2be67703d8dba61511ecb88a231db
Robert Nishihara <robertnishihara@gmail.com>
2017-05-31 15:21:27 -0700

    Add logging statement when disconnecting client from plasma store.

--- src/plasma/plasma_store.cc
+++ src/plasma/plasma_store.cc
@@ -410,6 +410,7 @@ void PlasmaStore::disconnect_client(Client *client) {
   loop_->remove_file_event(client->fd);
   // Close the socket.
   close(client->fd);
+  LOG_INFO("Disconnecting client on fd %d", client->fd);
   // If this client was using any objects, remove it from the appropriate
   // lists.
   for (const auto &entry : store_info_.objects) {
