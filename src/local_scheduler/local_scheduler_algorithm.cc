78ad65736f9436386920c263c06f9f1cd7cb3595
Robert Nishihara <robertnishihara@gmail.com>
2017-06-22 12:54:48 -0700

    Bug fix.

--- src/local_scheduler/local_scheduler_algorithm.cc
+++ src/local_scheduler/local_scheduler_algorithm.cc
@@ -592,7 +592,7 @@ int reconstruct_object_timeout_handler(event_loop *loop,
   int64_t num_object_ids = object_id_vec.size();
 
   int64_t max_num_to_reconstruct = 10000;
-  int64_t num_to_reconstruct = std::min(num_object_ids, num_to_reconstruct);
+  int64_t num_to_reconstruct = std::min(num_object_ids, max_num_to_reconstruct);
   /* Initiate reconstruction for some of the missing task dependencies. */
   for (int64_t i = 0; i < num_to_reconstruct; i++) {
     reconstruct_object(
