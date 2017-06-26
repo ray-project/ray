f86c3fc9ed1fd9d8e6524c5f7d277ee4dd5332fb
Eric Liang <ekhliang@gmail.com>
2017-06-25 19:52:00 -0700

    name is too long

--- python/ray/rllib/a3c/__init__.py
+++ python/ray/rllib/a3c/__init__.py
@@ -1,3 +1,3 @@
-from ray.rllib.a3c.a3c import AsynchronousAdvantageActorCritic, DEFAULT_CONFIG
+from ray.rllib.a3c.a3c import A3C, DEFAULT_CONFIG
 
-__all__ = ["AsynchronousAdvantageActorCritic", "DEFAULT_CONFIG"]
+__all__ = ["A3C", "DEFAULT_CONFIG"]
