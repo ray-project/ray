f012e597c26a97ab0df690bbecbd48e3bd551db8
Eric Liang <ekhliang@gmail.com>
2017-07-07 11:37:00 -0700

    [rllib] Basic port of baselines/deepq to rllib (#709)
    * rllib v0
    
    * fix imports
    
    * lint
    
    * comments
    
    * update docs
    
    * a3c wip
    
    * a3c wip
    
    * report stats
    
    * update doc
    
    * add common logdir attr
    
    * name is too long
    
    * fix small bug
    
    * propagate exception on error
    
    * fetch metrics
    
    * initial port
    
    * fix lint
    
    * add right license
    
    * port to common alg format
    
    * fix lint
    
    * rename dqn
    
    * add imports from future
    
    * fix lint

---
+++ python/ray/rllib/dqn/README.md
@@ -0,0 +1,1 @@
+Code in this package is adapted from https://github.com/openai/baselines/tree/master/baselines/deepq.
---
+++ python/ray/rllib/dqn/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from ray.rllib.dqn.dqn import DQN, DEFAULT_CONFIG
+
+__all__ = ["DQN", "DEFAULT_CONFIG"]
