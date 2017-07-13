180878a696df403a9d6ec7b5d8cdde574456e72a
Philipp Moritz <pcmoritz@gmail.com>
2017-07-13 16:35:56 +0000

    fix linting

--- python/ray/rllib/evolution_strategies/example.py
+++ python/ray/rllib/evolution_strategies/example.py
@@ -32,8 +32,8 @@ if __name__ == "__main__":
 
   config = DEFAULT_CONFIG.copy()
   config.update({
-    "num_workers": num_workers,
-    "stepsize": stepsize,
+      "num_workers": num_workers,
+      "stepsize": stepsize,
   })
 
   alg = EvolutionStrategies(env_name, config)
