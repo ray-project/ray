1f9c4f75b53f6f65c8533aee97075827a35d95d5
Richard Liaw <rliaw@berkeley.edu>
2017-08-04 00:25:48 +0000

    nit

--- python/ray/rllib/a3c/example.py
+++ python/ray/rllib/a3c/example.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                         type=str, help="The gym environment to use.")
     parser.add_argument("--redis-address", default=None, type=str,
                         help="The Redis address of the cluster.")
-    parser.add_argument("--num-workers", default=4, type=int,
+    parser.add_argument("--num-workers", default=16, type=int,
                         help="The number of A3C workers to use.")
     parser.add_argument("--iterations", default=-1, type=int,
                         help="The number of training iterations to run.")
