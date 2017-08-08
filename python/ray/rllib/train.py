862e56000b81e7311b779d9d633ad1fbc4f21933
Philipp Moritz <pcmoritz@gmail.com>
2017-08-07 19:05:48 -0700

    [rllib] Unify RLLib examples and add jenkins test for policy gradients (#815)
    * add jenkins test
    
    * correct handling of the number of iterations
    
    * convert policy gradient and evolution strategies script
    
    * convert DQN
    
    * fix A3C
    
    * fix
    
    * fix
    
    * fixes
    
    * remove redundant A3C example

--- python/ray/rllib/train.py
+++ python/ray/rllib/train.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 import argparse
 import json
 import os
+import sys
 
 import ray
 import ray.rllib.policy_gradient as pg
@@ -16,36 +17,45 @@ import ray.rllib.a3c as a3c
 
 parser = argparse.ArgumentParser(
     description=("Train a reinforcement learning agent."))
-parser.add_argument("--env", required=True, type=str)
-parser.add_argument("--alg", required=True, type=str)
-parser.add_argument("--config", default="{}", type=str)
-parser.add_argument("--upload-dir", default="file:///tmp/ray", type=str)
+parser.add_argument("--redis-address", default=None, type=str,
+                    help="The Redis address of the cluster.")
+parser.add_argument("--env", required=True, type=str,
+                    help="The gym environment to use.")
+parser.add_argument("--alg", required=True, type=str,
+                    help="The reinforcement learning algorithm to use.")
+parser.add_argument("--num-iterations", default=sys.maxsize, type=int,
+                    help="The number of training iterations to run.")
+parser.add_argument("--config", default="{}", type=str,
+                    help="The configuration options of the algorithm.")
+parser.add_argument("--upload-dir", default="file:///tmp/ray", type=str,
+                    help="Where the traces are stored.")
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    json_config = json.loads(args.config)
 
-    ray.init()
+    ray.init(redis_address=args.redis_address)
 
     env_name = args.env
     if args.alg == "PolicyGradient":
         config = pg.DEFAULT_CONFIG.copy()
-        config.update(json.loads(args.config))
+        config.update(json_config)
         alg = pg.PolicyGradient(
             env_name, config, upload_dir=args.upload_dir)
     elif args.alg == "EvolutionStrategies":
         config = es.DEFAULT_CONFIG.copy()
-        config.update(json.loads(args.config))
+        config.update(json_config)
         alg = es.EvolutionStrategies(
             env_name, config, upload_dir=args.upload_dir)
     elif args.alg == "DQN":
         config = dqn.DEFAULT_CONFIG.copy()
-        config.update(json.loads(args.config))
+        config.update(json_config)
         alg = dqn.DQN(
             env_name, config, upload_dir=args.upload_dir)
     elif args.alg == "A3C":
         config = a3c.DEFAULT_CONFIG.copy()
-        config.update(json.loads(args.config))
+        config.update(json_config)
         alg = a3c.A3C(
             env_name, config, upload_dir=args.upload_dir)
     else:
@@ -56,7 +66,7 @@ if __name__ == "__main__":
     result_logger = ray.rllib.common.RLLibLogger(
         os.path.join(alg.logdir, "result.json"))
 
-    while True:
+    for i in range(args.num_iterations):
         result = alg.train()
 
         # We need to use a custom json serializer class so that NaNs get
@@ -64,3 +74,5 @@ if __name__ == "__main__":
         json.dump(result._asdict(), result_logger,
                   cls=ray.rllib.common.RLLibEncoder)
         result_logger.write("\n")
+
+        print("current status: {}".format(result))
