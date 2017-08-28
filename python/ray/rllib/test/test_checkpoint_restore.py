218205214de06965fff3bb431afaf67ff2dc1b58
Eric Liang <ekhliang@gmail.com>
2017-08-27 17:00:26 -0700

    Update test

--- python/ray/rllib/test/test_checkpoint_restore.py
+++ python/ray/rllib/test/test_checkpoint_restore.py
@@ -1,29 +1,46 @@
 #!/usr/bin/env python
 
-import ray
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
-from ray.rllib.dqn import DQN, DEFAULT_CONFIG
-cls = DQN
+import ray
+import random
+
+from ray.rllib.dqn import DQN, DEFAULT_CONFIG as DQN_CONFIG
+from ray.rllib.evolution_strategies import (
+    EvolutionStrategies, DEFAULT_CONFIG as ES_CONFIG)
+from ray.rllib.policy_gradient import (
+    PolicyGradient, DEFAULT_CONFIG as PG_CONFIG)
+from ray.rllib.a3c import A3C, DEFAULT_CONFIG as A3C_CONFIG
 
 ray.init()
-config = DEFAULT_CONFIG.copy()
-config["num_sgd_iter"] = 5
-config["episodes_per_batch"] = 100
-config["timesteps_per_batch"] = 1000
-alg1 = cls('CartPole-v0', config)
+for (cls, default_config) in [
+        (DQN, DQN_CONFIG),
+        # TODO(ekl) this fails with multiple ES instances in a process
+        (EvolutionStrategies, ES_CONFIG),
+        (PolicyGradient, PG_CONFIG),
+        (A3C, A3C_CONFIG)]:
+    config = default_config.copy()
+    config["num_sgd_iter"] = 5
+    config["episodes_per_batch"] = 100
+    config["timesteps_per_batch"] = 1000
+    alg1 = cls('CartPole-v0', config)
+    alg2 = cls('CartPole-v0', config)
 
-print("Test action", alg1.compute_action([0, 0, 0, 0]))
-
-res = alg1.train()
-while res.episode_reward_mean < 180:
-    for _ in range(5):
+    for _ in range(3):
         res = alg1.train()
         print("current status: " + str(res))
-    checkpoint = alg1.save()
-    print('-------------------------------------------------')
-    alg1 = cls('CartPole-v0', config)
-    alg1.restore(checkpoint)
 
-print()
-print("Finished: " + str(res))
-print()
+    # Sync the models
+    alg2.restore(alg1.save())
+
+    for _ in range(10):
+        obs = [
+            random.random(), random.random(), random.random(), random.random()]
+        a1 = alg1.compute_action(obs)
+        a2 = alg2.compute_action(obs)
+        print("Checking computed actions", obs, a1, a2)
+
+        # TODO(ekl) this fails for stochastic policies
+        assert(a1 == a2)
