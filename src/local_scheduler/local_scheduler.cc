c094c49072b8831606aea16a23e2664deac19745
Stephanie <swang@cs.berkeley.edu>
2017-08-30 14:46:30 -0700

    Remove race between local scheduler disconnecting and global scheduler assigning a task

--- src/local_scheduler/local_scheduler.cc
+++ src/local_scheduler/local_scheduler.cc
@@ -147,6 +147,9 @@ void LocalSchedulerState_free(LocalSchedulerState *state) {
   /* Reset the SIGTERM handler to default behavior, so we try to clean up the
    * local scheduler at most once. If a SIGTERM is caught afterwards, there is
    * the possibility of orphan worker processes. */
+  /* TODO(swang): Add a null heartbeat that tells the global scheduler that we
+   * are dead. This avoids having to wait for the timeout before marking us as
+   * dead in the db_client table, in cases where we can do a clean exit. */
   signal(SIGTERM, SIG_DFL);
 
   /* Kill any child processes that didn't register as a worker yet. */
@@ -170,12 +173,6 @@ void LocalSchedulerState_free(LocalSchedulerState *state) {
   ARROW_CHECK_OK(state->plasma_conn->Disconnect());
   delete state->plasma_conn;
   state->plasma_conn = NULL;
-
-  /* Disconnect from the database. */
-  if (state->db != NULL) {
-    db_disconnect(state->db);
-    state->db = NULL;
-  }
 
   /* Free the command for starting new workers. */
   if (state->config.start_worker_command != NULL) {
