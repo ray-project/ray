3afacdb6ec9fac0d4a4eee24d7e463655556ba24
Eric Liang <ekhliang@gmail.com>
2017-09-05 20:07:07 -0700

    update

--- python/ray/experimental/ui.py
+++ python/ray/experimental/ui.py
@@ -447,8 +447,8 @@ def task_timeline():
         # Display the task trace within the Jupyter notebook
         clear_output(wait=True)
         print(
-            "To view fullscreen, load `{}` in chrome://tracing".format(
-                json_tmp))
+            "To view fullscreen, open chrome://tracing in Google Chrome "
+            "and load `{}`".format(json_tmp))
         display(IFrame(html_file_path, 900, 800))
 
     path_input.on_click(handle_submit)
