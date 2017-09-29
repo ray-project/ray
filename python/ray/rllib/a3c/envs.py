4942749cefbb3e2e4b243e1b59bdc9b1ffca6281
Richard Liaw <rliaw@berkeley.edu>
2017-09-29 08:52:16 +0000

    removed comments

--- python/ray/rllib/a3c/envs.py
+++ python/ray/rllib/a3c/envs.py
@@ -34,42 +34,6 @@ class RLLibPreprocessing(gym.ObservationWrapper):
         return self.preprocessor.transform(observation).squeeze(0)
 
 
-#OpenAI Preprocessing
-
-# def _process_frame42(frame):
-#     frame = frame[34:(34 + 160), :160]
-#     # Resize by half, then down to 42x42 (essentially mipmapping). If we resize
-#     # directly we lose pixels that, when mapped to 42x42, aren't close enough
-#     # to the pixel boundary.
-#     frame = cv2.resize(frame, (80, 80))
-#     frame = cv2.resize(frame, (42, 42))
-#     frame = frame.mean(2)
-#     frame = frame.astype(np.float32)
-#     frame *= (1.0 / 255.0)
-#     frame = np.reshape(frame, [42, 42, 1])
-#     return frame
-
-
-# def _process_frame80(frame):
-#     frame = frame[34:(34 + 160), :160]
-#     # Resize by half, then down to 80x80.
-#     frame = cv2.resize(frame, (80, 80))
-#     frame = frame.mean(2)
-#     frame = frame.astype(np.float32)
-#     frame *= (1.0 / 255.0)
-#     frame = np.reshape(frame, [80, 80, 1])
-#     return frame
-
-
-# class AtariProcessing(gym.ObservationWrapper):
-#     def __init__(self, env=None):
-#         super(AtariProcessing, self).__init__(env)
-#         self.observation_space = Box(0.0, 1.0, [42, 42, 1])
-
-#     def _observation(self, observation):
-#         return _process_frame42(observation)
-
-
 class Diagnostic(gym.Wrapper):
     def __init__(self, env=None):
         super(Diagnostic, self).__init__(env)
