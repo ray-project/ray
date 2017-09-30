2d0f439b7b2d7e55dba4f96320a1e2f7b92760df
Alexey Tumanov <atumanov@gmail.com>
2017-09-30 09:56:52 -0700

    hugepage + plasma directory support plumbing + documentation (#1030)
    * hugepage + plasma directory support plumbing + documentation
    
    * Indentation fix.
    
    * huge_pages_enabled --> huge_pages
    
    * One more change

--- python/ray/plasma/plasma.py
+++ python/ray/plasma/plasma.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import os
 import random
 import subprocess
+import sys
 import time
 
 
@@ -22,25 +23,40 @@ def random_name():
 
 def start_plasma_store(plasma_store_memory=DEFAULT_PLASMA_STORE_MEMORY,
                        use_valgrind=False, use_profiler=False,
-                       stdout_file=None, stderr_file=None):
+                       stdout_file=None, stderr_file=None,
+                       plasma_directory=None, huge_pages=False):
     """Start a plasma store process.
 
     Args:
-      use_valgrind (bool): True if the plasma store should be started inside of
-        valgrind. If this is True, use_profiler must be False.
-      use_profiler (bool): True if the plasma store should be started inside a
-        profiler. If this is True, use_valgrind must be False.
-      stdout_file: A file handle opened for writing to redirect stdout to. If
-        no redirection should happen, then this should be None.
-      stderr_file: A file handle opened for writing to redirect stderr to. If
-        no redirection should happen, then this should be None.
+        use_valgrind (bool): True if the plasma store should be started inside
+            of valgrind. If this is True, use_profiler must be False.
+        use_profiler (bool): True if the plasma store should be started inside
+            a profiler. If this is True, use_valgrind must be False.
+        stdout_file: A file handle opened for writing to redirect stdout to. If
+            no redirection should happen, then this should be None.
+        stderr_file: A file handle opened for writing to redirect stderr to. If
+            no redirection should happen, then this should be None.
+        plasma_directory: A directory where the Plasma memory mapped files will
+            be created.
+        huge_pages: a boolean flag indicating whether to start the
+            Object Store with hugetlbfs support. Requires plasma_directory.
 
     Return:
-      A tuple of the name of the plasma store socket and the process ID of the
-        plasma store process.
+        A tuple of the name of the plasma store socket and the process ID of
+            the plasma store process.
     """
     if use_valgrind and use_profiler:
         raise Exception("Cannot use valgrind and profiler at the same time.")
+
+    if huge_pages and not (sys.platform == "linux" or
+                           sys.platform == "linux2"):
+        raise Exception("The huge_pages argument is only supported on "
+                        "Linux.")
+
+    if huge_pages and plasma_directory is None:
+        raise Exception("If huge_pages is True, then the "
+                        "plasma_directory argument must be provided.")
+
     plasma_store_executable = os.path.join(os.path.abspath(
         os.path.dirname(__file__)),
         "../core/src/plasma/plasma_store")
@@ -48,6 +64,10 @@ def start_plasma_store(plasma_store_memory=DEFAULT_PLASMA_STORE_MEMORY,
     command = [plasma_store_executable,
                "-s", plasma_store_name,
                "-m", str(plasma_store_memory)]
+    if plasma_directory is not None:
+        command += ["-d", plasma_directory]
+    if huge_pages:
+        command += ["-h"]
     if use_valgrind:
         pid = subprocess.Popen(["valgrind",
                                 "--track-origins=yes",
@@ -79,24 +99,25 @@ def start_plasma_manager(store_name, redis_address,
     """Start a plasma manager and return the ports it listens on.
 
     Args:
-      store_name (str): The name of the plasma store socket.
-      redis_address (str): The address of the Redis server.
-      node_ip_address (str): The IP address of the node.
-      plasma_manager_port (int): The port to use for the plasma manager. If
-        this is not provided, a port will be generated at random.
-      use_valgrind (bool): True if the Plasma manager should be started inside
-        of valgrind and False otherwise.
-      stdout_file: A file handle opened for writing to redirect stdout to. If
-        no redirection should happen, then this should be None.
-      stderr_file: A file handle opened for writing to redirect stderr to. If
-        no redirection should happen, then this should be None.
+        store_name (str): The name of the plasma store socket.
+        redis_address (str): The address of the Redis server.
+        node_ip_address (str): The IP address of the node.
+        plasma_manager_port (int): The port to use for the plasma manager. If
+            this is not provided, a port will be generated at random.
+        use_valgrind (bool): True if the Plasma manager should be started
+            inside of valgrind and False otherwise.
+        stdout_file: A file handle opened for writing to redirect stdout to. If
+            no redirection should happen, then this should be None.
+        stderr_file: A file handle opened for writing to redirect stderr to. If
+            no redirection should happen, then this should be None.
 
-    Returns:
-      A tuple of the Plasma manager socket name, the process ID of the Plasma
-        manager process, and the port that the manager is listening on.
+      Returns:
+          A tuple of the Plasma manager socket name, the process ID of the
+              Plasma manager process, and the port that the manager is
+              listening on.
 
     Raises:
-      Exception: An exception is raised if the manager could not be started.
+        Exception: An exception is raised if the manager could not be started.
     """
     plasma_manager_executable = os.path.join(
         os.path.abspath(os.path.dirname(__file__)),
