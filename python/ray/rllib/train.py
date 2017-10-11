54b196faf5a6fa9b00a5267e3c87b5559f3b3279
Eric Liang <ekhliang@gmail.com>
2017-10-10 23:14:15 -0700

    train docs

--- python/ray/rllib/train.py
+++ python/ray/rllib/train.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+"""The main command line interface to RLlib.
+
+Arguments may either be specified on the command line or in JSON/YAML
+files. Additionally, the file-based interface supports hyperparameter
+exploration through grid or random search, though both interfaces allow
+for the concurrent execution of multiple trials on Ray.
+
+Single-trial example:
+    ./train.py --alg=DQN --env=CartPole-v0
+
+Hyperparameter grid search example:
+    ./train.py -f tuned_examples/cartpole-grid-search-example.yaml
+"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -10,13 +24,14 @@ import yaml
 import ray
 from ray.tune.config_parser import make_parser, parse_to_trials
 from ray.tune.trial_runner import TrialRunner
-from ray.tune.trial import Trial, TERMINATED
+from ray.tune.trial import Trial
 
 
 parser = make_parser("Train a reinforcement learning agent.")
 
 # Extends the base parser defined in ray/tune/config_parser, to add some
-# RLlib specific arguments.
+# RLlib specific arguments. For more arguments, see the configuration
+# defined there.
 parser.add_argument("--redis-address", default=None, type=str,
                     help="The Redis address of the cluster.")
 parser.add_argument("--restore", default=None, type=str,
@@ -48,5 +63,5 @@ if __name__ == "__main__":
         print(runner.debug_string())
 
     for trial in runner.get_trials():
-        if trial.status != TERMINATED:
+        if trial.status != Trial.TERMINATED:
             sys.exit(1)
