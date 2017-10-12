b58500188143bbefb2aa358f32c9042885ffec8e
Robert Nishihara <robertnishihara@gmail.com>
2017-10-12 00:04:38 -0700

    When a task is passed to the global scheduler, if it is not received,… (#1106)
    * When a task is passed to the global scheduler, if it is not received, then try again.
    * Call give_task_to_global_scheduler directly (same with local).

--- src/local_scheduler/local_scheduler_algorithm.cc
+++ src/local_scheduler/local_scheduler_algorithm.cc
@@ -14,6 +14,17 @@
 /* Declared for convenience. */
 void remove_actor(SchedulingAlgorithmState *algorithm_state, ActorID actor_id);
 
+void give_task_to_global_scheduler(LocalSchedulerState *state,
+                                   SchedulingAlgorithmState *algorithm_state,
+                                   TaskSpec *spec,
+                                   int64_t task_spec_size);
+
+void give_task_to_local_scheduler(LocalSchedulerState *state,
+                                  SchedulingAlgorithmState *algorithm_state,
+                                  TaskSpec *spec,
+                                  int64_t task_spec_size,
+                                  DBClientID local_scheduler_id);
+
 struct TaskQueueEntry {
   /** The task that is queued. */
   TaskSpec *spec;
@@ -892,8 +903,14 @@ void give_task_to_local_scheduler_retry(UniqueID id,
   CHECK(Task_state(task) == TASK_STATUS_SCHEDULED);
 
   TaskSpec *spec = Task_task_spec(task);
-  handle_actor_task_submitted(state, state->algorithm_state, spec,
-                              Task_task_spec_size(task));
+
+  ActorID actor_id = TaskSpec_actor_id(spec);
+  CHECK(!ActorID_equal(actor_id, NIL_ACTOR_ID));
+  CHECK(state->actor_mapping.count(actor_id) == 1);
+
+  give_task_to_local_scheduler(
+      state, state->algorithm_state, spec, Task_task_spec_size(task),
+      state->actor_mapping[actor_id].local_scheduler_id);
 }
 
 /**
@@ -927,6 +944,20 @@ void give_task_to_local_scheduler(LocalSchedulerState *state,
   task_table_add_task(state->db, task, &retryInfo, NULL, state);
 }
 
+void give_task_to_global_scheduler_retry(UniqueID id,
+                                         void *user_context,
+                                         void *user_data) {
+  LocalSchedulerState *state = (LocalSchedulerState *) user_context;
+  Task *task = (Task *) user_data;
+  CHECK(Task_state(task) == TASK_STATUS_WAITING);
+
+  TaskSpec *spec = Task_task_spec(task);
+  CHECK(ActorID_equal(TaskSpec_actor_id(spec), NIL_ACTOR_ID));
+
+  give_task_to_global_scheduler(state, state->algorithm_state, spec,
+                                Task_task_spec_size(task));
+}
+
 /**
  * Give a task to the global scheduler to schedule.
  *
@@ -948,7 +979,12 @@ void give_task_to_global_scheduler(LocalSchedulerState *state,
   DCHECK(state->config.global_scheduler_exists);
   Task *task = Task_alloc(spec, task_spec_size, TASK_STATUS_WAITING, NIL_ID);
   DCHECK(state->db != NULL);
-  task_table_add_task(state->db, task, NULL, NULL, NULL);
+  auto retryInfo = RetryInfo{
+      .num_retries = 0,  // This value is unused.
+      .timeout = 0,      // This value is unused.
+      .fail_callback = give_task_to_global_scheduler_retry,
+  };
+  task_table_add_task(state->db, task, &retryInfo, NULL, state);
 }
 
 bool resource_constraints_satisfied(LocalSchedulerState *state,
