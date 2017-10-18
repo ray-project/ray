f51d91218a7630ce015901baec6bdf34ef139905
Stephanie <swang@cs.berkeley.edu>
2017-10-18 12:44:53 -0700

    Terminate actor when the original handle goes out of scope

--- python/ray/actor.py
+++ python/ray/actor.py
@@ -584,12 +584,15 @@ def make_actor_handle_class(class_name):
         def __reduce__(self):
             raise Exception("Actor objects cannot be pickled.")
 
-        # def __del__(self):
-        #     """Kill the worker that is running this actor."""
-        #     if ray.worker.global_worker.connected:
-        #         self._actor_method_call("__ray_terminate__",
-        #                                 args=[self._ray_actor_id.id()])
-        # TODO(rkn): We can't just never clean up actors!!!
+        def __del__(self):
+            """Kill the worker that is running this actor."""
+            # TODO(swang): Also clean up forked actor handles.
+            # Kill the worker if this is the original actor handle, created
+            # with Class.remote().
+            if (ray.worker.global_worker.connected and
+                    self._ray_actor_handle_id.id() == ray.worker.NIL_ACTOR_ID):
+                self._actor_method_call("__ray_terminate__",
+                                        args=[self._ray_actor_id.id()])
 
     return ActorHandle
 
