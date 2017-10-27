2cff36a68ece92988d0013322cc261297593ceed
Richard Liaw <rliaw@berkeley.edu>
2017-10-26 22:49:57 -0700

    extraneous ipdb

--- python/ray/rllib/a3c/runner.py
+++ python/ray/rllib/a3c/runner.py
@@ -65,7 +65,6 @@ class Runner(object):
     def compute_gradient(self, params):
         self.policy.set_weights(params)
         rollout = self.pull_batch_from_queue()
-        import ipdb; ipdb.set_trace()
         batch = process_rollout(rollout, gamma=0.99, lambda_=1.0)
         gradient, info = self.policy.compute_gradients(batch)
         if "summary" in info:
