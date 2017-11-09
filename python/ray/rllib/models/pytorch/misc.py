214f35663c61ab135bb38977d1f47464edac58aa
Richard Liaw <rliaw@berkeley.edu>
2017-11-08 22:32:30 -0800

    removing weight initializer for lstm for now

--- python/ray/rllib/models/pytorch/misc.py
+++ python/ray/rllib/models/pytorch/misc.py
@@ -30,23 +30,5 @@ def normalized_columns_initializer(weights, std=1.0):
     return out
 
 
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        weight_shape = list(m.weight.data.size())
-        fan_in = np.prod(weight_shape[1:4])
-        fan_out = np.prod(weight_shape[2:4]) * weight_shape[0]
-        w_bound = np.sqrt(6. / (fan_in + fan_out))
-        m.weight.data.uniform_(-w_bound, w_bound)
-        m.bias.data.fill_(0)
-    elif classname.find('Linear') != -1:
-        weight_shape = list(m.weight.data.size())
-        fan_in = weight_shape[1]
-        fan_out = weight_shape[0]
-        w_bound = np.sqrt(6. / (fan_in + fan_out))
-        m.weight.data.uniform_(-w_bound, w_bound)
-        m.bias.data.fill_(0)
-
-
 def var_to_np(var):
     return var.data.numpy()[0]
