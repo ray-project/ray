d5c72d53e8b6cb7093cabf0e11629e6b253ccff0
Richard Liaw <rliaw@berkeley.edu>
2017-11-15 19:40:28 -0800

    testing_works

--- python/ray/tune/hyperband.py
+++ python/ray/tune/hyperband.py
@@ -8,12 +8,10 @@ from ray.tune.trial_scheduler import FIFOScheduler, TrialScheduler
 from ray.tune.trial import Trial
 
 
-def calculate_bracket_count(max_t_attr, eta):
-    return int(np.log(max_t_attr)/np.log(eta)) + 1
-
-
 class HyperBandScheduler(FIFOScheduler):
     """Implements HyperBand.
+
+    Blog post: https://people.eecs.berkeley.edu/~kjamieson/hyperband.html
 
     This implementation contains 3 logical levels.
     Each HyperBand iteration is a "band". There can be multiple
@@ -31,7 +29,11 @@ class HyperBandScheduler(FIFOScheduler):
     Trials added will be inserted into the most recent bracket
     and band and will spill over to new brackets/bands accordingly.
 
-    See https://people.eecs.berkeley.edu/~kjamieson/hyperband.html
+    This maintains the bracket size and max trial count per band
+    to 5 and 117 respectively, which correspond to that of
+    `max_attr=81, eta=3` from the blog post. Trials will fill up
+    from smallest bracket to largest, with largest
+    having the most rounds of successive halving.
 
     Args:
         time_attr (str): The TrainingResult attr to use for comparing time.
@@ -41,33 +43,24 @@ class HyperBandScheduler(FIFOScheduler):
         reward_attr (str): The TrainingResult objective value attribute. As
             with `time_attr`, this may refer to any objective value. Stopping
             procedures will use this attribute.
-        max_t_attr (int): maximum iterations per configuration. Altering
-            this with `eta=None` will not change the bracket count
-            nor size. If eta is changed to an int, then all will change.
-        eta (int): Default is None, This maintains the bracket size
-            and max trial count per band to 5 and 117 respectively, which
-            correspond to that of `max_t_attr=81, eta=3`. Trials do not
-            need to fill the band and will fill up from smallest bracket
-            to largest, with largest having the most rounds of successive
-            halving. If eta is changed, it will create trials and brackets
-            according to the default Hyperband formulation.
+        max_t (int): max time units per trial. Trials will be stopped after
+            max_t time units (determined by time_attr) have passed.
+            The HyperBand scheduler automatically tries to determine a
+            reasonable number of brackets based on this and eta.
     """
 
     def __init__(
             self, time_attr='training_iteration',
-            reward_attr='episode_reward_mean',
-            max_t_attr=81, eta=None):
-        assert max_t_attr > 0, "Max (time_attr) not valid!"
-        assert (eta is None or eta > 1), "Downsampling rate (eta) not valid!"
-
+            reward_attr='episode_reward_mean', max_t=81):
+        assert max_t > 0, "Max (time_attr) not valid!"
         FIFOScheduler.__init__(self)
-        self._eta = eta if eta else 3
-        self._s_max_1 = calculate_bracket_count(max_t_attr, eta) if eta else 5
+        self._eta = 3
+        self._s_max_1 = 5
         # bracket max trials
         self._get_n0 = lambda s: int(
             np.ceil(self._s_max_1/(s+1) * self._eta**s))
         # bracket initial iterations
-        self._get_r0 = lambda s: int((max_t_attr*self._eta**(-s)))
+        self._get_r0 = lambda s: int((max_t*self._eta**(-s)))
         self._hyperbands = [[]]  # list of hyperband iterations
         self._trial_info = {}  # Stores Trial -> Bracket, Band Iteration
 
@@ -153,7 +146,7 @@ class HyperBandScheduler(FIFOScheduler):
         if bracket.cur_iter_done():
             if bracket.finished():
                 self._cleanup_bracket(trial_runner, bracket)
-                return TrialScheduler.STOP
+                return TrialScheduler.CONTINUE
 
             good, bad = bracket.successive_halving(self._reward_attr)
             # kill bad trials
@@ -188,11 +181,14 @@ class HyperBandScheduler(FIFOScheduler):
         bracket.cleanup_trial(t)
 
     def _cleanup_bracket(self, trial_runner, bracket):
-        """Cleans up bracket after bracket is completely finished."""
+        """Cleans up bracket after bracket is completely finished.
+        Lets the last trial continue to run until termination condition
+        kicks in."""
         for trial in bracket.current_trials():
-            self._cleanup_trial(
-                trial_runner, trial, bracket,
-                hard=(trial.status == Trial.PAUSED))
+            if (trial.status == Trial.PAUSED):
+                self._cleanup_trial(
+                    trial_runner, trial, bracket,
+                    hard=True)
 
     def on_trial_complete(self, trial_runner, trial, result):
         """Cleans up trial info from bracket if trial completed early."""
