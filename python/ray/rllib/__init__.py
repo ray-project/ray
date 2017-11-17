4eba5dcc0ca500e921eb0f340885ec09099706ea
Richard Liaw <rliaw@berkeley.edu>
2017-11-17 10:33:25 -0800

    maybe?

--- python/ray/rllib/__init__.py
+++ python/ray/rllib/__init__.py
@@ -1,19 +1,19 @@
-from ray.tune.registry import _default_registry, TRAINABLE_CLASS
-from ray.rllib.agent import _MockAgent, _SigmoidFakeData
+def init():
+    from ray.tune.registry import _default_registry, TRAINABLE_CLASS
+    from ray.rllib.agent import _MockAgent, _SigmoidFakeData
 
-from ray.rllib import ppo, es, dqn, a3c, script_runner
+    from ray.rllib import ppo, es, dqn, a3c#, script_runner
 
+    _default_registry.register(TRAINABLE_CLASS, "PPO", ppo.PPOAgent)
 
-_default_registry.register(TRAINABLE_CLASS, "PPO", ppo.PPOAgent)
+    _default_registry.register(TRAINABLE_CLASS, "ES", es.ESAgent)
 
-_default_registry.register(TRAINABLE_CLASS, "ES", es.ESAgent)
+    _default_registry.register(TRAINABLE_CLASS, "DQN", dqn.DQNAgent)
 
-_default_registry.register(TRAINABLE_CLASS, "DQN", dqn.DQNAgent)
+    _default_registry.register(TRAINABLE_CLASS, "A3C", a3c.A3CAgent)
 
-_default_registry.register(TRAINABLE_CLASS, "A3C", a3c.A3CAgent)
+    # _default_registry.register(TRAINABLE_CLASS, "script", script_runner.ScriptRunner)
 
-_default_registry.register(TRAINABLE_CLASS, "script", script_runner.ScriptRunner)
+    _default_registry.register(TRAINABLE_CLASS, "__fake", _MockAgent)
 
-_default_registry.register(TRAINABLE_CLASS, "__fake", _MockAgent)
-
-_default_registry.register(TRAINABLE_CLASS, "__sigmoid_fake_data", _SigmoidFakeData)
+    _default_registry.register(TRAINABLE_CLASS, "__sigmoid_fake_data", _SigmoidFakeData)
