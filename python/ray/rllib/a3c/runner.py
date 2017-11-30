7caa586358041b9a01ce2522055fdc2dcab48e9e
Richard Liaw <rliaw@berkeley.edu>
2017-11-30 00:22:25 -0800

    [rllib] Generalizing A3C Sampling Classes (#1250)

--- python/ray/rllib/a3c/runner.py
+++ python/ray/rllib/a3c/runner.py
@@ -2,82 +2,72 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from ray.rllib.a3c.envs import create_and_wrap
-import tensorflow as tf
-import six.moves.queue as queue
-from ray.rllib.a3c.runner_thread import RunnerThread
-from ray.rllib.a3c.common import process_rollout
-from ray.rllib.a3c.tfpolicy import TFPolicy
 import ray
-import os
+from ray.rllib.a3c.envs import create_and_wrap
+from ray.rllib.a3c.common import process_rollout, get_policy_cls
+from ray.rllib.utils.filter import get_filter
+from ray.rllib.utils.sampler import AsyncSampler
 
 
 class Runner(object):
     """Actor object to start running simulation on workers.
 
     The gradient computation is also executed from this object.
+
+    Attributes:
+        policy: Copy of graph used for policy. Used by sampler and gradients.
+        rew_filter: Reward filter used in rollout post-processing.
+        sampler: Component for interacting with environment and generating
+            rollouts.
+        logdir: Directory for logging.
     """
-    def __init__(self, env_creator, policy_cls, actor_id, batch_size,
-                 preprocess_config, logdir):
-        env = create_and_wrap(env_creator, preprocess_config)
-        self.id = actor_id
+    def __init__(self, env_creator, config, logdir):
+        self.env = env = create_and_wrap(env_creator, config["model"])
+        policy_cls = get_policy_cls(config)
         # TODO(rliaw): should change this to be just env.observation_space
         self.policy = policy_cls(env.observation_space.shape, env.action_space)
-        self.runner = RunnerThread(env, self.policy, batch_size)
-        self.env = env
-        self.logdir = logdir
-        self.start()
+        obs_filter = get_filter(
+            config["observation_filter"], env.observation_space.shape)
+        self.rew_filter = get_filter(config["reward_filter"], ())
 
-    def pull_batch_from_queue(self):
-        """Take a rollout from the queue of the thread runner."""
-        rollout = self.runner.queue.get(timeout=600.0)
-        if isinstance(rollout, BaseException):
-            raise rollout
-        while not rollout.terminal:
-            try:
-                part = self.runner.queue.get_nowait()
-                if isinstance(part, BaseException):
-                    raise rollout
-                rollout.extend(part)
-            except queue.Empty:
-                break
-        return rollout
+        self.sampler = AsyncSampler(env, self.policy, config["batch_size"],
+                                    obs_filter)
+        self.logdir = logdir
+
+    def get_data(self):
+        """
+        Returns:
+            trajectory: trajectory information
+            obs_filter: Current state of observation filter
+            rew_filter: Current state of reward filter"""
+        rollout, obs_filter = self.sampler.get_data()
+        return rollout, obs_filter, self.rew_filter
 
     def get_completed_rollout_metrics(self):
         """Returns metrics on previously completed rollouts.
 
         Calling this clears the queue of completed rollout metrics.
         """
-        completed = []
-        while True:
-            try:
-                completed.append(self.runner.metrics_queue.get_nowait())
-            except queue.Empty:
-                break
-        return completed
+        return self.sampler.get_metrics()
 
-    def start(self):
-        summary_writer = tf.summary.FileWriter(
-            os.path.join(self.logdir, "agent_%d" % self.id))
-        self.summary_writer = summary_writer
-        if isinstance(self.policy, TFPolicy):
-            self.runner.start_runner(self.policy.sess, summary_writer)
-        else:
-            self.runner.start_runner(tf.Session(), summary_writer)
-
-    def compute_gradient(self, params):
-        self.policy.set_weights(params)
-        rollout = self.pull_batch_from_queue()
-        batch = process_rollout(rollout, gamma=0.99, lambda_=1.0)
+    def compute_gradient(self):
+        rollout, obsf_snapshot = self.sampler.get_data()
+        batch = process_rollout(
+            rollout, self.rew_filter, gamma=0.99, lambda_=1.0)
         gradient, info = self.policy.compute_gradients(batch)
-        if "summary" in info:
-            self.summary_writer.add_summary(
-                tf.Summary.FromString(info['summary']),
-                self.policy.local_steps)
-            self.summary_writer.flush()
-        info = {"id": self.id,
-                "size": len(batch.a)}
+        info["obs_filter"] = obsf_snapshot
+        info["rew_filter"] = self.rew_filter
         return gradient, info
+
+    def set_weights(self, params):
+        self.policy.set_weights(params)
+
+    def update_filters(self, obs_filter=None, rew_filter=None):
+        if rew_filter:
+            # No special handling required since outside of threaded code
+            self.rew_filter = rew_filter.copy()
+        if obs_filter:
+            self.sampler.update_obs_filter(obs_filter)
 
 
 RemoteRunner = ray.remote(Runner)
