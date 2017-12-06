c25094bbbfd6c370682202653a498c50200cb734
Peter Schafhalter <pschafhalter@berkeley.edu>
2017-12-05 17:21:36 -0800

    Added eval script

---
+++ python/ray/rllib/eval.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import argparse
+import gym
+import ray
+
+from dqn import DQNAgent
+from agent import get_agent_class
+
+
+EXAMPLE_USAGE = """
+example usage:
+    ./train.py /tmp/ray/checkpoint_dir/checkpoint-0 --run DQN --env CartPole-v0
+"""
+
+
+parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="Evaluates a reinforcement learning agent given a checkpoint.",
+        epilog=EXAMPLE_USAGE)
+
+parser.add_argument("checkpoint", type=str, help="Checkpoint which to evaluate.")
+required_named = parser.add_argument_group("required named arguments")
+required_named.add_argument(
+        "--run", type=str, required=True,
+        help="The algorithm or model to train. This may refer to the name "
+        "of a built-on algorithm (e.g. RLLib's DQN or PPO), or a "
+        "user-defined trainable function or class registered in the "
+        "tune registry.")
+required_named.add_argument(
+    "--env", type=str, required=True, help="The gym environment to use.")
+parser.add_argument(
+    "--hide", default=False, action="store_const", const=True,
+    help="Surpress rendering of the environment.")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    ray.init()
+
+    cls = get_agent_class(args.run)
+    agent = cls(env=args.env)
+    agent._restore(args.checkpoint)
+
+    env = gym.make(args.env)
+    state = env.reset()
+    done = False
+    while not done:
+        action = agent.compute_action(state)
+        state, reward, done, _ = env.step(action)
+        if not args.hide:
+            env.render()
+
