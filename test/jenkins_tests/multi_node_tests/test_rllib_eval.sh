d0e1de3aa77747641780eeb764d9c08c16134c0e
Peter Schafhalter <pschafhalter@berkeley.edu>
2017-12-05 17:29:51 -0800

    Fixed paths

--- test/jenkins_tests/multi_node_tests/test_rllib_eval.sh
+++ test/jenkins_tests/multi_node_tests/test_rllib_eval.sh
@@ -7,7 +7,7 @@ GYM_ENV='CartPole-v0'
 # Test for DQN
 ALG='DQN'
 EXPERIMENT_NAME=$GYM_ENV'_'$ALG
-python ~/workspace/ray/python/ray/rllib/train.py --run $ALG --env $GYM_ENV \
+python /ray/python/ray/rllib/train.py --run $ALG --env $GYM_ENV \
   --stop '{"training_iteration": 2}' --experiment-name $EXPERIMENT_NAME \
   --checkpoint-freq 1
 
@@ -15,7 +15,7 @@ EXPERIMENT_PATH='/tmp/ray/'$EXPERIMENT_NAME
 CHECKPOINT_FOLDER=$(ls $EXPERIMENT_PATH)
 CHECKPOINT=$EXPERIMENT_PATH'/'$CHECKPOINT_FOLDER'/checkpoint-0'
 
-python ~/workspace/ray/python/ray/rllib/eval.py $CHECKPOINT --run $ALG \
+python /ray/python/ray/rllib/eval.py $CHECKPOINT --run $ALG \
   --env $GYM_ENV --hide
 
 # Clean up
@@ -24,7 +24,7 @@ rm -rf $EXPERIMENT_PATH
 # Test for PPO
 ALG='PPO'
 EXPERIMENT_NAME=$GYM_ENV'_'$ALG
-python ~/workspace/ray/python/ray/rllib/train.py --run $ALG --env $GYM_ENV \
+python /ray/python/ray/rllib/train.py --run $ALG --env $GYM_ENV \
   --stop '{"training_iteration": 2}' --experiment-name $EXPERIMENT_NAME \
   --checkpoint-freq 1
 
@@ -33,7 +33,7 @@ CHECKPOINT_FOLDER=$(ls $EXPERIMENT_PATH)
 CHECKPOINT=$EXPERIMENT_PATH'/'$CHECKPOINT_FOLDER'/checkpoint-1'
 
 echo $CHECKPOINT
-python ~/workspace/ray/python/ray/rllib/eval.py $CHECKPOINT --run $ALG \
+python /ray/python/ray/rllib/eval.py $CHECKPOINT --run $ALG \
   --env $GYM_ENV --hide
 
 # Clean up
