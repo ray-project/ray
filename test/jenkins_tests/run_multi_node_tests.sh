2a20d85b48751edb1a2f32928d18cca42acee2bd
Philipp Moritz <pcmoritz@gmail.com>
2017-12-07 14:52:22 -0800

    whitespace

--- test/jenkins_tests/run_multi_node_tests.sh
+++ test/jenkins_tests/run_multi_node_tests.sh
@@ -57,11 +57,11 @@ python $ROOT_DIR/multi_node_docker_test.py \
 #     python /ray/examples/hyperopt/hyperopt_adaptive.py
 
 docker run --rm --shm-size=10G --memory=10G $DOCKER_SHA \
-   python /ray/python/ray/rllib/train.py \
-   --env PongDeterministic-v0 \
-   --run A3C \
-   --stop '{"training_iteration": 2}' \
-   --config '{"num_workers": 16}'
+    python /ray/python/ray/rllib/train.py \
+    --env PongDeterministic-v0 \
+    --run A3C \
+    --stop '{"training_iteration": 2}' \
+    --config '{"num_workers": 16}'
 
 docker run --rm --shm-size=10G --memory=10G $DOCKER_SHA \
     python /ray/python/ray/rllib/train.py \
