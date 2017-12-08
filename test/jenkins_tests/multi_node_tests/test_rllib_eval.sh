b915195b64133e5897cb86e997fe0b05c3ae424f
Peter Schafhalter <pschafhalter@berkeley.edu>
2017-12-08 00:02:30 -0800

    Updated test script to match fixed checkpoints (#1294)

--- test/jenkins_tests/multi_node_tests/test_rllib_eval.sh
+++ test/jenkins_tests/multi_node_tests/test_rllib_eval.sh
@@ -1,41 +1,23 @@
 #!/bin/sh
 
+# TODO: Test AC3
+ALGS='DQN PPO'
 GYM_ENV='CartPole-v0'
 
-# TODO: Test AC3
+for ALG in $ALGS
+do
+  EXPERIMENT_NAME=$GYM_ENV'_'$ALG
+  python /ray/python/ray/rllib/train.py --run $ALG --env $GYM_ENV \
+    --stop '{"training_iteration": 2}' --experiment-name $EXPERIMENT_NAME \
+    --checkpoint-freq 1
 
-# Test for DQN
-ALG='DQN'
-EXPERIMENT_NAME=$GYM_ENV'_'$ALG
-python /ray/python/ray/rllib/train.py --run $ALG --env $GYM_ENV \
-  --stop '{"training_iteration": 2}' --experiment-name $EXPERIMENT_NAME \
-  --checkpoint-freq 1
+  EXPERIMENT_PATH='/tmp/ray/'$EXPERIMENT_NAME
+  CHECKPOINT_FOLDER=$(ls $EXPERIMENT_PATH)
+  CHECKPOINT=$EXPERIMENT_PATH'/'$CHECKPOINT_FOLDER'/checkpoint-1'
 
-EXPERIMENT_PATH='/tmp/ray/'$EXPERIMENT_NAME
-CHECKPOINT_FOLDER=$(ls $EXPERIMENT_PATH)
-CHECKPOINT=$EXPERIMENT_PATH'/'$CHECKPOINT_FOLDER'/checkpoint-0'
+  python /ray/python/ray/rllib/eval.py $CHECKPOINT --run $ALG \
+    --env $GYM_ENV --no-render
 
-python /ray/python/ray/rllib/eval.py $CHECKPOINT --run $ALG \
-  --env $GYM_ENV --no-render
-
-# Clean up
-rm -rf $EXPERIMENT_PATH
-
-# Test for PPO
-ALG='PPO'
-EXPERIMENT_NAME=$GYM_ENV'_'$ALG
-python /ray/python/ray/rllib/train.py --run $ALG --env $GYM_ENV \
-  --stop '{"training_iteration": 2}' --experiment-name $EXPERIMENT_NAME \
-  --checkpoint-freq 1
-
-EXPERIMENT_PATH='/tmp/ray/'$EXPERIMENT_NAME
-CHECKPOINT_FOLDER=$(ls $EXPERIMENT_PATH)
-CHECKPOINT=$EXPERIMENT_PATH'/'$CHECKPOINT_FOLDER'/checkpoint-1'
-
-echo $CHECKPOINT
-python /ray/python/ray/rllib/eval.py $CHECKPOINT --run $ALG \
-  --env $GYM_ENV --no-render
-
-# Clean up
-rm -rf $EXPERIMENT_PATH
-
+  # Clean up
+  rm -rf $EXPERIMENT_PATH
+done
