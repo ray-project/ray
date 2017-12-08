3bf527f9cc9c3f290d10d666397255e387f2a81c
Robert Nishihara <robertnishihara@gmail.com>
2017-12-07 17:39:09 -0800

    Fixes

--- python/ray/actor.py
+++ python/ray/actor.py
@@ -468,13 +468,14 @@ class ActorHandleWrapper(object):
     can tell that an argument is an ActorHandle.
     """
     def __init__(self, actor_id, actor_handle_id, actor_cursor, actor_counter,
-                 actor_method_names, method_signatures, checkpoint_interval,
-                 class_name):
+                 actor_method_names, actor_method_num_return_vals,
+                 method_signatures, checkpoint_interval, class_name):
         self.actor_id = actor_id
         self.actor_handle_id = actor_handle_id
         self.actor_cursor = actor_cursor
         self.actor_counter = actor_counter
         self.actor_method_names = actor_method_names
+        self.actor_method_num_return_vals = actor_method_num_return_vals
         # TODO(swang): Fetch this information from Redis so that we don't have
         # to fall back to pickle.
         self.method_signatures = method_signatures
@@ -501,10 +502,10 @@ def wrap_actor_handle(actor_handle):
         actor_handle._ray_actor_cursor,
         0,  # Reset the actor counter.
         actor_handle._ray_actor_method_names,
+        actor_handle._ray_actor_method_num_return_vals,
         actor_handle._ray_method_signatures,
         actor_handle._ray_checkpoint_interval,
-        actor_handle._ray_class_name,
-        actor_handle._ray_actor_method_num_return_vals)
+        actor_handle._ray_class_name)
     actor_handle._ray_actor_forks += 1
     return wrapper
 
@@ -532,6 +533,7 @@ def unwrap_actor_handle(worker, wrapper):
         wrapper.actor_cursor,
         wrapper.actor_counter,
         wrapper.actor_method_names,
+        wrapper.actor_method_num_return_vals,
         wrapper.method_signatures,
         wrapper.checkpoint_interval)
     return actor_object
