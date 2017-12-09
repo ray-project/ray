38a94f1c0ef843fa2bdfaef6511935b8464f63b5
Robert Nishihara <robertnishihara@gmail.com>
2017-12-09 00:12:37 -0800

    Add comment and assert

--- python/ray/actor.py
+++ python/ray/actor.py
@@ -303,7 +303,7 @@ def register_actor_signatures(worker, driver_id, class_name,
         actor_method_num_return_vals: A list of the number of return values for
             each of the actor's methods.
     """
-
+    assert len(actor_method_names) == len(actor_method_num_return_vals)
     for actor_method_name, num_return_vals in zip(
             actor_method_names, actor_method_num_return_vals):
         # TODO(rkn): When we create a second actor, we are probably overwriting
@@ -312,6 +312,7 @@ def register_actor_signatures(worker, driver_id, class_name,
         function_id = compute_actor_method_function_id(class_name,
                                                        actor_method_name).id()
         worker.function_properties[driver_id][function_id] = (
+            # The extra return value is an actor dummy object.
             FunctionProperties(num_return_vals=num_return_vals + 1,
                                resources={"CPU": 1},
                                max_calls=0))
