9d9cf2d1125c6842d9bbc49cb616937c649d8847
Eric Liang <ekhliang@gmail.com>
2017-12-15 11:12:58 -0800

    fix osx

--- test/trial_runner_test.py
+++ test/trial_runner_test.py
@@ -66,7 +66,7 @@ class TrainableFunctionApiTest(unittest.TestCase):
 
     def testLogdir(self):
         def train(config, reporter):
-            assert os.getcwd().startswith("/tmp/logdir/foo"), os.getcwd()
+            assert "/tmp/logdir/foo" in os.getcwd(), os.getcwd()
             reporter(timesteps_total=1)
         register_trainable("f1", train)
         run_experiments({"foo": {
