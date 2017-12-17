fb2135d2e92a6d7e839ffb413e7977a9e9143786
Cathy Wu <cathywu@eecs.berkeley.edu>
2017-12-16 19:48:35 -0800

    PEP8

--- python/ray/rllib/models/catalog.py
+++ python/ray/rllib/models/catalog.py
@@ -132,7 +132,8 @@ class ModelCatalog(object):
         print("Observation shape is {}".format(obs_shape))
 
         if env_name in cls._registered_preprocessor:
-            return cls._registered_preprocessor[env_name](env.observation_space, options)
+            return cls._registered_preprocessor[env_name](
+                    env.observation_space, options)
 
         if obs_shape == ():
             print("Using one-hot preprocessor for discrete envs.")
