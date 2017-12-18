6306f9fa2b5a5bd37b42db0dc73645800a8bf213
Eric Liang <ekhliang@gmail.com>
2017-12-17 16:00:03 -0800

    add reward

--- examples/carla/env.py
+++ examples/carla/env.py
@@ -7,10 +7,9 @@ import random
 import signal
 import subprocess
 
-from carla.client import make_carla_client, CarlaClient
+from carla.client import CarlaClient
 from carla.sensor import Camera
 from carla.settings import CarlaSettings
-from carla.util import make_connection
 
 import gym
 from gym.spaces import Box
@@ -27,13 +26,15 @@ class CarlaEnv(gym.Env):
         self.server_process = None
         self.server_port = random.randint(10000, 60000)
         self.server_process = subprocess.Popen(
-            [os.environ.get("CARLA_SERVER", "/home/ubuntu/carla-0.7/CarlaUE4.sh"),
+            [os.environ.get(
+                "CARLA_SERVER", "/home/ubuntu/carla-0.7/CarlaUE4.sh"),
              "-carla-server", "-carla-world-port={}".format(self.server_port)],
             preexec_fn=os.setsid)
 
         self.client = CarlaClient("localhost", self.server_port)
         self.client.connect()
         self.num_steps = 0
+        self.prev_measurement = None
 
     def __del__(self):
         self.client.disconnect()
@@ -42,6 +43,7 @@ class CarlaEnv(gym.Env):
 
     def reset(self):
         self.num_steps = 0
+        self.prev_measurement = None
 
         # Create a CarlaSettings object. This object is a wrapper around
         # the CarlaSettings.ini file. Here we set the configuration we
@@ -85,7 +87,9 @@ class CarlaEnv(gym.Env):
         print('Starting new episode...')
         self.client.start_episode(player_start)
 
-        return self._read_observation()
+        image, measurements = self._read_observation()[0]
+        self.prev_measurements = measurements
+        return image
 
     def step(self, action):
         assert len(action) == 5, "Invalid action {}".format(action)
@@ -95,9 +99,13 @@ class CarlaEnv(gym.Env):
             brake=bool(action[2]),
             hand_brake=bool(action[3]),
             reverse=bool(action[4]))
-        reward = 1
-        done = self.num_steps > 1000
-        return self._read_observation(), reward, done
+        image, measurements = self._read_observation()
+        reward, done = compute_reward(self.prev_measurement, measurements)
+        self.prev_measurement = measurements
+        if self.num_steps > os.environ.get("CARLA_MAX_STEPS", 1000):
+            done = True
+        info = {}
+        return image, reward, done, info
 
     def _read_observation(self):
         # Read the data produced by the server this frame.
@@ -112,7 +120,49 @@ class CarlaEnv(gym.Env):
                 observation = image
 
         assert observation is not None, sensor_data
-        return observation
+        return observation, measurements
+
+
+def distance(x1, y1, x2, y2):
+    return ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+
+
+def compute_reward(prev, current):
+    target_x = os.environ.get("CARLA_TARGET_X", 0.0)
+    target_y = os.environ.get("CARLA_TARGET_Y", 0.0)
+    prev_x = prev.transform.location.x / 100  # cm -> m
+    prev_y = prev.transform.location.y / 100
+    cur_x = current.transform.location.x / 100  # cm -> m
+    cur_y = current.transform.location.y / 100
+
+    reward = 0.0
+    done = False
+
+    # Distance travelled toward the goal in m
+    reward += (
+        distance(prev_x, prev_y, target_x, target_y) -
+        distance(cur_x, cur_y, target_x, target_y))
+
+    # Change in speed (km/h)
+    reward += 0.05 * (current.forward_speed - prev.forward_speed)
+
+    # New collision damage
+    reward -= .00002 * (
+        current.collision_vehicles + current.collision_pedestrians +
+        current.collision_other - prev.collision_vehicles -
+        prev.collision_pedestrians - prev.collision_other)
+
+    # New sidewalk intersection
+    reward -= 2 * (current.intersection_offroad - prev.intersection_offroad)
+
+    # New opposite lane intersection
+    reward -= 2 * (
+        current.intersection_otherlane - prev.intersection_otherlane)
+
+    if distance(cur_x, cur_y, target_x, target_y) < 1.0:
+        done = True
+
+    return reward, done
 
 
 def print_measurements(measurements):
@@ -120,11 +170,12 @@ def print_measurements(measurements):
     player_measurements = measurements.player_measurements
     message = 'Vehicle at ({pos_x:.1f}, {pos_y:.1f}), '
     message += '{speed:.2f} km/h, '
-    message += 'Collision: {{vehicles={col_cars:.0f}, pedestrians={col_ped:.0f}, other={col_other:.0f}}}, '
+    message += 'Collision: {{vehicles={col_cars:.0f}, '
+    message += 'pedestrians={col_ped:.0f}, other={col_other:.0f}}}, '
     message += '{other_lane:.0f}% other lane, {offroad:.0f}% off-road, '
     message += '({agents_num:d} non-player agents in the scene)'
     message = message.format(
-        pos_x=player_measurements.transform.location.x / 100, # cm -> m
+        pos_x=player_measurements.transform.location.x / 100,  # cm -> m
         pos_y=player_measurements.transform.location.y / 100,
         speed=player_measurements.forward_speed,
         col_cars=player_measurements.collision_vehicles,
@@ -141,4 +192,4 @@ if __name__ == '__main__':
     obs = env.reset()
     print("reset", obs)
     for _ in range(100):
-       print("step", env.step([0, 0, 0, 0, 0]))
+        print("step", env.step([0, 0, 0, 0, 0]))
