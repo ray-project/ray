1a21aa125d9dfe534a777137e490538747beec65
Eric Liang <ekhliang@gmail.com>
2017-12-17 22:04:55 -0800

    tunify train.py

--- examples/carla/train.py
+++ examples/carla/train.py
@@ -1,14 +1,22 @@
 import ray
-from ray.tune.registry import get_registry, register_env
-from ray.rllib import ppo
+from ray.tune import register_env, run_experiments
 
 from env import CarlaEnv
 
 env_name = "carla_env"
 register_env(env_name, lambda: CarlaEnv())
 
-ray.init()
-alg = ppo.PPOAgent(env=env_name, registry=get_registry(), config={"num_workers": 1, "timesteps_per_batch": 100, "min_steps_per_task": 100, "sgd_batchsize": 16, "devices": ["/gpu:0"]})
-
-for i in range(10):
-    alg.train()
+run_experiments({
+    "carla": {
+        "run": "PPO",
+        "env": "carla_env",
+        "resources": {"cpu": 4, "gpu": 1},
+        "config": {
+            "num_workers": 1,
+            "timesteps_per_batch": 100,
+            "min_steps_per_task": 100,
+            "sgd_batchsize": 16,
+            "devices": ["/gpu:0"],
+        },
+    },
+})
