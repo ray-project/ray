a391e2ede6c6b1a88dc4a3557d765ffc541b3188
Eric Liang <ekhliang@gmail.com>
2017-12-18 15:21:32 -0800

    doc

--- examples/carla/train_dqn.py
+++ examples/carla/train_dqn.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
 from ray.tune import register_env, run_experiments
 
