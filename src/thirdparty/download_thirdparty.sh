ac934e7ea61af94caabf5d13328c0b101a8c2a1f
Philipp Moritz <pcmoritz@gmail.com>
2017-12-19 08:52:49 -0800

    rebase on apache repo

--- src/thirdparty/download_thirdparty.sh
+++ src/thirdparty/download_thirdparty.sh
@@ -8,9 +8,9 @@ set -e
 TP_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
 if [ ! -d $TP_DIR/arrow ]; then
-  git clone https://github.com/pcmoritz/arrow.git "$TP_DIR/arrow"
+  git clone https://github.com/apache/arrow.git "$TP_DIR/arrow"
 fi
 cd $TP_DIR/arrow
 git fetch origin master
 
-git checkout 6a2a83ddff5605b598c8aa0ce99a5acaf500eae7
+git checkout 9895181610c0a5ef1ba836300ea2036e1a3e5621
