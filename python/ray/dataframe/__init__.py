6bf51d604fb9123ade300fdaf986e0bc347469aa
Devin Petersohn <devin.petersohn@gmail.com>
2017-12-18 20:06:10 -0800

    Fixing flake8 errors from imports

--- python/ray/dataframe/__init__.py
+++ python/ray/dataframe/__init__.py
@@ -1,5 +1,6 @@
-# from .dataframe import DataFrame
-from .dataframe import *
+from .dataframe import DataFrame
+from .dataframe import from_pandas
+from .dataframe import to_pandas
 
 ray.register_custom_serializer(pd.DataFrame, use_pickle=True)
 ray.register_custom_serializer(pd.core.indexes.base.Index, use_pickle=True)
