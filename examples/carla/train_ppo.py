41d4e26c66a5aed2166d93e9ec6ca5fbed7bb1bf
Philipp Moritz <pcmoritz@gmail.com>
2017-12-18 17:16:48 -0800

    better ppo parameters

--- examples/carla/train_ppo.py
+++ examples/carla/train_ppo.py
@@ -24,10 +24,17 @@ run_experiments({
         "resources": {"cpu": 4, "gpu": 1},
         "config": {
             "num_workers": 1,
-            "timesteps_per_batch": 100,
+            "timesteps_per_batch": 2000,
             "min_steps_per_task": 100,
-            "sgd_batchsize": 16,
+            "lambda": 0.95,
+            "clip_param": 0.2,
+            "num_sgd_iter": 20,
+            "sgd_stepsize": 0.0001,
+            "sgd_batchsize": 32,
             "devices": ["/gpu:0"],
+            "tf_session_args": {
+              "gpu_options": {"allow_growth": True}
+            }
         },
     },
 }, redirect_output=True)
