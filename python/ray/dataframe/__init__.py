23b6222c5b504bcf843442e466f7d3b8dd407065
devin-petersohn <devin.petersohn@gmail.com>
2017-12-19 13:50:06 -0800

    Fixing import errors

--- python/ray/dataframe/__init__.py
+++ python/ray/dataframe/__init__.py
@@ -2,6 +2,7 @@ from .dataframe import DataFrame
 from .dataframe import from_pandas
 from .dataframe import to_pandas
 import ray
+import pandas as pd
 
 ray.register_custom_serializer(pd.DataFrame, use_pickle=True)
 ray.register_custom_serializer(pd.core.indexes.base.Index, use_pickle=True)
