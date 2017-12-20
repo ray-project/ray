e171e665ab15b579394c8418808a73d4f1959b65
Devin Petersohn <devin.petersohn@gmail.com>
2017-12-19 17:38:23 -0800

    Addressing lint error

--- python/ray/dataframe/__init__.py
+++ python/ray/dataframe/__init__.py
@@ -8,5 +8,7 @@ from .dataframe import to_pandas
 import ray
 import pandas as pd
 
+__all__ = ["DataFrame", "from_pandas", "to_pandas"]
+
 ray.register_custom_serializer(pd.DataFrame, use_pickle=True)
 ray.register_custom_serializer(pd.core.indexes.base.Index, use_pickle=True)
