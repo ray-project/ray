2dbd5762904f6b29e46c3c156051b607ea06faf0
Philipp Moritz <pcmoritz@gmail.com>
2017-12-20 16:59:10 -0800

    add fix

--- src/thirdparty/download_thirdparty.sh
+++ src/thirdparty/download_thirdparty.sh
@@ -8,9 +8,9 @@ set -e
 TP_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
 if [ ! -d $TP_DIR/arrow ]; then
-  git clone https://github.com/apache/arrow.git "$TP_DIR/arrow"
+  git clone https://github.com/pcmoritz/arrow.git "$TP_DIR/arrow"
 fi
 cd $TP_DIR/arrow
 git fetch origin master
 
-git checkout 9895181610c0a5ef1ba836300ea2036e1a3e5621
+git checkout f3dbd9d92a9b482005afcf5d78dca49dac40c206
