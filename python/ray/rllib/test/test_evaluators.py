3bef0b4b6f2a84de8d2be9ec20410f11212aee4d
Richard Liaw <rliaw@berkeley.edu>
2017-12-25 01:58:44 -0800

    evals

--- python/ray/rllib/test/test_evaluators.py
+++ python/ray/rllib/test/test_evaluators.py
@@ -3,24 +3,31 @@ from __future__ import division
 from __future__ import print_function
 
 import unittest
+from ray.rllib.optimizers import _MeanStdFilterEvaluator
+from ray.rllib.utils.filters import MeanStdFilter
 
 
 class EvaluatorTest(unittest.TestCase):
     def testMergeFilter(self):
-        # Make evaluator
-        # evaluator.filter = MeanStdFilter
-        # new_obs_filter = MeanstdFilter()
-        # new_rew_filter = MeanStdFilter()
-        pass
+        e = _MeanStdFilterEvaluator()
+        e.sample()
+        e.get_filters()
+        obs_filter = MeanStdFilter((4,))
+        rew_filter = MeanStdFilter(())
+        # TODO(rliaw)
 
     def testSyncFilter(self):
-        # evaluator.filter = MeanStdFilter
-        # new_obs_filter = MeanstdFilter()
-        # new_rew_filter = MeanStdFilter()
-        pass
+        e = _MeanStdFilterEvaluator()
+        e.sample()
+        e.get_filters()
+        obs_filter = MeanStdFilter((4,))
+        rew_filter = MeanStdFilter(())
+        # TODO(rliaw)
 
     def testCopyFilter(self):
-        # evaluator.filter = MeanStdFilter
-        # new_obs_filter = MeanstdFilter()
-        # new_rew_filter = MeanStdFilter()
-        pass
+        e = _MeanStdFilterEvaluator()
+        e.sample()
+        e.get_filters()
+        obs_filter = MeanStdFilter((4,))
+        rew_filter = MeanStdFilter(())
+        # TODO(rliaw)
