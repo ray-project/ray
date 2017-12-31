a5238090d552399774511c98f96b737bc9a13a7d
Eric Liang <ekhliang@gmail.com>
2017-12-30 18:32:06 -0800

    fix train

--- examples/carla/train_a3c.py
+++ examples/carla/train_a3c.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 from ray.tune import register_env, run_experiments
 
 from env import CarlaEnv, ENV_CONFIG
+from scenarios import TOWN2_STRAIGHT
 
 env_name = "carla_env"
 env_config = ENV_CONFIG.copy()
@@ -14,8 +15,8 @@ env_config.update({
     "y_res": 80,
     "use_depth_camera": False,
     "discrete_actions": False,
-    "max_steps": 200,
-    "weather": [1],
+    "server_map": "/Game/Maps/Town02",
+    "scenarios": TOWN2_STRAIGHT,
 })
 register_env(env_name, lambda: CarlaEnv(env_config))
 
