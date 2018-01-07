abcd7395f8043d58184739a93d1bba53fa45b445
Robert Nishihara <robertnishihara@gmail.com>
2018-01-06 17:04:12 -0800

    Bump version to 0.3.1.

--- python/setup.py
+++ python/setup.py
@@ -101,7 +101,7 @@ class BinaryDistribution(Distribution):
 
 setup(name="ray",
       # The version string is also in __init__.py. TODO(pcm): Fix this.
-      version="0.3.0",
+      version="0.3.1",
       packages=find_packages(),
       cmdclass={"build_ext": build_ext},
       # The BinaryDistribution argument triggers build_ext.
