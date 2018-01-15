40132176153756692ae85f792a1948ced1ef12c3
eugenevinitsky <eugenevinitsky@airbears2-10-142-38-21.airbears2.1918.berkeley.edu>
2018-01-15 15:51:03 -0800

    added lines to let docker file find multiagent tests

--- test/jenkins_tests/run_multi_node_tests.sh
+++ test/jenkins_tests/run_multi_node_tests.sh
@@ -163,6 +163,8 @@ docker run --rm --shm-size=10G --memory=10G $DOCKER_SHA \
     python /ray/python/ray/tune/examples/tune_mnist_ray.py \
     --fast
 
-python /ray/python/ray/rllib/examples/multiagent_mountaincar.py
+docker run --rm --shm-size=10G --memory=10G $DOCKER_SHA \
+    python /ray/python/ray/rllib/examples/multiagent_mountaincar.py
 
-python /ray/python/ray/rllib/examples/multiagent_pendulum.py
+docker run --rm --shm-size=10G --memory=10G $DOCKER_SHA \
+    python /ray/python/ray/rllib/examples/multiagent_pendulum.py
