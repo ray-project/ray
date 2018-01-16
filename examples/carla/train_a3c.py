4803325faeef74040a26bdee120e5819d8f9ae5f
Eric Liang <ekhliang@gmail.com>
2018-01-15 18:05:10 -0800

    Mon Jan 15 18:05:10 PST 2018

--- examples/carla/train_a3c.py
+++ examples/carla/train_a3c.py
@@ -2,11 +2,12 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+impor oray
 from ray.tune import register_env, run_experiments
 
 from env import CarlaEnv, ENV_CONFIG
 from models import register_carla_model
-from scenarios import TOWN2_ONE_CURVE
+from scenarios import TOWN1_ONE_CURVE
 
 env_name = "carla_env"
 env_config = ENV_CONFIG.copy()
@@ -15,18 +16,19 @@ env_config.update({
     "x_res": 80,
     "y_res": 80,
     "discrete_actions": True,
-    "server_map": "/Game/Maps/Town02",
-    "scenarios": TOWN2_ONE_CURVE,
+    "server_map": "/Game/Maps/Town01",
+    "scenarios": TOWN1_ONE_CURVE,
 })
 
 register_env(env_name, lambda env_config: CarlaEnv(env_config))
 register_carla_model()
+redis_address = ray.services.get_node_ip_address() + ":6379"
 
 run_experiments({
     "carla-a3c": {
         "run": "A3C",
         "env": "carla_env",
-        "resources": {"cpu": 4, "gpu": 1, "driver_gpu_limit": 0},
+        "resources": {"cpu": 4, "gpu": 4, "driver_gpu_limit": 0},
         "config": {
             "env_config": env_config,
             "use_gpu_for_workers": True,
@@ -43,7 +45,7 @@ run_experiments({
                 ],
             },
             "gamma": 0.95,
-            "num_workers": 1,
+            "num_workers": 4,
         },
     },
-}, redirect_output=True)
+}, redis_address=redis_address, redirect_output=True)
