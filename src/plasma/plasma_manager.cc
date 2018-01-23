1e798c6f0461698cb51d8ded6c7ad83e150e3c42
Melih Elibol <elibol@gmail.com>
2018-01-22 23:00:32 -0800

    close transfer sock on free.

--- src/plasma/plasma_manager.cc
+++ src/plasma/plasma_manager.cc
@@ -265,7 +265,7 @@ struct ClientConnection {
   int fd;
   /** Transfer file descriptor for the socket connected to the other
    *  plasma manager. */
-  int tfd;
+  int tfd = -1;
   /** Timer id for timing out wait (or fetch). */
   int64_t timer_id;
   /** The number of objects that we have left to return for
@@ -1488,6 +1488,9 @@ void ClientConnection_free(ClientConnection *client_conn) {
   }
   /* Close the manager connection and free the remaining state. */
   close(client_conn->fd);
+  if(client_conn->tfd != -1){
+    close(client_conn->tfd);
+  }
   delete client_conn;
 }
 
