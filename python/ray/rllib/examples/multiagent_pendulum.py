0a01d3c71fc163ddb217a3c2b53f3fbc6e97fc56
eugenevinitsky <eugenevinitsky@users.noreply.github.com>
2018-01-25 13:58:35 -0800

    [rllib] Mountaincar fix (#1472)
    * Fix for gym version 0.9.5.
    
    * fixed bug in reshaper that was causing discrete spaces to fail

--- python/ray/rllib/examples/multiagent_pendulum.py
+++ python/ray/rllib/examples/multiagent_pendulum.py
@@ -36,11 +36,11 @@ def create_env(env_config):
 if __name__ == '__main__':
     register_env(env_name, lambda env_config: create_env(env_config))
     config = ppo.DEFAULT_CONFIG.copy()
-    horizon = 100
-    num_cpus = 2
-    ray.init(num_cpus=num_cpus, redirect_output=False)
+    horizon = 10
+    num_cpus = 4
+    ray.init(num_cpus=num_cpus, redirect_output=True)
     config["num_workers"] = num_cpus
-    config["timesteps_per_batch"] = 100
+    config["timesteps_per_batch"] = 10
     config["num_sgd_iter"] = 10
     config["gamma"] = 0.999
     config["horizon"] = horizon
