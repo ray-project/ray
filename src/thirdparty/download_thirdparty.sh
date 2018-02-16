502b55848df9ffb46c5a612701e9685c27a3b966
Philipp Moritz <pcmoritz@gmail.com>
2018-02-15 18:06:49 -0800

    check out specific parquet-cpp commit

--- src/thirdparty/download_thirdparty.sh
+++ src/thirdparty/download_thirdparty.sh
@@ -17,5 +17,7 @@ git checkout e26f3dad3675288564ef0c0330a5c9afcac652f1
 cd $TP_DIR
 if [ ! -d $TP_DIR/parquet-cpp ]; then
   git clone https://github.com/apache/parquet-cpp.git "$TP_DIR/parquet-cpp"
+  pushd $TP_DIR/parquet-cpp
+    git checkout 76388ea4eb8b23656283116bc656b0c8f5db093b
+  popd
 fi
-
