2bf6043c2551cc1d27acb832be5828141bfcbffb
Robert Nishihara <robertnishihara@gmail.com>
2018-02-16 09:58:35 -0800

    Cleanup parquet installation.

--- src/thirdparty/build_parquet.sh
+++ src/thirdparty/build_parquet.sh
@@ -8,20 +8,12 @@ set -e
 unamestr="$(uname)"
 TP_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
-if [ "$unamestr" == "Darwin" ]; then
-  brew update > /dev/null
-  brew install boost && true
-  brew install openssl
-  brew install bison
-  export OPENSSL_ROOT_DIR=/usr/local/opt/openssl
-  export LD_LIBRARY_PATH=/usr/local/opt/openssl/lib:$LD_LIBRARY_PATH
-  export PATH="/usr/local/opt/bison/bin:$PATH"
-else
-  export BOOST_ROOT=$TP_DIR/boost
-fi
-
 cd $TP_DIR/parquet-cpp
-ARROW_HOME=$TP_DIR/arrow/cpp/build/cpp-install
+ARROW_HOME=$TP_DIR/arrow/cpp/build/cpp-install \
+BOOST_ROOT=$TP_DIR/boost \
+OPENSSL_ROOT_DIR=/usr/local/opt/openssl \
+LD_LIBRARY_PATH=/usr/local/opt/openssl/lib:$LD_LIBRARY_PATH \
+PATH="/usr/local/opt/bison/bin:$PATH" \
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=$PARQUET_HOME \
       -DPARQUET_BUILD_BENCHMARKS=off \
--- src/thirdparty/download_thirdparty.sh
+++ src/thirdparty/download_thirdparty.sh
@@ -10,14 +10,15 @@ TP_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 if [ ! -d $TP_DIR/arrow ]; then
   git clone https://github.com/apache/arrow.git "$TP_DIR/arrow"
 fi
-cd $TP_DIR/arrow
-git fetch origin master
-git checkout e26f3dad3675288564ef0c0330a5c9afcac652f1
+pushd $TP_DIR/arrow
+  git fetch origin master
+  git checkout e26f3dad3675288564ef0c0330a5c9afcac652f1
+popd
 
-cd $TP_DIR
 if [ ! -d $TP_DIR/parquet-cpp ]; then
   git clone https://github.com/apache/parquet-cpp.git "$TP_DIR/parquet-cpp"
-  pushd $TP_DIR/parquet-cpp
-    git checkout 76388ea4eb8b23656283116bc656b0c8f5db093b
-  popd
 fi
+pushd $TP_DIR/parquet-cpp
+  git fetch origin master
+  git checkout 76388ea4eb8b23656283116bc656b0c8f5db093b
+popd
