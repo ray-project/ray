c0f627254b915f449e7a9afa252ad42738496479
Alexey Tumanov <atumanov@gmail.com>
2018-02-16 18:19:46 -0800

    raylet policy and queue - wip checkpoint

--- src/ray/raylet/LsPolicy.cc
+++ src/ray/raylet/LsPolicy.cc
@@ -3,9 +3,12 @@
 #include "LsPolicy.h"
 namespace ray{
 
-
-LsPolicy::LsPolicy() {
-
+/// Given a set of cluster resources, produce a placement decision on all work
+/// in the queue.
+unordered_map<TaskID, DBClientID> LsPolicy::Schedule(
+    const unordered_map<DBClientID, LsResources> &cluster_resources) {
+  // TODO(atumanov): policy implementation.
 }
+
 }
 #endif
