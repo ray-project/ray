6cdcb119f3c13ddb947bfc00eafb721d9537a79f
Richard Liaw <rliaw@berkeley.edu>
2018-02-20 15:00:03 -0800

    getting this to work preliminarily

--- python/ray/tune/sigopt_tune.py
+++ python/ray/tune/sigopt_tune.py
@@ -8,6 +8,9 @@ import copy
 import os
 import time
 from sigopt import Connection
+import argparse
+import sys
+import yaml
 
 import ray
 from ray.tune.trial import Trial, DEBUG_PRINT_INTERVAL
@@ -15,8 +18,11 @@ from ray.tune.error import TuneError
 from ray.tune.variant_generator import to_argv
 from ray.tune.trial_runner import TrialRunner
 from ray.tune.trial_scheduler import FIFOScheduler, TrialScheduler
-from ray.tune.config_parser import make_parser, json_to_resources
-# from ray.tune.confidential import SIGOPT_KEY
+from ray.tune.config_parser import make_parser, json_to_resources, resources_to_json
+from ray.tune import Trainable, TrainingResult, register_trainable
+
+
+from ray.tune.confidential import SIGOPT_KEY
 
 
 class SigOptScheduler(FIFOScheduler):
@@ -27,12 +33,23 @@ class SigOptScheduler(FIFOScheduler):
         assert len(experiments) == 1, "Currently only support 1 experiment"
         name, spec = list(experiments.keys())[0], list(experiments.values())[0]
         self.spec = spec
+
+        spec = copy.deepcopy(self.spec)
+        if "env" in spec:
+            spec["config"] = spec.get("config", {})
+            spec["config"]["env"] = spec["env"]
+            del spec["env"]
+
+        self.parser = make_parser()
+        self.args = self.parser.parse_args(to_argv(spec))
+
         parameters = []
+        self.default_config = copy.deepcopy(spec["config"])
         for keyname, val in spec["config"].items():
-            assert "sigopt" in val
-            vcopy = copy.deepcopy(val["sigopt"])
-            vcopy["name"] = keyname
-            parameters.append(vcopy)
+            if type(val) is dict:
+                vcopy = copy.deepcopy(val)
+                vcopy["name"] = keyname
+                parameters.append(vcopy)
 
         self.conn = Connection(client_token=SIGOPT_KEY)
         self.max_concurrent = 10
@@ -43,7 +60,6 @@ class SigOptScheduler(FIFOScheduler):
         )
         self.suggestions = {}
         self._reward_attr = reward_attr
-        self.parser = make_parser()
 
     def generate_trial(self):
         """Generate trial from Sigopt Service"""
@@ -51,14 +67,14 @@ class SigOptScheduler(FIFOScheduler):
 
         suggestion = self.conn.experiments(
             self.experiment.id).suggestions().create()
-
         suggested_config = dict(suggestion.assignments)
-
-        args = self.parser.parse_args(to_argv(self.spec))
+        new_cfg = copy.deepcopy(self.default_config)
+        new_cfg.update(suggested_config)
+        args = self.args
         experiment_tag = "sigopt_{}_{}".format(self.experiment.id, suggestion.id)
         trial = Trial(
             trainable_name=self.spec["run"],
-            config=suggested_config,
+            config=new_cfg,
             local_dir=args.local_dir,
             experiment_tag=experiment_tag,
             resources=json_to_resources(self.spec.get("resources", {})),
@@ -67,6 +83,7 @@ class SigOptScheduler(FIFOScheduler):
             restore_path=self.spec.get("restore"),
             upload_dir=args.upload_dir)
         self.suggestions[trial] = suggestion
+        print("Adding new trial - {}".format(len(self.suggestions)))
 
         return trial
 
@@ -119,20 +136,76 @@ def run_experiments(experiments, with_server=False,
     return runner.get_trials()
 
 
-if __name__ == '__main__':
-    from ray.tune import Trainable, TrainingResult, register_trainable
-    import json
-    ray.init(redis_address=ray.services.get_node_ip_address() + ":6379"))
+def parse_config_eval(config):
+    new_copy = {}
+    for k in config:
+        if k == "eval":
+            assert len(config) == 1 and type(config[k]) is str
+            return eval(config[k])
+        elif type(config[k]) is dict:
+            new_copy[k] = parse_config_eval(config[k])
+            print(new_copy[k])
+        else:
+            new_copy[k] = config[k]
 
-    run_experiments({
-        "sigopt": {
-            "run": "PPO",
-            "stop": {"training_iteration": 10},
-            "repeat": 20,
-            "resources": {"cpu": 1, "gpu": 0},
-            "config": {
-                "width": {"sigopt": dict(type='double', bounds=dict(min=10.0, max=100.0))},
-                "height": {"sigopt": dict(type='double', bounds=dict(min=0.0, max=100.0))},
-            },
+    return new_copy
+
+
+if __name__ == '__main__':
+    parser = make_parser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="Train a reinforcement learning agent with SigOPT.")
+
+    # See also the base parser definition in ray/tune/config_parser.py
+    parser.add_argument(
+        "--redis-address", default=None, type=str,
+        help="The Redis address of the cluster.")
+    parser.add_argument(
+        "--num-cpus", default=None, type=int,
+        help="Number of CPUs to allocate to Ray.")
+    parser.add_argument(
+        "--num-gpus", default=None, type=int,
+        help="Number of GPUs to allocate to Ray.")
+    parser.add_argument(
+        "--experiment-name", default="default", type=str,
+        help="Name of the subdirectory under `local_dir` to put results in.")
+    parser.add_argument(
+        "--env", default=None, type=str, help="The gym environment to use.")
+    parser.add_argument(
+        "-f", "--config-file", default=None, type=str,
+        help="If specified, use config options from this file. Note that this "
+        "overrides any trial-specific options set via flags above.")
+
+    args = parser.parse_args(sys.argv[1:])
+    if args.config_file:
+        with open(args.config_file) as f:
+            experiments = yaml.load(f)
+    else:
+        # Note: keep this in sync with tune/config_parser.py
+        experiments = {
+            args.experiment_name: {  # i.e. log to ~/ray_results/default
+                "run": args.run,
+                "checkpoint_freq": args.checkpoint_freq,
+                "local_dir": args.local_dir,
+                "resources": resources_to_json(args.resources),
+                "stop": args.stop,
+                "config": dict(args.config, env=args.env),
+                "restore": args.restore,
+                "repeat": args.repeat,
+                "upload_dir": args.upload_dir,
+            }
         }
-    }, )
+
+    for exp in experiments.values():
+        if not exp.get("run"):
+            parser.error("the following arguments are required: --run")
+        if not exp.get("env") and not exp.get("config", {}).get("env"):
+            parser.error("the following arguments are required: --env")
+
+    ray.init(
+        redis_address=args.redis_address,
+        num_cpus=args.num_cpus, num_gpus=args.num_gpus)
+
+    for exper in experiments:
+        experiments[exper]["config"] = parse_config_eval(experiments[exper]["config"])
+    run_experiments(experiments)
