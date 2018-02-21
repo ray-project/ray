f7d788915073d6a766b38af1782c98be15dad2ec
Stephanie <swang@cs.berkeley.edu>
2018-02-20 17:00:21 -0800

    Implement LsQueue RemoveTasks and QueueReadyTasks

--- src/ray/raylet/LsQueue.cc
+++ src/ray/raylet/LsQueue.cc
@@ -2,8 +2,11 @@
 #define LS_QUEUE_CC
 
 #include "LsQueue.h"
-#include "ray/id.h"
+
 #include <list>
+
+#include "common.h"
+#include "ray/id.h"
 
 namespace ray {
 
@@ -23,15 +26,48 @@ const std::list<Task>& LsQueue::ready_methods() const {
   throw std::runtime_error("Method not implemented");
 }
 
-std::vector<Task> LsQueue::RemoveTasks(std::vector<std::list<Task>::iterator> tasks) {
-  throw std::runtime_error("Method not implemented");
+// Helper function to remove tasks in the given set of task_ids from a
+// queue, and append them to the given vector removed_tasks.
+void removeTasksFromQueue(
+    std::unordered_set<TaskID, UniqueIDHasher> task_ids,
+    std::vector<Task> &removed_tasks,
+    std::list<Task> &queue) {
+  for (auto it = queue.begin();
+       it != queue.end(); ) {
+    auto task_id = task_ids.find(it->GetTaskSpecification().TaskId());
+    if (task_id != task_ids.end()) {
+      task_ids.erase(task_id);
+      removed_tasks.push_back(std::move(*it));
+      it = queue.erase(it);
+    } else {
+      it++;
+    }
+  }
 }
+
+std::vector<Task> LsQueue::RemoveTasks(std::unordered_set<TaskID, UniqueIDHasher> task_ids) {
+  std::vector<Task> removed_tasks;
+
+  // Try to find the tasks to remove from the waiting tasks.
+  removeTasksFromQueue(task_ids, removed_tasks, waiting_tasks_);
+  removeTasksFromQueue(task_ids, removed_tasks, ready_tasks_);
+  removeTasksFromQueue(task_ids, removed_tasks, running_tasks_);
+  // TODO(swang): Remove from running methods.
+
+  CHECK(task_ids.size() == 0);
+  return removed_tasks;
+}
+
 void LsQueue::QueueWaitingTasks(const std::vector<Task> &tasks) {
   throw std::runtime_error("Method not implemented");
 }
+
 void LsQueue::QueueReadyTasks(const std::vector<Task> &tasks) {
-  throw std::runtime_error("Method not implemented");
+  for (auto &task : tasks) {
+    ready_tasks_.push_back(task);
+  }
 }
+
 void LsQueue::QueueRunningTasks(const std::vector<Task> &tasks) {
   throw std::runtime_error("Method not implemented");
 }
