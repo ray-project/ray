30e8f3d79a7a61b231ad0b316fac5c49e7fb7803
Alexey Tumanov <atumanov@gmail.com>
2018-02-20 16:59:42 -0800

    lspolicy::schedule ; adding lsqueue and lspolicy to the nodeserver

--- src/ray/raylet/LsPolicy.cc
+++ src/ray/raylet/LsPolicy.cc
@@ -12,8 +12,26 @@ namespace ray{
 /// in the queue.
 std::unordered_map<TaskID, DBClientID, UniqueIDHasher> LsPolicy::Schedule(
     const std::unordered_map<DBClientID, LsResources, UniqueIDHasher> &cluster_resources) {
-  std::runtime_error("method not implemented");
-  return std::unordered_map<TaskID, DBClientID, UniqueIDHasher>();
+  // return data structure
+  std::unordered_map<TaskID, DBClientID, UniqueIDHasher> decision;
+
+  // Iterate over running tasks, get their resource demand and try to schedule.
+  TaskID last_task_id = TaskID::nil();
+  const auto ready_tasks = lsqueue_.ready_tasks();
+  for (const auto &t : ready_tasks) {
+    // Get task's resource demand
+    const auto &resource_demand_map = t.GetTaskSpecification().GetRequiredResources();
+    const auto &resource_demand = ResourceSet(resource_demand_map);
+    // TODO(atumanov): iterate over cluster_resources and locate available capacity.
+    LsResources resource_supply = cluster_resources.at(DBClientID::nil());
+    ResourceSet resource_supply_set = resource_supply.GetAvailableResources();
+    bool task_feasible = resource_demand.isSubset(resource_supply_set);
+    if (task_feasible) {
+      last_task_id = t.GetTaskSpecification().TaskId();
+      decision[last_task_id] = DBClientID::nil();
+    }
+  }
+  return decision;
 }
 
 } // end namespace ray
