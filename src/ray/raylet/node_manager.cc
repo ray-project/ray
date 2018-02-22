b3717df186a80b877ad88456abb5ba7f48f633e1
Stephanie <swang@cs.berkeley.edu>
2018-02-22 12:10:48 -0800

    Move local scheduler to separate file

--- src/ray/raylet/node_manager.cc
+++ src/ray/raylet/node_manager.cc
@@ -4,12 +4,6 @@
 
 #include <boost/bind.hpp>
 
-#include "common.h"
-#include "common_protocol.h"
-#include "format/nm_generated.h"
-#include "Task.h"
-#include "TaskSpecification.h"
-
 using namespace std;
 namespace ray {
 
@@ -37,107 +31,6 @@ void NodeServer::handleAccept(const boost::system::error_code& error) {
   }
   // We're ready to accept another client.
   doAccept();
-}
-
-LocalScheduler::LocalScheduler(
-                       const std::string &socket_name,
-                       const ResourceSet &resource_config)
-      : local_resources_(resource_config),
-        worker_pool_(WorkerPool(0)),
-        local_queues_(LsQueue()),
-        sched_policy_(local_queues_) {
-  //// TODO(atumanov): need to add the self-knowledge of DBClientID, using nill().
-  //cluster_resource_map_[DBClientID::nil()] = local_resources_;
-}
-
-
-void LocalScheduler::ProcessClientMessage(shared_ptr<ClientConnection> client, int64_t message_type, const uint8_t *message_data) {
-  LOG_INFO("Message of type %" PRId64, message_type);
-
-  switch (message_type) {
-  case MessageType_RegisterClientRequest: {
-    auto message = flatbuffers::GetRoot<RegisterClientRequest>(message_data);
-    if (message->is_worker()) {
-      // Create a new worker from the registration request.
-      Worker worker(message->worker_pid(), client);
-      // Add the new worker to the pool.
-      worker_pool_.AddWorker(std::move(worker));
-    }
-
-    // Build the reply to the worker's registration request. TODO(swang): This
-    // is legacy code and should be removed once actor creation tasks are
-    // implemented.
-    flatbuffers::FlatBufferBuilder fbb;
-    auto reply =
-        CreateRegisterClientReply(fbb, fbb.CreateVector(std::vector<int>()));
-    fbb.Finish(reply);
-    // Reply to the worker's registration request, then listen for more
-    // messages.
-    client->WriteMessage(MessageType_RegisterClientReply, fbb.GetSize(), fbb.GetBufferPointer());
-  } break;
-  case MessageType_DisconnectClient: {
-    // Remove the dead worker from the pool and stop listening for messages.
-    worker_pool_.RemoveWorker(client);
-  } break;
-  case MessageType_SubmitTask: {
-    // Read the task submitted by the client.
-    auto message = flatbuffers::GetRoot<SubmitTaskRequest>(message_data);
-    TaskExecutionSpecification task_execution_spec(from_flatbuf(*message->execution_dependencies()));
-    TaskSpecification task_spec(*message->task_spec());
-    Task task(task_execution_spec, task_spec);
-    // Submit the task to the local scheduler.
-    submitTask(task);
-  } break;
-  default:
-    CHECK(0);
-  }
-}
-
-void LocalScheduler::submitTask(Task& task) {
-  local_queues_.QueueReadyTasks(std::vector<Task>({task}));
-
-  // Ask policy for scheduling decision.
-  // TODO(alexey): Give the policy all cluster resources instead of just the
-  // local one.
-  std::unordered_map<DBClientID, LsResources, UniqueIDHasher> cluster_resource_map;
-  cluster_resource_map[DBClientID::nil()] = local_resources_;
-  const auto &sched_policy_decision = sched_policy_.Schedule(cluster_resource_map);
-  // Extract decision for this local scheduler.
-  std::unordered_set<TaskID, UniqueIDHasher> task_ids;
-  for (auto &task_schedule : sched_policy_decision) {
-    if (task_schedule.second.is_nil()) {
-      task_ids.insert(task_schedule.first);
-    }
-  }
-
-  // Assign the tasks to a worker.
-  std::vector<Task> tasks = local_queues_.RemoveTasks(task_ids);
-  for (auto &task : tasks) {
-    assignTask(task);
-  }
-}
-
-void LocalScheduler::assignTask(Task& task) {
-  if (worker_pool_.PoolSize() == 0) {
-    // TODO(swang): Start a new worker and queue this task for future
-    // assignment.
-    return;
-  }
-
-  Worker worker = worker_pool_.PopWorker();
-  LOG_INFO("Assigning task to worker with pid %d", worker.Pid());
-
-  // TODO(swang): Acquire resources for the task.
-  //local_resources_.Acquire(task.GetTaskSpecification().GetRequiredResources());
-
-  flatbuffers::FlatBufferBuilder fbb;
-  const TaskSpecification &spec = task.GetTaskSpecification();
-  auto message =
-      CreateGetTaskReply(fbb, spec.ToFlatbuffer(fbb),
-                         fbb.CreateVector(std::vector<int>()));
-  fbb.Finish(message);
-  worker.Connection()->WriteMessage(MessageType_ExecuteTask, fbb.GetSize(), fbb.GetBufferPointer());
-  local_queues_.QueueRunningTasks(std::vector<Task>({task}));
 }
 
 } // end namespace ray
--- src/ray/raylet/node_manager.h
+++ src/ray/raylet/node_manager.h
@@ -6,53 +6,14 @@
 #include <boost/asio.hpp>
 #include <boost/asio/error.hpp>
 
+#include "local_scheduler.h"
 #include "LsResources.h"
-#include "LsQueue.h"
-#include "LsPolicy.h"
-#include "WorkerPool.h"
 
 using namespace std;
 namespace ray {
 
 class Task;
-class Worker;
-
-class ClientManager {
- public:
-  /// Process a message from a client, then listen for more messages if the
-  /// client is still alive.
-  virtual void ProcessClientMessage(
-      shared_ptr<ClientConnection> client,
-      int64_t message_type,
-      const uint8_t *message) = 0;
-};
-
-class LocalScheduler : public ClientManager {
- public:
-  LocalScheduler(
-      const std::string &socket_name,
-      const ResourceSet &resource_config);
-  /// Process a message from a client, then listen for more messages if the
-  /// client is still alive.
-  void ProcessClientMessage(shared_ptr<ClientConnection> client, int64_t message_type, const uint8_t *message);
- private:
-  /// Submit a task to this node.
-  void submitTask(Task& task);
-  /// Assign a task.
-  void assignTask(Task& task);
-
-  /// The resources local to this node.
-  LsResources local_resources_;
-  // TODO(alexey): Add resource information from other nodes.
-  //std::unordered_map<DBClientID, LsResources&, UniqueIDHasher> cluster_resource_map_;
-  // A set of workers, in a WorkerPool()
-  WorkerPool worker_pool_;
-  // A set of queues that maintain tasks enqueued in pending, ready, running
-  // states.
-  LsQueue local_queues_;
-  // Scheduling policy in effect for this local scheduler.
-  LsPolicy sched_policy_;
-};
+class LocalScheduler;
 
 class NodeServer {
  public:
