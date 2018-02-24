b4fb262412d3603224351723022ce7924ad48b93
Melih Elibol <elibol@gmail.com>
2018-02-24 12:56:52 -0800

    remove use namespace std from .h files. use ray:: for Status everywhere.

--- src/ray/om/object_manager.cc
+++ src/ray/om/object_manager.cc
@@ -23,32 +23,37 @@ ObjectManager::ObjectManager(boost::asio::io_service &io_service,
 ray::Status ObjectManager::Terminate() {
   this->od->Terminate();
   this->store_client->Terminate();
+  return ray::Status::OK();
 };
 
 //ray::Status ObjectManager::SubscribeObjAdded(void (*callback)(const ObjectID&)) {
 ray::Status ObjectManager::SubscribeObjAdded(std::function<void(const ObjectID&)> callback) {
   this->store_client->SubscribeObjAdded(callback);
+  return ray::Status::OK();
 };
 
 ray::Status ObjectManager::SubscribeObjDeleted(std::function<void(const ObjectID&)> callback) {
   this->store_client->SubscribeObjDeleted(callback);
+  return ray::Status::OK();
 };
 
 ray::Status ObjectManager::Push(const ObjectID &object_id,
                          const ClientID &dbclient_id) {
   this->ExecutePush(object_id, dbclient_id);
+  return ray::Status::OK();
 };
 
 ray::Status ObjectManager::Pull(const ObjectID &object_id) {
   this->od->GetLocations(object_id,
                          [this](const vector<ODRemoteConnectionInfo>& v,
-                             const ObjectID &object_id) {
-                          return this->GetLocationSuccess(v, object_id);
-  } ,
+                                const ObjectID &object_id) {
+                           return this->GetLocationsSuccess(v, object_id);
+                         } ,
                          [this](ray::Status status,
-                             const ObjectID &object_id) {
-                          return this->GetLocationsFailed(status, object_id);
-  });
+                                const ObjectID &object_id) {
+                           return this->GetLocationsFailed(status, object_id);
+                         });
+  return ray::Status::OK();
 };
 //                         bind(&ObjectManager::GetLocationsSuccess, this,
 //                              placeholders::_1,
@@ -61,38 +66,42 @@ ray::Status ObjectManager::Pull(const ObjectID &object_id) {
 // Private callback implementation for success on get location. Called inside OD.
 void ObjectManager::GetLocationsSuccess(const vector<ray::ODRemoteConnectionInfo> &v,
                                         const ray::ObjectID &object_id) {
-  this->ExecutePull(object_id, v.front().dbc_id);
+  this->ExecutePull(object_id, v.front().client_id);
 };
 
 // Private callback impelmentation for failure on get location. Called inside OD.
-void ObjectManager::GetLocationsFailed(Status status,
+void ObjectManager::GetLocationsFailed(ray::Status status,
                                        const ObjectID &object_id){
   throw std::runtime_error("GetLocations Failed.");
 };
 
 ray::Status ObjectManager::Pull(const ObjectID &object_id,
-                         const ClientID &dbclient_id) {
-  this->ExecutePull(object_id, dbclient_id);
+                                const ClientID &dbclient_id) {
+  return this->ExecutePull(object_id, dbclient_id);
 };
 
 ray::Status ObjectManager::ExecutePull(const ObjectID &object_id,
-                                const ClientID &dbclient_id) {
+                                       const ClientID &dbclient_id) {
   // TODO(hme): Lookup connection and pull.
+  return ray::Status::OK();
 };
 
 ray::Status ObjectManager::ExecutePush(const ObjectID &object_id,
-                                const ClientID &dbclient_id) {
+                                       const ClientID &dbclient_id) {
   // TODO(hme): Lookup connection and push.
+  return ray::Status::OK();
 };
 
 ray::Status ObjectManager::Cancel(const ObjectID &object_id) {
-  this->od->Cancel(object_id);
+  ray::Status status = this->od->Cancel(object_id);
+  return ray::Status::OK();
 };
 
-ray::Status ObjectManager::Wait(const list<ObjectID> &object_ids,
-                           uint64_t timeout_ms,
-                           int num_ready_objects,
-                           const WaitCallback &callback) {
+ray::Status ObjectManager::Wait(const vector<ObjectID> &object_ids,
+                                uint64_t timeout_ms,
+                                int num_ready_objects,
+                                const WaitCallback &callback) {
+  return ray::Status::OK();
 };
 
 } // end ray
