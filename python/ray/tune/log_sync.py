641f8db95d48f6981eeed568b685c2c93df7173f
Eric Liang <ekhliang@gmail.com>
2018-02-26 11:35:51 -0800

    [tune] Sync logs from workers and improve tensorboard reporting (#1567)

--- python/ray/tune/log_sync.py
+++ python/ray/tune/log_sync.py
@@ -4,9 +4,12 @@ from __future__ import print_function
 
 import distutils.spawn
 import os
+import pipes
 import subprocess
 import time
 
+import ray
+from ray.tune.cluster_info import get_ssh_key, get_ssh_user
 from ray.tune.error import TuneError
 from ray.tune.result import DEFAULT_RESULTS_DIR
 
@@ -15,20 +18,21 @@ from ray.tune.result import DEFAULT_RESULTS_DIR
 _syncers = {}
 
 
-def get_syncer(local_dir, remote_dir):
-    if not remote_dir.startswith("s3://"):
-        raise TuneError("Upload uri must start with s3://")
+def get_syncer(local_dir, remote_dir=None):
+    if remote_dir:
+        if not remote_dir.startswith("s3://"):
+            raise TuneError("Upload uri must start with s3://")
 
-    if not distutils.spawn.find_executable("aws"):
-        raise TuneError("Upload uri requires awscli tool to be installed")
+        if not distutils.spawn.find_executable("aws"):
+            raise TuneError("Upload uri requires awscli tool to be installed")
 
-    if local_dir.startswith(DEFAULT_RESULTS_DIR + "/"):
-        rel_path = os.path.relpath(local_dir, DEFAULT_RESULTS_DIR)
-        remote_dir = os.path.join(remote_dir, rel_path)
+        if local_dir.startswith(DEFAULT_RESULTS_DIR + "/"):
+            rel_path = os.path.relpath(local_dir, DEFAULT_RESULTS_DIR)
+            remote_dir = os.path.join(remote_dir, rel_path)
 
     key = (local_dir, remote_dir)
     if key not in _syncers:
-        _syncers[key] = _S3LogSyncer(local_dir, remote_dir)
+        _syncers[key] = _LogSyncer(local_dir, remote_dir)
 
     return _syncers[key]
 
@@ -38,23 +42,64 @@ def wait_for_log_sync():
         syncer.wait()
 
 
-class _S3LogSyncer(object):
-    def __init__(self, local_dir, remote_dir):
+class _LogSyncer(object):
+    """Log syncer for tune.
+
+    This syncs files from workers to the local node, and optionally also from
+    the local node to a remote directory (e.g. S3)."""
+
+    def __init__(self, local_dir, remote_dir=None):
         self.local_dir = local_dir
         self.remote_dir = remote_dir
         self.last_sync_time = 0
         self.sync_process = None
-        print("Created S3LogSyncer for {} -> {}".format(local_dir, remote_dir))
+        self.local_ip = ray.services.get_node_ip_address()
+        self.worker_ip = None
+        print("Created LogSyncer for {} -> {}".format(local_dir, remote_dir))
+
+    def set_worker_ip(self, worker_ip):
+        """Set the worker ip to sync logs from."""
+
+        self.worker_ip = worker_ip
 
     def sync_if_needed(self):
         if time.time() - self.last_sync_time > 300:
             self.sync_now()
 
     def sync_now(self, force=False):
-        print(
-            "Syncing files from {} -> {}".format(
-                self.local_dir, self.remote_dir))
         self.last_sync_time = time.time()
+        if not self.worker_ip:
+            print(
+                "Worker ip unknown, skipping log sync for {}".format(
+                    self.local_dir))
+            return
+
+        if self.worker_ip == self.local_ip:
+            worker_to_local_sync_cmd = None  # don't need to rsync
+        else:
+            ssh_key = get_ssh_key()
+            ssh_user = get_ssh_user()
+            if ssh_key is None or ssh_user is None:
+                print(
+                    "Error: log sync requires cluster to be setup with "
+                    "`ray create_or_update`.")
+                return
+            if not distutils.spawn.find_executable("rsync"):
+                print("Error: log sync requires rsync to be installed.")
+                return
+            worker_to_local_sync_cmd = (
+                ("""rsync -avz -e "ssh -i '{}' -o ConnectTimeout=120s """
+                 """-o StrictHostKeyChecking=no" '{}@{}:{}/' '{}/'""").format(
+                    ssh_key, ssh_user, self.worker_ip,
+                    pipes.quote(self.local_dir), pipes.quote(self.local_dir)))
+
+        if self.remote_dir:
+            local_to_remote_sync_cmd = (
+                "aws s3 sync '{}' '{}'".format(
+                    pipes.quote(self.local_dir), pipes.quote(self.remote_dir)))
+        else:
+            local_to_remote_sync_cmd = None
+
         if self.sync_process:
             self.sync_process.poll()
             if self.sync_process.returncode is None:
@@ -63,8 +108,17 @@ class _S3LogSyncer(object):
                 else:
                     print("Warning: last sync is still in progress, skipping")
                     return
-        self.sync_process = subprocess.Popen(
-            ["aws", "s3", "sync", self.local_dir, self.remote_dir])
+
+        if worker_to_local_sync_cmd or local_to_remote_sync_cmd:
+            final_cmd = ""
+            if worker_to_local_sync_cmd:
+                final_cmd += worker_to_local_sync_cmd
+            if local_to_remote_sync_cmd:
+                if final_cmd:
+                    final_cmd += " && "
+                final_cmd += local_to_remote_sync_cmd
+            print("Running log sync: {}".format(final_cmd))
+            self.sync_process = subprocess.Popen(final_cmd, shell=True)
 
     def wait(self):
         if self.sync_process:
