942af1e9d37829d262431f94fde7767ad5e1a342
Stephanie <swang@cs.berkeley.edu>
2018-02-26 16:03:08 -0800

    Simplify Task interface for getting dependencies

--- src/ray/raylet/Task.h
+++ src/ray/raylet/Task.h
@@ -16,24 +16,11 @@ public:
   const TaskSpecification &GetTaskSpecification() const;
   virtual ~Task() {}
 
-  /// Get the number of dependencies. This comprises the immutable task
-  /// arguments and the mutable execution dependencies.
+  /// Get the dependencies. This comprises the immutable task arguments and the
+  /// mutable execution dependencies.
   ///
-  /// @return The number of dependencies.
-  int64_t NumDependencies() const;
-
-  /// Get the number of object IDs at the given dependency index.
-  ///
-  /// @param dependency_index The dependency index whose object IDs to count.
-  /// @return The number of object IDs at the given dependency_index.
-  int DependencyIdCount(int64_t dependency_index) const;
-
-  /// Get the object ID of a given dependency index.
-  ///
-  /// @param dependency_index The index at which we should look up the object
-  ///        ID.
-  /// @param id_index The index of the object ID.
-  ObjectID DependencyId(int64_t dependency_index, int64_t id_index) const;
+  /// @return The object dependencies.
+  const std::vector<ObjectID> GetDependencies() const;
 
   /// Compute whether the task is dependent on an object ID.
   ///
@@ -41,14 +28,6 @@ public:
   /// @return bool This returns true if the task is dependent on the given
   ///         object ID and false otherwise.
   bool DependsOn(ObjectID object_id) const;
-
-  /// Returns whether the given dependency index is a static dependency (an
-  /// argument of the immutable task).
-  ///
-  /// @param dependency_index The requested dependency index.
-  /// @return bool This returns true if the requested dependency index is
-  ///         immutable (an argument of the task).
-  bool IsStaticDependency(int64_t dependency_index) const;
 
 private:
   /// Task execution specification object, consisting of all dynamic/mutable
