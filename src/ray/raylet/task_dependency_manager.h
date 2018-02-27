05f94c546c6f4285c1ae35dddad5c5f4166eec1c
Stephanie <swang@cs.berkeley.edu>
2018-02-26 17:00:25 -0800

    TaskDependencyManager tracks missing task dependencies and processes object add notifications

--- src/ray/raylet/task_dependency_manager.h
+++ src/ray/raylet/task_dependency_manager.h
@@ -15,15 +15,40 @@ class TaskDependencyManager {
  public:
   TaskDependencyManager(
       ObjectManager &object_manager,
-      ReconstructionPolicy &reconstruction_policy);
-  bool TaskReady(Task &task);
-  void RegisterTask(Task &task);
-  void DeregisterTask(Task &task);
-  void RegisterTaskReadyHandler(boost::function<void(const TaskID&)> handler);
+      ReconstructionPolicy &reconstruction_policy,
+      boost::function<void(const TaskID&)> handler);
+  // Check whether a task's object dependencies are locally available and is
+  // therefore ready to run.
+  bool TaskReady(Task &task) const;
+  // Subscribe to the TaskReady callback for a task that has missing
+  // dependencies. The registered TaskReady callback will be called when the
+  // task's dependencies are locally available.
+  void SubscribeTaskReady(Task &task);
+  // Stop waiting for a task's dependencies to become available.
+  void UnsubscribeTaskReady(const TaskID &task_id);
+  // Mark an object as locally available.
   void MarkDependencyReady(const ObjectID &object);
  private:
+  // Check whether the given list of objects are ready.
+  bool argumentsReady(const std::vector<ObjectID> arguments) const;
+  // Handle an object added to the object store.
+  void handleObjectReady(const ray::ObjectID& object_id);
+  // A reference to the object manager so that we can issue Pull requests of
+  // missing objects.
   ObjectManager &object_manager_;
+  // A reference to the reconstruction policy so that we can decide when
+  // objects should be reconstructed.
   ReconstructionPolicy &reconstruction_policy_;
+  // A mapping from task ID of each subscribed task to its list of
+  // dependencies.
+  std::unordered_map<ray::TaskID, std::vector<ray::ObjectID>, UniqueIDHasher> task_dependencies_;
+  // A mapping from object ID of each object that is not locally available to
+  // the list of subscribed tasks that are dependent on it.
+  std::unordered_map<ray::ObjectID, std::vector<ray::TaskID>, UniqueIDHasher> remote_object_dependencies_;
+  // The set of locally available objects.
+  std::unordered_set<ray::ObjectID, UniqueIDHasher> local_objects_;
+  // The callback to call when a subscribed task becomes ready.
+  boost::function<void(const TaskID&)> task_ready_callback_;
 };
 
 } // end namespace ray
