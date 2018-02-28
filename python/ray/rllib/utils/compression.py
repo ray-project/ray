80b83988bfd3c1a5462aeb84e2f5b7c019cf5f7a
Eric Liang <ekhliang@gmail.com>
2018-02-28 11:59:58 -0800

    Wed Feb 28 11:59:58 PST 2018

--- python/ray/rllib/utils/compression.py
+++ python/ray/rllib/utils/compression.py
@@ -5,28 +5,17 @@ from __future__ import print_function
 import base64
 import pyarrow
 
-try:
-    import snappy
-    SNAPPY_ENABLED = True
-except ImportError:
-    print("WARNING: python-snappy not available, disabling sample compression")
-    SNAPPY_ENABLED = False
+import snappy
 
 
 def pack(data):
-    if SNAPPY_ENABLED:
-        data = snappy.compress(
-            pyarrow.serialize(data).to_buffer().to_pybytes())
-        # TODO(ekl) we shouldn't need to base64 encode this data, but this
-        # seems to not survive a transfer through the object store if we don't.
-        return base64.b64encode(data)
-    else:
-        return data
+    data = snappy.compress(
+        pyarrow.serialize(data).to_buffer().to_pybytes())
+    # TODO(ekl) we shouldn't need to base64 encode this data, but this
+    # seems to not survive a transfer through the object store if we don't.
+    return base64.b64encode(data)
 
 
 def unpack(data):
-    if SNAPPY_ENABLED:
-        data = base64.b64decode(data)
-        return pyarrow.deserialize(snappy.decompress(data))
-    else:
-        return data
+    data = base64.b64decode(data)
+    return pyarrow.deserialize(snappy.decompress(data))
