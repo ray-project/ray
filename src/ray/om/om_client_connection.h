ef5b149900e4e61707de1e8196e81d9054d66ddf
Melih Elibol <elibol@gmail.com>
2018-02-27 22:50:48 -0800

    clean up connection code.

--- src/ray/om/om_client_connection.h
+++ src/ray/om/om_client_connection.h
@@ -28,55 +28,21 @@ class SenderConnection : public boost::enable_shared_from_this<SenderConnection>
   typedef std::deque<ray::ObjectID> SendQueueType;
 
   static pointer Create(boost::asio::io_service& io_service,
-                        const std::string &ip, ushort port){
-    return pointer(new SenderConnection(io_service, ip, port));
-  };
+                        const std::string &ip, ushort port);
 
   explicit SenderConnection(boost::asio::io_service& io_service,
-                            const std::string &ip, ushort port) :
-      socket_(io_service),
-      send_queue_()
-  {
-    boost::asio::ip::address addr = boost::asio::ip::address::from_string(ip);
-    boost::asio::ip::tcp::endpoint endpoint(addr, port);
-    socket_.connect(endpoint);
-  };
+const std::string &ip, ushort port);
 
-  boost::asio::ip::tcp::socket &GetSocket(){
-    return socket_;
-  };
+  boost::asio::ip::tcp::socket &GetSocket();
 
-  bool IsObjectIdQueueEmpty(){
-    return send_queue_.empty();
-  }
+  bool IsObjectIdQueueEmpty();
+  bool ObjectIdQueued(const ObjectID &object_id);
+  void QueueObjectId(const ObjectID &object_id);
+  ObjectID DeQueueObjectId();
 
-  bool ObjectIdQueued(const ObjectID &object_id){
-    return std::find(send_queue_.begin(),
-                     send_queue_.end(),
-                     object_id)!=send_queue_.end();
-  }
-
-  void QueueObjectId(const ObjectID &object_id){
-    send_queue_.push_back(ObjectID(object_id));
-  }
-
-  ObjectID DeQueueObjectId(){
-    ObjectID object_id = send_queue_.front();
-    send_queue_.pop_front();
-    return object_id;
-  }
-
-  void AddSendRequest(const ObjectID &object_id, SendRequest &send_request){
-    send_requests_.emplace(object_id, send_request);
-  }
-
-  void RemoveSendRequest(const ObjectID &object_id){
-    send_requests_.erase(object_id);
-  }
-
-  SendRequest &GetSendRequest(const ObjectID &object_id){
-    return send_requests_[object_id];
-  };
+  void AddSendRequest(const ObjectID &object_id, SendRequest &send_request);
+  void RemoveSendRequest(const ObjectID &object_id);
+  SendRequest &GetSendRequest(const ObjectID &object_id);
 
  private:
   boost::asio::ip::tcp::socket socket_;
