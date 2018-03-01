037003153dd19e8935f7f959435db28a07af2732
Stephanie <swang@cs.berkeley.edu>
2018-02-28 19:21:32 -0800

    Separate read and write buffers in ClientConnection; documentation

--- src/ray/raylet/client_connection.cc
+++ src/ray/raylet/client_connection.cc
@@ -33,9 +33,9 @@ void ClientConnection<T>::ProcessMessages() {
   // Wait for a message header from the client. The message header includes the
   // protocol version, the message type, and the length of the message.
   std::vector<boost::asio::mutable_buffer> header;
-  header.push_back(boost::asio::buffer(&version_, sizeof(version_)));
-  header.push_back(boost::asio::buffer(&type_, sizeof(type_)));
-  header.push_back(boost::asio::buffer(&length_, sizeof(length_)));
+  header.push_back(boost::asio::buffer(&read_version_, sizeof(read_version_)));
+  header.push_back(boost::asio::buffer(&read_type_, sizeof(read_type_)));
+  header.push_back(boost::asio::buffer(&read_length_, sizeof(read_length_)));
   boost::asio::async_read(socket_, header, boost::bind(&ClientConnection<T>::processMessageHeader, this->shared_from_this(), boost::asio::placeholders::error));
 }
 
@@ -43,18 +43,18 @@ template <class T>
 void ClientConnection<T>::processMessageHeader(const boost::system::error_code& error) {
   if (error) {
     // If there was an error, disconnect the client.
-    type_ = MessageType_DisconnectClient;
-    length_ = 0;
+    read_type_ = MessageType_DisconnectClient;
+    read_length_ = 0;
     processMessage(error);
     return;
   }
 
   // If there was no error, make sure the protocol version matches.
-  CHECK(version_ == RayConfig::instance().ray_protocol_version());
+  CHECK(read_version_ == RayConfig::instance().ray_protocol_version());
   // Resize the message buffer to match the received length.
-  message_.resize(length_);
+  read_message_.resize(read_length_);
   // Wait for the message to be read.
-  boost::asio::async_read(socket_, boost::asio::buffer(message_),
+  boost::asio::async_read(socket_, boost::asio::buffer(read_message_),
       boost::bind(&ClientConnection<T>::processMessage, this->shared_from_this(), boost::asio::placeholders::error)
       );
 }
@@ -62,13 +62,14 @@ void ClientConnection<T>::processMessageHeader(const boost::system::error_code& 
 template <class T>
 void ClientConnection<T>::WriteMessage(int64_t type, size_t length, const uint8_t *message) {
   std::vector<boost::asio::const_buffer> message_buffers;
-  version_ = RayConfig::instance().ray_protocol_version();
-  type_ = type;
-  length_ = length;
-  message_buffers.push_back(boost::asio::buffer(&version_, sizeof(version_)));
-  message_buffers.push_back(boost::asio::buffer(&type_, sizeof(type_)));
-  message_buffers.push_back(boost::asio::buffer(&length_, sizeof(length_)));
-  message_buffers.push_back(boost::asio::buffer(message, length));
+  write_version_ = RayConfig::instance().ray_protocol_version();
+  write_type_ = type;
+  write_length_ = length;
+  write_message_.assign(message, message + length);
+  message_buffers.push_back(boost::asio::buffer(&write_version_, sizeof(write_version_)));
+  message_buffers.push_back(boost::asio::buffer(&write_type_, sizeof(write_type_)));
+  message_buffers.push_back(boost::asio::buffer(&write_length_, sizeof(write_length_)));
+  message_buffers.push_back(boost::asio::buffer(write_message_));
   boost::system::error_code error;
   // Write the message and then wait for more messages.
   boost::asio::async_write(socket_, message_buffers, boost::bind(&ClientConnection<T>::processMessages, this->shared_from_this(), boost::asio::placeholders::error));
@@ -77,9 +78,9 @@ void ClientConnection<T>::WriteMessage(int64_t type, size_t length, const uint8_
 template <class T>
 void ClientConnection<T>::processMessage(const boost::system::error_code& error) {
   if (error) {
-    type_ = MessageType_DisconnectClient;
+    read_type_ = MessageType_DisconnectClient;
   }
-  manager_.ProcessClientMessage(this->shared_from_this(), type_, message_.data());
+  manager_.ProcessClientMessage(this->shared_from_this(), read_type_, read_message_.data());
 }
 
 template <class T>
