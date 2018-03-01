e27ee5ab9ec6594c0007466068c50aaa1538452b
Stephanie <swang@cs.berkeley.edu>
2018-03-01 14:41:13 -0800

    SchedulingQueue doc

--- src/ray/raylet/scheduling_queue.h
+++ src/ray/raylet/scheduling_queue.h
@@ -11,45 +11,97 @@
 
 namespace ray {
 
-/// SchedulingQueue: encapsulates task queues. Responsible for task queue transitions.
+/// \class SchedulingQueue
 ///
+/// Encapsulates task queues. Each queue represents a scheduling state for a
+/// task. The scheduling state is one of (1) waiting: for object dependencies
+/// to become available, (2) ready: object dependencies are available and the
+/// task is ready to be scheduled, (3) scheduled: the task has been scheduled
+/// but is waiting for a worker, or (4) running: the task has been scheduled
+/// and is running on a worker.
 class SchedulingQueue {
  public:
-  /// public constructor of the SchedulingQueue class
+  /// Create a scheduling queue.
   SchedulingQueue() {}
 
-  /// SchedulingQueue destructor
+  /// SchedulingQueue destructor.
   virtual ~SchedulingQueue() {}
 
-  /// Return a list of tasks in the waiting state.
+  /// Get the queue of tasks in the waiting state.
+  ///
+  /// \return A const reference to the queue of tasks that are waiting for
+  /// object dependencies to become available.
   const std::list<Task>& waiting_tasks() const;
-  /// Return a list of tasks in the ready state.
+
+  /// Get the queue of tasks in the ready state.
+  ///
+  /// \return A const reference to the queue of tasks that have all
+  /// dependencies local and that are waiting to be scheduled.
   const std::list<Task>& ready_tasks() const;
-  /// Return a list of methods in the ready state.
+
+  /// Get the queue of actor methods in the ready state.
+  ///
+  /// \return A const reference to the queue of actor methods that have all
+  /// dependencies local and that are waiting to be scheduled.
   const std::list<Task>& ready_methods() const;
-  /// Return a list of tasks in the scheduled state.
+
+  /// Get the queue of tasks in the scheduled state.
+  ///
+  /// \return A const reference to the queue of tasks that have been scheduled
+  /// to execute but that are waiting for a worker.
   const std::list<Task>& scheduled_tasks() const;
-  /// Return a list of tasks in the running state.
+
+  /// Get the queue of tasks in the running state.
+  ///
+  /// \return A const reference to the queue of tasks that are currently
+  /// executing on a worker.
   const std::list<Task>& running_tasks() const;
+
+  /// Remove tasks from the task queue.
+  ///
+  /// \param tasks The set of task IDs to remove from the queue. The
+  ///        corresponding tasks must be contained in the queue.
+  /// \return A vector of the tasks that were removed.
   std::vector<Task> RemoveTasks(std::unordered_set<TaskID, UniqueIDHasher> tasks);
+
+  /// Queue tasks in the waiting state.
+  ///
+  /// \param tasks The tasks to queue.
   void QueueWaitingTasks(const std::vector<Task> &tasks);
+
+  /// Queue tasks in the ready state.
+  ///
+  /// \param tasks The tasks to queue.
   void QueueReadyTasks(const std::vector<Task> &tasks);
+
+  /// Queue tasks in the scheduled state.
+  ///
+  /// \param tasks The tasks to queue.
   void QueueScheduledTasks(const std::vector<Task> &tasks);
+
+  /// Queue tasks in the running state.
+  ///
+  /// \param tasks The tasks to queue.
   void QueueRunningTasks(const std::vector<Task> &tasks);
+
+  /// Register an actor.
+  ///
+  /// \param actor_id The ID of the actor to register.
+  /// \param actor_information Information about the actor.
   bool RegisterActor(ActorID actor_id,
                      const ActorInformation &actor_information);
 
  private:
-  // Tasks that are waiting for an object dependency to appear locally.
+  /// Tasks that are waiting for an object dependency to appear locally.
   std::list<Task> waiting_tasks_;
-  // Tasks whose object dependencies are locally available, but that are
-  // waiting for resources to run.
+  /// Tasks whose object dependencies are locally available, but that are
+  /// waiting to be scheduled.
   std::list<Task> ready_tasks_;
-  // Tasks that have resources and are scheduled to run, but that are waiting
-  // for a worker.
+  /// Tasks that have been scheduled to run, but that are waiting for a worker.
   std::list<Task> scheduled_tasks_;
-  // Tasks that have acquired resources and are running on a worker.
+  /// Tasks that are running on a worker.
   std::list<Task> running_tasks_;
+  /// The registry of known actors.
   std::unordered_map<ActorID, ActorInformation, UniqueIDHasher> actor_registry_;
 };
 } // end namespace ray
