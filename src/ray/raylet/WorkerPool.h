ad0fd0da7beca1ae6ffa8a87a2bc6d090653dfdb
Stephanie <swang@cs.berkeley.edu>
2018-02-28 20:20:53 -0800

    WorkerPool documentation

--- src/ray/raylet/WorkerPool.h
+++ src/ray/raylet/WorkerPool.h
@@ -12,39 +12,67 @@ namespace ray {
 
 class Worker;
 
-/// WorkerPool class is responsible for managing a pool of workers and
-/// abstracts away the implementation details of workers. From Raylet's
-/// perspective a Worker is a container that encapsulates a unit of work.
+/// \class WorkerPool
+///
+/// The WorkerPool is responsible for managing a pool of Workers. Each Worker
+/// is a container for a unit of work.
 class WorkerPool {
 public:
-  /// Constructor that creates a pool with a set of workers of specified size.
+  /// Create a pool and asynchronously start the specified number of workers.
+  /// Once each worker process has registered with an external server, the
+  /// process should create and register a new Worker, then add itself to the
+  /// pool.
+  ///
+  /// \param num_workers The number of workers to start.
   WorkerPool(int num_workers);
-  /// Get the number of idle workers.
-  uint32_t PoolSize() const;
-  /// Start a worker. The worker will be added to the pool after it
-  /// connects, registers, and requests a task assignment.
-  bool StartWorker();
-  /// Register a new worker. The worker will be added to the pool after it
-  /// requests a task assignment.
-  void RegisterWorker(std::shared_ptr<Worker> worker);
-  /// Get the client connection's registered worker. Returns nullptr if the
-  /// client has not registered a worker yet.
-  const std::shared_ptr<Worker> GetRegisteredWorker(std::shared_ptr<LocalClientConnection> connection) const;
-  /// Disconnect the given worker. Returns true if the given worker was in the
-  /// pool of idle workers.
-  bool DisconnectWorker(shared_ptr<Worker> worker);
-
-  /// Add an idle worker to the pool.
-  void PushWorker(std::shared_ptr<Worker> worker);
-  /// Pop an idle worker from the pool. Returns nullptr if the pool is empty.
-  std::shared_ptr<Worker> PopWorker();
 
   /// Destructor responsible for freeing a set of workers owned by this class.
   ~WorkerPool();
+
+  /// Get the number of idle workers in the pool.
+  ///
+  /// \return The number of idle workers.
+  uint32_t PoolSize() const;
+
+  /// Asynchronously start a new worker process. Once the worker process has
+  /// registered with an external server, the process should create and
+  /// register a new Worker, then add itself to the pool.
+  ///
+  /// \return Whether the worker process was successfully started.
+  bool StartWorker();
+
+  /// Register a new worker. The Worker should be added by the caller to the
+  /// pool after it becomes idle (e.g., requests a work assignment).
+  ///
+  /// \param The Worker to be registered.
+  void RegisterWorker(std::shared_ptr<Worker> worker);
+
+  /// Get the client connection's registered worker.
+  ///
+  /// \param The client connection owned by a registered worker.
+  /// \return The Worker that owns the given client connection. Returns nullptr
+  /// if the client has not registered a worker yet.
+  const std::shared_ptr<Worker> GetRegisteredWorker(std::shared_ptr<LocalClientConnection> connection) const;
+
+  /// Disconnect a registered worker.
+  ///
+  /// \param The worker to disconnect. The worker must be registered.
+  /// \return Whether the given worker was in the pool of idle workers.
+  bool DisconnectWorker(shared_ptr<Worker> worker);
+
+  /// Add an idle worker to the pool. The worker's task assignment will be
+  /// reset.
+  ///
+  /// \param The idle worker to add.
+  void PushWorker(std::shared_ptr<Worker> worker);
+
+  /// Pop an idle worker from the pool. The caller is responsible for pushing
+  /// the worker back onto the pool once the worker has completed its work.
+  ///
+  /// \return An idle worker. Returns nullptr if the pool is empty.
+  std::shared_ptr<Worker> PopWorker();
+
 private:
-  /// The initial size of the worker pool. Current size is the size of the
-  /// worker pool container.
-  int init_size_;
   /// The pool of idle workers.
   std::list<std::shared_ptr<Worker>> pool_;
   /// All workers that have registered and are still connected, including both
