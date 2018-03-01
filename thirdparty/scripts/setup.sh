f1e5789c2694ae051c021775356efaf2c4b8f50c
Zhenyu Guo <imzhenyu@users.noreply.github.com>
2018-03-01 14:29:56 -0800

    restructure how to organize 3rd party libs (#1630)
    * restructure how to organize 3rd party libs
    
    * Minor whitespace changes.
    
    * Fix compilation on Linux.
    
    * Pass around Python executable so that the correct version of Python is used.

---
+++ thirdparty/scripts/setup.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -x
+
+# Cause the script to exit if a single command fails.
+set -e
+
+TP_SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
+TP_DIR=$TP_SCRIPT_DIR/..
+
+mkdir -p $TP_DIR/build
+mkdir -p $TP_DIR/pkg
+
+if [[ -z  "$1" ]]; then
+  PYTHON_EXECUTABLE=`which python`
+else
+  PYTHON_EXECUTABLE=$1
+fi
+echo "Using Python executable $PYTHON_EXECUTABLE."
+
+unamestr="$(uname)"
+
+##############################################
+# redis
+##############################################
+bash "$TP_SCRIPT_DIR/build_redis.sh"
+
+##############################################
+# boost if necessary
+##############################################
+if [[ "$unamestr" == "Linux" ]]; then
+  echo "building boost"
+  bash "$TP_SCRIPT_DIR/build_boost.sh"
+fi
+
+##############################################
+# flatbuffers if necessary
+##############################################
+if [[ "$unamestr" == "Linux" ]]; then
+  echo "building flatbuffers"
+  bash "$TP_SCRIPT_DIR/build_flatbuffers.sh"
+fi
+
+##############################################
+# arrow
+##############################################
+bash "$TP_SCRIPT_DIR/build_arrow.sh" $PYTHON_EXECUTABLE
+
+##############################################
+# parquet (skipped as it is inlined in build_arrow.sh)
+##############################################
+# bash "$TP_SCRIPT_DIR/build_parquet.sh"
+
+##############################################
+# catapult
+##############################################
+# Clone catapult and build the static HTML needed for the UI.
+bash "$TP_SCRIPT_DIR/build_ui.sh"
+
+##############################################
+# rDSN (optional)
+##############################################
+# bash "$TP_SCRIPT_DIR/build_rdsn.sh"
---
+++ thirdparty/scripts/thirdparty.cmake
@@ -0,0 +1,6 @@
+
+# add all thirdparty related path definitions here
+
+list(APPEND CMAKE_MODULE_PATH
+            ${CMAKE_CURRENT_LIST_DIR}/../build/arrow/python/cmake_modules)
+message (WARNING ${CMAKE_MODULE_PATH})
