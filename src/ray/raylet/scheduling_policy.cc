6eb07e30e655787f6b0180cfe4ec6ffffa7c7237
Alexey Tumanov <atumanov@gmail.com>
2018-03-01 23:37:18 -0800

    updating trivial policy

--- src/ray/raylet/scheduling_policy.cc
+++ src/ray/raylet/scheduling_policy.cc
@@ -16,23 +16,21 @@ SchedulingPolicy::SchedulingPolicy(const SchedulingQueue &sched_queue)
 std::unordered_map<TaskID, ClientID, UniqueIDHasher> SchedulingPolicy::Schedule(
     const std::unordered_map<ClientID, SchedulingResources, UniqueIDHasher>
     &cluster_resources) {
-  // return data structure
+
+  static ClientID local_node_id = ClientID::nil();
   std::unordered_map<TaskID, ClientID, UniqueIDHasher> decision;
+  // TODO(atumanov): consider all cluster resources.
+  SchedulingResources resource_supply = cluster_resources.at(local_node_id);
+  const auto &resource_supply_set = resource_supply.GetAvailableResources();
 
   // Iterate over running tasks, get their resource demand and try to schedule.
-  TaskID last_task_id = TaskID::nil();
-  const auto ready_tasks = sched_queue_.ready_tasks();
-  for (const auto &t : ready_tasks) {
+  for (const auto &t : sched_queue_.ready_tasks()) {
     // Get task's resource demand
-    const auto &resource_demand_map = t.GetTaskSpecification().GetRequiredResources();
-    const auto &resource_demand = ResourceSet(resource_demand_map);
-    // TODO(atumanov): iterate over cluster_resources and locate available capacity.
-    SchedulingResources resource_supply = cluster_resources.at(ClientID::nil());
-    ResourceSet resource_supply_set = resource_supply.GetAvailableResources();
+    const auto &resource_demand = t.GetTaskSpecification().GetRequiredResources();
     bool task_feasible = resource_demand.isSubset(resource_supply_set);
     if (task_feasible) {
-      last_task_id = t.GetTaskSpecification().TaskId();
-      decision[last_task_id] = ClientID::nil();
+      const TaskID &task_id = t.GetTaskSpecification().TaskId();
+      decision[task_id] = local_node_id;
     }
   }
   return decision;
