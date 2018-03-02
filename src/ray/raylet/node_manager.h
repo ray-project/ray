98ee42812dc9dfe6980b334ab0c224e3193e5c9d
Stephanie <swang@cs.berkeley.edu>
2018-03-01 16:29:40 -0800

    Node manager documentation

--- src/ray/raylet/node_manager.h
+++ src/ray/raylet/node_manager.h
@@ -14,14 +14,35 @@ namespace ray {
 
 class NodeManager : public ClientManager<boost::asio::local::stream_protocol> {
  public:
+  /// Create a node manager.
+  ///
+  /// @param socket_name The pathname of the Unix domain socket to listen at
+  ///        for local connections.
+  /// @param resource_config The initial set of node resources.
+  /// @param object_manager A reference to the local object manager.
   NodeManager(
       const std::string &socket_name,
       const ResourceSet &resource_config,
       ObjectManager &object_manager);
-  /// Process a message from a client, then listen for more messages if the
-  /// client is still alive.
-  void ProcessClientMessage(std::shared_ptr<LocalClientConnection> client, int64_t message_type, const uint8_t *message);
+
+  /// Process a message from a client. This method is responsible for
+  /// explicitly listening for more messages from the client if the client is
+  /// still alive.
+  ///
+  /// @param client The client that sent the message.
+  /// @param message_type The message type (e.g., a flatbuffer enum).
+  /// @param message A pointer to the message data.
+  void ProcessClientMessage(
+      std::shared_ptr<LocalClientConnection> client,
+      int64_t message_type,
+      const uint8_t *message);
+
+  /// Handle a task whose local dependencies were missing and are now
+  /// available.
+  ///
+  /// @param task_id The ID of the waiting task.
   void HandleWaitingTaskReady(const TaskID &task_id);
+
  private:
   /// Submit a task to this node.
   void submitTask(const Task& task);
@@ -36,14 +57,15 @@ class NodeManager : public ClientManager<boost::asio::local::stream_protocol> {
   SchedulingResources local_resources_;
   // TODO(alexey): Add resource information from other nodes.
   //std::unordered_map<DBClientID, SchedulingResources&, UniqueIDHasher> cluster_resource_map_;
-  // A set of workers, in a WorkerPool()
+  /// A pool of workers.
   WorkerPool worker_pool_;
-  // A set of queues that maintain tasks enqueued in pending, ready, running
-  // states.
+  /// A set of queues to maintain tasks.
   SchedulingQueue local_queues_;
-  // Scheduling policy in effect for this local scheduler.
+  /// The scheduling policy in effect for this local scheduler.
   SchedulingPolicy sched_policy_;
+  /// The reconstruction policy for deciding when to re-execute a task.
   ReconstructionPolicy reconstruction_policy_;
+  /// A manager to make waiting tasks's missing object dependencies available.
   TaskDependencyManager task_dependency_manager_;
 };
 
