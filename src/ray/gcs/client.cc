483154507a5358dd0dee5cc0689894012673a920
Philipp Moritz <pcmoritz@gmail.com>
2018-03-04 14:51:04 -0800

    Gcs Asio integration (#1633)

--- src/ray/gcs/client.cc
+++ src/ray/gcs/client.cc
@@ -24,6 +24,12 @@ Status Attach(plasma::EventLoop &event_loop) {
   return Status::OK();
 }
 
+Status AsyncGcsClient::Attach(boost::asio::io_service &io_service) {
+  asio_client_.reset(
+      new RedisAsioClient(io_service, context_->async_context()));
+  return Status::OK();
+}
+
 ObjectTable &AsyncGcsClient::object_table() {
   return *object_table_;
 }
