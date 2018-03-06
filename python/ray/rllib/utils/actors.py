4167f3947daaebeb34d45b2702abab12f96e290f
Eric Liang <ekhliang@gmail.com>
2018-03-06 15:43:41 -0800

    Tue Mar  6 15:43:41 PST 2018

--- python/ray/rllib/utils/actors.py
+++ python/ray/rllib/utils/actors.py
@@ -25,7 +25,7 @@ class TaskPool(object):
         return len(self._tasks)
 
 
-def keep_non_colocated(actors):
+def drop_colocated(actors):
     colocated, non_colocated = split_colocated(actors)
     for a in colocated:
         a.__ray_terminate__.remote(a._ray_actor_id.id())
