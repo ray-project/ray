95d2d41c6e788a54b239fcb723708a231a205041
Devin Petersohn <devin.petersohn@gmail.com>
2018-03-05 17:43:48 -0800

    Addressing comments

--- python/ray/dataframe/__init__.py
+++ python/ray/dataframe/__init__.py
@@ -1,14 +1,15 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-import threading
+
 import pandas as pd
+import threading
 
 pd_version = pd.__version__
 pd_major = int(pd_version.split(".")[0])
 pd_minor = int(pd_version.split(".")[1])
 
-if pd_major != 0 or pd_minor < 22:
+if pd_major == 0 and pd_minor < 22:
     raise Exception("In order to use Pandas on Ray, please upgrade your Pandas"
                     " version to >= 0.22.")
 
