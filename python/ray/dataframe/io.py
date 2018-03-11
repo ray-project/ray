db268d708a93683130c13327d5414f91ac5dfe9b
Adithya Girish <adithyagirish@berkeley.edu>
2018-03-11 01:29:18 -0800

    Adding read methods and tests

--- python/ray/dataframe/io.py
+++ python/ray/dataframe/io.py
@@ -6,12 +6,14 @@ from itertools import chain
 from io import BytesIO
 import os
 import re
+import warnings
 
 from pyarrow.parquet import ParquetFile
 import pandas as pd
 
 from .dataframe import ray, DataFrame
 from . import get_npartitions
+from .utils import from_pandas
 
 
 # Parquet
@@ -189,74 +191,273 @@ def read_csv(filepath,
               We only support local files for now.
         kwargs: Keyword arguments in pandas::from_csv
     """
-    kwargs = dict(
-        sep=sep,
-        delimiter=delimiter,
-        header=header,
-        names=names,
-        index_col=index_col,
-        usecols=usecols,
-        squeeze=squeeze,
-        prefix=prefix,
-        mangle_dupe_cols=mangle_dupe_cols,
-        dtype=dtype,
-        engine=engine,
-        converters=converters,
-        true_values=true_values,
-        false_values=false_values,
-        skipinitialspace=skipinitialspace,
-        skiprows=skiprows,
-        nrows=nrows,
-        na_values=na_values,
-        keep_default_na=keep_default_na,
-        na_filter=na_filter,
-        verbose=verbose,
-        skip_blank_lines=skip_blank_lines,
-        parse_dates=parse_dates,
-        infer_datetime_format=infer_datetime_format,
-        keep_date_col=keep_date_col,
-        date_parser=date_parser,
-        dayfirst=dayfirst,
-        iterator=iterator,
-        chunksize=chunksize,
-        compression=compression,
-        thousands=thousands,
-        decimal=decimal,
-        lineterminator=lineterminator,
-        quotechar=quotechar,
-        quoting=quoting,
-        escapechar=escapechar,
-        comment=comment,
-        encoding=encoding,
-        dialect=dialect,
-        tupleize_cols=tupleize_cols,
-        error_bad_lines=error_bad_lines,
-        warn_bad_lines=warn_bad_lines,
-        skipfooter=skipfooter,
-        skip_footer=skip_footer,
-        doublequote=doublequote,
-        delim_whitespace=delim_whitespace,
-        as_recarray=as_recarray,
-        compact_ints=compact_ints,
-        use_unsigned=use_unsigned,
-        low_memory=low_memory,
-        buffer_lines=buffer_lines,
-        memory_map=memory_map,
-        float_precision=float_precision)
 
-    offsets = _compute_offset(filepath, get_npartitions())
+    warnings.warn("Defaulting to Pandas implementation",
+                  PendingDeprecationWarning)
 
-    first_line = _get_firstline(filepath)
-    columns = _infer_column(first_line, kwargs=kwargs)
+    port_frame = pd.read_csv(filepath,
+                             sep=',',
+                             delimiter=None,
+                             header='infer',
+                             names=None,
+                             index_col=None,
+                             usecols=None,
+                             squeeze=False,
+                             prefix=None,
+                             mangle_dupe_cols=True,
+                             dtype=None,
+                             engine=None,
+                             converters=None,
+                             true_values=None,
+                             false_values=None,
+                             skipinitialspace=False,
+                             skiprows=None,
+                             nrows=None,
+                             na_values=None,
+                             keep_default_na=True,
+                             na_filter=True,
+                             verbose=False,
+                             skip_blank_lines=True,
+                             parse_dates=False,
+                             infer_datetime_format=False,
+                             keep_date_col=False,
+                             date_parser=None,
+                             dayfirst=False,
+                             iterator=False,
+                             chunksize=None,
+                             compression='infer',
+                             thousands=None,
+                             decimal=b'.',
+                             lineterminator=None,
+                             quotechar='"',
+                             quoting=0,
+                             escapechar=None,
+                             comment=None,
+                             encoding=None,
+                             dialect=None,
+                             tupleize_cols=None,
+                             error_bad_lines=True,
+                             warn_bad_lines=True,
+                             skipfooter=0,
+                             skip_footer=0,
+                             doublequote=True,
+                             delim_whitespace=False,
+                             as_recarray=None,
+                             compact_ints=None,
+                             use_unsigned=None,
+                             low_memory=True,
+                             buffer_lines=None,
+                             memory_map=False,
+                             float_precision=None)
+    ray_frame = from_pandas(port_frame, get_npartitions())
 
-    df_obj_ids = []
-    for start, end in offsets:
-        if start != 0:
-            df = _read_csv_with_offset.remote(
-                filepath, start, end, header=first_line, kwargs=kwargs)
-        else:
-            df = _read_csv_with_offset.remote(
-                filepath, start, end, kwargs=kwargs)
-        df_obj_ids.append(df)
+    return ray_frame
 
-    return DataFrame(row_partitions=df_obj_ids, columns=columns)
+
+def read_json(path_or_buf=None,
+              orient=None,
+              typ='frame',
+              dtype=True,
+              convert_axes=True,
+              convert_dates=True,
+              keep_default_dates=True,
+              numpy=False,
+              precise_float=False,
+              date_unit=None,
+              encoding=None,
+              lines=False,
+              chunksize=None,
+              compression='infer'):
+
+    warnings.warn("Defaulting to Pandas implementation",
+                  PendingDeprecationWarning)
+
+    port_frame = pd.read_json(path_or_buf, orient, typ, dtype,
+                              convert_axes, convert_dates, keep_default_dates,
+                              numpy, precise_float, date_unit, encoding,
+                              lines, chunksize, compression)
+    ray_frame = from_pandas(port_frame, get_npartitions())
+
+    return ray_frame
+
+
+def read_html(io,
+              match='.+',
+              flavor=None,
+              header=None,
+              index_col=None,
+              skiprows=None,
+              attrs=None,
+              parse_dates=False,
+              tupleize_cols=None,
+              thousands=',',
+              encoding=None,
+              decimal='.',
+              converters=None,
+              na_values=None,
+              keep_default_na=True):
+
+    warnings.warn("Defaulting to Pandas implementation",
+                  PendingDeprecationWarning)
+
+    port_frame = pd.read_html(io, match, flavor, header, index_col,
+                              skiprows, attrs, parse_dates, tupleize_cols,
+                              thousands, encoding, decimal, converters,
+                              na_values, keep_default_na)
+    ray_frame = from_pandas(port_frame[0], get_npartitions())
+
+    return ray_frame
+
+
+def read_clipboard(sep=r'\s+'):
+
+    warnings.warn("Defaulting to Pandas implementation",
+                  PendingDeprecationWarning)
+
+    port_frame = pd.read_clipboard(sep)
+    ray_frame = from_pandas(port_frame, get_npartitions())
+
+    return ray_frame
+
+
+def read_excel(io,
+               sheet_name=0,
+               header=0,
+               skiprows=None,
+               skip_footer=0,
+               index_col=None,
+               names=None,
+               usecols=None,
+               parse_dates=False,
+               date_parser=None,
+               na_values=None,
+               thousands=None,
+               convert_float=True,
+               converters=None,
+               dtype=None,
+               true_values=None,
+               false_values=None,
+               engine=None,
+               squeeze=False):
+
+    warnings.warn("Defaulting to Pandas implementation",
+                  PendingDeprecationWarning)
+
+    port_frame = pd.read_excel(io, sheet_name, header, skiprows, skip_footer,
+                               index_col, names, usecols, parse_dates,
+                               date_parser, na_values, thousands,
+                               convert_float, converters, dtype, true_values,
+                               false_values, engine, squeeze)
+    ray_frame = from_pandas(port_frame, get_npartitions())
+
+    return ray_frame
+
+
+def read_hdf(path_or_buf,
+             key=None,
+             mode='r'):
+
+    warnings.warn("Defaulting to Pandas implementation",
+                  PendingDeprecationWarning)
+
+    port_frame = pd.read_hdf(path_or_buf, key, mode)
+    ray_frame = from_pandas(port_frame, get_npartitions())
+
+    return ray_frame
+
+
+def read_feather(path,
+                 nthreads=1):
+
+    warnings.warn("Defaulting to Pandas implementation",
+                  PendingDeprecationWarning)
+
+    port_frame = pd.read_feather(path)
+    ray_frame = from_pandas(port_frame, get_npartitions())
+
+    return ray_frame
+
+
+def read_msgpack(path_or_buf,
+                 encoding='utf-8',
+                 iterator=False):
+
+    warnings.warn("Defaulting to Pandas implementation",
+                  PendingDeprecationWarning)
+
+    port_frame = pd.read_msgpack(path_or_buf, encoding, iterator)
+    ray_frame = from_pandas(port_frame, get_npartitions())
+
+    return ray_frame
+
+
+def read_stata(filepath_or_buffer,
+               convert_dates=True,
+               convert_categoricals=True,
+               encoding=None,
+               index_col=None,
+               convert_missing=False,
+               preserve_dtypes=True,
+               columns=None,
+               order_categoricals=True,
+               chunksize=None,
+               iterator=False):
+
+    warnings.warn("Defaulting to Pandas implementation",
+                  PendingDeprecationWarning)
+
+    port_frame = pd.read_stata(filepath_or_buffer, convert_dates,
+                               convert_categoricals, encoding, index_col,
+                               convert_missing, preserve_dtypes, columns,
+                               order_categoricals, chunksize, iterator)
+    ray_frame = from_pandas(port_frame, get_npartitions())
+
+    return ray_frame
+
+
+def read_sas(filepath_or_buffer,
+             format=None,
+             index=None,
+             encoding=None,
+             chunksize=None,
+             iterator=False):
+
+    warnings.warn("Defaulting to Pandas implementation",
+                  PendingDeprecationWarning)
+
+    port_frame = pd.read_sas(filepath_or_buffer, format, index, encoding,
+                             chunksize, iterator)
+    ray_frame = from_pandas(port_frame, get_npartitions())
+
+    return ray_frame
+
+
+def read_pickle(path,
+                compression='infer'):
+
+    warnings.warn("Defaulting to Pandas implementation",
+                  PendingDeprecationWarning)
+
+    port_frame = pd.read_pickle(path, compression)
+    ray_frame = from_pandas(port_frame, get_npartitions())
+
+    return ray_frame
+
+
+def read_sql(sql,
+             con,
+             index_col=None,
+             coerce_float=True,
+             params=None,
+             parse_dates=None,
+             columns=None,
+             chunksize=None):
+
+    warnings.warn("Defaulting to Pandas implementation",
+                  PendingDeprecationWarning)
+
+    port_frame = pd.read_sql(sql, con, index_col, coerce_float, params,
+                             parse_dates, columns, chunksize)
+    ray_frame = from_pandas(port_frame, get_npartitions())
+
+    return ray_frame
--- python/ray/dataframe/test/test_io.py
+++ python/ray/dataframe/test/test_io.py
@@ -12,6 +12,15 @@ from ray.dataframe.utils import to_pandas
 
 TEST_PARQUET_FILENAME = 'test.parquet'
 TEST_CSV_FILENAME = 'test.csv'
+TEST_JSON_FILENAME = 'test.json'
+TEST_HTML_FILENAME = 'test.html'
+TEST_EXCEL_FILENAME = 'test.xlsx'
+TEST_FEATHER_FILENAME = 'test.feather'
+TEST_HDF_FILENAME = 'test.hdf'
+TEST_MSGPACK_FILENAME = 'test.msg'
+TEST_STATA_FILENAME = 'test.dta'
+TEST_PICKLE_FILENAME = 'test.pkl'
+TEST_SQL_FILENAME = 'test.sql'
 SMALL_ROW_SIZE = 2000
 LARGE_ROW_SIZE = 7e6
 
@@ -57,6 +66,177 @@ def teardown_csv_file():
         os.remove(TEST_CSV_FILENAME)
 
 
+@pytest.fixture
+def setup_json_file(row_size, force=False):
+    if os.path.exists(TEST_JSON_FILENAME) and not force:
+        pass
+    else:
+        df = pd.DataFrame({
+            'col1': np.arange(row_size),
+            'col2': np.arange(row_size)
+        })
+        df.to_json(TEST_JSON_FILENAME)
+
+
+@pytest.fixture
+def teardown_json_file():
+    if os.path.exists(TEST_JSON_FILENAME):
+        os.remove(TEST_JSON_FILENAME)
+
+
+@pytest.fixture
+def setup_html_file(row_size, force=False):
+    if os.path.exists(TEST_HTML_FILENAME) and not force:
+        pass
+    else:
+        df = pd.DataFrame({
+            'col1': np.arange(row_size),
+            'col2': np.arange(row_size)
+        })
+        df.to_html(TEST_HTML_FILENAME)
+
+
+@pytest.fixture
+def teardown_html_file():
+    if os.path.exists(TEST_HTML_FILENAME):
+        os.remove(TEST_HTML_FILENAME)
+
+
+@pytest.fixture
+def setup_clipboard(row_size, force=False):
+    df = pd.DataFrame({
+        'col1': np.arange(row_size),
+        'col2': np.arange(row_size)
+    })
+    df.to_clipboard()
+
+
+@pytest.fixture
+def setup_excel_file(row_size, force=False):
+    if os.path.exists(TEST_EXCEL_FILENAME) and not force:
+        pass
+    else:
+        df = pd.DataFrame({
+            'col1': np.arange(row_size),
+            'col2': np.arange(row_size)
+        })
+        df.to_excel(TEST_EXCEL_FILENAME)
+
+
+@pytest.fixture
+def teardown_excel_file():
+    if os.path.exists(TEST_EXCEL_FILENAME):
+        os.remove(TEST_EXCEL_FILENAME)
+
+
+@pytest.fixture
+def setup_feather_file(row_size, force=False):
+    if os.path.exists(TEST_FEATHER_FILENAME) and not force:
+        pass
+    else:
+        df = pd.DataFrame({
+            'col1': np.arange(row_size),
+            'col2': np.arange(row_size)
+        })
+        df.to_feather(TEST_FEATHER_FILENAME)
+
+
+@pytest.fixture
+def teardown_feather_file():
+    if os.path.exists(TEST_FEATHER_FILENAME):
+        os.remove(TEST_FEATHER_FILENAME)
+
+
+@pytest.fixture
+def setup_hdf_file(row_size, force=False):
+    if os.path.exists(TEST_HDF_FILENAME) and not force:
+        pass
+    else:
+        df = pd.DataFrame({
+            'col1': np.arange(row_size),
+            'col2': np.arange(row_size)
+        })
+        df.to_hdf(TEST_HDF_FILENAME, 'test')
+
+
+@pytest.fixture
+def teardown_hdf_file():
+    if os.path.exists(TEST_HDF_FILENAME):
+        os.remove(TEST_HDF_FILENAME)
+
+
+@pytest.fixture
+def setup_msgpack_file(row_size, force=False):
+    if os.path.exists(TEST_MSGPACK_FILENAME) and not force:
+        pass
+    else:
+        df = pd.DataFrame({
+            'col1': np.arange(row_size),
+            'col2': np.arange(row_size)
+        })
+        df.to_msgpack(TEST_MSGPACK_FILENAME)
+
+
+@pytest.fixture
+def teardown_msgpack_file():
+    if os.path.exists(TEST_MSGPACK_FILENAME):
+        os.remove(TEST_MSGPACK_FILENAME)
+
+
+@pytest.fixture
+def setup_stata_file(row_size, force=False):
+    if os.path.exists(TEST_STATA_FILENAME) and not force:
+        pass
+    else:
+        df = pd.DataFrame({
+            'col1': np.arange(row_size),
+            'col2': np.arange(row_size)
+        })
+        df.to_stata(TEST_STATA_FILENAME)
+
+
+@pytest.fixture
+def teardown_stata_file():
+    if os.path.exists(TEST_STATA_FILENAME):
+        os.remove(TEST_STATA_FILENAME)
+
+
+@pytest.fixture
+def setup_pickle_file(row_size, force=False):
+    if os.path.exists(TEST_PICKLE_FILENAME) and not force:
+        pass
+    else:
+        df = pd.DataFrame({
+            'col1': np.arange(row_size),
+            'col2': np.arange(row_size)
+        })
+        df.to_pickle(TEST_PICKLE_FILENAME)
+
+
+@pytest.fixture
+def teardown_pickle_file():
+    if os.path.exists(TEST_PICKLE_FILENAME):
+        os.remove(TEST_PICKLE_FILENAME)
+
+
+@pytest.fixture
+def setup_sql_file(row_size, force=False):
+    if os.path.exists(TEST_SQL_FILENAME) and not force:
+        pass
+    else:
+        df = pd.DataFrame({
+            'col1': np.arange(row_size),
+            'col2': np.arange(row_size)
+        })
+        df.to_sql(TEST_SQL_FILENAME)
+
+
+@pytest.fixture
+def teardown_sql_file():
+    if os.path.exists(TEST_SQL_FILENAME):
+        os.remove(TEST_SQL_FILENAME)
+
+
 def test_from_parquet_small():
 
     setup_parquet_file(SMALL_ROW_SIZE)
@@ -90,12 +270,108 @@ def test_from_csv():
     teardown_csv_file()
 
 
-def test_from_csv_delimiter():
-    setup_csv_file(SMALL_ROW_SIZE, delimiter='|')
+def test_from_json():
+    setup_json_file(SMALL_ROW_SIZE)
 
-    pd_df = pd.read_csv(TEST_CSV_FILENAME)
-    ray_df = io.read_csv(TEST_CSV_FILENAME)
+    pd_df = pd.read_json(TEST_JSON_FILENAME)
+    ray_df = io.read_json(TEST_JSON_FILENAME, npartitions=2)
 
     assert ray_df_equals_pandas(ray_df, pd_df)
 
-    teardown_csv_file()
+    teardown_json_file()
+
+
+def test_from_html():
+    setup_html_file(SMALL_ROW_SIZE)
+
+    pd_df = pd.read_html(TEST_HTML_FILENAME)[0]
+    ray_df = io.read_html(TEST_HTML_FILENAME, npartitions=2)
+
+    assert ray_df_equals_pandas(ray_df, pd_df)
+
+    teardown_html_file()
+
+
+def test_from_clipboard():
+    setup_clipboard(SMALL_ROW_SIZE)
+
+    pd_df = pd.read_clipboard()
+    ray_df = io.read_clipboard(npartitions=2)
+
+    assert ray_df_equals_pandas(ray_df, pd_df)
+
+
+def test_from_excel():
+    setup_excel_file(SMALL_ROW_SIZE)
+
+    pd_df = pd.read_excel(TEST_EXCEL_FILENAME)
+    ray_df = io.read_excel(TEST_EXCEL_FILENAME, npartitions=2)
+
+    assert ray_df_equals_pandas(ray_df, pd_df)
+
+    teardown_excel_file()
+
+
+def test_from_feather():
+    setup_feather_file(SMALL_ROW_SIZE)
+
+    pd_df = pd.read_feather(TEST_FEATHER_FILENAME)
+    ray_df = io.read_feather(TEST_FEATHER_FILENAME, npartitions=2)
+
+    assert ray_df_equals_pandas(ray_df, pd_df)
+
+    teardown_feather_file()
+
+
+def test_from_hdf():
+    setup_hdf_file(SMALL_ROW_SIZE)
+
+    pd_df = pd.read_hdf(TEST_HDF_FILENAME, key='test')
+    ray_df = io.read_hdf(TEST_HDF_FILENAME, key='test', npartitions=2)
+
+    assert ray_df_equals_pandas(ray_df, pd_df)
+
+    teardown_hdf_file()
+
+
+def test_from_msgpack():
+    setup_msgpack_file(SMALL_ROW_SIZE)
+
+    pd_df = pd.read_msgpack(TEST_MSGPACK_FILENAME)
+    ray_df = io.read_msgpack(TEST_MSGPACK_FILENAME, npartitions=2)
+
+    assert ray_df_equals_pandas(ray_df, pd_df)
+
+    teardown_msgpack_file()
+
+
+def test_from_stata():
+    setup_stata_file(SMALL_ROW_SIZE)
+
+    pd_df = pd.read_stata(TEST_STATA_FILENAME)
+    ray_df = io.read_stata(TEST_STATA_FILENAME, npartitions=2)
+
+    assert ray_df_equals_pandas(ray_df, pd_df)
+
+    teardown_stata_file()
+
+
+def test_from_pickle():
+    setup_pickle_file(SMALL_ROW_SIZE)
+
+    pd_df = pd.read_pickle(TEST_PICKLE_FILENAME)
+    ray_df = io.read_pickle(TEST_PICKLE_FILENAME, npartitions=2)
+
+    assert ray_df_equals_pandas(ray_df, pd_df)
+
+    teardown_pickle_file()
+
+
+def test_from_sql():
+    # (TODO) Implement
+    assert True
+
+
+def test_from_sas():
+    # (TODO) Implement
+    assert True
