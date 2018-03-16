e97999f514f01ee536ca50dae511928806b780c1
Stephanie <swang@cs.berkeley.edu>
2018-03-16 16:18:59 -0700

    Scripts to start a local cluster of connected raylet processes

--- src/ray/raylet/main.cc
+++ src/ray/raylet/main.cc
@@ -5,25 +5,14 @@
 
 #ifndef RAYLET_TEST
 int main(int argc, char *argv[]) {
-  RAY_CHECK(argc == 2);
-
-  // start store
-  std::string executable_str = std::string(argv[0]);
-  std::string exec_dir = executable_str.substr(0, executable_str.find_last_of("/"));
-  std::string plasma_dir = exec_dir + "./../plasma";
-  std::string plasma_command =
-      plasma_dir +
-      "/plasma_store -m 1000000000 -s /tmp/store 1> /dev/null 2> /dev/null &";
-  RAY_LOG(INFO) << plasma_command;
-  int s = system(plasma_command.c_str());
-  RAY_CHECK(s == 0);
+  RAY_CHECK(argc == 3);
 
   // configure
   std::unordered_map<std::string, double> static_resource_conf;
   static_resource_conf = {{"CPU", 1}, {"GPU", 1}};
   ray::raylet::ResourceSet resource_config(std::move(static_resource_conf));
   ray::ObjectManagerConfig om_config;
-  om_config.store_socket_name = "/tmp/store";
+  om_config.store_socket_name = std::string(argv[2]);
 
   //  initialize mock gcs & object directory
   auto gcs_client = std::make_shared<ray::gcs::AsyncGcsClient>();
---
+++ src/ray/test/stop_raylets.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+killall raylet
+killall plasma_store
+killall redis-server
