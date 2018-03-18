5e2ce16fff516c08f8fc204d39bc24048d903c8b
Richard Liaw <rliaw@berkeley.edu>
2018-03-18 01:56:51 -0700

    run exp

--- python/ray/tune/tune.py
+++ python/ray/tune/tune.py
@@ -9,12 +9,12 @@ from ray.tune.hyperband import HyperBandScheduler
 from ray.tune.async_hyperband import AsyncHyperBandScheduler
 from ray.tune.median_stopping_rule import MedianStoppingRule
 from ray.tune.trial import Trial, DEBUG_PRINT_INTERVAL
-from ray.tune.result import DEFAULT_RESULTS_DIR
 from ray.tune.log_sync import wait_for_log_sync
 from ray.tune.trial_runner import TrialRunner
 from ray.tune.trial_scheduler import FIFOScheduler
 from ray.tune.web_server import TuneServer
 from ray.tune.variant_generator import generate_trials
+from ray.tune.experiment import Experiment
 
 
 _SCHEDULERS = {
@@ -23,55 +23,6 @@ _SCHEDULERS = {
     "HyperBand": HyperBandScheduler,
     "AsyncHyperBand": AsyncHyperBandScheduler,
 }
-
-
-class Experiment():
-    """Tracks experiment specifications.
-
-    Parameters:
-        name (str): Name of experiment.
-        run (str): The algorithm or model to train. This may refer to the
-            name of a built-on algorithm (e.g. RLLib’s DQN or PPO), or a
-            user-defined trainable function or class
-            registered in the tune registry.
-        stop (dict): The stopping criteria. The keys may be any field in
-            TrainingResult, whichever is reached first.
-        config (dict): Algorithm-specific configuration
-            (e.g. env, hyperparams).
-        resources (dict): Machine resources to allocate per trial,
-            e.g. ``{"cpu": 64, "gpu": 8}``. Note that GPUs will not be
-            assigned unless you specify them here. Defaults to 1 CPU and 0
-            GPUs.
-        repeat (int): Number of times to repeat each trial. Defaults to 1.
-        local_dir (str): Local dir to save training results to.
-            Defaults to ``~/ray_results``.
-        upload_dir (str): Optional URI to sync training results
-            to (e.g. ``s3://bucket``).
-        checkpoint_freq (int): How many training iterations between
-            checkpoints. A value of 0 (default) disables checkpointing.
-        max_failures (int): Try to recover a trial from its last
-            checkpoint at least this many times. Only applies if
-            checkpointing is enabled. Defaults to 3.
-    """
-    def __init__(self, name, run, stop, config,
-                 resources=None, repeat=1, local_dir=None,
-                 upload_dir=None, checkpoint_freq=0, max_failures=3):
-        spec = {
-            "run": run,
-            "stop": stop,
-            "config": config,
-            "resources": resources or {"cpu": 1, "gpu": 0},
-            "repeat": repeat,
-            "local_dir": local_dir or DEFAULT_RESULTS_DIR,
-            "upload_dir": upload_dir,
-            "checkpoint_freq": checkpoint_freq,
-            "max_failures": max_failures
-        }
-        self._trials = generate_trials(spec, name)
-
-    def trials(self):
-        for trial in self._trials:
-            yield trial
 
 
 def _make_scheduler(args):
@@ -88,7 +39,7 @@ def run_experiments(experiments, scheduler=None, with_server=False,
     """Tunes experiments.
 
     Args:
-        experiments (Experiment | list | dict): Experiment data.
+        experiments (Experiment | list | dict): Experiments to run.
         scheduler (TrialScheduler): Scheduler for executing
             the experiment. Choose among FIFO (default), MedianStopping,
             AsyncHyperBand, or HyperBand.
@@ -142,6 +93,3 @@ def run_experiments(experiments, scheduler=None, with_server=False,
     wait_for_log_sync()
     return runner.get_trials()
 
-
-if __name__ == '__main__':
-    run_experiments()
