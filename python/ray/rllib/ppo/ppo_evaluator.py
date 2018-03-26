tried fixing kl terms for shared model, there should only be one kl coefficient

python/ray/rllib/ppo/ppo_evaluator.py
@@ -55,10 +55,14 @@ class PPOEvaluator(PolicyEvaluator):
 
         # Defines the training inputs:
         # The coefficient of the KL penalty.
-        num_agents = len(self.config["model"].get("custom_options", {}).
+        num_kl_terms = len(self.config["model"].get("custom_options", {}).
                          get("multiagent_obs_shapes", [1]))
+        shared_model = (self.config["model"].get("custom_options", {}).
+                        get("multiagent_shared_model", False))
+        if shared_model:
+            num_kl_terms = 1
         self.kl_coeff = tf.placeholder(
-            name="newkl", shape=(num_agents,), dtype=tf.float32)
+            name="newkl", shape=(num_kl_terms,), dtype=tf.float32)
 
         # The input observations.
         self.observations = tf.placeholder(