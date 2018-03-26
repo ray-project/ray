bee5ce40c68d05a3736660223046d9c2f14f892d
Alexey Tumanov <atumanov@gmail.com>
2018-03-26 11:23:57 -0700

    test driver: separate tests with dependent chains and independent tasks, sharing the same implementation

--- src/ray/python/test_driver_taskchains.py
+++ src/ray/python/test_driver_taskchains.py
@@ -38,7 +38,7 @@ def submit_tasks_nodep(driver_handle, num_tasks):
         driver_handle.node_manager_client.submit(task)
         logger.debug("[DRIVER]: task return values", task.returns())
 
-def TEST_submit_task_chains(num_chains, tasks_per_chain):
+def submit_task_chains(num_chains, tasks_per_chain):
     #return task placement map on output
     chain_returns = []
     task_placement_map_ = {}
@@ -46,7 +46,7 @@ def TEST_submit_task_chains(num_chains, tasks_per_chain):
         last_task_returns = []
         task_placement_map_[chain_num] = []
         for i in range(tasks_per_chain):
-            task_returns = submit_task_withdep(driver, last_task_returns)
+            task_returns = submit_task_withdep(driver, task_object_dependencies=last_task_returns)
             last_task_returns = task_returns
             task_placement_map_[chain_num].append(task_returns[0])
         chain_returns.append(last_task_returns)
@@ -57,21 +57,12 @@ def TEST_submit_task_chains(num_chains, tasks_per_chain):
 
     return task_placement_map_
 
-if __name__ == '__main__':
-    args = parser.parse_args()
-
-    driver = Worker(args.raylet_socket_name, args.object_store_socket_name,
-                    is_worker=False)
-
-    # Set up the experiment : number of chains and tasks per chain.
-    num_chains = 1
-    tasks_per_chain = 100
-
-    task_placement_map = TEST_submit_task_chains(num_chains=num_chains, tasks_per_chain=tasks_per_chain)
+def TEST_run_task_chains(num_chains, tasks_per_chain):
+    task_placement_map = submit_task_chains(num_chains=num_chains, tasks_per_chain=tasks_per_chain)
     logger.debug("[DRIVER]: task placement information, per chain:")
     task_placement_total = []
     for chain_num in range(len(task_placement_map)):
-        task_placement_list = driver.get(task_placement_map[chain_num], timeout_ms=1000)
+        task_placement_list = driver.get(task_placement_map[chain_num], timeout_ms=5000)
         task_placement_total += [t[1]  for t in task_placement_list]
         logger.debug(chain_num, task_placement_list)
     logger.debug("task placement overall: ", task_placement_total)
@@ -80,3 +71,35 @@ if __name__ == '__main__':
     print("total tasks executed = ", num_total_tasks)
     assert(num_total_tasks == num_chains * tasks_per_chain)
     print("task placement breakdown: total=", task_placement_stats)
+
+
+def TEST_run_tasks_nodep(num_tasks):
+    #This test is the same as having num_tasks chains with 1 task per chain
+    #In this test we assume the num_tasks x 1 chain structure.
+    task_placement_map = submit_task_chains(num_chains=num_tasks, tasks_per_chain=1)
+    logger.debug("[DRIVER]: task placement information, per chain:")
+    task_placement_total = []
+    for chain_num in range(len(task_placement_map)):
+        task_placement_list = driver.get(task_placement_map[chain_num], timeout_ms=5000)
+        task_placement_total += [t[1]  for t in task_placement_list]
+        logger.debug(chain_num, task_placement_list)
+    logger.debug("task placement overall: ", task_placement_total)
+    task_placement_stats = [(v,task_placement_total.count(v)) for v in set(task_placement_total)]
+    num_total_tasks = sum([t[1] for t in task_placement_stats])
+    print("total tasks executed = ", num_total_tasks)
+    assert(num_total_tasks == num_tasks)
+    print("task placement breakdown: total=", task_placement_stats)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    driver = Worker(args.raylet_socket_name, args.object_store_socket_name,
+                    is_worker=False)
+
+    # Set up the experiment : number of chains and tasks per chain.
+    #TEST_run_task_chains(num_chains=10, tasks_per_chain=100)
+
+    TEST_run_tasks_nodep(10000)
+
+
