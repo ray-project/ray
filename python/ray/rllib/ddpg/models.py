995adc3ae7fdf468ad594131f30da48f663c1f9e
alvin <alvkao58@gmail.com>
2018-04-04 15:43:12 -0700

    formatting fixes

--- python/ray/rllib/ddpg/models.py
+++ python/ray/rllib/ddpg/models.py
@@ -3,11 +3,12 @@ from __future__ import division
 from __future__ import print_function
 
 from ray.rllib.ddpg.random_process import OrnsteinUhlenbeckProcess
-import ray.experimental.tfutils
+from ray.experimental.tfutils import TensorFlowVariables
 
 import numpy as np
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
+
 
 class DDPGModel():
     def __init__(self, registry, env, config):
@@ -174,14 +175,16 @@ class DDPGActorCritic():
                                      tf.GraphKeys.TRAINABLE_VARIABLES,
                                      tf.get_variable_scope().name
                                    )
-            self.critic_vars = ray.experimental.TensorFlowVariables(self.critic_loss, self.sess)
+            self.critic_vars = TensorFlowVariables(self.critic_loss,
+                                                   self.sess)
 
         with tf.variable_scope("actor"):
             self.actor_var_list = tf.get_collection(
                                     tf.GraphKeys.TRAINABLE_VARIABLES,
                                     tf.get_variable_scope().name
                                   )
-            self.actor_vars = ray.experimental.TensorFlowVariables(self.output_action, self.sess)
+            self.actor_vars = TensorFlowVariables(self.output_action,
+                                                  self.sess)
 
         if (self.config["parameter_noise"]):
             self.random_process = OrnsteinUhlenbeckProcess(size=1,
@@ -215,14 +218,18 @@ class DDPGActorCritic():
         w_normal = tf.truncated_normal_initializer()
         w_init = tf.random_uniform_initializer(minval=-0.0003, maxval=0.0003)
         net = slim.fully_connected(obs, 400, activation_fn=tf.nn.relu,
-                                  weights_initializer=w_normal)
-        t1 = slim.fully_connected(net, 300, activation_fn=None, biases_initializer=None,
+                                   weights_initializer=w_normal)
+        t1 = slim.fully_connected(net, 300,
+                                  activation_fn=None, biases_initializer=None,
                                   weights_initializer=w_normal)
         t2 = slim.fully_connected(action, 300, activation_fn=None,
                                   weights_initializer=w_normal)
         net = tf.nn.relu(tf.add(t1, t2))
 
-        out = slim.fully_connected(net, 1, activation_fn=None, weights_initializer=w_init)
+        out = slim.fully_connected(net,
+                                   1,
+                                   activation_fn=None,
+                                   weights_initializer=w_init)
         return out
 
     def _setup_actor_network(self, obs_space, ac_space):
@@ -233,8 +240,14 @@ class DDPGActorCritic():
         w_normal = tf.truncated_normal_initializer()
         w_init = tf.random_uniform_initializer(minval=-0.003, maxval=0.003)
 
-        net = slim.fully_connected(obs, 400, activation_fn=tf.nn.relu, weights_initializer=w_normal)
-        net = slim.fully_connected(net, 300, activation_fn=tf.nn.relu, weights_initializer=w_normal)
+        net = slim.fully_connected(obs,
+                                   400,
+                                   activation_fn=tf.nn.relu,
+                                   weights_initializer=w_normal)
+        net = slim.fully_connected(net,
+                                   300,
+                                   activation_fn=tf.nn.relu,
+                                   weights_initializer=w_normal)
         out = slim.fully_connected(net,
                                    self.ac_size,
                                    activation_fn=tf.nn.tanh,
