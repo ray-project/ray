81c0923daf13b624d1eac5b0241401665b21bd7e
Stephanie <swang@cs.berkeley.edu>
2018-04-18 14:57:22 -0700

    Initialize doubles

--- src/ray/raylet/node_manager.cc
+++ src/ray/raylet/node_manager.cc
@@ -358,7 +358,7 @@ void NodeManager::ProcessClientMessage(std::shared_ptr<LocalClientConnection> cl
         // Get the CPU resources required by the running task.
         const auto required_resources =
             task.GetTaskSpecification().GetRequiredResources();
-        double required_cpus;
+        double required_cpus = 0;
         RAY_CHECK(required_resources.GetResource(kCPU_ResourceLabel, &required_cpus));
         const std::unordered_map<std::string, double> cpu_resources = {
             {kCPU_ResourceLabel, required_cpus}};
@@ -388,7 +388,7 @@ void NodeManager::ProcessClientMessage(std::shared_ptr<LocalClientConnection> cl
       const auto &task = tasks.front();
       // Get the CPU resources required by the running task.
       const auto required_resources = task.GetTaskSpecification().GetRequiredResources();
-      double required_cpus;
+      double required_cpus = 0;
       RAY_CHECK(required_resources.GetResource(kCPU_ResourceLabel, required_cpus));
       const std::unordered_map<std::string, double> cpu_resources = {
           {kCPU_ResourceLabel, required_cpus}};
