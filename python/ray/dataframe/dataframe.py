8172029842b8c809fb750481d7787766b3effc8b
Kunal Gosar <gosarkunal9@gmail.com>
2018-04-22 01:08:00 -0700

    edge case fixes for __getitem__

--- python/ray/dataframe/dataframe.py
+++ python/ray/dataframe/dataframe.py
@@ -3212,6 +3212,9 @@ class DataFrame(object):
         """
         key = com._apply_if_callable(key, self)
 
+        if key is None:
+            pd.DataFrame()[None]
+
         # shortcut if we are an actual column
         is_mi_columns = isinstance(self.columns, pd.MultiIndex)
         try:
@@ -3266,7 +3269,7 @@ class DataFrame(object):
                              columns=columns,
                              index=index)
         else:
-            columns = self.columns[key]
+            columns = self._col_metadata[key].index
 
             indices_for_rows = [self.columns.index(new_col)
                                 for new_col in columns]
