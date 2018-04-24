4a9fa6b02422dd65ce542d8448df54536d88956f
Jae Min Kim <jaeminkim1324@berkeley.edu>
2018-04-24 13:44:47 -0700

    lint spacing

--- python/ray/dataframe/test/test_dataframe.py
+++ python/ray/dataframe/test/test_dataframe.py
@@ -2171,7 +2171,7 @@ def test_reindex():
                          'd': np.arange(4.0, 7.0, dtype='float64'),
                          'e': [True, False, True],
                          'f': pd.date_range('now', periods=3).values})
-    assert(trd.reindex(columns=['a','b'])['b'].equals(trd['b']))
+    assert(trd.reindex(columns=['a', 'b'])['b'].equals(trd['b']))
 
 
 def test_reindex_axis():
