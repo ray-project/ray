081d9dd54b5e82fbf19d72580c303d1c992911e4
Jae Min Kim <jaeminkim1324@berkeley.edu>
2018-04-24 21:06:01 -0700

    wrong testing func

--- python/ray/dataframe/test/test_dataframe.py
+++ python/ray/dataframe/test/test_dataframe.py
@@ -2168,7 +2168,7 @@ def test_reindex():
     ray_df = create_test_dataframe()
 
     with pytest.raises(NotImplementedError):
-        ray_df.rdiv(None)
+        ray_df.reindex(None)
 
 
 def test_reindex_axis():
