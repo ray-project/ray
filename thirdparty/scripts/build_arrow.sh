d4f11fae89d00f47372b84dba6ac58c24a1c6907
Philipp Moritz <pcmoritz@gmail.com>
2018-04-25 00:04:37 -0700

    fix arrow pointer

--- thirdparty/scripts/build_arrow.sh
+++ thirdparty/scripts/build_arrow.sh
@@ -47,7 +47,7 @@ if [[ ! -d $TP_DIR/../python/ray/pyarrow_files/pyarrow ]]; then
     # The PR for this commit is https://github.com/apache/arrow/pull/1939. We
     # include the link here to make it easier to find the right commit because
     # Arrow often rewrites git history and invalidates certain commits.
-    git checkout 84c19f901bc357a92b70c84a90dd232637725e3c
+    git checkout 9d724877b6f5bd7d12082dca96fca3b41444ecfd
 
     cd cpp
     if [ ! -d "build" ]; then
