2aa343b8e309ae2b87a8366899b0616503530957
Alok Singh <alokbeniwal@gmail.com>
2018-04-24 22:43:51 -0700

    Use faster loss calculation for SharedTorchPolicy
    `F.mse_loss` is faster and cleaner than the whole `(_ - _).pow(2).sum()`
    rigmarole.

--- python/ray/rllib/a3c/shared_torch_policy.py
+++ python/ray/rllib/a3c/shared_torch_policy.py
@@ -66,8 +66,8 @@ class SharedTorchPolicy(TorchPolicy):
 
         states, acs, advs, rs, _ = convert_batch(batch)
         values, ac_logprobs, entropy = self._evaluate(states, acs)
-        pi_err = -(advs * ac_logprobs).sum()
-        value_err = 0.5 * (values - rs).pow(2).sum()
+        policy_err = -advs.dot(ac_logprobs)
+        value_err = F.mse_loss(values, rs)
 
         self.optimizer.zero_grad()
         overall_err = (pi_err +
