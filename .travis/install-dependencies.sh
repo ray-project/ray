55ecc2ff63553f46ef0f5f9688021dcfdde6d5e8
Alok Singh <alokbeniwal@gmail.com>
2018-05-01 11:20:20 -0700

    Add flake8-comprehensions
    [flake8 plugin](https://github.com/adamchainz/flake8-comprehensions) that
    checks for useless constructions.

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -77,7 +77,7 @@ elif [[ "$LINT" == "1" ]]; then
   bash miniconda.sh -b -p $HOME/miniconda
   export PATH="$HOME/miniconda/bin:$PATH"
   # Install Python linting tools.
-  pip install -q flake8
+  pip install -q flake8 flake8-comprehensions
 elif [[ "$LINUX_WHEELS" == "1" ]]; then
   sudo apt-get install docker
   sudo usermod -a -G docker travis
