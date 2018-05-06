bfe7b924dd3486a1539c3ef7f56a6be9ca87ceb4
qyccc <910845102@qq.com>
2018-05-06 17:37:50 +0800

    f

--- python/ddpg_baselines/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from ray.rllib.ddpg_baselines.ddpg import DDPGAgent, DEFAULT_CONFIG
-
-__all__ = ["DDPGAgent", "DEFAULT_CONFIG"]
