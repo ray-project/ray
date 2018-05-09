b9ff97b0ea14b034493d60457eff420176037f14
Alok Singh <alokbeniwal@gmail.com>
2018-05-09 22:28:30 +0000

    Improve script
    1.  rm useless cd's
    2.  Unquote array since it turns out checking the truthiness of an array
        is a real pain in bash and is best done in Python or something.
    3.  rm useless `exit 0` since scripts exit with the status of the last
        command anyway.

--- .travis/yapf.sh
+++ .travis/yapf.sh
@@ -3,13 +3,11 @@
 # Cause the script to exit if a single command fails
 set -eo pipefail
 
-# should be $PROJECT_ROOT
-builtin cd "$(dirname "${BASH_SOURCE:-$0}")"
-PROJECT_ROOT="$(git rev-parse --show-toplevel)"
-builtin cd "$PROJECT_ROOT" && echo "Project Root: $PROJECT_ROOT"
+ROOT="$(git rev-parse --show-toplevel)"
+builtin cd "$ROOT"
 
 yapf \
-    --style "$PROJECT_ROOT/.style.yapf" \
+    --style "$ROOT/.style.yapf" \
     --in-place --recursive --parallel \
     --exclude 'python/ray/dataframe/' \
     --exclude 'python/ray/rllib/' \
@@ -17,11 +15,10 @@ yapf \
     -- \
     'test/' 'python/'
 
-CHANGED_FILES=("$(git diff --name-only)")
+CHANGED_FILES=($(git diff --name-only))
 
 if [[ "${#CHANGED_FILES[@]}" -gt 0 ]]; then
     echo 'Reformatted staged files. Please review and stage the changes.'
-    echo
     echo 'Files updated:'
 
     for file in "${CHANGED_FILES[@]}"; do
@@ -29,7 +26,5 @@ if [[ "${#CHANGED_FILES[@]}" -gt 0 ]]; then
     done
 
     exit 1
-else
-    exit 0
 fi
 
