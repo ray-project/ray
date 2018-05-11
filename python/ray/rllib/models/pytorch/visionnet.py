64ae2ab5f4cf7ae958059dd607e38a1227b3b56a
Alok Singh <alokbeniwal@gmail.com>
2018-05-11 11:13:03 +0000

    Fmt

--- python/ray/rllib/models/pytorch/visionnet.py
+++ python/ray/rllib/models/pytorch/visionnet.py
@@ -21,7 +21,7 @@ class VisionNetwork(Model):
         filters = options.get("conv_filters", [
             [16, [8, 8], 4],
             [32, [4, 4], 2],
-            [512, [10, 10], 1]
+            [512, [10, 10], 1],
         ])
         layers = []
         in_channels, in_size = inputs[0], inputs[1:]
@@ -36,7 +36,7 @@ class VisionNetwork(Model):
 
         out_channels, kernel, stride = filters[-1]
         layers.append(SlimConv2d(
-                in_channels, out_channels, kernel, stride, None))
+            in_channels, out_channels, kernel, stride, None))
         self._convs = nn.Sequential(*layers)
 
         self.logits = SlimFC(
