d71d4e4b74b45c8d7874002e1da63c5dc5a131c7
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-05-15 18:51:34 -0700

    Revert "Temporarily rename autoscaler tags due to gcp incompatibility"
    This reverts commit c938ee435f4b75854a14e78242ad7f1d1ed8ad4b.

--- python/ray/autoscaler/tags.py
+++ python/ray/autoscaler/tags.py
@@ -5,19 +5,19 @@ from __future__ import division
 from __future__ import print_function
 
 # Tag for the name of the node
-TAG_NAME = "ray_name"
+TAG_NAME = "Name"
 
 # Tag uniquely identifying all nodes of a cluster
-TAG_RAY_CLUSTER_NAME = "ray_cluster-name"
+TAG_RAY_CLUSTER_NAME = "ray:ClusterName"
 
 # Tag for the type of node (e.g. Head, Worker)
-TAG_RAY_NODE_TYPE = "ray_node-type"
+TAG_RAY_NODE_TYPE = "ray:NodeType"
 
 # Tag that reports the current state of the node (e.g. Updating, Up-to-date)
-TAG_RAY_NODE_STATUS = "ray_node-status"
+TAG_RAY_NODE_STATUS = "ray:NodeStatus"
 
 # Hash of the node launch config, used to identify out-of-date nodes
-TAG_RAY_LAUNCH_CONFIG = "ray_launch-config"
+TAG_RAY_LAUNCH_CONFIG = "ray:LaunchConfig"
 
 # Hash of the node runtime config, used to determine if updates are needed
-TAG_RAY_RUNTIME_CONFIG = "ray_runtime-config"
+TAG_RAY_RUNTIME_CONFIG = "ray:RuntimeConfig"
