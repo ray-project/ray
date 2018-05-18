88e09194d0137f5be54b9c939fdda606853085a0
Kunal Gosar <gosarkunal9@gmail.com>
2018-05-18 11:58:24 -0700

    begin writing tests

--- python/ray/dataframe/test/test_groupby.py
+++ python/ray/dataframe/test/test_groupby.py
@@ -3,15 +3,10 @@ from __future__ import division
 from __future__ import print_function
 
 import pytest
-import numpy as np
 import pandas as pd
-import pandas.util.testing as tm
-import ray.dataframe as rdf
 from ray.dataframe.utils import (
     from_pandas,
     to_pandas)
-
-from pandas.tests.frame.common import TestData
 
 
 @pytest.fixture
@@ -29,3 +24,35 @@ def ray_df_equals(ray_df1, ray_df2):
     return to_pandas(ray_df1).sort_index().equals(
         to_pandas(ray_df2).sort_index()
     )
+
+
+@pytest.fixture
+def ray_groupby_equals_pandas(ray_groupby, pandas_groupby):
+    for g1, g2 in zip(ray_groupby, pandas_groupby):
+        assert g1[0] == g2[0]
+        assert ray_df_equals_pandas(g1[1], g2[1])
+
+
+def test_simple_row_groupby():
+    pandas_df = pd.DataFrame({'col1': [0, 1, 2, 3],
+                              'col2': [4, 5, 6, 7],
+                              'col3': [8, 9, 10, 11],
+                              'col4': [12, 13, 14, 15],
+                              'col5': [0, 0, 0, 0]})
+
+    ray_df = from_pandas(pandas_df, 2)
+
+    by = [1, 2, 1, 2]
+
+    ray_groupby = ray_df.groupby(by=by)
+    pandas_groupby = pandas_df.groupby(by=by)
+
+    assert ray_groupby_equals_pandas(ray_groupby, pandas_groupby)
+
+
+
+
+
+    by_col_axis = [[1, 2, 3, 2, 1],
+                   [0, 0, 0, 0, 0],
+                   [1, 2, 3, 4, 5]]
