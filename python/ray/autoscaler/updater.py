44a34582237e6c0ae5f83840ecc81f13a9dbf8d3
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-05-20 16:47:13 -0700

    Expand local file mount paths in NodeUpdater

--- python/ray/autoscaler/updater.py
+++ python/ray/autoscaler/updater.py
@@ -45,7 +45,10 @@ class NodeUpdater(object):
         self.ssh_user = auth_config["ssh_user"]
         self.ssh_ip = self.provider.external_ip(node_id)
         self.node_id = node_id
-        self.file_mounts = file_mounts
+        self.file_mounts = {
+            remote: os.path.expanduser(local)
+            for remote, local in file_mounts.items()
+        }
         self.setup_cmds = setup_cmds
         self.runtime_hash = runtime_hash
         if redirect_output:
