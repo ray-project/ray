05a547b97fe899487648b114debd46a1c5be3dde
Richard Liaw <rliaw@berkeley.edu>
2018-05-21 15:28:59 -0700

    init benchmarks

---
+++ python/benchmarks/benchmarks.py
@@ -0,0 +1,36 @@
+# Write the benchmarking functions here.
+# See "Writing benchmarks" in the asv docs for more information.
+
+
+class TimeSuite:
+    """
+    An example benchmark that times the performance of various kinds
+    of iterating over dictionaries in Python.
+    """
+    def setup(self):
+        self.d = {}
+        for x in range(500):
+            self.d[x] = None
+
+    def time_keys(self):
+        for key in self.d.keys():
+            pass
+
+    def time_iterkeys(self):
+        for key in self.d.iterkeys():
+            pass
+
+    def time_range(self):
+        d = self.d
+        for key in range(500):
+            x = d[key]
+
+    def time_xrange(self):
+        d = self.d
+        for key in xrange(500):
+            x = d[key]
+
+
+class MemSuite:
+    def mem_list(self):
+        return [0] * 256
