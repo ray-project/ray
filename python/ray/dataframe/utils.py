bb498aabdd2b53889d12ce26d458551adf4bfb68
Kunal Gosar <gosarkunal9@gmail.com>
2018-05-22 16:34:07 -0700

    [DataFrame] Refactor GroupBy Methods and Implement Reindex (#2101)
    * fix 1D blocks case
    
    * Add groupby test code
    
    * begin writing tests
    
    * Fix bug on groupby(axis=1, ...)
    
    * implement reindex
    
    * fix index misalignment after groupby
    
    * fix df.apply bug
    
    * fix groupby.apply
    
    * fix agg funcs
    
    * finish groupby tests
    
    * finish test suite for groupby
    
    * fixing lint
    
    * undo new line
    
    * fix python2 index copy bug
    
    * Concat Series into ray.df
    
    * fixing python2 issues
    
    * resolving all python 2 tests
    
    * handle multiindex on apply
    
    * resolve comments
    
    * updating docstring
    
    * fix lint
    
    * fix lint again
    
    * address comments

--- python/ray/dataframe/utils.py
+++ python/ray/dataframe/utils.py
@@ -271,7 +271,12 @@ def _create_block_partitions(partitions, axis=0, length=None):
 
     # In the case that axis is 1 we have to transpose because we build the
     # columns into rows. Fortunately numpy is efficient at this.
-    return np.array(x) if axis == 0 else np.array(x).T
+    blocks = np.array(x) if axis == 0 else np.array(x).T
+
+    # Sometimes we only get a single column or row, which is
+    # problematic for building blocks from the partitions, so we
+    # add whatever dimension we're missing from the input.
+    return fix_blocks_dimensions(blocks, axis)
 
 
 @ray.remote
@@ -299,6 +304,7 @@ def create_blocks_helper(df, npartitions, axis):
 
     for block in blocks:
         block.columns = pd.RangeIndex(0, len(block.columns))
+        block.reset_index(inplace=True, drop=True)
     return blocks
 
 
@@ -372,12 +378,10 @@ def _reindex_helper(old_index, new_index, axis, npartitions, *df):
     df = pd.concat(df, axis=axis ^ 1)
     if axis == 1:
         df.index = old_index
-        df = df.reindex(new_index, copy=False)
-        df.reset_index(inplace=True, drop=True)
     elif axis == 0:
         df.columns = old_index
-        df = df.reindex(columns=new_index, copy=False)
-        df.columns = pd.RangeIndex(len(df.columns))
+
+    df = df.reindex(new_index, copy=False, axis=axis ^ 1)
     return create_blocks_helper(df, npartitions, axis)
 
 
@@ -461,3 +465,11 @@ def _correct_column_dtypes(*column):
     """
     concat_column = pd.concat(column, copy=False)
     return create_blocks_helper(concat_column, len(column), 1)
+
+
+def fix_blocks_dimensions(blocks, axis):
+    """Checks that blocks is 2D, and adds a dimension if not.
+    """
+    if blocks.ndim < 2:
+        return np.expand_dims(blocks, axis=axis ^ 1)
+    return blocks
