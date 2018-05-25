fa97acbc899fe1e97d907b865d2122ac8bd641a4
Zongheng Yang <concretevitamin@users.noreply.github.com>
2018-05-24 23:35:25 -0700

    Integrate credis with Ray & route task table entries into credis. (#1841)

--- src/ray/gcs/tables.cc
+++ src/ray/gcs/tables.cc
@@ -11,7 +11,6 @@ template <typename ID, typename Data>
 Status Log<ID, Data>::Append(const JobID &job_id, const ID &id,
                              std::shared_ptr<DataT> &dataT, const WriteCallback &done) {
   auto callback = [this, id, dataT, done](const std::string &data) {
-    RAY_CHECK(data.empty());
     if (done != nullptr) {
       (done)(client_, id, *dataT);
     }
@@ -141,8 +140,15 @@ Status Table<ID, Data>::Add(const JobID &job_id, const ID &id,
   flatbuffers::FlatBufferBuilder fbb;
   fbb.ForceDefaults(true);
   fbb.Finish(Data::Pack(fbb, dataT.get()));
-  return context_->RunAsync("RAY.TABLE_ADD", id, fbb.GetBufferPointer(), fbb.GetSize(),
-                            prefix_, pubsub_channel_, std::move(callback));
+  if (command_type_ == CommandType::kRegular) {
+    return context_->RunAsync("RAY.TABLE_ADD", id, fbb.GetBufferPointer(), fbb.GetSize(),
+                              prefix_, pubsub_channel_, std::move(callback));
+  } else {
+    RAY_CHECK(command_type_ == CommandType::kChain);
+    return context_->RunAsync("RAY.CHAIN.TABLE_ADD", id, fbb.GetBufferPointer(),
+                              fbb.GetSize(), prefix_, pubsub_channel_,
+                              std::move(callback));
+  }
 }
 
 template <typename ID, typename Data>
