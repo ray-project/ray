b81ab5ae32f4e880a37ecd3ced411c591ce59d48
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-05-26 13:52:34 -0700

    Replace '_' with '-' in autoscaler tag names

--- python/ray/autoscaler/tags.py
+++ python/ray/autoscaler/tags.py
@@ -5,19 +5,19 @@ from __future__ import division
 from __future__ import print_function
 
 # Tag for the name of the node
-TAG_RAY_NODE_NAME = "ray-node_name"
+TAG_RAY_NODE_NAME = "ray-node-name"
 
 # Tag for the type of node (e.g. Head, Worker)
-TAG_RAY_NODE_TYPE = "ray-node_type"
+TAG_RAY_NODE_TYPE = "ray-node-type"
 
 # Tag that reports the current state of the node (e.g. Updating, Up-to-date)
-TAG_RAY_NODE_STATUS = "ray-node_status"
+TAG_RAY_NODE_STATUS = "ray-node-status"
 
 # Tag uniquely identifying all nodes of a cluster
-TAG_RAY_CLUSTER_NAME = "ray-cluster_name"
+TAG_RAY_CLUSTER_NAME = "ray-cluster-name"
 
 # Hash of the node launch config, used to identify out-of-date nodes
-TAG_RAY_LAUNCH_CONFIG = "ray-launch_config"
+TAG_RAY_LAUNCH_CONFIG = "ray-launch-config"
 
 # Hash of the node runtime config, used to determine if updates are needed
-TAG_RAY_RUNTIME_CONFIG = "ray-runtime_config"
+TAG_RAY_RUNTIME_CONFIG = "ray-runtime-config"
