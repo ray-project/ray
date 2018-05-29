75ea9a717bc45655faac7ca1ebf54ed051084924
Alok Singh <alokbeniwal@gmail.com>
2018-05-28 23:34:06 -0700

    Squeeze action

--- python/ray/rllib/a3c/shared_torch_policy.py
+++ python/ray/rllib/a3c/shared_torch_policy.py
@@ -32,7 +32,7 @@ class SharedTorchPolicy(TorchPolicy):
         with self.lock:
             ob = torch.from_numpy(ob).float().unsqueeze(0)
             logits, values = self._model(ob)
-            samples = F.softmax(logits, dim=1).multinomial(1)
+            samples = F.softmax(logits, dim=1).multinomial(1).squeeze(0)
             values = values.squeeze()
             return var_to_np(samples), {"vf_preds": var_to_np(values)}
 
