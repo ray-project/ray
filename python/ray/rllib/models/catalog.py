9ddab7772f69b4007c9a3d1df475bd52455a2b4d
Richard Liaw <rliaw@berkeley.edu>
2018-05-29 21:49:19 -0700

    small lint

--- python/ray/rllib/models/catalog.py
+++ python/ray/rllib/models/catalog.py
@@ -186,7 +186,8 @@ class ModelCatalog(object):
         if obs_rank > 1:
             return PyTorchVisionNet(input_shape, num_outputs, options)
 
-        # TODO(alok): overhaul PyTorchFCNet so it can just take input shape directly
+        # TODO(alok): overhaul PyTorchFCNet so it can just
+        # take input shape directly
         return PyTorchFCNet(input_shape[0], num_outputs, options)
 
     @staticmethod
