fe83410e90ecb02882242cde185d81fda0f7bc1a
Alok Singh <alokbeniwal@gmail.com>
2018-05-29 19:21:50 -0700

    Allow yapf to lint individual files

--- .travis/yapf.sh
+++ .travis/yapf.sh
@@ -9,14 +9,33 @@ builtin cd "$(dirname "${BASH_SOURCE:-$0}")"
 ROOT="$(git rev-parse --show-toplevel)"
 builtin cd "$ROOT"
 
-yapf \
-    --style "$ROOT/.style.yapf" \
-    --in-place --recursive --parallel \
-    --exclude 'python/ray/cloudpickle' \
-    --exclude 'python/ray/dataframe' \
-    --exclude 'python/ray/rllib' \
-    -- \
-    'test' 'python'
+lint() {
+    yapf \
+        --style "$ROOT/.style.yapf" \
+        --in-place --recursive --parallel \
+        -- \
+        "$@"
+}
+
+lint_all() {
+
+    yapf \
+        --style "$ROOT/.style.yapf" \
+        --in-place --recursive --parallel \
+        --exclude 'python/ray/cloudpickle' \
+        --exclude 'python/ray/dataframe' \
+        --exclude 'python/ray/rllib' \
+        -- \
+        test python
+}
+
+# This flag lints individual files. --files *must* be the first command line
+# arg to use this option.
+if [[ "$1" == '--files' ]]; then
+    lint "${@:2}"
+else
+    lint_all 'test' 'python'
+fi
 
 if ! git diff --quiet; then
     echo 'Reformatted staged files. Please review and stage the changes.'
