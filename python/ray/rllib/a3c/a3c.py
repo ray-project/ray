da414fc154d700b813cb44ac441744fe479f3f66
Alok Singh <alokbeniwal@gmail.com>
2018-05-29 17:51:32 -0700

    fmt

--- python/ray/rllib/a3c/a3c.py
+++ python/ray/rllib/a3c/a3c.py
@@ -51,7 +51,7 @@ DEFAULT_CONFIG = {
         # (Image statespace) - Converts image to (dim, dim, C)
         "dim": 80,
         # (Image statespace) - Converts image shape to (C, dim, dim)
-        "channel_major" : False,
+        "channel_major": False,
     },
     # Arguments to pass to the rllib optimizer
     "optimizer": {
