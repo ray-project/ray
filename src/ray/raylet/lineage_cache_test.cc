51b24b1486716e30c6d60bf12309e4f47e38381f
Hao Chen <chenh1024@gmail.com>
2018-06-05 21:30:16 +0800

    Optimize lineage eviction

--- src/ray/raylet/lineage_cache_test.cc
+++ src/ray/raylet/lineage_cache_test.cc
@@ -52,6 +52,7 @@ class MockGcs : public gcs::TableInterface<TaskID, protocol::Task>,
     if (task_table_.count(task_id) == 1) {
       callbacks_.push_back({notification_callback_, task_id});
     }
+    num_requested_notifications_ += 1;
     return ray::Status::OK();
   }
 
@@ -75,11 +76,14 @@ class MockGcs : public gcs::TableInterface<TaskID, protocol::Task>,
 
   const std::unordered_set<TaskID> &SubscribedTasks() const { return subscribed_tasks_; }
 
+  const int NumRequestedNotifications() const { return num_requested_notifications_; }
+
  private:
   std::unordered_map<TaskID, std::shared_ptr<protocol::TaskT>> task_table_;
   std::vector<std::pair<gcs::raylet::TaskTable::WriteCallback, TaskID>> callbacks_;
   gcs::raylet::TaskTable::WriteCallback notification_callback_;
   std::unordered_set<TaskID> subscribed_tasks_;
+  int num_requested_notifications_;
 };
 
 class LineageCacheTest : public ::testing::Test {
@@ -397,7 +401,7 @@ TEST_F(LineageCacheTest, TestEviction) {
 TEST_F(LineageCacheTest, TestOutOfOrderEviction) {
   // Insert a chain of dependent tasks that is more than twice as long as the
   // maximum lineage size. This will ensure that we request notifications for
-  // at least 2 remote tasks.
+  // at most 2 remote tasks.
   uint64_t lineage_size = (2 * max_lineage_size_) + 1;
   size_t num_tasks_flushed = 0;
   std::vector<Task> tasks;
@@ -408,6 +412,8 @@ TEST_F(LineageCacheTest, TestOutOfOrderEviction) {
     auto task_id = task.GetTaskSpecification().TaskId();
     lineage_cache_.RemoveWaitingTask(task_id);
   }
+  // Check that we requested at most 2 notifications
+  ASSERT_TRUE(mock_gcs_.NumRequestedNotifications() <= 2);
 
   // Check that the last task in the chain still has all tasks in its
   // uncommitted lineage.
