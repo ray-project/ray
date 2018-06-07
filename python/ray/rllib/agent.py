b99b1a37fd58e984f4ee43d946fc6ddb7d74ded7
Ubuntu <ubuntu@ip-172-31-70-164.ec2.internal>
2018-06-07 08:50:34 +0000

    removed ddpg2 from codebase

--- python/ray/rllib/agent.py
+++ python/ray/rllib/agent.py
@@ -231,10 +231,7 @@ class _ParameterTuningAgent(_MockAgent):
 def get_agent_class(alg):
     """Returns the class of a known agent given its name."""
 
-    if alg == "DDPG2":
-        from ray.rllib import ddpg2
-        return ddpg2.DDPG2Agent
-    elif alg == "DDPG":
+    if alg == "DDPG":
         from ray.rllib import ddpg
         return ddpg.DDPGAgent
     elif alg == "APEX_DDPG":
