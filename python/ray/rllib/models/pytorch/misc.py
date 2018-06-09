f8f61281c8898f9d76268757b050b1ef4c9b14ab
Alok Singh <alokbeniwal@gmail.com>
2018-06-09 02:19:27 -0700

    misc

--- python/ray/rllib/models/pytorch/misc.py
+++ python/ray/rllib/models/pytorch/misc.py
@@ -24,8 +24,7 @@ def var_to_np(var):
 def normc_initializer(std=1.0):
     def initializer(tensor):
         tensor.data.normal_(0, 1)
-        tensor.data *= std / torch.sqrt(
-            tensor.data.pow(2).sum(1, keepdim=True))
+        tensor.data *= std / (tensor.data**2).sum(1, keepdim=True).sqrt()
 
     return initializer
 
