a3ae242f940bfde4aec3185b33480bb99fddae5b
suquark <zsy9509@mail.ustc.edu.cn>
2018-06-13 11:10:23 +0800

    [async] change code format, remove old files

--- python/ray/plasma/plasma_eventloop.py
+++ python/ray/plasma/plasma_eventloop.py
@@ -19,17 +19,17 @@ def _wait(fs, timeout, num_returns, loop):
 
     The fs argument must be a collection of Futures.
     """
-    
+
     assert fs, "Set of Futures is empty."
     assert 0 < num_returns <= len(fs)
-    
+
     waiter = loop.create_future()
     timeout_handle = None
     if timeout is not None:
         timeout_handle = loop.call_later(timeout, _release_waiter, waiter)
-    
+
     n_finished = 0
-    
+
     def _on_completion(f):
         nonlocal n_finished
         n_finished += 1
@@ -38,16 +38,16 @@ def _wait(fs, timeout, num_returns, loop):
                 timeout_handle.cancel()
             if not waiter.done():
                 waiter.set_result(None)
-    
+
     for f in fs:
         f.add_done_callback(_on_completion)
-    
+
     try:
         yield from waiter
     finally:
         if timeout_handle is not None:
             timeout_handle.cancel()
-    
+
     done, pending = [], []
     for f in fs:
         f.remove_done_callback(_on_completion)
@@ -63,51 +63,53 @@ class PlasmaObjectFuture(asyncio.Future):
         super().__init__(loop=loop)
         self.ref_count = 0
         self.object_id = object_id
-    
+
     def inc_refcount(self):
         self.ref_count += 1
-    
+
     def dec_refcount(self):
         assert self.ref_count > 0
         self.ref_count -= 1
         if self.ref_count <= 0:
             self.cancel()
-    
+
     def complete(self):
         self.set_result(self.object_id)
-    
+
     def __repr__(self):
-        return super().__repr__() + "{object_id=%s, ref_count=%d}" % (self.object_id, self.ref_count)
+        return super().__repr__() + "{object_id=%s, ref_count=%d}" % (
+            self.object_id, self.ref_count)
 
 
 class PlasmaEpoll(selectors.BaseSelector):
     def __init__(self, worker):
         self.worker = worker
         self.client = self.worker.plasma_client
-        self.socket = socket.fromfd(self.client.notification_fd, socket.AF_UNIX, socket.SOCK_STREAM, 0)
+        self.socket = socket.fromfd(self.client.notification_fd,
+                                    socket.AF_UNIX, socket.SOCK_STREAM, 0)
         self.waiting_dict = collections.defaultdict(list)
-    
+
     def _read_message(self):
         size_data = self.socket.recv(ctypes.sizeof(ctypes.c_int64))
         size = int.from_bytes(size_data, sys.byteorder, signed=True)
         data = self.socket.recv(size)
         return data
-    
+
     def _decode_data(self, data):
         # `ObjectInfo` is defined in `ray/src/common/format/common.fbs`
         # TODO: decode data
         raise NotImplementedError
-    
+
     def select(self, timeout=None):
         self.socket.settimeout(timeout)
-        
+
         try:
             data = self._read_message()
         except (BlockingIOError, socket.timeout):
             return []
         finally:
             self.socket.settimeout(None)
-        
+
         ready_keys = []
         object_ids = self._decode_data(data)
         for oid in object_ids:
@@ -115,21 +117,23 @@ class PlasmaEpoll(selectors.BaseSelector):
                 key = self.waiting_dict[oid]
                 ready_keys.append(key)
         return ready_keys
-    
+
     def register(self, plasma_fut, events=None, data=None):
         if plasma_fut.object_id in self.waiting_dict:
             raise Exception("ObjectID already been registered.")
         else:
-            key = selectors.SelectorKey(fileobj=plasma_fut, fd=plasma_fut.object_id, events=events, data=data)
+            key = selectors.SelectorKey(fileobj=plasma_fut,
+                                        fd=plasma_fut.object_id, events=events,
+                                        data=data)
             self.waiting_dict[key.fd] = key
             return key
-    
+
     def unregister(self, plasma_fut):
         return self.waiting_dict.pop(plasma_fut.object_id)
-    
+
     def get_map(self):
         return self.waiting_dict
-    
+
     def get_key(self, object_id):
         return self.waiting_dict[object_id]
 
@@ -138,57 +142,61 @@ class PlasmaPoll(selectors.BaseSelector):
     def __init__(self, worker):
         self.worker = worker
         self.waiting_dict = {}
-    
+
     def close(self):
         self.waiting_dict.clear()
-    
+
     def select(self, timeout=None):
         if not self.waiting_dict:
             return []
-        
+
         polling_ids = list(self.waiting_dict.keys())
         ready_keys = []
-        object_ids, _ = ray.wait(polling_ids, num_returns=len(polling_ids), timeout=timeout, worker=self.worker)
+        object_ids, _ = ray.wait(polling_ids, num_returns=len(polling_ids),
+                                 timeout=timeout, worker=self.worker)
         for oid in object_ids:
             key = self.waiting_dict[oid]
             ready_keys.append(key)
         return ready_keys
-    
+
     def register(self, plasma_fut, events=None, data=None):
         if plasma_fut.object_id in self.waiting_dict:
             raise Exception("ObjectID already been registered.")
         else:
-            key = selectors.SelectorKey(fileobj=plasma_fut, fd=plasma_fut.object_id, events=events, data=data)
+            key = selectors.SelectorKey(fileobj=plasma_fut,
+                                        fd=plasma_fut.object_id, events=events,
+                                        data=data)
             self.waiting_dict[key.fd] = key
             return key
-    
+
     def unregister(self, plasma_fut):
         return self.waiting_dict.pop(plasma_fut.object_id)
-    
+
     def get_map(self):
         return self.waiting_dict
-    
+
     def get_key(self, object_id):
         return self.waiting_dict[object_id]
 
 
 class PlasmaSelectorEventLoop(asyncio.BaseEventLoop):
-    
+
     def __init__(self, selector, worker):
         super().__init__()
         assert isinstance(selector, selectors.BaseSelector)
         self._selector = selector
         self._worker = worker
-    
+
     def _process_events(self, event_list):
         for key in event_list:
             handle = key.data
-            assert isinstance(handle, asyncio.events.Handle), "A Handle is required here"
+            assert isinstance(handle, asyncio.events.Handle), \
+                "A Handle is required here"
             if handle._cancelled:
                 return
             assert not isinstance(handle, asyncio.events.TimerHandle)
             self._ready.append(handle)
-    
+
     def close(self):
         if self.is_running():
             raise RuntimeError("Cannot close a running event loop")
@@ -198,13 +206,15 @@ class PlasmaSelectorEventLoop(asyncio.BaseEventLoop):
         if self._selector is not None:
             self._selector.close()
             self._selector = None
-    
+
     def _register_future(self, future):
         future = asyncio.ensure_future(future, loop=self)
-        fut = PlasmaObjectFuture(loop=self, object_id=ray.local_scheduler.ObjectID(b'\0' * 20))
+        fut = PlasmaObjectFuture(loop=self,
+                                 object_id=ray.local_scheduler.ObjectID(
+                                     b'\0' * 20))
         if self.get_debug():
             print("Processing indirect future %s" % future)
-        
+
         def callback(_future):
             object_id = _future.result()
             assert isinstance(object_id, ray.local_scheduler.ObjectID)
@@ -212,22 +222,22 @@ class PlasmaSelectorEventLoop(asyncio.BaseEventLoop):
                 print("Registering indirect future...")
             reg_future = self._register_id(object_id)
             fut.object_id = object_id  # here we get the waiting id
-            
+
             def reg_callback(_fut):
                 result = _fut.result()
                 fut.set_result(result)
-            
+
             reg_future.add_done_callback(reg_callback)
-        
+
         future.add_done_callback(callback)
         return fut
-    
+
     def _register_id(self, object_id):
         self._check_closed()
-        
+
         if not isinstance(object_id, ray.local_scheduler.ObjectID):
             return self._register_future(object_id)
-        
+
         try:
             key = self._selector.get_key(object_id)
         except KeyError:
@@ -240,47 +250,51 @@ class PlasmaSelectorEventLoop(asyncio.BaseEventLoop):
                 self._selector.unregister(future)
                 if self.get_debug():
                     print("%s removed from the selector." % future)
-            
+
             fut = PlasmaObjectFuture(loop=self, object_id=object_id)
             handle = asyncio.events.Handle(callback, args=[fut], loop=self)
             self._selector.register(fut, events=None, data=handle)
             if self.get_debug():
                 print("%s added to the selector." % fut)
         else:
-            # Keep a unique Future object for an object_id. Increase ref_count instead.
+            # Keep a unique Future object for an object_id.
+            # Increase ref_count instead.
             fut = key.fileobj
             if self.get_debug():
                 print("%s exists." % fut)
-        
+
         fut.inc_refcount()
-        
+
         return fut
-    
+
     def _release(self, *fut):
         for f in fut:
             f.dec_refcount()
             if f.cancelled():
                 self._selector.unregister(f)
-    
+
     @asyncio.coroutine
     def get(self, object_ids):
         if not isinstance(object_ids, list):
             ready_ids = yield from self._register_id(object_ids)
         else:
-            ready_ids = yield from asyncio.gather(*[self._register_id(oid) for oid in object_ids], loop=self)
-        
+            ready_ids = yield from asyncio.gather(
+                *[self._register_id(oid) for oid in object_ids], loop=self)
+
         return ray.get(ready_ids, worker=self._worker)
-    
+
     @asyncio.coroutine
-    def wait(self, object_ids, num_returns=1, timeout=None, return_exact_num=True):
+    def wait(self, object_ids, num_returns=1, timeout=None,
+             return_exact_num=True):
         futures = [self._register_id(oid) for oid in object_ids]
-        _done, _pending = yield from _wait(futures, timeout=timeout, num_returns=num_returns, loop=self)
-        
+        _done, _pending = yield from _wait(futures, timeout=timeout,
+                                           num_returns=num_returns, loop=self)
+
         self._release(*_pending)
         done = [fut.object_id for fut in _done]
         pending = [fut.object_id for fut in _pending]
-        
+
         if return_exact_num and len(done) > num_returns:
             done, pending = done[:num_returns], done[num_returns:] + pending
-        
+
         return done, pending
--- python/ray/plasma/test/test_async.py
+++ python/ray/plasma/test/test_async.py
@@ -20,12 +20,15 @@ def gen_hashflow(seed, width, depth):
     stages = []
     for _ in range(depth):
         nodes = [
-            HashFlowNode(parents=[random.randint(0, width - 1) for _ in range(n)], delay=random.random(), result=None)
+            HashFlowNode(
+                parents=[random.randint(0, width - 1) for _ in range(n)],
+                delay=random.random(), result=None)
             for _ in range(width)]
         stages.append(nodes)
-    
-    stages.append([HashFlowNode(parents=list(range(width)), delay=random.random(), result=None)])
-    
+
+    stages.append([HashFlowNode(parents=list(range(width)),
+                                delay=random.random(), result=None)])
+
     return inputs, stages
 
 
@@ -33,17 +36,17 @@ def gen_hashflow(seed, width, depth):
 def calc_hashflow(inputs, delay=None):
     import time
     import hashlib
-    
+
     if delay is not None:
         time.sleep(delay)
-    
+
     m = hashlib.sha256()
-    
+
     for item in inputs:
         if isinstance(item, ray.local_scheduler.ObjectID):
             item = ray.get(item)
         m.update(item)
-    
+
     return m.digest()
 
 
@@ -56,7 +59,7 @@ def default_hashflow_solution(inputs, stages, use_delay=False):
             delay = node.delay if use_delay else None
             new_inputs.append(calc_hashflow.remote(node_inputs, delay=delay))
         inputs = new_inputs
-    
+
     return ray.get(inputs[0])
 
 
@@ -66,7 +69,7 @@ def wait_and_solve(inputs, node, use_delay, loop):
         r_inputs = yield from a_inputs
         delay = node.delay if use_delay else None
         return calc_hashflow.remote(r_inputs, delay=delay)
-    
+
     return asyncio.ensure_future(_wait_and_solve(inputs), loop=loop)
 
 
@@ -74,16 +77,17 @@ def async_hashflow_solution_get(inputs, stages, use_delay=False):
     selector = PlasmaPoll(ray.worker.global_worker)
     loop = PlasmaSelectorEventLoop(selector, worker=ray.worker.global_worker)
     loop.set_debug(True)
-    
+
     inputs = list(map(ray.put, inputs))
     for i, stage in enumerate(stages):
         new_inputs = []
         for node in stage:
             node_inputs = [inputs[i] for i in node.parents]
             async_inputs = loop.get(node_inputs)
-            new_inputs.append(wait_and_solve(async_inputs, node, use_delay, loop=loop))
+            new_inputs.append(
+                wait_and_solve(async_inputs, node, use_delay, loop=loop))
         inputs = new_inputs
-    
+
     result = loop.run_until_complete(inputs[0])
     loop.close()
     return ray.get(result)
@@ -93,12 +97,12 @@ def async_hashflow_solution_wait(inputs, stages, use_delay=False):
     selector = PlasmaPoll(ray.worker.global_worker)
     loop = PlasmaSelectorEventLoop(selector, worker=ray.worker.global_worker)
     loop.set_debug(True)
-    
+
     @asyncio.coroutine
     def return_first_item(coro):
         result = yield from coro
         return result[0]
-    
+
     inputs = list(map(ray.put, inputs))
     for i, stage in enumerate(stages):
         new_inputs = []
@@ -106,9 +110,10 @@ def async_hashflow_solution_wait(inputs, stages, use_delay=False):
             node_inputs = [inputs[i] for i in node.parents]
             async_inputs = loop.wait(node_inputs, num_returns=len(node_inputs))
             ready = return_first_item(async_inputs)
-            new_inputs.append(wait_and_solve(ready, node, use_delay, loop=loop))
+            new_inputs.append(
+                wait_and_solve(ready, node, use_delay, loop=loop))
         inputs = new_inputs
-    
+
     result = loop.run_until_complete(inputs[0])
     loop.close()
     return ray.get(result)
@@ -118,65 +123,69 @@ class TestAsyncPlasmaBasic(unittest.TestCase):
     def setUp(self):
         # Start the Ray processes.
         ray.init(num_cpus=2)
-    
+
     def tearDown(self):
         ray.worker.cleanup()
-    
+
     def test_get(self):
         selector = PlasmaPoll(ray.worker.global_worker)
-        loop = PlasmaSelectorEventLoop(selector, worker=ray.worker.global_worker)
+        loop = PlasmaSelectorEventLoop(selector,
+                                       worker=ray.worker.global_worker)
         loop.set_debug(True)
-        
+
         @ray.remote
         def f(n):
             import time
             time.sleep(n)
             return n
-        
+
         tasks = [f.remote(i) for i in range(5)]
-        
+
         results = loop.run_until_complete(loop.get(tasks))
         self.assertListEqual(results, ray.get(tasks))
-        
+
         loop.close()
-    
+
     def test_wait(self):
         selector = PlasmaPoll(ray.worker.global_worker)
-        loop = PlasmaSelectorEventLoop(selector, worker=ray.worker.global_worker)
+        loop = PlasmaSelectorEventLoop(selector,
+                                       worker=ray.worker.global_worker)
         loop.set_debug(True)
-        
+
         @ray.remote
         def f(n):
             import time
             time.sleep(n)
             return n
-        
+
         tasks = [f.remote(i) for i in range(5)]
-        results, _ = loop.run_until_complete(loop.wait(tasks, num_returns=len(tasks)))
+        results, _ = loop.run_until_complete(
+            loop.wait(tasks, num_returns=len(tasks)))
         self.assertEqual(set(results), set(tasks))
         loop.close()
 
 
 class TestAsyncPlasmaWait(unittest.TestCase):
-    answer = b'U\x16\xc5c\x0fa\xdcx\x03\x1e\xf7\xd8&{\xece\x85-.O\x12\xed\x11[\xdc\xe6\xcc\xdf\x90\x91\xc7\xf7'
-    
+    answer = b'U\x16\xc5c\x0fa\xdcx\x03\x1e\xf7\xd8&{\xece' \
+             b'\x85-.O\x12\xed\x11[\xdc\xe6\xcc\xdf\x90\x91\xc7\xf7'
+
     def setUp(self):
         # Start the Ray processes.
         ray.init(num_cpus=2)
-    
+
     def tearDown(self):
         ray.worker.cleanup()
-    
+
     def test_baseline(self):
         inputs, stages = gen_hashflow(0, 16, 16)
         ground_truth = default_hashflow_solution(inputs, stages, True)
         self.assertEqual(ground_truth, self.answer)
-    
+
     def test_async_get(self):
         inputs, stages = gen_hashflow(0, 16, 16)
         result = async_hashflow_solution_get(inputs, stages, use_delay=True)
         self.assertEqual(result, self.answer)
-    
+
     def test_async_wait(self):
         inputs, stages = gen_hashflow(0, 16, 16)
         result = async_hashflow_solution_wait(inputs, stages, use_delay=True)
