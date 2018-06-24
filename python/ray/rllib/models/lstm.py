d0e1dc0a1c363127a1092e24f1819f52439527bf
Eric Liang <ekhliang@gmail.com>
2018-06-23 18:55:11 -0700

    fix lstm

--- python/ray/rllib/models/lstm.py
+++ python/ray/rllib/models/lstm.py
@@ -20,19 +20,20 @@ class LSTM(Model):
         use_tf100_api = (distutils.version.LooseVersion(tf.VERSION) >=
                          distutils.version.LooseVersion("1.0.0"))
 
-        self.x = x = inputs
+        last_layer = inputs
         for i in range(4):
-            x = tf.nn.elu(conv2d(x, 32, "l{}".format(i + 1), [3, 3], [2, 2]))
+            last_layer = tf.nn.elu(
+                conv2d(last_layer, 32, "l{}".format(i + 1), [3, 3], [2, 2]))
         # Introduce a "fake" batch dimension of 1 after flatten so that we can
         # do LSTM over the time dim.
-        x = tf.expand_dims(flatten(x), [0])
+        last_layer = tf.expand_dims(flatten(last_layer), [0])
 
         size = 256
         if use_tf100_api:
             lstm = rnn.BasicLSTMCell(size, state_is_tuple=True)
         else:
             lstm = rnn.rnn_cell.BasicLSTMCell(size, state_is_tuple=True)
-        step_size = tf.shape(self.x)[:1]
+        step_size = tf.shape(last_layer)[:1]
 
         c_init = np.zeros(lstm.state_size.c, np.float32)
         h_init = np.zeros(lstm.state_size.h, np.float32)
@@ -45,12 +46,13 @@ class LSTM(Model):
             state_in = rnn.LSTMStateTuple(c_in, h_in)
         else:
             state_in = rnn.rnn_cell.LSTMStateTuple(c_in, h_in)
-        lstm_out, lstm_state = tf.nn.dynamic_rnn(lstm, x,
+        lstm_out, lstm_state = tf.nn.dynamic_rnn(lstm, last_layer,
                                                  initial_state=state_in,
                                                  sequence_length=step_size,
                                                  time_major=False)
         lstm_c, lstm_h = lstm_state
-        x = tf.reshape(lstm_out, [-1, size])
-        logits = linear(x, num_outputs, "action", normc_initializer(0.01))
+        last_layer = tf.reshape(lstm_out, [-1, size])
+        logits = linear(
+            last_layer, num_outputs, "action", normc_initializer(0.01))
         self.state_out = [lstm_c[:1, :], lstm_h[:1, :]]
-        return logits, x
+        return logits, last_layer
