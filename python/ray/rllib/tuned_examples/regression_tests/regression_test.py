d5f42edb35d0fb27735def7c7e43ce039e00ec06
Richard Liaw <rliaw@berkeley.edu>
2018-06-25 16:34:05 -0700

    more tests

--- python/ray/rllib/tuned_examples/regression_tests/regression_test.py
+++ python/ray/rllib/tuned_examples/regression_tests/regression_test.py
@@ -5,6 +5,7 @@ from __future__ import division
 from __future__ import print_function
 
 from collections import defaultdict
+import numpy as np
 import glob
 import os
 import yaml
@@ -15,29 +16,47 @@ from ray import tune
 
 CONFIG_DIR = os.path.dirname(os.path.abspath(__file__))
 
+def _evaulate_config(filename):
+    with open(os.path.join(CONFIG_DIR, filename)) as f:
+        experiments = yaml.load(f)
+    ray.init()
+    trials = tune.run_experiments(experiments)
+    assert len(trials) == 1
+    results = defaultdict(list)
+    for t in trials:
+        results["time_total_s"] += [t.last_result.time_total_s]
+        results["episode_reward_mean"] += [t.last_result.episode_reward_mean]
+        results["training_iteration"] += [t.last_result.training_iteration]
 
-class Regressions():
-    _file = os.join(CONFIG_DIR, "cartpole-ppo.yaml")
-    _results = {}
+    return {k: np.median(v) for k, v in results.items()}
 
+
+class Regression():
     def setup_cache(self):
-        # load data from a file
-        with open(self._file) as f:
-            experiments = yaml.load(f)
-        ray.init()
-        trials = tune.run_experiments(experiments)
-        assert len(trials) == 1
-
-        self._results[self._file] = trials[0].last_result
+        raise NotImplementedError
 
     def teardown(self, *args):
         ray.worker.cleanup()
 
-    def track_time(self):
-        return self._results[self._file].time_total_s
+    def track_time(self, result):
+        return result["time_total_s"]
 
-    def track_reward(self):
-        return self._results[self._file].episode_reward_mean
+    def track_reward(self, result):
+        return result["episode_reward_mean"]
 
-    def track_iterations(self):
-        return self._results[self._file].training_iteration
+    def track_iterations(self, result):
+        return result["training_iteration"]
+
+
+class TestCartPolePPO(Regression):
+    _file = "cartpole-ppo.yaml"
+
+    def setup_cache(self):
+        return _evaulate_config(self._file)
+
+
+class TestCartPolePG(Regression):
+    _file = "cartpole-pg.yaml"
+
+    def setup_cache(self):
+        return _evaulate_config(self._file)
