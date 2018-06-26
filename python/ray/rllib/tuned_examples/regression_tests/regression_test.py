6e03079a1a83e4b031a9565d658c6aa148932774
Richard Liaw <rliaw@berkeley.edu>
2018-06-26 14:25:47 -0700

    comments

--- python/ray/rllib/tuned_examples/regression_tests/regression_test.py
+++ python/ray/rllib/tuned_examples/regression_tests/regression_test.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+"""
+This class runs the regression YAMLs in the ASV format.
+"""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -6,7 +9,6 @@ from __future__ import print_function
 
 from collections import defaultdict
 import numpy as np
-import glob
 import os
 import yaml
 
@@ -16,12 +18,12 @@ from ray import tune
 
 CONFIG_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
 def _evaulate_config(filename):
     with open(os.path.join(CONFIG_DIR, filename)) as f:
         experiments = yaml.load(f)
     ray.init()
     trials = tune.run_experiments(experiments)
-    assert len(trials) == 1
     results = defaultdict(list)
     for t in trials:
         results["time_total_s"] += [t.last_result.time_total_s]
@@ -33,6 +35,9 @@ def _evaulate_config(filename):
 
 class Regression():
     def setup_cache(self):
+        # We need to implement this in separate classes
+        # below so that ASV will register the setup/class
+        # as a separate test.
         raise NotImplementedError
 
     def teardown(self, *args):
@@ -57,6 +62,41 @@ class TestCartPolePPO(Regression):
 
 class TestCartPolePG(Regression):
     _file = "cartpole-pg.yaml"
+
+    def setup_cache(self):
+        return _evaulate_config(self._file)
+
+
+class TestPendulumDDPG(Regression):
+    _file = "pendulum-ddpg.yaml"
+
+    def setup_cache(self):
+        return _evaulate_config(self._file)
+
+
+class TestCartPoleES(Regression):
+    _file = "cartpole-es.yaml"
+
+    def setup_cache(self):
+        return _evaulate_config(self._file)
+
+
+class TestCartPoleDQN(Regression):
+    _file = "cartpole-dqn.yaml"
+
+    def setup_cache(self):
+        return _evaulate_config(self._file)
+
+
+class TestCartPoleA3C(Regression):
+    _file = "cartpole-a3c.yaml"
+
+    def setup_cache(self):
+        return _evaulate_config(self._file)
+
+
+class TestCartPoleA3CPyTorch(Regression):
+    _file = "cartpole-a3c-pytorch.yaml"
 
     def setup_cache(self):
         return _evaulate_config(self._file)
