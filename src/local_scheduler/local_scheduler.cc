2a6ebaa00fd14a616292936be525388307db6e3e
Philipp Moritz <pcmoritz@gmail.com>
2018-06-28 02:06:33 -0700

    linting

--- src/local_scheduler/local_scheduler.cc
+++ src/local_scheduler/local_scheduler.cc
@@ -360,7 +360,8 @@ LocalSchedulerState *LocalSchedulerState_init(
     RAY_CHECK_OK(state->gcs_client.Connect(std::string(redis_primary_addr),
                                            redis_primary_port, true));
     RAY_CHECK_OK(state->gcs_client.context()->AttachToEventLoop(loop));
-    RAY_CHECK_OK(state->gcs_client.auxiliary_context()->AttachToEventLoop(loop));
+    RAY_CHECK_OK(
+        state->gcs_client.auxiliary_context()->AttachToEventLoop(loop));
   } else {
     state->db = NULL;
   }
