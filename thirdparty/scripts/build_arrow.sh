f0aef7db9ac18c30a7cd957bf8e79eecf5913178
Philipp Moritz <pcmoritz@gmail.com>
2018-07-04 23:22:27 -0700

    use jemalloc zero fill

--- thirdparty/scripts/build_arrow.sh
+++ thirdparty/scripts/build_arrow.sh
@@ -53,7 +53,7 @@ if [[ ! -d $TP_DIR/../python/ray/pyarrow_files/pyarrow || \
     fi
 
     if [[ ! -d $TP_DIR/build/arrow ]]; then
-      git clone https://github.com/apache/arrow.git "$TP_DIR/build/arrow"
+      git clone https://github.com/pcmoritz/arrow.git "$TP_DIR/build/arrow"
     fi
 
     pushd $TP_DIR/build/arrow
@@ -61,7 +61,7 @@ if [[ ! -d $TP_DIR/../python/ray/pyarrow_files/pyarrow || \
     # The PR for this commit is https://github.com/apache/arrow/pull/2210. We
     # include the link here to make it easier to find the right commit because
     # Arrow often rewrites git history and invalidates certain commits.
-    git checkout e7aaf7bf3d3e326b5fe58d20f8fc45b5cec01cac
+    git checkout a916eebe0d088337cb5fb27dae62be085503701b
 
     cd cpp
     if [ ! -d "build" ]; then
@@ -88,7 +88,8 @@ if [[ ! -d $TP_DIR/../python/ray/pyarrow_files/pyarrow || \
         -DARROW_PYTHON=on \
         -DARROW_PLASMA=on \
         -DPLASMA_PYTHON=on \
-        -DARROW_JEMALLOC=off \
+        -DARROW_JEMALLOC=on \
+        -DARROW_JEMALLOC_BUILD_FLAGS:STRING=--enable-fill \
         -DARROW_WITH_BROTLI=off \
         -DARROW_WITH_LZ4=off \
         -DARROW_WITH_ZLIB=off \
