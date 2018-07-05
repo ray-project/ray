dee22780e44e736b117032fdde892cca371df244
Richard Liaw <rliaw@berkeley.edu>
2018-07-04 20:32:06 -0700

    compute_targets

--- python/ray/rllib/evaluation/__init__.py
+++ python/ray/rllib/evaluation/__init__.py
@@ -6,12 +6,12 @@ from ray.rllib.evaluation.torch_policy_graph import TorchPolicyGraph
 from ray.rllib.evaluation.sample_batch import SampleBatch, MultiAgentBatch, \
     SampleBatchBuilder, MultiAgentSampleBatchBuilder
 from ray.rllib.evaluation.sampler import SyncSampler, AsyncSampler
-from ray.rllib.evaluation.postprocessing import compute_advantages
+from ray.rllib.evaluation.postprocessing import compute_advantages, compute_targets
 from ray.rllib.evaluation.metrics import collect_metrics
 
 __all__ = [
     "EvaluatorInterface", "PolicyEvaluator", "PolicyGraph", "TFPolicyGraph",
     "TorchPolicyGraph", "SampleBatch", "MultiAgentBatch", "SampleBatchBuilder",
     "MultiAgentSampleBatchBuilder", "SyncSampler", "AsyncSampler",
-    "compute_advantages", "collect_metrics"
+    "compute_advantages", "compute_targets", "collect_metrics"
 ]
