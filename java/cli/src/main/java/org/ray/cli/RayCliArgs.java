0abed1afc71726e65ff1e3158ea32fc91fc2458c
Zhijun Fu <37800433+zhijunfu@users.noreply.github.com>
2018-07-10 01:20:41 +0800

    [Java] Java worker cluster support (#2359)

---
+++ java/cli/src/main/java/org/ray/cli/RayCliArgs.java
@@ -0,0 +1,12 @@
+package org.ray.cli;
+
+import com.beust.jcommander.Parameter;
+
+/**
+ * Arguments for Ray cli.
+ */
+public class RayCliArgs {
+
+  @Parameter(names = {"-h", "-help", "--help"}, description = "print this usage", help = true)
+  public boolean help;
+}
