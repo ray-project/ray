2f329045046a7fcc4de071eb3bccc9c5cf78304b
Peter Schafhalter <pschafhalter@berkeley.edu>
2018-07-09 17:55:04 -0700

    formatting

--- python/ray/experimental/test/test_queue.py
+++ python/ray/experimental/test/test_queue.py
@@ -38,7 +38,7 @@ class TestQueue(unittest.TestCase):
         q = Queue()
 
         items = set(range(10))
-        producers = [   # noqa
+        producers = [  # noqa
             put_async.remote(q, item, True, None, 0.5) for item in items
         ]
         consumers = [get_async.remote(q, True, None, 0) for _ in items]
