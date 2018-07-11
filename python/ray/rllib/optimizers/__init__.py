22ae6874e098141fad8cd50f1ca3e80254161137
Richard Liaw <rliaw@berkeley.edu>
2018-07-11 16:15:21 +0200

    Revert "num_envs_per_worker"
    This reverts commit b1b9208dbd9f995f8a39bcc1f7c1155f382a80e1.

--- python/ray/rllib/optimizers/__init__.py
+++ python/ray/rllib/optimizers/__init__.py
@@ -9,6 +9,7 @@ from ray.rllib.optimizers.multi_gpu_optimizer import LocalMultiGPUOptimizer
 
 def run_optimizer(optimizer, num_steps, tag=""):
     from ray.tune.logger import UnifiedLogger
+    from ray.tune.result import TrainingResult
     import os
     import shutil
     path = os.path.join("/tmp/demo/", tag)
@@ -20,16 +21,20 @@ def run_optimizer(optimizer, num_steps, tag=""):
         os.makedirs(path)
     except OSError:
         print("could not make path")
+    optimizer.collect_metrics() # hack to warmup
     logger = UnifiedLogger({}, path, verbose=False)
+    result = TrainingResult(episode_reward_mean=0)
+    result = result._replace(
+        training_iteration=0,
+        timesteps_total=0)
+    logger.on_result(result)
+    logger.flush()
+
     timesteps_total = 0
-    for itr in range(num_steps + 1):
+    for itr in range(1, num_steps + 1):
         optimizer.step()
-        if itr:  #warm start
-            result = optimizer.collect_metrics()
-            timesteps_total = optimizer.num_steps_sampled
-        else:
-            from ray.tune.result import TrainingResult
-            result = TrainingResult(episode_reward_mean=0)
+        result = optimizer.collect_metrics()
+        timesteps_total = optimizer.num_steps_sampled
         result = result._replace(
             training_iteration=itr,
             timesteps_total=timesteps_total)
