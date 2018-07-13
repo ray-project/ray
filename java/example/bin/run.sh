a3b0f45c1537c89cd5c5491e74b351d61d285428
Hao Chen <chenh1024@gmail.com>
2018-07-13 11:35:52 +0800

    Revert "merge from ray"
    This reverts commit 32b181ebbb1fa184026631e1a7368112c4c3118d.

--- java/example/bin/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#first you should run the ../test.sh to build
-ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
-export RAY_CONFIG=$ROOT_DIR/../ray.config.ini
-java -Djava.library.path=../../build/src/plasma:../../build/src/local_scheduler -cp .:target/ray-example-1.0.jar:lib/* org.ray.example.HelloWorld
