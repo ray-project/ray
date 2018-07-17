e3badb9b0938b500effc1feb09c8ef3109afa8d8
Yuhong Guo <guoyuhong1985@outlook.com>
2018-07-17 13:56:14 +0800

    Fix that parquet and arrow will build every time. (#2405)
    * Fix the bug that parquet and arrow will build every time.
    
    * Update build_arrow.sh
    
    * Update build_arrow.sh

--- thirdparty/scripts/build_arrow.sh
+++ thirdparty/scripts/build_arrow.sh
@@ -149,13 +149,18 @@ if [[ ! -d $TP_DIR/../python/ray/pyarrow_files/pyarrow ]] || \
     [[ "$LANGUAGE" == "java" && ! -f $TP_DIR/build/arrow/cpp/build/release/libplasma_java.dylib ]]; then
   build_arrow
 else
+  REBUILD=off
   pushd $TP_DIR/build/arrow
   if [[ "$TARGET_COMMIT_ID" != `git rev-parse HEAD` ]]; then
     # TARGET_COMMIT_ID may change to later commit.
     echo "Commit ID mismatches."
     git fetch origin master
     git checkout $TARGET_COMMIT_ID
+    REBUILD=on
   fi
   popd
-  build_arrow
+
+  if [[ "$REBUILD" == "on" ]]; then
+    build_arrow
+  fi
 fi
