a44d3f628e700c4af155825341ddcf5598a08ee0
Richard Liaw <rliaw@berkeley.edu>
2018-07-23 20:35:07 -0700

    yapf

--- python/ray/tune/logger.py
+++ python/ray/tune/logger.py
@@ -133,11 +133,12 @@ class _TFLogger(Logger):
         values = to_tf_values(tmp, ["ray", "tune"])
         train_stats = tf.Summary(value=values)
         self._file_writer.add_summary(train_stats, result.timesteps_total)
-        timesteps_value = to_tf_values(
-            {"timesteps_total": result.timesteps_total}, ["ray", "tune"])
+        timesteps_value = to_tf_values({
+            "timesteps_total": result.timesteps_total
+        }, ["ray", "tune"])
         timesteps_stats = tf.Summary(value=timesteps_value)
-        self._file_writer.add_summary(
-            timesteps_stats, result.training_iteration)
+        self._file_writer.add_summary(timesteps_stats,
+                                      result.training_iteration)
 
     def flush(self):
         self._file_writer.flush()
