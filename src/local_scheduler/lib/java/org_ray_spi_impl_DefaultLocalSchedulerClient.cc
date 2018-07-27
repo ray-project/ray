5da71aff7824aa3b717a6edc1aa1a0f451843ca7
Stephanie <swang@cs.berkeley.edu>
2018-07-27 11:11:10 -0400

    lint

--- src/local_scheduler/lib/java/org_ray_spi_impl_DefaultLocalSchedulerClient.cc
+++ src/local_scheduler/lib/java/org_ray_spi_impl_DefaultLocalSchedulerClient.cc
@@ -50,8 +50,8 @@ Java_org_ray_spi_impl_DefaultLocalSchedulerClient__1init(JNIEnv *env,
   UniqueIdFromJByteArray driver_id(env, driverId);
   const char *nativeString = env->GetStringUTFChars(sockName, JNI_FALSE);
   bool use_raylet = false;
-  auto client = LocalSchedulerConnection_init(nativeString, *worker_id.PID,
-                                              isWorker, *driver_id.PID, use_raylet);
+  auto client = LocalSchedulerConnection_init(
+      nativeString, *worker_id.PID, isWorker, *driver_id.PID, use_raylet);
   env->ReleaseStringUTFChars(sockName, nativeString);
   return reinterpret_cast<jlong>(client);
 }
