fcee2b82380ac9cc1c0d6fd1d528415ffbb71817
Richard Liaw <rliaw@berkeley.edu>
2018-08-06 16:41:04 -0700

    remove extra logger

--- python/ray/autoscaler/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-import sys
-
-logger = logging.getLogger("ray.autoscaler")
-logger.setLevel(logging.INFO)
-ch = logging.StreamHandler(sys.stderr)
-ch.setLevel(logging.INFO)
-logger.addHandler(ch)
