ebad40b727433d873bcd4fe82fd8cd819bd76429
Richard Liaw <rliaw@berkeley.edu>
2018-08-05 20:27:55 -0700

    revert result to subclass dict

--- python/ray/tune/result.py
+++ python/ray/tune/result.py
@@ -6,22 +6,19 @@ import os
 import sys
 import copy
 
-if sys.version_info[0] == 2:
-    from collections import MutableMapping
-elif sys.version_info[0] == 3:
-    from collections.abc import MutableMapping
-
 # Where Tune writes result files by default
 DEFAULT_RESULTS_DIR = os.path.expanduser("~/ray_results")
 
 
-class TrainingResult(MutableMapping):
+class TrainingResult(dict):
     """Dict for storing results for Tune, with extra properties.
 
     When using Tune with custom training scripts, you must periodically report
     training status back to Ray by calling reporter(result).
 
-    Properties:
+    See also: ray.tune.Trainable.
+
+    Attributes:
         done (bool): (Optional) If training is terminated.
         time_this_iter_s (float): (Auto-filled) Time in seconds
             this iteration took to run. This may be overriden in order to
@@ -47,75 +44,25 @@ class TrainingResult(MutableMapping):
         # `done` has special treatment because the responsibility
         # to set it is shared between user and system, and to ensure
         # CSV Writer doesn't fail, this is defaulted just in case.
-        self._store = dict(done=False)
-        self.update(dict(*args, **kwargs))
-
-    def __getitem__(self, key):
-        return self._store[key]
-
-    def __setitem__(self, key, value):
-        self._store[key] = value
-
-    def __delitem__(self, key):
-        del self._store[key]
-
-    def __iter__(self):
-        return iter(self._store)
-
-    def __len__(self):
-        return len(self._store)
+        default_values = {
+            "done": False
+        }
+        kwargs.update(default_values)
+        super(TrainingResult, self).__init__(*args, **kwargs)
 
     def copy(self):
-        return TrainingResult(**self._store)
+        return TrainingResult(**self)
 
     def __repr__(self):
-        return "TrainingResult({})".format(self._store)
+        return "TrainingResult({})".format(super(TrainingResult, self).__repr__())
 
-    def as_dict(self):
-        """Retrieves copy ofinternal dict, used for JSON dumping."""
-        return copy.deepcopy(self._store)
+    def __getattribute__(self, name):
+        """Overrides default to first query internal dict state.
 
-    @property
-    def done(self):
-        return self.get("done")
-
-    @property
-    def time_this_iter_s(self):
-        return self.get("time_this_iter_s")
-
-    @property
-    def time_total_s(self):
-        return self.get("time_total_s")
-
-    @property
-    def experiment_id(self):
-        return self.get("experiment_id")
-
-    @property
-    def training_iteration(self):
-        return self.get("training_iteration")
-
-    @property
-    def pid(self):
-        return self.get("pid")
-
-    @property
-    def date(self):
-        return self.get("date")
-
-    @property
-    def timestamp(self):
-        return self.get("timestamp")
-
-    @property
-    def hostname(self):
-        return self.get("hostname")
-
-    @property
-    def node_ip(self):
-        return self.get("node_ip")
-
-    @property
-    def metric(self):
-        # TODO(rliaw): expose this better?
-        return self.get("metric")
+        This is needed to support Pickle/PyArrow serialization."""
+        if name in self:
+            return self[name]
+        elif hasattr(super(TrainingResult, self), name):
+            return super(TrainingResult, self).__getattribute__(name)
+        else:
+            raise AttributeError("'{}' not found in TrainingResult!".format(name))
