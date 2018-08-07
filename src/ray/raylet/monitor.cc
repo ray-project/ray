bf1d467b9289000d9fdc0f85c242208b3a78ad32
Philipp Moritz <pcmoritz@gmail.com>
2018-08-06 21:26:40 -0700

    fix heartbeat_timeout_milliseconds in monitor

--- src/ray/raylet/monitor.cc
+++ src/ray/raylet/monitor.cc
@@ -16,7 +16,7 @@ namespace raylet {
 Monitor::Monitor(boost::asio::io_service &io_service, const std::string &redis_address,
                  int redis_port)
     : gcs_client_(),
-      heartbeat_timeout_ms_(RayConfig::instance().num_heartbeats_timeout()),
+      heartbeat_timeout_ms_(RayConfig::instance().heartbeat_timeout_milliseconds()),
       heartbeat_timer_(io_service) {
   RAY_CHECK_OK(gcs_client_.Connect(redis_address, redis_port, /*sharding=*/true));
   RAY_CHECK_OK(gcs_client_.Attach(io_service));
