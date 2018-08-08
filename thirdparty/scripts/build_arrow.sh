6dbdbed817a749964ec3ac3fd4efc32fe9d759b2
Philipp Moritz <pcmoritz@gmail.com>
2018-08-08 13:11:07 -0700

    upgrade arrow to include tensorflow fix

--- thirdparty/scripts/build_arrow.sh
+++ thirdparty/scripts/build_arrow.sh
@@ -40,10 +40,10 @@ else
   exit 1
 fi
 
-# The PR for this commit is https://github.com/apache/arrow/pull/2953. We
+# The PR for this commit is https://github.com/apache/arrow/pull/2368. We
 # include the link here to make it easier to find the right commit because
 # Arrow often rewrites git history and invalidates certain commits.
-TARGET_COMMIT_ID=d48dce2cfebdbd044a8260d0a77f5fe3d89a4a2d
+TARGET_COMMIT_ID=4660833b2c5ef63a97445e304b8f72a2e0170f9c
 build_arrow() {
   echo "building arrow"
 
