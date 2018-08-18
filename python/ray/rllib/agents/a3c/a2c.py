85a5846cdcce357ad053fb3ce0f0154d1b0ae247
Eric Liang <ekhliang@gmail.com>
2018-08-17 17:49:52 -0700

    gpu

--- python/ray/rllib/agents/a3c/a2c.py
+++ python/ray/rllib/agents/a3c/a2c.py
@@ -5,14 +5,16 @@ from __future__ import print_function
 from ray.rllib.agents.a3c.a3c import A3CAgent, DEFAULT_CONFIG as A3C_CONFIG
 from ray.rllib.optimizers import SyncSamplesOptimizer
 from ray.rllib.utils import merge_dicts
+from ray.tune.trial import Resources
 
 A2C_DEFAULT_CONFIG = merge_dicts(
     A3C_CONFIG,
     {
-        "sample_batch_size": 100,
+        "gpu": False,
+        "sample_batch_size": 20,
         "min_iter_time_s": 10,
         "optimizer": {
-            "timesteps_per_batch": 1000,
+            "timesteps_per_batch": 200,
         },
     },
 )
@@ -28,3 +30,12 @@ class A2CAgent(A3CAgent):
         return SyncSamplesOptimizer(self.local_evaluator,
                                     self.remote_evaluators,
                                     self.config["optimizer"])
+
+    @classmethod
+    def default_resource_request(cls, config):
+        cf = merge_dicts(cls._default_config, config)
+        return Resources(
+            cpu=1,
+            gpu=1 if cf["gpu"] else 0,
+            extra_cpu=cf["num_workers"],
+            extra_gpu=cf["use_gpu_for_workers"] and cf["num_workers"] or 0)
