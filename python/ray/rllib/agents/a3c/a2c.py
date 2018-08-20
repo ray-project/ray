e453543cf6ff8ec0631fd1b3ff2da920aab4b667
Eric Liang <ekhliang@gmail.com>
2018-08-19 18:56:57 -0700

    remove ts per batch

--- python/ray/rllib/agents/a3c/a2c.py
+++ python/ray/rllib/agents/a3c/a2c.py
@@ -13,9 +13,6 @@ A2C_DEFAULT_CONFIG = merge_dicts(
         "gpu": False,
         "sample_batch_size": 20,
         "min_iter_time_s": 10,
-        "optimizer": {
-            "timesteps_per_batch": 200,
-        },
         "observation_filter": "MeanStdFilter",
         "summarize": True,
         "sample_async": False,
