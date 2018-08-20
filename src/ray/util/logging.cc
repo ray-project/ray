caf33bdd9da4ee7c2859d98c88e8b37d538668ab
Yuhong Guo <yuhong.gyh@antfin.com>
2018-08-20 20:01:53 +0800

    Refine code, unify level, address comment

--- src/ray/util/logging.cc
+++ src/ray/util/logging.cc
@@ -5,40 +5,121 @@
 
 #ifdef RAY_USE_GLOG
 #include "glog/logging.h"
-static_assert(RAY_INFO == google::GLOG_INFO, "Log enum mismatch");
-static_assert(RAY_WARNING == google::GLOG_WARNING, "Log enum mismatch");
-static_assert(RAY_ERROR == google::GLOG_ERROR, "Log enum mismatch");
-static_assert(RAY_FATAL == google::GLOG_FATAL, "Log enum mismatch");
 #elif RAY_USE_LOG4CPLUS
-#include <log4cplus/logger.h>
 #include <log4cplus/consoleappender.h>
 #include <log4cplus/fileappender.h>
 #include <log4cplus/layout.h>
+#include <log4cplus/logger.h>
 #include <log4cplus/loggingmacros.h>
-
 #endif
 
 namespace ray {
 
-namespace internal {
+// This is the default implementation of ray log,
+// which is independent of any libs.
+class CerrLog {
+ public:
+  CerrLog(int severity)  // NOLINT(runtime/explicit)
+      : severity_(severity),
+        has_logged_(false) {}
+
+  virtual ~CerrLog() {
+    if (has_logged_) {
+      std::cerr << std::endl;
+    }
+    if (severity_ == RAY_FATAL) {
+      PrintBackTrace();
+      std::abort();
+    }
+  }
+
+  std::ostream &Stream() {
+    has_logged_ = true;
+    return std::cerr;
+  }
+
+  template <class T>
+  CerrLog &operator<<(const T &t) {
+    if (severity_ != RAY_DEBUG) {
+      has_logged_ = true;
+      std::cerr << t;
+    }
+    return *this;
+  }
+
+ protected:
+  const int severity_;
+  bool has_logged_;
+
+  void PrintBackTrace() {
+#if defined(_EXECINFO_H) || !defined(_WIN32)
+    void *buffer[255];
+    const int calls = backtrace(buffer, sizeof(buffer) / sizeof(void *));
+    backtrace_symbols_fd(buffer, calls, 1);
+#endif
+  }
+};
+
+// This is a adapter class to adapt the log levels for different log libs.
+class SeverityMapper {
+ public:
+  inline static int GetMappedSeverity(int severity) {
+    // Ray log level starts from -1 (RAY_DEBUG);
+    return severity_map[severity + 1];
+  }
+
+ private:
+  static int severity_map[];
+};
 
 void *RayLog::static_impl = nullptr;
-int RayLog::severity_threshold_ = RAY_DEBUG;
+int RayLog::severity_threshold_ = RAY_ERROR;
 
-#ifdef RAY_USE_LOG4CPLUS
+#ifdef RAY_USE_GLOG
+using namespace google;
 
+// Glog's severity map.
+int SeverityMapper::severity_map[] = {
+    // glog has no DEBUG level. It has verbose level but hard to adapt here.
+    GLOG_INFO,     // RAY_DEBUG
+    GLOG_INFO,     // RAY_INFO
+    GLOG_WARNING,  // RAY_WARNING
+    GLOG_ERROR,    // RAY_ERROR
+    GLOG_FATAL     // RAY_FATAL
+};
+
+#elif RAY_USE_LOG4CPLUS
 using namespace log4cplus;
 using namespace log4cplus::helpers;
 
-static log4cplus::Logger static_logger = Logger::getInstance("default");
+// Log4cplus's severity map.
+int SeverityMapper::severity_map[] = {
+    DEBUG_LOG_LEVEL,  // RAY_DEBUG
+    INFO_LOG_LEVEL,   // RAY_INFO
+    WARN_LOG_LEVEL,   // RAY_WARNING
+    ERROR_LOG_LEVEL,  // RAY_ERROR
+    FATAL_LOG_LEVEL   // RAY_FATAL
+};
 
-class DefaultLogInitializer {
+// This is a helper class for log4cplus.
+// Log4cplus needs initialized, so the ctor will do the default initialization.
+class Log4cplusHelper {
  public:
+  Log4cplusHelper() {
+    // This is the function to setup a default log4cplus log.
+    // `static Log4cplusHelper log4cplus_initializer;` is used to trigger this function.
+    AddConsoleAppender(static_logger, "default console appender");
+    static_logger.setLogLevel(ALL_LOG_LEVEL);
+    RayLog::SetStaticImplement(&static_logger);
+  }
+
   static std::string GetDefualtPatternString() {
+    // Default log format.
     return "%d{%m/%d/%y %H:%M:%S}  - %m [%l]%n";
   }
 
-  static void AddConsoleAppender(log4cplus::Logger &logger, const std::string& appender_name) {
+  static void AddConsoleAppender(log4cplus::Logger &logger,
+                                 const std::string &appender_name) {
     SharedObjectPtr<Appender> appender(new ConsoleAppender());
     appender->setName(appender_name);
     std::unique_ptr<Layout> layout(new PatternLayout(GetDefualtPatternString()));
@@ -46,47 +127,68 @@ class DefaultLogInitializer {
     logger.addAppender(appender);
   }
 
-  static void AddFileAppender(log4cplus::Logger &logger, const std::string& app_name, const std::string& log_dir) {
-    SharedObjectPtr<Appender> appender(new DailyRollingFileAppender("/tmp/Log4CPlus.log", DAILY, false, 50));
+  static void AddFileAppender(log4cplus::Logger &logger, const std::string &app_name,
+                              const std::string &log_dir) {
+    SharedObjectPtr<Appender> appender(
+        new DailyRollingFileAppender(log_dir + app_name, DAILY, true, 50));
     std::unique_ptr<Layout> layout(new PatternLayout(GetDefualtPatternString()));
     appender->setName(app_name);
     appender->setLayout(std::move(layout));
     logger.addAppender(appender);
   }
-  DefaultLogInitializer() {
-    //log4cplus::Logger logger = Logger::getInstance("default");
-    AddConsoleAppender(static_logger, "default console appender");
-    /* step 6: Set a priority for the logger  */
-    static_logger.setLogLevel(ALL_LOG_LEVEL);
-    RayLog::SetImplPointer(&static_logger);
+
+  static void InitLog4cplus(const std::string &app_name, int severity_threshold,
+                            const std::string &log_dir) {
+    static_logger = Logger::getInstance(app_name);
+    AddConsoleAppender(static_logger,
+                       (std::string(app_name) + "console appender").c_str());
+    AddFileAppender(static_logger, app_name, log_dir);
+    static_logger.setLogLevel(SeverityMapper::GetMappedSeverity(severity_threshold));
+    RayLog::SetStaticImplement(&static_logger);
   }
+
+ private:
+  static log4cplus::Logger static_logger;
+  static int severity_map[];
 };
 
-static DefaultLogInitializer log4cplus_initializaer;
+log4cplus::Logger Log4cplusHelper::static_logger = Logger::getInstance("default");
+static Log4cplusHelper log4cplus_initializer;
 
 #endif
 
-void RayLog::StartRayLog(const char *app_name, int severity_threshold, const char* log_dir){
+void RayLog::StartRayLog(const char *app_name, int severity_threshold,
+                         const char *log_dir) {
   std::string dir_ends_with_slash = std::string(log_dir);
-  if (dir_ends_with_slash.length() > 0) {
+  if (!dir_ends_with_slash.empty()) {
     if (dir_ends_with_slash[dir_ends_with_slash.length() - 1] != '/') {
       dir_ends_with_slash += "/";
     }
   }
-  severity_threshold_ = std::max(severity_threshold, google::GLOG_INFO);
-#ifdef RAY_USE_GLOG
-  if (app_name != nullptr && strlen(app_name) > 0) {
-    google::InitGoogleLogging(app_name);
-    google::SetLogFilenameExtension(app_name);
+  std::string app_name_str = app_name;
+  if (app_name_str.empty()) {
+    app_name_str = "DefaultApp";
   } else {
-    return;
+    // Find the app name without the path.
+    std::string full_name = app_name;
+    size_t pos = full_name.rfind('/');
+    if (pos != full_name.npos && pos + 1 < full_name.length()) {
+      app_name_str = full_name.substr(pos + 1);
+    }
   }
-  if (dir_ends_with_slash.length() > 0) {
-    google::SetLogDestination(severity_threshold_, dir_ends_with_slash.c_str());
+  severity_threshold_ = severity_threshold;
+#ifdef RAY_USE_GLOG
+  int mapped_severity_threshold_ = SeverityMapper::GetMappedSeverity(severity_threshold_);
+  google::InitGoogleLogging(app_name_str.c_str());
+  if (!dir_ends_with_slash.empty()) {
+    google::SetLogFilenameExtension(app_name_str.c_str());
+    google::SetLogDestination(mapped_severity_threshold_, dir_ends_with_slash.c_str());
   }
-  google::SetStderrLogging(severity_threshold_);
+  google::SetStderrLogging(mapped_severity_threshold_);
 #elif RAY_USE_LOG4CPLUS
-
+  int mapped_severity_threshold_ = SeverityMapper::GetMappedSeverity(severity_threshold_);
+  Log4cplusHelper::InitLog4cplus(app_name_str, mapped_severity_threshold_,
+                                 dir_ends_with_slash);
 #endif
 }
 
@@ -94,61 +196,58 @@ void RayLog::ShutDownRayLog() {
 #ifdef RAY_USE_GLOG
   google::ShutdownGoogleLogging();
 #elif RAY_USE_LOG4CPLUS
-
+// Do not need to do anything.
 #endif
 }
 
-void *RayLog::GetImplPointer() {
-  return RayLog::static_impl;
-}
-
-void RayLog::SetImplPointer(void *pointer) {
-  RayLog::static_impl = pointer;
-}
-
-RayLog::RayLog(const char *file_name, int line_number, int severity) 
-  : file_name_(file_name), line_number_(line_number),
-    severity_(severity), implement(nullptr) {
+RayLog::RayLog(const char *file_name, int line_number, int severity)
+    : file_name_(file_name),
+      line_number_(line_number),
+      severity_(severity),
+      implement(nullptr) {
 #ifdef RAY_USE_GLOG
-  severity_ = std::max(severity_, google::GLOG_INFO);
   if (severity_ >= severity_threshold_) {
-    implement = new google::LogMessage(file_name_, line_number_, std::max(severity_, google::GLOG_INFO));
+    implement = new google::LogMessage(file_name_, line_number_,
+                                       SeverityMapper::GetMappedSeverity(severity_));
   }
 #elif RAY_USE_LOG4CPLUS
-  severity_ = (severity_ + 1) * 10000;
   implement = new std::stringstream();
 #else
   implement = new CerrLog(severity_);
-  *reinterpret_cast<CerrLog*>(implement) << file_name_ << ":" << line_number_ << ": ";
+  *reinterpret_cast<CerrLog *>(implement) << file_name_ << ":" << line_number_ << ": ";
 #endif
 }
 
-std::ostream &RayLog::stream() {
+std::ostream &RayLog::Stream() {
 #ifdef RAY_USE_GLOG
-  return reinterpret_cast<google::LogMessage*>(implement)->stream();
+  return reinterpret_cast<google::LogMessage *>(implement)->stream();
 #elif RAY_USE_LOG4CPLUS
-    return *reinterpret_cast<std::stringstream*>(implement);
+  return *reinterpret_cast<std::stringstream *>(implement);
 #else
-  return reinterpret_cast<CerrLog*>(implement)->stream();
+  return reinterpret_cast<CerrLog *>(implement)->Stream();
 #endif
 }
 
 RayLog::~RayLog() {
 #ifdef RAY_USE_GLOG
-  delete reinterpret_cast<google::LogMessage*>(implement);
+  delete reinterpret_cast<google::LogMessage *>(implement);
 #elif RAY_USE_LOG4CPLUS
-  log4cplus::Logger& logger = *reinterpret_cast<log4cplus::Logger*>(static_impl);
-  if (logger.isEnabledFor(severity_)) {
-    log4cplus::detail::macro_forced_log(logger, severity_, reinterpret_cast<std::stringstream*>(implement)->str(), file_name_, line_number_, nullptr);
+  log4cplus::Logger &logger = *reinterpret_cast<log4cplus::Logger *>(static_impl);
+  int mapped_severity = SeverityMapper::GetMappedSeverity(severity_);
+  if (severity_ >= severity_threshold_) {
+    log4cplus::detail::macro_forced_log(
+        logger, mapped_severity, reinterpret_cast<std::stringstream *>(implement)->str(),
+        file_name_, line_number_, nullptr);
   }
-  delete reinterpret_cast<std::stringstream*>(implement);
+  delete reinterpret_cast<std::stringstream *>(implement);
+  // Log4cplus won't exit at fatal level.
+  if (severity_ == RAY_FATAL) {
+    std::abort();
+  }
 #else
-  delete reinterpret_cast<CerrLog*>(implement);
+  delete reinterpret_cast<CerrLog *>(implement);
 #endif
   implement = nullptr;
 }
 
-}  // namespace internal
-
 }  // namespace ray
-
--- src/ray/util/logging.h
+++ src/ray/util/logging.h
@@ -11,12 +11,6 @@
 #include "ray/util/macros.h"
 
 namespace ray {
-
-// Stubbed versions of macros defined in glog/logging.h, intended for
-// environments where glog headers aren't available.
-//
-// Add more as needed.
-
 // Log levels. LOG ignores them, so their values are abitrary.
 
 #define RAY_DEBUG (-1)
@@ -25,26 +19,21 @@ namespace ray {
 #define RAY_ERROR 2
 #define RAY_FATAL 3
 
-#define RAY_LOG_INTERNAL(level) \
-  ::ray::internal::RayLog(__FILE__, __LINE__, level)
+#define RAY_LOG_INTERNAL(level) ::ray::RayLog(__FILE__, __LINE__, level)
 
 #define RAY_LOG(level) RAY_LOG_INTERNAL(RAY_##level)
-#define RAY_IGNORE_EXPR(expr) ((void) (expr));
+#define RAY_IGNORE_EXPR(expr) ((void)(expr))
 
-#define RAY_CHECK(condition)                      \
-  (condition) ? ::ray::internal::NullLog() : ::ray::internal::RayLog( \
-      __FILE__, __LINE__, RAY_FATAL)     \
-      << " Check failed: " #condition " "
-/*#define RAY_CHECK(condition)                             \
-  (condition) ? 0 : ::ray::internal::FatalLog(RAY_FATAL) \
-                        << __FILE__ << ":" << __LINE__   \
-                        << " Check failed: " #condition " "*/
+#define RAY_CHECK(condition)                                                          \
+  (condition) ? RAY_IGNORE_EXPR(0) : ::ray::Voidify() &                               \
+                                         ::ray::RayLog(__FILE__, __LINE__, RAY_FATAL) \
+                                             << " Check failed: " #condition " "
 
 #ifdef NDEBUG
 
 #define RAY_DCHECK(condition) \
-  RAY_IGNORE_EXPR(condition)  \
-  while (false) ::ray::internal::RayLogBase()
+  RAY_IGNORE_EXPR(condition); \
+  while (false) ::ray::RayLogBase()
 
 #else
 
@@ -52,16 +41,17 @@ namespace ray {
 
 #endif  // NDEBUG
 
-namespace internal {
-
-// To make the logging lib plugable with other logging lib and make 
+// To make the logging lib plugable with other logging libs and make
 // the implementation unawared by the user, RayLog is only a declaration
 // which hide the implementation into logging.cc file.
-// In logging.cc, we can choose different log lib using different macroes.
+// In logging.cc, we can choose different log libs using different macros.
+
+// This is also a null log which does not output anything.
 class RayLogBase {
  public:
-  virtual ~RayLogBase() {};
-  template<typename T>
+  virtual ~RayLogBase(){};
+
+  template <typename T>
   RayLogBase &operator<<(const T &t) {
     RAY_IGNORE_EXPR(t);
     return *this;
@@ -72,21 +62,29 @@ class RayLog : public RayLogBase {
  public:
   RayLog(const char *file_name, int line_number, int severity);
   virtual ~RayLog();
-  template<typename T>
+
+  template <typename T>
   RayLogBase &operator<<(const T &t) {
     if (implement == nullptr) {
       RAY_IGNORE_EXPR(t);
     } else {
-      this->stream() << t;
+      this->Stream() << t;
     }
     return *this;
   }
-  static void StartRayLog(const char *appName, int severity_threshold = RAY_ERROR, const char* logDir = "/tmp/");
+
+  // The init function of ray log for a program which should be called only once.
+  static void StartRayLog(const char *appName, int severity_threshold = RAY_ERROR,
+                          const char *logDir = "/tmp");
+  // The shutdown function of ray log which should be used with StartRayLog as a pair.
   static void ShutDownRayLog();
-  static void *GetImplPointer();
-  static void SetImplPointer(void *pointer);
+
+  static void *GetStaticImplement() { return static_impl; }
+
+  static void SetStaticImplement(void *pointer) { static_impl = pointer; }
+
  private:
-  std::ostream& stream();
+  std::ostream &Stream();
   const char *file_name_;
   int line_number_;
   int severity_;
@@ -95,86 +93,14 @@ class RayLog : public RayLogBase {
   static int severity_threshold_;
 };
 
-// Clang-tidy isn't smart enough to determine that DCHECK using CerrLog doesn't
-// return so we create a new class to give it a hint.
-class RayFatalLog : public RayLog {
+// This class make RAY_CHECK compilation pass to change the << operator to void.
+class Voidify {
  public:
-  explicit RayFatalLog(const char *file_name, int line_number)  // NOLINT
-      : RayLog(file_name, line_number, RAY_FATAL) {}            // NOLINT
-  RAY_NORETURN ~RayFatalLog() {
-    std::abort();
-  }
+  Voidify() {}
+  // This has to be an operator with a precedence lower than << but
+  // higher than ?:
+  void operator&(RayLogBase &) {}
 };
-
-class NullLog : public RayLogBase {
- public:
-  NullLog() : RayLogBase() {}
-  template <class T>
-  RayLogBase &operator<<(const T &t) {
-    RAY_IGNORE_EXPR(t);
-    return *this;
-  }
-};
-
-class CerrLog {
- public:
-  CerrLog(int severity)  // NOLINT(runtime/explicit)
-      : severity_(severity),
-        has_logged_(false) {}
-
-  virtual ~CerrLog() {
-    if (has_logged_) {
-      std::cerr << std::endl;
-    }
-    if (severity_ == RAY_FATAL) {
-      PrintBackTrace();
-      std::abort();
-    }
-  }
-
-  std::ostream &stream() {
-    has_logged_ = true;
-    return std::cerr;
-  }
-
-  template <class T>
-  CerrLog &operator<<(const T &t) {
-    if (severity_ != RAY_DEBUG) {
-      has_logged_ = true;
-      std::cerr << t;
-    }
-    return *this;
-  }
-
- protected:
-  const int severity_;
-  bool has_logged_;
-  void PrintBackTrace() {
-#if defined(_EXECINFO_H) || !defined(_WIN32)
-    void *buffer[255];
-    const int calls = backtrace(buffer, sizeof(buffer) / sizeof(void *));
-    backtrace_symbols_fd(buffer, calls, 1);
-#endif
-  }
-};
-
-// Clang-tidy isn't smart enough to determine that DCHECK using CerrLog doesn't
-// return so we create a new class to give it a hint.
-class FatalLog : public CerrLog {
- public:
-  explicit FatalLog(int /* severity */)  // NOLINT
-      : CerrLog(RAY_FATAL) {}            // NOLINT
-
-  RAY_NORETURN ~FatalLog() {
-    if (has_logged_) {
-      std::cerr << std::endl;
-      PrintBackTrace();
-    }
-    std::abort();
-  }
-};
-
-}  // namespace internal
 
 }  // namespace ray
 
