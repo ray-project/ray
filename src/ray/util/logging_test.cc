256cd2eb5b2bcf0712d38df41fcc246d2c6b6e5c
Philipp Moritz <pcmoritz@gmail.com>
2018-08-23 09:29:08 -0700

    Update logging_test.cc

--- src/ray/util/logging_test.cc
+++ src/ray/util/logging_test.cc
@@ -40,7 +40,7 @@ TEST(PrintLogTest, LogTestWithInit) {
   RayLog::ShutDownRayLog();
 }
 
-// This test will output large amount of logs to stderr, should be diabled in travis.
+// This test will output large amount of logs to stderr, should be disabled in travis.
 TEST(LogPerfTest, PerfTest) {
   RayLog::StartRayLog("/fake/path/to/appdire/LogPerfTest", RAY_ERROR, "/tmp/");
   int rounds = 100000;
