3afa66f85fa2af33aa972b00d9d448cf52663b57
yiguang.wyg <yiguang.wyg@antfin.com>
2018-08-23 15:30:40 +0800

    syntax style fix.

--- python/ray/tune/automlboard/run.py
+++ python/ray/tune/automlboard/run.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
-# Author: Rujie Jiang rujie.jrj@antfin.com
-# Date: Tue May 15 11:46:20 2018
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
 import os
 import re
