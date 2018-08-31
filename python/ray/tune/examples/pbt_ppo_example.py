f87865932e1db60b60d238c7c9e8e987753b191e
Eric Liang <ekhliang@gmail.com>
2018-08-31 15:35:12 -0700

    fix

--- python/ray/tune/examples/pbt_ppo_example.py
+++ python/ray/tune/examples/pbt_ppo_example.py
@@ -21,8 +21,8 @@ if __name__ == "__main__":
     # Postprocess the perturbed config to ensure it's still valid
     def explore(config):
         # ensure we collect enough timesteps to do sgd
-        if config["timesteps_per_batch"] < config["sgd_batchsize"] * 2:
-            config["timesteps_per_batch"] = config["sgd_batchsize"] * 2
+        if config["train_batch_size"] < config["sgd_batchsize"] * 2:
+            config["train_batch_size"] = config["sgd_batchsize"] * 2
         # ensure we run at least one sgd iter
         if config["num_sgd_iter"] < 1:
             config["num_sgd_iter"] = 1
@@ -40,7 +40,7 @@ if __name__ == "__main__":
             "sgd_stepsize": [1e-3, 5e-4, 1e-4, 5e-5, 1e-5],
             "num_sgd_iter": lambda: random.randint(1, 30),
             "sgd_batchsize": lambda: random.randint(128, 16384),
-            "timesteps_per_batch": lambda: random.randint(2000, 160000),
+            "train_batch_size": lambda: random.randint(2000, 160000),
         },
         custom_explore_fn=explore)
 
@@ -67,7 +67,7 @@ if __name__ == "__main__":
                         lambda spec: random.choice([10, 20, 30]),
                     "sgd_batchsize":
                         lambda spec: random.choice([128, 512, 2048]),
-                    "timesteps_per_batch":
+                    "train_batch_size":
                         lambda spec: random.choice([10000, 20000, 40000])
                 },
             },
