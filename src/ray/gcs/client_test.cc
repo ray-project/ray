730f39ad8ecff2ecc09c589916a29fad50dbd6f3
Yucong He <yuconghe@calvisitor-10-105-135-76.calvisitor.1918.berkeley.edu>
2018-08-31 12:01:56 -0700

    final lint

--- src/ray/gcs/client_test.cc
+++ src/ray/gcs/client_test.cc
@@ -29,7 +29,7 @@ class TestGcs : public ::testing::Test {
  public:
   TestGcs(CommandType command_type) : num_callbacks_(0), command_type_(command_type) {
     client_ = std::make_shared<gcs::AsyncGcsClient>("127.0.0.1", 6379, command_type_,
-              /*is_test_client=*/true);
+                                                    /*is_test_client=*/true);
     job_id_ = JobID::from_random();
   }
 
