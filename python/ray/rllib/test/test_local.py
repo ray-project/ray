b340bb412912770b8282415bc56044fd053e8dd1
Robert Nishihara <robertnishihara@gmail.com>
2018-08-31 23:36:26 -0700

    Update test_local.py

--- python/ray/rllib/test/test_local.py
+++ python/ray/rllib/test/test_local.py
@@ -3,12 +3,14 @@ import unittest
 from ray.rllib.agents.ppo import PPOAgent, DEFAULT_CONFIG
 import ray
 
+
 class LocalModeTest(unittest.TestCase):
     def testLocal(self):
         ray.init(local_mode=True)
         cf = DEFAULT_CONFIG.copy()
         agent = PPOAgent(cf, "CartPole-v0")
         print(agent.train())
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
