antgroup/ant-ray

Add Plasma Free to java

java/api/src/main/java/org/ray/api/Ray.java
@@ -73,6 +73,16 @@ public final class Ray extends Rpc {
   }
 
   /**
+   * free a list of objects from Plasma Store.
+   *
+   * @param objectIds           object ids to free
+   * @param localOnly           only free objects for local object or not
+   */
+  public static void free(List<UniqueID> objectIds, boolean localOnly) {
+    impl.free(objectIds, localOnly);
+  }
+
+  /**
    * create actor object.
    */
   public static <T> RayActor<T> create(Class<T> cls) {

java/test/src/main/java/org/ray/api/test/PlasmaFreeTest.java
@@ -1,0 +1,50 @@
+package org.ray.api.test;
+
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ray.api.Ray;
+import org.ray.api.RayObject;
+import org.ray.api.RayRemote;
+import org.ray.api.UniqueID;
+import org.ray.api.WaitResult;
+
+
+@RunWith(MyRunner.class)
+public class PlasmaFreeTest {
+
+  @RayRemote
+  private static String hello() {
+    return "hello";
+  }
+
+  @Test
+  public void test() {
+    RayObject<String> helloId = Ray.call(PlasmaFreeTest::hello);
+    String helloString = helloId.get();
+    Assert.assertEquals("hello", helloString);
+    List<RayObject<String>> waitfor = ImmutableList.of(helloId);
+    WaitResult<String> waitResult = Ray.wait(waitfor, 1, 2 * 1000);
+    List<RayObject<String>> readyOnes = waitResult.getReadyOnes();
+    List<RayObject<String>> remainOnes = waitResult.getRemainOnes();
+    Assert.assertEquals(1, readyOnes.size());
+    Assert.assertEquals(0, remainOnes.size());
+
+    List<UniqueID> freeList = new ArrayList<>();
+    freeList.add(helloId.getId());
+    Ray.free(freeList, true);
+    // Flush: trigger the release function because Plasma Client has cache.
+    for (int i = 0; i < 128; i++) {
+      Ray.call(PlasmaFreeTest::hello).get();
+    }
+
+    waitResult = Ray.wait(waitfor, 1, 2 * 1000);
+    readyOnes = waitResult.getReadyOnes();
+    remainOnes = waitResult.getRemainOnes();
+    Assert.assertEquals(0, readyOnes.size());
+    Assert.assertEquals(1, remainOnes.size());
+  }
+}

ray-project/ray

java/api/src/main/java/org/ray/api/Ray.java
@@ -116,6 +116,16 @@ public final class Ray extends RayCall {
   }
 
   /**
+   * Free a list of objects from Plasma Store.
+   *
+   * @param objectIds The object ids to free.
+   * @param localOnly Whether only free objects for local object or not.
+   */
+  public static void free(List<UniqueId> objectIds, boolean localOnly) {
+    runtime.free(objectIds, localOnly);
+  }
+
+  /**
    * Get the underlying runtime instance.
    */
   static RayRuntime internal() {

java/test/src/main/java/org/ray/api/test/PlasmaFreeTest.java
@@ -1,0 +1,50 @@
+package org.ray.api.test;
+
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ray.api.Ray;
+import org.ray.api.RayObject;
+import org.ray.api.WaitResult;
+import org.ray.api.annotation.RayRemote;
+import org.ray.api.id.UniqueId;
+
+
+@RunWith(MyRunner.class)
+public class PlasmaFreeTest {
+
+  @RayRemote
+  private static String hello() {
+    return "hello";
+  }
+
+  @Test
+  public void test() {
+    RayObject<String> helloId = Ray.call(PlasmaFreeTest::hello);
+    String helloString = helloId.get();
+    Assert.assertEquals("hello", helloString);
+    List<RayObject<String>> waitfor = ImmutableList.of(helloId);
+    WaitResult<String> waitResult = Ray.wait(waitfor, 1, 2 * 1000);
+    List<RayObject<String>> readyOnes = waitResult.getReady();
+    List<RayObject<String>> remainOnes = waitResult.getUnready();
+    Assert.assertEquals(1, readyOnes.size());
+    Assert.assertEquals(0, remainOnes.size());
+
+    List<UniqueId> freeList = new ArrayList<>();
+    freeList.add(helloId.getId());
+    Ray.free(freeList, true);
+    // Flush: trigger the release function because Plasma Client has cache.
+    for (int i = 0; i < 128; i++) {
+      Ray.call(PlasmaFreeTest::hello).get();
+    }
+
+    waitResult = Ray.wait(waitfor, 1, 2 * 1000);
+    readyOnes = waitResult.getReady();
+    remainOnes = waitResult.getUnready();
+    Assert.assertEquals(0, readyOnes.size());
+    Assert.assertEquals(1, remainOnes.size());
+  }
+}