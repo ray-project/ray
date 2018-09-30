1e76e8a5c5588c7ec7658ebb9ca83b10d0a3ea81
Robert Nishihara <robertnishihara@gmail.com>
2018-09-30 17:37:51 -0500

    Update arrow with plasma store fix.

--- cmake/Modules/ArrowExternalProject.cmake
+++ cmake/Modules/ArrowExternalProject.cmake
@@ -14,10 +14,10 @@
 #  - PLASMA_SHARED_LIB
 
 set(arrow_URL https://github.com/apache/arrow.git)
-# The PR for this commit is https://github.com/apache/arrow/pull/2650. We
+# The PR for this commit is https://github.com/apache/arrow/pull/2664. We
 # include the link here to make it easier to find the right commit because
 # Arrow often rewrites git history and invalidates certain commits.
-set(arrow_TAG f981a114f3e3b7d886a8d37bc7d5ab0d0c1bfb69)
+set(arrow_TAG 3545186d6997b943ffc3d79634f2d08eefbd7322)
 
 set(ARROW_INSTALL_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/arrow-install)
 set(ARROW_HOME ${ARROW_INSTALL_PREFIX})
