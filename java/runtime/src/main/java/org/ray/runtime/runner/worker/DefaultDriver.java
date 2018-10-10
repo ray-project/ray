93944808738684b033e4a1f11efbc109fcaab990
kingchin1218 <kingchin1218@126.com>
2018-10-10 20:20:29 +0800

    Change the command line.

--- java/runtime/src/main/java/org/ray/runtime/runner/worker/DefaultDriver.java
+++ java/runtime/src/main/java/org/ray/runtime/runner/worker/DefaultDriver.java
@@ -8,12 +8,12 @@ public class DefaultDriver {
   /**
    * Command line:
    *
-   * <p>java -ea org.ray.runtime.runner.worker driverClassName [driverArg1, ...]
+   * <p>java -ea org.ray.runtime.runner.worker driverClassName [driverArg1,driverArgs2,...]
    *
    * <p>1. If you want to set `driver.id` to this driver, you should set it in system property,
    * like this:
    *       java -ea -Dray.driver.id=0123456789ABCDEF0123
-   *             org.ray.runtime.runner.worker driverClassName [driverArg1, ...]
+   *             org.ray.runtime.runner.worker driverClassName [driverArg1,driverArgs2,...]
    *
    * <p>2. DefaultDriver also needs `ray.conf` in classpath. You must specify
    *        driver jar package in classpath that DefaultDriver can load it to execute.
@@ -27,21 +27,17 @@ public class DefaultDriver {
     }
 
     String driverClassName = args[0];
-    final int driverArgsLength = args.length - 1;
-
-    String[] driverArgs;
-    if (driverArgsLength > 0) {
-      driverArgs = new String[args.length - 1];
-      System.arraycopy(args, 1, driverArgs, 0, args.length - 1);
-    } else {
-      driverArgs = new String [] {};
+    String driverArgs = null;
+    if (args.length > 1) {
+      driverArgs = args[1];
     }
+    String[] argsArray = (driverArgs != null) ? driverArgs.split(",") : (new String[] {});
 
     try {
       System.setProperty("ray.worker.mode", "DRIVER");
 
       Class<?> cls = Class.forName(driverClassName);
-      cls.getMethod("main", String[].class).invoke(null, (Object) driverArgs);
+      cls.getMethod("main", String[].class).invoke(null, (Object) argsArray);
     } catch (Throwable e) {
       e.printStackTrace();
       System.exit(-1);
