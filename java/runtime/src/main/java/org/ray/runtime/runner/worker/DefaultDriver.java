016efcb8d1bbd7023ed76626023868139e898150
kingchin1218 <kingchin1218@126.com>
2018-10-11 11:34:54 +0800

    Add init back.

--- java/runtime/src/main/java/org/ray/runtime/runner/worker/DefaultDriver.java
+++ java/runtime/src/main/java/org/ray/runtime/runner/worker/DefaultDriver.java
@@ -1,5 +1,7 @@
 package org.ray.runtime.runner.worker;
 
+import org.ray.api.Ray;
+
 /**
  * The main function of DefaultDriver.
  */
@@ -8,12 +10,12 @@ public class DefaultDriver {
   /**
    * Command line:
    *
-   * <p>java -ea org.ray.runtime.runner.worker driverClassName [driverArg1,driverArgs2,...]
+   * <p>java org.ray.runtime.runner.worker driverClassName [driverArg1, ...]
    *
    * <p>1. If you want to set `driver.id` to this driver, you should set it in system property,
    * like this:
-   *       java -ea -Dray.driver.id=0123456789ABCDEF0123
-   *             org.ray.runtime.runner.worker driverClassName [driverArg1,driverArgs2,...]
+   *       java -Dray.driver.id=0123456789ABCDEF0123
+   *             org.ray.runtime.runner.worker.DefaultDriver driverClassName [driverArg1, ...]
    *
    * <p>2. DefaultDriver also needs `ray.conf` in classpath. You must specify
    *        driver jar package in classpath that DefaultDriver can load it to execute.
@@ -27,20 +29,25 @@ public class DefaultDriver {
     }
 
     String driverClassName = args[0];
-    String driverArgs = null;
-    if (args.length > 1) {
-      driverArgs = args[1];
+    final int driverArgsLength = args.length - 1;
+
+    String[] driverArgs;
+    if (driverArgsLength > 0) {
+      driverArgs = new String[driverArgsLength];
+      System.arraycopy(args, 1, driverArgs, 0, driverArgsLength);
+    } else {
+      driverArgs = new String[] {};
     }
-    String[] argsArray = (driverArgs != null) ? driverArgs.split(",") : (new String[] {});
 
     try {
       System.setProperty("ray.worker.mode", "DRIVER");
-
+      Ray.init();
       Class<?> cls = Class.forName(driverClassName);
-      cls.getMethod("main", String[].class).invoke(null, (Object) argsArray);
-    } catch (Throwable e) {
-      e.printStackTrace();
-      System.exit(-1);
+      cls.getMethod("main", String[].class).invoke(null, (Object) driverArgs);
+    } catch (Throwable t) {
+      t.printStackTrace();
+    } finally {
+      Ray.shutdown();
     }
   }
 }
