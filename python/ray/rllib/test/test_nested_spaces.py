59ede85ec1fd46bf486681765cf85430136d2638
Eric Liang <ekhliang@gmail.com>
2018-10-12 01:14:30 -0700

    test

--- python/ray/rllib/test/test_nested_spaces.py
+++ python/ray/rllib/test/test_nested_spaces.py
@@ -17,7 +17,7 @@ from ray.rllib.models import ModelCatalog
 from ray.rllib.models.model import Model
 from ray.tune.registry import register_env
 
-NESTED_SPACE = spaces.Dict({
+DICT_SPACE = spaces.Dict({
     "sensors": spaces.Dict({
         "position": spaces.Box(low=-100, high=100, shape=(3, )),
         "velocity": spaces.Box(low=-1, high=1, shape=(3, )),
@@ -35,23 +35,54 @@ NESTED_SPACE = spaces.Dict({
     })
 })
 
-SAMPLES = [NESTED_SPACE.sample() for _ in range(10)]
+DICT_SAMPLES = [DICT_SPACE.sample() for _ in range(10)]
+
+TUPLE_SPACE = spaces.Tuple([
+    spaces.Box(low=-100, high=100, shape=(3, )),
+    spaces.Tuple((spaces.Box(low=0, high=1, shape=(10, 10, 3)),
+                  spaces.Box(low=0, high=1, shape=(10, 10, 3)))),
+    spaces.Discrete(5),
+])
+
+TUPLE_SAMPLES = [TUPLE_SPACE.sample() for _ in range(10)]
 
 
-class NestedEnv(gym.Env):
+def one_hot(i, n):
+    out = [0.0] * n
+    out[i] = 1.0
+    return out
+
+
+class NestedDictEnv(gym.Env):
     def __init__(self):
         self.action_space = spaces.Discrete(2)
-        self.observation_space = NESTED_SPACE
-        self._spec = EnvSpec("NestedEnv-v0")
+        self.observation_space = DICT_SPACE
+        self._spec = EnvSpec("NestedDictEnv-v0")
         self.steps = 0
 
     def reset(self):
         self.steps = 0
-        return SAMPLES[0]
+        return DICT_SAMPLES[0]
 
     def step(self, action):
         self.steps += 1
-        return SAMPLES[self.steps], 1, self.steps >= 5, {}
+        return DICT_SAMPLES[self.steps], 1, self.steps >= 5, {}
+
+
+class NestedTupleEnv(gym.Env):
+    def __init__(self):
+        self.action_space = spaces.Discrete(2)
+        self.observation_space = TUPLE_SPACE
+        self._spec = EnvSpec("NestedTupleEnv-v0")
+        self.steps = 0
+
+    def reset(self):
+        self.steps = 0
+        return TUPLE_SAMPLES[0]
+
+    def step(self, action):
+        self.steps += 1
+        return TUPLE_SAMPLES[self.steps], 1, self.steps >= 5, {}
 
 
 class InvalidModel(Model):
@@ -59,15 +90,15 @@ class InvalidModel(Model):
         return "not", "valid"
 
 
-class SpyModel(Model):
+class DictSpyModel(Model):
     capture_index = 0
 
     def _build_layers_v2(self, input_dict, num_outputs, options):
         def spy(pos, front_cam, task):
             ray.experimental.internal_kv._internal_kv_put(
-                "spy_in_{}".format(SpyModel.capture_index),
+                "d_spy_in_{}".format(DictSpyModel.capture_index),
                 pickle.dumps((pos, front_cam, task)))
-            SpyModel.capture_index += 1
+            DictSpyModel.capture_index += 1
             return 0
 
         spy_fn = tf.py_func(
@@ -85,6 +116,31 @@ class SpyModel(Model):
         return output, output
 
 
+class TupleSpyModel(Model):
+    capture_index = 0
+
+    def _build_layers_v2(self, input_dict, num_outputs, options):
+        def spy(pos, cam, task):
+            ray.experimental.internal_kv._internal_kv_put(
+                "t_spy_in_{}".format(TupleSpyModel.capture_index),
+                pickle.dumps((pos, cam, task)))
+            TupleSpyModel.capture_index += 1
+            return 0
+
+        spy_fn = tf.py_func(
+            spy, [
+                input_dict["obs"][0],
+                input_dict["obs"][1][0],
+                input_dict["obs"][2],
+            ],
+            tf.int64,
+            stateful=True)
+
+        with tf.control_dependencies([spy_fn]):
+            output = slim.fully_connected(input_dict["obs"][0], num_outputs)
+        return output, output
+
+
 class NestedSpacesTest(unittest.TestCase):
     def testInvalidModel(self):
         ModelCatalog.register_custom_model("invalid", InvalidModel)
@@ -95,9 +151,9 @@ class NestedSpacesTest(unittest.TestCase):
                 },
             }))
 
-    def testNested(self):
-        ModelCatalog.register_custom_model("composite", SpyModel)
-        register_env("nested", lambda _: NestedEnv())
+    def testNestedDict(self):
+        ModelCatalog.register_custom_model("composite", DictSpyModel)
+        register_env("nested", lambda _: NestedDictEnv())
         pg = PGAgent(
             env="nested",
             config={
@@ -109,22 +165,48 @@ class NestedSpacesTest(unittest.TestCase):
             })
         pg.train()
         batch = pickle.loads(
-            ray.experimental.internal_kv._internal_kv_get("spy_in_5"))
-
-        def one_hot(i, n):
-            out = [0.0] * n
-            out[i] = 1.0
-            return out
+            ray.experimental.internal_kv._internal_kv_get("d_spy_in_5"))
 
         # Check that the model sees the correct reconstructed observations
         for i in range(4):
             seen = pickle.loads(
                 ray.experimental.internal_kv._internal_kv_get(
-                    "spy_in_{}".format(i)))
-            pos_i = SAMPLES[i]["sensors"]["position"].tolist()
-            cam_i = SAMPLES[i]["sensors"]["front_cam"][0].tolist()
-            task_i = one_hot(SAMPLES[i]["inner_state"]["job_status"]["task"],
-                             5)
+                    "d_spy_in_{}".format(i)))
+            pos_i = DICT_SAMPLES[i]["sensors"]["position"].tolist()
+            cam_i = DICT_SAMPLES[i]["sensors"]["front_cam"][0].tolist()
+            task_i = one_hot(
+                DICT_SAMPLES[i]["inner_state"]["job_status"]["task"], 5)
+            self.assertEqual(seen[0][0].tolist(), pos_i)
+            self.assertEqual(seen[1][0].tolist(), cam_i)
+            self.assertEqual(seen[2][0].tolist(), task_i)
+            self.assertEqual(batch[0][i].tolist(), pos_i)
+            self.assertEqual(batch[1][i].tolist(), cam_i)
+            self.assertEqual(batch[2][i].tolist(), task_i)
+
+    def testNestedTuple(self):
+        ModelCatalog.register_custom_model("composite2", TupleSpyModel)
+        register_env("nested2", lambda _: NestedTupleEnv())
+        pg = PGAgent(
+            env="nested2",
+            config={
+                "num_workers": 0,
+                "sample_batch_size": 5,
+                "model": {
+                    "custom_model": "composite2",
+                },
+            })
+        pg.train()
+        batch = pickle.loads(
+            ray.experimental.internal_kv._internal_kv_get("t_spy_in_5"))
+
+        # Check that the model sees the correct reconstructed observations
+        for i in range(4):
+            seen = pickle.loads(
+                ray.experimental.internal_kv._internal_kv_get(
+                    "t_spy_in_{}".format(i)))
+            pos_i = TUPLE_SAMPLES[i][0].tolist()
+            cam_i = TUPLE_SAMPLES[i][1][0].tolist()
+            task_i = one_hot(TUPLE_SAMPLES[i][2], 5)
             self.assertEqual(seen[0][0].tolist(), pos_i)
             self.assertEqual(seen[1][0].tolist(), cam_i)
             self.assertEqual(seen[2][0].tolist(), task_i)
