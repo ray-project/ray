2eda03ecda3fce3f8b0fe7bc7758ecf6d0b0efaa
Eric Liang <ekhliang@gmail.com>
2018-10-21 17:15:17 -0700

    fix value error

--- python/ray/rllib/agents/agent.py
+++ python/ray/rllib/agents/agent.py
@@ -209,13 +209,12 @@ class Agent(Trainable):
 
         # Agents allow env ids to be passed directly to the constructor.
         self._env_id = env or config.get("env")
-        if not self._env_id:
-            raise ValueError("Must specify env (str) when creating agent")
 
         # Create a default logger creator if no logger_creator is specified
         if logger_creator is None:
             timestr = datetime.today().strftime("%Y-%m-%d_%H-%M-%S")
-            logdir_prefix = '_'.join([self._agent_name, self._env_id, timestr])
+            logdir_prefix = "{}_{}_{}".format(
+                [self._agent_name, self._env_id, timestr])
 
             def default_logger_creator(config):
                 """Creates a Unified logger with a default logdir prefix
