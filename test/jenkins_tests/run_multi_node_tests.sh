fc347de3b6c3db00c2121b3f499ee2af2d4caffc
Eric Liang <ekhliang@gmail.com>
2018-10-24 10:42:51 -0700

    f

--- test/jenkins_tests/run_multi_node_tests.sh
+++ test/jenkins_tests/run_multi_node_tests.sh
@@ -323,21 +323,23 @@ docker run --rm --shm-size=10G --memory=10G $DOCKER_SHA \
 docker run --rm --shm-size=10G --memory=10G $DOCKER_SHA \
     python /ray/python/ray/experimental/sgd/test_sgd.py --num-iters=2
 
+for i in `seq 1 10`; do
 # No Xray for PyTorch
-docker run -e RAY_USE_XRAY=0 --rm --shm-size=10G --memory=10G $DOCKER_SHA \
-    python /ray/python/ray/rllib/train.py \
-    --env PongDeterministic-v4 \
-    --run A3C \
-    --stop '{"training_iteration": 2}' \
-    --config '{"num_workers": 2, "use_pytorch": true, "model": {"use_lstm": false, "grayscale": true, "zero_mean": false, "dim": 84, "channel_major": true}, "preprocessor_pref": "rllib"}'
+    docker run --rm --shm-size=10G --memory=10G $DOCKER_SHA \
+        python /ray/python/ray/rllib/train.py \
+        --env PongDeterministic-v4 \
+        --run A3C \
+        --stop '{"training_iteration": 2}' \
+        --config '{"num_workers": 2, "use_pytorch": true, "model": {"use_lstm": false, "grayscale": true, "zero_mean": false, "dim": 84, "channel_major": true}, "preprocessor_pref": "rllib"}'
 
 # No Xray for PyTorch
-docker run -e RAY_USE_XRAY=0 --rm --shm-size=10G --memory=10G $DOCKER_SHA \
-    python /ray/python/ray/rllib/train.py \
-    --env CartPole-v1 \
-    --run A3C \
-    --stop '{"training_iteration": 2}' \
-    --config '{"num_workers": 2, "use_pytorch": true}'
+    docker run --rm --shm-size=10G --memory=10G $DOCKER_SHA \
+        python /ray/python/ray/rllib/train.py \
+        --env CartPole-v1 \
+        --run A3C \
+        --stop '{"training_iteration": 2}' \
+        --config '{"num_workers": 2, "use_pytorch": true}'
+done
 
 python3 $ROOT_DIR/multi_node_docker_test.py \
     --docker-image=$DOCKER_SHA \
