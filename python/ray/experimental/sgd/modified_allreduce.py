b11ff928b601250cb28c7894a7dcc44991694c8b
Philipp Moritz <pcmoritz@gmail.com>
2018-10-25 17:23:50 -0700

    fix lint

--- python/ray/experimental/sgd/modified_allreduce.py
+++ python/ray/experimental/sgd/modified_allreduce.py
@@ -584,7 +584,8 @@ def pack_small_tensors(tower_grads, max_bytes=0):
     if len(small_ranges):
         new_tower_grads = []
         for dev_idx, gv_list in enumerate(tower_grads):
-            assert len(gv_list) == num_gv, ("Possible cause: "
+            assert len(gv_list) == num_gv, (
+                "Possible cause: "
                 "Networks constructed on different workers "
                 "don't have the same number of variables. "
                 "If you use tf.GraphKeys or tf.global_variables() "
