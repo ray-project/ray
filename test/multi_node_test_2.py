f123c25bd64a2185c358f2fb6ba3b82ac60f7f5d
Hao Chen <chenh1024@gmail.com>
2018-11-05 20:59:12 +0800

    fix

--- test/multi_node_test_2.py
+++ test/multi_node_test_2.py
@@ -87,12 +87,14 @@ def test_actor_reconstruction(start_connected_cluster):
     actor = MyActor.remote()
 
     def kill_node():
-        # Kill the node that the actor resides on.
+        # Kill the node that the actor reside on.
         # Return node's object store socket name.
         object_store_socket = ray.get(actor.get_object_store_socket.remote())
+        node_to_remove = None
         for node in cluster.worker_nodes:
             if object_store_socket == node.get_plasma_store_name():
-                cluster.remove_node(node)
+                node_to_remove = node
+        cluster.remove_node(node_to_remove)
         return object_store_socket
 
     # Call increase 3 times.
