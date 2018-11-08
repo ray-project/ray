7f675f77e70faab268bd8ccaca1d41653aa3b374
Richard Liaw <rliaw@berkeley.edu>
2018-11-07 22:48:39 -0800

    fix up tests

--- python/ray/tune/test/cluster_tests.py
+++ python/ray/tune/test/cluster_tests.py
@@ -32,7 +32,6 @@ def start_connected_cluster():
             "_internal_config": json.dumps(
                 {"num_heartbeats_timeout": 10})})
     yield cluster
-    os.unlink(path)
     # The code after the yield will run as teardown code.
     ray.shutdown()
     cluster.shutdown()
@@ -53,13 +52,16 @@ class _Train(tune.Trainable):
         self.state = state
 
 
+@pytest.mark.skipif(pytest_timeout==None, reason="Timeout package"\
+    " not installed; skipping test that may hang.")
+@pytest.mark.timeout(10, method="thread")
 def test_counting_resources(start_connected_cluster):
     """Tests that Tune accounting is consistent with actual cluster."""
     cluster = start_connected_cluster
     assert ray.global_state.cluster_resources()["CPU"] == 1
     nodes = []
     nodes += [cluster.add_node(resources=dict(CPU=1))]
-    cluster.wait_for_nodes()
+    assert cluster.wait_for_nodes()
     assert ray.global_state.cluster_resources()["CPU"] == 2
 
 
@@ -77,13 +79,13 @@ def test_counting_resources(start_connected_cluster):
 
     runner.step()  # run 1
     cluster.remove_node(nodes.pop())
-    cluster.wait_for_nodes()
+    assert cluster.wait_for_nodes()
     assert ray.global_state.cluster_resources()["CPU"] == 1
     runner.step()  # run 2
 
     for i in range(5):
         nodes += [cluster.add_node(resources=dict(CPU=1))]
-    cluster.wait_for_nodes()
+    assert cluster.wait_for_nodes()
     assert ray.global_state.cluster_resources()["CPU"] == 6
 
     runner.step()  # 1 result
@@ -91,48 +93,49 @@ def test_counting_resources(start_connected_cluster):
     for i in range(5):
         node = nodes.pop()
         cluster.remove_node(node)
-    cluster.wait_for_nodes()
+    assert cluster.wait_for_nodes()
     assert ray.global_state.cluster_resources()["CPU"] == 1
 
 
+# Waiting for actor reconstruction PR
+# @pytest.mark.skipif(pytest_timeout==None, reason="Timeout package"\
+#     " not installed; skipping test that may hang.")
 # @pytest.mark.timeout(10, method="thread")
-@pytest.mark.skipif(pytest_timeout==None, reason="Timeout package"\
-    " not installed; skipping test that may hang.")
-def test_remove_node_before_result(start_connected_cluster):
-    """Removing a node should cause a Trial to be requeued."""
-    cluster = start_connected_cluster
-    node = cluster.add_node(resources=dict(CPU=1))
+# def test_remove_node_before_result(start_connected_cluster):
+#     """Removing a node should cause a Trial to be requeued."""
+#     cluster = start_connected_cluster
+#     node = cluster.add_node(resources=dict(CPU=1))
 
-    runner = TrialRunner(BasicVariantGenerator())
-    kwargs = {
-        "stopping_criterion": {
-            "training_iteration": 3
-        }
-    }
+#     runner = TrialRunner(BasicVariantGenerator())
+#     kwargs = {
+#         "stopping_criterion": {
+#             "training_iteration": 3
+#         }
+#     }
 
-    tune.register_trainable("test", _Train)
-    trials = [Trial("test", **kwargs), Trial("test", **kwargs)]
-    for t in trials:
-        runner.add_trial(t)
+#     tune.register_trainable("test", _Train)
+#     trials = [Trial("test", **kwargs), Trial("test", **kwargs)]
+#     for t in trials:
+#         runner.add_trial(t)
 
-    runner.step()  # run 1
-    runner.step()  # run 2
-    assert all(t.status == Trial.RUNNING for t in trials)
+#     runner.step()  # run 1
+#     runner.step()  # run 2
+#     assert all(t.status == Trial.RUNNING for t in trials)
 
-    runner.step()  # 1 result
-    print(runner.debug_string())
+#     runner.step()  # 1 result
+#     print(runner.debug_string())
 
-    cluster.remove_node(node)
-    cluster.wait_for_nodes()
-    assert ray.global_state.cluster_resources["CPU"] == 1
+#     cluster.remove_node(node)
+#     cluster.wait_for_nodes()
+#     assert ray.global_state.cluster_resources["CPU"] == 1
 
-    runner.step()  # recover
-    for i in range(5):
-        runner.step()
-    assert all(t.status == Trial.TERMINATED for t in trials)
+#     runner.step()  # recover
+#     for i in range(5):
+#         runner.step()
+#     assert all(t.status == Trial.TERMINATED for t in trials)
 
-    with pytest.raises(TuneError):
-        runner.step()
+#     with pytest.raises(TuneError):
+#         runner.step()
 
 
 def test_trial_migration(start_connected_cluster):
