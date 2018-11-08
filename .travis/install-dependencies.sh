8f761229d2f46586e10cb32963dbfa465f14c7ce
Philipp Moritz <pcmoritz@gmail.com>
2018-11-08 14:37:49 -0800

    llvm toolchain requirements

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -24,6 +24,8 @@ if [[ "$PYTHON" == "2.7" ]] && [[ "$platform" == "linux" ]]; then
   wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
   sudo apt-add-repository -y \
        "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-6.0 main"
+  sudo apt-add-repository -y \
+       "deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu trusty main"
   sudo apt-get update -qq
   sudo apt-get install -q clang-6.0
   # Install miniconda.
@@ -39,6 +41,8 @@ elif [[ "$PYTHON" == "3.5" ]] && [[ "$platform" == "linux" ]]; then
   wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
   sudo apt-add-repository -y \
        "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-6.0 main"
+  sudo apt-add-repository -y \
+       "deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu trusty main"
   sudo apt-get update -qq
   sudo apt-get install -q clang-6.0
   # Install miniconda.
