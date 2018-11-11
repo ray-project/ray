8941a8fb24e98951e195668579cd069c1cab7b35
Eric Liang <ekhliang@gmail.com>
2018-11-10 23:20:13 -0800

    add atexit test

--- python/ray/rllib/test/test_env_with_subprocess.py
+++ python/ray/rllib/test/test_env_with_subprocess.py
@@ -5,8 +5,11 @@ from __future__ import division
 from __future__ import print_function
 
 from gym.spaces import Discrete
+import atexit
 import gym
+import os
 import subprocess
+import tempfile
 import time
 
 import ray
@@ -15,15 +18,23 @@ from ray.tune.registry import register_env
 
 # Dummy command to run as a subprocess with a unique name
 UNIQUE_CMD = "sleep {}".format(str(time.time()))
+_, UNIQUE_FILE_0 = tempfile.mkstemp("test_env_with_subprocess")
+_, UNIQUE_FILE_1 = tempfile.mkstemp("test_env_with_subprocess")
 
 
 class EnvWithSubprocess(gym.Env):
     """Our env that spawns a subprocess."""
 
-    def __init__(self):
+    def __init__(self, config):
         self.action_space = Discrete(2)
         self.observation_space = Discrete(2)
+        # Subprocess that should be cleaned up
         self.subproc = subprocess.Popen(UNIQUE_CMD, shell=True)
+        # Exit handler should be called
+        if config.worker_index == 0:
+            atexit.register(lambda: os.unlink(UNIQUE_FILE_0))
+        else:
+            atexit.register(lambda: os.unlink(UNIQUE_FILE_1))
 
     def reset(self):
         return [0]
@@ -41,15 +52,19 @@ def leaked_processes():
 
 
 if __name__ == "__main__":
-    register_env("subproc", lambda config: EnvWithSubprocess())
+    register_env("subproc", lambda config: EnvWithSubprocess(config))
     ray.init()
-    assert not leaked_processes(), \
-        "Some leaked processes from before, try running 'killall sleep'?"
+    assert os.path.exists(UNIQUE_FILE_0)
+    assert os.path.exists(UNIQUE_FILE_1)
+    assert not leaked_processes()
     run_experiments({
         "demo": {
             "run": "PG",
             "env": "subproc",
             "num_samples": 1,
+            "config": {
+                "num_workers": 1,
+            },
             "stop": {
                 "training_iteration": 1
             },
@@ -57,4 +72,6 @@ if __name__ == "__main__":
     })
     leaked = leaked_processes()
     assert not leaked, "LEAKED PROCESSES: {}".format(leaked)
+    assert not os.path.exists(UNIQUE_FILE_0), "atexit handler not called"
+    assert not os.path.exists(UNIQUE_FILE_1), "atexit handler not called"
     print("OK")
