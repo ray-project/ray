ccf9c4f09a75cf543e96859478ff81235f3d4e37
Philipp Moritz <pcmoritz@gmail.com>
2018-11-12 21:56:20 -0800

    Update services.py

--- python/ray/services.py
+++ python/ray/services.py
@@ -975,7 +975,8 @@ def start_raylet(redis_address,
         modified_env = os.environ.copy()
         modified_env["LD_PRELOAD"] = os.environ["RAYLET_PERFTOOLS_PATH"]
         modified_env["CPUPROFILE"] = os.environ["RAYLET_PERFTOOLS_LOGFILE"]
-        pid = subprocess.Popen(command, stdout=stdout_file, stderr=stderr_file, env=modified_env)
+        pid = subprocess.Popen(
+            command, stdout=stdout_file, stderr=stderr_file, env=modified_env)
     else:
         pid = subprocess.Popen(command, stdout=stdout_file, stderr=stderr_file)
 
