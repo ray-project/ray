8996c4b281f374c6a39f254d4f796b49eecbc5e6
Philipp Moritz <pcmoritz@gmail.com>
2018-11-15 15:09:39 -0800

    Fix redis crash when duplicate messages are appended to log. (#3316)

--- src/ray/gcs/redis_module/ray_redis_module.cc
+++ src/ray/gcs/redis_module/ray_redis_module.cc
@@ -311,7 +311,23 @@ int TableAppend_DoWrite(RedisModuleCtx *ctx, RedisModuleString **argv, int argc,
     // must be unique, or else we will have gaps in the log.
     // TODO(rkn): We need to get rid of this uniqueness requirement. We can
     // easily have multiple log events with the same message.
-    RAY_CHECK(flags == REDISMODULE_ZADD_ADDED) << "Appended a duplicate entry";
+    if (flags != REDISMODULE_ZADD_ADDED) {
+      // The following code is a workaround to store the data at a new unique
+      // key. This is so redis doesn't crash (we currently have duplicate keys
+      // for error conditions, which get delivered via pubsub).
+      size_t len;
+      const char *id_str = RedisModule_StringPtrLen(id, &len);
+      RAY_LOG(INFO) << "Duplicate key: " << std::string(id_str, len);
+      // Store the value into a unique new key, just to keep track of it and
+      // make sure the log size grows.
+      std::string postfix = std::to_string(index);
+      RedisModuleString *new_id =
+          RedisString_Format(ctx, "%S:%b", id, postfix.data(), postfix.size());
+      RedisModuleKey *new_key = OpenPrefixedKey(
+          ctx, prefix_str, new_id, REDISMODULE_READ | REDISMODULE_WRITE, mutated_key_str);
+      RedisModule_ZsetAdd(new_key, index, data, &flags);
+      RAY_CHECK(flags == REDISMODULE_ZADD_ADDED);
+    }
     return REDISMODULE_OK;
   } else {
     // The requested index did not match the current length of the log. Return
