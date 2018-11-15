3c457ccdfbbbf96f12112726bd7d180b324a4520
Philipp Moritz <pcmoritz@gmail.com>
2018-11-15 12:04:10 -0800

    Update runtest.py

--- test/runtest.py
+++ test/runtest.py
@@ -2406,22 +2406,23 @@ def test_duplicate_error_messages(shutdown_only):
     ray.init(num_cpus=0)
 
     driver_id = ray.ray_constants.NIL_JOB_ID.id()
-    error_data = ray.gcs_utils.construct_error_message(driver_id, "test", "message", 0)
+    error_data = ray.gcs_utils.construct_error_message(driver_id,
+                                                       "test", "message", 0)
 
     # Push the same message to the GCS twice (they are the same because we
     # do not include a timestamp).
 
     r = ray.worker.global_worker.redis_client
 
+    r.execute_command("RAY.TABLE_APPEND", ray.gcs_utils.TablePrefix.ERROR_INFO,
+                      ray.gcs_utils.TablePubsub.ERROR_INFO, driver_id,
+                      error_data)
+
+    # Before https://github.com/ray-project/ray/pull/3316 this would
+    # give an error
     r.execute_command(
         "RAY.TABLE_APPEND", ray.gcs_utils.TablePrefix.ERROR_INFO,
         ray.gcs_utils.TablePubsub.ERROR_INFO, driver_id, error_data)
-
-    ok = r.execute_command(
-        "RAY.TABLE_APPEND", ray.gcs_utils.TablePrefix.ERROR_INFO,
-        ray.gcs_utils.TablePubsub.ERROR_INFO, driver_id, error_data)
-
-    assert ok == b'OK'
 
 
 @pytest.mark.skipif(
