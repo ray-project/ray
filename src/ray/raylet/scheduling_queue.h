ea75ad4a34162d22ae01786915fa7f2620acc987
Philipp Moritz <pcmoritz@gmail.com>
2018-11-16 12:50:56 -0800

    Update scheduling_queue.h

--- src/ray/raylet/scheduling_queue.h
+++ src/ray/raylet/scheduling_queue.h
@@ -318,13 +318,6 @@ class SchedulingQueue {
   /// \return Aggregate resource demand from ready tasks.
   ResourceSet GetReadyQueueResources() const;
 
-  /// Return a human-readable string indicating the number of tasks in each
-  /// queue.
-  ///
-  /// \return A string that can be used to display the contents of the queues
-  /// for debugging purposes.
-  const std::string ToString() const;
-
   /// Returns debug string for class.
   ///
   /// \return string.
