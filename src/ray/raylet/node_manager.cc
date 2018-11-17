6ee500941e2cf143e86770d7cbc13cfe91c25bb3
Robert Nishihara <robertnishihara@gmail.com>
2018-11-16 23:02:45 -0800

    Suppress duplicate pre-emptive object pushes. (#3276)
    * Suppress duplicate pre-emptive object pushes.
    
    * Add test.
    
    * Fix linting
    
    * Remove timer and inline recent_pushes_ into local_objects_.
    
    * Improve test.
    
    * Fix
    
    * Fix linting
    
    * Enable retrying pull from same object manager. Randomize object manager.
    
    * Speed up test
    
    * Linting
    
    * Add test.
    
    * Minor
    
    * Lengthen pull timeout and reissue pull every time a new object becomes available.
    
    * Increase pull timeout in test.
    
    * Wait for nodes to start in object manager test.
    
    * Wait longer for nodes to start up in test.
    
    * Small fixes.
    
    * _submit -> _remote
    
    * Change assert to warning.

--- src/ray/raylet/node_manager.cc
+++ src/ray/raylet/node_manager.cc
@@ -763,7 +763,7 @@ void NodeManager::ProcessDisconnectClientMessage(
     DispatchTasks(local_queues_.GetReadyTasks());
   } else if (is_driver) {
     // The client is a driver.
-    RAY_CHECK_OK(gcs_client_->driver_table().AppendDriverData(client->GetClientID(),
+    RAY_CHECK_OK(gcs_client_->driver_table().AppendDriverData(client->GetClientId(),
                                                               /*is_dead=*/true));
     auto driver_id = worker->GetAssignedTaskId();
     RAY_CHECK(!driver_id.is_nil());
