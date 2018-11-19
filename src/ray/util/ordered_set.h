24be13a150d65c507187d30deff79a0d913632b3
Robert Nishihara <rnishihara@calvisitor-10-105-137-27.calvisitor.1918.berkeley.edu>
2018-11-19 12:05:50 -0800

    Add iterators to ordered_set container implementation.

--- src/ray/util/ordered_set.h
+++ src/ray/util/ordered_set.h
@@ -14,6 +14,11 @@
 template <typename T>
 class ordered_set {
  public:
+  using elements_type = std::list<T>;
+  using positions_type = std::unordered_map<T, typename elements_type::iterator>;
+  using iterator_type = typename positions_type::iterator;
+
+
   ordered_set() {}
 
   ordered_set(const ordered_set &other) = delete;
@@ -21,32 +26,43 @@ class ordered_set {
   ordered_set &operator=(const ordered_set &other) = delete;
 
   void push_back(const T &value) {
-    RAY_CHECK(iterators_.find(value) == iterators_.end());
+    RAY_CHECK(positions_.find(value) == positions_.end());
     auto list_iterator = elements_.insert(elements_.end(), value);
-    iterators_[value] = list_iterator;
+    positions_[value] = list_iterator;
   }
 
-  size_t count(const T &k) const { return iterators_.count(k); }
+  size_t count(const T &k) const { return positions_.count(k); }
 
   void pop_front() {
-    iterators_.erase(elements_.front());
+    positions_.erase(elements_.front());
     elements_.pop_front();
   }
 
   const T &front() const { return elements_.front(); }
 
-  size_t size() const noexcept { return iterators_.size(); }
+  size_t size() const noexcept { return positions_.size(); }
 
   size_t erase(const T &k) {
-    auto it = iterators_.find(k);
-    RAY_CHECK(it != iterators_.end());
+    auto it = positions_.find(k);
+    RAY_CHECK(it != positions_.end());
     elements_.erase(it->second);
-    return iterators_.erase(k);
+    return positions_.erase(k);
   }
 
+  iterator_type erase(const iterator_type position) {
+    elements_.erase(position->second);
+    return positions_.erase(position);
+  }
+
+  // TODO(rkn): The iterator returned here is not ideal because you can't simply
+  // call *it to get the value, you need to do *(it->second).
+  iterator_type begin() noexcept { return positions_.begin(); }
+
+  iterator_type end() noexcept { return positions_.end(); }
+
  private:
-  std::list<T> elements_;
-  std::unordered_map<T, typename std::list<T>::iterator> iterators_;
+  elements_type elements_;
+  positions_type positions_;
 };
 
 #endif  // RAY_UTIL_ORDERED_SET_H
