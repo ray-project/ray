08f545a2c33ec95cf8ce6731a80d5493b0648cc8
Devin Petersohn <devin.petersohn@gmail.com>
2018-11-28 12:44:51 -0800

    fix

--- thirdparty/scripts/build_modin.sh
+++ thirdparty/scripts/build_modin.sh
@@ -15,16 +15,15 @@ PYTHON_VERSION="$($PYTHON_EXECUTABLE -c 'import sys; print(sys.version_info[0])'
 
 TP_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)/../
 MODIN_VERSION=0.2.4
-MODIN_WHEELS_FNAME="modin-$MODIN_VERSION-py$PYTHON_VERSION-none-any"
+MODIN_WHEELS_FNAME="modin-$MODIN_VERSION-py$PYTHON_VERSION-none-any.whl"
 MODIN_WHEELS_URL="https://github.com/modin-project/modin/releases/download/v$MODIN_VERSION/"
 
 pushd $TP_DIR/../python/ray/
 rm -rf modin
 mkdir modin
 pushd modin
-curl -k "$MODIN_WHEELS_URL$MODIN_WHEELS_FNAME.whl" -o "$MODIN_WHEELS_FNAME.whl"
-#mv "$MODIN_WHEELS_FNAME.whl" "$MODIN_WHEELS_FNAME.ZIP"
-unzip "$MODIN_WHEELS_FNAME.whl"
-rm "$MODIN_WHEELS_FNAME.ZIP"
+curl -kL "$MODIN_WHEELS_URL$MODIN_WHEELS_FNAME" -o "$MODIN_WHEELS_FNAME"
+unzip "$MODIN_WHEELS_FNAME"
+rm "$MODIN_WHEELS_FNAME"
 popd
 popd
