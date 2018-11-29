b4127ea3a738299a9550b96344627e403920e60e
Devin Petersohn <devin-petersohn@users.noreply.github.com>
2018-11-29 11:05:24 -0800

    Ship Modin with Ray. (#3109)

---
+++ thirdparty/scripts/build_modin.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -x
+
+# Cause the script to exit if a single command fails.
+set -e
+
+if [[ -z  "$1" ]]; then
+  PYTHON_EXECUTABLE=`which python`
+else
+  PYTHON_EXECUTABLE=$1
+fi
+
+PYTHON_VERSION="$($PYTHON_EXECUTABLE -c 'import sys; print(sys.version_info[0])')"
+
+TP_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)/../
+MODIN_VERSION=0.2.4
+MODIN_WHEELS_FNAME="modin-$MODIN_VERSION-py$PYTHON_VERSION-none-any.whl"
+MODIN_WHEELS_URL="https://github.com/modin-project/modin/releases/download/v$MODIN_VERSION/"
+
+pushd $TP_DIR/../python/ray/
+rm -rf modin
+mkdir modin
+pushd modin
+curl -kL "$MODIN_WHEELS_URL$MODIN_WHEELS_FNAME" -o "$MODIN_WHEELS_FNAME"
+unzip "$MODIN_WHEELS_FNAME"
+rm "$MODIN_WHEELS_FNAME"
+popd
+popd
