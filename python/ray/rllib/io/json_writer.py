dd438878fb7872e269445c93589e20bf05390659
Eric Liang <ekhliang@gmail.com>
2018-11-28 18:27:40 -0800

    json writer

---
+++ python/ray/rllib/io/json_writer.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+class JsonWriter(OutputWriter):
+    """Writer object that saves experiences in JSON file chunks."""
+
+    def __init__(self, ioctx, path, max_file_size=64000000):
+        self.ioctx = ioctx
+        self.path = path
+        self.max_file_size = max_file_size
+        try:
+            os.makedirs(path)
+        except OSError:
+            pass  # already exists
+        assert os.path.exists(path), "Failed to create {}".format(path)
+
+    def write(self, sample_batch):
+        raise NotImplementedError
