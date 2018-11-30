b4ce6db675d00dffab6e7398a9ebabbb8f920075
Philipp Moritz <pcmoritz@gmail.com>
2018-11-29 16:10:10 -0800

    try latest arrow

--- cmake/Modules/ArrowExternalProject.cmake
+++ cmake/Modules/ArrowExternalProject.cmake
@@ -14,11 +14,11 @@
 #  - PLASMA_STATIC_LIB
 #  - PLASMA_SHARED_LIB
 
-set(arrow_URL https://github.com/apache/arrow.git)
+set(arrow_URL https://github.com/pcmoritz/arrow.git)
 # The PR for this commit is https://github.com/apache/arrow/pull/2826. We
 # include the link here to make it easier to find the right commit because
 # Arrow often rewrites git history and invalidates certain commits.
-set(arrow_TAG b4f7ed6d6ed5cdb6dd136bac3181a438f35c8ea0)
+set(arrow_TAG 140e9509f99478d4d1e7aa52f7e13f81c3274251)
 
 set(ARROW_INSTALL_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/arrow-install)
 set(ARROW_HOME ${ARROW_INSTALL_PREFIX})
