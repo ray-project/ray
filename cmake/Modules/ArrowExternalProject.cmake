980bf002db70789b74772557f1a3008dec3f0696
Robert Nishihara <robertnishihara@gmail.com>
2018-11-30 14:25:13 -0800

    Update arrow.

--- cmake/Modules/ArrowExternalProject.cmake
+++ cmake/Modules/ArrowExternalProject.cmake
@@ -18,7 +18,7 @@ set(arrow_URL https://github.com/robertnishihara/arrow.git)
 # The PR for this commit is https://github.com/apache/arrow/pull/2826. We
 # include the link here to make it easier to find the right commit because
 # Arrow often rewrites git history and invalidates certain commits.
-set(arrow_TAG 0db9154bdcf8c8c2cc99bf81516ca9832f8a4cfc)
+set(arrow_TAG f04a7d26d7c1dff91cfa4ab24a04bed2685d1224)
 
 set(ARROW_INSTALL_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/arrow-install)
 set(ARROW_HOME ${ARROW_INSTALL_PREFIX})
