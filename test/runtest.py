9eb7808949726305844df539c6631fd8ef830741
Philipp Moritz <pcmoritz@gmail.com>
2018-12-04 14:55:30 -0800

    Update test/runtest.py
    Co-Authored-By: suquark <suquark@gmail.com>

--- test/runtest.py
+++ test/runtest.py
@@ -392,7 +392,7 @@ def test_serialization_final_fallback(ray_start):
 
     test_data = np.random.randint(0, 100, size=(50, 10))
     model = CatBoostClassifier(iterations=2, depth=2, learning_rate=1,
-                               loss_function='Logloss',
+                               loss_function="Logloss",
                                logging_level="Verbose")
     model = ray.get(fit.remote(model))
     preds_class = model.predict(test_data)
