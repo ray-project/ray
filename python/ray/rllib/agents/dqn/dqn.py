3608faf002ee6ed6515c090fd75383d0e5badae2
Eric Liang <ekhliang@gmail.com>
2018-12-07 00:54:59 -0800

    fix

--- python/ray/rllib/agents/dqn/dqn.py
+++ python/ray/rllib/agents/dqn/dqn.py
@@ -220,14 +220,12 @@ class DQNAgent(Agent):
 
         if self.config["per_worker_exploration"]:
             # Only collect metrics from the third of workers with lowest eps
-            result = collect_metrics(
-                self.local_evaluator,
-                self.remote_evaluators[-len(self.remote_evaluators) // 3:],
-                timeout_seconds=self.config["collect_metrics_timeout"])
+            result = self.optimizer.collect_metrics(
+                timeout_seconds=self.config["collect_metrics_timeout"],
+                selected_evaluators=self.remote_evaluators[
+                    -len(self.remote_evaluators) // 3:])
         else:
-            result = collect_metrics(
-                self.local_evaluator,
-                self.remote_evaluators,
+            result = self.optimizer.collect_metrics(
                 timeout_seconds=self.config["collect_metrics_timeout"])
 
         result.update(
