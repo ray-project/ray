5796781d3c38a8e813e35a580d6f8c0a1c1a951d
Richard Liaw <rliaw@berkeley.edu>
2018-12-13 23:00:44 -0800

    need to make pymarl not hijack os env

--- python/ray/rllib/examples/starcraft/starcraft_env.py
+++ python/ray/rllib/examples/starcraft/starcraft_env.py
@@ -3,18 +3,29 @@ from __future__ import division
 from __future__ import print_function
 
 import gym
+import os
 import random
-import unittest
+import sys
+import yaml
+from types import SimpleNamespace
 
 from ray.rllib.env.multi_agent_env import MultiAgentEnv
 
 
 
-class BasicMultiAgent(MultiAgentEnv):
+class SC2MultiAgentEnv(MultiAgentEnv):
     """Env of N independent agents, each of which exits after 25 steps."""
 
-    def __init__(self, num):
-        self._starcraft_env = pymarl.MultiAgentEnv()
+    def __init__(self):
+        PYMARL_PATH = os.environ.get("PYMARL_PATH")
+        sys.path.append(os.path.join(PYMARL_PATH, "src"))
+        from envs.starcraft2 import StarCraft2Env
+        with open(os.path.join(PYMARL_PATH, "src/config/envs/sc2.yaml")) as f:
+            pymarl_args = yaml.load(f)
+            # HACK
+            pymarl_args["env_args"]["seed"] = 0
+            
+        self._starcraft_env = StarCraft2Env(**pymarl_args)
 
     def reset(self):
         self._starcraft_env.reset()
@@ -25,3 +36,10 @@ class BasicMultiAgent(MultiAgentEnv):
         obs = self._starcraft_env.get_obs()
         return obs, rew, done, info
 
+
+if __name__ == "__main__":
+    path_to_pymarl = "/data/rliaw/pymarl/"
+    os.environ["PYMARL_PATH"] = path_to_pymarl
+    os.environ["SC2PATH"] = os.path.join(path_to_pymarl, "3rdparty/StarCraftII")
+    env = SC2MultiAgentEnv()
+    env.reset()
