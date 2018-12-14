a611a92acc30fceeda595a7c9ce33793ca100a1e
Yuhong Guo <yuhong.gyh@antfin.com>
2018-12-14 12:38:24 +0800

    Lint

--- test/failure_test.py
+++ test/failure_test.py
@@ -625,7 +625,6 @@ def test_raylet_crash_when_get(ray_start_regular):
 
     thread = threading.Thread(target=sleep_to_kill_raylet)
     thread.start()
-    with pytest.raises(
-            Exception, match=r".*raylet client may be closed.*"):
+    with pytest.raises(Exception, match=r".*raylet client may be closed.*"):
         ray.get(nonexistent_id)
     thread.join()
