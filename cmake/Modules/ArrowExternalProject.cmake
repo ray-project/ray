7c83c167b3d254f7ab1f362b342f973b0b671d82
Philipp Moritz <pcmoritz@gmail.com>
2018-12-18 19:12:28 -0800

    update

--- cmake/Modules/ArrowExternalProject.cmake
+++ cmake/Modules/ArrowExternalProject.cmake
@@ -18,7 +18,7 @@ set(arrow_URL https://github.com/pcmoritz/arrow.git)
 # The PR for this commit is https://github.com/apache/arrow/pull/XXXX. We
 # include the link here to make it easier to find the right commit because
 # Arrow often rewrites git history and invalidates certain commits.
-set(arrow_TAG 9e5a357673ced9d67a602acb78ade22c84fe58ed)
+set(arrow_TAG b1789851b0431d643eb27a02d3c0bcb778563adb)
 
 set(ARROW_INSTALL_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/arrow-install)
 set(ARROW_HOME ${ARROW_INSTALL_PREFIX})
