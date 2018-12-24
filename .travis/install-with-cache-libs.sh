38d98a78badb440297a51c052cc455517522287b
Yuhong Guo <yuhong.gyh@antfin.com>
2018-12-24 17:01:08 +0800

    Fix arrow build

--- .travis/install-with-cache-libs.sh
+++ .travis/install-with-cache-libs.sh
@@ -12,5 +12,11 @@ bash $ROOT_DIR/../thirdparty/scripts/collect_dependent_libs.sh -n
 # Import all the libs cached in local to environment variables.
 source $ROOT_DIR/../thirdparty/external_project_libs/resource.txt
 
+# Clean the arrow building libs and use the cache libs.
+pushd $ROOT_DIR/../build/external/arrow/src/arrow_ep
+echo "Clean Arrow building files."
+rm -rf $ROOT_DIR/../build/external/arrow/src/arrow_ep-build/
+popd
+
 # Rebuild ray with libs cache environment variables, which is fast.
 bash $ROOT_DIR/install-ray.sh
