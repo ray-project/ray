0de528f0f1850fb41173f79ea7ff9aaaa4c5bfbf
Eric Liang <ekhliang@gmail.com>
2018-12-23 18:00:20 -0800

    lint

--- .travis/setup-rllib-dev.py
+++ .travis/setup-rllib-dev.py
@@ -14,16 +14,15 @@ try:
 except (ImportError, AttributeError):
     pass
 
-
 if __name__ == "__main__":
     rllib_home = os.path.abspath(os.path.join(ray.__file__, "../rllib"))
-    local_home = os.path.abspath(os.path.join(__file__, "../../python/ray/rllib"))
+    local_home = os.path.abspath(
+        os.path.join(__file__, "../../python/ray/rllib"))
     assert os.path.isdir(rllib_home), rllib_home
     assert os.path.isdir(local_home), local_home
     print(
         "This will replace:\n  {}\nwith a symlink to:\n  {}".format(
-            rllib_home, local_home) +
-        "\nHit <Enter> to continue: ",
+            rllib_home, local_home) + "\nHit <Enter> to continue: ",
         end="")
     input()
     if os.access(os.path.dirname(rllib_home), os.W_OK):
@@ -34,12 +33,11 @@ if __name__ == "__main__":
             rllib_home))
         subprocess.check_call(["sudo", "rm", "-rf", rllib_home])
         subprocess.check_call(["sudo", "ln", "-s", local_home, rllib_home])
-    print(
-        "Created links.\n\nIf you run into issues initializing Ray, please "
-        "ensure that your local repo and the installed Ray is in sync "
-        "(pip install -U the latest wheels at "
-        "https://ray.readthedocs.io/en/latest/installation.html, "
-        "and ensure you are up-to-date on the master branch on git).\n\n"
-        "Note that you may need to delete the rllib symlink when pip "
-        "installing new Ray versions to prevent pip from overwriting files "
-        "in your git repo.")
+    print("Created links.\n\nIf you run into issues initializing Ray, please "
+          "ensure that your local repo and the installed Ray is in sync "
+          "(pip install -U the latest wheels at "
+          "https://ray.readthedocs.io/en/latest/installation.html, "
+          "and ensure you are up-to-date on the master branch on git).\n\n"
+          "Note that you may need to delete the rllib symlink when pip "
+          "installing new Ray versions to prevent pip from overwriting files "
+          "in your git repo.")
