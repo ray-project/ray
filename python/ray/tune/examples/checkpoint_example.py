9c9cd9d98477ad7cf3cd6cfcf53825bdec6204ed
Richard Liaw <rliaw@berkeley.edu>
2018-12-24 18:06:56 -0800

    add example

--- python/ray/tune/examples/checkpoint_example.py
+++ python/ray/tune/examples/checkpoint_example.py
@@ -8,6 +8,7 @@ import argparse
 import json
 import os
 import random
+import time
 
 import numpy as np
 
@@ -30,7 +31,7 @@ class MyTrainableClass(Trainable):
         self.timestep += 1
         v = np.tanh(float(self.timestep) / self.config["width"])
         v *= self.config["height"]
-
+        time.sleep(1)
         # Here we use `episode_reward_mean`, but you can also report other
         # objectives such as loss or accuracy.
         return {"episode_reward_mean": v}
@@ -51,24 +52,18 @@ if __name__ == "__main__":
     parser.add_argument(
         "--smoke-test", action="store_true", help="Finish quickly for testing")
     args, _ = parser.parse_known_args()
-    ray.init()
-
-    # Hyperband early stopping, configured with `episode_reward_mean` as the
-    # objective and `training_iteration` as the time unit,
-    # which is automatically filled by Tune.
-    hyperband = HyperBandScheduler(
-        time_attr="training_iteration",
-        reward_attr="episode_reward_mean",
-        max_t=100)
+    ray.init(num_cpus=4)
 
     exp = Experiment(
         name="hyperband_test",
         run=MyTrainableClass,
         num_samples=20,
-        stop={"training_iteration": 1 if args.smoke_test else 99999},
+        stop={"training_iteration": 50},
+        checkpoint_freq=4,
+        local_dir="~/ray_results/checkpoint_test/",
         config={
             "width": sample_from(lambda spec: 10 + int(90 * random.random())),
             "height": sample_from(lambda spec: int(100 * random.random()))
         })
 
-    run_experiments(exp, scheduler=hyperband)
+    run_experiments(exp, resume=True)
