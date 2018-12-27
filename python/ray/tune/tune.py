8503e1716cd5883c5ffcaeb8cb042725a636cd82
Eric Liang <ekhliang@gmail.com>
2018-12-27 21:00:00 +0800

    Update python/ray/tune/tune.py
    Co-Authored-By: richardliaw <rliaw@berkeley.edu>

--- python/ray/tune/tune.py
+++ python/ray/tune/tune.py
@@ -128,7 +128,12 @@ def run_experiments(experiments,
         elif resume is None:
              msg = "Found incomplete experiment at {}. Would you like to resume it?".format(
                 checkpoint_dir)
-            restore = click.confirm(msg, default=True)
+             restore = click.confirm(msg, default=True)
+             if restore:
+                 logger.info("Tip: to always resume, pass resume=True to run_experiments()"
+             else:
+                 logger.info("Tip: to always start a new experiment, pass resume=False to run_experiments()"
+
     else:
         logger.info(
             "Did not find checkpoint file in {}.".format(checkpoint_dir))
