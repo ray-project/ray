261c29405ec7d7ef6b3a0531daeec0b4d2830949
Richard Liaw <rliaw@berkeley.edu>
2018-12-29 12:33:20 +0800

    some first changes'

--- python/ray/tune/__init__.py
+++ python/ray/tune/__init__.py
@@ -2,12 +2,29 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import logging
+import sys
+
 from ray.tune.error import TuneError
 from ray.tune.tune import run_experiments
 from ray.tune.experiment import Experiment
 from ray.tune.registry import register_env, register_trainable
 from ray.tune.trainable import Trainable
 from ray.tune.suggest import grid_search, function, sample_from
+
+
+def _setup_logger():
+    logger = logging.getLogger("ray.tune")
+    handler = logging.StreamHandler(stream=sys.stdout)
+    handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s\t%(levelname)s %(filename)s:%(lineno)s -- %(message)s"
+        ))
+    logger.addHandler(handler)
+    logger.propagate = False
+
+
+_setup_logger()
 
 __all__ = [
     "Trainable",
