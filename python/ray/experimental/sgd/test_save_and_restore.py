15b2db72e6460f8a0d248f4c9972a26ce6110875
Peter Schafhalter <pschafhalter@berkeley.edu>
2019-01-06 01:29:38 -0800

    Add configurable checkpoint directory to test

--- python/ray/experimental/sgd/test_save_and_restore.py
+++ python/ray/experimental/sgd/test_save_and_restore.py
@@ -21,6 +21,7 @@ parser.add_argument("--grad-shard-bytes", default=10000000, type=int)
 parser.add_argument("--devices-per-worker", default=2, type=int)
 parser.add_argument("--all-reduce-alg", default="simple", type=str)
 parser.add_argument("--object-store-memory", default=None, type=int)
+parser.add_argument("--checkpoint-dir", default="/tmp", type=str)
 parser.add_argument(
     "--strategy", default="simple", type=str, help="One of 'simple' or 'ps'")
 parser.add_argument(
@@ -45,8 +46,9 @@ if __name__ == "__main__":
         grad_shard_bytes=args.grad_shard_bytes,
         all_reduce_alg=args.all_reduce_alg)
 
-    if not os.path.exists("/tmp/sgd_params"):
-        os.mkdir("/tmp/sgd_prams")
+    if not os.path.exists(args.checkpoint_dir):
+        raise ValueError(
+            "Checkpoint directory does not exist: %s" % args.checkpoint_dir)
 
     def step(i):
         start = time.time()
@@ -63,13 +65,13 @@ if __name__ == "__main__":
         i += 1
 
     print("Saving checkpoint...")
-    sgd.save_checkpoint("/tmp/sgd_params")
+    sgd.save_checkpoint(args.checkpoint_dir)
     print("Done saving checkpoint")
 
     step(i)
 
     print("Restoring checkpoint")
-    sgd.restore_checkpoint("/tmp/sgd_params")
+    sgd.restore_checkpoint(args.checkpoint_dir)
     print("Done restoring checkpoint")
 
     step(i)
