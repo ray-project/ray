8934e37a78cbe7b5cd7cec72cb4a67bb0f5d5a0c
Richard Liaw <rliaw@berkeley.edu>
2019-01-06 13:20:10 -0800

    [tune] Change log handling for Tune (#3661)
    Also provides a small retry mechanism for a transient error as reported
    by #3340.
    
    Closes #3653.

--- python/ray/tune/__init__.py
+++ python/ray/tune/__init__.py
@@ -2,12 +2,28 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import logging
+
 from ray.tune.error import TuneError
 from ray.tune.tune import run_experiments
 from ray.tune.experiment import Experiment
 from ray.tune.registry import register_env, register_trainable
 from ray.tune.trainable import Trainable
 from ray.tune.suggest import grid_search, function, sample_from
+
+
+def _setup_logger():
+    logger = logging.getLogger("ray.tune")
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s\t%(levelname)s %(filename)s:%(lineno)s -- %(message)s"
+        ))
+    logger.addHandler(handler)
+    logger.propagate = False
+
+
+_setup_logger()
 
 __all__ = [
     "Trainable",
