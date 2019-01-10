e61e211513b4ad1e63201b54d431df0d8cd14d83
Richard Liaw <rliaw@berkeley.edu>
2019-01-09 18:47:08 -0800

    promper logging

--- test/stress_tests/run_stress_tests.sh
+++ test/stress_tests/run_stress_tests.sh
@@ -4,35 +4,38 @@
 set -x
 
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
-RESULT_FILE="results-$(date '+%Y-%m-%d_%H-%M-%S').log"
-echo "Logging to" $RESULT_FILE
-touch $RESULT_FILE
 
 
 run_test(){
     local test_name=$1
-    pushd "$ROOT_DIR"
+    local RESULT_FILE=$2
 
-        local CLUSTER="stress_testing_config.yaml"
-        echo "Try running $1."
-        {
-            ray up -y $CLUSTER --cluster-name "$1"
-            sleep 1
-            ray submit $CLUSTER --cluster-name "$1" "$1.py"
-            echo "PASS: $1" >> $RESULT_FILE
-        } || echo "FAIL: $1" >> $RESULT_FILE
+    local CLUSTER="stress_testing_config.yaml"
+    echo "Try running $test_name."
+    {
+        ray up -y $CLUSTER --cluster-name "$test_name"
+        sleep 1
+        ray submit $CLUSTER --cluster-name "$test_name" "$test_name.py"
+        echo "PASS: $test_name" >> $RESULT_FILE
+    } || echo "FAIL: $test_name" >> $RESULT_FILE
 
-        # Tear down cluster.
-        if [ "$DEBUG_MODE" = "" ]; then
-            ray down -y $CLUSTER --cluster-name "$1"
-        else
-            echo "Not tearing down cluster" $CLUSTER
-        fi
-    popd
+    # Tear down cluster.
+    if [ "$DEBUG_MODE" = "" ]; then
+        ray down -y $CLUSTER --cluster-name "$test_name"
+    else
+        echo "Not tearing down cluster" $CLUSTER
+    fi
 }
 
-run_test test_many_tasks_and_transfers &
-run_test test_dead_actors &
+pushd "$ROOT_DIR"
+    RESULT_FILE="results-$(date '+%Y-%m-%d_%H-%M-%S').log"
+    echo "Logging to" $RESULT_FILE
+    touch $RESULT_FILE
 
-wait
-cat RESULT_FILE
+    run_test test_many_tasks_and_transfers $RESULT_FILE &
+    run_test test_dead_actors $RESULT_FILE &
+
+    wait
+    cat $RESULT_FILE
+popd
+
