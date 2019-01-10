c53de4741793b8204d30ff7fdb7274f644589e47
Richard Liaw <rliaw@berkeley.edu>
2019-01-09 18:01:57 -0800

    Fix up stress tests

--- test/stress_tests/run_stress_tests.sh
+++ test/stress_tests/run_stress_tests.sh
@@ -1,21 +1,38 @@
 #!/usr/bin/env bash
 
-# Cause the script to exit if a single command fails.
-set -e
-
 # Show explicitly which commands are currently running.
 set -x
 
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
+RESULT_FILE="results-$(date '+%Y-%m-%d_%H-%M-%S').log"
+echo "Logging to" $RESULT_FILE
+touch $RESULT_FILE
 
-pushd "$ROOT_DIR"
-  # Start a large cluster using the autoscaler.
-  ray up -y stress_testing_config.yaml
 
-  # Run a bunch of stress tests.
-  ray submit stress_testing_config.yaml test_many_tasks_and_transfers.py
-  ray submit stress_testing_config.yaml test_dead_actors.py
+run_test(){
+    local test_name=$1
+    pushd "$ROOT_DIR"
 
-  # Tear down the cluster.
-  ray down -y stress_testing_config.yaml
-popd
+        local CLUSTER="stress_testing_config.yaml"
+        echo "Try running $1."
+        {
+            ray up -y $CLUSTER --cluster-name "$1"
+            sleep 1
+            ray submit $CLUSTER --cluster-name "$1" "$1.py"
+            echo "PASS: $1" >> $RESULT_FILE
+        } || echo "FAIL: $1" >> $RESULT_FILE
+
+        # Tear down cluster.
+        if [ "$DEBUG_MODE" = "" ]; then
+            ray down -y $CLUSTER --cluster-name "$1"
+        else
+            echo "Not tearing down cluster" $CLUSTER
+        fi
+    popd
+}
+
+run_test test_many_tasks_and_transfers &
+run_test test_dead_actors &
+
+wait
+cat RESULT_FILE
