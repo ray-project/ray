4621a7da1f675ca9b82d86a117684a23b21e169d
Philipp Moritz <pcmoritz@gmail.com>
2019-01-12 12:56:48 -0800

    rebase arrow

--- cmake/Modules/ArrowExternalProject.cmake
+++ cmake/Modules/ArrowExternalProject.cmake
@@ -14,7 +14,7 @@
 #  - PLASMA_STATIC_LIB
 #  - PLASMA_SHARED_LIB
 
-set(arrow_URL https://github.com/ray-project/arrow.git)
+set(arrow_URL https://github.com/pcmoritz/arrow.git)
 # This commit is based on https://github.com/apache/arrow/pull/3197. We
 # include the link here to make it easier to find the right commit because
 # Arrow often rewrites git history and invalidates certain commits.
@@ -22,7 +22,7 @@ set(arrow_URL https://github.com/ray-project/arrow.git)
 # the patch is available at
 # https://github.com/ray-project/arrow/commit/c347cd571e51723fc8512922f1b3a8e45e45b169
 # See the discussion in https://github.com/apache/arrow/pull/3177
-set(arrow_TAG c347cd571e51723fc8512922f1b3a8e45e45b169)
+set(arrow_TAG 0e833593d1378757230ae72e9095a40c5e4238f4)
 
 set(ARROW_INSTALL_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/arrow-install)
 set(ARROW_HOME ${ARROW_INSTALL_PREFIX})
