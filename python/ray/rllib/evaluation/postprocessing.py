added fix to advantage calculation for pull request

python/ray/rllib/evaluation/postprocessing.py
@@ -19,7 +19,7 @@ def compute_advantages(rollout, last_r, gamma=0.9, lambda_=1.0, use_gae=True):
         last_r (float): Value estimation for last observation
         gamma (float): Discount factor.
         lambda_ (float): Parameter for GAE
-        use_gae (bool): Using Generalized Advantage Estamation
+        use_gae (bool): Using Generalized Advantage Estimation
 
     Returns:
         SampleBatch (SampleBatch): Object with experience from rollout and
@@ -44,8 +44,11 @@ def compute_advantages(rollout, last_r, gamma=0.9, lambda_=1.0, use_gae=True):
         rewards_plus_v = np.concatenate(
             [rollout["rewards"], np.array([last_r])])
         traj["advantages"] = discount(rewards_plus_v, gamma)[:-1]
-        # TODO(ekl): support using a critic without GAE
-        traj["value_targets"] = np.zeros_like(traj["advantages"])
+        if "vf_preds" in rollout:
+            traj["value_targets"] = traj["advantages"].copy().astype(np.float32)
+            traj["advantages"] -= traj["vf_preds"]
+        else:
+            traj["value_targets"] = np.zeros_like(traj["advantages"])
 
     traj["advantages"] = traj["advantages"].copy().astype(np.float32)
 