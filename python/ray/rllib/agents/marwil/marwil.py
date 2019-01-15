f3d7ceda476eb2d8abc202baa9f5a1db6f118772
Eric Liang <ekhliang@gmail.com>
2019-01-14 16:07:23 -0800

    revise

--- python/ray/rllib/agents/marwil/marwil.py
+++ python/ray/rllib/agents/marwil/marwil.py
@@ -2,14 +2,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import time
-
-from ray.rllib import optimizers
 from ray.rllib.agents.agent import Agent, with_common_config
 from ray.rllib.agents.marwil.marwil_policy_graph import MARWILPolicyGraph
-from ray.rllib.optimizers import SyncSamplesOptimizer
+from ray.rllib.optimizers import SyncBatchReplayOptimizer
 from ray.rllib.utils.annotations import override
-from ray.rllib.utils.schedules import ConstantSchedule, LinearSchedule
 
 # yapf: disable
 # __sphinx_doc_begin__
@@ -18,7 +14,7 @@ DEFAULT_CONFIG = with_common_config({
     # When beta is 0, MARWIL is reduced to imitation learning
     "beta": 1.0,
     # Balancing value estimation loss and policy optimization loss
-    "c": 1.0,
+    "vf_coeff": 1.0,
     # Whether to calculate cumulative rewards
     "postprocess_inputs": True,
     # Whether to rollout "complete_episodes" or "truncate_episodes"
@@ -26,17 +22,11 @@ DEFAULT_CONFIG = with_common_config({
     # Read data from historic data and evaluate by a sampler
     "input_evaluation": "simulation",
     # Learning rate for adam optimizer
-    "lr": 5e-5,
-    # If not None, clip gradients during optimization at this value
-    "grad_norm_clipping": 40,
-    # Size of batches collected from each worker
-    "sample_batch_size": 200,
+    "lr": 1e-3,
     # Number of timesteps collected for each SGD round
-    "train_batch_size": 4000,
-    # Total SGD batch size across all devices for SGD
-    "sgd_minibatch_size": 128,
-    # Number of SGD iterations in each outer loop
-    "num_sgd_iter": 2,
+    "train_batch_size": 2000,
+    # Number of steps max to keep in the batch replay buffer
+    "replay_buffer_size": 100000,
     # === Parallelism ===
     "num_workers": 0,
 })
@@ -57,9 +47,9 @@ class MARWILAgent(Agent):
             self.env_creator, self._policy_graph)
         self.remote_evaluators = self.make_remote_evaluators(
             self.env_creator, self._policy_graph, self.config["num_workers"])
-        self.optimizer = SyncSamplesOptimizer(
+        self.optimizer = SyncBatchReplayOptimizer(
             self.local_evaluator, self.remote_evaluators, {
-                "num_sgd_iter": self.config["num_sgd_iter"],
+                "buffer_size": self.config["replay_buffer_size"],
                 "train_batch_size": self.config["train_batch_size"],
             })
 
--- python/ray/rllib/agents/marwil/marwil_policy_graph.py
+++ python/ray/rllib/agents/marwil/marwil_policy_graph.py
@@ -2,33 +2,29 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from gym.spaces import Discrete
-import numpy as np
 import tensorflow as tf
-import tensorflow.contrib.layers as layers
 
 import ray
 from ray.rllib.models import ModelCatalog
 from ray.rllib.evaluation.postprocessing import compute_advantages
-from ray.rllib.evaluation.sample_batch import SampleBatch
 from ray.rllib.utils.annotations import override
-from ray.rllib.utils.error import UnsupportedSpaceException
 from ray.rllib.evaluation.policy_graph import PolicyGraph
 from ray.rllib.evaluation.tf_policy_graph import TFPolicyGraph
 from ray.rllib.agents.dqn.dqn_policy_graph import _scope_vars
+from ray.rllib.utils.explained_variance import explained_variance
 
 P_SCOPE = "p_func"
 V_SCOPE = "v_func"
 
 
 class ValueLoss(object):
-    def __init__(self, state_values, cummulative_rewards):
+    def __init__(self, state_values, cumulative_rewards):
         self.loss = 0.5 * tf.reduce_mean(
-            tf.square(state_values - cummulative_rewards))
+            tf.square(state_values - cumulative_rewards))
 
 
 class ReweightedImitationLoss(object):
-    def __init__(self, state_values, cummulative_rewards, logits, actions,
+    def __init__(self, state_values, cumulative_rewards, logits, actions,
                  action_space, beta):
         ma_adv_norm = tf.get_variable(
             name="moving_average_of_advantage_norm",
@@ -36,7 +32,7 @@ class ReweightedImitationLoss(object):
             initializer=100.0,
             trainable=False)
         # advantage estimation
-        adv = cummulative_rewards - state_values
+        adv = cumulative_rewards - state_values
         # update averaged advantage norm
         update_adv_norm = tf.assign_add(
             ref=ma_adv_norm,
@@ -52,7 +48,8 @@ class ReweightedImitationLoss(object):
         action_dist = dist_cls(logits)
         logprobs = action_dist.logp(actions)
 
-        self.loss = -1.0 * tf.reduce_mean(tf.stop_gradient(adv) * logprobs)
+        self.loss = -1.0 * tf.reduce_mean(
+            tf.stop_gradient(exp_advs) * logprobs)
 
 
 class MARWILPolicyGraph(TFPolicyGraph):
@@ -64,12 +61,13 @@ class MARWILPolicyGraph(TFPolicyGraph):
             action_space, self.config["model"])
 
         # Action inputs
-        self.cur_observations = tf.placeholder(
+        self.obs_t = tf.placeholder(
             tf.float32, shape=(None, ) + observation_space.shape)
 
         with tf.variable_scope(P_SCOPE) as scope:
-            logits = self._build_policy_network(self.cur_observations,
-                                                observation_space, logit_dim)
+            self.model = self._build_policy_network(
+                self.obs_t, observation_space, logit_dim)
+            logits = self.model.outputs
             self.p_func_vars = _scope_vars(scope.name)
 
         # Action outputs
@@ -77,8 +75,6 @@ class MARWILPolicyGraph(TFPolicyGraph):
         self.output_actions = action_dist.sample()
 
         # Training inputs
-        self.obs_t = tf.placeholder(
-            tf.float32, shape=(None, ) + observation_space.shape)
         self.act_t = tf.placeholder(tf.int32, [None], name="action")
         self.cum_rew_t = tf.placeholder(tf.float32, [None], name="reward")
 
@@ -88,16 +84,14 @@ class MARWILPolicyGraph(TFPolicyGraph):
                                                      observation_space)
             self.v_func_vars = _scope_vars(scope.name)
         self.v_loss = self._build_value_loss(state_values, self.cum_rew_t)
-
-        # p network evaluation
-        with tf.variable_scope(P_SCOPE, reuse=True) as scope:
-            logits = self._build_policy_network(self.obs_t, observation_space,
-                                                logit_dim)
         self.p_loss = self._build_policy_loss(state_values, self.cum_rew_t,
                                               logits, self.act_t, action_space)
 
         # which kind of objective to optimize
-        objective = self.p_loss.loss + self.config["c"] * self.v_loss.loss
+        objective = (
+            self.p_loss.loss + self.config["vf_coeff"] * self.v_loss.loss)
+        self.explained_variance = tf.reduce_mean(
+            explained_variance(self.cum_rew_t, state_values))
 
         # initialize TFPolicyGraph
         self.sess = tf.get_default_session()
@@ -111,25 +105,26 @@ class MARWILPolicyGraph(TFPolicyGraph):
             observation_space,
             action_space,
             self.sess,
-            obs_input=self.cur_observations,
+            obs_input=self.obs_t,
             action_sampler=self.output_actions,
-            loss=objective,
+            loss=self.model.loss() + objective,
             loss_inputs=self.loss_inputs,
-            update_ops=[])
+            state_inputs=self.model.state_in,
+            state_outputs=self.model.state_out)
         self.sess.run(tf.global_variables_initializer())
 
         self.stats_fetches = {
             "total_loss": objective,
+            "vf_explained_var": self.explained_variance,
             "policy_loss": self.p_loss.loss,
             "vf_loss": self.v_loss.loss
         }
 
     def _build_policy_network(self, obs, obs_space, logit_dim):
-        policy_network = ModelCatalog.get_model({
+        return ModelCatalog.get_model({
             "obs": obs,
             "is_training": self._get_is_training_placeholder(),
         }, obs_space, logit_dim, self.config["model"])
-        return policy_network.outputs
 
     def _build_value_network(self, obs, obs_space):
         value_model = ModelCatalog.get_model({
@@ -159,9 +154,15 @@ class MARWILPolicyGraph(TFPolicyGraph):
         if completed:
             last_r = 0.0
         else:
-            raise ValueError("last done mask in a batch should be True",
-                             len(sample_batch["dones"]),
-                             sample_batch["dones"][-1])
+            raise NotImplementedError(
+                "last done mask in a batch should be True. "
+                "For now, we only support reading experience batches produced "
+                "with batch_mode='complete_episodes'.",
+                len(sample_batch["dones"]), sample_batch["dones"][-1])
         batch = compute_advantages(
             sample_batch, last_r, gamma=self.config["gamma"], use_gae=False)
         return batch
+
+    @override(PolicyGraph)
+    def get_initial_state(self):
+        return self.model.state_init
