f0371b26089a8c6d9f224da455a90f45fb24cc18
Richard Liaw <rliaw@berkeley.edu>
2019-01-14 19:17:10 -0800

    temporary

--- test/stress_tests/run_jenkins_stress_test.sh
+++ test/stress_tests/run_jenkins_stress_test.sh
@@ -9,16 +9,17 @@ set -x
 MEMORY_SIZE="20G"
 SHM_SIZE="20G"
 
-docker build --no-cache -t ray-project/base-deps docker/base-deps
+#docker build --no-cache -t ray-project/base-deps docker/base-deps
 
 # Add Ray source
-git rev-parse HEAD > ./docker/deploy/git-rev
-git archive -o ./docker/deploy/ray.tar $(git rev-parse HEAD)
-DOCKER_SHA=$(docker build --no-cache -t ray-project/basic docker/basic)
+git rev-parse HEAD > ./docker/basic/git-rev
+git archive -o ./docker/basic/ray.tar $(git rev-parse HEAD)
+DOCKER_SHA=$(docker build --no-cache -q -t ray-project/basic docker/basic)
 
 echo "Using Docker image" $DOCKER_SHA
 
 docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} \
-    -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY $DOCKER_SHA \
+    $DOCKER_SHA \
     bash /ray/test/stress_tests/run_stress_tests.sh
 
+# -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY 
