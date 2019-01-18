7749ee2709eb7e679af61dd2c322d967bffc7d8f
Daniel Edgecumbe <daniel@longshotsystems.co.uk>
2019-01-18 10:14:56 +0000

    [autoscaler]: Fix intermittent unit test

--- test/autoscaler_test.py
+++ test/autoscaler_test.py
@@ -533,10 +533,11 @@ class AutoscalingTest(unittest.TestCase):
         self.waitForNodes(2)
         for node in self.provider.mock_nodes.values():
             node.state = "running"
-        assert len(
-            self.provider.nodes({
-                TAG_RAY_NODE_STATUS: "waiting-for-ssh"
-            })) == 2
+        num_waiting_nodes = sum(
+            len(self.provider.nodes({TAG_RAY_NODE_STATUS: x}))
+            for x in ("uninitialized", "waiting-for-ssh")
+        )
+        assert num_waiting_nodes == 2
         autoscaler.update()
         self.waitForNodes(2, tag_filters={TAG_RAY_NODE_STATUS: "up-to-date"})
 
@@ -555,10 +556,11 @@ class AutoscalingTest(unittest.TestCase):
         self.waitForNodes(2)
         for node in self.provider.mock_nodes.values():
             node.state = "running"
-        assert len(
-            self.provider.nodes({
-                TAG_RAY_NODE_STATUS: "waiting-for-ssh"
-            })) == 2
+        num_waiting_nodes = sum(
+            len(self.provider.nodes({TAG_RAY_NODE_STATUS: x}))
+            for x in ("uninitialized", "waiting-for-ssh")
+        )
+        assert num_waiting_nodes == 2
         autoscaler.update()
         self.waitForNodes(
             2, tag_filters={TAG_RAY_NODE_STATUS: "update-failed"})
