7498b3c91b01700ff941f6bf0841aa79e53d25dc
Philipp Moritz <pcmoritz@gmail.com>
2019-01-19 14:31:43 -0800

    update bazel

--- .travis/install-bazel.sh
+++ .travis/install-bazel.sh
@@ -6,7 +6,7 @@ set -e
 OS=linux
 ARCH=x86_64
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then OS=darwin; fi
-URL="https://github.com/bazelbuild/bazel/releases/download/0.16.0/bazel-0.16.0-installer-${OS}-x86_64.sh"
+URL="https://github.com/bazelbuild/bazel/releases/download/0.21.0/bazel-0.21.0-installer-${OS}-x86_64.sh"
 wget -O install.sh $URL
 chmod +x install.sh
 ./install.sh --user
