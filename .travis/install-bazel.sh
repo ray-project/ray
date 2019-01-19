604502d81f8120b46592cab8b23c27a6ba7256cf
Philipp Moritz <pcmoritz@gmail.com>
2019-01-18 18:17:26 -0800

    update

---
+++ .travis/install-bazel.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Cause the script to exit if a single command fails
+set -e
+
+OS=linux
+ARCH=x86_64
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then OS=darwin; fi
+URL="https://github.com/bazelbuild/bazel/releases/download/0.16.0/bazel-0.16.0-installer-${OS}-x86_64.sh"
+wget -O install.sh $URL
+chmod +x install.sh
+./install.sh --user
+rm -f install.sh
