d46cb07ef3055c239752614cc58e780294e24a46
Wang Qing <kingchin1218@126.com>
2019-01-24 03:56:25 +0800

    Implement actor dummy object gc in java (#3822)
    * Add dummy object gc in java
    
    * Fix
    
    * Address comments.
    
    * Refine
    
    * Address comments.

--- java/runtime/src/main/java/org/ray/runtime/task/TaskSpec.java
+++ java/runtime/src/main/java/org/ray/runtime/task/TaskSpec.java
@@ -68,8 +68,8 @@ public class TaskSpec {
 
   public TaskSpec(UniqueId driverId, UniqueId taskId, UniqueId parentTaskId, int parentCounter,
       UniqueId actorCreationId, int maxActorReconstructions, UniqueId actorId,
-      UniqueId actorHandleId, int actorCounter, FunctionArg[] args, UniqueId[] returnIds,
-      Map<String, Double> resources, FunctionDescriptor functionDescriptor) {
+      UniqueId actorHandleId, int actorCounter, UniqueId[] newActorHandles, FunctionArg[] args,
+      UniqueId[] returnIds, Map<String, Double> resources, FunctionDescriptor functionDescriptor) {
     this.driverId = driverId;
     this.taskId = taskId;
     this.parentTaskId = parentTaskId;
@@ -79,8 +79,7 @@ public class TaskSpec {
     this.actorId = actorId;
     this.actorHandleId = actorHandleId;
     this.actorCounter = actorCounter;
-    // TODO: Initialize the new actor handles.
-    this.newActorHandles = new UniqueId[] {};
+    this.newActorHandles = newActorHandles;
     this.args = args;
     this.returnIds = returnIds;
     this.resources = resources;
