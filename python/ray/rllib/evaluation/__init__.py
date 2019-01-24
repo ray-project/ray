0ba503afb1999765d13c8b601e28fb6919c18924
Eric Liang <ekhliang@gmail.com>
2019-01-23 21:27:26 -0800

    [rllib] annotate public vs developer vs private APIs (#3808)

--- python/ray/rllib/evaluation/__init__.py
+++ python/ray/rllib/evaluation/__init__.py
@@ -4,9 +4,9 @@ from ray.rllib.evaluation.interface import EvaluatorInterface
 from ray.rllib.evaluation.policy_graph import PolicyGraph
 from ray.rllib.evaluation.tf_policy_graph import TFPolicyGraph
 from ray.rllib.evaluation.torch_policy_graph import TorchPolicyGraph
-from ray.rllib.evaluation.sample_batch import (SampleBatch, MultiAgentBatch,
-                                               SampleBatchBuilder,
-                                               MultiAgentSampleBatchBuilder)
+from ray.rllib.evaluation.sample_batch import SampleBatch, MultiAgentBatch
+from ray.rllib.evaluation.sample_batch_builder import (
+    SampleBatchBuilder, MultiAgentSampleBatchBuilder)
 from ray.rllib.evaluation.sampler import SyncSampler, AsyncSampler
 from ray.rllib.evaluation.postprocessing import (compute_advantages,
                                                  compute_targets)
