a40d144c40cc0d998b461873e275546a3ae808cd
Megan Kawakami <megankawakami@berkeley.edu>
2019-01-25 10:19:54 -0800

    add torch pg

---
+++ python/ray/rllib/agents/pg/torch_pg_policy_graph.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+import ray
+from ray.rllib.models.catalog import ModelCatalog
+from ray.rllib.evaluation.postprocessing import compute_advantages
+from ray.rllib.evaluation.policy_graph import PolicyGraph
+from ray.rllib.evaluation.tf_policy_graph import TFPolicyGraph
+from ray.rllib.utils.annotations import override
