f694bb1c69971f18ec46f758839977e369e77337
Megan Kawakami <megankawakami@berkeley.edu>
2019-01-25 10:28:20 -0800

    add torch imports

--- python/ray/rllib/agents/pg/torch_pg_policy_graph.py
+++ python/ray/rllib/agents/pg/torch_pg_policy_graph.py
@@ -2,7 +2,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
+import torch
+import torch.nn.functional as F
+from torch import nn
 
 import ray
 from ray.rllib.models.catalog import ModelCatalog
