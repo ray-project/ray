1db2e1a5aba9c451898236cdc7f454c975ecae2e
xiaoming.xtm <xiaoming.xtm@alibaba-inc.com>
2019-01-27 12:59:36 +0800

    fix lint error

--- python/ray/tune/test/trial_runner_test.py
+++ python/ray/tune/test/trial_runner_test.py
@@ -698,8 +698,8 @@ class RunExperimentTest(unittest.TestCase):
         })
         for trial in trials:
             self.assertEqual(trial.status, Trial.TERMINATED)
-            self.assertTrue(os.path.exists(os.path.join(trial.logdir,
-                                                        "exported")))
+            self.assertTrue(
+                os.path.exists(os.path.join(trial.logdir,"exported")))
 
     def testDeprecatedResources(self):
         class train(Trainable):
