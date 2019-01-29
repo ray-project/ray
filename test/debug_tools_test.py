2d9205f2379ecc753cee7c6728bdb62e3978e9a2
William Ma <12377941+williamma12@users.noreply.github.com>
2019-01-29 00:57:33 -0800

    Fixed minor bugs in test cases

--- test/debug_tools_test.py
+++ test/debug_tools_test.py
@@ -3,11 +3,11 @@ from __future__ import division
 from __future__ import print_function
 
 import os
-import process
 try:
     import psutil
 except ImportError:
     psutil = None
+import pytest
 import ray
 
 
@@ -16,25 +16,28 @@ def f():
     return 42
 
 
-@pytest.parameterize((env_var, process_name), [("RAY_RAYLET_GDB", "raylet/raylet"), ("RAY_PLASMA_STORE", "plasma/plasma_store_server")])
-def test_raylet_gdb():
+@pytest.mark.parametrize("env_var, process_name", [("RAY_RAYLET_GDB", "raylet/raylet"), ("RAY_PLASMA_STORE", "plasma/plasma_store_server")])
+def test_raylet_gdb(env_var, process_name):
     # Save original environment variables and set new ones
     _environ = os.environ.copy()
     os.environ["RAY_RAYLET_GDB"] = "1"
 
     # Test ray works as expected
-    ray.init(num_cpus=1)
+    ray.init(num_cpus=1, ignore_reinit_error=True)
     assert ray.get(f.remote()) == 42
 
     # Test that gdb is running only if psutil is installed
     gdb_process = False
     if psutil is not None:
         for pid in psutil.pids():
-            proc = psutil.Process(pid)
-            if "gdb" in proc.name and "raylet/raylet" in proc.name:
+            try:
+                proc = psutil.Process(pid)
+            except:
+                continue
+            if "gdb" in proc.name() and process_name in proc.name():
                 gdb_process = True
                 proc.terminate()
-                break;
+                break
         assert gdb_process
 
     # Restore environment variables
