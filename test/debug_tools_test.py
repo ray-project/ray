1b9ebedf195faf1d0ebbbf11b10b17759751bdb7
William Ma <12377941+williamma12@users.noreply.github.com>
2019-01-29 00:40:34 -0800

    Reduce duplicated code in unit tests

--- test/debug_tools_test.py
+++ test/debug_tools_test.py
@@ -16,6 +16,7 @@ def f():
     return 42
 
 
+@pytest.parameterize((env_var, process_name), [("RAY_RAYLET_GDB", "raylet/raylet"), ("RAY_PLASMA_STORE", "plasma/plasma_store_server")])
 def test_raylet_gdb():
     # Save original environment variables and set new ones
     _environ = os.environ.copy()
@@ -32,6 +33,7 @@ def test_raylet_gdb():
             proc = psutil.Process(pid)
             if "gdb" in proc.name and "raylet/raylet" in proc.name:
                 gdb_process = True
+                proc.terminate()
                 break;
         assert gdb_process
 
@@ -39,26 +41,4 @@ def test_raylet_gdb():
     os.environ.clear()
     os.environ.update(_environ)
 
-
-def test_plasma_gdb():
-    # Save original environment variables and set new ones
-    _environ = os.environ.copy()
-    os.environ["RAY_PLASMA_STORE_GDB"] = "1"
-
-    # Test ray works as expected
-    ray.init(num_cpus=1)
-    assert ray.get(f.remote()) == 42
-
-    # Test that gdb is running only if psutil is installed
-    gdb_process = False
-    if psutil is not None:
-        for pid in psutil.pids():
-            proc = psutil.Process(pid)
-            if "gdb" in proc.name and "plasma/plasma_store_server" in proc.name:
-                gdb_process = True
-                break;
-        assert gdb_process
-
-    # Restore environment variables
-    os.environ.clear()
-    os.environ.update(_environ)
+    ray.shutdown()
