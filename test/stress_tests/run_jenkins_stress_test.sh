18f8cc5cd93bfb83223aa068fe9942c4167ab0ed
Richard Liaw <rliaw@berkeley.edu>
2019-02-01 22:42:25 -0800

    aws_ssh_key

--- test/stress_tests/run_jenkins_stress_test.sh
+++ test/stress_tests/run_jenkins_stress_test.sh
@@ -18,6 +18,6 @@ DOCKER_SHA=$(docker build --no-cache -q -t ray-project/stress_test docker/stress
 
 echo "Using Docker image" $DOCKER_SHA
 docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} \
-    -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e CLUSTER_SSH_KEY \
+    -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e RAY_AWS_SSH_KEY \
     $DOCKER_SHA \
     bash /ray/test/stress_tests/run_stress_tests.sh
