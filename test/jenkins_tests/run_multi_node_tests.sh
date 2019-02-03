0889df01890e74e0549ce3e3a8b8ec4dd4464bed
Richard Liaw <rliaw@berkeley.edu>
2019-02-02 22:41:24 -0800

    Update test/jenkins_tests/run_multi_node_tests.sh
    Co-Authored-By: andrewztan <andrewztan12@gmail.com>

--- test/jenkins_tests/run_multi_node_tests.sh
+++ test/jenkins_tests/run_multi_node_tests.sh
@@ -358,7 +358,7 @@ docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
     python /ray/python/ray/tune/examples/hyperopt_example.py \
     --smoke-test
 
-docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
+docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} -e SIGOPT_KEY $DOCKER_SHA \
     python /ray/python/ray/tune/examples/sigopt_example.py \
     --smoke-test
 
