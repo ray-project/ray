6a4e69adcfe03dcb432f68c3390e6809517a2e22
Hao Chen <chenh1024@gmail.com>
2019-02-03 12:58:56 +0800

    refine MultiLanguageClusterTest

--- java/test/src/main/java/org/ray/api/test/MultiLanguageClusterTest.java
+++ java/test/src/main/java/org/ray/api/test/MultiLanguageClusterTest.java
@@ -1,65 +1,70 @@
 package org.ray.api.test;
 
 import com.google.common.collect.ImmutableList;
+import java.io.File;
 import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.ray.api.Ray;
 import org.ray.api.RayObject;
 import org.ray.api.annotation.RayRemote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+/**
+ * Test starting a ray cluster with multi-language support.
+ */
 public class MultiLanguageClusterTest {
 
-  public static final String PLASMA_STORE_SOCKET_NAME = "/tmp/ray/test/plasma_store_socket";
-  public static final String RAYLET_SOCKET_NAME = "/tmp/ray/test/raylet_socket";
+  private static final Logger LOGGER = LoggerFactory.getLogger(MultiLanguageClusterTest.class);
+
+  private static final String PLASMA_STORE_SOCKET_NAME = "/tmp/ray/test/plasma_store_socket";
+  private static final String RAYLET_SOCKET_NAME = "/tmp/ray/test/raylet_socket";
 
   @RayRemote
   public static String echo(String word) {
     return word;
   }
 
-  @BeforeMethod
-  public void setUp() throws InterruptedException {
-    // The behaviors of executing `ray` command in different systems may be
-    // different. Some systems will throw IOException with `no such file` message,
-    // but some systems will return 0.
+  /**
+   * Execute an external command.
+   * @return Whether the command succeeded.
+   */
+  private boolean executeCommand(List<String> command, int waitTimeoutSeconds) {
     try {
-      if (0 != startCluster()) {
-        throw new SkipException("Since there is no ray command, we skip this test.");
-      }
-    } catch (IOException e) {
-      throw new SkipException("Since there is no ray command, we skip this test.");
+      LOGGER.info("Executing command: {}", String.join(" ", command));
+      Process process = new ProcessBuilder(command).redirectOutput(Redirect.INHERIT)
+          .redirectError(Redirect.INHERIT).start();
+      process.waitFor(waitTimeoutSeconds, TimeUnit.SECONDS);
+      return process.exitValue() == 0;
+    } catch (Exception e) {
+      throw new RuntimeException("Error executing command " + String.join(" ", command), e);
+    }
+  }
+
+  @BeforeMethod
+  public void setUp() {
+    // Check whether 'ray' command is installed.
+    boolean rayCommandExists = executeCommand(ImmutableList.of("which", "ray"), 5);
+    if (!rayCommandExists) {
+      throw new SkipException("Skipping test, because ray command doesn't exist.");
     }
 
-    System.setProperty("ray.home", "../..");
-    System.setProperty("ray.redis.address", "127.0.0.1:6379");
-    System.setProperty("ray.object-store.socket-name", PLASMA_STORE_SOCKET_NAME);
-    System.setProperty("ray.raylet.socket-name", RAYLET_SOCKET_NAME);
-    Ray.init();
-  }
+    // Delete existing socket files.
+    for (String socket : ImmutableList.of(RAYLET_SOCKET_NAME, PLASMA_STORE_SOCKET_NAME)) {
+      File file = new File(socket);
+      if (file.exists()) {
+        file.delete();
+      }
+    }
 
-  @AfterMethod
-  public void tearDown() throws IOException, InterruptedException {
-    Ray.shutdown();
-    System.clearProperty("ray.home");
-    System.clearProperty("ray.redis.address");
-    System.clearProperty("ray.object-store.socket-name");
-    System.clearProperty("ray.raylet.socket-name");
-    stopCluster();
-  }
-
-  @Test
-  public void testEnableJavaAndPythonWorker() {
-    RayObject<String> obj = Ray.call(MultiLanguageClusterTest::echo, "hello");
-    Assert.assertEquals("hello", obj.get());
-  }
-
-  private int startCluster() throws IOException, InterruptedException {
+    // Start ray cluster.
     final List<String> startCommand = ImmutableList.of(
         "ray",
         "start",
@@ -70,26 +75,41 @@ public class MultiLanguageClusterTest {
         String.format("--raylet-socket-name=%s", RAYLET_SOCKET_NAME),
         "--java-worker-options=-classpath ../../build/java/*:../../java/test/target/*"
     );
+    if (!executeCommand(startCommand, 10)) {
+      throw new RuntimeException("Couldn't start ray cluster.");
+    }
 
-    ProcessBuilder builder = new ProcessBuilder(startCommand);
-    Process process = builder.start();
-    process.waitFor(10, TimeUnit.SECONDS);
-    return process.exitValue();
+    // Connect to the cluster.
+    System.setProperty("ray.home", "../..");
+    System.setProperty("ray.redis.address", "127.0.0.1:6379");
+    System.setProperty("ray.object-store.socket-name", PLASMA_STORE_SOCKET_NAME);
+    System.setProperty("ray.raylet.socket-name", RAYLET_SOCKET_NAME);
+    Ray.init();
   }
 
-  private void stopCluster() throws IOException, InterruptedException {
+  @AfterMethod
+  public void tearDown() {
+    // Disconnect to the cluster.
+    Ray.shutdown();
+    System.clearProperty("ray.home");
+    System.clearProperty("ray.redis.address");
+    System.clearProperty("ray.object-store.socket-name");
+    System.clearProperty("ray.raylet.socket-name");
+
+    // Stop ray cluster.
     final List<String> stopCommand = ImmutableList.of(
         "ray",
         "stop"
     );
+    if (!executeCommand(stopCommand, 10)) {
+      throw new RuntimeException("Couldn't stop ray cluster");
+    }
+  }
 
-    ProcessBuilder builder = new ProcessBuilder(stopCommand);
-    Process process = builder.start();
-    process.waitFor(10, TimeUnit.SECONDS);
-
-    // make sure `ray stop` command get finished. Otherwise some kill commands
-    // will terminate the processes which is created by next test case.
-    TimeUnit.MILLISECONDS.sleep(1500);
+  @Test
+  public void testMultiLanguageCluster() {
+    RayObject<String> obj = Ray.call(MultiLanguageClusterTest::echo, "hello");
+    Assert.assertEquals("hello", obj.get());
   }
 
 }
