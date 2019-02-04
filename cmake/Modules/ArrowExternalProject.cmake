978d6609b2f9f12dedc11d660f2072445b59d19b
Philipp Moritz <pcmoritz@gmail.com>
2019-02-04 13:12:39 -0800

    update

--- cmake/Modules/ArrowExternalProject.cmake
+++ cmake/Modules/ArrowExternalProject.cmake
@@ -25,7 +25,7 @@ set(arrow_URL https://github.com/pcmoritz/arrow.git)
 # WARNING: If the arrow version is updated, you need to also update the
 # SETUPTOOLS_SCM_PRETEND_VERSION version string in the ThirdpartyToolchain.cmake
 # file
-set(arrow_TAG 9817457ba1f94d729558f2e014958fc4dbb11a04)
+set(arrow_TAG 9b297a73b5677977521645261b2447d0fd44749c)
 
 set(ARROW_INSTALL_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/arrow-install)
 set(ARROW_HOME ${ARROW_INSTALL_PREFIX})
