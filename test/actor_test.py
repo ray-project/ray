56d3e7cf9ae9d8e963518a02664c2b4c8d650362
Yuhong Guo <yuhong.gyh@antfin.com>
2019-02-09 19:49:31 +0800

    Enlarge waiting time in test_actors_and_tasks_with_gpus

--- test/actor_test.py
+++ test/actor_test.py
@@ -1074,7 +1074,7 @@ def test_actors_and_tasks_with_gpus(ray_start_cluster):
     @ray.remote(num_gpus=1)
     def f1():
         t1 = time.monotonic()
-        time.sleep(0.2)
+        time.sleep(0.4)
         t2 = time.monotonic()
         gpu_ids = ray.get_gpu_ids()
         assert len(gpu_ids) == 1
@@ -1085,7 +1085,7 @@ def test_actors_and_tasks_with_gpus(ray_start_cluster):
     @ray.remote(num_gpus=2)
     def f2():
         t1 = time.monotonic()
-        time.sleep(0.2)
+        time.sleep(0.4)
         t2 = time.monotonic()
         gpu_ids = ray.get_gpu_ids()
         assert len(gpu_ids) == 2
