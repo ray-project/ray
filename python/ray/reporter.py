df772428ca07bcfba4be16c9698bebe1d23e42a5
Daniel Edgecumbe <daniel@longshotsystems.co.uk>
2019-02-12 15:53:18 +0000

    Reporter process now actually sends information

--- python/ray/reporter.py
+++ python/ray/reporter.py
@@ -7,6 +7,11 @@ import logging
 import os
 import traceback
 import time
+import datetime
+from socket import AddressFamily
+
+import simplejson
+import psutil
 
 import ray.ray_constants as ray_constants
 import ray.utils
@@ -16,6 +21,18 @@ import ray.utils
 # entry/init points.
 logger = logging.getLogger(__name__)
 
+def determine_ip_address():
+    """Return the first IP address for an ethernet interface on the system."""
+    addrs = [
+        x.address
+        for k, v in psutil.net_if_addrs().items() if k[0] == "e"
+        for x in v
+        if x.family == AddressFamily.AF_INET
+    ]
+    return addrs[0]
+
+def to_posix_time(dt):
+    return (dt - datetime.datetime(1970, 1, 1)).total_seconds()
 
 class Reporter(object):
     """A monitor process for monitoring Ray nodes.
@@ -28,23 +45,98 @@ class Reporter(object):
 
     def __init__(self, redis_address, redis_password=None):
         """Initialize the reporter object."""
-        self.host = os.uname()[1]
-        self.redis_key = "REPORTER_{}".format(self.host)
+        self.cpu_counts = (psutil.cpu_count(), psutil.cpu_count(logical=False))
+        self.ip_addr = determine_ip_address()
+        self.hostname = os.uname().nodename
+
+        _ = psutil.cpu_percent()  # For initialization
+
+        self.redis_key = "{}.{}".format(ray.gcs_utils.REPORTER_CHANNEL, self.hostname)
         self.redis_client = ray.services.create_redis_client(
             redis_address, password=redis_password)
 
+    @staticmethod
+    def get_cpu_percent():
+        return psutil.cpu_percent()
+
+    @staticmethod
+    def get_boot_time():
+        return psutil.boot_time()
+
+    @staticmethod
+    def get_network_stats():
+        now = datetime.datetime.utcnow()
+        ifaces = [
+            v
+            for k, v in psutil.net_io_counters(pernic=True).items()
+            if k[0] == "e"
+        ]
+
+        sent = sum((iface.bytes_sent for iface in ifaces))
+        recv = sum((iface.bytes_recv for iface in ifaces))
+        return sent, recv
+
+    @staticmethod
+    def get_mem_usage():
+        vm = psutil.virtual_memory()
+        return vm.total, vm.available, vm.percent
+
+    @staticmethod
+    def get_disk_usage():
+        return {
+            x: psutil.disk_usage(x)
+            for x in ["/", "/tmp"]
+        }
+
+    @staticmethod
+    def get_workers():
+        return [
+            x.as_dict(attrs=["pid", "create_time", "cpu_times", "name", "memory_full_info"])
+            for x in psutil.process_iter()
+            if "ray" in x.name()
+        ]
+
+    def get_load_avg(self):
+        load = os.getloadavg()
+        per_cpu_load = tuple(
+            ( round(x/self.cpu_counts[0], 2) for x in load )
+        )
+        return load, per_cpu_load
+
+    def get_all_stats(self):
+        return {
+            "now": to_posix_time(datetime.datetime.utcnow()),
+            "hostname": self.hostname,
+            "ip": self.ip_addr,
+            "cpu": self.get_cpu_percent(),
+            "cpus": self.cpu_counts,
+            "mem": self.get_mem_usage(),
+            "workers": self.get_workers(),
+            "boot_time": self.get_boot_time(),
+            "load_avg": self.get_load_avg(),
+            "disk": self.get_disk_usage(),
+        }
+
     def perform_iteration(self):
         """Get any changes to the log files and push updates to Redis."""
-        A = [self.host]
-        self.redis_client.put(ray.gcs_utils.REPORTER_CHANNEL,
-                                  "\n".join(A))
+        stats = self.get_all_stats()
+
+        self.redis_client.publish(
+            #ray.gcs_utils.REPORTER_CHANNEL,
+            self.redis_key,
+            simplejson.dumps(stats, namedtuple_as_object=True),
+        )
 
     def run(self):
         """Run the reporter."""
         while True:
-            self.perform_iteration()
-            time.sleep(1)
+            try:
+                self.perform_iteration()
+            except:
+                traceback.print_exc()
+                pass
 
+            time.sleep(1)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
