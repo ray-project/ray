dac19696475314fb41c6cfa4552796c8fdea9ecc
Adi Zimmerman <adizim@berkeley.edu>
2019-02-12 11:00:04 -0800

    [tune] Add Nevergrad to Tune (#3985)

--- test/jenkins_tests/run_multi_node_tests.sh
+++ test/jenkins_tests/run_multi_node_tests.sh
@@ -362,6 +362,11 @@ docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} -e SIGOPT_KEY $DO
     python /ray/python/ray/tune/examples/sigopt_example.py \
     --smoke-test
 
+# Runs only on Python3
+# docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
+#     python /ray/python/ray/tune/examples/nevergrad_example.py \
+#     --smoke-test
+
 docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
     python /ray/python/ray/tune/examples/tune_mnist_keras.py \
     --smoke-test
