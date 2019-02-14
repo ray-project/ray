15bc3d9fca32d4dd1f0d323e51f917a66820343f
Philipp Moritz <pcmoritz@gmail.com>
2019-02-13 21:54:16 -0800

    use version 0.7.0.dev0 instead

--- python/ray/__init__.py
+++ python/ray/__init__.py
@@ -81,7 +81,7 @@ import ray.actor  # noqa: F401
 from ray.actor import method  # noqa: E402
 
 # Ray version string.
-__version__ = "0.7.0dev"
+__version__ = "0.7.0.dev0"
 
 __all__ = [
     "error_info", "init", "connect", "disconnect", "get", "put", "wait",
