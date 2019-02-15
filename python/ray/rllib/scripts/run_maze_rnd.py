386b3a1af3dc666ece7cdb3e4976cbb8c62ce082
Vishal Satish <vsatish@berkeley.edu>
2019-02-14 23:11:15 -0800

    WIP testing maze.

--- python/ray/rllib/scripts/run_maze_rnd.py
+++ python/ray/rllib/scripts/run_maze_rnd.py
@@ -4,16 +4,29 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
 import numpy as np
 import gym
-from gym.spaces import Discrete, Box
-from gym.envs.registration import EnvSpec
+import gym_maze
 
 import ray
 from ray.tune import run_experiments
+from ray.tune.registry import register_env
 
 
 if __name__ == "__main__":
+
+    def create_env(config):
+        import os
+        import gym
+        import gym_maze
+
+        os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+        return gym.make("maze-random-10x10-plus-v0")
+
+#    register_env("maze-random-10x10-plus-v0", lambda config: gym.make("maze-random-10x10-plus-v0"))
+    register_env("maze-random-10x10-plus-v0", create_env)
     ray.init()
     run_experiments({
         "without-rnd-test": {
@@ -23,13 +36,10 @@ if __name__ == "__main__":
             },
             "env": "maze-random-10x10-plus-v0",
             "config": {
-                "env_config": {
-                    "corridor_length": 1000,
-                },
                 "sample_batch_size": 50,
                 "train_batch_size": 500,
                 "num_workers": 1,
-                "num_envs_per_worker": 10,
+                "num_envs_per_worker": 1,
                 "rnd": 1
             },
         },
