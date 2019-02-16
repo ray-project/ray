f2ab8c4a7c44d49785944ccdd9b191d58f2c4d42
Philipp Moritz <pcmoritz@gmail.com>
2019-02-16 13:07:17 -0800

    update

--- python/ray/test/test_modin.py
+++ python/ray/test/test_modin.py
@@ -5,12 +5,19 @@ from __future__ import print_function
 import ray  # noqa F401
 
 
+def test_modin_import_with_ray_init():
+    ray.init()
+    import modin.pandas as pd
+    frame_data = [1, 2, 3, 4, 5, 6, 7, 8]
+    frame = pd.DataFrame(frame_data)
+    assert frame.sum().squeeze() == sum(frame_data)
+
 def test_modin_import():
     import modin.pandas as pd
     frame_data = [1, 2, 3, 4, 5, 6, 7, 8]
     frame = pd.DataFrame(frame_data)
     assert frame.sum().squeeze() == sum(frame_data)
 
-
 if __name__ == '__main__':
-    test_modin_import()
+    test_modin_import_with_ray_init()
+    # test_modin_import()
