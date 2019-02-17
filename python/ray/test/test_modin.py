2bfce25296bc80d9773a9239f551f008160967c1
Philipp Moritz <pcmoritz@gmail.com>
2019-02-17 14:08:08 -0800

    update

--- python/ray/test/test_modin.py
+++ python/ray/test/test_modin.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import pytest
+
 import ray  # noqa F401
 
 
@@ -12,14 +14,12 @@ def test_modin_import_with_ray_init():
     frame = pd.DataFrame(frame_data)
     assert frame.sum().squeeze() == sum(frame_data)
 
+
+# The following can be activated once we have a modin release that
+# includes https://github.com/modin-project/modin/pull/472.
+@pytest.mark.skip(reason="needs #472 in modin")
 def test_modin_import():
     import modin.pandas as pd
     frame_data = [1, 2, 3, 4, 5, 6, 7, 8]
     frame = pd.DataFrame(frame_data)
     assert frame.sum().squeeze() == sum(frame_data)
-
-if __name__ == "__main__":
-    test_modin_import_with_ray_init()
-    # The following can be activated once we have a modin release that
-    # includes https://github.com/modin-project/modin/pull/472.
-    # test_modin_import()
