59d3f3982eec974cecca51f405e8935c71db0994
Eric Liang <ekhliang@gmail.com>
2019-02-17 19:12:45 -0800

    wip

--- python/ray/rllib/models/model.py
+++ python/ray/rllib/models/model.py
@@ -58,6 +58,11 @@ class Model(object):
         self.state_init = []
         self.state_in = state_in or []
         self.state_out = []
+        self.obs_space = obs_space
+        self.num_outputs = num_outputs
+        self.options = options
+        self.scope = tf.get_variable_scope()
+        self.session = tf.get_default_session()
         if seq_lens is not None:
             self.seq_lens = seq_lens
         else:
@@ -139,17 +144,30 @@ class Model(object):
             linear(self.last_layer, 1, "value", normc_initializer(1.0)), [-1])
 
     @PublicAPI
-    def loss(self):
-        """Builds any built-in (self-supervised) loss for the model.
+    def custom_loss(self, policy_loss):
+        """Override to customize the loss function used to optimize this model.
 
-        For example, this can be used to incorporate auto-encoder style losses.
-        Note that this loss has to be included in the policy graph loss to have
-        an effect (done for built-in algorithms).
+        This can be used to incorporate self-supervised losses (by defining
+        a loss over existing input and output tensors of this model), and
+        supervised losses (by defining losses over a variable-sharing copy of
+        this model's layers).
+
+        You can find an runnable example in examples/supervised_loss.py.
+        
+        Arguments:
+            policy_loss (Tensor): scalar policy loss from the policy graph.
 
         Returns:
-            Scalar tensor for the self-supervised loss.
+            Scalar tensor for the customized loss for this model.
         """
-        return tf.constant(0.0)
+        if self.loss() is not None:
+            raise DeprecationWarning(
+                "self.loss() is deprecated, use self.custom_loss() instead.")
+        return policy_loss
+
+    def loss(self):
+        """Deprecated: use self.custom_loss()."""
+        return None
 
     def _validate_output_shape(self):
         """Checks that the model has the correct number of outputs."""
--- python/ray/rllib/offline/input_reader.py
+++ python/ray/rllib/offline/input_reader.py
@@ -2,7 +2,11 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import logging
+import threading
+
 from ray.rllib.utils.annotations import PublicAPI
+logger = logging.getLogger(__name__)
 
 
 @PublicAPI
@@ -17,3 +21,70 @@ class InputReader(object):
             SampleBatch or MultiAgentBatch read.
         """
         raise NotImplementedError
+
+    @PublicAPI
+    def tf_input_ops(self, queue_size=1):
+        """Returns TensorFlow queue ops for reading inputs from this reader.
+
+        The main use of these ops is for integration into custom model losses.
+        For example, you can use tf_input_ops() to read from files of external
+        experiences to add an imitation learning loss to your model.
+
+        Example:
+            >>> class MyModel(rllib.model.Model):
+            ...     def custom_loss(self, policy_loss):
+            ...         reader = JsonReader(...)
+            ...         input_ops = reader.tf_input_ops()
+            ...         self.scope.reuse_variables()
+            ...         logits, _ = self._build_layers_v2(
+            ...             _restore_original_dimensions(
+            ...                 {"obs": input_ops["obs"]}, self.obs_space),
+            ...                 self.num_outputs, self.options)
+            ...         il_loss = imitation_loss(logits, input_ops["action"])
+            ...         return policy_loss + il_loss
+            
+        You can find a runnable version of this in examples/supervised_loss.py.
+        
+        Returns:
+            dict of Tensors (single-agent), or dict keyed by policy ids of
+            these Tensors (multi-agent mode).
+        """
+
+        if hasattr(self, "_queue_runner"):
+            raise ValueError(
+                "A queue runner already exists for this input reader. "
+                "You can only call tf_input_ops() once per reader.")
+        batch = self.next()
+        keys = sorted(list(batch.keys()))
+        dtypes = [batch[k].dtype for k in keys]
+        queue = tf.FIFOQueue(capacity=queue_size, dtypes=dtypes, names=keys)
+        tensors = queue.dequeue()
+        self._queue_runner = _QueueRunner(self, queue, keys, dtypes)
+        self._queue_runner.start()
+        return {
+            k: tensors[i] for i, k in enumerate(keys)
+        }
+
+
+class _QueueRunner(threading.Thread):
+    def __init__(self, input_reader, queue, keys, dtypes)
+        threading.Thread.__init__(self)
+        self.sess = tf.get_default_session()
+        self.daemon = True
+        self.input_reader = input_reader
+        self.keys = keys
+        self.queue = queue
+        self.placeholders = [tf.Placeholder(dtype) for dtype in dtypes]
+        self.enqueue_op = queue.enqueue(self.placeholders)
+
+    def run(self):
+        while True:
+            try:
+                batch = self.input_reader.next()
+                self.sess.run(
+                    self.enqueue_op, feed_dict={
+                        self.placeholders[i]: batch[keys[i]]
+                        for i in range(self.keys)
+                    })
+            except:
+                logger.exception("Error reading from input")
