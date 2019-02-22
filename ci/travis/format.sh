644d56a78ab8eefe4ea638c869911081c202e680
Eric Liang <ekhliang@gmail.com>
2019-02-22 10:57:54 -0800

    fix it all

--- ci/travis/format.sh
+++ ci/travis/format.sh
@@ -55,8 +55,13 @@ format_changed() {
             git diff --name-only --diff-filter=ACM "$MERGEBASE" -- '*.py' | xargs -P 5 \
                  flake8 --exclude=python/ray/core/generated/,doc/source/conf.py,python/ray/cloudpickle/ \
                     --ignore=C408,E121,E123,E126,E226,E24,E704,W503,W504,W605
-            git diff --name-only --diff-filter=ACM "$MERGEBASE" -- '*.p*' | xargs -P 5 \
-                 flake8 --filename="*.pyx,*.pxd,*.pxi" --ignore=E211,E225,E226,E227,E999
+        fi
+    fi
+
+    if ! git diff --diff-filter=ACM --quiet --exit-code "$MERGEBASE" -- '*.pyx' '*.pxd' '*.pxi' &>/dev/null; then
+        if which flake8 >/dev/null; then
+            git diff --name-only --diff-filter=ACM "$MERGEBASE" -- '*.pyx' '*.pxd' '*.pxi' | xargs -P 5 \
+                 flake8 --ignore=E211,E225,E226,E227,E999
         fi
     fi
 
