18c76fb2652b48376ef542cd3994c66816374d24
Robert Nishihara <robertnishihara@gmail.com>
2019-02-25 23:07:11 -0800

    Add script which can be used to run infinitely long single-machine stress tests.

--- ci/long_running_tests/check_workloads.sh
+++ ci/long_running_tests/check_workloads.sh
@@ -1,1 +1,24 @@
 #!/usr/bin/env bash
+
+set -x
+
+ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
+
+pushd "$ROOT_DIR"
+
+echo "WORKLOAD 1"
+ray exec config.yaml --cluster-name workload1 "tmux capture-pane -p"
+echo ""
+echo ""
+
+echo "WORKLOAD 2"
+ray exec config.yaml --cluster-name workload2 "tmux capture-pane -p"
+echo ""
+echo ""
+
+echo "WORKLOAD 3"
+ray exec config.yaml --cluster-name workload3 "tmux capture-pane -p"
+echo ""
+echo ""
+
+popd
--- ci/long_running_tests/start_workloads.sh
+++ ci/long_running_tests/start_workloads.sh
@@ -1,7 +1,44 @@
 #!/usr/bin/env bash
 
+set -ex
+
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
-ray up -y config1.yaml
+pushd "$ROOT_DIR"
 
-ray exec config1.yaml --cluster-name workload1 'python workload1.py' --tmux
+ray up -y config.yaml --cluster-name workload1
+ray rsync_up config.yaml --cluster-name workload1 workload1.py workload1.py
+ray exec config.yaml --cluster-name workload1 "python workload1.py" --tmux
+
+ray up -y config.yaml --cluster-name workload2
+ray rsync_up config.yaml --cluster-name workload2 workload2.py workload2.py
+ray exec config.yaml --cluster-name workload2 "python workload2.py" --tmux
+
+ray up -y config.yaml --cluster-name workload3
+ray rsync_up config.yaml --cluster-name workload3 workload3.py workload3.py
+ray exec config.yaml --cluster-name workload3 "python workload3.py" --tmux
+
+popd
+
+echo ""
+echo ""
+
+echo "To kill the instances, use the following commands."
+echo ""
+echo "    ray down -y $ROOT_DIR/config.yaml --cluster-name=workload1"
+echo "    ray down -y $ROOT_DIR/config.yaml --cluster-name=workload2"
+echo "    ray down -y $ROOT_DIR/config.yaml --cluster-name=workload3"
+
+echo ""
+echo ""
+
+echo "Use the following commands to attach to the relevant drivers."
+echo ""
+echo "    ray attach $ROOT_DIR/config.yaml --cluster-name=workload1 --tmux"
+echo "    ray attach $ROOT_DIR/config.yaml --cluster-name=workload2 --tmux"
+echo "    ray attach $ROOT_DIR/config.yaml --cluster-name=workload3 --tmux"
+
+echo ""
+echo ""
+
+echo "To check up on the scripts, run $ROOT_DIR/check_workloads.sh"
