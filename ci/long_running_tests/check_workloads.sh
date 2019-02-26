4bb5c07f260df6922a9f848ed54e0aeae26b31e9
Robert Nishihara <robertnishihara@gmail.com>
2019-02-26 00:04:01 -0800

    Minor

--- ci/long_running_tests/check_workloads.sh
+++ ci/long_running_tests/check_workloads.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -x
-
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
 pushd "$ROOT_DIR"
--- ci/long_running_tests/start_workloads.sh
+++ ci/long_running_tests/start_workloads.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
