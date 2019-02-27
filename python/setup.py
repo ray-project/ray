ccaf2e787280cea77c25e957da73e0ebe5200b0d
Yuhong Guo <guoyuhong1985@outlook.com>
2019-02-28 05:05:25 +0800

    Downgrade six to 1.0.0 (#4180)

--- python/setup.py
+++ python/setup.py
@@ -152,7 +152,9 @@ requires = [
     "pytest",
     "pyyaml",
     "redis",
-    "six >= 1.12.0",
+    # NOTE: Don't upgrade the version of six! Doing so causes installation
+    # problems. See https://github.com/ray-project/ray/issues/4169.
+    "six >= 1.0.0",
     # The typing module is required by modin.
     "typing",
     "flatbuffers",
