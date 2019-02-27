f6e575191bbb0b3200b9d61891c9ad9a7118f8c2
Eric Liang <ekhliang@gmail.com>
2019-02-27 11:21:38 -0800

    Update async_samples_optimizer.py

--- python/ray/rllib/optimizers/async_samples_optimizer.py
+++ python/ray/rllib/optimizers/async_samples_optimizer.py
@@ -85,7 +85,7 @@ class AsyncSamplesOptimizer(PolicyOptimizer):
         self.learner.start()
 
         if len(self.remote_evaluators) == 0:
-            logger.warning("Starting optimizer without remote evaluators! Training will hang! Have you set num_workers to 0?")
+            logger.warning("Starting optimizer with num_workers=0. Training will hang!")
 
         # Stats
         self._optimizer_step_timer = TimerStat()
