1efb2f90e225056f9a235aa91e6bf61e6215ba4c
Philipp Moritz <pcmoritz@gmail.com>
2019-02-28 21:47:09 -0800

    put back EINTR handling

--- src/ray/common/client_connection.cc
+++ src/ray/common/client_connection.cc
@@ -52,16 +52,48 @@ template <class T>
 Status ServerConnection<T>::WriteBuffer(
     const std::vector<boost::asio::const_buffer> &buffer) {
   boost::system::error_code error;
-  boost::asio::write(socket_, buffer, error);
-  return boost_to_ray_status(error);
+  // Loop until all bytes are written while handling interrupts.
+  // When profiling with pprof, unhandled interrupts were being sent by the profiler to
+  // the raylet process, which was causing synchronous reads and writes to fail.
+  for (const auto &b : buffer) {
+    uint64_t bytes_remaining = boost::asio::buffer_size(b);
+    uint64_t position = 0;
+    while (bytes_remaining != 0) {
+      size_t bytes_written =
+          socket_.write_some(boost::asio::buffer(b + position, bytes_remaining), error);
+      position += bytes_written;
+      bytes_remaining -= bytes_written;
+      if (error.value() == EINTR) {
+        continue;
+      } else if (error.value() != boost::system::errc::errc_t::success) {
+        return boost_to_ray_status(error);
+      }
+    }
+  }
+  return ray::Status::OK();
 }
 
 template <class T>
 Status ServerConnection<T>::ReadBuffer(
     const std::vector<boost::asio::mutable_buffer> &buffer) {
   boost::system::error_code error;
-  boost::asio::read(socket_, buffer, error);
-  return boost_to_ray_status(error);
+  // Loop until all bytes are read while handling interrupts.
+  for (const auto &b : buffer) {
+    uint64_t bytes_remaining = boost::asio::buffer_size(b);
+    uint64_t position = 0;
+    while (bytes_remaining != 0) {
+      size_t bytes_read =
+          socket_.read_some(boost::asio::buffer(b + position, bytes_remaining), error);
+      position += bytes_read;
+      bytes_remaining -= bytes_read;
+      if (error.value() == EINTR) {
+        continue;
+      } else if (error.value() != boost::system::errc::errc_t::success) {
+        return boost_to_ray_status(error);
+      }
+    }
+  }
+  return Status::OK();
 }
 
 template <class T>
