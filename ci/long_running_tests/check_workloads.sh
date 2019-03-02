700195bddd8b2dc6d7cbbf9bdc36b67be009504f
Eric Liang <ekhliang@gmail.com>
2019-03-01 18:24:15 -0800

    add load flag

--- ci/long_running_tests/check_workloads.sh
+++ ci/long_running_tests/check_workloads.sh
@@ -4,21 +4,33 @@
 
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
-pushd "$ROOT_DIR"
+if [ "$1" == "--load" ]; then
+    check_load=true
+elif [ "$1" == "--logs" ]; then
+    check_load=false
+else
+    echo "Usage: $0 [--load|--logs]"
+    exit 1
+fi
+
+cd "$ROOT_DIR"
 
 for workload_file in "$ROOT_DIR"/workloads/*; do
   file_name=$(basename -- $workload_file)
   workload_name="${file_name%.*}"
-  echo "======================================================================"
-  echo "WORKLOAD: $workload_name"
-  echo "======================================================================"
+  if $check_load; then
+    echo -n "$workload_name: "
+    ray --logging-level=WARNING exec config.yaml --cluster-name="$workload_name" uptime 2>/dev/null
+  else
+    echo "======================================================================"
+    echo "WORKLOAD: $workload_name"
+    echo "======================================================================"
 
-  ray exec config.yaml --cluster-name="$workload_name" "tmux capture-pane -p"
-  echo ""
-  echo "ssh to this machine with:"
-  echo "    ray attach $ROOT_DIR/config.yaml --cluster-name=$workload_name"
-  echo ""
-  echo ""
+    ray exec config.yaml --cluster-name="$workload_name" "tmux capture-pane -p"
+    echo ""
+    echo "ssh to this machine with:"
+    echo "    ray attach $ROOT_DIR/config.yaml --cluster-name=$workload_name"
+    echo ""
+    echo ""
+  fi
 done
-
-popd
