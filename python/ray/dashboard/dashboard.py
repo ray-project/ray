b797633f010e2d375a583b35163a15c8094345c3
Daniel Edgecumbe <daniel@longshotsystems.co.uk>
2019-03-05 17:32:08 +0000

    [autoscaler] Add an aggressive_autoscaling flag
    This modifies the behaviour of the autoscaler such that it more aggressively
        autoscales if enabled.

--- python/ray/dashboard/dashboard.py
+++ python/ray/dashboard/dashboard.py
@@ -141,6 +141,7 @@ class Dashboard(object):
                 "min_workers": cfg["min_workers"],
                 "max_workers": cfg["max_workers"],
                 "initial_workers": cfg["initial_workers"],
+                "aggressive_autoscaling": cfg["aggressive_autoscaling"],
                 "idle_timeout_minutes": cfg["idle_timeout_minutes"],
             }
 
