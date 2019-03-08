d834008778fb47d182a313bbd054010b6143388b
Robert Nishihara <robertnishihara@gmail.com>
2019-03-07 16:01:09 -0800

    Linting

--- ci/travis/determine_tests_to_run.py
+++ ci/travis/determine_tests_to_run.py
@@ -4,28 +4,32 @@ from __future__ import division
 from __future__ import print_function
 
 import os
-import sys
 import subprocess
+
 
 def list_changed_files(commit_range):
     """
     Returns the list of files changed within commit_range.
     """
     command = ["git", "diff", "--name-only", commit_range]
-    proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     out, err = proc.communicate()
     if proc.returncode != 0:
         raise RuntimeError("Command {cmdline} failed with code {returncode}, "
-                           "stderr was:\n{stderr}\n"
-                           .format(cmdline=command, returncode=proc.returncode,
-                                   stderr=err.decode()))
+                           "stderr was:\n{stderr}\n".format(
+                               cmdline=command,
+                               returncode=proc.returncode,
+                               stderr=err.decode()))
 
     return list(filter(None, (s.strip() for s in out.decode().splitlines())))
 
+
 if __name__ == "__main__":
 
-    files = list_changed_files(os.environ['TRAVIS_COMMIT_RANGE'].replace('...', '..'))
+    files = list_changed_files(os.environ['TRAVIS_COMMIT_RANGE'].replace(
+        '...', '..'))
 
     RAY_CI_TUNE_AFFECTED = "0"
     RAY_CI_RLLIB_AFFECTED = "0"
@@ -53,7 +57,8 @@ if __name__ == "__main__":
             RAY_CI_MACOS_WHEELS_AFFECTED = "1"
         elif changed_file.startswith("src/"):
             pass
-        elif changed_file.startswith("java/") and not file.startswith("java/doc/"):
+        elif changed_file.startswith(
+                "java/") and not changed_file.startswith("java/doc/"):
             RAY_CI_JAVA_AFFECTED = "1"
         elif changed_file.startswith("doc/"):
             # nothing is run but linting in this case
@@ -82,5 +87,7 @@ if __name__ == "__main__":
     print("export RAY_CI_DOC_AFFECTED=\"%s\"" % RAY_CI_DOC_AFFECTED)
     print("export RAY_CI_JAVA_AFFECTED=\"%s\"" % RAY_CI_JAVA_AFFECTED)
     print("export RAY_CI_PYTHON_AFFECTED=\"%s\"" % RAY_CI_PYTHON_AFFECTED)
-    print("export RAY_CI_LINUX_WHEELS_AFFECTED=\"%s\"" % RAY_CI_LINUX_WHEELS_AFFECTED)
-    print("export RAY_CI_MACOS_WHEELS_AFFECTED=\"%s\"" % RAY_CI_MACOS_WHEELS_AFFECTED)
+    print("export RAY_CI_LINUX_WHEELS_AFFECTED=\"%s\"" %
+          RAY_CI_LINUX_WHEELS_AFFECTED)
+    print("export RAY_CI_MACOS_WHEELS_AFFECTED=\"%s\"" %
+          RAY_CI_MACOS_WHEELS_AFFECTED)
