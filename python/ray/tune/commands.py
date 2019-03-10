2b6dc4fa7747f603622e3e5d3ceb660f4f45960e
Andrew Tan <andrewztan12@gmail.com>
2019-03-10 01:26:38 -0800

    added output tag

--- python/ray/tune/commands.py
+++ python/ray/tune/commands.py
@@ -116,6 +116,7 @@ def _get_experiment_state(experiment_path, exit_on_fail=False):
 
 def list_trials(experiment_path,
                 sort=None,
+                output=None,
                 info_keys=DEFAULT_EXPERIMENT_INFO_KEYS,
                 result_keys=DEFAULT_RESULT_KEYS):
     """Lists trials in the directory subtree starting at the given path.
@@ -124,6 +125,7 @@ def list_trials(experiment_path,
         experiment_path (str): Directory where trials are located.
             Corresponds to Experiment.local_dir/Experiment.name.
         sort (str): Key to sort by.
+        output (str): Name of pickle file where output is saved.
         info_keys (list): Keys that are displayed.
         result_keys (list): Keys of last result that are displayed.
     """
@@ -162,9 +164,16 @@ def list_trials(experiment_path,
 
     print_format_output(checkpoints_df)
 
+    if output:
+        experiment_path = os.path.expanduser(experiment_path)
+        output_filepath = os.path.join(experiment_path, output + ".pkl")
+        checkpoints_df.to_pickle(output_filepath)
+        print("Output saved at:", output_filepath)
+
 
 def list_experiments(project_path,
                      sort=None,
+                     output=None,
                      info_keys=DEFAULT_PROJECT_INFO_KEYS):
     """Lists experiments in the directory subtree.
 
@@ -172,6 +181,7 @@ def list_experiments(project_path,
         project_path (str): Directory where experiments are located.
             Corresponds to Experiment.local_dir.
         sort (str): Key to sort by.
+        output (str): Name of pickle file where output is saved.
         info_keys (list): Keys that are displayed.
     """
     _check_tabulate()
@@ -233,6 +243,10 @@ def list_experiments(project_path,
 
     print_format_output(info_df)
 
+    if output:
+        output_filepath = os.path.join(base, output + ".pkl")
+        info_df.to_pickle(output_filepath)
+        print("Output saved at:", output_filepath)
 
 def add_note(path, filename="note.txt"):
     """Opens a txt file at the given path where user can add and save notes.
