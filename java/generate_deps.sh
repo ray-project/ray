61d0eb9b2594dbd2f04dab48b64086ffeb183578
ruifang.crf <ruifang.crf@alibaba-inc.com>
2019-03-11 20:55:23 +0800

    modify the error 'pushd not find'

--- java/generate_deps.sh
+++ java/generate_deps.sh
@@ -1,11 +1,12 @@
-#/bin/bash
+#!/usr/bin/env bash
 
 set -e
-# Show explicitly which commands are currently running.
 set -x
 
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
-pushd $ROOT_DIR/..
+cd $ROOT_DIR/..
 bazel run //java:bazel_deps -- generate -r `pwd` -s java/third_party/workspace.bzl -d java/dependencies.yaml
-popd
+
+set +x
+set +e
