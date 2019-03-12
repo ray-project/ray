f59d876f4e57c142015724bfb007de430c9681dc
Devin Petersohn <devin.petersohn@gmail.com>
2019-03-11 20:01:11 -0700

    Fix tests

--- ci/jenkins_tests/perf_integration_tests/run_perf_integration.sh
+++ ci/jenkins_tests/perf_integration_tests/run_perf_integration.sh
@@ -3,10 +3,12 @@
 # Show explicitly which commands are currently running.
 set -ex
 
-pytest --benchmark-autosave python/ray/tests/test_perf_integration.py
+python -m pytest --benchmark-autosave python/ray/tests/test_perf_integration.py
 
 # This is how Modin stores the values in an S3 bucket
 #sha_tag=`git rev-parse --verify --short HEAD`
 # save the results to S3
 #aws s3 cp .benchmarks/*/*.json s3://modin-jenkins-result/${sha_tag}-perf-${BUCKET_SUFFIX}/ --acl public-read
 #rm -rf .benchmarks
+cat .benchmarks/*/*.json
+rm -rf .benchmarks
--- ci/jenkins_tests/perf_integration_tests/setup_perf_integration.sh
+++ ci/jenkins_tests/perf_integration_tests/setup_perf_integration.sh
@@ -5,5 +5,3 @@ set -ex
 sha_tag=`git rev-parse --verify --short HEAD`
 docker build -t ray-project/perf-test:$sha_tag -f docker/performance_test/Dockerfile .
 docker run --rm --shm-size=5g ray-project/perf-test:$sha_tag
-
-cat .benchmarks/*/*.json
