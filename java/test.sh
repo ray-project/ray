af00ed900a08af63620b2b83898c71c64b7ac3ca
ruifang.crf <ruifang.crf@alibaba-inc.com>
2019-03-12 21:08:30 +0800

    add soft link to some path that cmake used

--- java/test.sh
+++ java/test.sh
@@ -17,7 +17,14 @@ bazel test //java:all -c opt
 bazel build //java:all -c opt --verbose_failures
 popd
 
-pushd $ROOT_DIR/../java/test
+# The following are soft links
+# TODO: remove this once cmake is removed
+mkdir -p $ROOT_DIR/../build/java/
+ln -sf $ROOT_DIR/../bazel-bin/java/* $ROOT_DIR/../build/java/
+mkdir -p $ROOT_DIR/tutorial/target/
+ln -sf $ROOT_DIR/../bazel-bin/java/org_ray_ray_tutorial_deploy.jar $ROOT_DIR/tutorial/target/ray-tutorial-0.1-SNAPSHOT.jar
+
+pushd $ROOT_DIR/test
 echo "Running tests under cluster mode."
 ENABLE_MULTI_LANGUAGE_TESTS=1 java -jar -Dray.home=$ROOT_DIR/../ $ROOT_DIR/../bazel-bin/java/all_tests_deploy.jar $ROOT_DIR/../java/testng.xml
 
