1e5333481df4d1e5b87b7008d5611712324b68e2
Devin Petersohn <devin.petersohn@gmail.com>
2019-03-13 13:12:54 -0700

    Fix script

--- ci/jenkins_tests/perf_integration_tests/setup_perf_integration.sh
+++ ci/jenkins_tests/perf_integration_tests/setup_perf_integration.sh
@@ -2,6 +2,13 @@
 
 set -ex
 
-sha_tag=`git rev-parse --verify --short HEAD`
-docker build -t ray-project/perf-test:$sha_tag -f docker/performance_test/Dockerfile .
-docker run --rm --shm-size=5g ray-project/perf-test:$sha_tag
+MEMORY_SIZE="20G"
+SHM_SIZE="20G"
+
+ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
+
+DOCKER_SHA=$($ROOT_DIR/../../build-docker.sh --output-sha --no-cache)
+SUPPRESS_OUTPUT=$ROOT_DIR/../suppress_output
+echo "Using Docker image" $DOCKER_SHA
+
+docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA ./ci/perf_integration_tests/setup_perf_integration.sh
