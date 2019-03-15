16d9dd609c64ae87402cb071c026a0509526367f
Jovan <jovany.wq@antfin.com>
2019-03-15 16:24:06 +0800

    Fix function manager test
    WIP
    
    WIP
    
    WIP
    
    WIP

---
+++ java/runtime/src/main/resources/DemoApp.java
@@ -0,0 +1,27 @@
+package demo;
+
+import org.ray.api.Ray;
+import org.ray.api.RayObject;
+import org.ray.api.annotation.RayRemote;
+
+public class DemoApp {
+
+  @RayRemote
+  public static String sayHello() {
+    String ret = "hello";
+    return ret;
+  }
+
+  public static void main(String []args) {
+    try {
+      Ray.init();
+      RayObject<String> hello = Ray.call(DemoApp::sayHello);
+      System.out.println(hello.get());
+    } catch (Throwable t) {
+      t.printStackTrace();
+    } finally {
+      Ray.shutdown();
+    }
+  }
+
+}
