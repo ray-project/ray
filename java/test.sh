40ceb4e2fab6bdf40630aac353143d8707c57ec3
ruifang.crf <ruifang.crf@alibaba-inc.com>
2019-03-19 12:32:46 +0800

    modify with raulchen's suggestions

--- java/test.sh
+++ java/test.sh
@@ -7,21 +7,15 @@ set -x
 
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
-# run this file before compile the targets
-sh $ROOT_DIR/generate_deps.sh
-
-echo "Compiling Java code."
 pushd $ROOT_DIR/..
-# Compile all the targets.
-bazel build //java:all_modules --verbose_failures
-# Lint Java code with checkstyle.
+echo "Linting Java code with checkstyle."
 bazel test //java:all --test_tag_filters="checkstyle"
 
 echo "Running tests under cluster mode."
 ENABLE_MULTI_LANGUAGE_TESTS=1 bazel test //java:all_tests --test_output="errors" || cluster_exit_code=$?
 
 # exit_code == 2 means there are some tests skiped.
-if [ $cluster_exit_code -eq 2 ] && [ $cluster_exit_code -eq 0 ] ; then
+if [ $cluster_exit_code -ne 2 ] && [ $cluster_exit_code -ne 0 ] ; then
     exit $exit_code
 fi
 
@@ -29,7 +23,7 @@ echo "Running tests under single-process mode."
 bazel test //java:all_tests --jvmopt="-Dray.run-mode=SINGLE_PROCESS" --test_output="errors" || single_exit_code=$?
 
 # exit_code == 2 means there are some tests skiped.
-if [ $single_exit_code -eq 2 ] && [ $single_exit_code -eq 0 ] ; then
+if [ $single_exit_code -ne 2 ] && [ $single_exit_code -ne 0 ] ; then
     exit $exit_code
 fi
 
