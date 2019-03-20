d9cbaf5d8dbfe16013f43babe0e32468bc3c84eb
Devin Petersohn <devin.petersohn@gmail.com>
2019-03-19 19:52:33 -0700

    Fix script

--- ci/jenkins_tests/perf_integration_tests/run_perf_integration.sh
+++ ci/jenkins_tests/perf_integration_tests/run_perf_integration.sh
@@ -7,6 +7,18 @@ python -m pip install pytest-benchmark
 
 python -m pytest --benchmark-autosave python/ray/tests/test_perf_integration.py
 
+python -c "import sys; print(sys.version_info)"
+
+cp -R .benchmarks current_pr_benchmarks
+
+pip install -U https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-0.7.0.dev1-cp36-cp36m-manylinux1_x86_64.whl
+python -m pytest --benchmark-autosave python/ray/tests/test_perf_integration.py
+
+echo "Old"
+cat .benchmarks/*/*.json
+echo "Current PR"
+cat current_pr_benchmarks/*/*.json
+
 # This is how Modin stores the values in an S3 bucket
 #sha_tag=`git rev-parse --verify --short HEAD`
 # save the results to S3
