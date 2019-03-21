5d2ab7d6da58fdf59707d975dace5667251ee553
Hao Chen <chenh1024@gmail.com>
2019-03-21 11:15:59 +0800

    refine comment

--- java/runtime/src/main/java/org/ray/runtime/functionmanager/FunctionDescriptor.java
+++ java/runtime/src/main/java/org/ray/runtime/functionmanager/FunctionDescriptor.java
@@ -1,9 +1,10 @@
 package org.ray.runtime.functionmanager;
 
 /**
- * Base interface of a function descriptor of a Ray task.
+ * Base interface of a Ray task's function descriptor.
  *
- * A function descriptor is a list of strings that can uniquely describe a function.
+ * A function descriptor is a list of strings that can uniquely describe a function. It's used to
+ * load a function in workers.
  */
 public interface FunctionDescriptor {
 
