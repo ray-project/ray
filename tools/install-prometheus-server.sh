1aa732e914ecdf073fe342008bcb01a21a8254ff
Robert Nishihara <robertnishihara@gmail.com>
2019-03-31 21:06:38 -0700

    Fix script

--- tools/install-prometheus-server.sh
+++ tools/install-prometheus-server.sh
@@ -8,6 +8,7 @@ TOOLS_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 pushd $TOOLS_DIR
 
 # Download Prometheus server.
+unamestr="$(uname)"
 if [[ "$unamestr" == "Linux" ]]; then
     echo "Downloading Prometheus server for linux system."
     PACKAGE_NAME=prometheus-2.8.0.linux-amd64
