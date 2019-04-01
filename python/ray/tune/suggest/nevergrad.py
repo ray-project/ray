f92bfbbc96d91065b10b967336fef9eab9cc7298
Jeremy Rapin <jrapin@fb.com>
2019-04-01 14:31:09 +0200

    Add compatibility to nevergrad 0.2.0+

--- python/ray/tune/suggest/nevergrad.py
+++ python/ray/tune/suggest/nevergrad.py
@@ -23,7 +23,8 @@ class NevergradSearch(SuggestionAlgorithm):
         optimizer (nevergrad.optimization.Optimizer): Optimizer provided
             from Nevergrad.
         parameter_names (list): List of parameter names. Should match
-            the dimension of the optimizer output.
+            the dimension of the optimizer output. Alternatively, set to None
+            if the optimizer is already instrumented with kwargs (see nevergrad v0.2.0+).
         max_concurrent (int): Number of maximum concurrent trials. Defaults
             to 10.
         reward_attr (str): The training result objective value attribute.
@@ -31,9 +32,21 @@ class NevergradSearch(SuggestionAlgorithm):
 
     Example:
         >>> from nevergrad.optimization import optimizerlib
-        >>> optimizer = optimizerlib.OnePlusOne(dimension=1, budget=100)
+        >>> instrumentation = 1
+        >>> optimizer = optimizerlib.OnePlusOne(instrumentation, budget=100)
         >>> algo = NevergradSearch(
-        >>>     optimizer, max_concurrent=4, reward_attr="neg_mean_loss")
+        >>>     optimizer, ["lr"], max_concurrent=4, reward_attr="neg_mean_loss")
+
+    Note:
+    In v0.2.0+ of nevergrad, optimizers can be instrumented. For instance, this will specifies searching for
+    "lr" from 1 to 2.
+        >>> from nevergrad.optimization import optimizerlib
+        >>> from nevergrad import instrumentation as inst
+        >>> instrumentation = inst.Instrumentation(lr=inst.var.Array(1).bounded(1, 2).asfloat())
+        >>> optimizer = optimizerlib.OnePlusOne(instrumentation, budget=100)
+        >>> algo = NevergradSearch(
+        >>>     optimizer, None, max_concurrent=4, reward_attr="neg_mean_loss")
+
     """
 
     def __init__(self,
@@ -50,12 +63,33 @@ class NevergradSearch(SuggestionAlgorithm):
         self._nevergrad_opt = optimizer
         self._live_trial_mapping = {}
         super(NevergradSearch, self).__init__(**kwargs)
+        # validate parameters
+        if hasattr(optimizer, "instrumentation"):  # added in v0.2.0
+            if optimizer.instrumentation.kwargs:
+                if optimizer.instrumentation.args:
+                    raise ValueError("Instrumented optimizers should use kwargs only")
+                if parameter_names is not None:
+                    raise ValueError("Instrumented optimizers should provide None as parameter_names")
+            else:
+                if parameter_names is None:
+                    raise ValueError("Non-instrumented optimizers should have a list of parameter_names")
+                if len(optimizer.instrumentation.args) != 1:
+                    raise ValueError("Instrumented optimizers should use kwargs only")
+        if parameter_names is not None and optimizer.dimension != len(parameter_names):
+            raise ValueError("len(parameters_names) must match optimizer dimension for non-instrumented optimizers")
 
     def _suggest(self, trial_id):
         if self._num_live_trials() >= self._max_concurrent:
             return None
         suggested_config = self._nevergrad_opt.ask()
         self._live_trial_mapping[trial_id] = suggested_config
+        # in v0.2.0+, output of ask() is a Candidate, with fields args and kwargs
+        if hasattr(self._nevergrad_opt, "instrumentation"):
+            if not suggested_config.kwargs:
+                return dict(zip(self._parameters, suggested_config.args[0]))
+            else:
+                return suggested_config.kwargs
+        # legacy: output of ask() is a np.ndarray
         return dict(zip(self._parameters, suggested_config))
 
     def on_trial_result(self, trial_id, result):
