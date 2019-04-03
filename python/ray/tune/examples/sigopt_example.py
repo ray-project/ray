9b369a41e93b27d6f217c72c3e91aaa616154651
Adi Zimmerman <adizim@berkeley.edu>
2019-04-03 08:52:45 -0700

    [tune] Search Alg delay import + CLI timing test (#4230)

--- python/ray/tune/examples/sigopt_example.py
+++ python/ray/tune/examples/sigopt_example.py
@@ -9,7 +9,7 @@ from __future__ import print_function
 import ray
 from ray.tune import run
 from ray.tune.schedulers import AsyncHyperBandScheduler
-from ray.tune.suggest import SigOptSearch
+from ray.tune.suggest.sigopt import SigOptSearch
 
 
 def easy_objective(config, reporter):
