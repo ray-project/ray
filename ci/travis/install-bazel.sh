74912bff4f732ab4bd79d0f666bf9b40bfd43491
cclauss <cclauss@me.com>
2019-04-04 16:51:52 +0200

    ${ext:-sh}

--- ci/travis/install-bazel.sh
+++ ci/travis/install-bazel.sh
@@ -14,12 +14,13 @@ elif [[ "$unamestr" == "Darwin" ]]; then
 elif [[ "$unamestr" == "MSYS_NT-10.0" ]]; then
   echo "Platform is Windows."
   platform="windows"
+  ext="exe"
 else
   echo "$unamestr is an unrecognized platform."
   exit 1
 fi
 
-URL="https://github.com/bazelbuild/bazel/releases/download/0.21.0/bazel-0.21.0-installer-${platform}-x86_64.sh"
+URL="https://github.com/bazelbuild/bazel/releases/download/0.21.0/bazel-0.21.0-installer-${platform}-x86_64.${ext:-sh}"
 wget -O install.sh $URL
 chmod +x install.sh
 ./install.sh --user
