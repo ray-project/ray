b347e1c06e5cb2b49d93791db2c516a1013d9b84
Eric Liang <ekhliang@gmail.com>
2019-04-03 21:56:11 -0700

    restore legacy agent class

---
+++ python/ray/rllib/agents/agent.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from ray.rllib.agents.trainer import Trainer
+
+# TODO(ekl) remove legacy agent name
+Agent = Trainer
