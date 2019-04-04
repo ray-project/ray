b75052e30ffa213906ddb313e3550eeb22b06baa
Philipp Moritz <pcmoritz@gmail.com>
2019-04-04 15:23:18 -0700

    update

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -543,7 +543,7 @@ class Worker(object):
         assert len(final_results) == len(object_ids)
         return final_results
 
-    def submit_task(self, function_descriptor_list, args, **kwargs):
+    def submit_task(self, function_descriptor_list, func_args, func_kwargs, **kwargs):
         """Submit a remote task to the scheduler.
 
         Tell the scheduler to schedule the execution of the function with
@@ -584,7 +584,7 @@ class Worker(object):
                 "submit_task",
                 enable=_global_node._ray_params.enable_profiling):
             return self.raylet_client.submit_task(
-                function_descriptor_list, args, self.current_task_id,
+                function_descriptor_list, func_args, func_kwargs, self.current_task_id,
                 self.task_context.task_index, put, **kwargs)
 
     def run_function_on_all_workers(self, function,
