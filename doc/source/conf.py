5bddc8ce43448ecb01624d738cd02a821eac4ba0
Richard Liaw <rliaw@berkeley.edu>
2019-04-03 19:13:53 -0700

    changes

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -74,6 +74,7 @@ sys.path.insert(0, os.path.abspath("../../python/"))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'sphinx_click.ext',
 ]
@@ -222,7 +223,7 @@ html_sidebars = {'**': ['index.html']}
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-html_show_sourcelink = True
+#html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #html_show_sphinx = True
