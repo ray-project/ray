820c71b7d018de7b947139d85029b39fea1e626e
Dušan Josipović <dusanix@gmail.com>
2019-04-07 05:01:54 +0200

    [tune/rllib] Add checkpoint eraser (#4490)

--- python/ray/rllib/train.py
+++ python/ray/rllib/train.py
@@ -104,6 +104,8 @@ def run(args, parser):
             args.experiment_name: {  # i.e. log to ~/ray_results/default
                 "run": args.run,
                 "checkpoint_freq": args.checkpoint_freq,
+                "keep_checkpoints_num": args.keep_checkpoints_num,
+                "checkpoint_score_attr": args.checkpoint_score_attr,
                 "local_dir": args.local_dir,
                 "resources_per_trial": (
                     args.resources_per_trial and
