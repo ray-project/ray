d4d138ade2f05edf5681165320080f0749f3cc68
Adi <adizim@berkeley.edu>
2019-04-08 09:20:44 -0700

    setup teardown

--- python/ray/tune/analysis/test_experiment_analysis.py
+++ python/ray/tune/analysis/test_experiment_analysis.py
@@ -4,41 +4,53 @@ from __future__ import print_function
 
 import unittest
 import subprocess
+import shutil
 
 from experiment_analysis import *
 from ray.tune.examples.async_hyperband_example import MyTrainableClass
 
+test_dir = "~/analysis_test"
+test_name = "analysis_exp"
+
+def run_test_exp():
+    ray.init()
+
+    ahb = AsyncHyperBandScheduler(
+        time_attr="training_iteration",
+        reward_attr="episode_reward_mean",
+        grace_period=5,
+        max_t=100)
+
+    run(MyTrainableClass,
+        name=test_name,
+        scheduler=ahb,
+        local_dir=test_dir,
+        **{
+            "stop": {
+                "training_iteration": 100
+            },
+            "num_samples": 20,
+            "resources_per_trial": {
+                "cpu": 1,
+                "gpu": 0
+            },
+            "config": {
+                "width": sample_from(
+                    lambda spec: 10 + int(90 * random.random())),
+                "height": sample_from(lambda spec: int(100 * random.random())),
+            },
+        })
+
+def remove_test_exp():
+    exp_dir = os.path.join(test_dir, test_name)
+    shutil.rmtree(exp_dir)
+
 class ExperimentAnalysisSuite(unittest.TestCase):
     def setup(self):
-        ray.init()
-
-        ahb = AsyncHyperBandScheduler(
-            time_attr="training_iteration",
-            reward_attr="episode_reward_mean",
-            grace_period=5,
-            max_t=100)
-
-        run(MyTrainableClass,
-            name="analysis_test",
-            scheduler=ahb,
-            **{
-                "stop": {
-                    "training_iteration": 100
-                },
-                "num_samples": 20,
-                "resources_per_trial": {
-                    "cpu": 1,
-                    "gpu": 0
-                },
-                "config": {
-                    "width": sample_from(
-                        lambda spec: 10 + int(90 * random.random())),
-                    "height": sample_from(lambda spec: int(100 * random.random())),
-                },
-            })
+        run_test_exp()
 
     def tearDown(self):
-        pass
+        remove_test_exp()
 
     def testDataframe(self):
         pass
