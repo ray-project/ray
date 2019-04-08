1743ab8d31b71394b6c0420b2218023e47422b0a
Robert Nishihara <robertnishihara@gmail.com>
2019-04-08 14:31:51 -0700

    Enforce quoting style in Travis.

--- python/ray/tests/test_autoscaler.py
+++ python/ray/tests/test_autoscaler.py
@@ -388,7 +388,7 @@ class AutoscalingTest(unittest.TestCase):
         rtc1.clear()
         autoscaler.update()
         # Synchronization: wait for launchy thread to be blocked on rtc1
-        if hasattr(rtc1, '_cond'):  # Python 3.5
+        if hasattr(rtc1, "_cond"):  # Python 3.5
             waiters = rtc1._cond._waiters
         else:  # Python 2.7
             waiters = rtc1._Event__cond._Condition__waiters
