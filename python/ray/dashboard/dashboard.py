965e163af8e4592e62f23fa6326f4d539513a6b5
Daniel Edgecumbe <daniel@longshotsystems.co.uk>
2019-04-08 12:53:11 +0100

    Fix autoscaling_mode in dashboard

--- python/ray/dashboard/dashboard.py
+++ python/ray/dashboard/dashboard.py
@@ -141,7 +141,7 @@ class Dashboard(object):
                 "min_workers": cfg["min_workers"],
                 "max_workers": cfg["max_workers"],
                 "initial_workers": cfg["initial_workers"],
-                "aggressive_autoscaling": cfg["aggressive_autoscaling"],
+                "autoscaling_mode": cfg["autoscaling_mode"],
                 "idle_timeout_minutes": cfg["idle_timeout_minutes"],
             }
 
