9dea9be095fd5afccad945798e27e6ba455147f9
Devin Petersohn <devin.petersohn@gmail.com>
2019-04-09 14:07:14 -0700

    Address comments

--- python/ray/tests/perf_integration_tests/test_perf_integration.py
+++ python/ray/tests/perf_integration_tests/test_perf_integration.py
@@ -20,16 +20,21 @@ def benchmark_task_submission(num_tasks):
     ray.get([dummy_task.remote(i) for i in range(num_tasks)])
 
 
+def warmup():
+    x = np.zeros(10**6, dtype=np.uint8)
+    for _ in range(5):
+        for _ in range(5):
+            ray.put(x)
+        for _ in range(5):
+            ray.get([dummy_task.remote(0) for _ in range(1000)])
+
+
 @pytest.mark.benchmark
 @pytest.mark.parametrize("num_tasks", num_tasks_submitted, ids=num_tasks_ids)
 def test_task_submission(benchmark, num_tasks):
     num_cpus = 16
     ray.init(num_cpus=num_cpus, object_store_memory=10**7)
     # warm up the plasma store
-    for _ in range(2):
-        [
-            ray.put(np.random.randint(0, 100, size=10**i))
-            for i in range(6, 0, -1)
-        ]
-    benchmark(benchmark_task_submission, num_tasks)
+    warmup()
+    benchmark(benchmark_task_submission, args=(num_tasks), rounds=10)
     ray.shutdown()
