92e9b1bcf75b09ed7deda9c020e854a2af978541
jovany-wang <kingchin1218@126.com>
2019-04-10 20:27:24 +0800

    Refine java test

--- java/test/src/main/java/org/ray/api/TestUtils.java
+++ java/test/src/main/java/org/ray/api/TestUtils.java
@@ -1,15 +1,44 @@
 package org.ray.api;
 
+import java.util.function.Supplier;
 import org.ray.runtime.AbstractRayRuntime;
 import org.ray.runtime.config.RunMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.SkipException;
 
 public class TestUtils {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(TestUtils.class);
+
+  private static final int WAIT_INTERVAL_MS = 5;
 
   public static void skipTestUnderSingleProcess() {
     AbstractRayRuntime runtime = (AbstractRayRuntime)Ray.internal();
     if (runtime.getRayConfig().runMode == RunMode.SINGLE_PROCESS) {
       throw new SkipException("This test doesn't work under single-process mode.");
     }
+  }
+
+  public static boolean waitForCondition(Supplier<Boolean> condition, int timeoutMillis) {
+    boolean result = false;
+    int waitTime = 0;
+    while (true) {
+      result = condition.get();
+      if (result) {
+        break;
+      }
+
+      try {
+        java.util.concurrent.TimeUnit.MILLISECONDS.sleep(WAIT_INTERVAL_MS);
+        waitTime += WAIT_INTERVAL_MS;
+        if (waitTime > timeoutMillis) {
+          break;
+        }
+      } catch (InterruptedException e) {
+        LOGGER.warn("Got exception when sleeping: ", e);
+      }
+    }
+    return result;
   }
 }
--- java/test/src/main/java/org/ray/api/test/PlasmaFreeTest.java
+++ java/test/src/main/java/org/ray/api/test/PlasmaFreeTest.java
@@ -1,9 +1,9 @@
 package org.ray.api.test;
 
 import com.google.common.collect.ImmutableList;
-import java.util.function.Supplier;
 import org.ray.api.Ray;
 import org.ray.api.RayObject;
+import org.ray.api.TestUtils;
 import org.ray.api.annotation.RayRemote;
 import org.ray.runtime.AbstractRayRuntime;
 import org.ray.runtime.RayNativeRuntime;
@@ -13,60 +13,32 @@ import org.testng.annotations.Test;
 
 public class PlasmaFreeTest extends BaseTest {
 
-  private static final int WAIT_INTERVAL = 5;
-  private static final int WAIT_TIMEOUT_LIMIT_MS = 50;
-
   @RayRemote
   private static String hello() {
     return "hello";
   }
 
   @Test
-  public void test() throws InterruptedException {
+  public void test() {
     RayObject<String> helloId = Ray.call(PlasmaFreeTest::hello);
     String helloString = helloId.get();
     Assert.assertEquals("hello", helloString);
     Ray.internal().free(ImmutableList.of(helloId.getId()), true, false);
 
-    final boolean result = waitForCondition(() -> {
-      return !((AbstractRayRuntime) Ray.internal())
-          .getObjectStoreProxy().get(helloId.getId(), 0).exists;
-    }, WAIT_INTERVAL, WAIT_TIMEOUT_LIMIT_MS);
-
+    final boolean result = TestUtils.waitForCondition(() -> !((AbstractRayRuntime) Ray.internal())
+        .getObjectStoreProxy().get(helloId.getId(), 0).exists, 50);
     Assert.assertTrue(result);
   }
 
   @Test
-  public void testDeleteCreatingTasks() throws InterruptedException {
+  public void testDeleteCreatingTasks() {
     RayObject<String> helloId = Ray.call(PlasmaFreeTest::hello);
     Assert.assertEquals("hello", helloId.get());
     Ray.internal().free(ImmutableList.of(helloId.getId()), true, true);
 
-    final boolean result = waitForCondition(() -> {
-      return !((RayNativeRuntime) Ray.internal())
-          .rayletTaskExistsInGcs(UniqueIdUtil.computeTaskId(helloId.getId()));
-    }, WAIT_INTERVAL, WAIT_TIMEOUT_LIMIT_MS);
-
+    final boolean result = TestUtils.waitForCondition(() -> !((RayNativeRuntime) Ray.internal())
+          .rayletTaskExistsInGcs(UniqueIdUtil.computeTaskId(helloId.getId())), 50);
     Assert.assertTrue(result);
-  }
-
-  private boolean waitForCondition(Supplier<Boolean> condition,
-                                   int interval, int timeout) throws InterruptedException {
-    boolean result = false;
-    int waitTime = 0;
-    while (true) {
-      result = condition.get();
-      if (result) {
-        break;
-      }
-
-      waitTime += interval;
-      if (waitTime > timeout) {
-        break;
-      }
-      java.util.concurrent.TimeUnit.MILLISECONDS.sleep(interval);
-    }
-    return result;
   }
 
 }
