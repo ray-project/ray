a848618952632f339a687e5fbeeb053c7977c631
Eric Liang <ekhliang@gmail.com>
2019-04-09 19:09:32 -0700

    add max queue size

--- python/ray/rllib/utils/memory.py
+++ python/ray/rllib/utils/memory.py
@@ -2,12 +2,19 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import ray
 import numpy as np
+import time
+
+import ray
+
+FREE_DELAY_S = 5.0
+MAX_FREE_QUEUE_SIZE = 100
+_last_free_time = 0.0
+_to_free = []
 
 
 def ray_get_and_free(object_ids):
-    """Call ray.get and then free the input object ids.
+    """Call ray.get and then queue the object ids for deletion.
 
     This function should be used whenever possible in RLlib, to optimize
     memory usage. The only exception is when an object_id is shared among
@@ -22,7 +29,17 @@ def ray_get_and_free(object_ids):
     result = ray.get(object_ids)
     if type(object_ids) is not list:
         object_ids = [object_ids]
-    ray.internal.free(object_ids)  # mark it for gc
+    _to_free.extend(object_ids)
+
+    global _last_free_time
+
+    now = time.time()
+    if (len(_to_free) > MAX_FREE_QUEUE_SIZE
+            or now - _last_free_time > FREE_DELAY_S):
+        ray.internal.free(_to_free)
+        _to_free.clear()
+        _last_free_time = now
+
     return result
 
 
