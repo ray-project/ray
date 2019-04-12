596720ab362ea5eec817a405bb800a7b684574ea
Szymon Wojdat <szymonwojdat13@gmail.com>
2019-04-12 07:07:20 +0200

    [WIP] Implement DQNTorchPolicyGraph and DQNTorchLoss

--- python/ray/rllib/agents/dqn/torch_dqn_policy_graph.py
+++ python/ray/rllib/agents/dqn/torch_dqn_policy_graph.py
@@ -1,15 +1,30 @@
 from __future__ import absolute_import
 
+from gym.spaces import Dict
 import torch.nn as nn
 from torch.optim import RMSprop
 
 from ray.rllib.agents.dqn.dqn import DEFAULT_CONFIG as DQN_DEFAULT_CONFIG
+from ray.rllib.agents.qmix.model import _get_size
 from ray.rllib.evaluation.policy_graph import PolicyGraph
 from ray.rllib.utils.annotations import override
+from ray.rllib.models.catalog import ModelCatalog
 
 
 class DQNTorchLoss(nn.Module):
-    pass
+    def __init__(self,
+                 model,
+                 n_agents,
+                 n_actions,
+                 gamma=0.99):
+        nn.Module.__init__(self)
+        self.model = model
+        self.n_agents = n_agents
+        self.n_actions = n_actions
+        self.gamma = gamma
+
+        def forward():
+            pass  # todo
 
 
 class DQNTorchPolicyGraph(PolicyGraph):
@@ -19,9 +34,35 @@ class DQNTorchPolicyGraph(PolicyGraph):
         self.config = config
         self.observation_space = observation_space
         self.action_space = action_space
+        self.cur_epsilon = 1.0
 
-        # todo: initialize the remaining attributes of this class
+        agent_obs_space = observation_space.original_space.spaces[0]
+        if isinstance(agent_obs_space, Dict):
+            space_keys = set(agent_obs_space.spaces.keys())
+            if space_keys != {"obs", "action_mask"}:
+                raise ValueError(
+                    "Dict obs space for agent must have keyset "
+                    "['obs', 'action_mask'], got {}".format(space_keys))
+            mask_shape = tuple(agent_obs_space.spaces["action_mask"].shape)
+            if mask_shape != (self.n_actions, ):
+                raise ValueError("Action mask shape must be {}, got {}".format(
+                    (self.n_actions, ), mask_shape))
+            self.has_action_mask = True
+            self.obs_size = _get_size(agent_obs_space.spaces["obs"])
+            # The real agent obs space is nested inside the dict
+            agent_obs_space = agent_obs_space.spaces["obs"]
+        else:
+            self.has_action_mask = False
+            self.obs_size = _get_size(agent_obs_space)
 
+        self.model = ModelCatalog.get_torch_model(
+            agent_obs_space,
+            self.n_actions,
+            config["model"])
+
+        # Setup optimiser
+        self.params = list(self.model.parameters())
+        self.loss - DQNTorchLoss()  # todo once DQNTorchLoss is implemented
         self.optimiser = RMSprop(
             params=self.params,
             lr=config["lr"],
@@ -37,27 +78,35 @@ class DQNTorchPolicyGraph(PolicyGraph):
                         info_batch=None,
                         episodes=None,
                         **kwargs):
-        pass
+        pass  # todo
 
     @override(PolicyGraph)
     def learn_on_batch(self, samples):
-        pass
+        pass  # todo
 
     @override(PolicyGraph)
     def get_initial_state(self):
-        pass
+        pass  # todo
 
     @override(PolicyGraph)
     def get_weights(self):
-        pass
+        return {"model": self.model.state_dict()}
+
+    @override(PolicyGraph)
+    def set_weights(self, weights):
+        self.model.load_state_dict(weights["model"])
 
     @override(PolicyGraph)
     def get_state(self):
-        pass
+        return {
+            "model": self.model.state_dict(),
+            "target_model": self.target_model.state_dict(),
+            "cur_epsilon": self.cur_epsilon
+        }
 
     @override(PolicyGraph)
     def set_state(self, state):
-        pass
+        pass  # todo
 
 
 def _validate(config):
@@ -65,3 +114,5 @@ def _validate(config):
         raise ValueError("Need to specify \"optim_alpha\" attribute of config, got: {}".format(config))
     if not hasattr(config, "optim_eps"):
         raise ValueError("Need to specify \"optim_eps\" attribute of config, got: {}".format(config))
+    if not hasattr(config, "model"):
+        raise ValueError("Need to specify \"model\" attribute of config, got: {}".format(config))
