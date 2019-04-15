3cc57868fce8df11a7ffe2d8f6285961304e959e
Eric Liang <ekhliang@gmail.com>
2019-04-14 19:32:36 -0700

    update

--- python/ray/rllib/utils/__init__.py
+++ python/ray/rllib/utils/__init__.py
@@ -10,14 +10,18 @@ logger = logging.getLogger(__name__)
 
 
 def renamed_class(cls):
+    """Helper class for renaming *Agent => *Trainer with a warning."""
+
     class DeprecationWrapper(cls):
-        def __init__(self, *args, **kwargs):
+        def __init__(self, config=None, env=None, logger_creator=None):
             old_name = cls.__name__.replace("Trainer", "Agent")
             new_name = cls.__name__
             logger.warn("DeprecationWarning: {} has been renamed to {}. ".
                         format(old_name, new_name) +
                         "This will raise an error in the future.")
-            cls.__init__(self, *args, **kwargs)
+            cls.__init__(self, config, env, logger_creator)
+
+    DeprecationWrapper.__name__ = cls.__name__
 
     return DeprecationWrapper
 
