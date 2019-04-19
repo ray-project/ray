b44a79ee69430665c290a857626a64026bd26ecd
Szymon Wojdat <szymonwojdat13@gmail.com>
2019-04-19 12:04:24 +0200

    [WIP] Port Kaixin's Rainbow

--- python/ray/rllib/agents/dqn/rainbow_torch_policy_graph.py
+++ python/ray/rllib/agents/dqn/rainbow_torch_policy_graph.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import
 
 from gym.spaces import Dict
+import math
+import torch as th
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.optim import RMSprop
 
 from ray.rllib.agents.dqn.dqn import DEFAULT_CONFIG as DQN_DEFAULT_CONFIG
@@ -11,18 +14,86 @@ from ray.rllib.utils.annotations import override
 from ray.rllib.models.catalog import ModelCatalog
 
 
-class RainbowTorchLoss(nn.Module):
-    def __init__(self,
-                 model,
-                 n_actions,
-                 gamma=0.99):
-        nn.Module.__init__(self)
-        self.model = model
-        self.n_actions = n_actions
-        self.gamma = gamma
+class NoisyLinear(nn.Module):
+    """
+        todo: add reference to Kaixhin's Rainbow etc.
+    """
+    def __init__(self, in_features, out_features, std_init=0.5):
+        super(NoisyLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.std_init = std_init
+        self.weight_mu = nn.Parameter(th.empty(out_features, in_features))
+        self.weight_sigma = nn.Parameter(th.empty(out_features, in_features))
+        self.register_buffer('weight_epsilon', th.empty(out_features, in_features))
+        self.bias_mu = nn.Parameter(th.empty(out_features))
+        self.bias_sigma = nn.Parameter(th.empty(out_features))
+        self.register_buffer('bias_epsilon', th.empty(out_features))
+        self.reset_parameters()
+        self.reset_noise()
 
-        def forward():
-            pass  # todo
+    def reset_parameters(self):
+        mu_range = 1 / math.sqrt(self.in_features)
+        self.weight_mu.data.uniform_(-mu_range, mu_range)
+        self.weight_sigma.data.fill_(self.std_init / math.sqrt(self.in_features))
+        self.bias_mu.data.uniform_(-mu_range, mu_range)
+        self.bias_sigma.data.fill_(self.std_init / math.sqrt(self.out_features))
+
+    def _scale_noise(self, size):
+        x = th.randn(size)
+        return x.sign().mul_(x.abs().sqrt_())
+
+    def reset_noise(self):
+        epsilon_in = self._scale_noise(self.in_features)
+        epsilon_out = self._scale_noise(self.out_features)
+        self.weight_epsilon.copy_(epsilon_out.ger(epsilon_in))
+        self.bias_epsilon.copy_(epsilon_out)
+
+    def forward(self, input_):
+        if self.training:
+            return F.linear(input_,
+                            self.weight_mu + self.weight_sigma * self.weight_epsilon,
+                            self.bias_mu + self.bias_sigma * self.bias_epsilon)
+        else:
+            return F.linear(input_, self.weight_mu, self.bias_mu)
+
+
+class RainbowTorchLoss(nn.Module):
+    """
+    todo: add reference to Kaixhin's Rainbow etc.
+    """
+    def __init__(self, args, action_space):
+        super().__init__()
+        self.atoms = args.atoms
+        self.action_space = action_space
+
+        self.conv1 = nn.Conv2d(args.history_length, 32, 8, stride=4, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, 3)
+        self.fc_h_v = NoisyLinear(3136, args.hidden_size, std_init=args.noisy_std)
+        self.fc_h_a = NoisyLinear(3136, args.hidden_size, std_init=args.noisy_std)
+        self.fc_z_v = NoisyLinear(args.hidden_size, self.atoms, std_init=args.noisy_std)
+        self.fc_z_a = NoisyLinear(args.hidden_size, action_space * self.atoms, std_init=args.noisy_std)
+
+    def forward(self, x, log=False):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.view(-1, 3136)
+        v = self.fc_z_v(F.relu(self.fc_h_v(x)))  # Value stream
+        a = self.fc_z_a(F.relu(self.fc_h_a(x)))  # Advantage stream
+        v, a = v.view(-1, 1, self.atoms), a.view(-1, self.action_space, self.atoms)
+        q = v + a - a.mean(1, keepdim=True)  # Combine streams
+        if log:  # Use log softmax for numerical stability
+          q = F.log_softmax(q, dim=2)  # Log probabilities with action over second dimension
+        else:
+          q = F.softmax(q, dim=2)  # Probabilities with action over second dimension
+        return q
+
+    def reset_noise(self):
+        for name, module in self.named_children():
+            if 'fc' in name:
+                module.reset_noise()
 
 
 class RainbowTorchPolicyGraph(PolicyGraph):
@@ -61,7 +132,7 @@ class RainbowTorchPolicyGraph(PolicyGraph):
 
         # Setup optimiser
         self.params = list(self.model.parameters())
-        self.loss - RainbowTorchLoss(self.model, self.n_actions)
+        self.loss = RainbowTorchLoss(self.model, self.n_actions)
         self.optimiser = RMSprop(
             params=self.params,
             lr=config["lr"],
@@ -90,18 +161,6 @@ class RainbowTorchPolicyGraph(PolicyGraph):
     @override(PolicyGraph)
     def set_weights(self, weights):
         self.model.load_state_dict(weights["model"])
-
-    @override(PolicyGraph)
-    def get_state(self):
-        return {
-            "model": self.model.state_dict(),
-            "target_model": self.target_model.state_dict(),
-            "cur_epsilon": self.cur_epsilon
-        }
-
-    @override(PolicyGraph)
-    def set_state(self, state):
-        pass  # todo
 
 
 def _validate(config):
