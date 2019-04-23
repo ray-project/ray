0692765efee448ce17b8e2565be49873258d4477
suquark <suquark@gmail.com>
2019-04-24 00:16:01 +0800

    fix webui

---
+++ python/ray/tests/test_webui.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import ray
+
+
+def test_get_webui():
+    addresses = ray.init(include_webui=True)
+    webui_url = addresses['webui_url']
+    assert ray.worker.get_webui_url() == webui_url
+
+    ray.shutdown()
