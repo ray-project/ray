14b96b8a11335237efbfe422d7faa8e5b23fbf17
Devin Petersohn <devin.petersohn@gmail.com>
2019-04-23 09:29:20 -0700

    Lint

--- python/ray/tests/perf_integration_tests/test_perf_integration.py
+++ python/ray/tests/perf_integration_tests/test_perf_integration.py
@@ -33,7 +33,9 @@ def warmup():
 @pytest.mark.parametrize("num_tasks", num_tasks_submitted, ids=num_tasks_ids)
 def test_task_submission(benchmark, num_tasks):
     num_cpus = 16
-    ray.init(num_cpus=num_cpus, object_store_memory=10**7, ignore_reinit_error=True)
+    ray.init(num_cpus=num_cpus,
+             object_store_memory=10**7,
+             ignore_reinit_error=True)
     # warm up the plasma store
     warmup()
     benchmark(benchmark_task_submission, num_tasks)
