143550f7c94814cf1521e990a9d3cd8143119a42
William Ma <12377941+williamma12@users.noreply.github.com>
2019-04-22 22:19:27 -0700

    linting

--- python/ray/tests/test_basic.py
+++ python/ray/tests/test_basic.py
@@ -2189,8 +2189,8 @@ def test_many_custom_resources(shutdown_only):
     ray.get(results)
 
 
-# TODO: 5 retry attempts may be too little for Travis and we may need to increase
-# it if this test begins to be flaky on Travis.
+# TODO: 5 retry attempts may be too little for Travis and we may need to 
+# increase it if this test begins to be flaky on Travis.
 def test_zero_capacity_deletion_semantics(shutdown_only):
     ray.init(num_cpus=2, num_gpus=1, resources={"test_resource": 1})
 
@@ -2205,7 +2205,8 @@ def test_zero_capacity_deletion_semantics(shutdown_only):
             retry_count += 1
 
         if retry_count >= MAX_RETRY_ATTEMPTS:
-            raise RuntimeError("Resources were available even after five retries.")
+            raise RuntimeError(
+                "Resources were available even after five retries.")
 
         return resources
 
