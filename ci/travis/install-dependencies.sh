4fc90c8fbe9b68f6d0ffe9df44b3c1be3bebd5df
suquark <suquark@gmail.com>
2019-04-26 23:37:40 +0800

    move dependencies to the script file

--- ci/travis/install-dependencies.sh
+++ ci/travis/install-dependencies.sh
@@ -34,7 +34,7 @@ elif [[ "$PYTHON" == "3.5" ]] && [[ "$platform" == "linux" ]]; then
   bash miniconda.sh -b -p $HOME/miniconda
   export PATH="$HOME/miniconda/bin:$PATH"
   pip install -q scipy tensorflow cython==0.29.0 gym opencv-python-headless pyyaml pandas==0.23.4 requests \
-    feather-format lxml openpyxl xlrd py-spy setproctitle pytest-timeout flaky networkx tabulate psutil
+    feather-format lxml openpyxl xlrd py-spy setproctitle pytest-timeout flaky networkx tabulate psutil aiohttp
 elif [[ "$PYTHON" == "2.7" ]] && [[ "$platform" == "macosx" ]]; then
   # Install miniconda.
   wget https://repo.continuum.io/miniconda/Miniconda2-4.5.4-MacOSX-x86_64.sh -O miniconda.sh -nv
@@ -48,7 +48,7 @@ elif [[ "$PYTHON" == "3.5" ]] && [[ "$platform" == "macosx" ]]; then
   bash miniconda.sh -b -p $HOME/miniconda
   export PATH="$HOME/miniconda/bin:$PATH"
   pip install -q cython==0.29.0 tensorflow gym opencv-python-headless pyyaml pandas==0.23.4 requests \
-    feather-format lxml openpyxl xlrd py-spy setproctitle pytest-timeout flaky networkx tabulate psutil
+    feather-format lxml openpyxl xlrd py-spy setproctitle pytest-timeout flaky networkx tabulate psutil aiohttp
 elif [[ "$LINT" == "1" ]]; then
   sudo apt-get update
   sudo apt-get install -y build-essential curl unzip
--- python/ray/tests/test_webui.py
+++ python/ray/tests/test_webui.py
@@ -3,26 +3,13 @@ from __future__ import division
 from __future__ import print_function
 
 import sys
-import subprocess
 import pytest
 import ray
 
 
-@pytest.mark.skipif(sys.version_info < (3, 5, 3),
-    reason="requires python3.5.3 or higher")
+@pytest.mark.skipif(
+    sys.version_info < (3, 5, 3), reason="requires python3.5.3 or higher")
 def test_get_webui():
-    # Try to install dependencies for this unittest.
-    # "aiohttp" requires that Python version >= 3.5.3.
-
-    try:
-        import aiohttp
-        import psutil
-    except (ImportError, ModuleNotFoundError):
-        # We install them here because they require python3,
-        # and they are not used by other unittests.
-        subprocess.check_call(
-            [sys.executable, "-m", "pip", "install", "aiohttp", "psutil"])
-
     addresses = ray.init(include_webui=True)
     webui_url = addresses["webui_url"]
     assert ray.worker.get_webui_url() == webui_url
