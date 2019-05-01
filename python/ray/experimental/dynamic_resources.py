137e57af0a32bdaba43a711c260a6b69a2dbc650
romilbhardwaj <romil.bhardwaj@gmail.com>
2019-05-01 14:26:45 -0700

    Fix pylinting

--- python/ray/experimental/dynamic_resources.py
+++ python/ray/experimental/dynamic_resources.py
@@ -3,18 +3,21 @@ import ray
 
 def set_resource(resource_name, capacity, client_id=None):
     """
-    Creates, updates or deletes a custom resource for a target clientId. If the resource already exists, it's capacity is updated to the new value.
+    Creates, updates or deletes a custom resource for a target clientId.
+    If the resource already exists, it's capacity is updated to the new value.
     If the capacity is set to 0, the resource is deleted.
-    If ClientID is not specified or set to None, the resource is created on the local client where the actor is running.
+    If ClientID is not specified or set to None,
+    the resource is created on the local client where the actor is running.
     :param resource_name: Name of the resource to be created
     :type str
-    :param capacity: Capacity of the new resource. Resource is deleted if capacity is 0.
+    :param capacity: Capacity of the new resource.
+    Resource is deleted if capacity is 0.
     :type int
     :param client_id: ClientId where the resource is to be created or updated.
     :type str
     :return: None
     """
-    if client_id != None:
+    if client_id is not None:
         client_id_obj = ray.ClientID(ray.utils.hex_to_binary(client_id))
     else:
         client_id_obj = ray.ClientID.nil()
--- python/ray/tests/test_dynres.py
+++ python/ray/tests/test_dynres.py
@@ -51,8 +51,10 @@ def test_dynamic_res_deletion(shutdown_only):
 
 
 def test_dynamic_res_infeasible_rescheduling(ray_start_regular):
-    # This test launches an infeasible task and then creates a resource to make the task feasible.
-    # This tests if the infeasible tasks get rescheduled when resources are created at runtime.
+    # This test launches an infeasible task and then creates a
+    # resource to make the task feasible. This tests if the
+    # infeasible tasks get rescheduled when resources are
+    # created at runtime.
     res_name = "test_res"
     res_capacity = 1.0
 
@@ -87,7 +89,7 @@ def test_dynamic_res_updation_clientid(ray_start_cluster):
 
     ray.init(redis_address=cluster.redis_address)
 
-    target_clientid = ray.global_state.client_table()[1]['ClientID']
+    target_clientid = ray.global_state.client_table()[1]["ClientID"]
 
     @ray.remote
     def set_res(resource_name, resource_capacity, client_id):
@@ -102,8 +104,8 @@ def test_dynamic_res_updation_clientid(ray_start_cluster):
     ray.get(set_res.remote(res_name, new_capacity, target_clientid))
 
     target_client = next(client for client in ray.global_state.client_table()
-                         if client['ClientID'] == target_clientid)
-    resources = target_client['Resources']
+                         if client["ClientID"] == target_clientid)
+    resources = target_client["Resources"]
 
     assert res_name in resources
     assert resources[res_name] == new_capacity
@@ -121,7 +123,7 @@ def test_dynamic_res_creation_clientid(ray_start_cluster):
 
     ray.init(redis_address=cluster.redis_address)
 
-    target_clientid = ray.global_state.client_table()[1]['ClientID']
+    target_clientid = ray.global_state.client_table()[1]["ClientID"]
 
     @ray.remote
     def set_res(resource_name, resource_capacity, res_client_id):
@@ -130,15 +132,16 @@ def test_dynamic_res_creation_clientid(ray_start_cluster):
 
     ray.get(set_res.remote(res_name, res_capacity, target_clientid))
     target_client = next(client for client in ray.global_state.client_table()
-                         if client['ClientID'] == target_clientid)
-    resources = target_client['Resources']
+                         if client["ClientID"] == target_clientid)
+    resources = target_client["Resources"]
 
     assert res_name in resources
     assert resources[res_name] == res_capacity
 
 
 def test_dynamic_res_creation_clientid_multiple(ray_start_cluster):
-    # This test creates resources on multiple clients using the clientid specifier
+    # This test creates resources on multiple clients using the clientid
+    # specifier
     cluster = ray_start_cluster
 
     res_name = "test_res"
@@ -150,7 +153,7 @@ def test_dynamic_res_creation_clientid_multiple(ray_start_cluster):
     ray.init(redis_address=cluster.redis_address)
 
     target_clientids = [
-        client['ClientID'] for client in ray.global_state.client_table()
+        client["ClientID"] for client in ray.global_state.client_table()
     ]
 
     @ray.remote
@@ -168,8 +171,8 @@ def test_dynamic_res_creation_clientid_multiple(ray_start_cluster):
     for cid in target_clientids:
         target_client = next(client
                              for client in ray.global_state.client_table()
-                             if client['ClientID'] == cid)
-        resources = target_client['Resources']
+                             if client["ClientID"] == cid)
+        resources = target_client["Resources"]
         assert resources[res_name] == res_capacity
 
 
@@ -182,12 +185,13 @@ def test_dynamic_res_deletion_clientid(ray_start_cluster):
     num_nodes = 5
 
     for i in range(num_nodes):
-        # Create resource on all nodes, but later we'll delete it from a target node
+        # Create resource on all nodes, but later we'll delete it from a
+        # target node
         cluster.add_node(resources={res_name: res_capacity})
 
     ray.init(redis_address=cluster.redis_address)
 
-    target_clientid = ray.global_state.client_table()[1]['ClientID']
+    target_clientid = ray.global_state.client_table()[1]["ClientID"]
 
     # Launch the delete task
     @ray.remote
@@ -198,14 +202,15 @@ def test_dynamic_res_deletion_clientid(ray_start_cluster):
     ray.get(delete_res.remote(res_name, target_clientid))
 
     target_client = next(client for client in ray.global_state.client_table()
-                         if client['ClientID'] == target_clientid)
-    resources = target_client['Resources']
+                         if client["ClientID"] == target_clientid)
+    resources = target_client["Resources"]
     print(ray.global_state.cluster_resources())
     assert res_name not in resources
 
 
 def test_dynamic_res_creation_scheduler_consistency(ray_start_cluster):
-    # This makes sure the resource is actually created and the state is consistent in the scheduler
+    # This makes sure the resource is actually created and the state is
+    # consistent in the scheduler
     # by launching a task which requests the created resource
     cluster = ray_start_cluster
 
@@ -219,7 +224,7 @@ def test_dynamic_res_creation_scheduler_consistency(ray_start_cluster):
     ray.init(redis_address=cluster.redis_address)
 
     clientids = [
-        client['ClientID'] for client in ray.global_state.client_table()
+        client["ClientID"] for client in ray.global_state.client_table()
     ]
 
     @ray.remote
@@ -242,8 +247,9 @@ def test_dynamic_res_creation_scheduler_consistency(ray_start_cluster):
 
 
 def test_dynamic_res_deletion_scheduler_consistency(ray_start_cluster):
-    # This makes sure the resource is actually deleted and the state is consistent in the scheduler
-    # by launching an infeasible task which requests the created resource
+    # This makes sure the resource is actually deleted and the state is
+    # consistent in the scheduler by launching an infeasible task which
+    # requests the created resource
     cluster = ray_start_cluster
 
     res_name = "test_res"
@@ -257,7 +263,7 @@ def test_dynamic_res_deletion_scheduler_consistency(ray_start_cluster):
     ray.init(redis_address=cluster.redis_address)
 
     clientids = [
-        client['ClientID'] for client in ray.global_state.client_table()
+        client["ClientID"] for client in ray.global_state.client_table()
     ]
 
     @ray.remote
@@ -289,7 +295,8 @@ def test_dynamic_res_deletion_scheduler_consistency(ray_start_cluster):
 
 
 def test_dynamic_res_concurrent_res_increment(ray_start_cluster):
-    # This test makes sure resource capacity is updated (increment) correctly when a task has already acquired some of the resource.
+    # This test makes sure resource capacity is updated (increment) correctly
+    # when a task has already acquired some of the resource.
 
     cluster = ray_start_cluster
 
@@ -305,7 +312,7 @@ def test_dynamic_res_concurrent_res_increment(ray_start_cluster):
     ray.init(redis_address=cluster.redis_address)
 
     clientids = [
-        client['ClientID'] for client in ray.global_state.client_table()
+        client["ClientID"] for client in ray.global_state.client_table()
     ]
     target_clientid = clientids[1]
 
@@ -329,9 +336,10 @@ def test_dynamic_res_concurrent_res_increment(ray_start_cluster):
         return 1
 
     # Create a object ID to have the task wait on
-    WAIT_OBJECT_ID_STR = ('a' * 20).encode("ascii")
+    WAIT_OBJECT_ID_STR = ("a" * 20).encode("ascii")
 
-    # Launch the task with resource requirement of 1, thus the new available capacity becomes 4
+    # Launch the task with resource requirement of 1, thus the new available
+    # capacity becomes 4
     task = wait_func._remote(
         args=[WAIT_OBJECT_ID_STR], resources={res_name: 1})
     # Sleep to make sure the wait_func is launched before updating resource
@@ -344,12 +352,14 @@ def test_dynamic_res_concurrent_res_increment(ray_start_cluster):
     ray.worker.global_worker.put_object(ray.ObjectID(WAIT_OBJECT_ID_STR), 1)
     ray.get(task)
 
-    # Check if scheduler state is consistent by launching a task requiring updated capacity
+    # Check if scheduler state is consistent by launching a task requiring
+    # updated capacity
     task_2 = test_func._remote(args=[], resources={res_name: updated_capacity})
     successful, unsuccessful = ray.wait([task_2], timeout=TIMEOUT_DURATION)
     assert successful  # The task completed
 
-    # Check if scheduler state is consistent by launching a task requiring updated capacity + 1. This should not execute
+    # Check if scheduler state is consistent by launching a task requiring
+    # updated capacity + 1. This should not execute
     task_3 = test_func._remote(
         args=[], resources={res_name: updated_capacity + 1
                             })  # This should be infeasible
@@ -359,7 +369,9 @@ def test_dynamic_res_concurrent_res_increment(ray_start_cluster):
 
 
 def test_dynamic_res_concurrent_res_decrement(ray_start_cluster):
-    # This test makes sure resource capacity is updated (decremented) correctly when a task has already acquired some of the resource.
+    # This test makes sure resource capacity is updated (decremented)
+    # correctly when a task has already acquired some
+    # of the resource.
 
     cluster = ray_start_cluster
 
@@ -375,7 +387,7 @@ def test_dynamic_res_concurrent_res_decrement(ray_start_cluster):
     ray.init(redis_address=cluster.redis_address)
 
     clientids = [
-        client['ClientID'] for client in ray.global_state.client_table()
+        client["ClientID"] for client in ray.global_state.client_table()
     ]
     target_clientid = clientids[1]
 
@@ -398,9 +410,10 @@ def test_dynamic_res_concurrent_res_decrement(ray_start_cluster):
         return 1
 
     # Create a object ID to have the task wait on
-    WAIT_OBJECT_ID_STR = ('a' * 20).encode("ascii")
+    WAIT_OBJECT_ID_STR = ("a" * 20).encode("ascii")
 
-    # Launch the task with resource requirement of 4, thus the new available capacity becomes 1
+    # Launch the task with resource requirement of 4, thus the new available
+    # capacity becomes 1
     task = wait_func._remote(
         args=[WAIT_OBJECT_ID_STR], resources={res_name: 4})
     time.sleep(
@@ -414,12 +427,14 @@ def test_dynamic_res_concurrent_res_decrement(ray_start_cluster):
     ray.worker.global_worker.put_object(ray.ObjectID(WAIT_OBJECT_ID_STR), 1)
     ray.get(task)
 
-    # Check if scheduler state is consistent by launching a task requiring updated capacity
+    # Check if scheduler state is consistent by launching a task requiring
+    # updated capacity
     task_2 = test_func._remote(args=[], resources={res_name: updated_capacity})
     successful, unsuccessful = ray.wait([task_2], timeout=TIMEOUT_DURATION)
     assert successful  # The task completed
 
-    # Check if scheduler state is consistent by launching a task requiring updated capacity + 1. This should not execute
+    # Check if scheduler state is consistent by launching a task requiring
+    # updated capacity + 1. This should not execute
     task_3 = test_func._remote(
         args=[], resources={res_name: updated_capacity + 1
                             })  # This should be infeasible
@@ -429,7 +444,8 @@ def test_dynamic_res_concurrent_res_decrement(ray_start_cluster):
 
 
 def test_dynamic_res_concurrent_res_delete(ray_start_cluster):
-    # This test makes sure resource gets deleted correctly when a task has already acquired the resource
+    # This test makes sure resource gets deleted correctly when a task has
+    # already acquired the resource
 
     cluster = ray_start_cluster
 
@@ -444,7 +460,7 @@ def test_dynamic_res_concurrent_res_delete(ray_start_cluster):
     ray.init(redis_address=cluster.redis_address)
 
     clientids = [
-        client['ClientID'] for client in ray.global_state.client_table()
+        client["ClientID"] for client in ray.global_state.client_table()
     ]
     target_clientid = clientids[1]
 
@@ -473,9 +489,10 @@ def test_dynamic_res_concurrent_res_delete(ray_start_cluster):
         return 1
 
     # Create a object ID to have the task wait on
-    WAIT_OBJECT_ID_STR = ('a' * 20).encode("ascii")
+    WAIT_OBJECT_ID_STR = ("a" * 20).encode("ascii")
 
-    # Launch the task with resource requirement of 1, thus the new available capacity becomes 4
+    # Launch the task with resource requirement of 1, thus the new available
+    # capacity becomes 4
     task = wait_func._remote(
         args=[WAIT_OBJECT_ID_STR], resources={res_name: 1})
     time.sleep(
@@ -489,7 +506,8 @@ def test_dynamic_res_concurrent_res_delete(ray_start_cluster):
     ray.worker.global_worker.put_object(ray.ObjectID(WAIT_OBJECT_ID_STR), 1)
     ray.get(task)
 
-    # Check if scheduler state is consistent by launching a task requiring the deleted resource  This should not execute
+    # Check if scheduler state is consistent by launching a task requiring
+    # the deleted resource  This should not execute
     task_2 = test_func._remote(
         args=[], resources={res_name: 1})  # This should be infeasible
     successful, unsuccessful = ray.wait([task_2], timeout=TIMEOUT_DURATION)
@@ -498,7 +516,8 @@ def test_dynamic_res_concurrent_res_delete(ray_start_cluster):
 
 
 def test_dynamic_res_creation_stress(ray_start_cluster):
-    # This stress tests creates many resources simultaneously on the same client and then checks if the final state is consistent
+    # This stress tests creates many resources simultaneously on the same
+    # client and then checks if the final state is consistent
 
     cluster = ray_start_cluster
 
@@ -512,7 +531,7 @@ def test_dynamic_res_creation_stress(ray_start_cluster):
     ray.init(redis_address=cluster.redis_address)
 
     clientids = [
-        client['ClientID'] for client in ray.global_state.client_table()
+        client["ClientID"] for client in ray.global_state.client_table()
     ]
     target_clientid = clientids[1]
 
