e64db8477f67db0c327985fa50bd136bc10d8259
Richard Liaw <rliaw@berkeley.edu>
2019-05-02 17:09:07 -0400

    fix tests

--- python/ray/tune/suggest/__init__.py
+++ python/ray/tune/suggest/__init__.py
@@ -1,16 +1,13 @@
 from ray.tune.suggest.search import SearchAlgorithm
 from ray.tune.suggest.basic_variant import BasicVariantGenerator
 from ray.tune.suggest.suggestion import SuggestionAlgorithm
-from ray.tune.suggest.variant_generator import grid_search, function, \
-    sample_from
+from ray.tune.suggest.variant_generator import grid_search
 
 __all__ = [
     "SearchAlgorithm",
     "BasicVariantGenerator",
     "SuggestionAlgorithm",
-    "grid_search",
-    "function",
-    "sample_from",
+    "grid_search"
 ]
 
 
