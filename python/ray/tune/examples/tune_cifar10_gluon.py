d4097a99189104820a180a72895db98cd444ea1d
Richard Liaw <rliaw@berkeley.edu>
2019-05-02 13:13:04 -0400

    lint

--- python/ray/tune/examples/tune_cifar10_gluon.py
+++ python/ray/tune/examples/tune_cifar10_gluon.py
@@ -14,83 +14,76 @@ from gluoncv.model_zoo import get_model
 from gluoncv.data import transforms as gcv_transforms
 
 # Training settings
-parser = argparse.ArgumentParser(description='CIFAR-10 Example')
+parser = argparse.ArgumentParser(description="CIFAR-10 Example")
 parser.add_argument(
-    '--model',
+    "--model",
     required=True,
     type=str,
-    default='resnet50_v1b',
-    help='name of the pretrained model from gluoncv model zoo'
-         '(default: resnet50_v1b).')
+    default="resnet50_v1b",
+    help="name of the pretrained model from gluoncv model zoo"
+    "(default: resnet50_v1b).")
 parser.add_argument(
-    '--batch_size',
+    "--batch_size",
     type=int,
     default=64,
-    metavar='N',
-    help='input batch size for training (default: 64)')
+    metavar="N",
+    help="input batch size for training (default: 64)")
 parser.add_argument(
-    '--epochs',
+    "--epochs",
     type=int,
     default=1,
-    metavar='N',
-    help='number of epochs to train (default: 1)')
+    metavar="N",
+    help="number of epochs to train (default: 1)")
 parser.add_argument(
-    '--num_gpus',
+    "--num_gpus",
     default=0,
     type=int,
-    help='number of gpus to use, 0 indicates cpu only (default: 0)')
+    help="number of gpus to use, 0 indicates cpu only (default: 0)")
 parser.add_argument(
-    '--num_workers',
+    "--num_workers",
     default=4,
     type=int,
-    help='number of preprocessing workers (default: 4)')
+    help="number of preprocessing workers (default: 4)")
 parser.add_argument(
-    '--classes',
+    "--classes",
     type=int,
     default=10,
-    metavar='N',
-    help='number of outputs (default: 10)')
+    metavar="N",
+    help="number of outputs (default: 10)")
 parser.add_argument(
-    '--lr',
+    "--lr",
     default=0.001,
     type=float,
-    help='initial learning rate (default: 0.001)')
+    help="initial learning rate (default: 0.001)")
 parser.add_argument(
-    '--momentum',
+    "--momentum",
     default=0.9,
     type=float,
-    help='initial momentum (default: 0.9)')
+    help="initial momentum (default: 0.9)")
 parser.add_argument(
-    '--wd',
-    default=1e-4,
-    type=float,
-    help='weight decay (default: 1e-4)')
+    "--wd", default=1e-4, type=float, help="weight decay (default: 1e-4)")
 parser.add_argument(
-    '--expname',
-    type=str,
-    default='cifar10exp',
-    help='experiments location')
+    "--expname", type=str, default="cifar10exp", help="experiments location")
 parser.add_argument(
-    '--num_samples',
+    "--num_samples",
     type=int,
     default=20,
-    metavar='N',
-    help='number of samples (default: 20)')
+    metavar="N",
+    help="number of samples (default: 20)")
 parser.add_argument(
-    '--scheduler',
+    "--scheduler",
     type=str,
-    default='fifo',
-    help='type of trial scheduler (default: fifo), options are fifo and AsyncHyperBandScheduler')
+    default="fifo",
+    help="FIFO or AsyncHyperBandScheduler."
+)
 parser.add_argument(
-    '--seed',
+    "--seed",
     type=int,
     default=1,
-    metavar='S',
-    help='random seed (default: 1)')
+    metavar="S",
+    help="random seed (default: 1)")
 parser.add_argument(
-    '--smoke_test',
-    action="store_true",
-    help="Finish quickly for testing")
+    "--smoke_test", action="store_true", help="Finish quickly for testing")
 args = parser.parse_args()
 
 
@@ -102,28 +95,36 @@ def train_cifar10(args, config, reporter):
 
     # Set Hyper-params
     batch_size = args.batch_size * max(args.num_gpus, 1)
-    ctx = [mx.gpu(i) for i in range(args.num_gpus)] if args.num_gpus > 0 else [mx.cpu()]
+    ctx = [mx.gpu(i)
+           for i in range(args.num_gpus)] if args.num_gpus > 0 else [mx.cpu()]
 
     # Define DataLoader
     transform_train = transforms.Compose([
         gcv_transforms.RandomCrop(32, pad=4),
         transforms.RandomFlipLeftRight(),
         transforms.ToTensor(),
-        transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
+        transforms.Normalize([0.4914, 0.4822, 0.4465],
+                             [0.2023, 0.1994, 0.2010])
     ])
 
     transform_test = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
+        transforms.Normalize([0.4914, 0.4822, 0.4465],
+                             [0.2023, 0.1994, 0.2010])
     ])
 
     train_data = gluon.data.DataLoader(
         gluon.data.vision.CIFAR10(train=True).transform_first(transform_train),
-        batch_size=batch_size, shuffle=True, last_batch='discard', num_workers=args.num_workers)
+        batch_size=batch_size,
+        shuffle=True,
+        last_batch="discard",
+        num_workers=args.num_workers)
 
     test_data = gluon.data.DataLoader(
         gluon.data.vision.CIFAR10(train=False).transform_first(transform_test),
-        batch_size=batch_size, shuffle=False, num_workers=args.num_workers)
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=args.num_workers)
 
     # Load model architecture and Initialize the net with pretrained model
     finetune_net = get_model(args.model, pretrained=True)
@@ -134,17 +135,20 @@ def train_cifar10(args, config, reporter):
     finetune_net.hybridize()
 
     # Define trainer
-    trainer = gluon.Trainer(finetune_net.collect_params(), 'sgd', {
-        'learning_rate': args.lr, 'momentum': args.momentum, 'wd': args.wd})
+    trainer = gluon.Trainer(finetune_net.collect_params(), "sgd", {
+        "learning_rate": args.lr,
+        "momentum": args.momentum,
+        "wd": args.wd
+    })
     L = gluon.loss.SoftmaxCrossEntropyLoss()
     metric = mx.metric.Accuracy()
 
     def train(epoch):
         for i, batch in enumerate(train_data):
-            data = gluon.utils.split_and_load(batch[0], ctx_list=ctx, batch_axis=0,
-                                              even_split=False)
-            label = gluon.utils.split_and_load(batch[1], ctx_list=ctx, batch_axis=0,
-                                               even_split=False)
+            data = gluon.utils.split_and_load(
+                batch[0], ctx_list=ctx, batch_axis=0, even_split=False)
+            label = gluon.utils.split_and_load(
+                batch[1], ctx_list=ctx, batch_axis=0, even_split=False)
             with ag.record():
                 outputs = [finetune_net(X) for X in data]
                 loss = [L(yhat, y) for yhat, y in zip(outputs, label)]
@@ -157,10 +161,10 @@ def train_cifar10(args, config, reporter):
     def test():
         test_loss = 0
         for i, batch in enumerate(test_data):
-            data = gluon.utils.split_and_load(batch[0], ctx_list=ctx, batch_axis=0,
-                                              even_split=False)
-            label = gluon.utils.split_and_load(batch[1], ctx_list=ctx, batch_axis=0,
-                                               even_split=False)
+            data = gluon.utils.split_and_load(
+                batch[0], ctx_list=ctx, batch_axis=0, even_split=False)
+            label = gluon.utils.split_and_load(
+                batch[1], ctx_list=ctx, batch_axis=0, even_split=False)
             outputs = [finetune_net(X) for X in data]
             loss = [L(yhat, y) for yhat, y in zip(outputs, label)]
 
@@ -184,9 +188,9 @@ if __name__ == "__main__":
     from ray.tune.schedulers import AsyncHyperBandScheduler, FIFOScheduler
 
     ray.init()
-    if args.scheduler == 'fifo':
+    if args.scheduler == "fifo":
         sched = FIFOScheduler()
-    elif args.scheduler == 'asynchyperband':
+    elif args.scheduler == "asynchyperband":
         sched = AsyncHyperBandScheduler(
             time_attr="training_iteration",
             reward_attr="neg_mean_loss",
