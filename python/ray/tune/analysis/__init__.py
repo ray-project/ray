36b71d14463648625caf7f1b4b43bda986f0b29d
Adi Zimmerman <adizim@berkeley.edu>
2019-05-03 23:51:26 -0700

    [Tune] Post-Experiment Tools (#4351)

---
+++ python/ray/tune/analysis/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from ray.tune.analysis.experiment_analysis import ExperimentAnalysis
+
+__all__ = ["ExperimentAnalysis"]
