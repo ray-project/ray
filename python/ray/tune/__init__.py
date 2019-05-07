7f50c96adbc606db0fa4aab0594c0d8870b9115a
Richard Liaw <rliaw@berkeley.edu>
2019-05-06 17:42:39 -0700

    [tune] Reduce sampling API clutter (#4739)
    Adds some sugar for tune sampling API (for commonplace sampling idioms).

--- python/ray/tune/__init__.py
+++ python/ray/tune/__init__.py
@@ -7,10 +7,12 @@ from ray.tune.tune import run_experiments, run
 from ray.tune.experiment import Experiment
 from ray.tune.registry import register_env, register_trainable
 from ray.tune.trainable import Trainable
-from ray.tune.suggest import grid_search, function, sample_from
+from ray.tune.suggest import grid_search
+from ray.tune.sample import (function, sample_from, uniform, choice, randint,
+                             randn)
 
 __all__ = [
     "Trainable", "TuneError", "grid_search", "register_env",
     "register_trainable", "run", "run_experiments", "Experiment", "function",
-    "sample_from"
+    "sample_from", "uniform", "choice", "randint", "randn"
 ]
