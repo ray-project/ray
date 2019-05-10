4baa134260c22b6cac8dd5b1bc3a9fafc1c2b224
romilbhardwaj <romil.bhardwaj@gmail.com>
2019-05-09 18:38:09 -0700

    fix python2 compatibility

--- python/ray/_raylet.pyx
+++ python/ray/_raylet.pyx
@@ -369,7 +369,7 @@ cdef class RayletClient:
         check_status(self.client.get().NotifyActorResumedFromCheckpoint(
             actor_id.native(), checkpoint_id.native()))
 
-    def set_resource(self, str resource_name, double capacity, ClientID client_id):
+    def set_resource(self, basestring resource_name, double capacity, ClientID client_id):
         self.client.get().SetResource(resource_name.encode("ascii"), capacity, CClientID.from_binary(client_id.binary()))
 
     @property
