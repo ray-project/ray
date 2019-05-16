66b65610bdac0d2327257044bbfe6d34fe7c0c03
Yuhong Guo <yuhong.gyh@antfin.com>
2019-05-16 15:29:24 +0800

    Add WorkerUncaughtExceptionHandler

--- java/runtime/src/main/java/org/ray/runtime/runner/worker/DefaultWorker.java
+++ java/runtime/src/main/java/org/ray/runtime/runner/worker/DefaultWorker.java
@@ -2,6 +2,7 @@ package org.ray.runtime.runner.worker;
 
 import org.ray.api.Ray;
 import org.ray.runtime.AbstractRayRuntime;
+import org.ray.runtime.util.WorkerUncaughtExceptionHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +16,7 @@ public class DefaultWorker {
   public static void main(String[] args) {
     try {
       System.setProperty("ray.worker.mode", "WORKER");
+      Thread.setDefaultUncaughtExceptionHandler(new WorkerUncaughtExceptionHandler());
       Ray.init();
       LOGGER.info("Worker started.");
       ((AbstractRayRuntime)Ray.internal()).loop();
