ffe61fcc70ac0c4ae1dd9441507679303ae6cde0
Richard Liaw <rliaw@berkeley.edu>
2019-05-16 23:10:07 -0700

    [tune] Support non-arg submit (#4803)

--- python/ray/scripts/scripts.py
+++ python/ray/scripts/scripts.py
@@ -609,7 +609,10 @@ def submit(cluster_config_file, docker, screen, tmux, stop, start,
     target = os.path.join("~", os.path.basename(script))
     rsync(cluster_config_file, script, target, cluster_name, down=False)
 
-    cmd = " ".join(["python", target, args])
+    command_parts = ["python", target]
+    if args is not None:
+        command_parts += [args]
+    cmd = " ".join(command_parts)
     exec_cluster(cluster_config_file, cmd, docker, screen, tmux, stop, False,
                  cluster_name, port_forward)
 
