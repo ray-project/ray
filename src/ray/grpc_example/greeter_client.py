a6a9e0c7d6339f3949348e51aa1f16004f01799c
Yuhong Guo <yuhong.gyh@antfin.com>
2019-05-22 15:06:51 +0800

    Lint bazel script and update bazel to 0.22.0

--- src/ray/grpc_example/greeter_client.py
+++ src/ray/grpc_example/greeter_client.py
@@ -8,12 +8,12 @@ import example.proto.greeter_pb2_grpc as greeter_pb2_grpc
 
 
 def run():
-    with grpc.insecure_channel('localhost:10086') as channel:
+    with grpc.insecure_channel("localhost:10086") as channel:
         stub = greeter_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(greeter_pb2.HelloRequest(name='ray-client'))
+        response = stub.SayHello(greeter_pb2.HelloRequest(name="ray-client"))
     print("Greeter client received: " + response.message)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig()
     run()
