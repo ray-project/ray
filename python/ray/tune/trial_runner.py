0699344bd5683fb04ef6357b57c2dacc14513ccd
Richard Liaw <rliaw@berkeley.edu>
2019-05-24 19:09:04 -0700

    update_with_slow

--- python/ray/tune/trial_runner.py
+++ python/ray/tune/trial_runner.py
@@ -153,6 +153,7 @@ class TrialRunner(object):
         self._metadata_checkpoint_dir = metadata_checkpoint_dir
 
         self._start_time = time.time()
+        self._last_checkpoint_timestamp = -float("inf")
         self._session_str = datetime.fromtimestamp(
             self._start_time).strftime("%Y-%m-%d_%H-%M-%S")
 
@@ -168,20 +169,23 @@ class TrialRunner(object):
         """Saves execution state to `self._metadata_checkpoint_dir`.
 
         Overwrites the current session checkpoint, which starts when self
-        is instantiated.
+        is instantiated. Only runs every 10 seconds.
         """
         if not self._metadata_checkpoint_dir:
+            return
+        if time.time() - self._last_checkpoint_timestamp < 10:
             return
         metadata_checkpoint_dir = self._metadata_checkpoint_dir
         if not os.path.exists(metadata_checkpoint_dir):
             os.makedirs(metadata_checkpoint_dir)
+        self._last_checkpoint_timestamp = time.time()
         runner_state = {
             "checkpoints": list(
                 self.trial_executor.get_checkpoints().values()),
             "runner_data": self.__getstate__(),
             "stats": {
                 "start_time": self._start_time,
-                "timestamp": time.time()
+                "timestamp": self._last_checkpoint_timestamp
             }
         }
         tmp_file_name = os.path.join(metadata_checkpoint_dir,
