472c36ed1eded314f9198b2b8a0bc0bf30b7f703
Zhijun Fu <37800433+zhijunfu@users.noreply.github.com>
2019-06-12 10:10:12 +0800

    [core worker] add task submission & execution interface (#4922)

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -1,9 +1,10 @@
-#include "core_worker.h"
-#include "context.h"
+#include "ray/core_worker/core_worker.h"
+#include "ray/core_worker/context.h"
 
 namespace ray {
 
-CoreWorker::CoreWorker(const enum WorkerType worker_type, const enum Language language,
+CoreWorker::CoreWorker(const enum WorkerType worker_type,
+                       const enum WorkerLanguage language,
                        const std::string &store_socket, const std::string &raylet_socket,
                        DriverID driver_id)
     : worker_type_(worker_type),
@@ -11,20 +12,28 @@ CoreWorker::CoreWorker(const enum WorkerType worker_type, const enum Language la
       worker_context_(worker_type, driver_id),
       store_socket_(store_socket),
       raylet_socket_(raylet_socket),
+      is_initialized_(false),
       task_interface_(*this),
       object_interface_(*this),
-      task_execution_interface_(*this) {}
+      task_execution_interface_(*this) {
+  switch (language_) {
+  case ray::WorkerLanguage::JAVA:
+    task_language_ = ::Language::JAVA;
+    break;
+  case ray::WorkerLanguage::PYTHON:
+    task_language_ = ::Language::PYTHON;
+    break;
+  default:
+    RAY_LOG(FATAL) << "Unsupported worker language: " << static_cast<int>(language_);
+    break;
+  }
+}
 
 Status CoreWorker::Connect() {
   // connect to plasma.
   RAY_ARROW_RETURN_NOT_OK(store_client_.Connect(store_socket_));
 
   // connect to raylet.
-  ::Language lang = ::Language::PYTHON;
-  if (language_ == ray::Language::JAVA) {
-    lang = ::Language::JAVA;
-  }
-
   // TODO: currently RayletClient would crash in its constructor if it cannot
   // connect to Raylet after a number of retries, this needs to be changed
   // so that the worker (java/python .etc) can retrieve and handle the error
@@ -32,7 +41,8 @@ Status CoreWorker::Connect() {
   raylet_client_ = std::unique_ptr<RayletClient>(
       new RayletClient(raylet_socket_, worker_context_.GetWorkerID(),
                        (worker_type_ == ray::WorkerType::WORKER),
-                       worker_context_.GetCurrentDriverID(), lang));
+                       worker_context_.GetCurrentDriverID(), task_language_));
+  is_initialized_ = true;
   return Status::OK();
 }
 
--- src/ray/core_worker/task_execution.h
+++ src/ray/core_worker/task_execution.h
@@ -1,13 +1,17 @@
 #ifndef RAY_CORE_WORKER_TASK_EXECUTION_H
 #define RAY_CORE_WORKER_TASK_EXECUTION_H
 
-#include "common.h"
 #include "ray/common/buffer.h"
 #include "ray/common/status.h"
+#include "ray/core_worker/common.h"
 
 namespace ray {
 
 class CoreWorker;
+
+namespace raylet {
+class TaskSpecification;
+}
 
 /// The interface that contains all `CoreWorker` methods that are related to task
 /// execution.
@@ -20,13 +24,26 @@ class CoreWorkerTaskExecutionInterface {
   /// \param ray_function[in] Information about the function to execute.
   /// \param args[in] Arguments of the task.
   /// \return Status.
-  using TaskExecutor = std::function<Status(const RayFunction &ray_function,
-                                            const std::vector<Buffer> &args)>;
+  using TaskExecutor = std::function<Status(
+      const RayFunction &ray_function, const std::vector<std::shared_ptr<Buffer>> &args,
+      const TaskID &task_id, int num_returns)>;
 
   /// Start receving and executes tasks in a infinite loop.
-  void Start(const TaskExecutor &executor);
+  /// \return Status.
+  Status Run(const TaskExecutor &executor);
 
  private:
+  /// Build arguments for task executor. This would loop through all the arguments
+  /// in task spec, and for each of them that's passed by reference (ObjectID),
+  /// fetch its content from store and; for arguments that are passed by value,
+  /// just copy their content.
+  ///
+  /// \param spec[in] Task specification.
+  /// \param args[out] The arguments for passing to task executor.
+  ///
+  Status BuildArgsForExecutor(const raylet::TaskSpecification &spec,
+                              std::vector<std::shared_ptr<Buffer>> *args);
+
   /// Reference to the parent CoreWorker instance.
   CoreWorker &core_worker_;
 };
