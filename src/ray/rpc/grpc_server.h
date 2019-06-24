7f635b479ed4d3e5b5a67d14a5a062b3266fa77c
Zhijun Fu <pingfu.fzj@antfin.com>
2019-06-24 16:02:29 +0800

    refactor grpc server

--- src/ray/rpc/grpc_server.h
+++ src/ray/rpc/grpc_server.h
@@ -12,7 +12,9 @@
 namespace ray {
 namespace rpc {
 
-/// Base class that represents an abstract gRPC server.
+class GrpcService;
+
+/// Class that represents an gRPC server.
 ///
 /// A `GrpcServer` listens on a specific port. It owns
 /// 1) a `ServerCompletionQueue` that is used for polling events from gRPC,
@@ -30,9 +32,8 @@ class GrpcServer {
   ///  will be chosen.
   /// \param[in] main_service The main event loop, to which service handler functions
   /// will be posted.
-  GrpcServer(const std::string &name, const uint32_t port,
-             boost::asio::io_service &main_service)
-      : name_(name), port_(port), main_service_(main_service) {}
+  GrpcServer(const std::string &name, const uint32_t port)
+      : name_(name), port_(port) {}
 
   /// Destruct this gRPC server.
   ~GrpcServer() {
@@ -46,36 +47,24 @@ class GrpcServer {
   /// Get the port of this gRPC server.
   int GetPort() const { return port_; }
 
- protected:
-  /// Subclasses should implement this method and register one or multiple gRPC services
-  /// to the given `ServerBuilder`.
+  /// Register a grpc service.
   ///
-  /// \param[in] builder The `ServerBuilder` instance to register services to.
-  virtual void RegisterServices(grpc::ServerBuilder &builder) = 0;
+  /// \param[in] builder The `GrpcService` to register to this server.
+  void RegisterService(GrpcService &service);
 
-  /// Subclasses should implement this method to initialize the `ServerCallFactory`
-  /// instances, as well as specify maximum number of concurrent requests that gRPC
-  /// server can "accept" (not "handle"). Each factory will be used to create
-  /// `accept_concurrency` `ServerCall` objects, each of which will be used to accept and
-  /// handle an incoming request.
-  ///
-  /// \param[out] server_call_factories_and_concurrencies The `ServerCallFactory` objects,
-  /// and the maximum number of concurrent requests that gRPC server can accept.
-  virtual void InitServerCallFactories(
-      std::vector<std::pair<std::unique_ptr<ServerCallFactory>, int>>
-          *server_call_factories_and_concurrencies) = 0;
+ protected:
 
   /// This function runs in a background thread. It keeps polling events from the
   /// `ServerCompletionQueue`, and dispaches the event to the `ServiceHandler` instances
   /// via the `ServerCall` objects.
   void PollEventsFromCompletionQueue();
 
-  /// The main event loop, to which the service handler functions will be posted.
-  boost::asio::io_service &main_service_;
   /// Name of this server, used for logging and debugging purpose.
   const std::string name_;
   /// Port of this server.
   int port_;
+  /// The `grpc::Service` objects which should be registered to `ServerBuilder`.
+  std::vector<std::reference_wrapper<grpc::Service>> services_;
   /// The `ServerCallFactory` objects, and the maximum number of concurrent requests that
   /// gRPC server can accept.
   std::vector<std::pair<std::unique_ptr<ServerCallFactory>, int>>
@@ -84,6 +73,50 @@ class GrpcServer {
   std::unique_ptr<grpc::ServerCompletionQueue> cq_;
   /// The `Server` object.
   std::unique_ptr<grpc::Server> server_;
+};
+
+
+/// Base class that represents an abstract gRPC service.
+///
+/// Subclass should implement `InitServerCallFactories` to decide
+/// which kinds of requests this server should accept.
+class GrpcService {
+ public:
+  /// Constructor.
+  ///
+  /// \param[in] name Name of this server, used for logging and debugging purpose.
+  /// \param[in] port The port to bind this server to. If it's 0, a random available port
+  ///  will be chosen.
+  /// \param[in] main_service The main event loop, to which service handler functions
+  /// will be posted.
+  GrpcService(boost::asio::io_service &main_service)
+      : main_service_(main_service) {}
+
+  /// Destruct this gRPC service.
+  ~GrpcService() {}
+
+ protected:
+  /// TODO
+  virtual grpc::Service &GetGrpcService() = 0;
+
+  /// Subclasses should implement this method to initialize the `ServerCallFactory`
+  /// instances, as well as specify maximum number of concurrent requests that gRPC
+  /// server can "accept" (not "handle"). Each factory will be used to create
+  /// `accept_concurrency` `ServerCall` objects, each of which will be used to accept and
+  /// handle an incoming request.
+  ///
+  /// \param[out] service The `grpc::Service` object.
+  /// \param[out] server_call_factories_and_concurrencies The `ServerCallFactory` objects,
+  /// and the maximum number of concurrent requests that gRPC server can accept.
+  virtual void InitServerCallFactories(
+      const std::unique_ptr<grpc::ServerCompletionQueue> &cq,
+      std::vector<std::pair<std::unique_ptr<ServerCallFactory>, int>>
+          *server_call_factories_and_concurrencies) = 0;
+
+  /// The main event loop, to which the service handler functions will be posted.
+  boost::asio::io_service &main_service_;
+
+  friend class GrpcServer;
 };
 
 }  // namespace rpc
