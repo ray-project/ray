2c793f6666c06c05eed6790adf9eab6358be6587
Philipp Moritz <pcmoritz@gmail.com>
2019-06-25 11:35:23 -0700

    fix

--- build.sh
+++ build.sh
@@ -102,7 +102,7 @@ pushd "$BUILD_DIR"
 # the commit listed in the command.
 $PYTHON_EXECUTABLE -m pip install \
     --target="$ROOT_DIR/python/ray/pyarrow_files" pyarrow==0.14.0.RAY \
-    --find-links https://s3-us-west-2.amazonaws.com/arrow-wheels/01b02ea41cd3f4522c50285805b2203c61225137/index.html
+    --find-links https://s3-us-west-2.amazonaws.com/arrow-wheels/6187b58f96d04de72d4110b25147b654ae267c9b/index.html
 export PYTHON_BIN_PATH="$PYTHON_EXECUTABLE"
 
 if [ "$RAY_BUILD_JAVA" == "YES" ]; then
