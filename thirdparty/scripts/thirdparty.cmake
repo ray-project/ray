3ca8440a1cc881c3e668a467b5c7bd42b0457b47
Philipp Moritz <pcmoritz@gmail.com>
2019-06-30 20:30:23 -0700

    remove scripts

--- thirdparty/scripts/thirdparty.cmake
+++
@@ -1,6 +0,0 @@
-
-# add all thirdparty related path definitions here
-
-list(APPEND CMAKE_MODULE_PATH
-            ${CMAKE_CURRENT_LIST_DIR}/../build/arrow/python/cmake_modules)
-message (WARNING ${CMAKE_MODULE_PATH})
