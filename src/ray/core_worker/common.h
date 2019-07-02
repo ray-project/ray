e1d374ba6d95b1b780538961a7dbea2036559d9d
Kai Yang <kfstorm@outlook.com>
2019-07-02 16:48:43 +0800

    [Core worker] Serialize ActorHandle in core worker. Make ActorHandle thread safe. (#5034)
    * Serialize ActorHandle in core worker. Make ActorHandle thread safe.
    
    * Address comments
    
    * Address comments
    
    * Address comments
    
    * Address comments
    
    * lint
    
    * Address comments
    
    * Address comments
    
    * Address comments
    
    * Address comments
    
    * Minor update
    
    * Address comments
    
    * lint

--- src/ray/core_worker/common.h
+++ src/ray/core_worker/common.h
@@ -5,6 +5,7 @@
 
 #include "ray/common/buffer.h"
 #include "ray/common/id.h"
+#include "ray/protobuf/gcs.pb.h"
 #include "ray/raylet/raylet_client.h"
 #include "ray/raylet/task_spec.h"
 
@@ -13,13 +14,10 @@ namespace ray {
 /// Type of this worker.
 enum class WorkerType { WORKER, DRIVER };
 
-/// Language of Ray tasks and workers.
-enum class WorkerLanguage { PYTHON, JAVA };
-
 /// Information about a remote function.
 struct RayFunction {
   /// Language of the remote function.
-  const WorkerLanguage language;
+  const ray::rpc::Language language;
   /// Function descriptor of the remote function.
   const std::vector<std::string> function_descriptor;
 };
@@ -108,6 +106,48 @@ class TaskSpec {
 enum class StoreProviderType { PLASMA };
 
 enum class TaskTransportType { RAYLET };
+
+/// Translate from ray::rpc::Language to Language type (required by raylet client).
+///
+/// \param[in] language Language for a task.
+/// \return Translated task language.
+inline ::Language ToRayletTaskLanguage(ray::rpc::Language language) {
+  switch (language) {
+  case ray::rpc::Language::JAVA:
+    return ::Language::JAVA;
+    break;
+  case ray::rpc::Language::PYTHON:
+    return ::Language::PYTHON;
+    break;
+  case ray::rpc::Language::CPP:
+    return ::Language::CPP;
+    break;
+  default:
+    RAY_LOG(FATAL) << "Invalid language specified: " << static_cast<int>(language);
+    break;
+  }
+}
+
+/// Translate from Language to ray::rpc::Language type (required by core worker).
+///
+/// \param[in] language Language for a task.
+/// \return Translated task language.
+inline ray::rpc::Language ToRpcTaskLanguage(::Language language) {
+  switch (language) {
+  case Language::JAVA:
+    return ray::rpc::Language::JAVA;
+    break;
+  case Language::PYTHON:
+    return ray::rpc::Language::PYTHON;
+    break;
+  case Language::CPP:
+    return ray::rpc::Language::CPP;
+    break;
+  default:
+    RAY_LOG(FATAL) << "Invalid language specified: " << static_cast<int>(language);
+    break;
+  }
+}
 
 }  // namespace ray
 
--- src/ray/core_worker/core_worker_test.cc
+++ src/ray/core_worker/core_worker_test.cc
@@ -124,7 +124,7 @@ class CoreWorkerTest : public ::testing::Test {
   void TearDown() {}
 
   void TestNormalTask(const std::unordered_map<std::string, double> &resources) {
-    CoreWorker driver(WorkerType::DRIVER, WorkerLanguage::PYTHON,
+    CoreWorker driver(WorkerType::DRIVER, ray::rpc::Language::PYTHON,
                       raylet_store_socket_names_[0], raylet_socket_names_[0],
                       JobID::FromRandom());
 
@@ -134,7 +134,7 @@ class CoreWorkerTest : public ::testing::Test {
 
       auto buffer1 = std::make_shared<LocalMemoryBuffer>(array1, sizeof(array1));
 
-      RayFunction func{ray::WorkerLanguage::PYTHON, {}};
+      RayFunction func{ray::rpc::Language::PYTHON, {}};
       std::vector<TaskArg> args;
       args.emplace_back(TaskArg::PassByValue(buffer1));
 
@@ -165,7 +165,7 @@ class CoreWorkerTest : public ::testing::Test {
       std::vector<TaskArg> args;
       args.emplace_back(TaskArg::PassByReference(object_id));
 
-      RayFunction func{ray::WorkerLanguage::PYTHON, {}};
+      RayFunction func{ray::rpc::Language::PYTHON, {}};
       TaskOptions options;
 
       std::vector<ObjectID> return_ids;
@@ -184,7 +184,7 @@ class CoreWorkerTest : public ::testing::Test {
   }
 
   void TestActorTask(const std::unordered_map<std::string, double> &resources) {
-    CoreWorker driver(WorkerType::DRIVER, WorkerLanguage::PYTHON,
+    CoreWorker driver(WorkerType::DRIVER, ray::rpc::Language::PYTHON,
                       raylet_store_socket_names_[0], raylet_socket_names_[0],
                       JobID::FromRandom());
 
@@ -195,7 +195,7 @@ class CoreWorkerTest : public ::testing::Test {
       uint8_t array[] = {1, 2, 3};
       auto buffer = std::make_shared<LocalMemoryBuffer>(array, sizeof(array));
 
-      RayFunction func{ray::WorkerLanguage::PYTHON, {}};
+      RayFunction func{ray::rpc::Language::PYTHON, {}};
       std::vector<TaskArg> args;
       args.emplace_back(TaskArg::PassByValue(buffer));
 
@@ -223,7 +223,7 @@ class CoreWorkerTest : public ::testing::Test {
 
       TaskOptions options{1, resources};
       std::vector<ObjectID> return_ids;
-      RayFunction func{ray::WorkerLanguage::PYTHON, {}};
+      RayFunction func{ray::rpc::Language::PYTHON, {}};
       RAY_CHECK_OK(driver.Tasks().SubmitActorTask(*actor_handle, func, args, options,
                                                   &return_ids));
       RAY_CHECK(return_ids.size() == 1);
@@ -302,8 +302,41 @@ TEST_F(ZeroNodeTest, TestWorkerContext) {
   ASSERT_EQ(context.GetNextPutIndex(), 3);
 }
 
+TEST_F(ZeroNodeTest, TestActorHandle) {
+  ActorHandle handle1(ActorID::FromRandom(), ActorHandleID::FromRandom(),
+                      ray::rpc::Language::JAVA,
+                      {"org.ray.exampleClass", "exampleMethod", "exampleSignature"});
+
+  auto forkedHandle1 = handle1.Fork();
+  ASSERT_EQ(1, handle1.NumForks());
+  ASSERT_EQ(handle1.ActorID(), forkedHandle1.ActorID());
+  ASSERT_NE(handle1.ActorHandleID(), forkedHandle1.ActorHandleID());
+  ASSERT_EQ(handle1.ActorLanguage(), forkedHandle1.ActorLanguage());
+  ASSERT_EQ(handle1.ActorCreationTaskFunctionDescriptor(),
+            forkedHandle1.ActorCreationTaskFunctionDescriptor());
+  ASSERT_EQ(handle1.ActorCursor(), forkedHandle1.ActorCursor());
+  ASSERT_EQ(0, forkedHandle1.TaskCounter());
+  ASSERT_EQ(0, forkedHandle1.NumForks());
+  auto forkedHandle2 = handle1.Fork();
+  ASSERT_EQ(2, handle1.NumForks());
+  ASSERT_EQ(0, forkedHandle2.TaskCounter());
+  ASSERT_EQ(0, forkedHandle2.NumForks());
+
+  std::string buffer;
+  handle1.Serialize(&buffer);
+  auto handle2 = ActorHandle::Deserialize(buffer);
+  ASSERT_EQ(handle1.ActorID(), handle2.ActorID());
+  ASSERT_EQ(handle1.ActorHandleID(), handle2.ActorHandleID());
+  ASSERT_EQ(handle1.ActorLanguage(), handle2.ActorLanguage());
+  ASSERT_EQ(handle1.ActorCreationTaskFunctionDescriptor(),
+            handle2.ActorCreationTaskFunctionDescriptor());
+  ASSERT_EQ(handle1.ActorCursor(), handle2.ActorCursor());
+  ASSERT_EQ(handle1.TaskCounter(), handle2.TaskCounter());
+  ASSERT_EQ(handle1.NumForks(), handle2.NumForks());
+}
+
 TEST_F(SingleNodeTest, TestObjectInterface) {
-  CoreWorker core_worker(WorkerType::DRIVER, WorkerLanguage::PYTHON,
+  CoreWorker core_worker(WorkerType::DRIVER, ray::rpc::Language::PYTHON,
                          raylet_store_socket_names_[0], raylet_socket_names_[0],
                          JobID::FromRandom());
 
@@ -367,11 +400,11 @@ TEST_F(SingleNodeTest, TestObjectInterface) {
 }
 
 TEST_F(TwoNodeTest, TestObjectInterfaceCrossNodes) {
-  CoreWorker worker1(WorkerType::DRIVER, WorkerLanguage::PYTHON,
+  CoreWorker worker1(WorkerType::DRIVER, ray::rpc::Language::PYTHON,
                      raylet_store_socket_names_[0], raylet_socket_names_[0],
                      JobID::FromRandom());
 
-  CoreWorker worker2(WorkerType::DRIVER, WorkerLanguage::PYTHON,
+  CoreWorker worker2(WorkerType::DRIVER, ray::rpc::Language::PYTHON,
                      raylet_store_socket_names_[1], raylet_socket_names_[1],
                      JobID::FromRandom());
 
@@ -458,7 +491,7 @@ TEST_F(TwoNodeTest, TestActorTaskCrossNodes) {
 
 TEST_F(SingleNodeTest, TestCoreWorkerConstructorFailure) {
   try {
-    CoreWorker core_worker(WorkerType::DRIVER, WorkerLanguage::PYTHON, "",
+    CoreWorker core_worker(WorkerType::DRIVER, ray::rpc::Language::PYTHON, "",
                            raylet_socket_names_[0], JobID::FromRandom());
   } catch (const std::exception &e) {
     std::cout << "Caught exception when constructing core worker: " << e.what();
