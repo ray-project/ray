1868a0dc2544bd60316eabdedb8208df87fdd5e6
dusan <dusanix@gmail.com>
2019-07-03 10:06:35 +0200

    Add a bit more verbose tensorboard entries for utilization monitor

--- python/ray/tune/util.py
+++ python/ray/tune/util.py
@@ -63,14 +63,14 @@ class UtilMonitor(Thread):
     def _read_utilization(self):
         with self.lock:
             if psutil is not None:
-                self.values["cpu"].append(
+                self.values["cpu_util_percent"].append(
                     float(psutil.cpu_percent(interval=None)))
-                self.values["ram"].append(
+                self.values["ram_util_percent"].append(
                     float(getattr(psutil.virtual_memory(), "percent")))
             if GPUtil is not None:
                 for gpu in GPUtil.getGPUs():
-                    self.values["gpu" + str(gpu.id)].append(float(gpu.load))
-                    self.values["vram" + str(gpu.id)].append(
+                    self.values["gpu_util_percent" + str(gpu.id)].append(float(gpu.load))
+                    self.values["vram_util_percent" + str(gpu.id)].append(
                         float(gpu.memoryUtil))
 
     def get_data(self):
