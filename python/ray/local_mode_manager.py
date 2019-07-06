517724b24df187924c27d76ffe21245a70c61e87
Robert Nishihara <robertnishihara@gmail.com>
2019-07-06 16:24:06 -0700

    Update local_mode_manager.py

--- python/ray/local_mode_manager.py
+++ python/ray/local_mode_manager.py
@@ -98,8 +98,8 @@ class LocalModeManager(object):
         results = []
         for object_id in object_ids:
             if not isinstance(object_id, LocalModeObjectID):
-                raise TypeError("Only LocalModeObjectIDs are supported" +
-                                "when running in LOCAL_MODE. Using " +
+                raise TypeError("Only LocalModeObjectIDs are supported "
+                                "when running in LOCAL_MODE. Using "
                                 "user-generated ObjectIDs will fail.")
             if not hasattr(object_id, "value"):
                 raise KeyError("Value for {} not found".format(object_id))
@@ -121,8 +121,8 @@ class LocalModeManager(object):
         """
         for object_id in object_ids:
             if not isinstance(object_id, LocalModeObjectID):
-                raise TypeError("Only LocalModeObjectIDs are supported" +
-                                "when running in LOCAL_MODE. Using " +
+                raise TypeError("Only LocalModeObjectIDs are supported "
+                                "when running in LOCAL_MODE. Using "
                                 "user-generated ObjectIDs will fail.")
             try:
                 del object_id.value
