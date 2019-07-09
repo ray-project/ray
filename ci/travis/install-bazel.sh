486d2c7b5536610471a68692f79ad43d87df2d05
Hao Chen <chenh1024@gmail.com>
2019-07-09 14:16:12 +0800

    debug

--- ci/travis/install-bazel.sh
+++ ci/travis/install-bazel.sh
@@ -26,4 +26,5 @@ if [[ "$TRAVIS" == "true"  ]]; then
   # Use bazel disk cache if this script is running in Travis.
   mkdir -p $HOME/ray-bazel-cache
   echo "build --disk_cache=$HOME/ray-bazel-cache" >> $HOME/.bazelrc
+  cat $HOME/.bazelrc
 fi
