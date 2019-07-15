ca96fa56a549a6f74246e629026c0aec09115637
Hao Chen <chenh1024@gmail.com>
2019-07-15 12:22:26 +0800

    Fix client call

--- src/ray/rpc/client_call.h
+++ src/ray/rpc/client_call.h
@@ -6,6 +6,7 @@
 
 #include "ray/common/grpc_util.h"
 #include "ray/common/status.h"
+#include "ray/rpc/cq_tag.h"
 
 namespace ray {
 namespace rpc {
@@ -38,20 +39,26 @@ class ClientCall {
 
 class ClientCallManager;
 
-/// Reprents the client callback function of a particular rpc method.
+/// Represents the client callback function of a particular rpc method.
 ///
 /// \tparam Reply Type of the reply message.
 template <class Reply>
 using ClientCallback = std::function<void(const Status &status, const Reply &reply)>;
 
-/// Implementaion of the `ClientCall`. It represents a `ClientCall` for a particular
+/// Implementation of the `ClientCall`. It represents a `ClientCall` for a particular
 /// RPC method.
 ///
 /// \tparam Reply Type of the Reply message.
 template <class Reply>
 class ClientCallImpl : public ClientCall {
  public:
+  /// Constructor.
+  ///
+  /// \param[in] callback The callback function to handle the reply.
+  explicit ClientCallImpl(const ClientCallback<Reply> &callback) : callback_(callback) {}
+
   Status GetStatus() override { return GrpcStatusToRayStatus(status_); }
+
   void OnReplyReceived() override {
     if (callback_ != nullptr) {
       callback_(GrpcStatusToRayStatus(status_), reply_);
@@ -59,11 +66,6 @@ class ClientCallImpl : public ClientCall {
   }
 
  private:
-  /// Constructor.
-  ///
-  /// \param[in] callback The callback function to handle the reply.
-  ClientCallImpl(const ClientCallback<Reply> &callback) : callback_(callback) {}
-
   /// The reply message.
   Reply reply_;
 
@@ -83,7 +85,10 @@ class ClientCallImpl : public ClientCall {
   friend class ClientCallManager;
 };
 
-/// Peprents the generic signature of a `FooService::Stub::PrepareAsyncBar`
+
+using ClientCallTag = CqTag<ClientCall>;
+
+/// Represents the generic signature of a `FooService::Stub::PrepareAsyncBar`
 /// function, where `Foo` is the service name and `Bar` is the rpc method name.
 ///
 /// \tparam GrpcService Type of the gRPC-generated service class.
@@ -100,14 +105,14 @@ using PrepareAsyncFunction = std::unique_ptr<grpc::ClientAsyncResponseReader<Rep
 /// It maintains a thread that keeps polling events from `CompletionQueue`, and post
 /// the callback function to the main event loop when a reply is received.
 ///
-/// Mutiple clients can share one `ClientCallManager`.
+/// Multiple clients can share one `ClientCallManager`.
 class ClientCallManager {
  public:
   /// Constructor.
   ///
   /// \param[in] main_service The main event loop, to which the callback functions will be
   /// posted.
-  ClientCallManager(boost::asio::io_service &main_service) : main_service_(main_service) {
+  explicit ClientCallManager(boost::asio::io_service &main_service) : main_service_(main_service) {
     // Start the polling thread.
     std::thread polling_thread(&ClientCallManager::PollEventsFromCompletionQueue, this);
     polling_thread.detach();
@@ -127,40 +132,41 @@ class ClientCallManager {
   /// \tparam Request Type of the request message.
   /// \tparam Reply Type of the reply message.
   template <class GrpcService, class Request, class Reply>
-  ClientCall *CreateCall(
+  std::shared_ptr<ClientCall> CreateCall(
       typename GrpcService::Stub &stub,
       const PrepareAsyncFunction<GrpcService, Request, Reply> prepare_async_function,
       const Request &request, const ClientCallback<Reply> &callback) {
-    // Create a new `ClientCall` object. This object will eventuall be deleted in the
-    // `ClientCallManager::PollEventsFromCompletionQueue` when reply is received.
-    auto call = new ClientCallImpl<Reply>(callback);
+    auto call = std::make_shared<ClientCallImpl<Reply>>(callback);
     // Send request.
     call->response_reader_ =
         (stub.*prepare_async_function)(&call->context_, request, &cq_);
     call->response_reader_->StartCall();
-    call->response_reader_->Finish(&call->reply_, &call->status_, (void *)call);
+    // Create a new tag object. This object will eventually be deleted in the
+    // `ClientCallManager::PollEventsFromCompletionQueue` when reply is received.
+    auto tag = new ClientCallTag(call);
+    call->response_reader_->Finish(&call->reply_, &call->status_, (void *)tag);
     return call;
   }
 
  private:
   /// This function runs in a background thread. It keeps polling events from the
-  /// `CompletionQueue`, and dispaches the event to the callbacks via the `ClientCall`
+  /// `CompletionQueue`, and dispatches the event to the callbacks via the `ClientCall`
   /// objects.
   void PollEventsFromCompletionQueue() {
     void *got_tag;
     bool ok = false;
     // Keep reading events from the `CompletionQueue` until it's shutdown.
     while (cq_.Next(&got_tag, &ok)) {
-      auto *call = reinterpret_cast<ClientCall *>(got_tag);
+      auto tag = reinterpret_cast<ClientCallTag *>(got_tag);
       if (ok) {
         // Post the callback to the main event loop.
-        main_service_.post([call]() {
-          call->OnReplyReceived();
+        main_service_.post([tag]() {
+          tag->GetCall()->OnReplyReceived();
           // The call is finished, we can delete the `ClientCall` object now.
-          delete call;
+          delete tag;
         });
       } else {
-        delete call;
+        delete tag;
       }
     }
   }
