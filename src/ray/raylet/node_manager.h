81e108e2d87da35cb21b044ecbd72ceb5cd69504
Eric Liang <ekhliang@gmail.com>
2019-07-18 14:41:57 -0700

    doc

--- src/ray/raylet/node_manager.h
+++ src/ray/raylet/node_manager.h
@@ -193,6 +193,11 @@ class NodeManager : public rpc::NodeManagerServiceHandler {
   /// \return Void.
   void TreatTaskAsFailed(const Task &task, const ErrorType &error_type,
                          const ObjectID *required_object_id = nullptr);
+  /// Mark the specified objects as failed with the given error type.
+  ///
+  /// \param error_type The type of the error that caused this task to fail.
+  /// \param object_ids The object ids to store error messages into.
+  /// \param job_id The optional job to push errors to if the writes fail.
   void MarkObjectsAsFailed(const ErrorType &error_type,
                            const std::vector<plasma::ObjectID> object_ids,
                            const JobID *job_id = nullptr);
