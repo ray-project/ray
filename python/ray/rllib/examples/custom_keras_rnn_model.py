d796b857c4e154829a8e525c9e215b4fbceb90bb
Eric Liang <ekhliang@gmail.com>
2019-07-23 20:23:22 -0700

    wip

--- python/ray/rllib/examples/custom_keras_rnn_model.py
+++ python/ray/rllib/examples/custom_keras_rnn_model.py
@@ -41,8 +41,8 @@ class MyKerasRNN(RecurrentTFModelV2):
         # Define input layers
         input_layer = tf.keras.layers.Input(
             shape=(None, obs_space.shape[0]), name="inputs")
-        state_in_c = tf.keras.layers.Input(shape=(cell_size, ), name="c")
         state_in_h = tf.keras.layers.Input(shape=(cell_size, ), name="h")
+        state_in_c = tf.keras.layers.Input(shape=(cell_size, ), name="c")
         seq_in = tf.keras.layers.Input(shape=(), name="seq_in")
 
         # Preprocess observation with a hidden layer and send to LSTM cell
@@ -52,7 +52,7 @@ class MyKerasRNN(RecurrentTFModelV2):
             cell_size, return_sequences=True, return_state=True, name="lstm")(
                 inputs=dense1,
                 mask=tf.sequence_mask(seq_in),
-                initial_state=[state_in_c, state_in_h])
+                initial_state=[state_in_h, state_in_c])
 
         # Postprocess LSTM output with another hidden layer and compute values
         dense2 = tf.keras.layers.Dense(
