bf9199ad77b7ea900aefd2624374c78a6e6c49d8
Eric Liang <ekhliang@gmail.com>
2019-07-25 11:02:53 -0700

    [rllib] ModelV2 support for pytorch (#5249)

--- python/ray/rllib/models/tf/tf_modelv2.py
+++ python/ray/rllib/models/tf/tf_modelv2.py
@@ -3,13 +3,18 @@ from __future__ import division
 from __future__ import print_function
 
 from ray.rllib.models.modelv2 import ModelV2
+from ray.rllib.utils.annotations import PublicAPI
 from ray.rllib.utils import try_import_tf
 
 tf = try_import_tf()
 
 
+@PublicAPI
 class TFModelV2(ModelV2):
-    """TF version of ModelV2."""
+    """TF version of ModelV2.
+
+    Note that this class by itself is not a valid model unless you
+    implement forward() in a subclass."""
 
     def __init__(self, obs_space, action_space, num_outputs, model_config,
                  name):
@@ -21,9 +26,22 @@ class TFModelV2(ModelV2):
             model_config,
             name,
             framework="tf")
+        self.var_list = []
 
     def update_ops(self):
         """Return the list of update ops for this model.
 
         For example, this should include any BatchNorm update ops."""
         return []
+
+    def register_variables(self, variables):
+        """Register the given list of variables with this model."""
+        self.var_list.extend(variables)
+
+    def variables(self):
+        """Returns the list of variables for this model."""
+        return list(self.var_list)
+
+    def trainable_variables(self):
+        """Returns the list of trainable variables for this model."""
+        return [v for v in self.variables() if v.trainable]
