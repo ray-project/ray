13932b1f5b5cffd8d87e028e7368b5bc67e4f77b
Richard Liaw <rliaw@berkeley.edu>
2019-07-26 16:16:59 -0700

    small change

--- python/ray/experimental/sgd/examples/train_example.py
+++ python/ray/experimental/sgd/examples/train_example.py
@@ -9,13 +9,16 @@ from ray.experimental.sgd.tests.pytorch_utils import (
     model_creator, optimizer_creator, data_creator)
 
 
-def train_example(num_replicas):
+def train_example(num_replicas=1, use_gpu=False):
     trainer1 = PyTorchTrainer(
         model_creator,
         data_creator,
         optimizer_creator,
         num_replicas=num_replicas,
-        resources_per_replica=Resources(num_cpus=1, num_gpus=1, resources={}))
+        resources_per_replica=Resources(
+            num_cpus=1,
+            num_gpus=int(use_gpu),
+            resources={}))
     trainer1.train()
     trainer1.shutdown()
 
@@ -27,8 +30,13 @@ if __name__ == "__main__":
         required=True,
         type=str,
         help="the address to use for Redis")
+    parser.add_argument(
+        "--use-gpu",
+        action="store_true",
+        default=False,
+        help="Enables GPU training")
     args, _ = parser.parse_known_args()
 
     import ray
     ray.init(redis_address=args.redis_address)
-    train_example(num_replicas=8)
+    train_example(num_replicas=2, use_gpus=args.use_gpu)
