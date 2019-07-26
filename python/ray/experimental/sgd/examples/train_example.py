86ec2a4be80ced53e62762fa06bf651046109beb
Richard Liaw <rliaw@berkeley.edu>
2019-07-26 16:01:03 -0700

    lint

--- python/ray/experimental/sgd/examples/train_example.py
+++ python/ray/experimental/sgd/examples/train_example.py
@@ -3,14 +3,11 @@ from __future__ import division
 from __future__ import print_function
 
 import argparse
-import os
-import pytest
-import tempfile
-import torch
 from ray.experimental.sgd.pytorch import PyTorchTrainer, Resources
 
 from ray.experimental.sgd.tests.pytorch_utils import (
     model_creator, optimizer_creator, data_creator)
+
 
 def train_example(num_replicas):
     trainer1 = PyTorchTrainer(
@@ -18,19 +15,12 @@ def train_example(num_replicas):
         data_creator,
         optimizer_creator,
         num_replicas=num_replicas,
-        resources_per_replica=Resources(
-            num_cpus=1, num_gpus=1, resources={}))
+        resources_per_replica=Resources(num_cpus=1, num_gpus=1, resources={}))
     trainer1.train()
-
-    filename = os.path.join(tempfile.mkdtemp(), "checkpoint")
-    trainer1.save(filename)
-
-    model1 = trainer1.get_model()
-
     trainer1.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--redis-address",
