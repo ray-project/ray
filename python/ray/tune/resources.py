a17620a69d8742a020ef5c62fa505a7a4d309f65
Jichan Chung <jichan3751@gmail.com>
2019-07-26 01:05:35 -0700

    fix bugs

--- python/ray/tune/resources.py
+++ python/ray/tune/resources.py
@@ -2,6 +2,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from collections import namedtuple
+import logging
+import json
+
 logger = logging.getLogger(__name__)
 
 class Resources(
--- python/ray/tune/trial.py
+++ python/ray/tune/trial.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from collections import namedtuple
 import ray.cloudpickle as cloudpickle
 import copy
 from datetime import datetime
