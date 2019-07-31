4301f2a3b9492e875ed27b2a048757af02222d13
Kristian Hartikainen <hartikainen@berkeley.edu>
2019-07-30 17:09:14 -0700

    First implementation of sac model

--- python/ray/rllib/agents/sac/sac_model.py
+++ python/ray/rllib/agents/sac/sac_model.py
@@ -29,12 +29,10 @@ class SACModel(TFModelV2):
                  model_config,
                  name,
                  actor_hidden_activation="relu",
-                 actor_hiddens=(400, 300),
+                 actor_hiddens=(256, 256),
                  critic_hidden_activation="relu",
-                 critic_hiddens=(400, 300),
-                 parameter_noise=False,
-                 twin_q=False,
-                 exploration_ou_sigma=0.2):
+                 critic_hiddens=(256, 256),
+                 twin_q=False):
         """Initialize variables of this model.
 
         Extra model kwargs:
@@ -42,9 +40,7 @@ class SACModel(TFModelV2):
             actor_hiddens (list): hidden layers sizes for actor network
             critic_hidden_activation (str): activation for critic network
             critic_hiddens (list): hidden layers sizes for critic network
-            parameter_noise (bool): use param noise exploration
             twin_q (bool): build twin Q networks
-            exploration_ou_sigma (float): ou noise sigma for exploration
 
         Note that the core layers for forward() are not defined here, this
         only defines the layers for the output heads. Those layers for
@@ -52,8 +48,7 @@ class SACModel(TFModelV2):
         """
 
         super(SACModel, self).__init__(obs_space, action_space, num_outputs,
-                                        model_config, name)
-        self.exploration_ou_sigma = exploration_ou_sigma
+                                       model_config, name)
 
         self.action_dim = np.product(action_space.shape)
         self.model_out = tf.keras.layers.Input(
@@ -61,70 +56,66 @@ class SACModel(TFModelV2):
         self.actions = tf.keras.layers.Input(
             shape=(self.action_dim, ), name="actions")
 
-        def build_action_net(action_out):
-            activation = getattr(tf.nn, actor_hidden_activation)
-            i = 0
-            for hidden in actor_hiddens:
-                if parameter_noise:
-                    import tensorflow.contrib.layers as layers
-                    action_out = layers.fully_connected(
-                        action_out,
-                        num_outputs=hidden,
-                        activation_fn=activation,
-                        normalizer_fn=layers.layer_norm)
-                else:
-                    action_out = tf.layers.dense(
-                        action_out,
-                        units=hidden,
-                        activation=activation,
-                        name="action_hidden_{}".format(i))
-                i += 1
-            return tf.layers.dense(
-                action_out,
-                units=self.action_dim,
+        self.action_net = tf.keras.Sequential([
+            tf.keras.layers.Dense(
+                units=hidden,
+                activation=getattr(tf.nn, actor_hidden_activation),
+                name="action_hidden_{}".format(i)
+            )
+            for i, hidden in enumerate(actor_hiddens)
+        ] + [
+            tf.keras.layers.Dense(
+                units=self.action_dim * 2,
                 activation=None,
-                name="action_out")
+                name="action_out"
+            )
+        ])
 
-        action_scope = name + "/action_net"
+        # TODO(hartikainen): Remove the unnecessary Model call here
+        self.action_net = tf.keras.Model(
+            self.model_out, self.action_net(self.model_out))
 
-        # TODO(ekl) use keras layers instead of variable scopes
-        def build_action_net_scope(model_out):
-            with tf.variable_scope(action_scope, reuse=tf.AUTO_REUSE):
-                return build_action_net(model_out)
-
-        pi_out = tf.keras.layers.Lambda(build_action_net_scope)(self.model_out)
-        self.action_net = tf.keras.Model(self.model_out, pi_out)
         self.register_variables(self.action_net.variables)
 
-        # Noise vars for P network except for layer normalization vars
-        if parameter_noise:
-            with tf.variable_scope(action_scope, reuse=tf.AUTO_REUSE):
-                self._build_parameter_noise([
-                    var for var in self.action_net.variables
-                    if "LayerNorm" not in var.name
-                ])
+        def build_q_net(name, inputs):
+            q_net = tf.keras.Sequential([
+                tf.keras.layers.Concatenate(axis=1),
+            ] + [
+                tf.keras.layers.Dense(
+                    units=units,
+                    activation=getattr(tf.nn, critic_hidden_activation),
+                    name="{}_hidden_{}".format(name, i)
+                )
+                for i, units in enumerate(critic_hiddens)
+            ] + [
+                tf.keras.layers.Dense(
+                    units=1,
+                    activation=None,
+                    name="{}_out".format(name)
+                )
+            ])
 
-        def build_q_net(name, model_out, actions):
-            q_out = tf.keras.layers.Concatenate(axis=1)([model_out, actions])
-            activation = getattr(tf.nn, critic_hidden_activation)
-            for i, n in enumerate(critic_hiddens):
-                q_out = tf.keras.layers.Dense(
-                    n,
-                    name="{}_hidden_{}".format(name, i),
-                    activation=activation)(q_out)
-            q_out = tf.keras.layers.Dense(
-                1, activation=None, name="{}_out".format(name))(q_out)
-            return tf.keras.Model([model_out, actions], q_out)
+            # TODO(hartikainen): Remove the unnecessary Model call here
+            q_net = tf.keras.Model(inputs, q_net(inputs))
+            return q_net
 
         self.q_net = build_q_net("q", self.model_out, self.actions)
         self.register_variables(self.q_net.variables)
 
         if twin_q:
-            self.twin_q_net = build_q_net("twin_q", self.model_out,
-                                          self.actions)
+            self.twin_q_net = build_q_net(
+                "twin_q", self.model_out, self.actions)
             self.register_variables(self.twin_q_net.variables)
         else:
             self.twin_q_net = None
+
+        self.log_alpha = tf.Variable(
+            'log_alpha',
+            dtype=tf.float32,
+            initializer=0.0)
+        self.alpha = tf.exp(self.log_alpha)
+
+        self.register_variables([self.log_alpha])
 
     def forward(self, input_dict, state, seq_lens):
         """This generates the model_out tensor input.
@@ -190,57 +181,3 @@ class SACModel(TFModelV2):
 
         return self.q_net.variables + (self.twin_q_net.variables
                                        if self.twin_q_net else [])
-
-    def update_action_noise(self, session, distance_in_action_space,
-                            exploration_ou_sigma, cur_noise_scale):
-        """Update the model action noise settings.
-
-        This is called internally by the SAC policy."""
-
-        self.pi_distance = distance_in_action_space
-        if (distance_in_action_space < exploration_ou_sigma * cur_noise_scale):
-            # multiplying the sampled OU noise by noise scale is
-            # equivalent to multiplying the sigma of OU by noise scale
-            self.parameter_noise_sigma_val *= 1.01
-        else:
-            self.parameter_noise_sigma_val /= 1.01
-        self.parameter_noise_sigma.load(
-            self.parameter_noise_sigma_val, session=session)
-
-    def _build_parameter_noise(self, pnet_params):
-        assert pnet_params
-        self.parameter_noise_sigma_val = self.exploration_ou_sigma
-        self.parameter_noise_sigma = tf.get_variable(
-            initializer=tf.constant_initializer(
-                self.parameter_noise_sigma_val),
-            name="parameter_noise_sigma",
-            shape=(),
-            trainable=False,
-            dtype=tf.float32)
-        self.parameter_noise = []
-        # No need to add any noise on LayerNorm parameters
-        for var in pnet_params:
-            noise_var = tf.get_variable(
-                name=var.name.split(":")[0] + "_noise",
-                shape=var.shape,
-                initializer=tf.constant_initializer(.0),
-                trainable=False)
-            self.parameter_noise.append(noise_var)
-        remove_noise_ops = list()
-        for var, var_noise in zip(pnet_params, self.parameter_noise):
-            remove_noise_ops.append(tf.assign_add(var, -var_noise))
-        self.remove_noise_op = tf.group(*tuple(remove_noise_ops))
-        generate_noise_ops = list()
-        for var_noise in self.parameter_noise:
-            generate_noise_ops.append(
-                tf.assign(
-                    var_noise,
-                    tf.random_normal(
-                        shape=var_noise.shape,
-                        stddev=self.parameter_noise_sigma)))
-        with tf.control_dependencies(generate_noise_ops):
-            add_noise_ops = list()
-            for var, var_noise in zip(pnet_params, self.parameter_noise):
-                add_noise_ops.append(tf.assign_add(var, var_noise))
-            self.add_noise_op = tf.group(*tuple(add_noise_ops))
-        self.pi_distance = None
