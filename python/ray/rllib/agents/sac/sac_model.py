afb42adf1fd0a73e467d847c7650fa200da66a29
Eric Liang <ekhliang@gmail.com>
2019-08-01 10:44:50 -0700

    lazy squash

--- python/ray/rllib/agents/sac/sac_model.py
+++ python/ray/rllib/agents/sac/sac_model.py
@@ -13,19 +13,25 @@ tfp = try_import_tfp()
 SCALE_DIAG_MIN_MAX = (-20, 2)
 
 
-class SquashBijector(tfp.bijectors.Bijector):
-    def __init__(self, validate_args=False, name="tanh"):
-        super(SquashBijector, self).__init__(
-            forward_min_event_ndims=0, validate_args=validate_args, name=name)
+def SquashBijector():
+    # lazy def since it depends on tfp
+    class SquashBijector(tfp.bijectors.Bijector):
+        def __init__(self, validate_args=False, name="tanh"):
+            super(SquashBijector, self).__init__(
+                forward_min_event_ndims=0,
+                validate_args=validate_args,
+                name=name)
 
-    def _forward(self, x):
-        return tf.nn.tanh(x)
+        def _forward(self, x):
+            return tf.nn.tanh(x)
 
-    def _inverse(self, y):
-        return tf.atanh(y)
+        def _inverse(self, y):
+            return tf.atanh(y)
 
-    def _forward_log_det_jacobian(self, x):
-        return 2. * (np.log(2.) - x - tf.nn.softplus(-2. * x))
+        def _forward_log_det_jacobian(self, x):
+            return 2. * (np.log(2.) - x - tf.nn.softplus(-2. * x))
+
+    return SquashBijector()
 
 
 class SACModel(TFModelV2):
