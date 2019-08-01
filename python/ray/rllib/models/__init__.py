7a9a007b1307015dbcbb8e6034c937b5e0c290a0
Eric Liang <ekhliang@gmail.com>
2019-07-31 18:09:44 -0700

    legacy compat

--- python/ray/rllib/models/__init__.py
+++ python/ray/rllib/models/__init__.py
@@ -2,6 +2,8 @@ from ray.rllib.models.action_dist import ActionDistribution
 from ray.rllib.models.catalog import ModelCatalog, MODEL_DEFAULTS
 from ray.rllib.models.model import Model
 from ray.rllib.models.preprocessors import Preprocessor
+from ray.rllib.models.tf.fcnet_v1 import FullyConnectedNetwork
+from ray.rllib.models.tf.visionnet_v1 import VisionNetwork
 
 __all__ = [
     "ActionDistribution",
@@ -9,4 +11,6 @@ __all__ = [
     "Model",
     "Preprocessor",
     "MODEL_DEFAULTS",
+    "FullyConnectedNetwork",  # legacy
+    "VisionNetwork",  # legacy
 ]
