a17516ed45f15bed450ab2a9d15adb4e98140bb3
jovany-wang <kingchin1218@126.com>
2019-08-02 22:20:00 +0800

    Fix python

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -720,12 +720,16 @@ class Worker(object):
             assert isinstance(job_id, JobID)
 
             if actor_creation_id is not None and not actor_creation_id.is_nil():
-                # This is actor creation task.
+                # This is an actor creation task.
                 task_id = TaskID.for_actor_creation_task(actor_creation_id)
             elif actor_id is not None and not actor_id.is_nil():
-                # This is actor task.
+                # This is an actor task.
                 task_id = TaskID.for_actor_task(self.current_job_id,
                     self.current_task_id, self.task_context.task_index, actor_id)
+            else:
+                # This is a normal task.
+                task_id = TaskID.for_normal_task(self.current_job_id,
+                    self.current_task_id, self.task_context.task_index)
 
             task = ray._raylet.TaskSpec(
                 task_id,
