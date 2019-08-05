c9af921d35230537c9e83a54b7477a3e132b778d
Richard Liaw <rliaw@berkeley.edu>
2019-08-05 00:52:37 -0700

    lint

--- python/ray/autoscaler/__init__.py
+++ python/ray/autoscaler/__init__.py
@@ -1,7 +1,6 @@
 import logging
 from logging import FileHandler
 
-
 logger = logging.getLogger(__name__)
 file_handler_info = FileHandler("test.log", mode="w")
 file_handler_info.setLevel(logging.DEBUG)
