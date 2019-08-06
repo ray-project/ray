ba1b53102da9c1d6b099289a4c39fd08946d8cc3
Eric Liang <ekhliang@gmail.com>
2019-08-06 14:04:48 -0700

    fix op leak

--- rllib/policy/tf_policy.py
+++ rllib/policy/tf_policy.py
@@ -117,6 +117,7 @@ class TFPolicy(Policy):
         self._sampler = action_sampler
         self._is_training = self._get_is_training_placeholder()
         self._action_logp = action_logp
+        self._action_prob = tf.exp(self._action_logp)
         self._state_inputs = state_inputs or []
         self._state_outputs = state_outputs or []
         self._seq_lens = seq_lens
@@ -307,7 +308,7 @@ class TFPolicy(Policy):
         """
         if self._action_logp is not None:
             return {
-                ACTION_PROB: tf.exp(self._action_logp),
+                ACTION_PROB: self._action_prob,
                 ACTION_LOGP: self._action_logp,
             }
         else:
