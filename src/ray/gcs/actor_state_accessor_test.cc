3a7ecb9ee7f1ca9d1cf528fd8bb81601623dbb8a
Philipp Moritz <pcmoritz@gmail.com>
2019-08-06 22:48:50 +0000

    fix

--- src/ray/gcs/actor_state_accessor_test.cc
+++ src/ray/gcs/actor_state_accessor_test.cc
@@ -102,14 +102,14 @@ TEST_F(ActorStateAccessorTest, RegisterAndGet) {
   // get
   for (const auto &elem : actor_datas_) {
     ++pending_count_;
-    RAY_CHECK_OK(actor_accessor.AsyncGet(elem.first,
-        [this](Status status, std::vector<ActorTableData> datas) {
+    RAY_CHECK_OK(actor_accessor.AsyncGet(
+        elem.first, [this](Status status, std::vector<ActorTableData> datas) {
           ASSERT_EQ(datas.size(), 1U);
           ActorID actor_id = ActorID::FromBinary(datas[0].actor_id());
           auto it = actor_datas_.find(actor_id);
           ASSERT_TRUE(it != actor_datas_.end());
           --pending_count_;
-	}));
+        }));
   }
 
   WaitPendingDone(timeout);
@@ -144,9 +144,9 @@ TEST_F(ActorStateAccessorTest, Subscribe) {
     ++sub_pending_count;
     ++register_pending_count;
     RAY_CHECK_OK(
-	actor_accessor.AsyncRegister(actor, [&register_pending_count](Status status) {
+        actor_accessor.AsyncRegister(actor, [&register_pending_count](Status status) {
           RAY_CHECK_OK(status);
-	  --register_pending_count;
+          --register_pending_count;
         }));
   }
   // Wait until register finishes.
