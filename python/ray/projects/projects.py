7556de195aa1bf311bb807409f2ce6a83ee3c5d5
Philipp Moritz <pcmoritz@gmail.com>
2019-08-06 12:20:25 -0700

    fixes

--- python/ray/projects/projects.py
+++ python/ray/projects/projects.py
@@ -4,31 +4,34 @@ import os
 import yaml
 
 
-def find_root(dir):
+def find_root(directory):
     """Find root directory of the ray project.
 
     Args:
-        dir (str): Directory to start the search in.
+        directory (str): Directory to start the search in.
 
     Returns:
         Path of the parent directory containing the .rayproject or
         None if no such project is found.
     """
-    prev, dir = None, os.path.abspath(dir)
-    while prev != dir:
-        if os.path.isdir(os.path.join(dir, ".rayproject")):
-            return dir
-        prev, dir = dir, os.path.abspath(os.path.join(dir, os.pardir))
+    prev, directory = None, os.path.abspath(directory)
+    while prev != directory:
+        if os.path.isdir(os.path.join(directory, ".rayproject")):
+            return directory
+        prev, directory = directory, os.path.abspath(
+            os.path.join(directory, os.pardir))
     return None
 
 
 def validate_project_schema(project_definition):
     """Validate a project file against the official ray project schema.
 
-    Raises an exception if the project file is not valid.
-
     Args:
         project_definition (dict): Parsed project yaml.
+
+    Raises:
+        jsonschema.exceptions.ValidationError: This exception is raised
+            if the project file is not valid.
     """
     dir = os.path.dirname(os.path.abspath(__file__))
     with open(os.path.join(dir, "schema.json")) as f:
@@ -40,12 +43,15 @@ def validate_project_schema(project_definition):
 def check_project_definition(project_root, project_definition):
     """Checks if the project definition is valid.
 
-    Raises an exception if the schema is not valid or if there are
-    other errors in the project definition (e.g. files not existing).
-
     Args:
         project_root (str): Path containing the .rayproject
         project_definition (dict): Project definition
+
+    Raises:
+        jsonschema.exceptions.ValidationError: This exception is raised
+            if the project file is not valid.
+        ValueError: This exception is raised if there are other errors in
+            the project definition (e.g. files not existing).
     """
 
     validate_project_schema(project_definition)
@@ -54,7 +60,9 @@ def check_project_definition(project_root, project_definition):
     if "cluster" in project_definition:
         cluster_file = os.path.join(project_root,
                                     project_definition["cluster"])
-        assert os.path.exists(cluster_file)
+        if not os.path.exists(cluster_file):
+            raise ValueError("'cluster' file does not exist "
+                             "in {}".format(project_root))
 
     if "environment" in project_definition:
         env = project_definition["environment"]
@@ -84,6 +92,12 @@ def load_project(current_dir):
 
     Returns:
         Dictionary containing the project definition.
+
+    Raises:
+        jsonschema.exceptions.ValidationError: This exception is raised
+            if the project file is not valid.
+        ValueError: This exception is raised if there are other errors in
+            the project definition (e.g. files not existing).
     """
     project_root = find_root(current_dir)
 
