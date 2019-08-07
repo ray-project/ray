19e24b43c7ace6d87ff026f8e09a9949e0a3d6fb
svc-tai-dev <svc-tai-dev@ct1-w-z240-linux1.freepoint.local>
2019-08-07 19:11:06 -0400

    added class median_stopping_result to schedulers and updated __init__

--- python/ray/tune/schedulers/__init__.py
+++ python/ray/tune/schedulers/__init__.py
@@ -8,10 +8,16 @@ from ray.tune.schedulers.hb_bohb import HyperBandForBOHB
 from ray.tune.schedulers.async_hyperband import (AsyncHyperBandScheduler,
                                                  ASHAScheduler)
 from ray.tune.schedulers.median_stopping_rule import MedianStoppingRule
+from ray.tune.schedulers.median_stopping_result import MedianStoppingResult
 from ray.tune.schedulers.pbt import PopulationBasedTraining
 
 __all__ = [
-    "TrialScheduler", "HyperBandScheduler", "AsyncHyperBandScheduler",
-    "ASHAScheduler", "MedianStoppingRule", "FIFOScheduler",
-    "PopulationBasedTraining", "HyperBandForBOHB"
+    "TrialScheduler",
+    "HyperBandScheduler",
+    "AsyncHyperBandScheduler",
+    "ASHAScheduler",
+    "MedianStoppingRule",
+    "FIFOScheduler",
+    "PopulationBasedTraining",
+    "MedianStoppingResult",
 ]
