42aa9250c861eb56ca747bf98cdf6da950a1dea1
nflu <nflugovoy@berkeley.edu>
2019-08-10 14:47:51 -0700

    ran linter

--- python/ray/tune/tests/test_reporter_no_usage.py
+++ python/ray/tune/tests/test_reporter_no_usage.py
@@ -8,16 +8,16 @@ from ray.tune import Experiment
 
 
 class TuneServerSuite(unittest.TestCase):
+    def test_reporter_no_usage(self):
+        ray.init()
 
-	def test_reporter_no_usage(self):
-		ray.init()
-		def run_task(config, reporter):
-			print('hello')
+        def run_task(config, reporter):
+            print('hello')
 
-		experiment = Experiment(run=run_task, name="ray_crash_repro")
-		ray.tune.run(experiment)
-		print("end")
+        experiment = Experiment(run=run_task, name="ray_crash_repro")
+        ray.tune.run(experiment)
+        print("end")
 
 
 if __name__ == "__main__":
-	unittest.main(verbosity=2)
+    unittest.main(verbosity=2)
