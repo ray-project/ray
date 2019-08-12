66c9a9f984071e5b45fde64147c8edc2a49ddc20
Kai Yang <kfstorm@outlook.com>
2019-08-12 17:05:02 +0800

    Address comment

--- src/ray/gcs/redis_context.cc
+++ src/ray/gcs/redis_context.cc
@@ -194,11 +194,15 @@ void RedisAsyncContextDisconnectCallback(const redisAsyncContext *context, int s
       ->AsyncDisconnectCallback(context, status);
 }
 
-template <typename HiredisContext, typename RedisConnectFunction>
+void SetDisconnectCallback(RedisContext *redis_context, redisAsyncContext *context) {
+  context->data = redis_context;
+  redisAsyncSetDisconnectCallback(context, RedisAsyncContextDisconnectCallback);
+}
+
+template <typename RedisContext, typename RedisConnectFunction>
 Status ConnectWithRetries(const std::string &address, int port,
                           const RedisConnectFunction &connect_function,
-                          bool is_async_context, RedisContext *redis_context,
-                          HiredisContext **context) {
+                          RedisContext **context) {
   int connection_attempts = 0;
   *context = connect_function(address.c_str(), port);
   while (*context == nullptr || (*context)->err) {
@@ -218,11 +222,6 @@ Status ConnectWithRetries(const std::string &address, int port,
     *context = connect_function(address.c_str(), port);
     connection_attempts += 1;
   }
-  if (is_async_context) {
-    redisAsyncContext *async_context = reinterpret_cast<redisAsyncContext *>(*context);
-    async_context->data = redis_context;
-    redisAsyncSetDisconnectCallback(async_context, RedisAsyncContextDisconnectCallback);
-  }
   return Status::OK();
 }
 
@@ -232,7 +231,7 @@ Status RedisContext::Connect(const std::string &address, int port, bool sharding
   RAY_CHECK(!async_context_);
   RAY_CHECK(!subscribe_context_);
 
-  RAY_CHECK_OK(ConnectWithRetries(address, port, redisConnect, false, this, &context_));
+  RAY_CHECK_OK(ConnectWithRetries(address, port, redisConnect, &context_));
   RAY_CHECK_OK(AuthenticateRedis(context_, password));
 
   redisReply *reply = reinterpret_cast<redisReply *>(
@@ -241,13 +240,13 @@ Status RedisContext::Connect(const std::string &address, int port, bool sharding
   freeReplyObject(reply);
 
   // Connect to async context
-  RAY_CHECK_OK(
-      ConnectWithRetries(address, port, redisAsyncConnect, true, this, &async_context_));
+  RAY_CHECK_OK(ConnectWithRetries(address, port, redisAsyncConnect, &async_context_));
+  SetDisconnectCallback(this, async_context_);
   RAY_CHECK_OK(AuthenticateRedis(async_context_, password));
 
   // Connect to subscribe context
-  RAY_CHECK_OK(ConnectWithRetries(address, port, redisAsyncConnect, true, this,
-                                  &subscribe_context_));
+  RAY_CHECK_OK(ConnectWithRetries(address, port, redisAsyncConnect, &subscribe_context_));
+  SetDisconnectCallback(this, subscribe_context_);
   RAY_CHECK_OK(AuthenticateRedis(subscribe_context_, password));
 
   return Status::OK();
