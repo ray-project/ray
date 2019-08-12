c15f2a398f695b9f20505b343232a013fa775078
micafan <550435771@qq.com>
2019-08-12 12:18:31 +0800

    Update subscription_executor_test.cc
    use new way to generate ActorID

--- src/ray/gcs/subscription_executor_test.cc
+++ src/ray/gcs/subscription_executor_test.cc
@@ -44,13 +44,13 @@ class SubscriptionExecutorTest : public AccessorTestBase<ActorID, ActorTableData
   virtual void GenTestData() {
     for (size_t i = 0; i < 2; ++i) {
       std::shared_ptr<ActorTableData> actor = std::make_shared<ActorTableData>();
-      ActorID actor_id = ActorID::FromRandom();
-      actor->set_actor_id(actor_id.Binary());
       actor->set_max_reconstructions(1);
       actor->set_remaining_reconstructions(1);
       JobID job_id = JobID::FromInt(i);
       actor->set_job_id(job_id.Binary());
       actor->set_state(ActorTableData::ALIVE);
+      ActorID actor_id = ActorID::Of(job_id, RandomTaskId(), /*parent_task_counter=*/i);
+      actor->set_actor_id(actor_id.Binary());
       id_to_data_[actor_id] = actor;
     }
   }
