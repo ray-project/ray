27f941a240afaeb906b69cef6bafac855a86976d
Richard Liaw <rliaw@berkeley.edu>
2019-08-11 23:47:30 -0700

    suggest

--- python/ray/tune/suggest/__init__.py
+++ python/ray/tune/suggest/__init__.py
@@ -2,10 +2,11 @@ from ray.tune.suggest.search import SearchAlgorithm
 from ray.tune.suggest.basic_variant import BasicVariantGenerator
 from ray.tune.suggest.suggestion import SuggestionAlgorithm
 from ray.tune.suggest.variant_generator import grid_search
+from ray.tune.suggest.bohb import TuneBOHB
 
 __all__ = [
     "SearchAlgorithm", "BasicVariantGenerator", "SuggestionAlgorithm",
-    "grid_search"
+    "grid_search", "TuneBOHB"
 ]
 
 
