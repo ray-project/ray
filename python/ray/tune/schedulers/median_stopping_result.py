eac6319df7fbb89d682926a8ad91935f306d8f0b
svc-tai-dev <svc-tai-dev@ct1-w-z240-linux1.freepoint.local>
2019-08-16 08:19:04 -0400

    modify median_stopping_result to synchronize result evaluation at regular intervals, driven by least common interval

--- python/ray/tune/schedulers/median_stopping_result.py
+++ python/ray/tune/schedulers/median_stopping_result.py
@@ -14,8 +14,6 @@ from ray.tune.trial import Trial, Checkpoint
 logger = logging.getLogger(__name__)
 
 
-
-
 class MedianStoppingResult(FIFOScheduler):
     """
     Args:
@@ -77,14 +75,13 @@ class MedianStoppingResult(FIFOScheduler):
         self._last_result_times = collections.defaultdict(int)
         self._evaluated_interval = 0
 
-    def fetch_active_trials(self):
+
+
+    def fetch_lowest_common_interval(self, trial_runner):
         _trials = [trial for trial in trial_runner.get_trials() if
             trial not in self._stopped_trials or
             trial not in self._completed_trials]
-        return _trials
 
-    def fetch_lowest_common_interval(self):
-        _trials = self.fetch_active_trials()
         completed_intervals = [int(self._last_result_times[trial]/self._eval_interval)
              for trial in _trials]
         return min(completed_intervals), _trials
@@ -105,38 +102,36 @@ class MedianStoppingResult(FIFOScheduler):
         self._results[trial].append(result)
         self._last_result_times[trial] = result_time
 
-        lc_interval, _active_trials = self.fetch_lowest_common_interval()
+        lc_interval, _active_trials = self.fetch_lowest_common_interval(trial_runner)
 
         _return = TrialScheduler.CONTINUE
         if  (lc_interval*self._eval_interval > self._grace_period) and \
                 (lc_interval > self._evaluated_interval):
 
-            _means, _best_results = self._get_trial_metrics(_active_trials, lc_interval * self._eval_interval)
-            _median = np.median(means)
-
+            _means, _best_results = self._get_trial_metrics(_active_trials,
+                lc_interval * self._eval_interval)
+            _median = np.median(_means)
             if self._mode == "max":
-                _trials_to_stop = np.array(_trials)[best_result < _median for best_result in _best_results]
+                _trials_to_stop = [best_result < _median for best_result in _best_results]
+                _trials_to_stop = np.array(_active_trials)[_trials_to_stop]
             else:
-                _trials_to_stop = np.array(_trials)[best_result > _median for best_result in _best_results]
-
-            if trial in _trials_to_stop:
-                self._stopped_trials.add(trial)
-                _return = TrialScheduler.STOP
-
-            for _trial in _trials_to_stop if _trial != trial:
-                # if self._verbose:
-                #     logger.info("MedianStoppingResult: "
-                #                 "early {} {} best res={} vs median res={} at t={}".format(
-                #                     "stopping" if self._hard_stop else "pausing",
-                #                     trial, best_result, median_result, result_time))
+                _trials_to_stop = [best_result > _median for best_result in _best_results]
+                _trials_to_stop = np.array(_active_trials)[_trials_to_stop]
+            for idx, _trial in enumerate(_trials_to_stop):
+                if self._verbose:
+                    logger.info("MedianStoppingResult: "
+                                "early stopping {} best res={} vs median res={} at t={}".format(
+                                    _trial, _best_results[idx], _median, lc_interval*self._eval_interval))
                 self._stopped_trials.add(_trial)
-                trial_runner.stop_trial(_trial)
-
+                if _trial == trial:
+                    _return = TrialScheduler.STOP
+                else:
+                    trial_runner.stop_trial(_trial)
+                self._evaluated_interval += 1
         _waiting = [_trial.status in [Trial.PENDING, Trial.PAUSED] for _trial in _active_trials]
 
-        if _waiting and _return==TrialScheduler.CONTINUE:
+        if any(_waiting) and _return==TrialScheduler.CONTINUE:
             _return= TrialScheduler.PAUSE
-
         return _return
 
     def on_trial_complete(self, trial_runner, trial, result):
