62228134b57286ae14585c2509cbf7cef5c48b63
Eric Liang <ekhliang@gmail.com>
2019-08-18 23:24:42 -0700

    del

--- python/ray/tests/test_multi_node_2.py
+++ python/ray/tests/test_multi_node_2.py
@@ -73,10 +73,14 @@ def verify_load_metrics(monitor, expected_resource_usage=None, timeout=10):
         monitor.process_messages()
         resource_usage = monitor.load_metrics.get_resource_usage()
 
-        del resource_usage[1]["memory"]
-        del resource_usage[1]["object_store_memory"]
-        del resource_usage[2]["memory"]
-        del resource_usage[2]["object_store_memory"]
+        if "memory" in resource_usage[1]:
+            del resource_usage[1]["memory"]
+        if "object_store_memory" in resource_usage[2]:
+            del resource_usage[1]["object_store_memory"]
+        if "memory" in resource_usage[2]:
+            del resource_usage[2]["memory"]
+        if "object_store_memory" in resource_usage[2]:
+            del resource_usage[2]["object_store_memory"]
 
         if expected_resource_usage is None:
             if all(x for x in resource_usage[1:]):
