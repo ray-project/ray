0a3fbab76e4ec8898873093d7204c13779d5992f
Zhijun Fu <pingfu.fzj@antfin.com>
2019-08-22 17:54:31 +0800

    asio_client.cc

---
+++ src/ray/rpc/asio_client.cc
@@ -0,0 +1,123 @@
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <thread>
+#include <utility>
+#include <boost/asio.hpp>
+
+#include "src/ray/common/client_connection.h"
+#include "src/ray/rpc/common.h"
+#include "src/ray/rpc/client.h"
+#include "src/ray/protobuf/asio.pb.h"
+#include "src/ray/rpc/asio_client.h"
+
+namespace ray {
+namespace rpc {
+
+Status AsioRpcClient::Connect() {
+  boost::asio::ip::tcp::socket socket(io_service_);
+  RAY_RETURN_NOT_OK(TcpConnect(socket, address_, port_));
+
+  ClientHandler<boost::asio::ip::tcp> client_handler =
+      [](TcpClientConnection &client) {
+        // Begin listening for messages.
+        client.ProcessMessages();
+      };
+  MessageHandler<boost::asio::ip::tcp> message_handler =
+      [this](std::shared_ptr<TcpClientConnection> client, int64_t message_type,
+              uint64_t length, const uint8_t *message) {
+        ProcessServerMessage(client, message_type, length, message);
+      };
+
+  const std::vector<std::string> asio_common_message_enum =
+      GenerateEnumNames(RpcServiceType);
+
+  // Accept a new TCP client and dispatch it to the node manager.
+  connection_ = TcpClientConnection::Create(
+      client_handler, message_handler, std::move(socket), name_,
+      asio_common_message_enum,
+      static_cast<int64_t>(ServiceMessageType::DisconnectClient));
+  // Prepare connect message.
+  ConnectClientMessage message;
+  message.set_service_type(service_type_);
+
+  std::string serialized_message;
+  message.SerializeToString(&serialized_message);
+
+  // Send synchronously.
+  RAY_RETURN_NOT_OK(connection_->WriteMessage(
+      static_cast<int64_t>(ServiceMessageType::ConnectClient),
+      static_cast<int64_t>(serialized_message.size()),
+      reinterpret_cast<const uint8_t *>(serialized_message.data())));
+
+  is_connected_ = true;
+
+  return Status::OK();
+}
+
+void AsioRpcClient::ProcessServerMessage(
+    const std::shared_ptr<TcpClientConnection> &client, int64_t message_type,
+    uint64_t length, const uint8_t *message_data) {
+    
+  if (message_type == static_cast<int64_t>(ServiceMessageType::DisconnectClient)) {       
+    ProcessDisconnectClientMessage(client);
+    // We don't need to receive future messages from this client,
+    // because it's already disconnected.
+    return;    
+  }
+
+  RpcReplyMessage reply_message;
+  reply_message.ParseFromArray(message_data, length);
+
+  RAY_LOG(DEBUG) << "Processing server message for request id: " << reply_message.request_id()
+                << ", service: " << name_ << ", message type: " << message_type;  
+
+  const auto request_id = reply_message.request_id();
+  ReplyCallback reply_callback;
+
+  {
+    std::unique_lock<std::mutex> guard(callback_mutex_); 
+    auto iter = pending_callbacks_.find(request_id);
+    if (iter != pending_callbacks_.end()) {
+      reply_callback = iter->second;
+      pending_callbacks_.erase(iter);
+    }
+  }
+
+  if (reply_callback != nullptr) {
+    reply_callback(reply_message);
+  }
+  // TODO: what would happen if we don't find an entry rom pending_callbacks_?
+
+  client->ProcessMessages();
+}
+
+void AsioRpcClient::ProcessDisconnectClientMessage(
+    const std::shared_ptr<TcpClientConnection> &client) {
+  RAY_LOG(INFO) << "Received DiconnectClient message from server "
+                << address_ << ":" << port_ << ", service: " << name_; 
+  
+  is_connected_ = false;
+
+  // Invoke all the callbacks that are pending replies, this is necessary so that
+  // the transport can put exceptions into store for these object ids, to avoid
+  // the client from getting blocked on `ray.get`.
+  {
+    std::unique_lock<std::mutex> guard(callback_mutex_);
+
+    for (const auto &entry : pending_callbacks_) {
+      Status status = Status::Invalid("rpc server died");
+      RpcReplyMessage reply_message;
+      reply_message.set_request_id(entry.first);
+      reply_message.set_error_code(static_cast<uint32_t>(status.code()));
+      reply_message.set_error_message(status.message());
+    
+      entry.second(reply_message);
+    }
+
+    pending_callbacks_.clear();
+  }
+}
+
+}  // namespace rpc
+}  // namespace ray
--- src/ray/rpc/asio_client.h
+++ src/ray/rpc/asio_client.h
@@ -56,46 +56,7 @@ class AsioRpcClient : public RpcClient {
       request_id_(0),
       is_connected_(false) {}
 
-  Status Connect() override {
-    boost::asio::ip::tcp::socket socket(io_service_);
-    RAY_RETURN_NOT_OK(TcpConnect(socket, address_, port_));
-
-    ClientHandler<boost::asio::ip::tcp> client_handler =
-        [](TcpClientConnection &client) {
-          // Begin listening for messages.
-          client.ProcessMessages();
-        };
-    MessageHandler<boost::asio::ip::tcp> message_handler =
-        [this](std::shared_ptr<TcpClientConnection> client, int64_t message_type,
-                uint64_t length, const uint8_t *message) {
-          ProcessServerMessage(client, message_type, length, message);
-        };
-
-    const std::vector<std::string> asio_common_message_enum =
-        GenerateEnumNames(RpcServiceType);
-
-    // Accept a new TCP client and dispatch it to the node manager.
-    connection_ = TcpClientConnection::Create(
-        client_handler, message_handler, std::move(socket), name_,
-        asio_common_message_enum,
-        static_cast<int64_t>(ServiceMessageType::DisconnectClient));
-    // Prepare connect message.
-    ConnectClientMessage message;
-    message.set_service_type(service_type_);
-
-    std::string serialized_message;
-    message.SerializeToString(&serialized_message);
-
-    // Send synchronously.
-    RAY_RETURN_NOT_OK(connection_->WriteMessage(
-        static_cast<int64_t>(ServiceMessageType::ConnectClient),
-        static_cast<int64_t>(serialized_message.size()),
-        reinterpret_cast<const uint8_t *>(serialized_message.data())));
-
-    is_connected_ = true;
-  
-    return Status::OK();
-  }
+  Status Connect() override;
 
   /// Create a new `ClientCall` and send request.
   ///
@@ -127,87 +88,54 @@ class AsioRpcClient : public RpcClient {
     request_message.set_request_id(request_id);           
     request.SerializeToString(request_message.mutable_request());
 
-    std::string serialized_message;
-    request_message.SerializeToString(&serialized_message);
+    std::shared_ptr<std::string> serialized_message = std::make_shared<std::string>();
+    request_message.SerializeToString(serialized_message.get());
 
     RAY_LOG(DEBUG) << "Calling method for service " << name_
                   << ", request id: " << request_id
                   << ", request type: " << static_cast<int>(request_type);
 
-    auto status = connection_->WriteMessage(request_type,
-        static_cast<int64_t>(serialized_message.size()),
-        reinterpret_cast<const uint8_t *>(serialized_message.data()));
-    if (status.ok()) {
-      // Send succeeds. Add the request to the records, so that
-      // we can invoke the callback after receivig the reply.
-      std::unique_lock<std::mutex> guard(callback_mutex_);
-      pending_callbacks_.emplace(request_id,
-        [callback, reply_type, this](const RpcReplyMessage &reply_message) {
+    // The invocation of `WriteMessageAsync` and its async callback needs to be done
+    // in the same thread, thus we need to dispatch it to io_thread_.
+    // NOTE(zhijunfu): use `WriteMessageAsync` is noticably faster than `WriteMessage`,
+    // about 2X faster on task submission, and 50% faster overall.
+    io_service_.dispatch([request_id, callback, request_type, reply_type, serialized_message, this] {
+      connection_->WriteMessageAsync(request_type,
+          static_cast<int64_t>(serialized_message->size()),
+          reinterpret_cast<const uint8_t *>(serialized_message->data()),
+          [request_id, callback, request_type, reply_type, this](const ray::Status &status) {
+            if (status.ok()) {
+              // Send succeeds. Add the request to the records, so that
+              // we can invoke the callback after receivig the reply.
+              std::unique_lock<std::mutex> guard(callback_mutex_);
+              pending_callbacks_.emplace(request_id,
+                [callback, reply_type, this](const RpcReplyMessage &reply_message) {
 
-          const auto request_id = reply_message.request_id();
-          auto error_code = static_cast<StatusCode>(reply_message.error_code());
-          auto error_message = reply_message.error_message();  
-          Status status = (error_code == StatusCode::OK) ?
-              Status::OK() : Status(error_code, error_message);
+                  const auto request_id = reply_message.request_id();
+                  auto error_code = static_cast<StatusCode>(reply_message.error_code());
+                  auto error_message = reply_message.error_message();  
+                  Status status = (error_code == StatusCode::OK) ?
+                      Status::OK() : Status(error_code, error_message);
 
-          Reply reply;
-          reply.ParseFromString(reply_message.reply());
+                  Reply reply;
+                  reply.ParseFromString(reply_message.reply());
 
-          callback(status, reply); 
-          RAY_LOG(DEBUG) << "Calling reply callback for message " << static_cast<int>(reply_type)
-                        << " for service " << name_ << ", request id " << request_id
-                        << ", status: " << status.ToString();                           
-      });
+                  callback(status, reply); 
+                  RAY_LOG(DEBUG) << "Calling reply callback for message " << static_cast<int>(reply_type)
+                                << " for service " << name_ << ", request id " << request_id
+                                << ", status: " << status.ToString();                           
+              });
 
-    } else {
-      // There are errors, invoke the callback.
-      Reply reply;
-      callback(status, reply);
-      RAY_LOG(DEBUG) << "Failed to write request message " << static_cast<int>(request_type)
-                    << " for service " << name_ << " to " << address_ << ":" << port_
-                    << ", request id " << request_id << ", status: " << status.ToString();              
-    }
-  
-
-/*
-
-
-    connection_->WriteMessageAsync(request_type,
-        static_cast<int64_t>(serialized_message.size()),
-        reinterpret_cast<const uint8_t *>(serialized_message.data()),
-        [request_id, callback, request_type, reply_type, this](const ray::Status &status) {
-          if (status.ok()) {
-            // Send succeeds. Add the request to the records, so that
-            // we can invoke the callback after receivig the reply.
-            std::unique_lock<std::mutex> guard(callback_mutex_);
-            pending_callbacks_.emplace(request_id,
-              [callback, reply_type](const RpcReplyMessage &reply_message) {
-
-                const auto request_id = reply_message.request_id();
-                auto error_code = static_cast<StatusCode>(reply_message.error_code());
-                auto error_message = reply_message.error_message();  
-                Status status = (error_code == StatusCode::OK) ?
-                    Status::OK() : Status(error_code, error_message);
-
-                Reply reply;
-                reply.ParseFromString(reply_message.reply());
-
-                callback(status, reply); 
-                RAY_LOG(INFO) << "Calling reply callback for message " << static_cast<int>(reply_type)
-                              << ", request id " << request_id << ", status: " << status.ToString();                           
-            });
-
-          } else {
-            // There are errors, invoke the callback.
-            Reply reply;
-            callback(status, reply);
-            RAY_LOG(INFO) << "Failed to write request message " << static_cast<int>(request_type)
-                          << " for service " << name_ << " to " << address_ << ":" << port_
-                          << ", request id " << request_id << ", status: " << status.ToString();              
-          }
-        });
-
-*/
+            } else {
+              // There are errors, invoke the callback.
+              Reply reply;
+              callback(status, reply);
+              RAY_LOG(DEBUG) << "Failed to write request message " << static_cast<int>(request_type)
+                            << " for service " << name_ << " to " << address_ << ":" << port_
+                            << ", request id " << request_id << ", status: " << status.ToString();              
+            }
+          });
+    });
 
     return Status::OK();
   }
@@ -216,67 +144,10 @@ class AsioRpcClient : public RpcClient {
 
   void ProcessServerMessage(
       const std::shared_ptr<TcpClientConnection> &client, int64_t message_type,
-      uint64_t length, const uint8_t *message_data) {
-      
-    if (message_type == static_cast<int64_t>(ServiceMessageType::DisconnectClient)) {       
-      ProcessDisconnectClientMessage(client);
-      // We don't need to receive future messages from this client,
-      // because it's already disconnected.
-      return;    
-    }
-
-    RpcReplyMessage reply_message;
-    reply_message.ParseFromArray(message_data, length);
-
-    RAY_LOG(DEBUG) << "Processing server message for request id: " << reply_message.request_id()
-                  << ", service: " << name_ << ", message type: " << message_type;  
-
-    const auto request_id = reply_message.request_id();
-    ReplyCallback reply_callback;
-
-    {
-      std::unique_lock<std::mutex> guard(callback_mutex_); 
-      auto iter = pending_callbacks_.find(request_id);
-      if (iter != pending_callbacks_.end()) {
-        reply_callback = iter->second;
-        pending_callbacks_.erase(iter);
-      }
-    }
-
-    if (reply_callback != nullptr) {
-      reply_callback(reply_message);
-    }
-    // TODO: what would happen if we don't find an entry rom pending_callbacks_?
-
-    client->ProcessMessages();
-  }
+      uint64_t length, const uint8_t *message_data);
 
   void ProcessDisconnectClientMessage(
-      const std::shared_ptr<TcpClientConnection> &client) {
-    RAY_LOG(INFO) << "Received DiconnectClient message from server "
-                  << address_ << ":" << port_ << ", service: " << name_; 
-    
-    is_connected_ = false;
-
-    // Invoke all the callbacks that are pending replies, this is necessary so that
-    // the transport can put exceptions into store for these object ids, to avoid
-    // the client from getting blocked on `ray.get`.
-    {
-      std::unique_lock<std::mutex> guard(callback_mutex_);
-
-      for (const auto &entry : pending_callbacks_) {
-        Status status = Status::Invalid("rpc server died");
-        RpcReplyMessage reply_message;
-        reply_message.set_request_id(entry.first);
-        reply_message.set_error_code(static_cast<uint32_t>(status.code()));
-        reply_message.set_error_message(status.message());
-      
-        entry.second(reply_message);
-      }
-
-      pending_callbacks_.clear();
-    }
-  }
+      const std::shared_ptr<TcpClientConnection> &client);
 
 
   using ReplyCallback = std::function<void(const RpcReplyMessage &)>;
