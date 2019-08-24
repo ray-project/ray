d3c7c90ee1ecde85f866a45eaec50e89630740eb
Kai Yang <kfstorm@outlook.com>
2019-08-24 19:24:18 +0800

    clean

--- java/runtime/src/main/java/org/ray/runtime/object/ObjectStore.java
+++ java/runtime/src/main/java/org/ray/runtime/object/ObjectStore.java
@@ -3,7 +3,6 @@ package org.ray.runtime.object;
 import com.google.common.base.Preconditions;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.IllegalFormatException;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
