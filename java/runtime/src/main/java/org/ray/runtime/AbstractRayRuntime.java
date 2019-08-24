b7abf8416304c8e3ec4885bf8ac6550668251acf
Hao Chen <chenh1024@gmail.com>
2019-08-24 17:20:27 +0800

    [Java] Automatically clean up temp files. (#5507)
    * Remove unused code
    
    * Auto clean up temp files
    
    * refine
    
    * fix
    
    * print warning
    
    * fix

--- java/runtime/src/main/java/org/ray/runtime/AbstractRayRuntime.java
+++ java/runtime/src/main/java/org/ray/runtime/AbstractRayRuntime.java
@@ -1,6 +1,7 @@
 package org.ray.runtime;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
 import org.ray.api.RayActor;
@@ -31,7 +32,6 @@ import org.ray.runtime.task.ArgumentsBuilder;
 import org.ray.runtime.task.FunctionArg;
 import org.ray.runtime.task.TaskExecutor;
 import org.ray.runtime.task.TaskSubmitter;
-import org.ray.runtime.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -201,7 +201,7 @@ public abstract class AbstractRayRuntime implements RayRuntime {
     List<FunctionArg> functionArgs = ArgumentsBuilder
         .wrap(args, functionDescriptor.getLanguage() != Language.JAVA);
     if (functionDescriptor.getLanguage() != Language.JAVA && options != null) {
-      Preconditions.checkState(StringUtil.isNullOrEmpty(options.jvmOptions));
+      Preconditions.checkState(Strings.isNullOrEmpty(options.jvmOptions));
     }
     RayActor actor = taskSubmitter
         .createActor(functionDescriptor, functionArgs,
