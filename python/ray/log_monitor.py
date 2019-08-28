12951b67d86bc4719fa0625c2ea39517723ce926
Philipp Moritz <pcmoritz@gmail.com>
2019-08-27 21:29:34 -0700

    update

--- python/ray/log_monitor.py
+++ python/ray/log_monitor.py
@@ -85,6 +85,12 @@ class LogMonitor(object):
             file_info = self.open_file_infos.pop(0)
             file_info.file_handle.close()
             file_info.file_handle = None
+            try:
+                # Test if the worker process is still alive
+                os.kill(file_info.worker_pid, 0)
+            except:
+                
+            if
             self.closed_file_infos.append(file_info)
         self.can_open_more_files = True
 
