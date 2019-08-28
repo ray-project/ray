d660f989ad3a55f794ff30aac1e85c3555f91bb9
Ed Oakes <ed.nmi.oakes@gmail.com>
2019-08-28 11:36:33 -0700

    lint

--- src/ray/core_worker/object_interface.cc
+++ src/ray/core_worker/object_interface.cc
@@ -124,7 +124,8 @@ Status CoreWorkerObjectInterface::Wait(const std::vector<ObjectID> &ids, int num
   (*results).resize(ids.size(), false);
 
   if (num_objects <= 0 || num_objects > static_cast<int>(ids.size())) {
-    return Status::Invalid("Number of objects to wait for must be between 1 and the number of ids.");
+    return Status::Invalid(
+        "Number of objects to wait for must be between 1 and the number of ids.");
   }
 
   EnumUnorderedMap<StoreProviderType, std::unordered_set<ObjectID>>
@@ -132,7 +133,7 @@ Status CoreWorkerObjectInterface::Wait(const std::vector<ObjectID> &ids, int num
   GroupObjectIdsByStoreProvider(ids, &object_ids_per_store_provider);
 
   size_t total_count = 0;
-  for (const auto& entry : object_ids_per_store_provider) {
+  for (const auto &entry : object_ids_per_store_provider) {
     total_count += entry.second.size();
   }
   if (total_count != ids.size()) {
@@ -165,16 +166,15 @@ Status CoreWorkerObjectInterface::Wait(const std::vector<ObjectID> &ids, int num
 }
 
 Status CoreWorkerObjectInterface::WaitFromMultipleStoreProviders(
-    EnumUnorderedMap<StoreProviderType, std::unordered_set<ObjectID>>
-        &ids_per_provider,
+    EnumUnorderedMap<StoreProviderType, std::unordered_set<ObjectID>> &ids_per_provider,
     int64_t timeout_ms, int num_objects, std::unordered_set<ObjectID> *ready) {
-
   auto remaining_timeout_ms = timeout_ms;
   for (auto &entry : ids_per_provider) {
     auto start_time = current_time_ms();
     int required_objects = std::min(static_cast<int>(entry.second.size()), num_objects);
-    RAY_RETURN_NOT_OK(store_providers_[entry.first]->Wait(entry.second, required_objects,
-                                            remaining_timeout_ms, worker_context_.GetCurrentTaskID(), ready));
+    RAY_RETURN_NOT_OK(store_providers_[entry.first]->Wait(
+        entry.second, required_objects, remaining_timeout_ms,
+        worker_context_.GetCurrentTaskID(), ready));
     if (remaining_timeout_ms > 0) {
       int64_t duration = current_time_ms() - start_time;
       remaining_timeout_ms =
