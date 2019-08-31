2a18a7dfcc0f121ee5a82f658be3c004a891b6e5
Eric Liang <ekhliang@gmail.com>
2019-08-31 16:00:10 -0700

    [tune] Deprecate tune.function (#5601)
    * remove tune function
    
    * remove examples
    
    * Update tune-usage.rst

--- python/ray/tune/sample.py
+++ python/ray/tune/sample.py
@@ -11,9 +11,6 @@ logger = logging.getLogger(__name__)
 class sample_from(object):
     """Specify that tune should sample configuration values from this function.
 
-    The use of function arguments in tune configs must be disambiguated by
-    either wrapped the function in tune.sample_from() or tune.function().
-
     Arguments:
         func: An callable function to draw a sample from.
     """
@@ -28,27 +25,10 @@ class sample_from(object):
         return "tune.sample_from({})".format(repr(self.func))
 
 
-class function(object):
-    """Wraps `func` to make sure it is not expanded during resolution.
-
-    The use of function arguments in tune configs must be disambiguated by
-    either wrapped the function in tune.sample_from() or tune.function().
-
-    Arguments:
-        func: A function literal.
-    """
-
-    def __init__(self, func):
-        self.func = func
-
-    def __call__(self, *args, **kwargs):
-        return self.func(*args, **kwargs)
-
-    def __str__(self):
-        return "tune.function({})".format(str(self.func))
-
-    def __repr__(self):
-        return "tune.function({})".format(repr(self.func))
+def function(func):
+    logger.warn("DeprecationWarning: wrapping {} with tune.function() is no "
+                "longer needed".format(func))
+    return func
 
 
 def uniform(*args, **kwargs):
