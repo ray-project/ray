b761c1ff561dce2f95497eba292978ed27d34cbd
Stephanie Wang <swang@cs.berkeley.edu>
2019-09-07 18:30:17 -0700

    [projects] Wrap ProjectDefinition in a class (#5654)

--- python/ray/projects/__init__.py
+++ python/ray/projects/__init__.py
@@ -2,10 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from ray.projects.projects import (check_project_definition, find_root,
-                                   load_project, validate_project_schema)
+from ray.projects.projects import ProjectDefinition
 
 __all__ = [
-    "check_project_definition", "find_root", "load_project",
-    "validate_project_schema"
+    "ProjectDefinition",
 ]
