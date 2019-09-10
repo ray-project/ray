429c9df5c131a3133369ccda6cdbb08232df4a41
Adi <adizim@berkeley.edu>
2019-09-10 15:27:13 -0700

    2 search algs

--- python/ray/tune/suggest/bayesopt.py
+++ python/ray/tune/suggest/bayesopt.py
@@ -33,6 +33,8 @@ class BayesOptSearch(SuggestionAlgorithm):
             provide values for the keys `kind`, `kappa`, and `xi`.
         random_state (int): Used to initialize BayesOpt.
         verbose (int): Sets verbosity level for BayesOpt packages.
+        use_early_terminated_trials (bool): Determines whether to use
+            early terminated trial results
 
     Example:
         >>> space = {
@@ -52,6 +54,7 @@ class BayesOptSearch(SuggestionAlgorithm):
                  utility_kwargs=None,
                  random_state=1,
                  verbose=0,
+                 use_early_terminated_trials=True,
                  **kwargs):
         assert byo is not None, (
             "BayesOpt must be installed!. You can install BayesOpt with"
@@ -76,6 +79,7 @@ class BayesOptSearch(SuggestionAlgorithm):
         elif mode == "min":
             self._metric_op = -1.
         self._live_trial_mapping = {}
+        self._use_early_terminated_trials = use_early_terminated_trials
 
         self.optimizer = byo.BayesianOptimization(
             f=None, pbounds=space, verbose=verbose, random_state=random_state)
@@ -103,6 +107,8 @@ class BayesOptSearch(SuggestionAlgorithm):
                           error=False,
                           early_terminated=False):
         """Passes the result to BayesOpt unless early terminated or errored"""
+        if early_terminated and self.use_early_terminated_trials is False:
+            return
         if result:
             self.optimizer.register(
                 params=self._live_trial_mapping[trial_id],
