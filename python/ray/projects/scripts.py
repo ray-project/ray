532e4a84f0bac3d32f2536b5e57afdcfa0e0a51a
Philipp Moritz <pcmoritz@gmail.com>
2019-09-10 13:46:38 -0700

    update

--- python/ray/projects/scripts.py
+++ python/ray/projects/scripts.py
@@ -144,6 +144,90 @@ def load_project_or_throw():
             "Project file validation failed. Please run "
             "`ray project validate` to inspect the error.")
 
+class SessionRunner(object):
+    def __init__(self, command, args, shell):
+        self.project_definition = load_project_or_throw()
+
+        # We try to parse the command here so we can fail before the cluster
+        # is started in case the command is malformed.
+        if shell:
+            self.command_to_run = command
+        else:
+            try:
+                self.command_to_run = self.project_definition.get_command_to_run(
+                    command=command, args=args)
+            except ValueError as e:
+                raise click.ClickException(e)
+
+        # Check for features we don't support right now
+        project_environment = self.project_definition.config["environment"]
+        need_docker = ("dockerfile" in project_environment
+                       or "dockerimage" in project_environment)
+        if need_docker:
+            raise click.ClickException(
+                "Docker support in session is currently not implemented. "
+                "Please file an feature request at"
+                "https://github.com/ray-project/ray/issues")
+
+    def create_cluster(self):
+        create_or_update_cluster(
+            config_file=self.project_definition.cluster_yaml(),
+            override_min_workers=None,
+            override_max_workers=None,
+            no_restart=False,
+            restart_only=False,
+            yes=True,
+            override_cluster_name=None,
+        )
+
+    def sync_files(self):
+        rsync(
+            self.project_definition.cluster_yaml(),
+            source=self.project_definition.root,
+            target=self.project_definition.working_directory(),
+            override_cluster_name=None,
+            down=False,
+        )
+
+    def setup_environment(self):
+        project_environment = self.project_definition.config["environment"]
+
+        if "requirements" in project_environment:
+            requirements_txt = project_environment["requirements"]
+
+            # Create a temporary requirements_txt in the head node.
+            remote_requirements_txt = (
+                "/tmp/" + "ray_project_requirements_txt_{}".format(time.time()))
+
+            rsync(
+                self.project_definition.cluster_yaml,
+                source=requirements_txt,
+                target=remote_requirements_txt,
+                override_cluster_name=None,
+                down=False,
+            )
+            self.execute_command(
+                "pip install -r {}".format(remote_requirements_txt))
+
+        if "shell" in project_environment:
+            for cmd in project_environment["shell"]:
+                self.execute_command(cmd)
+
+
+    def execute_command(self, cmd):
+        cwd = self.project_definition.working_directory()
+        cmd = "cd {cwd}; {cmd}".format(cwd=cwd, cmd=cmd)
+        exec_cluster(
+            config_file=self.project_definition.cluster_yaml(),
+            cmd=cmd,
+            docker=False,
+            screen=False,
+            tmux=False,
+            stop=False,
+            start=False,
+            override_cluster_name=None,
+            port_forward=None,
+        )
 
 @session_cli.command(help="Attach to an existing cluster")
 def attach():
@@ -167,65 +251,6 @@ def stop():
         override_cluster_name=None)
 
 
-def run_session_start(args, num_steps):
-    project_definition = load_project_or_throw()
-
-    # Check for features we don't support right now
-    project_environment = project_definition.config["environment"]
-    need_docker = ("dockerfile" in project_environment
-                   or "dockerimage" in project_environment)
-    if need_docker:
-        raise click.ClickException(
-            "Docker support in session is currently not implemented. "
-            "Please file an feature request at"
-            "https://github.com/ray-project/ray/issues")
-
-    logger.info("[1/{}] Creating cluster".format(num_steps))
-    create_or_update_cluster(
-        config_file=project_definition.cluster_yaml(),
-        override_min_workers=None,
-        override_max_workers=None,
-        no_restart=False,
-        restart_only=False,
-        yes=True,
-        override_cluster_name=None,
-    )
-
-    logger.info("[2/{}] Syncing the project".format(num_steps))
-    rsync(
-        project_definition.cluster_yaml(),
-        source=project_definition.root,
-        target=project_definition.working_directory(),
-        override_cluster_name=None,
-        down=False,
-    )
-
-    logger.info("[3/{}] Setting up environment".format(num_steps))
-    _setup_environment(
-        project_definition.cluster_yaml(),
-        project_environment,
-        cwd=project_definition.working_directory())
-
-
-def run_session_execute(command, args, shell):
-    project_definition = load_project_or_throw()
-
-    if shell:
-        command_to_run = command
-    else:
-        try:
-            command_to_run = project_definition.get_command_to_run(
-                command=command, args=args)
-        except ValueError as e:
-            raise click.ClickException(e)
-
-    logger.debug("Running {}".format(command))
-
-    session_exec_cluster(
-        project_definition.cluster_yaml(),
-        command_to_run,
-        cwd=project_definition.working_directory())
-
 @session_cli.command(
     name="start",
     context_settings=dict(ignore_unknown_options=True, ),
@@ -239,12 +264,23 @@ def run_session_execute(command, args, shell):
         "the command in the project config"),
     is_flag=True)
 def session_start(command, args, shell):
+    runner = SessionRunner(command, args, shell)
     if shell or command:
-        run_session_start(args, num_steps=4)
+        logger.info("[1/4] Creating cluster")
+        runner.create_cluster()
+        logger.info("[2/4] Syncing the project")
+        runner.sync_files()
+        logger.info("[3/4] Setting up environment")
+        runner.setup_environment()
         logger.info("[4/4] Running command")
-        run_session_execute(command, args, shell)
+        runner.execute_command(runner.command_to_run)
     else:
-        run_session_start(args, num_steps=3)
+        logger.info("[1/3] Creating cluster")
+        runner.create_cluster()
+        logger.info("[2/3] Syncing the project")
+        runner.sync_files()
+        logger.info("[3/3] Setting up environment")
+        runner.setup_environment()
 
 
 @session_cli.command(
@@ -260,46 +296,5 @@ def session_start(command, args, shell):
         "the command in the project config"),
     is_flag=True)
 def session_execute(command, args, shell):
-    run_session_execute(command, args, shell)
-
-
-def session_exec_cluster(cluster_yaml, cmd, cwd=None):
-    if cwd is not None:
-        cmd = "cd {cwd}; {cmd}".format(cwd=cwd, cmd=cmd)
-    exec_cluster(
-        config_file=cluster_yaml,
-        cmd=cmd,
-        docker=False,
-        screen=False,
-        tmux=False,
-        stop=False,
-        start=False,
-        override_cluster_name=None,
-        port_forward=None,
-    )
-
-
-def _setup_environment(cluster_yaml, project_environment, cwd):
-
-    if "requirements" in project_environment:
-        requirements_txt = project_environment["requirements"]
-
-        # Create a temporary requirements_txt in the head node.
-        remote_requirements_txt = (
-            "/tmp/" + "ray_project_requirements_txt_{}".format(time.time()))
-
-        rsync(
-            cluster_yaml,
-            source=requirements_txt,
-            target=remote_requirements_txt,
-            override_cluster_name=None,
-            down=False,
-        )
-        session_exec_cluster(
-            cluster_yaml,
-            "pip install -r {}".format(remote_requirements_txt),
-            cwd=cwd)
-
-    if "shell" in project_environment:
-        for cmd in project_environment["shell"]:
-            session_exec_cluster(cluster_yaml, cmd, cwd=cwd)
+    runner = SessionRunner(command, args, shell)
+    runner.execute_command(runner.command_to_run)
