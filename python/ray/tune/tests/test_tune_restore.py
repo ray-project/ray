916088c734844b344143a7e5e8ef788dc2b45a36
hgodse <hgodse@nvidia.com>
2019-09-10 04:03:22 -0700

    Test for Skopt. Failing 'io.UnsupportedOperation: fileno'

--- python/ray/tune/tests/test_tune_restore.py
+++ python/ray/tune/tests/test_tune_restore.py
@@ -8,6 +8,7 @@ import os
 import shutil
 import tempfile
 import unittest
+import skopt
 
 import ray
 from ray import tune
@@ -16,6 +17,7 @@ from ray.tune.util import validate_save_restore
 from ray.rllib import _register_all
 from ray.tune.suggest.hyperopt import HyperOptSearch
 from ray.tune.suggest.bayesopt import BayesOptSearch
+from ray.tune.suggest.skopt import SkOptSearch
 
 
 class TuneRestoreTest(unittest.TestCase):
@@ -216,6 +218,60 @@ class BayesoptWarmStartTest(unittest.TestCase):
         return tune.run(cost, num_samples=30, search_alg=search_alg3)
 
     def testBayesoptWarmStart(self):
+        results_exp_1 = self.run_exp_1()
+        results_exp_2 = self.run_exp_2()
+        results_exp_3 = self.run_exp_3()
+        trials_1_config = [trial.config for trial in results_exp_1.trials]
+        trials_2_config = [trial.config for trial in results_exp_2.trials]
+        trials_3_config = [trial.config for trial in results_exp_3.trials]
+        self.assertEqual(trials_1_config + trials_2_config, trials_3_config)
+
+
+class SkoptWarmStartTest(unittest.TestCase):
+    def setUp(self):
+        ray.init(local_mode=True)
+        self.tmpdir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tmpdir)
+        ray.shutdown()
+        _register_all()
+
+    def set_basic_conf(self):
+        optimizer = skopt.Optimizer([(0, 20), (-100, 100)])
+        previously_run_params = [[10, 0], [15, -20]]
+        known_rewards = [-189, -1144]
+
+        def cost(space, reporter):
+            loss = space["width"]**2 + space["height"]**2
+            reporter(loss=loss)
+
+        search_alg = SkOptSearch(
+            optimizer, ["width", "height"],
+            max_concurrent=1,
+            metric="loss",
+            mode="min",
+            points_to_evaluate=previously_run_params,
+            evaluated_rewards=known_rewards)
+        return search_alg, cost
+
+    def run_exp_1(self):
+        search_alg, cost = self.set_basic_conf()
+        results_exp_1 = tune.run(cost, num_samples=15, search_alg=search_alg)
+        self.log_dir = os.path.join(self.tmpdir, "trials_algo_sko.pkl")
+        search_alg.save(self.log_dir)
+        return results_exp_1
+
+    def run_exp_2(self):
+        search_alg2, cost = self.set_basic_conf()
+        search_alg2.restore(self.log_dir)
+        return tune.run(cost, num_samples=15, search_alg=search_alg2)
+
+    def run_exp_3(self):
+        search_alg3, cost = self.set_basic_conf()
+        return tune.run(cost, num_samples=30, search_alg=search_alg3)
+
+    def testSkoptWarmStart(self):
         results_exp_1 = self.run_exp_1()
         results_exp_2 = self.run_exp_2()
         results_exp_3 = self.run_exp_3()
