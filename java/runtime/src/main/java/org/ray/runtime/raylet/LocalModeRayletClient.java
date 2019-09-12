f2d3941bb0dd640c1ba1b8e42811801327e4f959
Kai Yang <kfstorm@outlook.com>
2019-09-12 13:32:25 +0800

    Replace NotImplementedException with UnsupportedOperationException

--- java/runtime/src/main/java/org/ray/runtime/raylet/LocalModeRayletClient.java
+++ java/runtime/src/main/java/org/ray/runtime/raylet/LocalModeRayletClient.java
@@ -1,8 +1,8 @@
 package org.ray.runtime.raylet;
 
-import org.apache.commons.lang3.NotImplementedException;
 import org.ray.api.id.ActorId;
 import org.ray.api.id.UniqueId;
+import org.ray.runtime.config.RunMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,20 +10,21 @@ import org.slf4j.LoggerFactory;
  * Raylet client for local mode.
  */
 public class LocalModeRayletClient implements RayletClient {
+
   private static final Logger LOGGER = LoggerFactory.getLogger(LocalModeRayletClient.class);
 
   @Override
   public UniqueId prepareCheckpoint(ActorId actorId) {
-    throw new NotImplementedException("Not implemented.");
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public void notifyActorResumedFromCheckpoint(ActorId actorId, UniqueId checkpointId) {
-    throw new NotImplementedException("Not implemented.");
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public void setResource(String resourceName, double capacity, UniqueId nodeId) {
-    LOGGER.error("Not implemented under SINGLE_PROCESS mode.");
+    LOGGER.error("setResource is unsupported under " + RunMode.SINGLE_PROCESS + " mode.");
   }
 }
