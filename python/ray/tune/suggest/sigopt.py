210c64163e8b3dd2e38bed8971cfd32fd178f405
Adi <adizim@berkeley.edu>
2019-09-20 16:07:12 -0700

    lint

--- python/ray/tune/suggest/sigopt.py
+++ python/ray/tune/suggest/sigopt.py
@@ -78,7 +78,8 @@ class SigOptSearch(SuggestionAlgorithm):
                 "version of Tune. "
                 "Setting `metric={}` and `mode=max`.".format(reward_attr))
         if "use_early_terminated_trials" in kwargs:
-            logger.warning("`use_early_terminated_trials` is not used in SigOptSearch.")
+            logger.warning(
+                "`use_early_terminated_trials` is not used in SigOptSearch.")
 
         self._max_concurrent = max_concurrent
         self._metric = metric
