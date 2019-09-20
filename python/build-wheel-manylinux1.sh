ce8954c3927df2b9563505deb3e554c66f21fd77
Mitchell Stern <stern.mitchell@gmail.com>
2019-09-20 14:57:14 -0700

    Move Node.js installation for Linux wheel build into build-wheel-manylinux1.sh

--- python/build-wheel-manylinux1.sh
+++ python/build-wheel-manylinux1.sh
@@ -30,10 +30,14 @@ export PATH=$PATH:/root/bin
 rm -f /usr/bin/python2
 ln -s /opt/python/cp27-cp27m/bin/python2 /usr/bin/python2
 
+# Install and use the latest version of Node.js in order to build the dashboard.
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+source $HOME/.nvm/nvm.sh
+nvm install node
+nvm use node
+
 # Build the dashboard so its static assets can be included in the wheel.
 pushd python/ray/dashboard/client
-  source $HOME/.nvm/nvm.sh
-  nvm use node
   npm ci
   npm run build
 popd
