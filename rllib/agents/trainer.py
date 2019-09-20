1e22e85eb9bb1347902f84d3e7d564ec379f4249
Kilian Batzner <git@kilians.net>
2019-09-20 12:22:04 +0000

    Remove trailing whitespace

--- rllib/agents/trainer.py
+++ rllib/agents/trainer.py
@@ -317,8 +317,8 @@ class Trainer(Trainable):
 
     _allow_unknown_configs = False
     _allow_unknown_subkeys = [
-        "tf_session_args", "local_tf_session_args", "env_config", "model", 
-        "optimizer", "multiagent", "custom_resources_per_worker", 
+        "tf_session_args", "local_tf_session_args", "env_config", "model",
+        "optimizer", "multiagent", "custom_resources_per_worker",
         "evaluation_config"
     ]
 
