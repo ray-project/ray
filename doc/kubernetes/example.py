7b1fd318cf63d3c97bb4d277caa32e948df5361f
Ed Oakes <ed.nmi.oakes@gmail.com>
2019-09-20 13:08:13 -0500

    better check

--- doc/kubernetes/example.py
+++ doc/kubernetes/example.py
@@ -50,9 +50,10 @@ if __name__ == "__main__":
     # NOTE: If you know you're running this on the head node, you can just
     # use "localhost" here.
     # redis_host = "localhost"
-    redis_host = os.environ["RAY_HEAD_SERVICE_HOST"]
-    if redis_host == "":
+    if ("RAY_HEAD_SERVICE_HOST" not in os.environ
+            or os.environ["RAY_HEAD_SERVICE_HOST"] == ""):
         raise ValueError("RAY_HEAD_SERVICE_HOST environment variable empty."
                          "Is there a ray cluster running?")
+    redis_host = os.environ["RAY_HEAD_SERVICE_HOST"]
     ray.init(address=redis_host + ":6379")
     main()
