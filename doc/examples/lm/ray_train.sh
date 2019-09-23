a62693c9b61be52545731901974f0f37d9a9ff50
Zhuohan Li <zhuohan123@vip.qq.com>
2019-09-22 19:10:52 -0700

    update ray_train.sh

--- doc/examples/lm/ray_train.sh
+++ doc/examples/lm/ray_train.sh
@@ -1,24 +1,24 @@
-TOTAL_UPDATES=100000    # Total number of training steps
-WARMUP_UPDATES=10000    # Warmup the learning rate over this many updates
-PEAK_LR=0.0005          # Peak learning rate, adjust as needed
-TOKENS_PER_SAMPLE=512   # Max sequence length
-MAX_POSITIONS=512       # Num. positional embeddings (usually same as above)
-MAX_SENTENCES=8        # Number of sequences per batch (batch size)
-UPDATE_FREQ=1          # Increase the batch size 16x
+TOTAL_UPDATES=125000       # Total number of training steps
+WARMUP_UPDATES=10000       # Warmup the learning rate over this many updates
+PEAK_LR=0.0005             # Peak learning rate, adjust as needed
+TOKENS_PER_SAMPLE=512      # Max sequence length
+MAX_POSITIONS=512          # Num. positional embeddings (usually same as above)
+MAX_SENTENCES=16           # Number of sequences per batch on one GPU (batch size)
+FIX_BATCH_SZIE=2048        # Number of batch size in total (max_sentences * update_freq * n_gpus)
+SAVE_INTERVAL_UPDATES=1000 # save a checkpoint every N updates
 
+LOG_DIR=$HOME/efs/log/
 DATA_DIR=$HOME/efs/data-bin/wikitext-103
+mkdir -p $LOG_DIR
 
-OUTPUT_PATH=$HOME/efs/log/
-
-mkdir -p $OUTPUT_PATH
-
-python $HOME/efs/ray_train.py $DATA_DIR \
+python $HOME/efs/ray_train.py --fp16 $DATA_DIR \
     --task masked_lm --criterion masked_lm \
     --arch roberta_base --sample-break-mode complete --tokens-per-sample $TOKENS_PER_SAMPLE \
     --optimizer adam --adam-betas '(0.9, 0.98)' --adam-eps 1e-6 --clip-norm 0.0 \
     --lr-scheduler polynomial_decay --lr $PEAK_LR --warmup-updates $WARMUP_UPDATES --total-num-update $TOTAL_UPDATES \
     --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
-    --max-sentences $MAX_SENTENCES --update-freq $UPDATE_FREQ \
+    --max-sentences $MAX_SENTENCES \
+    --fix-batch-size $FIX_BATCH_SZIE \
     --max-update $TOTAL_UPDATES --log-format simple --log-interval 1 \
-    --save-dir $OUTPUT_PATH --ddp-backend=no_c10d \
-    --save-interval-updates 100
+    --save-interval-updates $SAVE_INTERVAL_UPDATES \
+    --save-dir $LOG_DIR --ddp-backend=no_c10d
