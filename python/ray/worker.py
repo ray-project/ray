0d2a72ed31afc8353c3d07e88cef8171a63765e3
Eric Liang <ekhliang@gmail.com>
2019-09-27 22:17:30 -0700

    do it

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -1002,7 +1002,8 @@ class Worker(object):
     def _handle_process_task_failure(self, function_descriptor,
                                      return_object_ids, error, backtrace):
         function_name = function_descriptor.function_name
-        failure_object = RayTaskError(function_name, backtrace)
+        failure_object = RayTaskError(function_name, backtrace,
+                                      error.__class__)
         failure_objects = [
             failure_object for _ in range(len(return_object_ids))
         ]
@@ -2290,7 +2291,10 @@ def get(object_ids):
                 last_task_error_raise_time = time.time()
                 if isinstance(value, ray.exceptions.UnreconstructableError):
                     worker.dump_object_store_memory_usage()
-                raise value
+                if isinstance(value, RayTaskError):
+                    raise value.as_instanceof_cause()
+                else:
+                    raise value
 
         # Run post processors.
         for post_processor in worker._post_get_hooks:
