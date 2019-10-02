762f4619ca7ac43a91f65116e15940354104af87
Simon Mo <xmo@berkeley.edu>
2019-10-02 14:37:17 -0700

    Update docs

--- doc/source/serve.rst
+++ doc/source/serve.rst
@@ -1,11 +1,11 @@
 Ray Serve (Experimental)
 ========================
 
-Ray serve is a serving library that exposes python function/classes to HTTP.
+Ray Serve is a serving library that exposes python function/classes to HTTP.
 It has built-in support for flexible traffic policy. This means you can easy
 split incoming traffic to multiple implementations. 
 
-With ray serve, you can deploy your services at any scale.
+With Ray Serve, you can deploy your services at any scale.
 
 Quickstart
 ----------
--- python/ray/experimental/serve/examples/echo_full.py
+++ python/ray/experimental/serve/examples/echo_full.py
@@ -6,32 +6,33 @@ import ray
 import ray.experimental.serve as serve
 import requests
 
-# initialize ray serve system
-serve.init()
-# an endpoint is associated with an http URL
+# initialize ray serve system.
+# blocking=True will wait for HTTP server to be ready to serve request.
+serve.init(blocking=True)
+
+# an endpoint is associated with an http URL.
 serve.create_endpoint("my_endpoint", "/echo")
 
 
-# a backend can be a function or class
+# a backend can be a function or class.
 def echo_v1(request):
     return request
 
 
 serve.create_backend(echo_v1, "echo:v1")
 
-# we can link an endpoint to a backend, the means all the traffic
-# goes to my_endpoint will now goes to echo:v1 backend
+# We can link an endpoint to a backend, the means all the traffic
+# goes to my_endpoint will now goes to echo:v1 backend.
 serve.link("my_endpoint", "echo:v1")
 
 print(requests.get("http://127.0.0.1:8000/echo").json())
-# the service will be reachable from http
+# The service will be reachable from http
 
 print(ray.get(serve.get_handle("my_endpint").remote()))
+# as well as within the ray system.
 
-# as well as within the ray system
 
-
-# we can also add a new backend and split the traffic
+# We can also add a new backend and split the traffic.
 def echo_v2(request):
     # magic
     return "something new"
@@ -39,5 +40,10 @@ def echo_v2(request):
 
 serve.create_backend(echo_v2, "echo:v2")
 
-# The two backend will now split the traffic 50%-50%
+# The two backend will now split the traffic 50%-50%.
 serve.split("my_endpoint", {"echo:v1": 0.5, "echo:v2": 0.5})
+
+# Observe requests are now split between two backends.
+for _ in range(5):
+    print(requests.get("http://127.0.0.1:8000/echo").json())
+
