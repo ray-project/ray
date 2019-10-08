22f6be5c93416254570e7a01b39ab528e1692c38
Alind Khare <alindkhare@gatech.edu>
2019-10-08 10:48:59 -0400

    temp

--- python/ray/experimental/serve/examples/echo_pipeline.py
+++ python/ray/experimental/serve/examples/echo_pipeline.py
@@ -1,16 +1,23 @@
 import time
 
 import requests
+from werkzeug import urls
 
 from ray.experimental import serve
 from ray.experimental.serve.utils import pformat_color_json
 
 
 def echo1(context):
+	# query_string_dict = urls.url_decode(context["query_string"])
+	message = ""
+	# message += query_string_dict.get("message", "")
+	# message += " "
+	message += 'FROM MODEL1 -> '
+	return message
 	# context["query_string"] += 'FROM MODEL1 -> '
-	return context
+	# return context
 def echo2(context):
-	context["query_string"] += 'FROM MODEL2 -> '
+	context += 'FROM MODEL2 -> '
 	return context
 
 serve.init(blocking=True)
@@ -20,7 +27,7 @@ serve.create_endpoint_pipeline("pipeline1", "/echo", blocking=True)
 serve.create_backend(echo1, "echo:v1")
 serve.create_no_http_service("serve1")
 serve.link_service("serve1", "echo:v1")
-serve.add_service_to_pipeline("pipeline1","serve1")
+serve.add_service_to_pipeline("pipeline1","serve1",blocking=True)
 
 while True:
     resp = requests.get("http://127.0.0.1:8000/echo").json()
