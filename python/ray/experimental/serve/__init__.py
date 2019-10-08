1da01b7f3111cd9ec65b45bb513ca93e79da56f8
Alind Khare <alindkhare@gatech.edu>
2019-10-08 10:11:38 -0400

    included api calls in init

--- python/ray/experimental/serve/__init__.py
+++ python/ray/experimental/serve/__init__.py
@@ -2,11 +2,11 @@ import sys
 if sys.version_info < (3, 0):
     raise ImportError("serve is Python 3 only.")
 
-from ray.experimental.serve.api import (init, create_backend, create_endpoint,
-                                        link, split, rollback, get_handle,
+from ray.experimental.serve.api import (init, create_backend, create_endpoint_pipeline,create_no_http_service,
+                                        add_service_to_pipeline,link_service, split, rollback, get_handle,
                                         global_state)  # noqa: E402
 
 __all__ = [
-    "init", "create_backend", "create_endpoint", "link", "split", "rollback",
+    "init", "create_backend", "create_endpoint_pipeline","create_no_http_service","add_service_to_pipeline","link", "link_service", "rollback",
     "get_handle", "global_state"
 ]
