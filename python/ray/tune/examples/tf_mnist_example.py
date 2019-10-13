e20a7818750b04146fd254f1a98de759da50cf4c
Richard Liaw <rliaw@berkeley.edu>
2019-10-12 22:33:14 -0700

    tfmnist

--- python/ray/tune/examples/tf_mnist_example.py
+++ python/ray/tune/examples/tf_mnist_example.py
@@ -1,37 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# ##### Copyright 2019 The TensorFlow Authors.
+# This example showcases how to use TF2.0 APIs with Tune.
+#
+# As of 10/12/2019: One caveat of using TF2.0 is that TF AutoGraph
+# functionality does not interact nicely with Ray actors. One way to get around
+# this is to `import tensorflow` inside the Tune Trainable.
+#
+# Original code: https://www.tensorflow.org/tutorials/quickstart/advanced
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import tensorflow as tf
-
 from tensorflow.keras.layers import Dense, Flatten, Conv2D
 from tensorflow.keras import Model
 from tensorflow.keras.datasets.mnist import load_data
+
 from ray import tune
 
 
-def create_mnist_data():
-    (x_train, y_train), (x_test, y_test) = load_data()
-    x_train, x_test = x_train / 255.0, x_test / 255.0
-
-    # Add a channels dimension
-    x_train = x_train[..., tf.newaxis]
-    x_test = x_test[..., tf.newaxis]
-    return (x_train, y_train), (x_test, y_test)
-
+MAX_TRAIN_BATCH = 10
 
 class MyModel(Model):
-    def __init__(self):
+    def __init__(self, hiddens=128):
         super(MyModel, self).__init__()
         self.conv1 = Conv2D(32, 3, activation="relu")
         self.flatten = Flatten()
-        self.d1 = Dense(128, activation="relu")
+        self.d1 = Dense(hiddens, activation="relu")
         self.d2 = Dense(10, activation="softmax")
 
     def call(self, x):
@@ -41,47 +37,57 @@ class MyModel(Model):
         return self.d2(x)
 
 
-# import ipdb; ipdb.set_trace()
-
 class MNISTTrainable(tune.Trainable):
     def _setup(self, config):
-        (x_train, y_train), (x_test, y_test) = create_mnist_data()
+        # See the above note.
+        import tensorflow as tf
+        (x_train, y_train), (x_test, y_test) = load_data()
+        x_train, x_test = x_train / 255.0, x_test / 255.0
 
+        # Add a channels dimension
+        x_train = x_train[..., tf.newaxis]
+        x_test = x_test[..., tf.newaxis]
         self.train_ds = tf.data.Dataset.from_tensor_slices(
-                (x_train, y_train)).shuffle(10000).batch(32)
+                (x_train, y_train))
+        self.train_ds = self.train_ds.shuffle(10000).batch(
+            config.get("batch", 32))
 
         self.test_ds = tf.data.Dataset.from_tensor_slices(
             (x_test, y_test)).batch(32)
 
-        self.model = config["my_model"]()
-        # self.loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+        self.model = MyModel(hiddens=config.get("hiddens", 128))
+        self.loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
         self.optimizer = tf.keras.optimizers.Adam()
-        # self.train_loss = tf.keras.metrics.Mean(name="train_loss")
-        # self.train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
-        #     name="train_accuracy")
+        self.train_loss = tf.keras.metrics.Mean(name="train_loss")
+        self.train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+            name="train_accuracy")
 
-        # self.test_loss = tf.keras.metrics.Mean(name="test_loss")
-        # self.test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
-        #     name="test_accuracy")
-        self.tf_train_step = tf.function(self.train_step)
-        self.tf_test_step = tf.function(self.test_step)
+        self.test_loss = tf.keras.metrics.Mean(name="test_loss")
+        self.test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+            name="test_accuracy")
 
-    def train_step(self, images, labels):
-        with tf.GradientTape() as tape:
+        @tf.function
+        def train_step(images, labels):
+            with tf.GradientTape() as tape:
+                predictions = self.model(images)
+                loss = self.loss_object(labels, predictions)
+            gradients = tape.gradient(loss, self.model.trainable_variables)
+            self.optimizer.apply_gradients(
+                zip(gradients, self.model.trainable_variables))
+
+            self.train_loss(loss)
+            self.train_accuracy(labels, predictions)
+
+        @tf.function
+        def test_step(images, labels):
             predictions = self.model(images)
-            loss = self.loss_object(labels, predictions)
-        gradients = tape.gradient(loss, self.model.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+            t_loss = self.loss_object(labels, predictions)
 
-        self.train_loss(loss)
-        self.train_accuracy(labels, predictions)
+            self.test_loss(t_loss)
+            self.test_accuracy(labels, predictions)
 
-    def test_step(self, images, labels):
-        predictions = self.model(images)
-        t_loss = self.loss_object(labels, predictions)
-
-        self.test_loss(t_loss)
-        self.test_accuracy(labels, predictions)
+        self.tf_train_step = train_step
+        self.tf_test_step = test_step
 
     def _train(self):
         self.train_loss.reset_states()
@@ -89,26 +95,28 @@ class MNISTTrainable(tune.Trainable):
         self.test_loss.reset_states()
         self.test_accuracy.reset_states()
 
-        for images, labels in train_ds:
+        for idx, (images, labels) in enumerate(self.train_ds):
+            if idx > MAX_TRAIN_BATCH:  # This is optional and can be removed.
+                break
             self.tf_train_step(images, labels)
 
-        for test_images, test_labels in test_ds:
+        for test_images, test_labels in self.test_ds:
             self.tf_test_step(test_images, test_labels)
 
+        # It is important to return tf.Tensors as numpy objects.
         return {
-            "epoch": epoch+1,
-            "loss": self.train_loss.result(),
-            "accuracy": self.train_accuracy.result()*100,
-            "test_loss": self.test_loss.result(),
-            "test_accuracy": self.test_accuracy.result()*100
+            "epoch": self.iteration,
+            "loss": self.train_loss.result().numpy(),
+            "accuracy": self.train_accuracy.result().numpy()*100,
+            "test_loss": self.test_loss.result().numpy(),
+            "mean_accuracy": self.test_accuracy.result().numpy()*100
         }
 
-        # Reset the metrics for the next epoch
 
-
-# The image classifier is now trained to ~98% accuracy on this dataset. To learn more, read the [TensorFlow tutorials](https://www.tensorflow.org/tutorials).
 if __name__ == '__main__':
+    import ray
     tune.run(
         MNISTTrainable,
         stop={"training_iteration": 5},
-        config={"my_model": MyModel})
+        verbose=1,
+        config={"hiddens": tune.grid_search([32, 64, 128])})
