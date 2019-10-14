56a2c487fe2306d30de05919e687a463c3aa67bd
Richard Liaw <rliaw@berkeley.edu>
2019-10-14 10:32:24 -0700

    comment

--- python/ray/tune/integration/keras.py
+++ python/ray/tune/integration/keras.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow.keras as keras
+from tensorflow import keras
 from ray.tune import track
 
 
