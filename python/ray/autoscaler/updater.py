9fc7cc6f3e4851ae75bf3fb1a2b9cdf4c627813d
Philipp Moritz <pcmoritz@gmail.com>
2019-10-17 17:15:50 -0700

    add port forwarding for kubernetes

--- python/ray/autoscaler/updater.py
+++ python/ray/autoscaler/updater.py
@@ -58,9 +58,11 @@ class KubernetesCommandRunner(object):
         logger.info(self.log_prefix + "Running {}...".format(cmd))
 
         if port_forward:
+            if not isinstance(port_forward, list):
+                port_forward = [port_forward]
             port_forward_cmd = self.kubectl + [
                 "port-forward", self.node_id,
-                str(port_forward)
+                " ".join([str(fwd) for fwd in port_forward])
             ]
             port_forward_process = subprocess.Popen(port_forward_cmd)
             # Give port-forward a grace period to run and print output before
