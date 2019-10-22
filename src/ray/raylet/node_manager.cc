db064a722d357fd556c3c653e171dd7f5f7580f5
Ed Oakes <ed.nmi.oakes@gmail.com>
2019-10-22 13:22:48 -0700

    Unblock from client on wait

--- src/ray/raylet/node_manager.cc
+++ src/ray/raylet/node_manager.cc
@@ -1235,8 +1235,7 @@ void NodeManager::ProcessWaitRequestMessage(
 
   ray::Status status = object_manager_.Wait(
       object_ids, wait_ms, num_required_objects, wait_local,
-      [this, client_blocked, client, current_task_id](std::vector<ObjectID> found,
-                                                      std::vector<ObjectID> remaining) {
+      [this, client](std::vector<ObjectID> found, std::vector<ObjectID> remaining) {
         // Write the data.
         flatbuffers::FlatBufferBuilder fbb;
         flatbuffers::Offset<protocol::WaitReply> wait_reply = protocol::CreateWaitReply(
@@ -1246,12 +1245,7 @@ void NodeManager::ProcessWaitRequestMessage(
         auto status =
             client->WriteMessage(static_cast<int64_t>(protocol::MessageType::WaitReply),
                                  fbb.GetSize(), fbb.GetBufferPointer());
-        if (status.ok()) {
-          // The client is unblocked now because the wait call has returned.
-          if (client_blocked) {
-            HandleTaskUnblocked(client, current_task_id);
-          }
-        } else {
+        if (!status.ok()) {
           // We failed to write to the client, so disconnect the client.
           RAY_LOG(WARNING)
               << "Failed to send WaitReply to client, so disconnecting client";
