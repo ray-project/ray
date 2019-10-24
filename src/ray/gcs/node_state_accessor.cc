5034be050970a57a0f14fd9f4abaa8b45fde77dd
micafan <minmin.fmm@alipay.com>
2019-10-24 14:40:08 +0800

    raylet server gen node id

---
+++ src/ray/gcs/node_state_accessor.cc
@@ -0,0 +1,111 @@
+#include "ray/gcs/node_state_accessor.h"
+#include "ray/gcs/redis_gcs_client.h"
+#include "ray/util/logging.h"
+
+namespace ray {
+
+namespace gcs {
+
+NodeStateAccessor::NodeStateAccessor(RedisGcsClient &client_impl)
+    : client_impl_(client_impl) {}
+
+Status NodeStateAccessor::RegisterSelf(const GcsNodeInfo &local_node_info) {
+  ClientTable &client_table = client_impl_.client_table();
+  return client_table.Connect(local_node_info);
+}
+
+Status NodeStateAccessor::UnregisterSelf() {
+  ClientTable &client_table = client_impl_.client_table();
+  return client_table.Disconnect();
+}
+
+void NodeStateAccessor::RegisterWatcher(const NodeInfoCallback &node_added_callback,
+                                        const NodeInfoCallback &node_removed_callback) {
+  // TODO(micafan) Subscribe all and request notifications.
+  RAY_CHECK(node_added_callback != nullptr && node_removed_callback != nullptr);
+  ClientTable &client_table = client_impl_.client_table();
+
+  auto on_node_added = [node_added_callback](RedisGcsClient *client, const ClientID &id,
+                                             const GcsNodeInfo &node_info) {
+    node_added_callback(node_info);
+  };
+  client_table.RegisterClientAddedCallback(on_node_added);
+
+  auto on_node_removed = [node_removed_callback](RedisGcsClient *client,
+                                                 const ClientID &id,
+                                                 const GcsNodeInfo &node_info) {
+    node_removed_callback(node_info);
+  };
+  client_table.RegisterClientRemovedCallback(on_node_removed);
+}
+
+const ClientID &NodeStateAccessor::GetSelfId() const {
+  ClientTable &client_table = client_impl_.client_table();
+  return client_table.GetLocalClientId();
+}
+
+const GcsNodeInfo &NodeStateAccessor::GetSelfInfo() const {
+  ClientTable &client_table = client_impl_.client_table();
+  return client_table.GetLocalClient();
+}
+
+Status NodeStateAccessor::AsyncUnregister(const ClientID &node_id,
+                                          const StatusCallback &callback) {
+  ClientTable::WriteCallback on_done = nullptr;
+  if (callback != nullptr) {
+    on_done = [callback](RedisGcsClient *client, const ClientID &id,
+                         const GcsNodeInfo &data) { callback(Status::OK()); };
+  }
+  ClientTable &client_table = client_impl_.client_table();
+  return client_table.MarkDisconnected(node_id, on_done);
+}
+
+Status NodeStateAccessor::AsyncGetAll(const MultiItemCallback<GcsNodeInfo> &callback) {
+  RAY_CHECK(callback != nullptr);
+  auto on_done = [callback](RedisGcsClient *client, const ClientID &id,
+                            const std::vector<GcsNodeInfo> &data) {
+    callback(Status::OK(), data);
+  };
+  ClientTable &client_table = client_impl_.client_table();
+  return client_table.Lookup(on_done);
+}
+
+boost::optional<GcsNodeInfo> NodeStateAccessor::GetFromCache(
+    const ClientID &node_id) const {
+  ClientTable &client_table = client_impl_.client_table();
+  GcsNodeInfo node_info;
+  client_table.GetClient(node_id, node_info);
+
+  boost::optional<GcsNodeInfo> optional_node;
+  ClientID id = ClientID::FromBinary(node_info.node_id());
+  if (!id.IsNil()) {
+    optional_node = std::move(node_info);
+  }
+  return optional_node;
+}
+
+const std::unordered_map<ClientID, GcsNodeInfo> &NodeStateAccessor::GetAllFromCache()
+    const {
+  ClientTable &client_table = client_impl_.client_table();
+  return client_table.GetAllClients();
+}
+
+std::vector<ClientID> NodeStateAccessor::GetAllIdsFromCache() const {
+  std::vector<ClientID> node_ids;
+
+  ClientTable &client_table = client_impl_.client_table();
+  const auto &node_map = client_table.GetAllClients();
+  for (const auto &item : node_map) {
+    node_ids.emplace_back(item.first);
+  }
+  return node_ids;
+}
+
+bool NodeStateAccessor::IsRemoved(const ClientID &node_id) const {
+  ClientTable &client_table = client_impl_.client_table();
+  return client_table.IsRemoved(node_id);
+}
+
+}  // namespace gcs
+
+}  // namespace ray
---
+++ src/ray/gcs/node_state_accessor.h
@@ -0,0 +1,108 @@
+#ifndef RAY_GCS_NODE_STATE_ACCESSOR_H
+#define RAY_GCS_NODE_STATE_ACCESSOR_H
+
+#include <boost/optional.hpp>
+#include <map>
+#include <mutex>
+#include <vector>
+#include "ray/common/id.h"
+#include "ray/gcs/callback.h"
+#include "ray/gcs/tables.h"
+
+namespace ray {
+
+namespace gcs {
+
+class RedisGcsClient;
+
+/// \class NodeStateAccessor
+/// NodeStateAccessor class encapsulates the implementation details of
+/// reading or writing or subscribing of node's information (immutable fields like id,
+/// and mutable fields like runtime state).
+class NodeStateAccessor {
+ public:
+  explicit NodeStateAccessor(RedisGcsClient &client_impl);
+
+  ~NodeStateAccessor() {}
+
+  /// Register local node to GCS synchronously, and begins subscription to all nodes
+  /// from GCS (the node informations will be cached).
+  ///
+  /// \param node_info The information of node to register to GCS.
+  /// \return Status
+  Status RegisterSelf(const GcsNodeInfo &local_node_info);
+
+  /// Cancel registration of local node to GCS synchronously.
+  ///
+  /// \Status
+  Status UnregisterSelf();
+
+  /// This callback is used to receive node information when a node member changed.
+  using NodeInfoCallback = std::function<void(const GcsNodeInfo &node_info)>;
+
+  /// Register callbacks to monitor the changes of node members. Repeated register
+  /// will cause crash.
+  ///
+  /// \param node_added_callback Callback that will be called if a new node is added.
+  /// \param node_removed_callback Callback that will be called if a node is removed.
+  /// TODO(micafan) Begin subscription to all nodes.
+  void RegisterWatcher(const NodeInfoCallback &node_added_callback,
+                       const NodeInfoCallback &node_removed_callback);
+
+  /// Get id of local node which registered by 'RegisterSelf'.
+  ///
+  /// \return ClientID
+  const ClientID &GetSelfId() const;
+
+  /// Get information of local node which registered by 'RegisterSelf'.
+  ///
+  /// \return GcsNodeInfo
+  const GcsNodeInfo &GetSelfInfo() const;
+
+  /// Cancel registration of a node to GCS asynchronously.
+  ///
+  /// \param node_id The ID of node that to be unregistered.
+  /// \param callback Callback that will be called when unregistration is complete.
+  Status AsyncUnregister(const ClientID &node_id, const StatusCallback &callback);
+
+  /// Get information of all nodes from GCS asynchronously.
+  ///
+  /// \param callback Callback that will be called after lookup finishes.
+  Status AsyncGetAll(const MultiItemCallback<GcsNodeInfo> &callback);
+
+  /// Get node information from local cache.
+  /// Non-thread safe.
+  ///
+  /// \param node_id The ID of node to look up in local cache.
+  /// \return The item returned by GCS. If the item to read doesn't exist,
+  /// this optional object is empty.
+  boost::optional<GcsNodeInfo> GetFromCache(const ClientID &node_id) const;
+
+  /// Get information of all nodes from local cache.
+  /// Non-thread safe.
+  ///
+  /// \return All nodes in cache.
+  const std::unordered_map<ClientID, GcsNodeInfo> &GetAllFromCache() const;
+
+  /// Get the ids of all nodes from local cache.
+  /// Non-thread safe.
+  ///
+  /// \return The ids of all nodes.
+  std::vector<ClientID> GetAllIdsFromCache() const;
+
+  /// Search local cache to find out if the given node is removed.
+  /// Non-thread safe.
+  ///
+  /// \param node_id The id of the node to check.
+  /// \return Whether the node is removed.
+  bool IsRemoved(const ClientID &node_id) const;
+
+ private:
+  RedisGcsClient &client_impl_;
+};
+
+}  // namespace gcs
+
+}  // namespace ray
+
+#endif  // RAY_GCS_NODE_STATE_ACCESSOR_H
