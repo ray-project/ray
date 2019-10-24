4bd1a81e1055679dc92583a267bf5c4be737c93d
Eric Liang <ekhliang@gmail.com>
2019-10-23 17:02:45 -0700

    update

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -155,10 +155,6 @@ class Worker(object):
         # postprocessor must take two arguments ("object_ids", and "values").
         self._post_get_hooks = []
 
-        self._cached = False
-        self._cached_value = None
-        self._cached_value_serialized = None
-
     @property
     def connected(self):
         return self.node is not None
