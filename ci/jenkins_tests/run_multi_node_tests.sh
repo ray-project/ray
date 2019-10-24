5420be31d564d54183c0e2996a5bdf72882e25b5
Eric Liang <ekhliang@gmail.com>
2019-10-24 12:01:10 -0700

    fix multi node

--- ci/jenkins_tests/run_multi_node_tests.sh
+++ ci/jenkins_tests/run_multi_node_tests.sh
@@ -15,19 +15,6 @@ DOCKER_SHA=$($ROOT_DIR/../../build-docker.sh --output-sha --no-cache)
 SUPPRESS_OUTPUT=$ROOT_DIR/../suppress_output
 echo "Using Docker image" $DOCKER_SHA
 
-######################## RLLIB TESTS #################################
-
-source $ROOT_DIR/run_rllib_tests.sh
-
-######################## TUNE TESTS #################################
-
-bash $ROOT_DIR/run_tune_tests.sh ${MEMORY_SIZE} ${SHM_SIZE} $DOCKER_SHA
-
-######################## RAY BACKEND TESTS #################################
-
-$SUPPRESS_OUTPUT docker run --rm --shm-size=60G --memory=60G $DOCKER_SHA \
-    python /ray/ci/jenkins_tests/miscellaneous/large_memory_test.py
-
 ######################## EXAMPLE TESTS #################################
 
 $SUPPRESS_OUTPUT docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
@@ -44,6 +31,19 @@ $SUPPRESS_OUTPUT docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} 
 
 $SUPPRESS_OUTPUT docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
     python /ray/doc/examples/doc_code/tf_example.py
+
+######################## RLLIB TESTS #################################
+
+source $ROOT_DIR/run_rllib_tests.sh
+
+######################## TUNE TESTS #################################
+
+bash $ROOT_DIR/run_tune_tests.sh ${MEMORY_SIZE} ${SHM_SIZE} $DOCKER_SHA
+
+######################## RAY BACKEND TESTS #################################
+
+$SUPPRESS_OUTPUT docker run --rm --shm-size=60G --memory=60G $DOCKER_SHA \
+    python /ray/ci/jenkins_tests/miscellaneous/large_memory_test.py
 
 ######################## SGD TESTS #################################
 
