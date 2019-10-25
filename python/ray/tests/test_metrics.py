b9cb6b5f326e262f4c2304a1e83010b2cff77464
Stephanie Wang <swang@cs.berkeley.edu>
2019-10-25 12:13:57 -0700

    Add timeout in test

--- python/ray/tests/test_metrics.py
+++ python/ray/tests/test_metrics.py
@@ -3,12 +3,14 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+import grpc
 import psutil
+import time
 
 import ray
 from ray.core.generated import node_manager_pb2
 from ray.core.generated import node_manager_pb2_grpc
-import grpc
+from ray.tests.utils import RayTestTimeoutException
 
 
 def test_worker_stats(ray_start_regular):
@@ -25,13 +27,27 @@ def test_worker_stats(ray_start_regular):
     assert len(drivers) == 1
     assert os.getpid() == drivers[0].pid
 
-    # Check that the rest of the processes are workers, 1 for each CPU.
-    assert len(reply.workers_stats) == num_cpus + 1
-    # Check that all processes are Python.
-    pids = [worker.pid for worker in reply.workers_stats]
-    processes = [
-        p.info["name"] for p in psutil.process_iter(attrs=["pid", "name"])
-        if p.info["pid"] in pids
-    ]
-    for process in processes:
-        assert "python" in process
+    timeout_seconds = 20
+    start_time = time.time()
+    while True:
+        if time.time() - start_time > timeout_seconds:
+            raise RayTestTimeoutException(
+                "Timed out while waiting for worker processes")
+
+        # Wait for the workers to start.
+        if len(reply.workers_stats) < num_cpus + 1:
+            time.sleep(1)
+            reply = stub.GetNodeStats(node_manager_pb2.NodeStatsRequest())
+            continue
+
+        # Check that the rest of the processes are workers, 1 for each CPU.
+        assert len(reply.workers_stats) == num_cpus + 1
+        # Check that all processes are Python.
+        pids = [worker.pid for worker in reply.workers_stats]
+        processes = [
+            p.info["name"] for p in psutil.process_iter(attrs=["pid", "name"])
+            if p.info["pid"] in pids
+        ]
+        for process in processes:
+            assert "python" in process
+        break
