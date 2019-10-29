384ad15273a25579ecc5e7fa9717fbea1d878950
悟言 <wanxing.wwx@alibaba-inc.com>
2019-10-29 17:16:58 +0800

    add streaming queue code

---
+++ streaming/src/queue/config.h
@@ -0,0 +1,12 @@
+#ifndef _STREAMING_QUEUE_CONFIG_H_
+#define _STREAMING_QUEUE_CONFIG_H_
+
+namespace ray {
+namespace streaming {
+
+constexpr uint64_t PULL_UPPSTREAM_DATA_TIMEOUT_MS = 10*1000;
+constexpr uint64_t COMMON_SYNC_CALL_TIMEOUTT_MS = 5*1000;
+
+}
+}
+#endif
--- streaming/src/queue_interface.h
+++ streaming/src/queue_interface.h
@@ -7,6 +7,7 @@
 #include "ray/common/status.h"
 #include "ray/raylet/raylet_client.h"
 #include "ray/util/util.h"
+#include "queue/queue_manager.h"
 
 #include <iterator>
 #include <limits>
@@ -28,7 +29,7 @@ class QueueWriterInterface {
  public:
   QueueWriterInterface() {}
   virtual ~QueueWriterInterface() {}
-  virtual Status CreateQueue(const ObjectID &queue_id, int64_t data_size,
+  virtual Status CreateQueue(const ObjectID &queue_id, int64_t data_size, uint64_t actor_handle,
                              bool reconstruct_queue = false, bool clear = false) = 0;
   virtual Status PushQueueItem(const ObjectID &queue_id, uint64_t seq_id, uint8_t *data,
                                uint32_t data_size, uint64_t timestamp) = 0;
@@ -57,7 +58,7 @@ class QueueReaderInterface {
   QueueReaderInterface() {}
   virtual ~QueueReaderInterface() {}
   virtual bool GetQueue(const ObjectID &queue_id, int64_t timeout_ms,
-                        uint64_t start_seq_id) = 0;
+                        uint64_t start_seq_id, uint64_t actor_handle) = 0;
   virtual Status GetQueueItem(const ObjectID &object_id, uint8_t *&data,
                               uint32_t &data_size, uint64_t &seq_id,
                               uint64_t timeout_ms = -1) = 0;
@@ -68,6 +69,72 @@ class QueueReaderInterface {
                                    int64_t timeout_ms,
                                    std::vector<ObjectID> &failed_queues) = 0;
   virtual Status DeleteQueue(const ObjectID &queue_id) = 0;
+};
+
+/***
+ * code below is interface implementation of streaming queue
+ ***/
+class StreamingQueueWriter : public QueueWriterInterface {
+ public:
+  StreamingQueueWriter(CoreWorker *core_worker, RayFunction &async_func,
+                       RayFunction &sync_func);
+  ~StreamingQueueWriter() {}
+
+  Status CreateQueue(const ObjectID &queue_id, int64_t data_size,
+                     uint64_t actor_handle,
+                     bool reconstruct_queue = false, bool clear = false);
+  bool IsQueueFoundInLocal(const ObjectID &queue_id, const int64_t timeout_ms);
+  Status SetQueueEvictionLimit(const ObjectID &queue_id, uint64_t eviction_limit);
+
+  void PullQueueToLocal(const ObjectID &queue_id);
+  void WaitQueuesInCluster(const std::vector<ObjectID> &queue_ids, int64_t timeout_ms,
+                           std::vector<ObjectID> &failed_queues);
+  void GetMinConsumedSeqID(const ObjectID &queue_id, uint64_t &min_consumed_id);
+  void GetUnconsumedBytes(const ObjectID &queue_id, uint32_t &unconsumed_bytes);
+  Status PushQueueItem(const ObjectID &queue_id, uint64_t seq_id, uint8_t *data,
+                       uint32_t data_size, uint64_t timestamp);
+  bool NotifyResubscribe(const ObjectID &queue_id);
+  void CleanupSubscription(const ObjectID &queue_id);
+  void GetLastQueueItem(const ObjectID &queue_id, std::shared_ptr<uint8_t> &data,
+                        uint32_t &data_size, uint64_t &sequence_id);
+  virtual uint64_t GetLastMsgId(const ObjectID &queue_id, uint64_t &last_queue_seq_id);
+  Status DeleteQueue(const ObjectID &queue_id);
+  bool UsePull() { return false; }
+
+ private:
+  std::shared_ptr<ray::streaming::QueueManager> queue_manager_;
+  std::shared_ptr<ray::streaming::QueueWriter> queue_writer_;
+  ActorID actor_id_;
+
+  CoreWorker *core_worker_;
+  RayFunction async_func_;
+  RayFunction sync_func_;
+};
+
+class StreamingQueueReader : public QueueReaderInterface {
+ public:
+  StreamingQueueReader(CoreWorker *core_worker, RayFunction &async_func,
+                       RayFunction &sync_func);
+  ~StreamingQueueReader() {}
+
+  bool GetQueue(const ObjectID &queue_id, int64_t timeout_ms, uint64_t start_seq_id,
+                uint64_t actor_handle);
+  Status GetQueueItem(const ObjectID &object_id, uint8_t *&data, uint32_t &data_size,
+                      uint64_t &seq_id, uint64_t timeout_ms = -1);
+  void NotifyConsumedItem(const ObjectID &object_id, uint64_t seq_id);
+  void GetLastSeqID(const ObjectID &object_id, uint64_t &last_seq_id);
+
+  void WaitQueuesInCluster(const std::vector<ObjectID> &queue_ids, int64_t timeout_ms,
+                           std::vector<ObjectID> &failed_queues);
+  Status DeleteQueue(const ObjectID &queue_id);
+
+ private:
+  std::shared_ptr<ray::streaming::QueueManager> queue_manager_;
+  std::shared_ptr<ray::streaming::QueueReader> queue_reader_;
+  ActorID actor_id_;
+  CoreWorker *core_worker_;
+  RayFunction async_func_;
+  RayFunction sync_func_;
 };
 
 }  // namespace ray
