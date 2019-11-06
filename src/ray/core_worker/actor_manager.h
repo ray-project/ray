68f30f42fff6bb3030a6063b2d18618619d780f9
Stephanie Wang <swang@cs.berkeley.edu>
2019-11-05 21:20:15 -0800

    doc

--- src/ray/core_worker/actor_manager.h
+++ src/ray/core_worker/actor_manager.h
@@ -50,23 +50,47 @@ class ActorManager {
   /// \return Status::Invalid if we don't have this actor handle.
   Status GetActorHandle(const ActorID &actor_id, ActorHandle **actor_handle) const;
 
-  void RegisterChildActor(const ray::TaskSpecification &spec);
-
+  /// Add a reference to an actor. We will track its state, e.g., alive, and
+  /// its location.
   bool AddActorHandle(std::unique_ptr<ActorHandle> actor_handle);
+
+  /// Register an actor that we created. In addition to the accounting done for
+  /// normal actor references, we are also responsible for restarting these
+  /// actors on failure (if max reconstructions is nonzero) and for marking
+  /// these actors as dead on node failure.
+  ///
+  /// \param[in] spec The spec for the actor creation task. If the child actor
+  /// dies and max reconstructions is nonzero, then this task should be
+  /// resubmitted to recreate the actor via the ActorCreationCallback.
+  void RegisterChildActor(const ray::TaskSpecification &spec);
 
   /// Clear all state for actors. This should be called by non-actors when
   /// completing a task.
   void Clear();
 
+  /// Handler for a location update of an actor that we have a reference to.
+  ///
+  /// \param[in] actor_id The ID of the actor.
+  /// \param[in] node_id The ID of the node where the actor is now located.
+  /// \param[in] ip_address The IP address where the actor is now located.
+  /// \param[in] port The port that the actor is now listening on.
   void OnActorLocationChanged(const ActorID &actor_id, const ClientID &node_id,
                               const std::string &ip_address, const int port);
 
+  /// Handler for recoverable failure of an actor that we have a reference to.
+  ///
+  /// \param[in] actor_id The ID of the actor.
   void OnActorFailed(const ActorID &actor_id);
 
+  /// Handler for terminal failure of an actor that we have a reference to.
+  ///
+  /// \param[in] actor_id The ID of the actor.
   void OnActorDead(const ActorID &actor_id);
 
  private:
+  /// Interface to the class that maintains RPC clients to the actors.
   DirectActorClientsInterface &direct_actor_clients_;
+  /// Callback that is called if a failed actor needs to be recreated.
   const ActorCreationCallback actor_creation_callback_;
   /// Map of actors that we created.
   absl::flat_hash_map<ActorID, ChildActor> children_actors_;
