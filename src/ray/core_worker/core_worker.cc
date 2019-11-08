a4587f7fc02d457b893c30fa872592f28a670c68
Eric Liang <ekhliang@gmail.com>
2019-11-07 21:33:12 -0800

    fix merge

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -499,7 +499,7 @@ Status CoreWorker::SubmitTaskToRaylet(const TaskSpecification &task_spec) {
 
   if (task_deps->size() > 0) {
     for (size_t i = 0; i < num_returns; i++) {
-      reference_counter_.SetDependencies(task_spec.ReturnId(i), task_deps);
+      reference_counter_.SetDependencies(task_spec.ReturnIdForPlasma(i), task_deps);
     }
   }
 
