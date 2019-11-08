612e4f125a15c9e7cd6b186e9902a2008e939c30
Ed Oakes <ed.nmi.oakes@gmail.com>
2019-11-08 15:24:26 -0800

    add ray start command

--- python/ray/autoscaler/yarn/submit.py
+++ python/ray/autoscaler/yarn/submit.py
@@ -4,17 +4,20 @@ from __future__ import print_function
 
 import skein
 
-ray_start_script = "sleep infinity"
+ray_start_script = "ray start --head --redis-port=6379 --object-manager-port=8076 --autoscaling-config=~/ray_bootstrap_config.yaml"
 
 
 def main():
     client = skein.Client(address=None)
 
     # script should be the ray start command
-    ray_master = skein.Master(resources=None, script=ray_start_script)
+    ray_master = skein.Master(
+        resources=None,
+        script=ray_start_script,
+        files={"ray_bootstrap_config.yaml": "example-full.yaml"})
     worker_service = skein.Service(
         resources=skein.Resources(memory="1GiB", vcores=1),
-        script="sleep 1000")
+        script="sleep infinity")
     spec = skein.ApplicationSpec(
         services={"ray-worker": worker_service},
         master=ray_master,
