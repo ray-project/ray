eb9559985c2ffab293642c199a7a1bca1febf75b
ujvl <misraujval@gmail.com>
2019-11-13 12:41:56 -0800

    Fix test

--- python/ray/tune/tests/test_checkpoint_manager.py
+++ python/ray/tune/tests/test_checkpoint_manager.py
@@ -20,22 +20,16 @@ class CheckpointManagerTest(unittest.TestCase):
     def mock_result(i):
         return {"i": i}
 
-    def testOnCheckpoint(self):
-        """Tests monotonically increasing, then decreasing priorities."""
+    def testOnCheckpointOrdered(self):
+        """Tests monotonically increasing priorities."""
         keep_checkpoints_num = 2
         checkpoint_manager = CheckpointManager(keep_checkpoints_num, "i")
-
         checkpoints = [
             Checkpoint(Checkpoint.DISK, {i}, self.mock_result(i))
             for i in range(3)
         ]
-        checkpoints += [
-            Checkpoint(Checkpoint.DISK, {i}, self.mock_result(i))
-            for i in range(6, 3, -1)
-        ]
 
         with patch("shutil.rmtree") as rmtree_mock, patch("os.path"):
-
             for j in range(3):
                 checkpoint_manager.on_checkpoint(checkpoints[j])
                 expected_deletes = 0 if j != 2 else 1
@@ -43,23 +37,32 @@ class CheckpointManagerTest(unittest.TestCase):
                 self.assertEqual(checkpoint_manager.newest_checkpoint,
                                  checkpoints[j])
 
-            best_checkpoints = checkpoint_manager.best_checkpoints()
-            self.assertEqual(len(best_checkpoints), keep_checkpoints_num)
-            self.assertIn(checkpoints[1], best_checkpoints)
-            self.assertIn(checkpoints[2], best_checkpoints)
+        best_checkpoints = checkpoint_manager.best_checkpoints()
+        self.assertEqual(len(best_checkpoints), keep_checkpoints_num)
+        self.assertIn(checkpoints[1], best_checkpoints)
+        self.assertIn(checkpoints[2], best_checkpoints)
+
+    def testOnCheckpointUnordered(self):
+        """Tests priorities that aren't inserted in ascending order."""
+        keep_checkpoints_num = 2
+        checkpoint_manager = CheckpointManager(keep_checkpoints_num, "i")
+        checkpoints = [
+            Checkpoint(Checkpoint.DISK, {i}, self.mock_result(i))
+            for i in range(3, -1, -1)
+        ]
 
         with patch("shutil.rmtree") as rmtree_mock, patch("os.path"):
-            for j in range(3, len(checkpoints)):
+            for j in range(0, len(checkpoints)):
                 checkpoint_manager.on_checkpoint(checkpoints[j])
-                expected_deletes = 0 if j == 3 else 1
+                expected_deletes = 0 if j != 3 else 1
                 self.assertEqual(rmtree_mock.call_count, expected_deletes)
                 self.assertEqual(checkpoint_manager.newest_checkpoint,
                                  checkpoints[j])
 
-            best_checkpoints = checkpoint_manager.best_checkpoints()
-            self.assertEqual(len(best_checkpoints), keep_checkpoints_num)
-            self.assertIn(checkpoints[3], best_checkpoints)
-            self.assertIn(checkpoints[4], best_checkpoints)
+        best_checkpoints = checkpoint_manager.best_checkpoints()
+        self.assertEqual(len(best_checkpoints), keep_checkpoints_num)
+        self.assertIn(checkpoints[0], best_checkpoints)
+        self.assertIn(checkpoints[1], best_checkpoints)
 
     def testBestCheckpoints(self):
         keep_checkpoints_num = 4
