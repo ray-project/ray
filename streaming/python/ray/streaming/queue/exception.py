29f8682134b314690fe81c17da09a17a654e64ec
chaokunyang <shawn.ck.yang@gmail.com>
2019-11-18 14:16:20 +0800

    move queue exception to queue_interface.py

--- streaming/python/ray/streaming/queue/exception.py
+++
@@ -1,9 +0,0 @@
-class QueueInitException(Exception):
-    def __init__(self, msg, abnormal_queues):
-        self.abnormal_queues = abnormal_queues
-        self.msg = msg
-
-
-class QueueInterruptException(Exception):
-    def __init__(self, msg=None):
-        self.msg = msg
--- streaming/python/ray/streaming/queue/queue_interface.py
+++ streaming/python/ray/streaming/queue/queue_interface.py
@@ -192,3 +192,14 @@ class QueueConsumer:
     @abstractmethod
     def close(self):
         pass
+
+
+class QueueInitException(Exception):
+    def __init__(self, msg, abnormal_queues):
+        self.abnormal_queues = abnormal_queues
+        self.msg = msg
+
+
+class QueueInterruptException(Exception):
+    def __init__(self, msg=None):
+        self.msg = msg
