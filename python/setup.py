34fb8c290581bd50bb2ddfb000184c433dfa3a69
Mehrdad <noreply@github.com>
2019-11-18 22:18:48 -0800

    Get rid of zip_extract_members and use tarball for pickle5-backport instead

--- python/setup.py
+++ python/setup.py
@@ -10,18 +10,21 @@ import re
 import shutil
 import subprocess
 import sys
+import tarfile
 import tempfile
 import zipfile
 
 from itertools import chain
 
+import setuptools.command.build_ext as _build_ext
+from setuptools import setup, find_packages, Distribution
+
 if sys.version_info[0] >= 3:
+    import urllib.parse as urllib_parse
     import urllib.request as urllib_request
 else:
+    import urllib2 as urllib_parse
     import urllib2 as urllib_request
-
-from setuptools import setup, find_packages, Distribution
-import setuptools.command.build_ext as _build_ext
 
 # Ideally, we could include these files by putting them in a
 # MANIFEST.in or using the package_data argument to setup, but the
@@ -31,9 +34,14 @@ import setuptools.command.build_ext as _build_ext
 
 # NOTE: The lists below must be kept in sync with ray/BUILD.bazel.
 
-pyarrow_url = "https://s3-us-west-2.amazonaws.com/arrow-wheels/3a11193d9530fe8ec7fdb98057f853b708f6f6ae/index.html"
+pyarrow_url = (
+    "https://s3-us-west-2.amazonaws.com/"
+    "arrow-wheels/3a11193d9530fe8ec7fdb98057f853b708f6f6ae/index.html")
 pyarrow_version = "0.14.0.RAY"
-pickle5_backport_commit = "5186f9ca4ce55ae530027db196da51e08208a16b"
+pickle5_url = (
+    "https://files.pythonhosted.org/packages/"
+    "cd/5a/cbdf36134804809d55ffd4c248343bd36680a92b6425885a3fd204d32f7b"
+    "/pickle5-0.0.9.tar.gz")
 
 ray_files = [
     "ray/core/src/ray/thirdparty/redis/src/redis-server",
@@ -101,12 +109,18 @@ extras["all"] = list(set(chain.from_iterable(extras.values())))
 
 
 def is_native_windows_or_msys():
-    """Check to see if we are running on native Windows, but NOT WSL (which is seen as Linux)."""
-    return sys.platform == 'msys' or sys.platform == 'win32'
+    """Check to see if we are running on native Windows,
+    but NOT WSL (which is seen as Linux)."""
+    return sys.platform == "msys" or sys.platform == "win32"
+
 
 def is_invalid_windows_platform():
-    # 'GCC' check is how you detect MinGW: https://github.com/msys2/MINGW-packages/blob/abd06ca92d876b9db05dd65f27d71c4ebe2673a9/mingw-w64-python2/0410-MINGW-build-extensions-with-GCC.patch#L53
-    return sys.platform == 'msys' or sys.platform == 'win32' and sys.version_string and 'GCC' in sys.version_string
+    # 'GCC' check is how you detect MinGW:
+    # https://github.com/msys2/MINGW-packages/blob/abd06ca92d876b9db05dd65f27d71c4ebe2673a9/mingw-w64-python2/0410-MINGW-build-extensions-with-GCC.patch#L53
+    platform = sys.platform
+    ver = sys.version
+    return platform == "msys" or (platform == "win32" and ver and "GCC" in ver)
+
 
 def makedirs(path, exist_ok):
     """Python 2-compatible version of makedirs().
@@ -120,44 +134,29 @@ def makedirs(path, exist_ok):
         try:
             os.makedirs(path)
         except OSError as ex:
-            if not (exist_ok and ex.errno == errno.EEXIST and os.path.isdir(path)):
+            ok = exist_ok and ex.errno == errno.EEXIST and os.path.isdir(path)
+            if not ok:
                 raise
 
-def zip_extract_members(zip_file, desired_members, target_dir):
-    # Recursively extracts archive members onto the target directory:
-    # - Files are simply extracted as files inside the target directory
-    # - Directories have their _contents_ extracted into the target directory
-    desired_members = list(map(lambda m: m if isinstance(m, zipfile.ZipInfo) else zip_file.getinfo(m), desired_members))
-    members_to_targets = list(map(lambda m: (m, os.path.basename(m.filename)), desired_members))
-    desired_dirs = list(filter(lambda m: m.filename.endswith("/"), desired_members))
-    for member in zip_file.infolist():
-        for dir in desired_dirs:
-            if member.filename != dir.filename and member.filename.startswith(dir.filename):
-                members_to_targets.append((member, member.filename[len(dir.filename):]))
-    for desired_member, relative_target_path in members_to_targets:
-        member_target_path = os.path.join(target_dir, relative_target_path)
-        if desired_member.filename.endswith("/"):
-            makedirs(member_target_path, exist_ok=True)
-        else:
-            member_target_parent = os.path.dirname(member_target_path)
-            if member_target_parent:
-                makedirs(member_target_parent, exist_ok=True)
-            with open(member_target_path, "wb") as outfile:
-                with zip_file.open(desired_member, "r") as infile:
-                    shutil.copyfileobj(infile, outfile)
 
 def download(url):
     return urllib_request.urlopen(url).read()
 
-def bazel_build(root_dir, build_ext=None):  # Keep this as a global function so external scripts can call it (makes life easier during development)
+
+# Keep this as a global function so external scripts can call it
+# (makes life easier during development)
+def bazel_build(root_dir, build_ext=None):
     if is_native_windows_or_msys():
-        BAZEL_SH = os.getenv('BAZEL_SH')
-        SYSTEMROOT = os.getenv('SystemRoot')
-        if (not BAZEL_SH) and SYSTEMROOT and os.path.isfile(os.path.join(SYSTEMROOT, 'System32', 'bash.exe')):
-            msg = " ".join([
-                "You appear to have Bash from WSL, which Bazel is not compatible with.",
-                "To avoid potential problems, please explicitly set the {envvar!r} environment variable for Bazel."
-            ]).format(envvar='BAZEL_SH')
+        BAZEL_SH = os.getenv("BAZEL_SH")
+        SYSTEMROOT = os.getenv("SystemRoot")
+        wsl_bash = os.path.join(SYSTEMROOT, "System32", "bash.exe")
+        if (not BAZEL_SH) and SYSTEMROOT and os.path.isfile(wsl_bash):
+            msg = (
+                "You appear to have Bash from WSL,"
+                " which Bazel is not compatible with."
+                "To avoid potential problems,"
+                " please explicitly set the {envvar!r}"
+                " environment variable for Bazel.").format(envvar="BAZEL_SH")
             raise ValueError(msg)
 
     if not os.path.isabs(root_dir):
@@ -169,30 +168,42 @@ def bazel_build(root_dir, build_ext=None):  # Keep this as a global function so 
         # that certain flags will not be passed along such as --user or sudo.
         # TODO(rkn): Fix this.
         subprocess.check_call([
-            sys.executable, "-m", "pip", "install", "-q", "pyarrow==" + pyarrow_version,
-            "--find-links", pyarrow_url,
-            "--target", os.path.join(root_dir, "python", "ray", "pyarrow_files")
+            sys.executable, "-m", "pip", "install", "-q",
+            "pyarrow==" + pyarrow_version, "--find-links", pyarrow_url,
+            "--target",
+            os.path.join(root_dir, "python", "ray", "pyarrow_files")
         ])
         if (3, 6) <= sys.version_info[:2] <= (3, 7):
-            pickle5_project = "pickle5-backport"
-            pickle5_backport_url = "https://github.com/pitrou/%s/archive/%s.zip" % (pickle5_project, pickle5_backport_commit)
-            pickle5_dir = os.path.join(root_dir, "python", "ray", "pickle5_files")
+            subdir = os.path.join("python", "ray", "pickle5_files")
+            pickle5_dir = os.path.join(root_dir, subdir)
             work_dir = tempfile.mkdtemp()
             try:
-                zf = zipfile.ZipFile(io.BytesIO(download(pickle5_backport_url)), "r")
-                try: zip_extract_members(zf, [pickle5_project + "-" + pickle5_backport_commit + "/"], work_dir + os.sep)
-                finally: zf.close()
-                subprocess.check_call([sys.executable, "setup.py", "bdist_wheel"], cwd=work_dir)
-                for wheel in glob.glob(os.path.join(work_dir, "dist", "*.whl")):
+                urlpath = urllib_parse.urlparse(pickle5_url).path
+                pickle5_name = os.path.basename(urllib_parse.unquote(urlpath))
+                tgz = re.sub("\\.tar\\.gz$", ".tgz", pickle5_name, flags=re.I)
+                pickle5_dirname = os.path.splitext(tgz)[0]
+                tf = tarfile.open(None, "r", io.BytesIO(download(pickle5_url)))
+                try:
+                    tf.extractall(work_dir)
+                finally:
+                    tf.close()
+                subprocess.check_call(
+                    [sys.executable, "setup.py", "bdist_wheel"],
+                    cwd=os.path.join(work_dir, pickle5_dirname))
+                wheel_glob = os.path.join(work_dir, "dist", "*.whl")
+                for wheel in glob.glob(wheel_glob):
                     wzf = zipfile.ZipFile(wheel, "r")
-                    try: wzf.extractall(pickle5_dir)
-                    finally: wzf.close()
+                    try:
+                        wzf.extractall(pickle5_dir)
+                    finally:
+                        wzf.close()
             finally:
                 shutil.rmtree(work_dir)
 
     bazel_env = os.environ.copy()
-    bazel_env["PYTHON2_BIN_PATH"] = sys.executable  # apparently both must be set to the same executable
-    bazel_env["PYTHON3_BIN_PATH"] = sys.executable  # apparently both must be set to the same executable
+    # Apparently both Python 2 and 3 should be set to the same executable?
+    bazel_env["PYTHON2_BIN_PATH"] = sys.executable
+    bazel_env["PYTHON3_BIN_PATH"] = sys.executable
     bazel_build_cmd = ["bazel", "build", "--verbose_failures"]
     bazel_build_cmd.append("//:ray_pkg")
     if os.getenv("RAY_INSTALL_JAVA") == "1":
@@ -200,10 +211,13 @@ def bazel_build(root_dir, build_ext=None):  # Keep this as a global function so 
         bazel_build_cmd.append("//java:all")
     subprocess.check_call(bazel_build_cmd, env=bazel_env)
 
+
 class build_ext(_build_ext.build_ext):
     def run(self):
         if is_invalid_windows_platform():
-            raise OSError("Please use official native CPython on Windows, not Cygwin/MSYS/MSYS2/MinGW/etc.")
+            msg = ("Please use official native CPython on Windows,"
+                   " not Cygwin/MSYS/MSYS2/MinGW/etc.")
+            raise OSError(msg)
 
         root_dir = os.path.dirname(os.getcwd())
         bazel_build(root_dir, self)
@@ -287,15 +301,19 @@ requires = [
     "protobuf >= 3.8.0",
 ]
 
+
 def main():
     return setup(
         name="ray",
         version=find_version("ray", "__init__.py"),
         author="Ray Team",
         author_email="ray-dev@googlegroups.com",
-        description=("A system for parallel and distributed Python that unifies "
-                     "the ML ecosystem."),
-        long_description=open(os.path.join(os.path.dirname(__file__), os.path.pardir, "README.rst"), "r").read(),
+        description=("A system for parallel and distributed Python that"
+                     "unifies the ML ecosystem."),
+        long_description=open(
+            os.path.join(
+                os.path.dirname(__file__), os.path.pardir, "README.rst"),
+            "r").read(),
         url="https://github.com/ray-project/ray",
         keywords=("ray distributed parallel machine-learning "
                   "reinforcement-learning deep-learning python"),
@@ -309,12 +327,14 @@ def main():
         entry_points={
             "console_scripts": [
                 "ray=ray.scripts.scripts:main",
-                "rllib=ray.rllib.scripts:cli [rllib]", "tune=ray.tune.scripts:cli"
+                "rllib=ray.rllib.scripts:cli [rllib]",
+                "tune=ray.tune.scripts:cli"
             ]
         },
         include_package_data=True,
         zip_safe=False,
         license="Apache 2.0")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
