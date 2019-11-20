220dfb0ff24b5cfbeb86f68cf6fa9c12d2e69dc2
chaokunyang <shawn.ck.yang@gmail.com>
2019-11-20 20:22:01 +0800

    build python conf

--- streaming/python/ray/streaming/config.py
+++ streaming/python/ray/streaming/config.py
@@ -4,28 +4,20 @@ from __future__ import print_function
 
 
 class Config:
+    STREAMING_JOB_NAME = "streaming.job.name"
+    STREAMING_OP_NAME = "streaming.op_name"
+    TASK_JOB_ID = "streaming.task_job_id"
+    STREAMING_WORKER_NAME = "streaming.worker_name"
     # queue
     QUEUE_TYPE = "queue_type"
     MEMORY_QUEUE = "memory_queue"
     NATIVE_QUEUE = "streaming_queue"
     QUEUE_SIZE = "queue_size"
     QUEUE_SIZE_DEFAULT = 10 ** 8
-    TASK_JOB_ID = "TaskJobID"
+    IS_RECREATE = "streaming.is_recreate"
+    # return from StreamingReader.getBundle if only empty message read in this interval.
+    TIMER_INTERVAL_MS = "timer_interval_ms"
 
-    TIMER_INTERVAL_MS = "timerIntervalMs"
-    IS_RECREATE = "isRecreate"
-    STREAMING_PERSISTENCE_PATH = "StreamingPersistencePath"
-    STREAMING_JOB_NAME = "StreamingJobName"
-    STREAMING_OP_NAME = "StreamingOpName"
-    STREAMING_WORKER_NAME = "StreamingWorkerName"
-    STREAMING_LOG_PATH = "StreamingLogPath"
-
-    # uint config
-    STREAMING_LOG_LEVEL = "StreamingLogLevel"
-    STREAMING_RING_BUFFER_CAPACITY = "StreamingRingBufferCapacity"
-    STREAMING_EMPTY_MESSAGE_TIME_INTERVAL = "StreamingEmptyMessageTimeInterval"
-    STREAMING_FULL_QUEUE_TIME_INTERVAL = "StreamingFullQueueTimeInterval"
-    STREAMING_WRITER_CONSUMED_STEP = "StreamingWriterConsumedStep"
-    STREAMING_READER_CONSUMED_STEP = "StreamingReaderConsumedStep"
-    STREAMING_READER_CONSUMED_STEP_UPDATER = "StreamingReaderConsumedStepUpdater"
-    STREAMING_FLOW_CONTROL = "StreamingFlowControl"
+    STREAMING_RING_BUFFER_CAPACITY = "streaming.ring_buffer_capacity"
+    # write an empty message if there is no data to be written in this interval.
+    STREAMING_EMPTY_MESSAGE_INTERVAL = "streaming.empty_message_interval"
