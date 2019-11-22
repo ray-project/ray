a94f7e045f33bc8dd7738fc188ba71e9850065e1
chaokunyang <shawn.ck.yang@gmail.com>
2019-11-22 17:37:21 +0800

    add comments for queue_interface

--- streaming/python/ray/streaming/runtime/queue/queue_interface.py
+++ streaming/python/ray/streaming/runtime/queue/queue_interface.py
@@ -9,7 +9,14 @@ import ray.streaming.runtime.queue.queue_utils as queue_utils
 
 
 class QueueID:
+    """
+    QueueID is used to identify a queue which is the transfer channel between
+     a upstream operator and downstream operator.
+    """
     def __init__(self, str_qid: str):
+        """
+        :param str_qid: string representation of queue id
+        """
         self.str_qid = str_qid
         self.object_qid = ray.ObjectID(queue_utils.qid_str_to_bytes(str_qid))
 
@@ -30,9 +37,9 @@ class QueueID:
 
 class QueueItem:
     """
-    queue item interface
+    QueueItem interface lists all methods that a QueueItem subclass need to implement.
+    QueueItem represents an item pulled from queue. It maybe a data message or control message.
     """
-
     __metaclass__ = ABCMeta
 
     def __init__(self):
@@ -40,22 +47,31 @@ class QueueItem:
 
     @abstractmethod
     def body(self):
+        """
+        Queue item data
+        """
         pass
 
     @abstractmethod
     def timestamp(self):
+        """
+        Timestamp when item is written by upstream StreamingWriter
+        :return:
+        """
         pass
 
 
 class QueueMessage(QueueItem):
     """
-    queue message interface
+    Queue data message interface
     """
-
     __metaclass__ = ABCMeta
 
     @abstractmethod
     def queue_id(self):
+        """
+        :return: id of queue where data is coming from
+        """
         pass
 
 
@@ -63,7 +79,6 @@ class QueueLink:
     """
     queue link interface
     """
-
     __metaclass__ = ABCMeta
 
     @abstractmethod
@@ -115,7 +130,7 @@ class QueueLink:
 
 class QueueProducer:
     """
-    queue producer interface
+    Queue producer interface
     """
 
     __metaclass__ = ABCMeta
@@ -123,16 +138,17 @@ class QueueProducer:
     @abstractmethod
     def produce(self, queue_id: QueueID, item):
         """
-        Produce msg into the special queue
-        :param queue_id:  the specified queue id
-        :param item:  the message
+        Produce msg into the downstream queue
+        :param queue_id:  the downstream queue id
+        :param item:  the data message
+
         """
         pass
 
     @abstractmethod
     def stop(self):
         """
-        stop produce to avoid blocking
+        stop producer to avoid blocking
         :return: None
         """
         pass
@@ -147,21 +163,32 @@ class QueueProducer:
 
 class QueueConsumer:
     """
-    queue consumer interface
+    Queue consumer interface
     """
 
     __metaclass__ = ABCMeta
 
     @abstractmethod
     def pull(self, timeout_millis):
+        """
+        pull data from queue
+        :param timeout_millis: timeout millis when there is no data in queue for this duration
+        :return: queue item
+        """
         pass
 
     @abstractmethod
     def stop(self):
+        """
+        stop queue consumer
+        """
         pass
 
     @abstractmethod
     def close(self):
+        """
+        close queue consumer
+        """
         pass
 
 
