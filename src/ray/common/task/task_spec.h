162c03bf405519e2c9f81216644ae848f4bd0d78
Edward Oakes <ed.nmi.oakes@gmail.com>
2019-11-26 20:56:05 -0600

    Use one queue per resource shape in direct task transport (#6277)

--- src/ray/common/task/task_spec.h
+++ src/ray/common/task/task_spec.h
@@ -113,6 +113,12 @@ class TaskSpecification : public MessageWrapper<rpc::TaskSpec> {
   /// \return The resources that are required to place a task on a node.
   const ResourceSet &GetRequiredPlacementResources() const;
 
+  /// Return the dependencies of this task. This is recomputed each time, so it can
+  /// be used if the task spec is mutated.
+  ///
+  /// \return The recomputed dependencies for the task.
+  std::vector<ObjectID> GetDependencies() const;
+
   bool IsDriverTask() const;
 
   Language GetLanguage() const;
