c261f5a22c242f487fa625f315c1fd50c0f407c3
gehring <clement.gehring@gmail.com>
2019-11-26 20:53:40 -0500

    Removed unnecessary print statements

--- rllib/examples/dmlab_watermaze.py
+++ rllib/examples/dmlab_watermaze.py
@@ -7,16 +7,12 @@ from ray.rllib.agents import ppo
 
 class Watermaze(env.DMEnv):
 
-    LOOK_LR = "LOOK_LEFT_RIGHT_PIXELS_PER_FRAME"
-
     def __init__(self, env_config):
         lab = dmenv_module.Lab(
             "contributed/dmlab30/rooms_watermaze",
             ["RGBD"],
             config=env_config,
         )
-        print(lab.observation_spec())
-        print(lab.action_spec())
         super(Watermaze, self).__init__(lab)
 
 
