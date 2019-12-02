65cfff23d14885c38ba0ae0346dcad00fd1ca473
Mitchell Stern <stern.mitchell@gmail.com>
2019-12-02 10:27:54 -0500

    Add comments about Ray worker process titles

--- python/ray/dashboard/client/src/pages/dashboard/features/Workers.tsx
+++ python/ray/dashboard/client/src/pages/dashboard/features/Workers.tsx
@@ -5,6 +5,9 @@ export const NodeWorkers: NodeFeatureComponent = ({ node }) => (
   <React.Fragment>{node.workers.length}</React.Fragment>
 );
 
+// Ray worker process titles have one of the following forms: `ray::IDLE`,
+// `ray::function()`, `ray::Class`, or `ray::Class.method()`. We extract the
+// second portion here for display in the "Workers" column.
 export const WorkerWorkers: WorkerFeatureComponent = ({ worker }) => (
   <React.Fragment>{worker.cmdline[0].split("::", 2)[1]}</React.Fragment>
 );
