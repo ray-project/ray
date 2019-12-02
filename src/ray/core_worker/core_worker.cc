9a407f8a11ddd8a60d1e94b1b60197fbfc32f56b
Stephanie Wang <swang@cs.berkeley.edu>
2019-12-02 13:32:06 -0800

    Reply GetObjectStatus once the task completes

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -966,16 +966,24 @@ void CoreWorker::HandleGetObjectStatus(const rpc::GetObjectStatusRequest &reques
     // We may have owned this object in the past, but we are now executing some
     // other task or actor.
     reply->set_status(rpc::GetObjectStatusReply::WRONG_OWNER);
+    send_reply_callback(Status::OK(), nullptr, nullptr);
   } else {
+    // We own the task. Reply back to the borrower once the object has been
+    // created.
+    // TODO: We could probably just send the object value if it is small
+    // enough and we have it local.
+    reply->set_status(rpc::GetObjectStatusReply::CREATED);
     if (task_manager_->IsTaskPending(object_id.TaskId())) {
-      reply->set_status(rpc::GetObjectStatusReply::PENDING);
+      // The task is pending. Send the reply once the task finishes.
+      memory_store_->GetAsync(object_id,
+                              [send_reply_callback](std::shared_ptr<RayObject> obj) {
+                                send_reply_callback(Status::OK(), nullptr, nullptr);
+                              });
     } else {
-      // TODO: We could probably just send the object value if it is small
-      // enough and we have it local.
-      reply->set_status(rpc::GetObjectStatusReply::CREATED);
+      // The task is done. Send the reply immediately.
+      send_reply_callback(Status::OK(), nullptr, nullptr);
     }
   }
-  send_reply_callback(Status::OK(), nullptr, nullptr);
 }
 
 void CoreWorker::YieldCurrentFiber(FiberEvent &event) {
