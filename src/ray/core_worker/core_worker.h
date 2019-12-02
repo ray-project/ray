7730fc368d87daed13a1285171853d9db7fec7d0
Kai Yang <kfstorm@outlook.com>
2019-12-02 17:45:17 +0800

    fix actor handle multi-threading

--- src/ray/core_worker/core_worker.h
+++ src/ray/core_worker/core_worker.h
@@ -556,6 +556,8 @@ class CoreWorker {
   // Interface to submit non-actor tasks directly to leased workers.
   std::unique_ptr<CoreWorkerDirectTaskSubmitter> direct_task_submitter_;
 
+  mutable std::mutex actor_handles_mutex_;
+
   /// Map from actor ID to a handle to that actor.
   absl::flat_hash_map<ActorID, std::unique_ptr<ActorHandle>> actor_handles_;
 
