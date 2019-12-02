f4b488af146c2d8990d49bff6998c80b835f9606
Stephanie Wang <swang@cs.berkeley.edu>
2019-12-02 10:25:05 -0800

    [direct task] Fix bug that starts duplicate connections from the worker to the local raylet (#6307)
    * Fix bug and add unit test
    
    * rename

--- src/ray/core_worker/transport/direct_task_transport.h
+++ src/ray/core_worker/transport/direct_task_transport.h
@@ -34,12 +34,13 @@ class CoreWorkerDirectTaskSubmitter {
                                 LeaseClientFactoryFn lease_client_factory,
                                 std::shared_ptr<CoreWorkerMemoryStore> store,
                                 std::shared_ptr<TaskFinisherInterface> task_finisher,
-                                int64_t lease_timeout_ms)
+                                ClientID local_raylet_id, int64_t lease_timeout_ms)
       : local_lease_client_(lease_client),
         client_factory_(client_factory),
         lease_client_factory_(lease_client_factory),
         resolver_(store),
         task_finisher_(task_finisher),
+        local_raylet_id_(local_raylet_id),
         lease_timeout_ms_(lease_timeout_ms) {}
 
   /// Schedule a task for direct submission to a worker.
@@ -101,6 +102,10 @@ class CoreWorkerDirectTaskSubmitter {
   /// The timeout for worker leases; after this duration, workers will be returned
   /// to the raylet.
   int64_t lease_timeout_ms_;
+
+  /// The local raylet ID. Used to make sure that we use the local lease client
+  /// if a remote raylet tells us to spill the task back to the local raylet.
+  const ClientID local_raylet_id_;
 
   // Protects task submission state below.
   absl::Mutex mu_;
