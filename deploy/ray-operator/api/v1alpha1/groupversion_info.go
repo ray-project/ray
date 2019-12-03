b7277bb3073392c76080ecf15765744f4aee15ae
君熠 <qiaoshuai.qs@antfin.com>
2019-12-03 22:05:44 +0800

    Ray-Operator first PR: rename version from v1 to v1alpha1

--- deploy/ray-operator/api/v1alpha1/groupversion_info.go
+++ deploy/ray-operator/api/v1alpha1/groupversion_info.go
@@ -1,7 +1,7 @@
-// Package v1 contains API Schema definitions for the ray v1 API group
+// Package v1alpha1 contains API Schema definitions for the ray v1alpha1 API group
 // +kubebuilder:object:generate=true
 // +groupName=ray.io
-package v1
+package v1alpha1
 
 import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -10,7 +10,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "ray.io", Version: "v1"}
+	GroupVersion = schema.GroupVersion{Group: "ray.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
--- deploy/ray-operator/api/v1alpha1/raycluster_types.go
+++ deploy/ray-operator/api/v1alpha1/raycluster_types.go
@@ -1,4 +1,4 @@
-package v1
+package v1alpha1
 
 import (
 	v1 "k8s.io/api/core/v1"
