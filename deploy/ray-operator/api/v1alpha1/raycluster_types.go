41c5c503ff78d612c308fd94eef6404c39e0d227
君熠 <qiaoshuai.qs@antfin.com>
2019-12-05 10:43:25 +0800

    Ray-Operator first PR: add More Information about proposal

--- deploy/ray-operator/api/v1alpha1/raycluster_types.go
+++ deploy/ray-operator/api/v1alpha1/raycluster_types.go
@@ -12,11 +12,28 @@ import (
 type RayClusterSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	// ClusterName is unique identifier for RayCluster in one namespace.
 	ClusterName     string          `json:"clusterName"`
+	// Docker image.
 	Images          RayClusterImage `json:"images"`
+	// Image pull policy.
+	// One of Always, Never, IfNotPresent.
+	// Defaults to Always if :latest tag is specified, or IfNotPresent otherwise.
+	// Cannot be updated.
 	ImagePullPolicy string          `json:"imagePullPolicy,omitempty"`
+	// Specification of the desired behavior of pod group
 	Extensions      []Extension     `json:"extensions,omitempty"`
 }
+
+// RayCluster pod type
+type ReplicaType string
+
+const (
+	// ReplicaTypeHead means that this pod will be ray cluster head
+	ReplicaTypeHead ReplicaType = "head"
+	// ReplicaTypeWorker means that this pod will be ray cluster worker
+	ReplicaTypeWorker ReplicaType = "worker"
+)
 
 // RayClusterStatus defines the observed state of RayCluster
 type RayClusterStatus struct {
@@ -31,9 +48,11 @@ type RayClusterStatus struct {
 
 // RayCluster is the Schema for the RayClusters API
 type RayCluster struct {
+	// Standard object metadata.
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
+	// Specification of the desired behavior of the RayCluster.
 	Spec   RayClusterSpec   `json:"spec,omitempty"`
 	Status RayClusterStatus `json:"status,omitempty"`
 }
@@ -52,63 +71,67 @@ func init() {
 }
 
 type RayClusterImage struct {
+	// Default docker image
 	DefaultImage string `json:"defaultImage,omitempty"`
 }
 
 type Extension struct {
-	// the replicas of this pod group
-	Replicas int32 `json:"replicas"`
+	// Number of desired pods in this pod group. This is a pointer to distinguish between explicit
+	// zero and not specified. Defaults to 1.
+	Replicas *int32 `json:"replicas"`
 
-	// worker/head
-	Type string `json:"type,omitempty"`
+	// The pod type in this group worker/head.
+	Type ReplicaType `json:"type,omitempty"`
 
-	// pod image
+	// Pod docker image
 	Image string `json:"image,omitempty"`
 
-	// logical groupName for worker in same group, it's used for heterogeneous feature to distinguish different groups
-	// groupName is the unique identifier for the pods with the same configuration in one Ray Cluster
+	// Logical groupName for worker in same group, it's used for heterogeneous feature to distinguish different groups.
+	// GroupName is the unique identifier for the pods with the same configuration in one Ray Cluster
 	GroupName string `json:"groupName,omitempty"`
 
-	// command to start ray
+	// Command to start ray
 	Command string `json:"command,omitempty"`
 
-	// labels for pod, raycluster.component and rayclusters.ray.io/component-name are default labels, do not overwrite them.
+	// Labels for pod, raycluster.component and rayclusters.ray.io/component-name are default labels, do not overwrite them.
 	// Labels are intended to be used to specify identifying attributes of objects that are meaningful and relevant to users,
 	// but do not directly imply semantics to the core system. Labels can be used to organize and to select subsets of objects.
 	Labels map[string]string `json:"labels,omitempty"`
 
 	// NodeSelector specifies a map of key-value pairs. For the pod to be eligible
 	// to run on a node, the node must have each of the indicated key-value pairs as
-	// labels.Optional
+	// labels. Optional.
 	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
 
-	// the affinity for pod.Optional
-	// it allows you to constrain which nodes your pod is eligible to be scheduled on, based on labels on the node.
+	// Affinity is a group of affinity scheduling rules. Optional.
+	// It allows you to constrain which nodes your pod is eligible to be scheduled on, based on labels on the node.
 	Affinity *v1.Affinity `json:"affinity,omitempty"`
 
-	// the resource requirements for this group pod.
+	// ResourceRequirements describes the compute resource requirements.
 	Resources v1.ResourceRequirements `json:"resources,omitempty"`
 
-	// Tolerations specifies the pod's tolerations.Optional
+	// The pod this Toleration is attached to tolerates any taint that matches
+	// the triple <key,value,effect> using the matching operator <operator>. Optional.
 	// Tolerations are applied to pods, and allow (but do not require) the pods to schedule onto nodes with matching taints.
 	Tolerations []v1.Toleration `json:"tolerations,omitempty"`
 
-	// List of environment variables to set in the container.Optional
-	// define environment variables for a container. This can be helpful in defining variables before setting up the container.
+	// EnvVar represents an environment variable present in a Container. Optional.
+	// Define environment variables for a container. This can be helpful in defining variables before setting up the container.
 	ContainerEnv []v1.EnvVar `json:"containerEnv,omitempty"`
 
-	// Head service suffix. So head can be accessed by domain name: {namespace}.svc , follows Kubernetes standard
+	// Head service suffix. So head can be accessed by domain name: {namespace}.svc , follows Kubernetes standard.
 	HeadServiceSuffix string `json:"headServiceSuffix,omitempty"`
 
-	// Annotations for pod.Optional
-	// usage refers to https://kubernetes.io/docs/concepts/overview/working-with-objects/annotations/
+	// Annotations is an unstructured key value map stored with a resource that may be
+	// set by external tools to store and retrieve arbitrary metadata. They are not
+	// queryable and should be preserved when modifying objects. Optional.
+	// Usage refers to https://kubernetes.io/docs/concepts/overview/working-with-objects/annotations/
 	Annotations map[string]string `json:"annotations,omitempty"`
 
-	// Volume for the pod group.Optional
+	// Volume represents a named volume in a pod that may be accessed by any container in the pod. Optional.
 	// At its core, a volume is just a directory, possibly with some data in it, which is accessible to the Containers in a Pod.
 	Volumes []v1.Volume `json:"volumes,omitempty"`
 
-	// VolumeMount for the pod group.Optional
-	// mount the volume to a path via name
+	// VolumeMount describes a mounting of a Volume within a container. Optional.
 	VolumeMounts []v1.VolumeMount `json:"volumeMounts,omitempty"`
 }
