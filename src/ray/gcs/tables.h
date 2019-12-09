fb70b8dbe8980fa96ba5f7211a84b03a1820dadc
micafan <550435771@qq.com>
2019-12-09 20:34:44 +0800

    add comments

--- src/ray/gcs/tables.h
+++ src/ray/gcs/tables.h
@@ -444,7 +444,6 @@ class Set : private Log<ID, Data>,
   Set(const std::vector<std::shared_ptr<RedisContext>> &contexts, RedisGcsClient *client)
       : Log<ID, Data>(contexts, client) {}
 
-  // using Log<ID, Data>::Subscribe;
   using Log<ID, Data>::RequestNotifications;
   using Log<ID, Data>::CancelNotifications;
   using Log<ID, Data>::Lookup;
