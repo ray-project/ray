6c0d6021ba5b43fec0d80700842fb006aa1a929f
akhare39 <alindkhare@gatech.edu>
2019-12-11 17:40:23 -0500

    Fixed linting issues

--- python/ray/experimental/serve/queues.py
+++ python/ray/experimental/serve/queues.py
@@ -87,10 +87,10 @@ class CentralizedQueues:
 
         # backend_name -> worker payload queue
         # using blist sortedlist for deadline awareness
-        # blist is chosen because:  
-        # 1. pop operation should be O(1) (amortized) 
+        # blist is chosen because:
+        # 1. pop operation should be O(1) (amortized)
         #    (helpful even for batched pop)
-        # 2. There should not be significant overhead in 
+        # 2. There should not be significant overhead in
         #    maintaining the sorted list.
         # 3. The blist implementation is fast and uses C extensions.
         self.buffer_queues = defaultdict(sortedlist)
