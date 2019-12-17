6cb34b699eaa704871c8b738ec6070b5c3d4cfbb
Mitchell Stern <mitchellstern@users.noreply.github.com>
2019-12-16 18:22:37 -0800

    Expose extra node info from raylet stats (#6511)

--- python/ray/dashboard/client/src/pages/dashboard/state.ts
+++ python/ray/dashboard/client/src/pages/dashboard/state.ts
@@ -1,11 +1,16 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { NodeInfoResponse, RayConfigResponse } from "../../api";
+import {
+  NodeInfoResponse,
+  RayConfigResponse,
+  RayletInfoResponse
+} from "../../api";
 
 const name = "dashboard";
 
 interface State {
   rayConfig: RayConfigResponse | null;
   nodeInfo: NodeInfoResponse | null;
+  rayletInfo: RayletInfoResponse | null;
   lastUpdatedAt: number | null;
   error: string | null;
 }
@@ -13,6 +18,7 @@ interface State {
 const initialState: State = {
   rayConfig: null,
   nodeInfo: null,
+  rayletInfo: null,
   lastUpdatedAt: null,
   error: null
 };
@@ -24,8 +30,15 @@ const slice = createSlice({
     setRayConfig: (state, action: PayloadAction<RayConfigResponse>) => {
       state.rayConfig = action.payload;
     },
-    setNodeInfo: (state, action: PayloadAction<NodeInfoResponse>) => {
-      state.nodeInfo = action.payload;
+    setNodeInfoAndRayletInfo: (
+      state,
+      action: PayloadAction<{
+        nodeInfo: NodeInfoResponse;
+        rayletInfo: RayletInfoResponse;
+      }>
+    ) => {
+      state.nodeInfo = action.payload.nodeInfo;
+      state.rayletInfo = action.payload.rayletInfo;
       state.lastUpdatedAt = Date.now();
     },
     setError: (state, action: PayloadAction<string | null>) => {
