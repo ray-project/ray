0ca71390991142ee74ac62705f2c47b7e2bda121
灵洵 <fengbin.ffb@antfin.com>
2019-12-18 15:17:46 +0800

    extract status callback function

--- src/ray/gcs/gcs_server/actor_info_handler_impl.cc
+++ src/ray/gcs/gcs_server/actor_info_handler_impl.cc
@@ -9,18 +9,22 @@ void DefaultActorInfoHandler::HandleAsyncGet(const rpc::ActorAsyncGetRequest &re
                                              rpc::SendReplyCallback send_reply_callback) {
   RAY_LOG(DEBUG) << "Begin get actor info, actor id is:" << request.actor_id();
   ActorID actor_id = ActorID::FromBinary(request.actor_id());
-  auto on_done = [reply, send_reply_callback](
+  auto on_done = [request, reply, send_reply_callback](
                      Status status, const boost::optional<ActorTableData> &result) {
-    if (status.ok() && result) {
-      reply->mutable_actor_table_data()->CopyFrom(*result);
+    if (status.ok()) {
+      if (result) {
+        reply->mutable_actor_table_data()->CopyFrom(*result);
+      }
+    } else {
+      RAY_LOG(ERROR) << "Failed to get actor info:" << status.ToString()
+                     << ",actor id is:" << request.actor_id();
     }
     send_reply_callback(status, nullptr, nullptr);
   };
+
   Status status = gcs_client_.Actors().AsyncGet(actor_id, on_done);
   if (!status.ok()) {
-    RAY_LOG(ERROR) << "Failed to get actor info:" << status.ToString()
-                   << ",actor id is:" << request.actor_id();
-    send_reply_callback(status, nullptr, nullptr);
+    on_done(status, boost::optional<ActorTableData>());
   }
   RAY_LOG(DEBUG) << "Finish get actor info, actor id is:" << request.actor_id();
 }
@@ -32,14 +36,20 @@ void DefaultActorInfoHandler::HandleAsyncRegister(
                  << request.actor_table_data().actor_id();
   auto actor_table_data = std::make_shared<ActorTableData>();
   actor_table_data->CopyFrom(request.actor_table_data());
+  auto on_done = [request, send_reply_callback](Status status) {
+    if (!status.ok()) {
+      RAY_LOG(ERROR) << "Failed to register actor info:" << status.ToString()
+                     << ",actor id is:" << request.actor_table_data().actor_id();
+    }
+    send_reply_callback(status, nullptr, nullptr);
+  };
+
   Status status = gcs_client_.Actors().AsyncRegister(
       actor_table_data, [send_reply_callback](Status status) {
         send_reply_callback(status, nullptr, nullptr);
       });
   if (!status.ok()) {
-    RAY_LOG(ERROR) << "Failed to register actor info:" << status.ToString()
-                   << ",actor id is:" << request.actor_table_data().actor_id();
-    send_reply_callback(status, nullptr, nullptr);
+    on_done(status);
   }
   RAY_LOG(DEBUG) << "Finish register actor info, actor id is:"
                  << request.actor_table_data().actor_id();
@@ -52,14 +62,20 @@ void DefaultActorInfoHandler::HandleAsyncUpdate(
   ActorID actor_id = ActorID::FromBinary(request.actor_id());
   auto actor_table_data = std::make_shared<ActorTableData>();
   actor_table_data->CopyFrom(request.actor_table_data());
+  auto on_done = [request, send_reply_callback](Status status) {
+    if (!status.ok()) {
+      RAY_LOG(ERROR) << "Failed to update actor info:" << status.ToString()
+                     << ",actor id is:" << request.actor_id();
+    }
+    send_reply_callback(status, nullptr, nullptr);
+  };
+
   Status status = gcs_client_.Actors().AsyncUpdate(
       actor_id, actor_table_data, [send_reply_callback](Status status) {
         send_reply_callback(status, nullptr, nullptr);
       });
   if (!status.ok()) {
-    RAY_LOG(ERROR) << "Failed to update actor info:" << status.ToString()
-                   << ",actor id is:" << request.actor_id();
-    send_reply_callback(status, nullptr, nullptr);
+    on_done(status);
   }
   RAY_LOG(DEBUG) << "Finish update actor info, actor id is:" << request.actor_id();
 }
--- src/ray/gcs/gcs_server/job_info_handler_impl.cc
+++ src/ray/gcs/gcs_server/job_info_handler_impl.cc
@@ -9,15 +9,22 @@ void DefaultJobInfoHandler::HandleAddJob(const rpc::AddJobRequest &request,
                  << ",driver id is:" << request.data().driver_pid();
   auto job_table_data = std::make_shared<JobTableData>();
   job_table_data->CopyFrom(request.data());
+  auto on_done = [request, reply, send_reply_callback](Status status) {
+    if (!status.ok()) {
+      RAY_LOG(ERROR) << "Failed to add job, job id is:" << request.data().job_id()
+                     << ",driver id is:" << request.data().driver_pid();
+    }
+    reply->set_success(status.ok());
+    send_reply_callback(status, nullptr, nullptr);
+  };
+
   Status status = gcs_client_.Jobs().AsyncAdd(
       job_table_data, [reply, send_reply_callback](Status status) {
         reply->set_success(status.ok());
         send_reply_callback(status, nullptr, nullptr);
       });
   if (!status.ok()) {
-    RAY_LOG(ERROR) << "Failed to add job, job id is:" << request.data().job_id()
-                   << ",driver id is:" << request.data().driver_pid();
-    send_reply_callback(status, nullptr, nullptr);
+    on_done(status);
   }
   RAY_LOG(DEBUG) << "Finish add job, job id is:" << request.data().job_id()
                  << ",driver id is:" << request.data().driver_pid();
@@ -28,14 +35,21 @@ void DefaultJobInfoHandler::HandleMarkJobFinished(
     rpc::SendReplyCallback send_reply_callback) {
   RAY_LOG(DEBUG) << "Begin mark job finished, job id is:" << request.job_id();
   JobID job_id = JobID::FromBinary(request.job_id());
+  auto on_done = [request, reply, send_reply_callback](Status status) {
+    if (!status.ok()) {
+      RAY_LOG(ERROR) << "Failed to mark job finished, job id is:" << request.job_id();
+    }
+    reply->set_success(status.ok());
+    send_reply_callback(status, nullptr, nullptr);
+  };
+
   Status status = gcs_client_.Jobs().AsyncMarkFinished(
       job_id, [reply, send_reply_callback](Status status) {
         reply->set_success(status.ok());
         send_reply_callback(status, nullptr, nullptr);
       });
   if (!status.ok()) {
-    RAY_LOG(ERROR) << "Failed to mark job finished, job id is:" << request.job_id();
-    send_reply_callback(status, nullptr, nullptr);
+    on_done(status);
   }
   RAY_LOG(DEBUG) << "Finish mark job finished, job id is:" << request.job_id();
 }
