513da4fe6bbd472938fdd8ec23944d43b382b4a4
sven1977 <svenmika1977@gmail.com>
2019-12-20 09:01:43 +0100

    Fix PG __init__ file (after torch/tf unify). Add cartpole torch PG test to regression.

--- rllib/agents/pg/__init__.py
+++ rllib/agents/pg/__init__.py
@@ -1,6 +1,9 @@
 from ray.rllib.agents.pg.pg import PGTrainer, DEFAULT_CONFIG
+from ray.rllib.agents.pg.tf_pg_policy import tf_pg_loss, post_process_advantages
+from ray.rllib.agents.pg.torch_pg_policy import torch_pg_loss
 from ray.rllib.utils import renamed_agent
 
+# `Agent`=Legacy: Use PGTrainer instead.
 PGAgent = renamed_agent(PGTrainer)
 
-__all__ = ["PGAgent", "PGTrainer", "DEFAULT_CONFIG"]
+__all__ = ["PGAgent", "PGTrainer", "tf_pg_loss", "torch_pg_loss", "post_process_advantages", "DEFAULT_CONFIG"]
