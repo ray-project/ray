b0b6b56bb75a17476d9da0eadc3e0d2d3aad586a
mehrdadn <mehrdadn@users.noreply.github.com>
2019-12-20 12:32:07 -0800

    Platform shims for Windows (#6548)
    * Platform shims for Windows
    
    * Satisfy linter

--- src/shims/windows/unistd.h
+++ src/shims/windows/unistd.h
@@ -1,8 +1,45 @@
 #ifndef UNISTD_H
 #define UNISTD_H
 
+#include <getopt.h>
+
+#include <io.h>  // open/read/write/close
+#ifndef EXTERN_C
+#ifdef __cplusplus
+#define EXTERN_C extern "C"
+#else
+#define EXTERN_C
+#endif
+#endif
+#ifndef DECLSPEC_IMPORT
+#define DECLSPEC_IMPORT __declspec(dllimport)
+#endif
+#ifndef WINBASEAPI
+#define WINBASEAPI DECLSPEC_IMPORT
+#endif
+#ifndef WINAPI
+#define WINAPI __stdcall
+#endif
+typedef int BOOL;
+typedef void *HANDLE;
+typedef unsigned long DWORD;
+#ifdef _WIN64
+typedef unsigned long long UINT_PTR;
+typedef unsigned long long ULONG_PTR;
+typedef long long ssize_t;
+#else
+typedef unsigned int UINT_PTR;
+typedef unsigned long ULONG_PTR;
+typedef int ssize_t;
+#endif
 typedef int pid_t /* technically unsigned on Windows, but no practical concern */;
 enum { SIGKILL = 9 };
+typedef ULONG_PTR SIZE_T;
+EXTERN_C WINBASEAPI void WINAPI Sleep(DWORD dwMilliseconds);
+
+typedef unsigned int useconds_t;
+int usleep(useconds_t usec);
+unsigned sleep(unsigned seconds);
 
 __declspec(
     deprecated("Killing a process by ID has an inherent race condition on Windows"
