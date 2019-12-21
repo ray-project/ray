cfd94543f9e4ba84064cf4bfc435d62eeb16723f
chenk008 <kongchen28@gmail.com>
2019-12-21 22:12:57 +0800

    add pod watch and set pod's ownerReference

--- deploy/ray-operator/controllers/raycluster_controller.go
+++ deploy/ray-operator/controllers/raycluster_controller.go
@@ -22,6 +22,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
