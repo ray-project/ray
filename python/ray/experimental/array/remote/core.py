50fb26de682f4d6e4bc61d322cd38da151731b7b
mehrdadn <mehrdadn@users.noreply.github.com>
2019-12-22 13:02:34 -0800

    Fix FutureWarning: Using a non-tuple sequence for multidimensional indexing is deprecated; use `arr[tuple(seq)]` instead of `arr[seq]`. In the future this will be interpreted as an array index, `arr[np.array(seq)]`, which will result either in an error or a different result. (#6568)

--- python/ray/experimental/array/remote/core.py
+++ python/ray/experimental/array/remote/core.py
@@ -47,7 +47,8 @@ def hstack(*xs):
 # TODO(rkn): Be consistent about using "index" versus "indices".
 @ray.remote
 def subarray(a, lower_indices, upper_indices):
-    return a[[slice(l, u) for (l, u) in zip(lower_indices, upper_indices)]]
+    idx = tuple(slice(l, u) for (l, u) in zip(lower_indices, upper_indices))
+    return a[idx]
 
 
 @ray.remote
