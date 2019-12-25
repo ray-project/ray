32e5be0a1f56269ac4555ce69f43f208f2aa003f
Zhijun Fu <pingfu.fzj@antfin.com>
2019-12-25 14:49:35 +0800

    add a new test case

--- python/ray/tests/test_component_failures_3.py
+++ python/ray/tests/test_component_failures_3.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import os
 import sys
 import time
+import json
 
 import numpy as np
 import pytest
@@ -12,7 +13,9 @@ import pytest
 import ray
 import ray.ray_constants as ray_constants
 
+RAY_FORCE_DIRECT = ray_constants.direct_call_enabled()
 
+@pytest.mark.skipif(RAY_FORCE_DIRECT, reason="actor reconstruction not supported for actor creation task failure")
 @pytest.mark.parametrize(
     "ray_start_cluster", [{
         "num_cpus": 4,
@@ -62,6 +65,75 @@ def test_actor_creation_node_failure(ray_start_cluster):
         # node must be reconstructed.
         cluster.remove_node(cluster.list_all_nodes()[-1])
 
+# This test covers actor creation node failure scenario with the assumption
+# that the nodes will only fail after the actors are created or reconstructed.
+# NOTE(zhijunfu): This is because right now we only support actor reconstruction
+# after it has been created, and cannot cover worker failure whiling executing
+# actor creatiion task.
+@pytest.mark.skipif(not RAY_FORCE_DIRECT, reason="only test direct actors")
+@pytest.mark.parametrize(
+    "ray_start_cluster", [{
+        "num_cpus": 4,
+        "num_nodes": 3,
+        "do_init": True,
+        "_internal_config": json.dumps({
+            "initial_reconstruction_timeout_milliseconds": 1000,
+            "num_heartbeats_timeout": 100,
+        })
+    }],
+    indirect=True)
+def test_actor_creation_node_failure_after_actor_created(ray_start_cluster):
+    # TODO(swang): Refactor test_raylet_failed, etc to reuse the below code.
+    cluster = ray_start_cluster
+
+    @ray.remote(max_reconstructions=100)
+    class Child(object):
+        def __init__(self, death_probability):
+            self.death_probability = death_probability
+
+        def get_probability(self):
+            return self.death_probability
+
+        def ping(self):
+            # Exit process with some probability.
+            exit_chance = np.random.rand()
+            if exit_chance < self.death_probability:
+                sys.exit(-1)
+
+    num_children = 25
+    # Children actors will die about half the time.
+    death_probability = 0.5
+
+    children = [Child.remote(death_probability) for _ in range(num_children)]
+    while len(cluster.list_all_nodes()) > 1:
+        # Submit some tasks on the actors. About half of the actors will
+        # fail.
+        children_out = [child.ping.remote() for child in children]
+        # Wait a while for all the tasks to complete, so we can know
+        # which actors have died.
+        ready, _ = ray.wait(
+            children_out, num_returns=len(children_out), timeout=5 * 60.0)
+        assert len(ready) == len(children_out)
+
+        # Replace any actors that died.
+        for i, out in enumerate(children_out):
+            try:
+                ray.get(out)
+            except ray.exceptions.RayActorError:
+                children[i] = Child.remote(death_probability)
+
+
+        children_out = [child.get_probability.remote() for child in children]
+        # Wait for new created actors to finish creation before removing a node.
+        # This is needed because right now we don't support reconstructing actors
+        # that died in the process of being created.
+        ready, _ = ray.wait(
+            children_out, num_returns=len(children_out), timeout=5 * 60.0)
+        assert len(ready) == len(children_out)
+
+        # Remove a node. Any actor creation tasks that were forwarded to this
+        # node must be reconstructed.
+        cluster.remove_node(cluster.list_all_nodes()[-1])
 
 @pytest.mark.skipif(
     os.environ.get("RAY_USE_NEW_GCS") == "on",
