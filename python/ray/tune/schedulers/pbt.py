80003957983e89e84dfc5e733bdc377d6fca735c
Richard Liaw <rliaw@berkeley.edu>
2019-12-26 14:26:54 -0800

    fix

--- python/ray/tune/schedulers/pbt.py
+++ python/ray/tune/schedulers/pbt.py
@@ -185,9 +185,8 @@ class PopulationBasedTraining(FIFOScheduler):
                  log_config=True):
         for value in hyperparam_mutations.values():
             if not (isinstance(value, list) or callable(value)):
-                raise TypeError(
-                    "`hyperparam_mutation` values must be either "
-                    "a List or callable.")
+                raise TypeError("`hyperparam_mutation` values must be either "
+                                "a List or callable.")
 
         if not hyperparam_mutations and not custom_explore_fn:
             raise TuneError(
