dd634a7528f41366519ab7dbcb005e5df39fc3b9
Edward Oakes <ed.nmi.oakes@gmail.com>
2019-12-26 11:29:53 -0800

    Use background thread

--- python/ray/experimental/multiprocessing/pool.py
+++ python/ray/experimental/multiprocessing/pool.py
@@ -4,10 +4,67 @@ from __future__ import print_function
 
 from multiprocessing import TimeoutError
 import os
+import time
 import random
+import threading
+import queue
+import copy
 
 import ray
 
+class PoolTaskError(Exception):
+    def __init__(self, underlying):
+        self.underlying = underlying
+
+
+class ResultThread(threading.Thread):
+    def __init__(self, object_ids, callback=None, error_callback=None):
+        threading.Thread.__init__(self)
+        self._done = False
+        self._got_error = False
+        self._lock = threading.Lock()
+        self._object_ids = copy.copy(object_ids)
+        self._results = [None] * len(object_ids)
+        self._ready_index_queue = queue.Queue()
+        self._callback = callback
+        self._error_callback = error_callback
+        self._indices = {}
+        for i, object_id in enumerate(object_ids):
+            self._indices[object_id] = i
+
+    def run(self):
+        unready = self._object_ids
+        while len(unready) > 0:
+            ready, unready = ray.wait(unready, num_returns=1)
+            assert(len(ready) == 1)
+            ready_id = ready[0]
+
+            batch = ray.get(ready_id)
+            for result in batch:
+                if isinstance(result, Exception):
+                    self._got_error = True
+                    if self._error_callback is not None:
+                        self._error_callback(result)
+                elif self._callback is not None:
+                    self._callback(result)
+
+            with self._lock:
+                self._results[self._indices[ready_id]] = batch
+                self._ready_index_queue.put(self._indices[ready_id])
+
+        self._done = True
+
+    def done(self):
+        with self._lock:
+            return self._done
+
+    def got_error(self):
+        with self._lock:
+            return self._got_error
+
+    def results(self):
+        with self._lock:
+            return self._results
 
 class AsyncResult(object):
     def __init__(self,
@@ -15,58 +72,49 @@ class AsyncResult(object):
                  callback=None,
                  error_callback=None,
                  single_result=False):
-        if callback is not None or error_callback is not None:
-            raise NotImplementedError(
-                "Callbacks are not currently implemented.")
-        self._chunk_object_ids = chunk_object_ids
-        self._callback = callback
-        self._error_callback = error_callback
         self._single_result = single_result
-
-    def get(self, timeout=None):
-        if timeout is not None:
-            timeout = float(timeout)
-        try:
-            results = []
-            for chunk_results in ray.get(
-                    self._chunk_object_ids, timeout=timeout):
-                results.extend(chunk_results)
-            if self._single_result:
-                results = results[0]
-            return results
-        except ray.exceptions.RayTimeoutError:
-            raise TimeoutError
+        self._result_thread = ResultThread(chunk_object_ids, callback, error_callback)
+        self._result_thread.start()
 
     def wait(self, timeout=None):
-        if timeout is not None:
-            timeout = float(timeout)
-        ray.wait(
-            self._chunk_object_ids,
-            num_returns=len(self._chunk_object_ids),
-            timeout=timeout)
+        start = time.time()
+        while timeout is None or time.time()-start < timeout:
+            if self._result_thread.done():
+                break
+            time.sleep(0.001)
+
+    def get(self, timeout=None):
+        self.wait(timeout)
+        if not self._result_thread.done():
+            raise TimeoutError
+
+        results = []
+        for batch in self._result_thread.results():
+            for result in batch:
+                if isinstance(result, PoolTaskError):
+                    raise result.underlying
+            results.extend(batch)
+        if self._single_result:
+            return results[0]
+
+        return results
 
     def ready(self):
-        ready_ids, _ = ray.wait(
-            self._chunk_object_ids,
-            num_returns=len(self._chunk_object_ids),
-            timeout=0.0)
-        return len(ready_ids) == len(self._chunk_object_ids)
+        return self._result_thread.done()
 
     def successful(self):
         if not self.ready():
             raise ValueError("{0!r} not ready".format(self))
-        try:
-            ray.get(self._chunk_object_ids)
-        except Exception:
-            return False
-        return True
+        return not self._result_thread.got_error()
 
 
 class IMapIterator(object):
     def __init__(self, chunk_object_ids):
-        self._chunk_object_ids = chunk_object_ids
-        self._ready_objects = []
+        self._result_thread = ResultThread(chunk_object_ids)
+        self._result_thread.start()
         self._index = 0
+        self._ready_indices = [None] * len(chunk_object_ids)
+        self._ready_objects = []
 
     def __iter__(self):
         return self
@@ -139,7 +187,10 @@ class PoolActor(object):
                 args = tuple()
             if kwargs is None:
                 kwargs = {}
-            results.append(func(*args, **kwargs))
+            try:
+                results.append(func(*args, **kwargs))
+            except Exception as e:
+                results.append(PoolTaskError(e))
         return results
 
 
