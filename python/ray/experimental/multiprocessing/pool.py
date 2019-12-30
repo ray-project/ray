38e6ebefdd0107d15a60ebd28bf27add9494e9f5
Edward Oakes <ed.nmi.oakes@gmail.com>
2019-12-29 17:46:56 -0800

    docstrings

--- python/ray/experimental/multiprocessing/pool.py
+++ python/ray/experimental/multiprocessing/pool.py
@@ -106,6 +106,12 @@ class ResultThread(threading.Thread):
 
 
 class AsyncResult(object):
+    """A class that provides the interface to results from calls to
+    map_async or starmap_async.
+
+    This should not be constructed directly.
+    """
+
     def __init__(self,
                  chunk_object_ids,
                  callback=None,
@@ -117,6 +123,14 @@ class AsyncResult(object):
         self._result_thread.start()
 
     def wait(self, timeout=None):
+        """
+        Returns once the result is ready or the timeout expires (does not
+        raise TimeoutError).
+
+        Args:
+            timeout: timeout in milliseconds.
+        """
+
         self._result_thread.join(timeout)
 
     def get(self, timeout=None):
@@ -137,15 +151,27 @@ class AsyncResult(object):
         return results
 
     def ready(self):
+        """
+        Returns true if the result is ready, else false if the tasks are still
+        running.
+        """
+
         return not self._result_thread.is_alive()
 
     def successful(self):
+        """
+        Returns true if none of the submitted tasks errored, else false. Should
+        only be called once the result is ready (can be checked using `ready`).
+        """
+
         if not self.ready():
             raise ValueError("{0!r} not ready".format(self))
         return not self._result_thread.got_error()
 
 
 class IMapIterator(object):
+    """Base class for OrderedIMapIterator and UnorderedIMapIterator."""
+
     def __init__(self, pool, func, iterable, chunksize=None):
         self._pool = pool
         self._func = func
@@ -189,6 +215,15 @@ class IMapIterator(object):
 
 
 class OrderedIMapIterator(IMapIterator):
+    """Iterator to the results of tasks submitted using `imap`.
+
+    The results are returned in the same order that they were submitted, even
+    if they don't finish in that order. Only one batch of tasks per actor
+    process is submitted at a time - the rest are submitted as results come in.
+
+    Should not be constructed directly.
+    """
+
     def next(self, timeout=None):
         if len(self._ready_objects) == 0:
             if self._next_chunk_index == self._total_chunks:
@@ -216,6 +251,15 @@ class OrderedIMapIterator(IMapIterator):
 
 
 class UnorderedIMapIterator(IMapIterator):
+    """Iterator to the results of tasks submitted using `imap`.
+
+    The results are returned in the order that they finish. Only one batch of
+    tasks per actor process is submitted at a time - the rest are submitted as
+    results come in.
+
+    Should not be constructed directly.
+    """
+
     def next(self, timeout=None):
         if len(self._ready_objects) == 0:
             if self._next_chunk_index == self._total_chunks:
@@ -233,6 +277,8 @@ class UnorderedIMapIterator(IMapIterator):
 
 @ray.remote
 class PoolActor(object):
+    """Actor used to process tasks submitted to a Pool."""
+
     def __init__(self, initializer=None, initargs=None):
         if initializer:
             initargs = initargs or ()
@@ -257,6 +303,23 @@ class PoolActor(object):
 # https://docs.python.org/3/library/multiprocessing.html#module-multiprocessing.pool
 # TODO(edoakes): cloudpickle can't pickle generator objects.
 class Pool(object):
+    """A pool of actor processes that is used to process tasks in parallel.
+
+    Args:
+        processes: number of actor processes to start in the pool. Defaults to
+            the number of cores in the Ray cluster if one is already running,
+            otherwise the number of cores on this machine.
+        initializer: function to be run in each actor when it starts up.
+        initargs: iterable of arguments to the initializer function.
+        maxtasksperchild: maximum number of tasks to run in each actor process.
+            After a process has executed this many tasks, it will be killed and
+            replaced with a new one.
+        ray_address: address of the Ray cluster to run on. If None, a new local
+            Ray cluster will be started on this machine. Otherwise, this will
+            be passed to `ray.init()` to connect to a running cluster. This may
+            also be specified using the `RAY_ADDRESS` environment variable.
+    """
+
     def __init__(self,
                  processes=None,
                  initializer=None,
@@ -348,6 +411,18 @@ class Pool(object):
         return object_id
 
     def apply(self, func, args=None, kwargs=None):
+        """Run the given function on a random actor process and return the
+        result synchronously.
+
+        Args:
+            func: function to run.
+            args: optional arguments to the function.
+            kwargs: optional keyword arguments to the function.
+
+        Returns:
+            The result.
+        """
+
         return self.apply_async(func, args, kwargs).get()
 
     def apply_async(self,
@@ -356,57 +431,28 @@ class Pool(object):
                     kwargs=None,
                     callback=None,
                     error_callback=None):
+        """Run the given function on a random actor process and return an
+        asynchronous interface to the result.
+
+        Args:
+            func: function to run.
+            args: optional arguments to the function.
+            kwargs: optional keyword arguments to the function.
+            callback: callback to be executed on the result once it is finished
+                if it succeeds.
+            error_callback: callback to be executed the result once it is
+                finished if the task errors. The exception raised by the
+                task will be passed as the only argument to the callback.
+
+        Returns:
+            AsyncResult containing the result.
+        """
+
         self._check_running()
         object_id = self._run_batch(self._random_actor_index(), func,
                                     [(args, kwargs)])
         return AsyncResult(
             [object_id], callback, error_callback, single_result=True)
-
-    def map(self, func, iterable, chunksize=None):
-        self._check_running()
-        return self._map_async(
-            func, iterable, chunksize=chunksize, unpack_args=False).get()
-
-    def map_async(self,
-                  func,
-                  iterable,
-                  chunksize=None,
-                  callback=None,
-                  error_callback=None):
-        self._check_running()
-        return self._map_async(
-            func,
-            iterable,
-            chunksize=chunksize,
-            unpack_args=False,
-            callback=callback,
-            error_callback=error_callback)
-
-    def starmap(self, func, iterable, chunksize=None):
-        self._check_running()
-        return self._map_async(
-            func, iterable, chunksize=chunksize, unpack_args=True).get()
-
-    def starmap_async(self, func, iterable, callback=None,
-                      error_callback=None):
-        self._check_running()
-        return self._map_async(
-            func,
-            iterable,
-            unpack_args=True,
-            callback=callback,
-            error_callback=error_callback)
-
-    def _map_async(self,
-                   func,
-                   iterable,
-                   chunksize=None,
-                   unpack_args=False,
-                   callback=None,
-                   error_callback=None):
-        object_ids = self._chunk_and_run(
-            func, iterable, chunksize=chunksize, unpack_args=unpack_args)
-        return AsyncResult(object_ids, callback, error_callback)
 
     def _calculate_chunksize(self, iterable):
         chunksize, extra = divmod(len(iterable), len(self._actor_pool) * 4)
@@ -457,11 +503,120 @@ class Pool(object):
 
         return chunk_object_ids
 
+    def _map_async(self,
+                   func,
+                   iterable,
+                   chunksize=None,
+                   unpack_args=False,
+                   callback=None,
+                   error_callback=None):
+        self._check_running()
+        object_ids = self._chunk_and_run(
+            func, iterable, chunksize=chunksize, unpack_args=unpack_args)
+        return AsyncResult(object_ids, callback, error_callback)
+
+    def map(self, func, iterable, chunksize=None):
+        """Run the given function on each element in the iterable round-robin
+        on the actor processes and return the results synchronously.
+
+        Args:
+            func: function to run.
+            iterable: iterable of objects to be passed as the sole argument to
+                func.
+            chunksize: number of tasks to submit as a batch to each actor
+                process. If unspecified, a suitable chunksize will be chosen.
+
+        Returns:
+            A list of results.
+        """
+
+        return self._map_async(
+            func, iterable, chunksize=chunksize, unpack_args=False).get()
+
+    def map_async(self,
+                  func,
+                  iterable,
+                  chunksize=None,
+                  callback=None,
+                  error_callback=None):
+        """Run the given function on each element in the iterable round-robin
+        on the actor processes and return an asynchronous interface to the
+        results.
+
+        Args:
+            func: function to run.
+            iterable: iterable of objects to be passed as the only argument to
+                func.
+            chunksize: number of tasks to submit as a batch to each actor
+                process. If unspecified, a suitable chunksize will be chosen.
+            callback: callback to be executed on each successful result once it
+                is finished.
+            error_callback: callback to be executed on each errored result once
+                it is finished. The exception raised by the task will be passed
+                as the only argument to the callback.
+
+        Returns:
+            AsyncResult
+        """
+        return self._map_async(
+            func,
+            iterable,
+            chunksize=chunksize,
+            unpack_args=False,
+            callback=callback,
+            error_callback=error_callback)
+
+    def starmap(self, func, iterable, chunksize=None):
+        """Same as `map`, but unpacks each element of the iterable as the
+        arguments to func like: [func(*args) for args in iterable].
+        """
+
+        return self._map_async(
+            func, iterable, chunksize=chunksize, unpack_args=True).get()
+
+    def starmap_async(self, func, iterable, callback=None,
+                      error_callback=None):
+        """Same as `map_async`, but unpacks each element of the iterable as the
+        arguments to func like: [func(*args) for args in iterable].
+        """
+
+        return self._map_async(
+            func,
+            iterable,
+            unpack_args=True,
+            callback=callback,
+            error_callback=error_callback)
+
     def imap(self, func, iterable, chunksize=1):
+        """Same as `map`, but only submits one batch of tasks to each actor
+        process at a time.
+
+        This can be useful if the iterable of arguments is very large or each
+        task's arguments consumes a large amount of resources.
+
+        The results are returned in the order corresponding to their arguments
+        in the iterable.
+
+        Returns:
+            OrderedIMapIterator
+        """
+
         self._check_running()
         return OrderedIMapIterator(self, func, iterable, chunksize=chunksize)
 
     def imap_unordered(self, func, iterable, chunksize=1):
+        """Same as `map`, but only submits one batch of tasks to each actor
+        process at a time.
+
+        This can be useful if the iterable of arguments is very large or each
+        task's arguments consumes a large amount of resources.
+
+        The results are returned in the order that they finish.
+
+        Returns:
+            UnorderedIMapIterator
+        """
+
         self._check_running()
         return UnorderedIMapIterator(self, func, iterable, chunksize=chunksize)
 
@@ -477,17 +632,37 @@ class Pool(object):
         self.terminate()
 
     def close(self):
+        """Close the pool.
+
+        Prevents any more tasks from being submitted on the pool but allows
+        outstanding work to finish.
+        """
+
         for actor, _ in self._actor_pool:
             self._stop_actor(actor)
         self._closed = True
 
     def terminate(self):
+        """Close the pool.
+
+        Prevents any more tasks from being submitted on the pool and stops
+        outstanding work.
+        """
+
         if not self._closed:
             self.close()
         for actor, _ in self._actor_pool:
             actor.__ray_kill__()
 
     def join(self):
+        """Wait for the actors in a closed pool to exit.
+
+        If the pool was closed using `close`, this will return once all
+        outstanding work is completed.
+
+        If the pool was closed using `terminate`, this will return quickly.
+        """
+
         if not self._closed:
             raise ValueError("Pool is still running")
         self._wait_for_stopping_actors()
