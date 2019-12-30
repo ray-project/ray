2d48f79984ad2c45c0b0679c223cdfd54b4ed0b7
chaokunyang <shawn.ck.yang@gmail.com>
2019-12-30 15:47:12 +0800

    install ray-java only when RAY_INSTALL_JAVA is set

--- ci/travis/install-ray.sh
+++ ci/travis/install-ray.sh
@@ -9,7 +9,7 @@ echo "PYTHON is $PYTHON"
 
 # If we are in Travis, most of the compilation result will be cached.
 # This means we are I/O bounded. By default, Bazel set the number of concurrent
-# jobs to the the number cores on the machine, which are not efficient for 
+# jobs to the the number cores on the machine, which are not efficient for
 # network bounded cache downloading workload. Therefore we increase the number
 # of jobs to 50
 if [[ "$TRAVIS" == "true" ]]; then
@@ -46,8 +46,10 @@ else
   exit 1
 fi
 
-pushd "$ROOT_DIR/../../java/dist"
-echo "Install ray-java package"
-python setup.py install --user
-popd
+if [ "$RAY_INSTALL_JAVA" == "1" ]; then
+  pushd "$ROOT_DIR/../../java/dist"
+  echo "Install ray-java package"
+  python setup.py install --user
+  popd
+fi
 
