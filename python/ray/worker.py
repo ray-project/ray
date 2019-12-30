e2bc489a18c6c78a2513cd803c79337c9990d891
Eric Liang <ekhliang@gmail.com>
2019-12-29 19:19:43 -0800

    Port webui nits from original pr that enables it (#6628)
    * backport changes
    
    * Update test_webui.py

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -548,7 +548,7 @@ def init(address=None,
          plasma_directory=None,
          huge_pages=False,
          include_webui=None,
-         webui_host="127.0.0.1",
+         webui_host="localhost",
          job_id=None,
          configure_logging=True,
          logging_level=logging.INFO,
@@ -631,8 +631,8 @@ def init(address=None,
             is None, then the UI will be started if the relevant dependencies
             are present.
         webui_host: The host to bind the web UI server to. Can either be
-            127.0.0.1 (localhost) or 0.0.0.0 (available from all interfaces).
-            By default, this is set to 127.0.0.1 to prevent access from
+            localhost (127.0.0.1) or 0.0.0.0 (available from all interfaces).
+            By default, this is set to localhost to prevent access from
             external machines.
         job_id: The ID of this job.
         configure_logging: True if allow the logging cofiguration here.
