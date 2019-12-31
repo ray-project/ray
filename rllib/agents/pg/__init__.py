a80869739aad1188a5c989790bc843cab41d03cf
Sven <svenmika1977@gmail.com>
2019-12-31 17:31:52 +0100

    Fix LINT line-len errors.

--- rllib/agents/pg/__init__.py
+++ rllib/agents/pg/__init__.py
@@ -6,4 +6,5 @@ from ray.rllib.utils import renamed_agent
 # `Agent`=Legacy: Use PGTrainer instead.
 PGAgent = renamed_agent(PGTrainer)
 
-__all__ = ["PGAgent", "PGTrainer", "tf_pg_loss", "torch_pg_loss", "post_process_advantages", "DEFAULT_CONFIG"]
+__all__ = ["PGAgent", "PGTrainer", "tf_pg_loss", "torch_pg_loss",
+           "post_process_advantages", "DEFAULT_CONFIG"]
--- rllib/agents/pg/tf_pg_policy.py
+++ rllib/agents/pg/tf_pg_policy.py
@@ -3,7 +3,8 @@ from __future__ import division
 from __future__ import print_function
 
 import ray
-from ray.rllib.evaluation.postprocessing import compute_advantages, Postprocessing
+from ray.rllib.evaluation.postprocessing import compute_advantages, \
+    Postprocessing
 from ray.rllib.policy.tf_policy_template import build_tf_policy
 from ray.rllib.policy.sample_batch import SampleBatch
 from ray.rllib.utils import try_import_tf
@@ -15,12 +16,15 @@ def tf_pg_loss(policy, model, dist_class, train_batch):
     """The basic policy gradients loss."""
     logits, _ = model.from_batch(train_batch)
     action_dist = dist_class(logits, model)
-    return -tf.reduce_mean(action_dist.logp(train_batch[SampleBatch.ACTIONS]) * train_batch[Postprocessing.ADVANTAGES])
+    return -tf.reduce_mean(action_dist.logp(train_batch[SampleBatch.ACTIONS])
+                           * train_batch[Postprocessing.ADVANTAGES])
 
 
-def post_process_advantages(policy, sample_batch, other_agent_batches=None, episode=None):
+def post_process_advantages(policy, sample_batch, other_agent_batches=None,
+                            episode=None):
     """This adds the "advantages" column to the sample train_batch."""
-    return compute_advantages(sample_batch, 0.0, policy.config["gamma"], use_gae=False)
+    return compute_advantages(sample_batch, 0.0, policy.config["gamma"],
+                              use_gae=False)
 
 
 PGTFPolicy = build_tf_policy(
