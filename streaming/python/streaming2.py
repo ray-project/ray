73d56be9cf72cabd9d5e7ace5261783a00433bd2
chaokunyang <shawn.ck.yang@gmail.com>
2020-01-02 15:11:13 +0800

    add Builder for StreamingContext

--- streaming/python/streaming2.py
+++ streaming/python/streaming2.py
@@ -4,17 +4,28 @@ from ray.streaming.gateway_client import GatewayClient
 class StreamingContext:
     """Wrapper of java org.ray.streaming.api.context.StreamingContext"""
 
+    class Builder:
+
+        def __init__(self):
+            self._options = {}
+
+        def config(self, key=None, value=None, conf=None):
+            if conf is None:
+                self._options[key] = str(value)
+            else:
+                for k, v in conf.items():
+                    self._options[k] = v
+            return self
+
+        def build(self):
+            ctx = StreamingContext()
+            ctx._gateway_client.with_config(self._options)
+            return ctx
+
     def __init__(self):
         self._python_gateway_actor = self.__create_python_gateway_actor()
         self.__gateway_client = GatewayClient(self._python_gateway_actor)
         self._j_ctx = self._gateway_client.create_streaming_context()
-
-    @staticmethod
-    def build_context():
-        return StreamingContext()
-
-    def with_config(self, job_config):
-        self._gateway_client.with_config(job_config)
 
     def execute(self):
         """Construct job DAG, and execute the job.
@@ -30,5 +41,3 @@ class StreamingContext:
     @property
     def _gateway_client(self):
         return self.__gateway_client
-
-
