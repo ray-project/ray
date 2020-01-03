ebb5181fa08e44fd1fb31f08800117496c6a3f19
micafan <550435771@qq.com>
2020-01-03 15:07:24 +0800

    rm DirectActorTable

--- src/ray/gcs/subscription_executor.cc
+++ src/ray/gcs/subscription_executor.cc
@@ -185,7 +185,6 @@ Status SubscriptionExecutor<ID, Data, Table>::AsyncUnsubscribe(
 }
 
 template class SubscriptionExecutor<ActorID, ActorTableData, ActorTable>;
-template class SubscriptionExecutor<ActorID, ActorTableData, DirectActorTable>;
 template class SubscriptionExecutor<JobID, JobTableData, JobTable>;
 template class SubscriptionExecutor<TaskID, TaskTableData, raylet::TaskTable>;
 template class SubscriptionExecutor<ObjectID, ObjectChangeNotification, ObjectTable>;
