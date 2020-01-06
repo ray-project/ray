30444a3d3520eda67a60e9237f65a79b4f5c4d8b
Zhijun Fu <37800433+zhijunfu@users.noreply.github.com>
2020-01-06 21:35:40 +0800

    remove unused params in grpc macros (#6677)
    * remove unused params in grpc macros
    
    * format
    
    * fix
    
    * format
    
    * fix

--- src/ray/rpc/gcs_server/gcs_rpc_client.h
+++ src/ray/rpc/gcs_server/gcs_rpc_client.h
@@ -29,92 +29,78 @@ class GcsRpcClient {
   };
 
   /// Add job info to gcs server.
-  VOID_RPC_CLIENT_METHOD(JobInfoGcsService, AddJob, request, callback,
-                         job_info_grpc_client_, )
+  VOID_RPC_CLIENT_METHOD(JobInfoGcsService, AddJob, job_info_grpc_client_, )
 
   /// Mark job as finished to gcs server.
-  VOID_RPC_CLIENT_METHOD(JobInfoGcsService, MarkJobFinished, request, callback,
-                         job_info_grpc_client_, )
+  VOID_RPC_CLIENT_METHOD(JobInfoGcsService, MarkJobFinished, job_info_grpc_client_, )
 
   /// Get actor data from GCS Service.
-  VOID_RPC_CLIENT_METHOD(ActorInfoGcsService, GetActorInfo, request, callback,
-                         actor_info_grpc_client_, )
+  VOID_RPC_CLIENT_METHOD(ActorInfoGcsService, GetActorInfo, actor_info_grpc_client_, )
 
   /// Register an actor to GCS Service.
-  VOID_RPC_CLIENT_METHOD(ActorInfoGcsService, RegisterActorInfo, request, callback,
+  VOID_RPC_CLIENT_METHOD(ActorInfoGcsService, RegisterActorInfo,
                          actor_info_grpc_client_, )
 
   ///  Update actor info in GCS Service.
-  VOID_RPC_CLIENT_METHOD(ActorInfoGcsService, UpdateActorInfo, request, callback,
-                         actor_info_grpc_client_, )
+  VOID_RPC_CLIENT_METHOD(ActorInfoGcsService, UpdateActorInfo, actor_info_grpc_client_, )
 
   ///  Add actor checkpoint data to GCS Service.
-  VOID_RPC_CLIENT_METHOD(ActorInfoGcsService, AddActorCheckpoint, request, callback,
+  VOID_RPC_CLIENT_METHOD(ActorInfoGcsService, AddActorCheckpoint,
                          actor_info_grpc_client_, )
 
   ///  Get actor checkpoint data from GCS Service.
-  VOID_RPC_CLIENT_METHOD(ActorInfoGcsService, GetActorCheckpoint, request, callback,
+  VOID_RPC_CLIENT_METHOD(ActorInfoGcsService, GetActorCheckpoint,
                          actor_info_grpc_client_, )
 
   ///  Get actor checkpoint id data from GCS Service.
-  VOID_RPC_CLIENT_METHOD(ActorInfoGcsService, GetActorCheckpointID, request, callback,
+  VOID_RPC_CLIENT_METHOD(ActorInfoGcsService, GetActorCheckpointID,
                          actor_info_grpc_client_, )
 
   /// Register a node to GCS Service.
-  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, RegisterNode, request, callback,
-                         node_info_grpc_client_, )
+  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, RegisterNode, node_info_grpc_client_, )
 
   /// Unregister a node from GCS Service.
-  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, UnregisterNode, request, callback,
-                         node_info_grpc_client_, )
+  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, UnregisterNode, node_info_grpc_client_, )
 
   /// Get information of all nodes from GCS Service.
-  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, GetAllNodeInfo, request, callback,
-                         node_info_grpc_client_, )
+  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, GetAllNodeInfo, node_info_grpc_client_, )
 
   /// Report heartbeat of a node to GCS Service.
-  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, ReportHeartbeat, request, callback,
-                         node_info_grpc_client_, )
+  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, ReportHeartbeat, node_info_grpc_client_, )
 
   /// Report batch heartbeat to GCS Service.
-  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, ReportBatchHeartbeat, request, callback,
+  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, ReportBatchHeartbeat,
                          node_info_grpc_client_, )
 
   /// Get node's resources from GCS Service.
-  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, GetResources, request, callback,
-                         node_info_grpc_client_, )
+  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, GetResources, node_info_grpc_client_, )
 
   /// Update resources of a node in GCS Service.
-  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, UpdateResources, request, callback,
-                         node_info_grpc_client_, )
+  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, UpdateResources, node_info_grpc_client_, )
 
   /// Delete resources of a node in GCS Service.
-  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, DeleteResources, request, callback,
-                         node_info_grpc_client_, )
+  VOID_RPC_CLIENT_METHOD(NodeInfoGcsService, DeleteResources, node_info_grpc_client_, )
 
   /// Get object's locations from GCS Service.
-  VOID_RPC_CLIENT_METHOD(ObjectInfoGcsService, GetObjectLocations, request, callback,
+  VOID_RPC_CLIENT_METHOD(ObjectInfoGcsService, GetObjectLocations,
                          object_info_grpc_client_, )
 
   /// Add location of object to GCS Service.
-  VOID_RPC_CLIENT_METHOD(ObjectInfoGcsService, AddObjectLocation, request, callback,
+  VOID_RPC_CLIENT_METHOD(ObjectInfoGcsService, AddObjectLocation,
                          object_info_grpc_client_, )
 
   /// Remove location of object to GCS Service.
-  VOID_RPC_CLIENT_METHOD(ObjectInfoGcsService, RemoveObjectLocation, request, callback,
+  VOID_RPC_CLIENT_METHOD(ObjectInfoGcsService, RemoveObjectLocation,
                          object_info_grpc_client_, )
 
   /// Add a task to GCS Service.
-  VOID_RPC_CLIENT_METHOD(TaskInfoGcsService, AddTask, request, callback,
-                         task_info_grpc_client_, )
+  VOID_RPC_CLIENT_METHOD(TaskInfoGcsService, AddTask, task_info_grpc_client_, )
 
   /// Get task information from GCS Service.
-  VOID_RPC_CLIENT_METHOD(TaskInfoGcsService, GetTask, request, callback,
-                         task_info_grpc_client_, )
+  VOID_RPC_CLIENT_METHOD(TaskInfoGcsService, GetTask, task_info_grpc_client_, )
 
   /// Delete tasks from GCS Service.
-  VOID_RPC_CLIENT_METHOD(TaskInfoGcsService, DeleteTasks, request, callback,
-                         task_info_grpc_client_, )
+  VOID_RPC_CLIENT_METHOD(TaskInfoGcsService, DeleteTasks, task_info_grpc_client_, )
 
  private:
   /// The gRPC-generated stub.
