11c6b32c2edd59e3da6814fcf73694a3c0fe7480
Edward Oakes <ed.nmi.oakes@gmail.com>
2020-01-07 14:25:00 -0800

    Set version to 0.8.1

--- python/ray/__init__.py
+++ python/ray/__init__.py
@@ -129,7 +129,7 @@ from ray.actor import method  # noqa: E402
 from ray.runtime_context import _get_runtime_context  # noqa: E402
 
 # Ray version string.
-__version__ = "0.9.0.dev0"
+__version__ = "0.8.1"
 
 __all__ = [
     "jobs",
