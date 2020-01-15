4fd8981f0a6e593fd14af49982c18403b0b0518d
灵洵 <fengbin.ffb@antfin.com>
2020-01-15 17:11:53 +0800

    capture type instead of request

--- src/ray/gcs/gcs_server/error_info_handler_impl.cc
+++ src/ray/gcs/gcs_server/error_info_handler_impl.cc
@@ -7,14 +7,14 @@ void DefaultErrorInfoHandler::HandleReportJobError(
     const ReportJobErrorRequest &request, ReportJobErrorReply *reply,
     SendReplyCallback send_reply_callback) {
   JobID job_id = JobID::FromBinary(request.error_data().job_id());
-  RAY_LOG(DEBUG) << "Reporting job error, job id = " << job_id
-                 << ", type = " << request.error_data().type();
+  std::string type = request.error_data().type();
+  RAY_LOG(DEBUG) << "Reporting job error, job id = " << job_id << ", type = " << type;
   auto error_table_data = std::make_shared<ErrorTableData>();
   error_table_data->CopyFrom(request.error_data());
-  auto on_done = [job_id, request, send_reply_callback](Status status) {
+  auto on_done = [job_id, type, send_reply_callback](Status status) {
     if (!status.ok()) {
       RAY_LOG(ERROR) << "Failed to report job error, job id = " << job_id
-                     << ", type = " << request.error_data().type();
+                     << ", type = " << type;
     }
     send_reply_callback(status, nullptr, nullptr);
   };
@@ -24,7 +24,7 @@ void DefaultErrorInfoHandler::HandleReportJobError(
     on_done(status);
   }
   RAY_LOG(DEBUG) << "Finished reporting job error, job id = " << job_id
-                 << ", type = " << request.error_data().type();
+                 << ", type = " << type;
 }
 
 }  // namespace rpc
