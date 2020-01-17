b9e36c8bbed233f3b4f1b57e1647098e56ace2a9
chentianyi <chentianyi.cty@antfin.com>
2020-01-17 12:04:37 +0800

    Fix doc check style.

--- streaming/java/streaming-api/src/main/java/org/ray/streaming/jobgraph/JobGraph.java
+++ streaming/java/streaming-api/src/main/java/org/ray/streaming/jobgraph/JobGraph.java
@@ -30,11 +30,11 @@ public class JobGraph implements Serializable {
   /**
    * Generate direct-graph(made up of a set of vertices and connected by edges)
    * by current job graph for simple log printing.
-   * @return Digraph in string type.
-   *
    * Notice:
    * This is temporarily implemented in hard code.
    * May use 'guru.nidi:graphviz-java' as 3rd dependency in the future if needed.
+   *
+   * @return Digraph in string type.
    */
   public String generateDigraph() {
     StringBuilder digraph = new StringBuilder();
