a2c0aa17cc65125edee8f3ebf134eda8c6bc20f1
chaokunyang <shawn.ck.yang@gmail.com>
2020-01-19 14:08:01 +0800

    force msgpack version

--- python/setup.py
+++ python/setup.py
@@ -83,7 +83,7 @@ extras = {
     "dashboard": ["aiohttp", "google", "grpcio", "psutil", "setproctitle"],
     "serve": ["uvicorn", "pygments", "werkzeug", "flask", "pandas", "blist"],
     "tune": ["tabulate"],
-    "streaming": ["msgpack"],
+    "streaming": ["msgpack >= 0.6.2"],
 }
 
 extras["all"] = list(set(chain.from_iterable(extras.values())))
