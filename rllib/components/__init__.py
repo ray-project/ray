ba2fa9e8bbce49ed279909906a268c1d208189fa
sven1977 <svenmika1977@gmail.com>
2020-01-20 16:38:44 +0100

    Add `from_config` util method for generating framework-independent components from an arbitrary config (dict, json, yaml, file). Plus tests.

--- rllib/components/__init__.py
+++
@@ -1,8 +0,0 @@
-from ray.rllib.components.component import Component
-
-__all__ = [
-    "Component",
-]
-
-# TODO: Move all sharable components here (from their current locations).
-# TODO: e.g. Memories, Optimizers, ...
