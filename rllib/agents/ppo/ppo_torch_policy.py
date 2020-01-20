6ee90450ae8c663e8f5708925061d1231ab06e20
sven1977 <svenmika1977@gmail.com>
2020-01-20 11:54:56 +0100

    Add back detach() calls to ppo_torch_policy.py::kl_and_loss_stats().

--- rllib/agents/ppo/ppo_torch_policy.py
+++ rllib/agents/ppo/ppo_torch_policy.py
@@ -144,15 +144,15 @@ def kl_and_loss_stats(policy, train_batch):
     return {
         "cur_kl_coeff": policy.kl_coeff,
         "cur_lr": policy.cur_lr,
-        "total_loss": policy.loss_obj.loss.cpu().numpy(),
-        "policy_loss": policy.loss_obj.mean_policy_loss.cpu().numpy(),
-        "vf_loss": policy.loss_obj.mean_vf_loss.cpu().numpy(),
+        "total_loss": policy.loss_obj.loss.cpu().detach().numpy(),
+        "policy_loss": policy.loss_obj.mean_policy_loss.cpu().detach().numpy(),
+        "vf_loss": policy.loss_obj.mean_vf_loss.cpu().detach().numpy(),
         "vf_explained_var": explained_variance(
             train_batch[Postprocessing.VALUE_TARGETS],
             policy.model.value_function(),
-            framework="torch").cpu().numpy(),
-        "kl": policy.loss_obj.mean_kl.cpu().numpy(),
-        "entropy": policy.loss_obj.mean_entropy.cpu().numpy(),
+            framework="torch").cpu().detach().numpy(),
+        "kl": policy.loss_obj.mean_kl.cpu().detach().numpy(),
+        "entropy": policy.loss_obj.mean_entropy.cpu().detach().numpy(),
         "entropy_coeff": policy.entropy_coeff,
     }
 
