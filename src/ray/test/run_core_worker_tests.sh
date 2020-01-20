905e7792a08f61546f934bbeb46afd6e947d4a20
Zhijun Fu <pingfu.fzj@antfin.com>
2020-01-20 11:41:04 +0800

    remove unintended change

--- src/ray/test/run_core_worker_tests.sh
+++ src/ray/test/run_core_worker_tests.sh
@@ -22,7 +22,7 @@ fi
 set -e
 set -x
 
-bazel build "//:core_worker_test" "//:mock_worker"  "//:raylet" "//:raylet_monitor" "//:libray_redis_module.so" "@plasma//:plasma_store_server"
+bazel build -c dbg $RAY_BAZEL_CONFIG "//:core_worker_test" "//:mock_worker"  "//:raylet" "//:raylet_monitor" "//:libray_redis_module.so" "@plasma//:plasma_store_server"
 
 # Get the directory in which this script is executing.
 SCRIPT_DIR="`dirname \"$0\"`"
@@ -39,7 +39,7 @@ if [ ! -d "$RAY_ROOT/python" ]; then
 fi
 
 REDIS_MODULE="./bazel-bin/libray_redis_module.so"
-BAZEL_BIN_PREFIX="$(bazel info bazel-bin)"
+BAZEL_BIN_PREFIX="$(bazel info -c dbg $RAY_BAZEL_CONFIG bazel-bin)"
 LOAD_MODULE_ARGS="--loadmodule ${REDIS_MODULE}"
 STORE_EXEC="$BAZEL_BIN_PREFIX/external/plasma/plasma_store_server"
 RAYLET_EXEC="$BAZEL_BIN_PREFIX/raylet"
@@ -56,7 +56,7 @@ sleep 2s
 bazel run "//:redis-server" -- --loglevel warning ${LOAD_MODULE_ARGS} --port 6380 &
 sleep 2s
 # Run tests.
-bazel run $"//:core_worker_test" $STORE_EXEC $RAYLET_EXEC $RAYLET_PORT $RAYLET_MONITOR_EXEC $MOCK_WORKER_EXEC
+bazel run -c dbg $RAY_BAZEL_CONFIG "//:core_worker_test" $STORE_EXEC $RAYLET_EXEC $RAYLET_PORT $RAYLET_MONITOR_EXEC $MOCK_WORKER_EXEC
 sleep 1s
 bazel run "//:redis-cli" -- -p 6379 shutdown
 bazel run "//:redis-cli" -- -p 6380 shutdown
