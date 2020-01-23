33e8bf85a266135f4acad6573c282007902ed030
Alex Wu <itswu.alex@gmail.com>
2020-01-22 18:26:07 -0800

    Ran formatter

--- python/ray/autoscaler/updater.py
+++ python/ray/autoscaler/updater.py
@@ -277,8 +277,8 @@ class SSHCommandRunner:
         ])
 
     def remote_shell_command_str(self):
-        return "ssh -o IdentitiesOnly=yes -i {} {}@{}\n".format(self.ssh_private_key, self.ssh_user,
-                                          self.ssh_ip)
+        return "ssh -o IdentitiesOnly=yes -i {} {}@{}\n".format(
+            self.ssh_private_key, self.ssh_user, self.ssh_ip)
 
 
 class NodeUpdater:
