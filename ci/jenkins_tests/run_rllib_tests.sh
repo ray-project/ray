446cbdf2e0e3bcd0476adb9931af4cdacc00225d
Sven Mika <svenmika1977@gmail.com>
2020-01-24 19:29:35 +0100

    [RLlib] Fix issue (bug): LSTM + non-shared vf + PPO + tuple actions (#6890)
    * Add `RandomEnv` example to examples folder.
    Convert warning into Error message when using an LSTM in a non-shared-vf network (after the warning, the program would crash).
    
    * LINT.
    
    * Fix issue #6884. LSTM + non-shared vf NN + PPO crashes when using a Tuple action space.
    
    * LINT
    
    * Change warning message for Model: shared_vf=False, LSTM=True cases.
    
    * Bug fix.
    
    * Add examples/random_env.py test to Jenkins.

--- ci/jenkins_tests/run_rllib_tests.sh
+++ ci/jenkins_tests/run_rllib_tests.sh
@@ -487,3 +487,6 @@ docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
 
 docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
     /ray/ci/suppress_output python /ray/rllib/tests/test_env_with_subprocess.py
+
+docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
+    /ray/ci/suppress_output python /ray/rllib/examples/random_env.py
