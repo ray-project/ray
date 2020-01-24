1a06982d8d2d24479bd8e6489e41f6f38ccceb25
sven1977 <svenmika1977@gmail.com>
2020-01-24 10:42:49 +0100

    Add atari to travis requirements. Fix num-cpus for IMPALA test with aggregation workers. Add sample_async=False for torch-A3C.

--- ci/travis/install-dependencies.sh
+++ ci/travis/install-dependencies.sh
@@ -24,19 +24,21 @@ if [[ "$PYTHON" == "3.5" ]] && [[ "$platform" == "linux" ]]; then
   wget -q https://repo.continuum.io/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh -O miniconda.sh -nv
   bash miniconda.sh -b -p $HOME/miniconda
   export PATH="$HOME/miniconda/bin:$PATH"
-  pip install -q scipy tensorflow cython==0.29.0 gym opencv-python-headless pyyaml pandas==0.24.2 requests \
-    feather-format lxml openpyxl xlrd py-spy setproctitle pytest-timeout networkx tabulate psutil aiohttp \
-    uvicorn dataclasses pygments werkzeug kubernetes flask grpcio pytest-sugar pytest-rerunfailures pytest-asyncio \
-    blist torch torchvision
+  pip install -q scipy tensorflow cython==0.29.0 gym gym[atari] \
+    opencv-python-headless pyyaml pandas==0.24.2 requests feather-format lxml \
+    openpyxl xlrd py-spy setproctitle pytest-timeout networkx tabulate psutil \
+    aiohttp uvicorn dataclasses pygments werkzeug kubernetes flask grpcio \
+    pytest-sugar pytest-rerunfailures pytest-asyncio blist torch torchvision
 elif [[ "$PYTHON" == "3.5" ]] && [[ "$platform" == "macosx" ]]; then
   # Install miniconda.
   wget -q https://repo.continuum.io/miniconda/Miniconda3-4.5.4-MacOSX-x86_64.sh -O miniconda.sh -nv
   bash miniconda.sh -b -p $HOME/miniconda
   export PATH="$HOME/miniconda/bin:$PATH"
-  pip install -q cython==0.29.0 tensorflow gym opencv-python-headless pyyaml pandas==0.24.2 requests \
-    feather-format lxml openpyxl xlrd py-spy setproctitle pytest-timeout networkx tabulate psutil aiohttp \
-    uvicorn dataclasses pygments werkzeug kubernetes flask grpcio pytest-sugar pytest-rerunfailures pytest-asyncio \
-    blist torch torchvision
+  pip install -q cython==0.29.0 tensorflow gym gym[atari] \
+    opencv-python-headless pyyaml pandas==0.24.2 requests feather-format lxml \
+    openpyxl xlrd py-spy setproctitle pytest-timeout networkx tabulate psutil \
+    aiohttp uvicorn dataclasses pygments werkzeug kubernetes flask grpcio \
+    pytest-sugar pytest-rerunfailures pytest-asyncio blist torch torchvision
 elif [[ "$LINT" == "1" ]]; then
   sudo apt-get update
   sudo apt-get install -y build-essential curl unzip
