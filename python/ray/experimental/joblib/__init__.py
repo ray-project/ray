6442ac9556f882fd150b2563ad441d01ff769653
Ameer Haj Ali <ameerh@berkeley.edu>
2020-01-26 14:37:07 -0800

    Apply suggestions from code review
    Co-Authored-By: Richard Liaw <rliaw@berkeley.edu>

--- python/ray/experimental/joblib/__init__.py
+++ python/ray/experimental/joblib/__init__.py
@@ -4,7 +4,7 @@ from joblib.parallel import register_parallel_backend
 def register_ray():
     """ Register Ray Backend if called with parallel_backend("ray") """
     try:
-        from ._raybackend import RayBackend
+        from ray.experimental.joblib._raybackend import RayBackend
         register_parallel_backend("ray", RayBackend)
     except ImportError:
         msg = ("To use the ray backend you must install ray"
