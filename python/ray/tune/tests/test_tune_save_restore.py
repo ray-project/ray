e73c6a31f2deeff9bafbcd4b68263d2693715e63
Eric Liang <ekhliang@gmail.com>
2020-01-27 15:53:40 -0800

    [tune] Fix directory naming regression (#6839)

--- python/ray/tune/tests/test_tune_save_restore.py
+++ python/ray/tune/tests/test_tune_save_restore.py
@@ -47,6 +47,8 @@ class SerialTuneRelativeLocalDirTest(unittest.TestCase):
         _register_all()
 
     def _get_trial_dir(self, absoulte_exp_dir):
+        print("looking for", self.MockTrainable._name)
+        print("in", os.listdir(absoulte_exp_dir))
         trial_dirname = next(
             (child_dir for child_dir in os.listdir(absoulte_exp_dir)
              if (os.path.isdir(os.path.join(absoulte_exp_dir, child_dir))
