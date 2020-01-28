2ef23ae0f9bccc786242632426436b51b3d9badf
sven1977 <svenmika1977@gmail.com>
2020-01-28 16:24:19 +0100

    WIP

--- ci/travis/determine_tests_to_run.py
+++ ci/travis/determine_tests_to_run.py
@@ -127,9 +127,7 @@ if __name__ == "__main__":
 
     # Log the modified environment variables visible in console.
     for output_stream in [sys.stdout, sys.stderr]:
-        def _print(str):
-            print(str)
-            print(str, file=output_stream)
+        _print = partial(print, file=output_stream)
         _print("export RAY_CI_TUNE_AFFECTED={}".format(RAY_CI_TUNE_AFFECTED))
         _print("export RAY_CI_RLLIB_AFFECTED={}".format(RAY_CI_RLLIB_AFFECTED))
         _print("export RAY_CI_SERVE_AFFECTED={}".format(RAY_CI_SERVE_AFFECTED))
--- rllib/agents/__init__.py
+++ rllib/agents/__init__.py
@@ -2,4 +2,3 @@ from ray.rllib.agents.trainer import Trainer, with_common_config
 from ray.rllib.agents.agent import Agent
 
 __all__ = ["Agent", "Trainer", "with_common_config"]
-
