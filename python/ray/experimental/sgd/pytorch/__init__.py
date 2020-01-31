64c8996a4320f3dd6e1534f5fd2e674fee6e059c
Maksim Smolin <maximsmol@gmail.com>
2020-01-31 01:16:57 -0800

    [raysgd] Update to fix examples out of the box (#6966)
    * Update tf-example-sgd dependencies, AMI, and instance type
    
    * Make PyTorch dependency optional
    
    * Re-implement optional torch import
    
    * Update tensorflow_train_example
    
    * Setup tf-example-sgd config for SGD development
    
    * Document the MultiWorkerMirroredStrategy behavior
    
    * Run scripts/format
    
    * Undo GPU default for CI
    
    * Remove dev deploy file_mounts
    
    * Update docs on tf_runner and tf_trainer
    
    * Fix formatting
    
    * Remove the debug file-mounts again
    
    * Disable cifar example GPU usage by default so CI runs properly
    
    * Mark failing PyTorch test as flaky
    
    * Clarify the tf SGD sanity check
    
    * Run format script
    
    * Update tf-example-sgd.yaml
    
    Co-authored-by: Richard Liaw <rliaw@berkeley.edu>

--- python/ray/experimental/sgd/pytorch/__init__.py
+++ python/ray/experimental/sgd/pytorch/__init__.py
@@ -1,4 +1,15 @@
-from ray.experimental.sgd.pytorch.pytorch_trainer import (PyTorchTrainer,
-                                                          PyTorchTrainable)
+import logging
+logger = logging.getLogger(__name__)
 
-__all__ = ["PyTorchTrainer", "PyTorchTrainable"]
+PyTorchTrainer = None
+PyTorchTrainable = None
+
+try:
+    import torch  # noqa: F401
+
+    from ray.experimental.sgd.pytorch.pytorch_trainer import (PyTorchTrainer,
+                                                              PyTorchTrainable)
+
+    __all__ = ["PyTorchTrainer", "PyTorchTrainable"]
+except ImportError:
+    logger.warning("PyTorch not found. PyTorchTrainer will not be available")
