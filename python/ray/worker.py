341a921d81e15cc05b691d4b4bb2e19acd54a7ae
Edward Oakes <ed.nmi.oakes@gmail.com>
2020-01-31 16:52:43 -0800

    Remove vanilla pickle serialization for task arguments (#6948)

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -1523,8 +1523,6 @@ def put(value, weakref=False):
     """Store an object in the object store.
 
     The object may not be evicted while a reference to the returned ID exists.
-    Note that this pinning only applies to the particular object ID returned
-    by put, not object IDs in general.
 
     Args:
         value: The Python object to be stored.
