da0bdd55be4649722eb9780ffc49e6849974bd03
sven1977 <svenmika1977@gmail.com>
2020-02-02 14:43:06 +0100

    WIP

--- ci/jenkins_tests/run_rllib_tests.sh
+++ ci/jenkins_tests/run_rllib_tests.sh
@@ -4,39 +4,6 @@
 # TO THIS FILE. INSTEAD, ADD NEW RLLIB TEST CASES TO /ray/rllib/BUILD
 # -------------------------------------------------------------------
 # -------------------------------------------------------------------
-docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
-    /ray/ci/suppress_output python /ray/rllib/examples/custom_eval.py --custom-eval
-
-docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
-    /ray/ci/suppress_output python /ray/rllib/tests/test_catalog.py
-
-docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
-    /ray/ci/suppress_output python /ray/rllib/tests/test_optimizers.py
-
-docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
-    /ray/ci/suppress_output python /ray/rllib/tests/test_filters.py
-
-docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
-    /ray/ci/suppress_output python /ray/rllib/tests/test_evaluators.py
-
-
-# Not moved to BAZEL yet due to *.runfile problem (declaring external input files and making them available
-# at runtime for py_test).
-docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
-    /ray/ci/suppress_output /ray/rllib/train.py \
-    --env CartPole-v0 \
-    --run MARWIL \
-    --stop '{"training_iteration": 1}' \
-    --config '{"input": "/ray/rllib/tests/data/cartpole_small", "learning_starts": 0, "input_evaluation": ["wis", "is"], "shuffle_buffer_size": 10}'
-
-# Not moved to BAZEL yet due to *.runfile problem (declaring external input files and making them available
-# at runtime for py_test).
-docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
-    /ray/ci/suppress_output /ray/rllib/train.py \
-    --env CartPole-v0 \
-    --run DQN \
-    --stop '{"training_iteration": 1}' \
-    --config '{"input": "/ray/rllib/tests/data/cartpole_small", "learning_starts": 0, "input_evaluation": ["wis", "is"], "soft_q": true}'
 
 # Not moved to BAZEL yet due to *.sh.
 docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} $DOCKER_SHA \
--- rllib/tests/run_regression_tests.py
+++ rllib/tests/run_regression_tests.py
@@ -2,38 +2,68 @@
 # Runs one or more regression tests. Retries tests up to 3 times.
 #
 # Example usage:
-#   ./run_regression_tests.sh regression-tests/cartpole-es.yaml
+# $ python run_regression_tests.py regression-tests/cartpole-es.yaml
+#
+# When using in BAZEL (with py_test), e.g. see in ray/rllib/BUILD:
+#py_test(
+#    name = "run_regression_tests",
+#    main = "tests/run_regression_tests.py",
+#    size = "large",
+#    srcs = ["tests/run_regression_tests.py"],
+#    data = glob(["tuned_examples/regression_tests/**"]),
+#    args = glob(["tuned_examples/regression_tests/**"])
+#)
 
-import yaml
+from pathlib import Path
 import sys
+import yaml
 
 import ray
 from ray.tune import run_experiments
 
+
 if __name__ == "__main__":
+    # Bazel regression test mode: Get path to look for yaml files from argv[2].
+    if sys.argv[1] == "BAZEL":
+        # Get the path to use.
+        rllib_dir = Path(__file__).parent.parent
+        print("rllib dir={}".format(rllib_dir))
+        yaml_files = rllib_dir.rglob(sys.argv[2] + "/*.yaml")
+        yaml_files = list(sorted(
+            map(lambda path: str(path.absolute()), yaml_files)))
+    # Normal mode: Get yaml files to run from command line.
+    else:
+        yaml_files = sys.argv[1:]
+
+    print("Will run the following regression files:")
+    for yaml_file in yaml_files:
+        print("->", yaml_file)
 
     ray.init()
 
-    for test in sys.argv[1:]:
-        experiments = yaml.load(open(test).read())
-
+    # Loop through all collected files.
+    for yaml_file in yaml_files:
+        experiments = yaml.load(open(yaml_file).read())
+    
         print("== Test config ==")
         print(yaml.dump(experiments))
 
+        passed = False
         for i in range(3):
             trials = run_experiments(experiments, resume=False)
-
-            num_failures = 0
+    
             for t in trials:
-                if (t.last_result["episode_reward_mean"] <
+                if (t.last_result["episode_reward_mean"] >=
                         t.stopping_criterion["episode_reward_mean"]):
-                    num_failures += 1
-
-            if not num_failures:
+                    passed = True
+                    break
+    
+            if passed:
                 print("Regression test PASSED")
-                sys.exit(0)
-
-            print("Regression test flaked, retry", i)
-
-        print("Regression test FAILED")
-        sys.exit(1)
+                break
+            else:
+                print("Regression test FAILED on attempt {}", i+1)
+        
+        if not passed:
+            print("Overall regression FAILED: Exiting with Error.")
+            sys.exit(1)
