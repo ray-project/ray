3f41b33c3cc630e0594d6ead20a37c9001879cbb
simon-mo <xmo@berkeley.edu>
2020-02-05 11:37:10 -0800

    Add import ordering checker

--- ci/travis/format.sh
+++ ci/travis/format.sh
@@ -141,6 +141,11 @@ else
     format_changed
 fi
 
+# Ensure import ordering
+# Make sure that for every import psutil; import setpproctitle
+# There's a import ray above it.
+python ci/travis/check_import_order.py . -s ci -s python/ray/pyarrow_files
+
 if ! git diff --quiet &>/dev/null; then
     echo 'Reformatted changed files. Please review and stage the changes.'
     echo 'Files updated:'
