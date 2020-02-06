b1353b76c5ec25f12f9b0f187a5e5b14ccaaf77c
simon-mo <xmo@berkeley.edu>
2020-02-06 10:15:26 -0800

    Make readthedoc build again

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -62,6 +62,7 @@ sys.modules["tensorflow"].VERSION = "9.9.9"
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath("../../python/"))
+sys.path.insert(0, os.path.abspath("../../python/ray/thirdparty_files"))
 
 # -- General configuration ------------------------------------------------
 
