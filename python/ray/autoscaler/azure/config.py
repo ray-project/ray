409278466012686b71024904eee59410fd7ed70b
Scott Graham <5720537+gramhagen@users.noreply.github.com>
2020-02-05 22:43:42 -0500

    adding todos and switching to auth file for service principal authentication

--- python/ray/autoscaler/azure/config.py
+++ python/ray/autoscaler/azure/config.py
@@ -1,20 +1,22 @@
 import datetime
+import json
 import logging
 import os
+import secrets
+import string
 import uuid
 
-from azure.common.client_factory import get_client_from_cli_profile
-from azure.common.credentials import ServicePrincipalCredentials
+from azure.common.client_factory import get_client_from_cli_profile, get_client_from_auth_file
 from azure.graphrbac import GraphRbacManagementClient
-from azure.graphrbac.models import PasswordCredential
 from azure.mgmt.network import NetworkManagementClient
 from azure.mgmt.resource import ResourceManagementClient
 import paramiko
 
-
+# TODO: add asserts on validity of names for all resources
 RAY = "ray-autoscaler"
 INSTANCE_NAME_MAX_LEN = 64
 INSTANCE_NAME_UUID_LEN = 8
+PASSWORD_MIN_LENGTH = 16
 SSH_KEYS_MAX_COUNT = 10
 IP_CONFIG_NAME = 'ray-ip-config'
 SUBNET_NAME = 'ray-subnet'
@@ -42,7 +44,9 @@ def _configure_resource_group(config):
 
     resource_group_name = config['provider']['resource_group']
     logger.info("Creating resource group: %s", resource_group_name)
-    resource_client.resource_groups.create_or_update(resource_group_name=resource_group_name).wait()
+    params = dict(location=config['provider']['location'])
+    resource_client.resource_groups.create_or_update(resource_group_name=resource_group_name,
+                                                     parameters=params)
 
     return config
 
@@ -59,10 +63,28 @@ def _configure_service_principal(config):
     else:
         app_name = sp_name.split('://', 1)[-1]
 
-    password = os.environ.get('AZURE_CLIENT_SECRET')
-    if password is None:
+    location = config['provider']['location']
+    resource_group = config['provider']['resource_group']
+    auth_name = "azure_credentials_{}_{}_{}.json".format(RAY, location, resource_group)
+    auth_path = os.path.expanduser("~/.azure/{}".format(auth_name))
+
+    new_auth = False
+    if os.path.exists(auth_path):
+        with open(auth_path, 'r') as f:
+            credentials = json.load(f)
+        password = credentials['clientSecret']
+    else:
+        new_auth = True
         logger.info('AZURE_CLIENT_SECRET environment variable not set, generating new password')
-        password = uuid.uuid4().hex
+        alphabet = ''.join([string.ascii_lowercase, string.ascii_uppercase, string.digits, string.punctuation])
+        while True:
+            password = ''.join(secrets.choice(alphabet) for _ in range(PASSWORD_MIN_LENGTH))
+            if (any(c.islower() for c in password) and
+                any(c.isupper() for c in password) and
+                any(c.isdigit() for c in password) and
+                any(not c.isalnum() for c in password)):
+                break
+        credentials = dict(clientSecret=password)
 
     try:
         # find existing application
@@ -71,17 +93,16 @@ def _configure_service_principal(config):
     except StopIteration:
         # create new application
         logger.info("Creating Application: %s", app_name)
-        app_start_date = datetime.datetime.utcnow()
+        app_start_date = datetime.datetime.now(datetime.timezone.utc)
         app_end_date = app_start_date.replace(year=app_start_date.year + 1)
 
-        password_creds = PasswordCredential(start_date=app_start_date,
-                                            end_date=app_end_date,
-                                            key_id=uuid.uuid4().hex,
-                                            value=password)
-
+        password_credentials = dict(start_date=app_start_date,
+                                    end_date=app_end_date,
+                                    key_id=uuid.uuid4().hex,
+                                    value=password)
         app_params = dict(display_name=app_name,
                           identifier_uris=[sp_name],
-                          password_credentials=password_creds)
+                          password_credentials=[password_credentials])
         app = graph_client.applications.create(parameters=app_params)
 
     try:
@@ -96,10 +117,13 @@ def _configure_service_principal(config):
 
         # TODO: set role / scope for service principal
 
-    config['provider']['client_id'] = sp.app_id
-    config['provider']['secret'] = password
-    config['provider']['tenant'] = graph_client.config.tenant_id
+    if new_auth:
+        credentials['clientId'] = sp.app_id
+        credentials['subscriptionId'] = config['provider']['subscription_id']
+        credentials['tenantId'] = graph_client.config.tenant_id
+        store_azure_auth_file(credentials=credentials, auth_path=auth_path)
 
+    config['provider']['auth_path'] = auth_path
     return config
 
 
@@ -110,13 +134,13 @@ def _configure_key_pair(config):
         assert os.path.exists(ssh_private_key)
         return config
 
-    provider = config['provider']['type']
     location = config['provider']['location']
     resource_group = config['provider']['resource_group']
     ssh_user = config['auth']['ssh_user']
 
     # look for an existing key pair
-    key_name = "{}_{}_{}_{}".format(RAY, provider, location, resource_group, ssh_user)
+    # TODO: other services store key pairs in cloud? why try multiple times?
+    key_name = "{}_azure_{}_{}".format(RAY, location, resource_group, ssh_user)
     public_key_path = os.path.expanduser("~/.ssh/{}.pub".format(key_name))
     private_key_path = os.path.expanduser("~/.ssh/{}.pem".format(key_name))
     if os.path.exists(public_key_path) and os.path.exists(private_key_path):
@@ -137,17 +161,14 @@ def _configure_key_pair(config):
 def _configure_nic(config):
     # skip this if nic is manually set in configuration yaml
     head_node_nic = config['head_node'].get('network_profile', {}).get('network_interfaces', [])
-    worker_node_nic = config['worker_node'].get('network_profile', {}).get('network_interfaces', [])
-    if head_node_nic and worker_node_nic:
+    worker_nodes_nic = config['worker_nodes'].get('network_profile', {}).get('network_interfaces', [])
+    if head_node_nic and worker_nodes_nic:
         return config
 
     location = config['provider']['location']
     resource_group = config['provider']['resource_group']
-    credentials = ServicePrincipalCredentials(client_id=config['provider']['client_id'],
-                                              secret=config['provider']['secret'],
-                                              tenant=config['provider']['tenant'])
-    network_client = NetworkManagementClient(credentials=credentials,
-                                             subscription_id=config['provider']['subscription_id'])
+    auth_path = config['provider']['auth_path']
+    network_client = get_client_from_auth_file(NetworkManagementClient, auth_path=auth_path)
 
     # create VNet
     logger.info('Creating VNet: %s', VNET_NAME)
@@ -174,7 +195,7 @@ def _configure_nic(config):
                                                              parameters=nic_params).result()
 
     config['head_node']['network_profile'] = dict(network_interfaces=[dict(id=nic.id)])
-    config['worker_node']['network_profile'] = dict(network_interfaces=[dict(id=nic.id)])
+    config['worker_nodes']['network_profile'] = dict(network_interfaces=[dict(id=nic.id)])
 
     return config
 
@@ -194,8 +215,24 @@ def generate_ssh_keys(key_name):
     os.chmod(private_key_path, 0o600)
 
     with open(public_key_path, 'w') as public_key_file:
+        # TODO: check if this is the necessary format
         public_key = '%s %s' % (key.get_name(), key.get_base64())
         public_key_file.write(public_key)
     os.chmod(public_key_path, 0o644)
 
-    return private_key_path
+    return public_key, public_key_path, private_key_path
+
+
+def store_azure_auth_file(credentials, auth_path):
+    endpoints = {
+        "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
+        "resourceManagerEndpointUrl": "https://management.azure.com/",
+        "activeDirectoryGraphResourceId": "https://graph.windows.net/",
+        "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/",
+        "galleryEndpointUrl": "https://gallery.azure.com/",
+        "managementEndpointUrl": "https://management.core.windows.net/"
+    }
+
+    endpoints.update(credentials)
+    with open(auth_path, 'w') as f:
+        json.dump(endpoints, f)
