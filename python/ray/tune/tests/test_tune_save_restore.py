24911f5fe6722ac858abe49639a9c36abcac9aef
Tom Havlik <thomashavlik@adams-mac-mini.midcontinentcoontrols.int>
2020-02-07 13:08:59 -0600

    unit test

--- python/ray/tune/tests/test_tune_save_restore.py
+++ python/ray/tune/tests/test_tune_save_restore.py
@@ -11,6 +11,123 @@ from ray.rllib import _register_all
 from ray.tune import Trainable
 
 
+class WarmStartTest(unittest.TestCase):
+    local_mode = True
+
+    class MockTrainable(Trainable):
+        _name = "MockTrainable"
+
+        def _setup(self, config):
+            self.state = {"hi": 1}
+
+        def _train(self):
+            return {"timesteps_this_iter": 1, "done": True}
+
+        def _save(self, checkpoint_dir):
+            checkpoint_path = os.path.join(
+                checkpoint_dir, "checkpoint-{}".format(self._iteration))
+            with open(checkpoint_path, "wb") as f:
+                pickle.dump(self.state, f)
+            return checkpoint_path
+
+        def _restore(self, checkpoint_path):
+            with open(checkpoint_path, "rb") as f:
+                extra_data = pickle.load(f)
+            self.state.update(extra_data)
+
+    def setUp(self):
+        ray.init(num_cpus=1, num_gpus=0, local_mode=self.local_mode)
+
+    def tearDown(self):
+        shutil.rmtree(self.absolute_local_dir, ignore_errors=True)
+        self.absolute_local_dir = None
+        ray.shutdown()
+        # Without this line, test_tune_server.testAddTrial would fail.
+        _register_all()
+
+    def _train(self,
+               exp_name,
+               local_dir,
+               absolute_local_dir,
+               num_samples=1,
+               restore=None):
+        trials = tune.run(
+            self.MockTrainable,
+            num_samples=num_samples,
+            restore=restore,
+            name=exp_name,
+            stop={
+                "training_iteration": 1
+            },
+            checkpoint_freq=1,
+            local_dir=local_dir,
+            config={
+                "env": "CartPole-v0",
+                "log_level": "DEBUG"
+            }).trials
+
+        exp_dir = os.path.join(absolute_local_dir, exp_name)
+
+        checkpoint_paths = []
+
+        for trial in trials:
+            self.assertIsNone(trial.error_file)
+            self.assertEqual(trial.local_dir, exp_dir)
+            checkpoint_path = os.path.join(trial.logdir,
+                                           "checkpoint_1/checkpoint-1")
+            self.assertTrue(os.path.isfile(checkpoint_path))
+            checkpoint_path.append(checkpoint_path)
+
+        return trials, checkpoint_paths
+
+    def testSimpleRestore(self):
+        # test base functionality of restore=<path_to_checkpoint>
+        exp_name = self.prefix + "SimpleRestore"
+        local_dir = "./test_simple_restore"
+        absolute_local_dir = os.path.abspath(local_dir)
+        self.assertFalse(os.path.exists(absolute_local_dir))
+        trials, paths_to_checkpoints = self._train(
+            exp_name,
+            local_dir=local_dir,
+            absolute_local_dir=absolute_local_dir)
+        new_trials, new_paths_to_checkpoints = self._train(
+            exp_name,
+            local_dir=local_dir,
+            absolute_local_dir=absolute_local_dir,
+            restore=paths_to_checkpoints[0])
+        # ensure the restored trial was given a new ID
+        self.assertNotEqual(trials[0].trial_id, new_trials[0].trial_id)
+        # the new trial's name should have restore=<reference_to_restored_trial>
+        self.assertTrue('restore=' in new_paths_to_checkpoints[0])
+        self.assertTrue(trials[0].trial_id in new_paths_to_checkpoints[0])
+
+    def testWarmStartGridSearch(self):
+        exp_name = self.prefix + "WarmStartGridSearch"
+        local_dir = "./warm_start_grid_search"
+        absolute_local_dir = os.path.abspath(local_dir)
+        self.assertFalse(os.path.exists(absolute_local_dir))
+        # train a small number of trials
+        trials, paths_to_checkpoints = self._train(
+            exp_name,
+            local_dir=local_dir,
+            absolute_local_dir=absolute_local_dir,
+            num_samples=4)
+        # start a new set of trials using the checkpoints
+        # as a starting point
+        new_trials, new_checkpoints = self._train(
+            exp_name, restore=tune.grid_search(paths_to_checkpoints))
+        for old_trial, old_checkpoint in zip(trials, paths_to_checkpoints):
+            used = False
+            for new_trial, new_checkpoint in zip(new_trials, new_checkpoints):
+                self.assertNotEqual(old_trial.trial_id, new_trial.trial_id)
+                self.assertTrue('restore=' in new_checkpoint)
+                if old_trial.trial_id in new_checkpoint:
+                    used = True
+                    break
+            self.assertTrue(
+                used, 'checkpoint {} not used in grid_search' % old_checkpoint)
+
+
 class SerialTuneRelativeLocalDirTest(unittest.TestCase):
     local_mode = True
     prefix = "Serial"
