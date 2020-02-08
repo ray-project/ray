13d705518c013ee58f72460de18b926d916b618c
Mehrdad <noreply@github.com>
2020-02-07 18:37:49 -0800

    Add ~/bin to PATH for Bazel

--- ci/travis/build-helper.sh
+++ ci/travis/build-helper.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 prep_build_env() {
+  export PATH="${PATH}:${HOME}/bin"
   if [ "${OSTYPE}" = "msys" ]; then
     export MSYS2_ARG_CONV_EXCL="*"  # Don't let MSYS2 attempt to auto-translate arguments that look like paths
     local latest_python_bin=""
