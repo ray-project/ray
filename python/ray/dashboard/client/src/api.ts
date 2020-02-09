ray-project/ray

commit 0648bd28efa3d86956c2e5029e26dedf289b98da

[xlang] Cross language Python support (#6709)

python/ray/dashboard/dashboard.py
@@ -441,12 +441,6 @@ class NodeStats(threading.Thread):
                         if addr in self._addr_to_actor_id:
                             actor_info = flattened_tree[self._addr_to_actor_id[
                                 addr]]
-                            if "currentTaskFuncDesc" in core_worker_stats:
-                                core_worker_stats[
-                                    "currentTaskFuncDesc"] = list(
-                                        map(
-                                            b64_decode, core_worker_stats[
-                                                "currentTaskFuncDesc"]))
                             format_reply_id(core_worker_stats)
                             actor_info.update(core_worker_stats)
                             actor_info["averageTaskExecutionSpeed"] = round(
@@ -464,8 +458,6 @@ class NodeStats(threading.Thread):
                 caller_id = self._addr_to_actor_id.get(caller_addr, "root")
                 child_to_parent[actor_id] = caller_id
                 infeasible_task["state"] = -1
-                infeasible_task["functionDescriptor"] = list(
-                    map(b64_decode, infeasible_task["functionDescriptor"]))
                 format_reply_id(infeasible_tasks)
                 flattened_tree[actor_id] = infeasible_task
 

doc/source/conf.py
@@ -63,6 +63,19 @@ sys.modules["tensorflow"].VERSION = "9.9.9"
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath("../../python/"))
 
+import ray
+
+
+# Avoid @ray.remote run when doc generating
+def fake_remote(*args, **kwargs):
+    def _inner_wrapper(cls_or_func):
+        return cls_or_func
+
+    return _inner_wrapper
+
+
+ray.remote = fake_remote
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.

java/runtime/src/main/java/org/ray/runtime/functionmanager/FunctionManager.java
@@ -84,8 +84,8 @@ public class FunctionManager {
       SerializedLambda serializedLambda = LambdaUtils.getSerializedLambda(func);
       final String className = serializedLambda.getImplClass().replace('/', '.');
       final String methodName = serializedLambda.getImplMethodName();
-      final String typeDescriptor = serializedLambda.getImplMethodSignature();
-      functionDescriptor = new JavaFunctionDescriptor(className, methodName, typeDescriptor);
+      final String signature = serializedLambda.getImplMethodSignature();
+      functionDescriptor = new JavaFunctionDescriptor(className, methodName, signature);
       RAY_FUNC_CACHE.get().put(func.getClass(), functionDescriptor);
     }
     return getFunction(jobId, functionDescriptor);
@@ -167,13 +167,26 @@ public class FunctionManager {
           }
         }
       }
-      return classFunctions.get(ImmutablePair.of(descriptor.name, descriptor.typeDescriptor));
+      final Pair<String, String> key = ImmutablePair.of(descriptor.name, descriptor.signature);
+      RayFunction func = classFunctions.get(key);
+      if (func == null) {
+        if (classFunctions.containsKey(key)) {
+          throw new RuntimeException(
+                  String.format("RayFunction %s is overloaded, the signature can't be empty.",
+                          descriptor.toString()));
+        } else {
+          throw new RuntimeException(
+                  String.format("RayFunction %s not found", descriptor.toString()));
+        }
+      }
+      return func;
     }
 
     /**
      * Load all functions from a class.
      */
     Map<Pair<String, String>, RayFunction> loadFunctionsForClass(String className) {
+      // If RayFunction is null, the function is overloaded.
       Map<Pair<String, String>, RayFunction> map = new HashMap<>();
       try {
         Class clazz = Class.forName(className, true, classLoader);
@@ -187,10 +200,17 @@ public class FunctionManager {
           final String methodName = e instanceof Method ? e.getName() : CONSTRUCTOR_NAME;
           final Type type =
               e instanceof Method ? Type.getType((Method) e) : Type.getType((Constructor) e);
-          final String typeDescriptor = type.getDescriptor();
+          final String signature = type.getDescriptor();
           RayFunction rayFunction = new RayFunction(e, classLoader,
-              new JavaFunctionDescriptor(className, methodName, typeDescriptor));
-          map.put(ImmutablePair.of(methodName, typeDescriptor), rayFunction);
+              new JavaFunctionDescriptor(className, methodName, signature));
+          map.put(ImmutablePair.of(methodName, signature), rayFunction);
+          // For cross language call java function without signature
+          final Pair<String, String> emptyDescriptor = ImmutablePair.of(methodName, "");
+          if (map.containsKey(emptyDescriptor)) {
+            map.put(emptyDescriptor, null); // Mark this function as overloaded.
+          } else {
+            map.put(emptyDescriptor, rayFunction);
+          }
         }
       } catch (Exception e) {
         throw new RuntimeException("Failed to load functions from class " + className, e);

java/runtime/src/main/java/org/ray/runtime/functionmanager/JavaFunctionDescriptor.java
@@ -19,14 +19,14 @@ public final class JavaFunctionDescriptor implements FunctionDescriptor {
    */
   public final String name;
   /**
-   * Function's type descriptor.
+   * Function's signature.
    */
-  public final String typeDescriptor;
+  public final String signature;
 
-  public JavaFunctionDescriptor(String className, String name, String typeDescriptor) {
+  public JavaFunctionDescriptor(String className, String name, String signature) {
     this.className = className;
     this.name = name;
-    this.typeDescriptor = typeDescriptor;
+    this.signature = signature;
   }
 
   @Override
@@ -45,17 +45,17 @@ public final class JavaFunctionDescriptor implements FunctionDescriptor {
     JavaFunctionDescriptor that = (JavaFunctionDescriptor) o;
     return Objects.equal(className, that.className) &&
         Objects.equal(name, that.name) &&
-        Objects.equal(typeDescriptor, that.typeDescriptor);
+        Objects.equal(signature, that.signature);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hashCode(className, name, typeDescriptor);
+    return Objects.hashCode(className, name, signature);
   }
 
   @Override
   public List<String> toList() {
-    return ImmutableList.of(className, name, typeDescriptor);
+    return ImmutableList.of(className, name, signature);
   }
 
   @Override

java/runtime/src/main/java/org/ray/runtime/functionmanager/PyFunctionDescriptor.java
@@ -28,7 +28,7 @@ public class PyFunctionDescriptor implements FunctionDescriptor {
 
   @Override
   public List<String> toList() {
-    return Arrays.asList(moduleName, className, functionName);
+    return Arrays.asList(moduleName, className, functionName, "" /* function hash */);
   }
 
   @Override

java/runtime/src/main/java/org/ray/runtime/runner/RunManager.java
@@ -146,8 +146,12 @@ public class RunManager {
       e.printStackTrace();
     }
     if (!p.isAlive()) {
-      throw new RuntimeException(
-          String.format("Failed to start %s. Exit code: %d.", name, p.exitValue()));
+      String message = String.format("Failed to start %s. Exit code: %d.",
+          name, p.exitValue());
+      if (rayConfig.redirectOutput) {
+        message += String.format(" Logs are redirected to %s and %s.", stdout, stderr);
+      }
+      throw new RuntimeException(message);
     }
     processes.add(Pair.of(name, p));
     if (LOGGER.isInfoEnabled()) {

java/runtime/src/main/java/org/ray/runtime/task/LocalModeTaskSubmitter.java
@@ -28,6 +28,7 @@ import org.ray.runtime.actor.LocalModeRayActor;
 import org.ray.runtime.context.LocalModeWorkerContext;
 import org.ray.runtime.functionmanager.FunctionDescriptor;
 import org.ray.runtime.functionmanager.JavaFunctionDescriptor;
+import org.ray.runtime.generated.Common;
 import org.ray.runtime.generated.Common.ActorCreationTaskSpec;
 import org.ray.runtime.generated.Common.ActorTaskSpec;
 import org.ray.runtime.generated.Common.Language;
@@ -153,14 +154,20 @@ public class LocalModeTaskSubmitter implements TaskSubmitter {
                                               List<FunctionArg> args) {
     byte[] taskIdBytes = new byte[TaskId.LENGTH];
     new Random().nextBytes(taskIdBytes);
+    List<String> functionDescriptorList = functionDescriptor.toList();
+    Preconditions.checkState(functionDescriptorList.size() >= 3);
     return TaskSpec.newBuilder()
         .setType(taskType)
         .setLanguage(Language.JAVA)
         .setJobId(
             ByteString.copyFrom(runtime.getRayConfig().getJobId().getBytes()))
         .setTaskId(ByteString.copyFrom(taskIdBytes))
-        .addAllFunctionDescriptor(functionDescriptor.toList().stream().map(ByteString::copyFromUtf8)
-            .collect(Collectors.toList()))
+        .setFunctionDescriptor(org.ray.runtime.generated.Common.FunctionDescriptor.newBuilder()
+                .setJavaFunctionDescriptor(
+                        org.ray.runtime.generated.Common.JavaFunctionDescriptor.newBuilder()
+                        .setClassName(functionDescriptorList.get(0))
+                        .setFunctionName(functionDescriptorList.get(1))
+                        .setSignature(functionDescriptorList.get(2))))
         .addAllArgs(args.stream().map(arg -> arg.id != null ? TaskArg.newBuilder()
             .addObjectIds(ByteString.copyFrom(arg.id.getBytes())).build()
             : TaskArg.newBuilder().setData(ByteString.copyFrom(arg.value.data))
@@ -307,9 +314,17 @@ public class LocalModeTaskSubmitter implements TaskSubmitter {
   }
 
   private static JavaFunctionDescriptor getJavaFunctionDescriptor(TaskSpec taskSpec) {
-    List<ByteString> functionDescriptor = taskSpec.getFunctionDescriptorList();
-    return new JavaFunctionDescriptor(functionDescriptor.get(0).toStringUtf8(),
-        functionDescriptor.get(1).toStringUtf8(), functionDescriptor.get(2).toStringUtf8());
+    org.ray.runtime.generated.Common.FunctionDescriptor functionDescriptor =
+            taskSpec.getFunctionDescriptor();
+    if (functionDescriptor.getFunctionDescriptorCase() ==
+            Common.FunctionDescriptor.FunctionDescriptorCase.JAVA_FUNCTION_DESCRIPTOR) {
+      return new JavaFunctionDescriptor(
+              functionDescriptor.getJavaFunctionDescriptor().getClassName(),
+              functionDescriptor.getJavaFunctionDescriptor().getFunctionName(),
+              functionDescriptor.getJavaFunctionDescriptor().getSignature());
+    } else {
+      throw new RuntimeException("Can't build non java function descriptor");
+    }
   }
 
   private static List<FunctionArg> getFunctionArgs(TaskSpec taskSpec) {

java/runtime/src/main/java/org/ray/runtime/task/TaskExecutor.java
@@ -48,11 +48,11 @@ public abstract class TaskExecutor {
 
     List<NativeRayObject> returnObjects = new ArrayList<>();
     ClassLoader oldLoader = Thread.currentThread().getContextClassLoader();
-    // Find the executable object.
-    RayFunction rayFunction = runtime.getFunctionManager()
-        .getFunction(jobId, parseFunctionDescriptor(rayFunctionInfo));
-    Preconditions.checkNotNull(rayFunction);
+    JavaFunctionDescriptor functionDescriptor = parseFunctionDescriptor(rayFunctionInfo);
+    RayFunction rayFunction = null;
     try {
+      // Find the executable object.
+      rayFunction = runtime.getFunctionManager().getFunction(jobId, functionDescriptor);
       Thread.currentThread().setContextClassLoader(rayFunction.classLoader);
       runtime.getWorkerContext().setCurrentClassLoader(rayFunction.classLoader);
 
@@ -91,7 +91,9 @@ public abstract class TaskExecutor {
     } catch (Exception e) {
       LOGGER.error("Error executing task " + taskId, e);
       if (taskType != TaskType.ACTOR_CREATION_TASK) {
-        if (rayFunction.hasReturn()) {
+        boolean hasReturn = rayFunction != null && rayFunction.hasReturn();
+        boolean isCrossLanguage = functionDescriptor.signature.equals("");
+        if (hasReturn || isCrossLanguage) {
           returnObjects.add(ObjectSerializer
               .serialize(new RayTaskException("Error executing task " + taskId, e)));
         }

commit 85cf1577c09b3ea0fb43f76da6d95a0811c8e219

Fixed a bug caused by https://github.com/ray-project/ray/pull/6709

python/ray/dashboard/client/src/api.ts
@@ -103,7 +103,7 @@ export interface RayletInfoResponse {
           actorTitle: string;
           averageTaskExecutionSpeed: number;
           children: RayletInfoResponse["actors"];
-          currentTaskFuncDesc: string[];
+          // currentTaskFuncDesc: string[];
           ipAddress: string;
           isDirectCall: boolean;
           jobId: string;

python/ray/dashboard/client/src/pages/dashboard/logical-view/Actor.tsx
@@ -165,11 +165,11 @@ class Actor extends React.Component<Props & WithStyles<typeof styles>, State> {
             {
               label: "UsedLocalObjectMemory",
               value: actor.usedObjectStoreMemory.toLocaleString()
-            },
-            {
-              label: "Task",
-              value: actor.currentTaskFuncDesc.join(".")
             }
+            // {
+            //   label: "Task",
+            //   value: actor.currentTaskFuncDesc.join(".")
+            // }
           ]
         : [
             {

python/ray/dashboard/dashboard.py
@@ -464,9 +464,8 @@ class NodeStats(threading.Thread):
                 child_to_parent[actor_id] = caller_id
                 task["state"] = -1
                 task["invalidStateType"] = invalid_state_type
-                task["functionDescriptor"] = list(
-                    map(b64_decode, task["functionDescriptor"]))
-                _, task["actorTitle"], _ = task["functionDescriptor"]
+                task["actorTitle"] = task["functionDescriptor"][
+                    "pythonFunctionDescriptor"]["className"]
                 format_reply_id(task)
                 flattened_tree[actor_id] = task
 