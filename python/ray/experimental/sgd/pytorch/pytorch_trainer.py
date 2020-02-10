bea67f6c36faf4a988ce2fa34607beed1b742c8f
Richard Liaw <rliaw@berkeley.edu>
2020-02-10 09:56:41 -0800

    improve safety

--- python/ray/experimental/sgd/pytorch/pytorch_trainer.py
+++ python/ray/experimental/sgd/pytorch/pytorch_trainer.py
@@ -170,6 +170,10 @@ class PyTorchTrainer:
         self.max_replicas = num_replicas
 
         self.use_fp16 = use_fp16
+
+        if apex_args and type(apex_args) is not dict:
+            raise ValueError("apex_args needs to be a dict object.")
+
         self.apex_args = apex_args
         self.temp_dir = tempfile.mkdtemp(prefix="raysgd")
         self._num_failures = 0
