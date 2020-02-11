a7e0ef66eab019231370b19ee232fba6201afb9c
zuochuang.zc <zuochuang.zc@antfin.com>
2020-02-11 12:33:26 +0800

    rename module to ray-rps-facade.

--- deploy/java/ray-rps-k8s-impl/src/main/java/org/ray/deploy/rps/k8s/model/raycluster/RayClusterList.java
+++
@@ -1,7 +0,0 @@
-package org.ray.deploy.rps.k8s.model.raycluster;
-
-import io.fabric8.kubernetes.client.CustomResourceList;
-
-public class RayClusterList extends CustomResourceList<RayCluster> {
-
-}
