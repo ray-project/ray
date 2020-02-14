f3703bafa3b16c80d03f0ce18860e8e5eb468bed
Qing Wang <kingchin1218@126.com>
2020-02-14 13:02:39 +0800

    [Java] Support concurrent actor calls API. (#7022)
    * WIP
    
    Temp change
    
    Attach native thread to jvm
    
    * Fix run mode
    
    * Address comments.

--- src/ray/core_worker/core_worker.h
+++ src/ray/core_worker/core_worker.h
@@ -47,7 +47,7 @@ class CoreWorker : public rpc::CoreWorkerServiceHandler {
       const std::vector<std::shared_ptr<RayObject>> &args,
       const std::vector<ObjectID> &arg_reference_ids,
       const std::vector<ObjectID> &return_ids,
-      std::vector<std::shared_ptr<RayObject>> *results)>;
+      std::vector<std::shared_ptr<RayObject>> *results, const ray::WorkerID &worker_id)>;
 
  public:
   /// Construct a CoreWorker instance.
