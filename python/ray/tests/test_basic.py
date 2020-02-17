8000e5ee1ac2c6c0c36005dfd6dac97264cb077c
Siyuan <suquark@gmail.com>
2020-02-17 15:04:57 -0800

    further lint

--- python/ray/tests/test_basic.py
+++ python/ray/tests/test_basic.py
@@ -458,7 +458,7 @@ def test_reducer_override_no_reference_cycle(ray_start_regular):
     # bpo-39492: reducer_override used to induce a spurious reference cycle
     # inside the Pickler object, that could prevent all serialized objects
     # from being garbage-collected without explicity invoking gc.collect.
-    f = lambda: 4669201609102990671853203821578
+    f = lambda: 4669201609102990671853203821578  # pylint: disable=E731
 
     wr = weakref.ref(f)
 
