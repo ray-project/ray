3cc188cc397f78510585d74b6c4f639f19833ee4
simon-mo <xmo@berkeley.edu>
2020-02-17 18:04:36 -0800

    update version key 0.8.2

--- python/ray/__init__.py
+++ python/ray/__init__.py
@@ -99,7 +99,7 @@ from ray.cross_language import java_function, java_actor_class  # noqa: E402
 from ray import util  # noqa: E402
 
 # Ray version string.
-__version__ = "0.9.0.dev0"
+__version__ = "0.8.2"
 
 __all__ = [
     "jobs",
