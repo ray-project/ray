e4405c07d9133cc7c4366bbc8af441e711c21b96
Edward Oakes <ed.nmi.oakes@gmail.com>
2020-02-19 11:18:40 -0800

    import order

--- python/ray/tests/test_reference_counting.py
+++ python/ray/tests/test_reference_counting.py
@@ -1,14 +1,16 @@
 # coding: utf-8
-import os
-import json
 import copy
-import tempfile
-import numpy as np
-import time
-import pytest
-import logging
-import uuid
 import gc
+import json
+import logging
+import os
+import tempfile
+import time
+import uuid
+
+import numpy as np
+
+import pytest
 
 import ray
 import ray.cluster_utils
