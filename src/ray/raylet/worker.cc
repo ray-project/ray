1ae330a33cbbca509d6bfe11d1310fea9116fd6a
mehrdadn <mehrdadn@users.noreply.github.com>
2020-02-18 17:44:46 -0800

    Use Process instead of pid_t (round 2) (#6882)
    * Revert "Revert "Use Boost.Process instead of pid_t (#6510)" (#6909)"
    
    This reverts commit bde575b8dd5792e67564fceb9e72228a76e2a44f.
    
    * Process wrapper, using Boost.Process on Windows
    
    - Reverts bde575b8dd5792e67564fceb9e72228a76e2a44f.
    - Re-applies fb8e3615d59067679fba9daa6840fff3a2d46bbc after some refactoring.
    
    * Remove Boost.Process dependency
    
    * Don't open /proc file on Linux
    
    * Change FATAL to ERROR and modify error message when process doesn't exist

--- src/ray/raylet/worker.cc
+++ src/ray/raylet/worker.cc
@@ -12,11 +12,10 @@ namespace ray {
 namespace raylet {
 
 /// A constructor responsible for initializing the state of a worker.
-Worker::Worker(const WorkerID &worker_id, pid_t pid, const Language &language, int port,
+Worker::Worker(const WorkerID &worker_id, const Language &language, int port,
                std::shared_ptr<LocalClientConnection> connection,
                rpc::ClientCallManager &client_call_manager)
     : worker_id_(worker_id),
-      pid_(pid),
       language_(language),
       port_(port),
       connection_(connection),
@@ -42,7 +41,12 @@ bool Worker::IsBlocked() const { return blocked_; }
 
 WorkerID Worker::WorkerId() const { return worker_id_; }
 
-pid_t Worker::Pid() const { return pid_; }
+Process Worker::GetProcess() const { return proc_; }
+
+void Worker::SetProcess(Process proc) {
+  RAY_CHECK(proc_.IsNull());  // this procedure should not be called multiple times
+  proc_ = std::move(proc);
+}
 
 Language Worker::GetLanguage() const { return language_; }
 
