334bf3583deedc7a7f67691e75a649be0818711e
Eric Liang <ekhliang@gmail.com>
2020-02-18 16:39:45 -0800

    speed up

--- rllib/agents/a3c/tests/test_a2c.py
+++ rllib/agents/a3c/tests/test_a2c.py
@@ -1,4 +1,3 @@
-import numpy as np
 import unittest
 
 import ray
@@ -6,6 +5,8 @@ from ray.rllib.agents.a3c import a2c_workflow
 
 
 class TestA2C(unittest.TestCase):
+    """Sanity tests for A2CWorkflow."""
+
     def setUp(self):
         ray.init()
 
@@ -13,8 +14,17 @@ class TestA2C(unittest.TestCase):
         ray.shutdown()
 
     def test_a2c_workflow(ray_start_regular):
-        """Sanity test for A2CWorkflow."""
-        trainer = a2c_workflow.A2CWorkflow(env="CartPole-v0")
+        trainer = a2c_workflow.A2CWorkflow(
+            env="CartPole-v0", config={"min_iter_time_s": 0})
+        assert isinstance(trainer.train(), dict)
+
+    def test_a2c_workflow_microbatch(ray_start_regular):
+        trainer = a2c_workflow.A2CWorkflow(
+            env="CartPole-v0",
+            config={
+                "min_iter_time_s": 0,
+                "microbatch_size": 10
+            })
         assert isinstance(trainer.train(), dict)
 
 
