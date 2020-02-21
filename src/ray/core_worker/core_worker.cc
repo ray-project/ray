d15f9a381970842855dae003f5925013b0740529
Edward Oakes <ed.nmi.oakes@gmail.com>
2020-02-21 13:19:23 -0800

    remove logs

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -481,10 +481,8 @@ Status CoreWorker::Get(const std::vector<ObjectID> &ids, const int64_t timeout_m
   auto start_time = current_time_ms();
 
   if (!memory_object_ids.empty()) {
-    RAY_LOG(ERROR) << "doing in-memory get";
     RAY_RETURN_NOT_OK(memory_store_->Get(memory_object_ids, timeout_ms, worker_context_,
                                          &result_map, &got_exception));
-    RAY_LOG(ERROR) << "in-memory get done";
   }
 
   if (!got_exception) {
@@ -493,7 +491,7 @@ Status CoreWorker::Get(const std::vector<ObjectID> &ids, const int64_t timeout_m
     // the transport type again and return them for the original direct call ids.
     for (const auto &pair : result_map) {
       if (pair.second->IsInPlasmaError()) {
-        RAY_LOG(ERROR) << pair.first << " in plasma, doing fetch-and-get";
+        RAY_LOG(DEBUG) << pair.first << " in plasma, doing fetch-and-get";
         plasma_object_ids.insert(pair.first);
       }
     }
