b304c3df18128533ae7ce59d94e62e558fa5c954
Edward Oakes <ed.nmi.oakes@gmail.com>
2020-02-24 17:13:51 -0800

    lint

--- python/ray/tests/test_basic.py
+++ python/ray/tests/test_basic.py
@@ -1733,7 +1733,12 @@ def test_wait(ray_start_regular):
 
 def test_duplicate_args(ray_start_regular):
     @ray.remote
-    def f(arg1, arg2, arg1_duplicate, kwarg1=None, kwarg2=None, kwarg1_duplicate=None):
+    def f(arg1,
+          arg2,
+          arg1_duplicate,
+          kwarg1=None,
+          kwarg2=None,
+          kwarg1_duplicate=None):
         assert arg1 == kwarg1
         assert arg1 != arg2
         assert arg1 == arg1_duplicate
@@ -1743,12 +1748,16 @@ def test_duplicate_args(ray_start_regular):
     # Test by-value arguments.
     arg1 = [1]
     arg2 = [2]
-    ray.get(f.remote(arg1, arg2, arg1, kwarg1=arg1, kwarg2=arg2, kwarg1_duplicate=arg1))
+    ray.get(
+        f.remote(
+            arg1, arg2, arg1, kwarg1=arg1, kwarg2=arg2, kwarg1_duplicate=arg1))
 
     # Test by-reference arguments.
     arg1 = ray.put([1])
     arg2 = ray.put([2])
-    ray.get(f.remote(arg1, arg2, arg1, kwarg1=arg1, kwarg2=arg2, kwarg1_duplicate=arg1))
+    ray.get(
+        f.remote(
+            arg1, arg2, arg1, kwarg1=arg1, kwarg2=arg2, kwarg1_duplicate=arg1))
 
 
 if __name__ == "__main__":
