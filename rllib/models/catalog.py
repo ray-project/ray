7891d472e43f7493172184daa5645b9b4ecdc57e
mwbrulhardt <matthew.brulhardt@stonybrook.edu>
2020-02-25 17:07:46 -0500

    Cleaned the code to be more readable.

--- rllib/models/catalog.py
+++ rllib/models/catalog.py
@@ -229,8 +229,8 @@ class ModelCatalog:
                 else:
                     all_discrete = False
                     size += np.product(action_space.spaces[i].shape)
-            return (tf.int64
-                    if all_discrete else tf.float32, (None, int(size)))
+            size = int(size)
+            return (tf.int64 if all_discrete else tf.float32, (None, size))
         elif isinstance(action_space, gym.spaces.Dict):
             raise NotImplementedError(
                 "Dict action spaces are not supported, consider using "
