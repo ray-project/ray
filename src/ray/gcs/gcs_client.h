3bc72405d71458053af166febab4f3127de8531d
灵洵 <fengbin.ffb@antfin.com>
2020-02-26 15:44:33 +0800

    rm GcsClient Reconnect function

--- src/ray/gcs/gcs_client.h
+++ src/ray/gcs/gcs_client.h
@@ -60,9 +60,6 @@ class GcsClient : public std::enable_shared_from_this<GcsClient> {
   /// Disconnect with GCS Service. Non-thread safe.
   virtual void Disconnect() = 0;
 
-  /// Reconnect to GCS Service. Non-thread safe.
-  void Reconnect(){};
-
   /// Return client information for debug.
   virtual std::string DebugString() const { return ""; }
 
