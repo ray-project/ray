6b54dc4efbeb7c9a68ecbc7d2716ac038ccaa9e0
sven1977 <svenmika1977@gmail.com>
2020-02-27 11:50:50 +0100

    Revert test_checkpoint_restore.py (debugging) changes.

--- rllib/tests/test_checkpoint_restore.py
+++ rllib/tests/test_checkpoint_restore.py
@@ -146,13 +146,13 @@ def test_export(algo_name, failures):
 if __name__ == "__main__":
     failures = []
     for use_object_store in [False, True]:
-        for name in ["DDPG"]:
+        for name in [
+                "SAC", "ES", "DQN", "DDPG", "PPO", "A3C", "APEX_DDPG", "ARS"
+        ]:
             test_ckpt_restore(use_object_store, name, failures)
 
     assert not failures, failures
     print("All checkpoint restore tests passed!")
-
-    quit()
 
     failures = []
     for name in ["SAC", "DQN", "DDPG", "PPO", "A3C"]:
