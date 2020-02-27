52d7aad40cb7d11df62306602aefedf6753d6298
Edward Oakes <ed.nmi.oakes@gmail.com>
2020-02-26 16:57:02 -0800

    Move plasma retry logic into plasma store provider (#7328)

--- python/ray/tests/test_reference_counting.py
+++ python/ray/tests/test_reference_counting.py
@@ -25,6 +25,7 @@ logger = logging.getLogger(__name__)
 def one_worker_100MiB(request):
     config = json.dumps({
         "distributed_ref_counting_enabled": 1,
+        "object_store_full_max_retries": 1,
     })
     yield ray.init(
         num_cpus=1,
