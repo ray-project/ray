88ba871ee1f97348e5925cbe17dd130af1c6220d
Ruofan Kong <rukon@MacBook-New.local>
2020-02-26 17:22:37 -0800

    The convergence tests use Pendulum.

--- python/ray/rllib/agents/sac/tests/test_sac.py
+++ python/ray/rllib/agents/sac/tests/test_sac.py
@@ -31,8 +31,6 @@ def config(env_id):
     config["env_config"] = dict(env_id=env_id)
     if env_id == "Pendulum-v0":
         config["timesteps_per_iteration"] = 100
-    elif env_id == "LunarLander-v2":
-        config["timesteps_per_iteration"] = 1000
     config["num_workers"] = 8
     config["num_envs_per_worker"] = 8
     return config
@@ -45,13 +43,13 @@ def trainer(config):
 
 @pytest.mark.parametrize(
     "env_id, threshold", [
-        ('LunarLander-v2', -750),
+        ("Pendulum-v0", -750),
     ]
 )
 @pytest.mark.usefixtures("ray_env")
 def test_convergence(trainer, env_id, threshold):
     mean_episode_reward = -float("inf")
-    for i in range(300):
+    for i in range(250):
         result = trainer.train()
         mean_episode_reward = result["episode_reward_mean"]
         print(f"{i}: {mean_episode_reward}")
