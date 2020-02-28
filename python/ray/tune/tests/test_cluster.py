1fea65deb3fe2b33348b8211b52b4d6916fa21fb
Richard Liaw <rliaw@berkeley.edu>
2020-02-27 18:17:39 -0800

    Apply suggestions from code review

--- python/ray/tune/tests/test_cluster.py
+++ python/ray/tune/tests/test_cluster.py
@@ -303,7 +303,6 @@ def test_trial_migration(start_connected_emptyhead_cluster, trainable_id):
         runner.step()
 
 
-@pytest.mark.skip(reason="Fault tolerance tests are failing.")
 @pytest.mark.parametrize("trainable_id", ["__fake", "__fake_durable"])
 def test_trial_requeue(start_connected_emptyhead_cluster, trainable_id):
     """Removing a node in full cluster causes Trial to be requeued."""
