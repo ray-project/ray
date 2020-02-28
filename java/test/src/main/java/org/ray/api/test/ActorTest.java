d4c09b09e576268d5a8fd9e7ed47cffe647aec8a
jovany-wang <kingchin1218@126.com>
2020-02-28 14:16:07 +0800

    Refine

--- java/test/src/main/java/org/ray/api/test/ActorTest.java
+++ java/test/src/main/java/org/ray/api/test/ActorTest.java
@@ -129,6 +129,8 @@ public class ActorTest extends BaseTest {
 
   public void testUnreconstructableActorObject() throws InterruptedException {
     TestUtils.skipTestUnderSingleProcess();
+    // TODO (kfstorm): This should be supported by direct actor call.
+    TestUtils.skipTestIfDirectActorCallEnabled();
     RayActor<Counter> counter = Ray.createActor(Counter::new, 100);
     // Call an actor method.
     RayObject value = Ray.call(Counter::getValue, counter);
