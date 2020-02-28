ce9551054b613e817a9d83d7020bea5cead36fed
sven1977 <svenmika1977@gmail.com>
2020-02-28 13:41:30 +0100

    WIP.

--- rllib/agents/sac/tests/test_sac.py
+++ rllib/agents/sac/tests/test_sac.py
@@ -1,5 +1,6 @@
 import unittest
 
+import ray
 import ray.rllib.agents.sac as sac
 from ray.rllib.utils.framework import try_import_tf
 
@@ -8,18 +9,21 @@ tf = try_import_tf()
 
 class TestSAC(unittest.TestCase):
     def test_sac_compilation(self):
-        """Test whether an SACTrainer can be built with both frameworks."""
+        """Test whether an SACTrainer can be built with all frameworks."""
+        ray.init()
         config = sac.DEFAULT_CONFIG.copy()
         config["num_workers"] = 0  # Run locally.
         num_iterations = 2
 
         # eager (discrete and cont. actions).
         for fw in ["eager", "tf", "torch"]:
+            print("framework={}".format(fw))
             if fw == "torch":
                 continue
             config["eager"] = fw == "eager"
             config["use_pytorch"] = fw == "torch"
-            for env in ["Pendulum-v0", "CartPole-v0"]:
+            for env in ["CartPole-v0", "Pendulum-v0", ]:
+                print("Env={}".format(env))
                 trainer = sac.SACTrainer(config=config, env=env)
                 for i in range(num_iterations):
                     results = trainer.train()
