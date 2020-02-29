625ea7f73e28b75577ceade3f19ecfaba72e933b
Allen Yin <allenyin@anyscale.io>
2020-02-28 19:35:03 -0800

    Capture autoscaler output

--- python/ray/autoscaler/updater.py
+++ python/ray/autoscaler/updater.py
@@ -49,6 +49,7 @@ class KubernetesCommandRunner:
             allocate_tty=False,
             exit_on_fail=False,
             port_forward=None):
+
         if cmd and port_forward:
             raise Exception(
                 "exec with Kubernetes can't forward ports and execute"
@@ -82,7 +83,8 @@ class KubernetesCommandRunner:
                 "--",
             ] + with_interactive(cmd)
             try:
-                self.process_runner.check_call(" ".join(final_cmd), shell=True)
+                return self.process_runner.check_output(" ".join(final_cmd), shell=True)
+
             except subprocess.CalledProcessError:
                 if exit_on_fail:
                     quoted_cmd = " ".join(final_cmd[:-1] +
@@ -260,7 +262,8 @@ class SSHCommandRunner:
             # still create an interactive shell in some ssh versions.
             final_cmd.append("while true; do sleep 86400; done")
         try:
-            self.process_runner.check_call(final_cmd)
+            return self.process_runner.check_output(final_cmd)
+
         except subprocess.CalledProcessError:
             if exit_on_fail:
                 quoted_cmd = " ".join(final_cmd[:-1] + [quote(final_cmd[-1])])
