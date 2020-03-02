8f24290d12309f4bcfb37ce9611a3c9411c0febe
Eric Liang <ekhliang@gmail.com>
2020-03-02 15:16:37 -0800

    [rllib] Support multi-agent training in pipeline impls, add easy flag to enable (#7338)

--- rllib/agents/pg/pg.py
+++ rllib/agents/pg/pg.py
@@ -1,6 +1,8 @@
 from ray.rllib.agents.trainer import with_common_config
 from ray.rllib.agents.trainer_template import build_trainer
 from ray.rllib.agents.pg.pg_tf_policy import PGTFPolicy
+from ray.rllib.utils.experimental_dsl import (
+    ParallelRollouts, ConcatBatches, TrainOneStep, StandardMetricsReporting)
 
 # yapf: disable
 # __sphinx_doc_begin__
@@ -22,8 +24,26 @@ def get_policy_class(config):
         return PGTFPolicy
 
 
+# Experimental pipeline-based impl; enable with "use_pipeline_impl": True.
+def training_pipeline(workers, config):
+    # Collects experiences in parallel from multiple RolloutWorker actors.
+    rollouts = ParallelRollouts(workers, mode="bulk_sync")
+
+    # Combine experiences batches until we hit `train_batch_size` in size.
+    # Then, train the policy on those experiences and update the workers.
+    train_op = rollouts \
+        .combine(ConcatBatches(
+            min_batch_size=config["train_batch_size"])) \
+        .for_each(TrainOneStep(workers))
+
+    # Add on the standard episode reward, etc. metrics reporting. This returns
+    # a LocalIterator[metrics_dict] representing metrics for each train step.
+    return StandardMetricsReporting(train_op, workers, config)
+
+
 PGTrainer = build_trainer(
     name="PG",
     default_config=DEFAULT_CONFIG,
     default_policy=PGTFPolicy,
-    get_policy_class=get_policy_class)
+    get_policy_class=get_policy_class,
+    training_pipeline=training_pipeline)
--- rllib/agents/registry.py
+++ rllib/agents/registry.py
@@ -100,21 +100,6 @@ def _import_marwil():
     return marwil.MARWILTrainer
 
 
-def _import_a2c_pipeline():
-    from ray.rllib.agents import a3c
-    return a3c.A2CPipeline
-
-
-def _import_a3c_pipeline():
-    from ray.rllib.agents import a3c
-    return a3c.A3CPipeline
-
-
-def _import_pg_pipeline():
-    from ray.rllib.agents import pg
-    return pg.PGPipeline
-
-
 ALGORITHMS = {
     "SAC": _import_sac,
     "DDPG": _import_ddpg,
@@ -135,11 +120,6 @@ ALGORITHMS = {
     "APPO": _import_appo,
     "DDPPO": _import_ddppo,
     "MARWIL": _import_marwil,
-
-    # Experimental pipeline-based impls.
-    "A2C_pl": _import_a2c_pipeline,
-    "A3C_pl": _import_a3c_pipeline,
-    "PG_pl": _import_pg_pipeline,
 }
 
 
