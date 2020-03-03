dce043eed5566b4546b44d731b4ff10b15fcb84a
Edward Oakes <ed.nmi.oakes@gmail.com>
2020-03-02 20:24:47 -0800

    remove irrelevant TODO

--- python/ray/serialization.py
+++ python/ray/serialization.py
@@ -194,7 +194,6 @@ class SerializationContext:
                 outer_id = context.get_outer_object_id()
                 # outer_id is None in the case that this ObjectID was closed
                 # over in a function or pickled directly using pickle.dumps().
-                # TODO(edoakes): make sure these objects are pinned forever.
                 if outer_id is None:
                     outer_id = ray.ObjectID.nil()
                 worker.core_worker.deserialize_and_register_object_id(
