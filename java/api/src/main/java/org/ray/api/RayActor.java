75ac18e91ca215d589889bcab051cc74f284e1cc
Hao Chen <chenh1024@gmail.com>
2020-03-04 10:53:14 +0800

    comment

--- java/api/src/main/java/org/ray/api/RayActor.java
+++ java/api/src/main/java/org/ray/api/RayActor.java
@@ -3,7 +3,26 @@ package org.ray.api;
 import org.ray.api.id.ActorId;
 
 /**
- * A handle to an actor.
+ * A handle to an actor. <p>
+ *
+ * A handle can be used to invoke a remote actor method, with the {@code "call"} method. For
+ * example:
+ * <pre> {@code
+ * class MyActor {
+ *   public int echo(int x) {
+ *     return x;
+ *   }
+ * }
+ * // Create an actor, and get a handle.
+ * RayActor<MyActor> myActor = Ray.createActor(RayActor::new);
+ * // Call the `echo` method remotely.
+ * RayObject<Integer> result = myActor.call(MyActor::echo, 1);
+ * // Get the result of the remote `echo` method.
+ * Assert.assertEqual(result.get(), 1);
+ * }</pre>
+ *
+ * Note, the {@code "call"} method is defined in {@link ActorCall} interface, with multiple
+ * overloaded versions.
  *
  * @param <A> The type of the concrete actor class.
  */
