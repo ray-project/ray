be06d70b23babd57752f6f60ba87b834ce4bde83
chaokunyang <shawn.ck.yang@gmail.com>
2020-03-05 21:22:45 +0800

    fix class name

--- streaming/python/tests/test_cross_lang_stream.py
+++ streaming/python/tests/test_cross_lang_stream.py
@@ -17,12 +17,11 @@ def test_data_stream():
     if not ray.is_initialized():
         ray.init(load_code_from_local=True, include_java=True)
     ctx = StreamingContext.Builder().build()
-    s = ctx.from_values("a", "b", "c")
     stream = ctx.from_values("a", "b", "c") \
         .key_by(lambda x: x) \
         .as_java() \
-        .map("org.ray.streaming.runtime.demo.CrossLangStreamTest.Mapper1") \
-        .filter("org.ray.streaming.runtime.demo.CrossLangStreamTest.Filter1") \
+        .map("org.ray.streaming.runtime.demo.CrossLangStreamTest$Mapper1") \
+        .filter("org.ray.streaming.runtime.demo.CrossLangStreamTest$Filter1") \
         .as_python() \
         .sink(print)
     ctx.submit("cross_lang_word_count")
