239cc60ca88487b88c24408e3386f7a71d26836e
Eric Liang <ekhliang@gmail.com>
2020-03-07 19:58:56 -0800

    fix tests labels

--- rllib/tests/test_exec_api.py
+++ rllib/tests/test_exec_api.py
@@ -42,13 +42,14 @@ class TestDistributedExecution(unittest.TestCase):
             })
         res1 = trainer.train()
         checkpoint = trainer.save()
-        res2 = trainer.train()
+        for _ in range(2):
+            res2 = trainer.train()
         assert res2["timesteps_total"] > res1["timesteps_total"], (res1, res2)
         trainer.restore(checkpoint)
 
         # Should restore the timesteps counter to the same as res2.
         res3 = trainer.train()
-        assert res3["timesteps_total"] == res2["timesteps_total"], (res2, res3)
+        assert res3["timesteps_total"] < res2["timesteps_total"], (res2, res3)
 
 
 if __name__ == "__main__":
