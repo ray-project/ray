f7dde982732ba2cd7176224c959e5f5036ee0a84
Richard Liaw <rliaw@berkeley.edu>
2020-03-10 19:00:10 -0700

    experimental

--- python/ray/util/sgd/torch/__init__.py
+++ python/ray/util/sgd/torch/__init__.py
@@ -8,10 +8,10 @@ TrainingOperator = None
 try:
     import torch  # noqa: F401
 
-    from ray.util.sgd.torch.torch_trainer import (TorchTrainer, TorchTrainable)
+    from ray.util.sgd.torch.torch_trainer import TorchTrainer
 
     from ray.util.sgd.torch.training_operator import TrainingOperator
 
-    __all__ = ["TorchTrainer", "TorchTrainable", "TrainingOperator"]
+    __all__ = ["TorchTrainer", "TrainingOperator"]
 except ImportError:
     logger.warning("PyTorch not found. TorchTrainer will not be available")
