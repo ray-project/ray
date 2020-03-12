d530e97f78e7fc560d8ec770ed179832b6cfec0f
Ian Rodney <ian.rodney@gmail.com>
2020-03-11 22:04:46 -0700

    Modifying script and adding docs

--- ci/travis/check_symbol_collisions.py
+++ ci/travis/check_symbol_collisions.py
@@ -3,30 +3,23 @@ This script ensures that various libraries do not conflict with ray by
 trying to import both libraries in both orders.
 A specific example is that importing ray after pyarrow causes a Segfault.
 """
-import sys
+import pytest
 import subprocess
-import importlib
 
 TESTED_LIBRARIES = ["pyarrow"]
 
 
-def try_imports(library1, library2):
-    program_name = sys.argv[0]
-    return_info = subprocess.run(["python", program_name, library1, library2])
-    if return_info.returncode != 0:
-        print("Importing {} before {} caused an error".format(
-            library1, library2))
-    return return_info.returncode
+def test_imports():
+    def try_imports(library1, library2):
+        return_info = subprocess.run([
+            "python3", "-c", "import {}; import {}".format(library1, library2)
+        ])
+        if return_info.returncode != 0:
+            print("Importing {} before {} caused an error".format(
+                library1, library2))
+        return return_info.returncode
 
-
-if len(sys.argv) == 1:
     final_exit_code = 0
     for library in TESTED_LIBRARIES:
-        final_exit_code += try_imports("ray", library)
-        final_exit_code += try_imports(library, "ray")
-    sys.exit(final_exit_code)
-
-if len(sys.argv) == 3:
-    importlib.import_module(sys.argv[1])
-    importlib.import_module(sys.argv[2])
-    sys.exit(0)
+        assert try_imports("ray", library) == 0
+        assert try_imports(library, "ray") == 0
