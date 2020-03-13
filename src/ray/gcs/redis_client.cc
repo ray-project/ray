9bb6ee7c752eb1515dd963bbfcf8921610b71208
micafan <550435771@qq.com>
2020-03-13 17:11:35 +0800

    add RedisClient

---
+++ src/ray/gcs/redis_client.cc
@@ -0,0 +1,147 @@
+#include "ray/gcs/redis_client.h"
+
+#include <unistd.h>
+#include "ray/common/ray_config.h"
+#include "ray/gcs/redis_context.h"
+
+namespace ray {
+
+namespace gcs {
+
+static void GetRedisShards(redisContext *context, std::vector<std::string> *addresses,
+                           std::vector<int> *ports) {
+  // Get the total number of Redis shards in the system.
+  int num_attempts = 0;
+  redisReply *reply = nullptr;
+  while (num_attempts < RayConfig::instance().redis_db_connect_retries()) {
+    // Try to read the number of Redis shards from the primary shard. If the
+    // entry is present, exit.
+    reply = reinterpret_cast<redisReply *>(redisCommand(context, "GET NumRedisShards"));
+    if (reply->type != REDIS_REPLY_NIL) {
+      break;
+    }
+
+    // Sleep for a little, and try again if the entry isn't there yet. */
+    freeReplyObject(reply);
+    usleep(RayConfig::instance().redis_db_connect_wait_milliseconds() * 1000);
+    num_attempts++;
+  }
+  RAY_CHECK(num_attempts < RayConfig::instance().redis_db_connect_retries())
+      << "No entry found for NumRedisShards";
+  RAY_CHECK(reply->type == REDIS_REPLY_STRING)
+      << "Expected string, found Redis type " << reply->type << " for NumRedisShards";
+  int num_redis_shards = atoi(reply->str);
+  RAY_CHECK(num_redis_shards >= 1) << "Expected at least one Redis shard, "
+                                   << "found " << num_redis_shards;
+  freeReplyObject(reply);
+
+  // Get the addresses of all of the Redis shards.
+  num_attempts = 0;
+  while (num_attempts < RayConfig::instance().redis_db_connect_retries()) {
+    // Try to read the Redis shard locations from the primary shard. If we find
+    // that all of them are present, exit.
+    reply =
+        reinterpret_cast<redisReply *>(redisCommand(context, "LRANGE RedisShards 0 -1"));
+    if (static_cast<int>(reply->elements) == num_redis_shards) {
+      break;
+    }
+
+    // Sleep for a little, and try again if not all Redis shard addresses have
+    // been added yet.
+    freeReplyObject(reply);
+    usleep(RayConfig::instance().redis_db_connect_wait_milliseconds() * 1000);
+    num_attempts++;
+  }
+  RAY_CHECK(num_attempts < RayConfig::instance().redis_db_connect_retries())
+      << "Expected " << num_redis_shards << " Redis shard addresses, found "
+      << reply->elements;
+
+  // Parse the Redis shard addresses.
+  for (size_t i = 0; i < reply->elements; ++i) {
+    // Parse the shard addresses and ports.
+    RAY_CHECK(reply->element[i]->type == REDIS_REPLY_STRING);
+    std::string addr;
+    std::stringstream ss(reply->element[i]->str);
+    getline(ss, addr, ':');
+    addresses->emplace_back(std::move(addr));
+    int port;
+    ss >> port;
+    ports->emplace_back(port);
+  }
+  freeReplyObject(reply);
+}
+
+RedisClient::RedisClient(const RedisClientOptions &options)
+    : options_(options) {}
+
+Status RedisClient::Connect(boost::asio::io_service &io_service) {
+  RAY_CHECK(!is_connected_);
+
+  if (options_.server_ip_.empty()) {
+    RAY_LOG(ERROR) << "Failed to connect, redis server address is empty.";
+    return Status::Invalid("Redis server address is invalid!");
+  }
+
+  primary_context_ = std::make_shared<RedisContext>(io_service);
+
+  RAY_CHECK_OK(primary_context_->Connect(options_.server_ip_, options_.server_port_,
+                                         /*sharding=*/true,
+                                         /*password=*/options_.password_));
+
+  if (!options_.is_test_client_) {
+    // Moving sharding into constructor defaultly means that sharding = true.
+    // This design decision may worth a look.
+    std::vector<std::string> addresses;
+    std::vector<int> ports;
+    GetRedisShards(primary_context_->sync_context(), &addresses, &ports);
+    if (addresses.empty()) {
+      RAY_CHECK(ports.empty());
+      addresses.push_back(options_.server_ip_);
+      ports.push_back(options_.server_port_);
+    }
+
+    for (size_t i = 0; i < addresses.size(); ++i) {
+      // Populate shard_contexts.
+      shard_contexts_.push_back(std::make_shared<RedisContext>(io_service));
+      RAY_CHECK_OK(shard_contexts_[i]->Connect(addresses[i], ports[i], /*sharding=*/true,
+                                               /*password=*/options_.password_));
+    }
+  } else {
+    shard_contexts_.push_back(std::make_shared<RedisContext>(io_service));
+    RAY_CHECK_OK(shard_contexts_[0]->Connect(options_.server_ip_, options_.server_port_,
+                                             /*sharding=*/true,
+                                             /*password=*/options_.password_));
+  }
+
+  Attach(io_service);
+
+  is_connected_ = true;
+  RAY_LOG(DEBUG) << "RedisClient connected.";
+
+  return Status::OK();
+}
+
+void RedisClient::Attach(boost::asio::io_service &io_service) {
+  // Take care of sharding contexts.
+  RAY_CHECK(shard_asio_async_clients_.empty()) << "Attach shall be called only once";
+  for (std::shared_ptr<RedisContext> context : shard_contexts_) {
+    shard_asio_async_clients_.emplace_back(
+        new RedisAsioClient(io_service, context->async_context()));
+    shard_asio_subscribe_clients_.emplace_back(
+        new RedisAsioClient(io_service, context->subscribe_context()));
+  }
+  asio_async_auxiliary_client_.reset(
+      new RedisAsioClient(io_service, primary_context_->async_context()));
+  asio_subscribe_auxiliary_client_.reset(
+      new RedisAsioClient(io_service, primary_context_->subscribe_context()));
+}
+
+void RedisClient::Disconnect() {
+  RAY_CHECK(is_connected_);
+  is_connected_ = false;
+  RAY_LOG(DEBUG) << "RedisClient disconnected.";
+}
+
+}  // namespace gcs
+
+}  // namespace ray
---
+++ src/ray/gcs/redis_client.h
@@ -0,0 +1,85 @@
+#ifndef RAY_GCS_REDIS_CLIENT_H
+#define RAY_GCS_REDIS_CLIENT_H
+
+#include <map>
+#include <string>
+
+#include "ray/common/id.h"
+#include "ray/common/status.h"
+#include "ray/gcs/asio.h"
+#include "ray/util/logging.h"
+
+namespace ray {
+
+namespace gcs {
+
+class RedisContext;
+
+class RedisClientOptions {
+ public:
+  RedisClientOptions(const std::string &ip, int port, const std::string &password,
+                     bool is_test_client = false)
+      : server_ip_(ip),
+        server_port_(port),
+        password_(password),
+        is_test_client_(is_test_client) {}
+
+    // Redis server address
+  std::string server_ip_;
+  int server_port_;
+
+  // Password of Redis.
+  std::string password_;
+
+  // Whether this client is used for tests.
+  bool is_test_client_{false};
+};
+
+/// \class RedisClient
+/// Implementation of 
+class RedisClient {
+ public:
+  RedisClient(const RedisClientOptions &options);
+
+  virtual ~RedisClient();
+
+  /// Connect to Redis. Non-thread safe.
+  /// Call this function before calling other functions.
+  ///
+  /// \param io_service The event loop for this client.
+  /// Must be single-threaded io_service (get more information from RedisAsioClient).
+  /// \return Status
+  Status Connect(boost::asio::io_service &io_service);
+
+  /// Disconnect with Redis. Non-thread safe.
+  void Disconnect();
+
+  std::vector<std::shared_ptr<RedisContext>> GetShardContexts() { return shard_contexts_; }
+
+  std::shared_ptr<RedisContext> GetPrimaryContext() { return primary_context_; }
+
+ protected:
+  /// Attach this client to an asio event loop. Note that only
+  /// one event loop should be attached at a time.
+  void Attach(boost::asio::io_service &io_service);
+
+  RedisClientOptions options_;
+
+  /// Whether this client is connected to redis.
+  bool is_connected_{false};
+
+  // The following contexts write to the data shard
+  std::vector<std::shared_ptr<RedisContext>> shard_contexts_;
+  std::vector<std::unique_ptr<RedisAsioClient>> shard_asio_async_clients_;
+  std::vector<std::unique_ptr<RedisAsioClient>> shard_asio_subscribe_clients_;
+  // The following context writes everything to the primary shard
+  std::shared_ptr<RedisContext> primary_context_;
+  std::unique_ptr<RedisAsioClient> asio_async_auxiliary_client_;
+  std::unique_ptr<RedisAsioClient> asio_subscribe_auxiliary_client_;
+};
+
+}  // namespace gcs
+
+}  // namespace ray
+
+#endif  // RAY_GCS_REDIS_CLIENT_H
