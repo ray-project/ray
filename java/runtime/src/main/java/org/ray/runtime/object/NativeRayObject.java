57f0ed774a840956f44c32394a6d8eb24619b64d
cp <caopeng.caopeng@antfin.com>
2020-03-16 11:42:50 +0800

    add: innerObjectIds of NativeRayObject

--- java/runtime/src/main/java/org/ray/runtime/object/NativeRayObject.java
+++ java/runtime/src/main/java/org/ray/runtime/object/NativeRayObject.java
@@ -1,6 +1,10 @@
 package org.ray.runtime.object;
 
 import com.google.common.base.Preconditions;
+import org.ray.api.id.ObjectId;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Binary representation of a ray object. See `RayObject` class in C++ for details.
@@ -9,11 +13,20 @@ public class NativeRayObject {
 
   public byte[] data;
   public byte[] metadata;
+  private List<ObjectId> containedObjectIds = Collections.emptyList();
 
   public NativeRayObject(byte[] data, byte[] metadata) {
     Preconditions.checkState(bufferLength(data) > 0 || bufferLength(metadata) > 0);
     this.data = data;
     this.metadata = metadata;
+  }
+
+  public List<ObjectId> getContainedObjectIds() {
+    return containedObjectIds;
+  }
+
+  public void setContainedObjectIds(List<ObjectId> containedObjectIds) {
+    this.containedObjectIds = containedObjectIds;
   }
 
   private static int bufferLength(byte[] buffer) {
