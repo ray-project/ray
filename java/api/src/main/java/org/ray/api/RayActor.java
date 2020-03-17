1f8b69353947de8ed94df2ab5a0ea4f35f3c9372
Kai Yang <kfstorm@outlook.com>
2020-03-17 21:45:56 +0800

    [Java] Make both `RayActor` and `RayPyActor` inheriting from `BaseActor` (#7462)

--- java/api/src/main/java/org/ray/api/RayActor.java
+++ java/api/src/main/java/org/ray/api/RayActor.java
@@ -1,9 +1,7 @@
 package org.ray.api;
 
-import org.ray.api.id.ActorId;
-
 /**
- * A handle to an actor. <p>
+ * A handle to a Java actor. <p>
  *
  * A handle can be used to invoke a remote actor method, with the {@code "call"} method. For
  * example:
@@ -14,7 +12,7 @@ import org.ray.api.id.ActorId;
  *   }
  * }
  * // Create an actor, and get a handle.
- * RayActor<MyActor> myActor = Ray.createActor(RayActor::new);
+ * RayActor<MyActor> myActor = Ray.createActor(MyActor::new);
  * // Call the `echo` method remotely.
  * RayObject<Integer> result = myActor.call(MyActor::echo, 1);
  * // Get the result of the remote `echo` method.
@@ -26,20 +24,6 @@ import org.ray.api.id.ActorId;
  *
  * @param <A> The type of the concrete actor class.
  */
-public interface RayActor<A> extends ActorCall<A> {
+public interface RayActor<A> extends BaseActor, ActorCall<A> {
 
-  /**
-   * @return The id of this actor.
-   */
-  ActorId getId();
-
-  /**
-   * Kill the actor immediately. This will cause any outstanding tasks submitted to the actor to
-   * fail and the actor to exit in the same way as if it crashed.
-   *
-   * @param noReconstruction If set to true, the killed actor will not be reconstructed anymore.
-   */
-  default void kill(boolean noReconstruction) {
-    Ray.internal().killActor(this, noReconstruction);
-  }
 }
