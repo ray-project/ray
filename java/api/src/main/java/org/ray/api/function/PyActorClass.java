6571db4b194b70ed8af7708d16f0648a240df694
刘宝 <po.lb@antfin.com>
2020-03-20 14:50:01 +0800

    Fix doc style

--- java/api/src/main/java/org/ray/api/function/PyActorClass.java
+++ java/api/src/main/java/org/ray/api/function/PyActorClass.java
@@ -3,21 +3,25 @@ package org.ray.api.function;
 /**
  * A class that represents a Python actor class.
  *
+ * <pre>
  * example_package/
  * ├──__init__.py
  * └──example_module.py
  *
  * in example_module.py there is an actor class A.
  *
- * @ray.remote
+ * \@ray.remote
  * class A(object):
  *     def __init__(self, x):
  *         self.x = x
  *
  * we can create this Python actor from Java:
  *
+ * {@code
  * RayPyActor actor = Ray.createActor(new PyActorClass("example_package.example_module", "A"),
  *                                    "the value for x");
+ * }
+ * </pre>
  */
 public class PyActorClass {
   // The full module name of this actor class
--- java/api/src/main/java/org/ray/api/function/PyActorMethod.java
+++ java/api/src/main/java/org/ray/api/function/PyActorMethod.java
@@ -6,22 +6,28 @@ package org.ray.api.function;
  * Note, information about the actor will be inferred from the actor handle,
  * so it's not specified in this class.
  *
+ * <pre>
  * there is a Python actor class A.
  *
- * @ray.remote
+ * \@ray.remote
  * class A(object):
  *     def foo(self):
  *         return "Hello world!"
  *
  * suppose we have got the Python actor class A's handle in Java
  *
+ * {@code
  * RayPyActor actor = ...; // returned from Ray.createActor or passed from Python
+ * }
  *
  * then we can call the actor method:
  *
+ * {@code
  * // A.foo returns a string, so we have to set the returnType to String.class
  * RayObject<String> res = actor.call(new PyActorMethod<>("foo", String.class));
  * String x = res.get();
+ * }
+ * </pre>
  */
 public class PyActorMethod<R> {
   // The name of this actor method
