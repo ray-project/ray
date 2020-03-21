95b5ab081edf198021dffeacbee2c69d8102cde1
simon-mo <xmo@berkeley.edu>
2020-03-21 00:57:28 -0700

    Catch Exception in get_py_stack

--- python/ray/_raylet.pyx
+++ python/ray/_raylet.pyx
@@ -569,7 +569,12 @@ cdef void get_py_stack(c_string* stack_out) nogil:
     """
 
     with gil:
-        frame = inspect.currentframe()
+        try:
+            frame = inspect.currentframe()
+        except ValueError:
+            stack_out[0] = "".encode("ascii")
+            return
+
         msg = ""
         while frame:
             filename = frame.f_code.co_filename
