9357e6e317b923f5ecd9b8fcd7c178a3d3b4ca14
simon-mo <xmo@berkeley.edu>
2020-03-21 22:50:40 -0700

    Reinstall all version of python

--- python/build-wheel-macos.sh
+++ python/build-wheel-macos.sh
@@ -13,24 +13,24 @@ MACPYTHON_URL=https://www.python.org/ftp/python
 MACPYTHON_PY_PREFIX=/Library/Frameworks/Python.framework/Versions
 DOWNLOAD_DIR=python_downloads
 
-PY_VERSIONS=(#"3.5.3"
-             #"3.6.1"
-             #"3.7.0"
+PY_VERSIONS=("3.5.3"
+             "3.6.1"
+             "3.7.0"
              "3.8.1")
-PY_INSTS=(#"python-3.5.3-macosx10.6.pkg"
-          #"python-3.6.1-macosx10.6.pkg"
-          #"python-3.7.0-macosx10.6.pkg"
+PY_INSTS=("python-3.5.3-macosx10.6.pkg"
+          "python-3.6.1-macosx10.6.pkg"
+          "python-3.7.0-macosx10.6.pkg"
           "python-3.8.1-macosx10.9.pkg")
-PY_MMS=(#"3.5"
-        #"3.6"
-        #"3.7"
+PY_MMS=("3.5"
+        "3.6"
+        "3.7"
         "3.8")
 
 # The minimum supported numpy version is 1.14, see
 # https://issues.apache.org/jira/browse/ARROW-3141
-NUMPY_VERSIONS=(#"1.14.5"
-                #"1.14.5"
-                #"1.14.5"
+NUMPY_VERSIONS=("1.14.5"
+                "1.14.5"
+                "1.14.5"
                 "1.14.5")
 
 ./ci/travis/install-bazel.sh
