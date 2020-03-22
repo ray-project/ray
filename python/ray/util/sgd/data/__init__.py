c944a5bc92a257459b104c7e5d103efaadac6131
Alex Wu <itswu.alex@gmail.com>
2020-03-22 15:17:03 -0700

    Formatting

--- python/ray/util/sgd/data/__init__.py
+++ python/ray/util/sgd/data/__init__.py
@@ -1,8 +1,7 @@
-import logging
-logger = logging.getLogger(__name__)
-
 from ray.util.sgd.data.dataset import Dataset
 from ray.util.sgd.data.local_dataset import LocalDataset
 from ray.util.sgd.data.imagenet_dataset import ImagenetDataset
+import logging
+logger = logging.getLogger(__name__)
 
 __all__ = ["Dataset", "LocalDataset", "ImagenetDataset"]
--- python/ray/util/sgd/data/utils.py
+++ python/ray/util/sgd/data/utils.py
@@ -3,8 +3,7 @@ def get_arr_partition(arr, n, i):
     extra = len(arr) % n
     if i < extra:
         start = (per_part + 1) * i
-        return arr[start: start + per_part + 1]
+        return arr[start:start + per_part + 1]
     else:
         start = (per_part + 1) * extra + per_part * (i - extra)
-        return arr[start : start + per_part]
-
+        return arr[start:start + per_part]
