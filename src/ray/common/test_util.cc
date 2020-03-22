bff250a48469375f2578ec83e65d1f0a45938202
senlin.zsl <senlin.zsl@antfin.com>
2020-03-22 09:13:09 +0800

    fix compile error

--- src/ray/common/test_util.cc
+++ src/ray/common/test_util.cc
@@ -51,7 +51,9 @@ void RedisServiceManagerForTest::FlushAll() {
   std::string flush_all_redis_command =
       REDIS_CLIENT_EXEC_PATH + " -p " + std::to_string(REDIS_SERVER_PORT) + " flushall";
   RAY_LOG(INFO) << "Cleaning up redis with command: " << flush_all_redis_command;
-  system(flush_all_redis_command.c_str());
+  if (system(flush_all_redis_command.c_str()) != 0) {
+    RAY_LOG(WARNING) << "Failed to flush redis. The redis process may no longer exist.";
+  }
   usleep(100 * 1000);
 }
 
