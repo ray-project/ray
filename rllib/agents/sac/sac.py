d3042b9a0eafd305304ab0101b5a2cb8ad5f3657
sven1977 <svenmika1977@gmail.com>
2020-03-23 21:56:32 +0100

    WIP.

--- rllib/agents/sac/sac.py
+++ rllib/agents/sac/sac.py
@@ -99,8 +99,18 @@ DEFAULT_CONFIG = with_common_config({
 # __sphinx_doc_end__
 # yapf: enable
 
+
+def get_policy_class(config):
+    if config.get("use_pytorch") is True:
+        from ray.rllib.agents.sac.sac_torch_policy import SACTorchPolicy
+        return SACTorchPolicy
+    else:
+        return SACTFPolicy
+
+
 SACTrainer = GenericOffPolicyTrainer.with_updates(
     name="SAC",
     default_config=DEFAULT_CONFIG,
     default_policy=SACTFPolicy,
+    get_policy_class=
 )
