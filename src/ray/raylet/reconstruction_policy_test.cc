98ee43dc95aa3286b12927d06ff9b10cd10094f6
Edward Oakes <ed.nmi.oakes@gmail.com>
2020-03-23 09:32:12 -0500

    fix reconstruction_policy_test

--- src/ray/raylet/reconstruction_policy_test.cc
+++ src/ray/raylet/reconstruction_policy_test.cc
@@ -270,8 +270,7 @@ class ReconstructionPolicyTest : public ::testing::Test {
 
 TEST_F(ReconstructionPolicyTest, TestReconstructionSimple) {
   TaskID task_id = ForNormalTask();
-  ObjectID object_id =
-      ObjectID::ForTaskReturn(task_id, /*index=*/1, /*transport_type=*/0);
+  ObjectID object_id = ObjectID::ForTaskReturn(task_id, /*index=*/1);
 
   // Listen for an object.
   reconstruction_policy_->ListenAndMaybeReconstruct(object_id);
@@ -289,8 +288,7 @@ TEST_F(ReconstructionPolicyTest, TestReconstructionSimple) {
 
 TEST_F(ReconstructionPolicyTest, TestReconstructionEvicted) {
   TaskID task_id = ForNormalTask();
-  ObjectID object_id =
-      ObjectID::ForTaskReturn(task_id, /*index=*/1, /*transport_type=*/0);
+  ObjectID object_id = ObjectID::ForTaskReturn(task_id, /*index=*/1);
   mock_object_directory_->SetObjectLocations(object_id, {ClientID::FromRandom()});
 
   // Listen for both objects.
@@ -313,8 +311,7 @@ TEST_F(ReconstructionPolicyTest, TestReconstructionEvicted) {
 
 TEST_F(ReconstructionPolicyTest, TestReconstructionObjectLost) {
   TaskID task_id = ForNormalTask();
-  ObjectID object_id =
-      ObjectID::ForTaskReturn(task_id, /*index=*/1, /*transport_type=*/0);
+  ObjectID object_id = ObjectID::ForTaskReturn(task_id, /*index=*/1);
   ClientID client_id = ClientID::FromRandom();
   mock_object_directory_->SetObjectLocations(object_id, {client_id});
 
@@ -338,10 +335,8 @@ TEST_F(ReconstructionPolicyTest, TestReconstructionObjectLost) {
 TEST_F(ReconstructionPolicyTest, TestDuplicateReconstruction) {
   // Create two object IDs produced by the same task.
   TaskID task_id = ForNormalTask();
-  ObjectID object_id1 =
-      ObjectID::ForTaskReturn(task_id, /*index=*/1, /*transport_type=*/0);
-  ObjectID object_id2 =
-      ObjectID::ForTaskReturn(task_id, /*index=*/2, /*transport_type=*/0);
+  ObjectID object_id1 = ObjectID::ForTaskReturn(task_id, /*index=*/1);
+  ObjectID object_id2 = ObjectID::ForTaskReturn(task_id, /*index=*/2);
 
   // Listen for both objects.
   reconstruction_policy_->ListenAndMaybeReconstruct(object_id1);
@@ -360,8 +355,7 @@ TEST_F(ReconstructionPolicyTest, TestDuplicateReconstruction) {
 
 TEST_F(ReconstructionPolicyTest, TestReconstructionSuppressed) {
   TaskID task_id = ForNormalTask();
-  ObjectID object_id =
-      ObjectID::ForTaskReturn(task_id, /*index=*/1, /*transport_type=*/0);
+  ObjectID object_id = ObjectID::ForTaskReturn(task_id, /*index=*/1);
   // Run the test for much longer than the reconstruction timeout.
   int64_t test_period = 2 * reconstruction_timeout_ms_;
 
@@ -388,8 +382,7 @@ TEST_F(ReconstructionPolicyTest, TestReconstructionSuppressed) {
 
 TEST_F(ReconstructionPolicyTest, TestReconstructionContinuallySuppressed) {
   TaskID task_id = ForNormalTask();
-  ObjectID object_id =
-      ObjectID::ForTaskReturn(task_id, /*index=*/1, /*transport_type=*/0);
+  ObjectID object_id = ObjectID::ForTaskReturn(task_id, /*index=*/1);
 
   // Listen for an object.
   reconstruction_policy_->ListenAndMaybeReconstruct(object_id);
@@ -417,8 +410,7 @@ TEST_F(ReconstructionPolicyTest, TestReconstructionContinuallySuppressed) {
 
 TEST_F(ReconstructionPolicyTest, TestReconstructionCanceled) {
   TaskID task_id = ForNormalTask();
-  ObjectID object_id =
-      ObjectID::ForTaskReturn(task_id, /*index=*/1, /*transport_type=*/0);
+  ObjectID object_id = ObjectID::ForTaskReturn(task_id, /*index=*/1);
 
   // Listen for an object.
   reconstruction_policy_->ListenAndMaybeReconstruct(object_id);
@@ -444,8 +436,7 @@ TEST_F(ReconstructionPolicyTest, TestReconstructionCanceled) {
 
 TEST_F(ReconstructionPolicyTest, TestSimultaneousReconstructionSuppressed) {
   TaskID task_id = ForNormalTask();
-  ObjectID object_id =
-      ObjectID::ForTaskReturn(task_id, /*index=*/1, /*transport_type=*/0);
+  ObjectID object_id = ObjectID::ForTaskReturn(task_id, /*index=*/1);
 
   // Log a reconstruction attempt to simulate a different node attempting the
   // reconstruction first. This should suppress this node's first attempt at
