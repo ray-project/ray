b5cbee43b51da900446640fb8f33d4d280de93ec
久龙 <guyang.sgy@antfin.com>
2020-03-23 21:30:50 +0800

    use cc_test in c++ worker test

--- cpp/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
-
-api_test=$($ROOT_DIR/../bazel-bin/cpp/api_test)
-echo "${api_test}"
-[[ ${api_test} =~ "FAILED" ]] && exit 1
-echo "cpp worker ci test finished"
-
