1a68a9d844efd0bb34f9780addd83be3a3cabde7
Eric Liang <ekhliang@gmail.com>
2020-03-25 16:45:37 -0700

    wip

---
+++ rllib/execution/common.py
@@ -0,0 +1,56 @@
+import logging
+from typing import List, Any, Tuple, Union
+import numpy as np
+import queue
+import random
+import time
+
+import ray
+from ray.util.iter import from_actors, LocalIterator, _NextValueNotReady
+from ray.util.iter_metrics import SharedMetrics
+from ray.rllib.optimizers.replay_buffer import PrioritizedReplayBuffer, \
+    ReplayBuffer
+from ray.rllib.evaluation.metrics import collect_episodes, \
+    summarize_episodes, get_learner_stats
+from ray.rllib.evaluation.rollout_worker import get_global_worker
+from ray.rllib.evaluation.worker_set import WorkerSet
+from ray.rllib.policy.sample_batch import SampleBatch, MultiAgentBatch, \
+    DEFAULT_POLICY_ID
+from ray.rllib.utils.compression import pack_if_needed
+
+
+# Counters for training progress (keys for metrics.counters).
+STEPS_SAMPLED_COUNTER = "num_steps_sampled"
+STEPS_TRAINED_COUNTER = "num_steps_trained"
+
+# Counters to track target network updates.
+LAST_TARGET_UPDATE_TS = "last_target_update_ts"
+NUM_TARGET_UPDATES = "num_target_updates"
+
+# Performance timers (keys for metrics.timers).
+APPLY_GRADS_TIMER = "apply_grad"
+COMPUTE_GRADS_TIMER = "compute_grads"
+WORKER_UPDATE_TIMER = "update"
+GRAD_WAIT_TIMER = "grad_wait"
+SAMPLE_TIMER = "sample"
+LEARN_ON_BATCH_TIMER = "learn"
+
+# Instant metrics (keys for metrics.info).
+LEARNER_INFO = "learner"
+
+# Type aliases.
+GradientType = dict
+SampleBatchType = Union[SampleBatch, MultiAgentBatch]
+
+
+# Asserts that an object is a type of SampleBatch.
+def _check_sample_batch_type(batch):
+    if not isinstance(batch, SampleBatchType.__args__):
+        raise ValueError("Expected either SampleBatch or MultiAgentBatch, "
+                         "got {}: {}".format(type(batch), batch))
+
+
+# Returns pipeline global vars that should be periodically sent to each worker.
+def _get_global_vars():
+    metrics = LocalIterator.get_metrics()
+    return {"timestep": metrics.counters[STEPS_SAMPLED_COUNTER]}
---
+++ rllib/execution/metric_ops.py
@@ -0,0 +1,119 @@
+
+
+def StandardMetricsReporting(train_op: LocalIterator[Any], workers: WorkerSet,
+                             config: dict) -> LocalIterator[dict]:
+    """Operator to periodically collect and report metrics.
+
+    Arguments:
+        train_op (LocalIterator): Operator for executing training steps.
+            We ignore the output values.
+        workers (WorkerSet): Rollout workers to collect metrics from.
+        config (dict): Trainer configuration, used to determine the frequency
+            of stats reporting.
+
+    Returns:
+        A local iterator over training results.
+
+    Examples:
+        >>> train_op = ParallelRollouts(...).for_each(TrainOneStep(...))
+        >>> metrics_op = StandardMetricsReporting(train_op, workers, config)
+        >>> next(metrics_op)
+        {"episode_reward_max": ..., "episode_reward_mean": ..., ...}
+    """
+
+    output_op = train_op \
+        .filter(OncePerTimeInterval(max(2, config["min_iter_time_s"]))) \
+        .for_each(CollectMetrics(
+            workers, min_history=config["metrics_smoothing_episodes"],
+            timeout_seconds=config["collect_metrics_timeout"]))
+    return output_op
+
+
+
+
+class CollectMetrics:
+    """Callable that collects metrics from workers.
+
+    The metrics are smoothed over a given history window.
+
+    This should be used with the .for_each() operator. For a higher level
+    API, consider using StandardMetricsReporting instead.
+
+    Examples:
+        >>> output_op = train_op.for_each(CollectMetrics(workers))
+        >>> print(next(output_op))
+        {"episode_reward_max": ..., "episode_reward_mean": ..., ...}
+    """
+
+    def __init__(self, workers, min_history=100, timeout_seconds=180):
+        self.workers = workers
+        self.episode_history = []
+        self.to_be_collected = []
+        self.min_history = min_history
+        self.timeout_seconds = timeout_seconds
+
+    def __call__(self, _):
+        # Collect worker metrics.
+        episodes, self.to_be_collected = collect_episodes(
+            self.workers.local_worker(),
+            self.workers.remote_workers(),
+            self.to_be_collected,
+            timeout_seconds=self.timeout_seconds)
+        orig_episodes = list(episodes)
+        missing = self.min_history - len(episodes)
+        if missing > 0:
+            episodes.extend(self.episode_history[-missing:])
+            assert len(episodes) <= self.min_history
+        self.episode_history.extend(orig_episodes)
+        self.episode_history = self.episode_history[-self.min_history:]
+        res = summarize_episodes(episodes, orig_episodes)
+
+        # Add in iterator metrics.
+        metrics = LocalIterator.get_metrics()
+        timers = {}
+        counters = {}
+        info = {}
+        info.update(metrics.info)
+        for k, counter in metrics.counters.items():
+            counters[k] = counter
+        for k, timer in metrics.timers.items():
+            timers["{}_time_ms".format(k)] = round(timer.mean * 1000, 3)
+            if timer.has_units_processed():
+                timers["{}_throughput".format(k)] = round(
+                    timer.mean_throughput, 3)
+        res.update({
+            "num_healthy_workers": len(self.workers.remote_workers()),
+            "timesteps_total": metrics.counters[STEPS_SAMPLED_COUNTER],
+        })
+        res["timers"] = timers
+        res["info"] = info
+        res["info"].update(counters)
+        return res
+
+
+class OncePerTimeInterval:
+    """Callable that returns True once per given interval.
+
+    This should be used with the .filter() operator to throttle / rate-limit
+    metrics reporting. For a higher-level API, consider using
+    StandardMetricsReporting instead.
+
+    Examples:
+        >>> throttled_op = train_op.filter(OncePerTimeInterval(5))
+        >>> start = time.time()
+        >>> next(throttled_op)
+        >>> print(time.time() - start)
+        5.00001  # will be greater than 5 seconds
+    """
+
+    def __init__(self, delay):
+        self.delay = delay
+        self.last_called = 0
+
+    def __call__(self, item):
+        now = time.time()
+        if now - self.last_called > self.delay:
+            self.last_called = now
+            return True
+        return False
+
