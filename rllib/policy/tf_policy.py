3e437efc75f24fa93572542f02e56a22604e4f5b
sven1977 <svenmika1977@gmail.com>
2020-03-26 18:07:37 +0100

    LINT.

--- rllib/policy/tf_policy.py
+++ rllib/policy/tf_policy.py
@@ -348,7 +348,7 @@ class TFPolicy(Policy):
             builder.add_meta_graph_and_variables(
                 self._sess, [tf.saved_model.tag_constants.SERVING],
                 signature_def_map=signature_def_map,
-                saver=tf.summary.FileWriter(export_dir).add_graph(  # <-- HERE
+                saver=tf.summary.FileWriter(export_dir).add_graph(
                     graph=self._sess.graph))
             builder.save()
 
