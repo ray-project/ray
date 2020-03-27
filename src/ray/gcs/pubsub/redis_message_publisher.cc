91106c192572c8fadbed59edf41e0f2f7b846c5b
micafan <550435771@qq.com>
2020-03-27 16:37:11 +0800

    add RedisMessagePublisher

---
+++ src/ray/gcs/pubsub/redis_message_publisher.cc
@@ -0,0 +1,48 @@
+#include "ray/gcs/redis_message_publisher.h"
+
+namespace ray {
+
+namespace gcs {
+
+RedisMessagePublisher::RedisMessagePublisher(const RedisClientOptions &options)
+    : redis_client_(new RedisClient(options)) {}
+
+Status RedisMessagePublisher::Connect(std::shared_ptr<IOServicePool> io_service_pool) {
+  auto io_services = io_service_pool->GetAll();
+  Status status = redis_client_->Connect(io_services);
+  RAY_LOG(INFO) << "RedisMessagePublisher::Connect finished with status " << status.ToString();
+  return status;
+}
+
+void RedisMessagePublisher::Disconnect() {
+  redis_client_->Disconnect();
+  RAY_LOG(INFO) << "RedisMessagePublisher disconnected.";
+}
+
+Status RedisMessagePublisher::PublishMessage(
+    const std::string &channel, const std::string &message,
+    const StatusCallback &callback) {
+  std::vector<std::string> args = {"PUBLISH", channel, message};
+
+  RedisCallback pub_callback = nullptr;
+  if (callback) {
+    pub_callback = [callback](std::shared_ptr<CallbackReply> reply) {
+      int64_t deleted_count = reply->ReadAsInteger();
+      callback(Status::OK());
+    };
+  }
+  // Select shard context by channel.
+  auto shard_context = redis_client_->GetShardContext(channel);
+  return shard_context->RunArgvAsync(args, pub_callback);
+}
+
+template<typename Message>
+Status RedisMessagePublisher::PublishMessage(
+    const std::string &channel, const Message &message,
+    const StatusCallback &callback) {
+  return PublishMessage(channel, message.SerilizeToString(), callback);
+}
+
+}  // namespace gcs
+
+}  // namespace ray
---
+++ src/ray/gcs/pubsub/redis_message_publisher.h
@@ -0,0 +1,34 @@
+#ifndef RAY_GCS_PUBSUB_REDIS_MESSAGE_PUBLISHER_H
+#define RAY_GCS_PUBSUB_REDIS_MESSAGE_PUBLISHER_H
+
+#include "ray/gcs/redis_client.h"
+#include "ray/gcs/pubsub/message_publisher.h"
+
+namespace ray {
+
+namespace gcs {
+
+class RedisMessagePublisher : public MessagePublisher {
+ public:
+  RedisMessagePublisher(std::shared_ptr<RedisClient> redis_client);
+
+  Status Connect(std::shared_ptr<IOServicePool> io_service_pool) override;
+
+  void Disconnect() override;
+
+  Status PublishMessage(const std::string &channel, const std::string &message,
+                        const StatusCallback &callback) override;
+ 
+  template<typename Message>
+  Status PublishMessage(const std::string &channel, const Message &message,
+                        const StatusCallback &callback) override;
+
+ private:
+  std::shared_ptr<RedisClient> redis_client_;
+};
+
+}  // namespace gcs
+
+}  // namespace ray
+
+#endif  // RAY_GCS_PUBSUB_REDIS_MESSAGE_PUBLISHER_H
