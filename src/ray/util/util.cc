9b427c9bdb07c26bb5ae62ddd9c464c36129d1b7
Mehrdad <noreply@github.com>
2020-03-27 20:44:41 -0700

    Polishing

--- src/ray/util/util.cc
+++ src/ray/util/util.cc
@@ -8,27 +8,39 @@
 #include "ray/util/logging.h"
 
 static std::vector<std::string> ParsePosixCommandLine(const std::string &s) {
+  // See the unit-tests for examples. To compare against the platform's behavior, try:
+  //  $ python3 -c "import sys; [print(a) for a in sys.argv[1:]]" \x\\y "\x\\y" '\x\\y'
   std::vector<std::string> result;
-  const std::string single_quoted = R"('([^']*)')",
-                    double_quoted = R"(\"((?:[^"\\]|\\.)*)\")",
-                    unquoted = R"(((?:\\(?:.|$)|[^"'\s\\])+))", boundary = R"(\s+|$)";
+  const std::string
+      // Single-quoted strings lack single-quotes
+      single_quoted = "'([^']*)'",
+      // Double-quoted strings lack double-quotes, except after unescaped backslashes
+      double_quoted = R"(\"((?:[^"\\]|\\.)*)\")",
+      // Unquoted tokens lack spaces & quotes, except after unescaped backslashes
+      unquoted = R"(((?:\\(?:.|$)|[^"'\s\\])+))",
+      // Whitespace & EOF are delimiters
+      boundary = R"(\s+|$)";
   std::regex re_cmdline(single_quoted + "|" + double_quoted + "|" + unquoted + "|" +
-                        boundary),
-      re_dq_escape(R"(\\([\\"]))"), re_escape(R"(\\(.))");
+                        boundary);
+  std::regex re_dq_escape(R"(\\([\\"]))"), re_escape(R"(\\(.))");
   bool was_space = true;
   std::string arg;
   for (std::sregex_iterator i(s.begin(), s.end(), re_cmdline), end; i != end; ++i) {
-    const std::smatch &match = *i;
+    const std::smatch &groups = *i;
     bool space = false;
-    size_t j = 0;
-    if (match[++j].matched) {
-      arg += match[j];
-    } else if (match[++j].matched) {
-      arg += std::regex_replace(match[j].str(), re_dq_escape, "$1");
-    } else if (match[++j].matched) {
-      arg += std::regex_replace(match[j].str(), re_escape, "$1");
+    if (groups[1].matched) {
+      // single_quoted: No escaping: '\x\\y' is just \x\\y verbatim
+      arg += groups[1];
+    } else if (groups[2].matched) {
+      // double_quoted: Backslashes stay if they don't escape anything: "\x\\y" == '\x\y'
+      arg += std::regex_replace(groups[2].str(), re_dq_escape, "$1");
+    } else if (groups[3].matched) {
+      // unquoted: Backslashes get removed if they don't escape anything: \x\\y == 'x\y'
+      arg += std::regex_replace(groups[3].str(), re_escape, "$1");
     } else {
+      // boundary
       if (!was_space) {
+        // The previous token was an argument we just finished, not a contiguous delimiter
         result.push_back(arg);
         arg.clear();
       }
@@ -40,24 +52,36 @@ static std::vector<std::string> ParsePosixCommandLine(const std::string &s) {
 }
 
 static std::vector<std::string> ParseWindowsCommandLine(const std::string &s) {
+  // See the unit-tests for examples. To compare against the platform's behavior, try:
+  //  > python3 -c "import sys; [print(a) for a in sys.argv[1:]]" \x\\y "\x\\y"
   std::vector<std::string> result;
-  const std::string double_quoted = R"(\"((?:[^"\\]|\\.)*)\")",
-                    unquoted = R"(((?:\\(?:.|$)|[^"\s\\])+))", boundary = R"(\s+|$)";
-  std::regex re_cmdline(double_quoted + "|" + unquoted + "|" + boundary),
-      re_dq_escape(R"((\\*)\1(?:\\(")|$))"), re_escape(R"((\\*)\1\\("))");
+  const std::string
+      // double-quoted tokens lack double-quotes, except after unescaped backslashes
+      double_quoted = R"(\"((?:[^"\\]|\\.)*)\")",
+      // Unquoted tokens lack spaces & quotes, except after unescaped backslashes
+      unquoted = R"(((?:\\(?:.|$)|[^"\s\\])+))",
+      // Whitespace & EOF are delimiters
+      boundary = R"(\s+|$)";
+  std::regex re_cmdline(double_quoted + "|" + unquoted + "|" + boundary);
+  std::regex re_dq_escape(R"((\\*)\1(?:\\(")|$))"), re_escape(R"((\\*)\1\\("))");
   bool was_space = false;
   std::string arg;
   for (std::sregex_iterator i(s.begin(), s.end(), re_cmdline), end; i != end; ++i) {
-    const std::smatch &match = *i;
+    const std::smatch &groups = *i;
     bool space = false;
-    size_t j = 0;
-    if (match[++j].matched) {
-      arg += std::regex_replace(match[j].str(), re_dq_escape, "$1$2");
-    } else if (match[++j].matched) {
-      arg += std::regex_replace(match[j].str(), re_escape, "$2");
-    } else if (!was_space) {
-      result.push_back(arg);
-      arg.clear();
+    if (groups[1].matched) {
+      // double_quoted: Backslashes are escapes only if they precede a double-quote
+      arg += std::regex_replace(groups[1].str(), re_dq_escape, "$1$2");
+    } else if (groups[2].matched) {
+      // unquoted: Backslashes are escapes only if they precede a double-quote
+      arg += std::regex_replace(groups[2].str(), re_escape, "$2");
+    } else {
+      // boundary
+      if (!was_space) {
+        // The previous token was an argument we just finished, not a contiguous delimiter
+        result.push_back(arg);
+        arg.clear();
+      }
       space = true;
     }
     was_space = space;
@@ -94,8 +118,8 @@ std::string CreatePosixCommandLine(const std::vector<std::string> &args) {
   for (size_t i = 0; i != args.size(); ++i) {
     std::string arg = args[i];
     if (std::regex_search(arg, unsafe_chars)) {
-      arg = std::regex_replace(arg, single_quote, "'\\$1'");
-      arg = "'" + arg + "'";
+      // Prefer single-quotes. Double-quotes have unpredictable behavior, e.g. for "\!".
+      arg = "'" + std::regex_replace(arg, single_quote, "'\\$1'") + "'";
     }
     if (i > 0) {
       result += ' ';
@@ -112,9 +136,9 @@ static std::string CreateWindowsCommandLine(const std::vector<std::string> &args
   for (size_t i = 0; i != args.size(); ++i) {
     std::string arg = args[i];
     if (std::regex_search(arg, unsafe_chars)) {
+      // Escape only backslashes that precede double-quotes
       arg = std::regex_replace(arg, end, "$1$1");
-      arg = std::regex_replace(arg, double_quote, "$1$1\\$2");
-      arg = '"' + arg + '"';
+      arg = '"' + std::regex_replace(arg, double_quote, "$1$1\\$2") + '"';
     }
     if (i > 0) {
       result += ' ';
