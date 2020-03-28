065911ebc8ac45240b464d6850ce6d4be3c18212
Mehrdad <noreply@github.com>
2020-03-27 22:06:57 -0700

    Use login shell so that .bashrc is processed (for PATH modifications etc.)
    Also add ~/bin to beginning of PATH after installing Bazel

--- ci/travis/install-bazel.sh
+++ ci/travis/install-bazel.sh
@@ -38,6 +38,10 @@ else
   rm -f "${target}"
 fi
 
+cat <<"EOF" >> "${HOME}/.bashrc"
+export PATH="${HOME}/bin:${PATH}"
+EOF
+
 if [ "${TRAVIS-}" = true ]; then
   # Use bazel disk cache if this script is running in Travis.
   mkdir -p "${HOME}/ray-bazel-cache"
@@ -47,6 +51,25 @@ build --show_timestamps  # Travis doesn't have an option to show timestamps, but
 EOF
 fi
 if [ -n "${GITHUB_WORKFLOW-}" ]; then
+  cat <<"EOF" >> "${HOME}/.profile"
+if [ "${OSTYPE}" = "msys" ]; then
+  export USE_CLANG_CL=1
+  export MSYS2_ARG_CONV_EXCL="*"  # Don't let MSYS2 attempt to auto-translate arguments that look like paths
+  latest_python_bin=""
+  for latest_python_bin in /proc/registry/HKEY_LOCAL_MACHINE/Software/Python/PythonCore/*/InstallPath/@; do
+    if [ -f "${latest_python_bin}" ]; then
+      read -r latest_python_bin < "${latest_python_bin}"
+      latest_python_bin="${latest_python_bin}\\"
+    else
+      latest_python_bin=""
+    fi
+  done
+  latest_python_bin="${latest_python_bin}python.exe"
+  if [ -f "${latest_python_bin}" ]; then
+    export PYTHON2_BIN_PATH="${latest_python_bin}" PYTHON3_BIN_PATH="${latest_python_bin}"
+  fi
+fi
+EOF
   cat <<EOF >> "${HOME}/.bazelrc"
 --output_base=".bazel-out"  # On GitHub Actions, staying on the same volume seems to be faster
 EOF
@@ -54,6 +77,7 @@ fi
 if [ "${TRAVIS-}" = true ] || [ -n "${GITHUB_WORKFLOW-}" ]; then
   cat <<EOF >> "${HOME}/.bazelrc"
 # CI output doesn't scroll, so don't use curses
+build --color=yes
 build --curses=no
 build --progress_report_interval=60
 # Use ray google cloud cache
