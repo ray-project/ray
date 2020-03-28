337c1ea63a53267bf169f3ab7e23d79c8b073095
Richard Liaw <rliaw@berkeley.edu>
2020-03-27 19:06:00 -0700

    fixtests

--- python/ray/util/sgd/torch/examples/cifar_pytorch_example.py
+++ python/ray/util/sgd/torch/examples/cifar_pytorch_example.py
@@ -85,7 +85,7 @@ def train_example(num_workers=1,
         backend="nccl" if use_gpu else "gloo",
         scheduler_step_freq="epoch",
         use_fp16=use_fp16,
-        tqdm=True)
+        use_tqdm=True)
     pbar = trange(num_epochs, unit="epoch")
     for i in pbar:
         info = {"num_steps": 1} if test_mode else {}
