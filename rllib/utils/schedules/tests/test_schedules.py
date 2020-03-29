b3e6acb18ad5b2ecce2ec392162b7d514710bd8a
konichuvak <arialinvlad@gmail.com>
2020-03-29 01:03:30 -0400

    fixing polynomial schedule horizon

--- rllib/utils/schedules/tests/test_schedules.py
+++ rllib/utils/schedules/tests/test_schedules.py
@@ -34,23 +34,23 @@ class TestSchedules(unittest.TestCase):
                 check(out, value)
 
     def test_linear_schedule(self):
-        ts = [0, 50, 10, 100, 90, 2, 1, 99, 23]
+        ts = [0, 50, 10, 100, 90, 2, 1, 99, 23, 1000]
         config = {"schedule_timesteps": 100, "initial_p": 2.1, "final_p": 0.6}
         for fw in ["tf", "torch", None]:
             linear = from_config(LinearSchedule, config, framework=fw)
             for t in ts:
                 out = linear(t)
-                check(out, 2.1 - (t / 100) * (2.1 - 0.6), decimals=4)
+                check(out, 2.1 - (min(t, 100) / 100) * (2.1 - 0.6), decimals=4)
 
         # Test eager as well.
         with eager_mode():
             linear = from_config(LinearSchedule, config, framework="tf")
             for t in ts:
                 out = linear(t)
-                check(out, 2.1 - (t / 100) * (2.1 - 0.6), decimals=4)
+                check(out, 2.1 - (min(t, 100) / 100) * (2.1 - 0.6), decimals=4)
 
     def test_polynomial_schedule(self):
-        ts = [0, 5, 10, 100, 90, 2, 1, 99, 23]
+        ts = [0, 5, 10, 100, 90, 2, 1, 99, 23, 1000]
         config = dict(
             type="ray.rllib.utils.schedules.polynomial_schedule."
             "PolynomialSchedule",
@@ -63,7 +63,8 @@ class TestSchedules(unittest.TestCase):
             polynomial = from_config(config)
             for t in ts:
                 out = polynomial(t)
-                check(out, 0.5 + (2.0 - 0.5) * (1.0 - t / 100)**2, decimals=4)
+                check(out, 0.5 + (2.0 - 0.5) * (1.0 - min(t, 100) / 100)**2,
+                      decimals=4)
 
         # Test eager as well.
         with eager_mode():
@@ -71,7 +72,8 @@ class TestSchedules(unittest.TestCase):
             polynomial = from_config(config)
             for t in ts:
                 out = polynomial(t)
-                check(out, 0.5 + (2.0 - 0.5) * (1.0 - t / 100)**2, decimals=4)
+                check(out, 0.5 + (2.0 - 0.5) * (1.0 - min(t, 100) / 100)**2,
+                      decimals=4)
 
     def test_exponential_schedule(self):
         ts = [0, 5, 10, 100, 90, 2, 1, 99, 23]
