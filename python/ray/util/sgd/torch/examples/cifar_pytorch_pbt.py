8e49cd8834ac51ea8eea9f4c2730c12cf0ad5dc8
Richard Liaw <rliaw@berkeley.edu>
2020-03-30 12:21:00 -0700

    better_example

--- python/ray/util/sgd/torch/examples/cifar_pytorch_pbt.py
+++ python/ray/util/sgd/torch/examples/cifar_pytorch_pbt.py
@@ -1,7 +1,10 @@
+import numpy as np
 import os
 import torch
 import torch.nn as nn
 import argparse
+from ray import tune
+from ray.tune.schedulers import PopulationBasedTraining
 from torch.utils.data import DataLoader, Subset
 from torchvision.datasets import CIFAR10
 import torchvision.transforms as transforms
@@ -9,6 +12,7 @@ import torchvision.transforms as transforms
 from tqdm import trange
 
 import ray
+from ray.tune import CLIReporter
 from ray.util.sgd.torch import TorchTrainer
 from ray.util.sgd.torch.resnet import ResNet18
 from ray.util.sgd.utils import BATCH_SIZE
@@ -43,7 +47,7 @@ def cifar_creator(config):
     validation_dataset = CIFAR10(
         root="~/data", train=False, download=False, transform=transform_test)
 
-    if config["test_mode"]:
+    if config.get("test_mode"):
         train_dataset = Subset(train_dataset, list(range(64)))
         validation_dataset = Subset(validation_dataset, list(range(64)))
 
@@ -60,11 +64,6 @@ def optimizer_creator(model, config):
         model.parameters(),
         lr=config.get("lr", 0.1),
         momentum=config.get("momentum", 0.9))
-
-
-def scheduler_creator(optimizer, config):
-    return torch.optim.lr_scheduler.MultiStepLR(
-        optimizer, milestones=[150, 250, 350], gamma=0.1)
 
 
 if __name__ == "__main__":
@@ -101,37 +100,51 @@ if __name__ == "__main__":
         "--tune", action="store_true", default=False, help="Tune training")
 
     args, _ = parser.parse_known_args()
-
     ray.init(address=args.address, log_to_driver=True)
 
-    trainer1 = TorchTrainer(
+    TorchTrainable = TorchTrainer.as_trainable(
         model_creator=ResNet18,
         data_creator=cifar_creator,
         optimizer_creator=optimizer_creator,
         loss_creator=nn.CrossEntropyLoss,
-        scheduler_creator=scheduler_creator,
         initialization_hook=initialization_hook,
         num_workers=args.num_workers,
         config={
-            "lr": 0.1,
-            "test_mode": args.smoke_test,  # subset the data
-            # this will be split across workers.
-            BATCH_SIZE: 128 * args.num_workers
+            "test_mode": args.test_mode,  # whether to to subset the data
+            BATCH_SIZE: 128 * args.num_workers,
         },
         use_gpu=args.use_gpu,
-        scheduler_step_freq="epoch",
-        use_fp16=args.fp16,
-        use_tqdm=True)
-    pbar = trange(args.num_epochs, unit="epoch")
-    for i in pbar:
-        info = {"num_steps": 1} if args.test_mode else {}
-        info["epoch_idx"] = i
-        info["num_epochs"] = args.num_epochs
-        # Increase `max_retries` to turn on fault tolerance.
-        trainer1.train(max_retries=1, info=info)
-        val_stats = trainer1.validate()
-        pbar.set_postfix(dict(acc=val_stats["val_accuracy"]))
+        use_fp16=args.use_fp16)
 
-    print(trainer1.validate())
-    trainer1.shutdown()
-    print("success!")
+    pbt_scheduler = PopulationBasedTraining(
+        time_attr="training_iteration",
+        metric="val_loss",
+        mode="min",
+        perturbation_interval=1,
+        hyperparam_mutations={
+            # distribution for resampling
+            "lr": lambda: np.random.uniform(0.001, 1),
+            # allow perturbations within this set of categorical values
+            "momentum": [0.8, 0.9, 0.99],
+        })
+
+    reporter = CLIReporter()
+    reporter.add_metric_column("val_loss", "loss")
+    reporter.add_metric_column("val_accuracy", "acc")
+
+    analysis = tune.run(
+        TorchTrainable,
+        num_samples=4,
+        config={
+            "lr": tune.choice([0.001, 0.01, 0.1]),
+            "momentum": 0.8
+        },
+        stop={"training_iteration": 2 if args.test_mode else 100},
+        max_failures=3,  # used for fault tolerance
+        checkpoint_freq=3,  # used for fault tolerance
+        keep_checkpoints_num=1,  # used for fault tolerance
+        verbose=2,
+        progress_reporter=reporter,
+        scheduler=pbt_scheduler)
+
+    print(analysis.get_best_config(metric="val_loss", mode="min"))
