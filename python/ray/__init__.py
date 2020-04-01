89aa47012a2e77c7ddcf5b82e1dcdb2fd1d0bed1
simon-mo <xmo@berkeley.edu>
2020-04-01 13:24:18 -0700

    Bump version key to 0.8.4

--- python/ray/__init__.py
+++ python/ray/__init__.py
@@ -93,7 +93,7 @@ from ray import util  # noqa: E402
 
 # Replaced with the current commit when building the wheels.
 __commit__ = "{{RAY_COMMIT_SHA}}"
-__version__ = "0.9.0.dev0"
+__version__ = "0.8.4"
 
 __all__ = [
     "jobs",
