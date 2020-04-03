9922913a0835807cb8bc1ce96c8f2aec99f4c4be
Markus Cozowicz <marcozo@microsoft.com>
2020-04-04 00:51:56 +0200

    [autoscaler] Switch to ARM for Azure deployment (#7717)
    * switch to ARM templates for config and VMs
    
    * switch to ARM templates for config and VMs
    
    * auto-formatting
    
    * addressed Scotts comment
    
    * added missing imports
    
    * fixed gpu templates
    fixed wheel reference
    
    * added missing reference
    
    * cleanup wording and yamls
    
    * Update doc/source/autoscaling.rst
    
    Co-Authored-By: Scott Graham <5720537+gramhagen@users.noreply.github.com>
    
    Co-authored-by: Ubuntu <marcozo@marcozodev2.zqvgrdyupqrudayw1il1agipig.jx.internal.cloudapp.net>
    Co-authored-by: Scott Graham <5720537+gramhagen@users.noreply.github.com>

--- python/setup.py
+++ python/setup.py
@@ -42,6 +42,8 @@ optional_ray_files = []
 ray_autoscaler_files = [
     "ray/autoscaler/aws/example-full.yaml",
     "ray/autoscaler/azure/example-full.yaml",
+    "ray/autoscaler/azure/azure-vm-template.json",
+    "ray/autoscaler/azure/azure-config-template.json",
     "ray/autoscaler/gcp/example-full.yaml",
     "ray/autoscaler/local/example-full.yaml",
     "ray/autoscaler/kubernetes/example-full.yaml",
