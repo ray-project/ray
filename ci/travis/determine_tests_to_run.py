c210c7fcfb9101b0e282661fd3bd3e2443c4c3bb
sven1977 <svenmika1977@gmail.com>
2020-04-07 22:08:30 +0200

    WIP.

--- ci/travis/determine_tests_to_run.py
+++ ci/travis/determine_tests_to_run.py
@@ -1,4 +1,8 @@
 # Script used for checking changes for incremental testing cases
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import os
 import re
 import subprocess
