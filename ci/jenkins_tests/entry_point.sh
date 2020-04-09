466a5dde41c4cfd94a13620b74f31a26dde0921d
simon-mo <xmo@berkeley.edu>
2020-04-09 01:55:46 -0700

    Make sigopt test optional: no secrets in PR, can't test

--- ci/jenkins_tests/entry_point.sh
+++ ci/jenkins_tests/entry_point.sh
@@ -14,6 +14,14 @@ ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
 SUPPRESS_OUTPUT=$ROOT_DIR/../suppress_output
 
-[[ ! -z "$RUN_TUNE_TESTS" ]] && bash $ROOT_DIR/run_tune_tests.sh ${MEMORY_SIZE} ${SHM_SIZE}
-[[ ! -z "$RUN_DOC_TESTS" ]] && bash $ROOT_DIR/run_doc_tests.sh ${MEMORY_SIZE} ${SHM_SIZE}
-[[ ! -z "$RUN_SGD_TESTS" ]] && bash $ROOT_DIR/run_sgd_tests.sh ${MEMORY_SIZE} ${SHM_SIZE}
+if [[ ! -z "$RUN_TUNE_TESTS" ]]; then 
+    bash $ROOT_DIR/run_tune_tests.sh ${MEMORY_SIZE} ${SHM_SIZE}
+fi
+
+if [[ ! -z "$RUN_DOC_TESTS" ]]; then
+    bash $ROOT_DIR/run_doc_tests.sh ${MEMORY_SIZE} ${SHM_SIZE}
+fi
+
+if [[ ! -z "$RUN_SGD_TESTS" ]]; then
+    bash $ROOT_DIR/run_sgd_tests.sh ${MEMORY_SIZE} ${SHM_SIZE}
+fi
