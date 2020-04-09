2e8fcfa3c1a98f967a2d9598e4c8ec2dfbafcb51
simon-mo <xmo@berkeley.edu>
2020-04-09 01:13:44 -0700

    Skip building deploy & examples, just use tune

--- ci/jenkins_tests/entry_point.sh
+++ ci/jenkins_tests/entry_point.sh
@@ -12,10 +12,8 @@ SHM_SIZE="4G"
 
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
-DOCKER_SHA=$($ROOT_DIR/../../build-docker.sh --output-sha --no-cache)
 SUPPRESS_OUTPUT=$ROOT_DIR/../suppress_output
-echo "Using Docker image" $DOCKER_SHA
 
-[[ ! -z "$RUN_TUNE_TESTS" ]] && bash $ROOT_DIR/run_tune_tests.sh ${MEMORY_SIZE} ${SHM_SIZE} $DOCKER_SHA
-[[ ! -z "$RUN_DOC_TESTS" ]] && bash $ROOT_DIR/run_doc_tests.sh ${MEMORY_SIZE} ${SHM_SIZE} $DOCKER_SHA
-[[ ! -z "$RUN_SGD_TESTS" ]] && bash $ROOT_DIR/run_sgd_tests.sh ${MEMORY_SIZE} ${SHM_SIZE} $DOCKER_SHA
+[[ ! -z "$RUN_TUNE_TESTS" ]] && bash $ROOT_DIR/run_tune_tests.sh ${MEMORY_SIZE} ${SHM_SIZE}
+[[ ! -z "$RUN_DOC_TESTS" ]] && bash $ROOT_DIR/run_doc_tests.sh ${MEMORY_SIZE} ${SHM_SIZE}
+[[ ! -z "$RUN_SGD_TESTS" ]] && bash $ROOT_DIR/run_sgd_tests.sh ${MEMORY_SIZE} ${SHM_SIZE}
