79320d7ecebbb97f823735ffe14a8bb53f4eeb5b
sven1977 <svenmika1977@gmail.com>
2020-04-10 12:23:25 +0200

    Fixes.

--- rllib/agents/sac/sac.py
+++ rllib/agents/sac/sac.py
@@ -99,8 +99,10 @@ DEFAULT_CONFIG = with_common_config({
 # __sphinx_doc_end__
 # yapf: enable
 
+
 SACTrainer = GenericOffPolicyTrainer.with_updates(
     name="SAC",
     default_config=DEFAULT_CONFIG,
+    validate_config=None,
     default_policy=SACTFPolicy,
     get_policy_class=lambda c: SACTFPolicy)
