84d2898c2cb3d891c011e3d41b2c224c1eb33a29
jovany-wang <kingchin1218@126.com>
2020-04-15 11:24:16 +0800

    Fix

--- java/test.sh
+++ java/test.sh
@@ -8,7 +8,10 @@ set -x
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 
 run_testng() {
+    # The command `$@` will exited immediately once `set -e`.
+    set +e
     $@
+    set -e
     local exit_code=$?
     # exit_code == 2 means there are skipped tests.
     if [ $exit_code -ne 2 ] && [ $exit_code -ne 0 ] ; then
