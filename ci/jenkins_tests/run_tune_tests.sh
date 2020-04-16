5c274fe631d9e3d15d4c3016202e9fee51a0dae1
Servon <servon@hhu.edu.cn>
2020-04-16 12:13:29 +0800

    [Tune] Add ZOOpt search algorithm (#7960)
    * add zoopt
    
    * add zoopt search algo
    
    * add zoopt
    
    * fix zoopt
    
    * add zoopt requirements
    
    * fix zoopt
    
    * remove generated guides
    
    * Apply suggestions from code review
    
    Co-authored-by: Richard Liaw <rliaw@berkeley.edu>

--- ci/jenkins_tests/run_tune_tests.sh
+++ ci/jenkins_tests/run_tune_tests.sh
@@ -127,6 +127,10 @@ $SUPPRESS_OUTPUT docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} 
     python /ray/python/ray/tune/examples/dragonfly_example.py \
     --smoke-test
 
+$SUPPRESS_OUTPUT docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} --memory-swap=-1 $DOCKER_SHA \
+    python /ray/python/ray/tune/examples/zoopt_example.py \
+    --smoke-test
+
 # Commenting out because flaky
 # $SUPPRESS_OUTPUT docker run --rm --shm-size=${SHM_SIZE} --memory=${MEMORY_SIZE} --memory-swap=-1 $DOCKER_SHA \
 #     python /ray/python/ray/tune/examples/pbt_memnn_example.py \
