41f0191e5257edc50ba67ed180fe8ce02d5534db
sven1977 <svenmika1977@gmail.com>
2020-04-20 14:02:33 +0200

    WIP.

--- rllib/models/torch/torch_action_dist.py
+++ rllib/models/torch/torch_action_dist.py
@@ -1,14 +1,18 @@
+import functools
 from math import log
 import numpy as np
 
 from ray.rllib.models.action_dist import ActionDistribution
+from ray.rllib.utils import try_import_tree
 from ray.rllib.utils.annotations import override
-from ray.rllib.utils import try_import_torch
+from ray.rllib.utils.framework import try_import_torch
 from ray.rllib.utils.numpy import SMALL_NUMBER, MIN_LOG_NN_OUTPUT, \
     MAX_LOG_NN_OUTPUT
+from ray.rllib.utils.space_utils import get_base_struct_from_space
 from ray.rllib.utils.torch_ops import atanh
 
 torch, nn = try_import_torch()
+tree = try_import_tree()
 
 
 class TorchDistributionWrapper(ActionDistribution):
@@ -305,3 +309,95 @@ class TorchDeterministic(TorchDistributionWrapper):
     @override(ActionDistribution)
     def required_model_output_shape(action_space, model_config):
         return np.prod(action_space.shape)
+
+
+class TorchMultiActionDistribution(TorchDistributionWrapper):
+    """Action distribution that operates for list of (flattened) actions.
+
+    Args:
+        inputs (Tensor list): A list of tensors from which to compute samples.
+    """
+
+    def __init__(self,
+                 inputs,
+                 model,
+                 *,
+                 child_distributions,
+                 input_lens,
+                 action_space,
+                 action_space_struct=None):
+
+        if not isinstance(inputs, torch.Tensor):
+            inputs = torch.Tensor(inputs)
+        super().__init__(inputs, model)
+
+        self.action_space_struct = action_space_struct
+        if self.action_space_struct is None:
+            self.action_space_struct = get_base_struct_from_space(action_space)
+        split_inputs = torch.split(inputs, input_lens, dim=1)
+        self.flat_child_distributions = tree.map_structure(
+            lambda dist, input_: dist(input_, model), child_distributions,
+            list(split_inputs))
+
+    @override(ActionDistribution)
+    def logp(self, x):
+        if isinstance(x, np.ndarray):
+            x = torch.Tensor(x)
+        # Single tensor input (all merged).
+        if isinstance(x, torch.Tensor):
+            split_indices = []
+            for dist in self.flat_child_distributions:
+                if isinstance(dist, TorchCategorical):
+                    split_indices.append(1)
+                else:
+                    split_indices.append(dist.sample().size()[1])
+            split_x = list(torch.split(x, split_indices, dim=1))
+        # Structured or flattened (by single action component) input.
+        else:
+            split_x = tree.flatten(x)
+
+        def map_(val, dist):
+            # Remove extra categorical dimension.
+            if isinstance(dist, TorchCategorical):
+                val = torch.squeeze(val, dim=-1).int()
+            return dist.logp(val)
+
+        # Remove extra categorical dimension and take the logp of each
+        # component.
+        flat_logps = tree.map_structure(map_, split_x,
+                                        self.flat_child_distributions)
+
+        return functools.reduce(lambda a, b: a + b, flat_logps)
+
+    @override(ActionDistribution)
+    def kl(self, other):
+        kl_list = [
+            d.kl(o) for d, o in zip(self.flat_child_distributions,
+                                    other.flat_child_distributions)
+        ]
+        return functools.reduce(lambda a, b: a + b, kl_list)
+
+    @override(ActionDistribution)
+    def entropy(self):
+        entropy_list = [d.entropy() for d in self.flat_child_distributions]
+        return functools.reduce(lambda a, b: a + b, entropy_list)
+
+    @override(ActionDistribution)
+    def sample(self):
+        child_distributions = tree.unflatten_as(self.action_space_struct,
+                                                self.flat_child_distributions)
+        return tree.map_structure(lambda s: s.sample(), child_distributions)
+
+    @override(ActionDistribution)
+    def deterministic_sample(self):
+        child_distributions = tree.unflatten_as(self.action_space_struct,
+                                                self.flat_child_distributions)
+        return tree.map_structure(lambda s: s.deterministic_sample(),
+                                  child_distributions)
+
+    @override(TorchDistributionWrapper)
+    def sampled_action_logp(self):
+        p = self.flat_child_distributions[0].sampled_action_logp()
+        for c in self.flat_child_distributions[1:]:
+            p += c.sampled_action_logp()
+        return p
---
+++ rllib/utils/space_utils.py
@@ -0,0 +1,42 @@
+from gym.spaces import Tuple, Dict
+
+
+def flatten_space(space):
+    """Flattens a gym.Space into its primitive components.
+
+    Primitive components are any non Tuple/Dict spaces.
+
+    Args:
+        space(gym.Space): The gym.Space to flatten. This may be any
+            supported type (including nested Tuples and Dicts).
+
+    Returns:
+        List[gym.Space]: The flattened list of primitive Spaces. This list
+            does not contain Tuples or Dicts anymore.
+    """
+
+    def _helper_flatten(space_, l):
+        if isinstance(space_, Tuple):
+            for s in space_:
+                _helper_flatten(s, l)
+        elif isinstance(space_, Dict):
+            for k in space_.spaces:
+                _helper_flatten(space_[k], l)
+        else:
+            l.append(space_)
+
+    ret = []
+    _helper_flatten(space, ret)
+    return ret
+
+
+def get_base_struct_from_space(space):
+    def _helper_struct(space_):
+        if isinstance(space_, Tuple):
+            return tuple([_helper_struct(s) for s in space_])
+        elif isinstance(space_, Dict):
+            return {k: _helper_struct(space_[k]) for k in space_.spaces}
+        else:
+            return space_
+
+    return _helper_struct(space)
