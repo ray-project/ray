21bf95e4b27f9d301f31f7098a089acc78d402ce
Edward Oakes <ed.nmi.oakes@gmail.com>
2020-04-20 11:27:45 -0500

    separate test

--- python/ray/serve/tests/test_nonblocking.py
+++ python/ray/serve/tests/test_nonblocking.py
@@ -1,4 +1,3 @@
-import pytest
 import requests
 import sys
 
@@ -15,8 +14,9 @@ def test_nonblocking():
     serve.create_backend(function, "echo:v1")
     serve.link("endpoint", "echo:v1")
 
-    resp = requests.get("http://127.0.0.1:8001/api").json()["method"]
+    resp = requests.get("http://127.0.0.1:8000/api").json()["method"]
     assert resp == "GET"
+
 
 if __name__ == "__main__":
     import pytest
