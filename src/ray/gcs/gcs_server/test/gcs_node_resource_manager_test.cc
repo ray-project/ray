6547d763402fe7a77342fdedb4bed696a7b1e332
灵洵 <fengbin.ffb@antfin.com>
2020-04-21 22:07:21 +0800

    rm unused import

--- src/ray/gcs/gcs_server/test/gcs_node_resource_manager_test.cc
+++ src/ray/gcs/gcs_server/test/gcs_node_resource_manager_test.cc
@@ -12,14 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <ray/gcs/test/gcs_test_util.h>
-
-#include <memory>
 #include "gtest/gtest.h"
 
 #include "ray/gcs/gcs_server/gcs_node_resource_manager.h"
 
 namespace ray {
+
 class GcsNodeResourceManagerTest : public ::testing::Test {};
 
 TEST_F(GcsNodeResourceManagerTest, TestApi) {
