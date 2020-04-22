fc66efb879079ad64942140c98880dfd1210469f
chentianyi <chentianyi.cty@antfin.com>
2020-04-22 10:37:04 +0800

    Fix ut(remove powermock annotation using).

--- streaming/java/streaming-runtime/src/test/java/io/ray/streaming/runtime/master/JobMasterTest.java
+++ streaming/java/streaming-runtime/src/test/java/io/ray/streaming/runtime/master/JobMasterTest.java
@@ -14,7 +14,7 @@ public class JobMasterTest {
     Assert.assertNull(jobMaster.getGraphManager());
     Assert.assertNull(jobMaster.getResourceManager());
     Assert.assertNull(jobMaster.getJobMasterActor());
-    Assert.assertFalse(jobMaster.init(false));
+    Assert.assertFalse(jobMaster.init());
   }
 
 }
