2386b1a63f1e56ff4684eb8bc26dfaa01a0605e9
刘宝 <po.lb@antfin.com>
2020-04-23 10:34:04 +0800

    Add java_worker_options for ray.init()

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -542,6 +542,7 @@ def init(address=None,
          raylet_socket_name=None,
          temp_dir=None,
          load_code_from_local=False,
+         java_worker_options=None,
          use_pickle=True,
          _internal_config=None,
          lru_evict=False):
@@ -647,6 +648,7 @@ def init(address=None,
             conventional location, e.g., "/tmp/ray".
         load_code_from_local: Whether code should be loaded from a local
             module or from the GCS.
+        java_worker_options: Overwrite the options to start Java workers.
         use_pickle: Deprecated.
         _internal_config (str): JSON configuration for overriding
             RayConfig defaults. For testing purposes ONLY.
@@ -743,6 +745,7 @@ def init(address=None,
             raylet_socket_name=raylet_socket_name,
             temp_dir=temp_dir,
             load_code_from_local=load_code_from_local,
+            java_worker_options=java_worker_options,
             _internal_config=_internal_config,
         )
         # Start the Ray processes. We set shutdown_at_exit=False because we
@@ -793,6 +796,9 @@ def init(address=None,
         if raylet_socket_name is not None:
             raise ValueError("When connecting to an existing cluster, "
                              "raylet_socket_name must not be provided.")
+        if java_worker_options is not None:
+            raise ValueError("When connecting to an existing cluster, "
+                             "java_worker_options must not be provided.")
         if _internal_config is not None:
             logger.warning(
                 "When connecting to an existing cluster, "
