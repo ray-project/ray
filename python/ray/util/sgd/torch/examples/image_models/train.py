72953dcd48d975d1a17ec25453b5262aa93194f8
Maksim Smolin <maximsmol@gmail.com>
2020-04-27 08:44:39 -0700

    Update CI

--- python/ray/util/sgd/torch/examples/image_models/train.py
+++ python/ray/util/sgd/torch/examples/image_models/train.py
@@ -7,12 +7,8 @@
 # Note: other authors MUST include themselves in the above copyright notice
 #       in order to abide by the terms of the Apache license
 
-import random
-import os
 from os.path import join
 
-import numpy as np
-import PIL
 from tqdm import trange
 
 import torch.nn as nn
@@ -30,13 +26,13 @@ from ray.util.sgd import TorchTrainer
 # from ray.util.sgd.torch import TrainingOperator
 
 from ray.util.sgd.torch.examples.image_models.args import parse_args
-
+import ray.util.sgd.torch.examples.image_models.util as util
 
 def model_creator(config):
     args = config["args"]
 
     model = create_model(
-        "resnet101",  # args.model,
+        args.model,
         pretrained=args.pretrained,
         num_classes=args.num_classes,
         drop_rate=args.drop,
@@ -66,37 +62,7 @@ def data_creator(config):
     val_dir = join(args.data, "val")
 
     if args.mock_data:
-        os.makedirs(train_dir, exist_ok=True)
-        os.makedirs(val_dir, exist_ok=True)
-
-        max_cls_n = 99999999
-        total_classes = 3
-        per_cls = max_cls_n // total_classes
-
-        max_img_n = 99999999
-        total_imgs = 3
-        per_img = max_img_n // total_imgs
-
-        def mock_class(base, n):
-            random_cls = random.randint(per_cls * n, per_cls * n + per_cls)
-            sub_dir = join(base, "n{:08d}".format(random_cls))
-            os.makedirs(sub_dir, exist_ok=True)
-
-            for i in range(total_imgs):
-                random_img = random.randint(per_img * i, per_img * i + per_img)
-                file = join(sub_dir,
-                            "ILSVRC2012_val_{:08d}.JPEG".format(random_img))
-
-                PIL.Image.fromarray(np.zeros((375, 500, 3),
-                                             dtype=np.uint8)).save(file)
-
-        existing_train_cls = len(os.listdir(train_dir))
-        for i in range(existing_train_cls, total_classes):
-            mock_class(train_dir, i)
-
-        existing_val_cls = len(os.listdir(val_dir))
-        for i in range(existing_val_cls, total_classes):
-            mock_class(val_dir, i)
+        util.mock_data(train_dir, val_dir)
 
     # todo: verbose should depend on rank
     data_config = resolve_data_config(vars(args), verbose=True)
