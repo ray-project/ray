03babb7d20039e4b78e7291b08ec78a3f9bcf964
Ian Rodney <ian.rodney@gmail.com>
2020-04-27 14:09:52 -0700

    Space

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -1685,10 +1685,10 @@ def cancel(object_id, force=False):
     """Cancels a locally-submitted task according to the following conditions.
 
     If the specified task is pending execution, it will not be executed. If
-    the task is currently executing, the behavior depends on the ``force`` flag.
-    When ``force=False``, a KeyboardInterrupt will be raised in Python and when
-    ``force=True``, the executing the task will immediately exit. If the task is
-    already finished, nothing will happen.
+    the task is currently executing, the behavior depends on the ``force``
+    flag. When ``force=False``, a KeyboardInterrupt will be raised in Python
+    and when ``force=True``, the executing the task will immediately exit. If
+    the task is already finished, nothing will happen.
 
     Only non-actor tasks can be canceled. Canceled tasks will not be
     retried (max_retries will not be respected).
