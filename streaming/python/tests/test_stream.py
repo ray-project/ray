e92403708513db404078cdb2202fc3d8e77c43af
chaokunyang <shawn.ck.yang@gmail.com>
2020-04-28 17:15:10 +0800

    fix test

--- streaming/python/tests/test_stream.py
+++ streaming/python/tests/test_stream.py
@@ -3,8 +3,7 @@ from ray.streaming import StreamingContext
 
 
 def test_data_stream():
-    if not ray.is_initialized():
-        ray.init(load_code_from_local=True, include_java=True)
+    ray.init(load_code_from_local=True, include_java=True)
     ctx = StreamingContext.Builder().build()
     stream = ctx.from_values(1, 2, 3)
     java_stream = stream.as_java_stream()
@@ -14,11 +13,11 @@ def test_data_stream():
     python_stream.set_parallelism(10)
     assert stream.get_parallelism() == java_stream.get_parallelism()
     assert stream.get_parallelism() == python_stream.get_parallelism()
+    ray.shutdown()
 
 
 def test_key_data_stream():
-    if not ray.is_initialized():
-        ray.init(load_code_from_local=True, include_java=True)
+    ray.init(load_code_from_local=True, include_java=True)
     ctx = StreamingContext.Builder().build()
     key_stream = ctx.from_values(
         "a", "b", "c").map(lambda x: (x, 1)).key_by(lambda x: x[0])
@@ -29,3 +28,4 @@ def test_key_data_stream():
     python_stream.set_parallelism(10)
     assert key_stream.get_parallelism() == java_stream.get_parallelism()
     assert key_stream.get_parallelism() == python_stream.get_parallelism()
+    ray.shutdown()
