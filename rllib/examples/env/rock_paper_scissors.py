c876a67d057b7033f54b945f531492e721055dd9
sven1977 <svenmika1977@gmail.com>
2020-04-30 17:02:55 +0200

    WIP.

--- rllib/examples/env/rock_paper_scissors.py
+++ rllib/examples/env/rock_paper_scissors.py
@@ -11,10 +11,13 @@ class RockPaperScissors(MultiAgentEnv):
     ROCK = 0
     PAPER = 1
     SCISSORS = 2
+    LIZARD = 3
+    SPOCK = 4
 
-    def __init__(self, _):
+    def __init__(self, config):
         self.action_space = Discrete(3)
         self.observation_space = Discrete(3)
+        self.sheldon_cooper = config.get("sheldon_cooper", False)
         self.player1 = "player1"
         self.player2 = "player2"
         self.last_move = None
@@ -31,6 +34,10 @@ class RockPaperScissors(MultiAgentEnv):
     def step(self, action_dict):
         move1 = action_dict[self.player1]
         move2 = action_dict[self.player2]
+        if self.sheldon_cooper is False:
+            assert move1 not in [self.LIZARD, self.SPOCK]
+            assert move2 not in [self.LIZARD, self.SPOCK]
+
         self.last_move = (move1, move2)
         obs = {
             self.player1: self.last_move[1],
@@ -46,6 +53,23 @@ class RockPaperScissors(MultiAgentEnv):
             (self.SCISSORS, self.ROCK): (-1, 1),
             (self.SCISSORS, self.PAPER): (1, -1),
             (self.SCISSORS, self.SCISSORS): (0, 0),
+            # Sheldon Cooper extension:
+            (self.LIZARD, self.LIZARD): (0, 0),
+            (self.LIZARD, self.SPOCK): (1, -1),  # Lizard poisons Spock
+            (self.LIZARD, self.ROCK): (-1, 1),  # Rock crushes lizard
+            (self.LIZARD, self.PAPER): (1, -1),  # Lizard eats paper
+            (self.LIZARD, self.SCISSORS): (-1, 1),  # Scissors decapitate Lizrd
+            (self.ROCK, self.LIZARD): (1, -1),  # Rock crushes lizard
+            (self.PAPER, self.LIZARD): (-1, 1),  # Lizard eats paper
+            (self.SCISSORS, self.LIZARD): (1, -1),  # Scissors decapitate Lizrd
+            (self.SPOCK, self.SPOCK): (0, 0),
+            (self.SPOCK, self.LIZARD): (-1, 1),  # Lizard poisons Spock
+            (self.SPOCK, self.ROCK): (1, -1),  # Spock vaporizes rock
+            (self.SPOCK, self.PAPER): (-1, 1),  # Paper disproves Spock
+            (self.SPOCK, self.SCISSORS): (1, -1),  # Spock smashes scissors
+            (self.ROCK, self.SPOCK): (-1, 1),  # Spock vaporizes rock
+            (self.PAPER, self.SPOCK): (1, -1),  # Paper disproves Spock
+            (self.SCISSORS, self.SPOCK): (-1, 1),  # Spock smashes scissors
         }[move1, move2]
         rew = {
             self.player1: r1,
