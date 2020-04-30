7eaea4b2385c593e9c5b0a0c40d4effa82a28c66
simon-mo <xmo@berkeley.edu>
2020-04-30 11:00:32 -0700

    Build from source in jenkins

--- ci/jenkins_tests/run_multi_node_tests.sh
+++ ci/jenkins_tests/run_multi_node_tests.sh
@@ -5,5 +5,6 @@ ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 export RUN_TUNE_TESTS=1
 export RUN_DOC_TESTS=1
 export RUN_SGD_TESTS=1
+export CI_BUILD_FROM_SOURCE=1
 
 bash $ROOT_DIR/entry_point.sh
