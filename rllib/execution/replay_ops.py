60c126269faa169edce0af045ba2db2f97a09cca
Eric Liang <ekhliang@gmail.com>
2020-05-06 14:14:03 -0700

    fix

--- rllib/execution/replay_ops.py
+++ rllib/execution/replay_ops.py
@@ -91,3 +91,74 @@ def Replay(*,
                 yield item
 
     return LocalIterator(gen_replay, SharedMetrics())
+
+
+class MixInReplay:
+    """This operator adds replay to a stream of experiences.
+
+    It takes input batches, and returns a list of batches that include replayed
+    data as well. The number of replayed batches is determined by the
+    configured replay proportion. The max age of a batch is determined by the
+    number of replay slots.
+    """
+
+    def __init__(self, num_slots, replay_proportion: float = None):
+        """Initialize MixInReplay.
+
+        Args:
+            num_slots (int): Number of batches to store in total.
+            replay_proportion (float): If None, one batch will be replayed per
+                each input batch. Otherwise, the input batch will be returned
+                and an additional number of batches proportional to this value
+                will be added as well.
+
+        Examples:
+            # 1:1 mode (default)
+            >>> replay_op = MixInReplay(rollouts, 100)
+            >>> print(next(replay_op))
+            SampleBatch(<replay>)
+
+            # proportional mode
+            >>> replay_op = MixInReplay(rollouts, 100, replay_proportion=2)
+            >>> print(next(replay_op))
+            [SampleBatch(<input>), SampleBatch(<replay>), SampleBatch(<rep.>)]
+
+            # proportional mode, replay disabled
+            >>> replay_op = MixInReplay(rollouts, 100, replay_proportion=0)
+            >>> print(next(replay_op))
+            [SampleBatch(<input>)]
+        """
+        if replay_proportion is not None:
+            if replay_proportion > 0 and num_slots == 0:
+                raise ValueError(
+                    "You must set num_slots > 0 if replay_proportion > 0.")
+        elif num_slots == 0:
+            raise ValueError(
+                "You must set num_slots > 0 if replay_proportion = None.")
+        self.num_slots = num_slots
+        self.replay_proportion = replay_proportion
+        self.replay_batches = []
+        self.replay_index = 0
+
+    def __call__(self, sample_batch):
+        # Put in replay buffer if enabled.
+        if self.num_slots > 0:
+            if len(self.replay_batches) < self.num_slots:
+                self.replay_batches.append(sample_batch)
+            else:
+                self.replay_batches[self.replay_index] = sample_batch
+                self.replay_index += 1
+                self.replay_index %= self.num_slots
+
+        # 1:1 replay mode.
+        if self.replay_proportion is None:
+            return random.choice(self.replay_batches)
+
+        # Proportional replay mode.
+        output_batches = [sample_batch]
+        f = self.replay_proportion
+        while random.random() < f:
+            f -= 1
+            replay_batch = random.choice(self.replay_batches)
+            output_batches.append(replay_batch)
+        return output_batches
