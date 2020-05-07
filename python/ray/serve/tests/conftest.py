b205b265db8d8ea819a1ea5285bc72a5956f125f
Edward Oakes <ed.nmi.oakes@gmail.com>
2020-05-07 11:00:00 -0500

    clear state between serve tests

--- python/ray/serve/tests/conftest.py
+++ python/ray/serve/tests/conftest.py
@@ -15,10 +15,12 @@ def _serve_instance():
     serve.init(blocking=True, ray_init_kwargs={"num_cpus": 36})
     yield
 
+
 @pytest.fixture
 def serve_instance(_serve_instance):
     yield
     master = serve.api._get_master_actor()
+    # Clear all state between tests to avoid naming collisions.
     for endpoint in retry_actor_failures(master.get_all_endpoints):
         serve.delete_endpoint(endpoint)
     for backend in retry_actor_failures(master.get_all_backends):
