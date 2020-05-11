77280fdb528243eb1b2828db787e52955027aa4d
Max Fitton <max@semprehealth.com>
2020-05-11 15:17:37 -0700

    remove import accidentally added

--- python/ray/actor.py
+++ python/ray/actor.py
@@ -4,7 +4,6 @@ import weakref
 
 from abc import ABCMeta, abstractmethod
 from collections import namedtuple
-from ray.core.src.ray.raylet 
 import ray.ray_constants as ray_constants
 import ray._raylet
 import ray.signature as signature
