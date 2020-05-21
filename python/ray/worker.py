d27e6da1b291887707533cc10f066cd0e888607b
Hao Chen <chenh1024@gmail.com>
2020-05-21 16:12:44 +0800

    Fix a lint issue (#8530)

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -1265,8 +1265,8 @@ def connect(node,
     )
 
     # Create an object for interfacing with the global state.
-    # Note, global state should be intialized after `CoreWorker`, because it will
-    # use glog, which is intialized in `CoreWorker`.
+    # Note, global state should be intialized after `CoreWorker`, because it
+    # will use glog, which is intialized in `CoreWorker`.
     ray.state.state._initialize_global_state(
         node.redis_address, redis_password=node.redis_password)
 
