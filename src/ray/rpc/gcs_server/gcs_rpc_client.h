83935d870e1339c8474109602f4c153c23f4b74f
灵洵 <fengbin.ffb@antfin.com>
2020-05-22 20:46:42 +0800

    fix windows compile bug

--- src/ray/rpc/gcs_server/gcs_rpc_client.h
+++ src/ray/rpc/gcs_server/gcs_rpc_client.h
@@ -15,6 +15,7 @@
 #ifndef RAY_RPC_GCS_RPC_CLIENT_H
 #define RAY_RPC_GCS_RPC_CLIENT_H
 
+#include <unistd.h>
 #include "src/ray/common/network_util.h"
 #include "src/ray/protobuf/gcs_service.grpc.pb.h"
 #include "src/ray/rpc/grpc_client.h"
