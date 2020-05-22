168fe2990dddb4a148bde0acb16a266d13e9f24c
灵洵 <fengbin.ffb@antfin.com>
2020-05-22 12:32:30 +0800

    add function comments

--- src/ray/gcs/accessor.h
+++ src/ray/gcs/accessor.h
@@ -151,6 +151,10 @@ class ActorInfoAccessor {
       const ActorID &actor_id,
       const OptionalItemCallback<rpc::ActorCheckpointIdData> &callback) = 0;
 
+  /// Reestablish subscription.
+  /// This should be called when GCS server restarts from a failure.
+  ///
+  /// \return Status
   virtual Status AsyncReSubscribe() = 0;
 
  protected:
