e2a57354cecaf46c92be23a1d63a9e15c371b3c5
sven1977 <svenmika1977@gmail.com>
2020-05-26 19:44:46 +0200

    WIP.

--- rllib/tests/test_attention_net_learning.py
+++ rllib/tests/test_attention_net_learning.py
@@ -1,7 +1,7 @@
 import unittest
 
 from ray import tune
-import ray.rllib.agents.pg as pg
+from ray.tune import grid_search
 from ray.rllib.examples.env.stateless_cartpole import StatelessCartPole
 from ray.rllib.models.catalog import ModelCatalog
 from ray.rllib.models.tf.attention_net import GTrXLNet
@@ -65,11 +65,25 @@ class TestAttentionNetLearning(unittest.TestCase):
     def test_impala_attention_net_learning(self):
         ModelCatalog.register_custom_model("attention_net", GTrXLNet)
         config = dict(self.config, **{
+            #"num_workers": 4,
             "num_gpus": 0,
-            #"entropy_coeff": 0.0001,
-            "vf_loss_coeff": 1e-3,
-            "lr": 0.00005,
+            "entropy_coeff": 0.01, #grid_search([0.01, 0.001]),
+            "vf_loss_coeff": 0.001, #grid_search([1e-4, 1e-3]),
+            "lr": 0.001,  #grid_search([0.0005, 0.001, 0.002]),
+            #"lr_schedule": [[0, 5e-4], [1000000, 0.000001]],
             #"num_sgd_iter": 5,
+            "model": {
+                "custom_model": "attention_net",
+                "max_seq_len": 65,  #grid_search([30, 70, 80]),
+                "custom_model_config": {
+                    "num_transformer_units": grid_search([1, 2]),
+                    "attn_dim": grid_search([32, 64]),
+                    "num_heads": 1,
+                    "memory_tau": grid_search([10, 5]),
+                    "head_dim": 32,
+                    "ff_hidden_dim": 32,
+                },
+            },
         })
         tune.run("IMPALA", config=config, stop=self.stop, verbose=1)
 
