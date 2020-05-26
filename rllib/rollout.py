33eda984f7167baa11110c6a4db147d1fe352bbc
sven1977 <svenmika1977@gmail.com>
2020-05-26 12:06:44 +0200

    WIP.

--- rllib/rollout.py
+++ rllib/rollout.py
@@ -16,7 +16,7 @@ from ray.rllib.env.base_env import _DUMMY_AGENT_ID
 from ray.rllib.evaluation.worker_set import WorkerSet
 from ray.rllib.policy.sample_batch import DEFAULT_POLICY_ID
 from ray.rllib.utils.deprecation import deprecation_warning
-from ray.rllib.utils.space_utils import flatten_to_single_ndarray
+from ray.rllib.utils.spaces.space_utils import flatten_to_single_ndarray
 from ray.tune.utils import merge_dicts
 from ray.tune.registry import get_trainable_cls
 
