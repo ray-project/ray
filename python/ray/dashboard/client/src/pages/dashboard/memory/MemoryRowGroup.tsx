483d63214d94a6040ed5c3aa41e090c6ae732566
Max Fitton <max@semprehealth.com>
2020-05-26 19:41:42 -0700

    Update MemoryRowGroup to use useState hook instead of being a class

--- python/ray/dashboard/client/src/pages/dashboard/memory/MemoryRowGroup.tsx
+++ python/ray/dashboard/client/src/pages/dashboard/memory/MemoryRowGroup.tsx
@@ -9,15 +9,12 @@ import {
 import AddIcon from "@material-ui/icons/Add";
 import RemoveIcon from "@material-ui/icons/Remove";
 import classNames from "classnames";
-import React from "react";
+import React, { useState } from "react";
 import {
   MemoryTableEntry,
   MemoryTableResponse,
   MemoryTableSummary,
 } from "../../../api";
-import {
-  descendingComparator
-} from "../../../common/tableUtils";
 import MemorySummary from "./MemorySummary";
 import { MemoryTableRow } from "./MemoryTableRow";
 
@@ -47,80 +44,63 @@ type Props = {
   initialExpanded: boolean;
 };
 
-type State = {
-  expanded: boolean;
-};
+const MemoryRowGroup = (props: Props & WithStyles<typeof styles>) => {
+  const { classes, groupKey, memoryTableGroups } = props;
+  const [expanded, setExpanded] = useState(props.initialExpanded);
+  const toggleExpanded = () => setExpanded(!expanded);
 
-class MemoryRowGroup extends React.Component<
-  Props & WithStyles<typeof styles>,
-  State
-> {
-  state: State = {
-    expanded: this.props.initialExpanded,
-  };
+  const features = [
+    "node_ip_address",
+    "pid",
+    "type",
+    "object_id",
+    "object_size",
+    "reference_type",
+    "call_site",
+  ];
 
-  toggleExpand = () => {
-    this.setState((state) => ({
-      expanded: !state.expanded,
-    }));
-  };
+  const memoryTableGroup = memoryTableGroups[groupKey];
+  const entries: Array<MemoryTableEntry> = memoryTableGroup["entries"];
+  const summary: MemoryTableSummary = memoryTableGroup["summary"];
 
-  render() {
-    const { classes, groupKey, memoryTableGroups } = this.props;
-    const { expanded } = this.state;
-
-    const features = [
-      "node_ip_address",
-      "pid",
-      "type",
-      "object_id",
-      "object_size",
-      "reference_type",
-      "call_site",
-    ];
-
-    const memoryTableGroup = memoryTableGroups[groupKey];
-    const entries: Array<MemoryTableEntry> = memoryTableGroup["entries"];
-    const summary: MemoryTableSummary = memoryTableGroup["summary"];
-
-    return (
-      <React.Fragment>
-        <TableRow hover>
-          <TableCell
-            className={classNames(classes.cell, classes.expandCollapseCell)}
-            onClick={this.toggleExpand}
-          >
-            {!expanded ? (
-              <AddIcon className={classes.expandCollapseIcon} />
-            ) : (
-              <RemoveIcon className={classes.expandCollapseIcon} />
-            )}
+  return (
+    <React.Fragment>
+      <TableRow hover>
+        <TableCell
+          className={classNames(classes.cell, classes.expandCollapseCell)}
+          onClick={toggleExpanded}
+        >
+          {!expanded ? (
+            <AddIcon className={classes.expandCollapseIcon} />
+          ) : (
+            <RemoveIcon className={classes.expandCollapseIcon} />
+          )}
+        </TableCell>
+        {features.map((feature, index) => (
+          <TableCell className={classes.cell} key={index}>
+            {// TODO(sang): For now, it is always grouped by node_ip_address.
+            feature === "node_ip_address" ? groupKey : ""}
           </TableCell>
-          {features.map((feature, index) => (
-            <TableCell className={classes.cell} key={index}>
-              {// TODO(sang): For now, it is always grouped by node_ip_address.
-              feature === "node_ip_address" ? groupKey : ""}
-            </TableCell>
-          ))}
-        </TableRow>
-        {expanded && (
-          <React.Fragment>
-            <MemorySummary
-              initialExpanded={false}
-              memoryTableSummary={summary}
-            />
-            {entries.map((memoryTableEntry, index) => {
+        ))}
+      </TableRow>
+      {expanded && (
+        <React.Fragment>
+          <MemorySummary
+            initialExpanded={false}
+            memoryTableSummary={summary}
+          />
+          {entries.map((memoryTableEntry, index) => {
               return <MemoryTableRow 
                 memoryTableEntry={memoryTableEntry}
                 key={`${index}`}
                 cellClassName={classes.cell}
               />
             })}
-          </React.Fragment>
-        )}
-      </React.Fragment>
-    );
-  }
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
 }
+
 
 export default withStyles(styles)(MemoryRowGroup);
