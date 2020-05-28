675ccbc799e30ab8ca1bf074204881ff325a342c
Tao Wang <dooku.wt@antfin.com>
2020-05-28 10:27:38 +0800

    Resubscribe worker table info when gcs service restart (#8606)

--- src/ray/gcs/gcs_client/service_based_gcs_client.cc
+++ src/ray/gcs/gcs_client/service_based_gcs_client.cc
@@ -52,6 +52,7 @@ Status ServiceBasedGcsClient::Connect(boost::asio::io_service &io_service) {
     RAY_CHECK_OK(actor_accessor_->AsyncReSubscribe());
     RAY_CHECK_OK(node_accessor_->AsyncReSubscribe());
     RAY_CHECK_OK(task_accessor_->AsyncReSubscribe());
+    RAY_CHECK_OK(worker_accessor_->AsyncReSubscribe());
   };
 
   // Connect to gcs service.
