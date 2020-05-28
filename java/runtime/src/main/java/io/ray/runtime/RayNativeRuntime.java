5ee425a24c9eecb27dab1608ec154d85f71a45db
lingxuan.zlx <skyzlxuan@gmail.com>
2020-05-28 13:41:01 +0800

    remove duplicated reset config

--- java/runtime/src/main/java/io/ray/runtime/RayNativeRuntime.java
+++ java/runtime/src/main/java/io/ray/runtime/RayNativeRuntime.java
@@ -108,7 +108,6 @@ public final class RayNativeRuntime extends AbstractRayRuntime {
         manager.cleanup();
         manager = null;
       }
-      RayConfig.reset();
     }
     if (null != gcsClient) {
       gcsClient.destroy();
