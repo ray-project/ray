f9d0cba36ac322bff65d5e227b29daa941b2e4eb
Ian Rodney <ian.rodney@gmail.com>
2020-06-02 19:31:30 -0700

    switched to flag

--- python/ray/autoscaler/log_timer.py
+++ python/ray/autoscaler/log_timer.py
@@ -5,18 +5,19 @@ logger = logging.getLogger(__name__)
 
 
 class LogTimer:
-    def __init__(self, message, success_string=""):
+    def __init__(self, message, show_status=True):
         self._message = message
-        self._status_string = success_string
+        self._show_status = show_status
 
     def __enter__(self):
         self._start_time = datetime.datetime.utcnow()
 
     def __exit__(self, *error_vals):
         td = datetime.datetime.utcnow() - self._start_time
-        if any(error_vals):
-            self._status_string = "failed"
+        status = ""
+        if self._show_status:
+            status = "failed" if any(error_vals) else "succeeded"
         logger.info(" ".join([
-            self._message, self._status_string,
+            self._message, status,
             "[LogTimer={:.0f}ms]".format(td.total_seconds() * 1000)
         ]))
--- python/ray/autoscaler/updater.py
+++ python/ray/autoscaler/updater.py
@@ -423,16 +423,15 @@ class NodeUpdater:
             # Run init commands
             self.provider.set_node_tags(
                 self.node_id, {TAG_RAY_NODE_STATUS: STATUS_SETTING_UP})
-            with LogTimer(self.log_prefix + "Initialization commands",
-                          "completed"):
+            with LogTimer(self.log_prefix + "Initialization commands"):
                 for cmd in self.initialization_commands:
                     self.cmd_runner.run(cmd)
 
-            with LogTimer(self.log_prefix + "Setup commands", "completed"):
+            with LogTimer(self.log_prefix + "Setup commands"):
                 for cmd in self.setup_commands:
                     self.cmd_runner.run(cmd)
 
-        with LogTimer(self.log_prefix + "Ray start commands", "completed"):
+        with LogTimer(self.log_prefix + "Ray start commands"):
             for cmd in self.ray_start_commands:
                 self.cmd_runner.run(cmd)
 
