72f048134df5936eb2b23d6657c3fa3ca6763a25
灵洵 <fengbin.ffb@antfin.com>
2020-06-03 16:58:04 +0800

    fix review comments

--- python/ray/tests/test_gcs_fault_tolerance.py
+++ python/ray/tests/test_gcs_fault_tolerance.py
@@ -1,8 +1,6 @@
 import sys
 import time
 
-from numpy.testing import assert_equal
-
 import ray
 
 
@@ -20,21 +18,25 @@ def test_gcs_server_restart():
 
     actor1 = Increase.remote()
     result = ray.get(actor1.method.remote(1))
-    assert_equal(result, 3)
+    assert result == 3
 
     ray.worker._global_node.kill_gcs_server()
     ray.worker._global_node.start_gcs_server()
+
+    # TODO(ffbin): After gcs server restarts, if an RPC request is sent to
+    # gcs server immediately, gcs server cannot receive the request,
+    # but the request will return success. We will fix this in the next pr.
     time.sleep(1)
 
     result = ray.get(actor1.method.remote(7))
-    assert_equal(result, 9)
+    assert result == 9
 
     actor2 = Increase.remote()
     result = ray.get(actor2.method.remote(2))
-    assert_equal(result, 4)
+    assert result == 4
 
     result = ray.get(increase.remote(1))
-    assert_equal(result, 2)
+    assert result == 2
     ray.shutdown()
 
 
