3c612fa8619b5ae6cac712e8a7173fbd27c2fea5
sven1977 <svenmika1977@gmail.com>
2020-06-05 11:01:52 +0200

    Fixes and LINT.

--- rllib/tests/test_rollout.py
+++ rllib/tests/test_rollout.py
@@ -28,7 +28,7 @@ def rollout_test(algo, env="CartPole-v0", test_episode_rollout=False):
                   "--checkpoint-freq=1 ".format(rllib_dir, tmp_dir, algo) +
                   "--config='{" + "\"num_workers\": 1, \"num_gpus\": 0{}{}".
                   format(fw_, extra_config) +
-                  ", \"timesteps_per_iter\": 5,\"min_iter_time_s\": 0.1, "
+                  ", \"timesteps_per_iteration\": 5,\"min_iter_time_s\": 0.1, "
                   "\"model\": {\"fcnet_hiddens\": [10]}"
                   "}' --stop='{\"training_iteration\": 1}'" +
                   " --env={}".format(env))
