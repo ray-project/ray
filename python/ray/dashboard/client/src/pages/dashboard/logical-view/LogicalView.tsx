9d465a2561656f030e6685ccd40fc7b1337d586f
Max Fitton <max@semprehealth.com>
2020-06-08 14:52:26 -0700

    compiles checkpoint. I've added the filtration logic but yet to add component that sets the filter.

--- python/ray/dashboard/client/src/pages/dashboard/logical-view/LogicalView.tsx
+++ python/ray/dashboard/client/src/pages/dashboard/logical-view/LogicalView.tsx
@@ -1,48 +1,44 @@
 import {
-  createStyles,
-  Theme,
-  Typography,
-  WithStyles,
-  withStyles,
+  Typography
 } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { StoreState } from "../../../store";
-import Actors from "./Actors";
-
-const styles = (theme: Theme) =>
-  createStyles({
-    warning: {
-      fontSize: "0.8125rem",
-      marginBottom: theme.spacing(2),
-    },
-    warningIcon: {
-      fontSize: "1.25em",
-      verticalAlign: "text-bottom",
-    },
-  });
+import Actors from "./Actors"
+import { RayletInfoResponse, RayletActorInfo } from "../../../api";
 
 const mapStateToProps = (state: StoreState) => ({
   rayletInfo: state.dashboard.rayletInfo,
 });
 
-class LogicalView extends React.Component<
-  WithStyles<typeof styles> & ReturnType<typeof mapStateToProps>
-> {
-  render() {
-    const { rayletInfo } = this.props;
-    return (
-      <div>
-        {rayletInfo === null ? (
-          <Typography color="textSecondary">Loading...</Typography>
-        ) : Object.entries(rayletInfo.actors).length === 0 ? (
-          <Typography color="textSecondary">No actors found.</Typography>
-        ) : (
-          <Actors actors={rayletInfo.actors} />
-        )}
-      </div>
-    );
+const filterObj = (obj: Object, filterFn: any) => Object.fromEntries(Object.entries(obj).filter(filterFn))
+
+type LogicalViewProps = {
+  rayletInfo: RayletInfoResponse | null;
+} & ReturnType<typeof mapStateToProps>;
+
+const LogicalView = (props: LogicalViewProps) => {
+  const { rayletInfo } = props;
+  const [nameFilter, setNameFilter] = useState<string | null>(null);
+  const filterActors = (actors: RayletActorInfo[], nameFilter: string) =>
+      actors.filter(actor => actor.actorTitle.search(nameFilter) !== -1);
+  if (rayletInfo === null) {
+    return <Typography color="textSecondary">Loading...</Typography>
   }
+  let filteredActors = rayletInfo.actors;
+  if (nameFilter !== null) {
+    filteredActors = filterObj(filteredActors, ([_, actor]: [any, RayletActorInfo]) => actor.actorTitle.search(nameFilter) !== -1)
+  }
+  return (
+    <div>
+      {
+      Object.entries(rayletInfo.actors).length === 0 ? (
+        <Typography color="textSecondary">No actors found.</Typography>
+      ) : (
+        <Actors actors={filteredActors} />
+      )}
+    </div>
+  );
 }
 
-export default connect(mapStateToProps)(withStyles(styles)(LogicalView));
+export default connect(mapStateToProps)(LogicalView);
