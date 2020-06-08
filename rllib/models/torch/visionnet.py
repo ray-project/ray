c6e785be99db65e08412324ae9162b3526cb5edd
sven1977 <svenmika1977@gmail.com>
2020-06-08 11:20:18 +0200

    WIP.

--- rllib/models/torch/visionnet.py
+++ rllib/models/torch/visionnet.py
@@ -56,7 +56,7 @@ class VisionNetwork(TorchModelV2):
                     num_outputs,
                     kernel,
                     stride,
-                    None,
+                    None,  # padding=valid
                     activation_fn=activation))
         # Finish network normally (w/o overriding last layer size with
         # `num_outputs`), then add another linear one of size `num_outputs`.
@@ -67,7 +67,7 @@ class VisionNetwork(TorchModelV2):
                     out_channels,
                     kernel,
                     stride,
-                    None,
+                    None,  # padding=valid
                     activation_fn=activation))
 
             if num_outputs:
@@ -90,69 +90,80 @@ class VisionNetwork(TorchModelV2):
         self._convs = nn.Sequential(*layers)
 
         # Build the value layers
+        self._value_branch_separate = self._value_branch = None
         if vf_share_layers:
-            #last_layer = tf.keras.layers.Lambda(
-            #    lambda x: tf.squeeze(x, axis=[1, 2]))(last_layer)
             self._value_branch = SlimFC(
-                out_channels, 1, initializer=normc_initializer(0.01))
+                self.num_outputs, 1, initializer=normc_initializer(0.01))
         else:
-            # build a parallel set of hidden layers for the value net
-            last_layer = inputs
-            for i, (out_size, kernel, stride) in enumerate(filters[:-1], 1):
-                last_layer = SlimConv2d(
-                    #in_size=,
-                    out_size,
+            vf_layers = []
+            (w, h, in_channels) = obs_space.shape
+            in_size = [w, h]
+            for out_channels, kernel, stride in filters[:-1]:
+                padding, out_size = same_padding(in_size, kernel,
+                                                 [stride, stride])
+                vf_layers.append(
+                    SlimConv2d(
+                        in_channels,
+                        out_channels,
+                        kernel,
+                        stride,
+                        padding,
+                        activation_fn=activation))
+                in_channels = out_channels
+                in_size = out_size
+
+            out_channels, kernel, stride = filters[-1]
+            vf_layers.append(
+                SlimConv2d(
+                    in_channels,
+                    out_channels,
                     kernel,
-                    strides=(stride, stride),
-                    activation=activation,
-                    padding="same",
-                    data_format="channels_last",
-                    name="conv_value_{}".format(i))(last_layer)
-            out_size, kernel, stride = filters[-1]
-            last_layer = SlimConv2d(
-                #in_size=,
-                out_size,
-                kernel,
-                strides=(stride, stride),
-                activation=activation,
-                padding="valid",
-                data_format="channels_last",
-                name="conv_value_{}".format(i + 1))(last_layer)
-            last_layer = SlimConv2d(
-                #in_size=,
-                1, [1, 1],
-                activation=None,
-                padding="same",
-                data_format="channels_last",
-                name="conv_value_out")(last_layer)
-            value_out = tf.keras.layers.Lambda(
-                lambda x: tf.squeeze(x, axis=[1, 2]))(last_layer)
+                    stride,
+                    None,
+                    activation_fn=activation))
+
+            vf_layers.append(
+                SlimConv2d(
+                    in_channels=out_channels,
+                    out_channels=1,
+                    kernel=1,
+                    stride=1,
+                    padding=None))
+            self._value_branch_separate = nn.Sequential(*vf_layers)
 
         # Holds the current "base" output (before logits layer).
         self._features = None
 
     @override(TorchModelV2)
     def forward(self, input_dict, state, seq_lens):
-        self._features = self._convs(input_dict["obs"].float().permute(
-            0, 3, 1, 2))
+        self._features = input_dict["obs"].float().permute(0, 3, 1, 2)
+        conv_out = self._convs(self._features)
+        if not self._value_branch_separate:
+            self._features = conv_out
 
         if not self.last_layer_is_flattened:
-            logits = self._logits(self._features)
+            logits = self._logits(conv_out)
             logits = logits.squeeze(3)
             logits = logits.squeeze(2)
             return logits, state
 
-        return self._features, state
+        return conv_out, state
 
     @override(TorchModelV2)
     def value_function(self):
         assert self._features is not None, "must call forward() first"
-        if not self.last_layer_is_flattened:
-            features = self._features.squeeze(3)
-            features = features.squeeze(2)
+        if self._value_branch_separate:
+            value = self._value_branch_separate(self._features)
+            value = value.squeeze(3)
+            value = value.squeeze(2)
+            return value.squeeze(1)
         else:
-            features = self._features
-        return self._value_branch(features).squeeze(1)
+            if not self.last_layer_is_flattened:
+                features = self._features.squeeze(3)
+                features = features.squeeze(2)
+            else:
+                features = self._features
+            return self._value_branch(features).squeeze(1)
 
     def _hidden_layers(self, obs):
         res = self._convs(obs.permute(0, 3, 1, 2))  # switch to channel-major
