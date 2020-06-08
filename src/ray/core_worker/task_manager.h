35eb73981b602c453cab89d4d36b1bc26df66d77
Stephanie Wang <swang@cs.berkeley.edu>
2020-06-08 12:23:16 -0700

    Update src/ray/core_worker/task_manager.h
    Co-authored-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/core_worker/task_manager.h
+++ src/ray/core_worker/task_manager.h
@@ -245,8 +245,7 @@ class TaskManager : public TaskFinisherInterface, public TaskResubmissionInterfa
 
   /// Called to check whether a raylet is still alive. This is used when
   /// processing a worker's reply, to check whether the node that the worker
-  /// was on is still alive. This is used to check whether a plasma object
-  /// returned by the task is still present.
+  /// was on is still alive. If the node is down, the plasma objects returned by the task are marked as failed.
   const std::function<bool(const ClientID &node_id)> check_node_alive_;
   /// Called when processing a worker's reply if the node that the worker was
   /// on died. This should be called to attempt to recover a plasma object
