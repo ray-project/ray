8079e73f7f95f8448012674471b660c2a727afe3
simon-mo <xmo@berkeley.edu>
2020-06-08 10:03:21 -0700

    Lint

--- python/ray/serve/backend_worker.py
+++ python/ray/serve/backend_worker.py
@@ -297,7 +297,7 @@ class RayServeWorker:
                 get_call_method = attrgetter("call_method")
                 sorted_batch = sorted(batch, key=get_call_method)
                 for _, group in groupby(sorted_batch, key=get_call_method):
-                    group = list(sorted(group))
+                    group = sorted(group)
                     evaluated = asyncio.ensure_future(self.invoke_batch(group))
                     all_evaluated_futures.append(evaluated)
                     result_futures = [q.async_future for q in group]
