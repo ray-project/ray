ec25f2a260f59505e47096c23ee27d195336764f
chaokunyang <shawn.ck.yang@gmail.com>
2020-06-09 15:12:30 +0800

    remove api package

---
+++ java/test/src/main/java/io/ray/test/ActorConcurrentCallTest.java
@@ -0,0 +1,47 @@
+package io.ray.test;
+
+import com.google.common.collect.ImmutableList;
+import io.ray.api.ActorHandle;
+import io.ray.api.ObjectRef;
+import io.ray.api.Ray;
+import io.ray.api.options.ActorCreationOptions;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test
+public class ActorConcurrentCallTest extends BaseTest {
+
+  public static class ConcurrentActor {
+    private final CountDownLatch countDownLatch = new CountDownLatch(3);
+
+    public String countDown() {
+      countDownLatch.countDown();
+      try {
+        countDownLatch.await();
+        return "ok";
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  public void testConcurrentCall() {
+    TestUtils.skipTestUnderSingleProcess();
+
+    ActorCreationOptions op = new ActorCreationOptions.Builder()
+        .setMaxConcurrency(3)
+        .createActorCreationOptions();
+    ActorHandle<ConcurrentActor> actor = Ray.createActor(ConcurrentActor::new, op);
+    ObjectRef<String> obj1 = actor.call(ConcurrentActor::countDown);
+    ObjectRef<String> obj2 = actor.call(ConcurrentActor::countDown);
+    ObjectRef<String> obj3 = actor.call(ConcurrentActor::countDown);
+
+    List<Integer> expectedResult = ImmutableList.of(1, 2, 3);
+    Assert.assertEquals(obj1.get(), "ok");
+    Assert.assertEquals(obj2.get(), "ok");
+    Assert.assertEquals(obj3.get(), "ok");
+  }
+
+}
