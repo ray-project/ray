30b2fd24e1ddafe21cddb4fe9ab6dd542661fbf5
kisuke95 <2522134184@qq.com>
2020-06-09 17:12:49 +0800

    remove unused java import

--- java/runtime/src/main/java/io/ray/runtime/gcs/GcsClient.java
+++ java/runtime/src/main/java/io/ray/runtime/gcs/GcsClient.java
@@ -9,7 +9,6 @@ import io.ray.api.id.JobId;
 import io.ray.api.id.TaskId;
 import io.ray.api.id.UniqueId;
 import io.ray.api.runtimecontext.NodeInfo;
-import io.ray.runtime.config.RayConfig;
 import io.ray.runtime.generated.Gcs;
 import io.ray.runtime.generated.Gcs.ActorCheckpointIdData;
 import io.ray.runtime.generated.Gcs.GcsNodeInfo;
