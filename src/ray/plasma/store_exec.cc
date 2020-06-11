981146e51a1c3a88b35532564042ebb80c65b1f5
Siyuan <suquark@gmail.com>
2020-06-11 11:51:56 -0700

    lint

--- src/ray/plasma/store_exec.cc
+++ src/ray/plasma/store_exec.cc
@@ -22,30 +22,30 @@ int main(int argc, char *argv[]) {
   int c;
   while ((c = getopt(argc, argv, "s:m:d:e:h:z")) != -1) {
     switch (c) {
-      case 'd':
-        plasma_directory = std::string(optarg);
-        break;
-      case 'e':
-        external_store_endpoint = std::string(optarg);
-        break;
-      case 'h':
-        hugepages_enabled = true;
-        break;
-      case 's':
-        socket_name = std::string(optarg);
-        break;
-      case 'm': {
-        char extra;
-        int scanned = sscanf(optarg, "%" SCNd64 "%c", &system_memory, &extra);
-        ARROW_CHECK(scanned == 1);
-        break;
-      }
-      case 'z': {
-        keep_idle = true;
-        break;
-      }
-      default:
-        exit(-1);
+    case 'd':
+      plasma_directory = std::string(optarg);
+      break;
+    case 'e':
+      external_store_endpoint = std::string(optarg);
+      break;
+    case 'h':
+      hugepages_enabled = true;
+      break;
+    case 's':
+      socket_name = std::string(optarg);
+      break;
+    case 'm': {
+      char extra;
+      int scanned = sscanf(optarg, "%" SCNd64 "%c", &system_memory, &extra);
+      ARROW_CHECK(scanned == 1);
+      break;
+    }
+    case 'z': {
+      keep_idle = true;
+      break;
+    }
+    default:
+      exit(-1);
     }
   }
 
@@ -54,8 +54,9 @@ int main(int argc, char *argv[]) {
                                      plasma_directory, external_store_endpoint);
     runner.Start();
   } else {
-    printf("The Plasma Store is started with the '-z' flag, "
-           "and it will run idle as a placeholder.");
+    printf(
+        "The Plasma Store is started with the '-z' flag, "
+        "and it will run idle as a placeholder.");
     while (true) {
       std::this_thread::sleep_for(std::chrono::hours(1000));
     }
