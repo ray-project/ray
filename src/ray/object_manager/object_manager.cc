e2b90dba01bb06b2809ae98e161673180ebb2669
Siyuan <suquark@gmail.com>
2020-06-11 18:02:22 -0700

    without forcefully kill the plasma store thread

--- src/ray/object_manager/object_manager.cc
+++ src/ray/object_manager/object_manager.cc
@@ -15,9 +15,6 @@
 #include "ray/object_manager/object_manager.h"
 
 #include <chrono>
-#ifdef _WIN32
-#include <Windows.h>
-#endif
 
 #include "ray/common/common_protocol.h"
 #include "ray/stats/stats.h"
@@ -44,14 +41,7 @@ ObjectStoreRunner::ObjectStoreRunner(const ObjectManagerConfig &config) {
 
 ObjectStoreRunner::~ObjectStoreRunner() {
   if (plasma_store_ != nullptr) {
-    // Try to stop the eventloop gracefully with a timeout.
     plasma_store_->Stop();
-    std::this_thread::sleep_for(std::chrono::microseconds(500));
-#ifdef _WIN32
-    TerminateThread(store_thread_.native_handle(), 0);
-#else
-    pthread_kill(store_thread_.native_handle(), SIGTERM);
-#endif
     store_thread_.join();
     plasma_store_.reset();
   }
