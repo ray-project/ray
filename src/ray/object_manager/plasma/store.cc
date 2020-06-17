f82a97dd99d10b2e2c5f7e3258739b2dd3b14569
Eric Liang <ekhliang@gmail.com>
2020-06-16 17:03:55 -0700

    debug

--- src/ray/object_manager/plasma/store.cc
+++ src/ray/object_manager/plasma/store.cc
@@ -730,7 +730,7 @@ void PlasmaStore::DisconnectClient(int client_fd) {
   loop_->RemoveFileEvent(client_fd);
   // Close the socket.
   close(client_fd);
-  ARROW_LOG(INFO) << "Disconnecting client on fd " << client_fd;
+  ARROW_LOG(DEBUG) << "Disconnecting client on fd " << client_fd;
   // Release all the objects that the client was using.
   auto client = it->second.get();
   eviction_policy_.ClientDisconnected(client);
