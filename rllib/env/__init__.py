64d128fa34971bf00f82ea476df831b5af04c264
Michael Luo <michael.luo@berkeley.edu>
2020-06-20 00:02:39 +0000

    Changes

--- rllib/env/__init__.py
+++ rllib/env/__init__.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from ray.rllib.env.base_env import BaseEnv
 from ray.rllib.env.dm_env_wrapper import DMEnv
+from ray.rllib.env.dm_control_wrapper import DMCEnv
 from ray.rllib.env.multi_agent_env import MultiAgentEnv
 from ray.rllib.env.external_env import ExternalEnv
 from ray.rllib.env.external_multi_agent_env import ExternalMultiAgentEnv
@@ -21,6 +22,7 @@ __all__ = [
     "VectorEnv",
     "EnvContext",
     "DMEnv",
+    "DMCEnv",
     "PolicyClient",
     "PolicyServerInput",
 ]
--- rllib/env/dm_control_wrapper.py
+++ rllib/env/dm_control_wrapper.py
@@ -28,8 +28,6 @@ from gym import core, spaces
 from dm_control import suite
 from dm_env import specs
 import numpy as np
-import gym
-from gym.envs.registration import register
 
 
 def _spec_to_box(spec):
@@ -62,7 +60,7 @@ def _flatten_obs(obs):
     return np.concatenate(obs_pieces, axis=0)
 
 
-class DMCWrapper(core.Env):
+class DMCEnv(core.Env):
     def __init__(self,
                  domain_name,
                  task_name,
@@ -188,51 +186,3 @@ class DMCWrapper(core.Env):
         camera_id = camera_id or self._camera_id
         return self._env.physics.render(
             height=height, width=width, camera_id=camera_id)
-
-
-def DMCMake(domain_name,
-            task_name,
-            seed=1,
-            visualize_reward=True,
-            from_pixels=False,
-            height=84,
-            width=84,
-            camera_id=0,
-            frame_skip=1,
-            episode_length=1000,
-            environment_kwargs=None,
-            time_limit=None,
-            channels_first=True):
-    env_id = "dmc_%s_%s-v1" % (domain_name, task_name)
-
-    if from_pixels and visualize_reward:
-        raise ValueError("Cannot visualize reward from pixels")
-
-    # shorten episode length
-    max_episode_steps = (episode_length + frame_skip - 1) // frame_skip
-
-    if env_id not in gym.envs.registry.env_specs:
-        task_kwargs = {}
-        if seed is not None:
-            task_kwargs["random"] = seed
-        if time_limit is not None:
-            task_kwargs["time_limit"] = time_limit
-        register(
-            id=env_id,
-            entry_point="ray.rllib.env.dm_control_wrapper:DMCWrapper",
-            kwargs=dict(
-                domain_name=domain_name,
-                task_name=task_name,
-                task_kwargs=task_kwargs,
-                environment_kwargs=environment_kwargs,
-                visualize_reward=visualize_reward,
-                from_pixels=from_pixels,
-                height=height,
-                width=width,
-                camera_id=camera_id,
-                frame_skip=frame_skip,
-                channels_first=channels_first,
-            ),
-            max_episode_steps=max_episode_steps,
-        )
-    return gym.make(env_id)
