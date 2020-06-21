25a302f3057349d8c5221ffc1415bebc5c36e6dc
sven1977 <svenmika1977@gmail.com>
2020-06-21 20:05:13 +0200

    Change ViewRequirement into dataclass.

--- rllib/models/views.py
+++ rllib/models/views.py
@@ -1,47 +1,41 @@
-from collections import namedtuple
+from dataclasses import dataclass
 import numpy as np
-"""
-Single column view requirement specified by a Model
-(`ModelV2.get_view_requirements()`) to tell the caller, which columns and
-timesteps it needs to perform a forward pass.
-"""
-ViewRequirement = namedtuple(
-    "ViewRequirement",
-    [
-        # The data column name from the SampleBatch (str key).
-        "col",
 
-        # List of relative (or absolute timesteps) to be present in the
-        # input_dict.
-        "timesteps",
 
-        # The name under which the specified view of `col` will be accessible
-        # in the input_dict. Default: None (use `col`).
-        # TODO: (sven)
-        # "name",
+@dataclass
+class ViewRequirement:
+    """Single view requirement (for one column in a Trajectory data, e.g. OBS).
 
-        # Switch on absolute timestep mode. Default: False.
-        # TODO: (sven)
-        # "absolute_timesteps",
-        # The fill mode in case t<0 or t>H: One of "zeros", "tile".
-        "fill_mode",
-        # The repeat-mode (one of "all" or "only_first"). E.g. for training,
-        # we only want the first internal state timestep (the NN will
-        # calculate all others again anyways).
-        "repeat_mode",
+    Returned by a Model (via `ModelV2.get_view_requirements()`) to tell the
+    caller (e.g. the Policy), which timesteps it needs (for a given column) to
+    perform a forward pass.
+    The Model always returns a list of ViewRequirement objects (one for each
+    column the Model is "interested" in). The default ViewRequirement for a
+    Model is: `[ViewRequirement("obs", 0)]` <- "provide the current obs as
+    input".
+    """
+    # The data column name from the SampleBatch (str key).
+    col: str
+    # List of relative (or absolute timesteps) to be present in the
+    # input_dict.
+    timesteps: int = 0
+    # The name under which the specified view of `col` will be accessible
+    # in the input_dict. Default: None (use `col`).
+    # TODO: (sven)
+    # "name",
+    # Switch on absolute timestep mode. Default: False.
+    # TODO: (sven)
+    # "absolute_timesteps",
+    # The fill mode in case t<0 or t>H: One of "zeros", "tile".
+    fill_mode: str = "zeros"
+    # The repeat-mode (one of "all" or "only_first"). E.g. for training,
+    # we only want the first internal state timestep (the NN will
+    # calculate all others again anyways).
+    repeat_mode: str = "all"
 
-        # Provide all data as time major (default: False).
-        # TODO: (sven)
-        # "time_major",
-    ],
-    defaults=[
-        0,  # ts
-        # None,  # name
-        # False,  # absolute?
-        "zeros",  # fill mode
-        "all",  # repeat mode
-        # False,  # time-major
-    ])
+    # Provide all data as time major (default: False).
+    # TODO: (sven)
+    # "time_major",
 
 
 def get_view(model, trajectories, is_training=False):
