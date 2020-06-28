a0572c1b3612f3cac2c8105fef55cc9627583acd
chaokunyang <shawn.ck.yang@gmail.com>
2020-06-28 11:17:41 +0800

    add driver test

--- java/test/src/main/java/io/ray/test/NamedActorTest.java
+++ java/test/src/main/java/io/ray/test/NamedActorTest.java
@@ -2,7 +2,10 @@ package io.ray.test;
 
 import io.ray.api.ActorHandle;
 import io.ray.api.Ray;
+import io.ray.runtime.config.RayConfig;
+import java.io.IOException;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -34,18 +37,52 @@ public class NamedActorTest extends BaseTest {
   }
 
   @Test
-  public void testGlobalActor() {
+  public void testGlobalActor() throws IOException, InterruptedException {
     String name = "global-actor-counter";
     // Create an actor.
     ActorHandle<Counter> actor = Ray.actor(Counter::new).setGlobalName(name).remote();
     Assert.assertEquals(actor.task(Counter::increment).remote().get(), Integer.valueOf(1));
+
     Assert.assertFalse(Ray.getActor(name).isPresent());
-    // Get the named actor.
+
+    // Get the global actor.
     Optional<ActorHandle<Counter>> namedActor = Ray.getGlobalActor(name);
     Assert.assertTrue(namedActor.isPresent());
     // Verify that this handle is correct.
     Assert.assertEquals(namedActor.get().task(Counter::increment).remote().get(),
         Integer.valueOf(2));
+
+    // Get the global actor from another driver.
+    RayConfig rayConfig = TestUtils.getRuntime().getRayConfig();
+    ProcessBuilder builder = new ProcessBuilder(
+        "java",
+        "-cp",
+        System.getProperty("java.class.path"),
+        name,
+        "-Dray.redis.address=" + rayConfig.getRedisAddress(),
+        "-Dray.object-store.socket-name=" + rayConfig.objectStoreSocketName,
+        "-Dray.raylet.socket-name=" + rayConfig.rayletSocketName,
+        "-Dray.raylet.node-manager-port=" + rayConfig.getNodeManagerPort(),
+        NamedActorTest.class.getName());
+    builder.redirectError(ProcessBuilder.Redirect.INHERIT);
+    Process driver = builder.start();
+    Assert.assertTrue(driver.waitFor(60, TimeUnit.SECONDS));
+    Assert.assertEquals(driver.exitValue(), 0,
+        "The driver exited with code " + driver.exitValue());
+
+    Assert.assertEquals(namedActor.get().task(Counter::increment).remote().get(),
+        Integer.valueOf(4));
+  }
+
+  public static void main(String[] args) {
+    Ray.init();
+    String actorName = args[0];
+    // Get the global actor.
+    Optional<ActorHandle<Counter>> namedActor = Ray.getGlobalActor(actorName);
+    Assert.assertTrue(namedActor.isPresent());
+    // Verify that this handle is correct.
+    Assert.assertEquals(namedActor.get().task(Counter::increment).remote().get(),
+        Integer.valueOf(3));
   }
 
   @Test(expectedExceptions = IllegalArgumentException.class)
