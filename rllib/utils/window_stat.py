a8df05e85761b1e69df0d50d44f18208e32d78d2
09wakharet <twakhare@gmail.com>
2020-06-30 13:02:10 -0400

    WindowStat error catching, which processes NaNs properly instead of erroring. This ought to resolve issue #7910.
    https://github.com/ray-project/ray/issues/7910

--- rllib/utils/window_stat.py
+++ rllib/utils/window_stat.py
@@ -2,11 +2,11 @@ import numpy as np
 
 
 class WindowStat:
-    def __init__(self, name, n, count):
+    def __init__(self, name, n):
         self.name = name
         self.items = [None] * n
         self.idx = 0
-        self.count = count
+        self.count = 0
 
     def push(self, obj):
         self.items[self.idx] = obj
@@ -15,26 +15,10 @@ class WindowStat:
         self.idx %= len(self.items)
 
     def stats(self):
-        #Should I just try except all of these instead
-        if np.isnan(self.count) or not self.count:
-            _count = np.nan
-        else:
-            _count = int(self.count)
-
-        if np.isnan(self.count) or not self.count or \
-                np.isnan(self.items[:self.count]).any():
-            _mean = np.nan
-            _var = np.nan
-            _quantiles = []
-        else:
-            _mean = float(np.mean(self.items[:self.count]))
-            _var = float(np.std(self.items[:self.count]))
-            _quantiles = np.percentile(self.items[:self.count],
-                                       [0, 10, 50, 90, 100]).tolist()
-
         return {
-            self.name + "_count": _count,
-            self.name + "_mean": _mean,
-            self.name + "_std": _var,
-            self.name + "_quantiles": _quantiles,
+            self.name + "_count": int(self.count),
+            self.name + "_mean": float(np.nanmean(self.items[:self.count])),
+            self.name + "_std": float(np.nanstd(self.items[:self.count])),
+            self.name + "_quantiles": np.nanpercentile(
+                self.items[:self.count], [0, 10, 50, 90, 100]).tolist(),
         }
