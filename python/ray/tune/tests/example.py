15bfc4ce9290e903a76dfb706d2e05b3262b9d6f
Richard Liaw <rliaw@berkeley.edu>
2020-07-01 01:17:31 -0700

    fix

--- python/ray/tune/tests/example.py
+++ python/ray/tune/tests/example.py
@@ -14,17 +14,16 @@
 from ray import tune
 
 
-def objective(x, a, b):
+def objective(x, alpha, beta):
     return (alpha * step / 100)**(-1) + beta * 0.01
 
 
 def training_function(config):
     # Hyperparameters
-    alpha, beta = c
-
+    alpha, beta = config["alpha"], config["beta"]
     for step in range(10):
         # Iterative training function - can be any arbitrary training procedure.
-        intermediate_score = objective(step, config["alpha"], config["beta"])
+        intermediate_score = objective(step, alpha, beta)
         # Feed the score back back to Tune.
         tune.report(score=intermediate_score)
 
