c4ccbfdfa96ffddeeb170b7cb79b80c7e1a6c9b4
Sven Mika <sven@anyscale.io>
2020-07-02 13:03:10 +0200

    [RLlib] tf-eager support for ES and ARS (tf2.x preparation). (#9207)

--- rllib/agents/es/es_tf_policy.py
+++ rllib/agents/es/es_tf_policy.py
@@ -76,29 +76,40 @@ class ESTFPolicy:
         self.observation_filter = get_filter(config["observation_filter"],
                                              self.preprocessor.shape)
         self.single_threaded = config.get("single_threaded", False)
-        self.sess = make_session(single_threaded=self.single_threaded)
-        self.inputs = tf1.placeholder(tf.float32,
-                                      [None] + list(self.preprocessor.shape))
+        if config["framework"] == "tf":
+            self.sess = make_session(single_threaded=self.single_threaded)
+            self.inputs = tf1.placeholder(
+                tf.float32, [None] + list(self.preprocessor.shape))
+        else:
+            if not tf1.executing_eagerly():
+                tf1.enable_eager_execution()
+            self.sess = self.inputs = None
 
         # Policy network.
-        dist_class, dist_dim = ModelCatalog.get_action_dist(
+        self.dist_class, dist_dim = ModelCatalog.get_action_dist(
             self.action_space, config["model"], dist_type="deterministic")
+
         self.model = ModelCatalog.get_model_v2(
             obs_space=self.preprocessor.observation_space,
             action_space=action_space,
             num_outputs=dist_dim,
             model_config=config["model"])
-        dist_inputs, _ = self.model({SampleBatch.CUR_OBS: self.inputs})
-        dist = dist_class(dist_inputs, self.model)
-        self.sampler = dist.sample()
 
-        self.variables = ray.experimental.tf_utils.TensorFlowVariables(
-            dist_inputs, self.sess)
+        self.sampler = None
+        if self.sess:
+            dist_inputs, _ = self.model({SampleBatch.CUR_OBS: self.inputs})
+            dist = self.dist_class(dist_inputs, self.model)
+            self.sampler = dist.sample()
+            self.variables = ray.experimental.tf_utils.TensorFlowVariables(
+                dist_inputs, self.sess)
+            self.sess.run(tf1.global_variables_initializer())
+        else:
+            self.variables = ray.experimental.tf_utils.TensorFlowVariables(
+                [], None, self.model.variables())
 
         self.num_params = sum(
             np.prod(variable.shape.as_list())
             for _, variable in self.variables.variables.items())
-        self.sess.run(tf1.global_variables_initializer())
 
     def compute_actions(self,
                         observation,
@@ -111,8 +122,17 @@ class ESTFPolicy:
         observation = self.preprocessor.transform(observation)
         observation = self.observation_filter(observation[None], update=update)
         # `actions` is a list of (component) batches.
-        actions = self.sess.run(
-            self.sampler, feed_dict={self.inputs: observation})
+        # Eager mode.
+        if not self.sess:
+            dist_inputs, _ = self.model({SampleBatch.CUR_OBS: observation})
+            dist = self.dist_class(dist_inputs, self.model)
+            actions = dist.sample()
+            actions = tree.map_structure(lambda a: a.numpy(), actions)
+        # Graph mode.
+        else:
+            actions = self.sess.run(
+                self.sampler, feed_dict={self.inputs: observation})
+
         if add_noise:
             actions = tree.map_structure(self._add_noise, actions,
                                          self.action_space_struct)
