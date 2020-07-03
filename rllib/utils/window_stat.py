5320bc90f3ea913d1b716773a54cd8e44b24c61a
09wakharet <twakhare@gmail.com>
2020-07-02 23:55:54 -0400

    WindowStat error catching, which processes NaNs properly instead of erroring. This ought to resolve issue #7910.
    https://github.com/ray-project/ray/issues/7910

--- rllib/utils/window_stat.py
+++ rllib/utils/window_stat.py
@@ -15,10 +15,14 @@ class WindowStat:
         self.idx %= len(self.items)
 
     def stats(self):
+        if not self.count:
+            _quantiles = []
+        else:
+            _quantiles = np.nanpercentile(self.items[:self.count],
+                                      [0, 10, 50, 90, 100]).tolist()
         return {
             self.name + "_count": int(self.count),
             self.name + "_mean": float(np.nanmean(self.items[:self.count])),
             self.name + "_std": float(np.nanstd(self.items[:self.count])),
-            self.name + "_quantiles": np.nanpercentile(
-                self.items[:self.count], [0, 10, 50, 90, 100]).tolist(),
+            self.name + "_quantiles": _quantiles,
         }
