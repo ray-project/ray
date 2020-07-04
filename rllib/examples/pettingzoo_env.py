899e18ce00d6df5246ace3d2460505e7f2c7c36d
Ben Black <weepingwillowben@gmail.com>
2020-07-03 21:40:36 -0400

    fixed pettingzoo env flake8, added test

--- rllib/examples/pettingzoo_env.py
+++ rllib/examples/pettingzoo_env.py
@@ -7,13 +7,12 @@ except ImportError:
 from ray.tune.registry import register_env
 from ray.rllib.env import PettingZooEnv
 from pettingzoo.gamma import prison_v0
-from pettingzoo.mpe import simple_spread_v0
 from supersuit.aec_wrappers import normalize_obs, dtype, color_reduction
 
 from numpy import float32
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     '''For this script, you need:
     1. Algorithm name and according module, e.g.: 'PPo' + agents.ppo as agent
     2. Name of the aec game you want to train on, e.g.: 'prison'.
@@ -23,16 +22,24 @@ if __name__ == "__main__":
     Does require SuperSuit
     '''
     alg_name = 'PPO'
+
     # function that outputs the environment you wish to register.
-    env_creator = lambda config: normalize_obs(color_reduction(dtype(prison_v0.env(num_floors=config.get("num_floors", 4)), dtype=float32), mode="R"))
-    num_cpus= 1
+    def env_creator(config):
+        env = prison_v0.env(num_floors=config.get('num_floors', 4))
+        env = dtype(env, dtype=float32)
+        env = color_reduction(env, dtype=float32)
+        env = normalize_obs(env, mode='R')
+        return env
+
+    num_cpus = 1
     num_rollouts = 2
 
-    # 1. Get's default training configuration and specifies the POMgame to load.
+    # 1. Gets default training configuration and specifies the POMgame to load.
     config = deepcopy(get_agent_class(alg_name)._default_config)
 
-    # 2. Set environment config. This will be passed to the env_creator function via the register env lambda below
-    config['env_config'] = {"num_floors": 5}
+    # 2. Set environment config. This will be passed to
+    # the env_creator function via the register env lambda below
+    config['env_config'] = {'num_floors': 5}
 
     # 3. Register env
     register_env('prison', lambda config: PettingZooEnv(env_creator(config)))
@@ -41,27 +48,30 @@ if __name__ == "__main__":
     test_env = PettingZooEnv(env_creator({}))
     obs_space = test_env.observation_space
     act_space = test_env.action_space
-    #test_env.aec_env.render()
-    test_env.close()
 
     # 5. Configuration for multiagent setup with policy sharing:
-    config["multiagent"] = {
-            "policies": {
+    config['multiagent'] = {
+            'policies': {
                 # the first tuple value is None -> uses default policy
-                "av": (None, obs_space, act_space, {}),
+                'av': (None, obs_space, act_space, {}),
             },
-            "policy_mapping_fn": lambda agent_id: 'av'
+            'policy_mapping_fn': lambda agent_id: 'av'
             }
 
     config['log_level'] = 'DEBUG'
     config['num_workers'] = 1
-    config['sample_batch_size'] = 30     # Fragment length, collected at once from each worker and for each agent!
-    config['train_batch_size'] = 200     # Training batch size -> Fragments are concatenated up to this point.
-    config['horizon'] = 200              # After n steps, force reset simulation
-    config['no_done_at_end'] = False     # Default: False
-    # Info: If False, each agents trajectory is expected to have maximum one done=True in the last step of the trajectory.
-    # If no_done_at_end = True, environment is not resetted when dones[__all__]= True.
-
+    # Fragment length, collected at once from each worker and for each agent!
+    config['sample_batch_size'] = 30
+    # Training batch size -> Fragments are concatenated up to this point.
+    config['train_batch_size'] = 200
+    # After n steps, force reset simulation
+    config['horizon'] = 200
+    # Default: False
+    config['no_done_at_end'] = False
+    # Info: If False, each agents trajectory is expected to have
+    # maximum one done=True in the last step of the trajectory.
+    # If no_done_at_end = True, environment is not resetted
+    # when dones[__all__]= True.
 
     # 6. Initialize ray and trainer object
     ray.init(num_cpus=num_cpus+1)
