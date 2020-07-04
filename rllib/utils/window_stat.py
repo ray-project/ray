008a8e5fc0a6c043f20bd6a739b7c7de7505f9a4
09wakharet <twakhare@gmail.com>
2020-07-03 20:07:49 -0400

    Linting

--- rllib/utils/window_stat.py
+++ rllib/utils/window_stat.py
@@ -19,7 +19,7 @@ class WindowStat:
             _quantiles = []
         else:
             _quantiles = np.nanpercentile(self.items[:self.count],
-                                      [0, 10, 50, 90, 100]).tolist()
+                                          [0, 10, 50, 90, 100]).tolist()
         return {
             self.name + "_count": int(self.count),
             self.name + "_mean": float(np.nanmean(self.items[:self.count])),
