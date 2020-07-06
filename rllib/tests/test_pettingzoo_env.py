9a743cd0de1aede102d137786d4aa063d4848ba0
Ben Black <weepingwillowben@gmail.com>
2020-07-06 16:02:11 -0400

    fixed pettingzoo test

--- rllib/tests/test_pettingzoo_env.py
+++ rllib/tests/test_pettingzoo_env.py
@@ -1,37 +1,52 @@
-"""Integration test: (1) pendulum works, (2) single-agent multi-agent works."""
 import unittest
+from copy import deepcopy
 
 import ray
 from ray.tune import run_experiments
 from ray.tune.registry import register_env
 from ray.rllib.env import PettingZooEnv
+from ray.rllib.agents.registry import get_agent_class
 from ray.rllib.utils.test_utils import framework_iterator
-from pettingzoo.mpe import simple_world_comm_v0
+
+from pettingzoo.mpe import simple_spread_v0
 
 
-class TestMultiAgentPendulum(unittest.TestCase):
+class TestPettingZooEnv(unittest.TestCase):
     def setUp(self) -> None:
         ray.init()
 
     def tearDown(self) -> None:
         ray.shutdown()
 
-    def test_multi_agent_pendulum(self):
-        register_env("prison", lambda _: PettingZooEnv(simple_world_comm_v0.env()))
+    def test_pettingzoo_env(self):
+        register_env("prison", lambda _: PettingZooEnv(simple_spread_v0.env()))
 
-        ray.tune(
-            "A2C",
-            stop={"episodes_total": 60},
-            checkpoint_freq=100,
-            config={
-                "train_batch_size": 128,
-                "num_workers": 0,
-                "num_envs_per_worker": 10,
-                "lambda": 0.1,
-                "gamma": 0.95,
-                "lr": 0.0003,
-            }
-        )
+        agent_class = get_agent_class("PPO")
+
+        config = deepcopy(agent_class._default_config)
+
+        test_env = PettingZooEnv(simple_spread_v0.env())
+        obs_space = test_env.observation_space
+        act_space = test_env.action_space
+        test_env.close()
+
+        config["multiagent"] = {
+                "policies": {
+                    # the first tuple value is None -> uses default policy
+                    "av": (None, obs_space, act_space, {}),
+                },
+                "policy_mapping_fn": lambda agent_id: 'av'
+                }
+
+        config['log_level'] = 'DEBUG'
+        config['num_workers'] = 0
+        config['sample_batch_size'] = 30     # Fragment length, collected at once from each worker and for each agent!
+        config['train_batch_size'] = 200     # Training batch size -> Fragments are concatenated up to this point.
+        config['horizon'] = 200              # After n steps, force reset simulation
+        config['no_done_at_end'] = False
+        ray.init(num_cpus=1)
+        agent = agent_class(env='prison', config=config)
+        agent.train()
 
 
 if __name__ == "__main__":
