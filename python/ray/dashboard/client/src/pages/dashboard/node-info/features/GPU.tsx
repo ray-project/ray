ca54da66b619b8d09035a22437a5d7a34d81a5b0
Max Fitton <maxfitton@gmail.com>
2020-07-07 16:58:22 -0700

    Worker <> GPU Mapping Information in Dashboard (#9163)

--- python/ray/dashboard/client/src/pages/dashboard/node-info/features/GPU.tsx
+++ python/ray/dashboard/client/src/pages/dashboard/node-info/features/GPU.tsx
@@ -1,5 +1,7 @@
-import { Typography } from "@material-ui/core";
+import { Box, Tooltip, Typography } from "@material-ui/core";
 import React from "react";
+import { GPUStats, ResourceSlot } from "../../../../api";
+import { RightPaddedTypography } from "../../../../common/CustomTypography";
 import UsageBar from "../../../../common/UsageBar";
 import { getWeightedAverage, sum } from "../../../../common/util";
 import {
@@ -9,9 +11,14 @@ import {
   WorkerFeatureComponent,
 } from "./types";
 
+const GPU_COL_WIDTH = 120;
+
 const clusterUtilization = (nodes: Array<Node>): number => {
   const utils = nodes
-    .map((node) => ({ weight: node.gpus.length, value: nodeUtilization(node) }))
+    .map((node) => ({
+      weight: node.gpus.length,
+      value: nodeAverageUtilization(node),
+    }))
     .filter((util) => !isNaN(util.value));
   if (utils.length === 0) {
     return NaN;
@@ -19,7 +26,7 @@ const clusterUtilization = (nodes: Array<Node>): number => {
   return getWeightedAverage(utils);
 };
 
-const nodeUtilization = (node: Node): number => {
+const nodeAverageUtilization = (node: Node): number => {
   if (!node.gpus || node.gpus.length === 0) {
     return NaN;
   }
@@ -31,7 +38,7 @@ const nodeUtilization = (node: Node): number => {
 export const ClusterGPU: ClusterFeatureComponent = ({ nodes }) => {
   const clusterAverageUtilization = clusterUtilization(nodes);
   return (
-    <div style={{ minWidth: 60 }}>
+    <div style={{ minWidth: GPU_COL_WIDTH }}>
       {isNaN(clusterAverageUtilization) ? (
         <Typography color="textSecondary" component="span" variant="inherit">
           N/A
@@ -47,17 +54,60 @@ export const ClusterGPU: ClusterFeatureComponent = ({ nodes }) => {
 };
 
 export const NodeGPU: NodeFeatureComponent = ({ node }) => {
-  const nodeUtil = nodeUtilization(node);
+  const hasGPU = node.gpus !== undefined && node.gpus.length !== 0;
   return (
-    <div style={{ minWidth: 60 }}>
-      {isNaN(nodeUtil) ? (
+    <div style={{ minWidth: GPU_COL_WIDTH }}>
+      {hasGPU ? (
+        node.gpus.map((gpu, i) => <NodeGPUEntry gpu={gpu} slot={i} />)
+      ) : (
         <Typography color="textSecondary" component="span" variant="inherit">
           N/A
         </Typography>
-      ) : (
-        <UsageBar percent={nodeUtil} text={`${nodeUtil.toFixed(1)}%`} />
       )}
     </div>
+  );
+};
+
+type NodeGPUEntryProps = {
+  slot: number;
+  gpu: GPUStats;
+};
+
+const NodeGPUEntry: React.FC<NodeGPUEntryProps> = ({ gpu, slot }) => {
+  return (
+    <Box display="flex" style={{ minWidth: GPU_COL_WIDTH }}>
+      <Tooltip title={gpu.name}>
+        <RightPaddedTypography variant="body1">[{slot}]:</RightPaddedTypography>
+      </Tooltip>
+      <UsageBar
+        percent={gpu.utilization_gpu}
+        text={`${gpu.utilization_gpu.toFixed(1)}%`}
+      />
+    </Box>
+  );
+};
+
+type WorkerGPUEntryProps = {
+  resourceSlot: ResourceSlot;
+};
+
+const WorkerGPUEntry: React.FC<WorkerGPUEntryProps> = ({ resourceSlot }) => {
+  const { allocation, slot } = resourceSlot;
+  // This is a bit of  a dirty hack . For some reason, the slot GPU slot
+  // 0 as assigned always shows up as undefined in the API response.
+  // There are other times, such as a partial allocation, where we truly don't
+  // know the slot, however this will just plug the hole of 0s coming through
+  // as undefined. I have not been able to figure out the root cause.
+  const slotMsg =
+    allocation >= 1 && slot === undefined
+      ? "0"
+      : slot === undefined
+      ? "?"
+      : slot.toString();
+  return (
+    <Typography variant="body1">
+      [{slotMsg}]: {allocation}
+    </Typography>
   );
 };
 
@@ -72,13 +122,19 @@ export const WorkerGPU: WorkerFeatureComponent = ({ rayletWorker }) => {
       </Typography>
     );
   } else {
-    const aggregateAllocation = sum(
-      workerUsedGPUResources.resourceSlots.map(
-        (resourceSlot) => resourceSlot.allocation,
-      ),
-    );
-    const plural = aggregateAllocation === 1 ? "" : "s";
-    message = <b>{`${aggregateAllocation} GPU${plural} in use`}</b>;
+    message = workerUsedGPUResources.resourceSlots
+      .sort((slot1, slot2) => {
+        if (slot1.slot === undefined && slot2.slot === undefined) {
+          return 0;
+        } else if (slot1.slot === undefined) {
+          return 1;
+        } else if (slot2.slot === undefined) {
+          return -1;
+        } else {
+          return slot1.slot - slot2.slot;
+        }
+      })
+      .map((resourceSlot) => <WorkerGPUEntry resourceSlot={resourceSlot} />);
   }
   return <div style={{ minWidth: 60 }}>{message}</div>;
 };
