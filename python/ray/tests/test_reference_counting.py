ea5cb59f81528272a0ea4bbee2fdf2e88331894d
灵洵 <fengbin.ffb@antfin.com>
2020-07-08 14:06:53 +0800

    fix lint error

--- python/ray/tests/test_reference_counting.py
+++ python/ray/tests/test_reference_counting.py
@@ -528,8 +528,8 @@ def test_basic_nested_ids(one_worker_100MiB):
 
 
 def _all_actors_dead():
-    dead_state = ray.gcs_utils.ActorTableData.DEAD
-    return all(actor["State"] == dead_state for actor in list(ray.actors().values()))
+    return all(actor["State"] == ray.gcs_utils.ActorTableData.DEAD
+               for actor in list(ray.actors().values()))
 
 
 def test_kill_actor_immediately_after_creation(ray_start_regular):
