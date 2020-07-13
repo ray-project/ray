f41b9b523539427effc4e34661262ee9dadd7cef
Ian Rodney <ian.rodney@gmail.com>
2020-07-13 16:36:09 -0700

    Include base-deps image  in rayproject Docker Hub

--- ci/travis/build-autoscaler-images.sh
+++ ci/travis/build-autoscaler-images.sh
@@ -11,11 +11,12 @@ DOCKER_USERNAME="raytravisbot"
 if [[ "$TRAVIS" == "true" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-    docker build -q -t rayproject/base-deps docker/base-deps
-
     wheel=$(cd $ROOT_DIR/.whl; ls | grep cp36m-manylinux)
     commit_sha=$(echo $TRAVIS_COMMIT | head -c 6)
     cp -r $ROOT_DIR/.whl $ROOT_DIR/docker/autoscaler/.whl
+
+    docker build -q -t rayproject/base-deps:$commit_sha docker/base-deps
+    docker push rayproject/base-deps:$commit_sha
 
     docker build \
         --build-arg WHEEL_PATH=".whl/$wheel" \
@@ -29,10 +30,14 @@ if [[ "$TRAVIS" == "true" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
        # Replace / in branch name to - so it is legal tag name
        normalized_branch_name=$(echo $TRAVIS_BRANCH | sed -e "s/\//-/")
        docker tag rayproject/autoscaler:$commit_sha rayproject/autoscaler:$normalized_branch_name
+       docker tag rayproject/base-deps:$commit_sha rayproject/base-deps:$normalized_branch_name
        docker push rayproject/autoscaler:$normalized_branch_name
+       docker push rayproject/base-deps:$normalized_branch_name
     else
        docker tag rayproject/autoscaler:$commit_sha rayproject/autoscaler:latest
+       docker tag rayproject/base-deps:$commit_sha rayproject/base-deps:latest
        docker push rayproject/autoscaler:latest
+       docker push rayproject/base-deps:latest
     fi
 fi
 
