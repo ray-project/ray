630251755771f445f8b34f5c40dc0e53083b25e7
Edward Oakes <ed.nmi.oakes@gmail.com>
2020-07-13 13:20:08 -0500

    Revert "Revert "Support accessing underlying attributes in RayTaskErrors (#9210)" (#9263)"
    This reverts commit bfb8a28a3cca940621841a52854ab3dd8ef37a76.

--- python/ray/tests/test_failure.py
+++ python/ray/tests/test_failure.py
@@ -63,7 +63,12 @@ def test_failed_task(ray_start_regular):
             assert False
 
     class CustomException(ValueError):
-        pass
+        def __init__(self, msg):
+            super().__init__(msg)
+            self.field = 1
+
+        def f(self):
+            return 2
 
     @ray.remote
     def f():
@@ -73,9 +78,12 @@ def test_failed_task(ray_start_regular):
         ray.get(f.remote())
     except Exception as e:
         assert "This function failed." in str(e)
+        assert isinstance(e, ValueError)
         assert isinstance(e, CustomException)
         assert isinstance(e, ray.exceptions.RayTaskError)
         assert "RayTaskError(CustomException)" in repr(e)
+        assert e.field == 1
+        assert e.f() == 2
     else:
         # ray.get should throw an exception.
         assert False
