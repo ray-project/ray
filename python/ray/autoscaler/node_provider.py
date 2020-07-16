50f09d9223ab299bd8f8d3c94cb389dc24b9f1bc
Ameer Haj Ali <ameerhajali@Ameers-MacBook-Pro.local>
2020-07-16 06:34:12 +0300

    filling in -> adding for lint

--- python/ray/autoscaler/node_provider.py
+++ python/ray/autoscaler/node_provider.py
@@ -215,7 +215,7 @@ class NodeProvider:
 
     @staticmethod
     def bootstrap_config(cluster_config):
-        """Bootstraps the cluster config by filling in env defaults if needed."""
+        """Bootstraps the cluster config by adding env defaults if needed."""
         return cluster_config
 
     def get_command_runner(self,
