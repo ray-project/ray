de30c285fe92f5660bb577d0624415e963c27616
灵洵 <fengbin.ffb@antfin.com>
2020-07-22 22:04:48 +0800

    add part code

--- java/api/src/main/java/io/ray/api/placementgroup/Bundle.java
+++ java/api/src/main/java/io/ray/api/placementgroup/Bundle.java
@@ -3,27 +3,11 @@ package io.ray.api.placementgroup;
 import io.ray.api.id.PlacementGroupId;
 
 public class Bundle {
-  public PlacementGroupId placementGroupId;
-  public int bundleIndex;
+  public final PlacementGroupId placementGroupId;
+  public final int bundleIndex;
 
   public Bundle(PlacementGroupId placementGroupId, int bundleIndex) {
     this.placementGroupId = placementGroupId;
-    this.bundleIndex = bundleIndex;
-  }
-
-  public PlacementGroupId getPlacementGroupId() {
-    return placementGroupId;
-  }
-
-  public void setPlacementGroupId(PlacementGroupId placementGroupId) {
-    this.placementGroupId = placementGroupId;
-  }
-
-  public int getBundleIndex() {
-    return bundleIndex;
-  }
-
-  public void setBundleIndex(int bundleIndex) {
     this.bundleIndex = bundleIndex;
   }
 }
