9ee9a5fce98d91793d8dc1e977ca942160c79929
Ameer Haj Ali <ameerhajali@Ameers-MacBook-Pro.local>
2020-07-23 21:21:59 +0300

    lint

--- python/ray/autoscaler/local/config.py
+++ python/ray/autoscaler/local/config.py
@@ -11,14 +11,11 @@ def bootstrap_local(config):
         # Verify no autoscaling.
         valid_num_workers = (
             config["max_workers"] == config["min_workers"]
-            and config["min_workers"] == config["initial_workers"]
-        )
+            and config["min_workers"] == config["initial_workers"])
         if not valid_num_workers:
             logger.error(
-                "Autoscaling is not supported for on prem clusters."
-                + " Please make sure max_workers=min_workers=initial_workers."
-            )
+                "Autoscaling is not supported for on prem clusters." +
+                " Please make sure max_workers=min_workers=initial_workers.")
             logger.warning(
-                "Setting min_workers and initial_workers to max_workers ..."
-            )
+                "Setting min_workers and initial_workers to max_workers ...")
     return config
