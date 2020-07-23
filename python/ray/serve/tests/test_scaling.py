da36407af883d5565935cac87f203041b242fd61
simon-mo <simon.mo@hey.com>
2020-07-23 16:02:33 -0700

    Use a different port

--- python/ray/serve/tests/test_scaling.py
+++ python/ray/serve/tests/test_scaling.py
@@ -17,7 +17,7 @@ def test_multiple_routers():
     ray.init(head_node.address)
     node_ids = ray.state.node_ids()
     assert len(node_ids) == 2
-    serve.init()
+    serve.init(http_port=8005)
 
     # two actors should be started
     head_http = ray.get_actor(SERVE_PROXY_NAME +
@@ -25,11 +25,11 @@ def test_multiple_routers():
     ray.get_actor(SERVE_PROXY_NAME + "-{}-{}".format(node_ids[0], 1))
 
     # we can send requests to them
-    assert requests.get("http://127.0.0.1:8000/-/routes").status_code == 200
+    assert requests.get("http://127.0.0.1:8005/-/routes").status_code == 200
 
     # kill the head_http server, the HTTP server should still functions
     ray.kill(head_http, no_restart=True)
-    assert requests.get("http://127.0.0.1:8000/-/routes").status_code == 200
+    assert requests.get("http://127.0.0.1:8005/-/routes").status_code == 200
 
 
 if __name__ == "__main__":
