db7e2b5696b6f4b984964eed75b01f1f42836b39
Basasuya <961256668@qq.com>
2020-07-27 00:17:47 +0800

    [EVENT] basic function and defination

---
+++ src/ray/util/event.cc
@@ -0,0 +1,106 @@
+// Copyright 2017 The Ray Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "ray/util/event.h"
+
+namespace ray {
+
+EventManager &EventManager::Instance() {
+  static EventManager INSTANCE;
+  return INSTANCE;
+}
+
+bool EventManager::IsEmpty() { return reporter_map_.empty(); }
+
+void EventManager::Publish(rpc::Event &event) {
+  for (const auto &element : reporter_map_) {
+    (element.second)->Report(event);
+  }
+}
+
+void EventManager::AddReporter(std::shared_ptr<LogBasedEventReporter> reporter) {
+  reporter_map_.emplace(reporter->GetReporterKey(), reporter);
+}
+
+void EventManager::ClearReporters() { reporter_map_.clear(); }
+
+thread_local std::unique_ptr<RayEventContext> RayEventContext::instance_ = nullptr;
+
+RayEventContext &RayEventContext::Instance() {
+  if (instance_ == nullptr) {
+    instance_ = std::unique_ptr<RayEventContext>(new RayEventContext());
+  }
+  return *instance_;
+}
+
+void RayEventContext::SetEventContext(
+    rpc::Event_SourceType source_type,
+    std::unordered_map<std::string, std::string> custom_field) {
+  source_type_ = source_type;
+  custom_field_ = custom_field;
+}
+
+void RayEventContext::ResetEventContext() {
+  source_type_ = rpc::Event_SourceType::Event_SourceType_COMMON;
+  custom_field_.clear();
+}
+
+void RayEventContext::SetCustomField(std::string key, std::string value) {
+  custom_field_[key] = value;
+}
+
+void RayEventContext::SetCustomField(
+    std::unordered_map<std::string, std::string> custom_field) {
+  custom_field_ = custom_field;
+}
+
+void RayEvent::ReportEvent(std::string severity, std::string label, std::string message) {
+  rpc::Event_Severity severity_ele =
+      rpc::Event_Severity::Event_Severity_Event_Severity_INT_MIN_SENTINEL_DO_NOT_USE_;
+  RAY_CHECK(rpc::Event_Severity_Parse(severity, &severity_ele));
+  RayEvent(severity_ele, label) << message;
+}
+
+RayEvent::~RayEvent() { SendMessage(osstream_.str()); }
+
+void RayEvent::SendMessage(const std::string &message) {
+  RAY_CHECK(rpc::Event_SourceType_IsValid(RayEventContext::Instance().GetSourceType()));
+  RAY_CHECK(rpc::Event_Severity_IsValid(severity_));
+
+  if (EventManager::Instance().IsEmpty()) {
+    return;
+  }
+
+  rpc::Event event;
+
+  std::string event_id_buffer = std::string(18, ' ');
+  FillRandom(&event_id_buffer);
+  event.set_event_id(StringToHex(event_id_buffer));
+
+  event.set_source_type(RayEventContext::Instance().GetSourceType());
+  event.set_source_hostname(RayEventContext::Instance().GetSourceHostname());
+  event.set_source_pid(RayEventContext::Instance().GetSourcePid());
+
+  event.set_severity(severity_);
+  event.set_label(label_);
+  event.set_message(message);
+  event.set_timestamp(current_sys_time_us());
+
+  auto mp = RayEventContext::Instance().GetCustomField();
+  event.mutable_custom_field()->insert(mp.begin(), mp.end());
+
+  EventManager::Instance().Publish(event);
+}
+
+}  // namespace ray
---
+++ src/ray/util/event.h
@@ -0,0 +1,152 @@
+// Copyright 2017 The Ray Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#ifndef RAY_EVENT_H_
+#define RAY_EVENT_H_
+
+#include <boost/asio/ip/host_name.hpp>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <vector>
+#include "ray/util/logging.h"
+#include "ray/util/util.h"
+#include "src/ray/protobuf/event.pb.h"
+
+namespace ray {
+
+#define RAY_EVENT(event_type, label) \
+  ::ray::RayEvent(::ray::rpc::Event_Severity::Event_Severity_##event_type, label)
+
+class LogBasedEventReporter {
+ public:
+  virtual void Init() = 0;
+
+  virtual void Report(rpc::Event &event) = 0;
+
+  virtual void Close() = 0;
+
+  virtual std::string GetReporterKey() = 0;
+
+ protected:
+  virtual std::string EventToString(rpc::Event &event) = 0;
+
+ protected:
+  std::string file_name_;
+  std::string log_dir_;
+};
+
+class EventManager final {
+ public:
+  static EventManager &Instance();
+
+  bool IsEmpty();
+
+  void Publish(rpc::Event &event);
+
+  void AddReporter(std::shared_ptr<LogBasedEventReporter> reporter);
+
+  void ClearReporters();
+
+ private:
+  EventManager() {}
+
+  EventManager(const EventManager &manager) = delete;
+
+  const EventManager &operator=(const EventManager &manager) = delete;
+
+ protected:
+  std::unordered_map<std::string, std::shared_ptr<LogBasedEventReporter>> reporter_map_;
+};
+
+class RayEventContext final {
+ public:
+  static RayEventContext &Instance();
+
+  // SetEventContext should only be called once in a process, unless ResetEventContext is
+  // called.
+  void SetEventContext(rpc::Event_SourceType source_type,
+                       std::unordered_map<std::string, std::string> custom_field =
+                           std::unordered_map<std::string, std::string>());
+
+  void SetCustomField(std::string key, std::string value);
+
+  void SetCustomField(std::unordered_map<std::string, std::string> custom_field);
+
+  void ResetEventContext();
+
+  inline const rpc::Event_SourceType &GetSourceType() const { return source_type_; }
+
+  inline const std::string &GetSourceHostname() const { return source_hostname_; }
+
+  inline int32_t GetSourcePid() const { return source_pid_; }
+
+  inline const std::unordered_map<std::string, std::string> &GetCustomField() const {
+    return custom_field_;
+  }
+
+ private:
+  RayEventContext()
+      : source_type_(rpc::Event_SourceType::Event_SourceType_COMMON),
+        source_hostname_(boost::asio::ip::host_name()),
+        source_pid_(getpid()) {}
+
+  RayEventContext(const RayEventContext &event_context) = delete;
+
+  const RayEventContext &operator=(const RayEventContext &event_context) = delete;
+
+ private:
+  rpc::Event_SourceType source_type_;
+  std::string source_hostname_;
+  int32_t source_pid_;
+  std::unordered_map<std::string, std::string> custom_field_;
+
+  static thread_local std::unique_ptr<RayEventContext> instance_;
+};
+
+class RayEvent {
+ public:
+  RayEvent(rpc::Event_Severity severity, std::string label)
+      : severity_(severity), label_(label) {}
+
+  template <typename T>
+  RayEvent &operator<<(const T &t) {
+    osstream_ << t;
+    return *this;
+  }
+
+  static void ReportEvent(std::string severity, std::string label, std::string message);
+
+  ~RayEvent();
+
+ private:
+  RayEvent() {}
+
+  void SendMessage(const std::string &message);
+
+  RayEvent(const RayEvent &event) = delete;
+
+  const RayEvent &operator=(const RayEvent &event) = delete;
+
+ private:
+  rpc::Event_Severity severity_;
+  std::string label_;
+  std::ostringstream osstream_;
+};
+
+}  // namespace ray
+
+#endif
