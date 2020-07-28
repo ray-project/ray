e9ba9b7f2014049664709a202e0bfe782543f7c1
Ian Rodney <ian.rodney@gmail.com>
2020-07-27 23:28:07 -0700

    Run _with_interactive in Docker

--- python/ray/autoscaler/command_runner.py
+++ python/ray/autoscaler/command_runner.py
@@ -390,6 +390,7 @@ class DockerCommandRunner(SSHCommandRunner):
 
         if run_env == "docker":
             cmd = self._docker_expand_user(cmd, any_char=True)
+            cmd = " ".join(_with_interactive(cmd))
             cmd = with_docker_exec(
                 [cmd], container_name=self.docker_name,
                 with_interactive=True)[0]
