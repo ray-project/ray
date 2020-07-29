46ce45f2d15d6f259df66a7e79d569ac07993e11
Kai Yang <kfstorm@outlook.com>
2020-07-29 18:01:48 +0800

    test

--- src/ray/gcs/gcs_client/service_based_gcs_client.cc
+++ src/ray/gcs/gcs_client/service_based_gcs_client.cc
@@ -91,9 +91,7 @@ void ServiceBasedGcsClient::Disconnect() {
   RAY_CHECK(is_connected_);
   is_connected_ = false;
   detect_timer_->cancel();
-  gcs_pub_sub_.reset();
   redis_gcs_client_->Disconnect();
-  redis_gcs_client_.reset();
   RAY_LOG(DEBUG) << "ServiceBasedGcsClient Disconnected.";
 }
 
