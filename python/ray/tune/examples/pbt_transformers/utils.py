a2e83197d4752323b5e1d0d8102b6235f55f3dc4
Amog Kamsetty <amogkamsetty@yahoo.com>
2020-07-29 12:31:05 -0700

    formatting

--- python/ray/tune/examples/pbt_transformers/utils.py
+++ python/ray/tune/examples/pbt_transformers/utils.py
@@ -5,11 +5,11 @@ from typing import Callable, Dict
 import numpy as np
 from transformers import EvalPrediction
 from transformers import glue_compute_metrics, glue_output_modes
-
 """From transformers/examples/text-classification/run_glue.py"""
 
 
-def build_compute_metrics_fn(task_name: str) -> Callable[[EvalPrediction], Dict]:
+def build_compute_metrics_fn(
+        task_name: str) -> Callable[[EvalPrediction], Dict]:
     output_mode = glue_output_modes[task_name]
 
     def compute_metrics_fn(p: EvalPrediction):
@@ -29,7 +29,10 @@ def download_data(model_name, task_name, data_dir="./data"):
     import urllib
     import zipfile
     if task_name == "rte":
-        url = "https://firebasestorage.googleapis.com/v0/b/mtl-sentence-representations.appspot.com/o/data%2FRTE.zip?alt=media&token=5efa7e85-a0bb-4f19-8ea2-9e1840f077fb"
+        url = "https://firebasestorage.googleapis.com/v0/b/" \
+            "mtl-sentence-representations.appspot.com" \
+            "/o/data%2FRTE.zip?alt=media" \
+            "&token=5efa7e85-a0bb-4f19-8ea2-9e1840f077fb"
     else:
         raise ValueError("Unknown task: {}".format(task_name))
     data_file = os.path.join(data_dir, "{}.zip".format(task_name))
