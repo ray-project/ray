27be720bb1165c49e150eb8db9245217eb9cd853
simon-mo <simon.mo@hey.com>
2020-07-29 12:42:06 -0700

    Try build docker in PR as well

--- ci/travis/build-autoscaler-images.sh
+++ ci/travis/build-autoscaler-images.sh
@@ -9,9 +9,20 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 ROOT_DIR=$(cd "$SCRIPT_DIR"/../../; pwd)
 DOCKER_USERNAME="raytravisbot"
 
+docker_push() {
+    if ["$TRAVIS_PULL_REQUEST" == "false"]; then
+        docker push $@
+    else
+        echo "Skipping docker push because it's in PR environment."
+    fi
+}
+
 # We will only build and push when we are building branch build.
-if [[ "$TRAVIS" == "true" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+if [[ "$TRAVIS" == "true" ]]; then
+
+    if ["$TRAVIS_PULL_REQUEST" == "false"]; then
+        echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    fi
 
     wheel="$(basename "$ROOT_DIR"/.whl/*cp36m-manylinux*)"
     commit_sha=$(echo "$TRAVIS_COMMIT" | head -c 6)
@@ -25,9 +36,9 @@ if [[ "$TRAVIS" == "true" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
         -t rayproject/autoscaler:"$commit_sha" \
         "$ROOT_DIR"/docker/autoscaler
 
-    docker tag rayproject/base-deps rayproject/base-deps:"$commit_sha" 
-    docker push rayproject/base-deps:"$commit_sha"
-    docker push rayproject/autoscaler:"$commit_sha"
+    docker tag rayproject/base-deps rayproject/base-deps:"$commit_sha"
+    docker_push rayproject/base-deps:"$commit_sha"
+    docker_push rayproject/autoscaler:"$commit_sha"
 
 
     # We have a branch build, e.g. release/v0.7.0
@@ -36,13 +47,13 @@ if [[ "$TRAVIS" == "true" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
        normalized_branch_name=$(echo "$TRAVIS_BRANCH" | sed -e "s/\//-/")
        docker tag rayproject/autoscaler:"$commit_sha" rayproject/autoscaler:"$normalized_branch_name"
        docker tag rayproject/base-deps:"$commit_sha" rayproject/base-deps:"$normalized_branch_name"
-       docker push rayproject/autoscaler:"$normalized_branch_name"
-       docker push rayproject/base-deps:"$normalized_branch_name"
+       docker_push rayproject/autoscaler:"$normalized_branch_name"
+       docker_push rayproject/base-deps:"$normalized_branch_name"
     else
        docker tag rayproject/autoscaler:"$commit_sha" rayproject/autoscaler:latest
        docker tag rayproject/base-deps:"$commit_sha" rayproject/base-deps:latest
-       docker push rayproject/autoscaler:latest
-       docker push rayproject/base-deps:latest
+       docker_push rayproject/autoscaler:latest
+       docker_push rayproject/base-deps:latest
     fi
 fi
 
