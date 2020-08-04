ebdac125e8ec3d5b4deb899ab2e440549a1fd6bb
Barak Michener <me@barakmich.com>
2020-08-04 22:12:45 +0000

    rpc: Add basic core_worker_client_pool

---
+++ src/ray/rpc/worker/core_worker_client_pool.cc
@@ -0,0 +1,38 @@
+#include "ray/rpc/worker/core_worker_client_pool.h"
+
+namespace ray {
+namespace rpc {
+
+optional<shared_ptr<CoreWorkerClientInterface>> CoreWorkerClientPool::GetByID(ray::WorkerID id) {
+  absl::MutexLock lock(&mu_);
+  auto it = client_map_.find(id);
+  if (it == client_map_.end()) {
+    return {};
+  }
+  return it->second;
+}
+
+shared_ptr<CoreWorkerClientInterface> CoreWorkerClientPool::GetOrConnect(const WorkerAddress& addr) {
+  auto existing = GetByID(addr.worker_id);
+  if (existing.has_value()) {
+    return existing.value();
+  }
+  absl::MutexLock lock(&mu_);
+  auto connection = client_factory_(addr.ToProto());
+  client_map_[addr.worker_id] = connection;
+
+  RAY_LOG(INFO) << "Connected to " << addr.ip_address << ":" << addr.port;
+  return connection;
+}
+
+void CoreWorkerClientPool::Disconnect(ray::WorkerID id) {
+  absl::MutexLock lock(&mu_);
+  auto it = client_map_.find(id);
+  if (it == client_map_.end()) {
+    return;
+  }
+  client_map_.erase(it);
+}
+
+}  // namespace rpc
+}  // namespace ray
---
+++ src/ray/rpc/worker/core_worker_client_pool.h
@@ -0,0 +1,59 @@
+// Copyright 2020 The Ray Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "absl/base/thread_annotations.h"
+#include "absl/synchronization/mutex.h"
+#include "absl/container/flat_hash_map.h"
+
+#include "ray/common/id.h"
+#include "ray/rpc/worker/core_worker_client.h"
+
+using std::shared_ptr;
+using absl::optional;
+
+
+namespace ray {
+namespace rpc {
+
+class CoreWorkerClientPool {
+ public:
+  CoreWorkerClientPool(rpc::ClientCallManager& ccm):
+      client_factory_(defaultClientFactory(ccm)) {};
+
+  CoreWorkerClientPool(ClientFactoryFn client_factory):
+      client_factory_(client_factory) {};
+
+  optional<shared_ptr<CoreWorkerClientInterface>> GetByID(ray::WorkerID id);
+  shared_ptr<CoreWorkerClientInterface> GetOrConnect(const WorkerAddress& addr);
+  void Disconnect(ray::WorkerID id);
+
+ private:
+  ClientFactoryFn defaultClientFactory(rpc::ClientCallManager& ccm) const {
+    return [&](const rpc::Address& addr) {
+      return std::shared_ptr<rpc::CoreWorkerClient>(
+        new rpc::CoreWorkerClient(addr, ccm));
+    };
+  };
+
+  ClientFactoryFn client_factory_;
+
+  absl::Mutex mu_;
+
+  absl::flat_hash_map<ray::WorkerID, shared_ptr<CoreWorkerClientInterface>> client_map_ GUARDED_BY(mu_);
+};
+
+}  // namespace rpc
+}  // namespace ray
