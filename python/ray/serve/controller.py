2ef29fae65f795883a965b882cd4e0ceaa0f1b47
architkulkarni <arkulkar@gmail.com>
2020-08-05 14:37:39 -0700

    Added error message suggesting use of set_traffic

--- python/ray/serve/controller.py
+++ python/ray/serve/controller.py
@@ -658,7 +658,11 @@ class ServeController:
             err_prefix = "Cannot create endpoint."
             if route in self.routes:
                 if self.routes[route] == (endpoint, methods):
-                    return
+                    raise ValueError(
+                        "Route {} is already registered to endpoint {} with "
+                        "methods {}.  To set the backend for this endpoint,"
+                        " please use serve.set_traffic().".format(route, 
+                            endpoint, methods))
                 else:
                     raise ValueError(
                         "{} Route '{}' is already registered.".format(
