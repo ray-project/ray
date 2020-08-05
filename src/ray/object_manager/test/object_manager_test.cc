e88b7340dd0d0527da38bd5e0bba6396cb4744a9
Zhuohan Li <zhuohan123@vip.qq.com>
2020-08-05 16:08:09 -0700

    lint

--- src/ray/object_manager/test/object_manager_test.cc
+++ src/ray/object_manager/test/object_manager_test.cc
@@ -281,7 +281,8 @@ class TestObjectManager : public TestObjectManagerBase {
     UniqueID wait_id = UniqueID::FromRandom();
 
     RAY_CHECK_OK(server1->object_manager_.AddWaitRequest(
-        wait_id, object_ids, std::unordered_map<ObjectID, rpc::Address>(), timeout_ms, required_objects, false,
+        wait_id, object_ids, std::unordered_map<ObjectID, rpc::Address>(), timeout_ms,
+        required_objects, false,
         [this, sub_id, object_1, object_ids, start_time](
             const std::vector<ray::ObjectID> &found,
             const std::vector<ray::ObjectID> &remaining) {
@@ -354,7 +355,8 @@ class TestObjectManager : public TestObjectManagerBase {
 
     boost::posix_time::ptime start_time = boost::posix_time::second_clock::local_time();
     RAY_CHECK_OK(server1->object_manager_.Wait(
-        object_ids, std::unordered_map<ObjectID, rpc::Address>(), timeout_ms, required_objects, false,
+        object_ids, std::unordered_map<ObjectID, rpc::Address>(), timeout_ms,
+        required_objects, false,
         [this, object_ids, num_objects, timeout_ms, required_objects, start_time](
             const std::vector<ray::ObjectID> &found,
             const std::vector<ray::ObjectID> &remaining) {
