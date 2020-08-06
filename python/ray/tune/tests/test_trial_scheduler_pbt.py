b699eefbf461f6f5e47015217c36964ede036e76
Amog Kamsetty <amogkamsetty@yahoo.com>
2020-08-06 10:32:54 -0700

    add unit test

--- python/ray/tune/tests/test_trial_scheduler_pbt.py
+++ python/ray/tune/tests/test_trial_scheduler_pbt.py
@@ -31,6 +31,25 @@ class MockTrainable(tune.Trainable):
         with open(checkpoint_path, "rb") as fp:
             self.a, self.b = pickle.load(fp)
 
+def MockTrainingFunc(config, checkpoint_dir=None):
+    iter = 0
+    a = config["a"]
+    b = config["b"]
+    c = config["c"]
+
+    if checkpoint_dir:
+        checkpoint_path = os.path.join(checkpoint_dir, "model.mock")
+        with open(checkpoint_path, "rb") as fp:
+            a, b = pickle.load(fp)
+
+    while True:
+        iter += 1
+        tune.report(mean_accuracy=(a - iter) * b)
+        with tune.checkpoint_dir(step=iter) as checkpoint_dir:
+            checkpoint_path = os.path.join(checkpoint_dir, "model.mock")
+            with open(checkpoint_path, "rb") as fp:
+                pickle.dump((a, b), fp)
+
 
 class MockParam(object):
     def __init__(self, params):
@@ -42,8 +61,36 @@ class MockParam(object):
         self._index += 1
         return val
 
-
 class PopulationBasedTrainingResumeTest(unittest.TestCase):
+    def testPermutationContinuationFunc(self):
+        scheduler = PopulationBasedTraining(
+            time_attr="training_iteration",
+            metric="mean_accuracy",
+            mode="max",
+            perturbation_interval=1,
+            log_config=True,
+            hyperparam_mutations={"c": lambda: 1})
+        param_a = MockParam([10, 20, 30, 40])
+        param_b = MockParam([1.2, 0.9, 1.1, 0.8])
+        random.seed(100)
+        np.random.seed(1000)
+        tune.run(
+            MockTrainingFunc,
+            config={
+                "a": tune.sample_from(lambda _: param_a()),
+                "b": tune.sample_from(lambda _: param_b()),
+                "c": 1
+            },
+            fail_fast=True,
+            num_samples=20,
+            global_checkpoint_period=1,
+            checkpoint_freq=1,
+            checkpoint_at_end=True,
+            keep_checkpoints_num=1,
+            checkpoint_score_attr="min-training_iteration",
+            scheduler=scheduler,
+            name="testPermutationContinuationFunct",
+            stop={"training_iteration": 5})
     def testPermutationContinuation(self):
         """
         Tests continuation of runs after permutation.
