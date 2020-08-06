0bee1081aedde9e954e42bf6364635d2bdf566d3
Amog Kamsetty <amogkamsetty@yahoo.com>
2020-08-06 10:14:01 -0700

    adding function convnet example

--- python/ray/tune/examples/pbt_convnet_function_example.py
+++ python/ray/tune/examples/pbt_convnet_function_example.py
@@ -13,61 +13,49 @@ from ray.tune.examples.mnist_pytorch import train, test, ConvNet,\
 import ray
 from ray import tune
 from ray.tune.schedulers import PopulationBasedTraining
-from ray.tune.utils import validate_save_restore
 from ray.tune.trial import ExportFormat
 
 # __tutorial_imports_end__
 
+def train_convnet(config, checkpoint_dir=None):
+    step = 0
+    train_loader, test_loader = get_data_loaders()
+    model = ConvNet()
+    optimizer = optim.SGD(
+        model.parameters(),
+        lr=config.get("lr", 0.01),
+        momentum=config.get("momentum", 0.9)
+    )
 
-# __trainable_begin__
-class PytorchTrainable(tune.Trainable):
-    """Train a Pytorch ConvNet with Trainable and PopulationBasedTraining
-       scheduler. The example reuse some of the functions in mnist_pytorch,
-       and is a good demo for how to add the tuning function without
-       changing the original training code.
-    """
+    if checkpoint_dir:
+        path = os.path.join(checkpoint_dir, "checkpoint")
+        checkpoint = torch.load(path)
+        model.load_state_dict(checkpoint["model_state_dict"])
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        step = checkpoint["step"]
 
-    def setup(self, config):
-        self.train_loader, self.test_loader = get_data_loaders()
-        self.model = ConvNet()
-        self.optimizer = optim.SGD(
-            self.model.parameters(),
-            lr=config.get("lr", 0.01),
-            momentum=config.get("momentum", 0.9))
+    for param_group in optimizer.param_groups:
+        if "lr" in config:
+            param_group["lr"] = config["lr"]
+        if "momentum" in config:
+            param_group["momentum"] = config["momentum"]
 
-    def step(self):
-        train(self.model, self.optimizer, self.train_loader)
-        acc = test(self.model, self.test_loader)
-        return {"mean_accuracy": acc}
-
-    def save_checkpoint(self, checkpoint_dir):
-        checkpoint_path = os.path.join(checkpoint_dir, "model.pth")
-        torch.save(self.model.state_dict(), checkpoint_path)
-        return checkpoint_path
-
-    def load_checkpoint(self, checkpoint_path):
-        self.model.load_state_dict(torch.load(checkpoint_path))
-
-    def _export_model(self, export_formats, export_dir):
-        if export_formats == [ExportFormat.MODEL]:
-            path = os.path.join(export_dir, "exported_convnet.pt")
-            torch.save(self.model.state_dict(), path)
-            return {export_formats[0]: path}
-        else:
-            raise ValueError("unexpected formats: " + str(export_formats))
-
-    def reset_config(self, new_config):
-        for param_group in self.optimizer.param_groups:
-            if "lr" in new_config:
-                param_group["lr"] = new_config["lr"]
-            if "momentum" in new_config:
-                param_group["momentum"] = new_config["momentum"]
-
-        self.config = new_config
-        return True
-
-
-# __trainable_end__
+    while True:
+        train(model, optimizer, train_loader)
+        acc = test(model, test_loader)
+        if step % 5 == 0:
+            with tune.checkpoint_dir(step=step) as checkpoint_dir:
+                path = os.path.join(checkpoint_dir, "checkpoint")
+                torch.save({
+                    "step": step,
+                    "model_state_dict": model.state_dict(),
+                    "optimizer_state_dict": optimizer.state_dict(),
+                    "mean_accuracy": acc
+                }, path)
+        step += 1
+        tune.report(
+            mean_accuracy=acc,
+        )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -75,12 +63,8 @@ if __name__ == "__main__":
         "--smoke-test", action="store_true", help="Finish quickly for testing")
     args, _ = parser.parse_known_args()
 
-    ray.init()
+    ray.init(local_mode=True)
     datasets.MNIST("~/data", train=True, download=True)
-
-    # check if PytorchTrainble will save/restore correctly before execution
-    validate_save_restore(PytorchTrainable)
-    validate_save_restore(PytorchTrainable, use_object_store=True)
 
     # __pbt_begin__
     scheduler = PopulationBasedTraining(
@@ -111,13 +95,13 @@ if __name__ == "__main__":
         def stop_all(self):
             return self.should_stop
 
+
     stopper = CustomStopper()
 
     analysis = tune.run(
-        PytorchTrainable,
+        train_convnet,
         name="pbt_test",
         scheduler=scheduler,
-        reuse_actors=True,
         verbose=1,
         stop=stopper,
         export_formats=[ExportFormat.MODEL],
@@ -132,11 +116,12 @@ if __name__ == "__main__":
     # __tune_end__
 
     best_trial = analysis.get_best_trial("mean_accuracy")
-    best_checkpoint = max(
-        analysis.get_trial_checkpoints_paths(best_trial, "mean_accuracy"))
-    restored_trainable = PytorchTrainable()
-    restored_trainable.restore(best_checkpoint[0])
-    best_model = restored_trainable.model
+    all_checkpoints = analysis.get_trial_checkpoints_paths(best_trial, "mean_accuracy")
+    print(all_checkpoints)
+    best_checkpoint_path = max(all_checkpoints, key=lambda x: x[1])
+    best_model = ConvNet()
+    best_checkpoint = torch.load(best_checkpoint_path[0])
+    best_model.load_state_dict(best_checkpoint["model_state_dict"])
     # Note that test only runs on a small random set of the test data, thus the
     # accuracy may be different from metrics shown in tuning process.
     test_acc = test(best_model, get_data_loaders()[1])
