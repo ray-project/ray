ba5f27ef6eb3c9d35ebb0bbc800158420b0189a0
Richard Liaw <rliaw@berkeley.edu>
2020-08-10 00:46:46 -0700

    Revert "fixtest"
    This reverts commit d6f54353e1b891c784417bb8d0304c18cc5bcdd8.

--- python/ray/autoscaler/command_runner.py
+++ python/ray/autoscaler/command_runner.py
@@ -573,13 +573,11 @@ class DockerCommandRunner(SSHCommandRunner):
         no_exist = "not_present"
         cmd = check_docker_running_cmd(self.docker_name) + " ".join(
             ["||", "echo", quote(no_exist)])
-        output = self.ssh_command_runner.run(cmd, with_output=True)
-        if output is not None:
-            output = output.decode("utf-8").strip()
-            if no_exist in output:
-                return False
-            return "true" in output.lower()
-        return False
+        output = self.ssh_command_runner.run(
+            cmd, with_output=True).decode("utf-8").strip()
+        if no_exist in output:
+            return False
+        return "true" in output.lower()
 
     def _docker_expand_user(self, string, any_char=False):
         user_pos = string.find("~")
