e06a198df5a970af023e7008cf5d0770183a987b
fyrestone <fyrestone@outlook.com>
2020-08-12 15:37:35 +0800

    Update java/runtime/src/main/java/io/ray/runtime/exception/CrossLanguageException.java
    Co-authored-by: Hao Chen <chenh1024@gmail.com>

--- java/runtime/src/main/java/io/ray/runtime/exception/CrossLanguageException.java
+++ java/runtime/src/main/java/io/ray/runtime/exception/CrossLanguageException.java
@@ -7,7 +7,7 @@ public class CrossLanguageException extends RayException {
   private Language language;
 
   public CrossLanguageException(io.ray.runtime.generated.Common.RayException exception) {
-    super(String.format("An exception from %s:\n%s", exception.getLanguage().name(),
+    super(String.format("An exception raised from %s:\n%s", exception.getLanguage().name(),
         exception.getFormattedExceptionString()));
     this.language = exception.getLanguage();
   }
