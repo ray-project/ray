5a2fe5d1673940ff8f99e484e49911085d7722c3
灵洵 <fengbin.ffb@antfin.com>
2020-08-14 15:35:09 +0800

    fix lint error

--- src/ray/core_worker/lib/java/io_ray_runtime_task_NativeTaskSubmitter.cc
+++ src/ray/core_worker/lib/java/io_ray_runtime_task_NativeTaskSubmitter.cc
@@ -162,9 +162,12 @@ inline ray::ActorCreationOptions ToActorCreationOptions(JNIEnv *env,
 
 inline ray::PlacementStrategy ConvertStrategy(jint java_strategy) {
   switch (java_strategy) {
-    case 0:return ray::rpc::PACK;
-    case 1:return ray::rpc::SPREAD;
-    default:return ray::rpc::STRICT_PACK;
+  case 0:
+    return ray::rpc::PACK;
+  case 1:
+    return ray::rpc::SPREAD;
+  default:
+    return ray::rpc::STRICT_PACK;
   }
 }
 
