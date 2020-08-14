ed6cc9975ea7aa8eddca2cae15b119809655c55b
SangBin Cho <rkooo567@gmail.com>
2020-08-13 19:04:24 -0700

    Linting.

--- python/ray/experimental/__init__.py
+++ python/ray/experimental/__init__.py
@@ -2,9 +2,5 @@ from .api import get, wait
 from .dynamic_resources import set_resource
 from .placement_group import (placement_group, placement_group_table)
 __all__ = [
-    "get",
-    "wait",
-    "set_resource",
-    "placement_group",
-    "placement_group_table"
+    "get", "wait", "set_resource", "placement_group", "placement_group_table"
 ]
