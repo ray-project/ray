bc9ea0c816dbb6a299a6575cc54f5c171866b5e7
Alex Wu <itswu.alex@gmail.com>
2020-08-14 17:24:20 -0700

    .

---
+++ job.py
@@ -0,0 +1,13 @@
+import ray
+from time import sleep
+
+ray.init(address="auto")
+
+@ray.remote(num_gpus=1)
+def foo():
+    while True:
+        print("boop")
+        sleep(1)
+        pass
+
+ray.get(foo.remote())
