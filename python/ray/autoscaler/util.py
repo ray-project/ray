21c4ac833cd59486183bf1d646c2730f3774b17e
Eric Liang <ekhliang@gmail.com>
2020-08-19 17:13:09 -0700

    fix launch

--- python/ray/autoscaler/util.py
+++ python/ray/autoscaler/util.py
@@ -75,9 +75,16 @@ def validate_config(config: Dict[str, Any]) -> None:
             raise ValueError(
                 "You must specify `head_node_type` if `available_node_types "
                 "is set.")
+        if config["head_node_type"] not in config["available_node_types"]:
+            raise ValueError(
+                "`head_node_type` must be one of `available_node_types`.")
         if "worker_default_node_type" not in config:
             raise ValueError("You must specify `worker_default_node_type` if "
                              "`available_node_types is set.")
+        if (config["worker_default_node_type"] not in config[
+                "available_node_types"]):
+            raise ValueError("`worker_default_node_type` must be one of "
+                             "`available_node_types`.")
 
 
 def prepare_config(config):
