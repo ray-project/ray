89214a5327d9a189514d9deb721124c82a6455f8
sven1977 <svenmika1977@gmail.com>
2020-08-21 12:16:30 +0200

    WIP

--- ci/rllib_tests/rllib_gpu_tests/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Run all test cases, but with a forced num_gpus=1.
-# TODO: (sven) chose correct dir and run over all RLlib tests and example scripts!
-source activate tensorflow_p36 && export RAY_FORCE_NUM_GPUS=1 && cd ~ && python -m pytest test_attention_net_learning.py
