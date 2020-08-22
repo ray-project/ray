19b23f2519d33c1d0a677606b182832530f96825
Ian Rodney <ian.rodney@gmail.com>
2020-08-21 17:00:47 -0700

    comments

--- python/ray/autoscaler/command_runner.py
+++ python/ray/autoscaler/command_runner.py
@@ -619,6 +619,8 @@ class DockerCommandRunner(SSHCommandRunner):
         self.ssh_command_runner.run_rsync_up(source, target)
         if self._check_container_status():
             if os.path.isdir(source):
+                # Adding a "." means that docker copies the *contents*
+                # Without it, docker copies the source *into* the target
                 target += "/."
             self.ssh_command_runner.run("docker cp {} {}:{}".format(
                 target, self.docker_name,
@@ -632,6 +634,8 @@ class DockerCommandRunner(SSHCommandRunner):
             f"mkdir -p {os.path.dirname(source.rstrip('/'))}")
         if protected_path[-1] == "/":
             protected_path += "."
+            # Adding a "." means that docker copies the *contents*
+            # Without it, docker copies the source *into* the target
         self.ssh_command_runner.run("docker cp {}:{} {}".format(
             self.docker_name, self._docker_expand_user(protected_path),
             source))
