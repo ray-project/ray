70337a596b54a9c96c7cee7f9a5d7ab5bec35b54
Richard Liaw <rliaw@berkeley.edu>
2020-08-22 20:05:35 -0700

    fix-bad-stack

--- python/ray/signature.py
+++ python/ray/signature.py
@@ -113,8 +113,8 @@ def flatten_args(signature_parameters, args, kwargs):
         parameters=signature_parameters)
     try:
         reconstructed_signature.bind(*args, **kwargs)
-    except TypeError as exc:
-        raise TypeError(str(exc))
+    except TypeError as exc:  # capture a friendlier stacktrace
+        raise TypeError(str(exc)) from None
     list_args = []
     for arg in args:
         list_args += [DUMMY_TYPE, arg]
