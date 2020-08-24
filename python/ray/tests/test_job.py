eb12eab446a551bfd57c65dc87d97f968ec4f3d1
Kai Yang <kfstorm@outlook.com>
2020-08-24 18:03:28 +0800

    fix lint

--- python/ray/tests/test_job.py
+++ python/ray/tests/test_job.py
@@ -72,6 +72,7 @@ ray.get(_.value.remote())
 """.format(address)
 
     p = run_string_as_driver_nonblocking(driver)
+
     # Wait for actor to be created
     def actor_is_created():
         actor_table = ray.actors()
@@ -80,6 +81,7 @@ ray.get(_.value.remote())
             if actor_info["State"] == ray.gcs_utils.ActorTableData.ALIVE:
                 return True
         return False
+
     wait_for_condition(actor_is_created)
 
     actor_table = ray.actors()
