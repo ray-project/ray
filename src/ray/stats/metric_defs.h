916a19363f44c9054f300463fbf77b7070a5aab2
Edward Oakes <ed.nmi.oakes@gmail.com>
2020-08-26 10:21:15 -0500

    Clean up actor metrics (#10317)

--- src/ray/stats/metric_defs.h
+++ src/ray/stats/metric_defs.h
@@ -54,8 +54,12 @@ static Gauge LocalTotalResource("local_total_resource",
                                 "The total resources on this node.", "pcs",
                                 {ResourceNameKey});
 
-static Gauge ActorStats("actor_stats", "Stat metrics of the actors in raylet.", "pcs",
-                        {ValueTypeKey});
+static Gauge LiveActors("live_actors", "Number of live actors.", "actors");
+
+static Gauge RestartingActors("restarting_actors", "Number of restarting actors.",
+                              "actors");
+
+static Gauge DeadActors("dead_actors", "Number of dead actors.", "actors");
 
 static Gauge ObjectManagerStats("object_manager_stats",
                                 "Stat the metric values of object in raylet", "pcs",
@@ -68,10 +72,6 @@ static Gauge TaskDependencyManagerStats("task_dependency_manager_stats",
 static Gauge SchedulingQueueStats("scheduling_queue_stats",
                                   "Stats the metric values of scheduling queue.", "pcs",
                                   {ValueTypeKey});
-
-static Gauge ReconstructionPolicyStats(
-    "reconstruction_policy_stats", "Stats the metric values of reconstruction policy.",
-    "pcs", {ValueTypeKey});
 
 static Gauge ConnectionPoolStats("connection_pool_stats",
                                  "Stats the connection pool metrics.", "pcs",
