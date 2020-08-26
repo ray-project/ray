a3845687e19da405c9b54ebae8b7c1f9960de9b0
Lixin Wei <wlx65005@gmail.com>
2020-08-26 15:10:16 +0800

    fixed

--- python/ray/runtime_context.py
+++ python/ray/runtime_context.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import ray.worker
 import json
 import logging
@@ -12,25 +8,27 @@ logger = logging.getLogger(__name__)
 class RuntimeContext(object):
     """A class used for getting runtime context."""
 
-    def __init__(self, worker=None):
+    def __init__(self, worker):
+        assert worker is not None
         self.worker = worker
 
     @property
     def current_job_id(self):
-        """Get current driver ID for this worker or driver.
+        """Get current job ID for this worker or driver.
 
         Returns:
-            If called by a driver, this returns the driver ID. If called in
-                a task, return the driver ID of the associated driver.
+            If called by a driver, this returns the job ID. If called in
+                a task, return the job ID of the associated driver.
         """
-        assert self.worker is not None
         return self.worker.current_job_id
 
     @property
     def get_job_configs(self):
+        # TODO: make a job table
         key = "_JOB_CONFIG_{}".format(self.current_driver_id.hex())
         result = ray.state.state._execute_command_on_primary("GET", key)
-        assert result != None
+        if result is None:
+            raise Exception("Can not get job conig.")
         return json.loads(result, encoding="UTF-8")
 
     @property
@@ -40,37 +38,20 @@ class RuntimeContext(object):
         Returns:
             The current driver id in this worker.
         """
-        assert self.worker is not None
-        assert self.worker.mode == ray.worker.WORKER_MODE
-
+        # only worker mode has actor_id
+        if self.worker.mode != ray.worker.WORKER_MODE:
+            raise Exception("This method should only be called in worker mode")
         return self.worker.actor_id
 
     @property
-    def raylet_socket_name(self):
-
-        assert self.worker is not None
-        return ray.worker._global_node.raylet_socket_name
-
-    @property
-    def object_store_socket_name(self):
-        assert self.worker is not None
-        return ray.worker._global_node.plasma_store_socket_name
-
-    # Note that this method should not be called in a normal task.
-    @property
     def was_current_actor_reconstructed(self):
+        # TODO: this method should not be called in a normal task.
         # None means call this in a normal task.
         assert self.worker is not None
         actor_info = ray.state.actors(self.current_actor_id.hex())
         if actor_info and actor_info["NumRestarts"] != 0:
             return True
         return False
-
-    @property
-    def redis_address(self):
-        assert self.worker is not None
-        return ray.state.state.get_redis_address()
-
 
 _runtime_context = None
 
