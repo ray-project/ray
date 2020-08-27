861046230645cfc520981e978c4b09ad688ce781
Philipp Moritz <pcmoritz@gmail.com>
2020-08-27 02:25:28 -0700

    lint

--- python/ray/autoscaler/aws/config.py
+++ python/ray/autoscaler/aws/config.py
@@ -161,8 +161,7 @@ def log_to_cli(config):
 
         if ("KeyName" in config["head_node"]
                 and "KeyName" in config["worker_nodes"]):
-            print_info(
-                "EC2 Key pair", "KeyName", "keypair_src", "keypair_src")
+            print_info("EC2 Key pair", "KeyName", "keypair_src", "keypair_src")
 
         print_info(
             "VPC Subnets",
