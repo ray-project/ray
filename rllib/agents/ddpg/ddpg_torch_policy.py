0475d7fe54fc0297d7056dbb7b54293860c1440f
Raphael Avalos <raphael@avalos.fr>
2020-08-28 15:22:51 +0200

    LINT

--- rllib/agents/ddpg/ddpg_torch_policy.py
+++ rllib/agents/ddpg/ddpg_torch_policy.py
@@ -6,7 +6,6 @@ from ray.rllib.agents.ddpg.ddpg_tf_policy import build_ddpg_models, \
     get_distribution_inputs_and_class, validate_spaces
 from ray.rllib.agents.dqn.dqn_tf_policy import postprocess_nstep_and_prio, \
     PRIO_WEIGHTS
-from ray.rllib.agents.a3c.a3c_torch_policy import apply_grad_clipping
 from ray.rllib.models.torch.torch_action_dist import TorchDeterministic
 from ray.rllib.policy.sample_batch import SampleBatch
 from ray.rllib.policy.torch_policy_template import build_torch_policy
