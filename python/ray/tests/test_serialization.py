f0c3910d598e7964a28b4245bd6e4024114bdbd2
Siyuan (Ryans) Zhuang <suquark@gmail.com>
2020-08-29 23:11:28 -0700

    [Serialization] Update CloudPickle to 1.6.0 (#9694)
    * update cloudpickle to 1.6.0
    
    * fix CI timeout

--- python/ray/tests/test_serialization.py
+++ python/ray/tests/test_serialization.py
@@ -5,6 +5,7 @@ import logging
 import re
 import string
 import sys
+import weakref
 
 import numpy as np
 import pytest
@@ -495,6 +496,51 @@ def test_register_class(ray_start_2_cpus):
         assert not hasattr(c1, "method2")
         assert not hasattr(c2, "method0")
         assert not hasattr(c2, "method1")
+
+
+def test_deserialized_from_buffer_immutable(ray_start_shared_local_modes):
+    x = np.full((2, 2), 1.)
+    o = ray.put(x)
+    y = ray.get(o)
+    with pytest.raises(
+            ValueError, match="assignment destination is read-only"):
+        y[0, 0] = 9.
+
+
+def test_reducer_override_no_reference_cycle(ray_start_shared_local_modes):
+    # bpo-39492: reducer_override used to induce a spurious reference cycle
+    # inside the Pickler object, that could prevent all serialized objects
+    # from being garbage-collected without explicity invoking gc.collect.
+
+    # test a dynamic function
+    def f():
+        return 4669201609102990671853203821578
+
+    wr = weakref.ref(f)
+
+    bio = io.BytesIO()
+    from ray.cloudpickle import CloudPickler, loads, dumps
+    p = CloudPickler(bio, protocol=5)
+    p.dump(f)
+    new_f = loads(bio.getvalue())
+    assert new_f() == 4669201609102990671853203821578
+
+    del p
+    del f
+
+    assert wr() is None
+
+    # test a dynamic class
+    class ShortlivedObject:
+        def __del__(self):
+            print("Went out of scope!")
+
+    obj = ShortlivedObject()
+    new_obj = weakref.ref(obj)
+
+    dumps(obj)
+    del obj
+    assert new_obj() is None
 
 
 if __name__ == "__main__":
