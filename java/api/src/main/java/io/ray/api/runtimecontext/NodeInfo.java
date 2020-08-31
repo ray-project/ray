6a409d88931d4bd2001add25a19a0324d1283c79
chaokunyang <shawn.ck.yang@gmail.com>
2020-08-31 20:55:36 +0800

    adjust NodeInfo arguments order

--- java/api/src/main/java/io/ray/api/runtimecontext/NodeInfo.java
+++ java/api/src/main/java/io/ray/api/runtimecontext/NodeInfo.java
@@ -14,25 +14,25 @@ public class NodeInfo {
 
   public final String nodeHostname;
 
+  public final int nodeManagerPort;
+
   public final String objectStoreSocketName;
 
   public final String rayletSocketName;
-
-  public final int nodeManagerPort;
 
   public final boolean isAlive;
 
   public final Map<String, Double> resources;
 
-  public NodeInfo(UniqueId nodeId,  String nodeAddress, String nodeHostname,
-                  String objectStoreSocketName, String rayletSocketName, int nodeManagerPort,
+  public NodeInfo(UniqueId nodeId,  String nodeAddress, String nodeHostname, int nodeManagerPort,
+                  String objectStoreSocketName, String rayletSocketName,
                   boolean isAlive, Map<String, Double> resources) {
     this.nodeId = nodeId;
     this.nodeAddress = nodeAddress;
     this.nodeHostname = nodeHostname;
+    this.nodeManagerPort = nodeManagerPort;
     this.objectStoreSocketName = objectStoreSocketName;
     this.rayletSocketName = rayletSocketName;
-    this.nodeManagerPort = nodeManagerPort;
     this.isAlive = isAlive;
     this.resources = resources;
   }
