1303fd7687fb4d250aee5f9a29a03ec24b47fcdd
chaokunyang <shawn.ck.yang@gmail.com>
2020-08-31 20:14:55 +0800

    fix RAY_INSTALL_JAVA

--- ci/travis/ci.sh
+++ ci/travis/ci.sh
@@ -285,7 +285,7 @@ build_wheels() {
         -e "encrypted_1c30b31fe1ee_iv=${encrypted_1c30b31fe1ee_iv-}"
         -e "TRAVIS_COMMIT=${TRAVIS_COMMIT}"
         -e "CI=${CI}"
-        -e "RAY_INSTALL_JAVA=${RAY_INSTALL_JAVA}"
+        -e "RAY_INSTALL_JAVA=${RAY_INSTALL_JAVA:-}"
       )
 
       # This command should be kept in sync with ray/python/README-building-wheels.md,
