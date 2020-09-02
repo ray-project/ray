4ba02865bac87902d2f3193375f19d4b3a29f116
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-09-02 19:26:55 +0200

    Updated integration to keras

--- python/ray/tune/integration/keras.py
+++ python/ray/tune/integration/keras.py
@@ -1,47 +1,67 @@
-from tensorflow import keras
+"""Sub-module introducing keras Callback for reporting the model metrics."""
+from typing import Dict
+from tensorflow.keras.callbacks import Callback
+from ...reporter import Reporter
 
 
-class TuneReporterCallback(keras.callbacks.Callback):
-    """Tune Callback for Keras."""
+class TuneReporterCallback(Callback):
+    """Tune Callback for Keras.
 
-    def __init__(self, reporter=None, freq="batch", logs=None):
-        """Initializer.
+    The pourpose of the class is to make easy to execute schedulers such as
+    ASHA on Keras models for any of the considered metrics.
+
+    Keep in mind that, if you desire to track metrics such as the validation
+    metrics (the ones beginning with 'val_') you will need to use 'epoch'
+    as value for the frequency.
+    """
+
+    FREQUENCIES = {"batch", "epoch"}
+
+    def __init__(self, reporter: Reporter, frequency: str = "epoch"):
+        """Initialize new TuneReporterCallback object.
 
         Args:
-            freq (str): Sets the frequency of reporting intermediate results.
-                One of ["batch", "epoch"].
+            reporter (Reporter): reporter object to use to send the results.
+            frequency (str): frequency of reporting intermediate results.
+                The frequency can either be epoch (default) or batch.
+                Epoch is preferred over batch to avoid adding significant
+                overhead to the training loop.
+
+        Raises:
+            ValueError: when an unsupported frequency is given.
         """
-        self.iteration = 0
-        logs = logs or {}
-        if freq not in ["batch", "epoch"]:
-            raise ValueError("{} not supported as a frequency.".format(freq))
-        self.freq = freq
+        if frequency not in TuneReporterCallback.FREQUENCIES:
+            raise ValueError(
+                "{} not supported as a frequency.".format(frequency)
+            )
+        self._frequency = frequency
+        self._reporter = reporter
         super(TuneReporterCallback, self).__init__()
 
-    def on_batch_end(self, batch, logs=None):
-        from ray import tune
-        logs = logs or {}
-        if not self.freq == "batch":
-            return
-        self.iteration += 1
-        for metric in list(logs):
-            if "loss" in metric and "neg_" not in metric:
-                logs["neg_" + metric] = -logs[metric]
-        if "acc" in logs:
-            tune.report(keras_info=logs, mean_accuracy=logs["acc"])
-        else:
-            tune.report(keras_info=logs, mean_accuracy=logs.get("accuracy"))
+    def on_batch_end(self, batch: int, logs: Dict = None):
+        """Report metrics on the end of each batch if requested.
 
-    def on_epoch_end(self, batch, logs=None):
-        from ray import tune
-        logs = logs or {}
-        if not self.freq == "epoch":
-            return
-        self.iteration += 1
-        for metric in list(logs):
-            if "loss" in metric and "neg_" not in metric:
-                logs["neg_" + metric] = -logs[metric]
-        if "acc" in logs:
-            tune.report(keras_info=logs, mean_accuracy=logs["acc"])
-        else:
-            tune.report(keras_info=logs, mean_accuracy=logs.get("accuracy"))
+        Args:
+            batch: int,
+                Number of the current batch.
+            logs: Dict = None,
+                Available informations for the current batch, tipically
+                including the required metrics in the 'metrics' keyword
+                argument of the model.
+        """
+        if self.freq == "batch" and logs is not None:
+            self._reporter.report(**logs)
+
+    def on_epoch_end(self, epoch: int, logs: Dict = None):
+        """Report metrics on the end of each epoch if requested.
+
+        Args:
+            epoch: int,
+                Number of the current epoch.
+            logs: Dict = None,
+                Available informations for the current epoch, tipically
+                including the required metrics in the 'metrics' keyword
+                argument of the model.
+        """
+        if self.freq == "epoch" and logs is not None:
+            self._reporter.report(**logs)
