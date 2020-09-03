6b2187f15ef0c95b2f8ec2ae24660ce5728b7df8
Sumanth Ratna <sumanthratna@gmail.com>
2020-09-02 20:25:28 -0400

    Remove shim from tune.schedulers.__init__.__all__

--- python/ray/tune/schedulers/__init__.py
+++ python/ray/tune/schedulers/__init__.py
@@ -47,5 +47,5 @@ __all__ = [
     "TrialScheduler", "HyperBandScheduler", "AsyncHyperBandScheduler",
     "ASHAScheduler", "MedianStoppingRule", "FIFOScheduler",
     "PopulationBasedTraining", "PopulationBasedTrainingReplay",
-    "HyperBandForBOHB", "create_scheduler"
+    "HyperBandForBOHB"
 ]
