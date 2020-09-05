68262694a8d7c9af927d7046c66eb7d5771ba5f1
Eric Liang <ekhliang@gmail.com>
2020-09-04 17:18:55 -0700

    Update __init__.py

--- python/ray/experimental/__init__.py
+++ python/ray/experimental/__init__.py
@@ -1,5 +1,7 @@
 from .dynamic_resources import set_resource
 from .object_spilling import force_spill_objects, force_restore_spilled_objects
 __all__ = [
-    "set_resource", "force_spill_objects", "force_restore_spilled_objects",
+    "set_resource",
+    "force_spill_objects",
+    "force_restore_spilled_objects",
 ]
