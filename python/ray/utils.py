ac94f82d2d3884cd8db4ecc79cc00946fbb0dd64
Alex Wu <itswu.alex@gmail.com>
2020-09-05 16:29:00 -0700

    lint

--- python/ray/utils.py
+++ python/ray/utils.py
@@ -377,9 +377,11 @@ def resources_from_resource_arguments(
             object_store_memory, round_up=True)
 
     if runtime_accelerator_type is not None:
-        resources[f"{ray_constants.RESOURCE_CONSTRAINT_PREFIX}{runtime_accelerator_type}"] = 0.001
+        resources[f"{ray_constants.RESOURCE_CONSTRAINT_PREFIX}"
+                  f"{runtime_accelerator_type}"] = 0.001
     elif default_accelerator_type is not None:
-        resources[f"{ray_constants.RESOURCE_CONSTRAINT_PREFIX}{default_accelerator_type}"] = 0.001
+        resources[f"{ray_constants.RESOURCE_CONSTRAINT_PREFIX}"
+                  f"{default_accelerator_type}"] = 0.001
 
     return resources
 
