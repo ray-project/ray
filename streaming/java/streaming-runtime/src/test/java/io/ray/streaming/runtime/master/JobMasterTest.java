e1757c0ca0991c61ae3332e01c5475b7c4e82fe1
Lixin Wei <wlx65005@gmail.com>
2020-09-08 12:12:57 +0800

    test fixed

--- streaming/java/streaming-runtime/src/test/java/io/ray/streaming/runtime/master/JobMasterTest.java
+++ streaming/java/streaming-runtime/src/test/java/io/ray/streaming/runtime/master/JobMasterTest.java
@@ -1,26 +1,35 @@
 package io.ray.streaming.runtime.master;
 
 import io.ray.api.Ray;
+import io.ray.streaming.runtime.BaseUnitTest;
 import java.util.HashMap;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class JobMasterTest {
+public class JobMasterTest extends BaseUnitTest {
+
+  @BeforeMethod
+  public void init() {
+    // ray init
+    Ray.init();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    Ray.shutdown();
+  }
 
   @Test
   public void testCreation() {
-    try {
-      Ray.init();
-      JobMaster jobMaster = new JobMaster(new HashMap<>());
-      Assert.assertNotNull(jobMaster.getRuntimeContext());
-      Assert.assertNotNull(jobMaster.getConf());
-      Assert.assertNull(jobMaster.getGraphManager());
-      Assert.assertNull(jobMaster.getResourceManager());
-      Assert.assertNull(jobMaster.getJobMasterActor());
-      Assert.assertFalse(jobMaster.init(false));
-    } finally {
-      Ray.shutdown();
-    }
+    JobMaster jobMaster = new JobMaster(new HashMap<>());
+    Assert.assertNotNull(jobMaster.getRuntimeContext());
+    Assert.assertNotNull(jobMaster.getConf());
+    Assert.assertNull(jobMaster.getGraphManager());
+    Assert.assertNull(jobMaster.getResourceManager());
+    Assert.assertNull(jobMaster.getJobMasterActor());
+    Assert.assertFalse(jobMaster.init(false));
   }
 
 }
