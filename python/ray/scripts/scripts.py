4ac651a564fa71b1d176f0b42a6de1dbbdab04d6
Alex Wu <itswu.alex@gmail.com>
2020-09-08 21:59:55 -0700

    readded include_java

--- python/ray/scripts/scripts.py
+++ python/ray/scripts/scripts.py
@@ -309,6 +309,11 @@ def dashboard(cluster_config_file, cluster_name, port, remote_port):
     default=None,
     help="manually specify the root temporary dir of the Ray process")
 @click.option(
+    "--include-java",
+    is_flag=True,
+    default=None,
+    help="Enable Java worker support.")
+@click.option(
     "--java-worker-options",
     required=False,
     hidden=True,
@@ -363,9 +368,9 @@ def start(node_ip_address, address, port, redis_password, redis_shard_ports,
           include_dashboard, dashboard_host, dashboard_port, block,
           plasma_directory, autoscaling_config, no_redirect_worker_output,
           no_redirect_output, plasma_store_socket_name, raylet_socket_name,
-          temp_dir, java_worker_options, load_code_from_local, system_config,
-          lru_evict, enable_object_reconstruction, metrics_export_port,
-          log_style, log_color, verbose):
+          temp_dir, include_java, java_worker_options, load_code_from_local,
+          system_config, lru_evict, enable_object_reconstruction,
+          metrics_export_port, log_style, log_color, verbose):
     """Start Ray processes manually on the local machine."""
     cli_logger.log_style = log_style
     cli_logger.color_mode = log_color
@@ -422,7 +427,7 @@ def start(node_ip_address, address, port, redis_password, redis_shard_ports,
         plasma_store_socket_name=plasma_store_socket_name,
         raylet_socket_name=raylet_socket_name,
         temp_dir=temp_dir,
-        include_java=None,
+        include_java=include_java,
         include_dashboard=include_dashboard,
         dashboard_host=dashboard_host,
         dashboard_port=dashboard_port,
@@ -553,6 +558,12 @@ def start(node_ip_address, address, port, redis_password, redis_shard_ports,
             raise ValueError(
                 "If --head is not passed in, the --include-dashboard"
                 "flag is not relevant.")
+        if include_java is not None:
+            cli_logger.abort("`{}` should not be specified without `{}`.",
+                             cf.bold("--include-java"), cf.bold("--head"))
+
+            raise ValueError("--include-java should only be set for the head "
+                             "node.")
         # Wait for the Redis server to be started. And throw an exception if we
         # can't connect to it.
         services.wait_for_redis_to_start(
