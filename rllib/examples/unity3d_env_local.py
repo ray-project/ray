4b278c36fcdeda1a519f43104ed881746f3a863b
Sven Mika <sven@anyscale.io>
2020-09-09 17:33:21 +0200

    [RLlib] Behavioral Cloning (from MARWIL). (#10619)

--- rllib/examples/unity3d_env_local.py
+++ rllib/examples/unity3d_env_local.py
@@ -117,18 +117,18 @@ if __name__ == "__main__":
         config["exploration_config"] = {
             "type": "Curiosity",
             "eta": 0.1,
-            "lr": tune.grid_search([0.0003, 0.001]),
+            "lr": 0.001,
             # No actual feature net: map directly from observations to feature
             # vector (linearly).
             "feature_net_config": {
-                "fcnet_hiddens": tune.grid_search([[], [256]]),
+                "fcnet_hiddens": [],
                 "fcnet_activation": "relu",
             },
             "sub_exploration": {
                 "type": "StochasticSampling",
             },
-            "forward_net_activation": tune.grid_search(["relu", "swish"]),
-            "inverse_net_activation": tune.grid_search(["relu", "swish"]),
+            "forward_net_activation": "relu",
+            "inverse_net_activation": "relu",
         }
 
     stop = {
