29d9db8ad495c63be3a151d2dd402edf2987a966
Alex Wu <alex@anyscale.io>
2020-09-10 12:45:16 -0700

    Update resource_demand_scheduler.py

--- python/ray/autoscaler/resource_demand_scheduler.py
+++ python/ray/autoscaler/resource_demand_scheduler.py
@@ -93,7 +93,6 @@ class ResourceDemandScheduler:
                     f"available_node_types. This node's resources will be "
                     f"ignored. If you are using an unmanaged node, manually "
                     f"set the user_node_type tag to \"{NODE_KIND_UNMANAGED}\""
-                    
                     f"in your cloud provider's management console.")
                 return None
             # Careful not to include the same dict object multiple times.
