ea6fe0f2a1ad82702b78a36ac5bfcefbc620f8bb
Ian Rodney <ian.rodney@gmail.com>
2020-09-10 17:06:56 -0700

    [autoscaler] use default value (#10706)

--- python/ray/autoscaler/autoscaler.py
+++ python/ray/autoscaler/autoscaler.py
@@ -430,6 +430,8 @@ class StandardAutoscaler:
         return fields
 
     def _get_node_specific_docker_config(self, node_id):
+        if "docker" not in self.config:
+            return {}
         docker_config = copy.deepcopy(self.config.get("docker", {}))
         node_specific_docker = self._get_node_type_specific_fields(
             node_id, "docker")
