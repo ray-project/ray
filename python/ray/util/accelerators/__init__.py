dd3eccebc75adc0f8c91b655df22e5e69adeb924
Alex Wu <itswu.alex@gmail.com>
2020-09-10 17:32:16 -0700

    .

--- python/ray/util/accelerators/__init__.py
+++ python/ray/util/accelerators/__init__.py
@@ -1,4 +1,5 @@
-from ray.util.accelerators.accelerators import *
+from ray.util.accelerators.accelerators import NVIDIA_TESLA_V100, \
+    NVIDIA_TESLA_P100, NVIDIA_TESLA_T4, NVIDIA_TESLA_P4, NVIDIA_TESLA_K80
 
 __all__ = [
     "NVIDIA_TESLA_V100", "NVIDIA_TESLA_P100", "NVIDIA_TESLA_T4",
