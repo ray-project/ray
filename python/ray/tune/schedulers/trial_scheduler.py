ae2180be49353849a40e415b6189a96e1f0cf8d1
Kai Fricke <kai@anyscale.com>
2020-09-15 14:14:45 +0100

    Typing for schedulers

--- python/ray/tune/schedulers/trial_scheduler.py
+++ python/ray/tune/schedulers/trial_scheduler.py
@@ -1,6 +1,7 @@
-from typing import Optional
+from typing import Dict, Optional
 
 from ray.tune.trial import Trial
+from ray.tune.trial_runner import TrialRunner
 
 
 class TrialScheduler:
@@ -23,19 +24,20 @@ class TrialScheduler:
         """
         return True
 
-    def on_trial_add(self, trial_runner, trial):
+    def on_trial_add(self, trial_runner: TrialRunner, trial: Trial):
         """Called when a new trial is added to the trial runner."""
 
         raise NotImplementedError
 
-    def on_trial_error(self, trial_runner, trial):
+    def on_trial_error(self, trial_runner: TrialRunner, trial: Trial):
         """Notification for the error of trial.
 
         This will only be called when the trial is in the RUNNING state."""
 
         raise NotImplementedError
 
-    def on_trial_result(self, trial_runner, trial, result):
+    def on_trial_result(self, trial_runner: TrialRunner, trial: Trial,
+                        result: Dict) -> str:
         """Called on each intermediate result returned by a trial.
 
         At this point, the trial scheduler can make a decision by returning
@@ -44,7 +46,8 @@ class TrialScheduler:
 
         raise NotImplementedError
 
-    def on_trial_complete(self, trial_runner, trial, result):
+    def on_trial_complete(self, trial_runner: TrialRunner, trial: Trial,
+                          result: Dict):
         """Notification for the completion of trial.
 
         This will only be called when the trial is in the RUNNING state and
@@ -52,7 +55,7 @@ class TrialScheduler:
 
         raise NotImplementedError
 
-    def on_trial_remove(self, trial_runner, trial):
+    def on_trial_remove(self, trial_runner: TrialRunner, trial: Trial):
         """Called to remove trial.
 
         This is called when the trial is in PAUSED or PENDING state. Otherwise,
@@ -60,7 +63,8 @@ class TrialScheduler:
 
         raise NotImplementedError
 
-    def choose_trial_to_run(self, trial_runner):
+    def choose_trial_to_run(self,
+                            trial_runner: TrialRunner) -> Optional[Trial]:
         """Called to choose a new trial to run.
 
         This should return one of the trials in trial_runner that is in
@@ -70,7 +74,7 @@ class TrialScheduler:
 
         raise NotImplementedError
 
-    def debug_string(self):
+    def debug_string(self) -> str:
         """Returns a human readable message for printing to the console."""
 
         raise NotImplementedError
@@ -79,22 +83,25 @@ class TrialScheduler:
 class FIFOScheduler(TrialScheduler):
     """Simple scheduler that just runs trials in submission order."""
 
-    def on_trial_add(self, trial_runner, trial):
+    def on_trial_add(self, trial_runner: TrialRunner, trial: Trial):
         pass
 
-    def on_trial_error(self, trial_runner, trial):
+    def on_trial_error(self, trial_runner: TrialRunner, trial: Trial):
         pass
 
-    def on_trial_result(self, trial_runner, trial, result):
+    def on_trial_result(self, trial_runner: TrialRunner, trial: Trial,
+                        result: Dict) -> str:
         return TrialScheduler.CONTINUE
 
-    def on_trial_complete(self, trial_runner, trial, result):
+    def on_trial_complete(self, trial_runner: TrialRunner, trial: Trial,
+                          result: Dict):
         pass
 
-    def on_trial_remove(self, trial_runner, trial):
+    def on_trial_remove(self, trial_runner: TrialRunner, trial: Trial):
         pass
 
-    def choose_trial_to_run(self, trial_runner):
+    def choose_trial_to_run(self,
+                            trial_runner: TrialRunner) -> Optional[Trial]:
         for trial in trial_runner.get_trials():
             if (trial.status == Trial.PENDING
                     and trial_runner.has_resources(trial.resources)):
@@ -105,5 +112,5 @@ class FIFOScheduler(TrialScheduler):
                 return trial
         return None
 
-    def debug_string(self):
+    def debug_string(self) -> str:
         return "Using FIFO scheduling algorithm."
