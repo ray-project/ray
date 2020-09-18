b8f1bb9e9c95e30fb404f75daf9ee32699d96555
architkulkarni <arkulkar@gmail.com>
2020-09-18 10:19:48 -0700

    add file

--- ci/long_running_tests/workloads/serve_handle.py
+++ ci/long_running_tests/workloads/serve_handle.py
@@ -1,3 +1,2 @@
-# A test that stresses the serve handles. 
-# E.g., spin up a backend with a bunch of replicas that just forward requests
-# to another backend.
+# A test that stresses the serve handles. E.g., spin up a backend with a bunch
+# of replicas that just forward requests to another backend.
