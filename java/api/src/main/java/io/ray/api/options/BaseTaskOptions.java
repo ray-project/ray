f4e72d145262153239ecd3d87f35213959d4be87
zhongchun <zhongchunyu@gmail.com>
2020-09-22 21:13:26 +0800

    Fix format

--- java/api/src/main/java/io/ray/api/options/BaseTaskOptions.java
+++ java/api/src/main/java/io/ray/api/options/BaseTaskOptions.java
@@ -20,8 +20,8 @@ public abstract class BaseTaskOptions {
         throw new IllegalArgumentException(String.format("Resource values should be "
             + "positive. Specified resource: %s = %s.", entry.getKey(), entry.getValue()));
       }
-      // Note: A resource value should be an integer if it is greater than 1.0. e.g. 3.0 is a valid resource value,
-      // but 3.5 is not.
+      // Note: A resource value should be an integer if it is greater than 1.0.
+      // e.g. 3.0 is a valid resource value, but 3.5 is not.
       if (entry.getValue().compareTo(1.0) >= 0
           && entry.getValue().compareTo(Math.floor(entry.getValue())) != 0) {
         throw new IllegalArgumentException(String.format("A resource value should be "
