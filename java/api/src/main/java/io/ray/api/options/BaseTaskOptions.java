83365b8b64f0d04c8c9c20baa4d37ef0f22e246e
zhongchun <zhongchunyu@gmail.com>
2020-09-22 17:51:16 +0800

    Add a resource checker for java worker creation

--- java/api/src/main/java/io/ray/api/options/BaseTaskOptions.java
+++ java/api/src/main/java/io/ray/api/options/BaseTaskOptions.java
@@ -16,9 +16,17 @@ public abstract class BaseTaskOptions {
 
   public BaseTaskOptions(Map<String, Double> resources) {
     for (Map.Entry<String, Double> entry : resources.entrySet()) {
-      if (entry.getValue().compareTo(0.0) <= 0) {
-        throw new IllegalArgumentException(String.format("Resource capacity should be " +
-            "positive, but got resource %s = %f.", entry.getKey(), entry.getValue()));
+      if (entry.getValue() == null || entry.getValue().compareTo(0.0) <= 0) {
+        throw new IllegalArgumentException(String.format("Resource capacity should be "
+            + "positive, but got resource %s = %s.", entry.getKey(), entry.getValue()));
+      }
+      // Note: resource value should be an integer if it is greater than 1.0, like 3.0 is legal,
+      // but 3.5 is illegal.
+      if (entry.getValue().compareTo(1.0) >= 0
+          && entry.getValue().compareTo(Math.floor(entry.getValue())) != 0) {
+        throw new IllegalArgumentException(String.format("Resource capacity should be "
+                + "a positive integer if it is greater than 1.0, but got resource %s = %s.",
+            entry.getKey(), entry.getValue()));
       }
     }
     this.resources = resources;
