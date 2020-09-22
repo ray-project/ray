d54d5677a08be844a878d9d0724718de7af22694
灵洵 <fengbin.ffb@antfin.com>
2020-09-22 20:52:42 +0800

    fix bug

--- src/ray/gcs/gcs_server/gcs_placement_group_scheduler.cc
+++ src/ray/gcs/gcs_server/gcs_placement_group_scheduler.cc
@@ -386,17 +386,28 @@ void GcsPlacementGroupScheduler::CommitAllBundles(
     const auto &node = gcs_node_manager_.GetNode(node_id);
     const auto &bundle = bundle_to_commit.second.second;
 
-    // TODO(sang) Handle the case nodes are dead.
-    CommitResources(
-        bundle, node,
-        [this, lease_status_tracker, bundle, node_id, schedule_failure_handler,
-         schedule_success_handler](const Status &status) {
-          lease_status_tracker->MarkCommitRequestReturned(node_id, bundle, status);
-          if (lease_status_tracker->AllCommitRequestReturned()) {
-            OnAllBundleCommitRequestReturned(
-                lease_status_tracker, schedule_failure_handler, schedule_success_handler);
-          }
-        });
+    if (node != nullptr) {
+      CommitResources(
+          bundle, node,
+          [this, lease_status_tracker, bundle, node_id, schedule_failure_handler,
+           schedule_success_handler](const Status &status) {
+            lease_status_tracker->MarkCommitRequestReturned(node_id, bundle, status);
+            if (lease_status_tracker->AllCommitRequestReturned()) {
+              OnAllBundleCommitRequestReturned(lease_status_tracker,
+                                               schedule_failure_handler,
+                                               schedule_success_handler);
+            }
+          });
+    } else {
+      RAY_LOG(WARNING)
+          << "Failed to commit resources because the node is dead, node id = " << node_id;
+      lease_status_tracker->MarkCommitRequestReturned(
+          node_id, bundle, Status::Interrupted("Node is dead"));
+      if (lease_status_tracker->AllCommitRequestReturned()) {
+        OnAllBundleCommitRequestReturned(lease_status_tracker, schedule_failure_handler,
+                                         schedule_success_handler);
+      }
+    }
   }
 }
 
