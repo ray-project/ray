6cfbd0e1a06160345b5814072994a924bcd61a5a
architkulkarni <arkulkar@gmail.com>
2020-09-23 09:15:14 -0700

    add scalability test

--- ci/long_running_tests/workloads/serve_handle.py
+++ ci/long_running_tests/workloads/serve_handle.py
@@ -1,26 +1,36 @@
-# A test that stresses the serve handles. E.g., spin up a backend with a bunch
+# A test that stresses the serve handles. We spin up a backend with a bunch
 # of replicas that just forward requests to another backend.
 
 import ray
 from ray import serve
 from ray.serve import BackendConfig
+import logging
 
 num_replicas = 4
 num_queries = 10
-# TODO(architkulkarni): edit to work on cluster
-# ray.init(address="auto")
-ray.init()
 
+ray.init(address="auto")
 client = serve.start()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def hello_world(_):
     return "Hello World"
 
+class forwardActor:
+    def __init__(self):
+        self.handle = client.get_handle("hello_world")
+
+    def __call__(self, _):
+        return ray.get(handle.remote())
+
+# Here the handle is created each time the function is called, so it may stress
+# the system more.
 def forward(_):
     handle = client.get_handle("hello_world")
     return ray.get(handle.remote())
 
-# TODO(architkulkarni): edit to work on cluster
 config = BackendConfig(num_replicas=num_replicas)
 
 client.create_backend("forward", forward, config=config)
@@ -29,8 +39,14 @@ client.create_endpoint("forward", backend="forward")
 client.create_backend("hello_world", hello_world)
 client.create_endpoint("hello_world", backend="hello_world")
 
-handle = client.get_handle("forward")
-print("Starting serve handle stress testing")
+handle = client.get_handle("forwardActor")
+logger.info("Starting serve handle stress testing (actor)")
 for _ in range(num_queries):
     ray.get(handle.remote())
-print("Finished serve handle stress testing")
+logger.info("Finished serve handle stress testing (actor)")
+
+handle = client.get_handle("forward")
+logger.info("Starting serve handle stress testing (function)")
+for _ in range(num_queries):
+    ray.get(handle.remote())
+logger.info("Finished serve handle stress testing (function)")
---
+++ ci/long_running_tests/workloads/serve_scalability.py
@@ -0,0 +1,72 @@
+# A multi-node scalability test. We put an http proxy on the head node and spin
+# up e.g. 20 nodes and put a bunch of replicas on the cluster, and run a stress
+# test.
+
+import logging
+import time
+import subprocess
+from subprocess import PIPE
+
+import ray
+from ray import serve
+from ray.serve import BackendConfig
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+logger.info("Downloading load testing tool")
+subprocess.call([
+    "bash", "-c", "rm hey_linux_amd64 || true;"
+    "wget https://storage.googleapis.com/hey-release/hey_linux_amd64;"
+    "chmod +x hey_linux_amd64"
+])
+
+ray.init(address="auto")
+client = serve.start()
+
+# These numbers need to correspond with the autoscaler config file.
+# The number of remote nodes in the autoscaler should upper bound
+# these because sometimes nodes fail to update.
+num_remote_nodes = 20
+head_node_cpus = 2
+num_remote_cpus = num_remote_nodes * head_node_cpus
+
+# Wait until the expected number of nodes have joined the cluster.
+while True:
+    num_nodes = len(ray.nodes())
+    logger.info("Waiting for nodes {}/{}".format(num_nodes,
+                                                 num_remote_nodes + 1))
+    if num_nodes >= num_remote_nodes + 1:
+        break
+    time.sleep(5)
+logger.info("Nodes have all joined. There are %s resources.",
+            ray.cluster_resources())
+
+def hey(_):
+    time.sleep(0.01)  # Sleep for 10ms
+    return "hey"
+
+client.create_backend("hey", hey, BackendConfig(num_replicas=num_remote_cpus))
+client.create_endpoint("hey", backend="hey", route="/hey")
+
+logger.info("Warming up for ~3 seconds")
+for _ in range(5):
+    resp = requests.get("http://127.0.0.1:8000/hey").text
+    logger.info("Received response \'" + resp + "\'")
+    time.sleep(0.5)
+
+connections = int(num_remote_cpus * 0.75)
+
+while True:
+    proc = subprocess.Popen(
+        [
+            "./hey_linux_amd64", "-c",
+            str(connections), "-z", "60m", "http://127.0.0.1:8000/hey"
+        ],
+        stdout=PIPE,
+        stderr=PIPE)
+    logger.info("Started load testing")
+    proc.wait()
+    out, err = proc.communicate()
+    print(out.decode())
+    print(err.decode())
