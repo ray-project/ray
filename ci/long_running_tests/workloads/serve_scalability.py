6f52ce983005a1da6678e5ddf812f4f94066248d
architkulkarni <arkulkar@gmail.com>
2020-09-24 10:25:00 -0700

    fix backendconfig syntax

--- ci/long_running_tests/workloads/serve_scalability.py
+++ ci/long_running_tests/workloads/serve_scalability.py
@@ -49,7 +49,7 @@ def hey(_):
     return "hey"
 
 
-client.create_backend("hey", hey, BackendConfig(num_replicas=num_remote_cpus))
+client.create_backend("hey", hey, config=BackendConfig(num_replicas=num_remote_nodes))
 client.create_endpoint("hey", backend="hey", route="/hey")
 
 logger.info("Warming up for ~3 seconds")
