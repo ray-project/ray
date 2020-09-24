277d6ff3bc3c2103003f7e33fde46e76a93ed3c7
architkulkarni <arkulkar@gmail.com>
2020-09-24 15:21:45 -0700

    change stress test to wrk

--- ci/long_running_tests/workloads/serve_scalability.py
+++ ci/long_running_tests/workloads/serve_scalability.py
@@ -5,7 +5,6 @@
 import logging
 import time
 import subprocess
-from subprocess import PIPE
 import requests
 
 import ray
@@ -15,20 +14,13 @@ from ray.serve import BackendConfig
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-logger.info("Downloading load testing tool")
-subprocess.call([
-    "bash", "-c", "rm hey_linux_amd64 || true;"
-    "wget https://storage.googleapis.com/hey-release/hey_linux_amd64;"
-    "chmod +x hey_linux_amd64"
-])
-
 ray.init(address="auto")
 client = serve.start()
 
 # These numbers need to correspond with the autoscaler config file.
 # The number of remote nodes in the autoscaler should upper bound
 # these because sometimes nodes fail to update.
-num_remote_nodes = 20
+num_remote_nodes = 4
 head_node_cpus = 2
 num_remote_cpus = num_remote_nodes * head_node_cpus
 
@@ -48,9 +40,9 @@ def hey(_):
     time.sleep(0.01)  # Sleep for 10ms
     return "hey"
 
-
+#TODO(architkulkarni): how many replicas?
 client.create_backend(
-    "hey", hey, config=BackendConfig(num_replicas=num_remote_nodes - 2))
+    "hey", hey, config=BackendConfig(num_replicas=num_remote_nodes/2))
 client.create_endpoint("hey", backend="hey", route="/hey")
 
 logger.info("Warming up for ~3 seconds")
@@ -59,18 +51,14 @@ for _ in range(5):
     logger.info("Received response \'" + resp + "\'")
     time.sleep(0.5)
 
-connections = int(num_remote_cpus * 0.75)
+num_connections = int(num_remote_cpus * 0.75)
+num_threads = 2
+time_to_run = "10s"
 
-while True:
-    proc = subprocess.Popen(
-        [
-            "./hey_linux_amd64", "-c",
-            str(connections), "-z", "60m", "http://127.0.0.1:8000/hey"
-        ],
-        stdout=PIPE,
-        stderr=PIPE)
-    logger.info("Started load testing")
-    proc.wait()
-    out, err = proc.communicate()
-    print(out.decode())
-    print(err.decode())
+#TODO(architkulkarni): make long-running?
+result = subprocess.run([
+    "wrk", "-c",
+    str(num_connections), "-t",
+    str(num_threads), "-s", time_to_run, "http://127.0.0.1:8000/hey"
+], stdout=subprocess.PIPE)
+logger.info(result.stdout.decode())
