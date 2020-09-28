8869457a1037d87e97990cc9d13e78560a955af9
Kai Yang <kfstorm@outlook.com>
2020-09-28 16:58:04 +0800

    lint

--- java/test/src/main/java/io/ray/test/AnnotationTransformer.java
+++ java/test/src/main/java/io/ray/test/AnnotationTransformer.java
@@ -2,7 +2,6 @@ package io.ray.test;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
-
 import org.testng.IAnnotationTransformer;
 import org.testng.annotations.ITestAnnotation;
 
