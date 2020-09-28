7dec3f924211a840fff21ce8ec74b22d6e875752
sven1977 <svenmika1977@gmail.com>
2020-09-28 15:25:24 +0200

    Fixes.

--- rllib/utils/exploration/gaussian_noise.py
+++ rllib/utils/exploration/gaussian_noise.py
@@ -147,7 +147,7 @@ class GaussianNoise(Exploration):
         # Apply exploration.
         if explore:
             # Random exploration phase.
-            if self.last_timestep <= self.random_timesteps:
+            if self.last_timestep < self.random_timesteps:
                 action, _ = \
                     self.random_exploration.get_torch_exploration_action(
                         action_dist, explore=True)
