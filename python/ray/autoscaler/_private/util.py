e018213e2b630fdb405b5722ffc01e5f45f992e7
Eric Liang <ekhliang@gmail.com>
2020-09-29 15:53:36 -0700

    update

--- python/ray/autoscaler/_private/util.py
+++ python/ray/autoscaler/_private/util.py
@@ -8,7 +8,7 @@ from typing import Any, Dict
 
 import ray
 import ray._private.services as services
-from ray.autoscaler.node_provider import _get_default_config
+from ray.autoscaler._private.providers import _get_default_config
 from ray.autoscaler._private.docker import validate_docker_config
 
 REQUIRED, OPTIONAL = True, False
