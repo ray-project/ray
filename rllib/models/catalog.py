943685697e7c18f38383cea7787522ef3f16ee87
sven1977 <svenmika1977@gmail.com>
2020-09-29 16:08:45 +0200

    LINT.

--- rllib/models/catalog.py
+++ rllib/models/catalog.py
@@ -167,7 +167,8 @@ class ModelCatalog:
                     else Deterministic
         # Discrete Space -> Categorical.
         elif isinstance(action_space, gym.spaces.Discrete):
-            dist_cls = TorchCategorical if framework == "torch" else Categorical
+            dist_cls = (
+                TorchCategorical if framework == "torch" else Categorical)
         # Tuple/Dict Spaces -> MultiAction.
         elif dist_type in (MultiActionDistribution,
                            TorchMultiActionDistribution) or \
