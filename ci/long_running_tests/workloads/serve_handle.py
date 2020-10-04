c7ba4e95e7f26685c1b01c3fde9dd880b547cc96
architkulkarni <arkulkar@gmail.com>
2020-10-03 22:21:17 -0700

    clean up handle test

--- ci/long_running_tests/workloads/serve_handle.py
+++ ci/long_running_tests/workloads/serve_handle.py
@@ -1,22 +1,36 @@
 # A test that stresses the serve handles. We spin up a backend with a bunch
-# of replicas that just forward requests to another backend.
+# (0-2) of replicas that just forward requests to another backend.
+#
+# By comparing using the forward replicas with just calling the worker
+# replicas, we measure the (latency) overhead in the handle. This answers
+# the question: How much of a latency/throughput hit is there when I
+# compose models?
+#
+# By comparing the qps as we fix the number of forwarder replicas and vary the
+# number of worker replicas, we measure the limit of a single async actor. This
+# answers the question: How many "ForwardActor"s or these kinds of high-level
+# pipeline workers do I need to provision for my workload? every 1k qps,
+# 2k qps?
+#
+# Sample output:
+# INFO:__main__:0 forwarders and 1 worker replicas: 534 requests/s
+# INFO:__main__:0 forwarders and 5 worker replicas: 508 requests/s
+# INFO:__main__:0 forwarders and 10 worker replicas: 466 requests/s
+# INFO:__main__:1 forwarders and 1 worker replicas: 273 requests/s
+# INFO:__main__:1 forwarders and 5 worker replicas: 258 requests/s
+# INFO:__main__:1 forwarders and 10 worker replicas: 254 requests/s
+# INFO:__main__:2 forwarders and 1 worker replicas: 268 requests/s
+# INFO:__main__:2 forwarders and 5 worker replicas: 250 requests/s
+# INFO:__main__:2 forwarders and 10 worker replicas: 250 requests/s
 
 import ray
 from ray import serve
 from ray.serve import BackendConfig
 import logging
-import argparse
+import time
 
-parser = argparse.ArgumentParser()
-parser.add_argument("num_replicas")
-parser.add_argument("num_queries")
-
-args = parser.parse_args()
-# TODO(architkulkarni): ensure there are enough workers in cluster yaml
-num_replicas = args.num_replicas
-num_queries = args.num_queries
-
-ray.init(address="auto")
+# ray.init(address="auto")
+ray.init()
 client = serve.start()
 
 logging.basicConfig(level=logging.INFO)
@@ -29,38 +43,46 @@ def hello_world(_):
 
 class ForwardActor:
     def __init__(self):
+        client = serve.connect()
         self.handle = client.get_handle("hello_world")
 
     async def __call__(self, _):
         return ray.get(self.handle.remote())
 
 
-# Here the handle is created each time the function is called, so it may stress
-# the system more.
-def forward(_):
-    handle = client.get_handle("hello_world")
-    return ray.get(handle.remote())
-
-
-config = BackendConfig(num_replicas=num_replicas)
-
-client.create_backend("forward", forward, config=config)
-client.create_endpoint("forward", backend="forward")
-
 client.create_backend("hello_world", hello_world)
 client.create_endpoint("hello_world", backend="hello_world")
 
-client.create_backend("ForwardActor", ForwardActor, config=config)
+client.create_backend("ForwardActor", ForwardActor)
 client.create_endpoint("ForwardActor", backend="ForwardActor")
 
-handle = client.get_handle("ForwardActor")
-logger.info("Starting serve handle stress testing (actor)")
-for _ in range(num_queries):
-    ray.get(handle.remote())
-logger.info("Finished serve handle stress testing (actor)")
 
-handle = client.get_handle("forward")
-logger.info("Starting serve handle stress testing (function)")
-for _ in range(num_queries):
-    ray.get(handle.remote())
-logger.info("Finished serve handle stress testing (function)")
+def run_test(num_replicas, num_forwarders):
+    replicas_config = BackendConfig(num_replicas=num_replicas)
+    client.update_backend_config("hello_world", replicas_config)
+
+    if (num_forwarders == 0):
+        handle = client.get_handle("hello_world")
+    else:
+        forwarders_config = BackendConfig(num_replicas=num_forwarders)
+        client.update_backend_config("ForwardActor", forwarders_config)
+        handle = client.get_handle("ForwardActor")
+
+    # warmup - helpful to wait for gc.collect() and actors to start
+    start = time.time()
+    while time.time() - start < 1:
+        ray.get(handle.remote())
+
+    # real test
+    start = time.time()
+    count = 0
+    while time.time() - start < 1:
+        ray.get(handle.remote())
+        count += 1
+    logger.info("{} forwarders and {} worker replicas: {} requests/s".format(
+        num_forwarders, num_replicas, count))
+
+
+for num_forwarders in [0, 1, 2]:
+    for num_replicas in [1, 5, 10]:
+        run_test(num_replicas, num_forwarders)
