55bccf59f6762a2769406247440bf4880e9c02b6
Max Fitton <max@semprehealth.com>
2020-10-09 18:00:10 -0400

    small change to trigger new tests

--- dashboard/modules/reporter/reporter_agent.py
+++ dashboard/modules/reporter/reporter_agent.py
@@ -117,7 +117,7 @@ class ReporterAgent(dashboard_utils.DashboardAgentModule,
             gpus = gpustat.new_query().gpus
         except Exception as e:
             logger.debug(
-                "gpustat failed to retrieve GPU information: {}".format(e))
+                f"gpustat failed to retrieve GPU information: {e}")
         for gpu in gpus:
             # Note the keys in this dict have periods which throws
             # off javascript so we change .s to _s
