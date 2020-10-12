1f1b25fac8271fcd04a42de75f68a485619379b2
WangTaoTheTonic <dooku.wt@antfin.com>
2020-10-12 16:44:01 +0800

    update raylet map first

--- python/ray/tests/test_multi_node_2.py
+++ python/ray/tests/test_multi_node_2.py
@@ -67,9 +67,9 @@ def test_system_config(ray_start_cluster_head):
 def setup_monitor(address):
     monitor = Monitor(
         address, None, redis_password=ray_constants.REDIS_DEFAULT_PASSWORD)
+    monitor.update_raylet_map(_append_port=True)
     monitor.psubscribe(ray.gcs_utils.XRAY_HEARTBEAT_BATCH_PATTERN)
     monitor.psubscribe(ray.gcs_utils.XRAY_JOB_PATTERN)  # TODO: Remove?
-    monitor.update_raylet_map(_append_port=True)
     return monitor
 
 
