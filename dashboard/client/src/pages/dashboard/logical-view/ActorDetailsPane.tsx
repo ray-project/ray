f7e1c003f077d8145c273ddf9fd5382f87f91806
Max Fitton <max@semprehealth.com>
2020-10-12 21:46:22 -0400

    reconfigure how ui displays according to Ed's feedback

--- dashboard/client/src/pages/dashboard/logical-view/ActorDetailsPane.tsx
+++ dashboard/client/src/pages/dashboard/logical-view/ActorDetailsPane.tsx
@@ -18,77 +18,77 @@ type ActorDatum = {
 const labeledActorData = (actor: ActorInfo) => (
   isFullActorInfo(actor)
     ? [
-        {
-          label: "Resources",
-          value:
-            actor.usedResources &&
-            Object.entries(actor.usedResources).length > 0 &&
-            Object.entries(actor.usedResources)
-              .sort((a, b) => a[0].localeCompare(b[0]))
-              .map(
-                ([key, value]) =>
-                  `${sum(
-                    value.resourceSlots.map((slot) => slot.allocation),
-                  )} ${key}`,
-              )
-              .join(", "),
-        },
-        {
-          label: "Number of pending tasks",
-          value: actor.taskQueueLength?.toLocaleString() ?? "0",
-          tooltip:
-            "The number of tasks that are currently pending to execute on this actor. If this number " +
-            "remains consistently high, it may indicate that this actor is a bottleneck in your application.",
-        },
-        {
-          label: "Number of executed tasks",
-          value: actor.numExecutedTasks?.toLocaleString() ?? "0",
-          tooltip:
-            "The number of tasks this actor has executed throughout its lifetimes.",
-        },
-        {
-          label: "Number of ObjectRefs in scope",
-          value: actor.numObjectRefsInScope?.toLocaleString() ?? "0",
-          tooltip:
-            "The number of ObjectRefs that this actor is keeping in scope via its internal state. " +
-            "This does not imply that the objects are in active use or colocated on the node with the actor " +
-            `currently. This can be useful for debugging memory leaks. See the docs at ${memoryDebuggingDocLink} ` +
-            "for more information.",
-        },
-        {
-          label: "Number of local objects",
-          value: actor.numLocalObjects?.toLocaleString() ?? "0",
-          tooltip:
-            "The number of small objects that this actor has stored in its local in-process memory store. This can be useful for " +
-            `debugging memory leaks. See the docs at ${memoryDebuggingDocLink} for more information`,
-        },
-        {
-          label: "Object store memory used (MiB)",
-          value: actor.usedObjectStoreMemory?.toLocaleString() ?? "0",
-          tooltip:
-            "The total amount of memory that this actor is occupying in the Ray object store. " +
-            "If this number is increasing without bounds, you might have a memory leak. See " +
-            `the docs at: ${memoryDebuggingDocLink} for more information.`,
-        },
-      ]
+      {
+        label: "Resources",
+        value:
+          actor.usedResources &&
+          Object.entries(actor.usedResources).length > 0 &&
+          Object.entries(actor.usedResources)
+            .sort((a, b) => a[0].localeCompare(b[0]))
+            .map(
+              ([key, value]) =>
+                `${sum(
+                  value.resourceSlots.map((slot) => slot.allocation),
+                )} ${key}`,
+            )
+            .join(", "),
+      },
+      {
+        label: "Number of pending tasks",
+        value: actor.taskQueueLength?.toLocaleString() ?? "0",
+        tooltip:
+          "The number of tasks that are currently pending to execute on this actor. If this number " +
+          "remains consistently high, it may indicate that this actor is a bottleneck in your application.",
+      },
+      {
+        label: "Number of executed tasks",
+        value: actor.numExecutedTasks?.toLocaleString() ?? "0",
+        tooltip:
+          "The number of tasks this actor has executed throughout its lifetimes.",
+      },
+      {
+        label: "Number of ObjectRefs in scope",
+        value: actor.numObjectRefsInScope?.toLocaleString() ?? "0",
+        tooltip:
+          "The number of ObjectRefs that this actor is keeping in scope via its internal state. " +
+          "This does not imply that the objects are in active use or colocated on the node with the actor " +
+          `currently. This can be useful for debugging memory leaks. See the docs at ${memoryDebuggingDocLink} ` +
+          "for more information.",
+      },
+      {
+        label: "Number of local objects",
+        value: actor.numLocalObjects?.toLocaleString() ?? "0",
+        tooltip:
+          "The number of small objects that this actor has stored in its local in-process memory store. This can be useful for " +
+          `debugging memory leaks. See the docs at ${memoryDebuggingDocLink} for more information`,
+      },
+      {
+        label: "Object store memory used (MiB)",
+        value: actor.usedObjectStoreMemory?.toLocaleString() ?? "0",
+        tooltip:
+          "The total amount of memory that this actor is occupying in the Ray object store. " +
+          "If this number is increasing without bounds, you might have a memory leak. See " +
+          `the docs at: ${memoryDebuggingDocLink} for more information.`,
+      },
+    ]
     : [
-        {
-          label: "Actor ID",
-          value: actor.actorId,
-          tooltip: "",
-        },
-        {
-          label: "Required resources",
-          value:
-            actor.requiredResources &&
-            Object.entries(actor.requiredResources).length > 0 &&
-            Object.entries(actor.requiredResources)
-              .sort((a, b) => a[0].localeCompare(b[0]))
-              .map(([key, value]) => `${value.toLocaleString()} ${key}`)
-              .join(", "),
-          tooltip: "",
-        },
-      ]);
+      {
+        label: "Actor ID",
+        value: actor.actorId,
+        tooltip: "",
+      },
+      {
+        label: "Required resources",
+        value:
+          actor.requiredResources &&
+          Object.entries(actor.requiredResources).length > 0 &&
+          Object.entries(actor.requiredResources)
+            .sort((a, b) => a[0].localeCompare(b[0]))
+            .map(([key, value]) => `${value.toLocaleString()} ${key}`)
+            .join(", "),
+        tooltip: "",
+      },
+    ]);
 
 
 type ActorDetailsPaneProps = {
@@ -122,6 +122,42 @@ const ActorDetailsPane: React.FC<ActorDetailsPaneProps> = ({
         <div>{actor.actorClass}</div>
         <ActorStateRepr state={actor.state} />
       </div>
+      {isFullActorInfo(actor) &&
+        <Grid container className={classes.detailsPane}>
+          <Grid container item xs={6}>
+            <Grid item xs={4}>
+              <Typography>CPU Usage</Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <UsageBar
+                percent={actor.processStats?.cpuPercent ?? 0}
+                text={`${actor.processStats?.cpuPercent ?? 0}%`}
+              />
+            </Grid>
+            <Grid item xs={4} />
+          </Grid>
+        { actor.gpus.length > 0 &&
+          <Grid container item xs={6}>
+            <Grid item xs={12}>
+              <Typography>GPU Usage</Typography>
+            </Grid>
+            {actor.gpus.map(gpu => (
+              <React.Fragment key={gpu.uuid}>
+                <Grid item xs={4}>
+                  {`[${gpu.name}]`}
+                </Grid>
+                <Grid item xs={4}>
+                  <UsageBar
+                    percent={gpu.utilizationGpu * 100}
+                    text={`${gpu.utilizationGpu * 100}%`}
+                  />
+                </Grid>
+                <Grid item xs={4} />
+              </React.Fragment>
+            ))}
+          </Grid>
+        }
+        </Grid>}
       <Divider className={classes.divider} />
       <Grid container className={classes.detailsPane}>
         {actorData.map(
@@ -137,42 +173,6 @@ const ActorDetailsPane: React.FC<ActorDetailsPaneProps> = ({
             ),
         )}
       </Grid>
-      {isFullActorInfo(actor) &&
-        <Grid container className={classes.detailsPane}>
-          <Grid container item xs={6}>
-            <Grid item xs={12}>
-              <Typography>CPU Usage</Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <UsageBar
-                percent={actor.processStats?.cpuPercent ?? 0}
-                text={`${actor.processStats?.cpuPercent ?? 0}`}
-              />
-            </Grid>
-        </Grid>
-
-        { actor.gpus.length > 0 &&
-          <Grid container item xs={6}>
-            <Grid item xs={12}>
-              <Typography>GPU Usage</Typography>
-            </Grid>
-            {actor.gpus.map(gpu => (
-              <React.Fragment key={gpu.uuid}>
-                <Grid item xs={3}>
-                  {`[${gpu.name}]`}
-                </Grid>
-                <Grid item xs={9}>
-                  <UsageBar
-                    percent={gpu.utilizationGpu * 100}
-                    text={`${gpu.utilizationGpu * 100}%`}
-                  />
-                </Grid>
-              </React.Fragment>
-            ))}
-          </Grid>
-
-        }
-        </Grid>}
     </React.Fragment>
   );
 };
