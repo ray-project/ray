2ec295126762f75c6e71e5fc6909dab9253d0711
久龙 <guyang.sgy@antfin.com>
2020-10-14 14:44:16 +0800

    fix actor creation task

--- cpp/src/ray/runtime/task/native_task_submitter.cc
+++ cpp/src/ray/runtime/task/native_task_submitter.cc
@@ -8,17 +8,18 @@
 namespace ray {
 namespace api {
 
-ObjectID NativeTaskSubmitter::Submit(const InvocationSpec &invocation) {
+RayFunction BuildRayFunction(const InvocationSpec &invocation) {
   auto base_addr =
       GetBaseAddressOfLibraryFromAddr((void *)invocation.fptr.function_pointer);
-
   auto func_offset = (size_t)(invocation.fptr.function_pointer - base_addr);
   auto exec_func_offset = (size_t)(invocation.fptr.exec_function_pointer - base_addr);
   auto function_descriptor = FunctionDescriptorBuilder::BuildCpp(
       invocation.lib_name, std::to_string(func_offset), std::to_string(exec_func_offset));
-  auto ray_function = RayFunction(Language::CPP, function_descriptor);
+  return RayFunction(Language::CPP, function_descriptor);
+}
 
-  std::vector<std::unique_ptr<ray::TaskArg>> args;
+void BuildTaskArgs(const InvocationSpec &invocation,
+                   std::vector<std::unique_ptr<ray::TaskArg>> &args) {
   if (invocation.args->size() > 0) {
     auto buffer = std::make_shared<::ray::LocalMemoryBuffer>(
         reinterpret_cast<uint8_t *>(invocation.args->data()), invocation.args->size(),
@@ -27,14 +28,19 @@ ObjectID NativeTaskSubmitter::Submit(const InvocationSpec &invocation) {
         std::make_shared<::ray::RayObject>(buffer, nullptr, std::vector<ObjectID>()));
     args.emplace_back(task_arg);
   }
+}
+
+ObjectID NativeTaskSubmitter::Submit(const InvocationSpec &invocation) {
+  std::vector<std::unique_ptr<ray::TaskArg>> args;
+  BuildTaskArgs(invocation, args);
   auto &core_worker = CoreWorkerProcess::GetCoreWorker();
   std::vector<ObjectID> return_ids;
   if (invocation.task_type == TaskType::ACTOR_TASK) {
-    core_worker.SubmitActorTask(invocation.actor_id, ray_function, args, TaskOptions(),
-                                &return_ids);
+    core_worker.SubmitActorTask(invocation.actor_id, BuildRayFunction(invocation), args,
+                                TaskOptions(), &return_ids);
   } else {
-    core_worker.SubmitTask(ray_function, args, TaskOptions(), &return_ids, 1,
-                           std::make_pair(PlacementGroupID::Nil(), -1), true);
+    core_worker.SubmitTask(BuildRayFunction(invocation), args, TaskOptions(), &return_ids,
+                           1, std::make_pair(PlacementGroupID::Nil(), -1), true);
   }
   return return_ids[0];
 }
@@ -44,22 +50,8 @@ ObjectID NativeTaskSubmitter::SubmitTask(const InvocationSpec &invocation) {
 }
 
 ActorID NativeTaskSubmitter::CreateActor(const InvocationSpec &invocation) {
-  auto base_addr =
-      GetBaseAddressOfLibraryFromAddr((void *)invocation.fptr.function_pointer);
-
-  auto func_offset = (size_t)(invocation.fptr.function_pointer - base_addr);
-  auto exec_func_offset = (size_t)(invocation.fptr.exec_function_pointer - base_addr);
-  auto function_descriptor = FunctionDescriptorBuilder::BuildCpp(
-      invocation.lib_name, std::to_string(func_offset), std::to_string(exec_func_offset));
-  auto ray_function = RayFunction(Language::CPP, function_descriptor);
-
-  auto buffer = std::make_shared<::ray::LocalMemoryBuffer>(
-      reinterpret_cast<uint8_t *>(invocation.args->data()), invocation.args->size(),
-      true);
   std::vector<std::unique_ptr<ray::TaskArg>> args;
-  auto task_arg = new TaskArgByValue(
-      std::make_shared<::ray::RayObject>(buffer, nullptr, std::vector<ObjectID>()));
-  args.emplace_back(task_arg);
+  BuildTaskArgs(invocation, args);
 
   auto &core_worker = CoreWorkerProcess::GetCoreWorker();
 
@@ -75,11 +67,11 @@ ActorID NativeTaskSubmitter::CreateActor(const InvocationSpec &invocation) {
                                      name,
                                      /*is_asyncio=*/false};
   ActorID actor_id;
-  auto status = core_worker.CreateActor(ray_function, args, actor_options, "", &actor_id);
+  auto status = core_worker.CreateActor(BuildRayFunction(invocation), args, actor_options,
+                                        "", &actor_id);
   if (!status.ok()) {
     throw RayException("Create actor error");
   }
-
   return actor_id;
 }
 
