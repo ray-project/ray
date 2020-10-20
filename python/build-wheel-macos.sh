0d212039093759e3d2ae9bff696a459eca924018
Max Fitton <max@semprehealth.com>
2020-10-20 02:49:13 -0400

    issue not excluding the dashboard/client dir in the git clean cmd building wheels

--- python/build-wheel-macos.sh
+++ python/build-wheel-macos.sh
@@ -54,7 +54,7 @@ for ((i=0; i<${#PY_VERSIONS[@]}; ++i)); do
   # The -f flag is passed twice to also run git clean in the arrow subdirectory.
   # The -d flag removes directories. The -x flag ignores the .gitignore file,
   # and the -e flag ensures that we don't remove the .whl directory.
-  git clean -f -f -x -d -e .whl -e $DOWNLOAD_DIR -e python/ray/new_dashboard/client
+  git clean -f -f -x -d -e .whl -e $DOWNLOAD_DIR -e python/ray/new_dashboard/client -e dashboard/client
 
   # Install Python.
   INST_PATH=python_downloads/$PY_INST
