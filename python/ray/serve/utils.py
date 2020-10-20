2c5cb95b42496dc418141082548bfcdb8d9f2a86
Simon Mo <simon.mo@hey.com>
2020-10-20 10:44:23 -0700

    [Serve] Get ServeHandle on the same node (#11477)

--- python/ray/serve/utils.py
+++ python/ray/serve/utils.py
@@ -299,3 +299,9 @@ def get_all_node_ids():
             node_ids.append(("{}-{}".format(node_id, index), node_id))
 
     return node_ids
+
+
+def get_node_id_for_actor(actor_handle):
+    """Given an actor handle, return the node id it's placed on."""
+
+    return ray.actors()[actor_handle._actor_id.hex()]["Address"]["NodeID"]
