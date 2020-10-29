7053dc049a80339b2c28dce078a1e4caa06feebb
Kai Fricke <kai@anyscale.com>
2020-10-29 17:34:19 +0100

    Fix sync test

--- python/ray/tune/tests/test_sync.py
+++ python/ray/tune/tests/test_sync.py
@@ -87,8 +87,8 @@ class TestSyncFunctionality(unittest.TestCase):
 
     def testClusterProperString(self):
         """Tests that invalid commands throw.."""
-        with self.assertRaises(TuneError):
-            # This raises TuneError because logger is init in safe zone.
+        with self.assertRaises(ValueError):
+            # This raises ValueError because logger is init in safe zone.
             sync_config = tune.SyncConfig(sync_to_driver="ls {target}")
             [trial] = tune.run(
                 "__fake",
@@ -100,8 +100,8 @@ class TestSyncFunctionality(unittest.TestCase):
                 sync_config=sync_config,
             ).trials
 
-        with self.assertRaises(TuneError):
-            # This raises TuneError because logger is init in safe zone.
+        with self.assertRaises(ValueError):
+            # This raises ValueError because logger is init in safe zone.
             sync_config = tune.SyncConfig(sync_to_driver="ls {source}")
             [trial] = tune.run(
                 "__fake",
