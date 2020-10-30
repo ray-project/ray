d6628cdbfb5ade209b060a79bb83fa460d9c8e7b
Max Fitton <maxfitton@anyscale.com>
2020-10-29 23:11:50 -0700

    [Dashboard] Fix null gpu utilization (#11650)
    * update dashboard to work if GPU utilization field is missing from GPU payload
    
    * lint
    
    * lint

--- dashboard/client/src/pages/dashboard/logical-view/ActorDetailsPane.tsx
+++ dashboard/client/src/pages/dashboard/logical-view/ActorDetailsPane.tsx
@@ -144,20 +144,33 @@ const ActorDetailsPane: React.FC<ActorDetailsPaneProps> = ({ actor }) => {
               <Grid item xs={12}>
                 <Typography>GPU Usage</Typography>
               </Grid>
-              {actor.gpus.map((gpu) => (
-                <React.Fragment key={gpu.uuid}>
-                  <Grid item xs={4}>
-                    {`[${gpu.name}]`}
-                  </Grid>
-                  <Grid item xs={4}>
-                    <UsageBar
-                      percent={gpu.utilizationGpu * 100}
-                      text={`${gpu.utilizationGpu * 100}%`}
-                    />
-                  </Grid>
-                  <Grid item xs={4} />
-                </React.Fragment>
-              ))}
+              {actor.gpus.map((gpu) => {
+                const gpuUtilization = gpu.utilizationGpu ? (
+                  <UsageBar
+                    percent={gpu.utilizationGpu * 100}
+                    text={`${gpu.utilizationGpu * 100}%`}
+                  />
+                ) : (
+                  <Typography
+                    color="textSecondary"
+                    component="span"
+                    variant="inherit"
+                  >
+                    N/A
+                  </Typography>
+                );
+                return (
+                  <React.Fragment key={gpu.uuid}>
+                    <Grid item xs={4}>
+                      {`[${gpu.name}]`}
+                    </Grid>
+                    <Grid item xs={4}>
+                      {gpuUtilization}
+                    </Grid>
+                    <Grid item xs={4} />
+                  </React.Fragment>
+                );
+              })}
             </Grid>
           )}
         </Grid>
