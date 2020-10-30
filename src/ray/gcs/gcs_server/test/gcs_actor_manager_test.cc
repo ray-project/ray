9c91275b155fcc7c6d7455d60fe56f99e18b2829
SangBin Cho <rkooo567@gmail.com>
2020-10-29 23:23:26 -0700

    Fix a build error.

--- src/ray/gcs/gcs_server/test/gcs_actor_manager_test.cc
+++ src/ray/gcs/gcs_server/test/gcs_actor_manager_test.cc
@@ -87,7 +87,7 @@ class GcsActorManagerTest : public ::testing::Test {
     gcs_table_storage_ = std::make_shared<gcs::InMemoryGcsTableStorage>(io_service_);
     gcs_actor_manager_.reset(new gcs::GcsActorManager(
         mock_actor_scheduler_, gcs_table_storage_, gcs_pub_sub_,
-        [this](const ActorID &actor_id) {},
+        [](const ActorID &actor_id) {},
         [this](const rpc::Address &addr) { return worker_client_; }));
   }
 
