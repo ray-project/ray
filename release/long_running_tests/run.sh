05c4e3fb2a3b36ceea90cac4104d8466a0cc2ac5
Barak Michener <me@barakmich.com>
2020-11-03 19:36:32 -0800

    [build] Build wheels with manylinux2014 (#11621)
    * necessary changes
    
    * Split bazel install
    
    * manylinux2014
    
    * change references to manylinux2014
    
    * Fix lint
    
    * port alex's docker build changes
    
    * fix config issue
    
    * remove extra manylinux2010 requirement script
    
    * revert SHA overwrite
    
    * wip
    
    * incompatible_linklibs
    
    * fix nits

--- release/long_running_tests/run.sh
+++ release/long_running_tests/run.sh
@@ -48,7 +48,7 @@ echo "commit: $commit"
 echo "branch: $ray_branch"
 echo "workload: $workload"
 
-wheel="https://s3-us-west-2.amazonaws.com/ray-wheels/$ray_branch/$commit/ray-$ray_version-cp36-cp36m-manylinux1_x86_64.whl"
+wheel="https://s3-us-west-2.amazonaws.com/ray-wheels/$ray_branch/$commit/ray-$ray_version-cp36-cp36m-manylinux2014_x86_64.whl"
 
 echo set-window-option -g mouse on > ~/.tmux.conf
 echo 'termcapinfo xterm* ti@:te@' > ~/.screenrc
