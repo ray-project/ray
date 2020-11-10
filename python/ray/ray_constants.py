50dbf1a3078b953ebaaad4ed9432a9302df2757b
Richard Liaw <rliaw@berkeley.edu>
2020-11-10 14:57:57 -0800

    [core] Support configurable number of "check for redis" attempts (#11902)
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>

--- python/ray/ray_constants.py
+++ python/ray/ray_constants.py
@@ -137,6 +137,10 @@ RESOURCES_ENVIRONMENT_VARIABLE = "RAY_OVERRIDE_RESOURCES"
 # The reporter will report its statistics this often (milliseconds).
 REPORTER_UPDATE_INTERVAL_MS = env_integer("REPORTER_UPDATE_INTERVAL_MS", 2500)
 
+# Number of attempts to ping the Redis server. See
+# `services.py:wait_for_redis_to_start`.
+START_REDIS_WAIT_RETRIES = env_integer("RAY_START_REDIS_WAIT_RETRIES", 12)
+
 LOGGER_FORMAT = (
     "%(asctime)s\t%(levelname)s %(filename)s:%(lineno)s -- %(message)s")
 LOGGER_FORMAT_HELP = f"The logging format. default='{LOGGER_FORMAT}'"
