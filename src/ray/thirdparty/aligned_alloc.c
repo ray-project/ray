7becdab035d4f585e3b845ccd6b5deb95a724b0e
Siyuan <suquark@gmail.com>
2020-11-09 21:02:04 -0800

    license. fix compiler warnings

--- src/ray/thirdparty/aligned_alloc.c
+++ src/ray/thirdparty/aligned_alloc.c
@@ -1,3 +1,29 @@
+/*
+Adopted from https://github.com/NickStrupat/AlignedMalloc
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Nick Strupat
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "ray/thirdparty/aligned_alloc.h"
 
 #if defined(__APPLE__) || defined(__linux__)
@@ -6,7 +32,7 @@
 
 void *aligned_malloc(size_t size, size_t alignment) {
 	void *pointer;
-	posix_memalign(&pointer, alignment, size);
+	(void)posix_memalign(&pointer, alignment, size);
 	return pointer;
 }
 
