065e3077d0cd93b48c587428aba65641dd55bf8d
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2020-11-11 20:25:25 +0200

    Add test_multi_level_nested

--- python/ray/tune/utils/util_test.py
+++ python/ray/tune/utils/util_test.py
@@ -8,6 +8,27 @@ class UnflattenDictTest(unittest.TestCase):
         result = unflatten_dict({'a/b': 1, 'c/d': 2, 'e': 3})
         assert result == {'a': {'b': 1}, 'c': {'d': 2}, 'e': 3}
 
+    def test_multi_level_nested(self):
+        result = unflatten_dict({'a/b/c/d': 1, 'a/b/c': 2, 'c/d': 3, 'e': 4})
+        assert result == {
+            'a': {
+                'b': {
+                    'c': {
+                        'd': 1,
+                    },
+                },
+            },
+            'a': {
+                'b': {
+                    'c': 2,
+                },
+            },
+            'c': {
+                'd': 3,
+            },
+            'e': 4,
+        }
+
 
 if __name__ == "__main__":
     import pytest
