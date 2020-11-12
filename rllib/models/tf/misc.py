59ccbc0fc7b5dcbaf25c85e83da165bcff92e06c
Michael Luo <michael.luo123456789@gmail.com>
2020-11-12 03:18:50 -0800

    [RLlib] Model Annotations: Tensorflow (#11964)

--- rllib/models/tf/misc.py
+++ rllib/models/tf/misc.py
@@ -1,10 +1,13 @@
 import numpy as np
+from typing import Tuple, Any, Optional
+
 from ray.rllib.utils.framework import try_import_tf
+from ray.rllib.utils.typing import TensorType
 
 tf1, tf, tfv = try_import_tf()
 
 
-def normc_initializer(std=1.0):
+def normc_initializer(std: float = 1.0) -> Any:
     def _initializer(shape, dtype=None, partition_info=None):
         out = np.random.randn(*shape).astype(np.float32)
         out *= std / np.sqrt(np.square(out).sum(axis=0, keepdims=True))
@@ -13,14 +16,14 @@ def normc_initializer(std=1.0):
     return _initializer
 
 
-def conv2d(x,
-           num_filters,
-           name,
-           filter_size=(3, 3),
-           stride=(1, 1),
-           pad="SAME",
-           dtype=None,
-           collections=None):
+def conv2d(x: TensorType,
+           num_filters: int,
+           name: str,
+           filter_size: Tuple[int, int] = (3, 3),
+           stride: Tuple[int, int] = (1, 1),
+           pad: str = "SAME",
+           dtype: Optional[Any] = None,
+           collections: Optional[Any] = None) -> TensorType:
     if dtype is None:
         dtype = tf.float32
 
@@ -53,7 +56,11 @@ def conv2d(x,
         return tf1.nn.conv2d(x, w, stride_shape, pad) + b
 
 
-def linear(x, size, name, initializer=None, bias_init=0):
+def linear(x: TensorType,
+           size: int,
+           name: str,
+           initializer: Optional[Any] = None,
+           bias_init: float = 0.0) -> TensorType:
     w = tf1.get_variable(
         name + "/w", [x.get_shape()[1], size], initializer=initializer)
     b = tf1.get_variable(
@@ -61,5 +68,5 @@ def linear(x, size, name, initializer=None, bias_init=0):
     return tf.matmul(x, w) + b
 
 
-def flatten(x):
+def flatten(x: TensorType) -> TensorType:
     return tf.reshape(x, [-1, np.prod(x.get_shape().as_list()[1:])])
