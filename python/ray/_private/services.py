33524b4f8d3ab5f203b90e0a490271841fd8511a
architkulkarni <arkulkar@gmail.com>
2020-11-23 11:01:00 -0800

    revert local maxclients fix

--- python/ray/_private/services.py
+++ python/ray/_private/services.py
@@ -985,7 +985,8 @@ def _start_redis_instance(executable,
         # 10000 - redis_client_buffer.
         redis_client_buffer = 32
         if current_max_clients < ulimit_n - redis_client_buffer:
-            redis_client.config_set("maxclients", 10000)
+            redis_client.config_set("maxclients",
+                                    ulimit_n - redis_client_buffer)
 
     # Increase the hard and soft limits for the redis client pubsub buffer to
     # 128MB. This is a hack to make it less likely for pubsub messages to be
