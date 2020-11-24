1ae4d2873af5c1815e702fbb2b8bc2c6ad29ec0e
ZhuSenlin <wumuzi520@126.com>
2020-11-24 21:11:18 +0800

    [GCS] refactor gcs initialization (#11890)

--- src/ray/gcs/gcs_server/gcs_node_manager.h
+++ src/ray/gcs/gcs_server/gcs_node_manager.h
@@ -18,6 +18,7 @@
 #include "absl/container/flat_hash_set.h"
 #include "ray/common/id.h"
 #include "ray/gcs/accessor.h"
+#include "ray/gcs/gcs_server/gcs_init_data.h"
 #include "ray/gcs/gcs_server/gcs_table_storage.h"
 #include "ray/gcs/pubsub/gcs_pub_sub.h"
 #include "ray/rpc/client_call.h"
@@ -152,11 +153,11 @@ class GcsNodeManager : public rpc::NodeInfoHandler {
     node_added_listeners_.emplace_back(std::move(listener));
   }
 
-  /// Load initial data from gcs storage to memory cache asynchronously.
+  /// Initialize with the gcs tables data synchronously.
   /// This should be called when GCS server restarts after a failure.
   ///
-  /// \param done Callback that will be called when load is complete.
-  void LoadInitialData(const EmptyCallback &done);
+  /// \param gcs_init_data.
+  void Initialize(const GcsInitData &gcs_init_data);
 
   /// Start node failure detector.
   void StartNodeFailureDetector();
