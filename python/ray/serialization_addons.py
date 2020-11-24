19098cc757288e79daebcbed8bfc9a268bcd2c89
Siyuan <suquark@gmail.com>
2020-11-24 11:42:22 -0800

    lint

--- python/ray/serialization_addons.py
+++ python/ray/serialization_addons.py
@@ -10,7 +10,6 @@ try:
 
     _TORCH_WARNING_FILTER_ACTIVATE = True
 
-
     class _TorchTensorReducingHelper:
         def __init__(self, tensor):
             self.tensor = tensor
@@ -23,8 +22,10 @@ try:
             # we would only deal with it for the first time.
             if _TORCH_WARNING_FILTER_ACTIVATE:
                 with warnings.catch_warnings():
-                    warnings.filterwarnings("ignore", category=UserWarning,
-                                            message="The given NumPy array is not writeable")
+                    warnings.filterwarnings(
+                        "ignore",
+                        category=UserWarning,
+                        message="The given NumPy array is not writeable")
                     storage = torch.from_numpy(ndarray).storage()
                 _TORCH_WARNING_FILTER_ACTIVATE = False
             else:
@@ -36,19 +37,19 @@ try:
             if self.tensor.is_sparse:
                 # Torch will help us reduce the sparse tensor into
                 # several continuous tensors.
-                return _TorchTensorReducingHelper, (self.tensor,)
+                return _TorchTensorReducingHelper, (self.tensor, )
             # By only replacing the storage with a numpy array, we can reuse
-            # zero-copy serialization while keeping all other params of the torch tensor.
+            # zero-copy serialization while keeping all other params of the
+            # torch tensor.
             _rebuild_func, content = self.tensor.__reduce_ex__(protocol)
-            return self.rebuild_tensor, (_rebuild_func, self.tensor.numpy(), content[1:])
-
+            return self.rebuild_tensor, (_rebuild_func, self.tensor.numpy(),
+                                         content[1:])
 
     def _unwrap_tensor(s):
         return s.tensor
 
-
     def torch_tensor_reducer(tensor):
-        return _unwrap_tensor, (_TorchTensorReducingHelper(tensor),)
+        return _unwrap_tensor, (_TorchTensorReducingHelper(tensor), )
 
 except ImportError:
     pass
