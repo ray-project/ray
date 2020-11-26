daa51e2fbc9e1e5efbfddf28dfe6bba7019507ca
Richard Liaw <rliaw@berkeley.edu>
2020-11-25 17:07:17 -0800

    add-file
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>

--- python/ray/util/xgboost/__init__.py
+++ python/ray/util/xgboost/__init__.py
@@ -7,9 +7,9 @@ predict = None
 RayDMatrix = None
 
 try:
-    from xgboost_ray import train, predict, RayDMatrix
+    from xgboost_ray import train, predict, RayDMatrix, RayFileType
 except ImportError:
     logger.info("xgboost_ray is not installed. Please run "
                 "`pip install git+https://github.com/ray-project/xgboost_ray`.")
 
-__all__ = ["train", "predict", "RayDMatrix"]
+__all__ = ["train", "predict", "RayDMatrix", "RayFileType"]
