663e9a7a6aed0c61d8852ea48f234d9246c1263d
Amog Kamsetty <amogkamsetty@yahoo.com>
2020-11-26 11:07:18 -0800

    update example

--- python/ray/util/xgboost/simple_tune.py
+++ python/ray/util/xgboost/simple_tune.py
@@ -29,38 +29,42 @@ def train_model(config):
     bst.save_model("model.xgb")
 # __train_end__
 
-# __tune_begin__
-from ray import tune
+def main():
+    # __tune_begin__
+    from ray import tune
 
-# Set config
-config = {
-    "tree_method": "approx",
-    "objective": "binary:logistic",
-    "eval_metric": ["logloss", "error"],
-    "eta": tune.loguniform(1e-4, 1e-1),
-    "subsample": tune.uniform(0.5, 1.0),
-    "max_depth": tune.randint(1, 9)
-}
-# __tune_end__
+    # Set config
+    config = {
+        "tree_method": "approx",
+        "objective": "binary:logistic",
+        "eval_metric": ["logloss", "error"],
+        "eta": tune.loguniform(1e-4, 1e-1),
+        "subsample": tune.uniform(0.5, 1.0),
+        "max_depth": tune.randint(1, 9)
+    }
+    # __tune_end__
 
-# __tune_run_begin__
-analysis = tune.run(
-    train_model,
-    config=config,
-    metric="eval-error",
-    mode="min",
-    num_samples=4,
-    resources_per_trial={"cpu": 1, "extra_cpu": num_actors * num_cpus_per_actor})
+    # __tune_run_begin__
+    analysis = tune.run(
+        train_model,
+        config=config,
+        metric="eval-error",
+        mode="min",
+        num_samples=4,
+        resources_per_trial={"cpu": 1, "extra_cpu": num_actors * num_cpus_per_actor})
 
-# Load the best model checkpoint
-import xgboost as xgb
-import os
+    # Load the best model checkpoint
+    import xgboost as xgb
+    import os
 
-# Load in the best performing model.
-best_bst = xgb.Booster()
-best_bst.load_model(os.path.join(analysis.best_logdir, "simple.xgb"))
+    # Load in the best performing model.
+    best_bst = xgb.Booster()
+    best_bst.load_model(os.path.join(analysis.best_logdir, "simple.xgb"))
 
-accuracy = 1. - analysis.best_result["eval-error"]
-print(f"Best model parameters: {analysis.best_config}")
-print(f"Best model total accuracy: {accuracy:.4f}")
-# __tune_run_end__
+    accuracy = 1. - analysis.best_result["eval-error"]
+    print(f"Best model parameters: {analysis.best_config}")
+    print(f"Best model total accuracy: {accuracy:.4f}")
+    # __tune_run_end__
+
+if __name__ == "__main__":
+    main()
