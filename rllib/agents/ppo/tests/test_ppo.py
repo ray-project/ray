f2227d3cc9c1ad5527fdac121f57e50d51de487b
sven1977 <svenmika1977@gmail.com>
2020-11-26 16:04:48 +0100

    WIP.

--- rllib/agents/ppo/tests/test_ppo.py
+++ rllib/agents/ppo/tests/test_ppo.py
@@ -64,7 +64,7 @@ class MyCallbacks(DefaultCallbacks):
 class TestPPO(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        ray.init(local_mode=True)#TODO
+        ray.init()
 
     @classmethod
     def tearDownClass(cls):
