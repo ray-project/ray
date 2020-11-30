d302a518baf6f24ea1402f65245d927f63eacf92
灵洵 <fengbin.ffb@antgroup.com>
2020-12-01 00:04:52 +0800

    add part code

--- java/runtime/src/main/java/io/ray/runtime/RayDevRuntime.java
+++ java/runtime/src/main/java/io/ray/runtime/RayDevRuntime.java
@@ -102,6 +102,13 @@ public class RayDevRuntime extends AbstractRayRuntime {
   }
 
   @Override
+  public boolean waitPlacementGroupReady(PlacementGroupId id, int timeoutMs) {
+    // TODO(ffbin): We need a LocalGcsClient before implements this.
+    throw new UnsupportedOperationException(
+            "Ray doesn't support placement group operations in local mode.");
+  }
+
+  @Override
   public void exitActor() {
 
   }
