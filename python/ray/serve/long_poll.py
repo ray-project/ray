f345716a887f8f2bbdbbdf9a5c83c0e0bf7f4c9e
simon-mo <simon.mo@hey.com>
2020-11-30 17:30:23 -0800

    LINT

--- python/ray/serve/long_poll.py
+++ python/ray/serve/long_poll.py
@@ -68,9 +68,10 @@ class LongPollerAsyncClient:
                 self._update(updates)
                 logger.debug(f"LongPollerClient received udpates: {updates}")
                 for key, updated_object in updates.items():
-                    # NOTE(simon): This blocks the loop from doing another poll.
-                    # Consider use loop.create_task here or poll first then call
-                    # the callbacks.
+                    # NOTE(simon):
+                    # This blocks the loop from doing another poll. Consider
+                    # use loop.create_task here or poll first then call the
+                    # callbacks.
                     callback = self.key_listeners[key]
                     await callback(updated_object.object_snapshot)
             except ray.exceptions.RayActorError:
