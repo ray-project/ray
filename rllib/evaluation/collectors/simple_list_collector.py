01deb2199faf5347dd1490c1097459e2f14ab9bf
sven1977 <svenmika1977@gmail.com>
2020-12-02 19:08:31 +0100

    Fix.

--- rllib/evaluation/collectors/simple_list_collector.py
+++ rllib/evaluation/collectors/simple_list_collector.py
@@ -292,8 +292,10 @@ class _PolicyCollector:
         """
         for view_col, data in batch.items():
             # TODO(ekl) how do we handle this for policies that don't extend
-            # Torch / TF Policy template (no inference of view reqs)?
+            #   Torch/TFPolicy template (no inference of view reqs)?
             # Skip columns that are not used for training.
+            if view_requirements[view_col].is_input_dict:
+                continue
             # if view_col not in view_requirements or \
             #         not view_requirements[view_col].used_for_training:
             #     continue
