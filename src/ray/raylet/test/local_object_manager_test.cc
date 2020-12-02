443339ab195af383c886fc352bd70db50f481127
Stephanie Wang <swang@cs.berkeley.edu>
2020-12-02 13:25:54 -0500

    [core] Move out-of-memory handling into the plasma store and support async object creation (#12186)
    * Refactor to extract creation request queue
    
    * timer on oom
    
    * move timer out
    
    * Move evict_if_full and on_store_full into plasma store
    
    * Remove client-side code
    
    * revert
    
    * Distinguish between transient and permanent OOM delays
    
    * update
    
    * Move out create request queue, unit test
    
    * unit test
    
    * Fix max retries
    
    * test
    
    * Do not pin restored objects
    
    * First pass to add polling requests, unit test passes
    
    * worker plasma client retries plasma requests
    
    * cleanup
    
    * Clean up after disconnected clients, check memory leaks
    
    * Support immediate requests in request queue
    
    * Option to try creating immediately
    
    * lint
    
    * Fix build, address comments
    
    * doc
    
    * fixes
    
    * debug travis
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * Revert "debug"
    
    This reverts commit 6bf2f6ee5640e71630c4aecdb7ebf54911ea32db.
    
    Revert "debug"
    
    This reverts commit 73017099c9b06cdaae1217bf0e0f4d23ed68a9e5.
    
    Revert "debug"
    
    This reverts commit 5a155529e28cee9461a598b0cdf7b6a3cc194c93.
    
    Revert "debug"
    
    This reverts commit b50c2101afd45d4cf663daae857bfe1b40387703.
    
    Revert "debug travis"
    
    This reverts commit 012b8721dedf9bca46294ae75eee2815b160368b.
    
    * Skip if new scheduler enabled
    
    * error message
    
    * merge

--- src/ray/raylet/test/local_object_manager_test.cc
+++ src/ray/raylet/test/local_object_manager_test.cc
@@ -240,8 +240,7 @@ class LocalObjectManagerTest : public ::testing::Test {
                   for (const auto &object_id : object_ids) {
                     freed.insert(object_id);
                   }
-                },
-                [&]() { num_callbacks_fired++; }),
+                }),
         unpins(std::make_shared<std::unordered_map<ObjectID, int>>()) {
     RayConfig::instance().initialize({{"object_spilling_config", "mock_config"}});
   }
@@ -263,7 +262,6 @@ class LocalObjectManagerTest : public ::testing::Test {
   // This hashmap is incremented when objects are unpinned by destroying their
   // unique_ptr.
   std::shared_ptr<std::unordered_map<ObjectID, int>> unpins;
-  size_t num_callbacks_fired = 0;
 };
 
 TEST_F(LocalObjectManagerTest, TestPin) {
@@ -292,7 +290,6 @@ TEST_F(LocalObjectManagerTest, TestPin) {
   }
   std::unordered_set<ObjectID> expected(object_ids.begin(), object_ids.end());
   ASSERT_EQ(freed, expected);
-  ASSERT_EQ(num_callbacks_fired, 0);
 }
 
 TEST_F(LocalObjectManagerTest, TestRestoreSpilledObject) {
@@ -305,7 +302,6 @@ TEST_F(LocalObjectManagerTest, TestRestoreSpilledObject) {
     num_times_fired++;
   });
   ASSERT_EQ(num_times_fired, 1);
-  ASSERT_EQ(num_callbacks_fired, 0);
 }
 
 TEST_F(LocalObjectManagerTest, TestExplicitSpill) {
@@ -348,7 +344,6 @@ TEST_F(LocalObjectManagerTest, TestExplicitSpill) {
   for (const auto &id : object_ids) {
     ASSERT_EQ((*unpins)[id], 1);
   }
-  ASSERT_TRUE(num_callbacks_fired > 0);
 }
 
 TEST_F(LocalObjectManagerTest, TestDuplicateSpill) {
@@ -400,7 +395,6 @@ TEST_F(LocalObjectManagerTest, TestDuplicateSpill) {
   for (const auto &id : object_ids) {
     ASSERT_EQ((*unpins)[id], 1);
   }
-  ASSERT_TRUE(num_callbacks_fired > 0);
 }
 
 TEST_F(LocalObjectManagerTest, TestSpillObjectsOfSize) {
@@ -456,7 +450,6 @@ TEST_F(LocalObjectManagerTest, TestSpillObjectsOfSize) {
   // Check that this returns the total number of bytes currently being spilled.
   num_bytes_required = manager.SpillObjectsOfSize(0, 0);
   ASSERT_EQ(num_bytes_required, 0);
-  ASSERT_TRUE(num_callbacks_fired > 0);
 }
 
 TEST_F(LocalObjectManagerTest, TestSpillError) {
@@ -500,7 +493,6 @@ TEST_F(LocalObjectManagerTest, TestSpillError) {
   ASSERT_EQ(num_times_fired, 2);
   ASSERT_EQ(object_table.object_urls[object_id], url);
   ASSERT_EQ((*unpins)[object_id], 1);
-  ASSERT_TRUE(num_callbacks_fired > 0);
 }
 
 TEST_F(LocalObjectManagerTest, TestDeleteNoSpilledObjects) {
