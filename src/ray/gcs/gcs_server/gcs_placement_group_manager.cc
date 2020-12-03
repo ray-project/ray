435a940f421ecb0b7a155a4b798efa02d95f7a86
灵洵 <fengbin.ffb@antgroup.com>
2020-12-03 16:22:47 +0800

    fix review comments

--- src/ray/gcs/gcs_server/gcs_placement_group_manager.cc
+++ src/ray/gcs/gcs_server/gcs_placement_group_manager.cc
@@ -414,7 +414,7 @@ void GcsPlacementGroupManager::HandleWaitPlacementGroupUntilReady(
   const auto &iter = registered_placement_groups_.find(placement_group_id);
   if (iter == registered_placement_groups_.end()) {
     GCS_RPC_SEND_REPLY(send_reply_callback, reply,
-                       Status::Invalid("Placement group is not exist."));
+                       Status::NotFound("Placement group is not exist."));
   } else if (iter->second->GetState() == rpc::PlacementGroupTableData::CREATED) {
     GCS_RPC_SEND_REPLY(send_reply_callback, reply, Status::OK());
   }
