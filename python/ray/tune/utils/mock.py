6d9567d0313d4d4d3dc94de8d79cbb13473760af
Richard Liaw <rliaw@berkeley.edu>
2020-12-03 13:32:07 -0800

    lint
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>

--- python/ray/tune/utils/mock.py
+++ python/ray/tune/utils/mock.py
@@ -22,8 +22,8 @@ LOCAL_DELETE_TEMPLATE = "rm -rf {target}"
 def mock_storage_client():
     """Mocks storage client that treats a local dir as durable storage."""
     client = get_sync_client(LOCAL_SYNC_TEMPLATE, LOCAL_DELETE_TEMPLATE)
-    path = os.path.join(
-        ray.utils.get_user_temp_dir(), f"mock-client-{uuid.uuid4().hex[:4]}")
+    path = os.path.join(ray.utils.get_user_temp_dir(),
+                        f"mock-client-{uuid.uuid4().hex[:4]}")
     os.makedirs(path, exist_ok=True)
     client.set_logdir(path)
     return client
