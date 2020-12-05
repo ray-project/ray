4136fa97e2c8d05c62108294417a4b3eb20e2bde
Hao Zhang <hao.zhang@petuum.com>
2020-12-05 03:42:52 -0500

    linting 6

--- python/ray/util/collective/tests/ray_start_distributed_script.sh
+++ python/ray/util/collective/tests/ray_start_distributed_script.sh
@@ -10,6 +10,6 @@ sleep 0.5
 NCCL_DEBUG=INFO NCCL_SOCKET_IFNAME=enp179s0f0 ray start --head --object-manager-port=8076 --resources='{"machine":1}' --object-store-memory=32359738368
 sleep 0.5
 
-echo "=> node $i"
+echo "=> remote node..."
 ssh -o StrictHostKeyChecking=no -i /home/hao.zhang/.ssh/arion.pem hao.zhang@10.20.41.120 "source $ROOT_DIR/env/bin/activate; ray stop; NCCL_DEBUG=INFO NCCL_SOCKET_IFNAME=enp179s0f0 ray start --address='$MY_IPADDR:6379' --object-manager-port=8076 --resources='{\"machine\":1}' --object-store-memory=32359738368";
 wait
