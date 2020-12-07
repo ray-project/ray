18487559a6338df200ffdac8b73f4aaa4bf5e9c3
Edward Oakes <ed.nmi.oakes@gmail.com>
2020-12-06 20:36:31 -0600

    lint

--- python/ray/experimental/beam/runner.py
+++ python/ray/experimental/beam/runner.py
@@ -35,218 +35,222 @@ def get_my_ip():
 
 @Environment.register_urn("ray_worker_env", bytes)
 class RayWorkerEnvironment(Environment):
-  def __init__(
-      self,
-      command_string,  # type: str
-      capabilities=(),  # type: Iterable[str]
-      artifacts=(),  # type: Iterable[beam_runner_api_pb2.ArtifactInformation]
-  ):
-    super(RayWorkerEnvironment, self).__init__(capabilities, artifacts)
-    self.command_string = command_string
+    def __init__(
+            self,
+            command_string,  # type: str
+            capabilities=(),  # type: Iterable[str]
+            artifacts=(
+            ),  # type: Iterable[beam_runner_api_pb2.ArtifactInformation]
+    ):
+        super(RayWorkerEnvironment, self).__init__(capabilities, artifacts)
+        self.command_string = command_string
 
-  def __eq__(self, other):
-    return self.__class__ == other.__class__ \
-           and self.command_string == other.command_string
+    def __eq__(self, other):
+        return self.__class__ == other.__class__ \
+               and self.command_string == other.command_string
 
-  def __ne__(self, other):
-    # TODO(BEAM-5949): Needed for Python 2 compatibility.
-    return not self == other
+    def __ne__(self, other):
+        # TODO(BEAM-5949): Needed for Python 2 compatibility.
+        return not self == other
 
-  def __hash__(self):
-    # type: () -> int
-    return hash((self.__class__, self.command_string))
+    def __hash__(self):
+        # type: () -> int
+        return hash((self.__class__, self.command_string))
 
-  def __repr__(self):
-    # type: () -> str
-    return 'RayWorkerEnvironment(command_string=%s)' % self.command_string
+    def __repr__(self):
+        # type: () -> str
+        return 'RayWorkerEnvironment(command_string=%s)' % self.command_string
 
-  def to_runner_api_parameter(self, context):
-    # type: (PipelineContext) -> Tuple[str, bytes]
-    return "ray_worker_env", self.command_string.encode('utf-8')
+    def to_runner_api_parameter(self, context):
+        # type: (PipelineContext) -> Tuple[str, bytes]
+        return "ray_worker_env", self.command_string.encode('utf-8')
 
-  @staticmethod
-  def from_runner_api_parameter(payload,  # type: bytes
-                                capabilities,  # type: Iterable[str]
-                                artifacts,  # type: Iterable[beam_runner_api_pb2.ArtifactInformation]
-                                context  # type: PipelineContext
-                               ):
-    # type: (...) -> SubprocessSDKEnvironment
-    return RayWorkerEnvironment(
-        payload.decode('utf-8'), capabilities, artifacts)
+    @staticmethod
+    def from_runner_api_parameter(
+            payload,  # type: bytes
+            capabilities,  # type: Iterable[str]
+            artifacts,  # type: Iterable[beam_runner_api_pb2.ArtifactInformation]
+            context  # type: PipelineContext
+    ):
+        # type: (...) -> SubprocessSDKEnvironment
+        return RayWorkerEnvironment(
+            payload.decode('utf-8'), capabilities, artifacts)
 
-  @classmethod
-  def from_options(cls, options):
-    # type: (PortableOptions) -> SubprocessSDKEnvironment
-    return cls(
-        options.environment_config,
-        capabilities=python_sdk_capabilities(),
-        artifacts=python_sdk_dependencies(options))
+    @classmethod
+    def from_options(cls, options):
+        # type: (PortableOptions) -> SubprocessSDKEnvironment
+        return cls(
+            options.environment_config,
+            capabilities=python_sdk_capabilities(),
+            artifacts=python_sdk_dependencies(options))
 
 
 @ray.remote
 class BeamWorker(object):
-  def __init__(self):
-    print("Launching BEAM worker")
-    from apache_beam.runners.worker.sdk_worker_main import main
-    main([])
+    def __init__(self):
+        print("Launching BEAM worker")
+        from apache_beam.runners.worker.sdk_worker_main import main
+        main([])
 
-  def await_termination(self):
-    return "exited"
-
+    def await_termination(self):
+        return "exited"
 
 
 from apache_beam.portability.api import beam_fn_api_pb2_grpc
 from apache_beam.portability.api import endpoints_pb2
 
+
 class BeamFnLoggingServicer(beam_fn_api_pb2_grpc.BeamFnLoggingServicer):
-  def Logging(self, log_bundles, context=None):
-    for log_bundle in log_bundles:
-      for log_entry in log_bundle.log_entries:
-        _LOGGER.info('Worker: %s', str(log_entry).replace('\n', ' '))
-    return iter([])
+    def Logging(self, log_bundles, context=None):
+        for log_bundle in log_bundles:
+            for log_entry in log_bundle.log_entries:
+                _LOGGER.info('Worker: %s', str(log_entry).replace('\n', ' '))
+        return iter([])
 
 
 class RaySDKWorker(object):
-  """Manages a SDK worker implemented as a Ray actor."""
+    """Manages a SDK worker implemented as a Ray actor."""
 
-  def __init__(
-      self,
-      control_address,
-      worker_id=None):
+    def __init__(self, control_address, worker_id=None):
 
-    self._control_address = control_address.replace("localhost", get_my_ip())
-    self._worker_id = worker_id
+        self._control_address = control_address.replace(
+            "localhost", get_my_ip())
+        self._worker_id = worker_id
 
-  def run(self):
-    logging_server = grpc.server(
-        thread_pool_executor.shared_unbounded_instance())
-    logging_port = logging_server.add_insecure_port('[::]:0')
-    logging_server.start()
-    logging_servicer = BeamFnLoggingServicer()
-    beam_fn_api_pb2_grpc.add_BeamFnLoggingServicer_to_server(
-        logging_servicer, logging_server)
-    logging_descriptor = text_format.MessageToString(
-        endpoints_pb2.ApiServiceDescriptor(url='%s:%s' % (get_my_ip(), logging_port)))
+    def run(self):
+        logging_server = grpc.server(
+            thread_pool_executor.shared_unbounded_instance())
+        logging_port = logging_server.add_insecure_port('[::]:0')
+        logging_server.start()
+        logging_servicer = BeamFnLoggingServicer()
+        beam_fn_api_pb2_grpc.add_BeamFnLoggingServicer_to_server(
+            logging_servicer, logging_server)
+        logging_descriptor = text_format.MessageToString(
+            endpoints_pb2.ApiServiceDescriptor(url='%s:%s' % (get_my_ip(),
+                                                              logging_port)))
 
-    control_descriptor = text_format.MessageToString(
-        endpoints_pb2.ApiServiceDescriptor(url=self._control_address))
+        control_descriptor = text_format.MessageToString(
+            endpoints_pb2.ApiServiceDescriptor(url=self._control_address))
 
-    env_dict = dict(
-        os.environ,
-        CONTROL_API_SERVICE_DESCRIPTOR=control_descriptor,
-        LOGGING_API_SERVICE_DESCRIPTOR=logging_descriptor)
-    # only add worker_id when it is set.
-    if self._worker_id:
-      env_dict['WORKER_ID'] = self._worker_id
+        env_dict = dict(
+            os.environ,
+            CONTROL_API_SERVICE_DESCRIPTOR=control_descriptor,
+            LOGGING_API_SERVICE_DESCRIPTOR=logging_descriptor)
+        # only add worker_id when it is set.
+        if self._worker_id:
+            env_dict['WORKER_ID'] = self._worker_id
 
-    actor = BeamWorker.options(override_environment_variables=env_dict).remote()
-    try:
-      ray.get(actor.await_termination.remote())
-    finally:
-      logging_server.stop(0)
+        actor = BeamWorker.options(
+            override_environment_variables=env_dict).remote()
+        try:
+            ray.get(actor.await_termination.remote())
+        finally:
+            logging_server.stop(0)
 
 
 @WorkerHandler.register_environment("ray_worker_env", bytes)
 class RayWorkerHandler(GrpcWorkerHandler):
-  def __init__(self,
-               worker_command_line,  # type: bytes
-               state,  # type: StateServicer
-               provision_info,  # type: ExtendedProvisionInfo
-               grpc_server  # type: GrpcServer
-              ):
-    # type: (...) -> None
-    super(RayWorkerHandler,
-          self).__init__(state, provision_info, grpc_server)
-    self._worker_command_line = worker_command_line
+    def __init__(
+            self,
+            worker_command_line,  # type: bytes
+            state,  # type: StateServicer
+            provision_info,  # type: ExtendedProvisionInfo
+            grpc_server  # type: GrpcServer
+    ):
+        # type: (...) -> None
+        super(RayWorkerHandler, self).__init__(state, provision_info,
+                                               grpc_server)
+        self._worker_command_line = worker_command_line
 
-  def start_worker(self):
+    def start_worker(self):
 
-    self.worker = RaySDKWorker(self.control_address, self.worker_id)
-    self.worker_thread = threading.Thread(
-        name='run_worker', target=self.worker.run)
-    self.worker_thread.start()
-    print("Ray BEAM worker started")
+        self.worker = RaySDKWorker(self.control_address, self.worker_id)
+        self.worker_thread = threading.Thread(
+            name='run_worker', target=self.worker.run)
+        self.worker_thread.start()
+        print("Ray BEAM worker started")
 
-  def stop_worker(self):
-    print("Stop worker")
-    self.worker_thread.join()
+    def stop_worker(self):
+        print("Stop worker")
+        self.worker_thread.join()
 
-  def host_from_worker(self):
-    return get_my_ip()
+    def host_from_worker(self):
+        return get_my_ip()
+
 
 class MyParallelBundleManager(BundleManager):
+    def __init__(
+            self,
+            bundle_context_manager,  # type: execution.BundleContextManager
+            progress_frequency=None,  # type: Optional[float]
+            cache_token_generator=None,
+            **kwargs):
+        # type: (...) -> None
+        super(MyParallelBundleManager, self).__init__(
+            bundle_context_manager,
+            progress_frequency,
+            cache_token_generator=cache_token_generator)
+        self._num_workers = bundle_context_manager.num_workers
 
-  def __init__(
-      self,
-      bundle_context_manager,  # type: execution.BundleContextManager
-      progress_frequency=None,  # type: Optional[float]
-      cache_token_generator=None,
-      **kwargs):
-    # type: (...) -> None
-    super(MyParallelBundleManager, self).__init__(
-        bundle_context_manager,
-        progress_frequency,
-        cache_token_generator=cache_token_generator)
-    self._num_workers = bundle_context_manager.num_workers
+    def process_bundle(
+            self,
+            inputs,  # type: Mapping[str, execution.PartitionableBuffer]
+            expected_outputs,  # type: DataOutput
+            fired_timers,  # type: Mapping[Tuple[str, str], execution.PartitionableBuffer]
+            expected_output_timers,  # type: OutputTimers
+            dry_run=False,  # type: bool
+    ):
+        # type: (...) -> BundleProcessResult
+        part_inputs = [{} for _ in range(self._num_workers)
+                       ]  # type: List[Dict[str, List[bytes]]]
+        # Timers are only executed on the first worker
+        # TODO(BEAM-9741): Split timers to multiple workers
+        timer_inputs = [
+            fired_timers if i == 0 else {} for i in range(self._num_workers)
+        ]
+        for name, input in inputs.items():
+            for ix, part in enumerate(input.partition(self._num_workers)):
+                part_inputs[ix][name] = part
 
-  def process_bundle(self,
-                     inputs,  # type: Mapping[str, execution.PartitionableBuffer]
-                     expected_outputs,  # type: DataOutput
-                     fired_timers,  # type: Mapping[Tuple[str, str], execution.PartitionableBuffer]
-                     expected_output_timers,  # type: OutputTimers
-                     dry_run=False,  # type: bool
-                    ):
-    # type: (...) -> BundleProcessResult
-    part_inputs = [{} for _ in range(self._num_workers)
-                   ]  # type: List[Dict[str, List[bytes]]]
-    # Timers are only executed on the first worker
-    # TODO(BEAM-9741): Split timers to multiple workers
-    timer_inputs = [
-        fired_timers if i == 0 else {} for i in range(self._num_workers)
-    ]
-    for name, input in inputs.items():
-      for ix, part in enumerate(input.partition(self._num_workers)):
-        part_inputs[ix][name] = part
+        merged_result = None  # type: Optional[beam_fn_api_pb2.InstructionResponse]
+        split_result_list = [
+        ]  # type: List[beam_fn_api_pb2.ProcessBundleSplitResponse]
 
-    merged_result = None  # type: Optional[beam_fn_api_pb2.InstructionResponse]
-    split_result_list = [
-    ]  # type: List[beam_fn_api_pb2.ProcessBundleSplitResponse]
+        def execute(part_map_input_timers):
+            # type: (...) -> BundleProcessResult
+            part_map, input_timers = part_map_input_timers
+            bundle_manager = BundleManager(
+                self.bundle_context_manager,
+                self._progress_frequency,
+                cache_token_generator=self._cache_token_generator)
+            return bundle_manager.process_bundle(
+                part_map, expected_outputs, input_timers,
+                expected_output_timers, dry_run)
 
-    def execute(part_map_input_timers):
-      # type: (...) -> BundleProcessResult
-      part_map, input_timers = part_map_input_timers
-      bundle_manager = BundleManager(
-          self.bundle_context_manager,
-          self._progress_frequency,
-          cache_token_generator=self._cache_token_generator)
-      return bundle_manager.process_bundle(
-          part_map,
-          expected_outputs,
-          input_timers,
-          expected_output_timers,
-          dry_run)
+        print("=== PART INPUTS ({}) ===".format(len(part_inputs)))
+        print(part_inputs)
+        print("===================")
+        with thread_pool_executor.shared_unbounded_instance() as executor:
+            for result, split_result in executor.map(
+                    execute, zip(part_inputs, timer_inputs)):
+                #print("===== result below =====:")
+                #print(result)
+                #print("===== result above =====:")
+                split_result_list += split_result
+                if merged_result is None:
+                    merged_result = result
+                else:
+                    merged_result = beam_fn_api_pb2.InstructionResponse(
+                        process_bundle=beam_fn_api_pb2.ProcessBundleResponse(
+                            monitoring_infos=monitoring_infos.consolidate(
+                                itertools.chain(
+                                    result.process_bundle.monitoring_infos,
+                                    merged_result.process_bundle.
+                                    monitoring_infos))),
+                        error=result.error or merged_result.error)
+        assert merged_result is not None
+        return merged_result, split_result_list
 
-    print("=== PART INPUTS ({}) ===".format(len(part_inputs)))
-    print(part_inputs)
-    print("===================")
-    with thread_pool_executor.shared_unbounded_instance() as executor:
-      for result, split_result in executor.map(execute, zip(part_inputs, timer_inputs)):
-        #print("===== result below =====:")
-        #print(result)
-        #print("===== result above =====:")
-        split_result_list += split_result
-        if merged_result is None:
-          merged_result = result
-        else:
-          merged_result = beam_fn_api_pb2.InstructionResponse(
-              process_bundle=beam_fn_api_pb2.ProcessBundleResponse(
-                  monitoring_infos=monitoring_infos.consolidate(
-                      itertools.chain(
-                          result.process_bundle.monitoring_infos,
-                          merged_result.process_bundle.monitoring_infos))),
-              error=result.error or merged_result.error)
-    assert merged_result is not None
-    return merged_result, split_result_list
 
 #import apache_beam.runners.portability.fn_api_runner.fn_runner #ParallelBundleManager
 #apache_beam.runners.portability.fn_api_runner.fn_runner.ParallelBundleManager = MyParallelBundleManager
--- python/ray/experimental/beam/stream.py
+++ python/ray/experimental/beam/stream.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 """A streaming word-counting workflow.
 """
 
@@ -38,75 +37,78 @@ from runner import RayWorkerEnvironment
 
 
 def run(argv=None, save_main_session=True):
-  """Build and run the pipeline."""
-  parser = argparse.ArgumentParser()
-  known_args, pipeline_args = parser.parse_known_args(argv)
+    """Build and run the pipeline."""
+    parser = argparse.ArgumentParser()
+    known_args, pipeline_args = parser.parse_known_args(argv)
 
-  # We use the save_main_session option because one or more DoFn's in this
-  # workflow rely on global context (e.g., a module imported at module level).
-  pipeline_options = PipelineOptions(pipeline_args)
-  pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
-  pipeline_options.view_as(StandardOptions).streaming = True
+    # We use the save_main_session option because one or more DoFn's in this
+    # workflow rely on global context (e.g., a module imported at module level).
+    pipeline_options = PipelineOptions(pipeline_args)
+    pipeline_options.view_as(
+        SetupOptions).save_main_session = save_main_session
+    pipeline_options.view_as(StandardOptions).streaming = True
 
-  from apache_beam.portability.api import beam_runner_api_pb2
-  from apache_beam.runners.portability.fn_api_runner import FnApiRunner
-  from apache_beam.portability import python_urns
-  import sys
+    from apache_beam.portability.api import beam_runner_api_pb2
+    from apache_beam.runners.portability.fn_api_runner import FnApiRunner
+    from apache_beam.portability import python_urns
+    import sys
 
-#  env = beam_runner_api_pb2.Environment(urn="ray_worker")
-#  print(env)
-#  runner = FnApiRunner(default_environment=env)
-  cmd = 'python -m apache_beam.runners.worker.sdk_worker_main'
+    #  env = beam_runner_api_pb2.Environment(urn="ray_worker")
+    #  print(env)
+    #  runner = FnApiRunner(default_environment=env)
+    cmd = 'python -m apache_beam.runners.worker.sdk_worker_main'
 
-  runner = FnApiRunner(default_environment=RayWorkerEnvironment(cmd))
+    runner = FnApiRunner(default_environment=RayWorkerEnvironment(cmd))
 
-  pipeline_options.view_as(
-      SetupOptions).save_main_session = save_main_session
+    pipeline_options.view_as(
+        SetupOptions).save_main_session = save_main_session
 
-  import ray
-  ray.init()
-  with beam.Pipeline(options=pipeline_options, runner=runner) as p:
+    import ray
+    ray.init()
+    with beam.Pipeline(options=pipeline_options, runner=runner) as p:
 
-    # Read from PubSub into a PCollection.
-    messages = (
-        p
-        #| beam.io.ReadFromPubSub(topic="projects/pubsub-public-data/topics/taxirides-realtime").with_output_types(bytes))
-        | beam.io.ReadFromPubSub(subscription="projects/test-anyscale-project-7b2ae2e3/subscriptions/apache-beam-test").with_output_types(bytes))
+        # Read from PubSub into a PCollection.
+        messages = (
+            p
+            #| beam.io.ReadFromPubSub(topic="projects/pubsub-public-data/topics/taxirides-realtime").with_output_types(bytes))
+            | beam.io.ReadFromPubSub(
+                subscription=
+                "projects/test-anyscale-project-7b2ae2e3/subscriptions/apache-beam-test"
+            ).with_output_types(bytes))
 
-    lines = messages | 'decode' >> beam.Map(lambda x: x.decode('utf-8'))
-    lines | beam.Map(print)
+        lines = messages | 'decode' >> beam.Map(lambda x: x.decode('utf-8'))
+        lines | beam.Map(print)
 
-    # Count the occurrences of each word.
-    def count_ones(word_ones):
-      (word, ones) = word_ones
-      return (word, sum(ones))
+        # Count the occurrences of each word.
+        def count_ones(word_ones):
+            (word, ones) = word_ones
+            return (word, sum(ones))
 
-    counts = (
-        lines
-        | 'split' >>
-        (beam.ParDo(WordExtractingDoFn()).with_output_types(unicode))
-        | 'pair_with_one' >> beam.Map(lambda x: (x, 1))
-        | beam.WindowInto(window.FixedWindows(15, 0))
-        | 'group' >> beam.GroupByKey()
-        | 'count' >> beam.Map(count_ones))
+        counts = (lines
+                  | 'split' >>
+                  (beam.ParDo(WordExtractingDoFn()).with_output_types(unicode))
+                  | 'pair_with_one' >> beam.Map(lambda x: (x, 1))
+                  | beam.WindowInto(window.FixedWindows(15, 0))
+                  | 'group' >> beam.GroupByKey()
+                  | 'count' >> beam.Map(count_ones))
 
-    # Format the counts into a PCollection of strings.
-    def format_result(word_count):
-      (word, count) = word_count
-      return '%s: %d' % (word, count)
+        # Format the counts into a PCollection of strings.
+        def format_result(word_count):
+            (word, count) = word_count
+            return '%s: %d' % (word, count)
 
-    output = (
-        counts
-        | 'format' >> beam.Map(format_result)
-        | 'encode' >>
-        beam.Map(lambda x: x.encode('utf-8')).with_output_types(bytes))
+        output = (
+            counts
+            | 'format' >> beam.Map(format_result)
+            | 'encode' >>
+            beam.Map(lambda x: x.encode('utf-8')).with_output_types(bytes))
 
-    # Write to PubSub.
-    # pylint: disable=expression-not-assigned
-    #output | beam.io.WriteToPubSub(known_args.output_topic)
-    #output | beam.Map(print)
+        # Write to PubSub.
+        # pylint: disable=expression-not-assigned
+        #output | beam.io.WriteToPubSub(known_args.output_topic)
+        #output | beam.Map(print)
 
 
 if __name__ == '__main__':
-  logging.getLogger().setLevel(logging.INFO)
-  run()
+    logging.getLogger().setLevel(logging.INFO)
+    run()
