a776209aec95b22d28d89a7f036af1a093b87752
Stephanie Wang <swang@cs.berkeley.edu>
2020-12-09 17:20:38 -0500

    Revert "Fix dashboard agent check ppid is raylet pid (#12256)" (#12729)
    This reverts commit 3ce92869778ecb20375785beeb6e85dda9ab7821.

--- dashboard/agent.py
+++ dashboard/agent.py
@@ -91,12 +91,9 @@ class DashboardAgent(object):
         async def _check_parent():
             """Check if raylet is dead."""
             curr_proc = psutil.Process()
-            ppid = int(os.environ["RAY_NODE_PID"])
-            logger.info("Parent pid is %s", ppid)
             while True:
                 parent = curr_proc.parent()
-                if parent is None or parent.pid == 1 or (ppid and
-                                                         ppid != parent.pid):
+                if parent is None or parent.pid == 1:
                     logger.error("raylet is dead, agent will die because "
                                  "it fate-shares with raylet.")
                     sys.exit(0)
