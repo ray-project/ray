ecd0277977efc1777b4d9cc065875d555e9d0639
sven1977 <svenmika1977@gmail.com>
2020-12-11 22:43:05 +0100

    LINT

--- rllib/models/preprocessors.py
+++ rllib/models/preprocessors.py
@@ -23,6 +23,7 @@ class Preprocessor:
     Attributes:
         shape (List[int]): Shape of the preprocessed output.
     """
+
     @PublicAPI
     def __init__(self, obs_space: gym.Space, options: dict = None):
         legacy_patch_shapes(obs_space)
@@ -92,6 +93,7 @@ class GenericPixelPreprocessor(Preprocessor):
     Note: for Atari games, use config {"preprocessor_pref": "deepmind"}
     instead for deepmind-style Atari preprocessing.
     """
+
     @override(Preprocessor)
     def _init_shape(self, obs_space: gym.Space, options: dict) -> List[int]:
         self._grayscale = options.get("grayscale")
@@ -169,8 +171,8 @@ class NoPreprocessor(Preprocessor):
     @override(Preprocessor)
     def write(self, observation: TensorType, array: np.ndarray,
               offset: int) -> None:
-        array[offset:offset + self._size] = np.array(observation,
-                                                     copy=False).ravel()
+        array[offset:offset + self._size] = np.array(
+            observation, copy=False).ravel()
 
     @property
     @override(Preprocessor)
@@ -183,6 +185,7 @@ class TupleFlatteningPreprocessor(Preprocessor):
 
     RLlib models will unpack the flattened output before _build_layers_v2().
     """
+
     @override(Preprocessor)
     def _init_shape(self, obs_space: gym.Space, options: dict) -> List[int]:
         assert isinstance(self._obs_space, gym.spaces.Tuple)
@@ -217,6 +220,7 @@ class DictFlatteningPreprocessor(Preprocessor):
 
     RLlib models will unpack the flattened output before _build_layers_v2().
     """
+
     @override(Preprocessor)
     def _init_shape(self, obs_space: gym.Space, options: dict) -> List[int]:
         assert isinstance(self._obs_space, gym.spaces.Dict)
@@ -250,6 +254,7 @@ class DictFlatteningPreprocessor(Preprocessor):
 
 class RepeatedValuesPreprocessor(Preprocessor):
     """Pads and batches the variable-length list value."""
+
     @override(Preprocessor)
     def _init_shape(self, obs_space: gym.Space, options: dict) -> List[int]:
         assert isinstance(self._obs_space, Repeated)
