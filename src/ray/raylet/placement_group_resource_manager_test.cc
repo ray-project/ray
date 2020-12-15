8babdb96032401812cac829133168702f3057207
loushang.ls <loushang.ls@antfin.com>
2020-12-16 00:07:35 +0800

    last two bundle ut waiting.

--- src/ray/raylet/placement_group_resource_manager_test.cc
+++ src/ray/raylet/placement_group_resource_manager_test.cc
@@ -352,11 +352,110 @@ TEST_F(NewPlacementGroupResourceManagerTest, TestNewReturnBundleResource) {
   CheckRemainingResourceCorrect(remaining_resouece_instance);
 }
 
+TEST_F(NewPlacementGroupResourceManagerTest, TestNewMultipleBundlesCommitAndReturn) {
+  // 1. create two bundles spec.
+  auto group_id = PlacementGroupID::FromRandom();
+  std::unordered_map<std::string, double> unit_resource;
+  unit_resource.insert({"CPU", 1.0});
+  auto first_bundle_spec = Mocker::GenBundleCreation(group_id, 1, unit_resource);
+  auto second_bundle_spec = Mocker::GenBundleCreation(group_id, 2, unit_resource);
+  /// 2. init local available resource.
+  std::unordered_map<std::string, double> init_unit_resource;
+  init_unit_resource.insert({"CPU", 2.0});
+  InitLocalAvailableResource(init_unit_resource);
+  /// 3. prepare and commit two bundle resource.
+  ASSERT_TRUE(new_placement_group_resource_manager_->PrepareBundle(first_bundle_spec));
+  ASSERT_TRUE(new_placement_group_resource_manager_->PrepareBundle(second_bundle_spec));
+  new_placement_group_resource_manager_->CommitBundle(first_bundle_spec);
+  new_placement_group_resource_manager_->CommitBundle(second_bundle_spec);
+  /// 4. check remaining resources is correct after commit phase.
+  std::unordered_map<std::string, double> remaining_resources = {{"CPU_group_" + group_id.Hex(), 2.0}, {"CPU_group_1_" + group_id.Hex(), 1.0}, {"CPU_group_2_" + group_id.Hex(), 1.0}, {"CPU", 2.0}};
+  auto remaining_resource_scheduler = std::make_shared<ClusterResourceScheduler>("remaining", remaining_resources);
+  std::shared_ptr<TaskResourceInstances> resource_instances = std::make_shared<TaskResourceInstances>();
+  ASSERT_TRUE(remaining_resource_scheduler->AllocateLocalTaskResources(init_unit_resource, resource_instances));
+  auto remaining_resouece_instance = remaining_resource_scheduler->GetLocalResources();
+  CheckRemainingResourceCorrect(remaining_resouece_instance);
+  /// 5. return second bundle.
+  new_placement_group_resource_manager_->ReturnBundle(second_bundle_spec);
+  /// 6. check remaining resources is correct after return second bundle.
+  remaining_resources = {{"CPU_group_" + group_id.Hex(), 2.0}, {"CPU_group_1_" + group_id.Hex(), 1.0}, {"CPU", 2.0}};
+  remaining_resource_scheduler = std::make_shared<ClusterResourceScheduler>("remaining", remaining_resources);
+  ASSERT_TRUE(remaining_resource_scheduler->AllocateLocalTaskResources({{"CPU_group_" + group_id.Hex(), 1.0}, {"CPU", 1.0}}, resource_instances));
+  remaining_resouece_instance = remaining_resource_scheduler->GetLocalResources();
+  CheckRemainingResourceCorrect(remaining_resouece_instance);
+  /// 7. return first bundel.
+  new_placement_group_resource_manager_->ReturnBundle(first_bundle_spec);
+  /// 8. check remaining resources is correct after all bundle returned.
+  remaining_resources = {{"CPU", 2.0}};
+  remaining_resource_scheduler = std::make_shared<ClusterResourceScheduler>("remaining", remaining_resources);
+  remaining_resouece_instance = remaining_resource_scheduler->GetLocalResources();
+  CheckRemainingResourceCorrect(remaining_resouece_instance);
+}
+
+TEST_F(NewPlacementGroupResourceManagerTest, TestNewIdempotencyWithMultiPrepare) {
+  // 1. create one bundle spec.
+  auto group_id = PlacementGroupID::FromRandom();
+  std::unordered_map<std::string, double> unit_resource;
+  unit_resource.insert({"CPU", 1.0});
+  auto bundle_spec = Mocker::GenBundleCreation(group_id, 1, unit_resource);
+  /// 2. init local available resource.
+  std::unordered_map<std::string, double> available_resource = {
+      std::make_pair("CPU", 3.0)};
+  InitLocalAvailableResource(available_resource);
+  /// 3. prepare bundle resource 10 times.
+  for (int i = 0; i < 10; i++) {
+    new_placement_group_resource_manager_->PrepareBundle(bundle_spec);
+  }
+  /// 4. check remaining resources is correct.
+  std::unordered_map<std::string, double> remaining_resources = {{"CPU", 3.0}};
+  auto remaining_resource_scheduler = std::make_shared<ClusterResourceScheduler>("remaining", remaining_resources);
+  std::shared_ptr<TaskResourceInstances> resource_instances = std::make_shared<TaskResourceInstances>();
+  ASSERT_TRUE(remaining_resource_scheduler->AllocateLocalTaskResources(unit_resource, resource_instances));
+  auto remaining_resouece_instance = remaining_resource_scheduler->GetLocalResources();
+  CheckRemainingResourceCorrect(remaining_resouece_instance);
+}
+
+TEST_F(NewPlacementGroupResourceManagerTest, TestNewIdempotencyWithRandomOrder) {
+  // 1. create one bundle spec.
+  auto group_id = PlacementGroupID::FromRandom();
+  std::unordered_map<std::string, double> unit_resource;
+  unit_resource.insert({"CPU", 1.0});
+  auto bundle_spec = Mocker::GenBundleCreation(group_id, 1, unit_resource);
+  /// 2. init local available resource.
+  std::unordered_map<std::string, double> available_resource = {
+      std::make_pair("CPU", 3.0)};
+  InitLocalAvailableResource(available_resource);
+  /// 3. prepare bundle -> commit bundle -> prepare bundle.
+  ASSERT_TRUE(old_placement_group_resource_manager_->PrepareBundle(bundle_spec));
+  old_placement_group_resource_manager_->CommitBundle(bundle_spec);
+  ASSERT_FALSE(old_placement_group_resource_manager_->PrepareBundle(bundle_spec));
+  /// 4. check remaining resources is correct.
+  std::vector<std::string> resource_labels = {"CPU_group_" + group_id.Hex(),
+                                              "CPU_group_1_" + group_id.Hex(), "CPU"};
+  std::vector<double> resource_capacity = {1.0, 1.0, 2.0};
+  ResourceSet result_resource(resource_labels, resource_capacity);
+  CheckRemainingResourceCorrect(result_resource);
+  old_placement_group_resource_manager_->ReturnBundle(bundle_spec);
+  // 5. prepare bundle -> commit bundle -> commit bundle.
+  old_placement_group_resource_manager_->PrepareBundle(bundle_spec);
+  old_placement_group_resource_manager_->CommitBundle(bundle_spec);
+  old_placement_group_resource_manager_->CommitBundle(bundle_spec);
+  // 6. check remaining resources is correct.
+  CheckRemainingResourceCorrect(result_resource);
+  old_placement_group_resource_manager_->ReturnBundle(bundle_spec);
+  // 7. prepare bundle -> return bundle -> commit bundle.
+  old_placement_group_resource_manager_->PrepareBundle(bundle_spec);
+  old_placement_group_resource_manager_->ReturnBundle(bundle_spec);
+  old_placement_group_resource_manager_->CommitBundle(bundle_spec);
+  result_resource = ResourceSet(available_resource);
+  CheckRemainingResourceCorrect(result_resource);
+}
+
 
 }  // namespace ray
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  ::testing::GTEST_FLAG(filter) = "*TestNewReturnBundleResource*";
+  ::testing::GTEST_FLAG(filter) = "*TestNewIdempotencyWithMultiPrepare*";
   return RUN_ALL_TESTS();
 }
