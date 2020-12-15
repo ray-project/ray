c6cb12e85faf1a01f33a33ee37126358c27e95e8
Philipp Moritz <pcmoritz@gmail.com>
2020-12-14 17:22:48 -0800

    Fix continue for debugger

--- python/ray/tests/test_ray_debugger.py
+++ python/ray/tests/test_ray_debugger.py
@@ -39,6 +39,33 @@ def test_ray_debugger_breakpoint(shutdown_only):
 
 @pytest.mark.skipif(
     platform.system() == "Windows", reason="Failing on Windows.")
+def test_ray_debugger_commands(shutdown_only):
+    ray.init(num_cpus=2)
+
+    @ray.remote
+    def f():
+        ray.util.pdb.set_trace()
+
+    result1 = f.remote()
+    result2 = f.remote()
+
+    # Make sure that calling "continue" in the debugger
+    # gives back control to the debugger loop:
+    p = pexpect.spawn("ray debug")
+    p.expect("Enter breakpoint index or press enter to refresh: ")
+    p.sendline("0")
+    p.expect("-> ray.util.pdb.set_trace()")
+    p.sendline("c")
+    p.expect("Enter breakpoint index or press enter to refresh: ")
+    p.sendline("0")
+    p.expect("-> ray.util.pdb.set_trace()")
+    p.sendline("c")
+
+    ray.get([result1, result2])
+
+
+@pytest.mark.skipif(
+    platform.system() == "Windows", reason="Failing on Windows.")
 def test_ray_debugger_stepping(shutdown_only):
     ray.init(num_cpus=1)
 
