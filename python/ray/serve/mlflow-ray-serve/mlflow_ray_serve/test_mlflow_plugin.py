35aa6af2a2ea5258d3a01a3ac49658d8a0adec88
architkulkarni <arkulkar@gmail.com>
2020-12-15 16:55:25 -0800

    fix test

--- python/ray/serve/mlflow-ray-serve/mlflow_ray_serve/test_mlflow_plugin.py
+++ python/ray/serve/mlflow-ray-serve/mlflow_ray_serve/test_mlflow_plugin.py
@@ -1,21 +1,19 @@
 import mlflow.pyfunc
 
-# # Define the model class
-# class AddN(mlflow.pyfunc.PythonModel):
 
-#     def __init__(self, n):
-#         self.n = n
+# Define the model class
+class AddN(mlflow.pyfunc.PythonModel):
+    def __init__(self, n):
+        self.n = n
 
-#     def predict(self, context, model_input):
-#         return model_input.apply(lambda column: column + self.n)
+    def predict(self, context, model_input):
+        return model_input.apply(lambda column: column + self.n)
 
-# # Construct and save the model
-# model_path = "add_n_model"
-# add5_model = AddN(n=5)
-# mlflow.pyfunc.save_model(path=model_path, python_model=add5_model)
 
-# # Load the model in `python_function` format
-# loaded_model = mlflow.pyfunc.load_model(model_path)
+# Construct and save the model
+model_path = "/Users/archit/ray/add_n_model"
+add5_model = AddN(n=5)
+mlflow.pyfunc.save_model(path=model_path, python_model=add5_model)
 
 # Evaluate the model
 import pandas as pd
@@ -32,5 +30,3 @@ model_output = client.predict("add5", model_input)
 assert model_output.equals(pd.DataFrame([range(5, 15)]))
 
 client.delete_deployment("add5")
-
-
