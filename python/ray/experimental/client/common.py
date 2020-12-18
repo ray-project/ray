df54079b788e51de5721302e0688773cf4db665b
Barak Michener <me@barakmich.com>
2020-12-18 19:48:46 +0000

    options wip
    Change-Id: I3afaceb728c36bd2eeac01889ffb385cbeeceeef

--- python/ray/experimental/client/common.py
+++ python/ray/experimental/client/common.py
@@ -1,6 +1,11 @@
 import ray.core.generated.ray_client_pb2 as ray_client_pb2
 from ray.experimental.client import ray
 
+import json
+from typing import Any
+from typing import Dict
+from typing import Optional
+
 
 class ClientBaseRef:
     def __init__(self, id: bytes):
@@ -52,10 +57,11 @@ class ClientRemoteFunc(ClientStub):
         _ref: The ClientObjectRef of the pickled code of the function, _func
     """
 
-    def __init__(self, f):
+    def __init__(self, f, options = None):
         self._func = f
         self._name = f.__name__
         self._ref = None
+        self.options = options
 
     def __call__(self, *args, **kwargs):
         raise TypeError(f"Remote function cannot be called directly. "
@@ -100,10 +106,11 @@ class ClientActorClass(ClientStub):
         _ref: The ClientObjectRef of the pickled `actor_cls`
     """
 
-    def __init__(self, actor_cls):
+    def __init__(self, actor_cls, options=None):
         self.actor_cls = actor_cls
         self._name = actor_cls.__name__
         self._ref = None
+        self.options = options
 
     def __call__(self, *args, **kwargs):
         raise TypeError(f"Remote actor cannot be instantiated directly. "
@@ -184,9 +191,10 @@ class ClientRemoteMethod(ClientStub):
         method_name: The name of this method
     """
 
-    def __init__(self, actor_handle: ClientActorHandle, method_name: str):
+    def __init__(self, actor_handle: ClientActorHandle, method_name: str, options = None):
         self.actor_handle = actor_handle
         self.method_name = method_name
+        self.options = options
 
     def __call__(self, *args, **kwargs):
         raise TypeError(f"Remote method cannot be called directly. "
@@ -206,6 +214,62 @@ class ClientRemoteMethod(ClientStub):
         task.payload_id = self.actor_handle.actor_ref.id
         return task
 
+
+_valid_options = [
+    "num_cpus",
+    "num_gpus",
+    "resources",
+    "accelerator_type",
+    "max_calls",
+    "max_restarts",
+    "max_task_retries",
+    "max_retries",
+    "max_concurrency",
+    "name",
+    "lifetime",
+    "memory",
+    "object_store_memory",
+    "placement_group",
+    "placement_group_bundle_index",
+    "placement_group_capture_child_tasks",
+    "override_environment_variables",
+]
+
+
+def validate_options(kwargs_dict: Dict[str, Any], strict: bool = True) -> Dict[str, Any]:
+    out = {}
+    for k, v in kwargs_dict.items():
+        if k not in _valid_options:
+            if strict:
+                raise TypeError(f"Invalid option passed to remote(): {k}")
+            else:
+                continue
+        out[k] = v
+    return out
+
+
+class OptionWrapper:
+    def __init__(self, stub: ClientStub, options: Dict[str, Any]):
+        self.remote_stub = stub
+        self.options = validate_options(options)
+
+    def __getattr__(self, key):
+        return getattr(self.remote_stub, key)
+
+    def _prepare_client_task(self):
+        task = self.remote_stub._prepare_client_task()
+        set_task_options(task, self.options)
+        return task
+
+
+def set_task_options(
+    task: ray_client_pb2.ClientTask,
+    options: Optional[Dict[str, Any]]) -> None:
+    if options is None:
+        task.ClearField("options")
+        return
+    options_str = json.dumps(options)
+    task.options = options_str
 
 class DataEncodingSentinel:
     def __repr__(self) -> str:
