fd967c845c03ecdaf4b6320a332447be3217b88c
Philipp Moritz <pcmoritz@gmail.com>
2020-12-19 23:34:10 -0800

    oops

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -267,7 +267,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# html_theme = "sphinx_book_theme"
+html_theme = "sphinx_book_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
