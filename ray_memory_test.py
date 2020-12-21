d89ae31e0fe92be986acc2442e46d94dff0530c9
Micah Yong <micahtyong@gmail.com>
2020-12-21 01:24:38 -0800

    Add click options for sort_by and group_by

--- ray_memory_test.py
+++ ray_memory_test.py
@@ -3,12 +3,17 @@ import ray
 import time
 ray.init()
 
+
 @ray.remote
 def f(arg):
     return arg
 
+
 a = ray.put(None)
 b = f.remote(None)
+c = ray.put([1, 2, 3])
+d = ray.put([c])
+del c
 
-# Give some time to test 'ray memory' 
+# Give some time to test 'ray memory'
 time.sleep(60)
