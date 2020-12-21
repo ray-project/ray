51b4fd77ea4e2714cda92a811df984499211a378
Max Fitton <maxfitton@anyscale.com>
2020-12-21 11:20:44 -0800

    Update rllib regression_tests to install their dependencies via pip

--- release/rllib_tests/regression_tests/run.sh
+++ release/rllib_tests/regression_tests/run.sh
@@ -47,6 +47,7 @@ conda uninstall -y terminado
 pip install -U pip
 pip install -U "$wheel"
 pip install "ray[rllib]" "ray[debug]"
+pip install -r requirements.txt
 pip install terminado
 pip install torch==1.6 torchvision
 pip install boto3==1.4.8 cython==0.29.0
