9150912c1f595b2ac0e09a07bddee06b6ffafcd6
Micah Yong <micahtyong@gmail.com>
2020-12-21 00:56:17 -0800

    Ray memory burner test

--- ray_memory_test.py
+++ ray_memory_test.py
@@ -1,14 +1,11 @@
-# Burner file to test basic modifications to the 'ray memory' CLI
-# according to https://github.com/ray-project/ray/issues/12280
+# Burner file to test basic modifications to the 'ray memory' CLI according to https://github.com/ray-project/ray/issues/12280
 import ray
 import time
 ray.init()
 
-
 @ray.remote
 def f(arg):
     return arg
-
 
 a = ray.put(None)
 b = f.remote(None)
