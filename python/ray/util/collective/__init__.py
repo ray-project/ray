5b48480e2932de0aaa1853b8abcc3e1f69e59cd5
Hao Zhang <zhisbug@users.noreply.github.com>
2020-12-21 15:48:00 -0500

    [Collective][PR 3/6] Other collectives (#12864)

--- python/ray/util/collective/__init__.py
+++ python/ray/util/collective/__init__.py
@@ -1,9 +1,11 @@
-from .collective import nccl_available, mpi_available, is_group_initialized, \
-    init_collective_group, destroy_collective_group, get_rank, \
-    get_world_size, allreduce, barrier
+from ray.util.collective.collective import nccl_available, mpi_available, \
+    is_group_initialized, init_collective_group, destroy_collective_group, \
+    get_rank, get_world_size, allreduce, barrier, reduce, broadcast, \
+    allgather, reducescatter
 
 __all__ = [
     "nccl_available", "mpi_available", "is_group_initialized",
     "init_collective_group", "destroy_collective_group", "get_rank",
-    "get_world_size", "allreduce", "barrier"
+    "get_world_size", "allreduce", "barrier", "reduce", "broadcast",
+    "allgather", "reducescatter"
 ]
