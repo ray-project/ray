7770e407dcee05b7c35a555ca2e4ad0ab27e34d7
architkulkarni <arkulkar@gmail.com>
2020-12-23 14:27:26 -0800

    fix docstring

--- python/ray/serve/handle.py
+++ python/ray/serve/handle.py
@@ -110,7 +110,7 @@ class RayServeSyncHandle(RayServeHandle):
         Args:
             request_data(dict, Any): If it's a dictionary, the data will be
                 available in ``request.json()`` or ``request.form()``.
-                If it's a flask.Request object, it will be passed in to the
+                If it's a Starlette Request object, it will be passed in to the
                 backend directly, unmodified. Otherwise, the data will be
                 available in ``request.data``.
             ``**kwargs``: All keyword arguments will be available in
