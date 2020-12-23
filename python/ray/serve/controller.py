c562f073cdedf43723400f4ebc1f7f1e348653b7
Edward Oakes <ed.nmi.oakes@gmail.com>
2020-12-23 10:54:45 -0600

    cleanup

--- python/ray/serve/controller.py
+++ python/ray/serve/controller.py
@@ -83,16 +83,19 @@ class TrafficPolicy:
 class HTTPState:
     def __init__(self, controller_name: str, detached: bool,
                  config: HTTPConfig):
-        self.controller_name = controller_name
-        self.detached = detached
-        self.config = config
-        self.proxy_actors: Dict[NodeId, ActorHandle] = dict()
+        self._controller_name = controller_name
+        self._detached = detached
+        self._config = config
+        self._proxy_actors: Dict[NodeId, ActorHandle] = dict()
 
         # Will populate self.proxy_actors with existing actors.
         self._start_proxies_if_needed()
 
-    def http_proxy_handles(self) -> List[ActorHandle]:
-        return list(self.proxy_actors.values())
+    def get_config(self):
+        return self._config
+
+    def get_http_proxy_handles(self) -> Dict[NodeId, ActorHandle]:
+        return self._proxy_actors
 
     def update(self):
         self._start_proxies_if_needed()
@@ -100,25 +103,25 @@ class HTTPState:
 
     def _start_proxies_if_needed(self) -> None:
         """Start a proxy on every node if it doesn't already exist."""
-        if self.config.host is None:
+        if self._config.host is None:
             return
 
         for node_id, node_resource in get_all_node_ids():
-            if node_id in self.proxy_actors:
+            if node_id in self._proxy_actors:
                 continue
 
-            name = format_actor_name(SERVE_PROXY_NAME, self.controller_name,
+            name = format_actor_name(SERVE_PROXY_NAME, self._controller_name,
                                      node_id)
             try:
                 proxy = ray.get_actor(name)
             except ValueError:
                 logger.info("Starting HTTP proxy with name '{}' on node '{}' "
                             "listening on '{}:{}'".format(
-                                name, node_id, self.config.host,
-                                self.config.port))
+                                name, node_id, self._config.host,
+                                self._config.port))
                 proxy = HTTPProxyActor.options(
                     name=name,
-                    lifetime="detached" if self.detached else None,
+                    lifetime="detached" if self._detached else None,
                     max_concurrency=ASYNC_CONCURRENCY,
                     max_restarts=-1,
                     max_task_retries=-1,
@@ -126,25 +129,25 @@ class HTTPState:
                         node_resource: 0.01
                     },
                 ).remote(
-                    self.config.host,
-                    self.config.port,
-                    controller_name=self.controller_name,
-                    http_middlewares=self.config.middlewares)
+                    self._config.host,
+                    self._config.port,
+                    controller_name=self._controller_name,
+                    http_middlewares=self._config.middlewares)
 
-            self.proxy_actors[node_id] = proxy
+            self._proxy_actors[node_id] = proxy
 
     def _stop_proxies_if_needed(self) -> bool:
         """Removes proxy actors from any nodes that no longer exist."""
         all_node_ids = {node_id for node_id, _ in get_all_node_ids()}
         to_stop = []
-        for node_id in self.proxy_actors:
+        for node_id in self._proxy_actors:
             if node_id not in all_node_ids:
                 logger.info("Removing HTTP proxy on removed node '{}'.".format(
                     node_id))
                 to_stop.append(node_id)
 
         for node_id in to_stop:
-            proxy = self.proxy_actors.pop(node_id)
+            proxy = self._proxy_actors.pop(node_id)
             ray.kill(proxy, no_restart=True)
 
 
@@ -643,9 +646,9 @@ class ServeController:
         return await (
             self.long_poll_host.listen_for_change(keys_to_snapshot_ids))
 
-    def get_http_proxies(self) -> Dict[str, ActorHandle]:
+    def get_http_proxies(self) -> Dict[NodeId, ActorHandle]:
         """Returns a dictionary of node ID to http_proxy actor handles."""
-        return self.http_state.proxy_actors
+        return self.http_state.get_http_proxy_handles()
 
     def _checkpoint(self) -> None:
         """Checkpoint internal state and write it to the KV store."""
@@ -1045,13 +1048,13 @@ class ServeController:
 
     def get_http_config(self):
         """Return the HTTP proxy configuration."""
-        return self.http_state.config
+        return self.http_state.get_config()
 
     async def shutdown(self) -> None:
         """Shuts down the serve instance completely."""
         async with self.write_lock:
-            for http_proxy in self.http_state.http_proxy_handles():
-                ray.kill(http_proxy, no_restart=True)
+            for proxy in self.http_state.get_http_proxy_handles().values():
+                ray.kill(proxy, no_restart=True)
             for replica in self.actor_reconciler.get_replica_handles():
                 ray.kill(replica, no_restart=True)
             self.kv_store.delete(CHECKPOINT_KEY)
