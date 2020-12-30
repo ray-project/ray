e61c27a3babbd435ae28b578190009e41df7f062
architkulkarni <arkulkar@gmail.com>
2020-12-30 14:29:00 -0800

    lint

--- python/ray/serve/examples/doc/aiohttp_app.py
+++ python/ray/serve/examples/doc/aiohttp_app.py
@@ -10,7 +10,8 @@ ray.init(address="auto")
 # Connect to the running Ray Serve instance.
 client = serve.connect()
 
-my_handle = client.get_handle("my_endpoint") # Returns a ServeHandle object.
+my_handle = client.get_handle("my_endpoint")  # Returns a ServeHandle object.
+
 
 # Define our AIOHTTP request handler.
 async def handle_request(request):
@@ -18,8 +19,10 @@ async def handle_request(request):
     result = await my_handle.remote("dummy input")
     return web.Response(text=result)
 
+
 app = web.Application()
-app.add_routes([web.get('/dummy-model', handle_request)]) # Sets up an HTTP endpoint.
+app.add_routes([web.get('/dummy-model',
+                        handle_request)])  # Sets up an HTTP endpoint.
 
 if __name__ == '__main__':
     web.run_app(app)
--- python/ray/serve/examples/doc/aiohttp_deploy.py
+++ python/ray/serve/examples/doc/aiohttp_deploy.py
@@ -8,10 +8,12 @@ ray.init(address="auto")
 # Start a detached Ray Serve instance.  It will persist after the script exits.
 client = serve.start(http_host=None, detached=True)
 
+
 # Define a function to serve. Alternatively, you could define a stateful class.
 async def my_model(request):
     data = await request.body()
     return f"Model received data: {data}"
+
 
 client.create_backend("my_backend", my_model)
 client.create_endpoint("my_endpoint", backend="my_backend")
