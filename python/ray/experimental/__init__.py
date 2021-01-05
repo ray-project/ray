027efcfa4ff7a6a6803bc0eae416662dfce864aa
lingxuan.zlx <skyzlxuan@gmail.com>
2021-01-05 20:24:19 +0800

    export metric types for users

--- python/ray/experimental/__init__.py
+++ python/ray/experimental/__init__.py
@@ -1,4 +1,14 @@
 from .dynamic_resources import set_resource
+from .metrics import (
+    Count,
+    Histogram,
+    Gauge,
+    Sum,
+)
 __all__ = [
     "set_resource",
+    "Count",
+    "Histogram",
+    "Gauge",
+    "Sum",
 ]
