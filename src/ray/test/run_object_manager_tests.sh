88eaa87e3a4fef123308bb45457a09f5fddeb68b
fangfengbin <869218239a@zju.edu.cn>
2021-01-05 11:09:36 +0800

    Remove unused file(object_manager_integration_test.cc) (#12989)

--- src/ray/test/run_object_manager_tests.sh
+++ src/ray/test/run_object_manager_tests.sh
@@ -41,7 +41,3 @@ sleep 1s
 ./bazel-bin/object_manager_test $STORE_EXEC 1000 $GCS_SERVER_EXEC
 bazel run //:redis-cli -- -p 6379 shutdown
 bazel run //:redis-cli -- -p 6380 shutdown
-sleep 1s
-
-# Include raylet integration test once it's ready.
-# ./bazel-bin/object_manager_integration_test $STORE_EXEC
