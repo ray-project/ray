11605bda42f249e9a29098f594f3515816a2d903
lingxuan.zlx <skyzlxuan@gmail.com>
2021-01-05 20:55:00 +0800

    metric api exposing

--- python/ray/experimental/__init__.py
+++ python/ray/experimental/__init__.py
@@ -1,14 +1,7 @@
 from .dynamic_resources import set_resource
-from .metrics import (
-    Count,
-    Histogram,
-    Gauge,
-    Sum,
-)
+from . import metrics
+
 __all__ = [
     "set_resource",
-    "Count",
-    "Histogram",
-    "Gauge",
-    "Sum",
+    "metrics",
 ]
