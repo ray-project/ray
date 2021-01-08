be57289b9d6b5600f8fa823b63b2febe997effd1
Philipp Moritz <pcmoritz@gmail.com>
2021-01-07 18:42:04 -0800

    more metadata

--- python/ray/util/client/worker.py
+++ python/ray/util/client/worker.py
@@ -240,7 +240,7 @@ class Worker:
     def get_cluster_info(self, type: ray_client_pb2.ClusterInfoType.TypeEnum):
         req = ray_client_pb2.ClusterInfoRequest()
         req.type = type
-        resp = self.server.ClusterInfo(req)
+        resp = self.server.ClusterInfo(req, metadata=self.metadata)
         if resp.WhichOneof("response_type") == "resource_table":
             # translate from a proto map to a python dict
             output_dict = {k: v for k, v in resp.resource_table.table.items()}
