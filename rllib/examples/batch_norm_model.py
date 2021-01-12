3e3b2bcda7e0a9bdca0c0fa9c053c0d77710b3cc
Kai Fricke <krfricke@users.noreply.github.com>
2021-01-12 09:44:17 +0100

    Revert "[RLlib] Make TFModelV2 behave more like TorchModelV2: Obsolete register_variables. Unify variable dicts. (#13339)"
    This reverts commit e2b2abb88b82c0c2402a338bba51e5dbd1739419.

--- rllib/examples/batch_norm_model.py
+++ rllib/examples/batch_norm_model.py
@@ -33,7 +33,6 @@ if __name__ == "__main__":
         "model": {
             "custom_model": "bn_model",
         },
-        "lr": 0.0003,
         # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
         "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
         "num_workers": 0,
@@ -46,7 +45,7 @@ if __name__ == "__main__":
         "episode_reward_mean": args.stop_reward,
     }
 
-    results = tune.run(args.run, stop=stop, config=config, verbose=2)
+    results = tune.run(args.run, stop=stop, config=config, verbose=1)
 
     if args.as_test:
         check_learning_achieved(results, args.stop_reward)
