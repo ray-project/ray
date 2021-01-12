184ea15ae2494f6ffc9ae3ba9993cb689a04850b
dHannasch <David.A.Hannasch@gmail.com>
2021-01-11 21:58:23 -0700

    missing space
    Co-authored-by: fangfengbin <869218239a@zju.edu.cn>

--- src/ray/gcs/redis_client.cc
+++ src/ray/gcs/redis_client.cc
@@ -135,7 +135,7 @@ Status RedisClient::Connect(std::vector<boost::asio::io_service *> io_services) 
       Status portReachable = shard_contexts_[i]->PingPort(addresses[i], ports[i]);
       if (!portReachable.ok()) {
         RAY_LOG(WARNING) << "Received Redis shard address " << addresses[i] << ":"
-                         << ports[i] << "from Redis head " << options_.server_ip_ << ":"
+                         << ports[i] << " from Redis head " << options_.server_ip_ << ":"
                          << options_.server_port_ << ". " << portReachable.message()
                          << " Since we successfully connected to the address you "
                          << "provided " << options_.server_ip_ << ":"
