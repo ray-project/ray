cebb87527796489874b1f78f3e81f5a081c88a7f
Amog Kamsetty <amogkamsetty@yahoo.com>
2021-01-13 12:45:46 -0800

    wip

---
+++ python/ray/util/lightning_accelerators/horovod_ray_accelerator.py
@@ -0,0 +1,63 @@
+from pytorch_lightning.accelerators.horovod_accelerator import HorovodAccelerator
+
+try:
+    import horovod.torch as hvd
+    from horovod.ray import RayExecutor
+except (ModuleNotFoundError, ImportError):
+    HOROVOD_AVAILABLE = False
+else:
+    HOROVOD_AVAILABLE = True
+
+
+def get_executable_cls():
+    # Only used for testing purposes, currently.
+    # We need to override this in tests to ensure test path is set correctly.
+    return None
+
+
+class HorovodRayAccelerator(HorovodAccelerator):
+    def __init__(self, trainer, cluster_environment=None):
+        super().__init__(trainer, cluster_environment)
+        self.nickname = 'horovod_ray'
+
+        settings = RayExecutor.create_settings(timeout_s=30)
+        self.executor = RayExecutor(settings,
+                                    num_hosts=self.trainer.num_nodes,
+                                    num_slots=self.trainer.num_processes,
+                                    use_gpu=self.trainer.on_gpu)
+
+    def setup(self, model):
+        self.trainer.model = model
+        self.executor.start(executable_cls=get_executable_cls())
+
+    def train(self):
+        results = self.executor.run(self.train_remote)
+        results, state_dict, best_path = results[0]
+
+        self.trainer.model.load_state_dict(state_dict)
+        if self.trainer.checkpoint_callback:
+            self.trainer.checkpoint_callback.best_model_path = best_path
+
+        return results
+
+    def train_remote(self):
+        hvd.init()
+        if self.trainer.on_gpu:
+            # Horovod assigns one local GPU per process
+            self.trainer.root_gpu = hvd.local_rank()
+
+        self.setup_worker(self.trainer.model)
+        results = self.train_worker()
+        if hvd.rank() != 0:
+            # Only want results from the first worker
+            return None
+
+        best_model_path = None
+        if self.trainer.checkpoint_callback is not None:
+            best_model_path = self.trainer.checkpoint_callback.best_model_path
+
+        model = self.trainer.model
+        return results, model.state_dict(), best_model_path
+
+    def teardown(self):
+        self.executor.shutdown()
---
+++ python/ray/util/lightning_accelerators/tests/test_horovod_ray_accelerator.py
@@ -0,0 +1,101 @@
+import os
+import platform
+import sys
+from unittest import mock
+
+import pytest
+import ray
+import torch
+
+import tests.base.develop_pipelines as tpipes
+from tests.base import EvalModelTemplate
+
+path_here = os.path.abspath(os.path.dirname(__file__))
+path_root = os.path.abspath(os.path.join(path_here, '..', '..'))
+
+
+try:
+    import horovod
+    from horovod.common.util import nccl_built
+except ImportError:
+    HOROVOD_AVAILABLE = False
+else:
+    HOROVOD_AVAILABLE = True
+
+
+def _nccl_available():
+    if not HOROVOD_AVAILABLE:
+        return False
+
+    try:
+        return nccl_built()
+    except AttributeError:
+        return False
+
+
+@pytest.fixture
+def ray_start_2_cpus():
+    address_info = ray.init(num_cpus=2)
+    try:
+        yield address_info
+    finally:
+        ray.shutdown()
+
+
+@pytest.fixture
+def ray_start_2_gpus():
+    address_info = ray.init(num_cpus=2, num_gpus=2)
+    try:
+        yield address_info
+    finally:
+        ray.shutdown()
+
+
+def create_mock_executable():
+    # Some modules in site-packages conflict with the tests module
+    # used by PyTorch Lightning. As a result, we need to make sure all
+    # the Ray workers push the correct tests path to the front of the sys path.
+    class MockExecutable:
+        def __init__(self):
+            sys.path.insert(0, os.path.abspath(path_root))
+    return MockExecutable
+
+
+@mock.patch('pytorch_lightning.accelerators.horovod_ray_accelerator.get_executable_cls')
+@pytest.mark.skipif(platform.system() == "Windows", reason="Horovod is not supported on Windows")
+def test_horovod_cpu(mock_executable_cls, tmpdir, ray_start_2_cpus):
+    """Test Horovod running multi-process on CPU."""
+    mock_executable_cls.return_value = create_mock_executable()
+    trainer_options = dict(
+        default_root_dir=tmpdir,
+        max_epochs=1,
+        limit_train_batches=10,
+        limit_val_batches=10,
+        num_processes=2,
+        distributed_backend='horovod_ray',
+        progress_bar_refresh_rate=0
+    )
+
+    model = EvalModelTemplate()
+    tpipes.run_model_test(trainer_options, model)
+
+
+# @mock.patch('pytorch_lightning.accelerators.horovod_ray_accelerator.get_executable_cls')
+# @pytest.mark.skipif(platform.system() == "Windows", reason="Horovod is not supported on Windows")
+# @pytest.mark.skipif(not _nccl_available(), reason="test requires Horovod with NCCL support")
+# @pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
+# def test_horovod_multi_gpu(mock_executable_cls, tmpdir, ray_start_2_gpus):
+#     """Test Horovod with multi-GPU support."""
+#     mock_executable_cls.return_value = create_mock_executable()
+#     trainer_options = dict(
+#         default_root_dir=tmpdir,
+#         max_epochs=1,
+#         limit_train_batches=10,
+#         limit_val_batches=10,
+#         gpus=2,
+#         distributed_backend='horovod_ray',
+#         progress_bar_refresh_rate=0
+#     )
+#
+#     model = EvalModelTemplate()
+#     tpipes.run_model_test(trainer_options, model)
