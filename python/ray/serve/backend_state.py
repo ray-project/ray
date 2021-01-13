ecdd6534aa738a16f37f0ab68faf55c793102687
Ian <ian.rodney@gmail.com>
2021-01-13 07:51:32 +0000

    add checkpointing

--- python/ray/serve/backend_state.py
+++ python/ray/serve/backend_state.py
@@ -43,12 +43,22 @@ class ReplicaState(Enum):
 class BackendReplica:
     def __init__(self, controller_name, replica_tag, backend_tag):
         self._actor_name = format_actor_name(replica_tag, controller_name)
+        self._replica_tag = replica_tag
         self._actor_handle = None
         self._startup_obj_ref = None
         self._shutdown_future = None
         self._state = ReplicaState.SHOULD_START
 
     def recover_from_checkpoint(self):
+        if self._state == ReplicaState.STARTING:
+            pass
+        elif self._state == ReplicaState.RUNNING:
+            # Fetch actor handles for all backend replicas in the system.
+            # All of these backend_replicas are guaranteed to already exist
+            # because they would not be written to a checkpoint in
+            # until they were created.
+            self._actor_handle = ray.get_actor(self._actor_name)
+
         try:
             replica_handle = ray.get_actor(replica_name)
         except ValueError:
@@ -126,6 +136,9 @@ class BackendReplica:
         except asyncio.TimeoutError:
             return False
 
+    def get_handle(self):
+        assert self._state == ReplicaState.RUNNING
+        return self._actor_handle
 
 class BackendState:
     """Manages all state for backends in the system.
@@ -144,40 +157,18 @@ class BackendState:
         self._replicas: Dict[BackendTag, Dict[ReplicaState, List[BackendReplica]]] = defaultdict(lambda: defaultdict(list))
         self._backend_metadata: Dict[BackendTag, BackendInfo] = dict()
         self._target_replicas: Dict[BackendTag, int] = dict()
+        self._goals: Dict[BackendTag, GoalId] = dict()
 
-
-        # Checkpointed state.
-        self.backends: Dict[BackendTag, BackendInfo] = dict()
-        self.backend_replicas: Dict[BackendTag, Dict[
-            ReplicaTag, ActorHandle]] = defaultdict(dict)
-        self.goals: Dict[BackendTag, GoalId] = dict()
-        self.backend_replicas_to_start: Dict[BackendTag, List[
-            ReplicaTag]] = defaultdict(list)
-        self.backend_replicas_to_stop: Dict[BackendTag, List[Tuple[
-            ReplicaTag, Duration]]] = defaultdict(list)
+        # Un-Checkpointed state.
         self.pending_goals: Dict[GoalId, asyncio.Event] = dict()
 
         checkpoint = self._kv_store.get(CHECKPOINT_KEY)
         if checkpoint is not None:
-            # (self.backends, self.backend_replicas, self.goals,
-            #  self.backend_replicas_to_start, self.backend_replicas_to_stop,
-            #  self.backend_to_remove,
-            #  pending_goal_ids) = pickle.loads(checkpoint)
-            (self._replicas, self._backend_metadata, self._target_replicas) = pickle.loads(checkpoint)
+            (self._replicas, self._backend_metadata, self._target_replicas,
+                self._goals, pending_goal_ids) =  pickle.loads(checkpoint)
 
             for goal_id in pending_goal_ids:
                 self._create_goal(goal_id)
-
-            # Fetch actor handles for all backend replicas in the system.
-            # All of these backend_replicas are guaranteed to already exist
-            # because they would not be written to a checkpoint in
-            # self.backend_replicas until they were created.
-            for backend_tag, replica_dict in self.backend_replicas.items():
-                for replica_tag in replica_dict.keys():
-                    replica_name = format_actor_name(replica_tag,
-                                                     self._controller_name)
-                    self.backend_replicas[backend_tag][
-                        replica_tag] = ray.get_actor(replica_name)
 
         self._notify_backend_configs_changed()
         self._notify_replica_handles_changed()
@@ -186,20 +177,26 @@ class BackendState:
         self._kv_store.put(
             CHECKPOINT_KEY,
             pickle.dumps(
-                (self.backends, self.backend_replicas, self.goals,
-                 self.backend_replicas_to_start, self.backend_replicas_to_stop,
-                 list(self.pending_goals.keys()))))
+                (self._replicas, self._backend_metadata, self._target_replicas,
+                self._goals, list(self.pending_goals.keys()))
+            )
+        )
 
     def _notify_backend_configs_changed(self) -> None:
         self._long_poll_host.notify_changed(LongPollKey.BACKEND_CONFIGS,
                                             self.get_backend_configs())
 
+    def get_replica_handles(
+            self) -> Dict[BackendTag, Dict[ReplicaTag, ActorHandle]]:
+        return             {backend_tag : dict(backend_replica._replica_tag : backend_replica.get_handle() for handle in state_to_replica_dict[ReplicaState.RUNNING])
+            for backend_tag, state_to_replica_dict in self._replicas.items()}
+
     def _notify_replica_handles_changed(self) -> None:
         self._long_poll_host.notify_changed(
-            LongPollKey.REPLICA_HANDLES, {
-                backend_tag: list(replica_dict.values())
-                for backend_tag, replica_dict in self.backend_replicas.items()
-            })
+            LongPollKey.REPLICA_HANDLES, 
+            {backend_tag : list(replica_dict.values()) for backend_tag, replica_dict in 
+            self.get_replica_handles()})
+
 
     def get_backend_configs(self) -> Dict[BackendTag, BackendConfig]:
         return {
@@ -207,9 +204,6 @@ class BackendState:
             for tag, info in self._backend_metadata.items()
         }
 
-    def get_replica_handles(
-            self) -> Dict[BackendTag, Dict[ReplicaTag, ActorHandle]]:
-        return self.backend_replicas
 
     def get_backend(self, backend_tag: BackendTag) -> Optional[BackendInfo]:
         return self._backend_metadata.get(backend_tag)
@@ -243,14 +237,14 @@ class BackendState:
 
     def _set_backend_goal(self, backend_tag: BackendTag,
                           backend_info: Optional[BackendInfo]) -> None:
-        existing_goal = self.goals.get(backend_tag)
+        existing_goal = self._goals.get(backend_tag)
         new_goal = self._create_goal()
 
         if backend_info is not None:
             self._backend_metadata[backend_tag] = backend_info
             self._target_replicas[backend_tag] = backend_info.backend_config.num_replicas
 
-        self.goals[backend_tag] = new_goal
+        self._goals[backend_tag] = new_goal
 
         return new_goal, existing_goal
 
@@ -461,22 +455,23 @@ class BackendState:
 
     def _completed_goals(self) -> List[GoalId]:
         completed_goals = []
-        all_tags = set(self.backend_replicas.keys()).union(
-            set(self.backends.keys()))
+        all_tags = set(self._replicas.keys()).union(
+            set(self._backend_metadata.keys()))
 
         for backend_tag in all_tags:
-            desired_info = self.backends.get(backend_tag)
-            existing_info = self.backend_replicas.get(backend_tag)
+            desired_info = self._backend_metadata.get(backend_tag)
+            existing_info = self._replicas.get(backend_tag)
+            # TODO(ilr): FIX
             # Check for deleting
             if (not desired_info or
                     desired_info.backend_config.num_replicas == 0) and \
                     (not existing_info or len(existing_info) == 0):
-                completed_goals.append(self.goals.get(backend_tag))
+                completed_goals.append(self._goals.get(backend_tag)
 
             # Check for a non-zero number of backends
             if desired_info and existing_info and desired_info.backend_config.\
                     num_replicas == len(existing_info):
-                completed_goals.append(self.goals.get(backend_tag))
+                completed_goals.append(self._goals.get(backend_tag)
         return [goal for goal in completed_goals if goal]
 
     async def update(self) -> bool:
