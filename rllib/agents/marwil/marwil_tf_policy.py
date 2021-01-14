3e832516bbec4ebd502bb1de1f3d5525540202e2
sven1977 <svenmika1977@gmail.com>
2021-01-14 20:39:20 +0100

    LINT.

--- rllib/agents/marwil/marwil_tf_policy.py
+++ rllib/agents/marwil/marwil_tf_policy.py
@@ -7,7 +7,6 @@ from ray.rllib.evaluation.postprocessing import compute_advantages, \
 from ray.rllib.policy.tf_policy_template import build_tf_policy
 from ray.rllib.utils.framework import try_import_tf, get_variable
 from ray.rllib.utils.tf_ops import explained_variance, make_tf_callable
-from ray.util import log_once
 
 tf1, tf, tfv = try_import_tf()
 
