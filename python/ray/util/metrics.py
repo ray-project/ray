05aabc7a15ee9256eecb5bdb7f7122bbd8759413
Edward Oakes <ed.nmi.oakes@gmail.com>
2021-01-14 14:44:30 -0600

    fix test

--- python/ray/util/metrics.py
+++ python/ray/util/metrics.py
@@ -79,9 +79,11 @@ class Metric:
             value(float): The value to be recorded as a metric point.
         """
         assert self._metric is not None
-        for val in tags.values():
-            if not isinstance(val, str):
-                raise TypeError(f"Tag values must be str, got {type(val)}.")
+        if tags is not None:
+            for val in tags.values():
+                if not isinstance(val, str):
+                    raise TypeError(
+                        f"Tag values must be str, got {type(val)}.")
 
         default_tag_copy = self._default_tags.copy()
         default_tag_copy.update(tags or {})
