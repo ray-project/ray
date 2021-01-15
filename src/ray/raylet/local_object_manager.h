0bd5111937e2da48b2de9a3176745baa8f4d360d
SangBin Cho <rkooo567@gmail.com>
2021-01-14 16:31:49 -0800

    Addressed code review.

--- src/ray/raylet/local_object_manager.h
+++ src/ray/raylet/local_object_manager.h
@@ -193,6 +193,9 @@ class LocalObjectManager {
   // written to the object directory.
   absl::flat_hash_map<ObjectID, std::unique_ptr<RayObject>> objects_pending_spill_;
 
+  /// Objects that were spilled on this node but that are being restored.
+  /// The field is used to dedup the same restore request while restoration is in
+  /// progress.
   absl::flat_hash_set<ObjectID> objects_pending_restore_;
 
   /// The time that we last sent a FreeObjects request to other nodes for
