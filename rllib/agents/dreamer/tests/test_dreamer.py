abdcdfb81669d76aaded1ea92eae3fa0c254cf4d
sven1977 <svenmika1977@gmail.com>
2021-01-19 15:34:31 +0100

    wip.

--- rllib/agents/dreamer/tests/test_dreamer.py
+++ rllib/agents/dreamer/tests/test_dreamer.py
@@ -1,7 +1,9 @@
 import unittest
 
 import ray
+from ray import tune
 import ray.rllib.agents.dreamer as dreamer
+from ray.rllib.examples.env.dm_control_suite import hopper_hop
 from ray.rllib.utils.test_utils import check_compute_single_action, \
     framework_iterator
 
@@ -18,12 +20,13 @@ class TestDreamer(unittest.TestCase):
     def test_dreamer_compilation(self):
         """Test whether an DreamerTrainer can be built with all frameworks."""
         config = dreamer.DEFAULT_CONFIG.copy()
+        tune.register_env("dm_control_hopper_hop", lambda _: hopper_hop())
 
         num_iterations = 1
 
         # Test against all frameworks.
         for _ in framework_iterator(config, frameworks="torch"):
-            for env in ["PongDeterministic-v0"]:
+            for env in ["dm_control_hopper_hop"]:
                 trainer = dreamer.DREAMERTrainer(config=config, env=env)
                 for i in range(num_iterations):
                     results = trainer.train()
