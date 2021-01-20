7662e1450d61617708f338750b739fb7c9f370c3
Stephanie Wang <swang@cs.berkeley.edu>
2021-01-20 13:38:37 -0800

    osx build

--- src/ray/object_manager/object_manager.cc
+++ src/ray/object_manager/object_manager.cc
@@ -102,7 +102,7 @@ ObjectManager::ObjectManager(asio::io_service &main_service, const NodeID &self_
   pull_manager_.reset(new PullManager(
       self_node_id_, object_is_local, send_pull_request, restore_spilled_object_,
       get_time, config.pull_timeout_ms, available_memory,
-      [this, spill_objects_callback, object_store_full_callback]() {
+      [spill_objects_callback, object_store_full_callback]() {
         // TODO(swang): This copies the out-of-memory handling in the
         // CreateRequestQueue. It would be nice to unify these.
         if (object_store_full_callback) {
