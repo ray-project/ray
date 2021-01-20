2b9c90a5c7d36fa0004f818d14665d3f07789fcd
Kai Yang <kfstorm@outlook.com>
2021-01-20 21:10:13 +0800

    [Java] [Test] Move multi-worker config to ray.conf file

--- java/test/src/main/java/io/ray/test/KillActorTest.java
+++ java/test/src/main/java/io/ray/test/KillActorTest.java
@@ -14,17 +14,14 @@ import org.testng.annotations.Test;
 @Test(groups = {"cluster"})
 public class KillActorTest extends BaseTest {
 
-  private String oldNumWorkersPerProcess;
-
   @BeforeClass
   public void setUp() {
-    oldNumWorkersPerProcess = System.getProperty("ray.job.num-java-workers-per-process");
     System.setProperty("ray.job.num-java-workers-per-process", "1");
   }
 
   @AfterClass
   public void tearDown() {
-    System.setProperty("ray.job.num-java-workers-per-process", oldNumWorkersPerProcess);
+    System.clearProperty("ray.job.num-java-workers-per-process");
   }
 
   public static class HangActor {
