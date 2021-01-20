e8e0e899a073ae938714bfcb20f726d2f13c8e11
Amog Kamsetty <amogkamsetty@yahoo.com>
2021-01-20 10:44:56 -0800

    fix

--- python/ray/util/lightning_accelerators/tests/test_horovod_ray_accelerator.py
+++ python/ray/util/lightning_accelerators/tests/test_horovod_ray_accelerator.py
@@ -5,7 +5,8 @@ import numpy as np
 import torch
 import pytest
 import ray
-from pl_bolts.datamodules import MNISTDataModule
+from pl_bolts.datamodules.mnist_datamodule import MNISTDataModule
+from ray.util.sgd.tests.test_ptl import PTL_Module
 from ray.tune.examples.mnist_ptl_mini import LightningMNISTClassifier
 from ray.util.lightning_accelerators import HorovodRayAccelerator
 import pytorch_lightning as pl
@@ -52,85 +53,19 @@ def ray_start_2_gpus():
 def seed():
     pl.seed_everything(0)
 
-
-class LinearDataset(torch.utils.data.Dataset):
-    """y = a * x + b"""
-
-    def __init__(self, a, b, x=None, size=1000):
-        if x is None:
-            x = np.arange(0, 10, 10 / size, dtype=np.float32)
-        self.x = torch.from_numpy(x)
-        self.y = torch.from_numpy(a * x + b)
-
-    def __getitem__(self, index):
-        return self.x[index, None], self.y[index, None]
-
-    def __len__(self):
-        return len(self.x)
-
-
-class PTL_Module(pl.LightningModule):
-    def __init__(self,
-                 lr=1e-2,
+def get_model(lr=1e-2,
                  hidden_size=1,
                  data_size=10,
                  val_size=10,
                  batch_size=2):
-        super().__init__()
-        self.save_hyperparameters()
-        self.lr = lr
-        self.data_size = data_size
-        self.val_size = val_size
-        self.hidden_size = hidden_size
-        self.batch_size = batch_size
-        self.layer = torch.nn.Linear(1, hidden_size)
-
-    def forward(self, x):
-        return self.layer.forward(x)
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=self.lr)
-        return [optimizer]
-
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        #print(x, y)
-        output = self(x)
-        #print(output)
-        loss = self.loss(output, y)
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        x, y = batch
-        output = self(x)
-        loss = self.loss(output, y)
-        _, predicted = torch.max(output.data, 1)
-        num_correct = (predicted == y).sum().item()
-        num_samples = y.size(0)
-        return {"val_loss": loss.item(), "val_acc": num_correct / num_samples}
-
-    def setup(self, stage):
-        train_dataset = LinearDataset(2, 5, size=self.data_size)
-        val_dataset = LinearDataset(2, 5, size=self.val_size)
-        self.train_loader = torch.utils.data.DataLoader(
-            train_dataset,
-            batch_size=self.batch_size,
-        )
-        self.val_loader = torch.utils.data.DataLoader(
-            val_dataset, batch_size=self.batch_size)
-        self.loss = torch.nn.MSELoss()
-
-    def train_dataloader(self):
-        return self.train_loader
-
-    def val_dataloader(self):
-        return self.val_loader
-
-    def test_dataloader(self):
-        #inputs = np.random.randint(0, 10, 10).astype(np.float32)
-        inputs = np.arange(0, 10, dtype=np.float32)
-        test_dataset = LinearDataset(2, 5, x=inputs)
-        return torch.utils.data.DataLoader(test_dataset, batch_size=2)
+    config = {
+        "lr": lr,
+        "hidden_size": hidden_size,
+        "data_size": data_size,
+        "val_size": val_size,
+        "batch_size": batch_size
+    }
+    return PTL_Module(config)
 
 
 def get_trainer(dir,
@@ -151,12 +86,11 @@ def get_trainer(dir,
         progress_bar_refresh_rate=progress_bar_refresh_rate,
         checkpoint_callback=True,
         accelerator=accelerator)
-    assert False, trainer.distributed_backend
     return trainer
 
 
 def train_test(trainer, model):
-    initial_values = initial_values = torch.tensor(
+    initial_values = torch.tensor(
         [torch.sum(torch.abs(x)) for x in model.parameters()])
     result = trainer.fit(model)
     post_train_values = torch.tensor(
@@ -165,27 +99,20 @@ def train_test(trainer, model):
     # Check that the model is actually changed post-training.
     assert torch.norm(initial_values - post_train_values) > 0.1
 
-
 @pytest.mark.parametrize("num_processes", [1, 2])
 def test_train(tmpdir, ray_start_2_cpus, seed, num_processes):
-    model = PTL_Module()
+    model = get_model()
 
     trainer = get_trainer(tmpdir, num_processes=num_processes)
     train_test(trainer, model)
 
-
-@pytest.mark.skipif(
-    sys.platform == "win32",
-    reason="Horovod is not "
-    "supported on "
-    "Windows")
 @pytest.mark.skipif(
     not _nccl_available(), reason="test requires Horovod with NCCL support")
 @pytest.mark.skipif(
     torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
 @pytest.mark.parametrize("num_gpus", [1, 2])
 def test_train_gpu(tmpdir, ray_start_2_gpus, seed, num_gpus):
-    model = PTL_Module()
+    model = get_model()
     trainer = get_trainer(tmpdir, gpus=num_gpus)
     train_test(trainer, model)
 
@@ -193,29 +120,23 @@ def test_train_gpu(tmpdir, ray_start_2_gpus, seed, num_gpus):
 def load_test(trainer, model):
     trainer.fit(model)
     trained_model = PTL_Module.load_from_checkpoint(
-        trainer.checkpoint_callback.best_model_path)
+        trainer.checkpoint_callback.best_model_path, config=model.config)
     assert trained_model is not None, 'loading model failed'
-
 
 @pytest.mark.parametrize("num_processes", [1, 2])
 def test_load(tmpdir, ray_start_2_cpus, seed, num_processes):
-    model = PTL_Module()
+    model = get_model()
     trainer = get_trainer(tmpdir, num_processes=num_processes)
     load_test(trainer, model)
 
 
-@pytest.mark.skipif(
-    sys.platform == "win32",
-    reason="Horovod is not "
-    "supported on "
-    "Windows")
 @pytest.mark.skipif(
     not _nccl_available(), reason="test requires Horovod with NCCL support")
 @pytest.mark.skipif(
     torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
 @pytest.mark.parametrize("num_gpus", [1, 2])
 def test_load_gpu(tmpdir, ray_start_2_gpus, seed, num_gpus):
-    model = PTL_Module()
+    model = get_model()
     trainer = get_trainer(tmpdir, gpus=num_gpus)
     load_test(trainer, model)
 
