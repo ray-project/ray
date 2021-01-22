186298833762e229561b2b3c2e6ad07597a446e9
Amog Kamsetty <amogkamsetty@yahoo.com>
2021-01-21 18:16:07 -0800

    add gpu

--- python/ray/util/lightning_accelerators/tests/test_horovod_ray_accelerator.py
+++ python/ray/util/lightning_accelerators/tests/test_horovod_ray_accelerator.py
@@ -45,7 +45,7 @@ def ray_start_2_gpus():
     # This env var is set by Pytorch Lightning.
     # Make sure to reset it after each test.
     #TODO: Upstream to PTL to not set this env var if using Ray.
-    #del os.environ["CUDA_VISIBLE_DEVICES"]
+    del os.environ["CUDA_VISIBLE_DEVICES"]
 
 
 @pytest.fixture
@@ -82,7 +82,7 @@ def get_trainer(dir,
     trainer = pl.Trainer(
         default_root_dir=dir,
         #num_processes=num_processes,
-        #gpus=gpus,
+        gpus=1 if use_gpu else 0,
         max_epochs=max_epochs,
         limit_train_batches=limit_train_batches,
         limit_val_batches=limit_val_batches,
