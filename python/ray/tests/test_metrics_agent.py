2da0c52d64c5c5f73e5fc546c6e050d8d151a7f0
architkulkarni <arkulkar@gmail.com>
2021-01-22 00:16:30 -0800

    move e2e test to top of file to avoid shutdown bug

--- python/ray/tests/test_metrics_agent.py
+++ python/ray/tests/test_metrics_agent.py
@@ -14,54 +14,6 @@ from ray.util.metrics import Count, Histogram, Gauge
 from ray.test_utils import wait_for_condition, SignalActor, fetch_prometheus
 
 
-def test_prometheus_file_based_service_discovery(ray_start_cluster):
-    # Make sure Prometheus service discovery file is correctly written
-    # when number of nodes are dynamically changed.
-    NUM_NODES = 5
-    cluster = ray_start_cluster
-    nodes = [cluster.add_node() for _ in range(NUM_NODES)]
-    cluster.wait_for_nodes()
-    addr = ray.init(address=cluster.address)
-    redis_address = addr["redis_address"]
-    writer = PrometheusServiceDiscoveryWriter(
-        redis_address, ray.ray_constants.REDIS_DEFAULT_PASSWORD, "/tmp/ray")
-
-    def get_metrics_export_address_from_node(nodes):
-        return [
-            "{}:{}".format(node.node_ip_address, node.metrics_export_port)
-            for node in nodes
-        ]
-
-    loaded_json_data = json.loads(writer.get_file_discovery_content())[0]
-    assert (set(get_metrics_export_address_from_node(nodes)) == set(
-        loaded_json_data["targets"]))
-
-    # Let's update nodes.
-    for _ in range(3):
-        nodes.append(cluster.add_node())
-
-    # Make sure service discovery file content is correctly updated.
-    loaded_json_data = json.loads(writer.get_file_discovery_content())[0]
-    assert (set(get_metrics_export_address_from_node(nodes)) == set(
-        loaded_json_data["targets"]))
-
-
-@pytest.mark.skipif(
-    platform.system() == "Windows", reason="Failing on Windows.")
-def test_prome_file_discovery_run_by_dashboard(shutdown_only):
-    ray.init(num_cpus=0)
-    global_node = ray.worker._global_node
-    temp_dir = global_node.get_temp_dir_path()
-
-    def is_service_discovery_exist():
-        for path in pathlib.Path(temp_dir).iterdir():
-            if PROMETHEUS_SERVICE_DISCOVERY_FILE in str(path):
-                return True
-        return False
-
-    wait_for_condition(is_service_discovery_exist)
-
-
 @pytest.fixture
 def _setup_cluster_for_test(ray_start_cluster):
     NUM_NODES = 2
@@ -188,6 +140,54 @@ def test_metrics_export_end_to_end(_setup_cluster_for_test):
         print(
             f"The components are {pformat(fetch_prometheus(prom_addresses))}")
         test_cases()  # Should fail assert
+
+
+def test_prometheus_file_based_service_discovery(ray_start_cluster):
+    # Make sure Prometheus service discovery file is correctly written
+    # when number of nodes are dynamically changed.
+    NUM_NODES = 5
+    cluster = ray_start_cluster
+    nodes = [cluster.add_node() for _ in range(NUM_NODES)]
+    cluster.wait_for_nodes()
+    addr = ray.init(address=cluster.address)
+    redis_address = addr["redis_address"]
+    writer = PrometheusServiceDiscoveryWriter(
+        redis_address, ray.ray_constants.REDIS_DEFAULT_PASSWORD, "/tmp/ray")
+
+    def get_metrics_export_address_from_node(nodes):
+        return [
+            "{}:{}".format(node.node_ip_address, node.metrics_export_port)
+            for node in nodes
+        ]
+
+    loaded_json_data = json.loads(writer.get_file_discovery_content())[0]
+    assert (set(get_metrics_export_address_from_node(nodes)) == set(
+        loaded_json_data["targets"]))
+
+    # Let's update nodes.
+    for _ in range(3):
+        nodes.append(cluster.add_node())
+
+    # Make sure service discovery file content is correctly updated.
+    loaded_json_data = json.loads(writer.get_file_discovery_content())[0]
+    assert (set(get_metrics_export_address_from_node(nodes)) == set(
+        loaded_json_data["targets"]))
+
+
+@pytest.mark.skipif(
+    platform.system() == "Windows", reason="Failing on Windows.")
+def test_prome_file_discovery_run_by_dashboard(shutdown_only):
+    ray.init(num_cpus=0)
+    global_node = ray.worker._global_node
+    temp_dir = global_node.get_temp_dir_path()
+
+    def is_service_discovery_exist():
+        for path in pathlib.Path(temp_dir).iterdir():
+            if PROMETHEUS_SERVICE_DISCOVERY_FILE in str(path):
+                return True
+        return False
+
+    wait_for_condition(is_service_discovery_exist)
 
 
 @pytest.fixture
