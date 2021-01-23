f0ceac751b0854630b0fd49939c41333692415fc
Barak Michener <me@barakmich.com>
2021-01-23 02:05:09 +0000

    fix tests, split init test
    Change-Id: Ie691ed9ac082639c583eec4f6af7578b0841c744

--- python/ray/tests/test_client.py
+++ python/ray/tests/test_client.py
@@ -10,37 +10,6 @@ from ray.util.client.common import ClientObjectRef
 from ray.util.client.ray_client_helpers import ray_start_client_server
 
 
-def test_num_clients(ray_start_regular_shared):
-    # Tests num clients reporting; useful if you want to build an app that
-    # load balances clients between Ray client servers.
-    server = ray_client_server.serve("localhost:50051")
-    try:
-        api1 = RayAPIStub()
-        info1 = api1.connect("localhost:50051")
-        assert info1["num_clients"] == 1, info1
-        api2 = RayAPIStub()
-        info2 = api2.connect("localhost:50051")
-        assert info2["num_clients"] == 2, info2
-
-        # Disconnect the first two clients.
-        api1.disconnect()
-        api2.disconnect()
-        time.sleep(1)
-
-        api3 = RayAPIStub()
-        info3 = api3.connect("localhost:50051")
-        assert info3["num_clients"] == 1, info3
-
-        # Check info contains ray and python version.
-        assert isinstance(info3["ray_version"], str), info3
-        assert isinstance(info3["ray_commit"], str), info3
-        assert isinstance(info3["python_version"], str), info3
-        api3.disconnect()
-    finally:
-        server.stop(0)
-        time.sleep(1)
-
-
 @pytest.mark.skipif(sys.platform == "win32", reason="Failing on Windows.")
 def test_real_ray_fallback(ray_start_regular_shared):
     with ray_start_client_server() as ray:
@@ -378,20 +347,23 @@ def test_internal_kv(ray_start_regular_shared):
 
 def test_startup_retry(ray_start_regular_shared):
     from ray.util.client import ray as ray_client
+    ray_client._inside_client_test = True
+
     with pytest.raises(ConnectionError):
         ray_client.connect("localhost:50051", connection_retries=1)
 
-    def run_server(result):
-        time.sleep(3)
-        result["server"] = ray_client_server.serve("localhost:50051")
+    def run_client():
+        ray_client.connect("localhost:50051")
+        ray_client.disconnect()
 
-    result = {}
-    thread = threading.Thread(target=run_server, args=(result, ))
+
+    thread = threading.Thread(target=run_client, daemon=True)
     thread.start()
-    ray_client.connect("localhost:50051")
-    ray_client.disconnect()
+    time.sleep(3)
+    server = ray_client_server.serve("localhost:50051")
     thread.join()
-    result["server"].stop(0)
+    server.stop(0)
+    ray_client._inside_client_test = False
 
 
 if __name__ == "__main__":
