e675e5b75a4470c01b4df577d4028b00e01d3d53
Barak Michener <me@barakmich.com>
2021-01-23 23:11:39 -0800

    [ray_client]: Add more retry logic (#13478)

--- python/ray/util/client/worker.py
+++ python/ray/util/client/worker.py
@@ -5,6 +5,7 @@ to the server.
 import base64
 import json
 import logging
+import time
 import uuid
 from collections import defaultdict
 from typing import Any
@@ -33,6 +34,13 @@ INITIAL_TIMEOUT_SEC = 5
 MAX_TIMEOUT_SEC = 30
 
 
+def backoff(timeout: int) -> int:
+    timeout = timeout + 5
+    if timeout > MAX_TIMEOUT_SEC:
+        timeout = MAX_TIMEOUT_SEC
+    return timeout
+
+
 class Worker:
     def __init__(self,
                  conn_str: str = "",
@@ -59,23 +67,59 @@ class Worker:
         else:
             self.channel = grpc.insecure_channel(conn_str)
 
+        # Retry the connection until the channel responds to something
+        # looking like a gRPC connection, though it may be a proxy.
         conn_attempts = 0
         timeout = INITIAL_TIMEOUT_SEC
-        while conn_attempts < connection_retries + 1:
+        ray_ready = False
+        while conn_attempts < max(connection_retries, 1):
             conn_attempts += 1
             try:
+                # Let gRPC wait for us to see if the channel becomes ready.
+                # If it throws, we couldn't connect.
                 grpc.channel_ready_future(self.channel).result(timeout=timeout)
-                break
+                # The HTTP2 channel is ready. Wrap the channel with the
+                # RayletDriverStub, allowing for unary requests.
+                self.server = ray_client_pb2_grpc.RayletDriverStub(
+                    self.channel)
+                # Now the HTTP2 channel is ready, or proxied, but the
+                # servicer may not be ready. Call is_initialized() and if
+                # it throws, the servicer is not ready. On success, the
+                # `ray_ready` result is checked.
+                ray_ready = self.is_initialized()
+                if ray_ready:
+                    # Ray is ready! Break out of the retry loop
+                    break
+                # Ray is not ready yet, wait a timeout
+                time.sleep(timeout)
             except grpc.FutureTimeoutError:
-                if conn_attempts >= connection_retries:
-                    raise ConnectionError("ray client connection timeout")
-                logger.info(f"Couldn't connect in {timeout} seconds, retrying")
-                timeout = timeout + 5
-                if timeout > MAX_TIMEOUT_SEC:
-                    timeout = MAX_TIMEOUT_SEC
+                logger.info(
+                    f"Couldn't connect channel in {timeout} seconds, retrying")
+                # Note that channel_ready_future constitutes its own timeout,
+                # which is why we do not sleep here.
+            except grpc.RpcError as e:
+                if e.code() == grpc.StatusCode.UNAVAILABLE:
+                    # UNAVAILABLE is gRPC's retryable error,
+                    # so we do that here.
+                    logger.info("Ray client server unavailable, "
+                                f"retrying in {timeout}s...")
+                    logger.debug(f"Received when checking init: {e.details()}")
+                    # Ray is not ready yet, wait a timeout
+                    time.sleep(timeout)
+                else:
+                    # Any other gRPC error gets a reraise
+                    raise e
+            # Fallthrough, backoff, and retry at the top of the loop
+            logger.info("Waiting for Ray to become ready on the server, "
+                        f"retry in {timeout}s...")
+            timeout = backoff(timeout)
 
-        self.server = ray_client_pb2_grpc.RayletDriverStub(self.channel)
+        # If we made it through the loop without ray_ready it means we've used
+        # up our retries and should error back to the user.
+        if not ray_ready:
+            raise ConnectionError("ray client connection timeout")
 
+        # Initialize the streams to finish protocol negotiation.
         self.data_client = DataClient(self.channel, self._client_id,
                                       self.metadata)
         self.reference_count: Dict[bytes, int] = defaultdict(int)
