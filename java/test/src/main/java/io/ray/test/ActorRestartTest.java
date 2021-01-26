dfd99fe6e90537b7db7e172ee32627d705d5f090
Amog Kamsetty <amogkamsetty@yahoo.com>
2021-01-25 16:48:14 -0800

    formatting

--- java/test/src/main/java/io/ray/test/ActorRestartTest.java
+++ java/test/src/main/java/io/ray/test/ActorRestartTest.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-@Test(groups = {"cluster"}, enabled=false)
+@Test(groups = {"cluster"}, enabled = false)
 public class ActorRestartTest extends BaseTest {
 
   public static class Counter {
--- java/test/src/main/java/io/ray/test/ExitActorTest.java
+++ java/test/src/main/java/io/ray/test/ExitActorTest.java
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-@Test(groups = {"cluster"}, enabled=false)
+@Test(groups = {"cluster"}, enabled = false)
 public class ExitActorTest extends BaseTest {
 
   private static class ExitingActor {
