fe8b681adc2354cbe11c4a1607587204b98c318d
simon-mo <simon.mo@hey.com>
2021-01-27 11:36:40 -0800

    [CI] Add retry to java doc test
    Should help resolve https://github.com/ray-project/ray/issues/13742
    but it's retreating the symptom not the cause

--- java/test.sh
+++ java/test.sh
@@ -34,6 +34,32 @@ run_testng() {
     fi
 }
 
+# https://unix.stackexchange.com/questions/82598/how-do-i-write-a-retry-logic-in-script-to-keep-retrying-to-run-it-upto-5-times
+function fail {
+  echo "$1" >&2
+  exit 1
+}
+
+function retry {
+  local n=1
+  local max=3
+  local delay=0
+  while true; do
+    if "$@"; then
+      break
+    else
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed. Attempt $n/$max:"
+        sleep $delay;
+      else
+        fail "The command has failed after $n attempts."
+        break
+      fi
+    fi
+  done
+}
+
 pushd "$ROOT_DIR"/..
 echo "Build java maven deps."
 bazel build //java:gen_maven_deps
@@ -77,7 +103,7 @@ for file in "$docdemo_path"*.java; do
   file=${file#"$docdemo_path"}
   class=${file%".java"}
   echo "Running $class"
-  java -cp bazel-bin/java/all_tests_deploy.jar "io.ray.docdemo.$class"
+  retry java -cp bazel-bin/java/all_tests_deploy.jar "io.ray.docdemo.$class"
 done
 popd
 
