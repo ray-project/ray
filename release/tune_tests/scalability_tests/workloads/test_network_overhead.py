58adb5be78d2e0483fe563db4fbb85502f28311c
Kai Fricke <kai@anyscale.com>
2021-01-28 15:19:25 +0100

    Network overhead cluster

--- release/tune_tests/scalability_tests/workloads/test_network_overhead.py
+++ release/tune_tests/scalability_tests/workloads/test_network_overhead.py
@@ -1,6 +1,5 @@
 import ray
 from ray import tune
-from ray.tune.cluster_info import is_ray_cluster
 
 from _trainable import timed_tune_run
 
@@ -14,16 +13,13 @@ def main():
 
     max_runtime = 500
 
-    if is_ray_cluster():
-        # Add constant overhead for SSH connection
-        max_runtime = 500
-
     timed_tune_run(
         name="result network overhead",
         num_samples=num_samples,
         results_per_second=results_per_second,
         trial_length_s=trial_length_s,
         max_runtime=max_runtime,
+        resources_per_trial={"cpu": 2},  # One per node
         sync_config=tune.SyncConfig(sync_to_driver=True))
 
 
