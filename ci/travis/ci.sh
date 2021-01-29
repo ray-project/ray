194656731dea7c22deeadf12ebd4a21bffefac26
Simon Mo <simon.mo@hey.com>
2021-01-29 15:47:21 -0800

    [CI] Deflake test_basics and skip test_component_failures_3 (#13801)

--- ci/travis/ci.sh
+++ ci/travis/ci.sh
@@ -145,8 +145,11 @@ test_python() {
       -python/ray/tests:test_advanced_3  # test_invalid_unicode_in_worker_log() fails on Windows
       -python/ray/tests:test_autoscaler_aws
       -python/ray/tests:test_component_failures
+      -python/ray/tests:test_component_failures_3 # timeout
       -python/ray/tests:test_basic_2  # hangs on shared cluster tests
       -python/ray/tests:test_basic_2_client_mode
+      -python/ray/tests:test_basic_3  # timeout
+      -python/ray/tests:test_basic_3_client_mode
       -python/ray/tests:test_cli
       -python/ray/tests:test_failure
       -python/ray/tests:test_global_gc
