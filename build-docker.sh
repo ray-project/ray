69a1a06a2a5c0f059b80c72c0e2c7990182642c2
Holden Karau <holden@pigscanfly.ca>
2021-01-29 16:58:09 -0800

    Cross-build of base-deps works
    Get this working on ARM

--- build-docker.sh
+++ build-docker.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 # This script is for users to build docker images locally. It is most useful for users wishing to edit the
-# base-deps, ray-deps, or ray images. This script is *not* tested, so please look at the 
+# base-deps, ray-deps, or ray images. This script is *not* tested, so please look at the
 # scripts/build-docker-images.py if there are problems with using this script.
 
-set -x
+set -ex
 
 GPU=""
-BASE_IMAGE="ubuntu:focal"
-WHEEL_URL="https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-2.0.0.dev0-cp37-cp37m-manylinux2014_x86_64.whl"
-PYTHON_VERSION="3.7.7"
-
+BASE_IMAGE=${BASE_IMAGE:-"ubuntu:focal"}
+DOCKER_PREFIX=${DOCKER_PREFIX:-"rayproject/"}
 
 while [[ $# -gt 0 ]]
 do
@@ -29,8 +27,8 @@ case $key in
     BUILD_EXAMPLES=YES
     ;;
     --shas-only)
-    # output the SHA sum of each build. This is useful for scripting tests, 
-    # especially when builds of different versions are running on the same machine. 
+    # output the SHA sum of each build. This is useful for scripting tests,
+    # especially when builds of different versions are running on the same machine.
     # It also can facilitate cleanup.
     OUTPUT_SHA=YES
     ;;
@@ -39,56 +37,35 @@ case $key in
     echo "not implemented, just hardcode me :'("
     exit 1
     ;;
-    --python-version)
-    # Python version to install. e.g. 3.7.7.
-    # Changing python versions may require a different wheel.
-    # If not provided defaults to 3.7.7
-    shift
-    PYTHON_VERSION=$1
-    ;;
     *)
-    echo "Usage: build-docker.sh [ --no-cache-build ] [ --shas-only ] [ --build-development-image ] [ --build-examples ] [ --wheel-to-use ] [ --python-version ]"
+    echo "Usage: build-docker.sh [ --no-cache-build ] [ --shas-only ] [ --build-development-image ] [ --build-examples ] [ --wheel-to-use ]"
     exit 1
 esac
 shift
 done
 
-WHEEL_DIR=$(mktemp -d)
-wget --quiet "$WHEEL_URL" -P "$WHEEL_DIR"
-WHEEL="$WHEEL_DIR/$(basename "$WHEEL_DIR"/*.whl)"
 # Build base-deps, ray-deps, and ray.
-for IMAGE in "base-deps" "ray-deps" "ray"
+#for IMAGE in "base-deps" "ray-deps" "ray"
+for IMAGE in "ray"
 do
-    cp "$WHEEL" "docker/$IMAGE/$(basename "$WHEEL")"
+  rm ./docker/${IMAGE}/ray.tar ./docker/${IMAGE}/git-rev ./docker/${IMAGE}/requirements.txt || echo "No need to cleanup"
+  git rev-parse HEAD > ./docker/${IMAGE}/git-rev
+  git archive -o ./docker/${IMAGE}/ray.tar "$(git rev-parse HEAD)"
+  cp ./python/requirements.txt ./docker/${IMAGE}/requirements.txt
+    IMAGE_SHA=$(docker buildx build $NO_CACHE --build-arg GPU="$GPU" --build-arg BASE_IMAGE="$BASE_IMAGE" --build-arg DOCKER_PREFIX="$DOCKER_PREFIX"  -t ${DOCKER_PREFIX}$IMAGE:nightly$GPU --platform linux/arm64,linux/amd64 --push docker/$IMAGE )
     if [ $OUTPUT_SHA ]; then
-        IMAGE_SHA=$(docker build $NO_CACHE --build-arg GPU="$GPU" --build-arg BASE_IMAGE="$BASE_IMAGE" --build-arg WHEEL_PATH="$(basename "$WHEEL")" --build-arg PYTHON_VERSION="$PYTHON_VERSION" -q -t rayproject/$IMAGE:nightly$GPU docker/$IMAGE)
-        echo "rayproject/$IMAGE:nightly$GPU SHA:$IMAGE_SHA"
-    else
-        docker build $NO_CACHE  --build-arg GPU="$GPU" --build-arg BASE_IMAGE="$BASE_IMAGE" --build-arg WHEEL_PATH="$(basename "$WHEEL")" --build-arg PYTHON_VERSION="$PYTHON_VERSION" -t rayproject/$IMAGE:nightly$GPU docker/$IMAGE
+	echo "rayproject/$IMAGE:nightly$GPU SHA:$IMAGE_SHA"
     fi
-    rm "docker/$IMAGE/$(basename "$WHEEL")"
-done 
+    rm ./docker/${IMAGE}/ray.tar ./docker/${IMAGE}/git-rev ./docker/${IMAGE}/requirements.txt
+done
 
 
-# Build the current Ray source
-if [ $BUILD_DEV ]; then 
-    git rev-parse HEAD > ./docker/development/git-rev
-    git archive -o ./docker/development/ray.tar "$(git rev-parse HEAD)"
+if [ $BUILD_EXAMPLES ]; then
     if [ $OUTPUT_SHA ]; then
-        IMAGE_SHA=$(docker build --no-cache -q -t rayproject/development docker/development)
-        echo "rayproject/development:latest SHA:$IMAGE_SHA"
+	IMAGE_SHA=$(docker build $NO_CACHE -q -t rayproject/examples docker/examples)
+	echo "rayproject/examples:latest SHA:$IMAGE_SHA"
     else
-        docker build --no-cache -t rayproject/development docker/development
-    fi
-    rm ./docker/development/ray.tar ./docker/development/git-rev
-fi
-
-if [ $BUILD_EXAMPLES ]; then 
-    if [ $OUTPUT_SHA ]; then
-        IMAGE_SHA=$(docker build $NO_CACHE -q -t rayproject/examples docker/examples)
-        echo "rayproject/examples:latest SHA:$IMAGE_SHA"
-    else
-        docker build $NO_CACHE -t rayproject/examples docker/examples
+	docker buildx build $NO_CACHE -t ${DOCKER_PREFIX}examples --platform linux/arm64,linux/amd64 --push docker/examples
     fi
 fi
 
