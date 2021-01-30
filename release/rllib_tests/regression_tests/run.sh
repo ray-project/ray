ceb60965aee4c9fdc8cd1c4d752b1fe6871c16d3
Alex <alex@anyscale.com>
2021-01-30 04:32:18 +0000

    rllib regression

--- release/rllib_tests/regression_tests/run.sh
+++ release/rllib_tests/regression_tests/run.sh
@@ -30,18 +30,13 @@ case "$i" in
 esac
 done
 
-if [[ $ray_version == "" || $commit == "" || $ray_branch == "" ]]
-then
-    echo "Provide --ray-version, --commit, and --ray-branch"
-    exit 1
-fi
-
 echo "version: $ray_version"
 echo "commit: $commit"
 echo "branch: $ray_branch"
 echo "workload: ignored"
 
 wheel="https://s3-us-west-2.amazonaws.com/ray-wheels/$ray_branch/$commit/ray-$ray_version-cp37-cp37m-manylinux2014_x86_64.whl"
+wheel="https://ray-wheels.s3-us-west-2.amazonaws.com/releases/1.2.0/b4d87b8fc5ff03b868adba35b37da32b7be9f71f/ray-1.2.0-cp37-cp37m-manylinux2014_x86_64.whl"
 
 conda uninstall -y terminado
 pip install -U pip
@@ -50,6 +45,8 @@ pip install "ray[rllib]" "ray"
 pip install terminado
 pip install torch==1.6 torchvision
 pip install boto3==1.4.8 cython==0.29.0
+
+ray stop
 
 # Run tf learning tests.
 rllib train -f compact-regression-tests-tf.yaml
