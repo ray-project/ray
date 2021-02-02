c61d1377a4d716b832822d7636332b7491de5187
Kai Fricke <kai@anyscale.com>
2021-02-02 14:42:01 +0100

    Update xgboost tests

--- release/xgboost_tests/workloads/_train.py
+++ release/xgboost_tests/workloads/_train.py
@@ -19,6 +19,7 @@ def train_ray(path,
               ray_params=None,
               xgboost_params=None,
               **kwargs):
+    path = os.path.expanduser(path)
     if not os.path.exists(path):
         raise ValueError(f"Path does not exist: {path}")
 
