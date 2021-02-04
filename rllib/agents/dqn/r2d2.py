5e7b0cf4bd13845a77c6ffd3692d6f7b1d61457e
sven1977 <svenmika1977@gmail.com>
2021-02-04 15:06:31 +0100

    wip.

---
+++ rllib/agents/dqn/r2d2.py
@@ -0,0 +1,195 @@
+"""
+Recurrent Experience Replay in Distributed Reinforcement Learning (R2D2)
+========================================================================
+
+This file defines the distributed Trainer class for the R2D2
+algorithm. See `r2d2_[tf|torch]_policy.py` for the definition of the policies.
+
+Detailed documentation:
+https://docs.ray.io/en/master/rllib-algorithms.html#deep-q-networks-dqn-rainbow-parametric-dqn
+"""  # noqa: E501
+#TODO: ^^ update documentation link
+
+import logging
+from typing import List, Optional, Type
+
+import ray.rllib.agents.dqn as dqn
+from ray.rllib.agents.dqn.r2d2_torch_policy import R2D2TorchPolicy
+from ray.rllib.evaluation.worker_set import WorkerSet
+from ray.rllib.execution.concurrency_ops import Concurrently
+from ray.rllib.execution.metric_ops import StandardMetricsReporting
+from ray.rllib.execution.replay_buffer import LocalReplayBuffer
+from ray.rllib.execution.replay_ops import Replay, StoreToReplayBuffer
+from ray.rllib.execution.rollout_ops import ParallelRollouts
+from ray.rllib.execution.train_ops import TrainOneStep, UpdateTargetNetwork
+from ray.rllib.policy.policy import LEARNER_STATS_KEY, Policy
+from ray.rllib.utils.typing import TrainerConfigDict
+from ray.util.iter import LocalIterator
+
+logger = logging.getLogger(__name__)
+
+# yapf: disable
+# __sphinx_doc_begin__
+DEFAULT_CONFIG = dqn.DQNTrainer.merge_trainer_configs(
+    dqn.DEFAULT_CONFIG,  # See keys in impala.py, which are also supported.
+    {
+        # If True, assume a zero-initialized state input (no matter where in the episode
+        # the sequence is located).
+        # If False, store the initial states along with each SampleBatch, use it (as
+        # initial state when running through the network for training), and update that
+        # initial state during training (from the internal state outputs of the
+        # immediately preceding sequence).
+        "zero_init_states": True,
+        # If > 0, use the `burn_in` first steps of each replay-sampled sequence
+        # (starting either from all 0.0-values if `zero_init_state=True` or from the
+        # already stored values) to calculate an even more accurate initial states for
+        # the actual sequence (starting after this burn-in window). In the burn-in case,
+        # the actual length of the sequence used for loss calculation is `n - burn_in`
+        # time steps (n=LSTM’s/attention net’s max_seq_len).
+        "burn_in": 0,
+    },
+    _allow_unknown_configs=True,
+)
+# __sphinx_doc_end__
+# yapf: enable
+
+
+def validate_config(config: TrainerConfigDict) -> None:
+    """Checks and updates the config based on settings.
+
+    Rewrites rollout_fragment_length to take into account n_step truncation.
+    """
+    #if config["exploration_config"]["type"] == "ParameterNoise":
+    #    if config["batch_mode"] != "complete_episodes":
+    #        logger.warning(
+    #            "ParameterNoise Exploration requires `batch_mode` to be "
+    #            "'complete_episodes'. Setting batch_mode=complete_episodes.")
+    #        config["batch_mode"] = "complete_episodes"
+    #    if config.get("noisy", False):
+    #        raise ValueError(
+    #            "ParameterNoise Exploration and `noisy` network cannot be "
+    #            "used at the same time!")
+
+    # Update effective batch size to include n-step
+    #adjusted_batch_size = max(config["rollout_fragment_length"],
+    #                          config.get("n_step", 1))
+    #config["rollout_fragment_length"] = adjusted_batch_size
+
+    #if config.get("prioritized_replay"):
+    #    if config["multiagent"]["replay_mode"] == "lockstep":
+    #        raise ValueError("Prioritized replay is not supported when "
+    #                         "replay_mode=lockstep.")
+    #    elif config["replay_sequence_length"] > 1:
+    #        raise ValueError("Prioritized replay is not supported when "
+    #                         "replay_sequence_length > 1.")
+
+
+def execution_plan(workers: WorkerSet,
+                   config: TrainerConfigDict) -> LocalIterator[dict]:
+    """Execution plan of the DQN algorithm. Defines the distributed dataflow.
+
+    Args:
+        workers (WorkerSet): The WorkerSet for training the Polic(y/ies)
+            of the Trainer.
+        config (TrainerConfigDict): The trainer's configuration dict.
+
+    Returns:
+        LocalIterator[dict]: A local iterator over training metrics.
+    """
+    if config.get("prioritized_replay"):
+        prio_args = {
+            "prioritized_replay_alpha": config["prioritized_replay_alpha"],
+            "prioritized_replay_beta": config["prioritized_replay_beta"],
+            "prioritized_replay_eps": config["prioritized_replay_eps"],
+        }
+    else:
+        prio_args = {}
+
+    local_replay_buffer = LocalReplayBuffer(
+        num_shards=1,
+        learning_starts=config["learning_starts"],
+        buffer_size=config["buffer_size"],
+        replay_batch_size=config["train_batch_size"],
+        replay_mode=config["multiagent"]["replay_mode"],
+        replay_sequence_length=config["replay_sequence_length"],
+        **prio_args)
+
+    rollouts = ParallelRollouts(workers, mode="bulk_sync")
+
+    # We execute the following steps concurrently:
+    # (1) Generate rollouts and store them in our local replay buffer. Calling
+    # next() on store_op drives this.
+    store_op = rollouts.for_each(
+        StoreToReplayBuffer(local_buffer=local_replay_buffer))
+
+    def update_prio(item):
+        samples, info_dict = item
+        if config.get("prioritized_replay"):
+            prio_dict = {}
+            for policy_id, info in info_dict.items():
+                # TODO(sven): This is currently structured differently for
+                #  torch/tf. Clean up these results/info dicts across
+                #  policies (note: fixing this in torch_policy.py will
+                #  break e.g. DDPPO!).
+                td_error = info.get("td_error",
+                                    info[LEARNER_STATS_KEY].get("td_error"))
+                prio_dict[policy_id] = (samples.policy_batches[policy_id]
+                                        .data.get("batch_indexes"), td_error)
+            local_replay_buffer.update_priorities(prio_dict)
+        return info_dict
+
+    # (2) Read and train on experiences from the replay buffer. Every batch
+    # returned from the LocalReplay() iterator is passed to TrainOneStep to
+    # take a SGD step, and then we decide whether to update the target network.
+    post_fn = config.get("before_learn_on_batch") or (lambda b, *a: b)
+    replay_op = Replay(local_buffer=local_replay_buffer) \
+        .for_each(lambda x: post_fn(x, workers, config)) \
+        .for_each(TrainOneStep(workers)) \
+        .for_each(update_prio) \
+        .for_each(UpdateTargetNetwork(
+            workers, config["target_network_update_freq"]))
+
+    # Alternate deterministically between (1) and (2). Only return the output
+    # of (2) since training metrics are not available until (2) runs.
+    train_op = Concurrently(
+        [store_op, replay_op],
+        mode="round_robin",
+        output_indexes=[1],
+        round_robin_weights=calculate_rr_weights(config))
+
+    return StandardMetricsReporting(train_op, workers, config)
+
+
+def calculate_rr_weights(config: TrainerConfigDict) -> List[float]:
+    """Calculate the round robin weights for the rollout and train steps"""
+    if not config["training_intensity"]:
+        return [1, 1]
+    # e.g., 32 / 4 -> native ratio of 8.0
+    native_ratio = (
+        config["train_batch_size"] / config["rollout_fragment_length"])
+    # Training intensity is specified in terms of
+    # (steps_replayed / steps_sampled), so adjust for the native ratio.
+    weights = [1, config["training_intensity"] / native_ratio]
+    return weights
+
+
+def get_policy_class(config: TrainerConfigDict) -> Optional[Type[Policy]]:
+    """Policy class picker function. Class is chosen based on DL-framework.
+
+    Args:
+        config (TrainerConfigDict): The trainer's configuration dict.
+
+    Returns:
+        Optional[Type[Policy]]: The Policy class to use with DQNTrainer.
+            If None, use `default_policy` provided in build_trainer().
+    """
+    if config["framework"] == "torch":
+        return R2D2TorchPolicy
+
+
+# Build a DQN trainer, which uses the framework specific Policy
+# determined in `get_policy_class()` above.
+R2D2Trainer = dqn.DQNTrainer.with_updates(
+    name="DQN", default_policy=R2D2TorchPolicy, default_config=DEFAULT_CONFIG)
+
+#TODO ^^TFPolicy
---
+++ rllib/agents/dqn/tests/test_r2d2.py
@@ -0,0 +1,34 @@
+import unittest
+
+import ray.rllib.agents.dqn as dqn
+from ray.rllib.utils.framework import try_import_tf
+from ray.rllib.utils.test_utils import check_compute_single_action, \
+    framework_iterator
+
+tf1, tf, tfv = try_import_tf()
+
+
+class TestR2D2(unittest.TestCase):
+    def test_r2d2_compilation(self):
+        """Test whether a SimpleQTrainer can be built on all frameworks."""
+        config = dqn.R2D2_DEFAULT_CONFIG.copy()
+        config["num_workers"] = 0  # Run locally.
+        config["model"]["use_lstm"] = True  # Wrap with an LSTM.
+        num_iterations = 2
+
+        for _ in framework_iterator(config, frameworks="torch"):
+            trainer = dqn.R2D2Trainer(config=config, env="CartPole-v0")
+            rw = trainer.workers.local_worker()
+            for i in range(num_iterations):
+                sb = rw.sample()
+                assert sb.count == config["rollout_fragment_length"]
+                results = trainer.train()
+                print(results)
+
+            check_compute_single_action(trainer)
+
+
+if __name__ == "__main__":
+    import pytest
+    import sys
+    sys.exit(pytest.main(["-v", __file__]))
