641571abe352c2a266fa2be8824106a0afd66fec
Clark Zinzow <clarkzinzow@gmail.com>
2021-02-05 17:59:12 -0700

    Lint failure once again

--- python/ray/tests/test_dask_scheduler.py
+++ python/ray/tests/test_dask_scheduler.py
@@ -36,7 +36,8 @@ def test_ray_dask_persist(ray_start_regular_shared):
     arr = da.ones(5) + 2
     result = arr.persist(scheduler=ray_dask_get)
     np.testing.assert_array_equal(
-        next(iter(result.dask.values())), np.ones(5) + 2)
+        next(iter(result.dask.values())),
+        np.ones(5) + 2)
 
 
 if __name__ == "__main__":
