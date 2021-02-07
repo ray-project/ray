64ad536584fabe4718dedc773b58a1f17cab26d9
Richard Liaw <rliaw@berkeley.edu>
2021-02-07 15:38:14 -0800

    fix
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>

--- python/ray/util/client/ray_client_helpers.py
+++ python/ray/util/client/ray_client_helpers.py
@@ -22,3 +22,22 @@ def ray_start_client_server_pair():
         ray._inside_client_test = False
         ray.disconnect()
         server.stop(0)
+
+
+@contextmanager
+def ray_start_cluster_client_server_pair(address):
+    ray._inside_client_test = True
+
+    def ray_connect_handler():
+        import ray
+        ray.init(address=address)
+
+    server = ray_client_server.serve(
+        "localhost:50051", ray_connect_handler=ray_connect_handler)
+    ray.connect("localhost:50051")
+    try:
+        yield ray, server
+    finally:
+        ray._inside_client_test = False
+        ray.disconnect()
+        server.stop(0)
