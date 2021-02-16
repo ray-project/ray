676666c3f627e5da9f755ffd7a5e18033cd55cc4
Alex Wu <alex@anyscale.com>
2021-02-15 20:11:03 -0800

    .

--- python/ray/autoscaler/_private/process_runner_interceptor.py
+++ python/ray/autoscaler/_private/process_runner_interceptor.py
@@ -25,9 +25,14 @@ class ProcessRunnerInterceptor:
         # TODO (Alex): We're currently abusing check_output by trying to print
         # its output and parse it at the same time.
         self.stream.write(output)
+        self.stream.flush()
+        print("check_output called with", args)
+        print("Output is", output)
         return output
 
     def check_call(self, *args, **kwargs):
         with_defaults = {"stdout": self.stream, "stderr": self.err_stream}
         with_defaults.update(kwargs)
-        return self.process_runner.check_call(*args, **with_defaults)
+        return_code = self.process_runner.check_call(*args, **with_defaults)
+        print("check_output called with", args)
+        return return_code
