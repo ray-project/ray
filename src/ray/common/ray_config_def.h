4c3abf021af1227f26989c0a628dd665a5f6a88f
SangBin Cho <rkooo567@gmail.com>
2021-02-16 22:32:13 -0800

    Revert "[Ownership based object directory] Turn on by default. (#13964)"
    This reverts commit 04d2df40cd99a4b4b6999f979841939b511eb576.

--- src/ray/common/ray_config_def.h
+++ src/ray/common/ray_config_def.h
@@ -314,7 +314,7 @@ RAY_CONFIG(int64_t, kill_idle_workers_interval_ms, 200)
 RAY_CONFIG(int64_t, idle_worker_killing_time_threshold_ms, 1000)
 
 /// Whether start the Plasma Store as a Raylet thread.
-RAY_CONFIG(bool, ownership_based_object_directory_enabled, true)
+RAY_CONFIG(bool, ownership_based_object_directory_enabled, false)
 
 // The interval where metrics are exported in milliseconds.
 RAY_CONFIG(uint64_t, metrics_report_interval_ms, 10000)
