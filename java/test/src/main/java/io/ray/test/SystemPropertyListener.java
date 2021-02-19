cbc998ec409bbaf5ed2a35c5c3eaa4b302917ce8
Kai Yang <kfstorm@outlook.com>
2021-02-19 17:18:53 +0800

    rename to SystemPropertyListener

--- java/test/src/main/java/io/ray/test/SystemPropertyListener.java
+++ java/test/src/main/java/io/ray/test/SystemPropertyListener.java
@@ -3,7 +3,7 @@ package io.ray.test;
 import org.testng.IClassListener;
 import org.testng.ITestClass;
 
-public class ConfigListener implements IClassListener {
+public class SystemPropertyListener implements IClassListener {
 
   @Override
   public void onAfterClass(ITestClass testClass) {
