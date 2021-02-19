4efe28fa10e2d512360fa8483efed16a87458576
Lixin Wei <wlx65005@gmail.com>
2021-02-19 21:13:21 +0800

    add file

---
+++ java/runtime/src/main/java/io/ray/runtime/exception/RayActorCreationTaskException.java
@@ -0,0 +1,41 @@
+package io.ray.runtime.exception;
+
+import io.ray.api.id.ActorId;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+/**
+ * Indicates that the actor died unexpectedly because an exception thrown in creation
+ * task.
+ */
+public class RayActorCreationTaskException extends RayActorException {
+  private static final String DEFAULT_MESSAGE =
+      "The actor died unexpectedly because an exception thrown in creation task.";
+
+  public RayActorCreationTaskException() {
+    super(DEFAULT_MESSAGE);
+  }
+
+  public RayActorCreationTaskException(Throwable cause) {
+    super(DEFAULT_MESSAGE, cause);
+  }
+
+  public RayActorCreationTaskException(ActorId actorId) {
+    super(String.format(
+        "The actor %s died unexpectedly because an exception thrown in creation task.",
+        actorId));
+    this.actorId = actorId;
+  }
+
+  public RayActorCreationTaskException(String message) {
+    super(message);
+  }
+
+  public RayActorCreationTaskException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public String serializeToString() {
+    return ExceptionUtils.getStackTrace(this);
+  }
+}
