ac8bca37637ce5716651a7e0c8ab3f5966b40d69
simon-mo <simon.mo@hey.com>
2021-02-22 15:18:08 -0800

    Enrich the test output and some namings

---
+++ dashboard/tests/run_ui_tests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -x
+set -euo pipefail
+
+ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
+cd "$ROOT_DIR"
+
+clean_up() {
+  ray stop --force
+}
+trap clean_up EXIT
+
+which cypress || sudo npm install cypress -g
+
+ray stop --force
+ray start --head --dashboard-port=8653
+cypress run --project . --headless
