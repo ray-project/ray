5a2b12586ceddde8cd39c62b592ede33d56088a2
dominoniole <niole.nelson@dominodatalab.com>
2021-02-23 09:17:02 -0800

    Add commends to requestHandlers

--- dashboard/client/src/service/requestHandlers.ts
+++ dashboard/client/src/service/requestHandlers.ts
@@ -1,21 +1,29 @@
+/**
+ * This utility file formats and sends HTTP requests such that
+ * they fullfill the requirements expected by users of the dashboard.
+ *
+ * All HTTP requests should be sent using the helpers in this file.
+ *
+ * More HTTP Methods helpers should be added to this file when the need
+ * arises.
+ */
+
 import axios, { AxiosResponse, AxiosRequestConfig } from "axios";
 
-const unformattedPrefix = (process.env.REACT_APP_API_PREFIX || "").trim();
-
-const getPrefix = (): string => {
-  if (unformattedPrefix[unformattedPrefix.length - 1] === "/") {
-    return unformattedPrefix.slice(0, unformattedPrefix.length - 1);
-  }
-  return unformattedPrefix;
-};
-
+/**
+ * This function formats URLs such that the user's browser
+ * sets the HTTP request's Request URL relative to the path at
+ * which the dashboard is served.
+ * This works behind a reverse proxy.
+ *
+ * @param {String} url The URL to be hit
+ * @return {String}    The reverse proxy compatible URL
+ */
 export const formatUrl = (url: string): string => {
-  const prefix = getPrefix();
-
   if (url.startsWith("/")) {
-    return `${prefix}${url}`;
+    return url.slice(1);
   }
-  return `${prefix}/${url}`;
+  return url;
 };
 
 export function get<T = any, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
