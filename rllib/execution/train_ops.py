e401679ea5930489c32d1e1b989b0389c374ceda
sven1977 <svenmika1977@gmail.com>
2021-02-24 16:20:52 +0100

    wip.

--- rllib/execution/train_ops.py
+++ rllib/execution/train_ops.py
@@ -76,7 +76,8 @@ class TrainOneStep:
                     info, "custom_metrics")
             learn_timer.push_units_processed(batch.count)
         metrics.counters[STEPS_TRAINED_COUNTER] += batch.count
-        metrics.counters[AGENT_STEPS_TRAINED_COUNTER] += batch.agent_steps()
+        if isinstance(batch, MultiAgentBatch):
+            metrics.counters[AGENT_STEPS_TRAINED_COUNTER] += batch.agent_steps()
         # Update weights - after learning on the local worker - on all remote
         # workers.
         if self.workers.remote_workers():
