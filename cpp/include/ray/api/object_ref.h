ac0ef5f087ae14fe12d86843a5cbcf62239a2917
qicosmos <qicosmos@163.com>
2021-02-28 16:55:40 +0800

    remove circular references

--- cpp/include/ray/api/object_ref.h
+++ cpp/include/ray/api/object_ref.h
@@ -1,7 +1,7 @@
 
 #pragma once
 
-#include <ray/api/ray_runtime.h>
+#include <ray/api/init_runtime.h>
 #include <ray/api/serializer.h>
 
 #include <memory>
@@ -58,7 +58,7 @@ class ObjectRef {
 // ---------- implementation ----------
 template <typename T>
 inline static std::shared_ptr<T> Get(const ObjectRef<T> &object) {
-  auto packed_object = Get(object.ID());
+  auto packed_object = InitRuntime::Instance().Runtime()->Get(object.ID());
   return Serializer::Deserialize<std::shared_ptr<T>>(packed_object->data(),
                                                      packed_object->size());
 }
