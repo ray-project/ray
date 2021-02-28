496f4c082aef61cd7602abb609b81cb220701800
Micah Yong <micahtyong@gmail.com>
2021-02-27 21:37:52 -0800

    Change from stats-only to store-true

--- python/ray/scripts/scripts.py
+++ python/ray/scripts/scripts.py
@@ -1366,7 +1366,7 @@ def timeline(address):
     help="Sort object references in ascending order by a SortingType \
 (e.g. PID, OBJECT_SIZE, or REFERENCE_TYPE).")
 @click.option(
-    "--stats-only",
+    "--store-true",
     type=bool,
     default=False,
     help="Display node stats store summary only.")
@@ -1376,14 +1376,14 @@ def timeline(address):
     default=True,
     help="Wrap lines when displaying output. \
 Terminal must be sufficiently large.")
-def memory(address, redis_password, group_by, sort_by, line_wrap, stats_only):
+def memory(address, redis_password, group_by, sort_by, line_wrap, store_true):
     """Print object references held in a Ray cluster."""
     if not address:
         address = services.get_ray_address_to_use_or_die()
     time = datetime.now()
     header = "=" * 8 + f" Object references status: {time} " + "=" * 8
     mem_stats = memory_summary_wrapper(address, redis_password, group_by,
-                                       sort_by, line_wrap, stats_only)
+                                       sort_by, line_wrap, store_true)
     print(f"{header}\n{mem_stats}")
 
 
