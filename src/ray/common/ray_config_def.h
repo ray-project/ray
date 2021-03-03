def04dc0856a94516d9877e9ce4aae15eeaf7518
Alex <alex@anyscale.com>
2021-03-03 02:13:06 +0000

    toggle spillback strategies

--- src/ray/common/ray_config_def.h
+++ src/ray/common/ray_config_def.h
@@ -107,6 +107,11 @@ RAY_CONFIG(bool, new_scheduler_enabled,
            getenv("RAY_ENABLE_NEW_SCHEDULER") == nullptr ||
                getenv("RAY_ENABLE_NEW_SCHEDULER") == std::string("1"))
 
+RAY_CONFIG(bool, scheduler_loadbalance_spillback,
+           getenv("RAY_SCHEDULER_LOADBALANCE_SPILLBACK") != nullptr &&
+           getenv("RAY_SCHEDULER_LOADBALANCE_SPILLBACK") == std::string("1")
+           )
+
 // The max allowed size in bytes of a return object from direct actor calls.
 // Objects larger than this size will be spilled/promoted to plasma.
 RAY_CONFIG(int64_t, max_direct_call_object_size, 100 * 1024)
