ebcfb6412af1cecc087c34814bfd52dd9182db62
Yi Cheng <chengyidna@gmail.com>
2021-03-04 03:41:40 +0000

    fix comments

--- python/ray/job_config.py
+++ python/ray/job_config.py
@@ -1,16 +1,5 @@
 import ray
 
-
-class RuntimeEnv:
-    def __init__(self,
-                 working_dir: str = None,
-                 working_dir_uri: str = None,
-                 local_modules=[]):
-        self.working_dir = working_dir
-        self.working_dir_uri = working_dir_uri
-        self.local_modules = local_modules
-
-
 class JobConfig:
     """A class used to store the configurations of a job.
 
@@ -23,6 +12,14 @@ class JobConfig:
         code_search_path (list): A list of directories or jar files that
             specify the search path for user code. This will be used as
             `CLASSPATH` in Java and `PYTHONPATH` in Python.
+        runtime_env (dict): A dict used to specify runtime environment of
+            the job. There are three important fields.
+            - `working_dir (str)`: A string of working directory
+            - `working_dir_uri (str)`: A string of uri of the package stored.
+            - `local_modules (list[module])`: List of python modules.
+            `working_dir` and `working_dir_uri` are used for the same thing.
+            `working_dir_uri` will be used with higher priority. This field
+            is used for environment managed by user.
     """
 
     def __init__(self,
@@ -30,12 +27,12 @@ class JobConfig:
                  num_java_workers_per_process=1,
                  jvm_options=None,
                  code_search_path=None,
-                 runtime_env=RuntimeEnv()):
+                 runtime_env=None):
         self.worker_env = worker_env or dict()
         self.num_java_workers_per_process = num_java_workers_per_process
         self.jvm_options = jvm_options or []
         self.code_search_path = code_search_path or []
-        self.runtime_env = runtime_env
+        self.runtime_env = runtime_env or dict()
 
     def serialize(self):
         job_config = ray.gcs_utils.JobConfig()
@@ -45,7 +42,7 @@ class JobConfig:
             self.num_java_workers_per_process)
         job_config.jvm_options.extend(self.jvm_options)
         job_config.code_search_path.extend(self.code_search_path)
-        if self.runtime_env.working_dir_uri:
+        if self.runtime_env.get('working_dir_uri'):
             job_config.runtime_env.working_dir_uri = \
-              self.runtime_env.working_dir_uri
+              self.runtime_env.get('working_dir_uri')
         return job_config.SerializeToString()
