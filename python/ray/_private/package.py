bbc5ee0ced4e62a65ea90c1a55956eef27c7c83a
Yi Cheng <chengyidna@gmail.com>
2021-03-05 00:31:23 +0000

    address comments

--- python/ray/_private/package.py
+++ python/ray/_private/package.py
@@ -18,14 +18,23 @@ Path(PKG_DIR).mkdir(parents=True, exist_ok=True)
 
 logger = logging.getLogger(__name__)
 
+FILE_SIZE_WARNING = 10 * 1024 * 1024  # 10MB
+FILE_SIZE_LIMIT = 50 * 1024 * 1024  # 50MB
+
 
 class Protocol(Enum):
-    # gcs is for package created by system and will be managed by
-    # ray cluster automatically through reference counting
-    GCS = "gcs"
-    # pin_gcs is for package created mannually. User needs to
-    # maintain the lifetime of this object
-    PIN_GCS = "pingcs"
+    """A enum for supported backend storage."""
+
+    # For docstring
+    def __new__(cls, value, doc=None):
+        self = object.__new__(cls)
+        self._value_ = value
+        if doc is not None:
+            self.__doc__ = doc
+        return self
+
+    GCS = "gcs", "For package created and managed by system."
+    PIN_GCS = "pingcs", "For package created and managed by users."
 
 
 def _xor_bytes(left, right):
@@ -35,22 +44,32 @@ def _xor_bytes(left, right):
 
 
 def _zip_module(path, relative_path, zip_handler):
+    """Go through all files and zip them into a zip file"""
     for from_file_name in path.glob("**/*"):
+        file_size = from_file_name.stat().st_size
+        if file_size >= FILE_SIZE_LIMIT:
+            raise RuntimeError(f"File {from_file_name} is too big, "
+                               "which currently is not allowd ")
+        if file_size >= FILE_SIZE_WARNING:
+            logger.warning(
+                f"File {from_file_name} is too big ({file_size} bytes). "
+                "Consider exclude this file in working directory.")
         to_file_name = from_file_name.relative_to(relative_path)
         zip_handler.write(from_file_name, to_file_name)
 
 
 def _hash_modules(path):
     """
-
+    Helper function to create hash of a directory.
+    It'll go through all the files in the directory and xor
+    hash(file_name, file_content) to create a hash value.
     """
     hash_val = None
     BUF_SIZE = 4096 * 1024
     for from_file_name in path.glob("**/*"):
         md5 = hashlib.md5()
-        if Path(from_file_name).is_dir():
-            md5.update(str(from_file_name).encode())
-        else:
+        md5.update(str(from_file_name).encode())
+        if not Path(from_file_name).is_dir():
             with open(from_file_name, mode="rb") as f:
                 data = f.read(BUF_SIZE)
                 if not data:
@@ -65,8 +84,26 @@ def _get_local_path(pkg_uri) -> str:
     return os.path.join(PKG_DIR, pkg_name)
 
 
+def _parse_uri(pkg_uri: str) -> (Protocol, str):
+    uri = urlparse(pkg_uri)
+    protocol = Protocol(uri.scheme)
+    return (protocol, uri.netloc)
+
+
 # TODO(yic): Fix this later to handle big directories in better way
 def get_project_package_name(working_dir, modules) -> str:
+    """This function will generate the name of the package by the working
+    directory and modules. It'll go through all the files in working_dir
+    and modules and hash the contents of these files to get the hash value
+    of this package. The final package name is: _ray_pkg_<HASH_VAL>.zip
+
+    Args:
+        working_dir (str): The working directory.
+        modules (list[module]): The python module.
+
+    Returns:
+        Package name as a string.
+    """
     RAY_PKG_PREFIX = "_ray_pkg_"
     hash_val = None
     if working_dir:
@@ -81,6 +118,17 @@ def get_project_package_name(working_dir, modules) -> str:
 
 
 def create_project_package(pkg_file: Path, working_dir: str, modules):
+    """This function is used to create a package file based on working directory
+    and python local modules.
+
+    Args:
+        pkg_file (pathlib.Path): The path of file to be created.
+        working_dir (str): The working directory.
+        modules (list[module]): The python modules to be included.
+
+    Returns:
+        None
+    """
     with ZipFile(pkg_file, "w") as zip_handler:
         if working_dir:
             # put all files in /path/working_dir into zip
@@ -95,13 +143,18 @@ def create_project_package(pkg_file: Path, working_dir: str, modules):
             _zip_module(module_path, module_path.parent, zip_handler)
 
 
-def _parse_uri(pkg_uri: str) -> (Protocol, str):
-    uri = urlparse(pkg_uri)
-    protocol = Protocol(uri.scheme)
-    return (protocol, uri.netloc)
-
-
 def fetch_package(pkg_uri: str, pkg_file: Path):
+    """This function is used to fetch a pacakge from the given uri to local
+    filesystem.
+
+    Args:
+        pkg_uri (str): The uri of the package to download.
+        pkg_file (pathlib.Path): The path in local filesystem to download the
+            package.
+
+    Returns:
+        The number of bytes downloaded.
+    """
     (protocol, pkg_name) = _parse_uri(pkg_uri)
     if protocol in (Protocol.GCS, Protocol.PIN_GCS):
         code = internal_kv._internal_kv_get(pkg_uri)
@@ -112,17 +165,39 @@ def fetch_package(pkg_uri: str, pkg_file: Path):
         raise NotImplementedError(f"Protocol {protocol} is not supported")
 
 
-def push_package(pkg_uri: str, pkg_file: Path):
-    (protocol, pkg_name) = _parse_uri(pkg_uri)
-    if protocol in (Protocol.GCS, Protocol.PIN_GCS):
-        data = pkg_file.read_bytes()
-        internal_kv._internal_kv_put(pkg_uri, data)
-    else:
-        raise NotImplementedError(f"Protocol {protocol} is not supported")
+def _store_package_in_gcs(gcs_key: str, data: bytes):
+    internal_kv._internal_kv_put(gcs_key, data)
     return len(data)
 
 
+def push_package(pkg_uri: str, pkg_file: Path):
+    """This function is to push a local file to remote uri. Right now, only GCS
+    is supported.
+
+    Args:
+        pkg_uri (str): The uri of the package to upload to.
+        pkg_file (Path): Path of the local file.
+
+    Returns:
+        The number of bytes uploaded.
+    """
+    (protocol, pkg_name) = _parse_uri(pkg_uri)
+    data = pkg_file.read_bytes()
+    if protocol in (Protocol.GCS, Protocol.PIN_GCS):
+        _store_package_in_gcs(pkg_uri, data)
+    else:
+        raise NotImplementedError(f"Protocol {protocol} is not supported")
+
+
 def package_exists(pkg_uri: str):
+    """Check whether the package with given uri exists or not.
+
+    Args:
+        pkg_uri (str): The uri of the package
+
+    Return:
+        True for package existing and False for not.
+    """
     (protocol, pkg_name) = _parse_uri(pkg_uri)
     if protocol in (Protocol.GCS, Protocol.PIN_GCS):
         return internal_kv._internal_kv_exists(pkg_uri)
@@ -131,6 +206,16 @@ def package_exists(pkg_uri: str):
 
 
 def update_runtime_env(job_config: JobConfig):
+    """This function is used to update the runtime field in job_config. The
+    runtime field will be generated based on the hash of required files and
+    modules.
+
+    Args:
+        job_config (JobConfig): The job config.
+
+    Return:
+        None
+    """
     # For now, we only support local directory and packages
     working_dir = job_config.runtime_env.get("working_dir")
     required_modules = job_config.runtime_env.get("local_modules")
@@ -143,6 +228,14 @@ def update_runtime_env(job_config: JobConfig):
 
 
 def driver_runtime_init(job_config: JobConfig):
+    """Init runtime env for driver. It'll check whether the runtime
+    environment exists in the cluster or not. If it doesn't exist, a package
+    will be created based on the working directory and modules defined in job
+    config. The package will be uploaded to the cluster after this.
+
+    Args:
+        job_config (JobConfig): The job config of driver.
+    """
     pkg_uri = job_config.get_package_uri()
     if not pkg_uri:
         return
@@ -160,6 +253,12 @@ def driver_runtime_init(job_config: JobConfig):
 
 
 def worker_runtime_init(runtime_env: RuntimeEnv):
+    """Init runtime env for worker. Necessary packages required to run the job will
+    be downloaded into local file system if it doesn't exist.
+
+    Args:
+        runtime_env (RuntimeEnv): Runtime environment of the job
+    """
     pkg_uri = runtime_env.working_dir_uri
     if not pkg_uri:
         return
