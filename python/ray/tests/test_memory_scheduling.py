17d0ea689def9740e0e13eff7c0f05ad39da70f5
Eric Liang <ekhliang@gmail.com>
2021-03-06 16:29:14 -0800

    fix mem sched test

--- python/ray/tests/test_memory_scheduling.py
+++ python/ray/tests/test_memory_scheduling.py
@@ -7,6 +7,10 @@ from ray.test_utils import wait_for_condition
 MB = 1024 * 1024
 
 
+def close(a, b, delta=MB):
+    return abs(a - b) < delta
+
+
 @ray.remote(memory=100 * MB)
 class Actor:
     def __init__(self):
@@ -37,17 +41,21 @@ def test_object_store_memory_reporting():
     try:
         ray.init(num_cpus=1, object_store_memory=500 * MB)
         wait_for_condition(
-            lambda: ray.available_resources()["object_store_memory"] == 10.0)
+            lambda: close(ray.available_resources()["object_store_memory"], 500 * MB)
+        )
         x1 = ray.put(np.zeros(150 * 1024 * 1024, dtype=np.uint8))
         wait_for_condition(
-            lambda: ray.available_resources()["object_store_memory"] == 7.0)
+            lambda: close(ray.available_resources()["object_store_memory"], 350 * MB)
+        )
         x2 = ray.put(np.zeros(75 * 1024 * 1024, dtype=np.uint8))
         wait_for_condition(
-            lambda: ray.available_resources()["object_store_memory"] == 5.5)
+            lambda: close(ray.available_resources()["object_store_memory"], 275 * MB)
+        )
         del x1
         del x2
         wait_for_condition(
-            lambda: ray.available_resources()["object_store_memory"] == 10.0)
+            lambda: close(ray.available_resources()["object_store_memory"], 500 * MB)
+        )
     finally:
         ray.shutdown()
 
@@ -60,13 +68,16 @@ def test_object_store_memory_reporting_task():
     try:
         ray.init(num_cpus=1, object_store_memory=500 * MB)
         wait_for_condition(
-            lambda: ray.available_resources()["object_store_memory"] == 10.0)
+            lambda: close(ray.available_resources()["object_store_memory"], 500 * MB)
+        )
         x1 = f.remote(np.zeros(150 * 1024 * 1024, dtype=np.uint8))
         wait_for_condition(
-            lambda: ray.available_resources()["object_store_memory"] == 7.0)
+            lambda: close(ray.available_resources()["object_store_memory"], 350 * MB)
+        )
         ray.cancel(x1, force=True)
         wait_for_condition(
-            lambda: ray.available_resources()["object_store_memory"] == 10.0)
+            lambda: close(ray.available_resources()["object_store_memory"], 500 * MB)
+        )
     finally:
         ray.shutdown()
 
