4152810de5bb2fc283c62985204fa607e9f3ce2a
architkulkarni <arkulkar@gmail.com>
2021-03-08 11:33:42 -0800

    add runtime_env to JobConfig

--- python/ray/job_config.py
+++ python/ray/job_config.py
@@ -1,10 +1,14 @@
 import ray
+from ray.utils import get_conda_env_dir
 
 
 class JobConfig:
     """A class used to store the configurations of a job.
 
     Attributes:
+        runtime_env (dict): Specifies environment for worker processes.
+            Supports the key "conda_env" which maps to the name (str) of an
+            existing conda env the worker process should start in.
         worker_env (dict): Environment variables to be set on worker
             processes.
         num_java_workers_per_process (int): The number of java workers per
@@ -17,6 +21,7 @@ class JobConfig:
 
     def __init__(
             self,
+            runtime_env=None,
             worker_env=None,
             num_java_workers_per_process=1,
             jvm_options=None,
@@ -26,6 +31,18 @@ class JobConfig:
             self.worker_env = dict()
         else:
             self.worker_env = worker_env
+        if runtime_env:
+            conda_env = runtime_env.get("conda_env")
+            if conda_env is not None:
+                conda_env_dir = get_conda_env_dir(conda_env)
+                if self.worker_env.get("PYTHONHOME") is not None:
+                    raise ValueError(
+                        f"worker_env specifies PYTHONHOME="
+                        f"{self.worker_env['PYTHONHOME']} which "
+                        f"conflicts with PYTHONHOME={conda_env_dir} "
+                        f"required by the specified conda env "
+                        f"{runtime_env['conda_env']}.")
+                self.worker_env.update(PYTHONHOME=conda_env_dir)
         self.num_java_workers_per_process = num_java_workers_per_process
         if jvm_options is None:
             self.jvm_options = []
