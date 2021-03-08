810e8baf1f1dc534c8b17c39b87f77b0dc3032d7
sven1977 <svenmika1977@gmail.com>
2021-03-08 10:32:20 +0100

    wip.

--- rllib/examples/custom_keras_model.py
+++ rllib/examples/custom_keras_model.py
@@ -11,7 +11,6 @@ from ray.rllib.models import ModelCatalog
 from ray.rllib.models.tf.misc import normc_initializer
 from ray.rllib.models.tf.tf_modelv2 import TFModelV2
 from ray.rllib.models.tf.visionnet import VisionNetwork as MyVisionNetwork
-from ray.rllib.policy.policy import LEARNER_STATS_KEY
 from ray.rllib.policy.sample_batch import DEFAULT_POLICY_ID
 from ray.rllib.utils.framework import try_import_tf
 
@@ -85,7 +84,7 @@ class MyKerasQModel(DistributionalQTFModel):
             kernel_initializer=normc_initializer(1.0))(layer_1)
         self.base_model = tf.keras.Model(self.inputs, layer_out)
 
-    # Implement the core forward method.
+    # Implement the core forward method
     def forward(self, input_dict, state, seq_lens):
         model_out = self.base_model(input_dict["obs"])
         return model_out, state
@@ -108,7 +107,7 @@ if __name__ == "__main__":
     def check_has_custom_metric(result):
         r = result["result"]["info"]["learner"]
         if DEFAULT_POLICY_ID in r:
-            r = r[DEFAULT_POLICY_ID][LEARNER_STATS_KEY]
+            r = r[DEFAULT_POLICY_ID]
         assert r["model"]["foo"] == 42, result
 
     if args.run == "DQN":
