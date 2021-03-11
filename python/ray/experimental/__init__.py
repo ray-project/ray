532ce114897c0c62578e3aa1272b0a6cc0a40af5
Eric Liang <ekhliang@gmail.com>
2021-03-10 17:13:21 -0800

    fix it

--- python/ray/experimental/__init__.py
+++ python/ray/experimental/__init__.py
@@ -1,3 +1,3 @@
-from .dynamic_resources import set_resource
-from .packaging.load_package import load_package
+from ray.experimental.dynamic_resources import set_resource
+from ray.experimental.packaging.load_package import load_package
 __all__ = ["set_resource", "load_package"]
