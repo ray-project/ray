a1978d456cb5a7e48611a888ee2c2e9efcacb6c4
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2021-03-12 12:41:32 -0800

    Fix uptime

--- dashboard/modules/reporter/reporter_agent.py
+++ dashboard/modules/reporter/reporter_agent.py
@@ -194,6 +194,9 @@ class ReporterAgent(dashboard_utils.DashboardAgentModule,
 
     @staticmethod
     def _get_boot_time():
+        if IN_KUBERNETES_POD:
+            # Return start time of container entrypoint
+            return psutil.Process(pid = 1).create_time()
         return psutil.boot_time()
 
     @staticmethod
