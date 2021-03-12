439a3bfa71a07da895b6f5a63d8f98aa2b523786
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2021-03-11 17:25:31 -0800

    add ray/_private to setup-dev

--- python/ray/setup-dev.py
+++ python/ray/setup-dev.py
@@ -74,6 +74,7 @@ if __name__ == "__main__":
     do_link("experimental", force=args.yes)
     do_link("util", force=args.yes)
     do_link("serve", force=args.yes)
+    do_link("_private", force=args.yes)
     # Link package's `new_dashboard` directly to local (repo's) dashboard.
     # The repo's `new_dashboard` is a file, soft-linking to which will not work
     # on Mac.
