de4dd00b4974951ca94c10b50a0451e800f0bea6
Ian <ian.rodney@gmail.com>
2021-03-17 20:14:14 +0000

    warn  loudly

--- python/ray/services.py
+++ python/ray/services.py
@@ -1,6 +1,13 @@
+import warnings
+
 import ray
 
 
-# TODO(ekl) deprecate and move this to ray.util
+# TODO(ekl) deprecate this
 def get_node_ip_address(address="8.8.8.8:53"):
+    warnings.warn(
+        DeprecationWarning(
+            "ray.services.get_node_ip_address has been moved to "
+            "ray.util.get_node_ip_address."),
+        stacklevel=2)
     return ray.util.get_node_ip_address(address)
