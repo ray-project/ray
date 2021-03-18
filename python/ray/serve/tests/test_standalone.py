971855a35376a74bb227155274a2d49a742eac1c
Ian Rodney <ian.rodney@gmail.com>
2021-03-18 07:26:55 -0700

    [Serve] Disable Final Standalone Test on Windows (#14761)

--- python/ray/serve/tests/test_standalone.py
+++ python/ray/serve/tests/test_standalone.py
@@ -284,6 +284,7 @@ def test_no_http(ray_shutdown):
         serve.shutdown()
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="Failing on Windows")
 def test_http_head_only(ray_cluster):
     cluster = ray_cluster
     head_node = cluster.add_node(num_cpus=4)
