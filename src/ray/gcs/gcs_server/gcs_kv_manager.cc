1e06338e50d59b30d6b337fbac7101ba1ffe925c
Yi Cheng <chengyidna@gmail.com>
2021-03-23 17:20:12 +0000

    compiling fix

--- src/ray/gcs/gcs_server/gcs_kv_manager.cc
+++ src/ray/gcs/gcs_server/gcs_kv_manager.cc
@@ -7,13 +7,13 @@ void GcsInternalKVManager::HandleInternalKVGet(
     const rpc::InternalKVGetRequest &request, rpc::InternalKVGetReply *reply,
     rpc::SendReplyCallback send_reply_callback) {
   std::vector<std::string> cmd = {"HGET", request.key(), "value"};
-  redis_client_->GetPrimaryContext()->RunArgvAsync(
+  RAY_CHECK_OK(redis_client_->GetPrimaryContext()->RunArgvAsync(
       cmd, [reply, send_reply_callback](auto redis_reply) {
         if (!redis_reply->IsNil()) {
           reply->set_value(redis_reply->ReadAsString());
         }
         GCS_RPC_SEND_REPLY(send_reply_callback, reply, Status::OK());
-      });
+      }));
 }
 
 void GcsInternalKVManager::HandleInternalKVPut(
@@ -21,48 +21,48 @@ void GcsInternalKVManager::HandleInternalKVPut(
     rpc::SendReplyCallback send_reply_callback) {
   std::vector<std::string> cmd = {request.overwrite() ? "HSET" : "HSETNX", request.key(),
                                   "value", request.value()};
-  redis_client_->GetPrimaryContext()->RunArgvAsync(
+  RAY_CHECK_OK(redis_client_->GetPrimaryContext()->RunArgvAsync(
       cmd, [reply, send_reply_callback](auto redis_reply) {
         reply->set_added_num(redis_reply->ReadAsInteger());
         GCS_RPC_SEND_REPLY(send_reply_callback, reply, Status::OK());
-      });
+      }));
 }
 
 void GcsInternalKVManager::HandleInternalKVDel(
     const rpc::InternalKVDelRequest &request, rpc::InternalKVDelReply *reply,
     rpc::SendReplyCallback send_reply_callback) {
   std::vector<std::string> cmd = {"HDEL", request.key(), "value"};
-  redis_client_->GetPrimaryContext()->RunArgvAsync(
+  RAY_CHECK_OK(redis_client_->GetPrimaryContext()->RunArgvAsync(
       cmd, [reply, send_reply_callback](auto redis_reply) {
         reply->set_deleted_num(redis_reply->ReadAsInteger());
         GCS_RPC_SEND_REPLY(send_reply_callback, reply, Status::OK());
-      });
+      }));
 }
 
 void GcsInternalKVManager::HandleInternalKVExists(
     const rpc::InternalKVExistsRequest &request, rpc::InternalKVExistsReply *reply,
     rpc::SendReplyCallback send_reply_callback) {
   std::vector<std::string> cmd = {"HEXISTS", request.key(), "value"};
-  redis_client_->GetPrimaryContext()->RunArgvAsync(
+  RAY_CHECK_OK(redis_client_->GetPrimaryContext()->RunArgvAsync(
       cmd, [reply, send_reply_callback](auto redis_reply) {
         bool exists = redis_reply->ReadAsInteger() > 0;
         reply->set_exists(exists);
         GCS_RPC_SEND_REPLY(send_reply_callback, reply, Status::OK());
-      });
+      }));
 }
 
 void GcsInternalKVManager::HandleInternalKVKeys(
     const rpc::InternalKVKeysRequest &request, rpc::InternalKVKeysReply *reply,
     rpc::SendReplyCallback send_reply_callback) {
   std::vector<std::string> cmd = {"KEYS", request.prefix() + "*"};
-  redis_client_->GetPrimaryContext()->RunArgvAsync(
+  RAY_CHECK_OK(redis_client_->GetPrimaryContext()->RunArgvAsync(
       cmd, [reply, send_reply_callback](auto redis_reply) {
         const auto &results = redis_reply->ReadAsStringArray();
         for (const auto &result : results) {
           reply->add_results(result);
         }
         GCS_RPC_SEND_REPLY(send_reply_callback, reply, Status::OK());
-      });
+      }));
 }
 
 }  // namespace gcs
