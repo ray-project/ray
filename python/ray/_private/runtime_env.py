e1e5717aecb6449cf4ef26bda474301757f5d42d
Jialin Liu <jialin.liu@bytedance.com>
2021-03-24 14:00:02 -0700

    continue read all data to update the hash

--- python/ray/_private/runtime_env.py
+++ python/ray/_private/runtime_env.py
@@ -140,9 +140,9 @@ def _hash_modules(path: Path) -> bytes:
         if not Path(from_file_name).is_dir():
             with open(from_file_name, mode="rb") as f:
                 data = f.read(BUF_SIZE)
-                if not data:
-                    continue
-                md5.update(data)
+                while data is not None:
+                    md5.update(data)
+                    data = f.read(BUF_SIZE)
         hash_val = _xor_bytes(hash_val, md5.digest())
     return hash_val
 
