af00ded9b69cc37bb8927a8ace00000e8de3731f
Ezra-H <huangrh9@gmail.com>
2021-03-30 02:08:18 +0800

    add base trainer

---
+++ python/ray/util/distml/base_trainer.py
@@ -0,0 +1,82 @@
+import ray
+import ray.util.collective as col
+
+class BaseTrainer:
+    def __init__(self, 
+                 *, 
+                 training_operator_cls, 
+                 world_size=1, 
+                 backend="auto", 
+                 num_cpus_per_worker=1,
+                 num_gpus_per_worker=1,
+                 distributed_strategy="allreduce"
+                 ):
+    
+        self.world_size = world_size
+        self.backend = backend
+        self.distributed_strategy = distributed_strategy
+
+        if not ray.is_initialized() and self.max_replicas > 1:
+            logger.info("Automatically initializing single-node Ray. To use "
+                        "multi-node training, be sure to run `ray.init("
+                        "address='auto')` before instantiating the Trainer.")
+            ray.init()
+        self._start_workers(training_operator_cls, 
+                            num_cpus_per_worker, 
+                            num_gpus_per_worker, 
+                            world_size, 
+                            backend)
+
+        self._init_strategy(distributed_strategy)
+
+    def _start_workers(self,
+                       training_operator_cls, 
+                       num_cpus_per_worker, 
+                       num_gpus_per_worker, 
+                       world_size, 
+                       backend):
+        """Create worker(actor), maybe need worker group to manager these workers.
+           Or, send these workers to strategy to manager?
+
+           set workers or worker group
+           set worker info, record rank, backend, use_num_gpus?
+        """
+        pass
+
+    def train(self):
+        """Call operator train_one_epoch. Or run all epoches?
+        """
+        pass
+
+    def validate(self):
+        """Call operator validate to evaluate val_dataloader.
+        """
+        pass
+
+    def step(self):
+        """Call step in self.train(). different strategy calling here?
+        """
+        pass
+
+    def state_dict(self):
+        """return model parameter.
+        """
+
+    def load_state_dict(self, state_dict, blocking=False):
+        """load model parameter.
+        """
+
+    def save(self, save_fn, checkpoint):
+        """Saves the Trainer state to the provided checkpoint path.
+
+        Args:
+            save_fn (function): The saving function for different machine learning systems.
+            checkpoint (str): Path to target checkpoint file.
+        """
+        save_fn(self.state_dict(), checkpoint)
+        return checkpoint
+
+    def shutdown(self, force=False):
+        """Kill all workers.
+        """
+        pass
--- python/ray/util/distml/jax_operator.py
+++ python/ray/util/distml/jax_operator.py
@@ -1,1 +1,134 @@
 """TODO(Runhui): JAX Operator"""
+
+class TrainingOperator:
+
+    def __init__(self):
+        pass
+
+    def setup():
+        """Function that needs to be override by users."""
+        pass
+
+    def register(self, *, model, optimizer, critierion):
+        """Register a few critical information about the model to operator."""
+        model = ...
+        optimizer = ...
+        self._register_model(model)
+        self._register_optimizer(optimizer)
+
+    def _register_model(self, model):
+        pass
+
+    def _register_optimizer(self, optimizer):
+        pass
+
+    def register_data(self, *, train_loader=None, validation_loader=None):
+
+        self.train_loader = train_loader
+        self.validation_loader = validation_loader
+
+
+    def train_epoch(self, iterator, info):
+        if not self.train_dataloader:
+            raise RuntimeError("Train dataloader hasn't been set.")
+
+        for idx, batch in enumerate(self.train_dataloader):
+            self.opt_state = self.update(self.steps,
+                                            self.opt_state,
+                                            batch)
+
+        params = self.get_params(self.opt_state)
+        # train_acc = self.accuracy(params, self.train_dataloader)
+        test_acc = self.accuracy(params, self.test_dataloader)
+        test_time = time.time() - test_start_time
+        print("Epoch {} in {:0.2f} sec, test time {:0.2f} sec."
+            .format(epoch, epoch_time, test_time))
+        # print("Training set accuracy {}".format(train_acc))
+        print("Test set accuracy {}".format(test_acc))
+
+        if not hasattr(self, "model"):
+            raise RuntimeError("Either set self.model in setup function or "
+                               "override this method to implement a custom "
+                               "training loop.")
+        if not hasattr(self, "optimizer"):
+            raise RuntimeError("Either set self.optimizer in setup function "
+                               "or override this method to implement a custom "
+                               "training loop.")
+        if not hasattr(self, "criterion"):
+            raise RuntimeError("Either set self.criterion in setup function "
+                               "or override this method to implement a custom "
+                               "training loop.")
+        model = self.model
+        optimizer = self.optimizer
+        criterion = self.criterion
+        # unpack features into list to support multiple inputs model
+        *features, target = batch
+        # Create non_blocking tensors for distributed training
+        if self.use_gpu:
+            features = [
+                feature.cuda(non_blocking=True) for feature in features
+            ]
+            target = target.cuda(non_blocking=True)
+
+        # Compute output.
+        output = model(*features)
+        loss = criterion(output, target)
+
+
+        # Call step of optimizer to update model params.
+        with self.timers.record("apply"):
+            optimizer.step()
+
+        return {"train_loss": loss.item(), NUM_SAMPLES: features[0].size(0)}
+
+
+if __name__ == "__main__":
+    class Dataloader:
+        def __init__(self, data, target, batch_size=128, shuffle=False):
+            '''
+            data: shape(width, height, channel, num)
+            target: shape(num, num_classes)
+            '''
+            self.data = data
+            self.target = target
+            self.batch_size = batch_size
+            num_data = self.target.shape[0]
+            num_complete_batches, leftover = divmod(num_data, batch_size)
+            self.num_batches = num_complete_batches + bool(leftover)
+            self.shuffle = shuffle
+
+        def synth_batches(self):
+            num_imgs = self.target.shape[0]
+            rng = npr.RandomState(np.random.randint(10))
+            perm = rng.permutation(num_imgs) if self.shuffle else np.arange(num_imgs)
+            for i in range(self.num_batches):
+                batch_idx = perm[i * self.batch_size:(i + 1) * self.batch_size]
+                img_batch = self.data[:, :, :, batch_idx]
+                label_batch = self.target[batch_idx]
+                yield img_batch, label_batch
+
+        def __iter__(self):
+            return self.synth_batches()
+
+
+    class MyTrainingOperator(TrainingOperator):
+        def setup(self, config):
+            model = nn.Linear(1, 1)
+            optimizer = torch.optim.SGD(
+                model.parameters(), lr=config.get("lr", 1e-4))
+            loss = torch.nn.MSELoss()
+
+            batch_size = config["batch_size"]
+            train_data, val_data = LinearDataset(2, 5), LinearDataset(2, 5)
+            train_target, val_target = LinearDataset(2, 5), LinearDataset(2, 5)
+            train_loader = Dataloader(train_data, train_target, batch_size=batch_size, shuffle=True)
+            val_loader = Dataloader(val_data, val_target, batch_size=batch_size)
+
+            self.model, self.optimizer = self.register(
+                models=model,
+                optimizers=optimizer,
+                criterion=loss)
+
+            self.register_data(
+                train_loader=train_loader,
+                validation_loader=val_loader)
