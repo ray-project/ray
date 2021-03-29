cb649fd25856d0af76c467e7095d976de3eb3736
Yi Cheng <chengyidna@gmail.com>
2021-03-29 04:46:09 +0000

    fix compatible issue

--- python/ray/experimental/internal_kv.py
+++ python/ray/experimental/internal_kv.py
@@ -1,25 +1,40 @@
 import ray
+import grpc
 from typing import List, Union
 
 from ray._private.client_mode_hook import client_mode_hook
 
+redis = False
 
 @client_mode_hook
 def _internal_kv_initialized():
+    global redis
     worker = ray.worker.global_worker
-    return hasattr(worker, "mode") and worker.mode is not None
+    inited = hasattr(worker, "mode") and worker.mode is not None
+    if inited:
+        try:
+            _internal_kv_exist("dummy")
+        except grpc.RpcError:
+            redis = True
+    return inited
 
 
 @client_mode_hook
 def _internal_kv_get(key: Union[str, bytes]) -> bytes:
     """Fetch the value of a binary key."""
-    return ray.worker.global_worker.gcs_client.kv_get(key)
+    if redis:
+        return ray.worker.global_worker.redis_client.hget(key, "value")
+    else:
+        return ray.worker.global_worker.gcs_client.kv_get(key)
 
 
 @client_mode_hook
 def _internal_kv_exists(key: Union[str, bytes]) -> bool:
     """Check key exists or not."""
-    return ray.worker.global_worker.gcs_client.kv_exists(key)
+    if redis:
+        return ray.worker.global_worker.redis_client.hexists(key, "value")
+    else:
+        return ray.worker.global_worker.gcs_client.kv_exists(key)
 
 
 @client_mode_hook
@@ -33,17 +48,33 @@ def _internal_kv_put(key: Union[str, bytes],
     Returns:
         already_exists (bool): whether the value already exists.
     """
-
-    return not ray.worker.global_worker.gcs_client.kv_put(
-        key, value, overwrite)
+    if redis:
+        if overwrite:
+            updated = worker.redis_client.hset(key, "value", value)
+        else:
+            updated = worker.redis_client.hsetnx(key, "value", value)
+        return updated == 0  # already exists
+    else:
+        return not ray.worker.global_worker.gcs_client.kv_put(
+            key, value, overwrite)
 
 
 @client_mode_hook
 def _internal_kv_del(key: Union[str, bytes]):
-    return ray.worker.global_worker.gcs_client.kv_del(key)
+    if redis:
+        return ray.worker.global_worker.redis_client.delete(key)
+    else:
+        return ray.worker.global_worker.gcs_client.kv_del(key)
 
 
 @client_mode_hook
 def _internal_kv_list(prefix: Union[str, bytes]) -> List[bytes]:
     """List all keys in the internal KV store that start with the prefix."""
-    return ray.worker.global_worker.gcs_client.kv_keys(prefix)
+    if redis:
+        if isinstance(prefix, bytes):
+            pattern = prefix + b"*"
+        else:
+            pattern = prefix + "*"
+        return ray.worker.global_worker.redis_client.keys(pattern=pattern)
+    else:
+        return ray.worker.global_worker.gcs_client.kv_keys(prefix)
