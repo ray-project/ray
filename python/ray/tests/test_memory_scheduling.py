cf601ce2e624789c6d92ed6f82e798ce8549df88
SangBin Cho <rkooo567@gmail.com>
2021-03-28 21:26:10 -0700

    Fix memory scheduling flaky test

--- python/ray/tests/test_memory_scheduling.py
+++ python/ray/tests/test_memory_scheduling.py
@@ -8,7 +8,7 @@ MB = 1024 * 1024
 
 
 def object_store_memory(a, delta=MB):
-    b = ray.available_resources()["object_store_memory"]
+    b = ray.available_resources().get("object_store_memory", 0)
     return abs(a - b) < delta
 
 
