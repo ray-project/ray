81a7d8dee452670b35ea42f2173ad854b47da1d5
Edward Oakes <ed.nmi.oakes@gmail.com>
2021-03-29 21:12:02 -0500

    Revert "Add support for Python 3.9 (#12613)"
    This reverts commit 208cde8d9ba4948712b689d8ae76e799876e81d4.

--- python/setup.py
+++ python/setup.py
@@ -27,7 +27,7 @@ logger = logging.getLogger(__name__)
 # before these files have been created, so we have to move the files
 # manually.
 
-SUPPORTED_PYTHONS = [(3, 6), (3, 7), (3, 8), (3, 9)]
+SUPPORTED_PYTHONS = [(3, 6), (3, 7), (3, 8)]
 SUPPORTED_BAZEL = (3, 2, 0)
 
 ROOT_DIR = os.path.dirname(__file__)
@@ -130,8 +130,7 @@ install_requires = [
     "grpcio >= 1.28.1",
     "jsonschema",
     "msgpack >= 1.0.0, < 2.0.0",
-    "numpy >= 1.16; python_version < '3.9'",
-    "numpy >= 1.19.3; python_version >= '3.9'",
+    "numpy >= 1.16",
     "protobuf >= 3.15.3",
     "py-spy >= 0.2.0",
     "pyyaml",
@@ -432,12 +431,6 @@ setuptools.setup(
     url="https://github.com/ray-project/ray",
     keywords=("ray distributed parallel machine-learning hyperparameter-tuning"
               "reinforcement-learning deep-learning serving python"),
-    classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
     packages=setuptools.find_packages(),
     cmdclass={"build_ext": build_ext},
     # The BinaryDistribution argument triggers build_ext.
