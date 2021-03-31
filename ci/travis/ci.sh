b52218e8adcd948fa09b5350469abe10c95a8791
Eric Liang <ekhliang@gmail.com>
2021-03-31 15:07:17 -0700

    flaky 2

--- ci/travis/ci.sh
+++ ci/travis/ci.sh
@@ -143,6 +143,7 @@ test_python() {
       -python/ray/serve:test_handle # "fatal error" (?) https://github.com/ray-project/ray/pull/13695
       -python/ray/serve:test_backend_worker # memory error
       -python/ray/tests:test_actor_advanced # timeout
+      -python/ray/tests:test_actor_failures # flako
       -python/ray/tests:test_advanced_2
       -python/ray/tests:test_advanced_3  # test_invalid_unicode_in_worker_log() fails on Windows
       -python/ray/tests:test_autoscaler_aws
