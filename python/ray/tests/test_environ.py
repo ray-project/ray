5d20ee491948e49755e5acd441aa014945043c7b
chenk008 <kongchen28@gmail.com>
2021-04-02 07:44:49 +0000

    fix ut

--- python/ray/tests/test_environ.py
+++ python/ray/tests/test_environ.py
@@ -1,20 +1,29 @@
 import os
 import pytest
+import ray
+
+@pytest.mark.skipif("sys.platform != 'linux'")
+def test_environ_file_on_linux(ray_start_10_cpus):
+
+    @ray.remote
+    class Actor1:
+        def __init__(self):
+            pass
+
+        def get_env_from_proc(self):
+            pid = os.getpid()
+            f = open('/proc/%d/environ' % pid, 'r')
+            return f.readlines()
+
+        def get_os_environ(self):
+            return os.environ
 
 
-@pytest.mark.linux
-def test_environ_file_on_linux():
-    def get_env(pid):
-        f = open('/proc/%d/environ' % pid, 'r')
-        return f.readlines()
-
-    pid = os.getpid()
-    orig_env = get_env(pid)
-
-    import ray
-
-    new_env = get_env(pid)
-    assert new_env == orig_env
+    a = Actor1.remote()
+    actor_proc_environ = ray.get(a.get_env_from_proc.remote())
+    actor_os_environ = ray.get(a.get_os_environ.remote())
+    assert len(actor_proc_environ) > 0 
+    assert len(actor_os_environ) > 0
 
 
 if __name__ == "__main__":
