c1bffc27849ed795938ccdee2d2078a38af9f1f7
Ian Rodney <ian.rodney@gmail.com>
2021-04-05 13:57:55 -0700

    [Core] Pick Up GCS Port From Env Variable (#15097)

--- python/ray/node.py
+++ python/ray/node.py
@@ -208,6 +208,10 @@ class Node:
 
         if head:
             ray_params.update_if_absent(num_redis_shards=1)
+            gcs_server_port = os.getenv(
+                ray_constants.GCS_PORT_ENVIRONMENT_VARIABLE)
+            if gcs_server_port:
+                ray_params.update_if_absent(gcs_server_port=gcs_server_port)
             self._webui_url = None
         else:
             self._webui_url = (
