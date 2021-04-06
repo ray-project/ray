c669e61505d9e538b645c0a6d5fc1629794a1ad2
Edward Oakes <ed.nmi.oakes@gmail.com>
2021-04-06 09:26:35 -0500

    fix lint

--- python/ray/serve/batching.py
+++ python/ray/serve/batching.py
@@ -183,11 +183,11 @@ def batch(_func=None, max_batch_size=10, batch_wait_timeout_s=0.0):
     or `batch_wait_timeout_s` has elapsed, whichever occurs first.
 
     Example:
-        @serve.batch(max_batch_size=50, batch_wait_timeout_s=0.5)
-        async def handle_batch(self, batch: List[str]):
+    >>> @serve.batch(max_batch_size=50, batch_wait_timeout_s=0.5)
+        async def handle_batch(batch: List[str]):
             return [s.lower() for s in batch]
 
-        async def handle_single(self, s: str):
+    >>> async def handle_single(s: str):
             # Will return s.lower().
             return await handle_batch(s)
 
