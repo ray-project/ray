e6cd764751d5a0f7539d7b4076c53e76c668f06e
simon-mo <simon.mo@hey.com>
2021-04-07 13:26:16 -0700

    fix test

--- python/ray/serialization_addons.py
+++ python/ray/serialization_addons.py
@@ -13,23 +13,23 @@ def register_pydantic_serializer(serialization_context):
     # Pydantic's Cython validators are not serializable.
     # https://github.com/cloudpipe/cloudpickle/issues/408
     serialization_context._register_cloudpickle_serializer(
-            pydantic.fields.ModelField,
-            serializer=lambda o: {
-                "name": o.name,
-                # outer_type_ is the original type for ModelFields,
-                # while type_ can be updated later with the nested type
-                # like int for List[int].
-                "type_": o.outer_type_,
-                "class_validators": o.class_validators,
-                "model_config": o.model_config,
-                "default": o.default,
-                "default_factory": o.default_factory,
-                "required": o.required,
-                "alias": o.alias,
-                "field_info": o.field_info,
-            },
-            deserializer=lambda kwargs: pydantic.fields.ModelField(**kwargs),
-        )
+        pydantic.fields.ModelField,
+        custom_serializer=lambda o: {
+            "name": o.name,
+            # outer_type_ is the original type for ModelFields,
+            # while type_ can be updated later with the nested type
+            # like int for List[int].
+            "type_": o.outer_type_,
+            "class_validators": o.class_validators,
+            "model_config": o.model_config,
+            "default": o.default,
+            "default_factory": o.default_factory,
+            "required": o.required,
+            "alias": o.alias,
+            "field_info": o.field_info,
+        },
+        custom_deserializer=lambda kwargs: pydantic.fields.ModelField(**kwargs)
+    )
 
 
 def register_starlette_serializer(serialization_context):
@@ -42,8 +42,8 @@ def register_starlette_serializer(serialization_context):
     # because it overrides __getattr__
     serialization_context._register_cloudpickle_serializer(
         starlette.datastructures.State,
-        serializer=lambda s: s._state,
-        deserializer=lambda s: starlette.datastructures.State(s),
+        custom_serializer=lambda s: s._state,
+        custom_deserializer=lambda s: starlette.datastructures.State(s),
     )
 
 
