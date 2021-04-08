79e9d660b670d64cf4ce9d0f70f60194bb68d437
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2021-04-07 19:43:12 -0700

    unused_import

--- python/ray/tests/test_client_library_integration.py
+++ python/ray/tests/test_client_library_integration.py
@@ -6,11 +6,10 @@ import ray
 import ray.util.client.server.server as ray_client_server
 
 from ray.rllib.examples import rock_paper_scissors_multiagent
-from ray.util.client.common import ClientObjectRef
-from ray.util.client.ray_client_helpers import ray_start_client_server
+
 
 def test_rllib_integration(call_ray_stop_only):
-    ray.init(num_cpus = 1)
+    ray.init(num_cpus=1)
     ray_client_server.serve("localhost:50051")
     # Connection timeout. Help.
     ray.util.connect("localhost:50051")
