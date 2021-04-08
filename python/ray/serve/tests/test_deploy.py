38d1d8f7672ea358585ad298b220e7ce53ddebb0
Edward Oakes <ed.nmi.oakes@gmail.com>
2021-04-08 13:00:39 -0500

    passing test_handle.py

--- python/ray/serve/tests/test_deploy.py
+++ python/ray/serve/tests/test_deploy.py
@@ -191,7 +191,8 @@ def test_redeploy_single_replica(serve_instance, use_handle):
     @ray.remote
     def call(block=False):
         if use_handle:
-            ret = ray.get(serve.get_handle(name).remote(block=str(block)))
+            handle = serve.get_deployment(name).get_handle()
+            ret = ray.get(handle.handler.remote(block))
         else:
             ret = requests.get(
                 f"http://localhost:8000/{name}", params={
@@ -204,16 +205,25 @@ def test_redeploy_single_replica(serve_instance, use_handle):
     signal = SignalActor.options(name=signal_name).remote()
 
     @serve.deployment(name=name, version="1")
-    async def v1(request):
-        if request.query_params["block"] == "True":
-            signal = ray.get_actor(signal_name)
-            await signal.wait.remote()
-        return f"1|{os.getpid()}"
+    class V1:
+        async def handler(self, block: bool):
+            if block:
+                signal = ray.get_actor(signal_name)
+                await signal.wait.remote()
 
-    def v2(*args):
-        return f"2|{os.getpid()}"
+            return f"1|{os.getpid()}"
 
-    v1.deploy()
+        async def __call__(self, request):
+            return await self.handler(request.query_params["block"] == "True")
+
+    class V2:
+        async def handler(self, *args):
+            return f"2|{os.getpid()}"
+
+        async def __call__(self, request):
+            return await self.handler()
+
+    V1.deploy()
     ref1 = call.remote(block=False)
     val1, pid1 = ray.get(ref1)
     assert val1 == "1"
@@ -224,8 +234,8 @@ def test_redeploy_single_replica(serve_instance, use_handle):
 
     # Redeploy new version. This should not go through until the old version
     # replica completely stops.
-    v2 = v1.options(backend_def=v2, version="2")
-    goal_ref = v2.deploy(_blocking=False)
+    V2 = V1.options(backend_def=V2, version="2")
+    goal_ref = V2.deploy(_blocking=False)
     assert not client._wait_for_goal(goal_ref, timeout=0.1)
 
     # It may take some time for the handle change to propagate and requests
@@ -272,8 +282,8 @@ def test_redeploy_multiple_replicas(serve_instance, use_handle):
     @ray.remote(num_cpus=0)
     def call(block=False):
         if use_handle:
-            handle = serve.get_handle(name, missing_ok=True)
-            ret = ray.get(handle.remote(block=str(block)))
+            handle = serve.get_deployment(name).get_handle()
+            ret = ray.get(handle.handler.remote(block))
         else:
             ret = requests.get(
                 f"http://localhost:8000/{name}", params={
@@ -285,14 +295,24 @@ def test_redeploy_multiple_replicas(serve_instance, use_handle):
     signal_name = f"signal-{get_random_letters()}"
     signal = SignalActor.options(name=signal_name).remote()
 
-    async def v1(request):
-        if request.query_params["block"] == "True":
-            signal = ray.get_actor(signal_name)
-            await signal.wait.remote()
-        return f"1|{os.getpid()}"
+    @serve.deployment(name=name, version="1")
+    class V1:
+        async def handler(self, block: bool):
+            if block:
+                signal = ray.get_actor(signal_name)
+                await signal.wait.remote()
 
-    def v2(*args):
-        return f"2|{os.getpid()}"
+            return f"1|{os.getpid()}"
+
+        async def __call__(self, request):
+            return await self.handler(request.query_params["block"] == "True")
+
+    class V2:
+        async def handler(self, *args):
+            return f"2|{os.getpid()}"
+
+        async def __call__(self, request):
+            return await self.handler()
 
     def make_nonblocking_calls(expected, expect_blocking=False):
         # Returns dict[val, set(pid)].
@@ -318,8 +338,8 @@ def test_redeploy_multiple_replicas(serve_instance, use_handle):
 
         return responses, blocking
 
-    v1 = serve.deployment(name=name, version="1", num_replicas=2)(v1)
-    v1.deploy()
+    V1 = serve.deployment(name=name, version="1", num_replicas=2)(V1)
+    V1.deploy()
     responses1, _ = make_nonblocking_calls({"1": 2})
     pids1 = responses1["1"]
 
@@ -334,8 +354,8 @@ def test_redeploy_multiple_replicas(serve_instance, use_handle):
 
     # Redeploy new version. Since there is one replica blocking, only one new
     # replica should be started up.
-    v2 = v1.options(backend_def=v2, version="2")
-    goal_ref = v2.deploy(_blocking=False)
+    V2 = V1.options(backend_def=V2, version="2")
+    goal_ref = V2.deploy(_blocking=False)
     assert not client._wait_for_goal(goal_ref, timeout=0.1)
     responses3, blocking3 = make_nonblocking_calls(
         {
@@ -361,19 +381,16 @@ def test_redeploy_scale_down(serve_instance, use_handle):
     name = "test"
 
     @serve.deployment(name=name, version="1", num_replicas=4)
-    def v1(request):
+    def v1(*args):
         return f"1|{os.getpid()}"
 
     @ray.remote(num_cpus=0)
-    def call(block=False):
+    def call():
         if use_handle:
             handle = v1.get_handle()
-            ret = ray.get(handle.remote(block=str(block)))
+            ret = ray.get(handle.remote())
         else:
-            ret = requests.get(
-                f"http://localhost:8000/{name}", params={
-                    "block": block
-                }).text
+            ret = requests.get("http://localhost:8000/{name}").text
 
         return ret.split("|")[0], ret.split("|")[1]
 
@@ -382,7 +399,7 @@ def test_redeploy_scale_down(serve_instance, use_handle):
         responses = defaultdict(set)
         start = time.time()
         while time.time() - start < 30:
-            refs = [call.remote(block=False) for _ in range(10)]
+            refs = [call.remote() for _ in range(10)]
             ready, not_ready = ray.wait(refs, timeout=0.5)
             for ref in ready:
                 val, pid = ray.get(ref)
