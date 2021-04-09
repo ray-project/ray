b485bdb42d3cc94b475b5a352046b5fc5e0670b4
chenk008 <kongchen28@gmail.com>
2021-04-09 04:17:49 +0000

    fix ut

--- python/ray/tests/test_environ.py
+++ python/ray/tests/test_environ.py
@@ -27,7 +27,6 @@ def test_environ_file_on_linux(ray_start_10_cpus):
 
     a = Actor1.remote()
     actor_proc_environ = ray.get(a.get_env_from_proc.remote())
-    print(len(actor_proc_environ))
     actor_os_environ = ray.get(a.get_os_environ.remote())
     assert len(actor_proc_environ) > 0
     assert len(actor_os_environ) > 0
