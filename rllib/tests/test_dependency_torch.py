d8ba25ee6132f29a4997750f36fc4ade06fe6a91
sven1977 <svenmika1977@gmail.com>
2021-04-11 14:13:58 +0200

    fix and LINT.

--- rllib/tests/test_dependency_torch.py
+++ rllib/tests/test_dependency_torch.py
@@ -2,17 +2,14 @@
 
 import os
 import sys
-import pytest
 
-
-@pytest.mark.skip(reason="Upstream change make it failed.")
-def test_dependency_torch():
-    # Do not import torch for testing purposes.
+if __name__ == "__main__":
+    # Do not import tf for testing purposes.
     os.environ["RLLIB_TEST_NO_TORCH_IMPORT"] = "1"
 
     from ray.rllib.agents.a3c import A2CTrainer
     assert "torch" not in sys.modules, \
-        "Torch initially present, when it shouldn't."
+        "PyTorch initially present, when it shouldn't."
 
     # note: no ray.init(), to test it works without Ray
     trainer = A2CTrainer(
@@ -20,15 +17,11 @@ def test_dependency_torch():
             "framework": "tf",
             "num_workers": 0
         })
-    #trainer.train()
+    trainer.train()
 
-    assert "torch" not in sys.modules, "Torch should not be imported"
+    assert "torch" not in sys.modules, "PyTorch should not be imported"
 
     # Clean up.
     del os.environ["RLLIB_TEST_NO_TORCH_IMPORT"]
 
     print("ok")
-
-
-if __name__ == "__main__":
-    sys.exit(pytest.main(["-sv", __file__]))
