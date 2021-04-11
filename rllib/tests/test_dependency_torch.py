0cf1e053fe8d1f061cba79a3c13e09da026803db
sven1977 <svenmika1977@gmail.com>
2021-04-11 15:37:38 +0200

    fix and LINT.

--- rllib/tests/test_dependency_torch.py
+++ rllib/tests/test_dependency_torch.py
@@ -3,13 +3,17 @@
 import os
 import sys
 
+
 if __name__ == "__main__":
     # Do not import tf for testing purposes.
     os.environ["RLLIB_TEST_NO_TORCH_IMPORT"] = "1"
 
     from ray.rllib.agents.a3c import A2CTrainer
+    #if "torch" in sys.modules:
+        #import inspect
+        #print(inspect.getframeinfo(inspect.getouterframes(inspect.currentframe())[1][0])[0])
     assert "torch" not in sys.modules, \
-        "PyTorch initially present, when it shouldn't."
+        "PyTorch initially present, when it shouldn't!"
 
     # note: no ray.init(), to test it works without Ray
     trainer = A2CTrainer(
@@ -17,9 +21,11 @@ if __name__ == "__main__":
             "framework": "tf",
             "num_workers": 0
         })
-    trainer.train()
+    #trainer.train()
 
-    assert "torch" not in sys.modules, "PyTorch should not be imported"
+    assert "torch" not in sys.modules, \
+        "PyTorch should not be imported after creating and " \
+        "training A3C agent!"
 
     # Clean up.
     del os.environ["RLLIB_TEST_NO_TORCH_IMPORT"]
