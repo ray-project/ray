b45c0ddaf8e0e8616fcfa2cc0e20a49bc1863601
sven1977 <svenmika1977@gmail.com>
2021-04-13 16:17:13 +0200

    wip and LINT.

---
+++ rllib/examples/partial_gpus.py
@@ -0,0 +1,99 @@
+"""Example of a custom gym environment and model. Run this for a demo.
+
+This example shows:
+  - using a custom environment
+  - using a custom model
+  - using Tune for grid search
+
+You can visualize experiment results in ~/ray_results using TensorBoard.
+"""
+import argparse
+import gym
+from gym.spaces import Discrete, Box
+import numpy as np
+import os
+import random
+
+import ray
+from ray import tune
+from ray.tune import grid_search
+from ray.rllib.env.env_context import EnvContext
+from ray.rllib.models import ModelCatalog
+from ray.rllib.models.tf.tf_modelv2 import TFModelV2
+from ray.rllib.models.tf.fcnet import FullyConnectedNetwork
+from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
+from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
+from ray.rllib.utils.framework import try_import_tf, try_import_torch
+from ray.rllib.utils.test_utils import check_learning_achieved
+
+tf1, tf, tfv = try_import_tf()
+torch, nn = try_import_torch()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--run", type=str, default="PPO")
+parser.add_argument("--framework", choices=["tf", "tf2", "tfe", "torch"],
+                    default="tf")
+parser.add_argument("--as-test", action="store_true")
+parser.add_argument("--stop-iters", type=int, default=50)
+parser.add_argument("--stop-timesteps", type=int, default=100000)
+parser.add_argument("--stop-reward", type=float, default=0.1)
+
+
+"""class SimpleCorridor(gym.Env):
+    ""Example of a custom env in which you have to walk down a corridor.
+
+    You can configure the length of the corridor via the env config.""
+
+    def __init__(self, config: EnvContext):
+        self.end_pos = config["corridor_length"]
+        self.cur_pos = 0
+        self.action_space = Discrete(2)
+        self.observation_space = Box(
+            0.0, self.end_pos, shape=(1, ), dtype=np.float32)
+        # Set the seed. This is only used for the final (reach goal) reward.
+        self.seed(config.worker_index * config.num_workers)
+
+    def reset(self):
+        self.cur_pos = 0
+        return [self.cur_pos]
+
+    def step(self, action):
+        assert action in [0, 1], action
+        if action == 0 and self.cur_pos > 0:
+            self.cur_pos -= 1
+        elif action == 1:
+            self.cur_pos += 1
+        done = self.cur_pos >= self.end_pos
+        # Produce a random reward when we reach the goal.
+        return [self.cur_pos], \
+            random.random() * 2 if done else -0.1, done, {}
+
+    def seed(self, seed=None):
+        random.seed(seed)
+"""
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    ray.init()
+
+    config = {
+        "env": "CartPole-v0",
+        "num_gpus": 0.25,
+        "num_workers": 2,  # parallelism
+        "num_envs_per_worker": 1,
+        "lr": tune.grid_search([0.1, 0.01, 0.001, 0.0001]),
+        "framework": args.framework,
+    }
+
+    stop = {
+        "training_iteration": args.stop_iters,
+        "timesteps_total": args.stop_timesteps,
+        "episode_reward_mean": args.stop_reward,
+    }
+
+    results = tune.run(args.run, config=config, stop=stop)
+
+    if args.as_test:
+        check_learning_achieved(results, args.stop_reward)
+    ray.shutdown()
