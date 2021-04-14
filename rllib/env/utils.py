6ea27677e9ea2a50d1c4e16e69e309a456ba9d2f
sven1977 <svenmika1977@gmail.com>
2021-04-14 18:47:57 +0200

    wip.

--- rllib/env/utils.py
+++ rllib/env/utils.py
@@ -2,7 +2,7 @@ from ray.rllib.env.env_context import EnvContext
 
 
 def gym_env_creator(env_context: EnvContext, env_descriptor: str):
-    """Tries to create a gym env given a descriptor and EnvContext object.
+    """Tries to create a gym env given am EnvContext object and descriptor.
 
     Note: This function tries to construct the env from a string descriptor
     only using possibly installed RL env packages (such as gym, pybullet_envs,
@@ -11,18 +11,18 @@ def gym_env_creator(env_context: EnvContext, env_descriptor: str):
     necessary imports and construction logic below.
 
     Args:
-        env_descriptor (str): The env descriptor, e.g. CartPole-v0,
-            MsPacmanNoFrameskip-v4, VizdoomBasic-v0, or
-            CartPoleContinuousBulletEnv-v0.
         env_context (EnvContext): The env context object to configure the env.
             Note that this is a config dict, plus the properties:
             `worker_index`, `vector_index`, and `remote`.
+        env_descriptor (str): The env descriptor, e.g. CartPole-v0,
+            MsPacmanNoFrameskip-v4, VizdoomBasic-v0, or
+            CartPoleContinuousBulletEnv-v0.
 
     Returns:
         gym.Env: The actual gym environment object.
 
     Raises:
-         gym.error.Error: If the env cannot be constructed.
+        gym.error.Error: If the env cannot be constructed.
     """
     import gym
     # Allow for PyBullet or VizdoomGym envs to be used as well
