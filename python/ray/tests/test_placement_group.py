c4039681ca955fcbbc9102ab96ab48e5f2ab669d
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2021-04-14 16:10:28 -0700

    context manager

--- python/ray/tests/test_placement_group.py
+++ python/ray/tests/test_placement_group.py
@@ -20,6 +20,8 @@ from ray._raylet import PlacementGroupID
 from ray.util.placement_group import (PlacementGroup, placement_group,
                                       remove_placement_group,
                                       get_current_placement_group)
+from ray.util.client.ray_client_helpers import connect_to_client_or_not
+from ray._private.client_mode_hook import client_mode_should_convert
 
 
 @ray.remote
@@ -28,7 +30,8 @@ class Increase:
         return x + 2
 
 
-def test_placement_group_pack(ray_start_cluster):
+@pytest.mark.parametrize("connect_to_client", [False, True])
+def test_placement_group_pack(ray_start_cluster, connect_to_client):
     @ray.remote(num_cpus=2)
     class Actor(object):
         def __init__(self):
@@ -43,44 +46,56 @@ def test_placement_group_pack(ray_start_cluster):
         cluster.add_node(num_cpus=4)
     ray.init(address=cluster.address)
 
-    placement_group = ray.util.placement_group(
-        name="name",
-        strategy="PACK",
-        bundles=[
-            {
-                "CPU": 2,
-                "GPU": 0  # Test 0 resource spec doesn't break tests.
-            },
-            {
-                "CPU": 2
-            }
-        ])
-    ray.get(placement_group.ready())
-    actor_1 = Actor.options(
-        placement_group=placement_group,
-        placement_group_bundle_index=0).remote()
-    actor_2 = Actor.options(
-        placement_group=placement_group,
-        placement_group_bundle_index=1).remote()
+    with connect_to_client_or_not(connect_to_client):
+        # Quick validation of the context manager:
+        if connect_to_client:
+            # Client mode is on.
+            assert client_mode_should_convert() is True
+            # We're connected to Ray client.
+            assert ray.util.client.ray.is_connected() is True
+        else:
+            assert client_mode_should_convert() is False
+            assert ray.util.client.ray.is_connected() is False
 
-    ray.get(actor_1.value.remote())
-    ray.get(actor_2.value.remote())
+        placement_group = ray.util.placement_group(
+            name="name",
+            strategy="PACK",
+            bundles=[
+                {
+                    "CPU": 2,
+                    "GPU": 0  # Test 0 resource spec doesn't break tests.
+                },
+                {
+                    "CPU": 2
+                }
+            ])
+        ray.get(placement_group.ready())
+        actor_1 = Actor.options(
+            placement_group=placement_group,
+            placement_group_bundle_index=0).remote()
+        actor_2 = Actor.options(
+            placement_group=placement_group,
+            placement_group_bundle_index=1).remote()
 
-    # Get all actors.
-    actor_infos = ray.actors()
+        ray.get(actor_1.value.remote())
+        ray.get(actor_2.value.remote())
 
-    # Make sure all actors in counter_list are collocated in one node.
-    actor_info_1 = actor_infos.get(actor_1._actor_id.hex())
-    actor_info_2 = actor_infos.get(actor_2._actor_id.hex())
+        # Get all actors.
+        actor_infos = ray.actors()
 
-    assert actor_info_1 and actor_info_2
+        # Make sure all actors in counter_list are collocated in one node.
+        actor_info_1 = actor_infos.get(actor_1._actor_id.hex())
+        actor_info_2 = actor_infos.get(actor_2._actor_id.hex())
 
-    node_of_actor_1 = actor_info_1["Address"]["NodeID"]
-    node_of_actor_2 = actor_info_2["Address"]["NodeID"]
-    assert node_of_actor_1 == node_of_actor_2
+        assert actor_info_1 and actor_info_2
+
+        node_of_actor_1 = actor_info_1["Address"]["NodeID"]
+        node_of_actor_2 = actor_info_2["Address"]["NodeID"]
+        assert node_of_actor_1 == node_of_actor_2
 
 
-def test_placement_group_strict_pack(ray_start_cluster):
+@pytest.mark.parametrize("connect_to_client", [False, True])
+def test_placement_group_strict_pack(ray_start_cluster, connect_to_client):
     @ray.remote(num_cpus=2)
     class Actor(object):
         def __init__(self):
@@ -95,42 +110,43 @@ def test_placement_group_strict_pack(ray_start_cluster):
         cluster.add_node(num_cpus=4)
     ray.init(address=cluster.address)
 
-    placement_group = ray.util.placement_group(
-        name="name",
-        strategy="STRICT_PACK",
-        bundles=[
-            {
-                "memory": 50 * 1024 *
-                1024,  # Test memory resource spec doesn't break tests.
-                "CPU": 2
-            },
-            {
-                "CPU": 2
-            }
-        ])
-    ray.get(placement_group.ready())
-    actor_1 = Actor.options(
-        placement_group=placement_group,
-        placement_group_bundle_index=0).remote()
-    actor_2 = Actor.options(
-        placement_group=placement_group,
-        placement_group_bundle_index=1).remote()
+    with connect_to_client_or_not(connect_to_client):
+        placement_group = ray.util.placement_group(
+            name="name",
+            strategy="STRICT_PACK",
+            bundles=[
+                {
+                    "memory": 50 * 1024 *
+                    1024,  # Test memory resource spec doesn't break tests.
+                    "CPU": 2
+                },
+                {
+                    "CPU": 2
+                }
+            ])
+        ray.get(placement_group.ready())
+        actor_1 = Actor.options(
+            placement_group=placement_group,
+            placement_group_bundle_index=0).remote()
+        actor_2 = Actor.options(
+            placement_group=placement_group,
+            placement_group_bundle_index=1).remote()
 
-    ray.get(actor_1.value.remote())
-    ray.get(actor_2.value.remote())
+        ray.get(actor_1.value.remote())
+        ray.get(actor_2.value.remote())
 
-    # Get all actors.
-    actor_infos = ray.actors()
+        # Get all actors.
+        actor_infos = ray.actors()
 
-    # Make sure all actors in counter_list are collocated in one node.
-    actor_info_1 = actor_infos.get(actor_1._actor_id.hex())
-    actor_info_2 = actor_infos.get(actor_2._actor_id.hex())
+        # Make sure all actors in counter_list are collocated in one node.
+        actor_info_1 = actor_infos.get(actor_1._actor_id.hex())
+        actor_info_2 = actor_infos.get(actor_2._actor_id.hex())
 
-    assert actor_info_1 and actor_info_2
+        assert actor_info_1 and actor_info_2
 
-    node_of_actor_1 = actor_info_1["Address"]["NodeID"]
-    node_of_actor_2 = actor_info_2["Address"]["NodeID"]
-    assert node_of_actor_1 == node_of_actor_2
+        node_of_actor_1 = actor_info_1["Address"]["NodeID"]
+        node_of_actor_2 = actor_info_2["Address"]["NodeID"]
+        assert node_of_actor_1 == node_of_actor_2
 
 
 def test_placement_group_spread(ray_start_cluster):
