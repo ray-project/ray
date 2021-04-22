7b67d61f774e854005f392a4332322109383b3e4
Alex <alex@anyscale.com>
2021-04-22 05:09:09 +0000

    lint

--- src/ray/gcs/gcs_server/gcs_resource_report_broadcaster.cc
+++ src/ray/gcs/gcs_server/gcs_resource_report_broadcaster.cc
@@ -4,14 +4,90 @@
 namespace ray {
 namespace gcs {
 
-
 GcsResourceReportBroadcaster::GcsResourceReportBroadcaster(
-                                                           std::shared_ptr<rpc::NodeManagerClientPool> raylet_client_pool,
-                                                           std::function<void(rpc::ResourceUsageBatchData &)> get_resource_usage_batch_for_broadcast,
-                                                           std::function<void(const rpc::Address &, std::shared_ptr<rpc::NodeManagerClientPool> &, std::string &)> send_batch
-                                                           )
-{}
+    std::shared_ptr<rpc::NodeManagerClientPool> raylet_client_pool,
+    std::function<void(rpc::ResourceUsageBatchData &)>
+        get_resource_usage_batch_for_broadcast,
+    std::function<void(const rpc::Address &,
+                       std::shared_ptr<rpc::NodeManagerClientPool> &, std::string &)>
+        send_batch)
+    : ticker_(broadcast_service_),
+      raylet_client_pool_(raylet_client_pool),
+      get_resource_usage_batch_for_broadcast_(get_resource_usage_batch_for_broadcast),
+      send_batch_(send_batch),
+      broadcast_period_ms_(
+          RayConfig::instance().raylet_report_resources_period_milliseconds()) {}
 
+GcsResourceReportBroadcaster::~GcsResourceReportBroadcaster() {}
 
+void GcsResourceReportBroadcaster::Initialize(const GcsInitData &gcs_init_data) {
+  for (const auto &pair : gcs_init_data.Nodes()) {
+    HandleNodeAdded(pair.second);
+  }
 }
+
+void GcsResourceReportBroadcaster::Start() {
+  broadcast_thread_.reset(new std::thread{[this]() {
+    SetThreadName("resource_report_broadcaster");
+    boost::asio::io_service::work work(broadcast_service_);
+
+    broadcast_service_.run();
+    RAY_LOG(DEBUG)
+        << "GCSResourceReportBroadcaster has stopped. This should only happen if "
+           "the cluster has stopped";
+  }});
+  ticker_.RunFnPeriodically(
+      [this] { SendBroadcast(); }, broadcast_period_ms_,
+      "GcsResourceReportBroadcaster.deadline_timer.pull_resource_report");
 }
+
+void GcsResourceReportBroadcaster::Stop() {
+  if (broadcast_thread_ != nullptr) {
+    // TODO (Alex): There's technically a race condition here if we start and stop the
+    // thread in rapid succession.
+    broadcast_service_.stop();
+    if (broadcast_thread_->joinable()) {
+      broadcast_thread_->join();
+    }
+  }
+}
+
+void GcsResourceReportBroadcaster::HandleNodeAdded(const rpc::GcsNodeInfo &node_info) {
+  rpc::Address address;
+  address.set_raylet_id(node_info.node_id());
+  address.set_ip_address(node_info.node_manager_address());
+  address.set_port(node_info.node_manager_port());
+
+  NodeID node_id = NodeID::FromBinary(node_info.node_id());
+
+  absl::MutexLock guard(&mutex_);
+  nodes_[node_id] = std::move(address);
+}
+
+void GcsResourceReportBroadcaster::HandleNodeRemoved(const rpc::GcsNodeInfo &node_info) {
+  NodeID node_id = NodeID::FromBinary(node_info.node_id());
+  {
+    absl::MutexLock guard(&mutex_);
+    nodes_.erase(node_id);
+    RAY_CHECK(!nodes_.count(node_id));
+    RAY_LOG(DEBUG) << "Node removed (node_id: " << node_id
+                   << ")# of remaining nodes: " << nodes_.size();
+  }
+}
+
+void GcsResourceReportBroadcaster::SendBroadcast() {
+  rpc::ResourceUsageBatchData batch;
+  get_resource_usage_batch_for_broadcast_(batch);
+
+  // Serializing is relatively expensive on large batches, so we should only do it once.
+  std::string serialized_batch = batch.SerializeAsString();
+
+  absl::MutexLock guard(&mutex_);
+  for (const auto &pair : nodes_) {
+    const auto *address = pair.second;
+    send_batch_(address, raylet_client_pool_, serialized_batch);
+  }
+}
+
+}  // namespace gcs
+}  // namespace ray
--- src/ray/gcs/gcs_server/gcs_resource_report_broadcaster.h
+++ src/ray/gcs/gcs_server/gcs_resource_report_broadcaster.h
@@ -7,15 +7,14 @@ namespace gcs {
 
 /// Broadcasts resource report batches to raylets from a separate thread.
 class GcsResourceReportBroadcaster {
-
  public:
-
   GcsResourceReportBroadcaster(
-                               std::shared_ptr<rpc::NodeManagerClientPool> raylet_client_pool,
-                               std::function<void(rpc::ResourceUsageBatchData &)> get_resource_usage_batch_for_broadcast,
-                               std::function<void(const rpc::Address &, std::shared_ptr<rpc::NodeManagerClientPool> &, std::string &)> send_batch
-                               );
-
+      std::shared_ptr<rpc::NodeManagerClientPool> raylet_client_pool,
+      std::function<void(rpc::ResourceUsageBatchData &)>
+          get_resource_usage_batch_for_broadcast,
+      std::function<void(const rpc::Address &,
+                         std::shared_ptr<rpc::NodeManagerClientPool> &, std::string &)>
+          send_batch);
   ~GcsResourceReportBroadcaster();
 
   void Initialize(const GcsInitData &gcs_init_data);
@@ -33,11 +32,30 @@ class GcsResourceReportBroadcaster {
   void HandleNodeRemoved(const rpc::GcsNodeInfo &node_info) LOCKS_EXCLUDED(mutex_);
 
  private:
+  // An asio service which does the broadcasting work.
+  instrumented_io_context broadcast_service_;
+  // The associated thread it runs on.
+  std::unique_ptr<std::thread> broadcast_thread_;
+  // Timer tick to send the next broadcast round.
+  PeriodicalRunner ticker_;
+
   // The shared, thread safe pool of raylet clients, which we use to minimize connections.
   std::shared_ptr<rpc::NodeManagerClientPool> raylet_client_pool_;
-  /// See GcsResourcManager::GetResourceUsageBatchForBroadcast. This is passed as an argument for unit testing purposes only.
-  std::function<void(rpc::ResourceUsageBatchData &)> get_resource_usage_batch_for_broadcast_;
-  std::function<void(const rpc::Address &, std::shared_ptr<rpc::NodeManagerClientPool> &, std::string &)> send_batch_;
+  /// See GcsResourcManager::GetResourceUsageBatchForBroadcast. This is passed as an
+  /// argument for unit testing purposes only.
+  std::function<void(rpc::ResourceUsageBatchData &)>
+      get_resource_usage_batch_for_broadcast_;
+  std::function<void(const rpc::Address &, std::shared_ptr<rpc::NodeManagerClientPool> &,
+                     std::string &)>
+      send_batch_;
 
+  // A lock to protect nodes_
+  absl::Mutex mutex_;
+  std::unordered_map<NodeID, rpc::Address> nodes_ GUARDED_BY(mutex_);
 
-}
+  uint64_t broadcast_period_ms_;
+
+  void SendBroadcast();
+};
+}  // namespace gcs
+}  // namespace ray
