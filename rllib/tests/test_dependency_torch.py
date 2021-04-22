f148ddc4ae12dcc37f601396f70fac1f07b2ef9f
sven1977 <svenmika1977@gmail.com>
2021-04-22 14:44:38 +0200

    wip

--- rllib/tests/test_dependency_torch.py
+++ rllib/tests/test_dependency_torch.py
@@ -8,21 +8,21 @@ if __name__ == "__main__":
     # Do not import torch for testing purposes.
     os.environ["RLLIB_TEST_NO_TORCH_IMPORT"] = "1"
 
-    from ray.rllib.utils.framework import try_import_tf
-    tf1, tf, tfv = try_import_tf()
-    import re
-    tf_file = tf.__file__
-    torch_file = re.sub("tensorflow", "torch", tf_file)
-    print(torch_file)
-    assert os.path.isfile(torch_file)
-    with open(torch_file, "w") as f:
-        print("""
-import traceback
-print('someone is trying to import torch!')
-for line in traceback.format_stack():
-    print(line.strip())
-raise ImportError('bad torch-importer!!')
-""", file=f)
+    #from ray.rllib.utils.framework import try_import_tf
+    #tf1, tf, tfv = try_import_tf()
+    #import re
+    #tf_file = tf.__file__
+    #torch_file = re.sub("tensorflow", "torch", tf_file)
+    #print(torch_file)
+    #assert os.path.isfile(torch_file)
+    #with open(torch_file, "w") as f:
+    #    print("""
+#import traceback
+#print('someone is trying to import torch!')
+#for line in traceback.format_stack():
+#    print(line.strip())
+#raise ImportError('bad torch-importer!!')
+#""", file=f)
 
     from ray.rllib.agents.a3c import A2CTrainer
     #if "torch" in sys.modules:
