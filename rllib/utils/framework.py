9043d46670e8f0037656843dfd462c602446e8bf
sven1977 <svenmika1977@gmail.com>
2021-04-22 12:23:24 +0200

    wip

--- rllib/utils/framework.py
+++ rllib/utils/framework.py
@@ -175,15 +175,13 @@ def try_import_torch(error=False):
         return _torch_stubs()
 
     try:
-        print("trying to import torch")
         import torch
         import torch.nn as nn
         return torch, nn
-    except Exception as e:
-        print("failed!")
-        #if error:
-        raise e
-        #return _torch_stubs()
+    except ImportError as e:
+        if error:
+            raise e
+        return _torch_stubs()
 
 
 def _torch_stubs():
