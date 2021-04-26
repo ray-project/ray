053bdf2a410c1cc36219d5ae83789e4b7887108a
Alex <alex@anyscale.com>
2021-04-26 20:02:38 +0000

    lint

--- src/ray/gcs/gcs_server/test/gcs_server_test_util.h
+++ src/ray/gcs/gcs_server/test/gcs_server_test_util.h
@@ -248,7 +248,7 @@ struct GcsServerMocker {
 
     /// ResourceUsageInterface
     void UpdateResourceUsage(
-                             std::string &address,
+        std::string &address,
         const rpc::ClientCallback<rpc::UpdateResourceUsageReply> &callback) override {
       RAY_CHECK(false) << "Unused";
     };
