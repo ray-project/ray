1f43976e48f509e8abbdc3fff10a0b017bdaf16d
Sven Mika <sven@anyscale.io>
2021-04-27 08:41:20 +0200

    Apply suggestions from code review

--- rllib/agents/sac/sac_tf_policy.py
+++ rllib/agents/sac/sac_tf_policy.py
@@ -294,7 +294,8 @@ def sac_actor_critic_loss(
             model_out_t, tf.cast(train_batch[SampleBatch.ACTIONS], tf.float32))
         if policy.config["twin_q"]:
             twin_q_t = model.get_twin_q_values(
-                model_out_t, tf.cast(train_batch[SampleBatch.ACTIONS], tf.float32))
+                model_out_t,
+                tf.cast(train_batch[SampleBatch.ACTIONS], tf.float32))
 
         # Q-values for current policy in given current state.
         q_t_det_policy = model.get_q_values(model_out_t, policy_t)
