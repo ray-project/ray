cdd37db2f697ea5516f4ceada5cc8f7f74154b8e
Edward Oakes <ed.nmi.oakes@gmail.com>
2021-04-29 16:57:38 -0500

    up to core APIs

--- python/ray/serve/examples/doc/aiohttp/aiohttp_deploy_serve.py
+++ python/ray/serve/examples/doc/aiohttp/aiohttp_deploy_serve.py
@@ -6,7 +6,7 @@ from ray import serve
 ray.init(address="auto")
 
 # Start a detached Ray Serve instance.  It will persist after the script exits.
-client = serve.start(http_host=None, detached=True)
+serve.start(http_host=None, detached=True)
 
 
 # Define a function to serve. Alternatively, you could define a stateful class.
@@ -17,5 +17,5 @@ async def my_model(request):
 
 # Set up a backend with the desired number of replicas and set up an endpoint.
 backend_config = serve.BackendConfig(num_replicas=2)
-client.create_backend("my_backend", my_model, config=backend_config)
-client.create_endpoint("my_endpoint", backend="my_backend")
+serve.create_backend("my_backend", my_model, config=backend_config)
+serve.create_endpoint("my_endpoint", backend="my_backend")
