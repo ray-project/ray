9b2ac0a019cbe99170c037c136c86f969588b9a9
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2021-04-29 11:44:27 -0700

    Hook added (#15573)
    * add hook for client server
    
    * fix

--- python/ray/util/client/server/__main__.py
+++ python/ray/util/client/server/__main__.py
@@ -1,3 +1,8 @@
 if __name__ == "__main__":
     from ray.util.client.server.server import main
+    try:
+        from anyscale.utils.runtime_env import runtime_env_setup
+        runtime_env_setup()
+    except:
+        print("Failed to run setup")
     main()
