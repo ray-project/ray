59409e9e45745524ca5a29cd0d7d923499cbb66b
Yi Cheng <chengyidna@gmail.com>
2021-04-29 18:43:54 +0000

    fix

--- python/ray/util/client/server/__main__.py
+++ python/ray/util/client/server/__main__.py
@@ -1,8 +1,8 @@
 if __name__ == "__main__":
     from ray.util.client.server.server import main
     try:
-        import anyscale
-        anyscale.utils.runtime_env.runtime_env_setup()
+        from anyscale.utils.runtime_env import runtime_env_setup
+        runtime_env_setup()
     except:
         print("Failed to run setup")
     main()
