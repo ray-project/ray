bfdded04f913f9b4ed1bf870266d5a1c709e7466
Kathryn Zhou <kathrynzhou@Kathryns-MacBook-Pro.local>
2021-04-30 09:10:48 -1000

    remove tracing test from windows

--- ci/travis/ci.sh
+++ ci/travis/ci.sh
@@ -177,6 +177,7 @@ test_python() {
       -python/ray/tests:test_stress  # timeout
       -python/ray/tests:test_stress_sharded  # timeout
       -python/ray/tests:test_k8s_operator_mock
+      -python/ray/tests:test_tracing  # tracing not enabled on windows
     )
   fi
   if [ 0 -lt "${#args[@]}" ]; then  # Any targets to test?
