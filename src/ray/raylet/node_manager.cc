f1510e227573e01cd218d8f232aed30b1bad07aa
SangBin Cho <rkooo567@gmail.com>
2021-05-06 09:50:36 -0700

    Improved bad error message.

--- src/ray/raylet/node_manager.cc
+++ src/ray/raylet/node_manager.cc
@@ -1215,8 +1215,13 @@ void NodeManager::DisconnectClient(
         // TODO(rkn): Define this constant somewhere else.
         std::string type = "worker_died";
         std::ostringstream error_message;
-        error_message << "A worker died or was killed while executing task " << task_id
-                      << ".";
+        error_message
+            << "A worker died or was killed while executing task by an unexepcted system "
+               "error. To troubleshoot the problem, check out a worker log. Task id: "
+            << task_id << " Worker id: " << worker->WorkerId()
+            << " Node id: " << self_node_id_
+            << " Worker IP address: " << worker->IpAddress()
+            << " Worker port: " << worker->Port();
         auto error_data_ptr = gcs::CreateErrorTableData(type, error_message.str(),
                                                         current_time_ms(), job_id);
         RAY_CHECK_OK(gcs_client_->Errors().AsyncReportJobError(error_data_ptr, nullptr));
