57088ed22dcdf95e4f47f56ca0327487f3d00b5c
Eric Liang <ekhliang@gmail.com>
2021-05-06 11:59:34 -0700

    udpate

--- release/nightly_tests/shuffle/shuffle_10gb.py
+++ release/nightly_tests/shuffle/shuffle_10gb.py
@@ -1,10 +1,14 @@
-from ray.experimental import shuffle
-
+import subprocess
+import time
+import json
 
 if __name__ == "__main__":
     start = time.time()
     # 10GB shuffle
-    subprocess.check_call(["python", "-m", "ray.experimental.shuffle", "--ray-address=auto", "--num-partitions=50", "--partition-size=200e6"])
+    subprocess.check_call([
+        "python", "-m", "ray.experimental.shuffle", "--ray-address=auto",
+        "--num-partitions=50", "--partition-size=200e6"
+    ])
     delta = time.time() - start
     with open("/tmp/shuffle_10gb.json", "w") as f:
         f.write(json.dumps({"shuffle_time": delta}))
