2fd52c1e496705578536a361343246215224e948
Kai Fricke <kai@anyscale.com>
2021-05-07 14:01:19 +0100

    Adjust script/release test json

--- release/nightly_tests/shuffle/shuffle_10gb.py
+++ release/nightly_tests/shuffle/shuffle_10gb.py
@@ -1,6 +1,8 @@
 import subprocess
 import time
 import json
+import os
+
 
 if __name__ == "__main__":
     start = time.time()
@@ -10,5 +12,9 @@ if __name__ == "__main__":
         "--num-partitions=50", "--partition-size=200e6"
     ])
     delta = time.time() - start
-    with open("/tmp/shuffle_10gb.json", "w") as f:
+
+    test_output_json = os.environ.get("TEST_OUTPUT_JSON",
+                                      "/tmp/shuffle_10gb.json")
+
+    with open(test_output_json, "wt") as f:
         f.write(json.dumps({"shuffle_time": delta}))
