43183eaa6df276ccbe9bdc20d9a3ef3a3fe8d3e6
Ian <ian.rodney@gmail.com>
2021-05-09 05:16:31 +0000

    docstrings

--- python/ray/client_builder.py
+++ python/ray/client_builder.py
@@ -13,6 +13,9 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class ClientInfo:
+    """
+    Basic information of the remote server for a given Ray Client connection.
+    """
     dashboard_url: str
     python_version: str
     ray_version: str
@@ -21,15 +24,24 @@ class ClientInfo:
 
 
 class ClientBuilder:
+    """
+    Builder for a Ray Client connection.
+    """
     def __init__(self, address: Optional[str]) -> None:
         self.address = address
         self.job_config = JobConfig()
 
     def env(self, env: Dict[str, Any]) -> "ClientBuilder":
+        """
+        Set an environment for the session.
+        """
         self.job_config = JobConfig(runtime_env=env)
         return self
 
     def connect(self) -> ClientInfo:
+        """
+        Begin a connection to the address passed in via ray.client(...).
+        """
         client_info_tuple = ray.util.connect(
             self.address, job_config=self.job_config)
         return ClientInfo(
@@ -55,6 +67,14 @@ def _get_builder_from_address(address: Optional[str]) -> ClientBuilder:
 
 
 def client(address: Optional[str] = None) -> ClientBuilder:
+    """
+    Creates a ClientBuilder based on the provided address. The address can be
+    of the following forms:
+    * None -> Connects to or creates a local cluster and connects to it.
+    * local -> Creates a new cluster locally and connects to it.
+    * IP:Port -> Connects to a Ray Client Server at the given address.
+    * module://inner_addres -> load module.ClientBuilder and pass inner_address
+    """
     override_address = os.environ.get(RAY_ADDRESS_ENVIRONMENT_VARIABLE)
     if override_address:
         logger.debug(
