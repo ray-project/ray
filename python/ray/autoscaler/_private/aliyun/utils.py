bc12e6ce26de2df442ef07b4d2fd493988ffc560
zhuang <zhengchicheng.zcc@alibaba-inc.com>
2021-05-12 06:47:57 +0800

    fix format

--- python/ray/autoscaler/_private/aliyun/utils.py
+++ python/ray/autoscaler/_private/aliyun/utils.py
@@ -75,16 +75,17 @@ class AcsClient:
             return instance_list
         return None
 
-    def create_instance(self,
-                        instance_type,
-                        image_id,
-                        tags,
-                        key_pair_name,
-                        optimized="optimized",
-                        instance_charge_type="PostPaid",
-                        spot_strategy="SpotWithPriceLimit",
-                        internet_charge_type="PayByTraffic",
-                        internet_max_bandwidth_out=5,
+    def create_instance(
+        self,
+        instance_type,
+        image_id,
+        tags,
+        key_pair_name,
+        optimized="optimized",
+        instance_charge_type="PostPaid",
+        spot_strategy="SpotWithPriceLimit",
+        internet_charge_type="PayByTraffic",
+        internet_max_bandwidth_out=5,
     ):
         request = CreateInstanceRequest()
         request.set_InstanceType(instance_type)
@@ -107,19 +108,19 @@ class AcsClient:
         return None
 
     def run_instances(
-            self,
-            instance_type,
-            image_id,
-            tags,
-            security_group_id,
-            vswitch_id,
-            key_pair_name,
-            amount=1,
-            optimized="optimized",
-            instance_charge_type="PostPaid",
-            spot_strategy="SpotWithPriceLimit",
-            internet_charge_type="PayByTraffic",
-            internet_max_bandwidth_out=1,
+        self,
+        instance_type,
+        image_id,
+        tags,
+        security_group_id,
+        vswitch_id,
+        key_pair_name,
+        amount=1,
+        optimized="optimized",
+        instance_charge_type="PostPaid",
+        spot_strategy="SpotWithPriceLimit",
+        internet_charge_type="PayByTraffic",
+        internet_max_bandwidth_out=1,
     ):
         request = RunInstancesRequest()
         request.set_InstanceType(instance_type)
