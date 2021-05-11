ddaa538bbad24e7100307dea886421354a4cbc9f
Ian <ian.rodney@gmail.com>
2021-05-11 15:33:06 +0000

    responding to first comments

--- python/ray/client_builder.py
+++ python/ray/client_builder.py
@@ -2,11 +2,12 @@ import os
 import importlib
 import logging
 from dataclasses import dataclass
+from urllib.parse import urlparse
 from typing import Any, Dict, Optional, Tuple
 
 from ray.ray_constants import RAY_ADDRESS_ENVIRONMENT_VARIABLE
 from ray.job_config import JobConfig
-import ray.util
+import ray.util.client_connect
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +17,7 @@ class ClientInfo:
     """
     Basic information of the remote server for a given Ray Client connection.
     """
-    dashboard_url: str
+    dashboard_url: Optional[str]
     python_version: str
     ray_version: str
     ray_commit: str
@@ -43,10 +44,12 @@ class ClientBuilder:
         """
         Begin a connection to the address passed in via ray.client(...).
         """
-        client_info_tuple = ray.util.connect(
+        client_info_tuple = ray.util.client_connect.connect(
             self.address, job_config=self.job_config)
+        dashboard_url = ray.get(
+            ray.remote(ray.worker.get_dashboard_url).remote())
         return ClientInfo(
-            dashboard_url=None,
+            dashboard_url=dashboard_url,
             python_version=client_info_tuple["python_version"],
             ray_version=client_info_tuple["ray_version"],
             ray_commit=client_info_tuple["ray_commit"],
@@ -59,25 +62,20 @@ class _LocalClientBuilder(ClientBuilder):
 
 def _split_address(address: str) -> Tuple[str, str]:
     """
-    Splits address by "://" into a module string and an inner_address.
-    NOTE: inner_address may contain "://"
+    Splits address into a module string (scheme) and an inner_address.
     """
-    module_start_idx = address.find("://")
-    module_string = address[0:module_start_idx]
-    inner_address_start_idx = module_start_idx + len("://")
-    inner_address = address[inner_address_start_idx:]
+    url_object = urlparse(address)
+    module_string = url_object.scheme or "ray"
+    inner_address = "".join(url_object[1:])
     return (module_string, inner_address)
 
 
 def _get_builder_from_address(address: Optional[str]) -> ClientBuilder:
     if address is None or address == "local":
         return _LocalClientBuilder(address)
-    elif address.find("://") == -1:
-        return ClientBuilder(address)
-    else:
-        module_string, inner_address = _split_address(address)
-        module = importlib.import_module(module_string)
-        return module.ClientBuilder(inner_address)
+    module_string, inner_address = _split_address(address)
+    module = importlib.import_module(module_string)
+    return module.ClientBuilder(inner_address)
 
 
 def client(address: Optional[str] = None) -> ClientBuilder:
@@ -87,7 +85,7 @@ def client(address: Optional[str] = None) -> ClientBuilder:
     * None -> Connects to or creates a local cluster and connects to it.
     * local -> Creates a new cluster locally and connects to it.
     * IP:Port -> Connects to a Ray Client Server at the given address.
-    * module://inner_addres -> load module.ClientBuilder and pass inner_address
+    * module://inner_address -> load module.ClientBuilder & pass inner_address
     """
     override_address = os.environ.get(RAY_ADDRESS_ENVIRONMENT_VARIABLE)
     if override_address:
