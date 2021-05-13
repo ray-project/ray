0c3e8bda3e5f0bc96f243ba23a177f597c95c120
Fabiano Cardoso <fabianomcardoso@gmail.com>
2021-05-13 17:49:13 -0300

    Fixing script error

--- ci/travis/install-bazel.sh
+++ ci/travis/install-bazel.sh
@@ -77,13 +77,13 @@ if [ "${TRAVIS-}" = true ]; then
   # of jobs to 50
   # NOTE: Normally --jobs should be under 'build:ci-travis' in .bazelrc, but we put
   # it under 'build' here avoid conflicts with other --config options.
-  num_cores = `nproc`
-  echo "build --jobs=" `expr $num_cores + 8` >> ~/.bazelrc
+  num_cores=`nproc`
+  echo "build --jobs="`expr $num_cores + 8` >> ~/.bazelrc
 fi
 if [ "${GITHUB_ACTIONS-}" = true ]; then
   echo "build --config=ci-github" >> ~/.bazelrc
-  num_cores = `nproc`
-  echo "build --jobs=" `expr $num_cores + 8` >> ~/.bazelrc
+  num_cores=`nproc`
+  echo "build --jobs="`expr $num_cores + 8` >> ~/.bazelrc
 fi
 if [ "${CI-}" = true ]; then
   echo "build --config=ci" >> ~/.bazelrc
