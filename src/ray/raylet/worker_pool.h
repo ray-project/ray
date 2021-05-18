1c9d23712fe11f4686fab06618b1c18ec46ef5ac
architkulkarni <arkulkar@gmail.com>
2021-05-17 19:51:39 -0700

    add comments for private members of WorkerCacheKey

--- src/ray/raylet/worker_pool.h
+++ src/ray/raylet/worker_pool.h
@@ -76,8 +76,12 @@ class WorkerCacheKey {
   int IntHash() const;
 
  private:
+  /// The environment variable overrides for this worker.
   const std::unordered_map<std::string, std::string> override_environment_variables;
+  /// The JSON-serialized runtime env for this worker.
   const std::string serialized_runtime_env;
+  /// The cached hash of the worker's environment.  This is set to 0
+  /// for unspecified or empty environments.
   mutable std::size_t hash_ = 0;
 };
 
