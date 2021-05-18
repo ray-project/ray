56a4e1fb30e0deb3ed2349a1732d0c06f7faeb5b
JAEHYUN SIM <jaehyunsim@JAEHYUNs-MacBook-Pro-2.local>
2021-05-18 04:18:50 -0400

    Added check for no existing deployment case for prev_version

--- python/ray/serve/controller.py
+++ python/ray/serve/controller.py
@@ -284,9 +284,8 @@ class ServeController:
                 existing_backend_info = self.backend_state.get_backend(name)
                 if (existing_backend_info is None
                         or not existing_backend_info.version):
-                    raise ValueError(
-                        f"prev_version '{prev_version}' is specified but "
-                        "there is no existing deployment")
+                    raise ValueError(f"prev_version '{prev_version}' is specified but "
+                                     "there is no existing deployment")
                 if existing_backend_info.version != prev_version:
                     raise ValueError(f"prev_version '{prev_version}' does not match with "
                                      "the existing version '{existing_backend_info.version}'")
