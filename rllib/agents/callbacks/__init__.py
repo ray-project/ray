5818d65758c23276fccacfde9f875b6eb74637ea
sven1977 <svenmika1977@gmail.com>
2021-05-18 11:49:11 +0200

    wip

--- rllib/agents/callbacks/__init__.py
+++
@@ -1,9 +0,0 @@
-from ray.rllib.agents.callbacks.default_callbacks import DefaultCallbacks
-from ray.rllib.agents.callbacks.memory_callbacks import MemoryTrackingCallbacks
-from ray.rllib.agents.callbacks.multi_callbacks import MultiCallbacks
-
-__all__ = [
-    "DefaultCallbacks",
-    "MemoryTrackingCallbacks",
-    "MultiCallbacks",
-]
