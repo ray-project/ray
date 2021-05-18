831af01046df8f970ea33dcc0201f622aba82010
Frank Luan <lsf@berkeley.edu>
2021-05-18 16:18:15 -0700

    Fix test

--- python/ray/tests/test_object_spilling.py
+++ python/ray/tests/test_object_spilling.py
@@ -8,8 +8,7 @@ import numpy as np
 import pytest
 import ray
 from ray.tests.conftest import (file_system_object_spilling_config,
-                                mock_distributed_fs_object_spilling_config,
-                                slow_spilling_config)
+                                mock_distributed_fs_object_spilling_config)
 from ray.external_storage import (create_url_with_offset,
                                   parse_url_with_offset)
 from ray.test_utils import wait_for_condition
