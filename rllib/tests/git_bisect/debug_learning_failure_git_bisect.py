13a267e381012966ffb3ad7dbf403d86cd868d14
sven1977 <svenmika1977@gmail.com>
2021-05-19 13:52:42 +0200

    wip

--- rllib/tests/git_bisect/debug_learning_failure_git_bisect.py
+++ rllib/tests/git_bisect/debug_learning_failure_git_bisect.py
@@ -39,38 +39,53 @@ parser.add_argument(
     "--run",
     type=str,
     default=None,
-    help="The RLlib-registered algorithm to use.")
-parser.add_argument("--torch", action="store_true")
-parser.add_argument("--skip-install-ray", action="store_true")
-parser.add_argument("--stop-iters", type=int, default=None)
-parser.add_argument("--stop-timesteps", type=int, default=None)
-parser.add_argument("--stop-time", type=int, default=None)
-parser.add_argument("--stop-reward", type=float, default=None)
-parser.add_argument("-f", type=str, default=None)
-parser.add_argument("--config", type=str, default=None)
-parser.add_argument("--env", type=str, default=None)
+    help="The RLlib-registered algorithm to use, even if -f (yaml file) given "
+         "(will override yaml run setting).")
+parser.add_argument(
+    "--framework",
+    choices=["tf", "tf2", "tfe", "torch"],
+    default=None,
+    help="The DL framework specifier.")
+parser.add_argument(
+    "--skip-install-ray", action="store_true",
+    help="If set, do not attempt to re-build ray from source.")
+parser.add_argument(
+    "--stop-iters",
+    type=int,
+    default=None,
+    help="Number of iterations to train. Skip if this criterium is not "
+         "important.")
+parser.add_argument(
+    "--stop-timesteps", type=int, default=None,
+    help="Number of env timesteps to train. Can be used in combination with "
+         "--stop-time to assertain we reach a certain (env) "
+         "timesteps per (wall) time interval. Skip if this "
+         "criterium is not important.")
+parser.add_argument(
+    "--stop-time", type=int, default=None,
+    help="Time in seconds, when to stop the run. Can be used in combination "
+         "with --stop-timesteps to assertain we reach a certain (env) "
+         "timesteps per (wall) time interval. Skip if this criterium is "
+         "not important.")
+parser.add_argument(
+    "--stop-reward", type=float, default=None,
+    help="The minimum reward that must be reached within the given "
+         "time/timesteps/iters. Skip if this criterium is not important.")
+parser.add_argument(
+    "-f", type=str, default=None,
+    help="The yaml file to use as config. Alternatively, use --run, --config, "
+         "and --env.")
+parser.add_argument(
+    "--config", type=str, default=None,
+    help="If no -f (yaml file) given, use this config instead.")
+parser.add_argument(
+    "--env", type=str, default=None,
+    help="Sets the env to use, even if -f (yaml file) given "
+         "(will override yaml env setting).")
 
 if __name__ == "__main__":
 
     args = parser.parse_args()
-
-    try:
-        subprocess.run(["ray", "stop"])
-    except Exception:
-        pass
-
-    # Install ray from the checked out repo.
-    if not args.skip_install_ray:
-        # Assume we are in the ray (git clone) directory.
-        try:
-            subprocess.run(["pip", "uninstall", "-y", "ray"])
-        except Exception:
-            pass
-        subprocess.run(["ci/travis/install-bazel.sh"])
-        os.chdir("python")
-        subprocess.run(["pip", "install", "-e", ".", "--verbose"])
-
-    subprocess.run(["ray", "start", "--head", "--include-dashboard", "false"])
 
     run = None
 
@@ -93,19 +108,11 @@ if __name__ == "__main__":
     if args.run:
         run = args.run
 
-    # Explicit --torch framework.
-    if args.torch:
-        config["framework"] = "torch"
-    # Framework not specified in config, try to infer it.
-    if "framework" not in config:
-        config["framework"] = "torch" if args.torch else "tf"
+    # Set --framework, if provided.
+    if args.framework:
+        config["framework"] = args.framework
 
-    import ray
-    from ray import tune
-    from ray.rllib.utils.test_utils import check_learning_achieved
-
-    ray.init()
-
+    # Define stopping criteria.
     stop = {}
     if args.stop_iters:
         stop["training_iteration"] = args.stop_iters
@@ -113,6 +120,40 @@ if __name__ == "__main__":
         stop["timesteps_total"] = args.stop_timesteps
     if args.stop_reward:
         stop["episode_reward_mean"] = args.stop_reward
+    if args.stop_time:
+        stop["time_total_s"] = args.stop_time
+
+    # - Stop ray.
+    # - Uninstall and re-install ray (from source) if required.
+    # - Start ray.
+    try:
+        subprocess.run(["ray", "stop"])
+    except Exception:
+        pass
+
+    # Install ray from the checked out repo.
+    if not args.skip_install_ray:
+        subprocess.run(["sudo", "apt-get", "update"])
+        subprocess.run(["sudo", "apt-get", "install", "-y",
+                        "build-essential", "curl", "unzip", "psmisc"])
+        subprocess.run(["pip", "install", "cython==0.29.0", "pytest"])
+        # Assume we are in the ray (git clone) directory.
+        try:
+            subprocess.run(["pip", "uninstall", "-y", "ray"])
+        except Exception:
+            pass
+        subprocess.run(["ci/travis/install-bazel.sh"])
+        os.chdir("python")
+        subprocess.run(["pip", "install", "-e", ".", "--verbose"])
+        os.chdir("../")
+
+    subprocess.run(["ray", "start", "--head", "--include-dashboard", "false"])
+
+    # Run the training experiment.
+    import ray
+    from ray import tune
+
+    ray.init()
 
     results = tune.run(run, stop=stop, config=config)
 
@@ -123,21 +164,18 @@ if __name__ == "__main__":
         if avg_reward < args.stop_reward:
             raise ValueError(
                 "`stop-reward` of {} not reached!".format(args.stop_reward))
-        print("ok")
-
-        check_learning_achieved(results, args.stop_reward)
     # Criterium is to have run through n env timesteps in some wall time m.
     elif args.stop_timesteps and args.stop_time:
         last_result = results.trials[0].last_result
         total_timesteps = last_result["timesteps_total"]
         # We stopped because we reached the time limit ->
         # Means throughput is too slow (time steps not reached).
-        if args.stop_timesteps < total_timesteps - 100:
+        if total_timesteps - 100 < args.stop_timesteps:
             raise ValueError(
                 "`stop-timesteps` of {} not reached in {}sec!".format(
                     args.stop_timesteps, args.stop_time))
-        print("ok")
     else:
         raise ValueError("Invalid pass criterium!")
 
+    print("ok")
     ray.shutdown()
