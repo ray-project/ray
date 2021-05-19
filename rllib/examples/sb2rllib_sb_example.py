5cbe4986a2842d556aebdb02991baf435c0fed87
Sven Mika <sven@anyscale.io>
2021-05-19 09:25:20 +0200

    Update rllib/examples/sb2rllib_sb_example.py

--- rllib/examples/sb2rllib_sb_example.py
+++ rllib/examples/sb2rllib_sb_example.py
@@ -2,7 +2,7 @@
 Example script on how to train, save, load, and test a stable baselines 2 agent
 Code taken and adjusted from SB2 docs:
 https://stable-baselines.readthedocs.io/en/master/guide/quickstart.html
-Equivalent script with RLlib: sb2rllib_sb_example.py
+Equivalent script with RLlib: sb2rllib_rllib_example.py
 """
 import gym
 
