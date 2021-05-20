a183e43098d6ccef28fe966cb898ac375c95c7fa
Amog Kamsetty <amogkamsetty@yahoo.com>
2021-05-20 09:18:27 -0700

    update cifar example

--- python/ray/tune/examples/cifar10_pytorch.py
+++ python/ray/tune/examples/cifar10_pytorch.py
@@ -160,25 +160,6 @@ def train_cifar(config, checkpoint_dir=None):
 
 
 # __test_acc_begin__
-def test_accuracy(net, device="cpu"):
-    trainset, testset = load_data()
-
-    testloader = torch.utils.data.DataLoader(
-        testset, batch_size=4, shuffle=False, num_workers=2)
-
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data
-            images, labels = images.to(device), labels.to(device)
-            outputs = net(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-    return correct / total
-# __test_acc_end__
 
 def test_best_model(best_trial):
     best_trained_model = Net(best_trial.config["l1"], best_trial.config["l2"])
@@ -190,8 +171,26 @@ def test_best_model(best_trial):
     model_state, optimizer_state = torch.load(checkpoint_path)
     best_trained_model.load_state_dict(model_state)
 
-    test_acc = test_accuracy(best_trained_model, device)
-    print("Best trial test set accuracy: {}".format(test_acc))
+    trainset, testset = load_data()
+
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=4, shuffle=False, num_workers=2)
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            outputs = best_trained_model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+
+    print("Best trial test set accuracy: {}".format(correct / total))
+
+# __test_acc_end__
 
 # __main_begin__
 def main(num_samples=10, max_num_epochs=10, gpus_per_trial=2):
