02f0c50d7dd5b49abb21b5de2cf0157a24165375
Alex <alex@anyscale.com>
2021-05-20 19:52:11 +0000

    merge

--- python/ray/tests/test_client_builder.py
+++ python/ray/tests/test_client_builder.py
@@ -10,6 +10,7 @@ from ray.test_utils import run_string_as_driver_nonblocking,\
 
 from ray.cluster_utils import Cluster
 
+
 @pytest.mark.parametrize("address", [
     "localhost:1234", "localhost:1234/url?params",
     "1.2.3.4/cluster-1?test_param=param1?"
