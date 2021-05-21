4ce9a35aa5b8e529a4e33a1ce7bd95cb1fc7e6d5
Daniel Dworak <dworak.daniel.94@gmail.com>
2021-05-21 12:21:28 +0200

    fix rnnsac policy to correctly store td_error (changes from older version)

--- rllib/agents/sac/rnnsac_torch_policy.py
+++ rllib/agents/sac/rnnsac_torch_policy.py
@@ -384,7 +384,12 @@ def actor_critic_loss(
     policy.q_t = q_t
     policy.policy_t = policy_t
     policy.log_pis_t = log_pis_t
-    policy.td_error = td_error
+
+    # Store td-error in model, such that for multi-GPU, we do not override
+    # them during the parallel loss phase. TD-error tensor in final stats
+    # can then be concatenated and retrieved for each individual batch item.
+    model.td_error = td_error
+
     policy.actor_loss = actor_loss
     policy.critic_loss = critic_loss
     policy.alpha_loss = alpha_loss
