c73d90a5387c570b55e3d19795353d857c68c483
Eric Liang <ekhliang@gmail.com>
2021-05-21 16:07:02 -0700

    note

--- src/ray/object_manager/plasma/store.cc
+++ src/ray/object_manager/plasma/store.cc
@@ -232,7 +232,9 @@ uint8_t *PlasmaStore::AllocateMemory(size_t size, MEMFD_TYPE *fd, int64_t *map_s
     int64_t space_needed = eviction_policy_.RequireSpace(size, &objects_to_evict);
     EvictObjects(objects_to_evict);
     // More space is still needed. Try to spill objects to external storage to
-    // make room.
+    // make room. NOTE(ekl) if we can't achieve this after a number of retries,
+    // it's because memory fragmentation in dlmalloc prevents us from allocating
+    // even if our footprint tracker here still says we have free space.
     if (space_needed > 0 || num_tries++ > 10) {
       *error = PlasmaError::OutOfMemory;
       break;
