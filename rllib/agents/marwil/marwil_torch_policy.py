0b6ef21e3cb2428db3709e7734f2e315d28223b7
sven1977 <svenmika1977@gmail.com>
2021-05-24 13:44:32 +0200

    wip

--- rllib/agents/marwil/marwil_torch_policy.py
+++ rllib/agents/marwil/marwil_torch_policy.py
@@ -25,26 +25,38 @@ def marwil_loss(policy: Policy, model: ModelV2, dist_class: ActionDistribution,
     advantages = train_batch[Postprocessing.ADVANTAGES]
     actions = train_batch[SampleBatch.ACTIONS]
 
+    #rewards_plus_v = torch.cat(
+    #    [train_batch[SampleBatch.REWARDS],
+    #     state_values[-1].unsqueeze(0)])
+    #discounted_returns = discount_cumsum(rewards_plus_v,
+    #                                     gamma)[:-1].astype(np.float32)
+    #gamma = policy.config["gamma"]
+    #discounted_returns = scipy.signal.lfilter([1], [1, float(-gamma)], x[::-1], axis=0)[::-1]
+
+    adv = advantages.detach() - state_values
+    #rollout[Postprocessing.VALUE_TARGETS] = discounted_returns
+
     # Advantage estimation.
-    adv = advantages - state_values
+    #adv = advantages - state_values
     adv_squared = torch.mean(torch.pow(adv, 2.0))
 
     # Value loss.
-    policy.v_loss = 0.5 * adv_squared
+    policy.v_loss = 0.0 #0.0 * adv_squared
 
     # Policy loss.
     # Update averaged advantage norm.
     policy.ma_adv_norm.add_(1e-7 * (adv_squared - policy.ma_adv_norm))
     # Exponentially weighted advantages.
     exp_advs = torch.exp(policy.config["beta"] *
-                         (adv / (1e-8 + torch.pow(policy.ma_adv_norm, 0.5))))
+                         (adv / (1e-8 + torch.pow(policy.ma_adv_norm, 0.5)).detach()))
     # log\pi_\theta(a|s)
     logprobs = action_dist.logp(actions)
-    policy.p_loss = -1.0 * torch.mean(exp_advs.detach() * logprobs)
+    #policy.p_loss = -1.0 * torch.mean(exp_advs.detach() * logprobs)
+    policy.p_loss = -1.0 * torch.mean(exp_advs * logprobs)
 
     # Combine both losses.
-    policy.total_loss = policy.p_loss + policy.config["vf_coeff"] * \
-        policy.v_loss
+    policy.total_loss = policy.p_loss # + policy.config["vf_coeff"] * \
+        #policy.v_loss
     explained_var = explained_variance(advantages, state_values)
     policy.explained_variance = torch.mean(explained_var)
 
