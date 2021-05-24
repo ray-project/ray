816d5539aff92b307b7f63a5e59471cd5b088308
Amog Kamsetty <amogkamsetty@yahoo.com>
2021-05-24 09:20:06 -0700

    updates

--- release/long_running_distributed_tests/workloads/pytorch_pbt_failure.py
+++ release/long_running_distributed_tests/workloads/pytorch_pbt_failure.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+import time
 
 import numpy as np
 import os
@@ -10,14 +12,12 @@ import torchvision.transforms as transforms
 
 import ray
 from ray import tune
-from ray.tune import CLIReporter
+from ray.tune import CLIReporter, Callback
 from ray.tune.schedulers import PopulationBasedTraining
 from ray.tune.utils.mock import FailureInjectorCallback
 from ray.util.sgd.torch import TorchTrainer, TrainingOperator
 from ray.util.sgd.torch.resnet import ResNet18
 from ray.util.sgd.utils import BATCH_SIZE
-
-from release.long_running_distributed_tests.utils import JsonCallback
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -26,6 +26,35 @@ parser.add_argument(
     default=False,
     help="Finish quickly for training.")
 args = parser.parse_args()
+
+
+class JsonCallback(Callback):
+    """Tune Callback to periodically write results to output JSON file."""
+
+    def __init__(self, output_json_file):
+        self.output_file = output_json_file
+
+    def on_step_end(self, iteration: int, trials, **info):
+        """Write output every 6 tuning loop step."""
+        if iteration % 3 == 0:
+            current_time = time.time()
+            trial_ids = [trial.trial_id for trial in trials]
+            trial_names = [str(trial) for trial in trials]
+            training_iterations = [
+                trial.last_result.get("training_iteration", None)
+                for trial in trials
+            ]
+            trial_statuses = [trial.status for trial in trials]
+
+            output = {
+                "last_update": current_time,
+                "trial_ids": trial_ids,
+                "trial_names": trial_names,
+                "training_iterations": training_iterations,
+                "trial_statuses": trial_statuses
+            }
+            with open(self.output_file, "at") as f:
+                json.dump(output, f)
 
 
 def initialization_hook():
