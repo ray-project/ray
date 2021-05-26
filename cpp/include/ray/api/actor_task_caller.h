498da139447e953c080d0ba9f02d6f0868a32a44
qicosmos <383121719@qq.com>
2021-05-26 16:45:56 +0800

    [C++ worker] Impove cpp worker (#15907)

--- cpp/include/ray/api/actor_task_caller.h
+++ cpp/include/ray/api/actor_task_caller.h
@@ -16,10 +16,12 @@ class ActorTaskCaller {
 
   ActorTaskCaller(RayRuntime *runtime, std::string id,
                   RemoteFunctionHolder remote_function_holder)
-      : runtime_(runtime), id_(id), remote_function_holder_(remote_function_holder) {}
+      : runtime_(runtime),
+        id_(id),
+        remote_function_holder_(std::move(remote_function_holder)) {}
 
   template <typename... Args>
-  ObjectRef<boost::callable_traits::return_type_t<F>> Remote(Args... args);
+  ObjectRef<boost::callable_traits::return_type_t<F>> Remote(Args &&... args);
 
  private:
   RayRuntime *runtime_;
@@ -33,11 +35,11 @@ class ActorTaskCaller {
 template <typename F>
 template <typename... Args>
 ObjectRef<boost::callable_traits::return_type_t<F>> ActorTaskCaller<F>::Remote(
-    Args... args) {
+    Args &&... args) {
   using ReturnType = boost::callable_traits::return_type_t<F>;
   StaticCheck<F, Args...>();
 
-  Arguments::WrapArgs(&args_, args...);
+  Arguments::WrapArgs(&args_, std::forward<Args>(args)...);
   auto returned_object_id = runtime_->CallActor(remote_function_holder_, id_, args_);
   return ObjectRef<ReturnType>(returned_object_id);
 }
