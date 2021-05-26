2a4d53ce4275cb189626a45f9a6746ff19709f83
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2021-05-26 10:18:33 -0700

    Fix requirements.txt

--- python/setup.py
+++ python/setup.py
@@ -97,12 +97,14 @@ extras = {
     "default": ["colorful"],
     "serve": ["uvicorn", "requests", "starlette", "fastapi"],
     "tune": ["pandas", "tabulate", "tensorboardX>=1.9"],
-    "k8s": ["kopf", "kubernetes"],
+    "k8s": ["kubernetes"],
     "observability": [
         "opentelemetry-api==1.1.0", "opentelemetry-sdk==1.1.0",
         "opentelemetry-exporter-otlp==1.1.0"
     ]
 }
+if sys.version_info >= (3, 7, 0):
+    extras["k8s"].append("kopf")
 
 extras["rllib"] = extras["tune"] + [
     "dm_tree",
