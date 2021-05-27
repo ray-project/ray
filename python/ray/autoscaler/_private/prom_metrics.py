849c1baf85f527544010fc8dda32257e063ddb57
Chris Wong <cwong@anyscale.com>
2021-05-27 09:22:17 -0700

    better descriptions

--- python/ray/autoscaler/_private/prom_metrics.py
+++ python/ray/autoscaler/_private/prom_metrics.py
@@ -10,35 +10,35 @@ class AutoscalerPrometheusMetrics:
     def __init__(self, registry: CollectorRegistry):
         self.registry: CollectorRegistry = registry
         # Buckets: 30 seconds, 1 minute, 2 minutes, 4 minutes
-        #           8 minutes, 15 minutes, 30 minutes, 1 hour
+        #          8 minutes, 15 minutes, 30 minutes, 1 hour
         self.worker_startup_time: Histogram = Histogram(
             "worker_startup_time_seconds",
-            "Worker startup time",
+            "Worker startup time.",
             unit="seconds",
             namespace="autoscaler",
             registry=self.registry,
             buckets=[30, 60, 120, 240, 480, 900, 1800, 3600])
         self.started_nodes: Counter = Counter(
             "started_nodes",
-            "Number of nodes started",
+            "Number of nodes started.",
             unit="nodes",
             namespace="autoscaler",
             registry=self.registry)
         self.stopped_nodes: Counter = Counter(
             "stopped_nodes",
-            "Number of nodes stopped",
+            "Number of nodes stopped.",
             unit="nodes",
             namespace="autoscaler",
             registry=self.registry)
         self.running_nodes: Gauge = Gauge(
             "running_nodes",
-            "Number of nodes running",
+            "Number of nodes running.",
             unit="nodes",
             namespace="autoscaler",
             registry=self.registry)
         self.exceptions: Counter = Counter(
             "exceptions",
-            "Number of exceptions",
+            "Number of exceptions raised during execution of autoscaler.",
             unit="exceptions",
             namespace="autoscaler",
             registry=self.registry)
