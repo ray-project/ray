43d19e071f61ba55eb8294fe462da042aceee97b
Amog Kamsetty <amogkamsetty@yahoo.com>
2021-05-26 18:00:42 -0700

    doc

--- python/ray/tune/utils/util.py
+++ python/ray/tune/utils/util.py
@@ -772,7 +772,16 @@ def get_current_node_resource_key() -> str:
 
 def force_on_current_node(task_or_actor):
     """Given a task or actor, place it on the current node.
+
     If using Ray Client, the current node is the client server node.
+
+    Args:
+        task_or_actor: A Ray remote function or class to place on the
+            current node.
+
+    Returns:
+        The provided task or actor, but with options modified to force
+            placement on the current node.
     """
     node_resource_key = get_current_node_resource_key()
     options = {"resources": {node_resource_key: 0.01}}
