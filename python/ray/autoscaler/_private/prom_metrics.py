09a8998c2023ed7c8fed37bcb9246b0aded6e5a6
Chris Wong <cwong@anyscale.com>
2021-05-27 09:14:30 -0700

    add worker startup time buckets

--- python/ray/autoscaler/_private/prom_metrics.py
+++ python/ray/autoscaler/_private/prom_metrics.py
@@ -9,12 +9,15 @@ from prometheus_client import (
 class AutoscalerPrometheusMetrics:
     def __init__(self, registry: CollectorRegistry):
         self.registry: CollectorRegistry = registry
+        # Buckets: 30 seconds, 1 minute, 2 minutes, 4 minutes
+        #           8 minutes, 15 minutes, 30 minutes, 1 hour
         self.worker_startup_time: Histogram = Histogram(
             "worker_startup_time_seconds",
             "Worker startup time",
             unit="seconds",
             namespace="autoscaler",
-            registry=self.registry)
+            registry=self.registry,
+            buckets=[30, 60, 120, 240, 480, 900, 1800, 3600])
         self.started_nodes: Counter = Counter(
             "started_nodes",
             "Number of nodes started",
