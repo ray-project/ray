fcfc02e25ea43abaa1755d7a4b01380670ab16c3
Amog Kamsetty <amogkamsetty@yahoo.com>
2021-05-26 17:27:17 -0700

    address comments

--- release/nightly_gpu_tests/sgd_gpu/sgd_gpu_test.py
+++ release/nightly_gpu_tests/sgd_gpu/sgd_gpu_test.py
@@ -10,12 +10,16 @@ if __name__ == "__main__":
     start_time = time.time()
     success = True
     try:
-        train_cifar(num_workers=2, use_gpu=True, num_epochs=5, fp16=True,
-                    test_mode=False)
+        train_cifar(
+            num_workers=2,
+            use_gpu=True,
+            num_epochs=5,
+            fp16=True,
+            test_mode=False)
     except Exception as e:
         print(f"The test failed with {e}")
         success = False
 
     delta = time.time() - start_time
     with open(os.environ["TEST_OUTPUT_JSON"], "w") as f:
-        f.write(json.dumps({"shuffle_time": delta, "success": success}))
+        f.write(json.dumps({"train_time": delta, "success": success}))
