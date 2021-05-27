f2dddaf4b29222c9dbfb9c841acb2de51f6059f5
Amog Kamsetty <amogkamsetty@yahoo.com>
2021-05-27 02:48:50 -0700

    add main method

--- python/ray/util/xgboost/tests/test_client.py
+++ python/ray/util/xgboost/tests/test_client.py
@@ -20,3 +20,7 @@ def test_simple_tune(start_client_server):
     assert ray.util.client.ray.is_connected()
     from ray.util.xgboost.simple_tune import main
     main()
+
+if __name__ == "__main__":
+    import pytest
+    sys.exit(pytest.main(["-v", __file__]))
