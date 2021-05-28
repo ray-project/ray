ec4679476715d41386378ceca68ebf9f3659cdfb
Ian Rodney <ian.rodney@gmail.com>
2021-05-28 10:15:44 -0700

    [Client] Add ray.client().disconnect() (#16021)

--- python/ray/client_builder.py
+++ python/ray/client_builder.py
@@ -3,6 +3,7 @@ import importlib
 import logging
 from dataclasses import dataclass
 from urllib.parse import urlparse
+import sys
 from typing import Any, Dict, Optional, Tuple
 
 from ray.ray_constants import RAY_ADDRESS_ENVIRONMENT_VARIABLE
@@ -13,16 +14,43 @@ logger = logging.getLogger(__name__)
 
 
 @dataclass
-class ClientInfo:
+class ClientContext:
     """
-    Basic information of the remote server for a given Ray Client connection.
+    Basic context manager for a ClientBuilder connection.
     """
     dashboard_url: Optional[str]
     python_version: str
     ray_version: str
     ray_commit: str
-    protocol_version: str
+    protocol_version: Optional[str]
     _num_clients: int
+
+    def __enter__(self) -> "ClientContext":
+        return self
+
+    def __exit__(self, *exc) -> None:
+        self.disconnect()
+
+    def disconnect(self) -> None:
+        """
+        Disconnect Ray. This either disconnects from the remote Client Server
+        or shuts the current driver down.
+        """
+        if ray.util.client.ray.is_connected():
+            # This is only a client connected to a server.
+            ray.util.client_connect.disconnect()
+            ray._private.client_mode_hook._explicitly_disable_client_mode()
+        elif ray.worker.global_worker.node is None:
+            # Already disconnected.
+            return
+        elif ray.worker.global_worker.node.is_head():
+            logger.debug(
+                "The current Ray Cluster is scoped to this process. "
+                "Disconnecting is not possible as it will shutdown the "
+                "cluster.")
+        else:
+            # This is only a driver connected to an existing cluster.
+            ray.shutdown()
 
 
 class ClientBuilder:
@@ -56,7 +84,7 @@ class ClientBuilder:
         self._job_config.set_ray_namespace(namespace)
         return self
 
-    def connect(self) -> ClientInfo:
+    def connect(self) -> ClientContext:
         """
         Begin a connection to the address passed in via ray.client(...).
 
@@ -69,7 +97,7 @@ class ClientBuilder:
             self.address, job_config=self._job_config)
         dashboard_url = ray.get(
             ray.remote(ray.worker.get_dashboard_url).remote())
-        return ClientInfo(
+        return ClientContext(
             dashboard_url=dashboard_url,
             python_version=client_info_dict["python_version"],
             ray_version=client_info_dict["ray_version"],
@@ -79,11 +107,19 @@ class ClientBuilder:
 
 
 class _LocalClientBuilder(ClientBuilder):
-    def connect(self) -> ClientInfo:
+    def connect(self) -> ClientContext:
         """
         Begin a connection to the address passed in via ray.client(...).
         """
-        return ray.init(address=self.address, job_config=self._job_config)
+        connection_dict = ray.init(
+            address=self.address, job_config=self._job_config)
+        return ClientContext(
+            dashboard_url=connection_dict["webui_url"],
+            python_version="{}.{}.{}".format(
+                sys.version_info[0], sys.version_info[1], sys.version_info[2]),
+            ray_version=ray.__version__,
+            ray_commit=ray.__commit__,
+            protocol_version=None)
 
 
 def _split_address(address: str) -> Tuple[str, str]:
@@ -103,7 +139,11 @@ def _get_builder_from_address(address: Optional[str]) -> ClientBuilder:
         return _LocalClientBuilder(None)
     if address is None:
         try:
-            with open("/tmp/ray/current_cluster", "r") as f:
+            # NOTE: This is not placed in `Node::get_temp_dir_path`, because
+            # this file is accessed before the `Node` object is created.
+            cluster_file = os.path.join(ray._private.utils.get_user_temp_dir(),
+                                        "ray_current_cluster")
+            with open(cluster_file, "r") as f:
                 address = f.read()
                 print(address)
         except FileNotFoundError:
