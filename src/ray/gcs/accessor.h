abf5ab04a22b021e3d3b9cd402883288fdd73f18
Tao Wang <wangtaothetonic@163.com>
2021-06-02 22:47:30 +0800

    Update src/ray/gcs/accessor.h
    Co-authored-by: Alex Wu <itswu.alex@gmail.com>

--- src/ray/gcs/accessor.h
+++ src/ray/gcs/accessor.h
@@ -183,7 +183,7 @@ class JobInfoAccessor {
   /// \param is_pubsub_server_restarted Whether pubsub server is restarted.
   virtual void AsyncResubscribe(bool is_pubsub_server_restarted) = 0;
 
-  /// Get next job id.
+  /// Increment and get next job id. This is not idempotent.
   ///
   /// \param done Callback that will be called when request successfully.
   /// \return Status
