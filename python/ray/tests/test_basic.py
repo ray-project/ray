f23844545b54563eb9c4d953ac7b6ef9ad5d0ee8
Eric Liang <ekhliang@gmail.com>
2021-06-01 21:18:24 -0700

    update

--- python/ray/tests/test_basic.py
+++ python/ray/tests/test_basic.py
@@ -33,11 +33,12 @@ def test_ignore_http_proxy(shutdown_only):
 
 # https://github.com/ray-project/ray/issues/16025
 def test_release_resources_race(shutdown_only):
-    # This test fails if you set the timeout = 0.
+    # This test fails if you set the timeout = 0. We set it to something
+    # higher than the default value of 10 to reduce flakiness.
     ray.init(
         num_cpus=2,
         object_store_memory=700e6,
-        _system_config={"release_resources_timeout_milliseconds": 10})
+        _system_config={"release_resources_timeout_milliseconds": 100})
     refs = []
     for _ in range(10):
         refs.append(ray.put(np.zeros(20 * 1024 * 1024, dtype=np.uint8)))
