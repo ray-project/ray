867cdd9662ae1bfeee9ed59a9243b1c4085627c0
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2021-06-03 12:39:32 -0700

    fix

--- python/ray/autoscaler/_private/node_launcher.py
+++ python/ray/autoscaler/_private/node_launcher.py
@@ -46,7 +46,7 @@ class NodeLauncher(threading.Thread):
                 config["available_node_types"][node_type]["node_config"])
         launch_hash = hash_launch_conf(launch_config, config["auth"])
         self.log("Launching {} nodes, type {}.".format(count, node_type))
-        node_config = copy.deepcopy(config["worker_nodes"])
+        node_config = copy.deepcopy(config.get("worker_nodes", {}))
         node_tags = {
             TAG_RAY_NODE_NAME: "ray-{}-worker".format(config["cluster_name"]),
             TAG_RAY_NODE_KIND: NODE_KIND_WORKER,
