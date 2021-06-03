fe572d9019c94ec59c0c7f0c67db071b1e702e5e
scv119 <scv119@gmail.com>
2021-06-02 20:29:35 -0700

    lint

--- python/ray/internal/internal_api.py
+++ python/ray/internal/internal_api.py
@@ -204,20 +204,15 @@ def get_object_locations(obj_refs: List[ObjectRef], timeout_ms: int = -1
         - primary_node_id:
         The hex ID of the node who has the primary copy of the object.
         It could be None if the object is pending resolution.
-
         - object_size:
         The size of data + metadata in bytes.
-
         - node_ids:
         The hex IDs of the nodes that have an in memory copy of this
         object.
-
         - is_spilled:
         Wether the object has been spilled.
-
         - spilled_url:
         The spilled location, None if not spilled.
-
         - spilled_node_id:
         The hex node ID which spilled the object. None if the object
         is not spilled, or was spilled to a distributed external
