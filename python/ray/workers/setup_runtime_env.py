3871d544769855004b166c3fc620b8d8967e0a68
architkulkarni <architkulkarni@users.noreply.github.com>
2021-06-05 15:32:50 -0700

    [Core] [runtime env] Fix injection of ray[default] (#16275)

--- python/ray/workers/setup_runtime_env.py
+++ python/ray/workers/setup_runtime_env.py
@@ -51,7 +51,7 @@ def setup(input_args):
                                       sys.version_info[:3]))  # like 3.6.10
             ray_pip = current_ray_pip_specifier()
             if ray_pip and not runtime_env.get("_skip_inject_ray"):
-                extra_pip_dependencies = [ray_pip]
+                extra_pip_dependencies = [ray_pip, "ray[default]"]
             else:
                 extra_pip_dependencies = []
             conda_dict = inject_dependencies(conda_dict, py_version,
