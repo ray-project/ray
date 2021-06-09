c6f93367dfd7e864435527513a6b70cf93ef354b
Mingwei Tian <mwtian@anyscale.com>
2021-06-08 23:58:27 -0700

    Upgrade setproctitle in thirdparty

--- python/setup.py
+++ python/setup.py
@@ -269,7 +269,7 @@ def build(build_python, build_java):
     # that certain flags will not be passed along such as --user or sudo.
     # TODO(rkn): Fix this.
     if not os.getenv("SKIP_THIRDPARTY_INSTALL"):
-        pip_packages = ["psutil", "setproctitle==1.1.10"]
+        pip_packages = ["psutil", "setproctitle==1.2.2"]
         subprocess.check_call(
             [
                 sys.executable, "-m", "pip", "install", "-q",
