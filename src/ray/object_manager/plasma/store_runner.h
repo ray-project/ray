85a34e1d0fb07949e1b09666a862dd8fda0155fd
Eric Liang <ekhliang@gmail.com>
2021-06-10 10:53:21 -0700

    fix

--- src/ray/object_manager/plasma/store_runner.h
+++ src/ray/object_manager/plasma/store_runner.h
@@ -12,7 +12,8 @@ namespace plasma {
 class PlasmaStoreRunner {
  public:
   PlasmaStoreRunner(std::string socket_name, int64_t system_memory,
-                    bool hugepages_enabled, std::string plasma_directory);
+                    bool hugepages_enabled, std::string plasma_directory,
+                    std::string fallback_directory);
   void Start(ray::SpillObjectsCallback spill_objects_callback,
              std::function<void()> object_store_full_callback,
              ray::AddObjectCallback add_object_callback,
