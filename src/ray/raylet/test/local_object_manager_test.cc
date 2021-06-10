6ff2aac77299738bdd354f193ee831140547e797
Eric Liang <ekhliang@gmail.com>
2021-06-09 17:47:30 -0700

    remove scheduler flag

--- src/ray/raylet/test/local_object_manager_test.cc
+++ src/ray/raylet/test/local_object_manager_test.cc
@@ -1169,7 +1169,7 @@ TEST_F(LocalObjectManagerTest, TestDeleteURLRefCountRaceCondition) {
   ASSERT_EQ(deleted_urls_size, 1);
 }
 
-}  // namespace raylet
+}  // namespace ray
 
 }  // namespace ray
 
