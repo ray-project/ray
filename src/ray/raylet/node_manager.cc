1828387052cadc5d5a0d18cd7bda3d0261ede80c
Eric Liang <ekhliang@gmail.com>
2021-06-12 20:32:09 -0700

    fix

--- src/ray/raylet/node_manager.cc
+++ src/ray/raylet/node_manager.cc
@@ -189,9 +189,7 @@ NodeManager::NodeManager(instrumented_io_context &io_service, const NodeID &self
             // NOTE: It means the local object manager should be thread-safe.
             io_service_.post(
                 [this]() {
-                  if (global_gc_throttler_.AbleToRun()) {
-                    TriggerGlobalGC();
-                  }
+                  TriggerGlobalGC();
                   plasma::plasma_store_runner->EvictObjectsIfPossible();
                   GetLocalObjectManager().SpillObjectUptoMaxThroughput();
                 },
