302b297fe3130c6f6a58856905b09d6f9c71401d
WangTaoTheTonic <dooku.wt@antfin.com>
2021-06-16 19:52:57 +0800

    lint

--- src/ray/core_worker/lib/java/io_ray_runtime_gcs_GlobalStateAccessor.cc
+++ src/ray/core_worker/lib/java/io_ray_runtime_gcs_GlobalStateAccessor.cc
@@ -59,8 +59,9 @@ JNIEXPORT jobject JNICALL Java_io_ray_runtime_gcs_GlobalStateAccessor_nativeGetA
       });
 }
 
-JNIEXPORT jbyteArray JNICALL Java_io_ray_runtime_gcs_GlobalStateAccessor_nativeGetNextJobID(
-    JNIEnv *env, jobject o, jlong gcs_accessor_ptr) {
+JNIEXPORT jbyteArray JNICALL
+Java_io_ray_runtime_gcs_GlobalStateAccessor_nativeGetNextJobID(JNIEnv *env, jobject o,
+                                                               jlong gcs_accessor_ptr) {
   auto *gcs_accessor =
       reinterpret_cast<ray::gcs::GlobalStateAccessor *>(gcs_accessor_ptr);
   const auto &job_id = gcs_accessor->GetNextJobID();
