a7bf42ee837698586e78a29214f76010f16cc30b
sven1977 <svenmika1977@gmail.com>
2021-06-21 11:20:13 +0200

    LINT

--- rllib/evaluation/sampler.py
+++ rllib/evaluation/sampler.py
@@ -219,10 +219,9 @@ class SyncSampler(SamplerInput):
         # Create the rollout generator to use for calls to `get_data()`.
         self.rollout_provider = _env_runner(
             worker, self.base_env, self.extra_batches.put,
-            self.rollout_fragment_length, self.horizon,
-            clip_rewards, normalize_actions, clip_actions,
-            multiple_episodes_in_batch, callbacks, tf_sess,
-            self.perf_stats, soft_horizon, no_done_at_end,
+            self.rollout_fragment_length, self.horizon, clip_rewards,
+            normalize_actions, clip_actions, multiple_episodes_in_batch,
+            callbacks, tf_sess, self.perf_stats, soft_horizon, no_done_at_end,
             observation_fn, self.sample_collector, self.render)
         self.metrics_queue = queue.Queue()
 
@@ -399,12 +398,11 @@ class AsyncSampler(threading.Thread, SamplerInput):
                 lambda x: self.extra_batches.put(x, timeout=600.0))
         rollout_provider = _env_runner(
             self.worker, self.base_env, extra_batches_putter,
-            self.rollout_fragment_length, self.horizon,
-            self.clip_rewards, self.normalize_actions, self.clip_actions,
-            self.multiple_episodes_in_batch, self.callbacks,
-            self.tf_sess, self.perf_stats, self.soft_horizon,
-            self.no_done_at_end, self.observation_fn, self.sample_collector,
-            self.render)
+            self.rollout_fragment_length, self.horizon, self.clip_rewards,
+            self.normalize_actions, self.clip_actions,
+            self.multiple_episodes_in_batch, self.callbacks, self.tf_sess,
+            self.perf_stats, self.soft_horizon, self.no_done_at_end,
+            self.observation_fn, self.sample_collector, self.render)
         while not self.shutdown:
             # The timeout variable exists because apparently, if one worker
             # dies, the other workers won't die with it, unless the timeout is
