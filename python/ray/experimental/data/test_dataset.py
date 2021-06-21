50932229c1ca96c916669d974a5170dac8394786
scv119 <scv119@gmail.com>
2021-06-21 16:05:10 -0700

    add arrow dataset

--- python/ray/experimental/data/test_dataset.py
+++ python/ray/experimental/data/test_dataset.py
@@ -1,5 +1,6 @@
+from ray.experimental.data import ArrowDataset, ArrowBlock
 import pytest
-
+import builtins
 import ray
 from ray.tests.conftest import ray_start_regular_shared
 
@@ -7,6 +8,32 @@ from ray.tests.conftest import ray_start_regular_shared
 def test_basic(ray_start_regular_shared):
     ds = ray.experimental.data.range(5)
     assert ds.map(lambda x: x + 1).take() == [1, 2, 3, 4, 5]
+
+
+def range_arrow(n: int, num_blocks: int = 200) -> "ArrowDataset":
+    block_size = max(1, n // num_blocks)
+    blocks: List[BlockRef] = []
+    i = 0
+
+    @ray.remote
+    def gen_block(start: int, count: int) -> "ArrowBlock":
+        return {"a": list(builtins.range(start, start + count))}
+
+    while i < n:
+        blocks.append(gen_block.remote(block_size * i, min(block_size, n - i)))
+        i += block_size
+
+    return ArrowDataset(blocks, ArrowBlock)
+
+
+def test_pyarrow(ray_start_regular_shared):
+    ds = range_arrow(5)
+    assert ds.map(lambda x: {"b": x["a"] + 2}).take() == \
+        [{"b": 2}, {"b": 3}, {"b": 4}, {"b": 5}, {"b": 6}]
+    assert ds.map(lambda x: {"b": x["a"] + 2}).filter(lambda x: x["b"] % 2 == 0).take() == \
+        [{"b": 2}, {"b": 4}, {"b": 6}]
+    assert ds.filter(lambda x: x["a"] == 0).flat_map(lambda x: [{"b": x["a"] + 2}, {"b": x["a"] + 20}]).take() == \
+        [{"b": 2}, {"b": 20}]
 
 
 if __name__ == "__main__":
