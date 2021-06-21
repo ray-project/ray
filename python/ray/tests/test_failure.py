a0bf2accb9f02ed7b2310aa3a249c5761cd34026
qicosmos <qicosmos@163.com>
2021-06-21 21:24:50 +0800

    move to test_failure

--- python/ray/tests/test_failure.py
+++ python/ray/tests/test_failure.py
@@ -4,7 +4,9 @@ import tempfile
 import time
 
 import numpy as np
+import psutil
 import pytest
+import signal
 
 import ray
 import ray._private.utils
@@ -615,6 +617,24 @@ def test_warning_task_waiting_on_actor(shutdown_only):
     errors = get_error_message(p, 1, ray_constants.RESOURCE_DEADLOCK_ERROR)
     assert len(errors) == 1
     assert errors[0].type == ray_constants.RESOURCE_DEADLOCK_ERROR
+
+
+def get_pid(name):
+    pids = psutil.process_iter()
+    for pid in pids:
+        if (pid.name() == name):
+            return pid.pid
+
+
+def test_kill_raylet_signal_log(ray_start_shared_local_modes):
+    session_dir = ray.worker._global_node.get_session_dir_path()
+    raylet_out_path = "{}/logs/raylet.err".format(session_dir)
+    pid = get_pid("raylet")
+    os.kill(pid, signal.SIGABRT)
+    time.sleep(1)
+    with open(raylet_out_path) as f:
+        s = f.read()
+        assert "SIGABRT" in s
 
 
 if __name__ == "__main__":
