1535c1fb3891282f25017765fb6a02c36a876d0a
Richard Liaw <rliaw@berkeley.edu>
2021-06-21 21:34:58 -0700

    update-webcache
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>

--- doc/examples/web_cache.py
+++ doc/examples/web_cache.py
@@ -2,7 +2,12 @@
 Web Cache
 =========
 
-This program illustrates how to use a Ray actor to cache data coming from an external API. It starts up a Flask server and the actor, makes a couple fake API calls to fetch the same data, and prints out the run time of each call. The second call is substantially faster because the data exists in the cache.
+This program illustrates how to use a Ray actor to cache data coming
+from an external API. It starts up a Flask server and
+the actor, makes a couple fake API calls to fetch the same data,
+and prints out the run time of each
+call. The second call is substantially faster
+because the data exists in the cache.
 
 To run the example, you need to install the dependencies
 
@@ -18,32 +23,35 @@ and then execute this script as follows:
   python web_cache.py
 
 
+Setting up imports
+------------------
+
+Let's first import the necessary dependencies and start the Ray runtime.
 
 """
+from typing import Dict
 import time
 import requests
 import ray
 import flask
-import time
 
 ray.init()
 app = flask.Flask(__name__)
-"""
 
-The following code block defines a WebCache Ray Actor. This
-allows the class to be run in a multi-process environment.
-Ray handles serialization of all remote method calls to this
-actor so that you do not need to worry about race conditions.
-We will use this to store and retrieve
-the results of expensive API calls across our Flask handlers.
+#####################################################################
+# The following code block defines a WebCache Ray Actor. This
+# allows the class to be run in a multi-process environment.
+# Ray handles serialization of all remote method calls to this
+# actor so that you do not need to worry about race conditions.
+# We will use this to store and retrieve
+# the results of expensive API calls across our Flask handlers.
 
-"""
 
 @ray.remote
 class WebCache:
     def __init__(self):
         # A dict will serve as our cache
-        self.users: Dict[int, any]  = {}
+        self.users: Dict[int, any] = {}
 
     def get_user(self, uid: int):
         return self.users.get(uid)
@@ -51,27 +59,26 @@ class WebCache:
     def put_user(self, uid: int, user: any):
         self.users[uid] = user
 
-"""
-This line instantiates a WebCache actor.
 
-Rather than saving a handle to the actor, we use a "detached actor".
-
-This is an actor that can be fetched by name, as opposed to having
-to pass its handle around. The advantage here is that you can instantiate
-an actor once at app initialization time and use it from all your
-handlers without passing a new argument through to them.
-"""
+####################################################################
+# Now, instantiate a WebCache actor.
+#
+# Rather than saving a handle to the actor, we use a "detached actor".
+#
+# This is an actor that can be fetched by name, as opposed to having
+# to pass its handle around. You can instantiate
+# an actor once at app initialization time and use it from all your
+# handlers without passing a new argument through to them.
 
 WebCache.options(name="web_cache", lifetime="detached").remote()
 
-"""
-Finally, this is the handler that we use to illustrate the concept.
-The code first fetches the WebCache actor by name. It then
-checks whether the necessary data exists in the cache. If the
-data exists, the code returns it from the cache, otherwise
-it makes an expensive API call and populates the cache with the
-data.
-"""
+#############################################################################
+# Finally, this is the handler that we use to illustrate the concept.
+# The code first fetches the WebCache actor by name. It then
+# checks whether the necessary data exists in the cache. If the
+# data exists, the code returns it from the cache, otherwise
+# it makes an expensive API call and populates the cache with the
+# data.
 
 
 def fetch_user_from_api(uid: int):
@@ -80,13 +87,14 @@ def fetch_user_from_api(uid: int):
     return {"name": "max", "id": uid}
 
 
-@app.route('/get_user/<uid>')
+@app.route("/get_user/<uid>")
 def fetch_external_user_handler(uid):
     """This handler retrieves data from an external API if the
        data isn't cached, otherwise it returns the cached data."""
     uid = int(uid)
     # Check whether the data we want is already cached.
-    cache = ray.get_actor("web_cache") # This is used for fetching detached actors
+    cache = ray.get_actor(
+        "web_cache")  # This is used for fetching detached actors
     if not cache:
         print("not cache!")
     user = ray.get(cache.get_user.remote(uid))
@@ -98,13 +106,14 @@ def fetch_external_user_handler(uid):
     cache.put_user.remote(uid, user)
     return user
 
-"""
-You can then run this with the following:
-"""
 
+##################################################################
+# To test this web cache, we define the following function.
 # This function is being run as a Ray task because the flask
 # server is blocking, so we need to run this code from another process.
 # Keeping these in the same file is for example purposes only.
+
+
 @ray.remote
 def test_web_cache():
     # sleep for a moment to make sure the server has started up.
@@ -112,12 +121,12 @@ def test_web_cache():
     # Request the same user id twice. We expect the first call to take
     # substantially longer because the value isn't cached yet.
     timer_st = time.monotonic()
-    resp = requests.get("http://localhost:3000/get_user/4")
+    requests.get("http://localhost:3000/get_user/4")
     timer_end = time.monotonic()
     first_req_runtime_s = timer_end - timer_st
 
     timer_st = time.monotonic()
-    resp = requests.get("http://localhost:3000/get_user/4")
+    requests.get("http://localhost:3000/get_user/4")
     timer_end = time.monotonic()
     second_req_runtime_s = timer_end - timer_st
 
@@ -125,6 +134,8 @@ def test_web_cache():
     print(f"The second request took {second_req_runtime_s} seconds.")
 
 
+###################################################################
+# Below, we instantiate the application and test the cache.
+
 test_web_cache.remote()
 app.run(host="localhost", port=3000)
-
