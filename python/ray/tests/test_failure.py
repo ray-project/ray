d284d81235a8888c832c84588c490e3d582b67f8
qicosmos <qicosmos@163.com>
2021-06-23 07:47:51 +0800

    move to an independent file

--- python/ray/tests/test_failure.py
+++ python/ray/tests/test_failure.py
@@ -16,26 +16,6 @@ from ray.test_utils import (wait_for_condition, SignalActor, init_error_pubsub,
                             get_error_message)
 
 
-def get_pid(name):
-    pids = psutil.process_iter()
-    for pid in pids:
-        if (pid.name() == name):
-            return pid.pid
-
-
-def test_kill_raylet_signal_log(shutdown_only):
-    ray.init(num_cpus=1)
-    session_dir = ray.worker._global_node.get_session_dir_path()
-    raylet_out_path = "{}/logs/raylet.err".format(session_dir)
-    pid = get_pid("raylet")
-    p = psutil.Process(pid)
-    p.send_signal(signal.SIGABRT)
-    p.wait()
-    with open(raylet_out_path) as f:
-        s = f.read()
-        assert "SIGABRT" in s
-
-
 def test_unhandled_errors(ray_start_regular):
     @ray.remote
     def f():
