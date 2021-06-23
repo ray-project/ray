453138b5c578e84a761e8b1fd7c2f4f0b9a5ee23
Scott Graham <scgraham@microsoft.com>
2021-06-23 13:32:57 -0400

    azure autoscaler update: support new versions of azure-mgmt-resource sdk, azure autoscaler yaml updates: lastest vm image, add pause after docker usermod for better robustness, remove conda env in setup for docker based config, update conda env to use python 3.8 versions

--- python/ray/autoscaler/_private/azure/node_provider.py
+++ python/ray/autoscaler/_private/azure/node_provider.py
@@ -213,7 +213,11 @@ class AzureNodeProvider(NodeProvider):
         }
 
         # TODO: we could get the private/public ips back directly
-        self.resource_client.deployments.create_or_update(
+        if hasattr(self.resource_client.deployments, "create_or_update"):
+            create_or_update = self.resource_client.deployments.create_or_update
+        else:
+            create_or_update = self.resource_client.deployments.begin_create_or_update
+        create_or_update(
             resource_group_name=resource_group,
             deployment_name="ray-vm-{}".format(name_tag),
             parameters=parameters).wait()
