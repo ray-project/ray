59ed3ed6d93a67a52e63558cf5e968f32324daf9
qicosmos <qicosmos@163.com>
2021-06-24 12:08:58 +0800

    add more test cases

--- cpp/src/ray/test/benchmark/benchmark.cc
+++ cpp/src/ray/test/benchmark/benchmark.cc
@@ -1,8 +1,19 @@
 #include <gtest/gtest.h>
 #include <ray/api.h>
+
 #include <chrono>
+#include <set>
 
 using namespace ::ray::api;
+
+void NoReturn() {}
+int ReturnVal() { return 1; }
+
+void Input(std::string str) {}
+
+std::string Echo(std::string str) { return str; }
+
+RAY_REMOTE(NoReturn, ReturnVal, Input, Echo);
 
 class Counter {
  public:
@@ -12,7 +23,6 @@ class Counter {
 
   static Counter *FactoryCreate() { return new Counter(0); }
 
-  /// non static function
   int Add() {
     count += 1;
     return count;
@@ -23,19 +33,20 @@ class Counter {
 
 RAY_REMOTE(Counter::FactoryCreate, &Counter::Add, &Counter::Get);
 
-TEST(BenchmarkTest, Benchmark) {
-  ActorHandle<Counter> actor = Ray::Actor(Counter::FactoryCreate).Remote();
-  size_t num_repeats = 1000000;
+template <typename F>
+void Execute(F f, std::string func_name, size_t num_repeats) {
   auto start = std::chrono::system_clock::now();
   for (size_t i = 0; i < num_repeats; i++) {
-    actor.Task(&Counter::Add).Remote();
+    f();
   }
   auto end = std::chrono::system_clock::now();
   auto duration =
       std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
 
-  std::string out = "Benchmark finished, repeated ";
-  out.append(std::to_string(num_repeats))
+  std::string out = "Benchmark ";
+  out.append(func_name)
+      .append(" finished, repeated ")
+      .append(std::to_string(num_repeats))
       .append(" times, total duration ")
       .append(std::to_string(duration / 1000000))
       .append(" ms,")
@@ -45,16 +56,69 @@ TEST(BenchmarkTest, Benchmark) {
   RAYLOG(INFO) << out;
 
   std::cout << out << std::endl;
+}
 
+template <typename R, typename F>
+void ExecuteLatancy(F f, std::string func_name, size_t num_repeats) {
+  std::set<uint64_t> set;
+  for (size_t i = 0; i < num_repeats; i++) {
+    auto start = std::chrono::system_clock::now();
+    ObjectRef<R> object_ref = f();
+    object_ref.Get();
+    auto end = std::chrono::system_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    set.emplace(duration);
+  }
+
+  std::string out = "Latency ";
+  out.append(func_name).append(": \n");
+
+  for (auto &time : set) {
+    out.append(std::to_string(time)).append("ns\n");
+  }
+  RAYLOG(INFO) << out;
+
+  std::cout << out << std::endl;
+}
+
+TEST(BenchmarkTest, SimpleLatancyTest) {
+  ActorHandle<Counter> actor = Ray::Actor(Counter::FactoryCreate).Remote();
+  size_t num_repeats = 20;
+  ExecuteLatancy<int>([&actor] { return actor.Task(&Counter::Add).Remote(); },
+                      "&Counter::Add", num_repeats);
+
+  ExecuteLatancy<void>([] { return Ray::Task(NoReturn).Remote(); }, "NoReturn",
+                       num_repeats);
+
+  ExecuteLatancy<int>([] { return Ray::Task(ReturnVal).Remote(); }, "ReturnVal",
+                      num_repeats);
+}
+
+TEST(BenchmarkTest, SimpleRemoteTest) {
+  ActorHandle<Counter> actor = Ray::Actor(Counter::FactoryCreate).Remote();
+  size_t num_repeats = 1000000;
+  Execute([&actor] { actor.Task(&Counter::Add).Remote(); }, "&Counter::Add", num_repeats);
+
+  Execute([] { Ray::Task(NoReturn).Remote(); }, "NoReturn", num_repeats);
+
+  Execute([] { Ray::Task(ReturnVal).Remote(); }, "ReturnVal", num_repeats);
+}
+
+TEST(BenchmarkTest, RemoteWithArgsTest) {
+  size_t num_repeats = 1000000;
+  std::string small_str(100, ' ');
+  Execute([&small_str] { Ray::Task(Input).Remote(small_str); }, "Input small_str",
+          num_repeats);
+  Execute([&small_str] { Ray::Task(Echo).Remote(small_str); }, "Echo small_str",
+          num_repeats);
   Ray::Shutdown();
 }
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
-  /// configuration
   ray::api::RayConfig config;
 
-  /// initialization
   Ray::Init(config, &argc, &argv);
   return RUN_ALL_TESTS();
 }
