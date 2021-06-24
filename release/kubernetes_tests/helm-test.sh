07c3472be880fd9fb7fbe1c9839e6f5b7d7bbc0c
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2021-06-24 13:10:38 -0700

    delete-commented-out

--- release/kubernetes_tests/helm-test.sh
+++ release/kubernetes_tests/helm-test.sh
@@ -2,7 +2,6 @@
 set -x
 kubectl create namespace helm-test
 kubectl create namespace helm-test2
-#KUBERNETES_OPERATOR_PULL_POLICY=IfNotPresent
 KUBERNETES_OPERATOR_TEST_NAMESPACE=helm-test KUBERNETES_OPERATOR_TEST_IMAGE="$IMAGE" python ../../python/ray/tests/kubernetes_e2e/test_helm.py
 kubectl delete namespace helm-test
 kubectl delete namespace helm-test2
--- release/kubernetes_tests/k8s-test.sh
+++ release/kubernetes_tests/k8s-test.sh
@@ -2,7 +2,6 @@
 set -x
 kubectl create namespace basic-test
 kubectl apply -f ../../deploy/charts/ray/crds/cluster_crd.yaml
-#KUBERNETES_OPERATOR_PULL_POLICY=IfNotPresent
 KUBERNETES_OPERATOR_TEST_NAMESPACE=basic-test KUBERNETES_OPERATOR_TEST_IMAGE="$IMAGE" python ../../python/ray/tests/kubernetes_e2e/test_k8s_operator_basic.py
 kubectl -n basic-test delete --all rayclusters
 kubectl -n basic-test delete deployment ray-operator
