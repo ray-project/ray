a60941f86aebe152cec15882b421b44955651e76
Eric Liang <ekhliang@gmail.com>
2021-06-23 17:04:59 -0700

    more docs

--- python/ray/experimental/data/dataset.py
+++ python/ray/experimental/data/dataset.py
@@ -20,12 +20,41 @@ BatchType = Union["pandas.DataFrame", pyarrow.Table]
 
 
 class Dataset(Generic[T]):
-    def __init__(self, blocks: List[ObjectRef[Block]], block_cls: Any):
-        self._blocks: List[ObjectRef[Block]] = blocks
+    """A dataset represents a set of records in the Ray object store.
+
+    Datasets are implemented as a list of ``ObjectRef[Block[T]]``. The block
+    also determines the unit of parallelism. The most common type of block is
+    the ``ArrowBlock``, which is backed by a ``pyarrow.Table`` object. Other
+    Python objects are represented with ``ListBlock`` (a plain Python list).
+
+    Since Datasets are just lists of Ray objects, they can be freely passed
+    between Ray tasks and actors just like any other value. Datasets support
+    conversion to/from several more featureful dataframe libraries
+    (e.g., Spark, Dask), and are also compatible with TensorFlow / PyTorch.
+
+    Dataset support parallel transformations such as .map(), .map_batches(),
+    but currently do not support operations that require partitioning.
+    """
+
+    def __init__(self, blocks: List[ObjectRef[Block[T]]], block_cls: Any):
+        self._blocks: List[ObjectRef[Block[T]]] = blocks
         self._block_cls = block_cls
 
     def map(self, fn: Callable[[T], U], compute="tasks",
             **ray_remote_args) -> "Dataset[U]":
+        """Apply the given function to each record of this dataset.
+
+        This is a blocking operation. Note that mapping individual records
+        can be quite slow. Consider using `.map_batches()` for performance.
+
+        Args:
+            fn: The function to apply to each record.
+            compute: The compute strategy, either "tasks" to use Ray tasks,
+                or "actors" to use an autoscaling Ray actor pool.
+            ray_remote_args: Additional resource requirements to request from
+                ray (e.g., num_gpus=1 to request GPUs for the map tasks).
+        """
+
         def transform(serialized: Any) -> Any:
             block = self._block_cls.deserialize(serialized)
             builder = block.builder()
@@ -45,12 +74,40 @@ class Dataset(Generic[T]):
                     compute: str = "tasks",
                     batch_format: str = "pandas",
                     **ray_remote_args) -> "Dataset[dict]":
+        """Apply the given function to batches of records of this dataset.
+
+        This is a blocking operation.
+
+        Args:
+            fn: The function to apply to each record batch.
+            batch_size: Request a specific batch size, or leave unspecified
+                for the system to select a batch size.
+            compute: The compute strategy, either "tasks" to use Ray tasks,
+                or "actors" to use an autoscaling Ray actor pool.
+            batch_format: Specify "pandas" to select ``pandas.DataFrame`` as
+                the batch format, or "pyarrow" to select ``pyarrow.Table``.
+            ray_remote_args: Additional resource requirements to request from
+                ray (e.g., num_gpus=1 to request GPUs for the map tasks).
+        """
         raise NotImplementedError  # P0
 
     def flat_map(self,
                  fn: Callable[[T], Iterator[U]],
                  compute="tasks",
                  **ray_remote_args) -> "Dataset[U]":
+        """Apply the given function to each record and then flatten results.
+
+        This is a blocking operation. Consider using ``.map_batches()`` for
+        better performance (the batch size can be altered in map_batches).
+
+        Args:
+            fn: The function to apply to each record.
+            compute: The compute strategy, either "tasks" to use Ray tasks,
+                or "actors" to use an autoscaling Ray actor pool.
+            ray_remote_args: Additional resource requirements to request from
+                ray (e.g., num_gpus=1 to request GPUs for the map tasks).
+        """
+
         def transform(serialized: Any) -> Any:
             block = self._block_cls.deserialize(serialized)
             builder = block.builder()
@@ -69,6 +126,19 @@ class Dataset(Generic[T]):
                fn: Callable[[T], bool],
                compute="tasks",
                **ray_remote_args) -> "Dataset[T]":
+        """Filter out records that do not satisfy the given predicate.
+
+        This is a blocking operation. Consider using ``.map_batches()`` for
+        better performance (you can implement filter by dropping records).
+
+        Args:
+            fn: The predicate function to apply to each record.
+            compute: The compute strategy, either "tasks" to use Ray tasks,
+                or "actors" to use an autoscaling Ray actor pool.
+            ray_remote_args: Additional resource requirements to request from
+                ray (e.g., num_gpus=1 to request GPUs for the map tasks).
+        """
+
         def transform(serialized: Any) -> Any:
             block = self._block_cls.deserialize(serialized)
             builder = block.builder()
@@ -84,6 +154,14 @@ class Dataset(Generic[T]):
             self._block_cls)
 
     def take(self, limit: int = 20) -> List[T]:
+        """Take up to the given number of records from the dataset.
+
+        Args:
+            limit: The max number of records to return.
+
+        Returns:
+            A list of up to ``limit`` records from the dataset.
+        """
         output = []
         for row in self.to_local_iterator():
             output.append(row)
@@ -92,16 +170,32 @@ class Dataset(Generic[T]):
         return output
 
     def show(self, limit: int = 20) -> None:
+        """Print up to the given number of records from the dataset.
+
+        Args:
+            limit: The max number of records to print.
+        """
         for row in self.take(limit):
             print(row)
 
     def to_local_iterator(self) -> Generator:
+        """Return an iterator that can be used to scan the dataset serially.
+
+        Returns:
+            A local iterator over the entire dataset.
+        """
         for b in self._blocks:
             block = self._block_cls.deserialize(ray.get(b))
             for row in block.iter_rows():
                 yield row
 
     def count(self) -> int:
+        """Count the number of records in the dataset.
+
+        Returns:
+            The number of records in the dataset.
+        """
+
         @ray.remote
         def count(serialized: Any) -> int:
             block = self._block_cls.deserialize(serialized)
@@ -110,31 +204,83 @@ class Dataset(Generic[T]):
         return sum(ray.get([count.remote(block) for block in self._blocks]))
 
     def schema(self) -> Union[type, pyarrow.lib.Schema]:
+        """Return the schema of the dataset.
+
+        For datasets of Arrow records, this will return the Arrow schema.
+        For dataset of Python objects, this returns their Python type.
+
+        Returns:
+            The Python type or Arrow schema of the records.
+        """
         raise NotImplementedError  # P0
 
     def size_bytes(self) -> int:
+        """Return the in-memory size of the dataset.
+
+        Returns:
+            The in-memory size of the dataset in bytes.
+        """
         raise NotImplementedError  # P0
 
     def input_files(self) -> List[str]:
+        """Return the list of input files for the dataset.
+
+        Returns:
+            The list of input files used to create the dataset.
+        """
         raise NotImplementedError  # P0
 
     def write_parquet(path: str,
                       filesystem: Optional[pyarrow.fs.FileSystem] = None
                       ) -> None:
+        """Write the dataset to parquet.
+
+        This is only supported for datasets convertible to Arrow records.
+
+        Args:
+            path: The path in the filesystem to write to.
+            filesystem: The filesystem implementation to write to.
+        """
         raise NotImplementedError  # P0
 
     def write_json(path: str,
                    filesystem: Optional[pyarrow.fs.FileSystem] = None) -> None:
+        """Write the dataset to json.
+
+        This is only supported for datasets convertible to Arrow records.
+
+        Args:
+            path: The path in the filesystem to write to.
+            filesystem: The filesystem implementation to write to.
+        """
         raise NotImplementedError  # P0
 
     def write_csv(path: str,
                   filesystem: Optional[pyarrow.fs.FileSystem] = None) -> None:
+        """Write the dataset to csv.
+
+        This is only supported for datasets convertible to Arrow records.
+
+        Args:
+            path: The path in the filesystem to write to.
+            filesystem: The filesystem implementation to write to.
+        """
         raise NotImplementedError  # P0
 
     def to_torch(self, **todo) -> "ray.util.sgd.torch.TorchMLDataset":
+        """Return a dataset that can be used for Torch distributed training.
+
+        Returns:
+            A TorchMLDataset.
+        """
         raise NotImplementedError  # P1
 
     def to_tf(self, **todo) -> "ray.util.sgd.tf.TFMLDataset":
+        """Return a dataset that can be used for TF distributed training.
+
+        Returns:
+            A TFMLDataset.
+        """
         raise NotImplementedError  # P1
 
     def to_batch_iterators(
@@ -144,16 +290,56 @@ class Dataset(Generic[T]):
             output_location_prefs: List[Any] = None,
             batch_format: str = "pandas",
             repeatable: bool = False) -> List[Iterator[BatchType]]:
+        """Return a list of distributed iterators over record batches.
+
+        This returns a list of iterators that can be passed to Ray tasks
+        and actors, and used to read the dataset records in parallel.
+
+        Args:
+            num_shards: Number of iterators to return.
+            batch_size: Record batch size, or None to let the system pick.
+            output_location_prefs: A list of Ray actor handles of size
+                ``num_shards``. The system will try to co-locate the objects
+                given to the ith iterator with the ith actor to maximize data
+                locality.
+            batch_format: Specify "pandas" to select ``pandas.DataFrame`` as
+                the batch format, or "pyarrow" to select ``pyarrow.Table``.
+            repeatable: Whether each iterator should loop over data forever
+                or stop after reading all records of the shard.
+
+        Returns:
+            A list of iterators over record batches.
+        """
         raise NotImplementedError  # P1
 
     def to_dask(self) -> "dask.DataFrame":
+        """Convert this dataset into a Dask dataframe.
+
+        Returns:
+            A Dask dataframe created from this dataset.
+        """
         raise NotImplementedError  # P1
 
     def to_modin(self) -> "modin.DataFrame":
+        """Convert this dataset into a Modin dataframe.
+
+        Returns:
+            A Modin dataframe created from this dataset.
+        """
         raise NotImplementedError  # P1
 
     def to_pandas(self) -> Iterator[ObjectRef["pandas.DataFrame"]]:
+        """Convert this dataset into a set of Pandas dataframes.
+
+        Returns:
+            A list of remote Pandas dataframes created from this dataset.
+        """
         raise NotImplementedError  # P1
 
     def to_spark(self) -> "pyspark.sql.DataFrame":
+        """Convert this dataset into a Spark dataframe.
+
+        Returns:
+            A Spark dataframe created from this dataset.
+        """
         raise NotImplementedError  # P2
