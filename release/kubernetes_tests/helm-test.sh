85f7a0d1295bc895bde68df2ac3153690cc6dde0
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2021-06-24 12:24:41 -0700

    wip

--- release/kubernetes_tests/helm-test.sh
+++ release/kubernetes_tests/helm-test.sh
@@ -2,7 +2,7 @@ set -x
 kubectl create namespace helm-test
 kubectl create namespace helm-test2
 #KUBERNETES_OPERATOR_PULL_POLICY=IfNotPresent
-KUBERNETES_OPERATOR_TEST_NAMESPACE=helm-test KUBERNETES_OPERATOR_TEST_IMAGE="$IMAGE" python ../ray/python/ray/tests/kubernetes_e2e/test_helm.py
+KUBERNETES_OPERATOR_TEST_NAMESPACE=helm-test KUBERNETES_OPERATOR_TEST_IMAGE="$IMAGE" python ../../python/ray/tests/kubernetes_e2e/test_helm.py
 kubectl delete namespace helm-test
 kubectl delete namespace helm-test2
-kubectl delete -f ../ray/deploy/charts/ray/crds/cluster_crd.yaml
+kubectl delete -f ../../deploy/charts/ray/crds/cluster_crd.yaml
--- release/kubernetes_tests/k8s-test-scale.sh
+++ release/kubernetes_tests/k8s-test-scale.sh
@@ -1,10 +1,10 @@
 set -x
 kubectl create namespace scale-test
 kubectl create namespace scale-test2
-KUBERNETES_OPERATOR_TEST_NAMESPACE=scale-test KUBERNETES_OPERATOR_TEST_IMAGE="$IMAGE" python ../ray/python/ray/tests/kubernetes_e2e/test_k8s_operator_scaling.py
+KUBERNETES_OPERATOR_TEST_NAMESPACE=scale-test KUBERNETES_OPERATOR_TEST_IMAGE="$IMAGE" python ../../python/ray/tests/kubernetes_e2e/test_k8s_operator_scaling.py
 kubectl -n scale-test delete --all rayclusters
 kubectl -n scale-test2 delete --all rayclusters
-kubectl delete -f ../ray/deploy/components/operator_cluster_scoped.yaml
+kubectl delete -f ../../deploy/components/operator_cluster_scoped.yaml
 kubectl delete namespace scale-test
 kubectl delete namespace scale-test2
-kubectl delete -f ../ray/deploy/charts/ray/crds/cluster_crd.yaml
+kubectl delete -f ../../deploy/charts/ray/crds/cluster_crd.yaml
