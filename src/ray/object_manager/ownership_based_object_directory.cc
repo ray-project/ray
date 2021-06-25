1f9f4363812549c04cc72582f6dca78ed46a8c3d
SangBin Cho <rkooo567@gmail.com>
2021-06-24 17:47:36 -0700

    format

--- src/ray/object_manager/ownership_based_object_directory.cc
+++ src/ray/object_manager/ownership_based_object_directory.cc
@@ -216,7 +216,8 @@ void OwnershipBasedObjectDirectory::ObjectLocationSubscriptionCallback(
                    << ": " << it->second.current_object_locations.size()
                    << " locations, spilled_url: " << it->second.spilled_url
                    << ", spilled node ID: " << it->second.spilled_node_id
-                   << ", object size: " << it->second.object_size << ", lookup failed: " << location_lookup_failed;
+                   << ", object size: " << it->second.object_size
+                   << ", lookup failed: " << location_lookup_failed;
     metrics_num_object_location_updates_++;
     cum_metrics_num_object_location_updates_++;
     // Copy the callbacks so that the callbacks can unsubscribe without interrupting
