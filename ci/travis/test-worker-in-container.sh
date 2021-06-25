ceada8b8805db1281512683a00e6c93f1d8a3560
wuhua.ck <wuhua.ck@alibaba-inc.com>
2021-06-25 15:49:16 +0800

    test ci

---
+++ ci/travis/test-worker-in-container.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+cleanup() { if [ "${BUILDKITE_PULL_REQUEST}" = "false" ]; then ./ci/travis/upload_build_info.sh; fi }; trap cleanup EXIT
+
+set -exo pipefail
+
+export STORAGE_DRIVER=vfs
+podman load --input /var/lib/containers/images.tar
+
+cat > /ray/docker/ray-nest-container/test-Dockerfile << EOF
+FROM rayproject/ray-nest-container:nightly-py36-cpu
+RUN $HOME/anaconda3/bin/pip --no-cache-dir install pandas
+EOF
+
+podman build -f /ray/docker/ray-nest-container/test-Dockerfile -t rayproject/ray-nest-container:nightly-py36-cpu-pandas .
+
+export RAY_BACKEND_LOG_LEVEL=debug
+"$HOME/anaconda3/bin/pip" install --no-cache-dir pytest
+/ray/ci/travis/install-bazel.sh
+if [ -f /etc/profile.d/bazel.sh ]; then
+  . /etc/profile.d/bazel.sh
+fi
+# shellcheck disable=SC2046
+bazel test --config=ci $(/ray/scripts/bazel_export_options) \
+--test_tag_filters=-kubernetes,-jenkins_only,worker-nest-container,-flaky \
+/ray/python/ray/tests/...
