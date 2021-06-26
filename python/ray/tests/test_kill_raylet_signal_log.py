c69df1f9e2e303692a3c4660d615108ef9bbb100
qicosmos <qicosmos@163.com>
2021-06-26 08:48:54 +0800

    support windows

--- python/ray/tests/test_kill_raylet_signal_log.py
+++ python/ray/tests/test_kill_raylet_signal_log.py
@@ -15,14 +15,14 @@ def get_pid(name):
 def test_kill_raylet_signal_log(shutdown_only):
     ray.init(num_cpus=1)
     session_dir = ray.worker._global_node.get_session_dir_path()
-    raylet_out_path = "{}/logs/raylet.err".format(session_dir)
+    raylet_out_path = "{}/logs/raylet.out".format(session_dir)
     pid = get_pid("raylet")
     p = psutil.Process(pid)
-    p.send_signal(signal.SIGABRT)
+    p.send_signal(signal.SIGTERM)
     p.wait()
     with open(raylet_out_path) as f:
         s = f.read()
-        assert "SIGABRT" in s
+        assert "SIGTERM" in s
 
 
 if __name__ == "__main__":
