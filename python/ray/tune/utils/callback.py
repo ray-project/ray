b9b17d784da013b711ac77b5b4e2a5640b6353d9
Travis Addair <tgaddair@gmail.com>
2021-06-27 21:35:10 -0700

    [tune] Only use TBXLoggerCallback when torch is installed

--- python/ray/tune/utils/callback.py
+++ python/ray/tune/utils/callback.py
@@ -11,6 +11,12 @@ from ray.tune.logger import CSVLoggerCallback, CSVLogger, LoggerCallback, \
     TBXLoggerCallback, TBXLogger
 from ray.tune.syncer import SyncerCallback
 
+try:
+    import torch
+    _HAS_TORCH = True
+except ImportError:
+    _HAS_TORCH = False
+
 logger = logging.getLogger(__name__)
 
 
@@ -108,7 +114,7 @@ def create_default_callbacks(callbacks: Optional[List[Callback]],
         if not has_json_logger:
             callbacks.append(JsonLoggerCallback())
             last_logger_index = len(callbacks) - 1
-        if not has_tbx_logger:
+        if not has_tbx_logger and _HAS_TORCH:
             callbacks.append(TBXLoggerCallback())
             last_logger_index = len(callbacks) - 1
 
