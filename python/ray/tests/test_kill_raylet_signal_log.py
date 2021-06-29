42f49f647635429710ab27b29de93087f0eb59b7
qicosmos <qicosmos@163.com>
2021-06-29 12:00:11 +0800

    fix for windows

--- python/ray/tests/test_kill_raylet_signal_log.py
+++ python/ray/tests/test_kill_raylet_signal_log.py
@@ -1,28 +1,53 @@
+import os
 import psutil
 import signal
 import sys
+import time
 import pytest
+import threading
+
 import ray
 
+from ray.test_utils import (
+    wait_for_pid_to_exit,
+)
 
-def get_pid(name):
-    pids = psutil.process_iter()
-    for pid in pids:
-        if (pid.name() == name):
-            return pid.pid
+SIGKILL = signal.SIGKILL if sys.platform != "win32" else signal.SIGTERM
+
+
+def check_result(path):
+    with open(path) as f:
+        time.sleep(1)
+        s = f.read()
+        assert len(s) > 0
+        print(s)
+        assert "SIGTERM" in s   
+
+
+class check_thread(threading.Thread):
+    def __init__(self, raylet_out_path):
+        threading.Thread.__init__(self)
+        self._daemonic = True
+        self.raylet_out_path = raylet_out_path
+    def run(self):
+        check_result(self.raylet_out_path)
 
 
 def test_kill_raylet_signal_log(shutdown_only):
+
+    @ray.remote
+    def f():
+        return os.getpid()
+
     ray.init(num_cpus=1)
     session_dir = ray.worker._global_node.get_session_dir_path()
     raylet_out_path = "{}/logs/raylet.out".format(session_dir)
-    pid = get_pid("raylet")
-    p = psutil.Process(pid)
-    p.send_signal(signal.SIGTERM)
-    p.wait()
-    with open(raylet_out_path) as f:
-        s = f.read()
-        assert "SIGTERM" in s
+    assert len(raylet_out_path) > 0
+    pid = ray.get(f.remote())
+    os.kill(pid, SIGKILL)
+    wait_for_pid_to_exit(pid)
+    thread = check_thread(raylet_out_path)
+    thread.start()
 
 
 if __name__ == "__main__":
