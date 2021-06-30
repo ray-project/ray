57db2ed31cefe35309b40c5cd9bdd9ded52a42bc
Siyuan <suquark@gmail.com>
2021-06-29 18:15:01 -0700

    storage_or_url -> storage

--- python/ray/experimental/workflow/api.py
+++ python/ray/experimental/workflow/api.py
@@ -7,10 +7,11 @@ import ray
 
 from ray.experimental.workflow import workflow_context
 from ray.experimental.workflow import recovery
-from ray.experimental.workflow import storage
 from ray.experimental.workflow.common import Workflow
 from ray.experimental.workflow.workflow_manager import WorkflowStepFunction
 from ray.experimental.workflow.step_executor import postprocess_workflow_step
+from ray.experimental.workflow.storage import (
+    Storage, create_storage, get_global_storage, set_global_storage)
 from ray.experimental.workflow.workflow_access import resolve_workflow_outputs
 
 logger = logging.getLogger(__name__)
@@ -30,14 +31,14 @@ def step(func) -> WorkflowStepFunction:
 
 
 def run(entry_workflow: Workflow,
-        storage_or_url: Optional[Union[str, storage.Storage]] = None,
+        storage: Optional[Union[str, Storage]] = None,
         workflow_id: Optional[str] = None) -> ray.ObjectRef:
     """
     Run a workflow asynchronously.
 
     Args:
         entry_workflow: The workflow to run.
-        storage_or_url: The storage or the URL of an external storage used for
+        storage: The storage or the URL of an external storage used for
             checkpointing.
         workflow_id: The ID of the workflow. The ID is used to identify
             the workflow.
@@ -49,13 +50,13 @@ def run(entry_workflow: Workflow,
     if workflow_id is None:
         # Workflow ID format: {Entry workflow UUID}.{Unix time to nanoseconds}
         workflow_id = f"{entry_workflow.id}.{time.time():.9f}"
-    if isinstance(storage_or_url, str):
-        storage.set_global_storage(storage.create_storage(storage_or_url))
-    elif isinstance(storage_or_url, storage.Storage):
-        storage.set_global_storage(storage_or_url)
-    elif storage_or_url is not None:
-        raise TypeError("storage_or_url should be None, str, or Storage type.")
-    storage_url = storage.get_global_storage().storage_url
+    if isinstance(storage, str):
+        set_global_storage(create_storage(storage))
+    elif isinstance(storage, Storage):
+        set_global_storage(storage)
+    elif storage is not None:
+        raise TypeError("'storage' should be None, str, or Storage type.")
+    storage_url = get_global_storage().storage_url
     logger.info(f"Workflow job created. [id=\"{workflow_id}\", storage_url="
                 f"\"{storage_url}\"].")
     try:
@@ -72,29 +73,28 @@ def run(entry_workflow: Workflow,
 
 
 def resume(workflow_id: str,
-           storage_or_url: Optional[Union[str, storage.Storage]] = None
-           ) -> ray.ObjectRef:
+           storage: Optional[Union[str, Storage]] = None) -> ray.ObjectRef:
     """
     Resume a workflow asynchronously. This workflow maybe fail previously.
 
     Args:
         workflow_id: The ID of the workflow. The ID is used to identify
             the workflow.
-        storage_or_url: The storage or the URL of an external storage used for
+        storage: The storage or the URL of an external storage used for
             checkpointing.
 
     Returns:
         The execution result of the workflow, represented by Ray ObjectRef.
     """
     assert ray.is_initialized()
-    if isinstance(storage_or_url, str):
-        store = storage.create_storage(storage_or_url)
-    elif isinstance(storage_or_url, storage.Storage):
-        store = storage_or_url
-    elif storage_or_url is None:
-        store = storage.get_global_storage()
+    if isinstance(storage, str):
+        store = create_storage(storage)
+    elif isinstance(storage, Storage):
+        store = storage
+    elif storage is None:
+        store = get_global_storage()
     else:
-        raise TypeError("storage_or_url should be None, str, or Storage type.")
+        raise TypeError("'storage' should be None, str, or Storage type.")
     r = recovery.resume_workflow_job(workflow_id, store)
     logger.info(f"Resuming workflow [id=\"{workflow_id}\", storage_url="
                 f"\"{store.storage_url}\"].")
