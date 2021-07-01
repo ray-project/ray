34cd6a862abf41332058379c8d166b7203398a1a
Chen Shen <scv119@gmail.com>
2021-07-01 13:03:34 -0700

    [dataset] implements split (#16765)

--- python/ray/experimental/data/tests/test_dataset.py
+++ python/ray/experimental/data/tests/test_dataset.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 
+from unittest.mock import patch
 import dask.dataframe as dd
 import pandas as pd
 import pyarrow as pa
@@ -235,6 +236,135 @@ def test_map_batch(ray_start_regular_shared, tmp_path):
     with pytest.raises(ValueError):
         ds_list = ds.map_batches(
             lambda df: 1, batch_size=2, batch_format="pyarrow").take()
+
+
+def test_split(ray_start_regular_shared):
+    ds = ray.experimental.data.range(20, parallelism=10)
+    assert ds.num_blocks() == 10
+    assert ds.sum() == 190
+    assert ds._block_sizes() == [2] * 10
+
+    datasets = ds.split(5)
+    assert [2] * 5 == [len(dataset._blocks) for dataset in datasets]
+    assert 190 == sum([dataset.sum() for dataset in datasets])
+
+    datasets = ds.split(3)
+    assert [4, 3, 3] == [len(dataset._blocks) for dataset in datasets]
+    assert 190 == sum([dataset.sum() for dataset in datasets])
+
+    datasets = ds.split(1)
+    assert [10] == [len(dataset._blocks) for dataset in datasets]
+    assert 190 == sum([dataset.sum() for dataset in datasets])
+
+    datasets = ds.split(10)
+    assert [1] * 10 == [len(dataset._blocks) for dataset in datasets]
+    assert 190 == sum([dataset.sum() for dataset in datasets])
+
+    datasets = ds.split(11)
+    assert [1] * 10 + [0] == [len(dataset._blocks) for dataset in datasets]
+    assert 190 == sum([dataset.sum() for dataset in datasets])
+
+
+def test_split_hints(ray_start_regular_shared):
+    @ray.remote
+    class Actor(object):
+        def __init__(self):
+            pass
+
+    def assert_split_assignment(block_node_ids, actor_node_ids,
+                                expected_split_result):
+        """Helper function to setup split hints test.
+
+        Args:
+            block_node_ids: a list of blocks with their locations. For
+                example ["node1", "node2"] represents two blocks with
+                "node1", "node2" as their location respectively.
+            actor_node_ids: a list of actors with their locations. For
+                example ["node1", "node2"] represents two actors with
+                "node1", "node2" as their location respectively.
+            expected_split_result: a list of allocation result, each entry
+                in the list stores the block_index in the split dataset.
+                For example, [[0, 1], [2]] represents the split result has
+                two datasets, datasets[0] contains block 0 and 1; and
+                datasets[1] contains block 2.
+        """
+        num_blocks = len(block_node_ids)
+        ds = ray.experimental.data.range(num_blocks, parallelism=num_blocks)
+        blocks = list(ds._blocks)
+        assert len(block_node_ids) == len(blocks)
+        actors = [Actor.remote() for i in range(len(actor_node_ids))]
+        with patch("ray.experimental.get_object_locations") as location_mock:
+            with patch("ray.state.actors") as state_mock:
+                block_locations = {}
+                for i, node_id in enumerate(block_node_ids):
+                    if node_id:
+                        block_locations[blocks[i]] = {"node_ids": [node_id]}
+                location_mock.return_value = block_locations
+
+                actor_state = {}
+                for i, node_id in enumerate(actor_node_ids):
+                    actor_state[actors[i]._actor_id.hex()] = {
+                        "Address": {
+                            "NodeID": node_id
+                        }
+                    }
+
+                state_mock.return_value = actor_state
+
+                datasets = ds.split(len(actors), actors)
+                assert len(datasets) == len(actors)
+                for i in range(len(actors)):
+                    assert {blocks[j]
+                            for j in expected_split_result[i]} == set(
+                                datasets[i]._blocks)
+
+    assert_split_assignment(["node2", "node1", "node1"], ["node1", "node2"],
+                            [[1, 2], [0]])
+    assert_split_assignment(["node1", "node1", "node1"], ["node1", "node2"],
+                            [[2, 1], [0]])
+    assert_split_assignment(["node2", "node2", None], ["node1", "node2"],
+                            [[0, 2], [1]])
+    assert_split_assignment(["node2", "node2", None], [None, None],
+                            [[2, 1], [0]])
+    assert_split_assignment(["n1", "n2", "n3", "n1", "n2"], ["n1", "n2"],
+                            [[0, 2, 3], [1, 4]])
+
+    assert_split_assignment(["n1", "n2"], ["n1", "n2", "n3"], [[0], [1], []])
+
+    # perfect split:
+    #
+    # split 300 blocks
+    #   with node_ids interleaving between "n0", "n1", "n2"
+    #
+    # to 3 actors
+    #   with has node_id "n1", "n2", "n0"
+    #
+    # expect that block 1, 4, 7... are assigned to actor with node_id n1
+    #             block 2, 5, 8... are assigned to actor with node_id n2
+    #             block 0, 3, 6... are assigned to actor with node_id n0
+    assert_split_assignment(
+        ["n0", "n1", "n2"] * 100, ["n1", "n2", "n0"],
+        [range(1, 300, 3),
+         range(2, 300, 3),
+         range(0, 300, 3)])
+
+    # even split regardless of locality:
+    #
+    # split 301 blocks
+    #   with block 0 to block 50 on "n0",
+    #        block 51 to block 300 on "n1"
+    #
+    # to 3 actors
+    #   with node_ids "n1", "n2", "n0"
+    #
+    # expect that block 200 to block 300 are assigned to actor with node_id n1
+    #             block 100 to block 199 are assigned to actor with node_id n2
+    #             block 0 to block 99 are assigned to actor with node_id n0
+    assert_split_assignment(["n0"] * 50 + ["n1"] * 251, ["n1", "n2", "n0"], [
+        range(200, 301),
+        range(100, 200),
+        list(range(0, 50)) + list(range(50, 100))
+    ])
 
 
 def test_from_dask(ray_start_regular_shared):
