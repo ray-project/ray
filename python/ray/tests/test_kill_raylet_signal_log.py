95abfad7b7f75910ea7b00f9b5150bc38fc248f1
qicosmos <qicosmos@163.com>
2021-07-01 14:43:54 +0800

    add wait timeout

--- python/ray/tests/test_kill_raylet_signal_log.py
+++ python/ray/tests/test_kill_raylet_signal_log.py
@@ -11,15 +11,17 @@ def get_pid(name):
         if name in pid.name():
             return pid.pid
 
+    return -1
 
 def check_result(filename, num_signal, check_key):
     ray.init(num_cpus=1)
     session_dir = ray.worker._global_node.get_session_dir_path()
     raylet_out_path = filename.format(session_dir)
     pid = get_pid("raylet")
+    assert pid > 0
     p = psutil.Process(pid)
     p.send_signal(num_signal)
-    p.wait()
+    p.wait(timeout=15)
     with open(raylet_out_path) as f:
         s = f.read()
         assert check_key in s
