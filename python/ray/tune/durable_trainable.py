883ef0c9317fb5ddd0e338ad67fb40a25df50b51
Kai Fricke <krfricke@users.noreply.github.com>
2021-07-06 10:50:59 +0200

    [tune] Pass custom `sync_to_cloud` templates to durable trainables (#16739)

--- python/ray/tune/durable_trainable.py
+++ python/ray/tune/durable_trainable.py
@@ -1,4 +1,4 @@
-from typing import Callable, Type, Union
+from typing import Callable, Optional, Type, Union
 
 import inspect
 import logging
@@ -6,6 +6,7 @@ import os
 
 from ray.tune.function_runner import wrap_function
 from ray.tune.registry import get_trainable_cls
+from ray.tune.sync_client import get_sync_client
 from ray.tune.trainable import Trainable, TrainableUtil
 from ray.tune.syncer import get_cloud_sync_client
 
@@ -31,15 +32,23 @@ class DurableTrainable(Trainable):
 
     >>> tune.run(MyDurableTrainable, sync_to_driver=False)
     """
+    _sync_function_tpl = None
 
-    def __init__(self, remote_checkpoint_dir, *args, **kwargs):
+    def __init__(self,
+                 remote_checkpoint_dir: str,
+                 sync_function_tpl: Optional[str] = None,
+                 *args,
+                 **kwargs):
         """Initializes a DurableTrainable.
 
         Args:
             remote_checkpoint_dir (str): Upload directory (S3 or GS path).
+            sync_function_tpl (str): Sync function template to use. Defaults
+              to `cls._sync_function` (which defaults to `None`).
         """
         super(DurableTrainable, self).__init__(*args, **kwargs)
         self.remote_checkpoint_dir = remote_checkpoint_dir
+        self.sync_function_tpl = sync_function_tpl or self._sync_function_tpl
         self.storage_client = self._create_storage_client()
 
     def save(self, checkpoint_dir=None):
@@ -96,7 +105,9 @@ class DurableTrainable(Trainable):
 
     def _create_storage_client(self):
         """Returns a storage client."""
-        return get_cloud_sync_client(self.remote_checkpoint_dir)
+        return get_sync_client(
+            self.sync_function_tpl) or get_cloud_sync_client(
+                self.remote_checkpoint_dir)
 
     def _storage_path(self, local_path):
         rel_local_path = os.path.relpath(local_path, self.logdir)
