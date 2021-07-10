77e2d6115beec40c9ee76be88b7d902003faa732
Julius <juliustfrost@gmail.com>
2021-07-09 20:13:02 -0400

    lint

--- python/ray/tune/registry.py
+++ python/ray/tune/registry.py
@@ -94,7 +94,7 @@ def register_input(name: str, input_creator: Callable):
 
     Args:
         name (str): Name to register.
-        input_creator (IOContext -> InputReader): Callable that creates an 
+        input_creator (IOContext -> InputReader): Callable that creates an
             input reader.
     """
     if not callable(input_creator):
