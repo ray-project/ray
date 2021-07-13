b11b5055207f0ceaf5e76f831f03ac7e03271667
Yi Cheng <chengyidna@gmail.com>
2021-07-13 19:22:16 +0000

    up

--- python/ray/tests/test_placement_group.py
+++ python/ray/tests/test_placement_group.py
@@ -29,7 +29,7 @@ class Increase:
 
 
 @pytest.mark.parametrize("connect_to_client", [True, False])
-def test_placement_ready(ray_start_regular_shared, connect_to_client):
+def test_placement_ready(ray_start_regular, connect_to_client):
     @ray.remote
     class Actor:
         def __init__(self):
