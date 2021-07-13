3fcdb251d6c9f9dfb4871755c8f4ccfa9ded7253
Philipp Moritz <pcmoritz@gmail.com>
2021-07-13 15:52:45 -0700

    print breakpoints as table

--- python/ray/scripts/scripts.py
+++ python/ray/scripts/scripts.py
@@ -194,6 +194,15 @@ def continue_debug_session():
                 time.sleep(1.0)
 
 
+def format_table(table):
+    "Format a table as a list of lines with aligned columns."
+    result = []
+    col_width = [max(len(x) for x in col) for col in zip(*table)]
+    for line in table:
+        result.append(" | ".join("{0:{1}}".format(x, col_width[i]) for i, x in enumerate(line)))
+    return result
+
+
 @cli.command()
 @click.option(
     "--address",
@@ -217,12 +226,15 @@ def debug(address):
             data = json.loads(
                 ray.experimental.internal_kv._internal_kv_get(active_session))
             sessions_data.append(data)
-        sessions_data = sorted(sessions_data, key=lambda data: data["timestamp"])
+        sessions_data = sorted(sessions_data, key=lambda data: data["timestamp"], reverse=True)
+        table = [["index", "timestamp", "Ray task", "filename:lineno"]]
         for i, data in enumerate(sessions_data):
-            print(
-                str(i) + ": " + data["timestamp"] + " | " + data["proctitle"] + " | " + data["filename"] +
-                ":" + str(data["lineno"]))
-            print(data["traceback"])
+            date = datetime.utcfromtimestamp(data["timestamp"]).strftime("%Y-%m-%d %H:%M:%S")
+            table.append([str(i), date, data["proctitle"], data["filename"] + ":" + str(data["lineno"])])
+        for i, line in enumerate(format_table(table)):
+            print(line)
+            if i >= 1 and not sessions_data[i-1]["traceback"].startswith("NoneType: None"):
+                print(sessions_data[i-1]["traceback"])
         inp = input("Enter breakpoint index or press enter to refresh: ")
         if inp == "":
             print()
