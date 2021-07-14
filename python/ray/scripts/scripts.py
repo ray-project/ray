4e9f24744e36bae30c2ae4afba6d504346529b97
Amog Kamsetty <amogkamsetty@yahoo.com>
2021-07-13 19:50:52 -0700

    fix

--- python/ray/scripts/scripts.py
+++ python/ray/scripts/scripts.py
@@ -1359,7 +1359,7 @@ def stack():
     COMMAND = """
 pyspy=`which py-spy`
 if [ ! -e "$pyspy" ]; then
-    echo "ERROR: Please 'pip install py-spy' or `pip install ray[default]
+    echo "ERROR: Please 'pip install py-spy' or 'pip install ray[default]'
     first"
     exit 1
 fi
