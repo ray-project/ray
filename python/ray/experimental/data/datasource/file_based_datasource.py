40e92ac9d29eb2f79cb956dfc9fe2e289ae088d3
Clark Zinzow <clarkzinzow@gmail.com>
2021-07-16 21:47:33 +0000

    Use urllib to parse and trim URI to path.

--- python/ray/experimental/data/datasource/file_based_datasource.py
+++ python/ray/experimental/data/datasource/file_based_datasource.py
@@ -1,5 +1,6 @@
 import logging
 from typing import Optional, List, Tuple, Union, TYPE_CHECKING
+from urllib.parse import urlparse
 
 if TYPE_CHECKING:
     import pyarrow
@@ -173,21 +174,12 @@ def _resolve_paths_and_filesystem(
     return expanded_paths, file_infos, filesystem
 
 
-S3_PROTOCOL = "s3://"
-S3A_PROTOCOL = "s3a://"
-S3N_PROTOCOL = "s3n://"
-PROTOCOLS = [S3_PROTOCOL, S3A_PROTOCOL, S3N_PROTOCOL]
-
-
 def _unwrap_protocol(path):
     """
     Slice off any protocol prefixes on path.
     """
-    for protocol in PROTOCOLS:
-        if path.startswith(protocol):
-            path = path[len(protocol):]
-            break
-    return path
+    parsed = urlparse(path)
+    return parsed.netloc + parsed.path
 
 
 class _S3FileSystemWrapper:
