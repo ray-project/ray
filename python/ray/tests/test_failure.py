618fa17785cee98dec84ac5c7dc6dedc664aaab0
Eric Liang <ekhliang@gmail.com>
2021-07-15 20:18:54 -0700

    fix

--- python/ray/tests/test_failure.py
+++ python/ray/tests/test_failure.py
@@ -524,7 +524,8 @@ def test_export_large_objects(ray_start_regular, error_pubsub):
     p = error_pubsub
     import ray.ray_constants as ray_constants
 
-    large_object = np.zeros(2 * ray_constants.FUNCTION_SIZE_WARN_THRESHOLD)
+    large_object = np.zeros(
+        2 * ray_constants.FUNCTION_SIZE_WARN_THRESHOLD, dtype=np.uint8)
 
     @ray.remote
     def f():
