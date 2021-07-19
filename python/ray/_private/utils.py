d59da075a6a8d9e985f121af59adea9aa6073b4a
Eric Liang <ekhliang@gmail.com>
2021-07-19 15:45:03 -0700

    Re-merge TMPDIR support, but only for Linux. OSX requires RAY_TMPDIR (#17190)

--- python/ray/_private/utils.py
+++ python/ray/_private/utils.py
@@ -46,7 +46,11 @@ win32_AssignProcessToJobObject = None
 
 
 def get_user_temp_dir():
-    if sys.platform.startswith("darwin") or sys.platform.startswith("linux"):
+    if "RAY_TMPDIR" in os.environ:
+        return os.environ["RAY_TMPDIR"]
+    elif sys.platform.startswith("linux") and "TMPDIR" in os.environ:
+        return os.environ["TMPDIR"]
+    elif sys.platform.startswith("darwin") or sys.platform.startswith("linux"):
         # Ideally we wouldn't need this fallback, but keep it for now for
         # for compatibility
         tempdir = os.path.join(os.sep, "tmp")
