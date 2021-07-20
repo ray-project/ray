4d00abfb9be9156e29a6ceb0f49ebcd50bd09a36
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2021-07-20 10:07:27 -0700

    Revert node provider

--- python/ray/autoscaler/node_provider.py
+++ python/ray/autoscaler/node_provider.py
@@ -5,7 +5,6 @@ from typing import Any, Dict, List, Optional
 from ray.autoscaler.command_runner import CommandRunnerInterface
 from ray.autoscaler._private.command_runner import \
     SSHCommandRunner, DockerCommandRunner
-from ray.autoscaler._private.util import ConcurrentCounter
 
 logger = logging.getLogger(__name__)
 
@@ -132,29 +131,6 @@ class NodeProvider:
             logger.info("NodeProvider: "
                         "{}: Terminating node".format(node_id))
             self.terminate_node(node_id)
-
-    def before_autoscaler_update(self) -> None:
-        """Optional hook to run at the start of an autoscaler update.
-        See autoscaler.py
-
-        Example use: Receive updates from a remote operator.
-        """
-        return
-
-    def after_autoscaler_update(self,
-                                pending_launches: ConcurrentCounter) -> None:
-        """Optional hook to run at the end of an autoscaler update.
-        See autoscaler.py
-
-        Example use: Send a scale request to a remote operator.
-
-        The parameter pending_launches can be used by a NodeProvider, e.g. to
-        wait for pending node launches to finish processing.
-
-        Args:
-            pending_launches: ConcurrentCounter of pending node launches.
-        """
-        return
 
     @staticmethod
     def bootstrap_config(cluster_config: Dict[str, Any]) -> Dict[str, Any]:
