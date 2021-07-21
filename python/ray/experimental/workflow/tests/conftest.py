a0c2a63a0664c48dce4020d4252c98374c94d1e5
Yi Cheng <chengyidna@gmail.com>
2021-07-21 00:27:25 +0000

    check

--- python/ray/experimental/workflow/tests/conftest.py
+++ python/ray/experimental/workflow/tests/conftest.py
@@ -40,6 +40,32 @@ def s3_storage(aws_credentials, s3_server):
         storage.set_global_storage(storage.create_storage(url))
         yield storage.get_global_storage()
 
+
+@contextmanager
+def _workflow_start(**kwargs):
+    init_kwargs = get_default_fixture_ray_kwargs()
+    init_kwargs.update(kwargs)
+    # Start the Ray processes.
+    address_info = ray.init(**init_kwargs)
+    # workflow.init()
+    yield address_info
+    # The code after the yield will run as teardown code.
+    ray.shutdown()
+
+
+@pytest.fixture
+def workflow_start_regular(raw_storage, request):
+    param = getattr(request, "param", {})
+    with _workflow_start(**param) as res:
+        yield res
+
+
+@pytest.fixture(scope="module")
+def workflow_start_regular_shared(raw_storage, request):
+    param = getattr(request, "param", {})
+    with _workflow_start(**param) as res:
+        yield res
+
 def pytest_generate_tests(metafunc):
     if "raw_storage" in metafunc.fixturenames:
         metafunc.parametrize("raw_storage", [lazy_fixture("s3_storage"), lazy_fixture("filesystem_storage")])
