1addc11c4491ba125e1ca6ebf17ef6f85b07001e
scv119 <scv119@gmail.com>
2021-07-21 15:10:20 -0700

    Revert "[core] Do not spill back tasks blocked on args to blocked nodes (#16488)"
    This reverts commit dad8db46e1ed2acdb8e9b29b1c91b72c2800d60d.

--- src/ray/raylet/scheduling/cluster_resource_data.h
+++ src/ray/raylet/scheduling/cluster_resource_data.h
@@ -65,9 +65,6 @@ class ResourceRequest {
   std::vector<FixedPoint> predefined_resources;
   /// List of custom resources required by the task.
   std::unordered_map<int64_t, FixedPoint> custom_resources;
-  /// Whether this task requires object store memory.
-  /// TODO(swang): This should be a quantity instead of a flag.
-  bool requires_object_store_memory = false;
   /// Check whether the request contains no resources.
   bool IsEmpty() const;
   /// Returns human-readable string for this task request.
@@ -147,22 +144,18 @@ class NodeResources {
   NodeResources() {}
   NodeResources(const NodeResources &other)
       : predefined_resources(other.predefined_resources),
-        custom_resources(other.custom_resources),
-        object_pulls_queued(other.object_pulls_queued) {}
+        custom_resources(other.custom_resources) {}
   /// Available and total capacities for predefined resources.
   std::vector<ResourceCapacity> predefined_resources;
   /// Map containing custom resources. The key of each entry represents the
   /// custom resource ID.
   absl::flat_hash_map<int64_t, ResourceCapacity> custom_resources;
-  bool object_pulls_queued = false;
-
   /// Amongst CPU, memory, and object store memory, calculate the utilization percentage
   /// of each resource and return the highest.
   float CalculateCriticalResourceUtilization() const;
   /// Returns true if the node has the available resources to run the task.
   /// Note: This doesn't account for the binpacking of unit resources.
-  bool IsAvailable(const ResourceRequest &resource_request,
-                   bool ignore_at_capacity = false) const;
+  bool IsAvailable(const ResourceRequest &resource_request) const;
   /// Returns true if the node's total resources are enough to run the task.
   /// Note: This doesn't account for the binpacking of unit resources.
   bool IsFeasible(const ResourceRequest &resource_request) const;
@@ -227,7 +220,6 @@ NodeResources ResourceMapToNodeResources(
 /// Convert a map of resources to a ResourceRequest data structure.
 ResourceRequest ResourceMapToResourceRequest(
     StringIdMap &string_to_int_map,
-    const std::unordered_map<std::string, double> &resource_map,
-    bool requires_object_store_memory);
+    const std::unordered_map<std::string, double> &resource_map);
 
 }  // namespace ray
