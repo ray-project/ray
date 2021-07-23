77b8ba23e7d250d0dda955912c0693f2f38e8c0c
Yi Cheng <chengyidna@gmail.com>
2021-07-23 23:42:58 +0000

    up

--- python/ray/experimental/workflow/step_executor.py
+++ python/ray/experimental/workflow/step_executor.py
@@ -189,12 +189,12 @@ def _wrap_run(func: Callable, step_type: StepType, step_id: "StepID",
             break
         except BaseException as e:
             if i + 1 == max_retries:
-                retry_msg = f"Maximum retry reached."
+                retry_msg = "Maximum retry reached."
             else:
                 retry_msg = "The step will be retried."
             logger.error(
-                f"{workflow_context.get_step_name()} failed with error {e}. {retry_msg}"
-            )
+                f"{workflow_context.get_step_name()} failed with error"
+                f" {e}. {retry_msg}")
             exception = e
 
     if catch_exceptions:
