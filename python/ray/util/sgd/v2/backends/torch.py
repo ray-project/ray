fe9697c40dbad0afba100aace8a4823176d703a0
Matthew Deng <matthew.j.deng@gmail.com>
2021-07-23 17:17:19 -0700

    add method interfaces

--- python/ray/util/sgd/v2/backends/torch.py
+++ python/ray/util/sgd/v2/backends/torch.py
@@ -3,10 +3,19 @@ from dataclasses import dataclass
 from ray.util.sgd.v2.backends.backend import BackendExecutor, BackendConfig
 
 
-class TorchExecutor(BackendExecutor):
+@dataclass
+class TorchConfig(BackendConfig):
+    def backend_name(self):
+        return "torch"
+
+
+def setup_torch_process_group():
     pass
 
 
-@dataclass
-class TorchConfig(BackendConfig):
+def shutdown_torch():
+    pass
+
+
+class TorchExecutor(BackendExecutor):
     pass
--- python/ray/util/sgd/v2/worker_group.py
+++ python/ray/util/sgd/v2/worker_group.py
@@ -1,9 +1,27 @@
-from dataclasses import dataclass
-from typing import Callable, Any
+from typing import Callable, Any, List
+
+import ray
+from ray.types import ObjectRef
 
 
 class WorkerGroup:
-    pass
+    def __init__(self,
+                 num_workers: int = 1,
+                 num_cpus_per_worker: int = 1,
+                 num_gpus_per_worker: int = 0):
+        self.workers = self._start_workers()
+
+    def _start_workers(self):
+        pass
+
+    def shutdown(self):
+        pass
+
+    def execute_async(self, func: Callable) -> List[ObjectRef]:
+        return [w.execute(func) for w in self.workers]
+
+    def execute(self, func: Callable) -> List[Any]:
+        return ray.get(self.execute_async(func))
 
 
 class BaseWorker:
@@ -13,8 +31,3 @@ class BaseWorker:
             func(Callable): A function that does not take any arguments.
         """
         return func()
-
-
-@dataclass
-class WorkerConfig:
-    pass
