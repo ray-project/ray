dd7e18a92798f74c774b09ae21bd3f19a3949543
architkulkarni <arkulkar@gmail.com>
2021-07-26 08:47:30 -0700

    update test

--- release/runtime_env_tests/workloads/many_tasks_actors.py
+++ release/runtime_env_tests/workloads/many_tasks_actors.py
@@ -9,18 +9,26 @@ Acceptance criteria: Should run through and print "PASSED."
 
 import ray
 import random
+import os
 
-ray.client().env({"pip": ["requests==2.26.0"]}).connect()
+# ray.client().env({"pip": ["requests==2.26.0"]}).connect()
 
-versions = ["2.21.0", "2.22.0", "2.23.0", "2.24.0", "2.25.0", "2.26.0"]
+ray.init(runtime_env={"pip": ["requests==2.26.0"]})
+versions = ["2.25.0", "2.26.0"]
 envs = [{
     "pip": [f"requests=={versions[i]}"]
 } for i in range(len(versions) - 1)]
-envs.append({})  # In an empty runtime env, we should have requests==2.26.0.
+# If a task's env is {}, we should have requests==2.26.0 from the job's env.
+envs.append({})  
 
-NUM_ITERATIONS = 1  # 100
-NUM_CALLS_PER_ITERATION = 1  # 1000
-NUM_CPUS = 12
+NUM_ITERATIONS = 10# 10
+NUM_CALLS_PER_ITERATION = 1000# 1000
+NUM_ENVS_PER_ITERATION = 5# 5
+
+if os.environ.get("IS_SMOKE_TEST"):
+    NUM_ITERATIONS = 100
+    NUM_CALLS_PER_ITERATION = 1
+    NUM_CPUS = 1
 
 
 @ray.remote
@@ -31,37 +39,40 @@ def check_version_task(expected_version: str):
 
 for i in range(NUM_ITERATIONS):
     results = []
-    for i in range(12):
-        env, expected_version = random.choice(zip(envs, versions))
+    for j in range(NUM_ENVS_PER_ITERATION):
+        (env, expected_version) = random.choice(list(zip(envs, versions)))
         remote_task = check_version_task.options(runtime_env=env)
         results.extend([
             remote_task.remote(expected_version)
             for _ in range(NUM_CALLS_PER_ITERATION)
         ])
     ray.get(results)
+    print(f"Finished iteration {i+1}/{NUM_ITERATIONS}")
 
 
 @ray.remote
 class TestActor:
-    def check_version(expected_version: str):
+    def check_version(self, expected_version: str):
         import requests
         assert requests.__version__ == expected_version
 
-    def nested_check_version(expected_version: str):
+    def nested_check_version(self, expected_version: str):
         ray.get(check_version_task.remote(expected_version))
 
 
-for i in range(NUM_ITERATIONS):
-    results = []
-    for i in range(12):
-        env, expected_version = random.choice(zip(envs, versions))
-        actor = TestActor.options(runtime_env=env).remote()
-        results.extend([
-            actor.check_version.remote(expected_version)
-            for _ in range(NUM_CALLS_PER_ITERATION)
-        ])
-        results.extend([
-            actor.nested_check_version.remote(expected_version)
-            for _ in range(NUM_CALLS_PER_ITERATION)
-        ])
-    ray.get(results)
+# for i in range(NUM_ITERATIONS):
+#     results = []
+#     for j in range(NUM_ENVS_PER_ITERATION):
+#         env, expected_version = random.choice(list(zip(envs, versions)))
+#         actor = TestActor.options(runtime_env=env).remote()
+#         results.extend([
+#             actor.check_version.remote(expected_version)
+#             for _ in range(NUM_CALLS_PER_ITERATION)
+#         ])
+#         # results.extend([
+#         #     actor.nested_check_version.remote(expected_version)
+#         #     for _ in range(NUM_CALLS_PER_ITERATION)
+#         # ])
+#         print("sub iteration", j)
+#     ray.get(results)
+#     print("--\n\nDONE WITH ITERATION ", i)
