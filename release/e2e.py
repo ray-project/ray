fb322e446815695bb2170712a76b6136c13d0bde
SangBin Cho <rkooo567@gmail.com>
2021-07-28 16:05:14 -0700

    done

--- release/e2e.py
+++ release/e2e.py
@@ -1027,8 +1027,6 @@ def run_test_config(
                 Key: Name
                 Value: S3 URL
     """
-    session_url = None
-    runtime = None
     # Todo (mid-term): Support other cluster definitions
     #  (not only cluster configs)
     cluster_config_rel_path = test_config["cluster"].get(
@@ -1205,6 +1203,9 @@ def run_test_config(
             ))
 
     def _run(logger):
+        # These values will be set as the test runs.
+        session_url = None
+        runtime = None
         anyscale.conf.CLI_TOKEN = GLOBAL_CONFIG["ANYSCALE_CLI_TOKEN"]
 
         session_id = None
