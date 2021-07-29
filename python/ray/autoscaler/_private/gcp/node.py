361489f6e89e5e0f6396f2f782a81fa25095719a
Ian <ian.rodney@gmail.com>
2021-07-29 14:22:12 +0000

    fix node.py

--- python/ray/autoscaler/_private/gcp/node.py
+++ python/ray/autoscaler/_private/gcp/node.py
@@ -386,9 +386,21 @@ class GCPCompute(GCPResource):
 
     def _convert_resources_to_urls(
             self, configuration_dict: Dict[str, Any]) -> Dict[str, Any]:
+        """Ensures that resources are in their full URL form.
+
+        GCP expects machineType and accleratorType to be a full URL (e.g.
+        `zones/us-west1/machineTypes/n1-standard-2`) instead of just the
+        type (`n1-standard-2`)
+
+        Args:
+            configuration_dict: Dict of options that will be passed to GCP
+        Returns:
+            Input dictionary, but with possibly expanding `machineType` and
+                `acceleratorType`.
+        """
         configuration_dict = deepcopy(configuration_dict)
         existing_machine_type = configuration_dict["machineType"]
-        if not re.search("zones/*/machineTypes/*", existing_machine_type):
+        if not re.search(".*/machineTypes/.*", existing_machine_type):
             configuration_dict["machineType"] = (
                 "zones/{zone}/machineTypes/{machine_type}"
                 "".format(
@@ -397,8 +409,7 @@ class GCPCompute(GCPResource):
 
         for accelerator in configuration_dict.get("guestAccelerators", []):
             gpu_type = accelerator["acceleratorType"]
-            if not re.search("projects/.*/zones/.*/acceleratorTypes/.*",
-                             gpu_type):
+            if not re.search(".*/acceleratorTypes/.*", gpu_type):
                 accelerator["acceleratorType"] = (
                     "projects/{project}/zones/{zone}/"
                     "acceleratorTypes/{accelerator}".format(
