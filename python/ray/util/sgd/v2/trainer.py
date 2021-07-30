2e48023dbd178159dc872dbd1da9d19fb7122a95
Matthew Deng <matthew.j.deng@gmail.com>
2021-07-29 17:46:45 -0700

    move arguments

--- python/ray/util/sgd/v2/trainer.py
+++ python/ray/util/sgd/v2/trainer.py
@@ -1,4 +1,4 @@
-from typing import Union, Callable, List, TypeVar, Optional, Any, Dict, Type
+from typing import Union, Callable, List, TypeVar, Optional, Any, Dict
 
 from ray.util.sgd.v2.backends.backend import BackendConfig
 from ray.util.sgd.v2.callbacks.callback import Callback
@@ -11,12 +11,9 @@ class Trainer:
     def __init__(self,
                  backend: Union[str, BackendConfig],
                  num_workers: int = 1,
-                 num_cpus_per_worker: float = 1,
-                 num_gpus_per_worker: float = 0,
-                 train_cls: Optional[Type[S]] = None,
-                 callbacks: Optional[List[Callback]] = None):
+                 use_gpu: bool = False,
+                 resources_per_worker: Optional[Dict[str, float]] = None):
         """A class for distributed training.
-
         Args:
             backend (Union[str, BackendConfig]): The backend used for
                 distributed communication. If configurations are needed,
@@ -24,20 +21,16 @@ class Trainer:
                 Supported ``str`` values: {"torch"}.
             num_workers (int): The number of workers (Ray actors) to launch.
                 Defaults to 1.
-            num_cpus_per_worker (float): The number of CPUs to reserve for each
-                worker. Fractional values are allowed. Defaults to 1.
-            num_gpus_per_worker (float): The number of GPUs to reserve for each
-                worker. Fractional values are allowed. Defaults to 0.
-            train_cls (Optional[Type]): The training class that each worker
-                should be instantiated as.
-            callbacks (Optional[List[Callback]]): A list of Callbacks which
-                will be executed during training. If this is not set,
-                currently there are NO default Callbacks.
+            use_gpu (bool): If True, training will be done on GPUs.
+                Defaults to False.
+            resources_per_worker (Optional[Dict]): If specified, the resources
+                defined in the Dict will be reserved for each worker.
         """
         pass
 
     def start(self,
               initialization_hook: Optional[Callable[[], None]] = None,
+              train_cls: Optional[S] = None,
               *args,
               **kwargs):
         """Starts the training execution service.
@@ -45,19 +38,25 @@ class Trainer:
         Args:
             initialization_hook (Optional[Callable]): The function to call on
                 each worker when it instantiated.
+            train_cls (Optional[cls]): The training class that each worker
+                should be instantiated as.
             args, kwargs: The arguments to pass into ``train_cls.__init__``.
         """
         pass
 
     def run(self,
             train_func: Callable[[Dict[str, Any]], T],
-            config: Optional[Dict[str, Any]] = None) -> List[T]:
+            config: Optional[Dict[str, Any]] = None,
+            callbacks: Optional[List[Callback]] = None) -> List[T]:
         """Runs a training function in a distributed manner.
 
         Args:
             train_func (Callable): The training function to execute.
             config (Optional[Dict]): Configurations to pass into
                 ``train_func``. If None then an empty Dict will be created.
+            callbacks (Optional[List[Callback]]): A list of Callbacks which
+                will be executed during training. If this is not set,
+                currently there are NO default Callbacks.
         Returns:
             A list of results from the training function. Each value in the
             list corresponds to the value returned by one call of the training
