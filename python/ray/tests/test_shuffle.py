eee361da417ca2e9d53c239ea70c2e2f2d74f99e
SangBin Cho <rkooo567@gmail.com>
2021-07-30 10:45:50 -0700

    Fix issues.

--- python/ray/tests/test_shuffle.py
+++ python/ray/tests/test_shuffle.py
@@ -12,6 +12,33 @@ def test_shuffle():
         ray.shutdown()
 
 
+def test_shuffle_no_streaming():
+    try:
+        shuffle.main(no_streaming=True)
+    finally:
+        ray.shutdown()
+
+
+def test_shuffle_multi_node(ray_start_cluster):
+    cluster = ray_start_cluster
+    for _ in range(4):
+        cluster.add_node(num_cpus=2, object_store_memory=1e9)
+
+    shuffle.main(ray_address="auto", num_partitions=200, partition_size=10e6)
+
+
+def test_shuffle_multi_node_no_streaming(ray_start_cluster):
+    cluster = ray_start_cluster
+    for _ in range(4):
+        cluster.add_node(num_cpus=2, object_store_memory=1e9)
+
+    shuffle.main(
+        ray_address="auto",
+        num_partitions=200,
+        partition_size=10e6,
+        no_streaming=True)
+
+
 # https://github.com/ray-project/ray/pull/16408
 @pytest.mark.skipif(sys.platform == "win32", reason="Failing on Windows.")
 def test_shuffle_hang():
