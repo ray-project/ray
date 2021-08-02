5057df2be53cabd75d84b02df541cab8e8c6cf42
chenk008 <kongchen28@gmail.com>
2021-08-02 22:32:39 +0800

    add log

--- golang/pkg/driver/test.go
+++ golang/pkg/driver/test.go
@@ -3,12 +3,15 @@ package main
 import (
     "github.com/ray-project/ray-go-worker/pkg/actor"
     "github.com/ray-project/ray-go-worker/pkg/ray"
+    "github.com/ray-project/ray-go-worker/pkg/util"
 )
 import "fmt"
 
 func main() {
     ray.Init("127.0.0.1:6379", "5241590000000000")
+    util.Logger.Infof("finish init")
     actor_ref := ray.Actor((*actor.Count)(nil)).Remote()
+    util.Logger.Infof("created actor ref")
     actor_ref.Task("Get")
     fmt.Println("ok!")
 }
