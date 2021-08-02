c8e374f4b438a50891dc0a545d0250bcb8ac7502
Eric Liang <ekhliang@gmail.com>
2021-08-02 14:48:22 -0700

    wip

---
+++ python/ray/experimental/data/impl/remote_fn.py
@@ -0,0 +1,13 @@
+from typing import Any
+
+import ray
+
+CACHED_FUNCTIONS = {}
+
+def cached_remote_fn(fn: Any, **ray_remote_args) -> Any:
+    if fn not in CACHED_FUNCTIONS:
+        if ray_remote_args:
+            CACHED_FUNCTIONS[fn] = ray.remote(**ray_remote_args)(fn)
+        else:
+            CACHED_FUNCTIONS[fn] = ray.remote(fn)
+    return CACHED_FUNCTIONS[fn]
