7a58f2169718d6bf8e48616a26f0ebc1f08eb1f8
Eric Liang <ekhliang@gmail.com>
2021-08-02 15:16:50 -0700

    update

--- python/ray/experimental/data/impl/remote_fn.py
+++ python/ray/experimental/data/impl/remote_fn.py
@@ -6,6 +6,12 @@ CACHED_FUNCTIONS = {}
 
 
 def cached_remote_fn(fn: Any, **ray_remote_args) -> Any:
+    """Lazily defines a ray.remote function.
+
+    This is used in Datasets to avoid circular import issues with ray.remote.
+    (ray imports ray.data in order to allow ``ray.data.read_foo()`` to work,
+    which means ray.remote cannot be used top-level in ray.data).
+    """
     if fn not in CACHED_FUNCTIONS:
         if ray_remote_args:
             CACHED_FUNCTIONS[fn] = ray.remote(**ray_remote_args)(fn)
