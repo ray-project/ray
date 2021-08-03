f9552765cbb89453173d6bc7a07bbabe87e6de0f
Eric Liang <ekhliang@gmail.com>
2021-08-02 18:15:25 -0700

    Avoid re-exporting same function repeatedly in dataset (#17522)

--- python/ray/experimental/data/impl/sort.py
+++ python/ray/experimental/data/impl/sort.py
@@ -23,6 +23,7 @@ import ray
 from ray.experimental.data.block import Block, BlockAccessor
 from ray.experimental.data.impl.block_list import BlockList
 from ray.experimental.data.impl.progress_bar import ProgressBar
+from ray.experimental.data.impl.remote_fn import cached_remote_fn
 
 T = TypeVar("T")
 
@@ -40,11 +41,11 @@ def sample_boundaries(blocks: BlockList[T], key: SortKeyT,
     """
     n_samples = int(num_reducers * 10 / len(blocks))
 
-    @ray.remote
-    def sample_block(block: Block[T]) -> np.ndarray:
-        return BlockAccessor.for_block(block).sample(n_samples, key)
+    sample_block = cached_remote_fn(_sample_block)
 
-    sample_results = [sample_block.remote(block) for block in blocks]
+    sample_results = [
+        sample_block.remote(block, n_samples, key) for block in blocks
+    ]
     sample_bar = ProgressBar("Sort Sample", len(sample_results))
     sample_bar.block_until_complete(sample_results)
     sample_bar.close()
@@ -76,28 +77,22 @@ def sort_impl(blocks: BlockList[T], key: SortKeyT,
     if descending:
         boundaries.reverse()
 
-    @ray.remote(num_returns=num_reducers)
-    def sort_block(block, boundaries):
-        return BlockAccessor.for_block(block).sort_and_partition(
-            boundaries, key, descending)
-
-    @ray.remote(num_returns=2)
-    def merge_sorted_blocks(*blocks: List[Block[T]]) -> Block[T]:
-        if len(blocks) == 1:
-            blocks = blocks[0]  # Python weirdness
-        return BlockAccessor.for_block(blocks[0]).merge_sorted_blocks(
-            list(blocks), key, descending)
+    sort_block = cached_remote_fn(_sort_block).options(
+        num_returns=num_reducers)
+    merge_sorted_blocks = cached_remote_fn(_merge_sorted_blocks, num_returns=2)
 
     map_results = np.empty((num_mappers, num_reducers), dtype=object)
     for i, block in enumerate(blocks):
-        map_results[i, :] = sort_block.remote(block, boundaries)
+        map_results[i, :] = sort_block.remote(block, boundaries, key,
+                                              descending)
     map_bar = ProgressBar("Sort Map", len(map_results))
     map_bar.block_until_complete([ret[0] for ret in map_results])
     map_bar.close()
 
     reduce_results = []
     for j in range(num_reducers):
-        ret = merge_sorted_blocks.remote(*map_results[:, j].tolist())
+        ret = merge_sorted_blocks.remote(key, descending,
+                                         *map_results[:, j].tolist())
         reduce_results.append(ret)
     merge_bar = ProgressBar("Sort Merge", len(reduce_results))
     merge_bar.block_until_complete([ret[0] for ret in reduce_results])
@@ -106,3 +101,20 @@ def sort_impl(blocks: BlockList[T], key: SortKeyT,
     blocks = [b for b, _ in reduce_results]
     metadata = ray.get([m for _, m in reduce_results])
     return BlockList(blocks, metadata)
+
+
+def _sample_block(block: Block[T], n_samples: int,
+                  key: SortKeyT) -> np.ndarray:
+    return BlockAccessor.for_block(block).sample(n_samples, key)
+
+
+def _sort_block(block, boundaries, key, descending):
+    return BlockAccessor.for_block(block).sort_and_partition(
+        boundaries, key, descending)
+
+
+def _merge_sorted_blocks(key, descending, *blocks: List[Block[T]]) -> Block[T]:
+    if len(blocks) == 1:
+        blocks = blocks[0]  # Python weirdness
+    return BlockAccessor.for_block(blocks[0]).merge_sorted_blocks(
+        list(blocks), key, descending)
