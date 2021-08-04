6db63990afaff91f0ae19d7bee90c8606d43982c
Eric Liang <ekhliang@gmail.com>
2021-08-04 16:09:45 -0700

    Don't capture child tasks in placement groups by default (#17527)

--- python/ray/data/examples/per_epoch_shuffle.py
+++ python/ray/data/examples/per_epoch_shuffle.py
@@ -1,6 +1,7 @@
 import ray
+import numpy as np
 
-ray.init()
+ray.init(num_cpus=8, object_store_memory=4e9)
 
 
 @ray.remote
@@ -10,9 +11,22 @@ def consume(name, split):
         i += 1
 
 
-pipeline = ray.data.range(100000).repeat(100).random_shuffle()
-a, b = pipeline.split(2)
+def gen(i):
+    return {
+        "a": 4.0,
+        "b": "hello" * 1000,
+        "c": np.zeros(25000),
+    }
+
+
+ds = ray.data.range(10000).map(gen)
+print(ds, ds.size_bytes())
+pipeline = ds.repeat(100).random_shuffle()
+print(pipeline)
+a, b, c, d, e = pipeline.split(5, equal=True)
 x1 = consume.remote("consumer A", a)
 x2 = consume.remote("consumer B", b)
-ray.get(x1)
-ray.get(x2)
+x3 = consume.remote("consumer C", c)
+x4 = consume.remote("consumer D", d)
+x5 = consume.remote("consumer E", e)
+ray.get([x1, x2, x3, x4, x5])
