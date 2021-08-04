9227c48e19ca577a8b5634dfa8d2693432de990d
chenk008 <kongchen28@gmail.com>
2021-08-04 04:03:36 +0000

    fix

--- golang/pkg/ray/task_executor.go
+++ golang/pkg/ray/task_executor.go
@@ -4,6 +4,7 @@ import "C"
 import (
     "fmt"
     "reflect"
+    "unsafe"
 
     "github.com/ray-project/ray-go-worker/pkg/generated"
     "github.com/ray-project/ray-go-worker/pkg/util"
@@ -12,9 +13,11 @@ import (
 var actor interface{}
 
 //export go_worker_execute
-func go_worker_execute(taskType int, rayFunctionInfo []string, args []C.struct_DataBuffer) {
+func go_worker_execute(taskType int, rayFunctionInfo []*C.char, args []C.struct_DataBuffer) {
     if taskType == int(generated.TaskType_ACTOR_CREATION_TASK) {
-        go_type_name := rayFunctionInfo[0]
+        d := (*reflect.SliceHeader)(unsafe.Pointer(&rayFunctionInfo))
+	util.Logger.Debugf("slice info:%v",*d)
+        go_type_name := C.GoString((*C.char)(unsafe.Pointer(rayFunctionInfo[0])))
         go_type, ok := typesMap[go_type_name]
         if !ok {
             panic(fmt.Errorf("type not found:%s", go_type_name))
