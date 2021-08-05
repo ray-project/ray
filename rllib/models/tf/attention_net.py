d9bca63dc342196d0f54e35a86a139d1686c09f1
Sumanth Ratna <sumanthratna@gmail.com>
2021-08-05 15:12:49 -0400

    Fix `step` kwarg in `AttentionWrapper.forward`

--- rllib/models/tf/attention_net.py
+++ rllib/models/tf/attention_net.py
@@ -452,9 +452,8 @@ class AttentionWrapper(TFModelV2):
                         one_hot(input_dict[SampleBatch.PREV_ACTIONS][:, i],
                                 self.action_space))
             elif isinstance(self.action_space, MultiDiscrete):
-                for i in range(
-                        self.use_n_prev_actions,
-                        step=self.action_space.shape[0]):
+                for i in range(0, self.use_n_prev_actions,
+                               self.action_space.shape[0]):
                     prev_a_r.append(
                         one_hot(
                             tf.cast(
@@ -804,7 +803,7 @@ class Keras_AttentionWrapper(tf.keras.Model if tf else object):
                                 self.action_space))
             elif isinstance(self.action_space, MultiDiscrete):
                 for i in range(0, self.use_n_prev_actions,
-                              self.action_space.shape[0]):
+                               self.action_space.shape[0]):
                     prev_a_r.append(
                         one_hot(
                             tf.cast(
