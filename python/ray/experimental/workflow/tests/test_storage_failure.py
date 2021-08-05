4bb0c6c5b27f84c005bb08e484a8693813f80df2
Siyuan <suquark@gmail.com>
2021-08-05 11:41:05 -0700

    update

--- python/ray/experimental/workflow/tests/test_storage_failure.py
+++ python/ray/experimental/workflow/tests/test_storage_failure.py
@@ -3,5 +3,11 @@ from ray.experimental.workflow.storage.filesystem import FilesystemStorageImpl
 
 
 class LoggedFileSystemStorage(FilesystemStorageImpl):
+    def __init__(self, workflow_root_dir: str):
+        super().__init__(workflow_root_dir)
+        self._log_dir = self._workflow_root_dir / "log"
+        if not self._log_dir.exists():
+            self._log_dir.mkdir()
+
     async def put(self, key: str, data: Any, is_json: bool = False) -> None:
         await super().put(key, data, is_json)
