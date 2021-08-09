7a9274afb3883db4359249a4ea56ffeaa0269afd
SangBin Cho <rkooo567@gmail.com>
2021-08-08 22:46:21 -0700

    Fix

--- src/ray/gcs/gcs_client/test/service_based_gcs_client_test.cc
+++ src/ray/gcs/gcs_client/test/service_based_gcs_client_test.cc
@@ -1441,7 +1441,8 @@ int main(int argc, char **argv) {
   InitShutdownRAII ray_log_shutdown_raii(ray::RayLog::StartRayLog,
                                          ray::RayLog::ShutDownRayLog, argv[0],
                                          ray::RayLogLevel::INFO,
-                                         /*log_dir=*/"");
+                                         /*log_dir=*/"",
+                                         /*emit_console_log_on_fatal*/ false);
   ::testing::InitGoogleTest(&argc, argv);
   RAY_CHECK(argc == 3);
   ray::TEST_REDIS_SERVER_EXEC_PATH = argv[1];
