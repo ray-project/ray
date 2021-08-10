6b8bd49fe2b5f6e0b53d5deefa78be6af764d277
chenk008 <kongchen28@gmail.com>
2021-08-10 09:51:40 +0800

    sleep in ci

--- python/ray/util/sgd/tests/test_torch_failure.py
+++ python/ray/util/sgd/tests/test_torch_failure.py
@@ -101,8 +101,8 @@ def test_resize(ray_start_2_cpus, use_local):  # noqa: F811
                 return 1
 
         dummy_handler = DummyActor.remote()
-        time.sleep(1)
         trainer1.train(max_retries=1)
+        time.sleep(1)
         assert trainer1.worker_group.num_workers == 1
         assert trainer1._num_failures == 1
 
