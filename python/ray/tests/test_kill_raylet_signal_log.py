272743781fb357a2547b8492ba449c66112e1553
SangBin Cho <rkooo567@gmail.com>
2021-08-10 10:10:25 -0700

    Done

--- python/ray/tests/test_kill_raylet_signal_log.py
+++ python/ray/tests/test_kill_raylet_signal_log.py
@@ -45,6 +45,7 @@ def test_kill_raylet_signal_log(shutdown_only):
 
 
 @pytest.mark.skipif(sys.platform != "win32", reason="Only run on Windows.")
+@pytest.mark.skip(reason="Flaky on Windows")
 def test_kill_raylet_signal_log_win(shutdown_only):
     check_result("{}/logs/raylet.out", signal.CTRL_BREAK_EVENT, "SIGTERM")
 
