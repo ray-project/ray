89216fcd92a350e87ad623be2303e53326fa3cb1
chenk008 <kongchen28@gmail.com>
2021-08-10 15:42:04 +0800

    fix

--- python/ray/util/sgd/torch/worker_group.py
+++ python/ray/util/sgd/torch/worker_group.py
@@ -411,6 +411,24 @@ class RemoteWorkerGroup(WorkerGroupInterface):
             logger.warning("Failed to shutdown gracefully.")
             return False
 
+    def wait_for_condition(condition_predictor, timeout=10, retry_interval_ms=100):
+        """Wait until a condition is met or time out with an exception.
+
+        Args:
+            condition_predictor: A function that predicts the condition.
+            timeout: Maximum timeout in seconds.
+            retry_interval_ms: Retry interval in milliseconds.
+
+        Raises:
+            RuntimeError: If the condition is not met before the timeout expires.
+        """
+        start = time.time()
+        while time.time() - start <= timeout:
+            if condition_predictor():
+                return
+            time.sleep(retry_interval_ms / 1000.0)
+        raise RuntimeError("The condition wasn't met before the timeout expired.")
+
     def shutdown(self, force=False):
         print("shutdown:{}".format(force))
         force_kill = force
@@ -428,31 +446,12 @@ class RemoteWorkerGroup(WorkerGroupInterface):
             self._worker_placement_group = None
 
             def is_placement_group_removed():
-                table = ray.util.placement_group_table(placement_group)
+                table = ray.util.placement_group_table(self._worker_placement_group)
                 if "state" not in table:
                     return False
                 return table["state"] == "REMOVED"
 
-            wait_for_condition(is_placement_group_removed)
-
-
-    def wait_for_condition(condition_predictor, timeout=10, retry_interval_ms=100):
-        """Wait until a condition is met or time out with an exception.
- 
-        Args:
-            condition_predictor: A function that predicts the condition.
-            timeout: Maximum timeout in seconds.
-            retry_interval_ms: Retry interval in milliseconds.
-
-        Raises:
-            RuntimeError: If the condition is not met before the timeout expires.
-        """
-        start = time.time()
-        while time.time() - start <= timeout:
-            if condition_predictor():
-                return
-            time.sleep(retry_interval_ms / 1000.0)
-        raise RuntimeError("The condition wasn't met before the timeout expired.")
+            self.wait_for_condition(is_placement_group_removed)
 
     def reset(self):
         self.shutdown(force=True)
