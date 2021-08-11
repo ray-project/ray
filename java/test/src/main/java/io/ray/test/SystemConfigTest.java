2e9cb02c707e4d4c3ac1d186cdd828ae6e80eb96
Qing Wang <kingchin1218@gmail.com>
2021-08-11 10:36:44 +0800

    Fix Java cases.

--- java/test/src/main/java/io/ray/test/SystemConfigTest.java
+++ java/test/src/main/java/io/ray/test/SystemConfigTest.java
@@ -10,7 +10,5 @@ public class SystemConfigTest extends BaseTest {
   public void testDefaultConfigs() {
     long ret = ((Double) SystemConfig.get("max_direct_call_object_size")).longValue();
     Assert.assertEquals(ret, 100 * 1024);
-    boolean ret2 = (boolean) SystemConfig.get("worker_process_watchdog_enabled");
-    Assert.assertTrue(ret2);
   }
 }
