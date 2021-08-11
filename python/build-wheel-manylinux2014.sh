6b1b01b3005eb49db282aeb924b64fcfafc2106c
SangBin Cho <rkooo567@gmail.com>
2021-08-10 17:35:04 -0700

    Try preparing more deps.

--- python/build-wheel-manylinux2014.sh
+++ python/build-wheel-manylinux2014.sh
@@ -25,10 +25,9 @@ NUMPY_VERSIONS=("1.14.5"
 yum -y install unzip zip sudo
 yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel xz
 yum -y install openssl
-# if [ "${RAY_DEBUG_BUILD}" = "asan" ]; then
-# Only used for ASAN build.
+yum install libasan-4.8.5-44.el7.x86_64 -y
 yum install devtoolset-9-libasan-devel.x86_64 -y
-# fi
+
 
 java -version
 java_bin=$(readlink -f "$(command -v java)")
