e19e681ccecb096a52f5dd61a9988fd54e815c16
qicosmos <qicosmos@163.com>
2021-08-12 22:53:59 +0800

    fix

--- cpp/example/run.sh
+++ cpp/example/run.sh
@@ -1,6 +1,6 @@
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 bazel build //:example
-if mac; then
+if [[ "$OSTYPR" == "darwin"* ]]; then
     DYLD_LIBRARY_PATH="$ROOT_DIR/thirdparty/lib" $ROOT_DIR/bazel-bin/example
 else
     bazel run //:example
