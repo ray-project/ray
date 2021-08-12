1e4ee7c6a93f8d4d323485e60770540b746eeb56
qicosmos <qicosmos@163.com>
2021-08-13 00:41:18 +0800

    fix typo

--- cpp/example/run.sh
+++ cpp/example/run.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
+
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
 bazel build //:example
-if [[ "$OSTYPR" == "darwin"* ]]; then
+if [[ "$OSTYPE" == "darwin"* ]]; then
     DYLD_LIBRARY_PATH="$ROOT_DIR/thirdparty/lib" $ROOT_DIR/bazel-bin/example
 else
     LD_LIBRARY_PATH="$ROOT_DIR/thirdparty/lib" $ROOT_DIR/bazel-bin/example
