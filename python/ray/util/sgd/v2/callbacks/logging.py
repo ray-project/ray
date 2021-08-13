874cac2186bf66a0aa6651c15d143f72b0262e74
Antoni Baum <antoni.baum@protonmail.com>
2021-08-13 18:34:59 +0000

    TBXLoggerCallback

--- python/ray/util/sgd/v2/callbacks/logging.py
+++ python/ray/util/sgd/v2/callbacks/logging.py
@@ -1,12 +1,19 @@
-from typing import Iterable, List, Optional, Dict, Union
+from typing import Iterable, List, Optional, Dict, Set, Tuple, Union
 import abc
-
+import warnings
+import logging
+import numpy as np
 import json
 from pathlib import Path
 
+from ray.util.debug import log_once
+from ray.util.ml_utils.dict import flatten_dict
 from ray.util.ml_utils.json import SafeFallbackEncoder
 from ray.util.sgd.v2.callbacks import SGDCallback
-from ray.util.sgd.v2.constants import RESULT_FILE_JSON
+from ray.util.sgd.v2.constants import (RESULT_FILE_JSON, TRAINING_ITERATION,
+                                       TIME_TOTAL_S, TIMESTAMP, PID)
+
+logger = logging.getLogger(__name__)
 
 
 class SGDSingleFileLoggingCallback(SGDCallback, metaclass=abc.ABCMeta):
@@ -17,7 +24,8 @@ class SGDSingleFileLoggingCallback(SGDCallback, metaclass=abc.ABCMeta):
             should be. If None, will be set by the Trainer.
         filename (Optional[str]): Filename in logdir to save results to.
         workers_to_log (int|List[int]|None): Worker indices to log.
-            If None, will log all workers.
+            If None, will log all workers. By default, will log the
+            worker with index 0.
     """
 
     # Defining it like this ensures it will be overwritten
@@ -30,7 +38,28 @@ class SGDSingleFileLoggingCallback(SGDCallback, metaclass=abc.ABCMeta):
                  workers_to_log: Optional[Union[int, List[int]]] = 0) -> None:
         self._logdir = logdir
         self._filename = filename
-        self._workers_to_log = workers_to_log
+        self._workers_to_log = self._validate_workers_to_log(workers_to_log)
+
+    def _validate_workers_to_log(self, workers_to_log) -> List[int]:
+        if isinstance(workers_to_log, int):
+            workers_to_log = [workers_to_log]
+
+        if workers_to_log is not None:
+            if not isinstance(workers_to_log, Iterable):
+                raise TypeError("workers_to_log must be an Iterable, got "
+                                f"{type(workers_to_log)}.")
+            if not all(isinstance(worker, int) for worker in workers_to_log):
+                raise TypeError(
+                    "All elements of workers_to_log must be integers.")
+            if len(workers_to_log) < 1:
+                raise ValueError(
+                    "At least one worker must be specified in workers_to_log.")
+        return workers_to_log
+
+    def _create_log_path(self, logdir_path: Path, filename: Path) -> Path:
+        if not filename:
+            raise ValueError("filename cannot be None or empty.")
+        return logdir_path.joinpath(Path(filename))
 
     def start_training(self, logdir: str, **info):
         if self._logdir:
@@ -44,19 +73,7 @@ class SGDSingleFileLoggingCallback(SGDCallback, metaclass=abc.ABCMeta):
         if not self._filename:
             filename = self._default_filename
 
-        self._log_path = logdir_path.joinpath(Path(filename))
-        if isinstance(self._workers_to_log, int):
-            workers_to_log = [self._workers_to_log]
-
-        if workers_to_log is not None:
-            if not isinstance(workers_to_log, Iterable):
-                raise TypeError("workers_to_log must be an Iterable, got "
-                                f"{type(workers_to_log)}.")
-            if not all(isinstance(worker, int) for worker in workers_to_log):
-                raise TypeError(
-                    "All elements of workers_to_log must be integers.")
-
-        self._workers_to_log = workers_to_log
+        self._log_path = self._create_log_path(logdir_path, filename)
 
     @property
     def log_path(self) -> Path:
@@ -72,7 +89,8 @@ class JsonLoggerCallback(SGDSingleFileLoggingCallback):
             should be. If None, will be set by the Trainer.
         filename (Optional[str]): Filename in logdir to save results to.
         workers_to_log (int|List[int]|None): Worker indices to log.
-            If None, will log all workers.
+            If None, will log all workers. By default, will log the
+            worker with index 0.
     """
 
     _default_filename: Union[str, Path] = RESULT_FILE_JSON
@@ -97,3 +115,129 @@ class JsonLoggerCallback(SGDSingleFileLoggingCallback):
             f.seek(0)
             json.dump(
                 loaded_results + [results_to_log], f, cls=SafeFallbackEncoder)
+
+
+class SGDSingleFileSingleWorkerLoggingCallback(
+        SGDSingleFileLoggingCallback, metaclass=abc.ABCMeta):
+    """Abstract SGD logging callback class.
+
+    Allows only for single-worker logging.
+
+    Args:
+        logdir (Optional[str]): Path to directory where the results file
+            should be. If None, will be set by the Trainer.
+        filename (Optional[str]): Filename in logdir to save results to.
+        workers_to_log (int): Worker index to log. By default, will log the
+            worker with index 0.
+    """
+
+    # it's still workers_to_log, not worker, for easier switching
+    # this is defined again for different type hints
+    def __init__(self,
+                 logdir: Optional[str] = None,
+                 filename: Optional[str] = None,
+                 workers_to_log: int = 0) -> None:
+        return super().__init__(
+            logdir=logdir, filename=filename, workers_to_log=workers_to_log)
+
+    def _validate_workers_to_log(self, workers_to_log) -> int:
+        if isinstance(workers_to_log, Iterable):
+            workers_to_log = list(workers_to_log)
+            if len(workers_to_log) > 1:
+                raise ValueError(
+                    f"{self.__class__.__name__} only supports logging "
+                    "from a single worker.")
+            elif len(workers_to_log) < 1:
+                raise ValueError(
+                    "At least one worker must be specified in workers_to_log.")
+            workers_to_log = workers_to_log[0]
+        if not isinstance(workers_to_log, int):
+            raise TypeError("workers_to_log must be an integer.")
+        return workers_to_log
+
+
+class TBXLoggerCallback(SGDSingleFileSingleWorkerLoggingCallback):
+    """Logs SGD results in TensorboardX format.
+
+    Args:
+        logdir (Optional[str]): Path to directory where the results file
+            should be. If None, will be set by the Trainer.
+        filename (Optional[str]): Ignored.
+        workers_to_log (int): Worker index to log. By default, will log the
+            worker with index 0.
+    """
+
+    _default_filename: Union[str, Path] = None
+    VALID_SUMMARY_TYPES: Tuple[type] = (int, float, np.float32, np.float64,
+                                        np.int32, np.int64)
+    IGNORE_KEYS: Set[str] = {PID, TIMESTAMP, TIME_TOTAL_S, TRAINING_ITERATION}
+
+    def __init__(self,
+                 logdir: Optional[str] = None,
+                 filename: Optional[str] = None,
+                 workers_to_log: int = 0) -> None:
+        if filename:
+            warnings.warn("filename argument is ignored by TBXLoggerCallback."
+                          "Set filename to None to avoid this warning.")
+
+        super().__init__(
+            logdir=logdir, filename=filename, workers_to_log=workers_to_log)
+
+    def _create_log_path(self, logdir_path: Path, filename: Path) -> Path:
+        return logdir_path
+
+    def start_training(self, logdir: str, **info):
+        super().start_training(logdir, **info)
+
+        try:
+            from tensorboardX import SummaryWriter
+        except ImportError:
+            if log_once("tbx-install"):
+                warnings.warn(
+                    "pip install 'tensorboardX' to see TensorBoard files.")
+            raise
+
+        self._file_writer = SummaryWriter(self.log_path, flush_secs=30)
+
+    def handle_result(self, results: Optional[List[Dict]], **info):
+        result = results[self._workers_to_log]
+        step = result[TRAINING_ITERATION]
+        result = {k: v for k, v in result.items() if k not in self.IGNORE_KEYS}
+        flat_result = flatten_dict(result, delimiter="/")
+        path = ["ray", "sgd"]
+        valid_result = {}
+
+        # same logic as in ray.tune.logger.TBXLogger
+        for attr, value in flat_result.items():
+            full_attr = "/".join(path + [attr])
+            if (isinstance(value, self.VALID_SUMMARY_TYPES)
+                    and not np.isnan(value)):
+                valid_result[full_attr] = value
+                self._file_writer.add_scalar(
+                    full_attr, value, global_step=step)
+            elif ((isinstance(value, list) and len(value) > 0)
+                  or (isinstance(value, np.ndarray) and value.size > 0)):
+                valid_result[full_attr] = value
+
+                # Must be video
+                if isinstance(value, np.ndarray) and value.ndim == 5:
+                    self._file_writer.add_video(
+                        full_attr, value, global_step=step, fps=20)
+                    continue
+
+                try:
+                    self._file_writer.add_histogram(
+                        full_attr, value, global_step=step)
+                # In case TensorboardX still doesn't think it's a valid value
+                # (e.g. `[[]]`), warn and move on.
+                except (ValueError, TypeError):
+                    if log_once("invalid_tbx_value"):
+                        warnings.warn(
+                            "You are trying to log an invalid value ({}={}) "
+                            "via {}!".format(full_attr, value,
+                                             type(self).__name__))
+
+        self._file_writer.flush()
+
+    def finish_training(self, error: bool = False, **info):
+        self._file_writer.close()
