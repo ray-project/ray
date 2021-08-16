95b519aa6bd20a94e6c640eac119aa86cacb1e87
qicosmos <qicosmos@163.com>
2021-08-16 13:05:58 +0800

    fix

--- cpp/example/run.sh
+++ cpp/example/run.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")" || exit; pwd)"
-
 bazel --nosystem_rc --nohome_rc build //:example
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    sudo install_name_tool -change bazel-out/darwin-opt/bin/cpp/libray_api.so ./thirdparty/lib/libray_api.so ./bazel-bin/example.so
-    sudo install_name_tool -change bazel-out/darwin-opt/bin/cpp/libray_api.so ./thirdparty/lib/libray_api.so ./bazel-bin/example
+    install_name_tool -change bazel-out/darwin-opt/bin/cpp/libray_api.so ./thirdparty/lib/libray_api.so ./bazel-bin/example.so
+    install_name_tool -change bazel-out/darwin-opt/bin/cpp/libray_api.so ./thirdparty/lib/libray_api.so ./bazel-bin/example
     ./bazel-bin/example
 else
     ./bazel-bin/example
