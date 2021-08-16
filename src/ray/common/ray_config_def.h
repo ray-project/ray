1209a87ead53ac3a6d0124d6138a424485e213a6
Alex Wu <alex@anyscale.io>
2021-08-16 12:03:38 -0700

    [core] Remove push based resource report code path (#17825)

--- src/ray/common/ray_config_def.h
+++ src/ray/common/ray_config_def.h
@@ -241,9 +241,6 @@ RAY_CONFIG(int64_t, gcs_dump_debug_log_interval_minutes, 1)
 RAY_CONFIG(int, gcs_resource_report_poll_period_ms, 100)
 // The number of concurrent polls to polls to GCS.
 RAY_CONFIG(uint64_t, gcs_max_concurrent_resource_pulls, 100)
-// Feature flag to turn on resource report polling. Polling and raylet pushing are
-// mutually exlusive.
-RAY_CONFIG(bool, pull_based_resource_reporting, true)
 // Feature flag to use grpc instead of redis for resource broadcast.
 // TODO(ekl) broken as of https://github.com/ray-project/ray/issues/16858
 RAY_CONFIG(bool, grpc_based_resource_broadcast, false)
