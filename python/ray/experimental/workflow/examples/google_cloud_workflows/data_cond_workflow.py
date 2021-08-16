f3545b9773d37ec647fc294f9a3a08a12f4f73a6
Eric Liang <ekhliang@gmail.com>
2021-08-16 14:09:54 -0700

    add

---
+++ python/ray/experimental/workflow/examples/google_cloud_workflows/data_cond_workflow.py
@@ -0,0 +1,36 @@
+from ray import workflow
+
+
+@workflow.step
+def make_request() -> int:
+    return int(requests.get("https://www.example.com/callA").text)
+
+
+@workflow.step
+def small() -> str:
+    return requests.get("https://www.example.com/SmallFunc").text
+
+
+@workflow.step
+def medium() -> str:
+    return requests.get("https://www.example.com/MediumFunc").text
+
+
+@workflow.step
+def large() -> str:
+    return requests.get("https://www.example.com/LargeFunc").text
+
+
+@workflow.step
+def decide(result: int) -> str:
+    if result < 10:
+        return small.step(result)
+    elif result < 100:
+        return medium.step(result)
+    else:
+        return large.step(result)
+
+
+if __name__ == "__main__":
+    workflow.init()
+    print(decide.step(make_request.step()).run())
