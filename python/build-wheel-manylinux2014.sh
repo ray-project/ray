ccf7da8c7cfd988d30dadfa7381c98afaeee4663
SangBin Cho <rkooo567@gmail.com>
2021-08-16 11:51:46 -0700

    Fix issues

--- python/build-wheel-manylinux2014.sh
+++ python/build-wheel-manylinux2014.sh
@@ -93,10 +93,8 @@ for ((i=0; i<${#PYTHONS[@]}; ++i)); do
     PATH=/opt/python/${PYTHON}/bin:/root/bazel-3.2.0/output:$PATH \
     /opt/python/"${PYTHON}"/bin/python setup.py bdist_wheel
     # build ray-cpp wheel
-    if [ "${RAY_DEBUG_BUILD}" != "asan" ]; then
-      PATH=/opt/python/${PYTHON}/bin:/root/bazel-3.2.0/output:$PATH \
-      RAY_INSTALL_CPP=1 /opt/python/"${PYTHON}"/bin/python setup.py bdist_wheel
-    fi
+    PATH=/opt/python/${PYTHON}/bin:/root/bazel-3.2.0/output:$PATH \
+    RAY_INSTALL_CPP=1 /opt/python/"${PYTHON}"/bin/python setup.py bdist_wheel
     # In the future, run auditwheel here.
     mv dist/*.whl ../.whl/
   popd
