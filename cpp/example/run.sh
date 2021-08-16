4bbb5476b2eea63b131b1e3b9030c301723eb4ca
qicosmos <qicosmos@163.com>
2021-08-16 11:41:08 +0800

    change so path

--- cpp/example/run.sh
+++ cpp/example/run.sh
@@ -4,7 +4,9 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")" || exit; pwd)"
 
 bazel --nosystem_rc --nohome_rc build //:example
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    DYLD_LIBRARY_PATH="$ROOT_DIR/thirdparty/lib" "${ROOT_DIR}"/bazel-bin/example
+    sudo install_name_tool -change bazel-out/darwin-opt/bin/cpp/libray_api.so ./thirdparty/lib/libray_api.so ./bazel-bin/example.so
+    sudo install_name_tool -change bazel-out/darwin-opt/bin/cpp/libray_api.so ./thirdparty/lib/libray_api.so ./bazel-bin/example
+    ./bazel-bin/example
 else
-    LD_LIBRARY_PATH="$ROOT_DIR/thirdparty/lib" "${ROOT_DIR}"/bazel-bin/example
+    ./bazel-bin/example
 fi
