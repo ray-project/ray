8d1291d8bdd2a5fb8fe12d07c4e3e40cc80d4aed
Eric Liang <ekhliang@gmail.com>
2021-08-16 13:19:08 -0700

    wip

---
+++ python/ray/experimental/workflow/examples/comparisons/argo/hello_world_workflow.py
@@ -0,0 +1,15 @@
+import sys
+
+import ray
+from ray import workflow
+
+
+@workflow.step(runtime_env={"pip": ["whalesay"]})
+def hello(msg: str) -> None:
+    import whalesay
+    whalesay.print(msg)
+
+
+if __name__ == "__main__":
+    workflow.init()
+    hello.step(sys.argv[1]).run()
