a18815571c5574afa3f2cea7057d5a67bcf7dbbc
Camille Potard <cpotard@gmail.com>
2021-08-17 17:53:35 -0700

    test launch buildkite

--- .buildkite/windows/install/main.ps1
+++ .buildkite/windows/install/main.ps1
@@ -1,4 +1,4 @@
-echo Main
+echo Mainprocess
 $Env:TOOLSET_JSON_PATH = "C:/Install/toolset/toolset-2019.json"
 echo $Env:TOOLSET_JSON_PATH
 Set-Service -Name wuauserv -StartupType Manual; Start-Service -Name wuauserv;
