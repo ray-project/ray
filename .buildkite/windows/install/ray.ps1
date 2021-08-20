8e0086fdfd3e4998c57a5e51f3527c964570a499
Camille Potard <cpotard@gmail.com>
2021-08-20 11:06:27 -0700

    cleanup

--- .buildkite/windows/install/ray.ps1
+++ .buildkite/windows/install/ray.ps1
@@ -4,13 +4,7 @@ echo started
 git clone --single-branch --branch windows-dockerfile-v3 -c core.symlinks=true https://github.com/ellimac54/ray.git
 cd ray
 bash -c "certutil -generateSSTFromWU roots.sst && certutil -addstore -f root roots.sst && rm roots.sst"
-echo shortname
-fsutil 8dot3name query
 bash -c ". ./ci/travis/ci.sh init"
 bash -c ". ./ci/travis/ci.sh build"
 bash -c ". ./ci/travis/ci.sh test_core"
-echo shortname
-fsutil 8dot3name query
 bash -c ". ./ci/travis/ci.sh test_python"
-
-#pip install -e . --verbose
