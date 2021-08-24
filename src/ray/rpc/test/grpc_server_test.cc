21a9ca306ac536d0e6c72f7138c32acf9ce9d63c
Lixin Wei <wlx65005@gmail.com>
2021-08-24 15:10:07 +0800

    stash

--- src/ray/rpc/test/grpc_server_test.cc
+++ src/ray/rpc/test/grpc_server_test.cc
@@ -1,6 +1,7 @@
 #include "ray/rpc/grpc_server.h"
 #include <chrono>
 #include "gtest/gtest.h"
+#include "ray/rpc/grpc_client.h"
 #include "src/ray/protobuf/gcs_service.grpc.pb.h"
 
 namespace ray {
@@ -20,9 +21,9 @@ class TestGrpcService : public GrpcService {
   /// Constructor.
   ///
   /// \param[in] handler The service handler that actually handle the requests.
-  explicit TestGrpcService(instrumented_io_context &io_service,
+  explicit TestGrpcService(instrumented_io_context &handler_io_service_,
                            TestServiceHandler &handler)
-      : GrpcService(io_service), service_handler_(handler){};
+      : GrpcService(handler_io_service_), service_handler_(handler){};
 
  protected:
   grpc::Service &GetGrpcService() override { return service_; }
@@ -44,39 +45,45 @@ class TestGrpcServerFixture : public ::testing::Test {
  public:
   void SetUp() {
     // Prepare and start test server.
-    thread = std::make_unique<std::thread>([this]() {
-      /// The asio work to keep io_service_ alive.
-      boost::asio::io_service::work io_service_work_(io_service);
-      io_service.run();
+    thread_ = std::make_unique<std::thread>([this]() {
+      /// The asio work to keep handler_io_service__ alive.
+      boost::asio::io_service::work handler_io_service__work_(
+          handler_io_service_);
+      handler_io_service_.run();
     });
-    test_service.reset(new TestGrpcService(io_service, test_service_handler));
-    grpc_server.reset(new GrpcServer("test", 123321));
-    grpc_server->RegisterService(*test_service);
-    grpc_server->Run();
+    test_service_.reset(new TestGrpcService(handler_io_service_, test_service_handler_));
+    grpc_server_.reset(new GrpcServer("test", 123321));
+    grpc_server_->RegisterService(*test_service_);
+    grpc_server_->Run();
 
     // Prepare a client
-    channel = grpc::CreateChannel("localhost:123321", grpc::InsecureChannelCredentials());
-    test_service_stub = TestService::NewStub(channel);
+    channel_ =
+        grpc::CreateChannel("localhost:123321", grpc::InsecureChannelCredentials());
+    test_service_stub_ = TestService::NewStub(channel_);
   }
 
   void TearDown() {
     // Cleanup stuffs.
-    grpc_server->Shutdown();
-    io_service.stop();
-    if (thread->joinable()) {
-      thread->join();
+    grpc_server_->Shutdown();
+    handler_io_service_.stop();
+    if (thread_->joinable()) {
+      thread_->join();
     }
   }
 
  protected:
-  TestServiceHandler test_service_handler;
-  instrumented_io_context io_service;
-  std::unique_ptr<std::thread> thread;
-  std::unique_ptr<TestGrpcService> test_service;
-  std::unique_ptr<GrpcServer> grpc_server;
-  grpc::CompletionQueue cq;
-  std::shared_ptr<grpc::Channel> channel;
-  std::unique_ptr<TestService::Stub> test_service_stub;
+  // Server
+  TestServiceHandler test_service_handler_;
+  instrumented_io_context handler_io_service_;
+  std::unique_ptr<std::thread> thread_;
+  std::unique_ptr<TestGrpcService> test_service_;
+  std::unique_ptr<GrpcServer> grpc_server_;
+  grpc::CompletionQueue cq_;
+  std::shared_ptr<grpc::Channel> channel_;
+  std::unique_ptr<TestService::Stub> test_service_stub_;
+  // Client
+  instrumented_io_context client_io_service_;
+  std::unique_ptr<ClientCallManager> client_call_manager_;
 };
 
 TEST_F(TestGrpcServerFixture, TestBasic) {
@@ -86,13 +93,32 @@ TEST_F(TestGrpcServerFixture, TestBasic) {
   grpc::ClientContext context;
   request.set_sleep_time_ms(1000);
   std::unique_ptr<grpc::ClientAsyncResponseReader<SleepReply>> rpc(
-      test_service_stub->AsyncSleep(&context, request, &cq));
+      test_service_stub_->AsyncSleep(&context, request, &cq_));
   grpc::Status status;
   rpc->Finish(&reply, &status, (void *)1);
   // Wait for reply
   void *got_tag;
   bool ok = false;
-  cq.Next(&got_tag, &ok);
+  cq_.Next(&got_tag, &ok);
+  if (ok && got_tag == (void *)1) {
+    RAY_LOG(INFO) << ok;
+  }
+}
+
+TEST_F(TestGrpcServerFixture, TestMaxActiveRpcs) {
+  // Send request
+  SleepRequest request;
+  SleepReply reply;
+  grpc::ClientContext context;
+  request.set_sleep_time_ms(1000);
+  std::unique_ptr<grpc::ClientAsyncResponseReader<SleepReply>> rpc(
+      test_service_stub_->AsyncSleep(&context, request, &cq_));
+  grpc::Status status;
+  rpc->Finish(&reply, &status, (void *)1);
+  // Wait for reply
+  void *got_tag;
+  bool ok = false;
+  cq_.Next(&got_tag, &ok);
   if (ok && got_tag == (void *)1) {
     RAY_LOG(INFO) << ok;
   }
