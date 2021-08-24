a64aac4f58cc00a2404cba7513028a8f0fcde196
Lixin Wei <wlx65005@gmail.com>
2021-08-24 16:12:39 +0800

    fix

--- src/ray/rpc/test/grpc_server_test.cc
+++ src/ray/rpc/test/grpc_server_test.cc
@@ -45,10 +45,9 @@ class TestGrpcServerFixture : public ::testing::Test {
  public:
   void SetUp() {
     // Prepare and start test server.
-    thread_ = std::make_unique<std::thread>([this]() {
-      /// The asio work to keep handler_io_service__ alive.
-      boost::asio::io_service::work handler_io_service__work_(
-          handler_io_service_);
+    handler_thread_ = std::make_unique<std::thread>([this]() {
+      /// The asio work to keep handler_io_service_ alive.
+      boost::asio::io_service::work handler_io_service_work_(handler_io_service_);
       handler_io_service_.run();
     });
     test_service_.reset(new TestGrpcService(handler_io_service_, test_service_handler_));
@@ -57,70 +56,60 @@ class TestGrpcServerFixture : public ::testing::Test {
     grpc_server_->Run();
 
     // Prepare a client
-    channel_ =
-        grpc::CreateChannel("localhost:123321", grpc::InsecureChannelCredentials());
-    test_service_stub_ = TestService::NewStub(channel_);
+    client_thread_ = std::make_unique<std::thread>([this]() {
+      /// The asio work to keep client_io_service_ alive.
+      boost::asio::io_service::work client_io_service_work_(client_io_service_);
+      client_io_service_.run();
+    });
+    client_call_manager_.reset(new ClientCallManager(client_io_service_));
+    grpc_client_.reset(
+        new GrpcClient<TestService>("localhost", 123321, *client_call_manager_));
   }
 
   void TearDown() {
     // Cleanup stuffs.
     grpc_server_->Shutdown();
     handler_io_service_.stop();
-    if (thread_->joinable()) {
-      thread_->join();
+    if (handler_thread_->joinable()) {
+      handler_thread_->join();
+    }
+    grpc_client_.reset();
+    client_call_manager_.reset();
+    client_io_service_.stop();
+    if (client_thread_->joinable()) {
+      client_thread_->join();
     }
   }
 
  protected:
+  VOID_RPC_CLIENT_METHOD(TestService, Sleep, grpc_client_, )
   // Server
   TestServiceHandler test_service_handler_;
   instrumented_io_context handler_io_service_;
-  std::unique_ptr<std::thread> thread_;
+  std::unique_ptr<std::thread> handler_thread_;
   std::unique_ptr<TestGrpcService> test_service_;
   std::unique_ptr<GrpcServer> grpc_server_;
   grpc::CompletionQueue cq_;
   std::shared_ptr<grpc::Channel> channel_;
-  std::unique_ptr<TestService::Stub> test_service_stub_;
   // Client
   instrumented_io_context client_io_service_;
+  std::unique_ptr<std::thread> client_thread_;
   std::unique_ptr<ClientCallManager> client_call_manager_;
+  std::unique_ptr<GrpcClient<TestService>> grpc_client_;
 };
 
 TEST_F(TestGrpcServerFixture, TestBasic) {
   // Send request
   SleepRequest request;
-  SleepReply reply;
-  grpc::ClientContext context;
   request.set_sleep_time_ms(1000);
-  std::unique_ptr<grpc::ClientAsyncResponseReader<SleepReply>> rpc(
-      test_service_stub_->AsyncSleep(&context, request, &cq_));
-  grpc::Status status;
-  rpc->Finish(&reply, &status, (void *)1);
-  // Wait for reply
-  void *got_tag;
-  bool ok = false;
-  cq_.Next(&got_tag, &ok);
-  if (ok && got_tag == (void *)1) {
-    RAY_LOG(INFO) << ok;
-  }
-}
-
-TEST_F(TestGrpcServerFixture, TestMaxActiveRpcs) {
-  // Send request
-  SleepRequest request;
-  SleepReply reply;
-  grpc::ClientContext context;
-  request.set_sleep_time_ms(1000);
-  std::unique_ptr<grpc::ClientAsyncResponseReader<SleepReply>> rpc(
-      test_service_stub_->AsyncSleep(&context, request, &cq_));
-  grpc::Status status;
-  rpc->Finish(&reply, &status, (void *)1);
-  // Wait for reply
-  void *got_tag;
-  bool ok = false;
-  cq_.Next(&got_tag, &ok);
-  if (ok && got_tag == (void *)1) {
-    RAY_LOG(INFO) << ok;
+  bool done = false;
+  Sleep(request, [&done](const Status status, const SleepReply &reply) {
+    RAY_LOG(INFO) << "replied, status=" << status;
+    done = true;
+  });
+  while (!done) {
+    RAY_LOG(INFO) << "waiting";
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
 }
 }  // namespace rpc
