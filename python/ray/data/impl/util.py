0ab3d63e8d823381ff76dbfc619e5b62427afc94
Clark Zinzow <clarkzinzow@gmail.com>
2021-08-25 21:14:50 +0000

    Better error message.

--- python/ray/data/impl/util.py
+++ python/ray/data/impl/util.py
@@ -12,13 +12,14 @@ def _check_pyarrow_version():
         import pkg_resources
         try:
             version_info = pkg_resources.require("pyarrow")
-            version = tuple(int(n) for n in version_info[0].version.split("."))
+            version_str = version_info[0].version
+            version = tuple(int(n) for n in version_str.split("."))
             if version < MIN_PYARROW_VERSION:
                 raise ImportError(
-                    "You are using an old, incompatible version of pyarrow, "
-                    "please use version "
-                    f"{'.'.join(str(n) for n in MIN_PYARROW_VERSION)} or "
-                    "higher by e.g. running pip install -U pyarrow")
+                    "Datasets requires pyarrow >= "
+                    f"{'.'.join(str(n) for n in MIN_PYARROW_VERSION)}, "
+                    f"but {version_str} is installed. Upgrade with "
+                    "`pip install -U pyarrow`.")
         except pkg_resources.DistributionNotFound:
             logger.warning("You are using the 'pyarrow' module, but "
                            "the exact version is unknown (possibly carried as "
