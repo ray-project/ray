46dd705809f6339470f6bd967d51e352cd5c3507
Kai Fricke <kai@anyscale.com>
2021-08-25 15:47:56 +0200

    Install minimal script

---
+++ ci/travis/install-minimal.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+ROOT_DIR=$(builtin cd "$(dirname "${BASH_SOURCE:-$0}")" || exit; pwd)
+WORKSPACE_DIR="${ROOT_DIR}/../.."
+
+# Installs conda and python 3.7
+MINIMAL_INSTALL=1 PYTHON=3.7 "${WORKSPACE_DIR}/ci/travis/install-dependencies.sh"
+
+# Create new conda env
+conda create -y -n minimal python=3.7
+conda activate minimal
+
+# Install Ray wheels
+python -m pip install -e "${WORKSPACE_DIR}/python/"
+
+# Re-install psutil
+CC=gcc python -m pip install -U psutil setproctitle==1.2.2 colorama --target="${WORKSPACE_DIR}/python/ray/thirdparty_files"
+
+# Install test requirements
+CC=gcc python -m pip install -U \
+  pytest \
+  pytest-asyncio \
+  pytest-rerunfailures \
+  pytest-sugar \
+  pytest-lazy-fixture \
+  pytest-timeout
