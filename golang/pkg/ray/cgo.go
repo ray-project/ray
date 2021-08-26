95adb9a7f56a73117d1a124a6c036e11020b6ac7
chenk008 <kongchen28@gmail.com>
2021-08-26 11:45:10 +0800

    linux

--- golang/pkg/ray/cgo.go
+++ golang/pkg/ray/cgo.go
@@ -3,6 +3,7 @@ package ray
 /*
    #cgo CFLAGS: -I${SRCDIR}/../../../src/ray/core_worker/lib/golang
    #cgo LDFLAGS: -lcore_worker_library_go
+   #cgo linux,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/linux-amd64 -L${SRCDIR}/packaged/lib/linux-amd64
    #cgo darwin,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/darwin-amd64 -L${SRCDIR}/packaged/lib/darwin-amd64
    #include <string.h>
    #include "go_worker.h"
