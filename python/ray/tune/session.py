220017bd62c92f3a1673b8cef2eb111971711789
Kai Fricke <krfricke@users.noreply.github.com>
2021-08-26 09:31:05 +0200

    Update python/ray/tune/session.py

--- python/ray/tune/session.py
+++ python/ray/tune/session.py
@@ -50,8 +50,8 @@ def init(reporter, ignore_reinit_error=True):
             "If you are using ray.init(local_mode=True), "
             "you must set ray.init(..., num_cpus=1, num_gpus=1) to limit "
             "available concurrency. If you are supplying a wrapped "
-            "Searcher(concurrency, repeating) or customized SearchAlgo. Please"
-            " try limiting the concurrency to 1 there.")
+            "Searcher(concurrency, repeating) or customized SearchAlgo. "
+            "Please try limiting the concurrency to 1 there.")
         if ignore_reinit_error:
             logger.warning(reinit_msg)
             return
