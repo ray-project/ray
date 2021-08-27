7a4edcd6686238013a2f21c86d0615bb9d62a000
Mingwei Tian <mwtian@anyscale.com>
2021-08-26 18:23:02 -0700

    Fix clang-format to always compare against master

--- ci/travis/check-git-clang-format-output.sh
+++ ci/travis/check-git-clang-format-output.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if [ -z "${TRAVIS_PULL_REQUEST-}" ] || [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  # Not in a pull request, so compare against parent commit
-  base_commit="HEAD^"
-  echo "Running clang-format against parent commit $(git rev-parse "$base_commit")"
+# Compare against the master branch, because most development is done against it.
+base_commit="$(git merge-base HEAD master)"
+if [ "$base_commit" = $(git rev-parse HEAD) ]; then
+  # Prefix of master branch, so compare against parent commit
+  base_commit=$(git rev-parse HEAD^)
+  echo "Running clang-format against parent commit $base_commit"
 else
-  base_commit="$(git merge-base "${TRAVIS_BRANCH}" HEAD)"
-  echo "Running clang-format against branch $base_commit, with hash $(git rev-parse "$base_commit")"
+  echo "Running clang-format against parent commit $base_commit from master branch"
 fi
+
 exclude_regex="(.*thirdparty/|.*redismodule.h|.*.java|.*.jsx?|.*.tsx?)"
-output="$(ci/travis/git-clang-format --binary clang-format --commit "$base_commit" --diff --exclude "$exclude_regex")"
+output="$(ci/travis/git-clang-format --commit "$base_commit" --diff --exclude "$exclude_regex")"
 if [ "$output" = "no modified files to format" ] || [ "$output" = "clang-format did not modify any files" ] ; then
   echo "clang-format passed."
   exit 0
