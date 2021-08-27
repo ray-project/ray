b4bbf832de485423eccba9fc5fe40493e6051dba
Kai Fricke <kai@anyscale.com>
2021-08-27 15:15:07 +0200

    Reset Python for minimal install if version did not change

--- ci/travis/install-dependencies.sh
+++ ci/travis/install-dependencies.sh
@@ -144,6 +144,12 @@ install_miniconda() {
       echo "Updating Anaconda Python ${python_version} to ${PYTHON}..."
       "${WORKSPACE_DIR}"/ci/suppress_output conda install -q -y python="${PYTHON}"
     )
+  elif [ "${MINIMAL_INSTALL}" = "1" ]; then  # Reset environment
+    (
+      set +x
+      echo "Resetting Anaconda Python ${python_version}..."
+      "${WORKSPACE_DIR}"/ci/suppress_output conda install -q -y --rev 0
+    )
   fi
 
   command -V python
@@ -275,7 +281,7 @@ install_dependencies() {
   install_toolchains
 
   install_upgrade_pip
-  if [ -n "${PYTHON-}" ] || [ "${LINT-}" = 1 ]; then
+  if [ -n "${PYTHON-}" ] || [ "${LINT-}" = 1 ] || [ "${MINIMAL_INSTALL}" = "1" ]; then
     install_miniconda
     # Upgrade the miniconda pip.
     install_upgrade_pip
