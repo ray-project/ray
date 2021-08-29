fae0623cc007a62f36114d796ede1d8358798fac
Yi Cheng <chengyidna@gmail.com>
2021-08-29 21:33:48 +0000

    up

---
+++ src/mock/ray/core_worker/actor_creator.h
@@ -0,0 +1,28 @@
+namespace ray {
+namespace core {
+
+class MockActorCreatorInterface : public ActorCreatorInterface {
+ public:
+  MOCK_METHOD(Status, RegisterActor, (const TaskSpecification &task_spec), (override));
+  MOCK_METHOD(Status, AsyncRegisterActor, (const TaskSpecification &task_spec, gcs::StatusCallback callback), (override));
+  MOCK_METHOD(Status, AsyncCreateActor, (const TaskSpecification &task_spec, const gcs::StatusCallback &callback), (override));
+  MOCK_METHOD(void, AsyncWaitForActorRegisterFinish, (const ActorID &actor_id, gcs::StatusCallback callback), (override));
+  MOCK_METHOD(bool, IsActorInRegistering, (const ActorID &actor_id), (const, override));
+};
+
+}  // namespace core
+}  // namespace ray
+
+namespace ray {
+namespace core {
+
+class MockDefaultActorCreator : public DefaultActorCreator {
+ public:
+  MOCK_METHOD(Status, RegisterActor, (const TaskSpecification &task_spec), (override));
+  MOCK_METHOD(Status, AsyncRegisterActor, (const TaskSpecification &task_spec, gcs::StatusCallback callback), (override));
+  MOCK_METHOD(bool, IsActorInRegistering, (const ActorID &actor_id), (const, override));
+  MOCK_METHOD(Status, AsyncCreateActor, (const TaskSpecification &task_spec, const gcs::StatusCallback &callback), (override));
+};
+
+}  // namespace core
+}  // namespace ray
