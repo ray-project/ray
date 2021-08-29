ceaa64a55a5e91845b8008fd2b7b327a71194b94
Yi Cheng <chengyidna@gmail.com>
2021-08-29 21:33:56 +0000

    format

--- src/mock/ray/core_worker/actor_creator.h
+++ src/mock/ray/core_worker/actor_creator.h
@@ -4,9 +4,14 @@ namespace core {
 class MockActorCreatorInterface : public ActorCreatorInterface {
  public:
   MOCK_METHOD(Status, RegisterActor, (const TaskSpecification &task_spec), (override));
-  MOCK_METHOD(Status, AsyncRegisterActor, (const TaskSpecification &task_spec, gcs::StatusCallback callback), (override));
-  MOCK_METHOD(Status, AsyncCreateActor, (const TaskSpecification &task_spec, const gcs::StatusCallback &callback), (override));
-  MOCK_METHOD(void, AsyncWaitForActorRegisterFinish, (const ActorID &actor_id, gcs::StatusCallback callback), (override));
+  MOCK_METHOD(Status, AsyncRegisterActor,
+              (const TaskSpecification &task_spec, gcs::StatusCallback callback),
+              (override));
+  MOCK_METHOD(Status, AsyncCreateActor,
+              (const TaskSpecification &task_spec, const gcs::StatusCallback &callback),
+              (override));
+  MOCK_METHOD(void, AsyncWaitForActorRegisterFinish,
+              (const ActorID &actor_id, gcs::StatusCallback callback), (override));
   MOCK_METHOD(bool, IsActorInRegistering, (const ActorID &actor_id), (const, override));
 };
 
@@ -19,9 +24,13 @@ namespace core {
 class MockDefaultActorCreator : public DefaultActorCreator {
  public:
   MOCK_METHOD(Status, RegisterActor, (const TaskSpecification &task_spec), (override));
-  MOCK_METHOD(Status, AsyncRegisterActor, (const TaskSpecification &task_spec, gcs::StatusCallback callback), (override));
+  MOCK_METHOD(Status, AsyncRegisterActor,
+              (const TaskSpecification &task_spec, gcs::StatusCallback callback),
+              (override));
   MOCK_METHOD(bool, IsActorInRegistering, (const ActorID &actor_id), (const, override));
-  MOCK_METHOD(Status, AsyncCreateActor, (const TaskSpecification &task_spec, const gcs::StatusCallback &callback), (override));
+  MOCK_METHOD(Status, AsyncCreateActor,
+              (const TaskSpecification &task_spec, const gcs::StatusCallback &callback),
+              (override));
 };
 
 }  // namespace core
