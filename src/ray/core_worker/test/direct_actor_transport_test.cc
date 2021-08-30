f6e31e7936fdbb4e595ca539649996bbcebf939d
Yi Cheng <chengyidna@gmail.com>
2021-08-30 05:44:15 +0000

    up

--- src/ray/core_worker/test/direct_actor_transport_test.cc
+++ src/ray/core_worker/test/direct_actor_transport_test.cc
@@ -14,8 +14,6 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include "mock/ray/core_worker/actor_creator.h"
-#include "mock/ray/core_worker/task_manager.h"
 #include "ray/common/asio/instrumented_io_context.h"
 #include "ray/common/task/task_spec.h"
 #include "ray/common/test_util.h"
@@ -23,6 +21,8 @@
 #include "ray/core_worker/transport/direct_task_transport.h"
 #include "ray/raylet_client/raylet_client.h"
 #include "ray/rpc/worker/core_worker_client.h"
+#include "mock/ray/core_worker/actor_creator.h"
+#include "mock/ray/core_worker/task_manager.h"
 namespace ray {
 namespace core {
 
