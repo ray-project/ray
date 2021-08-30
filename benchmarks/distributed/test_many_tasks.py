ca86098680ff9c8e2980370e691ae1e812226515
Alex Wu <alex@anyscale.io>
2021-08-30 10:35:23 -0700

    Revert "[core] Refactor test_many_tasks (#18169)" (#18216)
    This reverts commit eb6fd20d53c443aba71cd0dd255f26a3e04d0b3f.

--- benchmarks/distributed/test_many_tasks.py
+++ benchmarks/distributed/test_many_tasks.py
@@ -1,110 +1,46 @@
-import asyncio
 import click
 import json
 import os
 import ray
+import ray._private.test_utils as test_utils
 import time
 import tqdm
-import numpy as np
 
-
-@ray.remote(num_cpus=0)
-class SignalActor:
-    def __init__(self):
-        self.timestamps = []
-
-    def reset(self, num_expected):
-        self.start_time = time.time()
-        self.num_expected = num_expected
-
-    def send(self):
-        self.timestamps.append(time.time() - self.start_time)
-        assert len(self.timestamps) <= self.num_expected
-
-    def get_timestamps(self):
-        return self.timestamps
-
-
-@ray.remote
-def task(i, signal, semaphore):
-    signal.send.remote()
-    ref = semaphore.acquire.remote()
-    ready = False
-    while not ready:
-        ready, _ = ray.wait([ref], timeout=0)
-        time.sleep(1)
-
-
-@ray.remote(num_cpus=0)
-class Semaphore:
-    def __init__(self, value=1):
-        self._sema = asyncio.Semaphore(value=value)
-
-    async def acquire(self):
-        await self._sema.acquire()
-
-    async def release(self):
-        self._sema.release()
-
-    async def locked(self):
-        return self._sema.locked()
-
-
-def wait_for_condition(condition_predictor, timeout=10, retry_interval_ms=100):
-    """Wait until a condition is met or time out with an exception.
-
-    Args:
-        condition_predictor: A function that predicts the condition.
-        timeout: Maximum timeout in seconds.
-        retry_interval_ms: Retry interval in milliseconds.
-
-    Raises:
-        RuntimeError: If the condition is not met before the timeout expires.
-    """
-    start = time.time()
-    while time.time() - start <= timeout:
-        if condition_predictor():
-            return
-        time.sleep(retry_interval_ms / 1000.0)
-    raise RuntimeError("The condition wasn't met before the timeout expired.")
+sleep_time = 300
 
 
 def test_max_running_tasks(num_tasks):
     cpus_per_task = 0.25
 
-    semaphore_actor = Semaphore.remote()
-    ray.get(semaphore_actor.acquire.remote())
-    signal_actor = SignalActor.remote()
-    ray.get(signal_actor.reset.remote(num_tasks))
+    @ray.remote(num_cpus=cpus_per_task)
+    def task():
+        time.sleep(sleep_time)
 
     refs = [
-        task.options(num_cpus=cpus_per_task).remote(i, signal_actor,
-                                                    semaphore_actor)
-        for i in tqdm.trange(num_tasks, desc="Launching tasks")
+        task.remote() for _ in tqdm.trange(num_tasks, desc="Launching tasks")
     ]
 
-    timestamps = []
-    with tqdm.tqdm(
-            total=num_tasks, desc="Waiting for tasks to be scheduled") as pbar:
-        while len(timestamps) < num_tasks:
-            new_timestamps = ray.get(signal_actor.get_timestamps.remote())
-            pbar.update(len(new_timestamps) - len(timestamps))
-            timestamps = new_timestamps
-            time.sleep(1)
+    max_cpus = ray.cluster_resources()["CPU"]
+    min_cpus_available = max_cpus
+    for _ in tqdm.trange(int(sleep_time / 0.1), desc="Waiting"):
+        try:
+            cur_cpus = ray.available_resources().get("CPU", 0)
+            min_cpus_available = min(min_cpus_available, cur_cpus)
+        except Exception:
+            # There are race conditions `.get` can fail if a new heartbeat
+            # comes at the same time.
+            pass
+        time.sleep(0.1)
 
-    for _ in range(num_tasks):
-        semaphore_actor.release.remote()
+    # There are some relevant magic numbers in this check. 10k tasks each
+    # require 1/4 cpus. Therefore, ideally 2.5k cpus will be used.
+    err_str = f"Only {max_cpus - min_cpus_available}/{max_cpus} cpus used."
+    threshold = num_tasks * cpus_per_task * 0.75
+    assert max_cpus - min_cpus_available > threshold, err_str
 
     for _ in tqdm.trange(num_tasks, desc="Ensuring all tasks have finished"):
         done, refs = ray.wait(refs)
         assert ray.get(done[0]) is None
-
-    timestamps = ray.get(signal_actor.get_timestamps.remote())
-    print(f"p50: {np.percentile(timestamps, 50)}s")
-    print(f"p95: {np.percentile(timestamps, 95)}s")
-    print(f"p100: {np.percentile(timestamps, 100)}s")
-
-    return timestamps
 
 
 def no_resource_leaks():
@@ -114,29 +50,18 @@ def no_resource_leaks():
 @click.command()
 @click.option(
     "--num-tasks", required=True, type=int, help="Number of tasks to launch.")
-@click.option(
-    "--local",
-    is_flag=True,
-    type=bool,
-    default=False,
-    help="Whether to run the test locally, with a simulated cluster.")
-def test(num_tasks, local):
-    if local:
-        num_cpus = 2 * num_tasks // 4
-        ray.init(num_cpus=num_cpus)
-    else:
-        ray.init(address="auto")
+def test(num_tasks):
+    ray.init(address="auto")
 
-    wait_for_condition(no_resource_leaks)
+    test_utils.wait_for_condition(no_resource_leaks)
     start_time = time.time()
-    timestamps = test_max_running_tasks(num_tasks)
+    test_max_running_tasks(num_tasks)
     end_time = time.time()
-    wait_for_condition(no_resource_leaks)
+    test_utils.wait_for_condition(no_resource_leaks)
 
-    max_timestamp = max(timestamps)
-    rate = num_tasks / max_timestamp
+    rate = num_tasks / (end_time - start_time - sleep_time)
 
-    print(f"Success! Started {num_tasks} tasks in {max_timestamp}s. "
+    print(f"Success! Started {num_tasks} tasks in {end_time - start_time}s. "
           f"({rate} tasks/s)")
 
     if "TEST_OUTPUT_JSON" in os.environ:
