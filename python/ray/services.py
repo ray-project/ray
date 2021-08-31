bdf66da606cb3e24f5c06525cd9d375599a00885
Jiajun Yao <jeromeyjj@gmail.com>
2021-08-31 12:02:03 -0700

    Push retry message to the driver

--- python/ray/services.py
+++ python/ray/services.py
@@ -7,7 +7,7 @@ import ray
 def get_node_ip_address(address="8.8.8.8:53"):
     warnings.warn(
         DeprecationWarning(
-            "ray.services.get_node_ip_address has been moved to "
+            "rray.services.get_node_ip_address has been moved to "
             "ray.util.get_node_ip_address. 'ray.services' will be "
             "removed after Ray 1.4."),
         stacklevel=2)
