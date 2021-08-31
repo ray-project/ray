536ce0d11cc1e942eef980a6ee3429ec316ab693
Yi Cheng <chengyidna@gmail.com>
2021-08-31 05:57:51 +0000

    lint

--- src/ray/object_manager/plasma/test/object_lifecycle_manager_test.cc
+++ src/ray/object_manager/plasma/test/object_lifecycle_manager_test.cc
@@ -120,8 +120,8 @@ TEST_F(ObjectLifecycleManagerTest, CreateObjectTriggerGC) {
   // gc returns object to evict
   EXPECT_CALL(*eviction_policy_, RequireSpace(_, _))
       .Times(1)
-      .WillOnce(Invoke([&](auto size, auto to_evict) {
-        to_evict->push_back(id1_);
+      .WillOnce(Invoke([&](auto size, auto &to_evict) {
+        to_evict.push_back(id1_);
         return 0;
       }));
 
