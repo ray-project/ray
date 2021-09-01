54915344b065e502239fd7dab9f0185ab2ac0d69
Jun Gong <jungong@mbpro.local>
2021-08-31 23:47:17 -0700

    lint format.

--- rllib/env/tests/test_record_env_wrapper.py
+++ rllib/env/tests/test_record_env_wrapper.py
@@ -6,14 +6,16 @@ from ray.rllib.env.utils import VideoMonitor, record_env_wrapper
 from ray.rllib.examples.env.mock_env import MockEnv2
 from ray.rllib.examples.env.multi_agent import BasicMultiAgent
 
+
 class TestRecordEnvWrapper(unittest.TestCase):
     def test_wrap_gym_env(self):
-        wrapped = record_env_wrapper(env=MockEnv2(10),
-                                     record_env=tempfile.gettempdir(),
-                                     log_dir="",
-                                     policy_config={
-                                         "in_evaluation": False,
-                                     })
+        wrapped = record_env_wrapper(
+            env=MockEnv2(10),
+            record_env=tempfile.gettempdir(),
+            log_dir="",
+            policy_config={
+                "in_evaluation": False,
+            })
         # Type is wrappers.Monitor.
         self.assertTrue(isinstance(wrapped, wrappers.Monitor))
         self.assertFalse(isinstance(wrapped, VideoMonitor))
@@ -28,12 +30,13 @@ class TestRecordEnvWrapper(unittest.TestCase):
         self.assertEqual(wrapped.get_episode_rewards(), [1000.0])
 
     def test_wrap_multi_agent_env(self):
-        wrapped = record_env_wrapper(env=BasicMultiAgent(3),
-                                     record_env=tempfile.gettempdir(),
-                                     log_dir="",
-                                     policy_config={
-                                         "in_evaluation": False,
-                                     })
+        wrapped = record_env_wrapper(
+            env=BasicMultiAgent(3),
+            record_env=tempfile.gettempdir(),
+            log_dir="",
+            policy_config={
+                "in_evaluation": False,
+            })
         # Type is VideoMonitor.
         self.assertTrue(isinstance(wrapped, wrappers.Monitor))
         self.assertTrue(isinstance(wrapped, VideoMonitor))
@@ -41,7 +44,7 @@ class TestRecordEnvWrapper(unittest.TestCase):
         wrapped.reset()
         # BasicMultiAgent is hardcoded to run 25-step episodes.
         for i in range(25):
-            wrapped.step({0:0, 1:0, 2:0})
+            wrapped.step({0: 0, 1: 0, 2: 0})
 
         # However VideoMonitor's _after_step is overwritten to not
         # use stats_recorder. So nothing to verify here, except that
