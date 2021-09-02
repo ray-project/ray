b41d59c18a2478b8aee5f7f3d7a6b0a7eed37635
Chris Wong <cwong@anyscale.com>
2021-09-02 01:24:07 -0700

    fix local tests

--- python/ray/util/client/server/dataservicer.py
+++ python/ray/util/client/server/dataservicer.py
@@ -24,7 +24,7 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 QUEUE_JOIN_SECONDS = 5
-WAIT_FOR_CLIENT_RECONNECT_SECONDS = 60 * 5  # TODO: Set this to a sane value
+WAIT_FOR_CLIENT_RECONNECT_SECONDS = 5 * 60  # TODO: Set this to a sane value
 
 
 def _get_reconnecting_from_context(context: Any) -> bool:
@@ -78,6 +78,7 @@ class DataServicer(ray_client_pb2_grpc.RayletDataStreamerServicer):
             return
         logger.debug(f"New data connection from client {client_id}: ")
         accepted_connection = self._init(client_id, context, start_time)
+        replay_cache = self.replay_caches[client_id]
         async_cache: AsyncReplayCache = self.async_caches[client_id]
         if not accepted_connection:
             return
@@ -100,6 +101,11 @@ class DataServicer(ray_client_pb2_grpc.RayletDataStreamerServicer):
                     continue
 
                 req_id = req.req_id
+                thread_id = req.thread_id
+
+                cached_result = replay_cache.check_cache(thread_id, req_id)
+                if cached_result is not None:
+                    yield cached_result
 
                 assert isinstance(req, ray_client_pb2.DataRequest)
                 resp = None
@@ -169,6 +175,7 @@ class DataServicer(ray_client_pb2_grpc.RayletDataStreamerServicer):
                     raise Exception(f"Unreachable code: Request type "
                                     f"{req_type} not handled in Datapath")
                 resp.req_id = req.req_id
+                replay_cache.update_cache(thread_id, req_id, resp)
 
                 yield resp
         finally:
