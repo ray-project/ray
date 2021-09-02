ad294c3643f7e60a6d9b969ed6fc54bb2a758606
Chong-Li <lc300133@antgroup.com>
2021-09-02 23:45:58 +0800

    reorder test cases.

--- python/ray/tests/test_advanced.py
+++ python/ray/tests/test_advanced.py
@@ -644,47 +644,6 @@ def test_task_arguments_inline_bytes_limit(ray_start_cluster):
     ray.get(bar.remote())
 
 
-# This case tests whether gcs-based actor scheduler distributes actors
-# in a balanced way. By default, it uses the `SPREAD` strategy of
-# gcs resource scheduler.
-@pytest.mark.parametrize("args", [[5, 20], [5, 3]])
-def test_actor_distribution_balance(ray_start_cluster, args):
-    cluster = ray_start_cluster
-
-    node_count = args[0]
-    actor_count = args[1]
-
-    for i in range(node_count):
-        cluster.add_node(
-            memory=1024**3,
-            _system_config={"gcs_actor_scheduling_enabled": True}
-            if i == 0 else {})
-    ray.init(address=cluster.address)
-    cluster.wait_for_nodes()
-
-    @ray.remote(memory=100 * 1024**2, num_cpus=0.01)
-    class Foo:
-        def method(self):
-            return ray.worker.global_worker.node.unique_id
-
-    actor_distribution = {}
-    actor_list = [Foo.remote() for _ in range(actor_count)]
-    for actor in actor_list:
-        node_id = ray.get(actor.method.remote())
-        if node_id not in actor_distribution.keys():
-            actor_distribution[node_id] = []
-        actor_distribution[node_id].append(actor)
-
-    if node_count >= actor_count:
-        assert len(actor_distribution) == actor_count
-        for node_id, actors in actor_distribution.items():
-            assert len(actors) == 1
-    else:
-        assert len(actor_distribution) == node_count
-        for node_id, actors in actor_distribution.items():
-            assert len(actors) <= int(actor_count / node_count)
-
-
 # This case tests whether gcs-based actor scheduler works properly with
 # a normal task co-existed.
 def test_schedule_actor_and_normal_task(ray_start_cluster):
@@ -769,6 +728,47 @@ def test_schedule_many_actors_and_normal_tasks(ray_start_cluster):
 
     for object in ray.get(normal_task_object_list):
         assert object == 1
+
+
+# This case tests whether gcs-based actor scheduler distributes actors
+# in a balanced way. By default, it uses the `SPREAD` strategy of
+# gcs resource scheduler.
+@pytest.mark.parametrize("args", [[5, 20], [5, 3]])
+def test_actor_distribution_balance(ray_start_cluster, args):
+    cluster = ray_start_cluster
+
+    node_count = args[0]
+    actor_count = args[1]
+
+    for i in range(node_count):
+        cluster.add_node(
+            memory=1024**3,
+            _system_config={"gcs_actor_scheduling_enabled": True}
+            if i == 0 else {})
+    ray.init(address=cluster.address)
+    cluster.wait_for_nodes()
+
+    @ray.remote(memory=100 * 1024**2, num_cpus=0.01)
+    class Foo:
+        def method(self):
+            return ray.worker.global_worker.node.unique_id
+
+    actor_distribution = {}
+    actor_list = [Foo.remote() for _ in range(actor_count)]
+    for actor in actor_list:
+        node_id = ray.get(actor.method.remote())
+        if node_id not in actor_distribution.keys():
+            actor_distribution[node_id] = []
+        actor_distribution[node_id].append(actor)
+
+    if node_count >= actor_count:
+        assert len(actor_distribution) == actor_count
+        for node_id, actors in actor_distribution.items():
+            assert len(actors) == 1
+    else:
+        assert len(actor_distribution) == node_count
+        for node_id, actors in actor_distribution.items():
+            assert len(actors) <= int(actor_count / node_count)
 
 
 # This case tests whether RequestWorkerLeaseReply carries normal task resources
