698277fe73a4606f894450b578dfd68b0e5e678d
Lixin Wei <wlx65005@gmail.com>
2021-09-03 15:17:01 +0800

    lint

--- src/ray/rpc/test/grpc_server_client_test.cc
+++ src/ray/rpc/test/grpc_server_client_test.cc
@@ -12,10 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "ray/rpc/grpc_server.h"
 #include <chrono>
 #include "gtest/gtest.h"
 #include "ray/rpc/grpc_client.h"
+#include "ray/rpc/grpc_server.h"
 #include "src/ray/protobuf/test_service.grpc.pb.h"
 
 namespace ray {
