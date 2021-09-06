1da05209b9303e2fd527306b93e98991657f597f
qicosmos <383121719@qq.com>
2021-09-06 11:46:46 +0800

    [C++ Worker]Add get actor API. (#17897)
    * linkopts shared
    
    * add get actor api
    
    * fix
    
    * improve
    
    * reduce some duplicate code
    
    * improve some

--- cpp/src/ray/runtime/task/task_submitter.h
+++ cpp/src/ray/runtime/task/task_submitter.h
@@ -37,12 +37,26 @@ class TaskSubmitter {
 
   virtual ObjectID SubmitActorTask(InvocationSpec &invocation,
                                    const CallOptions &call_options) = 0;
+
+  virtual ActorID GetActor(bool global, const std::string &actor_name) const = 0;
+
   virtual ray::PlacementGroup CreatePlacementGroup(
       const ray::internal::PlacementGroupCreationOptions &create_options) = 0;
+
   virtual void RemovePlacementGroup(const std::string &group_id) = 0;
+
   virtual bool WaitPlacementGroupReady(const std::string &group_id, int timeout_seconds) {
     return true;
   }
+
+ protected:
+  std::string GetFullName(bool global, const std::string &name) const {
+    if (name.empty()) {
+      return "";
+    }
+    return global ? name : GetCurrentJobID().Hex() + "-" + name;
+  }
+  virtual JobID GetCurrentJobID() const = 0;
 };
 }  // namespace internal
 }  // namespace ray
