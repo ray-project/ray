5aed4325225e69ba2d9758740ac09578fc822673
Chong-Li <lc300133@antgroup.com>
2021-09-10 13:44:26 +0800

    Skip gcs-based-scheduling tests on Windows.

--- python/ray/tests/test_advanced.py
+++ python/ray/tests/test_advanced.py
@@ -646,6 +646,7 @@ def test_task_arguments_inline_bytes_limit(ray_start_cluster):
 
 # This case tests whether gcs-based actor scheduler works properly with
 # a normal task co-existed.
+@pytest.mark.skipif(sys.platform == "win32", reason="Time out on Windows")
 def test_schedule_actor_and_normal_task(ray_start_cluster):
     cluster = ray_start_cluster
     cluster.add_node(
@@ -691,6 +692,7 @@ def test_schedule_actor_and_normal_task(ray_start_cluster):
 
 # This case tests whether gcs-based actor scheduler works properly
 # in a large scale.
+@pytest.mark.skipif(sys.platform == "win32", reason="Time out on Windows")
 def test_schedule_many_actors_and_normal_tasks(ray_start_cluster):
     cluster = ray_start_cluster
 
@@ -733,6 +735,7 @@ def test_schedule_many_actors_and_normal_tasks(ray_start_cluster):
 # This case tests whether gcs-based actor scheduler distributes actors
 # in a balanced way. By default, it uses the `SPREAD` strategy of
 # gcs resource scheduler.
+@pytest.mark.skipif(sys.platform == "win32", reason="Time out on Windows")
 @pytest.mark.parametrize("args", [[5, 20], [5, 3]])
 def test_actor_distribution_balance(ray_start_cluster, args):
     cluster = ray_start_cluster
