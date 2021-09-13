f0fab124c6e93358a2ae60825e6b8438123cd395
久龙 <guyang.sgy@antfin.com>
2021-09-13 19:01:39 +0800

    fix

--- release/util/sanity_check_cpp.sh
+++ release/util/sanity_check_cpp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This script generate a ray C++ template an run example
+# This script generate a ray C++ template and run example
 set -e
 rm -rf ray-template && mkdir ray-template
 ray cpp --generate-bazel-project-template-to ray-template
