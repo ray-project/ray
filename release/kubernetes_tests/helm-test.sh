e6d4562a5f651ffe5f1175391a72e2a2138ba958
Kai Fricke <kai@anyscale.com>
2021-09-14 20:40:37 +0100

    Remove kubernetes_tests

--- release/kubernetes_tests/helm-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-kubectl create namespace helm-test
-kubectl create namespace helm-test2
-KUBERNETES_OPERATOR_TEST_NAMESPACE=helm-test KUBERNETES_OPERATOR_TEST_IMAGE="$IMAGE" python ../../python/ray/tests/kubernetes_e2e/test_helm.py
-kubectl delete namespace helm-test
-kubectl delete namespace helm-test2
-kubectl delete -f ../../deploy/charts/ray/crds/cluster_crd.yaml
