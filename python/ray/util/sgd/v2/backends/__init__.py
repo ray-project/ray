380a65378723c288305751aa023fbc6174b26c39
matthewdeng <matt@anyscale.com>
2021-09-14 09:07:25 -0700

    [SGD] update SGDv2 user guide docs (#18270)
    * [SGD] update SGDv2 user guide docs
    
    * Update doc/source/raysgd/v2/user_guide.rst
    
    Co-authored-by: Antoni Baum <antoni.baum@protonmail.com>
    
    * add new line
    
    * update docs
    
    * fix header line length
    
    * lint
    
    * lint
    
    * lint
    
    * lint
    
    * fix remaining lint issues
    
    * Update doc/source/raysgd/v2/user_guide.rst
    
    Co-authored-by: Amog Kamsetty <amogkam@users.noreply.github.com>
    
    * Update doc/source/raysgd/v2/user_guide.rst
    
    Co-authored-by: Amog Kamsetty <amogkam@users.noreply.github.com>
    
    * address comments
    
    * address comments
    
    * add TODO for iterator API
    
    * Update doc/source/raysgd/v2/user_guide.rst
    
    Co-authored-by: Amog Kamsetty <amogkam@users.noreply.github.com>
    
    * address comments
    
    * address comments
    
    * add tune doc
    
    * restructure table of contents
    
    * add examples; rename example files to include example suffix
    
    * add quick start, porting code
    
    * address comments
    
    Co-authored-by: Antoni Baum <antoni.baum@protonmail.com>
    Co-authored-by: Amog Kamsetty <amogkam@users.noreply.github.com>

--- python/ray/util/sgd/v2/backends/__init__.py
+++ python/ray/util/sgd/v2/backends/__init__.py
@@ -1,6 +1,11 @@
 from ray.util.sgd.v2.backends.backend import BackendConfig
-from ray.util.sgd.v2.backends.torch import TorchConfig
-from ray.util.sgd.v2.backends.tensorflow import TensorflowConfig
 from ray.util.sgd.v2.backends.horovod import HorovodConfig
+from ray.util.sgd.v2.backends.tensorflow import TensorflowConfig
+from ray.util.sgd.v2.backends.torch import TorchConfig
 
-__all__ = ["BackendConfig", "HorovodConfig", "TensorflowConfig", "TorchConfig"]
+__all__ = [
+    "BackendConfig",
+    "HorovodConfig",
+    "TensorflowConfig",
+    "TorchConfig",
+]
