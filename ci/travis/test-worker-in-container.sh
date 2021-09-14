b1fc59702368b58f6f5d7e3549b8475fedf3e16f
chenk008 <kongchen28@gmail.com>
2021-09-14 20:58:39 +0800

    remove unused

--- ci/travis/test-worker-in-container.sh
+++ ci/travis/test-worker-in-container.sh
@@ -21,9 +21,6 @@ export RAY_BACKEND_LOG_LEVEL=debug
 pushd /ray || true
 bash ./ci/travis/install-bazel.sh --system
 
-ls -al /sys/fs/cgroup
-mkdir /sys/fs/cgroup/cpu/ray-aIBs5jS9
-
 # shellcheck disable=SC2046
 bazel test --test_timeout 60 --config=ci $(./scripts/bazel_export_options) \
 --test_tag_filters=-kubernetes,-jenkins_only,privilege_test,-flaky \
