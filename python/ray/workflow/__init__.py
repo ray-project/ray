7d1f408de966f90c95efe5b0cf488e82cd1c6e17
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2021-09-13 17:45:18 -0700

    [workflow] Move `experimental/workflow` to `workflow` (#18521)

---
+++ python/ray/workflow/__init__.py
@@ -0,0 +1,22 @@
+from ray.workflow.api import (
+    step,
+    init,
+    virtual_actor,
+    get_output,
+    get_actor,
+    get_status,
+    resume,
+    cancel,
+    list_all,
+    resume_all,
+)
+from ray.workflow.workflow_access import WorkflowExecutionError
+from ray.workflow.common import WorkflowStatus
+
+__all__ = [
+    "step", "virtual_actor", "resume", "get_output", "get_actor",
+    "WorkflowExecutionError", "resume_all", "cancel", "get_status", "list_all",
+    "init"
+]
+
+globals().update(WorkflowStatus.__members__)
