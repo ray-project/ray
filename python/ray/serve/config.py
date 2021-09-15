829adac31320f21e9226f8646440395ad09ccf50
architkulkarni <arkulkar@gmail.com>
2021-09-15 16:48:39 -0700

    add autoscaling config options

--- python/ray/serve/config.py
+++ python/ray/serve/config.py
@@ -9,6 +9,37 @@ from ray import cloudpickle as cloudpickle
 from ray.serve.constants import DEFAULT_HTTP_HOST, DEFAULT_HTTP_PORT
 
 
+class AutoscalingConfig(BaseModel):
+    # Publicly exposed options
+    max_replicas: int
+    min_replicas: int
+    target_num_ongoing_requests_per_replica: int = 1
+
+    # Private options below
+
+    # Metrics scraping options
+    metrics_interval_s: float = 10.0
+    loop_period_s: float = 30.0
+    look_back_period_s: float = 30.0
+
+    # Internal autoscaling configuration options
+
+    # How long to wait before scaling down replicas
+    downscale_delay_s: float = 600.0
+    # How long to wait before scaling up replicas
+    upscale_delay_s: float = 30.0
+    # Multiplicative "gain" factor to limit scaling decisions
+    smoothing_factor: float = 1.0
+    # The number of replicas to start with when creating the deployment
+    initial_replicas: int = 1
+    # The num_ongoing_requests_per_replica error ratio (desired / current)
+    # threshold for overriding `upscale_delay_s`
+    panic_mode_threshold: float = 2.0
+
+    # TODO(architkulkarni): Add reasonable defaults
+    # TODO(architkulkarni): Add pydantic validation.  E.g. max_replicas>=min
+
+
 class BackendConfig(BaseModel):
     """Configuration options for a backend, to be set by the user.
 
@@ -37,6 +68,8 @@ class BackendConfig(BaseModel):
 
     experimental_graceful_shutdown_wait_loop_s: NonNegativeFloat = 2.0
     experimental_graceful_shutdown_timeout_s: NonNegativeFloat = 20.0
+
+    autoscaling_config: Optional[AutoscalingConfig] = None
 
     class Config:
         validate_assignment = True
