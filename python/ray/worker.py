7fed9a212f0b91cb94be157237f82958f11335af
Ed Oakes <ed.nmi.oakes@gmail.com>
2021-09-17 12:17:02 -0500

    Revert "fix worker runtime_env"
    This reverts commit 16cf5a53bc144c85d2e1e44a00350262d3acb7c6.

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -191,7 +191,8 @@ class Worker:
     @property
     def runtime_env(self):
         """Get the runtime env in json format"""
-        return self.core_worker.get_current_runtime_env_dict()
+        return json.loads(self.core_worker.get_job_config()
+                          .runtime_env.serialized_runtime_env)
 
     def get_serialization_context(self, job_id=None):
         """Get the SerializationContext of the job that this worker is processing.
