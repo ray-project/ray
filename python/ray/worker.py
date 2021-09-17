16cf5a53bc144c85d2e1e44a00350262d3acb7c6
Ed Oakes <ed.nmi.oakes@gmail.com>
2021-09-17 10:34:50 -0500

    fix worker runtime_env

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -191,8 +191,7 @@ class Worker:
     @property
     def runtime_env(self):
         """Get the runtime env in json format"""
-        return json.loads(self.core_worker.get_job_config()
-                          .runtime_env.serialized_runtime_env)
+        return self.core_worker.get_current_runtime_env_dict()
 
     def get_serialization_context(self, job_id=None):
         """Get the SerializationContext of the job that this worker is processing.
