a9849b502c6df003c73038151421efadf0a91e70
Jiao Dong <jiaodong@anyscale.com>
2021-09-16 19:44:56 -0700

    nits

--- python/ray/util/client/server/demo_script.py
+++ python/ray/util/client/server/demo_script.py
@@ -6,6 +6,7 @@ import time
 import ray
 import ray.experimental.internal_kv as ray_kv
 
+
 @ray.remote
 class StepActor:
     def __init__(self, interval_s=1, total_steps=3):
@@ -16,8 +17,8 @@ class StepActor:
 
         worker = ray.worker.global_worker
         worker_id = worker.core_worker.get_actor_id()
-        ray_kv._internal_kv_put(f"JOB:{worker_id}", self.current_step, overwrite=True)
-
+        ray_kv._internal_kv_put(
+            f"JOB:{worker_id}", self.current_step, overwrite=True)
 
     def run(self):
         worker = ray.worker.global_worker
@@ -25,12 +26,14 @@ class StepActor:
 
         while self.current_step <= self.total_steps:
             if not self.stopped:
-                print(f"Sleeping {self.interval_s} secs to executing step {self.current_step}")
+                print(f"Sleeping {self.interval_s} secs to executing "
+                      f"step {self.current_step}")
                 time.sleep(self.interval_s)
                 self.current_step += 1
-                ray_kv._internal_kv_put(f"JOB:{worker_id}", self.current_step, overwrite=True)
+                ray_kv._internal_kv_put(
+                    f"JOB:{worker_id}", self.current_step, overwrite=True)
             else:
-                print(f"Stop called or reached final step.")
+                print("Stop called or reached final step.")
                 break
 
         self.stopped = True
@@ -60,9 +63,9 @@ if __name__ == "__main__":
         help="Password for connecting to Redis")
     args, _ = parser.parse_known_args()
 
-
     ray.init()
-    step_actor = StepActor.remote(interval_s=args.interval_s, total_steps=args.total_steps)
+    step_actor = StepActor.remote(
+        interval_s=args.interval_s, total_steps=args.total_steps)
     ref = step_actor.run.remote()
     print(ray.get([ref]))
     job_key = ray_kv._internal_kv_list("JOB:")[0]
