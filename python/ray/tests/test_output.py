0fc8fe517968d8eca3ef2a540ca2d40488c7d6c3
Eric Liang <ekhliang@gmail.com>
2021-09-22 12:08:12 -0700

    update

--- python/ray/tests/test_output.py
+++ python/ray/tests/test_output.py
@@ -60,6 +60,30 @@ time.sleep(25)
 
 
 @pytest.mark.skipif(sys.platform == "win32", reason="Failing on Windows.")
+def test_autoscaler_no_spam():
+    script = """
+import ray
+import time
+
+ray.init(num_cpus=1)
+
+@ray.remote(num_cpus=1)
+def f():
+    time.sleep(1)
+
+ray.get([f.remote() for _ in range(5)])
+    """
+
+    proc = run_string_as_driver_nonblocking(script)
+    out_str = proc.stdout.read().decode("ascii")
+    err_str = proc.stderr.read().decode("ascii")
+
+    print(out_str, err_str)
+    assert "Tip:" not in out_str
+    assert "Tip:" not in err_str
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="Failing on Windows.")
 def test_fail_importing_actor(ray_start_regular, error_pubsub):
     script = """
 import os
