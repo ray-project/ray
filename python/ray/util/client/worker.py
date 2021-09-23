3a5f0850aa1fa8fb2799b41f0fa91ef517aa903e
Ian <ian.rodney@gmail.com>
2021-09-23 21:26:48 +0000

    fix reconnect tests

--- python/ray/util/client/worker.py
+++ python/ray/util/client/worker.py
@@ -129,6 +129,9 @@ class Worker:
         self._connect_channel()
         self._has_connected = True
 
+        # Has Ray been initialized on the server?
+        self._serverside_ray_initialized = False
+
         # Initialize the streams to finish protocol negotiation.
         self.data_client = DataClient(self, self._client_id, self.metadata)
         self.reference_count: Dict[bytes, int] = defaultdict(int)
@@ -635,10 +638,17 @@ class Worker:
                             metadata=self.metadata).actors_json)
 
     def is_initialized(self) -> bool:
-        if self.server is not None:
-            return self.get_cluster_info(
+        if not self.is_connected() or self.server is None:
+            return False
+        if not self._serverside_ray_initialized:
+            # We only check that Ray is initialized on the server once to
+            # avoid making an RPC every time this function is called. This is
+            # safe to do because Ray only 'un-initializes' on the server when
+            # the Client connection is torn down.
+            self._serverside_ray_initialized = self.get_cluster_info(
                 ray_client_pb2.ClusterInfoType.IS_INITIALIZED)
-        return False
+
+        return self._serverside_ray_initialized
 
     def ping_server(self, timeout=None) -> bool:
         """Simple health check.
