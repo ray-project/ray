bcb2ec1eb5bc9fb9dd6259d3099c6d460f0cbb37
sven1977 <svenmika1977@gmail.com>
2021-09-24 18:34:12 +0200

    wip.

--- rllib/agents/ars/tests/test_ars.py
+++ rllib/agents/ars/tests/test_ars.py
@@ -3,7 +3,7 @@ import unittest
 import ray
 import ray.rllib.agents.ars as ars
 from ray.rllib.utils.test_utils import framework_iterator, \
-    check_compute_single_action, check_train_results
+    check_compute_single_action
 
 
 class TestARS(unittest.TestCase):
@@ -26,7 +26,6 @@ class TestARS(unittest.TestCase):
             trainer = ars.ARSTrainer(config=plain_config, env="CartPole-v0")
             for i in range(num_iterations):
                 results = trainer.train()
-                check_train_results(results)
                 print(results)
 
             check_compute_single_action(trainer)
