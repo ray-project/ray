c0d4b38ea0f53ac362636e6272d178b2750aa6e7
久龙 <guyang.sgy@antfin.com>
2021-09-24 18:36:31 +0800

    fix

--- cpp/example/run.sh
+++ cpp/example/run.sh
@@ -4,6 +4,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")" || exit; pwd)"
 
 bazel --nosystem_rc --nohome_rc build //:example
 if [[ "$OSTYPE" == "darwin"* ]]; then
+    # TODO: We use `install_name_tool` to walk around the issue of dynamic libraries loading in macOS.
     install_name_tool -change bazel-out/darwin-opt/bin/cpp/libray_api.so ./thirdparty/lib/libray_api.so ./bazel-bin/example.so
     install_name_tool -change bazel-out/darwin-opt/bin/cpp/libray_api.so ./thirdparty/lib/libray_api.so ./bazel-bin/example
     "${ROOT_DIR}"/bazel-bin/example
