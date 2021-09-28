1781636dac965fa2eb5b518e1fcd2745cb477d46
SangBin Cho <rkooo567@gmail.com>
2021-09-28 05:43:26 -0700

    remove test files

--- a.py
+++
@@ -1,8 +0,0 @@
-import ray
-ray.init()
-
-@ray.remote 
-def some_function(x: int, y: int, z, *arg) -> float:
-    return x / y
-a = ray.put([1])
-ray.get(some_function.remote(1, 0, a, 5))
--- b.py
+++
@@ -1,4 +0,0 @@
-def some_function(x: int, y: int) -> float: 
-    raise Exception
-
-some_function(1, 0)
