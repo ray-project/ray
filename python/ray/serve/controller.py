1543604d8b9c8debf1c8f2ffbb8dc08904d16ea6
architkulkarni <arkulkar@gmail.com>
2021-09-28 14:53:46 -0700

    lint

--- python/ray/serve/controller.py
+++ python/ray/serve/controller.py
@@ -296,14 +296,13 @@ class ServeController:
                     f"prev_version '{prev_version}' is specified but "
                     "there is no existing deployment.")
             if existing_backend_info.version != prev_version:
-                raise ValueError(
-                    f"prev_version '{prev_version}' "
-                    "does not match with the existing "
-                    f"version '{existing_backend_info.version}'.")
+                raise ValueError(f"prev_version '{prev_version}' "
+                                 "does not match with the existing "
+                                 f"version '{existing_backend_info.version}'.")
         backend_info = BackendInfo(
             actor_def=ray.remote(
-                create_backend_replica(
-                    name, replica_config.serialized_backend_def)),
+                create_backend_replica(name,
+                                       replica_config.serialized_backend_def)),
             version=version,
             backend_config=backend_config,
             replica_config=replica_config,
