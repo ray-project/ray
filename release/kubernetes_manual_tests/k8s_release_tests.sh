30ff473d9508291e9cdbc3131f9f7e2b18b260c9
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2021-09-29 14:08:36 -0700

    lint

--- release/kubernetes_manual_tests/k8s_release_tests.sh
+++ release/kubernetes_manual_tests/k8s_release_tests.sh
@@ -7,21 +7,21 @@ HELM_SUCCEEDED=$?
 IMAGE="$IMAGE" bash k8s-test-scale.sh
 SCALE_SUCCEEDED=$?
 
-if (( $BASIC_SUCCEEDED == 0 ))
+if (( BASIC_SUCCEEDED == 0 ))
 then
     echo "k8s-test.sh succeeded"
 else
     echo "k8s-test.sh test failed"
 fi
 
-if (( $HELM_SUCCEEDED == 0 ))
+if (( HELM_SUCCEEDED == 0 ))
 then
     echo "helm-test.sh test succeeded";
 else
     echo "helm-test.sh test failed"
 fi
 
-if (( $SCALE_SUCCEEDED == 0))
+if (( SCALE_SUCCEEDED == 0))
 then
     echo "k8s-test-scale.sh test succeeded";
 else
