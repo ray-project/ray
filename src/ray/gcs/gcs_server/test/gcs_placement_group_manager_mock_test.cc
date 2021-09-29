3c63732f19f83e68f115d25adc41151c1cb3876e
Yi Cheng <chengyidna@gmail.com>
2021-09-29 08:06:07 +0000

    up

--- src/ray/gcs/gcs_server/test/gcs_placement_group_manager_mock_test.cc
+++ src/ray/gcs/gcs_server/test/gcs_placement_group_manager_mock_test.cc
@@ -29,26 +29,19 @@ using namespace ray::gcs;
 namespace ray {
 namespace gcs {
 
-class GcsPlacementGroupManagerMockTest : public  Test {
+class GcsPlacementGroupManagerMockTest : public Test {
  public:
   void SetUp() override {
     store_client_ = std::make_shared<MockStoreClient>();
     gcs_table_storage_ = std::make_shared<GcsTableStorage>(store_client_);
-    gcs_placement_group_scheduler_ = std::make_shared<MockGcsPlacementGroupSchedulerInterface>();
-    resource_manager_ = std::make_shared<MockGcsResourceManager>(
-        io_context_,
-        nullptr,
-        nullptr,
-        true);
+    gcs_placement_group_scheduler_ =
+        std::make_shared<MockGcsPlacementGroupSchedulerInterface>();
+    resource_manager_ =
+        std::make_shared<MockGcsResourceManager>(io_context_, nullptr, nullptr, true);
 
     gcs_placement_group_manager_ = std::make_unique<GcsPlacementGroupManager>(
-        io_context_,
-        gcs_placement_group_scheduler_,
-        gcs_table_storage_,
-        *resource_manager_,
-        [](auto&) {
-          return "";
-        });
+        io_context_, gcs_placement_group_scheduler_, gcs_table_storage_,
+        *resource_manager_, [](auto &) { return ""; });
   }
   std::unique_ptr<GcsPlacementGroupManager> gcs_placement_group_manager_;
   std::shared_ptr<MockGcsPlacementGroupSchedulerInterface> gcs_placement_group_scheduler_;
@@ -58,14 +51,13 @@ class GcsPlacementGroupManagerMockTest : public  Test {
   instrumented_io_context io_context_;
 };
 
-
 TEST_F(GcsPlacementGroupManagerMockTest, PendingQueuePriorityReschedule) {
   // Test priority works
   //   When return with reschedule, it should be given with the highest pri
-  auto req = Mocker::GenCreatePlacementGroupRequest("", rpc::PlacementStrategy::SPREAD, 1);
+  auto req =
+      Mocker::GenCreatePlacementGroupRequest("", rpc::PlacementStrategy::SPREAD, 1);
   auto pg = std::make_shared<GcsPlacementGroup>(req, "");
-  auto cb = [](Status s) {
-            };
+  auto cb = [](Status s) {};
   PGSchedulingFailureCallback failure_callback;
   PGSchedulingSuccessfulCallback success_callback;
   StatusCallback put_cb;
@@ -75,7 +67,7 @@ TEST_F(GcsPlacementGroupManagerMockTest, PendingQueuePriorityReschedule) {
       .WillOnce(DoAll(SaveArg<1>(&failure_callback), SaveArg<2>(&success_callback)));
   auto now = absl::GetCurrentTimeNanos();
   gcs_placement_group_manager_->RegisterPlacementGroup(pg, cb);
-  auto& pending_queue = gcs_placement_group_manager_->pending_placement_groups_;
+  auto &pending_queue = gcs_placement_group_manager_->pending_placement_groups_;
   ASSERT_EQ(1, pending_queue.size());
   ASSERT_LE(now, pending_queue.begin()->first);
   ASSERT_GE(absl::GetCurrentTimeNanos(), pending_queue.begin()->first);
@@ -89,10 +81,10 @@ TEST_F(GcsPlacementGroupManagerMockTest, PendingQueuePriorityReschedule) {
 TEST_F(GcsPlacementGroupManagerMockTest, PendingQueuePriorityFailed) {
   // Test priority works
   //   When return with a failure, exp backoff should work
-  auto req = Mocker::GenCreatePlacementGroupRequest("", rpc::PlacementStrategy::SPREAD, 1);
+  auto req =
+      Mocker::GenCreatePlacementGroupRequest("", rpc::PlacementStrategy::SPREAD, 1);
   auto pg = std::make_shared<GcsPlacementGroup>(req, "");
-  auto cb = [](Status s) {
-            };
+  auto cb = [](Status s) {};
   PGSchedulingFailureCallback failure_callback;
   PGSchedulingSuccessfulCallback success_callback;
   StatusCallback put_cb;
@@ -100,10 +92,11 @@ TEST_F(GcsPlacementGroupManagerMockTest, PendingQueuePriorityFailed) {
       .WillOnce(DoAll(SaveArg<3>(&put_cb), Return(Status::OK())));
   EXPECT_CALL(*gcs_placement_group_scheduler_, ScheduleUnplacedBundles(_, _, _))
       .Times(2)
-      .WillRepeatedly(DoAll(SaveArg<1>(&failure_callback), SaveArg<2>(&success_callback)));
+      .WillRepeatedly(
+          DoAll(SaveArg<1>(&failure_callback), SaveArg<2>(&success_callback)));
   auto now = absl::GetCurrentTimeNanos();
   gcs_placement_group_manager_->RegisterPlacementGroup(pg, cb);
-  auto& pending_queue = gcs_placement_group_manager_->pending_placement_groups_;
+  auto &pending_queue = gcs_placement_group_manager_->pending_placement_groups_;
   ASSERT_EQ(1, pending_queue.size());
   ASSERT_LE(now, pending_queue.begin()->first);
   ASSERT_GE(absl::GetCurrentTimeNanos(), pending_queue.begin()->first);
@@ -112,13 +105,13 @@ TEST_F(GcsPlacementGroupManagerMockTest, PendingQueuePriorityFailed) {
   now = absl::GetCurrentTimeNanos();
   failure_callback(pg, true);
   auto exp_backer = ExponentialBackOff(
-      1000000 *
-      RayConfig::instance().gcs_create_placement_group_retry_min_interval_ms(),
+      1000000 * RayConfig::instance().gcs_create_placement_group_retry_min_interval_ms(),
       RayConfig::instance().gcs_create_placement_group_retry_multiplier(),
-      1000000 *
-      RayConfig::instance().gcs_create_placement_group_retry_max_interval_ms());
+      1000000 * RayConfig::instance().gcs_create_placement_group_retry_max_interval_ms());
   auto next = exp_backer.Next();
-  EXPECT_DOUBLE_EQ(next, 1000000 * RayConfig::instance().gcs_create_placement_group_retry_min_interval_ms());
+  ASSERT_DOUBLE_EQ(
+      next,
+      1000000 * RayConfig::instance().gcs_create_placement_group_retry_min_interval_ms());
   ASSERT_EQ(1, pending_queue.size());
   auto rank = pending_queue.begin()->first;
   ASSERT_LT(now + next, rank);
@@ -134,8 +127,9 @@ TEST_F(GcsPlacementGroupManagerMockTest, PendingQueuePriorityFailed) {
   now = absl::GetCurrentTimeNanos();
   failure_callback(pg, true);
   next = RayConfig::instance().gcs_create_placement_group_retry_multiplier() * next;
-
+  ASSERT_EQ(1, pending_queue.size());
+  ASSERT_DOUBLE_EQ(next, pending_queue.begin()->first);
 }
 
-}
-}
+}  // namespace gcs
+}  // namespace ray
