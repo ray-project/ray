f653442f1d6a4ec9fe66f3b7fd518cb25ec7f0f9
Chen Shen <scv119@gmail.com>
2021-10-01 16:15:44 -0700

    Apply suggestions from code review
    Co-authored-by: Clark Zinzow <clarkzinzow@gmail.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -162,10 +162,10 @@ versionwarning_messages = {
 
 versionwarning_body_selector = "#main-content"
 sphinx_gallery_conf = {
-    "examples_dirs": ["../examples",
-                      "tune/_tutorials"],  # path to example scripts
+    "examples_dirs": ["../examples", "tune/_tutorials",
+                      "data/_examples"],  # path to example scripts
     # path where to save generated examples
-    "gallery_dirs": ["auto_examples", "tune/tutorials"],
+    "gallery_dirs": ["auto_examples", "tune/tutorials", "data/examples"],
     "ignore_pattern": "../examples/doc_code/",
     "plot_gallery": "False",
     "min_reported_time": sys.maxsize,
