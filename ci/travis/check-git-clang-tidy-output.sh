2084c4f791f7763bc9813303bb13f22d686c3e51
Mingwei Tian <mwtian@anyscale.com>
2021-10-04 09:26:52 -0700

    Better messages

--- ci/travis/check-git-clang-tidy-output.sh
+++ ci/travis/check-git-clang-tidy-output.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# TODO: integrate this script into pull request workflow.
-
-printError() {
-    printf '\033[31mERROR:\033[0m %s\n' "$@"
+printWarning() {
+    printf '\033[31mLINT WARNING (clang-tidy):\033[0m %s\n' "$@"
 }
 
 printInfo() {
-    printf '\033[32mINFO:\033[0m %s\n' "$@"
+    printf '\033[34mLINT (clang-tidy):\033[0m %s\n' "$@"
 }
 
 log_err() {
-    printError "Setting up clang-tidy encountered an error"
+    printWarning "Setting up clang-tidy encountered an error"
 }
 
 set -eo pipefail
@@ -28,8 +26,6 @@ else
   printInfo "Running clang-tidy against parent commit $base_commit from master branch"
 fi
 
-printInfo "Fetching workspace info ..."
-
 WORKSPACE=$(bazel info workspace)
 BAZEL_ROOT=$(bazel info execution_root)
 
@@ -37,20 +33,18 @@ printInfo "Generating compilation database ..."
 
 case "${OSTYPE}" in
   linux*)
-    printInfo "Running on Linux, using clang to build C++ targets. Please make sure it is installed with install-llvm-binaries.sh"
+    printInfo "Running on Linux, using clang to build C++ targets ..."
     bazel build //ci/generate_compile_commands:extract_compile_command //:ray_pkg --config=llvm \
         --experimental_action_listener=//ci/generate_compile_commands:compile_command_listener;;
   darwin*)
-    printInfo "Running on MacOS, assuming default C++ compiler is clang."
+    printInfo "Running on MacOS, using default compiler to build C++ targets ..."
     bazel build //ci/generate_compile_commands:extract_compile_command //:ray_pkg \
         --experimental_action_listener=//ci/generate_compile_commands:compile_command_listener;;
   msys*)
-    printInfo "Running on Windows, using clang-cl to build C++ targets. Please make sure it is installed."
+    printInfo "Running on Windows, using clang-cl to build C++ targets ..."
     CC=clang-cl bazel build //ci/generate_compile_commands:extract_compile_command //:ray_pkg \
         --experimental_action_listener=//ci/generate_compile_commands:compile_command_listener;;
 esac
-
-printInfo "Assembling compilation database ..."
 
 TMPFILE=$(mktemp)
 printf '[\n' >"$TMPFILE"
@@ -68,22 +62,21 @@ fi
 OUTFILE=$WORKSPACE/compile_commands.json
 
 if hash jq 2>/dev/null; then
-    printInfo "Formatting compilation database ..."
     jq . "$TMPFILE" >"$OUTFILE"
 else
-    printInfo "Can not find jq. Skip formatting compilation database."
     cp --no-preserve=mode "$TMPFILE" "$OUTFILE"
 fi
 
 trap - EXIT
 
+printInfo "Running clang-tidy ..."
 output="$(git diff -U0 "$base_commit" | ci/travis/clang-tidy-diff.py -p1 -fix)"
 if [[ ! "$output" =~ "error: " ]]; then
   printInfo "clang-tidy passed."
 else
-  printError "clang-tidy issued warnings. See below for details. Suggested fixes have also been applied."
-  printError "$output"
-  printError "If a warning is too pedantic, a proposed fix is incorrect or you are unsure about how to fix a warning,"
-  printError "feel free to raise the issue on the pull request."
-  printError "clang-tidy warnings can also be suppressed with NOLINT"
+  printWarning "clang-tidy issued warnings. See below for details. Suggested fixes have also been applied."
+  printWarning "$output"
+  printWarning "If a warning is too pedantic, a proposed fix is incorrect or you are unsure about how to fix a warning,"
+  printWarning "feel free to raise the issue on the pull request."
+  printWarning "clang-tidy warnings can also be suppressed with NOLINT"
 fi
