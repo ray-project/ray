c3cc0ead488669cc4613ac84da9ce4688421f8ca
Ed Oakes <ed.nmi.oakes@gmail.com>
2021-10-05 11:37:20 -0500

    Dict import

--- python/ray/serve/config.py
+++ python/ray/serve/config.py
@@ -1,7 +1,7 @@
 import inspect
 import pickle
 from enum import Enum
-from typing import Any, Callable, List, Optional, Tuple
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
 import pydantic
 from google.protobuf.json_format import MessageToDict
