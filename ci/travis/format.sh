5d9e3a01218d951294cc94a4122e2bf737e4d824
mwtian <81660174+mwtian@users.noreply.github.com>
2021-10-05 14:03:27 -0700

    [Lint] run `clang-tidy` in `scripts/format.h`, update clang-tidy rules (#19055)

--- ci/travis/format.sh
+++ ci/travis/format.sh
@@ -5,6 +5,18 @@
 # Cause the script to exit if a single command fails
 set -euo pipefail
 
+printWarning() {
+    printf '\033[31mLINT WARNING:\033[0m %s\n' "$@"
+}
+
+printInfo() {
+    printf '\033[34mLINT:\033[0m %s\n' "$@"
+}
+
+if ! git diff --quiet &>/dev/null; then
+    printWarning 'Found unstaged changes before lint. They will be mixed with reformatting, if there is any.'
+fi
+
 FLAKE8_VERSION_REQUIRED="3.9.1"
 YAPF_VERSION_REQUIRED="0.23.0"
 SHELLCHECK_VERSION_REQUIRED="0.7.1"
@@ -26,11 +38,11 @@ check_command_exist() {
             VERSION=$MYPY_VERSION_REQUIRED
             ;;
         *)
-            echo "$1 is not a required dependency"
+            printWarning "$1 is not a required dependency"
             exit 1
     esac
     if ! [ -x "$(command -v "$1")" ]; then
-        echo "$1 not installed. pip install $1==$VERSION"
+        printWarning "$1 not installed. pip install $1==$VERSION"
         exit 1
     fi
 }
@@ -54,7 +66,16 @@ GOOGLE_JAVA_FORMAT_JAR=/tmp/google-java-format-1.7-all-deps.jar
 # params: tool name, tool version, required version
 tool_version_check() {
     if [ "$2" != "$3" ]; then
-        echo "WARNING: Ray uses $1 $3, You currently are using $2. This might generate different results."
+        printWarning "Ray uses $1 $3, You currently are using $2. This might generate different results."
+    fi
+}
+
+# params: tool name, version command, desired version
+tool_version_cmd_check() {
+    local tool_version
+    tool_version="$($2)"
+    if [[ ! "$tool_version" =~ $3 ]]; then
+        printWarning "Ray uses $1 $3, but running $2 indicates otherwise. This might generate different results."
     fi
 }
 
@@ -63,24 +84,40 @@ tool_version_check "yapf" "$YAPF_VERSION" "$YAPF_VERSION_REQUIRED"
 tool_version_check "shellcheck" "$SHELLCHECK_VERSION" "$SHELLCHECK_VERSION_REQUIRED"
 tool_version_check "mypy" "$MYPY_VERSION" "$MYPY_VERSION_REQUIRED"
 
-if which clang-format >/dev/null; then
-  CLANG_FORMAT_VERSION=$(clang-format --version | awk '{print $3}')
-  tool_version_check "clang-format" "$CLANG_FORMAT_VERSION" "12.0.0"
+if command -v clang-format >/dev/null; then
+  tool_version_cmd_check "clang-format" "clang-format --version" "12.0"
 else
-    echo "WARNING: clang-format is not installed!"
+    printWarning "clang-format 12 is not installed!"
+    printWarning "To install on MacOS: brew install clang-format"
+    printWarning "To install on Ubuntu: use package manager e.g. sudo apt install clang-format-12 -y && sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-12 100"
+    printWarning "Or run scripts/install-llvm-binaries.sh"
+fi
+
+if command -v clang-tidy >/dev/null; then
+  tool_version_cmd_check "clang-tidy" "clang-tidy --version" "12.0"
+else
+    printWarning "clang-tidy 12 is not installed!"
+    printWarning "To install on MacOS: brew install clang-tidy"
+    printWarning "To install on Ubuntu: use package manager e.g. sudo apt install clang-tidy-12 -y && sudo update-alternatives --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-12 100"
+    printWarning "Or run scripts/install-llvm-binaries.sh"
+fi
+
+if ! command -v clang >/dev/null; then
+    printWarning "clang is not installed! clang-tidy will be disabled."
+    printWarning "To install, use package manager, run scripts/install-llvm-binaries.sh (Ubuntu only), or compile from source."
 fi
 
 if command -v java >/dev/null; then
   if [ ! -f "$GOOGLE_JAVA_FORMAT_JAR" ]; then
-    echo "Java code format tool google-java-format.jar is not installed, start to install it."
+    printWarning "Java code format tool google-java-format.jar is not installed, start to install it."
     wget https://github.com/google/google-java-format/releases/download/google-java-format-1.7/google-java-format-1.7-all-deps.jar -O "$GOOGLE_JAVA_FORMAT_JAR"
   fi
 else
-    echo "WARNING:java is not installed, skip format java files!"
+    printWarning "Java is not installed, skip formatting Java files!"
 fi
 
 if [[ $(flake8 --version) != *"flake8_quotes"* ]]; then
-    echo "WARNING: Ray uses flake8 with flake8_quotes. Might error without it. Install with: pip install flake8-quotes"
+    printWarning "Ray uses flake8 with flake8_quotes. Might error without it. Install with: pip install flake8-quotes"
 fi
 
 if [[ $(flake8 --version) != *"flake8-bugbear"* ]]; then
@@ -88,9 +125,9 @@ if [[ $(flake8 --version) != *"flake8-bugbear"* ]]; then
 fi
 
 SHELLCHECK_FLAGS=(
-  --exclude=1090  # "Can't follow non-constant source. Use a directive to specify location."
-  --exclude=1091  # "Not following {file} due to some error"
-  --exclude=2207  # "Prefer mapfile or read -a to split command output (or quote to avoid splitting)." -- these aren't compatible with macOS's old Bash
+  "--exclude=1090"  # "Can't follow non-constant source. Use a directive to specify location."
+  "--exclude=1091"  # "Not following {file} due to some error"
+  "--exclude=2207"  # "Prefer mapfile or read -a to split command output (or quote to avoid splitting)." -- these aren't compatible with macOS's old Bash
 )
 
 YAPF_FLAGS=(
@@ -155,7 +192,7 @@ shellcheck_scripts() {
 mypy_on_each() {
     pushd python/ray
     for file in "$@"; do
-       echo "Running mypy on $file"
+       printInfo "Running mypy on $file"
        mypy ${MYPY_FLAGS[@]+"${MYPY_FLAGS[@]}"} "$file"
     done
     popd
@@ -187,7 +224,7 @@ format_files() {
       elif [ -z "${suffix}" ] && [ "${shebang}" != "${shebang%sh}" ] || [ "${suffix}" != "${suffix#.sh}" ]; then
         shell_files+=("${name}")
       else
-        echo "error: failed to determine file type: ${name}" 1>&2
+        printWarning "failed to determine file type: ${name}" 1>&2
         return 1
       fi
     done
@@ -204,7 +241,7 @@ format_files() {
         printf "%s" "${difference}" | patch -p1
       fi
     else
-      echo "error: this version of shellcheck does not support diffs"
+      printWarning "this version of shellcheck does not support diffs"
     fi
 }
 
@@ -212,13 +249,13 @@ format_all_scripts() {
     command -v flake8 &> /dev/null;
     HAS_FLAKE8=$?
 
-    echo "$(date)" "YAPF...."
+    printInfo "$(date)" "YAPF...."
     git ls-files -- '*.py' "${GIT_LS_EXCLUDES[@]}" | xargs -P 10 \
       yapf --in-place "${YAPF_EXCLUDES[@]}" "${YAPF_FLAGS[@]}"
-    echo "$(date)" "MYPY...."
+    printInfo "$(date)" "MYPY...."
     mypy_on_each "${MYPY_FILES[@]}"
     if [ $HAS_FLAKE8 ]; then
-      echo "$(date)" "Flake8...."
+      printInfo "$(date)" "Flake8...."
       git ls-files -- '*.py' "${GIT_LS_EXCLUDES[@]}" | xargs -P 5 \
         flake8 --config=.flake8
 
@@ -234,7 +271,7 @@ format_all_scripts() {
         shell_files+=($(git --no-pager grep -l -- '^#!\(/usr\)\?/bin/\(env \+\)\?\(ba\)\?sh' "${non_shell_files[@]}" || true))
       fi
       if [ 0 -lt "${#shell_files[@]}" ]; then
-        echo "$(date)" "shellcheck scripts...."
+        printInfo "$(date)" "shellcheck scripts...."
         shellcheck_scripts "${shell_files[@]}"
       fi
     fi
@@ -245,17 +282,17 @@ format_all_scripts() {
 format_all() {
     format_all_scripts "${@}"
 
-    echo "$(date)" "clang-format...."
+    printInfo "$(date)" "clang-format...."
     if command -v clang-format >/dev/null; then
       git ls-files -- '*.cc' '*.h' '*.proto' "${GIT_LS_EXCLUDES[@]}" | xargs -P 5 clang-format -i
     fi
 
-    echo "$(date)" "format java...."
+    printInfo "$(date)" "format java...."
     if command -v java >/dev/null & [ -f "$GOOGLE_JAVA_FORMAT_JAR" ]; then
       git ls-files -- '*.java' "${GIT_LS_EXCLUDES[@]}" | sed -E "\:$JAVA_EXCLUDES_REGEX:d" | xargs -P 5 java -jar "$GOOGLE_JAVA_FORMAT_JAR" -i
     fi
 
-    echo "$(date)" "done!"
+    printInfo "$(date)" "done!"
 }
 
 # Format files that differ from main branch. Ignores dirs that are not slated
@@ -269,35 +306,42 @@ format_changed() {
     # exist on both branches.
     MERGEBASE="$(git merge-base upstream/master HEAD)"
 
+    printInfo "Running fake8 on *.py ..."
     if ! git diff --diff-filter=ACRM --quiet --exit-code "$MERGEBASE" -- '*.py' &>/dev/null; then
         git diff --name-only --diff-filter=ACRM "$MERGEBASE" -- '*.py' | xargs -P 5 \
              yapf --in-place "${YAPF_EXCLUDES[@]}" "${YAPF_FLAGS[@]}"
-        if which flake8 >/dev/null; then
+        if command -v flake8 >/dev/null; then
             git diff --name-only --diff-filter=ACRM "$MERGEBASE" -- '*.py' | xargs -P 5 \
                  flake8 --config=.flake8
         fi
     fi
 
+    printInfo "Running fake8 on cython files ..."
     if ! git diff --diff-filter=ACRM --quiet --exit-code "$MERGEBASE" -- '*.pyx' '*.pxd' '*.pxi' &>/dev/null; then
-        if which flake8 >/dev/null; then
+        if command -v flake8 >/dev/null; then
             git diff --name-only --diff-filter=ACRM "$MERGEBASE" -- '*.pyx' '*.pxd' '*.pxi' | xargs -P 5 \
                  flake8 --config=.flake8 "$FLAKE8_PYX_IGNORES"
         fi
     fi
 
-    if which clang-format >/dev/null; then
-        if ! git diff --diff-filter=ACRM --quiet --exit-code "$MERGEBASE" -- '*.cc' '*.h' &>/dev/null; then
-            git diff --name-only --diff-filter=ACRM "$MERGEBASE" -- '*.cc' '*.h' | xargs -P 5 \
-                 clang-format -i
-        fi
+    printInfo "Running clang-format ..."
+    if command -v clang-format >/dev/null; then
+        ci/travis/check-git-clang-format-output.sh
     fi
 
+    printInfo "Running clang-tidy ..."
+    if command -v clang-tidy >/dev/null && command -v clang >/dev/null; then
+        ci/travis/check-git-clang-tidy-output.sh
+    fi
+
+    printInfo "Running Java format ..."
     if command -v java >/dev/null & [ -f "$GOOGLE_JAVA_FORMAT_JAR" ]; then
        if ! git diff --diff-filter=ACRM --quiet --exit-code "$MERGEBASE" -- '*.java' &>/dev/null; then
             git diff --name-only --diff-filter=ACRM "$MERGEBASE" -- '*.java' | sed -E "\:$JAVA_EXCLUDES_REGEX:d" | xargs -P 5 java -jar "$GOOGLE_JAVA_FORMAT_JAR" -i
         fi
     fi
 
+    printInfo "Running shellcheck ..."
     if command -v shellcheck >/dev/null; then
         local shell_files non_shell_files
         non_shell_files=($(git diff --name-only --diff-filter=ACRM "$MERGEBASE" -- ':(exclude)*.sh'))
@@ -332,7 +376,7 @@ else
     fi
 
     # Only fetch master since that's the branch we're diffing against.
-    git fetch upstream master || true
+    git fetch --quiet upstream master || true
 
     # Format only the files that changed in last commit.
     format_changed
@@ -347,9 +391,7 @@ PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE:-python}
 $PYTHON_EXECUTABLE ci/travis/check_import_order.py . -s ci -s python/ray/thirdparty_files -s python/build -s lib
 
 if ! git diff --quiet &>/dev/null; then
-    echo 'Reformatted changed files. Please review and stage the changes.'
-    echo 'Files updated:'
-    echo
+    printWarning 'Found unstaged changes after lint, possibly from reformatting:'
 
     git --no-pager diff --name-only
 
