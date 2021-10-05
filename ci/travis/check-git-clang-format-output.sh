52e1a268385f316ca4c2138cd16efb82a6b4b2a5
Eric Liang <ekhliang@gmail.com>
2021-10-05 15:54:26 -0700

    Revert "[Lint] run `clang-tidy` in `scripts/format.h`, update clang-tidy rules (#19055)"
    This reverts commit 5d9e3a01218d951294cc94a4122e2bf737e4d824.

--- ci/travis/check-git-clang-format-output.sh
+++ ci/travis/check-git-clang-format-output.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 
-printWarning() {
-    printf '\033[31mLINT WARNING (clang-format):\033[0m %s\n' "$@"
-}
-
-printInfo() {
-    printf '\033[34mLINT (clang-format):\033[0m %s\n' "$@"
-}
-
 # Compare against the master branch, because most development is done against it.
 base_commit="$(git merge-base HEAD master)"
-if [ "$base_commit" = "$(git rev-parse HEAD)" ] && [ "$(git status --porcelain | wc -l)" -eq 0 ]; then
+if [ "$base_commit" = "$(git rev-parse HEAD)" ]; then
   # Prefix of master branch, so compare against parent commit
   base_commit="$(git rev-parse HEAD^)"
-  printInfo "Running clang-format against parent commit $base_commit"
+  echo "Running clang-format against parent commit $base_commit"
 else
-  printInfo "Running clang-format against commit $base_commit from master branch"
+  echo "Running clang-format against parent commit $base_commit from master branch"
 fi
 
 exclude_regex="(.*thirdparty/|.*redismodule.h|.*.java|.*.jsx?|.*.tsx?)"
 output="$(ci/travis/git-clang-format --commit "$base_commit" --diff --exclude "$exclude_regex")"
 if [ "$output" = "no modified files to format" ] || [ "$output" = "clang-format did not modify any files" ] ; then
-  printInfo "clang-format passed."
+  echo "clang-format passed."
   exit 0
 else
-  printWarning "clang-format failed:"
-  printWarning "$output"
+  echo "clang-format failed:"
+  echo "$output"
   exit 1
 fi
