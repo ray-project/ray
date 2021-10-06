bfa7ede924e067f18e0e75eda302418d20b8406f
sven1977 <svenmika1977@gmail.com>
2021-10-06 17:53:02 +0200

    wip

--- rllib/examples/mmo_mockup_example.py
+++ rllib/examples/mmo_mockup_example.py
@@ -20,7 +20,9 @@ def get_cli_args():
 
     # general args
     parser.add_argument(
-        "--run", default="PPO", help="The RLlib-registered algorithm to use.")
+        "--run",
+        default="APPO",
+        help="The RLlib-registered algorithm to use.")
     parser.add_argument("--num-cpus", type=int, default=3)
     parser.add_argument(
         "--framework",
@@ -29,6 +31,19 @@ def get_cli_args():
         help="The DL framework specifier.")
     parser.add_argument("--num-workers", type=int, default=2,
                         help="The number of workers to use.")
+    parser.add_argument("--num-envs-per-worker", type=int, default=20,
+                        help="The number of sub-env (vectorized) per worker.")
+    parser.add_argument(
+        "--max-episode-len",
+        type=int,
+        default=1000,
+        help="The maximum number of env steps an episode will take.")
+    parser.add_argument(
+        "--p-done",
+        type=float,
+        default=0.0,
+        help="The probability for terminating the ongoing episode "
+        "for each agent at each timestep. ")
     parser.add_argument(
         "--stop-iters",
         type=int,
@@ -84,25 +99,31 @@ if __name__ == "__main__":
                 # gym.spaces.Discrete(100),
             )),
             "action_space": gym.spaces.Discrete(10),
-            "max_episode_len": 10000,
-            "p_done": 0.0005,
+            "max_episode_len": args.max_episode_len,
+            "p_done": args.p_done,
         },
         "gamma": 0.99,
         # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
         "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", 0)),
         "num_workers": args.num_workers,
-        "num_envs_per_worker": 20,
+        "num_envs_per_worker": args.num_envs_per_worker,
 
         "entropy_coeff": 0.001,
-
-        # APPO settings.
-        "replay_proportion": 1.0,
-        "replay_buffer_num_slots": 50,
-        #"num_sgd_iter": 10,
-
         "vf_loss_coeff": 1e-5,
         "framework": args.framework,
     }
+
+    # APPO/IMPALA settings.
+    if args.run in ["APPO", "IMPALA"]:
+        config.update({
+            "replay_proportion": 1.0,
+            "replay_buffer_num_slots": 50,
+        })
+    # PPO settings.
+    elif args.run == "PPO":
+        config.update({
+            "num_sgd_iter": 10,
+        })
 
     stop = {
         "training_iteration": args.stop_iters,
