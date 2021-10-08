6f9a857855f7494f4bea0a12c9ac0b1a08a37842
Jun Gong <jungong@anyscale.com>
2021-10-08 14:35:35 -0700

    lint

--- src/ray/object_manager/plasma/store.cc
+++ src/ray/object_manager/plasma/store.cc
@@ -297,10 +297,9 @@ int PlasmaStore::AbortObject(const ObjectID &object_id,
 void PlasmaStore::ConnectClient(const boost::system::error_code &error) {
   if (!error) {
     // Accept a new local client and dispatch it to the node manager.
-    auto new_connection = Client::Create(
-        boost::bind(&PlasmaStore::ProcessMessage,
-		    this, ph::_1, ph::_2, ph::_3),  // NOLINT
-        std::move(socket_));
+    auto new_connection = Client::Create(boost::bind(&PlasmaStore::ProcessMessage, this,
+                                                     ph::_1, ph::_2, ph::_3),  // NOLINT
+                                         std::move(socket_));
   }
   // We're ready to accept another client.
   DoAccept();
