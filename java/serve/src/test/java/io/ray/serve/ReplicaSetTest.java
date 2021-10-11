5353c5c2f1238e3099489c2ace888129c417c996
liuyang-my <84125729+liuyang-my@users.noreply.github.com>
2021-10-11 14:39:04 +0800

    Define Java Proxy and RayServeHandle (#18630)

--- java/serve/src/test/java/io/ray/serve/ReplicaSetTest.java
+++ java/serve/src/test/java/io/ray/serve/ReplicaSetTest.java
@@ -4,37 +4,64 @@ import io.ray.api.ActorHandle;
 import io.ray.api.ObjectRef;
 import io.ray.api.Ray;
 import io.ray.runtime.serializer.MessagePackSerializer;
+import io.ray.serve.generated.ActorSet;
 import io.ray.serve.generated.BackendConfig;
 import io.ray.serve.generated.BackendLanguage;
 import io.ray.serve.generated.BackendVersion;
 import io.ray.serve.generated.RequestMetadata;
-import io.ray.serve.generated.RequestWrapper;
-import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class RayServeReplicaTest {
+public class ReplicaSetTest {
+
+  private String backendTag = "ReplicaSetTest";
+
+  @Test
+  public void setMaxConcurrentQueriesTest() {
+    ReplicaSet replicaSet = new ReplicaSet(backendTag);
+    BackendConfig.Builder builder = BackendConfig.newBuilder();
+    builder.setMaxConcurrentQueries(200);
+
+    replicaSet.setMaxConcurrentQueries(builder.build());
+    Assert.assertEquals(replicaSet.getMaxConcurrentQueries(), 200);
+  }
+
+  @Test
+  public void updateWorkerReplicasTest() {
+    ReplicaSet replicaSet = new ReplicaSet(backendTag);
+    ActorSet.Builder builder = ActorSet.newBuilder();
+
+    replicaSet.updateWorkerReplicas(builder.build());
+    Map<ActorHandle<RayServeWrappedReplica>, Set<ObjectRef<Object>>> inFlightQueries =
+        replicaSet.getInFlightQueries();
+    Assert.assertTrue(inFlightQueries.isEmpty());
+  }
 
   @SuppressWarnings("unused")
   @Test
-  public void test() throws IOException {
+  public void assignReplicaTest() {
     boolean inited = Ray.isInitialized();
     Ray.init();
 
     try {
-      String controllerName = "RayServeReplicaTest";
-      String backendTag = "b_tag";
-      String replicaTag = "r_tag";
+      String controllerName = backendTag + "_controller";
+      String replicaTag = backendTag + "_replica";
+      String actorName = replicaTag;
       String version = "v1";
 
+      // Controller
       ActorHandle<DummyServeController> controllerHandle =
           Ray.actor(DummyServeController::new).setName(controllerName).remote();
 
+      // Replica
       BackendConfig.Builder backendConfigBuilder = BackendConfig.newBuilder();
       backendConfigBuilder.setBackendLanguage(BackendLanguage.JAVA);
       byte[] backendConfigBytes = backendConfigBuilder.build().toByteArray();
+
       Object[] initArgs = new Object[] {backendTag, replicaTag, controllerName, new Object()};
       byte[] initArgsBytes = MessagePackSerializer.encode(initArgs).getLeft();
 
@@ -45,47 +72,33 @@ public class RayServeReplicaTest {
       deploymentInfo.setReplicaConfig(
           new ReplicaConfig("io.ray.serve.ReplicaContext", initArgsBytes, new HashMap<>()));
 
-      ActorHandle<RayServeWrappedReplica> backendHandle =
+      ActorHandle<RayServeWrappedReplica> replicaHandle =
           Ray.actor(
                   RayServeWrappedReplica::new,
                   backendTag,
                   replicaTag,
                   deploymentInfo,
                   controllerName)
+              .setName(actorName)
               .remote();
+      replicaHandle.task(RayServeWrappedReplica::ready).remote();
 
-      // ready
-      backendHandle.task(RayServeWrappedReplica::ready).remote();
+      // ReplicaSet
+      ReplicaSet replicaSet = new ReplicaSet(backendTag);
+      ActorSet.Builder builder = ActorSet.newBuilder();
+      builder.addNames(actorName);
+      replicaSet.updateWorkerReplicas(builder.build());
 
-      // handle request
+      // assign
+
       RequestMetadata.Builder requestMetadata = RequestMetadata.newBuilder();
       requestMetadata.setRequestId(RandomStringUtils.randomAlphabetic(10));
       requestMetadata.setCallMethod("getBackendTag");
-      RequestWrapper.Builder requestWrapper = RequestWrapper.newBuilder();
 
-      ObjectRef<Object> resultRef =
-          backendHandle
-              .task(
-                  RayServeWrappedReplica::handleRequest,
-                  requestMetadata.build().toByteArray(),
-                  requestWrapper.build().toByteArray())
-              .remote();
+      Query query = new Query(requestMetadata.build(), null);
+      ObjectRef<Object> resultRef = replicaSet.assignReplica(query);
+
       Assert.assertEquals((String) resultRef.get(), backendTag);
-
-      // reconfigure
-      ObjectRef<byte[]> versionRef =
-          backendHandle.task(RayServeWrappedReplica::reconfigure, (Object) null).remote();
-      Assert.assertEquals(BackendVersion.parseFrom(versionRef.get()).getCodeVersion(), version);
-
-      // get version
-      versionRef = backendHandle.task(RayServeWrappedReplica::getVersion).remote();
-      Assert.assertEquals(BackendVersion.parseFrom(versionRef.get()).getCodeVersion(), version);
-
-      // prepare for shutdown
-      ObjectRef<Boolean> shutdownRef =
-          backendHandle.task(RayServeWrappedReplica::prepareForShutdown).remote();
-      Assert.assertTrue(shutdownRef.get());
-
     } finally {
       if (!inited) {
         Ray.shutdown();
--- java/serve/src/test/java/io/ray/serve/RouterTest.java
+++ java/serve/src/test/java/io/ray/serve/RouterTest.java
@@ -4,37 +4,39 @@ import io.ray.api.ActorHandle;
 import io.ray.api.ObjectRef;
 import io.ray.api.Ray;
 import io.ray.runtime.serializer.MessagePackSerializer;
+import io.ray.serve.generated.ActorSet;
 import io.ray.serve.generated.BackendConfig;
 import io.ray.serve.generated.BackendLanguage;
 import io.ray.serve.generated.BackendVersion;
 import io.ray.serve.generated.RequestMetadata;
-import io.ray.serve.generated.RequestWrapper;
-import java.io.IOException;
 import java.util.HashMap;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class RayServeReplicaTest {
+public class RouterTest {
 
-  @SuppressWarnings("unused")
   @Test
-  public void test() throws IOException {
+  public void test() {
     boolean inited = Ray.isInitialized();
     Ray.init();
 
     try {
-      String controllerName = "RayServeReplicaTest";
-      String backendTag = "b_tag";
-      String replicaTag = "r_tag";
+      String backendTag = "RouterTest";
+      String controllerName = backendTag + "_controller";
+      String replicaTag = backendTag + "_replica";
+      String actorName = replicaTag;
       String version = "v1";
 
+      // Controller
       ActorHandle<DummyServeController> controllerHandle =
           Ray.actor(DummyServeController::new).setName(controllerName).remote();
 
+      // Replica
       BackendConfig.Builder backendConfigBuilder = BackendConfig.newBuilder();
       backendConfigBuilder.setBackendLanguage(BackendLanguage.JAVA);
       byte[] backendConfigBytes = backendConfigBuilder.build().toByteArray();
+
       Object[] initArgs = new Object[] {backendTag, replicaTag, controllerName, new Object()};
       byte[] initArgsBytes = MessagePackSerializer.encode(initArgs).getLeft();
 
@@ -45,47 +47,30 @@ public class RayServeReplicaTest {
       deploymentInfo.setReplicaConfig(
           new ReplicaConfig("io.ray.serve.ReplicaContext", initArgsBytes, new HashMap<>()));
 
-      ActorHandle<RayServeWrappedReplica> backendHandle =
+      ActorHandle<RayServeWrappedReplica> replicaHandle =
           Ray.actor(
                   RayServeWrappedReplica::new,
                   backendTag,
                   replicaTag,
                   deploymentInfo,
                   controllerName)
+              .setName(actorName)
               .remote();
+      replicaHandle.task(RayServeWrappedReplica::ready).remote();
 
-      // ready
-      backendHandle.task(RayServeWrappedReplica::ready).remote();
+      // Router
+      Router router = new Router(controllerHandle, backendTag);
+      ActorSet.Builder builder = ActorSet.newBuilder();
+      builder.addNames(actorName);
+      router.getReplicaSet().updateWorkerReplicas(builder.build());
 
-      // handle request
+      // assign
       RequestMetadata.Builder requestMetadata = RequestMetadata.newBuilder();
       requestMetadata.setRequestId(RandomStringUtils.randomAlphabetic(10));
       requestMetadata.setCallMethod("getBackendTag");
-      RequestWrapper.Builder requestWrapper = RequestWrapper.newBuilder();
 
-      ObjectRef<Object> resultRef =
-          backendHandle
-              .task(
-                  RayServeWrappedReplica::handleRequest,
-                  requestMetadata.build().toByteArray(),
-                  requestWrapper.build().toByteArray())
-              .remote();
+      ObjectRef<Object> resultRef = router.assignRequest(requestMetadata.build(), null);
       Assert.assertEquals((String) resultRef.get(), backendTag);
-
-      // reconfigure
-      ObjectRef<byte[]> versionRef =
-          backendHandle.task(RayServeWrappedReplica::reconfigure, (Object) null).remote();
-      Assert.assertEquals(BackendVersion.parseFrom(versionRef.get()).getCodeVersion(), version);
-
-      // get version
-      versionRef = backendHandle.task(RayServeWrappedReplica::getVersion).remote();
-      Assert.assertEquals(BackendVersion.parseFrom(versionRef.get()).getCodeVersion(), version);
-
-      // prepare for shutdown
-      ObjectRef<Boolean> shutdownRef =
-          backendHandle.task(RayServeWrappedReplica::prepareForShutdown).remote();
-      Assert.assertTrue(shutdownRef.get());
-
     } finally {
       if (!inited) {
         Ray.shutdown();
