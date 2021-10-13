da3c148f00b9e562aec28f477623ce93fe65587c
Rohan138 <rohanpotdar138@gmail.com>
2021-10-13 00:11:04 -0400

    Added comments

--- rllib/examples/pettingzoo_env.py
+++ rllib/examples/pettingzoo_env.py
@@ -2,11 +2,14 @@ from ray import tune
 from ray.rllib.models import ModelCatalog
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
 from ray.tune.registry import register_env
+
 # from ray.rllib.utils import try_import_tf
 from ray.rllib.env.wrappers.pettingzoo_env import ParallelPettingZooEnv
 from pettingzoo.butterfly import pistonball_v4
 import supersuit as ss
 from torch import nn
+import os
+import argparse
 
 # This tutorial uses the PettingZoo (https://www.pettingzoo.ml/)
 # Multi-Agent Environment API with RLlib.
@@ -42,6 +45,8 @@ class CNNModelV2(TorchModelV2, nn.Module):
         return self._value_out.flatten()
 
 
+# Function that outputs the environment you want to register
+# We use SuperSuit to wrap the environment and preprocess observations
 def env_creator(args):
     env = pistonball_v4.parallel_env(
         n_pistons=20,
@@ -63,71 +68,100 @@ def env_creator(args):
     return env
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--framework",
+    choices=["tf", "tf2", "tfe", "torch"],
+    default="torch",
+    help="The DL framework specifier.")
+parser.add_argument(
+    "--timesteps",
+    type=int,
+    default=100000,
+    help="Number of timesteps to train.")
+parser.add_argument(
+    "--checkpoint-freq",
+    type=int,
+    default=100,
+    help="Checkpoint frequency during learning.")
+parser.add_argument(
+    "--log-level",
+    choices=["ERROR", "WARNING", "INFO", "DEBUG"],
+    default="ERROR",
+    help="Which messages to log.")
+parser.add_argument(
+    "--num-workers",
+    type=int,
+    default=8,
+    help="Number of RLlib workers during training.")
+parser.add_argument(
+    "--num-envs-per-workers",
+    type=int,
+    default=1,
+    help="Number of environments per RLlib worker.")
+
 if __name__ == "__main__":
+    args = parser.parse_args()
     env_name = "pistonball_v4"
 
     register_env(env_name,
                  lambda config: ParallelPettingZooEnv(env_creator(config)))
 
-    test_env = ParallelPettingZooEnv(env_creator({}))
-    obs_space = test_env.observation_space
-    act_space = test_env.action_space
-
     ModelCatalog.register_custom_model("CNNModelV2", CNNModelV2)
-
-    def gen_policy(i):
-        config = {
-            "model": {
-                "custom_model": "CNNModelV2",
-            },
-            "gamma": 0.99,
-        }
-        return (None, obs_space, act_space, config)
-
-    policies = {"policy_0": gen_policy(0)}
-
-    policy_ids = list(policies.keys())
 
     tune.run(
         "PPO",
-        name="PPO",
-        stop={"timesteps_total": 5000000},
-        checkpoint_freq=10,
+        name="PPO_pistonball",
+        stop={"timesteps_total": args.timesteps},
+        checkpoint_freq=args.checkpoint_freq,
         local_dir="~/ray_results/" + env_name,
         config={
             # Environment specific
             "env": env_name,
             # General
-            "log_level": "ERROR",
-            "framework": "torch",
-            "num_gpus": 1,
-            "num_workers": 1,
-            "num_envs_per_worker": 1,
-            "compress_observations": False,
+            "log_level": args.log_level,
+            "framework": args.framework,
+            # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+            "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
+            "num_workers": args.num_workers,
+            "num_envs_per_worker": args.num_envs_per_workers,
+            "compress_observations": True,
             "batch_mode": "truncate_episodes",
-
+            # Fragment length, collected from each worker and for each agent
+            "rollout_fragment_length": 64,
+            # train batch size, fragments are concatenated up to this point
+            "train_batch_size": 1024,
+            # Force reset environment after this many steps
+            "horizon": 125,
+            # minibatch size for PPO to optimize over
+            "sgd_minibatch_size": 64,
+            # number of optimization iterations over each minibatch
+            "num_sgd_iter": 10,
+            "lr": 5e-4,
+            "model": {
+                "custom_model": "CNNModelV2",
+            },
             # "use_critic": True,
             "use_gae": True,
             "lambda": 0.9,
-            "gamma": .99,
-
+            "gamma": 0.99,
             # "kl_coeff": 0.001,
             # "kl_target": 1000.,
             "clip_param": 0.4,
             "grad_clip": None,
             "entropy_coeff": 0.1,
             "vf_loss_coeff": 0.25,
-            "sgd_minibatch_size": 64,
-            "num_sgd_iter": 10,  # epoc
-            "rollout_fragment_length": 512,
-            "train_batch_size": 512,
-            "lr": 2e-05,
             "clip_actions": True,
-
-            # Method specific
+            # Configuration for multiagent setup with policy sharing:
             "multiagent": {
-                "policies": policies,
-                "policy_mapping_fn": (lambda agent_id: policy_ids[0]),
+                # Setup a single, shared policy for all agents: "policy_0".
+                # Use a simple set of strings (PolicyID) here. RLlib will
+                # automatically determine the policy class
+                # (Trainer's default class), observation and action spaces
+                # (inferred from the env), and config overrides ({} here)
+                "policies": {"policy_0"},
+                # Map all agents to the "policy_0" PolicyID.
+                "policy_mapping_fn": lambda agent_id: "policy_0",
             },
         },
     )
