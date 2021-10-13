6e69b38c0f44ff27b82150b96f7c6e7e9b4b39aa
Carlo Grisetti <carlo.grisetti@dsgroup.it>
2021-10-13 03:04:43 +0200

    Refactor code to deduplicate

--- python/ray/_private/runtime_env/context.py
+++ python/ray/_private/runtime_env/context.py
@@ -37,21 +37,27 @@ class RuntimeEnvContext:
 
     def exec_worker(self, passthrough_args: List[str], language: Language):
         os.environ.update(self.env_vars)
-        if language == Language.PYTHON:
-            executable = f"{self.py_executable}"
-        else:
-            executable = ""
 
-        # We here need to distinguish between Windows process execution and
-        # Linux process execution. Default behavior is to use 'bash' in any
-        # case except when running on Windows
+        # We need to distinguish between Windows process execution and Linux
+        # process execution.
         if sys.platform == "win32":
-            exec_command = " ".join([f'"{executable}"'] + passthrough_args)
-            command_str = " && ".join(self.command_prefix + [exec_command])
+            executable = ""
+        else:
+            executable = "exec "
+
+        # Add the python interpreter if the language is set to Python
+        if language == Language.PYTHON:
+            executable += f"{self.py_executable}"
+
+        # And then compose the command string to be executed
+        exec_command = " ".join([f'"{executable}"'] + passthrough_args)
+        command_str = " && ".join(self.command_prefix + [exec_command])
+
+        # Use the appropriate interpreter. Default behavior is to use 'bash'
+        # in any case except when running on Windows
+        if sys.platform == "win32":
             os.system(command_str)
         else:
-            exec_command = " ".join([f"exec {executable}"] + passthrough_args)
-            command_str = " && ".join(self.command_prefix + [exec_command])
             os.execvp(file="bash", args=["bash", "-c", command_str])
 
         logger.info(f"Exec'ing worker with command: {command_str}")
