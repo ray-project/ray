ray-project/ray

commit 6252a6c1f93851c2daf730fab94b8c399191cd0e

[tune] Force no result buffering for hyperband schedulers (#19140)

python/ray/tune/tune.py
@@ -384,8 +384,34 @@ def run(
     if num_samples == -1:
         num_samples = sys.maxsize
 
+    result_buffer_length = None
+
+    # Create scheduler here as we need access to some of its properties
+    if isinstance(scheduler, str):
+        # importing at top level causes a recursive dependency
+        from ray.tune.schedulers import create_scheduler
+        scheduler = create_scheduler(scheduler)
+    scheduler = scheduler or FIFOScheduler()
+
+    if scheduler.supports_buffered_results:
+        # Result buffering with a Hyperband scheduler is a bad idea, as
+        # hyperband tries to stop trials when processing brackets. With result
+        # buffering, we might trigger this multiple times when evaluating
+        # a single trial, which leads to unexpected behavior.
+        env_result_buffer_length = os.getenv("TUNE_RESULT_BUFFER_LENGTH", "")
+        if env_result_buffer_length:
+            warnings.warn(
+                f"You are using a {type(scheduler)} scheduler, but "
+                f"TUNE_RESULT_BUFFER_LENGTH is set "
+                f"({env_result_buffer_length}). This can lead to undesired "
+                f"and faulty behavior, so the buffer length was forcibly set "
+                f"to 1 instead.")
+        result_buffer_length = 1
+
     trial_executor = trial_executor or RayTrialExecutor(
-        reuse_actors=reuse_actors, queue_trials=queue_trials)
+        reuse_actors=reuse_actors,
+        queue_trials=queue_trials,
+        result_buffer_length=result_buffer_length)
     if isinstance(run_or_experiment, list):
         experiments = run_or_experiment
     else:
@@ -438,11 +464,6 @@ def run(
     if is_local_mode:
         max_concurrent_trials = 1
 
-    if isinstance(scheduler, str):
-        # importing at top level causes a recursive dependency
-        from ray.tune.schedulers import create_scheduler
-        scheduler = create_scheduler(scheduler)
-
     if not search_alg:
         search_alg = BasicVariantGenerator(
             max_concurrent=max_concurrent_trials or 0)
@@ -490,7 +511,6 @@ def run(
                 "does not contain any more parameter definitions - include "
                 "them in the search algorithm's search space if necessary.")
 
-    scheduler = scheduler or FIFOScheduler()
     if not scheduler.set_search_properties(metric, mode):
         raise ValueError(
             "You passed a `metric` or `mode` argument to `tune.run()`, but "

python/ray/tune/schedulers/hyperband.py
@@ -80,6 +80,8 @@ class HyperBandScheduler(FIFOScheduler):
             reaching max_t. Defaults to True.
     """
 
+    _supports_buffered_results = False
+
     def __init__(self,
                  time_attr: str = "training_iteration",
                  metric: Optional[str] = None,

python/ray/tune/schedulers/trial_scheduler.py
@@ -14,9 +14,15 @@ class TrialScheduler:
 
     _metric = None
 
+    _supports_buffered_results = True
+
     @property
     def metric(self):
         return self._metric
+
+    @property
+    def supports_buffered_results(self):
+        return self._supports_buffered_results
 
     def set_search_properties(self, metric: Optional[str],
                               mode: Optional[str]) -> bool:

python/ray/tune/suggest/bohb.py
@@ -235,10 +235,10 @@ class TuneBOHB(Searcher):
 
     def on_pause(self, trial_id: str):
         self.paused.add(trial_id)
-        self.running.remove(trial_id)
+        self.running.discard(trial_id)
 
     def on_unpause(self, trial_id: str):
-        self.paused.remove(trial_id)
+        self.paused.discard(trial_id)
         self.running.add(trial_id)
 
     @staticmethod

commit 29aa6f84fb0d4a03b3b05c371eeb56edd19e4893

[tune] Fix result buffering case check (fixes bug introduced in #19140)

python/ray/tune/tune.py
@@ -393,8 +393,8 @@ def run(
         scheduler = create_scheduler(scheduler)
     scheduler = scheduler or FIFOScheduler()
 
-    if scheduler.supports_buffered_results:
-        # Result buffering with a Hyperband scheduler is a bad idea, as
+    if not scheduler.supports_buffered_results:
+        # Result buffering with e.g. a Hyperband scheduler is a bad idea, as
         # hyperband tries to stop trials when processing brackets. With result
         # buffering, we might trigger this multiple times when evaluating
         # a single trial, which leads to unexpected behavior.