62467df33c5a0c4d767013e4965eabe100377581
Siyuan <suquark@gmail.com>
2021-10-13 20:32:26 -0700

    update cloudpickle to v2.0.0

--- python/ray/cloudpickle/__init__.py
+++ python/ray/cloudpickle/__init__.py
@@ -1,17 +1,14 @@
 from __future__ import absolute_import
 
-import os
-from pickle import PicklingError
 
-from ray.cloudpickle.cloudpickle import *  # noqa
-from ray.cloudpickle.cloudpickle_fast import CloudPickler, dumps, dump  # noqa
-
+from cloudpickle.cloudpickle import *  # noqa
+from cloudpickle.cloudpickle_fast import CloudPickler, dumps, dump  # noqa
 
 # Conform to the convention used by python serialization libraries, which
 # expose their Pickler subclass at top-level under the  "Pickler" name.
 Pickler = CloudPickler
 
-__version__ = "1.6.0"
+__version__ = '2.0.0'
 
 
 def _warn_msg(obj, method, exc):
