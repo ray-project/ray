54de24eb60427a5a34ddae34f2a1f038fe522ff0
Siyuan <suquark@gmail.com>
2021-10-14 20:17:48 -0700

    fix

--- python/ray/cloudpickle/__init__.py
+++ python/ray/cloudpickle/__init__.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import
 
+import os
+from pickle import PicklingError
 
-from cloudpickle.cloudpickle import *  # noqa
-from cloudpickle.cloudpickle_fast import CloudPickler, dumps, dump  # noqa
+from ray.cloudpickle.cloudpickle import *  # noqa
+from ray.cloudpickle.cloudpickle_fast import CloudPickler, dumps, dump  # noqa
 
 # Conform to the convention used by python serialization libraries, which
 # expose their Pickler subclass at top-level under the  "Pickler" name.
