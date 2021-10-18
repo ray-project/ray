a78c1f522fa1fcc7bd7e630c704a2f28be7a6808
qicosmos <qicosmos@163.com>
2021-10-18 21:06:41 +0800

    lint

--- cpp/src/ray/test/benchmark/benchmark.cc
+++ cpp/src/ray/test/benchmark/benchmark.cc
@@ -12,12 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "ray/api.h"
-
 #include <chrono>
 #include <set>
 
 #include "gtest/gtest.h"
+#include "ray/api.h"
 
 using namespace ::ray;
 
