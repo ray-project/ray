1e00213a5dd3287533149aa886fd05586cddb736
Matthew Deng <matthew.j.deng@gmail.com>
2021-10-18 17:37:56 -0700

    sgd -> train

--- ci/travis/determine_tests_to_run.py
+++ ci/travis/determine_tests_to_run.py
@@ -111,7 +111,7 @@ if __name__ == "__main__":
                 RAY_CI_SGD_AFFECTED = 1
                 RAY_CI_LINUX_WHEELS_AFFECTED = 1
                 RAY_CI_MACOS_WHEELS_AFFECTED = 1
-            elif changed_file.startswith("python/ray/sgd"):
+            elif changed_file.startswith("python/ray/train"):
                 RAY_CI_TRAIN_AFFECTED = 1
                 RAY_CI_LINUX_WHEELS_AFFECTED = 1
                 RAY_CI_MACOS_WHEELS_AFFECTED = 1
