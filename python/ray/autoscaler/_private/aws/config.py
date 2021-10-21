2129fd5cdfa6ba576a411749d2a7ca4b7d3869fa
Xiayue Charles Lin <xcl@anyscale.com>
2021-10-21 16:32:14 -0700

    Running lint.

--- python/ray/autoscaler/_private/aws/config.py
+++ python/ray/autoscaler/_private/aws/config.py
@@ -405,15 +405,15 @@ def _configure_subnet(config):
     # for all node types. (It is a user error if explicitly specified
     # subnets are invalid.)
     need_to_configure_subnets = any(
-        "SubnetIds" not in node_type_config["node_config"] 
-        for node_type_config in config["available_node_types"].values()
-    )
+        "SubnetIds" not in node_type_config["node_config"]
+        for node_type_config in config["available_node_types"].values())
 
     if not need_to_configure_subnets:
-        _set_config_info(subnet_src={
-            node_type: "config"
-            for node_type in config["available_node_types"].keys()
-        })
+        _set_config_info(
+            subnet_src={
+                node_type: "config"
+                for node_type in config["available_node_types"].keys()
+            })
         return config
 
     ec2 = _resource("ec2", config)
