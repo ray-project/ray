d375c9487eebc4a308465d487e67062661a00539
Eric Liang <ekhliang@gmail.com>
2021-10-25 12:32:35 -0700

    lint

--- python/ray/data/impl/size_estimator.py
+++ python/ray/data/impl/size_estimator.py
@@ -35,26 +35,26 @@ class SizeEstimator:
 # Adapted from the RLlib MeanStdFilter.
 class RunningMean:
     def __init__(self):
-        self._n = 0
-        self._M = 0
+        self._weight = 0
+        self._mean = 0
 
     def add(self, x: int, weight: int = 1) -> None:
         if weight == 0:
             return
-        n1 = self._n
+        n1 = self._weight
         n2 = weight
         n = n1 + n2
-        M = (n1 * self._M + n2 * x) / n
-        self._n = n
-        self._M = M
+        M = (n1 * self._mean + n2 * x) / n
+        self._weight = n
+        self._mean = M
 
     @property
     def n(self) -> int:
-        return self._n
+        return self._weight
 
     @property
     def mean(self) -> float:
-        return self._M
+        return self._mean
 
     def __repr__(self):
         return "(n={}, mean={})".format(self.n, self.mean)
