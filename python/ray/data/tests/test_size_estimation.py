a470f7073a44bc50e9df57bafddc6e66290f2ffe
Eric Liang <ekhliang@gmail.com>
2021-10-24 22:37:19 -0700

    update

--- python/ray/data/tests/test_size_estimation.py
+++ python/ray/data/tests/test_size_estimation.py
@@ -10,7 +10,7 @@ ARROW_SMALL_VALUE = {"value": "a" * 100}
 ARROW_LARGE_VALUE = {"value": "a" * 10000}
 
 
-def assert_close(actual, expected, tolerance=.5):
+def assert_close(actual, expected, tolerance=.3):
     print("assert_close", actual, expected)
     assert abs(actual - expected) / expected < tolerance, \
         (actual, expected)
@@ -39,37 +39,37 @@ def test_py_size_diff_values(ray_start_regular_shared):
     assert b.get_estimated_memory_usage() == 0
     for _ in range(10):
         b.add(LARGE_VALUE)
-    assert_close(b.get_estimated_memory_usage(), 100000)
+    assert_close(b.get_estimated_memory_usage(), 100120)
     for _ in range(100):
         b.add(SMALL_VALUE)
-    assert_close(b.get_estimated_memory_usage(), 110000)
+    assert_close(b.get_estimated_memory_usage(), 121120)
     for _ in range(100):
         b.add(LARGE_VALUE)
-    assert_close(b.get_estimated_memory_usage(), 1110000)
+    assert_close(b.get_estimated_memory_usage(), 1166875)
     for _ in range(100):
         b.add(LARGE_VALUE)
-    assert_close(b.get_estimated_memory_usage(), 2110000)
-    b.add_block([SMALL_VALUE] * 10000)
-    assert_close(b.get_estimated_memory_usage(), 2210000)
-    assert len(b.build()) == 10310
+    assert_close(b.get_estimated_memory_usage(), 2182927)
+    b.add_block([SMALL_VALUE] * 1000)
+    assert_close(b.get_estimated_memory_usage(), 2240613)
+    assert len(b.build()) == 1310
 
 
 def test_arrow_size(ray_start_regular_shared):
     b = ArrowBlockBuilder()
     assert b.get_estimated_memory_usage() == 0
     b.add(ARROW_SMALL_VALUE)
-    assert_close(b.get_estimated_memory_usage(), 111)
+    assert_close(b.get_estimated_memory_usage(), 118)
     b.add(ARROW_SMALL_VALUE)
-    assert_close(b.get_estimated_memory_usage(), 222)
+    assert_close(b.get_estimated_memory_usage(), 236)
     for _ in range(8):
         b.add(ARROW_SMALL_VALUE)
-    assert_close(b.get_estimated_memory_usage(), 1110)
+    assert_close(b.get_estimated_memory_usage(), 1180)
     for _ in range(90):
         b.add(ARROW_SMALL_VALUE)
-    assert_close(b.get_estimated_memory_usage(), 11100)
+    assert_close(b.get_estimated_memory_usage(), 11800)
     for _ in range(900):
         b.add(ARROW_SMALL_VALUE)
-    assert_close(b.get_estimated_memory_usage(), 111000)
+    assert_close(b.get_estimated_memory_usage(), 118000)
     assert b.build().num_rows == 1000
 
 
@@ -78,24 +78,24 @@ def test_arrow_size_diff_values(ray_start_regular_shared):
     assert b.get_estimated_memory_usage() == 0
     b.add(ARROW_LARGE_VALUE)
     assert b._num_compactions == 0
-    assert_close(b.get_estimated_memory_usage(), 10000)
+    assert_close(b.get_estimated_memory_usage(), 10019)
     b.add(ARROW_LARGE_VALUE)
     assert b._num_compactions == 0
-    assert_close(b.get_estimated_memory_usage(), 20000)
+    assert_close(b.get_estimated_memory_usage(), 20038)
     for _ in range(10):
         b.add(ARROW_SMALL_VALUE)
-    assert_close(b.get_estimated_memory_usage(), 20000)
+    assert_close(b.get_estimated_memory_usage(), 25178)
     for _ in range(100):
         b.add(ARROW_SMALL_VALUE)
     assert b._num_compactions == 0
-    assert_close(b.get_estimated_memory_usage(), 30000)
+    assert_close(b.get_estimated_memory_usage(), 35394)
     for _ in range(13000):
         b.add(ARROW_LARGE_VALUE)
-    assert_close(b.get_estimated_memory_usage(), 130200000)
+    assert_close(b.get_estimated_memory_usage(), 130131680)
     assert b._num_compactions == 2
     for _ in range(4000):
         b.add(ARROW_LARGE_VALUE)
-    assert_close(b.get_estimated_memory_usage(), 170200000)
+    assert_close(b.get_estimated_memory_usage(), 170129189)
     assert b._num_compactions == 3
     assert b.build().num_rows == 17112
 
@@ -109,7 +109,7 @@ def test_arrow_size_add_block(ray_start_regular_shared):
     for _ in range(5):
         b2.add_block(block)
     assert b2._num_compactions == 0
-    assert_close(b2.get_estimated_memory_usage(), 100000000)
+    assert_close(b2.get_estimated_memory_usage(), 100040020)
     assert b2.build().num_rows == 10000
 
 
