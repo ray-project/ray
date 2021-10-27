45a3dc99b8b6cc8008df43e04c9f4f1989e6d98d
Jun Gong <jungong@anyscale.com>
2021-10-27 14:07:51 -0700

    Add ray.init()

--- release/rllib_tests/connect_tests/run_connect_tests.py
+++ release/rllib_tests/connect_tests/run_connect_tests.py
@@ -6,9 +6,17 @@ Runs a couple of hard learning tests using Anyscale connect.
 import json
 import os
 
+import ray
 from ray.rllib.examples.tune_framework import run
 
 if __name__ == "__main__":
+    addr = os.environ.get("RAY_ADDRESS")
+    job_name = os.environ.get("RAY_JOB_NAME", "rllib_connect_tests")
+    if addr is not None and addr.startswith("anyscale://"):
+        ray.init(address=addr, job_name=job_name)
+    else:
+        ray.init(address="auto")
+
     results = run()
 
     test_output_json = os.environ.get("TEST_OUTPUT_JSON",
