72c1b7b1d906813ee9f2955d7d1ba4fae217b05b
SangBin Cho <rkooo567@gmail.com>
2021-10-26 22:47:05 -0700

    lint

--- src/ray/raylet/test/util.h
+++ src/ray/raylet/test/util.h
@@ -126,7 +126,7 @@ class MockWorker : public WorkerInterface {
     RAY_CHECK(false) << "Method unused";
     return address_;
   }
-  
+
   void DirectActorCallArgWaitComplete(int64_t tag) {
     RAY_CHECK(false) << "Method unused";
   }
