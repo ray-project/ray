9af088a3ee024122c0f66e26d2faf4b340bd4797
Eric Liang <ekhliang@gmail.com>
2021-10-29 16:32:31 -0700

    wip

---
+++ python/ray/data/context.py
@@ -0,0 +1,56 @@
+import ray
+
+from ray.util.annotations import DeveloperAPI
+
+
+# The context singleton on this process.
+_default_context = None
+
+
+@DeveloperAPI
+class DatasetContext:
+    """Singleton for shared Dataset resources and configurations.
+
+    This object is automatically propagated to workers and can be retrieved
+    from the driver and remote workers via DatasetContext.get_instance().
+    """
+
+    def __init__(
+            self,
+            block_owner: ray.actor.ActorHandle,
+            target_max_block_size: int):
+        """Private constructor (use get_instance() instead)."""
+        self.block_owner = block_owner
+        self.target_max_block_size = target_max_block_size
+
+    @staticmethod
+    def get_instance():
+        global _default_context
+        if _default_context is None:
+            _default_context = DatasetContext(None, 500 * 1024 * 1024)
+        return _default_context
+
+    @staticmethod
+    def _set_current(context: DatasetContext) -> None:
+        global default_context = context
+
+
+@ray.remote(num_cpus=0, placement_group=None)
+class _DesignatedBlockOwner:
+    def ping(self):
+        return "ok"
+
+
+# We manually assign the owner so ray.put() blocks do not fate share with the
+# worker that created them.
+# TODO(ekl) remove this once we implement automatic ownership transfer.
+def _get_or_create_block_owner_actor() -> ray.actor.ActorHandle:
+    name = "datasets_global_block_owner"
+    namespace = "datasets_global_namespace"
+    try:
+        actor = ray.get_actor(name=name, namespace=namespace)
+    except ValueError:
+        actor = _DesignatedBlockOwner.options(
+            name=name, namespace=namespace, lifetime="detached").remote()
+    ray.get(actor.ping.remote())
+    return actor
--- python/ray/data/dataset.py
+++ python/ray/data/dataset.py
@@ -25,6 +25,7 @@ from ray.types import ObjectRef
 from ray.util.annotations import DeveloperAPI, PublicAPI
 from ray.data.block import Block, BlockAccessor, BlockMetadata, T, U, \
     BlockPartition, BlockPartitionMetadata
+from ray.data.context import DatasetContext
 from ray.data.datasource import (
     Datasource, CSVDatasource, JSONDatasource, NumpyDatasource,
     ParquetDatasource, BlockWritePathProvider, DefaultBlockWritePathProvider)
@@ -120,8 +121,10 @@ class Dataset(Generic[T]):
         """
 
         fn = cache_wrapper(fn)
+        context = DatasetContext.get_instance()
 
         def transform(block: Block) -> Block:
+            DatasetContext._set_current(context)
             block = BlockAccessor.for_block(block)
             builder = DelegatingArrowBlockBuilder()
             for row in block.iter_rows():
@@ -185,8 +188,10 @@ class Dataset(Generic[T]):
         import pandas as pd
 
         fn = cache_wrapper(fn)
+        context = DatasetContext.get_instance()
 
         def transform(block: Block) -> Block:
+            DatasetContext._set_current(context)
             block = BlockAccessor.for_block(block)
             total_rows = block.num_rows()
             max_batch_size = batch_size
@@ -255,8 +260,10 @@ class Dataset(Generic[T]):
         """
 
         fn = cache_wrapper(fn)
+        context = DatasetContext.get_instance()
 
         def transform(block: Block) -> Block:
+            DatasetContext._set_current(context)
             block = BlockAccessor.for_block(block)
             builder = DelegatingArrowBlockBuilder()
             for row in block.iter_rows():
@@ -295,8 +302,10 @@ class Dataset(Generic[T]):
         """
 
         fn = cache_wrapper(fn)
+        context = DatasetContext.get_instance()
 
         def transform(block: Block) -> Block:
+            DatasetContext._set_current(context)
             block = BlockAccessor.for_block(block)
             builder = block.builder()
             for row in block.iter_rows():
