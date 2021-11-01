ccc8b0b5e53a0848f417aafea81006a882990d0c
Mingwei Tian <mwtian@anyscale.com>
2021-11-01 14:14:25 -0700

    enable

--- src/ray/common/ray_config_def.h
+++ src/ray/common/ray_config_def.h
@@ -250,7 +250,7 @@ RAY_CONFIG(uint64_t, gcs_max_concurrent_resource_pulls, 100)
 // TODO(ekl) broken as of https://github.com/ray-project/ray/issues/16858
 RAY_CONFIG(bool, grpc_based_resource_broadcast, false)
 // Feature flag to enable grpc based pubsub in GCS.
-RAY_CONFIG(bool, gcs_grpc_based_pubsub, false)
+RAY_CONFIG(bool, gcs_grpc_based_pubsub, true)
 
 /// Duration to sleep after failing to put an object in plasma because it is full.
 RAY_CONFIG(uint32_t, object_store_full_delay_ms, 10)
