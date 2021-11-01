c23989c1f6decaf041caf800bb2600d224962248
Xiaowei Jiang <xwjiang2010@gmail.com>
2021-11-01 16:48:33 -0700

    Fix test_trial_scheduler.py

--- python/ray/tune/tests/test_trial_scheduler.py
+++ python/ray/tune/tests/test_trial_scheduler.py
@@ -259,8 +259,8 @@ class _MockTrialExecutor(TrialExecutor):
     def get_running_trials(self):
         return []
 
-    def has_resources(self):
-        return False
+    def has_resources_for_trial(self, trial: Trial):
+        return True
 
     def resource_string(self):
         return "This is a mock resource_string."
@@ -295,12 +295,6 @@ class _MockTrialRunner():
 
     def get_trials(self):
         return self.trials
-
-    def has_resources_for_trial(self, trial):
-        return True
-
-    def has_resources(self, resources):
-        return True
 
     def _pause_trial(self, trial):
         self.trial_executor.save(trial, Checkpoint.MEMORY, None)
