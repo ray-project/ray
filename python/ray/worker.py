1febb020f3b60150de95ed6fe1246cd6823a9d78
Yi Cheng <chengyidna@gmail.com>
2021-11-02 03:21:41 +0000

    remove

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -1008,8 +1008,6 @@ def shutdown(_exiting_interpreter: bool = False):
     # We need to destruct the core worker here because after this function,
     # we will tear down any processes spawned by ray.init() and the background
     # IO thread in the core worker doesn't currently handle that gracefully.
-    if hasattr(global_worker, "gcs_client"):
-        del global_worker.gcs_client
     if hasattr(global_worker, "core_worker"):
         global_worker.core_worker.shutdown()
         del global_worker.core_worker
@@ -1419,7 +1417,6 @@ def connect(node,
         driver_name, log_stdout_file_path, log_stderr_file_path,
         serialized_job_config, node.metrics_agent_port, runtime_env_hash,
         worker_shim_pid, startup_token)
-    worker.gcs_client = worker.core_worker.get_gcs_client()
 
     # Notify raylet that the core worker is ready.
     worker.core_worker.notify_raylet()
