fb13884a449c1caaf5fdafc83093eb10c66168e0
Amog Kamsetty <amogkamsetty@yahoo.com>
2021-11-04 09:22:51 -0700

    wip

--- release/ml_user_tests/ray-lightning/driver_setup.sh
+++ release/ml_user_tests/ray-lightning/driver_setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Hack to remove brackets, so that requirements_tune.txt can be used as a constraint in driver_requirements.txt
+sed '1d' ./../../../python/requirements/ml/requirements_tune.txt | sed 's/\[//g;s/\]//g' > tune_constraints.txt
+
 pip install -U -r ./driver_requirements.txt
