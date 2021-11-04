e182556daa07c74a2b33e72ff4d3dff5c5e7f100
sven1977 <svenmika1977@gmail.com>
2021-11-04 10:04:18 +0100

    wip.

--- rllib/agents/pg/__init__.py
+++ rllib/agents/pg/__init__.py
@@ -2,6 +2,10 @@ from ray.rllib.trainer.pg.pg_trainer import PGTrainer, DEFAULT_CONFIG
 from ray.rllib.trainer.pg.pg_tf_policy import pg_tf_loss, \
     post_process_advantages, PGTFPolicy
 from ray.rllib.trainer.pg.pg_torch_policy import pg_torch_loss, PGTorchPolicy
+from ray.rllib.utils.deprecation import deprecation_warning
+
+deprecation_warning(
+    old="ray.rllib.agents.pg", new="ray.rllib.trainer.pg", error=False)
 
 __all__ = [
     "pg_tf_loss",
--- rllib/trainer/pg/pg_trainer.py
+++ rllib/trainer/pg/pg_trainer.py
@@ -9,34 +9,16 @@ Detailed documentation: https://docs.ray.io/en/master/rllib-algorithms.html#pg
 """
 from typing import Type
 
-from ray.rllib.agents.trainer import Trainer, with_common_config
+from ray.rllib.agents.trainer import Trainer
+from ray.rllib.trainer.pg.default_config import DEFAULT_CONFIG
 from ray.rllib.trainer.pg.pg_tf_policy import PGTFPolicy
 from ray.rllib.trainer.pg.pg_torch_policy import PGTorchPolicy
 from ray.rllib.policy.policy import Policy
-from ray.rllib.utils.annotations import ExperimentalAPI, override
+from ray.rllib.utils.annotations import override
 from ray.rllib.utils.typing import TrainerConfigDict
-
-# yapf: disable
-# __sphinx_doc_begin__
-
-# Adds the following updates to the (base) `Trainer` config in
-# rllib/agents/trainer.py (`COMMON_CONFIG` dict).
-DEFAULT_CONFIG = with_common_config({
-    # No remote workers by default.
-    "num_workers": 0,
-    # Learning rate.
-    "lr": 0.0004,
-})
-
-# __sphinx_doc_end__
-# yapf: enable
 
 
 class PGTrainer(Trainer):
-    @ExperimentalAPI
-    def train_one_step(self):
-        pass  # TODO: implement pythonic exec function for PG
-
     @override(Trainer)
     def get_default_policy_class(self) -> Type[Policy]:
         return PGTorchPolicy if self.config["framework"] == "torch" \
