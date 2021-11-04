c55e3ee9e68ae1dc3e1e0e3a5d3e739cc00dda59
sven1977 <svenmika1977@gmail.com>
2021-11-04 21:50:10 +0100

    wip.

--- rllib/trainer/pg/pg_trainer.py
+++ rllib/trainer/pg/pg_trainer.py
@@ -1,10 +1,10 @@
 from typing import Type
 
 from ray.rllib.agents.trainer import Trainer
+from ray.rllib.policy.policy import Policy
 from ray.rllib.trainer.pg.default_config import DEFAULT_CONFIG
 from ray.rllib.trainer.pg.pg_tf_policy import PGTFPolicy
 from ray.rllib.trainer.pg.pg_torch_policy import PGTorchPolicy
-from ray.rllib.policy.policy import Policy
 from ray.rllib.utils.annotations import override
 from ray.rllib.utils.typing import TrainerConfigDict
 
