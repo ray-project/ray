5a4dbefb34929ffaa6078c22f23b5556ff15c8c3
Matthew Deng <matthew.j.deng@gmail.com>
2021-11-04 16:53:44 -0700

    [train] add placement group support

--- python/ray/train/backends/backend.py
+++ python/ray/train/backends/backend.py
@@ -11,12 +11,16 @@ from ray.ray_constants import env_integer
 from ray.train.checkpoint import CheckpointManager, CheckpointStrategy, \
     TuneCheckpointManager
 from ray.train.constants import ENABLE_DETAILED_AUTOFILLED_METRICS_ENV, \
-    TUNE_INSTALLED, ENABLE_SHARE_CUDA_VISIBLE_DEVICES_ENV
+    TUNE_INSTALLED, ENABLE_SHARE_CUDA_VISIBLE_DEVICES_ENV, \
+    TRAIN_ENABLE_WORKER_SPREAD_ENV, \
+    TRAIN_PLACEMENT_GROUP_TIMEOUT_S_ENV
 from ray.train.session import TrainingResultType, TrainingResult
 from ray.train.session import init_session, get_session, shutdown_session
 from ray.train.utils import RayDataset
 from ray.train.utils import check_for_failure
 from ray.train.worker_group import WorkerGroup
+from ray.util.placement_group import get_current_placement_group, \
+    remove_placement_group
 
 if TUNE_INSTALLED:
     from ray import tune
@@ -93,6 +97,7 @@ class BackendExecutor:
             self._max_failures = float("inf")
         self._num_failures = 0
         self._initialization_hook = None
+        self._placement_group = None
 
         if tune is not None and tune.is_session_enabled():
             self.checkpoint_manager = TuneCheckpointManager()
@@ -110,6 +115,8 @@ class BackendExecutor:
               train_cls_args: Optional[Tuple] = None,
               train_cls_kwargs: Optional[Dict] = None):
         """Starts the worker group."""
+        self._create_placement_group()
+        placement_group = self._placement_group or "default"
         self.worker_group = WorkerGroup(
             num_workers=self._num_workers,
             num_cpus_per_worker=self._num_cpus_per_worker,
@@ -118,7 +125,8 @@ class BackendExecutor:
             _additional_resources_per_worker,
             actor_cls=train_cls,
             actor_cls_args=train_cls_args,
-            actor_cls_kwargs=train_cls_kwargs)
+            actor_cls_kwargs=train_cls_kwargs,
+            placement_group=placement_group)
         try:
             if initialization_hook:
                 self._initialization_hook = initialization_hook
@@ -136,6 +144,55 @@ class BackendExecutor:
             logger.exception(str(exc))
             self._increment_failures()
             self._restart()
+
+    def _create_placement_group(self):
+        """Creates a placement group if it does not exist.
+
+        If a placement group is already detected (Tune) this will be a no-op.
+
+        By default the placement group will be created with PACK strategy.
+        This is optimized for colocating GPUs on a minimal number of nodes.
+        This behavior can be overridden to use the SPREAD strategy by defining
+        ``TRAIN_ENABLE_WORKER_SPREAD_ENV``
+
+        If a placement group is created it will be stored as
+        self._placement_group.
+        """
+        current_placement_group = get_current_placement_group()
+        should_capture_child_tasks_in_placement_group = \
+            ray.worker.global_worker \
+            .should_capture_child_tasks_in_placement_group
+        should_create_placement_group = \
+            current_placement_group is None or \
+            not should_capture_child_tasks_in_placement_group
+
+        if should_create_placement_group:
+            bundle = {
+                "CPU": self._num_cpus_per_worker,
+                "GPU": int(self._num_gpus_per_worker),
+                **self._additional_resources_per_worker
+            }
+            bundles = [bundle.copy() for _ in range(self._num_workers)]
+
+            use_spread = bool(env_integer(TRAIN_ENABLE_WORKER_SPREAD_ENV, 0))
+            strategy = "SPREAD" if use_spread else "PACK"
+
+            placement_group = ray.util.placement_group(
+                bundles, strategy=strategy)
+            logger.debug("Waiting for placement group to start.")
+            timeout = env_integer(TRAIN_PLACEMENT_GROUP_TIMEOUT_S_ENV, 100)
+            ready, _ = ray.wait([placement_group.ready()], timeout=timeout)
+            if ready:
+                logger.debug("Placement group has started.")
+            else:
+                raise TimeoutError(
+                    "Placement group creation timed out. Make sure "
+                    "your cluster either has enough resources or use "
+                    "an autoscaling cluster. Current resources "
+                    "available: {}, resources requested by the "
+                    "placement group: {}".format(ray.available_resources(),
+                                                 placement_group.bundle_specs))
+            self._placement_group = placement_group
 
     def _share_cuda_visible_devices(self):
         """Sets CUDA_VISIBLE_DEVICES on all workers.
@@ -528,6 +585,11 @@ class BackendExecutor:
                            "expected if one of the workers has crashed.")
         self.worker_group.shutdown()
         self.worker_group = InactiveWorkerGroup()
+
+        if self._placement_group:
+            remove_placement_group(self._placement_group)
+            self._placement_group = None
+
         self.dataset_shards = None
 
     @property
