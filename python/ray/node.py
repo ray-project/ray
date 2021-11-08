1ee107da0b6ecb6c47c7a8db8d5431b3a3c57b69
kShines <850874665@qq.com>
2021-11-08 11:57:53 +0800

    add_switch

--- python/ray/node.py
+++ python/ray/node.py
@@ -353,24 +353,26 @@ class Node:
             return num_cpus, num_gpus, memory, object_store_memory, result
 
         if not self._resource_spec:
-            cpu_info = get_cpu_info()
+            env_resources = {}
             required_cpu_instruction_sets_str = \
                 os.getenv(
                     "CPU_INSTRUCTION_SET", ray_constants.CPU_INSTRUCTION_SETS)
             required_cpu_instruction_sets = [
                 x.strip() for x in required_cpu_instruction_sets_str.split(",")
             ]
-            env_resources = {
-                required_ins:
-                ray_constants.DEFAULT_CPU_INSTRUCTION_SET_RESOURCE_NUMBER
-                for required_ins in
-                set(required_cpu_instruction_sets) & set(cpu_info["flags"])
-            }
+            if len(required_cpu_instruction_sets) > 0:
+                cpu_info = get_cpu_info()
+                env_resources += {
+                    required_ins:
+                    ray_constants.DEFAULT_CPU_INSTRUCTION_SET_RESOURCE_NUMBER
+                    for required_ins in
+                    set(required_cpu_instruction_sets) & set(cpu_info["flags"])
+                }
             env_string = os.getenv(
                 ray_constants.RESOURCES_ENVIRONMENT_VARIABLE)
             if env_string:
                 try:
-                    env_resources = json.loads(env_string)
+                    env_resources += json.loads(env_string)
                 except Exception:
                     logger.exception("Failed to load {}".format(env_string))
                     raise
