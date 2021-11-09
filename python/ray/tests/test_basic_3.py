95573237e696c0857b9ce109bc887f9b8cc4fad8
Alex Wu <alex@anyscale.com>
2021-11-09 15:37:07 -0800

    .

--- python/ray/tests/test_basic_3.py
+++ python/ray/tests/test_basic_3.py
@@ -138,7 +138,10 @@ def test_many_fractional_resources(shutdown_only):
 
 
 def test_background_tasks_with_max_calls(shutdown_only):
-    ray.init(num_cpus=2)
+    ray.init(
+        # TODO (Alex): We need to fix
+        # https://github.com/ray-project/ray/issues/20203 to remove this flag.
+        num_cpus=2, _system_config={"worker_cap_initial_backoff_delay_ms": 0})
 
     @ray.remote
     def g():
