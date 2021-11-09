5d1acc0c8bfa1deba575d954fb47474dd6804898
Yi Cheng <chengyidna@gmail.com>
2021-11-09 22:45:34 +0000

    up

--- python/ray/tests/conftest.py
+++ python/ray/tests/conftest.py
@@ -443,20 +443,3 @@ def ray_start_chaos_cluster(request):
     cluster.shutdown()
     del os.environ["RAY_num_heartbeats_timeout"]
     del os.environ["RAY_raylet_heartbeat_period_milliseconds"]
-
-
-@pytest.fixture(scope="function", params=["ray_client", "no_ray_client"])
-def start_cluster(ray_start_cluster, request):
-    assert request.param in {"ray_client", "no_ray_client"}
-    use_ray_client: bool = request.param == "ray_client"
-
-    cluster = ray_start_cluster
-    cluster.add_node(num_cpus=4)
-    if use_ray_client:
-        cluster.head_node._ray_params.ray_client_server_port = "10003"
-        cluster.head_node.start_ray_client_server()
-        address = "ray://localhost:10003"
-    else:
-        address = cluster.address
-
-    yield cluster, address
