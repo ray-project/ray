4472e9d8ee09d9209e33c2e70ab9c6bd0e3e58ad
Jun Gong <jungong@anyscale.com>
2021-11-09 13:34:26 -0800

    debug 2

--- release/golden_notebook_tests/workloads/rllib_tune_serve_test.py
+++ release/golden_notebook_tests/workloads/rllib_tune_serve_test.py
@@ -195,12 +195,12 @@ def test_serve_endpoint(serve_url):
 
     data = {"state": 1}
     resp = sess.post(resp.url, json=data)
-    print(str(resp))  # DEBUG
+    print("111: ", str(resp))  # DEBUG
     print(resp.json())
 
     data = {"state": 3}
     resp = sess.post(resp.url, json=data)
-    print(str(resp))  # DEBUG
+    print("222: ", str(resp))  # DEBUG
     print(resp.json())
 
 
