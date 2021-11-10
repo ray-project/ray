0202e03e434fd561db8e38908bed283e55e1eb27
SangBin Cho <rkooo567@gmail.com>
2021-11-09 20:47:42 -0800

    Fix a bug.

--- src/ray/core_worker/actor_manager.cc
+++ src/ray/core_worker/actor_manager.cc
@@ -234,13 +234,15 @@ void ActorManager::HandleActorStateNotification(const ActorID &actor_id,
   if (actor_data.state() == rpc::ActorTableData::RESTARTING) {
     direct_actor_submitter_->DisconnectActor(actor_id, actor_data.num_restarts(), false);
   } else if (actor_data.state() == rpc::ActorTableData::DEAD) {
-    if (!actor_data.name().empty()) {
-      absl::MutexLock lock(&cache_mutex_);
-      cached_actor_name_to_ids_.erase(
-          GenerateCachedActorName(actor_data.ray_namespace(), actor_data.name()));
-    }
-    std::shared_ptr<rpc::RayException> creation_task_exception = nullptr;
     InvalidateActorHandleCache(actor_id);
+    std::shared_ptr<rpc::RayException> creation_task_exception = nullptr;
+    if (actor_data.has_creation_task_exception()) {
+      RAY_LOG(INFO) << "Creation task formatted exception: "
+                    << actor_data.creation_task_exception().formatted_exception_string()
+                    << ", actor_id: " << actor_id;
+      creation_task_exception =
+          std::make_shared<rpc::RayException>(actor_data.creation_task_exception());
+    }
     direct_actor_submitter_->DisconnectActor(actor_id, actor_data.num_restarts(), true,
                                              creation_task_exception);
     // We cannot erase the actor handle here because clients can still
