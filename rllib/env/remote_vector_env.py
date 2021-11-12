ed8db2f9f0b739cd1597cb6b8daa70ef2a4f4d59
sven1977 <svenmika1977@gmail.com>
2021-11-12 10:52:27 +0100

    fix.

--- rllib/env/remote_vector_env.py
+++ rllib/env/remote_vector_env.py
@@ -1,7 +1,6 @@
 from ray.rllib.env.remote_base_env import RemoteBaseEnv
 from ray.rllib.utils.deprecation import deprecation_warning
 
-
 deprecation_warning(
     old="rllib.env.remote_base_env.RemoteVectorEnv",
     new="ray.rllib.env.remote_base_env.RemoteBaseEnv",
