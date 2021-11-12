3474a0d7b903b4abc99b268f6c638ac5929e72c8
Yi Cheng <chengyidna@gmail.com>
2021-11-12 06:20:16 +0000

    up

--- python/ray/experimental/internal_kv.py
+++ python/ray/experimental/internal_kv.py
@@ -1,4 +1,4 @@
-from typing import List, Union, Optional, Tuple
+from typing import List, Union, Optional
 
 from ray._private.client_mode_hook import client_mode_hook
 from ray._private.gcs_utils import GcsClient
