4d9b160b742acb9075d08cebd1a170748d2572fd
Yi Cheng <chengyidna@gmail.com>
2021-11-15 23:10:36 +0000

    format

--- python/ray/_private/gcs_utils.py
+++ python/ray/_private/gcs_utils.py
@@ -131,7 +131,8 @@ def _auto_reconnect(f):
             except grpc.RpcError as e:
                 if remaining_retry <= 0:
                     raise
-                if e.code() in (grpc.StatusCode.UNAVAILABLE, grpc.StatusCode.UNKNOWN):
+                if e.code() in (grpc.StatusCode.UNAVAILABLE,
+                                grpc.StatusCode.UNKNOWN):
                     logger.error(
                         "Failed to send request to gcs, reconnecting. "
                         f"Error {e}")
