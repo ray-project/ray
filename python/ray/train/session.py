ef7967476c588cfb3ac11c7275ec384f79295d71
Amog Kamsetty <amogkam@users.noreply.github.com>
2021-11-15 09:14:12 -0800

    [Train] Torch data transfer automatic conversion (#20333)
    * update
    
    * formatting
    
    * fix failures
    
    * fix session tests
    
    * address comments
    
    * add to api docs
    
    * package refactor
    
    * wip
    
    * wip
    
    * wip
    
    * finish
    
    * finish
    
    * fix
    
    * comment
    
    * fix
    
    * install horovod for docs
    
    * address comment
    
    * Update python/ray/train/session.py
    
    Co-authored-by: matthewdeng <matthew.j.deng@gmail.com>
    
    * Update python/ray/train/torch.py
    
    Co-authored-by: matthewdeng <matthew.j.deng@gmail.com>
    
    * address comments
    
    * try fix docs
    
    * fix doc build failure
    
    * wip
    
    * fix
    
    * fix
    
    * fix
    
    * try fix doc highlighting
    
    * fix docs
    
    * finish
    
    * formatting
    
    * address comments and fix tests
    
    * address comments and fix test
    
    Co-authored-by: matthewdeng <matthew.j.deng@gmail.com>

--- python/ray/train/session.py
+++ python/ray/train/session.py
@@ -37,6 +37,7 @@ class Session:
                  world_size: int,
                  dataset_shard: Optional[RayDataset] = None,
                  checkpoint: Optional[Dict] = None,
+                 encode_data_fn: Callable = None,
                  detailed_autofilled_metrics: bool = False):
 
         self.dataset_shard = dataset_shard
@@ -48,6 +49,15 @@ class Session:
         self.local_rank = local_rank
         self.world_size = world_size
         self.loaded_checkpoint = checkpoint
+
+        # Function to encode checkpoint dict before sending to the driver.
+        if not encode_data_fn:
+
+            def noop(x):
+                return x
+
+            encode_data_fn = noop
+        self._encode_data_fn = encode_data_fn
 
         # This lock is used to control the execution of the training thread.
         self.continue_lock = threading.Semaphore(0)
@@ -161,9 +171,9 @@ class Session:
         if self.ignore_report:
             return
 
-        kwargs = self._auto_fill_metrics(kwargs)
+        kwargs = self._encode_data_fn(self._auto_fill_metrics(kwargs))
 
-        result = TrainingResult(TrainingResultType.REPORT, kwargs.copy())
+        result = TrainingResult(TrainingResultType.REPORT, kwargs)
 
         # Add result to a thread-safe queue.
         self.result_queue.put(result, block=True)
@@ -196,7 +206,8 @@ class Session:
         if self.world_rank != 0:
             kwargs = {}
         else:
-            kwargs = self._auto_fill_checkpoint_metrics(kwargs)
+            kwargs = self._encode_data_fn(
+                self._auto_fill_checkpoint_metrics(kwargs))
 
         result = TrainingResult(TrainingResultType.CHECKPOINT, kwargs)
         # Add result to a thread-safe queue.
