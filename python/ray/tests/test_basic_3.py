75f421a3fd0d22840cc15c8c8b15a1a00cf2728f
Alex Wu <alex@anyscale.io>
2021-11-15 17:39:50 -0800

    [core] Nested task support via task depth + backpressure (#17887)
    * needs depth
    
    * depth
    
    * .
    
    * .
    
    * .
    
    * lint
    
    * .
    
    * lint
    
    * fix tests
    
    * .
    
    * .
    
    * .
    
    * .
    
    * cleanup
    
    * .
    
    * tests
    
    * .
    
    * more tests
    
    * fix rest(?) of tests
    
    * cleanup
    
    * .
    
    * .
    
    * .
    
    * .
    
    * lint
    
    * fix test basic
    
    * fix ref counting?
    
    * cleanup
    
    * lint
    
    * .
    
    * pass dataset pipeline test
    
    * .
    
    * stephanie's comments + fix tests
    
    * cleanup
    
    * cleanup
    
    * minor cleanup, then fix merge conflict
    
    * lint
    
    * cast
    
    * feature flag
    
    * lint
    
    * lint
    
    * refactor
    
    * needs cleanup
    
    * should pass
    
    * lint
    
    * .
    
    * .
    
    * .
    
    * work?
    
    * .
    
    * works?
    
    * lint
    
    * work?
    
    * .
    
    * fix cpp tests
    
    * .
    
    * .
    
    * split test
    
    * fix windows?
    
    * fix windows?
    
    * fix test + check
    
    * .
    
    * all passing
    
    * tests
    
    * lint
    
    * cleanup
    
    * .
    
    * most stephanie ocmments
    
    * lint
    
    * remove timer
    
    * .
    
    * allowed - capacity
    
    * .
    
    * everything except barrier
    
    * addd guard
    
    * works
    
    * lint
    
    * works?
    
    * debug string
    
    * last comment?
    
    * short comments
    
    * most comments
    
    * lint
    
    * done?
    
    * done?
    
    * .
    
    * .
    
    * .
    
    * .
    
    * done?
    
    * done?
    
    * update
    
    * lint
    
    * fix last test
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * debug
    
    * .
    
    * .
    
    * .
    
    * .
    
    * fix type
    
    * .
    
    * .
    
    * cleanup
    
    Co-authored-by: Alex Wu <alex@anyscale.com>

--- python/ray/tests/test_basic_3.py
+++ python/ray/tests/test_basic_3.py
@@ -140,7 +140,11 @@ def test_many_fractional_resources(shutdown_only):
 
 @pytest.mark.skipif(sys.platform == "win32", reason="Fails on windows")
 def test_background_tasks_with_max_calls(shutdown_only):
-    ray.init(num_cpus=2)
+    ray.init(
+        # TODO (Alex): We need to fix
+        # https://github.com/ray-project/ray/issues/20203 to remove this flag.
+        num_cpus=2,
+        _system_config={"worker_cap_initial_backoff_delay_ms": 0})
 
     @ray.remote
     def g():
