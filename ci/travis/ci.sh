18d605fa7c224e59a60f81928c50a437a6b8dcec
Simon Mo <simon.mo@hey.com>
2021-11-16 20:22:09 -0800

    [Serve] Add experimental CLI for `serve deploy` (#20371)

--- ci/travis/ci.sh
+++ ci/travis/ci.sh
@@ -141,6 +141,7 @@ test_python() {
       python/ray/tests/...
       -python/ray/serve:conda_env # runtime_env unsupported on Windows
       -python/ray/serve:test_api # segfault on windows? https://github.com/ray-project/ray/issues/12541
+      -python/ray/serve:test_cli # cli
       -python/ray/serve:test_router # timeout
       -python/ray/serve:test_handle # "fatal error" (?) https://github.com/ray-project/ray/pull/13695
       -python/ray/serve:test_controller_crashes # timeout
