a0fce3c27d756d8e7c749557f5dae9061acaf4e4
Kai Fricke <kai@anyscale.com>
2021-11-17 12:38:05 +0000

    Remove debug echo

--- release/run_e2e.sh
+++ release/run_e2e.sh
@@ -32,13 +32,13 @@ done
 
 export RAY_REPO RAY_BRANCH RAY_VERSION RAY_WHEELS RAY_TEST_REPO RAY_TEST_BRANCH RELEASE_RESULTS_DIR
 
-echo pip install -q -r release/requirements.txt,
-echo pip install -U boto3 botocore,
-echo git clone -b "${RAY_TEST_BRANCH}" "${RAY_TEST_REPO}" ~/ray
+pip install -q -r release/requirements.txt,
+pip install -U boto3 botocore,
+git clone -b "${RAY_TEST_BRANCH}" "${RAY_TEST_REPO}" ~/ray
 
-echo python e2e.py "$@"
+python e2e.py "$@"
 EXIT_CODE=$?
 
-echo sudo cp -rf /tmp/artifacts/* /tmp/ray_release_test_artifacts || true
+sudo cp -rf /tmp/artifacts/* /tmp/ray_release_test_artifacts || true
 echo "e2e command exited with exit code ${EXIT_CODE}"
 exit $EXIT_CODE
