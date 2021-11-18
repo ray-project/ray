fec194d71a1b3a6a90a6ecb87bf52538cf66e24e
Artur Niederfahrenhorst <artur.niederfahrenhorst@rwth-aachen.de>
2021-11-18 21:53:59 +0100

    args of minibatch buffer are now clearer

--- rllib/execution/buffers/minibatch_buffer.py
+++ rllib/execution/buffers/minibatch_buffer.py
@@ -22,13 +22,15 @@ class MinibatchBuffer:
            size (int): Max number of data items to buffer.
            timeout (float): Queue timeout
            num_passes (int): Max num times each data item should be emitted.
-           init_num_passes (int): Initial max passes for each data item
+           init_num_passes (int): Initial passes for each data item.
+           Maxiumum number of passes per item are increased to num_passes over
+           time.
        """
         self.inqueue = inqueue
         self.size = size
         self.timeout = timeout
-        self.max_ttl = num_passes
-        self.cur_max_ttl = init_num_passes
+        self.max_initial_ttl = num_passes
+        self.cur_initial_ttl = init_num_passes
         self.buffers = [None] * size
         self.ttl = [0] * size
         self.idx = 0
@@ -42,9 +44,9 @@ class MinibatchBuffer:
         """
         if self.ttl[self.idx] <= 0:
             self.buffers[self.idx] = self.inqueue.get(timeout=self.timeout)
-            self.ttl[self.idx] = self.cur_max_ttl
-            if self.cur_max_ttl < self.max_ttl:
-                self.cur_max_ttl += 1
+            self.ttl[self.idx] = self.cur_initial_ttl
+            if self.cur_initial_ttl < self.max_initial_ttl:
+                self.cur_initial_ttl += 1
         buf = self.buffers[self.idx]
         self.ttl[self.idx] -= 1
         released = self.ttl[self.idx] <= 0
