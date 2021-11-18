e350717173debf72c37855f98a1a6b8a13bcb28c
Shreyas Krishnaswamy <shrekris@anyscale.com>
2021-11-17 18:32:50 -0800

    Remove dependency tests for smart_open

--- python/ray/tests/test_runtime_env_ray_minimal.py
+++ python/ray/tests/test_runtime_env_ray_minimal.py
@@ -12,7 +12,6 @@ ray[default] but not in ray (e.g., `pip uninstall aiohttp`) and set
 import os
 import sys
 import pytest
-import tempfile
 
 import ray
 from ray.exceptions import RuntimeEnvSetupError
@@ -102,76 +101,6 @@ def test_ray_client_init(call_ray_start):
     with pytest.raises(ConnectionAbortedError) as excinfo:
         ray.init("ray://localhost:25552", runtime_env={"pip": ["requests"]})
     assert "ray[default]" in str(excinfo.value)
-
-
-@pytest.mark.skipif(
-    sys.platform == "win32", reason="runtime_env unsupported on Windows.")
-@pytest.mark.skipif(
-    os.environ.get("RAY_MINIMAL") != "1",
-    reason="This test is only run in CI with a minimal Ray installation.")
-@pytest.mark.parametrize(
-    "call_ray_start",
-    ["ray start --head --ray-client-server-port 25552 --port 0"],
-    indirect=True)
-@pytest.mark.parametrize("option", ["working_dir", "py_modules"])
-def test_remote_package_uri(call_ray_start, option):
-    """
-    Test is based on test_remote_package_uri from
-    test_runtime_env_working_dir.py file. Ensures that runtime_env logic
-    gracefully raises error if smart_open is not installed and a remote URI
-    is specified for a dependency package.
-    """
-    address = "ray://localhost:25552"
-    uri = "s3://bucket/file.zip"
-
-    if option == "working_dir":
-        env = {"working_dir": uri}
-    elif option == "py_modules":
-        env = {"py_modules": [uri]}
-
-    with pytest.raises(Exception) as err:
-        ray.init(address, runtime_env=env)
-    assert "pip install smart_open" in str(err.value)
-
-
-@pytest.mark.skipif(
-    sys.platform == "win32", reason="runtime_env unsupported on Windows.")
-@pytest.mark.skipif(
-    os.environ.get("RAY_MINIMAL") != "1",
-    reason="This test is only run in CI with a minimal Ray installation.")
-@pytest.mark.parametrize(
-    "call_ray_start",
-    ["ray start --head --ray-client-server-port 25552 --port 0"],
-    indirect=True)
-def test_empty_working_dir(call_ray_start):
-    """
-    Test is based on test_empty_working_dir from
-    test_runtime_env_working_dir.py file. Ensures that runtime_env logic
-    still supports uploading local packages to GCS, even when smart_open is
-    not installed.
-    """
-    address = "ray://localhost:25552"
-    with tempfile.TemporaryDirectory() as working_dir:
-        ray.init(address, runtime_env={"working_dir": working_dir})
-
-        @ray.remote
-        def listdir():
-            return os.listdir()
-
-        assert len(ray.get(listdir.remote())) == 0
-
-        @ray.remote
-        class A:
-            def listdir(self):
-                return os.listdir()
-                pass
-
-        a = A.remote()
-        assert len(ray.get(a.listdir.remote())) == 0
-
-        # Test that we can reconnect with no errors
-        ray.shutdown()
-        ray.init(address, runtime_env={"working_dir": working_dir})
 
 
 if __name__ == "__main__":
