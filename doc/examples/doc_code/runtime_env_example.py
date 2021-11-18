7e1d2b8310f29ed21ee08471f7746fee3f7b014c
architkulkarni <arkulkar@gmail.com>
2021-11-17 16:39:39 -0800

    add comments to code example

--- doc/examples/doc_code/runtime_env_example.py
+++ doc/examples/doc_code/runtime_env_example.py
@@ -49,18 +49,27 @@ def f():
     pass
 
 @ray.remote
-class A:
+class SomeClass:
     pass
 
 # __per_task_per_actor_start__
+# Invoke a remote task that will run in a specified runtime environment.
 f.options(runtime_env=runtime_env).remote()
-actor = A.options(runtime_env=runtime_env).remote()
 
+# Instantiate an actor that will run in a specified runtime environment.
+actor = SomeClass.options(runtime_env=runtime_env).remote()
+
+# Specify a runtime environment in the task definition.  Future invocations via
+# `g.remote()` will use this runtime environment unless overridden by using
+# `.options()` as above.
 @ray.remote(runtime_env=runtime_env)
 def g():
     pass
 
+# Specify a runtime environment in the actor definition.  Future instantiations
+# via `MyClass.remote()` will use this runtime environment unless overridden by
+# using `.options()` a above.`
 @ray.remote(runtime_env=runtime_env)
-class B:
+class MyClass:
     pass
 # __per_task_per_actor_end__
