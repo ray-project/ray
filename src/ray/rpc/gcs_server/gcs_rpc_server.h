8f41d2c7792120f2d324739b8521183d2e420567
Yi Cheng <chengyidna@gmail.com>
2021-11-23 05:24:27 +0000

    up

--- src/ray/rpc/gcs_server/gcs_rpc_server.h
+++ src/ray/rpc/gcs_server/gcs_rpc_server.h
@@ -64,7 +64,7 @@ namespace rpc {
 
 #define INTERNAL_KV_SERVICE_RPC_HANDLER(HANDLER)     \
   RPC_SERVICE_HANDLER(InternalKVGcsService, HANDLER, \
-                      RayConfig::instance().gcs_max_active_rpcs_per_handler())
+                      -1)
 
 // Unlimited max active RPCs, because of long poll.
 #define INTERNAL_PUBSUB_SERVICE_RPC_HANDLER(HANDLER) \
