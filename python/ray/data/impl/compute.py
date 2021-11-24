15fe1271a7f02a3b26dd146fd35c2b808e17bd1c
Eric Liang <ekhliang@gmail.com>
2021-11-23 18:12:58 -0800

    revert

--- python/ray/data/impl/compute.py
+++ python/ray/data/impl/compute.py
@@ -67,7 +67,8 @@ class TaskPool(ComputeStrategy):
             all_refs = [
                 map_block.remote(b, fn, m.input_files) for b, m in blocks
             ]
-            data_refs, refs = zip(*all_refs)
+            data_refs = [r[0] for r in all_refs]
+            refs = [r[1] for r in all_refs]
 
         # Common wait for non-data refs.
         try:
