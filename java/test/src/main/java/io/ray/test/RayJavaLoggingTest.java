c37ecc8995940eae80c4fd96427d0e66ec201d0c
Qing Wang <kingchin1218@gmail.com>
2021-11-29 00:26:08 +0800

    Add unit test for Java log level.

--- java/test/src/main/java/io/ray/test/RayJavaLoggingTest.java
+++ java/test/src/main/java/io/ray/test/RayJavaLoggingTest.java
@@ -5,16 +5,25 @@ import io.ray.api.ActorHandle;
 import io.ray.api.Ray;
 import io.ray.api.id.JobId;
 import io.ray.runtime.util.SystemUtil;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 @Test(groups = {"cluster"})
 public class RayJavaLoggingTest extends BaseTest {
 
   private static final Logger LOG = LoggerFactory.getLogger(RayJavaLoggingTest.class);
+
+  @BeforeClass
+  public void setup() {
+    System.setProperty("ray.job.logging-level", "DEBUG");
+  }
 
   private static class HeavyLoggingActor {
     public int getPid() {
@@ -48,5 +57,21 @@ public class RayJavaLoggingTest extends BaseTest {
       long fileSize = rotatedFile.length();
       Assert.assertTrue(fileSize > 1024 && fileSize < 1024 * 2);
     }
+  }
+
+  @Test
+  public void testDebugLogLevel() throws FileNotFoundException {
+    String fileName = Ray.task(RayJavaLoggingTest::getCurrentLogFileName).remote().get();
+    File logFile = new File(fileName);
+    Assert.assertTrue(logFile.exists());
+    FileReader fr = new FileReader(logFile);
+    BufferedReader br = new BufferedReader(fr);
+    Assert.assertTrue(br.lines().anyMatch(line -> line.contains("DEBUG")));
+  }
+
+  private static String getCurrentLogFileName() {
+    String currLogDir = "/tmp/ray/session_latest/logs";
+    final JobId jobId = Ray.getRuntimeContext().getCurrentJobId();
+    return String.format("%s/java-worker-%s-%d.log", currLogDir, jobId, SystemUtil.pid());
   }
 }
