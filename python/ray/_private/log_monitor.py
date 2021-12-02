93732231cb05d2b0ce874940f74d326a7cbcf31f
Chu Xiangyang <xychu2008@gmail.com>
2021-12-02 15:47:26 +0800

    [Core] Update job id to hex in JOB_LOG_PATTERN (#20612) (#20816)
    * [Core] Update job id to hex in JOB_LOG_PATTERN (#20612)
    
    * Add test to log pid with hex job id

--- python/ray/_private/log_monitor.py
+++ python/ray/_private/log_monitor.py
@@ -25,7 +25,7 @@ from ray._private.ray_logging import setup_component_logger
 logger = logging.getLogger(__name__)
 
 # The groups are worker id, job id, and pid.
-JOB_LOG_PATTERN = re.compile(".*worker-([0-9a-f]+)-(\d+)-(\d+)")
+JOB_LOG_PATTERN = re.compile(".*worker-([0-9a-f]+)-([0-9a-f]+)-(\d+)")
 # The groups are job id.
 RUNTIME_ENV_SETUP_PATTERN = re.compile(".*runtime_env_setup-(\d+).log")
 # Log name update interval under pressure.
