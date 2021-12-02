63730328fc4388418efd7c7143ff46f8f12cbefe
Yi Cheng <chengyidna@gmail.com>
2021-12-02 21:08:54 +0000

    up

---
+++ src/ray/gcs/store_client/store_client_factory.cc
@@ -0,0 +1,22 @@
+#include "ray/gcs/store_client/store_client_factory.h"
+
+#include "ray/common/ray_config.h"
+#include "ray/gcs/store_client/in_memory_store_client.h"
+#include "ray/gcs/store_client/redis_store_client.h"
+
+namespace ray {
+namespace gcs {
+
+std::unique_ptr<StoreClient> MakeStoreClient(std::shared_ptr<RedisClient> redis_client,
+                                             instrumented_io_context &main_io_service) {
+  if (RayConfig::instance().gcs_storage_backend() == "legacy") {
+    return std::make_unique<RedisStoreClient>(redis_client);
+  } else if (RayConfig::instance().gcs_storage_backend() == "memory") {
+    return std::make_unique<InMemoryStoreClient>(main_io_service);
+  }
+  RAY_LOG(FATAL) << "Invalid gcs_storage_backend "
+                 << RayConfig::instance().gcs_storage_backend();
+  return nullptr;
+}
+}  // namespace gcs
+}  // namespace ray
