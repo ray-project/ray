97df34f03cb0e946801bee72e00a47991063b81b
architkulkarni <arkulkar@gmail.com>
2021-12-03 13:15:39 -0800

    lint

--- python/ray/serve/tests/test_cli.py
+++ python/ray/serve/tests/test_cli.py
@@ -8,7 +8,8 @@ import pytest
 import requests
 
 from ray import serve
-from ray.tests.conftest import tmp_working_dir # noqa: F401, E501
+from ray.tests.conftest import tmp_working_dir  # noqa: F401, E501
+
 
 @pytest.fixture
 def ray_start_stop():
