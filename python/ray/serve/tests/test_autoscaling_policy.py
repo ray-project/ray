e2784957dee8470836d55ad035abc60439490505
Shreyas Krishnaswamy <shrekris@anyscale.com>
2021-12-06 16:18:57 -0800

    Test autoscaling behavior under bursty workloads

--- python/ray/serve/tests/test_autoscaling_policy.py
+++ python/ray/serve/tests/test_autoscaling_policy.py
@@ -402,6 +402,59 @@ def test_single_replica_receives_all_requests(ongoing_requests):
     assert new_num_replicas == sum(ongoing_requests) / target_requests
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="Failing on Windows.")
+def test_e2e_bursty(serve_instance):
+    """Sends 100 requests in bursts.
+       Uses delays to smooth out the provisioning."""
+
+    signal = SignalActor.remote()
+
+    @serve.deployment(
+        _autoscaling_config={
+            "metrics_interval_s": 0.1,
+            "min_replicas": 1,
+            "max_replicas": 2,
+            "look_back_period_s": 0.2,
+            "downscale_delay_s": 0.2,
+            "upscale_delay_s": 0.2
+        },
+        # We will send over a lot of queries. This will make sure replicas are
+        # killed quickly during cleanup.
+        _graceful_shutdown_timeout_s=1,
+        max_concurrent_queries=1000,
+        version="v1")
+    class A:
+        def __call__(self):
+            ray.get(signal.wait.remote())
+
+    A.deploy()
+
+    controller = serve_instance._controller
+    start_time = get_deployment_start_time(controller, A)
+
+    handle = A.get_handle()
+    [handle.remote() for _ in range(100)]
+
+    wait_for_condition(lambda: get_num_running_replicas(controller, A) >= 2)
+    num_replicas = get_num_running_replicas(controller, A)
+    signal.send.remote()
+
+    import time
+
+    # Execute a bursty workload that issues 100 requests every 0.05 seconds
+    for _ in range(5):
+        time.sleep(0.05)
+        assert get_num_running_replicas(controller, A) == num_replicas
+        [handle.remote() for _ in range(100)]
+        signal.send.remote()
+
+    # As the queue is drained, we should scale back down.
+    wait_for_condition(lambda: get_num_running_replicas(controller, A) <= 1)
+
+    # Make sure start time did not change for the deployment
+    assert get_deployment_start_time(controller, A) == start_time
+
+
 if __name__ == "__main__":
     import sys
     import pytest
