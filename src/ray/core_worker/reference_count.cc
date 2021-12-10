c4f5f092553227c1bede06ba6bc7a3890d307dab
Qing Wang <kingchin1218@gmail.com>
2021-12-11 00:46:10 +0800

    ADDRESSed comments.

--- src/ray/core_worker/reference_count.cc
+++ src/ray/core_worker/reference_count.cc
@@ -256,12 +256,14 @@ void ReferenceCounter::SetNestedRefInUseRecursive(ReferenceTable::iterator inner
 
 void ReferenceCounter::ReleaseAllLocalReferences() {
   absl::MutexLock lock(&mutex_);
-  /// NOTE: A copy here to avoid the risk of iterator invalidation issue.
-  auto all_refs_copy = object_id_refs_;
-  for (auto &ref : all_refs_copy) {
+  std::vector<ObjectID> refs_to_remove;
+  for (auto &ref : object_id_refs_) {
     for (int i = ref.second.local_ref_count; i > 0; --i) {
-      RemoveLocalReferenceInternal(ref.first, nullptr);
+      refs_to_remove.push_back(ref.first);
     }
+  }
+  for (const auto &object_id_to_remove : refs_to_remove) {
+    RemoveLocalReferenceInternal(object_id_to_remove, nullptr);
   }
 }
 
