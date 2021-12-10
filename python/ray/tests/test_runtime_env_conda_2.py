202b81f19bc333ed2d064f62e2f6581e0b69c700
久龙 <guyang.sgy@antfin.com>
2021-12-10 17:35:11 +0800

    seperate test

--- python/ray/tests/test_runtime_env_conda_2.py
+++ python/ray/tests/test_runtime_env_conda_2.py
@@ -1,8 +1,8 @@
 import os
 import pytest
 import sys
-from ray._private.test_utils import (wait_for_condition, 
-                check_local_files_gced, generate_runtime_env_dict)
+from ray._private.test_utils import (
+    wait_for_condition, check_local_files_gced, generate_runtime_env_dict)
 
 import ray
 
@@ -10,6 +10,36 @@ if not os.environ.get("CI"):
     # This flags turns on the local development that link against current ray
     # packages and fall back all the dependencies to current python's site.
     os.environ["RAY_RUNTIME_ENV_LOCAL_DEV_MODE"] = "1"
+
+
+@pytest.mark.skipif(
+    os.environ.get("CI") and sys.platform != "linux",
+    reason="Requires PR wheels built in CI, so only run on linux CI machines.")
+@pytest.mark.parametrize("field", ["conda", "pip"])
+@pytest.mark.parametrize("spec_format", ["file", "python_object"])
+def test_actor_level_gc(start_cluster, field, spec_format, tmp_path):
+    """Tests that actor-level working_dir is GC'd when the actor exits."""
+    cluster, address = start_cluster
+
+    ray.init(address)
+
+    runtime_env = generate_runtime_env_dict(field, spec_format, tmp_path)
+
+    @ray.remote
+    class A:
+        def test_import(self):
+            import pip_install_test  # noqa: F401
+            return True
+
+    NUM_ACTORS = 5
+    actors = [
+        A.options(runtime_env=runtime_env).remote() for _ in range(NUM_ACTORS)
+    ]
+    ray.get([a.test_import.remote() for a in actors])
+    for i in range(5):
+        assert not check_local_files_gced(cluster)
+        ray.kill(actors[i])
+    wait_for_condition(lambda: check_local_files_gced(cluster))
 
 
 @pytest.mark.skipif(
