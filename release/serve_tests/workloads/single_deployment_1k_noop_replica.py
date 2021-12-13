517dec108b26245f0f3ba9b2b7ce6e2c56fc5976
Shreyas Krishnaswamy <shrekris@anyscale.com>
2021-12-13 12:17:04 -0800

    Index into tuple before accessing content's properties

--- release/serve_tests/workloads/single_deployment_1k_noop_replica.py
+++ release/serve_tests/workloads/single_deployment_1k_noop_replica.py
@@ -99,7 +99,7 @@ def main(num_replicas: Optional[int], trial_length: Optional[str],
         num_nodes = int(math.ceil(num_replicas / NUM_CPU_PER_NODE))
         logger.info(
             f"Setting up local ray cluster with {num_nodes} nodes ..\n")
-        serve_client = setup_local_single_node_cluster(num_nodes)
+        serve_client = setup_local_single_node_cluster(num_nodes)[0]
     else:
         num_replicas = num_replicas or DEFAULT_FULL_TEST_NUM_REPLICA
         trial_length = trial_length or DEFAULT_FULL_TEST_TRIAL_LENGTH
