9aa92746f146d4cbfc5bfe2cca559fa0d92ccb9a
Yi Cheng <chengyidna@gmail.com>
2021-12-20 06:25:26 +0000

    fix tset

--- python/ray/tests/test_metrics_agent.py
+++ python/ray/tests/test_metrics_agent.py
@@ -245,8 +245,8 @@ def test_prometheus_file_based_service_discovery(ray_start_cluster):
     addr = ray.init(address=cluster.address)
     redis_address = addr["redis_address"]
     writer = PrometheusServiceDiscoveryWriter(
-        redis_address, addr["gcs_address"],
-        ray.ray_constants.REDIS_DEFAULT_PASSWORD, "/tmp/ray")
+        redis_address, ray.ray_constants.REDIS_DEFAULT_PASSWORD, addr["gcs_address"],
+        "/tmp/ray")
 
     def get_metrics_export_address_from_node(nodes):
         node_export_addrs = [
