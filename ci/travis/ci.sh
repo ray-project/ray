15321ebf5821489b1c796928a6d5fa0fc92f1065
mattip <matti.picus@gmail.com>
2021-12-23 12:13:30 +0200

    WINDOWS: enable passing part of runtime_env_plugin test

--- ci/travis/ci.sh
+++ ci/travis/ci.sh
@@ -174,7 +174,6 @@ test_python() {
       -python/ray/tests:test_ray_init  # test_redis_port() seems to fail here, but pass in isolation
       -python/ray/tests:test_resource_demand_scheduler
       -python/ray/tests:test_reference_counting  # too flaky 9/25/21
-      -python/ray/tests:test_runtime_env_plugin # runtime_env not supported on Windows
       -python/ray/tests:test_runtime_env_complicated # conda install slow leading to timeout
       -python/ray/tests:test_stress  # timeout
       -python/ray/tests:test_stress_sharded  # timeout
