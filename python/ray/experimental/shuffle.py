6dc009eb17a24e6d15bc75633d937c13d97a69e8
Jiajun Yao <jeromeyjj@gmail.com>
2021-12-29 16:47:42 -0800

    debug

--- python/ray/experimental/shuffle.py
+++ python/ray/experimental/shuffle.py
@@ -104,30 +104,23 @@ class _StatusTracker:
         self.reduce_refs = []
 
     def register_objectrefs(self, map_refs, reduce_refs):
-        print("register_objectrefs start")
         self.map_refs = map_refs
         self.reduce_refs = reduce_refs
-        print("register_objectrefs end")
 
     def get_progress(self):
-        print("get_progress start")
         if self.map_refs:
-            print("wait map_refs start")
             ready, self.map_refs = ray.wait(
                 self.map_refs,
                 timeout=1,
                 num_returns=len(self.map_refs),
                 fetch_local=False)
-            print("wait map_refs end")
             self.num_map += len(ready)
         elif self.reduce_refs:
-            print("wait reduce_refs start")
             ready, self.reduce_refs = ray.wait(
                 self.reduce_refs,
                 timeout=1,
                 num_returns=len(self.reduce_refs),
                 fetch_local=False)
-            print("wait reduce_refs end")
             self.num_reduce += len(ready)
         return self.num_map, self.num_reduce
 
