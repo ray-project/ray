1c99e37b55c64d92f8284aac5b42d61fa912b58a
architkulkarni <arkulkar@gmail.com>
2022-01-03 15:49:41 -0800

    revert unrelated release test fix

--- release/runtime_env_tests/workloads/rte_ray_client.py
+++ release/runtime_env_tests/workloads/rte_ray_client.py
@@ -38,7 +38,8 @@ def test_pip_requirements_files(tmpdir: str):
         import requests
         return requests.__version__
 
-    assert ray.get(get_version.remote()) == "2.16.0"
+    # TODO(architkulkarni): Uncomment after #19002 is fixed
+    # assert ray.get(get_version.remote()) == "2.16.0"
     assert ray.get(
         get_version.options(runtime_env=env_18).remote()) == "2.18.0"
 
@@ -48,11 +49,9 @@ def test_pip_requirements_files(tmpdir: str):
             import requests
             return requests.__version__
 
-    actor_18 = VersionActor.remote()
-    assert ray.get(actor_18.get_version.remote()) == "2.18.0"
-
-    actor_16 = VersionActor.options(runtime_env=env_16).remote()
-    assert ray.get(actor_16.get_version.remote()) == "2.16.0"
+    # TODO(architkulkarni): Uncomment after #19002 is fixed
+    # actor_18 = VersionActor.remote()
+    # assert ray.get(actor_18.get_version.remote()) == "2.18.0"
 
 
 if __name__ == "__main__":
