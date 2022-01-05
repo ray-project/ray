9262794355ac4e49c07544d93f3fc76c049965ad
Qing Wang <kingchin1218@gmail.com>
2022-01-05 13:48:19 +0800

    Add gcs_job_manager_test

--- src/ray/gcs/gcs_server/test/gcs_job_manager_test.cc
+++ src/ray/gcs/gcs_server/test/gcs_job_manager_test.cc
@@ -12,38 +12,52 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "ray/gcs/gcs_server/gcs_job_manager.h"
+
 #include <memory>
 
 #include "gtest/gtest.h"
 #include "ray/gcs/gcs_server/test/gcs_server_test_util.h"
 #include "ray/gcs/test/gcs_test_util.h"
-#include "ray/gcs/gcs_server/gcs_job_manager.h"
 // #include "mock/ray/gcs/store_client/store_client.h"
-#include "ray/gcs/store_client/in_memory_store_client.h"
 #include "ray/common/test_util.h"
+#include "ray/gcs/store_client/in_memory_store_client.h"
 namespace ray {
+
+class MockInMemoryStoreClient : public gcs::InMemoryStoreClient {
+ public:
+  explicit MockInMemoryStoreClient(instrumented_io_context &main_io_service)
+      : gcs::InMemoryStoreClient(main_io_service) {}
+
+  Status AsyncPut(const std::string &table_name, const std::string &key,
+                  const std::string &data, const gcs::StatusCallback &callback) override {
+    callback(Status::OK());
+    return Status::OK();
+  }
+};
 
 class GcsJobManagerTest : public ::testing::Test {
  public:
-  GcsJobManagerTest() :runtime_env_manager_(nullptr) {
+  GcsJobManagerTest() : runtime_env_manager_(nullptr) {
     std::promise<bool> promise;
     thread_io_service_ = std::make_unique<std::thread>([this, &promise] {
       std::unique_ptr<boost::asio::io_service::work> work(
           new boost::asio::io_service::work(io_service_));
       promise.set_value(true);
-      io_service_.run();});
+      io_service_.run();
+    });
     promise.get_future().get();
 
-      gcs_publisher_ = std::make_shared<gcs::GcsPublisher>(
+    gcs_publisher_ = std::make_shared<gcs::GcsPublisher>(
         std::make_unique<GcsServerMocker::MockGcsPubSub>(redis_client_));
-      store_client_ = std::make_shared<gcs::InMemoryStoreClient>(io_service_);
-      gcs_table_storage_ = std::make_shared<gcs::GcsTableStorage>(store_client_);
+    store_client_ = std::make_shared<MockInMemoryStoreClient>(io_service_);
+    gcs_table_storage_ = std::make_shared<gcs::GcsTableStorage>(store_client_);
   }
 
- ~GcsJobManagerTest() {
-   io_service_.stop();
-   thread_io_service_->join();
- }
+  ~GcsJobManagerTest() {
+    io_service_.stop();
+    thread_io_service_->join();
+  }
 
  protected:
   instrumented_io_context io_service_;
@@ -57,34 +71,36 @@ class GcsJobManagerTest : public ::testing::Test {
 };
 
 TEST_F(GcsJobManagerTest, TestGetJobConfig) {
-  gcs::GcsJobManager gcs_job_manager(gcs_table_storage_, gcs_publisher_, runtime_env_manager_);
+  gcs::GcsJobManager gcs_job_manager(gcs_table_storage_, gcs_publisher_,
+                                     runtime_env_manager_);
 
   auto job_id1 = JobID::FromInt(1);
   auto job_id2 = JobID::FromInt(2);
   gcs::GcsInitData gcs_init_data(gcs_table_storage_);
   gcs_job_manager.Initialize(/*init_data=*/gcs_init_data);
-  auto add_job_request1 = Mocker::GenAddJobRequest(
-    job_id1, "namespace_1", 4);
+  auto add_job_request1 = Mocker::GenAddJobRequest(job_id1, "namespace_1", 4);
 
   rpc::AddJobReply empty_reply;
 
-  gcs_job_manager.HandleAddJob(*add_job_request1,
-    &empty_reply, [](Status, std::function<void()>, std::function<void()>) {});
-   auto add_job_request2 = Mocker::GenAddJobRequest(
-    job_id2, "namespace_2", 8);
-  gcs_job_manager.HandleAddJob(*add_job_request2, &empty_reply, [](Status, std::function<void()>, std::function<void()>) {});
+  gcs_job_manager.HandleAddJob(
+      *add_job_request1, &empty_reply,
+      [](Status, std::function<void()>, std::function<void()>) {});
+  auto add_job_request2 = Mocker::GenAddJobRequest(job_id2, "namespace_2", 8);
+  gcs_job_manager.HandleAddJob(
+      *add_job_request2, &empty_reply,
+      [](Status, std::function<void()>, std::function<void()>) {});
 
-  ASSERT_TRUE(true);
-  // EXPECT_TRUE(WaitForCondition([job_id1, &gcs_job_manager]() {
-      // auto job_config1 = gcs_job_manager.GetJobConfig(job_id1);
-      // return "namespace_1" == job_config1->ray_namespace();
-  // }, timeout_ms_.count()));
-  ASSERT_TRUE(true);
+  auto job_config1 = gcs_job_manager.GetJobConfig(job_id1);
+  ASSERT_EQ("namespace_1", job_config1->ray_namespace());
+  ASSERT_EQ(4, job_config1->num_java_workers_per_process());
 
+  auto job_config2 = gcs_job_manager.GetJobConfig(job_id2);
+  ASSERT_EQ("namespace_2", job_config2->ray_namespace());
+  ASSERT_EQ(8, job_config2->num_java_workers_per_process());
 }  // namespace ray
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
-}
+}  // namespace ray
