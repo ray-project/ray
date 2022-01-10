8b598b511fa29ef083d72ca73a6a2ee9648d33c8
Kai Fricke <kai@anyscale.com>
2022-01-10 14:45:42 -0800

    [tune] Test autoscaling behavior for newly added nodes

--- python/ray/tune/tests/test_multinode_sync.py
+++ python/ray/tune/tests/test_multinode_sync.py
@@ -3,6 +3,7 @@ import time
 import unittest
 
 import ray
+from ray import tune
 from ray.autoscaler._private.fake_multi_node.test_utils import DockerCluster
 
 
@@ -51,6 +52,54 @@ class MultiNodeSyncTest(unittest.TestCase):
                     num_cpus=1, num_gpus=1, placement_group=pg).remote(5)))
 
         print("Autoscaling worked")
+
+    def testAutoscalingNewNode(self):
+        """Test that newly added nodes from autoscaling are not stale.
+
+        When new nodes are added to the cluster while a Tune job is running,
+        trials get scheduled on them immediately. However, Tune only
+        updates its internal list of available nodes every 10 seconds.
+
+        The new trial is thus detected as living on a stale node and
+        errored out.
+
+        We fixed thiws behavior by disabling this check. This test
+        previously failed, and we add it now to make sure that autoscaling
+        works as expected.
+        """
+        self.cluster.update_config({
+            "provider": {
+                "head_resources": {
+                    "CPU": 4,
+                    "GPU": 0
+                }
+            },
+            "available_node_types": {
+                "ray.worker.cpu": {
+                    "resources": {
+                        "CPU": 4
+                    },
+                    "min_workers": 0,  # No minimum nodes
+                    "max_workers": 2,
+                },
+                "ray.worker.gpu": {
+                    "min_workers": 0,
+                    "max_workers": 0,  # No GPU nodes
+                }
+            },
+        })
+        self.cluster.start()
+        self.cluster.connect(client=True, timeout=120)
+
+        def autoscaling_train(config):
+            time.sleep(120)
+            tune.report(1.)
+
+        tune.run(
+            autoscaling_train,
+            num_samples=3,
+            resources_per_trial={"cpu": 4},
+            fail_fast=True)
 
 
 if __name__ == "__main__":
