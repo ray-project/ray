6ba45137773eec5ef2698075fe17f6d4d75d2e3c
Antoni Baum <antoni.baum@protonmail.com>
2022-01-11 15:58:20 -0800

    [tune] Load experiment into searcher (#21506)
    This PR adds a new method to the Searcher class, add_evaluated_trials. This method wraps around add_evaluated_point and allows the user to pass a Trial, list of Trials or ExperimentAnalysis to load into the searcher. Furthermore, this PR updates the HEBO version to the latest and removes outdated documentation, and adds add_evaluated_point methods to Dragonfly and SkOpt searchers.

--- python/ray/tune/suggest/suggestion.py
+++ python/ray/tune/suggest/suggestion.py
@@ -2,10 +2,14 @@ import copy
 import glob
 import logging
 import os
-from typing import Dict, Optional, List
+import warnings
+from typing import Dict, Optional, List, Union, Any, TYPE_CHECKING
 
 from ray.tune.suggest.util import set_search_properties_backwards_compatible
 from ray.util.debug import log_once
+if TYPE_CHECKING:
+    from ray.tune.trial import Trial
+    from ray.tune.analysis import ExperimentAnalysis
 
 logger = logging.getLogger(__name__)
 
@@ -211,6 +215,66 @@ class Searcher:
 
         """
         raise NotImplementedError
+
+    def add_evaluated_trials(self, trials_or_analysis: Union["Trial", List[
+            "Trial"], "ExperimentAnalysis"], metric: str):
+        """Pass results from trials that have been evaluated separately.
+
+        This method allows for information from outside the
+        suggest - on_trial_complete loop to be passed to the search
+        algorithm.
+        This functionality depends on the underlying search algorithm
+        and may not be always available (same as ``add_evaluated_point``.)
+
+        Args:
+            trials_or_analysis (Trial|List[Trial]|ExperimentAnalysis):
+                Trials to pass results form to the searcher.
+            metric (str): Metric name reported by trials used for
+                determining the objective value.
+
+        """
+        if self.add_evaluated_point == Searcher.add_evaluated_point:
+            raise NotImplementedError
+
+        # lazy imports to avoid circular dependencies
+        from ray.tune.trial import Trial
+        from ray.tune.analysis import ExperimentAnalysis
+        from ray.tune.result import DONE
+
+        if isinstance(trials_or_analysis, Trial):
+            trials_or_analysis = [trials_or_analysis]
+        elif isinstance(trials_or_analysis, ExperimentAnalysis):
+            trials_or_analysis = trials_or_analysis.trials
+
+        any_trial_had_metric = False
+
+        def trial_to_points(trial: Trial) -> Dict[str, Any]:
+            nonlocal any_trial_had_metric
+            has_trial_been_pruned = (trial.status == Trial.TERMINATED and
+                                     not trial.last_result.get(DONE, False))
+            has_trial_finished = (trial.status == Trial.TERMINATED
+                                  and trial.last_result.get(DONE, False))
+            if not any_trial_had_metric:
+                any_trial_had_metric = (metric in trial.last_result
+                                        and has_trial_finished)
+            if Trial.TERMINATED and metric not in trial.last_result:
+                return None
+            return dict(
+                parameters=trial.config,
+                value=trial.last_result.get(metric, None),
+                error=trial.status == Trial.ERROR,
+                pruned=has_trial_been_pruned,
+                intermediate_values=None,  # we do not save those
+            )
+
+        for trial in trials_or_analysis:
+            kwargs = trial_to_points(trial)
+            if kwargs:
+                self.add_evaluated_point(**kwargs)
+
+        if not any_trial_had_metric:
+            warnings.warn("No completed trial returned the specified metric. "
+                          "Make sure the name you have passed is correct. ")
 
     def save(self, checkpoint_path: str):
         """Save state to path for this search algorithm.
