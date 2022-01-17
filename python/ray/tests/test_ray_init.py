ff41d2a568438f3f9b1f1df3c42963213961c98a
Gagandeep Singh <gdp.1807@gmail.com>
2022-01-17 15:19:28 +0530

    Fixed test in driver script

--- python/ray/tests/test_ray_init.py
+++ python/ray/tests/test_ray_init.py
@@ -106,7 +106,7 @@ def test_tmpdir_env_var(shutdown_only):
         """
 import ray
 context = ray.init()
-assert context["session_dir"].startswith("/tmp/qqq/"), context
+assert context["session_dir"].startswith("/tmp/qqq"), context
 print("passed")
 """,
         env={"RAY_TMPDIR": "/tmp/qqq"})
