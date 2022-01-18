c481c90ef4b1e8e94b0ed13c6da19a2a26534faa
Philipp Moritz <pcmoritz@gmail.com>
2022-01-17 18:47:51 -0800

    shell linting

--- python/ray/autoscaler/kuberay/init-config.sh
+++ python/ray/autoscaler/kuberay/init-config.sh
@@ -7,13 +7,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 DIR=$(mktemp -d -t "kuberay-XXXXXX")
 
-pushd "$DIR"
+pushd "$DIR" || exit
     git clone https://github.com/ray-project/kuberay/
-    pushd "kuberay"
+    pushd "kuberay" || exit
         git checkout 6f87ca64c107cd51d3ab955faf4be198e0094536
         # This would normally better be done with kustomization, but we don't want to make
         # kustomization a dependency for running this.
         git apply "$SCRIPT_DIR/kuberay-autoscaler.patch"
-        cp -r ray-operator/config $SCRIPT_DIR/
-    popd
-popd
+        cp -r ray-operator/config "$SCRIPT_DIR/"
+    popd || exit
+popd || exit
