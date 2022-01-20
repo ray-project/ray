5065156dd91f62f34b2a034ef7729f83d02cef84
Eric Liang <ekhliang@gmail.com>
2022-01-19 23:41:35 -0800

    Set task retry delay to zero (#21690)

--- src/ray/common/ray_config_def.h
+++ src/ray/common/ray_config_def.h
@@ -301,7 +301,7 @@ RAY_CONFIG(uint64_t, local_gc_min_interval_s, 10)
 RAY_CONFIG(uint64_t, global_gc_min_interval_s, 30)
 
 /// Duration to wait between retries for failed tasks.
-RAY_CONFIG(uint32_t, task_retry_delay_ms, 5000)
+RAY_CONFIG(uint32_t, task_retry_delay_ms, 0)
 
 /// Duration to wait between retrying to kill a task.
 RAY_CONFIG(uint32_t, cancellation_retry_ms, 2000)
