de1f8983ed7a591d87c5ee35179ead543317d3b9
Amog Kamsetty <amogkamsetty@yahoo.com>
2022-01-19 19:44:03 -0800

    finish

---
+++ release/train_tests/microbenchmarks/workloads/torch_microbenchmark.py
@@ -0,0 +1,215 @@
+import os
+import json
+from datetime import timedelta
+import time
+
+import torch
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel
+import torch.optim as optim
+import torch.multiprocessing as mp
+import torch.nn.functional as F
+
+from torchvision import models
+
+import ray
+from ray.util.ml_utils.util import find_free_port
+from ray import train
+from ray.train import Trainer
+from ray.train.torch import TorchConfig
+
+from ray.util.placement_group import placement_group
+
+NUM_NODES = 2
+NUM_GPUS_PER_NODE = 4
+
+CONFIG = {
+        "batch_size": 32,
+        "num_epochs": 10,
+        "num_batches_per_epoch": 100
+    }
+
+def torch_setup(rank, world_size, num_workers_per_node, address, port):
+    os.environ["MASTER_ADDR"] = address
+    os.environ["MASTER_PORT"] = str(port)
+
+    print(f"rank: {rank}, world size: {world_size}")
+
+    # initialize the process group
+    dist.init_process_group("nccl", rank=rank,
+                                       world_size=world_size,
+                            init_method="env://",
+                            timeout=timedelta(seconds=30))
+
+    print("finished process group setup")
+    os.environ["CUDA_VISIBLE_DEVICES"] = ",".join([str(i) for i in range(
+        num_workers_per_node)])
+
+def torch_cleanup():
+    dist.destroy_process_group()
+    torch.cuda.empty_cache()
+    del os.environ["CUDA_VISIBLE_DEVICES"]
+
+def torch_prepare_model(model, rank):
+    device = torch.device(f"cuda:{rank}")
+    model = model.to(device)
+    model = DistributedDataParallel(model, device_ids=[rank])
+    return model
+
+def train_func(config):
+    print("Starting Training...")
+
+    mode = config["mode"]
+
+    # Call function to get rank.
+    if mode == "torch":
+        rank = config["rank"]
+    else:
+        rank = train.local_rank()
+
+    # Create model and move to device.
+    device = torch.device(f"cuda:{rank}")
+
+    model = models.resnet50()
+
+    if mode == "torch":
+        model = torch_prepare_model(model, rank)
+    else:
+        model = ray.train.torch.prepare_model(model)
+
+    # Create optimizer.
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
+    loss_fn = F.cross_entropy
+
+    batch_size = config["batch_size"]
+
+    # Set up fixed fake data and move it to the appropriate device.
+    data = torch.randn(batch_size, 3, 224, 224)
+    target = torch.randint(high=1000, size=(batch_size,), dtype=torch.long)
+    data, target = data.to(device), target.to(device)
+
+    num_epochs = config["num_epochs"]
+    num_batches_per_epoch = config["num_batches_per_epoch"]
+
+
+    for i in range(num_epochs):
+        print("Starting epoch ", i)
+        current_time = time.time()
+        for _ in range(num_batches_per_epoch):
+            optimizer.zero_grad()
+            output = model(data)
+            loss = loss_fn(output, target)
+            loss.backward()
+            optimizer.step()
+        epoch_time = time.time() - current_time
+        img_sec = batch_size * num_batches_per_epoch / epoch_time
+        print("Iter #%d: %.1f img/sec per %s" % (i, img_sec, device))
+
+
+def torch_train(rank, world_size, num_workers_per_node, address, port):
+    """Training function for vanilla PyTorch."""
+    print("Setting up distributed torch.")
+    torch_setup(rank=rank, world_size=world_size,
+                num_workers_per_node=num_workers_per_node, address=address,
+                port=port)
+    config = CONFIG.copy()
+    config["mode"] = "torch"
+    config["rank"] = rank
+    train_func(config)
+    torch_cleanup()
+
+
+def torch_run_single_node(train_func, world_size, num_workers_per_node,
+                         address, port):
+    current_time = time.time()
+    mp.spawn(train_func,
+                 args=(world_size,num_workers_per_node,address,port),
+                 nprocs=num_workers_per_node,
+                 join=True)
+    total_time = time.time() - current_time
+    print("Total time for vanilla torch.distributed: ", time.time() -
+          current_time)
+    return total_time
+
+def ray_train_run(train_func, world_size):
+    current_time = time.time()
+    trainer = Trainer(num_workers=world_size, use_gpu=True,
+                      backend=TorchConfig(backend="nccl"))
+    trainer.start()
+
+    config = CONFIG.copy()
+    config["mode"] = "ray"
+    trainer.run(train_func, config=config)
+    total_time = time.time() - current_time
+    print("Total time for Ray Train: ", total_time)
+    return total_time
+
+def single_node_benchmark():
+    import ray
+    ray.init("auto")
+
+    # Time torch.distributed using torch.multiprocessing.
+    address = "127.0.0.1"
+    port = find_free_port()
+    torch_time = torch_run_single_node(train_func=torch_train, world_size=4,
+                           num_workers_per_node=4,
+                           address=address, port=port)
+
+    # Time using Ray Train.
+    ray_time = ray_train_run(train_func=train_func, world_size=4)
+
+    # Make sure that the torch.distributed time and Ray Train time are
+    # within 5% of each other.
+    assert abs(torch_time - ray_time) <= min(0.05 * torch_time,
+                                             0.05 * ray_time), \
+        f"torch.distributed time: {torch_time}, Ray Train time: {ray_time}"
+
+    with open(os.environ["TEST_OUTPUT_JSON"], "w") as f:
+        f.write(
+            json.dumps({"torch_time": torch_time, "ray_train_time": ray_time}))
+
+def torch_run_distributed(train_func, num_nodes, num_workers_per_node):
+    # Use Ray tasks and placement groups to set up distributed training.
+    # This allows us to run commands on all nodes in the cluster.
+    per_node_bundle = {"CPU": num_workers_per_node, "GPU": num_workers_per_node}
+    bundles = [per_node_bundle] * num_nodes
+    pg = placement_group(bundles, strategy="STRICT_SPREAD")
+    ray.get(pg.ready())
+
+    # Use current node for the address and port.
+    address = ray.util.get_node_ip_address()
+    port = find_free_port()
+
+    remote_tasks = []
+    for node in range(num_nodes):
+        remote_task = ray.remote(torch_run_single_node).options(
+            placement_group_bundle_index=node)
+        remote_tasks.append(remote_task)
+
+    current_time = time.time()
+    # Ray grpc overhead is negligible.
+    ray.get([task.remote(train_func=torch_train,
+                         world_size=num_nodes*num_workers_per_node,
+                         num_workers_per_node=num_workers_per_node,
+                         address=address, port=port) for
+             task in remote_tasks])
+    total_time = time.time() - current_time
+    return total_time
+
+
+def multi_node_benchmark():
+    import ray
+    ray.init("auto")
+
+    # Time using torch.distributed.
+    torch_time = torch_run_distributed(train_func=torch_train, world_size=4,
+                                       num_workers_per_node=4,
+                                       address=address, port=port)
+
+
+    # Time using Ray Train.
+    ray_time = ray_train_run(train_func=train_func, world_size=8)
+
+
+if __name__ == "__main__":
+    single_node_benchmark()
