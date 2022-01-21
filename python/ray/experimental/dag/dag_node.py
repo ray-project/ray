70b6f4dfaa68a61b28de82f257e20d523857c681
Eric Liang <ekhliang@gmail.com>
2022-01-21 15:55:03 -0800

    add nested arg suppor

--- python/ray/experimental/dag/dag_node.py
+++ python/ray/experimental/dag/dag_node.py
@@ -1,5 +1,7 @@
 import ray
 
+import io
+import pickle
 from typing import Union, List, Dict, Any, TypeVar, Callable
 
 T = TypeVar("T")
@@ -30,23 +32,19 @@ class DAGNode:
         if _cache is None:
             _cache = {}
 
-        if self not in _cache:
-            new_args, new_kwargs = [], {}
-            # TODO(ekl) handle nested args.
-            for a in self._bound_args:
-                if isinstance(a, DAGNode):
-                    new_args.append(a.transform_up(visitor, _cache))
-                else:
-                    new_args.append(a)
-            for k, v in self._bound_kwargs:
-                if isinstance(v, DAGNode):
-                    new_kwargs[k] = v.transform_up(visitor, _cache)
-                else:
-                    new_kwargs[k] = v
+        # Find all first-level nested DAGNode children in args.
+        f = _PyObjFindReplace()
+        children = f.find_nodes([self._bound_args, self._bound_kwargs])
 
-            _cache[self] = visitor(self.copy(new_args, new_kwargs))
+        # Update replacement table and execute the replace.
+        for node in children:
+            if node not in _cache:
+                new_node = node.transform_up(visitor, _cache)
+                _cache[node] = new_node
+        new_args, new_kwargs = f.replace_nodes(_cache)
 
-        return _cache[self]
+        # Apply visitor after args have been recursively updated.
+        return visitor(self.copy(new_args, new_kwargs))
 
     def execute(self) -> Union[ray.ObjectRef, ray.actor.ActorHandle]:
         """Execute this DAG using the Ray default executor."""
@@ -73,3 +71,52 @@ class DAGNode:
         serializable form.
         """
         raise ValueError("DAGNode cannot be serialized.")
+
+
+class _PyObjFindReplace(ray.cloudpickle.CloudPickler):
+    """Utility to find and replace DAGNodes in Python objects.
+
+    This uses pickle to walk the PyObj graph and find first-level DAGNode
+    instances on ``find_nodes()``. The caller can then compute a replacement
+    table and then replace the nodes via ``replace_nodes()``.
+    """
+
+    # TODO(ekl) static instance ref used in deserialization hook.
+    _cur = None
+
+    def __init__(self):
+        # Buffer to keep intermediate serialized state.
+        self._buf = io.BytesIO()
+        # List of top-level DAGNodes found during the serialization pass.
+        self._found = None
+        # Replacement table to consult during deserialization.
+        self._replace_table: Dict[DAGNode, T] = None
+        super().__init__(self._buf)
+
+    def find_nodes(self, obj: Any) -> List[DAGNode]:
+        """Find top-level DAGNodes."""
+        assert self._found is None, "find_nodes cannot be called twice"
+        self._found = []
+        self.dump(obj)
+        return self._found
+
+    def replace_nodes(self, table: Dict[DAGNode, T]) -> Any:
+        """Replace previously found DAGNodes per the given table."""
+        assert self._found is not None, "find_nodes must be called first"
+        _PyObjFindReplace._cur = self
+        self._replace_table = table
+        self._buf.seek(0)
+        return pickle.load(self._buf)
+
+    def _replace_index(self, i: int) -> DAGNode:
+        return self._replace_table[self._found[i]]
+
+    def reducer_override(self, obj):
+        if isinstance(obj, DAGNode):
+            index = len(self._found)
+            res = (lambda i: _PyObjFindReplace._cur._replace_index(i)), (
+                index, )
+            self._found.append(obj)
+            return res
+        else:
+            return super().reducer_override(obj)
--- python/ray/experimental/dag/test_dag.py
+++ python/ray/experimental/dag/test_dag.py
@@ -5,6 +5,18 @@ import ray
 from ray.experimental.dag.dag_node import DAGNode
 
 
+@ray.remote
+class Counter:
+    def __init__(self):
+        self.i = 0
+
+    def inc(self):
+        self.i += 1
+
+    def get(self):
+        return self.i
+
+
 def test_serialize_warning():
     node = DAGNode([], {})
     with pytest.raises(ValueError):
@@ -12,17 +24,6 @@ def test_serialize_warning():
 
 
 def test_basic_task_dag():
-    @ray.remote
-    class Counter:
-        def __init__(self):
-            self.i = 0
-
-        def inc(self):
-            self.i += 1
-
-        def get(self):
-            return self.i
-
     ct = Counter.remote()
 
     @ray.remote
@@ -49,6 +50,39 @@ def test_basic_task_dag():
     b_ref = b._bind(a_ref)
     c_ref = c._bind(a_ref)
     dag = d._bind(b_ref, c_ref)
+    print(dag.tree_string())
+
+    assert ray.get(dag.execute()) == 7
+    assert ray.get(ct.get.remote()) == 4
+
+
+def test_nested_args():
+    ct = Counter.remote()
+
+    @ray.remote
+    def a():
+        ray.get(ct.inc.remote())
+        return 2
+
+    @ray.remote
+    def b(**kwargs):
+        ray.get(ct.inc.remote())
+        return kwargs["x"] * 2
+
+    @ray.remote
+    def c(**kwargs):
+        ray.get(ct.inc.remote())
+        return kwargs["x"] + 1
+
+    @ray.remote
+    def d(nested):
+        ray.get(ct.inc.remote())
+        return ray.get(nested["x"]) + ray.get(nested["y"])
+
+    a_ref = a._bind()
+    b_ref = b._bind(x=a_ref)
+    c_ref = c._bind(x=a_ref)
+    dag = d._bind({"x": b_ref, "y": c_ref})
     print(dag.tree_string())
 
     assert ray.get(dag.execute()) == 7
