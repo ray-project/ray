ab36b69591c0401d43ff55a864f612523ade4f7b
Alex Wu <alex@anyscale.com>
2022-01-21 18:33:08 -0800

    test

--- python/ray/autoscaler/sdk/__init__.py
+++ python/ray/autoscaler/sdk/__init__.py
@@ -1,3 +1,10 @@
-from ray.autoscaler.sdk.sdk import (create_or_update_cluster, teardown_cluster, run_on_cluster, rsync, get_head_node_ip, get_worker_node_ips, request_resources, configure_logging, bootstrap_config)
+from ray.autoscaler.sdk.sdk import (create_or_update_cluster, teardown_cluster,
+                                    run_on_cluster, rsync, get_head_node_ip,
+                                    get_worker_node_ips, request_resources,
+                                    configure_logging, bootstrap_config)
 
-__all__ = ["create_or_update_cluster", "teardown_cluster", "run_on_cluster", "rsync", "get_head_node_ip", "get_worker_node_ips", "request_resources", "configure_logging", "bootstrap_config"]
+__all__ = [
+    "create_or_update_cluster", "teardown_cluster", "run_on_cluster", "rsync",
+    "get_head_node_ip", "get_worker_node_ips", "request_resources",
+    "configure_logging", "bootstrap_config"
+]
