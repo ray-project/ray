6086062833ab73e9aaee2414ecff2a777642147b
Yi Cheng <chengyidna@gmail.com>
2022-01-28 22:41:22 +0000

    update

--- release/nightly_tests/stress_tests/test_logging.py
+++ release/nightly_tests/stress_tests/test_logging.py
@@ -3,7 +3,7 @@ import argparse
 
 import random
 import string
-
+import logging
 parser = argparse.ArgumentParser(prog="Stress Test - Logging")
 parser.add_argument(
     "--total-num-tasks",
@@ -17,15 +17,13 @@ parser.add_argument(
     required=True)
 
 parser.add_argument(
-    "--log-line-size",
-    type=int,
-    help="Size of a log",
-    required=True)
+    "--log-line-size", type=int, help="Size of a log", required=True)
 
 args = parser.parse_args()
 ray.init(address="auto")
-import logging
+
 logger = logging.getLogger(__name__)
+
 
 @ray.remote
 def gen_logs(log_size, log_num):
@@ -33,7 +31,11 @@ def gen_logs(log_size, log_num):
     letters = string.ascii_uppercase + string.digits
 
     for _ in range(log_num):
-        log_line = ''.join(random.choice(letters) for _ in range(log_size))
+        log_line = "".join(random.choice(letters) for _ in range(log_size))
         logger.error(log_line)
 
-ray.get([gen_logs.remote(args.log_line_size, args.log_lines_per_task) for _ in range(args.total_num_tasks)])
+
+ray.get([
+    gen_logs.remote(args.log_line_size, args.log_lines_per_task)
+    for _ in range(args.total_num_tasks)
+])
