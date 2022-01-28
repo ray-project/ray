a2b31edf62d926f17112cfe74ba06e1cf24b135b
architkulkarni <arkulkar@gmail.com>
2022-01-28 12:59:14 -0800

    lint

--- python/ray/serve/examples/doc/snippet_model_composition.py
+++ python/ray/serve/examples/doc/snippet_model_composition.py
@@ -22,36 +22,39 @@ def model_one(data):
 @serve.deployment
 def model_two(data):
     print(f"Model 2 called with data:{data}")
-    # Use this data sent from model_one
+    # Use this data sent from model_one.
     return data
 
 
 # max_concurrent_queries is optional. By default, if you pass in an async
 # function, Ray Serve sets the limit to a high number.
-@serve.deployment(max_concurrent_queries=10, route_prefix='/composed')
+@serve.deployment(max_concurrent_queries=10, route_prefix="/composed")
 class ComposedModel:
     def __init__(self):
-        # Use the Python ServeHandle APIs
-        # Set sync=False to override default, which is use this in a synchronous mode.
-        # We want these deployments to be run within a asynchronous event loop for concurrency
-        # See documentation for Sync Async ServeHandle APIs for details:
-        # https://docs.ray.io/en/master/serve/http-servehandle.html#sync-and-async-handles
+        # Use the Python ServeHandle APIs.
+        # Set sync=False to override default, which is use this in a
+        # synchronous mode.  We want these deployments to be run within an
+        # asynchronous event loop for concurrency. See documentation for Sync
+        # and Async ServeHandle APIs for details:
+        # https://docs.ray.io/en/latest/serve/http-servehandle.html
         self.model_one = model_one.get_handle(sync=False)
         self.model_two = model_two.get_handle(sync=False)
 
-    # This method can be called concurrently
+    # This method can be called concurrently.
     async def __call__(self, starlette_request):
-        # at this point you are yielding to the event loop take in anothe request
+        # At this point you are yielding to the event loop to take in another
+        # request.
         data = await starlette_request.body()
 
         # Use await twice here for two reasons:
-        # 1. Since we are running within a async def callable function and we want to use
-        # this model_one deployment to run in an asynchronous fashion, this is standard
-        # async-await pattern. This await call will return an ObjectRef.
-        # 2. The second await waits on the ObjectRef to do an implicit ray.get(Object) to
-        # fetch the actual value returned.
+        # 1. Since we are running within a async def callable function and we
+        # want to use this model_one deployment to run in an asynchronous
+        # fashion, this is standard async-await pattern. This await call will
+        # return an ObjectRef.
+        # 2. The second await waits on the ObjectRef to do an implicit
+        # ray.get(Object) to fetch the actual value returned.
         # Hence two awaits.
-        score = await(await self.model_one.remote(data=data))
+        score = await (await self.model_one.remote(data=data))
         if score > 0.5:
             await (await self.model_two.remote(data=data))
             result = {"model_used: 1 & 2;  score": score}
@@ -61,25 +64,24 @@ class ComposedModel:
         return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    # start ray with 8 processes
+    # Start ray with 8 processes.
     if ray.is_initialized:
         ray.shutdown()
     ray.init(num_cpus=8)
     serve.start()
-    # Start deployment instances
+    # Start deployment instances.
     model_one.deploy()
     model_two.deploy()
     ComposedModel.deploy()
 
-    # Now send requests
+    # Now send requests.
     for _ in range(8):
         resp = requests.get("http://127.0.0.1:8000/composed", data="Hey!")
         print(resp.json())
 
     ray.shutdown()
-
 
 # Output
 # {'model_used: 1 ; score': 0.20814435670233788}
