6ffdd300fc95f97be6e8255cafb1e052aa4130ac
Jiajun Yao <jeromeyjj@gmail.com>
2022-01-28 16:17:09 -0800

    Remove work pipelining

--- src/ray/common/ray_config_def.h
+++ src/ray/common/ray_config_def.h
@@ -326,6 +326,9 @@ RAY_CONFIG(int64_t, enable_metrics_collection, true)
 // Max number bytes of inlined objects in a task rpc request/response.
 RAY_CONFIG(int64_t, task_rpc_inlined_bytes_limit, 10 * 1024 * 1024)
 
+/// This is deprecated and will be removed in the future release.
+RAY_CONFIG(uint32_t, max_tasks_in_flight_per_worker, 1)
+
 /// Maximum number of pending lease requests per scheduling category
 RAY_CONFIG(uint64_t, max_pending_lease_requests_per_scheduling_category, 10)
 
