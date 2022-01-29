dd27c02d064090c00cf92042fe560b3037943979
Eric Liang <ekhliang@gmail.com>
2022-01-28 18:32:59 -0800

    fix

--- python/ray/data/tests/test_dataset.py
+++ python/ray/data/tests/test_dataset.py
@@ -76,10 +76,10 @@ def test_bulk_lazy_eval_split_mode(shutdown_only, block_split, tmp_path):
 @pytest.mark.parametrize("pipelined", [False, True])
 def test_basic_actors(shutdown_only, pipelined):
     ray.init(num_cpus=2)
-    ds = ray.data.range(5)
+    ds = ray.data.range(20)
     ds = maybe_pipeline(ds, pipelined)
-    assert sorted(ds.map(lambda x: x + 1,
-                         compute="actors").take()) == [1, 2, 3, 4, 5]
+    assert sorted(ds.map(lambda x: x + 1, compute="actors").take()) == range(
+        1, 20)
 
 
 @pytest.mark.parametrize("pipelined", [False, True])
