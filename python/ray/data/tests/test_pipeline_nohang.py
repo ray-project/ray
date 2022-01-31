89f4c7740ff51d1810617ca736d72e410cea633a
Eric Liang <ekhliang@gmail.com>
2022-01-30 17:13:52 -0800

    format

--- python/ray/data/tests/test_pipeline_nohang.py
+++ python/ray/data/tests/test_pipeline_nohang.py
@@ -14,11 +14,12 @@ def test_basic_actors(shutdown_only):
     for _ in range(NUM_REPEATS):
         ds = ray.data.range(NUM_TASKS)
         ds = ds.window(blocks_per_window=1)
-        assert sorted(ds.map(lambda x: x + 1,
-                             compute="actors").take()) == list(
-                                 range(1, NUM_TASKS + 1))
+        assert sorted(ds.map(lambda x: x + 1, compute="actors").take()) == list(
+            range(1, NUM_TASKS + 1)
+        )
 
 
 if __name__ == "__main__":
     import sys
+
     sys.exit(pytest.main(["-v", __file__]))
