c86b370665a1cb03435d9f1b222c72367bfb3cf8
Jun Gong <jungong@anyscale.com>
2022-02-01 02:09:23 -0800

    lint

--- rllib/offline/resource.py
+++ rllib/offline/resource.py
@@ -5,7 +5,9 @@ from ray.rllib.utils.typing import PartialTrainerConfigDict
 from typing import Dict, List
 
 
-def get_offline_io_resource_bundles(config: PartialTrainerConfigDict) -> List[Dict]:
+def get_offline_io_resource_bundles(
+    config: PartialTrainerConfigDict
+) -> List[Dict[str, float]]:
     # DatasetReader is the only offline I/O component today that
     # requires compute resources.
     if config["input"] == "dataset":
