a4a86d65567b336129a71b653a6fcee472024b4a
Max Pumperla <max.pumperla@googlemail.com>
2022-02-03 09:13:04 +0100

    merge
    Signed-off-by: Max Pumperla <max.pumperla@googlemail.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -65,28 +65,27 @@ extensions = [
     "sphinxcontrib.yt",
     "versionwarning.extension",
     "sphinx_sitemap",
-    "myst_parser",
-    # "myst_nb",
+    "myst_nb",
     "sphinx.ext.doctest",
     "sphinx.ext.coverage",
     "sphinx_external_toc",
 ]
 
 myst_enable_extensions = [
-    # "dollarmath",
-    # "amsmath",
-    # "deflist",
-    # "fieldlist",
+    "dollarmath",
+    "amsmath",
+    "deflist",
     "html_admonition",
     "html_image",
-    # "colon_fence",
-    # "smartquotes",
-    # "replacements",
-    # "linkify",
-    # "strikethrough",
-    # "substitution",
-    # "tasklist",
+    "colon_fence",
+    "smartquotes",
+    "replacements",
 ]
+
+# Cache notebook outputs in _build/.jupyter_cache
+# To prevent notebook execution, set this to "off". To force re-execution, set this to "force".
+# To cache previous runs, set this to "cache".
+jupyter_execute_notebooks = os.getenv("RUN_NOTEBOOKS", "off")
 
 external_toc_exclude_missing = False
 external_toc_path = '_toc.yml'
@@ -240,6 +239,11 @@ html_theme_options = {
     "path_to_docs": "doc/source",
     "home_page_in_toc": False,
     "show_navbar_depth": 0,
+    "launch_buttons": {
+        "notebook_interface": "jupyterlab",
+        "binderhub_url": "https://mybinder.org",
+        "colab_url": "https://colab.research.google.com",
+    },
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
@@ -396,6 +400,9 @@ autodoc_member_order = "bysource"
 
 
 def setup(app):
+    # myst-nb adds notebooks into the registry, which leads to sphinx-gallery warnings
+    app.registry.source_suffix.pop(".ipynb", None)
+
     app.connect("html-page-context", update_context)
     # Custom CSS
     app.add_css_file("css/custom.css", priority=800)
