2d9c7df70c2c4fd6cbdfb3dbac26031b5d71b6b9
Mingwei Tian <mwtian@anyscale.com>
2022-02-04 09:25:44 -0800

    fix

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -143,9 +143,6 @@ class Worker:
         # running on.
         self.ray_debugger_external = False
         self._load_code_from_local = False
-        # Used to toggle whether or not logs should be filtered to only those
-        # produced in the same job.
-        self.filter_logs_by_job = True
 
     @property
     def connected(self):
@@ -502,11 +499,7 @@ class Worker:
                     data = json.loads(ray._private.utils.decode(msg["data"]))
 
                 # Don't show logs from other drivers.
-                if (
-                    self.filter_logs_by_job
-                    and data["job"]
-                    and job_id_hex != data["job"]
-                ):
+                if data["job"] and data["job"] != job_id_hex:
                     num_consecutive_messages_received = 0
                     last_polling_batch_size = 0
                     continue
