fa3293074a149e9eecab0dee8d2af02a3679de24
Jiajun Yao <jeromeyjj@gmail.com>
2022-02-04 23:53:12 +0000

    Spread ray.experimental.shuffle map and reduce tasks

--- python/ray/experimental/shuffle.py
+++ python/ray/experimental/shuffle.py
@@ -184,14 +184,14 @@ def simple_shuffle(
         List of outputs from the output writers.
     """
 
-    @ray.remote(num_returns=output_num_partitions)
+    @ray.remote(num_returns=output_num_partitions, scheduling_strategy="SPREAD")
     def shuffle_map(i: PartitionID) -> List[List[Union[Any, ObjectRef]]]:
         writers = [object_store_writer() for _ in range(output_num_partitions)]
         for out_i, item in partitioner(input_reader(i), output_num_partitions):
             writers[out_i].add(item)
         return [c.finish() for c in writers]
 
-    @ray.remote
+    @ray.remote(scheduling_strategy="SPREAD")
     def shuffle_reduce(
         i: PartitionID, *mapper_outputs: List[List[Union[Any, ObjectRef]]]
     ) -> OutType:
