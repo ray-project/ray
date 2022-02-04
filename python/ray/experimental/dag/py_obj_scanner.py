ad98ce4bcd048f030e8eb5afd617482078837907
Eric Liang <ekhliang@gmail.com>
2022-02-03 18:10:54 -0800

    make py3.6 compatible

--- python/ray/experimental/dag/py_obj_scanner.py
+++ python/ray/experimental/dag/py_obj_scanner.py
@@ -1,11 +1,15 @@
 import ray
 
+import copyreg
+import uuid
 import io
 import pickle
-from typing import List, Dict, Any, TypeVar
+from typing import List, Dict, Any, TypeVar, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ray.experimental.dag.dag_node import DAGNode
 
 T = TypeVar("T")
-DAGNodeT = TypeVar("DAGNodeT")
 
 
 class _PyObjScanner(ray.cloudpickle.CloudPickler):
@@ -16,43 +20,49 @@ class _PyObjScanner(ray.cloudpickle.CloudPickler):
     table and then replace the nodes via ``replace_nodes()``.
     """
 
-    # XXX(ekl) static instance ref used in deserialization hook.
-    _cur = None
+    # Used in deserialization hooks to reference scanner instances.
+    _instances: Dict[str, "_PyObjScanner"] = {}
 
-    def __init__(self, dag_node_type: DAGNodeT):
+    def __init__(self, dag_node_type: "DAGNode"):
         # Buffer to keep intermediate serialized state.
         self._buf = io.BytesIO()
         # List of top-level DAGNodes found during the serialization pass.
         self._found = None
         # Replacement table to consult during deserialization.
-        self._replace_table: Dict[DAGNodeT, T] = None
-        # Type of the DAGNode.
-        self._dag_node_type: DAGNodeT = dag_node_type
+        self._replace_table: Dict["DAGNode", T] = None
+        # UUID of this scanner.
+        self._uuid = uuid.uuid4().hex
+        _PyObjScanner._instances[self._uuid] = self
         super().__init__(self._buf)
+        # Register pickler override for DAGNodes.
+        from ray.experimental.dag.dag_node import DAGNode
 
-    def find_nodes(self, obj: Any) -> List[DAGNodeT]:
+        self.dispatch_table = copyreg.dispatch_table.copy()
+        self.dispatch_table[DAGNode] = self._reduce_dag_node
+
+    def find_nodes(self, obj: Any) -> List["DAGNode"]:
         """Find top-level DAGNodes."""
         assert self._found is None, "find_nodes cannot be called twice"
         self._found = []
         self.dump(obj)
         return self._found
 
-    def replace_nodes(self, table: Dict[DAGNodeT, T]) -> Any:
+    def replace_nodes(self, table: Dict["DAGNode", T]) -> Any:
         """Replace previously found DAGNodes per the given table."""
         assert self._found is not None, "find_nodes must be called first"
-        _PyObjScanner._cur = self
         self._replace_table = table
         self._buf.seek(0)
         return pickle.load(self._buf)
 
-    def _replace_index(self, i: int) -> DAGNodeT:
+    def _replace_index(self, i: int) -> "DAGNode":
         return self._replace_table[self._found[i]]
 
-    def reducer_override(self, obj):
-        if isinstance(obj, self._dag_node_type):
-            index = len(self._found)
-            res = (lambda i: _PyObjScanner._cur._replace_index(i)), (index,)
-            self._found.append(obj)
-            return res
-        else:
-            return super().reducer_override(obj)
+    def _reduce_dag_node(self, obj):
+        uuid = self._uuid
+        index = len(self._found)
+        res = (lambda i: _PyObjScanner._instances[uuid]._replace_index(i)), (index,)
+        self._found.append(obj)
+        return res
+
+    def __del__(self):
+        del _PyObjScanner._instances[self._uuid]
