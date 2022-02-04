54f608c20a834ba82eccac26551792b98e41db85
sven1977 <svenmika1977@gmail.com>
2022-02-04 11:37:29 +0100

    wip.

--- rllib/agents/a3c/a3c.py
+++ rllib/agents/a3c/a3c.py
@@ -3,13 +3,18 @@ from typing import Type
 
 from ray.rllib.agents.a3c.a3c_tf_policy import A3CTFPolicy
 from ray.rllib.agents.trainer import Trainer, with_common_config
+from ray.rllib.evaluation.rollout_worker import RolloutWorker
 from ray.rllib.evaluation.worker_set import WorkerSet
+from ray.rllib.execution.metric_ops import StandardMetricsReporting
+from ray.rllib.execution.parallel_requests import asynchronous_parallel_requests
 from ray.rllib.execution.rollout_ops import AsyncGradients
 from ray.rllib.execution.train_ops import ApplyGradients
-from ray.rllib.execution.metric_ops import StandardMetricsReporting
 from ray.rllib.policy.policy import Policy
 from ray.rllib.utils.annotations import override
-from ray.rllib.utils.typing import TrainerConfigDict
+from ray.rllib.utils.metrics import APPLY_GRADS_TIMER, GRAD_WAIT_TIMER, \
+    NUM_AGENT_STEPS_SAMPLED, NUM_ENV_STEPS_SAMPLED, \
+    SYNCH_WORKER_WEIGHTS_TIMER
+from ray.rllib.utils.typing import ResultDict, TrainerConfigDict
 from ray.util.iter import LocalIterator
 
 logger = logging.getLogger(__name__)
@@ -39,11 +44,16 @@ DEFAULT_CONFIG = with_common_config({
     "entropy_coeff": 0.01,
     # Entropy coefficient schedule
     "entropy_coeff_schedule": None,
-    # Min time per reporting
-    "min_time_s_per_reporting": 5,
+    # Min time (in seconds) per reporting.
+    # This causes not every call to `training_iteration` to be reported,
+    # but to wait until n seconds have passed and then to summarize the
+    # thus far collected results.
+    "min_time_s_per_reporting": 1,
     # Workers sample async. Note that this increases the effective
     # rollout_fragment_length by up to 5x due to async buffering of batches.
     "sample_async": True,
+    #TODO
+    "_disable_execution_plan_api": True,
 })
 # __sphinx_doc_end__
 # yapf: enable
@@ -92,3 +102,62 @@ class A3CTrainer(Trainer):
         train_op = grads.for_each(ApplyGradients(workers, update_all=False))
 
         return StandardMetricsReporting(train_op, workers, config)
+
+    def training_iteration(self) -> ResultDict:
+
+        # Shortcut.
+        local_worker = self.workers.local_worker()
+
+        def sample_and_compute_grads(worker: RolloutWorker):
+            """Call sample() and compute_gradients() remotely on workers."""
+            samples = worker.sample()
+            grads, infos = worker.compute_gradients(samples)
+            return {
+                "grads": grads,
+                "infos": infos,
+                "agent_steps": samples.agent_steps(),
+                "env_steps": samples.env_steps(),
+            }
+
+        # Perform rollouts and gradient calculations asynchronously.
+        with self._timers[GRAD_WAIT_TIMER]:
+            results = asynchronous_parallel_requests(
+                remote_requests_in_flight=self.remote_requests_in_flight,
+                actors=self.workers.remote_workers(),
+                ray_wait_timeout_s=0.0,
+                max_remote_requests_in_flight_per_actor=2,
+                remote_fn=sample_and_compute_grads,
+            )
+
+        global_vars = None
+        # Loop through all fetched worker-computed gradients (if any)
+        # and apply them - one by one - to the local worker's model.
+
+        result = None #TODO
+        for worker, result in results.items():
+            # Apply gradients to local worker.
+            with self._timers[APPLY_GRADS_TIMER]:
+                local_worker.apply_gradients(result["grads"])
+            self._timers[APPLY_GRADS_TIMER].push_units_processed(result["agent_steps"])
+
+            # Update step counters.
+            self._counters[NUM_AGENT_STEPS_SAMPLED] += result["agent_steps"]
+            self._counters[NUM_ENV_STEPS_SAMPLED] += result["env_steps"]
+            # Create current global vars.
+            global_vars = {
+                "timestep": self._counters[NUM_AGENT_STEPS_SAMPLED],
+            }
+
+            # Synch updated weights back to the particular worker.
+            with self._timers[SYNCH_WORKER_WEIGHTS_TIMER]:
+                weights = local_worker.get_weights(
+                    local_worker.get_policies_to_train()
+                )
+                worker.set_weights.remote(weights, global_vars)
+
+        # Update global vars of the local worker.
+        if global_vars:
+            local_worker.set_global_vars(global_vars)
+
+        #TODO: If we have processed more than one gradients
+        return result["infos"] if result else {}
