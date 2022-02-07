668136a42630381e828db61bb08b298390ba1d8b
Max Pumperla <max.pumperla@googlemail.com>
2022-02-07 10:59:49 +0100

    lint
    Signed-off-by: Max Pumperla <max.pumperla@googlemail.com>

--- python/ray/tune/tests/example.py
+++ python/ray/tune/tests/example.py
@@ -18,10 +18,11 @@ def objective(config):
     score = config["a"] ** 2 + config["b"]
     return {"score": score}
 
+
 # 2. Define a search space.
 search_space = {
     "a": tune.grid_search([0.001, 0.01, 0.1, 1.0]),
-    "b": tune.choice([1, 2, 3])
+    "b": tune.choice([1, 2, 3]),
 }
 
 # 3. Start a Tune run and print the best result.
