494c3ccb83e7cac34f37aa58c291a645467f4fa1
Gagandeep Singh <gdp.1807@gmail.com>
2022-02-10 16:16:08 +0530

    Detect bazel by paths and not environment variables

--- python/ray/tests/test_typing.py
+++ python/ray/tests/test_typing.py
@@ -9,7 +9,10 @@ TYPING_TEST_DIRS = os.path.join(os.path.dirname(__file__), "typing_files")
 
 def test_typing_good():
     script = "check_typing_good.py"
-    if os.getenv("RUNNING_BAZEL", None) != "true":
+    # the following check indicates that check_typing_good.py
+    # is not present in the current directory, so follow
+    # the path specified TYPING_TEST_DIRS
+    if not os.path.exists(script):
         script = os.path.join(TYPING_TEST_DIRS, script)
     _, msg, status_code = mypy_api.run([script])
     assert status_code == 0, msg
@@ -17,7 +20,10 @@ def test_typing_good():
 
 def test_typing_bad():
     script = "check_typing_bad.py"
-    if os.getenv("RUNNING_BAZEL", None) != "true":
+    # the following check indicates that check_typing_bad.py
+    # is not present in the current directory, so follow
+    # the path specified TYPING_TEST_DIRS
+    if not os.path.exists(script):
         script = os.path.join(TYPING_TEST_DIRS, script)
     _, msg, status_code = mypy_api.run([script])
     assert status_code == 1, msg
@@ -27,11 +33,14 @@ if __name__ == "__main__":
     # Make subprocess happy in bazel.
     os.environ["LC_ALL"] = "en_US.UTF-8"
     os.environ["LANG"] = "en_US.UTF-8"
-    os.environ["RUNNING_BAZEL"] = "true"
     # copying is necessary because otherwise, ray is imported from the source
     # files in directory, `Bazel.runfiles_*/runfiles/com_github_ray_project_ray/python`
     # and mypy is unable to detect attributes that should be present in ray.
     typing_files_dir = "python/ray/tests/typing_files/"
-    shutil.copy(typing_files_dir + "check_typing_good.py", os.getcwd())
-    shutil.copy(typing_files_dir + "check_typing_bad.py", os.getcwd())
+    checking_typing_good = typing_files_dir + "check_typing_good.py"
+    checking_typing_bad = typing_files_dir + "check_typing_bad.py"
+    if os.path.exists(checking_typing_good):
+        shutil.copy(checking_typing_good, os.getcwd())
+    if os.path.exists(checking_typing_bad):
+        shutil.copy(checking_typing_bad, os.getcwd())
     sys.exit(pytest.main(["-v", __file__]))
