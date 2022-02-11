b0d6515b454d5e28889b17cc665148639393b4ab
Jialin Liu <jialin.liu@bytedance.com>
2022-02-10 20:13:39 -0800

    fix lint

--- python/ray/data/tests/test_dataset.py
+++ python/ray/data/tests/test_dataset.py
@@ -1008,8 +1008,7 @@ def test_read_text(ray_start_regular_shared, tmp_path):
     ds = ray.data.read_text(path)
     assert sorted(ds.take()) == ["goodbye", "hello", "ray", "world"]
     ds = ray.data.read_text(path, drop_empty_lines=False)
-    assert ds.count()== 5
-
+    assert ds.count() == 5
 
 
 @pytest.mark.parametrize("pipelined", [False, True])
