bfed927c63bfc445f12eb1167fea0b0b7b1f99b1
Eric Liang <ekhliang@gmail.com>
2022-02-14 15:54:45 -0800

    wi

--- python/ray/data/impl/delegating_block_builder.py
+++ python/ray/data/impl/delegating_block_builder.py
@@ -32,6 +32,8 @@ class DelegatingBlockBuilder(BlockBuilder[T]):
         self._builder.add(item)
 
     def add_block(self, block: Block) -> None:
+        if not block:
+            return  # Dpn't infer types of empty lists.
         if self._builder is None:
             self._builder = BlockAccessor.for_block(block).builder()
         self._builder.add_block(block)
