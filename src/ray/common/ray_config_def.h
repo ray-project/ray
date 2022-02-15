a47240fa0a7b9073999e22c8875aa897e11af123
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2022-02-15 11:38:32 -0800

    [gcs/ha] Enable HA flags by default (#21608) (#22366)

--- src/ray/common/ray_config_def.h
+++ src/ray/common/ray_config_def.h
@@ -280,9 +280,9 @@ RAY_CONFIG(bool, grpc_based_resource_broadcast, true)
 // Feature flag to enable grpc based pubsub in GCS.
 RAY_CONFIG(bool, gcs_grpc_based_pubsub, true)
 // The storage backend to use for the GCS. It can be either 'redis' or 'memory'.
-RAY_CONFIG(std::string, gcs_storage, "redis")
+RAY_CONFIG(std::string, gcs_storage, "memory")
 // Feature flag to enable GCS based bootstrapping.
-RAY_CONFIG(bool, bootstrap_with_gcs, false)
+RAY_CONFIG(bool, bootstrap_with_gcs, true)
 
 /// Duration to sleep after failing to put an object in plasma because it is full.
 RAY_CONFIG(uint32_t, object_store_full_delay_ms, 10)
