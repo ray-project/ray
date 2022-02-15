4f3bf19afc3a6954d2a18f18a106ff81960e7553
久龙 <guyang.sgy@antfin.com>
2022-02-15 14:59:53 +0800

    fix test

--- python/ray/runtime_env.py
+++ python/ray/runtime_env.py
@@ -5,11 +5,11 @@ from ray._private.client_mode_hook import client_mode_hook
 
 _runtime_env = None
 
+
 @PublicAPI(stability="beta")
 @client_mode_hook(auto_init=False)
 def get_current_runtime_env():
-    """Get the runtime env of the current driver/worker.
-    """
+    """Get the runtime env of the current driver/worker."""
     global _runtime_env
     if _runtime_env is None:
         _runtime_env = dict(ray.get_runtime_context().runtime_env)
