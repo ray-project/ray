a33c513eb6a659db7c0abfb810badf5c2143f935
simon-mo <simon.mo@hey.com>
2022-02-16 11:13:32 -0800

    use k8s container to kubectl

--- ci/travis/prep-k8s-environment.sh
+++ ci/travis/prep-k8s-environment.sh
@@ -8,15 +8,16 @@ chmod +x kind-linux-amd64
 mv ./kind-linux-amd64 /usr/bin/kind
 kind --help
 
-# Install kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
-mv ./kubectl /usr/bin/kubectl
-kubectl version --client
+#Install kubectl
+#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#chmod +x kubectl
+#mv ./kubectl /usr/bin/kubectl
+#kubectl version --client
 
 # https://github.com/kubernetes-sigs/kind/issues/273#issuecomment-622180144
-export KIND_EXPERIMENTAL_DOCKER_NETWORK=dind-network
+#export KIND_EXPERIMENTAL_DOCKER_NETWORK=dind-network
 time kind create cluster --wait 120s --config ./ci/travis/kind.config.yaml
+alias kubectl="docker run --network kind bitnami/kubectl:latest"
 kubectl cluster-info --context kind-kind
 kubectl get nodes
 kubectl get pods
