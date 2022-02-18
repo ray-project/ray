2455c369701cf513fb58ca328219265d5a91d17f
Eric Liang <ekhliang@gmail.com>
2022-02-17 17:12:04 -0800

    fix test

--- src/ray/raylet/scheduling/cluster_task_manager_test.cc
+++ src/ray/raylet/scheduling/cluster_task_manager_test.cc
@@ -51,7 +51,7 @@ class MockWorkerPool : public WorkerPoolInterface {
   void PopWorker(const TaskSpecification &task_spec, const PopWorkerCallback &callback,
                  const std::string &allocated_instances_serialized_json) {
     num_pops++;
-    const WorkerCacheKey env = {task_spec.SerializedRuntimeEnv(), {}};
+    const WorkerCacheKey env = {task_spec.SerializedRuntimeEnv(), {}, false};
     const int runtime_env_hash = env.IntHash();
     callbacks[runtime_env_hash].push_back(callback);
   }
@@ -501,7 +501,7 @@ TEST_F(ClusterTaskManagerTest, DispatchQueueNonBlockingTest) {
   pool_.TriggerCallbacks();
 
   // Push a worker that can only run task A.
-  const WorkerCacheKey env_A = {serialized_runtime_env_A, {}};
+  const WorkerCacheKey env_A = {serialized_runtime_env_A, {}, false};
   const int runtime_env_hash_A = env_A.IntHash();
   std::shared_ptr<MockWorker> worker_A =
       std::make_shared<MockWorker>(WorkerID::FromRandom(), 1234, runtime_env_hash_A);
