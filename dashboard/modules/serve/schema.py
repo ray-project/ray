fb4b0cf37db4cee1f417248a2ba338524c23ff3c
Shreyas Krishnaswamy <shrekris@anyscale.com>
2022-02-22 03:17:11 -0800

    Linter

--- dashboard/modules/serve/schema.py
+++ dashboard/modules/serve/schema.py
@@ -15,29 +15,32 @@ class SupportedLanguage(str, Enum):
 class AppConfig(BaseModel):
     init_args: Union[Tuple, List] = Field(
         default=None,
-        description=("The application's init_args. Only works with Python 3 "
-                     "applications.")
+        description=(
+            "The application's init_args. Only works with Python 3 " "applications."
+        ),
     )
     init_kwargs: Dict = Field(
         default=None,
-        description=("The application's init_args. Only works with Python 3 "
-                     "applications.")
+        description=(
+            "The application's init_args. Only works with Python 3 " "applications."
+        ),
     )
     import_path: str = Field(
         default=None,
-        description=("The application's full import path. Should be of the "
-                     "form \"module.submodule_1...submodule_n."
-                     "MyClassOrFunction.\" This is equivalent to "
-                     "\"from module.submodule_1...submodule_n import "
-                     "MyClassOrFunction\". Only works with Python 3 "
-                     "applications."),
+        description=(
+            "The application's full import path. Should be of the "
+            'form "module.submodule_1...submodule_n.'
+            'MyClassOrFunction." This is equivalent to '
+            '"from module.submodule_1...submodule_n import '
+            'MyClassOrFunction". Only works with Python 3 '
+            "applications."
+        ),
         # This regex checks that there is at least one character, followed by
         # a dot, followed by at least one more character.
-        regex=r".+\..+"
+        regex=r".+\..+",
     )
     language: SupportedLanguage = Field(
-        ...,
-        description="The application's coding language."
+        ..., description="The application's coding language."
     )
 
     @validator("language")
@@ -60,82 +63,103 @@ class AppConfig(BaseModel):
 
         for attribute in required_attributes[v]:
             if attribute not in values or values[attribute] is None:
-                raise ValueError(f"{attribute} must be specified in the "
-                                 f"{v.value} language.")
-        
+                raise ValueError(
+                    f"{attribute} must be specified in the " f"{v.value} language."
+                )
+
         supported_attributes = required_attributes[v].union(optional_attributes[v])
         for attribute, value in values.items():
             if attribute not in supported_attributes and value is not None:
-                raise ValueError(f"Got {value} as {attribute}. However, "
-                                 f"{attribute} is not supported in the "
-                                 f"{v.value} language.")
+                raise ValueError(
+                    f"Got {value} as {attribute}. However, "
+                    f"{attribute} is not supported in the "
+                    f"{v.value} language."
+                )
 
 
 class DeploymentConfig(BaseModel):
     num_replicas: int = Field(
         default=None,
-        description=("The number of processes that handle requests to this "
-                     "deployment. Uses a default if null."),
-        gt=0
+        description=(
+            "The number of processes that handle requests to this "
+            "deployment. Uses a default if null."
+        ),
+        gt=0,
     )
     route_prefix: str = Field(
         default=None,
-        description=("Requests to paths under this HTTP path "
+        description=(
+            "Requests to paths under this HTTP path "
             "prefix will be routed to this deployment. When null, no HTTP "
             "endpoint will be created. Routing is done based on "
             "longest-prefix match, so if you have deployment A with "
-            "a prefix of \"/a\" and deployment B with a prefix of \"/a/b\", "
-            "requests to \"/a\", \"/a/\", and \"/a/c\" go to A and requests "
-            "to \"/a/b\", \"/a/b/\", and \"/a/b/c\" go to B. Routes must not "
-            "end with a \"/\" unless they're the root (just \"/\"), which "
-            "acts as a catch-all.")
+            'a prefix of "/a" and deployment B with a prefix of "/a/b", '
+            'requests to "/a", "/a/", and "/a/c" go to A and requests '
+            'to "/a/b", "/a/b/", and "/a/b/c" go to B. Routes must not '
+            'end with a "/" unless they\'re the root (just "/"), which '
+            "acts as a catch-all."
+        ),
     )
     max_concurrent_queries: int = Field(
         default=None,
-        description=("The max number of pending queries in a single replica. "
-                     "Uses a default if null."),
-        gt=0
+        description=(
+            "The max number of pending queries in a single replica. "
+            "Uses a default if null."
+        ),
+        gt=0,
     )
     user_config: Dict = Field(
         default=None,
-        description=("[EXPERIMENTAL] Config to pass into this deployment's "
-                     "reconfigure method. This can be updated dynamically "
-                     "without restarting replicas")
+        description=(
+            "[EXPERIMENTAL] Config to pass into this deployment's "
+            "reconfigure method. This can be updated dynamically "
+            "without restarting replicas"
+        ),
     )
     autoscaling_config: Dict = Field(
         default=None,
-        description=("[EXPERIMENTAL] Config specifying autoscaling "
-                     "parameters for the deployment's number of replicas. "
-                     "If null, the deployment won't autoscale its number of "
-                     "replicas; the number of replicas will be fixed at "
-                     "num_replicas.")
+        description=(
+            "[EXPERIMENTAL] Config specifying autoscaling "
+            "parameters for the deployment's number of replicas. "
+            "If null, the deployment won't autoscale its number of "
+            "replicas; the number of replicas will be fixed at "
+            "num_replicas."
+        ),
     )
     graceful_shutdown_wait_loop_s: float = Field(
         default=None,
-        description=("Duration that deployment replicas will wait until there "
-                     "is no more work to be done before shutting down. Uses a "
-                     "default if null."),
-        ge=0
+        description=(
+            "Duration that deployment replicas will wait until there "
+            "is no more work to be done before shutting down. Uses a "
+            "default if null."
+        ),
+        ge=0,
     )
     graceful_shutdown_timeout_s: float = Field(
         default=None,
-        description=("Serve controller waits for this duration before "
-                     "forcefully killing the replica for shutdown. Uses a "
-                     "default if null."),
-        ge=0
+        description=(
+            "Serve controller waits for this duration before "
+            "forcefully killing the replica for shutdown. Uses a "
+            "default if null."
+        ),
+        ge=0,
     )
     health_check_period_s: float = Field(
         default=None,
-        description=("Frequency at which the controller will health check "
-                     "replicas. Uses a default if null."),
-        gt=0
+        description=(
+            "Frequency at which the controller will health check "
+            "replicas. Uses a default if null."
+        ),
+        gt=0,
     )
     health_check_timeout_s: float = Field(
         default=None,
-        description=("Timeout that the controller will wait for a response "
-                     "from the replica's health check before marking it "
-                     "unhealthy. Uses a default if null."),
-        gt=0
+        description=(
+            "Timeout that the controller will wait for a response "
+            "from the replica's health check before marking it "
+            "unhealthy. Uses a default if null."
+        ),
+        gt=0,
     )
 
     @validator("route_prefix")
@@ -153,89 +177,86 @@ class DeploymentConfig(BaseModel):
             return
 
         if len(v) < 1 or v[0] != "/":
-            raise ValueError(f"Got \"{v}\" for route_prefix. Route prefix "
-                             "must start with \"/\".")
+            raise ValueError(
+                f'Got "{v}" for route_prefix. Route prefix ' 'must start with "/".'
+            )
         if v[-1] == "/" and len(v) > 1:
-            raise ValueError(f"Got \"{v}\" for route_prefix. Route prefix "
-                             "cannot end with \"/\" unless the "
-                             "entire prefix is just \"/\".")
+            raise ValueError(
+                f'Got "{v}" for route_prefix. Route prefix '
+                'cannot end with "/" unless the '
+                'entire prefix is just "/".'
+            )
         if "{" in v or "}" in v:
-            raise ValueError(f"Got \"{v}\" for route_prefix. Route prefix "
-                             "cannot contain wildcards, so it cannot "
-                             "contain \"{\" or \"}\".")
+            raise ValueError(
+                f'Got "{v}" for route_prefix. Route prefix '
+                "cannot contain wildcards, so it cannot "
+                'contain "{" or "}".'
+            )
 
 
 class ReplicaResources(BaseModel):
     num_cpus: float = Field(
         default=None,
-        description=("The number of CPUs required by the deployment's "
-                     "application per replica. This is the same as a ray "
-                     "actor's num_cpus. Uses a default if null."),
-        gt=0
+        description=(
+            "The number of CPUs required by the deployment's "
+            "application per replica. This is the same as a ray "
+            "actor's num_cpus. Uses a default if null."
+        ),
+        gt=0,
     )
     num_gpus: float = Field(
         default=None,
-        description=("The number of GPUs required by the deployment's "
-                     "application per replica. This is the same as a ray "
-                     "actor's num_gpus. Uses a default if null."),
-        gt=0
+        description=(
+            "The number of GPUs required by the deployment's "
+            "application per replica. This is the same as a ray "
+            "actor's num_gpus. Uses a default if null."
+        ),
+        gt=0,
     )
     memory: float = Field(
         default=None,
-        description=("Restrict the heap memory usage of each replica. Uses a "
-                     "default if null."),
-        gt=0
+        description=(
+            "Restrict the heap memory usage of each replica. Uses a " "default if null."
+        ),
+        gt=0,
     )
     object_store_memory: float = Field(
         default=None,
-        description=("Restrict the object store memory used per replica when "
-                     "creating objects. Uses a default if null."),
-        gt=0
+        description=(
+            "Restrict the object store memory used per replica when "
+            "creating objects. Uses a default if null."
+        ),
+        gt=0,
     )
     resources: Dict = Field(
-        default=None,
-        description=("The custom resources required by each replica.")
+        default=None, description=("The custom resources required by each replica.")
     )
     accelerator_type: str = Field(
         default=None,
-        description=("Forces replicas to run on nodes with the specified "
-                     "accelerator type.")
+        description=(
+            "Forces replicas to run on nodes with the specified " "accelerator type."
+        ),
     )
-    max_concurrency: int = Field(
-        default=None,
-        description=("")
-    )
-    max_task_retries: int = Field(
-        default=None,
-        description=("")
-    )
-    max_pending_calls: int = Field(
-        default=None,
-        description=("")
-    )
-    placement_group_bundle_index: int = Field(
-        default=None,
-        description=("")
-    )
-    placement_group_capture_child_tasks: bool = Field(
-        default=None,
-        description=("")
-    )
+    max_concurrency: int = Field(default=None, description=(""))
+    max_task_retries: int = Field(default=None, description=(""))
+    max_pending_calls: int = Field(default=None, description=(""))
+    placement_group_bundle_index: int = Field(default=None, description=(""))
+    placement_group_capture_child_tasks: bool = Field(default=None, description=(""))
 
 
 class FullDeploymentConfig(BaseModel):
     name: str = Field(
-        ...,
-        description=("Globally-unique name identifying this deployment.")
+        ..., description=("Globally-unique name identifying this deployment.")
     )
     runtime_env: dict = Field(
         default=None,
-        description=("This deployment's runtime_env. working_dir and "
-                     "py_modules may contain only remote URIs.")
+        description=(
+            "This deployment's runtime_env. working_dir and "
+            "py_modules may contain only remote URIs."
+        ),
     )
     namespace: str = Field(
-        default="serve",
-        description=("This deployment's namespace.")
+        default="serve", description=("This deployment's namespace.")
     )
     app_config: AppConfig = Field(...)
     deployment_config: DeploymentConfig = Field(...)
@@ -247,11 +268,11 @@ class FullDeploymentConfig(BaseModel):
 
         if v is None:
             return
-        
+
         uris = v.get("py_modules", [])
         if "working_dir" in v:
             uris.append(v["working_dir"])
-        
+
         for uri in uris:
             parse_uri(uri)
 
--- dashboard/modules/serve/tests/test_schema.py
+++ dashboard/modules/serve/tests/test_schema.py
@@ -8,19 +8,15 @@ from ray.dashboard.modules.serve.schema import (
     FullDeploymentConfig,
     ServeInstanceConfig,
 )
-from ray.util.accelerators.accelerators import (
-    NVIDIA_TESLA_V100,
-    NVIDIA_TESLA_P4
-)
+from ray.util.accelerators.accelerators import NVIDIA_TESLA_V100, NVIDIA_TESLA_P4
 
 
 class TestAppConfig:
-
     def test_invalid_language(self):
         # Unsupported languages should raise a ValidationError
         with pytest.raises(ValidationError):
             AppConfig(language="ocaml")
-    
+
     def test_invalid_python_attributes(self):
         # Test setting invalid attributes for Python to ensure a validation or
         # value error is raised.
@@ -30,28 +26,24 @@ class TestAppConfig:
             app_config = {
                 "language": f"python_3.{minor_version}",
                 "init_args": [1, 2],
-                "init_kwargs": {
-                    "threshold": 0.5,
-                    "version": "abcd"
-                },
+                "init_kwargs": {"threshold": 0.5, "version": "abcd"},
             }
 
             with pytest.raises(ValueError, match="must be specified"):
                 AppConfig.parse_obj(app_config)
-            
+
             # AppConfig should be generated once import_path is set
             app_config["import_path"] = "my_module.MyClass"
             AppConfig.parse_obj(app_config)
-        
+
         # Invalid import_path syntax should raise a ValidationError
         invalid_paths = ["", "MyClass", ".", "hello,world"]
         for path in invalid_paths:
             with pytest.raises(ValidationError):
                 AppConfig(language="python_3.8", import_path=path)
-    
+
 
 class TestDeploymentConfig:
-
     def test_valid_deployment_config(self):
         # Ensure a valid DeploymentConfig can be generated
 
@@ -59,39 +51,42 @@ class TestDeploymentConfig:
             "num_replicas": 5,
             "route_prefix": "/hello",
             "max_concurrent_queries": 20,
-            "user_config": {
-                "threshold": 0.2,
-                "pattern": "rainbow"
-            },
+            "user_config": {"threshold": 0.2, "pattern": "rainbow"},
             "graceful_shutdown_wait_loop_s": 50,
             "graceful_shutdown_timeout_s": 20,
             "health_check_period_s": 10,
-            "health_check_timeout_s": 30
+            "health_check_timeout_s": 30,
         }
 
         # Should not raise an error
         DeploymentConfig.parse_obj(deployment_config)
-    
+
     def test_gt_zero_deployment_config(self):
         # Ensure ValidationError is raised when any fields that must be greater
         # than zero is set to zero.
 
-        gt_zero_fields = ["num_replicas", "max_concurrent_queries", 
-                          "health_check_period_s", "health_check_timeout_s"]
+        gt_zero_fields = [
+            "num_replicas",
+            "max_concurrent_queries",
+            "health_check_period_s",
+            "health_check_timeout_s",
+        ]
         for field in gt_zero_fields:
             with pytest.raises(ValidationError):
                 DeploymentConfig.parse_obj({field: 0})
-    
+
     def test_ge_zero_deployment_config(self):
         # Ensure ValidationError is raised when any fields that must be greater
         # than or equal to zero is set to -1.
 
-        ge_zero_fields = ["graceful_shutdown_wait_loop_s",
-                          "graceful_shutdown_timeout_s"]
+        ge_zero_fields = [
+            "graceful_shutdown_wait_loop_s",
+            "graceful_shutdown_timeout_s",
+        ]
         for field in ge_zero_fields:
             with pytest.raises(ValidationError):
                 DeploymentConfig.parse_obj({field: -1})
-    
+
     def test_route_prefix(self):
         # route_prefix must start with a "/"
         with pytest.raises(ValueError):
@@ -100,12 +95,12 @@ class TestDeploymentConfig:
         # route_prefix must end with a "/"
         with pytest.raises(ValueError):
             DeploymentConfig(route_prefix="/hello/world/")
-        
+
         # route_prefix cannot contain wildcards, meaning it can't have
         # "{" or "}"
         with pytest.raises(ValueError):
             DeploymentConfig(route_prefix="/hello/{adjective}/world/")
-        
+
         # Ensure a valid route_prefix works
         DeploymentConfig(route_prefix="/hello/wonderful/world")
 
@@ -117,7 +112,6 @@ class TestDeploymentConfig:
 
 
 class TestReplicaResources:
-
     def test_valid_replica_resources(self):
         # Ensure a valid ReplicaResources can be generated
 
@@ -127,43 +121,45 @@ class TestReplicaResources:
             "memory": 3,
             "object_store_memory": 64,
             "resources": {"custom_asic": 12},
-            "accelerator_type": NVIDIA_TESLA_V100
+            "accelerator_type": NVIDIA_TESLA_V100,
         }
 
         ReplicaResources.parse_obj(replica_resources)
-    
+
     def test_gt_zero_replica_resources(self):
         # Ensure ValidationError is raised when any fields that must be greater
         # than zero is set to zero.
 
-        gt_zero_fields = ["num_cpus", "num_gpus", "memory",
-                          "object_store_memory"]
+        gt_zero_fields = ["num_cpus", "num_gpus", "memory", "object_store_memory"]
         for field in gt_zero_fields:
             with pytest.raises(ValidationError):
                 ReplicaResources.parse_obj({field: 0})
-    
+
 
 class TestFullDeploymentConfig:
-
     def test_valid_full_deployment_config(self):
         # Ensure a valid FullDeploymentConfig can be generated
 
         full_deployment_config = {
             "name": "shallow",
             "runtime_env": {
-                "working_dir": "https://github.com/shrekris-anyscale/test_module/archive/HEAD.zip",
+                "working_dir": (
+                    "https://github.com/shrekris-anyscale/"
+                    "test_module/archive/HEAD.zip"
+                ),
                 "py_modules": [
-                    "https://github.com/shrekris-anyscale/test_deploy_group/archive/HEAD.zip",
-                ]
+                    (
+                        "https://github.com/shrekris-anyscale/"
+                        "test_deploy_group/archive/HEAD.zip"
+                    ),
+                ],
             },
             "namespace": "serve",
             "app_config": {
                 "language": "python_3.8",
                 "init_args": [4, "glue"],
-                "init_kwargs": {
-                    "fuel": "diesel"
-                },
-                "import_path": "test_env.shallow_import.ShallowClass"
+                "init_kwargs": {"fuel": "diesel"},
+                "import_path": "test_env.shallow_import.ShallowClass",
             },
             "deployment_config": {
                 "num_replicas": 2,
@@ -174,7 +170,7 @@ class TestFullDeploymentConfig:
                 "_graceful_shutdown_wait_loop_s": 17,
                 "_graceful_shutdown_timeout_s": 49,
                 "_health_check_period_s": 11,
-                "_health_check_timeout_s": 11
+                "_health_check_timeout_s": 11,
             },
             "replica_resources": {
                 "num_cpus": 3,
@@ -183,11 +179,11 @@ class TestFullDeploymentConfig:
                 "object_store_memory": 3,
                 "resources": {"custom_asic": 8},
                 "accelerator_type": NVIDIA_TESLA_P4,
-            }
+            },
         }
 
         FullDeploymentConfig.parse_obj(full_deployment_config)
-    
+
     def test_runtime_env(self):
         # Test different runtime_env configurations
 
@@ -199,7 +195,7 @@ class TestFullDeploymentConfig:
                 "language": "python_3.8",
                 "init_args": None,
                 "init_kwargs": None,
-                "import_path": "test_env.shallow_import.ShallowClass"
+                "import_path": "test_env.shallow_import.ShallowClass",
             },
             "deployment_config": {
                 "num_replicas": None,
@@ -210,7 +206,7 @@ class TestFullDeploymentConfig:
                 "_graceful_shutdown_wait_loop_s": None,
                 "_graceful_shutdown_timeout_s": None,
                 "_health_check_period_s": None,
-                "_health_check_timeout_s": None
+                "_health_check_timeout_s": None,
             },
             "replica_resources": {
                 "num_cpus": None,
@@ -219,7 +215,7 @@ class TestFullDeploymentConfig:
                 "object_store_memory": None,
                 "resources": None,
                 "accelerator_type": None,
-            }
+            },
         }
 
         # full_deployment_config should work as is
@@ -230,16 +226,18 @@ class TestFullDeploymentConfig:
             "working_dir": ".",
             "py_modules": [
                 "/Desktop/my_project",
-                "https://github.com/shrekris-anyscale/test_deploy_group/archive/HEAD.zip",
-            ]
+                (
+                    "https://github.com/shrekris-anyscale/"
+                    "test_deploy_group/archive/HEAD.zip"
+                ),
+            ],
         }
 
         with pytest.raises(ValueError):
             FullDeploymentConfig.parse_obj(full_deployment_config)
-    
+
 
 class TestServeInstanceConfig:
-    
     def test_valid_serve_instance_config(self):
         # Ensure a valid ServeInstanceConfig can be generated
 
@@ -248,19 +246,23 @@ class TestServeInstanceConfig:
                 {
                     "name": "shallow",
                     "runtime_env": {
-                        "working_dir": "https://github.com/shrekris-anyscale/test_module/archive/HEAD.zip",
+                        "working_dir": (
+                            "https://github.com/shrekris-anyscale/"
+                            "test_module/archive/HEAD.zip"
+                        ),
                         "py_modules": [
-                            "https://github.com/shrekris-anyscale/test_deploy_group/archive/HEAD.zip",
-                        ]
+                            (
+                                "https://github.com/shrekris-anyscale/"
+                                "test_deploy_group/archive/HEAD.zip"
+                            ),
+                        ],
                     },
                     "namespace": "serve",
                     "app_config": {
                         "language": "python_3.10",
                         "init_args": [4, "glue"],
-                        "init_kwargs": {
-                            "fuel": "diesel"
-                        },
-                        "import_path": "test_env.shallow_import.ShallowClass"
+                        "init_kwargs": {"fuel": "diesel"},
+                        "import_path": "test_env.shallow_import.ShallowClass",
                     },
                     "deployment_config": {
                         "num_replicas": 2,
@@ -271,7 +273,7 @@ class TestServeInstanceConfig:
                         "_graceful_shutdown_wait_loop_s": 17,
                         "_graceful_shutdown_timeout_s": 49,
                         "_health_check_period_s": 11,
-                        "_health_check_timeout_s": 11
+                        "_health_check_timeout_s": 11,
                     },
                     "replica_resources": {
                         "num_cpus": 3,
@@ -280,7 +282,7 @@ class TestServeInstanceConfig:
                         "object_store_memory": 3,
                         "resources": {"custom_asic": 8},
                         "accelerator_type": NVIDIA_TESLA_P4,
-                    }
+                    },
                 },
                 {
                     "name": "deep",
@@ -290,7 +292,9 @@ class TestServeInstanceConfig:
                         "language": "python_3.8",
                         "init_args": None,
                         "init_kwargs": None,
-                        "import_path": "test_env.subdir1.subdir2.deep_import.DeepClass"
+                        "import_path": (
+                            "test_env.subdir1.subdir2." "deep_import.DeepClass"
+                        ),
                     },
                     "deployment_config": {
                         "num_replicas": None,
@@ -301,7 +305,7 @@ class TestServeInstanceConfig:
                         "_graceful_shutdown_wait_loop_s": None,
                         "_graceful_shutdown_timeout_s": None,
                         "_health_check_period_s": None,
-                        "_health_check_timeout_s": None
+                        "_health_check_timeout_s": None,
                     },
                     "replica_resources": {
                         "num_cpus": None,
@@ -310,8 +314,8 @@ class TestServeInstanceConfig:
                         "object_store_memory": None,
                         "resources": None,
                         "accelerator_type": None,
-                    }
-                }
+                    },
+                },
             ]
         }
 
