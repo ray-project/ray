19bbe70756ff9af65fba1aee74f18f46db377d60
Shreyas Krishnaswamy <shrekris@anyscale.com>
2022-02-22 19:52:31 -0800

    Linter

--- dashboard/modules/serve/tests/test_serve_head.py
+++ dashboard/modules/serve/tests/test_serve_head.py
@@ -3,7 +3,7 @@ import subprocess
 import requests
 import json
 from typing import List, Dict
-import time
+
 
 @pytest.fixture
 def serve_start_stop():
@@ -11,6 +11,7 @@ def serve_start_stop():
     subprocess.check_output(["serve", "start"])
     yield
     subprocess.check_output(["ray", "stop", "--force"])
+
 
 def deployments_match(list1: List[Dict], list2: List[Dict]) -> bool:
     # Helper that takes in 2 lists of deployment dictionaries and compares them
@@ -30,6 +31,7 @@ def deployments_match(list1: List[Dict], list2: List[Dict]) -> bool:
 
     return len(list2) == 0
 
+
 def test_put_get_success(serve_start_stop):
     URL = "http://localhost:8265/api/serve/deployments/"
     test_env_uri = (
@@ -39,23 +41,21 @@ def test_put_get_success(serve_start_stop):
         "https://github.com/shrekris-anyscale/test_module/archive/HEAD.zip"
     )
 
-    ray_actor_options = {
-        "runtime_env": {"py_modules": [test_env_uri, test_module_uri]}
-    }
+    ray_actor_options = {"runtime_env": {"py_modules": [test_env_uri, test_module_uri]}}
 
     shallow = dict(
         name="shallow",
         num_replicas=3,
         route_prefix="/shallow",
         ray_actor_options=ray_actor_options,
-        import_path="test_env.shallow_import.ShallowClass"
+        import_path="test_env.shallow_import.ShallowClass",
     )
 
     deep = dict(
         name="deep",
         route_prefix="/deep",
         ray_actor_options=ray_actor_options,
-        import_path="test_env.subdir1.subdir2.deep_import.DeepClass"
+        import_path="test_env.subdir1.subdir2.deep_import.DeepClass",
     )
 
     one = dict(
@@ -63,7 +63,7 @@ def test_put_get_success(serve_start_stop):
         num_replicas=3,
         route_prefix="/one",
         ray_actor_options=ray_actor_options,
-        import_path="test_module.test.one"
+        import_path="test_module.test.one",
     )
 
     # Ensure the REST API is idempotent
@@ -72,16 +72,15 @@ def test_put_get_success(serve_start_stop):
 
         put_response = requests.put(URL, json={"deployments": deployments})
         assert put_response.status_code == 200
-        assert requests.get("http://localhost:8000/shallow").text == "Hello shallow world!"
+        assert (
+            requests.get("http://localhost:8000/shallow").text == "Hello shallow world!"
+        )
         assert requests.get("http://localhost:8000/deep").text == "Hello deep world!"
         assert requests.get("http://localhost:8000/one").text == "2"
 
         get_response = requests.get(URL)
         assert get_response.status_code == 200
 
-        with open("check_json.json", "w+") as f:
-            f.write(get_response.json())
-        
         with open("three_deployments_response.json", "r") as f:
             response_deployments = json.loads(get_response.json())["deployments"]
             expected_deployments = json.load(f)["deployments"]
@@ -90,7 +89,9 @@ def test_put_get_success(serve_start_stop):
         deployments = [shallow, one]
         put_response = requests.put(URL, json={"deployments": deployments})
         assert put_response.status_code == 200
-        assert requests.get("http://localhost:8000/shallow").text == "Hello shallow world!"
+        assert (
+            requests.get("http://localhost:8000/shallow").text == "Hello shallow world!"
+        )
         assert requests.get("http://localhost:8000/deep").status_code == 404
         assert requests.get("http://localhost:8000/one").text == "2"
 
@@ -102,13 +103,16 @@ def test_put_get_success(serve_start_stop):
             expected_deployments = json.load(f)["deployments"]
             assert deployments_match(response_deployments, expected_deployments)
 
+
 def test_delete_success(serve_start_stop):
     URL = "http://localhost:8265/api/serve/deployments/"
 
     ray_actor_options = {
-        "runtime_env": {"working_dir": (
-            "https://github.com/shrekris-anyscale/test_deploy_group/archive/HEAD.zip"
-        )}
+        "runtime_env": {
+            "working_dir": (
+                "https://github.com/shrekris-anyscale/test_deploy_group/archive/HEAD.zip"
+            )
+        }
     }
 
     shallow = dict(
@@ -116,14 +120,16 @@ def test_delete_success(serve_start_stop):
         num_replicas=3,
         route_prefix="/shallow",
         ray_actor_options=ray_actor_options,
-        import_path="test_env.shallow_import.ShallowClass"
+        import_path="test_env.shallow_import.ShallowClass",
     )
 
     # Ensure the REST API is idempotent
     for _ in range(5):
         put_response = requests.put(URL, json={"deployments": [shallow]})
         assert put_response.status_code == 200
-        assert requests.get("http://localhost:8000/shallow").text == "Hello shallow world!"
+        assert (
+            requests.get("http://localhost:8000/shallow").text == "Hello shallow world!"
+        )
 
         delete_response = requests.delete(URL)
         assert delete_response.status_code == 200
