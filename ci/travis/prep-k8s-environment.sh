0736a953fb1c580de759ba7b027f19fcdfa94e38
Simon Mo <simon.mo@hey.com>
2022-02-22 17:54:25 -0800

    try host networking instead

--- ci/travis/prep-k8s-environment.sh
+++ ci/travis/prep-k8s-environment.sh
@@ -24,7 +24,7 @@ cp ~/.kube/config /ray-mount/kube-config
 chmod 777 /ray-mount/kube-config
 
 shopt -s expand_aliases
-alias kubectl='docker run --network kind --mount type=bind,src=/ray/kube-config,dst=/.kube/config --env KUBECONFIG=/.kube/config bitnami/kubectl:latest'
+alias kubectl='docker run --network host --mount type=bind,src=/ray/kube-config,dst=/.kube/config --env KUBECONFIG=/.kube/config bitnami/kubectl:latest'
 kubectl version
 kubectl cluster-info --context kind-kind
 kubectl get nodes
