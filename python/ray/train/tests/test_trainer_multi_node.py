9985f8780636856ca2ababd0d4580ad001137cec
Amog Kamsetty <amogkamsetty@yahoo.com>
2022-02-22 17:01:34 -0800

    uncomment

--- python/ray/train/tests/test_trainer_multi_node.py
+++ python/ray/train/tests/test_trainer_multi_node.py
@@ -10,34 +10,33 @@ from ray.train import Trainer
 
 @pytest.fixture
 def ray_start_2_node_2_cpu_each():
-    # cluster = DockerCluster()
-    # cluster.setup()
-    # cluster.update_config(
-    #     {
-    #         "provider": {"head_resources": {"CPU": 2, "GPU": 0}},
-    #         "available_node_types": {
-    #             "ray.head.default": {"resources": {"CPU": 2}},
-    #             "ray.worker.cpu": {
-    #                 "resources": {"CPU": 2},
-    #                 "min_workers": 1,
-    #                 "max_workers": 1,
-    #             },
-    #             "ray.worker.gpu": {
-    #                 "min_workers": 0,
-    #                 "max_workers": 0,  # No GPU nodes
-    #             },
-    #         },
-    #     }
-    # )
-    # cluster.start()
-    # cluster.connect(client=False, timeout=120)
-    # # ray.init("127.0.0.1:16379")
-    # cluster.wait_for_resources({"CPU": 4})
-    ray.init("ray://localhost:10002")
+    cluster = DockerCluster()
+    cluster.setup()
+    cluster.update_config(
+        {
+            "provider": {"head_resources": {"CPU": 2, "GPU": 0}},
+            "available_node_types": {
+                "ray.head.default": {"resources": {"CPU": 2}},
+                "ray.worker.cpu": {
+                    "resources": {"CPU": 2},
+                    "min_workers": 1,
+                    "max_workers": 1,
+                },
+                "ray.worker.gpu": {
+                    "min_workers": 0,
+                    "max_workers": 0,  # No GPU nodes
+                },
+            },
+        }
+    )
+    cluster.start()
+    cluster.connect(client=False, timeout=120)
+    # ray.init("127.0.0.1:16379")
+    cluster.wait_for_resources({"CPU": 4})
     assert len(ray.nodes()) >= 2
     yield
-    # cluster.stop()
-    # cluster.teardown()
+    cluster.stop()
+    cluster.teardown()
 
 
 def test_horovod_simple_multi_node(ray_start_2_node_2_cpu_each):
