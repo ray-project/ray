fc429bb29b5c41e64b18ac40601748e433de050e
Eric Liang <ekhliang@gmail.com>
2022-02-23 17:34:05 -0800

    Enable stage fusion by default for dataset pipelines (#22476)
    This PR enables stage fusion for dataset pipelines. This also requires:
    1. Removing the num_cpus=0.5 default for the read stage, to enable fusion of the read stage.
    2. Removing spread_resource_prefix (not supported for now).

--- python/ray/data/dataset_pipeline.py
+++ python/ray/data/dataset_pipeline.py
@@ -394,10 +394,6 @@ class DatasetPipeline(Generic[T]):
         This operation is only allowed for pipelines of a finite length. An
         error will be raised for pipelines of infinite length.
 
-        Transformations prior to the call to ``repeat()`` are evaluated once.
-        Transformations done on the repeated pipeline are evaluated on each
-        loop of the pipeline over the base pipeline.
-
         Note that every repeat of the pipeline is considered an "epoch" for
         the purposes of ``iter_epochs()``. If there are multiple repeat calls,
         the latest repeat takes precedence for the purpose of defining epochs.
@@ -424,10 +420,15 @@ class DatasetPipeline(Generic[T]):
                 # Still going through the original pipeline.
                 if self._original_iter:
                     try:
-                        res = next(self._original_iter)
-                        res._set_epoch(0)
-                        self._results.append(res)
-                        return lambda: res
+                        make_ds = next(self._original_iter)
+                        self._results.append(make_ds)
+
+                        def gen():
+                            res = make_ds()
+                            res._set_epoch(0)
+                            return res
+
+                        return gen
                     except StopIteration:
                         self._original_iter = None
                         # Calculate the cursor limit.
@@ -437,10 +438,16 @@ class DatasetPipeline(Generic[T]):
                             self._max_i = float("inf")
                 # Going through a repeat of the pipeline.
                 if self._i < self._max_i:
-                    res = self._results[self._i % len(self._results)]
-                    res._set_epoch(1 + self._i // len(self._results))
+                    make_ds = self._results[self._i % len(self._results)]
+                    epoch = 1 + self._i // len(self._results)
+
+                    def gen():
+                        res = make_ds()
+                        res._set_epoch(epoch)
+                        return res
+
                     self._i += 1
-                    return lambda: res
+                    return gen
                 else:
                     raise StopIteration
 
@@ -458,7 +465,11 @@ class DatasetPipeline(Generic[T]):
         else:
             length = None
 
-        return DatasetPipeline(RepeatIterable(self.iter_datasets()), length=length)
+        return DatasetPipeline(
+            RepeatIterable(iter(self._base_iterable)),
+            stages=self._stages.copy(),
+            length=length,
+        )
 
     def schema(self) -> Union[type, "pyarrow.lib.Schema"]:
         """Return the schema of the dataset pipeline.
