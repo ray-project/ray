236a1fc96ee71751a4351baa24486155963bbfb9
Eric Liang <ekhliang@gmail.com>
2022-02-23 19:32:34 -0800

    wipwip

--- python/ray/data/datasource/parquet_datasource.py
+++ python/ray/data/datasource/parquet_datasource.py
@@ -1,6 +1,8 @@
 import logging
+import itertools
 from typing import Any, Callable, Dict, Optional, List, Union, Iterator, TYPE_CHECKING
-from concurrent.futures import ThreadPoolExecutor, as_completed
+
+import numpy as np
 
 if TYPE_CHECKING:
     import pyarrow
@@ -12,19 +14,29 @@ from ray.data.datasource.file_based_datasource import (
     FileBasedDatasource,
     _resolve_paths_and_filesystem,
     _resolve_kwargs,
+    _wrap_s3_serialization_workaround,
+    _S3FileSystemWrapper,
 )
 from ray.data.impl.block_list import BlockMetadata
 from ray.data.impl.output_buffer import BlockOutputBuffer
 from ray.data.impl.progress_bar import ProgressBar
+from ray.data.impl.remote_fn import cached_remote_fn
 from ray.data.impl.util import _check_pyarrow_version
 
 logger = logging.getLogger(__name__)
 
-MAX_META_FETCH_THREADS = 100
+# Number of files per metadata fetch task.
+FILES_PER_META_FETCH = 6
 
 # The number of rows to read per batch. This is sized to generate 10MiB batches
 # for rows about 1KiB in size.
 PARQUET_READER_ROW_BATCH_SIZE = 100000
+
+
+class PartitionedFile:
+    def __init__(self, path: str, partition_keys: dict):
+        self.path = path
+        self.partition_keys = partition_keys
 
 
 class ParquetDatasource(FileBasedDatasource):
@@ -46,15 +58,16 @@ class ParquetDatasource(FileBasedDatasource):
         _block_udf: Optional[Callable[[Block], Block]] = None,
         **reader_args,
     ) -> List[ReadTask]:
+
         """Creates and returns read tasks for a Parquet file-based datasource."""
         # NOTE: We override the base class FileBasedDatasource.prepare_read
         # method in order to leverage pyarrow's ParquetDataset abstraction,
         # which simplifies partitioning logic. We still use
         # FileBasedDatasource's write side (do_write), however.
         _check_pyarrow_version()
-        from ray import cloudpickle
         import pyarrow as pa
         import pyarrow.parquet as pq
+        from pyarrow.dataset import _get_partition_keys
         import numpy as np
 
         paths, filesystem = _resolve_paths_and_filesystem(paths, filesystem)
@@ -63,7 +76,11 @@ class ParquetDatasource(FileBasedDatasource):
 
         dataset_kwargs = reader_args.pop("dataset_kwargs", {})
         pq_ds = pq.ParquetDataset(
-            paths, **dataset_kwargs, filesystem=filesystem, use_legacy_dataset=False
+            paths,
+            **dataset_kwargs,
+            filesystem=filesystem,
+            use_legacy_dataset=False,
+            split_row_groups=False,
         )
         if schema is None:
             schema = pq_ds.schema
@@ -72,51 +89,71 @@ class ParquetDatasource(FileBasedDatasource):
                 [schema.field(column) for column in columns], schema.metadata
             )
 
-        def read_pieces(serialized_pieces: str) -> Iterator[pa.Table]:
-            # Implicitly trigger S3 subsystem initialization by importing
-            # pyarrow.fs.
-            import pyarrow.fs  # noqa: F401
+        # Mapping from file path to partitioning keys.
+        paths_seen = set()
+        files_to_read: List[PartitionedFile] = []
+        for frag in pq_ds.fragments:
+            if frag.path not in paths_seen:
+                paths_seen.add(frag.path)
+                files_to_read.append(
+                    PartitionedFile(
+                        frag.path, _get_partition_keys(frag.partition_expression)
+                    )
+                )
 
-            # Deserialize after loading the filesystem class.
-            pieces: List["pyarrow._dataset.ParquetFileFragment"] = cloudpickle.loads(
-                serialized_pieces
-            )
+        # Prepare local variables for serialization in remote tasks.
+        # Partitioning isn't relevant when reading individual files, drop it now.
+        if "partitioning" in dataset_kwargs:
+            dataset_kwargs = dataset_kwargs.copy()
+            del dataset_kwargs["partitioning"]
+        filesystem = _wrap_s3_serialization_workaround(filesystem)
 
-            # Ensure that we're reading at least one dataset fragment.
-            assert len(pieces) > 0
+        def read_files(
+            files: List[PartitionedFile], filesystem, **dataset_kwargs
+        ) -> Iterator[pa.Table]:
+            if isinstance(filesystem, _S3FileSystemWrapper):
+                filesystem = filesystem.unwrap()
 
-            from pyarrow.dataset import _get_partition_keys
+            # Ensure that we're reading at least one file.
+            assert len(files) > 0
 
             ctx = DatasetContext.get_current()
             output_buffer = BlockOutputBuffer(
                 block_udf=_block_udf, target_max_block_size=ctx.target_max_block_size
             )
 
-            logger.debug(f"Reading {len(pieces)} parquet pieces")
+            logger.debug(f"Reading {len(files)} parquet files")
             use_threads = reader_args.pop("use_threads", False)
-            for piece in pieces:
-                part = _get_partition_keys(piece.partition_expression)
-                batches = piece.to_batches(
-                    use_threads=use_threads,
-                    columns=columns,
-                    schema=schema,
-                    batch_size=PARQUET_READER_ROW_BATCH_SIZE,
-                    **reader_args,
+            for f in files:
+                ds = pq.ParquetDataset(
+                    f.path,
+                    **dataset_kwargs,
+                    filesystem=filesystem,
+                    use_legacy_dataset=False,
+                    split_row_groups=False,
                 )
-                for batch in batches:
-                    table = pyarrow.Table.from_batches([batch], schema=schema)
-                    if part:
-                        for col, value in part.items():
-                            table = table.set_column(
-                                table.schema.get_field_index(col),
-                                col,
-                                pa.array([value] * len(table)),
-                            )
-                    # If the table is empty, drop it.
-                    if table.num_rows > 0:
-                        output_buffer.add_block(table)
-                        if output_buffer.has_next():
-                            yield output_buffer.next()
+                for fragment in ds.fragments:
+                    batches = fragment.to_batches(
+                        use_threads=use_threads,
+                        columns=columns,
+                        schema=schema,
+                        batch_size=PARQUET_READER_ROW_BATCH_SIZE,
+                        **reader_args,
+                    )
+                    for batch in batches:
+                        table = pa.Table.from_batches([batch], schema=schema)
+                        if f.partition_keys:
+                            for col, value in f.partition_keys.items():
+                                table = table.set_column(
+                                    table.schema.get_field_index(col),
+                                    col,
+                                    pa.array([value] * len(table)),
+                                )
+                        # If the table is empty, drop it.
+                        if table.num_rows > 0:
+                            output_buffer.add_block(table)
+                            if output_buffer.has_next():
+                                yield output_buffer.next()
             output_buffer.finalize()
             if output_buffer.has_next():
                 yield output_buffer.next()
@@ -137,17 +174,19 @@ class ParquetDatasource(FileBasedDatasource):
         else:
             inferred_schema = schema
         read_tasks = []
-        metadata = _fetch_metadata_parallel(pq_ds.pieces)
-        for piece_data in np.array_split(
-            list(zip(pq_ds.pieces, metadata)), parallelism
+        metadata = _fetch_metadata_remotely(files_to_read, filesystem, **dataset_kwargs)
+        for file_data in np.array_split(
+            list(zip(files_to_read, metadata)), parallelism
         ):
-            if len(piece_data) == 0:
+            if len(file_data) == 0:
                 continue
-            pieces, metadata = zip(*piece_data)
-            serialized_pieces = cloudpickle.dumps(pieces)
-            meta = _build_block_metadata(pieces, metadata, inferred_schema)
+            files, metadata = zip(*file_data)
+            meta = _build_block_metadata(files, metadata, inferred_schema)
             read_tasks.append(
-                ReadTask(lambda p=serialized_pieces: read_pieces(p), meta)
+                ReadTask(
+                    lambda files=files: read_files(files, filesystem, **dataset_kwargs),
+                    meta,
+                )
             )
 
         return read_tasks
@@ -168,32 +207,53 @@ class ParquetDatasource(FileBasedDatasource):
         return "parquet"
 
 
-def _fetch_metadata_parallel(
-    pieces: List["pyarrow._dataset.ParquetFileFragment"],
+def _fetch_metadata_remotely(
+    files: List[PartitionedFile],
+    filesystem,
+    **dataset_kwargs,
 ) -> List["pyarrow.parquet.FileMetaData"]:
+
+    remote_fetch_metadata = cached_remote_fn(_fetch_metadata)
     metas = []
-    meta_fetch_bar = ProgressBar("Metadata Fetch Progress", total=len(pieces))
-    with ThreadPoolExecutor(max_workers=MAX_META_FETCH_THREADS) as executor:
-        futures = [executor.submit(_fetch_metadata, p) for p in pieces]
-        for result in as_completed(futures):
-            meta_fetch_bar.update(1)
-    metas = [f.result() for f in futures]
-    return metas
+    parallelism = max(1, min(len(files) // FILES_PER_META_FETCH, 100))
+    meta_fetch_bar = ProgressBar("Metadata Fetch Progress", total=parallelism)
+    for fs in np.array_split(files, parallelism):
+        if len(fs) == 0:
+            continue
+        metas.append(remote_fetch_metadata.remote(fs, filesystem, **dataset_kwargs))
+    metas = meta_fetch_bar.fetch_until_complete(metas)
+    return list(itertools.chain.from_iterable(metas))
 
 
 def _fetch_metadata(
-    piece: "pyarrow.dataset.ParquetFileFragment",
-) -> "pyarrow.parquet.FileMetaData":
-    return piece.metadata
+    files: List[PartitionedFile],
+    filesystem,
+    **dataset_kwargs,
+) -> List["pyarrow.parquet.FileMetaData"]:
+    import pyarrow.parquet as pq
+
+    if isinstance(filesystem, _S3FileSystemWrapper):
+        filesystem = filesystem.unwrap()
+
+    pq_ds = pq.ParquetDataset(
+        [f.path for f in files],
+        **dataset_kwargs,
+        filesystem=filesystem,
+        use_legacy_dataset=False,
+        split_row_groups=False,
+    )
+    assert len(pq_ds.fragments) == len(files), pq_ds.fragments
+    metadata = {f.path: f.metadata for f in pq_ds.fragments}
+    return [metadata[f.path] for f in files]
 
 
 def _build_block_metadata(
-    pieces: List["pyarrow.dataset.ParquetFileFragment"],
+    files: List[PartitionedFile],
     metadata: List["pyarrow.parquet.FileMetaData"],
     schema: Optional[Union[type, "pyarrow.lib.Schema"]],
 ) -> BlockMetadata:
-    input_files = [p.path for p in pieces]
-    if len(metadata) == len(pieces):
+    input_files = list(set([p.path for p in files]))
+    if len(metadata) == len(files):
         # Piece metadata was available, construct a normal
         # BlockMetadata.
         block_metadata = BlockMetadata(
