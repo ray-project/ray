d19991762608f27ebfdaaef848103c97174096c7
Eric Liang <ekhliang@gmail.com>
2022-02-24 09:57:58 -0800

    update

--- python/ray/data/tests/test_dataset.py
+++ python/ray/data/tests/test_dataset.py
@@ -89,11 +89,11 @@ def test_basic_actors(shutdown_only, pipelined):
     ds = ray.data.range(n)
     ds = maybe_pipeline(ds, pipelined)
     assert sorted(
-        ds.map(lambda x: x + 1, compute=ray.data.ActorPool(4, 4)).take()
+        ds.map(lambda x: x + 1, compute=ray.data.ActorPoolStrategy(4, 4)).take()
     ) == list(range(1, n + 1))
 
     with pytest.raises(ValueError):
-        ray.data.range(10).map(lambda x: x, compute=ray.data.ActorPool(8, 4))
+        ray.data.range(10).map(lambda x: x, compute=ray.data.ActorPoolStrategy(8, 4))
 
 
 @pytest.mark.parametrize("pipelined", [False, True])
