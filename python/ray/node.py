d2f809468432b4712f6b5cb3b558f4af7a3af9f7
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2022-02-23 16:42:33 -0800

    [GCS-Ray][1.11.0 Cherry-pick] update doc and error message for GCS-Ray (#22596)
    Update documentation to reflect that Ray no longer starts Redis by default.
    
    
    
    Co-authored-by: mwtian <81660174+mwtian@users.noreply.github.com>

--- python/ray/node.py
+++ python/ray/node.py
@@ -1457,8 +1457,18 @@ class Node:
         for i in range(num_retries):
             try:
                 result = self.get_gcs_client().internal_kv_get(key, namespace)
-            except Exception:
-                logger.exception("Internal KV Get failed")
+            except Exception as e:
+                if isinstance(e, grpc.RpcError) and e.code() in (
+                        grpc.StatusCode.UNAVAILABLE,
+                        grpc.StatusCode.UNKNOWN,
+                ):
+                    logger.warning(
+                        f"Unable to connect to GCS at {self.gcs_address}. "
+                        "Check that (1) Ray GCS with matching version started "
+                        "successfully at the specified address, and (2) there is "
+                        "no firewall setting preventing access.")
+                else:
+                    logger.exception("Internal KV Get failed")
                 result = None
 
             if result is not None:
@@ -1467,8 +1477,9 @@ class Node:
                 logger.debug(f"Fetched {key}=None from redis. Retrying.")
                 time.sleep(2)
         if not result:
-            raise RuntimeError(f"Could not read '{key}' from GCS (redis). "
-                               "If using Redis, did Redis start successfully?")
+            raise RuntimeError(
+                f"Could not read '{key.decode()}' from GCS. Did GCS start successfully?"
+            )
         return result
 
     def _internal_kv_put_with_retry(self,
@@ -1482,8 +1493,19 @@ class Node:
             try:
                 return self.get_gcs_client().internal_kv_put(
                     key, value, overwrite=True, namespace=namespace)
-            except grpc.RpcError:
-                logger.exception("Internal KV Put failed")
+            except grpc.RpcError as e:
+                if e.code() in (
+                        grpc.StatusCode.UNAVAILABLE,
+                        grpc.StatusCode.UNKNOWN,
+                ):
+                    logger.warning(
+                        f"Unable to connect to GCS at {self.gcs_address}. "
+                        "Check that (1) Ray GCS with matching version started "
+                        "successfully at the specified address, and (2) there is "
+                        "no firewall setting preventing access.")
+                else:
+                    logger.exception("Internal KV Put failed")
                 time.sleep(2)
+                error = e
         # Reraise the last grpc.RpcError.
-        raise
+        raise error
