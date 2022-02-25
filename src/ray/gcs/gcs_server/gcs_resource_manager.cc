3b3b6dda86ab9472691ff485cd03330c1d0df923
Yi Cheng <chengyidna@gmail.com>
2022-02-25 00:36:43 +0000

    fix

--- src/ray/gcs/gcs_server/gcs_resource_manager.cc
+++ src/ray/gcs/gcs_server/gcs_resource_manager.cc
@@ -268,11 +268,11 @@ void GcsResourceManager::UpdateNodeResourceUsage(const NodeID &node_id,
     }
     if (resources.resource_load_changed()) {
       (*iter->second.mutable_resource_load()) = resources.resource_load();
+      (*iter->second.mutable_resource_load_by_shape()) = resources.resource_load_by_shape();
     }
     if (resources.resources_normal_task_changed()) {
       (*iter->second.mutable_resources_normal_task()) = resources.resources_normal_task();
     }
-    (*iter->second.mutable_resource_load_by_shape()) = resources.resource_load_by_shape();
     iter->second.set_cluster_full_of_actors_detected(
         resources.cluster_full_of_actors_detected());
   }
