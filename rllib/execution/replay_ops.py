0eb477a79f8745a572e83aa43cd42f2dc7bf662a
Jun Gong <jungong@anyscale.com>
2022-02-26 11:40:58 -0800

    [RLlib] Fix a memeory leak in SimpleReplyBuffer that completely kills sampling throughput

--- rllib/execution/replay_ops.py
+++ rllib/execution/replay_ops.py
@@ -131,8 +131,6 @@ class SimpleReplayBuffer:
         self.replay_batches = []
         self.replay_index = 0
 
-        self.last_added_batches = []
-
     def add_batch(self, sample_batch: SampleBatchType) -> None:
         warn_replay_capacity(item=sample_batch, num_items=self.num_slots)
         if self.num_slots > 0:
@@ -142,8 +140,6 @@ class SimpleReplayBuffer:
                 self.replay_batches[self.replay_index] = sample_batch
                 self.replay_index += 1
                 self.replay_index %= self.num_slots
-
-            self.last_added_batches.append(sample_batch)
 
     def replay(self) -> SampleBatchType:
         return random.choice(self.replay_batches)
