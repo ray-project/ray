9e49857555236eaf4727f32a6170603ec9dac7ef
Simon Zehnder <simon.zehnder@gmail.com>
2022-02-27 12:53:54 +0100

    Added a safeguard to check for multiple episodes in a batch when 'batch_mode':'complete_episodes'.

--- rllib/examples/custom_metrics_and_callbacks.py
+++ rllib/examples/custom_metrics_and_callbacks.py
@@ -78,12 +78,16 @@ class MyCallbacks(DefaultCallbacks):
         env_index: int,
         **kwargs
     ):
-        # Make sure this episode is really done.
-        assert episode.batch_builder.policy_collectors["default_policy"].batches[-1][
-            "dones"
-        ][-1], (
-            "ERROR: `on_episode_end()` should only be called " "after episode is done!"
-        )
+        # Check if there are multiple episodes in a batch, i.e.
+        # "batch_mode":"truncate_episodes".
+        if worker.sampler.sample_collector.multiple_episodes_in_batch:
+            # Make sure this episode is really done.
+            assert episode.batch_builder.policy_collectors["default_policy"].batches[
+                -1
+            ]["dones"][-1], (
+                "ERROR: `on_episode_end()` should only be called "
+                "after episode is done!"
+            )
         pole_angle = np.mean(episode.user_data["pole_angles"])
         print(
             "episode {} (env-idx={}) ended with length {} and pole "
@@ -137,7 +141,7 @@ class MyCallbacks(DefaultCallbacks):
 if __name__ == "__main__":
     args = parser.parse_args()
 
-    ray.init()
+    ray.init(local_mode=True)
     trials = tune.run(
         "PG",
         stop={
