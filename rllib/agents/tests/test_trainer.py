8ff0ee281379b47b7cc92cc3c9e15fc7bc9aea31
Jun Gong <jungong@anyscale.com>
2022-02-28 14:37:06 -0800

    slightly refactor test_add_delete_policy

--- rllib/agents/tests/test_trainer.py
+++ rllib/agents/tests/test_trainer.py
@@ -114,9 +114,9 @@ class TestTrainer(unittest.TestCase):
 
                 # Make sure evaluation worker also gets the restored policy.
                 def _has_policy(w):
-                    assert w.get_policy("p0") is not None
+                    return w.get_policy("p0") is not None
 
-                test.evaluation_workers.foreach_worker(_has_policy)
+                self.assertTrue(all(test.evaluation_workers.foreach_worker(_has_policy)))
 
                 # Make sure trainer can continue training the restored policy.
                 pol0 = test.get_policy("p0")
