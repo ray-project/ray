5a17afced7c250c86cf2a9ffe0cd3bb699ef7ef9
黑驰 <senlin.zsl@antgroup.com>
2022-03-04 16:33:24 +0800

    fix comment

--- src/ray/gcs/gcs_server/gcs_resource_scheduler.cc
+++ src/ray/gcs/gcs_server/gcs_resource_scheduler.cc
@@ -19,18 +19,8 @@ namespace gcs {
 
 double LeastResourceScorer::Score(const ResourceRequest &required_resources,
                                   const NodeResources &node_resources) {
-  // In GCS-based actor scheduling, the `resources_available_` (of class
-  // `SchedulingResources`) is only acquired or released by actor scheduling, instead of
-  // being updated by resource reports from raylets. So the 'actual' available resources
-  // (if there exist normal tasks) are equal to `resources_available_` -
-  // `resources_normal_tasks_`.
-
-  // TODO(Shanly): Take normal task resources into account later.
-  // if (!node_resources.GetNormalTaskResources().IsEmpty()) {
-  //   new_available_resource_set = node_resources.GetAvailableResources();
-  //   new_available_resource_set.SubtractResources(node_resources.GetNormalTaskResources());
-  //   available_resource_set = &new_available_resource_set;
-  // }
+  // TODO(Shanly): Take normal task resources into account later for GCS-based actor
+  // scheduling.
 
   double node_score = 0.;
 
