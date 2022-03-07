b686434488a2473a7656c4c445fd5a22bc1cd444
Balaji Veeramani <bveeramani@berkeley.edu>
2022-03-07 15:55:52 -0800

    Update backend.py

--- python/ray/train/backend.py
+++ python/ray/train/backend.py
@@ -235,8 +235,10 @@ class BackendExecutor:
                     "cluster either has enough resources or use an "
                     "autoscaling cluster. If you are running on a cluster, "
                     "make sure you specify an address in `ray.init()`, for example, "
-                    '`ray.init("auto")`. Current resources available: {}, resources '
-                    "requested by the placement group: {}".format(
+                    '`ray.init("auto")`. You can also increase the timeout by setting '
+                    "the TRAIN_PLACEMENT_GROUP_TIMEOUT_S environment variable. "
+                    "Current resources available: {}, resources requested by the "
+                    "placement group: {}".format(
                         ray.available_resources(), placement_group.bundle_specs
                     )
                 )
