19b2dcb0a18a01a520fa93f4c9179ec54d2b2144
Balaji Veeramani <bveeramani@berkeley.edu>
2022-03-07 13:05:09 -0800

    Update python/ray/train/torch.py
    Co-authored-by: Amog Kamsetty <amogkam@users.noreply.github.com>

--- python/ray/train/torch.py
+++ python/ray/train/torch.py
@@ -37,7 +37,7 @@ logger = logging.getLogger(__name__)
 
 
 class TorchAccelerator(Accelerator):
-    """An object that implements methods to accelerate PyTorch training."""
+    """A utility that implements methods to accelerate PyTorch training."""
 
     def prepare_model(
         self,
