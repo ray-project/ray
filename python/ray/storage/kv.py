6b4c904f86b323d8190447130b2ae5cac9f4edc9
Eric Liang <ekhliang@gmail.com>
2022-03-07 19:38:57 -0800

    update

---
+++ python/ray/storage/kv.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from typing import Dict, Union, Optional, Any, List
+
+from ray.types import ObjectRef
+
+
+@dataclass
+class ObjectEntry:
+    path: str
+    size: int
+
+
+def save_objects(namespace: str, objects: Dict[str, Union[Any, ObjectRef]]):
+    raise NotImplementedError
+
+
+def delete_objects(namespace: str, paths: List[str]):
+    raise NotImplementedError
+
+
+def get_object_info(namespace: str, paths: List[str]) -> List[ObjectEntry]:
+    raise NotImplementedError
+
+
+def list_objects(
+    namespace: str, prefix: str, limit: Optional[int] = None
+) -> List[ObjectEntry]:
+    raise NotImplementedError
+
+
+def load_objects(namespace: str, paths: List[str]) -> List[ObjectRef]:
+    raise NotImplementedError
