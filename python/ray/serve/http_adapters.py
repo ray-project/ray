2b67b86aeec4eada02ce6feeb301baafe72d371d
simon-mo <simon.mo@hey.com>
2022-03-08 12:15:59 -0800

    add batching changes

--- python/ray/serve/http_adapters.py
+++ python/ray/serve/http_adapters.py
@@ -1,23 +1,54 @@
-from typing import List, Union
-import numpy as np
-from ray.serve.model_wrappers import DataBatchType
-from pydantic import BaseModel
-
-
-class Ndarray(BaseModel):
-    array: Union[List[float], List[List[float]], List[List[List[float]]]]
-
-
-def serve_api_resolver(payload: Ndarray) -> DataBatchType:
-    return np.array(payload.array)
-
+from io import BytesIO
+from typing import List, Optional, Union
 
 from fastapi import File
-from PIL import Image
-from io import BytesIO
-import numpy as np
+from pydantic import BaseModel, Field
+
+from ray.serve.model_wrappers import DataBatchType
+from ray.serve.utils import require_packages
 
 
-async def image_to_databatch(img: bytes = File(...)) -> np.ndarray:
+_1DArray = List[float]
+_2DArray = List[List[float]]
+_3DArray = List[List[List[float]]]
+_4DArray = List[List[List[List[float]]]]
+
+
+class NdArray(BaseModel):
+    """Schema for numeric array input."""
+
+    array: Union[_1DArray, _2DArray, _3DArray, _4DArray] = Field(
+        ...,
+        description="The array content as a list or nested list. You can pass in 1D to 4D array in its original form.",
+    )
+    shape: Optional[List[int]] = Field(
+        ...,
+        description="The shape of the array. If present, the array will be reshaped via `reshape`.",
+    )
+    dtype: Optional[str] = Field(
+        ...,
+        description="The dtype of the array. If present, the array will be cast by `astype`.",
+    )
+
+
+@require_packages(["numpy"])
+def array_to_databatch(payload: NdArray) -> DataBatchType:
+    """Accept a NdArray from HTTP body, returns array."""
+    import numpy as np
+
+    arr = np.array(payload.array)
+    if payload.shape:
+        arr = arr.reshape(*payload.shape)
+    if payload.dtype:
+        arr = arr.astype(payload.dtype)
+    return arr
+
+
+@require_packages(["PIL", "numpy"])
+def image_to_databatch(img: bytes = File(...)) -> DataBatchType:
+    """Accept a PIL readable file from HTTP Form, returns array."""
+    from PIL import Image
+    import numpy as np
+
     image = Image.open(BytesIO(img))
     return np.array(image)
--- python/ray/serve/model_wrappers.py
+++ python/ray/serve/model_wrappers.py
@@ -1,118 +1,53 @@
-from abc import abstractclassmethod, abstractmethod
-from typing import Any, Callable, Type, Union
-from fastapi import Depends, FastAPI
-import numpy as np
-import pandas as pd
-from ray.serve.http_util import ASGIHTTPSender
+from typing import Any, Callable, Dict, Optional, Type, Union
+
 import starlette.requests
-import torch
-from ray import serve
+from fastapi import Depends, FastAPI
+
 from ray._private.utils import import_attr
+from ray.ml.checkpoint import Checkpoint
+from ray.ml.predictor import Predictor, DataBatchType
+from ray.serve.http_util import ASGIHTTPSender
+from ray import serve
+
+DEFAULT_INPUT_SCHEMA = "ray.serve.http_adapters.array_to_databatch"
+InputSchemaFn = Callable[[Any], DataBatchType]
 
 
-DataBatchType = Union[np.ndarray, pd.DataFrame]
+def _load_input_schema(
+    input_schema: Optional[Union[str, InputSchemaFn]]
+) -> InputSchemaFn:
+    if input_schema is None:
+        input_schema = DEFAULT_INPUT_SCHEMA
+
+    if isinstance(input_schema, str):
+        input_schema = import_attr(input_schema)
+
+    assert callable(input_schema), "input schema must be callable"
+    return input_schema
 
 
-class BaseCheckpoint:
-    @abstractclassmethod
-    def from_uri(cls, uri: str) -> "BaseCheckpoint":
-        pass
-
-    @abstractmethod
-    def load(self) -> Any:
-        pass
-
-
-class BaseModel:
-    @abstractclassmethod
-    def load_from_checkpoint(cls, checkpoint: BaseCheckpoint) -> "BaseModel":
-        pass
-
-    @abstractmethod
-    def predict(self, inp: DataBatchType) -> Any:
-        pass
-
-
-class TorchCheckpoint:
-    def __init__(self, on_disk_path: str):
-        self.on_disk_path = on_disk_path
-
-    @classmethod
-    def from_uri(cls, uri: str) -> "TorchCheckpoint":
-        return cls(uri)
-
-    def load(self):
-        return torch.load(self.on_disk_path)
-
-
-class TorchModel:
-    def __init__(self, nn_module) -> None:
-        self.model = nn_module.eval()
-
-    @classmethod
-    def load_from_checkpoint(cls, checkpoint: TorchCheckpoint) -> "TorchModel":
-        return cls(checkpoint.load())
-
-    def predict(self, inp: np.ndarray) -> np.ndarray:
-        with torch.inference_mode():
-            out = self.model(inp)
-        return out.numpy().tolist()
-
-
-# TODO(somehow infer the name and route_prefix for the Model)
 class ModelWrapper:
     def __init__(
         self,
-        model_cls: Type[BaseModel],
-        checkpoint: BaseCheckpoint,
-        input_schema: Union[None, str, Callable[[Any], DataBatchType]] = None,
+        predictor_cls: Type[Predictor],
+        checkpoint: Checkpoint,
+        input_schema: Optional[Union[str, InputSchemaFn]] = None,
+        batching_params: Optional[Dict[str, int]] = None,
     ) -> None:
-        self.model = model_cls.load_from_checkpoint(checkpoint)
-        if input_schema is None:
-            from ray.serve.http_adapters import serve_api_resolver
-
-            input_schema = serve_api_resolver
-        elif isinstance(input_schema, str):
-            input_schema = import_attr(input_schema)
-        assert callable(input_schema), "input schema must be callable"
-
+        self.model = predictor_cls.load_from_checkpoint(checkpoint)
         self.app = FastAPI()
-        # self.app.add_api_route("/", self.handle_request, methods=["POST"])
 
-        @self.app.post("/")
-        @serve.batch(max_batch_size=8, batch_wait_timeout_s=3)
+        input_schema = _load_input_schema(input_schema)
+        batching_params = batching_params or dict(
+            max_batch_size=8, batch_wait_timeout_s=0.01
+        )
+
+        @self.app.post("/predict")
+        @serve.batch(**batching_params)
         async def handle_request(inp=Depends(input_schema)):
-            # TODO(simon): implement custom JSON encoder for FastAPI to handle common type
-            print(f"Got batch size of {len(inp)}")
             return self.model.predict(inp)
-
-        # Inject the input schema as FastAPI dependencies
-        # sig = inspect.signature(self.handle_request.__func__)
-        # inp_param = sig.parameters["inp"]
-        # new_param = inp_param.replace(default=Depends(input_schema))
-        # setattr(
-        #     self.handle_request.__func__,
-        #     "__signature__",
-        #     sig.replace(parameters=[new_param]),
-        # )
-
-        # # Build FastAPI app
 
     async def __call__(self, request: starlette.requests.Request):
         sender = ASGIHTTPSender()
         await self.app(request.scope, receive=request.receive, send=sender)
         return sender.build_asgi_response()
-
-
-@serve.deployment
-class TorchModelWrapper(ModelWrapper):
-    def __init__(
-        self,
-        checkpoint_uri: str,
-        input_schema: str = "ray.serve.http_adapters.serve_api_resolver",
-    ) -> None:
-        super().__init__(
-            TorchModel,
-            TorchCheckpoint.from_uri(checkpoint_uri),
-            input_schema=input_schema,
-        )
