e0a67eb1c0f0ff3fb0c3ecf4099a7c710e42c1e4
Shreyas Krishnaswamy <shrekris@anyscale.com>
2022-03-08 21:44:50 -0800

    Use correct status function name

--- dashboard/modules/serve/tests/test_schema.py
+++ dashboard/modules/serve/tests/test_schema.py
@@ -579,7 +579,9 @@ def test_serve_application_to_schema_to_serve_application():
     assert requests.get("http://localhost:8000/hi").text == "Hello world!"
 
     # Check statuses
-    statuses = serve_application_status_to_schema(serve.get_statuses()).statuses
+    statuses = serve_application_status_to_schema(
+        serve.get_deployment_statuses()
+    ).statuses
     deployment_names = {"f1", "f2"}
     for deployment_status in statuses:
         assert deployment_status.status in {"UPDATING", "HEALTHY"}
