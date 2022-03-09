e713537691e5a5add1dfa2ae524cc398fd6d443e
Amog Kamsetty <amogkamsetty@yahoo.com>
2022-03-08 23:18:08 -0800

    fix

--- python/ray/data/dataset.py
+++ python/ray/data/dataset.py
@@ -2054,11 +2054,10 @@ Dict[str, List[str]]]): The names of the columns
                 use for the label column. If None, then automatically infer
                 the dtype.
             feature_column_dtypes (Union[None, torch.dtype, List[torch.dtype],\
-Dict[str, torch.dtype]]): The dtypes to use
-                for the feature tensors. This should match the format of
-                ``feature_columns``, or be a single dtype, in which case it
-                will be applied to all tensors. If None, then automatically
-                infer the dtype.
+Dict[str, torch.dtype]]): The dtypes to use for the feature
+                tensors. This should match the format of ``feature_columns``,
+                or be a single dtype, in which case it will be applied to
+                all tensors. If None, then automatically infer the dtype.
             batch_size (int): How many samples per batch to yield at a time.
                 Defaults to 1.
             prefetch_blocks (int): The number of blocks to prefetch ahead of
