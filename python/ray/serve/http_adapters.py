780b893b5e91c3fa8a31e7b6d2581c33b821bfd7
simon-mo <simon.mo@hey.com>
2022-03-09 09:18:06 -0800

    lint

--- python/ray/serve/http_adapters.py
+++ python/ray/serve/http_adapters.py
@@ -34,7 +34,8 @@ class NdArray(BaseModel):
     dtype: Optional[str] = Field(
         default=None,
         description=(
-            "The numpy dtype of the array. If present, the array will be cast by `astype`."
+            "The numpy dtype of the array. If present, the array will be cast "
+            "by `astype`."
         ),
     )
 
@@ -51,7 +52,9 @@ def array_to_databatch(payload: NdArray) -> DataBatchType:
 
 @require_packages(["PIL"])
 def image_to_databatch(img: bytes = File(...)) -> DataBatchType:
-    """Accepts a PIL-readable file from an HTTP form and converts it to a DataBatchType."""
+    """Accepts a PIL-readable file from an HTTP form and converts
+    it to a DataBatchType.
+    """
     from PIL import Image
 
     image = Image.open(BytesIO(img))
