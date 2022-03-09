75fe3d16907cc4ee34672630e325c1137353d73a
Antoni Baum <antoni.baum@protonmail.com>
2022-03-09 21:27:46 +0000

    [tune] Add `Trainable.postprocess_checkpoint`

--- python/ray/tune/function_runner.py
+++ python/ray/tune/function_runner.py
@@ -479,6 +479,8 @@ class FunctionRunner(Trainable):
             checkpoint, parent_dir, state
         )
 
+        self.postprocess_checkpoint(checkpoint_path)
+
         self._maybe_save_to_cloud(parent_dir)
 
         return checkpoint_path
