c940cdbac2a209090d3c4e67ae0a97d283bd3f4c
SangBin Cho <rkooo567@gmail.com>
2022-03-09 21:51:20 -0800

    Addressed code review.

--- release/ray_release/scripts/convert_legacy_config.py
+++ release/ray_release/scripts/convert_legacy_config.py
@@ -35,8 +35,7 @@ def main(legacy_config: str, prefix: str, group: str, alert: str):
     tests = []
     for old in config:
         test = {}
-        # test["name"] = f"{prefix}_{old['name']}"
-        test["name"] = old["name"]
+        test["name"] = f"{prefix}_{old['name']}"
 
         test["group"] = group
         test["working_dir"] = os.path.basename(os.path.dirname(legacy_config))
