ce34ccfb735384e98d187d7a67ebea38b3784f87
Siyuan Zhuang <suquark@gmail.com>
2022-03-10 15:44:05 -0800

    add test for objectrefs

--- python/ray/workflow/tests/test_dag_to_workflow.py
+++ python/ray/workflow/tests/test_dag_to_workflow.py
@@ -53,6 +53,61 @@ def test_dag_to_workflow_options(workflow_start_regular_shared):
     assert wf.data.step_options.ray_options["num_gpus"] == 100
 
 
+def test_dedupe_serialization_dag(workflow_start_regular_shared):
+    from ray.workflow import serialization
+
+    @ray.remote
+    def identity(x):
+        return x
+
+    @ray.remote
+    def gather(*args):
+        return args
+
+    def get_num_uploads():
+        manager = serialization.get_or_create_manager()
+        stats = ray.get(manager.export_stats.remote())
+        return stats.get("num_uploads", 0)
+
+    ref = ray.put("hello world 12345")
+    list_of_refs = [ref for _ in range(20)]
+
+    assert get_num_uploads() == 0
+
+    single = identity._bind((ref,))
+    double = identity._bind(list_of_refs)
+
+    result_ref, result_list = workflow.create(gather._bind(single, double)).run()
+
+    for result in result_list:
+        assert ray.get(*result_ref) == ray.get(result)
+
+    # One upload for the initial checkpoint, and one for the object ref after
+    # resuming.
+    assert get_num_uploads() == 2
+
+
+def test_same_object_many_dags(workflow_start_regular_shared):
+    """Ensure that when we dedupe uploads, we upload the object once per DAG,
+    since different DAGs shouldn't look in each others object directories.
+    """
+
+    @ray.remote
+    def f(a):
+        return [a[0]]
+
+    x = {0: ray.put(10)}
+
+    result1 = workflow.create(f._bind(x)).run()
+    result2 = workflow.create(f._bind(x)).run()
+    with InputNode() as dag_input:
+        result3 = workflow.create(f._bind(dag_input.x), x=x).run()
+
+    assert ray.get(*result1) == 10
+    assert ray.get(*result2) == 10
+    assert ray.get(*result3) == 10
+
+
 if __name__ == "__main__":
     import sys
 
