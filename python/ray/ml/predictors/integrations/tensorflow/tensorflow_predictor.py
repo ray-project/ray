6c69d8cae35f2acd39e83a796becb5adb7ba18ef
Antoni Baum <antoni.baum@protonmail.com>
2022-03-11 12:17:43 +0000

    [ML] Add `TensorflowPredictor` interface

---
+++ python/ray/ml/predictors/integrations/tensorflow/tensorflow_predictor.py
@@ -0,0 +1,126 @@
+from typing import Optional, Union, List, Type
+
+import tensorflow as tf
+
+from ray.ml.predictor import Predictor, DataBatchType
+from ray.ml.preprocessor import Preprocessor
+from ray.ml.checkpoint import Checkpoint
+
+# TensorFlow model objects cannot be pickled, therefore we use
+# a callable that returns the model, instead of a model object
+# itself.
+
+
+class TensorflowPredictor(Predictor):
+    """A predictor for TensorFlow models.
+
+    Args:
+        model_definition: A callable that returns a TensorFlow Keras model
+            to use for predictions.
+        preprocessor: A preprocessor used to transform data batches prior
+            to prediction.
+        model_weights: List of weights to use for the model.
+    """
+
+    def __init__(
+        self,
+        model_definition: Type[tf.keras.Model],
+        preprocessor: Preprocessor,
+        model_weights: Optional[list] = None,
+    ):
+        raise NotImplementedError
+
+    @classmethod
+    def from_checkpoint(
+        cls, checkpoint: Checkpoint, model_definition: Type[tf.keras.Model]
+    ) -> "TensorflowPredictor":
+        """Instantiate the predictor from a Checkpoint.
+
+        The checkpoint is expected to be a result of ``TorchTrainer``.
+
+        Args:
+            checkpoint: The checkpoint to load the model and
+                preprocessor from. It is expected to be from the result of a
+                ``TorchTrainer`` run.
+            model_definition: A callable that returns a TensorFlow Keras model
+                to use. Model weights will be loaded from the checkpoint.
+        """
+        raise NotImplementedError
+
+    def predict(
+        self,
+        data: DataBatchType,
+        column_names: Optional[List[str]] = None,
+        feature_columns: Optional[Union[List[str], List[List[str]]]] = None,
+        dtype: Optional[tf.dtypes.DType] = None,
+    ) -> DataBatchType:
+        """Run inference on data batch.
+
+        The data is converted into a torch Tensor before being inputted to
+        the model.
+
+        Args:
+            data: A batch of input data. Either a pandas DataFrame or numpy
+                array.
+            feature_columns: The names or indices of the columns in the
+                data to use as features to predict on. If this arg is a
+                list of lists, then the data batch will be converted into a
+                multiple tensors which are then concatenated before feeding
+                into the model. This is useful for multi-input models. If
+                None, then use all columns in ``data``.
+            dtype: The TensorFlow dtype to use when creating the TensorFlow tensor.
+                If set to None, then automatically infer the dtype.
+
+        Examples:
+
+        .. code-block:: python
+
+            import numpy as np
+            import tensorflow as tf
+            from ray.ml.predictors.tensorflow import TensorflowPredictor
+
+            def build_model(self):
+                return tf.keras.Sequential(
+                    [
+                        tf.keras.layers.InputLayer(input_shape=(1,)),
+                        tf.keras.layers.Dense(1),
+                    ]
+                )
+
+            predictor = TensorflowPredictor(model_definition=build_model)
+
+            data = np.array([[1, 2], [3, 4]])
+            predictions = predictor.predict(data)
+
+            # Only use first column as the feature
+            predictions = predictor.predict(data, feature_columns=[0])
+
+        .. code-block:: python
+
+            import pandas as pd
+            import tensorflow as tf
+            from ray.ml.predictors.tensorflow import TensorflowPredictor
+
+            def build_model(self):
+                return tf.keras.Sequential(
+                    [
+                        tf.keras.layers.InputLayer(input_shape=(1,)),
+                        tf.keras.layers.Dense(1),
+                    ]
+                )
+
+            predictor = TensorflowPredictor(model_definition=build_model)
+
+            # Pandas dataframe.
+            data = pd.DataFrame([[1, 2], [3, 4]], columns=["A", "B"])
+
+            predictions = predictor.predict(data)
+
+            # Only use first column as the feature
+            predictions = predictor.predict(data, feature_columns=["A"])
+
+
+        Returns:
+            DataBatchType: Prediction result.
+        """
+        raise NotImplementedError
