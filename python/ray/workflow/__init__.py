28d597e0097abd98493560208b035ed155d9fb42
Stephanie Wang <swang@cs.berkeley.edu>
2022-03-11 12:49:08 -0500

    Revert "[workflow] Convert DAG to workflow (#22925)" (#23081)
    This reverts commit 0a9f966e63a1a1d38a1e291338384b1e84b3a2a9.

--- python/ray/workflow/__init__.py
+++ python/ray/workflow/__init__.py
@@ -14,7 +14,6 @@ from ray.workflow.api import (
     sleep,
     delete,
     wait,
-    create,
 )
 from ray.workflow.workflow_access import WorkflowExecutionError
 from ray.workflow.common import WorkflowStatus
@@ -38,7 +37,6 @@ __all__ = [
     "EventListener",
     "delete",
     "wait",
-    "create",
 ]
 
 globals().update(WorkflowStatus.__members__)
