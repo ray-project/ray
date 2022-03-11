3f2ce89748088d35f92a6783c9a15bbd75b29ecd
Amog Kamsetty <amogkamsetty@yahoo.com>
2022-03-10 19:19:42 -0800

    address comments

--- python/ray/ml/predictors/integrations/torch/torch_predictor.py
+++ python/ray/ml/predictors/integrations/torch/torch_predictor.py
@@ -12,17 +12,18 @@ class TorchPredictor(Predictor):
 
     Args:
         model: The torch module to use for predictions.
-        preprocessor: The preprocessor used to transform data batches prior
+        preprocessor: A preprocessor used to transform data batches prior
             to prediction.
-
     """
 
-    def __init__(self, model: torch.nn.Module, preprocessor: Preprocessor):
+    def __init__(
+        self, model: torch.nn.Module, preprocessor: Optional[Preprocessor] = None
+    ):
         raise NotImplementedError
 
     @classmethod
     def from_checkpoint(
-        cls, checkpoint: Checkpoint, model_definition: Optional[torch.nn.Module] = None
+        cls, checkpoint: Checkpoint, model: Optional[torch.nn.Module] = None
     ) -> "TorchPredictor":
         """Instantiate the predictor from a Checkpoint.
 
@@ -32,36 +33,70 @@ class TorchPredictor(Predictor):
             checkpoint: The checkpoint to load the model and
                 preprocessor from. It is expected to be from the result of a
                 ``TorchTrainer`` run.
-            model_definition: If checkpoints
-                contains the model state dict, and not the model itself,
-                then the state dict will be loaded to the model_definition.
-
+            model: If the checkpoint contains a model state dict, and not
+                the model itself, then the state dict will be loaded to this
+                ``model``.
         """
         raise NotImplementedError
 
     def predict(
         self,
         data: DataBatchType,
-        column_names: Optional[List[str]] = None,
-        feature_columns: Optional[Union[List[str], List[List[str]]]] = None,
-        dtype: Optional[Union[torch.dtype, List[torch.dtype]]] = None,
+        feature_columns: Optional[
+            Union[List[str], List[List[str]], List[int], List[List[int]]]
+        ] = None,
+        dtype: Optional[torch.dtype] = None,
     ) -> DataBatchType:
         """Run inference on data batch.
 
         Args:
-            data: A batch of input data.
-            columns_names: If provided, the column names to set for the data
-                batch.
-            feature_columns: The names of the columns in the dataframe to
-                use as features to predict on. If this arg is a List[List[
-                str]], then the data batch will be converted into a list of
-                tensors. This is useful for multi-input models. If None,
-                then use all columns in the ``data_batch``.
-            dtype: The torch dtype to use for the tensor. If set to None,
-                then automatically infer the dtype.
+            data: A batch of input data. Either a pandas DataFrame or numpy
+                array.
+            feature_columns: The names or indices of the columns in the
+                data to use as features to predict on. If this arg is a
+                list of lists, then the data batch will be converted into a
+                multiple tensors which are then concatenated before feeding
+                into the model. This is useful for multi-input models. If
+                None, then use all columns in ``data``.
+            dtype: The torch dtype to use when creating the torch tensor.
+                If set to None, then automatically infer the dtype.
+
+        Examples:
+
+        .. code-block:: python
+
+            import numpy as np
+            import torch
+            from ray.ml.predictors.torch import TorchPredictor
+
+            model = torch.nn.Linear(1, 1)
+            predictor = TorchPredictor(model=model)
+
+            data = np.array([[1, 2], [3, 4]])
+            predictions = predictor.predict(data)
+
+            # Only use first column as the feature
+            predictions = predictor.predict(data, feature_columns=[0])
+
+        .. code-block:: python
+
+            import pandas as pd
+            import torch
+            from ray.ml.predictors.torch import TorchPredictor
+
+            model = torch.nn.Linear(1, 1)
+            predictor = TorchPredictor(model=model)
+
+            # Pandas dataframe.
+            data = pd.DataFrame([[1, 2], [3, 4]], columns=["A", "B"])
+
+            predictions = predictor.predict(data)
+
+            # Only use first column as the feature
+            predictions = predictor.predict(data, feature_columns=["A"])
+
 
         Returns:
             DataBatchType: Prediction result.
-
         """
         raise NotImplementedError
