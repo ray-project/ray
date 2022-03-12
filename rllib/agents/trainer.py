c8d85e48f6f3d2dd5b9261b660dd264a29d247d6
Artur Niederfahrenhorst <attaismyname@googlemail.com>
2022-03-12 08:52:04 +0530

    wip

--- rllib/agents/trainer.py
+++ rllib/agents/trainer.py
@@ -2752,8 +2752,8 @@ class Trainer(Trainable):
             None, if local replay buffer is not needed.
         """
         # Some agents do not need a replay buffer
-        if "replay_buffer_config" not in config or config[
-            "replay_buffer_config"].get(
+        if "replay_buffer_config" not in config or config.get(
+            "replay_buffer_config", {}).get(
             "no_local_replay_buffer", False
         ):
             return None
