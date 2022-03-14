08fb88a618c506ef8e11a963128af63017671340
Ed Oakes <ed.nmi.oakes@gmail.com>
2022-03-14 14:53:16 -0500

    WIP

--- python/ray/serve/scripts.py
+++ python/ray/serve/scripts.py
@@ -8,6 +8,7 @@ from typing import Tuple, List, Dict
 import argparse
 
 import ray
+from ray._private.utils import import_attr
 from ray.serve.config import DeploymentMode
 from ray import serve
 from ray.serve.constants import (
@@ -31,81 +32,6 @@ RAY_DASHBOARD_ADDRESS_HELP_STR = (
     "http://localhost:8265). Can also be specified using the "
     "RAY_ADDRESS environment variable."
 )
-
-
-def _process_args_and_kwargs(
-    args_and_kwargs: Tuple[str],
-) -> Tuple[List[str], Dict[str, str]]:
-    """
-    Takes in a Tuple of strings. Any string prepended with "--" is considered a
-    keyword. Keywords must be formatted as --keyword=value or --keyword value.
-    All other strings are considered args. All args must come before kwargs.
-
-    For example:
-
-    ("argval1", "argval2", "--kwarg1", "kwval1", "--kwarg2", "kwval2",)
-
-    becomes
-
-    args = ["argval1", "argval2"]
-    kwargs = {"kwarg1": "kwval1", "kwarg2": "kwval2"}
-    """
-
-    if args_and_kwargs is None:
-        return [], {}
-
-    class ErroringArgumentParser(argparse.ArgumentParser):
-        """
-        ArgumentParser prints and exits upon error. This subclass raises a
-        ValueError instead.
-        """
-
-        def error(self, message):
-            if message.find("unrecognized arguments") == 0:
-                # Give clear message when args come between or after kwargs
-                arg = message[message.find(":") + 2 :]
-                raise ValueError(
-                    f'Argument "{arg}" was separated from other args by '
-                    "keyword arguments. Args cannot be separated by "
-                    f"kwargs.\nMessage from parser: {message}"
-                )
-            elif message.endswith("expected one argument"):
-                # Give clear message when kwargs are undefined
-                kwarg = message[message.find("--") : message.rfind(":")]
-                raise ValueError(
-                    f'Got no value for argument "{kwarg}". All '
-                    "keyword arguments specified must have a value."
-                    f"\nMessage from parser: {message}"
-                )
-            else:
-                # Raise argparse's error otherwise
-                raise ValueError(message)
-
-    parser = ErroringArgumentParser()
-    parser.add_argument("args", nargs="*")
-    for arg_or_kwarg in args_and_kwargs:
-        if arg_or_kwarg[:2] == "--":
-            parser.add_argument(arg_or_kwarg.split("=")[0])
-
-    args_and_kwargs = vars(parser.parse_args(args_and_kwargs))
-    args = args_and_kwargs["args"]
-    del args_and_kwargs["args"]
-    return args, args_and_kwargs
-
-
-def _configure_runtime_env(deployment: Deployment, updates: Dict):
-    """Overwrites deployment's runtime_env with fields in updates.
-
-    Any fields in deployment's runtime_env that aren't in updates stay the
-    same.
-    """
-
-    if deployment.ray_actor_options is None:
-        deployment._ray_actor_options = {"runtime_env": updates}
-    else:
-        current_env = deployment.ray_actor_options.get("runtime_env", {})
-        updates.update(current_env)
-        deployment.ray_actor_options["runtime_env"] = updates
 
 
 @click.group(help="[EXPERIMENTAL] CLI for managing Serve instances on a Ray cluster.")
@@ -251,16 +177,12 @@ def deploy(config_file_name: str, address: str):
     short_help="Run a Serve app in a blocking way.",
     help=(
         "Runs the Serve app from the specified YAML config file or import path "
-        "for a deployment class or function (`my_module.MyClassOrFunction`).\n"
-        "If deploying via import path, you can pass args and kwargs to the "
-        "constructor using serve run -- [arg1]...[argn] "
-        "[--kwarg1=val1]...[--kwargn=valn].\n"
+        "to a bound deployment node or built application object.\n"
         "Blocks after deploying and logs status periodically. If you Ctrl-C "
         "this command, it tears down the app."
     ),
 )
 @click.argument("config_or_import_path")
-@click.argument("args_and_kwargs", required=False, nargs=-1)
 @click.option(
     "--runtime-env",
     type=str,
@@ -305,58 +227,32 @@ def run(
     working_dir: str,
     address: str,
 ):
-    # Check if path provided is for config or import
-    is_config = pathlib.Path(config_or_import_path).is_file()
-    args, kwargs = _process_args_and_kwargs(args_and_kwargs)
-
-    # Calculate deployments' runtime env updates requested via args
-    runtime_env_updates = parse_runtime_env_args(
+    final_runtime_env = parse_runtime_env_args(
         runtime_env=runtime_env,
         runtime_env_json=runtime_env_json,
         working_dir=working_dir,
     )
 
-    # Create ray.init()'s runtime_env
-    if "working_dir" in runtime_env_updates:
-        ray_runtime_env = {"working_dir": runtime_env_updates.pop("working_dir")}
-    else:
-        ray_runtime_env = {}
-
-    if is_config:
+    app_or_node = None
+    if pathlib.Path(config_or_import_path).is_file():
         config_path = config_or_import_path
-        # Delay serve.start() to catch invalid inputs without waiting
-        if len(args) + len(kwargs) > 0:
-            raise ValueError(
-                "ARGS_AND_KWARGS cannot be defined for a "
-                "config file deployment. Please specify the "
-                "init_args and init_kwargs inside the config file."
-            )
-
-        cli_logger.print("Deploying application in config file at " f"{config_path}.")
+        cli_logger.print(f"Loading app from config file: '{config_path}'.")
         with open(config_path, "r") as config_file:
-            app = Application.from_yaml(config_file)
-
+            app_or_node = Application.from_yaml(config_file)
     else:
         import_path = config_or_import_path
         if "." not in import_path:
             raise ValueError(
                 "Import paths must be of the form "
-                '"module.submodule_1...submodule_n.MyClassOrFunction".'
+                "'module.submodule.app_or_bound_deployment'."
             )
 
-        cli_logger.print(f'Deploying function or class imported from "{import_path}".')
+        cli_logger.print(f"Loading app from import path: '{import_path}'.")
+        app_or_node = import_attr(import_path)
 
-        deployment_name = import_path[import_path.rfind(".") + 1 :]
-        deployment = serve.deployment(name=deployment_name)(import_path)
-
-        app = Application([deployment.options(init_args=args, init_kwargs=kwargs)])
-
-    ray.init(address=address, namespace="serve", runtime_env=ray_runtime_env)
-
-    for deployment in app:
-        _configure_runtime_env(deployment, runtime_env_updates)
-
-    app.run(logger=cli_logger)
+    # Setting the runtime_env here will set defaults for the deployments.
+    ray.init(address=address, namespace="serve", runtime_env=final_runtime_env)
+    serve.run(app_or_node, host=host, port=port, logger=cli_logger)
 
 
 @cli.command(
