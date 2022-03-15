e9530218f09a6c53b25aaf70b9d55a41b27016be
捕牛 <hejialing.hjl@antgroup.com>
2022-03-15 20:06:36 +0800

    fix UT

--- python/ray/tests/test_runtime_env.py
+++ python/ray/tests/test_runtime_env.py
@@ -870,12 +870,12 @@ def test_runtime_env_config(start_cluster):
     bad_configs = []
     bad_configs.append({"setup_timeout_seconds": 10.0})
     bad_configs.append({"setup_timeout_seconds": 0})
-    bad_configs.append({"setup_timeout_seconds": -1})
     bad_configs.append({"setup_timeout_seconds": "10"})
     bad_configs.append({"bad_field": 10})
 
     good_configs = []
     good_configs.append({"setup_timeout_seconds": 10})
+    good_configs.append({"setup_timeout_seconds": -1})
 
     @ray.remote
     def f():
