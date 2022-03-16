c4d185c5f4327abe211c95a806848e4974cbacef
architkulkarni <arkulkar@gmail.com>
2022-03-16 14:42:46 -0700

    Handle case where entrypoint is unavailable

--- dashboard/modules/job/common.py
+++ dashboard/modules/job/common.py
@@ -109,7 +109,9 @@ class JobInfoStorageClient:
                 assert False, "Attempted to change job status from a terminal state."
             new_info = replace(old_info, status=status, message=message)
         else:
-            new_info = JobInfo(status=status, message=message)
+            new_info = JobInfo(
+                entrypoint="Entrypoint not found.", status=status, message=message
+            )
 
         if status.is_terminal():
             new_info.end_time = int(time.time())
