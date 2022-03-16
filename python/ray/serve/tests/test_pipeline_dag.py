8838a5880c98b1bbae930b0a84cff452424b7c46
Ed Oakes <ed.nmi.oakes@gmail.com>
2022-03-16 14:36:00 -0500

    tests passing maybe

--- python/ray/serve/tests/test_pipeline_dag.py
+++ python/ray/serve/tests/test_pipeline_dag.py
@@ -1,14 +1,21 @@
 import pytest
 import os
 import sys
+from typing import Union
 
 import numpy as np
 
 import ray
 from ray import serve
-from ray.serve.api import _get_deployments_from_node
+from ray.serve.api import Application, DeploymentNode, _get_deployments_from_node
 from ray.serve.handle import PipelineHandle
 from ray.serve.pipeline.pipeline_input_node import PipelineInputNode
+
+
+def maybe_build(
+    node: DeploymentNode, use_build: bool
+) -> Union[Application, DeploymentNode]:
+    return Application.from_dict(serve.build(node).to_dict())
 
 
 @serve.deployment
@@ -53,13 +60,15 @@ def test_single_node_deploy_success(serve_instance):
     assert ray.get(handle.remote(41)) == 42
 
 
-def test_single_node_driver_sucess(serve_instance):
+@pytest.mark.parametrize("use_build", [False, True])
+def test_single_node_driver_sucess(serve_instance, use_build):
     m1 = Adder.bind(1)
     m2 = Adder.bind(2)
     with PipelineInputNode() as input_node:
         out = m1.forward.bind(input_node)
         out = m2.forward.bind(out)
-    driver = Driver.bind(out)
+
+    driver = maybe_build(Driver.bind(out), use_build)
     handle = serve.run(driver)
     assert ray.get(handle.remote(39)) == 42
 
@@ -99,60 +108,71 @@ class TakeHandle:
         return ray.get(self.handle.remote(inp))
 
 
-def test_passing_handle(serve_instance):
+@pytest.mark.parametrize("use_build", [False, True])
+def test_passing_handle(serve_instance, use_build):
     child = Adder.bind(1)
     parent = TakeHandle.bind(child)
-    driver = Driver.bind(parent)
+    driver = maybe_build(Driver.bind(parent), use_build)
     handle = serve.run(driver)
     assert ray.get(handle.remote(1)) == 2
 
 
-def test_passing_handle_in_obj(serve_instance):
-    @serve.deployment
-    class Parent:
-        def __init__(self, d):
-            self._d = d
+@serve.deployment
+class DictParent:
+    def __init__(self, d):
+        self._d = d
 
-        async def __call__(self, key):
-            return await self._d[key].remote()
+    async def __call__(self, key):
+        return await self._d[key].remote()
+
+
+@pytest.mark.parametrize("use_build", [False, True])
+def test_passing_handle_in_obj(serve_instance, use_build):
 
     child1 = Echo.bind("ed")
     child2 = Echo.bind("simon")
-    parent = Parent.bind({"child1": child1, "child2": child2})
+    parent = maybe_build(
+        DictParent.bind({"child1": child1, "child2": child2}), use_build
+    )
 
     handle = serve.run(parent)
     assert ray.get(handle.remote("child1")) == "ed"
     assert ray.get(handle.remote("child2")) == "simon"
 
 
-def test_pass_handle_to_multiple(serve_instance):
-    @serve.deployment
-    class Child:
-        def __call__(self, *args):
-            return os.getpid()
+@serve.deployment
+class Child:
+    def __call__(self, *args):
+        return os.getpid()
 
-    @serve.deployment
-    class Parent:
-        def __init__(self, child):
-            self._child = child
 
-        def __call__(self, *args):
-            return ray.get(self._child.remote())
+@serve.deployment
+class Parent:
+    def __init__(self, child):
+        self._child = child
 
-    @serve.deployment
-    class GrandParent:
-        def __init__(self, child, parent):
-            self._child = child
-            self._parent = parent
+    def __call__(self, *args):
+        return ray.get(self._child.remote())
 
-        def __call__(self, *args):
-            # Check that the grandparent and parent are talking to the same child.
-            assert ray.get(self._child.remote()) == ray.get(self._parent.remote())
-            return "ok"
+
+@serve.deployment
+class GrandParent:
+    def __init__(self, child, parent):
+        self._child = child
+        self._parent = parent
+
+    def __call__(self, *args):
+        # Check that the grandparent and parent are talking to the same child.
+        assert ray.get(self._child.remote()) == ray.get(self._parent.remote())
+        return "ok"
+
+
+@pytest.mark.parametrize("use_build", [False, True])
+def test_pass_handle_to_multiple(serve_instance, use_build):
 
     child = Child.bind()
     parent = Parent.bind(child)
-    grandparent = GrandParent.bind(child, parent)
+    grandparent = maybe_build(GrandParent.bind(child, parent), use_build)
 
     handle = serve.run(grandparent)
     assert ray.get(handle.remote()) == "ok"
