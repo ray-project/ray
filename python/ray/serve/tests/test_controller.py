3f6db63269a212b56b9aa6ff455020148a7aa0cf
Shreyas Krishnaswamy <shrekris@anyscale.com>
2022-03-15 17:10:05 -0700

    Test _override_controller_namespace

--- python/ray/serve/tests/test_controller.py
+++ python/ray/serve/tests/test_controller.py
@@ -3,6 +3,7 @@ import time
 
 import ray
 from ray import serve
+from ray.serve.api import internal_get_global_client
 
 
 def test_redeploy_start_time(serve_instance):
@@ -29,6 +30,25 @@ def test_redeploy_start_time(serve_instance):
     start_time_ms_2 = deployment_info_2.start_time_ms
 
     assert start_time_ms_1 == start_time_ms_2
+
+
+@pytest.mark.parametrize("detached", [True, False])
+def test_override_namespace(detached):
+    """Test the _override_controller_namespace flag in serve.start()."""
+
+    if ray.is_initialized():
+        ray.shutdown()
+
+    ray_namespace = "ray_namespace"
+    controller_namespace = "controller_namespace"
+
+    ray.init(namespace=ray_namespace)
+    serve.start(detached=detached, _override_controller_namespace=controller_namespace)
+
+    controller_name = internal_get_global_client()._controller_name
+    ray.get_actor(controller_name, namespace=controller_namespace)
+
+    serve.shutdown()
 
 
 if __name__ == "__main__":
