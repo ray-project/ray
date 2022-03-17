762299e9db1faa6cd7de2e0f062de36263519113
Eric Liang <ekhliang@gmail.com>
2022-03-16 22:41:10 -0700

    update

--- doc/source/ray-core/doc_code/actor-pool.py
+++ doc/source/ray-core/doc_code/actor-pool.py
@@ -13,5 +13,5 @@ pool = ActorPool([a1, a2])
 
 # pool.map(..) returns a Python generator object ActorPool.map
 gen = pool.map(lambda a, v: a.double.remote(v), [1, 2, 3, 4])
-print(list(v for v in gen))
+print(list(gen))
 # [2, 4, 6, 8]
