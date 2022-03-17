7f131552df8ee0045aa7fde8c74aeabb434ffeb7
Siyuan Zhuang <suquark@gmail.com>
2022-03-16 18:13:34 -0700

    update test

--- python/ray/tune/tests/test_resource_updater.py
+++ python/ray/tune/tests/test_resource_updater.py
@@ -3,35 +3,37 @@ from ray.tests.conftest import *  # noqa
 from ray.tune.utils.resource_updater import ResourceUpdater
 
 
-def test_resource_updater(shutdown_only):
+def test_resource_updater(ray_start_cluster):
+    cluster = ray_start_cluster
+
     resource_updater = ResourceUpdater(refresh_period=100)
     # Before intialization, all resources are 0.
     assert resource_updater.get_num_cpus() == 0
     assert resource_updater.get_num_gpus() == 0
-    ray.shutdown()
+
+    cluster.add_node(num_cpus=1, num_gpus=2)
+    cluster.wait_for_nodes()
+    ray.init(address=cluster.address)
 
     # Resource updater will update resource immediately
     # after ray is initialized for the first time.
-    ray.init(num_cpus=1, num_gpus=2)
     assert resource_updater.get_num_cpus() == 1
     assert resource_updater.get_num_gpus() == 2
-    ray.shutdown()
 
     # It will not update the resource before "refresh_period".
-    ray.init(num_cpus=2, num_gpus=3)
+    cluster.add_node(num_cpus=1, num_gpus=1)
+    cluster.wait_for_nodes()
     assert resource_updater.get_num_cpus() == 1
     assert resource_updater.get_num_gpus() == 2
-    ray.shutdown()
 
-    ray.init(num_cpus=1, num_gpus=2)
     resource_updater = ResourceUpdater(refresh_period=0)
-    assert resource_updater.get_num_cpus() == 1
-    assert resource_updater.get_num_gpus() == 2
-    ray.shutdown()
-
-    ray.init(num_cpus=2, num_gpus=3)
     assert resource_updater.get_num_cpus() == 2
     assert resource_updater.get_num_gpus() == 3
+
+    cluster.add_node(num_cpus=1, num_gpus=1)
+    cluster.wait_for_nodes()
+    assert resource_updater.get_num_cpus() == 3
+    assert resource_updater.get_num_gpus() == 4
 
 
 if __name__ == "__main__":
