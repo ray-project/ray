0a0e6360d401b8dc24949803273c56d66b614cf5
Chong-Li <lc300133@antgroup.com>
2022-03-17 20:32:41 +0800

    Fix RemoveNegative

--- src/ray/gcs/gcs_server/gcs_resource_scheduler.cc
+++ src/ray/gcs/gcs_server/gcs_resource_scheduler.cc
@@ -32,7 +32,7 @@ double LeastResourceScorer::Score(const ResourceRequest &required_resources,
   if (!node_resources.normal_task_resources.IsEmpty()) {
     new_node_resources = node_resources;
     new_node_resources.available -= node_resources.normal_task_resources;
-    new_node_resources.RemoveNegative();
+    new_node_resources.available.RemoveNegative();
     node_resources_ptr = &new_node_resources;
   }
 
