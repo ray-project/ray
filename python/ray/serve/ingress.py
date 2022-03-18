0c824fd470fdbfc4eafc46605d4f287ac65fb4a2
simon-mo <simon.mo@hey.com>
2022-03-17 17:00:31 -0700

    [Serve] Implement Default DAGDriver

--- python/ray/serve/ingress.py
+++ python/ray/serve/ingress.py
@@ -1,21 +1,76 @@
-from typing import Callable, Optional, Union
+import inspect
+from abc import abstractmethod
+from typing import Any, Callable, Optional, Union
 
 import starlette
+from fastapi import Depends, FastAPI
 
+from ray._private.utils import import_attr
+from ray.ml.predictor import DataBatchType
+from ray.serve.api import RayServeDAGHandle
+from ray.serve.http_util import ASGIHTTPSender
 from ray import serve
-from ray.serve.api import DAGHandle
+
+DEFAULT_INPUT_SCHEMA = "ray.serve.http_adapters.array_to_databatch"
+InputSchemaFn = Callable[[Any], DataBatchType]
 
 
-@serve.deployment
-class DAGDriver:
-    def __init__(
-        self,
-        dag_handle: DAGHandle,
-        *,
-        input_schema: Optional[Union[str, Callable]] = None,
-    ):
+def _load_input_schema(
+    input_schema: Optional[Union[str, InputSchemaFn]]
+) -> InputSchemaFn:
+    if input_schema is None:
+        input_schema = DEFAULT_INPUT_SCHEMA
+
+    if isinstance(input_schema, str):
+        input_schema = import_attr(input_schema)
+
+    assert inspect.isfunction(input_schema), "input schema must be a callable function."
+    return input_schema
+
+
+class SimpleSchemaIngress:
+    def __init__(self, input_schema: Optional[Union[str, InputSchemaFn]] = None):
+        """Create a FastAPI endpoint annotated with input_schema dependency.
+
+        Args:
+            input_schema(str, InputSchemaFn, None): The FastAPI input conversion
+              function. By default, Serve will use the `NdArray` schema and convert to
+              numpy array. You can pass in any FastAPI dependency resolver that returns
+              an array. When you pass in a string, Serve will import it.
+              Please refer to Serve HTTP adatper documentation to learn more.
+        """
+        input_schema = _load_input_schema(input_schema)
+        self.app = FastAPI()
+
+        @self.app.get("/")
+        @self.app.post("/")
+        async def handle_request(inp=Depends(input_schema)):
+            resp = await self.predict(inp)
+            return resp
+
+    @abstractmethod
+    async def predict(self, inp):
         raise NotImplementedError()
 
     async def __call__(self, request: starlette.requests.Request):
-        """Parse input schema and pass the result to the DAG handle."""
-        raise NotImplementedError()
+        # NOTE(simon): This is now duplicated from ASGIAppWrapper because we need to
+        # generate FastAPI on the fly, we should find a way to unify the two.
+        sender = ASGIHTTPSender()
+        await self.app(request.scope, receive=request.receive, send=sender)
+        return sender.build_asgi_response()
+
+
+@serve.deployment(route_prefix="/")
+class DAGDriver(SimpleSchemaIngress):
+    def __init__(
+        self,
+        dag_handle: RayServeDAGHandle,
+        *,
+        input_schema: Optional[Union[str, Callable]] = None,
+    ):
+        self.dag_handle = dag_handle
+        super().__init__(input_schema)
+
+    async def predict(self, inp):
+        """Performing inference directly without HTTP."""
+        return await self.dag_handle.remote(inp)
---
+++ python/ray/serve/tests/test_pipeline_ingress.py
@@ -0,0 +1,82 @@
+import sys
+
+import pytest
+import numpy as np
+import requests
+from starlette.testclient import TestClient
+
+from ray.serve.ingress import DAGDriver, SimpleSchemaIngress
+from ray.experimental.dag.input_node import InputNode
+from ray import serve
+import ray
+
+
+def test_unit_schema_injection():
+    class Impl(SimpleSchemaIngress):
+        async def predict(self, inp):
+            return inp
+
+    async def resolver(my_custom_param: int):
+        return my_custom_param
+
+    server = Impl(input_schema=resolver)
+    client = TestClient(server.app)
+
+    response = client.post("/")
+    assert response.status_code == 422
+
+    response = client.post("/?my_custom_param=1")
+    assert response.status_code == 200
+    assert response.text == "1"
+
+    response = client.get("/openapi.json")
+    assert response.status_code == 200
+    assert response.json()["paths"]["/"]["get"]["parameters"][0] == {
+        "required": True,
+        "schema": {"title": "My Custom Param", "type": "integer"},
+        "name": "my_custom_param",
+        "in": "query",
+    }
+
+
+@serve.deployment
+def echo(inp):
+    # FastAPI can't handle this.
+    if isinstance(inp, np.ndarray):
+        return inp.tolist()
+    return inp
+
+
+def test_dag_driver_default(serve_instance):
+    with InputNode() as inp:
+        dag = echo.bind(inp)
+
+    handle = serve.run(DAGDriver.bind(dag))
+    assert ray.get(handle.predict.remote(42)) == 42
+
+    resp = requests.post("http://127.0.0.1:8000/", json={"array": [1]})
+    print(resp.text)
+
+    resp.raise_for_status()
+    assert resp.json() == [1.0]
+
+
+async def resolver(my_custom_param: int):
+    return my_custom_param
+
+
+def test_dag_driver_custom_schema(serve_instance):
+    with InputNode() as inp:
+        dag = echo.bind(inp)
+
+    handle = serve.run(DAGDriver.bind(dag, input_schema=resolver))
+    assert ray.get(handle.predict.remote(42)) == 42
+
+    resp = requests.get("http://127.0.0.1:8000/?my_custom_param=100")
+    print(resp.text)
+    resp.raise_for_status()
+    assert resp.json() == 100
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main(["-v", "-s", __file__]))
