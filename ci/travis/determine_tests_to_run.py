7859810cb67fd9ba1d1633a01b76e1e2678f6670
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2022-03-22 15:26:05 -0700

    Bazel attempt #1

--- ci/travis/determine_tests_to_run.py
+++ ci/travis/determine_tests_to_run.py
@@ -207,6 +207,9 @@ if __name__ == "__main__":
             elif changed_file.endswith("build-docker-images.py"):
                 RAY_CI_DOCKER_AFFECTED = 1
                 RAY_CI_LINUX_WHEELS_AFFECTED = 1
+            elif changed_file.startswith("python/ray/tests/kuberay"):
+                RAY_CI_DOCKER_AFFECTED = 1
+                RAY_CI_LINUX_WHEELS_AFFECTED = 1
             elif changed_file.startswith("src/"):
                 RAY_CI_ML_AFFECTED = 1
                 RAY_CI_TUNE_AFFECTED = 1
--- python/ray/tests/kuberay/test_autoscaling_e2e.py
+++ python/ray/tests/kuberay/test_autoscaling_e2e.py
@@ -229,4 +229,4 @@ if __name__ == "__main__":
     import pytest
     import sys
 
-    sys.exit(pytest.main(["-v", __file__]))
+    sys.exit(pytest.main(["-vv", __file__]))
