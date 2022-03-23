df7acc99d8a5af19d3774364c5a5bd21f16a22d9
Kai Yang <kfstorm@outlook.com>
2022-03-23 17:58:37 +0800

    Add C++ UT

--- src/ray/core_worker/test/direct_actor_transport_test.cc
+++ src/ray/core_worker/test/direct_actor_transport_test.cc
@@ -634,6 +634,38 @@ TEST_P(DirectActorSubmitterTest, TestActorRestartFailInflightTasks) {
   ASSERT_TRUE(worker_client_->ReplyPushTask(Status::IOError("")));
 }
 
+TEST_P(DirectActorSubmitterTest, TestActorRestartFastFail) {
+  auto execute_out_of_order = GetParam();
+  rpc::Address addr;
+  auto worker_id = WorkerID::FromRandom();
+  addr.set_worker_id(worker_id.Binary());
+  ActorID actor_id = ActorID::Of(JobID::FromInt(0), TaskID::Nil(), 0);
+  submitter_.AddActorQueueIfNotExists(
+      actor_id, -1, execute_out_of_order, /*enable_task_fast_fail=*/true);
+  addr.set_port(0);
+  submitter_.ConnectActor(actor_id, addr, 0);
+  ASSERT_EQ(worker_client_->callbacks.size(), 0);
+  ASSERT_EQ(num_clients_connected_, 1);
+
+  auto task1 = CreateActorTaskHelper(actor_id, worker_id, 0);
+  // Submit a task.
+  ASSERT_TRUE(CheckSubmitTask(task1));
+  EXPECT_CALL(*task_finisher_, CompletePendingTask(task1.TaskId(), _, _)).Times(1);
+  ASSERT_TRUE(worker_client_->ReplyPushTask(Status::OK()));
+
+  // Actor failed and is now restarting.
+  const auto death_cause = CreateMockDeathCause();
+  submitter_.DisconnectActor(actor_id, 1, /*dead=*/false, death_cause);
+
+  // Submit a new task. This task should fail immediately because "fast fail" is enabled.
+  auto task2 = CreateActorTaskHelper(actor_id, worker_id, 1);
+  ASSERT_TRUE(CheckSubmitTask(task2));
+  EXPECT_CALL(*task_finisher_, CompletePendingTask(task2.TaskId(), _, _)).Times(0);
+  EXPECT_CALL(*task_finisher_, FailOrRetryPendingTask(task2.TaskId(), _, _, _, _))
+      .Times(1);
+  ASSERT_EQ(io_context.poll_one(), 1);
+}
+
 TEST_P(DirectActorSubmitterTest, TestPendingTasks) {
   auto execute_out_of_order = GetParam();
   int32_t max_pending_calls = 10;
