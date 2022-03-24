19a0c8c9d1038cb6c22e93ff95095e4ba76424c1
jonch <9093549+jon-chuang@users.noreply.github.com>
2022-03-23 21:19:02 -0400

    fmt

--- src/ray/gcs/gcs_server/test/gcs_resource_scheduler_test.cc
+++ src/ray/gcs/gcs_server/test/gcs_resource_scheduler_test.cc
@@ -143,7 +143,7 @@ class GcsResourceSchedulerTest : public ::testing::Test {
     resources_list.emplace_back(std::vector(
         {std::make_pair(cpu_resource, 1.0), std::make_pair(custom_resource, 1.0)}));
     resources_list.emplace_back(std::vector(
-      {std::make_pair(cpu_resource, 1.0), std::make_pair(custom_resource, 2.0)}));
+        {std::make_pair(cpu_resource, 1.0), std::make_pair(custom_resource, 2.0)}));
 
     std::vector<NodeID> node_ids;
     for (auto r : resources_list) {
