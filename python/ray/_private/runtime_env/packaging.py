3f663be57f8e6361fe414517b4aeabfdd5501296
simon-mo <simon.mo@hey.com>
2022-03-24 17:08:21 -0700

    lint for yapf

--- python/ray/_private/runtime_env/packaging.py
+++ python/ray/_private/runtime_env/packaging.py
@@ -551,8 +551,8 @@ def remove_dir_from_filepaths(base_dir: str, rdir: str):
         rdir_children = os.listdir(os.path.join(tmp_dir, rdir))
         for child in rdir_children:
             shutil.move(
-                os.path.join(tmp_dir, rdir, child), os.path.join(base_dir, child)
-            )
+                os.path.join(tmp_dir, rdir, child),
+                os.path.join(base_dir, child))
 
 
 def unzip_package(package_path: str,
