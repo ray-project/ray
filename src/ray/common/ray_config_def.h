06e5d1aec27b64dd818f30a4543dfe5922932076
Yi Cheng <chengyidna@gmail.com>
2022-03-27 22:20:54 +0000

    remove redis pubsub

--- src/ray/common/ray_config_def.h
+++ src/ray/common/ray_config_def.h
@@ -289,8 +289,6 @@ RAY_CONFIG(uint32_t, maximum_gcs_dead_node_cached_count, 1000)
 RAY_CONFIG(int, gcs_resource_report_poll_period_ms, 100)
 // The number of concurrent polls to polls to GCS.
 RAY_CONFIG(uint64_t, gcs_max_concurrent_resource_pulls, 100)
-// Feature flag to enable grpc based pubsub in GCS.
-RAY_CONFIG(bool, gcs_grpc_based_pubsub, true)
 // The storage backend to use for the GCS. It can be either 'redis' or 'memory'.
 RAY_CONFIG(std::string, gcs_storage, "memory")
 
