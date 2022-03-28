de48eada58db015e41e5b3a458c4753e1b550732
Xiaowei Jiang <xwjiang2010@gmail.com>
2022-03-27 19:55:57 -0700

    Add state_dict test.

--- python/ray/ml/tests/test_horovod_trainer.py
+++ python/ray/ml/tests/test_horovod_trainer.py
@@ -6,7 +6,10 @@ from torchvision import datasets
 from torchvision.transforms import transforms
 
 import ray
-from ray.ml.examples.horovod.horovod_pytorch_example import train_func as hvd_train_func
+from ray.ml.examples.horovod.horovod_pytorch_example import (
+    train_func as hvd_train_func,
+    Net,
+)
 from ray.ml.predictors.integrations.torch import TorchPredictor
 from ray.ml.train.integrations.horovod import HorovodTrainer
 
@@ -25,14 +28,13 @@ def run_image_prediction(model: torch.nn.Module, images: torch.Tensor) -> torch.
         return torch.exp(model(images)).argmax(dim=1)
 
 
-@pytest.mark.parametrize("num_workers", [1, 2])
-def test_horovod(ray_start_4_cpus, num_workers):
+def test_horovod(ray_start_4_cpus):
     def train_func(config):
         result = hvd_train_func(config)
         assert len(result) == epochs
         assert result[-1] < result[0]
 
-    num_workers = num_workers
+    num_workers = 1
     epochs = 10
     scaling_config = {"num_workers": num_workers}
     config = {"num_epochs": epochs, "save_model_as_dict": False}
@@ -43,6 +45,43 @@ def test_horovod(ray_start_4_cpus, num_workers):
     )
     result = trainer.fit()
     predictor = TorchPredictor.from_checkpoint(result.checkpoint)
+
+    # Find some test data to run on.
+    test_set = datasets.MNIST(
+        "./data",
+        train=False,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        ),
+    )
+
+    test_dataloader = DataLoader(test_set, batch_size=10)
+    test_dataloader_iter = iter(test_dataloader)
+    images, labels = next(
+        test_dataloader_iter
+    )  # only running a batch inference of 10 images
+    predicted_labels = run_image_prediction(predictor.model, images)
+    assert torch.equal(predicted_labels, labels)
+
+
+def test_horovod_state_dict(ray_start_4_cpus):
+    def train_func(config):
+        result = hvd_train_func(config)
+        assert len(result) == epochs
+        assert result[-1] < result[0]
+
+    num_workers = 2
+    epochs = 10
+    scaling_config = {"num_workers": num_workers}
+    config = {"num_epochs": epochs, "save_model_as_dict": True}
+    trainer = HorovodTrainer(
+        train_loop_per_worker=train_func,
+        train_loop_config=config,
+        scaling_config=scaling_config,
+    )
+    result = trainer.fit()
+    predictor = TorchPredictor.from_checkpoint(result.checkpoint, model=Net())
 
     # Find some test data to run on.
     test_set = datasets.MNIST(
