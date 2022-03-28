ef6c28bc231dbd44a6513acdd6d2912f97e7540e
Yi Cheng <chengyidna@gmail.com>
2022-03-28 18:45:35 +0000

    fix

--- src/ray/common/ray_syncer/ray_syncer.h
+++ src/ray/common/ray_syncer/ray_syncer.h
@@ -177,7 +177,7 @@ class RaySyncer {
   std::shared_ptr<bool> stopped_;
 
   /// Test purpose
-  friend class SyncerServerTest;
+  friend struct SyncerServerTest;
   FRIEND_TEST(SyncerTest, Broadcast);
   FRIEND_TEST(SyncerTest, Test1To1);
   FRIEND_TEST(SyncerTest, Test1ToN);
