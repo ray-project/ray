55d03cc60cb68a17bd4c6983120e2860fd27afa0
Amog Kamsetty <amogkamsetty@yahoo.com>
2022-04-01 13:33:20 -0700

    formatting

--- python/ray/util/ml_utils/mlflow.py
+++ python/ray/util/ml_utils/mlflow.py
@@ -211,7 +211,9 @@ class MLflowLoggerUtil:
         if active_run:
             return active_run
 
-        return self._mlflow.start_run(run_name=run_name, tags=tags)
+        return self._mlflow.start_run(
+            run_name=run_name, experiment_id=self.experiment_id, tags=tags
+        )
 
     def _run_exists(self, run_id: str) -> bool:
         """Check if run with the provided id exists."""
