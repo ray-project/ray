a81ec5e2339659277f08ee2d877b6a2ea03a1fb0
Antoni Baum <antoni.baum@protonmail.com>
2022-04-04 15:30:03 +0000

    Add missing file

--- python/ray/ml/constants.py
+++ python/ray/ml/constants.py
@@ -7,3 +7,8 @@ MODEL_KEY = "model"
 # Key to denote which dataset is the training dataset.
 # This is the dataset that the preprocessor is fit on.
 TRAIN_DATASET_KEY = "train"
+
+# Key to denote which dataset is the validation dataset.
+# Only used in trainers which do not support multiple
+# validation datasets.
+VALIDATION_DATASET_KEY = "validation"
