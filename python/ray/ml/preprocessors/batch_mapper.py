dcb3c4d4472420ca58e7cc0ecfeacf0fad32ba63
Xiaowei Jiang <xwjiang2010@gmail.com>
2022-04-05 09:35:03 -0700

    Add doc string

--- python/ray/ml/preprocessors/batch_mapper.py
+++ python/ray/ml/preprocessors/batch_mapper.py
@@ -8,6 +8,16 @@ if TYPE_CHECKING:
 
 
 class BatchMapper(Preprocessor):
+    """Apply ``fn`` to batches of records of given dataset.
+
+    This is meant to be generic and supports low level operation on records.
+    One could easily leverage this preprocessor to achieve operations like
+    adding a new column or modifying a column in place.
+
+    Args:
+        fn: The udf function for batch operation.
+    """
+
     _is_fittable = False
 
     def __init__(self, fn: Callable[["pandas.DataFrame"], "pandas.DataFrame"]):
