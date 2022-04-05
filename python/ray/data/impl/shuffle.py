bc64e5b9d957b674cf46a029e7db0a228cd51131
Stephanie Wang <swang@cs.berkeley.edu>
2022-04-04 20:21:03 -0700

    doc, fix random_shuffle

--- python/ray/data/impl/shuffle.py
+++ python/ray/data/impl/shuffle.py
@@ -1,8 +1,6 @@
 import math
 from typing import TypeVar, List, Optional, Dict, Any, Tuple, Union, Callable, Iterable
 
-import numpy as np
-
 import ray
 from ray.data.block import Block, BlockAccessor, BlockMetadata, BlockExecStats
 from ray.data.impl.progress_bar import ProgressBar
@@ -14,6 +12,12 @@ T = TypeVar("T")
 
 
 class ShuffleOp:
+    """
+    A generic shuffle operator. Callers should implement the `map` and `reduce`
+    static methods. Any custom arguments for map and reduce tasks should be
+    specified by setting `ShuffleOp._map_args` and `ShuffleOp._reduce_args`.
+    """
+
     def __init__(self, map_args: List[Any] = None, reduce_args: List[Any] = None):
         self._map_args = map_args or []
         self._reduce_args = reduce_args or []
@@ -24,10 +28,18 @@ class ShuffleOp:
     def map(
         idx: int, block: Block, output_num_blocks: int, *map_args: List[Any]
     ) -> List[Union[BlockMetadata, Block]]:
+        """
+        Map function to be run on each input block.
+
+        Returns list of [BlockMetadata, O1, O2, O3, ...output_num_blocks].
+        """
         raise NotImplementedError
 
     @staticmethod
     def reduce(*mapper_outputs: List[Block]) -> (Block, BlockMetadata):
+        """
+        Reduce function to be run for each output block.
+        """
         raise NotImplementedError
 
     def execute(
@@ -102,13 +114,20 @@ class ShuffleOp:
 
 
 class ShufflePartitionOp(ShuffleOp):
+    """
+    Operator used for `random_shuffle` and `repartition` transforms.
+    """
+
     def __init__(
         self,
         block_udf=None,
         random_shuffle: bool = False,
         random_seed: Optional[int] = None,
     ):
-        super().__init__(map_args=[block_udf, random_shuffle, random_seed])
+        super().__init__(
+            map_args=[block_udf, random_shuffle, random_seed],
+            reduce_args=[random_shuffle, random_seed],
+        )
 
     @staticmethod
     def map(
@@ -119,7 +138,6 @@ class ShufflePartitionOp(ShuffleOp):
         random_shuffle: bool,
         random_seed: Optional[int],
     ) -> List[Union[BlockMetadata, Block]]:
-        """Returns list of [BlockMetadata, O1, O2, O3, ...output_num_blocks]."""
         stats = BlockExecStats.builder()
         if block_udf:
             # TODO(ekl) note that this effectively disables block splitting.
@@ -144,25 +162,26 @@ class ShufflePartitionOp(ShuffleOp):
         for i in range(output_num_blocks):
             slices.append(block.slice(i * slice_sz, (i + 1) * slice_sz, copy=True))
 
-        # Randomize the distribution order of the blocks (this matters when
-        # some blocks are larger than others).
-        if random_shuffle:
-            random = np.random.RandomState(seed_i)
-            random.shuffle(slices)
-
         num_rows = sum(BlockAccessor.for_block(s).num_rows() for s in slices)
         assert num_rows == block.num_rows(), (num_rows, block.num_rows())
         metadata = block.get_metadata(input_files=None, exec_stats=stats.build())
         return [metadata] + slices
 
     @staticmethod
-    def reduce(*mapper_outputs: List[Block]) -> (Block, BlockMetadata):
+    def reduce(
+        random_shuffle: bool, random_seed: Optional[int], *mapper_outputs: List[Block]
+    ) -> (Block, BlockMetadata):
         stats = BlockExecStats.builder()
         builder = DelegatingBlockBuilder()
         for block in mapper_outputs:
             builder.add_block(block)
         new_block = builder.build()
         accessor = BlockAccessor.for_block(new_block)
+        if random_shuffle:
+            new_block = accessor.random_shuffle(
+                random_seed if random_seed is not None else None
+            )
+            accessor = BlockAccessor.for_block(new_block)
         new_metadata = BlockMetadata(
             num_rows=accessor.num_rows(),
             size_bytes=accessor.size_bytes(),
