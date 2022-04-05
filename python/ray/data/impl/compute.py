76d2a0b1be9b7f2b2eaba48bc5e3842a65527daf
Clark Zinzow <clarkzinzow@gmail.com>
2022-04-05 01:03:02 +0000

    Disallow callable classes for task compute strategy.

--- python/ray/data/impl/compute.py
+++ python/ray/data/impl/compute.py
@@ -234,9 +234,12 @@ def cache_wrapper(
     """
     if isinstance(fn, CallableClass):
 
-        if compute is None:
+        if compute is None or (
+            compute != "actors" and not isinstance(compute, ActorPoolStrategy)
+        ):
             raise ValueError(
-                "``compute`` must be specified when using a callable class. "
+                "``compute`` must be specified when using a callable class, and must "
+                "specify the actor compute strategy. "
                 'For example, use ``compute="actors"`` or '
                 "``compute=ActorPoolStrategy(min, max)``."
             )
