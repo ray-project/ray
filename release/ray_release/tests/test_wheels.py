97bda16772921ec730f24eedef4257074152f381
Kai Fricke <kai@anyscale.com>
2022-04-05 16:39:58 -0700

    Trigger

--- release/ray_release/tests/test_wheels.py
+++ release/ray_release/tests/test_wheels.py
@@ -19,7 +19,7 @@ from ray_release.wheels import (
 
 class WheelsFinderTest(unittest.TestCase):
     def setUp(self) -> None:
-        # Test again
+        # Test
         pass
 
     def testGetRayVersion(self):
