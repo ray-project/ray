2439a214e1ef147c09577f674c6ef931ac623c39
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2022-04-06 15:47:13 -0700

    Lint
    Co-authored-by: Avnish Narayan <avnishn@users.noreply.github.com>

--- raylibraries_example.py
+++ raylibraries_example.py
@@ -2,4 +2,7 @@ import ray
 from ray import rllib
 
 
-ray.init(address="auto", runtime_env={"ray_libraries" : [rllib]})
+ray.init(address="auto", runtime_env={"ray_libraries": [rllib]})
+
+
+# rllib.__some_attribute__ = new_thing
