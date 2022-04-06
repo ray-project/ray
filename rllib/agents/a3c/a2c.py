02f358ec3b6ed3e06df09bac37c545b6d7371ec2
sven1977 <svenmika1977@gmail.com>
2022-04-06 09:05:24 +0200

    wip

--- rllib/agents/a3c/a2c.py
+++ rllib/agents/a3c/a2c.py
@@ -1,10 +1,18 @@
 import math
 
-from ray.util.iter import LocalIterator
 from ray.rllib.agents.a3c.a3c import DEFAULT_CONFIG as A3C_CONFIG, A3CTrainer
 from ray.rllib.agents.trainer import Trainer
+from ray.rllib.evaluation.worker_set import WorkerSet
+from ray.rllib.execution.common import (
+    STEPS_TRAINED_COUNTER,
+    STEPS_TRAINED_THIS_ITER_COUNTER,
+)
 from ray.rllib.execution.metric_ops import StandardMetricsReporting
-from ray.rllib.execution.rollout_ops import ParallelRollouts, ConcatBatches
+from ray.rllib.execution.rollout_ops import (
+    ParallelRollouts,
+    ConcatBatches,
+    synchronous_parallel_sample,
+)
 from ray.rllib.execution.train_ops import (
     ComputeGradients,
     AverageGradients,
@@ -12,10 +20,23 @@ from ray.rllib.execution.train_ops import (
     MultiGPUTrainOneStep,
     TrainOneStep,
 )
+from ray.rllib.policy.sample_batch import DEFAULT_POLICY_ID, SampleBatch
 from ray.rllib.utils import merge_dicts
 from ray.rllib.utils.annotations import override
-from ray.rllib.utils.typing import TrainerConfigDict
-from ray.rllib.evaluation.worker_set import WorkerSet
+from ray.rllib.utils.metrics import (
+    APPLY_GRADS_TIMER,
+    COMPUTE_GRADS_TIMER,
+    NUM_AGENT_STEPS_SAMPLED,
+    NUM_AGENT_STEPS_TRAINED,
+    NUM_ENV_STEPS_SAMPLED,
+    WORKER_UPDATE_TIMER,
+)
+from ray.rllib.utils.typing import (
+    PartialTrainerConfigDict,
+    ResultDict,
+    TrainerConfigDict,
+)
+from ray.util.iter import LocalIterator
 
 A2C_DEFAULT_CONFIG = merge_dicts(
     A3C_CONFIG,
@@ -28,8 +49,6 @@ A2C_DEFAULT_CONFIG = merge_dicts(
         # training with batch sizes much larger than can fit in GPU memory.
         # To enable, set this to a value less than the train batch size.
         "microbatch_size": None,
-        # Use `execution_plan` for A2C (no `training_iteration` implementation yet).
-        "_disable_execution_plan_api": False,
     },
 )
 
@@ -39,6 +58,94 @@ class A2CTrainer(A3CTrainer):
     @override(A3CTrainer)
     def get_default_config(cls) -> TrainerConfigDict:
         return A2C_DEFAULT_CONFIG
+
+    @override(Trainer)
+    def setup(self, config: PartialTrainerConfigDict):
+        super().setup(config)
+
+        if self.config["_disable_execution_plan_api"] is True:
+            self._microbatches = []
+
+    @override(Trainer)
+    def training_iteration(self) -> ResultDict:
+        # W/o microbatching: Identical to super's (Trainer) default implementation.
+        # Only difference to a default Trainer being the value function loss term
+        # and its value computations alongside each action.
+        if self.config["microbatch_size"] is None:
+            return super().training_iteration()
+
+        # In microbatch mode, we want to compute gradients on experience
+        # microbatches, average a number of these microbatches, and then
+        # apply the averaged gradient in one SGD step. This conserves GPU
+        # memory, allowing for extremely large experience batches to be
+        # used.
+        sample_batches = []
+        num_env_steps = 0
+        num_agent_steps = 0
+        while (
+            not self._by_agent_steps and num_env_steps < self.config["microbatch_size"]
+        ) or (
+            self._by_agent_steps and num_agent_steps < self.config["microbatch_size"]
+        ):
+            new_sample_batches = synchronous_parallel_sample(self.workers)
+            sample_batches.extend(new_sample_batches)
+            num_env_steps += sum(len(s) for s in new_sample_batches)
+            num_agent_steps += sum(
+                len(s) if isinstance(s, SampleBatch) else s.agent_steps()
+                for s in new_sample_batches
+            )
+        self._counters[NUM_ENV_STEPS_SAMPLED] += num_env_steps
+        self._counters[NUM_AGENT_STEPS_SAMPLED] += num_agent_steps
+
+        # Combine all batches at once
+        train_batch = SampleBatch.concat_samples(sample_batches)
+
+        with self._timers[COMPUTE_GRADS_TIMER]:
+            grad, info = self.workers.local_worker().compute_gradients(
+                train_batch, single_agent=True
+            )
+            self._microbatches.append((grad, train_batch.count))
+
+        # If `train_batch_size` reached: Accumulate gradients and apply.
+        num_microbatches = math.ceil(
+            self.config["train_batch_size"] / self.config["microbatch_size"]
+        )
+        if len(self._microbatches) > num_microbatches:
+            # Accumulate gradients.
+            acc_gradients = None
+            sum_count = 0
+            for grad, count in self._microbatches:
+                if acc_gradients is None:
+                    acc_gradients = grad
+                else:
+                    acc_gradients = [a + b for a, b in zip(acc_gradients, grad)]
+                sum_count += count
+
+            self._microbatches = []
+
+            # Apply gradients.
+            self._counters[STEPS_TRAINED_COUNTER] += sum_count
+            self._counters[STEPS_TRAINED_THIS_ITER_COUNTER] = sum_count
+
+            apply_timer = self._timers[APPLY_GRADS_TIMER]
+            with apply_timer:
+                self.workers.local_worker().apply_gradients(acc_gradients)
+                apply_timer.push_units_processed(sum_count)
+
+            # Also update global vars of the local worker.
+            # Create current global vars.
+            global_vars = {
+                "timestep": self._counters[NUM_AGENT_STEPS_SAMPLED],
+            }
+            with self._timers[WORKER_UPDATE_TIMER]:
+                self.workers.sync_weights(
+                    policies=self.workers.local_worker().get_policies_to_train(),
+                    global_vars=global_vars,
+                )
+
+        train_results = {DEFAULT_POLICY_ID: info}
+
+        return train_results
 
     @staticmethod
     @override(Trainer)
