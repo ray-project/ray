e61fdb0317e87a6df2af5f03a603e4b855eae920
sven1977 <svenmika1977@gmail.com>
2022-04-06 14:28:25 +0200

    fix

--- rllib/models/tf/misc.py
+++ rllib/models/tf/misc.py
@@ -9,7 +9,7 @@ tf1, tf, tfv = try_import_tf()
 
 def normc_initializer(std: float = 1.0) -> Any:
     def _initializer(shape, dtype=None, partition_info=None):
-        out = np.random.randn(*shape).astype(np.float32)
+        out = np.random.randn(*shape).astype(dtype)
         out *= std / np.sqrt(np.square(out).sum(axis=0, keepdims=True))
         return tf.constant(out)
 
