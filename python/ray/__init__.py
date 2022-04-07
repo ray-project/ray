caf7480ffd3b8aa9e1d792494382fcb3c91b68c8
Yi Cheng <chengyidna@gmail.com>
2022-04-07 17:52:15 +0000

    update

--- python/ray/__init__.py
+++ python/ray/__init__.py
@@ -218,27 +218,5 @@ __all__ += [
 ]
 
 
-# Remove modules from top-level ray
-def _ray_user_setup_function():
-    import os
-
-    user_setup_fn = os.environ.get("RAY_USER_SETUP_FUNCTION")
-    if user_setup_fn is not None:
-        try:
-            module_name, fn_name = user_setup_fn.rsplit(".", 1)
-            m = __import__(module_name, globals(), locals(), [fn_name])
-            getattr(m, fn_name)()
-        except Exception as e:
-            # We still need to allow ray to be imported, even there is
-            # something in the setup function.
-            logger.warning(
-                f"Failed to run user setup function: {user_setup_fn}. "
-                f"Error message {e}"
-            )
-
-
-_ray_user_setup_function()
-
 del os
 del logging
-del _ray_user_setup_function
