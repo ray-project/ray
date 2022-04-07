792f1c234e818af999d95593b1a89b6fdb5b8657
shrekris-anyscale <92341594+shrekris-anyscale@users.noreply.github.com>
2022-04-07 08:34:00 -0700

    [serve] Include full traceback in deployment update error message (#23752)
    When deployments fail to update, [Serve sets their status to UNHEALTHY and logs the error message](https://github.com/ray-project/ray/blob/46465abd6d866c3903b17c601e84e81b46c67190/python/ray/serve/deployment_state.py#L1507-L1511). However, the message lacks a traceback, making it impossible to find what caused it. [For example](https://console.anyscale.com/o/anyscale-internal/projects/prj_2xR6uT6t7jJuu1aCwWMsle/clusters/ses_SfGPJq8WWJUhAvmHHsDgJWUe?command-history-section=command_history):
    
    ```
    File "/home/ray/anaconda3/lib/python3.7/site-packages/ray/serve/api.py", line 328, in _wait_for_deployment_healthy
        raise RuntimeError(f"Deployment {name} is UNHEALTHY: {status.message}")
    RuntimeError: Deployment echo is UNHEALTHY: Failed to update deployment:
    '>' not supported between instances of 'NoneType' and 'int'.
    ```
    
    It's not clear where `'>' not supported between instances of 'NoneType' and 'int'.` is being triggered.
    
    The change includes the full traceback for this type of update failure. The new status message is easier to debug:
    
    ```
    File "/Users/shrekris/Desktop/ray/python/ray/serve/api.py", line 328, in _wait_for_deployment_healthy
        raise RuntimeError(f"Deployment {name} is UNHEALTHY: {status.message}")
    RuntimeError: Deployment A is UNHEALTHY: Failed to update deployment:
    Traceback (most recent call last):
      File "/Users/shrekris/Desktop/ray/python/ray/serve/deployment_state.py", line 1503, in update
        running_replicas_changed |= self._check_and_update_replicas()
      File "/Users/shrekris/Desktop/ray/python/ray/serve/deployment_state.py", line 1396, in _check_and_update_replicas
        a = 1/0
    ZeroDivisionError: division by zero
    ```
    
    (I forced a divide-by-zero error to get this traceback).

--- python/ray/serve/deployment_state.py
+++ python/ray/serve/deployment_state.py
@@ -8,6 +8,7 @@ import os
 import pickle
 import random
 import time
+import traceback
 from typing import Any, Callable, Dict, List, Optional, Tuple
 
 import ray
@@ -1376,6 +1377,7 @@ class DeploymentState:
 
         Returns if any running replicas transitioned to another state.
         """
+
         running_replicas_changed = False
         for replica in self._replicas.pop(states=[ReplicaState.RUNNING]):
             if replica.check_health():
@@ -1484,10 +1486,10 @@ class DeploymentState:
                 self._notify_running_replicas_changed()
 
             deleted = self._check_curr_status()
-        except Exception as e:
+        except Exception:
             self._curr_status_info = DeploymentStatusInfo(
                 status=DeploymentStatus.UNHEALTHY,
-                message=f"Failed to update deployment:\n{e}.",
+                message="Failed to update deployment:" f"\n{traceback.format_exc()}",
             )
             deleted = False
 
