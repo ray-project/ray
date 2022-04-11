1503e101127d46def1e2aa44f5a3a393dae82e3d
Yi Cheng <chengyidna@gmail.com>
2022-04-11 02:14:23 +0000

    update

--- src/ray/gcs/gcs_server/gcs_resource_manager.cc
+++ src/ray/gcs/gcs_server/gcs_resource_manager.cc
@@ -14,8 +14,6 @@
 
 #include "ray/gcs/gcs_server/gcs_resource_manager.h"
 
-#include <google/protobuf/util/json_util.h>
-
 #include "ray/common/ray_config.h"
 #include "ray/stats/metric_defs.h"
 
@@ -37,10 +35,6 @@ void GcsResourceManager::Update(std::shared_ptr<const syncer::RaySyncMessage> me
         rpc::ResourcesData resources;
         resources.ParseFromString(message->sync_message());
         resources.set_node_id(message->node_id());
-        // std::string json_str;
-        // google::protobuf::util::MessageToJsonString(resources, &json_str);
-        // RAY_LOG(ERROR) << "Received a message: (" << message->component_id() << ")\t"
-        //                << json_str;
         auto node_id = NodeID::FromBinary(message->node_id());
         if (message->component_id() == syncer::RayComponentId::RESOURCE_MANAGER) {
           if (RayConfig::instance().gcs_actor_scheduling_enabled()) {
