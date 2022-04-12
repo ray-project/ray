a560dffb8390855618f35e64cbcdb60c888f5588
Siyuan Zhuang <suquark@gmail.com>
2022-04-11 20:52:00 -0700

    enable debugging cloudpickle

--- python/ray/cloudpickle/compat.py
+++ python/ray/cloudpickle/compat.py
@@ -1,7 +1,23 @@
+import logging
+import os
 import sys
 
+logger = logging.getLogger(__name__)
 
-if sys.version_info < (3, 8):
+RAY_PICKLE_VERBOSE_DEBUG = os.environ.get("RAY_PICKLE_VERBOSE_DEBUG")
+
+if RAY_PICKLE_VERBOSE_DEBUG:
+    level = int(RAY_PICKLE_VERBOSE_DEBUG)
+
+if level > 1:
+    logger.warning(
+        "Environmental variable RAY_PICKLE_VERBOSE_DEBUG is set to "
+        f"'{level}', this enabled python-based serialization backend "
+        f"instead of C-Pickle. Serialization would be very slow."
+    )
+    from ray.cloudpickle import py_pickle as pickle
+    from ray.cloudpickle.py_pickle import Pickler
+elif sys.version_info < (3, 8):
     try:
         import pickle5 as pickle  # noqa: F401
         from pickle5 import Pickler  # noqa: F401
@@ -10,4 +26,4 @@ if sys.version_info < (3, 8):
         from pickle import _Pickler as Pickler  # noqa: F401
 else:
     import pickle  # noqa: F401
-    from _pickle import Pickler  # noqa: F401
+    from pickle import _Pickler as Pickler  # noqa: F401
