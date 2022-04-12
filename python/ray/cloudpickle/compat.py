2a47994b97b21872ad17ac146a4cc513211b0bad
Siyuan Zhuang <suquark@gmail.com>
2022-04-12 02:19:03 -0700

    fix

--- python/ray/cloudpickle/compat.py
+++ python/ray/cloudpickle/compat.py
@@ -5,14 +5,12 @@ import sys
 logger = logging.getLogger(__name__)
 
 RAY_PICKLE_VERBOSE_DEBUG = os.environ.get("RAY_PICKLE_VERBOSE_DEBUG")
+verbose_level = int(RAY_PICKLE_VERBOSE_DEBUG) if RAY_PICKLE_VERBOSE_DEBUG else 0
 
-if RAY_PICKLE_VERBOSE_DEBUG:
-    level = int(RAY_PICKLE_VERBOSE_DEBUG)
-
-if level > 1:
+if verbose_level > 1:
     logger.warning(
         "Environmental variable RAY_PICKLE_VERBOSE_DEBUG is set to "
-        f"'{level}', this enabled python-based serialization backend "
+        f"'{verbose_level}', this enabled python-based serialization backend "
         f"instead of C-Pickle. Serialization would be very slow."
     )
     from ray.cloudpickle import py_pickle as pickle
