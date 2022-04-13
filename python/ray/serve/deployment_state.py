b25e5f541bf0f05bdbed9ebc8061d17df8d525cb
Jiao Dong <sophchess@gmail.com>
2022-04-13 15:07:19 -0700

    wip

--- python/ray/serve/deployment_state.py
+++ python/ray/serve/deployment_state.py
@@ -266,7 +266,9 @@ class ActorReplicaWrapper:
         self._actor_resources = deployment_info.replica_config.resource_dict
         # it is currently not possiible to create a placement group
         # with no resources (https://github.com/ray-project/ray/issues/20401)
-        has_resources_assigned = all((r > 0 for r in self._actor_resources.values()))
+        has_resources_assigned = all(
+            (r is not None and r > 0 for r in self._actor_resources.values())
+        )
         if USE_PLACEMENT_GROUP and has_resources_assigned:
             self._placement_group = self.create_placement_group(
                 self._placement_group_name, self._actor_resources
@@ -756,7 +758,11 @@ class DeploymentReplica(VersionedReplica):
         if self._actor.actor_resources is None:
             return "UNKNOWN", "UNKNOWN"
 
-        required = {k: v for k, v in self._actor.actor_resources.items() if v > 0}
+        required = {
+            k: v
+            for k, v in self._actor.actor_resources.items()
+            if v is not None and v > 0
+        }
         available = {
             k: v for k, v in self._actor.available_resources.items() if k in required
         }
