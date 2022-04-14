9c81a97fd917a049b8fea59607dbb59a38680194
Siyuan (Ryans) Zhuang <suquark@gmail.com>
2022-04-13 22:09:14 -0700

    [core] Simplify options handling [Part 2] (#23882)
    * cleanup
    
    * keep the original semantics

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -2112,7 +2112,6 @@ def _mode(worker=global_worker):
 
 
 def _make_remote(function_or_class, options):
-    # filter out placeholders in options
     if inspect.isfunction(function_or_class) or is_cython(function_or_class):
         ray_option_utils.validate_task_options(options, in_options=False)
         return ray.remote_function.RemoteFunction(
