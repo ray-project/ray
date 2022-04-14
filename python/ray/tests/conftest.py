2a9d99c3eff6cefee5b241404423fd12ef6846a4
jonch <9093549+jon-chuang@users.noreply.github.com>
2022-04-14 11:33:40 -0400

    reorg

--- python/ray/tests/conftest.py
+++ python/ray/tests/conftest.py
@@ -696,11 +696,12 @@ def pytest_runtest_makereport(item, call):
             import platform
             from tempfile import gettempdir
 
-            if not os.path.exists(archive_dir):
-                os.makedirs(archive_dir)
-            output_file = f"{archive_dir}/{rep.nodeid.split('/')[-1]}-{time.time()}"
-            tmp_dir = "/tmp" if platform.system() == "Darwin" else gettempdir()
-            log_dir = f"{tmp_dir}/ray/session_latest/logs"
             # Limit to Linux for now
-            if os.path.exists(log_dir) and platform.system() == "Linux":
-                shutil.make_archive(output_file, "zip", log_dir)
+            if platform.system() == "Linux":
+                if not os.path.exists(archive_dir):
+                    os.makedirs(archive_dir)
+                output_file = f"{archive_dir}/{rep.nodeid.split('/')[-1]}-{time.time()}"
+                tmp_dir = "/tmp" if platform.system() == "Darwin" else gettempdir()
+                log_dir = f"{tmp_dir}/ray/session_latest/logs"
+                if os.path.exists(log_dir):
+                    shutil.make_archive(output_file, "zip", log_dir)
