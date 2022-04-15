28b9453d50bba6ae5c66a252461f3053dad06073
Siyuan <suquark@gmail.com>
2022-04-14 17:38:44 -0700

    update

--- python/ray/_private/ray_option_utils.py
+++ python/ray/_private/ray_option_utils.py
@@ -69,7 +69,6 @@ def _resource_option(name: str, default_value: Any = None):
 _common_options = {
     "accelerator_type": Option((str, type(None))),
     "memory": _resource_option("memory"),
-    "metadata": Option((dict, type(None))),
     "name": Option((str, type(None))),
     "num_cpus": _resource_option("num_cpus"),
     "num_gpus": _resource_option("num_gpus"),
@@ -97,6 +96,7 @@ _common_options = {
             NodeAffinitySchedulingStrategy,
         )
     ),
+    "_metadata": Option((dict, type(None))),
 }
 
 
