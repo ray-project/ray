e7356132074ae505bce340f9ad46845550e3d3c8
AurelianTactics <jamesdilorenzo@gmail.com>
2022-04-17 11:08:02 -0400

    on eval callback
    can do a callback on eval

--- rllib/agents/callbacks.py
+++ rllib/agents/callbacks.py
@@ -292,7 +292,18 @@ class DefaultCallbacks:
                     "result": result,
                 }
             )
-
+            
+    def on_evaluate_end(self, *, trainer: "Trainer", result: dict,
+                        **kwargs) -> None:
+        """Called at the end of Trainer.evaluate().
+        UNTESTED
+        Args:
+            trainer: Current trainer instance.
+            result: Dict of results returned from trainer.evaluate() call.
+                UNTESTED You can mutate this object to add additional metrics.
+            kwargs: Forward compatibility placeholder.
+        """
+        pass
 
 class MemoryTrackingCallbacks(DefaultCallbacks):
     """MemoryTrackingCallbacks can be used to trace and track memory usage
