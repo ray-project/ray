8f2ae8476efcd8d7c8f7429fc7f093604efd47dd
Amog Kamsetty <amogkamsetty@yahoo.com>
2022-04-18 18:19:25 -0700

    separate

--- ci/lint/format.sh
+++ ci/lint/format.sh
@@ -2,6 +2,8 @@
 # Black + Clang formatter (if installed). This script formats all changed files from the last mergebase.
 # You are encouraged to run this locally before pushing changes for review.
 
+# In CI, the formatter also checks
+
 # Cause the script to exit if a single command fails
 set -euo pipefail
 
@@ -9,20 +11,6 @@ FLAKE8_VERSION_REQUIRED="3.9.1"
 BLACK_VERSION_REQUIRED="21.12b0"
 SHELLCHECK_VERSION_REQUIRED="0.7.1"
 MYPY_VERSION_REQUIRED="0.782"
-BANNED_WORDS="RLLib Rllib"
-
-check_banned_words() {
-    echo "Checking for common mis-spellings..."
-    for word in $BANNED_WORDS; do
-        if grep -C2 -R --include="*.py" --include="*.rst" "$word" .; then
-            echo "******************************"
-            echo "*** Misspelled word found! ***"
-            echo "******************************"
-            echo "Please fix the capitalization/spelling of \"$word\" in the above files."
-            exit 1
-        fi
-    done
-}
 
 check_python_command_exist() {
     VERSION=""
@@ -49,7 +37,6 @@ check_python_command_exist() {
 check_python_command_exist black
 check_python_command_exist flake8
 check_python_command_exist mypy
-check_banned_words
 
 # this stops git rev-parse from failing if we run this from the .git directory
 builtin cd "$(dirname "${BASH_SOURCE:-$0}")"
