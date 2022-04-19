1c3329fa3842c45302bee64d3fb67348f817bc7e
SangBin Cho <rkooo567@gmail.com>
2022-04-19 13:15:43 +0900

    Revert "Revert "[State Observability] Basic functionality for central… (#23933)
    …ized data (#23744)" (#23918)"
    
    This reverts commit fb14e82.

--- python/ray/scripts/scripts.py
+++ python/ray/scripts/scripts.py
@@ -42,6 +42,7 @@ from ray.autoscaler._private.kuberay.run_autoscaler import run_autoscaler_with_r
 from ray.internal.internal_api import memory_summary
 from ray.autoscaler._private.cli_logger import add_click_logging_options, cli_logger, cf
 from ray.dashboard.modules.job.cli import job_cli_group
+from ray.experimental.state.state_cli import list_state_cli_group
 from distutils.dir_util import copy_tree
 
 logger = logging.getLogger(__name__)
@@ -2251,6 +2252,7 @@ cli.add_command(timeline)
 cli.add_command(install_nightly)
 cli.add_command(cpp)
 add_command_alias(job_cli_group, name="job", hidden=True)
+add_command_alias(list_state_cli_group, name="list", hidden=True)
 
 try:
     from ray.serve.scripts import serve_cli
