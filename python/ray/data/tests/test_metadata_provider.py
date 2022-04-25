0de1167fee5baa66527ac8fcadc32d54050c4d6a
Patrick Ames <pdames@amazon.com>
2022-04-25 15:58:37 -0700

    [Datasets] Add dedicated unit test suite for file metadata providers.

--- python/ray/data/tests/test_metadata_provider.py
+++ python/ray/data/tests/test_metadata_provider.py
@@ -7,11 +7,7 @@ import pyarrow as pa
 import pandas as pd
 import pyarrow.parquet as pq
 from pytest_lazyfixture import lazy_fixture
-from ray.data.datasource.file_based_datasource import (
-    _unwrap_protocol,
-    _resolve_paths_and_filesystem,
-)
-
+from ray.data.datasource.file_based_datasource import _resolve_paths_and_filesystem
 
 from ray.tests.conftest import *  # noqa
 from ray.data.datasource import (
@@ -33,7 +29,7 @@ def _get_parquet_file_meta_size_bytes(file_metas):
     )
 
 
-def _get_file_size_bytes(paths, fs):
+def _get_file_sizes_bytes(paths, fs):
     from pyarrow.fs import FileType
 
     file_sizes = []
@@ -43,7 +39,7 @@ def _get_file_size_bytes(paths, fs):
             file_sizes.append(file_info.size)
         else:
             raise FileNotFoundError(path)
-    return sum(size for size in file_sizes)
+    return file_sizes
 
 
 def test_file_metadata_providers_not_implemented():
@@ -79,29 +75,26 @@ def test_file_metadata_providers_not_implemented():
 )
 def test_default_parquet_metadata_provider(ray_start_regular_shared, fs, data_path):
     path_module = os.path if urllib.parse.urlparse(data_path).scheme else posixpath
+    paths = [
+        path_module.join(data_path, "test1.parquet"),
+        path_module.join(data_path, "test2.parquet"),
+    ]
+    paths, fs = _resolve_paths_and_filesystem(paths, fs)
 
-    print(f"path_module: {path_module}")
     df1 = pd.DataFrame({"one": [1, 2, 3], "two": ["a", "b", "c"]})
     table = pa.Table.from_pandas(df1)
-    setup_data_path = _unwrap_protocol(data_path)
-    path1 = path_module.join(setup_data_path, "test1.parquet")
-    pq.write_table(table, path1, filesystem=fs)
+    pq.write_table(table, paths[0], filesystem=fs)
     df2 = pd.DataFrame({"one": [4, 5, 6], "two": ["e", "f", "g"]})
     table = pa.Table.from_pandas(df2)
-    path2 = path_module.join(setup_data_path, "test2.parquet")
-    pq.write_table(table, path2, filesystem=fs)
-
-    paths = [path1, path2]
-    paths, fs = _resolve_paths_and_filesystem(paths, fs)
-    pq_ds = pq.ParquetDataset(paths, filesystem=fs, use_legacy_dataset=False)
+    pq.write_table(table, paths[1], filesystem=fs)
 
     meta_provider = DefaultParquetMetadataProvider()
+    pq_ds = pq.ParquetDataset(paths, filesystem=fs, use_legacy_dataset=False)
     file_metas = meta_provider.prefetch_file_metadata(pq_ds.pieces)
 
-    schema: pa.lib.Schema = pq_ds.schema
     meta = meta_provider(
         [p.path for p in pq_ds.pieces],
-        schema,
+        pq_ds.schema,
         pieces=pq_ds.pieces,
         prefetched_metadata=file_metas,
     )
@@ -110,7 +103,7 @@ def test_default_parquet_metadata_provider(ray_start_regular_shared, fs, data_pa
     assert meta.num_rows == 6
     assert len(paths) == 2
     assert all(path in meta.input_files for path in paths)
-    assert meta.schema.equals(schema)
+    assert meta.schema.equals(pq_ds.schema)
 
 
 @pytest.mark.parametrize(
@@ -131,32 +124,29 @@ def test_default_parquet_metadata_provider(ray_start_regular_shared, fs, data_pa
         ),
     ],
 )
-def test_default_file_metadata_provider(
-    ray_start_regular_shared,
-    fs,
-    data_path,
-    endpoint_url,
-):
+def test_default_file_metadata_provider(fs, data_path, endpoint_url):
     storage_options = (
         {}
         if endpoint_url is None
         else dict(client_kwargs=dict(endpoint_url=endpoint_url))
     )
+
     path_module = os.path if urllib.parse.urlparse(data_path).scheme else posixpath
-
-    df1 = pd.DataFrame({"one": [1, 2, 3], "two": ["a", "b", "c"]})
     path1 = path_module.join(data_path, "test1.csv")
-    df1.to_csv(path1, index=False, storage_options=storage_options)
-    df2 = pd.DataFrame({"one": [4, 5, 6], "two": ["e", "f", "g"]})
     path2 = path_module.join(data_path, "test2.csv")
-    df2.to_csv(path2, index=False, storage_options=storage_options)
-
     paths = [path1, path2]
     paths, fs = _resolve_paths_and_filesystem(paths, fs)
-    meta_provider = DefaultFileMetadataProvider()
 
+    df1 = pd.DataFrame({"one": [1, 2, 3], "two": ["a", "b", "c"]})
+    df1.to_csv(path1, index=False, storage_options=storage_options)
+    df2 = pd.DataFrame({"one": [4, 5, 6], "two": ["e", "f", "g"]})
+    df2.to_csv(path2, index=False, storage_options=storage_options)
+
+    meta_provider = DefaultFileMetadataProvider()
     file_paths, file_sizes = meta_provider.expand_paths(paths, fs)
     assert file_paths == paths
+    expected_file_sizes = _get_file_sizes_bytes(paths, fs)
+    assert file_sizes == expected_file_sizes
 
     meta = meta_provider(
         paths,
@@ -164,9 +154,7 @@ def test_default_file_metadata_provider(
         rows_per_file=3,
         file_sizes=file_sizes,
     )
-
-    expected_meta_size_bytes = _get_file_size_bytes(paths, fs)
-    assert meta.size_bytes == expected_meta_size_bytes
+    assert meta.size_bytes == sum(expected_file_sizes)
     assert meta.num_rows == 6
     assert len(paths) == 2
     assert all(path in meta.input_files for path in paths)
@@ -191,30 +179,25 @@ def test_default_file_metadata_provider(
         ),
     ],
 )
-def test_fast_file_metadata_provider(
-    ray_start_regular_shared,
-    fs,
-    data_path,
-    endpoint_url,
-):
+def test_fast_file_metadata_provider(fs, data_path, endpoint_url):
     storage_options = (
         {}
         if endpoint_url is None
         else dict(client_kwargs=dict(endpoint_url=endpoint_url))
     )
+
     path_module = os.path if urllib.parse.urlparse(data_path).scheme else posixpath
-
-    df1 = pd.DataFrame({"one": [1, 2, 3], "two": ["a", "b", "c"]})
     path1 = path_module.join(data_path, "test1.csv")
-    df1.to_csv(path1, index=False, storage_options=storage_options)
-    df2 = pd.DataFrame({"one": [4, 5, 6], "two": ["e", "f", "g"]})
     path2 = path_module.join(data_path, "test2.csv")
-    df2.to_csv(path2, index=False, storage_options=storage_options)
-
     paths = [path1, path2]
     paths, fs = _resolve_paths_and_filesystem(paths, fs)
-    meta_provider = FastFileMetadataProvider()
 
+    df1 = pd.DataFrame({"one": [1, 2, 3], "two": ["a", "b", "c"]})
+    df1.to_csv(path1, index=False, storage_options=storage_options)
+    df2 = pd.DataFrame({"one": [4, 5, 6], "two": ["e", "f", "g"]})
+    df2.to_csv(path2, index=False, storage_options=storage_options)
+
+    meta_provider = FastFileMetadataProvider()
     file_paths, file_sizes = meta_provider.expand_paths(paths, fs)
     assert file_paths == paths
     assert len(file_sizes) == len(file_paths)
@@ -225,7 +208,6 @@ def test_fast_file_metadata_provider(
         rows_per_file=3,
         file_sizes=file_sizes,
     )
-
     assert meta.size_bytes is None
     assert meta.num_rows == 6
     assert len(paths) == 2
