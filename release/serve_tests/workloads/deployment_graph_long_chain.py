7ffa00a873dcd8973b27f5870de3f16824c2ae1e
Ubuntu <ubuntu@ip-10-0-3-56.us-west-2.compute.internal>
2022-04-27 02:16:35 +0000

    add local latency and throughput test

--- release/serve_tests/workloads/deployment_graph_long_chain.py
+++ release/serve_tests/workloads/deployment_graph_long_chain.py
@@ -14,7 +14,6 @@ import asyncio
 import click
 import pytest
 import sys
-import logging
 import numpy as np
 from typing import Optional
 
@@ -26,22 +25,7 @@ from serve_test_cluster_utils import (
     setup_local_single_node_cluster,
     setup_anyscale_cluster,
 )
-
-logger = logging.getLogger(__file__)
-
-# @serve.deployment
-# class DAGDriver:
-#     def __init__(self, dag_handle):
-#         self.dag_handle = dag_handle
-
-#     async def predict(self, inp):
-#         """Perform inference directly without HTTP."""
-#         return await self.dag_handle.remote(inp)
-
-#     async def __call__(self, request):
-#         """HTTP endpoint of the DAG."""
-#         input_data = await request.json()
-#         return await self.predict(input_data)
+from serve_test_utils import save_test_results
 
 
 @serve.deployment
@@ -62,15 +46,13 @@ class Node:
 
 
 def test_long_chain_deployment_graph(
-    chain_length=20, init_delay_secs=0, compute_delay_secs=0
+    chain_length, init_delay_secs=0, compute_delay_secs=0
 ):
     with InputNode() as user_input:
         prev_output = user_input
         prev_node = None
         for i in range(chain_length):
-            node = Node.bind(
-                i, prev_node=prev_node, init_delay_secs=init_delay_secs
-            )
+            node = Node.bind(i, prev_node=prev_node, init_delay_secs=init_delay_secs)
             node_output = node.compute.bind(
                 prev_output, compute_delay_secs=compute_delay_secs
             )
@@ -82,7 +64,8 @@ def test_long_chain_deployment_graph(
 
     return serve_dag
 
-async def timeit(name, fn, multiplier=1):
+
+async def measure_throughput(name, fn, multiplier=1):
     # warmup
     start = time.time()
     while time.time() - start < 1:
@@ -97,49 +80,122 @@ async def timeit(name, fn, multiplier=1):
             count += 1
         end = time.time()
         stats.append(multiplier * count / (end - start))
-    logger.info(
-        "\t{} {} +- {} requests/s".format(
-            name, round(np.mean(stats), 2), round(np.std(stats), 2)
-        )
-    )
-    return round(np.mean(stats), 2)
 
-async def benchmark(dag_handle, num_calls=10):
+    mean = round(np.mean(stats), 2)
+    std = round(np.std(stats), 2)
+    print("\t{} {} +- {} requests/s".format(name, mean, std))
+    return mean, std
+
+
+async def measure_latency(name, fn):
+    # warmup
+    start = time.time()
+    while time.time() - start < 1:
+        await fn()
+    # real run
+    stats = []
+    for _ in range(4):
+        start = time.time()
+        while time.time() - start < 2:
+            await fn()
+        end = time.time()
+        stats.append(end - start)
+
+    mean = round(np.mean(stats), 2)
+    std = round(np.std(stats), 2)
+    print("\t{} {} +- {} sec".format(name, mean, std))
+    return mean, std
+
+
+async def benchmark_batch_throughput(dag_handle, expected, num_calls=10):
+    """Enqueue all requests to dag handle and gather them all."""
+
+    async def run():
+        futures = []
+        for _ in range(num_calls):
+            futures.append(dag_handle.predict.remote(0))
+        results = await asyncio.gather(*futures)
+        for rst in results:
+            assert rst == expected
+
+    return await measure_throughput(
+        "benchmark_batch_throughput", run, multiplier=num_calls
+    )
+
+
+async def benchmark_latency(dag_handle, expected, num_calls=10):
+    """Call deployment handle in a blocking for loop."""
+
     async def run():
         for _ in range(num_calls):
-            await dag_handle.predict.remote(0)
+            assert await dag_handle.predict.remote(0) == expected
 
-    return await timeit("Single client long chain", run, multiplier=1)
+    return await measure_latency("benchmark_batch_latency", run)
 
 
 @click.command()
 @click.option("--chain-length", type=int, default=4)
 @click.option("--init-delay-secs", type=int, default=0)
 @click.option("--compute-delay-secs", type=int, default=0)
+@click.option("--num-calls", type=int, default=10)
 @click.option("--local-test", type=bool, default=True)
 def main(
     chain_length: Optional[int],
     init_delay_secs: Optional[int],
     compute_delay_secs: Optional[int],
+    num_calls: Optional[int],
     local_test: Optional[bool],
 ):
     if local_test:
         setup_local_single_node_cluster(4, num_cpu_per_node=2)
         serve_dag = test_long_chain_deployment_graph(
-            chain_length=chain_length,
+            chain_length,
             init_delay_secs=init_delay_secs,
             compute_delay_secs=compute_delay_secs,
         )
         dag_handle = serve.run(serve_dag)
 
-        # 1 + 2 + 3 + 4 + ... chain_length
-        assert (
-            ray.get(dag_handle.predict.remote(0))
-            == ((1 + chain_length) * chain_length) / 2
-        )
+        # 1 + 2 + 3 + 4 + ... + chain_length
+        expected = ((1 + chain_length) * chain_length) / 2
+        assert ray.get(dag_handle.predict.remote(0)) == expected
         loop = asyncio.get_event_loop()
-        stats = loop.run_until_complete(benchmark(dag_handle))
-        logger.info(f"\n\n\n{stats}\n\n\n")
+
+        throughput_mean, throughput_std = loop.run_until_complete(
+            benchmark_batch_throughput(dag_handle, expected, num_calls=num_calls)
+        )
+        latency_mean, latency_std = loop.run_until_complete(
+            benchmark_latency(dag_handle, expected, num_calls=num_calls)
+        )
+
+        results = {
+            "chain_length": chain_length,
+            "init_delay_secs": init_delay_secs,
+            "compute_delay_secs": compute_delay_secs,
+            "local_test": local_test,
+        }
+        results["perf_metrics"] = [
+            {
+                "perf_metric_name": "requests_per_s_avg",
+                "perf_metric_value": throughput_mean,
+                "perf_metric_type": "THROUGHPUT",
+            },
+            {
+                "perf_metric_name": "requests_per_s_std",
+                "perf_metric_value": throughput_std,
+                "perf_metric_type": "THROUGHPUT",
+            },
+            {
+                "perf_metric_name": "latency_per_s_avg",
+                "perf_metric_value": latency_mean,
+                "perf_metric_type": "LATENCY",
+            },
+            {
+                "perf_metric_name": "requests_per_s_std",
+                "perf_metric_value": latency_std,
+                "perf_metric_type": "LATENCY",
+            },
+        ]
+        save_test_results(results)
     else:
         setup_anyscale_cluster()
         serve_dag = test_long_chain_deployment_graph(
