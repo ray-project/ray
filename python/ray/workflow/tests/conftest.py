ff89005996c0d3d2233effc7caef12bc5a6f97b8
Siyuan Zhuang <suquark@gmail.com>
2022-04-27 10:13:24 -0700

    reuse upstream test utils

--- python/ray/workflow/tests/conftest.py
+++ python/ray/workflow/tests/conftest.py
@@ -1,66 +1,13 @@
-import boto3
 from contextlib import contextmanager
 import pytest
 import ray
-from moto import mock_s3
-from mock_server import start_service, stop_process
 
-import tempfile
 from ray.tests.conftest import get_default_fixture_ray_kwargs
+from ray._private.test_utils import simulate_storage
 
 # Trigger pytest hook to automatically zip test cluster logs to archive dir on failure
 from ray.tests.conftest import pytest_runtest_makereport  # noqa
-import os
-import uuid
 from ray.workflow.tests import utils
-
-
-@contextmanager
-def aws_credentials():
-    old_env = os.environ
-    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
-    os.environ["AWS_SECURITY_TOKEN"] = "testing"
-    os.environ["AWS_SESSION_TOKEN"] = "testing"
-    yield
-    os.environ = old_env
-
-
-@contextmanager
-def moto_s3_server():
-    host = "localhost"
-    port = 5002
-    url = "http://{host}:{port}".format(host=host, port=port)
-    process = start_service("s3", host, port)
-    yield url
-    stop_process(process)
-
-
-@contextmanager
-def filesystem_storage():
-    with tempfile.TemporaryDirectory() as d:
-        yield d
-
-
-@contextmanager
-def s3_storage():
-    with moto_s3_server() as s3_server, aws_credentials(), mock_s3():
-        client = boto3.client("s3", region_name="us-west-2", endpoint_url=s3_server)
-        bucket = str(uuid.uuid1())
-        client.create_bucket(Bucket=bucket)
-        url = f"s3://{bucket}/workflow?region=us-west-2&endpoint_override={s3_server}"
-        yield url
-
-
-@contextmanager
-def storage(storage_type):
-    if storage_type == "s3":
-        with s3_storage() as url:
-            yield url
-    else:
-        with filesystem_storage() as url:
-            url = os.path.abspath(url)
-            yield "file://" + url
 
 
 @contextmanager
@@ -85,7 +32,7 @@ def _workflow_start(storage_url, shared, **kwargs):
 @pytest.fixture(scope="function")
 def workflow_start_regular(storage_type, request):
     param = getattr(request, "param", {})
-    with storage(storage_type) as storage_url, _workflow_start(
+    with simulate_storage(storage_type) as storage_url, _workflow_start(
         storage_url, False, **param
     ) as res:
         yield res
@@ -94,7 +41,7 @@ def workflow_start_regular(storage_type, request):
 @pytest.fixture(scope="module")
 def workflow_start_regular_shared(storage_type, request):
     param = getattr(request, "param", {})
-    with storage(storage_type) as storage_url, _workflow_start(
+    with simulate_storage(storage_type) as storage_url, _workflow_start(
         storage_url, True, **param
     ) as res:
         yield res
