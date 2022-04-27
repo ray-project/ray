3957a855d0271795895304384cc2810526556e48
sven1977 <svenmika1977@gmail.com>
2022-04-27 21:17:01 +0200

    wip

--- rllib/utils/tf_utils.py
+++ rllib/utils/tf_utils.py
@@ -240,9 +240,11 @@ def get_tf_eager_cls_if_necessary(
             assert tf1.executing_eagerly()
 
             from ray.rllib.policy.tf_policy import TFPolicy
+            from ray.rllib.policy.eager_tf_policy import EagerTFPolicy
 
-            # Create eager-class.
-            if hasattr(orig_cls, "as_eager"):
+            # Create eager-class (if not already one).
+            if hasattr(orig_cls, "as_eager") and \
+                    not issubclass(orig_cls, EagerTFPolicy):
                 cls = orig_cls.as_eager()
                 if config.get("eager_tracing"):
                     cls = cls.with_tracing()
