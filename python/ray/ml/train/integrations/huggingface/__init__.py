2c8f48c28fd8e594f33032c91a9b6b45b3422e2e
Antoni Baum <antoni.baum@protonmail.com>
2022-04-29 18:55:51 +0000

    Load huggingface checkpoint to staticmethod

--- python/ray/ml/train/integrations/huggingface/__init__.py
+++ python/ray/ml/train/integrations/huggingface/__init__.py
@@ -1,6 +1,5 @@
 from ray.ml.train.integrations.huggingface.huggingface_trainer import (
     HuggingFaceTrainer,
 )
-from ray.ml.utils.huggingface_checkpoint_utils import load_huggingface_checkpoint
 
-__all__ = ["HuggingFaceTrainer", "load_huggingface_checkpoint"]
+__all__ = ["HuggingFaceTrainer"]
--- python/ray/ml/train/integrations/huggingface/huggingface_trainer.py
+++ python/ray/ml/train/integrations/huggingface/huggingface_trainer.py
@@ -4,10 +4,15 @@ import shutil
 import tempfile
 from distutils.version import LooseVersion
 from pathlib import Path
-from typing import Any, Callable, Dict, Optional, Type
+from typing import Any, Callable, Dict, Optional, Tuple, Type, Union
+from ray.ml.utils.torch_utils import load_torch_model
 
+import torch
 import transformers
+import transformers.modeling_utils
 import transformers.trainer
+from transformers.trainer import WEIGHTS_NAME, TRAINING_ARGS_NAME
+import transformers.training_args
 import ray.cloudpickle as cpickle
 from torch.utils.data import Dataset as TorchDataset
 
@@ -314,9 +319,11 @@ class HuggingFaceTrainer(TorchTrainer):
     def _convert_directory_checkpoint_to_sync_if_needed(
         self, checkpoint: Checkpoint
     ) -> Checkpoint:
-        """Replace the directory checkpoint with a node ip & path dict checkpoint
-        used to sync the directory. If we use a directory checkpoint directly,
-        it will get deepcopied & serialized unnecessarily."""
+        """Replace the directory checkpoint with a node ip & path dict checkpoint.
+
+        This dict checkpoint will be used used to sync the directory.
+        If we were to use a directory checkpoint directly, it would get deepcopied &
+        serialized unnecessarily."""
         with checkpoint.as_directory() as checkpoint_path:
             # Load checkpoint from path.
             checkpoint_path = Path(checkpoint_path).expanduser().absolute()
@@ -369,6 +376,74 @@ class HuggingFaceTrainer(TorchTrainer):
             self._param_dict = original_param_dict
         return ret
 
+    @staticmethod
+    def load_huggingface_checkpoint(
+        checkpoint: Checkpoint,
+        model: Union[
+            Type[transformers.modeling_utils.PreTrainedModel], torch.nn.Module
+        ],
+        tokenizer: Optional[Type[transformers.PreTrainedTokenizer]] = None,
+        *,
+        tokenizer_kwargs: Optional[Dict[str, Any]] = None,
+        **pretrained_model_kwargs,
+    ) -> Tuple[
+        Union[transformers.modeling_utils.PreTrainedModel, torch.nn.Module],
+        transformers.training_args.TrainingArguments,
+        Optional[transformers.PreTrainedTokenizer],
+        Optional[Preprocessor],
+    ]:
+        """Load a Checkpoint from ``HuggingFaceTrainer``.
+
+        Return the model, ``TrainingArguments``, tokenizer and AIR preprocessor
+        contained within. Those can be used to initialize a ``transformers.Trainer``
+        object locally.
+
+        Args:
+            checkpoint: The checkpoint to load the model and
+                preprocessor from. It is expected to be from the result of a
+                ``HuggingFaceTrainer`` run.
+            model: Either a ``transformers.PreTrainedModel`` class
+                (eg. ``AutoModelForCausalLM``), or a PyTorch model to load the
+                weights to. This should be the same model used for training.
+            tokenizer: A ``transformers.PreTrainedTokenizer`` class to load
+                the model tokenizer to. If not specified, the tokenizer will
+                not be loaded. Will throw an exception if specified, but no
+                tokenizer was found in the checkpoint.
+            tokenizer_kwargs: Dict of kwargs to pass to ``tokenizer.from_pretrained``
+                call. Ignored if ``tokenizer`` is None.
+            **pretrained_model_kwargs: Kwargs to pass to ``mode.from_pretrained``
+                call. Ignored if ``model`` is not a ``transformers.PreTrainedModel``
+                class.
+        """
+        tokenizer_kwargs = tokenizer_kwargs or {}
+        with checkpoint.as_directory() as checkpoint_path:
+            preprocessor_path = os.path.join(checkpoint_path, PREPROCESSOR_KEY)
+            if os.path.exists(preprocessor_path):
+                with open(preprocessor_path, "rb") as f:
+                    preprocessor = cpickle.load(f)
+            else:
+                preprocessor = None
+            if isinstance(model, torch.nn.Module):
+                state_dict = torch.load(
+                    os.path.join(checkpoint_path, WEIGHTS_NAME), map_location="cpu"
+                )
+                model = load_torch_model(saved_model=state_dict, model_definition=model)
+            else:
+                model = model.from_pretrained(
+                    checkpoint_path, **pretrained_model_kwargs
+                )
+            if tokenizer:
+                tokenizer = tokenizer.from_pretrained(
+                    checkpoint_path, **tokenizer_kwargs
+                )
+            training_args_path = os.path.join(checkpoint_path, TRAINING_ARGS_NAME)
+            if os.path.exists(training_args_path):
+                with open(training_args_path, "rb") as f:
+                    training_args = torch.load(f, map_location="cpu")
+            else:
+                training_args = None
+        return model, training_args, tokenizer, preprocessor
+
 
 def _huggingface_train_loop_per_worker(config):
     """Per-worker training loop for HuggingFace Transformers."""
@@ -395,8 +470,8 @@ def _huggingface_train_loop_per_worker(config):
         raise ValueError(
             "`push_to_hub` parameter in `TrainingArgs` is not supported by "
             "`HuggingFaceTrainer`. If you would like to push your model to hub "
-            "after training, use the `load_huggingface_checkpoint` function "
-            "to obtain the model from a returned checkpoint, and use it to "
+            "after training, use the `HuggingFaceTrainer.load_huggingface_checkpoint`"
+            " method to obtain the model from a returned checkpoint, and use it to "
             "instantiate the `transformers.Trainer` class."
         )
 
