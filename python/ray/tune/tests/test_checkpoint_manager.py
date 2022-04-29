940bbf19d090b715186cc3025c9e0a54ccdcd14e
Antoni Baum <antoni.baum@protonmail.com>
2022-04-29 20:43:12 +0000

    Remove debug

--- python/ray/tune/tests/test_checkpoint_manager.py
+++ python/ray/tune/tests/test_checkpoint_manager.py
@@ -117,14 +117,12 @@ class CheckpointManagerTest(unittest.TestCase):
             )
             for i in range(2)
         ]
+        checkpoints += [
+            _TuneCheckpoint(_TuneCheckpoint.PERSISTENT, 3, self.mock_result(0, 3))
+        ]
 
         for checkpoint in checkpoints:
             checkpoint_manager.on_checkpoint(checkpoint)
-
-        print(checkpoint_manager._membership)
-        print(checkpoint_manager.best_checkpoints())
-
-        checkpoint_manager.on_checkpoint(_TuneCheckpoint(_TuneCheckpoint.PERSISTENT, 3, self.mock_result(0, 3)))
 
         best_checkpoints = checkpoint_manager.best_checkpoints()
         # best_checkpoints is sorted from worst to best
