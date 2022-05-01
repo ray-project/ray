fbe1f7786ef9bf0c36726fc6cd61a65288f0ca4f
Artur Niederfahrenhorst <attaismyname@googlemail.com>
2022-05-01 16:57:16 +0200

    use min_sample_timesteps_per_reporting in ddpg

--- rllib/agents/ddpg/ddpg.py
+++ rllib/agents/ddpg/ddpg.py
@@ -84,7 +84,7 @@ DEFAULT_CONFIG = with_common_config({
         "scale_timesteps": 10000,
     },
     # Number of env steps to optimize for before returning
-    "timesteps_per_iteration": 1000,
+    "min_sample_timesteps_per_reporting": 1000,
     # Extra configuration that disables exploration.
     "evaluation_config": {
         "explore": False
