fd524e984dd648278253e3c66b776002774f9039
sven1977 <svenmika1977@gmail.com>
2022-05-02 15:59:26 +0200

    wip

--- rllib/evaluation/worker_set.py
+++ rllib/evaluation/worker_set.py
@@ -710,8 +710,8 @@ class WorkerSet:
         local_worker = self.local_worker()
         if local_worker is not None:
             return [
-                local_worker.is_policy_to_train(pid, None)
-                for pid in local_worker.policy_map.keys()
+                pid for pid in local_worker.policy_map.keys()
+                if local_worker.is_policy_to_train(pid, None)
             ]
         else:
             raise NotImplementedError
