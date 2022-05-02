a58bf3964aedf575886dc423c534ae3167ae1fb3
Siyuan Zhuang <suquark@gmail.com>
2022-05-02 12:37:19 -0700

    remove unnecessary tests

--- python/ray/serve/tests/test_config.py
+++ python/ray/serve/tests/test_config.py
@@ -182,26 +182,6 @@ def test_replica_config_validation():
             ReplicaConfig(Class, ray_actor_options={option: None})
 
 
-@pytest.mark.parametrize(
-    "memory_omitted_options",
-    [
-        None,
-        {},
-        {"num_cpus": 1, "num_gpus": 3},
-        {"num_cpus": 1, "num_gpus": 3, "memory": None},
-    ],
-)
-def test_replica_config_default_memory_none(memory_omitted_options):
-    """Checks that ReplicaConfig's default memory is None."""
-
-    if memory_omitted_options is None:
-        config = ReplicaConfig("fake.import_path")
-        assert config.ray_actor_options.get("memory") is None
-
-    config = ReplicaConfig("fake.import_path", ray_actor_options=memory_omitted_options)
-    assert config.ray_actor_options.get("memory") is None
-
-
 def test_http_options():
     HTTPOptions()
     HTTPOptions(host="8.8.8.8", middlewares=[object()])
