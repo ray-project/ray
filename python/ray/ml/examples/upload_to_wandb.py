61b487c3e65d1b4d88df4ed5cdfca73c807e210a
Kai Fricke <kai@anyscale.com>
2022-05-04 20:18:41 +0100

    Only run example in branch build

--- python/ray/ml/examples/upload_to_wandb.py
+++ python/ray/ml/examples/upload_to_wandb.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.6
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
-# # Experiment tracking and model upload with Weights & Biases
 import ray
 
 from ray.ml import RunConfig
