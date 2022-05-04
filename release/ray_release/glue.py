0563b8a228b6bcd68662b79c07dc6e6e64eb1e95
SangBin Cho <rkooo567@gmail.com>
2022-05-03 20:02:37 -0700

    lint

--- release/ray_release/glue.py
+++ release/ray_release/glue.py
@@ -222,7 +222,9 @@ def run_release_test(
         if wait_for_nodes:
             buildkite_group(":stopwatch: Waiting for nodes to come up")
             num_nodes = test["run"]["wait_for_nodes"]["num_nodes"]
-            wait_timeout = test["run"]["wait_for_nodes"].get("timeout", DEFAULT_WAIT_FOR_NODES_TIMEOUT)
+            wait_timeout = test["run"]["wait_for_nodes"].get(
+                "timeout", DEFAULT_WAIT_FOR_NODES_TIMEOUT
+            )
             command_runner.wait_for_nodes(num_nodes, wait_timeout)
 
         prepare_cmd = test["run"].get("prepare", None)
