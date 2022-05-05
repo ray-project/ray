12a6d7b567d361a1783f40f5a56a071a1f3f64a3
lchu <lchu@us.ibm.com>
2022-05-04 22:47:30 -0400

    up

--- python/ray/workflow/tests/test_logging.py
+++ python/ray/workflow/tests/test_logging.py
@@ -2,8 +2,6 @@ from ray._private.test_utils import run_string_as_driver_nonblocking
 
 
 def test_basic_workflow_logs(workflow_start_regular):
-    from ray.internal.storage import _storage_uri
-
     script = f"""
 import ray
 from ray import workflow
@@ -29,8 +27,6 @@ f.step().run("wid")
 
 
 def test_chained_workflow_logs(workflow_start_regular):
-    from ray.internal.storage import _storage_uri
-
     script = f"""
 import ray
 from ray import workflow
@@ -62,8 +58,6 @@ f2.step(f1.step()).run("wid1")
 
 
 def test_dynamic_workflow_logs(workflow_start_regular):
-    from ray.internal.storage import _storage_uri
-
     script = f"""
 import ray
 from ray import workflow
@@ -95,8 +89,6 @@ f4.step(10).run("wid2")
 
 
 def test_virtual_actor_logs(workflow_start_regular):
-    from ray.internal.storage import _storage_uri
-
     script = f"""
 import ray
 from ray import workflow
