9ecf73d4e7fff7c3dde9e2d40a05e165b4f61c39
Jun Gong <jungong@anyscale.com>
2022-05-05 07:47:25 -0700

    fix typo

--- rllib/agents/dqn/simple_q_torch_policy.py
+++ rllib/agents/dqn/simple_q_torch_policy.py
@@ -18,7 +18,7 @@ from ray.rllib.models.torch.torch_action_dist import (
 from ray.rllib.policy import Policy
 from ray.rllib.policy.policy_template import build_policy_class
 from ray.rllib.policy.sample_batch import SampleBatch
-from ray.rllib.policy.torch_mixin import TargetNetworkMixin
+from ray.rllib.policy.torch_mixins import TargetNetworkMixin
 from ray.rllib.policy.torch_policy import TorchPolicy
 from ray.rllib.utils.annotations import override
 from ray.rllib.utils.framework import try_import_torch
