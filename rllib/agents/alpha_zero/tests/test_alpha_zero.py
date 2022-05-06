940433b2f6768ab2290bb80a5cc816b4363186e2
sven1977 <svenmika1977@gmail.com>
2022-05-06 16:41:25 +0200

    wip.

--- rllib/agents/alpha_zero/tests/test_alpha_zero.py
+++ rllib/agents/alpha_zero/tests/test_alpha_zero.py
@@ -2,8 +2,9 @@ import unittest
 
 import ray
 import ray.rllib.agents.alpha_zero as az
+from ray.rllib.agents.alpha_zero.models.custom_torch_models import DenseModel
+from ray.rllib.examples.env.cartpole_sparse_rewards import CartPoleSparseRewards
 from ray.rllib.utils.test_utils import (
-    check_compute_single_action,
     check_train_results,
     framework_iterator,
 )
@@ -21,17 +22,17 @@ class TestAlphaZero(unittest.TestCase):
     def test_alpha_zero_compilation(self):
         """Test whether an AlphaZeroTrainer can be built with all frameworks."""
         config = az.DEFAULT_CONFIG.copy()
+        config["env"] = CartPoleSparseRewards
+        config["model"]["custom_model"] = DenseModel
         num_iterations = 1
 
         # Only working for torch right now.
         for _ in framework_iterator(config, frameworks="torch"):
-            trainer = az.AlphaZeroTrainer(env="CartPole-v0")
+            trainer = az.AlphaZeroTrainer(config)
             for i in range(num_iterations):
                 results = trainer.train()
                 check_train_results(results)
                 print(results)
-
-            check_compute_single_action(trainer, include_prev_action_reward=True)
 
 
 if __name__ == "__main__":
