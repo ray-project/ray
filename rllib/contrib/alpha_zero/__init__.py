911153104b0005e645fae5cc36238c91682ba17a
sven1977 <svenmika1977@gmail.com>
2022-05-07 19:05:39 +0200

    wip.

--- rllib/contrib/alpha_zero/__init__.py
+++
@@ -1,7 +0,0 @@
-from ray.rllib.utils.deprecation import deprecation_warning
-
-deprecation_warning(
-    old="ray.rllib.contrib.alpha_zero",
-    new="ray.rllib.agents.alpha_zero",
-    error=True,
-)
