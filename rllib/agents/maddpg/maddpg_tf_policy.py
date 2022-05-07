68d3ee3e589245546d76fe7d4050ff3574d58391
sven1977 <svenmika1977@gmail.com>
2022-05-07 17:45:16 +0200

    wip.

--- rllib/agents/maddpg/maddpg_tf_policy.py
+++ rllib/agents/maddpg/maddpg_tf_policy.py
@@ -69,11 +69,11 @@ class MADDPGTFPolicy(MADDPGPostprocessing, TFPolicy):
                 )
 
         obs_space_n = [
-            _make_continuous_space(space or obs_space)
+            _make_continuous_space(space)
             for _, (_, space, _, _) in config["multiagent"]["policies"].items()
         ]
         act_space_n = [
-            _make_continuous_space(space or act_space)
+            _make_continuous_space(space)
             for _, (_, _, space, _) in config["multiagent"]["policies"].items()
         ]
 
