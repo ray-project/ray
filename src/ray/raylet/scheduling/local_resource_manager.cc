e185a34b2e27c4e9420dcdda7dbb6fbd2e945eaa
Yi Cheng <chengyidna@gmail.com>
2022-05-09 21:45:15 +0000

    fix comment

--- src/ray/raylet/scheduling/local_resource_manager.cc
+++ src/ray/raylet/scheduling/local_resource_manager.cc
@@ -404,7 +404,7 @@ std::optional<syncer::RaySyncMessage> LocalResourceManager::CreateSyncMessage(
   resources_data.set_resources_available_changed(true);
 
   msg.set_node_id(local_node_id_.Binary());
-  msg.set_version(curr_version);
+  msg.set_version(version_);
   msg.set_message_type(message_type);
   std::string serialized_msg;
   RAY_CHECK(resources_data.SerializeToString(&serialized_msg));
