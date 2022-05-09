7ee5df7aff5e5ba2a5cd23d1a7cf28f336f104cf
Stephanie Wang <swang@cs.berkeley.edu>
2022-05-09 11:23:21 -0700

    fix

--- python/ray/data/impl/arrow_ops/transform_pyarrow.py
+++ python/ray/data/impl/arrow_ops/transform_pyarrow.py
@@ -1,12 +1,17 @@
 from typing import List, TYPE_CHECKING
-import pyarrow
-import pyarrow.compute as pac
+
+try:
+    import pyarrow
+except ImportError:
+    pyarrow = None
 
 if TYPE_CHECKING:
     from ray.data.impl.sort import SortKeyT
 
 
 def sort(table: "pyarrow.Table", key: "SortKeyT", descending: bool) -> "pyarrow.Table":
+    import pyarrow.compute as pac
+
     indices = pac.sort_indices(table, sort_keys=key)
     return table.take(indices)
 
