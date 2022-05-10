dc60b9804c8d647e1f497ccc599e12d3c5404432
simon-mo <simon.mo@hey.com>
2022-05-10 15:56:23 -0700

    Revert "test changes"
    This reverts commit bc41890e2efdc0e871a86707c769c9950590dc78.

--- ci/pipeline/determine_tests_to_run.py
+++ ci/pipeline/determine_tests_to_run.py
@@ -216,21 +216,19 @@ if __name__ == "__main__":
                 RAY_CI_DASHBOARD_AFFECTED = 1
                 RAY_CI_DOC_AFFECTED = 1
             else:
-                # TODO(simon): remove me, this is for testing
-                pass
-                # RAY_CI_ML_AFFECTED = 1
-                # RAY_CI_TUNE_AFFECTED = 1
-                # RAY_CI_TRAIN_AFFECTED = 1
-                # RAY_CI_RLLIB_AFFECTED = 1
-                # RAY_CI_SERVE_AFFECTED = 1
-                # RAY_CI_CORE_CPP_AFFECTED = 1
-                # RAY_CI_CPP_AFFECTED = 1
-                # RAY_CI_JAVA_AFFECTED = 1
-                # RAY_CI_PYTHON_AFFECTED = 1
-                # RAY_CI_DOC_AFFECTED = 1
-                # RAY_CI_LINUX_WHEELS_AFFECTED = 1
-                # RAY_CI_MACOS_WHEELS_AFFECTED = 1
-                # RAY_CI_DASHBOARD_AFFECTED = 1
+                RAY_CI_ML_AFFECTED = 1
+                RAY_CI_TUNE_AFFECTED = 1
+                RAY_CI_TRAIN_AFFECTED = 1
+                RAY_CI_RLLIB_AFFECTED = 1
+                RAY_CI_SERVE_AFFECTED = 1
+                RAY_CI_CORE_CPP_AFFECTED = 1
+                RAY_CI_CPP_AFFECTED = 1
+                RAY_CI_JAVA_AFFECTED = 1
+                RAY_CI_PYTHON_AFFECTED = 1
+                RAY_CI_DOC_AFFECTED = 1
+                RAY_CI_LINUX_WHEELS_AFFECTED = 1
+                RAY_CI_MACOS_WHEELS_AFFECTED = 1
+                RAY_CI_DASHBOARD_AFFECTED = 1
     else:
         RAY_CI_ML_AFFECTED = 1
         RAY_CI_TUNE_AFFECTED = 1
