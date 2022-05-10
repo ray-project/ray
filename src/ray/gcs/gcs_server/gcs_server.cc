ff56f8b2a8c2b0a958e087ead2cda510214f1d38
Yi Cheng <chengyidna@gmail.com>
2022-05-10 06:47:17 +0000

    fix a bad merge

--- src/ray/gcs/gcs_server/gcs_server.cc
+++ src/ray/gcs/gcs_server/gcs_server.cc
@@ -257,8 +257,8 @@ void GcsServer::InitGcsResourceManager(const GcsInitData &gcs_init_data) {
   gcs_resource_manager_ = std::make_shared<GcsResourceManager>(
       main_service_,
       gcs_table_storage_,
-      cluster_resource_scheduler_->GetClusterResourceManager());
-  scheduling::NodeID(local_node_id_.Binary());
+      cluster_resource_scheduler_->GetClusterResourceManager(),
+      scheduling::NodeID(local_node_id_.Binary()));
 
   // Initialize by gcs tables data.
   gcs_resource_manager_->Initialize(gcs_init_data);
