e6685b83c93c9fea2902cbffe484aa0e4ef8d2e1
Amog Kamsetty <amogkam@users.noreply.github.com>
2022-05-11 14:55:31 -0700

    Update python/setup.py
    Co-authored-by: Richard Liaw <rliaw@berkeley.edu>

--- python/setup.py
+++ python/setup.py
@@ -257,7 +257,7 @@ if setup_spec.type == SetupType.RAY:
 
     # Ray AI Runtime should encompass Data, Tune, and Serve.
     setup_spec.extras["air"] = (
-        setup_spec.extras["tune"] + setup_spec["data"] + setup_spec.extras["serve"]
+        setup_spec.extras["tune"] + setup_spec.extras["data"] + setup_spec.extras["serve"]
     )
 
     setup_spec.extras["all"] = list(
