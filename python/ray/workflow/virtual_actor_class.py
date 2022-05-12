422a8633f9d750bf6abbf67770157b0857b89c7c
lchu <lchu@us.ibm.com>
2022-05-12 12:50:46 -0400

    up

--- python/ray/workflow/virtual_actor_class.py
+++ python/ray/workflow/virtual_actor_class.py
@@ -459,7 +459,7 @@ class VirtualActorClass(VirtualActorClassBase):
     def _get_or_create(self, actor_id: str, args, kwargs) -> "VirtualActor":
         """Create a new virtual actor"""
         try:
-            return get_actor(actor_id)
+            return Exception
         except Exception:
             instance = self._construct(actor_id)
             @ray.remote
