0e8c3ff57a10cda82abb6cb6fb00e5d009ff7c85
lchu <lchu@us.ibm.com>
2022-05-12 13:45:39 -0400

    up

--- python/ray/workflow/virtual_actor_class.py
+++ python/ray/workflow/virtual_actor_class.py
@@ -434,13 +434,11 @@ class VirtualActorClass(VirtualActorClassBase):
 
     def get_or_create(self, actor_id: str, *args, **kwargs) -> "VirtualActor":
         """Create an actor. See `VirtualActorClassBase.create()`."""
-        # if ray._private.client_mode_hook.is_client_mode_enabled:
-        #     _cls = self._metadata.cls
-        #     @ray.remote
-        #     def get_remote(actor_id, args, kwargs):
-        #         actor_cls = VirtualActorClass._from_class(_cls)
-        #         return actor_cls._get_or_create(actor_id, args=args, kwargs=kwargs)
-        #     return ray.get(get_remote.remote(actor_id, args=args, kwargs=kwargs))
+        if ray._private.client_mode_hook.is_client_mode_enabled:
+            @ray.remote
+            def get_remote(actor, actor_id, args, kwargs):
+                return actor._get_or_create(actor_id, args=args, kwargs=kwargs)
+            return ray.get(get_remote.remote(self, actor_id, args=args, kwargs=kwargs))
 
         return self._get_or_create(actor_id, args=args, kwargs=kwargs)
 
@@ -459,16 +457,16 @@ class VirtualActorClass(VirtualActorClassBase):
     def _get_or_create(self, actor_id: str, args, kwargs) -> "VirtualActor":
         """Create a new virtual actor"""
         try:
-            # return get_actor(actor_id)
-            raise Exception
+            return get_actor(actor_id)
+            # raise Exception
         except Exception:
             instance = self._construct(actor_id)
-            @ray.remote
-            def f(va, actor_id, args, kwargs):
-                ins = va._construct(actor_id)
-                ins._create(args, kwargs)
-            ray.get(f.remote(self, actor_id, args, kwargs))
-            # instance._create(args, kwargs)
+            # @ray.remote
+            # def f(va, actor_id, args, kwargs):
+            #     ins = va._construct(actor_id)
+            #     ins._create(args, kwargs)
+            # ray.get(f.remote(self, actor_id, args, kwargs))
+            instance._create(args, kwargs)
             return instance
 
     def _construct(self, actor_id: str) -> "VirtualActor":
