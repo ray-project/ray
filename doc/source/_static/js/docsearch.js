42e877d2f79ffd35b5e3b7ba5df0d1db3eea1b25
Max Pumperla <max.pumperla@googlemail.com>
2022-05-12 19:00:35 +0200

    [docs] full results on enter, fixes #24519 (#24722)
    Signed-off-by: Max Pumperla <max.pumperla@googlemail.com>

--- doc/source/_static/js/docsearch.js
+++ doc/source/_static/js/docsearch.js
@@ -4,4 +4,26 @@ docsearch({
     appId: 'LBHF0PABBL',
     inputSelector: '#search-input',
     debug: false,
+    algoliaOptions: {
+        hitsPerPage: 10,
+    },
+    handleSelected: function (input, event, suggestion, datasetNumber, context) {
+        if (context.selectionMethod === 'click') {
+            input.setVal('');
+            const windowReference = window.open(suggestion.url, "_self");
+            windowReference.focus();
+        }
+    }
+});
+
+const searchInput = document.getElementById("search-input");
+searchInput.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {
+        var searchForm = document.getElementsByClassName("bd-search")[0];
+
+        const text = searchInput.value;
+        searchForm.action = "search.html?q=" + text;
+
+        searchForm.submit();
+    }
 });
