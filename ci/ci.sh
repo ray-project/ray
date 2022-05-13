b169661650b8d044fd374bd6ced7843fce41cd8a
Yi Cheng <chengyidna@gmail.com>
2022-05-13 19:09:47 +0000

    fix error

--- ci/ci.sh
+++ ci/ci.sh
@@ -194,8 +194,8 @@ test_large() {
   # shellcheck disable=SC2046
   bazel test --config=ci $(./ci/run/bazel_export_options) --test_env=CONDA_EXE --test_env=CONDA_PYTHON_EXE \
       --test_env=CONDA_SHLVL --test_env=CONDA_PREFIX --test_env=CONDA_DEFAULT_ENV --test_env=CONDA_PROMPT_MODIFIER \
-      --test_env=CI --test_tag_filters="large_size_python_tests_shard_${BUILDKITE_PARALLEL_JOB}" \
-      -- python/ray/tests/... "$@"
+      --test_env=CI --test_tag_filters="large_size_python_tests_shard_${BUILDKITE_PARALLEL_JOB}"  "$@" \
+      -- python/ray/tests/...
 }
 
 test_cpp() {
