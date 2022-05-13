ffcbb30552586216b119cbcf19bbe5c7a716d63b
kourosh hakhamaneshi <31483498+kouroshHakha@users.noreply.github.com>
2022-05-13 09:43:36 -0700

    [RLlib] Move from `agents` to `algorithms` - CQL, MARWIL, AlphaStar, MAML, Dreamer, MBMPO. (#24739)

--- rllib/algorithms/alpha_star/__init__.py
+++ rllib/algorithms/alpha_star/__init__.py
@@ -9,10 +9,3 @@ __all__ = [
     "AlphaStarTrainer",
     "DEFAULT_CONFIG",
 ]
-
-
-from ray.rllib.utils.deprecation import deprecation_warning
-
-deprecation_warning(
-    "ray.rllib.agents.alpha_star", "ray.rllib.algorithms.alpha_star", error=False
-)
