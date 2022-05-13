f1dea7dfd0694a806501d906c6cd7d7153e2800e
Rohan138 <rapotdar@purdue.edu>
2022-05-12 19:06:58 -0700

    Clean slate

--- rllib/agents/maddpg/maddpg.py
+++ rllib/agents/maddpg/maddpg.py
@@ -13,6 +13,7 @@ import logging
 from typing import Type
 
 from ray.rllib.agents.maddpg.maddpg_tf_policy import MADDPGTFPolicy
+from ray.rllib.agents.maddpg.maddpg_torch_policy import MADDPGTorchPolicy
 from ray.rllib.agents.dqn.dqn import DQNTrainer
 from ray.rllib.agents.trainer import COMMON_CONFIG, with_common_config
 from ray.rllib.policy.policy import Policy
--- rllib/agents/maddpg/maddpg_tf_policy.py
+++ rllib/agents/maddpg/maddpg_tf_policy.py
@@ -27,8 +27,6 @@ class MADDPGPostprocessing:
     def postprocess_trajectory(
         self, sample_batch, other_agent_batches=None, episode=None
     ):
-        # FIXME: Get done from info is required since agentwise done is not
-        #  supported now.
         sample_batch[SampleBatch.DONES] = self.get_done_from_info(
             sample_batch[SampleBatch.INFOS]
         )
@@ -43,7 +41,7 @@ class MADDPGPostprocessing:
 class MADDPGTFPolicy(MADDPGPostprocessing, TFPolicy):
     def __init__(self, obs_space, act_space, config):
         # _____ Initial Configuration
-        config = dict(ray.rllib.agents.maddpg.DEFAULT_CONFIG, **config)
+        config = dict(ray.rllib.contrib.maddpg.DEFAULT_CONFIG, **config)
         self.config = config
         self.global_step = tf1.train.get_or_create_global_step()
 
