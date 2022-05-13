09dc0c8b085307a7f5af44d5d6d3e11da954798f
simon-mo <simon.mo@hey.com>
2022-05-13 14:13:40 -0700

    Revert "[RLlib] Move from `agents` to `algorithms` - CQL, MARWIL, AlphaStar, MAML, Dreamer, MBMPO. (#24739)"
    This reverts commit ffcbb30552586216b119cbcf19bbe5c7a716d63b.

--- rllib/agents/cql/__init__.py
+++ rllib/agents/cql/__init__.py
@@ -1,14 +1,8 @@
-from ray.rllib.algorithms.cql.cql import CQLTrainer, CQL_DEFAULT_CONFIG
-from ray.rllib.algorithms.cql.cql_torch_policy import CQLTorchPolicy
+from ray.rllib.agents.cql.cql import CQLTrainer, CQL_DEFAULT_CONFIG
+from ray.rllib.agents.cql.cql_torch_policy import CQLTorchPolicy
 
 __all__ = [
     "CQL_DEFAULT_CONFIG",
     "CQLTorchPolicy",
     "CQLTrainer",
 ]
-
-from ray.rllib.utils.deprecation import deprecation_warning
-
-deprecation_warning(
-    "ray.rllib.agents.dreamer", "ray.rllib.algorithms.dreamer", error=False
-)
--- rllib/algorithms/mbmpo/__init__.py
+++
@@ -1,6 +0,0 @@
-from ray.rllib.algorithms.mbmpo.mbmpo import MBMPOTrainer, DEFAULT_CONFIG
-
-__all__ = [
-    "MBMPOTrainer",
-    "DEFAULT_CONFIG",
-]
