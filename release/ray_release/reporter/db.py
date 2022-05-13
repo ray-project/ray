3961aae517bc9570d385c3e02dac024bb0475c82
Jiajun Yao <jeromeyjj@gmail.com>
2022-05-13 15:52:38 -0700

    update

--- release/ray_release/reporter/db.py
+++ release/ray_release/reporter/db.py
@@ -31,7 +31,7 @@ class DBReporter(Reporter):
             "runtime": result.runtime or -1.0,
             "stable": result.stable,
             "return_code": result.return_code,
-            "smoke_test": test.get("_is_smoke_test", False),
+            "smoke_test": result.smoke_test,
         }
 
         logger.debug(f"Result json: {json.dumps(result_json)}")
