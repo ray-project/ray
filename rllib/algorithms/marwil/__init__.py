85cdfd4733afd018c011067436ccfb23b61afb01
rapotdar <rapotdar@purdue.edu>
2022-05-16 13:47:40 -0700

    marwil init

--- rllib/algorithms/marwil/__init__.py
+++ rllib/algorithms/marwil/__init__.py
@@ -1,10 +1,16 @@
-from ray.rllib.algorithms.marwil.bc import BCTrainer, BC_DEFAULT_CONFIG
-from ray.rllib.algorithms.marwil.marwil import MARWILTrainer, DEFAULT_CONFIG
+from ray.rllib.algorithms.marwil.bc import BCTrainer, BCConfig, BC_DEFAULT_CONFIG
+from ray.rllib.algorithms.marwil.marwil import (
+    MARWILTrainer,
+    MARWILConfig,
+    DEFAULT_CONFIG,
+)
 from ray.rllib.algorithms.marwil.marwil_tf_policy import MARWILTFPolicy
 from ray.rllib.algorithms.marwil.marwil_torch_policy import MARWILTorchPolicy
 
 __all__ = [
     "BCTrainer",
+    "BCConfig",
+    "MARWILConfig",
     "BC_DEFAULT_CONFIG",
     "DEFAULT_CONFIG",
     "MARWILTFPolicy",
