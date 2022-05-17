37901ec33f880f77688a11b92ddd842dad3f4351
SangBin Cho <rkooo567@gmail.com>
2022-05-17 09:27:16 -0700

    Fixed the issue.

--- ci/ci.sh
+++ ci/ci.sh
@@ -693,34 +693,34 @@ test_minimal() {
   ./ci/env/env_info.sh
   python ./ci/env/check_minimal_install.py
   BAZEL_EXPORT_OPTIONS="$(./ci/run/bazel_export_options)"
-  bazel test --test_output=streamed --config=ci --test_env=RAY_MINIMAL=1 "${BAZEL_EXPORT_OPTIONS}"
-    python/ray/tests/test_basic
-  bazel test --test_output=streamed --config=ci "${BAZEL_EXPORT_OPTIONS}"
-    python/ray/tests/test_basic_2
-  bazel test --test_output=streamed --config=ci "${BAZEL_EXPORT_OPTIONS}"
-    python/ray/tests/test_basic_3
-  bazel test --test_output=streamed --config=ci "${BAZEL_EXPORT_OPTIONS}"
-    python/ray/tests/test_basic_4
-  bazel test --test_output=streamed --config=ci "${BAZEL_EXPORT_OPTIONS}"
-    python/ray/tests/test_basic_5
-  bazel test --test_output=streamed --config=ci --test_env=RAY_MINIMAL=1 "${BAZEL_EXPORT_OPTIONS}"
-    python/ray/tests/test_output
-  bazel test --test_output=streamed --config=ci --test_env=RAY_MINIMAL=1 "${BAZEL_EXPORT_OPTIONS}"
-    python/ray/tests/test_runtime_env_ray_minimal
-  bazel test --test_output=streamed --config=ci "${BAZEL_EXPORT_OPTIONS}"
-    python/ray/tests/test_runtime_env
-  bazel test --test_output=streamed --config=ci "${BAZEL_EXPORT_OPTIONS}"
-    python/ray/tests/test_runtime_env_2
-  bazel test --test_output=streamed --config=ci "${BAZEL_EXPORT_OPTIONS}"
-    python/ray/tests/test_runtime_env_complicated
-  bazel test --test_output=streamed --config=ci "${BAZEL_EXPORT_OPTIONS}"
-    python/ray/tests/test_runtime_env_validation
-  bazel test --test_output=streamed --config=ci --test_env=RAY_MINIMAL=1 "${BAZEL_EXPORT_OPTIONS}"
-    python/ray/tests/test_serve_ray_minimal
-  bazel test --test_output=streamed --config=ci --test_env=RAY_MINIMAL=1 "${BAZEL_EXPORT_OPTIONS}"
-    python/ray/dashboard/test_dashboard
-  bazel test --test_output=streamed --config=ci --test_env=RAY_MINIMAL=1 "${BAZEL_EXPORT_OPTIONS}"
-    python/ray/tests/test_usage_stats
+  # shellcheck disable=SC2086
+  bazel test --test_output=streamed --config=ci --test_env=RAY_MINIMAL=1 ${BAZEL_EXPORT_OPTIONS} python/ray/tests/test_basic
+  # shellcheck disable=SC2086
+  bazel test --test_output=streamed --config=ci ${BAZEL_EXPORT_OPTIONS} python/ray/tests/test_basic_2
+  # shellcheck disable=SC2086
+  bazel test --test_output=streamed --config=ci ${BAZEL_EXPORT_OPTIONS} python/ray/tests/test_basic_3
+  # shellcheck disable=SC2086
+  bazel test --test_output=streamed --config=ci ${BAZEL_EXPORT_OPTIONS} python/ray/tests/test_basic_4
+  # shellcheck disable=SC2086
+  bazel test --test_output=streamed --config=ci ${BAZEL_EXPORT_OPTIONS} python/ray/tests/test_basic_5
+  # shellcheck disable=SC2086
+  bazel test --test_output=streamed --config=ci --test_env=RAY_MINIMAL=1 ${BAZEL_EXPORT_OPTIONS} python/ray/tests/test_output
+  # shellcheck disable=SC2086
+  bazel test --test_output=streamed --config=ci --test_env=RAY_MINIMAL=1 ${BAZEL_EXPORT_OPTIONS} python/ray/tests/test_runtime_env_ray_minimal
+  # shellcheck disable=SC2086
+  bazel test --test_output=streamed --config=ci ${BAZEL_EXPORT_OPTIONS} python/ray/tests/test_runtime_env
+  # shellcheck disable=SC2086
+  bazel test --test_output=streamed --config=ci ${BAZEL_EXPORT_OPTIONS} python/ray/tests/test_runtime_env_2
+  # shellcheck disable=SC2086
+  bazel test --test_output=streamed --config=ci ${BAZEL_EXPORT_OPTIONS} python/ray/tests/test_runtime_env_complicated
+  # shellcheck disable=SC2086
+  bazel test --test_output=streamed --config=ci ${BAZEL_EXPORT_OPTIONS} python/ray/tests/test_runtime_env_validation
+  # shellcheck disable=SC2086
+  bazel test --test_output=streamed --config=ci --test_env=RAY_MINIMAL=1 ${BAZEL_EXPORT_OPTIONS} python/ray/tests/test_serve_ray_minimal
+  # shellcheck disable=SC2086
+  bazel test --test_output=streamed --config=ci --test_env=RAY_MINIMAL=1 ${BAZEL_EXPORT_OPTIONS} python/ray/dashboard/test_dashboard
+  # shellcheck disable=SC2086
+  bazel test --test_output=streamed --config=ci --test_env=RAY_MINIMAL=1 ${BAZEL_EXPORT_OPTIONS} python/ray/tests/test_usage_stats
 }
 
 _main() {
