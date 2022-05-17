25001f6d8dc466ccc4aba373b41c896b6478d463
Sven Mika <svenmika1977@gmail.com>
2022-05-17 10:31:07 +0200

    [RLlib] APPO Training iteration fn. (#24545)

--- rllib/agents/ddpg/apex.py
+++ rllib/agents/ddpg/apex.py
@@ -32,6 +32,7 @@ APEX_DDPG_DEFAULT_CONFIG = DDPGTrainer.merge_trainer_configs(
         "learning_starts": 50000,
         "train_batch_size": 512,
         "rollout_fragment_length": 50,
+        # Update the target network every `target_network_update_freq` sample timesteps.
         "target_network_update_freq": 500000,
         "min_sample_timesteps_per_reporting": 25000,
         "worker_side_prioritization": True,
