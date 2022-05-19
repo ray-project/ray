b03e8b8538008c3af59058196cd3405b1b8d4a5c
SangBin Cho <rkooo567@gmail.com>
2022-05-19 16:09:03 -0700

    .

--- ci/build/test-wheels.sh
+++ ci/build/test-wheels.sh
@@ -48,11 +48,12 @@ function retry {
 
 if [[ "$platform" == "linux" ]]; then
   # Install miniconda.
-  PY_WHEEL_VERSIONS=("36" "37" "38" "39")
+  PY_WHEEL_VERSIONS=("36" "37" "38" "39" "310")
   PY_MMS=("3.6.13"
           "3.7.10"
           "3.8.10"
-          "3.9.5")
+          "3.9.5"
+          "3.10.4")
   wget --quiet "https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh" -O miniconda3.sh
   "${ROOT_DIR}"/../suppress_output bash miniconda3.sh -b -p "$HOME/miniconda3"
   export PATH="$HOME/miniconda3/bin:$PATH"
@@ -95,11 +96,12 @@ if [[ "$platform" == "linux" ]]; then
 
 elif [[ "$platform" == "macosx" ]]; then
   MACPYTHON_PY_PREFIX=/Library/Frameworks/Python.framework/Versions
-  PY_WHEEL_VERSIONS=("36" "37" "38" "39")
+  PY_WHEEL_VERSIONS=("36" "37" "38" "39" "310")
   PY_MMS=("3.6"
           "3.7"
           "3.8"
-          "3.9")
+          "3.9"
+          "3.10")
 
   for ((i=0; i<${#PY_MMS[@]}; ++i)); do
     PY_MM="${PY_MMS[i]}"
