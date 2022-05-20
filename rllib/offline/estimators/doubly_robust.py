b0830c198493de3e747c4d959233694f57eaea1d
rapotdar <rapotdar@purdue.edu>
2022-05-19 18:15:30 -0700

    wip

--- rllib/offline/estimators/doubly_robust.py
+++ rllib/offline/estimators/doubly_robust.py
@@ -1,8 +1,8 @@
 from ray.rllib.offline.estimators.off_policy_estimator import OffPolicyEstimate
-from ray.rllib.policy import Policy
 from ray.rllib.offline.estimators.direct_method import DirectMethod
 from ray.rllib.utils.annotations import override
 from ray.rllib.utils.typing import SampleBatchType
+import numpy as np
 
 
 class DoublyRobust(DirectMethod):
@@ -15,5 +15,29 @@ class DoublyRobust(DirectMethod):
     def estimate(self, batch: SampleBatchType) -> OffPolicyEstimate:
         self.check_can_estimate_for(batch)
         estimates = []
+        for episode in batch.split_by_episode():
+            rewards, old_prob = episode["rewards"], episode["action_prob"]
+            new_prob = self.action_prob(episode)
 
+            # calculate importance ratios
+            p = []
+            for t in range(episode.count):
+                if t == 0:
+                    pt_prev = 1.0
+                else:
+                    pt_prev = p[t - 1]
+                p.append(pt_prev * new_prob[t] / old_prob[t])
+
+            V_prev, V_DR = 0.0, 0.0
+            for t in range(episode.count):
+                V_prev += rewards[t] * self.gamma ** t
+
+            estimates.append(OffPolicyEstimate(
+                "doubly_robust",
+                {
+                    "V_prev": V_prev,
+                    "V_DR": V_DR,
+                    "V_gain_est": V_DR / max(1e-8, V_prev),
+                },
+            ))
         return estimates
