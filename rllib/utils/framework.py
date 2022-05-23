48e182482be5def4877cb95d664472daa6143776
sven1977 <svenmika1977@gmail.com>
2022-05-23 11:52:02 +0200

    wip.

--- rllib/utils/framework.py
+++ rllib/utils/framework.py
@@ -4,7 +4,7 @@ import os
 import sys
 from typing import Any, Optional
 
-from ray.rllib.utils.annotations import PublicAPI
+from ray.rllib.utils.annotations import DeveloperAPI, PublicAPI
 from ray.rllib.utils.deprecation import Deprecated
 from ray.rllib.utils.typing import TensorShape, TensorType
 
@@ -166,7 +166,7 @@ class _ModuleStub:
         raise ImportError("Could not import `torch`.")
 
 
-@DeveloperAPI
+@PublicAPI
 def try_import_torch(error: bool = False):
     """Tries importing torch and returns the module (or None).
 
