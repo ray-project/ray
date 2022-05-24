2ccf22994d15b69fc30d5f7b4ff9b36224906fe9
Balaji Veeramani <bveeramani@berkeley.edu>
2022-05-24 14:01:23 -0700

    Update tensorflow_predictor.py

--- python/ray/ml/predictors/integrations/tensorflow/tensorflow_predictor.py
+++ python/ray/ml/predictors/integrations/tensorflow/tensorflow_predictor.py
@@ -132,28 +132,6 @@ class TensorflowPredictor(Predictor):
         if self.preprocessor:
             data = self.preprocessor.transform_batch(data)
 
-        import numpy as np
-        def get_df_values(df: "pandas.DataFrame") -> np.ndarray:
-            # TODO(Clark): Support unsqueezing column dimension API, similar to
-            # to_torch().
-            try:
-                values = df.values
-            except ValueError as e:
-                import pandas as pd
-
-                # Pandas DataFrame.values doesn't support extension arrays in all
-                # supported Pandas versions, so we check to see if this DataFrame
-                # contains any extensions arrays and do a manual conversion if so.
-                # See https://github.com/pandas-dev/pandas/pull/43160.
-                if any(
-                    isinstance(dtype, pd.api.extensions.ExtensionDtype)
-                    for dtype in df.dtypes
-                ):
-                    values = np.stack([col.to_numpy() for _, col in df.items()], axis=1)
-                else:
-                    raise e from None
-            return values
-
         if isinstance(data, pd.DataFrame):
             if feature_columns:
                 data = data[feature_columns]
