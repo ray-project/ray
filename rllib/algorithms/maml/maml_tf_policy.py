3c60e3194a06ad1314e937dd2404444f384157b4
Jun Gong <jungong@anyscale.com>
2022-05-24 03:38:58 -0700

    fix

--- rllib/algorithms/maml/maml_tf_policy.py
+++ rllib/algorithms/maml/maml_tf_policy.py
@@ -379,7 +379,7 @@ def get_maml_tf_policy(base: type) -> type:
             base.enable_eager_execution_if_necessary()
 
             config = dict(ray.rllib.algorithms.maml.maml.DEFAULT_CONFIG, **config)
-            validate_config(self, config)
+            validate_config(config)
 
             # Initialize base class.
             base.__init__(
