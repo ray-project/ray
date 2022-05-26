a3cde55863ec741c31b072f0134554068772082d
Yi Cheng <chengyidna@gmail.com>
2022-05-26 23:34:11 +0000

    fix comment

--- python/ray/tests/test_gcs_ha_e2e.py
+++ python/ray/tests/test_gcs_ha_e2e.py
@@ -134,7 +134,7 @@ print(pids)
 assert len(pids) == {num_replicas}
 """
 
-
+@pytest.mark.skipif(sys.platform != "linux", reason="Only works on linux.")
 def test_ray_server_basic(docker_cluster):
     # This test covers the basic cases for gcs ha (serve ha)
     # - It starts the serve on worker nodes.
