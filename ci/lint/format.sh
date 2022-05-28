219ed1b0e6256a45271892661c1b9f10aa1e2cbd
Eric Liang <ekhliang@gmail.com>
2022-05-27 23:01:24 -0700

    update msg

--- ci/lint/format.sh
+++ ci/lint/format.sh
@@ -34,11 +34,12 @@ check_python_command_exist() {
 
 check_docstyle() {
     violations=$(git ls-files | grep '.py$' | xargs grep -E '^[ a-z_]+ \([a-zA-Z]*\): ')
-    code=$?
-    if [[ $code == 0 ]]; then
-        echo "=== Found docstyle violations ==="
+    if [[ -n "$violations" ]]; then
+        echo
+        echo "=== Found Ray docstyle violations ==="
         echo $violations
-        echo "Per the Google pydoc style guide, omit types from pydoc args as they are redundant: https://docs.ray.io/en/latest/ray-contribute/getting-involved.html#code-style "
+        echo
+        echo "Per the Google pydoc style, omit types from pydoc args as they are redundant: https://docs.ray.io/en/latest/ray-contribute/getting-involved.html#code-style "
         exit 1
     fi
 }
