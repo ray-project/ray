ab6c3027e507385da7559fe7ffecdf00a5c82923
Sven Mika <svenmika1977@gmail.com>
2022-05-28 09:54:47 +0200

    [RLlib] A2/3C policy sub-classing schema. (#25078)

--- rllib/agents/a3c/a3c.py
+++ rllib/agents/a3c/a3c.py
@@ -2,7 +2,6 @@ import logging
 from typing import Any, Dict, List, Optional, Type, Union
 
 from ray.actor import ActorHandle
-from ray.rllib.agents.a3c.a3c_tf_policy import A3CTFPolicy
 from ray.rllib.agents.trainer import Trainer
 from ray.rllib.agents.trainer_config import TrainerConfig
 from ray.rllib.evaluation.rollout_worker import RolloutWorker
@@ -182,8 +181,14 @@ class A3CTrainer(Trainer):
             from ray.rllib.agents.a3c.a3c_torch_policy import A3CTorchPolicy
 
             return A3CTorchPolicy
+        elif config["framework"] == "tf":
+            from ray.rllib.agents.a3c.a3c_tf_policy import A3CStaticGraphTFPolicy
+
+            return A3CStaticGraphTFPolicy
         else:
-            return A3CTFPolicy
+            from ray.rllib.agents.a3c.a3c_tf_policy import A3CEagerTFPolicy
+
+            return A3CEagerTFPolicy
 
     def training_iteration(self) -> ResultDict:
         # Shortcut.
