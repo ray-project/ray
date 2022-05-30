d86b1c0441a3b29aac998c5de5e0d3fa5c133f4e
SangBin Cho <rkooo567@gmail.com>
2022-05-29 18:25:27 -0700

    Fix a test failure.

--- python/ray/tests/test_state_api.py
+++ python/ray/tests/test_state_api.py
@@ -247,7 +247,7 @@ async def test_api_manager_list_pgs(state_api_manager):
     """
     Test limit
     """
-    assert len(data) == 2
+    assert len(result.result) == 2
     result = await state_api_manager.list_placement_groups(
         option=list_api_options(limit=1)
     )
