4a68143ec91c2dea045d067cb25c2deff96ae09c
Shreyas Krishnaswamy <shrekris@anyscale.com>
2022-05-31 15:54:45 -0700

    Reduce num_cpus in world deployment_graph

--- python/ray/serve/tests/test_config_files/world.py
+++ python/ray/serve/tests/test_config_files/world.py
@@ -2,12 +2,12 @@ from ray import serve
 from ray.serve.deployment_graph import RayServeDAGHandle
 
 
-@serve.deployment
+@serve.deployment(ray_actor_options={"num_cpus": 0.1})
 def f(*args):
     return "wonderful world"
 
 
-@serve.deployment
+@serve.deployment(ray_actor_options={"num_cpus": 0.1})
 class BasicDriver:
     def __init__(self, dag: RayServeDAGHandle):
         self.dag = dag
