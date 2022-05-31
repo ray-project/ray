052b88f786679624b6a716f965c50ce53df6e117
Shreyas Krishnaswamy <shrekris@anyscale.com>
2022-05-31 15:48:13 -0700

    Revert "Revert "[Serve] Deploy Serve deployment graphs via REST API (#25073)" (#25330)"
    This reverts commit 7754645c8301bcb426666d032c30815a308e6626.

--- python/ray/serve/common.py
+++ python/ray/serve/common.py
@@ -136,7 +136,7 @@ class StatusOverview:
         )
 
     @classmethod
-    def from_proto(cls, proto: StatusOverviewProto):
+    def from_proto(cls, proto: StatusOverviewProto) -> "StatusOverview":
 
         # Recreate Serve Application info
         app_status = ApplicationStatusInfo.from_proto(proto.app_status)
