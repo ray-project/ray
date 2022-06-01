4b4e73e54eb1ab8ac4c65eacabc9275bba5ca434
sven1977 <svenmika1977@gmail.com>
2022-06-01 07:10:57 +0200

    wip

--- rllib/agents/a3c/__init__.py
+++ rllib/agents/a3c/__init__.py
@@ -4,13 +4,18 @@ from ray.rllib.algorithms.a2c.a2c import (
     A2C_DEFAULT_CONFIG,
 )
 from ray.rllib.algorithms.a3c.a3c import A3CConfig, A3C as A3CTrainer, DEFAULT_CONFIG
+from ray.rllib.utils.deprecation import deprecation_warning
+
 
 __all__ = [
     "A2CConfig",
     "A2C_DEFAULT_CONFIG",  # deprecated
     "A2CTrainer",
-
     "A3CConfig",
     "A3CTrainer",
     "DEFAULT_CONFIG",  # A3C default config (deprecated)
 ]
+
+deprecation_warning(
+    "ray.rllib.agents.a3c", "ray.rllib.algorithms.[a3c|a2c]", error=False
+)
