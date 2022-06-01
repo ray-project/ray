97190e4574af7cb60e6b6e6d24396d253db19cd5
Jiao <sophchess@gmail.com>
2022-06-01 11:26:56 -0700

    [Deployment Graph] Remove _execute_impl and json serde code for DeploymentNode IR (#25331)

--- python/ray/serve/pipeline/deployment_function_node.py
+++ python/ray/serve/pipeline/deployment_function_node.py
@@ -1,15 +1,12 @@
 import inspect
 from typing import Any, Callable, Dict, List, Union
-from ray import ObjectRef
 
 from ray.experimental.dag.dag_node import DAGNode
 from ray.experimental.dag.format_utils import get_dag_node_str
-from ray.experimental.dag.constants import DAGNODE_TYPE_KEY
 from ray.serve.deployment import Deployment, schema_to_deployment
 from ray.serve.config import DeploymentConfig
 from ray.serve.handle import RayServeLazySyncHandle
 from ray.serve.schema import DeploymentSchema
-from ray.serve.utils import get_deployment_import_path
 
 
 class DeploymentFunctionNode(DAGNode):
@@ -92,48 +89,8 @@ class DeploymentFunctionNode(DAGNode):
             other_args_to_resolve=new_other_args_to_resolve,
         )
 
-    def _execute_impl(self, *args, **kwargs) -> ObjectRef:
-        """Executor of DeploymentFunctionNode by calling .remote() on the
-        deployment handle.
-
-        Deployment method always default to __call__.
-        """
-        return self._deployment_handle.remote(*self._bound_args, **self._bound_kwargs)
-
     def __str__(self) -> str:
         return get_dag_node_str(self, str(self._body))
 
     def get_deployment_name(self):
         return self._deployment_name
-
-    def get_import_path(self):
-        if (
-            "is_from_serve_deployment" in self._bound_other_args_to_resolve
-        ):  # built by serve top level api, this is ignored for serve.run
-            return "dummy"
-        return get_deployment_import_path(self._deployment)
-
-    def to_json(self) -> Dict[str, Any]:
-        return {
-            DAGNODE_TYPE_KEY: DeploymentFunctionNode.__name__,
-            "deployment_name": self.get_deployment_name(),
-            # Will be overriden by build()
-            "import_path": self.get_import_path(),
-            "args": self.get_args(),
-            "kwargs": self.get_kwargs(),
-            # .options() should not contain any DAGNode type
-            "options": self.get_options(),
-            "other_args_to_resolve": self.get_other_args_to_resolve(),
-        }
-
-    @classmethod
-    def from_json(cls, input_json):
-        assert input_json[DAGNODE_TYPE_KEY] == DeploymentFunctionNode.__name__
-        return cls(
-            input_json["import_path"],
-            input_json["deployment_name"],
-            input_json["args"],
-            input_json["kwargs"],
-            input_json["options"],
-            other_args_to_resolve=input_json["other_args_to_resolve"],
-        )
