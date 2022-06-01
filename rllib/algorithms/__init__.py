680f1dfcd4ed5caf15ed2e5e7730164059f860ea
sven1977 <svenmika1977@gmail.com>
2022-06-01 20:56:07 +0200

    wip.

--- rllib/algorithms/__init__.py
+++ rllib/algorithms/__init__.py
@@ -2,6 +2,8 @@ from ray.rllib.algorithms.a2c.a2c import A2C, A2CConfig
 from ray.rllib.algorithms.a3c.a3c import A3C, A3CConfig
 from ray.rllib.algorithms.alpha_star.alpha_star import AlphaStarTrainer, AlphaStarConfig
 from ray.rllib.algorithms.alpha_zero.alpha_zero import AlphaZeroTrainer, AlphaZeroConfig
+from ray.rllib.algorithms.apex_ddpg.apex_ddpg import ApexDDPG, ApexDDPGConfig
+from ray.rllib.algorithms.apex_dqn.apex_dqn import ApexDQN, ApexDQNConfig
 from ray.rllib.algorithms.appo.appo import APPO, APPOConfig
 from ray.rllib.algorithms.ars.ars import ARSTrainer, ARSConfig
 from ray.rllib.algorithms.bandit.bandit import (
@@ -10,22 +12,26 @@ from ray.rllib.algorithms.bandit.bandit import (
     BanditLinUCBTrainer,
     BanditLinUCBConfig,
 )
+from ray.rllib.algorithms.bc.bc import BC, BCConfig
 from ray.rllib.algorithms.cql.cql import CQLTrainer, CQLConfig
 from ray.rllib.algorithms.ddpg.ddpg import DDPGTrainer, DDPGConfig
 from ray.rllib.algorithms.ddppo.ddppo import DDPPO, DDPPOConfig
-from ray.rllib.algorithms.dqn.dqn import DQNTrainer, DQNConfig
+from ray.rllib.algorithms.dqn.dqn import DQN, DQNConfig
 from ray.rllib.algorithms.dreamer.dreamer import DREAMERTrainer, DREAMERConfig
 from ray.rllib.algorithms.es.es import ESTrainer, ESConfig
 from ray.rllib.algorithms.impala.impala import Impala, ImpalaConfig
 from ray.rllib.algorithms.maddpg.maddpg import MADDPGTrainer, MADDPGConfig
 from ray.rllib.algorithms.maml.maml import MAMLTrainer, MAMLConfig
-from ray.rllib.algorithms.marwil.marwil import MARWILTrainer, MARWILConfig
+from ray.rllib.algorithms.marwil.marwil import MARWIL, MARWILConfig
 from ray.rllib.algorithms.mbmpo.mbmpo import MBMPOTrainer, MBMPOConfig
 from ray.rllib.algorithms.pg.pg import PGTrainer, PGConfig
 from ray.rllib.algorithms.ppo.ppo import PPO, PPOConfig
 from ray.rllib.algorithms.qmix.qmix import QMixTrainer, QMixConfig
+from ray.rllib.algorithms.r2d2.r2d2 import R2D2Trainer, R2D2Config
 from ray.rllib.algorithms.sac.sac import SACTrainer, SACConfig
+from ray.rllib.algorithms.simple_q.simple_q import SimpleQ, SimpleQConfig
 from ray.rllib.algorithms.slateq.slateq import SlateQTrainer, SlateQConfig
+from ray.rllib.algorithms.td3.td3 import TD3, TD3Config
 
 
 __all__ = [
