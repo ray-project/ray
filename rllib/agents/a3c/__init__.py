759b21487865cbd567882f9f350845d27c138b18
sven1977 <svenmika1977@gmail.com>
2022-06-01 05:52:01 +0200

    LINT

--- rllib/agents/a3c/__init__.py
+++ rllib/agents/a3c/__init__.py
@@ -1,4 +1,8 @@
-from ray.rllib.algorithms.a2c.a2c import A2CConfig, A2C as A2CTrainer, A2C_DEFAULT_CONFIG
+from ray.rllib.algorithms.a2c.a2c import (
+    A2CConfig,
+    A2C as A2CTrainer,
+    A2C_DEFAULT_CONFIG,
+)
 from ray.rllib.algorithms.a3c.a3c import A3CConfig, A3C as A3CTrainer, DEFAULT_CONFIG
 
 __all__ = [
