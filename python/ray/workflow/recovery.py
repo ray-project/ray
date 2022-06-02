f3ffd329fe6af3325f456236344dacfceb888f14
Siyuan <suquark@gmail.com>
2022-06-02 01:11:48 -0700

    lint

--- python/ray/workflow/recovery.py
+++ python/ray/workflow/recovery.py
@@ -1,4 +1,4 @@
-from typing import List, Any, Union, Dict, Callable, Tuple, Optional
+from typing import List, Any, Union, Dict, Callable, Optional
 from collections import deque, defaultdict
 
 import ray
