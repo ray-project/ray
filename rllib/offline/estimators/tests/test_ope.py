bcd5c6060135fe277fd1cc8d56e9c5b06a22c110
rapotdar <rapotdar@purdue.edu>
2022-06-03 16:11:20 -0700

    Split up and shorten OPE tests

--- rllib/offline/estimators/tests/test_ope.py
+++ rllib/offline/estimators/tests/test_ope.py
@@ -14,37 +14,27 @@ import os
 import numpy as np
 import gym
 
-from ray.rllib.offline.estimators.off_policy_estimator import OffPolicyEstimator
-
 
 class TestOPE(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         ray.init(num_cpus=4)
-
-    @classmethod
-    def tearDownClass(cls):
-        ray.shutdown()
-
-    def test_build_ope_methods(self):
         rllib_dir = Path(__file__).parent.parent.parent.parent
         print("rllib dir={}".format(rllib_dir))
         data_file = os.path.join(rllib_dir, "tests/data/cartpole/large.json")
         print("data_file={} exists={}".format(data_file, os.path.isfile(data_file)))
 
         env_name = "CartPole-v0"
-        gamma = 0.99
+        cls.gamma = 0.99
         train_steps = 20000
-        n_batches = 10  # Approx. equal to n_episodes
+        n_batches = 20  # Approx. equal to n_episodes
         n_eval_episodes = 100
 
         config = (
             DQNConfig()
             .environment(env=env_name)
-            .offline_data(
-                input_=data_file,
-                off_policy_estimation_methods={"simulation": {"type": "simulation"}},
-            )
+            .training(gamma=cls.gamma)
+            .rollouts(num_rollout_workers=3)
             .exploration(
                 explore=True,
                 exploration_config={
@@ -55,7 +45,7 @@ class TestOPE(unittest.TestCase):
             .framework("torch")
             .rollouts(batch_mode="complete_episodes")
         )
-        trainer = config.build()
+        cls.trainer = config.build()
 
         # Train DQN for evaluation policy
         tune.run(
@@ -67,33 +57,14 @@ class TestOPE(unittest.TestCase):
 
         # Read n_batches of data
         reader = JsonReader(data_file)
-        batch = reader.next()
+        cls.batch = reader.next()
         for _ in range(n_batches - 1):
-            batch = batch.concat(reader.next())
-        n_episodes = len(batch.split_by_episode())
-        print("Episodes:", n_episodes, "Steps:", batch.count)
-        estimators = {
-            "is": {"type": ImportanceSampling},
-            "wis": {"type": WeightedImportanceSampling},
-            "dm_qreg": {
-                "type": DirectMethod,
-                "q_model_type": "qreg",
-            },
-            "dm_fqe": {
-                "type": DirectMethod,
-                "q_model_type": "fqe",
-            },
-            "dr_qreg": {
-                "type": DoublyRobust,
-                "q_model_type": "qreg",
-            },
-            "dr_fqe": {
-                "type": DoublyRobust,
-                "q_model_type": "fqe",
-            },
-        }
-        mean_ret = {}
-        std_ret = {}
+            cls.batch = cls.batch.concat(reader.next())
+        cls.n_episodes = len(cls.batch.split_by_episode())
+        print("Episodes:", cls.n_episodes, "Steps:", cls.batch.count)
+
+        cls.mean_ret = {}
+        cls.std_ret = {}
 
         # Simulate Monte-Carlo rollouts
         mc_ret = []
@@ -103,36 +74,111 @@ class TestOPE(unittest.TestCase):
             done = False
             rewards = []
             while not done:
-                act = trainer.compute_single_action(obs)
+                act = cls.trainer.compute_single_action(obs)
                 obs, reward, done, _ = env.step(act)
                 rewards.append(reward)
             ret = 0
             for r in reversed(rewards):
-                ret = r + gamma * ret
+                ret = r + cls.gamma * ret
             mc_ret.append(ret)
 
-        mean_ret["simulation"] = np.mean(mc_ret)
-        std_ret["simulation"] = np.std(mc_ret)
+        cls.mean_ret["simulation"] = np.mean(mc_ret)
+        cls.std_ret["simulation"] = np.std(mc_ret)
 
-        print("Discounted reward:", np.mean(mc_ret), "+-", np.std(mc_ret))
+        # Optional configs for the model-based estimators
+        cls.model_config = {"k": 2, "n_iters": 10}
 
-        # Run estimators on data
-        for name, method_config in estimators.items():
-            estimator_cls = method_config.pop("type")
-            estimator: OffPolicyEstimator = estimator_cls(
-                name=name,
-                policy=trainer.get_policy(),
-                gamma=gamma,
-                **method_config,
-            )
-            estimator.process(batch)
-            estimates = estimator.get_metrics()
-            assert len(estimates) == n_episodes
-            mean_ret[name] = np.mean([e.metrics["v_new"] for e in estimates])
-            std_ret[name] = np.std([e.metrics["v_new"] for e in estimates])
+    @classmethod
+    def tearDownClass(cls):
+        print("Mean:", cls.mean_ret)
+        print("Stddev:", cls.std_ret)
+        ray.shutdown()
 
-        print("mean: ", mean_ret)
-        print("stddev: ", std_ret)
+    def test_is(self):
+        name = "is"
+        estimator = ImportanceSampling(
+            name=name,
+            policy=self.trainer.get_policy(),
+            gamma=self.gamma,
+        )
+        estimator.process(self.batch)
+        estimates = estimator.get_metrics()
+        assert len(estimates) == self.n_episodes
+        self.mean_ret[name] = np.mean([e.metrics["v_new"] for e in estimates])
+        self.std_ret[name] = np.std([e.metrics["v_new"] for e in estimates])
+
+    def test_wis(self):
+        name = "wis"
+        estimator = WeightedImportanceSampling(
+            name=name,
+            policy=self.trainer.get_policy(),
+            gamma=self.gamma,
+        )
+        estimator.process(self.batch)
+        estimates = estimator.get_metrics()
+        assert len(estimates) == self.n_episodes
+        self.mean_ret[name] = np.mean([e.metrics["v_new"] for e in estimates])
+        self.std_ret[name] = np.std([e.metrics["v_new"] for e in estimates])
+
+    def test_dm_qreg(self):
+        name = "dm_qreg"
+        estimator = DirectMethod(
+            name=name,
+            policy=self.trainer.get_policy(),
+            gamma=self.gamma,
+            q_model_type="qreg",
+            **self.model_config,
+        )
+        estimator.process(self.batch)
+        estimates = estimator.get_metrics()
+        assert len(estimates) == self.n_episodes
+        self.mean_ret[name] = np.mean([e.metrics["v_new"] for e in estimates])
+        self.std_ret[name] = np.std([e.metrics["v_new"] for e in estimates])
+
+    def test_dm_fqe(self):
+        name = "dm_fqe"
+        estimator = DirectMethod(
+            name=name,
+            policy=self.trainer.get_policy(),
+            gamma=self.gamma,
+            q_model_type="fqe",
+            **self.model_config,
+        )
+        estimator.process(self.batch)
+        estimates = estimator.get_metrics()
+        assert len(estimates) == self.n_episodes
+        self.mean_ret[name] = np.mean([e.metrics["v_new"] for e in estimates])
+        self.std_ret[name] = np.std([e.metrics["v_new"] for e in estimates])
+
+    def test_dr_qreg(self):
+        name = "dr_qreg"
+        estimator = DoublyRobust(
+            name=name,
+            policy=self.trainer.get_policy(),
+            gamma=self.gamma,
+            q_model_type="qreg",
+            **self.model_config,
+        )
+        estimator.process(self.batch)
+        estimates = estimator.get_metrics()
+        assert len(estimates) == self.n_episodes
+        self.mean_ret[name] = np.mean([e.metrics["v_new"] for e in estimates])
+        self.std_ret[name] = np.std([e.metrics["v_new"] for e in estimates])
+
+    def test_dr_fqe(self):
+        name = "dr_fqe"
+        estimator = DoublyRobust(
+            name=name,
+            policy=self.trainer.get_policy(),
+            gamma=self.gamma,
+            q_model_type="fqe",
+            **self.model_config,
+        )
+        estimator.process(self.batch)
+        estimates = estimator.get_metrics()
+        assert len(estimates) == self.n_episodes
+        self.mean_ret[name] = np.mean([e.metrics["v_new"] for e in estimates])
+        self.std_ret[name] = np.std([e.metrics["v_new"] for e in estimates])
 
     def test_ope_in_trainer(self):
         # TODO (rohan): Add performance tests for off_policy_estimation_methods,
