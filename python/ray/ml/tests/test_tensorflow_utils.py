b196645d3c43724d9b23ccb164050ed4ad5e68e6
Balaji Veeramani <bveeramani@berkeley.edu>
2022-06-03 10:10:46 -0700

    Add test

--- python/ray/ml/tests/test_tensorflow_utils.py
+++ python/ray/ml/tests/test_tensorflow_utils.py
@@ -11,11 +11,19 @@ def test_convert_simple_df_to_tensor():
 
     actual_tensor = convert_pandas_to_tf_tensor(df)
     expected_tensor = tf.constant([[1, 2], [3, 4]], dtype=tf.dtypes.int64)
-    print(actual_tensor.dtype, expected_tensor.dtype)
     tf.debugging.assert_equal(actual_tensor, expected_tensor)
 
     actual_tensor = convert_pandas_to_tf_tensor(df[["X1"]])
     expected_tensor = tf.constant([[1], [3]], dtype=tf.dtypes.int64)
+    tf.debugging.assert_equal(actual_tensor, expected_tensor)
+
+
+def test_convert_simple_df_to_tensor_with_dtype():
+    df = pd.DataFrame({"X1": [1, 3], "X2": [2, 4]})
+
+    actual_tensor = convert_pandas_to_tf_tensor(df, dtype=tf.dtypes.float16)
+
+    expected_tensor = tf.constant([[1, 2], [3, 4]], dtype=tf.dtypes.float16)
     tf.debugging.assert_equal(actual_tensor, expected_tensor)
 
 
