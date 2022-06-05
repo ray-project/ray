6e1248fb37c15c42c754f652c197463a0192052e
Sebastián Ramírez <tiangolo@gmail.com>
2022-06-05 16:01:24 -0500

    🚚 Move worker types to the module to improve static analysis (#25439)
    Currently, there are separated type annotations in `worker.pyi` that include the types for `func.remote()`, but they don't include types for the other things declared in `worker.py`. Because of that, editors can end up showing support only for the things in the `worker.pyi` file.
    
    For example:
    
    <img width="349" alt="Screenshot 2022-06-03 at 06 01 36" src="https://user-images.githubusercontent.com/1326112/171841977-ec7a0b9a-b4a5-4422-acd9-b73c1e263261.png">
    
    After this change, the editor and other tools will be able to provide support for other things defined in the same file:
    
    <img width="760" alt="Screenshot 2022-06-03 at 06 04 24" src="https://user-images.githubusercontent.com/1326112/171842204-1915dd2a-6cc6-41b7-8785-5124beec37e8.png">
    
    And the typing support for `func.remote()` keeps working as before:
    
    <img width="760" alt="Screenshot 2022-06-03 at 06 07 15" src="https://user-images.githubusercontent.com/1326112/171842528-f318753e-9f47-4236-b0a4-d86d00c0bb11.png">
    
    This is the recommended approach by PyRight/Pylance/VS Code. I also recommend it as it's a lot easier to maintain types in the same file while editing than remembering to go to an external independent file to add those types. Also, to have proper support when using an external `.pyi` file *all* the things declared in `worker.py` would have to be declared in the `worker.pyi` file.
    
    Ref: https://github.com/microsoft/pyright/blob/main/docs/typed-libraries.md#inlined-type-annotations-and-type-stubs

--- python/ray/worker.py
+++ python/ray/worker.py
@@ -16,7 +16,20 @@ import warnings
 from abc import ABCMeta, abstractmethod
 from collections.abc import Mapping
 from dataclasses import dataclass
-from typing import Any, Callable, Dict, Iterator, List, Optional, Tuple, Union
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Generic,
+    Iterator,
+    List,
+    Optional,
+    Sequence,
+    Tuple,
+    TypeVar,
+    Union,
+    overload,
+)
 
 # Ray modules
 import ray.cloudpickle as pickle
@@ -87,6 +100,143 @@ RESTORE_WORKER_MODE = 4
 # into the program using Ray. Ray provides a default configuration at
 # entry/init points.
 logger = logging.getLogger(__name__)
+
+
+T0 = TypeVar("T0")
+T1 = TypeVar("T1")
+T2 = TypeVar("T2")
+T3 = TypeVar("T3")
+T4 = TypeVar("T4")
+T5 = TypeVar("T5")
+T6 = TypeVar("T6")
+T7 = TypeVar("T7")
+T8 = TypeVar("T8")
+T9 = TypeVar("T9")
+R = TypeVar("R")
+
+
+class RemoteFunction(Generic[R, T0, T1, T2, T3, T4, T5, T6, T7, T8, T9]):
+    def __init__(
+        self, function: Callable[[T0, T1, T2, T3, T4, T5, T6, T7, T8, T9], R]
+    ) -> None:
+        pass
+
+    @overload
+    def remote(self) -> "ObjectRef[R]":
+        ...
+
+    @overload
+    def remote(self, arg0: "Union[T0, ObjectRef[T0]]") -> "ObjectRef[R]":
+        ...
+
+    @overload
+    def remote(
+        self, arg0: "Union[T0, ObjectRef[T0]]", arg1: "Union[T1, ObjectRef[T1]]"
+    ) -> "ObjectRef[R]":
+        ...
+
+    @overload
+    def remote(
+        self,
+        arg0: "Union[T0, ObjectRef[T0]]",
+        arg1: "Union[T1, ObjectRef[T1]]",
+        arg2: "Union[T2, ObjectRef[T2]]",
+    ) -> "ObjectRef[R]":
+        ...
+
+    @overload
+    def remote(
+        self,
+        arg0: "Union[T0, ObjectRef[T0]]",
+        arg1: "Union[T1, ObjectRef[T1]]",
+        arg2: "Union[T2, ObjectRef[T2]]",
+        arg3: "Union[T3, ObjectRef[T3]]",
+    ) -> "ObjectRef[R]":
+        ...
+
+    @overload
+    def remote(
+        self,
+        arg0: "Union[T0, ObjectRef[T0]]",
+        arg1: "Union[T1, ObjectRef[T1]]",
+        arg2: "Union[T2, ObjectRef[T2]]",
+        arg3: "Union[T3, ObjectRef[T3]]",
+        arg4: "Union[T4, ObjectRef[T4]]",
+    ) -> "ObjectRef[R]":
+        ...
+
+    @overload
+    def remote(
+        self,
+        arg0: "Union[T0, ObjectRef[T0]]",
+        arg1: "Union[T1, ObjectRef[T1]]",
+        arg2: "Union[T2, ObjectRef[T2]]",
+        arg3: "Union[T3, ObjectRef[T3]]",
+        arg4: "Union[T4, ObjectRef[T4]]",
+        arg5: "Union[T5, ObjectRef[T5]]",
+    ) -> "ObjectRef[R]":
+        ...
+
+    @overload
+    def remote(
+        self,
+        arg0: "Union[T0, ObjectRef[T0]]",
+        arg1: "Union[T1, ObjectRef[T1]]",
+        arg2: "Union[T2, ObjectRef[T2]]",
+        arg3: "Union[T3, ObjectRef[T3]]",
+        arg4: "Union[T4, ObjectRef[T4]]",
+        arg5: "Union[T5, ObjectRef[T5]]",
+        arg6: "Union[T6, ObjectRef[T6]]",
+    ) -> "ObjectRef[R]":
+        ...
+
+    @overload
+    def remote(
+        self,
+        arg0: "Union[T0, ObjectRef[T0]]",
+        arg1: "Union[T1, ObjectRef[T1]]",
+        arg2: "Union[T2, ObjectRef[T2]]",
+        arg3: "Union[T3, ObjectRef[T3]]",
+        arg4: "Union[T4, ObjectRef[T4]]",
+        arg5: "Union[T5, ObjectRef[T5]]",
+        arg6: "Union[T6, ObjectRef[T6]]",
+        arg7: "Union[T7, ObjectRef[T7]]",
+    ) -> "ObjectRef[R]":
+        ...
+
+    @overload
+    def remote(
+        self,
+        arg0: "Union[T0, ObjectRef[T0]]",
+        arg1: "Union[T1, ObjectRef[T1]]",
+        arg2: "Union[T2, ObjectRef[T2]]",
+        arg3: "Union[T3, ObjectRef[T3]]",
+        arg4: "Union[T4, ObjectRef[T4]]",
+        arg5: "Union[T5, ObjectRef[T5]]",
+        arg6: "Union[T6, ObjectRef[T6]]",
+        arg7: "Union[T7, ObjectRef[T7]]",
+        arg8: "Union[T8, ObjectRef[T8]]",
+    ) -> "ObjectRef[R]":
+        ...
+
+    @overload
+    def remote(
+        self,
+        arg0: "Union[T0, ObjectRef[T0]]",
+        arg1: "Union[T1, ObjectRef[T1]]",
+        arg2: "Union[T2, ObjectRef[T2]]",
+        arg3: "Union[T3, ObjectRef[T3]]",
+        arg4: "Union[T4, ObjectRef[T4]]",
+        arg5: "Union[T5, ObjectRef[T5]]",
+        arg6: "Union[T6, ObjectRef[T6]]",
+        arg7: "Union[T7, ObjectRef[T7]]",
+        arg8: "Union[T8, ObjectRef[T8]]",
+        arg9: "Union[T9, ObjectRef[T9]]",
+    ) -> "ObjectRef[R]":
+        ...
+
+    def remote(self, *args, **kwargs) -> "ObjectRef[R]":
+        ...
 
 
 # Visible for testing.
@@ -1770,10 +1920,29 @@ def show_in_dashboard(message: str, key: str = "", dtype: str = "text"):
 blocking_get_inside_async_warned = False
 
 
+@overload
+def get(
+    object_refs: "Sequence[ObjectRef[Any]]", *, timeout: Optional[float] = None
+) -> List[Any]:
+    ...
+
+
+@overload
+def get(
+    object_refs: "Sequence[ObjectRef[R]]", *, timeout: Optional[float] = None
+) -> List[R]:
+    ...
+
+
+@overload
+def get(object_refs: "ObjectRef[R]", *, timeout: Optional[float] = None) -> R:
+    ...
+
+
 @PublicAPI
 @client_mode_hook(auto_init=True)
 def get(
-    object_refs: Union[ray.ObjectRef, List[ray.ObjectRef]],
+    object_refs: Union[ray.ObjectRef, Sequence[ray.ObjectRef]],
     *,
     timeout: Optional[float] = None,
 ) -> Union[Any, List[Any]]:
@@ -2161,6 +2330,90 @@ def _make_remote(function_or_class, options):
     raise TypeError(
         "The @ray.remote decorator must be applied to either a function or a class."
     )
+
+
+@overload
+def remote(
+    function: Callable[[], R]
+) -> RemoteFunction[R, None, None, None, None, None, None, None, None, None, None]:
+    ...
+
+
+@overload
+def remote(
+    function: Callable[[T0], R]
+) -> RemoteFunction[R, T0, None, None, None, None, None, None, None, None, None]:
+    ...
+
+
+@overload
+def remote(
+    function: Callable[[T0, T1], R]
+) -> RemoteFunction[R, T0, T1, None, None, None, None, None, None, None, None]:
+    ...
+
+
+@overload
+def remote(
+    function: Callable[[T0, T1, T2], R]
+) -> RemoteFunction[R, T0, T1, T2, None, None, None, None, None, None, None]:
+    ...
+
+
+@overload
+def remote(
+    function: Callable[[T0, T1, T2, T3], R]
+) -> RemoteFunction[R, T0, T1, T2, T3, None, None, None, None, None, None]:
+    ...
+
+
+@overload
+def remote(
+    function: Callable[[T0, T1, T2, T3, T4], R]
+) -> RemoteFunction[R, T0, T1, T2, T3, T4, None, None, None, None, None]:
+    ...
+
+
+@overload
+def remote(
+    function: Callable[[T0, T1, T2, T3, T4, T5], R]
+) -> RemoteFunction[R, T0, T1, T2, T3, T4, T5, None, None, None, None]:
+    ...
+
+
+@overload
+def remote(
+    function: Callable[[T0, T1, T2, T3, T4, T5, T6], R]
+) -> RemoteFunction[R, T0, T1, T2, T3, T4, T5, T6, None, None, None]:
+    ...
+
+
+@overload
+def remote(
+    function: Callable[[T0, T1, T2, T3, T4, T5, T6, T7], R]
+) -> RemoteFunction[R, T0, T1, T2, T3, T4, T5, T6, T7, None, None]:
+    ...
+
+
+@overload
+def remote(
+    function: Callable[[T0, T1, T2, T3, T4, T5, T6, T7, T8], R]
+) -> RemoteFunction[R, T0, T1, T2, T3, T4, T5, T6, T7, T8, None]:
+    ...
+
+
+@overload
+def remote(
+    function: Callable[[T0, T1, T2, T3, T4, T5, T6, T7, T8, T9], R]
+) -> RemoteFunction[R, T0, T1, T2, T3, T4, T5, T6, T7, T8, T9]:
+    ...
+
+
+# Pass on typing actors for now. The following makes it so no type errors
+# are generated for actors.
+@overload
+def remote(t: type) -> Any:
+    ...
 
 
 @PublicAPI
