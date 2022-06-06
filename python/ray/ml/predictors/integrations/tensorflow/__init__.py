487855eee676eab3b6f0a382538c8faf6b821995
Xiaowei Jiang <xwjiang2010@gmail.com>
2022-06-06 12:07:53 -0700

    remove file

--- python/ray/ml/predictors/integrations/tensorflow/__init__.py
+++
@@ -1,6 +0,0 @@
-from ray.ml.predictors.integrations.tensorflow.tensorflow_predictor import (
-    TensorflowPredictor,
-)
-from ray.ml.predictors.integrations.tensorflow.utils import to_air_checkpoint
-
-__all__ = ["TensorflowPredictor", "to_air_checkpoint"]
