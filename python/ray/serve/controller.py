ad7d0143fa1afa3bae459b4d57accec071c35e74
Shreyas Krishnaswamy <shrekris@anyscale.com>
2022-06-07 15:12:11 -0700

    Simplify code that searches for ray_actor_options

--- python/ray/serve/controller.py
+++ python/ray/serve/controller.py
@@ -586,13 +586,14 @@ def run_graph(
     for options in deployment_override_options:
         name = options["name"]
 
-        # Merge graph-level and deployment-level runtime_envs
-        ray_actor_options = options.get("ray_actor_options")
-        if ray_actor_options is None:
-            if app.deployments[name].ray_actor_options:
-                ray_actor_options = app.deployments[name].ray_actor_options
-            else:
-                ray_actor_options = {}
+        # Merge graph-level and deployment-level runtime_envs. This logic
+        # gets the deployment's ray_actor_options from the config, if
+        # specified. If not specified there, it gets the options from the graph
+        # code, and if there are no options there, it defaults to an empty dict.
+        if "ray_actor_options" in options:
+            ray_actor_options = options["ray_actor_options"]
+        else:
+            ray_actor_options = app.deployments[name].ray_actor_options or {}
 
         deployment_env = ray_actor_options.get("runtime_env", {})
         merged_env = merge_runtime_envs(graph_env, deployment_env)
