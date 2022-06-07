aef809f8f8d5b32c14869ca8aa546fe10fb21e51
Lixin Wei <wlx65005@gmail.com>
2022-06-07 17:33:54 +0800

    draft

--- python/ray/serialization.py
+++ python/ray/serialization.py
@@ -1,6 +1,7 @@
 import logging
 import threading
 import traceback
+from abc import ABC, abstractmethod
 
 import ray.cloudpickle as pickle
 from ray import ray_constants
@@ -419,3 +420,61 @@ class SerializationContext:
             return RawSerializedObject(value)
         else:
             return self._serialize_to_msgpack(value)
+
+
+class RaySerializer(ABC):
+    @abstractmethod
+    def serialize(self, instance) -> RaySerializationResult:
+        pass
+
+    @abstractmethod
+    def deserialize(
+        self, in_band_buffer: bytes, oob_buffers: Map[str, Map[int, memoryview]]
+    ):
+        pass
+
+
+class RaySerializationResult:
+    type_id: str
+    in_band_buffer: bytes
+    # type ID -> buffer ID -> buffer
+    out_of_band_buffers: Optional[Map[str, Map[int, memoryview]]]
+
+
+# python type -> (type ID, serializer)
+_ray_serializer_map: Map[type, Tuple[str, RaySerializer]] = {}
+# type ID -> python type
+_ray_type_id_to_type: Map[str, type] = {}
+
+
+def _register_serializer(type_id: str, class_type: type, serializer: RaySerializer):
+    _ray_serializer_map[class_type] = (serializer_id, serializer)
+    _ray_type_id_to_type[serializer_id] = class_type
+
+
+def _get_serializer(serializer_indicator) -> RaySerializer:
+    try:
+        if type(serializer_indicator) is type:
+            return _ray_serializer_map[serializer_indicator][1]
+        elif type(serializer_indicator) is str:
+            class_type = _ray_type_id_to_type[serializer_indicator]
+            return _ray_serializer_map[class_type][1]
+        else:
+            raise TypeError(
+                f"Can't get serializer by {str(type(str))}, a type or str should be provided!"
+            )
+    except KeyError:
+        logger.exception(f"Can't find a serializer by {serializer_indicator}!")
+
+
+def _serialize(instance) -> RaySerializationResult:
+    class_type = type(instance)
+    return _get_serializer(class_type).serialize(instance)
+
+
+def _deserialize(ray_serialization_result: RaySerializationResult):
+    serializer = _get_serializer(ray_serialization_result.type_id)
+    return serializer.deserialize(
+        ray_serialization_result.in_band_buffer,
+        ray_serialization_result.out_of_band_buffers,
+    )
