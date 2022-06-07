20c8aade5862598455d59c17845d160ea5a7b387
Amog Kamsetty <amogkamsetty@yahoo.com>
2022-06-07 15:52:15 -0700

    add missing file

---
+++ python/ray/train/error.py
@@ -0,0 +1,5 @@
+from ray.util.annotations import PublicAPI
+
+@PublicAPI(stability="beta")
+class SessionMisuseError(Exception):
+    """Indicates a method or function was used outside of a session."""
