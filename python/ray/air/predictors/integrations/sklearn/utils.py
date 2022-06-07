9fd30f1f5ab715290e0f821f20540d313c322460
xwjiang2010 <87673679+xwjiang2010@users.noreply.github.com>
2022-06-07 14:50:39 -0700

    [air] add to_air_checkpoint method for inference only workload. (#25444)
    Follow up on our last discussion for supporting piecemeal fashion air users.
    Only did for tensorflow for now, want to collect some feedback on API naming, package structure etc and I will add others.

---
+++ python/ray/air/predictors/integrations/sklearn/utils.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+import os
+from sklearn.base import BaseEstimator
+
+from ray.air.checkpoint import Checkpoint
+from ray.air.constants import MODEL_KEY
+from ray.air.preprocessor import Preprocessor
+from ray.air._internal.checkpointing import (
+    save_preprocessor_to_dir,
+)
+import ray.cloudpickle as cpickle
+
+
+def to_air_checkpoint(
+    path: str,
+    estimator: BaseEstimator,
+    preprocessor: Optional[Preprocessor] = None,
+) -> Checkpoint:
+    """Convert a pretrained model to AIR checkpoint for serve or inference.
+
+    Args:
+        path: The directory path where model and preprocessor steps are stored to.
+        estimator: A pretrained model.
+        preprocessor: A fitted preprocessor. The preprocessing logic will
+            be applied to serve/inference.
+    Returns:
+        A Ray Air checkpoint.
+    """
+    with open(os.path.join(path, MODEL_KEY), "wb") as f:
+        cpickle.dump(estimator, f)
+
+    if preprocessor:
+        save_preprocessor_to_dir(preprocessor, path)
+
+    checkpoint = Checkpoint.from_directory(path)
+
+    return checkpoint
