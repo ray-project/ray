606c4649c74eb1ffa4d639b3184653a8be0cdd98
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2022-06-08 17:50:29 -0700

    Update commit.

--- python/ray/autoscaler/kuberay/init-config.sh
+++ python/ray/autoscaler/kuberay/init-config.sh
@@ -19,7 +19,7 @@ pushd "$DIR" || exit
         # get updated together. It is important to keep this in mind when making
         # changes. The CRD is designed to be stable so one operator can run many
         # different versions of Ray.
-        git checkout e80e2038d398ab1b683677fd3d409ace7e3186b6
+        git checkout 69ecfceef5c966193ab87f22a9f49250b17e35fb
         # Here is where we specify the docker image that is used for the operator.
         # If you want to use your own version of Kuberay, you should change the content
         # of kuberay-autoscaler.patch to point to your operator.
