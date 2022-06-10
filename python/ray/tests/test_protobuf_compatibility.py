ab44d7227993e7c25ab56e4be513800a1af77d1e
scv119 <scv119@gmail.com>
2022-06-10 15:27:18 -0700

    add

---
+++ python/ray/tests/test_protobuf_compatibility.py
@@ -0,0 +1,25 @@
+import os
+import pytest
+import ray
+
+
+def test_protobuf_compatibility(shutdown_only):
+    protobuf_4_21_1= {"pip": ["protobuf==4.21.1"]}
+    protobuf_3_20_0= {"pip": ["protobuf==3.20.0"]}
+
+    ray.init()
+
+    @ray.remote
+    def load_ray_tune():
+        import google.protobuf 
+        from ray import tune #noqa
+        return google.protobuf.__version__
+
+    assert "4.21.1" == ray.get(load_ray_tune.options(runtime_env=protobuf_4_21_1).remote())
+    assert "3.20.0" == ray.get(load_ray_tune.options(runtime_env=protobuf_3_20_0).remote())
+   
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(pytest.main(["-sv", __file__]))
