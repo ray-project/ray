670534809dc8260bcfca337d84bc7303bd0e0400
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-06-10 17:13:09 +0000

    adding the alpa trainer

---
+++ python/ray/train/alpa/__init__.py
@@ -0,0 +1,11 @@
+try:
+    import alpa  # noqa: F401
+except ModuleNotFoundError:
+    raise ModuleNotFoundError(
+        "Alpa isn't installed. To install Alpa, Refer to the document:" 
+        "https://alpa-projects.github.io/install.html#install-from-source"
+    )
+
+from ray.train.alpa.alpa_trainer import AlpaTrainer
+
+__all__ = ["AlpaTrainer"]
