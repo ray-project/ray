ace23a28b8e639a84099effafaf38e7bfacd8297
scv119 <scv119@gmail.com>
2022-06-10 16:23:41 -0700

    add

--- python/ray/tests/test_protobuf_compatibility.py
+++ python/ray/tests/test_protobuf_compatibility.py
@@ -9,7 +9,7 @@ def test_protobuf_compatibility(shutdown_only):
     ray.init()
 
     @ray.remote
-    def load_ray_tune():
+    def load_ray():
         import google.protobuf
 
         # verfiy this no longer crashes. see
@@ -19,10 +19,10 @@ def test_protobuf_compatibility(shutdown_only):
         return google.protobuf.__version__
 
     assert "4.21.1" == ray.get(
-        load_ray_tune.options(runtime_env=protobuf_4_21_1).remote()
+        load_ray.options(runtime_env=protobuf_4_21_1).remote()
     )
     assert "3.20.0" == ray.get(
-        load_ray_tune.options(runtime_env=protobuf_3_20_0).remote()
+        load_ray.options(runtime_env=protobuf_3_20_0).remote()
     )
 
 
