7fc346235006a37211a697c34db3d9e6cc5f5a1f
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-06-10 18:34:52 +0000

    adding the alpa trainer

--- python/ray/air/examples/alpa/alpa_mnist_example.py
+++ python/ray/air/examples/alpa/alpa_mnist_example.py
@@ -12,7 +12,6 @@ from typing import Dict
 import argparse
 import time
 import numpy as np
-import functools
 import ray
 from ray.data.datasource import SimpleTensorFlowDatasource
 import pandas as pd
@@ -90,23 +89,6 @@ def train_func(config: Dict):
         params = mlp.init(rng, jnp.ones([1, 28, 28, 1]))["params"]
         tx = optax.sgd(learning_rate, momentum)
         return train_state.TrainState.create(apply_fn=MLP().apply, params=params, tx=tx)
-
-    def apply_model(state, images, labels):
-        """Computes gradients, loss and accuracy for a single batch."""
-
-        def loss_fn(params):
-            logits = MLP().apply({"params": params}, images)
-            one_hot = jax.nn.one_hot(labels, 10)
-            loss = jnp.mean(optax.softmax_cross_entropy(logits=logits, labels=one_hot))
-            return loss, logits
-
-        grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
-        (loss, logits), grads = grad_fn(state.params)
-        accuracy = jnp.mean(jnp.argmax(logits, -1) == labels)
-        return grads, loss, accuracy
-
-    def update_model(state, grads):
-        return state.apply_gradients(grads=grads)
 
     @alpa.parallelize
     def train_step(state, images, labels):
