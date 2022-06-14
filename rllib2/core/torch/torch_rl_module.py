93fbad30b1d3f225b8c1cea0364773b66636ad23
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2022-06-13 18:21:12 -0700

    Finished up the draft for RLModule and UnitTrainer with PPO examples.

--- rllib2/core/torch/torch_rl_module.py
+++ rllib2/core/torch/torch_rl_module.py
@@ -1,47 +1,144 @@
 from dataclasses import dataclass
+import torch.nn as nn
 
-class TorchRLModule(nn.Module):
-
-    def __init__(self):
-        pass
-
-    def forward(self):
-        pass
-
-    def forward_train(self, sample_dict) -> RLmoduleTrainOutput:
-        pass
+from rllib2.models.torch.pi import PiOutput, Pi
+from rllib2.utils import NNOutput
 
 
+"""
+Examples:
+    
+    configs: RLModuleConfig = ...
+    self.model = PPOModule(configs)
+    
+    # The user of the following use-cases are RLlib methods. So they should have a 
+    # pre-defined signature that is familiar to RLlib.
+    
+    # Inference during sampling env or during evaluating policy
+    out = self.model({'obs': s[None]}, explore=True/False, inference=True)
 
-############ Examples
-
-
-
-###################################################
-################# PPO RLModule
-###################################################
+    # During sample collection for training
+    action = out.behavioral_sample()
+    # During sample collection during evaluation
+    action = out.target_sample()
+    
+    #TODO: I don't know if we'd need explore=True / False to change the behavior of sampling
+    another alternative is to use explore and only have one sampling method action = out.sample()
+    
+    # computing (e.g. actor/critic) loss during policy update
+    # The output in this use-case will be consumed by the loss function which is 
+    # defined by the user (the author of the algorithm).
+    # So the structure should flexible to accommodate the various user needs. 
+    out = self.model(batch, explore=False, inference=False)
+    
+    # Note: user knows xyz should exist when forward_train() gets called
+    print(out.xyz)
+    
+"""
 
 @dataclass
-class PPORLOutput:
-    act_dist =
+class RLModuleConfig(NNConfig):
+    """dataclass for holding the nested configuration parameters"""
+    pass
+
+@dataclass
+class RLModuleOutput(NNOutput):
+    """dataclass for holding the outputs of RLModule forward_train() calls"""
+    pass
+
+
+class TorchRLModule(nn.Module, ModelIO):
+
+    def __init__(self, configs):
+        super().__init__()
+        self.configs = configs
+
+
+    def __call__(self, batch, *args, explore=False, inference=False, **kwargs):
+        if inference:
+            return self.forward(batch, explore=explore, **kwargs)
+        return self.forward_train(batch, **kwargs)
+
+    def forward(self, batch: SampleBatch, explore=False, **kwargs) -> PiDistribution:
+        """Forward-pass during online sample collection
+        Which could be either during training or evaluation based on explore parameter.
+        """
+        pass
+
+    def forward_train(self, batch: SampleBatch, **kwargs) -> RLModuleOutput:
+        """Forward-pass during computing loss function"""
+        pass
+
+
+"""Examples of TorchRLModules in RLlib"""
+####################################################
+################# PPO RLModule
+# This is what you customize when modifying TorchPPO
+####################################################
+
+# TODO: Make a decision about where explore=True / False should go?
+@dataclass
+class PPORLModuleConfig(RLModuleConfig):
+    action_space: Optional[rllib.env.Space] = None
+    obs_space: Optional[rllib.env.Space] = None
+    pi: Optional[PiConfig] = None
+    vf: Optional[VFuctionConfig] = None
+    kl_coeff: Optional[float] = None
+
+@dataclass
+class PPOModuleOutput(RLModuleOutput):
+    pi_out_cur: Optional[PiOutput] = None
+    pi_out_prev: Optional[PiOutput] = None
+    vf: Optional[VFOutput] = None
 
 
 class PPOTorchRLModule(TorchRLModule):
 
-    def __init__(self, config):
-        super(PPOTorchRLModule, self).__init__()
+    def __init__(self, config: PPORLModuleConfig):
+        super().__init__(config)
 
-        if self.config:
-            self.pi = ...
-        else:
-            self.pi = model_catalog.make_pi(config.pi)
-        self.vf = model_catalog.make_vf(config.vf)
+        self.pi: Pi = model_catalog.make_pi(
+            obs_space=config.obs_space,
+            action_space=config.action_space,
+            pi_config=config.pi
+        )
 
-    def forward(self, batch: SampleBatch) -> :
-        pass
+        self.vf = None
+        if config.use_ciritic:
+            self.vf = model_catalog.make_vf(
+                obs_space=config.obs_space,
+                action_space=config.action_space,
+                vf_config=config.vf
+            )
 
-    def load(self):
-        pass
+        if config.kl_coeff is not None:
+            kl_coeff = nn.Parameter(
+                torch.Tensor(config.kl_coeff),
+                requires_grad=True
+            )
+            self.register_parameter('kl_coeff', kl_coeff)
 
-    def save(self):
-        pass
+    def forward(self, batch: SampleBatch, explore=False, **kwargs) -> PiDistribution:
+
+        # option 1: when pi_output has separate target_sample() and behavioral_sample()
+        # in this case explore is totally ignored inside the forward() method.
+        pi_output: PiOutput = self.pi(batch)
+
+        # option 2: when pi_output has one sample() method whose behavior changes
+        # depending on a flag
+        pi_output: PiOutput = self.pi(batch, explore=explore)
+
+        return pi_output
+
+    def forward_train(self, batch: SampleBatch, **kwargs) -> RLModuleOutput:
+        """Forward-pass during computing loss function"""
+        pi_out_cur: PiOutput = self.pi(batch)
+        pi_out_prev = self.pi({'obs': batch[SampleBatch.ACTION_DIST_INPUTS]})
+
+        vf = None
+        if self.vf:
+            vf = self.vf(batch)
+
+        return RLModuleOutput(pi_out_cur=pi_out_cur, pi_out_prev=pi_out_prev, vf=vf)
+
+
--- rllib2/core/torch/torch_unit_trainer.py
+++ rllib2/core/torch/torch_unit_trainer.py
@@ -1,3 +1,5 @@
+from typing import Type, Union
+from dataclasses import dataclass
 from typing import Dict
 import abc
 
@@ -5,14 +7,16 @@ import torch
 from torch.optim import Optimizer
 
 from rllib2.data.sample_batch import SampleBatch
+from rllib2.models.torch.torch_rl_module import TorchRLModule, RLModuleConfig
 
-from .torch_rl_module import TorchRLModule
+@dataclass
+class UnitTrainerConfig:
+    model_class: Optional[Union[str, Type[TorchRLModule]]] = None
+    model_config: Optional[RLModuleConfig] = None
+    optimizer_config: Optional[Dict[str, Any]] = None
 
-config = {
-    'rl_module_class': 'kourosh.rllmodule'
-}
 
-class UnitTrainer:
+class TorchUnitTrainer:
 
     def __init__(self, config):
         self._config = config
@@ -24,19 +28,19 @@ class UnitTrainer:
         self._optimizers: Dict[str, Optimizer] = self.make_optimizer()
 
     @property
-    def config(self):
+    def config(self) -> UnitTrainerConfig:
         return self._config
 
     @property
-    def model(self):
+    def model(self) -> TorchRLModule:
         return self._model
 
     @property
-    def optimizers(self):
+    def optimizers(self) -> Dict[str, Optimizer]:
         return self._optimizers
 
     @property
-    def default_rl_module(self) -> str:
+    def default_rl_module(self) -> Union[str, Type[TorchRLModule]]:
         return ''
 
     @abc.abstractmethod
@@ -46,8 +50,8 @@ class UnitTrainer:
     @abc.abstractmethod
     def _make_model(self) -> TorchRLModule:
         config = self.config
-        rl_module_class = config.get('rl_module_class', self.default_rl_module())
-        rl_module_config = config['rl_module_config']
+        rl_module_class = config.get('model_class', self.default_rl_module())
+        rl_module_config = config['model_config']
 
         # import rl_module_class with rl_module_config
         # TODO
@@ -82,3 +86,98 @@ class UnitTrainer:
             self._optimizers[loss_key].zero_grad()
             loss_value.backward()
             self._optimizers[loss_key].step()
+
+
+"""
+Some examples of pre-defined RLlib unit_trainers
+"""
+
+#######################################################
+########### PPO
+#######################################################
+from rllib2.core.torch.torch_rl_module import (
+    PPOTorchRLModule, PPORLModuleConfig, PPOModuleOutput
+)
+
+class PPOUnitTrainerConfig(UnitTrainerConfig):
+    vf_clip_param: Optional[float] = None
+    vf_loss_coeff: Optional[float] = None
+
+
+
+class PPOUnitTrainer(TorchUnitTrainer):
+
+    def __init__(self, config: PPOUnitTrainerConfig):
+        super().__init__(config)
+
+    def default_rl_module(self) -> Union[str, Type[TorchRLModule]]:
+        return PPOTorchRLModule
+
+    def make_optimizer(self) -> Dict[str, Optimizer]:
+        config = self.config.optimizer_config
+        return {'total_loss': torch.optim.Adam(self.model.parameters(), lr=config.lr)}
+
+    def loss(self, train_batch: SampleBatch, fwd_train_dict: PPOModuleOutput) -> Dict[str, torch.Tensor]:
+
+        pi_out_cur = fwd_train_dict.pi_out_cur
+        pi_out_prev = fwd_train_dict.pi_out_prev
+        vf = fwd_train_dict.vf
+
+        kl_coeff = self.model.kl_coeff
+
+
+        logp_ratio = torch.exp(
+            pi_out_cur.log_prob(train_batch[SampleBatch.ACTIONS])
+            - train_batch[SampleBatch.ACTION_LOGP]
+        )
+
+        if kl_coeff:
+            action_kl = pi_out_prev.kl(pi_out_cur)
+            mean_kl_loss = reduce_mean_valid(action_kl)
+        else:
+            mean_kl_loss = torch.tensor(0.0, device=logp_ratio.device)
+
+        curr_entropy = pi_out_cur.entropy()
+        mean_entropy = reduce_mean_valid(curr_entropy)
+
+        surrogate_loss = torch.min(
+            train_batch[Postprocessing.ADVANTAGES] * logp_ratio,
+            train_batch[Postprocessing.ADVANTAGES]
+            * torch.clamp(
+                logp_ratio, 1 - self.config["clip_param"], 1 + self.config["clip_param"]
+            ),
+        )
+
+        mean_policy_loss = reduce_mean_valid(-surrogate_loss)
+
+        value_fn_out = 0
+        mean_vf_loss = vf_loss_clipped = 0.0
+        if vf:
+            value_fn_out = vf.values.reduce('min')
+            vf_loss = (value_fn_out -
+                       train_batch[Postprocessing.VALUE_TARGETS])**2
+            vf_loss_clipped = torch.clamp(vf_loss, 0, self.config["vf_clip_param"])
+            mean_vf_loss = reduce_mean_valid(vf_loss_clipped)
+
+        total_loss = reduce_mean_valid(
+            -surrogate_loss
+            + self.config.vf_loss_coeff * vf_loss_clipped
+            - self.config.entropy_coeff * curr_entropy
+        )
+
+        if kl_coeff:
+            total_loss = kl_coeff * mean_kl_loss
+
+
+        self.logger.tower_stats.update(
+            total_loss=total_loss,
+            mean_policy_loss=mean_policy_loss,
+            mean_vf_loss=mean_vf_loss,
+            vf_explained_var=explained_variance(train_batch[Postprocessing.VALUE_TARGETS], value_fn_out),
+            mean_entropy=mean_entropy,
+            mean_kl_loss=mean_kl_loss,
+        )
+
+        return {'total_loss': total_loss}
+
+
