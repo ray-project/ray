8a066ba18e73cc3ee010cbd630d36845ff9d0939
nanqi.yxf <tcyxf@mail.ustc.edu.cn>
2022-06-14 20:14:12 +0800

    format code style:serve/src/main/java/io/ray/serve/api/Serve.java

--- java/serve/src/main/java/io/ray/serve/api/Serve.java
+++ java/serve/src/main/java/io/ray/serve/api/Serve.java
@@ -195,7 +195,8 @@ public class Serve {
     }
 
     // Start controller in "serve" namespace if detached and currently in anonymous namespace.
-    if (null != controllerNamespace && ANONYMOUS_NAMESPACE_PATTERN.matcher(controllerNamespace).matches()) {
+    if (null != controllerNamespace
+        && ANONYMOUS_NAMESPACE_PATTERN.matcher(controllerNamespace).matches()) {
       controllerNamespace = "serve";
     }
     return controllerNamespace;
