dd9829aa5dc3a4e4de5c0857d411651076883485
scv119 <scv119@gmail.com>
2022-06-14 11:00:01 -0700

    update

--- python/ray/tests/test_protobuf_compatibility.py
+++ python/ray/tests/test_protobuf_compatibility.py
@@ -4,7 +4,7 @@ import ray
 
 def test_protobuf_compatibility(shutdown_only):
     protobuf_4_21_0 = {"pip": ["protobuf==4.21.0"]}
-    protobuf_3_12_0 = {"pip": ["protobuf==3.12.0"]}
+    protobuf_3_12_1 = {"pip": ["protobuf==3.12.1"]}
 
     ray.init()
 
@@ -19,7 +19,7 @@ def test_protobuf_compatibility(shutdown_only):
         return google.protobuf.__version__
 
     assert "4.21.0" == ray.get(load_ray.options(runtime_env=protobuf_4_21_0).remote())
-    assert "3.12.0" == ray.get(load_ray.options(runtime_env=protobuf_3_12_0).remote())
+    assert "3.12.1" == ray.get(load_ray.options(runtime_env=protobuf_3_12_1).remote())
 
 
 if __name__ == "__main__":
