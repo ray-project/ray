ca19b0ff0a3977b0cc238a56dffb3a0a6f79e364
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-06-15 23:35:23 +0000

    adding the shutdown

--- python/ray/train/alpa/config.py
+++ python/ray/train/alpa/config.py
@@ -21,6 +21,9 @@ from ray.train._internal.utils import check_for_failure
 from ray.train._internal.worker_group import WorkerGroup
 from ray.util.placement_group import get_current_placement_group, remove_placement_group
 
+import alpa
+from alpa.util import update_jax_platform
+from alpa.device_mesh import VirtualPhysicalMesh
 T = TypeVar("T")
 
 logger = logging.getLogger(__name__)
@@ -64,10 +67,21 @@ class _AlpaBackend(Backend):
         host_ip_2_host_info_dict = dict(zip(host_ips, host_info))
         node_info = [ host_ip_2_host_info_dict[node_ip] for node_ip in node_ips]
 
-        # alpa
-        # return DistributedPhysicalDeviceMesh(
-        #     host_ids=node_ids,
-        #     host_info=node_info,
-        #     head_ip=head_ip,
-        #     num_devices_per_host=num_devices_per_host,
-        #     parent=self)
+        self.vp_mesh = VirtualPhysicalMesh(host_ids=node_ids,
+                            host_info=node_info,
+                            head_ip=head_ip,
+                            num_devices_per_host=num_devices_per_host,
+                            parent=None)
+
+        alpa.device_mesh.set_global_virtual_physical_mesh(self.vp_mesh)
+
+    def on_shutdown(self, worker_group: WorkerGroup, backend_config: AlpaConfig):
+
+        alpa.device_mesh.global_cluster = None
+        update_jax_platform('gpu')
+
+        if self.vp_mesh:
+            if self.vp_mesh.launched_physical_mesh_group:
+                self.vp_mesh.launched_physical_mesh_group.shutdown()
+            alpa.device_mesh.global_virtual_physical_mesh = None
+            
