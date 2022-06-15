311b5014041d2f3433275cac82f9143e25af0379
Yi Cheng <chengyidna@gmail.com>
2022-06-15 07:53:02 +0000

    up

--- python/ray/tests/test_output.py
+++ python/ray/tests/test_output.py
@@ -591,6 +591,6 @@ if __name__ == "__main__":
         ray.shutdown()
     else:
         if os.environ.get("PARALLEL_CI"):
-        sys.exit(pytest.main(["-n", "auto", "--boxed", "-vs", __file__]))
-    else:
-        sys.exit(pytest.main(["-sv", __file__]))
+            sys.exit(pytest.main(["-n", "auto", "--boxed", "-vs", __file__]))
+        else:
+            sys.exit(pytest.main(["-sv", __file__]))
