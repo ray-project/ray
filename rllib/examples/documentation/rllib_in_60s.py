ddd343c48a383924c264baaa51daf1a897d4fd14
Zhe Zhang <zhz@anyscale.com>
2022-06-15 11:55:58 -0700

    Simplify config based on feedback

--- rllib/examples/documentation/rllib_in_60s.py
+++ rllib/examples/documentation/rllib_in_60s.py
@@ -12,15 +12,6 @@ config = {
     # Change this to "framework: torch", if you are using PyTorch.
     # Also, use "framework: tf2" for tf2.x eager execution.
     "framework": "tf",
-    # Tweak the default model provided automatically by RLlib,
-    # given the environment's observation- and action spaces.
-    "model": {
-        "fcnet_hiddens": [64, 64],
-        "fcnet_activation": "relu",
-    },
-    # Set up a separate evaluation worker set for the
-    # `algo.evaluate()` call after training (see below).
-    "evaluation_num_workers": 1,
     # Only for evaluation runs, render the env.
     "evaluation_config": {
         "render_env": True,
