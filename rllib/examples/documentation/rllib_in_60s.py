fc37d6790ee139d64985d7d274f16c7bcc8164e4
Zhe Zhang <zhz@anyscale.com>
2022-06-15 11:26:54 -0700

    Simplify config

--- rllib/examples/documentation/rllib_in_60s.py
+++ rllib/examples/documentation/rllib_in_60s.py
@@ -6,18 +6,6 @@ from ray.rllib.algorithms.ppo import PPO
 config = {
     # Environment (RLlib understands openAI gym registered strings).
     "env": "Taxi-v3",
-    # Use 2 environment workers (aka "rollout workers") that parallelly
-    # collect samples from their own environment clone(s).
-    "num_workers": 2,
-    # Change this to "framework: torch", if you are using PyTorch.
-    # Also, use "framework: tf2" for tf2.x eager execution.
-    "framework": "tf",
-    # Tweak the default model provided automatically by RLlib,
-    # given the environment's observation- and action spaces.
-    "model": {
-        "fcnet_hiddens": [64, 64],
-        "fcnet_activation": "relu",
-    },
     # Set up a separate evaluation worker set for the
     # `algo.evaluate()` call after training (see below).
     "evaluation_num_workers": 1,
