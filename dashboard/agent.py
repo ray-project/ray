1398ceae913d63280f007d07c56588b735a67028
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2022-06-16 14:11:15 -0700

    Initialize internal kv in dashboard agent

--- dashboard/agent.py
+++ dashboard/agent.py
@@ -12,11 +12,16 @@ import ray._private.services
 import ray._private.utils
 import ray.dashboard.consts as dashboard_consts
 import ray.dashboard.utils as dashboard_utils
+import ray.experimental.internal_kv as internal_kv
 import ray.ray_constants as ray_constants
 from ray._private.gcs_pubsub import GcsAioPublisher, GcsPublisher
 from ray._private.gcs_utils import GcsAioClient, GcsClient
 from ray._private.ray_logging import setup_component_logger
 from ray.core.generated import agent_manager_pb2, agent_manager_pb2_grpc
+from ray.experimental.internal_kv import (
+    _initialize_internal_kv,
+    _internal_kv_initialized,
+)
 
 # Import psutil after ray so the packaged version is used.
 import psutil
@@ -127,6 +132,8 @@ class DashboardAgent:
         # TODO(architkulkarni): Remove gcs_client once the agent exclusively uses
         # gcs_aio_client and not gcs_client.
         self.gcs_client = GcsClient(address=self.gcs_address)
+        _initialize_internal_kv(self.gcs_client)
+        assert _internal_kv_initialized()
         self.gcs_aio_client = GcsAioClient(address=self.gcs_address)
         self.publisher = GcsAioPublisher(address=self.gcs_address)
 
@@ -249,12 +256,12 @@ class DashboardAgent:
                 )
 
         # Write the dashboard agent port to kv.
+        # TODO: Use async version if performance is an issue
         # -1 should indicate that http server is not started.
         http_port = -1 if not self.http_server else self.http_server.http_port
-        await self.gcs_aio_client.internal_kv_put(
-            f"{dashboard_consts.DASHBOARD_AGENT_PORT_PREFIX}{self.node_id}".encode(),
-            json.dumps([http_port, self.grpc_port]).encode(),
-            overwrite=True,
+        internal_kv._internal_kv_put(
+            f"{dashboard_consts.DASHBOARD_AGENT_PORT_PREFIX}{self.node_id}",
+            json.dumps([http_port, self.grpc_port]),
             namespace=ray_constants.KV_NAMESPACE_DASHBOARD,
         )
 
