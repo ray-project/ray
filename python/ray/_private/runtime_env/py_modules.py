27f9d1b310295974ad8ab63b75c46c719a9f4a1a
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2022-06-16 10:46:29 -0700

    Remove internal kv from py_modules

--- python/ray/_private/runtime_env/py_modules.py
+++ python/ray/_private/runtime_env/py_modules.py
@@ -23,7 +23,6 @@ from ray._private.runtime_env.packaging import (
 from ray._private.runtime_env.plugin import RuntimeEnvPlugin
 from ray._private.runtime_env.working_dir import set_pythonpath_in_context
 from ray._private.utils import get_directory_size_bytes, try_to_create_directory
-from ray.experimental.internal_kv import _internal_kv_initialized
 
 default_logger = logging.getLogger(__name__)
 
@@ -133,7 +132,6 @@ class PyModulesPlugin(RuntimeEnvPlugin):
         self._resources_dir = os.path.join(resources_dir, "py_modules_files")
         self._gcs_aio_client = gcs_aio_client
         try_to_create_directory(self._resources_dir)
-        assert _internal_kv_initialized()
 
     def _get_local_dir_from_uri(self, uri: str):
         return get_local_dir_from_uri(uri, self._resources_dir)
