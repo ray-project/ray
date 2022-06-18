4e5b03060ae4e4bb6e0af194cbac0ccb2efc4aa0
Yi Cheng <chengyidna@gmail.com>
2022-06-18 07:54:59 +0000

    up

--- python/ray/tests/test_plasma_unlimited.py
+++ python/ray/tests/test_plasma_unlimited.py
@@ -328,6 +328,6 @@ if __name__ == "__main__":
     import sys
 
     if os.environ.get("PARALLEL_CI"):
-        sys.exit(pytest.main(["-n", "auto", "--boxed", "-vs", __file__]))
+        sys.exit(pytest.main(["-n", "2", "--boxed", "-vs", __file__]))
     else:
         sys.exit(pytest.main(["-sv", __file__]))
