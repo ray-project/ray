745105aa55d4805e80f3ef5c37798a2ba771bc28
Eric Liang <ekhliang@gmail.com>
2022-06-18 15:26:20 -0700

    fix

--- python/ray/data/tests/test_auto_parallelism.py
+++ python/ray/data/tests/test_auto_parallelism.py
@@ -30,24 +30,23 @@ def test_auto_parallelism_placement_group(shutdown_only):
         ds = ray.data.range_tensor(10000, shape=(100,), parallelism=-1)
         return ds.num_blocks()
 
-    # 1/16 * 4 = 8
+    # 1/16 * 4 * 16 = 8
     pg = ray.util.placement_group([{"CPU": 1}])
     num_blocks = ray.get(run.options(placement_group=pg).remote())
     assert num_blocks == 4, num_blocks
 
-    # 2/16 * 4 = 8
+    # 2/16 * 4 * 16 = 8
     pg = ray.util.placement_group([{"CPU": 2}])
     num_blocks = ray.get(run.options(placement_group=pg).remote())
     assert num_blocks == 8, num_blocks
 
-    # 1/8 * 4 = 8
+    # 1/8 * 4 * 16 = 8
     pg = ray.util.placement_group([{"CPU": 1, "GPU": 1}])
     num_blocks = ray.get(run.options(placement_group=pg).remote())
-    assert num_blocks == 8, num_blocks
+    assert num_blocks == 16, num_blocks
 
 
 if __name__ == "__main__":
     import sys
 
     sys.exit(pytest.main(["-v", __file__]))
-
