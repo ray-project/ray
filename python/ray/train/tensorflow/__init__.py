6a232d2f5986fad635e6faad9651a9232c6a4e10
Antoni Baum <antoni.baum@protonmail.com>
2022-06-20 23:36:12 +0200

    Update __init__.py

--- python/ray/train/tensorflow/__init__.py
+++ python/ray/train/tensorflow/__init__.py
@@ -12,7 +12,7 @@ from ray.train.tensorflow.config import TensorflowConfig
 from ray.train.tensorflow.tensorflow_predictor import TensorflowPredictor
 from ray.train.tensorflow.tensorflow_trainer import TensorflowTrainer
 from ray.train.tensorflow.train_loop_utils import prepare_dataset_shard
-from ray.train.tensorflow.utils import load_checkpoint, to_air_checkpoint
+from ray.train.tensorflow.utils import to_air_checkpoint, load_checkpoint
 
 __all__ = [
     "TensorflowConfig",
