5d671254c8f13ca810d7f6f01603ae77b7b495a4
Amog Kamsetty <amogkamsetty@yahoo.com>
2022-06-21 14:57:26 -0700

    update

--- python/ray/train/tests/test_batch_predictor.py
+++ python/ray/train/tests/test_batch_predictor.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import pytest
 import time
-from unittest import mock
 
 import ray
 from ray.air.checkpoint import Checkpoint
-from ray.train.predictor import Predictor, PredictorNotSerializableException
+from ray.train.predictor import Predictor
 from ray.data import Preprocessor
 from ray.train.batch_predictor import BatchPredictor
 
@@ -27,58 +26,6 @@ class DummyPredictor(Predictor):
 
     def _predict_pandas(self, data: pd.DataFrame, **kwargs) -> pd.DataFrame:
         return data * self.factor
-
-
-def test_serialization():
-    """Tests that Predictor instances are not serializable."""
-
-    # Class is serializable.
-    ray.put(DummyPredictor)
-
-    # Instance is not serializable.
-    predictor = DummyPredictor()
-    with pytest.raises(PredictorNotSerializableException):
-        ray.put(predictor)
-
-
-def test_from_checkpoint():
-    checkpoint = Checkpoint.from_dict({"factor": 2.0})
-    assert DummyPredictor.from_checkpoint(checkpoint).factor == 2.0
-
-
-@mock.patch(
-    "ray.air.predictor.convert_batch_type_to_pandas",
-    return_value=mock.DEFAULT,
-)
-@mock.patch(
-    "ray.air.predictor.convert_pandas_to_batch_type",
-    return_value=mock.DEFAULT,
-)
-def test_predict(convert_from_pandas_mock, convert_to_pandas_mock):
-    checkpoint = Checkpoint.from_dict({"factor": 2.0})
-    predictor = DummyPredictor.from_checkpoint(checkpoint)
-
-    input = pd.DataFrame({"x": [1, 2, 3]})
-    expected_output = input * 4.0
-    actual_output = predictor.predict(input)
-    assert actual_output.equals(expected_output)
-
-    # Ensure the proper conversion functions are called.
-    convert_to_pandas_mock.assert_called_once()
-    convert_from_pandas_mock.assert_called_once()
-
-
-@mock.patch.object(DummyPredictor, "_predict_pandas", return_value=mock.DEFAULT)
-def test_kwargs(predict_pandas_mock):
-    checkpoint = Checkpoint.from_dict({"factor": 2.0})
-    predictor = DummyPredictor.from_checkpoint(checkpoint)
-
-    input = pd.DataFrame({"x": [1, 2, 3]})
-    predictor.predict(input, extra_arg=1)
-
-    # Second element in call_args is the kwargs.
-    assert "extra_arg" in predict_pandas_mock.call_args[1]
-    assert predict_pandas_mock.call_args[1]["extra_arg"] == 1
 
 
 class DummyPredictorFS(DummyPredictor):
