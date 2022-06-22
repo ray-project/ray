3491623b9f2f73b7544948fab697150605b2b093
Yi Cheng <chengyidna@gmail.com>
2022-06-22 21:36:50 +0000

    update

--- python/ray/tests/test_object_spilling_3.py
+++ python/ray/tests/test_object_spilling_3.py
@@ -86,9 +86,7 @@ def test_multiple_directories(tmp_path, shutdown_only):
 
 def _check_spilled(address, num_objects_spilled=0):
     def ok():
-        s = ray._private.internal_api.memory_summary(
-            address=address,
-            stats_only=True)
+        s = ray._private.internal_api.memory_summary(address=address, stats_only=True)
         if num_objects_spilled == 0:
             return "Spilled " not in s
 
