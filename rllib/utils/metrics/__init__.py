dc793b452a83de4abbfb578190a90b6cbdb1a152
sven1977 <svenmika1977@gmail.com>
2022-06-23 19:16:32 +0200

    wip

--- rllib/utils/metrics/__init__.py
+++ rllib/utils/metrics/__init__.py
@@ -7,6 +7,9 @@ NUM_ENV_STEPS_TRAINED = "num_env_steps_trained"
 NUM_AGENT_STEPS_TRAINED = "num_agent_steps_trained"
 NUM_ENV_STEPS_TRAINED_THIS_ITER = "num_env_steps_trained_this_iter"
 NUM_AGENT_STEPS_TRAINED_THIS_ITER = "num_agent_steps_trained_this_iter"
+
+# Counters for keeping track of worker weight updates (synchronization
+# between local worker and remote workers).
 NUM_SYNCH_WORKER_WEIGHTS = "num_weight_broadcasts"
 NUM_TRAINING_STEP_CALLS_SINCE_LAST_SYNCH_WORKER_WEIGHTS = (
     "num_training_step_calls_since_last_synch_worker_weights"
@@ -16,7 +19,7 @@ NUM_TRAINING_STEP_CALLS_SINCE_LAST_SYNCH_WORKER_WEIGHTS = (
 LAST_TARGET_UPDATE_TS = "last_target_update_ts"
 NUM_TARGET_UPDATES = "num_target_updates"
 
-# Performance timers (keys for Algorithm._timers or metrics.timers).
+# Performance timers (keys for Algorithm._timers).
 TRAINING_ITERATION_TIMER = "training_iteration"
 APPLY_GRADS_TIMER = "apply_grad"
 COMPUTE_GRADS_TIMER = "compute_grads"
