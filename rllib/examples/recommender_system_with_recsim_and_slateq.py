6ec6d7c633c4cb0484f6aaa1582a0078f86c1ea0
Artur Niederfahrenhorst <artur@anyscale.com>
2022-06-23 12:26:01 +0200

    lint

--- rllib/examples/recommender_system_with_recsim_and_slateq.py
+++ rllib/examples/recommender_system_with_recsim_and_slateq.py
@@ -127,7 +127,7 @@ def main():
         "num_workers": args.num_workers,
         "env_config": env_config,
         "replay_buffer_config": {
-            "num_ts_added_before_sampling_starts": args.num_ts_added_before_sampling_starts,
+            "num_ts_added_before_sampling_starts": args.num_ts_added_before_sampling_starts,  # noqa E501
         },
     }
 
