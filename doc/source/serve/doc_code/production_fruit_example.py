fb66dd2f9ce0cf2b05d64a6341c53d6330ad1897
Shreyas Krishnaswamy <shrekris@anyscale.com>
2022-06-23 02:29:41 -0700

    Add tests to doc example

--- doc/source/serve/doc_code/production_fruit_example.py
+++ doc/source/serve/doc_code/production_fruit_example.py
@@ -104,21 +104,109 @@ with InputNode() as query:
 deployment_graph = DAGDriver.bind(net_price, http_adapter=json_request)
 # __fruit_example_end__
 
+
+# Test example's behavior
+import requests  # noqa: E402
+from ray.serve.schema import ServeApplicationSchema  # noqa: E402
+from ray.serve.api import build  # noqa: E402
+from ray._private.test_utils import wait_for_condition  # noqa: E402
+
+
+def check_fruit_deployment_graph():
+    """Checks the fruit deployment graph from this example."""
+
+    assert requests.post("http://localhost:8000/", json=["MANGO", 1]).json() == 3
+    assert requests.post("http://localhost:8000/", json=["ORANGE", 1]).json() == 2
+    assert requests.post("http://localhost:8000/", json=["PEAR", 1]).json() == 4
+    assert requests.post("http://localhost:8000/", json=["TOMATO", 1]).json() == -1
+
+
+def check_fruit_deployment_graph_updates():
+    """Checks the graph after updating all prices to 0."""
+
+    assert requests.post("http://localhost:8000/", json=["MANGO", 1]).json() == 0
+    assert requests.post("http://localhost:8000/", json=["ORANGE", 1]).json() == 0
+    assert requests.post("http://localhost:8000/", json=["PEAR", 1]).json() == 0
+
+
+# Test behavior from this documentation example
 serve.start()
-serve.run(deployment_graph)
-
-import requests
-
-assert requests.post("http://localhost:8000/", json=["MANGO", 1]).json() == 3
-assert requests.post("http://localhost:8000/", json=["ORANGE", 1]).json() == 2
-assert requests.post("http://localhost:8000/", json=["PEAR", 1]).json() == 4
-assert requests.post("http://localhost:8000/", json=["TOMATO", 1]).json() == -1
-
-MangoStand.options(user_config={"price": 0}).deploy()
-assert requests.post("http://localhost:8000/", json=["MANGO", 1]).json() == 0
-
+app = build(deployment_graph)
+for deployment in app.deployments.values():
+    deployment.set_options(ray_actor_options={"num_cpus": 0.1})
+serve.run(app)
+check_fruit_deployment_graph()
+MangoStand.options(name="MangoStand", user_config={"price": 0}).deploy()
 OrangeStand.options(user_config={"price": 0}).deploy()
-assert requests.post("http://localhost:8000/", json=["ORANGE", 1]).json() == 0
-
 PearStand.options(user_config={"price": 0}).deploy()
-assert requests.post("http://localhost:8000/", json=["PEAR", 1]).json() == 0
+print("Example ran successfully from the file.")
+serve.shutdown()
+
+# Check that deployments have been torn down
+try:
+    requests.post("http://localhost:8000/", json=["MANGO", 1]).json()
+    raise ValueError("Deployments should have been torn down!")
+except requests.exceptions.ConnectionError:
+    pass
+print("Deployments have been torn down.")
+
+# Check for regression in remote repository
+client = serve.start()
+config1 = {
+    "import_path": "fruit.deployment_graph",
+    "runtime_env": {
+        "working_dir": (
+            "https://github.com/ray-project/serve_config_examples/archive/HEAD.zip"
+        )
+    },
+    "deployments": [
+        {"name": "FruitMarket", "ray_actor_options": {"num_cpus": 0.1}},
+        {"name": "MangoStand", "ray_actor_options": {"num_cpus": 0.1}},
+        {"name": "OrangeStand", "ray_actor_options": {"num_cpus": 0.1}},
+        {"name": "PearStand", "ray_actor_options": {"num_cpus": 0.1}},
+        {"name": "DAGDriver", "ray_actor_options": {"num_cpus": 0.1}},
+    ],
+}
+client.deploy_app(ServeApplicationSchema.parse_obj(config1))
+wait_for_condition(
+    lambda: client.get_serve_status().app_status.deployment_timestamp > 0
+)
+wait_for_condition(lambda: client.get_serve_status().app_status.status == "RUNNING")
+dt1 = client.get_serve_status().app_status.deployment_timestamp
+check_fruit_deployment_graph()
+config2 = {
+    "import_path": "fruit.deployment_graph",
+    "runtime_env": {
+        "working_dir": (
+            "https://github.com/ray-project/serve_config_examples/archive/HEAD.zip"
+        )
+    },
+    "deployments": [
+        {"name": "FruitMarket", "ray_actor_options": {"num_cpus": 0.1}},
+        {
+            "name": "MangoStand",
+            "user_config": {"price": 0},
+            "ray_actor_options": {"num_cpus": 0.1},
+        },
+        {
+            "name": "OrangeStand",
+            "user_config": {"price": 0},
+            "ray_actor_options": {"num_cpus": 0.1},
+        },
+        {
+            "name": "PearStand",
+            "user_config": {"price": 0},
+            "ray_actor_options": {"num_cpus": 0.1},
+        },
+        {"name": "DAGDriver", "ray_actor_options": {"num_cpus": 0.1}},
+    ],
+}
+client.deploy_app(ServeApplicationSchema.parse_obj(config2))
+wait_for_condition(
+    lambda: client.get_serve_status().app_status.deployment_timestamp > dt1
+)
+print(client.get_serve_status().app_status.deployment_timestamp > dt1)
+print(client.get_serve_status().app_status.status)
+wait_for_condition(lambda: client.get_serve_status().app_status.status == "RUNNING")
+serve.shutdown()
+print("Example ran successfully from the remote repository.")
