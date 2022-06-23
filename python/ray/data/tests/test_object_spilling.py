459535f53383a6f76756938daf71708833b0295d
Ubuntu <ubuntu@ip-172-31-32-136.us-west-2.compute.internal>
2022-06-23 20:48:16 +0000

    test on parquet files

--- python/ray/data/tests/test_object_spilling.py
+++ python/ray/data/tests/test_object_spilling.py
@@ -1,16 +1,42 @@
+import argparse
 import ray
 import time
+import sys
 
+import pandas as pd
 import numpy as np
 
-# Create a dataset with 30 GiB, and 512 MiB/block
-NUM_GB = 30
-BLOCK_SIZE_GB = 0.5
-
-# 80*80*4*8*5000 = 1 GiB
-ds = ray.data.range_tensor(
-    5000 * NUM_GB, shape=(80, 80, 4), parallelism=int(NUM_GB / BLOCK_SIZE_GB)
+parser = argparse.ArgumentParser("Benchmarking spilling performance")
+parser.add_argument("--data-type", type=int, default=0)  # 0: tensor 1: parquet
+parser.add_argument(
+    "--batch-size",
+    type=int,
+    default=2500,
 )
+parser.add_argument("--num-files", type=int, default=30)
+args = parser.parse_args()
+
+
+def create_dataset():
+    if args.data_type == 0:
+        # Tensor data: create a dataset with 30 GiB, and 512 MiB/block
+        NUM_GB = 30
+        BLOCK_SIZE_GB = 0.5
+        # 80*80*4*8*5000 = 1 GiB
+        ds = ray.data.range_tensor(
+            5000 * NUM_GB, shape=(80, 80, 4), parallelism=int(NUM_GB / BLOCK_SIZE_GB)
+        )
+    else:
+        assert args.data_type == 1
+        # Parquet data
+        files = [
+            f"s3://shuffling-data-loader-benchmarks/data/r10_000_000_000-f1000"
+            f"/input_data_{i}.parquet.snappy"
+            for i in range(args.num_files)
+        ]
+        ds = ray.data.read_parquet(files)
+    return ds
+
 
 print("Starting consume the dataset")
 start = time.perf_counter()
@@ -18,12 +44,21 @@ batch_start = start
 num_batches, num_bytes = 0, 0
 batch_delays = []
 
-for batch in ds.iter_batches():
+ds = create_dataset()
+for batch in ds.iter_batches(batch_size=args.batch_size):
     num_batches += 1
     batch_delay = time.perf_counter() - batch_start
     batch_delays.append(batch_delay)
-    assert isinstance(batch, np.ndarray)
-    num_bytes += batch.nbytes
+
+    if isinstance(batch, pd.DataFrame):
+        num_bytes += int(batch.memory_usage(index=True, deep=True).sum())
+    elif isinstance(batch, np.ndarray):
+        num_bytes += batch.nbytes
+    else:
+        # NOTE: This isn't recursive and will just return the size of
+        # the object pointers if list of non-primitive types.
+        num_bytes += sys.getsizeof(batch)
+
     batch_start = time.perf_counter()
 
 duration = time.perf_counter() - start
