4a21684c7ed63af3b8c396951219b8db1d49af44
sven1977 <svenmika1977@gmail.com>
2022-06-27 20:55:30 +0200

    wip

--- rllib/evaluation/worker_set.py
+++ rllib/evaluation/worker_set.py
@@ -306,7 +306,19 @@ class WorkerSet:
             )
         return removed_workers
 
-    def recreate_failed_workers(self) -> Tuple[List[ActorHandle], List[ActorHandle]]:
+    def recreate_failed_workers(
+        self, local_worker: Optional[RolloutWorker] = None
+    ) -> Tuple[List[ActorHandle], List[ActorHandle]]:
+        """Recreates any failed workers (after health check).
+
+        Args:
+            local_worker: Optional local worker to use to synchronize the weights after
+                recreation. If not provided, will try use `self._local_worker`.
+
+        Returns:
+            A tuple consisting of two items: The list of removed workers and the list of
+            newly added ones.
+        """
         faulty_indices = self._worker_health_check()
         removed_workers = []
         new_workers = []
@@ -330,11 +342,17 @@ class WorkerSet:
                 config=self._remote_config,
             )
             # Sync new worker from local one.
-            if self.local_worker():
-                new_worker.set_weights.remote(
-                    weights=self.local_worker().get_weights(),
-                    global_vars=self.local_worker().get_global_vars(),
+            local_worker = local_worker or self.local_worker()
+            if not local_worker:
+                raise ValueError(
+                    "Cannot synchronize weights to newly created worker! "
+                    "No local worker in WorkerSet or none provided as argument to "
+                    "`recreate_failed_workers()`."
                 )
+            new_worker.set_weights.remote(
+                weights=local_worker.get_weights(),
+                global_vars=local_worker.get_global_vars(),
+            )
             # Add new worker to list of remote workers.
             self._remote_workers[worker_index - 1] = new_worker
             new_workers.append(new_worker)
