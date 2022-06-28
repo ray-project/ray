e0798d30498b8836804ac1524c50e2bdb0405f0e
Qing Wang <kingchin1218@gmail.com>
2022-06-28 16:24:50 +0800

    WIP

--- java/runtime/src/main/java/io/ray/runtime/RayNativeRuntime.java
+++ java/runtime/src/main/java/io/ray/runtime/RayNativeRuntime.java
@@ -266,6 +266,11 @@ public final class RayNativeRuntime extends AbstractRayRuntime {
     return nativeGetNamespace();
   }
 
+  @Override
+  public UniqueId getCurrentNodeId() {
+    return UniqueId.fromBytes(nativeGetCurrentNodeId());
+  }
+
   private static native void nativeInitialize(
       int workerMode,
       String ndoeIpAddress,
@@ -293,6 +298,8 @@ public final class RayNativeRuntime extends AbstractRayRuntime {
   private static native String nativeGetNamespace();
 
   private static native List<byte[]> nativeGetCurrentReturnIds(int numReturns, byte[] actorId);
+
+  private static native byte[] nativeGetCurrentNodeId();
 
   static class AsyncContext {
 
