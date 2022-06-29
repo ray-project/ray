7bd41de36cbff710b6c36a53bf2e9c9f40509a71
rapotdar <rapotdar@anyscale.com>
2022-06-29 13:54:20 -0700

    wip

---
+++ rllib/offline/estimators/dm_trainable.py
@@ -0,0 +1,84 @@
+import logging
+from typing import Dict, Any
+from ray.rllib.offline.estimators.off_policy_estimator import OffPolicyEstimator
+from ray.rllib.policy import Policy
+from ray.rllib.utils.annotations import DeveloperAPI, override
+from ray.rllib.utils.framework import try_import_torch
+from ray.rllib.utils.typing import SampleBatchType
+from ray.rllib.offline.estimators.fqe_torch_model import FQETorchModel
+from ray.rllib.offline.estimators.qreg_torch_model import QRegTorchModel
+from gym.spaces import Discrete
+
+from ray.rllib.offline.estimators.direct_method import DirectMethod
+
+torch, nn = try_import_torch()
+
+logger = logging.getLogger()
+
+
+@DeveloperAPI
+class DMTrainable(DirectMethod):
+    """The Direct Method estimator with a trainable Q-model.
+
+    DM estimator described in https://arxiv.org/pdf/1511.03722.pdf"""
+
+    @override(DirectMethod)
+    def __init__(
+        self,
+        name: str,
+        policy: Policy,
+        gamma: float,
+        q_model_config: Dict = None,
+    ):
+        """
+        Initializes a Direct Method OPE Estimator.
+
+        Args:
+            name: string to save OPE results under
+            policy: Policy to evaluate.
+            gamma: Discount factor of the environment.
+            q_model_type: Either "fqe" for Fitted Q-Evaluation
+                or "qreg" for Q-Regression, or a custom model that implements:
+                - `estimate_q(states, actions)`
+                - `estimate_v(states, action_probs)`
+            q_model_config: Arguments to specify the Q-model.
+        """
+
+        super().__init__(name, policy, gamma)
+        q_model_config = q_model_config or {"type": "fqe"}
+        q_model_type = q_model_config.pop("type")
+        # TODO (Rohan138): Add support for continuous action spaces
+        assert isinstance(
+            policy.action_space, Discrete
+        ), "DM Estimator only supports discrete action spaces!"
+        assert (
+            policy.config["batch_mode"] == "complete_episodes"
+        ), "DM Estimator only supports `batch_mode`=`complete_episodes`"
+
+        # TODO (Rohan138): Add support for TF!
+        if policy.framework == "torch":
+            if q_model_type == "qreg":
+                model_cls = QRegTorchModel
+            elif q_model_type == "fqe":
+                model_cls = FQETorchModel
+            else:
+                assert hasattr(
+                    q_model_type, "estimate_q"
+                ), "q_model_type must implement `estimate_q`!"
+                assert hasattr(
+                    q_model_type, "estimate_v"
+                ), "q_model_type must implement `estimate_v`!"
+        else:
+            raise ValueError(
+                f"{self.__class__.__name__}"
+                "estimator only supports `policy.framework`=`torch`"
+            )
+
+        self.model = model_cls(
+            policy=policy,
+            gamma=gamma,
+            **q_model_config,
+        )
+    
+    def train(self, batch: SampleBatchType) -> Dict[str,Any]:
+        return {self.name + "_loss", self.model.train_q(batch)}
---
+++ rllib/offline/estimators/dr_trainable.py
@@ -0,0 +1,71 @@
+from ray.rllib.offline.estimators.direct_method import DirectMethod, train_test_split
+from ray.rllib.utils.annotations import DeveloperAPI, override
+from ray.rllib.utils.typing import SampleBatchType
+from ray.rllib.policy.sample_batch import SampleBatch
+from ray.rllib.utils.numpy import convert_to_numpy
+import numpy as np
+from typing import Dict, Any
+
+
+@DeveloperAPI
+class DoublyRobust(DirectMethod):
+    """The Doubly Robust (DR) estimator.
+
+    DR estimator described in https://arxiv.org/pdf/1511.03722.pdf"""
+
+    @override(DirectMethod)
+    def estimate(self, batch: SampleBatchType) -> Dict[str, Any]:
+        self.check_can_estimate_for(batch)
+        estimates = {"v_old": [], "v_new": [], "v_gain": []}
+        # Split data into train and test batches
+        for train_episodes, test_episodes in train_test_split(
+            batch,
+            self.train_test_split_val,
+            self.k,
+        ):
+
+            # Train Q-function
+            if train_episodes:
+                # Reinitialize model
+                self.model.reset()
+                train_batch = SampleBatch.concat_samples(train_episodes)
+                self.model.train_q(train_batch)
+
+            # Calculate doubly robust OPE estimates
+            for episode in test_episodes:
+                rewards, old_prob = episode["rewards"], episode["action_prob"]
+                new_prob = np.exp(self.action_log_likelihood(episode))
+
+                v_old = 0.0
+                v_new = 0.0
+                q_values = self.model.estimate_q(
+                    episode[SampleBatch.OBS], episode[SampleBatch.ACTIONS]
+                )
+                q_values = convert_to_numpy(q_values)
+
+                all_actions = np.zeros([episode.count, self.policy.action_space.n])
+                all_actions[:] = np.arange(self.policy.action_space.n)
+                # Two transposes required for torch.distributions to work
+                tmp_episode = episode.copy()
+                tmp_episode[SampleBatch.ACTIONS] = all_actions.T
+                action_probs = np.exp(self.action_log_likelihood(tmp_episode)).T
+                v_values = self.model.estimate_v(episode[SampleBatch.OBS], action_probs)
+                v_values = convert_to_numpy(v_values)
+
+                for t in reversed(range(episode.count)):
+                    v_old = rewards[t] + self.gamma * v_old
+                    v_new = v_values[t] + (new_prob[t] / old_prob[t]) * (
+                        rewards[t] + self.gamma * v_new - q_values[t]
+                    )
+                v_new = v_new.item()
+
+                estimates["v_old"].append(v_old)
+                estimates["v_new"].append(v_new)
+                estimates["v_gain"].append(v_new / max(v_old, 1e-8))
+        estimates["v_old_std"] = np.std(estimates["v_old"])
+        estimates["v_old"] = np.mean(estimates["v_old"])
+        estimates["v_new_std"] = np.std(estimates["v_new"])
+        estimates["v_new"] = np.mean(estimates["v_new"])
+        estimates["v_gain_std"] = np.std(estimates["v_gain"])
+        estimates["v_gain"] = np.mean(estimates["v_gain"])
+        return estimates
