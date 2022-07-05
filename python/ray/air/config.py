2682e2223870b525ce8313eff0cef164bd44ac0c
Antoni Baum <antoni.baum@protonmail.com>
2022-07-05 19:51:22 +0000

    Add logic

--- python/ray/air/config.py
+++ python/ray/air/config.py
@@ -69,6 +69,8 @@ class ScalingConfigDataClass:
                 )
 
     def __eq__(self, o: "ScalingConfigDataClass") -> bool:
+        if not type(o) is type(self):
+            return False
         return self.as_placement_group_factory() == o.as_placement_group_factory()
 
     @property
