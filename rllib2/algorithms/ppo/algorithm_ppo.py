434ac1c66e3aad9678b96a275f6aa7ddcd68d735
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2022-07-05 19:42:32 -0700

    wip

--- rllib2/algorithms/ppo/algorithm_ppo.py
+++ rllib2/algorithms/ppo/algorithm_ppo.py
@@ -28,19 +28,28 @@ class PPO(Algorithm):
         policyID on each worker and collect the on-policy samples and put them inside 
         a MASampleBatch object.
 
-        Note: unlike before where the postprocess_trajectory inside the policy got called it won't get impliciltly called here. We can either provide a callback for env/architecture specific postprocessings or if it is an algorithm specific postprocessings (e.g. ppo needs compute_gae) we will be explicit about it during the loss computation inside RLModule. 
+        Note: unlike before where the postprocess_trajectory inside the policy got 
+        called it won't get impliciltly called here. We can either provide a callback 
+        for env/architecture specific postprocessings or if it is an algorithm specific 
+        postprocessings (e.g. ppo needs compute_gae) we will be explicit about it 
+        during the loss computation inside RLModule. 
         """
 
         train_batch: MultiAgentSampleBatch = ...
 
         """
         On the local process, update all the NNs based on the collected on-policy 
-        trajectories. In the multi-agent case it is assumed we will have a collection of PPOtrainers. So they all have the same logic for updating the NNs.
+        trajectories. In the multi-agent case it is assumed we will have a collection 
+        of PPOtrainers. So they all have the same logic for updating the NNs.
         
         Under the hood, the algorithm will loop through each RLModuleID key and call its
-        corresponding RLTrainer update method. ppo also updates kl divergence on every iteration. 
+        corresponding RLTrainer update method. ppo also updates kl divergence on every 
+        iteration. 
         
-        Note: We can control some event-based updates by setting specific kwargs flags to true. These flags will be broadcasted to each individual trainers update() method. It can also be a dictionary like update_kl = {'policy_1': True, 'policy_2': False} to provide different update rules for different policies.
+        Note: We can control some event-based updates by setting specific kwargs flags 
+        to true. These flags will be broadcasted to each individual trainers update() 
+        method. It can also be a dictionary like update_kl = {'policy_1': True, 
+        'policy_2': False} to provide different update rules for different policies.
         """
         self.rl_trainer.update(train_batch, update_kl=True)
 
