a28827e0068a7c30e895ed1c8fc0af2ef50ea87b
Cade Daniel <cade@anyscale.com>
2022-07-05 23:15:28 -0700

    Making prototype easier to run

--- meta_test.sh
+++ meta_test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
+# All changes under test should be committed, else they won't get picked up.
 git add release/
 git commit -m 'WIP'
 
-#CADE_RAY_WHEEL_URL=file://Users/cade/dev/oss-ray-cluster-test-infra/ray-3.0.0.dev0-cp37-cp37m-macosx_10_15_intel.whl \
 NO_INSTALL=1 \
     RELEASE_AWS_BUCKET=cade-test \
     RAY_TEST_REPO=/Users/cade/dev/oss-ray-cluster-test-infra/ray \
@@ -11,7 +11,10 @@ NO_INSTALL=1 \
     CADE_SKIP_REINSTALL_RAY=1 \
     ./release/run_release_test.sh \
     cade-test \
-    --ray-wheels http://localhost:8000/ray-3.0.0.dev0-cp37-cp37m-macosx_10_15_intel.whl
+    --ray-wheels https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-3.0.0.dev0-cp37-cp37m-macosx_10_15_intel.whl
+
+
+    #--ray-wheels http://localhost:8000/ray-3.0.0.dev0-cp37-cp37m-macosx_10_15_intel.whl
    # --ray-wheels https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-3.0.0.dev0-cp37-cp37m-macosx_10_15_intel.whl
 
     #CADE_SKIP_RAY_DOWN_COMMAND=1 \
