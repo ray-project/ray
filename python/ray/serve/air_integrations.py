c3fba8193d0539f51e25833538dc27dba0a707ab
Your Name <your@email.com>
2022-07-06 05:00:36 -0400

    nit

--- python/ray/serve/air_integrations.py
+++ python/ray/serve/air_integrations.py
@@ -255,7 +255,9 @@ class PredictorWrapper(SimpleSchemaIngress):
     def reconfigure(self, config):
         """Reconfigure Model from Config Checkpoint"""
         predictor_cls = _load_predictor_cls(config["predictor_cls"])
-        self.model = predictor_cls.from_checkpoint(config["checkpoint"])
+        self.model = predictor_cls.from_checkpoint(
+            Checkpoint.from_dict(config["checkpoint"])
+        )
 
 
 @serve.deployment
