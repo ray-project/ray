ray-project/ray

commit 48a52086455848a16df3f2f0e1d7198386ce11b2

Refactor ObjectManager wait logic to WaitManager (#21369)
- This PR moves the `ObjectManager::Wait` related logic to a separate WaitManager class.
- Fix the wait hang issue by not  relying on the async object location notification, but checking if wait is complete when the local object is added, at that time the object is guaranteed to be local.

src/ray/raylet/node_manager.cc
@@ -267,6 +267,14 @@ NodeManager::NodeManager(instrumented_io_context &io_service, const NodeID &self
       temp_dir_(config.temp_dir),
       initial_config_(config),
       dependency_manager_(object_manager_),
+      wait_manager_(/*is_object_local*/
+                    [this](const ObjectID &object_id) {
+                      return dependency_manager_.CheckObjectLocal(object_id);
+                    },
+                    /*delay_executor*/
+                    [this](std::function<void()> fn, int64_t delay_ms) {
+                      RAY_UNUSED(execute_after(io_service_, fn, delay_ms));
+                    }),
       node_manager_server_("NodeManager", config.node_manager_port,
                            config.node_manager_address == "127.0.0.1"),
       node_manager_service_(io_service, *this),
@@ -1384,10 +1392,6 @@ void NodeManager::ProcessWaitRequestMessage(
   std::vector<ObjectID> object_ids = from_flatbuf<ObjectID>(*message->object_ids());
   const auto refs =
       FlatbufferToObjectReference(*message->object_ids(), *message->owner_addresses());
-  std::unordered_map<ObjectID, rpc::Address> owner_addresses;
-  for (const auto &ref : refs) {
-    owner_addresses.emplace(ObjectID::FromBinary(ref.object_id()), ref.owner_address());
-  }
 
   bool resolve_objects = false;
   for (auto const &object_id : object_ids) {
@@ -1407,17 +1411,15 @@ void NodeManager::ProcessWaitRequestMessage(
     AsyncResolveObjects(client, refs, current_task_id, /*ray_get=*/false,
                         /*mark_worker_blocked*/ was_blocked);
   }
-  int64_t wait_ms = message->timeout();
   uint64_t num_required_objects = static_cast<uint64_t>(message->num_ready_objects());
-  // TODO Remove in the future since it should have already be done in other place
-  ray::Status status = object_manager_.Wait(
-      object_ids, owner_addresses, wait_ms, num_required_objects,
+  wait_manager_.Wait(
+      object_ids, message->timeout(), num_required_objects,
       [this, resolve_objects, was_blocked, client, current_task_id](
-          std::vector<ObjectID> found, std::vector<ObjectID> remaining) {
+          std::vector<ObjectID> ready, std::vector<ObjectID> remaining) {
         // Write the data.
         flatbuffers::FlatBufferBuilder fbb;
         flatbuffers::Offset<protocol::WaitReply> wait_reply = protocol::CreateWaitReply(
-            fbb, to_flatbuf(fbb, found), to_flatbuf(fbb, remaining));
+            fbb, to_flatbuf(fbb, ready), to_flatbuf(fbb, remaining));
         fbb.Finish(wait_reply);
 
         auto status =
@@ -1433,7 +1435,6 @@ void NodeManager::ProcessWaitRequestMessage(
           DisconnectClient(client);
         }
       });
-  RAY_CHECK_OK(status);
 }
 
 void NodeManager::ProcessWaitForDirectActorCallArgsRequestMessage(
@@ -1447,19 +1448,11 @@ void NodeManager::ProcessWaitForDirectActorCallArgsRequestMessage(
   // managers or store an error if the objects have failed.
   const auto refs =
       FlatbufferToObjectReference(*message->object_ids(), *message->owner_addresses());
-  std::unordered_map<ObjectID, rpc::Address> owner_addresses;
-  for (const auto &ref : refs) {
-    owner_addresses.emplace(ObjectID::FromBinary(ref.object_id()), ref.owner_address());
-  }
   AsyncResolveObjects(client, refs, TaskID::Nil(), /*ray_get=*/false,
                       /*mark_worker_blocked*/ false);
-  // Reply to the client once a location has been found for all arguments.
-  // NOTE(swang): ObjectManager::Wait currently returns as soon as any location
-  // has been found, so the object may still be on a remote node when the
-  // client receives the reply.
-  ray::Status status = object_manager_.Wait(
-      object_ids, owner_addresses, -1, object_ids.size(),
-      [this, client, tag](std::vector<ObjectID> found, std::vector<ObjectID> remaining) {
+  wait_manager_.Wait(
+      object_ids, -1, object_ids.size(),
+      [this, client, tag](std::vector<ObjectID> ready, std::vector<ObjectID> remaining) {
         RAY_CHECK(remaining.empty());
         std::shared_ptr<WorkerInterface> worker =
             worker_pool_.GetRegisteredWorker(client);
@@ -1469,7 +1462,6 @@ void NodeManager::ProcessWaitForDirectActorCallArgsRequestMessage(
           worker->DirectActorCallArgWaitComplete(tag);
         }
       });
-  RAY_CHECK_OK(status);
 }
 
 void NodeManager::ProcessPushErrorRequestMessage(const uint8_t *message_data) {
@@ -1950,6 +1942,9 @@ void NodeManager::HandleObjectLocal(const ObjectInfo &object_info) {
                  << " tasks ready";
   cluster_task_manager_->TasksUnblocked(ready_task_ids);
 
+  // Notify the wait manager that this object is local.
+  wait_manager_.HandleObjectLocal(object_id);
+
   auto waiting_workers = absl::flat_hash_set<std::shared_ptr<WorkerInterface>>();
   {
     absl::MutexLock guard(&plasma_object_notification_lock_);
@@ -2073,6 +2068,7 @@ std::string NodeManager::DebugString() const {
   result << "\n" << gcs_client_->DebugString();
   result << "\n" << worker_pool_.DebugString();
   result << "\n" << dependency_manager_.DebugString();
+  result << "\n" << wait_manager_.DebugString();
   result << "\n" << core_worker_subscriber_->DebugString();
   {
     absl::MutexLock guard(&plasma_object_notification_lock_);

src/ray/raylet/wait_manager.cc
@@ -1,0 +1,121 @@
+// Copyright 2022 The Ray Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "ray/raylet/wait_manager.h"
+
+namespace ray {
+namespace raylet {
+
+void WaitManager::Wait(const std::vector<ObjectID> &object_ids, int64_t timeout_ms,
+                       uint64_t num_required_objects, const WaitCallback &callback) {
+  RAY_CHECK(timeout_ms >= 0 || timeout_ms == -1);
+  RAY_CHECK(num_required_objects != 0);
+  RAY_CHECK(num_required_objects <= object_ids.size())
+      << num_required_objects << " " << object_ids.size();
+
+  const uint64_t wait_id = next_wait_id_++;
+  wait_requests_.emplace(
+      wait_id, WaitRequest(timeout_ms, callback, object_ids, num_required_objects));
+
+  auto &wait_request = wait_requests_.at(wait_id);
+  for (const auto &object_id : object_ids) {
+    if (is_object_local_(object_id)) {
+      wait_request.ready.emplace(object_id);
+    }
+  }
+
+  for (const auto &object_id : wait_request.object_ids) {
+    object_to_wait_requests_[object_id].emplace(wait_id);
+  }
+
+  if (wait_request.ready.size() >= wait_request.num_required_objects ||
+      wait_request.timeout_ms == 0) {
+    // Requirements already satisfied.
+    WaitComplete(wait_id);
+  } else if (wait_request.timeout_ms != -1) {
+    // If a timeout was provided, then set a timer. If there are no
+    // enough locally available objects by the time the timer expires,
+    // then we will return from the Wait.
+    delay_executor_(
+        [this, wait_id]() {
+          if (wait_requests_.find(wait_id) == wait_requests_.end()) {
+            // The wait is already complete by the time the timer expires,
+            // so we don't need to do anything here.
+            return;
+          }
+          WaitComplete(wait_id);
+        },
+        wait_request.timeout_ms);
+  }
+}
+
+void WaitManager::WaitComplete(uint64_t wait_id) {
+  auto iter = wait_requests_.find(wait_id);
+  RAY_CHECK(iter != wait_requests_.end());
+  auto &wait_request = iter->second;
+
+  for (const auto &object_id : wait_request.object_ids) {
+    auto &requests = object_to_wait_requests_.at(object_id);
+    requests.erase(wait_id);
+    if (requests.empty()) {
+      object_to_wait_requests_.erase(object_id);
+    }
+  }
+
+  // Order objects according to input order.
+  std::vector<ObjectID> ready;
+  std::vector<ObjectID> remaining;
+  for (const auto &object_id : wait_request.object_ids) {
+    if (ready.size() < wait_request.num_required_objects &&
+        wait_request.ready.count(object_id) > 0) {
+      ready.push_back(object_id);
+    } else {
+      remaining.push_back(object_id);
+    }
+  }
+  wait_request.callback(ready, remaining);
+  wait_requests_.erase(wait_id);
+  RAY_LOG(DEBUG) << "Wait request " << wait_id << " finished: ready " << ready.size()
+                 << " remaining " << remaining.size();
+}
+
+void WaitManager::HandleObjectLocal(const ray::ObjectID &object_id) {
+  if (object_to_wait_requests_.find(object_id) == object_to_wait_requests_.end()) {
+    return;
+  }
+
+  std::vector<uint64_t> complete_waits;
+  for (const auto &wait_id : object_to_wait_requests_.at(object_id)) {
+    auto wait_request_iter = wait_requests_.find(wait_id);
+    RAY_CHECK(wait_request_iter != wait_requests_.end());
+    auto &wait_request = wait_request_iter->second;
+    wait_request.ready.emplace(object_id);
+    if (wait_request.ready.size() >= wait_request.num_required_objects) {
+      complete_waits.emplace_back(wait_id);
+    }
+  }
+  for (const auto &wait_id : complete_waits) {
+    WaitComplete(wait_id);
+  }
+}
+
+std::string WaitManager::DebugString() const {
+  std::stringstream ss;
+  ss << "WaitManager:";
+  ss << "\n- num active wait requests: " << wait_requests_.size();
+  return ss.str();
+}
+
+}  // namespace raylet
+}  // namespace ray

BUILD.bazel
@@ -1358,6 +1358,18 @@ cc_test(
 )
 
 cc_test(
+    name = "wait_manager_test",
+    size = "small",
+    srcs = ["src/ray/raylet/wait_manager_test.cc"],
+    copts = COPTS,
+    tags = ["team:core"],
+    deps = [
+        ":raylet_lib",
+        "@com_google_googletest//:gtest_main",
+    ],
+)
+
+cc_test(
     name = "client_connection_test",
     size = "small",
     srcs = ["src/ray/common/test/client_connection_test.cc"],

src/ray/object_manager/object_manager.cc
@@ -505,197 +505,6 @@ void ObjectManager::SendObjectChunk(const UniqueID &push_id, const ObjectID &obj
   rpc_client->Push(push_request, callback);
 }
 
-ray::Status ObjectManager::Wait(
-    const std::vector<ObjectID> &object_ids,
-    const std::unordered_map<ObjectID, rpc::Address> &owner_addresses, int64_t timeout_ms,
-    uint64_t num_required_objects, const WaitCallback &callback) {
-  UniqueID wait_id = UniqueID::FromRandom();
-  RAY_LOG(DEBUG) << "Wait request " << wait_id << " on " << self_node_id_;
-  RAY_RETURN_NOT_OK(AddWaitRequest(wait_id, object_ids, owner_addresses, timeout_ms,
-                                   num_required_objects, callback));
-  RAY_RETURN_NOT_OK(LookupRemainingWaitObjects(wait_id));
-  // LookupRemainingWaitObjects invokes SubscribeRemainingWaitObjects once lookup has
-  // been performed on all remaining objects.
-  return ray::Status::OK();
-}
-
-ray::Status ObjectManager::AddWaitRequest(
-    const UniqueID &wait_id, const std::vector<ObjectID> &object_ids,
-    const std::unordered_map<ObjectID, rpc::Address> &owner_addresses, int64_t timeout_ms,
-    uint64_t num_required_objects, const WaitCallback &callback) {
-  RAY_CHECK(timeout_ms >= 0 || timeout_ms == -1);
-  RAY_CHECK(num_required_objects != 0);
-  RAY_CHECK(num_required_objects <= object_ids.size())
-      << num_required_objects << " " << object_ids.size();
-  if (object_ids.size() == 0) {
-    callback(std::vector<ObjectID>(), std::vector<ObjectID>());
-  }
-
-  // Initialize fields.
-  active_wait_requests_.emplace(wait_id, WaitState(*main_service_, timeout_ms, callback));
-  auto &wait_state = active_wait_requests_.find(wait_id)->second;
-  wait_state.object_id_order = object_ids;
-  wait_state.owner_addresses = owner_addresses;
-  wait_state.timeout_ms = timeout_ms;
-  wait_state.num_required_objects = num_required_objects;
-  for (const auto &object_id : object_ids) {
-    if (local_objects_.count(object_id) > 0) {
-      wait_state.found.insert(object_id);
-    } else {
-      wait_state.remaining.insert(object_id);
-    }
-  }
-
-  return ray::Status::OK();
-}
-
-ray::Status ObjectManager::LookupRemainingWaitObjects(const UniqueID &wait_id) {
-  auto &wait_state = active_wait_requests_.find(wait_id)->second;
-
-  if (wait_state.remaining.empty()) {
-    WaitComplete(wait_id);
-  } else {
-    // We invoke lookup calls immediately after checking which objects are local to
-    // obtain current information about the location of remote objects. Thus,
-    // we obtain information about all given objects, regardless of their location.
-    // This is required to ensure we do not bias returning locally available objects
-    // as ready whenever Wait is invoked with a mixture of local and remote objects.
-    for (const auto &object_id : wait_state.remaining) {
-      // Lookup remaining objects.
-      wait_state.requested_objects.insert(object_id);
-      RAY_RETURN_NOT_OK(object_directory_->LookupLocations(
-          object_id, wait_state.owner_addresses[object_id],
-          [this, wait_id](const ObjectID &lookup_object_id,
-                          const std::unordered_set<NodeID> &node_ids,
-                          const std::string &spilled_url, const NodeID &spilled_node_id,
-                          bool pending_creation, size_t object_size) {
-            auto &wait_state = active_wait_requests_.find(wait_id)->second;
-            // Note that the object is guaranteed to be added to local_objects_ before
-            // the notification is triggered.
-            if (local_objects_.count(lookup_object_id) > 0) {
-              wait_state.remaining.erase(lookup_object_id);
-              wait_state.found.insert(lookup_object_id);
-            }
-            RAY_LOG(DEBUG) << "Wait request " << wait_id << ": " << node_ids.size()
-                           << " locations found for object " << lookup_object_id;
-            wait_state.requested_objects.erase(lookup_object_id);
-            if (wait_state.requested_objects.empty()) {
-              SubscribeRemainingWaitObjects(wait_id);
-            }
-          }));
-    }
-  }
-  return ray::Status::OK();
-}
-
-void ObjectManager::SubscribeRemainingWaitObjects(const UniqueID &wait_id) {
-  auto &wait_state = active_wait_requests_.find(wait_id)->second;
-  if (wait_state.found.size() >= wait_state.num_required_objects ||
-      wait_state.timeout_ms == 0) {
-    // Requirements already satisfied.
-    WaitComplete(wait_id);
-    return;
-  }
-
-  // There are objects remaining whose locations we don't know. Request their
-  // locations from the object directory.
-  for (const auto &object_id : wait_state.object_id_order) {
-    if (wait_state.remaining.count(object_id) > 0) {
-      RAY_LOG(DEBUG) << "Wait request " << wait_id << ": subscribing to object "
-                     << object_id;
-      wait_state.requested_objects.insert(object_id);
-      // Subscribe to object notifications.
-      RAY_CHECK_OK(object_directory_->SubscribeObjectLocations(
-          wait_id, object_id, wait_state.owner_addresses[object_id],
-          [this, wait_id](const ObjectID &subscribe_object_id,
-                          const std::unordered_set<NodeID> &node_ids,
-                          const std::string &spilled_url, const NodeID &spilled_node_id,
-                          bool pending_creation, size_t object_size) {
-            auto object_id_wait_state = active_wait_requests_.find(wait_id);
-            if (object_id_wait_state == active_wait_requests_.end()) {
-              // Depending on the timing of calls to the object directory, we
-              // may get a subscription notification after the wait call has
-              // already completed. If so, then don't process the
-              // notification.
-              return;
-            }
-            auto &wait_state = object_id_wait_state->second;
-            // Note that the object is guaranteed to be added to local_objects_ before
-            // the notification is triggered.
-            if (local_objects_.count(subscribe_object_id) > 0) {
-              RAY_LOG(DEBUG) << "Wait request " << wait_id
-                             << ": subscription notification received for object "
-                             << subscribe_object_id;
-              wait_state.remaining.erase(subscribe_object_id);
-              wait_state.found.insert(subscribe_object_id);
-              wait_state.requested_objects.erase(subscribe_object_id);
-              RAY_CHECK_OK(object_directory_->UnsubscribeObjectLocations(
-                  wait_id, subscribe_object_id));
-              if (wait_state.found.size() >= wait_state.num_required_objects) {
-                WaitComplete(wait_id);
-              }
-            }
-          }));
-    }
-
-    // If a timeout was provided, then set a timer. If we don't find locations
-    // for enough objects by the time the timer expires, then we will return
-    // from the Wait.
-    if (wait_state.timeout_ms != -1) {
-      auto timeout = boost::posix_time::milliseconds(wait_state.timeout_ms);
-      wait_state.timeout_timer->expires_from_now(timeout);
-      wait_state.timeout_timer->async_wait(
-          [this, wait_id](const boost::system::error_code &error_code) {
-            if (error_code.value() != 0) {
-              return;
-            }
-            if (active_wait_requests_.find(wait_id) == active_wait_requests_.end()) {
-              // When a subscription callback is triggered first, WaitComplete will be
-              // called. The timer may at the same time goes off and may be an
-              // interruption will post WaitComplete to main_service_ the second time.
-              // This check will avoid the duplicated call of this function.
-              return;
-            }
-            WaitComplete(wait_id);
-          });
-    }
-  }
-}
-
-void ObjectManager::WaitComplete(const UniqueID &wait_id) {
-  auto iter = active_wait_requests_.find(wait_id);
-  RAY_CHECK(iter != active_wait_requests_.end());
-  auto &wait_state = iter->second;
-  // If we complete with outstanding requests, then timeout_ms should be non-zero or -1
-  // (infinite wait time).
-  if (!wait_state.requested_objects.empty()) {
-    RAY_CHECK(wait_state.timeout_ms > 0 || wait_state.timeout_ms == -1);
-  }
-  // Unsubscribe to any objects that weren't found in the time allotted.
-  for (const auto &object_id : wait_state.requested_objects) {
-    RAY_CHECK_OK(object_directory_->UnsubscribeObjectLocations(wait_id, object_id));
-  }
-  // Cancel the timer. This is okay even if the timer hasn't been started.
-  // The timer handler will be given a non-zero error code. The handler
-  // will do nothing on non-zero error codes.
-  wait_state.timeout_timer->cancel();
-  // Order objects according to input order.
-  std::vector<ObjectID> found;
-  std::vector<ObjectID> remaining;
-  for (const auto &item : wait_state.object_id_order) {
-    if (found.size() < wait_state.num_required_objects &&
-        wait_state.found.count(item) > 0) {
-      found.push_back(item);
-    } else {
-      remaining.push_back(item);
-    }
-  }
-  wait_state.callback(found, remaining);
-  active_wait_requests_.erase(wait_id);
-  RAY_LOG(DEBUG) << "Wait request " << wait_id << " finished: found " << found.size()
-                 << " remaining " << remaining.size();
-}
-
 /// Implementation of ObjectManagerServiceHandler
 void ObjectManager::HandlePush(const rpc::PushRequest &request, rpc::PushReply *reply,
                                rpc::SendReplyCallback send_reply_callback) {
@@ -848,7 +657,6 @@ std::string ObjectManager::DebugString() const {
   std::stringstream result;
   result << "ObjectManager:";
   result << "\n- num local objects: " << local_objects_.size();
-  result << "\n- num active wait requests: " << active_wait_requests_.size();
   result << "\n- num unfulfilled push requests: " << unfulfilled_push_requests_.size();
   result << "\n- num pull requests: " << pull_manager_->NumActiveRequests();
   result << "\n- num chunks received total: " << num_chunks_received_total_;

src/ray/object_manager/object_manager.h
@@ -211,24 +211,6 @@ class ObjectManager : public ObjectManagerInterface,
   /// \param pull_request_id The request to cancel.
   void CancelPull(uint64_t pull_request_id) override;
 
-  /// Callback definition for wait.
-  using WaitCallback = std::function<void(const std::vector<ray::ObjectID> &found,
-                                          const std::vector<ray::ObjectID> &remaining)>;
-  /// Wait until either num_required_objects are located or wait_ms has elapsed,
-  /// then invoke the provided callback.
-  ///
-  /// \param object_ids The object ids to wait on.
-  /// \param timeout_ms The time in milliseconds to wait before invoking the callback.
-  /// \param num_required_objects The minimum number of objects required before
-  /// invoking the callback.
-  /// \param callback Invoked when either timeout_ms is satisfied OR num_ready_objects
-  /// is satisfied.
-  /// \return Status of whether the wait successfully initiated.
-  ray::Status Wait(const std::vector<ObjectID> &object_ids,
-                   const std::unordered_map<ObjectID, rpc::Address> &owner_addresses,
-                   int64_t timeout_ms, uint64_t num_required_objects,
-                   const WaitCallback &callback);
-
   /// Free a list of objects from object store.
   ///
   /// \param object_ids the The list of ObjectIDs to be deleted.
@@ -264,49 +246,6 @@ class ObjectManager : public ObjectManagerInterface,
 
  private:
   friend class TestObjectManager;
-
-  struct WaitState {
-    WaitState(instrumented_io_context &service, int64_t timeout_ms,
-              const WaitCallback &callback)
-        : timeout_ms(timeout_ms),
-          timeout_timer(std::make_unique<boost::asio::deadline_timer>(
-              service, boost::posix_time::milliseconds(timeout_ms))),
-          callback(callback) {}
-    /// The period of time to wait before invoking the callback.
-    int64_t timeout_ms;
-    /// The timer used whenever wait_ms > 0.
-    std::unique_ptr<boost::asio::deadline_timer> timeout_timer;
-    /// The callback invoked when WaitCallback is complete.
-    WaitCallback callback;
-    /// Ordered input object_ids.
-    std::vector<ObjectID> object_id_order;
-    /// Objects' owners.
-    std::unordered_map<ObjectID, rpc::Address> owner_addresses;
-    /// The objects that have not yet been found.
-    std::unordered_set<ObjectID> remaining;
-    /// The objects that have been found.
-    std::unordered_set<ObjectID> found;
-    /// Objects that have been requested either by Lookup or Subscribe.
-    std::unordered_set<ObjectID> requested_objects;
-    /// The number of required objects.
-    uint64_t num_required_objects;
-  };
-
-  /// Creates a wait request and adds it to active_wait_requests_.
-  ray::Status AddWaitRequest(
-      const UniqueID &wait_id, const std::vector<ObjectID> &object_ids,
-      const std::unordered_map<ObjectID, rpc::Address> &owner_addresses,
-      int64_t timeout_ms, uint64_t num_required_objects, const WaitCallback &callback);
-
-  /// Lookup any remaining objects that are not local. This is invoked after
-  /// the wait request is created and local objects are identified.
-  ray::Status LookupRemainingWaitObjects(const UniqueID &wait_id);
-
-  /// Invoked when lookup for remaining objects has been invoked. This method subscribes
-  /// to any remaining objects if wait conditions have not yet been satisfied.
-  void SubscribeRemainingWaitObjects(const UniqueID &wait_id);
-  /// Completion handler for Wait.
-  void WaitComplete(const UniqueID &wait_id);
 
   /// Spread the Free request to all objects managers.
   ///
@@ -458,9 +397,6 @@ class ObjectManager : public ObjectManagerInterface,
   /// SubscribeObjectLocations. We only need one identifier because we never need to
   /// subscribe multiple times to the same object during Pull.
   UniqueID object_directory_pull_callback_id_ = UniqueID::FromRandom();
-
-  /// A set of active wait requests.
-  std::unordered_map<UniqueID, WaitState> active_wait_requests_;
 
   /// Maintains a map of push requests that have not been fulfilled due to an object not
   /// being local. Objects are removed from this map after push_timeout_ms have elapsed.

src/ray/raylet/node_manager.h
@@ -34,6 +34,7 @@
 #include "ray/raylet/scheduling/cluster_task_manager.h"
 #include "ray/raylet/scheduling/cluster_task_manager_interface.h"
 #include "ray/raylet/dependency_manager.h"
+#include "ray/raylet/wait_manager.h"
 #include "ray/raylet/worker_pool.h"
 #include "ray/rpc/worker/core_worker_client_pool.h"
 #include "ray/util/ordered_set.h"
@@ -655,6 +656,9 @@ class NodeManager : public rpc::NodeManagerServiceHandler {
   /// called `ray.get` or `ray.wait`.
   DependencyManager dependency_manager_;
 
+  /// A manager for wait requests.
+  WaitManager wait_manager_;
+
   std::shared_ptr<AgentManager> agent_manager_;
 
   /// The RPC server.
@@ -756,4 +760,4 @@ class NodeManager : public rpc::NodeManagerServiceHandler {
 
 }  // namespace raylet
 
-}  // end namespace ray
+}  // namespace ray

commit e31baebc4ea03f07df2136862cc38a2a3ab29f60

[Core] Fix WaitManager dealing with duplicate objects (#26256)
When calling an actor method with duplicate ObjectRefs, the actor method will never be executed. The root cause is that `WaitRequest::ready` is of type `std::unordered_set` rather than `std::vector`.

https://github.com/ray-project/ray/blob/b9ade079cb2de72332419167305b9ee49d2b0930/src/ray/raylet/wait_manager.h#L77

So the below if conditions won't be true.

https://github.com/ray-project/ray/blob/b9ade079cb2de72332419167305b9ee49d2b0930/src/ray/raylet/wait_manager.cc#L45-L48

https://github.com/ray-project/ray/blob/b9ade079cb2de72332419167305b9ee49d2b0930/src/ray/raylet/wait_manager.cc#L103-L105

The bug was introduced by https://github.com/ray-project/ray/pull/21369, so it exists in Ray 1.11.0+.

src/ray/raylet/node_manager.cc
@@ -1681,6 +1681,9 @@ void NodeManager::ProcessWaitForDirectActorCallArgsRequestMessage(
                       TaskID::Nil(),
                       /*ray_get=*/false,
                       /*mark_worker_blocked*/ false);
+  // De-duplicate the object IDs.
+  absl::flat_hash_set<ObjectID> object_id_set(object_ids.begin(), object_ids.end());
+  object_ids.assign(object_id_set.begin(), object_id_set.end());
   wait_manager_.Wait(
       object_ids,
       -1,

src/ray/raylet/wait_manager.cc
@@ -23,6 +23,10 @@ void WaitManager::Wait(const std::vector<ObjectID> &object_ids,
                        int64_t timeout_ms,
                        uint64_t num_required_objects,
                        const WaitCallback &callback) {
+  absl::flat_hash_set<ObjectID> object_id_set(object_ids.begin(), object_ids.end());
+  RAY_CHECK_EQ(object_id_set.size(), object_ids.size())
+      << "Waiting duplicate objects is not allowed. Please make sure all object IDs are "
+         "unique before calling `WaitManager::Wait`.";
   RAY_CHECK(timeout_ms >= 0 || timeout_ms == -1);
   RAY_CHECK_NE(num_required_objects, 0u);
   RAY_CHECK_LE(num_required_objects, object_ids.size());

python/ray/tests/test_basic_2.py
@@ -649,6 +649,31 @@ def test_duplicate_args(ray_start_regular_shared):
     arg2 = ray.put([2])
     ray.get(f.remote(arg1, arg2, arg1, kwarg1=arg1, kwarg2=arg2, kwarg1_duplicate=arg1))
 
+    # Test by-reference arguments on an actor task.
+    @ray.remote
+    class Actor:
+        def f(
+            self,
+            arg1,
+            arg2,
+            arg1_duplicate,
+            kwarg1=None,
+            kwarg2=None,
+            kwarg1_duplicate=None,
+        ):
+            assert arg1 == kwarg1
+            assert arg1 != arg2
+            assert arg1 == arg1_duplicate
+            assert kwarg1 != kwarg2
+            assert kwarg1 == kwarg1_duplicate
+
+    actor = Actor.remote()
+    ray.get(
+        actor.f.remote(
+            arg1, arg2, arg1, kwarg1=arg1, kwarg2=arg2, kwarg1_duplicate=arg1
+        )
+    )
+
 
 @pytest.mark.skipif(client_test_enabled(), reason="internal api")
 def test_get_correct_node_ip():