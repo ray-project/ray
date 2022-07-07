2edbab6ae6c1dd0c01de5dc992e9d11800825364
久龙 <guyang.sgy@antfin.com>
2022-07-07 12:02:21 +0800

    fix

--- tmp_test.sh
+++ tmp_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -x
-bazel test --config=ci $(./ci/run/bazel_export_options) \
+bazel test --config=ci $(./ci/run/bazel_export_options) --runs_per_test=1 \
 --test_tag_filters=post_wheel_build \
 --test_env=CONDA_EXE \
 --test_env=CONDA_PYTHON_EXE \
@@ -12,7 +12,7 @@ bazel test --config=ci $(./ci/run/bazel_export_options) \
 --test_env=RAY_CI_POST_WHEEL_TESTS=True \
 python/ray/tests/... python/ray/serve/... python/ray/tune/... rllib/... doc/...
 
-# cat /tmp/ray/session_*/logs/*
+cat /tmp/ray/session_*/logs/*
 
 
 
