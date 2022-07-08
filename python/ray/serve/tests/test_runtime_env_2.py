4264a897045c6bf3e614d4558fb573a604bc6dab
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2022-07-08 13:55:39 -0700

    Skip flaky client serve runtime_env test

--- python/ray/serve/tests/test_runtime_env_2.py
+++ python/ray/serve/tests/test_runtime_env_2.py
@@ -4,9 +4,8 @@ import sys
 from ray._private.test_utils import run_string_as_driver
 
 
-@pytest.mark.parametrize("use_ray_client", [False, True])
 @pytest.mark.skipif(sys.platform == "win32", reason="Fail to create temp dir.")
-def test_working_dir_deploy_new_version(ray_start, tmp_dir, use_ray_client):
+def test_working_dir_deploy_new_version(ray_start, tmp_dir):
     with open("hello", "w") as f:
         f.write("world")
 
@@ -15,10 +14,7 @@ import ray
 from ray import serve
 
 job_config = ray.job_config.JobConfig(runtime_env={{"working_dir": "."}})
-if {use_ray_client}:
-    ray.util.connect("{client_addr}", namespace="serve", job_config=job_config)
-else:
-    ray.init(address="auto", namespace="serve", job_config=job_config)
+ray.init(address="auto", namespace="serve", job_config=job_config)
 
 serve.start(detached=True)
 
@@ -30,9 +26,7 @@ class Test:
 Test.deploy()
 handle = Test.get_handle()
 assert ray.get(handle.remote()) == "world"
-""".format(
-        use_ray_client=use_ray_client, client_addr=ray_start
-    )
+"""
 
     run_string_as_driver(driver1)
 
@@ -44,10 +38,7 @@ import ray
 from ray import serve
 
 job_config = ray.job_config.JobConfig(runtime_env={{"working_dir": "."}})
-if {use_ray_client}:
-    ray.util.connect("{client_addr}", namespace="serve", job_config=job_config)
-else:
-    ray.init(address="auto", namespace="serve", job_config=job_config)
+ray.init(address="auto", namespace="serve", job_config=job_config)
 
 serve.start(detached=True)
 
@@ -60,28 +51,22 @@ Test.deploy()
 handle = Test.get_handle()
 assert ray.get(handle.remote()) == "world2"
 Test.delete()
-""".format(
-        use_ray_client=use_ray_client, client_addr=ray_start
-    )
+"""
 
     run_string_as_driver(driver2)
 
 
-@pytest.mark.parametrize("use_ray_client", [False, True])
 @pytest.mark.skipif(
     sys.platform == "win32", reason="Runtime env unsupported on Windows"
 )
-def test_pip_no_working_dir(ray_start, use_ray_client):
+def test_pip_no_working_dir(ray_start):
 
     driver = """
 import ray
 from ray import serve
 import requests
 
-if {use_ray_client}:
-    ray.util.connect("{client_addr}")
-else:
-    ray.init(address="auto")
+ray.init(address="auto")
 
 serve.start()
 
@@ -99,9 +84,7 @@ requests_version.options(
     }}).deploy()
 
 assert requests.get("http://127.0.0.1:8000/requests_version").text == "2.25.1"
-""".format(
-        use_ray_client=use_ray_client, client_addr=ray_start
-    )
+"""
 
     run_string_as_driver(driver)
 
