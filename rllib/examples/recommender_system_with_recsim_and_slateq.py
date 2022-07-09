6b5d9cd4401842caa11c3e11dfa46b6c59893310
Artur Niederfahrenhorst <artur@anyscale.com>
2022-07-09 21:08:44 +0200

    fix recsim example

--- rllib/examples/recommender_system_with_recsim_and_slateq.py
+++ rllib/examples/recommender_system_with_recsim_and_slateq.py
@@ -73,7 +73,7 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    "--min-size-for-sampling",
+    "--min_size",
     type=int,
     default=20000,
     help="The number of timesteps to add to the replay buffer before sampling from "
