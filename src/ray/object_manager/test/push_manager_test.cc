03af7dedf4f629fa68b4e0c6736ece16a4ca9f3b
scv119 <scv119@gmail.com>
2022-07-12 14:50:11 -0700

    add

--- src/ray/object_manager/test/push_manager_test.cc
+++ src/ray/object_manager/test/push_manager_test.cc
@@ -41,6 +41,7 @@ TEST(TestPushManager, TestSingleTransfer) {
 }
 
 TEST(TestPushManager, TestPushState) {
+  // normal sending.
   {
     std::vector<int64_t> sent_chunks;
     PushManager::PushState state{
@@ -76,7 +77,7 @@ TEST(TestPushManager, TestPushState) {
     ASSERT_TRUE(state.AllChunksComplete());
   }
 
-  // Resent all chunks.
+  // resend all chunks.
   {
     std::vector<int64_t> sent_chunks;
     PushManager::PushState state{
@@ -90,6 +91,7 @@ TEST(TestPushManager, TestPushState) {
     std::vector<int64_t> expected_chunks{0};
     ASSERT_EQ(sent_chunks, expected_chunks);
 
+    // resend chunks when 1 chunk is in flight.
     ASSERT_EQ(1, state.ResendAllChunks());
     ASSERT_EQ(state.num_chunks, 3);
     ASSERT_EQ(state.next_chunk_id, 1);
@@ -118,41 +120,44 @@ TEST(TestPushManager, TestPushState) {
 }
 
 TEST(TestPushManager, TestRetryDuplicates) {
-  // std::vector<int> results;
-  // results.resize(10);
-  // auto node_id = NodeID::FromRandom();
-  // auto obj_id = ObjectID::FromRandom();
-  // PushManager pm(5);
+  std::vector<int> results;
+  results.resize(10);
+  auto node_id = NodeID::FromRandom();
+  auto obj_id = ObjectID::FromRandom();
+  PushManager pm(5);
 
-  // // First send.
-  // pm.StartPush(node_id, obj_id, 10, [&](int64_t chunk_id) { results[chunk_id] = 1; });
-  // // Duplicates are all ignored.
-  // pm.StartPush(node_id, obj_id, 10, [&](int64_t chunk_id) { results[chunk_id] = 2; });
-  // ASSERT_EQ(pm.NumChunksInFlight(), 5);
-  // ASSERT_EQ(pm.NumChunksRemaining(), 15);
-  // ASSERT_EQ(pm.NumPushesInFlight(), 1);
-  // for (int i = 0; i < 10; i++) {
-  //   pm.StartPush(node_id, obj_id, 10, [&](int64_t chunk_id) { results[chunk_id] = 2;
-  //   }); pm.OnChunkComplete(node_id, obj_id);
-  // }
-  // ASSERT_EQ(pm.NumChunksInFlight(), 5);
-  // ASSERT_EQ(pm.NumChunksRemaining(), 0);
-  // ASSERT_EQ(pm.NumPushesInFlight(), 0);
-  // for (int i = 0; i < 10; i++) {
-  //   ASSERT_EQ(results[i], 1);
-  // }
-
-  // // Second allowed send.
-  // pm.StartPush(node_id, obj_id, 10, [&](int64_t chunk_id) { results[chunk_id] = 3; });
-  // for (int i = 0; i < 10; i++) {
-  //   pm.OnChunkComplete(node_id, obj_id);
-  // }
-  // ASSERT_EQ(pm.NumChunksInFlight(), 0);
-  // ASSERT_EQ(pm.NumChunksRemaining(), 0);
-  // ASSERT_EQ(pm.NumPushesInFlight(), 0);
-  // for (int i = 0; i < 10; i++) {
-  //   ASSERT_EQ(results[i], 3);
-  // }
+  // First push request.
+  pm.StartPush(node_id, obj_id, 10, [&](int64_t chunk_id) { results[chunk_id] = 1; });
+  ASSERT_EQ(pm.NumChunksInFlight(), 5);
+  ASSERT_EQ(pm.NumChunksRemaining(), 10);
+  ASSERT_EQ(pm.NumPushesInFlight(), 1);
+  // Second push request will resent the full chunks.
+  pm.StartPush(node_id, obj_id, 10, [&](int64_t chunk_id) { results[chunk_id] = 2; });
+  ASSERT_EQ(pm.NumChunksInFlight(), 5);
+  ASSERT_EQ(pm.NumChunksRemaining(), 15);
+  ASSERT_EQ(pm.NumPushesInFlight(), 1);
+  // first 5 chunks will be sent by first push request.
+  for (int i = 0; i < 5; i++) {
+    pm.OnChunkComplete(node_id, obj_id);
+  }
+  for (int i = 0; i < 5; i++) {
+    ASSERT_EQ(results[i], 1);
+  }
+  ASSERT_EQ(pm.NumChunksInFlight(), 5);
+  ASSERT_EQ(pm.NumChunksRemaining(), 10);
+  // we will resend all chunks by second push request.
+  for (int i = 0; i < 10; i++) {
+    pm.OnChunkComplete(node_id, obj_id);
+  }
+  for (int i = 0; i < 10; i++) {
+    ASSERT_EQ(results[i], 2);
+  }
+  ASSERT_EQ(pm.NumChunksInFlight(), 0);
+  ASSERT_EQ(pm.NumChunksRemaining(), 0);
+  ASSERT_EQ(pm.NumPushesInFlight(), 0);
+  for (int i = 0; i < 10; i++) {
+    ASSERT_EQ(results[i], 1);
+  }
 }
 
 TEST(TestPushManager, TestMultipleTransfers) {
