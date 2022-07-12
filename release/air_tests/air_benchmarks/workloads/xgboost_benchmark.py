936340953025a6ae22f25a16aa2e18e1fab64baf
Xiaowei Jiang <xwjiang2010@gmail.com>
2022-07-11 21:33:14 -0700

    up

--- release/air_tests/air_benchmarks/workloads/xgboost_benchmark.py
+++ release/air_tests/air_benchmarks/workloads/xgboost_benchmark.py
@@ -6,7 +6,12 @@ import xgboost as xgb
 
 import ray
 from ray import data
-from ray.train.xgboost import XGBoostTrainer, load_checkpoint, to_air_checkpoint, XGBoostPredictor
+from ray.train.xgboost import (
+    XGBoostTrainer,
+    load_checkpoint,
+    to_air_checkpoint,
+    XGBoostPredictor,
+)
 from ray.train.batch_predictor import BatchPredictor
 
 _XGB_MODEL_PATH = "model.json"
@@ -20,12 +25,15 @@ def time_it(f):
         time_taken = time.monotonic() - start
         print(f"{f.__name__} takes {time_taken} seconds.")
         return result, time_taken
+
     return wrapper
 
 
 @time_it
 def run_xgboost_training():
-    ds = data.read_parquet("s3://air-example-data-2/100G-xgboost-data.parquet/")  # silver tier
+    ds = data.read_parquet(
+        "s3://air-example-data-2/100G-xgboost-data.parquet/"
+    )  # silver tier
     params = {
         "objective": "binary:logistic",
         "eval_metric": ["logloss", "error"],
@@ -45,7 +53,9 @@ def run_xgboost_training():
 def run_xgboost_prediction(model_path: str):
     model = xgb.Booster()
     model.load_model(model_path)
-    ds = data.read_parquet("s3://air-example-data-2/100G-xgboost-data.parquet/")  # silver tier
+    ds = data.read_parquet(
+        "s3://air-example-data-2/100G-xgboost-data.parquet/"
+    )  # silver tier
     ckpt = to_air_checkpoint(".", model)
     batch_predictor = BatchPredictor.from_checkpoint(ckpt, XGBoostPredictor)
     result = batch_predictor.predict(ds.drop_columns(["labels"]))
