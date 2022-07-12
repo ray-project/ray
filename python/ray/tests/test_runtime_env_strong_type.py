0715d222ddd56e9ffee0b805bd70f3f5ef360bb4
久龙 <guyang.sgy@antfin.com>
2022-07-12 17:36:40 +0800

    address comments

--- python/ray/tests/test_runtime_env_strong_type.py
+++ python/ray/tests/test_runtime_env_strong_type.py
@@ -11,42 +11,38 @@ from dataclasses import dataclass
 
 @dataclass
 class ValueType:
-    nfeild1: List[str]
-    nfeild2: bool
+    nfield1: List[str]
+    nfield2: bool
 
 
 @dataclass
 class TestPlugin:
-    feild1: List[ValueType]
-    feild2: str
+    field1: List[ValueType]
+    field2: str
 
 
 def test_convert_from_and_to_dataclass():
     runtime_env = RuntimeEnv()
     test_plugin = TestPlugin(
-        feild1=[
-            ValueType(nfeild1=["a", "b", "c"], nfeild2=False),
-            ValueType(nfeild1=["d", "e"], nfeild2=True),
+        field1=[
+            ValueType(nfield1=["a", "b", "c"], nfield2=False),
+            ValueType(nfield1=["d", "e"], nfield2=True),
         ],
-        feild2="abc",
+        field2="abc",
     )
     runtime_env.set("test_plugin", test_plugin)
     serialized_runtime_env = runtime_env.serialize()
     assert "test_plugin" in serialized_runtime_env
     runtime_env_2 = RuntimeEnv.deserialize(serialized_runtime_env)
     test_plugin_2 = runtime_env_2.get("test_plugin", data_class=TestPlugin)
-    assert len(test_plugin_2.feild1) == 2
-    assert test_plugin_2.feild1[0].nfeild1 == ["a", "b", "c"]
-    assert test_plugin_2.feild1[0].nfeild2 is False
-    assert test_plugin_2.feild1[1].nfeild1 == ["d", "e"]
-    assert test_plugin_2.feild1[1].nfeild2 is True
-    assert test_plugin_2.feild2 == "abc"
+    assert len(test_plugin_2.field1) == 2
+    assert test_plugin_2.field1[0].nfield1 == ["a", "b", "c"]
+    assert test_plugin_2.field1[0].nfield2 is False
+    assert test_plugin_2.field1[1].nfield1 == ["d", "e"]
+    assert test_plugin_2.field1[1].nfield2 is True
+    assert test_plugin_2.field2 == "abc"
 
 
-@pytest.mark.skipif(
-    sys.platform == "win32",
-    reason="Pip option not supported on Windows.",
-)
 def test_pip(start_cluster):
     cluster, address = start_cluster
     ray.init(address)
