88a3aefb8da325d96e35157653ac39d172a17b7c
Chen Shen <scv119@gmail.com>
2022-07-12 18:46:02 -0700

    [Core][Data] Fix resend protocol (#26349)
    When Ray is under memory pressure, the pull manager might cancel ongoing pull request and retry it later. There is a race condition that a pull request is initiated and canceled, and the pull request for the same object is retried by pull manager shortly. When this happens, the pusher (where the object is being pulled) ignores the second pull request if it's still sending the object initiated by the first pull request; instead it will continue sending the remaining chunks. This leads to the puller receiving incomplete data chunks (as some chunks has already being received and then canceled), and the puller has to wait for 10 seconds timeout and retry the pull request.
    
    To fix the problem we simply always resent all chunks when a pull request is received. Since we always send chunks in order, we implement the resend logic by simply reset the remaining number of chunks to send; and treat the chunks as ring buffer.
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- src/ray/object_manager/test/push_manager_test.cc
+++ src/ray/object_manager/test/push_manager_test.cc
@@ -40,42 +40,123 @@ TEST(TestPushManager, TestSingleTransfer) {
   }
 }
 
-TEST(TestPushManager, TestSuppressDuplicates) {
+TEST(TestPushManager, TestPushState) {
+  // normal sending.
+  {
+    std::vector<int64_t> sent_chunks;
+    PushManager::PushState state{
+        2, [&](int64_t chunk_id) { sent_chunks.push_back(chunk_id); }};
+    ASSERT_EQ(state.num_chunks, 2);
+    ASSERT_EQ(state.next_chunk_id, 0);
+    ASSERT_EQ(state.num_chunks_inflight, 0);
+    ASSERT_EQ(state.num_chunks_to_send, 2);
+    ASSERT_TRUE(state.SendOneChunk());
+    ASSERT_FALSE(state.AllChunksComplete());
+    ASSERT_EQ(state.num_chunks, 2);
+    ASSERT_EQ(state.next_chunk_id, 1);
+    ASSERT_EQ(state.num_chunks_inflight, 1);
+    ASSERT_EQ(state.num_chunks_to_send, 1);
+    std::vector<int64_t> expected_chunks{0};
+    ASSERT_EQ(sent_chunks, expected_chunks);
+
+    ASSERT_TRUE(state.SendOneChunk());
+    ASSERT_EQ(state.num_chunks, 2);
+    ASSERT_EQ(state.next_chunk_id, 0);
+    ASSERT_EQ(state.num_chunks_inflight, 2);
+    ASSERT_EQ(state.num_chunks_to_send, 0);
+    std::vector<int64_t> expected_chunks1{0, 1};
+    ASSERT_EQ(sent_chunks, expected_chunks1);
+    ASSERT_FALSE(state.AllChunksComplete());
+
+    ASSERT_FALSE(state.SendOneChunk());
+    state.OnChunkComplete();
+    ASSERT_EQ(state.num_chunks_inflight, 1);
+    ASSERT_FALSE(state.AllChunksComplete());
+    state.OnChunkComplete();
+    ASSERT_EQ(state.num_chunks_inflight, 0);
+    ASSERT_TRUE(state.AllChunksComplete());
+  }
+
+  // resend all chunks.
+  {
+    std::vector<int64_t> sent_chunks;
+    PushManager::PushState state{
+        3, [&](int64_t chunk_id) { sent_chunks.push_back(chunk_id); }};
+    ASSERT_TRUE(state.SendOneChunk());
+    ASSERT_FALSE(state.AllChunksComplete());
+    ASSERT_EQ(state.num_chunks, 3);
+    ASSERT_EQ(state.next_chunk_id, 1);
+    ASSERT_EQ(state.num_chunks_inflight, 1);
+    ASSERT_EQ(state.num_chunks_to_send, 2);
+    std::vector<int64_t> expected_chunks{0};
+    ASSERT_EQ(sent_chunks, expected_chunks);
+
+    // resend chunks when 1 chunk is in flight.
+    ASSERT_EQ(1, state.ResendAllChunks([&](int64_t chunk_id) {
+      sent_chunks.push_back(chunk_id);
+    }));
+    ASSERT_EQ(state.num_chunks, 3);
+    ASSERT_EQ(state.next_chunk_id, 1);
+    ASSERT_EQ(state.num_chunks_inflight, 1);
+    ASSERT_EQ(state.num_chunks_to_send, 3);
+
+    for (auto i = 0; i < 3; i++) {
+      ASSERT_TRUE(state.SendOneChunk());
+      ASSERT_EQ(state.num_chunks, 3);
+      ASSERT_EQ(state.next_chunk_id, (2 + i) % 3);
+      ASSERT_EQ(state.num_chunks_inflight, 2 + i);
+      ASSERT_EQ(state.num_chunks_to_send, 3 - i - 1);
+    }
+    std::vector<int64_t> expected_chunks1{0, 1, 2, 0};
+    ASSERT_EQ(sent_chunks, expected_chunks1);
+
+    ASSERT_FALSE(state.SendOneChunk());
+    ASSERT_FALSE(state.AllChunksComplete());
+    state.OnChunkComplete();
+    state.OnChunkComplete();
+    state.OnChunkComplete();
+    ASSERT_FALSE(state.AllChunksComplete());
+    state.OnChunkComplete();
+    ASSERT_TRUE(state.AllChunksComplete());
+  }
+}
+
+TEST(TestPushManager, TestRetryDuplicates) {
   std::vector<int> results;
   results.resize(10);
   auto node_id = NodeID::FromRandom();
   auto obj_id = ObjectID::FromRandom();
   PushManager pm(5);
 
-  // First send.
+  // First push request.
   pm.StartPush(node_id, obj_id, 10, [&](int64_t chunk_id) { results[chunk_id] = 1; });
-  // Duplicates are all ignored.
-  pm.StartPush(node_id, obj_id, 10, [&](int64_t chunk_id) { results[chunk_id] = 2; });
   ASSERT_EQ(pm.NumChunksInFlight(), 5);
   ASSERT_EQ(pm.NumChunksRemaining(), 10);
   ASSERT_EQ(pm.NumPushesInFlight(), 1);
-  for (int i = 0; i < 10; i++) {
-    pm.StartPush(node_id, obj_id, 10, [&](int64_t chunk_id) { results[chunk_id] = 2; });
+  // Second push request will resent the full chunks.
+  pm.StartPush(node_id, obj_id, 10, [&](int64_t chunk_id) { results[chunk_id] = 2; });
+  ASSERT_EQ(pm.NumChunksInFlight(), 5);
+  ASSERT_EQ(pm.NumChunksRemaining(), 15);
+  ASSERT_EQ(pm.NumPushesInFlight(), 1);
+  // first 5 chunks will be sent by first push request.
+  for (int i = 0; i < 5; i++) {
     pm.OnChunkComplete(node_id, obj_id);
   }
-  ASSERT_EQ(pm.NumChunksInFlight(), 0);
-  ASSERT_EQ(pm.NumChunksRemaining(), 0);
-  ASSERT_EQ(pm.NumPushesInFlight(), 0);
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 5; i++) {
     ASSERT_EQ(results[i], 1);
   }
-
-  // Second allowed send.
-  pm.StartPush(node_id, obj_id, 10, [&](int64_t chunk_id) { results[chunk_id] = 3; });
+  ASSERT_EQ(pm.NumChunksInFlight(), 5);
+  ASSERT_EQ(pm.NumChunksRemaining(), 10);
+  // we will resend all chunks by second push request.
   for (int i = 0; i < 10; i++) {
     pm.OnChunkComplete(node_id, obj_id);
+  }
+  for (int i = 0; i < 10; i++) {
+    ASSERT_EQ(results[i], 2);
   }
   ASSERT_EQ(pm.NumChunksInFlight(), 0);
   ASSERT_EQ(pm.NumChunksRemaining(), 0);
   ASSERT_EQ(pm.NumPushesInFlight(), 0);
-  for (int i = 0; i < 10; i++) {
-    ASSERT_EQ(results[i], 3);
-  }
 }
 
 TEST(TestPushManager, TestMultipleTransfers) {
