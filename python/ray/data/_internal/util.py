4a75d313b9f6cfa5b0848278ffeaea3ea9df57b2
Eric Liang <ekhliang@gmail.com>
2022-07-13 16:15:21 -0700

    update

--- python/ray/data/_internal/util.py
+++ python/ray/data/_internal/util.py
@@ -68,7 +68,8 @@ def _autodetect_parallelism(
 ) -> (int, int):
     """Returns parallelism to use and the min safe parallelism to avoid OOMs.
 
-    This detects parallelism based on three heuristic functions:
+    This detects parallelism based on three heuristic functions (highest to lowest
+    precedence):
 
      1) Available CPUs. We detect how many CPUs are available for use in the cluster,
         and ensure the parallelism is high enough to make use of the CPUs.
@@ -76,9 +77,7 @@ def _autodetect_parallelism(
         to avoid the overhead of tiny blocks.
      3) Max block size. We try to avoid creating blocks larger than this threshold,
         since it can lead to OOM errors during processing.
-
-    These heuristics take precedence over the default parallelism of 200. If none of
-    the heuristics apply, the default value of 200 is used.
+     4) If none of the above apply, the default parallelism of 200 is used.
 
     Args:
         parallelism: The user-requested parallelism, or -1 for auto-detection.
