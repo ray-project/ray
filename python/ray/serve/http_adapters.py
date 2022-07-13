8c1c45639fd90e468221464927010b1deca251ab
simon-mo <simon.mo@hey.com>
2022-07-12 18:25:47 -0700

    [Serve][AIR] Fix pandas_read_json compatibility issue

--- python/ray/serve/http_adapters.py
+++ python/ray/serve/http_adapters.py
@@ -93,4 +93,4 @@ async def pandas_read_json(raw_request: Request):
     import pandas as pd
 
     raw_json = await raw_request.body()
-    return pd.read_json(raw_json, **raw_request.query_params)
+    return pd.read_json(raw_json.decode("utf-8"), **raw_request.query_params)
