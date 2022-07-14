7eb8597a01d9160c43985d2d5803e396be29f9d2
Amog Kamsetty <amogkamsetty@yahoo.com>
2022-07-13 17:40:49 -0700

    update

--- python/ray/serve/tests/test_air_integrations_gpu.py
+++ python/ray/serve/tests/test_air_integrations_gpu.py
@@ -1,8 +1,11 @@
+import pytest
 import requests
 
+import ray
 from ray.air.checkpoint import Checkpoint
 from ray.serve.air_integrations import PredictorDeployment
 from ray.train.predictor import Predictor
+
 
 def test_automatic_enable_gpu(serve_instance):
     """Test automatically set underlying Predictor use_gpu if deployment has GPUs."""
@@ -24,9 +27,15 @@ def test_automatic_enable_gpu(serve_instance):
                 raise ValueError("GPU enabled")
             return data
 
-    deployment = PredictorDeployment.options(name="GPU", ray_actor_options={"num_gpus":
-                                                                       1}).deploy(
-        predictor_cls=DummyGPUPredictor, checkpoint=Checkpoint.from_dict({"x": 1}))
+    _ = PredictorDeployment.options(
+        name="GPU", ray_actor_options={"num_gpus": 1}
+    ).deploy(predictor_cls=DummyGPUPredictor, checkpoint=Checkpoint.from_dict({"x": 1}))
 
     with pytest.raises(ValueError, "GPU enabled"):
         ray.get(send_request.remote(json={"array": [40]}))
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(pytest.main(["-v", "-s", __file__]))
