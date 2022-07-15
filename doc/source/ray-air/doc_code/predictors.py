ee73f0e874e1e8e489ca150514da871cc13dbf7f
Richard Liaw <rliaw@berkeley.edu>
2022-07-15 16:06:23 -0700

    update-get-code-working
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>

--- doc/source/ray-air/doc_code/predictors.py
+++ doc/source/ray-air/doc_code/predictors.py
@@ -11,7 +11,6 @@ from ray.train.tensorflow import (
     TensorflowPredictor,
 )
 
-# This can be a trained model.
 def build_model() -> tf.keras.Model:
     model = tf.keras.Sequential(
         [
@@ -21,14 +20,13 @@ def build_model() -> tf.keras.Model:
     )
     return model
 
-
 model = build_model()
 checkpoint = to_air_checkpoint(model)
 predictor = TensorflowPredictor.from_checkpoint(
     checkpoint, model_definition=build_model
 )
 
-data = np.array([[1, 2], [3, 4]])
+data = np.array([[1], [2], [3], [4]])
 predictions = predictor.predict(data)
 # __use_predictor_end__
 
@@ -68,34 +66,3 @@ print(batch_pred.predict_pipelined(ds, blocks_per_window=1))
 # > DatasetPipeline(num_windows=4, num_stages=3)
 # __pipelined_prediction_end__
 
-
-# __use_pretrained_model_start__
-import ray
-import tensorflow as tf
-from ray.train.batch_predictor import BatchPredictor
-from ray.train.tensorflow import (
-    to_air_checkpoint,
-    TensorflowPredictor,
-)
-
-
-# This can be a trained model
-def build_model() -> tf.keras.Model:
-    model = tf.keras.Sequential(
-        [
-            tf.keras.layers.InputLayer(input_shape=(1,)),
-            tf.keras.layers.Dense(1),
-        ]
-    )
-    return model
-
-
-model = build_model()
-checkpoint = to_air_checkpoint(model)
-batch_predictor = BatchPredictor(
-    checkpoint, TensorflowPredictor, model_definition=build_model
-)
-predict_dataset = ray.data.range(3)
-predictions = batch_predictor.predict(predict_dataset)
-
-# __use_pretrained_model_end__
