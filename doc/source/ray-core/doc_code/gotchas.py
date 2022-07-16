86fd0d4e83218b7b447dca2590660a8fdabc52fe
Richard Liaw <rliaw@berkeley.edu>
2022-07-16 00:11:59 -0700

    gotcha
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>

--- doc/source/ray-core/doc_code/gotchas.py
+++ doc/source/ray-core/doc_code/gotchas.py
@@ -2,13 +2,16 @@
 # __env_var_start__
 import ray
 import os
+
 ray.init()
+
 
 @ray.remote
 def myfunc():
     myenv = os.environ.get("FOO")
     print(f"myenv is {myenv}")
     return 1
+
 
 ray.get(myfunc.remote())
 # this prints: "myenv is None"
@@ -18,13 +21,14 @@ ray.shutdown()
 # __env_var_fix_start__
 ray.init(runtime_env={"env_vars": {"FOO": "bar"}})
 
+
 @ray.remote
 def myfunc():
-    myenv = os.environ.get("FOO");
+    myenv = os.environ.get("FOO")
     print(f"myenv is {myenv}")
     return 1
+
 
 ray.get(myfunc.remote())
 # this prints: "myenv is bar"
 # __env_var_fix_end__
-
