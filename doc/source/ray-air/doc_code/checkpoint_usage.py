b51ca5df5a31ce42db5f347cdd60b3f88fbb4a8f
Richard Liaw <rliaw@berkeley.edu>
2022-07-16 08:40:44 -0700

    [air/docs] update examples to remove pandas again (#26598)
    Signed-off-by: Stefan van der Kleij <s.vanderkleij@viroteq.com>

--- doc/source/ray-air/doc_code/checkpoint_usage.py
+++ doc/source/ray-air/doc_code/checkpoint_usage.py
@@ -23,18 +23,12 @@ checkpoint = to_air_checkpoint(model)
 
 
 # __use_trainer_checkpoint_start__
-import pandas as pd
 import ray
 from ray.air import train_test_split
 from ray.train.xgboost import XGBoostTrainer
 
 
-bc_df = pd.read_csv(
-    "https://air-example-data.s3.us-east-2.amazonaws.com/breast_cancer.csv"
-)
-dataset = ray.data.from_pandas(bc_df)
-# Optionally, read directly from s3
-# dataset = ray.data.read_csv("s3://air-example-data/breast_cancer.csv")
+dataset = ray.data.read_csv("s3://anonymous@air-example-data/breast_cancer.csv")
 
 # Split data into train and validation.
 train_dataset, valid_dataset = train_test_split(dataset, test_size=0.3)
@@ -59,9 +53,7 @@ from ray.train.batch_predictor import BatchPredictor
 from ray.train.xgboost import XGBoostPredictor
 
 # Create a test dataset by dropping the target column.
-test_dataset = valid_dataset.map_batches(
-    lambda df: df.drop("target", axis=1), batch_format="pandas"
-)
+test_dataset = valid_dataset.drop_columns(["target"])
 
 batch_predictor = BatchPredictor.from_checkpoint(checkpoint, XGBoostPredictor)
 
