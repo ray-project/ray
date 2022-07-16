605bc29f11c8aa639e4cf5b0b16148890794861c
Eric Liang <ekhliang@gmail.com>
2022-07-16 10:48:09 -0700

    [air/predictors] Allow creating Predictor directly from a UDF (#26603)

--- python/ray/train/batch_predictor.py
+++ python/ray/train/batch_predictor.py
@@ -1,5 +1,6 @@
 import inspect
-from typing import Any, Dict, Optional, List, Type, Union
+from typing import Any, Dict, Optional, List, Type, Union, Callable
+import pandas as pd
 
 import ray
 from ray.air import Checkpoint
@@ -40,6 +41,30 @@ class BatchPredictor:
         cls, checkpoint: Checkpoint, predictor_cls: Type[Predictor], **kwargs
     ) -> "BatchPredictor":
         return cls(checkpoint=checkpoint, predictor_cls=predictor_cls, **kwargs)
+
+    @classmethod
+    def from_pandas_udf(
+        cls, pandas_udf: Callable[[pd.DataFrame], pd.DataFrame]
+    ) -> "BatchPredictor":
+        """Create a Predictor from a Pandas UDF.
+
+        Args:
+            pandas_udf: A function that takes a pandas.DataFrame and other
+                optional kwargs and returns a pandas.DataFrame.
+        """
+
+        class PandasUDFPredictor(Predictor):
+            @classmethod
+            def from_checkpoint(cls, checkpoint, **kwargs):
+                return PandasUDFPredictor()
+
+            def _predict_pandas(self, df, **kwargs) -> "pd.DataFrame":
+                return pandas_udf(df, **kwargs)
+
+        return cls(
+            checkpoint=Checkpoint.from_dict({"dummy": 1}),
+            predictor_cls=PandasUDFPredictor,
+        )
 
     def predict(
         self,
