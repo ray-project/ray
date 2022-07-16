4cebdf499a01987bf0bfbbbf24465d6f9e6fe98a
Matthew Deng <matt@anyscale.com>
2022-07-15 17:32:39 -0700

    wip benchmark workload setup
    Signed-off-by: Matthew Deng <matt@anyscale.com>

--- release/air_tests/air_benchmarks/workloads/data_benchmark.py
+++ release/air_tests/air_benchmarks/workloads/data_benchmark.py
@@ -1,27 +1,14 @@
-import numpy as np
-import time
 import argparse
+import json
+import os
+import time
 
 import ray
-from ray.data.preprocessors import BatchMapper
-from ray.train.batch_predictor import BatchPredictor
-from ray.train.predictor import Predictor
-from ray.air.util.check_ingest import DummyTrainer
 from ray.air.config import DatasetConfig
-from ray.air.checkpoint import Checkpoint
+from ray.air.util.check_ingest import DummyTrainer
+from ray.data.preprocessors import BatchMapper
 
 GiB = 1024 * 1024 * 1024
-
-
-class DummyPredictor(Predictor):
-    @classmethod
-    def from_checkpoint(cls, checkpoint: Checkpoint, **kwargs) -> "Predictor":
-        return DummyPredictor()
-
-    def predict(self, data, **kwargs):
-        # For 20k records (200GiB), this amounts to 2000 seconds of work.
-        time.sleep(len(data) * 0.0001)
-        return np.array([42] * len(data))
 
 
 def make_ds(size_gb: int):
@@ -35,12 +22,15 @@ def make_ds(size_gb: int):
     return dataset
 
 
-# TODO: Verify this runs at scale.
-def run_ingest_bulk(dataset, num_workers):
+def run_ingest_bulk(dataset, num_workers, num_cpus_per_worker, placement_strategy):
     dummy_prep = BatchMapper(lambda df: df * 2)
     trainer = DummyTrainer(
-        scaling_config={"num_workers": num_workers, "trainer_resources": {"CPU": 0},
-                        "resources_per_worker": {"CPU": 3}},
+        scaling_config={
+            "num_workers": num_workers,
+            "trainer_resources": {"CPU": 0},
+            "resources_per_worker": {"CPU": num_cpus_per_worker},
+            "placement_strategy": placement_strategy,
+        },
         datasets={"train": dataset},
         preprocessor=dummy_prep,
         num_epochs=1,
@@ -49,69 +39,38 @@ def run_ingest_bulk(dataset, num_workers):
     )
     trainer.fit()
 
-
-# TODO: Verify this runs at scale.
-def run_ingest_streaming(dataset, num_workers):
-    dummy_prep = BatchMapper(lambda df: df * 2)
-    trainer = DummyTrainer(
-        scaling_config={"num_workers": num_workers, "trainer_resources": {"CPU": 0},
-                        "resources_per_worker": {"CPU": 3}},
-        datasets={"train": dataset},
-        preprocessor=dummy_prep,
-        num_epochs=1,
-        prefetch_blocks=1,
-        dataset_config={"train": DatasetConfig(split=True, use_stream_api=True, stream_window_size=num_workers * GiB)},
-    )
-    trainer.fit()
-
-
-# TODO: Verify this runs at scale.
-def run_infer_bulk(dataset, num_workers, post=None):
-    start = time.time()
-    checkpoint = Checkpoint.from_dict({"dummy": 1})
-    # TODO: add preprocessor here
-    predictor = BatchPredictor(checkpoint, DummyPredictor)
-    result = predictor.predict(
-        dataset,
-        batch_size=1024 // 8,
-        min_scoring_workers=num_workers,
-        max_scoring_workers=num_workers,
-        num_cpus_per_worker=1,
-    )
-    if post:
-        post(result)
-    print(result.stats())
-    print("Total runtime", time.time() - start)
-
-
-# TODO: Verify this runs at scale.
-def run_infer_streaming(dataset, num_workers):
-    dataset = dataset.window(bytes_per_window=num_workers * GiB // 4)
-
-    def post(result):
-        for b in result.iter_batches():
-            pass
-
-    return run_infer_bulk(dataset, num_workers, post)
+    return {}
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--benchmark", type=str, default="ingest", help="ingest or infer")
     parser.add_argument("--num-workers", type=int, default=4)
+    parser.add_argument(
+        "--num-cpus-per-worker",
+        type=int,
+        default=1,
+        help="Number of CPUs for each training worker.",
+    )
+    parser.add_argument(
+        "--placement-strategy",
+        type=str,
+        default="PACK",
+        help="Worker placement strategy.",
+    )
     parser.add_argument("--dataset-size-gb", type=int, default=200)
-    parser.add_argument("--streaming", action="store_true", default=False)
     args = parser.parse_args()
     ds = make_ds(args.dataset_size_gb)
-    if args.benchmark == "ingest":
-        if args.streaming:
-            run_ingest_streaming(ds, args.num_workers)
-        else:
-            run_ingest_bulk(ds, args.num_workers)
-    elif args.benchmark == "infer":
-        if args.streaming:
-            run_infer_streaming(ds, args.num_workers)
-        else:
-            run_infer_bulk(ds, args.num_workers)
-    else:
-        assert False
+
+    start = time.time()
+    run_ingest_bulk(
+        ds, args.num_workers, args.num_cpus_per_worker, args.placement_strategy
+    )
+    end = time.time()
+    time_taken = end - start
+
+    result = {"time_taken_s": time_taken}
+
+    print("Results:", result)
+    test_output_json = os.environ.get("TEST_OUTPUT_JSON", "/tmp/result.json")
+    with open(test_output_json, "wt") as f:
+        json.dump(result, f)
