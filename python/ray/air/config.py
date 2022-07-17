82cecacd1ce7c091462d2b4d6a997aec2e7ce016
Eric Liang <ekhliang@gmail.com>
2022-07-16 21:55:51 -0700

    [air] Add _max_cpu_fraction_per_node to ScalingConfig and documentation (#26634)
    Signed-off-by: Stefan van der Kleij <s.vanderkleij@viroteq.com>

--- python/ray/air/config.py
+++ python/ray/air/config.py
@@ -43,6 +43,12 @@ class ScalingConfigDataClass:
     placement_strategy: The placement strategy to use for the
         placement group of the Ray actors. See :ref:`Placement Group
         Strategies <pgroup-strategy>` for the possible options.
+    _max_cpu_fraction_per_node: (Experimental) The max fraction of CPUs per node that
+        Train will use for scheduling training actors. The remaining CPUs can be used
+        for dataset tasks. It is highly recommended that you set this to less than
+        1.0 (e.g., 0.8) when passing datasets to trainers, to avoid hangs / CPU
+        starvation of dataset tasks. Warning: this feature is experimental and is not
+        recommended for use with autoscaling (scale-up will not trigger properly).
     """
 
     trainer_resources: Optional[Dict] = None
@@ -50,6 +56,7 @@ class ScalingConfigDataClass:
     use_gpu: bool = False
     resources_per_worker: Optional[Dict] = None
     placement_strategy: str = "SPREAD"
+    _max_cpu_fraction_per_node: Optional[float] = None
 
     def __post_init__(self):
         if self.resources_per_worker:
@@ -126,7 +133,15 @@ class ScalingConfigDataClass:
             for _ in range(self.num_workers if self.num_workers else 0)
         ]
         bundles = trainer_bundle + worker_bundles
-        return PlacementGroupFactory(bundles, strategy=self.placement_strategy)
+        if self._max_cpu_fraction_per_node is not None:
+            kwargs = {
+                "_max_cpu_fraction_per_node": self._max_cpu_fraction_per_node,
+            }
+        else:
+            kwargs = {}
+        return PlacementGroupFactory(
+            bundles, strategy=self.placement_strategy, **kwargs
+        )
 
     @classmethod
     def from_placement_group_factory(
