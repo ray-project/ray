29ff1652ee9ef19f90aa0e27061b6329c0c589fe
Kai Fricke <kai@anyscale.com>
2022-07-18 11:28:44 +0100

    Add attributes
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- python/ray/util/ml_utils/checkpoint_manager.py
+++ python/ray/util/ml_utils/checkpoint_manager.py
@@ -222,11 +222,20 @@ class CheckpointConfig:
             ``checkpoint_score_attribute`` will be kept.
             If "min", then checkpoints with lowest values of
             ``checkpoint_score_attribute`` will be kept.
+        checkpoint_frequency: Number of iterations between checkpoints. If 0
+            this will disable checkpointing. This attribute is only supported
+            by trainers that don't take in custom training loops.
+        checkpoint_at_end: If True, will save a checkpoint at the end of training.
+            This attribute is only supported by trainers that don't take in
+            custom training loops.
     """
 
     num_to_keep: Optional[int] = None
     checkpoint_score_attribute: Optional[str] = None
     checkpoint_score_order: str = MAX
+
+    checkpoint_frequency: int = 0
+    checkpoint_at_end: bool = True
 
     def __post_init__(self):
         if self.num_to_keep is not None and self.num_to_keep < 0:
