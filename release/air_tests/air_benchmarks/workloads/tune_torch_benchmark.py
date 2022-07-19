f72f9477b126ed5ec5c947a8a30e0c5cc7b0ef40
Kai Fricke <kai@anyscale.com>
2022-07-19 14:49:03 +0100

    Multiple runs
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- release/air_tests/air_benchmarks/workloads/tune_torch_benchmark.py
+++ release/air_tests/air_benchmarks/workloads/tune_torch_benchmark.py
@@ -1,7 +1,9 @@
 import json
 import os
+import time
 import timeit
 import click
+import numpy as np
 
 import ray
 from ray.air import ScalingConfig
@@ -42,6 +44,7 @@ def get_trainer(num_workers: int = 4, use_gpu: bool = False):
             resources_per_worker={"CPU": 2},
             trainer_resources={"CPU": 0},
             use_gpu=use_gpu,
+            placement_strategy="STRICT_PACK",
         ),
     )
     return trainer
@@ -79,10 +82,13 @@ def tune_torch(num_workers: int = 4, num_trials: int = 8, use_gpu: bool = False)
 
 
 @click.command(help="Run Benchmark comparing Train to Tune.")
+@click.option("--num-runs", type=int, default=1)
 @click.option("--num-trials", type=int, default=8)
 @click.option("--num-workers", type=int, default=4)
 @click.option("--use-gpu", is_flag=True)
-def main(num_trials, num_workers, use_gpu):
+def main(
+    num_runs: int = 1, num_trials: int = 8, num_workers: int = 4, use_gpu: bool = False
+):
     ray.init(
         runtime_env={
             "working_dir": os.path.dirname(__file__),
@@ -90,23 +96,57 @@ def main(num_trials, num_workers, use_gpu):
         }
     )
     prepare_mnist()
-    train_time = timeit.timeit(
-        lambda: train_torch(num_workers=num_workers, use_gpu=use_gpu), number=1
-    )
-    tune_time = timeit.timeit(
-        lambda: tune_torch(
-            num_workers=num_workers, num_trials=num_trials, use_gpu=use_gpu
-        ),
-        number=1,
-    )
-    result = {"train_time": train_time, "tune_time": tune_time}
-    print("Results:", result)
 
-    assert tune_time < 1.2 * train_time, f"{tune_time} > 1.2 * {train_time}"
+    train_times = []
+    tune_times = []
+
+    for i in range(num_runs):
+        print(f"Run {i+1} / {num_runs}")
+
+        time.sleep(2)
+
+        train_time = timeit.timeit(
+            lambda: train_torch(num_workers=num_workers, use_gpu=use_gpu), number=1
+        )
+        train_times.append(train_time)
+
+        time.sleep(2)
+
+        tune_time = timeit.timeit(
+            lambda: tune_torch(
+                num_workers=num_workers, num_trials=num_trials, use_gpu=use_gpu
+            ),
+            number=1,
+        )
+        tune_times.append(tune_time)
+
+        result = {"train_time": train_time, "tune_time": tune_time}
+        print(f"Results run {i+1}: {result}")
+
+    mean_train_time = np.mean(train_times)
+    mean_tune_time = np.mean(tune_times)
+
+    full_results = {
+        "train_times": train_times,
+        "train_mean": mean_train_time,
+        "train_sd": np.std(train_times),
+        "tune_times": tune_times,
+        "tune_mean": mean_tune_time,
+        "tune_sd": np.std(tune_times),
+    }
+
+    print("Full results:", full_results)
+
+    factor = 1.2
+    threshold = mean_train_time * factor
+
+    assert (
+        mean_tune_time <= threshold
+    ), f"{mean_tune_time:.2f} > {threshold:.2f} = {factor:.1f} * {mean_train_time:.2f}"
 
     test_output_json = os.environ.get("TEST_OUTPUT_JSON", "/tmp/result.json")
     with open(test_output_json, "wt") as f:
-        json.dump(result, f)
+        json.dump(full_results, f)
 
 
 if __name__ == "__main__":
