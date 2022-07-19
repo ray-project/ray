450939d5917baa1fc567295e26f9e21f94b99ea4
Balaji Veeramani <bveeramani@berkeley.edu>
2022-07-18 23:34:10 -0700

    Add implementation

--- python/ray/train/sklearn/sklearn_checkpoint.py
+++ python/ray/train/sklearn/sklearn_checkpoint.py
@@ -3,6 +3,8 @@ from typing import TYPE_CHECKING, Optional
 
 from sklearn.base import BaseEstimator
 from ray.air.checkpoint import Checkpoint
+from ray.air.constants import MODEL_KEY, PREPROCESSOR_KEY
+from ray.train.data_parallel_trainer import _load_checkpoint
 from ray.util.annotations import PublicAPI
 
 if TYPE_CHECKING:
@@ -18,7 +20,6 @@ class SklearnCheckpoint(Checkpoint):
         cls,
         estimator: BaseEstimator,
         *,
-        path: os.PathLike,
         preprocessor: Optional["Preprocessor"] = None,
         **kwargs,
     ) -> "SklearnCheckpoint":
@@ -26,7 +27,6 @@ class SklearnCheckpoint(Checkpoint):
 
         Args:
             estimator: The ``Estimator`` to store in the checkpoint.
-            path: The directory where the checkpoint will be stored.
             preprocessor: A fitted preprocessor to be applied before inference.
             **kwargs: Arbitrary data to store in the checkpoint.
 
@@ -38,7 +38,7 @@ class SklearnCheckpoint(Checkpoint):
             >>> from sklearn.ensemble import RandomForestClassifier
             >>>
             >>> estimator = RandomForestClassifier()
-            >>> checkpoint = SklearnCheckpoint.from_estimator(estimator, path=".")
+            >>> checkpoint = SklearnCheckpoint.from_estimator(estimator)
 
             You can use a :py:class:`SklearnCheckpoint` to create an
             :py:class:`~ray.train.sklearn.SklearnPredictor` and preform inference.
@@ -47,8 +47,12 @@ class SklearnCheckpoint(Checkpoint):
             >>>
             >>> predictor = SklearnPredictor.from_checkpoint(checkpoint)
         """
-        raise NotImplementedError
+        checkpoint = SklearnCheckpoint.from_dict(
+            {PREPROCESSOR_KEY: preprocessor, MODEL_KEY: estimator, **kwargs}
+        )
+        return checkpoint
 
     def get_model(self) -> BaseEstimator:
         """Retrieve the ``Estimator`` stored in this checkpoint."""
-        raise NotImplementedError
+        model, _ = _load_checkpoint(self, "SklearnTrainer")
+        return model
