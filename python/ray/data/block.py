6c71acc683a4cd35967b689c8aea00a30221ebeb
Sebastián Ramírez <tiangolo@gmail.com>
2022-07-20 21:16:24 +0200

    ✨ Add extra types for datasets (#26648)
    ✨ Add extra types for datasets
    
    Why are these changes needed?
    By using Protocols for the definition of callable classes, the type parameter in datasets can be preserved for the methods that work on rows.
    
    This allows getting typing information, editor support, in transformed datasets. It also enables editor support, including autocompletion, inside of the parameters inside of lambdas passed to things like ds.map() and ds.filter().
    
    For example, after several transformations, ds.filter() still gets autocompletion for the x parameter in the lambda, the editor knows it's an int.
    
    https://user-images.githubusercontent.com/1326112/179423609-6d77da23-5f5e-47ce-a17f-6eb0d06d82d0.png
    
    I see there was a TODO comment to do this trick with the protocol, so Clark already had this idea. 💡 The good news is that it's not necessary to wait until only Python 3.8 is supported by using typing_extensions.
    
    Signed-off-by: Stefan van der Kleij <s.vanderkleij@viroteq.com>

--- python/ray/data/block.py
+++ python/ray/data/block.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 from dataclasses import dataclass
 from typing import (
@@ -29,6 +30,10 @@ try:
 except ImportError:
     resource = None
 
+if sys.version_info >= (3, 8):
+    from typing import Protocol
+else:
+    from typing_extensions import Protocol
 
 if TYPE_CHECKING:
     import pandas
@@ -39,8 +44,8 @@ if TYPE_CHECKING:
     from ray.data.aggregate import AggregateFn
 
 
-T = TypeVar("T")
-U = TypeVar("U")
+T = TypeVar("T", contravariant=True)
+U = TypeVar("U", covariant=True)
 KeyType = TypeVar("KeyType")
 AggType = TypeVar("AggType")
 
@@ -101,14 +106,19 @@ DataBatch = Union[Block, np.ndarray, Dict[str, np.ndarray]]
 # A class type that implements __call__.
 CallableClass = type
 
+
+class _CallableClassProtocol(Protocol[T, U]):
+    def __call__(self, __arg: T) -> U:
+        ...
+
+
 # A UDF on data batches.
 BatchUDF = Union[
     # TODO(Clark): Once Ray only supports Python 3.8+, use protocol to constraint batch
     # UDF type.
     # Callable[[DataBatch, ...], DataBatch]
     Callable[[DataBatch], DataBatch],
-    Callable[..., DataBatch],
-    CallableClass,
+    _CallableClassProtocol,
 ]
 
 # A UDF on data rows.
@@ -117,8 +127,7 @@ RowUDF = Union[
     # UDF type.
     # Callable[[T, ...], U]
     Callable[[T], U],
-    Callable[..., U],
-    CallableClass,
+    _CallableClassProtocol[T, U],
 ]
 
 # A list of block references pending computation by a single task. For example,
