ebd53fc7d5e670a5430972d4ed59e0a97b15acdb
Kai Fricke <kai@anyscale.com>
2022-07-20 18:58:21 +0100

    Using two dots instead
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- ci/pipeline/determine_tests_to_run.py
+++ ci/pipeline/determine_tests_to_run.py
@@ -24,12 +24,8 @@ def list_changed_files(commit_range):
     Returns:
         list: List of changed files within the commit range
     """
-    base, target = commit_range.split("...", maxsplit=1)
 
-    merge_base = subprocess.check_output(["git", "merge-base", base, target])
-
-    new_commit_range = f"{merge_base}...{target}"
-    command = ["git", "diff", "--name-only", new_commit_range, "--"]
+    command = ["git", "diff", "--name-only", commit_range, "--"]
     out = subprocess.check_output(command)
     return [s.strip() for s in out.decode().splitlines() if s is not None]
 
@@ -58,9 +54,9 @@ def get_commit_range():
         with open(os.environ["GITHUB_EVENT_PATH"], "rb") as f:
             event = json.loads(f.read())
         base = event["pull_request"]["base"]["sha"]
-        commit_range = "{}...{}".format(base, event.get("after", ""))
+        commit_range = "{}..{}".format(base, event.get("after", ""))
     elif os.environ.get("BUILDKITE"):
-        commit_range = "origin/{}...{}".format(
+        commit_range = "origin/{}..{}".format(
             os.environ["BUILDKITE_PULL_REQUEST_BASE_BRANCH"],
             os.environ["BUILDKITE_COMMIT"],
         )
