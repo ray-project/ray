2643e2cabe248ca8e85769e427383e0c19c20b2f
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2022-07-19 21:29:55 -0700

    comment grammar
    Signed-off-by: Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>

--- python/ray/autoscaler/_private/autoscaler.py
+++ python/ray/autoscaler/_private/autoscaler.py
@@ -271,7 +271,7 @@ class StandardAutoscaler:
 
         # By default, the autoscaler kills and/or tries to recover
         # a worker node if it hasn't produced a resource heartbeat in the last 30
-        # seconds. The worker_liveness_check allows us to disable this behavior in
+        # seconds. The worker_liveness_check falg allows us to disable this behavior in
         # settings where another component, such as a Kubernetes operator, is
         # responsible for healthchecks.
         self.worker_liveness_check = self.config["provider"].get(
