05d1a26a1aa59b52c44ae2f0b0d5cc25b758cd6b
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-07-22 21:27:46 +0000

    adding bundles

--- python/ray/train/alpa/utils.py
+++ python/ray/train/alpa/utils.py
@@ -64,7 +64,11 @@ class ScalingConfigWithIPs(ScalingConfig):
             {**worker_resources, **worker_resources_extra, **{f"node:{self.ips[_]}": 1e-3}}
             for _ in range(self.num_workers if self.num_workers else 0)
         ]
-        bundles = trainer_bundle + worker_bundles
+        daemon_worker_bundles = [
+            {**{f"node:{self.ips[_]}": 1e-3}}
+            for _ in range(self.num_workers if self.num_workers else 0)
+        ]
+        bundles = trainer_bundle + worker_bundles + daemon_worker_bundles
         return PlacementGroupFactory(bundles, strategy=self.placement_strategy)
     
     
