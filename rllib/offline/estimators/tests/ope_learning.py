8f3f441db07019d5008a3aae291249bb136ffa7b
Rohan138 <rapotdar@purdue.edu>
2022-07-22 16:30:58 -0700

    wip
    Signed-off-by: Rohan138 <rapotdar@purdue.edu>

--- rllib/offline/estimators/tests/ope_learning.py
+++ rllib/offline/estimators/tests/ope_learning.py
@@ -10,32 +10,7 @@ import tempfile
 import unittest
 
 
-def generate_data(checkpoint_dir: str, mean_reward: float, num_episodes: int):
-    config = (
-        DQNConfig()
-        .environment(env="CartPole-v0")
-        .framework("torch")
-        .rollouts(num_rollout_workers=4, batch_mode="complete_episodes")
-    )
-    algo = config.build()
-    results = algo.train()
-    while results["episode_reward_mean"] < mean_reward:
-        results = algo.train()
-    checkpoint = algo.save_checkpoint(checkpoint_dir)
-
-    batches = []
-    eps = 0
-    while eps < num_episodes:
-        batch = synchronous_parallel_sample(worker_set=algo.workers)
-        batches.append(batch)
-        eps += len(set(batch[SampleBatch.EPS_ID]))
-    batch = concat_samples(batches)
-    algo.stop()
-
-    return checkpoint, batch
-
-
-def test_fqe(checkpoint: str, batch: SampleBatch, mean_reward: float):
+def test_fqe(checkpoint: str, batch: SampleBatch, mean_ret: float):
     config = (
         DQNConfig()
         .environment(env="CartPole-v0")
@@ -54,43 +29,58 @@ def test_fqe(checkpoint: str, batch: SampleBatch, mean_reward: float):
     )
     losses = fqe.train(batch)
     estimates = fqe.estimate_v(batch)
-    algo.stop()
     estimates = convert_to_numpy(estimates)
-    print(np.mean(estimates), mean_reward, np.mean(losses))
+    print(np.mean(estimates), mean_ret, np.mean(losses))
+    algo.stop()
+
+
+def collect_episodes(algo, num_episodes):
+    batches = []
+    mean_ret = []
+    gamma = algo.config["gamma"]
+
+    for _ in range(num_episodes, algo.config["num_workers"]):
+        episodes = synchronous_parallel_sample(worker_set=algo.workers, concat=False)
+        batches.extend(episodes)
+        for episode in episodes:
+            ep_ret = 0
+            for r in episode[SampleBatch.REWARDS][::-1]:
+                ep_ret = r + gamma * ep_ret
+            mean_ret.append(ep_ret)
+
+    batch = concat_samples(batches)
+    mean_ret = np.mean(mean_ret)
+    return batch, mean_ret
 
 
 class TestOPELearning(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         ray.init()
-        cls.checkpoint_dir = tempfile.mkdtemp(prefix="fqe/")
-
-        cls.random_reward = 10
-        cls.mixed_reward = 120
-        cls.optimal_reward = 200
+        cls.checkpoint_dir = tempfile.mkdtemp()
         cls.num_episodes = 100
 
-        cls.random_path, cls.random_data = generate_data(
-            cls.checkpoint_dir,
-            cls.random_reward,
-            cls.num_episodes,
+        config = (
+            DQNConfig()
+            .environment(env="CartPole-v0")
+            .framework("torch")
+            .rollouts(num_rollout_workers=8, batch_mode="complete_episodes")
         )
-        print("Generated random batch")
+        algo = config.build()
+        cls.random_path = algo.save_checkpoint(cls.checkpoint_dir)
+        cls.random_batch, cls.random_reward = collect_episodes(algo, cls.num_episodes)
 
-        # cls.mixed_path, cls.mixed_data = generate_data(
-        #     cls.checkpoint_dir,
-        #     cls.mixed_reward,
-        #     cls.num_episodes,
-        # )
-        # print("Generated mixed batch")
+        results = algo.train()
+        while results["episode_reward_mean"] < 120:
+            results = algo.train()
+        cls.mixed_path = algo.save_checkpoint(cls.checkpoint_dir)
+        cls.mixed_batch, cls.mixed_reward = collect_episodes(algo, cls.num_episodes)
 
-        # cls.optimal_path, cls.optimal_data = generate_data(
-        #     cls.checkpoint_dir,
-        #     cls.checkpoint_dir + "optimal",
-        #     cls.optimal_reward,
-        #     cls.num_episodes,
-        # )
-        # print("Generated optimal batch")
+        while results["episode_reward_mean"] < 200:
+            results = algo.train()
+        cls.optimal_path = algo.save_checkpoint(cls.checkpoint_dir)
+        cls.optimal_batch, cls.optimal_reward = collect_episodes(algo, cls.num_episodes)
+        algo.stop()
 
     @classmethod
     def tearDownClass(cls):
@@ -98,13 +88,13 @@ class TestOPELearning(unittest.TestCase):
         ray.shutdown()
 
     def test_random(self):
-        test_fqe(self.random_path, self.random_data, self.random_reward)
+        test_fqe(self.random_path, self.random_batch, self.random_reward)
 
-    # def test_mixed(self):
-    #     test_fqe(self.mixed_path, self.mixed_data, self.mixed_reward)
+    def test_mixed(self):
+        test_fqe(self.mixed_path, self.mixed_batch, self.mixed_reward)
 
-    # def test_optimal(self):
-    #     test_fqe(self.optimal_path, self.optimal_data, self.optimal_reward)
+    def test_optimal(self):
+        test_fqe(self.optimal_path, self.optimal_batch, self.optimal_reward)
 
 
 if __name__ == "__main__":
