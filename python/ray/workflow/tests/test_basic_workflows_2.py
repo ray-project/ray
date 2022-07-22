f5f25d71dd9824969eba3407233b81cccc886241
Siyuan Zhuang <suquark@gmail.com>
2022-07-21 21:26:33 -0700

    fix
    Signed-off-by: Siyuan Zhuang <suquark@gmail.com>

--- python/ray/workflow/tests/test_basic_workflows_2.py
+++ python/ray/workflow/tests/test_basic_workflows_2.py
@@ -301,10 +301,15 @@ def test_get_named_step_default(workflow_start_regular, tmp_path):
 
     assert math.factorial(5) == workflow.run(factorial.bind(5), workflow_id="factorial")
     for i in range(5):
-        step_name = (
-            "python.ray.workflow.tests.test_basic_workflows_2."
-            "test_get_named_step_default.locals.factorial"
-        )
+        if not client_mode_should_convert(auto_init=False):
+            step_name = (
+                "python.ray.workflow.tests.test_basic_workflows_2."
+                "test_get_named_step_default.locals.factorial"
+            )
+        else:
+            step_name = (
+                "test_basic_workflows_2.test_get_named_step_default.locals.factorial"
+            )
         if i != 0:
             step_name += "_" + str(i)
         # All outputs will be 120
