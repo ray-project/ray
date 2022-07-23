1314ece1e2b045a0109c3ce9c956c9d34b8ac9cb
clarng <clarence.wyng@gmail.com>
2022-07-22 22:12:23 -0700

    Mark local mode as deprecated with warning message about possible memory leak (#26855)
    Mark local mode as deprecated with a warning message.
    
    Related issue number
    #24216
    #26095
    
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- python/ray/_private/worker.py
+++ python/ray/_private/worker.py
@@ -1303,6 +1303,13 @@ def init(
 
     if local_mode:
         driver_mode = LOCAL_MODE
+        warnings.warn(
+            "DeprecationWarning: local mode is an experimental feature that is no "
+            "longer maintained and will be removed in the future."
+            "For debugging consider using Ray debugger. ",
+            DeprecationWarning,
+            stacklevel=2,
+        )
     else:
         driver_mode = SCRIPT_MODE
 
