98e0de72d53241dc575ca2ee975480681fcef8f1
Alan Guo <aguo@anyscale.com>
2022-07-25 10:54:22 -0700

    [dashboard] Update cluster_activities endpoint to use pydantic. (#26609)
    Update cluster_activities endpoint to use pydantic so we have better data validation.
    
    Make timestamp a required field.
    Add pydantic to ray[default] requirements
    
    Signed-off-by: Rohan138 <rapotdar@purdue.edu>

--- dashboard/modules/snapshot/tests/test_snapshot.py
+++ dashboard/modules/snapshot/tests/test_snapshot.py
@@ -51,13 +51,14 @@ def set_ray_cluster_activity_hook(request):
         "ray._private.test_utils.external_ray_cluster_activity_hook2",
         "ray._private.test_utils.external_ray_cluster_activity_hook3",
         "ray._private.test_utils.external_ray_cluster_activity_hook4",
+        "ray._private.test_utils.external_ray_cluster_activity_hook5",
     ],
     indirect=True,
 )
-async def test_component_activities_hook(set_ray_cluster_activity_hook, call_ray_start):
+def test_component_activities_hook(set_ray_cluster_activity_hook, call_ray_start):
     """
     Tests /api/component_activities returns correctly for various
-    responses of RAY_CLUSTER_ACTIVITY_HOOK defined in ray._private.test_utils.
+    responses of RAY_CLUSTER_ACTIVITY_HOOK.
 
     Verify no active drivers are correctly reflected in response.
     """
@@ -82,12 +83,16 @@ async def test_component_activities_hook(set_ray_cluster_activity_hook, call_ray
     assert driver_ray_activity_response.reason is None
 
     # Validate external component response can be cast to RayActivityResponse object
-    if external_hook[-1] == "4":
+    if external_hook[-1] == "5":
+        external_activity_response = RayActivityResponse(**data["test_component5"])
+        assert external_activity_response.is_active == "ACTIVE"
+        assert external_activity_response.reason == "Counter: 1"
+    elif external_hook[-1] == "4":
         external_activity_response = RayActivityResponse(**data["external_component"])
         assert external_activity_response.is_active == "ERROR"
         assert (
-            external_activity_response.reason
-            == "Exception('Error in external cluster activity hook')"
+            "'Error in external cluster activity hook'"
+            in external_activity_response.reason
         )
     elif external_hook[-1] == "3":
         external_activity_response = RayActivityResponse(**data["external_component"])
@@ -112,7 +117,7 @@ async def test_component_activities_hook(set_ray_cluster_activity_hook, call_ray
 
 
 def test_active_component_activities(ray_start_with_dashboard):
-    # Verify drivers which don't have namespace starting with _ray_internal_job_info_
+    # Verify drivers which don't have namespace starting with _ray_internal_
     # are considered active.
 
     driver_template = """
@@ -125,9 +130,13 @@ ray.init(address="auto", namespace="{namespace}")
     run_string_as_driver_nonblocking(
         driver_template.format(namespace="_ray_internal_job_info_id1")
     )
+    # Simulate the default driver that gets created by dashboard
+    run_string_as_driver_nonblocking(
+        driver_template.format(namespace="_ray_internal_dashboard")
+    )
 
     # Wait 1 sec for drivers to start
-    time.sleep(1)
+    time.sleep(1.5)
 
     # Verify drivers are considered active after running script
     webui_url = ray_start_with_dashboard["webui_url"]
@@ -148,7 +157,7 @@ ray.init(address="auto", namespace="{namespace}")
     driver_ray_activity_response = RayActivityResponse(**data["driver"])
 
     assert driver_ray_activity_response.is_active == "ACTIVE"
-    # Drivers with namespace starting with "_ray_internal_job_info_" are not
+    # Drivers with namespace starting with "_ray_internal" are not
     # considered active drivers. Three active drivers are the two
     # run with namespace "my_namespace" and the one started
     # from ray_start_with_dashboard
