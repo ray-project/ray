91b7b4d61ec8491d90c931b30a4687b6165be364
Balaji Veeramani <bveeramani@berkeley.edu>
2022-07-26 10:10:57 -0700

    [AIR] Remove `CustomStatefulPreprocessor` (#26981)
    Signed-off-by: Rohan138 <rapotdar@purdue.edu>

--- python/ray/data/tests/test_preprocessors.py
+++ python/ray/data/tests/test_preprocessors.py
@@ -1,22 +1,17 @@
 import warnings
 from collections import Counter
-from typing import Dict
 from unittest.mock import patch
 
 import numpy as np
 import pandas as pd
 import pyarrow
 import pytest
-from pandas import DataFrame
 
 import ray
-from ray.data import Dataset
-from ray.data.aggregate import Max
 from ray.data.preprocessor import Preprocessor, PreprocessorNotFittedException
 from ray.data.preprocessors import (
     BatchMapper,
     Chain,
-    CustomStatefulPreprocessor,
     LabelEncoder,
     MinMaxScaler,
     OneHotEncoder,
@@ -1128,62 +1123,6 @@ def test_batch_mapper():
     )
 
     assert out_df.equals(expected_df)
-
-
-def test_custom_stateful_preprocessor():
-    """Tests basic CustomStatefulPreprocessor functionality."""
-
-    items = [
-        {"A": 1, "B": 10, "C": 1},
-        {"A": 2, "B": 20, "C": 2},
-        {"A": 3, "B": 30, "C": 3},
-    ]
-
-    ds = ray.data.from_items(items)
-
-    def get_max_a(ds: Dataset):
-        # Calculate max value for column A.
-        max_a = ds.aggregate(Max("A"))
-        return max_a
-
-    def subtract_max_a_from_a_and_add_max_a_to_b(df: DataFrame, stats: Dict):
-        # Subtract max A value from column A and subtract it from B.
-        max_a = stats["max(A)"]
-        df["A"] = df["A"] - max_a
-        df["B"] = df["B"] + max_a
-        return df
-
-    preprocessor = CustomStatefulPreprocessor(
-        get_max_a, subtract_max_a_from_a_and_add_max_a_to_b
-    )
-    preprocessor.fit(ds)
-    transformed_ds = preprocessor.transform(ds)
-
-    expected_items = [
-        {"A": -2, "B": 13, "C": 1},
-        {"A": -1, "B": 23, "C": 2},
-        {"A": 0, "B": 33, "C": 3},
-    ]
-    expected_ds = ray.data.from_items(expected_items)
-
-    assert transformed_ds.take(3) == expected_ds.take(3)
-
-    batch = pd.DataFrame(
-        {
-            "A": [5, 6],
-            "B": [10, 10],
-            "C": [5, 10],
-        }
-    )
-    transformed_batch = preprocessor.transform_batch(batch)
-    expected_batch = pd.DataFrame(
-        {
-            "A": [2, 3],
-            "B": [13, 13],
-            "C": [5, 10],
-        }
-    )
-    assert transformed_batch.equals(expected_batch)
 
 
 def test_power_transformer():
