46ef7e18eb9a324e506940b6c3e4bb10eb483ddb
Kai Fricke <kai@anyscale.com>
2022-07-26 20:08:13 +0100

    [wip] [air] Test docstrings
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- python/ray/train/xgboost/xgboost_trainer.py
+++ python/ray/train/xgboost/xgboost_trainer.py
@@ -21,22 +21,22 @@ class XGBoostTrainer(GBDTTrainer):
     using multiple Ray Actors.
 
     Example:
-        .. code-block:: python
 
-            import ray
-
-            from ray.train.xgboost import XGBoostTrainer
-            from ray.air.config import ScalingConfig
-
-            train_dataset = ray.data.from_items(
-                [{"x": x, "y": x + 1} for x in range(32)])
-            trainer = XGBoostTrainer(
-                label_column="y",
-                params={"objective": "reg:squarederror"},
-                scaling_config=ScalingConfig(num_workers=3),
-                datasets={"train": train_dataset}
-            )
-            result = trainer.fit()
+    >>> import ray
+    >>>
+    >>> from ray.train.xgboost import XGBoostTrainer
+    >>> from ray.air.config import ScalingConfig
+    >>>
+    >>> train_dataset = ray.data.from_items(
+    ...     [{"x": x, "y": x + 1} for x in range(32)])
+    >>> trainer = XGBoostTrainer(
+    ...     label_column="y",
+    ...     params={"objective": "reg:squarederror"},
+    ...     scaling_config=ScalingConfig(num_workers=3),
+    ...     datasets={"train": train_dataset}
+    ... )
+    >>> result = trainer.fit() # doctest: +ELLIPSIS
+    == Status ...
 
     Args:
         datasets: Ray Datasets to use for training and validation. Must include a
