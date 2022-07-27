402b1cdeedd191d7f08bec05a0a0c0384aef6160
Kai Fricke <krfricke@users.noreply.github.com>
2022-07-27 17:31:55 +0100

    [hotfix/xgboost/lightgbm] Fix checkpoint creation in test
    Signed-off-by: Kai Fricke <kai@anyscale.com>
    Signed-off-by: Stefan van der Kleij <s.vanderkleij@viroteq.com>

--- python/ray/train/tests/test_lightgbm_predictor.py
+++ python/ray/train/tests/test_lightgbm_predictor.py
@@ -1,5 +1,3 @@
-import tempfile
-
 import lightgbm as lgbm
 import numpy as np
 import pandas as pd
@@ -43,12 +41,7 @@ def create_checkpoint_preprocessor() -> Tuple[Checkpoint, Preprocessor]:
     preprocessor = DummyPreprocessor()
     preprocessor.attr = 1
 
-    with tempfile.TemporaryDirectory() as tmpdir:
-        checkpoint = LightGBMCheckpoint.from_model(
-            booster=model, path=tmpdir, preprocessor=preprocessor
-        )
-        # Serialize to dict so we can remove the temporary directory
-        checkpoint = LightGBMCheckpoint.from_dict(checkpoint.to_dict())
+    checkpoint = LightGBMCheckpoint.from_model(booster=model, preprocessor=preprocessor)
 
     return checkpoint, preprocessor
 
