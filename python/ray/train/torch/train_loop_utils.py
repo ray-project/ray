0f9e10127335e6b413091a9dbfb452a57f8f663f
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-07-26 17:40:10 -0700

    Update python/ray/train/torch/train_loop_utils.py
    Co-authored-by: Amog Kamsetty <amogkam@users.noreply.github.com>

--- python/ray/train/torch/train_loop_utils.py
+++ python/ray/train/torch/train_loop_utils.py
@@ -505,7 +505,7 @@ class _TorchAccelerator(Accelerator):
                     raise RuntimeError(
                         "CUDA_VISIBLE_DEVICES set incorrectly. "
                         f"Got {cuda_visible_str}, expected to include {gpu_id}. "
-                        "Did you override `CUDA_VISIBLE_DEVICES`? "
+                        "Did you override the `CUDA_VISIBLE_DEVICES`?  environment variable"
                         "If not, please help file an issue on Github."
                     )
             else:
