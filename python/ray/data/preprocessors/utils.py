cb80125cb681bc2d7ef5ec3801a77c7e5f6587db
Balaji Veeramani <bveeramani@berkeley.edu>
2022-07-27 16:36:30 -0700

    [AIR] Add more robust tokenization function

--- python/ray/data/preprocessors/utils.py
+++ python/ray/data/preprocessors/utils.py
@@ -1,10 +1,31 @@
 import hashlib
+import string
 from typing import List
 
 
 def simple_split_tokenizer(value: str) -> List[str]:
     """Tokenize a string using a split on spaces."""
     return value.split(" ")
+
+
+def default_tokenization_fn(value: str) -> List[str]:
+    """Split a string into a list of tokens.
+
+    This function converts ``value`` to lowercase, removes punctuation, and then
+    delimits tokens by whitespace.
+
+    Args:
+        value: The string to tokenize.
+
+    Examples:
+        >>> string = "Hello, world!\nReadability counts."
+        >>> default_tokenization_fn(string)
+        ['hello', 'world', 'readability', 'counts']
+    """
+    value = value.lower()
+    for character in string.punctuation:
+        value = value.replace(character, "")
+    return value.split()
 
 
 def simple_hash(value: object, num_features: int) -> int:
