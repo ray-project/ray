5e400161167a2a4cbb99207780f3dc66b8538582
rapotdar <rapotdar@purdue.edu>
2022-07-26 19:05:27 -0700

    wip
    Signed-off-by: rapotdar <rapotdar@purdue.edu>
    Signed-off-by: Rohan138 <rapotdar@purdue.edu>

--- rllib/offline/estimators/tests/test_dr.py
+++ rllib/offline/estimators/tests/test_dr.py
@@ -1,12 +1,12 @@
-import os
 import unittest
-from typing import Dict
+from ray.rllib.evaluation.worker_set import WorkerSet
+from ray.rllib.execution.rollout_ops import synchronous_parallel_sample
+from ray.rllib.algorithms import AlgorithmConfig
 
 import numpy as np
-from ray.rllib.algorithms import Algorithm
-from ray.rllib.algorithms.dqn import DQNConfig
-from ray.rllib.offline import JsonReader
 from ray.rllib.offline.estimators.doubly_robust import DoublyRobust
+from ray.rllib.offline.estimators.tests.utils import GridWorldPolicy, GridWorldEnv
+from ray.rllib.policy.policy import Policy
 from ray.rllib.policy.sample_batch import SampleBatch, concat_samples
 from ray.rllib.utils.framework import try_import_torch
 
@@ -19,102 +19,125 @@ class TestDR(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         ray.init()
-        checkpoint_dir = "/tmp/cliffwalking/"
-        num_episodes = 20
+        # Epsilon-greedy exploration values
+        random_eps = 0.5
+        mixed_eps = 0.3
+        expert_eps = 0.1
+        cls.num_episodes = 128
         cls.gamma = 0.99
+        num_workers = 8
 
-        config = (
-            DQNConfig()
-            .environment(env="CartPole-v0")
-            .framework("torch")
-            .rollouts(num_rollout_workers=0)
-        )
-        cls.algo = config.build()
+        # Config settings for FQE model used in DR estimator
         cls.q_model_config = {
-            "n_iters": 160,
-            "minibatch_size": 32,
+            "n_iters": 128,
+            "minibatch_size": 128,
             "tau": 1.0,
+            "model": {
+                "fcnet_hiddens": [48],
+                "activation": "linear",
+            },
         }
 
-        cls.random_path = os.path.join(checkpoint_dir, "checkpoint", "random")
+        env = GridWorldEnv()
+        cls.random_policy = GridWorldPolicy(
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            config={"epsilon": random_eps},
+        )
+        cls.mixed_policy = GridWorldPolicy(
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            config={"epsilon": mixed_eps},
+        )
+        cls.expert_policy = GridWorldPolicy(
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            config={"epsilon": expert_eps},
+        )
+        del env
+
+        config = (
+            AlgorithmConfig()
+            .rollouts(batch_mode="complete_episodes")
+            .environment(disable_env_checking=True)
+            .experimental(_disable_preprocessor_api=True)
+            .to_dict()
+        )
+
+        cls.workers = WorkerSet(
+            env_creator=lambda env_config: GridWorldEnv(),
+            policy_class=GridWorldPolicy,
+            trainer_config=config,
+            num_workers=num_workers,
+        )
+
         (
             cls.random_batch,
             cls.random_reward,
             cls.random_std,
-        ) = cls.get_batch_and_mean_ret(
-            os.path.join(checkpoint_dir, "data", "random"),
-            cls.gamma,
-            num_episodes,
-        )
+        ) = cls.get_batch_and_mean_ret(random_eps)
         print(
             f"Collected random batch of {cls.random_batch.count} steps "
             f"with return {cls.random_reward} stddev {cls.random_std}"
         )
 
-        cls.mixed_path = os.path.join(checkpoint_dir, "checkpoint", "mixed")
-        cls.mixed_batch, cls.mixed_reward, cls.mixed_std = cls.get_batch_and_mean_ret(
-            os.path.join(checkpoint_dir, "data", "mixed"),
-            cls.gamma,
-            num_episodes,
-        )
+        (
+            cls.mixed_batch,
+            cls.mixed_reward,
+            cls.mixed_std,
+        ) = cls.get_batch_and_mean_ret(mixed_eps)
         print(
             f"Collected mixed batch of {cls.mixed_batch.count} steps "
             f"with return {cls.mixed_reward} stddev {cls.mixed_std}"
         )
 
-        cls.expert_path = os.path.join(checkpoint_dir, "checkpoint", "expert")
         (
             cls.expert_batch,
             cls.expert_reward,
             cls.expert_std,
-        ) = cls.get_batch_and_mean_ret(
-            os.path.join(checkpoint_dir, "data", "expert"),
-            cls.gamma,
-            num_episodes,
-        )
+        ) = cls.get_batch_and_mean_ret(expert_eps)
         print(
             f"Collected expert batch of {cls.expert_batch.count} steps "
             f"with return {cls.expert_reward} stddev {cls.expert_std}"
         )
+        cls.workers.stop()
 
     @classmethod
     def tearDownClass(cls):
-        cls.algo.stop()
         ray.shutdown()
 
-    @staticmethod
-    def get_batch_and_mean_ret(data_path: str, gamma: float, num_episodes: int):
-        reader = JsonReader(data_path)
+    @classmethod
+    def get_batch_and_mean_ret(cls, epsilon: float):
+        cls.workers.foreach_policy(
+            func=lambda policy, _: policy.update_epsilon(epsilon)
+        )
         ep_ret = []
         batches = []
         n_eps = 0
-        while n_eps < num_episodes:
-            batch = reader.next()
-            batches.append(batch)
+        while n_eps < cls.num_episodes:
+            batch = synchronous_parallel_sample(worker_set=cls.workers)
             for episode in batch.split_by_episode():
                 ret = 0
                 for r in episode[SampleBatch.REWARDS][::-1]:
-                    ret = r + gamma * ret
+                    ret = r + cls.gamma * ret
                 ep_ret.append(ret)
                 n_eps += 1
+            batches.append(batch)
         return concat_samples(batches), np.mean(ep_ret), np.std(ep_ret)
 
-    @staticmethod
+    @classmethod
     def check(
-        algo: Algorithm,
-        checkpoint: str,
+        cls,
+        policy: Policy,
         batch: SampleBatch,
         mean_ret: float,
         std_ret: float,
-        q_model_config: Dict,
     ):
-        algo.load_checkpoint(checkpoint)
 
-        q_model_config = q_model_config.copy()
         estimator = DoublyRobust(
-            policy=algo.get_policy(),
-            gamma=algo.config["gamma"],
-            q_model_config=q_model_config,
+            policy=policy,
+            gamma=cls.gamma,
+            q_model_config=cls.q_model_config,
         )
         loss = estimator.train(batch)["loss"]
         estimates = estimator.estimate(batch)
@@ -133,100 +156,82 @@ class TestDR(unittest.TestCase):
     def test_random_random_data(self):
         print("Test random policy on random dataset")
         self.check(
-            self.algo,
-            self.random_path,
+            self.random_policy,
             self.random_batch,
             self.random_reward,
             self.random_std,
-            self.q_model_config,
         )
 
     def test_random_mixed_data(self):
         print("Test random policy on mixed dataset")
         self.check(
-            self.algo,
-            self.random_path,
+            self.random_policy,
             self.mixed_batch,
             self.random_reward,
             self.random_std,
-            self.q_model_config,
         )
 
     def test_random_expert_data(self):
         print("Test random policy on expert dataset")
         self.check(
-            self.algo,
-            self.random_path,
+            self.random_policy,
             self.expert_batch,
             self.random_reward,
             self.random_std,
-            self.q_model_config,
         )
 
     def test_mixed_random_data(self):
         print("Test mixed policy on random dataset")
         self.check(
-            self.algo,
-            self.mixed_path,
+            self.mixed_policy,
             self.random_batch,
             self.mixed_reward,
             self.mixed_std,
-            self.q_model_config,
         )
 
     def test_mixed_mixed_data(self):
         print("Test mixed policy on mixed dataset")
         self.check(
-            self.algo,
-            self.mixed_path,
+            self.mixed_policy,
             self.mixed_batch,
             self.mixed_reward,
             self.mixed_std,
-            self.q_model_config,
         )
 
     def test_mixed_expert_data(self):
         print("Test mixed policy on expert dataset")
         self.check(
-            self.algo,
-            self.mixed_path,
+            self.mixed_policy,
             self.expert_batch,
             self.mixed_reward,
             self.mixed_std,
-            self.q_model_config,
         )
 
     def test_expert_random_data(self):
         print("Test expert policy on random dataset")
         self.check(
-            self.algo,
-            self.expert_path,
+            self.expert_policy,
             self.random_batch,
             self.expert_reward,
             self.expert_std,
-            self.q_model_config,
         )
 
     def test_expert_mixed_data(self):
         print("Test expert policy on mixed dataset")
         self.check(
-            self.algo,
-            self.expert_path,
+            self.expert_policy,
             self.mixed_batch,
             self.expert_reward,
             self.expert_std,
-            self.q_model_config,
         )
 
     def test_expert_expert_data(self):
         print("Test expert policy on expert dataset")
         self.check(
-            self.algo,
-            self.expert_path,
+            self.expert_policy,
             self.expert_batch,
             self.expert_reward,
             self.expert_std,
-            self.q_model_config,
         )
 
 
