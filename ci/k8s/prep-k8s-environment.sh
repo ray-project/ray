f4f4f49e6312dfe6b84561d762e274f89250bdc5
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2022-07-26 22:30:20 -0700

    Delete spare newline
    Signed-off-by: Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>

--- ci/k8s/prep-k8s-environment.sh
+++ ci/k8s/prep-k8s-environment.sh
@@ -19,7 +19,6 @@ kubectl version --client
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
 mv ./kustomize /usr/bin/kustomize
 
-
 # Delete dangling clusters
 kind delete clusters --all
 
