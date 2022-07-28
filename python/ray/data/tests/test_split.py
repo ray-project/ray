ray-project/ray

commit fb54679a239e4c7368a72a2fe3023cac04380827

[Datasets] Refactor `split_at_indices()` to minimize number of split tasks and data movement. (#26363)
The current Dataset.split_at_indices() implementation suffers from O(n^2) memory usage in the small-split case (see issue) due to recursive splitting of the same blocks. This PR implements a split_at_indices() algorithm that minimizes the number of split tasks and data movement while ensuring that at most one block is used in each split task, for the sake of memory stability. Co-authored-by: scv119 <scv119@gmail.com>

python/ray/data/_internal/split.py
@@ -1,0 +1,249 @@
+import logging
+from typing import Iterable, Tuple, List
+
+import ray
+from ray.data._internal.remote_fn import cached_remote_fn
+from ray.data.block import (
+    Block,
+    BlockAccessor,
+    BlockExecStats,
+    BlockMetadata,
+)
+from ray.types import ObjectRef
+
+logger = logging.getLogger(__name__)
+
+
+def _calculate_blocks_rows(
+    blocks_with_metadata: List[Tuple[ObjectRef[Block], BlockMetadata]],
+) -> List[int]:
+    """Calculate the number of rows for a list of blocks with metadata."""
+    get_num_rows = cached_remote_fn(_get_num_rows)
+    block_rows = []
+    for block, metadata in blocks_with_metadata:
+        if metadata.num_rows is None:
+            # Need to fetch number of rows.
+            num_rows = ray.get(get_num_rows.remote(block))
+        else:
+            num_rows = metadata.num_rows
+        block_rows.append(num_rows)
+    return block_rows
+
+
+def _generate_valid_indices(
+    num_rows_per_block: List[int],
+    split_indices: List[int],
+) -> List[int]:
+    """Generate valid split indices by apply min(index, total_num_rows)
+    to every index."""
+    total_rows = sum(num_rows_per_block)
+    return [min(index, total_rows) for index in split_indices]
+
+
+def _generate_per_block_split_indices(
+    num_rows_per_block: List[int],
+    split_indices: List[int],
+) -> List[List[int]]:
+    """Given num rows per block and valid split indices, generate per block split indices.
+
+    Args:
+        num_rows_per_block: num of rows per block.
+        split_indices: The (global) indices at which to split the blocks.
+    Returns:
+        Per block split indices indicates each input block's split point(s).
+    """
+    # for each split index, we iterate though the currnet input block
+    # to see if the index falls into this block. if the index
+    # falls into this block, we push it back to the current block's
+    # split indices. Otherwise, we move on to the next block.
+    per_block_split_indices = []
+    current_input_block_id = 0
+    current_block_split_indices = []
+    current_block_global_offset = 0
+    current_index_id = 0
+
+    while current_index_id < len(split_indices):
+        split_index = split_indices[current_index_id]
+        current_block_row = num_rows_per_block[current_input_block_id]
+        if split_index - current_block_global_offset <= current_block_row:
+            current_block_split_indices.append(
+                split_index - current_block_global_offset
+            )
+            current_index_id += 1
+            continue
+        per_block_split_indices.append(current_block_split_indices)
+        current_block_split_indices = []
+        current_block_global_offset += num_rows_per_block[current_input_block_id]
+        current_input_block_id += 1
+
+    # we might finished all the indices but there are still blocks left, also
+    # current_block_split_indices might not be added yet.
+    while len(per_block_split_indices) < len(num_rows_per_block):
+        per_block_split_indices.append(current_block_split_indices)
+        current_block_split_indices = []
+    return per_block_split_indices
+
+
+def _split_single_block(
+    block_id: int,
+    block: Block,
+    meta: BlockMetadata,
+    block_row: int,
+    split_indices: List[int],
+) -> Tuple[int, List[Tuple[ObjectRef[Block], BlockMetadata]]]:
+    """Split the provided block at the given indices."""
+    split_result = []
+    block_accessor = BlockAccessor.for_block(block)
+    prev_index = 0
+    # append one more entry at the last so we don't
+    # need handle empty edge case.
+    split_indices.append(block_row)
+    for index in split_indices:
+        logger.debug(f"slicing block {prev_index}:{index}")
+        stats = BlockExecStats.builder()
+        split_block = block_accessor.slice(prev_index, index, copy=True)
+        accessor = BlockAccessor.for_block(split_block)
+        split_meta = BlockMetadata(
+            num_rows=accessor.num_rows(),
+            size_bytes=accessor.size_bytes(),
+            schema=meta.schema,
+            input_files=meta.input_files,
+            exec_stats=stats.build(),
+        )
+        split_result.append((ray.put(split_block), split_meta))
+        prev_index = index
+    return (block_id, split_result)
+
+
+def _split_all_blocks(
+    blocks_with_metadata: List[Tuple[ObjectRef[Block], BlockMetadata]],
+    block_rows: List[int],
+    per_block_split_indices: List[List[int]],
+) -> List[Tuple[ObjectRef[Block], BlockMetadata]]:
+    """Split all the input blocks based on the split indices"""
+    split_single_block = cached_remote_fn(_split_single_block)
+
+    all_blocks_split_results: List[List[Tuple[ObjectRef[Block], BlockMetadata]]] = [
+        None
+    ] * len(blocks_with_metadata)
+
+    split_single_block_futures = []
+
+    for block_id, block_split_indices in enumerate(per_block_split_indices):
+        (block_ref, meta) = blocks_with_metadata[block_id]
+        block_row = block_rows[block_id]
+        if len(block_split_indices) == 0:
+            # optimization: if no split is needed, we just need to add it to the
+            # result
+            all_blocks_split_results[block_id] = [(block_ref, meta)]
+        else:
+            # otherwise call split remote function.
+            split_single_block_futures.append(
+                split_single_block.remote(
+                    block_id,
+                    block_ref,
+                    meta,
+                    block_row,
+                    block_split_indices,
+                )
+            )
+    if split_single_block_futures:
+        split_single_block_results = ray.get(split_single_block_futures)
+        for block_id, block_split_result in split_single_block_results:
+            all_blocks_split_results[block_id] = block_split_result
+    return all_blocks_split_results
+
+
+def _generate_global_split_results(
+    all_blocks_split_results: List[List[Tuple[ObjectRef[Block], BlockMetadata]]],
+) -> Tuple[List[List[ObjectRef[Block]]], List[List[BlockMetadata]]]:
+    """Reassemble per block's split result into final split result."""
+    result_blocks = []
+    result_metas = []
+    current_blocks = []
+    current_meta = []
+
+    if len(all_blocks_split_results) == 0:
+        return ([], [])
+
+    for single_block_split_result in all_blocks_split_results:
+        assert len(single_block_split_result) > 0
+        for i, (block, meta) in enumerate(single_block_split_result):
+            # we should create a new global split whenever
+            # we encountered a new local split in the per block
+            # split result.
+            if i != 0:
+                result_blocks.append(current_blocks)
+                result_metas.append(current_meta)
+                current_blocks = []
+                current_meta = []
+            current_blocks.append(block)
+            current_meta.append(meta)
+
+    assert len(current_blocks) > 0
+    result_blocks.append(current_blocks)
+    result_metas.append(current_meta)
+
+    return result_blocks, result_metas
+
+
+def _split_at_indices(
+    blocks_with_metadata: Iterable[Tuple[ObjectRef[Block], BlockMetadata]],
+    indices: List[int],
+) -> Tuple[List[List[ObjectRef[Block]]], List[List[BlockMetadata]]]:
+    """Split blocks at the provided indices.
+
+    Args:
+        blocks_with_metadata: Block futures to split, including the associated metadata.
+        indices: The (global) indices at which to split the blocks.
+    Returns:
+        The block split futures and their metadata. If an index split is empty, the
+        corresponding block split will be empty .
+    """
+
+    # We implement the split in 3 phases.
+    # phase 1: calculate the per block split indices.
+    blocks_with_metadata = list(blocks_with_metadata)
+    if len(blocks_with_metadata) == 0:
+        return ([] * (len(indices) + 1), [] * (len(indices) + 1))
+    block_rows: List[int] = _calculate_blocks_rows(blocks_with_metadata)
+    valid_indices = _generate_valid_indices(block_rows, indices)
+    per_block_split_indices: List[List[int]] = _generate_per_block_split_indices(
+        block_rows, valid_indices
+    )
+
+    # phase 2: split each block based on the indices from previous step.
+    all_blocks_split_results: List[
+        List[Tuple[ObjectRef[Block], BlockMetadata]]
+    ] = _split_all_blocks(blocks_with_metadata, block_rows, per_block_split_indices)
+
+    # phase 3: generate the final split.
+    return _generate_global_split_results(all_blocks_split_results)
+
+
+def _get_num_rows(block: Block) -> int:
+    """Get the number of rows contained in the provided block."""
+    return BlockAccessor.for_block(block).num_rows()
+
+
+def _split_at_index(
+    blocks_with_metadata: Iterable[Tuple[ObjectRef[Block], BlockMetadata]],
+    index: int,
+    return_right_half: bool,
+) -> Tuple[
+    List[ObjectRef[Block]],
+    List[BlockMetadata],
+    List[ObjectRef[Block]],
+    List[BlockMetadata],
+]:
+    """Split blocks at the provided index.
+    Args:
+        blocks_with_metadata: Block futures to split, including the associated metadata.
+        index: The (global) index at which to split the blocks.
+        return_right_half: Whether we want to return or drop the data to the right of
+            the index.
+    Returns:
+        The block split futures and their metadata for left and right of the index.
+    """
+    blocks_splits, metadata_splits = _split_at_indices(blocks_with_metadata, [index])
+    return blocks_splits[0], metadata_splits[0], blocks_splits[1], metadata_splits[1]

python/ray/data/tests/test_split.py
@@ -1,3 +1,4 @@
+import itertools
 import math
 import random
 import time
@@ -10,6 +11,10 @@ import ray
 from ray.data._internal.block_list import BlockList
 from ray.data._internal.plan import ExecutionPlan
 from ray.data._internal.stats import DatasetStats
+from ray.data._internal.split import (
+    _generate_valid_indices,
+    _generate_per_block_split_indices,
+)
 from ray.data.block import BlockAccessor
 from ray.data.dataset import Dataset
 from ray.data.tests.conftest import *  # noqa
@@ -187,7 +192,7 @@ def test_split_small(ray_start_regular_shared, pipelined):
     assert not fail, fail
 
 
-def test_split_at_indices(ray_start_regular_shared):
+def test_split_at_indices_simple(ray_start_regular_shared):
     ds = ray.data.range(10, parallelism=3)
 
     with pytest.raises(ValueError):
@@ -218,6 +223,79 @@ def test_split_at_indices(ray_start_regular_shared):
     splits = ds.split_at_indices([0])
     r = [s.take() for s in splits]
     assert r == [[], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]
+
+
+@pytest.mark.parametrize("num_blocks", list(range(1, 20)) + [25, 40])
+@pytest.mark.parametrize(
+    "indices",
+    [
+        # Two-splits.
+        [5],
+        [10],
+        [15],
+        # Three-splits.
+        [5, 12],
+        [1, 18],
+        [9, 10],
+        # Misc.
+        [3, 10, 17],
+        [2, 4, 11, 12, 19],
+        list(range(20)),
+        list(range(0, 20, 2)),
+        # Empty splits.
+        [10, 10],
+        [5, 10, 10, 15],
+        # Out-of-bounds.
+        [25],
+        [7, 11, 23, 33],
+    ],
+)
+def test_split_at_indices_coverage(ray_start_regular_shared, num_blocks, indices):
+    # Test that split_at_indices() creates the expected splits on a set of partition and
+    # indices configurations.
+    ds = ray.data.range(20, parallelism=num_blocks)
+    splits = ds.split_at_indices(indices)
+    r = [s.take_all() for s in splits]
+    # Use np.array_split() semantics as our correctness ground-truth.
+    assert r == [arr.tolist() for arr in np.array_split(list(range(20)), indices)]
+
+
+@pytest.mark.parametrize("num_blocks", list(range(1, 5)) + [8, 10])
+@pytest.mark.parametrize(
+    "indices",
+    [
+        # Two-splits.
+        list(range(5)),
+    ]
+    + list(
+        # Three-splits.
+        map(list, itertools.combinations_with_replacement(list(range(5)), 2))
+    )
+    + list(
+        # Four-splits.
+        map(list, itertools.combinations_with_replacement(list(range(5)), 3))
+    )
+    + list(
+        # Five-splits.
+        map(list, itertools.combinations_with_replacement(list(range(5)), 4))
+    )
+    + list(
+        # Six-splits.
+        map(list, itertools.combinations_with_replacement(list(range(5)), 5))
+    ),
+)
+def test_split_at_indices_coverage_complete(
+    ray_start_regular_shared,
+    num_blocks,
+    indices,
+):
+    # Test that split_at_indices() creates the expected splits on a set of partition and
+    # indices configurations.
+    ds = ray.data.range(5, parallelism=num_blocks)
+    splits = ds.split_at_indices(indices)
+    r = [s.take_all() for s in splits]
+    # Use np.array_split() semantics as our correctness ground-truth.
+    assert r == [arr.tolist() for arr in np.array_split(list(range(5)), indices)]
 
 
 def test_split_proportionately(ray_start_regular_shared):
@@ -393,3 +471,18 @@ def test_split_hints(ray_start_regular_shared):
         ["n1", "n2", "n0"],
         [range(200, 301), range(100, 200), list(range(0, 50)) + list(range(50, 100))],
     )
+
+
+def test_generate_valid_indices():
+    assert [1, 2, 3] == _generate_valid_indices([10], [1, 2, 3])
+    assert [1, 2, 2] == _generate_valid_indices([1, 1], [1, 2, 3])
+
+
+def test_generate_per_block_split_indices():
+    assert [[1], [1, 2], [], []] == _generate_per_block_split_indices(
+        [3, 3, 3, 1], [1, 4, 5]
+    )
+    assert [[3], [], [], [1, 1]] == _generate_per_block_split_indices(
+        [3, 3, 3, 1], [3, 10, 10]
+    )
+    assert [[], [], [], []] == _generate_per_block_split_indices([3, 3, 3, 1], [])

python/ray/data/dataset.py
@@ -47,6 +47,7 @@ from ray.data._internal.stage_impl import (
 )
 from ray.data._internal.progress_bar import ProgressBar
 from ray.data._internal.remote_fn import cached_remote_fn
+from ray.data._internal.split import _split_at_index, _split_at_indices, _get_num_rows
 from ray.data._internal.stats import DatasetStats
 from ray.data._internal.table_block import VALUE_COL_NAME
 from ray.data.aggregate import AggregateFn, Max, Mean, Min, Std, Sum
@@ -55,7 +56,6 @@ from ray.data.block import (
     BatchUDF,
     Block,
     BlockAccessor,
-    BlockExecStats,
     BlockMetadata,
     BlockPartition,
     BlockPartitionMetadata,
@@ -1203,16 +1203,25 @@ class Dataset(Generic[T]):
             raise ValueError("indices must be sorted")
         if indices[0] < 0:
             raise ValueError("indices must be positive")
-
-        rest = self
+        start_time = time.perf_counter()
+        blocks_with_metadata = self._plan.execute().get_blocks_with_metadata()
+        blocks, metadata = _split_at_indices(blocks_with_metadata, indices)
+        split_duration = time.perf_counter() - start_time
+        parent_stats = self._plan.stats()
         splits = []
-        prev = 0
-        for i in indices:
-            first, rest = rest._split(i - prev, return_right_half=True)
-            prev = i
-            splits.append(first)
-        splits.append(rest)
-
+        for bs, ms in zip(blocks, metadata):
+            stats = DatasetStats(stages={"split": ms}, parent=parent_stats)
+            stats.time_total_s = split_duration
+            splits.append(
+                Dataset(
+                    ExecutionPlan(
+                        BlockList(bs, ms),
+                        stats,
+                    ),
+                    self._epoch,
+                    self._lazy,
+                )
+            )
         return splits
 
     def split_proportionately(self, proportions: List[float]) -> List["Dataset[T]"]:
@@ -3278,41 +3287,12 @@ class Dataset(Generic[T]):
         self, index: int, return_right_half: bool
     ) -> ("Dataset[T]", "Dataset[T]"):
         start_time = time.perf_counter()
-        get_num_rows = cached_remote_fn(_get_num_rows)
-        split_block = cached_remote_fn(_split_block, num_returns=4)
-
-        count = 0
-        left_blocks = []
-        left_metadata = []
-        right_blocks = []
-        right_metadata = []
-        it = self._plan.execute().get_blocks_with_metadata()
-        for b, m in it:
-            if m.num_rows is None:
-                num_rows = ray.get(get_num_rows.remote(b))
-            else:
-                num_rows = m.num_rows
-            if count >= index:
-                if not return_right_half:
-                    break
-                right_blocks.append(b)
-                right_metadata.append(m)
-            elif count + num_rows < index:
-                left_blocks.append(b)
-                left_metadata.append(m)
-            elif count + num_rows == index:
-                left_blocks.append(b)
-                left_metadata.append(m)
-            else:
-                b0, m0, b1, m1 = split_block.remote(
-                    b, m, index - count, return_right_half
-                )
-                left_blocks.append(b0)
-                left_metadata.append(ray.get(m0))
-                right_blocks.append(b1)
-                right_metadata.append(ray.get(m1))
-            count += num_rows
-
+        blocks_with_metadata = self._plan.execute().get_blocks_with_metadata()
+        left_blocks, left_metadata, right_blocks, right_metadata = _split_at_index(
+            blocks_with_metadata,
+            index,
+            return_right_half,
+        )
         split_duration = time.perf_counter() - start_time
         left_meta_for_stats = [
             BlockMetadata(
@@ -3528,11 +3508,6 @@ class Dataset(Generic[T]):
             )
 
 
-def _get_num_rows(block: Block) -> int:
-    block = BlockAccessor.for_block(block)
-    return block.num_rows()
-
-
 def _get_size_bytes(block: Block) -> int:
     block = BlockAccessor.for_block(block)
     return block.size_bytes()
@@ -3579,37 +3554,6 @@ def _sliding_window(iterable: Iterable, n: int):
     for elem in it:
         window.append(elem)
         yield tuple(window)
-
-
-def _split_block(
-    block: Block, meta: BlockMetadata, count: int, return_right_half: bool
-) -> (Block, BlockMetadata, Optional[Block], Optional[BlockMetadata]):
-    stats = BlockExecStats.builder()
-    block = BlockAccessor.for_block(block)
-    logger.debug("Truncating last block to size: {}".format(count))
-    b0 = block.slice(0, count, copy=True)
-    a0 = BlockAccessor.for_block(b0)
-    m0 = BlockMetadata(
-        num_rows=a0.num_rows(),
-        size_bytes=a0.size_bytes(),
-        schema=meta.schema,
-        input_files=meta.input_files,
-        exec_stats=stats.build(),
-    )
-    if return_right_half:
-        b1 = block.slice(count, block.num_rows(), copy=True)
-        a1 = BlockAccessor.for_block(b1)
-        m1 = BlockMetadata(
-            num_rows=a1.num_rows(),
-            size_bytes=a1.size_bytes(),
-            schema=meta.schema,
-            input_files=meta.input_files,
-            exec_stats=stats.build(),
-        )
-    else:
-        b1 = None
-        m1 = None
-    return b0, m0, b1, m1
 
 
 def _do_write(

python/ray/data/tests/test_dataset.py
@@ -1392,7 +1392,7 @@ def test_repartition_noshuffle(ray_start_regular_shared):
     blocks = ray.get(ds4.get_internal_block_refs())
     assert all(isinstance(block, list) for block in blocks), blocks
     assert ds4.sum() == 190
-    assert ds4._block_num_rows() == ([1] * 20) + ([0] * 20)
+    assert ds4._block_num_rows() == [0, 1] * 20
 
     ds5 = ray.data.range(22).repartition(4)
     assert ds5.num_blocks() == 4

python/ray/data/tests/test_stats.py
@@ -160,7 +160,7 @@ def test_dataset_stats_read_parquet(ray_start_regular_shared, tmp_path):
 
 def test_dataset_split_stats(ray_start_regular_shared, tmp_path):
     ds = ray.data.range(100, parallelism=10).map(lambda x: x + 1)
-    dses = ds.split_at_indices([50])
+    dses = ds.split_at_indices([49])
     dses = [ds.map(lambda x: x + 1) for ds in dses]
     for ds_ in dses:
         stats = canonicalize(ds_.stats())
@@ -174,9 +174,13 @@ def test_dataset_split_stats(ray_start_regular_shared, tmp_path):
 * Output size bytes: N min, N max, N mean, N total
 * Tasks per node: N min, N max, N mean; N nodes used
 
-Stage N split: N/N blocks split from parent in T
+Stage N split: N/N blocks executed in T
+* Remote wall time: T min, T max, T mean, T total
+* Remote cpu time: T min, T max, T mean, T total
+* Peak heap memory usage (MiB): N min, N max, N mean
 * Output num rows: N min, N max, N mean, N total
 * Output size bytes: N min, N max, N mean, N total
+* Tasks per node: N min, N max, N mean; N nodes used
 
 Stage N map: N/N blocks executed in T
 * Remote wall time: T min, T max, T mean, T total

commit 0d4990165126d77e09e0655f7d3f48817a906aef

[Data][Split] Fix split ownership (#27149)
fb54679 introduced a bug by calling ray.put in the remote _split_single_block. This changes the ownership from driver to the worker who runs _split_single_block, which breaks dataset's lineage requirement and failed the chaos test.

To fix the issue we need to ensure the split block refs are created by the driver, which we can achieved by creating the block_refs as part of function returns.

python/ray/data/_internal/split.py
@@ -1,6 +1,6 @@
 import itertools
 import logging
-from typing import Iterable, Tuple, List
+from typing import Union, Iterable, Tuple, List
 
 import ray
 from ray.data._internal.block_list import BlockList
@@ -93,9 +93,23 @@ def _split_single_block(
     block: Block,
     meta: BlockMetadata,
     split_indices: List[int],
-) -> Tuple[int, BlockPartition]:
-    """Split the provided block at the given indices."""
-    split_result = []
+) -> Tuple[Union[Tuple[int, List[BlockMetadata]], Block], ...]:
+    """Split the provided block at the given indices.
+
+    Args:
+        block_id: the id of this block in the block list.
+        block: block to be split.
+        meta: metadata of the block, we expect meta.num is valid.
+        split_indices: the indices where the block should be split.
+    Returns:
+        returns block_id, split blocks metadata, and a list of blocks
+        in the following form. We return blocks in this way
+        so that the owner of blocks could be the caller(driver)
+        instead of worker itself.
+        Tuple(block_id, split_blocks_meta), block0, block1 ...
+    """
+    split_meta = []
+    split_blocks = []
     block_accessor = BlockAccessor.for_block(block)
     prev_index = 0
     # append one more entry at the last so we don't
@@ -106,16 +120,19 @@ def _split_single_block(
         stats = BlockExecStats.builder()
         split_block = block_accessor.slice(prev_index, index, copy=True)
         accessor = BlockAccessor.for_block(split_block)
-        split_meta = BlockMetadata(
+        _meta = BlockMetadata(
             num_rows=accessor.num_rows(),
             size_bytes=accessor.size_bytes(),
             schema=meta.schema,
             input_files=meta.input_files,
             exec_stats=stats.build(),
         )
-        split_result.append((ray.put(split_block), split_meta))
+        split_meta.append(_meta)
+        split_blocks.append(split_block)
         prev_index = index
-    return (block_id, split_result)
+    results = [(block_id, split_meta)]
+    results.extend(split_blocks)
+    return tuple(results)
 
 
 def _drop_empty_block_split(block_split_indices: List[int], num_rows: int) -> List[int]:
@@ -145,8 +162,10 @@ def _split_all_blocks(
     blocks_with_metadata = block_list.get_blocks_with_metadata()
     all_blocks_split_results: List[BlockPartition] = [None] * len(blocks_with_metadata)
 
-    split_single_block_futures = []
+    per_block_split_metadata_futures = []
+    per_block_split_block_refs = []
 
+    # tracking splitted blocks for gc.
     blocks_splitted = []
     for block_id, block_split_indices in enumerate(per_block_split_indices):
         (block_ref, meta) = blocks_with_metadata[block_id]
@@ -158,19 +177,27 @@ def _split_all_blocks(
             all_blocks_split_results[block_id] = [(block_ref, meta)]
         else:
             # otherwise call split remote function.
-            split_single_block_futures.append(
-                split_single_block.options(scheduling_strategy="SPREAD").remote(
-                    block_id,
-                    block_ref,
-                    meta,
-                    block_split_indices,
-                )
+            object_refs = split_single_block.options(
+                scheduling_strategy="SPREAD", num_returns=2 + len(block_split_indices)
+            ).remote(
+                block_id,
+                block_ref,
+                meta,
+                block_split_indices,
             )
+            per_block_split_metadata_futures.append(object_refs[0])
+            per_block_split_block_refs.append(object_refs[1:])
+
             blocks_splitted.append(block_ref)
-    if split_single_block_futures:
-        split_single_block_results = ray.get(split_single_block_futures)
-        for block_id, block_split_result in split_single_block_results:
-            all_blocks_split_results[block_id] = block_split_result
+
+    if per_block_split_metadata_futures:
+        # only get metadata.
+        per_block_split_metadata = ray.get(per_block_split_metadata_futures)
+        for (block_id, meta), block_refs in zip(
+            per_block_split_metadata, per_block_split_block_refs
+        ):
+            assert len(meta) == len(block_refs)
+            all_blocks_split_results[block_id] = zip(block_refs, meta)
 
     # We make a copy for the blocks that have been splitted, so the input blocks
     # can be cleared if they are owned by consumer (consumer-owned blocks will

python/ray/data/tests/test_split.py
@@ -522,47 +522,63 @@ def _create_blocklist(blocks):
 
 def test_split_single_block(ray_start_regular_shared):
     block = [1, 2, 3]
-    meta = _create_meta(3)
+    metadata = _create_meta(3)
 
-    block_id, splits = ray.get(
-        ray.remote(_split_single_block).remote(234, block, meta, [])
+    results = ray.get(
+        ray.remote(_split_single_block)
+        .options(num_returns=2)
+        .remote(234, block, metadata, [])
     )
+    block_id, meta = results[0]
+    blocks = results[1:]
     assert 234 == block_id
-    assert len(splits) == 1
-    assert ray.get(splits[0][0]) == [1, 2, 3]
-    assert splits[0][1].num_rows == 3
+    assert len(blocks) == 1
+    assert blocks[0] == [1, 2, 3]
+    assert meta[0].num_rows == 3
 
-    block_id, splits = ray.get(
-        ray.remote(_split_single_block).remote(234, block, meta, [1])
+    results = ray.get(
+        ray.remote(_split_single_block)
+        .options(num_returns=3)
+        .remote(234, block, metadata, [1])
     )
+    block_id, meta = results[0]
+    blocks = results[1:]
     assert 234 == block_id
-    assert len(splits) == 2
-    assert ray.get(splits[0][0]) == [1]
-    assert splits[0][1].num_rows == 1
-    assert ray.get(splits[1][0]) == [2, 3]
-    assert splits[1][1].num_rows == 2
+    assert len(blocks) == 2
+    assert blocks[0] == [1]
+    assert meta[0].num_rows == 1
+    assert blocks[1] == [2, 3]
+    assert meta[1].num_rows == 2
 
-    block_id, splits = ray.get(
-        ray.remote(_split_single_block).remote(234, block, meta, [0, 1, 1, 3])
+    results = ray.get(
+        ray.remote(_split_single_block)
+        .options(num_returns=6)
+        .remote(234, block, metadata, [0, 1, 1, 3])
     )
+    block_id, meta = results[0]
+    blocks = results[1:]
     assert 234 == block_id
-    assert len(splits) == 5
-    assert ray.get(splits[0][0]) == []
-    assert ray.get(splits[1][0]) == [1]
-    assert ray.get(splits[2][0]) == []
-    assert ray.get(splits[3][0]) == [2, 3]
-    assert ray.get(splits[4][0]) == []
+    assert len(blocks) == 5
+    assert blocks[0] == []
+    assert blocks[1] == [1]
+    assert blocks[2] == []
+    assert blocks[3] == [2, 3]
+    assert blocks[4] == []
 
     block = []
-    meta = _create_meta(0)
+    metadata = _create_meta(0)
 
-    block_id, splits = ray.get(
-        ray.remote(_split_single_block).remote(234, block, meta, [0])
+    results = ray.get(
+        ray.remote(_split_single_block)
+        .options(num_returns=3)
+        .remote(234, block, metadata, [0])
     )
+    block_id, meta = results[0]
+    blocks = results[1:]
     assert 234 == block_id
-    assert len(splits) == 2
-    assert ray.get(splits[0][0]) == []
-    assert ray.get(splits[1][0]) == []
+    assert len(blocks) == 2
+    assert blocks[0] == []
+    assert blocks[1] == []
 
 
 def test_drop_empty_block_split():