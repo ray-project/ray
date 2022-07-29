0f96f9a89933bfb3020eeea43080334a6dd49439
Alan Guo <aguo@anyscale.com>
2022-07-29 11:36:10 -0700

    Force grpcio to be >= 1.42.0 for python 3.10
    fixes #27160
    
    There is a known issue with grpcio < 1.42.0 using a parameter that was removed in python 3.10
    https://github.com/grpc/grpc/issues/27888
    
    Signed-off-by: Alan Guo <aguo@anyscale.com>

--- python/setup.py
+++ python/setup.py
@@ -287,6 +287,10 @@ if setup_spec.type == SetupType.RAY:
         "dataclasses; python_version < '3.7'",
         "filelock",
         "grpcio >= 1.28.1, != 1.44.*, != 1.45.*, != 1.46.*, != 1.47.*",
+        (
+            "grpcio >= 1.42.0, != 1.44.*, != 1.45.*, != 1.46.*, != 1.47.*;"
+            " python_version >= '3.10'"
+        ),
         "jsonschema",
         "msgpack >= 1.0.0, < 2.0.0",
         "numpy >= 1.16; python_version < '3.9'",
