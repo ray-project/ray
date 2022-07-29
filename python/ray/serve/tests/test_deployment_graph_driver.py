3fc32e1aa0b195c554406836cfe033502e4e8770
Sihan Wang <sihanwang41@gmail.com>
2022-07-28 17:15:23 -0700

    Add more tests & rebase
    Signed-off-by: Sihan Wang <sihanwang41@gmail.com>

--- python/ray/serve/tests/test_deployment_graph_driver.py
+++ python/ray/serve/tests/test_deployment_graph_driver.py
@@ -100,24 +100,85 @@ async def json_resolver(request: starlette.requests.Request):
 
 
 def test_multi_dag(serve_instance):
+    """Test multi dags within dag deployment"""
+
     @serve.deployment
     class D1:
-        def __call__(self, input):
-            return input
+        def forward(self):
+            return "D1"
 
     @serve.deployment
-    def D2():
-        return "D2"
+    class D2:
+        def forward(self):
+            return "D2"
 
+    @serve.deployment
+    class D3:
+        def __call__(self):
+            return "D3"
+
+    @serve.deployment
+    def D4():
+        return "D4"
+
+    d1 = D1.bind()
+    d2 = D2.bind()
+    d3 = D3.bind()
+    d4 = D4.bind()
     dag = DAGDriver.bind(
-        {"/my_D1": D1.bind(), "/my_D2": D2.bind()}, http_adapter=json_resolver
+        {
+            "/my_D1": d1.forward.bind(),
+            "/my_D2": d2.forward.bind(),
+            "/my_D3": d3,
+            "/my_D4": d4,
+        }
     )
     handle = serve.run(dag)
 
+    for i in range(1, 5):
+        assert ray.get(handle.predict_with_route.remote(f"/my_D{i}")) == f"D{i}"
+        assert requests.post(f"http://127.0.0.1:8000/my_D{i}", json=1).json() == f"D{i}"
+        assert requests.get(f"http://127.0.0.1:8000/my_D{i}", json=1).json() == f"D{i}"
+
+
+def test_multi_dag_with_inputs(serve_instance):
+    @serve.deployment
+    class D1:
+        def forward(self, input):
+            return input
+
+    @serve.deployment
+    class D2:
+        def forward(self, input1, input2):
+            return input1 + input2
+
+    @serve.deployment
+    def D3(input):
+        return input
+
+    d1 = D1.bind()
+    d2 = D2.bind()
+
+    with InputNode() as dag_input:
+        dag = DAGDriver.bind(
+            {
+                "/my_D1": d1.forward.bind(dag_input),
+                "/my_D2": d2.forward.bind(dag_input[0], dag_input[1]),
+                "/my_D3": D3.bind(dag_input),
+            },
+            http_adapter=json_resolver,
+        )
+        handle = serve.run(dag)
+
     assert ray.get(handle.predict_with_route.remote("/my_D1", 1)) == 1
-    assert ray.get(handle.predict_with_route.remote(route_path="/my_D2")) == "D2"
+    assert ray.get(handle.predict_with_route.remote("/my_D2", 10, 2)) == 12
+    assert ray.get(handle.predict_with_route.remote("/my_D3", 100)) == 100
     assert requests.post("http://127.0.0.1:8000/my_D1", json=1).json() == 1
-    assert requests.post("http://127.0.0.1:8000/my_D2").json() == "D2"
+    assert requests.post("http://127.0.0.1:8000/my_D2", json=[1, 2]).json() == 3
+    assert requests.post("http://127.0.0.1:8000/my_D3", json=100).json() == 100
+    assert requests.get("http://127.0.0.1:8000/my_D1", json=1).json() == 1
+    assert requests.get("http://127.0.0.1:8000/my_D2", json=[1, 2]).json() == 3
+    assert requests.get("http://127.0.0.1:8000/my_D3", json=100).json() == 100
 
 
 if __name__ == "__main__":
