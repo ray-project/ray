f1f40a4236ac1261a4c0451cc1651c0059aa9e79
Clarence Ng <clarence.wyng@gmail.com>
2022-07-31 11:49:09 -0700

    f
    Signed-off-by: Clarence Ng <clarence.wyng@gmail.com>

--- src/ray/util/process.cc
+++ src/ray/util/process.cc
@@ -380,11 +380,11 @@ std::string Process::Exec(const std::string command) {
   /// https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-the-output-of-the-command-within-c-using-po
   std::array<char, 128> buffer;
   std::string result;
-  #ifdef _WIN32
-      std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(command.c_str(), "r"), _pclose);
-  #else
-      std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.c_str(), "r"), pclose);
-  #endif
+#ifdef _WIN32
+  std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(command.c_str(), "r"), _pclose);
+#else
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.c_str(), "r"), pclose);
+#endif
   RAY_CHECK(pipe) << "popen() failed for command: " + command;
   while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
     result += buffer.data();
