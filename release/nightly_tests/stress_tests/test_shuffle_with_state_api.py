cb2a47b02ba75169e7600236d3f8927187ffa5fd
rickyyx <ricky@anyscale.com>
2022-08-02 10:51:01 -0700

    wip
    Signed-off-by: rickyyx <ricky@anyscale.com>

--- release/nightly_tests/stress_tests/test_shuffle_with_state_api.py
+++ release/nightly_tests/stress_tests/test_shuffle_with_state_api.py
@@ -1,22 +1,31 @@
 import click
 import json
 import os
-import time
 
 import ray
-from ray._private.state_api_test_utils import (
-    StateAPICallSpec,
-    periodic_invoke_state_apis_with_actor,
-)
-import ray._private.test_utils as test_utils
-from ray._private.worker import RayContext
 
 from ray.experimental.state.api import (
     list_actors,
     list_nodes,
     list_objects,
     list_tasks,
+    summarize_actors,
+    summarize_objects,
+    summarize_tasks,
 )
+
+from ray._private.state_api_test_utils import (
+    StateAPICallSpec,
+    run_workload,
+    run_workload_with_state_api,
+    STATE_LIST_LIMIT,
+)
+
+from ray._private.worker import RayContext
+
+
+# Number of warm up iterations
+NUM_WARM_UP_ITER = 3
 
 
 # Run some long-running tasks
@@ -36,23 +45,17 @@ def run_shuffle(ctx: RayContext, num_partitions: int, partition_size: float):
     subprocess.check_call(commands)
 
 
-def run_workload(fn, **kwargs):
+def _warm_up_with_shuffle(num_partitions: int, partition_size: int):
 
-    monitor_actor = test_utils.monitor_memory_usage()
-
-    start = time.perf_counter()
-    fn(**kwargs)
-    end = time.perf_counter()
-    # Collect mem usage
-    ray.get(monitor_actor.stop_run.remote())
-    used_gb, usage = ray.get(monitor_actor.get_peak_memory_info.remote())
-
-    results = {
-        "duration": end - start,
-        "peak_memory": round(used_gb, 2),
-        "peak_process_memory": usage,
-    }
-    return results
+    ctx = ray.init(log_to_driver=False)
+    for _ in range(NUM_WARM_UP_ITER):
+        run_workload(
+            run_shuffle,
+            ctx=ctx,
+            num_partitions=num_partitions // 10,
+            partition_size=partition_size,
+        )
+    ray.shutdown()
 
 
 @click.command()
@@ -66,7 +69,7 @@ def run_workload(fn, **kwargs):
 @click.option("--num-partitions", type=int, default=100, help="number of partitions")
 @click.option("--partition-size", type=str, default="20e6", help="partition size")
 @click.option(
-    "--call-interval-s", type=int, default=2, help="interval of state api calls"
+    "--call-interval-s", type=int, default=3, help="interval of state api calls"
 )
 def test(
     smoke_test,
@@ -76,76 +79,32 @@ def test(
 ):
     # Stage 0: Warm-up with a shuffle workload
     if not smoke_test:
-        ctx = ray.init(log_to_driver=False)
-        run_workload(
-            run_shuffle,
-            ctx=ctx,
-            num_partitions=num_partitions // 10,
-            partition_size=partition_size,
+        _warm_up_with_shuffle(
+            num_partitions=num_partitions, partition_size=partition_size
         )
-        ray.shutdown()
     else:
         num_partitions = 100
 
-    # Stage 1: Run with state APIs
-    start_time = time.perf_counter()
-    ctx = ray.init()
-
+    # Set up state API calling methods
     def not_none(res):
         return res is not None
 
     apis = [
-        StateAPICallSpec(list_nodes, not_none),
-        StateAPICallSpec(list_objects, not_none),
-        StateAPICallSpec(list_tasks, not_none),
-        StateAPICallSpec(list_actors, not_none),
+        StateAPICallSpec(list_nodes, not_none, {"limit": STATE_LIST_LIMIT}),
+        StateAPICallSpec(list_objects, not_none, {"limit": STATE_LIST_LIMIT}),
+        StateAPICallSpec(list_tasks, not_none, {"limit": STATE_LIST_LIMIT}),
+        StateAPICallSpec(list_actors, not_none, {"limit": STATE_LIST_LIMIT}),
+        StateAPICallSpec(summarize_tasks, not_none),
+        StateAPICallSpec(summarize_actors, not_none),
+        StateAPICallSpec(summarize_objects, not_none),
     ]
 
-    api_caller = periodic_invoke_state_apis_with_actor(
-        apis=apis, call_interval_s=call_interval_s, print_interval_s=30
-    )
-
-    stats_with_state_apis = run_workload(
+    results = run_workload_with_state_api(
         run_shuffle,
-        ctx=ctx,
-        num_partitions=num_partitions,
-        partition_size=partition_size,
+        {"num_partitions": num_partitions, "partition_size": partition_size},
+        apis,
+        call_interval_s=call_interval_s,
     )
-
-    ray.get(api_caller.stop.remote())
-    print(json.dumps(ray.get(api_caller.get_stats.remote()), indent=2))
-    ray.shutdown()
-
-    # Stage 2: Run without API generator
-    ctx = ray.init()
-    stats = run_workload(
-        run_shuffle,
-        ctx=ctx,
-        num_partitions=num_partitions,
-        partition_size=partition_size,
-    )
-
-    end_time = time.perf_counter()
-
-    # Dumping results
-    results = {
-        "time": end_time - start_time,
-        "success": "1",
-        "perf_metrics": [
-            {
-                "perf_metric_name": "state_api_extra_latency_sec",
-                "perf_metric_value": stats_with_state_apis["duration"]
-                - stats["duration"],
-                "perf_metric_type": "LATENCY",
-            },
-            {
-                "perf_metric_name": "state_api_extra_mem",
-                "perf_metric_value": stats_with_state_apis["peak_memory"]
-                - stats["peak_memory"],
-                "perf_metric_type": "MEMORY",
-            },
-        ],
-    }
 
     if "TEST_OUTPUT_JSON" in os.environ:
         out_file = open(os.environ["TEST_OUTPUT_JSON"], "w")
