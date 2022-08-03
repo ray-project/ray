384f157124f3429df3441663f879c8345296941e
Simon Mo <simon.mo@hey.com>
2022-08-02 23:05:47 -0700

    [Serve][Nightly] Environment for Nightly K8s Tests (#27126)
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

---
+++ release/k8s_tests/run_gcs_ft_on_k8s.py
@@ -0,0 +1,10 @@
+from kubernetes import client, config
+
+# Configs can be set in Configuration class directly or using helper utility
+config.load_kube_config()
+
+v1 = client.CoreV1Api()
+print("Listing pods with their IPs:")
+ret = v1.list_pod_for_all_namespaces(watch=False)
+for i in ret.items:
+    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
