48549c9473f58166848ca2ab55e2132bb4c2c8bf
Alan Guo <aguo@anyscale.com>
2022-08-03 14:24:51 -0700

    Make it so pydantic is required before we launch dashboard api server (#27345)
    * Make it so pydantic is required before we launch dashboard api server
    
    Signed-off-by: Alan Guo <aguo@anyscale.com>
    Signed-off-by: Stefan van der Kleij <s.vanderkleij@viroteq.com>

--- python/setup.py
+++ python/setup.py
@@ -211,6 +211,8 @@ if setup_spec.type == SetupType.RAY:
             "fsspec",
         ],
         "default": [
+            # If adding dependencies necessary to launch the dashboard api server,
+            # please add it to dashboard/optional_deps.py as well.
             "aiohttp >= 3.7",
             "aiohttp_cors",
             "colorful",
