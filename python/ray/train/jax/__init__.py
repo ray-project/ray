c57a36d1ef09d31dd61d4c90b4790181405d4f55
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-08-04 19:28:38 +0000

    adding instruction

--- python/ray/train/jax/__init__.py
+++ python/ray/train/jax/__init__.py
@@ -2,8 +2,8 @@ try:
     import jax  # noqa: F401
 except ModuleNotFoundError:
     raise ModuleNotFoundError(
-        "Jax isn't installed. To install Jax on cpu or gpu, please checkout"
-        " `https://github.com/google/jax#installation` for the details."
+        "Jax isn't installed. To install Jax on cpu or gpu, please check"
+        " `https://github.com/google/jax#installation` for the instructions."
     )
 
 from ray.train.jax.jax_trainer import JaxTrainer
