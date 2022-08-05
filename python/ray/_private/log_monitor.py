667c1112c39421eb60da2777f6b4697ec2801118
Alex <alex@anyscale.com>
2022-08-04 17:54:55 -0700

    .

--- python/ray/_private/log_monitor.py
+++ python/ray/_private/log_monitor.py
@@ -74,7 +74,7 @@ class LogFileInfo:
         semantics.
         """
         open_inode = None
-        if self.file_handle is not None:
+        if self.file_handle is not None and not self.file_handle.closed:
             open_inode = os.fstat(self.file_handle.fileno()).st_ino
         new_inode = os.stat(self.filename).st_ino
         if open_inode != new_inode:
