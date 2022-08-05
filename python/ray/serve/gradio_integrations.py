419ba8efd38829287e58da7b4c263c88d1a9e69b
zcin <cindyzyx9@gmail.com>
2022-08-05 15:44:41 -0700

    [serve] Integrate and Document Bring-Your-Own Gradio Applications (#2… (#27560)

---
+++ python/ray/serve/gradio_integrations.py
@@ -0,0 +1,31 @@
+from ray import serve
+from ray.serve._private.http_util import ASGIHTTPSender
+from ray.util.annotations import PublicAPI
+
+import starlette
+
+try:
+    import gradio as gr
+except ModuleNotFoundError:
+    print("Gradio isn't installed. Run `pip install gradio` to install Gradio.")
+    raise
+
+
+@PublicAPI(stability="alpha")
+# __doc_gradio_ingress_begin__
+class GradioIngress:
+    """User-facing class that wraps a Gradio App in a Serve Deployment"""
+
+    def __init__(self, io: gr.Blocks):
+        self.app = gr.routes.App.create_app(io)
+
+    async def __call__(self, request: starlette.requests.Request):
+        sender = ASGIHTTPSender()
+        await self.app(request.scope, receive=request.receive, send=sender)
+        return sender.build_asgi_response()
+
+
+# __doc_gradio_ingress_end__
+
+
+GradioServer = serve.deployment(GradioIngress)
