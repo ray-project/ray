a2525aae072b4541dd3c1680b140baed13e6f0f6
Jun Gong <jungong@anyscale.com>
2022-08-05 17:08:27 -0700

    reduce the size of lin_ucb_recsim test.

--- rllib/examples/bandit/tune_lin_ucb_train_recsim_env.py
+++ rllib/examples/bandit/tune_lin_ucb_train_recsim_env.py
@@ -42,8 +42,8 @@ if __name__ == "__main__":
     }
 
     # Actual env timesteps per `train()` call will be
-    # 10 * min_sample_timesteps_per_iteration (100 by default) = 1,000
-    training_iterations = 5000
+    # 100 * min_sample_timesteps_per_iteration (100 by default) = 10,000
+    training_iterations = 100
 
     print("Running training for %s time steps" % training_iterations)
 
