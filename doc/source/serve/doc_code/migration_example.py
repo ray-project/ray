f795667729024ac7174e31e2de40af5eb35c3d1d
Simon Mo <simon.mo@hey.com>
2022-08-06 22:23:44 -0700

    [Serve] Use Async Handle for DAG Execution (#27411)
    Signed-off-by: Stefan van der Kleij <s.vanderkleij@viroteq.com>

--- doc/source/serve/doc_code/migration_example.py
+++ doc/source/serve/doc_code/migration_example.py
@@ -2,7 +2,7 @@ import ray
 from ray import serve
 from ray.serve.drivers import DAGDriver
 from ray.dag.input_node import InputNode
-from ray.serve.handle import RayServeLazySyncHandle
+from ray.serve.handle import RayServeDeploymentHandle
 from ray.serve.handle import RayServeSyncHandle
 
 import requests
@@ -204,12 +204,14 @@ class Model:
 
 @serve.deployment
 class Preprocess:
-    def __init__(self, model_handle: RayServeLazySyncHandle):
+    def __init__(self, model_handle: RayServeDeploymentHandle):
         self.model_handle = model_handle
 
     async def __call__(self, input):
         # do some preprocessing works for your inputs
-        return await self.model_handle.forward.remote(input)
+        ref = await self.model_handle.forward.remote(input)
+        result = await ref
+        return result
 
 
 handle = serve.run(Preprocess.bind(Model.bind()))
