47a4df49ef6c7cf931b229714b803ca117d2ccbf
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-08-08 23:56:13 +0000

    remove the environment

--- python/ray/train/examples/jax_mnist_example_tpu.py
+++ python/ray/train/examples/jax_mnist_example_tpu.py
@@ -213,8 +213,9 @@ if __name__ == "__main__":
 
     import ray
 
-    ray.init('auto', runtime_env = {"env_vars": {"RAY_TPU_DEV": "1"},
-                                    "LD_LIBRARY_PATH": "/usr/local/lib"})
+    ray.init('auto')
+    # ray.init('auto', runtime_env = {"env_vars": {"RAY_TPU_DEV": "1"},
+    #                                 "LD_LIBRARY_PATH": "/usr/local/lib"})
     train_mnist(
         num_workers=args.num_workers,
         use_gpu=args.use_tpu,
