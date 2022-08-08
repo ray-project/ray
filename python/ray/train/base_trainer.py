17a865349c7b10d753f3f64361f686a3ec5dc3ee
Cheng Su <scnju13@gmail.com>
2022-08-08 16:01:44 -0700

    Replace to_torch with iter_torch_batches (#27656)
    Signed-off-by: Cheng Su <scnju13@gmail.com>

--- python/ray/train/base_trainer.py
+++ python/ray/train/base_trainer.py
@@ -84,16 +84,17 @@ class BaseTrainer(abc.ABC):
                 # preprocessed by self.preprocessor
                 dataset = self.datasets["train"]
 
-                torch_ds = dataset.to_torch(label_column="y")
+                torch_ds = dataset.iter_torch_batches(dtypes=torch.float)
                 loss_fn = torch.nn.MSELoss()
 
                 for epoch_idx in range(10):
                     loss = 0
                     num_batches = 0
-                    for X, y in iter(torch_ds):
+                    for batch in torch_ds:
+                        X, y = torch.unsqueeze(batch["x"], 1), batch["y"]
                         # Compute prediction error
                         pred = self.model(X)
-                        batch_loss = loss_fn(pred, y.float())
+                        batch_loss = loss_fn(pred, y)
 
                         # Backpropagation
                         self.optimizer.zero_grad()
