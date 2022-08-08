1074231565594ccc59d50083a5f787a4c80e4af9
simon-mo <simon.mo@hey.com>
2022-08-08 10:36:05 -0700

    Revert "Revert "[serve][xlang]Support deploying Python deployment from Java. (#26877)" (#27626)"
    This reverts commit 8c190e2d0962dc2ac83ebd60c99cad1cf0998d12.

--- python/ray/serve/_private/router.py
+++ python/ray/serve/_private/router.py
@@ -45,6 +45,7 @@ class Query:
     args: List[Any]
     kwargs: Dict[Any, Any]
     metadata: RequestMetadata
+    return_num: int = 2
 
     async def resolve_async_tasks(self):
         """Find all unresolved asyncio.Task and gather them all at once."""
