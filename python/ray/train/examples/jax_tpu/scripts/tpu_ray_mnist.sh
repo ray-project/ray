f639a8eb236e0d006ee19e2466a2f37c23f246fa
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-08-09 16:07:10 +0000

    upload the bash file

--- python/ray/train/examples/jax_tpu/scripts/tpu_ray_mnist.sh
+++ python/ray/train/examples/jax_tpu/scripts/tpu_ray_mnist.sh
@@ -1,3 +1,3 @@
-
+gcloud alpha compute tpus tpu-vm scp --recurse _tpu_ray_mnist.sh jax-trainer-mnist-tpu-pod: --zone=us-central1-a --worker all
 # this one is not run SPMD on TPUs!!
-gcloud alpha compute tpus tpu-vm ssh jax-trainer-mnist-tpu-pod --zone=us-central1-a --command "bash ray_jax_trainer_setup.sh" --worker 0
+gcloud alpha compute tpus tpu-vm ssh jax-trainer-mnist-tpu-pod --zone=us-central1-a --command "bash _tpu_ray_mnist.sh" --worker 0
