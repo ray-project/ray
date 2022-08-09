9d5b0dd0e36dfe16b14fdf886142ff82e93d4b7d
Eric Liang <ekhliang@gmail.com>
2022-08-08 21:44:57 -0700

    new figure
    Signed-off-by: Eric Liang <ekhliang@gmail.com>

--- doc/source/custom_directives.py
+++ doc/source/custom_directives.py
@@ -180,12 +180,6 @@ EXTERNAL_MARKDOWN_FILES = [
         "README.md",
         "ray-more-libs/lightgbm-ray.md",
     ),
-    (
-        "ray-project/ray_lightning",
-        "6aed848f757a03c03166c1a9bddfeea5153e7b90",
-        "README.md",
-        "ray-more-libs/ray-lightning.md",
-    ),
 ]
 
 
