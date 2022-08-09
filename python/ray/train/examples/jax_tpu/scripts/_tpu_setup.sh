d5ea151a43616eef196924279d01df6c7e6ec91d
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-08-09 22:37:39 +0000

    adding the ""

--- python/ray/train/examples/jax_tpu/scripts/_tpu_setup.sh
+++ python/ray/train/examples/jax_tpu/scripts/_tpu_setup.sh
@@ -12,6 +12,3 @@ pip3 install tensorflow --upgrade
 pip3 install scipy --upgrade
 pip3 install pandas --upgrade
 pip3 install flax
-
-cat <<EOF > /var/www/html/index.html
-<html><body><p>Linux startup script added directly.</p></body></html>
