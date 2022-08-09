994d2af85059b23b1aae95e02b3d3e25bc373937
Alex <alex@anyscale.com>
2022-08-08 22:00:28 -0700

    .

--- python/ray/autoscaler/_private/node_provider_availability_tracker.py
+++ python/ray/autoscaler/_private/node_provider_availability_tracker.py
@@ -114,7 +114,7 @@ class NodeProviderAvailabilityTracker:
         timer: Callable[[], float] = time.time,
         ttl: float = AUTOSCALER_NODE_AVAILABILITY_MAX_STALENESS_S,
     ):
-        """ A cache that tracks the availability of nodes and throw away
+        """A cache that tracks the availability of nodes and throw away
         entries which have grown too stale.
 
         Args:
@@ -176,7 +176,7 @@ class NodeProviderAvailabilityTracker:
           node_type: The node type.
           timestamp: The timestamp that this information is accurate as of.
           node_launch_exception: Details about why the node launch failed. If
-            empty, the node type will be considered available. """
+            empty, the node type will be considered available."""
         with self.lock:
             self._unsafe_update_node_availability(
                 node_type, timestamp, node_launch_exception
