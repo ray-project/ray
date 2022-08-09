f59c3194e089ff6ef3304cf1a11e4c56ff4bf067
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-08-09 17:46:59 +0000

    nit

--- python/ray/train/examples/jax_tpu/jax_mnist_example_tpu.py
+++ python/ray/train/examples/jax_tpu/jax_mnist_example_tpu.py
@@ -213,9 +213,8 @@ if __name__ == "__main__":
 
     import ray
 
-    ray.init('auto')
-    # ray.init('auto', runtime_env = {"env_vars": {"RAY_TPU_DEV": "1"},
-    #                                 "LD_LIBRARY_PATH": "/usr/local/lib"})
+    ray.init(address=args.address)
+
     train_mnist(
         num_workers=args.num_workers,
         use_gpu=args.use_tpu,
