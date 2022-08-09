89c2a54d38ca2015f6af05c3cb082a2673f087a3
Sven Mika <svenmika1977@gmail.com>
2022-08-09 16:53:00 +0200

    [RLlib] CRR framework torch by default. (#27161)
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- rllib/algorithms/crr/crr.py
+++ rllib/algorithms/crr/crr.py
@@ -53,11 +53,13 @@ class CRRConfig(AlgorithmConfig):
         self.actor_lr = 3e-4
         self.tau = 5e-3
 
-        # overriding the trainer config default
+        # Overriding the trainer config default:
+        # Only PyTorch supported thus far. Make this the default framework.
+        self.framework_str = "torch"
         # If data ingestion/sample_time is slow, increase this
         self.num_workers = 4
         self.offline_sampling = True
-        self.min_iter_time_s = 10.0
+        self.min_time_s_per_iteration = 10.0
 
     def training(
         self,
