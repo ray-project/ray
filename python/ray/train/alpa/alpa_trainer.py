5d78bdfe5dd6b5b31c4af47d6a7dfb497ff4bf55
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-08-10 18:51:13 +0000

    adding the scaling configure check

--- python/ray/train/alpa/alpa_trainer.py
+++ python/ray/train/alpa/alpa_trainer.py
@@ -1,5 +1,6 @@
 from typing import Optional, Dict, Union, Callable, TYPE_CHECKING
 
+from ray.air._internal.config import ensure_only_allowed_dataclass_keys_updated
 
 from ray.air.checkpoint import Checkpoint
 from ray.air.config import ScalingConfig, RunConfig
@@ -22,6 +23,7 @@ from ray.train.constants import (
 from ray.air.config import DatasetConfig
 
 from ray.train.alpa.utils import AlpaManager
+
 
 logger = logging.getLogger(__name__)
 
@@ -67,6 +69,9 @@ class AlpaTrainer(BaseTrainer):
         self._train_loop = train_loop_per_worker
         self._train_loop_config = train_loop_config
 
+        # validate scaling config
+        scaling_config = self._validate_scaling_config(scaling_config)
+
         super(AlpaTrainer, self).__init__(
             scaling_config=scaling_config,
             run_config=run_config,
@@ -77,6 +82,7 @@ class AlpaTrainer(BaseTrainer):
 
     def training_loop(self) -> None:
         """Training loop for AlpaTrainer."""
+
         # intialize alpa cluster
         self.alpa_manager.init_global_cluster()
 
@@ -84,3 +90,36 @@ class AlpaTrainer(BaseTrainer):
             self._train_loop(self._train_loop_config)
         else:
             self._train_loop()
+
+    @classmethod
+    def _validate_scaling_config(cls, scaling_config: ScalingConfig) -> ScalingConfig:
+        """Return scaling config dataclass after validating updated keys."""
+        ensure_only_allowed_dataclass_keys_updated(
+            dataclass=scaling_config,
+            allowed_keys=cls._scaling_config_allowed_keys,
+        )
+
+        use_gpu = scaling_config.use_gpu
+
+        # cpu or tpu parallelism are not supported yet in alpa
+        if not use_gpu:
+            raise ValueError(
+                "cpu or tpu parallelism is not supported yet in alpa "
+                "Please use distributed GPU training instead. "
+            )
+
+        # change the default value of `placment_strategy` to `SPREAD`.
+        # using `SPREAD`: the alpatrainer will spread the workers across different nodes
+        # between the nodes, the connections is to be established by NCCL;
+        # within the nodes, alpa can already enable communicates automatically.
+        # so, no end to use alpa manager to make the connections.
+        # NOT `STRICT_SPREAD`: if the users use the trainer on one of GPU nodes instead
+        # of on the extra cpu nodes, then `STRICT_SPREAD` will hang on for the resources
+        if "PACK" in scaling_config.placement_strategy:
+            scaling_config.placement_strategy = "SPREAD"
+            logger.info(
+                "In AlpaTrainer, the `placement_stategy` need to be `SPREAD`."
+                " Placement strategy is now changed to `SPREAD`"
+            )
+
+        return scaling_config
