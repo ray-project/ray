903ac97b164e38b4986d8681d2c30b876bde4662
JiahaoYao <jiahaoyao.math@gmail.com>
2022-08-10 10:05:13 -0700

    move the jax  example to #27749

--- python/ray/train/examples/jax_tpu/scripts/tpu_launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-folder=$(dirname "$0")
-
-gcloud alpha compute tpus tpu-vm create jax-trainer-mnist-tpu-pod \
-  --zone=us-central1-a \
-  --accelerator-type=v2-32 \
-  --version=tpu-vm-tf-2.8.0 \
-  --metadata-from-file=startup-script="$folder/_tpu_setup.sh"
-  
--- python/ray/train/examples/jax_tpu/scripts/tpu_shutdown.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gcloud alpha compute tpus tpu-vm delete jax-trainer-mnist-tpu-pod \
-  --zone=us-central1-a 
