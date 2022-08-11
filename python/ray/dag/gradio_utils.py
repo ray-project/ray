d29ce9f77cbfbdd55f3f09d16ec274fac134e2c1
Cindy Zhang <cindyzyx9@gmail.com>
2022-08-11 09:24:47 -0700

    wip
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- python/ray/dag/gradio_utils.py
+++ python/ray/dag/gradio_utils.py
@@ -1,8 +1,8 @@
-from PIL import ImageFile
+# from PIL import ImageFile
 
 
 def type_to_string(_type):
-    if isinstance(_type, type) and issubclass(_type, ImageFile.ImageFile):
-        return str(ImageFile.ImageFile)
+    # if isinstance(_type, type) and issubclass(_type, ImageFile.ImageFile):
+    #     return str(ImageFile.ImageFile)
 
     return str(_type)
