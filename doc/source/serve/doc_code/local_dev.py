d717a4e9bba3eba467795891d7b1154048d9d665
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2022-08-11 11:50:25 -0700

    Lint

--- doc/source/serve/doc_code/local_dev.py
+++ doc/source/serve/doc_code/local_dev.py
@@ -5,23 +5,27 @@ from ray import serve
 from ray.dag.input_node import InputNode
 import starlette.requests
 
+
 @serve.deployment
 class Doubler:
     def double(self, s: str):
         return s + " " + s
 
+
 @serve.deployment
 class HelloDeployment:
     def __init__(self, doubler):
         self.doubler = doubler
+
     async def say_hello_twice(self, name: str):
         ref = await self.doubler.double.remote(f"Hello, {name}!")
         return await ref
+
     async def __call__(self, request: starlette.requests.Request):
         return await self.say_hello_twice(request.query_params["name"])
 
-graph = HelloDeployment.bind(Doubler.bind())
 
+graph = HelloDeployment.bind(Doubler.bind())
 # __local_dev_end__
 
 # __local_dev_handle_start__
