4d67d4caf2491aa921102dd55fb0a4f7dc250bd4
shrekris-anyscale <92341594+shrekris-anyscale@users.noreply.github.com>
2022-08-12 09:05:31 -0700

    [Serve] [Docs] Update the "Monitoring Ray Serve" Page (#27777)
    The "Monitoring Ray Serve" page explains how to inspect your Ray Serve applications. This change updates the page to remove outdated metrics that Serve no longer exposes and to upgrade code samples to use 2.0 APIs. It also improves the content's readability and organization.
    
    Link to updated "Monitoring Ray Serve" page: https://ray--27777.org.readthedocs.build/en/27777/serve/monitoring.html
    
    Signed-off-by: Stefan van der Kleij <s.vanderkleij@viroteq.com>

--- doc/source/serve/doc_code/monitoring/monitoring.py
+++ doc/source/serve/doc_code/monitoring/monitoring.py
@@ -1,10 +1,10 @@
-# __start_monitoring__
+# flake8: noqa
+
+# __start__
 # File name: monitoring.py
 
 from ray import serve
-
 import logging
-import requests
 
 logger = logging.getLogger("ray.serve")
 
@@ -17,8 +17,10 @@ class SayHello:
 
 
 say_hello = SayHello.bind()
+# __end__
 
-serve.run(say_hello)
+# serve.run(say_hello)
 
-requests.get("http://localhost:8000/")
-# __end_monitoring__
+# import requests
+# response = requests.get("http://localhost:8000/")
+# assert response.text == "hi"
