7e72898d8dd40257f72dfc98beaf9445d53ebade
Philipp Moritz <pcmoritz@gmail.com>
2022-08-13 15:08:40 -0700

    update
    Signed-off-by: Philipp Moritz <pcmoritz@gmail.com>

--- python/ray/data/read_api.py
+++ python/ray/data/read_api.py
@@ -565,7 +565,7 @@ def read_csv(
         >>> # Use different delimiter. The partition_filter=None is needed here
         >>> # because by default read_csv only reads .csv files.
         >>> from pyarrow import csv
-        >>> parse_options = csv.ParseOptions(delimiter="\t")
+        >>> parse_options = csv.ParseOptions(delimiter="\\t")
         >>> ray.data.read_csv(
         ...     "example://iris.tsv", parse_options=parse_options, partition_filter=None)
 
