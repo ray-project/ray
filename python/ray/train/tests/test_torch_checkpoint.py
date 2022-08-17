a4472e24d5516a1726e97bde732739db47b1c602
Balaji Veeramani <bveeramani@berkeley.edu>
2022-08-17 16:15:22 -0700

    Add `TorchCheckpoint.from_state_dict`

---
+++ python/ray/train/tests/test_torch_checkpoint.py
@@ -0,0 +1,11 @@
+import torch
+
+from ray.train.torch import TorchCheckpoint
+
+
+def test_from_state_dict():
+    model = torch.nn.Linear(1, 1)
+    expected_state_dict = model.state_dict()
+    checkpoint = TorchCheckpoint.from_state_dict(expected_state_dict)
+    actual_state_dict = checkpoint.get_model(torch.nn.Linear(1, 1)).state_dict()
+    assert actual_state_dict == expected_state_dict
