afbe8e6347016c54a23d329c92a57348b1d111a2
Edward Oakes <ed.nmi.oakes@gmail.com>
2022-08-17 17:11:46 -0500

    fix docstring
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/api.py
+++ python/ray/serve/api.py
@@ -520,10 +520,11 @@ def build(target: Union[ClassNode, FunctionNode]) -> Application:
     Serve application consisting of one or more deployments. This is intended
     to be used for production scenarios and deployed via the Serve REST API or
     CLI, so there are some restrictions placed on the deployments:
-        1) All of the deployments must be importable. That is, they cannot be
-           defined in __main__ or inline defined. The deployments will be
-           imported in production using the same import path they were here.
-        2) All arguments bound to the deployment must be JSON-serializable.
+
+    1) All of the deployments must be importable. That is, they cannot be
+       defined in __main__ or inline defined. The deployments will be
+       imported in production using the same import path they were here.
+    2) All arguments bound to the deployment must be JSON-serializable.
 
     The returned Application object can be exported to a dictionary or YAML
     config.
