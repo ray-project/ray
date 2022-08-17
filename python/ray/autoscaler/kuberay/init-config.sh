9b4cf468f1e49d468fbb43eddfd03a7387b10525
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2022-08-17 12:49:01 -0700

    fix
    Signed-off-by: Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>

--- python/ray/autoscaler/kuberay/init-config.sh
+++ python/ray/autoscaler/kuberay/init-config.sh
@@ -2,7 +2,7 @@
 
 # Clone pinned Kuberay commit to temporary directory, copy the CRD definitions
 # into the autoscaler folder.
-KUBERAY_COMMIT="v2.3.0-rc.2"
+KUBERAY_COMMIT="v0.3.0-rc.2"
 OPERATOR_TAG="v0.3.0-rc.2"
 
 # Requires Kustomize (dependency to be removed after KubeRay 1.3.0 cut)
@@ -17,7 +17,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DIR=$(mktemp -d -t "kuberay-XXXXXX")
 
 pushd "$DIR" || exit
-    git clone https://github.com/ray-project/kuberay/ -b "$KUBERAY_COMMIT"
+    git clone https://github.com/ray-project/kuberay/ --branch "$KUBERAY_COMMIT" --depth 1
     pushd kuberay/ray-operator/config/default || exit
         kustomize edit set image kuberay/operator=kuberay/operator:"$OPERATOR_TAG"
     popd || exit
