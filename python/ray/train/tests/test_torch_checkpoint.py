07ceae5c355c15fd1b1470f423ba71ad35ccd9c8
Justin Yu <justinvyu@berkeley.edu>
2022-08-18 09:18:52 -0700

    Cast checkpoint as TorchCheckpoint before get_model
    Signed-off-by: Justin Yu <justinvyu@berkeley.edu>

--- python/ray/train/tests/test_torch_checkpoint.py
+++ python/ray/train/tests/test_torch_checkpoint.py
@@ -9,6 +9,7 @@ import torch.nn as nn
 def simple_linear_model():
     return nn.Linear(1, 1)
 
+
 def assert_equal_torch_models(model1, model2):
     # Check equality by comparing their `state_dict`
     model1_state = model1.state_dict()
@@ -18,12 +19,14 @@ def assert_equal_torch_models(model1, model2):
         assert key in model2_state
         assert torch.equal(model1_state[key], model2_state[key])
 
+
 def test_torch_checkpoint(simple_linear_model):
     checkpoint = TorchCheckpoint.from_model(simple_linear_model)
     path = checkpoint.to_directory()
 
-    loaded_checkpoint = Checkpoint.from_directory(path)
-    model = loaded_checkpoint.get_model()
+    checkpoint = Checkpoint.from_directory(path)
+    checkpoint = TorchCheckpoint.from_checkpoint(checkpoint)
+    model = checkpoint.get_model()
 
     assert_equal_torch_models(model, simple_linear_model)
 
