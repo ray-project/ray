870d07ca2417626c7593182418b0045135512033
Cindy Zhang <cindyzyx9@gmail.com>
2022-08-19 15:09:49 -0700

    add more test cases for the dfs
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- python/ray/serve/tests/test_gradio_visualization.py
+++ python/ray/serve/tests/test_gradio_visualization.py
@@ -46,6 +46,42 @@ def graph2():
     yield input_node, f_node, dag
 
 
+@pytest.fixture
+def graph3():
+    @serve.deployment
+    class Base:
+        def __init__(self, weight):
+            self.weight = weight
+
+        def eval(self, input1, input2) -> int:
+            return (input1 + input2) * self.weight
+
+    @serve.deployment
+    class Model:
+        def __init__(self, weight):
+            self.weight = weight
+
+        def forward(self, input) -> int:
+            return input * self.weight
+
+    @serve.deployment
+    def combine(x, y, z):
+        return x + y + z
+
+    with InputNode() as user_input:
+        input_nodes = (user_input[0], user_input[1], user_input[2])
+
+        b = Base.bind(1)
+        m1 = Model.bind(1)
+        m2 = Model.bind(1)
+        l_output = b.eval.bind(input_nodes[0], input_nodes[1])
+        m1_output = m1.forward.bind(l_output)
+        m2_output = m2.forward.bind(input_nodes[2])
+        dag = combine.bind(m1_output, m2_output, l_output)
+
+    yield input_nodes, b, m1, m2, l_output, m1_output, m2_output, dag
+
+
 @pytest.mark.asyncio
 async def test_execute_cached_object_ref(graph1):
     """Tests DAGNode.get_object_ref_from_last_execute() correctly returns object refs
@@ -57,6 +93,44 @@ async def test_execute_cached_object_ref(graph1):
     cache = await dag.get_object_refs_from_last_execute()
     assert await cache[f_node.get_stable_uuid()] == 1
     assert await cache[dag.get_stable_uuid()] == 2
+
+
+def test_graph_dfs_for_depths1(graph2):
+    """Tests that GraphVisualizer._fetch_depths, when passed into
+    DAGNode.apply_recursive, correctly retrieves the depths of each node.
+    """
+
+    (input_node, f_node, dag) = graph2
+
+    visualizer = GraphVisualizer()
+    depths = defaultdict(lambda: 0)
+    dag.apply_recursive(lambda node: visualizer._fetch_depths(node, depths))
+
+    assert (
+        depths[input_node.get_stable_uuid()] == 1
+        and depths[f_node.get_stable_uuid()] == 2
+        and depths[dag.get_stable_uuid()] == 3
+    )
+
+
+def test_graph_dfs_for_depths2(graph3):
+    """Tests that GraphVisualizer._fetch_depths, when passed into
+    DAGNode.apply_recursive, correctly retrieves the depths of each node.
+    """
+
+    (input_nodes, _, _, _, l_output, m1_output, m2_output, dag) = graph3
+
+    visualizer = GraphVisualizer()
+    depths = defaultdict(lambda: 0)
+    dag.apply_recursive(lambda node: visualizer._fetch_depths(node, depths))
+
+    assert (depths[input_node.get_stable_uuid()] == 1 for input_node in input_nodes)
+    assert (
+        depths[l_output.get_stable_uuid()] == 2
+        and depths[m2_output.get_stable_uuid()] == 2
+    )
+    assert depths[m1_output.get_stable_uuid()] == 3
+    assert depths[dag.get_stable_uuid()] == 4
 
 
 @pytest.mark.asyncio
@@ -117,7 +191,9 @@ async def test_gradio_visualization_e2e(graph1):
                     "fn_index": fn_index,
                 },
             ) as resp:
-                return (await resp.json())["data"]
+                ret = (await resp.json())["data"]
+                print(f"[fn_index={fn_index}] Fetched value: {ret}")
+                return ret
 
         values = await asyncio.gather(
             fetch([1, 2], 0),  # sends request to dag with input (1,2)
@@ -126,24 +202,6 @@ async def test_gradio_visualization_e2e(graph1):
         )
 
     assert [] in values and [1] in values and [2] in values
-
-
-def test_fetch_depths(graph2):
-    """Tests that GraphVisualizer._fetch_depths, when passed into
-    DAGNode.apply_recursive, correctly retrieves the depths of each node.
-    """
-
-    (input_node, f_node, dag) = graph2
-
-    visualizer = GraphVisualizer()
-    depths = defaultdict(lambda: 0)
-    dag.apply_recursive(lambda node: visualizer._fetch_depths(node, depths))
-
-    assert (
-        depths[input_node.get_stable_uuid()] == 1
-        and depths[f_node.get_stable_uuid()] == 2
-        and depths[dag.get_stable_uuid()] == 3
-    )
 
 
 if __name__ == "__main__":
