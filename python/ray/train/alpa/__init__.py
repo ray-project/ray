e428c7865a6bc8b48586bc92b0aa78711f7d1be3
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-08-21 21:01:10 +0000

    adding the lint avoid

--- python/ray/train/alpa/__init__.py
+++ python/ray/train/alpa/__init__.py
@@ -1,10 +1,13 @@
-try:
-    import alpa  # noqa: F401
-except ModuleNotFoundError:
-    raise ModuleNotFoundError(
-        "Alpa isn't installed. To install Alpa, Refer to the document:"
-        "https://alpa-projects.github.io/install.html#install-from-source"
-    )
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    try:
+        import alpa  # noqa: F401
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError(
+            "Alpa isn't installed. To install Alpa, Refer to the document:"
+            "https://alpa-projects.github.io/install.html#install-from-source"
+        )
 
 from ray.train.alpa.alpa_trainer import AlpaTrainer
 
