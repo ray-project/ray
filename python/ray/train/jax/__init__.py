86e7e64b7449b5590251bbf6a74fea0e0283d3d3
Jimmy Yao <jiahaoyao.math@gmail.com>
2022-08-30 10:16:28 -0700

    Update python/ray/train/jax/__init__.py
    Co-authored-by: Amog Kamsetty <amogkam@users.noreply.github.com>

--- python/ray/train/jax/__init__.py
+++ python/ray/train/jax/__init__.py
@@ -3,11 +3,11 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     try:
         import jax  # noqa: F401
-    except ModuleNotFoundError:
+    except ModuleNotFoundError as e:
         raise ModuleNotFoundError(
             "Jax isn't installed. To install Jax, please check"
             " `https://github.com/google/jax#installation` for the instructions."
-        )
+        ) from e
 
 from ray.train.jax.jax_trainer import JaxTrainer
 from ray.train.jax.config import JaxConfig
