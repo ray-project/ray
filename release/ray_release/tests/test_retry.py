cc605323a605a9c601178c57a4cc9ff9de0cf0ce
Clarence Ng <clarence.wyng@gmail.com>
2022-09-01 11:25:27 -0700

    format
    Signed-off-by: Clarence Ng <clarence.wyng@gmail.com>

--- release/ray_release/tests/test_retry.py
+++ release/ray_release/tests/test_retry.py
@@ -1,34 +1,38 @@
-
-
 import sys
 import pytest
 from retry import retry
 
 
 retry_once_counter = 0
+
+
 @retry((Exception), delay=0, max_delay=20, backoff=2, jitter=2, tries=2)
 def retry_once():
-  global retry_once_counter
-  retry_once_counter += 1
-  raise Exception()
+    global retry_once_counter
+    retry_once_counter += 1
+    raise Exception()
+
 
 no_retry_counter = 0
+
+
 @retry((Exception), delay=0, max_delay=20, backoff=2, jitter=2, tries=1)
 def no_retry():
-  global no_retry_counter
-  no_retry_counter += 1
-  raise ValueError()
+    global no_retry_counter
+    no_retry_counter += 1
+    raise ValueError()
 
 
 def test_retry():
-  with pytest.raises(Exception):
-    retry_once()
-  assert retry_once_counter == 2
+    with pytest.raises(Exception):
+        retry_once()
+    assert retry_once_counter == 2
+
 
 def test_no_retry():
-  with pytest.raises(ValueError):
-    no_retry()
-  assert no_retry_counter == 1
+    with pytest.raises(ValueError):
+        no_retry()
+    assert no_retry_counter == 1
 
 
 if __name__ == "__main__":
