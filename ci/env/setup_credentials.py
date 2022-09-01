a9a53fe427bbee468f2351bb0d00374c26fc4104
Kai Fricke <kai@anyscale.com>
2022-08-31 18:12:45 -0700

    [tune] Run SigOpt tests in CI
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- ci/env/setup_credentials.py
+++ ci/env/setup_credentials.py
@@ -3,8 +3,10 @@ This script is used to set up credentials for some services in the
 CI environment. For instance, it can fetch WandB API tokens and write
 the WandB configuration file so test scripts can use the service.
 """
+import json
 import os
 import sys
+from pathlib import Path
 
 import boto3
 
@@ -13,6 +15,9 @@ AWS_WANDB_SECRET_ARN = (
 )
 AWS_COMET_SECRET_ARN = (
     "arn:aws:secretsmanager:us-west-2:029272617770:secret:oss-ci/comet-ml-token-vw81C3"
+)
+AWS_SIGOPT_SECRET_ARN = (
+    "arn:aws:secretsmanager:us-west-2:029272617770:secret:oss-ci/sigopt-key-qEqYrk"
 )
 
 
@@ -28,9 +33,26 @@ def get_and_write_comet_ml_api_key(client):
         fp.write(f"[comet]\napi_key={api_key}\n")
 
 
+def get_and_write_sigopt_api_key(client):
+    api_key = client.get_secret_value(SecretId=AWS_SIGOPT_SECRET_ARN)["SecretString"]
+
+    sigopt_config_file = Path("~/.sigopt/client/config.json").expanduser()
+    sigopt_config_file.parent.mkdir(exist_ok=True)
+    with open(sigopt_config_file, "wt") as f:
+        json.dump(
+            {
+                "api_token": api_key,
+                "code_tracking_enabled": False,
+                "log_collection_enabled": False,
+            },
+            f,
+        )
+
+
 SERVICES = {
     "wandb": get_and_write_wandb_api_key,
     "comet_ml": get_and_write_comet_ml_api_key,
+    "sigopt": get_and_write_sigopt_api_key,
 }
 
 
