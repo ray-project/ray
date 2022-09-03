1a45770633c28497528b1d1c023bf6f32901bef5
Antoni Baum <antoni.baum@protonmail.com>
2022-09-03 11:21:03 +0200

    Update test_huggingface_trainer_steps.py

--- python/ray/train/tests/test_huggingface_trainer_steps.py
+++ python/ray/train/tests/test_huggingface_trainer_steps.py
@@ -111,13 +111,7 @@ def test_e2e_steps(ray_start_4_cpus, save_steps, logging_steps):
     result2 = trainer2.fit()
 
     assert result2.metrics["epoch"] == epochs + 1
-    if logging_steps % 2 != 0:
-        assert (
-            result2.metrics["training_iteration"]
-            == math.ceil(1 / logging_steps) + 1
-        )
-    else:
-        assert result2.metrics["training_iteration"] == math.ceil(1 / logging_steps)
+    assert result2.metrics["training_iteration"] == math.ceil(1 / logging_steps)
     assert result2.checkpoint
     assert "eval_loss" in result2.metrics
 
