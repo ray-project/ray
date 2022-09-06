213ef06a89bcc416d19ef996dad9527e21c9f3ca
Valentin Boettcher <hiro@protagon.space>
2022-09-06 16:51:33 +0200

    correct typo in error message (#28298)
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- src/ray/gcs/gcs_client/global_state_accessor.cc
+++ src/ray/gcs/gcs_client/global_state_accessor.cc
@@ -355,7 +355,7 @@ ray::Status GlobalStateAccessor::GetNodeToConnectForDriver(
 
       if (relevant_client_index < 0 && head_node_client_index >= 0) {
         RAY_LOG(INFO) << "This node has an IP address of " << node_ip_address
-                      << ", while we can not found the matched Raylet address. "
+                      << ", while we can not find the matched Raylet address. "
                       << "This maybe come from when you connect the Ray cluster "
                       << "with a different IP address or connect a container.";
         relevant_client_index = head_node_client_index;
