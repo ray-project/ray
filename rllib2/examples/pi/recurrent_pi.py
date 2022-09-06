34bd89c228369fa447b6d7ddfb3fa584fd2aea76
Steven Morad <smorad@anyscale.com>
2022-09-06 14:00:22 +0100

    Add recurrent pi example

--- rllib2/examples/pi/recurrent_pi.py
+++ rllib2/examples/pi/recurrent_pi.py
@@ -4,12 +4,28 @@
 ###################################################################
 ########### Recurrent Policies
 ###################################################################
+from rllib2.models.torch.pi import Pi, PiConfig
+from rllib2.models.torch.encoder import RNNEncoder
+
+rnn_config = ModelConfig()
+rnn_config.rnn_cell = "lstm"
+rnn_encoder = RNNEncoder(rnn_config)
 
 config = PiConfig(
     observation_space=Box(low=-1, high=1, shape=(10,)),
     action_space=Discrete(2),
-    is_rnn=True,
-    # free_log_std=True,
+    encoder=rnn_encoder,
 )
+# TODO: Do we want to pass pi_config to PPOConfig
+# or pi directly?
 pi = Pi(config)
 print(pi)
+
+if __name__ == "__main__":
+
+    config = PPOConfig()(
+        pi=pi,
+    )
+
+    config.build()
+    config.train()
