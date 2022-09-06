c1d50a11384bf5e2f53f2d3ed512ceacb709032e
Steven Morad <smorad@anyscale.com>
2022-09-06 14:30:26 +0100

    Add transformer pi example

--- rllib2/examples/pi/transformer_pi.py
+++ rllib2/examples/pi/transformer_pi.py
@@ -1,15 +1,29 @@
-
-
-
 ###################################################################
 ########### Transformer policy
 ###################################################################
 
+from rllib2.models.torch.encoder import TransformerEncoder
+from rllib2.models.torch.pi import Pi, PiConfig
+
+xformer_config = ModelConfig()
+xformer_config.rnn_cell = "lstm"
+encoder = TransformerEncoder(rnn_config)
+
 config = PiConfig(
-    observation_space=Box(low=-1, high=1, shape=(84, 84, 3)),
+    observation_space=Box(low=-1, high=1, shape=(10,)),
     action_space=Discrete(2),
-    is_transformer=True,
-    # free_log_std=True,
+    encoder=encoder,
 )
 pi = Pi(config)
 print(pi)
+
+if __name__ == "__main__":
+
+    config = PPOConfig()(
+        # TODO: Do we want to pass pi_config to PPOConfig
+        # or pi directly?
+        pi=pi,
+    )
+
+    config.build()
+    config.train()
