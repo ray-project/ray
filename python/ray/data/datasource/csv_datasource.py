d17a64161e65b5f72dd7e446da1c12b3bff162ce
Kai Fricke <kai@anyscale.com>
2022-09-07 11:08:08 +0100

    [data] Re-init CSV invalid row handler to solve serialization issue
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- python/ray/data/datasource/csv_datasource.py
+++ python/ray/data/datasource/csv_datasource.py
@@ -35,6 +35,10 @@ class CSVDatasource(FileBasedDatasource):
         read_options = reader_args.pop(
             "read_options", csv.ReadOptions(use_threads=False)
         )
+        parse_options = reader_args.pop("parse_options", csv.ParseOptions())
+        # Re-init invalid row handler: https://issues.apache.org/jira/browse/ARROW-17641
+        parse_options.invalid_row_handler = parse_options.invalid_row_handler
+
         reader = csv.open_csv(f, read_options=read_options, **reader_args)
         schema = None
         while True:
