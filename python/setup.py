ca31e5a1ef2dfd5d064b011784be19a2a224224a
Jialing He <burglarralgrub@gmail.com>
2022-09-08 07:17:50 +0800

    [runtime env][bug fix] lock the minimum version of virtualenv to 20.0.24 (#27906)
    the test case will pass when virtualenv==20.0.24 but fails when virtualenv==20.0.23
    test cmd: `pytest python/ray/tests/test_runtime_env.py::test_failed_job_env_no_hang -sv`
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- python/setup.py
+++ python/setup.py
@@ -305,7 +305,7 @@ if setup_spec.type == SetupType.RAY:
         # Light weight requirement, can be replaced with "typing" once
         # we deprecate Python 3.7 (this will take a while).
         "typing_extensions; python_version < '3.8'",
-        "virtualenv",  # For pip runtime env.
+        "virtualenv>=20.0.24",  # For pip runtime env.
     ]
 
 
