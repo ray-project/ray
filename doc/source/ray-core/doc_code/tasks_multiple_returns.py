c6e75d2e486b3e8751c86a68708bde7c7807a86d
Philipp Moritz <pcmoritz@gmail.com>
2022-09-07 19:22:35 -0700

    lint

--- doc/source/ray-core/doc_code/tasks_multiple_returns.py
+++ doc/source/ray-core/doc_code/tasks_multiple_returns.py
@@ -6,6 +6,7 @@ import ray
 def return_single():
     return 0, 1, 2
 
+
 object_ref = return_single.remote()
 assert ray.get(object_ref) == (0, 1, 2)
 
@@ -15,7 +16,8 @@ assert ray.get(object_ref) == (0, 1, 2)
 def return_multiple():
     return 0, 1, 2
 
-object_ref0, object_ref1,  object_ref2 = return_multiple.remote()
+
+object_ref0, object_ref1, object_ref2 = return_multiple.remote()
 assert ray.get(object_ref0) == 0
 assert ray.get(object_ref1) == 1
 assert ray.get(object_ref2) == 2
