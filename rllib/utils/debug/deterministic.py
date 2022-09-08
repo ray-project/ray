c93ba186af2cbd42b286da376608247c1afafa71
Peyton Murray <peynmurray@gmail.com>
2022-09-08 02:25:01 -0700

    [core/rllib/air] Fix multiple deprecation warnings (#28315)
    This PR fixes multiple deprecation warnings affecting AIR examples. There's still work to be done on other deprecations, but I've bundled these changes together to avoid making a huge PR.
    
    - Fixed scheduling strategy deprecation warnings by making use of `PlacementGroupSchedulingStrategy`. This change takes care of the warnings associated with:
      - `placement_group`
      - `placement_group_bundle_index`
      - `placement_group_capture_child_tasks`
    - Removed `object_store_memory` argument in a few actor instantiations to avoid deprecation warnings
    - Removed `traceback.print_stack` from `python/ray/__init__.py` which prints the call stack any time there is a deprecation warning.
    - Remove use of deprecated `distutils.version.Version` in favor of `packaging.version.Version`. Added `packaging` as a soft dependency: it is installed for Python >= 3.10; otherwise `distutils.version.Version` is still used. See [PEP 632](https://peps.python.org/pep-0632/) for more information about this deprecation. In short, `distutils` will be removed in Python 3.12, and is deprecated for 3.10 and 3.11.
    
    Signed-off-by: pdmurray <peynmurray@gmail.com>
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- rllib/utils/debug/deterministic.py
+++ rllib/utils/debug/deterministic.py
@@ -37,9 +37,12 @@ def update_global_seed_if_necessary(
         if cuda_version is not None and float(torch.version.cuda) >= 10.2:
             os.environ["CUBLAS_WORKSPACE_CONFIG"] = "4096:8"
         else:
-            from distutils.version import LooseVersion
+            try:
+                from packaging.version import Version
+            except ImportError:
+                from distutils.version import LooseVersion as Version
 
-            if LooseVersion(torch.__version__) >= LooseVersion("1.8.0"):
+            if Version(torch.__version__) >= Version("1.8.0"):
                 # Not all Operations support this.
                 torch.use_deterministic_algorithms(True)
             else:
