8cd00812fe072bcb6f836a1d2fbe3a3255f760d9
Jialing He <burglarralgrub@gmail.com>
2022-09-10 10:16:04 +0800

    [Job Submission][refactor 3/N] implement job log api in JobAgent (#28288)
    Implement the job log interface in the job agent：
    - step 1. Get the agent address of the node where SupervisorActor is running. via GCS::GetNamedActor.
    - step 2. Send a request to this agent, and get the job log.  
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- python/ray/_private/gcs_utils.py
+++ python/ray/_private/gcs_utils.py
@@ -418,6 +418,9 @@ class GcsAioClient:
         self._job_info_stub = gcs_service_pb2_grpc.JobInfoGcsServiceStub(
             self._channel.channel()
         )
+        self._actor_info_stub = gcs_service_pb2_grpc.ActorInfoGcsServiceStub(
+            self._channel.channel()
+        )
 
     @_auto_reconnect
     async def check_alive(
@@ -530,6 +533,19 @@ class GcsAioClient:
     ) -> gcs_service_pb2.GetAllJobInfoReply:
         req = gcs_service_pb2.GetAllJobInfoRequest()
         reply = await self._job_info_stub.GetAllJobInfo(req, timeout=timeout)
+        return reply
+
+    @_auto_reconnect
+    async def get_named_actor_info(
+        self,
+        actor_name: str,
+        ray_namespace: str = "",
+        timeout: Optional[float] = None,
+    ) -> gcs_service_pb2.GetNamedActorInfoReply:
+        req = gcs_service_pb2.GetNamedActorInfoRequest(
+            name=actor_name, ray_namespace=ray_namespace
+        )
+        reply = await self._actor_info_stub.GetNamedActorInfo(req, timeout=timeout)
         return reply
 
 
