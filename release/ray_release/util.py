ede01d016ae628ae8ad271eb3985bea69387d75c
Kai Fricke <kai@anyscale.com>
2022-09-12 22:25:51 +0100

    Revert extra import
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- release/ray_release/util.py
+++ release/ray_release/util.py
@@ -7,7 +7,6 @@ import time
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple
 
 import requests
-from anyscale.sdk.anyscale_client.models.create_project import CreateProject
 from ray_release.logger import logger
 
 if TYPE_CHECKING:
