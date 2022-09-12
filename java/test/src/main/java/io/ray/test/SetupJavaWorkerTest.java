dd5b34f9d8c51e0b43b1f4099b649e10b971aaad
sunkunjian1 <sunkunjian1@jd.com>
2022-09-12 15:24:10 +0800

    reformat code
    Signed-off-by: sunkunjian1 <sunkunjian1@jd.com>

--- java/test/src/main/java/io/ray/test/SetupJavaWorkerTest.java
+++ java/test/src/main/java/io/ray/test/SetupJavaWorkerTest.java
@@ -5,12 +5,6 @@ import io.ray.api.Ray;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-/**
- * afafafa
- *
- * @author sunkunjian1
- * @date 2022/09/12
- **/
 @Test
 public class SetupJavaWorkerTest {
 
@@ -25,23 +19,23 @@ public class SetupJavaWorkerTest {
     public int getValue() {
       return value;
     }
-
   }
 
   /**
-   * raylet will call setup_worker.py with arguments to start java workers after Ray 2.0.0,
-   * and setup_worker.py can not start java workers correctly when argumens contian space.
-   * This test case is used to test whether the bug is fixed.
+   * raylet will call setup_worker.py with arguments to start java workers after Ray 2.0.0, and
+   * setup_worker.py can not start java workers correctly when argumens contian space. This test
+   * case is used to test whether the bug is fixed.
    */
-  public void testCreateActorFail() throws InterruptedException, NoSuchFieldException, IllegalAccessException {
+  public void testCreateActorFail()
+      throws InterruptedException, NoSuchFieldException, IllegalAccessException {
     String oldCodeSearchPathStr = System.getProperty("java.class.path");
-    //mock a fake code search path contains space
+    // mock a fake code search path contains space
     String newCodeSearchPathStr = oldCodeSearchPathStr + ":fakepathStart fakePathEnd";
     System.setProperty("java.class.path", newCodeSearchPathStr);
     Ray.init();
     ActorHandle<ActorTest.Counter> actor = Ray.actor(ActorTest.Counter::new, 1).remote();
     // throw RayTimeoutException exception if actor is not started correctly by raylet
-    Integer value = actor.task(ActorTest.Counter::getValue).remote().get(3*1000);
+    Integer value = actor.task(ActorTest.Counter::getValue).remote().get(3 * 1000);
     Assert.assertEquals(Integer.valueOf(1), value);
     System.setProperty("java.class.path", oldCodeSearchPathStr);
     Ray.shutdown();
