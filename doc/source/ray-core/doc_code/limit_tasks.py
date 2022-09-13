ed1238c2136cc61fa087c5079a5dc020fcfa508c
Jiajun Yao <jeromeyjj@gmail.com>
2022-09-13 08:06:30 -0700

    Revamp ray core design patterns doc [6/n]: ray wait limits in-flight tasks
    Signed-off-by: Jiajun Yao <jeromeyjj@gmail.com>

--- doc/source/ray-core/doc_code/limit_tasks.py
+++ doc/source/ray-core/doc_code/limit_tasks.py
@@ -1,47 +1,43 @@
-import numpy as np
-
+# __without_backpressure_start__
 import ray
 
 ray.init()
 
 
-# __defining_actor_start__
 @ray.remote
 class Actor:
-    def heavy_compute(self, large_array):
+    async def heavy_compute(self):
         # taking a long time...
+        # await asyncio.sleep(5)
         return
 
 
-# __defining_actor_end__
-
-
-# __creating_actor_start__
 actor = Actor.remote()
-# __creating_actor_end__
 
-# __executing_task_start__
+NUM_TASKS = 1000
 result_refs = []
-results = []
-max_in_flight_tasks = 100
-for i in range(1000):
-    large_array = np.zeros(1_000_000)
+# When NUM_TASKS is large enough, this will eventually OOM.
+for _ in range(NUM_TASKS):
+    result_refs.append(actor.heavy_compute.remote())
+ray.get(result_refs)
+# __without_backpressure_end__
 
-    # Allow 100 in flight calls
+# __with_backpressure_start__
+MAX_NUM_IN_FLIGHT_TASKS = 100
+result_refs = []
+for _ in range(NUM_TASKS):
+    # Allow 100 in flight tasks.
     # For example, if i = 100, ray.wait blocks until
     # 1 of the object_refs in result_refs is ready
     # and available before we submit another.
-    if len(result_refs) > max_in_flight_tasks:
+    if len(result_refs) > MAX_NUM_IN_FLIGHT_TASKS:
+        num_ready = len(result_refs) - MAX_NUM_IN_FLIGHT_TASKS
         # update result_refs to only
         # track the remaining tasks.
-        num_ready = len(result_refs) - max_in_flight_tasks
         newly_completed, result_refs = ray.wait(result_refs, num_returns=num_ready)
-        for completed_ref in newly_completed:
-            results.append(ray.get(completed_ref))
+        ray.get(newly_completed)
 
-    result_refs.append(actor.heavy_compute.remote(large_array))
+    result_refs.append(actor.heavy_compute.remote())
 
-newly_completed, result_refs = ray.wait(result_refs, num_returns=len(result_refs))
-for completed_ref in newly_completed:
-    results.append(ray.get(completed_ref))
-# __executing_task_end__
+ray.get(result_refs)
+# __with_backpressure_end__
