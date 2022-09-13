ray-project/ray

commit 96cceb08e8bf73df990437002e25883c5a72d30c

[tune] Raise error in PGF if head and worker bundles are empty (#28445)
Scheduling empty placement groups is not supported by Ray core (see e.g. #28443), so we shouldn't allow them to be created in the first place.

If we need fully empty resource requests, we can include this in the upcoming execution/resource refactor.

Signed-off-by: Kai Fricke <kai@anyscale.com>

python/ray/tune/execution/placement_groups.py
@@ -55,6 +55,21 @@ def _get_tune_pg_prefix():
     return _tune_pg_prefix
 
 
+def _sum_bundles(bundles: List[Dict[str, float]]) -> Dict[str, float]:
+    """Sum all resources in a list of resource bundles.
+
+    Args:
+        bundles: List of resource bundles.
+
+    Returns: Dict containing all resources summed up.
+    """
+    resources = {}
+    for bundle in bundles:
+        for k, v in bundle.items():
+            resources[k] = resources.get(k, 0) + v
+    return resources
+
+
 @PublicAPI(stability="beta")
 class PlacementGroupFactory:
     """Wrapper class that creates placement groups for trials.
@@ -146,9 +161,10 @@ class PlacementGroupFactory:
         *args,
         **kwargs,
     ):
-        assert (
-            len(bundles) > 0
-        ), "Cannot initialize a PlacementGroupFactory with zero bundles."
+        if not bundles:
+            raise ValueError(
+                "Cannot initialize a PlacementGroupFactory with zero bundles."
+            )
 
         self._bundles = [
             {k: float(v) for k, v in bundle.items() if v != 0} for bundle in bundles
@@ -158,6 +174,12 @@ class PlacementGroupFactory:
             # This is when trainable itself doesn't need resources.
             self._head_bundle_is_empty = True
             self._bundles.pop(0)
+
+            if not self._bundles:
+                raise ValueError(
+                    "Cannot initialize a PlacementGroupFactory with an empty head "
+                    "and zero worker bundles."
+                )
         else:
             self._head_bundle_is_empty = False
 
@@ -193,11 +215,7 @@ class PlacementGroupFactory:
     @property
     def required_resources(self) -> Dict[str, float]:
         """Returns a dict containing the sums of all resources"""
-        resources = {}
-        for bundle in self._bundles:
-            for k, v in bundle.items():
-                resources[k] = resources.get(k, 0) + v
-        return resources
+        return _sum_bundles(self._bundles)
 
     @property
     @DeveloperAPI

python/ray/tune/schedulers/resource_changing_scheduler.py
@@ -11,7 +11,7 @@ from ray.tune.execution import trial_runner
 from ray.tune.resources import Resources
 from ray.tune.schedulers.trial_scheduler import FIFOScheduler, TrialScheduler
 from ray.tune.experiment import Trial
-from ray.tune.execution.placement_groups import PlacementGroupFactory
+from ray.tune.execution.placement_groups import PlacementGroupFactory, _sum_bundles
 
 logger = logging.getLogger(__name__)
 
@@ -117,8 +117,7 @@ class DistributeResources:
         """Get total sums of resources in bundles"""
         if not bundles:
             return {"CPU": 0, "GPU": 0}
-        pgf = PlacementGroupFactory(bundles)
-        return pgf.required_resources
+        return _sum_bundles(bundles)
 
     def _is_bundle_empty(self, bundle: Dict[str, float]) -> bool:
         return not (bundle.get("CPU", 0) or bundle.get("GPU", 0))

python/ray/tune/tests/test_ray_trial_executor.py
@@ -548,6 +548,15 @@ class RayExecutorPlacementGroupTest(unittest.TestCase):
         assert executor.has_resources_for_trial(trial2)
         assert not executor.has_resources_for_trial(trial3)
 
+    def testEmptyPlacementGroupFactory(self):
+        # Empty bundles
+        with self.assertRaises(ValueError):
+            PlacementGroupFactory([])
+
+        # Empty head, empty workers
+        with self.assertRaises(ValueError):
+            PlacementGroupFactory([{}])
+
 
 class LocalModeExecutorTest(RayTrialExecutorTest):
     def setUp(self):

commit 27323b255ebfe1ee7956fccc2d696180d5a46867

[air] Hotfix test_api failure (#28473)
Fails after #28445

Signed-off-by: Kai Fricke <kai@anyscale.com>

python/ray/air/tests/test_api.py
@@ -139,10 +139,17 @@ def test_scaling_config_pgf_equivalance(
         use_gpu=use_gpu,
         placement_strategy=placement_strategy,
     )
-    pgf = scaling_config.as_placement_group_factory()
-    scaling_config_from_pgf = ScalingConfig.from_placement_group_factory(pgf)
-    assert scaling_config == scaling_config_from_pgf
-    assert scaling_config_from_pgf.as_placement_group_factory() == pgf
+    try:
+        pgf = scaling_config.as_placement_group_factory()
+        scaling_config_from_pgf = ScalingConfig.from_placement_group_factory(pgf)
+        assert scaling_config == scaling_config_from_pgf
+        assert scaling_config_from_pgf.as_placement_group_factory() == pgf
+    except ValueError as e:
+        # We do not have to test invalid placement group factories
+        assert str(e) == (
+            "Cannot initialize a PlacementGroupFactory with an empty head "
+            "and zero worker bundles."
+        )
 
 
 def test_datasets():