195211d8ff44dccd76cb6e5e01a28dd3aa831b2b
Kai Fricke <kai@anyscale.com>
2022-09-13 11:37:36 +0100

    Report at end of epoch
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- release/air_tests/horovod/workloads/horovod_tune_test.py
+++ release/air_tests/horovod/workloads/horovod_tune_test.py
@@ -60,7 +60,6 @@ def train_loop_per_worker(config):
     trainloader = DataLoader(
         trainset, batch_size=int(config["batch_size"]), shuffle=True, num_workers=4
     )
-    trainloader_len = len(trainloader)
 
     for epoch in range(epoch, 40):  # loop over the dataset multiple times
         running_loss = 0.0
@@ -82,23 +81,21 @@ def train_loop_per_worker(config):
             # print statistics
             running_loss += loss.item()
             epoch_steps += 1
-            if i == trainloader_len - 1:
-                checkpoint = Checkpoint.from_dict(
-                    dict(
-                        model_state=net.state_dict(),
-                        optimizer_state=optimizer.state_dict(),
-                        epoch=epoch,
-                    )
-                )
-                session.report(
-                    dict(loss=running_loss / epoch_steps), checkpoint=checkpoint
-                )
 
             if i % 2000 == 1999:  # print every 2000 mini-batches
                 print(
                     "[%d, %5d] loss: %.3f"
                     % (epoch + 1, i + 1, running_loss / epoch_steps)
                 )
+
+        checkpoint = Checkpoint.from_dict(
+            dict(
+                model_state=net.state_dict(),
+                optimizer_state=optimizer.state_dict(),
+                epoch=epoch,
+            )
+        )
+        session.report(dict(loss=running_loss / epoch_steps), checkpoint=checkpoint)
 
 
 if __name__ == "__main__":
