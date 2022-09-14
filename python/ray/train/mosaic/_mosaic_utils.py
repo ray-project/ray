fbf5006dae82c19217dd22b81781a29dcf3d310c
ilee300a <ilee300@anyscale.com>
2022-09-14 16:43:17 -0700

    added checkpointing and logging along with their documentation
    Signed-off-by: ilee300a <ilee300@anyscale.com>

--- python/ray/train/mosaic/_mosaic_utils.py
+++ python/ray/train/mosaic/_mosaic_utils.py
@@ -1,6 +1,16 @@
-from typing import Tuple, Iterable
-from ray.data.dataset import Dataset
+from __future__ import annotations
 import math
+from typing import Any, Dict, Tuple, Union, Iterable
+
+from ray.air.checkpoint import Checkpoint
+from ray.air import session
+from ray.data.dataset import Dataset
+
+from composer.loggers import Logger
+from composer.loggers.logger import LogLevel
+from composer.loggers.logger_destination import LoggerDestination
+from composer.core.state import State
+from composer.callbacks.checkpoint_saver import CheckpointSaver
 
 
 class _mosaic_iterator:
@@ -41,3 +51,117 @@ def process_datasets(
         eval_torch_iterable = None
 
     return train_torch_iterable, eval_torch_iterable
+
+
+class RayLogger(LoggerDestination):
+    """A logger to relay all logged information to ray.
+
+    This logger allows utilizing all necessary logging and logged data handling provided
+    by ray. When some information is logged, then the logged information is reported as
+    metrics.
+
+    Args:
+        log_level: the granuality to log data. The default value is ``LogLevel.BATCH``
+    """
+
+    def __init__(self, log_level: Union[str, int, LogLevel] = LogLevel.BATCH) -> None:
+        self.log_level = LogLevel(log_level)
+        self.data = {}
+
+    def log_data(self, state: State, log_level: LogLevel, data: Dict[str, Any]):
+        if log_level > self.log_level:
+            # the logged metric is more verbose than what we want to record.
+            return
+        self.data.update(data.items())
+        session.report(self.data)
+
+
+class RayTrainReportCallback(CheckpointSaver):
+    """A callback that wraps Composer's ``CheckpointSaver``.
+
+    This class is used to wrap each Composer ``CheckpointSaver`` to be used in Composer
+    trainer. The main role of this callback is to report the paths of the checkpoints
+    saved by the Composer ``CheckpointSaver`` it wraps. In addition, when the training
+    ends, (either with or without exception) the last checkpoint and the list of all
+    the checkpoints that have been saved and the list of Composer loggers are reported.
+
+    Example:
+        .. code-block:: python
+            # create a MosaicTrainer
+            mosaic_trainer =  MosaicTrainer(
+                    trainer_init_per_worker=trainer_init_per_worker,
+                    datasets={"train": train_dataset},
+                    trainer_init_config=trainer_init_config,
+                    scaling_config=scaling_config
+                )
+            result = mosaic_trainer.fit()
+
+            chkpt_dict = result.checkpoint.to_dict()
+
+            loggers = chkpt_dict["loggers"]
+            last_checkpoint = chkpt_dict["last_checkpoint"]
+            all_checkpoints = chkpt_dict["all_checkpoints"]
+
+    Args:
+        loggers: The list of Composer loggers that would be used in Composer
+            trainer initialization.
+        checkpoint_saver: A Composer ``CheckpointSaver`` that the callback will wrap.
+            If this argument is provided, then the parent class is initialized with the
+            passed in ``CheckpointSaver`` object's attributes. Otherwise, the parent
+            class is initialized with args provided.
+        args: Arguments for initializing a Composer ``CheckpointSaver`` object.
+    """
+
+    def __init__(self, loggers=None, checkpoint_saver: CheckpointSaver = None, **args):
+        self.loggers = loggers
+        self.last_checkpoint = None
+        self.checkpoint_count = 0
+
+        if checkpoint_saver:
+            super(RayTrainReportCallback, self).__init__(
+                checkpoint_saver.folder,
+                checkpoint_saver.filename,
+                checkpoint_saver.artifact_name,
+                checkpoint_saver.latest_filename,
+                checkpoint_saver.latest_artifact_name,
+                checkpoint_saver.save_interval,
+                overwrite=checkpoint_saver.overwrite,
+                num_checkpoints_to_keep=checkpoint_saver.num_checkpoints_to_keep,
+                weights_only=checkpoint_saver.weights_only,
+                **args
+            )
+        else:
+            super(RayTrainReportCallback, self).__init__(**args)
+
+    def close(self, state: State, logger: Logger) -> None:
+        del logger  # unused
+        checkpoint = Checkpoint.from_dict(
+            {
+                "last_checkpoint": self.last_checkpoint,
+                "loggers": self.loggers,
+                "all_checkpoints": self.saved_checkpoints,
+            }
+        )
+        session.report(metrics={}, checkpoint=checkpoint)
+
+    def epoch_checkpoint(self, state: State, logger: Logger) -> None:
+        super().epoch_checkpoint(state, logger)
+        self._update_checkpoint(state)
+
+    def batch_checkpoint(self, state: State, logger: Logger) -> None:
+        super().batch_checkpoint(state, logger)
+        self._update_checkpoint(state)
+
+    def _update_checkpoint(self, state: State):
+        # check that the saved checkpoint is not redundant
+        if len(self.saved_checkpoints) > self.checkpoint_count:
+            self.last_checkpoint = [
+                chkpt_path.absolute() for chkpt_path in self.saved_checkpoints[-1][1]
+            ]
+            self.checkpoint_count = len(self.saved_checkpoints)
+            session.report(
+                metrics={},
+                checkpoint=Checkpoint.from_dict(
+                    {"last_checkpoint": self.last_checkpoint}
+                ),
+            )
--- python/ray/train/mosaic/mosaic_trainer.py
+++ python/ray/train/mosaic/mosaic_trainer.py
@@ -1,6 +1,5 @@
 import inspect
 import os
-import composer.trainer
 
 from ray.air import session
 from ray.train.constants import (
@@ -8,16 +7,23 @@ from ray.train.constants import (
     TRAIN_DATASET_KEY,
 )
 
-from typing import TYPE_CHECKING, Any, Callable, Dict, Optional, Iterable
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Iterable
 from ray.air.checkpoint import Checkpoint
 from ray.air.config import DatasetConfig, RunConfig, ScalingConfig
-from ray.train.mosaic._mosaic_utils import process_datasets
+from ray.train.mosaic._mosaic_utils import (
+    process_datasets,
+    RayLogger,
+    RayTrainReportCallback,
+)
 from ray.train.torch import TorchConfig, TorchTrainer
 from ray.train.trainer import GenDataset
 from ray.util import PublicAPI
 
 if TYPE_CHECKING:
     from ray.data.preprocessor import Preprocessor
+
+import composer.trainer
+from composer.callbacks.checkpoint_saver import CheckpointSaver
 
 
 @PublicAPI(stability="alpha")
@@ -33,6 +39,172 @@ class MosaicTrainer(TorchTrainer):
     specified ``trainer_init_per_worker`` function to obtain an instantiated
     ``composer.Trainer`` object. The ``trainer_init_per_worker`` function
     will have access to preprocessed train and evaluation datasets.
+
+    To use ``MosaicTrainer``, the ray dataset should be mapped to pandas DataFrame,
+    since the labels for the columns are used to create a batched iterable to be
+    passed to Composer trainer. The column labels that would be used for training
+    should be provided in the ``trainer_init_config``
+
+    With ``MosaicTrainer``, there will always be checkpointing, with a default
+    ``CheckpointSaver`` that saves checkpoints to `ray_tmp` folder. All
+    ``CheckpointSaver`` objects are wrapped with ``RayTrainReportCallback`` to
+    allow reporting the paths of saved checkpoints. (For more detail check out
+    ``RayTrainReportCallback``). When resuming training from checkpoint, if
+    ``load_path`` is provided in ``trainer_init_config``, it will be used to load
+    saved checkpoints. Otherwise, if ``resume_from_checkpoint`` is provided, the
+    last reported path will be passed in as ``load_path`` to the Composer trainer.
+
+    All logged information, whether there is a composer logger provided or not, is
+    reported as metrics via ``RayLogger``. ``RayLogger`` is added to the logger
+    configuration for Composer trainer initialization. (For more detail check out
+    ``RayLogger``.)
+
+    Example:
+
+            .. code-block:: python
+            from typing import Tuple
+            import pandas as pd
+
+            import ray
+            from ray.data.datasource import SimpleTorchDatasource
+            from ray.train.mosaic import MosaicTrainer
+            from ray.air.config import ScalingConfig
+            from ray.data.extensions import TensorArray
+
+            import torch
+            import torch.utils.data
+            from torchmetrics.classification.accuracy import Accuracy
+
+            import torchvision.datasets
+            import torchvision.transforms as transforms
+
+            import composer
+
+            # create ray datasets
+            transform = transforms.Compose(
+                [transforms.ToTensor(),
+                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+            )
+
+            def train_dataset_factory():
+                return torchvision.datasets.CIFAR10(root="./data",
+                    download=True, train=True, transform=transform)
+
+            def test_dataset_factory():
+                return torchvision.datasets.CIFAR10(root="./data",
+                    download=True, train=False, transform=transform)
+
+            train_dataset: ray.data.Dataset = \
+                ray.data.read_datasource(SimpleTorchDatasource(),
+                    dataset_factory=train_dataset_factory)
+            test_dataset: ray.data.Dataset = \
+                ray.data.read_datasource(SimpleTorchDatasource(),
+                    dataset_factory=test_dataset_factory)
+
+            # map the dataset to pandas DataFrame
+            def convert_batch_pair(batch: Tuple[torch.Tensor, int]):
+                images = TensorArray([image.numpy() for image, _ in batch])
+                labels = [label for _, label in batch]
+                df = pd.DataFrame({"image" : images, "label": labels})
+                return df.head(32)
+
+            train_dataset_mapped = train_dataset.map_batches(convert_batch_pair)
+            test_dataset_mapped = test_dataset.map_batches(convert_batch_pair)
+
+            # create trainer_init_per_worker
+            def trainer_init_per_worker(train_dataset, eval_dataset, **config):
+                model = config.pop("model")
+                optimizer = config.pop("optimizer")
+                scheduler = config.pop("scheduler")
+
+                evaluator = composer.core.evaluator.Evaluator(
+                    dataloader = eval_dataset,
+                    label = 'my_evaluator',
+                    metrics = Accuracy()
+                )
+
+                return composer.trainer.Trainer(
+                    model=model,
+                    run_name="logger_test",
+                    train_dataloader=train_dataset,
+                    eval_dataloader=evaluator,
+                    optimizers=optimizer,
+                    schedulers=scheduler,
+                    **config
+                )
+
+
+            # setup trainer_init_config
+            model = composer.models.composer_resnet_cifar(model_name='resnet_20',
+                num_classes=10)
+
+            optimizer = composer.optim.DecoupledSGDW(
+                model.parameters(),
+                lr=0.05,
+                momentum=0.9,
+                weight_decay=2.0e-3
+            )
+
+            lr_scheduler = composer.optim.LinearWithWarmupScheduler(
+                t_warmup="1ep",
+                alpha_i=1.0,
+                alpha_f=1.0
+            )
+
+            scaling_config = ScalingConfig(num_workers=1, use_gpu=False)
+
+            loggers = [composer.loggers.InMemoryLogger()]
+
+            trainer_init_config = {
+                "model" : model,
+                "optimizer" : optimizer,
+                "scheduler" : lr_scheduler,
+                "batch_size" : 32,
+                "max_duration" : "1ep",
+                "labels" : ["image","label"],
+                "loggers" : loggers,
+                "save_folder" : "my_folder",
+            }
+
+            # create MosaicTrainer
+            datasets = {"train": train_dataset_mapped,
+                "evaluation": test_dataset_mapped}
+            trainer = MosaicTrainer(
+                trainer_init_per_worker=trainer_init_per_worker,
+                datasets=datasets,
+                trainer_init_config=trainer_init_config,
+                scaling_config=scaling_config
+            )
+
+            # run trainer
+            result=trainer.fit()
+
+            # retrieve checkpoint information
+            chkpt_dict = result.checkpoint.to_dict()
+            loggers = chkpt_dict["loggers"]
+            last_checkpoint = chkpt_dict["last_checkpoint"]
+            all_checkpoints = chkpt_dict["all_checkpoints"]
+
+            # retrieve metrics
+            metrics = result.metrics
+
+            # get epoch timeseries from in memory logger
+            in_memory_logger = loggers[0]
+            epoch_time_series = in_memory_logger.get_timeseries('epoch')
+
+            # create a new MosaicTrainer to resume from the last checkpoint
+            trainer_init_config["max_duration"] = "2ep"
+            trainer = MosaicTrainer(
+                trainer_init_per_worker=trainer_init_per_worker,
+                datasets=datasets,
+                trainer_init_config=trainer_init_config,
+                scaling_config=scaling_config,
+                resume_from_checkpoint=result.checkpoint
+            )
+
+            # train
+            trainer.fit()
+
 
     Args:
         trainer_init_per_worker: The function that returns an instantiated
@@ -134,19 +306,48 @@ def _mosaic_train_loop_per_worker(config):
     train_dataset = session.get_dataset_shard(TRAIN_DATASET_KEY)
     eval_dataset = session.get_dataset_shard(EVALUATION_DATASET_KEY)
 
+    # convert ray dataset into iterable
     train_torch_iterable, eval_torch_iterable = process_datasets(
-        train_dataset, eval_dataset, config["batch_size"], config["labels"]
+        train_dataset, eval_dataset, config.pop("batch_size"), config.pop("labels")
     )
 
+    # resume from checkpoint if checkpoint exists
+    checkpoint = session.get_checkpoint()
+    # use the last checkpoint as the load_path if load_path is not already defined
+    if checkpoint and "load_path" not in config:
+        config["load_path"] = str(checkpoint.to_dict()["last_checkpoint"][-1])
+
+    # add RayLogger to Composer trainer loggers
+    if "loggers" in config:
+        if not isinstance(config["loggers"], List):
+            config["loggers"] = [config["loggers"]]
+        config["loggers"].append(RayLogger())
+    else:
+        config["loggers"] = [RayLogger()]
+
+    # initialize Composer trainer
     trainer: composer.trainer.Trainer = trainer_init_per_worker(
         train_torch_iterable, eval_torch_iterable, **config
     )
 
-    # TODO : add supports for logging
-
-    # TODO : add callbacks if needed
-
-    # TODO : add checkpoint if needed
+    # We wrap all existing CheckpointSavers with RayTrainReportCallback
+    # If none exists, then we create a default one with "ray_tmp" folder
+    is_report_call_added = False
+    for callback in trainer.state.callbacks:
+        if isinstance(callback, CheckpointSaver):
+            trainer.state.callbacks.remove(callback)
+            trainer.state.callbacks.append(
+                RayTrainReportCallback(
+                    loggers=config["loggers"][:-1], checkpoint_saver=callback
+                )
+            )
+            is_report_call_added = True
+    if not is_report_call_added:
+        trainer.state.callbacks.append(
+            RayTrainReportCallback(
+                loggers=config["loggers"][:-1], folder="ray_tmp", overwrite=True
+            )
+        )
 
     # call the trainer
     trainer.fit()
