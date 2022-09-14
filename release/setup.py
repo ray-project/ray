a428a8b9b3cd9ba2d54eea4ee334766ceb35d516
Kai Fricke <krfricke@users.noreply.github.com>
2022-09-14 07:00:30 +0100

    [ci/release] Fix release/ray_release/setup.py (#28441)
    Currently faulty and can't e.g. be used to build wheels. Also skipping most CI tests when only release tests are changed.
    
    Signed-off-by: Kai Fricke <kai@anyscale.com>
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- release/setup.py
+++ release/setup.py
@@ -2,7 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="ray_release",
-    packages=find_packages(where=".", include="ray_release"),
+    packages=find_packages(
+        where=".",
+        include=[
+            package for package in find_packages() if package.startswith("ray_release")
+        ],
+    ),
     version="0.0.1",
     author="Ray Team",
     description="The Ray OSS release testing package",
