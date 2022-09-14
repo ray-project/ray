b470b978ee49e8d39a80c2e63fbd9960655f2847
Dmitri Gekhtman <62982571+DmitriGekhtman@users.noreply.github.com>
2022-09-14 17:26:59 -0400

    [Telemetry][Kuberentes] Distinguish Kubernetes deployment stacks (#28490)
    Right now, Ray telemetry indicates the majority of Ray's CPU hour usage comes from Ray running within a Kubernetes cluster. However, we have no data on what method is used to deploy Ray on Kubernetes.
    
    This PR enables Ray telemetry to distinguish between three methods of deploying Ray on Kubernetes:
    
    KubeRay >= 0.4.0
    Legacy Ray Operator with Ray >= 2.1.0
    All other methods
    The strategy is to have the operators inject an env variable into the Ray container's environment.
    The variable identifies the deployment method.
    
    This PR also modifies the legacy Ray operator to inject the relevant env variable.
    A follow-up KubeRay PR changes the KubeRay operator to do the same thing: ray-project/kuberay#562
    
    Signed-off-by: Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- python/ray/tests/test_usage_stats.py
+++ python/ray/tests/test_usage_stats.py
@@ -768,6 +768,19 @@ available_node_types:
     assert cluster_config_to_report.head_node_instance_type is None
     assert cluster_config_to_report.worker_node_instance_types is None
 
+    monkeypatch.setenv("RAY_USAGE_STATS_KUBERAY_IN_USE", "1")
+    cluster_config_to_report = ray_usage_lib.get_cluster_config_to_report(
+        tmp_path / "does_not_exist.yaml"
+    )
+    assert cluster_config_to_report.cloud_provider == "kuberay"
+
+    monkeypatch.delenv("RAY_USAGE_STATS_KUBERAY_IN_USE")
+    monkeypatch.setenv("RAY_USAGE_STATS_LEGACY_OPERATOR_IN_USE", "1")
+    cluster_config_to_report = ray_usage_lib.get_cluster_config_to_report(
+        tmp_path / "does_not_exist.yaml"
+    )
+    assert cluster_config_to_report.cloud_provider == "legacy_ray_operator"
+
 
 @pytest.mark.skipif(
     sys.platform == "win32",
