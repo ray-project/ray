1d513e1c2be09c77d34f6840f4f5dff98cc94106
Balaji Veeramani <bveeramani@berkeley.edu>
2022-09-13 17:24:36 -0700

    Update tfrecords_datasource.py

--- python/ray/data/datasource/tfrecords_datasource.py
+++ python/ray/data/datasource/tfrecords_datasource.py
@@ -1,8 +1,7 @@
-import tempfile
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Iterable
 import warnings
+import struct
 
-import os
 import pandas as pd
 import pyarrow
 import tensorflow as tf
@@ -18,33 +17,21 @@ class TFRecordDatasource(FileBasedDatasource):
     def _read_file(self, f: "pyarrow.NativeFile", path: str, **reader_args) -> Block:
         from google.protobuf.message import DecodeError
 
-        with tempfile.NamedTemporaryFile() as temporary_file:
-            # TensorFlow doesn't expose a function for reading TFRecords with a file
-            # handle; you can only read TFRecords with a file path. So, if the TFRecord
-            # doesn't exist locally (e.g., because it's stored in S3), we have to
-            # download a copy.
-            local_path = path
-            if not os.path.exists(local_path):
-                f.write(temporary_file)
-                local_path = temporary_file.name
+        data = []
+        for record in _read_records(f):
+            example = tf.train.Example()
+            try:
+                example.ParseFromString(record)
+            except DecodeError:
+                warnings.warn(
+                    "`TFRecordDatasource` failed to parse `tf.train.Example` "
+                    f"message in '{path}'. This warning can occur if your TFRecord "
+                    "file contains a message type other than `tf.train.Example`."
+                )
 
-            dataset = tf.data.TFRecordDataset([local_path])
+            data.append(_convert_example_to_dict(example))
 
-            data = []
-            for record in dataset:
-                example = tf.train.Example()
-                try:
-                    example.ParseFromString(record.numpy())
-                except DecodeError:
-                    warnings.warn(
-                        "`TFRecordDatasource` failed to parse `tf.train.Example` "
-                        f"message in '{path}'. This warning can occur if your TFRecord "
-                        "file contains a message type other than `tf.train.Example`."
-                    )
-
-                data.append(_convert_example_to_dict(example))
-
-            return pd.DataFrame.from_records(data)
+        return pd.DataFrame.from_records(data)
 
 
 def _convert_example_to_dict(
@@ -76,3 +63,51 @@ def _get_feature_value(
         return list(feature.float_list.value)
     if feature.int64_list.value:
         return list(feature.int64_list.value)
+
+
+# Adapted from https://github.com/vahidk/tfrecord/blob/master/tfrecord/reader.py#L16-L96
+#
+# MIT License
+#
+# Copyright (c) 2020 Vahid Kazemi
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+def _read_records(
+    file: "pyarrow.NativeFile",
+) -> Iterable[memoryview]:
+    length_bytes = bytearray(8)
+    crc_bytes = bytearray(4)
+    datum_bytes = bytearray(1024 * 1024)
+    while True:
+        num_length_bytes_read = file.readinto(length_bytes)
+        if num_length_bytes_read == 0:
+            break
+        elif num_length_bytes_read != 8:
+            raise RuntimeError("Failed to read the record size.")
+        if file.readinto(crc_bytes) != 4:
+            raise RuntimeError("Failed to read the start token.")
+        (length,) = struct.unpack("<Q", length_bytes)
+        if length > len(datum_bytes):
+            datum_bytes = datum_bytes.zfill(int(length * 1.5))
+        datum_bytes_view = memoryview(datum_bytes)[:length]
+        if file.readinto(datum_bytes_view) != length:
+            raise RuntimeError("Failed to read the record.")
+        if file.readinto(crc_bytes) != 4:
+            raise RuntimeError("Failed to read the end token.")
+        yield datum_bytes_view
