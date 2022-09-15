b76dd40d1b79711d3457bbb39e770eb8efb4d07e
Eric Liang <ekhliang@gmail.com>
2022-09-15 16:17:45 -0700

    wip
    Signed-off-by: Eric Liang <ekhliang@gmail.com>

--- src/ray/core_worker/task_manager.h
+++ src/ray/core_worker/task_manager.h
@@ -19,6 +19,7 @@
 #include "absl/synchronization/mutex.h"
 #include "ray/common/id.h"
 #include "ray/common/task/task.h"
+#include "ray/core_worker/context.h"
 #include "ray/core_worker/store_provider/memory_store/memory_store.h"
 #include "ray/core_worker/task_group.h"
 #include "ray/stats/metric_defs.h"
