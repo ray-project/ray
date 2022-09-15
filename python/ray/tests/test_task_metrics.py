27bf0fc637c6466e32835346567c699135a428e4
Eric Liang <ekhliang@gmail.com>
2022-09-15 16:10:24 -0700

    add test
    Signed-off-by: Eric Liang <ekhliang@gmail.com>

--- python/ray/tests/test_task_metrics.py
+++ python/ray/tests/test_task_metrics.py
@@ -25,9 +25,12 @@ def tasks_by_state(info) -> dict:
     res = fetch_prometheus_metrics([metrics_page])
     if "ray_tasks" in res:
         states = defaultdict(int)
+        states_raw = defaultdict(list)
         for sample in res["ray_tasks"]:
             states[sample.labels["State"]] += sample.value
+            states_raw[(sample.labels["State"], sample.labels["WorkerId"])].append(sample.value)
         print("Tasks by state: {}".format(states))
+        print("Tasks by state raw: {}".format(states_raw))
         return states
     else:
         return {}
@@ -61,6 +64,41 @@ ray.get(a)
     # TODO(ekl) optimize the reporting interval to be faster for testing
     wait_for_condition(
         lambda: tasks_by_state(info) == expected, timeout=20, retry_interval_ms=500
+    )
+    proc.kill()
+
+
+def test_task_nested(shutdown_only):
+    info = ray.init(num_cpus=2, **METRIC_CONFIG)
+
+    driver = """
+import ray
+import time
+
+ray.init("auto")
+
+@ray.remote(num_cpus=0)
+def wrapper():
+    @ray.remote
+    def f():
+        time.sleep(999)
+
+    ray.get([f.remote() for _ in range(10)])
+
+w = wrapper.remote()
+ray.get(w)
+"""
+    proc = run_string_as_driver_nonblocking(driver)
+
+    expected = {
+        "RUNNING": 2.0,
+        "WAITING_FOR_EXECUTION": 0.0,
+        "SCHEDULED": 8.0,
+        "WAITING_FOR_DEPENDENCIES": 0.0,
+    }
+    # TODO(ekl) optimize the reporting interval to be faster for testing
+    wait_for_condition(
+        lambda: tasks_by_state(info) == expected, timeout=20, retry_interval_ms=2000
     )
     proc.kill()
 
