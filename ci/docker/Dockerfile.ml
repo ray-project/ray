9e5b86cb78a4a4ce24e971597df20cd6b0aac02c
Kai Fricke <kai@anyscale.com>
2022-09-16 07:28:17 +0100

    Run
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- ci/docker/Dockerfile.ml
+++ ci/docker/Dockerfile.ml
@@ -1,4 +1,4 @@
 ARG DOCKER_IMAGE_TEST
 FROM $DOCKER_IMAGE_TEST
 
-RLLIB_TESTING=1 TRAIN_TESTING=1 TUNE_TESTING=1 bash --login -i ./ci/env/install-dependencies.sh
+RUN RLLIB_TESTING=1 TRAIN_TESTING=1 TUNE_TESTING=1 bash --login -i ./ci/env/install-dependencies.sh
