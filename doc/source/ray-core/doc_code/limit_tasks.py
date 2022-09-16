7614014daf6f72ede36d6aba2c92232a7d2e0974
Jiajun Yao <jeromeyjj@gmail.com>
2022-09-15 23:05:31 -0700

    up
    Signed-off-by: Jiajun Yao <jeromeyjj@gmail.com>

--- doc/source/ray-core/doc_code/limit_tasks.py
+++ doc/source/ray-core/doc_code/limit_tasks.py
@@ -26,16 +26,11 @@ ray.get(result_refs)
 MAX_NUM_IN_FLIGHT_TASKS = 100
 result_refs = []
 for _ in range(NUM_TASKS):
-    # Allow 100 in flight tasks.
-    # For example, if i = 100, ray.wait blocks until
-    # 1 of the object_refs in result_refs is ready
-    # and available before we submit another.
     if len(result_refs) > MAX_NUM_IN_FLIGHT_TASKS:
-        num_ready = len(result_refs) - MAX_NUM_IN_FLIGHT_TASKS
         # update result_refs to only
         # track the remaining tasks.
-        newly_completed, result_refs = ray.wait(result_refs, num_returns=num_ready)
-        ray.get(newly_completed)
+        ready_refs, result_refs = ray.wait(result_refs, num_returns=1)
+        ray.get(ready_refs)
 
     result_refs.append(actor.heavy_compute.remote())
 
