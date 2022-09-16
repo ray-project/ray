1348cfa05b431df42765e5664511184b9eb66cd7
sven1977 <svenmika1977@gmail.com>
2022-09-16 14:02:24 +0200

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/tests/test_nested_observation_spaces.py
+++ rllib/tests/test_nested_observation_spaces.py
@@ -352,7 +352,7 @@ class TupleSpyModel(TFModelV2):
 class NestedObservationSpacesTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        ray.init(num_cpus=5, local_mode=True)  # TODO
+        ray.init(num_cpus=5)
 
     @classmethod
     def tearDownClass(cls):
