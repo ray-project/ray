5d5c31a79e9ca9b3fc549fe1aed6894b4a00035e
Sihan Wang <sihanwang41@gmail.com>
2022-09-19 12:02:07 -0700

    Add data size for benchmarking
    Signed-off-by: Sihan Wang <sihanwang41@gmail.com>

--- release/serve_tests/workloads/serve_grpc_benchmark.py
+++ release/serve_tests/workloads/serve_grpc_benchmark.py
@@ -19,12 +19,12 @@ class D:
         return input["a"]
 
 
-async def measure_grpc_throughput_tps(duration_secs=10):
+async def measure_grpc_throughput_tps(data_size=1, duration_secs=10):
 
     async with grpc.aio.insecure_channel("localhost:9000") as channel:
         stub = serve_pb2_grpc.PredictAPIsServiceStub(channel)
         tps_stats = []
-        data = bytes("123", "utf-8")
+        data = bytes("123" * data_size, "utf-8")
         for _ in range(duration_secs):
             start = time.time()
             request_completed = 0
@@ -36,10 +36,10 @@ async def measure_grpc_throughput_tps(duration_secs=10):
     return tps_stats
 
 
-async def measure_http_throughput_tps(duration_secs=10):
+async def measure_http_throughput_tps(data_size=1, duration_secs=10):
 
     tps_stats = []
-    data = "123"
+    data = "123" * data_size
 
     async def fetch(session):
         async with session.get("http://localhost:8000/", json={"a": data}) as response:
@@ -57,12 +57,12 @@ async def measure_http_throughput_tps(duration_secs=10):
     return tps_stats
 
 
-async def trial(measure_func, num_clients=1):
+async def trial(measure_func, data_size=1, num_clients=1):
 
     client_tasks = [measure_func for _ in range(num_clients)]
 
     throughput_stats_tps_list = await asyncio.gather(
-        *[client_task() for client_task in client_tasks]
+        *[client_task(data_size) for client_task in client_tasks]
     )
     throughput_stats_tps = []
     for client_rst in throughput_stats_tps_list:
@@ -79,18 +79,23 @@ async def json_resolver(request: starlette.requests.Request):
 
 @click.command()
 @click.option("--http-test", type=bool, default=False)
-def main(http_test: Optional[bool]):
+@click.option(
+    "--data-size",
+    default="1",
+    type=int,
+)
+def main(http_test: Optional[bool], data_size: Optional[int]):
 
     loop = asyncio.get_event_loop()
     if http_test:
         serve.run(DAGDriver.bind(D.bind(), http_adapter=json_resolver))
         throughput_mean_tps, throughput_std_tps = loop.run_until_complete(
-            trial(measure_http_throughput_tps)
+            trial(measure_http_throughput_tps, data_size=data_size)
         )
     else:
         serve.run(gRPCDriver.bind(D.bind()))
         throughput_mean_tps, throughput_std_tps = loop.run_until_complete(
-            trial(measure_grpc_throughput_tps)
+            trial(measure_grpc_throughput_tps, data_size=data_size)
         )
 
     print(throughput_mean_tps, throughput_std_tps)
