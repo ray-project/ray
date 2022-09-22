2a3df28f29e3e1ab447b9b0d413348281b3f9346
Max Pumperla <max.pumperla@googlemail.com>
2022-09-22 13:00:22 +0200

    rm argparse, typer for training
    Signed-off-by: Max Pumperla <max.pumperla@googlemail.com>

--- rllib/train.py
+++ rllib/train.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-import argparse
+import json
 import os
 from pathlib import Path
 import yaml
+import typer
 
 import ray
-from ray.tune.experiment.config_parser import _make_parser
 from ray.tune.result import DEFAULT_RESULTS_DIR
 from ray.tune.resources import resources_to_json
 from ray.tune.tune import run_experiments
@@ -18,177 +18,70 @@ from ray.rllib.utils.framework import try_import_tf, try_import_torch
 tf1, tf, tfv = try_import_tf()
 torch, _ = try_import_torch()
 
-EXAMPLE_USAGE = """
-Training example via RLlib CLI:
-    rllib train --run DQN --env CartPole-v0
 
-Grid search example via RLlib CLI:
-    rllib train -f tuned_examples/cartpole-ppo-grid-search-example.yaml
+def run(
+    run: str = typer.Option(...),
+    env: str = typer.Option(None),
+    config: str = typer.Option("{}"),
+    config_file: str = typer.Option(None),
+    stop: str = typer.Option("{}"),
+    experiment_name: str = typer.Option("default"),
+    v: bool = typer.Option(False),
+    vv: bool = typer.Option(False),
+    resume: bool = typer.Option(False),
+    num_samples: int = typer.Option(1),
+    checkpoint_freq: int = typer.Option(0),
+    checkpoint_at_end: bool = typer.Option(False),
+    local_dir: str = typer.Option(DEFAULT_RESULTS_DIR),
+    local_mode: bool = typer.Option(False),
+    restore: str = typer.Option(None),
+    framework: str = typer.Option(None),
+    resources_per_trial: str = typer.Option(None),
+    sync_on_checkpoint: bool = typer.Option(False),
+    keep_checkpoints_num: int = typer.Option(None),
+    checkpoint_score_attr: str = typer.Option("training_iteration"),
+    export_formats: str = typer.Option(None),
+    max_failures: int = typer.Option(3),
+    scheduler: str = typer.Option("FIFO"),
+    scheduler_config: str = typer.Option("{}"),
+    ray_address: str = typer.Option(None),
+    ray_ui: bool = typer.Option(False),
+    ray_num_cpus: int = typer.Option(None),
+    ray_num_gpus: int = typer.Option(None),
+    ray_num_nodes: int = typer.Option(None),
+    ray_object_store_memory: int = typer.Option(None),
+    upload_dir: str = typer.Option(""),
+    trace: bool = typer.Option(False),
+    torch: bool = typer.Option(False),  # deprecated
+    eager: bool = typer.Option(False),  # deprecated
+):
+    stop = json.loads(stop)
+    config = json.loads(config)
+    scheduler_config = json.loads(scheduler_config)
 
-Grid search example via executable:
-    ./train.py -f tuned_examples/cartpole-ppo-grid-search-example.yaml
-
-Note that -f overrides all other trial-specific command-line options.
-"""
-
-
-def create_parser(parser_creator=None):
-    parser = _make_parser(
-        parser_creator=parser_creator,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description="Train a reinforcement learning agent.",
-        epilog=EXAMPLE_USAGE,
-    )
-
-    # See also the base parser definition in ray/tune/experiment/__config_parser.py
-    parser.add_argument(
-        "--ray-address",
-        default=None,
-        type=str,
-        help="Connect to an existing Ray cluster at this address instead "
-        "of starting a new one.",
-    )
-    parser.add_argument(
-        "--ray-ui", action="store_true", help="Whether to enable the Ray web UI."
-    )
-    # Deprecated: Use --ray-ui, instead.
-    parser.add_argument(
-        "--no-ray-ui",
-        action="store_true",
-        help="Deprecated! Ray UI is disabled by default now. "
-        "Use `--ray-ui` to enable.",
-    )
-    parser.add_argument(
-        "--local-mode",
-        action="store_true",
-        help="Run ray in local mode for easier debugging.",
-    )
-    parser.add_argument(
-        "--ray-num-cpus",
-        default=None,
-        type=int,
-        help="--num-cpus to use if starting a new cluster.",
-    )
-    parser.add_argument(
-        "--ray-num-gpus",
-        default=None,
-        type=int,
-        help="--num-gpus to use if starting a new cluster.",
-    )
-    parser.add_argument(
-        "--ray-num-nodes",
-        default=None,
-        type=int,
-        help="Emulate multiple cluster nodes for debugging.",
-    )
-    parser.add_argument(
-        "--ray-object-store-memory",
-        default=None,
-        type=int,
-        help="--object-store-memory to use if starting a new cluster.",
-    )
-    parser.add_argument(
-        "--experiment-name",
-        default="default",
-        type=str,
-        help="Name of the subdirectory under `local_dir` to put results in.",
-    )
-    parser.add_argument(
-        "--local-dir",
-        default=DEFAULT_RESULTS_DIR,
-        type=str,
-        help="Local dir to save training results to. Defaults to '{}'.".format(
-            DEFAULT_RESULTS_DIR
-        ),
-    )
-    parser.add_argument(
-        "--upload-dir",
-        default="",
-        type=str,
-        help="Optional URI to sync training results to (e.g. s3://bucket).",
-    )
-    # This will override any framework setting found in a yaml file.
-    parser.add_argument(
-        "--framework",
-        choices=["tf", "tf2", "tfe", "torch"],
-        default=None,
-        help="The DL framework specifier.",
-    )
-    parser.add_argument(
-        "-v", action="store_true", help="Whether to use INFO level logging."
-    )
-    parser.add_argument(
-        "-vv", action="store_true", help="Whether to use DEBUG level logging."
-    )
-    parser.add_argument(
-        "--resume",
-        action="store_true",
-        help="Whether to attempt to resume previous Tune experiments.",
-    )
-    parser.add_argument(
-        "--trace",
-        action="store_true",
-        help="Whether to attempt to enable tracing for eager mode.",
-    )
-    parser.add_argument(
-        "--env", default=None, type=str, help="The gym environment to use."
-    )
-    parser.add_argument(
-        "-f",
-        "--config-file",
-        default=None,
-        type=str,
-        help="If specified, use config options from this file. Note that this "
-        "overrides any trial-specific options set via flags above.",
-    )
-
-    # Obsolete: Use --framework=torch|tf2|tfe instead!
-    parser.add_argument(
-        "--torch",
-        action="store_true",
-        help="Whether to use PyTorch (instead of tf) as the DL framework.",
-    )
-    parser.add_argument(
-        "--eager",
-        action="store_true",
-        help="Whether to attempt to enable TF eager execution.",
-    )
-
-    return parser
-
-
-def run(args, parser):
-    if args.config_file:
-        with open(args.config_file) as f:
+    if config_file:
+        with open(config_file) as f:
             experiments = yaml.safe_load(f)
     else:
         # Note: keep this in sync with tune/experiment/__config_parser.py
         experiments = {
-            args.experiment_name: {  # i.e. log to ~/ray_results/default
-                "run": args.run,
-                "checkpoint_freq": args.checkpoint_freq,
-                "checkpoint_at_end": args.checkpoint_at_end,
-                "keep_checkpoints_num": args.keep_checkpoints_num,
-                "checkpoint_score_attr": args.checkpoint_score_attr,
-                "local_dir": args.local_dir,
-                "resources_per_trial": (
-                    args.resources_per_trial
-                    and resources_to_json(args.resources_per_trial)
-                ),
-                "stop": args.stop,
-                "config": dict(args.config, env=args.env),
-                "restore": args.restore,
-                "num_samples": args.num_samples,
+            experiment_name: {  # i.e. log to ~/ray_results/default
+                "run": run,
+                "checkpoint_freq": checkpoint_freq,
+                "checkpoint_at_end": checkpoint_at_end,
+                "keep_checkpoints_num": keep_checkpoints_num,
+                "checkpoint_score_attr": checkpoint_score_attr,
+                "local_dir": local_dir,
+                "resources_per_trial": (resources_to_json(resources_per_trial)),
+                "stop": stop,
+                "config": dict(config, env=env),
+                "restore": restore,
+                "num_samples": num_samples,
                 "sync_config": {
-                    "upload_dir": args.upload_dir,
+                    "upload_dir": upload_dir,
                 },
             }
         }
-
-    # Ray UI.
-    if args.no_ray_ui:
-        deprecation_warning(old="--no-ray-ui", new="--ray-ui", error=False)
-        args.ray_ui = False
 
     verbose = 1
     for exp in experiments.values():
@@ -217,59 +110,60 @@ def run(args, parser):
 
             exp["config"]["input"] = patch_path(input_)
 
-        if not exp.get("run"):
-            parser.error("the following arguments are required: --run")
         if not exp.get("env") and not exp.get("config", {}).get("env"):
-            parser.error("the following arguments are required: --env")
-
-        if args.torch:
+            raise ValueError(
+                "You either need to provide an --env argument or pass"
+                "an `env` key with a valid environment to your `config`"
+                "argument."
+            )
+        if torch:
             deprecation_warning("--torch", "--framework=torch")
             exp["config"]["framework"] = "torch"
-        elif args.eager:
+        elif eager:
             deprecation_warning("--eager", "--framework=[tf2|tfe]")
             exp["config"]["framework"] = "tfe"
-        elif args.framework is not None:
-            exp["config"]["framework"] = args.framework
+        elif framework is not None:
+            exp["config"]["framework"] = framework
 
-        if args.trace:
+        if trace:
             if exp["config"]["framework"] not in ["tf2", "tfe"]:
                 raise ValueError("Must enable --eager to enable tracing.")
             exp["config"]["eager_tracing"] = True
 
-        if args.v:
+        if v:
             exp["config"]["log_level"] = "INFO"
             verbose = 3  # Print details on trial result
-        if args.vv:
+        if vv:
             exp["config"]["log_level"] = "DEBUG"
             verbose = 3  # Print details on trial result
 
-    if args.ray_num_nodes:
+    if ray_num_nodes:
         # Import this only here so that train.py also works with
         # older versions (and user doesn't use `--ray-num-nodes`).
         from ray.cluster_utils import Cluster
 
         cluster = Cluster()
-        for _ in range(args.ray_num_nodes):
+        for _ in range(ray_num_nodes):
             cluster.add_node(
-                num_cpus=args.ray_num_cpus or 1,
-                num_gpus=args.ray_num_gpus or 0,
-                object_store_memory=args.ray_object_store_memory,
+                num_cpus=ray_num_cpus or 1,
+                num_gpus=ray_num_gpus or 0,
+                object_store_memory=ray_object_store_memory,
             )
         ray.init(address=cluster.address)
     else:
         ray.init(
-            include_dashboard=args.ray_ui,
-            address=args.ray_address,
-            object_store_memory=args.ray_object_store_memory,
-            num_cpus=args.ray_num_cpus,
-            num_gpus=args.ray_num_gpus,
-            local_mode=args.local_mode,
+            include_dashboard=ray_ui,
+            address=ray_address,
+            object_store_memory=ray_object_store_memory,
+            num_cpus=ray_num_cpus,
+            num_gpus=ray_num_gpus,
+            local_mode=local_mode,
         )
 
     run_experiments(
         experiments,
-        scheduler=create_scheduler(args.scheduler, **args.scheduler_config),
-        resume=args.resume,
+        scheduler=create_scheduler(scheduler, **scheduler_config),
+        resume=resume,
         verbose=verbose,
         concurrent=True,
     )
@@ -278,9 +172,8 @@ def run(args, parser):
 
 
 def main():
-    parser = create_parser()
-    args = parser.parse_args()
-    run(args, parser)
+    """Run the CLI."""
+    typer.run(run)
 
 
 if __name__ == "__main__":
