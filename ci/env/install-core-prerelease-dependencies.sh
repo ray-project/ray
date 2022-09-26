ed4935a08d8fa9d97660daf2ab7cc371641cf6c3
scv119 <scv119@gmail.com>
2022-09-26 13:18:19 -0700

    address comments

--- ci/env/install-core-prerelease-dependencies.sh
+++ ci/env/install-core-prerelease-dependencies.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# insall all unbounded dependencies in setup.py for ray core
+set -e
+
+# install all unbounded dependencies in setup.py for ray core
 for dependency in attrs jsonschema aiosignal frozenlist requests grpcio
 do
     python -m pip install -U --pre $dependency
