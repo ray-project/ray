cce8d6ec448b5bc95b7b21ae541f3a29032001b3
scv119 <scv119@gmail.com>
2022-09-26 15:25:53 -0700

    make it eager

--- ci/env/install-core-prerelease-dependencies.sh
+++ ci/env/install-core-prerelease-dependencies.sh
@@ -5,5 +5,5 @@ set -e
 # install all unbounded dependencies in setup.py for ray core
 for dependency in attrs jsonschema aiosignal frozenlist requests grpcio
 do
-    python -m pip install -U --pre $dependency
+    python -m pip install -U --pre --upgrade-strategy=eager $dependency
 done
