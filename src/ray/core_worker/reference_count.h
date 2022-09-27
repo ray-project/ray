ae89cca96e079bea250ceb8b4bf19d964ba6860a
paer <chenqixiang.cqx@antgroup.com>
2022-09-27 20:03:38 +0800

    lint by format.sh
    Signed-off-by: paer <chenqixiang.cqx@antgroup.com>

--- src/ray/core_worker/reference_count.h
+++ src/ray/core_worker/reference_count.h
@@ -34,19 +34,20 @@
 namespace ray {
 namespace core {
 
-/// Extract a key to identify a Address so that the flyweight can look 
+/// Extract a key to identify a Address so that the flyweight can look
 /// up the Address from the pool.
 struct AddressWorkeridExtractor {
-  const std::string& operator()(const rpc::Address& addr)const
-  {
+  const std::string &operator()(const rpc::Address &addr) const {
     return addr.worker_id();
   }
 };
 
-/// Since the owner address has high redundency across objects, 
+/// Since the owner address has high redundency across objects,
 /// we use boost::flyweight to maintain an intern table to reduce
-/// memory footprint per object. 
-typedef boost::flyweight<boost::flyweights::key_value<std::string,rpc::Address,AddressWorkeridExtractor>> InternAddress;
+/// memory footprint per object.
+typedef boost::flyweight<
+    boost::flyweights::key_value<std::string, rpc::Address, AddressWorkeridExtractor>>
+    InternAddress;
 
 // Interface for mocking.
 class ReferenceCounterInterface {
