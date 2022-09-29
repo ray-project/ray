f7d3a614fadf17cf42f2c4f15d05c3911125a132
Clark Zinzow <clarkzinzow@gmail.com>
2022-09-29 14:45:07 -0700

    [AIR - Datasets] Ragged tensor extension type. (#27625)
    Ragged tensors pop up in NLP (textual sentences/documents of different lengths, N-grams), CV (images of different resolutions, ssd300_vgg16 detection outputs) and audio (differing durations) use cases. Our existing tensor extension doesn't support ragged tensors, failing when the we or the user attempts to cast such data to it.
    
    This PR adds support for non-nested ragged tensors, i.e. where each each tensor element (such as an image or sentence) is non-ragged. This allows us to support imagery columns of differing shapes or even differing number of dimensions, object detection outputs containing a variable number of detections (with a bounding box per detection) per sample, text columns containing N-gram embeddings, etc.
    
    In order to keep the PR as simple as possible while meeting the requirements of these use cases, this PR does not support arbitrary nesting of ragged tensors. Any such ragged tensor extension with arbitrary nesting should most likely be done in upstream Arrow, in C++.
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- python/ray/data/extensions/tensor_extension.py
+++ python/ray/data/extensions/tensor_extension.py
@@ -2,8 +2,11 @@ from ray.air.util.tensor_extensions.pandas import (  # noqa: F401
     TensorDtype,
     TensorArray,
     TensorArrayElement,
+    column_needs_tensor_extension,
 )
 from ray.air.util.tensor_extensions.arrow import (  # noqa: F401
     ArrowTensorType,
     ArrowTensorArray,
+    ArrowVariableShapedTensorType,
+    ArrowVariableShapedTensorArray,
 )
