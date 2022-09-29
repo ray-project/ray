7c0d8a846384bc576433088f15571cc2ef90dac7
Clarence Ng <clarence.wyng@gmail.com>
2022-09-29 05:19:53 -0700

    [core] prefer killing retriable tasks (prefer default tasks over actors)
    Signed-off-by: Clarence Ng <clarence.wyng@gmail.com>

--- python/ray/tests/test_memory_pressure.py
+++ python/ray/tests/test_memory_pressure.py
@@ -118,9 +118,30 @@ def has_metric_tagged_with_value(tag, value) -> bool:
 def test_memory_pressure_kill_actor(ray_with_memory_monitor):
     leaker = Leaker.options(max_restarts=0, max_task_retries=0).remote()
 
-    bytes_to_alloc = get_additional_bytes_to_reach_memory_usage_pct(
-        memory_usage_threshold_fraction + 0.1
+    bytes_to_alloc = get_additional_bytes_to_reach_memory_usage_pct(memory_usage_threshold_fraction - 0.1)
+    ray.get(leaker.allocate.remote(bytes_to_alloc, memory_monitor_interval_ms * 3))
+
+    bytes_to_alloc = get_additional_bytes_to_reach_memory_usage_pct(memory_usage_threshold_fraction + 0.1)
+    with pytest.raises(ray.exceptions.RayActorError) as _:
+        ray.get(leaker.allocate.remote(bytes_to_alloc, memory_monitor_interval_ms * 3))
+
+    wait_for_condition(
+        has_metric_tagged_with_value,
+        timeout=10,
+        retry_interval_ms=100,
+        tag="MemoryManager.ActorEviction.Total",
+        value=1.0,
     )
+
+
+@pytest.mark.skipif(
+    sys.platform != "linux" and sys.platform != "linux2",
+    reason="memory monitor only on linux currently",
+)
+def test_memory_pressure_kill_restartable_actor(ray_with_memory_monitor):
+    leaker = Leaker.options(max_restarts=1, max_task_retries=1).remote()
+
+    bytes_to_alloc = get_additional_bytes_to_reach_memory_usage_pct(memory_usage_threshold_fraction + 0.1)
     with pytest.raises(ray.exceptions.RayActorError) as _:
         ray.get(leaker.allocate.remote(bytes_to_alloc, memory_monitor_interval_ms * 3))
 
@@ -138,7 +159,7 @@ def test_memory_pressure_kill_actor(ray_with_memory_monitor):
     reason="memory monitor only on linux currently",
 )
 def test_memory_pressure_kill_task(ray_with_memory_monitor):
-    bytes_to_alloc = get_additional_bytes_to_reach_memory_usage_pct(0.95)
+    bytes_to_alloc = get_additional_bytes_to_reach_memory_usage_pct(1.1)
     with pytest.raises(ray.exceptions.OutOfMemoryError) as _:
         ray.get(allocate_memory.options(max_retries=0).remote(bytes_to_alloc))
 
@@ -148,6 +169,24 @@ def test_memory_pressure_kill_task(ray_with_memory_monitor):
         retry_interval_ms=100,
         tag="MemoryManager.TaskEviction.Total",
         value=1.0,
+    )
+
+
+@pytest.mark.skipif(
+    sys.platform != "linux" and sys.platform != "linux2",
+    reason="memory monitor only on linux currently",
+)
+def test_memory_pressure_kill_retryable_task(ray_with_memory_monitor):
+    bytes_to_alloc = get_additional_bytes_to_reach_memory_usage_pct(1.1)
+    with pytest.raises(ray.exceptions.OutOfMemoryError) as _:
+        ray.get(allocate_memory.options(max_retries=1).remote(bytes_to_alloc))
+
+    wait_for_condition(
+        has_metric_tagged_with_value,
+        timeout=10,
+        retry_interval_ms=100,
+        tag="MemoryManager.TaskEviction.Total",
+        value=2.0,
     )
 
 
