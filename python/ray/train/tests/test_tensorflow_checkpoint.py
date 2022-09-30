663122e6b8377380a300f10e2cebb996cb1a77b0
xwjiang2010 <xwjiang2010@gmail.com>
2022-09-30 13:13:05 -0700

    fix tests
    Signed-off-by: xwjiang2010 <xwjiang2010@gmail.com>

--- python/ray/train/tests/test_tensorflow_checkpoint.py
+++ python/ray/train/tests/test_tensorflow_checkpoint.py
@@ -42,7 +42,7 @@ def test_tensorflow_checkpoint_saved_model():
 def test_tensorflow_checkpoint_h5():
     # The test passes if the following can run successfully.
 
-    def train_fn():
+    def train_func():
         model = tf.keras.Sequential(
             [
                 tf.keras.layers.InputLayer(input_shape=()),
@@ -51,12 +51,12 @@ def test_tensorflow_checkpoint_h5():
                 tf.keras.layers.Dense(1),
             ]
         )
-        model.save("my_model")
-        checkpoint = TensorflowCheckpoint.from_saved_model("my_model")
+        model.save("my_model.h5")
+        checkpoint = TensorflowCheckpoint.from_h5("my_model.h5")
         session.report({"my_metric": 1}, checkpoint=checkpoint)
 
     trainer = TensorflowTrainer(
-        train_loop_per_worker=train_fn, scaling_config=ScalingConfig(num_workers=2)
+        train_loop_per_worker=train_func, scaling_config=ScalingConfig(num_workers=2)
     )
 
     result_checkpoint = trainer.fit().checkpoint
