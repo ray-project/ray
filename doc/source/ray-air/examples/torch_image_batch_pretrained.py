768b36c7841fa8a899869e322f6fa09df311102b
Clark Zinzow <clarkzinzow@gmail.com>
2022-10-03 20:42:07 +0000

    Fix examples and tests.

--- doc/source/ray-air/examples/torch_image_batch_pretrained.py
+++ doc/source/ray-air/examples/torch_image_batch_pretrained.py
@@ -23,7 +23,7 @@ def preprocess(df: pd.DataFrame) -> pd.DataFrame:
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
     )
-    df["image"] = [preprocess(x).numpy() for x in df["image"]]
+    df.loc[:, "image"] = [preprocess(x).numpy() for x in df["image"]]
     return df
 
 
