7892981d4508290eb9cd7d6e9636432000adef56
xwjiang2010 <xwjiang2010@gmail.com>
2022-10-04 08:13:12 -0700

    fix lint
    Signed-off-by: xwjiang2010 <xwjiang2010@gmail.com>

--- python/ray/train/tests/test_tensorflow_trainer.py
+++ python/ray/train/tests/test_tensorflow_trainer.py
@@ -5,7 +5,6 @@ import pytest
 
 import ray
 from ray.air import session
-from ray.air.checkpoint import Checkpoint
 from ray.air.examples.tf.tensorflow_regression_example import (
     get_dataset,
     train_func as tensorflow_linear_train_func,
