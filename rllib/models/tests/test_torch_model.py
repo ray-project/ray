bb44c14227c66520d8c0cd3fad89bf484c7a0fea
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2022-10-05 13:28:53 -0700

    1. converted class attributes to setters 2. use override decorator 3. SimpleModel should not have any T dimension, it can confuse folks. So I removed it. 4. I merged all the unittests under one class and separated them by methods names. It will be easier to use -k filter to run pytests later if we don't allow repetative method names. 5. used check instead of asserTrue torch.all() 6. refactor check_xxx to update_xxx
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- rllib/models/tests/test_torch_model.py
+++ rllib/models/tests/test_torch_model.py
@@ -4,6 +4,8 @@ import tempfile
 import torch
 from torch import nn
 
+from ray.rllib.utils.test_utils import check
+from ray.rllib.utils.annotations import override
 from ray.rllib.models.temp_spec_classes import TensorDict, SpecDict, ModelConfig
 from ray.rllib.models.torch.model import TorchRecurrentModel, TorchModel
 
@@ -11,11 +13,27 @@ B, T = 6, 8
 
 
 class SimpleRecurrentModel(TorchRecurrentModel):
-    input_spec = SpecDict({"in": "b t h"}, h=2)
-    output_spec = SpecDict({"out": "b t h"}, h=3)
-    prev_state_spec = SpecDict({"in": "b h"}, h=4)
-    next_state_spec = SpecDict({"out": "b h"}, h=5)
+    @property
+    @override(TorchRecurrentModel)
+    def input_spec(self) -> SpecDict:
+        return SpecDict({"in": "b t h"}, h=2)
 
+    @property
+    @override(TorchRecurrentModel)
+    def output_spec(self) -> SpecDict:
+        return SpecDict({"out": "b t h"}, h=3)
+
+    @property
+    @override(TorchRecurrentModel)
+    def prev_state_spec(self) -> SpecDict:
+        return SpecDict({"in": "b h"}, h=4)
+
+    @property
+    @override(TorchRecurrentModel)
+    def next_state_spec(self) -> SpecDict:
+        return SpecDict({"out": "b h"}, h=5)
+
+    @override(TorchRecurrentModel)
     def _unroll(self, input, prev_state):
         assert torch.all(input["in"] == torch.arange(B * T * 2).reshape(B, T, 2))
         assert torch.all(prev_state["in"] == torch.arange(B * 4).reshape(B, 4))
@@ -29,12 +47,20 @@ class SimpleRecurrentModel(TorchRecurrentModel):
 
 
 class SimpleModel(TorchModel):
-    input_spec = SpecDict({"in": "b t h"}, h=2)
-    output_spec = SpecDict({"out": "b t h"}, h=3)
+    @property
+    @override(TorchRecurrentModel)
+    def input_spec(self) -> SpecDict:
+        return SpecDict({"in": "b h"}, h=2)
 
+    @property
+    @override(TorchRecurrentModel)
+    def output_spec(self) -> SpecDict:
+        return SpecDict({"out": "b h"}, h=3)
+
+    @override(TorchModel)
     def _forward(self, input):
-        assert torch.all(input["in"] == torch.arange(B * T * 2).reshape(B, T, 2))
-        return TensorDict({"out": torch.arange(B * T * 3).reshape(B, T, 3)})
+        assert torch.all(input["in"] == torch.arange(B * 2).reshape(B, 2))
+        return TensorDict({"out": torch.arange(B * 3).reshape(B, 3)})
 
 
 class IOTorchModel(SimpleModel):
@@ -42,11 +68,12 @@ class IOTorchModel(SimpleModel):
         super().__init__(config=ModelConfig())
         self.weights = nn.Parameter(torch.tensor([value]))
 
+    @override(SimpleModel)
     def _forward(self, input):
         pass
 
 
-class TestTorchIO(unittest.TestCase):
+class TestTorchModel(unittest.TestCase):
     def test_save_load(self):
         """Test saving/restoring model weights"""
         with tempfile.TemporaryDirectory("test_torch_model.cpt") as d:
@@ -55,14 +82,12 @@ class TestTorchIO(unittest.TestCase):
             m.save(path)
             lo = IOTorchModel(value=2.0)
             lo.load(path)
-            self.assertTrue(torch.all(m.weights == lo.weights))
+            check(m.weights, lo.weights)
 
-
-class TestTorchRecurrentModel(unittest.TestCase):
-    def test_init(self):
+    def test_recurrent_init(self):
         SimpleRecurrentModel(config=ModelConfig())
 
-    def test_unroll_and_filter(self):
+    def test_recurrent_unroll_and_filter(self):
         """Ensures unused inputs are filtered out before _unroll and that
         outputs are correct."""
         inputs = TensorDict(
@@ -82,29 +107,28 @@ class TestTorchRecurrentModel(unittest.TestCase):
         desired_states = TensorDict({"out": torch.arange(B * 5).reshape(B, 5)})
 
         for k in outputs.flatten().keys() | desired.flatten().keys():
-            self.assertTrue(torch.all(outputs[k] == desired[k]))
+            check(outputs[k], desired[k])
+
         for k in out_states.flatten().keys() | desired_states.flatten().keys():
-            self.assertTrue(torch.all(out_states[k] == desired_states[k]))
+            check(out_states[k], desired_states[k])
 
-
-class TestTorchModel(unittest.TestCase):
-    def test_init(self):
+    def test_model_init(self):
         SimpleModel(config=ModelConfig())
 
-    def test_unroll_and_filter(self):
-        """Ensures unused inputs are filtered out before _unroll and that
+    def test_model_fwd_and_filter(self):
+        """Ensures unused inputs are filtered out before forward and that
         outputs are correct."""
         inputs = TensorDict(
             {
-                "in": torch.arange(B * T * 2).reshape(B, T, 2),
+                "in": torch.arange(B * 2).reshape(B, 2),
                 "bork": torch.arange(5 * 4).reshape(5, 4),
             }
         )
         outputs, _ = SimpleModel(ModelConfig()).unroll(inputs, TensorDict())
-        desired = TensorDict({"out": torch.arange(B * T * 3).reshape(B, T, 3)})
+        desired = TensorDict({"out": torch.arange(B * 3).reshape(B, 3)})
 
         for k in outputs.flatten().keys() | desired.flatten().keys():
-            self.assertTrue(torch.all(outputs[k] == desired[k]))
+            check(outputs[k], desired[k])
 
 
 if __name__ == "__main__":
