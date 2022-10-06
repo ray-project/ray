8486ddd1bf909449a22262c52a90f8359797fc16
Alan Guo <aguo@anyscale.com>
2022-10-06 13:03:24 -0700

    Make progress bar show as complete for finished jobs. (#29110)

--- dashboard/client/src/pages/job/TaskProgressBar.tsx
+++ dashboard/client/src/pages/job/TaskProgressBar.tsx
@@ -45,6 +45,18 @@ export const TaskProgressBar = ({
   return <ProgressBar progress={progress} />;
 };
 
+export type MiniTaskProgressBarProps = TaskProgress & {
+  /**
+   * Whether to color the entire progress bar as complete.
+   * For example, when the job is complete.
+   */
+  showAsComplete?: boolean;
+  /**
+   * Whether to show tooltip.
+   */
+  showTooltip?: boolean;
+};
+
 export const MiniTaskProgressBar = ({
   numFinished = 0,
   numRunning = 0,
@@ -52,34 +64,65 @@ export const MiniTaskProgressBar = ({
   numPendingNodeAssignment = 0,
   numSubmittedToWorker = 0,
   numUnknown = 0,
-}: TaskProgressBarProps) => {
+  showAsComplete = false,
+  showTooltip = true,
+}: MiniTaskProgressBarProps) => {
   const theme = useTheme<Theme>();
-  const progress: ProgressBarSegment[] = [
-    {
-      label: "Finished",
-      value: numFinished,
-      color: theme.palette.success.main,
-    },
-    {
-      label: "Running",
-      value: numRunning,
-      color: theme.palette.primary.main,
-    },
-    {
-      label: "Waiting for scheduling",
-      value: numPendingNodeAssignment + numSubmittedToWorker,
-      color: "#cfcf08",
-    },
-    {
-      label: "Waiting for dependencies",
-      value: numPendingArgsAvail,
-      color: "#f79e02",
-    },
-    {
-      label: "Unknown",
-      value: numUnknown,
-      color: "#5f6469",
-    },
-  ];
-  return <ProgressBar progress={progress} showLegend={false} showTooltip />;
+  if (showAsComplete) {
+    const total =
+      numFinished +
+      numRunning +
+      numPendingArgsAvail +
+      numPendingNodeAssignment +
+      numSubmittedToWorker +
+      numUnknown;
+    return (
+      <ProgressBar
+        progress={[
+          {
+            label: "Finished",
+            value: total,
+            color: theme.palette.success.main,
+          },
+        ]}
+        showLegend={false}
+        showTooltip={showTooltip}
+      />
+    );
+  } else {
+    const progress: ProgressBarSegment[] = [
+      {
+        label: "Finished",
+        value: numFinished,
+        color: theme.palette.success.main,
+      },
+      {
+        label: "Running",
+        value: numRunning,
+        color: theme.palette.primary.main,
+      },
+      {
+        label: "Waiting for scheduling",
+        value: numPendingNodeAssignment + numSubmittedToWorker,
+        color: "#cfcf08",
+      },
+      {
+        label: "Waiting for dependencies",
+        value: numPendingArgsAvail,
+        color: "#f79e02",
+      },
+      {
+        label: "Unknown",
+        value: numUnknown,
+        color: "#5f6469",
+      },
+    ];
+    return (
+      <ProgressBar
+        progress={progress}
+        showLegend={false}
+        showTooltip={showTooltip}
+      />
+    );
+  }
 };
