cca4d870bb0f2ca8ad66dec13ee43f897ebddd6b
Antoni Baum <antoni.baum@protonmail.com>
2022-10-06 23:30:16 +0000

    Split GPU tests
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>

--- python/ray/train/tests/test_gpu.py
+++ python/ray/train/tests/test_gpu.py
@@ -12,29 +12,14 @@ from torch.nn.parallel import DistributedDataParallel
 from torch.utils.data import DataLoader, DistributedSampler
 
 import ray
-from ray.air import Checkpoint, session
+from ray.air import session
 from ray.cluster_utils import Cluster
 from ray import tune
 
 import ray.train as train
 from ray.air.config import ScalingConfig
-from ray.train.constants import TRAINING_ITERATION, DEFAULT_NCCL_SOCKET_IFNAME
-from ray.train.examples.horovod.horovod_example import (
-    train_func as horovod_torch_train_func,
-)
-from ray.train.examples.tensorflow_mnist_example import (
-    train_func as tensorflow_mnist_train_func,
-)
-from ray.train.examples.torch_fashion_mnist_example import (
-    train_func as fashion_mnist_train_func,
-)
+from ray.train.constants import DEFAULT_NCCL_SOCKET_IFNAME
 from ray.train.examples.torch_linear_example import LinearDataset
-from ray.train.horovod.horovod_trainer import HorovodTrainer
-from ray.train.tests.test_tune import (
-    torch_fashion_mnist,
-    tune_tensorflow_mnist,
-)
-from ray.train.tensorflow.tensorflow_trainer import TensorflowTrainer
 from ray.train.torch.config import TorchConfig, _TorchBackend
 from ray.train.torch.torch_trainer import TorchTrainer
 from ray.train._internal.worker_group import WorkerGroup
@@ -473,88 +458,6 @@ def test_torch_auto_gpu_to_cpu(ray_start_4_cpus_2_gpus):
 
     for tensor in state_dict.values():
         assert not tensor.is_cuda
-
-
-def test_tensorflow_mnist_gpu(ray_start_4_cpus_2_gpus):
-    num_workers = 2
-    epochs = 3
-
-    config = {"lr": 1e-3, "batch_size": 64, "epochs": epochs}
-    trainer = TensorflowTrainer(
-        tensorflow_mnist_train_func,
-        train_loop_config=config,
-        scaling_config=ScalingConfig(num_workers=num_workers, use_gpu=True),
-    )
-    results = trainer.fit()
-
-    result = results.metrics
-
-    assert result[TRAINING_ITERATION] == epochs
-
-
-def test_torch_fashion_mnist_gpu(ray_start_4_cpus_2_gpus):
-    num_workers = 2
-    epochs = 3
-
-    config = {"lr": 1e-3, "batch_size": 64, "epochs": epochs}
-    trainer = TorchTrainer(
-        fashion_mnist_train_func,
-        train_loop_config=config,
-        scaling_config=ScalingConfig(num_workers=num_workers, use_gpu=True),
-    )
-    results = trainer.fit()
-
-    result = results.metrics
-
-    assert result[TRAINING_ITERATION] == epochs
-
-
-def test_horovod_torch_mnist_gpu(ray_start_4_cpus_2_gpus):
-    num_workers = 2
-    num_epochs = 2
-    trainer = HorovodTrainer(
-        horovod_torch_train_func,
-        train_loop_config={"num_epochs": num_epochs, "lr": 1e-3},
-        scaling_config=ScalingConfig(num_workers=num_workers, use_gpu=True),
-    )
-    results = trainer.fit()
-    result = results.metrics
-    assert result[TRAINING_ITERATION] == num_workers
-
-
-def test_horovod_torch_mnist_gpu_checkpoint(ray_start_4_cpus_2_gpus):
-    def checkpointing_func(config):
-        net = torch.nn.Linear(in_features=8, out_features=16)
-        net.to("cuda")
-
-        checkpoint = Checkpoint.from_dict({"model": net.state_dict()})
-        session.report({"metric": 1}, checkpoint=checkpoint)
-
-    num_workers = 2
-    trainer = HorovodTrainer(
-        checkpointing_func,
-        scaling_config=ScalingConfig(num_workers=num_workers, use_gpu=True),
-    )
-    result = trainer.fit()
-    assert not result.error
-
-
-def test_tune_fashion_mnist_gpu(ray_start_4_cpus_2_gpus):
-    torch_fashion_mnist(num_workers=2, use_gpu=True, num_samples=1)
-
-
-def test_concurrent_tune_fashion_mnist_gpu(ray_start_4_cpus_2_gpus):
-    torch_fashion_mnist(num_workers=1, use_gpu=True, num_samples=2)
-
-
-def test_tune_tensorflow_mnist_gpu(ray_start_4_cpus_2_gpus):
-    tune_tensorflow_mnist(num_workers=2, use_gpu=True, num_samples=1)
-
-
-def test_train_linear_dataset_gpu(ray_start_4_cpus_2_gpus):
-    from ray.air.examples.pytorch.torch_regression_example import train_regression
-
-    assert train_regression(num_workers=2, use_gpu=True)
 
 
 if __name__ == "__main__":
