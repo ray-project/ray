d99eff919bf785f911e4eebc87ddc4960344a139
Antoni Baum <antoni.baum@protonmail.com>
2022-10-08 04:31:54 +0200

    [AIR] Hard deprecate old Trainer, old callbacks (#29015)
    Hard deprecations for ray.train.Trainer, ray.train.callbacks and ray.train.checkpoint.CheckpointStrategy. Restart-on-failure logic from BackendExecutor has also been removed as it is superseded by Tune.
    
    Some tests have been refactored to use the new API. Tests that are no longer applicable have been removed.
    
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>
    Signed-off-by: Amog Kamsetty <amogkamsetty@yahoo.com>
    Co-authored-by: Amog Kamsetty <amogkamsetty@yahoo.com>

--- python/ray/train/tests/test_torch_trainer.py
+++ python/ray/train/tests/test_torch_trainer.py
@@ -1,6 +1,7 @@
 import pytest
 from ray.air import session
 from ray.air.checkpoint import Checkpoint
+from ray.train.torch.torch_checkpoint import TorchCheckpoint
 import torch
 
 import ray
@@ -176,6 +177,79 @@ def test_tune_torch_get_device_gpu(
 
     actors = [TrialActor.remote(_) for _ in range(num_samples)]
     ray.get([actor.run.remote() for actor in actors])
+
+
+def test_torch_auto_unwrap(ray_start_4_cpus):
+    """Tests if underlying model from DDP is extracted when saving ckpt."""
+
+    def train_fn():
+        model = torch.nn.Linear(1, 1)
+
+        # Wrap in DDP.
+        model = train.torch.prepare_model(model)
+
+        # Save DDP wrapped model.
+        session.report({"model": model}, checkpoint=TorchCheckpoint.from_model(model))
+
+    trainer = TorchTrainer(
+        train_loop_per_worker=train_fn,
+        scaling_config=ScalingConfig(num_workers=2),
+    )
+    results = trainer.fit()
+
+    last_checkpoint = results.checkpoint
+    model = last_checkpoint.get_model()
+    assert isinstance(model, torch.nn.Module) and not isinstance(
+        model, torch.nn.parallel.DistributedDataParallel
+    )
+
+    model_report = results.metrics["model"]
+    assert isinstance(model_report, torch.nn.Module) and not isinstance(
+        model_report, torch.nn.parallel.DistributedDataParallel
+    )
+
+
+def test_torch_amp(ray_start_4_cpus):
+    def train_fn():
+        train.torch.accelerate(amp=True)
+        model = torch.nn.Linear(1, 1)
+        model = train.torch.prepare_model(model)
+
+        session.report({}, checkpoint=TorchCheckpoint.from_model(model))
+
+    trainer = TorchTrainer(
+        train_fn,
+        scaling_config=ScalingConfig(num_workers=2),
+    )
+    results = trainer.fit()
+    assert results.checkpoint
+
+
+def test_torch_amp_with_custom_get_state(ray_start_4_cpus):
+    """Tests amp with a model that has a custom __getstate__ method defined.
+
+    See https://discuss.ray.io/t/ray-train-hangs-for-long-time/6333/7
+    """
+
+    def train_fn():
+        train.torch.accelerate(amp=True)
+
+        class CustomLinear(torch.nn.Linear):
+            def __getstate__(self):
+                return self.__dict__.copy()
+
+        model = CustomLinear(1, 1)
+        model = train.torch.prepare_model(model)
+
+        # Make sure model is serializable even with amp enabled.
+        session.report({}, checkpoint=TorchCheckpoint.from_model(model))
+
+    trainer = TorchTrainer(
+        train_fn,
+        scaling_config=ScalingConfig(num_workers=2),
+    )
+    results = trainer.fit()
+    assert results.checkpoint
 
 
 if __name__ == "__main__":
