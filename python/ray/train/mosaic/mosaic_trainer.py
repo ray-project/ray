cfa593a0a0859a20989a6f24d72b0e8c2b0e74c8
ilee300a <ilee300@anyscale.com>
2022-10-11 14:44:27 -0700

    created a basic composer trainer wrapper and a test
    Signed-off-by: ilee300a <ilee300@anyscale.com>

---
+++ python/ray/train/mosaic/mosaic_trainer.py
@@ -0,0 +1,124 @@
+import inspect
+import os
+
+from ray.air import session
+from ray.train.constants import (
+    EVALUATION_DATASET_KEY,
+    TRAIN_DATASET_KEY,
+)
+
+from typing import TYPE_CHECKING, Callable, Dict, Optional
+from ray.air.checkpoint import Checkpoint
+from ray.air.config import DatasetConfig, RunConfig, ScalingConfig
+
+from ray.train.torch import TorchConfig, TorchTrainer
+from ray.train.trainer import GenDataset
+from ray.util import PublicAPI
+
+if TYPE_CHECKING:
+    from ray.data.preprocessor import Preprocessor
+
+import composer.trainer
+
+@PublicAPI(stability="alpha")
+class MosaicTrainer(TorchTrainer):
+    """A Trainer for data parallel Mosaic Composers on PyTorch training.
+
+    This Trainer runs the ``composer.trainer.Trainer.fit()`` method on multiple
+    Ray Actors. The training is carried out in a distributed fashion through PyTorch
+    DDP. These actors already have the necessary torch process group already
+    configured for distributed PyTorch training.
+
+    The training function ran on every Actor will first run the
+    specified ``trainer_init_per_worker`` function to obtain an instantiated
+    ``composer.Trainer`` object. The ``trainer_init_per_worker`` function
+    will have access to preprocessed train and evaluation datasets.
+
+    Args:
+        trainer_init_per_worker: The function that returns an instantiated
+            ``composer.Trainer`` object and takes in an optional configuration
+            dictionary as an argument.
+        datasets: Any Ray Datasets to use for training.
+        trainer_init_config: Configurations to pass into ``trainer_init_per_worker`` as
+            kwargs.
+        torch_config: Configuration for setting up the PyTorch backend. If set to
+            None, use the default configuration. This replaces the ``backend_config``
+            arg of ``DataParallelTrainer``. Same as in ``TorchTrainer``.
+        scaling_config: Configuration for how to scale data parallel training.
+        dataset_config: Configuration for dataset ingest.
+        run_config: Configuration for the execution of the training run.
+        preprocessor: A ray.data.Preprocessor to preprocess the
+            provided datasets.
+        resume_from_checkpoint: A MosiacCheckpoint to resume training from.
+    """
+
+    def __init__(
+        self,
+        trainer_init_per_worker: Callable[[Optional[Dict]], composer.trainer.Trainer],
+        *,
+        datasets: Optional[Dict[str, GenDataset]] = None,
+        trainer_init_config: Optional[Dict] = None,
+        torch_config: Optional[TorchConfig] = None,
+        scaling_config: Optional[ScalingConfig] = None,
+        dataset_config: Optional[Dict[str, DatasetConfig]] = None,
+        run_config: Optional[RunConfig] = None,
+        preprocessor: Optional["Preprocessor"] = None,
+        resume_from_checkpoint: Optional[Checkpoint] = None,
+    ):
+
+        self._validate_trainer_init_per_worker(
+            trainer_init_per_worker, "trainer_init_per_worker"
+        )
+
+        trainer_init_config = trainer_init_config.copy() if trainer_init_config else {}
+        if "_trainer_init_per_worker" in trainer_init_config:
+            raise ValueError(
+                "'_trainer_init_per_worker' is a reserved key in `trainer_init_config`."
+            )
+        trainer_init_config["_trainer_init_per_worker"] = trainer_init_per_worker
+
+        super().__init__(
+            train_loop_per_worker=_mosaic_train_loop_per_worker,
+            train_loop_config=trainer_init_config,
+            torch_config=torch_config,
+            scaling_config=scaling_config,
+            dataset_config=dataset_config,
+            run_config=run_config,
+            datasets=datasets,
+            preprocessor=preprocessor,
+            resume_from_checkpoint=resume_from_checkpoint,
+        )
+
+    def _validate_trainer_init_per_worker(
+        self, trainer_init_per_worker: Callable, fn_name: str
+    ) -> None:
+        num_params = len(inspect.signature(trainer_init_per_worker).parameters)
+        if num_params > 1:
+            raise ValueError(
+                f"{fn_name} should take in at most 1 argument, "
+                f"but it accepts {num_params} arguments instead."
+            )
+
+
+def _mosaic_train_loop_per_worker(config):
+    """Per-worker training loop for Mosaic Composers."""
+    trainer_init_per_worker = config.pop("_trainer_init_per_worker")
+
+    os.environ["RANK"] = str(session.get_world_rank())
+    os.environ["WORLD_SIZE"] = str(session.get_world_size())
+    os.environ["LOCAL_RANK"] = str(session.get_local_rank())
+
+    # Arbitrary values set for these as they are needed for some composer functions
+    os.environ["LOCAL_WORLD_SIZE"] = os.environ["WORLD_SIZE"]
+    os.environ["NODE_RANK"] = "0"
+
+    # get dataset shard -- For now we don't support using ray dataset shards.
+    # the dataloader should be prepared inside the ``trainer_init_per_worker`` function
+    train_dataset = session.get_dataset_shard(TRAIN_DATASET_KEY)
+    eval_dataset = session.get_dataset_shard(EVALUATION_DATASET_KEY)
+
+    # initialize Composer trainer
+    trainer: composer.trainer.Trainer = trainer_init_per_worker(**config)
+
+    # call the trainer
+    trainer.fit()
---
+++ python/ray/train/tests/test_mosaic_trainer.py
@@ -0,0 +1,123 @@
+import pytest
+
+from pathlib import Path
+import os
+
+# torch libraries
+import torch
+import torch.utils.data
+
+# torchvision libraries
+import torchvision
+from torchvision import transforms, datasets
+
+# Import Ray libraries
+import ray
+from ray.air.config import ScalingConfig
+import ray.train as train
+from ray.air import session
+
+# import ray-mosaic integration libraries
+from ray.train.mosaic import MosaicTrainer
+
+# import composer training libraries
+from torchmetrics.classification.accuracy import Accuracy
+from composer.core.evaluator import Evaluator
+from composer.models.tasks import ComposerClassifier
+import composer.optim
+from composer.loggers import InMemoryLogger
+from composer.algorithms import LabelSmoothing
+
+
+mean = (0.507, 0.487, 0.441)
+std = (0.267, 0.256, 0.276)
+cifar10_transforms = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize(mean, std)]
+)
+# data_directory = "./data" ## TODO : remove the following line
+data_directory = "~/Desktop/workspace/data"
+train_dataset = datasets.CIFAR10(
+    data_directory, train=True, download=True, transform=cifar10_transforms
+)
+test_dataset = datasets.CIFAR10(
+    data_directory, train=False, download=True, transform=cifar10_transforms
+)
+
+scaling_config = ScalingConfig(num_workers=2, use_gpu=False)
+
+@pytest.fixture(autouse=True, scope="session")
+def ray_start_4_cpus():
+    address_info = ray.init(num_cpus=4)
+    yield address_info
+    # The code after the yield will run as teardown code.
+    ray.shutdown()
+
+
+def trainer_init_per_worker(**config):
+    BATCH_SIZE = 1024
+    # prepare the model for distributed training and wrap with ComposerClassifier for
+    # Composer Trainer compatibility
+    model = config.pop("model", torchvision.models.resnet18(num_classes=10))
+    model = ComposerClassifier(ray.train.torch.prepare_model(model))
+
+    # prepare train/test dataset
+    train_dataset = config.pop("train_dataset")
+    test_dataset = config.pop("test_dataset")
+
+    batch_size_per_worker = BATCH_SIZE // session.get_world_size()
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size_per_worker, shuffle=True
+    )
+    test_dataloader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=batch_size_per_worker, shuffle=True
+    )
+
+    train_dataloader = train.torch.prepare_data_loader(train_dataloader)
+    test_dataloader = train.torch.prepare_data_loader(test_dataloader)
+
+    evaluator = Evaluator(
+        dataloader=test_dataloader, label="my_evaluator", metrics=Accuracy()
+    )
+
+    # prepare optimizer
+    optimizer = composer.optim.DecoupledSGDW(
+        model.parameters(),
+        lr=0.05,
+        momentum=0.9,
+        weight_decay=2.0e-3,
+    )
+
+    if config.pop("eval", False):
+        config["eval_dataloader"] = evaluator
+
+    return composer.trainer.Trainer(
+        model=model, train_dataloader=train_dataloader, optimizers=optimizer, **config
+    )
+
+
+trainer_init_per_worker.__test__ = False
+
+def test_mosaic_e2e():
+    """Tests if the basic MosaicTrainer with minimum configuration runs and reports correct
+    Checkpoint dictionary.
+    """
+    trainer_init_config = {
+        "max_duration": "1ba",
+        "train_dataset": train_dataset,
+        "test_dataset": test_dataset,
+        "loggers": [InMemoryLogger()],
+        "algorithms": [LabelSmoothing()]
+    }
+
+    trainer = MosaicTrainer(
+        trainer_init_per_worker=trainer_init_per_worker,
+        trainer_init_config=trainer_init_config,
+        scaling_config=scaling_config,
+    )
+
+    trainer.fit()
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(pytest.main(["-v", "-x", __file__]))
