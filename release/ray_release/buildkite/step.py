d8c9f210ef624af7aeb63f0234124814e5c7ed1f
Kai Fricke <kai@anyscale.com>
2022-10-12 11:57:48 -0700

    default queue name
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- release/ray_release/buildkite/step.py
+++ release/ray_release/buildkite/step.py
@@ -20,10 +20,10 @@ from ray_release.util import python_version_str, DeferredEnvVar
 DEFAULT_ARTIFACTS_DIR_HOST = "/tmp/ray_release_test_artifacts"
 
 RELEASE_QUEUE_DEFAULT = DeferredEnvVar(
-    "RELEASE_QUEUE_DEFAULT", "runner_queue_tiny_branch"
+    "RELEASE_QUEUE_DEFAULT", "release_queue_small"
 )
 RELEASE_QUEUE_CLIENT = DeferredEnvVar(
-    "RELEASE_QUEUE_CLIENT", "runner_queue_small_branch"
+    "RELEASE_QUEUE_CLIENT", "release_queue_small"
 )
 
 DEFAULT_STEP_TEMPLATE: Dict[str, Any] = {
