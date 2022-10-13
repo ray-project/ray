d21ca2a0fa27a810682bdc4154277d59cc3f23c4
rickyyx <rickyx@anyscale.com>
2022-10-13 23:45:36 +0000

    trigger ci
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- python/ray/_private/utils.py
+++ python/ray/_private/utils.py
@@ -1576,8 +1576,8 @@ def get_or_create_event_loop() -> asyncio.BaseEventLoop:
     vers_info = sys.version_info
     if vers_info.major >= 3 and vers_info.minor >= 10:
         # This follows the implementation of the deprecating `get_event_loop`
-        # in python3.10's asyncio.
-        # See python3.10/asyncio/events.py::_get_event_loop()
+        # in python3.10's asyncio. See python3.10/asyncio/events.py 
+        # _get_event_loop()
         loop = None
         try:
             loop = asyncio.get_running_loop()
