cebb21591a5439b8cfd0becbf08f54a2cbb43b8e
Michael Mui <heyitsmui@gmail.com>
2022-10-14 02:59:49 +0200

    remove schema validation and let library error surface, add api to dataset_pipeline, and update docs
    Signed-off-by: Michael Mui <heyitsmui@gmail.com>

--- python/ray/data/tests/test_dataset.py
+++ python/ray/data/tests/test_dataset.py
@@ -2225,52 +2225,40 @@ def test_drop_columns(ray_start_regular_shared, tmp_path):
 
 
 def test_select_columns(ray_start_regular_shared):
-
-    # Test pandas
+    # Test pandas and arrow
     df = pd.DataFrame({"col1": [1, 2, 3], "col2": [2, 3, 4], "col3": [3, 4, 5]})
-    ds = ray.data.from_pandas(df)
-    assert ds._dataset_format() == "pandas"
-    assert ds.select_columns(columns=[]).take(1) == [{}]
-    assert ds.select_columns(columns=["col1", "col2", "col3"]).take(1) == [
-        {"col1": 1, "col2": 2, "col3": 3}
-    ]
-    assert ds.select_columns(columns=["col1", "col2"]).take(1) == [
-        {"col1": 1, "col2": 2}
-    ]
-    assert ds.select_columns(columns=["col2", "col1"]).take(1) == [
-        {"col1": 1, "col2": 2}
-    ]
-    assert ds.select_columns(columns=["col1", "col2", "col2"]).schema().names == [
-        "col1",
-        "col2",
-        "col2",
-    ]
-    with pytest.raises(ValueError):
-        ds.select_columns(columns=["col1", "col2", "dummy_col"])
+    ds1 = ray.data.from_pandas(df)
+    assert ds1._dataset_format() == "pandas"
 
-    # Test arrow
-    ds = ds.map_batches(lambda pa: pa, batch_size=1, batch_format="pyarrow")
-    assert ds._dataset_format() == "arrow"
-    assert ds.select_columns(columns=[]).take(1) == [{}]
-    assert ds.select_columns(columns=["col1", "col2", "col3"]).take(1) == [
-        {"col1": 1, "col2": 2, "col3": 3}
-    ]
-    assert ds.select_columns(columns=["col1", "col2"]).take(1) == [
-        {"col1": 1, "col2": 2}
-    ]
-    assert ds.select_columns(columns=["col1", "col2", "col2"]).schema().names == [
-        "col1",
-        "col2",
-        "col2",
-    ]
-    with pytest.raises(ValueError):
-        ds.select_columns(columns=["col1", "col2", "dummy_col"])
+    ds2 = ds1.map_batches(lambda pa: pa, batch_size=1, batch_format="pyarrow")
+    assert ds2._dataset_format() == "arrow"
+
+    for each_ds in [ds1, ds2]:
+        assert each_ds.select_columns(cols=[]).take(1) == [{}]
+        assert each_ds.select_columns(cols=["col1", "col2", "col3"]).take(1) == [
+            {"col1": 1, "col2": 2, "col3": 3}
+        ]
+        assert each_ds.select_columns(cols=["col1", "col2"]).take(1) == [
+            {"col1": 1, "col2": 2}
+        ]
+        assert each_ds.select_columns(cols=["col2", "col1"]).take(1) == [
+            {"col1": 1, "col2": 2}
+        ]
+        # Test selecting columns with duplicates
+        assert each_ds.select_columns(cols=["col1", "col2", "col2"]).schema().names == [
+            "col1",
+            "col2",
+            "col2",
+        ]
+        # Test selecting a column that is not in the dataset schema
+        with pytest.raises(KeyError):
+            each_ds.select_columns(cols=["col1", "col2", "dummy_col"])
 
     # Test simple
-    ds = ray.data.range(10)
-    assert ds._dataset_format() == "simple"
+    ds3 = ray.data.range(10)
+    assert ds3._dataset_format() == "simple"
     with pytest.raises(ValueError):
-        ds.select_columns(columns=[])
+        ds3.select_columns(cols=[])
 
 
 def test_map_batches_basic(ray_start_regular_shared, tmp_path):
