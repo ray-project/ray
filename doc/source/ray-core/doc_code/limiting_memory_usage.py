92ac7ed4c7abdf45bee4627825b6827a89790263
Clarence Ng <clarence.wyng@gmail.com>
2022-10-13 20:36:57 -0700

    [core] memory monitor docs
    Signed-off-by: Clarence Ng <clarence.wyng@gmail.com>

--- doc/source/ray-core/doc_code/limiting_memory_usage.py
+++ doc/source/ray-core/doc_code/limiting_memory_usage.py
@@ -1,13 +1,18 @@
+# flake8: noqa
+
+
 # fmt: off
 # __oom_start__
 import ray
 
+
 @ray.remote
 def allocate_memory():
     chunks = []
-    bits_to_allocate = 8 * 1024 * 1024 * 1024 # 1 GiB
+    bits_to_allocate = 8 * 1024 * 1024 * 1024  # 1 GiB
     while True:
         chunks.append([0] * bits_to_allocate)
+
 
 ray.get(allocate_memory.remote())
 # __oom_end__
@@ -18,16 +23,24 @@ ray.get(allocate_memory.remote())
 # __two_actors_start__
 from math import ceil
 import ray
-from ray._private.utils import get_system_memory # do not use outside of this example as these are private methods.
-from ray._private.utils import get_used_memory # do not use outside of this example as these are private methods.
+from ray._private.utils import (
+    get_system_memory,
+)  # do not use outside of this example as these are private methods.
+from ray._private.utils import (
+    get_used_memory,
+)  # do not use outside of this example as these are private methods.
+
 
 # estimates the number of bytes to allocate to reach the desired memory usage percentage.
 def get_additional_bytes_to_reach_memory_usage_pct(pct: float) -> int:
     used = get_used_memory()
     total = get_system_memory()
     bytes_needed = int(total * pct) - used
-    assert bytes_needed > 0, "memory usage is already above the target. Increase the target percentage."
+    assert (
+        bytes_needed > 0
+    ), "memory usage is already above the target. Increase the target percentage."
     return bytes_needed
+
 
 @ray.remote
 class MemoryHogger:
@@ -39,12 +52,17 @@ class MemoryHogger:
         new_list = [0] * ceil(bytes_to_allocate / 8)
         self.allocations.append(new_list)
 
-first_actor = MemoryHogger.options(max_restarts=1, max_task_retries=1, name="first_actor").remote()
-second_actor = MemoryHogger.options(max_restarts=0, max_task_retries=0, name="second_actor").remote()
+
+first_actor = MemoryHogger.options(
+    max_restarts=1, max_task_retries=1, name="first_actor"
+).remote()
+second_actor = MemoryHogger.options(
+    max_restarts=0, max_task_retries=0, name="second_actor"
+).remote()
 
 # each task requests 0.3 of the system memory when the memory threshold is 0.4.
 allocate_bytes = get_additional_bytes_to_reach_memory_usage_pct(0.3)
-      
+
 first_actor_task = first_actor.allocate.remote(allocate_bytes)
 second_actor_task = second_actor.allocate.remote(allocate_bytes)
 
@@ -64,8 +82,12 @@ print("finished second actor")
 
 # fmt: off
 # __set_num_cpus_summary_start__
-first_actor = MemoryHogger.options(max_restarts=1, max_task_retries=1, name="first_actor", num_cpus=2).remote()
-second_actor = MemoryHogger.options(max_restarts=0, max_task_retries=0, name="second_actor", num_cpus=2).remote()
+first_actor = MemoryHogger.options(
+    max_restarts=1, max_task_retries=1, name="first_actor", num_cpus=2
+).remote()
+second_actor = MemoryHogger.options(
+    max_restarts=0, max_task_retries=0, name="second_actor", num_cpus=2
+).remote()
 # __set_num_cpus_summary_end__
 # fmt: on
 
@@ -74,16 +96,23 @@ second_actor = MemoryHogger.options(max_restarts=0, max_task_retries=0, name="se
 # __set_num_cpus_start__
 from math import ceil
 import ray
-from ray._private.utils import get_system_memory # do not use outside of this example as these are private methods.
-from ray._private.utils import get_used_memory # do not use outside of this example as these are private methods.
+from ray._private.utils import (
+    get_system_memory,
+)  # do not use outside of this example as these are private methods.
+from ray._private.utils import (
+    get_used_memory,
+)  # do not use outside of this example as these are private methods.
 
 # estimates the number of bytes to allocate to reach the desired memory usage percentage.
 def get_additional_bytes_to_reach_memory_usage_pct(pct: float) -> int:
     used = get_used_memory()
     total = get_system_memory()
     bytes_needed = int(total * pct) - used
-    assert bytes_needed > 0, "memory usage is already above the target. Increase the target percentage."
+    assert (
+        bytes_needed > 0
+    ), "memory usage is already above the target. Increase the target percentage."
     return bytes_needed
+
 
 @ray.remote
 class MemoryHogger:
@@ -95,12 +124,17 @@ class MemoryHogger:
         new_list = [0] * ceil(bytes_to_allocate / 8)
         self.allocations.append(new_list)
 
-first_actor = MemoryHogger.options(max_restarts=1, max_task_retries=1, name="first_actor", num_cpus=2).remote()
-second_actor = MemoryHogger.options(max_restarts=0, max_task_retries=0, name="second_actor", num_cpus=2).remote()
+
+first_actor = MemoryHogger.options(
+    max_restarts=1, max_task_retries=1, name="first_actor", num_cpus=2
+).remote()
+second_actor = MemoryHogger.options(
+    max_restarts=0, max_task_retries=0, name="second_actor", num_cpus=2
+).remote()
 
 # each task requests 0.3 of the system memory when the memory threshold is 0.4.
 allocate_bytes = get_additional_bytes_to_reach_memory_usage_pct(0.3)
-      
+
 first_actor_task = first_actor.allocate.remote(allocate_bytes)
 second_actor_task = second_actor.allocate.remote(allocate_bytes)
 
@@ -119,8 +153,12 @@ print("finished second actor")
 # each task requests 0.55 of the system memory when the memory threshold is 0.8.
 allocate_bytes = get_additional_bytes_to_reach_memory_usage_pct(0.55)
 
-first_actor = MemoryHogger.options(max_restarts=1, max_task_retries=1, name="first_actor", memory=allocate_bytes).remote()
-second_actor = MemoryHogger.options(max_restarts=0, max_task_retries=0, name="second_actor", memory=allocate_bytes).remote()
+first_actor = MemoryHogger.options(
+    max_restarts=1, max_task_retries=1, name="first_actor", memory=allocate_bytes
+).remote()
+second_actor = MemoryHogger.options(
+    max_restarts=0, max_task_retries=0, name="second_actor", memory=allocate_bytes
+).remote()
 # __memory_summary_end__
 # fmt: on
 
@@ -129,16 +167,24 @@ second_actor = MemoryHogger.options(max_restarts=0, max_task_retries=0, name="se
 # __set_memory_start__
 from math import ceil
 import ray
-from ray._private.utils import get_system_memory # do not use outside of this example as these are private methods.
-from ray._private.utils import get_used_memory # do not use outside of this example as these are private methods.
+from ray._private.utils import (
+    get_system_memory,
+)  # do not use outside of this example as these are private methods.
+from ray._private.utils import (
+    get_used_memory,
+)  # do not use outside of this example as these are private methods.
+
 
 # estimates the number of bytes to allocate to reach the desired memory usage percentage.
 def get_additional_bytes_to_reach_memory_usage_pct(pct: float) -> int:
     used = get_used_memory()
     total = get_system_memory()
     bytes_needed = int(total * pct) - used
-    assert bytes_needed > 0, "memory usage is already above the target. Increase the target percentage."
+    assert (
+        bytes_needed > 0
+    ), "memory usage is already above the target. Increase the target percentage."
     return bytes_needed
+
 
 @ray.remote
 class MemoryHogger:
@@ -150,11 +196,16 @@ class MemoryHogger:
         new_list = [0] * ceil(bytes_to_allocate / 8)
         self.allocations.append(new_list)
 
+
 # each task requests 0.55 of the system memory when the memory threshold is 0.8.
 allocate_bytes = get_additional_bytes_to_reach_memory_usage_pct(0.55)
 
-first_actor = MemoryHogger.options(max_restarts=1, max_task_retries=1, name="first_actor", memory=allocate_bytes).remote()
-second_actor = MemoryHogger.options(max_restarts=0, max_task_retries=0, name="second_actor", memory=allocate_bytes).remote()
+first_actor = MemoryHogger.options(
+    max_restarts=1, max_task_retries=1, name="first_actor", memory=allocate_bytes
+).remote()
+second_actor = MemoryHogger.options(
+    max_restarts=0, max_task_retries=0, name="second_actor", memory=allocate_bytes
+).remote()
 
 first_actor_task = first_actor.allocate.remote(allocate_bytes)
 second_actor_task = second_actor.allocate.remote(allocate_bytes)
