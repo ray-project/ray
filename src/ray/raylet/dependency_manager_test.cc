d142efedbcb303b32fde49dca8068d6826acb46f
Eric Liang <ekhliang@gmail.com>
2022-10-18 16:19:09 -0700

    lint
    Signed-off-by: Eric Liang <ekhliang@gmail.com>

--- src/ray/raylet/dependency_manager_test.cc
+++ src/ray/raylet/dependency_manager_test.cc
@@ -56,9 +56,7 @@ class MockObjectManager : public ObjectManagerInterface {
            active_task_requests.count(request_id);
   }
 
-  int64_t PullManagerNumInactivePulls(const std::string& name) const {
-    return 0;
-  }
+  int64_t PullManagerNumInactivePulls(const std::string &name) const { return 0; }
 
   uint64_t req_id = 1;
   std::unordered_set<uint64_t> active_get_requests;
