ab285b61ef60998372ed4687ad80abaeab6c50a9
Artur Niederfahrenhorst <artur@anyscale.com>
2022-10-18 10:26:47 -0700

    lint
    Signed-off-by: Artur Niederfahrenhorst <artur@anyscale.com>

--- rllib/evaluation/tests/test_env_runner_v2.py
+++ rllib/evaluation/tests/test_env_runner_v2.py
@@ -17,7 +17,7 @@ register_env("basic_multiagent", lambda _: BasicMultiAgent(2))
 class TestEnvRunnerV2(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        ray.init()
+        ray.init(local_mode=True)
 
         # When dealing with two policies in these tests, simply alternate between the 2
         # policies to make sure we have data for inference for both policies for each
