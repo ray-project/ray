ae8a3361c42b310ee748757355255e66181bfb9f
Kai Fricke <kai@anyscale.com>
2022-10-18 17:34:10 -0700

    Bump tf + tf-estimator
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- ci/build/build-docker-images.py
+++ ci/build/build-docker-images.py
@@ -361,6 +361,7 @@ def prep_ray_ml():
         "python/requirements.txt",
         "python/requirements_test.txt",
         "python/requirements/ml/requirements_ml_docker.txt",
+        "python/requirements/ml/requirements_dl.txt",
         "python/requirements/ml/requirements_tune.txt",
         "python/requirements/ml/requirements_rllib.txt",
         "python/requirements/ml/requirements_train.txt",
