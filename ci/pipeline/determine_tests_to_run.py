9f15e730119db4bf0f690793aa6743b1ff66543d
Antoni Baum <antoni.baum@protonmail.com>
2022-10-19 23:26:08 +0200

    [CI] Parallelize Tune test runners (#29365)
    This PR moves our manual test sharding to use the native bazel system, removes the now unnecessary `tests_dir_[A-Z]` tags from the Tune BUILD file, adjusts some timeouts to match bazel suggestions and makes it so that changes to Tune are considered to affect AIR (as Tune is AIR's execution engine).
    
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>
    Signed-off-by: Kai Fricke <kai@anyscale.com>
    Co-authored-by: Kai Fricke <kai@anyscale.com>
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- ci/pipeline/determine_tests_to_run.py
+++ ci/pipeline/determine_tests_to_run.py
@@ -158,6 +158,7 @@ if __name__ == "__main__":
                 RAY_CI_LINUX_WHEELS_AFFECTED = 1
                 RAY_CI_MACOS_WHEELS_AFFECTED = 1
             elif changed_file.startswith("python/ray/tune"):
+                RAY_CI_ML_AFFECTED = 1
                 RAY_CI_DOC_AFFECTED = 1
                 RAY_CI_TUNE_AFFECTED = 1
                 RAY_CI_RLLIB_AFFECTED = 1
