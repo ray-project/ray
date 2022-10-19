260a0a70b2d56500c918dbf6ba023f444f48ff1c
Weichen Xu <weichen.xu@databricks.com>
2022-10-19 16:11:28 +0800

    use active spark session
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- python/ray/spark/utils.py
+++ python/ray/spark/utils.py
@@ -161,7 +161,13 @@ def check_port_open(host, port):
 
 def get_spark_session():
     from pyspark.sql import SparkSession
-    return SparkSession.builder.getOrCreate()
+    spark_session = SparkSession.getActiveSession()
+    if spark_session is None:
+        raise RuntimeError(
+            "Spark session haven't been initiated yet. Please use `SparkSession.builder` to "
+            "create a spark session and connect to a spark cluster."
+        )
+    return spark_session
 
 
 def get_spark_application_driver_host(spark):
