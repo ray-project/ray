17999c6eddde928af72dc1bcab2295819c5b3194
sunkunjian1 <sunkunjian1@jd.com>
2022-10-20 19:54:42 +0800

    refactor SetupJavaWorkerTest.java
    Signed-off-by: sunkunjian1 <sunkunjian1@jd.com>

--- java/test/src/main/java/io/ray/test/SetupJavaWorkerTest.java
+++ java/test/src/main/java/io/ray/test/SetupJavaWorkerTest.java
@@ -3,10 +3,14 @@ package io.ray.test;
 import io.ray.api.ActorHandle;
 import io.ray.api.Ray;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 @Test
-public class SetupJavaWorkerTest {
+public class SetupJavaWorkerTest extends BaseTest {
+
+  private static final String CODE_SEARCH_PATH = System.getProperty("java.class.path");;
 
   public static class Counter {
 
@@ -21,23 +25,27 @@ public class SetupJavaWorkerTest {
     }
   }
 
+  @BeforeClass
+  public void setup() {
+    // mock a fake code search path contains whitespace
+    String newCodeSearchPathStr = CODE_SEARCH_PATH + ":fakepathStart fakePathEnd";
+    System.setProperty("java.class.path", newCodeSearchPathStr);
+  }
+
+  @AfterClass
+  public void clean() {
+    // restore class path
+    System.setProperty("java.class.path", CODE_SEARCH_PATH);
+  }
   /**
    * raylet will call setup_worker.py with arguments to start java workers after Ray 2.0.0, and
-   * setup_worker.py can not start java workers correctly when argumens contian space. This test
-   * case is used to test whether the bug is fixed.
+   * setup_worker.py can not start java workers correctly when argumens contian whitespace. This
+   * test case is used to test whether the bug is fixed.
    */
-  public void testCreateActorFail()
-      throws InterruptedException, NoSuchFieldException, IllegalAccessException {
-    String oldCodeSearchPathStr = System.getProperty("java.class.path");
-    // mock a fake code search path contains space
-    String newCodeSearchPathStr = oldCodeSearchPathStr + ":fakepathStart fakePathEnd";
-    System.setProperty("java.class.path", newCodeSearchPathStr);
-    Ray.init();
+  public void testCreateActorFail() {
     ActorHandle<ActorTest.Counter> actor = Ray.actor(ActorTest.Counter::new, 1).remote();
     // throw RayTimeoutException exception if actor is not started correctly by raylet
     Integer value = actor.task(ActorTest.Counter::getValue).remote().get(3 * 1000);
     Assert.assertEquals(Integer.valueOf(1), value);
-    System.setProperty("java.class.path", oldCodeSearchPathStr);
-    Ray.shutdown();
   }
 }
