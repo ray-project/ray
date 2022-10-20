5c54b3bde798d43ed4c9434297703d8eec5eadb2
Clarence Ng <clarence.wyng@gmail.com>
2022-10-20 13:38:40 -0700

    [core] memory monitor documentation
    Signed-off-by: Clarence Ng <clarence.wyng@gmail.com>

--- doc/source/ray-core/doc_code/ray_oom_prevention.py
+++ doc/source/ray-core/doc_code/ray_oom_prevention.py
@@ -15,7 +15,7 @@ import ray
 @ray.remote
 def allocate_memory():
     chunks = []
-    bits_to_allocate = 8 * 1024 * 1024 * 1024  # 1 GiB
+    bits_to_allocate = 8 * 100 * 1024 * 1024  # ~0.1 GiB
     while True:
         chunks.append([0] * bits_to_allocate)
 
