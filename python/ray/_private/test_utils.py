bf24b124d25ea9f2033f076e2444a961f3d888a1
Alan Guo <aguo@anyscale.com>
2022-10-20 12:39:27 -0700

    Update unit test to test the same guage, different tags case
    Signed-off-by: Alan Guo <aguo@anyscale.com>

--- python/ray/_private/test_utils.py
+++ python/ray/_private/test_utils.py
@@ -908,6 +908,16 @@ def object_memory_usage() -> bool:
     avail = ray.available_resources().get("object_store_memory", 0)
     return total - avail
 
+def fetch_raw_prometheus(prom_addresses):
+    # Local import so minimal dependency tests can run without requests
+    import requests
+
+    for address in prom_addresses:
+        try:
+            response = requests.get(f"http://{address}/metrics")
+            yield address, response.text
+        except requests.exceptions.ConnectionError:
+            continue
 
 def fetch_prometheus(prom_addresses):
     # Local import so minimal dependency tests can run without requests
@@ -916,15 +926,13 @@ def fetch_prometheus(prom_addresses):
     components_dict = {}
     metric_names = set()
     metric_samples = []
+
     for address in prom_addresses:
         if address not in components_dict:
             components_dict[address] = set()
-        try:
-            response = requests.get(f"http://{address}/metrics")
-        except requests.exceptions.ConnectionError:
-            continue
-
-        for line in response.text.split("\n"):
+    
+    for address, response in fetch_raw_prometheus(prom_addresses):
+        for line in response.split("\n"):
             for family in text_string_to_metric_families(line):
                 for sample in family.samples:
                     metric_names.add(sample.name)
