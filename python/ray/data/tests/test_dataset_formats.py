175d0d74feedad6446906b935d155e08073b3edb
Xiayue Charles Lin <xcl@anyscale.com>
2022-10-21 15:15:15 -0700

    wip test
    Signed-off-by: Xiayue Charles Lin <xcl@anyscale.com>

--- python/ray/data/tests/test_dataset_formats.py
+++ python/ray/data/tests/test_dataset_formats.py
@@ -409,6 +409,113 @@ def test_read_tfrecords(ray_start_regular_shared, tmp_path):
     assert list(df["bytes_list"]) == [[b"abc", b"1234"]]
 
 
+def test_write_tfrecords(ray_start_regular_shared, tmp_path):
+    """Test that write_tfrecords writes TFRecords correctly.
+
+    Test this by writing a Dataset to a TFRecord (function under test),
+    reading it back out into a tf.train.Example,
+    and checking that the result is analogous to the original Dataset.
+    """
+
+    import tensorflow as tf
+
+    # The dataset we will write to a .tfrecords file.
+    ds = ray.data.from_items(
+        [
+            # Row one.
+            {
+                "int_item": 1,
+                "int_list": [2, 2, 3],
+                "float_item": 1.0,
+                "float_list": [2.0, 3.0, 4.0],
+                "bytes_item": b"abc",
+                "bytes_list": [b"abc", b"1234"]
+            }, 
+            # Row two.
+            {
+                "int_item": 2,
+                "int_list": [3, 3, 4],
+                "float_item": 2.0,
+                "float_list": [2.0, 2.0, 3.0],
+                "bytes_item": b"def",
+                "bytes_list": [b"def", b"1234"]
+            }
+        ]
+    )
+
+    # The corresponding tf.train.Example that we would expect to read
+    # from this dataset.
+
+    expected_records = [
+        # Record one (corresponding to row one).
+        tf.train.Example(
+            features=tf.train.Features(
+                feature={
+                    "int_item": tf.train.Feature(
+                        int64_list=tf.train.Int64List(value=[1])
+                    ),
+                    "int_list": tf.train.Feature(
+                        int64_list=tf.train.Int64List(value=[2, 2, 3])
+                    ),
+                    "float_item": tf.train.Feature(
+                        float_list=tf.train.FloatList(value=[1.0])
+                    ),
+                    "float_list": tf.train.Feature(
+                        float_list=tf.train.FloatList(value=[2.0, 3.0, 4.0])
+                    ),
+                    "bytes_item": tf.train.Feature(
+                        bytes_list=tf.train.BytesList(value=b"abc")
+                    ),
+                    "bytes_list": tf.train.Feature(
+                        bytes_list=tf.train.BytesList(value=[b"abc", b"1234"])
+                    ),
+                }
+            )
+        ),
+        # Record two (corresponding to row two).
+        tf.train.Example(
+            features=tf.train.Features(
+                feature={
+                    "int_item": tf.train.Feature(
+                        int64_list=tf.train.Int64List(value=[2])
+                    ),
+                    "int_list": tf.train.Feature(
+                        int64_list=tf.train.Int64List(value=[3, 3, 4])
+                    ),
+                    "float_item": tf.train.Feature(
+                        float_list=tf.train.FloatList(value=[2.0])
+                    ),
+                    "float_list": tf.train.Feature(
+                        float_list=tf.train.FloatList(value=[2.0, 2.0, 3.0])
+                    ),
+                    "bytes_item": tf.train.Feature(
+                        bytes_list=tf.train.BytesList(value=[b"def"])
+                    ),
+                    "bytes_list": tf.train.Feature(
+                        bytes_list=tf.train.BytesList(value=[b"def", b"1234"])
+                    ),
+                }
+            )
+        ),
+    ]
+
+    # Perform the test.
+    # Write the dataset to a .tfrecords file.
+    ds.write_tfrecords(tmp_path)
+
+    # Read the Examples back out from the .tfrecords file.
+    # This follows the offical TFRecords tutorial:
+    # https://www.tensorflow.org/tutorials/load_data/tfrecord#reading_a_tfrecord_file_2
+
+    files = sorted(os.listdir(tmp_path))
+    raw_dataset = tf.data.TFRecordDataset(filenames)
+    tfrecords = [
+        tf.train.Example().ParseFromString(raw_record.numpy())
+        for raw_record in raw_dataset
+    ]
+    assert tfrecords == expected_records
+
+
 if __name__ == "__main__":
     import sys
 
