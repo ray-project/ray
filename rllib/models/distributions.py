4bcad07ffab38d31e51b47038af38673302b9c8e
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2022-10-24 10:06:53 -0700

    added missing docstrings and some nit renames
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- rllib/models/distributions.py
+++ rllib/models/distributions.py
@@ -1,3 +1,4 @@
+"""This is the next version of action distribution base class."""
 from typing import Tuple
 import gym
 import abc
@@ -7,16 +8,13 @@ from ray.rllib.utils.typing import TensorType, Union, ModelConfigDict
 
 
 @ExperimentalAPI
-class ActionDistributionV2(abc.ABC):
-    """The policy action distribution of an agent.
-
-    Args:
-        inputs: input vector to define the distribution over.
+class Distribution(abc.ABC):
+    """The base class for distribution over a random variable.
 
     Examples:
         >>> model = ... # a model that outputs a vector of logits
         >>> action_logits = model.forward(obs)
-        >>> action_dist = ActionDistribution(action_logits)
+        >>> action_dist = Distribution(action_logits)
         >>> action = action_dist.sample()
         >>> logp = action_dist.logp(action)
         >>> kl = action_dist.kl(action_dist2)
@@ -32,16 +30,16 @@ class ActionDistributionV2(abc.ABC):
         return_logp: bool = False,
         **kwargs
     ) -> Union[TensorType, Tuple[TensorType, TensorType]]:
-        """Draw a sample from the action distribution.
+        """Draw a sample from the distribution.
 
         Args:
             sample_shape: The shape of the sample to draw.
-            return_logp: Whether to return the logp of the sampled action.
+            return_logp: Whether to return the logp of the sampled values.
             **kwargs: Forward compatibility placeholder.
 
         Returns:
-            The sampled action. If return_logp is True, returns a tuple of the
-            sampled action and its logp.
+            The sampled values. If return_logp is True, returns a tuple of the
+            sampled values and its logp.
         """
 
     @abc.abstractmethod
@@ -52,64 +50,71 @@ class ActionDistributionV2(abc.ABC):
         return_logp: bool = False,
         **kwargs
     ) -> Union[TensorType, Tuple[TensorType, TensorType]]:
-        """Draw a re-parameterized sample from the action distribution.
+        """Draw a re-parameterized sample from the action distribution. 
+        
+        If this method is implemented, we can take gradients of samples w.r.t. the 
+        distribution parameters.
 
         Args:
             sample_shape: The shape of the sample to draw.
-            return_logp: Whether to return the logp of the sampled action.
+            return_logp: Whether to return the logp of the sampled values.
             **kwargs: Forward compatibility placeholder.
 
         Returns:
-            The sampled action. If return_logp is True, returns a tuple of the
-            sampled action and its logp.
+            The sampled values. If return_logp is True, returns a tuple of the
+            sampled values and its logp.
         """
 
     @abc.abstractmethod
-    def logp(self, action: TensorType, **kwargs) -> TensorType:
-        """The log-likelihood of the action distribution.
+    def logp(self, value: TensorType, **kwargs) -> TensorType:
+        """The log-likelihood of the distribution computed at `value`
 
         Args:
-            action: The action to compute the log-likelihood for.
+            value: The value to compute the log-likelihood at.
             **kwargs: Forward compatibility placeholder.
 
         Returns:
-            The log-likelihood of the action.
+            The log-likelihood of the value.
         """
 
     @abc.abstractmethod
-    def kl(self, other: "ActionDistributionV2", **kwargs) -> TensorType:
-        """The KL-divergence between two action distributions.
+    def kl(self, other: "Distribution", **kwargs) -> TensorType:
+        """The KL-divergence between two distributions.
 
         Args:
-            other: The other action distribution.
+            other: The other distribution.
             **kwargs: Forward compatibility placeholder.
 
         Returns:
-            The KL-divergence between the two action distributions.
+            The KL-divergence between the two distributions.
         """
 
     @abc.abstractmethod
     def entropy(self, **kwargs) -> TensorType:
-        """The entropy of the action distribution.
+        """The entropy of the distribution.
 
         Args:
             **kwargs: Forward compatibility placeholder.
 
         Returns:
-            The entropy of the action distribution.
+            The entropy of the distribution.
         """
 
     @staticmethod
     @abc.abstractmethod
     def required_model_output_shape(
-        action_space: gym.Space, model_config: ModelConfigDict
+        space: gym.Space, model_config: ModelConfigDict
     ) -> Tuple[int, ...]:
         """Returns the required shape of an input parameter tensor for a
-        particular action space and an optional dict of distribution-specific
+        particular space and an optional dict of distribution-specific
         options.
 
+        Let's have this method here just as a reminder to the next developer that this 
+        was part of the old distribution classes that we may or may not keep depending 
+        on how the catalog gets written. 
+
         Args:
-            action_space: The action space this distribution will be used for,
+            space: The space this distribution will be used for,
                 whose shape attributes will be used to determine the required shape of
                 the input parameter tensor.
             model_config: Model's config dict (as defined in catalog.py)
