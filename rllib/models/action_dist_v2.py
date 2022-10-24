da8f916192bc4e2a56b4c255aa9ae919501bbd0b
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2022-10-23 23:53:24 -0700

    created action_dist_v2 for RLModule examples
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

---
+++ rllib/models/action_dist_v2.py
@@ -0,0 +1,119 @@
+from typing import Tuple
+import gym
+import abc
+
+from ray.rllib.utils.annotations import ExperimentalAPI
+from ray.rllib.utils.typing import TensorType, Union, ModelConfigDict
+
+
+@ExperimentalAPI
+class ActionDistributionV2(abc.ABC):
+    """The policy action distribution of an agent.
+
+    Args:
+        inputs: input vector to define the distribution over.
+
+    Examples:
+        >>> model = ... # a model that outputs a vector of logits
+        >>> action_logits = model.forward(obs)
+        >>> action_dist = ActionDistribution(action_logits)
+        >>> action = action_dist.sample()
+        >>> logp = action_dist.logp(action)
+        >>> kl = action_dist.kl(action_dist2)
+        >>> entropy = action_dist.entropy()
+
+    """
+
+    @abc.abstractmethod
+    def sample(
+        self,
+        *,
+        sample_shape: Tuple[int, ...] = None,
+        return_logp: bool = False,
+        **kwargs
+    ) -> Union[TensorType, Tuple[TensorType, TensorType]]:
+        """Draw a sample from the action distribution.
+
+        Args:
+            sample_shape: The shape of the sample to draw.
+            return_logp: Whether to return the logp of the sampled action.
+            **kwargs: Forward compatibility placeholder.
+
+        Returns:
+            The sampled action. If return_logp is True, returns a tuple of the
+            sampled action and its logp.
+        """
+
+    @abc.abstractmethod
+    def rsample(
+        self,
+        *,
+        sample_shape: Tuple[int, ...] = None,
+        return_logp: bool = False,
+        **kwargs
+    ) -> Union[TensorType, Tuple[TensorType, TensorType]]:
+        """Draw a re-parameterized sample from the action distribution.
+
+        Args:
+            sample_shape: The shape of the sample to draw.
+            return_logp: Whether to return the logp of the sampled action.
+            **kwargs: Forward compatibility placeholder.
+
+        Returns:
+            The sampled action. If return_logp is True, returns a tuple of the
+            sampled action and its logp.
+        """
+
+    @abc.abstractmethod
+    def logp(self, action: TensorType, **kwargs) -> TensorType:
+        """The log-likelihood of the action distribution.
+
+        Args:
+            action: The action to compute the log-likelihood for.
+            **kwargs: Forward compatibility placeholder.
+
+        Returns:
+            The log-likelihood of the action.
+        """
+
+    @abc.abstractmethod
+    def kl(self, other: "ActionDistributionV2", **kwargs) -> TensorType:
+        """The KL-divergence between two action distributions.
+
+        Args:
+            other: The other action distribution.
+            **kwargs: Forward compatibility placeholder.
+
+        Returns:
+            The KL-divergence between the two action distributions.
+        """
+
+    @abc.abstractmethod
+    def entropy(self, **kwargs) -> TensorType:
+        """The entropy of the action distribution.
+
+        Args:
+            **kwargs: Forward compatibility placeholder.
+
+        Returns:
+            The entropy of the action distribution.
+        """
+
+    @staticmethod
+    @abc.abstractmethod
+    def required_model_output_shape(
+        action_space: gym.Space, model_config: ModelConfigDict
+    ) -> Tuple[int, ...]:
+        """Returns the required shape of an input parameter tensor for a
+        particular action space and an optional dict of distribution-specific
+        options.
+
+        Args:
+            action_space: The action space this distribution will be used for,
+                whose shape attributes will be used to determine the required shape of
+                the input parameter tensor.
+            model_config: Model's config dict (as defined in catalog.py)
+
+        Returns:
+            size of the required input vector (minus leading batch dimension).
+        """
---
+++ rllib/models/torch/torch_action_dist_v2.py
@@ -0,0 +1,188 @@
+"""The main difference between this and the old action distribution is that this one
+has more explicit input args. So that the input format does not have to be guessed from
+the code. This matches the design pattern of torch distribution which developers may
+already be familiar with.
+"""
+import gym
+import numpy as np
+from typing import Optional
+import abc
+
+
+from ray.rllib.models.action_dist_v2 import ActionDistributionV2
+from ray.rllib.utils.annotations import override, DeveloperAPI
+from ray.rllib.utils.framework import try_import_torch
+from ray.rllib.utils.typing import TensorType, Union, Tuple, ModelConfigDict
+
+torch, nn = try_import_torch()
+
+
+@DeveloperAPI
+class TorchDistribution(ActionDistributionV2, abc.ABC):
+    """Wrapper class for torch.distributions."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.dist = self._get_distribution(*args, **kwargs)
+
+    @abc.abstractmethod
+    def _get_distribution(self, *args, **kwargs) -> torch.distributions.Distribution:
+        """Returns the torch.distributions.Distribution object to use."""
+
+    @override(ActionDistributionV2)
+    def logp(self, actions: TensorType, **kwargs) -> TensorType:
+        return self.dist.log_prob(actions, **kwargs)
+
+    @override(ActionDistributionV2)
+    def entropy(self) -> TensorType:
+        return self.dist.entropy()
+
+    @override(ActionDistributionV2)
+    def kl(self, other: "ActionDistributionV2") -> TensorType:
+        return torch.distributions.kl.kl_divergence(self.dist, other.dist)
+
+    @override(ActionDistributionV2)
+    def sample(
+        self, *, sample_shape=torch.Size(), return_logp: bool = False
+    ) -> Union[TensorType, Tuple[TensorType, TensorType]]:
+        sample = self.dist.sample(sample_shape)
+        if return_logp:
+            return sample, self.logp(sample)
+        return sample
+
+    @override(ActionDistributionV2)
+    def rsample(
+        self, *, sample_shape=torch.Size(), return_logp: bool = False
+    ) -> Union[TensorType, Tuple[TensorType, TensorType]]:
+        sample = self.dist.rsample(sample_shape)
+        if return_logp:
+            return sample, self.logp(sample)
+        return sample
+
+
+@DeveloperAPI
+class TorchCategorical(TorchDistribution):
+    """Wrapper class for PyTorch Categorical distribution."""
+
+    def __init__(
+        self,
+        probs: torch.Tensor = None,
+        logits: torch.Tensor = None,
+        temperature: float = 1.0,
+    ) -> None:
+        super().__init__(probs=probs, logits=logits, temperature=temperature)
+
+    @override(TorchDistribution)
+    def _get_distribution(
+        self,
+        probs: torch.Tensor = None,
+        logits: torch.Tensor = None,
+        temperature: float = 1.0,
+    ) -> torch.distributions.Distribution:
+        if logits is not None:
+            assert temperature > 0.0, "Categorical `temperature` must be > 0.0!"
+            logits /= temperature
+        return torch.distributions.categorical.Categorical(probs, logits)
+
+    @staticmethod
+    @override(ActionDistributionV2)
+    def required_model_output_shape(
+        action_space: gym.Space, model_config: ModelConfigDict
+    ) -> Tuple[int, ...]:
+        return (action_space.n,)
+
+
+@DeveloperAPI
+class TorchDiagGaussian(TorchDistribution):
+    """Wrapper class for PyTorch Normal distribution."""
+
+    @override(ActionDistributionV2)
+    def __init__(
+        self,
+        loc: torch.Tensor,
+        scale: Optional[torch.Tensor] = None,
+    ):
+        super().__init__(loc=loc, scale=scale)
+
+    def _get_distribution(self, loc, scale=None) -> torch.distributions.Distribution:
+        if scale is None:
+            loc, log_std = torch.chunk(self.inputs, 2, dim=1)
+            scale = torch.exp(log_std)
+        return torch.distributions.normal.Normal(loc, scale)
+
+    @override(TorchDistribution)
+    def logp(self, actions: TensorType) -> TensorType:
+        return super().logp(actions).sum(-1)
+
+    @override(TorchDistribution)
+    def entropy(self) -> TensorType:
+        return super().entropy().sum(-1)
+
+    @override(TorchDistribution)
+    def kl(self, other: "TorchDistribution") -> TensorType:
+        return super().kl(other).sum(-1)
+
+    @staticmethod
+    @override(ActionDistributionV2)
+    def required_model_output_shape(
+        action_space: gym.Space, model_config: ModelConfigDict
+    ) -> Tuple[int, ...]:
+        return tuple(np.prod(action_space.shape, dtype=np.int32) * 2)
+
+
+@DeveloperAPI
+class TorchDeterministic(ActionDistributionV2):
+    """Action distribution that returns the input values directly.
+
+    This is similar to DiagGaussian with standard deviation zero (thus only
+    requiring the "mean" values as NN output).
+    """
+
+    def __init__(self, loc: torch.Tensor) -> None:
+        super().__init__()
+        self.loc = loc
+
+    @override(ActionDistributionV2)
+    def sample(
+        self,
+        *,
+        sample_shape: Tuple[int, ...] = None,
+        return_logp: bool = False,
+        **kwargs
+    ) -> Union[TensorType, Tuple[TensorType, TensorType]]:
+        if return_logp:
+            raise ValueError("Cannot return logp for TorchDeterministic.")
+
+        if sample_shape is None:
+            sample_shape = torch.Size()
+        loc_shape = self.loc.shape
+        return torch.ones(sample_shape + loc_shape, device=self.loc.device) * self.loc
+
+    def rsample(
+        self,
+        *,
+        sample_shape: Tuple[int, ...] = None,
+        return_logp: bool = False,
+        **kwargs
+    ) -> Union[TensorType, Tuple[TensorType, TensorType]]:
+        raise NotImplementedError
+
+    @override(ActionDistributionV2)
+    def logp(self, action: TensorType, **kwargs) -> TensorType:
+        raise NotImplementedError
+
+    @override(ActionDistributionV2)
+    def entropy(self, **kwargs) -> TensorType:
+        raise torch.zeros_like(self.loc)
+
+    @override(ActionDistributionV2)
+    def kl(self, other: "ActionDistributionV2", **kwargs) -> TensorType:
+        raise ValueError("Cannot return kl for TorchDeterministic.")
+
+    @staticmethod
+    @override(ActionDistributionV2)
+    def required_model_output_shape(
+        action_space: gym.Space, model_config: ModelConfigDict
+    ) -> Tuple[int, ...]:
+        # TODO: This was copied from previous code. Is this correct? add unit test.
+        return tuple(np.prod(action_space.shape, dtype=np.int32))
