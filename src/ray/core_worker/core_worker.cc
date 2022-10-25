e23b5f11bb76d130bd2e288951ec7191dbd4addd
Eric Liang <ekhliang@gmail.com>
2022-10-25 14:28:19 -0700

    flush
    Signed-off-by: Eric Liang <ekhliang@gmail.com>

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -626,6 +626,8 @@ void CoreWorker::Disconnect(
     const std::string &exit_detail,
     const std::shared_ptr<LocalMemoryBuffer> &creation_task_exception_pb_bytes) {
   // Force stats export before exiting the worker.
+  task_manager_->RecordMetrics();
+  task_counter_.RecordMetrics();
   opencensus::stats::StatsExporter::ExportNow();
   if (connected_) {
     RAY_LOG(INFO) << "Disconnecting to the raylet.";
