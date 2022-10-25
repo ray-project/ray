42eabe4c77c7238f184417660af6d5bf4ec05b41
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2022-10-25 15:40:19 -0700

    nits on the initial pr
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- rllib/core/rl_module/torch/torch_rl_module.py
+++ rllib/core/rl_module/torch/torch_rl_module.py
@@ -11,8 +11,11 @@ class TorchRLModule(RLModule, nn.Module):
         nn.Module.__init__(self)
 
     def forward(self, batch: Mapping[str, Any], **kwargs) -> Mapping[str, Any]:
-        """This is only done because Torch DDP requires a forward method to be
-        implemented for backpropagation to work."""
+        """forward pass of the module.
+
+        This is aliased to forward_train because Torch DDP requires a forward method to
+        be implemented for backpropagation to work.
+        """
         return self.forward_train(batch, **kwargs)
 
     def get_state(self) -> Mapping[str, Any]:
