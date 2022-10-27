679bae74d4c7e78770614f7083360bf083d0684f
scv119 <scv119@gmail.com>
2022-10-27 16:40:05 -0700

    add

--- src/ray/object_manager/plasma/create_request_queue.cc
+++ src/ray/object_manager/plasma/create_request_queue.cc
@@ -138,18 +138,18 @@ Status CreateRequestQueue::ProcessRequests() {
                                 *request_it,
                                 /*spilling_required=*/nullptr);
         if (!status.ok()) {
+          (*request_it)->error = PlasmaError::OutOfDisk;
           std::string dump = "";
           if (dump_debug_info_callback_ && !logged_oom) {
             dump = dump_debug_info_callback_();
             logged_oom = true;
           }
-          RAY_LOG(INFO) << "Out-of-memory: Failed to create object "
+          RAY_LOG(INFO) << "Out-of-disk: Failed to create object "
                         << (*request_it)->object_id << " of size "
                         << (*request_it)->object_size / 1024 / 1024 << "MB\n"
                         << dump;
         }
         FinishRequest(request_it);
-        return Status::OutOfDisk("System running out of disk.");
       }
     }
   }
