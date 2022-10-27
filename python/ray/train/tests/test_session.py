6618d53dc1784dc2dd06044813bee722b81f3b10
Matthew Deng <matt@anyscale.com>
2022-10-27 16:01:36 -0700

    Revert "[AIR] Avoid checkpoint conversion, move encoding logic to checkpoints (#28794)"
    This reverts commit 4c20503a7316d5673064938879e96ebc659b3880.

--- python/ray/train/tests/test_session.py
+++ python/ray/train/tests/test_session.py
@@ -139,7 +139,7 @@ def test_checkpoint():
         next = session.get_next()
         assert next is not None
         assert next.type == TrainingResultType.CHECKPOINT
-        assert next.data.to_dict()["epoch"] == expected
+        assert next.data["epoch"] == expected
 
     init_session(training_func=train_func, world_rank=0, local_rank=0, world_size=1)
     session = get_session()
@@ -155,7 +155,7 @@ def test_checkpoint():
         next = session.get_next()
         assert next is not None
         assert next.type == TrainingResultType.CHECKPOINT
-        assert not next.data
+        assert next.data == {}
 
     init_session(training_func=train_func, world_rank=1, local_rank=1, world_size=1)
     session = get_session()
@@ -173,17 +173,13 @@ def test_encode_data():
         report(dict(epoch=0), checkpoint=Checkpoint.from_dict(dict(epoch=0)))
 
     def encode_checkpoint(checkpoint):
-        data = checkpoint.to_dict()
-        data["encoded"] = True
-        return checkpoint.from_dict(data)
+        checkpoint.update({"encoded": True})
+        return checkpoint
 
     def validate_encoded(result_type: TrainingResultType):
         next = session.get_next()
         assert next.type is result_type
-        data = next.data
-        if isinstance(data, Checkpoint):
-            data = data.to_dict()
-        assert data["encoded"] is True
+        assert next.data["encoded"] is True
 
     init_session(
         training_func=train_func,
@@ -197,7 +193,8 @@ def test_encode_data():
     session.start()
     # Validate checkpoint is encoded.
     validate_encoded(TrainingResultType.CHECKPOINT)
-    session.get_next()
+    # Validate report is encoded.
+    validate_encoded(TrainingResultType.REPORT)
     session.finish()
     shutdown_session()
 
@@ -213,7 +210,6 @@ def test_load_checkpoint_after_save():
     session = get_session()
     session.start()
     for i in range(2):
-        session.get_next()
         session.get_next()
     session.finish()
     shutdown_session()
