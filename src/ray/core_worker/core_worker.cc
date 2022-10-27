b3c57c46410c0a2434f78e9630cef36a3dec69da
Ricky Xu <xuchen727@hotmail.com>
2022-10-27 07:37:00 -0700

    [core][metric] Move periodic metric recording to be controlled by metric report interval and fix test_exit_observability (#29723)
    This PR does a couple of things:
    
    Fix the test_exit_observability test logic
    Couple task metric record with metric_report_interverval_ms rather than heart beat interval in CoreWorker
    Skip metric test on windows.

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -548,6 +548,10 @@ CoreWorker::CoreWorker(const CoreWorkerOptions &options, const WorkerID &worker_
       [this] { InternalHeartbeat(); },
       RayConfig::instance().core_worker_internal_heartbeat_ms());
 
+  periodical_runner_.RunFnPeriodically(
+      [this] { RecordMetrics(); },
+      RayConfig::instance().metrics_report_interval_ms() / 2);
+
 #ifndef _WIN32
   // Doing this last during CoreWorker initialization, so initialization logic like
   // registering with Raylet can finish with higher priority.
@@ -626,8 +630,8 @@ void CoreWorker::Disconnect(
     const std::string &exit_detail,
     const std::shared_ptr<LocalMemoryBuffer> &creation_task_exception_pb_bytes) {
   // Force stats export before exiting the worker.
-  task_manager_->RecordMetrics();
-  task_counter_.RecordMetrics();
+  RecordMetrics();
+
   opencensus::stats::StatsExporter::ExportNow();
   if (connected_) {
     RAY_LOG(INFO) << "Disconnecting to the raylet.";
@@ -645,7 +649,8 @@ void CoreWorker::Exit(
     const std::shared_ptr<LocalMemoryBuffer> &creation_task_exception_pb_bytes) {
   RAY_LOG(INFO) << "Exit signal received, this process will exit after all outstanding "
                    "tasks have finished"
-                << ", exit_type=" << rpc::WorkerExitType_Name(exit_type);
+                << ", exit_type=" << rpc::WorkerExitType_Name(exit_type)
+                << ", detail=" << detail;
   exiting_ = true;
   // Release the resources early in case draining takes a long time.
   RAY_CHECK_OK(
@@ -828,7 +833,9 @@ void CoreWorker::InternalHeartbeat() {
   if (options_.worker_type == WorkerType::DRIVER && options_.interactive) {
     memory_store_->NotifyUnhandledErrors();
   }
+}
 
+void CoreWorker::RecordMetrics() {
   // Record metrics for owned tasks.
   task_manager_->RecordMetrics();
   // Record metrics for executed tasks.
