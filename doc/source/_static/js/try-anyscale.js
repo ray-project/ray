150028607ceb671938a40c009746163bc4d099f2
sijie <sijie@anyscale.com>
2022-10-28 15:57:30 -0700

    Add a Learn about Anyscale link to docs
    Signed-off-by: sijie <sijie@anyscale.com>

---
+++ doc/source/_static/js/try-anyscale.js
@@ -0,0 +1,5 @@
+anyscaleButton = document.createElement("a");
+anyscaleButton.setAttribute("class", "try-anyscale");
+anyscaleButton.setAttribute("href", "https://www.anyscale.com/signup");
+anyscaleButton.innerText = "Learn about managed Ray on Anyscale"
+document.getElementsByClassName("topbar-main")[0].prepend(anyscaleButton)
