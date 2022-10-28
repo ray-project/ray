754a8805d8e77b51be0245e260d0d1af57f9054a
Aviv Haber <aviv@anyscale.com>
2022-10-28 10:53:37 -0700

    clang formatting
    Signed-off-by: Aviv Haber <aviv@anyscale.com>

--- src/ray/common/memory_monitor.h
+++ src/ray/common/memory_monitor.h
@@ -100,7 +100,8 @@ class MemoryMonitor {
 
   /// \param stat_path file path to the memory.stat file.
   /// \param usage_path file path to the memory.current file
-  /// \return the used memory for cgroup v2. May return negative value, which should be discarded.
+  /// \return the used memory for cgroup v2. May return negative value, which should be
+  /// discarded.
   static int64_t GetCGroupV2MemoryUsedBytes(const char *stat_path,
                                             const char *usage_path);
 
