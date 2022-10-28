beaf0db601a458e0e52737ef155e7cd8c35e743e
scv119 <scv119@gmail.com>
2022-10-28 16:45:49 -0700

    add

---
+++ release/cluster_tests/test_aws_cluster_launcher.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Test Ray Data bulk ingestion performance as the size of input files change.
+
+set -x -e
+
+ray down -y ./aws_cluster.yaml
+ray up -y ./aws_cluster.yaml
+ray exec config.yaml 'python -c "import ray; ray.init()"'
+ray down -y ./aws_cluster.yaml
