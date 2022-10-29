d2deed5abf97868847052d29a4bb37508bcb8e2a
scv119 <scv119@gmail.com>
2022-10-28 17:06:08 -0700

    add

--- release/cluster_tests/test_aws_cluster_launcher.sh
+++ release/cluster_tests/test_aws_cluster_launcher.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 # Test Ray Data bulk ingestion performance as the size of input files change.
 
-set -x -e
+set -e
 
+echo "===== clean up previously running cluster ====="
 ray down -y ./aws_cluster.yaml
+
+sleep 5
+echo "===== start cluster ====="
 ray up -y ./aws_cluster.yaml
+
+sleep 5
+echo "===== verify ray is running ====="
 ray exec ./aws_cluster.yaml 'python -c "import ray; ray.init()"'
+
+sleep 5
+echo "===== clean up ====="
 ray down -y ./aws_cluster.yaml
