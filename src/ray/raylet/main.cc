a74ccc1a90e42e2e14604acc3609ff5c252e01cb
Frank Luan <lsf@berkeley.edu>
2022-10-30 07:04:20 +0000

    Adjust rpc_service_threads_number

--- src/ray/raylet/main.cc
+++ src/ray/raylet/main.cc
@@ -243,7 +243,7 @@ int main(int argc, char *argv[]) {
         object_manager_config.fallback_directory = temp_dir;
         object_manager_config.huge_pages = huge_pages;
 
-        object_manager_config.rpc_service_threads_number = 60;
+        object_manager_config.rpc_service_threads_number = 20;
         // object_manager_config.rpc_service_threads_number =
         //     RayConfig::instance().object_manager_num_rpc_threads() == -1
         //         ? std::min(std::max(2, num_cpus / 4), 8)
