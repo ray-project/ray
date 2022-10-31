284fe2de92c212ebccc8cf0bfca5a92796ea97bd
SangBin Cho <rkooo567@gmail.com>
2022-10-31 09:01:20 -0700

    Add more useful debug info to ray start
    Signed-off-by: SangBin Cho <rkooo567@gmail.com>

--- python/ray/scripts/scripts.py
+++ python/ray/scripts/scripts.py
@@ -815,6 +815,17 @@ def start(
                     ),
                 )
             cli_logger.newline()
+            cli_logger.print("To see the status of the cluster, use")
+            cli_logger.print("  {}".format(cf.bold("ray status")))
+            dashboard_url = node.address_info["webui_url"]
+            if dashboard_url:
+                cli_logger.print("To monitor and debug Ray, view the dashboard at ")
+                cli_logger.print(
+                    "  {}".format(
+                        cf.bold(dashboard_url),
+                    )
+                )
+            cli_logger.newline()
             cli_logger.print(
                 cf.underlined(
                     "If connection fails, check your "
@@ -822,12 +833,6 @@ def start(
                     "network configuration."
                 )
             )
-            cli_logger.newline()
-            dashboard_url = node.address_info["webui_url"]
-            if dashboard_url:
-                cli_logger.print("To monitor and debug Ray, view the dashboard at {}".format(
-                    cf.yellow(dashboard_url),
-                ))
             cli_logger.newline()
             cli_logger.print("To terminate the Ray runtime, run")
             cli_logger.print(cf.bold("  ray stop"))
