5079dd4d8668808dc64f36a4a715e087c8e6a1c7
Amog Kamsetty <amogkamsetty@yahoo.com>
2022-10-31 18:01:38 -0700

    update
    Signed-off-by: Amog Kamsetty <amogkamsetty@yahoo.com>

--- python/ray/data/tests/test_batch_mapper.py
+++ python/ray/data/tests/test_batch_mapper.py
@@ -97,12 +97,12 @@ def test_batch_mapper_pandas_data_format(
 @pytest.mark.parametrize(
     "ds",
     [
-        # lazy_fixture("ds_pandas_single_column_format"),
-        # lazy_fixture("ds_pandas_multi_column_format"),
-        # lazy_fixture("ds_arrow_single_column_format"),
-        # lazy_fixture("ds_arrow_single_column_tensor_format"),
-        # lazy_fixture("ds_arrow_multi_column_format"),
-        # lazy_fixture("ds_numpy_single_column_tensor_format"),
+        lazy_fixture("ds_pandas_single_column_format"),
+        lazy_fixture("ds_pandas_multi_column_format"),
+        lazy_fixture("ds_arrow_single_column_format"),
+        lazy_fixture("ds_arrow_single_column_tensor_format"),
+        lazy_fixture("ds_arrow_multi_column_format"),
+        lazy_fixture("ds_numpy_single_column_tensor_format"),
         lazy_fixture("ds_numpy_list_of_ndarray_tensor_format"),
     ],
 )
