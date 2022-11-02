18cf7748be594a142e944f3673edc9fb9a783eb4
ilee300a <ilee300@anyscale.com>
2022-11-02 10:34:43 -0700

    remove MosaicPredictor - TorchPredictor can be used instead
    Signed-off-by: ilee300a <ilee300@anyscale.com>

--- python/ray/train/mosaic/__init__.py
+++ python/ray/train/mosaic/__init__.py
@@ -1,5 +1,4 @@
 from ray.train.mosaic.mosaic_trainer import MosaicTrainer
-from ray.train.mosaic.mosaic_predictor import MosaicPredictor
 from ray.train.mosaic.mosaic_checkpoint import MosaicCheckpoint
 
-__all__ = ["MosaicTrainer", "MosaicPredictor", "MosaicCheckpoint"]
+__all__ = ["MosaicTrainer", "MosaicCheckpoint"]
