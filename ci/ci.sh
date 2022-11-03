ea796f68ceb441653ef9bd5c5c776303e29a21bf
Alan Guo <aguo@anyscale.com>
2022-11-02 17:27:19 -0700

    fixup
    Signed-off-by: Alan Guo <aguo@anyscale.com>

--- ci/ci.sh
+++ ci/ci.sh
@@ -274,7 +274,7 @@ install_npm_project() {
     # Not Windows-compatible: https://github.com/npm/cli/issues/558#issuecomment-584673763
     { echo "WARNING: Skipping NPM due to module incompatibilities with Windows"; } 2> /dev/null
   else
-    npm install
+    npm ci
   fi
 }
 
