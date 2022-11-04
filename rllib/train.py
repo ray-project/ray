5883e9ae41dcee6f78b14621c33a8bfd02ca8044
Sven Mika <svenmika1977@gmail.com>
2022-11-04 10:53:58 +0100

    [RLlib] Enable .py files with AlgorithmConfig objects in them to be run by our `run_regression_test.py` script. (#29974)

--- rllib/train.py
+++ rllib/train.py
@@ -51,7 +51,9 @@ def _patch_path(path: str):
 
 
 def load_experiments_from_file(
-    config_file: str, file_type: SupportedFileType, checkpoint_config: dict
+    config_file: str,
+    file_type: SupportedFileType,
+    checkpoint_config: Optional[dict] = None,
 ) -> dict:
     """Load experiments from a file. Supports YAML, JSON and Python files.
     If you want to use a Python file, it has to have a 'config' variable
@@ -94,7 +96,7 @@ def load_experiments_from_file(
             experiments["default"]["stop"] = stop
 
     for key, val in experiments.items():
-        experiments[key]["checkpoint_config"] = checkpoint_config
+        experiments[key]["checkpoint_config"] = checkpoint_config or {}
 
     return experiments
 
