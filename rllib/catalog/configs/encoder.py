dc6db602bb1e90b325abee77eb63e2a3690c9bc3
Steven Morad <smorad@anyscale.com>
2022-11-06 16:14:34 +0000

    more fleshed out
    Signed-off-by: Steven Morad <smorad@anyscale.com>

---
+++ rllib/catalog/configs/encoder.py
@@ -0,0 +1,81 @@
+import abc
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, List
+from rllib.catalog.torch.encoders.vector import TorchVectorEncoder
+from rllib.models.specs.specs_dict import ModelSpec
+
+if TYPE_CHECKING:
+    from rllib.catalog.torch.encoders.vector import Encoder
+
+
+@dataclass(frozen=True)
+class EncoderConfig:
+    """The base config for encoder models. Each config should define a `build` method
+    that builds a model from the config.
+
+    All parameters known before runtime (e.g. framework, activation, num layers, etc.)
+    should be defined as attributes.
+
+    Parameters unknown before runtime (e.g. the output size of the module providing
+    input for this model) should be passed as arguments to `build`. This should be
+    as few params as possible.
+
+    `build` should return an instance of the encoder associated with the config.
+
+    Attributes:
+        framework: The tensor framework to construct a model for.
+            This can be 'torch', 'tf2', or 'jax'.
+    """
+
+    framework: str = "torch"
+
+    @abc.abstractmethod
+    def build(self, input_spec: ModelSpec, **kwargs) -> "Encoder":
+        """Builds the config into a model instance"""
+
+
+@dataclass(frozen=True)
+class VectorEncoderConfig:
+    """A basic MLP encoder.
+
+    Attributes:
+        activation: The type of activation function to use between hidden layers.
+            Options are 'relu', 'swish', 'tanh', or 'linear'
+        final_activation: The activation function to use after the final linear layer.
+            Options are the same as for activation.
+        hidden_layer_sizes: A list, where each element represents the number of neurons
+            in that layer. For example, [128, 64] would produce a two-layer MLP with
+            128 hidden neurons and 64 hidden neurons.
+        output_key: Write the output of the encoder to this key in the NestedDict.
+    """
+
+    activation: str = "relu"
+    final_activation: str = "linear"
+    hidden_layer_sizes: List[int] = [128, 128]
+    output_key: str = "encoding"
+
+    def build(self, input_spec: ModelSpec) -> TorchVectorEncoder:
+        assert (
+            len(self.hidden_layer_sizes) > 1
+        ), "Must have at least a single hidden layer"
+        # TODO: This requires modification/implementation in specs_base.py
+        # Do spec surgery:
+        #   input_spec == {"bork": (a, b, c, d), d=3}
+        # should produce:
+        #   output_spec == {"encoding": (a, b, c, d), d=self.hidden_layer_sizes[-1]}
+        output_spec = ModelSpec.output_from_input(
+            input_spec,
+            out_key=self.output_key,
+            feature_size=self.hidden_layer_sizes[-1],
+        )
+        for k in input_spec.shallow_keys():
+            assert isinstance(
+                input_spec[k].shape[-1], int
+            ), "All input specs must define the size of the feature (last) dimension"
+        if self.framework == "torch":
+            return TorchVectorEncoder(input_spec, output_spec, self)
+        else:
+            raise NotImplementedError(
+                "{self.__class__.__name__} not implemented"
+                " for framework {self.framework}"
+            )
--- rllib/catalog/torch/encoders/vector.py
+++ rllib/catalog/torch/encoders/vector.py
@@ -1,61 +1,56 @@
 from ray.rllib.utils.nested_dict import NestedDict
-from rllib.models.torch.model import TorchModel
+from ray.rllib.models.torch.model import TorchModel
+from ray.rllib.models.specs.specs_dict import ModelSpec
+from ray.rllib.models.utils import get_activation_fn
+from typing import TYPE_CHECKING
+
 from torch import nn
 import torch
-from ray.rllib.models.specs.specs_dict import ModelSpec
-from dataclasses import dataclass
-from typing import Any, Callable, List
 
-
-def get_feature_size(spec: ModelSpec) -> int:
-    shapes = [v.get_shape()[-1] for v in spec.values()]
-    return sum(shapes)
+if TYPE_CHECKING:
+    from rllib.catalog.configs.encoder import VectorEncoderConfig
 
 
 class TorchVectorEncoder(TorchModel):
+    """An MLP encoder"""
+
     @property
     def input_spec(self) -> ModelSpec:
-        return self._in_spec
+        return self._input_spec
 
     @property
     def output_spec(self) -> ModelSpec:
-        return self.config.out_spec
+        return self._output_spec
 
-    def __init__(self, in_spec: ModelSpec, config: "TorchVectorEncoderConfig"):
+    def __init__(
+        self,
+        input_spec: ModelSpec,
+        output_spec: ModelSpec,
+        config: "VectorEncoderConfig",
+    ):
         self.config = config
-        self._in_spec = in_spec
-        self._out_spec = self.config.output_spec
-        prev_size = get_feature_size(self.input_spec)
+        self._input_spec = input_spec
+        self._output_spec = output_spec
+        # Returns the size of the feature dimension for the input tensors
+        prev_size = sum([v.shape()[-1] for v in input_spec().values()])
         layers = []
+        act = (
+            nn.Identity()
+            if config.activation == "linear"
+            else get_activation_fn(config.activation)()
+        )
         for size in self.layer_sizes[:-1]:
-            layers += [
-                nn.Linear(prev_size, size),
-                config.activation,
-            ]
+            layers += [nn.Linear(prev_size, size), act]
             prev_size = size
         # final layer
-        layers += [nn.Linear(self.layer_sizes[-1], get_feature_size(self.output_spec))]
+        layers += [nn.Linear(self.layer_sizes[-1], self.output_spec.shape[-1])]
         layers += config.final_activation
 
         self.net = nn.Sequential(layers)
 
     def _forward(self, inputs: NestedDict) -> NestedDict:
+        # Concatenate all input along the feature dim
         x = torch.cat(inputs.values(), dim=-1)
         [out_key] = self.output_spec.keys()
         inputs[out_key] = self.net(x)
         return inputs
-
-
-@dataclass
-class TorchVectorEncoderConfig:
-    out_spec: ModelSpec
-    activation: Callable[[Any], Any] = nn.ReLU()
-    final_activation: Callable[[Any], Any] = nn.Identity()
-    hidden_layer_sizes: List[int] = [128, 128]
-    out_spec: ModelSpec = ModelSpec({"b, f"}, f=128)
-
-    def build(self, in_spec: ModelSpec, in_feature_dim: str) -> TorchVectorEncoder:
-        assert (
-            len(self.out_spec) == 1
-        ), "VectorEncoder output spec must only contain one element"
-        return TorchVectorEncoder(in_spec, self)
