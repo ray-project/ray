2e3ab7a928fa5e8ba4d9dace6454cf5e2b518164
sven1977 <svenmika1977@gmail.com>
2022-11-07 12:01:37 +0100

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/examples/env/deterministic_envs.py
+++ rllib/examples/env/deterministic_envs.py
@@ -3,11 +3,11 @@ import gymnasium as gym
 
 def create_cartpole_deterministic(config):
     env = gym.make("CartPole-v1")
-    env.seed(config.get("seed", 0))
+    env.reset(seed=config.get("seed", 0))
     return env
 
 
 def create_pendulum_deterministic(config):
     env = gym.make("Pendulum-v1")
-    env.seed(config.get("seed", 0))
+    env.reset(seed=config.get("seed", 0))
     return env
