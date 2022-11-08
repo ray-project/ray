eefbb2d4430f05631df202b9bb9fb1cd6aeedd4a
scv119 <scv119@gmail.com>
2022-11-07 17:41:48 -0800

    better-fix

--- ci/build/test-wheels.sh
+++ ci/build/test-wheels.sh
@@ -102,6 +102,8 @@ elif [[ "$platform" == "macosx" ]]; then
           "3.9"
           "3.10")
 
+  _PYTHONPATH="${PYTHONPATH}"
+
   for ((i=0; i<${#PY_MMS[@]}; ++i)); do
     PY_MM="${PY_MMS[i]}"
 
@@ -109,13 +111,14 @@ elif [[ "$platform" == "macosx" ]]; then
 
     PYTHON_EXE="$MACPYTHON_PY_PREFIX/$PY_MM/bin/python$PY_MM"
     PYTHON_SITE_PACKAGE_PATH="$MACPYTHON_PY_PREFIX/$PY_MM/lib/python$PY_MM/site-packages/"
+
     PIP_CMD="$(dirname "$PYTHON_EXE")/pip$PY_MM"
 
     # Find the appropriate wheel by grepping for the Python version.
     PYTHON_WHEEL="$(printf "%s\n" "$ROOT_DIR"/../../.whl/*"$PY_WHEEL_VERSION"* | head -n 1)"
 
     # Install the wheel.
-    PYTHONPATH="${PYTHONPATH}:${PYTHON_SITE_PACKAGE_PATH}" "$PIP_CMD" uninstall -y ray
+    PYTHONPATH="${_PYTHONPATH}:${PYTHON_SITE_PACKAGE_PATH}" "$PIP_CMD" uninstall -y ray
     "$PIP_CMD" install -q "$PYTHON_WHEEL"
 
     # Install the dependencies to run the tests.
