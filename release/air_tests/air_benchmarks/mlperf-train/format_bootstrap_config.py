8bf56b997cd9710cef1300b3c8cf14efd8fbf10a
Stephanie Wang <swang@cs.berkeley.edu>
2022-11-08 14:10:14 -0800

    config

--- release/air_tests/air_benchmarks/mlperf-train/format_bootstrap_config.py
+++ release/air_tests/air_benchmarks/mlperf-train/format_bootstrap_config.py
@@ -14,12 +14,14 @@ if __name__ == "__main__":
         yaml = json.load(f)
 
     node_types = yaml["available_node_types"]
-    assert (
-        "worker-node-type-0" in node_types
-    ), "Must specify exactly one worker node type using the cluster UI"
+    worker_label = None
+    for label in node_types:
+        if "worker" in label:
+            assert worker_label is None, "More than one worker node type specified in config"
+            worker_label = label
 
-    node_types["worker-node-type-0"]["min_workers"] = args.num_cpu_nodes
-    node_types["worker-node-type-0"]["max_workers"] = args.num_cpu_nodes
+    node_types[worker_label]["min_workers"] = args.num_cpu_nodes
+    node_types[worker_label]["max_workers"] = args.num_cpu_nodes
     # Cluster-level max workers includes 1 node for the head node.
     yaml["max_workers"] = args.num_cpu_nodes + 1
 
