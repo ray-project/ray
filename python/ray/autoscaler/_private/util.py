a06a80f28eee5ba19210e0456ff4595f22b3b4a7
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2022-11-10 12:58:17 -0800

    fix
    Signed-off-by: Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>

--- python/ray/autoscaler/_private/util.py
+++ python/ray/autoscaler/_private/util.py
@@ -21,9 +21,6 @@ from ray.autoscaler._private.docker import validate_docker_config
 from ray.autoscaler._private.local.config import prepare_local
 from ray.autoscaler._private.providers import _get_default_config
 from ray.autoscaler.tags import (
-    NODE_KIND_HEAD,
-    NODE_KIND_UNMANAGED,
-    NODE_KIND_WORKER,
     NODE_TYPE_LEGACY_HEAD,
     NODE_TYPE_LEGACY_WORKER,
 )
