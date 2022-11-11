d7215dbd79dee54c21d2971ec559affdefb8a2fa
Artur Niederfahrenhorst <artur@anyscale.com>
2022-11-11 00:36:11 -0800

    Revert "restore original argument names for tf policies"
    This reverts commit 95155a30f7e9687c8cd635ba53c950fe86d3d9a7.

--- rllib/algorithms/es/es_tf_policy.py
+++ rllib/algorithms/es/es_tf_policy.py
@@ -147,10 +147,10 @@ class ESTFPolicy(Policy):
         )
 
     @override(Policy)
-    def compute_actions(self, observation, add_noise=False, update=True, **kwargs):
+    def compute_actions(self, obs_batch, add_noise=False, update=True, **kwargs):
         # Squeeze batch dimension (we always calculate actions for only a
         # single obs).
-        observation = observation[0]
+        observation = obs_batch[0]
         observation = self.preprocessor.transform(observation)
         observation = self.observation_filter(observation[None], update=update)
         # `actions` is a list of (component) batches.
