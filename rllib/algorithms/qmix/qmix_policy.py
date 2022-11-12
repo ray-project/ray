f8939b29134e9c54ef9c2a4176912ecc4391b4fe
Artur Niederfahrenhorst <artur@anyscale.com>
2022-11-12 10:08:11 -0800

    delete qmix compute_actions
    Signed-off-by: Artur Niederfahrenhorst <artur@anyscale.com>

--- rllib/algorithms/qmix/qmix_policy.py
+++ rllib/algorithms/qmix/qmix_policy.py
@@ -326,42 +326,8 @@ class QMixTorchPolicy(TorchPolicy):
 
         return tuple(actions.transpose([1, 0])), hiddens, {}
 
-    @override(TorchPolicy)
-    def compute_actions(
-        self,
-        obs_batch: Union[List[TensorStructType], TensorStructType],
-        state_batches: Optional[List[TensorType]] = None,
-        prev_action_batch: Union[List[TensorStructType], TensorStructType] = None,
-        prev_reward_batch: Union[List[TensorStructType], TensorStructType] = None,
-        info_batch: Optional[Dict[str, list]] = None,
-        episodes: Optional[List["Episode"]] = None,
-        explore: Optional[bool] = None,
-        timestep: Optional[int] = None,
-        **kwargs,
-    ) -> Tuple[TensorStructType, List[TensorType], Dict[str, TensorType]]:
-        with torch.no_grad():
-            input_dict = self._lazy_tensor_dict(
-                {
-                    SampleBatch.CUR_OBS: obs_batch,
-                    "is_training": False,
-                    SampleBatch.SEQ_LENS: torch.ones(len(obs_batch), dtype=torch.int32),
-                }
-            )
-            if prev_action_batch is not None:
-                input_dict[SampleBatch.PREV_ACTIONS] = np.asarray(prev_action_batch)
-            if prev_reward_batch is not None:
-                input_dict[SampleBatch.PREV_REWARDS] = np.asarray(prev_reward_batch)
-            if state_batches:
-                state_batches = [
-                    convert_to_torch_tensor(s, self.device)
-                    for s in (state_batches or [])
-                ]
-                for i in range(len(state_batches)):
-                    input_dict[f"state_in_{i}"] = state_batches[i]
-
-            return self.compute_actions_from_input_dict(
-                input_dict=input_dict, explore=explore, timestep=timestep
-            )
+    def compute_actions(self, *args, **kwargs):
+        return self.compute_actions_from_input_dict(*args, **kwargs)
 
     @override(TorchPolicy)
     def compute_log_likelihoods(
