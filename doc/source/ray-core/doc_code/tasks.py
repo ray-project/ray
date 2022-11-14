e6b3aa29cba5f70d4d5e89db954f08a95914af80
scv119 <scv119@gmail.com>
2022-11-13 20:37:48 -0800

    add

--- doc/source/ray-core/doc_code/tasks.py
+++ doc/source/ray-core/doc_code/tasks.py
@@ -2,7 +2,6 @@
 
 # fmt: off
 # __tasks_start__
-
 import ray
 import time
 
@@ -39,7 +38,6 @@ def slow_function():
 for _ in range(4):
     # This doesn't block.
     slow_function.remote()
-
 # __tasks_end__
 # fmt: on
 
@@ -56,23 +54,19 @@ assert ray.get(obj_ref1) == 1
 # You can pass an object ref as an argument to another Ray task.
 obj_ref2 = function_with_an_argument.remote(obj_ref1)
 assert ray.get(obj_ref2) == 2
-
 # __pass_by_ref_end__
 # fmt: on
 
 # fmt: off
 # __wait_start__
-
 object_refs =  [slow_function.remote() for _ in range(2)]
 # Return as soon as one of the tasks finished execution.
 ready_refs, remaining_refs = ray.wait(object_refs, num_returns=1, timeout=None)
-
 # __wait_end__
 # fmt: on
 
 # fmt: off
 # __multiple_returns_start__
-
 # By default, a Ray task only returns a single Object Ref.
 @ray.remote
 def return_single():
@@ -93,7 +87,6 @@ object_ref0, object_ref1, object_ref2 = return_multiple.remote()
 assert ray.get(object_ref0) == 0
 assert ray.get(object_ref1) == 1
 assert ray.get(object_ref2) == 2
-
 # __multiple_returns_end__
 # fmt: on 
 
@@ -107,7 +100,6 @@ def return_multiple_as_generator():
 # NOTE: Similar to normal functions, these objects will not be available
 # until the full task is complete and all returns have been generated.
 a, b, c = return_multiple_as_generator.remote()
-
 # __generator_end__
 # fmt: on
 
@@ -138,7 +130,6 @@ def my_function():
 
 # fmt: off
 # __fraction_resource_start__
-
 # Ray also supports fractional resource requirements.
 @ray.remote(num_gpus=0.5)
 def h():
