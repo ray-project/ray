2a9572a2688a0bcf45618a8e6dde72b14b9f9b6f
jianoaix <iamjianxiao@gmail.com>
2022-11-16 03:33:10 +0000

    code readability
    Signed-off-by: jianoaix <iamjianxiao@gmail.com>

--- python/ray/data/read_api.py
+++ python/ray/data/read_api.py
@@ -255,6 +255,7 @@ def read_datasource(
 
     if ray_remote_args is None:
         ray_remote_args = {}
+
     paths = read_args.get("paths", None)
     if paths and _is_local_scheme(paths):
         if ray.util.client.ray.is_connected():
@@ -266,6 +267,12 @@ def read_datasource(
             soft=False,
         )
         read_args["local_uri"] = True
+
+    if (
+        "scheduling_strategy" not in ray_remote_args
+        and ctx.scheduling_strategy == DEFAULT_SCHEDULING_STRATEGY
+    ):
+        ray_remote_args["scheduling_strategy"] = "SPREAD"
 
     # TODO(ekl) remove this feature flag.
     force_local = "RAY_DATASET_FORCE_LOCAL_METADATA" in os.environ
@@ -322,12 +329,6 @@ def read_datasource(
             "increase the number of "
             "dataset blocks."
         )
-
-    if (
-        "scheduling_strategy" not in ray_remote_args
-        and ctx.scheduling_strategy == DEFAULT_SCHEDULING_STRATEGY
-    ):
-        ray_remote_args["scheduling_strategy"] = "SPREAD"
 
     block_list = LazyBlockList(
         read_tasks, ray_remote_args=ray_remote_args, owned_by_consumer=False
