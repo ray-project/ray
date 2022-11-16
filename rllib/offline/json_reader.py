ab8b1cae074f706869430f4322087e79bfe55503
Avnish <avnishnarayan@gmail.com>
2022-11-15 21:15:31 -0800

    Fix test_io failures when connectors enabled.

--- rllib/offline/json_reader.py
+++ rllib/offline/json_reader.py
@@ -26,6 +26,7 @@ from ray.rllib.policy.sample_batch import (
     MultiAgentBatch,
     SampleBatch,
     concat_samples,
+    convert_ma_batch_to_sample_batch,
 )
 from ray.rllib.utils.annotations import override, PublicAPI, DeveloperAPI
 from ray.rllib.utils.compression import unpack_if_needed
@@ -306,6 +307,11 @@ class JsonReader(InputReader):
     def _postprocess_if_needed(self, batch: SampleBatchType) -> SampleBatchType:
         if not self.ioctx.config.get("postprocess_inputs"):
             return batch
+
+        if isinstance(batch, MultiAgentBatch) and set(batch.policy_batches.keys()) == {
+            DEFAULT_POLICY_ID
+        }:
+            batch = convert_ma_batch_to_sample_batch(batch)
 
         if isinstance(batch, SampleBatch):
             out = []
