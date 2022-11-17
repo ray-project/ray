2a9e34a6dbbc3c8f6e4b66ac5420cae16eb72f09
Artur Niederfahrenhorst <artur@anyscale.com>
2022-11-16 16:30:59 -0800

    fix bad observation space recovering method
    Signed-off-by: Artur Niederfahrenhorst <artur@anyscale.com>

--- rllib/utils/test_utils.py
+++ rllib/utils/test_utils.py
@@ -333,14 +333,7 @@ def check_compute_single_action(
         if what is algorithm:
             # Get the obs-space from Workers.env (not Policy) due to possible
             # pre-processor up front.
-            worker_set = getattr(algorithm, "workers", None)
-            assert worker_set
-            if isinstance(worker_set, list):
-                obs_space = algorithm.get_policy(pid).observation_space
-            else:
-                obs_space = worker_set.local_worker().for_policy(
-                    lambda p: p.observation_space, policy_id=pid
-                )
+            obs_space = algorithm.get_policy(pid).observation_space
             obs_space = getattr(obs_space, "original_space", obs_space)
             call_kwargs["full_fetch"] = full_fetch
             call_kwargs["policy_id"] = pid
@@ -507,21 +500,6 @@ def check_compute_single_action(
     # Loop through: Policy vs Algorithm; Different API methods to calculate
     # actions; unsquash option; clip option; full fetch or not.
     for what in [pol, algorithm]:
-        if what is algorithm:
-            # Get the obs-space from Workers.env (not Policy) due to possible
-            # pre-processor up front.
-            worker_set = getattr(algorithm, "workers", None)
-            assert worker_set
-            if not worker_set.local_worker():
-                obs_space = algorithm.get_policy(pid).observation_space
-            else:
-                obs_space = worker_set.local_worker().for_policy(
-                    lambda p: p.observation_space, policy_id=pid
-                )
-            obs_space = getattr(obs_space, "original_space", obs_space)
-        else:
-            obs_space = pol.observation_space
-
         for method_to_test in ["single"] + (["input_dict"] if what is pol else []):
             for explore in [True, False]:
                 timestep = random.randint(0, 100000)
