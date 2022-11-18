0f73ed04bb58cd21aafd6082b71fe72c4217d3b4
sven1977 <svenmika1977@gmail.com>
2022-11-18 10:52:44 +0100

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/utils/tests/run_memory_leak_tests.py
+++ rllib/utils/tests/run_memory_leak_tests.py
@@ -95,7 +95,6 @@ if __name__ == "__main__":
         # For python files, need to make sure, we only deliver the module name into the
         # `load_experiments_from_file` function (everything from "/ray/rllib" on).
         if file.endswith(".py"):
-            file = re.sub("^.*/ray/rllib/", "ray/rllib/", file)
             experiments = load_experiments_from_file(file, SupportedFileType.python)
         else:
             experiments = load_experiments_from_file(file, SupportedFileType.yaml)
