f99b7700ad9bcb9560db70e9a8876542aed209c3
Clarence Ng <clarence.wyng@gmail.com>
2022-11-18 12:11:10 -0800

    [core] telemetry for oom occurences
    Signed-off-by: Clarence Ng <clarence.wyng@gmail.com>

--- python/ray/_private/usage/usage_lib.py
+++ python/ray/_private/usage/usage_lib.py
@@ -632,6 +632,7 @@ def get_extra_usage_tags_to_report(gcs_client) -> Dict[str, str]:
         except Exception as e:
             logger.info(f"Failed to parse extra usage tags env var. Error: {e}")
 
+    valid_tag_keys = [tag_key.name.lower() for tag_key in TagKey]
     try:
         keys = gcs_client.internal_kv_keys(
             usage_constant.EXTRA_USAGE_TAG_PREFIX.encode(),
@@ -643,6 +644,7 @@ def get_extra_usage_tags_to_report(gcs_client) -> Dict[str, str]:
             )
             key = key.decode("utf-8")
             key = key[len(usage_constant.EXTRA_USAGE_TAG_PREFIX) :]
+            assert key in valid_tag_keys
             extra_usage_tags[key] = value.decode("utf-8")
     except Exception as e:
         logger.info(f"Failed to get extra usage tags from kv store {e}")
