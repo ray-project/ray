699a7cc14f49b9d78f5f337793659809783b50fd
scv119 <scv119@gmail.com>
2022-11-20 15:45:35 -0800

    add

---
+++ doc/source/ray-core/doc_code/actors.py
@@ -0,0 +1,71 @@
+# flake8: noqa
+# __actor_start__
+import ray
+
+@ray.remote
+class Counter(object):
+    def __init__(self):
+        self.value = 0
+
+    def increment(self):
+        self.value += 1
+        return self.value
+
+    def get_counter(self):
+        return self.value
+
+# Create an actor from this class.
+counter = Counter.remote()
+# __actor_end__
+
+# __resource_start
+# Specify required resources for an actor.
+@ray.remote(num_cpus=2, num_gpus=0.5)
+class Actor(object):
+    pass
+# __resource_end
+
+# __call_start
+# Call the actor.
+obj_ref = counter.increment.remote()
+assert ray.get(obj_ref) == 1
+# __call_end
+
+
+# __multi_call_start
+# Create ten Counter actors.
+counters = [Counter.remote() for _ in range(10)]
+
+# Increment each Counter once and get the results. These tasks all happen in
+# parallel.
+results = ray.get([c.increment.remote() for c in counters])
+print(results)  # prints [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+# Increment the first Counter five times. These tasks are executed serially
+# and share state.
+results = ray.get([counters[0].increment.remote() for _ in range(5)])
+print(results)  # prints [2, 3, 4, 5, 6]
+# __multi_call_end
+
+
+# __pass_handle_f_start
+import time
+
+@ray.remote
+def f(counter):
+    for _ in range(1000):
+        time.sleep(0.1)
+        counter.increment.remote()
+# __pass_handle_f_end
+
+# __pass_handle_start
+counter = Counter.remote()
+
+# Start some tasks that use the actor.
+[f.remote(counter) for _ in range(3)]
+
+# Print the counter value.
+for _ in range(10):
+    time.sleep(1)
+    print(ray.get(counter.get_counter.remote()))
+# __pass_handle_end
