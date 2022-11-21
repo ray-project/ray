034e1dfa730a62a0df178e3a392ab919399ac908
SangBin Cho <rkooo567@gmail.com>
2022-11-21 01:04:22 -0800

    lint
    Signed-off-by: SangBin Cho <rkooo567@gmail.com>

--- src/ray/raylet/agent_manager.cc
+++ src/ray/raylet/agent_manager.cc
@@ -134,17 +134,15 @@ void AgentManager::StartAgent() {
 
     RAY_LOG(ERROR)
         << "The raylet exited immediately because the Ray agent failed. "
-            "The raylet fate shares with the agent. This can happen because the "
-            "Ray agent was unexpectedly killed or failed. See "
-            "`dashboard_agent.log` for the root cause.";
+           "The raylet fate shares with the agent. This can happen because the "
+           "Ray agent was unexpectedly killed or failed. See "
+           "`dashboard_agent.log` for the root cause.";
     // Sending a SIGTERM to itself is equivalent to gracefully shutting down raylet.
     RAY_CHECK(std::raise(SIGTERM) == 0) << "There was a failure while sending a "
-                                            "sigterm to itself. The process will not "
-                                            "gracefully shutdown.";
+                                           "sigterm to itself. The process will not "
+                                           "gracefully shutdown.";
     // If the process is not terminated within 10 seconds, forcefully kill itself.
-    delay_executor_([]() {
-      QuickExit();
-    }, /*ms*/10000)
+    delay_executor_([]() { QuickExit(); }, /*ms*/ 10000)
   });
   monitor_thread.detach();
 }
