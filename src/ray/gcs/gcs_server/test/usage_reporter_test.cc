35c6b5450d286bbefba570e734344df3eb2060f0
scv119 <scv119@gmail.com>
2022-11-20 23:52:47 -0800

    add

--- src/ray/gcs/gcs_server/test/usage_reporter_test.cc
+++ src/ray/gcs/gcs_server/test/usage_reporter_test.cc
@@ -16,17 +16,54 @@
 
 #include "gtest/gtest.h"
 #include "ray/common/test_util.h"
+#include "ray/gcs/gcs_server/store_client_kv.h"
+#include "ray/gcs/store_client/in_memory_store_client.h"
 
 namespace ray {
 
 class UsageReporterTest : public ::testing::Test {
  public:
-  void SetUp() override {}
+  void SetUp() override {
+    thread_io_service = std::make_unique<std::thread>([this] {
+      boost::asio::io_service::work work(io_service);
+      io_service.run();
+    });
+
+    kv = std::make_shared<ray::gcs::StoreClientInternalKV>(
+        std::make_unique<ray::gcs::InMemoryStoreClient>(io_service));
+    reporter = std::make_shared<ray::gcs::GcsUsageReporter>(io_service, kv);
+  }
+
+  void TearDown() override {
+    io_service.stop();
+    thread_io_service->join();
+    reporter.reset();
+    kv.reset();
+  }
+
+  void ExecuteIOService() {
+    std::promise<int> p;
+    std::future<int> f = p.get_future();
+    io_service.post([&p]() { p.set_value(1); }, "");
+    f.get();
+  }
+
+  instrumented_io_context io_service;
+  std::unique_ptr<std::thread> thread_io_service;
+  std::shared_ptr<ray::gcs::InternalKVInterface> kv;
+  std::shared_ptr<ray::gcs::GcsUsageReporter> reporter;
 };
 
 TEST_F(UsageReporterTest, SmokeTest) {
-  auto key = usage::TagKey::_TEST1;
-  ASSERT_TRUE(key == usage::TagKey::_TEST1);
+  reporter->ReportCounter(ray::usage::TagKey::_TEST1, 3);
+  ExecuteIOService();
+  kv->Get("usage_stats", "extra_usage_tag__test1", [](auto b) { ASSERT_EQ("3", *b); });
+  ExecuteIOService();
+  reporter->ReportValue(ray::usage::TagKey::_TEST2, "hello");
+  ExecuteIOService();
+  kv->Get(
+      "usage_stats", "extra_usage_tag__test2", [](auto b) { ASSERT_EQ("hello", *b); });
+  ExecuteIOService();
 }
 
 }  // namespace ray
