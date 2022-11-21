8b7e949b26fbfbcbce425c8de8b95b72310f32c3
Avnish <avnishnarayan@gmail.com>
2022-11-21 10:44:34 -0800

    Remove value error that disables stopping criteria
    Signed-off-by: Avnish <avnishnarayan@gmail.com>

--- rllib/train.py
+++ rllib/train.py
@@ -83,8 +83,6 @@ def load_experiments_from_file(
     if file_type == SupportedFileType.yaml:
         with open(config_file) as f:
             experiments = yaml.safe_load(f)
-            if stop is not None:
-                raise ValueError("`stop` criteria only supported for python files.")
     # Python file case (ensured by file type enum)
     else:
         module_name = os.path.basename(config_file).replace(".py", "")
