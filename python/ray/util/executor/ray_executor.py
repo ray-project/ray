9295548a3d7f92f00cf42e8e3719a1dc2591087d
mohamed <mohamed.nidabdella@tweag.io>
2022-11-22 15:36:49 +0100

    WIP add mapping and actor submit

--- python/ray/util/executor/ray_executor.py
+++ python/ray/util/executor/ray_executor.py
@@ -10,15 +10,22 @@ class RayExecutor(Executor):
     @staticmethod
     @ray.remote
     def __remote_fn(fn, arg, **kwargs):
-        return fn(arg, **kwargs)
+        return fn(arg, **kwargs) if args else fn()
+
+    @staticmethod
+    def __actor_fn(fn, args):
+        return fn.remote(args) if args else fn.remote()
 
     def submit(self, fn, /, *args, **kwargs):
         return self.__remote_fn.remote(fn, *args, **kwargs).future()
 
+    def submit_actors(self, fn, *args, **kwargs):
+        return self.__actor_fn(fn, *args, **kwargs).future()
+
     def map(self, func, *iterables, timeout=None, chunksize=1):
-        # self.timeout = timeout
-        # return map(lambda i: ray.get(self.__remote_fn.remote(func, i)), iterables)
-        raise NotImplemented
+        self.timeout = timeout
+        # https://docs.ray.io/en/releases-1.10.0/ray-design-patterns/map-reduce.html
+        return [ray.get(self.__remote_fn.remote(func, i)) for i in iterables]
 
     def shutdown(self, wait=True, *, cancel_futures=False):
         ray.shutdown()
