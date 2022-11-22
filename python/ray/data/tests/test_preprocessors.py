20d7ca77ae9d96124979e167c5160f8863b1e68f
Balaji Veeramani <bveeramani@berkeley.edu>
2022-11-21 23:19:07 -0800

    Add tests

--- python/ray/data/tests/test_preprocessors.py
+++ python/ray/data/tests/test_preprocessors.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 import pyarrow
 import pytest
+import torchvision
 
 import ray
 from ray.data.preprocessor import Preprocessor, PreprocessorNotFittedException
@@ -20,6 +21,7 @@ from ray.data.preprocessors import (
     OrdinalEncoder,
     SimpleImputer,
     StandardScaler,
+    TorchPreprocessor,
     CustomKBinsDiscretizer,
     UniformKBinsDiscretizer,
 )
@@ -1852,6 +1854,62 @@ def test_numpy_pandas_support_transform_batch_tensor(create_dummy_preprocessors)
     assert isinstance(with_pandas_and_numpy.transform_batch(np_data), np.ndarray)
     assert isinstance(with_pandas_and_numpy.transform_batch(np_single_column), dict)
     assert isinstance(with_pandas_and_numpy.transform_batch(np_multi_column), dict)
+
+
+class TestTorchPreprocessor:
+    def test_repr(self):
+        class StubTransform:
+            def __call__(self, tensor):
+                return tensor
+
+            def __repr__(self):
+                return "StubTransform()"
+
+        preprocessor = TorchPreprocessor(columns=["spam"], transform=StubTransform())
+        assert (
+            repr(preprocessor)
+            == "TorchPreprocessor(columns=['spam'], transform=StubTransform())"
+        )
+
+    def test_transform_images(self):
+        dataset = ray.data.from_items(
+            [
+                {"image": np.zeros((32, 32, 3)), "label": 0},
+                {"image": np.zeros((32, 32, 3)), "label": 1},
+            ]
+        )
+        transform = torchvision.transforms.ToTensor()
+        preprocessor = TorchPreprocessor(columns=["image"], transform=transform)
+
+        transformed_dataset = preprocessor.transform(dataset)
+
+        transformed_images = [
+            record["image"] for record in transformed_dataset.take_all()
+        ]
+        assert all(image.shape == (3, 32, 32) for image in transformed_images)
+        assert all(image.dtype == np.double for image in transformed_images)
+
+    def test_transform_ragged_images(self):
+        dataset = ray.data.from_items(
+            [
+                {"image": np.zeros((16, 16, 3)), "label": 0},
+                {"image": np.zeros((32, 32, 3)), "label": 1},
+            ]
+        )
+        transform = torchvision.transforms.ToTensor()
+        preprocessor = TorchPreprocessor(columns=["image"], transform=transform)
+
+        transformed_dataset = preprocessor.transform(dataset)
+
+        assert transformed_dataset.schema().names == ["image"]
+        transformed_images = [
+            record["image"] for record in transformed_dataset.take_all()
+        ]
+        assert sorted(image.shape for image in transformed_images) == [
+            (3, 16, 16),
+            (3, 32, 32),
+        ]
+        assert all(image.dtype == np.double for image in transformed_images)
 
 
 if __name__ == "__main__":
