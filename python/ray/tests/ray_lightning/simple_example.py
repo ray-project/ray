1230972ded9003fba31ae368f81031f40ce97858
amogkam <amogkamsetty@yahoo.com>
2022-11-21 17:45:59 -0800

    update
    Signed-off-by: amogkam <amogkamsetty@yahoo.com>

--- python/ray/tests/ray_lightning/simple_example.py
+++ python/ray/tests/ray_lightning/simple_example.py
@@ -1,29 +1,16 @@
 # This file is duplicated in release/ml_user_tests/ray-lightning
-import argparse
-import os
+# flake8: noqa
+
+# __pl_module_init__
 import torch
 from torch import nn
 import torch.nn.functional as F
-from torchvision.datasets import MNIST
-from torch.utils.data import DataLoader, random_split
-from torchvision import transforms
+
 import pytorch_lightning as pl
-
-from importlib_metadata import version
-from packaging.version import parse as v_parse
-
-rlt_use_master = v_parse(version("ray_lightning")) >= v_parse("0.3.0")
-
-if rlt_use_master:
-    # ray_lightning >= 0.3.0
-    from ray_lightning import RayStrategy
-else:
-    # ray_lightning < 0.3.0
-    from ray_lightning import RayPlugin
 
 
 class LitAutoEncoder(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, lr=1e-1):
         super().__init__()
         self.encoder = nn.Sequential(
             nn.Linear(28 * 28, 128), nn.ReLU(), nn.Linear(128, 3)
@@ -31,6 +18,7 @@ class LitAutoEncoder(pl.LightningModule):
         self.decoder = nn.Sequential(
             nn.Linear(3, 128), nn.ReLU(), nn.Linear(128, 28 * 28)
         )
+        self.lr = lr
 
     def forward(self, x):
         # in lightning, forward defines the prediction/inference actions
@@ -48,52 +36,31 @@ class LitAutoEncoder(pl.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         return optimizer
 
 
-def main(num_workers: int = 2, use_gpu: bool = False, max_steps: int = 10):
-    dataset = MNIST(os.getcwd(), download=True, transform=transforms.ToTensor())
-    train, val = random_split(dataset, [55000, 5000])
+# __pl_module_end__
 
-    autoencoder = LitAutoEncoder()
-    if rlt_use_master:
-        trainer = pl.Trainer(
-            strategy=RayStrategy(num_workers=num_workers, use_gpu=use_gpu),
-            max_steps=max_steps,
-        )
-    else:
-        trainer = pl.Trainer(
-            plugins=[RayPlugin(num_workers=num_workers, use_gpu=use_gpu)],
-            max_steps=max_steps,
-        )
-    trainer.fit(autoencoder, DataLoader(train), DataLoader(val))
+# __train_begin__
+import os
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import MNIST
+from torchvision import transforms
 
+from ray_lightning import RayStrategy
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Ray Lightning Example",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-    parser.add_argument(
-        "--num-workers",
-        type=int,
-        default=2,
-        help="Number of workers to use for training.",
-    )
-    parser.add_argument(
-        "--max-steps",
-        type=int,
-        default=10,
-        help="Maximum number of steps to run for training.",
-    )
-    parser.add_argument(
-        "--use-gpu",
-        action="store_true",
-        default=False,
-        help="Whether to enable GPU training.",
-    )
+num_workers = 2
+use_gpu = False
+max_steps = 10
 
-    args = parser.parse_args()
+dataset = MNIST(os.getcwd(), download=True, transform=transforms.ToTensor())
+train, val = random_split(dataset, [55000, 5000])
 
-    main(num_workers=args.num_workers, max_steps=args.max_steps, use_gpu=args.use_gpu)
+autoencoder = LitAutoEncoder()
+trainer = pl.Trainer(
+    strategy=RayStrategy(num_workers=num_workers, use_gpu=use_gpu),
+    max_steps=max_steps,
+)
+trainer.fit(autoencoder, DataLoader(train), DataLoader(val))
+# __train_end__
