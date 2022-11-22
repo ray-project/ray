3c06c2c782f70890d2bdbc190cc1209145968b38
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2022-11-22 15:09:38 -0800

    wip
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- rllib/evaluation/collectors/agent_collector.py
+++ rllib/evaluation/collectors/agent_collector.py
@@ -188,6 +188,12 @@ class AgentCollector:
             self.unroll_id = AgentCollector._next_unroll_id
             AgentCollector._next_unroll_id += 1
 
+        # convert init_obs to np.array (in case it is not a numpy array already)
+        try:
+            init_obs = np.asarray(init_obs)
+        except Exception:
+            pass
+
         # Check if view requirement dict has the SampleBatch.OBS key and warn once if
         # view requirement does not match init_obs
         self._check_view_requirement(SampleBatch.OBS, init_obs)
@@ -255,6 +261,12 @@ class AgentCollector:
         assert SampleBatch.OBS not in values
         values[SampleBatch.OBS] = values[SampleBatch.NEXT_OBS]
         del values[SampleBatch.NEXT_OBS]
+
+        # convert init_obs to np.array (in case it is not a numpy array already)
+        try:
+            values[SampleBatch.OBS] = np.asarray(values[SampleBatch.OBS])
+        except Exception:
+            pass
 
         # Default to next timestep if not provided in input values
         if SampleBatch.T not in input_values:
