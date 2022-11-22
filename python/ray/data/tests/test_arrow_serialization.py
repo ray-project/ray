a632a42fdf11bca3d8194a03579361ed2abcca02
Clark Zinzow <clarkzinzow@gmail.com>
2022-11-21 19:59:02 -0800

    [Datasets] Revert to manual buffer copying custom Arrow serializer. (#30532)
    Reverts custom Arrow serialization path to manual buffer copying, since using the Arrow IPC format currently breaks zero-copy pickle serialization for Arrow tables.

--- python/ray/data/tests/test_arrow_serialization.py
+++ python/ray/data/tests/test_arrow_serialization.py
@@ -1,4 +1,7 @@
+import logging
 import os
+import sys
+from unittest import mock
 
 from pkg_resources._vendor.packaging.version import parse as parse_version
 import pytest
@@ -10,10 +13,140 @@ import ray
 import ray.cloudpickle as pickle
 from ray._private.utils import _get_pyarrow_version
 from ray.tests.conftest import *  # noqa
+from ray.data._internal.arrow_serialization import (
+    _bytes_for_bits,
+    _align_bit_offset,
+    _copy_buffer_if_needed,
+    _copy_normal_buffer_if_needed,
+    _copy_bitpacked_buffer_if_needed,
+    _copy_offsets_buffer_if_needed,
+)
 from ray.data.extensions.tensor_extension import (
     ArrowTensorArray,
     ArrowVariableShapedTensorArray,
 )
+
+
+@pytest.mark.parametrize(
+    "n,expected",
+    [(0, 0)] + [(i, 8) for i in range(1, 9)] + [(i, 16) for i in range(9, 17)],
+)
+def test_bytes_for_bits_manual(n, expected):
+    assert _bytes_for_bits(n) == expected
+
+
+def test_bytes_for_bits_auto():
+    M = 128
+    expected = [((n - 1) // 8 + 1) * 8 for n in range(M)]
+    for n, e in enumerate(expected):
+        assert _bytes_for_bits(n) == e, n
+
+
+def test_align_bit_offset_auto():
+    M = 10
+    n = M * (2**8 - 1)
+    # Represent an integer as a Pyarrow buffer of bytes.
+    bytes_ = n.to_bytes(M, sys.byteorder)
+    buf = pa.py_buffer(bytes_)
+    for slice_len in range(1, M):
+        for bit_offset in range(1, n - slice_len * 8):
+            byte_length = _bytes_for_bits(bit_offset + slice_len * 8) // 8
+            # Shift the buffer to eliminate the offset.
+            out_buf = _align_bit_offset(buf, bit_offset, byte_length)
+            # Check that shifted buffer is equivalent to our base int shifted by the
+            # same number of bits.
+            assert int.from_bytes(out_buf.to_pybytes(), sys.byteorder) == (
+                n >> bit_offset
+            )
+
+
+@mock.patch("ray.data._internal.arrow_serialization._copy_normal_buffer_if_needed")
+@mock.patch("ray.data._internal.arrow_serialization._copy_bitpacked_buffer_if_needed")
+def test_copy_buffer_if_needed(mock_bitpacked, mock_normal):
+    # Test that type-based buffer copy dispatch works as expected.
+    bytes_ = b"abcd"
+    buf = pa.py_buffer(bytes_)
+    offset = 1
+    length = 2
+
+    # Normal (non-boolean) buffer copy path.
+    type_ = pa.int32()
+    _copy_buffer_if_needed(buf, type_, offset, length)
+    expected_byte_width = 4
+    mock_normal.assert_called_once_with(buf, expected_byte_width, offset, length)
+    mock_normal.reset_mock()
+
+    type_ = pa.int64()
+    _copy_buffer_if_needed(buf, type_, offset, length)
+    expected_byte_width = 8
+    mock_normal.assert_called_once_with(buf, expected_byte_width, offset, length)
+    mock_normal.reset_mock()
+
+    # Boolean buffer copy path.
+    type_ = pa.bool_()
+    _copy_buffer_if_needed(buf, type_, offset, length)
+    mock_bitpacked.assert_called_once_with(buf, offset, length)
+    mock_bitpacked.reset_mock()
+
+
+def test_copy_normal_buffer_if_needed():
+    bytes_ = b"abcd"
+    buf = pa.py_buffer(bytes_)
+    byte_width = 1
+    uncopied_buf = _copy_normal_buffer_if_needed(buf, byte_width, 0, len(bytes_))
+    assert uncopied_buf.address == buf.address
+    assert uncopied_buf.size == len(bytes_)
+    for offset in range(1, len(bytes_) - 1):
+        for length in range(1, len(bytes_) - offset):
+            copied_buf = _copy_normal_buffer_if_needed(buf, byte_width, offset, length)
+            assert copied_buf.address != buf.address
+            assert copied_buf.size == length
+
+
+def test_copy_bitpacked_buffer_if_needed():
+    M = 20
+    n = M * 8
+    # Represent an integer as a pyarrow buffer of bytes.
+    bytes_ = (n * 8).to_bytes(M, sys.byteorder)
+    buf = pa.py_buffer(bytes_)
+    for offset in range(0, n - 1):
+        for length in range(1, n - offset):
+            copied_buf = _copy_bitpacked_buffer_if_needed(buf, offset, length)
+            if offset > 0:
+                assert copied_buf.address != buf.address
+            else:
+                assert copied_buf.address == buf.address
+            # Buffer needs to include bits remaining in byte after adjusting for bit
+            # offset..
+            assert copied_buf.size == ((length + (offset % 8) - 1) // 8) + 1
+
+
+@pytest.mark.parametrize(
+    "arr_type,expected_offset_type",
+    [
+        (pa.list_(pa.int64()), pa.int32()),
+        (pa.string(), pa.int32()),
+        (pa.binary(), pa.int32()),
+        (pa.large_list(pa.int64()), pa.int64()),
+        (pa.large_string(), pa.int64()),
+        (pa.large_binary(), pa.int64()),
+    ],
+)
+def test_copy_offsets_buffer_if_needed(arr_type, expected_offset_type):
+    offset_arr = pa.array([0, 1, 3, 6, 10, 15, 21], type=expected_offset_type)
+    buf = offset_arr.buffers()[1]
+    offset = 2
+    length = 3
+    offset_buf, data_offset, data_length = _copy_offsets_buffer_if_needed(
+        buf, arr_type, offset, length
+    )
+    assert data_offset == 3
+    assert data_length == 12
+    truncated_offset_arr = pa.Array.from_buffers(
+        expected_offset_type, length, [None, offset_buf]
+    )
+    expected_offset_arr = pa.array([0, 3, 7], type=expected_offset_type)
+    assert truncated_offset_arr.equals(expected_offset_arr)
 
 
 pytest_custom_serialization_arrays = [
@@ -30,8 +163,15 @@ pytest_custom_serialization_arrays = [
     (pa.array([True, False] * 500), 0.8),
     # String array
     (pa.array(["foo", "bar", "bz", None, "quux"] * 200), 0.1),
+    # Large string array
+    (pa.array(["foo", "bar", "bz", None, "quux"] * 200, type=pa.large_string()), 0.1),
     # Binary array
     (pa.array([b"foo", b"bar", b"bz", None, b"quux"] * 200), 0.1),
+    # Large binary array
+    (
+        pa.array([b"foo", b"bar", b"bz", None, b"quux"] * 200, type=pa.large_binary()),
+        0.1,
+    ),
     # List array with nulls
     (pa.array(([None] + [list(range(9)) + [None]] * 9) * 100), 0.1),
     # Large list array with nulls
@@ -206,10 +346,86 @@ def test_custom_arrow_data_serializer(ray_start_regular_shared, data, cap_mult):
     assert view.equals(post_slice), post_slice
     # Check that the slice view was truncated upon serialization.
     assert len(s_view) <= cap_mult * len(s_arr)
-    # Check that offset was reset on slice.
-    for column in post_slice.columns:
+    for column, pre_column in zip(post_slice.columns, view.columns):
+        # Check that offset was reset on slice.
         if column.num_chunks > 0:
             assert column.chunk(0).offset == 0
+        # Check that null count was either properly cached or recomputed.
+        assert column.null_count == pre_column.null_count
+    if pyarrow_version >= parse_version("7.0.0"):
+        # Check that slice buffer only contains slice data.
+        slice_buf_size = post_slice.get_total_buffer_size()
+        if buf_size > 0:
+            assert buf_size / slice_buf_size - len(data) / len(post_slice) < 100
+
+
+@pytest.fixture
+def propagate_logs():
+    # Ensure that logs are propagated to ancestor handles. This is required if using the
+    # caplog fixture with Ray's logging.
+    # NOTE: This only enables log propagation in the driver process, not the workers!
+    logger = logging.getLogger("ray")
+    logger.propagate = True
+    yield
+    logger.propagate = False
+
+
+def test_custom_arrow_data_serializer_fallback(
+    ray_start_regular_shared, propagate_logs, caplog
+):
+    # Reset serialization fallback set so warning is logged.
+    import ray.data._internal.arrow_serialization as arrow_ser_module
+
+    arrow_ser_module._serialization_fallback_set = set()
+
+    data = pa.table(
+        {
+            "a": pa.UnionArray.from_dense(
+                pa.array([0, 1] * 500, type=pa.int8()),
+                pa.array(
+                    [i if i % 2 == 0 else (i % 3) % 2 for i in range(1000)],
+                    type=pa.int32(),
+                ),
+                [pa.array(list(range(1000))), pa.array([True, False])],
+            )
+        }
+    )
+    cap_mult = 0.1
+    ray._private.worker.global_worker.get_serialization_context()
+    data.validate()
+    pyarrow_version = parse_version(_get_pyarrow_version())
+    if pyarrow_version >= parse_version("7.0.0"):
+        # get_total_buffer_size API was added in Arrow 7.0.0.
+        buf_size = data.get_total_buffer_size()
+    # Create a zero-copy slice view of data.
+    view = data.slice(10, 10)
+    # Confirm that (1) fallback works, and (2) warning is logged.
+    with caplog.at_level(
+        logging.WARNING,
+        logger="ray.data._internal.arrow_serialization",
+    ):
+        s_arr = pickle.dumps(data)
+    assert "Failed to complete optimized serialization" in caplog.text
+    caplog.clear()
+    # Confirm that we only warn once per process.
+    with caplog.at_level(
+        logging.WARNING,
+        logger="ray.data._internal.arrow_serialization",
+    ):
+        s_view = pickle.dumps(view)
+    assert "Failed to complete optimized serialization" not in caplog.text
+    post_slice = pickle.loads(s_view)
+    post_slice.validate()
+    # Check for round-trip equality.
+    assert view.equals(post_slice), post_slice
+    # Check that the slice view was truncated upon serialization.
+    assert len(s_view) <= cap_mult * len(s_arr)
+    for column, pre_column in zip(post_slice.columns, view.columns):
+        # Check that offset was reset on slice.
+        if column.num_chunks > 0:
+            assert column.chunk(0).offset == 0
+        # Check that null count was either properly cached or recomputed.
+        assert column.null_count == pre_column.null_count
     if pyarrow_version >= parse_version("7.0.0"):
         # Check that slice buffer only contains slice data.
         slice_buf_size = post_slice.get_total_buffer_size()
@@ -234,6 +450,7 @@ def test_custom_arrow_data_serializer_parquet_roundtrip(
 
 def test_custom_arrow_data_serializer_disable(shutdown_only):
     ray.shutdown()
+    ray.worker._post_init_hooks = []
     context = ray.worker.global_worker.get_serialization_context()
     context._unregister_cloudpickle_reducer(pa.Table)
     # Disable custom Arrow array serialization.
