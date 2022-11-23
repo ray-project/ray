8d9924ff09a46229cea127122c9589988b48a42b
clarng <clarence.wyng@gmail.com>
2022-11-23 14:43:18 -0800

    dash (#30594)
    Signed-off-by: Clarence Ng <clarence.wyng@gmail.com>
    
    Fix for Dashboard Tests.
    
    Currently the build breaks unless we migrate from createMuiTheme to createTheme

--- dashboard/client/src/theme.ts
+++ dashboard/client/src/theme.ts
@@ -1,5 +1,5 @@
 import { blueGrey, grey, lightBlue } from "@material-ui/core/colors";
-import { createMuiTheme, ThemeOptions } from "@material-ui/core/styles";
+import { createTheme, ThemeOptions } from "@material-ui/core/styles";
 
 const basicTheme: ThemeOptions = {
   typography: {
@@ -27,7 +27,7 @@ const basicTheme: ThemeOptions = {
   },
 };
 
-export const lightTheme = createMuiTheme({
+export const lightTheme = createTheme({
   ...basicTheme,
   palette: {
     primary: {
@@ -53,7 +53,7 @@ export const lightTheme = createMuiTheme({
   },
 });
 
-export const darkTheme = createMuiTheme({
+export const darkTheme = createTheme({
   ...basicTheme,
   palette: {
     primary: {
