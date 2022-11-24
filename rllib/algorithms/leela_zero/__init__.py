35e495fe353ceabe1cc30135a3eda4c6b0592356
capiru <gabriel_s_prado@hotmail.com>
2022-11-24 16:03:26 +0100

    added leela zero contrib
    Signed-off-by: Capiru <gabriel_s_prado@hotmail.com>

---
+++ rllib/algorithms/leela_zero/__init__.py
@@ -0,0 +1,13 @@
+from ray.rllib.algorithms.leela_zero.leela_zero import (
+    LeelaZero,
+    LeelaZeroConfig,
+    DEFAULT_CONFIG,
+)
+from ray.rllib.algorithms.leela_zero.leela_zero_policy import LeelaZeroPolicy
+
+__all__ = [
+    "LeelaZero",
+    "LeelaZeroConfig",
+    "LeelaZeroPolicy",
+    "DEFAULT_CONFIG",
+]
