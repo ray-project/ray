57da2b0f15a06f288952070480868378603b718c
jeicher <johanneicher@gmail.com>
2022-11-25 11:50:01 +0100

    expanded args, added some tests

--- python/ray/tests/test_executor.py
+++ python/ray/tests/test_executor.py
@@ -4,10 +4,6 @@ import sys
 import pytest
 from ray.util.executor.ray_executor import RayExecutor
 
-def f(i, **kwargs):
-    r = [x * x for x in range(i)]
-    return len(r)
-
 # @pytest.mark.skipif(
 #     sys.platform == "win32", reason="PSUtil does not work the same on windows."
 # )
@@ -19,24 +15,55 @@ def f(i, **kwargs):
 # def test_remote_function_runs_on_local_instance(call_ray_start):
 def test_remote_function_runs_on_local_instance():
     with RayExecutor() as ex:
-        result = ex.submit(f, 1_000)
-        assert result.result() == 1000
+        result = ex.submit(lambda x: len([i for i in range(x)]), 100).result()
+    assert result == 100
 
 @ray.remote
-class ActorTest:
+class ActorTest0:
     def __init__(self, name):
         self.name = name
 
-    def actor_function(self, args):
-        return f"{self.name}-Actor-{args[0]}"
+    def actor_function(self, arg):
+        return f"{self.name}-Actor-{arg}"
 
 def test_remote_actor_on_local_instance():
-    a = ActorTest.options(name="A", get_if_exists=True).remote("A")
+    a = ActorTest0.options(name="A", get_if_exists=True).remote("A")
     with RayExecutor() as ex:
-        name = ex.submit_actors(a.actor_function, 0)
+        name = ex.submit_actor_function(a.actor_function, 0)
     result = name.result()
     assert result == "A-Actor-0"
 
+@ray.remote
+class ActorTest1:
+    def __init__(self, name):
+        self.name = name
+
+    def actor_function(self, arg0, arg1, arg2, extra=None):
+        return f"{self.name}-Actor-{arg0}-{arg1}-{arg2}-{extra}"
+
+def test_remote_actor_on_local_instance_multiple_args():
+    a = ActorTest1.options(name="A", get_if_exists=True).remote("A")
+    with RayExecutor() as ex:
+        name = ex.submit_actor_function(a.actor_function, 0, 1, 2, extra=3)
+    result = name.result()
+    assert result == "A-Actor-0-1-2-3"
+
+@ray.remote
+class ActorTest2:
+    def __init__(self):
+        self.value = 0
+
+    def actor_function(self, i):
+        self.value += i
+        return self.value
+
+def test_remote_actor_on_local_instance_keeps_state():
+    a = ActorTest2.options(name="A", get_if_exists=True).remote()
+    with RayExecutor() as ex:
+        value1 = ex.submit_actor_function(a.actor_function, 1)
+        value2 = ex.submit_actor_function(a.actor_function, 1)
+    assert value1.result() == 1
+    assert value2.result() == 2
 
 
 if __name__ == "__main__":
--- python/ray/util/executor/ray_executor.py
+++ python/ray/util/executor/ray_executor.py
@@ -9,18 +9,18 @@ class RayExecutor(Executor):
 
     @staticmethod
     @ray.remote
-    def __remote_fn(fn, args, **kwargs):
-        return fn(args, **kwargs) if args else fn()
+    def __remote_fn(fn, *args, **kwargs):
+        return fn(*args, **kwargs)
 
     @staticmethod
-    def __actor_fn(fn, args):
-        return fn.remote(args) if args else fn.remote()
+    def __actor_fn(fn, *args, **kwargs):
+        return fn.remote(*args, **kwargs)
 
     def submit(self, fn, /, *args, **kwargs):
         return self.__remote_fn.remote(fn, *args, **kwargs).future()
 
-    def submit_actors(self, fn, *args, **kwargs):
-        return self.__actor_fn(fn, args, **kwargs).future()
+    def submit_actor_function(self, fn, *args, **kwargs):
+        return self.__actor_fn(fn, *args, **kwargs).future()
 
     def map(self, func, *iterables, timeout=None, chunksize=1):
         self.timeout = timeout
@@ -28,8 +28,8 @@ class RayExecutor(Executor):
         # https://docs.ray.io/en/releases-1.10.0/ray-design-patterns/map-reduce.html
         # Don't use ray.get inside loops:
         # https://docs.ray.io/en/releases-1.10.0/ray-design-patterns/ray-get-loop.html
-        map_remote = [self.__remote_fn.remote(func, i) for i in iterables]
-        return ray.get(map_remote)
+        futures = [self.__remote_fn.remote(func, i) for i in iterables]
+        return ray.get(futures)
 
     def shutdown(self, wait=True, *, cancel_futures=False):
         ray.shutdown()
