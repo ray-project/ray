7f4b8f9a7562c3dbe3c446687f7b86cbb4d7385f
Capiru <gabriel_s_prado@hotmail.com>
2022-11-25 08:38:29 +0100

    linting changes
    Signed-off-by: Capiru <gabriel_s_prado@hotmail.com>

--- rllib/examples/env/pettingzoo_chess.py
+++ rllib/examples/env/pettingzoo_chess.py
@@ -7,6 +7,179 @@ import chess as ch
 import numpy as np
 
 
+def PettingChessEnvWrapper():
+    env = PettingChessEnv()
+    env = wrappers.CaptureStdoutWrapper(env)
+    env = wrappers.TerminateIllegalWrapper(env, illegal_reward=-1)
+    env = wrappers.AssertOutOfBoundsWrapper(env)
+    env = wrappers.OrderEnforcingWrapper(env)
+    return env
+
+
+class PettingChessEnv(AECEnv):
+
+    metadata = {
+        "render_modes": ["human"],
+        "name": "chess_v6",
+        "is_parallelizable": False,
+        "render_fps": 2,
+    }
+
+    def __init__(self):
+        super().__init__()
+
+        self.board = ch.Board()
+
+        self.agents = [f"player_{i}" for i in range(2)]
+        self.possible_agents = self.agents[:]
+
+        self._agent_selector = agent_selector(self.agents)
+
+        self.action_spaces = {name: spaces.Discrete(8 * 8 * 73) for name in self.agents}
+        self.observation_spaces = {
+            name: spaces.Dict(
+                {
+                    "observation": spaces.Box(
+                        low=0, high=1, shape=(8, 8, 111), dtype=bool
+                    ),
+                    "action_mask": spaces.Box(
+                        low=0, high=1, shape=(4672,), dtype=np.int8
+                    ),
+                }
+            )
+            for name in self.agents
+        }
+
+        self.rewards = {name: 0 for name in self.agents}
+        self.dones = {name: False for name in self.agents}
+        self.infos = {name: {} for name in self.agents}
+
+        self.agent_selection = None
+
+        self.board_history = np.zeros((8, 8, 104), dtype=bool)
+
+    def observation_space(self, agent):
+        return self.observation_spaces[agent]
+
+    def action_space(self, agent):
+        return self.action_spaces[agent]
+
+    def observe(self, agent):
+        observation = chess_utils.get_observation(
+            self.board, self.possible_agents.index(agent)
+        )
+        observation = np.dstack((observation[:, :, :7], self.board_history))
+        legal_moves = (
+            chess_utils.legal_moves(self.board) if agent == self.agent_selection else []
+        )
+
+        action_mask = np.zeros(4672, "int8")
+        for i in legal_moves:
+            action_mask[i] = 1
+
+        return {"observation": observation, "action_mask": action_mask}
+
+    def reset(self, seed=None, return_info=False, options=None):
+        self.has_reset = True
+
+        self.agents = self.possible_agents[:]
+
+        self.board = ch.Board()
+
+        self._agent_selector = agent_selector(self.agents)
+        self.agent_selection = self._agent_selector.reset()
+
+        self.rewards = {name: 0 for name in self.agents}
+        self._cumulative_rewards = {name: 0 for name in self.agents}
+        self.dones = {name: False for name in self.agents}
+        self.infos = {name: {} for name in self.agents}
+
+        self.board_history = np.zeros((8, 8, 104), dtype=bool)
+        self.game_over = False
+
+    def set_game_result(self, result_val):
+        for i, name in enumerate(self.agents):
+            self.dones[name] = True
+            result_coef = 1 if i == 0 else -1
+            self.rewards[name] = result_val * result_coef
+            self.infos[name] = {"legal_moves": []}
+
+    def step(self, action):
+        if action is None or self.dones["player_0"] or self.dones["player_1"]:
+            return self._was_done_step(action)
+        current_agent = self.agent_selection
+        current_index = self.agents.index(current_agent)
+        # This current index mirrors the move
+        try:
+            chosen_move = chess_utils.action_to_move(self.board, action, current_index)
+        except Exception:
+            chosen_move = chess_utils.action_to_move(
+                self.board, action["player_" + str(current_index)], current_index
+            )
+        try:
+            assert chosen_move in list(self.board.legal_moves)
+        except AssertionError:
+            print(f"{current_agent} {current_index} chose {chosen_move} {action}")
+            print("legal moves:", self.board.legal_moves)
+            print(self.board)
+            self.game_over = True
+            self.set_game_result(not self.board.turn)
+            self._accumulate_rewards()
+            self.agent_selection = (
+                self._agent_selector.next()
+            )  # Give turn to the next agent
+            self.dones["player_0"], self.dones["player_1"] = True, True
+            return None
+
+        self.board.push(chosen_move)
+
+        next_legal_moves = chess_utils.legal_moves(self.board)
+
+        is_stale_or_checkmate = not any(next_legal_moves)
+
+        # claim draw is set to be true to align with normal tournament rules
+        is_repetition = self.board.is_repetition(3)
+        is_50_move_rule = self.board.can_claim_fifty_moves()
+        is_claimable_draw = is_repetition or is_50_move_rule
+        game_over = is_claimable_draw or is_stale_or_checkmate
+
+        if game_over:
+            self.game_over = True
+            result = self.board.result(claim_draw=True)
+            result_val = chess_utils.result_to_int(result)
+            self.set_game_result(result_val)
+
+        self._accumulate_rewards()
+
+        # Update board after applying action
+        next_board = chess_utils.get_observation(self.board, current_agent)
+        self.board_history = np.dstack(
+            (next_board[:, :, 7:], self.board_history[:, :, :-13])
+        )
+        self.agent_selection = (
+            self._agent_selector.next()
+        )  # Give turn to the next agent
+
+    def render(self, mode="human"):
+        print(self.board)
+
+    def close(self):
+        pass
+
+    def get_state(self):
+        return copy.deepcopy(self.env.board)
+
+    def set_state(self, state):
+        self.board = state
+        return self.board
+
+    def random_start(self, random_moves):
+        self.board = ch.Board()
+        for i in range(random_moves):
+            self.board.push(np.random.choice(list(self.board.legal_moves)))
+        return self.board
+
+
 class MultiAgentChess(MultiAgentEnv):
     """An interface to the PettingZoo MARL environment library.
     See: https://github.com/Farama-Foundation/PettingZoo
@@ -71,7 +244,7 @@ class MultiAgentChess(MultiAgentEnv):
     def __init__(
         self,
         config: Dict[Any, Any] = None,
-        env: AECEnv = chess_v5(),
+        env: PettingChessEnv = PettingChessEnv(),
     ):
         super().__init__()
         self.env = env
