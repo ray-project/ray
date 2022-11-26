699f387377ce3d2f8f90efe8edf9857093afcfd2
SangBin Cho <rkooo567@gmail.com>
2022-11-26 22:08:18 +0900

    Revert "[core] OOM killer: adjust defaults (#30553)" (#30652)
    This reverts commit e6940a1edcce93e2ab9f4b422e4b9829b8914f0f.
    
    Signed-off-by: Capiru <gabriel_s_prado@hotmail.com>

--- src/ray/common/ray_config_def.h
+++ src/ray/common/ray_config_def.h
@@ -84,7 +84,11 @@ RAY_CONFIG(uint64_t, raylet_check_gc_period_milliseconds, 100)
 /// memory_usage_threshold and free space is below the min_memory_free_bytes then
 /// it will start killing processes to free up the space.
 /// Ranging from [0, 1]
+<<<<<<< HEAD
 RAY_CONFIG(float, memory_usage_threshold, 0.95)
+=======
+RAY_CONFIG(float, memory_usage_threshold_fraction, 0.98)
+>>>>>>> ae0f8d72f (Revert "[core] OOM killer: adjust defaults (#30553)" (#30652))
 
 /// The interval between runs of the memory usage monitor.
 /// Monitor is disabled when this value is 0.
@@ -97,7 +101,7 @@ RAY_CONFIG(uint64_t, memory_monitor_refresh_ms, 250)
 /// This value is useful for larger host where the memory_usage_threshold could
 /// represent a large chunk of memory, e.g. a host with 64GB of memory and 0.9 threshold
 /// means 6.4 GB of the memory will not be usable.
-RAY_CONFIG(int64_t, min_memory_free_bytes, (int64_t)-1)
+RAY_CONFIG(int64_t, min_memory_free_bytes, (int64_t)512 * 1024 * 1024)
 
 /// The TTL for when the task failure entry is considered
 /// eligble for garbage colletion.
@@ -399,7 +403,7 @@ RAY_CONFIG(uint32_t, task_retry_delay_ms, 0)
 
 /// The base retry delay for exponential backoff when the task fails due to OOM.
 /// No delay if this value is zero.
-RAY_CONFIG(uint32_t, task_oom_retry_delay_base_ms, 1000)
+RAY_CONFIG(uint32_t, task_oom_retry_delay_base_ms, 0)
 
 /// Duration to wait between retrying to kill a task.
 RAY_CONFIG(uint32_t, cancellation_retry_ms, 2000)
