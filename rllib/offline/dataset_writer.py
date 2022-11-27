f22c9dca0f78be8dbd7085f8c6c87089dde45609
Simon Zehnder <simon.zehnder@gmail.com>
2022-11-27 20:33:45 +0000

    Ran the linter. All clear.
    Signed-off-by: Simon Zehnder <simon.zehnder@gmail.com>

--- rllib/offline/dataset_writer.py
+++ rllib/offline/dataset_writer.py
@@ -50,7 +50,7 @@ class DatasetWriter(OutputWriter):
         ), "output_config.path must be specified when using Dataset output."
 
         self.format = output_config["format"]
-        self.path = pathlib.Path(output_config["path"]).expanduser().resolve()      
+        self.path = pathlib.Path(output_config["path"]).expanduser().resolve()
         self.max_num_samples_per_file = (
             output_config["max_num_samples_per_file"]
             if "max_num_samples_per_file" in output_config
