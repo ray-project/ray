247eb12d5f85986d4bc5633fd1bbf8d5d1cd7a05
Scott Lee <sjl@anyscale.com>
2022-11-28 11:33:29 -0800

    update test to read directly from log file
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- python/ray/data/tests/test_dataset_logger.py
+++ python/ray/data/tests/test_dataset_logger.py
@@ -14,21 +14,16 @@ from ray.data._internal.dataset_logger import DatasetLogger
 
 def test_dataset_logger(shutdown_only):
     ray.init()
-    log_name, msg = "test_name", "test message 1234"
-    with patch("logging.open", mock_open(), create=True) as open_mock:
+    log_name, msg = "test_name", "test_message_1234"
+    logger = DatasetLogger(log_name).logger
+    logger.info(msg)
 
-        logger = DatasetLogger(log_name).logger
-        logger.info(msg)
-
+    # Read from log file, and parse each component of emitted log row
     session_dir = ray._private.worker._global_node.get_session_dir_path()
     log_file_path = os.path.join(session_dir, DatasetLogger.DEFAULT_DATASET_LOG_PATH)
-    open_mock.assert_called_with(log_file_path, "a", encoding=None)
-    open_mock.return_value.write.assert_called_once()
-
-    # Parse each component of emitted log row
-    raw_logged_msg = open_mock.return_value.write.call_args.args[0].strip()
-    log_header, logged_msg = raw_logged_msg.split(" -- ")
-    logged_ds, logged_ts, logged_info, logged_callpath = log_header.split()
+    with open(log_file_path, 'r') as f:
+        raw_logged_msg = f.read()
+    logged_ds, logged_ts, logged_level, logged_filepath, sep, logged_msg = raw_logged_msg.split()
 
     # Could not use freezegun to test exact timestamp value
     # (values off by some milliseconds), so instead we check
@@ -38,8 +33,8 @@ def test_dataset_logger(shutdown_only):
     except ValueError:
         raise Exception(f"Invalid log timestamp: {logged_ds} {logged_ts}")
 
-    assert logged_info == logging.getLevelName(logging.INFO)
-    assert re.match(r"test_dataset_logger.py:\d+", logged_callpath)
+    assert logged_level == logging.getLevelName(logging.INFO)
+    assert re.match(r"test_dataset_logger.py:\d+", logged_filepath)
     assert logged_msg == msg
 
 
