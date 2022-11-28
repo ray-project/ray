3ba8d86e794fba8f2e276656aef774c6c5ef49d1
kourosh hakhamaneshi <31483498+kouroshHakha@users.noreply.github.com>
2022-11-28 08:53:51 -0800

    [RLlib] Convert `MultiAgentBatch` to `SampleBatch` in offline_rl.py. (#30668)
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- rllib/examples/offline_rl.py
+++ rllib/examples/offline_rl.py
@@ -20,6 +20,7 @@ Use the generated file(s) as "input" in the CQL config below
 import argparse
 import numpy as np
 
+from ray.rllib.policy.sample_batch import convert_ma_batch_to_sample_batch
 from ray.rllib.algorithms import cql as cql
 from ray.rllib.utils.framework import try_import_torch
 from ray.rllib.execution.rollout_ops import (
@@ -136,6 +137,7 @@ if __name__ == "__main__":
     # Get a sample (MultiAgentBatch).
 
     batch = synchronous_parallel_sample(worker_set=cql_algorithm.workers)
+    batch = convert_ma_batch_to_sample_batch(batch)
     obs = torch.from_numpy(batch["obs"])
     # Pass the observations through our model to get the
     # features, which then to pass through the Q-head.
