1c94a0a6a5ea1faec4245412494558b9d5ae126e
Jun Gong <jungong@anyscale.com>
2022-11-30 14:54:35 -0800

    [RLlib] make sure adding connector enabled policies to Algorithm works. (#30732)
    Signed-off-by: Jun Gong <jungong@anyscale.com>

--- rllib/connectors/util.py
+++ rllib/connectors/util.py
@@ -92,11 +92,9 @@ def create_connectors_for_policy(policy: "Policy", config: TrainerConfigDict):
     """
     ctx: ConnectorContext = ConnectorContext.from_policy(policy)
 
-    assert policy.agent_connectors is None and policy.agent_connectors is None, (
-        "Can not create connectors for a policy that already has connectors. This "
-        "can happen if you add a Policy that has connectors attached to a "
-        "RolloutWorker with add_policy()."
-    )
+    assert (
+        policy.agent_connectors is None and policy.agent_connectors is None
+    ), "Can not create connectors for a policy that already has connectors."
 
     policy.agent_connectors = get_agent_connectors_from_config(ctx, config)
     policy.action_connectors = get_action_connectors_from_config(ctx, config)
