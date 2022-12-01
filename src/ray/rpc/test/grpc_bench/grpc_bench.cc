2ef1e6f8b67c5c4b0b5086423f1d44ca1d0aa7ec
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2022-12-01 06:40:03 +0000

    format Signed-off-by: Yi Cheng <74173148+iycheng@users.noreply.github.com>

--- src/ray/rpc/test/grpc_bench/grpc_bench.cc
+++ src/ray/rpc/test/grpc_bench/grpc_bench.cc
@@ -1,8 +1,8 @@
 #include "ray/common/asio/instrumented_io_context.h"
 #include "ray/rpc/grpc_server.h"
 #include "ray/rpc/server_call.h"
-#include "src/ray/rpc/test/grpc_bench/helloworld.pb.h"
 #include "src/ray/rpc/test/grpc_bench/helloworld.grpc.pb.h"
+#include "src/ray/rpc/test/grpc_bench/helloworld.pb.h"
 
 using namespace ray;
 using namespace ray::rpc;
@@ -10,17 +10,17 @@ using namespace helloworld;
 
 class GreeterHandler {
  public:
-  virtual void HandleSayHello(SayHelloRequest request,             
-                              SayHelloReply *reply,                
-                              SendReplyCallback send_reply_callback)  = 0;
+  virtual void HandleSayHello(SayHelloRequest request,
+                              SayHelloReply *reply,
+                              SendReplyCallback send_reply_callback) = 0;
   virtual ~GreeterHandler() {}
 };
 
 class GreeterServiceHandler : public GreeterHandler {
  public:
-  void HandleSayHello(SayHelloRequest request,             
-                      SayHelloReply *reply,                
-                      SendReplyCallback send_reply_callback)  override {
+  void HandleSayHello(SayHelloRequest request,
+                      SayHelloReply *reply,
+                      SendReplyCallback send_reply_callback) override {
     *reply->mutable_response() = std::move(*request.mutable_request());
     send_reply_callback(Status::OK(), nullptr, nullptr);
   }
@@ -37,10 +37,9 @@ class GreeterGrpcService : public GrpcService {
 
   void InitServerCallFactories(
       const std::unique_ptr<grpc::ServerCompletionQueue> &cq,
-      std::vector<std::unique_ptr<ServerCallFactory>> *server_call_factories) override {
-    RPC_SERVICE_HANDLER_SERVER_METRICS_DISABLED(Greeter, SayHello, -1)
-  }
-  
+      std::vector<std::unique_ptr<ServerCallFactory>> *server_call_factories) override{
+      RPC_SERVICE_HANDLER_SERVER_METRICS_DISABLED(Greeter, SayHello, -1)}
+
   /// The grpc async service object.
   Greeter::AsyncService service_;
 
@@ -50,9 +49,8 @@ class GreeterGrpcService : public GrpcService {
 
 int main() {
   const auto env = std::getenv("GRPC_SERVER_CPUS");
-  const auto parallelism =
-      env ? std::atoi(env) : std::thread::hardware_concurrency();
-  
+  const auto parallelism = env ? std::atoi(env) : std::thread::hardware_concurrency();
+
   GrpcServer server("grpc_bench", 50051, false, parallelism);
   instrumented_io_context main_service;
   std::thread t([&main_service] {
