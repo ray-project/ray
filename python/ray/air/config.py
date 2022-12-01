19a047b32d7bcd157ae93e674e61abe1e30ce862
amogkam <amogkamsetty@yahoo.com>
2022-11-30 16:38:54 -0800

    Add tests
    Signed-off-by: amogkam <amogkamsetty@yahoo.com>

--- python/ray/air/config.py
+++ python/ray/air/config.py
@@ -476,7 +476,7 @@ class FailureConfig:
         if self.fail_fast and self.max_failures != 0:
             raise ValueError("max_failures must be 0 if fail_fast=True.")
 
-        # Similar check as in TrialRunner
+        # Same check as in TrialRunner
         if not (isinstance(self.fail_fast, bool) or self.fail_fast.upper() == "RAISE"):
             raise ValueError(
                 "fail_fast must be one of {bool, 'raise'}. " f"Got {self.fail_fast}."
