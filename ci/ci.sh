7eefa40886fb7385c67c5b428dfc49e3ef55b44d
matthewdeng <matt@anyscale.com>
2022-12-01 11:23:28 -0800

    Revert "[CI] Add `__init__.py` to test directories (#28020)" (#30819)
    This reverts commit 03d172d8b6d266907745741eb87c26be56ce1b6e.
    
    Signed-off-by: tmynn <hovhannes.tamoyan@gmail.com>

--- ci/ci.sh
+++ ci/ci.sh
@@ -574,17 +574,6 @@ lint_web() {
   )
 }
 
-check_python_test_directories_contain_init_file() {
-  cd "${WORKSPACE_DIR}"
-  while IFS= read -r -d '' test_directory
-  do
-    if [ ! -e "$test_directory"/__init__.py ]; then
-      echo "Add '__init__.py' to '$test_directory'"
-      exit 1
-    fi
-  done <   <(find python -name "tests" -type d -print0)
-}
-
 lint_copyright() {
   (
     "${ROOT_DIR}"/lint/copyright-format.sh -c
@@ -621,9 +610,6 @@ _lint() {
 
   # Run annotations check.
   lint_annotations
-
-  # Check Python test directories contain `__init__.py` file.
-  check_python_test_directories_contain_init_file
 
   # Make sure that the README is formatted properly.
   lint_readme
