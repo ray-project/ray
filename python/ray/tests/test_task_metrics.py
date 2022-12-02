c9217863c31776325a1c7a027e5f808228da2404
Eric Liang <ekhliang@gmail.com>
2022-12-01 16:01:57 -0800

    add a test for actor task retries
    Signed-off-by: Eric Liang <ekhliang@gmail.com>

--- python/ray/tests/test_task_metrics.py
+++ python/ray/tests/test_task_metrics.py
@@ -367,6 +367,59 @@ time.sleep(999)
     proc.kill()
 
 
+def test_actor_task_retry(shutdown_only):
+    info = ray.init(num_cpus=2, **METRIC_CONFIG)
+
+    driver = """
+import ray
+import os
+import time
+
+ray.init("auto")
+
+@ray.remote
+class Phaser:
+    def __init__(self):
+        self.i = 0
+
+    def inc(self):
+        self.i += 1
+        if self.i < 3:
+            raise ValueError("First two tries will fail")
+
+phaser = Phaser.remote()
+
+@ray.remote(max_restarts=10, max_task_retries=10)
+class F:
+    def f(self):
+        try:
+            ray.get(phaser.inc.remote())
+        except Exception:
+            print("RESTART")
+            os._exit(1)
+
+f = F.remote()
+ray.get(f.f.remote())
+time.sleep(999)
+"""
+
+    proc = run_string_as_driver_nonblocking(driver)
+    expected = {
+        ("F.__init__", "FINISHED", "0"): 1.0,
+        ("F.f", "FAILED", "0"): 1.0,
+        ("F.f", "FAILED", "1"): 1.0,
+        ("F.f", "FINISHED", "1"): 1.0,
+        ("Phaser.__init__", "FINISHED", "0"): 1.0,
+        ("Phaser.inc", "FINISHED", "0"): 1.0,
+    }
+    wait_for_condition(
+        lambda: tasks_by_all(info) == expected,
+        timeout=20,
+        retry_interval_ms=500,
+    )
+    proc.kill()
+
+
 @pytest.mark.skipif(sys.platform == "win32", reason="Flaky on Windows.")
 def test_task_failure(shutdown_only):
     info = ray.init(num_cpus=2, **METRIC_CONFIG)
