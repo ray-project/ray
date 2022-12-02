333c4eafe40d80309acd34fbe970c19996c62f2a
avnish <avnish@anyscale.com>
2022-12-02 09:32:19 -0800

    Add tests for set and get state methods
    Signed-off-by: avnish <avnish@anyscale.com>

--- rllib/core/optim/tests/test_rl_optimizer.py
+++ rllib/core/optim/tests/test_rl_optimizer.py
@@ -1,4 +1,5 @@
 import gym
+import pytest
 import numpy as np
 import torch
 from typing import Any, List, Mapping
@@ -11,10 +12,7 @@ from ray.rllib.offline.dataset_reader import (
 )
 
 import ray
-from ray.rllib.core.examples.simple_ppo_rl_module import (
-    FCNet,
-    FCConfig
-)
+from ray.rllib.core.examples.simple_ppo_rl_module import FCNet, FCConfig
 from ray.rllib.core.optim.rl_optimizer import RLOptimizer
 from ray.rllib.core.rl_module.rl_module import RLModule
 from ray.rllib.core.rl_module.torch.torch_rl_module import TorchRLModule
@@ -22,20 +20,23 @@ from ray.rllib.core.rl_module.torch.tests.test_torch_rl_module import (
     to_numpy,
     to_tensor,
 )
+from ray.rllib.models.distributions import Distribution
+from ray.rllib.models.specs.specs_dict import ModelSpec
+from ray.rllib.models.specs.specs_torch import TorchTensorSpec
 from ray.rllib.models.torch.torch_distributions import (
-    TorchDeterministic, TorchCategorical)
+    TorchDeterministic,
+    TorchCategorical,
+)
 from ray.rllib.policy.sample_batch import SampleBatch
 from ray.rllib.utils.nested_dict import NestedDict
 from ray.rllib.utils.numpy import convert_to_numpy
+from ray.rllib.utils.test_utils import check
 from ray.rllib.utils.torch_utils import convert_to_torch_tensor
 
-from ray.rllib.models.specs.specs_dict import ModelSpec, check_specs
-from ray.rllib.models.specs.specs_torch import TorchTensorSpec
-from ray.rllib.models.distributions import Distribution
 
 def model_norm(model: torch.nn.Module) -> float:
     """Compute the norm of the weights of a model.
-    
+
     Args:
         model: The model whose weights to compute a norm over.
 
@@ -46,11 +47,13 @@ def model_norm(model: torch.nn.Module) -> float:
     for p in model.parameters():
         param_norm = p.detach().data.norm(2)
         total_norm += param_norm.item() ** 2
-    total_norm = total_norm ** 0.5
+    total_norm = total_norm**0.5
     return total_norm
 
 
-def do_rollouts(env: gym.Env, module_for_inference: RLModule, num_rollouts: int) -> SampleBatch:
+def do_rollouts(
+    env: gym.Env, module_for_inference: RLModule, num_rollouts: int
+) -> SampleBatch:
     _returns = []
     _rollouts = []
     for _ in range(num_rollouts):
@@ -59,7 +62,9 @@ def do_rollouts(env: gym.Env, module_for_inference: RLModule, num_rollouts: int)
         _return = -0
         for _ in range(env._max_episode_steps):
             _obs.append(obs)
-            fwd_out = module_for_inference.forward_inference({"obs": to_tensor(obs)[None]})
+            fwd_out = module_for_inference.forward_inference(
+                {"obs": to_tensor(obs)[None]}
+            )
             action = to_numpy(fwd_out["action_dist"].sample().squeeze(0))
             next_obs, reward, done, _ = env.step(action)
             _next_obs.append(next_obs)
@@ -85,7 +90,6 @@ def do_rollouts(env: gym.Env, module_for_inference: RLModule, num_rollouts: int)
 
 
 class BCTorchModule(TorchRLModule):
-
     def __init__(self, config: FCConfig) -> None:
         super().__init__(config)
         self.policy = FCNet(config)
@@ -165,25 +169,29 @@ class BCTorchOptimizer(RLOptimizer):
     def __init__(self, module, config):
         super().__init__(module, config)
 
-    def compute_loss(self, batch: NestedDict, fwd_out: Mapping[str, Any]) -> Mapping[str, Any]:
+    def compute_loss(
+        self, batch: NestedDict, fwd_out: Mapping[str, Any]
+    ) -> Mapping[str, Any]:
         """Compute a loss"""
         action_dist = fwd_out["action_dist"]
-        loss = - action_dist.logp(batch[SampleBatch.ACTIONS]).mean()
+        loss = -action_dist.logp(batch[SampleBatch.ACTIONS]).mean()
         loss_dict = {
             "total_loss": loss,
         }
         return loss_dict
 
     def _configure_optimizers(self) -> List[torch.optim.Optimizer]:
-        return [
-            torch.optim.SGD(self.module.parameters(), lr=self._config.get("lr", 1e-3))
-        ]
+        return {
+            "module": torch.optim.SGD(
+                self.module.parameters(), lr=self._config.get("lr", 1e-3)
+            )
+        }
 
     def get_state(self):
-        return {"module" : optim.state_dict() for optim in self._optimizers}
+        return {key: optim.state_dict() for key, optim in self.get_optimizers().items()}
 
     def set_state(self, state: Mapping[Any, Any]) -> None:
-        assert set(state.keys()) == set(self.get_optimizers().keys())
+        assert set(state.keys()) == set(self.get_state().keys())
         for key, optim_dict in state.items():
             self.get_optimizers()[key].load_state_dict(optim_dict)
 
@@ -196,12 +204,14 @@ class BCTorchTrainer:
         self._rl_optimizer = BCTorchOptimizer(self._module, optimizer_config)
 
     @staticmethod
-    def on_after_compute_gradients(gradients_dict: Mapping[str, Any]) -> Mapping[str, Any]:
+    def on_after_compute_gradients(
+        gradients_dict: Mapping[str, Any]
+    ) -> Mapping[str, Any]:
         """Called after gradients have been computed.
-        
+
         Args:
             gradients_dict (Mapping[str, Any]): A dictionary of gradients.
-        
+
         Note the relative order of operations looks like this:
             fwd_out = forward_train(batch)
             loss = compute_loss(batch, fwd_out)
@@ -215,10 +225,14 @@ class BCTorchTrainer:
         return gradients_dict
 
     def compile_results(
-        self, batch: NestedDict, fwd_out: Mapping[str, Any], postprocessed_loss: Mapping[str, Any], post_processed_gradients: Mapping[str, Any]
-    )-> Mapping[str, Any]:
+        self,
+        batch: NestedDict,
+        fwd_out: Mapping[str, Any],
+        postprocessed_loss: Mapping[str, Any],
+        post_processed_gradients: Mapping[str, Any],
+    ) -> Mapping[str, Any]:
         """Compile results from the update.
-        
+
         Args:
             batch: The batch that was used for the update.
             fwd_out: The output of the forward train pass.
@@ -239,10 +253,10 @@ class BCTorchTrainer:
 
     def update(self, batch: NestedDict) -> Mapping[str, Any]:
         """Perform an update on this Trainer.
-        
+
         Args:
             batch: A batch of data.
-        
+
         Returns:
             A dictionary of results.
         """
@@ -252,12 +266,12 @@ class BCTorchTrainer:
         gradients = self.compute_gradients(loss)
         post_processed_gradients = self.on_after_compute_gradients(gradients)
         self.apply_gradients(post_processed_gradients)
-        results = self.compile_results(
-            batch, fwd_out, loss, post_processed_gradients
-        )
+        results = self.compile_results(batch, fwd_out, loss, post_processed_gradients)
         return results
 
-    def compute_gradients(self, optimization_vars: Mapping[str, Any]) -> Mapping[str, Any]:
+    def compute_gradients(
+        self, optimization_vars: Mapping[str, Any]
+    ) -> Mapping[str, Any]:
         """Perform an update on self._module
 
             For example compute and apply gradients to self._module if
@@ -276,7 +290,7 @@ class BCTorchTrainer:
 
     def apply_gradients(self, gradients: Mapping[str, Any]) -> None:
         """Perform an update on self._module"""
-        for optimizer in self._rl_optimizer.get_optimizers():
+        for optimizer in self._rl_optimizer.get_optimizers().values():
             optimizer.step()
             optimizer.zero_grad()
 
@@ -292,12 +306,15 @@ class BCTorchTrainer:
         }
 
 
-def test_rl_optimizer_torch():
+def test_rl_optimizer_example_torch():
     ray.init()
     torch.manual_seed(1)
     env = gym.make("CartPole-v1")
-    module_config = FCConfig(input_dim=sum(env.observation_space.shape),
-        output_dim=sum(env.action_space.shape or [env.action_space.n]), hidden_layers=[32])
+    module_config = FCConfig(
+        input_dim=sum(env.observation_space.shape),
+        output_dim=sum(env.action_space.shape or [env.action_space.n]),
+        hidden_layers=[32],
+    )
     module_for_inference = BCTorchModule(module_config)
 
     trainer = BCTorchTrainer({"module_config": module_config})
@@ -311,12 +328,12 @@ def test_rl_optimizer_torch():
     )
     batch_size = 500
     ioctx = IOContext(
-            config=(
-                AlgorithmConfig()
-                .training(train_batch_size=batch_size)
-                .offline_data(actions_in_input_normalized=True)
-            ),
-            worker_index=0,
+        config=(
+            AlgorithmConfig()
+            .training(train_batch_size=batch_size)
+            .offline_data(actions_in_input_normalized=True)
+        ),
+        worker_index=0,
     )
     reader = DatasetReader(dataset, ioctx)
     num_epochs = 100
@@ -330,11 +347,32 @@ def test_rl_optimizer_torch():
         avg_return, _, _ = do_rollouts(env, module_for_inference, 10)
         if avg_return > 50:
             break
-    assert avg_return > 50, f"Return for training behavior cloning is too low: avg_return={avg_return}!"
+    assert (
+        avg_return > 50
+    ), f"Return for training behavior cloning is too low: avg_return={avg_return}!"
+
+
+def test_rl_optimizer_set_state_get_state_torch():
+    env = gym.make("CartPole-v1")
+    module_config = FCConfig(
+        input_dim=sum(env.observation_space.shape),
+        output_dim=sum(env.action_space.shape or [env.action_space.n]),
+        hidden_layers=[32],
+    )
+    module = BCTorchModule(module_config)
+    optim1 = BCTorchOptimizer(module, {"lr": 0.1})
+    optim2 = BCTorchOptimizer(module, {"lr": 0.2})
+
+    assert isinstance(
+        optim1.get_state(), dict
+    ), "rl_optimizer.get_state() should return a dict"
+    with pytest.raises(AssertionError):
+        check(optim1.get_state(), optim2.get_state())
+    optim1.set_state(optim2.get_state())
+    check(optim1.get_state(), optim2.get_state())
 
 
 if __name__ == "__main__":
-    import pytest
     import sys
 
     sys.exit(pytest.main(["-v", __file__]))
