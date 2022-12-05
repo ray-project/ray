201675111d7c37edabe369f6d090645823600928
Justin Yu <justinvyu@berkeley.edu>
2022-12-05 10:37:40 -0800

    [Tune] Fix validation error message and docstring of `num_to_keep` in `CheckpointConfig` (#30782)
    This PR makes the following fixes:
    1. The `CheckpointConfig` docstring should accurately reflect the requirement that `num_to_keep >= 1`.
    2. `keep_checkpoints_num` in the error should be `num_to_keep` instead, since the Tune checkpoint manager takes in a `CheckpointConfig` now.
    
    Signed-off-by: Justin Yu <justinvyu@berkeley.edu>
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- python/ray/tune/execution/checkpoint_manager.py
+++ python/ray/tune/execution/checkpoint_manager.py
@@ -19,9 +19,8 @@ class _CheckpointManager(CommonCheckpointManager):
     initialized to Checkpoint objects with values of None.
 
     Args:
-        keep_checkpoints_num: Keep at least this many checkpoints.
-        checkpoint_score_attr: Attribute to use to determine which
-            checkpoints to keep.
+        checkpoint_config: An optional checkpoint configuration that
+            determines checkpointing strategy.
         delete_fn: Function that deletes checkpoints. Must be
             idempotent.
     """
@@ -34,13 +33,6 @@ class _CheckpointManager(CommonCheckpointManager):
         delete_fn: Optional[Callable[["_TrackedCheckpoint"], None]] = None,
     ):
         checkpoint_config = checkpoint_config or CheckpointConfig()
-
-        if checkpoint_config.num_to_keep == 0:
-            raise RuntimeError(
-                "If checkpointing is enabled, Ray Tune requires `keep_checkpoints_num` "
-                "to be None or a number greater than 0"
-            )
-
         super().__init__(checkpoint_strategy=checkpoint_config, delete_fn=delete_fn)
 
     def handle_checkpoint(self, checkpoint: _TrackedCheckpoint):
