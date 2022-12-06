e88bc5514d03fa0ad0ab5ee12d306f0b35f3d3ff
Steven Morad <smorad@anyscale.com>
2022-12-06 08:37:58 -1000

    bork
    Signed-off-by: Steven Morad <smorad@anyscale.com>

--- rllib/torch_cnn.py
+++ rllib/torch_cnn.py
@@ -21,10 +21,11 @@ config = PPOConfig()
 batch_size = 1024
 # feature_size = (84, 84, 1)
 num_epochs = 10
-device = 'cpu'
+device = "cpu"
 env = gym.wrappers.AtariPreprocessing(gym.make("SpaceInvaders"), frame_skip=1)
 # h, w, c
 feature_size = env.observation_space.shape
+
 
 def get_torch_data(device="cpu", unsqueeze=False):
     obs = torch.empty(batch_size, *feature_size, device=device)
@@ -39,8 +40,9 @@ def get_torch_data(device="cpu", unsqueeze=False):
         "advantages": torch.rand(batch_size, device=device),
         "action_dist_inputs": torch.rand(batch_size, 6, device=device),
     }
-    #data = SampleBatch(**data)
+    # data = SampleBatch(**data)
     return data
+
 
 def get_tf_data():
     pass
@@ -48,6 +50,7 @@ def get_tf_data():
 
 class TorchNatureCNN(nn.Module):
     tower_stats = {}
+
     def __init__(self, obs_space, action_space, num_outputs, *args, **kwargs):
         super().__init__()
         """
@@ -96,9 +99,9 @@ class TorchNatureCNN(nn.Module):
         x = input_dict["obs"].reshape(-1, *feature_size, 1).permute(0, 3, 1, 2).float()
         x = self.cnn(x)
         logits = self.logits(x).reshape(-1, 6)
-        #x = self.cnn(x)
-        #x = self.mlp(x)
-        #logits = self.logits(x)
+        # x = self.cnn(x)
+        # x = self.mlp(x)
+        # logits = self.logits(x)
         self.value = self.vf(x).reshape(-1)
         return logits, []
 
@@ -142,7 +145,11 @@ class TF2V2NatureCNN(TFModelV2):
         )
         TorchNatureCNN.__init__(self)
         self.cnn = keras.models.Sequential()
-        self.cnn.add(keras.layers.Conv2d(32, 8, 4, activation="relu", input_shape=(feature_size[-1], 3)))
+        self.cnn.add(
+            keras.layers.Conv2d(
+                32, 8, 4, activation="relu", input_shape=(feature_size[-1], 3)
+            )
+        )
         self.cnn.add(keras.layers.Conv2d(64, 4, 2, activation="relu"))
         self.cnn.add(keras.layers.Conv2d(64, 3, 1, activation="relu"))
 
@@ -164,25 +171,31 @@ class TF2V2NatureCNN(TFModelV2):
 
     def value_function(self):
         return self.value
-    
+
 
 torch_policy = PPOTorchPolicy(env.observation_space, env.action_space, PPOConfig())
 
 torch_basemodel = TorchNatureCNN(env.observation_space, env.action_space, 6).to(device)
-torchv2_model = TorchV2NatureCNN(env.observation_space, env.action_space, 6, ModelConfigDict(), "bork").to(device)
-obs_space = gym.spaces.Box(dtype=np.uint8, low=0, high=255, shape=(84,84,1))
+torchv2_model = TorchV2NatureCNN(
+    env.observation_space, env.action_space, 6, ModelConfigDict(), "bork"
+).to(device)
+obs_space = gym.spaces.Box(dtype=np.uint8, low=0, high=255, shape=(84, 84, 1))
 # 16
 # 32
 # 256
-torch_visionnet = VisionNetwork(obs_space, env.action_space, 6, ModelConfigDict(vf_share_layers=True), "bork").to(device)
+torch_visionnet = VisionNetwork(
+    obs_space, env.action_space, 6, ModelConfigDict(vf_share_layers=True), "bork"
+).to(device)
 # 21, 21, 16
 # 11, 11, 32
 # 1, 1, 256,
 # 1, 1, 6
-tf_visionnet = KerasVisionNetwork(obs_space, env.action_space, 6, ModelConfigDict(vf_share_layers=True), "bork")
+tf_visionnet = KerasVisionNetwork(
+    obs_space, env.action_space, 6, ModelConfigDict(vf_share_layers=True), "bork"
+)
 
-#torch_models = [torch_basemodel, torchv2_model, torch_visionnet]
-torch_models = []
+torch_models = [torch_basemodel, torchv2_model, torch_visionnet]
+#torch_models = []
 
 for model in torch_models:
     print("Model:", model.__class__.__name__)
@@ -190,15 +203,17 @@ for model in torch_models:
     unsqueeze = isinstance(model, VisionNetwork)
     datas = [get_torch_data(device, unsqueeze) for _ in range(num_epochs)]
     times = []
-    if device != 'cpu':
+    if device != "cpu":
         torch.cuda.synchronize()
     for i in range(num_epochs):
         start = time.time()
         torch_opt.zero_grad()
-        loss = torch_policy.loss(model=model, dist_class=TorchCategorical, train_batch=datas[i])
+        loss = torch_policy.loss(
+            model=model, dist_class=TorchCategorical, train_batch=datas[i]
+        )
         loss.backward()
         torch_opt.step()
-        if device != 'cpu':
+        if device != "cpu":
             torch.cuda.synchronize()
         end = time.time()
         times.append(end - start)
@@ -212,7 +227,7 @@ for i in range(num_epochs):
     loss = tf_visionnet.loss(existing_model=model, train_batch=datas[i])
     loss.backward()
     torch_opt.step()
-    if device != 'cpu':
+    if device != "cpu":
         torch.cuda.synchronize()
     end = time.time()
     times.append(end - start)
