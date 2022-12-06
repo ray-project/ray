f38002b4007a7bd97c078b5f0d196e7c27995fc3
rickyyx <rickyx@anyscale.com>
2022-12-06 21:58:21 +0000

    disabled
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -229,10 +229,10 @@ CoreWorker::CoreWorker(const CoreWorkerOptions &options, const WorkerID &worker_
       worker_context_, options_.node_ip_address, io_service_, gcs_client_);
 
   // Initialize the task state event buffer.
+  auto task_event_gcs_client = std::make_unique<gcs::GcsClient>(options_.gcs_options);
+  task_event_buffer_ =
+      std::make_unique<worker::TaskEventBufferImpl>(std::move(task_event_gcs_client));
   if (RayConfig::instance().task_events_report_interval_ms() > 0) {
-    auto task_event_gcs_client = std::make_unique<gcs::GcsClient>(options_.gcs_options);
-    task_event_buffer_ =
-        std::make_unique<worker::TaskEventBufferImpl>(std::move(task_event_gcs_client));
     if (!task_event_buffer_->Start().ok()) {
       RAY_CHECK(!task_event_buffer_->Enabled()) << "TaskEventBuffer should be disabled.";
     }
