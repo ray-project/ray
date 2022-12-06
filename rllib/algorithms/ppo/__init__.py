69c41aa8b6ffa618060e81dbae28b94d50cedb41
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2022-12-05 19:07:54 -0800

    removed rl_module from importing ppo
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- rllib/algorithms/ppo/__init__.py
+++ rllib/algorithms/ppo/__init__.py
@@ -1,9 +1,6 @@
 from ray.rllib.algorithms.ppo.ppo import PPOConfig, PPO, DEFAULT_CONFIG
 from ray.rllib.algorithms.ppo.ppo_tf_policy import PPOTF1Policy, PPOTF2Policy
 from ray.rllib.algorithms.ppo.ppo_torch_policy import PPOTorchPolicy
-from ray.rllib.algorithms.ppo.torch.ppo_torch_policy_with_rl_module import (
-    PPOTorchPolicyWithRLModule,
-)
 
 __all__ = [
     "PPOConfig",
@@ -12,5 +9,4 @@ __all__ = [
     "PPOTorchPolicy",
     "PPO",
     "DEFAULT_CONFIG",
-    "PPOTorchPolicyWithRLModule",
 ]
