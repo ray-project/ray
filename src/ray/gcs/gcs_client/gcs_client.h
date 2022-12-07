5d088eff91830e9fe7195a46810ae4feb104a7df
Ricky Xu <xuchen727@hotmail.com>
2022-12-06 18:58:07 -0800

    [core][state] Task events backend - worker task event buffer implementation [1/n] (#30867)
    In this PR:
    
    Added a TaskEventBuffer class which serves as an abstraction to store task events, and push those events to GCS in batches periodically.
    Each CoreWorker will own one single TaskEventBuffer, and events (both task status change events and profiling events) will be added to a local in-memory buffer.
    The TaskEventBuffer also owns its own GCS client and io thread, which is independent from the main io_contexts.
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- src/ray/gcs/gcs_client/gcs_client.h
+++ src/ray/gcs/gcs_client/gcs_client.h
@@ -136,6 +136,11 @@ class RAY_EXPORT GcsClient : public std::enable_shared_from_this<GcsClient> {
     return *stats_accessor_;
   }
 
+  TaskInfoAccessor &Tasks() {
+    RAY_CHECK(task_accessor_ != nullptr);
+    return *task_accessor_;
+  }
+
   /// Get the sub-interface for accessing worker information in GCS.
   /// This function is thread safe.
   WorkerInfoAccessor &Workers() {
@@ -170,6 +175,7 @@ class RAY_EXPORT GcsClient : public std::enable_shared_from_this<GcsClient> {
   std::unique_ptr<WorkerInfoAccessor> worker_accessor_;
   std::unique_ptr<PlacementGroupInfoAccessor> placement_group_accessor_;
   std::unique_ptr<InternalKVAccessor> internal_kv_accessor_;
+  std::unique_ptr<TaskInfoAccessor> task_accessor_;
 
  private:
   const UniqueID gcs_client_id_ = UniqueID::FromRandom();
