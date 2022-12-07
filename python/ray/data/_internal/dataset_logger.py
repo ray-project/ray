108be71a36564ae6e963a1bb3e0ecf2ed2425c0d
Scott Lee <scottjlee@users.noreply.github.com>
2022-12-07 13:40:57 -0800

    [Datasets] Persist Datasets statistics to log file (#30557)
    Currently, when we print Dataset stats after execution, there is no way to retrieve this information in case of job failure/crash. By persisting the logs to a separate file, we can access the stats which could be helpful for debugging. By default, this is configured to write to /logs/ray-data.log.
    
    The new logger, DatasetLogger, is configured to always write logs to the ray-data.log file, and optionally also writes to stdout (this is enabled by default). The motivation behind this is so that users can easily use the specific log file to filter for Dataset logs, while still maintaining console logs for those who use them.
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

---
+++ python/ray/data/_internal/dataset_logger.py
@@ -0,0 +1,75 @@
+import logging
+import os
+
+import ray
+from ray._private.ray_constants import LOGGER_FORMAT, LOGGER_LEVEL
+
+
+class DatasetLogger:
+    """Logger for Ray Datasets which writes logs to a separate log file
+    at `DatasetLogger.DEFAULT_DATASET_LOG_PATH`. Can optionally turn off
+    logging to stdout to reduce clutter (but always logs to the aformentioned
+    Datasets-specific log file).
+
+    After initialization, always use the `get_logger()` method to correctly
+    set whether to log to stdout. Example usage:
+    ```
+    logger = DatasetLogger(__name__)
+    logger.get_logger().info("This logs to file and stdout")
+    logger.get_logger(log_to_stdout=False).info("This logs to file only)
+    logger.get_logger().warning("Can call the usual Logger methods")
+    ```
+    """
+
+    DEFAULT_DATASET_LOG_PATH = "logs/ray-data.log"
+
+    def __init__(self, log_name: str):
+        """Initialize DatasetLogger for a given `log_name`.
+
+        Args:
+            log_name: Name of logger (usually passed into `logging.getLogger(...)`)
+        """
+        # Logger used to logging to log file (in addition to the root logger,
+        # which logs to stdout as normal). For logging calls made with the
+        # parameter `log_to_stdout = False`, `_logger.propagate` will be set
+        # to `False` in order to prevent the root logger from writing the log
+        # to stdout.
+        self._logger = logging.getLogger(f"{log_name}.logfile")
+        # We need to set the log level again when explicitly
+        # initializing a new logger (otherwise can have undesirable level).
+        self._logger.setLevel(LOGGER_LEVEL.upper())
+
+        # Add log handler which writes to a separate Datasets log file
+        # at `DatasetLogger.DEFAULT_DATASET_LOG_PATH`
+        global_node = ray._private.worker._global_node
+        if global_node is not None:
+            # With current implementation, we can only get session_dir
+            # after ray.init() is called. A less hacky way could potentially fix this
+            session_dir = global_node.get_session_dir_path()
+            self.datasets_log_path = os.path.join(
+                session_dir,
+                DatasetLogger.DEFAULT_DATASET_LOG_PATH,
+            )
+            # Add a FileHandler to write to the specific Ray Datasets log file,
+            # using the standard default logger format used by the root logger
+            file_log_handler = logging.FileHandler(self.datasets_log_path)
+            file_log_formatter = logging.Formatter(fmt=LOGGER_FORMAT)
+            file_log_handler.setFormatter(file_log_formatter)
+            self._logger.addHandler(file_log_handler)
+
+    def get_logger(self, log_to_stdout: bool = True):
+        """
+        Returns the underlying Logger, with the `propagate` attribute set
+        to the same value as `log_to_stdout`. For example, when
+        `log_to_stdout = False`, we do not want the `DatasetLogger` to
+        propagate up to the base Logger which writes to stdout.
+
+        This is a workaround needed due to the DatasetLogger wrapper object
+        not having access to the log caller's scope in Python <3.8.
+        In the future, with Python 3.8 support, we can use the `stacklevel` arg,
+        which allows the logger to fetch the correct calling file/line and
+        also removes the need for this getter method:
+        `logger.info(msg="Hello world", stacklevel=2)`
+        """
+        self._logger.propagate = log_to_stdout
+        return self._logger
