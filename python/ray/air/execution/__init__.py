1ec026d5721a5dec6f6990496e90e70bacbc4b40
Kai Fricke <kai@anyscale.com>
2022-12-06 18:17:04 -0800

    Update docstring
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- python/ray/air/execution/__init__.py
+++ python/ray/air/execution/__init__.py
@@ -1,7 +1,6 @@
 from ray.air.execution.resources.request import ResourceRequest, AllocatedResource
 from ray.air.execution.resources.resource_manager import ResourceManager
 from ray.air.execution.resources.fixed import FixedResourceManager
-from ray.air.execution.resources.virtual import VirtualResourceManager
 from ray.air.execution.resources.placement_group import PlacementGroupResourceManager
 
 __all__ = [
@@ -9,6 +8,5 @@ __all__ = [
     "AllocatedResource",
     "ResourceManager",
     "FixedResourceManager",
-    "VirtualResourceManager",
     "PlacementGroupResourceManager",
 ]
