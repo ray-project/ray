fb3cef1a1f620593b4ee98fc83cb308938185b8e
Cheng Su <scnju13@gmail.com>
2022-12-08 17:50:38 -0800

    Address comments
    Signed-off-by: Cheng Su <scnju13@gmail.com>

--- release/nightly_tests/dataset/map_batches_benchmark.py
+++ release/nightly_tests/dataset/map_batches_benchmark.py
@@ -1,6 +1,7 @@
-from typing import Literal
+from typing import Literal, Optional, Union
 
 import ray
+from ray.data._internal.compute import ActorPoolStrategy, ComputeStrategy
 from ray.data.dataset import Dataset
 
 from benchmark import Benchmark
@@ -10,13 +11,17 @@ def map_batches(
     input_ds: Dataset,
     batch_size: int,
     batch_format: Literal["default", "pandas", "pyarrow", "numpy"],
-    num_calls: int,
+    compute: Optional[Union[str, ComputeStrategy]] = None,
+    num_calls: Optional[int] = 1,
 ) -> Dataset:
 
     ds = input_ds
     for _ in range(num_calls):
         ds = ds.map_batches(
-            lambda ds: ds, batch_format=batch_format, batch_size=batch_size
+            lambda ds: ds,
+            batch_format=batch_format,
+            batch_size=batch_size,
+            compute=compute,
         )
     return ds
 
@@ -62,24 +67,54 @@ def run_map_batches_benchmark(benchmark: Benchmark):
     # Test multiple calls of map_batches.
     for batch_format in batch_formats:
         for num_calls in num_calls_list:
-            batch_size = 4096
-            test_name = f"map-batches-{batch_format}-{batch_size}-{num_calls}-default"
+            for compute in ["tasks", ActorPoolStrategy(1, 1)]:
+                batch_size = 4096
+                if compute == "tasks":
+                    compute_strategy = "task"
+                else:
+                    compute_strategy = "actor"
+
+                test_name = (
+                    f"map-batches-{batch_format}-{batch_size}-{num_calls}-"
+                    f"{compute_strategy}-default"
+                )
+                benchmark.run(
+                    test_name,
+                    map_batches,
+                    input_ds=input_ds,
+                    batch_format=batch_format,
+                    batch_size=batch_size,
+                    compute=compute,
+                    num_calls=num_calls,
+                )
+                test_name = (
+                    f"map-batches-{batch_format}-{batch_size}-{num_calls}-"
+                    f"{compute_strategy}-lazy"
+                )
+                benchmark.run(
+                    test_name,
+                    map_batches,
+                    input_ds=lazy_input_ds,
+                    batch_format=batch_format,
+                    batch_size=batch_size,
+                    compute=compute,
+                    num_calls=num_calls,
+                )
+
+    # Test different batch formats of map_batches.
+    for current_format in ["pyarrow", "pandas", "numpy"]:
+        new_input_ds = input_ds.map_batches(
+            lambda ds: ds, batch_format=current_format, batch_size=None
+        ).fully_executed()
+        for new_format in ["pyarrow", "pandas", "numpy"]:
+            test_name = f"map-batches-{current_format}-to-{new_format}"
             benchmark.run(
                 test_name,
                 map_batches,
-                input_ds=input_ds,
-                batch_format=batch_format,
-                batch_size=batch_size,
-                num_calls=num_calls,
-            )
-            test_name = f"map-batches-{batch_format}-{batch_size}-{num_calls}-lazy"
-            benchmark.run(
-                test_name,
-                map_batches,
-                input_ds=lazy_input_ds,
-                batch_format=batch_format,
-                batch_size=batch_size,
-                num_calls=num_calls,
+                input_ds=new_input_ds,
+                batch_format=new_format,
+                batch_size=None,
+                num_calls=1,
             )
 
 
