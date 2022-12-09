0e0a495343f1ebedc255196e94bea2e23e13b483
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2022-12-08 16:28:06 -0800

    [runtime env] Print the failed key or value type when env_vars validation fails (#30974)
    The current env_vars validation currently doesn't show the key or value that violated the validation. This PR prints the offending key or value and its type for easier debugging. This came up in a UX study where a numeric value was passed instead of a str and it took a while to figure out the issue from the existing error message.
    
    Signed-off-by: tmynn <hovhannes.tamoyan@gmail.com>

--- python/ray/_private/runtime_env/validation.py
+++ python/ray/_private/runtime_env/validation.py
@@ -231,18 +231,39 @@ def parse_and_validate_env_vars(env_vars: Dict[str, str]) -> Optional[Dict[str, 
     This is validated to verify that all keys and vals are strings.
 
     If an empty dictionary is passed, we return `None` for consistency.
+
+    Args:
+        env_vars: A dictionary of environment variables to set in the
+            runtime environment.
+
+    Returns:
+        The validated env_vars dictionary, or None if it was empty.
+
+    Raises:
+        TypeError: If the env_vars is not a dictionary of strings. The error message
+            will include the type of the invalid value.
     """
     assert env_vars is not None
     if len(env_vars) == 0:
         return None
 
-    if not (
-        isinstance(env_vars, dict)
-        and all(
-            isinstance(k, str) and isinstance(v, str) for (k, v) in env_vars.items()
+    if not isinstance(env_vars, dict):
+        raise TypeError(
+            "runtime_env['env_vars'] must be of type "
+            f"Dict[str, str], got {type(env_vars)}"
         )
-    ):
-        raise TypeError("runtime_env['env_vars'] must be of type Dict[str, str]")
+
+    for key, val in env_vars.items():
+        if not isinstance(key, str):
+            raise TypeError(
+                "runtime_env['env_vars'] must be of type "
+                f"Dict[str, str], but the key {key} is of type {type(key)}"
+            )
+        if not isinstance(val, str):
+            raise TypeError(
+                "runtime_env['env_vars'] must be of type "
+                f"Dict[str, str], but the value {val} is of type {type(val)}"
+            )
 
     return env_vars
 
