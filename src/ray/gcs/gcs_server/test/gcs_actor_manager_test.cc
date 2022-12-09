20e3361caabf5a0113a25919f1d2b140e1c3270b
SangBin Cho <rkooo567@gmail.com>
2022-12-09 14:56:56 +0900

    [Metrics] Fix the dead actors are not counted properly when the driver exits (#30654)
    Signed-off-by: SangBin Cho <rkooo567@gmail.com>
    
    It's incorrect because as soon as the job is terminated we garbage collect GcsActor. It fixes the issue by never decrementing the DEAD count when the GCSActor is GC'ed. This is safe because we don't allow to transit states from DEAD -> other states.
    
    It is a replacement of #30523
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- src/ray/gcs/gcs_server/test/gcs_actor_manager_test.cc
+++ src/ray/gcs/gcs_server/test/gcs_actor_manager_test.cc
@@ -118,6 +118,12 @@ class GcsActorManagerTest : public ::testing::Test {
         delay_(0),
         skip_delay_(true),
         periodical_runner_(io_service_) {
+    RayConfig::instance().initialize(
+        R"(
+{
+  "maximum_gcs_destroyed_actor_cached_count": 10
+}
+  )");
     std::promise<bool> promise;
     thread_io_service_.reset(new std::thread([this, &promise] {
       std::unique_ptr<boost::asio::io_service::work> work(
@@ -306,6 +312,43 @@ TEST_F(GcsActorManagerTest, TestBasic) {
   ASSERT_EQ(actor->GetState(), rpc::ActorTableData::DEAD);
   RAY_CHECK_EQ(gcs_actor_manager_->CountFor(rpc::ActorTableData::ALIVE, ""), 0);
   RAY_CHECK_EQ(gcs_actor_manager_->CountFor(rpc::ActorTableData::DEAD, ""), 1);
+}
+
+TEST_F(GcsActorManagerTest, TestDeadCount) {
+  ///
+  /// Verify the DEAD count is correct after actors are GC'ed from the GCS.
+  /// Actors are GC'ed from the GCS when there are more than
+  /// maximum_gcs_destroyed_actor_cached_count dead actors.
+  ///
+
+  // Make sure we can cache only up to 10 dead actors.
+  ASSERT_EQ(RayConfig::instance().maximum_gcs_destroyed_actor_cached_count(), 10);
+  auto job_id = JobID::FromInt(1);
+
+  // Create 20 actors.
+  for (int i = 0; i < 20; i++) {
+    auto registered_actor = RegisterActor(job_id);
+    rpc::CreateActorRequest create_actor_request;
+    create_actor_request.mutable_task_spec()->CopyFrom(
+        registered_actor->GetCreationTaskSpecification().GetMessage());
+
+    Status status =
+        gcs_actor_manager_->CreateActor(create_actor_request,
+                                        [](const std::shared_ptr<gcs::GcsActor> &actor,
+                                           const rpc::PushTaskReply &reply,
+                                           bool creation_cancelled) {});
+    RAY_CHECK_OK(status);
+    auto actor = mock_actor_scheduler_->actors.back();
+    mock_actor_scheduler_->actors.pop_back();
+    // Check that the actor is in state `ALIVE`.
+    actor->UpdateAddress(RandomAddress());
+    gcs_actor_manager_->OnActorCreationSuccess(actor, rpc::PushTaskReply());
+    WaitActorCreated(actor->GetActorID());
+    // Actor is killed.
+    ASSERT_TRUE(worker_client_->Reply());
+    ASSERT_EQ(actor->GetState(), rpc::ActorTableData::DEAD);
+  }
+  RAY_CHECK_EQ(gcs_actor_manager_->CountFor(rpc::ActorTableData::DEAD, ""), 20);
 }
 
 TEST_F(GcsActorManagerTest, TestSchedulingFailed) {
