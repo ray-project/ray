40164f3b04ced09cc6fef0eb461e2a5f4823c450
Kai Fricke <kai@anyscale.com>
2022-12-09 16:25:24 -0800

    continue
    Signed-off-by: Kai Fricke <kai@anyscale.com>

---
+++ python/ray/air/execution/actor_request.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+from typing import Dict, Type
+
+from ray.air.execution.resources.request import (
+    ResourceRequest,
+    AllocatedResource,
+)
+
+
+@dataclass
+class ActorRequest:
+    cls: Type
+    kwargs: Dict
+    resources: ResourceRequest
+
+    def __hash__(self):
+        return hash(id(self))
+
+    def __eq__(self, other):
+        return self.__hash__() == other.__hash__()
+
+
+@dataclass
+class ActorInfo:
+    actor_request: ActorRequest
+    allocated_resource: AllocatedResource
+
+    def __hash__(self):
+        return hash(id(self))
+
+    def __eq__(self, other):
+        return self.__hash__() == other.__hash__()
