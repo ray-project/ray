214a559a79f4c65d2875140f41083d9f6c465742
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2022-12-10 01:29:07 +0000

    up

--- python/ray/tests/test_actor_failures.py
+++ python/ray/tests/test_actor_failures.py
@@ -406,7 +406,10 @@ def test_caller_task_reconstruction(ray_start_regular):
     "ray_start_cluster_head",
     [
         generate_system_config_map(
-            object_timeout_milliseconds=1000, num_heartbeats_timeout=10
+            object_timeout_milliseconds=1000,
+            health_check_initial_delay_ms=0,
+            health_check_period_ms=1000,
+            health_check_failure_threshold=10
         )
     ],
     indirect=True,
