6c32f4222215a292569b21e47b0f4c8cd5ea74bb
Weichen Xu <weichen.xu@databricks.com>
2022-12-12 15:15:41 +0800

    head node PR_SET_PDEATHSIG
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- python/ray/util/spark/cluster_init.py
+++ python/ray/util/spark/cluster_init.py
@@ -1,7 +1,6 @@
 import os
-import shutil
+import fcntl
 import socket
-import subprocess
 import sys
 import time
 import threading
@@ -23,9 +22,9 @@ from .utils import (
     get_dbutils,
     get_max_num_concurrent_tasks,
     get_target_spark_tasks,
-    _allocate_port_range_and_start_lock_barrier_thread_for_ray_worker_node_startup,
     _display_databricks_driver_proxy_url,
     gen_cmd_exec_failure_msg,
+    setup_sigterm_on_parent_death,
 )
 
 if not sys.platform.startswith("linux"):
@@ -175,6 +174,138 @@ def _convert_ray_node_options(options):
 _RAY_HEAD_STARTUP_TIMEOUT = 40
 _BACKGROUND_JOB_STARTUP_WAIT = 30
 _RAY_CLUSTER_STARTUP_PROGRESS_CHECKING_INTERVAL = 3
+
+
+def _allocate_port_range_and_start_lock_barrier_thread_for_ray_worker_node_startup():
+    """
+    If we start multiple ray workers on a machine concurrently, some ray worker processes
+    might fail due to ray port conflicts, this is because race condition on getting free
+    port and opening the free port.
+    To address the issue, this function use an exclusive file lock to delay the worker processes
+    to ensure that port acquisition does not create a resource contention issue due to a race
+    condition.
+
+    After acquiring lock, it will allocate port range for worker ports
+    (for ray node config --min-worker-port and --max-worker-port).
+    Because on a spark cluster, multiple ray cluster might be created, so on one spark worker
+    machine, there might be multiple ray worker nodes running, these worker nodes might belong
+    to different ray cluster, and we must ensure these ray nodes on the same machine using
+    non-overlapping worker port range, to achieve this, in this function, it creates a file
+    `/tmp/ray_on_spark_worker_port_allocation.txt` file, the file format is composed of multiple
+    lines, each line contains 2 number: `pid` and `port_range_slot_index`,
+    each port range slot allocates 1000 ports, and corresponding port range is:
+    range_begin (inclusive): 20000 + port_range_slot_index * 1000
+    range_end (exclusive): range_begin + 1000
+    In this function, it first scans `/tmp/ray_on_spark_worker_port_allocation.txt` file,
+    removing lines that containing dead process pid, then find the first unused
+    port_range_slot_index, then regenerate this file, and return the allocated port range.
+
+    Returns: Allocated port range for current worker ports
+    """
+    import psutil
+
+    def acquire_lock(file_path):
+        mode = os.O_RDWR | os.O_CREAT | os.O_TRUNC
+        try:
+            fd = os.open(file_path, mode)
+            # The lock file must be readable / writable to all users.
+            os.chmod(file_path, 0o0777)
+            # Allow for retrying getting a file lock a maximum number of seconds
+            max_lock_iter = 600
+            for _ in range(max_lock_iter):
+                try:
+                    fcntl.flock(fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+                except BlockingIOError:
+                    # Lock is used by other processes, continue loop to wait for lock available
+                    pass
+                else:
+                    # Acquire lock successfully.
+                    return fd
+                time.sleep(10)
+            raise TimeoutError(f"Acquiring lock on file {file_path} timeout.")
+        except Exception:
+            os.close(fd)
+
+    lock_file_path = "/tmp/ray_on_spark_worker_startup_barrier_lock.lock"
+    try:
+        lock_fd = acquire_lock(lock_file_path)
+    except TimeoutError:
+        # If timeout happens, the file lock might be hold by another process and that process
+        # does not release the lock in time by some unexpected reason.
+        # In this case, remove the existing lock file and create the file again, and then
+        # acquire file lock on the new file.
+        try:
+            os.remove(lock_file_path)
+        except Exception:
+            pass
+        lock_fd = acquire_lock(lock_file_path)
+
+    def release_lock():
+        fcntl.flock(lock_fd, fcntl.LOCK_UN)
+        os.close(lock_fd)
+
+    try:
+        port_alloc_file = "/tmp/ray_on_spark_worker_port_allocation.txt"
+
+        # NB: reading / writing `port_alloc_file` is protected by exclusive lock
+        # on file `lock_file_path`
+        if os.path.exists(port_alloc_file):
+            with open(port_alloc_file, mode="r") as fp:
+                port_alloc_data = fp.read()
+            port_alloc_table = [line.split(" ") for line in port_alloc_data.strip().split("\n")]
+            port_alloc_table = [
+                (int(pid_str), int(slot_index_str))
+                for pid_str, slot_index_str in port_alloc_table
+            ]
+        else:
+            port_alloc_table = []
+            with open(port_alloc_file, mode="w"):
+                pass
+            # The port range allocation file must be readable / writable to all users.
+            os.chmod(port_alloc_file, 0o0777)
+
+        port_alloc_map = {
+            pid: slot_index
+            for pid, slot_index in port_alloc_table
+            if psutil.pid_exists(pid)  # remove slot used by dead process
+        }
+
+        allocated_slot_set = set(port_alloc_map.values())
+
+        if len(allocated_slot_set) == 0:
+            new_slot_index = 0
+        else:
+            new_slot_index = max(allocated_slot_set) + 1
+            for index in range(new_slot_index):
+                if index not in allocated_slot_set:
+                    new_slot_index = index
+                    break
+
+        port_alloc_map[os.getpid()] = new_slot_index
+
+        with open(port_alloc_file, mode="w") as fp:
+            for pid, slot_index in port_alloc_map.items():
+                fp.write(f"{pid} {slot_index}\n")
+
+        worker_port_range_begin = 20000 + new_slot_index * 1000
+        worker_port_range_end = worker_port_range_begin + 1000
+
+        if worker_port_range_end > 65536:
+            raise RuntimeError(
+                "Too many ray worker nodes are running on this machine, cannot "
+                "allocate worker port range for new ray worker node."
+            )
+    except Exception:
+        release_lock()
+        raise
+
+    def hold_lock_for_10s_and_release():
+        time.sleep(10)
+        release_lock()
+
+    threading.Thread(target=hold_lock_for_10s_and_release, args=()).start()
+
+    return worker_port_range_begin, worker_port_range_end
 
 
 def _init_ray_cluster(
@@ -342,9 +473,12 @@ def _init_ray_cluster(
 
     _logger.info(f"Starting Ray head, command: {' '.join(ray_head_node_cmd)}")
 
+    # `preexec_fn=setup_sigterm_on_parent_death` ensures the ray head node being killed
+    # if parent process died unexpectedly.
     ray_head_proc, tail_output_deque = exec_cmd(
         ray_head_node_cmd,
         synchronous=False,
+        preexec_fn=setup_sigterm_on_parent_death,
     )
 
     # wait ray head node spin up.
@@ -424,7 +558,6 @@ def _init_ray_cluster(
             f"--temp-dir={ray_temp_dir}",
             f"--num-cpus={num_spark_task_cpus}",
             "--block",
-            "--disable-usage-stats",
             f"--address={ray_head_ip}:{ray_head_port}",
             f"--memory={ray_worker_node_heap_mem_bytes}",
             f"--object-store-memory={ray_worker_node_object_store_mem_bytes}",
@@ -459,36 +592,15 @@ def _init_ray_cluster(
 
         _worker_logger.info(f"Start Ray worker, command: {' '.join(ray_worker_node_cmd)}")
 
-        def setup_sigterm_on_parent_death():
-            """
-            Uses prctl to automatically send SIGTERM to the command process when its parent is
-            dead.
-
-            This handles the case when the parent is a PySpark worker process.
-            If a user cancels the PySpark job, the worker process gets killed, regardless of
-            PySpark daemon and worker reuse settings.
-            We use prctl to ensure the command process receives SIGTERM after spark job
-            cancellation.
-            The command process itself should handle SIGTERM properly.
-            This is a no-op on macOS because prctl is not supported.
-
-            Note:
-            When a pyspark job cancelled, the UDF python process are killed by signal "SIGKILL",
-            This case neither "atexit" nor signal handler can capture SIGKILL signal.
-            prctl is the only way to capture SIGKILL signal.
-            """
-            try:
-                import ctypes
-                import signal
-
-                libc = ctypes.CDLL("libc.so.6")
-                # Set the parent process death signal of the command process to SIGTERM.
-                libc.prctl(1, signal.SIGTERM)  # PR_SET_PDEATHSIG, see prctl.h
-            except OSError as e:
-                _worker_logger.warning(
-                    f"Setup libc.prctl PR_SET_PDEATHSIG failed, error {repr(e)}."
-                )
-
+        # `preexec_fn=setup_sigterm_on_parent_death` handles the case:
+        # If a user cancels the PySpark job, the worker process gets killed, regardless of
+        # PySpark daemon and worker reuse settings.
+        # We use prctl to ensure the command process receives SIGTERM after spark job
+        # cancellation.
+        # Note:
+        # When a pyspark job cancelled, the UDF python process are killed by signal "SIGKILL",
+        # This case neither "atexit" nor signal handler can capture SIGKILL signal.
+        # prctl is the only way to capture SIGKILL signal.
         exec_cmd(
             ray_worker_node_cmd,
             synchronous=True,
