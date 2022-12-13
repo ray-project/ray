6e0c3e24d8263681ccf330a3b3dbbcb330790731
Artur Niederfahrenhorst <artur@anyscale.com>
2022-12-13 15:48:31 +0100

    fix deprecation warning
    Signed-off-by: Artur Niederfahrenhorst <artur@anyscale.com>

--- rllib/algorithms/algorithm_config.py
+++ rllib/algorithms/algorithm_config.py
@@ -1345,6 +1345,7 @@ class AlgorithmConfig:
             self.model.update(model)
             if model.get("_use_default_native_models"):
                 deprecation_warning(
+                    old="AlgorithmConfig.training(_use_default_native_models=True)",
                     help="_use_default_native_models is not supported "
                     "anymore. To get rid of this error, set `experimental("
                     "_enable_rl_module_api` to True. Native models will "
