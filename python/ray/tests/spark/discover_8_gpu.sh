96e33e71b1b26093473abf7667e7e8a80e013748
Weichen Xu <weichen.xu@databricks.com>
2022-12-13 19:09:47 +0800

    update ci pipeline
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- python/ray/tests/spark/discover_8_gpu.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# This script is used in spark GPU cluster config for discovering available GPU.
-echo "{\"name\":\"gpu\",\"addresses\":[\"0\",\"1\",\"2\",\"3\",\"4\",\"5\",\"6\",\"7\"]}"
