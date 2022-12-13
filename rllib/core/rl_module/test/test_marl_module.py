d97afd937c23db664fd1969d97f30ee42520c96d
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2022-12-13 09:45:11 -0800

    created a new isolated unittest for marl_module
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

---
+++ rllib/core/rl_module/test/test_marl_module.py
@@ -0,0 +1,115 @@
+import unittest
+
+
+from ray.rllib.core.rl_module.marl_module import MultiAgentRLModule
+from ray.rllib.core.testing.torch.bc_module import DiscreteBCTorchModule
+from ray.rllib.env.multi_agent_env import make_multi_agent
+from ray.rllib.utils.test_utils import check
+
+DEFAULT_POLICY_ID = "default_policy"
+
+
+class TestMARLModule(unittest.TestCase):
+    def test_from_config(self):
+
+        env_class = make_multi_agent("CartPole-v0")
+        env = env_class({"num_agents": 2})
+        module1 = DiscreteBCTorchModule.from_env(env)
+        module2 = DiscreteBCTorchModule.from_env(env)
+
+        multi_agent_dict = {"module1": module1, "module2": module2}
+        marl_module = MultiAgentRLModule.from_config(multi_agent_dict)
+
+        self.assertEqual(set(marl_module.keys()), {"module1", "module2"})
+        self.assertIsInstance(marl_module["module1"], DiscreteBCTorchModule)
+        self.assertIsInstance(marl_module["module2"], DiscreteBCTorchModule)
+
+    def test_from_multi_agent_config(self):
+
+        env_class = make_multi_agent("CartPole-v0")
+        env = env_class({"num_agents": 2})
+        config1 = DiscreteBCTorchModule.from_env(env, return_config=True)
+        config2 = DiscreteBCTorchModule.from_env(env, return_config=True)
+
+        multi_agent_dict = {
+            "module1": (DiscreteBCTorchModule, config1),
+            "module2": (DiscreteBCTorchModule, config2),
+        }
+        marl_module = MultiAgentRLModule.from_multi_agent_config(multi_agent_dict)
+
+        self.assertEqual(set(marl_module.keys()), {"module1", "module2"})
+        self.assertIsInstance(marl_module["module1"], DiscreteBCTorchModule)
+        self.assertIsInstance(marl_module["module2"], DiscreteBCTorchModule)
+
+    def test_as_multi_agent(self):
+
+        env_class = make_multi_agent("CartPole-v0")
+        env = env_class({"num_agents": 2})
+
+        marl_module = DiscreteBCTorchModule.from_env(env).as_multi_agent()
+
+        self.assertNotIsInstance(marl_module, DiscreteBCTorchModule)
+        self.assertIsInstance(marl_module, MultiAgentRLModule)
+        self.assertEqual({DEFAULT_POLICY_ID}, set(marl_module.keys()))
+
+        # check as_multi_agent() for the second time
+        marl_module2 = marl_module.as_multi_agent()
+        self.assertEqual(id(marl_module), id(marl_module2))
+
+    def test_get_set_state(self):
+
+        env_class = make_multi_agent("CartPole-v0")
+        env = env_class({"num_agents": 2})
+
+        module = DiscreteBCTorchModule.from_env(env).as_multi_agent()
+
+        state = module.get_state()
+        self.assertIsInstance(state, dict)
+        self.assertEqual(set(state.keys()), set(module.keys()))
+        self.assertEqual(
+            set(state[DEFAULT_POLICY_ID].keys()),
+            set(module[DEFAULT_POLICY_ID].get_state().keys()),
+        )
+
+        module2 = DiscreteBCTorchModule.from_env(env).as_multi_agent()
+        state2 = module2.get_state()
+        check(state, state2, false=True)
+
+        module2.set_state(state)
+        state2_after = module2.get_state()
+        check(state, state2_after)
+
+    def test_add_remove_modules(self):
+        # TODO (Avnish): Modify this test to make sure that the distributed
+        # functionality won't break the add / remove.
+
+        env_class = make_multi_agent("CartPole-v0")
+        env = env_class({"num_agents": 2})
+        config = DiscreteBCTorchModule.from_env(env, return_config=True)
+        module = MultiAgentRLModule.from_multi_agent_config(
+            {DEFAULT_POLICY_ID: (DiscreteBCTorchModule, config)}
+        )
+
+        module.add_module("test", DiscreteBCTorchModule.from_env(env))
+        self.assertEqual(set(module.keys()), {DEFAULT_POLICY_ID, "test"})
+        module.remove_module("test")
+        self.assertEqual(set(module.keys()), {DEFAULT_POLICY_ID})
+
+        # test if add works with a conflicting name
+        self.assertRaises(
+            ValueError,
+            lambda: module.add_module(
+                DEFAULT_POLICY_ID, DiscreteBCTorchModule.from_env(env)
+            ),
+        )
+
+        module.add_module(
+            DEFAULT_POLICY_ID, DiscreteBCTorchModule.from_env(env), override=True
+        )
+
+
+if __name__ == "__main__":
+    import pytest
+    import sys
+
+    sys.exit(pytest.main(["-v", __file__]))
