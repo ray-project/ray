287c21fa84cc628ccf8d8c2d108d2679626ac487
Artur Niederfahrenhorst <artur@anyscale.com>
2022-12-13 10:57:11 +0100

    revert other test_sample_batch changes
    Signed-off-by: Artur Niederfahrenhorst <artur@anyscale.com>

--- rllib/policy/tests/test_sample_batch.py
+++ rllib/policy/tests/test_sample_batch.py
@@ -1,3 +1,4 @@
+import copy
 import functools
 import os
 import unittest
@@ -8,7 +9,7 @@ import tree
 
 import ray
 from ray.rllib.models.repeated_values import RepeatedValues
-from ray.rllib.policy.sample_batch import SampleBatch
+from ray.rllib.policy.sample_batch import SampleBatch, attempt_count_timesteps
 from ray.rllib.utils.compression import is_compressed
 from ray.rllib.utils.framework import try_import_torch
 from ray.rllib.utils.test_utils import check
@@ -358,7 +359,7 @@ class TestSampleBatch(unittest.TestCase):
         check(s["a"], [2, 3, 4, 3, 4, 5, 4, 5, 6, 5, 6, 7, 6, 7, 8], false=True)
 
     def test_to_device(self):
-        """Tests whether to_device works properly under different circumestances"""
+        """Tests whether to_device works properly under different circumstances"""
         torch, _ = try_import_torch()
 
         # sample batch includes
@@ -375,14 +376,15 @@ class TestSampleBatch(unittest.TestCase):
         s = SampleBatch(
             {
                 "a": np.array([1, 2]),
-                "b": {"c": (np.array([4, 5]), np.array([5, 6])), "e": [{"f": None}]},
+                "b": {"c": (np.array([4, 5]), np.array([5, 6]))},
                 "c": {"d": torch.Tensor([1, 2]), "g": (torch.Tensor([3, 4]), 1)},
                 "d": torch.Tensor([1.0, 2.0]).double(),
                 "e": torch.Tensor([1.0, 2.0]).double().to(cuda_if_possible),
                 "f": RepeatedValues(np.array([[1, 2, 0, 0]]), lengths=[2], max_len=4),
                 SampleBatch.SEQ_LENS: np.array([2, 3, 1]),
                 "state_in_0": np.array([1.0, 3.0, 4.0]),
-                SampleBatch.INFOS: np.array([{"a": 1}, {"b": 2}, {"c": 3}]),
+                # INFO can have arbitrary elements, others need to conform in size
+                SampleBatch.INFOS: np.array([{"a": 1}, {"b": [1, 2]}, {"c": None}]),
             }
         )
 
@@ -415,57 +417,111 @@ class TestSampleBatch(unittest.TestCase):
         check(s["f"].values, torch.from_numpy(np.asarray([[1, 2, 0, 0]])))
 
         # check infos
-        check(s[SampleBatch.INFOS], np.array([{"a": 1}, {"b": 2}, {"c": 3}]))
+        check(s[SampleBatch.INFOS], np.array([{"a": 1}, {"b": [1, 2]}, {"c": None}]))
 
         # check c/g/1
         self.assertEqual(s["c"]["g"][1], torch.from_numpy(np.asarray(1)))
-
-        # check b/e/0/f
-        self.assertEqual(s["b"]["e"][0]["f"], np.asarray(None))
 
         with self.assertRaises(NotImplementedError):
             # should raise an error if framework is not torch
             s.to_device(cuda_if_possible, framework="tf")
 
-    def test_interceptors(self):
-        # Tests whether interceptors work as intended
+    def test_count(self):
+        # Tests if counts are what we would expect from different batches
 
-        some_array = np.array([1, 2, 3])
-        batch = SampleBatch({SampleBatch.OBS: some_array})
+        input_dicts_and_lengths = [
+            (
+                {
+                    SampleBatch.OBS: {
+                        "a": np.array([[1], [2], [3]]),
+                        "b": np.array([[0], [0], [1]]),
+                        "c": np.array([[4], [5], [6]]),
+                    }
+                },
+                3,
+            ),
+            (
+                {
+                    SampleBatch.OBS: {
+                        "a": np.array([[1, 2, 3]]),
+                        "b": np.array([[0, 0, 1]]),
+                        "c": np.array([[4, 5, 6]]),
+                    }
+                },
+                1,
+            ),
+            (
+                {
+                    SampleBatch.INFOS: {
+                        "a": np.array([[1], [2], [3]]),
+                        "b": np.array([[0], [0], [1]]),
+                        "c": np.array([[4], [5], [6]]),
+                    }
+                },
+                0,  # This should have a length of zero, since we can ignore INFO
+            ),
+            (
+                {
+                    "state_in_0": {
+                        "a": [[[1], [2], [3]], [[1], [2], [3]], [[1], [2], [3]]],
+                        "b": [[[1], [2], [3]], [[1], [2], [3]], [[1], [2], [3]]],
+                        "c": [[[1], [2], [3]], [[1], [2], [3]], [[1], [2], [3]]],
+                    },
+                    "state_out_0": {
+                        "a": [[[1], [2], [3]], [[1], [2], [3]], [[1], [2], [3]]],
+                        "b": [[[1], [2], [3]], [[1], [2], [3]], [[1], [2], [3]]],
+                        "c": [[[1], [2], [3]], [[1], [2], [3]], [[1], [2], [3]]],
+                    },
+                    SampleBatch.OBS: {
+                        "a": np.array([1, 2, 3]),
+                        "b": np.array([0, 0, 1]),
+                        "c": np.array([4, 5, 6]),
+                    },
+                },
+                3,  # This should have a length of three - we count from OBS
+            ),
+            (
+                {
+                    "state_in_0": {
+                        "a": [[[1], [2], [3]], [[1], [2], [3]], [[1], [2], [3]]],
+                        "b": [[[1], [2], [3]], [[1], [2], [3]], [[1], [2], [3]]],
+                        "c": [[[1], [2], [3]], [[1], [2], [3]], [[1], [2], [3]]],
+                    },
+                    "state_out_0": {
+                        "a": [[[1], [2], [3]], [[1], [2], [3]], [[1], [2], [3]]],
+                        "b": [[[1], [2], [3]], [[1], [2], [3]], [[1], [2], [3]]],
+                        "c": [[[1], [2], [3]], [[1], [2], [3]], [[1], [2], [3]]],
+                    },
+                },
+                0,  # This should have a length of zero, we don't attempt to count
+            ),
+            (
+                {
+                    SampleBatch.OBS: {
+                        "a": np.array([[1], [2], [3]]),
+                        "b": np.array([[0], [0], [1]]),
+                        "c": np.array([[4], [5], [6]]),
+                    },
+                    SampleBatch.SEQ_LENS: np.array([[1], [2], [3]]),
+                },
+                6,  # This should have a length of six, since we don't try to infer
+                # from inputs but count by sequence lengths
+            ),
+            (
+                {
+                    SampleBatch.NEXT_OBS: {
+                        "a": {"b": np.array([[1], [2], [3]])},
+                        "c": np.array([[4], [5], [6]]),
+                    },
+                },
+                3,  # Test if we properly support nesting
+            ),
+        ]
 
-        device = torch.device("cpu")
-
-        batch.set_get_interceptor(
-            functools.partial(convert_to_torch_tensor, device=device)
-        )
-
-        self.assertTrue(
-            all(
-                convert_to_torch_tensor(some_array, device=device)
-                == batch[SampleBatch.OBS]
-            )
-        )
-
-        # This test requires a GPU, otherwise we can't test whether we are
-        # moving between devices
-        if not torch.cuda.is_available():
-            raise ValueError("This test can only fail if cuda is available.")
-
-        another_array = np.array([4, 5, 6])
-        another_batch = SampleBatch({SampleBatch.OBS: another_array})
-
-        another_device = torch.device("cuda")
-
-        another_batch.set_get_interceptor(
-            functools.partial(convert_to_torch_tensor, device=another_device)
-        )
-
-        self.assertTrue(
-            all(
-                convert_to_torch_tensor(another_array, device=another_device)
-                == another_batch[SampleBatch.OBS]
-            )
-        )
+        for input_dict, length in input_dicts_and_lengths:
+            self.assertEqual(attempt_count_timesteps(copy.deepcopy(input_dict)), length)
+            s = SampleBatch(input_dict)
+            self.assertEqual(s.count, length)
 
     def test_interceptors(self):
         # Tests whether interceptors work as intended
