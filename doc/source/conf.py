16176d790539b0257218d3b7870d44d578df6f34
Balaji Veeramani <balaji@anyscale.com>
2022-12-15 11:56:36 -0800

    [Docs] Prevent ">>>" from getting copied (#31111)
    This PR updates the `sphinx_copybutton` config so that interactive prompts like ">>>" aren't copied. 
    
    If you click the copy button, interactive prompts are copied. This makes the copy button hard to use, because the interactive prompts aren't valid Python, and your code won't run.
    
    Signed-off-by: tmynn <hovhannes.tamoyan@gmail.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -81,8 +81,12 @@ external_toc_path = "_toc.yml"
 
 html_extra_path = ["robots.txt"]
 
-# Omit prompt when using copy button
-copybutton_prompt_text = r"\$ "
+# This pattern matches:
+# - Python Repl prompts (">>> ") and it's continuation ("... ")
+# - Bash prompts ("$ ")
+# - IPython prompts ("In []: ", "In [999]: ") and it's continuations
+#   ("  ...: ", "     : ")
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
 copybutton_prompt_is_regexp = True
 
 
