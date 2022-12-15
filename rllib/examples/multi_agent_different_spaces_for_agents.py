d90d2d95e9c388cf68c58c7008f437879b8c68ef
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2022-12-15 13:15:11 -0800

    Updates to examples/multi_agent_different_spaces_for_agents.py:
    1. The test now runs on PPO by default instead of APPO
    2. Tests pass when enable_rl_module_api
    
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- rllib/examples/multi_agent_different_spaces_for_agents.py
+++ rllib/examples/multi_agent_different_spaces_for_agents.py
@@ -73,7 +73,7 @@ def get_cli_args():
 
     # general args
     parser.add_argument(
-        "--run", type=str, default="APPO", help="The RLlib-registered algorithm to use."
+        "--run", type=str, default="PPO", help="The RLlib-registered algorithm to use."
     )
     parser.add_argument("--num-cpus", type=int, default=0)
     parser.add_argument(
@@ -103,6 +103,9 @@ def get_cli_args():
         action="store_true",
         help="Init Ray in local mode for easier debugging.",
     )
+    parser.add_argument(
+        "--enable-rl-module-api", action="store_true", help="Use RLModule API."
+    )
 
     args = parser.parse_args()
     print(f"Running with following CLI args: {args}")
@@ -120,31 +123,34 @@ if __name__ == "__main__":
         "episode_reward_mean": args.stop_reward,
     }
 
+    config = {
+        "env": BasicMultiAgentMultiSpaces,
+        # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+        "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
+        "num_workers": 1,
+        "multiagent": {
+            # Use a simple set of policy IDs. Spaces for the individual policies
+            # will be inferred automatically using reverse lookup via the
+            # `policy_mapping_fn` and the env provided spaces for the different
+            # agents. Alternatively, you could use:
+            # policies: {main0: PolicySpec(...), main1: PolicySpec}
+            "policies": {"main0", "main1"},
+            # Simple mapping fn, mapping agent0 to main0 and agent1 to main1.
+            "policy_mapping_fn": (lambda aid, episode, worker, **kw: f"main{aid[-1]}"),
+            # Only train main0.
+            "policies_to_train": ["main0"],
+        },
+        "framework": args.framework,
+        "eager_tracing": args.eager_tracing,
+    }
+
+    if args.enable_rl_module_api:
+        config.update(_enable_rl_module_api=True, enable_connectors=True)
+
     tune.Tuner(
         args.run,
         run_config=air.RunConfig(
             stop=stop,
         ),
-        param_space={
-            "env": BasicMultiAgentMultiSpaces,
-            # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
-            "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
-            "num_workers": 1,
-            "multiagent": {
-                # Use a simple set of policy IDs. Spaces for the individual policies
-                # will be inferred automatically using reverse lookup via the
-                # `policy_mapping_fn` and the env provided spaces for the different
-                # agents. Alternatively, you could use:
-                # policies: {main0: PolicySpec(...), main1: PolicySpec}
-                "policies": {"main0", "main1"},
-                # Simple mapping fn, mapping agent0 to main0 and agent1 to main1.
-                "policy_mapping_fn": (
-                    lambda aid, episode, worker, **kw: f"main{aid[-1]}"
-                ),
-                # Only train main0.
-                "policies_to_train": ["main0"],
-            },
-            "framework": args.framework,
-            "eager_tracing": args.eager_tracing,
-        },
+        param_space=config,
     ).fit()
