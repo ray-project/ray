77d746132a65a001bdbb45e68cfe603bf1e45b94
Jeroen Bédorf <jeroen@bedorf.net>
2022-12-16 01:44:28 +0100

    [Core/Python] Turn hardcoded forward slashes into os.path.join operations (#31093)
    The hardcoded usage of the forward slashes is problematic when Ray is run from special Windows folder paths. Specifically if Ray is run from a path that is preceded with `\\?\` . This type of prefix ([Win32 File Namespace](https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file#win32-file-namespaces)) tells Windows to not parse the rest of the path name, so currently you end up with something like this:  `\\?\C:\python-location\ray\core/src/ray/raylet/raylet.exe`
    This path does not get properly parsed and this results in Ray not being able to launch. 
    
    This typically happens when Ray is used as a dependency in a Bazel build/test project. 
    
    The changes made here ensure that the paths get properly set and can be used, e.g. you end up with the proper path, like this:
    `\\\\?\\C:\\python-location\\ray\\core\\src\\ray\\raylet\\raylet.exe`
    
    Signed-off-by: tmynn <hovhannes.tamoyan@gmail.com>

--- python/ray/_private/node.py
+++ python/ray/_private/node.py
@@ -137,7 +137,9 @@ class Node:
             resources={},
             temp_dir=ray._private.utils.get_ray_temp_dir(),
             worker_path=os.path.join(
-                os.path.dirname(os.path.abspath(__file__)), "workers/default_worker.py"
+                os.path.dirname(os.path.abspath(__file__)),
+                "workers",
+                "default_worker.py",
             ),
             setup_worker_path=os.path.join(
                 os.path.dirname(os.path.abspath(__file__)),
