58db43405103961d7a219ee948dd2e0539d6ed18
scv119 <scv119@gmail.com>
2022-12-16 14:43:46 -0800

    ad

--- src/ray/raylet/scheduling/cluster_task_manager_test.cc
+++ src/ray/raylet/scheduling/cluster_task_manager_test.cc
@@ -289,7 +289,9 @@ class ClusterTaskManagerTest : public ::testing::Test {
             /* announce_infeasible_task= */
             [this](const RayTask &task) { announce_infeasible_task_calls_++; },
             local_task_manager_,
-            /*get_time=*/[this]() { return current_time_ms_; }) {}
+            /*get_time=*/[this]() { return current_time_ms_; }) {
+    RayConfig::instance().initialize("({\"schedule_top_k_absolute\": 1,})");
+  }
 
   void SetUp() {
     static rpc::GcsNodeInfo node_info;
