a2dc97d7f8f08cfc7e1058831a7949a19b5af533
Artur Niederfahrenhorst <artur@anyscale.com>
2022-12-16 13:06:27 +0100

    [RLlib] Remove native Keras Models. (#30986)
    Signed-off-by: tmynn <hovhannes.tamoyan@gmail.com>

--- rllib/algorithms/algorithm_config.py
+++ rllib/algorithms/algorithm_config.py
@@ -1423,6 +1423,20 @@ class AlgorithmConfig:
                     error=True,
                 )
             self.model.update(model)
+            if (
+                model.get("_use_default_native_models", DEPRECATED_VALUE)
+                != DEPRECATED_VALUE
+            ):
+                deprecation_warning(
+                    old="AlgorithmConfig.training(_use_default_native_models=True)",
+                    help="_use_default_native_models is not supported "
+                    "anymore. To get rid of this error, set `experimental("
+                    "_enable_rl_module_api` to True. Native models will "
+                    "be better supported by the upcoming RLModule API.",
+                    # Error out if user tries to enable this
+                    error=model["_use_default_native_models"],
+                )
+
         if optimizer is not NotProvided:
             self.optimizer = merge_dicts(self.optimizer, optimizer)
         if max_requests_in_flight_per_sampler_worker is not NotProvided:
