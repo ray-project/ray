ad76a6dac2a27c0217d53d892d4779cc3afa0be3
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2022-12-16 18:45:35 -0800

    reverting debugging changes
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- rllib/examples/self_play_with_open_spiel.py
+++ rllib/examples/self_play_with_open_spiel.py
@@ -138,46 +138,44 @@ class SelfPlayCallback(DefaultCallbacks):
         # If win rate is good -> Snapshot current policy and play against
         # it next, keeping the snapshot fixed and only improving the "main"
         # policy.
-        # if win_rate > args.win_rate_threshold:
-        #     print("="*80)
-        #     print("win-rate threshold reached -> snapshotting current policy.")
-        #     self.current_opponent += 1
-        #     new_pol_id = f"main_v{self.current_opponent}"
-        #     print(f"adding new opponent to the mix ({new_pol_id}).")
+        if win_rate > args.win_rate_threshold:
+            print("=" * 80)
+            print("win-rate threshold reached -> snapshotting current policy.")
+            self.current_opponent += 1
+            new_pol_id = f"main_v{self.current_opponent}"
+            print(f"adding new opponent to the mix ({new_pol_id}).")
 
-        #     # Re-define the mapping function, such that "main" is forced
-        #     # to play against any of the previously played policies
-        #     # (excluding "random").
-        #     def policy_mapping_fn(agent_id, episode, worker, **kwargs):
-        #         # agent_id = [0|1] -> policy depends on episode ID
-        #         # This way, we make sure that both policies sometimes play
-        #         # (start player) and sometimes agent1 (player to move 2nd).
-        #         pid = (
-        #             "main"
-        #             if episode.episode_id % 2 == agent_id
-        #             else "main_v{}".format(
-        #                 np.random.choice(list(range(1, self.current_opponent + 1)))
-        #             )
-        #         )
-        #         print(f"agent_id = {agent_id} -> pid = {pid}")
-        #         return pid
+            # Re-define the mapping function, such that "main" is forced
+            # to play against any of the previously played policies
+            # (excluding "random").
+            def policy_mapping_fn(agent_id, episode, worker, **kwargs):
+                # agent_id = [0|1] -> policy depends on episode ID
+                # This way, we make sure that both policies sometimes play
+                # (start player) and sometimes agent1 (player to move 2nd).
+                return (
+                    "main"
+                    if episode.episode_id % 2 == agent_id
+                    else "main_v{}".format(
+                        np.random.choice(list(range(1, self.current_opponent + 1)))
+                    )
+                )
 
-        #     new_policy = algorithm.add_policy(
-        #         policy_id=new_pol_id,
-        #         policy_cls=type(algorithm.get_policy("main")),
-        #         policy_mapping_fn=policy_mapping_fn,
-        #     )
+            new_policy = algorithm.add_policy(
+                policy_id=new_pol_id,
+                policy_cls=type(algorithm.get_policy("main")),
+                policy_mapping_fn=policy_mapping_fn,
+            )
 
-        #     # Set the weights of the new policy to the main policy.
-        #     # We'll keep training the main policy, whereas `new_pol_id` will
-        #     # remain fixed.
-        #     main_state = algorithm.get_policy("main").get_state()
-        #     new_policy.set_state(main_state)
-        #     # We need to sync the just copied local weights (from main policy)
-        #     # to all the remote workers as well.
-        #     algorithm.workers.sync_weights()
-        # else:
-        #     print("not good enough; will keep learning ...")
+            # Set the weights of the new policy to the main policy.
+            # We'll keep training the main policy, whereas `new_pol_id` will
+            # remain fixed.
+            main_state = algorithm.get_policy("main").get_state()
+            new_policy.set_state(main_state)
+            # We need to sync the just copied local weights (from main policy)
+            # to all the remote workers as well.
+            algorithm.workers.sync_weights()
+        else:
+            print("not good enough; will keep learning ...")
 
         # +2 = main + random
         result["league_size"] = self.current_opponent + 2
@@ -192,16 +190,12 @@ if __name__ == "__main__":
         return OpenSpielEnv(pyspiel.load_game(args.env))
 
     register_env("open_spiel_env", lambda _: OpenSpielEnv(pyspiel.load_game(args.env)))
-    breakpoint()
 
     def policy_mapping_fn(agent_id, episode, worker, **kwargs):
         # agent_id = [0|1] -> policy depends on episode ID
         # This way, we make sure that both policies sometimes play agent0
         # (start player) and sometimes agent1 (player to move 2nd).
-        # pid = "main" if episode.episode_id % 2 == agent_id else "random"
-        pid = "main" if agent_id % 2 == 0 else "random"
-        print(f"agent_id = {agent_id} -> pid = {pid}")
-        return pid
+        return "main" if episode.episode_id % 2 == agent_id else "random"
 
     config = (
         PPOConfig()
