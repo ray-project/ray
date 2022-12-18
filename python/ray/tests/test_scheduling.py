ba9b070a9d4256ee4d362b5d075910e46f9f542d
scv119 <scv119@gmail.com>
2022-12-18 10:13:57 -0800

    add

--- python/ray/tests/test_scheduling.py
+++ python/ray/tests/test_scheduling.py
@@ -64,10 +64,14 @@ def test_load_balancing(ray_start_cluster):
 def test_hybrid_policy(ray_start_cluster):
 
     cluster = ray_start_cluster
+
     num_nodes = 2
     num_cpus = 10
     for _ in range(num_nodes):
-        cluster.add_node(num_cpus=num_cpus, memory=num_cpus)
+        cluster.add_node(num_cpus=num_cpus, memory=num_cpus,
+         _system_config={
+            "scheduler_top_k_absolute": 1,
+        },)
     cluster.wait_for_nodes()
     ray.init(address=cluster.address)
 
