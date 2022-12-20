d0285f5f05130df05bb0cc917d0b946b46b317c5
Alex <alex@anyscale.com>
2022-12-20 15:49:46 -0800

    .

--- python/ray/tests/test_autoscaler_e2e.py
+++ python/ray/tests/test_autoscaler_e2e.py
@@ -2,11 +2,13 @@ import subprocess
 from ray.autoscaler._private.constants import AUTOSCALER_METRIC_PORT
 
 import ray
+import sys
 from ray._private.test_utils import (
     wait_for_condition,
     get_metric_check_condition,
 )
 from ray.cluster_utils import AutoscalingCluster
+from ray.autoscaler.node_launch_exception import NodeLaunchException
 
 
 def test_ray_status_e2e(shutdown_only):
@@ -109,6 +111,25 @@ def test_metrics(shutdown_only):
 
     finally:
         cluster.shutdown()
+
+
+def test_node_launch_exception_serialization(shutdown_only):
+    ray.init(num_cpus=1)
+
+    exc_info = None
+    try:
+        raise Exception("Test exception.")
+    except Exception:
+        exc_info = sys.exc_info()
+    assert exc_info is not None
+
+    exc = NodeLaunchException("cat", "desc", exc_info)
+
+    after_serialization = ray.get(ray.put(exc))
+
+    assert after_serialization.category == exc.category
+    assert after_serialization.description == exc.description
+    assert after_serialization.src_exc_info is None
 
 
 if __name__ == "__main__":
