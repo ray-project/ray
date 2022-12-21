01d4b2cf89d7591c85d6df2ac995aa3bede98544
jianoaix <iamjianxiao@gmail.com>
2022-12-21 23:31:25 +0000

    Block bundling: add more tests
    Signed-off-by: jianoaix <iamjianxiao@gmail.com>

--- python/ray/data/tests/test_execution.py
+++ python/ray/data/tests/test_execution.py
@@ -49,22 +49,50 @@ def test_basic_bulk():
 
 
 def test_block_bundling():
-    executor = BulkExecutor(ExecutionOptions())
-    inputs = _make_ref_bundles([[x] for x in range(20)])
-    o1 = InputDataBuffer(inputs)
-    o2 = MapOperator(
-        make_transform(lambda block: [b * -1 for b in block]), o1, target_block_size=3
+    def check_execution(input_list, output_list, target_block_size, num_tasks):
+        executor = BulkExecutor(ExecutionOptions())
+        inputs = _make_ref_bundles(input_list)
+        o1 = InputDataBuffer(inputs)
+        o2 = MapOperator(
+            make_transform(lambda block: [b * -1 for b in block]),
+            o1,
+            target_block_size=target_block_size,
+        )
+        o3 = MapOperator(
+            make_transform(lambda block: [b * 2 for b in block]),
+            o2,
+            target_block_size=target_block_size,
+        )
+        it = executor.execute(o3)
+        assert o3._execution_state._next_task_index == num_tasks
+        output = ref_bundles_to_list(it)
+        assert output == output_list, (output, output_list)
+
+    import math
+
+    # 1 row per block, with target block size from 1 to 21.
+    for target in range(1, 22):
+        check_execution(
+            [[x] for x in range(20)],
+            [[x * -2] for x in range(20)],
+            target,
+            math.ceil(20 / target),
+        )
+
+    # 2 rows per block, with target block size from 1 to 21.
+    check_execution(
+        [[x, x + 1] for x in range(20)],
+        [[x * -2, x * -2 - 2] for x in range(20)],
+        1,
+        20,
     )
-    o3 = MapOperator(
-        make_transform(lambda block: [b * 2 for b in block]), o2, target_block_size=3
-    )
-    it = executor.execute(o3)
-    # For 20 blocks, 1 row per block and target_block_size=3, there will be 7 tasks
-    # launched.
-    assert o3._execution_state._next_task_index == 7
-    output = ref_bundles_to_list(it)
-    expected = [[x * -2] for x in range(20)]
-    assert output == expected, (output, expected)
+    for target in range(2, 22):
+        check_execution(
+            [[x, x + 1] for x in range(20)],
+            [[x * -2, x * -2 - 2] for x in range(20)],
+            target,
+            math.ceil(20 / math.floor(target / 2)),
+        )
 
 
 def test_actor_strategy():
