1f18c261e7b144f51bb14b0b88de1c8f24228b38
SangBin Cho <rkooo567@gmail.com>
2022-12-21 20:36:22 -0800

    fix
    Signed-off-by: SangBin Cho <rkooo567@gmail.com>

--- python/setup.py
+++ python/setup.py
@@ -237,7 +237,8 @@ if setup_spec.type == SetupType.RAY:
             "aiohttp_cors",
             "colorful",
             "py-spy >= 0.2.0",
-            "memray >= 1.5.0" "requests",
+            "memray >= 1.5.0",
+            "requests",
             "gpustat >= 1.0.0",  # for windows
             "opencensus",
             "pydantic",
