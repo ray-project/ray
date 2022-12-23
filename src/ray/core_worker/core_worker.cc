8ea297894c7dbf3e796d8b750e83599e9b49fa65
Jialing He <burglarralgrub@gmail.com>
2022-12-23 14:06:54 +0800

    [core][Object Store][Bug fix] Fix: ray.put with argument _owner will lost the reference of inner objects (#30415)
    In function [HandleAssignObjectOwner](https://github.com/ray-project/ray/blob/master/src/ray/core_worker/core_worker.cc#L3355), inner objects are not borrowed by the outer object's owner, will cause fewer references to inner objects than expected.
    
    we need to add nested objects after assigning the owner.
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -1104,6 +1104,11 @@ Status CoreWorker::CreateOwnedAndIncrementLocalRef(
                             });
     // Block until the remote call `AssignObjectOwner` returns.
     status = status_promise.get_future().get();
+    // Must call `AddNestedObjectIds` after finished assign owner.
+    // Otherwise, it will cause the reference count of those contained objects
+    // to be less than expected. Details: https://github.com/ray-project/ray/issues/30341
+    reference_counter_->AddNestedObjectIds(
+        *object_id, contained_object_ids, real_owner_address);
   }
 
   if (options_.is_local_mode && owned_by_us && inline_small_object) {
