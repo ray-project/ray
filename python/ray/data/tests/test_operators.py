d810c611da7bdf2796f38e0611782c7d96308440
Eric Liang <ekhliang@gmail.com>
2022-12-22 16:20:28 -0800

    add test todos
    Signed-off-by: Eric Liang <ekhliang@gmail.com>

--- python/ray/data/tests/test_operators.py
+++ python/ray/data/tests/test_operators.py
@@ -8,14 +8,36 @@ from ray.data._internal.execution.operators.input_data_buffer import InputDataBu
 
 
 def test_input_data_buffer():
+    # Create with bundles.
+    # Check we return all bundles in order.
     assert False
 
 
 def test_map_operator():
+    # Create with inputs.
+    # Check we return transformed bundles in order.
+    # Check memory stats and ownership flag.
+    assert False
+
+
+def test_map_operator_ray_args():
+    # Check it uses ray remote args.
+    assert False
+
+
+def test_map_operator_shutdown():
+    # Check shutdown cancels tasks.
+    assert False
+
+
+def test_map_operator_min_rows_per_batch():
+    # Test batching behavior.
     assert False
 
 
 def test_all_to_all_operator():
+    # Create with bundles.
+    # Check we return transformed bundles.
     assert False
 
 
