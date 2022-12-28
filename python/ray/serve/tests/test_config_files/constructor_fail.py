8710c2506584665fc7ff0e7eb1a3224d535acb3d
Cindy Zhang <cindyzyx9@gmail.com>
2022-12-28 10:46:17 -0800

    remove unused test file
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- python/ray/serve/tests/test_config_files/constructor_fail.py
+++
@@ -1,8 +0,0 @@
-from ray import serve
-
-@serve.deployment
-class ConstructorFailure:
-    def __init__(self):
-        raise RuntimeError("Intentionally failing.")
-
-app = ConstructorFailure.bind()
