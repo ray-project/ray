c4b8550d0578a3a58c0bb1332d6776719c18c3c2
Simon Zehnder <simon.zehnder@gmail.com>
2023-01-02 14:09:13 +0100

    Ran the linter.
    Signed-off-by: Simon Zehnder <simon.zehnder@gmail.com>

--- rllib/offline/dataset_writer.py
+++ rllib/offline/dataset_writer.py
@@ -16,6 +16,7 @@ logger = logging.getLogger(__name__)
 
 pa = try_import_arrow()
 
+
 @PublicAPI
 class DatasetWriter(OutputWriter):
     """Writer object that saves experiences using Datasets."""
--- rllib/utils/offline.py
+++ rllib/utils/offline.py
@@ -5,19 +5,20 @@ from ray.rllib.utils.annotations import PublicAPI
 
 logger = logging.getLogger(__name__)
 
+
 @PublicAPI
 def try_import_arrow(error: bool = False):
     """Tries importing Arrow and returns the module.
-    
-    Args: 
+
+    Args:
         error: Whether to raise an error if pyarrow cannot be
                imported.
 
     Returns:
         The pyarrow module
-          
+
     Raises:
-        ImportError: If error=True and pyarrow is not insntalled.        
+        ImportError: If error=True and pyarrow is not insntalled.
     """
     if "RLLIB_TEST_NO_ARROW_IMPORT" in os.environ:
         logger.warning("Not importing Arrow for test purposes.")
