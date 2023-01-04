8fa29b4703ab807e5c72efed9631186cf7d31212
amogkam <amogkamsetty@yahoo.com>
2023-01-03 18:09:51 -0800

    formatting
    Signed-off-by: amogkam <amogkamsetty@yahoo.com>

--- python/ray/data/_internal/block_batching.py
+++ python/ray/data/_internal/block_batching.py
@@ -1,8 +1,6 @@
-import collections
 from contextlib import nullcontext
-import itertools
 import queue
-from typing import TYPE_CHECKING, Iterable, Iterator, Optional, Union, Dict
+from typing import TYPE_CHECKING, Iterator, Optional, Union, Dict
 
 import numpy as np
 
@@ -31,22 +29,98 @@ BatchType = Union[
 PREFETCHER_ACTOR_NAMESPACE = "ray.dataset"
 
 
-def _resolve_blocks(block_ref_iter: Iterator[ObjectRef[Block]], stats: Union[DatasetStats, DatasetPipelineStats]=None) -> Iterator[Block]:
+def batch_blocks(
+    blocks: Iterator[ObjectRef[Block]],
+    stats: Optional[Union[DatasetStats, DatasetPipelineStats]] = None,
+    *,
+    prefetch_blocks: int = 0,
+    clear_block_after_read: bool = False,
+    batch_size: Optional[int] = None,
+    batch_format: str = "default",
+    drop_last: bool = False,
+    shuffle_buffer_min_size: Optional[int] = None,
+    shuffle_seed: Optional[int] = None,
+) -> Iterator[BatchType]:
+    """Create formatted batches of data from 1 or more blocks.
+
+    This takes a block iterator and creates batch_size batches, slicing,
+    unioning, shuffling, prefetching, and formatting blocks as needed.
+
+    This is used by both Dataset.iter_batches()/DatasetPipeline.iter_batches()
+    and Dataset.map_batches()/DatasetPipeline.map_batches().
+
+    Args:
+        blocks: An iterator over block object references.
+        prefetch_blocks: The number of blocks to prefetch ahead of the
+            current block during the scan.
+        clear_block_after_read: Whether to clear the block from object store
+            manually (i.e. without waiting for Python's automatic GC) after it
+            is read. Doing so will reclaim memory faster and hence reduce the
+            memory footprint. However, the caller has to ensure the safety, i.e.
+            the block will never be accessed again.
+        batch_size: Record batch size, or None to let the system pick.
+        batch_format: The format in which to return each batch.
+            Specify "default" to use the current block format (promoting
+            Arrow to pandas automatically), "pandas" to
+            select ``pandas.DataFrame`` or "pyarrow" to select
+            ``pyarrow.Table``. Default is "default".
+        drop_last: Whether to drop the last batch if it's incomplete.
+        shuffle_buffer_min_size: If non-None, the data will be randomly shuffled using a
+            local in-memory shuffle buffer, and this value will serve as the minimum
+            number of rows that must be in the local in-memory shuffle buffer in order
+            to yield a batch.
+        shuffle_seed: The seed to use for the local random shuffle.
+
+    Returns:
+        An iterator over record batches.
+    """
+
+    block_iter = _resolve_blocks(
+        _prefetch_blocks(
+            block_ref_iter=blocks,
+            stats=stats,
+            num_blocks_to_prefetch=prefetch_blocks,
+            clear_block_after_read=clear_block_after_read,
+        ),
+        stats=stats,
+    )
+
+    batch_iter = _format_batches(
+        _blocks_to_batches(
+            block_iter=block_iter,
+            stats=stats,
+            batch_size=batch_size,
+            drop_last=drop_last,
+            shuffle_buffer_min_size=shuffle_buffer_min_size,
+            shuffle_seed=shuffle_seed,
+        ),
+        batch_format=batch_format,
+    )
+
+    for formatted_batch in batch_iter:
+        user_timer = stats.iter_user_s.timer() if stats else nullcontext()
+        with user_timer:
+            yield formatted_batch
+
+
+def _resolve_blocks(
+    block_ref_iter: Iterator[ObjectRef[Block]],
+    stats: Optional[Union[DatasetStats, DatasetPipelineStats]] = None,
+) -> Iterator[Block]:
     """Given an iterator of unresolved blocks (as Ray object references), returns an
-    iterator of resolved blocks. 
-        
+    iterator of resolved blocks.
+
     The length of the returned iterator may be less than the length of the original
     if any of the references in the original iterator are None.
 
     Args:
         block_ref_iter: An iterator over block object references.
         stats: Dataset stats object used to store block fetching time.
-    
+
     Returns:
         An iterator over resolved blocks.
     """
 
-    
     for block_ref in block_ref_iter:
         if block_ref is not None:
             stats_timer = stats.iter_get_s.timer() if stats else nullcontext()
@@ -54,9 +128,16 @@ def _resolve_blocks(block_ref_iter: Iterator[ObjectRef[Block]], stats: Union[Dat
                 block = ray.get(block_ref)
             yield block
 
-def _prefetch_blocks(block_ref_iter: Iterator[ObjectRef[Block]], num_blocks_to_prefetch: int, clear_block_after_read: bool = False) -> Iterator[ObjectRef[Block]]:
-    """Given an iterable of Block Object References, returns an iterator 
-    over these object reference while prefetching `num_block_to_prefetch` blocks in advance.
+
+def _prefetch_blocks(
+    block_ref_iter: Iterator[ObjectRef[Block]],
+    num_blocks_to_prefetch: int,
+    clear_block_after_read: bool = False,
+    stats: Optional[Union[DatasetStats, DatasetPipelineStats]] = None,
+) -> Iterator[ObjectRef[Block]]:
+    """Given an iterable of Block Object References, returns an iterator
+    over these object reference while prefetching `num_block_to_prefetch`
+    blocks in advance.
 
     Args:
         block_ref_iter: An iterator over block object references.
@@ -67,11 +148,12 @@ def _prefetch_blocks(block_ref_iter: Iterator[ObjectRef[Block]], num_blocks_to_p
             is read. Doing so will reclaim memory faster and hence reduce the
             memory footprint. However, the caller has to ensure the safety, i.e.
             the block will never be accessed again.
+        stats: Dataset stats object used to store block wait time.
     """
 
     if num_blocks_to_prefetch == 0:
         yield from block_ref_iter
-    
+
     context = DatasetContext.get_current()
 
     if (
@@ -105,19 +187,29 @@ def _prefetch_blocks(block_ref_iter: Iterator[ObjectRef[Block]], num_blocks_to_p
         if clear_block_after_read:
             ray._private.internal_api.free(block_ref, local_only=False)
 
-def _batch_blocks(block_iter: Iterator[Block], stats: Union[DatasetStats, DatasetPipelineStats]=None, batch_size: Optional[int] = None, drop_last: bool = False, shuffle_buffer_min_size: Optional[int]=None, shuffle_seed: Optional[int] = None) -> Iterator[Block]:
-    """Given an iterator over blocks, returns an iterator over blocks with each blockas the with the given batch size. 
-    
-    If the shuffling configurations are specified, then the output batches are shuffled.
+
+def _blocks_to_batches(
+    block_iter: Iterator[Block],
+    stats: Optional[Union[DatasetStats, DatasetPipelineStats]] = None,
+    batch_size: Optional[int] = None,
+    drop_last: bool = False,
+    shuffle_buffer_min_size: Optional[int] = None,
+    shuffle_seed: Optional[int] = None,
+) -> Iterator[Block]:
+    """Given an iterator over blocks, returns an iterator over blocks
+    of the appropriate bacth size.
+
+    If the shuffling configurations are specified, then the
+    output blocks contain shuffled data.
 
     Args:
         block_iter: An iterator over blocks.
         stats: Dataset stats object used to store block batching time.
         batch_size: Record batch size, or None to let the system pick.
         drop_last: Whether to drop the last batch if it's incomplete.
-    
+
     Returns:
-        An iterator over data batches.
+        An iterator over blocks of the given size that are potentially shuffled.
     """
     if shuffle_buffer_min_size is not None:
         batcher = ShufflingBatcher(
@@ -128,15 +220,14 @@ def _batch_blocks(block_iter: Iterator[Block], stats: Union[DatasetStats, Datase
     else:
         batcher = Batcher(batch_size=batch_size)
 
-   
     for block in block_iter:
-         batcher.add(block)
-         while batcher.has_batch():
+        batcher.add(block)
+        while batcher.has_batch():
             timer = stats.iter_next_batch_s.timer() if stats else nullcontext()
             with timer:
                 batch = batcher.next_batch()
             yield batch
-    
+
     # Signal to the batcher that there are no more blocks to add.
     batcher.done_adding()
 
@@ -148,173 +239,21 @@ def _batch_blocks(block_iter: Iterator[Block], stats: Union[DatasetStats, Datase
         yield batch
 
 
-def _format_batches(batch_iter: Iterator[BatchType]) -> Iterator[]
-
-
-
-def _format_batches():
-    pass
-
-
-
-def batch_blocks(
-    blocks: Iterator[ObjectRef[Block]],
-    stats: Union[DatasetStats, DatasetPipelineStats],
-    *,
-    prefetch_blocks: int = 0,
-    clear_block_after_read: bool = False,
-    batch_size: Optional[int] = None,
-    batch_format: str = "default",
-    drop_last: bool = False,
-    shuffle_buffer_min_size: Optional[int] = None,
-    shuffle_seed: Optional[int] = None,
+def _format_batches(
+    block_iter: Iterator[Block], batch_format: str
 ) -> Iterator[BatchType]:
-    """Create batches of data from 1 or more blocks.
-
-    This takes a block iterator and creates batch_size batches, slicing and unioning
-    blocks as needed.
-
-    This is used by both Dataset.iter_batches() and DatasetPipeline.iter_batches().
+    """Given an iterator of blocks, returns an iterator of formatted batches.
 
     Args:
-        prefetch_blocks: The number of blocks to prefetch ahead of the
-            current block during the scan.
-        clear_block_after_read: Whether to clear the block from object store
-            manually (i.e. without waiting for Python's automatic GC) after it
-            is read. Doing so will reclaim memory faster and hence reduce the
-            memory footprint. However, the caller has to ensure the safety, i.e.
-            the block will never be accessed again.
-        batch_size: Record batch size, or None to let the system pick.
-        batch_format: The format in which to return each batch.
-            Specify "default" to use the current block format (promoting
-            Arrow to pandas automatically), "pandas" to
-            select ``pandas.DataFrame`` or "pyarrow" to select
-            ``pyarrow.Table``. Default is "default".
-        drop_last: Whether to drop the last batch if it's incomplete.
-        shuffle_buffer_min_size: If non-None, the data will be randomly shuffled using a
-            local in-memory shuffle buffer, and this value will serve as the minimum
-            number of rows that must be in the local in-memory shuffle buffer in order
-            to yield a batch.
-        shuffle_seed: The seed to use for the local random shuffle.
+        block_iter: An iterator over blocks.
+        batch_format: The batch format to use.
 
     Returns:
-        An iterator over record batches.
+        An iterator over formatted batches.
     """
-    if shuffle_buffer_min_size is not None:
-        batcher = ShufflingBatcher(
-            batch_size=batch_size,
-            shuffle_buffer_min_size=shuffle_buffer_min_size,
-            shuffle_seed=shuffle_seed,
-        )
-    else:
-        batcher = Batcher(batch_size=batch_size)
-
-    def get_batches(block: Optional[ObjectRef[Block]] = None) -> Iterator[BatchType]:
-        if block is not None:
-            with stats.iter_get_s.timer():
-                block = ray.get(block)
-            # NOTE: Since we add one block at a time and then immediately consume
-            # batches, we don't need to check batcher.can_add() before adding the block;
-            # it will always be True, and batcher.add() will assert this internally.
-            batcher.add(block)
-        else:
-            batcher.done_adding()
-        while batcher.has_batch():
-            # While the batcher has full batches, yield batches.
-            with stats.iter_next_batch_s.timer():
-                batch = batcher.next_batch()
-            with stats.iter_format_batch_s.timer():
-                result = _format_batch(batch, batch_format)
-            with stats.iter_user_s.timer():
-                yield result
-        # Handle remainder batches.
-        if block is None and not drop_last and batcher.has_any():
-            with stats.iter_next_batch_s.timer():
-                batch = batcher.next_batch()
-            with stats.iter_format_batch_s.timer():
-                result = _format_batch(batch, batch_format)
-            with stats.iter_user_s.timer():
-                yield result
-
-    block_window = []  # Handle empty sliding window gracefully.
-    context = DatasetContext.get_current()
-    if (
-        prefetch_blocks > 0
-        and context.actor_prefetcher_enabled
-        and not ray.util.client.ray.is_connected()
-    ):
-        prefetcher = ActorBlockPrefetcher()
-    else:
-        prefetcher = WaitBlockPrefetcher()
-
-    # Batch blocks over the prefetch windows.
-    for block_window in _sliding_window(
-        blocks, prefetch_blocks + 1, clear_block_after_read
-    ):
-        block_window = list(block_window)
-        with stats.iter_wait_s.timer():
-            prefetcher.prefetch_blocks(block_window)
-        yield from get_batches(block_window[0])
-
-    # Consume remainder of final block window.
-    for block in block_window[1:]:
-        yield from get_batches(block)
-
-    # Consume any remaining batches, now that we're done adding blocks to the batcher.
-    yield from get_batches()
-
-
-def _format_batch(batch: Block, batch_format: str) -> BatchType:
-    if batch_format == "default" or batch_format == "native":
-        batch = BlockAccessor.for_block(batch).to_default()
-    elif batch_format == "pandas":
-        batch = BlockAccessor.for_block(batch).to_pandas()
-    elif batch_format == "pyarrow":
-        batch = BlockAccessor.for_block(batch).to_arrow()
-    elif batch_format == "numpy":
-        batch = BlockAccessor.for_block(batch).to_numpy()
-    else:
-        raise ValueError(
-            f"The given batch format '{batch_format}' is invalid. Supported "
-            f"`batch_format` values: {'default', 'pandas', 'pyarrow', 'numpy'}."
-        )
-    return batch
-
-
-def _sliding_window(iterable: Iterable, n: int, clear_block_after_read: bool = False):
-    """Creates an iterator consisting of n-width sliding windows over
-    iterable. The sliding windows are constructed lazily such that an
-    element on the base iterator (iterable) isn't consumed until the
-    first sliding window containing that element is reached.
-
-    If n > len(iterable), then a single len(iterable) window is
-    returned.
-
-    Args:
-        iterable: The iterable on which the sliding window will be
-            created.
-        n: The width of the sliding window.
-        clear_block_after_read: Whether to clear the leftmost block
-            from object store manually (i.e. without waiting for Python's
-            automatic GC) when it's out of the sliding window (i.e. been
-            consumed), so as to reclaim memory faster. The caller has to
-            ensure safety, i.e. the block will never be accessed again.
-
-    Returns:
-        An iterator of n-width windows over iterable.
-        If n > len(iterable), then a single len(iterable) window is
-        returned.
-    """
-    it = iter(iterable)
-    window = collections.deque(itertools.islice(it, n), maxlen=n)
-    if len(window) > 0:
-        yield tuple(window)
-    for elem in it:
-        block_ref = window.popleft()
-        if clear_block_after_read:
-            ray._private.internal_api.free(block_ref, local_only=False)
-        window.append(elem)
-        yield tuple(window)
+    for block in block_iter:
+        batch = BlockAccessor.for_block(block).to_batch_format(batch_format)
+        yield batch
 
 
 class BlockPrefetcher:
