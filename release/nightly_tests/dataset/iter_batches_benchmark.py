ce28a341d6e51fe2d67ce6ce37e190a3cf51758b
jianoaix <iamjianxiao@gmail.com>
2023-01-04 20:33:41 +0000

    comment
    Signed-off-by: jianoaix <iamjianxiao@gmail.com>

--- release/nightly_tests/dataset/iter_batches_benchmark.py
+++ release/nightly_tests/dataset/iter_batches_benchmark.py
@@ -30,21 +30,34 @@ def iter_batches(
             local_shuffle_buffer_size=local_shuffle_buffer_size,
         ):
             num_batches += 1
-    print("iter_batches done, num_rows:", ds.count(), "num_batches:", num_batches)
+    print(
+        "iter_batches done, block_format:",
+        ds.dataset_format(),
+        "batch_format:",
+        batch_format,
+        "num_rows:",
+        ds.count(),
+        "num_blocks:",
+        ds.num_blocks(),
+        "num_batches:",
+        num_batches,
+    )
     return ds
 
 
 def run_iter_batches_benchmark(benchmark: Benchmark):
+    # Total number of rows: 8,759,874
+    # Avg rows per block: 729,989
     ds = (
         ray.data.read_parquet(
             "s3://anonymous@air-example-data/ursa-labs-taxi-data/by_year/2018/01"
         )
-        .repartition(24)
+        .repartition(12)
         .fully_executed()
     )
 
     batch_formats = ["pandas", "numpy"]
-    batch_sizes = [128, 256, 512]
+    batch_sizes = [4 * 1024, 16 * 1024, 64 * 1024]
 
     # Test default args.
     test_name = "iter-batches-default"
@@ -52,7 +65,6 @@ def run_iter_batches_benchmark(benchmark: Benchmark):
         test_name,
         iter_batches,
         ds=ds,
-        batch_format="INVALID",
         use_default_params=True,
     )
 
@@ -85,6 +97,24 @@ def run_iter_batches_benchmark(benchmark: Benchmark):
                     batch_format=batch_format,
                     local_shuffle_buffer_size=shuffle_buffer_size,
                 )
+
+    # Test block concatnation to create batches.
+    # Total number of rows: 8,759,874
+    # Avg rows per block: 17,108
+    new_ds = ds.repartition(512)
+    new_ds = new_ds.map_batches(
+        lambda ds: ds, batch_format="pandas", batch_size=None
+    ).fully_executed()
+    print("ds:", new_ds)
+    for batch_size in [32 * 1024, 64 * 1024, 256 * 1024]:
+        test_name = f"iter-batches-block-concat-to-batch-{batch_size}"
+        benchmark.run(
+            test_name,
+            iter_batches,
+            ds=new_ds,
+            batch_format="pandas",
+            batch_size=batch_size,
+        )
 
     # TODO(jian): test the prefetch as we set up multi-node cluster test.
 
