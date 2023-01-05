d2d9e3efd0831adeb34ff33123ab96f63601b517
Cindy Zhang <cindyzyx9@gmail.com>
2023-01-05 14:59:34 -0800

    [Jobs] Track real job process (#31306)
    Resolves the issue described in https://github.com/ray-project/ray/issues/31274. On Linux systems, when a stop signal is sent, instead of killing + waiting on only the shell process (which starts the actual job as a child process), we want to kill all the children of the shell process along with the shell process itself, and poll all processes until they exit or send a force SIGKILL on timeout. (This change is compatible with Mac OSX systems as well)
    
    Co-authored-by: shrekris-anyscale <92341594+shrekris-anyscale@users.noreply.github.com>

--- dashboard/modules/job/tests/test_job_manager.py
+++ dashboard/modules/job/tests/test_job_manager.py
@@ -774,7 +774,10 @@ class TestTailLogs:
             job_manager.stop_job(job_id)
 
             async for lines in job_manager.tail_job_logs(job_id):
-                assert all(s == "Waiting..." for s in lines.strip().split("\n"))
+                assert all(
+                    s == "Waiting..." or s == "Terminated"
+                    for s in lines.strip().split("\n")
+                )
                 print(lines, end="")
 
             await async_wait_for_condition_async_predicate(
@@ -850,6 +853,14 @@ while True:
 
     assert job_manager.stop_job(job_id) is True
 
+    with pytest.raises(RuntimeError):
+        await async_wait_for_condition_async_predicate(
+            check_job_stopped,
+            job_manager=job_manager,
+            job_id=job_id,
+            timeout=stop_timeout - 1,
+        )
+
     await async_wait_for_condition(
         lambda: "SIGTERM signal handled!" in job_manager.get_job_logs(job_id)
     )
@@ -858,7 +869,7 @@ while True:
         check_job_stopped,
         job_manager=job_manager,
         job_id=job_id,
-        timeout=stop_timeout + 10,
+        timeout=10,
     )
 
 
