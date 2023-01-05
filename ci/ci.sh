71dfb8a9604c13dc42aa3fd7637d089a7cba7015
Kornel Csernai <749306+csko@users.noreply.github.com>
2023-01-04 20:39:06 -0600

    match __commit__ only on the beginning of the line in wheel check
    Signed-off-by: Kornel Csernai <749306+csko@users.noreply.github.com>

--- ci/ci.sh
+++ ci/ci.sh
@@ -414,7 +414,7 @@ validate_wheels_commit_str() {
       continue
     fi
 
-    WHL_COMMIT=$(unzip -p "$whl" | grep "__commit__" | awk -F'"' '{print $2}')
+    WHL_COMMIT=$(unzip -p "$whl" | grep "^__commit__" | awk -F'"' '{print $2}')
 
     if [ "${WHL_COMMIT}" != "${EXPECTED_COMMIT}" ]; then
       echo "Error: Observed wheel commit (${WHL_COMMIT}) is not expected commit (${EXPECTED_COMMIT}). Aborting."
