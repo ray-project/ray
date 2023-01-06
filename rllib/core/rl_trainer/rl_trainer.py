098ca6748ecbc1b7ce7ce05ad3d83efde03dc77d
avnish <avnish@anyscale.com>
2023-01-05 17:24:19 -0800

    Move rl optimizer logic to RLTrainer
    Signed-off-by: avnish <avnish@anyscale.com>

--- rllib/core/rl_trainer/rl_trainer.py
+++ rllib/core/rl_trainer/rl_trainer.py
@@ -1,5 +1,5 @@
 import abc
-from typing import Any, Mapping, Tuple, Union, Type
+from typing import Any, Mapping, NewType, Tuple, Union, Type
 
 from ray.rllib.core.rl_module.rl_module import RLModule, ModuleID
 from ray.rllib.core.rl_module.marl_module import MultiAgentRLModule
@@ -9,6 +9,8 @@ from ray.rllib.policy.sample_batch import MultiAgentBatch
 from ray.rllib.utils.nested_dict import NestedDict
 from ray.rllib.utils.numpy import convert_to_numpy
 from ray.rllib.utils.typing import TensorType
+
+Optimizer = NewType("Optimizer", object)
 
 
 class RLTrainer:
@@ -73,6 +75,68 @@ class RLTrainer:
         self.optimizer_kwargs = optimizer_kwargs
         self.scaling_config = scaling_config
         self.distributed = distributed
+        self._rl_optimizers: Mapping[ModuleID, Any] = {}
+
+    @abc.abstractmethod
+    def compute_loss(
+        self, batch: Mapping[str, Any], fwd_out: Mapping[str, Any]
+    ) -> Union[TensorType, Mapping[str, Any]]:
+        """Computes variables for optimizing self._module based on fwd_out.
+
+        Args:
+            fwd_out: Output from a call to `forward_train` on self._module during
+                training.
+            batch: The data that was used to compute fwd_out.
+
+        Returns:
+            Either a single loss tensor which can be used for computing
+            gradients through, or a dictionary of losses. NOTE the dictionary
+            must contain one protected key "total_loss" which will be used for
+            computing gradients through.
+        """
+
+    def update(self, batch: MultiAgentBatch) -> Mapping[str, Any]:
+        """Perform an update on this Trainer.
+
+        Args:
+            batch: A batch of data.
+
+        Returns:
+            A dictionary of results.
+        """
+        if not self.distributed:
+            fwd_out = self._module.forward_train(batch)
+            loss = self.compute_loss(batch, fwd_out)
+            gradients = self.compute_gradients(loss)
+            post_processed_gradients = self.on_after_compute_gradients(gradients)
+            self.apply_gradients(post_processed_gradients)
+        else:
+            self.do_distributed_update(batch)
+        return self.compile_results(batch, fwd_out, loss, post_processed_gradients)
+
+    @abc.abstractmethod
+    def compute_gradients(
+        self, loss: Union[TensorType, Mapping[str, Any]]
+    ) -> Mapping[str, Any]:
+        """Perform an update on self._module.
+
+        For example compute and apply gradients to self._module if
+        necessary.
+
+        Args:
+            loss: variable(s) used for optimizing self._module.
+
+        Returns:
+            A dictionary of extra information and statistics.
+        """
+
+    @abc.abstractmethod
+    def apply_gradients(self, gradients: Mapping[str, Any]) -> None:
+        """Perform an update on self._module
+
+        Args:
+            gradients: A dictionary of gradients.
+        """
 
     def on_after_compute_gradients(
         self, gradients_dict: Mapping[str, Any]
@@ -120,49 +184,6 @@ class RLTrainer:
             **loss_numpy,
         }
 
-    def update(self, batch: MultiAgentBatch) -> Mapping[str, Any]:
-        """Perform an update on this Trainer.
-
-        Args:
-            batch: A batch of data.
-
-        Returns:
-            A dictionary of results.
-        """
-        if not self.distributed:
-            fwd_out = self._module.forward_train(batch)
-            loss = self._rl_optimizer.compute_loss(batch, fwd_out)
-            gradients = self.compute_gradients(loss)
-            post_processed_gradients = self.on_after_compute_gradients(gradients)
-            self.apply_gradients(post_processed_gradients)
-        else:
-            self.do_distributed_update(batch)
-        return self.compile_results(batch, fwd_out, loss, post_processed_gradients)
-
-    @abc.abstractmethod
-    def compute_gradients(
-        self, loss: Union[TensorType, Mapping[str, Any]]
-    ) -> Mapping[str, Any]:
-        """Perform an update on self._module.
-
-        For example compute and apply gradients to self._module if
-        necessary.
-
-        Args:
-            loss: variable(s) used for optimizing self._module.
-
-        Returns:
-            A dictionary of extra information and statistics.
-        """
-
-    @abc.abstractmethod
-    def apply_gradients(self, gradients: Mapping[str, Any]) -> None:
-        """Perform an update on self._module
-
-        Args:
-            gradients: A dictionary of gradients.
-        """
-
     def set_state(self, state: Mapping[str, Any]) -> None:
         """Set the state of the trainer.
 
@@ -204,10 +225,7 @@ class RLTrainer:
             optimizer_kwargs: The config for the optimizer.
 
         """
-        module = module_cls.from_model_config(**module_kwargs)
-        self._module.add_module(module_id, module)
-        optimizer = optimizer_cls(module, optimizer_kwargs)
-        self._rl_optimizer.add_optimizer(module_id, optimizer)
+        raise NotImplementedError
 
     def remove_module(self, module_id: ModuleID) -> None:
         """Remove a module from the trainer.
@@ -217,39 +235,50 @@ class RLTrainer:
 
         """
         self._module.remove_module(module_id)
-        self._rl_optimizer.remove_optimizer(module_id)
+        del self._rl_optimizers[module_id]
+
+    @abc.abstractmethod
+    def init_module(self, config: Mapping[str, Any]) -> RLModule:
+        """Initialize the RLModule or MARLModule that is going to be trained.
+
+        Args:
+            config: The config to use for the initializing the module.
+
+        Returns:
+            The initialized module.
+
+        Note: if an RLModule is returned it will be wrapped in as a
+            MultiAgentRLModule.
+        """
+
+    @abc.abstractmethod
+    def init_optimizers(self, module: MultiAgentRLModule) -> NestedDict[Optimizer]:
+        """Configure the optimizer(s) to use.
+
+        Returns:
+            The optimizer(s) to use for updating the module being.
+        """
 
     def init_trainer(self) -> None:
         """Initialize the model."""
         if self.distributed:
             self._module, self._rl_optimizer = self._make_distributed()
         else:
-            if issubclass(self.module_class, MultiAgentRLModule):
-                self._module = self.module_class.from_multi_agent_config(
-                    **self.module_kwargs
-                )
-            elif issubclass(self.module_class, RLModule):
-                self._module = self.module_class.from_model_config(
-                    **self.module_kwargs
-                ).as_multi_agent()
-            else:
+            module = self.init_module(self.module_kwargs)
+
+            if not isinstance(module, MultiAgentRLModule) and isinstance(
+                module, RLModule
+            ):
+                module = module.as_multi_agent()
+
+            if not (
+                isinstance(module, MultiAgentRLModule) or isinstance(module, RLModule)
+            ):
                 raise ValueError(
                     f"Module class {self.module_class} is not a subclass of "
                     f"RLModule or MultiAgentRLModule."
                 )
-            if issubclass(self.optimizer_class, RLOptimizer):
-                self._rl_optimizer = self.optimizer_class.from_module(
-                    self._module, **self.optimizer_kwargs
-                ).as_multi_agent()
-            elif issubclass(self.optimizer_class, MultiAgentRLOptimizer):
-                self._rl_optimizer = self.optimizer_class.from_marl_module(
-                    self._module, **self.optimizer_kwargs
-                )
-            else:
-                raise ValueError(
-                    f"Optimizer class {self.optimizer_class} is not a subclass of "
-                    f"RLOptimizer or MultiAgentRLOptimizer."
-                )
+            self._rl_optimizers = self.configure_optimizers()
 
     def do_distributed_update(self, batch: MultiAgentBatch) -> Mapping[str, Any]:
         """Perform a distributed update on this Trainer.
