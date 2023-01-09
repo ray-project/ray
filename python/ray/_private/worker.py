c66b645fc62a7de25e7b948e345d897875b9586a
Chen Shen <scv119@gmail.com>
2023-01-09 12:46:24 -0800

    Update python/ray/_private/worker.py
    Co-authored-by: Cade Daniel <edacih@gmail.com>
    Signed-off-by: Chen Shen <scv119@gmail.com>

--- python/ray/_private/worker.py
+++ python/ray/_private/worker.py
@@ -688,11 +688,11 @@ class Worker:
         )
 
     @Deprecated(
-        message="This function is deprecated and will be removed by Ray 2.4, "
-        "please use Runtime Environment "
+        message="This function is deprecated and will be removed by Ray 2.4. "
+        "Please use Runtime Environments "
         f"https://docs.ray.io/en/{get_ray_doc_version()}/ray-core"
         "/handling-dependencies.html "
-        "to manage worker's dependencies.",
+        "to manage dependencies in workers.",
         warning=True,
     )
     def run_function_on_all_workers(self, function: callable):
