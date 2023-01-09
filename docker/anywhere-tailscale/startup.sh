eedf6d40c42dd726da7095e6accc5345d1b86903
jcoffi <jcoffi@gmail.com>
2023-01-09 11:24:48 -0800

    updated hostnames

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -17,16 +17,6 @@ dhcppipe=/tmp/d$RANDOMSTRING
 mkfifo $edgepipe
 mkfifo $dhcppipe
 
-# If NODETYPE is "head", run the supernode command and append some text to .bashrc
-if [ "$NODETYPE" = "head" ]; then
-sudo tailscaled &
-sudo tailscale up --authkey=tskey-auth-kTSQbo3CNTRL-bWzNQtfVbgfmqTbd9zc5mffSAWJoMLLTB --accept-risk=all --hostname=nexus --accept-dns
-else
-sudo tailscaled &
-sudo tailscale up --authkey=tskey-auth-kTSQbo3CNTRL-bWzNQtfVbgfmqTbd9zc5mffSAWJoMLLTB --accept-risk=all --accept-dns
-fi
-
-
 set -ae
 
 # GC logging set to default value of path.logs
@@ -39,11 +29,36 @@ mkdir -pv $CRATE_GC_LOG_DIR $CRATE_HEAP_DUMP_PATH
 # Special VM options for Java in Docker
 CRATE_JAVA_OPTS="-Des.cgroups.hierarchy.override=/ $CRATE_JAVA_OPTS"
 
+sudo tailscaled &
+
+# If NODETYPE is "head", run the supernode command and append some text to .bashrc
+if [ "$NODETYPE" = "head" ]; then
+
+sudo tailscale up --authkey=tskey-auth-kTSQbo3CNTRL-bWzNQtfVbgfmqTbd9zc5mffSAWJoMLLTB --accept-risk=all --hostname=nexus --accept-dns
+
 /crate/bin/crate -Cnetwork.host=_${N2N_INTERFACE}_ \
-            #-Cnode.name=${DDNS_HOST} \
+            -Cnode.name=nexus.chimp-beta.ts.net \
             -Cdiscovery.type=zen
-            -Ccluster.initial_master_nodes=nexus \
+            -Ccluster.initial_master_nodes=nexus.chimp-beta.ts.net \
             -Cgateway.expected_data_nodes=2 \
             -Cgateway.recover_after_data_nodes=1 \
             &
-#need to make it so that we discover and connect via n2n ips
+else
+
+sudo tailscale up --authkey=tskey-auth-kTSQbo3CNTRL-bWzNQtfVbgfmqTbd9zc5mffSAWJoMLLTB --accept-risk=all --accept-dns
+nexus=$(tailscale ip nexus)
+/crate/bin/crate -Cnetwork.host=_${N2N_INTERFACE}_ \
+            #-Cnode.name=${DDNS_HOST} \
+            -Cdiscovery.type=zen
+            -Ccluster.initial_master_nodes=nexus.chimp-beta.ts.net \
+            -Cgateway.expected_data_nodes=2 \
+            -Cgateway.recover_after_data_nodes=1 \
+            &
+fi
+
+
+
+
+
+
+
