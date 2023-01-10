99d793876e57cdf049d0cbe04d313bfe10bfa9dd
jcoffi <jcoffi@gmail.com>
2023-01-09 19:32:29 -0800

    removed             -Cnode.master=false \

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -59,7 +59,6 @@ done
 nexus=$(tailscale ip -4 nexus)
 
 /crate/bin/crate -Cnetwork.host=_${N2N_INTERFACE}_ \
-            -Cnode.master=false \
             -Cnode.data=true \
             -Cnode.store.allow_mmap=false \
             -Cdiscovery.seed_hosts=nexus:4300 \
