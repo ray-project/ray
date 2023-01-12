284f5a18c46617421d81e19f5ca0f9aa09ef2e7d
SangBin Cho <rkooo567@gmail.com>
2023-01-12 07:36:14 -0800

    minor
    Signed-off-by: SangBin Cho <rkooo567@gmail.com>

--- python/ray/_private/metrics_agent.py
+++ python/ray/_private/metrics_agent.py
@@ -32,7 +32,6 @@ from opencensus.tags import tag_value as tag_value_module
 import ray
 from ray._private.gcs_utils import GcsClient
 
-# from opencensus.proto.metrics.v1.metrics_pb2 import Metric
 from ray.core.generated.metrics_pb2 import Metric
 
 logger = logging.getLogger(__name__)
