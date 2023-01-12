4a2a733b213d92d142c844269c6bdd3020f0b2b8
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2023-01-12 13:31:17 -0800

    added tests to build the trainer_runner from algorithm_config
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- rllib/core/rl_trainer/tests/test_trainer_runner_config.py
+++ rllib/core/rl_trainer/tests/test_trainer_runner_config.py
@@ -1,3 +1,4 @@
+import dataclasses
 import gym
 import unittest
 
@@ -5,6 +6,7 @@ import ray
 from ray.rllib.core.rl_trainer.trainer_runner_config import TrainerRunnerConfig
 from ray.rllib.core.testing.tf.bc_module import DiscreteBCTFModule
 from ray.rllib.core.testing.tf.bc_rl_trainer import BCTfRLTrainer
+from ray.rllib.algorithms.algorithm_config import AlgorithmConfig
 
 
 class TestAlgorithmConfig(unittest.TestCase):
@@ -17,6 +19,7 @@ class TestAlgorithmConfig(unittest.TestCase):
         ray.shutdown()
 
     def test_trainer_runner_build(self):
+        """Tests whether the trainer_runner can be constructed and built."""
 
         env = gym.make("CartPole-v1")
 
@@ -34,6 +37,28 @@ class TestAlgorithmConfig(unittest.TestCase):
         )
         config = config.testing(True)
         config.build()
+
+    def test_trainer_runner_build_from_algorithm_config(self):
+        """Tests whether we can build a trainer runner object from algorithm_config."""
+
+        env = gym.make("CartPole-v1")
+
+        @dataclasses.dataclass
+        class DummyPolicy:
+            observation_space = env.observation_space
+            action_space = env.action_space
+
+        policy = DummyPolicy()
+
+        config = (
+            AlgorithmConfig()
+            .rl_module(rl_module_class=DiscreteBCTFModule)
+            .training(rl_trainer_class=BCTfRLTrainer)
+            .training(model={"hidden_dim": 32})
+        )
+        config.freeze()
+        runner_config = config.get_trainer_runner_config(policy)
+        runner_config.build()
 
 
 if __name__ == "__main__":
