a82ec83693da7186b17b0be12ead1514d71c1d5e
Alan Guo <aguo@anyscale.com>
2023-01-11 16:54:00 -0800

    Only return error if permission error is detected
    Signed-off-by: Alan Guo <aguo@anyscale.com>

--- dashboard/modules/reporter/profile_manager.py
+++ dashboard/modules/reporter/profile_manager.py
@@ -7,10 +7,29 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+PYSPY_PERMISSIONS_ERROR_MESSAGE = """
+Note that this command requires `py-spy` to be installed with root permissions. You
+can install `py-spy` and give it root permissions as follows:
+  $ pip install py-spy
+  $ sudo chown root:root `which py-spy`
+  $ sudo chmod u+s `which py-spy`
+
+Alternatively, you can start Ray with passwordless sudo / root permissions.
+
+"""
+
 
 def _format_failed_pyspy_command(cmd, stdout, stderr) -> str:
-    return f"""Failed to execute `{cmd}`.
+    stderr_str = stderr.decode("utf-8")
 
+    # If some sort of permission error returned, show a message about how
+    # to set up permissions correctly.
+    extra_message = (
+        PYSPY_PERMISSIONS_ERROR_MESSAGE if "permission" in stderr_str else ""
+    )
+
+    return f"""Failed to execute `{cmd}`.
+{extra_message}
 === stderr ===
 {stderr.decode("utf-8")}
 
