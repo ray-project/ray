fdfdb0a2f58ed9ab67e7c53dc8780b5f06b90701
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2023-01-13 09:02:29 +0000

    fix Signed-off-by: Yi Cheng <74173148+iycheng@users.noreply.github.com>

--- src/ray/gcs/gcs_server/gcs_server.cc
+++ src/ray/gcs/gcs_server/gcs_server.cc
@@ -741,7 +741,8 @@ std::shared_ptr<RedisClient> GcsServer::GetOrConnectRedis() {
     // Init redis failure detector.
     gcs_redis_failure_detector_ = std::make_shared<GcsRedisFailureDetector>(
         main_service_, redis_client_->GetPrimaryContext(), [this]() {
-          RAY_LOG(FATAL) << "Redis failed. Shutdown GCS."; });
+          RAY_LOG(FATAL) << "Redis failed. Shutdown GCS.";
+        });
     gcs_redis_failure_detector_->Start();
   }
   return redis_client_;
