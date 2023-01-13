e819abd54f598f8a0c409bab2e5dff587a829d47
Larry <554538252@qq.com>
2023-01-13 14:15:23 +0800

    Support cpp worker get java actor by name (#30886)
    - [x] Cpp worker support get java actor by actor name.
    - [x] Support Java worker get cpp actor which created by cpp worker.
    
    1.  get java actor by actor name api:
    ```
    boost::optional<ActorHandleXlang> GetActor(const std::string &actor_name,
                                                      const std::string &ray_namespce = "") ;
    ```
    2. Support Java worker get cpp actor which created by cpp worker：
    Now when java obtains the c++ worker created by the c++ worker through the actor name, an error will be reported. The reason for the error is that the c++ actor created by the c++ worker lacks the class_name parameter. So this modification plan is that the class_name parameter will be filled in when the c++ worker is created by the c++ worker.
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- java/test/src/main/java/io/ray/test/Counter.java
+++ java/test/src/main/java/io/ray/test/Counter.java
@@ -1,15 +1,52 @@
 package io.ray.test;
 
+import io.ray.api.ActorHandle;
+import io.ray.api.Ray;
+import org.testng.Assert;
+
 public class Counter {
 
-  public Counter(int v) {
-    value = v;
+  private int value;
+
+  private ActorHandle<Counter> childActor;
+
+  public Counter(int initValue) {
+    this.value = initValue;
   }
 
-  public int increase(int delta) {
+  public int getValue() {
+    return value;
+  }
+
+  public void increase(int delta) {
+    value += delta;
+  }
+
+  public int increaseAndGet(int delta) {
     value += delta;
     return value;
   }
 
-  private int value;
+  public String echo(String str) {
+    return str;
+  }
+
+  public String createChildActor(String actorName) {
+    childActor = Ray.actor(Counter::new, 0).setName(actorName).remote();
+    Assert.assertEquals(Integer.valueOf(0), childActor.task(Counter::getValue).remote().get());
+    return "OK";
+  }
+
+  public static class NestedActor {
+
+    public NestedActor(String s) {
+      str = s;
+    }
+
+    public String concat(String s) {
+      return str + s;
+    }
+
+    private String str;
+  }
 }
