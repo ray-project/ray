e4e89c91c002c1dcfda996cffe4e00a2e4c5bfa0
Ricky Xu <xuchen727@hotmail.com>
2023-01-13 19:18:22 -0500

    [core][state] Task state fault tolerance - GCS marks children task failed if parent task fails. [1/x]  (#31524)
    Signed-off-by: rickyyx <rickyx@anyscale.com>
    
    We currently don't mark children tasks as failed if the parent fails. This PR adds a synchronous routine MarkTaskTreeFailed for each newly reported task event. And marks all children tasks in the task tree, with the reported task as the root, as failed, if:
    
    The reported task itself is a task failure.
    The parent of the reported task has already failed.
    In the next PRs:
    
    When a job failed, all tasks and their children with the failed job id will be marked as failed.
    Detached actor's tasks should not be failed if their parents fail.
    We will add a column to TaskState for task exit details, e.g.exit_detail = Task failed because of parent task XXX failed at timestamp YYY.
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- python/ray/tests/test_task_events.py
+++ python/ray/tests/test_task_events.py
@@ -6,11 +6,14 @@ from ray._private.test_utils import (
     raw_metrics,
     wait_for_condition,
 )
+from ray.experimental.state.api import list_tasks
+
 from ray._private.worker import RayContext
 
 _SYSTEM_CONFIG = {
     "task_events_report_interval_ms": 100,
     "metrics_report_interval_ms": 200,
+    "enable_timeline": False,
 }
 
 
@@ -30,18 +33,15 @@ def aggregate_task_event_metric(info: RayContext) -> Dict:
     task_events_info = defaultdict(int)
     if "ray_gcs_task_manager_task_events_dropped" in res:
         for sample in res["ray_gcs_task_manager_task_events_dropped"]:
-            print(sample)
             if "Type" in sample.labels and sample.labels["Type"] != "":
                 task_events_info["DROPPED_" + sample.labels["Type"]] += sample.value
 
     if "ray_gcs_task_manager_task_events_stored" in res:
         for sample in res["ray_gcs_task_manager_task_events_stored"]:
-            print(sample)
             task_events_info["STORED"] += sample.value
 
     if "ray_gcs_task_manager_task_events_reported" in res:
         for sample in res["ray_gcs_task_manager_task_events_reported"]:
-            print(sample)
             task_events_info["REPORTED"] += sample.value
 
     return task_events_info
@@ -72,4 +72,49 @@ def test_status_task_events_metrics(shutdown_only):
         verify,
         timeout=20,
         retry_interval_ms=100,
+    )
+
+
+def test_fault_tolerance_parent_failed(shutdown_only):
+    ray.init(num_cpus=4, _system_config=_SYSTEM_CONFIG)
+    import time
+
+    # Each parent task spins off 2 child task, where each child spins off
+    # 1 grand_child task.
+    NUM_CHILD = 2
+
+    @ray.remote
+    def grand_child():
+        time.sleep(999)
+
+    @ray.remote
+    def child():
+        ray.get(grand_child.remote())
+
+    @ray.remote
+    def parent():
+        for _ in range(NUM_CHILD):
+            child.remote()
+        # Sleep for a bit and kill itself.
+        time.sleep(3)
+        raise ValueError("parent task is expected to fail")
+
+    parent.remote()
+
+    def verify():
+        tasks = list_tasks()
+        assert len(tasks) == 5, (
+            "Incorrect number of tasks are reported. "
+            "Expected length: 1 parent + 2 child + 2 grand_child tasks"
+        )
+        print(tasks)
+        for task in tasks:
+            assert task["scheduling_state"] == "FAILED"
+
+        return True
+
+    wait_for_condition(
+        verify,
+        timeout=10,
+        retry_interval_ms=500,
     )
--- src/ray/gcs/pb_util.h
+++ src/ray/gcs/pb_util.h
@@ -216,6 +216,108 @@ inline std::string RayErrorInfoToString(const ray::rpc::RayErrorInfo &error_info
   return ss.str();
 }
 
+/// Get the parent task id from the task event.
+///
+/// \param task_event Task event.
+/// \return TaskID::Nil() if parent task id info not available, else the parent task id
+/// for the task.
+inline TaskID GetParentTaskId(const rpc::TaskEvents &task_event) {
+  if (task_event.has_task_info()) {
+    return TaskID::FromBinary(task_event.task_info().parent_task_id());
+  }
+  return TaskID::Nil();
+}
+
+/// Get the timestamp of the task status if available.
+///
+/// \param task_event Task event.
+/// \return Timestamp of the task status change if status update available, nullopt
+/// otherwise.
+inline absl::optional<int64_t> GetTaskStatusTimeFromStateUpdates(
+    const ray::rpc::TaskStatus &task_status, const rpc::TaskStateUpdate &state_updates) {
+  switch (task_status) {
+  case rpc::TaskStatus::PENDING_ARGS_AVAIL: {
+    if (state_updates.has_pending_args_avail_ts()) {
+      return state_updates.pending_args_avail_ts();
+    }
+    break;
+  }
+  case rpc::TaskStatus::SUBMITTED_TO_WORKER: {
+    if (state_updates.has_submitted_to_worker_ts()) {
+      return state_updates.submitted_to_worker_ts();
+    }
+    break;
+  }
+  case rpc::TaskStatus::PENDING_NODE_ASSIGNMENT: {
+    if (state_updates.has_pending_node_assignment_ts()) {
+      return state_updates.pending_node_assignment_ts();
+    }
+    break;
+  }
+  case rpc::TaskStatus::FINISHED: {
+    if (state_updates.has_finished_ts()) {
+      return state_updates.finished_ts();
+    }
+    break;
+  }
+  case rpc::TaskStatus::FAILED: {
+    if (state_updates.has_failed_ts()) {
+      return state_updates.failed_ts();
+    }
+    break;
+  }
+  case rpc::TaskStatus::RUNNING: {
+    if (state_updates.has_running_ts()) {
+      return state_updates.running_ts();
+    }
+    break;
+  }
+  default: {
+    UNREACHABLE;
+  }
+  }
+  return absl::nullopt;
+}
+
+/// Fill the rpc::TaskStateUpdate with the timestamps according to the status change.
+///
+/// \param task_status The task status.
+/// \param timestamp The timestamp.
+/// \param[out] state_updates The state updates with timestamp to be updated.
+inline void FillTaskStatusUpdateTime(const ray::rpc::TaskStatus &task_status,
+                                     int64_t timestamp,
+                                     ray::rpc::TaskStateUpdate *state_updates) {
+  switch (task_status) {
+  case rpc::TaskStatus::PENDING_ARGS_AVAIL: {
+    state_updates->set_pending_args_avail_ts(timestamp);
+    break;
+  }
+  case rpc::TaskStatus::SUBMITTED_TO_WORKER: {
+    state_updates->set_submitted_to_worker_ts(timestamp);
+    break;
+  }
+  case rpc::TaskStatus::PENDING_NODE_ASSIGNMENT: {
+    state_updates->set_pending_node_assignment_ts(timestamp);
+    break;
+  }
+  case rpc::TaskStatus::FINISHED: {
+    state_updates->set_finished_ts(timestamp);
+    break;
+  }
+  case rpc::TaskStatus::FAILED: {
+    state_updates->set_failed_ts(timestamp);
+    break;
+  }
+  case rpc::TaskStatus::RUNNING: {
+    state_updates->set_running_ts(timestamp);
+    break;
+  }
+  default: {
+    UNREACHABLE;
+  }
+  }
+}
+
 }  // namespace gcs
 
 }  // namespace ray
