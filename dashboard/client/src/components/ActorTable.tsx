b161cb8383312cb2b623250876a2a49b272b181b
WeichenXu <weichen.xu@databricks.com>
2023-01-14 11:23:55 +0800

    init (#31659)
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>
    
    Make Ray worker traceback/cpu_profile page link available when forwarding dashboard service with proxy.
    
    On databricks, we does not allow user to access web service on cluster driver node.
    instead, databricks provide a proxy that forwards service setup on https://{driver_ip}:{port} to https://{driver_ip}/driver-proxy/o/xxx/xxx/{port}
    But, the worker traceback/cpu_profile page does not work after forwarding. So I filed the PR to fix it.
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- dashboard/client/src/components/ActorTable.tsx
+++ dashboard/client/src/components/ActorTable.tsx
@@ -333,7 +333,7 @@ const ActorTable = ({
                       </Link>
                       <br />
                       <a
-                        href={`/worker/traceback?pid=${pid}&ip=${address?.ipAddress}&native=0`}
+                        href={`worker/traceback?pid=${pid}&ip=${address?.ipAddress}&native=0`}
                         target="_blank"
                         title="Sample the current Python stack trace for this worker."
                         rel="noreferrer"
@@ -342,7 +342,7 @@ const ActorTable = ({
                       </a>
                       <br />
                       <a
-                        href={`/worker/cpu_profile?pid=${pid}&ip=${address?.ipAddress}&duration=5&native=0`}
+                        href={`worker/cpu_profile?pid=${pid}&ip=${address?.ipAddress}&duration=5&native=0`}
                         target="_blank"
                         title="Profile the Python worker for 5 seconds (default) and display a CPU flame graph."
                         rel="noreferrer"
