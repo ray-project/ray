c57332f573d43d192a5d4d7755302c8dad881322
jcoffi <jcoffi@gmail.com>
2023-01-16 11:50:57 -0800

    added sudo command for mknod

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -23,7 +23,7 @@ if [ ! -d /dev/net ]; then
     mkdir -p /dev/net
 fi
 if [ ! -c /dev/net/tun ]; then
-    mknod /dev/net/tun c 10 200
+    sudo mknod /dev/net/tun c 10 200
 fi
 
 if [ -d /dev/net/tun ]; then
