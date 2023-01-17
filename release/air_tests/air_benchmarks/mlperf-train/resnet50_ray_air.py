565eab44d7968b884bf8ec24892aa3b67d77f3f1
Clarence Ng <clarence@anyscale.com>
2023-01-17 05:14:08 -0800

    rt
    Signed-off-by: Clarence Ng <clarence@anyscale.com>

--- release/air_tests/air_benchmarks/mlperf-train/resnet50_ray_air.py
+++ release/air_tests/air_benchmarks/mlperf-train/resnet50_ray_air.py
@@ -12,6 +12,9 @@ from ray.air import session
 from ray.train.tensorflow import prepare_dataset_shard, TensorflowTrainer
 from ray.air.config import ScalingConfig
 from ray.data.preprocessors import BatchMapper
+from ray import tune
+from ray.tune import Tuner
+
 
 from tf_utils import (
     DEFAULT_IMAGE_SIZE,
@@ -449,6 +452,21 @@ if __name__ == "__main__":
             "Default value taken from MLPerf reference implementation."
         ),
     )
+    parser.add_argument(
+        "--trainer-resources-cpu",
+        default=1,
+        type=int,
+        help=("CPU resources requested per AIR trainer instance" "Defaults to 1."),
+    )
+    parser.add_argument(
+        "--tune-trials",
+        default=0,
+        type=int,
+        help=(
+            "Number of Tune trials to run. Defaults to 0, "
+            "which disables Tune and executes a Trainer instance directly."
+        ),
+    )
     parser.add_argument("--output-file", default="out.csv", type=str)
     parser.add_argument("--use-gpu", action="store_true")
     parser.add_argument("--online-processing", action="store_true")
@@ -456,9 +474,15 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     ray.init(
+        _system_config={
+            "worker_killing_policy": "group_by_owner",
+            "task_oom_retries": -1,
+            "memory_usage_threshold": 0.7,
+        },
+        num_cpus=8,
         runtime_env={
             "working_dir": os.path.dirname(__file__),
-        }
+        },
     )
 
     if args.use_tf_data or args.use_ray_data:
@@ -530,17 +554,40 @@ if __name__ == "__main__":
 
     trainer = TensorflowTrainer(
         train_loop_for_worker,
-        scaling_config=ScalingConfig(num_workers=1, use_gpu=args.use_gpu),
+        scaling_config=ScalingConfig(
+            num_workers=1,
+            use_gpu=args.use_gpu,
+            trainer_resources={"CPU": args.trainer_resources_cpu},
+        ),
         datasets=datasets,
         preprocessor=preprocessor,
         train_loop_config=train_loop_config,
     )
+
+    tuner = None
+    if args.tune_trials > 0:
+        tuner = Tuner(
+            trainer,
+            param_space={
+                "train_loop_config": {
+                    "random_var": tune.grid_search(list(range(1, args.tune_trials + 1)))
+                }
+            },
+            tune_config=tune.TuneConfig(
+                metric="time_total_s", mode="min", num_samples=1
+            ),
+        )
+
     result = {}
     exc = None
     start_time_s = time.perf_counter()
     ray_spill_stats_start = get_ray_spilled_and_restored_mb()
     try:
-        result = trainer.fit()
+        if tuner:
+            result_grid = tuner.fit()
+            result = result_grid.get_best_result()
+        else:
+            result = trainer.fit()
         result = result.metrics
     except Exception as e:
         exc = e
