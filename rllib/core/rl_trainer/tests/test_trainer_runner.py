2cc51854250476a5a5a82a5e11e5b9c922a2f3cd
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2023-01-17 14:56:05 -0800

    merged tf and torch train_runner tests
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- rllib/core/rl_trainer/tests/test_trainer_runner.py
+++ rllib/core/rl_trainer/tests/test_trainer_runner.py
@@ -2,13 +2,72 @@ import gymnasium as gym
 import unittest
 
 import tensorflow as tf
+import torch
 import ray
 
+from typing import Type, Union
+
+from ray.rllib.core.rl_module import RLModule
+from ray.rllib.core.rl_trainer.rl_trainer import RLTrainer
 from ray.rllib.core.rl_trainer.trainer_runner import TrainerRunner
-from ray.rllib.core.testing.tf.bc_module import DiscreteBCTFModule
-from ray.rllib.core.testing.tf.bc_rl_trainer import BCTfRLTrainer
 from ray.rllib.policy.sample_batch import DEFAULT_POLICY_ID, MultiAgentBatch
 from ray.rllib.utils.test_utils import get_cartpole_dataset_reader
+
+Optimizer = Union[tf.keras.optimizers.Optimizer, torch.optim.Optimizer]
+
+
+def _get_trainer_class(framework: str) -> Type[RLTrainer]:
+    if framework == "tf":
+        from ray.rllib.core.testing.tf.bc_rl_trainer import BCTfRLTrainer
+
+        return BCTfRLTrainer
+    elif framework == "torch":
+        from ray.rllib.core.testing.torch.bc_rl_trainer import BCTorchRLTrainer
+
+        return BCTorchRLTrainer
+    else:
+        raise ValueError(f"Unsupported framework: {framework}")
+
+
+def _get_module_class(framework: str) -> Type[RLModule]:
+    if framework == "tf":
+        from ray.rllib.core.testing.tf.bc_module import DiscreteBCTFModule
+
+        return DiscreteBCTFModule
+    elif framework == "torch":
+        from ray.rllib.core.testing.torch.bc_module import DiscreteBCTorchModule
+
+        return DiscreteBCTorchModule
+    else:
+        raise ValueError(f"Unsupported framework: {framework}")
+
+
+def _get_optimizer_default_class(framework: str) -> Type[Optimizer]:
+    if framework == "tf":
+        return tf.keras.optimizers.Adam
+    elif framework == "torch":
+        return torch.optim.Adam
+    else:
+        raise ValueError(f"Unsupported framework: {framework}")
+
+
+def _get_trainer_runner(
+    framework: str, env: gym.Env, compute_config: dict
+) -> TrainerRunner:
+    trainer_class = _get_trainer_class(framework)
+    trainer_cfg = dict(
+        module_class=_get_module_class(framework),
+        module_kwargs={
+            "observation_space": env.observation_space,
+            "action_space": env.action_space,
+            "model_config": {"hidden_dim": 32},
+        },
+        optimizer_config={"lr": 0.1},
+        in_test=True,
+    )
+    runner = TrainerRunner(trainer_class, trainer_cfg, compute_config=compute_config)
+
+    return runner
 
 
 class TestTrainerRunner(unittest.TestCase):
@@ -26,124 +85,107 @@ class TestTrainerRunner(unittest.TestCase):
 
     def test_update_multigpu(self):
         """Test training in a 2 gpu setup and that weights are synchronized."""
-        env = gym.make("CartPole-v1")
-        trainer_class = BCTfRLTrainer
-        trainer_cfg = dict(
-            module_class=DiscreteBCTFModule,
-            module_kwargs={
-                "observation_space": env.observation_space,
-                "action_space": env.action_space,
-                "model_config": {"hidden_dim": 32},
-            },
-            optimizer_config={"lr": 1e-3},
-            in_test=True,
-        )
-        runner = TrainerRunner(
-            trainer_class, trainer_cfg, compute_config=dict(num_gpus=2)
-        )
 
-        reader = get_cartpole_dataset_reader(batch_size=500)
+        for fw in ["torch"]:  # , "torch"]:
+            print(f"Testing framework: {fw}.")
+            env = gym.make("CartPole-v1")
+            runner = _get_trainer_runner(fw, env, compute_config=dict(num_gpus=2))
+            reader = get_cartpole_dataset_reader(batch_size=500)
 
-        min_loss = float("inf")
-        for iter_i in range(1000):
-            batch = reader.next()
-            results_worker_0, results_worker_1 = runner.update(batch.as_multi_agent())
+            min_loss = float("inf")
+            for iter_i in range(1000):
+                batch = reader.next()
+                res_0, res_1 = runner.update(batch.as_multi_agent())
 
-            loss = (
-                results_worker_0["loss"]["total_loss"]
-                + results_worker_1["loss"]["total_loss"]
-            ) / 2
-            min_loss = min(loss, min_loss)
-            print(f"[iter = {iter_i}] Loss: {loss:.3f}, Min Loss: {min_loss:.3f}")
-            # The loss is initially around 0.69 (ln2). When it gets to around
-            # 0.57 the return of the policy gets to around 100.
-            if min_loss < 0.57:
-                break
-            self.assertEqual(
-                results_worker_0["mean_weight"]["default_policy"],
-                results_worker_1["mean_weight"]["default_policy"],
-            )
-        self.assertLess(min_loss, 0.57)
+                loss = (res_0["loss"]["total_loss"] + res_1["loss"]["total_loss"]) / 2
+                min_loss = min(loss, min_loss)
+                print(f"[iter = {iter_i}] Loss: {loss:.3f}, Min Loss: {min_loss:.3f}")
+                # The loss is initially around 0.69 (ln2). When it gets to around
+                # 0.57 the return of the policy gets to around 100.
+                if min_loss < 0.57:
+                    break
+                self.assertEqual(
+                    res_0["mean_weight"]["default_policy"],
+                    res_1["mean_weight"]["default_policy"],
+                )
+            self.assertLess(min_loss, 0.57)
+
+            # make sure the runner resources are freed up
+            del runner
 
     def test_add_remove_module(self):
-        env = gym.make("CartPole-v1")
-        trainer_class = BCTfRLTrainer
-        trainer_cfg = dict(
-            module_class=DiscreteBCTFModule,
-            module_kwargs={
-                "observation_space": env.observation_space,
-                "action_space": env.action_space,
-                "model_config": {"hidden_dim": 32},
-            },
-            optimizer_config={"lr": 1e-3},
-            in_test=True,
-        )
-        runner = TrainerRunner(
-            trainer_class, trainer_cfg, compute_config=dict(num_gpus=2)
-        )
 
-        reader = get_cartpole_dataset_reader(batch_size=500)
-        batch = reader.next()
+        for fw in ["torch"]:  # , "torch"]:
+            print(f"Testing framework: {fw}.")
+            env = gym.make("CartPole-v1")
+            runner = _get_trainer_runner(fw, env, compute_config=dict(num_gpus=2))
+            reader = get_cartpole_dataset_reader(batch_size=500)
+            batch = reader.next()
 
-        # update once with the default policy
-        results = runner.update(batch.as_multi_agent())
-        module_ids_before_add = {DEFAULT_POLICY_ID}
-        new_module_id = "test_module"
+            # update once with the default policy
+            results = runner.update(batch.as_multi_agent())
+            module_ids_before_add = {DEFAULT_POLICY_ID}
+            new_module_id = "test_module"
 
-        # add a test_module
-        runner.add_module(
-            module_id=new_module_id,
-            module_cls=DiscreteBCTFModule,
-            module_kwargs={
-                "observation_space": env.observation_space,
-                "action_space": env.action_space,
-                "model_config": {"hidden_dim": 32},
-            },
-            optimizer_cls=tf.keras.optimizers.Adam,
-        )
-
-        # do training that includes the test_module
-        results = runner.update(
-            MultiAgentBatch(
-                {new_module_id: batch, DEFAULT_POLICY_ID: batch}, batch.count
+            # add a test_module
+            runner.add_module(
+                module_id=new_module_id,
+                module_cls=_get_module_class(fw),
+                module_kwargs={
+                    "observation_space": env.observation_space,
+                    "action_space": env.action_space,
+                    "model_config": {"hidden_dim": 32},
+                },
+                optimizer_cls=_get_optimizer_default_class(fw),
             )
-        )
 
-        # check that module weights are updated across workers and synchronized
-        for i in range(1, len(results)):
-            for module_id in results[i]["mean_weight"].keys():
-                assert (
-                    results[i]["mean_weight"][module_id]
-                    == results[i - 1]["mean_weight"][module_id]
+            # do training that includes the test_module
+            results = runner.update(
+                MultiAgentBatch(
+                    {new_module_id: batch, DEFAULT_POLICY_ID: batch}, batch.count
+                )
+            )
+
+            # check that module weights are updated across workers and synchronized
+            for i in range(1, len(results)):
+                for module_id in results[i]["mean_weight"].keys():
+                    assert (
+                        results[i]["mean_weight"][module_id]
+                        == results[i - 1]["mean_weight"][module_id]
+                    )
+
+            # check that module ids are updated to include the new module
+            module_ids_after_add = {DEFAULT_POLICY_ID, new_module_id}
+            for result in results:
+                # remove the total_loss key since its not a module key
+                self.assertEqual(
+                    set(result["loss"]) - {"total_loss"}, module_ids_after_add
                 )
 
-        # check that module ids are updated to include the new module
-        module_ids_after_add = {DEFAULT_POLICY_ID, new_module_id}
-        for result in results:
-            # remove the total_loss key since its not a module key
-            self.assertEqual(set(result["loss"]) - {"total_loss"}, module_ids_after_add)
+            # remove the test_module
+            runner.remove_module(module_id=new_module_id)
 
-        # remove the test_module
-        runner.remove_module(module_id=new_module_id)
+            # run training without the test_module
+            results = runner.update(batch.as_multi_agent())
 
-        # run training without the test_module
-        results = runner.update(batch.as_multi_agent())
+            # check that module weights are updated across workers and synchronized
+            for i in range(1, len(results)):
+                for module_id in results[i]["mean_weight"].keys():
+                    assert (
+                        results[i]["mean_weight"][module_id]
+                        == results[i - 1]["mean_weight"][module_id]
+                    )
 
-        # check that module weights are updated across workers and synchronized
-        for i in range(1, len(results)):
-            for module_id in results[i]["mean_weight"].keys():
-                assert (
-                    results[i]["mean_weight"][module_id]
-                    == results[i - 1]["mean_weight"][module_id]
+            # check that module ids are updated after remove operation to not
+            # include the new module
+            for result in results:
+                # remove the total_loss key since its not a module key
+                self.assertEqual(
+                    set(result["loss"]) - {"total_loss"}, module_ids_before_add
                 )
 
-        # check that module ids are updated after remove operation to not
-        # include the new module
-        for result in results:
-            # remove the total_loss key since its not a module key
-            self.assertEqual(
-                set(result["loss"]) - {"total_loss"}, module_ids_before_add
-            )
+            # make sure the runner resources are freed up
+            del runner
 
 
 if __name__ == "__main__":
