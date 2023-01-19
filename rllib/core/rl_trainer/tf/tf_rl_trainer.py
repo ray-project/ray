fixed the scaling config and in_test issues introduced after the merge.
Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

rllib/core/rl_trainer/tf/tf_rl_trainer.py
@@ -27,6 +27,8 @@ from ray.rllib.utils.framework import try_import_tf
 from ray.rllib.utils.typing import TensorType
 from ray.rllib.utils.nested_dict import NestedDict
 
+from ray.air.config import ScalingConfig
+
 tf1, tf, tfv = try_import_tf()
 tf1.enable_eager_execution()
 
@@ -41,7 +43,6 @@ class TfRLTrainer(RLTrainer):
         module_kwargs: The kwargs for the (MA)RLModule.
         optimizer_config: The config for the optimizer.
         distributed: Whether this trainer is distributed or not.
-        in_test: Whether to enable additional logging behavior for testing purposes.
         enable_tf_function: Whether to enable tf.function tracing for the update
             function.
 
@@ -79,7 +80,7 @@ class TfRLTrainer(RLTrainer):
 
     """
 
-    TOTAL_LOSS_KEY = "total_loss"
+    framework: str = "tf"
 
     def __init__(
         self,
@@ -87,6 +88,7 @@ class TfRLTrainer(RLTrainer):
         module_kwargs: Mapping[str, Any],
         optimizer_config: Mapping[str, Any],
         distributed: bool = False,
+        scaling_config: Optional[ScalingConfig] = None,
         enable_tf_function: bool = True,
     ):
         super().__init__(
@@ -94,6 +96,7 @@ class TfRLTrainer(RLTrainer):
             module_kwargs=module_kwargs,
             optimizer_config=optimizer_config,
             distributed=distributed,
+            scaling_config=scaling_config,
         )
 
         self._enable_tf_function = enable_tf_function

rllib/core/rl_trainer/torch/torch_rl_trainer.py
@@ -11,6 +11,7 @@ from typing import (
 )
 import torch
 
+from ray.air.config import ScalingConfig
 from ray.train.torch.train_loop_utils import _TorchAccelerator
 
 from ray.rllib.core.rl_module.rl_module import RLModule, ModuleID
@@ -32,24 +33,31 @@ logger = logging.getLogger(__name__)
 
 
 class TorchRLTrainer(RLTrainer):
+
+    framework: str = "torch"
+
     def __init__(
         self,
         module_class: Union[Type[RLModule], Type[MultiAgentRLModule]],
         module_kwargs: Mapping[str, Any],
-        scaling_config: Mapping[str, Any],
         optimizer_config: Mapping[str, Any],
         distributed: bool = False,
+        scaling_config: Optional[Mapping[str, Any]] = None,
     ):
         super().__init__(
             module_class=module_class,
             module_kwargs=module_kwargs,
-            scaling_config=scaling_config,
             optimizer_config=optimizer_config,
             distributed=distributed,
+            scaling_config=scaling_config,
         )
 
-        self._world_size = scaling_config.get("num_workers", 1)
-        self._use_gpu = scaling_config.get("use_gpu", False)
+        # TODO (Kourosh): Scaling config is required for torch trainer to do proper DDP
+        # wraping setup but not so much required for tf. we need to
+        scaling_config = scaling_config or ScalingConfig()
+        self._world_size = scaling_config.num_workers or 1
+        self._use_gpu = scaling_config.use_gpu
+
         # These attributes are set in the `build` method.
         self._device = None
 