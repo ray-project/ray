794ce274eced9901a552353518a7181135c55acc
Kai Fricke <kai@anyscale.com>
2023-01-19 16:13:01 -0800

    [tune] Improve docstring for `tune.grid_search()`
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- python/ray/tune/search/variant_generator.py
+++ python/ray/tune/search/variant_generator.py
@@ -59,11 +59,38 @@ def generate_variants(
 
 
 @PublicAPI(stability="beta")
-def grid_search(values: Iterable) -> Dict[str, List]:
-    """Convenience method for specifying grid search over a value.
+def grid_search(values: Iterable) -> Dict[str, Iterable]:
+    """Specify a grid of values to search over.
 
-    Arguments:
-        values: An iterable whose parameters will be gridded.
+    Values specified in a grid search are guaranteed to be sampled.
+
+    If multiple grid search variables are defined, they are combined with the
+    combinatorial product. This means every possible combination of values will
+    be sampled.
+
+    Example:
+
+        >>> from ray import tune
+        >>> param_space={
+        ...   "x": tune.grid_search([10, 20]),
+        ...   "y": tune.grid_search["a", "b", "c"])
+        ... }
+
+        This will create a grid of 6 samples:
+        ``{"x": 10, "y": "a"}, {"x": 10, "y": "b"}, `` etc.
+
+    When specifying ``num_samples`` in the
+    :class:`TuneConfig <ray.tune.tune_config.TuneConfig>` in addition to
+    grid search parameters, the number of samples will be drawn for each grid
+    parameter.
+
+    For instance, in the example above, if ``num_samples=4``,
+    a total of 24 trials will be started -
+    4 trials for each of the 6 grid search combinations.
+
+    Args:
+        values: An iterable whose parameters will be used for creating a trial grid.
+
     """
     return {"grid_search": values}
 
