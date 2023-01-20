dfa04c3f7133787deae11e7d39493351b3b5fbf0
Kai Fricke <krfricke@users.noreply.github.com>
2023-01-20 11:37:31 -0800

    [air/execution] Actor+future manager 1: Abstractions (#31236)
    This PR adds the abstractions for an event-based manager around Ray core primitives.
    
    The RayEventManager will be used by the AIR libraries to start and stop actors and to schedule futures on them.
    Instead of imperatively resolving the futures manually using `ray.wait` and `ray.get`, we define callbacks that are invoked when specific event happen. In particular, we will be able to specify callbacks for the following events:
    
    - An actor started
    - An actor terminated (gracefully_
    - An actor failed
    - An actor task resolved successfully
    - An actor task failed
    
    This event processing matches the patterns we currently implement in our libraries. By abstracting this into a separate management class, we remove the Ray core primitive lifecycle management from our library code. This leads to better testability and maintainability of our code base, and it will make it easier for us to extend our functionality in the future.
    
    
    Signed-off-by: Kai Fricke <kai@anyscale.com>

---
+++ python/ray/air/execution/_internal/tracked_actor.py
@@ -0,0 +1,37 @@
+from typing import Callable
+
+
+class TrackedActor:
+    """Actor tracked by an actor manager.
+
+    This object is used to reference a Ray actor on an actor manager
+
+    Existence of this object does not mean that the Ray actor has already been started.
+    Actor state can be inquired from the actor manager tracking the Ray actor.
+
+    Note:
+        Objects of this class are returned by the :class:`RayEventManager`.
+        This class should not be instantiated manually.
+
+    Attributes:
+        actor_id: ID for identification of the actor within the actor manager. This
+            ID is not related to the Ray actor ID.
+
+    """
+
+    def __init__(self, actor_id: int):
+        self.actor_id = actor_id
+
+    def on_start(self, callback: Callable[["TrackedActor"], None]) -> "TrackedActor":
+        """Set callback to invoke when actor started."""
+        raise NotImplementedError
+
+    def on_stop(self, callback: Callable[["TrackedActor"], None]) -> "TrackedActor":
+        """Set callback to invoke when actor stopped gracefully."""
+        raise NotImplementedError
+
+    def on_error(
+        self, callback: Callable[["TrackedActor", Exception], None]
+    ) -> "TrackedActor":
+        """Set callback to invoke when actor died."""
+        raise NotImplementedError
