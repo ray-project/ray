819517139ed7204dbf0548bbd5a1eeabe617e284
Cindy Zhang <cindyzyx9@gmail.com>
2023-01-19 17:32:16 -0800

    [serve] Change Gradio integration to take a builder function to avoid serialization issues (#31619)
    New versions of Gradio make Gradio objects un-serializable in Ray. This makes it impossible to directly pass in a Gradio object to `GradioIngress`, so to avoid this the API is changed to accept a builder function that constructs the Gradio object on the Ray cluster.
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- doc/source/serve/doc_code/gradio-original.py
+++ doc/source/serve/doc_code/gradio-original.py
@@ -1,5 +1,6 @@
 import gradio as gr
 from transformers import pipeline
+import requests
 
 # __doc_code_begin__
 generator1 = pipeline("text-generation", model="gpt2")
@@ -21,5 +22,12 @@ def model2(text):
 demo = gr.Interface(
     lambda text: f"{model1(text)}\n------------\n{model2(text)}", "textbox", "textbox"
 )
-demo.launch()
 # __doc_code_end__
+
+# Test example code
+demo.launch(prevent_thread_lock=True)
+response = requests.post(
+    "http://127.0.0.1:7860/api/predict/", json={"data": ["My name is Lewis"]}
+)
+assert response.status_code == 200
+print("gradio-original.py: Response from example code is", response.json()["data"])
