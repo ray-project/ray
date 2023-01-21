843f1b66f723c8a863310c1958c59045f72733f5
Chen Shen <scv119@gmail.com>
2023-01-20 22:22:30 -0800

    [Core] allow worker lazily bind to job_id (#30883)
    Why are these changes needed?
    This PR enables worker lazy binding to job_ids. This enables us to pre-start job agnostic cpu workers in the follow up PRs.
    The main changes in this PR:
    
    change the worker side context (WorkerContext) to allow lazy binding to job_id.
    update the raylet side context (Worker) to allow lazy binding to job_id.
    change worker_pool to prestart workers without id specified.
    it also fixed a bug to where the runtime_env_hash is not set when prestart workers.
    
    Signed-off-by: Chen Shen <scv119@gmail.com>
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>
    Co-authored-by: Antoni Baum <antoni.baum@protonmail.com>
    Signed-off-by: Andrea Pisoni <andreapiso@gmail.com>

--- python/ray/tests/test_advanced.py
+++ python/ray/tests/test_advanced.py
@@ -193,7 +193,9 @@ def test_caching_functions_to_run(shutdown_only):
     ray._private.worker.global_worker.run_function_on_all_workers(f)
 
 
-@pytest.mark.skipif(client_test_enabled(), reason="internal api")
+@pytest.mark.skipif(
+    client_test_enabled() or sys.platform == "win32", reason="internal api"
+)
 def test_running_function_on_all_workers(ray_start_regular):
     def f(worker_info):
         sys.path.append("fake_directory")
