ffdf6800f4642b132c475fa94f41e8044c91393d
Alex Wu <alex@anyscale.io>
2023-01-23 11:54:02 -0800

    Revert "[AIR] Deprecations for 2.3 (#31763)" (#31866)
    This reverts commit 91b632b809a1ebb5e5f314e55b673e82b229433e.
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- python/ray/train/__init__.py
+++ python/ray/train/__init__.py
@@ -1,6 +1,15 @@
 from ray._private.usage import usage_lib
 from ray.train.backend import BackendConfig
 from ray.train.constants import TRAIN_DATASET_KEY
+from ray.train.train_loop_utils import (
+    get_dataset_shard,
+    load_checkpoint,
+    local_rank,
+    report,
+    save_checkpoint,
+    world_rank,
+    world_size,
+)
 from ray.train.trainer import TrainingIterator
 
 
@@ -8,6 +17,13 @@ usage_lib.record_library_usage("train")
 
 __all__ = [
     "BackendConfig",
+    "get_dataset_shard",
+    "load_checkpoint",
+    "local_rank",
+    "report",
+    "save_checkpoint",
     "TrainingIterator",
+    "world_rank",
+    "world_size",
     "TRAIN_DATASET_KEY",
 ]
