872388e56ca32239c567e1cd4c0281853f1fbd60
scv119 <scv119@gmail.com>
2023-01-23 14:51:52 -0800

    add

--- ci/env/install-core-prerelease-dependencies.sh
+++ ci/env/install-core-prerelease-dependencies.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install all unbounded dependencies in setup.py for ray core
-for dependency in attrs jsonschema aiosignal frozenlist requests grpcio protobuf
+for dependency in attrs jsonschema aiosignal frozenlist requests protobuf #grpcio
 do
     python -m pip install -U --pre --upgrade-strategy=eager $dependency
 done
