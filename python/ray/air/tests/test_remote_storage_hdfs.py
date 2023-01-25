d73ccccb4315fc8f9d323c93610afcd7ae4af5b5
xwjiang2010 <xwjiang2010@gmail.com>
2023-01-25 15:16:51 -0800

    [air] Add test for remote_storage with real hdfs backend.
    Signed-off-by: xwjiang2010 <xwjiang2010@gmail.com>

---
+++ python/ray/air/tests/test_remote_storage_hdfs.py
@@ -0,0 +1,21 @@
+"""Test remote_storage in a ci environment with real hdfs setup."""
+import os
+
+import pytest
+
+from ray.air._internal.remote_storage import get_fs_and_path
+
+
+def test_get_fs_and_path_hdfs():
+    hostname = os.getenv("HOSTNAME")
+    hdfs_uri = f"hdfs://{hostname}:8020/test/"
+    # do it twice should yield the same result
+    _, path = get_fs_and_path(hdfs_uri)
+    _, cached_path = get_fs_and_path(hdfs_uri)
+    assert path == cached_path
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(pytest.main(["-v", __file__]))
