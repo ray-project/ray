c86083813b65eb683d4ba7119a55442ac062de12
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2023-01-25 13:22:36 -0800

    Lint
    Signed-off-by: Archit Kulkarni <architkulkarni@users.noreply.github.com>

--- src/ray/gcs/gcs_server/gcs_job_manager.cc
+++ src/ray/gcs/gcs_server/gcs_job_manager.cc
@@ -153,29 +153,34 @@ void GcsJobManager::HandleGetAllJobInfo(rpc::GetAllJobInfoRequest request,
       reply->add_job_info_list()->CopyFrom(data.second);
     }
 
-    std::shared_ptr<std::atomic<int>> num_processed_jobs = std::make_shared<std::atomic<int>>(0);
-    
-    // For each job with a submission id, we query the internal KV store to get the Ray Job API
-    // JobInfo, and send the gRPC reply in the KV callback. If there are *no* jobs with a submission
-    // id, we must send the gRPC reply in the current function instead. Use this flag to track it.
+    std::shared_ptr<std::atomic<int>> num_processed_jobs =
+        std::make_shared<std::atomic<int>>(0);
+
+    // For each job with a submission id, we query the internal KV store to get the Ray
+    // Job API JobInfo, and send the gRPC reply in the KV callback. If there are *no* jobs
+    // with a submission id, we must send the gRPC reply in the current function instead.
+    // Use this flag to track it.
     bool any_jobs_with_submission_id = false;
 
     for (int i = 0; i < reply->job_info_list_size(); i++) {
-      const auto& metadata = reply->job_info_list(i).config().metadata();
-      if(metadata.find("job_submission_id") == metadata.end()) {
+      const auto &metadata = reply->job_info_list(i).config().metadata();
+      if (metadata.find("job_submission_id") == metadata.end()) {
         num_processed_jobs->fetch_add(1);
         continue;
-
       }
-      const auto& job_submission_id = metadata.at("job_submission_id");
+      const auto &job_submission_id = metadata.at("job_submission_id");
       any_jobs_with_submission_id = true;
-      auto kv_get_callback = [reply, send_reply_callback, num_processed_jobs](std::optional<std::string> job_info_json) {
+      auto kv_get_callback = [reply, send_reply_callback, num_processed_jobs](
+                                 std::optional<std::string> job_info_json) {
         if (job_info_json) {
           rpc::JobsAPIInfo jobs_api_info;
-          RAY_CHECK(google::protobuf::util::JsonStringToMessage(*job_info_json, &jobs_api_info).ok());
-        }
-        else {
-          RAY_LOG(ERROR) << "Failed to look up Ray Job API JobInfo for job with submission id " << job_submission_id;
+          RAY_CHECK(
+              google::protobuf::util::JsonStringToMessage(*job_info_json, &jobs_api_info)
+                  .ok());
+        } else {
+          RAY_LOG(ERROR)
+              << "Failed to look up Ray Job API JobInfo for job with submission id "
+              << job_submission_id;
         }
 
         // Send reply if all jobs have been processed.
