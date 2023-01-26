3257cd340e97daf139dac0b340c5704194df1bd5
Weichen Xu <weichen.xu@databricks.com>
2023-01-26 21:51:57 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- python/ray/util/spark/__init__.py
+++ python/ray/util/spark/__init__.py
@@ -3,5 +3,7 @@ from ray.util.spark.cluster_init import (
     shutdown_ray_cluster,
     MAX_NUM_WORKER_NODES,
 )
+from ray.util.spark.data import load_spark_dataset
 
-__all__ = ["setup_ray_cluster", "shutdown_ray_cluster", "MAX_NUM_WORKER_NODES"]
+__all__ = ["setup_ray_cluster", "shutdown_ray_cluster", "MAX_NUM_WORKER_NODES",
+           "load_spark_dataset"]
