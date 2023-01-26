f44b74c7a45591643b00532c1eb60d669941f4e1
feefs <feefs@berkeley.edu>
2023-01-25 22:30:45 -0800

    [Datasets] Fix Dataset inside DatasetIterator
    Signed-off-by: feefs <feefs@berkeley.edu>

--- python/ray/data/dataset_iterator.py
+++ python/ray/data/dataset_iterator.py
@@ -37,7 +37,7 @@ class DatasetIterator(abc.ABC):
         >>> ds
         <Dataset num_blocks=5, num_rows=5, schema=<class 'int'>>
         >>> ds.iterator()
-        DatasetIterator(<Dataset num_blocks=5, num_rows=5, schema=<class 'int'>)>
+        DatasetIterator(<Dataset num_blocks=5, num_rows=5, schema=<class 'int'>>)
         >>> ds = ds.repeat(); ds
         DatasetPipeline(num_windows=inf, num_stages=2)
         >>> ds.iterator()
@@ -180,7 +180,7 @@ class DatasetIterator(abc.ABC):
             ...     "s3://anonymous@air-example-data/iris.csv"
             ... )
             >>> it = ds.iterator(); it
-            DatasetIterator(<Dataset num_blocks=1, num_rows=150, schema={sepal length (cm): double, sepal width (cm): double, petal length (cm): double, petal width (cm): double, target: int64})>
+            DatasetIterator(<Dataset num_blocks=1, num_rows=150, schema={sepal length (cm): double, sepal width (cm): double, petal length (cm): double, petal width (cm): double, target: int64}>)
 
             If your model accepts a single tensor as input, specify a single feature column.
 
@@ -200,7 +200,7 @@ class DatasetIterator(abc.ABC):
             >>> preprocessor = Concatenator(output_column_name="features", exclude="target")
             >>> it = preprocessor.transform(ds).iterator()
             >>> it
-            DatasetIterator(<Dataset num_blocks=1, num_rows=150, schema={target: int64, features: TensorDtype(shape=(4,), dtype=float64)})>
+            DatasetIterator(<Dataset num_blocks=1, num_rows=150, schema={target: int64, features: TensorDtype(shape=(4,), dtype=float64)}>)
             >>> it.to_tf("features", "target")  # doctest: +SKIP
             <_OptionsDataset element_spec=(TensorSpec(shape=(None, 4), dtype=tf.float64, name='features'), TensorSpec(shape=(None,), dtype=tf.int64, name='target'))>
 
