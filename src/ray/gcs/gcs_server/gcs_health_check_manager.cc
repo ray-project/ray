ee370a308496d788abd2c5428f28ac4ab37bc70c
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2023-01-26 00:44:11 +0000

    format Signed-off-by: Yi Cheng <74173148+iycheng@users.noreply.github.com>

--- src/ray/gcs/gcs_server/gcs_health_check_manager.cc
+++ src/ray/gcs/gcs_server/gcs_health_check_manager.cc
@@ -62,7 +62,7 @@ void GcsHealthCheckManager::RemoveNode(const NodeID &node_id) {
 void GcsHealthCheckManager::FailNode(const NodeID &node_id) {
   RAY_LOG(WARNING) << "Node " << node_id << " is dead because the health check failed.";
   auto iter = health_check_contexts_.find(node_id);
-  if(iter != health_check_contexts_.end()) {
+  if (iter != health_check_contexts_.end()) {
     on_node_death_callback_(node_id);
     health_check_contexts_.erase(iter);
   }
