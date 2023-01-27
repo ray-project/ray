5dd8d3bf5728f72f806681cbe85edfb588e5ec77
Kai Fricke <kai@anyscale.com>
2023-01-27 13:39:20 -0800

    Event manager abstraction
    Signed-off-by: Kai Fricke <kai@anyscale.com>

---
+++ python/ray/air/execution/_internal/event_manager.py
@@ -0,0 +1,68 @@
+import random
+
+import ray
+from typing import Any, Callable, Dict, Iterable, Optional, Set, Tuple, Union
+
+_ResultCallback = Callable[[Any], None]
+_ErrorCallback = Callable[[Exception], None]
+
+
+class RayEventManager:
+    def __init__(self, shuffle_futures: bool = True):
+        self._shuffle_futures = shuffle_futures
+
+        # Map of futures to callbacks (result, error)
+        self._tracked_futures: Dict[
+            ray.ObjectRef, Tuple[Optional[_ResultCallback], Optional[_ErrorCallback]]
+        ] = {}
+
+    def track_future(
+        self,
+        future: ray.ObjectRef,
+        on_result: Optional[_ResultCallback],
+        on_error: Optional[_ErrorCallback],
+    ):
+        self._tracked_futures[future] = (on_result, on_error)
+
+    def track_futures(
+        self,
+        futures: Iterable[ray.ObjectRef],
+        on_result: Optional[_ResultCallback],
+        on_error: Optional[_ErrorCallback],
+    ):
+        for future in futures:
+            self.track_future(future, on_result=on_result, on_error=on_error)
+
+    def get_futures(self) -> Set[ray.ObjectRef]:
+        return set(self._tracked_futures)
+
+    def resolve_future(self, future: ray.ObjectRef):
+        try:
+            on_result, on_error = self._tracked_futures.pop(future)
+        except KeyError as e:
+            raise ValueError(
+                f"Future {future} is not tracked by this RayEventManager"
+            ) from e
+
+        try:
+            result = ray.get(future)
+        except Exception as e:
+            on_error(e)
+        else:
+            on_result(result)
+
+    def wait(
+        self,
+        timeout: Optional[Union[float, int]] = None,
+        num_results: Optional[int] = None,
+    ):
+        futures = list(self.get_futures())
+
+        if self._shuffle_futures:
+            random.shuffle(futures)
+
+        num_results = num_results or len(futures)
+
+        ready, _ = ray.wait(list(futures), timeout=timeout, num_returns=num_results)
+        for future in ready:
+            self.resolve_future(future)
