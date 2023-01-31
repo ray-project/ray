61ca61edc2c5041c00aae2efbb0041fb2368e2f8
Cheng Su <scnju13@gmail.com>
2023-01-31 11:18:44 -0800

    [Datasets] Add logical operator for repartition() (#32102)
    This PR adds logical operator for `repartition()`. Only implement shuffle repartition (`repartition.py:generate_repartition_fn()`).
    
    Non-shuffle repartition is left as TODO, as the corresponding code in [fast_repartition.py](https://github.com/ray-project/ray/blob/master/python/ray/data/_internal/fast_repartition.py) involves `BlockList`, `ExecutionPlan` and `Dataset.split()`, so it needs a deeper refactoring and code change.
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- python/ray/data/_internal/planner/plan_all_to_all_op.py
+++ python/ray/data/_internal/planner/plan_all_to_all_op.py
@@ -4,9 +4,11 @@ from ray.data._internal.logical.operators.all_to_all_operator import (
     AbstractAllToAll,
     RandomShuffle,
     RandomizeBlocks,
+    Repartition,
 )
 from ray.data._internal.planner.random_shuffle import generate_random_shuffle_fn
 from ray.data._internal.planner.randomize_blocks import generate_randomize_blocks_fn
+from ray.data._internal.planner.repartition import generate_repartition_fn
 
 
 def _plan_all_to_all_op(
@@ -22,6 +24,8 @@ def _plan_all_to_all_op(
         fn = generate_randomize_blocks_fn(op._seed)
     elif isinstance(op, RandomShuffle):
         fn = generate_random_shuffle_fn(op._seed, op._num_outputs)
+    elif isinstance(op, Repartition):
+        fn = generate_repartition_fn(op._num_outputs, op._shuffle)
     else:
         raise ValueError(f"Found unknown logical operator during planning: {op}")
 
