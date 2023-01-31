5bd50fab9b7e22cabdd53e73c92925f17d3c3f12
Justin Coffi <jcoffi+github@gmail.com>
2023-01-31 09:57:24 -0800

    fixed a typo at line 87

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -84,7 +84,7 @@ while [ ! $status = "Running" ]
 done
 
 echo "net.ipv6.conf.all.disable_ipv6=1" | sudo tee -a /etc/sysctl.conf
-echo "net.ipv6.conf.default.disable_ipv6=1" | sudo -a tee /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6=1" | sudo tee -a /etc/sysctl.conf
 echo "net.ipv6.conf.lo.disable_ipv6=1" | sudo tee -a /etc/sysctl.conf
 echo "vm.max_map_count = 262144" | sudo tee -a /etc/sysctl.conf
 
