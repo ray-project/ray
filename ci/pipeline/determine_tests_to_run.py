869f8d082ef772179b3b7ef08165fc4bc68d8724
Clark Zinzow <clarkzinzow@gmail.com>
2023-01-31 16:21:59 -0800

    [CI] [Datasets] Run Datasets test suites on AIR changes (#32118)
    Datasets depends on ray.air for several key features (tensor extensions, Arrow transformations, data batch conversions), and not running the Datasets test suite in PR builds on ray.air changes has caused breaks to go undetected. This PR changes this so when files under python/ray/air change, we trigger the Datasets test suite in CI.
    
    Signed-off-by: Clark Zinzow <clarkzinzow@gmail.com>
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- ci/pipeline/determine_tests_to_run.py
+++ ci/pipeline/determine_tests_to_run.py
@@ -148,6 +148,7 @@ if __name__ == "__main__":
                 RAY_CI_TRAIN_AFFECTED = 1
                 RAY_CI_TUNE_AFFECTED = 1
                 RAY_CI_RLLIB_AFFECTED = 1
+                RAY_CI_DATA_AFFECTED = 1
                 RAY_CI_LINUX_WHEELS_AFFECTED = 1
                 RAY_CI_MACOS_WHEELS_AFFECTED = 1
             elif changed_file.startswith("python/ray/data"):
