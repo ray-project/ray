b4da7aeaad6ec0b17752fb1ee920ac41f39a8959
Justin Coffi <jcoffi+github@gmail.com>
2023-02-01 14:37:25 -0800

    guh3

--- docker/anywhere-tailscale/buildfiles/getstartup.sh
+++ docker/anywhere-tailscale/buildfiles/getstartup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -fSL -O "https://raw.githubusercontent.com/jcoffi/cluster-anywhere/master/docker/anywhere-tailscale/startup.sh" /home/ray/startup.sh
+curl -fSL -O "https://raw.githubusercontent.com/jcoffi/cluster-anywhere/master/docker/anywhere-tailscale/startup.sh"
 sudo chmod +x /home/ray/startup.sh
-curl -fSL -O "https://raw.githubusercontent.com/jcoffi/cluster-anywhere/master/docker/anywhere-tailscale/run_tests.sh" /home/ray/run_tests.sh
+curl -fSL -O "https://raw.githubusercontent.com/jcoffi/cluster-anywhere/master/docker/anywhere-tailscale/run_tests.sh"
 sudo chmod +x /home/ray/run_tests.sh
-./home/ray/run_tests.sh
+/home/ray/run_tests.sh
