cc556adcc1f7499f233d9ba020e791f27648ebf0
Chen Shen <scv119@gmail.com>
2023-01-31 20:37:23 -0800

    [Core] Pick node from top k by default. (#31868)
    This PR takes over #28179
    
    Why are these changes needed?
    Today with the default scheduling policy, Ray will try to pack tasks on nodes until the resource utilization is beyond a certain threshold and spread tasks afterwards.
    This has caused slow down the scheduling speed for embarrassingly parallel jobs:
    
    we will only move on to another node before the current node's resource if sufficiently utilized,
    for each node, the overhead of accepting new job and starting of a new workers is not negligible.
    the overall scheduling speed doesn't scale with the number of nodes;
    This PR is one proposal to address the problem: instead of stick to one node, we randomly choose one node from top-k nodes for the default scheduling, where the node is sorted by it's resource utilization in reverse order.
    
    Intuitively, this allows us to kick off the workers startup on multiple node in parallel of the scheduling.
    
    benchmark result:
    
    baseline: 10 parallelism, top 1, 25 tasks/second
    10 parallelism, top 6, 30 tasks/second
    64 parallelism, top 6, 126
    100 parallelism, top 6, 150
    1000 parallelism, top 6, 374.8676886257549
    10 concurrent, top 12, 176
    64 concurrent, top 12, 182.59477988042443 tasks/s
    128 concurrent, top 12, 245.9862948998163
    256 concurrent, top 12, 298…
    
    Signed-off-by: Shomil Jain <shomil@anyscale.com>

--- src/ray/common/ray_config_def.h
+++ src/ray/common/ray_config_def.h
@@ -191,6 +191,17 @@ RAY_CONFIG(int64_t, worker_cap_max_backoff_delay_ms, 1000 * 10)
 /// even balancing of load. Low values (min 0.0) encourage more load spreading.
 RAY_CONFIG(float, scheduler_spread_threshold, 0.5)
 
+/// Used by the default hybrid policy only. The scheduler will randomly pick
+/// one node from the top k in the cluster to improve load balancing. The
+/// scheduler guarantees k is at least equal to this fraction * the number of
+/// nodes in the cluster.
+RAY_CONFIG(float, scheduler_top_k_fraction, 0.2);
+
+/// Used by the default hybrid policy only. The scheduler will randomly pick
+/// one node from the top k in the cluster to improve load balancing. The
+/// scheduler guarantees k is at least equal to scheduler_top_k_absolute.
+RAY_CONFIG(int32_t, scheduler_top_k_absolute, 1);
+
 /// Whether to only report the usage of pinned copies of objects in the
 /// object_store_memory resource. This means nodes holding secondary copies only
 /// will become eligible for removal in the autoscaler.
