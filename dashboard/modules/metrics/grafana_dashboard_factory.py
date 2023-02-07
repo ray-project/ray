01113387e939fbe357946a564218eaa9a3847205
Alan Guo <aguo@anyscale.com>
2023-02-06 17:30:57 -0800

    Add ray version as a tag
    Signed-off-by: Alan Guo <aguo@anyscale.com>

--- dashboard/modules/metrics/grafana_dashboard_factory.py
+++ dashboard/modules/metrics/grafana_dashboard_factory.py
@@ -4,8 +4,9 @@ import copy
 import json
 import os
 from dataclasses import dataclass
-
 from typing import List, Dict, Optional
+
+import ray
 
 
 @dataclass
@@ -532,6 +533,9 @@ def generate_grafana_dashboard(override_uid: Optional[str] = None) -> str:
         open(os.path.join(os.path.dirname(__file__), "grafana_dashboard_base.json"))
     )
     base_json["panels"] = _generate_grafana_panels()
+    tags = base_json.get("tags", []) or []
+    tags.append(f"rayVersion:{ray.__version__}")
+    base_json["tags"] = tags
     if override_uid:
         base_json["uid"] = override_uid
     return json.dumps(base_json, indent=4)
