bcdab01f7b1775017fb6387da0e59de1aa185c4e
Alan Guo <aguo@anyscale.com>
2023-02-07 14:23:26 -0800

    Fix overview page to work with the new DASHBOARD_UID env var (#32279)
    In #32255 , i added a new env var to customize grafana dashboard uid. I forgot to use this var in the overview page.
    I also made the "View in Grafana" button take the user directly to the dashboard instead of the homepage of Grafana.
    
    Signed-off-by: Alan Guo aguo@anyscale.com
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- dashboard/client/src/pages/overview/cards/NodeCountCard.tsx
+++ dashboard/client/src/pages/overview/cards/NodeCountCard.tsx
@@ -38,10 +38,13 @@ type NodeCountCardProps = {
 export const NodeCountCard = ({ className }: NodeCountCardProps) => {
   const classes = useStyles();
 
-  const { grafanaHost, prometheusHealth, sessionName } =
-    useContext(GlobalContext);
-  const path =
-    "/d-solo/rayDefaultDashboard/default-dashboard?orgId=1&theme=light&panelId=24";
+  const {
+    grafanaHost,
+    prometheusHealth,
+    sessionName,
+    grafanaDefaultDashboardUid = "rayDefaultDashboard",
+  } = useContext(GlobalContext);
+  const path = `/d-solo/${grafanaDefaultDashboardUid}/default-dashboard?orgId=1&theme=light&panelId=24`;
   const timeRangeParams = "&from=now-30m&to=now";
 
   return (
