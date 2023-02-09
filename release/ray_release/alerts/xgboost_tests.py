b6d2539e1dbd76aa150054a74215aae7e9d847ae
xwjiang2010 <xwjiang2010@gmail.com>
2023-02-08 16:50:24 -0800

    Revert "format"
    This reverts commit 3140401b52d1f76e21e8d16c52aeabe407b3274d.

--- release/ray_release/alerts/xgboost_tests.py
+++ release/ray_release/alerts/xgboost_tests.py
@@ -6,11 +6,7 @@ from ray_release.result import Result
 
 def require_result(test: Test):
     test_name = test["legacy"]["test_name"]
-    return test_name not in [
-        "distributed_api_test",
-        "ft_small_elastic",
-        "ft_small_nonelastic",
-    ]
+    return test_name not in ["distributed_api_test", "ft_small_elastic", "ft_small_nonelastic"]
 
 
 def handle_result(
