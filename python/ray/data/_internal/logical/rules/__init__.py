edb58e43fd30022375c24a409a07eb7b50de8e2f
amogkam <amogkamsetty@yahoo.com>
2023-02-09 11:37:39 -0800

    fix
    Signed-off-by: amogkam <amogkamsetty@yahoo.com>

--- python/ray/data/_internal/logical/rules/__init__.py
+++ python/ray/data/_internal/logical/rules/__init__.py
@@ -1,4 +1,4 @@
-from ray.data._internal.logical.rules.randomize_blocks import RandomizeBlockOrderRule
+from ray.data._internal.logical.rules.randomize_blocks import ReorderRandomizeBlocksRule
 from ray.data._internal.logical.rules.operator_fusion import OperatorFusionRule
 
-__all__ = ["RandomizeBlockOrderRule", "OperatorFusionRule"]
+__all__ = ["ReorderRandomizeBlocksRule", "OperatorFusionRule"]
