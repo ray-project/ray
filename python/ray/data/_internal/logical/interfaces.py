1bfd22b230e66b9c161bdff66d80f248d140cad6
amogkam <amogkamsetty@yahoo.com>
2023-02-09 11:41:53 -0800

    fix
    Signed-off-by: amogkam <amogkamsetty@yahoo.com>

--- python/ray/data/_internal/logical/interfaces.py
+++ python/ray/data/_internal/logical/interfaces.py
@@ -31,7 +31,7 @@ class Operator:
     def post_order_iter(self) -> Iterator["Operator"]:
         """Depth-first traversal of this operator and its input dependencies."""
         for op in self.input_dependencies:
-            yield from op
+            yield from op.post_order_iter()
         yield self
 
     def __repr__(self) -> str:
