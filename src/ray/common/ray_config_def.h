ab779591693e09b2914cb9173669ffa0f34e3870
rickyyx <rickyx@anyscale.com>
2023-02-10 01:56:52 +0000

    desc
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- src/ray/common/ray_config_def.h
+++ src/ray/common/ray_config_def.h
@@ -457,9 +457,8 @@ RAY_CONFIG(uint64_t, gcs_service_address_check_interval_milliseconds, 1000)
 /// The batch size for metrics export.
 RAY_CONFIG(int64_t, metrics_report_batch_size, 100)
 
-/// The interval duration for which task state events will be reported to GCS.
-/// The reported data should only be used for observability.
-/// Setting the value to 0 disables the task event recording and reporting.
+/// If worker should be reporting metrics related to task events.
+/// Default to false to prevent too high of metric cardinality.
 RAY_CONFIG(int64_t, task_events_worker_report_metrics, false)
 
 /// The interval duration for which task state events will be reported to GCS.
