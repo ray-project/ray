edbeaae7444f5a91c8cf8579c1addd369e1a7c19
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2023-02-14 13:53:20 -0800

    wip
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- rllib/examples/learner/multi_agent_cartpole_ppo.py
+++ rllib/examples/learner/multi_agent_cartpole_ppo.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
         .training(num_sgd_iter=10)
         .multi_agent(policies=policies, policy_mapping_fn=policy_mapping_fn)
         .rl_module(_enable_rl_module_api=True)
-        .training(_enable_learneri=True)
+        .training(_enable_learner_api=True)
         .resources(**scaling_config)
     )
 
