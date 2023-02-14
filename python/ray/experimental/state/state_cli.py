a3ed435041153f295ea36bc6bd2cf624f6c6a433
Jack He <jackhe2345@gmail.com>
2023-02-13 17:54:18 -0800

    remove redundant check
    Signed-off-by: Jack He <jackhe2345@gmail.com>

--- python/ray/experimental/state/state_cli.py
+++ python/ray/experimental/state/state_cli.py
@@ -174,12 +174,7 @@ def output_with_format(
         return get_table_output(state_data, schema)
     if format == AvailableFormat.YAML:
         augmented_state_data = [
-            {
-                ("task_id: " + state["task_id"])
-                if "task_id" in state
-                else "No task_id found": state
-            }
-            for state in state_data
+            {("task_id: " + state["task_id"]): state} for state in state_data
         ]
         return yaml.dump(augmented_state_data, indent=4, explicit_start=True)
     elif format == AvailableFormat.JSON:
