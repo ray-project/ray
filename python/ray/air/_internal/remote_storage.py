fd9c19e24e01ec39d829e036ae0d150640050a06
Justin Yu <justinvyu@anyscale.com>
2023-02-14 12:58:54 -0800

    [Tune] Update trainable `remote_checkpoint_dir` upon actor reuse (#32420)
    This PR fixes trainable actor reuse to update the remote trial directory that it's writing checkpoints to.
    
    Signed-off-by: Justin Yu <justinvyu@berkeley.edu>
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/air/_internal/remote_storage.py
+++ python/ray/air/_internal/remote_storage.py
@@ -1,6 +1,7 @@
 import fnmatch
 import os
 import urllib.parse
+from pathlib import Path
 from pkg_resources import packaging
 from typing import List, Optional, Tuple
 
@@ -281,6 +282,7 @@ def _upload_to_uri_with_exclude(
             full_source_path = os.path.normpath(os.path.join(local_path, candidate))
             full_target_path = os.path.normpath(os.path.join(bucket_path, candidate))
 
+            _ensure_directory(str(Path(full_target_path).parent))
             _pyarrow_fs_copy_files(
                 full_source_path, full_target_path, destination_filesystem=fs
             )
