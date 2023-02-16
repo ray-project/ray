e7cc604bdf795d34fde0d68804afaa005d763ff7
Amog Kamsetty <amogkam@users.noreply.github.com>
2023-02-16 15:42:59 -0800

    [Data] Fix `PipelineIterator.to_tf` (#32530)
    We move to_tf logic to DatasetIterator, leveraging the respective iter_batches implementations for BulkDatasetIterator and PipelineDatasetIterator.
    
    Signed-off-by: Balaji Veeramani <balaji@anyscale.com>
    Signed-off-by: amogkam <amogkamsetty@yahoo.com>
    Co-authored-by: Balaji Veeramani <balaji@anyscale.com>
    Signed-off-by: elliottower <elliot@elliottower.com>

--- python/ray/data/_internal/bulk_dataset_iterator.py
+++ python/ray/data/_internal/bulk_dataset_iterator.py
@@ -1,10 +1,9 @@
-from typing import TYPE_CHECKING, Dict, List, Optional, Union, Iterator
+from typing import TYPE_CHECKING, Dict, Optional, Union, Iterator
 
 from ray.data.block import DataBatch
 from ray.data.dataset_iterator import DatasetIterator
 
 if TYPE_CHECKING:
-    import tensorflow as tf
     import torch
     from ray.data import Dataset
     from ray.data._internal.torch_iterable_dataset import TorchTensorBatchType
@@ -58,27 +57,6 @@ class BulkDatasetIterator(DatasetIterator):
             batch_size=batch_size,
             dtypes=dtypes,
             device=device,
-            drop_last=drop_last,
-            local_shuffle_buffer_size=local_shuffle_buffer_size,
-            local_shuffle_seed=local_shuffle_seed,
-        )
-
-    def to_tf(
-        self,
-        feature_columns: Union[str, List[str]],
-        label_columns: Union[str, List[str]],
-        *,
-        prefetch_blocks: int = 0,
-        batch_size: int = 1,
-        drop_last: bool = False,
-        local_shuffle_buffer_size: Optional[int] = None,
-        local_shuffle_seed: Optional[int] = None,
-    ) -> "tf.data.Dataset":
-        return self._base_dataset.to_tf(
-            feature_columns,
-            label_columns,
-            prefetch_blocks=prefetch_blocks,
-            batch_size=batch_size,
             drop_last=drop_last,
             local_shuffle_buffer_size=local_shuffle_buffer_size,
             local_shuffle_seed=local_shuffle_seed,
--- python/ray/data/tests/test_dataset_iterator.py
+++ python/ray/data/tests/test_dataset_iterator.py
@@ -6,6 +6,20 @@ import torch
 import ray
 
 
+def build_model():
+    import tensorflow as tf
+
+    model = tf.keras.Sequential(
+        [
+            tf.keras.layers.InputLayer(input_shape=()),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(1),
+        ]
+    )
+    model.compile(optimizer="sgd", loss="mse")
+    return model
+
+
 def test_basic_dataset(ray_start_regular_shared):
     ds = ray.data.range(100)
     it = ds.iterator()
@@ -42,6 +56,52 @@ def test_tf_conversion(ray_start_regular_shared):
         assert all(row[1] == i)
         assert isinstance(row[0], tf.Tensor)
         assert isinstance(row[1], tf.Tensor)
+
+
+def test_tf_e2e(ray_start_regular_shared):
+    ds = ray.data.range_table(5)
+    it = ds.iterator()
+    model = build_model()
+    model.fit(it.to_tf("value", "value"), epochs=3)
+
+
+def test_tf_e2e_pipeline(ray_start_regular_shared):
+    ds = ray.data.range_table(5).repeat(2)
+    it = ds.iterator()
+    model = build_model()
+    model.fit(it.to_tf("value", "value"), epochs=2)
+
+    ds = ray.data.range_table(5).repeat(2)
+    it = ds.iterator()
+    model = build_model()
+    # 3 epochs fails since we only repeated twice.
+    with pytest.raises(Exception, match=r"generator raised StopIteration"):
+        model.fit(it.to_tf("value", "value"), epochs=3)
+
+
+def test_tf_conversion_pipeline(ray_start_regular_shared):
+    ds = ray.data.range_table(5).repeat(2)
+    it = ds.iterator()
+    tf_dataset = it.to_tf("value", "value")
+    for i, row in enumerate(tf_dataset):
+        assert all(row[0] == i)
+        assert all(row[1] == i)
+        assert isinstance(row[0], tf.Tensor)
+        assert isinstance(row[1], tf.Tensor)
+
+    # Repeated twice.
+    tf_dataset = it.to_tf("value", "value")
+    for i, row in enumerate(tf_dataset):
+        assert all(row[0] == i)
+        assert all(row[1] == i)
+        assert isinstance(row[0], tf.Tensor)
+        assert isinstance(row[1], tf.Tensor)
+
+    # Fails on third try.
+    with pytest.raises(Exception, match=r"generator raised StopIteration"):
+        tf_dataset = it.to_tf("value", "value")
+        for _ in tf_dataset:
+            pass
 
 
 def test_torch_conversion(ray_start_regular_shared):
