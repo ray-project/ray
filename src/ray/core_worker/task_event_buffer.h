2f48e93924d4d9fe1f6911849f45630e37d7d173
rickyyx <rickyx@anyscale.com>
2023-02-16 01:59:20 +0000

    push
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- src/ray/core_worker/task_event_buffer.h
+++ src/ray/core_worker/task_event_buffer.h
@@ -24,6 +24,7 @@
 #include "ray/common/asio/instrumented_io_context.h"
 #include "ray/common/asio/periodical_runner.h"
 #include "ray/common/id.h"
+#include "ray/common/ray_config.h"
 #include "ray/common/task/task_spec.h"
 #include "ray/gcs/gcs_client/gcs_client.h"
 #include "src/ray/protobuf/gcs.pb.h"
