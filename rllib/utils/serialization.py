3391df1fdaf36dca542ce07f043ef1ac0b9453b7
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2023-02-16 18:39:31 -0800

    Fixes a bug that during checkpointing a policy we would lose the nested structure of observation and action space
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- rllib/utils/serialization.py
+++ rllib/utils/serialization.py
@@ -186,7 +186,7 @@ def gym_space_to_dict(space: gym.spaces.Space) -> Dict:
 def space_to_dict(space: gym.spaces.Space) -> Dict:
     d = {"space": gym_space_to_dict(space)}
     if "original_space" in space.__dict__:
-        d["original_space"] = gym_space_to_dict(space.original_space)
+        d["original_space"] = space_to_dict(space.original_space)
     return d
 
 
@@ -280,7 +280,7 @@ def gym_space_from_dict(d: Dict) -> gym.spaces.Space:
 def space_from_dict(d: Dict) -> gym.spaces.Space:
     space = gym_space_from_dict(d["space"])
     if "original_space" in d:
-        space.original_space = gym_space_from_dict(d["original_space"])
+        space.original_space = space_from_dict(d["original_space"])
     return space
 
 
