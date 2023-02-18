fd69679a80e00b28b9fa486b3fe2b2c51c134783
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2023-02-17 16:37:44 -0800

    Add release test
    Signed-off-by: Archit Kulkarni <architkulkarni@users.noreply.github.com>

--- release/cluster_tests/test_aws_cluster_launcher.sh
+++ release/cluster_tests/test_aws_cluster_launcher.sh
@@ -3,17 +3,32 @@
 
 set -e
 
+# Check if a command line argument was provided
+if [[ -z "$1" ]]; then
+    echo "Please provide a path to the cluster configuration file as a command line argument."
+    exit 1
+fi
+
+# Check if the file exists and is readable
+if [[ ! -r "$1" ]]; then
+    echo "Error: Cannot read cluster configuration file: $1"
+    exit 1
+fi
+
+# Log the name of the file that will be used
+echo "Using cluster configuration file: $1"
+
 echo "===== clean up previously running cluster ====="
-ray down -y ./aws_cluster.yaml
+ray down -y "$1"
 
 sleep 5
 echo "===== start cluster ====="
-ray up -y ./aws_cluster.yaml
+ray up -y "$1"
 
 sleep 5
 echo "===== verify ray is running ====="
-ray exec ./aws_cluster.yaml 'python -c "import ray; ray.init()"'
+ray exec "$1" 'python -c "import ray; ray.init()"'
 
 sleep 5
 echo "===== clean up ====="
-ray down -y ./aws_cluster.yaml
+ray down -y "$1"
