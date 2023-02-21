7d4b613a39abf83bac63e758ab79df6e56d9023e
Matthew Tang <matthew29tang@gmail.com>
2023-02-21 15:40:41 -0800

    [Data] Fix legacy do_write (#32661)
    With the new `write` added (from #32015 and #32440), Ray Data intends to support both the `write` and `do_write` functions for now. The check currently uses the `hasattr()` function to ensure the datasource object has a `write` method before using it.
    
    However, this is insufficient for a custom datasource that inherits from `Datasource` since `Datasource` has the `write` method implemented. If the custom datasource only has `do_write` implemented, `hasattr(datasource, "write")` will return True since `hasattr()` will detect methods via inheritance.
    
    The solution is to check if the `write` method was overwritten from `Datasource.write`. Any class that has not implemented `write` will have the equality check return True
    
    Signed-off-by: elliottower <elliot@elliottower.com>

--- python/ray/data/dataset.py
+++ python/ray/data/dataset.py
@@ -2693,7 +2693,7 @@ class Dataset(Generic[T]):
                 soft=False,
             )
 
-        if hasattr(datasource, "write"):
+        if type(datasource).write != Datasource.write:
             plan = self._plan.with_stage(
                 OneToOneStage(
                     "write",
