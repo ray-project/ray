bf6eccb9c05b8fe3315fde39ff18c6fe154bff1b
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2023-02-22 15:52:04 -0800

    [core]Update boost in ray deps (#32550)
    Update boost version to the latest ones.
    
    - updated the rules to build boost
    - update the bazel version so it can work with the latest rules
    - update boost version
    
    Signed-off-by: elliottower <elliot@elliottower.com>

--- python/setup.py
+++ python/setup.py
@@ -24,7 +24,7 @@ SUPPORTED_PYTHONS = [(3, 6), (3, 7), (3, 8), (3, 9), (3, 10), (3, 11)]
 # When the bazel version is updated, make sure to update it
 # in WORKSPACE file as well.
 
-SUPPORTED_BAZEL = (4, 2, 2)
+SUPPORTED_BAZEL = (5, 4, 0)
 
 ROOT_DIR = os.path.dirname(__file__)
 BUILD_JAVA = os.getenv("RAY_INSTALL_JAVA") == "1"
