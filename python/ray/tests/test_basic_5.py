e41c2a3c8addc808b1741c8bc1c14be5cf417b01
Dmitri Gekhtman <62982571+DmitriGekhtman@users.noreply.github.com>
2023-02-23 10:53:31 -0800

    [core] Allow worker Python interpeters to inherit -S and -s flags (#32690)
    Starting the Python interpreter with flags "-S" and "-s" disables site package imports. This is useful when running Python scripts in hermetic build environments.
    
    This PR allows the Python interpreter in worker processes to inherit these flags -- if you've gone through the trouble of starting Ray using these flags, it makes enough sense to pass the flags on to the Python workers.
    
    Signed-off-by: Dmitri Gekhtman <dmitri.gekhtman@getcruise.com>

--- python/ray/tests/test_basic_5.py
+++ python/ray/tests/test_basic_5.py
@@ -5,6 +5,7 @@ import os
 import sys
 import time
 import subprocess
+from unittest.mock import Mock, patch
 
 import pytest
 
@@ -248,6 +249,34 @@ def test_worker_kv_calls(monkeypatch, shutdown_only):
     """
     # !!!If you want to increase this number, please let ray-core knows this!!!
     assert freqs["internal_kv_get"] == 4
+
+
+@pytest.mark.parametrize("root_process_no_site", [0, 1])
+@pytest.mark.parametrize("root_process_no_user_site", [0, 1])
+def test_site_flag_inherited(
+    shutdown_only, monkeypatch, root_process_no_site, root_process_no_user_site
+):
+    # The flags we're testing could prevent Python workers in the test environment
+    # from locating site packages; the workers would fail to find Ray and would thus
+    # fail to start. To prevent that, set the PYTHONPATH env. The env will be inherited
+    # by the Python workers, so that they are able to import Ray.
+    monkeypatch.setenv("PYTHONPATH", ":".join(sys.path))
+
+    @ray.remote
+    def get_flags():
+        return sys.flags.no_site, sys.flags.no_user_site
+
+    with patch.multiple(
+        "ray._private.services",
+        _no_site=Mock(return_value=root_process_no_site),
+        _no_user_site=Mock(return_value=root_process_no_user_site),
+    ):
+        ray.init()
+        worker_process_no_site, worker_process_no_user_site = ray.get(
+            get_flags.remote()
+        )
+        assert worker_process_no_site == root_process_no_site
+        assert worker_process_no_user_site == root_process_no_user_site
 
 
 if __name__ == "__main__":
