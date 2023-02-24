32d50eb09d177af514d3a9b1d2f8eaf5b7088f2d
Justin Yu <justinvyu@berkeley.edu>
2023-02-23 16:18:01 -0800

    Fix batch prediction example to use numpy
    Signed-off-by: Justin Yu <justinvyu@berkeley.edu>

--- doc/source/examples/01_batch_inference/batch_inference.py
+++ doc/source/examples/01_batch_inference/batch_inference.py
@@ -1,4 +1,5 @@
-import pandas as pd
+import numpy as np
+from typing import Dict
 
 import ray
 from ray.air import Checkpoint
@@ -9,9 +10,10 @@ from ray.train.batch_predictor import BatchPredictor
 
 
 def load_trained_model():
-    def model(batch: pd.DataFrame) -> pd.DataFrame:
+    # Replace this with loading your own model.
+    def model(batch: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
         predict = batch["passenger_count"] >= 2
-        return pd.DataFrame({"score": predict})
+        return {"score": predict}
 
     return model
 
@@ -30,15 +32,18 @@ class CustomPredictor(Predictor):
         super().__init__()
         self.model = model
 
-    def _predict_pandas(self, batch: pd.DataFrame) -> pd.DataFrame:
-        return self.model(batch)
+    def _predict_numpy(
+        self, data: Dict[str, np.ndarray], **kwargs
+    ) -> Dict[str, np.ndarray]:
+        # Replace this with doing inference with your own model.
+        return self.model(data)
 
     @classmethod
     def from_checkpoint(cls, checkpoint: Checkpoint, **kwargs) -> "CustomPredictor":
         return CustomPredictor(checkpoint.to_dict()["model"])
 
 
-def preprocess(batch):
+def preprocess(batch: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
     batch["passenger_count"] -= 1.0
     return batch
 
