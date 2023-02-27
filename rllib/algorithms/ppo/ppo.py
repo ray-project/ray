6118373b6e392c39c349ac2ce5b66fde874d236c
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2023-02-27 09:47:08 -0800

    fxied the enabling
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- rllib/algorithms/ppo/ppo.py
+++ rllib/algorithms/ppo/ppo.py
@@ -262,15 +262,15 @@ class PPOConfig(PGConfig):
 
     @override(AlgorithmConfig)
     def validate(self) -> None:
-        # Call super's validation method.
-        super().validate()
-
         # Turn RLModule and Learner API on by default (only for torch and tf2)
         if self.framework_str in ["torch", "tf2"]:
             # only for this class and not it subclasses
             if self.__class__.__name__ == PPOConfig.__name__:
                 self._enable_rl_module_api = True
                 self._enable_learner_api = True
+
+        # Call super's validation method.
+        super().validate()
 
         # SGD minibatch size must be smaller than train_batch_size (b/c
         # we subsample a batch of `sgd_minibatch_size` from the train-batch for
