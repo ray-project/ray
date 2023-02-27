0c1b576a2ef95c91a02abe4a2037a49beb4af063
Tom <tmbdev@gmail.com>
2023-02-26 22:47:54 -0800

    cleanup

--- python/ray/data/datasource/webdataset_datasource.py
+++ python/ray/data/datasource/webdataset_datasource.py
@@ -22,9 +22,11 @@ def base_plus_ext(path):
 
     Returns base, allext.
 
-    :param path: path with extensions
-    :param returns: path with all extensions removed
+    Args:
+        path (str): path with extensions
 
+    Returns:
+        str: path with all extensions removed
     """
     match = re.match(r"^((?:.*/|)[^.]+)[.]([^/]*)$", path)
     if not match:
@@ -35,7 +37,8 @@ def base_plus_ext(path):
 def valid_sample(sample):
     """Check whether a sample is valid.
 
-    :param sample: sample to be checked
+    Args:
+        sample (dict): sample to be checked
     """
     return (
         sample is not None
@@ -45,12 +48,13 @@ def valid_sample(sample):
     )
 
 
-def tar_file_iterator(fileobj, skipfn=lambda fname: False, meta={}):
+def tar_file_iterator(fileobj, skipfn=lambda fname: False):
     """Iterate over tar file, yielding filename, content pairs for the given tar stream.
 
-    :param fileobj: byte stream suitable for tarfile
-    :param skip_meta: regexp for keys that are skipped entirely (Default value = r"__[^/]*__($|/)")
-
+    Args:
+        fileobj (file): file object
+        skipfn (function): function indicating that files should be skipped
+        meta (dict): metadata to be added to each sample
     """
     global verbose_open
     stream = tarfile.open(fileobj=fileobj, mode="r|*")
@@ -61,35 +65,31 @@ def tar_file_iterator(fileobj, skipfn=lambda fname: False, meta={}):
         if not tarinfo.isreg() or fname is None or skipfn(fname):
             continue
         data = stream.extractfile(tarinfo).read()
-        result = dict(fname=fname, data=data, **meta)
+        result = dict(fname=fname, data=data)
         yield result
     if verbose_open:
         print(f"done {meta}")
 
 
-def group_by_keys(data, keys=base_plus_ext, lcase=True, suffixes=None, trace=False):
+def group_by_keys(data, keys=base_plus_ext, suffixes=None, meta={}):
     """Return function over iterator that groups key, value pairs into samples.
 
-    :param keys: function that splits the key into key and extension (base_plus_ext)
-    :param lcase: convert suffixes to lower case (Default value = True)
+    Args:
+        data (iterator): iterator over key, value pairs
+        keys (function): function that returns key, suffix for a given key
+        suffixes (list): list of suffixes to be included in the sample
+        meta (dict): metadata to be added to each sample
     """
     current_sample = None
     for filesample in data:
         assert isinstance(filesample, dict)
         fname, value = filesample["fname"], filesample["data"]
         prefix, suffix = keys(fname)
-        if trace:
-            print(
-                prefix,
-                suffix,
-                current_sample.keys() if isinstance(current_sample, dict) else None,
-            )
         if prefix is None:
             continue
-        if lcase:
-            suffix = suffix.lower()
         if current_sample is None or prefix != current_sample["__key__"]:
             if valid_sample(current_sample):
+                current_sample.update(meta)
                 yield current_sample
             current_sample = dict(__key__=prefix)
             if "__url__" in filesample:
@@ -101,12 +101,15 @@ def group_by_keys(data, keys=base_plus_ext, lcase=True, suffixes=None, trace=Fal
         if suffixes is None or suffix in suffixes:
             current_sample[suffix] = value
     if valid_sample(current_sample):
+        current_sample.update(meta)
         yield current_sample
+
 
 def table_to_list(table):
     """Convert a pyarrow table to a list of dictionaries.
 
-    :param table: pyarrow table
+    Args:
+        table (pyarrow table): pyarrow table
     """
     result = []
     for i in range(table.num_rows):
@@ -118,8 +121,8 @@ def table_to_list(table):
 
 
 def default_decoder(sample: Dict[str, Any]):
-    """A default decoder for webdataset. 
-    
+    """A default decoder for webdataset.
+
     This handles common file extensions: .txt, .cls, .cls2, .jpg, .png, .json, .npy, .mp, .pt, .pth, .pickle, .pkl.
 
     Args:
@@ -136,28 +139,34 @@ def default_decoder(sample: Dict[str, Any]):
             sample[key] = int(value.decode("utf-8"))
         elif extension in [".jpg", ".png", ".ppm", ".pgm", ".pbm", ".pnm"]:
             import PIL.Image
+
             sample[key] = PIL.Image.open(io.BytesIO(value))
         elif extension == ".json":
             import json
+
             sample[key] = json.loads(value)
         elif extension == ".npy":
             import numpy as np
+
             sample[key] = np.load(io.BytesIO(value))
         elif extension == ".mp":
             import msgpack
+
             sample[key] = msgpack.unpackb(value, raw=False)
         elif extension in [".pt", ".pth"]:
             import torch
+
             sample[key] = torch.load(io.BytesIO(value))
         elif extension in [".pickle", ".pkl"]:
             import pickle
+
             sample[key] = pickle.loads(value)
     return sample
 
 
 def default_encoder(sample: Dict[str, Any]):
     """A default encoder for webdataset.
-    
+
     This handles common file extensions: .txt, .cls, .cls2, .jpg, .png, .json, .npy, .mp, .pt, .pth, .pickle, .pkl
 
     Args:
@@ -175,6 +184,7 @@ def default_encoder(sample: Dict[str, Any]):
         elif extension in [".jpg", ".png", ".ppm", ".pgm", ".pbm", ".pnm"]:
             import PIL.Image
             import numpy as np
+
             if isinstance(value, np.ndarray):
                 value = PIL.Image.fromarray(value)
             assert isinstance(value, PIL.Image.Image)
@@ -183,22 +193,27 @@ def default_encoder(sample: Dict[str, Any]):
             sample[key] = stream.getvalue()
         elif extension == ".json":
             import json
+
             sample[key] = json.dumps(value).encode("utf-8")
         elif extension == ".npy":
             import numpy as np
+
             stream = io.BytesIO()
             np.save(stream, value)
             sample[key] = stream.getvalue()
         elif extension == ".mp":
             import msgpack
+
             sample[key] = msgpack.dumps(value)
         elif extension in [".pt", ".pth"]:
             import torch
+
             stream = io.BytesIO()
             torch.save(value, stream)
             sample[key] = stream.getvalue()
         elif extension in [".pickle", ".pkl"]:
             import pickle
+
             stream = io.BytesIO()
             pickle.dump(value, stream)
             sample[key] = stream.getvalue()
@@ -207,13 +222,23 @@ def default_encoder(sample: Dict[str, Any]):
 
 @PublicAPI(stability="alpha")
 class WebDatasetDatasource(FileBasedDatasource):
+    """A Datasource for WebDataset datasets (tar format with naming conventions)."""
+
     _FILE_EXTENSION = "tar"
 
-    def _read_stream(self, stream: "pyarrow.NativeFile", path: str, decoder=default_decoder, **kw):
+    def _read_stream(
+        self,
+        stream: "pyarrow.NativeFile",
+        path: str,
+        decoder=default_decoder,
+        skipfn=lambda _: False,
+        suffixes=None,
+        **kw,
+    ):
         import pyarrow as pa
 
-        files = tar_file_iterator(stream, meta=dict(__url__=path))
-        samples = group_by_keys(files)
+        files = tar_file_iterator(stream, skipfn=skipfn)
+        samples = group_by_keys(files, meta=dict(__url__=path), suffixes=suffixes)
         for sample in samples:
             if decoder is not None:
                 sample = decoder(sample)
@@ -229,6 +254,7 @@ class WebDatasetDatasource(FileBasedDatasource):
         **kw,
     ):
         import pyarrow
+
         table = block.to_arrow()
         # to_pylist is missing from these tables for some reason
         # samples = table.to_pylist()
@@ -236,7 +262,7 @@ class WebDatasetDatasource(FileBasedDatasource):
         stream = tarfile.open(fileobj=f, mode="w|")
         for sample in samples:
             if encoder is not None:
-                sample = encoder(sample)            
+                sample = encoder(sample)
             if not "__key__" in sample:
                 sample["__key__"] = uuid.uuid4().hex
             key = sample["__key__"]
