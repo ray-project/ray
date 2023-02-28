139b4be0545b8a0b5315170b91256bc85fcb5f97
rickyyx <rickyx@anyscale.com>
2023-02-28 20:05:37 +0000

    typo
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- release/ray_release/scripts/run_release_test.py
+++ release/ray_release/scripts/run_release_test.py
@@ -57,7 +57,7 @@ from ray_release.wheels import find_and_wait_for_ray_wheels_url
         "Ray master branch. Can also be `<repo_url>:<branch>` or "
         "`<repo_url>:<commit>` to specify a different repository to "
         "fetch wheels from, if available. Can also be "
-        "`file://<path to local wheel` for a wheel built locally."
+        "`file://<path to local wheel>` for wheels built locally."
     ),
 )
 @click.option(
