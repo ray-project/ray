78e2266435f1836d1f893505e6bde37a7c88c293
Jian Xiao <99709935+jianoaix@users.noreply.github.com>
2023-03-01 13:55:57 -0800

    Streaming executor fixes #4 (#32882)
    Signed-off-by: elliottower <elliot@elliottower.com>

--- python/ray/data/tests/test_dataset.py
+++ python/ray/data/tests/test_dataset.py
@@ -382,12 +382,13 @@ def test_zip_different_num_blocks_split_smallest(
         [{str(i): i for i in range(num_cols1, num_cols1 + num_cols2)}] * n,
         parallelism=num_blocks2,
     )
-    ds = ds1.zip(ds2)
+    ds = ds1.zip(ds2).fully_executed()
+    num_blocks = ds._plan._snapshot_blocks.executed_num_blocks()
     assert ds.take() == [{str(i): i for i in range(num_cols1 + num_cols2)}] * n
     if should_invert:
-        assert ds.num_blocks() == num_blocks2
+        assert num_blocks == num_blocks2
     else:
-        assert ds.num_blocks() == num_blocks1
+        assert num_blocks == num_blocks1
 
 
 def test_zip_pandas(ray_start_regular_shared):
