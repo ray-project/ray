fb306310bbc6bc5a542fc119f318c3d2511f02e2
Zhi Lin <zhi.lin@intel.com>
2023-03-02 16:04:06 +0000

    add a test to test to_pandas
    Signed-off-by: Zhi Lin <zhi.lin@intel.com>

--- python/ray/data/tests/test_raydp_dataset.py
+++ python/ray/data/tests/test_raydp_dataset.py
@@ -2,6 +2,7 @@ import pytest
 import ray
 import raydp
 import torch
+import pandas
 
 
 @pytest.fixture(scope="function")
@@ -49,6 +50,13 @@ def test_raydp_to_torch_iter(spark):
     data_features, data_labels = next(dataset.__iter__())
     assert torch.equal(data_features, features) and torch.equal(data_labels, labels)
 
+
+def test_to_pandas(spark):
+    df = spark.range(100)
+    ds = ray.data.from_spark(df)
+    pdf = ds.to_pandas()
+    pdf2 = df.toPandas()
+    pandas.testing.assert_frame_equal(pdf, pdf2)
 
 if __name__ == "__main__":
     import sys
