d0c6a1f1e1779d5ef32b95f3a73f1de541fdf452
jianoaix <iamjianxiao@gmail.com>
2023-03-02 01:52:56 +0000

    add test

--- python/ray/data/tests/test_util.py
+++ python/ray/data/tests/test_util.py
@@ -2,7 +2,7 @@ import pytest
 import ray
 import numpy as np
 
-from ray.data._internal.util import _check_pyarrow_version
+from ray.data._internal.util import _check_pyarrow_version, _split_list
 from ray.data._internal.memory_tracing import (
     trace_allocation,
     trace_deallocation,
@@ -70,6 +70,18 @@ def test_memory_tracing(enabled):
         assert "test3" not in report, report
         assert "test4" not in report, report
         assert "test5" not in report, report
+
+
+def test_list_splits():
+    with pytest.raises(AssertionError):
+        _split_list(list(range(5)), 0)
+
+    with pytest.raises(AssertionError):
+        _split_list(list(range(5)), -1)
+
+    assert _split_list(list(range(5)), 7) == [[0], [1], [2], [3], [4], [], []]
+    assert _split_list(list(range(5)), 2) == [[0, 1, 2], [3, 4]]
+    assert _split_list(list(range(6)), 2) == [[0, 1, 2], [3, 4, 5]]
 
 
 if __name__ == "__main__":
