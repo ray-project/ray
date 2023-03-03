5475351dcab7b279c465bad8144b66cbfdd365f6
amogkam <amogkamsetty@yahoo.com>
2023-03-03 10:04:23 -0800

    revert
    Signed-off-by: amogkam <amogkamsetty@yahoo.com>

--- python/ray/air/_internal/torch_utils.py
+++ python/ray/air/_internal/torch_utils.py
@@ -54,9 +54,7 @@ def get_device() -> Union[torch.device, List[torch.device]]:
             # 0th device.
             device_ids.append(0)
 
-        devices = sorted(
-            [torch.device(f"cuda:{device_id}") for device_id in device_ids]
-        )
+        devices = [torch.device(f"cuda:{device_id}") for device_id in device_ids]
         device = devices[0] if len(devices) == 1 else devices
     else:
         device = torch.device("cpu")
