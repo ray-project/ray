f5f26f9049cd9fe8625c294cdf498a6777ebe7b6
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2023-03-06 22:11:14 +0000

    fix Signed-off-by: Yi Cheng <74173148+iycheng@users.noreply.github.com>

--- src/ray/raylet/scheduling/cluster_task_manager_interface.h
+++ src/ray/raylet/scheduling/cluster_task_manager_interface.h
@@ -49,6 +49,7 @@ class ClusterTaskManagerInterface {
       rpc::RequestWorkerLeaseReply::SchedulingFailureType failure_type =
           rpc::RequestWorkerLeaseReply::SCHEDULING_CANCELLED_INTENDED,
       const std::string &scheduling_failure_message = "") = 0;
+
   virtual bool CancelAllTaskOwnedBy(
       const WorkerID &worker_id,
       rpc::RequestWorkerLeaseReply::SchedulingFailureType failure_type =
