4e8ec493c5caa418e57d799fc870884aeee089a2
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2023-03-07 12:00:52 -0800

    fixed multi-gpu bugs
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- rllib/core/rl_module/torch/torch_rl_module.py
+++ rllib/core/rl_module/torch/torch_rl_module.py
@@ -40,7 +40,7 @@ class TorchRLModule(nn.Module, RLModule):
         return False
 
 
-class TorchDDPRLModule(nn.parallel.DistributedDataParallel, RLModule):
+class TorchDDPRLModule(RLModule, nn.parallel.DistributedDataParallel):
     def __init__(self, *args, **kwargs) -> None:
         nn.parallel.DistributedDataParallel.__init__(self, *args, **kwargs)
         # we do not want to call RLModule.__init__ here because all we need is
