99bb3c68550f2742dd5210a69379c63f56b53859
Eric Liang <ekhliang@gmail.com>
2023-03-06 17:20:02 -0800

    fix
    Signed-off-by: Eric Liang <ekhliang@gmail.com>

--- python/ray/data/_internal/execution/streaming_executor_state.py
+++ python/ray/data/_internal/execution/streaming_executor_state.py
@@ -159,11 +159,11 @@ class OpState:
                 # Scan the queue and look for outputs tagged for the given index.
                 for i in range(len(self.outqueue)):
                     bundle = self.outqueue[i]
-                    if bundle is None:
+                    if bundle is None or isinstance(bundle, BaseException):
                         # End of stream for this index! Note that we
                         # do not remove the None, so that it can act
                         # as the termination signal for all indices.
-                        return None
+                        return bundle
                     elif bundle.output_split_idx == output_split_idx:
                         self.outqueue.remove(bundle)
                         return bundle
