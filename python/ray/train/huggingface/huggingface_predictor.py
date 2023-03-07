e863d22f40e6a3f8d8267d96a859b481b5e49152
Antoni Baum <antoni.baum@protonmail.com>
2023-03-07 00:58:16 +0000

    Improve predictor
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>

--- python/ray/train/huggingface/huggingface_predictor.py
+++ python/ray/train/huggingface/huggingface_predictor.py
@@ -74,7 +74,8 @@ class HuggingFacePredictor(Predictor):
                 "prediction will only use CPU. Please consider explicitly "
                 "setting `HuggingFacePredictor(use_gpu=True)` or "
                 "`batch_predictor.predict(ds, num_gpus_per_worker=1)` to "
-                "enable GPU prediction."
+                "enable GPU prediction. Ignore if you have set `device` or "
+                "`device_map` arguments in the `pipeline` manually."
             )
 
         super().__init__(preprocessor)
@@ -111,14 +112,14 @@ class HuggingFacePredictor(Predictor):
                 initialization. If ``pipeline`` is None, this must contain
                 the 'task' argument. Cannot contain 'model'. Can be used
                 to override the tokenizer with 'tokenizer'. If ``use_gpu`` is
-                True, 'device' will be set to 0 by default.
+                True, 'device' will be set to 0 by default, unless 'device_map' is
+                passed.
         """
         if not pipeline_cls and "task" not in pipeline_kwargs:
             raise ValueError(
                 "If `pipeline_cls` is not specified, 'task' must be passed as a kwarg."
             )
-        if use_gpu:
-            # default to using the GPU with the first index
+        if use_gpu and "device_map" not in pipeline_kwargs:
             pipeline_kwargs.setdefault("device", 0)
         pipeline_cls = pipeline_cls or pipeline_factory
         preprocessor = checkpoint.get_preprocessor()
