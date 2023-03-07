27c963da11fc11c69e27852d8305e0b077d73e63
Clarence Ng <clarence.wyng@gmail.com>
2023-03-06 22:37:19 -0800

    [core] add check for potential worker leak when returning worker
    Signed-off-by: Clarence Ng <clarence.wyng@gmail.com>

--- release/ray_release/cluster_manager/cluster_manager.py
+++ release/ray_release/cluster_manager/cluster_manager.py
@@ -59,6 +59,7 @@ class ClusterManager(abc.ABC):
         self.cluster_env["env_vars"][
             "RAY_USAGE_STATS_EXTRA_TAGS"
         ] = f"test_name={self.test_name};smoke_test={self.smoke_test}"
+        self.cluster_env["env_vars"]["RAY_report_worker_leak"] = "true"
 
         self.cluster_env_name = (
             f"{self.project_name}_{self.project_id[4:8]}"
