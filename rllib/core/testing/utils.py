d127b25ee1fb57a27932318828daead1ec69a1c7
Artur Niederfahrenhorst <artur@anyscale.com>
2023-03-08 05:17:11 -0800

    Extend length of test and provide catalog to default agent spec in BC testsg
    Signed-off-by: Artur Niederfahrenhorst <artur@anyscale.com>

--- rllib/core/testing/utils.py
+++ rllib/core/testing/utils.py
@@ -5,6 +5,7 @@ from ray.rllib.core.rl_module.rl_module import SingleAgentRLModuleSpec
 from ray.rllib.utils.annotations import DeveloperAPI
 from ray.rllib.core.learner.learner_group import LearnerGroup
 from ray.rllib.core.learner.learner import LearnerSpec, FrameworkHPs
+from ray.rllib.core.models.catalog import Catalog
 from ray.rllib.core.learner.scaling_config import LearnerGroupScalingConfig
 
 from ray.rllib.core.rl_module.marl_module import (
@@ -62,6 +63,8 @@ def get_module_spec(framework: str, env: "gym.Env", is_multi_agent: bool = False
         observation_space=env.observation_space,
         action_space=env.action_space,
         model_config_dict={"fcnet_hiddens": [32]},
+        # TODO(Artur): Implement BCCatalog.
+        catalog_class=Catalog,
     )
 
     if is_multi_agent:
