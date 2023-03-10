edf761f4a96d182c56c5fe869726e1f1a7194b5e
rickyyx <rickyx@anyscale.com>
2023-03-10 00:30:36 +0000

    category
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- dashboard/agent.py
+++ dashboard/agent.py
@@ -50,11 +50,13 @@ logger = logging.getLogger(__name__)
 
 # We would want to suppress deprecating warnings from aiogrpc library
 # with the usage of asyncio.get_event_loop() in python version >=3.10
+# This could be removed once https://github.com/grpc/grpc/issues/32526
+# is released, and we used higher versions of grpcio that that.
 if sys.version_info.major >= 3 and sys.version_info.minor >= 10:
     import warnings
 
     with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
+        warnings.simplefilter("ignore", category=DeprecationWarning)
         aiogrpc.init_grpc_aio()
 else:
     aiogrpc.init_grpc_aio()
