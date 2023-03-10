36432742336093ec7721e1b7131bddcacbba6c7b
Justin Yu <justinvyu@berkeley.edu>
2023-03-10 10:19:30 -0800

    Update mmt example with larger version
    Signed-off-by: Justin Yu <justinvyu@berkeley.edu>

--- doc/source/examples/02_many_model_training/many_model_training.py
+++ doc/source/examples/02_many_model_training/many_model_training.py
@@ -1,8 +1,12 @@
-from ray import tune
-
 # For reading from cloud storage paths.
 from smart_open import smart_open
 import pandas as pd
+
+import ray
+from ray import air, tune
+from ray.air import session
+
+ray.init()
 
 
 def trainable_func(config: dict):
@@ -11,19 +15,24 @@ def trainable_func(config: dict):
     # Train your model here.
     from sklearn.linear_model import LinearRegression
 
-    lr = LinearRegression()
-    lr.fit(data[["id4", "id5"]], data["v3"])
+    X, y = data[["id4", "id5"]], data["v3"]
+    reg = LinearRegression().fit(X, y)
+    score = reg.score(X, y)
 
-    # Return a single dict of the model and stats, etc.
-    return {
-        "coef": lr.coef_,
-        "intercept": lr.intercept_,
-        "customer_id": data["customer_id"][0],
-    }
+    # Report a single dict of the model and stats, etc.
+    session.report(
+        metrics={
+            "coef": reg.coef_,
+            "intercept": reg.intercept_,
+            "customer_id": data["customer_id"][0],
+            "score": score,
+        }
+    )
 
 
 # Tune is designed for up to thousands of trials.
-num_trials = 1000
+num_nodes = len(ray.nodes())
+num_trials = 250 * num_nodes
 
 param_space = {
     "file_path": tune.grid_search(
@@ -34,7 +43,15 @@ param_space = {
     )
 }
 
-tuner = tune.Tuner(trainable_func, param_space=param_space)
+tuner = tune.Tuner(
+    trainable_func,
+    param_space=param_space,
+    run_config=air.RunConfig(
+        name="many_model_training_template",
+        local_dir="/mnt/cluster_storage/",
+        sync_config=tune.SyncConfig(syncer=None),
+    ),
+)
 result_grid = tuner.fit()
 
 print(result_grid.get_dataframe())
