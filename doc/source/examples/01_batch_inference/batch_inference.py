3a677ac760a6d80081c8ea28e47a08bbee9cbb25
Justin Yu <justinvyu@berkeley.edu>
2023-03-10 12:20:59 -0800

    Fix lint
    Signed-off-by: Justin Yu <justinvyu@berkeley.edu>

--- doc/source/examples/01_batch_inference/batch_inference.py
+++ doc/source/examples/01_batch_inference/batch_inference.py
@@ -8,6 +8,9 @@ from ray.data.datasource.partitioning import Partitioning
 from ray.data.preprocessors import BatchMapper
 from ray.train.batch_predictor import BatchPredictor
 
+# Replace this with your own framework/custom checkpoint/predictors!
+from ray.train.torch import TorchCheckpoint, TorchPredictor
+
 
 # 1. Load your own data with Ray Data!
 # Start
@@ -18,9 +21,12 @@ ds = ray.data.read_images(
 )
 # End
 
+
 # 2. Replace this with your own custom preprocessing logic!
-# Start
+
+
 def preprocess(batch: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
+    # Start
     def to_tensor(batch: np.ndarray) -> torch.Tensor:
         tensor = torch.as_tensor(batch, dtype=torch.float)
         # (B, H, W, C) -> (B, C, H, W)
@@ -37,32 +43,32 @@ def preprocess(batch: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
         ]
     )
     return {"image": transform(batch["image"]).numpy()}
+    # End
 
 
 preprocessor = BatchMapper(fn=preprocess, batch_format="numpy")
-# End
 
 # 3. Replace these with the framework of your choice!
 # Start
-from ray.train.torch import TorchCheckpoint, TorchPredictor
-
 predictor_cls = TorchPredictor
 checkpoint_cls = TorchCheckpoint
 # End
 
 # 4. Replace this with building and loading your own model!
-# Start
+
+
 def build_model_checkpoint():
+    # Start
     from torchvision import models
 
     # Load the pretrained resnet model and construct a checkpoint
     model = models.resnet152(pretrained=True)
     checkpoint = checkpoint_cls.from_model(model=model, preprocessor=preprocessor)
     return checkpoint
+    # End
 
 
 checkpoint = build_model_checkpoint()
-# End
 
 # 5. Finally, build the BatchPredictor and perform GPU batch prediction!
 batch_predictor = BatchPredictor(checkpoint=checkpoint, predictor_cls=predictor_cls)
--- doc/source/examples/03_serving_stable_diffusion/client.py
+++ doc/source/examples/03_serving_stable_diffusion/client.py
@@ -2,25 +2,34 @@ import aiohttp
 import asyncio
 import os
 import halo
-import ray
+import requests
 import time
 import uuid
 
+import ray
 
 ray.init()
 NUM_NODES = len(ray.nodes())
-
+ENDPOINT = "http://localhost:8000/imagine"
 spinner = halo.Halo(text="Generating image...", spinner="dots")
 
 
 async def generate_image(session, prompt):
     req = {"prompt": prompt, "img_size": 776}
-    async with session.get("http://localhost:8000/imagine", params=req) as resp:
+    async with session.get(ENDPOINT, params=req) as resp:
         image_data = await resp.read()
     return image_data
 
 
 async def main():
+    try:
+        requests.get(ENDPOINT, timeout=0.1)
+    except Exception as e:
+        raise RuntimeWarning(
+            "Did you setup the Ray Serve model replicas with "
+            "`serve run app:entrypoint` in another terminal yet?"
+        ) from e
+
     while True:
         example_prompt = "dog reading a newspaper in 4k hd ultra realistic"
         prompt = input(f"Enter a prompt (ex: '{example_prompt}'): ")
@@ -44,7 +53,8 @@ async def main():
         elapsed = time.time() - start
 
         print(
-            f"\nGenerated {len(images)} image(s) in {elapsed:.2f} seconds to: {dirname}\n"
+            f"\nGenerated {len(images)} image(s) in {elapsed:.2f} seconds to: "
+            f"{dirname}\n"
         )
 
 
