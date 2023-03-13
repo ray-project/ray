fe3ce1a42efd8a45cd1e74556e3a0eeb4e8b3343
rickyyx <rickyx@anyscale.com>
2023-03-13 19:25:39 +0000

    lint
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- python/ray/tests/test_exit_observability.py
+++ python/ray/tests/test_exit_observability.py
@@ -329,31 +329,37 @@ def test_worker_start_end_time(shutdown_only):
     # Test normal exit.
     worker = Worker.remote()
     pid = ray.get(worker.ready.remote())
+
     def verify():
         workers = list_workers(detail=True, filters=[("pid", "=", pid)])[0]
         print(workers)
         assert workers["start_time_ms"] > 0
         assert workers["end_time_ms"] == 0
         return True
+
     wait_for_condition(verify)
 
     ray.kill(worker)
+
     def verify():
         workers = list_workers(detail=True, filters=[("pid", "=", pid)])[0]
         assert workers["start_time_ms"] > 0
         assert workers["end_time_ms"] > 0
         return True
+
     wait_for_condition(verify)
 
     # Test unexpected exit.
     worker = Worker.remote()
     pid = ray.get(worker.ready.remote())
     os.kill(pid, signal.SIGKILL)
+
     def verify():
         workers = list_workers(detail=True, filters=[("pid", "=", pid)])[0]
         assert workers["start_time_ms"] > 0
         assert workers["end_time_ms"] > 0
         return True
+
     wait_for_condition(verify)
 
 
