685212bec19fe60ec80983179b6aa3a5a9921eb5
Avnish Narayan <38871737+avnishn@users.noreply.github.com>
2023-03-14 10:28:30 -0700

    [RLlib] Marl_checkpoint (#33195)
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>
    Signed-off-by: Jack He <jackhe2345@gmail.com>

--- rllib/core/rl_module/torch/torch_rl_module.py
+++ rllib/core/rl_module/torch/torch_rl_module.py
@@ -29,22 +29,15 @@ class TorchRLModule(nn.Module, RLModule):
     def set_state(self, state_dict: Mapping[str, Any]) -> None:
         self.load_state_dict(state_dict)
 
+    def _module_state_file_name(self) -> pathlib.Path:
+        return pathlib.Path("module_state.pt")
+
     @override(RLModule)
-    def save_state_to_file(self, path: Union[str, pathlib.Path]) -> str:
-        if isinstance(path, str):
-            path = pathlib.Path(path)
-        module_state_path = path / "module_state.pt"
-        torch.save(self.state_dict(), str(module_state_path))
-        return str(module_state_path)
+    def save_state_to_file(self, path: Union[str, pathlib.Path]):
+        torch.save(self.state_dict(), str(path))
 
     @override(RLModule)
     def load_state_from_file(self, path: Union[str, pathlib.Path]) -> None:
-        if isinstance(path, str):
-            path = pathlib.Path(path)
-        if not path.exists():
-            raise ValueError(
-                f"While loading state from path, the path does not exist: {path}"
-            )
         self.set_state(torch.load(str(path)))
 
     @override(RLModule)
@@ -85,6 +78,14 @@ class TorchDDPRLModule(RLModule, nn.parallel.DistributedDataParallel):
     @override(RLModule)
     def set_state(self, *args, **kwargs):
         self.module.set_state(*args, **kwargs)
+
+    @override(RLModule)
+    def save_state_to_file(self, *args, **kwargs) -> str:
+        return self.module.save_state_to_file(*args, **kwargs)
+
+    @override(RLModule)
+    def load_state_from_file(self, *args, **kwargs):
+        self.module.load_state_from_file(*args, **kwargs)
 
     @override(RLModule)
     def make_distributed(self, dist_config: Mapping[str, Any] = None) -> None:
