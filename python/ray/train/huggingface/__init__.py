78ae3f7e28f36916a454a2a50946ce9612d23313
Antoni Baum <antoni.baum@protonmail.com>
2023-03-14 23:39:38 +0000

    Remove from init
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>

--- python/ray/train/huggingface/__init__.py
+++ python/ray/train/huggingface/__init__.py
@@ -3,11 +3,11 @@ from ray.train.huggingface.huggingface_predictor import HuggingFacePredictor
 from ray.train.huggingface.huggingface_trainer import (
     HuggingFaceTrainer,
 )
-from ray.train.huggingface.accelerate_trainer import AccelerateTrainer
+# from ray.train.huggingface.accelerate_trainer import AccelerateTrainer
 
 __all__ = [
     "HuggingFaceCheckpoint",
     "HuggingFacePredictor",
     "HuggingFaceTrainer",
-    "AccelerateTrainer",
+    # "AccelerateTrainer",
 ]
