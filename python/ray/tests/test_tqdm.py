7dbd79e5d1a67ca61a06f78ce460e6177b6fcba6
Eric Liang <ekhliang@gmail.com>
2023-03-13 17:39:56 -0700

    [data] [distributed tqdm] Avoid corrupting tqdm output when log monitor prints worker logs (#33261)
    Signed-off-by: Jack He <jackhe2345@gmail.com>

--- python/ray/tests/test_tqdm.py
+++ python/ray/tests/test_tqdm.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import time
 
 import pytest
 
@@ -11,18 +10,21 @@ from ray._private.test_utils import wait_for_condition
 
 def test_distributed_tqdm_remote():
     @ray.remote
-    def foo():
-        print("foo started")
-        try:
-            foo = tqdm_ray.tqdm(desc="foo", total=100, position=0)
-            foo.update(42)
-        except Exception as e:
-            print(e)
-        print("foo wait")
-        time.sleep(999)  # Keep the bar open.
+    class Actor:
+        def __init__(self):
+            try:
+                self.bar = tqdm_ray.tqdm(desc="foo", total=100, position=0)
+                self.bar.update(42)
+            except Exception as e:
+                print(e)
 
-    foo.remote()
+        def print_something(self):
+            print("hello there")
 
+        def update(self):
+            self.bar.update(1)
+
+    a = Actor.remote()
     mgr = tqdm_ray.instance()
     wait_for_condition(lambda: len(mgr.bar_groups) == 1)
     bar_group = list(mgr.bar_groups.values())[0]
@@ -30,6 +32,13 @@ def test_distributed_tqdm_remote():
     bar = list(bar_group.bars_by_uuid.values())[0]
     assert bar.bar.n == 42, bar.bar.n
     assert "foo" in bar.bar.desc, bar.bar.desc
+    assert not mgr.in_hidden_state
+
+    # Test stdout save/restore clearing.
+    assert mgr.num_hides == 0
+    ray.get(a.print_something.remote())
+    wait_for_condition(lambda: mgr.num_hides == 1)
+    wait_for_condition(lambda: not mgr.in_hidden_state)
 
 
 def test_distributed_tqdm_local():
