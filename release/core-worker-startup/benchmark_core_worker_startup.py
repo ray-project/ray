d426b32269f3760a43ef0ccbb75ad081d347c61a
Cade Daniel <edacih@gmail.com>
2023-03-13 17:21:40 -0700

    lint

--- release/core-worker-startup/benchmark_core_worker_startup.py
+++ release/core-worker-startup/benchmark_core_worker_startup.py
@@ -5,41 +5,43 @@ import sys
 import asyncio
 from ray.job_submission import JobSubmissionClient, JobStatus
 import random
-import os
 from dataclasses import dataclass
 from ray._private.test_utils import safe_write_to_results_json
 from collections import defaultdict
 import statistics
 import argparse
 
+
 def main(
-        num_cpus_in_cluster: int,
-        num_tasks_or_actors_per_run: int,
-        num_measurements_per_configuration: int
-    ):
-    metrics_actor_name = 'metrics_actor'
-    metrics_actor_namespace = 'metrics_actor_namespace'
-    metrics_actor = MetricsActor.options(
+    num_cpus_in_cluster: int,
+    num_tasks_or_actors_per_run: int,
+    num_measurements_per_configuration: int,
+):
+    metrics_actor_name = "metrics_actor"
+    metrics_actor_namespace = "metrics_actor_namespace"
+    metrics_actor = MetricsActor.options(  # noqa: F841
         name=metrics_actor_name,
         namespace=metrics_actor_namespace,
     ).remote(
         expected_measurements_per_test=num_measurements_per_configuration,
     )
-    
+
     run_matrix = generate_test_matrix(
         num_cpus_in_cluster=num_cpus_in_cluster,
         num_tasks_or_actors_per_run=num_tasks_or_actors_per_run,
         num_measurements_per_test=num_measurements_per_configuration,
     )
-    print(f'List of tests: {run_matrix}')
+    print(f"List of tests: {run_matrix}")
 
     for test in random.sample(list(run_matrix), k=len(run_matrix)):
         print(f"Running test {test}")
-        asyncio.run(run_and_stream_logs(
-            metrics_actor_name,
-            metrics_actor_namespace,
-            test,
-        ))
+        asyncio.run(
+            run_and_stream_logs(
+                metrics_actor_name,
+                metrics_actor_namespace,
+                test,
+            )
+        )
 
 
 @ray.remote(num_cpus=0)
@@ -49,18 +51,27 @@ class MetricsActor:
         self.expected_measurements_per_test = expected_measurements_per_test
 
     def submit(self, test_name: str, latency: float):
-        print(f'got latency {latency} s for test {test_name}')
+        print(f"got latency {latency} s for test {test_name}")
         self.measurements[test_name].append(latency)
         results = self.create_results_dict_from_measurements()
         safe_write_to_results_json(results)
 
-        assert len(self.measurements[test_name]) <= self.expected_measurements_per_test, f"Expected {self.measurements[test_name]} to not have more elements than {self.expected_measurements_per_test}"
+        assert (
+            len(self.measurements[test_name]) <= self.expected_measurements_per_test
+        ), (
+            f"Expected {self.measurements[test_name]} to not have more elements than "
+            f"{self.expected_measurements_per_test}"
+        )
 
     def create_results_dict_from_measurements(self):
         results = {}
 
         for test_name, measurements in self.measurements.items():
-            p50 = statistics.median(measurements) if len(measurements) == self.expected_measurements_per_test else -1
+            p50 = (
+                statistics.median(measurements)
+                if len(measurements) == self.expected_measurements_per_test
+                else -1
+            )
             test_summary = {
                 "p50": p50,
                 "measurements": measurements,
@@ -70,7 +81,11 @@ class MetricsActor:
         return results
 
 
-def generate_test_matrix(num_cpus_in_cluster: int, num_tasks_or_actors_per_run: int, num_measurements_per_test: int):
+def generate_test_matrix(
+    num_cpus_in_cluster: int,
+    num_tasks_or_actors_per_run: int,
+    num_measurements_per_test: int,
+):
 
     num_repeated_jobs_or_runs = num_measurements_per_test
     total_num_tasks_or_actors = num_tasks_or_actors_per_run * num_repeated_jobs_or_runs
@@ -81,7 +96,9 @@ def generate_test_matrix(num_cpus_in_cluster: int, num_tasks_or_actors_per_run: 
         for with_gpu in [True, False]:
             for with_tasks in [True, False]:
                 num_tasks_or_actors_per_job = total_num_tasks_or_actors // num_jobs
-                num_runs_per_job = num_tasks_or_actors_per_job // num_tasks_or_actors_per_run
+                num_runs_per_job = (
+                    num_tasks_or_actors_per_job // num_tasks_or_actors_per_run
+                )
 
                 test = TestConfiguration(
                     num_jobs=num_jobs,
@@ -94,7 +111,7 @@ def generate_test_matrix(num_cpus_in_cluster: int, num_tasks_or_actors_per_run: 
                     num_nodes_in_cluster=1,
                 )
                 tests.add(test)
-    
+
     return tests
 
 
@@ -113,63 +130,74 @@ class TestConfiguration:
         with_gpu_str = "with-gpu" if self.with_gpu else "without-gpu"
         executable_unit = "tasks" if self.with_tasks else "actors"
         cold_or_warm_start = "cold" if self.num_jobs > 1 else "warm"
-        single_node_or_multi_node = 'single-node' if self.num_nodes_in_cluster == 1 else 'multi-node'
-        return '_'.join([
-                f"seconds-to-{cold_or_warm_start}-start-{self.num_tasks_or_actors_per_run}-{self.expensive_import}-{executable_unit}-over-{self.num_cpus_in_cluster}-cpus",
+        single_node_or_multi_node = (
+            "single-node" if self.num_nodes_in_cluster == 1 else "multi-node"
+        )
+        return "_".join(
+            [
+                f"seconds-to-{cold_or_warm_start}-start-{self.num_tasks_or_actors_per_run}-{self.expensive_import}-{executable_unit}-over-{self.num_cpus_in_cluster}-cpus",  # noqa: E501
                 f"{with_gpu_str}",
                 f"{single_node_or_multi_node}",
-            ])
+            ]
+        )
 
-async def run_and_stream_logs(metrics_actor_name, metrics_actor_namespace, test: TestConfiguration):
-        client = JobSubmissionClient("http://127.0.0.1:8265")
-    
-        task_or_actor_arg = "--with_tasks" if test.with_tasks else "--with_actors"
-        with_gpu_arg = "--with_gpu" if test.with_gpu else "--without_gpu"
 
-        for i in range(test.num_jobs):
-            print(f"Running job {i} for {test}")
-            job_id = client.submit_job(
-                entrypoint=" ".join([
-                            f"python ./benchmark_single_configuration.py",
-                            f"--metrics_actor_name {metrics_actor_name}",
-                            f"--metrics_actor_namespace {metrics_actor_namespace}",
-                            f"--test_name {test}",
-                            f"--num_runs {test.num_runs_per_job} ",
-                            f"--num_tasks_or_actors_per_run {test.num_tasks_or_actors_per_run}",
-                            f"--num_cpus_in_cluster {test.num_cpus_in_cluster}",
-                            f"{task_or_actor_arg}",
-                            f"{with_gpu_arg}",
-                            f"--library_to_import {test.expensive_import}",
-                        ]),
-                runtime_env={"working_dir": "./"}
-            )
+async def run_and_stream_logs(
+    metrics_actor_name, metrics_actor_namespace, test: TestConfiguration
+):
+    client = JobSubmissionClient("http://127.0.0.1:8265")
 
-            try:
-                async for lines in client.tail_job_logs(job_id):
-                    print(lines, end="")
-            except KeyboardInterrupt:
-                print(f'Stopping job {job_id}')
-                client.stop_job(job_id)
-                raise
-            
-            job_status = client.get_job_status(job_id)
-            if job_status != JobStatus.SUCCEEDED:
-                raise ValueError(f'Job {job_id} was not successful; status is {job_status}')
+    task_or_actor_arg = "--with_tasks" if test.with_tasks else "--with_actors"
+    with_gpu_arg = "--with_gpu" if test.with_gpu else "--without_gpu"
+
+    for i in range(test.num_jobs):
+        print(f"Running job {i} for {test}")
+        job_id = client.submit_job(
+            entrypoint=" ".join(
+                [
+                    f"python ./benchmark_single_configuration.py",
+                    f"--metrics_actor_name {metrics_actor_name}",
+                    f"--metrics_actor_namespace {metrics_actor_namespace}",
+                    f"--test_name {test}",
+                    f"--num_runs {test.num_runs_per_job} ",
+                    f"--num_tasks_or_actors_per_run {test.num_tasks_or_actors_per_run}",
+                    f"--num_cpus_in_cluster {test.num_cpus_in_cluster}",
+                    f"{task_or_actor_arg}",
+                    f"{with_gpu_arg}",
+                    f"--library_to_import {test.expensive_import}",
+                ]
+            ),
+            runtime_env={"working_dir": "./"},
+        )
+
+        try:
+            async for lines in client.tail_job_logs(job_id):
+                print(lines, end="")
+        except KeyboardInterrupt:
+            print(f"Stopping job {job_id}")
+            client.stop_job(job_id)
+            raise
+
+        job_status = client.get_job_status(job_id)
+        if job_status != JobStatus.SUCCEEDED:
+            raise ValueError(f"Job {job_id} was not successful; status is {job_status}")
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_cpus_in_cluster', type=int, required=True)
-    parser.add_argument('--num_tasks_or_actors_per_run', type=int, required=True)
-    parser.add_argument('--num_measurements_per_configuration', type=int, required=True)
+    parser.add_argument("--num_cpus_in_cluster", type=int, required=True)
+    parser.add_argument("--num_tasks_or_actors_per_run", type=int, required=True)
+    parser.add_argument("--num_measurements_per_configuration", type=int, required=True)
 
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
-    sys.exit(main(
-        args.num_cpus_in_cluster,
-        args.num_tasks_or_actors_per_run,
-        args.num_measurements_per_configuration,
-    ))
+    sys.exit(
+        main(
+            args.num_cpus_in_cluster,
+            args.num_tasks_or_actors_per_run,
+            args.num_measurements_per_configuration,
+        )
+    )
--- release/core-worker-startup/benchmark_single_configuration.py
+++ release/core-worker-startup/benchmark_single_configuration.py
@@ -5,71 +5,81 @@ import time
 import sys
 import argparse
 
+
 @ray.remote
 class Actor:
     def run_code(self):
-        import torch
+        import torch  # noqa: F401
+
 
 @ray.remote
 def task():
-    import torch
+    import torch  # noqa: F401
+
 
 def main(
-        metrics_actor,
-        test_name: str,
-        num_runs: int,
-        num_tasks_or_actors_per_run: int,
-        num_cpus_in_cluster: int,
-        use_actors: bool,
-        with_gpu: bool
-    ):
+    metrics_actor,
+    test_name: str,
+    num_runs: int,
+    num_tasks_or_actors_per_run: int,
+    num_cpus_in_cluster: int,
+    use_actors: bool,
+    with_gpu: bool,
+):
 
     num_gpus = 0.0001 if with_gpu else 0
     num_cpus = (num_cpus_in_cluster / num_tasks_or_actors_per_run) - 0.01
 
-    print(f'Assigning each task/actor {num_cpus} num_cpus and {num_gpus} num_gpus')
+    print(f"Assigning each task/actor {num_cpus} num_cpus and {num_gpus} num_gpus")
 
     actor_with_resources = Actor.options(num_gpus=num_gpus, num_cpus=num_cpus)
     task_with_resources = task.options(num_gpus=num_gpus, num_cpus=num_cpus)
 
     def with_actors():
-        actors = [actor_with_resources.remote() for _ in range(num_tasks_or_actors_per_run)]
+        actors = [
+            actor_with_resources.remote() for _ in range(num_tasks_or_actors_per_run)
+        ]
         ray.get([actor.run_code.remote() for actor in actors])
 
     def with_tasks():
-        ray.get([task_with_resources.remote() for _ in range(num_tasks_or_actors_per_run)])
+        ray.get(
+            [task_with_resources.remote() for _ in range(num_tasks_or_actors_per_run)]
+        )
 
     func_to_measure = with_actors if use_actors else with_tasks
-    
+
     for run in range(num_runs):
-        print(f'Starting measurement for run {run}')
+        print(f"Starting measurement for run {run}")
         start = time.time()
         func_to_measure()
         dur_s = time.time() - start
         ray.get(metrics_actor.submit.remote(test_name, dur_s))
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--metrics_actor_name', type=str, required=True)
-    parser.add_argument('--metrics_actor_namespace', type=str, required=True)
-    parser.add_argument('--test_name', type=str, required=True)
-    parser.add_argument('--num_runs', type=int, required=True)
-    parser.add_argument('--num_tasks_or_actors_per_run', type=int, required=True)
-    parser.add_argument('--num_cpus_in_cluster', type=int, required=True)
-    parser.add_argument('--library_to_import', type=str, required=True, choices=['torch'])
+    parser.add_argument("--metrics_actor_name", type=str, required=True)
+    parser.add_argument("--metrics_actor_namespace", type=str, required=True)
+    parser.add_argument("--test_name", type=str, required=True)
+    parser.add_argument("--num_runs", type=int, required=True)
+    parser.add_argument("--num_tasks_or_actors_per_run", type=int, required=True)
+    parser.add_argument("--num_cpus_in_cluster", type=int, required=True)
+    parser.add_argument(
+        "--library_to_import", type=str, required=True, choices=["torch"]
+    )
 
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--with_actors', action='store_true')
-    group.add_argument('--with_tasks', action='store_true')
+    group.add_argument("--with_actors", action="store_true")
+    group.add_argument("--with_tasks", action="store_true")
 
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--with_gpu', action='store_true')
-    group.add_argument('--without_gpu', action='store_true')
+    group.add_argument("--with_gpu", action="store_true")
+    group.add_argument("--without_gpu", action="store_true")
 
     return parser.parse_args()
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = parse_args()
 
     metrics_actor = ray.get_actor(
@@ -77,12 +87,14 @@ if __name__ == '__main__':
         args.metrics_actor_namespace,
     )
 
-    sys.exit(main(
-        metrics_actor,
-        args.test_name,
-        args.num_runs,
-        args.num_tasks_or_actors_per_run,
-        args.num_cpus_in_cluster,
-        args.with_actors,
-        args.with_gpu,
-    ))
+    sys.exit(
+        main(
+            metrics_actor,
+            args.test_name,
+            args.num_runs,
+            args.num_tasks_or_actors_per_run,
+            args.num_cpus_in_cluster,
+            args.with_actors,
+            args.with_gpu,
+        )
+    )
