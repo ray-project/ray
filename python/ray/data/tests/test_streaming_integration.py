ray-project/ray

commit ad784545dbfbcbd353ec2f58be66bfe9c3636171

[Data] Add `iter_rows` to `DatasetIterator` (#33180)
The iter_rows API that HuggingfaceTrainer uses internally is not available in DatasetIterator, which falls back to the base dataset and outputs User warnings about the change to DatasetIterator.

This PR adds an iter_rows API to DatasetIterator so we don't fallback to the base dataset.

---------

Signed-off-by: amogkam <amogkamsetty@yahoo.com>

python/ray/data/_internal/dataset_iterator_impl.py
@@ -2,6 +2,10 @@ from typing import TYPE_CHECKING, Optional, Union, Iterator, Callable, Any
 import time
 import warnings
 
+from ray.air.util.data_batch_conversion import BlockFormat
+from ray.data.block import BlockAccessor, T
+from ray.data.context import DatasetContext
+from ray.data.row import TableRow
 from ray.data.block import DataBatch
 from ray.data.dataset_iterator import DatasetIterator
 from ray.data._internal.block_batching import batch_block_refs
@@ -52,6 +56,35 @@ class DatasetIteratorImpl(DatasetIterator):
 
         stats.iter_total_s.add(time.perf_counter() - time_start)
 
+    def iter_rows(self, *, prefetch_blocks: int = 0) -> Iterator[Union[T, TableRow]]:
+        # During row-based ops, we also choose a batch format that lines up with the
+        # current dataset format in order to eliminate unnecessary copies and type
+        # conversions.
+        ctx = DatasetContext.get_current()
+        if ctx.use_streaming_executor:
+            # TODO: calling dataset_format() triggers bulk execution.
+            batch_format = "default"
+        else:
+            try:
+                dataset_format = self._base_dataset.dataset_format()
+            except ValueError:
+                # Dataset is empty or cleared, so fall back to "default".
+                batch_format = "default"
+            else:
+                batch_format = (
+                    "pyarrow"
+                    if dataset_format == BlockFormat.ARROW
+                    else "pandas"
+                    if dataset_format == BlockFormat.PANDAS
+                    else "default"
+                )
+        for batch in self.iter_batches(
+            batch_size=None, prefetch_blocks=prefetch_blocks, batch_format=batch_format
+        ):
+            batch = BlockAccessor.for_block(BlockAccessor.batch_to_block(batch))
+            for row in batch.iter_rows():
+                yield row
+
     def stats(self) -> str:
         return self._base_dataset.stats()
 
@@ -60,16 +93,20 @@ class DatasetIteratorImpl(DatasetIterator):
 
     def __getattr__(self, name):
         if name == "_base_dataset":
-            return None
+            raise AttributeError()
 
-        # Warning for backwards compatibility. TODO: remove this method in 2.5.
-        warnings.warn(
-            "session.get_dataset_shard returns a ray.data.DatasetIterator "
-            "instead of a Dataset/DatasetPipeline as of Ray v2.3. "
-            "Use iter_torch_batches(), to_tf(), or iter_batches() to "
-            "iterate over one epoch. See "
-            "https://docs.ray.io/en/latest/data/api/dataset_iterator.html "
-            "for full DatasetIterator docs."
-        )
+        if hasattr(self._base_dataset, name):
+            # Warning for backwards compatibility. TODO: remove this method in 2.5.
+            warnings.warn(
+                "session.get_dataset_shard returns a ray.data.DatasetIterator "
+                "instead of a Dataset/DatasetPipeline as of Ray v2.3. "
+                "Use iter_torch_batches(), to_tf(), or iter_batches() to "
+                "iterate over one epoch. See "
+                "https://docs.ray.io/en/latest/data/api/dataset_iterator.html "
+                "for full DatasetIterator docs.",
+                stacklevel=4,
+            )
 
-        return getattr(self._base_dataset, name)
+            return getattr(self._base_dataset, name)
+        else:
+            return super().__getattr__(name)

python/ray/data/_internal/pipelined_dataset_iterator.py
@@ -1,7 +1,8 @@
 from typing import TYPE_CHECKING, Any, Callable, Optional, Union, Iterator
 import warnings
 
-from ray.data.block import DataBatch
+from ray.data.block import DataBatch, T
+from ray.data.row import TableRow
 from ray.data.dataset_iterator import DatasetIterator
 
 if TYPE_CHECKING:
@@ -49,6 +50,12 @@ class PipelinedDatasetIterator(DatasetIterator):
             _collate_fn=_collate_fn,
         )
 
+    def iter_rows(self, *, prefetch_blocks: int = 0) -> Iterator[Union[T, TableRow]]:
+        ds = self._get_next_dataset()
+        return ds.iter_rows(
+            prefetch_blocks=prefetch_blocks,
+        )
+
     def stats(self) -> str:
         return self._base_dataset_pipeline.stats()
 
@@ -57,16 +64,19 @@ class PipelinedDatasetIterator(DatasetIterator):
 
     def __getattr__(self, name):
         if name == "_base_dataset_pipeline":
-            return None
+            raise AttributeError
 
-        # Warning for backwards compatibility. TODO: remove this method in 2.5.
-        warnings.warn(
-            "session.get_dataset_shard returns a ray.data.DatasetIterator "
-            "instead of a Dataset/DatasetPipeline as of Ray v2.3. "
-            "Use iter_torch_batches(), to_tf(), or iter_batches() to "
-            "iterate over one epoch. See "
-            "https://docs.ray.io/en/latest/data/api/dataset_iterator.html "
-            "for full DatasetIterator docs."
-        )
+        if hasattr(self._base_dataset_pipeline, name):
+            # Warning for backwards compatibility. TODO: remove this method in 2.5.
+            warnings.warn(
+                "session.get_dataset_shard returns a ray.data.DatasetIterator "
+                "instead of a Dataset/DatasetPipeline as of Ray v2.3. "
+                "Use iter_torch_batches(), to_tf(), or iter_batches() to "
+                "iterate over one epoch. See "
+                "https://docs.ray.io/en/latest/data/api/dataset_iterator.html "
+                "for full DatasetIterator docs."
+            )
 
-        return getattr(self._base_dataset_pipeline, name)
+            return getattr(self._base_dataset_pipeline, name)
+        else:
+            return super().__getattr__(name)

python/ray/data/dataset_iterator.py
@@ -3,7 +3,8 @@ import numpy as np
 import sys
 from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Union, Iterator
 
-from ray.data.block import DataBatch
+from ray.data.block import DataBatch, T
+from ray.data.row import TableRow
 from ray.util.annotations import PublicAPI
 from ray.data._internal.util import _is_tensor_schema
 
@@ -104,6 +105,32 @@ class DatasetIterator(abc.ABC):
         raise NotImplementedError
 
     @abc.abstractmethod
+    def iter_rows(self, *, prefetch_blocks: int = 0) -> Iterator[Union[T, TableRow]]:
+        """Return a local row iterator over the dataset.
+
+        If the dataset is a tabular dataset (Arrow/Pandas blocks), dict-like mappings
+        :py:class:`~ray.data.row.TableRow` are yielded for each row by the iterator.
+        If the dataset is not tabular, the raw row is yielded.
+
+        Examples:
+            >>> import ray
+            >>> dataset = ray.data.range(10)
+            >>> next(iter(dataset.iterator().iter_rows()))
+            0
+
+        Time complexity: O(1)
+
+        Args:
+            prefetch_blocks: The number of blocks to prefetch ahead of the
+                current block during the scan.
+
+        Returns:
+            An iterator over rows of the dataset.
+        """
+
+        raise NotImplementedError
+
+    @abc.abstractmethod
     def stats(self) -> str:
         """Returns a string containing execution timing information."""
         raise NotImplementedError
@@ -136,13 +163,10 @@ class DatasetIterator(abc.ABC):
 
         Examples:
             >>> import ray
-            >>> for batch in ray.data.range( # doctest: +SKIP
-            ...     12,
-            ... ).iterator().iter_torch_batches(batch_size=4):
-            ...     print(batch.shape) # doctest: +SKIP
-            torch.Size([4, 1])
-            torch.Size([4, 1])
-            torch.Size([4, 1])
+            >>> for row in ray.data.range(
+            ...     1000000
+            ... ).iterator().iter_rows(): # doctest: +SKIP
+            ...     print(row) # doctest: +SKIP
 
         Time complexity: O(1)
 

python/ray/data/dataset.py
@@ -2895,33 +2895,8 @@ class Dataset(Generic[T]):
         Returns:
             A local iterator over the entire dataset.
         """
-        # During row-based ops, we also choose a batch format that lines up with the
-        # current dataset format in order to eliminate unnecessary copies and type
-        # conversions.
-        ctx = DatasetContext.get_current()
-        if ctx.use_streaming_executor:
-            # TODO: calling dataset_format() triggers bulk execution.
-            batch_format = "default"
-        else:
-            try:
-                dataset_format = self.dataset_format()
-            except ValueError:
-                # Dataset is empty or cleared, so fall back to "default".
-                batch_format = "default"
-            else:
-                batch_format = (
-                    "pyarrow"
-                    if dataset_format == BlockFormat.ARROW
-                    else "pandas"
-                    if dataset_format == BlockFormat.PANDAS
-                    else "default"
-                )
-        for batch in self.iter_batches(
-            batch_size=None, prefetch_blocks=prefetch_blocks, batch_format=batch_format
-        ):
-            batch = BlockAccessor.for_block(BlockAccessor.batch_to_block(batch))
-            for row in batch.iter_rows():
-                yield row
+
+        return self.iterator().iter_rows(prefetch_blocks=prefetch_blocks)
 
     @ConsumptionAPI
     def iter_batches(

python/ray/data/tests/test_dataset_iterator.py
@@ -25,7 +25,7 @@ def build_model():
 def test_basic_dataset(ray_start_regular_shared):
     ds = ray.data.range(100)
     it = ds.iterator()
-    for _ in range(3):
+    for _ in range(2):
         result = []
         for batch in it.iter_batches():
             result += batch
@@ -37,13 +37,40 @@ def test_basic_dataset(ray_start_regular_shared):
     # assert it.stats() == ds.stats()
 
 
+def test_basic_dataset_iter_rows(ray_start_regular_shared):
+    ds = ray.data.range(100)
+    it = ds.iterator()
+    for _ in range(2):
+        result = []
+        for row in it.iter_rows():
+            result.append(row)
+        assert result == list(range(100))
+
+    # TODO(swang): This check currently fails nondeterministically because
+    # stats are stored in an actor.
+    # https://github.com/ray-project/ray/issues/31571
+    # assert it.stats() == ds.stats()
+
+
 def test_basic_dataset_pipeline(ray_start_regular_shared):
     ds = ray.data.range(100).window(bytes_per_window=1).repeat()
     it = ds.iterator()
-    for _ in range(3):
+    for _ in range(2):
         result = []
         for batch in it.iter_batches():
             result += batch
+        assert result == list(range(100))
+
+    assert it.stats() == ds.stats()
+
+
+def test_basic_dataset_pipeline_iter_rows(ray_start_regular_shared):
+    ds = ray.data.range(100).window(bytes_per_window=1).repeat()
+    it = ds.iterator()
+    for _ in range(2):
+        result = []
+        for row in it.iter_rows():
+            result.append(row)
         assert result == list(range(100))
 
     assert it.stats() == ds.stats()

commit a3b89f679bb2f9295551b2ea8473efa6281da084

[Datasets] Refactor iter_rows() for DatasetIterator classes (#33257)
This is to fix CI test failure introduced in https://github.com/ray-project/ray/pull/33180 ([example of buildkite failure](https://buildkite.com/ray-project/oss-ci-build-branch/builds/2690#0186da0a-17c7-40dd-a43c-1de36c4894f4)). We need to add `iter_rows()` method for StreamSplitDatasetIterator as well.


<img width="1848" alt="Screen Shot 2023-03-13 at 11 24 05 AM" src="https://user-images.githubusercontent.com/4629931/224795253-ecf4e362-4253-4d60-80ff-f87e138ef773.png">

python/ray/data/_internal/dataset_iterator_impl.py
@@ -1,11 +1,9 @@
 from typing import TYPE_CHECKING, Optional, Union, Iterator, Callable, Any
 import time
 import warnings
+import sys
 
-from ray.air.util.data_batch_conversion import BlockFormat
-from ray.data.block import BlockAccessor, T
-from ray.data.context import DatasetContext
-from ray.data.row import TableRow
+from ray.data._internal.util import _default_batch_format
 from ray.data.block import DataBatch
 from ray.data.dataset_iterator import DatasetIterator
 from ray.data._internal.block_batching import batch_block_refs
@@ -13,6 +11,11 @@ from ray.data._internal.block_batching import batch_block_refs
 if TYPE_CHECKING:
     import pyarrow
     from ray.data import Dataset
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
 
 
 class DatasetIteratorImpl(DatasetIterator):
@@ -56,40 +59,14 @@ class DatasetIteratorImpl(DatasetIterator):
 
         stats.iter_total_s.add(time.perf_counter() - time_start)
 
-    def iter_rows(self, *, prefetch_blocks: int = 0) -> Iterator[Union[T, TableRow]]:
-        # During row-based ops, we also choose a batch format that lines up with the
-        # current dataset format in order to eliminate unnecessary copies and type
-        # conversions.
-        ctx = DatasetContext.get_current()
-        if ctx.use_streaming_executor:
-            # TODO: calling dataset_format() triggers bulk execution.
-            batch_format = "default"
-        else:
-            try:
-                dataset_format = self._base_dataset.dataset_format()
-            except ValueError:
-                # Dataset is empty or cleared, so fall back to "default".
-                batch_format = "default"
-            else:
-                batch_format = (
-                    "pyarrow"
-                    if dataset_format == BlockFormat.ARROW
-                    else "pandas"
-                    if dataset_format == BlockFormat.PANDAS
-                    else "default"
-                )
-        for batch in self.iter_batches(
-            batch_size=None, prefetch_blocks=prefetch_blocks, batch_format=batch_format
-        ):
-            batch = BlockAccessor.for_block(BlockAccessor.batch_to_block(batch))
-            for row in batch.iter_rows():
-                yield row
-
     def stats(self) -> str:
         return self._base_dataset.stats()
 
     def schema(self) -> Union[type, "pyarrow.lib.Schema"]:
         return self._base_dataset.schema()
+
+    def _default_batch_format(self) -> Literal["default", "pandas", "pyarrow", "numpy"]:
+        return _default_batch_format(self._base_dataset)
 
     def __getattr__(self, name):
         if name == "_base_dataset":

python/ray/data/_internal/pipelined_dataset_iterator.py
@@ -1,8 +1,7 @@
 from typing import TYPE_CHECKING, Any, Callable, Optional, Union, Iterator
 import warnings
 
-from ray.data.block import DataBatch, T
-from ray.data.row import TableRow
+from ray.data.block import DataBatch
 from ray.data.dataset_iterator import DatasetIterator
 
 if TYPE_CHECKING:
@@ -48,12 +47,6 @@ class PipelinedDatasetIterator(DatasetIterator):
             local_shuffle_buffer_size=local_shuffle_buffer_size,
             local_shuffle_seed=local_shuffle_seed,
             _collate_fn=_collate_fn,
-        )
-
-    def iter_rows(self, *, prefetch_blocks: int = 0) -> Iterator[Union[T, TableRow]]:
-        ds = self._get_next_dataset()
-        return ds.iter_rows(
-            prefetch_blocks=prefetch_blocks,
         )
 
     def stats(self) -> str:

python/ray/data/_internal/stream_split_dataset_iterator.py
@@ -14,6 +14,7 @@ from typing import (
 )
 
 import ray
+from ray.data._internal.util import _default_batch_format
 
 from ray.data.dataset_iterator import DatasetIterator
 from ray.data.block import Block, DataBatch
@@ -122,6 +123,9 @@ class StreamSplitDatasetIterator(DatasetIterator):
     def schema(self) -> Union[type, "pyarrow.lib.Schema"]:
         """Implements DatasetIterator."""
         return self._base_dataset.schema()
+
+    def _default_batch_format(self) -> Literal["default", "pandas", "pyarrow", "numpy"]:
+        return _default_batch_format(self._base_dataset)
 
 
 @ray.remote(num_cpus=0)

python/ray/data/_internal/util.py
@@ -9,10 +9,17 @@ import numpy as np
 
 import ray
 from ray.air.constants import TENSOR_COLUMN_NAME
+from ray.air.util.data_batch_conversion import BlockFormat
 from ray.data.context import DatasetContext
 from ray._private.utils import _get_pyarrow_version
 
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
 if TYPE_CHECKING:
+    from ray.data.dataset import Dataset
     from ray.data.datasource import Reader
     from ray.util.placement_group import PlacementGroup
 
@@ -397,3 +404,28 @@ def _split_list(arr: List[Any], num_splits: int) -> List[List[Any]]:
         arr[i * q + min(i, r) : (i + 1) * q + min(i + 1, r)] for i in range(num_splits)
     ]
     return splits
+
+
+def _default_batch_format(
+    ds: "Dataset",
+) -> Literal["default", "pandas", "pyarrow", "numpy"]:
+    """Get the best batch format that lines up with the dataset format."""
+    ctx = DatasetContext.get_current()
+    if ctx.use_streaming_executor:
+        # TODO: calling dataset_format() triggers bulk execution.
+        batch_format = "default"
+    else:
+        try:
+            dataset_format = ds.dataset_format()
+        except ValueError:
+            # Dataset is empty or cleared, so fall back to "default".
+            batch_format = "default"
+        else:
+            batch_format = (
+                "pyarrow"
+                if dataset_format == BlockFormat.ARROW
+                else "pandas"
+                if dataset_format == BlockFormat.PANDAS
+                else "default"
+            )
+    return batch_format

python/ray/data/dataset_iterator.py
@@ -3,7 +3,7 @@ import numpy as np
 import sys
 from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Union, Iterator
 
-from ray.data.block import DataBatch, T
+from ray.data.block import BlockAccessor, DataBatch, T
 from ray.data.row import TableRow
 from ray.util.annotations import PublicAPI
 from ray.data._internal.util import _is_tensor_schema
@@ -104,7 +104,6 @@ class DatasetIterator(abc.ABC):
         """
         raise NotImplementedError
 
-    @abc.abstractmethod
     def iter_rows(self, *, prefetch_blocks: int = 0) -> Iterator[Union[T, TableRow]]:
         """Return a local row iterator over the dataset.
 
@@ -127,8 +126,13 @@ class DatasetIterator(abc.ABC):
         Returns:
             An iterator over rows of the dataset.
         """
-
-        raise NotImplementedError
+        target_format = self._default_batch_format()
+        for batch in self.iter_batches(
+            batch_size=None, prefetch_blocks=prefetch_blocks, batch_format=target_format
+        ):
+            batch = BlockAccessor.for_block(BlockAccessor.batch_to_block(batch))
+            for row in batch.iter_rows():
+                yield row
 
     @abc.abstractmethod
     def stats(self) -> str:
@@ -700,3 +704,11 @@ class DatasetIterator(abc.ABC):
         if schema is None or isinstance(schema, type):
             return False
         return _is_tensor_schema(schema.names)
+
+    def _default_batch_format(self) -> Literal["default", "pandas", "pyarrow", "numpy"]:
+        """Returns the best batch format that lines up with the dataset format.
+
+        NOTE: Return "default" here. Subclass can override this method to decide best
+        batch format.
+        """
+        return "default"

python/ray/data/tests/test_streaming_integration.py
@@ -1,3 +1,4 @@
+import itertools
 import pytest
 import threading
 import time
@@ -89,12 +90,16 @@ def test_output_split_e2e(ray_start_10_cpus_shared):
 
 
 def test_streaming_split_e2e(ray_start_10_cpus_shared):
-    def get_lengths(*iterators):
+    def get_lengths(*iterators, use_iter_batches=True):
         lengths = []
         for it in iterators:
             x = 0
-            for batch in it.iter_batches():
-                x += len(batch)
+            if use_iter_batches:
+                for batch in it.iter_batches():
+                    x += len(batch)
+            else:
+                for _ in it.iter_rows():
+                    x += 1
             lengths.append(x)
         lengths.sort()
         return lengths
@@ -124,13 +129,15 @@ def test_streaming_split_e2e(ray_start_10_cpus_shared):
     assert lengths == [0, 1], lengths
 
     ds = ray.data.range(1000, parallelism=10)
-    i1, i2, i3 = ds.streaming_split(3, equal=True)
-    lengths = get_lengths(i1, i2, i3)
-    assert lengths == [333, 333, 333], lengths
-
-    i1, i2, i3 = ds.streaming_split(3, equal=False)
-    lengths = get_lengths(i1, i2, i3)
-    assert lengths == [300, 300, 400], lengths
+    for equal_split, use_iter_batches in itertools.product(
+        [True, False], [True, False]
+    ):
+        i1, i2, i3 = ds.streaming_split(3, equal=equal_split)
+        lengths = get_lengths(i1, i2, i3, use_iter_batches=use_iter_batches)
+        if equal_split:
+            assert lengths == [333, 333, 333], lengths
+        else:
+            assert lengths == [300, 300, 400], lengths
 
 
 def test_e2e_option_propagation(ray_start_10_cpus_shared, restore_dataset_context):