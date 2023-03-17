f2c1d0cac69f8b81cdedadc9669037d529c8ea10
Scott Lee <sjl@anyscale.com>
2023-03-17 12:34:36 -0700

    lazy import tensorflow_metadata
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- python/ray/data/datasource/tfrecords_datasource.py
+++ python/ray/data/datasource/tfrecords_datasource.py
@@ -143,7 +143,6 @@ def _get_feature_value(
     schema_feature_type: Optional["schema_pb2.FeatureType"] = None,
 ) -> "pyarrow.Array":
     import pyarrow as pa
-    from tensorflow_metadata.proto.v0 import schema_pb2
 
     underlying_feature_type = {
         "bytes": feature.HasField("bytes_list"),
@@ -156,7 +155,8 @@ def _get_feature_value(
     assert sum(bool(value) for value in underlying_feature_type.values()) <= 1
 
     if schema_feature_type is not None:
-        # If a schema is specified, compare to the
+        from tensorflow_metadata.proto.v0 import schema_pb2
+        # If a schema is specified, compare to the underlying type
         specified_feature_type = {
             "bytes": schema_feature_type == schema_pb2.FeatureType.BYTES,
             "float": schema_feature_type == schema_pb2.FeatureType.FLOAT,
@@ -207,7 +207,6 @@ def _value_to_feature(
 ) -> "tf.train.Feature":
     import tensorflow as tf
     import pyarrow as pa
-    from tensorflow_metadata.proto.v0 import schema_pb2
 
     if isinstance(value, pa.ListScalar):
         # Use the underlying type of the ListScalar's value in
@@ -230,6 +229,7 @@ def _value_to_feature(
     assert sum(bool(value) for value in underlying_value_type.values()) <= 1
 
     if schema_feature_type is not None:
+        from tensorflow_metadata.proto.v0 import schema_pb2
         specified_feature_type = {
             "bytes": schema_feature_type == schema_pb2.FeatureType.BYTES,
             "float": schema_feature_type == schema_pb2.FeatureType.FLOAT,
