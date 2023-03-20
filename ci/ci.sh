5c17ef925369a5d1021e5ff3352fe6235e941c9b
Cuong Nguyen <can@anyscale.com>
2023-03-20 09:32:55 -0700

    Improve wheel commit validation error message
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- ci/ci.sh
+++ ci/ci.sh
@@ -418,7 +418,7 @@ validate_wheels_commit_str() {
     WHL_COMMIT=$(unzip -p "$whl" "*ray/__init__.py" | grep "^__commit__" | awk -F'"' '{print $2}')
 
     if [ "${WHL_COMMIT}" != "${EXPECTED_COMMIT}" ]; then
-      echo "Error: Observed wheel commit (${WHL_COMMIT}) is not expected commit (${EXPECTED_COMMIT}). Aborting."
+      echo "Wheel ${basename} has incorrect commit: (${WHL_COMMIT}) is not expected commit (${EXPECTED_COMMIT}). Aborting."
       exit 1
     fi
 
