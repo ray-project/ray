b8ead6d7b827d013518ae8fc7954b3bd09f0ada1
Scott Lee <sjl@anyscale.com>
2023-03-21 16:41:52 -0700

    double bracket
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- ci/run/run_bazel_test_with_sharding.sh
+++ ci/run/run_bazel_test_with_sharding.sh
@@ -24,7 +24,7 @@ done
 
 
 SHARD=$(python ./ci/run/bazel_sharding/bazel_sharding.py --exclude_manual --index "${BUILDKITE_PARALLEL_JOB}" --count "${BUILDKITE_PARALLEL_JOB_COUNT}" --tag_filters="$test_tag_filters" "${targets[@]}")
-if [ -z "$SHARD" ]
+if [[ -z "$SHARD" ]]
 # if no targets are assigned to this shard, skip bazel test run
 then
     echo "$SHARD"
