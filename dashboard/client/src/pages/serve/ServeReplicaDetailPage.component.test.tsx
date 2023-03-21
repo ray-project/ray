0be747d2a3a1742b72f12214df6fb38ff47a1476
Alan Guo <aguo@anyscale.com>
2023-03-21 13:30:44 -0700

    fix lint
    Signed-off-by: Alan Guo <aguo@anyscale.com>

--- dashboard/client/src/pages/serve/ServeReplicaDetailPage.component.test.tsx
+++ dashboard/client/src/pages/serve/ServeReplicaDetailPage.component.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import React from "react";
 import { useParams } from "react-router-dom";
 import { getServeApplications } from "../../service/serve";
