930dbadc830b5ba2125310875c41c33f78f02aa2
SangBin Cho <rkooo567@gmail.com>
2023-03-22 16:49:29 -0700

    [Dashboard] Better support per worker gpu usage from the cluster view.  (#33515)
    Signed-off-by: SangBin Cho <rkooo567@gmail.com>
    
    I manually tested. Please let me know if you have good ideas to unit test this.
    
    Also cc @alanwguo I think we may want to unit test these components... Is it possible for you or Chao to take it?
    
    After we fixed the issue from the infra where per worker GPU usage is not recorded, it seems like per worker GPU stat is already working.
    
    However, I found several problems from existing code, and this PR fixes issues. It includes;
    
    Right now, instead of gpu.index, it uses a random arbitrary index. I fixed this issue so that the GPU index matches the output of gpustat (i.e., nvidia-smi)
    Per worker GPU usage shows the logical GPU allocation. I fixed it so that it will now display the physical usage (which is consistent with the node row). However, GPUstat cannot figure out the "per proc" GPU usage, meaning the GPU usage per proc will be identical to the GPU usage per node for a given index (device).
    
    Signed-off-by: Jack He <jackhe2345@gmail.com>

--- dashboard/client/src/pages/node/GPUColumn.tsx
+++ dashboard/client/src/pages/node/GPUColumn.tsx
@@ -1,37 +1,19 @@
-import { Box, Tooltip, Typography } from "@material-ui/core";
+import { Box, makeStyles, Tooltip, Typography } from "@material-ui/core";
 import React from "react";
 import { RightPaddedTypography } from "../../common/CustomTypography";
 import UsageBar from "../../common/UsageBar";
 import { GPUStats, NodeDetail } from "../../type/node";
-import { ResourceSlot, Worker } from "../../type/worker";
+import { Worker } from "../../type/worker";
 
-export const GPU_COL_WIDTH = 120;
-
-type WorkerGPUEntryProps = {
-  resourceSlot: ResourceSlot;
-};
-
-export const WorkerGPUEntry: React.FC<WorkerGPUEntryProps> = ({
-  resourceSlot,
-}) => {
-  const { allocation, slot } = resourceSlot;
-  // This is a bit of  a dirty hack . For some reason, the slot GPU slot
-  // 0 as assigned always shows up as undefined in the API response.
-  // There are other times, such as a partial allocation, where we truly don't
-  // know the slot, however this will just plug the hole of 0s coming through
-  // as undefined. I have not been able to figure out the root cause.
-  const slotMsg =
-    allocation >= 1 && slot === undefined
-      ? "0"
-      : slot === undefined
-      ? "?"
-      : slot.toString();
-  return (
-    <Typography variant="body1">
-      [{slotMsg}]: {allocation}
-    </Typography>
-  );
-};
+const useStyles = makeStyles((theme) => ({
+  gpuColumn: {
+    minWidth: 120,
+  },
+  box: {
+    display: "flex",
+    minWidth: 120,
+  },
+}));
 
 export type NodeGPUEntryProps = {
   slot: number;
@@ -39,8 +21,9 @@ export type NodeGPUEntryProps = {
 };
 
 export const NodeGPUEntry: React.FC<NodeGPUEntryProps> = ({ gpu, slot }) => {
+  const classes = useStyles();
   return (
-    <Box display="flex" style={{ minWidth: GPU_COL_WIDTH }}>
+    <Box className={classes.box}>
       <Tooltip title={gpu.name}>
         <RightPaddedTypography variant="body1">[{slot}]:</RightPaddedTypography>
       </Tooltip>
@@ -59,11 +42,12 @@ export const NodeGPUEntry: React.FC<NodeGPUEntryProps> = ({ gpu, slot }) => {
 };
 
 export const NodeGPUView = ({ node }: { node: NodeDetail }) => {
+  const classes = useStyles();
   return (
-    <div style={{ minWidth: GPU_COL_WIDTH }}>
+    <div className={classes.gpuColumn}>
       {node.gpus !== undefined && node.gpus.length !== 0 ? (
         node.gpus.map((gpu, i) => (
-          <NodeGPUEntry key={gpu.uuid} gpu={gpu} slot={i} />
+          <NodeGPUEntry key={gpu.uuid} gpu={gpu} slot={gpu.index} />
         ))
       ) : (
         <Typography color="textSecondary" component="span" variant="inherit">
@@ -74,32 +58,31 @@ export const NodeGPUView = ({ node }: { node: NodeDetail }) => {
   );
 };
 
-export const WorkerGPU = ({ worker }: { worker: Worker }) => {
-  const workerRes = worker.coreWorkerStats[0]?.usedResources;
-  const workerUsedGPUResources = workerRes?.["GPU"];
-  let message;
-  if (workerUsedGPUResources === undefined) {
-    message = (
-      <Typography color="textSecondary" component="span" variant="inherit">
-        N/A
-      </Typography>
-    );
-  } else {
-    message = workerUsedGPUResources.resourceSlots
-      .sort((slot1, slot2) => {
-        if (slot1.slot === undefined && slot2.slot === undefined) {
-          return 0;
-        } else if (slot1.slot === undefined) {
-          return 1;
-        } else if (slot2.slot === undefined) {
-          return -1;
-        } else {
-          return slot1.slot - slot2.slot;
-        }
-      })
-      .map((resourceSlot) => (
-        <WorkerGPUEntry key={resourceSlot.slot} resourceSlot={resourceSlot} />
-      ));
-  }
-  return <div style={{ minWidth: 60 }}>{message}</div>;
+export const WorkerGpuRow = ({
+  worker,
+  node,
+}: {
+  worker: Worker;
+  node: NodeDetail;
+}) => {
+  const classes = useStyles();
+  const workerGPUEntries = (node.gpus ?? [])
+    .map((gpu, i) => {
+      const process = gpu.processes?.find(
+        (process) => process.pid === worker.pid,
+      );
+      if (!process) {
+        return undefined;
+      }
+      return <NodeGPUEntry key={gpu.uuid} gpu={gpu} slot={gpu.index} />;
+    })
+    .filter((entry) => entry !== undefined);
+
+  return workerGPUEntries.length === 0 ? (
+    <Typography color="textSecondary" component="span" variant="inherit">
+      N/A
+    </Typography>
+  ) : (
+    <div className={classes.gpuColumn}>{workerGPUEntries}</div>
+  );
 };
