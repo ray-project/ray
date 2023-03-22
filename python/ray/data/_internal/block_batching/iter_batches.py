3cafc47d9c99910516126c6409f310467c1affba
amogkam <amogkamsetty@yahoo.com>
2023-03-22 14:30:35 -0700

    wip
    Signed-off-by: amogkam <amogkamsetty@yahoo.com>

---
+++ python/ray/data/_internal/block_batching/iter_batches.py
@@ -0,0 +1,260 @@
+import random
+from typing import Any, Callable, Iterator, List, Optional, Tuple
+
+from ray.types import ObjectRef
+from ray.data.block import Block, BlockMetadata, BlockAccessor, DataBatch
+from ray.data._internal.block_batching.interfaces import Batch, LogicalBatch, BlockPrefetcher
+from ray.data._internal.delegating_block_builder import DelegatingBlockBuilder
+from ray.data._internal.memory_tracing import trace_deallocation
+
+def _bundle_block_refs_to_logical_batches(
+    block_ref_iterator: Iterator[Tuple[ObjectRef[Block], BlockMetadata]],
+    batch_size: Optional[int],
+    drop_last: bool = False,
+) -> Iterator[LogicalBatch]:
+    """Given an iterator of block object references, and their corresponding metadata,
+    bundles the block object references into groups of the provided `batch_size`.
+
+    The output iterator returns an iterator over LogicalBatch objects.
+
+    This function does not do any slicing or creation of actual batch objects.
+    """
+    batch_buffer: List[ObjectRef[Block]] = []
+    buffer_size = 0
+    starting_index = 0
+
+    global_index = 0
+    num_rows_in_last_block = 0
+
+    if batch_size is None:
+        for block_ref, metadata in block_ref_iterator:
+            yield LogicalBatch(global_index, [block_ref], 0, None, metadata.num_rows)
+            global_index += 1
+    else:
+        while True:
+            if buffer_size < batch_size:
+                # Pull next block from iterator if current buffer is not enough to fill
+                # a batch.
+                try:
+                    block_ref, metadata = next(block_ref_iterator)
+                except StopIteration:
+                    break
+                batch_buffer.append(block_ref)
+                buffer_size += metadata.num_rows
+                num_rows_in_last_block = metadata.num_rows
+
+            if buffer_size == batch_size:
+                # If equal to batch size, then yield the full buffer.
+                yield LogicalBatch(
+                    global_index, batch_buffer, starting_index, None, buffer_size
+                )
+                batch_buffer = []
+                buffer_size = 0
+                starting_index = 0
+                num_rows_in_last_block = 0
+                global_index += 1
+
+            if buffer_size > batch_size:
+                # If current buffer is greater than batch size, then yield part of the
+                # buffer, and carryover the remainder to the next batch.
+                num_rows_to_leave_behind = buffer_size - batch_size
+                ending_index = num_rows_in_last_block - num_rows_to_leave_behind
+                assert ending_index > 0, ending_index
+                yield LogicalBatch(
+                    global_index, batch_buffer, starting_index, ending_index, batch_size
+                )
+                global_index += 1
+                # Carryover to next batch.
+                batch_buffer = [batch_buffer[-1]]
+                buffer_size = num_rows_to_leave_behind
+                starting_index = ending_index
+
+    # Yield any leftover batches if necessary.
+    if buffer_size > 0 and not drop_last:
+        assert buffer_size < batch_size
+        yield LogicalBatch(
+            global_index, batch_buffer, starting_index, None, buffer_size
+        )
+        global_index += 1
+
+def _local_shuffle_logical_batches(
+    logical_batch_iterator: Iterator[LogicalBatch],
+    shuffle_buffer_min_size: int,
+    shuffle_seed: Optional[int] = None,
+) -> Iterator[LogicalBatch]:
+    """Shuffles the logical batch iterator using a buffer of the provided size."""
+
+    if shuffle_seed is not None:
+        random.seed(shuffle_seed)
+
+    shuffle_buffer: List[LogicalBatch] = []
+    shuffle_buffer_size = 0
+    global_counter = 0
+
+    for logical_batch in logical_batch_iterator:
+        shuffle_buffer.append(logical_batch)
+        shuffle_buffer_size += logical_batch.num_rows
+
+        while shuffle_buffer_size >= shuffle_buffer_min_size:
+            output_batch = shuffle_buffer.pop(
+                random.randint(0, len(shuffle_buffer) - 1)
+            )
+            output_batch.batch_idx = global_counter
+            yield output_batch
+            shuffle_buffer_size -= output_batch.num_rows
+            global_counter += 1
+
+    # Yield any leftover.
+    while len(shuffle_buffer) > 0:
+        output_batch = shuffle_buffer.pop(random.randint(0, len(shuffle_buffer) - 1))
+        output_batch.batch_idx = global_counter
+        yield output_batch
+        global_counter += 1
+
+def _prefetch_batches_locally(
+    logical_batch_iter: Iterator[LogicalBatch],
+    prefetcher: BlockPrefetcher,
+    num_batches_to_prefetch: int,
+) -> Iterator[LogicalBatch]:
+    """Given an iterator of logical batches, returns an iterator over the same logical
+    batches, while prefetching `num_batches_to_prefetch` batches in advance.
+
+    Args:
+        logical_batch_iter: An iterator over logical batches.
+        prefetcher: The prefetcher to use.
+        num_batches_to_prefetch: The number of batches to prefetch ahead of the
+            current batch during the scan.
+    """
+
+    def get_next_batches() -> Iterator[List[LogicalBatch]]:
+        """Return lists of logical batches corresponding to `num_batches_to_prefetch`"""
+        next_batches = []
+        while True:
+            try:
+                next_batches.append(next(logical_batch_iter))
+                if len(next_batches) == num_batches_to_prefetch:
+                    yield next_batches
+                    next_batches = []
+            except StopIteration:
+                break
+
+        if len(next_batches) > 0:
+            yield next_batches
+
+    # Fetch the initial set of batches.
+    batch_iterator = get_next_batches()
+    try:
+        batches = next(batch_iterator)
+    except StopIteration:
+        return
+
+    block_refs = [block_ref for batch in batches for block_ref in batch.block_refs]
+    prefetcher.prefetch_blocks(block_refs)
+
+    for next_batches in batch_iterator:
+        # Prefetch the next batches.
+        block_refs = [
+            block_ref for batch in next_batches for block_ref in batch.block_refs
+        ]
+        prefetcher.prefetch_blocks(block_refs)
+
+        for batch in batches:
+            yield batch
+
+        batches = next_batches
+
+    # Yield the final set of batches.
+    for batch in batches:
+        yield batch
+
+def _construct_batch_from_logical_batch(
+    resolved_logical_batch_iter: Iterator[LogicalBatch],
+    ensure_copy: bool = False,
+) -> Iterator[Tuple[int, Block]]:
+    """Given an iterator over logical batches, returns an iterator over actual
+    constructed batches.
+
+    Args:
+        resolved_logical_batch_iter: An iterator over resolved logical batches.
+        stats: Dataset stats object used to store block batching time.
+        ensure_copy: Whether batches are always copied from the underlying base
+            blocks (not zero-copy views).
+
+    Returns:
+        An iterator over batch index and batches of the given size.
+    """
+
+    for logical_batch in resolved_logical_batch_iter:
+        output = DelegatingBlockBuilder()
+        slice_indices = [[0, None] for _ in range(len(logical_batch.blocks))]
+        if logical_batch.starting_block_idx > 0:
+            slice_indices[0][0] = logical_batch.starting_block_idx
+        if logical_batch.ending_block_idx is not None:
+            slice_indices[-1][1] = logical_batch.ending_block_idx
+
+        for i, block in enumerate(logical_batch.blocks):
+            accessor = BlockAccessor.for_block(block)
+            slice_index = slice_indices[i]
+            output.add_block(
+                accessor.slice(
+                    slice_index[0],
+                    slice_index[1]
+                    if slice_index[1] is not None
+                    else accessor.num_rows(),
+                    copy=False,
+                )
+            )
+
+        batch = output.build()
+        assert len(batch) == logical_batch.num_rows, (
+            len(batch),
+            logical_batch.num_rows,
+        )
+        if ensure_copy:
+            # Need to ensure that the batch is a fresh copy.
+            batch = BlockAccessor.for_block(batch)
+            batch = batch.slice(0, batch.num_rows(), copy=True)
+
+        yield Batch(logical_batch.batch_idx, batch, logical_batch)
+
+def _format_batches(
+    block_iter: Iterator[Batch],
+    batch_format: str,
+) -> Iterator[Batch]:
+    """Given an iterator of blocks, returns an iterator of formatted batches.
+
+    Args:
+        block_iter: An iterator over blocks.
+        batch_format: The batch format to use.
+        stats: An optional stats object to record formatting times.
+
+    Returns:
+        An iterator over batch index and the formatted batch.
+    """
+    for batch in block_iter:
+        formatted_batch = BlockAccessor.for_block(batch.data).to_batch_format(batch_format)
+        batch.data = formatted_batch
+        yield batch
+
+def _collate(
+    batch_iter: Iterator[Batch],
+    collate_fn: Optional[Callable[[DataBatch], Any]],
+) -> Iterator[Tuple[int, Any]]:
+    """Returns an iterator with the provided collate_fn applied to items of the batch
+    iterator.
+
+    Args:
+        batch_iter: An iterator over formatted batches.
+        stats: An optional stats object to record collation time.
+    """
+    for batch in batch_iter:
+        batch.data = collate_fn(batch.data)
+        yield batch
+
+def _eagerly_free_blocks(batch_iter: Iterator[Batch]):
+    """Eagerly free the block references in the batch iterator."""
+
+    for batch in batch_iter:
+        block_refs = batch.logical_batch.block_refs
+        for block_ref in block_refs:
+            trace_deallocation(block_ref, )
---
+++ python/ray/data/tests/block_batching/test_iter_batches.py
@@ -0,0 +1,244 @@
+from copy import copy
+import pytest
+from typing import Iterator, List, Tuple
+
+import numpy as np
+import pandas as pd
+import pyarrow as pa
+
+from ray.data.block import Block, BlockMetadata
+from ray.data._internal.block_batching.interfaces import Batch, LogicalBatch, BlockPrefetcher
+from ray.data._internal.block_batching.iter_batches import _bundle_block_refs_to_logical_batches, _local_shuffle_logical_batches, _prefetch_batches_locally, _construct_batch_from_logical_batch, _format_batches, _collate
+
+
+def block_generator(
+    num_rows: int, num_blocks: int
+) -> Iterator[Tuple[Block, BlockMetadata]]:
+    for i in range(num_blocks):
+        yield pa.table({"foo": [i] * num_rows}), BlockMetadata(
+            num_rows=num_rows,
+            size_bytes=0,
+            schema=None,
+            input_files=[],
+            exec_stats=None,
+        )
+
+def logical_batch_generator(
+    num_rows: int, num_blocks: int, batch_size: int = None
+) -> Iterator[LogicalBatch]:
+    logical_batch_iter = _bundle_block_refs_to_logical_batches(
+        block_generator(num_rows=num_rows, num_blocks=num_blocks), batch_size=batch_size
+    )
+
+    # Force resolve to True for testing purposes.
+    for logical_batch in logical_batch_iter:
+        logical_batch._resolved = True
+        logical_batch._blocks = logical_batch.block_refs
+        yield logical_batch
+
+
+def test_bundle_block_refs_to_logical_batches():
+    # Case 1: `batch_size` is None.
+    num_blocks = 4
+    num_rows_per_block = 2
+    batch_size = None
+    block_iter = block_generator(num_rows=num_rows_per_block, num_blocks=num_blocks)
+    block_refs = list(block_iter)
+    logical_batch_iter = _bundle_block_refs_to_logical_batches(
+        iter(block_refs), batch_size=batch_size
+    )
+    logical_batches = list(logical_batch_iter)
+    assert logical_batches == [
+        LogicalBatch(0, [block_refs[0][0]], 0, None, num_rows_per_block),
+        LogicalBatch(1, [block_refs[1][0]], 0, None, num_rows_per_block),
+        LogicalBatch(2, [block_refs[2][0]], 0, None, num_rows_per_block),
+        LogicalBatch(3, [block_refs[3][0]], 0, None, num_rows_per_block),
+    ]
+
+    # Case 2: Multiple batches in a block (`batch_size` is 1).
+    num_blocks = 2
+    num_rows_per_block = 2
+    batch_size = 1
+    block_iter = block_generator(num_rows=num_rows_per_block, num_blocks=num_blocks)
+    block_refs = list(block_iter)
+    logical_batch_iter = _bundle_block_refs_to_logical_batches(
+        iter(block_refs), batch_size=batch_size
+    )
+    logical_batches = list(logical_batch_iter)
+    assert logical_batches == [
+        LogicalBatch(0, [block_refs[0][0]], 0, 1, batch_size),
+        LogicalBatch(1, [block_refs[0][0]], 1, None, batch_size),
+        LogicalBatch(2, [block_refs[1][0]], 0, 1, batch_size),
+        LogicalBatch(3, [block_refs[1][0]], 1, None, batch_size),
+    ]
+
+    # Case 3: Multiple blocks in a batch (`batch_size` is 2)
+    num_blocks = 4
+    num_rows_per_block = 1
+    batch_size = 2
+    block_iter = block_generator(num_rows=num_rows_per_block, num_blocks=num_blocks)
+    block_refs = list(block_iter)
+    logical_batch_iter = _bundle_block_refs_to_logical_batches(
+        iter(block_refs), batch_size=batch_size
+    )
+    logical_batches = list(logical_batch_iter)
+    assert logical_batches == [
+        LogicalBatch(0, [block_refs[0][0], block_refs[1][0]], 0, None, batch_size),
+        LogicalBatch(1, [block_refs[2][0], block_refs[3][0]], 0, None, batch_size),
+    ]
+
+    # Case 4: Batches overlap across multiple blocks unevenly
+    num_blocks = 4
+    num_rows_per_block = 2
+    batch_size = 3
+    block_iter = block_generator(num_rows=num_rows_per_block, num_blocks=num_blocks)
+    block_refs = list(block_iter)
+    logical_batch_iter = _bundle_block_refs_to_logical_batches(
+        iter(block_refs), batch_size=batch_size
+    )
+    logical_batches = list(logical_batch_iter)
+    assert logical_batches == [
+        LogicalBatch(0, [block_refs[0][0], block_refs[1][0]], 0, 1, batch_size),
+        LogicalBatch(1, [block_refs[1][0], block_refs[2][0]], 1, None, batch_size),
+        LogicalBatch(2, [block_refs[3][0]], 0, None, 2),  # Leftover block.
+    ]
+
+    # Case 5: Batches overlap across multiple blocks unevenly, dropping the last
+    # incomplete batch.
+    num_blocks = 4
+    num_rows_per_block = 2
+    batch_size = 3
+    block_iter = block_generator(num_rows=num_rows_per_block, num_blocks=num_blocks)
+    block_refs = list(block_iter)
+    logical_batch_iter = _bundle_block_refs_to_logical_batches(
+        iter(block_refs), batch_size=batch_size, drop_last=True
+    )
+    logical_batches = list(logical_batch_iter)
+    assert logical_batches == [
+        LogicalBatch(0, [block_refs[0][0], block_refs[1][0]], 0, 1, batch_size),
+        LogicalBatch(1, [block_refs[1][0], block_refs[2][0]], 1, None, batch_size),
+    ]
+
+def test_local_shuffle_logical_batches():
+    # Case 1: Shuffle buffer min size is smaller than a batch.
+    # In this case, there is effectively no shuffling since the buffer
+    # never contains more than 1 batch.
+    shuffle_seed = 42
+    num_blocks = 4
+    num_rows_per_block = 2
+    shuffle_buffer_min_size = 1
+    logical_batches = list(logical_batch_generator(num_rows_per_block, num_blocks))
+    shuffled_batches = list(
+        _local_shuffle_logical_batches(
+            iter(logical_batches),
+            shuffle_buffer_min_size=shuffle_buffer_min_size,
+            shuffle_seed=shuffle_seed,
+        )
+    )
+    assert shuffled_batches == logical_batches
+
+    # Case 2: Shuffle buffer min size is greater than a batch.
+    shuffle_seed = 42
+    num_blocks = 4
+    num_rows_per_block = 1
+    shuffle_buffer_min_size = 2
+    logical_batches = list(logical_batch_generator(num_rows_per_block, num_blocks))
+    shuffled_batches = list(
+        _local_shuffle_logical_batches(
+            iter(logical_batches),
+            shuffle_buffer_min_size=shuffle_buffer_min_size,
+            shuffle_seed=shuffle_seed,
+        )
+    )
+
+    expected_output_ordering = [0, 1, 3, 2]
+    expected_output = [copy(logical_batches[i]) for i in expected_output_ordering]
+    for i in range(len(expected_output)):
+        expected_output[i].batch_idx = i
+
+    assert shuffled_batches == expected_output
+
+@pytest.mark.parametrize("num_batches_to_prefetch", [1, 2])
+def test_prefetch_batches_locally(num_batches_to_prefetch):
+    class DummyPrefetcher(BlockPrefetcher):
+        def __init__(self):
+            self.windows = []
+
+        def prefetch_blocks(self, blocks: List[Block]):
+            self.windows.append(blocks)
+
+    num_blocks = 10
+    prefetcher = DummyPrefetcher()
+    logical_batches = list(logical_batch_generator(1, num_blocks))
+    prefetch_batch_iter = _prefetch_batches_locally(
+        iter(logical_batches),
+        prefetcher=prefetcher,
+        num_batches_to_prefetch=num_batches_to_prefetch,
+    )
+
+    # Test that we are actually prefetching.
+    # We should prefetch a new set of batches after the current set
+    # finishes.
+    sets_prefetched = 1
+    output_batches = []
+    for i, batch in enumerate(prefetch_batch_iter):
+        if i % num_batches_to_prefetch == 0:
+            # If all the batches are already prefetched, then skip the check.
+            if not sets_prefetched * num_batches_to_prefetch >= len(logical_batches):
+                assert len(prefetcher.windows) == sets_prefetched + 1
+        sets_prefetched = len(prefetcher.windows)
+        output_batches.append(batch)
+
+    windows = prefetcher.windows
+    assert all(len(window) == num_batches_to_prefetch for window in windows)
+
+    # Check that the output iterator is the same as the input iterator.
+    assert output_batches == logical_batches
+
+@pytest.mark.parametrize("block_size", [1, 10])
+def test_construct_batch_from_logical_batch(block_size):
+    num_blocks = 5
+    batch_size = 3
+    logical_batches = list(
+        logical_batch_generator(block_size, num_blocks, batch_size=batch_size)
+    )
+    
+    created_batches = list(
+        _construct_batch_from_logical_batch(iter(logical_batches))
+    )
+
+    for i, batch in enumerate(created_batches):
+        assert i == batch.batch_idx
+        assert len(batch.data) == logical_batches[i].num_rows
+
+@pytest.mark.parametrize("batch_format", ["pandas", "numpy", "pyarrow"])
+def test_format_batches(batch_format):
+    batches = [Batch(i, data[0], None) for i, data in enumerate(block_generator(num_rows=2, num_blocks=2))]
+    batch_iter = _format_batches(batches, batch_format=batch_format)
+
+    for i, batch in enumerate(batch_iter):
+        assert batch.batch_idx == i
+        if batch_format == "pandas":
+            assert isinstance(batch.data, pd.DataFrame)
+        elif batch_format == "arrow":
+            assert isinstance(batch.data, pa.Table)
+        elif batch_format == "numpy":
+            assert isinstance(batch.data, dict)
+            assert isinstance(batch.data["foo"], np.ndarray)
+
+
+def test_collate():
+    def collate_fn(batch):
+        return pa.table({"bar": [1] * 2})
+
+    batches = [Batch(i, data[0], None) for i, data in enumerate(block_generator(num_rows=2, num_blocks=2))]
+    batch_iter = _collate(batches, collate_fn=collate_fn)
+
+    for i, batch in enumerate(batch_iter):
+        assert batch.batch_idx == i
+        assert batch.data == pa.table({"bar": [1] * 2})
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(pytest.main(["-v", __file__]))
