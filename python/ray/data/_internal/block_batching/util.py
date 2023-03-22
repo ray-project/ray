f9d526ff7a9a95bccd5a79f293b48c365eb01775
amogkam <amogkamsetty@yahoo.com>
2023-03-22 16:20:04 -0700

    comments
    Signed-off-by: amogkam <amogkamsetty@yahoo.com>

--- python/ray/data/_internal/block_batching/util.py
+++ python/ray/data/_internal/block_batching/util.py
@@ -18,7 +18,7 @@ def _make_async_gen(
     in an async fashion using a threadpool.
 
     Each thread in the threadpool will fetch data from the base_iterator in a
-    thread-safe fashion, and apply the provided computation.
+    thread-safe fashion, and apply the provided `fn` computation concurrently.
 
     Args:
         base_iterator: The iterator to asynchronously fetch from.
@@ -29,6 +29,7 @@ def _make_async_gen(
         An iterator with the same elements as outputted from `fn`.
     """
 
+    # Use a lock to fetch from the base_iterator in a thread-safe fashion.
     def convert_to_threadsafe_iterator(base_iterator: Iterator[T]) -> Iterator[T]:
         class ThreadSafeIterator:
             def __init__(self, it):
@@ -52,6 +53,9 @@ def _make_async_gen(
 
     output_queue = queue.Queue(1)
 
+    # Because pulling from the base iterator cannot happen concurrently,
+    # we must execute the expensive computation in a separate step which
+    # can be parallelized via a threadpool.
     def execute_computation(thread_index: int):
         try:
             for item in fn(thread_safe_generator):
