177b3dce8cec6ce58496b375d59a5fd41825d3a2
Kai Fricke <krfricke@users.noreply.github.com>
2023-03-22 12:37:35 -0700

    [tune] Evict object cache for actor re-use when search ended (#33593)
    Similar regression that was originally fixed in #31974, but re-surfaced after #33045.
    
    With actor re-use, we speculatively keep one cached actor around in case it is needed when new trials are added (e.g. if we add trials one-by-one). However, we should only do this when the search has not ended, as otherwise we keep an extra actor until the end of the experiment, wasting resources. This leads the tune_scale_up_down release test fail.
    
    In our refactor to #33045 we generalized object caching, but the eviction logic here explicitly keeps one object in cache. Our previous implementation in RayTrialExecutor relied on not calling the eviction function at all when trials were still coming up, essentially not adjusting the number of cached actors down. This is rarely a problem in practice, but does not make a clean contract when separated out into a component.
    
    In this PR, we change the logic as follows: When the search ended, no trials are pending execution, and we don't want to explicitly cache an actor, we force eviction of all cached objects.
    
    We are refactoring our execution backend, thus I believe it's sufficient to keep the release test to catch this regression. In the new backend we can add light weight unit tests to capture this behavior.
    
    Signed-off-by: Kai Fricke <kai@anyscale.com>
    Signed-off-by: Jack He <jackhe2345@gmail.com>

--- python/ray/tune/utils/object_cache.py
+++ python/ray/tune/utils/object_cache.py
@@ -40,6 +40,11 @@ class _ObjectCache:
     def num_cached_objects(self):
         return self._num_cached_objects
 
+    @property
+    def total_max_objects(self):
+        # Counter.total() is only available for python 3.10+
+        return sum(self._max_num_objects.values())
+
     def increase_max(self, key: T, by: int = 1) -> None:
         """Increase number of max objects for this key.
 
