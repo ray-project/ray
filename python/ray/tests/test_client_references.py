69cbce7f8922798ad04faa65ca4c33445aff88df
SangBin Cho <rkooo567@gmail.com>
2023-03-23 05:51:56 -0700

    Fix a lint issue.
    Signed-off-by: SangBin Cho <rkooo567@gmail.com>

--- python/ray/tests/test_client_references.py
+++ python/ray/tests/test_client_references.py
@@ -5,7 +5,6 @@ import pytest
 import ray as real_ray
 from ray._private.test_utils import object_memory_usage, wait_for_condition
 from ray._raylet import ActorID, ObjectRef
-from ray.core.generated.gcs_pb2 import ActorTableData
 from ray.util.client import _ClientContext
 from ray.util.client.common import ClientActorRef, ClientObjectRef
 from ray.util.client.ray_client_helpers import (
