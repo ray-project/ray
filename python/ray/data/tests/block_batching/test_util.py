29dc9ff013e94f95f656d3316c90e62e62e25119
Amog Kamsetty <amogkam@users.noreply.github.com>
2023-03-22 18:06:24 -0700

    [Data] [1/n] Async `iter_batches`: Add Threadpool util (#33575)
    Part 1 of async iter_batches support. Converts the existing _make_async_gen util to be able to use a threadpool instead of a single thread.
    
    ---------
    
    Signed-off-by: amogkam <amogkamsetty@yahoo.com>
    Signed-off-by: elliottower <elliot@elliottower.com>

---
+++ python/ray/data/tests/block_batching/test_util.py
@@ -0,0 +1,90 @@
+import pytest
+import time
+
+from ray.data._internal.block_batching.util import _make_async_gen
+
+
+def test_make_async_gen_fail():
+    """Tests that any errors raised in async threads are propagated to the main
+    thread."""
+
+    def gen(base_iterator):
+        raise ValueError("Fail")
+
+    iterator = _make_async_gen(base_iterator=iter([1]), fn=gen)
+
+    with pytest.raises(ValueError) as e:
+        for _ in iterator:
+            pass
+
+    assert e.match("Fail")
+
+
+def test_make_async_gen():
+    """Tests that make_async_gen overlaps compute."""
+
+    num_items = 10
+
+    def gen(base_iterator):
+        for i in base_iterator:
+            time.sleep(2)
+            yield i
+
+    def sleep_udf(item):
+        time.sleep(3)
+        return item
+
+    iterator = _make_async_gen(
+        base_iterator=iter(range(num_items)), fn=gen, num_workers=1
+    )
+
+    start_time = time.time()
+    outputs = []
+    for item in iterator:
+        print(item)
+        outputs.append(sleep_udf(item))
+    end_time = time.time()
+
+    assert outputs == list(range(num_items))
+
+    # Three second buffer.
+    assert end_time - start_time < num_items * 3 + 3
+
+
+def test_make_async_gen_multiple_threads():
+    """Tests that using multiple threads can overlap compute even more."""
+
+    num_items = 5
+
+    def gen(base_iterator):
+        for i in base_iterator:
+            time.sleep(4)
+            yield i
+
+    def sleep_udf(item):
+        time.sleep(5)
+        return item
+
+    # All 5 items should be fetched concurrently.
+    iterator = _make_async_gen(
+        base_iterator=iter(range(num_items)), fn=gen, num_workers=5
+    )
+
+    start_time = time.time()
+
+    # Only sleep for first item.
+    sleep_udf(next(iterator))
+
+    # All subsequent items should already be prefetched and should be ready.
+    for _ in iterator:
+        pass
+    end_time = time.time()
+
+    # 4 second for first item, 5 seconds for udf, 0.5 seconds buffer
+    assert end_time - start_time < 9.5
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(pytest.main(["-v", __file__]))
