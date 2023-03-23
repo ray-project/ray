8a4412cc616f224f38dfb8941acbe50369ae0568
Scott Lee <sjl@anyscale.com>
2023-03-23 11:14:26 -0700

    clean up
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- ci/run/run_bazel_test_with_sharding.sh
+++ ci/run/run_bazel_test_with_sharding.sh
@@ -22,10 +22,10 @@ for arg in "$@"; do
     fi
 done
 
-
 SHARD=$(python ./ci/run/bazel_sharding/bazel_sharding.py --exclude_manual --index "${BUILDKITE_PARALLEL_JOB}" --count "${BUILDKITE_PARALLEL_JOB_COUNT}" --tag_filters="$test_tag_filters" "${targets[@]}")
 
-# If no targets are assigned to this shard, skip bazel test run
+# If no targets are assigned to this shard, skip bazel test run.
+# Otherwise, pass the list of targets to `bazel test`
 if [[ -z "$SHARD" ]]
 then
     echo "No targets found for this shard, exiting"
