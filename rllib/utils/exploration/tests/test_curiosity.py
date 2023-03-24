d02a2c1f27fabe79a435c29f6ff18520ac2d9f76
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2023-03-23 23:23:43 -0700

    [RLModule] Disabled Centralized Critic and Curiosity tests with RLModules
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- rllib/utils/exploration/tests/test_curiosity.py
+++ rllib/utils/exploration/tests/test_curiosity.py
@@ -167,7 +167,9 @@ class TestCuriosity(unittest.TestCase):
             # Limit horizon to make it really hard for non-curious agent to reach
             # the goal state.
             .rollouts(num_rollout_workers=0)
-            .training(lr=0.001)
+            # TODO (Kourosh): We need to provide examples on how we do curiosity with
+            # RLModule API.
+            .training(lr=0.001, _enable_learner_api=False)
             .exploration(
                 exploration_config={
                     "type": "Curiosity",
@@ -183,6 +185,7 @@ class TestCuriosity(unittest.TestCase):
                     },
                 }
             )
+            .rl_module(_enable_rl_module=False)
         )
 
         num_iterations = 10
