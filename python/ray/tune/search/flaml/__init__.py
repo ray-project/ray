beb659140538020b63befb69e2d14724f93ec86e
xwjiang2010 <xwjiang2010@gmail.com>
2023-03-24 08:13:49 -0700

    not tagging flaml case. let it be CUSTOM.
    Signed-off-by: xwjiang2010 <xwjiang2010@gmail.com>

--- python/ray/tune/search/flaml/__init__.py
+++ python/ray/tune/search/flaml/__init__.py
@@ -1,7 +1,3 @@
 from ray.tune.search.flaml.flaml_search import CFO, BlendSearch
 
-from ray._private.usage.usage_lib import TagKey, record_extra_usage_tag
-
-record_extra_usage_tag(TagKey.TUNE_SEARCHER, "FLAML")
-
 __all__ = ["BlendSearch", "CFO"]
