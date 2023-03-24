55c2bd1ebcb6aac3778b34c1c0c2b618c5d85a19
xwjiang2010 <87673679+xwjiang2010@users.noreply.github.com>
2023-03-24 05:53:22 -0700

    [core] Setting ray default handler to use `logging._StderrHandler()`. (#33652)
    * [core] Setting ray default handler to use `logger.lastResort`.
    
    Setting ray default handler to use `logger.lastResort`, which is the same as `_StderrHandler`.
    
    And the doc string says it all:
    """
        This class is like a StreamHandler using sys.stderr, but always uses
        whatever sys.stderr is currently set to rather than the value of
        sys.stderr at handler construction time.
    """
    
    This is needed for Rich FileProxy redirect to work properly, no matter whether
    a particular logger is instantiated outside of rich.live.Live() or inside of it.
    
    Signed-off-by: xwjiang2010 <xwjiang2010@gmail.com>
    
    * change to logging._StderrHandler()
    
    Signed-off-by: xwjiang2010 <xwjiang2010@gmail.com>
    
    ---------
    
    Signed-off-by: xwjiang2010 <xwjiang2010@gmail.com>
    Signed-off-by: Jack He <jackhe2345@gmail.com>

--- python/ray/_private/ray_logging.py
+++ python/ray/_private/ray_logging.py
@@ -22,7 +22,7 @@ def setup_logger(
     logger.setLevel(logging_level)
     global _default_handler
     if _default_handler is None:
-        _default_handler = logging.StreamHandler()
+        _default_handler = logging._StderrHandler()
         logger.addHandler(_default_handler)
     _default_handler.setFormatter(logging.Formatter(logging_format))
     # Setting this will avoid the message
