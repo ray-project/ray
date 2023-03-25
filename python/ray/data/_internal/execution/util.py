ec891cd945821e981bf3ee8d9383cf148ae8cee2
Eric Liang <ekhliang@gmail.com>
2023-03-25 13:54:45 -0700

    fix size sensitive test
    Signed-off-by: Eric Liang <ekhliang@gmail.com>

--- python/ray/data/_internal/execution/util.py
+++ python/ray/data/_internal/execution/util.py
@@ -32,7 +32,7 @@ def make_ref_bundles(simple_data: List[Block]) -> List["RefBundle"]:
 
 def memory_string(num_bytes: int) -> str:
     """Return a human-readable memory string for the given amount of bytes."""
-    if num_bytes > 1024 * 1024 * 1024:
+    if num_bytes >= 1024 * 1024 * 1024:
         mem = str(round(num_bytes / (1024 * 1024 * 1024), 2)) + " GiB"
     else:
         mem = str(round(num_bytes / (1024 * 1024), 2)) + " MiB"
