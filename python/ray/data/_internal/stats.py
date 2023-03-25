6313be7f1500b108c306b5d23f2a4763a2310b12
amogkam <amogkamsetty@yahoo.com>
2023-03-24 17:33:47 -0700

    remove lock
    Signed-off-by: amogkam <amogkamsetty@yahoo.com>

--- python/ray/data/_internal/stats.py
+++ python/ray/data/_internal/stats.py
@@ -2,7 +2,6 @@ import collections
 from dataclasses import dataclass
 import time
 from contextlib import contextmanager
-import threading
 from typing import Dict, List, Optional, Set, Tuple, Union, Any
 
 import numpy as np
@@ -39,16 +38,13 @@ class Timer:
         self._max: float = 0
         self._total_count: float = 0
 
-        self.lock = threading.Lock()
-
     @contextmanager
     def timer(self) -> None:
         time_start = time.thread_time()
         try:
             yield
         finally:
-            with self.lock:
-                self.add(time.thread_time() - time_start)
+            self.add(time.thread_time() - time_start)
 
     def add(self, value: float) -> None:
         self._value += value
