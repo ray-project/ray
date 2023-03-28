9a356e49e4e13fbedbfef070f1debfc6599426e6
Artur Niederfahrenhorst <artur@anyscale.com>
2023-03-27 17:11:43 -0700

    Remove local_mode
    Signed-off-by: Artur Niederfahrenhorst <artur@anyscale.com>

--- rllib/algorithms/ppo/tests/test_ppo_with_rl_module.py
+++ rllib/algorithms/ppo/tests/test_ppo_with_rl_module.py
@@ -82,7 +82,7 @@ class MyCallbacks(DefaultCallbacks):
 class TestPPO(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        ray.init(local_mode=True)
+        ray.init()
 
     @classmethod
     def tearDownClass(cls):
