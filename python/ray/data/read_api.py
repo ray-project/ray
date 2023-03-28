7dc59a1292dd6d93866fa6ef4f5d730e6f57c7dc
Cheng Su <scnju13@gmail.com>
2023-03-27 20:59:12 -0700

    Add meta_provider parameter into read_images
    Signed-off-by: Cheng Su <scnju13@gmail.com>

--- python/ray/data/read_api.py
+++ python/ray/data/read_api.py
@@ -62,6 +62,7 @@ from ray.data.datasource.file_based_datasource import (
     _unwrap_arrow_serialization_workaround,
     _wrap_arrow_serialization_workaround,
 )
+from ray.data.datasource.image_datasource import _ImageFileMetadataProvider
 from ray.data.datasource.partitioning import Partitioning
 from ray.types import ObjectRef
 from ray.util.annotations import Deprecated, DeveloperAPI, PublicAPI
@@ -572,6 +573,7 @@ def read_images(
     *,
     filesystem: Optional["pyarrow.fs.FileSystem"] = None,
     parallelism: int = -1,
+    meta_provider: BaseFileMetadataProvider = _ImageFileMetadataProvider(),
     partition_filter: Optional[
         PathPartitionFilter
     ] = ImageDatasource.file_extension_filter(),
@@ -658,6 +660,7 @@ def read_images(
         paths=paths,
         filesystem=filesystem,
         parallelism=parallelism,
+        meta_provider=meta_provider,
         partition_filter=partition_filter,
         partitioning=partitioning,
         size=size,
