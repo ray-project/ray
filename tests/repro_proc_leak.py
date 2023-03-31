35c543c5bd6bdbbc239a4752fbe5d3281ea71097
Cade Daniel <edacih@gmail.com>
2023-03-31 12:50:16 -0700

    py lint;

--- tests/repro_proc_leak.py
+++ tests/repro_proc_leak.py
@@ -1,49 +1,64 @@
 #!/usr/bin/env python3
 
-import ray
+import subprocess
 import time
 from multiprocessing import Process
-import subprocess
+
+import ray
+
 import psutil
 
 ray.init()
 
+
 def target():
     import os
-    print(f'pid {os.getpid()}')
+
+    print(f"pid {os.getpid()}")
     time.sleep(1000)
+
 
 @ray.remote
 class MyActor:
-
     def run(self):
         print("Starting proc")
         p = Process(target=target, daemon=True)
         p.start()
         print(f"Done, p: {p}")
 
-print('Before')
-subprocess.run("ps aux | grep ray:: | grep 'ray::MyActor.run' | grep -v 'grep'", shell=True)
+
+print("Before")
+subprocess.run(
+    "ps aux | grep ray:: | grep 'ray::MyActor.run' | grep -v 'grep'", shell=True
+)
 
 actor = MyActor.remote()
 ray.get(actor.run.remote())
 
-procs = [x for x in psutil.process_iter(['name']) if x.name().startswith('ray::MyActor.run')]
+procs = [
+    x for x in psutil.process_iter(["name"]) if x.name().startswith("ray::MyActor.run")
+]
 if procs:
     print(procs[0].ppid())
 
 del actor
 
 while True:
-    procs = [x for x in psutil.process_iter(['name']) if x.name().startswith('ray::MyActor.run')]
+    procs = [
+        x
+        for x in psutil.process_iter(["name"])
+        if x.name().startswith("ray::MyActor.run")
+    ]
     if procs:
         print(procs[0].ppid())
     else:
         break
     time.sleep(1)
 
-print('After')
-subprocess.run("ps aux | grep ray:: | grep 'ray::MyActor.run' | grep -v 'grep'", shell=True)
+print("After")
+subprocess.run(
+    "ps aux | grep ray:: | grep 'ray::MyActor.run' | grep -v 'grep'", shell=True
+)
 
-#cat /proc/2611/status | grep -i ppid
-#subprocess.run("ps aux | grep ray:: | grep 'ray::MyActor.run' | grep -v 'grep' | awk '{print $2}' | xargs kill -9", shell=True)
+# cat /proc/2611/status | grep -i ppid
+# subprocess.run("ps aux | grep ray:: | grep 'ray::MyActor.run' | grep -v 'grep' | awk '{print $2}' | xargs kill -9", shell=True)
