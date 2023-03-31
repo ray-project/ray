eebf726f4b0735c6dca9349d151150fb178fe301
Alex <alex@anyscale.com>
2023-03-31 11:53:25 -0700

    .

--- python/ray/tests/test_autoscaler_e2e.py
+++ python/ray/tests/test_autoscaler_e2e.py
@@ -130,8 +130,16 @@ def test_metrics(shutdown_only):
                     value=2,
                     partial_label_match={"resource": "CPU"},
                 ),
-                MetricSamplePattern(name="autoscaler_pending_nodes", value=0, partial_label_match={"NodeType": "type-i"}),
-                MetricSamplePattern(name="autoscaler_pending_nodes", value=0, partial_label_match={"NodeType": "type-ii"}),
+                MetricSamplePattern(
+                    name="autoscaler_pending_nodes",
+                    value=0,
+                    partial_label_match={"NodeType": "type-i"},
+                ),
+                MetricSamplePattern(
+                    name="autoscaler_pending_nodes",
+                    value=0,
+                    partial_label_match={"NodeType": "type-ii"},
+                ),
                 MetricSamplePattern(
                     name="autoscaler_active_nodes",
                     value=1,
