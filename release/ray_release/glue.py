2fa8ebe0f68e763e6fe9cdf9824946635c55c0e1
Cuong Nguyen <can@anyscale.com>
2023-03-31 14:58:28 -0700

    Add back job run type. They are still used in CLI so not deprecate them yet.
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/glue.py
+++ release/ray_release/glue.py
@@ -55,6 +55,7 @@ from ray_release.util import (
 type_str_to_command_runner = {
     "command": SDKRunner,
     "sdk_command": SDKRunner,
+    "job": JobRunner,
     "anyscale_job": AnyscaleJobRunner,
 }
 
