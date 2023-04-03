fba39f8fd98809de1e4f501f99b15fc403ddbbf3
Scott Lee <sjl@anyscale.com>
2023-04-03 15:02:31 -0700

    temp change to trigger wheel build
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- python/ray/data/context.py
+++ python/ray/data/context.py
@@ -76,7 +76,7 @@ DEFAULT_NEW_EXECUTION_BACKEND = bool(
 # Whether to use the streaming executor. This only has an effect if the new execution
 # backend is enabled.
 DEFAULT_USE_STREAMING_EXECUTOR = bool(
-    int(os.environ.get("RAY_DATASET_USE_STREAMING_EXECUTOR", "1"))
+    int(os.environ.get("RAY_DATASET_USE_STREAMING_EXECUTOR", "0"))
 )
 
 # Whether to eagerly free memory (new backend only).
@@ -200,7 +200,6 @@ class DatasetContext:
         global _default_context
 
         with _context_lock:
-
             if _default_context is None:
                 _default_context = DatasetContext(
                     block_splitting_enabled=DEFAULT_BLOCK_SPLITTING_ENABLED,
