9b7a381ef0f3030879d12e5a59bcdb242b2e94b7
chaowang <chaowang@anyscale.com>
2023-04-04 18:12:59 +0000

    debug

--- dashboard/client/src/components/ActorTable.component.test.tsx
+++ dashboard/client/src/components/ActorTable.component.test.tsx
@@ -1,15 +1,12 @@
 import { render } from "@testing-library/react";
 import React from "react";
+import { MemoryRouter } from "react-router-dom";
 import { Actor } from "../type/actor";
 import ActorTable from "./ActorTable";
 describe("ActorTable", () => {
-  const ACTOR_1 = "1a77333eac321119fae2f60601000000";
-  const ACTOR_2 = "2a77333eac321119fae2f60601000000";
-  const ACTOR_3 = "3a77333eac321119fae2f60601000000";
   const actors: { [actorId: string]: Actor } = {
-    //actor1
-    "1a77333eac321119fae2f60601000000": {
-      actorId: "1a77333eac321119fae2f60601000000",
+    ACTOR_1: {
+      actorId: "ACTOR_1",
       jobId: "01000000",
       address: {
         rayletId: "426854e68e4225b3941deaf03c8dcfcb1daacc69a92711d370dbb0e1",
@@ -28,9 +25,8 @@ describe("ActorTable", () => {
       requiredResources: {},
       placementGroupId: "123",
     },
-    //actor2
-    "2a77333eac321119fae2f60601000000": {
-      actorId: "2a77333eac321119fae2f60601000000",
+    ACTOR_2: {
+      actorId: "ACTOR_2",
       jobId: "01000000",
       address: {
         rayletId: "426854e68e4225b3941deaf03c8dcfcb1daacc69a92711d370dbb0e1",
@@ -49,31 +45,14 @@ describe("ActorTable", () => {
       requiredResources: {},
       placementGroupId: "123",
     },
-    //actor3
-    "3a77333eac321119fae2f60601000000": {
-      actorId: "3a77333eac321119fae2f60601000000",
-      jobId: "01000000",
-      address: {
-        rayletId: "426854e68e4225b3941deaf03c8dcfcb1daacc69a92711d370dbb0e1",
-        ipAddress: "172.31.11.178",
-        port: 10003,
-        workerId: "b8b276a03612644098ed7a929c3b0e50f5bde894eb0d8cab288fbb6d",
-      },
-      state: "ALIVE",
-      numRestarts: "0",
-      name: "",
-      pid: 25322,
-      startTime: 1679010689151,
-      endTime: 0,
-      actorClass: "Counter",
-      exitDetail: "-",
-      requiredResources: {},
-      placementGroupId: "123",
-    },
   };
-  it("renders a table of actors sorted by state and startTime", () => {
-    const { getByRole } = render(<ActorTable actors={actors} />);
-
+  it("renders a table of actors sorted by state", () => {
+    const actors1 = JSON.parse(JSON.stringify(actors));
+    const { getByRole } = render(
+      <MemoryRouter>
+        <ActorTable actors={actors1} />
+      </MemoryRouter>,
+    );
     // Check that the table is rendered
     const table = getByRole("table");
 
@@ -81,22 +60,33 @@ describe("ActorTable", () => {
     const rows = table.querySelectorAll("tbody tr");
     const actor1Row = rows[0];
     const actor2Row = rows[1];
-    expect(actor1Row).toHaveTextContent("1a77333eac321119fae2f60601000000"); // actor1
-    expect(actor2Row).toHaveTextContent("2a77333eac321119fae2f60601000000"); // actor2
-    expect(actor2Row.compareDocumentPosition(actor1Row)).toBe(2); // 2 = Node.DOCUMENT_POSITION_FOLLOWING
+    expect(actor1Row.textContent).toContain("ACTOR_1");
+    expect(actor2Row.textContent).toContain("ACTOR_2");
+    expect(actor1Row.compareDocumentPosition(actor2Row)).toBe(
+      Node.DOCUMENT_POSITION_FOLLOWING,
+    ); // actor2Row appear after actor1Row
   });
-  it("renders a table of actors sorted by state and startTime", () => {
-    const { getByRole } = render(<ActorTable actors={actors} />);
+
+  it("renders a table of actors sorted by startTime increasingly when states are the same", () => {
+    const actors2 = JSON.parse(JSON.stringify(actors));
+    actors2["ACTOR_2"].state = "ALIVE";
+
+    const { getByRole } = render(
+      <MemoryRouter>
+        <ActorTable actors={actors2} />
+      </MemoryRouter>,
+    );
 
     // Check that the table is rendered
     const table = getByRole("table");
-
     // Check that the actors are sorted by state and startTime
     const rows = table.querySelectorAll("tbody tr");
     const actor1Row = rows[0];
     const actor2Row = rows[1];
-    expect(actor1Row).toHaveTextContent("1a77333eac321119fae2f60601000000"); // actor1
-    expect(actor2Row).toHaveTextContent("2a77333eac321119fae2f60601000000"); // actor2
-    expect(actor2Row.compareDocumentPosition(actor1Row)).toBe(2); // 2 = Node.DOCUMENT_POSITION_FOLLOWING
+    expect(actor1Row.textContent).toContain("ACTOR_1");
+    expect(actor2Row.textContent).toContain("ACTOR_2");
+    expect(actor1Row.compareDocumentPosition(actor2Row)).toBe(
+      Node.DOCUMENT_POSITION_FOLLOWING,
+    ); // actor2Row appear after actor1Row
   });
 });
