3c9a156dd2ccaa9197a02a7c7b43e8da42b3c8ad
Cuong Nguyen <can@anyscale.com>
2023-04-04 10:59:29 -0700

    Exit buildkite job using buildkite return code
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/result.py
+++ release/ray_release/result.py
@@ -1,5 +1,6 @@
 import enum
 import os
+import os
 from dataclasses import dataclass
 from typing import Optional, Dict, Tuple
 
@@ -33,6 +34,7 @@ class Result:
     stable: bool = True
     smoke_test: bool = False
 
+    buildkite_return_code: BuildkiteExitCode.SUCCESS
     buildkite_url: Optional[str] = None
     wheels_url: Optional[str] = None
     cluster_url: Optional[str] = None
