bfc5f390f670c48b031a6d1c4d1e9377472ccf52
jianoaix <iamjianxiao@gmail.com>
2023-04-04 16:36:08 +0000

    more tests

--- python/ray/data/tests/test_object_gc.py
+++ python/ray/data/tests/test_object_gc.py
@@ -27,6 +27,41 @@ def check_to_torch_no_spill(ctx, pipe):
     assert "Spilled" not in meminfo, meminfo
 
 
+def check_iter_torch_batches_no_spill(ctx, pipe):
+    # Run up to 10 epochs of the pipeline to stress test that
+    # no spilling will happen.
+    max_epoch = 10
+    for p in pipe.iter_epochs(max_epoch):
+        for _ in p.iter_torch_batches(batch_size=None):
+            pass
+    meminfo = memory_summary(ctx.address_info["address"], stats_only=True)
+    assert "Spilled" not in meminfo, meminfo
+
+
+def check_to_tf_no_spill(ctx, pipe):
+    # Run up to 10 epochs of the pipeline to stress test that
+    # no spilling will happen.
+    max_epoch = 10
+    for p in pipe.iter_epochs(max_epoch):
+        for _ in p.to_tf(
+            feature_columns="__value__", label_columns="label", batch_size=None
+        ):
+            pass
+    meminfo = memory_summary(ctx.address_info["address"], stats_only=True)
+    assert "Spilled" not in meminfo, meminfo
+
+
+def check_iter_tf_batches_no_spill(ctx, pipe):
+    # Run up to 10 epochs of the pipeline to stress test that
+    # no spilling will happen.
+    max_epoch = 10
+    for p in pipe.iter_epochs(max_epoch):
+        for _ in p.iter_tf_batches():
+            pass
+    meminfo = memory_summary(ctx.address_info["address"], stats_only=True)
+    assert "Spilled" not in meminfo, meminfo
+
+
 def test_iter_batches_no_spilling_upon_no_transformation(shutdown_only):
     # The object store is about 300MB.
     ctx = ray.init(num_cpus=1, object_store_memory=300e6)
@@ -35,8 +70,38 @@ def test_iter_batches_no_spilling_upon_no_transformation(shutdown_only):
 
     check_no_spill(ctx, ds.repeat())
     check_no_spill(ctx, ds.window(blocks_per_window=20))
+
+
+def test_torch_iteration(shutdown_only):
+    # The object store is about 300MB.
+    ctx = ray.init(num_cpus=1, object_store_memory=300e6)
+    # The size of dataset is 500*(80*80*4)*8B, about 100MB.
+    ds = ray.data.range_tensor(500, shape=(80, 80, 4), parallelism=100)
+
+    # to_torch
     check_to_torch_no_spill(ctx, ds.repeat())
     check_to_torch_no_spill(ctx, ds.window(blocks_per_window=20))
+    # iter_torch_batches
+    check_iter_torch_batches_no_spill(ctx, ds.repeat())
+    check_iter_torch_batches_no_spill(ctx, ds.window(blocks_per_window=20))
+
+
+def test_tf_iteration(shutdown_only):
+    # The object store is about 800MB.
+    ctx = ray.init(num_cpus=1, object_store_memory=800e6)
+    # The size of dataset is 500*(80*80*4)*8B, about 100MB.
+    ds = ray.data.range_tensor(500, shape=(80, 80, 4), parallelism=100).add_column(
+        "label", lambda x: 1
+    )
+
+    # to_tf
+    check_to_tf_no_spill(ctx, ds.repeat().map(lambda x: x))
+    check_to_tf_no_spill(ctx, ds.window(blocks_per_window=20).map(lambda x: x))
+    # iter_tf_batches
+    check_iter_tf_batches_no_spill(ctx, ds.repeat().map(lambda x: x))
+    check_iter_tf_batches_no_spill(
+        ctx, ds.window(blocks_per_window=20).map(lambda x: x)
+    )
 
 
 def test_iter_batches_no_spilling_upon_rewindow(shutdown_only):
@@ -46,9 +111,6 @@ def test_iter_batches_no_spilling_upon_rewindow(shutdown_only):
     ds = ray.data.range_tensor(500, shape=(80, 80, 4), parallelism=100)
 
     check_no_spill(
-        ctx, ds.window(blocks_per_window=20).repeat().rewindow(blocks_per_window=10)
-    )
-    check_to_torch_no_spill(
         ctx, ds.window(blocks_per_window=20).repeat().rewindow(blocks_per_window=10)
     )
 
