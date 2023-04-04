90ae8d0ecb7ad497d58f48e39b6e42c41c2d01da
Cuong Nguyen <can@anyscale.com>
2023-04-04 10:37:11 -0700

    Auto-retry for infrastructure errors
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/result.py
+++ release/ray_release/result.py
@@ -63,34 +63,39 @@ class ExitCode(enum.Enum):
     COMMAND_TIMEOUT = 42
     PREPARE_ERROR = 43
 
+class BuildkiteExitCode(enum.Enum):
+    """ 
+    Final exit code the test runner passes to buildkite-agent. This exit code is used
+    to determine job policies, such as automatic retries
+    """ 
+    SUCCESS = 0
+    UNKNOWN = 1
+    TRANSIENT_INFRA_ERROR = 10
+    INFRA_ERROR = 11
+    INFRA_TIMEOUT = 30
+    ERROR = 40 
+    TIMEOUT = 42 
 
-def handle_exception(e: Exception) -> Tuple[ExitCode, str, Optional[int]]:
+def handle_exception(e: Exception) -> Tuple[ExitCode, BuildkiteExitCode, Optional[int]]:
     from ray_release.exception import ReleaseTestError
 
-    if isinstance(e, ReleaseTestError):
-        exit_code = e.exit_code
-        # Legacy reporting
-        if 1 <= exit_code.value < 10:
-            error_type = "runtime_error"
-            runtime = None
-        elif 10 <= exit_code.value < 20:
-            error_type = "infra_error"
-            runtime = None
-        elif 30 <= exit_code.value < 40:
-            error_type = "infra_timeout"
-            runtime = None
-        elif exit_code == ExitCode.COMMAND_TIMEOUT:
-            error_type = "timeout"
-            runtime = 0
-        elif 40 <= exit_code.value < 50:
-            error_type = "error"
-            runtime = 0
-        else:
-            error_type = "error"
-            runtime = 0
-    else:
-        exit_code = ExitCode.UNKNOWN
-        error_type = "unknown error"
+    if not isinstance(e, ReleaseTestError):
+      return ExitCode.UNKNOWN, BuildkiteExitCode.UNKNOWN, 0
+    exit_code = e.exit_code
+    if 1 <= exit_code.value < 10:
+        error_type = BuildkiteExitCode.UNKNOWN
+        runtime = None
+    elif 10 <= exit_code.value < 20:
+        error_type = BuildkiteExitCode.INFRA_ERROR
+        runtime = None
+    elif 30 <= exit_code.value < 40:
+        error_type = BuildkiteExitCode.INFRA_TIMEOUT
+        runtime = None
+    elif exit_code == ExitCode.COMMAND_TIMEOUT:
+        error_type = BuildkiteExitCode.TIMEOUT
+        runtime = 0
+    elif 40 <= exit_code.value:
+        error_type = BuildkiteExitCode.ERROR
         runtime = 0
 
     return exit_code, error_type, runtime
