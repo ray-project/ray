8e5b7c7d9b304033619a6712e15c488043a4a425
chaowang <chaowang@anyscale.com>
2023-04-05 22:32:40 +0000

    review

--- dashboard/client/src/components/ActorTable.component.test.tsx
+++ dashboard/client/src/components/ActorTable.component.test.tsx
@@ -1,90 +1,91 @@
-import { render } from "@testing-library/react";
+import { render, within } from "@testing-library/react";
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import { Actor } from "../type/actor";
 import ActorTable from "./ActorTable";
+const ACTORS: { [actorId: string]: Actor } = {
+  ACTOR_1: {
+    actorId: "ACTOR_1",
+    jobId: "01000000",
+    address: {
+      rayletId: "426854e68e4225b3941deaf03c8dcfcb1daacc69a92711d370dbb0e1",
+      ipAddress: "172.31.11.178",
+      port: 10003,
+      workerId: "b8b276a03612644098ed7a929c3b0e50f5bde894eb0d8cab288fbb6d",
+    },
+    state: "ALIVE",
+    numRestarts: "0",
+    name: "",
+    pid: 25321,
+    startTime: 1679010689148,
+    endTime: 0,
+    actorClass: "Counter",
+    exitDetail: "-",
+    requiredResources: {},
+    placementGroupId: "123",
+  },
+  ACTOR_2: {
+    actorId: "ACTOR_2",
+    jobId: "01000000",
+    address: {
+      rayletId: "426854e68e4225b3941deaf03c8dcfcb1daacc69a92711d370dbb0e1",
+      ipAddress: "172.31.11.178",
+      port: 10003,
+      workerId: "b8b276a03612644098ed7a929c3b0e50f5bde894eb0d8cab288fbb6d",
+    },
+    state: "DEAD",
+    numRestarts: "0",
+    name: "",
+    pid: 25322,
+    startTime: 1679010689150,
+    endTime: 0,
+    actorClass: "Counter",
+    exitDetail: "-",
+    requiredResources: {},
+    placementGroupId: "123",
+  },
+};
 describe("ActorTable", () => {
-  const actors: { [actorId: string]: Actor } = {
-    ACTOR_1: {
-      actorId: "ACTOR_1",
-      jobId: "01000000",
-      address: {
-        rayletId: "426854e68e4225b3941deaf03c8dcfcb1daacc69a92711d370dbb0e1",
-        ipAddress: "172.31.11.178",
-        port: 10003,
-        workerId: "b8b276a03612644098ed7a929c3b0e50f5bde894eb0d8cab288fbb6d",
-      },
-      state: "ALIVE",
-      numRestarts: "0",
-      name: "",
-      pid: 25321,
-      startTime: 1679010689148,
-      endTime: 0,
-      actorClass: "Counter",
-      exitDetail: "-",
-      requiredResources: {},
-      placementGroupId: "123",
-    },
-    ACTOR_2: {
-      actorId: "ACTOR_2",
-      jobId: "01000000",
-      address: {
-        rayletId: "426854e68e4225b3941deaf03c8dcfcb1daacc69a92711d370dbb0e1",
-        ipAddress: "172.31.11.178",
-        port: 10003,
-        workerId: "b8b276a03612644098ed7a929c3b0e50f5bde894eb0d8cab288fbb6d",
-      },
-      state: "DEAD",
-      numRestarts: "0",
-      name: "",
-      pid: 25322,
-      startTime: 1679010689150,
-      endTime: 0,
-      actorClass: "Counter",
-      exitDetail: "-",
-      requiredResources: {},
-      placementGroupId: "123",
-    },
-  };
   it("renders a table of actors sorted by state", () => {
-    const actors1 = JSON.parse(JSON.stringify(actors));
     const { getByRole } = render(
       <MemoryRouter>
-        <ActorTable actors={actors1} />
+        <ActorTable actors={ACTORS} />
       </MemoryRouter>,
     );
-    // Check that the table is rendered
-    const table = getByRole("table");
 
-    // Check that the actors are sorted by state and startTime
-    const rows = table.querySelectorAll("tbody tr");
-    const actor1Row = rows[0];
-    const actor2Row = rows[1];
-    expect(actor1Row.textContent).toContain("ACTOR_1");
-    expect(actor2Row.textContent).toContain("ACTOR_2");
+    const actor1Row = getByRole("row", {
+      name: /ACTOR_1/,
+    });
+    const actor2Row = getByRole("row", {
+      name: /ACTOR_2/,
+    });
+
+    expect(within(actor1Row).getByText("ACTOR_1")).toBeInTheDocument();
+    expect(within(actor2Row).getByText("ACTOR_2")).toBeInTheDocument();
+
     expect(actor1Row.compareDocumentPosition(actor2Row)).toBe(
       Node.DOCUMENT_POSITION_FOLLOWING,
     ); // actor2Row appear after actor1Row
   });
 
-  it("renders a table of actors sorted by startTime increasingly when states are the same", () => {
-    const actors2 = JSON.parse(JSON.stringify(actors));
-    actors2["ACTOR_2"].state = "ALIVE";
+  it("renders a table of actors sorted by startTime asc when states are the same", () => {
+    ACTORS["ACTOR_2"].state = "ALIVE";
 
     const { getByRole } = render(
       <MemoryRouter>
-        <ActorTable actors={actors2} />
+        <ActorTable actors={ACTORS} />
       </MemoryRouter>,
     );
+    const actor1Row = getByRole("row", {
+      name: /ACTOR_1/,
+    });
+    const actor2Row = getByRole("row", {
+      name: /ACTOR_2/,
+    });
 
-    // Check that the table is rendered
-    const table = getByRole("table");
-    // Check that the actors are sorted by state and startTime
-    const rows = table.querySelectorAll("tbody tr");
-    const actor1Row = rows[0];
-    const actor2Row = rows[1];
-    expect(actor1Row.textContent).toContain("ACTOR_1");
-    expect(actor2Row.textContent).toContain("ACTOR_2");
+    expect(within(actor1Row).getByText("ACTOR_1")).toBeInTheDocument();
+    expect(within(actor2Row).getByText("ACTOR_2")).toBeInTheDocument();
+
     expect(actor1Row.compareDocumentPosition(actor2Row)).toBe(
       Node.DOCUMENT_POSITION_FOLLOWING,
     ); // actor2Row appear after actor1Row
