8383e84834945d609c17219b3aa4396fbaadc70f
Balaji Veeramani <balaji@anyscale.com>
2023-04-05 10:55:11 -0700

    Revert "Revert "Merge remote-tracking branch 'upstream/master' into gce-inference""
    This reverts commit f88b44db2f1129b0d5c559a9b8c5e6e1310a4e36.
    
    Signed-off-by: Balaji Veeramani <balaji@anyscale.com>

--- ci/lint/check_api_annotations.py
+++ ci/lint/check_api_annotations.py
@@ -99,7 +99,13 @@ if __name__ == "__main__":
     verify(ray.air, set(), ok, output)
     verify(ray.train, set(), ok, output)
     verify(ray.tune, set(), ok, output)
-    verify(ray, set(), ok, output, ignore=["ray.workflow", "ray.tune", "ray.serve"])
+    verify(
+        ray,
+        set(),
+        ok,
+        output,
+        ignore=["ray.workflow", "ray.tune", "ray.serve"],
+    )
     verify(ray.serve, set(), ok, output)
     assert len(ok) >= 500, len(ok)
     # TODO(ekl) enable it for all modules.
--- rllib/algorithms/appo/tests/tf/test_appo_learner.py
+++ rllib/algorithms/appo/tests/tf/test_appo_learner.py
@@ -67,6 +67,9 @@ class TestImpalaTfLearner(unittest.TestCase):
                 _enable_rl_module_api=True,
             )
         )
+        # We have to set exploration_config here manually because setting it through
+        # config.exploration() only deepupdates it
+        config.exploration_config = {}
 
         for fw in framework_iterator(config, ("tf2")):
             trainer = config.build()
