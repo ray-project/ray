135bd572bf600b5d521344ea0f071ae037ac9bf7
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2023-04-07 03:31:03 +0000

    fix

---
+++ python/ray/tests/test_proxy.py
@@ -0,0 +1,26 @@
+import os
+from ray._private.test_utils import (
+    client_test_enabled,
+    run_string_as_driver,
+)
+
+# https://github.com/ray-project/ray/issues/6662
+@pytest.mark.skipif(client_test_enabled(), reason="interferes with grpc")
+def test_http_proxy(start_http_proxy, shutdown_only):
+    # C++ config `grpc_enable_http_proxy` only initializes once, so we have to
+    # run driver as a separate process to make sure the correct config value
+    # is initialized.
+    script = """
+import ray
+
+ray.init(num_cpus=1)
+
+@ray.remote
+def f():
+    return 1
+
+assert ray.get(f.remote()) == 1
+"""
+
+    env = start_http_proxy
+    run_string_as_driver(script, dict(os.environ, **env))
