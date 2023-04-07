2b5d6c7da6f594a078c4886e025e41ebdf4aa2df
SangBin Cho <rkooo567@gmail.com>
2023-04-06 19:49:01 -0700

    move log config inside init
    Signed-off-by: SangBin Cho <rkooo567@gmail.com>

--- python/ray/_private/worker.py
+++ python/ray/_private/worker.py
@@ -44,6 +44,7 @@ else:
     from typing_extensions import Literal, Protocol
 
 import ray
+from ray._private import log  # isort: skip # noqa: F401
 import ray._private.gcs_utils as gcs_utils
 import ray._private.import_thread as import_thread
 import ray._private.node
@@ -1277,6 +1278,7 @@ def init(
             arguments is passed in.
     """
     if configure_logging:
+        log.generate_logging_config()
         setup_logger(logging_level, logging_format or ray_constants.LOGGER_FORMAT)
     else:
         logging.getLogger("ray").handlers.clear()
