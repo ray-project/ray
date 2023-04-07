bd7c00c6a252011f34fd2f14b8ab61a7f83bbdfd
Yi Cheng <yic@yic-QRJ2YL4L59.local.meter>
2023-04-06 19:33:34 -0700

    fix

--- src/ray/thirdparty/dlmalloc.c
+++ src/ray/thirdparty/dlmalloc.c
@@ -521,6 +521,7 @@ MAX_RELEASE_CHECK_RATE   default: 4095 unless not HAVE_MMAP
   improvement at the expense of carrying around more memory.
 */
 
+#pragma clang diagnostic ignored "-Weverything"
 
 /* Version identifier to allow people to support multiple versions */
 #ifndef DLMALLOC_VERSION
