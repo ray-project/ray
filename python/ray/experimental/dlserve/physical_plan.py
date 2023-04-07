b988e89600ea10c237be134becef73b4be2c690b
scv119 <scv119@gmail.com>
2023-04-07 11:17:00 -0700

    update

--- python/ray/experimental/dlserve/physical_plan.py
+++ python/ray/experimental/dlserve/physical_plan.py
@@ -1,5 +1,6 @@
 from abc import ABC
 from ray.dag import DAGNode
+from ray.util.placement_group import PlacementGroup
 
 class PhysicalPlan:
     def __init__(self) -> None:
@@ -7,11 +8,19 @@ class PhysicalPlan:
         self.replica_schedules = []
         self.replica_configs = []
 
+    def deploy(self):
+        pass
+
 
 class PhysicalPlanner(ABC):
     """The physical planner takes a logical plan (DAG) and number of
     availalble resources in the cluster, and generates a physical plan:
     i.e. number of replicas of each stage, and execution schedule of each replica."""
 
-    def plan(self, logical_plan: DAGNode) -> PhysicalPlan:
+    def plan(self, logical_plan: DAGNode, pg: PlacementGroup) -> PhysicalPlan:
+        pass
+
+
+class SimplePhysicalPlanner(PhysicalPlanner):
+    def plan(self, logical_plan: DAGNode, pg: PlacementGroup) -> PhysicalPlan:
         pass
