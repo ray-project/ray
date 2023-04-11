2400e7c1a4cb2ec9d87f7cff9621f3fa5f09edf1
scv119 <scv119@gmail.com>
2023-04-10 17:23:03 -0700

    update

--- python/ray/autoscaler/v2/instance_manager/storage.py
+++ python/ray/autoscaler/v2/instance_manager/storage.py
@@ -1,5 +1,6 @@
 import copy
 from abc import ABCMeta, abstractmethod
+from collections import defaultdict
 from threading import Lock
 from typing import Dict, List, Optional, Tuple
 
@@ -9,9 +10,9 @@ class Storage(metaclass=ABCMeta):
     def update(
         self,
         table: str,
-        mutation: Dict[str, str],
-        deletion: List[str],
-        expected_version: Optional[int],
+        mutation: Dict[str, str] = {},
+        deletion: List[str] = [],
+        expected_version: Optional[int] = None,
     ) -> Tuple[bool, int]:
         raise NotImplementedError("put() has to be implemented")
 
@@ -31,28 +32,28 @@ class Storage(metaclass=ABCMeta):
 class InMemoryStorage(Storage):
     def __init__(self):
         self._version = 0
-        self._tables = {}
+        self._tables = defaultdict(dict)
         self._lock = Lock()
 
     def update(
         self,
         table: str,
-        mutation: Dict[str, str],
-        deletion: List[str],
-        expected_version: Optional[int],
-    ) -> bool:
+        mutation: Dict[str, str] = {},
+        deletion: List[str] = [],
+        expected_version: Optional[int] = None,
+    ) -> Tuple[bool, int]:
         with self._lock:
             if expected_version is not None and expected_version != self._version:
                 return [False, self._version]
             self._version += 1
-            self._tables[table] = self._tables.get(table, {}).update(mutation)
+            self._tables[table].update(mutation)
             for deleted_key in deletion:
                 self._tables[table].pop(deleted_key, None)
             return [True, self._version]
 
     def get_all(self, table: str) -> Tuple[Dict[str, str], int]:
         with self._lock:
-            return (copy.deepcopy(self._tables.get(table, {})), self._version)
+            return (copy.deepcopy(self._tables[table]), self._version)
 
     def get(self, table: str, keys: List[str]) -> Tuple[Dict[str, str], int]:
         with self._lock:
