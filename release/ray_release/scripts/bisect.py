d0d1eaf3e756b7eaf9185625c2d04788976a4e80
Cuong Nguyen <can@anyscale.com>
2023-04-11 16:57:16 -0700

    Skeleton for bisect job
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/scripts/bisect.py
+++ release/ray_release/scripts/bisect.py
@@ -1,5 +1,5 @@
 import click
-import logger
+import math
 import subprocess
 from typing import List
 
@@ -7,16 +7,29 @@ from typing import List
 @click.argument("test_name", required=True, type=str)
 @click.argument("passing_commit", required=True, type=str)
 @click.argument("failing_commit", required=True, type=str)
+def main(test_name: str, passing_commit: str, failing_commit: str) -> None:
+    lists = _get_commit_lists(passing_commit, failing_commit)
+    while len(lists) > 1:
+        middle_commit = lists[math.floor(len(lists)/2)]
+        is_passing = _run_test(test_name, middle_commit)
+        if is_passing:
+            passing_commit = middle_commit
+        else:
+            failing_commit = middle_commit
+        lists = _get_commit_lists(passing_commit, failing_commit)
+        break
+    
+    print(f'Blamed commit found: {failing_commit}')
+
+def _run_test(test_name: str, failing_commit: str) -> bool:
+    return True
 
 def _get_commit_lists(passing_commit: str, failing_commit: str) -> List[str]:
-    try 
-        subprocess.check_output(
-        )
-    except Exception as e:
-        logger.info(f'Failed to get commit list: {e}')
-
-def main(test_name: str, passing_commit: str, failing_commit: str) -> None:
-    return None
+    commit_lists = subprocess.check_output(
+        f'git rev-list --ancestry-path {passing_commit}..{failing_commit}',
+        shell=True,
+    )
+    return commit_lists.decode('utf-8').split("\n")
 
 if __name__ == "__main__":
     main()
