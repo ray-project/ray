5c5a643fec7ee4bcdce2f591330cecc6d21c7231
Cuong Nguyen <can@anyscale.com>
2023-04-12 10:23:43 -0700

    Add unit tests
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/tests/test_bisect.py
+++ release/ray_release/tests/test_bisect.py
@@ -20,3 +20,15 @@ def test_bisect():
     ):
         blamed_commit = _bisect("test", list(commit_to_test_result.keys()))
         assert blamed_commit == "c3"
+
+def test_bisect():
+    commit_to_test_result = {
+      'c0': True, 
+      'c1': True, 
+      'c2': True, 
+      'c3': False, 
+      'c4': False, 
+    }
+    def _mock_get_commit_lists(passing_commit: str, failing_commit: str) -> List[str]:
+    blamed_commit = _bisect('test', 'c0', 'c4')
+    assert blamed_commit == 'c3'
