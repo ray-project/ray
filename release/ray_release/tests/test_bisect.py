1840431bae9449ff5d9d8624a02ad88c0998ba00
Cuong Nguyen <can@anyscale.com>
2023-04-13 09:47:16 -0700

    @krfricke's comments
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/tests/test_bisect.py
+++ release/ray_release/tests/test_bisect.py
@@ -63,28 +63,50 @@ def test_bisect():
         "c4": False,
     }
 
-    def _mock_get_commit_lists(passing_commit: str, failing_commit: str) -> List[str]:
-        commits = []
-        in_range = False
-        for commit in commit_to_test_result:
-            if commit == failing_commit:
-                break
-            if in_range:
-                commits.append(commit)
-            if commit == passing_commit:
-                commits.append(commit)
-                in_range = True
-        return commits
+    def _mock_run_test(test_name: str, commit: str) -> bool:
+        return commit_to_test_result[commit]
+
+    with mock.patch(
+        "ray_release.scripts.ray_bisect._run_test",
+        side_effect=_mock_run_test,
+    ):
+        blamed_commit = _bisect("test", list(commit_to_test_result.keys()))
+        assert blamed_commit == "c3"
+
+def test_bisect():
+    commit_to_test_result = {
+        "c0": True,
+        "c1": True,
+        "c2": True,
+        "c3": False,
+        "c4": False,
+    }
 
     def _mock_run_test(test_name: str, commit: str) -> bool:
         return commit_to_test_result[commit]
 
     with mock.patch(
-        "ray_release.scripts.ray_bisect._get_commit_lists",
-        side_effect=_mock_get_commit_lists,
-    ), mock.patch(
         "ray_release.scripts.ray_bisect._run_test",
         side_effect=_mock_run_test,
     ):
-        blamed_commit = _bisect("test", "c0", "c4")
+        blamed_commit = _bisect("test", list(commit_to_test_result.keys()))
+        assert blamed_commit == "c3"
+
+def test_bisect():
+    commit_to_test_result = {
+        "c0": True,
+        "c1": True,
+        "c2": True,
+        "c3": False,
+        "c4": False,
+    }
+
+    def _mock_run_test(test_name: str, commit: str) -> bool:
+        return commit_to_test_result[commit]
+
+    with mock.patch(
+        "ray_release.scripts.ray_bisect._run_test",
+        side_effect=_mock_run_test,
+    ):
+        blamed_commit = _bisect("test", list(commit_to_test_result.keys()))
         assert blamed_commit == "c3"
