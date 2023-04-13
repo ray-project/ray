43f720f6747bc6a72c7300660a649abc6e30251f
Cuong Nguyen <can@anyscale.com>
2023-04-13 09:47:16 -0700

    @krfricke's comments
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/scripts/ray_bisect.py
+++ release/ray_release/scripts/ray_bisect.py
@@ -1,5 +1,4 @@
 import click
-import math
 import subprocess
 from typing import List
 
@@ -9,21 +8,21 @@ from typing import List
 @click.argument("passing_commit", required=True, type=str)
 @click.argument("failing_commit", required=True, type=str)
 def main(test_name: str, passing_commit: str, failing_commit: str) -> None:
-    blamed_commit = _bisect(test_name, passing_commit, failing_commit)
+    commit_lists = _get_commit_lists(passing_commit, failing_commit)
+    blamed_commit = _bisect(test_name, commit_lists)
     print(f"Blamed commit found: {blamed_commit}")
 
 
-def _bisect(test_name: str, passing_commit: str, failing_commit: str) -> str:
-    lists = _get_commit_lists(passing_commit, failing_commit)
-    while len(lists) > 1:
-        middle_commit = lists[math.floor(len(lists) / 2)]
+def _bisect(test_name: str, commit_list: List[str]) -> str:
+    while len(commit_list) > 1:
+        middle_commit_idx = len(commit_list) // 2
+        middle_commit = commit_list[middle_commit_idx]
         is_passing = _run_test(test_name, middle_commit)
         if is_passing:
-            passing_commit = middle_commit
-        else:
-            failing_commit = middle_commit
-        lists = _get_commit_lists(passing_commit, failing_commit)
-    return failing_commit
+            commit_list = commit_list[middle_commit_idx + 1:] 
+        else: 
+            commit_list = commit_list[:middle_commit_idx]
+    return commit_list[-1]
 
 
 def _run_test(test_name: str, commit: str) -> bool:
