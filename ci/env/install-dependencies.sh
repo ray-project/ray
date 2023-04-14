a884dc461e7b6d4eeb6d1a73711ca615bdeb6eb9
Kai Fricke <kai@anyscale.com>
2023-04-14 13:46:53 +0200

    no constraints for single packages
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- ci/env/install-dependencies.sh
+++ ci/env/install-dependencies.sh
@@ -493,7 +493,7 @@ install_pip_packages() {
   done
 
   if [ -n "${pip_add}" ]; then
-    pip_cmd="pip install -c ${WORKSPACE_DIR}/python/requirements_pinned.txt $pip_add"
+    pip_cmd="pip install $pip_add"
     # Run the pip command to install all collected requirements files
     echo Running pip install: "$pip_cmd"
     eval "${pip_cmd}"
