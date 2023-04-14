62ee1bc61d9b578720d09784e6eec06b9dd618ec
chaowang <chaowang@anyscale.com>
2023-04-14 00:06:03 +0000

    feat: unit test
    Signed-off-by: chaowang <chaowang@anyscale.com>

--- dashboard/client/src/pages/node/NodeRow.component.test.tsx
+++ dashboard/client/src/pages/node/NodeRow.component.test.tsx
@@ -1,50 +1,72 @@
 import { render, screen } from "@testing-library/react";
+import { noop } from "lodash";
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import { NodeDetail } from "../../type/node";
 import { CoreWorkerStats, Worker } from "../../type/worker";
 import { NodeRow, WorkerRow } from "./NodeRow";
 
+const NODE: NodeDetail = {
+  hostname: "test-hostname",
+  ip: "192.168.0.1",
+  cpu: 15,
+  mem: [100, 95, 5],
+  state: "ALIVE",
+  disk: {
+    "/": {
+      used: 20000000,
+      total: 200000000,
+      free: 180000000,
+      percent: 10,
+    },
+    "/tmp": {
+      used: 0,
+      total: 200,
+      free: 200,
+      percent: 0,
+    },
+  },
+  networkSpeed: [5, 10],
+  raylet: {
+    state: "ALIVE",
+    nodeId: "1234567890ab",
+    isHeadNode: true,
+    numWorkers: 0,
+    pid: 2345,
+    startTime: 100,
+    terminateTime: -1,
+    brpcPort: 3456,
+    nodeManagerPort: 5890,
+    objectStoreAvailableMemory: 40,
+    objectStoreUsedMemory: 10,
+  },
+  logUrl: "http://192.16.0.1/logs",
+} as NodeDetail;
+
+const WORKER: Worker = {
+  cmdline: ["echo hi"],
+  pid: 3456,
+  cpuPercent: 14,
+  memoryInfo: {
+    rss: 75,
+    vms: 0,
+    pageins: 0,
+    pfaults: 0,
+  },
+  coreWorkerStats: [
+    {
+      workerId: "worker-12345",
+    } as CoreWorkerStats,
+  ],
+} as Worker;
+
+const DEAD_NODE = { ...NODE, state: "DEAD" };
+
 describe("NodeRow", () => {
   it("renders", async () => {
-    const node: NodeDetail = {
-      hostname: "test-hostname",
-      ip: "192.168.0.1",
-      cpu: 15,
-      mem: [100, 95, 5],
-      disk: {
-        "/": {
-          used: 20000000,
-          total: 200000000,
-          free: 180000000,
-          percent: 10,
-        },
-        "/tmp": {
-          used: 0,
-          total: 200,
-          free: 200,
-          percent: 0,
-        },
-      },
-      networkSpeed: [5, 10],
-      raylet: {
-        state: "ALIVE",
-        nodeId: "1234567890ab",
-        isHeadNode: true,
-        numWorkers: 0,
-        pid: 2345,
-        startTime: 100,
-        terminateTime: -1,
-        brpcPort: 3456,
-        nodeManagerPort: 5890,
-        objectStoreAvailableMemory: 40,
-        objectStoreUsedMemory: 10,
-      },
-      logUrl: "http://192.16.0.1/logs",
-    } as NodeDetail;
     render(
       <NodeRow
-        node={node}
+        node={NODE}
         expanded
         onExpandButtonClick={() => {
           /* purposefully empty */
@@ -77,64 +99,30 @@ describe("NodeRow", () => {
     expect(screen.getByText(/5.0000B\/s/)).toBeVisible();
     expect(screen.getByText(/10.0000B\/s/)).toBeVisible();
   });
+  it("Disable actions for Dead node", async () => {
+    render(
+      <NodeRow node={DEAD_NODE} expanded={false} onExpandButtonClick={noop} />,
+      {
+        wrapper: ({ children }) => (
+          <MemoryRouter>
+            <table>
+              <tbody>{children}</tbody>
+            </table>
+          </MemoryRouter>
+        ),
+      },
+    );
+    await screen.findByText("test-hostname");
+    // Could not access logs for Dead nodes(the log is hidden)
+    expect(screen.queryByLabelText(/Log/)).not.toBeInTheDocument();
+
+    expect(screen.getByText(/3456/)).toBeVisible();
+  });
 });
 
 describe("WorkerRow", () => {
   it("renders", async () => {
-    const node: NodeDetail = {
-      hostname: "test-hostname",
-      ip: "192.168.0.1",
-      cpu: 15,
-      mem: [100, 95, 5],
-      disk: {
-        "/": {
-          used: 20000000,
-          total: 200000000,
-          free: 180000000,
-          percent: 10,
-        },
-        "/tmp": {
-          used: 0,
-          total: 200,
-          free: 200,
-          percent: 0,
-        },
-      },
-      networkSpeed: [5, 10],
-      raylet: {
-        state: "ALIVE",
-        nodeId: "1234567890ab",
-        isHeadNode: true,
-        numWorkers: 0,
-        pid: 2345,
-        startTime: 100,
-        terminateTime: -1,
-        brpcPort: 3456,
-        nodeManagerPort: 5890,
-        objectStoreAvailableMemory: 40,
-        objectStoreUsedMemory: 10,
-      },
-      logUrl: "http://192.16.0.1/logs",
-    } as NodeDetail;
-
-    const worker: Worker = {
-      cmdline: ["echo hi"],
-      pid: 3456,
-      cpuPercent: 14,
-      memoryInfo: {
-        rss: 75,
-        vms: 0,
-        pageins: 0,
-        pfaults: 0,
-      },
-      coreWorkerStats: [
-        {
-          workerId: "worker-12345",
-        } as CoreWorkerStats,
-      ],
-    } as Worker;
-
-    render(<WorkerRow node={node} worker={worker} />, {
+    render(<WorkerRow node={NODE} worker={WORKER} />, {
       wrapper: ({ children }) => (
         <MemoryRouter>
           <table>
@@ -146,6 +134,29 @@ describe("WorkerRow", () => {
 
     await screen.findByText("echo hi");
     expect(screen.getByText(/ALIVE/)).toBeVisible();
+    expect(screen.getByText(/worker-12345/)).toBeVisible();
+    expect(screen.getByText(/3456/)).toBeVisible();
+    // CPU Usage
+    expect(screen.getByText(/14%/)).toBeVisible();
+    // Memory Usage
+    expect(screen.getByText(/75\.0000B\/100\.0000B\(75\.0%\)/)).toBeVisible();
+  });
+  it("Disable CPU profiling & stacktrace for dead worker", async () => {
+    render(<WorkerRow node={DEAD_NODE} worker={WORKER} />, {
+      wrapper: ({ children }) => (
+        <MemoryRouter>
+          <table>
+            <tbody>{children}</tbody>
+          </table>
+        </MemoryRouter>
+      ),
+    });
+    await screen.findByText("echo hi");
+    // Could not access Stack Trace and CPU Flame Graph for Dead workers, but the log is still available)
+    expect(screen.getByText(/Log/)).toBeInTheDocument();
+    expect(screen.queryByLabelText(/Stack Trace/)).not.toBeInTheDocument();
+    expect(screen.queryByLabelText(/CPU Flame Graph/)).not.toBeInTheDocument();
+
     expect(screen.getByText(/worker-12345/)).toBeVisible();
     expect(screen.getByText(/3456/)).toBeVisible();
     // CPU Usage
