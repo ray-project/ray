47c003ccc1f2221ed6bd9e5fbe95df05eda3fe9d
sven1977 <svenmika1977@gmail.com>
2023-04-15 11:30:59 +0200

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/algorithms/appo/torch/appo_torch_policy_rlm.py
+++ rllib/algorithms/appo/torch/appo_torch_policy_rlm.py
@@ -1,21 +1,25 @@
-from ray.rllib.algorithms.ppo.ppo_tf_policy import validate_config
+import logging
+
+from ray.rllib.algorithms.ppo.ppo_torch_policy import validate_config
 from ray.rllib.policy.sample_batch import SampleBatch
-from ray.rllib.policy.eager_tf_policy_v2 import EagerTFPolicyV2
+from ray.rllib.policy.torch_policy_v2 import TorchPolicyV2
 from ray.rllib.utils.annotations import override
+from ray.rllib.utils.framework import try_import_torch
+
+torch, _ = try_import_torch()
+
+logger = logging.getLogger(__name__)
 
 
 # TODO: Remove once we have a RLModule capable sampler class that can replace
 #  `Policy.compute_actions_from_input_dict()`.
-class APPOTfPolicyWithRLModule(EagerTFPolicyV2):
+class APPOTorchPolicyWithRLModule(TorchPolicyV2):
     def __init__(self, observation_space, action_space, config):
         validate_config(config)
-        EagerTFPolicyV2.enable_eager_execution_if_necessary()
-        self.framework = "tf2"
-        EagerTFPolicyV2.__init__(self, observation_space, action_space, config)
-        # Initiate TargetNetwork ops after loss initialization.
-        self.maybe_initialize_optimizer_and_loss()
+        TorchPolicyV2.__init__(self, observation_space, action_space, config)
+        self._initialize_loss_from_dummy_batch()
 
-    @override(EagerTFPolicyV2)
+    @override(TorchPolicyV2)
     def loss(self, model, dist_class, train_batch):
         self.stats = {}
         train_batch[SampleBatch.ACTION_LOGP]
@@ -24,6 +28,6 @@ class APPOTfPolicyWithRLModule(EagerTFPolicyV2):
         train_batch[SampleBatch.TERMINATEDS]
         return 0.0
 
-    @override(EagerTFPolicyV2)
+    @override(TorchPolicyV2)
     def get_batch_divisibility_req(self) -> int:
         return self.config["rollout_fragment_length"]
