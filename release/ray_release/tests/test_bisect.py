a45407fed8ebb903e3bea8ea770b7d2b092ff839
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2023-04-15 00:25:38 -0700

    [CI][Bisect][1] Skeleton for automated bisect of release tests (#34329)
    A script to bisect release test failures. This PR only contains a skeleton and unit-tests
    
    Signed-off-by: Cuong Nguyen <can@anyscale.com>
    Signed-off-by: elliottower <elliot@elliottower.com>

---
+++ release/ray_release/tests/test_bisect.py
@@ -0,0 +1,22 @@
+from unittest import mock
+from ray_release.scripts.ray_bisect import _bisect
+
+
+def test_bisect():
+    commit_to_test_result = {
+        "c0": True,
+        "c1": True,
+        "c2": True,
+        "c3": False,
+        "c4": False,
+    }
+
+    def _mock_run_test(test_name: str, commit: str) -> bool:
+        return commit_to_test_result[commit]
+
+    with mock.patch(
+        "ray_release.scripts.ray_bisect._run_test",
+        side_effect=_mock_run_test,
+    ):
+        blamed_commit = _bisect("test", list(commit_to_test_result.keys()))
+        assert blamed_commit == "c3"
