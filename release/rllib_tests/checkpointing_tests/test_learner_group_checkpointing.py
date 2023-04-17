81d1ba103e846388b6165436da0b1a5f76ce5ea1
Avnish <avnishnarayan@gmail.com>
2023-04-17 15:36:15 -0700

    Fix script with broken imports
    Signed-off-by: Avnish <avnishnarayan@gmail.com>

--- release/rllib_tests/checkpointing_tests/test_learner_group_checkpointing.py
+++ release/rllib_tests/checkpointing_tests/test_learner_group_checkpointing.py
@@ -1,16 +1,50 @@
 import gymnasium as gym
 import itertools
+import numpy as np
 import tempfile
 import unittest
 
 import ray
-from ray.rllib.algorithms.ppo.tests.test_ppo_learner import FAKE_BATCH
-from ray.rllib.core.learner.tests.test_learner_group import REMOTE_SCALING_CONFIGS
+from ray.rllib.core.learner.scaling_config import LearnerGroupScalingConfig
+from ray.rllib.core.testing.utils import get_learner_group
 from ray.rllib.policy.sample_batch import SampleBatch
-from ray.rllib.core.testing.utils import (
-    get_learner_group,
-)
 from ray.rllib.utils.test_utils import check
+
+
+FAKE_BATCH = {
+    SampleBatch.OBS: np.array(
+        [[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8], [0.9, 1.0, 1.1, 1.2]],
+        dtype=np.float32,
+    ),
+    SampleBatch.NEXT_OBS: np.array(
+        [[0.1, 0.2, 0.3, 0.4], [0.5, 0.6, 0.7, 0.8], [0.9, 1.0, 1.1, 1.2]],
+        dtype=np.float32,
+    ),
+    SampleBatch.ACTIONS: np.array([0, 1, 1]),
+    SampleBatch.PREV_ACTIONS: np.array([0, 1, 1]),
+    SampleBatch.REWARDS: np.array([1.0, -1.0, 0.5], dtype=np.float32),
+    SampleBatch.PREV_REWARDS: np.array([1.0, -1.0, 0.5], dtype=np.float32),
+    SampleBatch.TERMINATEDS: np.array([False, False, True]),
+    SampleBatch.TRUNCATEDS: np.array([False, False, False]),
+    SampleBatch.VF_PREDS: np.array([0.5, 0.6, 0.7], dtype=np.float32),
+    SampleBatch.ACTION_DIST_INPUTS: np.array(
+        [[-2.0, 0.5], [-3.0, -0.3], [-0.1, 2.5]], dtype=np.float32
+    ),
+    SampleBatch.ACTION_LOGP: np.array([-0.5, -0.1, -0.2], dtype=np.float32),
+    SampleBatch.EPS_ID: np.array([0, 0, 0]),
+    SampleBatch.AGENT_INDEX: np.array([0, 0, 0]),
+}
+
+
+REMOTE_SCALING_CONFIGS = {
+    "remote-cpu": LearnerGroupScalingConfig(num_workers=1),
+    "remote-gpu": LearnerGroupScalingConfig(num_workers=1, num_gpus_per_worker=1),
+    "multi-gpu-ddp": LearnerGroupScalingConfig(num_workers=2, num_gpus_per_worker=1),
+    "multi-cpu-ddp": LearnerGroupScalingConfig(num_workers=2, num_cpus_per_worker=2),
+    # "multi-gpu-ddp-pipeline": LearnerGroupScalingConfig(
+    #     num_workers=2, num_gpus_per_worker=2
+    # ),
+}
 
 
 class TestLearnerGroupCheckpointing(unittest.TestCase):
