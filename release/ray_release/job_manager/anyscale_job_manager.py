4df01469b20969728c39d85817867ab5375f49e1
Cuong Nguyen <can@anyscale.com>
2023-04-17 15:34:18 -0700

    Add comments for why we need to look across many ray logs for error patterns
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/job_manager/anyscale_job_manager.py
+++ release/ray_release/job_manager/anyscale_job_manager.py
@@ -332,6 +332,9 @@ class AnyscaleJobManager:
                     )
                     print("", flush=True)
             output = buf.getvalue().strip()
+            # Many of Ray components have their separated logs (e.g. dashboard, 
+            # gcs_server, etc.), so the interesting errors are not always in the 
+            # job logs. If the job has no logs, check other ray logs for error patterns.
             if "### Starting ###" not in output:
                 output = self._get_ray_error_logs()
             assert output, "No logs fetched"
