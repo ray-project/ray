72a1ccc4ac70908fda453208afc3fa0f779dfb69
angelinalg <122562471+angelinalg@users.noreply.github.com>
2023-04-17 15:46:02 -0700

    Update tutorial.py
    Does that look right, @matthewdeng? BTW, thanks for catching that.
    
    Signed-off-by: angelinalg <122562471+angelinalg@users.noreply.github.com>

--- python/ray/tune/tests/tutorial.py
+++ python/ray/tune/tests/tutorial.py
@@ -12,6 +12,7 @@ from torch.utils.data import DataLoader
 import torch.nn.functional as F
 
 from ray import air, tune
+from ray.air import session
 from ray.tune.schedulers import ASHAScheduler
 # __tutorial_imports_end__
 # fmt: on
