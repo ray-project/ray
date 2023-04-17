496f9378246b6e75474a9a6036ea6677e14c1465
Artur Niederfahrenhorst <artur@anyscale.com>
2023-04-17 09:01:39 -0700

    turn off rl modules by default again
    Signed-off-by: Artur Niederfahrenhorst <artur@anyscale.com>

--- rllib/algorithms/ppo/ppo.py
+++ rllib/algorithms/ppo/ppo.py
@@ -132,10 +132,6 @@ class PPOConfig(PGConfig):
             # Add constructor kwargs here (if any).
         }
 
-        # enable the rl module api by default
-        self.rl_module(_enable_rl_module_api=True)
-        self._enable_learner_api = True
-
     @override(AlgorithmConfig)
     def get_default_rl_module_spec(self) -> SingleAgentRLModuleSpec:
         if self.framework_str == "torch":
@@ -273,11 +269,6 @@ class PPOConfig(PGConfig):
 
     @override(AlgorithmConfig)
     def validate(self) -> None:
-
-        # Turn RLModule and Learner API on by default (only for torch and tf2)
-        if self.framework_str == "tf":
-            self.rl_module(_enable_rl_module_api=False)
-            self._enable_learner_api = False
 
         # Call super's validation method.
         super().validate()
