1aac43df8b759d6d6b9b8c8832d8fa4b1572b831
chaowang <chaowang@anyscale.com>
2023-04-18 00:44:24 +0000

    fix_eslint
    Signed-off-by: chaowang <chaowang@anyscale.com>

--- dashboard/client/src/pages/overview/cards/RecentJobsCard.component.test.tsx
+++ dashboard/client/src/pages/overview/cards/RecentJobsCard.component.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import fireEvent from "@testing-library/user-event";
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import { useJobList } from "../../job/hook/useJobList";
