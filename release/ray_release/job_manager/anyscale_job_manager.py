3eac9ad08775573e923db68e6d6a1602f8182488
Cuong Nguyen <can@anyscale.com>
2023-04-18 14:59:38 -0700

    Rebase
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/job_manager/anyscale_job_manager.py
+++ release/ray_release/job_manager/anyscale_job_manager.py
@@ -262,8 +262,7 @@ class AnyscaleJobManager:
         )
         return self._wait_job(timeout)
 
-    @staticmethod
-    def _get_ray_error_logs(cluster_id: str) -> Optional[str]:
+    def _get_ray_error_logs(self) -> Optional[str]:
         """
         Obtain any ray logs that contain keywords that indicate a crash, such as
         ERROR or Traceback
@@ -334,8 +333,7 @@ class AnyscaleJobManager:
                     print("", flush=True)
             output = buf.getvalue().strip()
             if "### Starting ###" not in output:
-                output = AnyscaleJobManager._get_ray_error_logs(
-                    self.cluster_manager.cluster_id)
+                output = self._get_ray_error_logs()
             assert output, "No logs fetched"
             return "\n".join(output.splitlines()[-LAST_LOGS_LENGTH * 3 :])
 
