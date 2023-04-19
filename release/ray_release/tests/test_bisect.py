8ebaf0bca299ad7b3b5df5754b1fb28d10aa5d6b
Cuong Nguyen <can@anyscale.com>
2023-04-18 20:19:53 -0700

    Rebase
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/tests/test_bisect.py
+++ release/ray_release/tests/test_bisect.py
@@ -20,29 +20,8 @@ def test_bisect():
 
     for output, input in test_cases.items():
 
-        def _mock_run_test(test_name: str, commit: str) -> bool:
-            return input[commit]
-
-        with mock.patch(
-            "ray_release.scripts.ray_bisect._run_test",
-            side_effect=_mock_run_test,
-        ), mock.patch(
-            "ray_release.scripts.ray_bisect._get_test",
-            return_value={},
-        ):
-            assert _bisect("test", list(input.keys())) == output
-
-def test_bisect():
-    commit_to_test_result = {
-      'c0': True, 
-      'c1': True, 
-      'c2': True, 
-      'c3': False, 
-      'c4': False, 
-    }
-
-    def _mock_run_test(test_name: str, commit: str) -> bool:
-        return commit_to_test_result[commit]
+        def _mock_run_test(test: Test, commit: List[str]) -> Dict[str, str]:
+            return input
 
         with mock.patch(
             "ray_release.scripts.ray_bisect._run_test",
