fb5bd7078fb92d8060319b4871712169897f6d91
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2023-04-19 11:09:23 -0700

    Add CLI Integration test
    Signed-off-by: Archit Kulkarni <architkulkarni@users.noreply.github.com>

--- dashboard/modules/job/tests/test_cli_integration.py
+++ dashboard/modules/job/tests/test_cli_integration.py
@@ -153,6 +153,14 @@ class TestJobSubmit:
         )
         stdout, _ = _run_cmd(f"ray job submit -- {cmd}")
         assert "succeeded" in stdout
+    
+    def test_metadata(self, ray_start_stop):
+        cmd = "echo hello"
+        stdout, _ = _run_cmd(
+            f"ray job submit --metadata='{{\"key\": \"value\"}}' -- {cmd}"
+        )
+        assert "hello" in stdout
+        assert "succeeded" in stdout
 
 
 class TestRuntimeEnv:
