f5d03c20242a1c24da178983ed69f1e5aa9907f4
chaowang <chaowang@anyscale.com>
2023-04-21 00:31:48 +0000

    feat: fill in serve card

--- dashboard/client/src/pages/overview/cards/RecentServeCard.tsx
+++ dashboard/client/src/pages/overview/cards/RecentServeCard.tsx
@@ -9,7 +9,8 @@ import {
 } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import { UnifiedJob } from "../../../type/job";
-import { useJobList } from "../../job/hook/useJobList";
+import { ServeApplication, ServeApplicationStatus } from "../../../type/serve";
+import { useServeApplications } from "../../serve/hook/useServeApplications";
 import { LinkWithArrow, OverviewCard } from "./OverviewCard";
 
 const useStyles = makeStyles((theme) =>
@@ -36,34 +37,99 @@ type RecentServeCardProps = {
   className?: string;
 };
 
+/*
+{
+    "": {
+        "name": "",
+        "route_prefix": "/test",
+        "docs_path": null,
+        "status": "RUNNING",
+        "message": "",
+        "last_deployed_time_s": 1682029771.0748637,
+        "deployed_app_config": null,
+        "deployments": {
+            "MyModelDeployment": {
+                "name": "MyModelDeployment",
+                "status": "HEALTHY",
+                "message": "",
+                "deployment_config": {
+                    "name": "MyModelDeployment",
+                    "num_replicas": 1,
+                    "max_concurrent_queries": 100,
+                    "user_config": null,
+                    "autoscaling_config": null,
+                    "graceful_shutdown_wait_loop_s": 2,
+                    "graceful_shutdown_timeout_s": 20,
+                    "health_check_period_s": 10,
+                    "health_check_timeout_s": 30,
+                    "ray_actor_options": {
+                        "runtime_env": {},
+                        "num_cpus": 1
+                    },
+                    "is_driver_deployment": false
+                },
+                "replicas": [
+                    {
+                        "replica_id": "MyModelDeployment#oJRaQg",
+                        "state": "RUNNING",
+                        "pid": 364224,
+                        "actor_name": "SERVE_REPLICA::MyModelDeployment#oJRaQg",
+                        "actor_id": "b8c9082697cd69c16109eeb804000000",
+                        "node_id": "3434841e491012452165c643fea4919d80d078554059d3e008d51713",
+                        "node_ip": "172.31.5.171",
+                        "start_time_s": 1682029903.403788
+                    }
+                ]
+            }
+        }
+    }
+}
+
+*/
+
+/*
+  Design: 
+    Header -> Title
+
+    Body ->
+    CardItem
+    itemsCount
+    styles
+
+    Footer -> <LinkWithArrow>
+*/
 export const RecentServeCard = ({ className }: RecentServeCardProps) => {
   const classes = useStyles();
 
-  const { jobList } = useJobList();
+  const { allServeApplications: applications } = useServeApplications();
 
-  const sortedJobs = _.orderBy(jobList, ["startTime"], ["desc"]).slice(0, 6);
+  const sortedApplications = _.orderBy(
+    applications,
+    ["last_deployed_time_s"],
+    ["desc"],
+  ).slice(0, 6);
 
   return (
     <OverviewCard className={classNames(classes.root, className)}>
-      <Typography variant="h3">Recent jobs</Typography>
+      <Typography variant="h3">Recent Applications</Typography>
       <div className={classes.listContainer}>
-        {sortedJobs.map((job) => (
-          <RecentJobListItem
-            key={job.job_id ?? job.submission_id}
+        {sortedApplications.map((app) => (
+          <RecentApplicationListItem
+            key={app.name}
             className={classes.listItem}
-            job={job}
+            app={app}
           />
         ))}
-        {sortedJobs.length === 0 && (
-          <Typography variant="h4">No jobs yet...</Typography>
+        {sortedApplications.length === 0 && (
+          <Typography variant="h4">No Applications yet...</Typography>
         )}
       </div>
-      <LinkWithArrow text="View all jobs" to="/jobs" />
+      <LinkWithArrow text="View all applications" to="/serve" />
     </OverviewCard>
   );
 };
 
-const useRecentJobListItemStyles = makeStyles((theme) =>
+const useRecentApplicationListItemStyles = makeStyles((theme) =>
   createStyles({
     root: {
       display: "flex",
@@ -115,24 +181,26 @@ const useRecentJobListItemStyles = makeStyles((theme) =>
   }),
 );
 
-type RecentJobListItemProps = {
-  job: UnifiedJob;
-  className?: string;
+type RecentApplicationListItemProps = {
+  className: string;
+  app: Pick<ServeApplication, "name" | "route_prefix" | "status">;
 };
 
-const RecentJobListItem = ({ job, className }: RecentJobListItemProps) => {
-  const classes = useRecentJobListItemStyles();
+const RecentApplicationListItem = ({
+  app,
+  className,
+}: RecentApplicationListItemProps) => {
+  const classes = useRecentApplicationListItemStyles();
 
   const icon = (() => {
-    switch (job.status) {
-      case "SUCCEEDED":
+    switch (app.status) {
+      case ServeApplicationStatus.NOT_STARTED:
         return (
           <RiCheckboxCircleFill
             className={classNames(classes.icon, classes.colorSuccess)}
           />
         );
-      case "FAILED":
-      case "STOPPED":
+      case ServeApplicationStatus.DEPLOY_FAILED:
         return (
           <RiCloseCircleFill
             className={classNames(classes.icon, classes.colorError)}
@@ -151,22 +219,22 @@ const RecentJobListItem = ({ job, className }: RecentJobListItemProps) => {
       {icon}
       <div className={classes.textContainer}>
         <Typography className={classes.title} variant="body2">
-          {job.job_id ?? job.submission_id}
+          {app.name}
         </Typography>
         <Typography
           className={classes.entrypoint}
-          title={job.entrypoint}
+          title={app.route_prefix}
           variant="caption"
         >
-          {job.entrypoint}
+          {app.route_prefix}
         </Typography>
       </div>
     </React.Fragment>
   );
   return (
     <div className={className}>
-      {job.job_id !== null && job.job_id !== "" ? (
-        <Link className={classes.root} to={`/jobs/${job.job_id}`}>
+      {app.name !== null ? (
+        <Link className={classes.root} to={`/serve/applications/${app.name}`}>
           {cardContent}
         </Link>
       ) : (
