93b161f577cd0734b51a0bafaad68857c7651a54
Kai Fricke <kai@anyscale.com>
2023-04-24 17:35:14 +0100

    remove custom gcsfs handler
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- python/ray/air/_internal/remote_storage.py
+++ python/ray/air/_internal/remote_storage.py
@@ -22,22 +22,8 @@ try:
     import pyarrow
     import pyarrow.fs
 
-    # TODO(krfricke): Remove this once gcsfs > 2022.3.0 is released
-    # (and make sure to pin)
-    class _CustomGCSHandler(pyarrow.fs.FSSpecHandler):
-        """Custom FSSpecHandler that avoids a bug in gcsfs <= 2022.3.0."""
-
-        def create_dir(self, path, recursive):
-            try:
-                # GCSFS doesn't expose `create_parents` argument,
-                # so it is omitted here
-                self.fs.mkdir(path)
-            except FileExistsError:
-                pass
-
 except (ImportError, ModuleNotFoundError):
     pyarrow = None
-    _CustomGCSHandler = None
 
 from ray import logger
 
@@ -244,9 +230,6 @@ def _get_fsspec_fs_and_path(uri: str) -> Optional["pyarrow.fs.FileSystem"]:
                 )
         except ImportError:
             pass
-
-        # GS doesn't support `create_parents` arg in `create_dir()`
-        fsspec_handler = _CustomGCSHandler
 
     fs = pyarrow.fs.PyFileSystem(fsspec_handler(fsspec_fs))
     return fs
