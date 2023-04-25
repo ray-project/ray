fc7a9fbc7f472ecaf9e126ffe2cf03044353782c
Avnish <avnishnarayan@gmail.com>
2023-04-25 16:11:23 -0700

    Address comments
    Signed-off-by: Avnish <avnishnarayan@gmail.com>

--- rllib/tests/test_algorithm_save_load_checkpoint_learner.py
+++ rllib/tests/test_algorithm_save_load_checkpoint_learner.py
@@ -2,6 +2,7 @@ import tempfile
 import unittest
 
 import ray
+from ray.rllib.algorithms.algorithm_config import AlgorithmConfig
 from ray.rllib.utils.test_utils import check, framework_iterator
 from ray.rllib.algorithms.ppo import PPOConfig
 from ray.rllib.policy.sample_batch import DEFAULT_POLICY_ID
@@ -14,27 +15,22 @@ algorithms_and_configs = {
 
 
 @ray.remote
-def save_and_train(algo_cfg, env, tmpdir):
-    algo_cfg = (
-        algo_cfg.training(_enable_learner_api=True)
-        .rl_module(_enable_rl_module_api=True)
-        .rollouts(num_rollout_workers=0)
-        .reporting(min_time_s_per_iteration=0, min_sample_timesteps_per_iteration=1)
-        .debugging(seed=10)
-    )
-    algo1 = algo_cfg.environment(env).build()
+def save_and_train(algo_cfg: AlgorithmConfig, env: str, tmpdir):
+    """Create an algo, checkpoint it, then train for 2 iterations.
 
-    print(tmpdir)
-    tmpdir = str(tmpdir)
-    algo1.save_checkpoint(tmpdir)
-    print("======= training algo 1 =======")
-    algo1.train()
-    results_algo1 = algo1.train()
-    return results_algo1["info"][LEARNER_INFO][DEFAULT_POLICY_ID][LEARNER_STATS_KEY]
+    Note: This function uses a seeded algorithm that can modify the global random state.
+        Running it multiple times in the same process can affect other algorithms.
+        Making it a Ray task runs it in a separate process and prevents it from
+        affecting other algorithms' random state.
 
+    Args:
+        algo_cfg: The algorithm config to build the algo from.
+        env: The gym genvironment to train on.
+        tmpdir: The temporary directory to save the checkpoint to.
 
-@ray.remote
-def load_and_train(algo_cfg, env, tmpdir):
+    Returns:
+        The learner stats after 2 iterations of training.
+    """
     algo_cfg = (
         algo_cfg.training(_enable_learner_api=True)
         .rl_module(_enable_rl_module_api=True)
@@ -43,20 +39,54 @@ def load_and_train(algo_cfg, env, tmpdir):
         .debugging(seed=10)
     )
     algo = algo_cfg.environment(env).build()
-    print(tmpdir)
+
+    tmpdir = str(tmpdir)
+    algo.save_checkpoint(tmpdir)
+    for _ in range(2):
+        results = algo.train()
+    return results["info"][LEARNER_INFO][DEFAULT_POLICY_ID][LEARNER_STATS_KEY]
+
+
+@ray.remote
+def load_and_train(algo_cfg: AlgorithmConfig, env: str, tmpdir):
+    """Loads the checkpoint saved by save_and_train and trains for 2 iterations.
+
+    Note: This function uses a seeded algorithm that can modify the global random state.
+        Running it multiple times in the same process can affect other algorithms.
+        Making it a Ray task runs it in a separate process and prevents it from
+        affecting other algorithms' random state.
+
+    Args:
+        algo_cfg: The algorithm config to build the algo from.
+        env: The gym genvironment to train on.
+        tmpdir: The temporary directory to save the checkpoint to.
+
+    Returns:
+        The learner stats after 2 iterations of training.
+
+    """
+    algo_cfg = (
+        algo_cfg.training(_enable_learner_api=True)
+        .rl_module(_enable_rl_module_api=True)
+        .rollouts(num_rollout_workers=0)
+        .reporting(min_time_s_per_iteration=0, min_sample_timesteps_per_iteration=1)
+        .debugging(seed=10)
+    )
+    algo = algo_cfg.environment(env).build()
     tmpdir = str(tmpdir)
     algo.load_checkpoint(tmpdir)
-    print("======= training algo 2 =======")
-    algo.train()
-    results_algo2 = algo.train()
-    return results_algo2["info"][LEARNER_INFO][DEFAULT_POLICY_ID][LEARNER_STATS_KEY]
+    for _ in range(2):
+        results = algo.train()
+    return results["info"][LEARNER_INFO][DEFAULT_POLICY_ID][LEARNER_STATS_KEY]
 
 
 class TestAlgorithmWithLearnerSaveAndRestore(unittest.TestCase):
-    def setUp(self) -> None:
+    @classmethod
+    def setUpClass(cls) -> None:
         ray.init()
 
-    def tearDown(self) -> None:
+    @classmethod
+    def tearDowClass(cls) -> None:
         ray.shutdown()
 
     def test_save_and_restore(self):
@@ -64,22 +94,24 @@ class TestAlgorithmWithLearnerSaveAndRestore(unittest.TestCase):
             config = algorithms_and_configs[algo_name]
             for _ in framework_iterator(config, frameworks=["torch", "tf2"]):
                 with tempfile.TemporaryDirectory() as tmpdir:
-                    results_algo_1 = ray.get(
-                        save_and_train.remote(config, "CartPole-v1", tmpdir)
-                    )
+                    # create an algorithm, checkpoint it, then train for 2 iterations
+                    ray.get(save_and_train.remote(config, "CartPole-v1", tmpdir))
+                    # load that checkpoint into a new algorithm and train for 2
+                    # iterations
                     results_algo_2 = ray.get(
                         load_and_train.remote(config, "CartPole-v1", tmpdir)
                     )
 
+                    # load that checkpoint into another new algorithm and train for 2
+                    # iterations
                     results_algo_3 = ray.get(
                         load_and_train.remote(config, "CartPole-v1", tmpdir)
                     )
 
-                    print(results_algo_1)
-                    print(results_algo_2)
-                    print(results_algo_3)
-
                     # check that the results are the same across loaded algorithms
+                    # they won't be the same as the first algorithm since the random
+                    # state that is used for each algorithm is not preserved across
+                    # checkpoints.
                     check(results_algo_3, results_algo_2)
 
 
