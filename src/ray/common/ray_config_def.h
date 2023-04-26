92e8cc622f20f2b13e207cab1724ee78d52b862f
SangBin Cho <rkooo567@gmail.com>
2023-04-25 18:11:08 -0700

    done
    Signed-off-by: SangBin Cho <rkooo567@gmail.com>

--- src/ray/common/ray_config_def.h
+++ src/ray/common/ray_config_def.h
@@ -267,7 +267,7 @@ RAY_CONFIG(int64_t, fetch_warn_timeout_milliseconds, 60000)
 /// object directory.
 RAY_CONFIG(int64_t, fetch_fail_timeout_milliseconds, 600000)
 
-/// Temporary workaround for https://github.com/ray-project/ray/pull/16402.
+/// Temporary workaround for https://gitsb.com/ray-project/ray/pull/16402.
 RAY_CONFIG(bool, yield_plasma_lock_workaround, true)
 
 // Whether to inline object status in serialized references.
@@ -799,8 +799,8 @@ RAY_CONFIG(bool, kill_idle_workers_of_terminated_job, true)
 // Example: RAY_preload_python_modules=tensorflow,pytorch
 RAY_CONFIG(std::vector<std::string>, preload_python_modules, {})
 
-// By default, raylet send a self liveness check to GCS every 60s
-RAY_CONFIG(int64_t, raylet_liveness_self_check_interval_ms, 60000)
+// By default, raylet send a self liveness check to GCS every 5s
+RAY_CONFIG(int64_t, raylet_liveness_self_check_interval_ms, 5000)
 
 // Instruct the CoreWorker to kill its child processes while
 // it exits. This prevents certain classes of resource leaks
