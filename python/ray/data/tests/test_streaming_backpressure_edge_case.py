e3d7c4bbfeabbfad49312307bfa1753624d49398
Eric Liang <ekhliang@gmail.com>
2023-04-27 16:24:49 -0700

    fix
    Signed-off-by: Eric Liang <ekhliang@gmail.com>

--- python/ray/data/tests/test_streaming_backpressure_edge_case.py
+++ python/ray/data/tests/test_streaming_backpressure_edge_case.py
@@ -61,8 +61,8 @@ def test_input_backpressure_e2e(restore_data_context, shutdown_only):
     ctx = ray.data.DataContext.get_current()
     ctx.execution_options.resource_limits.object_store_memory = 10e6
 
-    # 1GiB dataset.
-    ds = ray.data.read_datasource(source, n=1000, parallelism=100)
+    # 10GiB dataset.
+    ds = ray.data.read_datasource(source, n=10000, parallelism=1000)
     it = ds.iter_batches(batch_size=None, prefetch_batches=0)
     next(it)
     time.sleep(3)
