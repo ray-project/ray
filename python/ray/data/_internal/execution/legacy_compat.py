5410dfcd25452b711497ce2766e9cd10c5809438
Eric Liang <ekhliang@gmail.com>
2023-04-27 11:52:42 -0700

    const
    Signed-off-by: Eric Liang <ekhliang@gmail.com>

--- python/ray/data/_internal/execution/legacy_compat.py
+++ python/ray/data/_internal/execution/legacy_compat.py
@@ -40,6 +40,9 @@ from ray.data._internal.execution.interfaces import (
 )
 from ray.data._internal.execution.util import make_callable_class_concurrent
 
+# Warn about tasks larger than this.
+TASK_SIZE_WARN_THRESHOLD_BYTES = 100000
+
 
 def execute_to_legacy_block_iterator(
     executor: Executor,
@@ -200,7 +203,7 @@ def _blocks_to_input_buffer(blocks: BlockList, owns_blocks: bool) -> PhysicalOpe
             block_meta = read_task.get_metadata()
             task_size = len(cloudpickle.dumps(read_task))
             if block_meta.size_bytes is None or task_size > block_meta.size_bytes:
-                if task_size > 100000:
+                if task_size > TASK_SIZE_WARN_THRESHOLD_BYTES:
                     print(
                         f"WARNING: the read task size ({task_size} bytes) is larger "
                         "than the reported output size of the task "
