df67bd85bc00d2e5b0d09bfe35d3c368a3d4b15b
Eric Liang <ekhliang@gmail.com>
2023-04-28 11:18:28 -0700

    lint
    Signed-off-by: Eric Liang <ekhliang@gmail.com>

--- release/nightly_tests/dataset/sort.py
+++ release/nightly_tests/dataset/sort.py
@@ -10,7 +10,6 @@ import psutil
 
 import ray
 from ray._private.internal_api import memory_summary
-from ray.data._internal.arrow_block import ArrowRow
 from ray.data._internal.util import _check_pyarrow_version
 from ray.data.block import Block, BlockMetadata
 from ray.data.context import DataContext
