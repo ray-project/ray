3c9d666c49b3af12e4fa0ad03324df68d6b1da90
Cuong Nguyen <can@anyscale.com>
2023-04-28 18:12:30 -0700

    Fix test name
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/buildkite/concurrency.py
+++ release/ray_release/buildkite/concurrency.py
@@ -96,7 +96,7 @@ def parse_condition(cond: int, limit: float = float("inf")) -> float:
 
 
 def get_concurrency_group(test: Test) -> Tuple[str, int]:
-    if test.get('name').contains('chaos_many_actors'):
+    if test.get('name') == 'chaos_many_actors.aws':
         return default_concurrent.group, 100
     if test.get("env", None) == "gce":
         concurrent_group = gce_gpu_cpu_to_concurrent_groups
