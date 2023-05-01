e6dc822bd4c75c6883dd802892e4a9520ec27a25
Chen Shen <scv119@gmail.com>
2023-05-01 16:57:31 -0700

    Revert "[CI] Deprecate sdk_runner and prod_v1 from OSS release tests (#34804)"
    This reverts commit a34b2dc7bbf1b83eba91284c6348163ea439f7ea.

--- python/ray/autoscaler/aws/tests/aws_launch_and_verify_cluster.py
+++ python/ray/autoscaler/aws/tests/aws_launch_and_verify_cluster.py
@@ -65,13 +65,11 @@ def download_ssh_key():
     s3_client = boto3.client("s3", region_name="us-west-2")
 
     # Set the name of the S3 bucket and the key to download
-    bucket_name = "aws-cluster-launcher-test"
-    key_name = "aws-cluster-launcher-test.pem"
+    bucket_name = "oss-release-test-ssh-keys"
+    key_name = "ray-autoscaler_59_us-west-2.pem"
 
     # Download the key from the S3 bucket to a local file
     local_key_path = os.path.expanduser(f"~/.ssh/{key_name}")
-    if not os.path.exists(os.path.dirname(local_key_path)):
-        os.makedirs(os.path.dirname(local_key_path))
     s3_client.download_file(bucket_name, key_name, local_key_path)
 
     # Set permissions on the key file
