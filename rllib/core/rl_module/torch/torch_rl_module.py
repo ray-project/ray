53ad1ba68d888b561e2cbcfad6b2372210472934
Artur Niederfahrenhorst <attaismyname@googlemail.com>
2023-05-01 14:14:58 -0700

    lint
    Signed-off-by: Artur Niederfahrenhorst <attaismyname@googlemail.com>

--- rllib/core/rl_module/torch/torch_rl_module.py
+++ rllib/core/rl_module/torch/torch_rl_module.py
@@ -22,12 +22,8 @@ class TorchRLModule(nn.Module, RLModule):
             self.is_compiled = False
 
         if self.is_compiled:
-            self.__compiled_forward_train = torch.compile(
-                super().forward_train
-            )
-            self.__compiled_forward_inference = torch.compile(
-                super().forward_inference
-            )
+            self.__compiled_forward_train = torch.compile(super().forward_train)
+            self.__compiled_forward_inference = torch.compile(super().forward_inference)
             self.__compiled_forward_exploration = torch.compile(
                 super().forward_exploration
             )
