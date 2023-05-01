89a6ec32bff6faef0816ce67ecb8741036a9d190
Hao Chen <chenh1024@gmail.com>
2023-05-01 10:26:45 -0700

    refine __init__.py
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- python/ray/__init__.py
+++ python/ray/__init__.py
@@ -192,11 +192,10 @@ serialization = _DeprecationWrapper("serialization", ray._private.serialization)
 state = _DeprecationWrapper("state", ray._private.state)
 
 
-__all__ = [
+RAY_APIS = {
     "__version__",
     "_config",
     "get_runtime_context",
-    "actor",
     "autoscaler",
     "available_resources",
     "cancel",
@@ -207,7 +206,6 @@ __all__ = [
     "get_actor",
     "get_gpu_ids",
     "init",
-    "internal",
     "is_initialized",
     "java_actor_class",
     "java_function",
@@ -221,19 +219,67 @@ __all__ = [
     "shutdown",
     "show_in_dashboard",
     "timeline",
-    "util",
     "wait",
-    "widgets",
     "LOCAL_MODE",
     "SCRIPT_MODE",
     "WORKER_MODE",
-]
+}
+
+# Public APIs that should automatically trigger ray.init().
+AUTO_INIT_APIS = {
+    "available_resources",
+    "cancel",
+    "cluster_resources",
+    "get",
+    "get_actor",
+    "get_gpu_ids",
+    "get_runtime_context",
+    "kill",
+    "nodes",
+    "put",
+    "timeline",
+    "wait",
+}
+
+# Public APIs that should not automatically trigger ray.init().
+NON_AUTO_INIT_APIS = {
+    "ClientBuilder",
+    "LOCAL_MODE",
+    "Language",
+    "SCRIPT_MODE",
+    "WORKER_MODE",
+    "__version__",
+    "_config",
+    "autoscaler",
+    "client",
+    "cpp_function",
+    "init",
+    "is_initialized",
+    "java_actor_class",
+    "java_function",
+    "method",
+    "remote",
+    "show_in_dashboard",
+    "shutdown",
+}
+
+assert RAY_APIS == AUTO_INIT_APIS | NON_AUTO_INIT_APIS
+from ray._private.auto_init_hook import wrap_auto_init_for_all_apis
+
+wrap_auto_init_for_all_apis(AUTO_INIT_APIS)
+
+
+__all__ = list(RAY_APIS)
 
 # Subpackages
 __all__ += [
-    "data",
-    "workflow",
+    "actor",
     "autoscaler",
+    "data",
+    "internal",
+    "util",
+    "widgets",
+    "workflow",
 ]
 
 # ID types
@@ -251,23 +297,6 @@ __all__ += [
     "UniqueID",
     "PlacementGroupID",
 ]
-
-
-# Public APIs that should automatically trigger ray.init().
-AUTO_INIT_APIS = [
-    "cancel",
-    "get",
-    "get_actor",
-    "get_gpu_ids",
-    "get_runtime_context",
-    "kill",
-    "put",
-    "wait",
-]
-
-from ray._private.auto_init_hook import wrap_auto_init_for_all_apis
-
-wrap_auto_init_for_all_apis(AUTO_INIT_APIS)
 
 
 # Delay importing of expensive, isolated subpackages.
