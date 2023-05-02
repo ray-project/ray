27997f73bd729ad30791f1e4dcc4917fb11808e4
scv119 <scv119@gmail.com>
2023-05-02 14:11:57 -0700

    update

--- python/ray/autoscaler/v2/tests/test_storage.py
+++ python/ray/autoscaler/v2/tests/test_storage.py
@@ -1,4 +1,7 @@
 # coding: utf-8
+import os
+import sys
+
 import pytest  # noqa
 from ray.autoscaler.v2.instance_manager.storage import InMemoryStorage
 
@@ -10,7 +13,10 @@ def test_storage(storage):
     assert storage.get(table="test_table", keys=[]) == ({}, 0)
     assert storage.get(table="test_table", keys=["key1"]) == ({}, 0)
 
-    assert storage.batch_update(table="test_table", mutation={"key1": "value1"}) == [True, 1]
+    assert storage.batch_update(table="test_table", mutation={"key1": "value1"}) == [
+        True,
+        1,
+    ]
 
     assert storage.get_version() == 1
 
@@ -40,9 +46,20 @@ def test_storage(storage):
     )
 
     assert storage.get(table="test_table", keys=["key2", "key1"]) == (
-        {"key2": ("value3", 3)}
+        {"key2": ("value3", 3)},
         3,
     )
+
+    assert storage.update(table="test_table", key="key2", value="value5") == [True, 4]
+    assert storage.update(
+        table="test_table", key="key2", value="value5", insert_only=True
+    ) == [False, 4]
+    assert storage.update(
+        table="test_table", key="key2", value="value5", expected_entry_version=3
+    ) == [False, 4]
+    assert storage.update(
+        table="test_table", key="key2", value="value6", expected_entry_version=4
+    ) == [True, 5]
 
 
 if __name__ == "__main__":
