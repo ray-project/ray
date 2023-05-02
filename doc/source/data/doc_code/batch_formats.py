9f35bda690274eb3a01c7f27a88b3d137f08cedc
Max Pumperla <max.pumperla@googlemail.com>
2023-05-02 23:38:55 +0200

    [docs] batch prediction guide, strict mode (#34969)
    Signed-off-by: Max Pumperla <max.pumperla@googlemail.com>
    Signed-off-by: amogkam <amogkamsetty@yahoo.com>
    Co-authored-by: amogkam <amogkamsetty@yahoo.com>

--- doc/source/data/doc_code/batch_formats.py
+++ doc/source/data/doc_code/batch_formats.py
@@ -10,6 +10,9 @@ ds = ray.data.read_csv("example://iris.csv")
 def map_function(data):
     return data[data["sepal.length"] < 5]
 
+batch = ds.take_batch(10)
+mapped_batch = map_function(batch)
+
 transformed = ds.map_batches(map_function, batch_size=10)
 # __simple_map_function_end__
 
@@ -21,7 +24,6 @@ ds = ray.data.read_csv("example://iris.csv")
 ds.show(1)
 # -> {'sepal.length': 5.1, ..., 'petal.width': 0.2, 'variety': 'Setosa'}
 
-ds.default_batch_format()
 # pandas.core.frame.DataFrame
 
 def transform_pandas(df_batch: pd.DataFrame) -> pd.DataFrame:
@@ -30,20 +32,27 @@ def transform_pandas(df_batch: pd.DataFrame) -> pd.DataFrame:
     df_batch = df_batch.drop(columns=["sepal.length"])
     return df_batch
 
-ds.map_batches(transform_pandas).show(1)
+ds.map_batches(transform_pandas, batch_format="pandas").show(1)
 # -> {..., 'variety': 'Versicolor', 'normalized.sepal.length': 1.0}
 # __simple_pandas_end__
 
 # __simple_numpy_start__
+from typing import Dict
+
 import ray
 import numpy as np
 
-ds = ray.data.range_tensor(1000, shape=(2, 2))
-ds.default_batch_format()
-# 'numpy.ndarray'
 
-def transform_numpy(arr: np.ndarray) -> np.ndarray:
-    return arr * 2
+ds = ray.data.range_tensor(1000, shape=(2, 2))
+
+def transform_numpy(arr: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
+    arr["data"] = arr["data"] * 2
+    return arr
+
+
+# test map function on a batch
+batch = ds.take_batch(1)
+mapped_batch = transform_numpy(batch)
 
 ds.map_batches(transform_numpy)
 # __simple_numpy_end__
@@ -56,9 +65,15 @@ import pyarrow.compute as pac
 
 ds = ray.data.read_csv("example://iris.csv")
 
+
 def transform_pyarrow(batch: pa.Table) -> pa.Table:
     batch = batch.filter(pac.equal(batch["variety"], "Versicolor"))
     return batch.drop(["sepal.length"])
+
+
+# test map function on a batch
+batch = ds.take_batch(1)
+mapped_batch = transform_pyarrow(batch)
 
 ds.map_batches(transform_pyarrow, batch_format="pyarrow").show(1)
 # -> {'sepal.width': 3.2, ..., 'variety': 'Versicolor'}
