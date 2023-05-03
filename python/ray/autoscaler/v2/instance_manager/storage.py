dfe35f08332047c32f97b3d6987e8963920ef305
scv119 <scv119@gmail.com>
2023-05-03 10:12:23 -0700

    update

--- python/ray/autoscaler/v2/instance_manager/storage.py
+++ python/ray/autoscaler/v2/instance_manager/storage.py
@@ -1,12 +1,17 @@
 import copy
 from abc import ABCMeta, abstractmethod
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from threading import Lock
 from typing import Dict, List, Optional, Tuple
+
+StoreStatus = namedtuple("StoreStatus", ["success", "version"])
+VersionedValue = namedtuple("VersionedValue", ["value", "version"])
 
 
 class Storage(metaclass=ABCMeta):
     """Interface for a storage backend that stores the state of nodes in the cluster.
+
+    The storage is thread-safe.
 
     The storage is versioned, which means that each successful stage change to the
     storage will bump the version number. The version number can be used to
@@ -20,10 +25,10 @@ class Storage(metaclass=ABCMeta):
     def batch_update(
         self,
         table: str,
-        mutation: Dict[str, str] = {},
-        deletion: List[str] = [],
+        mutation: Optional[Dict[str, str]] = None,
+        deletion: Optional[List[str]] = None,
         expected_storage_version: Optional[int] = None,
-    ) -> Tuple[bool, int]:
+    ) -> StoreStatus:
         """Batch update the storage table. This method is atomic.
 
         Args:
@@ -31,11 +36,11 @@ class Storage(metaclass=ABCMeta):
             mutation: A dictionary of key-value pairs to be updated.
             deletion: A list of keys to be deleted.
             expected_storage_version: The expected storage version. The
-                update will fail is the version does not match the
+                update will fail if the version does not match the
                 current storage version.
 
         Returns:
-            Tuple[bool, int]: A tuple of (success, version). If the update is
+            StoreStatus: A tuple of (success, version). If the update is
                 successful, returns (True, new_version).
                 Otherwise, returns (False, current_version).
         """
@@ -49,7 +54,7 @@ class Storage(metaclass=ABCMeta):
         value: str,
         expected_entry_version: Optional[int] = None,
         insert_only: bool = False,
-    ) -> Tuple[bool, int]:
+    ) -> StoreStatus:
         """Update a single entry in the storage table.
 
         Args:
@@ -58,10 +63,11 @@ class Storage(metaclass=ABCMeta):
             value: The value of the entry.
             expected_entry_version: The expected version of the entry.
                 The update will fail if the version does not match the current
-                version of the entry. insert_only (bool): If True, the update will
+                version of the entry.
+            insert_only: If True, the update will
                 fail if the entry already exists.
         Returns:
-            Tuple[bool, int]: A tuple of (success, version). If the update is
+            StoreStatus: A tuple of (success, version). If the update is
                 successful, returns (True, new_version). Otherwise,
                 returns (False, current_version).
         """
@@ -83,7 +89,7 @@ class Storage(metaclass=ABCMeta):
                 all entries in the table will be returned.
 
         Returns:
-            Tuple[Dict[str, Tuple[str, int]], int]: A tuple of
+            Tuple[Dict[str, VersionedValue], int]: A tuple of
                 (entries, storage_version). The entries is a dictionary of
                 (key, (value, entry_version)) pairs. The entry_version is the
                 version of the entry when it was last updated. The
@@ -113,21 +119,24 @@ class InMemoryStorage(Storage):
     def batch_update(
         self,
         table: str,
-        mutation: Dict[str, str] = {},
-        deletion: List[str] = [],
+        mutation: Dict[str, str] = None,
+        deletion: List[str] = None,
         expected_version: Optional[int] = None,
-    ) -> Tuple[bool, int]:
+    ) -> StoreStatus:
+        mutation = mutation if mutation else {}
+        deletion = deletion if deletion else []
         with self._lock:
             if expected_version is not None and expected_version != self._version:
-                return [False, self._version]
+                return StoreStatus(False, self._version)
             self._version += 1
             key_value_pairs_with_version = {
-                key: (value, self._version) for key, value in mutation.items()
+                key: VersionedValue(value, self._version)
+                for key, value in mutation.items()
             }
             self._tables[table].update(key_value_pairs_with_version)
             for deleted_key in deletion:
                 self._tables[table].pop(deleted_key, None)
-            return [True, self._version]
+            return StoreStatus(True, self._version)
 
     def update(
         self,
@@ -137,29 +146,27 @@ class InMemoryStorage(Storage):
         expected_entry_version: Optional[int] = None,
         expected_storage_version: Optional[int] = None,
         insert_only: bool = False,
-    ) -> Tuple[bool, int]:
+    ) -> StoreStatus:
         with self._lock:
             if (
                 expected_storage_version is not None
                 and expected_storage_version != self._version
             ):
-                return [False, self._version]
+                return StoreStatus(False, self._version)
             if insert_only and key in self._tables[table]:
-                return [False, self._version]
+                return StoreStatus(False, self._version)
             _, version = self._tables[table].get(key, (None, -1))
             if expected_entry_version is not None and expected_entry_version != version:
-                return [False, self._version]
+                return StoreStatus(False, self._version)
             self._version += 1
-            self._tables[table][key] = (value, self._version)
-            return [True, self._version]
+            self._tables[table][key] = VersionedValue(value, self._version)
+            return StoreStatus(True, self._version)
 
-    def get_all(self, table: str) -> Tuple[Dict[str, Tuple[str, int]], int]:
+    def get_all(self, table: str) -> Tuple[Dict[str, VersionedValue], int]:
         with self._lock:
             return (copy.deepcopy(self._tables[table]), self._version)
 
-    def get(
-        self, table: str, keys: List[str]
-    ) -> Tuple[Dict[str, Tuple[str, int]], int]:
+    def get(self, table: str, keys: List[str]) -> Tuple[Dict[str, VersionedValue], int]:
         if not keys:
             return self.get_all(table)
         with self._lock:
@@ -167,7 +174,7 @@ class InMemoryStorage(Storage):
             for key in keys:
                 if key in self._tables.get(table, {}):
                     result[key] = self._tables[table][key]
-            return (result, self._version)
+            return StoreStatus(result, self._version)
 
     def get_version(self) -> int:
         return self._version
