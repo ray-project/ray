7a22459c145b99b5f418c91f250ba78b6faea98b
Cuong Nguyen <can@anyscale.com>
2023-05-03 10:39:45 -0700

    Improvements
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- ci/test/ray_coverage.py
+++ ci/test/ray_coverage.py
@@ -3,7 +3,6 @@ import logging
 import os
 import subprocess
 import sys
-from contextlib import contextmanager
 
 
 @click.command()
@@ -11,19 +10,24 @@ from contextlib import contextmanager
 def main(test_name: str) -> None:
     logger = _get_logger()
     logger.info(f"Collecting coverage for test: {test_name}")
-    with _pushd("release"):
-        subprocess.check_output(
-            ["python", "-m", "coverage", "run", "-m", "pytest", test_name]
-        )
-        report = subprocess.check_output(
-            [
-                "python",
-                "-m",
-                "coverage",
-                "report",
-            ]
-        ).decode("utf-8")
-        logger.info(report)
+    subprocess.check_output(
+        [
+            "bazel",
+            "test",
+            test_name,
+            "--test_env=PYTEST_ADDOPTS='--cov=ray_release'",
+            f"--test_env=COVERAGE_FILE='{os.getcwd()}/.coverage'",
+        ]
+    )
+    report = subprocess.check_output(
+        [
+            "python",
+            "-m",
+            "coverage",
+            "report",
+        ]
+    ).decode("utf-8")
+    logger.info(report)
 
     return 0
 
@@ -37,16 +41,6 @@ def _get_logger():
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     return logger
-
-
-@contextmanager
-def _pushd(new_dir):
-    previous_dir = os.getcwd()
-    os.chdir(new_dir)
-    try:
-        yield
-    finally:
-        os.chdir(previous_dir)
 
 
 if __name__ == "__main__":
