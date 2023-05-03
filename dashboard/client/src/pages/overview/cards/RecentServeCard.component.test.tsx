01199cddbe974643416db1e66fc48b11d40b60bf
chaowang <chaowang@anyscale.com>
2023-05-03 18:14:53 +0000

    first case

--- dashboard/client/src/pages/overview/cards/RecentServeCard.component.test.tsx
+++ dashboard/client/src/pages/overview/cards/RecentServeCard.component.test.tsx
@@ -1,22 +1,60 @@
 import { render } from "@testing-library/react";
 import React from "react";
-import { useServeApplications } from "../../serve/hook/useServeApplications";
+import { getServeApplications } from "../../../service/serve";
+import {
+  ServeApplicationStatus,
+  ServeDeploymentMode,
+} from "../../../type/serve";
 import { RecentServeCard } from "./RecentServeCard";
 
-jest.mock("../../serve/hook/useServeApplications");
-const mockedUseServeApplications = jest.mocked(useServeApplications);
-mockedUseServeApplications.mockReturnValue({
-  allServeApplications: [],
-} as any);
+jest.mock("../../../service/serve");
+
+const mockGetServeApplications = jest.mocked(getServeApplications);
 
 describe("RecentServeCard", () => {
-  it("renders correctly with empty Serve Applications", () => {
+  beforeEach(() => {
+    mockGetServeApplications.mockResolvedValue({
+      data: {
+        http_options: { host: "1.2.3.4", port: 8000 },
+        proxy_location: ServeDeploymentMode.EveryNode,
+        applications: {
+          home: {
+            name: "home",
+            route_prefix: "/",
+            message: null,
+            status: ServeApplicationStatus.RUNNING,
+            deployed_app_config: {
+              import_path: "home:graph",
+            },
+            last_deployed_time_s: new Date().getTime() / 1000,
+            deployments: {
+              FirstDeployment: {},
+              SecondDeployment: {},
+            },
+          },
+          "second-app": {
+            name: "second-app",
+            route_prefix: "/second-app",
+            message: null,
+            status: ServeApplicationStatus.DEPLOYING,
+            deployed_app_config: null,
+            last_deployed_time_s: new Date().getTime() / 1000,
+            deployments: {
+              ThirdDeployment: {},
+            },
+          },
+        },
+      },
+    } as any);
+  });
+  it("should display serve applications with and without deployed_app_config", () => {
     const { getByText } = render(<RecentServeCard />);
-    expect(getByText("Recent Applications")).toBeInTheDocument();
-    expect(getByText("No Applications yet...")).toBeInTheDocument();
-    expect(getByText("View all applications")).toHaveAttribute(
-      "href",
-      "/serve",
-    );
+
+    expect(getByText("app1")).toBeInTheDocument();
+    expect(getByText("/app1")).toBeInTheDocument();
+    expect(getByText("dep1")).toBeInTheDocument();
+    expect(getByText("app2")).toBeInTheDocument();
+    expect(getByText("/app2")).toBeInTheDocument();
+    expect(getByText("dep2")).toBeInTheDocument();
   });
 });
