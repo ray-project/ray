1068d7eb0d1a6a79e089316ab62e84c210971073
Cuong Nguyen <can@anyscale.com>
2023-05-04 23:19:23 -0700

    Make function private
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- ci/pipeline/ray_release_test.py
+++ ci/pipeline/ray_release_test.py
@@ -10,19 +10,13 @@ def main() -> int:
     This script determines and runs the right tests for the PR changes. To be used in
     CI and buildkite environment only.
     """
+    logger = get_logger()
     changed_files = _get_changed_files()
-=======
-    changed_files = get_changed_files()
->>>>>>> 0ccebbe38a (Add a script to run tests using coverage information):ci/test/ray_release_test.py
     logger.info(f"Changed files: {changed_files}")
     return 0
 
 
-<<<<<<< HEAD:ci/pipeline/ray_release_test.py
 def _get_changed_files() -> List[str]:
-=======
-def get_changed_files() -> List[str]:
->>>>>>> 0ccebbe38a (Add a script to run tests using coverage information):ci/test/ray_release_test.py
     """
     Get the list of changed files in the current PR.
     """
