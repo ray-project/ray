e5d75db95a95b1cd55a8cf7575417248e278bb57
sven1977 <svenmika1977@gmail.com>
2023-05-06 08:27:11 +0200

    merge
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/algorithms/impala/tests/test_impala_learner.py
+++ rllib/algorithms/impala/tests/test_impala_learner.py
@@ -98,7 +98,7 @@ class TestImpalaLearner(unittest.TestCase):
             learner_group_config.num_learner_workers = 0
             learner_group = learner_group_config.build()
             learner_group.set_weights(algo.get_weights())
-            learner_group.update(train_batch.as_multi_agent())
+            learner_group.update(batches=[train_batch.as_multi_agent()])
 
             algo.stop()
 
