73fbed3e912ebd6405386aef2c7a2b12a9faa8bd
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2023-05-08 11:11:26 -0700

    Add "check permissions" script for testing
    Signed-off-by: Archit Kulkarni <architkulkarni@users.noreply.github.com>

---
+++ python/ray/autoscaler/check_permissions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get the active account email
+ACCOUNT_EMAIL=$(gcloud config get-value account)
+echo "Active account: $ACCOUNT_EMAIL"
+
+# Get the current project
+PROJECT=$(gcloud config get-value project)
+echo "Active project: $PROJECT"
+
+# List all roles and permissions for the active account in the current project
+gcloud projects get-iam-policy $PROJECT --flatten="bindings[].members" --format="table(bindings.role,bindings.members)" --filter="bindings.members:$ACCOUNT_EMAIL"
