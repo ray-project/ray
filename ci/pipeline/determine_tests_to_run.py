0230a03ecf05c8fc502ab2d7d524548719e4f12f
Avnish <avnishnarayan@gmail.com>
2023-05-09 09:50:55 -0700

    Move if statement
    Signed-off-by: Avnish <avnishnarayan@gmail.com>

--- ci/pipeline/determine_tests_to_run.py
+++ ci/pipeline/determine_tests_to_run.py
@@ -181,10 +181,9 @@ if __name__ == "__main__":
                 RAY_CI_RLLIB_DIRECTLY_AFFECTED = 1
                 RAY_CI_LINUX_WHEELS_AFFECTED = 1
                 RAY_CI_MACOS_WHEELS_AFFECTED = 1
-            elif re.match("rllib_contrib/", changed_file) and not changed_file.endswith(
-                ".md"
-            ):
-                RAY_CI_RLLIB_CONTRIB_AFFECTED = 1
+            elif re.match("rllib_contrib/", changed_file):
+                if not changed_file.endswith(".md"):
+                    RAY_CI_RLLIB_CONTRIB_AFFECTED = 1
             elif changed_file.startswith("python/ray/serve"):
                 RAY_CI_DOC_AFFECTED = 1
                 RAY_CI_SERVE_AFFECTED = 1
