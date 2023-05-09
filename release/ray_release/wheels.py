c4bb30e3de804b1434aed90097d50a0ce7e82956
Cuong Nguyen <can@anyscale.com>
2023-05-08 18:33:38 -0700

    testing
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/wheels.py
+++ release/ray_release/wheels.py
@@ -34,6 +34,7 @@ INIT_URL_TPL = (
 
 DEFAULT_REPO = REPO_URL_TPL.format(owner=DEFAULT_GIT_OWNER, package=DEFAULT_GIT_PACKAGE)
 
+#
 # Modules to be reloaded after installing a new local ray version
 RELOAD_MODULES = ["ray", "ray.job_submission"]
 
