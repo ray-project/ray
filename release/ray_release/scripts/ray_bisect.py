a90e499de9ab11477c088dd079d1152bf7016830
Cuong Nguyen <can@anyscale.com>
2023-05-09 12:58:24 -0700

    Bisect with the right python version
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/scripts/ray_bisect.py
+++ release/ray_release/scripts/ray_bisect.py
@@ -8,6 +8,8 @@ from ray_release.logger import logger
 from ray_release.buildkite.step import get_step
 from ray_release.config import (
     read_and_validate_release_test_collection,
+    parse_python_version,
+    DEFAULT_PYTHON_VERSION,
     DEFAULT_WHEEL_WAIT_TIMEOUT,
     Test,
 )
@@ -122,9 +124,13 @@ def _run_test(
 
 
 def _trigger_test_run(test: Test, commit: str, run_per_commit: int) -> None:
+    python_version = DEFAULT_PYTHON_VERSION
+    if "python" in test:
+        python_version = parse_python_version(test["python"])
     ray_wheels_url = find_and_wait_for_ray_wheels_url(
         commit,
         timeout=DEFAULT_WHEEL_WAIT_TIMEOUT,
+        python_version=python_version,
     )
     for run in range(run_per_commit):
         step = get_step(
