95dc7a5a74e2a01be72a6ae0341398ba4d0855ab
sven1977 <svenmika1977@gmail.com>
2023-05-09 14:24:45 +0200

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/algorithms/appo/torch/appo_torch_rl_module.py
+++ rllib/algorithms/appo/torch/appo_torch_rl_module.py
@@ -1,7 +1,6 @@
 from typing import List
 
 from ray.rllib.algorithms.appo.appo_learner import (
-    OLD_ACTION_DIST_KEY,
     OLD_ACTION_DIST_LOGITS_KEY,
 )
 from ray.rllib.algorithms.ppo.torch.ppo_torch_rl_module import PPOTorchRLModule
