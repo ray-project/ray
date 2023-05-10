4d8398d1dac4a53328cb2bfccd6d00fcd1d3ecbb
chaowang <chaowang@anyscale.com>
2023-05-10 19:37:08 +0000

    Change lint errors
    signed-off-by: Chao Wang<chaowang@anyscale.com>

--- dashboard/client/src/components/ListItemCard.tsx
+++ dashboard/client/src/components/ListItemCard.tsx
@@ -1,6 +1,5 @@
 import { createStyles, makeStyles, Typography } from "@material-ui/core";
 import classNames from "classnames";
-import _ from "lodash";
 import React, { ReactNode } from "react";
 import { Link } from "react-router-dom";
 import { ClassNameProps } from "../common/props";
