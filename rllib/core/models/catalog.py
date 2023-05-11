941d27ba25529ee2c29773449121608b40c097c0
sven1977 <svenmika1977@gmail.com>
2023-05-11 07:18:23 +0200

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/core/models/catalog.py
+++ rllib/core/models/catalog.py
@@ -372,7 +372,13 @@ class Catalog:
                         "conv_use_layernorm", False
                     ),
                     output_dims=[encoder_latent_dim],
-                    output_activation=output_activation,
+                    # TODO (sven): Setting this to None here helps with the existing
+                    #  APPO Pong benchmark (actually, leaving this at default=tanh does
+                    #  NOT learn at all!).
+                    #  We need to remove the last Dense layer from CNNEncoder in general
+                    #  AND establish proper ModelConfig objects (instead of hacking
+                    #  everything with the old default model config dict).
+                    output_activation=None,
                 )
             # input_space is a 2D Box
             elif (
