b678cf84a391d9109a4644775f8a3006f640b884
Cuong Nguyen <can@anyscale.com>
2023-05-10 22:19:28 -0700

    yayy
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- ci/pipeline/ray_python_test.py
+++ ci/pipeline/ray_python_test.py
@@ -13,10 +13,40 @@ from ray_coverage import (
 )
 from typing import List, Set
 
-S3_BUCKET_FILE_PREFIX = "ray_python_small_client.cov"
+S3_BUCKET_FILE_PREFIX = "ray_python_"
+TEST_TYPE_TO_BAZEL_CONFIG = {
+    "small": {
+        "test_tag_filters": "client_tests,small_size_python_tests",
+    },
+    "medium_a_j": {
+        "test_tag_filters": "-kubernetes,medium_size_python_tests_a_to_j",
+    },
+    "medium_k_z": {
+        "test_tag_filters": "-kubernetes,medium_size_python_tests_k_to_z",
+    },
+    "external_redis_small": {
+        "test_tag_filters": "client_tests,small_size_python_tests",
+        "test_env": [
+            "TEST_EXTERNAL_REDIS=1",
+        ],
+    },
+    "external_redis_medium_a_j": {
+        "test_tag_filters": "-kubernetes,medium_size_python_tests_a_to_j",
+        "test_env": [
+            "TEST_EXTERNAL_REDIS=1",
+        ],
+    },
+    "external_redis_medium_k_z": {
+        "test_tag_filters": "-kubernetes,medium_size_python_tests_k_to_z",
+        "test_env": [
+            "TEST_EXTERNAL_REDIS=1",
+        ],
+    },
+}
 
 
 @click.command()
+@click.argument("test_type", required=True, type=str)
 @click.option(
     "--artifact-dir",
     default="/artifact-mount",
@@ -26,7 +56,7 @@ S3_BUCKET_FILE_PREFIX = "ray_python_small_client.cov"
         "In buildkite CI, this is usually artifact-mount."
     ),
 )
-def main(artifact_dir: str) -> int:
+def main(test_type: str, artifact_dir: str) -> int:
     """
     This script determines and runs the right tests for the PR changes. To be used in
     CI and buildkite environment only.
@@ -34,44 +64,34 @@ def main(artifact_dir: str) -> int:
     logger = get_logger()
     changed_files = _get_changed_files()
     logger.info(f"Changed files: {changed_files}")
-    test_targets = _get_test_targets_for_changed_files(changed_files, artifact_dir)
+    test_targets = set()
+    for i in range(6):
+        test_targets.update(
+            _get_test_targets_for_changed_files(changed_files, artifact_dir, i),
+        )
     logger.info(f"Found the following test targets to run: {test_targets}")
     if not test_targets:
         logger.info("No test targets found, skipping tests.")
         return 0
-    _run_tests(test_targets)
+    _run_tests(test_type, test_targets)
     return 0
 
 
-def _run_tests(test_targets: Set[str]) -> None:
+def _run_tests(test_type: str, test_targets: Set[str]) -> None:
     """
     Run the tests.
     """
-    subprocess.check_call(
-        [
-            "DL=1",
-            "./ci/env/install-dependencies.sh",
-        ]
-    )
-    subprocess.check_call(
-        [
-            "bash",
-            "./ci/ci.sh",
-            "prepare_docker",
-        ]
-    )
-    subprocess.check_call(
-        [
-            "./ci/env/env_info.sh",
-        ]
-    )
     subprocess.check_call(
         [
             "bazel",
             "test",
             "--config=ci",
             "$(./ci/run/bazel_export_options)",
-            "--test_tag_filters=client_tests,small_size_python_tests",
+            f"--test_tag_filters={TEST_TYPE_TO_BAZEL_CONFIG[test_type]['test_tag_filters']}",
+        ]
+        + [
+            f"--test_env={env}"
+            for env in TEST_TYPE_TO_BAZEL_CONFIG[test_type].get("test_env", [])
         ]
         + list(test_targets)
     )
@@ -80,11 +100,12 @@ def _run_tests(test_targets: Set[str]) -> None:
 def _get_test_targets_for_changed_files(
     changed_files: List[str],
     artifact_dir: str,
+    index: int,
 ) -> Set[str]:
     """
     Get the test target for the changed files.
     """
-    coverage_file = _get_coverage_file(artifact_dir)
+    coverage_file = _get_coverage_file(artifact_dir, index)
     logger = get_logger()
     try:
         subprocess.check_call(
@@ -119,14 +140,18 @@ def _get_test_targets_for_changed_files(
         context = data["contexts"]
         for tests in context.values():
             for test in tests:
-                test_paths = test.split("::")[0].split("/")
-                test_targets.add(
-                    f"//{'/'.join(test_paths[:-1])}:{test_paths[-1][:-3]}",
+                test_paths = (
+                    test.split("::")[0]
+                    .split("com_github_ray_project_ray")[-1]
+                    .split("/")
                 )
+                test_target = f"/{'/'.join(test_paths[:-1])}:{test_paths[-1][:-3]}"
+                if "test" in test_target:
+                    test_targets.add(test_target)
     return test_targets
 
 
-def _get_coverage_file(artifact_dir: str) -> str:
+def _get_coverage_file(artifact_dir: str, index: int) -> str:
     """
     Get the location of the test coverage file.
     """
@@ -137,7 +162,7 @@ def _get_coverage_file(artifact_dir: str) -> str:
     s3 = boto3.client("s3")
     files = s3.list_objects_v2(
         Bucket=S3_BUCKET_NAME,
-        Prefix=f"{S3_BUCKET_DIR}/{S3_BUCKET_FILE_PREFIX}",
+        Prefix=f"{S3_BUCKET_DIR}/ray_python_{index}.cov",
     )["Contents"]
     latest_coverage = sorted(files, key=_get_last_modified)[-1]
     coverage_file_name = os.path.join(artifact_dir, COVERAGE_FILE_NAME)
