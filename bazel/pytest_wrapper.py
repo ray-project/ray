d8fcc6ba785f1fbfa14ba8fec79263de8a5f7048
Balaji Veeramani <balaji@anyscale.com>
2023-05-12 10:06:53 -0700

    Update pytest_wrapper.py
    Signed-off-by: Balaji Veeramani <balaji@anyscale.com>

--- bazel/pytest_wrapper.py
+++ bazel/pytest_wrapper.py
@@ -2,13 +2,9 @@ import sys
 
 import pytest
 
-import ray
-
 if __name__ == "__main__":
     exit_code = pytest.main(sys.argv[1:])
     if exit_code is pytest.ExitCode.NO_TESTS_COLLECTED:
         exit_code = pytest.ExitCode.OK
-
-    ray.shutdown()
 
     sys.exit(exit_code)
