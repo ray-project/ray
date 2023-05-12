6705b21aeea8ee466bff2a3f9c5fbd4ac84550ca
Sihan Wang <sihanwang41@gmail.com>
2023-05-12 08:49:01 -0700

    [Serve] Add route tags with custom metrics (#35246)
    - Add route value automatically with custom metrics
    - Fix some metrics tests.
    
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- python/ray/serve/tests/test_metrics.py
+++ python/ray/serve/tests/test_metrics.py
@@ -406,6 +406,10 @@ class TestRequestContextMetrics:
             ]
         return metrics_summary_route, metrics_summary_app
 
+    def verify_metrics(self, metric, expected_output):
+        for key in expected_output:
+            assert metric[key] == expected_output[key]
+
     def test_request_context_pass_for_http_proxy(self, serve_start_shutdown):
         """Test HTTP proxy passing request context"""
 
@@ -475,7 +479,7 @@ class TestRequestContextMetrics:
             "serve_deployment_processing_latency_ms_sum",
         ]:
             metrics_route, metrics_app_name = self._generate_metrics_summary(
-                get_metric_dictionaries("serve_handle_request_counter")
+                get_metric_dictionaries(metric_name)
             )
             assert metrics_route["app1_f"] == {"/app1"}
             assert metrics_route["app2_g"] == {"/app2"}
@@ -552,6 +556,7 @@ class TestRequestContextMetrics:
                     tag_keys=(
                         "my_static_tag",
                         "my_runtime_tag",
+                        "route",
                     ),
                 )
                 self.counter.set_default_tags({"my_static_tag": "static_value"})
@@ -562,6 +567,7 @@ class TestRequestContextMetrics:
                     tag_keys=(
                         "my_static_tag",
                         "my_runtime_tag",
+                        "route",
                     ),
                 )
                 self.histogram.set_default_tags({"my_static_tag": "static_value"})
@@ -571,6 +577,7 @@ class TestRequestContextMetrics:
                     tag_keys=(
                         "my_static_tag",
                         "my_runtime_tag",
+                        "route",
                     ),
                 )
                 self.gauge.set_default_tags({"my_static_tag": "static_value"})
@@ -591,29 +598,42 @@ class TestRequestContextMetrics:
             lambda: len(get_metric_dictionaries("my_gauge")) == 1,
             timeout=20,
         )
+
         counter_metrics = get_metric_dictionaries("my_counter")
         assert len(counter_metrics) == 1
-        counter_metrics[0]["my_static_tag"] == "static_value"
-        counter_metrics[0]["my_runtime_tag"] == "100"
-        counter_metrics[0]["replica"] == replica_tag
-        counter_metrics[0]["deployment"] == deployment_name
-        counter_metrics[0]["application"] == "app"
+        expected_metrics = {
+            "my_static_tag": "static_value",
+            "my_runtime_tag": "100",
+            "replica": replica_tag,
+            "deployment": deployment_name,
+            "application": "app",
+            "route": "/app",
+        }
+        self.verify_metrics(counter_metrics[0], expected_metrics)
 
+        expected_metrics = {
+            "my_static_tag": "static_value",
+            "my_runtime_tag": "300",
+            "replica": replica_tag,
+            "deployment": deployment_name,
+            "application": "app",
+            "route": "/app",
+        }
         gauge_metrics = get_metric_dictionaries("my_gauge")
         assert len(counter_metrics) == 1
-        gauge_metrics[0]["my_static_tag"] == "static_value"
-        gauge_metrics[0]["my_runtime_tag"] == "300"
-        gauge_metrics[0]["replica"] == replica_tag
-        gauge_metrics[0]["deployment"] == deployment_name
-        gauge_metrics[0]["application"] == "app"
+        self.verify_metrics(gauge_metrics[0], expected_metrics)
 
+        expected_metrics = {
+            "my_static_tag": "static_value",
+            "my_runtime_tag": "200",
+            "replica": replica_tag,
+            "deployment": deployment_name,
+            "application": "app",
+            "route": "/app",
+        }
         histogram_metrics = get_metric_dictionaries("my_histogram_sum")
         assert len(histogram_metrics) == 1
-        histogram_metrics[0]["my_static_tag"] == "static_value"
-        histogram_metrics[0]["my_runtime_tag"] == "200"
-        histogram_metrics[0]["replica"] == replica_tag
-        histogram_metrics[0]["deployment"] == deployment_name
-        gauge_metrics[0]["application"] == "app"
+        self.verify_metrics(histogram_metrics[0], expected_metrics)
 
     @pytest.mark.parametrize("use_actor", [False, True])
     def test_serve_metrics_outside_serve(self, use_actor, serve_start_shutdown):
@@ -714,20 +734,30 @@ class TestRequestContextMetrics:
             lambda: len(get_metric_dictionaries("my_gauge")) == 1,
             timeout=20,
         )
+
         counter_metrics = get_metric_dictionaries("my_counter")
         assert len(counter_metrics) == 1
-        counter_metrics[0]["my_static_tag"] == "static_value"
-        counter_metrics[0]["my_runtime_tag"] == "100"
+        expected_metrics = {
+            "my_static_tag": "static_value",
+            "my_runtime_tag": "100",
+        }
+        self.verify_metrics(counter_metrics[0], expected_metrics)
 
         gauge_metrics = get_metric_dictionaries("my_gauge")
         assert len(counter_metrics) == 1
-        gauge_metrics[0]["my_static_tag"] == "static_value"
-        gauge_metrics[0]["my_runtime_tag"] == "300"
+        expected_metrics = {
+            "my_static_tag": "static_value",
+            "my_runtime_tag": "300",
+        }
+        self.verify_metrics(gauge_metrics[0], expected_metrics)
 
         histogram_metrics = get_metric_dictionaries("my_histogram_sum")
         assert len(histogram_metrics) == 1
-        histogram_metrics[0]["my_static_tag"] == "static_value"
-        histogram_metrics[0]["my_runtime_tag"] == "200"
+        expected_metrics = {
+            "my_static_tag": "static_value",
+            "my_runtime_tag": "200",
+        }
+        self.verify_metrics(histogram_metrics[0], expected_metrics)
 
 
 def test_actor_summary(serve_instance):
