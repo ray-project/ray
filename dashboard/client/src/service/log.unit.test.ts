d39423b8ab2a95484803b86eb0f73ad464efb62a
Alan Guo <aguo@anyscale.com>
2023-05-12 15:16:44 -0700

    fixup
    Signed-off-by: Alan Guo <aguo@anyscale.com>

---
+++ dashboard/client/src/service/log.unit.test.ts
@@ -0,0 +1,63 @@
+import { getStateApiDownloadLogUrl } from "./log";
+
+describe("getStateApiDownloadLogUrl", () => {
+  it("only uses parameters provided but doesn't fetch when parameters are null", () => {
+    expect.assertions(8);
+
+    expect(
+      getStateApiDownloadLogUrl({
+        nodeId: "node-id",
+        filename: "file.log",
+      }),
+    ).toStrictEqual("api/v0/logs/file?node_id=node-id&filename=file.log");
+
+    expect(
+      getStateApiDownloadLogUrl({
+        taskId: "task-id",
+        suffix: "err",
+      }),
+    ).toStrictEqual("api/v0/logs/file?task_id=task-id&suffix=err");
+
+    expect(
+      getStateApiDownloadLogUrl({
+        taskId: "task-id",
+        suffix: "out",
+      }),
+    ).toStrictEqual("api/v0/logs/file?task_id=task-id&suffix=out");
+
+    expect(
+      getStateApiDownloadLogUrl({
+        actorId: "actor-id",
+        suffix: "err",
+      }),
+    ).toStrictEqual("api/v0/logs/file?actor_id=actor-id&suffix=err");
+
+    expect(
+      getStateApiDownloadLogUrl({
+        nodeId: null,
+        filename: "file.log",
+      }),
+    ).toBeNull();
+
+    expect(
+      getStateApiDownloadLogUrl({
+        nodeId: null,
+        filename: null,
+      }),
+    ).toBeNull();
+
+    expect(
+      getStateApiDownloadLogUrl({
+        taskId: null,
+        suffix: "err",
+      }),
+    ).toBeNull();
+
+    expect(
+      getStateApiDownloadLogUrl({
+        actorId: null,
+        suffix: "err",
+      }),
+    ).toBeNull();
+  });
+});
