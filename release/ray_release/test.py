385a449ece8dcbbb5e1fb46c33bef1f0ad886269
Cuong Nguyen <can@anyscale.com>
2023-05-16 13:10:25 -0700

    Rebase
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/test.py
+++ release/ray_release/test.py
@@ -50,6 +50,7 @@ class Test(dict):
         Returns the ray docker image to use for this test. If the commit hash is not
         specified, use the nightly ray image.
         """
+        # TDOD(can): re-enable this test once we have a custom image
         # ray_version = os.environ.get("BUILDKITE_COMMIT", "")[:6] or "nightly"
         ray_version = "nightly"
         ray_project = "ray-ml" if self.get_byod_type() == "gpu" else "ray"
