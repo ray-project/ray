1e9b9934500421e2f340c8f5e92ba6e5ece3f4ac
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2023-05-16 00:27:28 +0000

    fix Signed-off-by: Yi Cheng <74173148+iycheng@users.noreply.github.com>

--- python/ray/tests/test_actor_advanced.py
+++ python/ray/tests/test_actor_advanced.py
@@ -1076,8 +1076,8 @@ def test_actor_timestamps(ray_start_regular):
         time.sleep(1)
         actor.kill_self.remote()
         time.sleep(1)
+        wait_for_condition(lambda: ray._private.state.actors()[actor_id]["EndTime"] != 0)
         state_after_ending = ray._private.state.actors()[actor_id]
-
         assert state_after_starting["StartTime"] == state_after_ending["StartTime"]
 
         start_time = state_after_ending["StartTime"]
@@ -1085,7 +1085,7 @@ def test_actor_timestamps(ray_start_regular):
         lapsed = end_time - start_time
 
         assert end_time > start_time > 0, f"Start: {start_time}, End: {end_time}"
-        assert 500 < lapsed < 1500, f"Start: {start_time}, End: {end_time}"
+        assert 1500 < lapsed < 2500, f"Start: {start_time}, End: {end_time}"
 
     def restarted():
         actor = Foo.options(max_restarts=1, max_task_retries=-1).remote()
