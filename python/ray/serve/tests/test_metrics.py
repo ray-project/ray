53c638fd1b977e0ca5f23317792b880eca6aa8da
Sihan Wang <sihanwang41@gmail.com>
2023-05-16 15:05:56 -0700

    [Serve] Mutliplexed information report impl (#35372)
    - Multiplexed metrics.
    - Pass multiplexed information into controller and make it available at `RunningReplicaInfo`.

--- python/ray/serve/tests/test_metrics.py
+++ python/ray/serve/tests/test_metrics.py
@@ -760,6 +760,49 @@ class TestRequestContextMetrics:
         self.verify_metrics(histogram_metrics[0], expected_metrics)
 
 
+def test_multiplexed_metrics(serve_start_shutdown):
+    """Tests multiplexed API corresponding metrics."""
+
+    @serve.deployment
+    class Model:
+        @serve.multiplexed(max_num_models_per_replica=2)
+        async def get_model(self, model_id: str):
+            return model_id
+
+        async def __call__(self, model_id: str):
+            await self.get_model(model_id)
+            return
+
+    handle = serve.run(Model.bind(), name="app", route_prefix="/app")
+    handle.remote("model1")
+    handle.remote("model2")
+    # Trigger model eviction.
+    handle.remote("model3")
+    expected_metrics = [
+        "serve_multiplexed_model_load_latency_s",
+        "serve_multiplexed_model_unload_latency_s",
+        "serve_num_multiplexed_models",
+        "serve_multiplexed_models_load_counter",
+        "serve_multiplexed_models_unload_counter",
+    ]
+
+    def verify_metrics():
+        try:
+            resp = requests.get("http://127.0.0.1:9999").text
+        # Requests will fail if we are crashing the controller
+        except requests.ConnectionError:
+            return False
+        for metric in expected_metrics:
+            assert metric in resp
+        return True
+
+    wait_for_condition(
+        verify_metrics,
+        timeout=20,
+        retry_interval_ms=1000,
+    )
+
+
 def test_actor_summary(serve_instance):
     @serve.deployment
     def f():
