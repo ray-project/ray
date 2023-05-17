acf0afbd7e6fee41834a4c7e1bdfb8503cbf6bb5
Cuong Nguyen <can@anyscale.com>
2023-05-17 11:19:00 -0700

    Build byod on a PR
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/test.py
+++ release/ray_release/test.py
@@ -45,14 +45,26 @@ class Test(dict):
         """
         return self.get("python", ".".join(str(v) for v in DEFAULT_PYTHON_VERSION))
 
+    def _get_ray_image_for_pr(self) -> str:
+        """
+        Returns the ray docker image built for a PR
+        """
+        commit = os.environ.get("BUILDKITE_COMMIT")
+        if not commit:
+            raise ValueError("BUILDKITE_COMMIT is not set for a PR build")
+        if self.get_byod_type() == "gpu":
+            return f"{RAY_CI_ERC_REPO}:oss-ci-gpu_{commit}"
+        else:
+            return f"{RAY_CI_ERC_REPO}:oss-ci-build_{commit}"
+
     def get_ray_image(self) -> str:
         """
         Returns the ray docker image to use for this test. If the commit hash is not
         specified, use the nightly ray image.
         """
-        # TDOD(can): re-enable this test once we have a custom image
-        # ray_version = os.environ.get("BUILDKITE_COMMIT", "")[:6] or "nightly"
-        ray_version = "nightly"
+        if os.environ.get("BUILDKITE_PULL_REQUEST"):
+            return self._get_ray_image_for_pr()
+        ray_version = os.environ.get("BUILDKITE_COMMIT", "")[:6] or "nightly"
         ray_project = "ray-ml" if self.get_byod_type() == "gpu" else "ray"
         image_suffix = "-gpu" if self.get_byod_type() == "gpu" else ""
         python_version = f"py{self.get_python_version().replace('.',   '')}"
@@ -62,8 +74,13 @@ class Test(dict):
         """
         Returns the anyscale byod image to use for this test.
         """
-        tag = self.get_ray_image().replace("rayproject/", "").replace(":", "-")
-        return f"{DOCKER_REPO}:{tag}"
+        tag = (
+            self.get_ray_image()
+            .replace("rayproject/", "")
+            .replace(RAY_CI_ERC_REPO, "")
+            .replace(":", "-")
+        )
+        return f"{DATAPLANE_ECR_REPO}:{tag}"
 
 
 class TestDefinition(dict):
--- release/ray_release/tests/test_test.py
+++ release/ray_release/tests/test_test.py
@@ -4,7 +4,7 @@ import pytest
 
 from ray_release.test import (
     Test,
-    DOCKER_REPO,
+    DATAPLANE_ECR_REPO,
 )
 
 
@@ -49,7 +49,10 @@ def test_get_ray_image():
 
 def test_get_anyscale_byod_image():
     os.environ.pop("BUILDKITE_COMMIT", None)
-    assert _stub_test({}).get_anyscale_byod_image() == f"{DOCKER_REPO}:ray-nightly-py37"
+    assert (
+        _stub_test().get_anyscale_byod_image()
+        == f"{DATAPLANE_ECR_REPO}:ray-nightly-py37"
+    )
     os.environ["BUILDKITE_COMMIT"] = "1234567890"
     # TODO(can): re-enable this test once we have a custom image
     # assert Test().get_anyscale_byod_image() == "anyscale/ray:123456-py37"
