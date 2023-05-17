e2f66327a901d2f565f1b2c66cc47fe43986d899
Cade Daniel <edacih@gmail.com>
2023-05-17 11:12:31 -0700

    Revert "[core] Sending ReportWorkerFailure after the process died. (#35320)"
    This reverts commit 9cd509723494eb861c77a989cfa880ec384d31c6.

--- python/ray/tests/test_usage_stats.py
+++ python/ray/tests/test_usage_stats.py
@@ -247,12 +247,6 @@ def test_worker_crash_increment_stats():
             timeout=4,
         )
 
-        wait_for_condition(
-            lambda: "worker_crash_oom"
-            in ray_usage_lib.get_extra_usage_tags_to_report(gcs_client),
-            timeout=4,
-        )
-
         result = ray_usage_lib.get_extra_usage_tags_to_report(gcs_client)
 
         assert "worker_crash_system_error" in result
