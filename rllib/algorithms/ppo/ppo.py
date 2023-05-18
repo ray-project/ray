1f205bf9d4d900058a8d1276f21f565d34e756cc
Artur Niederfahrenhorst <attaismyname@googlemail.com>
2023-05-17 17:59:00 -0700

    Use function calls to disable learner/rlmodule in validate
    Signed-off-by: Artur Niederfahrenhorst <attaismyname@googlemail.com>

--- rllib/algorithms/ppo/ppo.py
+++ rllib/algorithms/ppo/ppo.py
@@ -137,7 +137,7 @@ class PPOConfig(PGConfig):
 
         # enable the rl module api by default
         self.rl_module(_enable_rl_module_api=True)
-        self._enable_learner_api = True
+        self.training(_enable_learner_api=True)
 
     @override(AlgorithmConfig)
     def get_default_rl_module_spec(self) -> SingleAgentRLModuleSpec:
@@ -292,11 +292,10 @@ class PPOConfig(PGConfig):
 
     @override(AlgorithmConfig)
     def validate(self) -> None:
-
-        # Turn RLModule and Learner API on by default (only for torch and tf2)
+        # Can not use Tf with learner api.
         if self.framework_str == "tf":
-            self._enable_rl_module_api = False
-            self._enable_learner_api = False
+            self.rl_module(_enable_rl_module_api=False)
+            self.training(_enable_learner_api=False)
 
         # Call super's validation method.
         super().validate()
