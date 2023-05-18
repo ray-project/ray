ab6bfc1df02cfecc60d5ccea0a288de2e7d822f0
Hao Chen <chenh1024@gmail.com>
2023-05-18 11:55:45 -0700

    Revert "Fix "ImportError: sys.meta_path is None, Python is likely shutting down" (#35304)"
    This reverts commit 1c859f0ff0ab5db5d4fb50a83d718b45d0fbe6d3.

--- python/ray/data/dataset.py
+++ python/ray/data/dataset.py
@@ -4390,6 +4390,8 @@ class Dataset:
         self._current_executor = None
 
     def __del__(self):
+        if sys.meta_path is None:
+            return
         if self._current_executor and ray is not None and ray.is_initialized():
             self._current_executor.shutdown()
 
