b00268b46797f2f1b68e624cecc757ce40c14898
Guyang Song <guyang.sgy@gmail.com>
2023-05-19 19:30:30 +0800

    Update ci.sh
    Signed-off-by: Guyang Song <guyang.sgy@gmail.com>

--- ci/ci.sh
+++ ci/ci.sh
@@ -471,9 +471,9 @@ build_wheels() {
         docker run --rm -w /ray -v "${PWD}":/ray "${MOUNT_BAZEL_CACHE[@]}" \
         "${IMAGE_NAME}:${IMAGE_TAG}" /ray/python/build-wheel-manylinux2014.sh
         # Build Jar on manylinux2014 to resolve incompatibilities.
-        if [[ "${LINUX_JARS}"  == "1" ]]; then
+        if [ -z "${LINUX_JARS}" ]; then
           docker run --rm -w /ray -v "${PWD}":/ray "${MOUNT_BAZEL_CACHE[@]}" \
-          "${IMAGE_NAME}:${IMAGE_TAG}" /ray/java/build-jar-multiplatform.sh linux
+          "${IMAGE_NAME}:${IMAGE_TAG}" /ray/java/build-jar-multiplatform.sh many-linux
         fi
       else
         rm -rf /ray-mount/*
@@ -485,9 +485,9 @@ build_wheels() {
         docker run --rm -v /ray:/ray-mounted ubuntu:focal ls /ray-mounted
         docker run --rm -w /ray -v /ray:/ray "${MOUNT_BAZEL_CACHE[@]}" \
           "${IMAGE_NAME}:${IMAGE_TAG}" /ray/python/build-wheel-manylinux2014.sh
-        if [[ "${LINUX_JARS}"  == "1" ]]; then
+        if [-z "${LINUX_JARS}" ]; then
           docker run --rm -w /ray -v /ray:/ray "${MOUNT_BAZEL_CACHE[@]}" \
-          "${IMAGE_NAME}:${IMAGE_TAG}" /ray/java/build-jar-multiplatform.sh linux
+          "${IMAGE_NAME}:${IMAGE_TAG}" /ray/java/build-jar-multiplatform.sh many-linux
         fi
         cp -rT /ray-mount /ray # copy new files back here
         find . | grep whl # testing
