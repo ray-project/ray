b240e5693ebf0b4c2a01cbcc18c17c6080b78914
Cuong Nguyen <can@anyscale.com>
2023-05-19 10:59:36 -0700

    Rebase
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/byod/build.py
+++ release/ray_release/byod/build.py
@@ -4,6 +4,7 @@ import boto3
 import hashlib
 import subprocess
 import sys
+import time
 
 from ray_release.logger import logger
 from ray_release.test import Test
@@ -22,43 +23,57 @@ def build_anyscale_byod_images(tests: List[Test]) -> None:
     for test in tests:
         if not test.is_byod_cluster():
             continue
-        ray_image = test.get_ray_image()
-        if ray_image in built:
-            continue
-        byod_image = test.get_anyscale_byod_image()
-        logger.info(f"Building {byod_image} from {ray_image}")
-        with open(DATAPLANE_FILENAME, "rb") as build_context:
-            subprocess.check_call(
-                [
-                    "docker",
-                    "build",
-                    "--build-arg",
-                    f"BASE_IMAGE={ray_image}",
-                    "-t",
-                    byod_image,
-                    "-",
-                ],
-                stdin=build_context,
-                stdout=sys.stderr,
-                env={"DOCKER_BUILDKIT": "1"},
-            )
-            subprocess.check_call(
-                ["docker", "push", byod_image],
-                stdout=subprocess.DEVNULL,
-            )
-            built.add(ray_image)
+        to_be_built[test.get_ray_image()] = test.get_anyscale_byod_image()
+
+    timeout = 0
+    # ray images are built on post-merge, so we can wait for them to be available
+    while len(built) < len(to_be_built) and timeout < 7200:
+        for ray_image, byod_image in to_be_built.items():
+            if _byod_image_exist(byod_image.replace(f"{DATAPLANE_ECR_REPO}:", "")):
+                logger.info(f"Image {byod_image} already exists")
+                built.add(ray_image)
+                continue
+            logger.info(f"Building {byod_image} from {ray_image}")
+            with open(DATAPLANE_FILENAME, "rb") as build_file:
+                try:
+                    subprocess.check_call(
+                        [
+                            "docker",
+                            "build",
+                            "--build-arg",
+                            f"BASE_IMAGE={ray_image}",
+                            "-t",
+                            byod_image,
+                            "-",
+                        ],
+                        stdin=build_file,
+                        stdout=subprocess.DEVNULL,
+                        env={"DOCKER_BUILDKIT": "1"},
+                    )
+                except subprocess.CalledProcessError:
+                    # If the ray image does not exist yet, we will retry later
+                    logger.info(f"Retry for another {7200 - timeout}s ...")
+                    time.sleep(30)
+                    timeout += 30
+                    continue
+                subprocess.check_call(
+                    ["docker", "push", byod_image],
+                    stdout=subprocess.DEVNULL,
+                )
+                built.add(ray_image)
+    return
 
 
-def _download_dataplane_build_file() -> None:
+def _byod_image_exist(image_tag: str) -> bool:
     """
-    Downloads the dataplane build file from S3.
+    Checks if the given Anyscale BYOD image exists.
     """
-    s3 = boto3.client("s3")
-    s3.download_file(
-        Bucket=DATAPLANE_S3_BUCKET,
-        Key=DATAPLANE_FILENAME,
-        Filename=DATAPLANE_FILENAME,
-    )
-    with open(DATAPLANE_FILENAME, "rb") as build_context:
-        digest = hashlib.sha256(build_context.read()).hexdigest()
-        assert digest == DATAPLANE_DIGEST, "Mismatched dataplane digest found!"
+    client = boto3.client("ecr")
+    try:
+        client.describe_images(
+            repositoryName="anyscale",
+            imageIds=[{"imageTag": image_tag}],
+        )
+        return True
+    except client.exceptions.ImageNotFoundException:
+        return False
