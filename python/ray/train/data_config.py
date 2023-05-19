b25e97c5a928f1dd9d3213a3c96333f309fa3bd6
Eric Liang <ekhliang@gmail.com>
2023-05-19 13:37:20 -0700

    pydocs
    Signed-off-by: Eric Liang <ekhliang@gmail.com>

--- python/ray/train/data_config.py
+++ python/ray/train/data_config.py
@@ -17,11 +17,26 @@ from ray.data.preprocessor import Preprocessor
 
 @PublicAPI
 class DataConfig:
+    """Class responsible for configuring Train dataset preprocessing.
+
+    For advanced use cases, this class can be subclassed and the `configure()` method
+    overriden for custom data preprocessing.
+    """
+
     def __init__(
         self,
         datasets_to_split: Optional[List[str]] = None,
         execution_options: Optional[ExecutionOptions] = None,
     ):
+        """Construct a DataConfig.
+
+        Args:
+            datasets_to_split: The list of dataset names to split between workers.
+                By default, only the "train" dataset will be split.
+            execution_options: The execution options to pass to Ray Data. By default,
+                the options will be optimized for data ingest. When overriding this,
+                base your options off of `DataConfig.default_ingest_options()`.
+        """
         self._datasets_to_split: List[str] = (
             datasets_to_split if datasets_to_split is not None else [TRAIN_DATASET_KEY]
         )
@@ -38,6 +53,20 @@ class DataConfig:
         worker_node_ids: Optional[List[NodeIdStr]],
         **kwargs,
     ) -> List[Dict[str, DataIterator]]:
+        """Configure how Train datasets should be assigned to workers.
+
+        Args:
+            datasets: The datasets dict passed to Train by the user.
+            world_size: The number of Train workers in total.
+            worker_handles: The actor handles of the Train workers.
+            worker_node_ids: The node ids of the Train workers.
+            kwargs: Forwards compatibility placeholder.
+
+        Returns:
+            A list of dataset splits for each worker. The size of the list must be
+            equal to `world_size`. Each element of the list contains the assigned
+            `DataIterator` instances by name for the worker.
+        """
         output = [{} for i in range(world_size)]
 
         for name, ds in datasets.items():
@@ -64,7 +93,6 @@ class DataConfig:
         the node the data will be consumed. We also set the object store memory limit
         to a fixed smaller value, to avoid using too much memory per Train worker.
         """
-
         return ExecutionOptions(
             locality_with_output=True,
             resource_limits=ExecutionResources(object_store_memory=2e9),
@@ -73,11 +101,19 @@ class DataConfig:
     def _legacy_preprocessing(
         self, datasets: Dict[str, Dataset], preprocessor: Optional[Preprocessor]
     ) -> Dict[str, Dataset]:
-        """Legacy hook for backwards compatiblity; will be removed in future."""
+        """Legacy hook for backwards compatiblity.
+
+        This will be removed in the future.
+        """
         return datasets  # No-op for non-legacy configs.
 
 
 class _LegacyDataConfigWrapper(DataConfig):
+    """Backwards-compatibility wrapper for the legacy dict-based datasets config.
+
+    This will be removed in the future.
+    """
+
     def __init__(
         self,
         cls_config: Dict[str, DatasetConfig],
