1815c22889d1a830128869d2382b3090ee022fb7
Cuong Nguyen <can@anyscale.com>
2023-05-19 10:27:28 -0700

    Rebase
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/scripts/build_pipeline.py
+++ release/ray_release/scripts/build_pipeline.py
@@ -6,7 +6,6 @@ import sys
 import tempfile
 from typing import Optional
 
-import boto3
 import click
 
 from ray_release.buildkite.filter import filter_tests, group_tests
@@ -17,11 +16,6 @@ from ray_release.config import (
     read_and_validate_release_test_collection,
     DEFAULT_WHEEL_WAIT_TIMEOUT,
     parse_python_version,
-)
-from ray_release.test import (
-    Test,
-    S3_BUCKET,
-    DATAPLANE_FILENAME,
 )
 from ray_release.exception import ReleaseTestCLIError, ReleaseTestConfigError
 from ray_release.logger import logger
@@ -244,48 +238,6 @@ def main(
 
     steps_str = json.dumps(steps)
     print(steps_str)
-
-
-def _build_anyscale_byod_images(tests: List[Tuple[Test, bool]]) -> None:
-    """
-    Builds the Anyscale BYOD images for the given tests.
-    """
-    s3 = boto3.client("s3")
-    s3.download_file(
-        Bucket=S3_BUCKET,
-        Key=DATAPLANE_FILENAME,
-        Filename=DATAPLANE_FILENAME,
-    )
-    built = set()
-    for test, _ in tests:
-        if not test.is_byod_cluster():
-            continue
-        ray_image = test.get_ray_image()
-        if ray_image in built:
-            continue
-        byod_image = test.get_anyscale_byod_image()
-        logger.info(f"Building {byod_image} from {ray_image}")
-        with open(DATAPLANE_FILENAME, "rb") as build_file:
-            subprocess.check_call(
-                [
-                    "docker",
-                    "build",
-                    "--build-arg",
-                    f"BASE_IMAGE={ray_image}",
-                    "-t",
-                    byod_image,
-                    "-",
-                ],
-                stdin=build_file,
-                stdout=subprocess.DEVNULL,
-                env={"DOCKER_BUILDKIT": "1"},
-            )
-            subprocess.check_call(
-                ["docker", "push", byod_image],
-                stdout=subprocess.DEVNULL,
-            )
-            built.add(ray_image)
-    return
 
 
 if __name__ == "__main__":
