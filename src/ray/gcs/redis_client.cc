fd8659adc1b56ebee4fe49b51137874744abf50f
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2023-05-19 16:00:09 -0700

    Revert "[core] Serialize requests in redis store client. (#35123)"
    This reverts commit 8b264df18bbdbf96c79f94ac22b528aaadc6efdd.

--- src/ray/gcs/redis_client.cc
+++ src/ray/gcs/redis_client.cc
@@ -220,9 +220,10 @@ void RedisClient::Disconnect() {
 }
 
 std::shared_ptr<RedisContext> RedisClient::GetShardContext(const std::string &shard_key) {
-  // TODO (iycheng) Remove shard context from RedisClient
-  RAY_CHECK(shard_contexts_.size() == 1);
-  return shard_contexts_[0];
+  RAY_CHECK(!shard_contexts_.empty());
+  static std::hash<std::string> hash;
+  size_t index = hash(shard_key) % shard_contexts_.size();
+  return shard_contexts_[index];
 }
 
 int RedisClient::GetNextJobID() {
