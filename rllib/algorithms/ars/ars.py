0a1f435534487da0970d99a7c81122869f15e8bc
Artur Niederfahrenhorst <artur@anyscale.com>
2023-05-21 19:49:38 +0200

    [RLlib] Fit ES and ARS results dict to rest of RLlib, enable results to be fetched in release tests and and CI learning tests (#35533)
    Signed-off-by: Artur Niederfahrenhorst <attaismyname@googlemail.com>

--- rllib/algorithms/ars/ars.py
+++ rllib/algorithms/ars/ars.py
@@ -539,12 +539,16 @@ class ARS(Algorithm):
             "episodes_this_iter": noisy_lengths.size,
             "episodes_so_far": self.episodes_so_far,
         }
-        result = dict(
-            episode_reward_mean=np.mean(self.reward_list[-self.report_length :]),
-            episode_len_mean=eval_lengths.mean(),
-            timesteps_this_iter=noisy_lengths.sum(),
-            info=info,
-        )
+
+        reward_mean = np.mean(self.reward_list[-self.report_length :])
+        result = {
+            "sampler_results": {
+                "episode_reward_mean": reward_mean,
+                "episode_len_mean": eval_lengths.mean(),
+            },
+            "timesteps_this_iter": noisy_lengths.sum(),
+            "info": info,
+        }
 
         return result
 
