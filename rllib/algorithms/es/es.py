e7d92f331431b8cac91033232fb0b72df873ed46
Artur Niederfahrenhorst <attaismyname@googlemail.com>
2023-05-20 19:12:51 -0700

    Swap sampler_results/xy keys for dict in ars.py and es.py
    Signed-off-by: Artur Niederfahrenhorst <attaismyname@googlemail.com>

--- rllib/algorithms/es/es.py
+++ rllib/algorithms/es/es.py
@@ -531,8 +531,10 @@ class ES(Algorithm):
 
         reward_mean = np.mean(self.reward_list[-self.report_length :])
         result = {
-            "sampler_results/episode_reward_mean": reward_mean,
-            "sampler_results/episode_len_mean": eval_lengths.mean(),
+            "sampler_results": {
+                "episode_reward_mean": reward_mean,
+                "episode_len_mean": eval_lengths.mean(),
+            },
             "timesteps_this_iter": noisy_lengths.sum(),
             "info": info,
         }
