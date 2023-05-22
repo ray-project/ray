fb4e3ef0099888767b875042923e5dda7486af5c
Cuong Nguyen <can@anyscale.com>
2023-05-22 16:35:34 -0700

    @aslonnie's comments
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/test.py
+++ release/ray_release/test.py
@@ -4,7 +4,8 @@ from typing import Optional, List
 DEFAULT_PYTHON_VERSION = tuple(
     int(v) for v in os.environ.get("RELEASE_PY", "3.7").split(".")
 )
-DATAPLANE_ECR_REPO = "029272617770.dkr.ecr.us-west-2.amazonaws.com/anyscale"
+DATAPLANE_ECR_REPO = "029272617770.dkr.ecr.us-west-2.amazonaws.com/anyscale/ray"
+DATAPLANE_ECR_ML_REPO = "029272617770.dkr.ecr.us-west-2.amazonaws.com/anyscale/ray-ml"
 
 
 class Test(dict):
@@ -45,27 +46,32 @@ class Test(dict):
         """
         return self.get("python", ".".join(str(v) for v in DEFAULT_PYTHON_VERSION))
 
-    def get_ray_image(self) -> str:
-        """
-        Returns the ray docker image to use for this test. If the commit hash is not
-        specified, use the nightly ray image.
-        """
+    def _get_image_tag(self) -> str:
         ray_version = (
             os.environ.get("COMMIT_TO_TEST", "")[:6]
             or os.environ.get("BUILDKITE_COMMIT", "")[:6]
-            or "nightly"
         )
-        ray_project = "ray-ml" if self.get_byod_type() == "gpu" else "ray"
         image_suffix = "-gpu" if self.get_byod_type() == "gpu" else ""
         python_version = f"py{self.get_python_version().replace('.',   '')}"
-        return f"rayproject/{ray_project}:{ray_version}-{python_version}{image_suffix}"
+        return f"{ray_version}-{python_version}{image_suffix}"
+
+    def get_ray_image(self) -> str:
+        """
+        Returns the ray docker image to use for this test.
+        """
+        ray_project = "ray-ml" if self.get_byod_type() == "gpu" else "ray"
+        return f"rayproject/{ray_project}:{self._get_image_tag()}"
 
     def get_anyscale_byod_image(self) -> str:
         """
         Returns the anyscale byod image to use for this test.
         """
-        tag = self.get_ray_image().replace("rayproject/", "").replace(":", "-")
-        return f"{DATAPLANE_ECR_REPO}:{tag}"
+        repo = (
+            DATAPLANE_ECR_ML_REPO
+            if self.get_byod_type() == "gpu"
+            else DATAPLANE_ECR_REPO
+        )
+        return f"{repo}:{self._get_image_tag()}"
 
 
 class TestDefinition(dict):
--- release/ray_release/tests/test_test.py
+++ release/ray_release/tests/test_test.py
@@ -5,6 +5,7 @@ import pytest
 from ray_release.test import (
     Test,
     DATAPLANE_ECR_REPO,
+    DATAPLANE_ECR_ML_REPO,
 )
 
 
@@ -25,10 +26,8 @@ def test_get_python_version():
 
 
 def test_get_ray_image():
-    os.environ.pop("BUILDKITE_COMMIT", None)
-    assert (
-        _stub_test({"python": "3.8"}).get_ray_image() == "rayproject/ray:nightly-py38"
-    )
+    os.environ["BUILDKITE_COMMIT"] = "1234567890"
+    assert _stub_test({"python": "3.8"}).get_ray_image() == "rayproject/ray:123456-py38"
     assert (
         _stub_test(
             {
@@ -40,22 +39,27 @@ def test_get_ray_image():
                 },
             }
         ).get_ray_image()
-        == "rayproject/ray-ml:nightly-py38-gpu"
+        == "rayproject/ray-ml:123456-py38-gpu"
     )
-    os.environ["BUILDKITE_COMMIT"] = "1234567890"
-    assert _stub_test({}).get_ray_image() == "rayproject/ray:123456-py37"
 
 
 def test_get_anyscale_byod_image():
-    os.environ.pop("BUILDKITE_COMMIT", None)
-    assert (
-        _stub_test({}).get_anyscale_byod_image()
-        == f"{DATAPLANE_ECR_REPO}:ray-nightly-py37"
-    )
     os.environ["BUILDKITE_COMMIT"] = "1234567890"
     assert (
-        _stub_test({}).get_anyscale_byod_image()
-        == f"{DATAPLANE_ECR_REPO}:ray-123456-py37"
+        _stub_test({}).get_anyscale_byod_image() == f"{DATAPLANE_ECR_REPO}:123456-py37"
+    )
+    assert (
+        _stub_test(
+            {
+                "python": "3.8",
+                "cluster": {
+                    "byod": {
+                        "type": "gpu",
+                    }
+                },
+            }
+        ).get_anyscale_byod_image()
+        == f"{DATAPLANE_ECR_ML_REPO}:123456-py38-gpu"
     )
 
 
