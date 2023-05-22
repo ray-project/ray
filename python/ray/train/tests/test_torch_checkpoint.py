6e202ee9cbcd3033a3aea927b690302af88be125
woshiyyya <xiaoyunxuan1998@gmail.com>
2023-05-22 16:11:52 -0700

    restrict unittest to python>3.8
    Signed-off-by: woshiyyya <xiaoyunxuan1998@gmail.com>

--- python/ray/train/tests/test_torch_checkpoint.py
+++ python/ray/train/tests/test_torch_checkpoint.py
@@ -35,10 +35,13 @@ def test_from_state_dict():
 
 
 def test_pickle_large_model():
-    # TorchCheckpoint should be able to serialize large checkpoints(> 4 GiB)
-    data_dict = {"key": "1" * (4 * 1024 * 1024 * 1024 + 100)}
-    checkpoint = TorchCheckpoint(data_dict=data_dict)
-    pickle.dumps(checkpoint)
+    import sys
+
+    if sys.version_info >= (3, 8):
+        # TorchCheckpoint should be able to serialize large checkpoints(> 4 GiB)
+        data_dict = {"key": "1" * (4 * 1024 * 1024 * 1024 + 100)}
+        checkpoint = TorchCheckpoint(data_dict=data_dict)
+        pickle.dumps(checkpoint)
 
 
 if __name__ == "__main__":
