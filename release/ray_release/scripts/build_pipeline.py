dbb111bc7bfe55aa65ea24706cf59edf0fd56890
Cuong Nguyen <can@anyscale.com>
2023-05-22 10:31:07 -0700

    Fix lints
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- release/ray_release/scripts/build_pipeline.py
+++ release/ray_release/scripts/build_pipeline.py
@@ -17,12 +17,6 @@ from ray_release.config import (
     DEFAULT_WHEEL_WAIT_TIMEOUT,
     parse_python_version,
 )
-from ray_release.test import (
-    Test,
-    DOCKER_REPO,
-    S3_BUCKET,
-    DATAPLANE_FILENAME,
-)
 from ray_release.exception import ReleaseTestCLIError, ReleaseTestConfigError
 from ray_release.logger import logger
 from ray_release.wheels import (
@@ -244,15 +238,6 @@ def main(
 
     steps_str = json.dumps(steps)
     print(steps_str)
-
-
-def _build_anyscale_byod_images(tests: List[Tuple[Test, bool]]) -> None:
-    """
-    Builds the Anyscale BYOD images for the given tests.
-    """
-    _ = set(test.get_ray_image() for test, _ in tests)
-    # TODO(aslonnie): Build anyscale byod images for the given ray images
-    return
 
 
 if __name__ == "__main__":
