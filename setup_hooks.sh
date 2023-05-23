e172609a01bec415123316b67ad16b3362cb1918
%eax <eax@anyscale.com>
2023-05-23 08:25:20 -0700

    [git] enforce signoff in commit hooks (#35502)
    Problem
    Ray requires git signoff but it is easy to forget to do so.
    
    Solution
    Automate the signoff by adding a hook to Ray's suggested hooks.
    
    Signed-off-by: Eitan Adler <eax@anyscale.com>
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- setup_hooks.sh
+++ setup_hooks.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 chmod +x "$PWD"/ci/lint/pre-push
 ln -s "$PWD"/ci/lint/pre-push "$PWD"/.git/hooks/pre-push
+ln -s "$PWD"/ci/lint/prepare-commit-msg "$PWD"/.git/hooks/prepare-commit-msg
