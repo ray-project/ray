5213474190df3792cd93c9a0507ebd15a14ad440
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2023-05-23 22:06:20 +0000

    fix Signed-off-by: Yi Cheng <74173148+iycheng@users.noreply.github.com>

--- src/ray/rpc/gcs_server/gcs_rpc_client.cc
+++ src/ray/rpc/gcs_server/gcs_rpc_client.cc
@@ -29,12 +29,11 @@ grpc::ChannelArguments GetGcsRpcClientArguments() {
 
 std::shared_ptr<grpc::Channel> GcsRpcClient::GetDefaultChannel(const std::string &address,
                                                                int port) {
-  static std::shared_ptr<grpc::Channel> channel_;
   static std::mutex mu_;
-
+  static std::shared_ptr<grpc::Channel> channel_;
   static std::string address_;
-  static int port_;
-  std::lock_guard<std::mutex> guard(mu_);
+  static int port_ = 0;
+
   // Don't reuse channel if proxy or tls is set
   // TODO: Reuse the channel even it's tls.
   // Right now, if we do this, python/ray/serve/tests/test_grpc.py
@@ -44,19 +43,27 @@ std::shared_ptr<grpc::Channel> GcsRpcClient::GetDefaultChannel(const std::string
     return BuildChannel(address, port, GetGcsRpcClientArguments());
   }
 
-  if (channel_ == nullptr) {
+  std::lock_guard<std::mutex> guard(mu_);
+  if (channel_ == nullptr || (address_ != address || port_ != port)) {
     address_ = address;
     port_ = port;
+
+    // This condition shouldn't happen in most cases. It could only happen when
+    // ray driver wanted to talk with different GCS.
+    // - This mostly happens in testing, where the test main process is the driver.
+    //   It calls ray.init and then ray.shutdown and later ray.init with a different
+    //   GCS address.
+    // - Potentially it can also happen in the user's driver where there are two
+    //   ray clusters and the user ray.init and ray.shutdown and then tries to
+    //   connect to a different GCS.
+    if (channel_ != nullptr) {
+      RAY_LOG(WARNING) << "Generate a new GCS channel: " << address << ":" << port
+                       << ". Potentially it will increase GCS socket numbers";
+    }
     channel_ = BuildChannel(address, port, GetGcsRpcClientArguments());
   }
 
-  if (address_ == address && port_ == port) {
-    return channel_;
-  } else {
-    RAY_LOG(WARNING) << "Generate a new GCS channel: " << address << ":" << port
-                     << ". Potentially it will increase GCS socket numbers";
-    return BuildChannel(address, port, GetGcsRpcClientArguments());
-  }
+  return channel_;
 }
 
 }  // namespace rpc
