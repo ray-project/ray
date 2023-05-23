ef770a2114c9747f1740a3f494ec5cf211414f93
Artur Niederfahrenhorst <attaismyname@googlemail.com>
2023-05-23 16:23:57 -0700

    Revert "Merge branch 'fixmultiagentppofails' into restoffixesforenablepporlleanrers"
    This reverts commit 3ff3efdab4e0be4296f55a274fbc3e0e7240b9df, reversing
    changes made to e446c78c76b59d48e2596cd43872096bd40297f2.

--- rllib/models/utils.py
+++ rllib/models/utils.py
@@ -269,8 +269,6 @@ def get_initializer(name, framework="tf"):
             return nn.init.xavier_uniform_
         elif name == "xavier_normal":
             return nn.init.xavier_normal_
-        elif name == "truncate_normal":
-            return nn.init.trunc_normal_
     else:
         assert framework in ["tf", "tf2"], "Unsupported framework `{}`!".format(
             framework
@@ -283,9 +281,7 @@ def get_initializer(name, framework="tf"):
             return tf.keras.initializers.GlorotUniform
         elif name == "xavier_normal":
             return tf.keras.initializers.GlorotNormal
-        elif name == "truncate_normal":
-            return tf.keras.initializers.TruncatedNormal(stddev=1.0)
 
     raise ValueError(
-        "Unknown initializer ({}) for framework={}!".format(name, framework)
+        "Unknown activation ({}) for framework={}!".format(name, framework)
     )
