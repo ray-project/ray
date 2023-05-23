a24363a25a9f9f6af353823337c93edb5ec7e4dc
amogkam <amogkamsetty@yahoo.com>
2023-05-23 11:18:40 -0700

    update
    Signed-off-by: amogkam <amogkamsetty@yahoo.com>

--- python/ray/data/_internal/numpy_support.py
+++ python/ray/data/_internal/numpy_support.py
@@ -83,14 +83,14 @@ def convert_udf_returns_to_numpy(udf_return_col: Any) -> Any:
                     # https://github.com/apache/arrow/issues/26470,
                     # we use object dtype.
                     # https://github.com/ray-project/ray/issues/35586#issuecomment-1558148261
-                    dtype = object
+                    has_object = True
                 elif not np.isscalar(e):
                     has_object = True
             if has_object or len(shapes) > 1:
                 # This util works around some limitations of np.array(dtype=object).
                 udf_return_col = create_ragged_ndarray(udf_return_col)
             else:
-                udf_return_col = np.array(udf_return_col, dtype=dtype)
+                udf_return_col = np.array(udf_return_col)
         except Exception as e:
             raise ValueError(
                 "Failed to convert column values to numpy array: "
