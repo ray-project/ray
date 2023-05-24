d7979b33c4f58caf6abdbec4e7d7c8d3f56b5061
Philipp Moritz <pcmoritz@gmail.com>
2023-05-24 12:30:48 -0700

    Clean up imports

--- python/ray/_raylet.pyx
+++ python/ray/_raylet.pyx
@@ -151,7 +151,7 @@ import ray.cloudpickle as ray_pickle
 from ray.core.generated.common_pb2 import ActorDiedErrorContext
 from ray._private.async_compat import sync_to_async, get_new_event_loop
 from ray._private.client_mode_hook import disable_client_hook
-import ray._private.gcs_utils as gcs_utils
+import ray.core.generated.common_pb2 as common_pb2
 import ray._private.memory_monitor as memory_monitor
 import ray._private.profiling as profiling
 from ray._private.utils import decode, DeferSigint
@@ -3556,7 +3556,7 @@ cdef class CoreWorker:
         # the job config will not change after a job is submitted.
         if self.job_config is None:
             c_job_config = CCoreWorkerProcess.GetCoreWorker().GetJobConfig()
-            self.job_config = gcs_utils.JobConfig()
+            self.job_config = common_pb2.JobConfig()
             self.job_config.ParseFromString(c_job_config.SerializeAsString())
         return self.job_config
 
--- python/ray/job_config.py
+++ python/ray/job_config.py
@@ -1,7 +1,7 @@
 import uuid
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
 
-import ray._private.gcs_utils as gcs_utils
+import ray.core.generated.common_pb2 as common_pb2
 from ray.util.annotations import PublicAPI
 
 if TYPE_CHECKING:
@@ -136,10 +136,10 @@ class JobConfig:
             default_actor_lifetime: The default actor lifetime to set.
         """
         if default_actor_lifetime == "detached":
-            self._default_actor_lifetime = gcs_utils.JobConfig.ActorLifetime.DETACHED
+            self._default_actor_lifetime = common_pb2.JobConfig.ActorLifetime.DETACHED
         elif default_actor_lifetime == "non_detached":
             self._default_actor_lifetime = (
-                gcs_utils.JobConfig.ActorLifetime.NON_DETACHED
+                common_pb2.JobConfig.ActorLifetime.NON_DETACHED
             )
         else:
             raise ValueError(
@@ -164,7 +164,7 @@ class JobConfig:
         from ray._private.utils import get_runtime_env_info
 
         if self._cached_pb is None:
-            pb = gcs_utils.JobConfig()
+            pb = common_pb2.JobConfig()
             if self.ray_namespace is None:
                 pb.ray_namespace = str(uuid.uuid4())
             else:
