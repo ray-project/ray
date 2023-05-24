7d2ac6e4e662de7b693cf71a12c94450d07f91ec
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2023-05-24 13:11:15 -0700

    [ci][byod/6] build byod based on a commit hash (#35453)
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- release/ray_release/tests/test_test.py
+++ release/ray_release/tests/test_test.py
@@ -4,7 +4,9 @@ import pytest
 
 from ray_release.test import (
     Test,
-    DOCKER_REPO,
+    DATAPLANE_ECR,
+    DATAPLANE_ECR_REPO,
+    DATAPLANE_ECR_ML_REPO,
 )
 
 
@@ -25,10 +27,8 @@ def test_get_python_version():
 
 
 def test_get_ray_image():
-    os.environ.pop("BUILDKITE_COMMIT", None)
-    assert (
-        _stub_test({"python": "3.8"}).get_ray_image() == "rayproject/ray:nightly-py38"
-    )
+    os.environ["BUILDKITE_COMMIT"] = "1234567890"
+    assert _stub_test({"python": "3.8"}).get_ray_image() == "rayproject/ray:123456-py38"
     assert (
         _stub_test(
             {
@@ -40,19 +40,29 @@ def test_get_ray_image():
                 },
             }
         ).get_ray_image()
-        == "rayproject/ray-ml:nightly-py38-gpu"
+        == "rayproject/ray-ml:123456-py38-gpu"
     )
-    os.environ["BUILDKITE_COMMIT"] = "1234567890"
-    # TODO(can): re-enable this test once we have a custom image
-    # assert Test().get_ray_image() == "rayproject/ray:123456-py37"
 
 
 def test_get_anyscale_byod_image():
-    os.environ.pop("BUILDKITE_COMMIT", None)
-    assert _stub_test({}).get_anyscale_byod_image() == f"{DOCKER_REPO}:ray-nightly-py37"
     os.environ["BUILDKITE_COMMIT"] = "1234567890"
-    # TODO(can): re-enable this test once we have a custom image
-    # assert Test().get_anyscale_byod_image() == "anyscale/ray:123456-py37"
+    assert (
+        _stub_test({}).get_anyscale_byod_image()
+        == f"{DATAPLANE_ECR}/{DATAPLANE_ECR_REPO}:123456-py37"
+    )
+    assert (
+        _stub_test(
+            {
+                "python": "3.8",
+                "cluster": {
+                    "byod": {
+                        "type": "gpu",
+                    }
+                },
+            }
+        ).get_anyscale_byod_image()
+        == f"{DATAPLANE_ECR}/{DATAPLANE_ECR_ML_REPO}:123456-py38-gpu"
+    )
 
 
 if __name__ == "__main__":
