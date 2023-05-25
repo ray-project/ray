68fd4ee25e282ae1c76680280a5fea727e93ea4f
Philipp Moritz <pcmoritz@gmail.com>
2023-05-24 21:33:13 -0700

    update

--- dashboard/head.py
+++ dashboard/head.py
@@ -7,6 +7,7 @@ from concurrent.futures import Future
 from queue import Queue
 
 import ray._private.services
+import ray._private.tls_utils
 import ray._private.utils
 import ray.dashboard.consts as dashboard_consts
 import ray.dashboard.utils as dashboard_utils
