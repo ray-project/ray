2a9caef75a945e4a130546741da8413dd0da0829
Philipp Moritz <pcmoritz@gmail.com>
2023-05-25 03:01:13 -0700

    update

--- python/ray/_raylet.pyx
+++ python/ray/_raylet.pyx
@@ -82,7 +82,6 @@ from ray.includes.common cimport (
     CJobConfig,
     CConcurrencyGroup,
     CGrpcStatusCode,
-    CActorLifetime,
     move,
     LANGUAGE_CPP,
     LANGUAGE_JAVA,
@@ -177,9 +176,6 @@ OPTIMIZED = __OPTIMIZE__
 
 GRPC_STATUS_CODE_UNAVAILABLE = CGrpcStatusCode.UNAVAILABLE
 GRPC_STATUS_CODE_UNKNOWN = CGrpcStatusCode.UNKNOWN
-
-RAY_ACTOR_LIFETIME_DETACHED = CActorLifetime.DETACHED
-RAY_ACTOR_LIFETIME_NON_DETACHED = CActorLifetime.NON_DETACHED
 
 logger = logging.getLogger(__name__)
 
