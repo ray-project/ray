b047feb4a86197182dcf64cb1d616954316b2481
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-05-25 11:35:58 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- doc/source/serve/doc_code/streaming_example.py
+++ doc/source/serve/doc_code/streaming_example.py
@@ -10,6 +10,7 @@ from starlette.requests import Request
 
 from ray import serve
 
+
 @serve.deployment
 class StreamingResponder:
     def generate_numbers(self, max: int) -> Generator[str, None, None]:
@@ -21,6 +22,7 @@ class StreamingResponder:
         max = request.query_params.get("max", "25")
         gen = self.generate_numbers(int(max))
         return StreamingResponse(gen, status_code=200, media_type="text/plain")
+
 
 serve.run(StreamingResponder.bind())
 
