2bffbf8985d866a3bc825e5cefcd6100df94cb7c
Ricky Xu <xuchen727@hotmail.com>
2023-05-26 21:37:39 +0800

    [core][state] Record file offsets instead of logging magic token to track task log (#35572)
    We have seen regression from 1_1_actor_calls_concurrent and 1_1_actor_calls_async due to the additional overheads of writing magic token to worker files when starting and finishing execution of a task.
    It's extremely bad for concurrent actors due to the contention on the same worker file.
    
    This PR:
    
    Skips recording the task start/end for concurrent actor tasks. And augment the task log querying behaviour to be:
    
    Normal task log: we will still be able record the offsets and stream task log from dashboard + CLI
    Normal actor task log: same as above ^
    Async actor task + threaded actor task (max_concurrency>1) :
    On CLI: we will raise error and let users know to use ray logs actor --id for actor log.
    Revert to the old way of recording file offsets rather than writing magic tokens so that the overhead is actually lower and prevents the pollution of actor logs.

--- dashboard/modules/log/log_manager.py
+++ dashboard/modules/log/log_manager.py
@@ -16,10 +16,28 @@ from ray.util.state.state_manager import StateDataSourceClient
 # TODO(sang): Remove the usage of this class.
 from ray.dashboard.datacenter import DataSource
 
+from pydantic import BaseModel
+
 
 logger = logging.getLogger(__name__)
 
 WORKER_LOG_PATTERN = re.compile(".*worker-([0-9a-f]+)-([0-9a-f]+)-(\d+).(out|err)")
+
+
+class ResolvedStreamFileInfo(BaseModel):
+    # The node id where the log file is located.
+    node_id: str
+
+    # The log file path name. Could be a relative path relative to ray's logging folder,
+    # or an absolute path.
+    filename: str
+
+    # Start offset in the log file to stream from. None to indicate beginning of
+    # the file, or determined by last tail lines.
+    start_offset: Optional[int]
+
+    # End offset in the log file to stream from. None to indicate the end of the file.
+    end_offset: Optional[int]
 
 
 class LogsManager:
@@ -76,7 +94,7 @@ class LogsManager:
         """
         node_id = options.node_id or self.ip_to_node_id(options.node_ip)
 
-        log_file_name, node_id = await self.resolve_filename(
+        res = await self.resolve_filename(
             node_id=node_id,
             log_filename=options.filename,
             actor_id=options.actor_id,
@@ -91,8 +109,8 @@ class LogsManager:
 
         keep_alive = options.media_type == "stream"
         stream = await self.client.stream_log(
-            node_id=node_id,
-            log_file_name=log_file_name,
+            node_id=res.node_id,
+            log_file_name=res.filename,
             keep_alive=keep_alive,
             lines=options.lines,
             interval=options.interval,
@@ -100,8 +118,8 @@ class LogsManager:
             # otherwise the stream will be terminated forcefully
             # after the deadline is expired.
             timeout=options.timeout if not keep_alive else None,
-            task_id=options.task_id,
-            attempt_number=options.attempt_number,
+            start_offset=res.start_offset,
+            end_offset=res.end_offset,
         )
 
         async for streamed_log in stream:
@@ -186,6 +204,151 @@ class LogsManager:
                     return filename
         return None
 
+    async def _resolve_actor_filename(
+        self,
+        actor_id: str,
+        get_actor_fn: Callable[[str], Dict],
+        suffix: str,
+        timeout: int,
+    ):
+        """
+        Resolve actor log file
+            Args:
+                actor_id: The actor id.
+                get_actor_fn: The function to get actor information.
+                suffix: The suffix of the log file.
+                timeout: Timeout in seconds.
+            Returns:
+                The log file name and node id.
+
+            Raises:
+                ValueError if actor data is not found or get_actor_fn is not provided.
+        """
+        if get_actor_fn is None:
+            raise ValueError("get_actor_fn needs to be specified for actor_id")
+
+        actor_data = get_actor_fn(actor_id)
+        if actor_data is None:
+            raise ValueError(f"Actor ID {actor_id} not found.")
+
+        # TODO(sang): Only the latest worker id can be obtained from
+        # actor information now. That means, if actors are restarted,
+        # there's no way for us to get the past worker ids.
+        worker_id = actor_data["address"].get("workerId")
+        if not worker_id:
+            raise ValueError(
+                f"Worker ID for Actor ID {actor_id} not found. "
+                "Actor is not scheduled yet."
+            )
+        node_id = actor_data["address"].get("rayletId")
+        if not node_id:
+            raise ValueError(
+                f"Node ID for Actor ID {actor_id} not found. "
+                "Actor is not scheduled yet."
+            )
+        self._verify_node_registered(node_id)
+        log_filename = await self._resolve_worker_file(
+            node_id=node_id,
+            worker_id=worker_id,
+            pid=None,
+            suffix=suffix,
+            timeout=timeout,
+        )
+        return node_id, log_filename
+
+    async def _resolve_task_filename(
+        self, task_id: str, attempt_number: int, suffix: str, timeout: int
+    ):
+        """
+        Resolve log file for a task.
+
+        Args:
+            task_id: The task id.
+            attempt_number: The attempt number.
+            suffix: The suffix of the log file, e.g. out or err
+            timeout: Timeout in seconds.
+
+        Returns:
+            The log file name, node id, the start and end offsets of the
+            corresponding task log in the file.
+
+        Raises:
+            FileNotFoundError if the log file is not found.
+            ValueError if the suffix is not out or err.
+
+        """
+        log_filename = None
+        node_id = None
+        start_offset = None
+        end_offset = None
+
+        if suffix not in ["out", "err"]:
+            raise ValueError(f"Suffix {suffix} is not supported.")
+
+        reply = await self.client.get_all_task_info(
+            filters=[("task_id", "=", task_id)], timeout=timeout
+        )
+        # Check if the task is found.
+        if len(reply.events_by_task) == 0:
+            raise FileNotFoundError(
+                f"Could not find log file for task: {task_id}"
+                f" (attempt {attempt_number}) with suffix: {suffix}"
+            )
+        task_event = None
+        for t in reply.events_by_task:
+            if t.attempt_number == attempt_number:
+                task_event = t
+                break
+
+        if task_event is None:
+            raise FileNotFoundError(
+                "Could not find log file for task attempt:"
+                f"{task_id}({attempt_number})"
+            )
+        # Get the worker id and node id.
+        task = protobuf_to_task_state_dict(task_event)
+
+        worker_id = task.get("worker_id", None)
+        node_id = task.get("node_id", None)
+        log_info = task.get("task_log_info", None)
+        actor_id = task.get("actor_id", None)
+
+        if node_id is None:
+            raise FileNotFoundError(
+                "Could not find log file for task attempt."
+                f"{task_id}({attempt_number}) due to missing node info."
+            )
+
+        if log_info is None and actor_id is not None:
+            # This is a concurrent actor task. The logs will be interleaved.
+            # So we return the log file of the actor instead.
+            raise FileNotFoundError(
+                f"For concurrent actor task, please query actor log for "
+                f"actor({actor_id}): e.g. ray logs actor --id {actor_id} ."
+                "Because tasks from concurrent actor will have logs interleaved, "
+                "and Ray is not able to locate the exact log file for the task."
+            )
+        elif log_info is None:
+            raise FileNotFoundError(
+                "Could not find log file for task attempt:"
+                f"{task_id}({attempt_number})."
+                f"Worker id = {worker_id}, node id = {node_id},"
+                f"log_info = {log_info}"
+            )
+
+        filename_key = "stdout_file" if suffix == "out" else "stderr_file"
+        log_filename = log_info.get(filename_key, None)
+        if log_filename is None:
+            raise FileNotFoundError(
+                f"Missing log filename info in {log_info} for task {task_id},"
+                f"attempt {attempt_number}"
+            )
+
+        start_offset = log_info.get(f"std{suffix}_start", None)
+        end_offset = log_info.get(f"std{suffix}_end", None)
+
+        return node_id, log_filename, start_offset, end_offset
+
     async def resolve_filename(
         self,
         *,
@@ -199,7 +362,7 @@ class LogsManager:
         timeout: int = DEFAULT_RPC_TIMEOUT,
         suffix: str = "out",
         submission_id: Optional[str] = None,
-    ) -> Tuple[str, str]:
+    ) -> ResolvedStreamFileInfo:
         """Return the file name given all options.
 
         Args:
@@ -215,86 +378,30 @@ class LogsManager:
                 resolving by other ids'. Default to "out".
             submission_id: The submission id for a submission job.
         """
+        start_offset = None
+        end_offset = None
+        if suffix not in ["out", "err"]:
+            raise ValueError(f"Suffix {suffix} is not supported. ")
+
+        # TODO(rickyx): We should make sure we do some sort of checking on the log
+        # filename
         if actor_id:
-            if get_actor_fn is None:
-                raise ValueError("get_actor_fn needs to be specified for actor_id")
-
-            actor_data = get_actor_fn(actor_id)
-            if actor_data is None:
-                raise ValueError(f"Actor ID {actor_id} not found.")
-
-            # TODO(sang): Only the latest worker id can be obtained from
-            # actor information now. That means, if actors are restarted,
-            # there's no way for us to get the past worker ids.
-            worker_id = actor_data["address"].get("workerId")
-            if not worker_id:
-                raise ValueError(
-                    f"Worker ID for Actor ID {actor_id} not found. "
-                    "Actor is not scheduled yet."
-                )
-            node_id = actor_data["address"].get("rayletId")
-            if not node_id:
-                raise ValueError(
-                    f"Node ID for Actor ID {actor_id} not found. "
-                    "Actor is not scheduled yet."
-                )
-            self._verify_node_registered(node_id)
-
-            log_filename = await self._resolve_worker_file(
-                node_id=node_id,
-                worker_id=worker_id,
-                pid=None,
-                suffix=suffix,
-                timeout=timeout,
+            node_id, log_filename = await self._resolve_actor_filename(
+                actor_id, get_actor_fn, suffix, timeout
             )
+
         elif task_id:
-            reply = await self.client.get_all_task_info(
-                filters=[("task_id", "=", task_id)], timeout=timeout
+            (
+                node_id,
+                log_filename,
+                start_offset,
+                end_offset,
+            ) = await self._resolve_task_filename(
+                task_id, attempt_number, suffix, timeout
             )
-            # Check if the task is found.
-            if len(reply.events_by_task) == 0:
-                raise FileNotFoundError(
-                    f"Could not find log file for task: {task_id}"
-                    f" (attempt {attempt_number}) with suffix: {suffix}"
-                )
-            task_event = None
-            for t in reply.events_by_task:
-                if t.attempt_number == attempt_number:
-                    task_event = t
-                    break
 
-            if task_event is None:
-                raise FileNotFoundError(
-                    "Could not find log file for task attempt:"
-                    f"{task_id}({attempt_number})"
-                )
-            # Get the worker id and node id.
-            task = protobuf_to_task_state_dict(task_event)
-
-            worker_id = task.get("worker_id", None)
-            node_id = task.get("node_id", None)
-
-            if worker_id is None or node_id is None:
-                raise FileNotFoundError(
-                    "Could not find log file for task attempt:"
-                    f"{task_id}({attempt_number})."
-                    f"Worker id = {worker_id}, node id = {node_id}"
-                )
-
-            log_filename = await self._resolve_worker_file(
-                node_id=node_id,
-                worker_id=worker_id,
-                pid=None,
-                suffix=suffix,
-                timeout=timeout,
-            )
         elif submission_id:
             node_id, log_filename = await self._resolve_job_filename(submission_id)
-
-            logger.info(
-                f"Resolving job {submission_id} on node {node_id} with "
-                f"filename {log_filename}"
-            )
 
         elif pid:
             if node_id is None:
@@ -315,16 +422,24 @@ class LogsManager:
             raise FileNotFoundError(
                 "Could not find a log file. Please make sure the given "
                 "option exists in the cluster.\n"
-                f"\node_id: {node_id}\n"
-                f"\filename: {log_filename}\n"
+                f"\tnode_id: {node_id}\n"
+                f"\tfilename: {log_filename}\n"
                 f"\tactor_id: {actor_id}\n"
-                f"\task_id: {task_id}\n"
+                f"\ttask_id: {task_id}\n"
                 f"\tpid: {pid}\n"
                 f"\tsuffix: {suffix}\n"
                 f"\tsubmission_id: {submission_id}\n"
+                f"\tattempt_number: {attempt_number}\n"
             )
-        logger.info(f"Resolved log file: {log_filename} on node {node_id}")
-        return log_filename, node_id
+
+        res = ResolvedStreamFileInfo(
+            node_id=node_id,
+            filename=log_filename,
+            start_offset=start_offset,
+            end_offset=end_offset,
+        )
+        logger.info(f"Resolved log file: {res}")
+        return res
 
     def _categorize_log_files(self, log_files: List[str]) -> Dict[str, List[str]]:
         """Categorize the given log files after filterieng them out using a given glob.
