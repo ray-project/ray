09c43e213a0a539cb742a12f6f2228136c70c809
Sven Mika <svenmika1977@gmail.com>
2023-05-30 16:24:00 +0200

    [RLlib] Fix duplicate setup() in RLModules; In framework-specific RLModule sub-classes, flip inheritance order and remove c'tors. (#35659)
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- rllib/algorithms/dreamerv3/tf/dreamerv3_tf_rl_module.py
+++ rllib/algorithms/dreamerv3/tf/dreamerv3_tf_rl_module.py
@@ -12,17 +12,13 @@ from ray.rllib.utils.nested_dict import NestedDict
 tf1, tf, _ = try_import_tf()
 
 
-class DreamerV3TfRLModule(DreamerV3RLModule, TfRLModule):
+class DreamerV3TfRLModule(TfRLModule, DreamerV3RLModule):
     """The tf-specific RLModule class for DreamerV3.
 
     Serves mainly as a thin-wrapper around the `DreamerModel` (a tf.keras.Model) class.
     """
 
     framework: str = "tf2"
-
-    def __init__(self, *args, **kwargs):
-        TfRLModule.__init__(self, *args, **kwargs)
-        DreamerV3RLModule.__init__(self, *args, **kwargs)
 
     @override(RLModule)
     def _forward_inference(self, batch: NestedDict) -> Mapping[str, Any]:
