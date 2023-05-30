cd933f168975ae5c2436c132a88a40355f0da42f
shrekris-anyscale <92341594+shrekris-anyscale@users.noreply.github.com>
2023-05-30 08:40:37 -0700

    [serve] Support generators in `@serve.batch` (#35729)
    Make serve.batch compatible with generators
    
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>
    
    Co-authored-by: Edward Oakes <ed.nmi.oakes@gmail.com>
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- python/ray/serve/tests/test_batching.py
+++ python/ray/serve/tests/test_batching.py
@@ -1,9 +1,14 @@
-import asyncio
-
 import pytest
+import asyncio
+import requests
+from typing import List
+from functools import partial
+from starlette.responses import StreamingResponse
+from concurrent.futures.thread import ThreadPoolExecutor
 
 import ray
 from ray import serve
+from ray.serve.exceptions import RayServeException
 from ray._private.utils import get_or_create_event_loop
 
 
@@ -289,6 +294,113 @@ async def test_batch_args_kwargs(mode, use_class):
 
     result = await asyncio.gather(*coros)
     assert result == [("hi1", "hi2"), ("hi3", "hi4")]
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("mode", ["args", "kwargs", "mixed", "out-of-order"])
+@pytest.mark.parametrize("use_class", [True, False])
+@pytest.mark.parametrize("generator_length", [0, 2, 5])
+async def test_batch_generator_basic(mode, use_class, generator_length):
+    if use_class:
+
+        class MultipleArgs:
+            @serve.batch(max_batch_size=2, batch_wait_timeout_s=1000)
+            async def method(self, key1, key2):
+                for gen_idx in range(generator_length):
+                    yield [(gen_idx, key1[i], key2[i]) for i in range(len(key1))]
+
+        instance = MultipleArgs()
+        func = instance.method
+
+    else:
+
+        @serve.batch(max_batch_size=2, batch_wait_timeout_s=1000)
+        async def func(key1, key2):
+            for gen_idx in range(generator_length):
+                yield [(gen_idx, key1[i], key2[i]) for i in range(len(key1))]
+
+    if mode == "args":
+        generators = [func("hi1", "hi2"), func("hi3", "hi4")]
+    elif mode == "kwargs":
+        generators = [func(key1="hi1", key2="hi2"), func(key1="hi3", key2="hi4")]
+    elif mode == "mixed":
+        generators = [func("hi1", key2="hi2"), func("hi3", key2="hi4")]
+    elif mode == "out-of-order":
+        generators = [func(key2="hi2", key1="hi1"), func(key2="hi4", key1="hi3")]
+
+    results = [
+        [result async for result in generators[0]],
+        [result async for result in generators[1]],
+    ]
+
+    assert results == [
+        [(gen_idx, "hi1", "hi2") for gen_idx in range(generator_length)],
+        [(gen_idx, "hi3", "hi4") for gen_idx in range(generator_length)],
+    ]
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("error_type", ["runtime_error", "mismatched_lengths"])
+async def test_batch_generator_exceptions(error_type):
+    GENERATOR_LENGTH = 5
+    ERROR_IDX = 2
+    ERROR_MSG = "Testing error"
+
+    @serve.batch(max_batch_size=2, batch_wait_timeout_s=1000)
+    async def func(key1, key2):
+        for gen_idx in range(GENERATOR_LENGTH):
+            results = [(gen_idx, key1[i], key2[i]) for i in range(len(key1))]
+            if gen_idx == ERROR_IDX:
+                if error_type == "runtime_error":
+                    raise RuntimeError(ERROR_MSG)
+                elif error_type == "mismatched_lengths":
+                    yield results * 2
+            yield results
+
+    generators = [func("hi1", "hi2"), func("hi3", "hi4")]
+
+    for generator in generators:
+        for _ in range(ERROR_IDX):
+            await generator.__anext__()
+
+        if error_type == "runtime_error":
+            with pytest.raises(RuntimeError, match=ERROR_MSG):
+                await generator.__anext__()
+        elif error_type == "mismatched_lengths":
+            with pytest.raises(RayServeException):
+                await generator.__anext__()
+
+        with pytest.raises(StopAsyncIteration):
+            await generator.__anext__()
+
+
+@pytest.mark.asyncio
+async def test_batch_generator_streaming_response_integration_test(serve_instance):
+    NUM_YIELDS = 10
+
+    @serve.deployment
+    class Textgen:
+        @serve.batch(max_batch_size=4, batch_wait_timeout_s=1000)
+        async def batch_handler(self, prompts: List[str]):
+            for _ in range(NUM_YIELDS):
+                prompt_responses = prompts
+                yield prompt_responses
+
+        async def __call__(self, request):
+            prompt = request.query_params["prompt"]
+            return StreamingResponse(self.batch_handler(prompt))
+
+    serve.run(Textgen.bind())
+
+    prompt_prefix = "hola"
+    url = f"http://localhost:8000/?prompt={prompt_prefix}"
+    with ThreadPoolExecutor() as pool:
+        futs = [pool.submit(partial(requests.get, url + str(idx))) for idx in range(4)]
+        responses = [fut.result() for fut in futs]
+
+    for idx, response in enumerate(responses):
+        assert response.status_code == 200
+        assert response.text == "".join([prompt_prefix + str(idx)] * NUM_YIELDS)
 
 
 if __name__ == "__main__":
