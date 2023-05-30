35ad2500597ebb7c6d5fc93fab3472e92c7ed79b
Gene Der Su <gdsu@ucdavis.edu>
2023-05-30 09:14:37 -0700

    Update python/ray/serve/tests/test_api.py
    Co-authored-by: Edward Oakes <ed.nmi.oakes@gmail.com>
    Signed-off-by: Gene Der Su <gdsu@ucdavis.edu>

--- python/ray/serve/tests/test_api.py
+++ python/ray/serve/tests/test_api.py
@@ -831,7 +831,7 @@ class TestAppBuilder:
 
 
 def test_no_slash_route_prefix(serve_instance):
-    """Test serve run with no slash route_prefix
+    """Test serve run with no slash route_prefix.
 
     This test ensure when serve runs with no prefix slash in route_prefix, it will throw
     good error message.
