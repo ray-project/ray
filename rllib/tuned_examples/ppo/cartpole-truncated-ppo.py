f72502b049df9d8967a5dbd3fc1ea1e41253185c
Artur Niederfahrenhorst <attaismyname@googlemail.com>
2023-05-31 13:39:54 -0700

    remove docstrings
    Signed-off-by: Artur Niederfahrenhorst <attaismyname@googlemail.com>

--- rllib/tuned_examples/ppo/cartpole-truncated-ppo.py
+++ rllib/tuned_examples/ppo/cartpole-truncated-ppo.py
@@ -18,7 +18,6 @@ config = (
         evaluation_interval=1,
         evaluation_num_workers=1,
     )
-    # This speeds up this test for tf2 a lot.
     .framework(eager_tracing=True)
 )
 
--- rllib/tuned_examples/ppo/multi_agent_pendulum_ppo.py
+++ rllib/tuned_examples/ppo/multi_agent_pendulum_ppo.py
@@ -22,7 +22,6 @@ config = (
         model={"fcnet_activation": "relu"},
         vf_clip_param=10.0,
     )
-    # This speeds up this test for tf2 a lot.
     .framework(eager_tracing=True)
 )
 
