610afa19b98392542dc1fb1a092e19bafb4b70f4
Hao Chen <chenh1024@gmail.com>
2023-05-31 19:11:08 -0700

    logger
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- python/ray/data/_internal/logical/operators/map_operator.py
+++ python/ray/data/_internal/logical/operators/map_operator.py
@@ -1,12 +1,14 @@
 from typing import Any, Dict, Iterable, Optional, Union
 
+from ray.data._internal.dataset_logger import DatasetLogger
 from ray.data._internal.logical.interfaces import LogicalOperator
 from ray.data._internal.compute import ComputeStrategy, TaskPoolStrategy
 from ray.data.block import UserDefinedFunction
 from ray.data.context import DEFAULT_BATCH_SIZE
 
 import inspect
-import logging
+
+logger = DatasetLogger(__name__)
 
 
 class AbstractMap(LogicalOperator):
@@ -47,7 +49,7 @@ def _get_udf_name(fn: UserDefinedFunction) -> str:
             # callable object.
             return fn.__class__.__name__
     except AttributeError as e:
-        logging.error("Failed to get name of UDF %s: %s", fn, e)
+        logger.get_logger().error("Failed to get name of UDF %s: %s", fn, e)
         return "<unknown>"
 
 
