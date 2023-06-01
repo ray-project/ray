f870b3ddf40fd609585c65865f4eefae574776a2
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-06-01 09:56:00 -0500

    WIP
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/gradio_integrations.py
+++ python/ray/serve/gradio_integrations.py
@@ -1,6 +1,9 @@
+import asyncio
+import logging
 from typing import Callable
 
 from ray.util.annotations import PublicAPI
+from ray._private.utils import get_or_create_event_loop
 
 from ray import serve
 from ray.serve._private.http_util import (
@@ -13,18 +16,27 @@ except ModuleNotFoundError:
     print("Gradio isn't installed. Run `pip install gradio` to install Gradio.")
     raise
 
+logger = logging.getLogger(__file__)
+
 
 @PublicAPI(stability="alpha")
 class GradioIngress(ASGIAppReplicaWrapper):
     """User-facing class that wraps a Gradio App in a Serve Deployment."""
 
-    async def __init__(self, builder: Callable[[], Blocks]):
-        """
-        Takes a builder function which should take no arguments and return the Gradio
-        App (of type Interface or Blocks) to deploy as a Serve Deployment.
+    def __init__(self, builder: Callable[[], Blocks]):
+        """Builds and wraps an ASGI app from the provided builder.
+
+        The builder should take no arguments and return a Gradio App (of type Interface
+        or Blocks).
         """
         io: Blocks = builder()
-        await super().__init__(routes.App.create_app(io))
+
+        # ASGIAppReplicaWrapper's constructor is `async def` because it calls the ASGI
+        # LifespanOn event, but we want this constructor to be sync because it's a
+        # public API.
+        asyncio.run_coroutine_threadsafe(
+            super().__init__(routes.App.create_app(io)), get_or_create_event_loop()
+        ).result()
 
 
 GradioServer = serve.deployment(GradioIngress)
