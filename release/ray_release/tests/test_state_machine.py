3d7f7181231d6217a906f686ec2ad3ecae8f5ad0
can <can@anyscale.com>
2023-06-01 19:31:19 +0000

    Fix tests
    Signed-off-by: can <can@anyscale.com>

--- release/ray_release/tests/test_state_machine.py
+++ release/ray_release/tests/test_state_machine.py
@@ -4,6 +4,7 @@ import pytest
 
 from ray_release.test import (
     Test,
+    TestResult,
     TestState,
 )
 from ray_release.result import (
@@ -15,11 +16,18 @@ from ray_release.test_automation.state_machine import TestStateMachine
 
 def test_move():
     test = Test()
-    test.test_results = []
-    test.add_test_result(Result(status=ResultStatus.SUCCESS))
+    test.test_results = [
+        TestResult.from_result(Result(status=ResultStatus.SUCCESS)),
+    ]
     assert test.get_state() == TestState.PASSING
-    test.add_test_result(Result(status=ResultStatus.ERROR))
-    test.add_test_result(Result(status=ResultStatus.ERROR))
+    test.test_results.insert(
+        0,
+        TestResult.from_result(Result(status=ResultStatus.ERROR)),
+    )
+    test.test_results.insert(
+        0,
+        TestResult.from_result(Result(status=ResultStatus.ERROR)),
+    )
     sm = TestStateMachine(test)
     sm.move()
     assert test.get_state() == TestState.FAILING
