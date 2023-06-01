3735b8c30fd7d0a84cfc315097f641797e012617
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-06-01 09:54:02 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/gradio_integrations.py
+++ python/ray/serve/gradio_integrations.py
@@ -1,7 +1,9 @@
+import asyncio
 import logging
 from typing import Callable
 
 from ray.util.annotations import PublicAPI
+from ray._private.utils import get_or_create_event_loop
 
 from ray import serve
 from ray.serve._private.http_util import (
@@ -21,16 +23,20 @@ logger = logging.getLogger(__file__)
 class GradioIngress(ASGIAppReplicaWrapper):
     """User-facing class that wraps a Gradio App in a Serve Deployment."""
 
-    async def __init__(self, builder: Callable[[], Blocks]):
+    def __init__(self, builder: Callable[[], Blocks]):
         """
         Takes a builder function which should take no arguments and return the Gradio
         App (of type Interface or Blocks) to deploy as a Serve Deployment.
         """
-        # Used in `replica.py` to detect the usage of this class.
-        self._is_serve_asgi_wrapper = True
-
         io: Blocks = builder()
-        await super().__init__(routes.App.create_app(io))
+
+        # ASGIAppReplicaWrapper's constructor is `async def` because it calls the ASGI
+        # LifespanOn event, but we want this constructor to be sync because it's a
+        # public API.
+        asyncio.run_coroutine_threadsafe(
+            super().__init__(routes.App.create_app(io)),
+            get_or_create_event_loop()
+        ).result()
 
 
 GradioServer = serve.deployment(GradioIngress)
