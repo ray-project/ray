941d7b32c59c75762b9eb8340881d826904e7b95
can <can@anyscale.com>
2023-06-05 19:53:42 +0000

    Trigger bisect on test failures
    Signed-off-by: can <can@anyscale.com>

--- release/ray_release/tests/test_state_machine.py
+++ release/ray_release/tests/test_state_machine.py
@@ -24,20 +24,43 @@ class MockRepo:
         return MockIssue(10)
 
 
+class MockBuildkiteBuild:
+    def create_build(self, *args, **kwargs):
+        return {
+            "number": 1,
+            "jobs": [{"id": "1"}],
+        }
+
+    def list_all_for_pipeline(self, *args, **kwargs):
+        return []
+
+
+class MockBuildkiteJob:
+    def unblock_job(self, *args, **kwargs):
+        return {}
+
+
+class MockBuildkite:
+    def builds(self):
+        return MockBuildkiteBuild()
+
+    def jobs(self):
+        return MockBuildkiteJob()
+
+
 TestStateMachine.ray_repo = MockRepo()
+TestStateMachine.ray_buildkite = MockBuildkite()
 
 
 def test_move_from_passing_to_failing():
     test = Test(name="test", team="devprod")
     # Test original state
     test.test_results = [
-        TestResult.from_result(Result(status=ResultStatus.SUCCESS.value))
+        TestResult.from_result(Result(status=ResultStatus.SUCCESS.value)),
     ]
     assert test.get_state() == TestState.PASSING
-    test.test_results.insert(
-        0,
-        TestResult.from_result(Result(status=ResultStatus.ERROR.value)),
-    )
+
+    # Test moving from passing to failing
     test.test_results.insert(
         0,
         TestResult.from_result(Result(status=ResultStatus.ERROR.value)),
@@ -45,8 +68,16 @@ def test_move_from_passing_to_failing():
     sm = TestStateMachine(test)
     sm.move()
     assert test.get_state() == TestState.FAILING
+    assert test[Test.KEY_BISECT_BUILD_NUMBER] == 1
 
-    # Test github issue is created
+    # Test moving from failing to consistently failing
+    test.test_results.insert(
+        0,
+        TestResult.from_result(Result(status=ResultStatus.ERROR.value)),
+    )
+    sm = TestStateMachine(test)
+    sm.move()
+    assert test.get_state() == TestState.CONSITENTLY_FAILING
     assert test[Test.KEY_GITHUB_ISSUE_NUMBER] == 10
 
 
