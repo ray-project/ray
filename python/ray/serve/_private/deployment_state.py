c0d8522d2d9fe229d82b23ff431834a240086f00
Sihan Wang <sihanwang41@gmail.com>
2023-06-05 10:31:07 -0700

    Fix java
    Signed-off-by: Sihan Wang <sihanwang41@gmail.com>

--- python/ray/serve/_private/deployment_state.py
+++ python/ray/serve/_private/deployment_state.py
@@ -461,15 +461,15 @@ class ActorReplicaWrapper:
         deployment_config.user_config = self._format_user_config(
             deployment_config.user_config
         )
-        replica_ready_check_func = self._actor_handle.initialize_and_get_metadata
         if self._is_cross_language:
             self._actor_handle = JavaActorHandleProxy(self._actor_handle)
             self._allocated_obj_ref = self._actor_handle.is_allocated.remote()
-            self._ready_obj_ref = replica_ready_check_func.remote(
+            self._ready_obj_ref = self._actor_handle.is_initialized.remote(
                 deployment_config.to_proto_bytes()
             )
         else:
             self._allocated_obj_ref = self._actor_handle.is_allocated.remote()
+            replica_ready_check_func = self._actor_handle.initialize_and_get_metadata
             self._ready_obj_ref = replica_ready_check_func.remote(
                 deployment_config,
                 # Ensure that `is_allocated` will execute before `reconfigure`,
