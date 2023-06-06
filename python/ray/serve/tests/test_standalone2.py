9183b3a3c4fd5473fe347e30680b2b302d50f017
Cindy Zhang <cindyzyx9@gmail.com>
2023-06-05 23:53:24 -0700

    revert unrelated changes
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- python/ray/serve/tests/test_standalone2.py
+++ python/ray/serve/tests/test_standalone2.py
@@ -1471,10 +1471,7 @@ class TestDeployApp:
         )
 
     def test_deploy_one_app_failed(self, client: ServeControllerClient):
-        """Deploy two applications where one fails health check upon startup. The other
-        application should deploy successfully, independent of the first application's
-        failures.
-        """
+        """Deploy two applications with separate runtime envs."""
 
         world_import_path = "ray.serve.tests.test_config_files.world.DagNode"
         fail_import_path = "ray.serve.tests.test_config_files.fail.node"
