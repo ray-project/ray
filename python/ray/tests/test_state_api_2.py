4dc2ca2ccfc608d65964736a4169ae80b4f242e0
Ricky Xu <xuchen727@hotmail.com>
2023-06-08 06:32:11 +0800

    [core][ci] Update state api scale test (#35543)
    We are using a signal block to block the execution of tasks because tasks states were not persistent when they finished before (so we need to have X number of tasks running to simulate a large task response).
    
    However, this is no longer true.
    
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- python/ray/tests/test_state_api_2.py
+++ python/ray/tests/test_state_api_2.py
@@ -1,8 +1,11 @@
 import asyncio
 import json
 import os
+import sys
+from pathlib import Path
 
 from collections import defaultdict
+from ray._private.test_utils import check_call_subprocess
 
 import ray
 import requests
@@ -277,7 +280,7 @@ def test_experimental_import_deprecation():
         from ray.experimental.state.util import convert_string_to_type  # noqa: F401
 
 
-def test_actor_task_with_repr_name():
+def test_actor_task_with_repr_name(ray_start_with_dashboard):
     @ray.remote
     class ReprActor:
         def __init__(self, x) -> None:
@@ -336,6 +339,20 @@ def test_actor_task_with_repr_name():
         return True
 
     wait_for_condition(verify)
+
+
+@pytest.mark.skipif(
+    sys.platform == "win32", reason="Release test not expected to work on non-linux."
+)
+def test_state_api_scale_smoke(shutdown_only):
+    ray.init()
+    release_test_file_path = (
+        "../../release/nightly_tests/stress_tests/test_state_api_scale.py"
+    )
+    full_path = Path(ray.__file__).parents[0] / release_test_file_path
+    assert full_path.exists()
+
+    check_call_subprocess(["python", str(full_path), "--smoke-test"])
 
 
 if __name__ == "__main__":
