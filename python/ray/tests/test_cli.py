c47469d90b6999a26de53f11395ea15307278270
Kai Fricke <kai@anyscale.com>
2023-06-07 15:07:13 +0100

    skip test [build_base]
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- python/ray/tests/test_cli.py
+++ python/ray/tests/test_cli.py
@@ -475,6 +475,9 @@ def test_ray_start_head_block_and_signals(
 
 
 @pytest.mark.skipif(
+    sys.version_info.minor >= 8, reason="Currently fails with Python 3.8+"
+)
+@pytest.mark.skipif(
     sys.platform == "darwin",
     reason=("Mac builds don't provide proper locale support. "),
 )
