14998f983054a7cbeefffe5501794fdf6ac9be67
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2023-06-07 16:31:53 -0700

    Trigger bisect on test failures (#35898)
    Signed-off-by: can <can@anyscale.com>
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- release/ray_release/test.py
+++ release/ray_release/test.py
@@ -32,6 +32,7 @@ class TestState(enum.Enum):
 
     JAILED = "jailed"
     FAILING = "failing"
+    CONSITENTLY_FAILING = "consistently_failing"
     PASSING = "passing"
 
 
@@ -71,6 +72,7 @@ class Test(dict):
     """A class represents a test to run on buildkite"""
 
     KEY_GITHUB_ISSUE_NUMBER = "github_issue_number"
+    KEY_BISECT_BUILD_NUMBER = "bisect_build_number"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
