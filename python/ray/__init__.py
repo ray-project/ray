33090a332daf4f756876819f0ee814c6aa9a1bd2
Artur Niederfahrenhorst <attaismyname@googlemail.com>
2023-06-08 16:52:19 -0700

    Revert "Remove rc0 from version numbers (#36096)"
    This reverts commit 9a9d853e74a38f85df7c5af463614cdfa15a41aa.

--- python/ray/__init__.py
+++ python/ray/__init__.py
@@ -104,7 +104,7 @@ del _configure_system
 
 # Replaced with the current commit when building the wheels.
 __commit__ = "{{RAY_COMMIT_SHA}}"
-__version__ = "2.5.0"
+__version__ = "2.5.0rc0"
 
 import ray._raylet  # noqa: E402
 
