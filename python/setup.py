7e6e78a50314343938a8be22f36b7bc80da3bef1
Philipp Moritz <pcmoritz@gmail.com>
2023-06-07 21:39:07 -0700

    try to loosen upper bound

--- python/setup.py
+++ python/setup.py
@@ -263,8 +263,8 @@ if setup_spec.type == SetupType.RAY:
             "grpcio >= 1.32.0, <= 1.49.1; python_version < '3.10' and sys_platform == 'darwin'",  # noqa:E501
             "grpcio >= 1.42.0, <= 1.49.1; python_version >= '3.10' and sys_platform == 'darwin'",  # noqa:E501
             # Original issue: https://github.com/ray-project/ray/issues/33833
-            "grpcio >= 1.32.0, <= 1.50.0; python_version < '3.10' and sys_platform != 'darwin'",  # noqa:E501
-            "grpcio >= 1.42.0, <= 1.50.0; python_version >= '3.10' and sys_platform != 'darwin'",  # noqa:E501
+            "grpcio >= 1.32.0, <= 1.51.1; python_version < '3.10' and sys_platform != 'darwin'",  # noqa:E501
+            "grpcio >= 1.42.0, <= 1.51.1; python_version >= '3.10' and sys_platform != 'darwin'",  # noqa:E501
         ],
         "serve": ["uvicorn", "requests", "starlette", "fastapi", "aiorwlock"],
         "tune": ["pandas", "tensorboardX>=1.9", "requests", pyarrow_dep],
