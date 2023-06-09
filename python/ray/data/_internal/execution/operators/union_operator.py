5a492c9f0f3cbc7c431eba1fd20e8c41d09079bc
Scott Lee <sjl@anyscale.com>
2023-06-09 15:01:46 -0700

    move to receiving from inputs immediately for fully streamed union
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- python/ray/data/_internal/execution/operators/union_operator.py
+++ python/ray/data/_internal/execution/operators/union_operator.py
@@ -7,11 +7,7 @@ from ray.data.block import BlockMetadata
 
 
 class UnionOperator(NAryOperator):
-    """An operator that combines blocks from input operators.
-
-    The order of the blocks from the input operators is preserved,
-    as is the relative ordering between the input operators.
-    """
+    """An operator that combines blocks from input operators."""
 
     def __init__(
         self,
@@ -22,8 +18,14 @@ class UnionOperator(NAryOperator):
         Args:
             input_ops: Operators generating input data for this operator to union.
         """
-
         self._input_buffers: List[List[RefBundle]] = [[] for _ in range(len(input_ops))]
+        # Whether the input dependency still has blocks remaining.
+        self._input_dependency_has_next: List[bool] = [
+            True for _ in range(len(input_ops))
+        ]
+        # The index of the input dependency that is currently the source of the output buffer.
+        # New inputs from this input dependency will be added directly to the output buffer.
+        self._input_idx_to_output = None
         self._output_buffer: List[RefBundle] = []
         self._stats: StatsDict = {}
         super().__init__(*input_ops)
@@ -42,14 +44,46 @@ class UnionOperator(NAryOperator):
     def add_input(self, refs: RefBundle, input_index: int) -> None:
         assert not self.completed()
         assert 0 <= input_index <= len(self._input_dependencies), input_index
-        self._input_buffers[input_index].append(refs)
+        if self._input_idx_to_output is None:
+            self._input_idx_to_output = input_index
+
+        # If the input block is from the current source of the output buffer,
+        # add it directly to the output buffer. Otherwise, save them in the
+        # corresponding input buffer.
+        if self._input_idx_to_output == input_index:
+            self._output_buffer.append(refs)
+        else:
+            self._input_buffers[input_index].append(refs)
+
+        # If the current input dependency no longer has any remaining outputs,
+        # mark it as such, and add its remaining blocks to the output buffer.
+        if not self._input_dependencies[input_index].has_next():
+            self._input_dependency_has_next[input_index] = False
+            self._output_buffer.extend(self._input_buffers[input_index])
+            # Clear the input buffer, since all its blocks
+            # have now been added to the output buffer.
+            self._input_buffers[input_index].clear()
+            self._change_output_buffer_source()
 
     def inputs_done(self) -> None:
-        self._output_buffer, self._stats = self._union(self._input_buffers)
+        # Move the remaining blocks from the input buffers to the output buffer.
+        for idx, input_buffer in enumerate(self._input_buffers):
+            if self._input_dependency_has_next[idx]:
+                self._output_buffer.extend(input_buffer)
+                self._input_dependency_has_next[idx] = False
+                input_buffer.clear()
         self._input_buffers.clear()
         super().inputs_done()
 
     def has_next(self) -> bool:
+        # If the output buffer still contains at least one block, return True.
+        if len(self._output_buffer) > 0:
+            return True
+
+        # If the output buffer is empty, check if at least one of the input
+        # buffers still has blocks remaining.
+        if any(self._input_dependency_has_next):
+            self._change_output_buffer_source()
         return len(self._output_buffer) > 0
 
     def get_next(self) -> RefBundle:
@@ -58,20 +92,19 @@ class UnionOperator(NAryOperator):
     def get_stats(self) -> StatsDict:
         return self._stats
 
-    def get_transformation_fn(self) -> Callable:
-        return self._union
+    def _change_output_buffer_source(self):
+        """Change the source of the output buffer to the input buffer
+        with the most blocks remaining, adding its blocks to the output buffer
+        and clearing the input buffer."""
 
-    def _union(
-        self, inputs: List[List[RefBundle]]
-    ) -> Tuple[List[RefBundle], StatsDict]:
-        output_refs: List[RefBundle] = []
-        output_metadata: List[BlockMetadata] = []
-        # Collect all the refs and metadata from the input refs.
-        for curr_input_refs in inputs:
-            output_refs.extend(curr_input_refs)
-            for ref_bundle in curr_input_refs:
-                for _, block_metadata in ref_bundle.blocks:
-                    output_metadata.append(block_metadata)
+        # Use the input buffer with the most blocks as the next source for the output buffer.
+        num_blocks_in_input_buffers = [len(b) for b in self._input_buffers]
+        self._input_idx_to_output = max(
+            range(len(num_blocks_in_input_buffers)),
+            key=num_blocks_in_input_buffers.__getitem__,
+        )
+        # Add the existing blocks in the new input buffer source to the output buffer.
+        self._output_buffer.extend(self._input_buffers[self._input_idx_to_output])
 
-        stats = {self._name: output_metadata}
-        return output_refs, stats
+        # Clear the input buffer, since all its blocks have now been added to the output buffer.
+        self._input_buffers[self._input_idx_to_output].clear()
