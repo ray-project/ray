db18547978c6f213cbf500d8664a2b0f2322dd50
Matthew Deng <matt@anyscale.com>
2023-06-08 17:29:15 -0700

    [data] fix lint and broken link
    Signed-off-by: Matthew Deng <matt@anyscale.com>

--- python/ray/data/tests/test_stats.py
+++ python/ray/data/tests/test_stats.py
@@ -35,7 +35,6 @@ def dummy_map_batches(x):
 
 
 def test_streaming_split_stats(ray_start_regular_shared):
-    context = DataContext.get_current()
     ds = ray.data.range(1000, parallelism=10)
     it = ds.map_batches(dummy_map_batches).streaming_split(1)[0]
     list(it.iter_batches())
@@ -52,7 +51,7 @@ def test_streaming_split_stats(ray_start_regular_shared):
 * Extra metrics: {'obj_store_mem_alloc': N, 'obj_store_mem_freed': N, \
 'obj_store_mem_peak': N}
 
-Stage N split(N, equal=False): 
+Stage N split(N, equal=False):
 * Extra metrics: {'num_output_N': N}
 """
     )
