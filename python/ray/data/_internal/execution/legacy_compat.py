a70142286c90c4198fa5b5670372978dc09373dc
Scott Lee <sjl@anyscale.com>
2023-06-09 17:27:25 -0700

    remove op skip for missing logical plan
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- python/ray/data/_internal/execution/legacy_compat.py
+++ python/ray/data/_internal/execution/legacy_compat.py
@@ -131,8 +131,8 @@ def _get_execution_dag(
     # Get DAG of physical operators and input statistics.
     if (
         DataContext.get_current().optimizer_enabled
-        # TODO(hchen): Remove this when all operators support logical plan.
-        and getattr(plan, "_logical_plan", None) is not None
+        # # TODO(hchen): Remove this when all operators support logical plan.
+        # and getattr(plan, "_logical_plan", None) is not None
     ):
         dag = get_execution_plan(plan._logical_plan).dag
         stats = _get_initial_stats_from_plan(plan)
