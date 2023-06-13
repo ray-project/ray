9dd07f8d97c322598dfc3a6225b5fd44812122ee
Hao Chen <chenh1024@gmail.com>
2023-06-12 17:07:35 -0700

    fix remote args
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- python/ray/data/_internal/planner/exchange/push_based_shuffle_task_scheduler.py
+++ python/ray/data/_internal/planner/exchange/push_based_shuffle_task_scheduler.py
@@ -396,6 +396,8 @@ class PushBasedShuffleTaskScheduler(ExchangeTaskScheduler):
             map_ray_remote_args = {}
         if reduce_ray_remote_args is None:
             reduce_ray_remote_args = {}
+        map_ray_remote_args["num_cpus"] = 1
+        reduce_ray_remote_args["num_cpus"] = 1
         # The placement strategy for reduce tasks is overwritten to colocate
         # them with their inputs from the merge stage, so remove any
         # pre-specified scheduling strategy here.
