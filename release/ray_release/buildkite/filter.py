cad679295d32a5f1e714b69aea55a4a9849903a5
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2023-06-13 20:42:08 -0700

    Avoid modify test object before running tests (#36354)
    Signed-off-by: can <can@anyscale.com>
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- release/ray_release/buildkite/filter.py
+++ release/ray_release/buildkite/filter.py
@@ -1,4 +1,5 @@
 import re
+import copy
 from collections import defaultdict
 from typing import List, Optional, Tuple, Dict, Any
 
@@ -40,8 +41,9 @@ def filter_tests(
         if not run_jailed_tests:
             if test.get("jailed", False):
                 continue
-            test.update_from_s3()
-            if test.is_jailed_with_open_issue(TestStateMachine.get_ray_repo()):
+            clone_test = copy.deepcopy(test)
+            clone_test.update_from_s3()
+            if clone_test.is_jailed_with_open_issue(TestStateMachine.get_ray_repo()):
                 continue
 
         test_frequency = get_frequency(test["frequency"])
