9dc9bde74ce425d9628be8b322e00c707ab07338
rickyyx <rickyx@anyscale.com>
2023-06-15 19:59:28 +0000

    nit
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- release/ray_release/scripts/run_release_test.py
+++ release/ray_release/scripts/run_release_test.py
@@ -167,9 +167,6 @@ def main(
             no_terminate=no_terminate,
         )
         return_code = result.return_code
-        from rich import print
-
-        print(result)
     except ReleaseTestError as e:
         logger.exception(e)
         return_code = e.exit_code.value
