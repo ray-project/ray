b61673f72ebb1333cab162bdbbe09c5c97bbbf91
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-06-15 11:32:57 -0500

    iterate
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/tests/test_failure.py
+++ python/ray/serve/tests/test_failure.py
@@ -212,36 +212,49 @@ def test_no_available_replicas_does_not_block_proxy(serve_instance):
 
     This is essential so that other requests and health checks can pass while a
     deployment is deploying/updating.
+
+    See https://github.com/ray-project/ray/issues/36460.
     """
-    signal_actor = SignalActor.remote()
 
     @serve.deployment
     class SlowStarter:
-        def __init__(self):
-            ray.get(signal_actor.wait.remote())
+        def __init__(self, starting_actor, finish_starting_actor):
+            ray.get(starting_actor.send.remote())
+            ray.get(finish_starting_actor.wait.remote())
 
         def __call__(self):
             return "hi"
 
-    serve.run(SlowStarter.bind(), _blocking=False)
-
     @ray.remote
     def make_blocked_request():
-        print("MAKE BLOCKED")
         r = requests.get("http://localhost:8000/")
         r.raise_for_status()
-        print("BLOCKED DONE:", r.text)
         return r.text
 
-    blocked_ref = make_blocked_request.remote()
-    with pytest.raises(TimeoutError):
-        ray.get(blocked_ref, timeout=1)
+    # Loop twice: first iteration tests deploying from nothing, second iteration
+    # tests updating the replicas of an existing deployment.
+    for _ in range(2):
+        starting_actor = SignalActor.remote()
+        finish_starting_actor = SignalActor.remote()
+        serve.run(
+            SlowStarter.bind(starting_actor, finish_starting_actor), _blocking=False
+        )
 
-    requests.get("http://localhost:8000/-/routes").raise_for_status()
-    requests.get("http://localhost:8000/-/healthz").raise_for_status()
+        # Ensure that the replica has been started (we use _blocking=False).
+        ray.get(starting_actor.wait.remote())
 
-    ray.get(signal_actor.send.remote())
-    assert ray.get(blocked_ref) == "hi"
+        # The request shouldn't complete until the replica has finished started.
+        blocked_ref = make_blocked_request.remote()
+        with pytest.raises(TimeoutError):
+            ray.get(blocked_ref, timeout=1)
+
+        # If the proxy's loop was blocked, these would hang.
+        requests.get("http://localhost:8000/-/routes").raise_for_status()
+        requests.get("http://localhost:8000/-/healthz").raise_for_status()
+
+        # Signal the replica to finish starting; request should complete.
+        ray.get(finish_starting_actor.send.remote())
+        assert ray.get(blocked_ref) == "hi"
 
 
 if __name__ == "__main__":
