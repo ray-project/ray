fcf514701f1352fdafdb2c4ef1faf377df0d7c2d
Philipp Moritz <pcmoritz@gmail.com>
2023-06-15 12:55:08 -0700

    [Data] Clean up ray.data.aggregate namespace

---
+++ python/ray/data/aggregate/__init__.py
@@ -0,0 +1,23 @@
+from ray.data.aggregate._aggregate import (
+    AggregateFn,
+    Count,
+    Sum,
+    Min,
+    Max,
+    Mean,
+    Std,
+    AbsMax,
+    Quantile,
+)
+
+__all__ = [
+    "AggregateFn",
+    "Count",
+    "Sum",
+    "Min",
+    "Max",
+    "Mean",
+    "Std",
+    "AbsMax",
+    "Quantile",
+]
