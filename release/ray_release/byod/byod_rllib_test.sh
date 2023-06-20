8d0c641fccfea21bc8f00365093965f1221097b9
can <can@anyscale.com>
2023-06-20 01:03:09 +0000

    Add all rllib tests to byod
    Signed-off-by: can <can@anyscale.com>

--- release/ray_release/byod/byod_rllib_test.sh
+++ release/ray_release/byod/byod_rllib_test.sh
@@ -16,6 +16,7 @@ mkdir ~/.mujoco
 mv mujoco210-linux-x86_64.tar.gz ~/.mujoco/.
 cd ~/.mujoco
 tar -xf ~/.mujoco/mujoco210-linux-x86_64.tar.gz
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ray/.mujoco/mujoco210/bin' >> /home/ray/.bashrc
 
 # not strictly necessary, but makes debugging easier
 git clone https://github.com/ray-project/ray.git
