4b841e6a8fd3cf647aa68d600ab10e9204823247
Kai Fricke <krfricke@users.noreply.github.com>
2023-06-20 10:34:48 +0200

    Revert "[RLlib] DreamerV3: Main algo code and required changes to some RLlib APIs (RolloutWorker). (#35386)"
    This reverts commit 8290bd112c181769bcdf6cc77572b48697bcee88.

--- rllib/policy/eager_tf_policy_v2.py
+++ rllib/policy/eager_tf_policy_v2.py
@@ -870,12 +870,7 @@ class EagerTFPolicyV2(Policy):
             actions = fwd_out[SampleBatch.ACTIONS]
         # Otherwise, sample actions from the distribution.
         else:
-            if action_dist is None:
-                raise KeyError(
-                    "Your RLModule's `forward_exploration()` method must return a dict"
-                    f"with either the {SampleBatch.ACTIONS} key or the "
-                    f"{SampleBatch.ACTION_DIST_INPUTS} key in it (or both)!"
-                )
+            assert action_dist
             actions = action_dist.sample()
 
         # Anything but action_dist and state_out is an extra fetch
@@ -931,12 +926,7 @@ class EagerTFPolicyV2(Policy):
             actions = fwd_out[SampleBatch.ACTIONS]
         # Otherwise, sample actions from the distribution.
         else:
-            if action_dist is None:
-                raise KeyError(
-                    "Your RLModule's `forward_inference()` method must return a dict"
-                    f"with either the {SampleBatch.ACTIONS} key or the "
-                    f"{SampleBatch.ACTION_DIST_INPUTS} key in it (or both)!"
-                )
+            assert action_dist
             actions = action_dist.sample()
 
         # Anything but action_dist and state_out is an extra fetch
