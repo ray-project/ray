a86ed1963e1e9b74a12713e926b48871e12b7e0b
Gene Su <e870252314@gmail.com>
2023-06-21 10:36:05 -0700

    add new INACTIVE to sort order and color map
    Signed-off-by: Gene Su <e870252314@gmail.com>

--- dashboard/client/src/components/StatusChip.tsx
+++ dashboard/client/src/components/StatusChip.tsx
@@ -78,6 +78,7 @@ const colorMap = {
     [ServeSystemActorStatus.HEALTHY]: green,
     [ServeSystemActorStatus.UNHEALTHY]: red,
     [ServeSystemActorStatus.STARTING]: orange,
+    [ServeSystemActorStatus.INACTIVE]: white,
   },
   serveController: {
     [ServeSystemActorStatus.HEALTHY]: green,
