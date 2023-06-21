ded7d128bc8e07dc4c3bd84b258bf0ca26640bbb
Philipp Moritz <pcmoritz@gmail.com>
2023-06-21 08:24:20 +0200

    lint

--- dashboard/head.py
+++ dashboard/head.py
@@ -7,7 +7,6 @@ from concurrent.futures import Future
 from queue import Queue
 
 import ray._private.services
-import ray._private.utils
 import ray.dashboard.consts as dashboard_consts
 import ray.dashboard.utils as dashboard_utils
 import ray.experimental.internal_kv as internal_kv
