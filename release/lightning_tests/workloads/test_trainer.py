1ae3539ffe179aa5fb54e7047e8093a62c9f159c
woshiyyya <xiaoyunxuan1998@gmail.com>
2023-06-23 13:07:13 -0700

    init
    Signed-off-by: woshiyyya <xiaoyunxuan1998@gmail.com>

--- release/lightning_tests/workloads/test_trainer.py
+++ release/lightning_tests/workloads/test_trainer.py
@@ -1,13 +1,8 @@
 import os
 import time
 import json
-from pytorch_lightning.loggers.csv_logs import CSVLogger
 
 import ray
-from ray.air.config import ScalingConfig
-from ray.train.lightning import LightningTrainer, LightningConfigBuilder
-
-from lightning_test_utils import MNISTClassifier, MNISTDataModule
 
 
 if __name__ == "__main__":
@@ -15,7 +10,9 @@ if __name__ == "__main__":
 
     start = time.time()
 
-    os.system("aws s3 sync s3://large-dl-models-mirror/restricted/models--lmsys--vicuna-13b-delta-v1.1/main-safetensors/ /tmp/vicuna")
+    os.system(
+        "aws s3 sync s3://large-dl-models-mirror/restricted/models--lmsys--vicuna-13b-delta-v1.1/main-safetensors/ /tmp/vicuna"
+    )
     print(os.listdir("/tmp/vicuna"))
 
     taken = time.time() - start
