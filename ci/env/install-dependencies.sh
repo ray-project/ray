873e3076f3dbc083381d275a3607e2db66c00588
sven1977 <svenmika1977@gmail.com>
2023-06-23 14:37:35 +0200

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- ci/env/install-dependencies.sh
+++ ci/env/install-dependencies.sh
@@ -360,8 +360,9 @@ install_pip_packages() {
     mkdir -p /root/.mujoco
     mv mujoco210-linux-x86_64.tar.gz /root/.mujoco/.
     (cd /root/.mujoco && tar -xf /root/.mujoco/mujoco210-linux-x86_64.tar.gz)
-    echo 'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:/root/.mujoco/mujoco210/bin' >> /root/.bashrc
-    source /root/.bashrc
+    #echo 'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:/root/.mujoco/mujoco210/bin' >> /root/.bashrc
+    #source /root/.bashrc
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mujoco210/bin
     #export LD_LIBRARY_PATH=/root/.mujoco/mujoco210/bin
   fi
 
