b4daf8f34deda89fef4dd1e2cc8d7ad30eada9c4
Lonnie Liu <lonnie@anyscale.com>
2023-06-24 16:09:18 +0000

    nit

--- runtime/ci/build-forge.sh
+++ runtime/ci/build-forge.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 export DOCKER_BUILDKIT=1
+
+echo "--- Build rayforge"
 
 tar --mtime="UTC 2020-01-01" -c -f - \
 	runtime/ci/rayforge/Dockerfile \
