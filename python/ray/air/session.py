c26abb1e3057983f4f4049b3a177cdf440162460
Philipp Moritz <pcmoritz@gmail.com>
2023-06-26 12:38:38 +0200

    update

--- python/ray/air/session.py
+++ python/ray/air/session.py
@@ -1,2 +1,2 @@
-from ray.air._internal.session import _get_session
-from ray.train._internal.session import *
+from ray.air._internal.session import _get_session  # noqa: F401
+from ray.train._internal.session import *  # noqa: F401,F403
--- python/ray/train/__init__.py
+++ python/ray/train/__init__.py
@@ -2,7 +2,13 @@ from ray._private.usage import usage_lib
 from ray.train.backend import BackendConfig
 from ray.train.data_config import DataConfig
 from ray.train.constants import TRAIN_DATASET_KEY
-from ray.train._internal.session import report, get_dataset_shard, get_world_size
+from ray.train._internal.session import (
+    get_dataset_shard,
+    get_trial_resources,
+    get_world_rank,
+    get_world_size,
+    report,
+)
 from ray.train.trainer import TrainingIterator
 
 from ray.air import Checkpoint
@@ -20,6 +26,8 @@ __all__ = [
     "TrainingIterator",
     "TRAIN_DATASET_KEY",
     "get_dataset_shard",
+    "get_trial_resources",
+    "get_world_rank",
     "get_world_size",
     "report",
 ]
