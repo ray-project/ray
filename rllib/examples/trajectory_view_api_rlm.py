b36814c7bf8fe63e30bedc5d24ea55bae405483e
Artur Niederfahrenhorst <attaismyname@googlemail.com>
2023-06-26 11:29:49 +0200

    clean up trajectory view api test
    Signed-off-by: Artur Niederfahrenhorst <attaismyname@googlemail.com>

--- rllib/examples/trajectory_view_api_rlm.py
+++ rllib/examples/trajectory_view_api_rlm.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     )
 
     stop = {
-        "training_iteration": 0,
+        "training_iteration":  args.stop_iters,
         "timesteps_total": args.stop_timesteps,
         "episode_reward_mean": args.stop_reward,
     }
