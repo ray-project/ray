8eda52cd3390bfd2af468a90bcccfb278c7cfc32
Shreyas Krishnaswamy <shrekris@anyscale.com>
2023-06-27 09:49:52 -0700

    Linter
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- python/ray/serve/tests/test_config_files/broken_app.py
+++ python/ray/serve/tests/test_config_files/broken_app.py
@@ -1,11 +1,13 @@
 from ray import serve
 import ray.cloudpickle as pickle
 
+
 class NonserializableException(Exception):
     """This exception cannot be serialized."""
 
     def __reduce__(self):
         raise RuntimeError("This exception cannot be serialized!")
+
 
 # Confirm that NonserializableException cannot be serialized.
 try:
@@ -15,8 +17,10 @@ except RuntimeError as e:
 
 raise NonserializableException("custom exception info")
 
+
 @serve.deployment
 def f():
     pass
+
 
 app = f.bind()
