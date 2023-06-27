9fe55d5e21efc23afe7bbbdd3b9e47db4d7befdc
Avnish <avnishnarayan@gmail.com>
2023-06-26 17:13:53 -0700

    Add more deprecations
    Signed-off-by: Avnish <avnishnarayan@gmail.com>

--- rllib/models/torch/mingpt.py
+++ rllib/models/torch/mingpt.py
@@ -20,6 +20,8 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 from ray.rllib.utils.annotations import DeveloperAPI
+from ray.rllib.utils.deprecation import deprecation_warning
+from ray.util import log_once
 
 
 @DeveloperAPI
@@ -48,6 +50,8 @@ class NewGELU(nn.Module):
     """
 
     def forward(self, x):
+        if log_once("torch_gelu_deprecation"):
+            deprecation_warning(old="ray.rllib.models.torch.min_gpt.NewGELU")
         return (
             0.5
             * x
@@ -68,6 +72,10 @@ class CausalSelfAttention(nn.Module):
     """
 
     def __init__(self, config: GPTConfig):
+        if log_once("torch_casual_self_attention_deprecation"):
+            deprecation_warning(
+                old="ray.rllib.models.torch.min_gpt.CausalSelfAttention"
+            )
         super().__init__()
         assert config.n_embed % config.n_head == 0
         # key, query, value projections for all heads, but in a batch
@@ -123,6 +131,8 @@ class Block(nn.Module):
     """an unassuming Transformer block"""
 
     def __init__(self, config: GPTConfig):
+        if log_once("torch_block_deprecation"):
+            deprecation_warning(old="ray.rllib.models.torch.min_gpt.Block")
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.n_embed)
         self.attn = CausalSelfAttention(config)
@@ -167,6 +177,10 @@ def configure_gpt_optimizer(
 
     # separate out all parameters to those that will and won't experience
     # regularizing weight decay
+    if log_once("torch_configure_gpt_optimizer_deprecation"):
+        deprecation_warning(
+            old="ray.rllib.models.torch.min_gpt.configure_gpt_optimizer"
+        )
     decay = set()
     no_decay = set()
     whitelist_w_modules = (torch.nn.Linear,)
@@ -220,6 +234,8 @@ class GPT(nn.Module):
     """GPT Transformer Model"""
 
     def __init__(self, config: GPTConfig):
+        if log_once("torch_gpt_deprecation"):
+            deprecation_warning(old="ray.rllib.models.torch.min_gpt.GPT")
         super().__init__()
         assert config.block_size is not None
         self.block_size = config.block_size
--- rllib/models/torch/misc.py
+++ rllib/models/torch/misc.py
@@ -6,12 +6,17 @@ from ray.rllib.models.utils import get_activation_fn
 from ray.rllib.utils.annotations import DeveloperAPI
 from ray.rllib.utils.framework import try_import_torch
 from ray.rllib.utils.typing import TensorType
+from ray.rllib.utils.deprecation import deprecation_warning
+from ray.util import log_once
 
 torch, nn = try_import_torch()
 
 
 @DeveloperAPI
 def normc_initializer(std: float = 1.0) -> Any:
+    if log_once("normc_initializer_deprecation"):
+        deprecation_warning(old="ray.rllib.models.torch.misc.normc_initializer")
+
     def initializer(tensor):
         tensor.data.normal_(0, 1)
         tensor.data *= std / torch.sqrt(tensor.data.pow(2).sum(1, keepdim=True))
