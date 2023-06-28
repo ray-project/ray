9709afd4e771fa05d285da0419144b98961a4ef3
Philipp Moritz <pcmoritz@gmail.com>
2023-06-28 13:58:43 -0700

    update

--- python/ray/train/__init__.py
+++ python/ray/train/__init__.py
@@ -2,13 +2,7 @@ from ray._private.usage import usage_lib
 from ray.train.backend import BackendConfig
 from ray.train.data_config import DataConfig
 from ray.train.constants import TRAIN_DATASET_KEY
-from ray.train._internal.session import (
-    get_dataset_shard,
-    get_trial_resources,
-    get_world_rank,
-    get_world_size,
-    report,
-)
+from ray.train._internal import session as context
 from ray.train.trainer import TrainingIterator
 
 from ray.air.config import RunConfig, ScalingConfig
@@ -16,15 +10,11 @@ from ray.air.config import RunConfig, ScalingConfig
 usage_lib.record_library_usage("train")
 
 __all__ = [
+    "context",
     "BackendConfig",
     "DataConfig",
     "RunConfig",
     "ScalingConfig",
     "TrainingIterator",
     "TRAIN_DATASET_KEY",
-    "get_dataset_shard",
-    "get_trial_resources",
-    "get_world_rank",
-    "get_world_size",
-    "report",
 ]
