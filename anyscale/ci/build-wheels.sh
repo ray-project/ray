f9cfa55fdb37e310fbb260f6c335b07d46931a6b
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2023-06-28 17:53:03 -0700

    [runtime ci] build wheels for different python versions in parallel (#12)
    so that it is faster.
    
    the rayforge step is repeated (for now), but it only takes around 2
    minutes, so not a big deal.

--- anyscale/ci/build-wheels.sh
+++ anyscale/ci/build-wheels.sh
@@ -2,16 +2,12 @@
 
 set -euo pipefail
 
-export TRAVIS_PULL_REQUEST=true
 export TRAVIS_COMMIT="$(git rev-parse HEAD)"
-export CI=true
-export BUILD_ONE_PYTHON_ONLY=""
 
 DOCKER_RUN_ARGS=(
     -e "TRAVIS=true"
-    -e "TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST:-false}"
     -e "TRAVIS_COMMIT=${TRAVIS_COMMIT}"
-    -e "CI=${CI}"
+    -e "CI=true"
     -e "RAY_INSTALL_JAVA=${RAY_INSTALL_JAVA:-}"
     -e "BUILDKITE=${BUILDKITE:-}"
     -e "BUILDKITE_PULL_REQUEST=${BUILDKITE_PULL_REQUEST:-}"
