694729d2c84039f15fb768ddcc66c117175c66d7
vitsai <victoria@anyscale.com>
2023-06-29 13:58:27 -0700

    comments
    Signed-off-by: vitsai <victoria@anyscale.com>

--- src/ray/gcs/gcs_server/gcs_server.h
+++ src/ray/gcs/gcs_server/gcs_server.h
@@ -14,8 +14,6 @@
 
 #pragma once
 
-#include <atomic>
-
 #include "ray/common/asio/instrumented_io_context.h"
 #include "ray/common/ray_syncer/ray_syncer.h"
 #include "ray/common/runtime_env_manager.h"
@@ -190,8 +188,7 @@ class GcsServer {
   /// Get cluster id if persisted, otherwise generate
   /// a new one and persist as necessary.
   /// Expected to be idempotent while server is up.
-  void RetrieveAndCacheClusterId(
-      std::function<void(ClusterID cluster_id)> &&continuation);
+  void GetOrGenerateClusterId(std::function<void(ClusterID cluster_id)> &&continuation);
 
   /// Print the asio event loop stats for debugging.
   void PrintAsioStats();
