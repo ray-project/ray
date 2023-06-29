3812ef5be174498f7a6ab37ce6b3c2232a40e905
Balaji Veeramani <balaji@anyscale.com>
2023-06-29 13:21:29 -0700

    [Data] Remove `_convert_block_to_tabular_block` (#36943)
    `_convert_block_to_tabular_block` is dead code. This PR removes it.
    
    **Context**
    
    https://github.com/ray-project/ray/pull/28413 introduced the `partitioning` parameter. With this feature, you could include data from file paths in your dataset. For example, if you read a Parquet file like `s3://bucket/year=2023/piece.parquet`, the year would be included in your dataset.
    
    The problem was that `Datasets` could represent unstructured data like a list of numbers. In this case, we needed to convert the `Dataset` to a tabular dataset before adding the partition data.
    
    With https://github.com/ray-project/ray/pull/36472, `Datasets` always represent structured data, so `_convert_block_to_tabular_block` is unused.
    
    Signed-off-by: Balaji Veeramani <balaji@anyscale.com>
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- python/ray/data/datasource/binary_datasource.py
+++ python/ray/data/datasource/binary_datasource.py
@@ -1,5 +1,5 @@
 from io import BytesIO
-from typing import TYPE_CHECKING, List, Optional, Tuple, Union
+from typing import TYPE_CHECKING
 
 from ray.data._internal.arrow_block import ArrowBlockBuilder
 from ray.data.datasource.file_based_datasource import FileBasedDatasource
@@ -57,28 +57,6 @@ class BinaryDatasource(FileBasedDatasource):
                 return [(path, data)]
             else:
                 return [data]
-
-    def _convert_block_to_tabular_block(
-        self,
-        block: List[Union[bytes, Tuple[bytes, str]]],
-        column_name: Optional[str] = None,
-    ) -> "pyarrow.Table":
-        import pyarrow as pa
-
-        if isinstance(block, pa.Table):
-            return block
-        else:
-            if column_name is None:
-                column_name = self._COLUMN_NAME
-
-            assert len(block) == 1
-            record = block[0]
-
-            if isinstance(record, tuple):
-                path, data = record
-                return pa.table({column_name: [data], "path": [path]})
-            else:
-                return pa.table({column_name: [record]})
 
     def _rows_per_file(self):
         return 1
--- python/ray/data/datasource/file_based_datasource.py
+++ python/ray/data/datasource/file_based_datasource.py
@@ -250,26 +250,6 @@ class FileBasedDatasource(Datasource):
             "Subclasses of FileBasedDatasource must implement _read_file()."
         )
 
-    def _convert_block_to_tabular_block(
-        self, block: Block, column_name: Optional[str] = None
-    ) -> Union["pyarrow.Table", "pd.DataFrame"]:
-        """Convert block returned by `_read_file` or `_read_stream` to a tabular block.
-
-        If your `_read_file` or `_read_stream` implementation returns a list,
-        then you need to implement this method. Otherwise, `FileBasedDatasource` won't
-        be able to include partition data.
-        """
-        import pandas as pd
-        import pyarrow as pa
-
-        if isinstance(block, (pd.DataFrame, pa.Table)):
-            return block
-
-        raise NotImplementedError(
-            "If your `_read_file` or `_read_stream` implementation returns a list, "
-            "then you need to implement `_convert_block_to_tabular_block."
-        )
-
     def write(
         self,
         blocks: Iterable[Block],
@@ -442,8 +422,6 @@ class _FileBasedDatasourceReader(Reader):
 
         paths, file_sizes = self._paths, self._file_sizes
         read_stream = self._delegate._read_stream
-        convert_block_to_tabular_block = self._delegate._convert_block_to_tabular_block
-        column_name = reader_args.get("column_name", None)
         filesystem = _wrap_s3_serialization_workaround(self._filesystem)
 
         if open_stream_args is None:
@@ -499,7 +477,6 @@ class _FileBasedDatasourceReader(Reader):
                 with open_input_source(fs, read_path, **open_stream_args) as f:
                     for data in read_stream(f, read_path, **reader_args):
                         if partitions:
-                            data = convert_block_to_tabular_block(data, column_name)
                             data = _add_partitions(data, partitions)
 
                         output_buffer.add_block(data)
