8265adefaec2569d2bfc94a974d31046c362e530
Eric Liang <ekhliang@gmail.com>
2023-06-30 11:16:35 -0700

    stub tests
    Signed-off-by: Eric Liang <ekhliang@gmail.com>

--- python/ray/tune/tests/test_new_persistence.py
+++ python/ray/tune/tests/test_new_persistence.py
@@ -36,9 +36,45 @@ def train_model(config):
         )
 
 
-# Test writing to a FAKE nfs path.
 @pytest.mark.parametrize("storage_path", [None, FAKE_NFS_DIR])
-def test_storage_path(storage_path):
+def test_multirank_trainer_storage_path(storage_path):
+    """Test PyTorch trainer with two workers.
+
+    Test the storage format and rank organization of artifacts and checkpoints."""
+    raise NotImplementedError("TODO")
+
+
+@pytest.mark.parametrize("storage_path", [None, FAKE_NFS_DIR])
+def test_multirank_trainer_restore_checkpoint(storage_path):
+    """Test you can restore a multi-rank PyTorch trainer from a checkpoint."""
+    raise NotImplementedError("TODO")
+
+
+def test_custom_storage_filesystem(storage_path):
+    """Test that user can pass a custom storage filesystem.
+
+    TODO: how do we check the filesystem is used? Maybe a custom filesystem can write
+    data to a mocked local bucket directory."""
+    raise NotImplementedError("TODO")
+
+
+def test_raise_error_if_storage_path_not_readable(storage_path):
+    """Check that if storage path _valid file not readable an error is raised.
+
+    This happens if the user sets a path that isn't a NFS / sets an invalid storage
+    filesystem that isn't readable from workers.
+
+    TODO: How do we test this without a real cluster?"""
+    raise NotImplementedError("TODO")
+
+
+@pytest.mark.parametrize("storage_path", [None, FAKE_NFS_DIR])
+def test_tuner_storage_path(storage_path):
+    """Check that the right files are created and synced in the basic setup.
+
+    Test with and without storage path set. We can fully mock this locally by setting
+    a storage path to a separate dir from ray results.
+    """
     shutil.rmtree(RAY_RESULTS, ignore_errors=True)
     shutil.rmtree(FAKE_NFS_DIR, ignore_errors=True)
 
@@ -59,7 +95,7 @@ def test_storage_path(storage_path):
     if storage_path:
         assert storage_path in results[0].checkpoint.path
     else:
-        assert RAY_RESULTS_DIR in results[0].checkpoint.path
+        assert RAY_RESULTS in results[0].checkpoint.path
 
     # One experiment result dir.
     if storage_path:
@@ -70,12 +106,14 @@ def test_storage_path(storage_path):
     assert len(glob.glob(f"{RAY_RESULTS}/train_model*")) == 1
     if storage_path:
         assert len(glob.glob(f"{storage_path}/train_model*/train_*")) == 2
+    # TODO fix
     #        assert len(glob.glob(f"{storage_path}/train_model*/basic-variant-state*.json")) == 1
     #        assert len(glob.glob(f"{storage_path}/train_model*/experiment_state*.json")) == 1
     #        assert len(glob.glob(f"{storage_path}/train_model*/tuner.pkl")) == 1
     assert len(glob.glob(f"{RAY_RESULTS}/train_model*/train_*")) == 2
     assert len(glob.glob(f"{RAY_RESULTS}/train_model*/basic-variant-state*.json")) == 1
     assert len(glob.glob(f"{RAY_RESULTS}/train_model*/experiment_state*.json")) == 1
+    # TODO fix
     #    assert len(glob.glob(f"{RAY_RESULTS}/train_model*/tuner.pkl")) == 1
 
     # Artifact and metrics sync.
