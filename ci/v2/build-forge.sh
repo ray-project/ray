0646889372c2ed1587436316208a49652a49966d
Lonnie Liu <lonnie@anyscale.com>
2023-07-08 21:56:22 +0000

    no lsb

--- ci/v2/build-forge.sh
+++ ci/v2/build-forge.sh
@@ -12,8 +12,10 @@ apt-get install -y ca-certificates curl zip unzip sudo gnupg tzdata
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
 apt-get install -y docker-ce-cli awscli
