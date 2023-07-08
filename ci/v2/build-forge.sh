2986963e967e55df40af4775e7381a7d684adeb4
Lonnie Liu <lonnie@anyscale.com>
2023-07-08 21:52:46 +0000

    ubuntu does not use lsb

--- ci/v2/build-forge.sh
+++ ci/v2/build-forge.sh
@@ -12,7 +12,7 @@ apt-get install -y ca-certificates curl zip unzip sudo gnupg tzdata
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 apt-get update
