799f66ad49f53b2e6439d4a7624539df1d62cd85
Lonnie Liu <lonnie@anyscale.com>
2023-07-08 21:15:09 +0000

    .

--- ci/v2/build-forge.sh
+++ ci/v2/build-forge.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+apt-get update
+apt-get upgrade -y
+apt-get install -y ca-certificates curl zip unzip sudo gnupg tzdata
+
 # Add docker APT repo
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -9,9 +13,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
 
-
-apt-get update -y
-apt-get upgrade -y
+apt-get update
 apt-get install -y docker-ce-cli awscli
 
 export DOCKER_BUILDKIT=1
@@ -22,3 +24,5 @@ tar --mtime="UTC 2020-01-01" -c -f - \
     ci/v2/forge/Dockerfile \
     | docker build --progress=plain -t forge \
         -f ci/v2/forge/Dockerfile -
+
+# TODO(aslonnie): push to RAYCI_TEMP_CR_REPO
