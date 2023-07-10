313b9236d526ea7ff35260efa0b376dd0fd4b724
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-07-10 17:51:58 -0500

    [serve] Document that generators raise `asyncio.CancelledError` when the client disconnects (#37268)
    Documents cancellation behavior for streaming responses.
    
    As a fly-by I also removed the info about setting the streaming feature flag (it's on by default now).
    
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- doc/source/serve/doc_code/http_guide/streaming_example.py
+++ doc/source/serve/doc_code/http_guide/streaming_example.py
@@ -38,3 +38,45 @@ r = requests.get("http://localhost:8000?max=10", stream=True)
 r.raise_for_status()
 for i, chunk in enumerate(r.iter_content(chunk_size=None, decode_unicode=True)):
     assert chunk == str(i)
+
+
+# __begin_cancellation__
+import asyncio
+import time
+from typing import AsyncGenerator
+
+import requests
+from starlette.responses import StreamingResponse
+from starlette.requests import Request
+
+from ray import serve
+
+
+@serve.deployment
+class StreamingResponder:
+    async def generate_forever(self) -> AsyncGenerator[str, None]:
+        try:
+            i = 0
+            while True:
+                yield str(i)
+                i += 1
+                await asyncio.sleep(0.1)
+        except asyncio.CancelledError:
+            print("Cancelled! Exiting.")
+
+    def __call__(self, request: Request) -> StreamingResponse:
+        gen = self.generate_forever()
+        return StreamingResponse(gen, status_code=200, media_type="text/plain")
+
+
+serve.run(StreamingResponder.bind())
+
+r = requests.get("http://localhost:8000?max=10", stream=True)
+start = time.time()
+r.raise_for_status()
+for i, chunk in enumerate(r.iter_content(chunk_size=None, decode_unicode=True)):
+    print(f"Got result {round(time.time()-start, 1)}s after start: '{chunk}'")
+    if i == 10:
+        print("Client disconnecting")
+        break
+# __end_cancellation__
