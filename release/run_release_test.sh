f738be96a37cd5127575033069fa5cd6cc8e10c0
can <can@anyscale.com>
2023-07-11 18:11:37 +0000

    Fix
    Signed-off-by: can <can@anyscale.com>

--- release/run_release_test.sh
+++ release/run_release_test.sh
@@ -40,7 +40,7 @@ if [ -n "${RAY_COMMIT_OF_WHEEL-}" ]; then
   HEAD_COMMIT=$(git rev-parse HEAD)
   HEAD_BRANCH=$(git rev-parse --abbrev-ref HEAD)
   echo "The test repo has head commit of ${HEAD_COMMIT}"
-  if [[ "${HEAD_COMMIT}" != "${RAY_COMMIT_OF_WHEEL}" && ("${HEAD_BRANCH}" == "master" || "${HEAD_BRANCH}" = "releases/*") ]]; then
+  if [[ "${HEAD_COMMIT}" != "${RAY_COMMIT_OF_WHEEL}" && ("${HEAD_BRANCH}" == "master" || "${HEAD_BRANCH}" = releases/*) ]]; then
     echo "The checked out test code doesn't match with the installed wheel. \
           This is likely due to a racing condition when a PR is landed between \
           a wheel is installed and test code is checked out."
