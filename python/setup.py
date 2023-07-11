7432a09a636024bf5d430c0e267b7e19e4bc01ba
Ruiyang Wang <56065503+rynewang@users.noreply.github.com>
2023-07-10 19:23:51 -0700

    [core][1/N] Add standalone runtime_env_agent. (#37158)
    Why are these changes needed?
    Breaks runtime env agent from dashboard agent into a standalone process. Part of issue #35472.
    
    The whole initiative is very big (see PR #36917) so we break it down into smaller pieces.
    
    This PR introduces a new standalone runtime_env_agent that is a HTTP server and is independent from dashboard_agent. It uses aiohttp so we vendor it in wheel building time. The vendor path (ray/_private/runtime_env/thirdparty_files) are .gitignored and excluded from lints.
    
    The file test_runtime_env_agent.py is almost identical to the pre-existing dashboard/modules/runtime_env/tests/test_runtime_env_agent.py, only with these changes:
    
    import paths
    removal of grpc server
    extra loggings
    The main.py is the "driver" which serves arg parsing and http server.
    
    To limit the PR size, this change does NOT invoke or run the agent anywhere (other than the unit test).
    
    Signed-off-by: Ruiyang Wang <rywang014@gmail.com>
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- python/setup.py
+++ python/setup.py
@@ -43,8 +43,15 @@ BAZEL_LIMIT_CPUS = os.getenv("BAZEL_LIMIT_CPUS")
 
 PICKLE5_SUBDIR = os.path.join("ray", "pickle5_files")
 THIRDPARTY_SUBDIR = os.path.join("ray", "thirdparty_files")
+RUNTIME_ENV_AGENT_THIRDPARTY_SUBDIR = os.path.join(
+    "ray", "_private", "runtime_env", "agent", "thirdparty_files"
+)
 
-CLEANABLE_SUBDIRS = [PICKLE5_SUBDIR, THIRDPARTY_SUBDIR]
+CLEANABLE_SUBDIRS = [
+    PICKLE5_SUBDIR,
+    THIRDPARTY_SUBDIR,
+    RUNTIME_ENV_AGENT_THIRDPARTY_SUBDIR,
+]
 
 # In automated builds, we do a few adjustments before building. For instance,
 # the bazel environment is set up slightly differently, and symlinks are
@@ -557,6 +564,20 @@ def build(build_python, build_java, build_cpp):
             + pip_packages,
             env=dict(os.environ, CC="gcc"),
         )
+
+    # runtime env agent dependenceis
+    runtime_env_agent_pip_packages = ["aiohttp"]
+    subprocess.check_call(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "-q",
+            "--target=" + os.path.join(ROOT_DIR, RUNTIME_ENV_AGENT_THIRDPARTY_SUBDIR),
+        ]
+        + runtime_env_agent_pip_packages
+    )
 
     bazel_flags = ["--verbose_failures"]
     if BAZEL_ARGS:
