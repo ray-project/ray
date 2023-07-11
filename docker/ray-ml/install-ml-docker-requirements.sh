eadc1fe58a09f641b119513445f65920c324dfdb
Kai Fricke <kai@anyscale.com>
2023-07-11 11:25:04 -0700

    device-specific constraints [build_base]
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- docker/ray-ml/install-ml-docker-requirements.sh
+++ docker/ray-ml/install-ml-docker-requirements.sh
@@ -33,14 +33,18 @@ pip --no-cache-dir install -U \
            -r tune-test-requirements.txt \
            -r ray-docker-requirements.txt
 
+
+# Remove any device-specific constraints from requirements_compiled.txt.
+# E.g.: torch-scatter==2.1.1+pt20cpu or torchvision==0.15.2+cpu
+# These are replaced with gpu-specific requirements in dl-gpu-requirements.txt.
+sed "/[0-9]\+cpu/d;/[0-9]\+pt/d" "requirements_compiled.txt" > requirements_compiled_gpu.txt
+
 # explicitly install (overwrite) pytorch with CUDA support
-# do not use compiled requirements as constraint here as some
-# device-specific constraints can't be met (e.g. torch+cpu).
-# Instead, we don't use `-U` so other dependencies don't get updated.
 pip --no-cache-dir install \
            -c requirements.txt \
+           -c requirements_compiled_gpu.txt \
            -r dl-gpu-requirements.txt
 
 sudo apt-get clean
 
-sudo rm ./*requirements.txt
+sudo rm ./*requirements*.txt
