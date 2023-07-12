ebb468c16344ebc72650a6aaa553a83a6f5a335b
Chen Shen <scv119@gmail.com>
2023-07-12 09:14:38 -0700

    Revert "[Core] Fix the race condition where grpc requests are handled while c… (#37301)"
    This reverts commit 0138bdc68315e3f1b3edbab868b40586d07c301f.

--- src/ray/rpc/server_call.h
+++ src/ray/rpc/server_call.h
@@ -64,16 +64,6 @@ enum class ServerCallState {
 
 class ServerCallFactory;
 
-/// Represents a service handler that might not
-/// be ready to serve RPCs immediately after construction.
-class DelayedServiceHandler {
- public:
-  virtual ~DelayedServiceHandler() = default;
-
-  /// Blocks until the service is ready to serve RPCs.
-  virtual void WaitUntilInitialized() = 0;
-};
-
 /// Represents an incoming request of a gRPC server.
 ///
 /// The lifecycle and state transition of a `ServerCall` is as follows:
@@ -158,7 +148,6 @@ class ServerCallImpl : public ServerCall {
   /// \param[in] io_service The event loop.
   /// \param[in] call_name The name of the RPC call.
   /// \param[in] record_metrics If true, it records and exports the gRPC server metrics.
-  /// request.
   ServerCallImpl(
       const ServerCallFactory &factory,
       ServiceHandler &service_handler,
@@ -207,9 +196,6 @@ class ServerCallImpl : public ServerCall {
   }
 
   void HandleRequestImpl() {
-    if constexpr (std::is_base_of_v<DelayedServiceHandler, ServiceHandler>) {
-      service_handler_.WaitUntilInitialized();
-    }
     state_ = ServerCallState::PROCESSING;
     // NOTE(hchen): This `factory` local variable is needed. Because `SendReply` runs in
     // a different thread, and will cause `this` to be deleted.
