382f290444828445fae54c560663888834b370db
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-07-12 12:05:13 -0500

    [serve] Add test for Gradio queues (which use websockets) (#36372)
    Adds a test for the Gradio queue interface which uses websockets under the hood.
    
    Rather than reaching into the implementation details, I'm using the `gradio_client` package to make the requests, so also updated the existing tests to use this.
    
    Signed-off-by: NripeshN <nn2012@hw.ac.uk>

--- python/ray/serve/tests/test_gradio.py
+++ python/ray/serve/tests/test_gradio.py
@@ -2,27 +2,17 @@ import os
 import sys
 
 import gradio as gr
+from gradio_client import Client
 import pytest
-import requests
 
-import ray
 from ray._private.test_utils import wait_for_condition
 
 from ray import serve
 from ray.serve.gradio_integrations import GradioIngress, GradioServer
 
 
-@pytest.fixture
-def serve_start_shutdown():
-    ray.init()
-    serve.start()
-    yield
-    serve.shutdown()
-    ray.shutdown()
-
-
 @pytest.mark.parametrize("use_user_defined_class", [False, True])
-def test_gradio_ingress_correctness(serve_start_shutdown, use_user_defined_class: bool):
+def test_gradio_ingress_correctness(serve_instance, use_user_defined_class: bool):
     """
     Ensure a Gradio app deployed to a cluster through GradioIngress still
     produces the correct output.
@@ -48,16 +38,11 @@ def test_gradio_ingress_correctness(serve_start_shutdown, use_user_defined_class
 
     serve.run(app)
 
-    test_input = "Alice"
-    response = requests.post(
-        "http://127.0.0.1:8000/api/predict/", json={"data": [test_input]}
-    )
-    assert response.status_code == 200 and response.json()["data"][0] == greet(
-        test_input
-    )
+    client = Client("http://localhost:8000")
+    assert client.predict("Alice") == "Good morning Alice!"
 
 
-def test_gradio_ingress_scaling(serve_start_shutdown):
+def test_gradio_ingress_scaling(serve_instance):
     """
     Check that a Gradio app that has been deployed to a cluster through
     GradioIngress scales as needed, i.e. separate client requests are served by
@@ -67,26 +52,15 @@ def test_gradio_ingress_scaling(serve_start_shutdown):
     def f(*args):
         return os.getpid()
 
-    app = GradioServer.options(num_replicas=2).bind(
-        lambda: gr.Interface(fn=f, inputs="text", outputs="text")
-    )
-    serve.run(app)
-
-    def two_pids_returned():
-        @ray.remote
-        def get_pid_from_request():
-            r = requests.post(
-                "http://127.0.0.1:8000/api/predict/", json={"data": ["input"]}
-            )
-            r.raise_for_status()
-            return r.json()["data"][0]
-
-        return (
-            len(set(ray.get([get_pid_from_request.remote() for _ in range(10)]))) == 2
+    serve.run(
+        GradioServer.options(num_replicas=2).bind(
+            lambda: gr.Interface(fn=f, inputs="text", outputs="text")
         )
+    )
 
+    client = Client("http://localhost:8000")
     # Verify that the requests are handled by two separate replicas.
-    wait_for_condition(two_pids_returned)
+    wait_for_condition(lambda: len({client.predict("input") for _ in range(3)}) == 2)
 
 
 if __name__ == "__main__":
