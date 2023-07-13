9d65e46b762278dca3b6115f9505f4090b43089a
Hao Chen <chenh1024@gmail.com>
2023-07-12 18:02:23 -0700

    [data][doc] Add DatasetConfig -> DataConfig migration guide  (#37278)
    - Add DatasetConfig -> DataConfig migration guide
    - Move ray.train.data_config to ray.train._internal.data_config
    
    ---------
    
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- python/ray/train/__init__.py
+++ python/ray/train/__init__.py
@@ -2,10 +2,8 @@ from ray._private.usage import usage_lib
 from ray.train._internal.data_config import DataConfig
 from ray.train._internal.session import get_dataset_shard, report
 from ray.train.backend import BackendConfig
-from ray.train.data_config import DataConfig
-from ray.train.context import get_context
 from ray.train.constants import TRAIN_DATASET_KEY
-from ray.train._internal.session import get_dataset_shard, report
+from ray.train.context import get_context
 from ray.train.trainer import TrainingIterator
 
 from ray.air import Checkpoint
