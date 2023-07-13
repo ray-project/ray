6f5bc2bfce46ff27cca97a40129779bdebb6886b
Scott Lee <sjl@anyscale.com>
2023-07-12 22:13:03 -0700

    update more examples with different csv format
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- doc/source/data/doc_code/batch_formats.py
+++ doc/source/data/doc_code/batch_formats.py
@@ -8,7 +8,7 @@ import ray
 ds = ray.data.read_csv("s3://anonymous@ray-example-data/iris.csv")
 
 def map_function(data):
-    return data[data["sepal.length"] < 5]
+    return data[data["sepal length (cm)"] < 5]
 
 batch = ds.take_batch(10, batch_format="pandas")
 mapped_batch = map_function(batch)
@@ -22,16 +22,16 @@ import pandas as pd
 
 ds = ray.data.read_csv("s3://anonymous@ray-example-data/iris.csv")
 ds.show(1)
-# -> {'sepal.length': 5.1, ..., 'petal.width': 0.2, 'variety': 'Setosa'}
+# -> {'sepal length (cm)': 5.1, ..., 'petal width (cm)': 0.2, 'target': 0}
 
 def transform_pandas(df_batch: pd.DataFrame) -> pd.DataFrame:
-    df_batch = df_batch[df_batch["variety"] == "Versicolor"]
-    df_batch.loc[:, "normalized.sepal.length"] = df_batch["sepal.length"] / df_batch["sepal.length"].max()
-    df_batch = df_batch.drop(columns=["sepal.length"])
+    df_batch = df_batch[df_batch["target"] == 2]
+    df_batch.loc[:, "normalized.sepal length (cm)"] = df_batch["sepal length (cm)"] / df_batch["sepal length (cm)"].max()
+    df_batch = df_batch.drop(columns=["sepal length (cm)"])
     return df_batch
 
 ds.map_batches(transform_pandas, batch_format="pandas").show(1)
-# -> {..., 'variety': 'Versicolor', 'normalized.sepal.length': 1.0}
+# -> {..., 'target': 2, 'normalized.sepal length (cm)': 1.0}
 # __simple_pandas_end__
 
 # __simple_numpy_start__
@@ -66,8 +66,8 @@ ds = ray.data.read_csv("s3://anonymous@ray-example-data/iris.csv")
 
 
 def transform_pyarrow(batch: pa.Table) -> pa.Table:
-    batch = batch.filter(pac.equal(batch["variety"], "Versicolor"))
-    return batch.drop(["sepal.length"])
+    batch = batch.filter(pac.equal(batch["target"], 1))
+    return batch.drop(["sepal length (cm)"])
 
 
 # test map function on a batch
@@ -75,6 +75,6 @@ batch = ds.take_batch(1, batch_format="pyarrow")
 mapped_batch = transform_pyarrow(batch)
 
 ds.map_batches(transform_pyarrow, batch_format="pyarrow").show(1)
-# -> {'sepal.width': 3.2, ..., 'variety': 'Versicolor'}
+# -> {'sepal width (cm)': 3.2, ..., 'target': 1}
 # __simple_pyarrow_end__
 # fmt: on
