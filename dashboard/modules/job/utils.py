b7489de05c0b0981c44fca5f8de52820e0806d23
Alan Guo <aguo@anyscale.com>
2023-07-14 13:34:58 -0700

    Support both pydantic 1.x and 2.x (#37055)
    These changes fixes a couple of things when ray and pydantic 2.0 is installed:
    
    ray can start up now
    ray jobs works now
    gets rid of some deprecation warnings when using old pydantic field names.
    Some things are still broken:
    
    ray serve doesn't work because it depends on fastapi and fastapi does not support pydantic 2.0 yet: Prepare for Pydantic V2 release tiangolo/fastapi#6051
    Pickling pydantic 2 models is not possible, although users can still pickle pydantic 1.0 models via the compatibility pydantic.v1 import.

--- dashboard/modules/job/utils.py
+++ dashboard/modules/job/utils.py
@@ -168,7 +168,7 @@ async def get_driver_jobs(
             driver = DriverInfo(
                 id=job_id,
                 node_ip_address=job_table_entry.driver_address.ip_address,
-                pid=job_table_entry.driver_pid,
+                pid=str(job_table_entry.driver_pid),
             )
             job = JobDetails(
                 job_id=job_id,
@@ -190,7 +190,7 @@ async def get_driver_jobs(
             driver = DriverInfo(
                 id=job_id,
                 node_ip_address=job_table_entry.driver_address.ip_address,
-                pid=job_table_entry.driver_pid,
+                pid=str(job_table_entry.driver_pid),
             )
             submission_job_drivers[job_submission_id] = driver
 
