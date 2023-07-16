13e9f62120c1e31fe8fc06496ea50ab79d2c782d
Philipp Moritz <pcmoritz@gmail.com>
2023-07-15 17:28:40 -0700

    update

--- python/ray/serve/tests/test_air_integrations_gpu.py
+++ python/ray/serve/tests/test_air_integrations_gpu.py
@@ -3,8 +3,8 @@ import pytest
 import requests
 
 import ray
+from ray import serve
 from ray.air.checkpoint import Checkpoint
-from ray.serve.air_integrations import PredictorDeployment
 from ray.train.predictor import Predictor
 
 
@@ -28,9 +28,17 @@ def test_automatic_enable_gpu(serve_instance):
                 raise ValueError("GPU not enabled")
             return [{"value": val} for val in data.tolist()]
 
-    _ = PredictorDeployment.options(
+    @serve.deployment
+    class DummyGPUDeployment:
+        def __init__(self, checkpoint):
+            self.predictor = DummyPredictor.from_checkpoint(checkpoint)
+
+        async def __call__(self, data):
+            return self.predictor.predict(data)
+
+    _ = DummyGPUDeployment.options(
         name="GPU", ray_actor_options={"num_gpus": 1}
-    ).deploy(predictor_cls=DummyGPUPredictor, checkpoint=Checkpoint.from_dict({"x": 1}))
+    ).deploy(Checkpoint.from_dict({"x": 1}))
 
     assert ray.get(send_request.remote(json={"array": [40]})) == {"value": [40]}
 
