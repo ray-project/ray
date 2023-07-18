0682b718e5c3a568be38186401be8b639eb58d52
Hao Chen <chenh1024@gmail.com>
2023-07-18 12:03:58 -0700

    split logical interfaces
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

---
+++ python/ray/data/_internal/logical/interfaces/logical_plan.py
@@ -0,0 +1,13 @@
+from .plan import Plan
+from .logical_operator import LogicalOperator
+
+class LogicalPlan(Plan):
+    """The plan with a DAG of logical operators."""
+
+    def __init__(self, dag: LogicalOperator):
+        self._dag = dag
+
+    @property
+    def dag(self) -> LogicalOperator:
+        """Get the DAG of logical operators."""
+        return self._dag
