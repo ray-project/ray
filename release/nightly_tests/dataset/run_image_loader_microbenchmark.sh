50b9cb4e19329eddf1a72555c0cdb0b4600d4dc3
Stephanie Wang <swang@cs.berkeley.edu>
2023-07-18 16:55:24 -0700

    [data] Add microbenchmark for torch vs ray.data image-loading and transform (#37498)
    Adds a microbenchmark that tests ray.data.read_images and .map_batches performance compared to torch dataloader.
    
    ---------
    
    Signed-off-by: Stephanie Wang <swang@cs.berkeley.edu>
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

---
+++ release/nightly_tests/dataset/run_image_loader_microbenchmark.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+INPUT_DIR=~/imagenet-1gb
+OUTPUT_DIR=~/imagenet-1gb-data
+
+# Download 1GB dataset from S3 to local disk.
+aws s3 sync s3://air-cuj-imagenet-1gb $INPUT_DIR
+
+# Preprocess files to get to the directory structure that torch dataloader
+# expects.
+for filename in "$INPUT_DIR"/*; do
+    filename=$(basename "$filename")
+    class_dir=$(echo "$filename" | awk '{split($0, array, "_"); print array[1]}')
+    img_path=$(echo "$filename" | awk '{split($0, array, "_"); print array[2]}')
+    mkdir -p "$OUTPUT_DIR"/"$class_dir"
+    out_path="$OUTPUT_DIR/$class_dir/$img_path"
+    echo "$out_path"
+    cp "$INPUT_DIR"/"$filename" "$out_path"
+done
+
+python image_loader_microbenchmark.py
