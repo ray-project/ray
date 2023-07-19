d13cf303cc115d22f1bc351eba0a00c9a93c8e58
Philipp Moritz <pcmoritz@gmail.com>
2023-07-18 18:30:19 -0700

    update

--- doc/source/ray-air/doc_code/computer_vision.py
+++ doc/source/ray-air/doc_code/computer_vision.py
@@ -419,7 +419,7 @@ def online_predict_tensorflow(checkpoint):
     response = requests.get("http://placekitten.com/200/300")
     image = Image.open(BytesIO(response.content))
 
-    payload = {"image": {"array": np.array(image).tolist(), "dtype": "float32"}}
+    payload = {"image": {"array": [np.array(image).tolist()], "dtype": "float32"}}
     response = requests.post("http://localhost:8000/", json=payload)
     predictions = response.json()
     # __tensorflow_online_predict_stop__
