2ea88542f8479d8e130423a65571ca1c37a378b7
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2023-07-20 09:23:54 -0700

    Fix lint (#37601)
    Signed-off-by: can <can@anyscale.com>
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- release/ray_release/byod/byod_dataset_shuffle.sh
+++ release/ray_release/byod/byod_dataset_shuffle.sh
@@ -4,9 +4,11 @@
 
 set -exo pipefail
 
-echo "yes N | sudo mkfs -t ext4 /dev/nvme1n1 || true" >> ~/.bashrc
-echo "mkdir -p /tmp/data0" >> ~/.bashrc
-echo "mkdir -p /tmp/data1" >> ~/.bashrc
-echo "sudo chmod 0777 /tmp/data0" >> ~/.bashrc
-echo "sudo chmod 0777 /tmp/data1" >> ~/.bashrc
-echo "sudo mount /dev/nvme1n1 /tmp/data1 || true" >> ~/.bashrc
+{
+  echo "yes N | sudo mkfs -t ext4 /dev/nvme1n1 || true"
+  echo "mkdir -p /tmp/data0"
+  echo "mkdir -p /tmp/data1"
+  echo "sudo chmod 0777 /tmp/data0"
+  echo "sudo chmod 0777 /tmp/data1"
+  echo "sudo mount /dev/nvme1n1 /tmp/data1 || true"
+} >> ~/.bashrc
