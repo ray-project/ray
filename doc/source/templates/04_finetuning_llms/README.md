ef37ece94a2e77cee58fe767910f8ace2522a3a6
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2023-07-20 00:20:20 -0700

    cleaned up and moved the checkpoints to s3
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- doc/source/templates/04_finetuning_llms/README.md
+++ doc/source/templates/04_finetuning_llms/README.md
@@ -14,15 +14,38 @@ data/test.jsonl # 1.3k test data
 tokens.json # a list of special tokens used to encode the dataset with
 ```
 
+### End-to-end script
+You can launch the training jobs by running the provided bash scripts. 
+
+```
+./run_llama_7b_chat.sh
+```
+
+### Downloading the model weights to remote nodes
+
+To make things faster and easier we have created a mirror link of huggingface weights on to S3. We first will download the weights on the nvme storage of any node that has a GPU. This will save us some time when iterating on the training script. 
+
+Note: For some reason the mirrored links are twice the size that they should be. 
+
+```
+python prepare_nodes.py --hf-model-id meta-llama/Llama-2-7b-chat-hf
+```
+
 ### Launching fine-tuning
 
-The script is written using Ray Train + Deepspeed integration via accelerate API. 
-The script is general enough that it can be used to fine-tune all released sizes of Llama-2 models. 
+The script is written using Ray Train + Deepspeed integration via accelerate API. The script is general enough that it can be used to fine-tune all released sizes of Llama-2 models. 
 
 The CLI for launching the training job is as follows:
 
 ```
-python finetune_llama.py --bs 4 --model_name [hf_model_id] --output_dir /mnt/shared_storage/finetuning-llama/ --train_path ./data/train.jsonl --test_path ./data/test.jsonl  --special_token_path ./gsm/tokens.json 
+python finetune_hf_llm.py \
+    -bs 16 \
+    -nd 16 \
+    --model_name ${MODEL_ID} \
+    --output_dir /mnt/cluster_storage/finetuning-llama/ \
+    --train_path ./data/train.jsonl \
+    --test_path ./data/test.jsonl  \
+    --special_token_path ./data/tokens.json
 ```
 
 This script was tested across three model sizes on the following cluster configurations on Anyscale platform. 
@@ -30,6 +53,6 @@ This script was tested across three model sizes on the following cluster configu
 
 | Model Size | HF Model ID                     | Batch size per device | Instance Type  | GPUs         |
 |------------|--------------------------------|------------------------|----------------|--------------|
-| 7B         | `daryl149/llama-2-7b-chat-hf`   | 16         | 16x `g5.xlarge` | 8x A10G (24G) |
-| 13B        | `daryl149/llama-2-13b-chat-hf`  | 16         | 4x `g5.24xlarge`| 32x A10G (24G)|
-| 70B        | `anonymous4chan/llama-2-70b`   | 4          | 4x `g5.24xlarge`| 32x A10G (24G)|
+| 7B         | `meta-llama/Llama-2-7b-chat-hf`   | 16         | 16x `g5.xlarge` | 8x A10G (24G) |
+| 13B        | `meta-llama/Llama-2-13b-chat-hf`  | 16         | 4x `g5.24xlarge`| 32x A10G (24G)|
+| 70B        | `meta-llama/Llama-2-70b-chat-hf`   | 4          | 4x `g5.24xlarge`| 32x A10G (24G)|
--- doc/source/templates/04_finetuning_llms/finetune_hf_llm.py
+++ doc/source/templates/04_finetuning_llms/finetune_hf_llm.py
@@ -15,6 +15,7 @@ from accelerate import Accelerator, DeepSpeedPlugin
 from accelerate.utils import DummyOptim, DummyScheduler, set_seed
 from transformers import (
     AutoModelForCausalLM,
+    AutoTokenizer,
     get_linear_schedule_with_warmup,
 )
 
@@ -25,23 +26,19 @@ from ray.air import session
 from ray.train.torch import TorchTrainer
 import ray.util.scheduling_strategies
 
-EVAL_BATCH_SIZE = 32
-MODEL = "tiiuae/falcon-7b"
-# MODEL = "tiiuae/falcon-40b"
-BLOCK_SIZE = 512
-OVERLAP_LENGTH = 128
+from utils import get_checkpoint_and_refs_dir, get_mirror_link
+
 OPTIM_BETAS = (0.9, 0.999)
 OPTIM_EPS = 1e-8
 OPTIM_WEIGHT_DECAY = 0.
 
-def collate_fn(batch, tokenizer, device):
-    print('-'*80)
-    print(type(batch["input"]))
-    print('-'*80)
+MIRROR_LINK = "s3://llama-2-weights/"
+
+def collate_fn(batch, tokenizer, block_size, device):
     out_batch = tokenizer(
         list(batch["input"]), 
         padding="longest", 
-        max_length=BLOCK_SIZE, 
+        max_length=block_size, 
         truncation=True, 
         return_tensors="pt"    
     )
@@ -51,19 +48,26 @@ def collate_fn(batch, tokenizer, device):
     
     return out_batch
 
+def get_pretrained_path(model_id: str):
+    mirror_uri = get_mirror_link(model_id)    
+    ckpt_path, _ = get_checkpoint_and_refs_dir(
+        model_id=model_id,
+        bucket_uri=mirror_uri
+    )
+    return ckpt_path
+
 
 def get_tokenizer(model_name, special_tokens):
-    
-    from transformers.models.llama.tokenization_llama import LlamaTokenizer
 
-    tokenizer = LlamaTokenizer.from_pretrained(model_name)
+    pretrained_path = get_pretrained_path(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_path)
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.add_tokens(special_tokens, special_tokens=True)
     
     return tokenizer
     
     
-def get_ds(path):
+def create_ray_dataset(path):
     # jsonl file
     with open(path, 'r') as json_file:
         items = [json.loads(x) for x in json_file]
@@ -76,7 +80,7 @@ def get_ds(path):
     df = pd.DataFrame.from_dict(dataset)
     return ray.data.from_pandas(df)
 
-def evaluate(args, model, eval_dataloader, accelerator, eval_dataset):
+def evaluate(model, eval_dataset_len, eval_dataloader, accelerator):
     model.eval()
     losses = []
     for step, batch in enumerate(eval_dataloader):
@@ -87,7 +91,7 @@ def evaluate(args, model, eval_dataloader, accelerator, eval_dataset):
         losses.append(accelerator.gather(loss.repeat(EVAL_BATCH_SIZE)))
 
     losses = torch.cat(losses)
-    losses = losses[: len(eval_dataset)]
+    losses = losses[: eval_dataset_len]
     try:
         eval_loss = torch.mean(losses)
         perplexity = math.exp(eval_loss)
@@ -108,12 +112,11 @@ def training_function(kwargs: dict):
     config = kwargs["config"]
     args = argparse.Namespace(**kwargs["args"])
     special_tokens = kwargs.get("special_tokens", [])
+    model_id = config["model_name"]
+    output_dir = config["output_dir"]
 
-    if not args.output_dir:
-        raise ValueError("--output_dir must be specified")
-    
-    if not Path(args.output_dir).exists():
-        Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    if not Path(output_dir).exists():
+        Path(output_dir).mkdir(parents=True, exist_ok=True)
 
     # Sample hyper-parameters for learning rate, batch size, seed and a few other HPs
     lr = config["lr"]
@@ -190,20 +193,26 @@ def training_function(kwargs: dict):
     valid_ds_len = len(list(valid_ds.iter_batches(batch_size=1)))
     
     tokenizer = get_tokenizer(model_name=args.model_name, special_tokens=special_tokens)
-    collate_partial = functools.partial(collate_fn, tokenizer=tokenizer, device=accelerator.device)
-        
-
+    collate_partial = functools.partial(
+        collate_fn, 
+        tokenizer=tokenizer,
+        block_size=config["block_size"], 
+        device=accelerator.device
+    )
+    
+    ckpt_path = session.get_trial_dir()
     if accelerator.is_main_process:
         print("Saving tokenizer and config.")
-        tokenizer.save_pretrained(args.output_dir)
+        tokenizer.save_pretrained(ckpt_path)
 
-    print("Loading model ...")
+
+    pretrained_path = get_pretrained_path(model_id)    
+    print(f"Loading model from {pretrained_path} ...")
     s = time.time()
     model = AutoModelForCausalLM.from_pretrained(
-        MODEL,
+        pretrained_path,
         trust_remote_code=True, 
-        use_cache=False, 
-        torch_dtype=torch.bfloat16
+        torch_dtype=torch.bfloat16,
     )
     print(f"Done loading model in {time.time() - s} seconds.")
     model.resize_token_embeddings(len(tokenizer))    
@@ -234,24 +243,20 @@ def training_function(kwargs: dict):
         accelerator.state.deepspeed_plugin is None
         or "scheduler" not in accelerator.state.deepspeed_plugin.deepspeed_config
     ):
-        print("linear scheduler")
         lr_scheduler = get_linear_schedule_with_warmup(
             optimizer=optimizer,
             num_warmup_steps=100,
             num_training_steps=(train_ds_len * num_epochs) // gradient_accumulation_steps,
         )
     else:
-        print("dummy scheduler")
         lr_scheduler = DummyScheduler(
             optimizer, total_num_steps=(train_ds_len * num_epochs) // gradient_accumulation_steps, warmup_num_steps=100
         )
 
     # Prepare everything
-    # There is no specific order to remember, we just need to unpack the objects in the same order we gave them to the
-    # prepare method.
+    # There is no specific order to remember, we just need to unpack the objects in the same order we gave them to the prepare method.
     s = time.time()
     model, optimizer, lr_scheduler = accelerator.prepare(model, optimizer, lr_scheduler)
-    
     print(f"Prepare done in {time.time() - s} seconds.")
 
     # Now we train the model
@@ -272,7 +277,6 @@ def training_function(kwargs: dict):
     
         for step, batch in enumerate(train_dataloader):
             # We could avoid this line since we set the accelerator with `device_placement=True`.
-            # batch = tree.map_structure(lambda x: x.to(accelerator.device), batch)
             with accelerator.accumulate(model):
                 s_fwd = time.time()
                 outputs = model(**batch)
@@ -296,19 +300,39 @@ def training_function(kwargs: dict):
                 avg_opt_step_time += e_opt_step - s_opt_step
                 
             if accelerator.is_main_process:
-                accelerator.print(f"[epoch {epoch} step {step}] loss: {loss.item()} steptime: {e_opt_step - s_fwd}")
+                accelerator.print(
+                    f"[epoch {epoch} step {step}] loss: {loss.item()} step-time: {e_opt_step - s_fwd}"
+                )
+                
+            # as long as this is not the last step report here
+            if step != (train_ds_len // batch_size - 1):
+                session.report(
+                    {
+                        "epoch": epoch,
+                        "step": step,
+                        "train_loss": loss_sum.item() / train_ds_len,
+                        "eval_loss": None,
+                        "perplexity": None,
+                        "number of iterations": step + 1,
+                        "Train time per epoch": None,
+                        "Eval time per epoch": None,
+                        "avg fwd time": avg_fwd_time / (step + 1),
+                        "avg bwd time": avg_bwd_time / (step + 1),
+                    },
+                )
 
         e_epoch = time.time()
         accelerator.print("Train time per epoch: ", e_epoch - s_epoch)
 
-        eval_s_epoch = time.time()
-        
+        eval_s_epoch = time.time()        
         valid_dataloader = valid_ds.iter_torch_batches(
             batch_size=batch_size,
             collate_fn=collate_partial,
         )
         
-        perplex, eloss = evaluate(args, model, valid_dataloader, accelerator, valid_ds)
+        perplex, eloss = evaluate(
+            model, valid_ds_len, valid_dataloader, accelerator
+        )
         accelerator.print("Eval result loss", eloss)
         accelerator.print("Eval perplex", perplex)
 
@@ -321,6 +345,7 @@ def training_function(kwargs: dict):
         session.report(
             {
                 "epoch": epoch,
+                "iteration": step,
                 "train_loss": loss_sum.item() / train_ds_len,
                 "eval_loss": eloss.item() / train_ds_len,
                 "perplexity": perplex / valid_ds_len,
@@ -332,24 +357,21 @@ def training_function(kwargs: dict):
             },
         )
         
-        if args.output_dir is not None:
-            accelerator.print(f"Saving bin version of model in {args.output_dir}")
-            accelerator.wait_for_everyone()
-            with accelerator.main_process_first():
-                if not os.path.exists(os.path.join(args.output_dir, str(epoch))):
-                    os.makedirs(os.path.join(args.output_dir, str(epoch)))
-            unwrapped_model = accelerator.unwrap_model(model)
-            unwrapped_model.save_pretrained(
-                os.path.join(args.output_dir, str(epoch)),
-                is_main_process=accelerator.is_main_process,
-                save_function=accelerator.save,
-                safe_serialization=False,
-                state_dict=accelerator.get_state_dict(model),
-            )
+        accelerator.print(f"Saving the model in {ckpt_path}")
+        accelerator.wait_for_everyone()
+        with accelerator.main_process_first():
+            ckpt_path_epoch = Path(ckpt_path) / f"epoch-{epoch}"
+            ckpt_path_epoch.mkdir(parents=True, exist_ok=True)
+        unwrapped_model = accelerator.unwrap_model(model)
+        unwrapped_model.save_pretrained(
+            ckpt_path_epoch,
+            is_main_process=accelerator.is_main_process,
+            save_function=accelerator.save,
+            safe_serialization=True,
+            state_dict=accelerator.get_state_dict(model),
+        )
 
-def main():
-    global MODEL
-    global EVAL_BATCH_SIZE
+def parse_args():
     
     parser = argparse.ArgumentParser(description="Simple example of training script.")
     parser.add_argument(
@@ -361,56 +383,121 @@ def main():
         "between fp16 and bf16 (bfloat16). Bf16 requires PyTorch >= 1.10."
         "and an Nvidia Ampere GPU.",
     )
-    parser.add_argument("--bs", type=int, default=64, help="Batch size to use.")
-    parser.add_argument("--grad_accum", type=int, default=1, help="Gradient accumulation steps.")
-    parser.add_argument("--train_path", type=str, help="Path to training jsonl file")
-    parser.add_argument("--test_path", type=str, help="Path to testing jsonl file")
-    parser.add_argument("--special_token_path", type=str, help="Path to token json file")    
-    parser.add_argument("--no-grad-ckpt", action="store_true", help="If passed, will not use gradient checkpointing.")
-    parser.add_argument("--output_dir", type=str, help="Path to output directory.")
-    parser.add_argument("--model_name", default="tiiuae/falcon-7b", type=str)
-    parser.add_argument("--num-epochs", type=int, default=10, help="Number of epochs to train for.")
-    parser.add_argument("--lr", type=float, default=5e-6, help="Learning rate to use.")
+    
+    parser.add_argument(
+        "--batch-size-per-device", 
+        "-bs", 
+        type=int, 
+        default=16, 
+        help="Batch size to use per device."
+    )
+    
+    parser.add_argument(
+        "--eval-batch-size-per-device", 
+        type=int, 
+        default=16, 
+        help="Batch size to use per device (For evaluation)."
+    )
+    
+    parser.add_argument(
+        "--num-devices",
+        "-nd", 
+        type=int, 
+        default=4, 
+        help="Number of devices to use."
+    )
+    parser.add_argument(
+        "--grad_accum", 
+        type=int, 
+        default=1, 
+        help="Gradient accumulation steps."
+    )
+    parser.add_argument(
+        "--train_path", 
+        type=str, 
+        help="Path to training jsonl file"
+    )
+    parser.add_argument(
+        "--test_path", 
+        type=str, 
+        help="Path to testing jsonl file"
+    )
+    parser.add_argument(
+        "--special_token_path", 
+        type=str, 
+        help="Path to token json file"
+    )    
+    parser.add_argument(
+        "--no-grad-ckpt", 
+        action="store_true", 
+        help="If passed, will not use gradient checkpointing."
+    )
+    parser.add_argument(
+        "--output_dir", 
+        type=str, 
+        help="Path to output directory."
+    )
+    parser.add_argument(
+        "--model_name", 
+        default="meta-llama/Llama-2-7b-chat-hf", 
+        type=str
+    )
+    parser.add_argument(
+        "--num-epochs", 
+        type=int, 
+        default=1, 
+        help="Number of epochs to train for."
+    )
+    parser.add_argument(
+        "--lr", 
+        type=float, 
+        default=5e-6, 
+        help="Learning rate to use."
+    )
+    
+    parser.add_argument(
+        "--ctx-len", 
+        type=int, 
+        default=512, 
+        help="Learning rate to use."
+    )
     args = parser.parse_args()
+    
+    return args
+
+def main():
+    
+    args = parse_args()
+    
+    if not args.output_dir:
+        raise ValueError("--output_dir must be specified")
+    
     config = {
         "lr": args.lr, 
         "num_epochs": args.num_epochs, 
         "seed": 42, 
-        "batch_size": args.bs, 
+        "batch_size": args.batch_size_per_device, 
         "gradient_accumulation_steps": args.grad_accum,
-        "model_name": MODEL,
-        "block_size": BLOCK_SIZE,
-        "overlap": OVERLAP_LENGTH,
-        "eval_batch_size": EVAL_BATCH_SIZE,
+        "model_name": args.model_name,
+        "block_size": args.ctx_len,
+        "eval_batch_size": args.eval_batch_size_per_device,
     }
-    MODEL = args.model_name
-    EVAL_BATCH_SIZE = args.bs
-    os.environ["RAY_memory_monitor_refresh_ms"] = "0"
-    os.environ["TUNE_RESULT_DIR"] = args.output_dir
+    
     ray.init(
         runtime_env={
-            "pip": [
-                "einops",
-                "torch>=2.0",
-                "evaluate",
-                "datasets==2.13.1",
-                "accelerate>=0.20.2"
-            ],
             "env_vars": {
-                "RAY_memory_monitor_refresh_ms": "0",
                 "HF_HOME": "/mnt/local_storage/.cache/huggingface",
+                "TUNE_RESULT_DIR": args.output_dir,
             }
         }
     )
     
-    from ray.train.torch import TorchConfig
     
-    train_ds = get_ds(args.train_path)
+    train_ds = create_ray_dataset(args.train_path)
     if args.test_path is not None:
-        valid_ds = get_ds(args.test_path)
+        valid_ds = create_ray_dataset(args.test_path)
     else:
         valid_ds = None
-    
     
     # json file
     with open(args.special_token_path, 'r') as json_file:
@@ -418,10 +505,13 @@ def main():
     
     trainer = TorchTrainer(
         training_function,
-        train_loop_config={"config": config, "args": vars(args), "special_tokens": special_tokens},
-        # accelerate_config={}, # set to None to grab the default config
+        train_loop_config={
+            "config": config, 
+            "args": vars(args), 
+            "special_tokens": special_tokens
+        },
         scaling_config=air.ScalingConfig(
-            num_workers=32, 
+            num_workers=args.num_devices, 
             use_gpu=True,  
             resources_per_worker={"GPU": 1},
         ),
