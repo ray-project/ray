620f802352eee7a3b787c49ad654944de117433f
Ricky Xu <xuchen727@hotmail.com>
2023-07-22 00:35:45 -0700

    [core][autoscaler] Fix env variable overwrite not able to be used if command itself uses the env (#37675)
    ---------
    
    Signed-off-by: rickyyx <rickyx@anyscale.com>
    Co-authored-by: scv119 <scv119@gmail.com>
    Signed-off-by: Victor <vctr.y.m@example.com>

--- python/ray/autoscaler/_private/util.py
+++ python/ray/autoscaler/_private/util.py
@@ -395,13 +395,15 @@ def with_envs(cmds: List[str], kv: Dict[str, str]) -> str:
 
     Example:
         with_envs(["echo $FOO"], {"FOO": "BAR"})
-            -> ["FOO=BAR echo $FOO"]
+            -> ["export FOO=BAR; echo $FOO"]
     """
     out_cmds = []
     for cmd in cmds:
         kv_str = ""
         for k, v in kv.items():
-            kv_str += f"{k}={v} "
+            # We will need to do export here so that it works correctly with
+            # shell if the cmd args uses the argument.
+            kv_str += f"export {k}={v}; "
 
         out_cmds.append(f"{kv_str}{cmd}")
     return out_cmds
