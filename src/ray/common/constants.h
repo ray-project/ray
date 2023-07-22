e6775b7be3a36e7b8658a58628fd24eaad1b69c4
rickyyx <rickyx@anyscale.com>
2023-07-22 15:49:30 +0000

    Update version to 2.6.1
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- src/ray/common/constants.h
+++ src/ray/common/constants.h
@@ -61,7 +61,7 @@ constexpr int kMessagePackOffset = 9;
 constexpr char kSetupWorkerFilename[] = "setup_worker.py";
 
 /// The version of Ray
-constexpr char kRayVersion[] = "2.6.0";
+constexpr char kRayVersion[] = "2.6.1";
 
 /*****************************/
 /* ENV labels for autoscaler */
