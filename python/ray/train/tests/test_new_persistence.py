9c5ad3dd136044d96bf8a38a543ff5884b5fdc70
Justin Yu <justinvyu@anyscale.com>
2023-07-23 12:46:44 -0700

    Add e2e test
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

---
+++ python/ray/train/tests/test_new_persistence.py
@@ -0,0 +1,112 @@
+from contextlib import contextmanager
+import os
+import pickle
+import pytest
+import tempfile
+import time
+
+from ray import air, train, tune
+from ray.air.tests.test_checkpoints import mock_s3_bucket_uri
+from ray.air._internal.remote_storage import download_from_uri
+
+
+@contextmanager
+def dummy_context_manager():
+    yield "dummy value"
+
+
+def train_fn(config):
+    start = 0
+
+    checkpoint = train.get_context().get_checkpoint()
+    if checkpoint:
+        with checkpoint.as_directory() as checkpoint_dir:
+            with open(os.path.join(checkpoint_dir, "dummy.pkl"), "rb") as f:
+                state = pickle.load(f)
+        print("Loaded back state from checkpoint:", state)
+        start = state["iter"] + 1
+
+    for i in range(start, config.get("num_iterations", 5)):
+        time.sleep(0.5)
+
+        with open(f"artifact-{i}.txt", "w") as f:
+            f.write(f"{i}")
+
+        temp_dir = tempfile.mkdtemp()
+        with open(os.path.join(temp_dir, "dummy.pkl"), "wb") as f:
+            pickle.dump({"iter": i}, f)
+
+        train.report({"iter": i}, checkpoint=air.Checkpoint.from_directory(temp_dir))
+
+
+@pytest.mark.parametrize("storage_path_type", [None, "nfs", "cloud", "custom_fs"])
+def test_tuner(monkeypatch, storage_path_type, tmp_path):
+    # Set the cache dir to some temp directory
+    LOCAL_CACHE_DIR = tmp_path / "ray_results"
+    monkeypatch.setenv("RAY_AIR_LOCAL_CACHE_DIR", str(LOCAL_CACHE_DIR))
+
+    context_manager = (
+        mock_s3_bucket_uri if storage_path_type == "cloud" else dummy_context_manager
+    )
+
+    exp_name = "simple_persistence_test"
+
+    with context_manager() as cloud_storage_path:
+        storage_filesystem = None
+        if storage_path_type is None:
+            storage_path = None
+        elif storage_path_type == "nfs":
+            storage_path = str(tmp_path / "fake_nfs")
+        elif storage_path_type == "cloud":
+            storage_path = str(cloud_storage_path)
+        elif storage_path_type == "custom_fs":
+            from fsspec.implementations.dirfs import DirFileSystem
+            from fsspec.implementations.local import LocalFileSystem
+
+            import pyarrow.fs
+
+            storage_path = "mock_bucket"
+            storage_filesystem = pyarrow.fs.PyFileSystem(
+                pyarrow.fs.FSSpecHandler(
+                    DirFileSystem(
+                        path=str(tmp_path / "custom_fs"), fs=LocalFileSystem()
+                    )
+                )
+            )
+
+        NUM_ITERATIONS = 6  # == num_checkpoints == num_artifacts
+        NUM_TRIALS = 2
+        tuner = tune.Tuner(
+            train_fn,
+            param_space={"num_iterations": NUM_ITERATIONS},
+            run_config=train.RunConfig(
+                storage_path=storage_path,
+                storage_filesystem=storage_filesystem,
+                name="simple_persistence_test",
+                verbose=0,
+                failure_config=train.FailureConfig(max_failures=1),
+            ),
+            # 2 samples, running 1 at at time to test with actor reuse
+            tune_config=tune.TuneConfig(
+                num_samples=NUM_TRIALS, max_concurrent_trials=1
+            ),
+        )
+        tuner.fit()
+
+        local_inspect_dir = tmp_path / "inspect"
+        if storage_path:
+            download_from_uri(
+                storage_path,
+                str(local_inspect_dir),
+                source_filesystem=storage_filesystem,
+            )
+        else:
+            local_inspect_dir = LOCAL_CACHE_DIR
+
+    assert len(list(local_inspect_dir.glob("*"))) == 1  # Only expect 1 experiment dir
+    exp_dir = local_inspect_dir / exp_name
+
+    # Files synced by the driver
+    assert len(list(exp_dir.glob("basic-variant-state-*"))) == 1
+    assert len(list(exp_dir.glob("experiment_state-*"))) == 1
+    assert len(list(exp_dir.glob("tuner.pkl"))) == 1
