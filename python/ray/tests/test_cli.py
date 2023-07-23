f7f51f8ce333031da82cc2a539a43d175b546fec
Viswanath Chadalapaka <viswanath.chadalapaka@gmail.com>
2023-07-23 11:34:53 -0700

    fixed tests
    Signed-off-by: Viswanath Chadalapaka <viswanath.chadalapaka@gmail.com>

--- python/ray/tests/test_cli.py
+++ python/ray/tests/test_cli.py
@@ -377,7 +377,7 @@ def test_ray_stop_all_clusters(configure_lang, monkeypatch, cleanup_ray):
     # Start 2 ray clusters
     port0 = 0
     port1 = 1
-    result_start0 = runner.invoke(
+    runner.invoke(
         scripts.start,
         [
             "--head",
@@ -388,7 +388,7 @@ def test_ray_stop_all_clusters(configure_lang, monkeypatch, cleanup_ray):
             str(port0),
         ],
     )
-    result_start1 = runner.invoke(
+    runner.invoke(
         scripts.start,
         [
             "--head",
@@ -399,17 +399,14 @@ def test_ray_stop_all_clusters(configure_lang, monkeypatch, cleanup_ray):
             str(port1),
         ],
     )
-    print("T1 RESULT0 START OUTPUT", result_start0.output)
-    print("T1 RESULT1 START OUTPUT", result_start1.output)
     assert len(services.find_gcs_addresses()) == 2
 
     # Ensure that ray stop with no address stops both clusters
-    result_stop = runner.invoke(
+    runner.invoke(
         scripts.stop,
         [],
     )
-    print("T1 RESULT STOP OUTPUT", result_stop.output)
-    assert len(services.find_gcs_addresses()) == 2
+    assert len(services.find_gcs_addresses()) == 0
     _die_on_error(runner.invoke(scripts.stop))
 
 
@@ -423,7 +420,7 @@ def test_ray_stop_single_cluster(configure_lang, monkeypatch, cleanup_ray):
     # Start 2 ray clusters
     port_cluster_0 = 0
     port_cluster_1 = 1
-    result_start0 = runner.invoke(
+    runner.invoke(
         scripts.start,
         [
             "--head",
@@ -434,7 +431,7 @@ def test_ray_stop_single_cluster(configure_lang, monkeypatch, cleanup_ray):
             str(port_cluster_0),
         ],
     )
-    result_start1 = runner.invoke(
+    runner.invoke(
         scripts.start,
         [
             "--head",
@@ -445,21 +442,18 @@ def test_ray_stop_single_cluster(configure_lang, monkeypatch, cleanup_ray):
             str(port_cluster_1),
         ],
     )
-    print("T2 RESULT0 START OUTPUT", result_start0.output)
-    print("T2 RESULT1 START OUTPUT", result_start1.output)
     gcs_addresses = list(services.find_gcs_addresses())
     assert len(gcs_addresses) == 2
 
     # Stop one of the clusters and ensure that the second cluster is still up
     cluster_address_to_stop, cluster_address_to_keep = gcs_addresses
-    result_stop = runner.invoke(
+    runner.invoke(
         scripts.stop,
         [
             "--address",
             cluster_address_to_stop,
         ],
     )
-    print("T2 RESULT STOP OUTPUT", result_stop.output)
     gcs_addresses = services.find_gcs_addresses()
     assert len(gcs_addresses) == 1
     assert cluster_address_to_keep in gcs_addresses
