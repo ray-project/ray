c47572b3092bb23918f205eca70078c5b1d2042a
Zhi Lin <zhi.lin@intel.com>
2023-07-24 20:29:03 +0000

    comment added part and see if ci pass
    Signed-off-by: Zhi Lin <zhi.lin@intel.com>

--- python/ray/_private/serialization.py
+++ python/ray/_private/serialization.py
@@ -8,7 +8,7 @@ import ray._private.utils
 import ray.cloudpickle as pickle
 from ray._private import ray_constants
 from ray._raylet import (
-    ArrowSerializedObject,
+    # ArrowSerializedObject,
     MessagePackSerializedObject,
     MessagePackSerializer,
     ObjectRefGenerator,
@@ -272,14 +272,14 @@ class SerializationContext:
                 if data is None:
                     return b""
                 return data.to_pybytes()
-            elif metadata_fields[0] == ray_constants.OBJECT_METADATA_TYPE_ARROW:
-                try:
-                    import pyarrow
-                except ImportError:
-                    pyarrow = None
+            # elif metadata_fields[0] == ray_constants.OBJECT_METADATA_TYPE_ARROW:
+            #     try:
+            #         import pyarrow
+            #     except ImportError:
+            #         pyarrow = None
 
-                reader = pyarrow.BufferReader(data)
-                return pyarrow.ipc.open_stream(reader).read_all()
+            #     reader = pyarrow.BufferReader(data)
+            #     return pyarrow.ipc.open_stream(reader).read_all()
             elif metadata_fields[0] == ray_constants.OBJECT_METADATA_TYPE_ACTOR_HANDLE:
                 obj = self._deserialize_msgpack_data(data, metadata_fields)
                 return _actor_handle_deserializer(obj)
@@ -474,16 +474,16 @@ class SerializationContext:
             # that this object can also be read by Java.
             return RawSerializedObject(value)
 
-        try:
-            import pyarrow
-        except ImportError:
-            pyarrow = None
+        # try:
+        #     import pyarrow
+        # except ImportError:
+        #     pyarrow = None
 
-        # Check whether arrow is installed. If so, use Arrow IPC format
-        # to serialize this object, then it can also be read by Java.
-        if pyarrow is not None and (
-            isinstance(value, pyarrow.Table) or isinstance(value, pyarrow.RecordBatch)
-        ):
-            return ArrowSerializedObject(value)
+        # # Check whether arrow is installed. If so, use Arrow IPC format
+        # # to serialize this object, then it can also be read by Java.
+        # if pyarrow is not None and (
+        #     isinstance(value, pyarrow.Table) or isinstance(value, pyarrow.RecordBatch)
+        # ):
+        #     return ArrowSerializedObject(value)
 
         return self._serialize_to_msgpack(value)
