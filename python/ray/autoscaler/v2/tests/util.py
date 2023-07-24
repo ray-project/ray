d7f98943737e1cc2b661556ed24ccf6acd523387
scv119 <scv119@gmail.com>
2023-07-24 15:01:37 -0700

    lint

--- python/ray/autoscaler/v2/tests/util.py
+++ python/ray/autoscaler/v2/tests/util.py
@@ -9,7 +9,6 @@ def get_cluster_resource_state(stub) -> autoscaler_pb2.ClusterResourceState:
     request = autoscaler_pb2.GetClusterResourceStateRequest(
         last_seen_cluster_resource_state_version=0
     )
-    
     return stub.GetClusterResourceState(request).cluster_resource_state
 
 
