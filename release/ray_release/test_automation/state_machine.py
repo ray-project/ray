254c89b8dea2aeb142588c2e793495be93dc1173
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2023-07-25 15:18:40 -0700

    Reduce the time threshold to jail tests (#37788)
    Signed-off-by: can <can@anyscale.com>
    Signed-off-by: NripeshN <nn2012@hw.ac.uk>

--- release/ray_release/test_automation/state_machine.py
+++ release/ray_release/test_automation/state_machine.py
@@ -14,7 +14,7 @@ RAY_REPO = "ray-project/ray"
 AWS_SECRET_GITHUB = "ray_ci_github_token"
 AWS_SECRET_BUILDKITE = "ray_ci_buildkite_token"
 MAX_BISECT_PER_DAY = 10  # Max number of bisects to run per day for all tests
-CONTINUOUS_FAILURE_TO_JAIL = 5  # Number of continuous failures before jailing
+CONTINUOUS_FAILURE_TO_JAIL = 3  # Number of continuous failures before jailing
 BUILDKITE_ORGANIZATION = "ray-project"
 BUILDKITE_BISECT_PIPELINE = "release-tests-bisect"
 
