b20e9822666c7af7190009fb1b960c1cafb484ea
Chen Shen <scv119@gmail.com>
2023-07-24 21:10:05 -0700

    [autoscaler v2][5/n] introducing reconciler (#34985)
    Why are these changes needed?
    this is the stack of PRs to introduce new node_provider for autoscaler v2.
    
    Stack of PRs
    #34976
    #34977
    #34979
    #34983
    #34985 <- this PR
    
    Added a reconciler that reconciles the state from instance_storage to node_provider. Specifically, it subscribes to changes in the storage, and triggers following operations in sequence:
    
    launch new instances based on the queued requests
    install ray on newly allocated instances
    handle various ray failures on allocated instances (ray install failed, or ray stopped)
    reconcile the instance status with node provider.
    
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- python/ray/autoscaler/v2/tests/util.py
+++ python/ray/autoscaler/v2/tests/util.py
@@ -2,6 +2,7 @@ from typing import Dict, List
 
 from ray.autoscaler.v2.schema import ResourceUsage
 from ray.core.generated import autoscaler_pb2
+from ray.core.generated.instance_manager_pb2 import Instance
 
 
 def get_cluster_resource_state(stub) -> autoscaler_pb2.ClusterResourceState:
@@ -9,6 +10,28 @@ def get_cluster_resource_state(stub) -> autoscaler_pb2.ClusterResourceState:
         last_seen_cluster_resource_state_version=0
     )
     return stub.GetClusterResourceState(request).cluster_resource_state
+
+
+class FakeCounter:
+    def dec(self, *args, **kwargs):
+        pass
+
+
+def create_instance(
+    instance_id,
+    status=Instance.UNKNOWN,
+    version=0,
+    ray_status=Instance.RAY_STATUS_UNKOWN,
+    instance_type="worker_nodes1",
+):
+    return Instance(
+        instance_id=instance_id,
+        status=status,
+        version=version,
+        instance_type=instance_type,
+        ray_status=ray_status,
+        timestamp_since_last_modified=1,
+    )
 
 
 def report_autoscaling_state(stub, autoscaling_state: autoscaler_pb2.AutoscalingState):
