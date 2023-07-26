77b412e4288bcfcafe8ed5b303329d55c738faec
Ruiyang Wang <rywang014@gmail.com>
2023-07-25 17:16:09 -0700

    lint
    Signed-off-by: Ruiyang Wang <rywang014@gmail.com>

--- python/ray/util/state/state_manager.py
+++ python/ray/util/state/state_manager.py
@@ -226,7 +226,7 @@ class StateDataSourceClient:
     # Returns all node_ids who has runtime_env_agent listening.
     def get_all_registered_runtime_env_agent_ids(self) -> List[str]:
         return self._runtime_env_agent_addresses.keys()
-    
+
     # Returns all nod_ids which registered their log_agent_stub.
     def get_all_registered_log_agent_ids(self) -> List[str]:
         return self._log_agent_stub.keys()
