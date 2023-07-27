53d5514609ddc9795224dbfa2a20419ca19b61a0
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2023-07-26 19:27:58 -0700

    added other shells
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- doc/source/templates/04_finetuning_llms_with_deepspeed/run_llama_70b.sh
+++ doc/source/templates/04_finetuning_llms_with_deepspeed/run_llama_70b.sh
@@ -9,18 +9,33 @@ TRAIN_PATH="${DATA_DIR}/train.jsonl"
 TEST_PATH="${DATA_DIR}/test.jsonl"
 TOKEN_PATH="${DATA_DIR}/tokens.json"
 
+# Make sure to exit when any command fails
+set -e
+
 # Setup AWS
 echo "Setting up AWS..."
-./setup_aws.sh
+chmod +x ./setup_aws.sh && ./setup_aws.sh
+if [ $? -ne 0 ]; then
+    echo "Failed to setup AWS. Exiting..."
+    exit 1
+fi
 
 # Prepare nodes
 echo "Preparing nodes..."
 python prepare_nodes.py --hf-model-id ${MODEL_ID}
+if [ $? -ne 0 ]; then
+    echo "Failed to prepare nodes. Exiting..."
+    exit 1
+fi
 
 # Check if data directory exists, if not, run create_dataset.py
 if [ ! -d "${DATA_DIR}" ]; then
     echo "Data directory not found. Creating dataset..."
     python create_dataset.py
+    if [ $? -ne 0 ]; then
+        echo "Failed to create dataset. Exiting..."
+        exit 1
+    fi
 fi
 
 # Check if --as-test was passed as an argument
@@ -40,5 +55,10 @@ python finetune_hf_llm.py \
     --test_path ${TEST_PATH}  \
     --special_token_path ${TOKEN_PATH} \
     $params
+
+if [ $? -ne 0 ]; then
+    echo "Failed to fine-tune the model. Exiting..."
+    exit 1
+fi
 
 echo "Process completed."
