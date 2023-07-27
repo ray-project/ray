298fef53df6f4d3c25894965e69dabc0dc5f42ec
Gene Der Su <e870252314@gmail.com>
2023-07-27 08:22:33 -0700

    [Serve] Graceful shutdown `MetricsPusher` (#37715)
    Re: https://github.com/ray-project/ray/pull/37109 Seems like simply setting `metrics_pusher` to `None` might not get the object to garbage collected before ray is shutdown. Explicitly call `__del__()` on all `MetricsPusher` instances and drop their references to ensure graceful shutdown.
    Signed-off-by: NripeshN <nn2012@hw.ac.uk>

--- python/ray/serve/_private/replica.py
+++ python/ray/serve/_private/replica.py
@@ -518,7 +518,7 @@ class RayServeReplica:
 
         self.restart_counter.inc()
 
-        self.metrics_pusher = self.metrics_pusher = MetricsPusher()
+        self.metrics_pusher = MetricsPusher()
         if autoscaling_config:
             process_remote_func = controller_handle.record_autoscaling_metrics.remote
             config = autoscaling_config
@@ -845,7 +845,8 @@ class RayServeReplica:
         # We set the del method to noop after successfully calling it so the
         # destructor is called only once.
         async with self.delete_lock:
-            self.metrics_pusher = None
+            if self.metrics_pusher:
+                self.metrics_pusher.shutdown()
 
             if not hasattr(self, "callable"):
                 return
