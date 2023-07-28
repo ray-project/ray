08fa17a6b6e8900726b6b8ed223cd12209711edb
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2023-07-28 22:44:38 +0000

    fix Signed-off-by: Yi Cheng <74173148+iycheng@users.noreply.github.com>

--- scale-down-test.py
+++ scale-down-test.py
@@ -1,9 +1,10 @@
-import ray
-from ray.cluster_utils import Cluster
-import psutil
+import os
 import subprocess
 
-import os
+import ray
+from ray.cluster_utils import Cluster
+
+import psutil
 
 cluster = Cluster()
 
@@ -19,9 +20,24 @@ n.remove(node1.node_manager_port)
 
 n = list(n)[0]
 
+
 @ray.remote(num_cpus=0, num_gpus=1, num_returns=2)
 def f():
     return ray.put(os.getpid()), ray.put(b"x" * 1024 * 1024)
+
+
+
+@ray.remote(num_cpus=0, num_gpus=1)
+class A:
+    def __init__(self):
+        self.data = ray.put(b"x" * 1024 * 1024)
+
+
+    def fetch(self):
+        return self.data
+
+
+a = A.remote()
 
 pid, obj = f.remote()
 
@@ -29,13 +45,19 @@ p = pid
 
 pid = ray.get(ray.get(pid))
 
-subprocess.run(f"grpcurl -use-reflection -plaintext localhost:{n} ray.rpc.NodeManagerService.DrainObjectStore", shell=True)
+a_data = a.fetch.remote()
+
+subprocess.run(
+    f"grpcurl -use-reflection -plaintext localhost:{n} ray.rpc.NodeManagerService.DrainObjectStore",
+    shell=True,
+)
 
 
 # proc = psutil.Process(pid)
 # proc.kill()
 
 cluster.remove_node(node2)
+
 
 @ray.remote(num_gpus=0, num_cpus=1)
 def g(o):
@@ -44,4 +66,6 @@ def g(o):
     ray.get(o)
     print("SUCC")
 
+
 print(ray.get(g.remote(obj)))
+print(ray.get(ray.get(a_data))[0:10])
