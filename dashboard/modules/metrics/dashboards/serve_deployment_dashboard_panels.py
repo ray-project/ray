ff21be6d2be6bc054e3954c7e044136a0fee22cb
Sihan Wang <sihanwang41@gmail.com>
2023-07-30 12:47:20 -0700

    Update
    Signed-off-by: Sihan Wang <sihanwang41@gmail.com>

--- dashboard/modules/metrics/dashboards/serve_deployment_dashboard_panels.py
+++ dashboard/modules/metrics/dashboards/serve_deployment_dashboard_panels.py
@@ -232,7 +232,7 @@ SERVE_DEPLOYMENT_GRAFANA_PANELS = [
         targets=[
             Target(
                 expr="ray_serve_registered_multiplexed_model_id{{{global_filters}}}",
-                legend="{{model_id}}",
+                legend="{{replica}}:{{model_id}}",
             ),
         ],
         grid_pos=GridPos(16, 4, 8, 8),
