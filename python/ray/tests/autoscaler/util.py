9d6d5e103e8187150cfa7af44bb425011405f041
chaowang <chaowang@anyscale.com>
2023-07-31 05:27:12 +0000

    feat
    Signed-off-by: chaowang <chaowang@anyscale.com>

--- python/ray/tests/autoscaler/util.py
+++ python/ray/tests/autoscaler/util.py
@@ -22,36 +22,9 @@ class TestGetPerNodeBreakdown(unittest.TestCase):
         result = get_per_node_breakdown_as_dict(self.lm_summary_mock)
 
         expected_output = {
-            "e9919752e5e8d757765d97d8bec910a2e78e8826f20bce46fd58f92e": "0.0/8.0 CPU\n0B/26.05GiB memory\n0B/13.02GiB object_store_memory"
-        }
-
-        self.assertEqual(result, expected_output)
-
-    def test_get_per_node_breakdown_as_dict_multiple_nodes(self):
-        # Test with multiple nodes in the lm_summary
-        lm_summary_mock_data = {
-            "e9919752e5e8d757765d97d8bec910a2e78e8826f20bce46fd58f92e": {
-                "node:172.31.6.57": [0.0, 1.0],
-                "object_store_memory": [0.0, 13984228147.0],
-                "memory": [0.0, 27968456295.0],
-                "node:__internal_head__": [0.0, 1.0],
-                "CPU": [0.0, 8.0],
-            },
-            "e9919752e5e8d757765d97d8bec910a2e78e8826f20bce46fd58f92f": {
-                "node:172.31.6.58": [0.0, 1.0],
-                "object_store_memory": [0.0, 13984228147.0],
-                "memory": [0.0, 27968456295.0],
-                "node:__internal_head__": [0.0, 1.0],
-                "CPU": [0.0, 8.0],
-            },
-        }
-        self.lm_summary_mock.usage_by_node = lm_summary_mock_data
-
-        result = get_per_node_breakdown_as_dict(self.lm_summary_mock)
-
-        expected_output = {
-            "e9919752e5e8d757765d97d8bec910a2e78e8826f20bce46fd58f92e": "0.0/8.0 CPU\n0B/26.05GiB memory\n0B/13.02GiB object_store_memory",
-            "e9919752e5e8d757765d97d8bec910a2e78e8826f20bce46fd58f92f": "0.0/8.0 CPU\n0B/26.05GiB memory\n0B/13.02GiB object_store_memory",
+            "e9919752e5e8d757765d97d8bec910a2e78e8826f20bce46fd58f92e": (
+                "0.0/8.0 CPU\n0B/26.05GiB memory\n0B/13.02GiB object_store_memory"
+            )
         }
 
         self.assertEqual(result, expected_output)
@@ -83,7 +56,8 @@ class TestGetPerNodeBreakdown(unittest.TestCase):
         result = get_per_node_breakdown_as_dict(self.lm_summary_mock)
 
         expected_output = {
-            "e9919752e5e8d757765d97d8bec910a2e78e8826f20bce46fd58f92e": "0.0/8.0 CPU\n0B/13.02GiB object_store_memory"
+            "e9919752e5e8d757765d97d8bec910a2e78e8826f20bce46fd58f92e": "0.0/8.0 CPU\n"
+            "0B/13.02GiB object_store_memory"
         }
 
         self.assertEqual(result, expected_output)
