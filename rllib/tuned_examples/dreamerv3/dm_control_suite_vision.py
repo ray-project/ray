5259c7e7906eca0e6f86af0530709e24c2c8bc2d
kourosh hakhamaneshi <31483498+kouroshHakha@users.noreply.github.com>
2023-08-02 15:23:54 -0700

    Revert "[RLlib] DreamerV3: Minor fixes and enhancements. (#37977)" (#38035)
    This reverts commit 48488f50de39a9d7ef51094b5c5eefabee3ba08c.
    
    Signed-off-by: Victor <vctr.y.m@example.com>

--- rllib/tuned_examples/dreamerv3/dm_control_suite_vision.py
+++ rllib/tuned_examples/dreamerv3/dm_control_suite_vision.py
@@ -7,39 +7,29 @@ https://arxiv.org/pdf/2301.04104v1.pdf
 D. Hafner, T. Lillicrap, M. Norouzi, J. Ba
 https://arxiv.org/pdf/2010.02193.pdf
 """
+from ray.rllib.algorithms.dreamerv3.dreamerv3 import DreamerV3Config
+
 # Run with:
 # python run_regression_tests.py --dir [this file] --env DMC/[task]/[domain]
 # e.g. --env=DMC/cartpole/swingup
-
-from ray.rllib.algorithms.dreamerv3.dreamerv3 import DreamerV3Config
-
-
-# Number of GPUs to run on.
-num_gpus = 1
 
 config = (
     DreamerV3Config()
     # Use image observations.
     .environment(env_config={"from_pixels": True})
     .resources(
-        num_learner_workers=0 if num_gpus == 1 else num_gpus,
-        num_gpus_per_learner_worker=1 if num_gpus else 0,
+        num_learner_workers=1,
+        num_gpus_per_learner_worker=1,
         num_cpus_for_local_worker=1,
     )
-    .rollouts(num_envs_per_worker=4 * (num_gpus or 1), remote_worker_envs=True)
-    .reporting(
-        metrics_num_episodes_for_smoothing=(num_gpus or 1),
-        report_images_and_videos=False,
-        report_dream_data=False,
-        report_individual_batch_item_stats=False,
-    )
+    .rollouts(num_envs_per_worker=4, remote_worker_envs=True)
     # See Appendix A.
     .training(
         model_size="S",
         training_ratio=512,
-        batch_size_B=16 * (num_gpus or 1),
         # TODO
         model={
+            "batch_size_B": 16,
             "batch_length_T": 64,
             "horizon_H": 15,
             "gamma": 0.997,
