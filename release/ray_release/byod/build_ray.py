7fae91bfc4f0e914788ebcaef116ce49519c4ce6
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2023-08-01 19:10:24 -0700

    [ci] Build and push docker image on release pr pipeline (#37981)
    Always build docker image for release branch pr
    
    Signed-off-by: can <can@anyscale.com>
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- release/ray_release/byod/build_ray.py
+++ release/ray_release/byod/build_ray.py
@@ -61,9 +61,8 @@ def _get_build(py_version: str) -> Dict[str, Any]:
         "pip install -q docker aws_requests_auth boto3",
         "./ci/env/env_info.sh",
         "python .buildkite/copy_files.py --destination docker_login",
-        "python ./ci/build/build-docker-images.py "
-        f"--py-versions {py_version} -T cpu -T cu118 "
-        "--build-type BUILDKITE --build-base --push-pr-build",
+        f"python ./ci/build/build-docker-images.py --py-versions {py_version} "
+        "-T cpu -T cu118 --build-type BUILDKITE --build-base",
     ]
     return {
         "label": py_version,
@@ -99,6 +98,7 @@ def _get_build(py_version: str) -> Dict[str, Any]:
                         "BUILDKITE_BUILD_ID",
                         "BUILDKITE_MESSAGE",
                         "BUILDKITE_BUILD_NUMBER",
+                        "BUILDKITE_PIPELINE_ID",
                     ],
                     "mount-checkout": "false",
                 },
