5d2e1c622e46b30ba51ce8eee8cb163e8b55f88b
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2023-08-02 19:00:31 -0700

    [civ2][flakiness/6] support run tests with multiple size (#37799)
    * create a script to run ci tests
    
    Signed-off-by: can <can@anyscale.com>
    
    * merge dag tests
    
    Signed-off-by: can <can@anyscale.com>
    
    * support tests with multiple size
    
    Signed-off-by: can <can@anyscale.com>
    
    * add flaky test jobs to civ02 (#37798)
    
    Signed-off-by: can <can@anyscale.com>
    
    ---------
    
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/runner.py
+++ ci/ray_ci/runner.py
@@ -26,12 +26,35 @@ bazel_workspace_dir = os.environ.get("BUILD_WORKSPACE_DIRECTORY", "")
     type=int,
     help=("Index of the concurrent shard to run."),
 )
-def main(targets: List[str], team: str, concurrency: int, shard: int) -> None:
+@click.option(
+    "--size",
+    default="small,medium,large",
+    type=str,
+    help=("Size of tests to run."),
+)
+@click.option(
+    "--run-flaky-tests",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help=("Run flaky tests."),
+)
+def main(
+    targets: List[str],
+    team: str,
+    concurrency: int,
+    shard: int,
+    size: str,
+    run_flaky_tests: bool,
+) -> None:
     if not bazel_workspace_dir:
         raise Exception("Please use `bazelisk run //ci/ray_ci`")
     os.chdir(bazel_workspace_dir)
 
-    test_targets = _get_test_targets(targets, team, concurrency, shard)
+    if run_flaky_tests:
+        test_targets = _get_flaky_test_targets(team)
+    else:
+        test_targets = _get_test_targets(targets, team, concurrency, shard, size)
     if not test_targets:
         logging.info("No tests to run")
         return
@@ -58,16 +81,14 @@ def _get_test_targets(
     team: str,
     concurrency: int,
     shard: int,
+    size: str,
     yaml_dir: Optional[str] = None,
 ) -> List[str]:
     """
     Get test targets to run for a particular shard
     """
-    if not yaml_dir:
-        yaml_dir = os.path.join(bazel_workspace_dir, "ci/ray_ci")
-
     return _chunk_into_n(
-        _get_all_test_targets(targets, team, yaml_dir=yaml_dir),
+        _get_all_test_targets(targets, team, size, yaml_dir=yaml_dir),
         concurrency,
     )[shard]
 
@@ -77,29 +98,52 @@ def _chunk_into_n(list: List[str], n: int):
     return [list[x * size : x * size + size] for x in range(n)]
 
 
-def _get_all_test_query(targets: List[str], team: str) -> str:
+def _get_all_test_query(targets: List[str], team: str, size: str) -> str:
+    """
+    Bazel query to get all test targets given a team and test size
+    """
     test_query = " union ".join([f"tests({target})" for target in targets])
     team_query = f"attr(tags, team:{team}, {test_query})"
     size_query = " union ".join(
-        [f"attr(size, {s}, {test_query})" for s in ["small", "medium"]]
+        [f"attr(size, {s}, {test_query})" for s in size.split(",")]
+    )
+    except_query = " union ".join(
+        [
+            f"attr(tags, {t}, {test_query})"
+            for t in ["debug_tests", "asan_tests", "ray_ha"]
+        ]
     )
 
-    return f"{team_query} intersect ({size_query})"
+    return f"({team_query} intersect ({size_query})) except ({except_query})"
 
 
-def _get_all_test_targets(targets: str, team: str, yaml_dir: str) -> List[str]:
+def _get_all_test_targets(
+    targets: str, team: str, size: str, yaml_dir: str
+) -> List[str]:
     """
     Get all test targets that are not flaky
     """
 
     test_targets = (
         subprocess.check_output(
-            ["bazel", "query", _get_all_test_query(targets, team)],
+            ["bazel", "query", _get_all_test_query(targets, team, size)],
         )
         .decode("utf-8")
         .split("\n")
     )
+    flaky_tests = _get_flaky_test_targets(team, yaml_dir)
+
+    return [test for test in test_targets if test and test not in flaky_tests]
+
+
+def _get_flaky_test_targets(team: str, yaml_dir: Optional[str] = None) -> List[str]:
+    """
+    Get all test targets that are flaky
+    """
+    if not yaml_dir:
+        yaml_dir = os.path.join(bazel_workspace_dir, "ci/ray_ci")
+
     with open(f"{yaml_dir}/{team}.tests.yml", "rb") as f:
         flaky_tests = yaml.safe_load(f)["flaky_tests"]
 
-    return [test for test in test_targets if test and test not in flaky_tests]
+    return flaky_tests
