641d3844663ae258e64a5d2d4efa413793d9b1f1
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2023-08-02 19:02:38 -0700

    [civ2][flakiness/5] migrate ray dag tests to civ2 (#37796)
    Migrate small/medium/large and DAG tests to using civ2/runner
    
    Signed-off-by: can <can@anyscale.com>
    Signed-off-by: NripeshN <nn2012@hw.ac.uk>

--- ci/ray_ci/runner.py
+++ ci/ray_ci/runner.py
@@ -12,8 +12,8 @@ bazel_workspace_dir = os.environ.get("BUILD_WORKSPACE_DIRECTORY", "")
 
 
 @click.command()
-@click.argument("targets", required=True, type=str)
-@click.argument("team", required=True, type=str)
+@click.argument("targets", required=True, type=str, nargs=-1)
+@click.argument("team", required=True, type=str, nargs=1)
 @click.option(
     "--concurrency",
     default=3,
@@ -26,13 +26,35 @@ bazel_workspace_dir = os.environ.get("BUILD_WORKSPACE_DIRECTORY", "")
     type=int,
     help=("Index of the concurrent shard to run."),
 )
-def main(targets: str, team: str, concurrency: int, shard: int) -> None:
+@click.option(
+    "--size",
+    default="small,medium,large",
+    type=str,
+    help=("Size of tests to run."),
+)
+@click.option(
+    "--run-flaky-tests",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help=("Run flaky tests."),
+)
+def main(
+    targets: List[str],
+    team: str,
+    concurrency: int,
+    shard: int,
+    size: str,
+    run_flaky_tests: bool,
+) -> None:
     if not bazel_workspace_dir:
         raise Exception("Please use `bazelisk run //ci/ray_ci`")
-
     os.chdir(bazel_workspace_dir)
 
-    test_targets = _get_test_targets(targets, team, concurrency, shard)
+    if run_flaky_tests:
+        test_targets = _get_flaky_test_targets(team)
+    else:
+        test_targets = _get_test_targets(targets, team, concurrency, shard, size)
     if not test_targets:
         logging.info("No tests to run")
         return
@@ -50,13 +72,7 @@ def _run_tests(test_targets: List[str]) -> None:
         .split()
     )
     subprocess.check_call(
-        [
-            "bazel",
-            "test",
-            "--config=ci",
-        ]
-        + bazel_options
-        + test_targets
+        ["bazel", "test", "--config=ci"] + bazel_options + test_targets
     )
 
 
@@ -65,16 +81,14 @@ def _get_test_targets(
     team: str,
     concurrency: int,
     shard: int,
+    size: str,
     yaml_dir: Optional[str] = None,
 ) -> List[str]:
     """
     Get test targets to run for a particular shard
     """
-    if not yaml_dir:
-        yaml_dir = os.path.join(bazel_workspace_dir, "ci/ray_ci")
-
     return _chunk_into_n(
-        _get_all_test_targets(targets, team, yaml_dir=yaml_dir),
+        _get_all_test_targets(targets, team, size, yaml_dir=yaml_dir),
         concurrency,
     )[shard]
 
@@ -84,28 +98,52 @@ def _chunk_into_n(list: List[str], n: int):
     return [list[x * size : x * size + size] for x in range(n)]
 
 
-def _get_all_test_targets(targets: str, team: str, yaml_dir: str) -> List[str]:
+def _get_all_test_query(targets: List[str], team: str, size: str) -> str:
+    """
+    Bazel query to get all test targets given a team and test size
+    """
+    test_query = " union ".join([f"tests({target})" for target in targets])
+    team_query = f"attr(tags, team:{team}, {test_query})"
+    size_query = " union ".join(
+        [f"attr(size, {s}, {test_query})" for s in size.split(",")]
+    )
+    except_query = " union ".join(
+        [
+            f"attr(tags, {t}, {test_query})"
+            for t in ["debug_tests", "asan_tests", "ray_ha"]
+        ]
+    )
+
+    return f"({team_query} intersect ({size_query})) except ({except_query})"
+
+
+def _get_all_test_targets(
+    targets: str, team: str, size: str, yaml_dir: str
+) -> List[str]:
     """
     Get all test targets that are not flaky
     """
 
     test_targets = (
         subprocess.check_output(
-            [
-                "bazel",
-                "query",
-                f"attr(tags, team:{team}, tests({targets})) intersect ("
-                # TODO(can): Remove this once we have a better way
-                # to filter out test size
-                f"attr(size, small, tests({targets})) union "
-                f"attr(size, medium, tests({targets}))"
-                ")",
-            ]
+            ["bazel", "query", _get_all_test_query(targets, team, size)],
         )
         .decode("utf-8")
         .split("\n")
     )
+    flaky_tests = _get_flaky_test_targets(team, yaml_dir)
+
+    return [test for test in test_targets if test and test not in flaky_tests]
+
+
+def _get_flaky_test_targets(team: str, yaml_dir: Optional[str] = None) -> List[str]:
+    """
+    Get all test targets that are flaky
+    """
+    if not yaml_dir:
+        yaml_dir = os.path.join(bazel_workspace_dir, "ci/ray_ci")
+
     with open(f"{yaml_dir}/{team}.tests.yml", "rb") as f:
         flaky_tests = yaml.safe_load(f)["flaky_tests"]
 
-    return [test for test in test_targets if test and test not in flaky_tests]
+    return flaky_tests
--- ci/ray_ci/test_runner.py
+++ ci/ray_ci/test_runner.py
@@ -8,7 +8,9 @@ import pytest
 
 from ci.ray_ci.runner import (
     _get_all_test_targets,
+    _get_all_test_query,
     _get_test_targets,
+    _get_flaky_test_targets,
     _run_tests,
     _chunk_into_n,
 )
@@ -48,20 +50,41 @@ def test_get_test_targets() -> None:
             "//python/ray/tests:flaky_test_01",
             "",
         ]
-        targets = "python/ray/tests"
         with mock.patch(
             "subprocess.check_output",
             return_value="\n".join(test_targets).encode("utf-8"),
         ):
-            assert _get_all_test_targets(targets, "core", yaml_dir=tmp) == [
+            assert _get_all_test_targets("targets", "core", "small", yaml_dir=tmp) == [
                 "//python/ray/tests:good_test_01",
                 "//python/ray/tests:good_test_02",
                 "//python/ray/tests:good_test_03",
             ]
-            assert _get_test_targets(targets, "core", 2, 0, yaml_dir=tmp) == [
+            assert _get_test_targets(
+                "targets", "core", 2, 0, "small", yaml_dir=tmp
+            ) == [
                 "//python/ray/tests:good_test_01",
                 "//python/ray/tests:good_test_02",
             ]
+
+
+def test_get_all_test_query() -> None:
+    assert _get_all_test_query(["a", "b"], "core", "small,medium") == (
+        "(attr(tags, team:core, tests(a) union tests(b)) intersect "
+        "(attr(size, small, tests(a) union tests(b)) union "
+        "attr(size, medium, tests(a) union tests(b)))) except "
+        "(attr(tags, debug_tests, tests(a) union tests(b)) union "
+        "attr(tags, asan_tests, tests(a) union tests(b)) union "
+        "attr(tags, ray_ha, tests(a) union tests(b)))"
+    )
+
+
+def test_get_flaky_test_targets() -> None:
+    _TEST_YAML = "flaky_tests: [//target]"
+
+    with TemporaryDirectory() as tmp:
+        with open(os.path.join(tmp, "core.tests.yml"), "w") as f:
+            f.write(_TEST_YAML)
+        assert _get_flaky_test_targets("core", yaml_dir=tmp) == ["//target"]
 
 
 if __name__ == "__main__":
