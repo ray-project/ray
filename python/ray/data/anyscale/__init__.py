fa170df3956f4591d617646c6827e12f4ce561eb
Balaji Veeramani <balaji@anyscale.com>
2023-08-03 19:00:10 -0500

    [Data] Add `AnyscaleMetadataProvider` (#33)
    This PR adds `AnyscaleMetadataProvider`. It performs sampling to more
    efficiently collect metadata.
    
    This code is ported over from
    https://github.com/anyscale/ray-extensions.

--- python/ray/data/anyscale/__init__.py
+++ python/ray/data/anyscale/__init__.py
@@ -1,3 +1,4 @@
+from ray.data.anyscale.anyscale_metadata_provider import AnyscaleFileMetadataProvider
 from ray.data.anyscale.video_datasource import VideoDatasource
 
-__all__ = ["VideoDatasource"]
+__all__ = ["AnyscaleFileMetadataProvider", "VideoDatasource"]
