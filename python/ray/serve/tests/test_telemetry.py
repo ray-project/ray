6cc77193cd49d78b4dab05d6cd7e46648089a55b
Sihan Wang <sihanwang41@gmail.com>
2023-08-06 12:46:11 -0700

    Update
    Signed-off-by: Sihan Wang <sihanwang41@gmail.com>

--- python/ray/serve/tests/test_telemetry.py
+++ python/ray/serve/tests/test_telemetry.py
@@ -117,7 +117,6 @@ def start_telemetry_app():
     return storage
 
 
-'''
 def test_fastapi_detected(manage_ray):
     """
     Check that FastAPI is detected by telemetry.
@@ -491,7 +490,6 @@ def test_lightweight_config_options(manage_ray, lightweight_option, value):
     for tagkey in lightweight_tagkeys:
         if not tagkey == f"serve_{lightweight_option}_lightweight_updated":
             assert tagkey not in report["extra_usage_tags"]
-'''
 
 
 @pytest.mark.parametrize("use_new_handle_api", [False, True])
