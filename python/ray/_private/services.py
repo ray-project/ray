ae709c3f126632cbb419010f774fd667ce7a63d8
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2023-08-07 16:25:10 -0400

    fix indent, add log_dir arg missing

--- python/ray/_private/services.py
+++ python/ray/_private/services.py
@@ -1963,6 +1963,7 @@ def build_julia_worker_command(
     raylet_name: str,
     redis_password: str,
     session_dir: str,
+    log_dir: str,
     node_ip_address: str,
     setup_worker_path: str,
 ):
@@ -1983,7 +1984,7 @@ def build_julia_worker_command(
     """
 
     # TODO: --project, --startup-file=no, --banner=no, ...?
-     command = [
+    command = [
         sys.executable,
         setup_worker_path,
         DEFAULT_WORKER_EXECUTABLE,
