d072ea387570dddaaa905deba7aa98e0755ac37d
vitsai <victoria@anyscale.com>
2023-08-07 10:45:00 -0700

    lint
    Signed-off-by: vitsai <victoria@anyscale.com>

--- python/ray/train/tests/test_gpu_2.py
+++ python/ray/train/tests/test_gpu_2.py
@@ -1,28 +1,15 @@
-import os
-import time
-
-from unittest.mock import patch
 import pytest
 import numpy as np
 import torch
-import torchvision
-from torch.nn.parallel import DistributedDataParallel
-from torch.utils.data import DataLoader, DistributedSampler
 
 import ray
 import ray.data
-from ray.exceptions import RayTaskError
-from ray.air import session
 from ray import tune
 
 import ray.train as train
 from ray.air.config import ScalingConfig
-from ray.train.constants import DEFAULT_NCCL_SOCKET_IFNAME
 from ray.train.examples.pytorch.torch_linear_example import LinearDataset
-from ray.train.torch.config import TorchConfig, _TorchBackend
 from ray.train.torch.torch_trainer import TorchTrainer
-from ray.train.trainer import TrainingFailedError
-from ray.train._internal.worker_group import WorkerGroup
 
 
 class LinearDatasetDict(LinearDataset):
