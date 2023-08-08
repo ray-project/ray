6aa2c155f973333895c205d035ed194fa4e7b206
Chao Wang <125417081+chaowanggg@users.noreply.github.com>
2023-08-08 14:59:40 -0700

    [Cluster] Add custom resources in node list (#37118)
    Frontend: Add a column Logical Resources in Cluster page, to display logical usage(CPU, Memory and Object Store) in order to help users to have a better understanding of the task scheduling info
    Backend: Add a field node_logical_resources in the existing api/cluster_status to get logical resources info for all nodes
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- dashboard/client/src/pages/node/NodeRow.tsx
+++ dashboard/client/src/pages/node/NodeRow.tsx
@@ -1,6 +1,8 @@
 import {
   Box,
+  createStyles,
   IconButton,
+  makeStyles,
   TableCell,
   TableRow,
   Tooltip,
@@ -10,6 +12,7 @@ import React, { useState } from "react";
 import { RiArrowDownSLine, RiArrowRightSLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import useSWR from "swr";
+import { CodeDialogButtonWithPreview } from "../../common/CodeDialogButton";
 import { API_REFRESH_INTERVAL_MS } from "../../common/constants";
 import { NodeLink } from "../../common/links";
 import rowStyles from "../../common/RowStyles";
@@ -35,6 +38,39 @@ type NodeRowProps = Pick<NodeRowsProps, "node"> & {
   onExpandButtonClick: () => void;
 };
 
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    tableContainer: {
+      overflowX: "scroll",
+    },
+    expandCollapseIcon: {
+      color: theme.palette.text.secondary,
+      fontSize: "1.5em",
+      verticalAlign: "middle",
+    },
+    idCol: {
+      display: "block",
+      width: "50px",
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      whiteSpace: "nowrap",
+    },
+    OverflowCol: {
+      display: "block",
+      width: "100px",
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      whiteSpace: "nowrap",
+    },
+    helpInfo: {
+      marginLeft: theme.spacing(1),
+    },
+    logicalResources: {
+      maxWidth: 200,
+    },
+  }),
+);
+
 /**
  * A single row that represents the node information only.
  * Does not show any data about the node's workers.
@@ -53,9 +89,10 @@ export const NodeRow = ({
     networkSpeed = [0, 0],
     raylet,
     logUrl,
+    logicalResources,
   } = node;
 
-  const classes = rowStyles();
+  const classes = useStyles();
 
   const objectStoreTotalMemory =
     raylet.objectStoreAvailableMemory + raylet.objectStoreUsedMemory;
@@ -149,6 +186,17 @@ export const NodeRow = ({
       </TableCell>
       <TableCell align="center">{memoryConverter(networkSpeed[0])}/s</TableCell>
       <TableCell align="center">{memoryConverter(networkSpeed[1])}/s</TableCell>
+      <TableCell align="center">
+        {logicalResources ? (
+          <CodeDialogButtonWithPreview
+            className={classes.logicalResources}
+            title="Logical Resources"
+            code={logicalResources}
+          />
+        ) : (
+          "-"
+        )}
+      </TableCell>
     </TableRow>
   );
 };
@@ -247,6 +295,7 @@ export const WorkerRow = ({ node, worker }: WorkerRowProps) => {
       </TableCell>
       <TableCell>N/A</TableCell>
       <TableCell>N/A</TableCell>
+      <TableCell align="center">N/A</TableCell>
       <TableCell align="center">N/A</TableCell>
       <TableCell align="center">N/A</TableCell>
     </TableRow>
