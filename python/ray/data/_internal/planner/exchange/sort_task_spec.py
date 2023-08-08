d44ba5df79d3b4c43594121009c4d72109441080
Anton Kukushkin <kukushkin.anton@gmail.com>
2023-08-08 17:28:26 +0100

    Comments
    Signed-off-by: Anton Kukushkin <kukushkin.anton@gmail.com>

--- python/ray/data/_internal/planner/exchange/sort_task_spec.py
+++ python/ray/data/_internal/planner/exchange/sort_task_spec.py
@@ -79,6 +79,7 @@ class SortTaskSpec(ExchangeTaskSpec):
         """
         Return (num_reducers - 1) items in ascending order from the blocks that
         partition the domain into ranges with approximately equally many elements.
+        Each boundary item is a tuple of a form (col1_value, col2_value, ...).
         """
         columns = sort_key.get_columns()
         n_samples = int(num_reducers * 10 / len(blocks))
@@ -107,7 +108,7 @@ class SortTaskSpec(ExchangeTaskSpec):
         # Compute sorted indices of the samples. In np.lexsort last key is the
         # primary key hence have to reverse the order.
         indices = np.lexsort(list(reversed(sample_dict.values())))
-        # Sort each column by indices, and calculate q-ths quantile items
+        # Sort each column by indices, and calculate q-ths quantile items.
         # Ignore the 1st item as it's not required for the boundary
         for k, v in sample_dict.items():
             sorted_v = v[indices]
@@ -115,7 +116,8 @@ class SortTaskSpec(ExchangeTaskSpec):
                 np.quantile(sorted_v, q, interpolation="nearest")
                 for q in np.linspace(0, 1, num_reducers)
             ][1:]
-        # Return tuples of sample values
+        # Return the list of boundaries as tuples
+        # of a form (col1_value, col2_value, ...)
         return [
             tuple(sample_dict[k][i] for k in sample_dict)
             for i in range(num_reducers - 1)
