1c8a9f5c8cf7d53fe22460204c715d2f151e0b93
Balaji Veeramani <balaji@anyscale.com>
2023-08-08 16:00:15 -0700

    Revert "Merge branch 'binary-datasource-cleanup' of https://github.com/bveeramani/ray into binary-datasource-cleanup"
    This reverts commit 963a7dc4585f890a707524e6230081f8fe119cf6, reversing
    changes made to ca15303810c27da970eb8415fa1b5e1d2b9b82a3.
    
    Signed-off-by: Balaji Veeramani <balaji@anyscale.com>

--- python/ray/data/datasource/binary_datasource.py
+++ python/ray/data/datasource/binary_datasource.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 from typing import TYPE_CHECKING
 
-from ray.data._internal.delegating_block_builder import DelegatingBlockBuilder
+from ray.data._internal.arrow_block import ArrowBlockBuilder
 from ray.data.datasource.file_based_datasource import FileBasedDatasource
 from ray.util.annotations import PublicAPI
 
@@ -43,14 +43,20 @@ class BinaryDatasource(FileBasedDatasource):
         else:
             data = f.readall()
 
-        if include_paths:
-            item = {self._COLUMN_NAME: data, "path": path}
+        output_arrow_format = reader_args.pop("output_arrow_format", False)
+        if output_arrow_format:
+            builder = ArrowBlockBuilder()
+            if include_paths:
+                item = {self._COLUMN_NAME: data, "path": path}
+            else:
+                item = {self._COLUMN_NAME: data}
+            builder.add(item)
+            return builder.build()
         else:
-            item = {self._COLUMN_NAME: data}
-
-        block_builder = DelegatingBlockBuilder()
-        block_builder.add(item)
-        return block_builder.build()
+            if include_paths:
+                return [(path, data)]
+            else:
+                return [data]
 
     def _rows_per_file(self):
         return 1
