8380dd3c7ab9a1e176408c6edcffdbc44579d138
Justin Yu <justinvyu@anyscale.com>
2023-08-08 23:15:19 -0700

    test_result [NOT WORKING]
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/air/tests/test_result.py
+++ python/ray/air/tests/test_result.py
@@ -6,9 +6,11 @@ import ray
 from ray.air.constants import EXPR_RESULT_FILE
 from ray import train
 from ray.train import Result, CheckpointConfig, RunConfig, ScalingConfig
-from ray.train.torch import TorchCheckpoint, TorchTrainer
+from ray.train.torch import TorchTrainer
 from ray.train.base_trainer import TrainingFailedError
 from ray.tune import TuneConfig, Tuner
+
+from ray.train.tests.util import create_dict_checkpoint, load_dict_checkpoint
 
 
 @pytest.fixture
@@ -22,10 +24,10 @@ def ray_start_4_cpus():
 def build_dummy_trainer(configs):
     def worker_loop():
         for i in range(configs["NUM_ITERATIONS"]):
-            train.report(
-                metrics={"metric_a": i, "metric_b": -i},
-                checkpoint=TorchCheckpoint.from_dict({"iter": i}),
-            )
+            with create_dict_checkpoint({"iter": i}) as checkpoint:
+                train.report(
+                    metrics={"metric_a": i, "metric_b": -i}, checkpoint=checkpoint
+                )
         raise RuntimeError()
 
     trainer = TorchTrainer(
@@ -96,10 +98,10 @@ def test_result_restore(ray_start_4_cpus, tmpdir, mode):
     """
     # Check if the checkpoints bounded with correct metrics
     best_ckpt_a = result.get_best_checkpoint(metric="metric_a", mode="max")
-    assert best_ckpt_a.to_dict()["iter"] == NUM_ITERATIONS - 1
+    assert load_dict_checkpoint(best_ckpt_a)["iter"] == NUM_ITERATIONS - 1
 
     best_ckpt_b = result.get_best_checkpoint(metric="metric_b", mode="max")
-    assert best_ckpt_b.to_dict()["iter"] == NUM_ITERATIONS - NUM_CHECKPOINTS
+    assert load_dict_checkpoint(best_ckpt_b)["iter"] == NUM_ITERATIONS - NUM_CHECKPOINTS
 
     with pytest.raises(RuntimeError, match="Invalid metric name.*"):
         result.get_best_checkpoint(metric="invalid_metric", mode="max")
@@ -116,10 +118,10 @@ def test_result_restore(ray_start_4_cpus, tmpdir, mode):
     assert len(result.best_checkpoints) == NUM_CHECKPOINTS
 
     best_ckpt_a = result.get_best_checkpoint(metric="metric_a", mode="max")
-    assert best_ckpt_a.to_dict()["iter"] == NUM_ITERATIONS - 1
+    assert load_dict_checkpoint(best_ckpt_a)["iter"] == NUM_ITERATIONS - 1
 
     best_ckpt_b = result.get_best_checkpoint(metric="metric_b", mode="max")
-    assert best_ckpt_b.to_dict()["iter"] == NUM_ITERATIONS - NUM_CHECKPOINTS
+    assert load_dict_checkpoint(best_ckpt_b)["iter"] == NUM_ITERATIONS - NUM_CHECKPOINTS
 
     assert isinstance(result.error, RuntimeError)
 
