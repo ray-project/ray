1dd1449f731ab0641c06445cf95f183c3d62fa25
Sihan Wang <sihanwang41@gmail.com>
2023-08-10 09:28:27 -0700

    [Serve] Speed up test_replica_scheduler
    Signed-off-by: Sihan Wang <sihanwang41@gmail.com>

--- python/ray/serve/tests/test_replica_scheduler.py
+++ python/ray/serve/tests/test_replica_scheduler.py
@@ -84,7 +84,7 @@ def pow_2_scheduler() -> PowerOfTwoChoicesReplicaScheduler:
 
     # Update the RAY_SERVE_MULTIPLEXED_MODEL_ID_MATCHING_TIMEOUT_S
     # to 0.1s to speed up the test.
-    os.environ.update({"RAY_SERVE_MULTIPLEXED_MODEL_ID_MATCHING_TIMEOUT_S": "0.1"})
+    os.environ.update({"RAY_SERVE_MULTIPLEXED_MODEL_ID_MATCHING_TIMEOUT_S": "0.01"})
     importlib.reload(ray.serve._private.constants)
     importlib.reload(ray.serve._private.router)
 
