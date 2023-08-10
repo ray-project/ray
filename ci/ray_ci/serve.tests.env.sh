460ab202f4ff6acbf36ec0d99b8f45b260cc37b8
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2023-08-10 12:29:28 -0700

    [civ2] migrate serve over to civ02 (#38044)
    Migrate all normal (non-feature flag) version of serve tests to civ02
    
    Signed-off-by: can <can@anyscale.com>
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- ci/ray_ci/serve.tests.env.sh
+++ ci/ray_ci/serve.tests.env.sh
@@ -3,4 +3,15 @@
 
 set -exo pipefail
 
-true
+TORCH_VERSION=1.9.0 ./ci/env/install-dependencies.sh
+
+# The docker is only used in the ha test and it uses nightly. We will
+# skip that test because it is not hermetic.
+# bash ./ci/ci.sh prepare_docker
+
+if [[ ! -f /usr/local/bin/wrk ]]; then
+	git clone https://github.com/wg/wrk.git /tmp/wrk
+	make -j -C /tmp/wrk
+	cp /tmp/wrk/wrk /usr/local/bin/.
+	rm -rf /tmp/wrk
+fi
