aaa29e2b0d4d8d6c029505cbc774546542c6c3ad
Eric Liang <ekhliang@gmail.com>
2023-08-10 07:39:51 -0700

    [serve] Allow pluggable router classes via handle config (experimental) (#38270)
    Add a way to configure the router via the handle config. This can be used to implement custom routing strategies. It may be better in the long term to make the routing configured via the deployment decorator.
    
    Signed-off-by: Victor <vctr.y.m@example.com>

--- python/ray/_private/utils.py
+++ python/ray/_private/utils.py
@@ -1987,3 +1987,17 @@ def pasre_pg_formatted_resources_to_original(
         original_resources[key] = value
 
     return original_resources
+
+
+def load_class(path):
+    """Load a class at runtime given a full path.
+
+    Example of the path: mypkg.mysubpkg.myclass
+    """
+    class_data = path.split(".")
+    if len(class_data) < 2:
+        raise ValueError("You need to pass a valid path like mymodule.provider_class")
+    module_path = ".".join(class_data[:-1])
+    class_str = class_data[-1]
+    module = importlib.import_module(module_path)
+    return getattr(module, class_str)
