bc761b54a59b447c1b56e7dbcc9b719caa8f003c
kourosh hakhamaneshi <31483498+kouroshHakha@users.noreply.github.com>
2023-08-11 15:26:13 -0700

    [Train] Fix a few reported issues (awsv2, s3fs) on Llama-2 ws (#38369)
    Signed-off-by: Kourosh <kourosh@anyscale.com>
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- doc/source/templates/04_finetuning_llms_with_deepspeed/utils.py
+++ doc/source/templates/04_finetuning_llms_with_deepspeed/utils.py
@@ -12,7 +12,7 @@ def get_hash_from_bucket(
 
     s3_sync_args = s3_sync_args or []
     subprocess.run(
-        ["aws", "s3", "cp", "--quiet"]
+        ["awsv2", "s3", "cp", "--quiet"]
         + s3_sync_args
         + [os.path.join(bucket_uri, "refs", "main"), "."]
     )
@@ -67,7 +67,7 @@ def download_model(
     path = os.path.join(TRANSFORMERS_CACHE, f"models--{model_id.replace('/', '--')}")
 
     cmd = (
-        ["aws", "s3", "sync"]
+        ["awsv2", "s3", "sync"]
         + s3_sync_args
         + (["--exclude", "*", "--include", "*token*"] if tokenizer_only else [])
         + [bucket_uri, path]
