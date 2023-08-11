c178f4baa1bdc7bd419bc6a5f4abe1e598c38292
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-08-11 14:54:21 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/tests/test_request_timeout.py
+++ python/ray/serve/tests/test_request_timeout.py
@@ -259,7 +259,7 @@ def test_streaming_request_already_sent_and_timed_out(ray_instance, shutdown_ser
 @pytest.mark.parametrize(
     "ray_instance",
     [
-        {"RAY_SERVE_REQUEST_PROCESSING_TIMEOUT_S": "0.1"},
+        {"RAY_SERVE_REQUEST_PROCESSING_TIMEOUT_S": "0.5"},
     ],
     indirect=True,
 )
