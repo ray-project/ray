3d69d159b25eaa9f75314dc32ba9f333aead74cd
Yunxuan Xiao <yunxuanx@anyscale.com>
2023-08-11 18:33:24 -0700

    [Unified TorchTrainer] Add HF Transformers TorchTrainer Utilities (#38083)
    Signed-off-by: woshiyyya <xiaoyunxuan1998@gmail.com>
    Signed-off-by: Yunxuan Xiao <xiaoyunxuan1998@gmail.com>
    Co-authored-by: matthewdeng <matthew.j.deng@gmail.com>

--- python/ray/train/huggingface/transformers/_transformers_utils.py
+++ python/ray/train/huggingface/transformers/_transformers_utils.py
@@ -1,24 +1,38 @@
+import os
 import logging
+import shutil
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, Iterator, Optional, Tuple, Type
+from typing import Any, Iterator, Optional, Tuple, Type
+from tempfile import TemporaryDirectory
 
-import datasets.iterable_dataset
-import transformers.trainer
-from transformers.trainer_callback import TrainerCallback
-from transformers.trainer_utils import IntervalStrategy
+from torch.utils.data import DataLoader, Dataset, IterableDataset
 
+import ray
 from ray.air import session
 from ray.data import DataIterator
+from ray.data.iterator import _IterableFromIterator
 from ray.data.dataset import MaterializedDataset
 from ray.data._internal.iterator.stream_split_iterator import StreamSplitDataIterator
+from ray.train import Checkpoint
 from ray.train.huggingface.transformers.transformers_checkpoint import (
     TransformersCheckpoint,
 )
-
-if TYPE_CHECKING:
-    from torch.utils.data import IterableDataset
+from ray.util import PublicAPI
 
 logger = logging.getLogger(__name__)
+
+
+TRANSFORMERS_IMPORT_ERROR: Optional[ImportError] = None
+
+try:
+    import datasets.iterable_dataset
+    import transformers.trainer
+    from transformers import Trainer
+    from transformers.trainer_callback import TrainerCallback
+    from transformers.trainer_utils import IntervalStrategy
+except ImportError as e:
+    TRANSFORMERS_IMPORT_ERROR = e
+    TrainerCallback = object
 
 
 def maybe_add_length(obj: Any, length: Optional[int]) -> Any:
@@ -40,9 +54,7 @@ def maybe_add_length(obj: Any, length: Optional[int]) -> Any:
     return obj
 
 
-def wrap_transformers_trainer(
-    trainer: transformers.trainer.Trainer,
-) -> transformers.trainer.Trainer:
+def wrap_transformers_trainer(trainer: "Trainer") -> "Trainer":
     """Change the class of trainer to a subclass implementing Ray-specific logic."""
     base_trainer_class: Type[transformers.trainer.Trainer] = trainer.__class__
 
@@ -213,3 +225,105 @@ class TrainReportCallback(TrainerCallback):
             **self.delayed_report["metrics"],
         }
         self._report()
+
+
+@PublicAPI(stability="alpha")
+class RayTrainReportCallback(TrainerCallback):
+    """A simple callback to report checkpoints and metrics to Ray Tarin.
+
+    This callback overrides the `TrainerCallback.on_save()` method. After
+    a new checkpoint get saved, it fetches the latest metric dictionary
+    from `TrainerState.log_history` and reports it with the latest checkpoint
+    to Ray Train.
+
+    If you want more customized reporting logics, please implement your own
+    callbacks following the Transformers integration user guides.
+
+    Note that users should ensure that the logging, evaluation, and saving frequencies
+    are properly configured so that the monitoring metric is always up-to-date
+    when `transformers.Trainer` saves a checkpoint.
+
+    Suppose the monitoring metric is reported from evaluation stage:
+
+    Some valid configurations:
+        - evaluation_strategy == save_strategy == "epoch"
+        - evaluation_strategy == save_strategy == "steps", save_steps % eval_steps == 0
+
+    Some invalid configurations:
+        - evaluation_strategy != save_strategy
+        - evaluation_strategy == save_strategy == "steps", save_steps % eval_steps != 0
+
+    For more info, see:
+    :ref:`Saving and Loading Checkpoints <train-dl-saving-checkpoints>`.
+
+    """
+
+    def on_save(self, args, state, control, **kwargs):
+        """Event called after a checkpoint save."""
+        with TemporaryDirectory() as tmpdir:
+            # Aggregate all the logged metrics
+            metrics = {}
+            for log in state.log_history:
+                metrics.update(log)
+
+            # Copy ckpt files and construct a Ray Train Checkpoint
+            source_ckpt_path = transformers.trainer.get_last_checkpoint(args.output_dir)
+            target_ckpt_path = os.path.join(tmpdir, "checkpoint")
+            shutil.copytree(source_ckpt_path, target_ckpt_path)
+            checkpoint = Checkpoint.from_directory(tmpdir)
+
+            # Report latest metrics and checkpoint to Ray Train
+            ray.train.report(metrics=metrics, checkpoint=checkpoint)
+
+
+class RayTorchIterableDataset(IterableDataset):
+    """Wrapper class for ray data iterables."""
+
+    def __init__(self, data_iterable) -> None:
+        super().__init__()
+        self.data_iterable = data_iterable
+
+    def __iter__(self) -> Iterator:
+        return iter(self.data_iterable)
+
+
+@PublicAPI(stability="alpha")
+def prepare_trainer(trainer: "Trainer") -> "Trainer":
+    """Prepare your HuggingFace Transformer Trainer for Ray Train.
+
+    This utility function enable the trainer integrates with Ray Data Integration.
+    Internally, it overrides the `get_train_dataloader` and `get_eval_dataloader`
+    methods and inject the data integration logics if the `train_dataset` and
+    `eval_dataset` are Ray Data Iterables.
+    """
+
+    if TRANSFORMERS_IMPORT_ERROR is not None:
+        raise TRANSFORMERS_IMPORT_ERROR
+
+    base_trainer_class: Type[transformers.trainer.Trainer] = trainer.__class__
+
+    class RayTransformersTrainer(base_trainer_class):
+        """A Wrapper of `transformers.Trainer` for Ray Data Integration."""
+
+        def get_train_dataloader(self) -> DataLoader:
+            if isinstance(self.train_dataset, _IterableFromIterator):
+                dataset = RayTorchIterableDataset(self.train_dataset)
+                return DataLoader(dataset, batch_size=1, collate_fn=lambda x: x[0])
+            else:
+                return super().get_train_dataloader()
+
+        def get_eval_dataloader(
+            self, eval_dataset: Optional[Dataset] = None
+        ) -> DataLoader:
+            if eval_dataset is None:
+                eval_dataset = self.eval_dataset
+
+            if isinstance(eval_dataset, _IterableFromIterator):
+                dataset = RayTorchIterableDataset(eval_dataset)
+                return DataLoader(dataset, batch_size=1, collate_fn=lambda x: x[0])
+            else:
+                return super().get_eval_dataloader(eval_dataset)
+
+    trainer.__class__ = RayTransformersTrainer
+
+    return trainer
