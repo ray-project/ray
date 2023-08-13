9113fd4ca8bb3168a3afcc2a06c13e5d2bd9f152
Christopher Field <chris.field@theiascientific.com>
2023-08-13 10:16:27 -0400

    Add watchfiles dependency
    Signed-off-by: Christopher Field <chris.field@theiascientific.com>

--- python/setup.py
+++ python/setup.py
@@ -336,6 +336,7 @@ if setup_spec.type == SetupType.RAY:
         "aiosignal",
         "frozenlist",
         "requests",
+        "watchfiles",
         # Light weight requirement, can be replaced with "typing" once
         # we deprecate Python 3.7 (this will take a while).
         "typing_extensions; python_version < '3.8'",
