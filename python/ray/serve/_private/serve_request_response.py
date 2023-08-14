7ba0b2038794131a83eef52c4eed9d67cb32591e
Gene Su <e870252314@gmail.com>
2023-08-13 22:20:49 -0700

    [Serve] Add gRPC proxy related data models
    Signed-off-by: Gene Su <e870252314@gmail.com>

---
+++ python/ray/serve/_private/serve_request_response.py
@@ -0,0 +1,137 @@
+import grpc
+import logging
+import pickle
+
+from starlette.types import Receive, Scope, Send
+from typing import Any, Callable, List, Generator, Optional, Tuple
+
+from ray.serve._private.constants import SERVE_LOGGER_NAME
+from ray.serve._private.utils import DEFAULT
+
+logger = logging.getLogger(SERVE_LOGGER_NAME)
+
+
+class ServeRequest:
+    """Base ServeRequest class to use in the common interface among proxies"""
+
+    pass
+
+
+class ASGIServeRequest(ServeRequest):
+    """ServeRequest implementation to wrap ASGI scope, receive, and send."""
+
+    def __init__(self, scope: Scope, receive: Receive, send: Send):
+        self.scope = scope
+        self.receive = receive
+        self.send = send
+
+    @property
+    def request_type(self) -> str:
+        return self.scope.get("type", "")
+
+    @property
+    def client(self) -> str:
+        return self.scope.get("client", "")
+
+    @property
+    def method(self) -> str:
+        return self.scope.get("method", "websocket").upper()
+
+    @property
+    def root_path(self) -> str:
+        return self.scope.get("root_path", "")
+
+    @property
+    def route_path(self) -> str:
+        return self.scope.get("path", "")[len(self.root_path) :]
+
+    @property
+    def path(self) -> str:
+        return self.scope.get("path", "")
+
+    @property
+    def headers(self) -> List[Tuple[bytes, bytes]]:
+        return self.scope.get("headers", [])
+
+    def set_path(self, path: str):
+        self.scope["path"] = path
+
+    def set_root_path(self, root_path: str):
+        self.scope["root_path"] = root_path
+
+
+class gRPCServeRequest(ServeRequest):
+    """ServeRequest implementation to wrap gRPC request protobuf and metadata."""
+
+    def __init__(
+        self,
+        request_proto: Any,
+        context: "grpc._cython.cygrpc._ServicerContext",
+        match_target: Callable[[str], Optional[str]],
+        service_method: str,
+        stream: bool,
+    ):
+        self.request = request_proto
+        self.context = context
+        self.match_target = match_target
+        self.service_method = service_method
+        self.stream = stream
+        self.app_name = ""
+        self.route_path = None
+        self.request_id = None
+        self.method_name = "__call__"
+        self.multiplexed_model_id = DEFAULT.VALUE
+        self.setup_variables()
+
+    def setup_variables(self):
+        if self.service_method == "/ray.serve.ServeAPIService/ServeRoutes":
+            self.route_path = "/-/routes"
+        elif self.service_method == "/ray.serve.ServeAPIService/ServeHealthz":
+            self.route_path = "/-/healthz"
+        else:
+            service_method_split = self.service_method.split("/")
+            self.request = pickle.dumps(self.request)
+            self.method_name = service_method_split[-1].lower()
+            for key, value in self.context.invocation_metadata():
+                if key == "application":
+                    self.app_name = value
+                elif key == "request_id":
+                    self.request_id = value
+                elif key == "multiplexed_model_id":
+                    self.multiplexed_model_id = value
+            self.route_path = self.match_target(self.app_name)
+
+    @property
+    def user_request(self) -> bytes:
+        return self.request
+
+    @property
+    def request_type(self) -> str:
+        return "grpc"
+
+    @property
+    def method(self) -> str:
+        return "GRPC"
+
+    def send_request_id(self, request_id: str):
+        self.context.set_trailing_metadata([("request_id", request_id)])
+
+    def send_status_code(self, status_code: grpc.StatusCode):
+        self.context.set_code(status_code)
+
+    def send_details(self, message: str):
+        self.context.set_details(message)
+
+
+class ServeResponse:
+    """ServerResponse class to use in the common interface among proxies"""
+
+    def __init__(
+        self,
+        status_code: str,
+        response: Optional[bytes] = None,
+        streaming_response: Optional[Generator[bytes, None, None]] = None,
+    ):
+        self.status_code = status_code
+        self.response = response
+        self.streaming_response = streaming_response
---
+++ python/ray/serve/tests/test_serve_request_response.py
@@ -0,0 +1,242 @@
+import pickle
+from typing import Generator
+from unittest.mock import MagicMock
+
+import grpc
+import pytest
+from ray.serve._private.serve_request_response import (
+    ASGIServeRequest,
+    gRPCServeRequest,
+    ServeRequest,
+    ServeResponse,
+)
+from ray.serve.generated import serve_pb2
+
+
+class TestASGIServeRequest:
+    def create_asgi_serve_request(self, scope: dict) -> ASGIServeRequest:
+        receive = MagicMock()
+        send = MagicMock()
+        return ASGIServeRequest(scope=scope, receive=receive, send=send)
+
+    def test_request_type(self):
+        """Test calling request_type on an instance of ASGIServeRequest.
+        When the request_type is not passed into the scope, it returns empty string.
+        When the request_type is passed into the scopt, it returns the correct value.
+        """
+        serve_request = self.create_asgi_serve_request(scope={})
+        assert isinstance(serve_request, ServeRequest)
+        assert serve_request.request_type == ""
+
+        request_type = "fake-request_type"
+        serve_request = self.create_asgi_serve_request(scope={"type": request_type})
+        assert isinstance(serve_request, ServeRequest)
+        assert serve_request.request_type == request_type
+
+    def test_client(self):
+        """Test calling client on an instance of ASGIServeRequest.
+        When the client is not passed into the scope, it returns empty string.
+        When the request_type is passed into the scopt, it returns the correct value.
+        """
+        serve_request = self.create_asgi_serve_request(scope={})
+        assert isinstance(serve_request, ServeRequest)
+        assert serve_request.client == ""
+
+        client = "fake-client"
+        serve_request = self.create_asgi_serve_request(scope={"client": client})
+        assert isinstance(serve_request, ServeRequest)
+        assert serve_request.client == client
+
+    def test_method(self):
+        """Test calling request_type on an instance of ASGIServeRequest.
+        When the request_type is not passed into the scope, it returns empty string.
+        When the request_type is passed into the scopt, it returns the correct value.
+        """
+        serve_request = self.create_asgi_serve_request(scope={})
+        assert isinstance(serve_request, ServeRequest)
+        assert serve_request.method == "WEBSOCKET"
+
+        method = "fake-method"
+        serve_request = self.create_asgi_serve_request(scope={"method": method})
+        assert isinstance(serve_request, ServeRequest)
+        assert serve_request.method == method.upper()
+
+    def test_root_path(self):
+        """Test calling request_type on an instance of ASGIServeRequest.
+        When the request_type is not passed into the scope, it returns empty string.
+        When the request_type is passed into the scopt, it returns the correct value.
+        """
+        serve_request = self.create_asgi_serve_request(scope={})
+        assert isinstance(serve_request, ServeRequest)
+        assert serve_request.root_path == ""
+
+        root_path = "fake-root_path"
+        serve_request.set_root_path(root_path)
+        assert serve_request.root_path == root_path
+
+        serve_request = self.create_asgi_serve_request(scope={"root_path": root_path})
+        assert isinstance(serve_request, ServeRequest)
+        assert serve_request.root_path == root_path
+
+    def test_path(self):
+        """Test calling request_type on an instance of ASGIServeRequest.
+        When the request_type is not passed into the scope, it returns empty string.
+        When the request_type is passed into the scopt, it returns the correct value.
+        """
+        serve_request = self.create_asgi_serve_request(scope={})
+        assert isinstance(serve_request, ServeRequest)
+        assert serve_request.path == ""
+
+        path = "fake-path"
+        serve_request.set_path(path)
+        assert serve_request.path == path
+
+        serve_request = self.create_asgi_serve_request(scope={"path": path})
+        assert isinstance(serve_request, ServeRequest)
+        assert serve_request.path == path
+
+    def test_headers(self):
+        """Test calling request_type on an instance of ASGIServeRequest.
+        When the request_type is not passed into the scope, it returns empty string.
+        When the request_type is passed into the scopt, it returns the correct value.
+        """
+        serve_request = self.create_asgi_serve_request(scope={})
+        assert isinstance(serve_request, ServeRequest)
+        assert serve_request.headers == []
+
+        headers = [(b"fake-header-key", b"fake-header-value")]
+        serve_request = self.create_asgi_serve_request(scope={"headers": headers})
+        assert isinstance(serve_request, ServeRequest)
+        assert serve_request.headers == headers
+
+
+class TestgRPCServeRequest:
+    def test_calling_routes_method(self):
+        """Test initialize gRPCServeRequest with routes service method.
+        When the gRPCServeRequest is initialized with routes service method, route_path
+        should be set to "/-/routes". `send_status_code()` and `send_details()` should
+        also work accordingly to be able to send the into back to the client.
+        """
+        context = MagicMock()
+        service_method = "/ray.serve.ServeAPIService/ServeRoutes"
+        serve_request = gRPCServeRequest(
+            request_proto=MagicMock(),
+            context=context,
+            match_target=MagicMock(),
+            service_method=service_method,
+            stream=MagicMock(),
+        )
+        assert isinstance(serve_request, ServeRequest)
+        assert serve_request.route_path == "/-/routes"
+
+        status_code = grpc.StatusCode.OK
+        serve_request.send_status_code(status_code=status_code)
+        context.set_code.assert_called_with(status_code)
+
+        message = "success"
+        serve_request.send_details(message=message)
+        context.set_details.assert_called_with(message)
+
+    def test_calling_healthz_method(self):
+        """Test initialize gRPCServeRequest with healthz service method.
+        When the gRPCServeRequest is initialized with healthz service method, route_path
+        should be set to "/-/healthz". `send_status_code()` and `send_details()` should
+        also work accordingly to be able to send the into back to the client.
+        """
+        context = MagicMock()
+        service_method = "/ray.serve.ServeAPIService/ServeHealthz"
+        serve_request = gRPCServeRequest(
+            request_proto=MagicMock(),
+            context=context,
+            match_target=MagicMock(),
+            service_method=service_method,
+            stream=MagicMock(),
+        )
+        assert isinstance(serve_request, ServeRequest)
+        assert serve_request.route_path == "/-/healthz"
+
+        status_code = grpc.StatusCode.OK
+        serve_request.send_status_code(status_code=status_code)
+        context.set_code.assert_called_with(status_code)
+
+        message = "success"
+        serve_request.send_details(message=message)
+        context.set_details.assert_called_with(message)
+
+    def test_calling_user_defined_method(self):
+        """Test initialize gRPCServeRequest with user defined service method.
+        When the gRPCServeRequest is initialized with user defined service method,
+        all attributes should be setup accordingly. `send_request_id()` should
+        also work accordingly to be able to send the into back to the client.
+        """
+        # request_proto = serve_pb2.UserDefinedMessage(name="foo", num=30, foo="bar")
+        request_proto = serve_pb2.PredictResponse(prediction=b"1")
+        application = "fake-application"
+        request_id = "fake-request_id"
+        multiplexed_model_id = "fake-multiplexed_model_id"
+        metadata = (
+            ("foo", "bar"),
+            ("application", application),
+            ("request_id", request_id),
+            ("multiplexed_model_id", multiplexed_model_id),
+        )
+        context = MagicMock()
+        context.invocation_metadata.return_value = metadata
+        method_name = "Method1"
+        service_method = f"/custom.defined.Service/{method_name}"
+
+        def match_target(app_name: str) -> str:
+            if app_name == application:
+                return "matched_path"
+            return "unmatched_path"
+
+        serve_request = gRPCServeRequest(
+            request_proto=request_proto,
+            context=context,
+            match_target=match_target,
+            service_method=service_method,
+            stream=MagicMock(),
+        )
+        assert isinstance(serve_request, ServeRequest)
+        assert serve_request.route_path == "matched_path"
+        assert pickle.loads(serve_request.request) == request_proto
+        assert serve_request.method_name == method_name.lower()
+        assert serve_request.app_name == application
+        assert serve_request.request_id == request_id
+        assert serve_request.multiplexed_model_id == multiplexed_model_id
+
+        serve_request.send_request_id(request_id=request_id)
+        context.set_trailing_metadata.assert_called_with([("request_id", request_id)])
+
+
+def test_serve_response():
+    """Test ServeResponse.
+    When a ServeResponse object is initialized with status_code, response, and
+    streaming_response, the object is able to return the correct values.
+    """
+    status_code = "200"
+    response = b"unary_response"
+
+    def streaming_response(i: int) -> bytes:
+        return f"{i} from generator".encode()
+
+    def test_generator() -> Generator[bytes, None, None]:
+        for i in range(10):
+            yield streaming_response(i=i)
+
+    serve_response = ServeResponse(
+        status_code=status_code,
+        response=response,
+        streaming_response=test_generator(),
+    )
+
+    assert serve_response.status_code == status_code
+    assert serve_response.response == response
+    for idx, resp in enumerate(serve_response.streaming_response):
+        assert resp == streaming_response(idx)
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(pytest.main(["-v", "-s", __file__]))
