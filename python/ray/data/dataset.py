9aa6ca6e93391a6e3fec8fe2ee1be8d6814812ae
Cheng Su <scnju13@gmail.com>
2023-08-15 13:04:45 -0700

    [Data] Fix to_pandas() incomplete docstring (#38445)
    Somehow the `to_pandas()` doc has an incomplete ending - https://docs.ray.io/en/master/data/api/doc/ray.data.Dataset.to_pandas.html . This PR is trying to fix it.
    
    Signed-off-by: Cheng Su <scnju13@gmail.com>
    Signed-off-by: Victor <vctr.y.m@example.com>

--- python/ray/data/dataset.py
+++ python/ray/data/dataset.py
@@ -4145,7 +4145,7 @@ class Dataset:
 
         Raises:
             ValueError: if the number of rows in the :class:`~ray.data.Dataset` exceeds
-            ``limit``.
+                ``limit``.
         """
         count = self.count()
         if limit is not None and count > limit:
