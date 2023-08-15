e677a69ade3493f7ac79bf9bf832eac82b0876a8
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-08-15 17:25:34 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/handle.py
+++ python/ray/serve/handle.py
@@ -52,7 +52,7 @@ def _create_or_get_async_loop_in_thread():
 
 
 @dataclass(frozen=True)
-class HandleOptions:
+class _HandleOptions:
     """Options for each ServeHandle instance.
 
     These fields can be changed by calling `.options()` on a handle.
@@ -69,8 +69,8 @@ class HandleOptions:
         multiplexed_model_id: Union[str, DEFAULT] = DEFAULT.VALUE,
         stream: Union[bool, DEFAULT] = DEFAULT.VALUE,
         _router_cls: Union[str, DEFAULT] = DEFAULT.VALUE,
-    ) -> "HandleOptions":
-        return HandleOptions(
+    ) -> "_HandleOptions":
+        return _HandleOptions(
             method_name=(
                 self.method_name if method_name == DEFAULT.VALUE else method_name
             ),
@@ -131,12 +131,12 @@ class RayServeHandle:
         self,
         deployment_name: EndpointTag,
         *,
-        handle_options: Optional[HandleOptions] = None,
+        handle_options: Optional[_HandleOptions] = None,
         _router: Optional[Router] = None,
         _is_for_http_requests: bool = False,
     ):
         self.deployment_name = deployment_name
-        self.handle_options = handle_options or HandleOptions()
+        self.handle_options = handle_options or _HandleOptions()
         self._is_for_http_requests = _is_for_http_requests
 
         self.request_counter = metrics.Counter(
