0fbd49b9e037398436c98578d2024aed6397119d
Justin Yu <justinvyu@anyscale.com>
2023-08-15 10:25:34 -0700

    [lightgbm] fix legacy doctest
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/train/lightgbm/lightgbm_checkpoint.py
+++ python/ray/train/lightgbm/lightgbm_checkpoint.py
@@ -93,13 +93,13 @@ class LegacyLightGBMCheckpoint(Checkpoint):
         Examples:
             >>> import lightgbm
             >>> import numpy as np
-            >>> from ray.train.lightgbm import LightGBMCheckpoint
+            >>> from ray.train.lightgbm import LegacyLightGBMCheckpoint
             >>>
             >>> train_X = np.array([[1, 2], [3, 4]])
             >>> train_y = np.array([0, 1])
             >>>
             >>> model = lightgbm.LGBMClassifier().fit(train_X, train_y)
-            >>> checkpoint = LightGBMCheckpoint.from_model(model.booster_)
+            >>> checkpoint = LegacyLightGBMCheckpoint.from_model(model.booster_)
 
             You can use a :py:class:`LightGBMCheckpoint` to create an
             :py:class:`~ray.train.lightgbm.LightGBMPredictor` and preform inference.
