9327c878db6bd10930e06da97a774ac24dc6259f
kourosh hakhamaneshi <31483498+kouroshHakha@users.noreply.github.com>
2023-08-15 10:31:18 -0700

    [Train] Simplify llama 2 workspace template (#38444)
    * Remove the need for prepare_node stuff by enabling the downloading as part of the training function
    * Added a script to create job submission yamls
    * Simplified the ray dataset creation by directly reading the json file into a ray dataset.
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- doc/source/templates/04_finetuning_llms_with_deepspeed/finetune_hf_llm.py
+++ doc/source/templates/04_finetuning_llms_with_deepspeed/finetune_hf_llm.py
@@ -9,12 +9,12 @@ import re
 import functools
 import time
 import tree
-import pandas as pd
 from pathlib import Path
 import torch.nn as nn
 from ray import tune  # noqa: F401
 import tqdm
 import tempfile
+from filelock import FileLock
 
 from accelerate import Accelerator, DeepSpeedPlugin
 from accelerate.utils import DummyOptim, DummyScheduler, set_seed
@@ -31,7 +31,12 @@ from ray.air import session
 from ray.train.torch import TorchTrainer
 import ray.util.scheduling_strategies
 
-from utils import get_checkpoint_and_refs_dir, get_mirror_link
+from utils import (
+    get_checkpoint_and_refs_dir,
+    get_mirror_link,
+    download_model,
+    get_download_path,
+)
 
 OPTIM_BETAS = (0.9, 0.999)
 OPTIM_EPS = 1e-8
@@ -75,20 +80,6 @@ def get_tokenizer(model_name, special_tokens):
     tokenizer.add_tokens(special_tokens, special_tokens=True)
 
     return tokenizer
-
-
-def create_ray_dataset(path):
-    # jsonl file
-    with open(path, "r") as json_file:
-        items = [json.loads(x) for x in json_file]
-
-    dataset = {"input": []}
-    for item in items:
-        assert set(item.keys()) == {"input"}
-        dataset["input"].append(item["input"])
-
-    df = pd.DataFrame.from_dict(dataset)
-    return ray.data.from_pandas(df)
 
 
 def evaluate(
@@ -173,6 +164,18 @@ def training_function(kwargs: dict):
     args = argparse.Namespace(**kwargs["args"])
     special_tokens = kwargs.get("special_tokens", [])
     model_id = config["model_name"]
+
+    # We need to download the model weights on this machine if they don't exit.
+    # We need to acquire a lock to ensure that only one process downloads the model
+    bucket_uri = get_mirror_link(model_id)
+    download_path = get_download_path(model_id)
+    base_path = Path(download_path).parent
+    base_path.mkdir(parents=True, exist_ok=True)
+    lock_file = str(base_path / f'{model_id.replace("/",  "--")}.lock')
+    with FileLock(lock_file):
+        download_model(
+            model_id=model_id, bucket_uri=bucket_uri, s3_sync_args=["--no-sign-request"]
+        )
 
     # Sample hyper-parameters for learning rate, batch size, seed and a few other HPs
     lr = config["lr"]
@@ -559,9 +562,10 @@ def main():
         }
     )
 
-    train_ds = create_ray_dataset(args.train_path)
+    # Read data
+    train_ds = ray.data.read_json(args.train_path)
     if args.test_path is not None:
-        valid_ds = create_ray_dataset(args.test_path)
+        valid_ds = ray.data.read_json(args.test_path)
     else:
         valid_ds = None
 
