061b73d82df420ff80e8b1a903ed76667f74cc8b
kourosh hakhamaneshi <31483498+kouroshHakha@users.noreply.github.com>
2023-08-15 10:31:18 -0700

    [Train] Simplify llama 2 workspace template (#38444)
    * Remove the need for prepare_node stuff by enabling the downloading as part of the training function
    * Added a script to create job submission yamls
    * Simplified the ray dataset creation by directly reading the json file into a ray dataset.
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>
    Signed-off-by: Victor <vctr.y.m@example.com>

--- doc/source/templates/04_finetuning_llms_with_deepspeed/utils.py
+++ doc/source/templates/04_finetuning_llms_with_deepspeed/utils.py
@@ -46,6 +46,13 @@ def get_checkpoint_and_refs_dir(
     return checkpoint_dir, refs_dir
 
 
+def get_download_path(model_id: str):
+    from transformers.utils.hub import TRANSFORMERS_CACHE
+
+    path = os.path.join(TRANSFORMERS_CACHE, f"models--{model_id.replace('/', '--')}")
+    return path
+
+
 def download_model(
     model_id: str,
     bucket_uri: str,
@@ -59,12 +66,8 @@ def download_model(
     The downloaded model may have a 'hash' file containing the commit hash corresponding
     to the commit on Hugging Face Hub.
     """
-
-    from transformers.utils.hub import TRANSFORMERS_CACHE
-
     s3_sync_args = s3_sync_args or []
-
-    path = os.path.join(TRANSFORMERS_CACHE, f"models--{model_id.replace('/', '--')}")
+    path = get_download_path(model_id)
 
     cmd = (
         ["awsv2", "s3", "sync"]
