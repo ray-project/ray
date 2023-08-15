b7f588707d1425594a7d0c07f27ac473d01da113
shrekris-anyscale <92341594+shrekris-anyscale@users.noreply.github.com>
2023-08-15 08:39:04 -0700

    [Serve] Print fewer logs to terminal (#38438)
    This change:
    
    1. Truncates the number of seconds logged about the controller's recovery to 2 decimal places.
    2. Stops printing some logs to terminal.
    
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- python/ray/serve/_private/http_proxy.py
+++ python/ray/serve/_private/http_proxy.py
@@ -352,10 +352,16 @@ class HTTPProxy:
         """
 
         if draining and (not self._is_draining()):
-            logger.info(f"Start to drain the proxy actor on node {self._node_id}.")
+            logger.info(
+                f"Start to drain the proxy actor on node {self._node_id}.",
+                extra={"log_to_stderr": False},
+            )
             self._draining_start_time = time.time()
         if (not draining) and self._is_draining():
-            logger.info(f"Stop draining the proxy actor on node {self._node_id}.")
+            logger.info(
+                f"Stop draining the proxy actor on node {self._node_id}.",
+                extra={"log_to_stderr": False},
+            )
             self._draining_start_time = None
 
     async def _not_found(self, scope, receive, send):
