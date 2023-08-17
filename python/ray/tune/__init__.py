0a4bc103683572028f7ee271a25a14c24cee95fa
Kai Fricke <krfricke@users.noreply.github.com>
2023-08-17 11:12:48 +0200

    [train/tune] Raise actionable error message for missing dependencies (#38497)
    When users `pip install ray` but not `ray[train]`/`ray[tune]`, they can run into confusing error messages, such as:
    
    ```
    ModuleNotFoundError: No module named 'pyarrow'
    ```
    
    With this PR, we import the core library requirements in the respective `__init__.py` and raise an actionable error message if they are not found:
    
    ```
    ImportError: Can't import ray.train as some dependencies are missing. Run `pip install ray[train]` to fix.
    ```
    
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- python/ray/tune/__init__.py
+++ python/ray/tune/__init__.py
@@ -1,3 +1,15 @@
+# Try import ray[tune] core requirements (defined in setup.py)
+try:
+    import pandas  # noqa: F401
+    import requests  # noqa: F401
+    import pyarrow  # noqa: F401
+except ImportError as exc:
+    raise ImportError(
+        "Can't import ray.tune as some dependencies are missing. "
+        'Run `pip install "ray[tune]"` to fix.'
+    ) from exc
+
+
 from ray.tune.error import TuneError
 from ray.tune.tune import run_experiments, run
 from ray.tune.syncer import SyncConfig
