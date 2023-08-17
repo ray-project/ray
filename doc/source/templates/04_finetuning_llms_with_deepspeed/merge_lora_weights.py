846bfdd415fb9c4fb71680d961a2c79644ba54d7
Artur Niederfahrenhorst <artur@anyscale.com>
2023-08-17 13:13:43 -0700

    polish merge_lora_weights.py

--- doc/source/templates/04_finetuning_llms_with_deepspeed/merge_lora_weights.py
+++ doc/source/templates/04_finetuning_llms_with_deepspeed/merge_lora_weights.py
@@ -1,6 +1,6 @@
 """
 This script merges the weights of a LoRA checkpoint with the base model weights
-to create a single model that can be used for inference.
+to create a single model that can be used for model evaluation.
 """
 
 import torch
@@ -17,12 +17,13 @@ from transformers import (
 
 from utils import download_model, get_mirror_link, get_checkpoint_and_refs_dir
 
+# In addition to merging the lora weights, you can also formulate a prompt for the model here to quickly test it after merging
+TEST_EVAL = False
 TEST_PROMPT = "<START_Q>Natalia sold clips to 48 of her friends in April, and then she sold half as many clips in May. How many clips did Natalia sell altogether in April and May?<END_Q><START_A>"
 STOP_TOKEN = "<END_A>"
 
 
 def parse_args():
-
     parser = argparse.ArgumentParser(description="Simple example of training script.")
     parser.add_argument(
         "--mx",
@@ -48,6 +49,33 @@ def parse_args():
 
     return args
 
+def test_eval(model, tokenizer):
+    """Query the model with a single prompt to sanity check it."""
+
+    model.eval()
+    model.to("cuda")
+
+    print(f"Prompting model with promtp : " , TEST_PROMPT)
+    input_ids = tokenizer(TEST_PROMPT, return_tensors="pt")["input_ids"].to("cuda")
+
+    stop_token_embeding = tokenizer(STOP_TOKEN, return_tensors="pt", add_special_tokens=False)["input_ids"].to("cuda")
+      
+    def custom_stopping_criteria(embeddings, *args, **kwargs) -> bool:
+        return stop_token_embeding in embeddings
+
+    stopping_criteria = StoppingCriteriaList([custom_stopping_criteria])
+
+    with torch.no_grad():
+        generation_output = model.generate(
+            input_ids=input_ids,
+            output_scores=True,
+            max_new_tokens=500,
+            stopping_criteria=stopping_criteria,
+    )
+    
+    decoded = tokenizer.batch_decode(generation_output)
+    print("Outputs: ", decoded)
+    
 
 def main():
     args = parse_args()
@@ -62,14 +90,12 @@ def main():
     
     Path(args.output_path).mkdir(parents=True, exist_ok=True)
 
-
     # Load orignal model
     s = time.time()
     model_id = f"meta-llama/Llama-2-{args.model_name}-hf"
     print(f"Downloading original model {model_id} ...")
     s3_bucket = get_mirror_link(model_id)
     ckpt_path, _ = get_checkpoint_and_refs_dir(model_id=model_id, bucket_uri=s3_bucket)
-
 
     print(f"Loading tokenizer...")
     tokenizer = AutoTokenizer.from_pretrained(args.checkpoint, legacy=True)
@@ -107,32 +133,9 @@ def main():
 
     print(f"Saved merged model to {args.output_path}")
 
-    # Sanity check model
-    model.eval()
-    model.to("cuda")
+    if TEST_EVAL:
+        test_eval(model, tokenizer)
 
-    #prompt = "<s>Let's answer only the following question briefly. Question: Hi, what is 2 times 2? Answer: "
-    print(f"Prompting model with promtp : " , TEST_PROMPT)
-    input_ids = tokenizer(TEST_PROMPT, return_tensors="pt")["input_ids"].to("cuda")
-
-    stop_token_embeding = tokenizer(STOP_TOKEN, return_tensors="pt", add_special_tokens=False)["input_ids"].to("cuda")
-      
-    def custom_stopping_criteria(embeddings, *args, **kwargs) -> bool:
-        return stop_token_embeding in embeddings
-
-    stopping_criteria = StoppingCriteriaList([custom_stopping_criteria])
-
-    with torch.no_grad():
-        generation_output = model.generate(
-            input_ids=input_ids,
-            output_scores=True,
-            max_new_tokens=200,
-            stopping_criteria=stopping_criteria,
-    )
-    
-    decoded = tokenizer.batch_decode(generation_output)
-    print("Outputs: ", decoded)
-    
 
 if __name__ == "__main__":
     main()
