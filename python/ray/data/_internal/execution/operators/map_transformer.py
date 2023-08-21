1771a12af8afa9d9811efbcb76e76377961861e6
Hao Chen <chenh1024@gmail.com>
2023-08-21 12:55:00 -0700

    [data] revert changes on building empty blocks in map_batches (#38667)
    #38546 tried to fix the issue of missing schema in empty blocks. But that approach doesn't work for ragged tensor arrays.
    Reverting that change. Per discussion, we'll stop scheduling empty blocks for the map operator instead.
    
    Signed-off-by: Hao Chen <chenh1024@gmail.com>
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- python/ray/data/_internal/execution/operators/map_transformer.py
+++ python/ray/data/_internal/execution/operators/map_transformer.py
@@ -182,7 +182,7 @@ def input_blocks_to_batches(
     if first is None:
         return []
     blocks = itertools.chain([first], block_iter)
-    empty_block = BlockAccessor.for_block(first).slice(0, 0, copy=True)
+    empty_block = BlockAccessor.for_block(first).builder().build()
     # Don't hold the first block in memory, so we reset the reference.
     first = None
 
