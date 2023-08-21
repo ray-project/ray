d8a18e98af2be1fb071371ad1e045943b5eb40c2
Hao Chen <chenh1024@gmail.com>
2023-08-21 14:53:15 -0700

    lint
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- python/ray/data/_internal/planner/plan_udf_map_op.py
+++ python/ray/data/_internal/planner/plan_udf_map_op.py
@@ -12,9 +12,9 @@ from ray.data._internal.execution.interfaces import PhysicalOperator
 from ray.data._internal.execution.interfaces.task_context import TaskContext
 from ray.data._internal.execution.operators.map_operator import MapOperator
 from ray.data._internal.execution.operators.map_transformer import (
-    BuildOutputBlocksMapTransformFn,
     BlocksToBatchesMapTransformFn,
     BlocksToRowsMapTransformFn,
+    BuildOutputBlocksMapTransformFn,
     MapTransformCallable,
     MapTransformer,
     MapTransformFn,
