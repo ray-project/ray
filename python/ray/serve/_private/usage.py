3b31e36f06771d42283a85b20e35981d8983fa49
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-08-21 09:16:56 -0700

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/_private/usage.py
+++ python/ray/serve/_private/usage.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Dict, Optional
 
 from ray._private.usage.usage_lib import TagKey, record_extra_usage_tag
 
@@ -29,3 +30,10 @@ class ServeUsageTag(Enum):
     def record(self, value: str):
         """Record telemetry value."""
         record_extra_usage_tag(self.value, value)
+
+    def get_value_from_report(self, report: Dict) -> Optional[str]:
+        """Returns `None` if the tag isn't in the report."""
+        if "extra_usage_tags" not in report:
+            return None
+
+        return report["extra_usage_tags"].get(TagKey.Name(self.value).lower(), None)
