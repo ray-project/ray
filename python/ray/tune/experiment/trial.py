8c7387c22eca0564fdd502d66dd6facd3504eb7a
Eric Liang <ekhliang@gmail.com>
2023-08-21 14:49:37 -0700

    [tune/train] Get rid of downsyncing path and download just the result.json / result.csv on result (#38598)
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- python/ray/tune/experiment/trial.py
+++ python/ray/tune/experiment/trial.py
@@ -26,6 +26,7 @@ from ray.air.constants import (
 
 import ray.cloudpickle as cloudpickle
 from ray.exceptions import RayActorError, RayTaskError
+from ray.train._checkpoint import Checkpoint
 from ray.train._internal.checkpoint_manager import (
     _TrainingResult,
     _CheckpointManager as _NewCheckpointManager,
@@ -832,14 +833,16 @@ class Trial:
         return config.checkpoint_frequency
 
     @property
-    def checkpoint(self):
-        """Returns the most recent checkpoint.
-
-        If the trial is in ERROR state, the most recent PERSISTENT checkpoint
-        is returned.
-        """
+    def checkpoint(self) -> Optional[Checkpoint]:
+        """Returns the most recent checkpoint if one has been saved."""
         if _use_storage_context():
-            return self.run_metadata.checkpoint_manager.latest_checkpoint_result
+            checkpoint_manager = self.run_metadata.checkpoint_manager
+            latest_checkpoint_result = checkpoint_manager.latest_checkpoint_result
+            return (
+                latest_checkpoint_result.checkpoint
+                if latest_checkpoint_result
+                else None
+            )
 
         if self.status == Trial.ERROR:
             checkpoint = (
