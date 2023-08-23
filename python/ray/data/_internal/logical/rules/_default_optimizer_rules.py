a8d1c1af050494ff1fba725e59b00be709972754
Hao Chen <chenh1024@gmail.com>
2023-08-23 10:03:27 -0700

    default rule
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- python/ray/data/_internal/logical/rules/_default_optimizer_rules.py
+++ python/ray/data/_internal/logical/rules/_default_optimizer_rules.py
@@ -1,6 +1,8 @@
 from ray.data._internal.logical.rules.operator_fusion import OperatorFusionRule
 from ray.data._internal.logical.rules.randomize_blocks import ReorderRandomizeBlocksRule
 
+from ray.data._internal.logical.rules.zero_copy_map_fusion import ReadOpZeroCopyMapFusion
+
 
 def get_logical_optimizer_rules():
     rules = [ReorderRandomizeBlocksRule]
@@ -8,5 +10,5 @@ def get_logical_optimizer_rules():
 
 
 def get_physical_optimizer_rules():
-    rules = [OperatorFusionRule]
+    rules = [OperatorFusionRule, ReadOpZeroCopyMapFusion]
     return rules
