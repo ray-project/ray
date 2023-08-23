998865d83ae616f722c0c4943f16a311d447af47
Hao Chen <chenh1024@gmail.com>
2023-08-23 11:38:40 -0700

    lint
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- python/ray/data/_internal/logical/rules/_default_optimizer_rules.py
+++ python/ray/data/_internal/logical/rules/_default_optimizer_rules.py
@@ -1,7 +1,8 @@
 from ray.data._internal.logical.rules.operator_fusion import OperatorFusionRule
 from ray.data._internal.logical.rules.randomize_blocks import ReorderRandomizeBlocksRule
-
-from ray.data._internal.logical.rules.zero_copy_map_fusion import ReadOpZeroCopyMapFusion
+from ray.data._internal.logical.rules.zero_copy_map_fusion import (
+    ReadOpZeroCopyMapFusion,
+)
 
 
 def get_logical_optimizer_rules():
