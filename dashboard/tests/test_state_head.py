5b8e717b9f91d477315be29d97cf6b260b83bd88
chaowang <chaowang@anyscale.com>
2023-08-23 08:26:24 +0000

    feat
    Signed-off-by: chaowang <chaowang@anyscale.com>

--- dashboard/tests/test_state_head.py
+++ dashboard/tests/test_state_head.py
@@ -4,7 +4,7 @@ import ray
 from ray._private.test_utils import format_web_url, wait_for_condition
 import requests
 import pytest
-from ray.dashboard.modules.state.state_head import RateLimitedModule
+from ray.dashboard.modules.state.state_head import RateLimitedModule, filter_events
 import os
 
 
@@ -117,6 +117,47 @@ async def test_list_events(shutdown_only):
         return True
 
     wait_for_condition(verify)
+
+
+events = [
+    {"severity": "ERROR", "source_type": "GCS", "custom_fields": {"job_id": 1}},
+    {
+        "severity": "DEBUG",
+        "source_type": "CORE_WORKER",
+        "custom_fields": {"serve_app_name": 42},
+    },
+    {
+        "severity": "WARNING",
+        "source_type": "GCS",
+        "custom_fields": {"job_id": 2, "serve_replica_id": 10},
+    },
+    {
+        "severity": "INFO",
+        "source_type": "RAYLET",
+        "custom_fields": {"serve_replica_id": 3},
+    },
+]
+
+
+@pytest.mark.parametrize(
+    "severity_levels, source_types, entity_name, entity_id, expected_output",
+    [
+        (["ERROR"], None, None, None, [events[0]]),
+        (None, ["GCS"], None, None, [events[0], events[2]]),
+        (None, None, "job_id", "1", [events[0]]),
+        (None, None, "serve_app_name", "42", [events[1]]),
+        (None, None, "serve_replica_id", "10", [events[2]]),
+        (["WARNING"], ["GCS"], "job_id", "2", [events[2]]),
+        (None, None, "serve_replica_id", "*", [events[2], events[3]]),
+    ],
+)
+def test_filter_events(
+    severity_levels, source_types, entity_name, entity_id, expected_output
+):
+    result = filter_events(
+        events, severity_levels, source_types, entity_name, entity_id
+    )
+    assert result == expected_output
 
 
 if __name__ == "__main__":
