3dcc66993c63fc9b194151af2da97ccbc7530585
Scott Lee <sjl@anyscale.com>
2023-08-23 11:12:02 -0700

    insert debugging statements and try removing dynamic transformers import
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- python/ray/data/datasource/huggingface_datasource.py
+++ python/ray/data/datasource/huggingface_datasource.py
@@ -13,39 +13,39 @@ if TYPE_CHECKING:
     import datasets
 
 
-TRANSFORMERS_IMPORT_ERROR: Optional[ImportError] = None
+# TRANSFORMERS_IMPORT_ERROR: Optional[ImportError] = None
 
-try:
-    # Due to HF Dataset's dynamic module system, we need to dynamically import the
-    # datasets_modules module on every actor when training.
-    # We accomplish this by simply running the following bit of code directly
-    # in module you are currently viewing. This ensures that when we
-    # unpickle the Dataset, it will be ran before pickle tries to
-    # import datasets_modules and prevents an exception from being thrown.
-    # Same logic is present inside ray's TransformersTrainer and HF Transformers Ray
-    # integration: https://github.com/huggingface/transformers/blob/\
-    # 7d5fde991d598370d961be8cb7add6541e2b59ce/src/transformers/integrations.py#L271
-    # Also see https://github.com/ray-project/ray/issues/28084
-    from transformers.utils import is_datasets_available
+# try:
+#     # Due to HF Dataset's dynamic module system, we need to dynamically import the
+#     # datasets_modules module on every actor when training.
+#     # We accomplish this by simply running the following bit of code directly
+#     # in module you are currently viewing. This ensures that when we
+#     # unpickle the Dataset, it will be ran before pickle tries to
+#     # import datasets_modules and prevents an exception from being thrown.
+#     # Same logic is present inside ray's TransformersTrainer and HF Transformers Ray
+#     # integration: https://github.com/huggingface/transformers/blob/\
+#     # 7d5fde991d598370d961be8cb7add6541e2b59ce/src/transformers/integrations.py#L271
+#     # Also see https://github.com/ray-project/ray/issues/28084
+#     from transformers.utils import is_datasets_available
 
-    if "datasets_modules" not in sys.modules and is_datasets_available():
-        import importlib
-        import os
+#     if "datasets_modules" not in sys.modules and is_datasets_available():
+#         import importlib
+#         import os
 
-        import datasets.load
+#         import datasets.load
 
-        dynamic_modules_path = os.path.join(
-            datasets.load.init_dynamic_modules(), "__init__.py"
-        )
-        # load dynamic_modules from path
-        spec = importlib.util.spec_from_file_location(
-            "datasets_modules", dynamic_modules_path
-        )
-        datasets_modules = importlib.util.module_from_spec(spec)
-        sys.modules[spec.name] = datasets_modules
-        spec.loader.exec_module(datasets_modules)
-except ImportError as e:
-    TRANSFORMERS_IMPORT_ERROR = e
+#         dynamic_modules_path = os.path.join(
+#             datasets.load.init_dynamic_modules(), "__init__.py"
+#         )
+#         # load dynamic_modules from path
+#         spec = importlib.util.spec_from_file_location(
+#             "datasets_modules", dynamic_modules_path
+#         )
+#         datasets_modules = importlib.util.module_from_spec(spec)
+#         sys.modules[spec.name] = datasets_modules
+#         spec.loader.exec_module(datasets_modules)
+# except ImportError as e:
+#     TRANSFORMERS_IMPORT_ERROR = e
 
 
 @DeveloperAPI
@@ -64,8 +64,8 @@ class HuggingFaceDatasource(Datasource):
         self,
         dataset: Union["datasets.Dataset", "datasets.IterableDataset"],
     ) -> "_HuggingFaceDatasourceReader":
-        if TRANSFORMERS_IMPORT_ERROR is not None:
-            raise TRANSFORMERS_IMPORT_ERROR
+        # if TRANSFORMERS_IMPORT_ERROR is not None:
+        #     raise TRANSFORMERS_IMPORT_ERROR
         return _HuggingFaceDatasourceReader(dataset)
 
 
