1dc07c7bbedc55acdc66a83dc9461ec3c6a0b818
Ruiyang Wang <rywang014@gmail.com>
2023-08-22 18:26:55 -0700

    rewrite test_many_runtime_envs.py with env vars
    Signed-off-by: Ruiyang Wang <rywang014@gmail.com>

--- release/nightly_tests/stress_tests/test_many_runtime_envs.py
+++ release/nightly_tests/stress_tests/test_many_runtime_envs.py
@@ -13,46 +13,28 @@ logger = logging.getLogger(__name__)
 ray.init(address="auto")
 
 
-@ray.remote
-def assert_file_content(file_name, expected_content):
-    with open(file_name, "r") as file:
-        actual_content = file.read()
-    if actual_content != expected_content:
-        raise ValueError(f"expected {expected_content}, got {actual_content}")
-
-
-@ray.remote
-def assert_n_files_in_working_dir(n):
-    files = os.listdir()
-    num_files = len(files)
-    if num_files != n:
-        raise ValueError(
-            f"Expected {n} files in working dir, but found {num_files} files."
-        )
+@ray.remote(num_cpus=0.01)
+def assert_env_var(prefix, expected_count, expected_value):
+    count = 0
+    for k, v in os.environ.items():
+        if k.startswith(prefix):
+            assert v == expected_value
+            count += 1
+    assert count == expected_count
 
 
 def parse_script_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file-name", type=str)
-    parser.add_argument("--expected-content", type=str)
-    parser.add_argument("--expected-file-count-in-working-dir", type=int)
-    parser.add_argument("--node_id", type=str)
+    parser.add_argument("--num_runtime_envs", type=int)
     return parser.parse_known_args()
 
 
 if __name__ == "__main__":
     args, unknown = parse_script_args()
-    strategy = ray.util.scheduling_strategies.NodeAffinitySchedulingStrategy(
-        node_id=args.node_id,
-        soft=True,
-    )
-    ray.get(
-        [
-            assert_file_content.options(strategy=strategy).remote(
-                args.file_name, args.expected_content
-            ),
-            assert_n_files_in_working_dir.options(strategy=strategy).remote(
-                args.expected_file_count_in_working_dir
-            ),
-        ]
-    )
+    tasks = []
+    for i in range(args.num_runtime_envs):
+        task = assert_env_var.options(
+            runtime_env={"env_vars": {f"STRESS_TEST_{j}": "val" for j in range(i)}}
+        ).remote("STRESS_TEST_", i, "val")
+        tasks.append(task)
+    ray.get(tasks)
