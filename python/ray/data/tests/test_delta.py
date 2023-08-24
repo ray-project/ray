f4ffceebff184c2b1a2eed624785b90ec35eccb0
Serena Ruan <serena.rxy@gmail.com>
2023-08-24 14:09:58 +0800

    add test for spark session
    Signed-off-by: Serena Ruan <serena.rxy@gmail.com>

--- python/ray/data/tests/test_delta.py
+++ python/ray/data/tests/test_delta.py
@@ -10,9 +10,8 @@ import ray.data
 from ray.data import read_delta
 
 
-@pytest.fixture(scope="module")
-def spark():
-    with (
+def get_or_create_spark_session():
+    return (
         SparkSession.builder.config(
             "spark.jars.packages", "io.delta:delta-core_2.12:2.2.0"
         )
@@ -22,12 +21,16 @@ def spark():
             "org.apache.spark.sql.delta.catalog.DeltaCatalog",
         )
         .getOrCreate()
-    ) as spark:
+    )
+
+
+@pytest.fixture(scope="module")
+def spark():
+    with (get_or_create_spark_session()) as spark:
         yield spark
 
 
 def test_read_delta_basic(spark):
-    spark = SparkSession.builder.getOrCreate()
     spark_df = spark.range(200).repartition(8)
     with tempfile.TemporaryDirectory() as tempdir:
         uri = os.path.join(tempdir, "DeltaTable")
@@ -53,6 +56,18 @@ def test_read_ray_data(spark):
         assert data.schema().names == ["one", "two"]
         assert data.schema().types[0] == "int64"
         assert data.schema().types[1] == "string"
+
+
+def test_existing_spark_session_success():
+    spark = SparkSession.builder.getOrCreate()
+    spark = get_or_create_spark_session()
+    spark_df = spark.range(200).repartition(8)
+    with tempfile.TemporaryDirectory() as tempdir:
+        spark_df.write.format("delta").save(tempdir)
+        data = read_delta(tempdir)
+        assert data.count() == 200
+
+    spark.stop()
 
 
 if __name__ == "__main__":
