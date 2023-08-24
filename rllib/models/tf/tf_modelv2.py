df03eb3a51c5bddc22f4193fdaa4b9ddbba940b8
sven1977 <svenmika1977@gmail.com>
2023-08-24 13:12:27 +0200

    fixes
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/models/tf/tf_modelv2.py
+++ rllib/models/tf/tf_modelv2.py
@@ -6,7 +6,7 @@ from typing import Dict, List, Union
 from ray.util import log_once
 from ray.rllib.models.modelv2 import ModelV2
 from ray.rllib.utils.annotations import override, PublicAPI
-from ray.rllib.utils.deprecation import Deprecated, deprecation_warning
+from ray.rllib.utils.deprecation import deprecation_warning
 from ray.rllib.utils.framework import try_import_tf
 from ray.rllib.utils.typing import ModelConfigDict, TensorType
 
@@ -72,9 +72,11 @@ class TFModelV2(ModelV2):
         For example, this should include any BatchNorm update ops."""
         return []
 
-    @Deprecated(old="TFModelV2.register_variables", error=True)
     def register_variables(self, variables: List[TensorType]) -> None:
-        pass
+        """Register the given list of variables with this model."""
+        if log_once("deprecated_tfmodelv2_register_variables"):
+            deprecation_warning(old="TFModelV2.register_variables", error=False)
+        self.var_list.extend(variables)
 
     @override(ModelV2)
     def variables(
