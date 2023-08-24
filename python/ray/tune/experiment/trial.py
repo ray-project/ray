d4ae3c77e47ec977b2babfa8ddb2fd48f17a8386
Justin Yu <justinvyu@anyscale.com>
2023-08-24 10:37:24 -0700

    [train] New persistence mode: Support the `tune.run(restore="ckpt_path")` API (#38804)
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>
    Signed-off-by: e428265 <arvind.chandramouli@lmco.com>

--- python/ray/tune/experiment/trial.py
+++ python/ray/tune/experiment/trial.py
@@ -549,6 +549,12 @@ class Trial:
 
         # Restoration fields
         self.restore_path = restore_path
+        self._restore_checkpoint_result: Optional[_TrainingResult] = None
+        if restore_path:
+            # tune.run(restore) passes in a path without metrics.
+            self._restore_checkpoint_result = _TrainingResult(
+                checkpoint=Checkpoint.from_directory(restore_path), metrics={}
+            )
 
         if trial_name_creator:
             self.custom_trial_name = trial_name_creator(self)
@@ -833,14 +839,21 @@ class Trial:
         return config.checkpoint_frequency
 
     @property
+    def latest_checkpoint_result(self) -> Optional[_TrainingResult]:
+        # NOTE: Fallback to the checkpoint passed in from `tune.run(restore)`
+        # if the trial hasn't saved any checkpoints itself yet.
+        return (
+            self.run_metadata.checkpoint_manager.latest_checkpoint_result
+            or self._restore_checkpoint_result
+        )
+
+    @property
     def checkpoint(self) -> Optional[Checkpoint]:
         """Returns the most recent checkpoint if one has been saved."""
         if _use_storage_context():
-            checkpoint_manager = self.run_metadata.checkpoint_manager
-            latest_checkpoint_result = checkpoint_manager.latest_checkpoint_result
             return (
-                latest_checkpoint_result.checkpoint
-                if latest_checkpoint_result
+                self.latest_checkpoint_result.checkpoint
+                if self.latest_checkpoint_result
                 else None
             )
 
@@ -1070,9 +1083,19 @@ class Trial:
         )
 
     def has_checkpoint(self):
+        if _use_storage_context():
+            return self.checkpoint is not None
+
         return self.checkpoint.dir_or_data is not None
 
     def clear_checkpoint(self):
+        if _use_storage_context():
+            if self.latest_checkpoint_result:
+                self.latest_checkpoint_result.checkpoint = None
+            self.temporary_state.restoring_from = None
+            self.run_metadata.invalidate_cache()
+            return
+
         self.checkpoint.dir_or_data = None
         self.temporary_state.restoring_from = None
         self.run_metadata.invalidate_cache()
