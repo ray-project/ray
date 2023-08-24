62db08e913dfe0a9b8d989d42e8593d1dfb24b87
Gene Su <e870252314@gmail.com>
2023-08-24 14:54:33 -0700

    WIP: trying...
    Signed-off-by: Gene Su <e870252314@gmail.com>

--- python/ray/serve/benchmarks/proxy_benchmark.py
+++ python/ray/serve/benchmarks/proxy_benchmark.py
@@ -2,6 +2,7 @@
 
 import aiohttp
 import asyncio
+from grpc import aio
 import logging
 from pprint import pprint
 import time
@@ -14,6 +15,8 @@ import ray
 from ray import serve
 from ray.serve.handle import RayServeHandle
 from ray.serve.generated import serve_pb2, serve_pb2_grpc
+from ray.serve.config import gRPCOptions
+from random import random
 
 NUM_CLIENTS = 8
 CALLS_PER_BATCH = 100
@@ -63,9 +66,8 @@ class Client:
 
 def build_app(
     num_replicas: int,
-    max_batch_size: int,
     max_concurrent_queries: int,
-    is_grpc: bool = True,
+    data_size: int,
 ):
     @serve.deployment(max_concurrent_queries=1000)
     class DataPreprocessing:
@@ -76,14 +78,15 @@ def build_app(
             logging.getLogger("ray.serve").setLevel(logging.WARNING)
 
         async def __call__(self, req: Request):
+            # TODO: fixit
             obj_ref = await self._handle.remote(await req.body())
-            return await obj_ref
+            return obj_ref
 
         async def grpc_call(self, raq_data):
             raw = np.asarray(raq_data.nums)
             processed = (raw - np.min(raw)) / (np.max(raw) - np.min(raw))
-            obj_ref = await self._handle.remote(processed)
-            return await obj_ref
+            model_output = await self._handle.remote(processed)
+            return serve_pb2.ModelOutput(output=model_output)
 
     @serve.deployment(
         num_replicas=num_replicas,
@@ -93,100 +96,102 @@ def build_app(
         def __init__(self):
             # Turn off access log.
             logging.getLogger("ray.serve").setLevel(logging.WARNING)
+            self._model = np.random.randn(data_size, data_size)
 
-        @serve.batch(max_batch_size=max_batch_size)
-        async def batch(self, reqs):
-            return [b"ok"] * len(reqs)
-
-        async def __call__(self, req: Union[bytes, Request]):
-            if max_batch_size > 1:
-                return await self.batch(req)
-            else:
-                return b"ok"
+        async def __call__(self, processed: np.ndarray) -> float:
+            model_output = np.dot(processed, self._model)
+            return sum(model_output)
 
     return DataPreprocessing.bind(ModelInference.bind())
 
 
 async def trial(
     num_replicas: int,
-    max_batch_size: int,
     max_concurrent_queries: int,
-    data_size: str,
+    data_size: int,
 ) -> Dict[str, float]:
     results = {}
 
     trial_key_base = (
-        f"replica:{num_replicas}/batch_size:{max_batch_size}/"
+        f"replica:{num_replicas}/"
         f"concurrent_queries:{max_concurrent_queries}/"
-        f"data_size:{data_size}/intermediate_handle:{intermediate_handles}"
+        f"data_size:{data_size}"
     )
 
     print(
         f"num_replicas={num_replicas},"
-        f"max_batch_size={max_batch_size},"
         f"max_concurrent_queries={max_concurrent_queries},"
         f"data_size={data_size}"
     )
 
     app = build_app(
-        intermediate_handles, num_replicas, max_batch_size, max_concurrent_queries
+        num_replicas=num_replicas,
+        max_concurrent_queries=max_concurrent_queries,
+        data_size=data_size,
     )
     serve.run(app)
 
-    if data_size == "small":
-        data = None
-    elif data_size == "large":
-        data = b"a" * 1024 * 1024
-    else:
-        raise ValueError("data_size should be 'small' or 'large'.")
+    data = [random() for _ in range(data_size)]
 
-    async with aiohttp.ClientSession() as session:
+    async with aio.insecure_channel("localhost:9000") as channel:
+        stub = serve_pb2_grpc.RayServeBenchmarkServiceStub(channel)
+        response = await stub.grpc_call(serve_pb2.RawData(nums=data))
+        print("Model output: " + response.output)
 
-        async def single_client():
-            for _ in range(CALLS_PER_BATCH):
-                await fetch(session, data)
-
-        single_client_avg_tps = await timeit(
-            "single client {} data".format(data_size),
-            single_client,
-            multiplier=CALLS_PER_BATCH,
-        )
-        key = f"num_client:1/{trial_key_base}"
-        results[key] = single_client_avg_tps
-
-    clients = [Client.remote() for _ in range(NUM_CLIENTS)]
-    ray.get([client.ready.remote() for client in clients])
-
-    async def many_clients():
-        ray.get([a.do_queries.remote(CALLS_PER_BATCH, data) for a in clients])
-
-    multi_client_avg_tps = await timeit(
-        "{} clients {} data".format(len(clients), data_size),
-        many_clients,
-        multiplier=CALLS_PER_BATCH * len(clients),
-    )
-
-    results[f"num_client:{len(clients)}/{trial_key_base}"] = multi_client_avg_tps
+    # async with aiohttp.ClientSession() as session:
+    #     async def single_client():
+    #         for _ in range(CALLS_PER_BATCH):
+    #             await fetch(session, data)
+    #
+    #     single_client_avg_tps = await timeit(
+    #         "single client {} data".format(data_size),
+    #         single_client,
+    #         multiplier=CALLS_PER_BATCH,
+    #     )
+    #     key = f"num_client:1/{trial_key_base}"
+    #     results[key] = single_client_avg_tps
+    #
+    # clients = [Client.remote() for _ in range(NUM_CLIENTS)]
+    # ray.get([client.ready.remote() for client in clients])
+    #
+    # async def many_clients():
+    #     ray.get([a.do_queries.remote(CALLS_PER_BATCH, data) for a in clients])
+    #
+    # multi_client_avg_tps = await timeit(
+    #     "{} clients {} data".format(len(clients), data_size),
+    #     many_clients,
+    #     multiplier=CALLS_PER_BATCH * len(clients),
+    # )
+    #
+    # results[f"num_client:{len(clients)}/{trial_key_base}"] = multi_client_avg_tps
     return results
 
 
 async def main():
     results = {}
-    for num_replicas in [1, 8]:
-        for max_batch_size, max_concurrent_queries in [
-            (1, 1),
-            (1, 10000),
-            (10000, 10000),
-        ]:
-            for data_size in ["small"]:
-                results.update(
-                    await trial(
-                        num_replicas,
-                        max_batch_size,
-                        max_concurrent_queries,
-                        data_size,
-                    )
-                )
+
+    results.update(
+        await trial(
+            num_replicas=1,
+            max_concurrent_queries=1,
+            data_size=1,
+        )
+    )
+    # for num_replicas in [1, 8]:
+    #     for max_batch_size, max_concurrent_queries in [
+    #         (1, 1),
+    #         (1, 10000),
+    #         (10000, 10000),
+    #     ]:
+    #         for data_size in ["small"]:
+    #             results.update(
+    #                 await trial(
+    #                     num_replicas,
+    #                     max_batch_size,
+    #                     max_concurrent_queries,
+    #                     data_size,
+    #                 )
+    #             )
 
     print("Results from all conditions:")
     pprint(results)
@@ -195,7 +200,18 @@ async def main():
 
 if __name__ == "__main__":
     ray.init()
-    serve.start()
+
+    grpc_port = 9000
+    grpc_servicer_functions = [
+        "ray.serve.generated.serve_pb2_grpc.add_UserDefinedServiceServicer_to_server",
+        "ray.serve.generated.serve_pb2_grpc.add_FruitServiceServicer_to_server",
+    ]
+    serve.start(
+        grpc_options=gRPCOptions(
+            port=grpc_port,
+            grpc_servicer_functions=grpc_servicer_functions,
+        )
+    )
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(main())
