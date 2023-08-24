a306e03a606374ca6dbd4e6de10379e147e5204a
Cheng Su <scnju13@gmail.com>
2023-08-24 10:03:35 -0700

    Disable EliminateBuildOutputBlocks rule temporarily
    Signed-off-by: Cheng Su <scnju13@gmail.com>

--- python/ray/data/_internal/logical/rules/_default_optimizer_rules.py
+++ python/ray/data/_internal/logical/rules/_default_optimizer_rules.py
@@ -1,8 +1,5 @@
 from ray.data._internal.logical.rules.operator_fusion import OperatorFusionRule
 from ray.data._internal.logical.rules.randomize_blocks import ReorderRandomizeBlocksRule
-from ray.data._internal.logical.rules.zero_copy_map_fusion import (
-    EliminateBuildOutputBlocks,
-)
 
 
 def get_logical_optimizer_rules():
@@ -11,7 +8,5 @@ def get_logical_optimizer_rules():
 
 
 def get_physical_optimizer_rules():
-    # Subclasses of ZeroCopyMapFusionRule (e.g., EliminateBuildOutputBlocks) should
-    # be run after OperatorFusionRule.
-    rules = [OperatorFusionRule, EliminateBuildOutputBlocks]
+    rules = [OperatorFusionRule]
     return rules
