348b448519913ced70c7be4537810a5f196521c1
Justin Yu <justinvyu@anyscale.com>
2023-08-24 23:23:55 -0700

    address sync config comments
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/train/_internal/syncer.py
+++ python/ray/train/_internal/syncer.py
@@ -44,33 +44,50 @@ _DEPRECATED_VALUE = "DEPRECATED"
 @PublicAPI
 @dataclass
 class SyncConfig:
-    """Configuration object for Train/Tune file syncing o `RunConfig(storage_path)`.
+    """Configuration object for Train/Tune file syncing to `RunConfig(storage_path)`.
 
     See :ref:`tune-persisted-experiment-data` for an overview of what data is
     synchronized.
 
-    The Tune driver (on the head node) syncs the experiment directory to the cloud
+    In Ray Train/Tune, here is where syncing (mainly uploading) happens:
+
+    The experiment driver (on the head node) syncs the experiment directory to storage
     (which includes experiment state such as searcher state, the list of trials
     and their statuses, and trial metadata).
+
+    For a Ray Tune run with many trials, each trial will upload its trial directory
+    to storage, which includes arbitrary files that you dumped during the run.
+    For a Ray Train run doing distributed training, each remote worker will similarly
+    upload its trial directory to storage.
 
     See :ref:`tune-storage-options` for more details and examples.
 
     Args:
         sync_period: Minimum time in seconds to wait between two sync operations.
-            A smaller ``sync_period`` will have more up-to-date data at the sync
-            location but introduces more syncing overhead.
-            Defaults to 5 minutes.
+            A smaller ``sync_period`` will have the data in storage updated more often
+            but introduces more syncing overhead. Defaults to 5 minutes.
         sync_timeout: Maximum time in seconds to wait for a sync process
-            to finish running. This is used to catch hanging sync operations
-            so that experiment execution can continue and the syncs can be retried.
-            Defaults to 30 minutes.
+            to finish running. A sync operation will run for at most this long
+            before raising a `TimeoutError`. Defaults to 30 minutes.
+        sync_artifacts: Whether or not to sync artifacts that are saved to the
+            trial directory (accessed via `train.get_context().get_trial_dir()`)
+            to the persistent storage configured via `train.RunConfig(storage_path)`.
+            The trial or remote worker will try to launch an artifact syncing
+            operation every time `train.report` happens, subject to `sync_period`
+            and `sync_artifacts_on_checkpoint`.
+            Defaults to False -- no artifacts are persisted by default.
+        sync_artifacts_on_checkpoint: If True, trial/worker artifacts are
+            forcefully synced on every reported checkpoint.
+            This only has an effect if `sync_artifacts` is True.
+            Defaults to True.
     """
 
     upload_dir: Optional[str] = _DEPRECATED_VALUE
     syncer: Optional[Union[str, "Syncer"]] = _DEPRECATED_VALUE
     sync_period: int = DEFAULT_SYNC_PERIOD
     sync_timeout: int = DEFAULT_SYNC_TIMEOUT
-    sync_artifacts: bool = _DEPRECATED_VALUE
+    sync_artifacts: bool = False
+    sync_artifacts_on_checkpoint: bool = True
     sync_on_checkpoint: bool = _DEPRECATED_VALUE
 
     def _deprecation_warning(self, attr_name: str, extra_msg: str):
@@ -195,7 +212,6 @@ class _BackgroundProcess:
         return result
 
 
-@DeveloperAPI
 class Syncer(abc.ABC):
     """Syncer class for synchronizing data between Ray nodes and remote (cloud) storage.
 
