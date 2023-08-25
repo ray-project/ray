026a5afcfac62f18152cbae0b76783121b310728
Hao Chen <chenh1024@gmail.com>
2023-08-25 11:33:31 -0700

    fix
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- python/ray/data/_internal/logical/rules/zero_copy_map_fusion.py
+++ python/ray/data/_internal/logical/rules/zero_copy_map_fusion.py
@@ -3,6 +3,8 @@ from typing import List
 
 from ray.data._internal.execution.operators.map_operator import MapOperator
 from ray.data._internal.execution.operators.map_transformer import (
+    BlocksToBatchesMapTransformFn,
+    BlocksToRowsMapTransformFn,
     BuildOutputBlocksMapTransformFn,
     MapTransformFn,
     MapTransformFnDataType,
@@ -54,17 +56,21 @@ class ZeroCopyMapFusionRule(Rule):
 
 class EliminateBuildOutputBlocks(ZeroCopyMapFusionRule):
     """This rule eliminates unnecessary BuildOutputBlocksMapTransformFn,
-    if the previous fn already outputs blocks.
+    if the previous fn already outputs blocks, and the next fn is
+    BlocksToRowsMapTransformFn or BlocksToBatchesMapTransformFn.
 
-    This happens for the "Read -> Map/Write" fusion.
+    This happens for the "Read -> Map" fusion.
     """
 
     def _optimize(self, transform_fns: List[MapTransformFn]) -> List[MapTransformFn]:
         # For the following subsquence,
         # 1. Any MapTransformFn with block output.
-        # 2. BuildOutputBlocksMapTransformFn
-        # 3. Any MapTransformFn with block input.
+        # 2. BuildOutputBlocksMapTransformFn.
+        # 3. BlocksToRowsMapTransformFn or BlocksToBatchesMapTransformFn
         # We drop the BuildOutputBlocksMapTransformFn in the middle.
+        # Note, for the 3rd fn, we need to check the concrete types, instead of accepting
+        # any fn with block input. This is because for read op with additional split, we
+        # cannot drop the BuildOutputBlocksMapTransformFn.
         new_transform_fns = []
 
         for i in range(len(transform_fns)):
@@ -77,9 +83,8 @@ class EliminateBuildOutputBlocks(ZeroCopyMapFusionRule):
             ):
                 prev_fn = transform_fns[i - 1]
                 next_fn = transform_fns[i + 1]
-                if (
-                    prev_fn.output_type == MapTransformFnDataType.Block
-                    and next_fn.input_type == MapTransformFnDataType.Block
+                if prev_fn.output_type == MapTransformFnDataType.Block and isinstance(
+                    next_fn, (BlocksToRowsMapTransformFn, BlocksToBatchesMapTransformFn)
                 ):
                     drop = True
             if not drop:
