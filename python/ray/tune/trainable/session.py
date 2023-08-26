d279f0fa6aae3bcc8277294ae436492cef604b79
Kai Fricke <kai@anyscale.com>
2023-08-26 15:59:58 +0200

    revert session
    Signed-off-by: Kai Fricke <kai@anyscale.com>

--- python/ray/tune/trainable/session.py
+++ python/ray/tune/trainable/session.py
@@ -207,22 +207,6 @@ def report(_metric=None, **kwargs):
             )
 
         return _session(_metric, **kwargs)
-    else:
-        # Backwards compatibility
-        from ray import train
-
-        if log_once("deprecate_tune_report"):
-            warnings.warn(
-                "`tune.report(**metrics)` is deprecated. Use `train.report(metrics)` "
-                "instead.",
-                DeprecationWarning,
-            )
-
-        report_dict = kwargs
-        if _metric is not None:
-            report_dict["_metric"] = _metric
-
-        train.report(report_dict)
 
 
 @Deprecated(message=_deprecation_msg)
