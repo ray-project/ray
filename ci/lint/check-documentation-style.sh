1d934acc5eaeaf9372d50fbd2a24ea5dd6ffe292
Balaji Veeramani <balaji@anyscale.com>
2023-08-28 15:55:48 -0700

    Change paths
    Signed-off-by: Balaji Veeramani <balaji@anyscale.com>

--- ci/lint/check-documentation-style.sh
+++ ci/lint/check-documentation-style.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 
 wget https://github.com/errata-ai/vale/releases/download/v2.28.0/vale_2.28.0_Linux_64-bit.tar.gz
 tar -xvzf vale_2.28.0_Linux_64-bit.tar.gz -C .
-vale doc/source
+vale doc/source/data
