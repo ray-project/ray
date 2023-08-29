a6ffec8815d3cb58302a0b903a1dc56f4c24d31d
Yunxuan Xiao <xiaoyunxuan1998@gmail.com>
2023-08-28 17:04:52 -0700

    Update horovod_tune_test.py
    Signed-off-by: Yunxuan Xiao <xiaoyunxuan1998@gmail.com>

--- release/air_tests/horovod/workloads/horovod_tune_test.py
+++ release/air_tests/horovod/workloads/horovod_tune_test.py
@@ -192,7 +192,7 @@ if __name__ == "__main__":
         run_config=RunConfig(
             stop={"training_iteration": 1} if args.smoke_test else None,
             failure_config=FailureConfig(fail_fast=False),
-            checkpoint_config=CheckpointConfig(num_to_keep=2),
+            checkpoint_config=CheckpointConfig(num_to_keep=4),
             callbacks=[ProgressCallback()],
             storage_path="/mnt/cluster_storage",
         ),
