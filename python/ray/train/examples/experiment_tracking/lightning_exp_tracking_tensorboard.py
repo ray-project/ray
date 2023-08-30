4ed493e99325d89f462f7839137d70dea2785557
xwjiang2010 <xwjiang2010@gmail.com>
2023-08-30 14:25:43 -0700

    .name
    Signed-off-by: xwjiang2010 <xwjiang2010@gmail.com>

--- python/ray/train/examples/experiment_tracking/lightning_exp_tracking_tensorboard.py
+++ python/ray/train/examples/experiment_tracking/lightning_exp_tracking_tensorboard.py
@@ -3,7 +3,7 @@
 import os
 import tempfile
 
-tempdir = tempfile.TemporaryDirectory()
+tempdir = tempfile.TemporaryDirectory().name
 os.environ["SHARED_STORAGE_PATH"] = tempdir
 
 from lightning_exp_tracking_model_dl import DummyModel, dataloader
