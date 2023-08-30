73b7a8d9171eb96354756c294eb834e42bc16392
Balaji Veeramani <balaji@anyscale.com>
2023-08-30 12:10:49 -0700

    Address review comments
    Signed-off-by: Balaji Veeramani <balaji@anyscale.com>

--- ci/lint/check-documentation-style.sh
+++ ci/lint/check-documentation-style.sh
@@ -2,8 +2,8 @@
 
 set -euxo pipefail
 
-wget https://github.com/errata-ai/vale/releases/download/v2.28.0/vale_2.28.0_Linux_64-bit.tar.gz
 VALE_BIN=$(mktemp -d)
-tar -xvzf vale_2.28.0_Linux_64-bit.tar.gz -C "$VALE_BIN" vale
+wget https://github.com/errata-ai/vale/releases/download/v2.28.0/vale_2.28.0_Linux_64-bit.tar.gz -P "$VALE_BIN"
+tar -xvzf "$VALE_BIN"/vale_2.28.0_Linux_64-bit.tar.gz -C "$VALE_BIN" vale
 "$VALE_BIN"/vale doc/source/data
 rm -rf "$VALE_BIN"
