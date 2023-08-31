be6395c01f0e78a13b4562c83c8f7e2f4d0bfc13
amogkam <amogkamsetty@yahoo.com>
2023-08-31 11:56:13 -0700

    wip
    Signed-off-by: amogkam <amogkamsetty@yahoo.com>

--- python/ray/data/_internal/remote_fn.py
+++ python/ray/data/_internal/remote_fn.py
@@ -4,6 +4,24 @@ import ray
 
 CACHED_FUNCTIONS = {}
 
+# Default remote args for read and write tasks.
+# Retry exceptions by default to handle transient errors 
+# (such as S3 connection failures).
+# Use the default scheduling strategy for all tasks so that we will
+# not inherit a placement group from the caller, if there is one.
+# The caller of this function may override the scheduling strategy
+# as needed.
+DEFAULT_READ_WRITE_REMOTE_ARGS = {
+    "retry_exceptions": False,
+    "scheduling_strategy": "DEFAULT"
+}
+
+DEFAULT_TASK_REMOTE_ARGS = {}
+    
+DEFAULT_ACTOR_REMOTE_ARGS = {}
+
+DEFAULT_ACTOR_TASK_REMOTE_ARGS = {}
+
 
 def cached_remote_fn(fn: Any, **ray_remote_args) -> Any:
     """Lazily defines a ray.remote function.
