71c4117748aeaf7a9a01dc6756b5f5516fcb88fa
matthewdeng <matt@anyscale.com>
2023-09-01 07:35:41 -0700

    [train][2.7][2/n] cherry-picks for documentations, tests, examples (#39195)
    * [CI] Remove tags for AIR and AIR smoke test (#39075)
    
    Signed-off-by: woshiyyya <xiaoyunxuan1998@gmail.com>
    
    * [train] New persistence mode: Finish migrating `Tune tests + examples (small)` (#39047)
    
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>
    
    * [train] Rename `train(config)` to `train_fn(config)` (#39065)
    
    Our API change from `tune.report()` to `train.report()` can lead to namespace clashes when the training function is called `train`.
    
    This has been an issue in many test migrations, including #39050 and the current failure of #38493.
    
    This PR does a global replace of all training function defined as `def train(config)` with `def train_fn(config)` to avoid future clashes.
    
    Signed-off-by: Kai Fricke <kai@anyscale.com>
    
    * [Data/Train] [Docs] Re-organize data loading performance tips (#39096)
    
    Re-organize data loading performance tips. We want the caching and the CPU nodes sections to be together since they are both addressing the same problems of optimizing performance when you have expensive CPU preprocessing, and the latter references the former.
    
    Signed-off-by: Amog Kamsetty <amogkam@users.noreply.github.com>
    
    * [air] Hard deprecate PredictorDeployment and PredictorWrapper (#39108)
    
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>
    
    * Update the DeepSpeed and Accelerate doc example with new Checkpoint API. (#39014)
    
    Signed-off-by: woshiyyya <xiaoyunxuan1998@gmail.com>
    
    * [train] New persistence mode: Remove some legacy `air.Checkpoint` dependencies (#39049)
    
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>
    
    * [train] Fix wandb/comet integration API calls (#38978)
    
    Removes remaining calls to checkpoint.dir_or_data in the wandb/comet integrations
    
    Signed-off-by: Kai Fricke <kai@anyscale.com>
    
    * [tune] Deprecate `tune.report`, `tune.checkpoint_dir`, `checkpoint_dir`, and `reporter` (#39093)
    
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>
    
    * [2.7][Example] Enable new APIs for Lightning `dolly-v2-7b` Fine-tuning Example (#39117)
    
    Signed-off-by: Yunxuan Xiao <xiaoyunxuan1998@gmail.com>
    Co-authored-by: matthewdeng <matthew.j.deng@gmail.com>
    
    * [train] New persistence mode: Re-enable py37 compatibility tests (#39121)
    
    
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>
    
    * [Ray 2.7 Examples][1/n] Revamp the LightningTrainer CoLA Example (#38009)
    
    Signed-off-by: Yunxuan Xiao <xiaoyunxuan1998@gmail.com>
    Co-authored-by: matthewdeng <matthew.j.deng@gmail.com>
    
    * [train] New persistence mode: Support `chdir_to_trial_dir` functionality with `RAY_CHDIR_TO_TRIAL_DIR` env var (#39107)
    
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>
    
    * [train] New persistence mode: Minimal `BackendExecutor` cleanup (#39187)
    
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>
    
    * [train/rllib] RLlib GPU storage context tests (#39166)
    
    Signed-off-by: Kai Fricke <kai@anyscale.com>
    Co-authored-by: matthewdeng <matthew.j.deng@gmail.com>
    
    * [docs][train] Update Train landing and Overview pages (#38808)
    
    Signed-off-by: angelinalg <122562471+angelinalg@users.noreply.github.com>
    Co-authored-by: matthewdeng <matthew.j.deng@gmail.com>
    
    ---------
    
    Signed-off-by: woshiyyya <xiaoyunxuan1998@gmail.com>
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>
    Signed-off-by: Kai Fricke <kai@anyscale.com>
    Signed-off-by: Amog Kamsetty <amogkam@users.noreply.github.com>
    Signed-off-by: Yunxuan Xiao <xiaoyunxuan1998@gmail.com>
    Signed-off-by: angelinalg <122562471+angelinalg@users.noreply.github.com>
    Co-authored-by: Yunxuan Xiao <yunxuanx@anyscale.com>
    Co-authored-by: Justin Yu <justinvyu@anyscale.com>
    Co-authored-by: Kai Fricke <krfricke@users.noreply.github.com>
    Co-authored-by: Amog Kamsetty <amogkam@users.noreply.github.com>
    Co-authored-by: angelinalg <122562471+angelinalg@users.noreply.github.com>

--- python/ray/tune/tests/test_trial_scheduler.py
+++ python/ray/tune/tests/test_trial_scheduler.py
@@ -2005,7 +2005,7 @@ class PopulationBasedTestingSuite(unittest.TestCase):
                 print("Cleaned up.", self.config)
                 self.active = False
 
-        def train(config):
+        def train_fn(config):
             with MockContext(config):
                 for i in range(10):
                     tune.report(metric=i + config["x"])
@@ -2016,7 +2016,9 @@ class PopulationBasedTestingSuite(unittest.TestCase):
 
         scheduler = MockScheduler()
 
-        out = tune.run(train, config={"x": tune.grid_search(vals)}, scheduler=scheduler)
+        out = tune.run(
+            train_fn, config={"x": tune.grid_search(vals)}, scheduler=scheduler
+        )
 
         ever_active = set()
         active = set()
@@ -2479,11 +2481,11 @@ class AsyncHyperBandSuite(unittest.TestCase):
         self._test_metrics(result2, "mean_loss", "min")
 
     def _testAnonymousMetricEndToEnd(self, scheduler_cls, searcher=None):
-        def train(config):
+        def train_fn(config):
             return config["value"]
 
         out = tune.run(
-            train,
+            train_fn,
             mode="max",
             num_samples=1,
             config={"value": tune.uniform(-2.0, 2.0)},
