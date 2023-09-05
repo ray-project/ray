6a31af92e3c4b965e773c9c4cb8ca7e7ac1e86ff
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2023-09-05 12:47:53 -0700

    [core] Ship the libjemalloc.so with wheel (#39241)
    ## Why are these changes needed?
    In [PR](https://github.com/ray-project/ray/pull/39192), jemalloc is added, but it's not delivered with the wheel. This PR fixed it.
    
    Signed-off-by: Victor <vctr.y.m@example.com>

--- python/setup.py
+++ python/setup.py
@@ -168,6 +168,9 @@ ray_files = [
     "ray/core/src/ray/raylet/raylet" + exe_suffix,
 ]
 
+if sys.platform == "linux":
+    ray_files.append("ray/core/libjemalloc.so")
+
 if BUILD_JAVA or os.path.exists(os.path.join(ROOT_DIR, "ray/jars/ray_dist.jar")):
     ray_files.append("ray/jars/ray_dist.jar")
 
