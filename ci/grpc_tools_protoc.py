c8f87c7cdc51bc2e633d95cfbe6cc7c29e815da0
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2023-09-06 01:57:01 +0000

    fix Signed-off-by: Yi Cheng <74173148+iycheng@users.noreply.github.com>

--- ci/grpc_tools_protoc.py
+++ ci/grpc_tools_protoc.py
@@ -1,7 +1,6 @@
-import subprocess
 import sys
 
 if __name__ == "__main__":
-    args = [sys.executable, "-m", "grpc_tools.protoc"]
-    args.extend(sys.argv[1:])
-    subprocess.check_call(args, stdout=sys.stdout, stderr=sys.stderr)
+    import grpc_tools.protoc
+
+    sys.exit(grpc_tools.protoc.main(sys.argv[1:]))
