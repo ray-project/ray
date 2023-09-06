d2f7c6d1af1af2f4c5717d23ed1b3439435bfe2f
Balaji Veeramani <balaji@anyscale.com>
2023-09-06 13:37:42 -0700

    [Data] Optimize audio benchmark (#140)
    This PR optimizes the audio benchmark by:
    1. Maximizing inference batch size.
    2. Moving preprocessing outside of GPU tasks.

--- release/anyscale/data/audio_batch_inference_benchmark.py
+++ release/anyscale/data/audio_batch_inference_benchmark.py
@@ -23,7 +23,7 @@ def parse_args():
 
     parser.add_argument(
         "--batch-size",
-        default=32,
+        default=110,
         type=int,
         help="Batch size to use.",
     )
@@ -39,16 +39,20 @@ def run_benchmark(args):
     ray.init()
     actor_pool_size = int(ray.cluster_resources().get("GPU"))
 
-    ds = ray.data.read_datasource(
-        AudioDatasource(),
-        paths=DATA_URI,
-        include_paths=True,
-        partition_filter=FileExtensionFilter("flac"),
-    ).map_batches(
-        BatchInference,
-        compute=ray.data.ActorPoolStrategy(size=actor_pool_size),
-        batch_size=args.batch_size,
-        num_gpus=1,
+    ds = (
+        ray.data.read_datasource(
+            AudioDatasource(),
+            paths=DATA_URI,
+            include_paths=True,
+            partition_filter=FileExtensionFilter("flac"),
+        )
+        .map(preprocess)
+        .map_batches(
+            BatchInference,
+            compute=ray.data.ActorPoolStrategy(size=actor_pool_size),
+            batch_size=args.batch_size,
+            num_gpus=1,
+        )
     )
 
     num_files = 0
@@ -80,32 +84,32 @@ def run_benchmark(args):
     print(results)
 
 
+def preprocess(row):
+    assert row["amplitude"].shape[0] == 1, (
+        "OpenAI Whisper only supports mono audio,"
+        f" but found {row['amplitude'].shape[0]} channels."
+    )
+    processed_amplitude = whisper.pad_or_trim(np.squeeze(row["amplitude"], axis=0))
+    row["audio_size_bytes"] = processed_amplitude.nbytes
+    row["mel"] = whisper.log_mel_spectrogram(processed_amplitude)
+    del row["amplitude"]
+    return row
+
+
 class BatchInference:
     def __init__(self):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model = whisper.load_model("base", self.device)
+        self.options = whisper.DecodingOptions()
 
     def __call__(self, batch: Dict[str, np.ndarray]):
-        inputs = []
-        audio_sizes_bytes = []
-        for audio_data in batch["amplitude"]:
-            assert audio_data.shape[0] == 1, (
-                "OpenAI Whisper only supports mono audio,"
-                f" but found {audio_data.shape[0]} channels."
-            )
-            processed_audio = whisper.pad_or_trim(np.squeeze(audio_data, axis=0))
-            mel = whisper.log_mel_spectrogram(processed_audio).to(self.device)
-            inputs.append(mel)
-            audio_sizes_bytes.append(processed_audio.nbytes)
-
-        inputs = torch.stack(inputs)
-        options = whisper.DecodingOptions()
+        inputs = torch.as_tensor(batch["mel"], device=self.device)
         # Pass the entire batch of processed inputs into the model at once.
-        outputs = whisper.decode(self.model, inputs, options)
+        outputs = whisper.decode(self.model, inputs, self.options)
         return {
             "path": batch["path"],
+            "audio_size_bytes": batch["audio_size_bytes"],
             "transcribed_text": [result.text for result in outputs],
-            "audio_size_bytes": audio_sizes_bytes,
         }
 
 
