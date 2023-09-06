f7eeda7c37b9ee95446661e1682e5812609408b4
can <can@anyscale.com>
2023-09-06 07:13:26 +0000

    move arrow 12 data tests to civ2
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/test_tester_container.py
+++ ci/ray_ci/test_tester_container.py
@@ -21,6 +21,19 @@ class MockPopen:
         return 0 if self.test_targets else 1
 
 
+def test_run_tests_in_docker() -> None:
+    def _mock_popen(input: List[str]) -> None:
+        input_str = " ".join(input)
+        assert (
+            "bazel test --config=ci $(./ci/run/bazel_export_options) "
+            "--test_env v=k t1 t2" in input_str
+        )
+
+    with mock.patch("subprocess.Popen", side_effect=_mock_popen):
+        container = TesterContainer("team")
+        container._run_tests_in_docker(["t1", "t2"], ["v=k"])
+
+
 def test_run_script_in_docker() -> None:
     def _mock_check_output(input: List[str]) -> None:
         input_str = " ".join(input)
@@ -32,7 +45,10 @@ def test_run_script_in_docker() -> None:
 
 
 def test_run_tests() -> None:
-    def _mock_run_tests_in_docker(test_targets: List[str]) -> MockPopen:
+    def _mock_run_tests_in_docker(
+        test_targets: List[str],
+        test_envs: List[str],
+    ) -> MockPopen:
         return MockPopen(test_targets)
 
     def _mock_shard_tests(tests: List[str], workers: int, worker_id: int) -> List[str]:
@@ -46,9 +62,9 @@ def test_run_tests() -> None:
     ):
         container = TesterContainer("team")
         # test_targets are not empty
-        assert container.run_tests(["t1", "t2"], 2)
+        assert container.run_tests(["t1", "t2"], [], 2)
         # test_targets is empty after chunking
-        assert not container.run_tests(["t1"], 2)
+        assert not container.run_tests(["t1"], [], 2)
 
 
 if __name__ == "__main__":
