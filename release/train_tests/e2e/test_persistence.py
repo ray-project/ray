61a2fa1a408cc74575d04cb4e2dd1746390c6622
Justin Yu <justinvyu@anyscale.com>
2023-09-06 13:09:10 -0700

    update
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- release/train_tests/e2e/test_persistence.py
+++ release/train_tests/e2e/test_persistence.py
@@ -1,14 +1,18 @@
+import collections
 from contextlib import contextmanager
 import json
 import os
 import pickle
 import time
+from typing import Any, Dict
 
+import numpy as np
 import pytest
 import torch
 import torch.distributed as dist
 
 from ray import train
+from ray._private.dict import flatten_dict
 from ray.train import Checkpoint
 from ray.train.torch import TorchTrainer
 from ray.train._internal.storage import (
@@ -31,6 +35,18 @@ class TestConstants:
     NUM_WORKERS = 8
 
     SCORE_KEY = "score"
+
+
+def update_output_json(metrics: Dict[str, Any]):
+    test_output_json = os.environ.get("TEST_OUTPUT_JSON", "/tmp/release_test_out.json")
+    data = {}
+    if os.path.exists(test_output_json):
+        with open(test_output_json, "r") as f:
+            data = json.load(f)
+
+    data.update(metrics)
+    with open(test_output_json, "w") as f:
+        json.dump(data, f)
 
 
 def create_checkpoint(checkpoint_dir) -> float:
@@ -64,6 +80,7 @@ def custom_restore_fn(checkpoint: Checkpoint):
         if train.get_context().get_world_rank() == 0:
             checkpoint.update_metadata({"time_to_load": time_tensor.item()})
 
+        # This is a file populated by the
         with open(os.path.join(checkpoint_dir, "checkpoint.pkl"), "rb") as f:
             state = pickle.load(f)
             return state
@@ -92,29 +109,19 @@ def custom_save_fn(temp_checkpoint_dir: str):
 
 
 @pytest.mark.parametrize(
-    "checkpoint_config",
+    "storage_path_storage_filesystem_label",
     [
-        train.CheckpointConfig(),
-        train.CheckpointConfig(
-            num_to_keep=1,
-            checkpoint_score_attribute=TestConstants.SCORE_KEY,
-            checkpoint_score_order="max",
-        ),
-    ],
-)
-@pytest.mark.parametrize(
-    "storage_path_storage_filesystem",
-    [
-        (os.environ["ANYSCALE_ARTIFACT_STORAGE"] + "/justinvyu-testing", None),
-        ("/mnt/cluster_storage", None),
+        (os.environ["ANYSCALE_ARTIFACT_STORAGE"] + "/justinvyu-testing", None, "cloud"),
+        ("/mnt/cluster_storage", None, "nfs"),
     ],
 )
 def test_trainer(
-    storage_path_storage_filesystem, checkpoint_config, tmp_path, monkeypatch
+    storage_path_storage_filesystem_label, checkpoint_config, tmp_path, monkeypatch
 ):
     monkeypatch.setenv("RAY_AIR_LOCAL_CACHE_DIR", str(tmp_path / "ray_results"))
 
-    storage_path, storage_filesystem = storage_path_storage_filesystem
+    storage_path, storage_filesystem, label = storage_path_storage_filesystem_label
+    checkpoint_config = train.CheckpointConfig(num_to_keep=4)
     exp_name = "test_trainer"
 
     # Delete the existing contents at the storage path (ex: from previous runs)
@@ -126,7 +133,7 @@ def test_trainer(
     trainer = TorchTrainer(
         train_fn,
         train_loop_config={
-            "fail_iters": [3],
+            "fail_iters": [2, 4],
             "num_iterations": TestConstants.NUM_ITERATIONS,
             "custom_save_fn": custom_save_fn,
             "custom_restore_fn": custom_restore_fn,
@@ -145,7 +152,42 @@ def test_trainer(
             sync_config=train.SyncConfig(sync_artifacts=True),
         ),
     )
+    print("\nStarting initial run.\n")
+    # with pytest.raises(TrainingFailedError):
     result = trainer.fit()
+
+    # print("\nStarting manually restored run.\n")
+    # restored_trainer = TorchTrainer.restore(
+    #     path=str(URI(storage_path or str(LOCAL_CACHE_DIR)) / exp_name),
+    #     storage_filesystem=storage_filesystem,
+    # )
+    # result = restored_trainer.fit()
 
     local_inspect_dir = tmp_path / "inspect_dir"
     _download_from_fs_path(fs, storage_fs_path, str(local_inspect_dir))
+    _assert_storage_contents(
+        local_inspect_dir,
+        exp_name,
+        checkpoint_config,
+        "TorchTrainer",
+        test_trainer=True,
+        constants=TestConstants,
+    )
+
+    all_checkpoint_timing_metrics = collections.defaultdict(list)
+    for checkpoint, _ in result.best_checkpoints:
+        metadata = checkpoint.get_metadata()
+        for metric, value in metadata.items():
+            all_checkpoint_timing_metrics[metric].append(value)
+
+    aggregated_metrics = {
+        key: np.mean(values) for key, values in all_checkpoint_timing_metrics.items()
+    }
+    print(aggregated_metrics)
+    update_output_json(flatten_dict({label: aggregated_metrics}))
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(pytest.main(["-v", __file__]))
