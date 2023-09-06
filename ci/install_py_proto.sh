b6e14b57519a4f217dbba40cbc5781a5a8e81853
Jiajun Yao <jeromeyjj@gmail.com>
2023-09-05 23:38:00 -0700

    lint
    Signed-off-by: Jiajun Yao <jeromeyjj@gmail.com>

--- ci/install_py_proto.sh
+++ ci/install_py_proto.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-mkdir -p ${BUILD_WORKSPACE_DIRECTORY}/python/ray/core/generated
-mkdir -p ${BUILD_WORKSPACE_DIRECTORY}/python/ray/serve/generated
+mkdir -p "${BUILD_WORKSPACE_DIRECTORY}"/python/ray/core/generated
+mkdir -p "${BUILD_WORKSPACE_DIRECTORY}"/python/ray/serve/generated
 
-tar -xvf $1 -C ${BUILD_WORKSPACE_DIRECTORY}/python/ray/core/generated
-mv ${BUILD_WORKSPACE_DIRECTORY}/python/ray/core/generated/serve_*.py ${BUILD_WORKSPACE_DIRECTORY}/python/ray/serve/generated
+tar -xvf "$1" -C "${BUILD_WORKSPACE_DIRECTORY}"/python/ray/core/generated
+mv "${BUILD_WORKSPACE_DIRECTORY}"/python/ray/core/generated/serve_*.py "${BUILD_WORKSPACE_DIRECTORY}"/python/ray/serve/generated
