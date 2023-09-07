c5d12db4c7993e34aabc05260dfb849bc9e2fd7f
Ruiyang Wang <rywang014@gmail.com>
2023-09-07 10:01:53 -0700

    make the default timeout to 1s
    Signed-off-by: Ruiyang Wang <rywang014@gmail.com>

--- src/ray/common/ray_config_def.h
+++ src/ray/common/ray_config_def.h
@@ -447,9 +447,11 @@ RAY_CONFIG(int32_t, minimum_gcs_reconnect_interval_milliseconds, 5000)
 
 /// gRPC channel reconnection related configs to GCS.
 /// Check https://grpc.github.io/grpc/core/group__grpc__arg__keys.html for details
+/// Note: `gcs_grpc_min_reconnect_backoff_ms` is (mis)used by gRPC as the connection
+/// timeout. If your cluster has a high latency, make these values larger.
 RAY_CONFIG(int32_t, gcs_grpc_max_reconnect_backoff_ms, 2000)
-RAY_CONFIG(int32_t, gcs_grpc_min_reconnect_backoff_ms, 500)
-RAY_CONFIG(int32_t, gcs_grpc_initial_reconnect_backoff_ms, 500)
+RAY_CONFIG(int32_t, gcs_grpc_min_reconnect_backoff_ms, 1000)
+RAY_CONFIG(int32_t, gcs_grpc_initial_reconnect_backoff_ms, 1000)
 
 /// Maximum bytes of request queued when RPC failed due to GCS is down.
 /// If reach the limit, the core worker will hang until GCS is reconnected.
