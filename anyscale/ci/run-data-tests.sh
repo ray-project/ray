8caeb0ff5cdc08ed9a2087b49323375cf668d2a1
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2023-09-07 12:10:13 -0700

    [ci] remove upstream data tests (#144)
    already captured by upstream ci settings

--- anyscale/ci/run-data-tests.sh
+++ anyscale/ci/run-data-tests.sh
@@ -16,8 +16,4 @@ bash ./ci/env/env_info.sh
 
 bazel test --config=ci --action_env=RAY_DATA_USE_STREAMING_EXECUTOR=1 \
 	--test_tag_filters=-data_integration,-doctest \
-	-- python/ray/data/... python/ray/anyscale/data/...
-
-bazel test --config=ci --action_env=RAY_DATA_USE_STREAMING_EXECUTOR=1 \
-	--test_tag_filters=ray_data,-doctest \
-	-- python/ray/air/...
+	-- python/ray/anyscale/data/...
