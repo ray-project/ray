8d81d6dfb5caa01412777e20700f7b5956d3c47b
xwjiang2010 <xwjiang2010@gmail.com>
2023-09-07 13:02:53 -0700

    update scaling config
    Signed-off-by: xwjiang2010 <xwjiang2010@gmail.com>

--- python/ray/train/examples/experiment_tracking/torch_exp_tracking_mlflow.py
+++ python/ray/train/examples/experiment_tracking/torch_exp_tracking_mlflow.py
@@ -72,7 +72,7 @@ trainer = TorchTrainer(
     train_loop_config={
         "save_dir": os.path.join(os.environ["SHARED_STORAGE_PATH"], "mlruns")
     },
-    scaling_config=ScalingConfig(num_workers=4),
+    scaling_config=ScalingConfig(num_workers=2),
 )
 trainer.fit()
 # __end__
