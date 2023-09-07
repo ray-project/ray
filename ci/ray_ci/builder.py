b07999f3ee66185639e654e0d83a2c378c24c780
can <can@anyscale.com>
2023-09-07 01:18:37 +0000

    build docker image
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/builder.py
+++ ci/ray_ci/builder.py
@@ -1,6 +1,7 @@
 import click
 
 from ci.ray_ci.builder_container import PYTHON_VERSIONS, BuilderContainer
+from ci.ray_ci.docker_container import DockerContainer
 from ci.ray_ci.forge_container import ForgeContainer
 from ci.ray_ci.container import _DOCKER_ECR_REPO
 from ci.ray_ci.utils import logger, docker_login
@@ -10,7 +11,7 @@ from ci.ray_ci.utils import logger, docker_login
 @click.argument(
     "artifact_type",
     required=True,
-    type=click.Choice(["wheel", "jar"]),
+    type=click.Choice(["wheel", "docker"]),
 )
 @click.option(
     "--python-version",
@@ -28,10 +29,28 @@ def main(
     docker_login(_DOCKER_ECR_REPO.split("/")[0])
     if artifact_type == "wheel":
         logger.info(f"Building wheel for Python {python_version}")
-        BuilderContainer(python_version).run()
-        ForgeContainer().upload_wheel()
-        logger.info("Successfully built wheel. Artifacts are in ray/.whl")
-    elif artifact_type == "jar":
-        raise NotImplementedError("Jar build not implemented yet")
-    else:
-        raise ValueError(f"Invalid artifact type {artifact_type}")
+        build_wheel(python_version)
+        return
+
+    if artifact_type == "docker":
+        logger.info(f"Building ray-cpu docker for Python {python_version}")
+        build_docker(python_version)
+        return
+
+    raise ValueError(f"Invalid artifact type {artifact_type}")
+
+
+def build_wheel(python_version: str) -> None:
+    """
+    Build a wheel artifact
+    """
+    BuilderContainer(python_version).run()
+    ForgeContainer().upload_wheel()
+
+
+def build_docker(python_version: str) -> None:
+    """
+    Build a container artifact
+    """
+    BuilderContainer(python_version).run()
+    DockerContainer(python_version).run()
