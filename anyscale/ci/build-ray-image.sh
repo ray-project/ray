695418bbf62f9b75e46192ed13f57dc042986089
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2023-09-08 18:02:04 -0700

    [ci] auto build byod images on runtime CI (#121)
    for easy testing and debugging for runtime developers, as runtime devs
    do not have publicly downloadable wheels and hence cannot use build
    farm.

--- anyscale/ci/build-ray-image.sh
+++ anyscale/ci/build-ray-image.sh
@@ -2,32 +2,48 @@
 
 set -euo pipefail
 
-PYTHON_VERSION_CODE="$1"
-IMAGE_TYPE="$2"
-ML_CUDA_VERSION="cu118"
+PY_VERSION_CODE="$1"
+IMG_TYPE="$2"
+: "${RAY_VERSION:=3.0.0.dev0}"
+
+function docker_push_as {
+    local SRC_IMG="$1"
+    local DEST_IMG="$2"
+    docker tag "${SRC_IMG}" "${DEST_IMG}"
+    docker push "${DEST_IMG}"
+    buildkite-agent annotate --style=info \
+        --context="${PY_VERSION_CODE}-images" --append "${DEST_IMG}<br/>"
+}
+
+function docker_push {
+    local IMG="$1"
+    docker push "${IMG}"
+    buildkite-agent annotate --style=info \
+        --context="${PY_VERSION_CODE}-images" --append "${IMG}<br/>"
+}
+
+readonly ML_CUDA_VERSION="cu118"
 
 if [[ "$(uname -m)" == "x86_64" ]]; then
     HOSTTYPE="x86_64"
-    IMAGE_SUFFIX=""
+    IMG_SUFFIX=""
 else
     HOSTTYPE="aarch64"
-    IMAGE_SUFFIX="-aarch64"
+    IMG_SUFFIX="-aarch64"
 fi
 
-: "${RAY_VERSION:=3.0.0.dev0}"
-
-if [[ "${PYTHON_VERSION_CODE}" == "py37" ]]; then
+if [[ "${PY_VERSION_CODE}" == "py37" ]]; then
     WHEEL_FILE="ray-${RAY_VERSION}-cp37-cp37m-manylinux2014_${HOSTTYPE}.whl"
-elif [[ "${PYTHON_VERSION_CODE}" == "py38" ]]; then
+elif [[ "${PY_VERSION_CODE}" == "py38" ]]; then
     WHEEL_FILE="ray-${RAY_VERSION}-cp38-cp38-manylinux2014_${HOSTTYPE}.whl"
-elif [[ "${PYTHON_VERSION_CODE}" == "py39" ]]; then
+elif [[ "${PY_VERSION_CODE}" == "py39" ]]; then
     WHEEL_FILE="ray-${RAY_VERSION}-cp39-cp39-manylinux2014_${HOSTTYPE}.whl"
-elif [[ "${PYTHON_VERSION_CODE}" == "py310" ]]; then
+elif [[ "${PY_VERSION_CODE}" == "py310" ]]; then
     WHEEL_FILE="ray-${RAY_VERSION}-cp310-cp310-manylinux2014_${HOSTTYPE}.whl"
-elif [[ "${PYTHON_VERSION_CODE}" == "py311" ]]; then
+elif [[ "${PY_VERSION_CODE}" == "py311" ]]; then
     WHEEL_FILE="ray-${RAY_VERSION}-cp311-cp311-manylinux2014_${HOSTTYPE}.whl"
 else
-    echo "Unknown python version code: ${PYTHON_VERSION_CODE}" >/dev/stderr
+    echo "Unknown python version code: ${PY_VERSION_CODE}" >/dev/stderr
     exit 1
 fi
 
@@ -39,7 +55,9 @@ if [[ "${PUSH_COMMIT_TAGS:-}" == "" ]]; then
     fi
 fi
 
-mkdir -p .whl
+BUILD_TMP="$(mktemp -d)"
+
+mkdir -p "${BUILD_TMP}/.whl"
 
 FULL_COMMIT="$(git rev-parse HEAD)"
 SHORT_COMMIT="${FULL_COMMIT:0:6}"  # Use 6 chars to be consistent with Ray upstream
@@ -50,28 +68,44 @@ fi
 
 echo "--- Fetch wheel and base image"
 
-aws s3 cp "${S3_TEMP}/${WHEEL_FILE}" ".whl/${WHEEL_FILE}"
+aws s3 cp "${S3_TEMP}/${WHEEL_FILE}" "${BUILD_TMP}/.whl/${WHEEL_FILE}"
+aws s3 cp "s3://runtime-release-test-artifacts/dataplane/20230831/dataplane.tar.gz" "${BUILD_TMP}/dataplane.tar.gz"
+DATAPLANE_TGZ_GOT="$(sha256sum "${BUILD_TMP}/dataplane.tar.gz" | cut -d' ' -f1)"
+DATAPLANE_TGZ_WANT="3658592a3b30a93b2e940269f99296e822446eed7ef36049d707b435ff868638"
+if [[ "${DATAPLANE_TGZ_GOT}" != "${DATAPLANE_TGZ_WANT}" ]]; then
+    echo "Dataplane tarball sha256 digest:" \
+        "got ${DATAPLANE_TGZ_GOT}, want ${DATAPLANE_TGZ_WANT}" >/dev/stderr
+    exit 1
+fi
 
-BASE_IMAGE="${CI_TMP_REPO}:${IMAGE_PREFIX}-base-${PYTHON_VERSION_CODE}-${IMAGE_TYPE}${IMAGE_SUFFIX}"
+BASE_IMG="${CI_TMP_REPO}:${IMAGE_PREFIX}-base-${PY_VERSION_CODE}-${IMG_TYPE}${IMG_SUFFIX}"
 
-docker pull "${BASE_IMAGE}"
+docker pull "${BASE_IMG}"
 # This retagging is required because the Dockerfile hardcodes the base image.
-docker tag "${BASE_IMAGE}" "rayproject/ray-deps:nightly-${PYTHON_VERSION_CODE}-${IMAGE_TYPE}"
+docker tag "${BASE_IMG}" "rayproject/ray-deps:nightly-${PY_VERSION_CODE}-${IMG_TYPE}"
 
-DEST_IMAGE="${RUNTIME_REPO}:${IMAGE_PREFIX}-${PYTHON_VERSION_CODE}-${IMAGE_TYPE}${IMAGE_SUFFIX}"
+# Everything is prepared, starts building now.
 
-echo "--- Build ${DEST_IMAGE}"
+export DOCKER_BUILDKIT=1
+
+BUILD_TAG="${IMAGE_PREFIX}-${PY_VERSION_CODE}-${IMG_TYPE}${IMG_SUFFIX}"
+COMMIT_TAG="${SHORT_COMMIT}-${PY_VERSION_CODE}-${IMG_TYPE}${IMG_SUFFIX}"
+COMMIT_GPU_TAG="${SHORT_COMMIT}-${PY_VERSION_CODE}-gpu${IMG_SUFFIX}"
+RAY_IMG="${RUNTIME_REPO}:${BUILD_TAG}"
+ANYSCALE_IMG="${RUNTIME_REPO}:${BUILD_TAG}-as"
+
+echo "--- Build ${RAY_IMG}"
 
 CPU_TMP="$(mktemp -d)"
 
 mkdir -p "${CPU_TMP}/.whl"
 
-cp ".whl/${WHEEL_FILE}" "${CPU_TMP}/.whl/${WHEEL_FILE}"
+cp "${BUILD_TMP}/.whl/${WHEEL_FILE}" "${CPU_TMP}/.whl/${WHEEL_FILE}"
 cp docker/ray/Dockerfile "${CPU_TMP}/Dockerfile"
 cp python/requirements_compiled.txt "${CPU_TMP}/."
 cp python/requirements_compiled_py37.txt "${CPU_TMP}/."
 
-if [[ "${PYTHON_VERSION_CODE}" == "py37" ]]; then
+if [[ "${PY_VERSION_CODE}" == "py37" ]]; then
     CONSTRAINTS_FILE="requirements_compiled_py37.txt"
 else
     CONSTRAINTS_FILE="requirements_compiled.txt"
@@ -81,46 +115,44 @@ fi
     cd "${CPU_TMP}"
     tar --mtime="UTC 2020-01-01" -c -f - . \
         | docker build --progress=plain \
-            --build-arg BASE_IMAGE="-${PYTHON_VERSION_CODE}-${IMAGE_TYPE}" \
+            --build-arg BASE_IMAGE="-${PY_VERSION_CODE}-${IMG_TYPE}" \
             --build-arg WHEEL_PATH=".whl/${WHEEL_FILE}" \
             --build-arg CONSTRAINTS_FILE="${CONSTRAINTS_FILE}" \
-            -t "${DEST_IMAGE}" -f Dockerfile -
+            -t "${RAY_IMG}" -f Dockerfile -
 )
 
-docker push "${DEST_IMAGE}"
+echo "--- Build ${ANYSCALE_IMG}"
+docker build --progress=plain \
+    --build-arg BASE_IMAGE="${RAY_IMG}" \
+    -t "${ANYSCALE_IMG}" -f Dockerfile - < "${BUILD_TMP}/dataplane.tar.gz"
+
+echo "--- Pushing images"
+
+docker_push "${RAY_IMG}"
+docker_push "${ANYSCALE_IMG}"
+
 
 if [[ "${PUSH_COMMIT_TAGS}" == "true" ]]; then
-    DEST_COMMIT_IMAGE="${RUNTIME_REPO}:${SHORT_COMMIT}-${PYTHON_VERSION_CODE}-${IMAGE_TYPE}${IMAGE_SUFFIX}"
-    docker tag "${DEST_IMAGE}" "${DEST_COMMIT_IMAGE}"
-    docker push "${DEST_COMMIT_IMAGE}"
-    if [[ "${IMAGE_TYPE}" == "${ML_CUDA_VERSION}" ]]; then
-        DEST_ALIAS_IMAGE="${RUNTIME_REPO}:${SHORT_COMMIT}-${PYTHON_VERSION_CODE}-gpu${IMAGE_SUFFIX}"
-        docker tag "${DEST_IMAGE}" "${DEST_ALIAS_IMAGE}"
-        docker push "${DEST_ALIAS_IMAGE}"
+    docker_push_as "${RAY_IMG}" "${RUNTIME_REPO}:${COMMIT_TAG}"
+    docker_push_as "${ANYSCALE_IMG}" "${RUNTIME_REPO}:${COMMIT_TAG}-as"
+    if [[ "${IMG_TYPE}" == "${ML_CUDA_VERSION}" ]]; then
+        docker_push_as "${RAY_IMG}" "${RUNTIME_REPO}:${COMMIT_GPU_TAG}"
+        docker_push_as "${ANYSCALE_IMG}" "${RUNTIME_REPO}:${COMMIT_GPU_TAG}-as"
     fi
 fi
 
 # This retagging is required because the Dockerfile hardcodes the base image.
-docker tag "${DEST_IMAGE}" "rayproject/ray:nightly-${PYTHON_VERSION_CODE}-${IMAGE_TYPE}${IMAGE_SUFFIX}"
+docker tag "${RAY_IMG}" "rayproject/ray:nightly-${PY_VERSION_CODE}-${IMG_TYPE}${IMG_SUFFIX}"
 
-buildkite-agent annotate --style=info \
-    --context="${PYTHON_VERSION_CODE}-images" \
-    --append "Image: ${DEST_IMAGE}<br/>"
-
-if [[ "${PUSH_COMMIT_TAGS}" == "true" ]]; then
-    buildkite-agent annotate --style=info \
-        --context="${PYTHON_VERSION_CODE}-images" \
-        --append "Image (commit tagged): ${DEST_COMMIT_IMAGE}<br/>"
-fi
-
-if [[ "${PYTHON_VERSION_CODE}" == "py37" || "${PYTHON_VERSION_CODE}" == "py311" || "${HOSTTYPE}" == "aarch64" ]]; then
-    echo "Skipping ML image for ${PYTHON_VERSION_CODE}" and host ${HOSTTYPE}
+if [[ "${PY_VERSION_CODE}" == "py37" || "${PY_VERSION_CODE}" == "py311" || "${HOSTTYPE}" == "aarch64" ]]; then
+    echo "Skipping ML image for ${PY_VERSION_CODE}" and host ${HOSTTYPE}
     exit 0
 fi
 
 # Build and push the ML image.
-DEST_ML_IMAGE="${RUNTIME_ML_REPO}:${IMAGE_PREFIX}-${PYTHON_VERSION_CODE}-${IMAGE_TYPE}${IMAGE_SUFFIX}"
-echo "--- Build ${DEST_ML_IMAGE}"
+RAY_ML_IMG="${RUNTIME_ML_REPO}:${BUILD_TAG}"
+ANYSCALE_ML_IMG="${RUNTIME_ML_REPO}:${BUILD_TAG}-as"
+echo "--- Build ${RAY_ML_IMG}"
 
 ML_TMP="$(mktemp -d)"
 
@@ -135,29 +167,25 @@ cp python/requirements/ml/*-requirements.txt "${ML_TMP}/."
     cd "${ML_TMP}"
     tar --mtime="UTC 2020-01-01" -c -f - . \
         | docker build --progress=plain \
-            --build-arg BASE_IMAGE="-${PYTHON_VERSION_CODE}-${IMAGE_TYPE}" \
-            -t "${DEST_ML_IMAGE}" -f Dockerfile -
+            --build-arg BASE_IMAGE="-${PY_VERSION_CODE}-${IMG_TYPE}" \
+            -t "${RAY_ML_IMG}" -f Dockerfile -
 )
 
-docker push "${DEST_ML_IMAGE}"
+echo "--- Build ${ANYSCALE_ML_IMG}"
+docker build --progress=plain \
+    --build-arg BASE_IMAGE="${RAY_ML_IMG}" \
+    -t "${ANYSCALE_ML_IMG}" -f Dockerfile - < "${BUILD_TMP}/dataplane.tar.gz"
+
+echo "--- Pushing ML images"
+docker_push "${RAY_ML_IMG}"
+docker_push "${ANYSCALE_ML_IMG}"
 
 if [[ "${PUSH_COMMIT_TAGS}" == "true" ]]; then
-    DEST_COMMIT_ML_IMAGE="${RUNTIME_ML_REPO}:${SHORT_COMMIT}-${PYTHON_VERSION_CODE}-${IMAGE_TYPE}${IMAGE_SUFFIX}"
-    docker tag "${DEST_ML_IMAGE}" "${DEST_COMMIT_ML_IMAGE}"
-    docker push "${DEST_COMMIT_ML_IMAGE}"
-    if [[ "${IMAGE_TYPE}" == "${ML_CUDA_VERSION}" ]]; then
-        DEST_ALIAS_IMAGE="${RUNTIME_ML_REPO}:${SHORT_COMMIT}-${PYTHON_VERSION_CODE}-gpu${IMAGE_SUFFIX}"
-        docker tag "${DEST_ML_IMAGE}" "${DEST_ALIAS_IMAGE}"
-        docker push "${DEST_ALIAS_IMAGE}"
+    docker_push_as "${RAY_ML_IMG}" "${RUNTIME_ML_REPO}:${COMMIT_TAG}"
+    docker_push_as "${ANYSCALE_ML_IMG}" "${RUNTIME_ML_REPO}:${COMMIT_TAG}-as"
+
+    if [[ "${IMG_TYPE}" == "${ML_CUDA_VERSION}" ]]; then
+        docker_push_as "${RAY_ML_IMG}" "${RUNTIME_ML_REPO}:${COMMIT_GPU_TAG}"
+        docker_push_as "${ANYSCALE_ML_IMG}" "${RUNTIME_ML_REPO}:${COMMIT_TAG}-as"
     fi
-fi
-
-buildkite-agent annotate --style=info \
-    --context="${PYTHON_VERSION_CODE}-images" \
-    --append "ML image: ${DEST_ML_IMAGE}<br/>"
-
-if [[ "${PUSH_COMMIT_TAGS}" == "true" ]]; then
-    buildkite-agent annotate --style=info \
-        --context="${PYTHON_VERSION_CODE}-images" \
-        --append "ML image (commit tagged): ${DEST_COMMIT_ML_IMAGE}<br/>"
 fi
