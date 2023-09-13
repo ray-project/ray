49142e9f35cb50a6060e9c753977bdd1357cd493
can <can@anyscale.com>
2023-09-13 13:32:30 +0000

    add a function to print stdout
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/test_docker_container.py
+++ ci/ray_ci/test_docker_container.py
@@ -9,7 +9,7 @@ from ci.ray_ci.test_base import RayCITestBase
 
 class TestDockerContainer(RayCITestBase):
     def test_run(self) -> None:
-        def _mock_check_output(input: List[str]) -> None:
+        def _mock_check_call(input: List[str], stdout, stderr) -> None:
             input_str = " ".join(input)
             assert "ray-3.0.0.dev0-cp38-cp38-manylinux2014_x86_64.whl" in input_str
             assert "rayproject/citemp:123-raypy38cu118base" in input_str
@@ -18,7 +18,7 @@ class TestDockerContainer(RayCITestBase):
 
         with mock.patch(
             "ci.ray_ci.docker_container.docker_pull", return_value=None
-        ), mock.patch("subprocess.check_output", side_effect=_mock_check_output):
+        ), mock.patch("subprocess.check_call", side_effect=_mock_check_call):
             container = DockerContainer("py38")
             container.run()
 
