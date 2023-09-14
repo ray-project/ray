3292e316bd6dc663d5609e09ec8e4ad03e72e75a
Jiajun Yao <jeromeyjj@gmail.com>
2023-09-14 14:32:55 -0700

    [Serve] AZ-aware deployment scheduler (#145)
    Try to spread replicas across AZs.
    
    ---------
    
    Signed-off-by: Jiajun Yao <jeromeyjj@gmail.com>

--- python/ray/serve/_private/default_impl.py
+++ python/ray/serve/_private/default_impl.py
@@ -1,8 +1,6 @@
-from ray.anyscale.serve._private.cluster_node_info_cache import (
-    AnyscaleClusterNodeInfoCache,
-)
 from ray.serve._private.cluster_node_info_cache import (
     ClusterNodeInfoCache,
+    DefaultClusterNodeInfoCache,
 )
 from ray.serve._private.deployment_scheduler import (
     DeploymentScheduler,
@@ -11,11 +9,41 @@ from ray.serve._private.deployment_scheduler import (
 from ray._raylet import GcsClient
 
 
-def create_cluster_node_info_cache(gcs_client: GcsClient):
-    return AnyscaleClusterNodeInfoCache(gcs_client)
+def create_cluster_node_info_cache(gcs_client: GcsClient) -> ClusterNodeInfoCache:
+    return DefaultClusterNodeInfoCache(gcs_client)
 
 
 def create_deployment_scheduler(
     cluster_node_info_cache: ClusterNodeInfoCache,
 ) -> DeploymentScheduler:
     return DefaultDeploymentScheduler(cluster_node_info_cache)
+
+
+# Anyscale overrides
+
+
+def create_cluster_node_info_cache(  # noqa: F811
+    gcs_client: GcsClient,
+) -> ClusterNodeInfoCache:
+    from ray.anyscale.serve._private.cluster_node_info_cache import (
+        AnyscaleClusterNodeInfoCache,
+    )
+
+    return AnyscaleClusterNodeInfoCache(gcs_client)
+
+
+def create_deployment_scheduler(  # noqa: F811
+    cluster_node_info_cache: ClusterNodeInfoCache,
+) -> DeploymentScheduler:
+    from ray.anyscale.serve._private.constants import (
+        ANYSCALE_RAY_SERVE_ENABLE_PROPRIETARY_DEPLOYMENT_SCHEDULER,
+    )
+
+    if ANYSCALE_RAY_SERVE_ENABLE_PROPRIETARY_DEPLOYMENT_SCHEDULER:
+        from ray.anyscale.serve._private.deployment_scheduler import (
+            AnyscaleDeploymentScheduler,
+        )
+
+        return AnyscaleDeploymentScheduler(cluster_node_info_cache)
+    else:
+        return DefaultDeploymentScheduler(cluster_node_info_cache)
