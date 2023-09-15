07579d9966b091fc8bf1b5cf1c7396b01eddf3eb
Yi Cheng <yic@anyscale.com>
2023-09-15 22:37:29 +0000

    fix

--- src/ray/raylet/worker_pool.cc
+++ src/ray/raylet/worker_pool.cc
@@ -88,23 +88,20 @@ WorkerPool::WorkerPool(instrumented_io_context &io_service,
       node_id_(node_id),
       node_address_(node_address),
       get_num_cpus_available_(get_num_cpus_available),
-      maximum_startup_concurrency_(maximum_startup_concurrency),
+      maximum_startup_concurrency_(
+          RayConfig::instance().worker_maximum_startup_concurrency() > 0 ?
+          // Overwrite the maximum concurrency.
+          RayConfig::instance().worker_maximum_startup_concurrency() : maximum_startup_concurrency),
       gcs_client_(std::move(gcs_client)),
       native_library_path_(native_library_path),
       starting_worker_timeout_callback_(starting_worker_timeout_callback),
       ray_debugger_external(ray_debugger_external),
       first_job_registered_python_worker_count_(0),
       first_job_driver_wait_num_python_workers_(
-          std::min(num_prestarted_python_workers, maximum_startup_concurrency)),
+          std::min(num_prestarted_python_workers, maximum_startup_concurrency_)),
       num_prestart_python_workers(num_prestarted_python_workers),
       periodical_runner_(io_service),
       get_time_(get_time) {
-  if (RayConfig::instance().worker_maximum_startup_concurrency() > 0) {
-    // Overwrite the maximum concurrency.
-    maximum_startup_concurrency_ =
-        RayConfig::instance().worker_maximum_startup_concurrency();
-  }
-
   RAY_CHECK(maximum_startup_concurrency_ > 0);
   // We need to record so that the metric exists. This way, we report that 0
   // processes have started before a task runs on the node (as opposed to the
