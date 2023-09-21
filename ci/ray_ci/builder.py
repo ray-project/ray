9c51c598c897359926b0a104c3bba6297493b26d
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2023-09-21 14:11:39 -0700

    [civ2][docker/5] build ray-ml image (#39153)
    Build ray-ml image for py38 in civ2. This is done via:
    
    create a wanda called raypy38gpubase, which is a layer of ray-deps < ray-ml
    build ray-ml image by installing ray on top of raypy38gpubase
    Notice that, comparing to civ1, this switches the order of ray installation and ml library installation. However, due to the requirement-compiled constraint file, the order doesn't matter.
    
    Signed-off-by: can <can@anyscale.com>
    Signed-off-by: Victor <vctr.y.m@example.com>

--- ci/ray_ci/builder.py
+++ ci/ray_ci/builder.py
@@ -1,8 +1,8 @@
 import click
 
 from ci.ray_ci.builder_container import PYTHON_VERSIONS, BuilderContainer
-from ci.ray_ci.docker_container import DockerContainer
 from ci.ray_ci.forge_container import ForgeContainer
+from ci.ray_ci.docker_container import PLATFORM, DockerContainer
 from ci.ray_ci.container import _DOCKER_ECR_REPO
 from ci.ray_ci.utils import logger, docker_login
 
@@ -19,22 +19,35 @@ from ci.ray_ci.utils import logger, docker_login
     type=click.Choice(list(PYTHON_VERSIONS.keys())),
     help=("Python version to build the wheel with"),
 )
+@click.option(
+    "--platform",
+    default="cu118",
+    type=click.Choice(list(PLATFORM)),
+    help=("Platform to build the docker with"),
+)
+@click.option(
+    "--image-type",
+    default="ray",
+    type=click.Choice(["ray", "ray-ml"]),
+)
 def main(
     artifact_type: str,
     python_version: str,
+    platform: str,
+    image_type: str,
 ) -> None:
     """
     Build a wheel or jar artifact
     """
     docker_login(_DOCKER_ECR_REPO.split("/")[0])
     if artifact_type == "wheel":
-        logger.info(f"Building wheel for Python {python_version}")
+        logger.info(f"Building wheel for {python_version}")
         build_wheel(python_version)
         return
 
     if artifact_type == "docker":
-        logger.info(f"Building ray-cpu docker for Python {python_version}")
-        build_docker(python_version)
+        logger.info(f"Building {image_type} docker for {python_version} on {platform}")
+        build_docker(python_version, platform, image_type)
         return
 
     raise ValueError(f"Invalid artifact type {artifact_type}")
@@ -48,9 +61,9 @@ def build_wheel(python_version: str) -> None:
     ForgeContainer().upload_wheel()
 
 
-def build_docker(python_version: str) -> None:
+def build_docker(python_version: str, platform: str, image_type: str) -> None:
     """
     Build a container artifact
     """
     BuilderContainer(python_version).run()
-    DockerContainer(python_version).run()
+    DockerContainer(python_version, platform, image_type).run()
