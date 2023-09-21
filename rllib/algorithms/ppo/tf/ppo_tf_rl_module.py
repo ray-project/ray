c1a6fd83b7d3381710e6cf26768dc6116aae8c69
sven1977 <svenmika1977@gmail.com>
2023-09-21 13:19:05 -0700

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/algorithms/ppo/tf/ppo_tf_rl_module.py
+++ rllib/algorithms/ppo/tf/ppo_tf_rl_module.py
@@ -18,17 +18,7 @@ class PPOTfRLModule(TfRLModule, PPORLModule):
 
     @override(RLModule)
     def _forward_inference(self, batch: NestedDict) -> Mapping[str, Any]:
-        output = {}
-
-        encoder_outs = self.encoder(batch)
-        if STATE_OUT in encoder_outs:
-            output[STATE_OUT] = encoder_outs[STATE_OUT]
-
-        # Actions
-        action_logits = self.pi(encoder_outs[ENCODER_OUT][ACTOR])
-        output[SampleBatch.ACTION_DIST_INPUTS] = action_logits
-
-        return output
+        return self._forward_exploration(batch)
 
     @override(RLModule)
     def _forward_exploration(self, batch: NestedDict) -> Mapping[str, Any]:
@@ -45,13 +35,17 @@ class PPOTfRLModule(TfRLModule, PPORLModule):
         if STATE_OUT in encoder_outs:
             output[STATE_OUT] = encoder_outs[STATE_OUT]
 
-        # Value head
+        # Value head.
+        # TODO (sven): Once we don't use Policy anymore for PPO (on new EnvRunner API),
+        #  we should move vf computations to the learner side and ONLY perform these
+        #  when we have an actual intent to update. Otherwise, even if this RLModule gets
+        #  deployed into a inference-only environment (in production), we would still
+        #  require vf computations to be performed and waste resources.
         vf_out = self.vf(encoder_outs[ENCODER_OUT][CRITIC])
         output[SampleBatch.VF_PREDS] = tf.squeeze(vf_out, axis=-1)
 
         # Policy head
         action_logits = self.pi(encoder_outs[ENCODER_OUT][ACTOR])
-
         output[SampleBatch.ACTION_DIST_INPUTS] = action_logits
 
         return output
