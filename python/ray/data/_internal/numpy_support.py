513983eb54ac4ce94dcb12a0c2c9fb4a3a020cb0
Michael Huang <michaelhly@gmail.com>
2023-09-26 19:04:28 -0400

    Don't convert to create_ragged_ndarray for lists of all strings
    Signed-off-by: Michael Huang <michaelhly@gmail.com>

--- python/ray/data/_internal/numpy_support.py
+++ python/ray/data/_internal/numpy_support.py
@@ -86,8 +86,10 @@ def convert_udf_returns_to_numpy(udf_return_col: Any) -> Any:
                 elif not np.isscalar(e):
                     has_object = True
             if has_object or len(shapes) > 1:
-                # This util works around some limitations of np.array(dtype=object).
-                udf_return_col = create_ragged_ndarray(udf_return_col)
+                if isinstance(e, list) and all(isinstance(l, str) for l in e):
+                    udf_return_col = np.array(udf_return_col)
+                else:
+                    udf_return_col = create_ragged_ndarray(udf_return_col)
             else:
                 udf_return_col = np.array(udf_return_col)
         except Exception as e:
@@ -104,5 +106,4 @@ def convert_udf_returns_to_numpy(udf_return_col: Any) -> Any:
                 "Failed to convert column values to numpy array: "
                 f"({_truncated_repr(udf_return_col)}): {e}."
             )
-
     return udf_return_col
