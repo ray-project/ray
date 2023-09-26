c399fcd7fccd3b3811c1c4f336505e1e6a2da9e0
Ricky Xu <xuchen727@hotmail.com>
2023-09-26 16:34:32 -0700

    [ci][core][cluster] Capture outputs for ray up/down commands to understand why it failed (#39578)
    ---------
    
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- python/ray/autoscaler/launch_and_verify_cluster.py
+++ python/ray/autoscaler/launch_and_verify_cluster.py
@@ -169,7 +169,15 @@ def cleanup_cluster(cluster_config):
     """
     print("======================================")
     print("Cleaning up cluster...")
-    subprocess.run(["ray", "down", "-v", "-y", str(cluster_config)], check=True)
+    try:
+        subprocess.run(
+            ["ray", "down", "-v", "-y", str(cluster_config)],
+            check=True,
+            capture_output=True,
+        )
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        raise e
 
 
 def run_ray_commands(cluster_config, retries, no_config_cache, num_expected_nodes=1):
@@ -192,7 +200,12 @@ def run_ray_commands(cluster_config, retries, no_config_cache, num_expected_node
     if no_config_cache:
         cmd.append("--no-config-cache")
     cmd.append(str(cluster_config))
-    subprocess.run(cmd, check=True)
+
+    try:
+        subprocess.run(cmd, check=True, capture_output=True)
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        raise e
 
     print("======================================")
     print("Verifying Ray is running...")
