e34ae6099372cb730997f1a852b4ec079bd61d3d
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2023-09-27 16:56:23 -0700

    simply test sharding (#39689)
    Currently we are calling test shardings twice, once to get test shards for a worker machine, and another to get shard per parallelism on that machine. This PR simplifies that logic and just call sharding once.
    
    Signed-off-by: can <can@anyscale.com>
    Signed-off-by: Victor <vctr.y.m@example.com>

--- ci/ray_ci/tester_container.py
+++ ci/ray_ci/tester_container.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-from typing import List
+from typing import List, Optional
 
 from ci.ray_ci.utils import shard_tests
 from ci.ray_ci.container import Container
@@ -12,20 +12,35 @@ class TesterContainer(Container):
     A wrapper for running tests in ray ci docker container
     """
 
-    def __init__(self, docker_tag: str) -> None:
+    def __init__(
+        self,
+        docker_tag: str,
+        shard_count: int = 1,
+        shard_ids: Optional[List[int]] = None,
+    ) -> None:
+        """
+        :param docker_tag: Name of the wanda build to be used as test container.
+        :param shard_count: The number of shards to split the tests into. This can be
+        used to run tests in a distributed fashion.
+        :param shard_ids: The list of shard ids to run. If none, run no shards.
+        """
         super().__init__(docker_tag)
+        self.shard_count = shard_count
+        self.shard_ids = shard_ids or []
+
         self.install_ray()
 
     def run_tests(
         self,
         test_targets: List[str],
         test_envs: List[str],
-        parallelism: int,
     ) -> bool:
         """
         Run tests parallelly in docker.  Return whether all tests pass.
         """
-        chunks = [shard_tests(test_targets, parallelism, i) for i in range(parallelism)]
+        chunks = [
+            shard_tests(test_targets, self.shard_count, i) for i in self.shard_ids
+        ]
         runs = [
             self._run_tests_in_docker(chunk, test_envs) for chunk in chunks if chunk
         ]
