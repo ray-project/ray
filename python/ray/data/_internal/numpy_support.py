d091339c3786f40e212c3b17ea98a9c802f65c0d
Michael Huang <michaelhly@gmail.com>
2023-09-26 20:23:03 -0400

    Use _create_possibly_ragged_ndarray instead of create_ragged_ndarray
    Signed-off-by: Michael Huang <michaelhly@gmail.com>

--- python/ray/data/_internal/numpy_support.py
+++ python/ray/data/_internal/numpy_support.py
@@ -2,7 +2,7 @@ from typing import Any
 
 import numpy as np
 
-from ray.air.util.tensor_extensions.utils import create_ragged_ndarray
+from ray.air.util.tensor_extensions.utils import _create_possibly_ragged_ndarray
 from ray.data._internal.dataset_logger import DatasetLogger
 from ray.data._internal.util import _truncated_repr
 
@@ -86,11 +86,8 @@ def convert_udf_returns_to_numpy(udf_return_col: Any) -> Any:
                 elif not np.isscalar(e):
                     has_object = True
             if has_object or len(shapes) > 1:
-                if isinstance(e, list) and all(isinstance(val, str) for val in e):
-                    udf_return_col = np.array(udf_return_col)
-                else:
-                    # This util works around some limitations of np.array(dtype=object).
-                    udf_return_col = create_ragged_ndarray(udf_return_col)
+                # This util works around some limitations of np.array(dtype=object).
+                udf_return_col = _create_possibly_ragged_ndarray(udf_return_col)
             else:
                 udf_return_col = np.array(udf_return_col)
         except Exception as e:
