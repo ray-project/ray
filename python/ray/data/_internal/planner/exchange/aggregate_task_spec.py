9af5484790bd51ab31a48a92ff640e2a7f9bdf0c
Michael Huang <michaelhly@gmail.com>
2023-09-28 19:27:54 -0400

    Skip type check
    Signed-off-by: Michael Huang <michaelhly@gmail.com>

--- python/ray/data/_internal/planner/exchange/aggregate_task_spec.py
+++ python/ray/data/_internal/planner/exchange/aggregate_task_spec.py
@@ -75,7 +75,7 @@ class SortAggregateTaskSpec(ExchangeTaskSpec):
         except AttributeError:
             # mapper_outputs might contain heterogeneous block types
             # normalize all blocks from mapper outputs and retry
-            normalized_blocks = normalize_blocks(mapper_outputs)
+            normalized_blocks = normalize_blocks(mapper_outputs, check_types=False)
             return BlockAccessor.for_block(
                 normalized_blocks[0]
             ).aggregate_combined_blocks(
