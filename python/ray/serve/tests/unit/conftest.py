f2545bd5470ce4721379bd0d09bcb90087106fb1
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-09-28 13:17:50 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/tests/unit/conftest.py
+++ python/ray/serve/tests/unit/conftest.py
@@ -2,11 +2,10 @@ import pytest
 
 import ray
 
+
 @pytest.fixture(autouse=True)
 def disallow_ray_init(monkeypatch):
     def raise_on_init():
-        raise RuntimeError(
-            "Unit tests should not depend on Ray being initialized."
-        )
+        raise RuntimeError("Unit tests should not depend on Ray being initialized.")
 
     monkeypatch.setattr(ray, "init", raise_on_init)
