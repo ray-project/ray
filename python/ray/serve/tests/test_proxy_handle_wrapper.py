93a024022e50e7814969179ce4c0898a3ab6f1f5
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-09-29 15:25:45 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/tests/test_proxy_handle_wrapper.py
+++ python/ray/serve/tests/test_proxy_handle_wrapper.py
@@ -1,12 +1,21 @@
 import asyncio
-import time
+from typing import Tuple
 
 import pytest
 
 from ray import serve
-from ray.serve.handle import DeploymentHandle
-from ray.serve._private.proxy_handle_wrapper import ProxyHandleWrapper
 from ray._private.test_utils import SignalActor, async_wait_for_condition
+from ray.serve._private.proxy_handle_wrapper import ProxyHandleWrapper
+
+
+def disconnect_task_and_event() -> Tuple[asyncio.Event, asyncio.Task]:
+    event = asyncio.Event()
+
+    async def wait_for_event():
+        return await event.wait()
+
+    return event, asyncio.ensure_future(wait_for_event())
+
 
 @pytest.mark.asyncio
 class TestUnary:
@@ -34,17 +43,23 @@ class TestUnary:
         assert len(responses) == 1
         assert set(responses) == {"Hello Alice!"}
 
-        responses = [r async for r in p.stream_request("Alice", method_name="other_method")]
+        responses = [
+            r async for r in p.stream_request("Alice", method_name="other_method")
+        ]
         assert len(responses) == 1
         assert set(responses) == {"Hello Alice from other method!"}
 
-        responses = [r async for r in p.stream_request("Alice", method_name="get_mmid", multiplexed_model_id="fake_model_id")]
+        responses = [
+            r
+            async for r in p.stream_request(
+                "Alice", method_name="get_mmid", multiplexed_model_id="fake_model_id"
+            )
+        ]
         assert len(responses) == 1
         assert set(responses) == {"Hello Alice: fake_model_id!"}
 
         with pytest.raises(RuntimeError, match="oopsies"):
             await p.stream_request("", method_name="error").__anext__()
-
 
     async def test_timeout_while_assigning(self, serve_instance):
         signal_actor = SignalActor.remote()
@@ -72,7 +87,6 @@ class TestUnary:
         assert await signal_actor.cur_num_waiters.remote() == 1
         await signal_actor.send.remote()
 
-
     async def test_timeout_while_executing(self, serve_instance):
         signal_actor = SignalActor.remote()
 
@@ -89,6 +103,74 @@ class TestUnary:
 
         with pytest.raises(TimeoutError):
             await p.stream_request("", timeout_s=0.1).__anext__()
+
+        assert await signal_actor.cur_num_waiters.remote() == 1
+        await signal_actor.send.remote()
+
+    async def test_disconnect_while_assigning(self, serve_instance):
+        signal_actor = SignalActor.remote()
+        disconnect_event, disconnect_task = disconnect_task_and_event()
+
+        @serve.deployment(max_concurrent_queries=1)
+        class D:
+            async def __call__(self, _: str) -> str:
+                await signal_actor.wait.remote()
+
+        h = serve.run(D.bind()).options(
+            use_new_handle_api=True,
+        )
+        h.remote("")
+
+        async def one_waiter():
+            return await signal_actor.cur_num_waiters.remote() == 1
+
+        await async_wait_for_condition(one_waiter)
+
+        p = ProxyHandleWrapper(h.options(stream=False))
+        gen = p.stream_request("", disconnected_task=disconnect_task)
+
+        async def get_next():
+            return await gen.__anext__()
+
+        gen_next = asyncio.ensure_future(get_next())
+        with pytest.raises(asyncio.exceptions.TimeoutError):
+            await asyncio.wait_for(gen_next, timeout=0.1)
+
+        # Set the disconnect event, causing the disconnect task to finish.
+        disconnect_event.set()
+        with pytest.raises(asyncio.CancelledError):
+            await gen_next
+
+        assert await signal_actor.cur_num_waiters.remote() == 1
+        await signal_actor.send.remote()
+
+    async def test_disconnect_while_executing(self, serve_instance):
+        signal_actor = SignalActor.remote()
+        disconnect_event, disconnect_task = disconnect_task_and_event()
+
+        @serve.deployment
+        class D:
+            async def __call__(self, _: str) -> str:
+                await signal_actor.wait.remote()
+
+        h = serve.run(D.bind()).options(
+            use_new_handle_api=True,
+        )
+
+        p = ProxyHandleWrapper(h.options(stream=False))
+        gen = p.stream_request("", disconnected_task=disconnect_task)
+
+        async def get_next():
+            return await gen.__anext__()
+
+        gen_next = asyncio.ensure_future(get_next())
+        with pytest.raises(asyncio.exceptions.TimeoutError):
+            await asyncio.wait_for(gen_next, timeout=0.1)
+
+        # Set the disconnect event, causing the disconnect task to finish.
+        disconnect_event.set()
+        with pytest.raises(asyncio.CancelledError):
+            await gen_next
 
         assert await signal_actor.cur_num_waiters.remote() == 1
         await signal_actor.send.remote()
@@ -124,11 +206,18 @@ class TestStreaming:
         assert len(responses) == 5
         assert set(responses) == {"Hello Alice!"}
 
-        responses = [r async for r in p.stream_request("Alice", method_name="other_method")]
+        responses = [
+            r async for r in p.stream_request("Alice", method_name="other_method")
+        ]
         assert len(responses) == 5
         assert set(responses) == {"Hello Alice from other method!"}
 
-        responses = [r async for r in p.stream_request("Alice", method_name="get_mmid", multiplexed_model_id="fake_model_id")]
+        responses = [
+            r
+            async for r in p.stream_request(
+                "Alice", method_name="get_mmid", multiplexed_model_id="fake_model_id"
+            )
+        ]
         assert len(responses) == 5
         assert set(responses) == {"Hello Alice: fake_model_id!"}
 
@@ -136,6 +225,127 @@ class TestStreaming:
         assert await gen.__anext__() == "Hello Alice!"
         with pytest.raises(RuntimeError, match="oopsies"):
             await gen.__anext__()
+
+    async def test_timeout_while_assigning(self, serve_instance):
+        signal_actor = SignalActor.remote()
+
+        @serve.deployment(max_concurrent_queries=1)
+        class D:
+            async def __call__(self, _: str) -> str:
+                yield "hi"
+                await signal_actor.wait.remote()
+
+        h = serve.run(D.bind()).options(
+            use_new_handle_api=True,
+        )
+        h.remote("")
+
+        async def one_waiter():
+            return await signal_actor.cur_num_waiters.remote() == 1
+
+        await async_wait_for_condition(one_waiter)
+
+        p = ProxyHandleWrapper(h.options(stream=True))
+
+        with pytest.raises(TimeoutError):
+            await p.stream_request("", timeout_s=0.1).__anext__()
+
+        assert await signal_actor.cur_num_waiters.remote() == 1
+        await signal_actor.send.remote()
+
+    async def test_timeout_while_executing(self, serve_instance):
+        signal_actor = SignalActor.remote()
+
+        @serve.deployment
+        class D:
+            async def __call__(self, _: str) -> str:
+                yield "hi"
+                await signal_actor.wait.remote()
+
+        h = serve.run(D.bind()).options(
+            use_new_handle_api=True,
+        )
+
+        p = ProxyHandleWrapper(h.options(stream=True))
+
+        gen = p.stream_request("", timeout_s=0.1)
+        assert (await gen.__anext__()) == "hi"
+        with pytest.raises(TimeoutError):
+            await gen.__anext__()
+
+        assert await signal_actor.cur_num_waiters.remote() == 1
+        await signal_actor.send.remote()
+
+    async def test_disconnect_while_assigning(self, serve_instance):
+        signal_actor = SignalActor.remote()
+        disconnect_event, disconnect_task = disconnect_task_and_event()
+
+        @serve.deployment(max_concurrent_queries=1)
+        class D:
+            async def __call__(self, _: str) -> str:
+                yield "hi"
+                await signal_actor.wait.remote()
+
+        h = serve.run(D.bind()).options(
+            use_new_handle_api=True,
+        )
+        h.remote("")
+
+        async def one_waiter():
+            return await signal_actor.cur_num_waiters.remote() == 1
+
+        await async_wait_for_condition(one_waiter)
+
+        p = ProxyHandleWrapper(h.options(stream=True))
+        gen = p.stream_request("", disconnected_task=disconnect_task)
+
+        async def get_next():
+            return await gen.__anext__()
+
+        gen_next = asyncio.ensure_future(get_next())
+        with pytest.raises(asyncio.exceptions.TimeoutError):
+            await asyncio.wait_for(gen_next, timeout=0.1)
+
+        # Set the disconnect event, causing the disconnect task to finish.
+        disconnect_event.set()
+        with pytest.raises(asyncio.CancelledError):
+            await gen_next
+
+        assert await signal_actor.cur_num_waiters.remote() == 1
+        await signal_actor.send.remote()
+
+    async def test_disconnect_while_executing(self, serve_instance):
+        signal_actor = SignalActor.remote()
+        disconnect_event, disconnect_task = disconnect_task_and_event()
+
+        @serve.deployment
+        class D:
+            async def __call__(self, _: str) -> str:
+                yield "hi"
+                await signal_actor.wait.remote()
+
+        h = serve.run(D.bind()).options(
+            use_new_handle_api=True,
+        )
+
+        p = ProxyHandleWrapper(h.options(stream=True))
+        gen = p.stream_request("", disconnected_task=disconnect_task)
+        assert (await gen.__anext__()) == "hi"
+
+        async def get_next():
+            return await gen.__anext__()
+
+        gen_next = asyncio.ensure_future(get_next())
+        with pytest.raises(asyncio.exceptions.TimeoutError):
+            await asyncio.wait_for(gen_next, timeout=0.1)
+
+        # Set the disconnect event, causing the disconnect task to finish.
+        disconnect_event.set()
+        with pytest.raises(asyncio.CancelledError):
+            await gen_next
+
+        assert await signal_actor.cur_num_waiters.remote() == 1
+        await signal_actor.send.remote()
 
 
 if __name__ == "__main__":
