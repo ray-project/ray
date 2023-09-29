177a1e367c270cfcbceebcd15ea3ee5dd1d5aee2
Stephanie Wang <swang@cs.berkeley.edu>
2023-09-29 16:04:07 -0600

    fix test script
    Signed-off-by: Stephanie Wang <swang@cs.berkeley.edu>

--- test.py
+++ test.py
@@ -12,26 +12,18 @@ class Actor:
         self._output_ref = output_refs[0]
 
     def execute(self):
-        buf = ray.get(self._input_ref)
-        last = 0
         while True:
-            # Hack to find out when the object has been unsealed and recreated.
-            cur = buf[0]
-            if cur != last:
-                ray.worker.global_worker.put_object(buf * 2, object_ref=self._output_ref)
-            last = cur
+            buf = get_and_reset(self._input_ref)
+            ray.worker.global_worker.put_object(buf * 2, object_ref=self._output_ref)
+            # Delete ref because we cannot call ray.get on an unsealed ref.
+            del buf
 
 
-def wait_for_completion_and_reset(input_ref, output_ref):
+def get_and_reset(ref):
     # Wait for the DAG to finish.
-    output_buf = ray.get(output_ref)
-
-    # Reset the input and output refs.
-    # NOTE(swang): Have the driver unseal because we don't have a way to signal
-    # the actor when the object has been unsealed. Alternatively, the actor
-    # should unseal its input ref BEFORE sealing the output ref.
-    ray.worker.global_worker.core_worker.unseal_object(input_ref)
-    ray.worker.global_worker.core_worker.unseal_object(output_ref)
+    output_buf = ray.get(ref)
+    # Reset the output ref.
+    ray.worker.global_worker.core_worker.unseal_object(ref)
     return output_buf
 
 
@@ -39,22 +31,24 @@ a = Actor.remote()
 
 input_ref = ray.put(np.zeros(100, dtype=np.uint8))
 output_ref = ray.put(np.zeros(100, dtype=np.uint8))
+print("Input:", input_ref)
+print("Output:", output_ref)
+
+ray.get([input_ref, output_ref])
 
 # Initialization.
 # - Give actor the references.
-# - Unseal all references.
-# - All refs are unsealed. Start the actor loop.
 ray.get(a._init.remote([input_ref], [output_ref]))
-# Get the input buf because we need to be using the object in order to unseal
-# it.
-input_buf = ray.get(input_ref)
-wait_for_completion_and_reset(input_ref, output_ref)
+# - Unseal all references.
+get_and_reset(input_ref)
+get_and_reset(output_ref)
+# - All refs are unsealed. Start the actor loop.
 a.execute.remote()
 
-for i in range(1, 10):
+for i in range(1, 128):
     ray.worker.global_worker.put_object(np.ones(100, dtype=np.uint8) * i, object_ref=input_ref)
 
-    output_buf = wait_for_completion_and_reset(input_ref, output_ref)
-    assert output_buf[0] == i * 2
+    output_buf = get_and_reset(output_ref)
+    assert output_buf[0] == (i * 2), (i, output_buf)
     # Delete ref because we cannot call ray.get on an unsealed ref.
     del output_buf
