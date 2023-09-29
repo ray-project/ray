00d09da15d302fa90c44eacc2251452815c2bbdd
Kamen Shah <kamenshah@kamenshah-NH9WWKH6X2.local.meter>
2023-09-29 13:32:16 -0700

    Update sidebar title

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -281,7 +281,7 @@ html_theme_options = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = f"Ray {release}"
+html_title = f"Version {release}"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 # html_short_title = None
