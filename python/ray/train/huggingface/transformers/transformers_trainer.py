bc538376047eee75a1ee2ee9e197d0afcfa3130f
Justin Yu <justinvyu@anyscale.com>
2023-10-03 11:43:36 -0700

    remove legacy air framework ckpts
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/train/huggingface/transformers/transformers_trainer.py
+++ python/ray/train/huggingface/transformers/transformers_trainer.py
@@ -10,13 +10,12 @@ from torch.utils.data import Dataset as TorchDataset
 
 from ray import train
 from ray.air import session
-from ray.air.checkpoint import Checkpoint
 from ray.air.config import RunConfig, ScalingConfig
 from ray.train.constants import (
     EVALUATION_DATASET_KEY,
     TRAIN_DATASET_KEY,
 )
-from ray.train import DataConfig
+from ray.train import Checkpoint, DataConfig
 from ray.train.data_parallel_trainer import DataParallelTrainer
 from ray.train.torch import TorchConfig, TorchTrainer
 from ray.train.trainer import GenDataset
@@ -450,14 +449,13 @@ def _huggingface_train_loop_per_worker(config):
 
     if trainer.args.load_best_model_at_end:
         raise ValueError(
-            "As Ray AIR replaces Transformers checkpointing, "
+            "Since Ray Train replaces Transformers checkpointing, "
             "`load_best_model_at_end` must be set to False.\n"
-            "You can obtain the AIR Checkpoint with "
-            "`Result.checkpoint` returned by the `fit()` method "
-            "of this Trainer, and the model itself by calling "
-            "`Checkpoint.get_model()`.\n"
-            "You can configure the checkpointing by setting "
-            "`run_config.checkpoint_config`."
+            "You can obtain the ray.train.Checkpoint with "
+            "`Result.checkpoint` from the result returned by the `fit()` method "
+            "of this Trainer, and access the model itself by inspecting the "
+            "checkpoint directory via `Checkpoint.as_directory` "
+            "/ `Checkpoint.to_directory`.\n"
         )
 
     if trainer.args.push_to_hub and not trainer.args.hub_token:
