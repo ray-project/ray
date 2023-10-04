4f0df9ab48207b86c69882e83e530cbf353925fd
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2023-10-04 10:42:48 -0700

    [civ2][docker/9] build anyscale image with release test requirements (#39819)
    Build anyscale image with release test requirements
    
    Signed-off-by: can <can@anyscale.com>
    Signed-off-by: Victor <vctr.y.m@example.com>

--- ci/build/build-anyscale-docker.sh
+++ ci/build/build-anyscale-docker.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 SOURCE_IMAGE="$1"
 DEST_IMAGE="$2"
+REQUIREMENTS="$3"
 
 DATAPLANE_S3_BUCKET="ray-release-automation-results"
 DATAPLANE_FILENAME="dataplane_20230718.tgz"
@@ -19,3 +20,11 @@ echo "${DATAPLANE_DIGEST}  ${DATAPLANE_FILENAME}" | sha256sum -c
 DOCKER_BUILDKIT=1 docker build \
     --build-arg BASE_IMAGE="$SOURCE_IMAGE" \
     -t "$DEST_IMAGE" - < "${DATAPLANE_FILENAME}"
+
+DOCKER_BUILDKIT=1 docker build \
+    --build-arg BASE_IMAGE="$DEST_IMAGE" \
+    --build-arg PIP_REQUIREMENTS="$REQUIREMENTS" \
+    --build-arg DEBIAN_REQUIREMENTS=requirements_debian_byod.txt \
+    -t "$DEST_IMAGE" \
+    -f release/ray_release/byod/byod.Dockerfile \
+    release/ray_release/byod
