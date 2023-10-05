99aca6fab4c5fb32bad0591f03f262a365ce618b
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-10-05 11:01:31 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/tests/test_pydantic_serialization.py
+++ python/ray/tests/test_pydantic_serialization.py
@@ -7,6 +7,28 @@ from pydantic import BaseModel
 
 import ray
 
+import pydantic
+from pydantic import BaseModel
+from pydantic._internal._model_construction import ModelMetaclass
+
+from pydantic_core import SchemaSerializer
+from pydantic_core._pydantic_core import SchemaSerializer
+
+class CloudpickleableSchemaSerializer:
+    def __init__(self, *args):
+        self._args = args
+        self._schema_serializer = SchemaSerializer(*args)
+
+    def __reduce__(self):
+        return CloudpickleableSchemaSerializer, self._args
+
+    def __getattr__(self, attr: str):
+        return getattr(self._schema_serializer, attr)
+
+pydantic._internal._model_construction.SchemaSerializer = CloudpickleableSchemaSerializer
+pydantic._internal._dataclasses.SchemaSerializer = CloudpickleableSchemaSerializer
+pydantic.type_adapter.SchemaSerializer = CloudpickleableSchemaSerializer
+
 
 @pytest.fixture(scope="session")
 def start_ray():
--- test.py
+++ test.py
@@ -1,8 +1,28 @@
 from copy import copy
 
+import pydantic
 from pydantic import BaseModel
-from pydantic_core import SchemaSerializer
 from pydantic._internal._model_construction import ModelMetaclass
+
+from pydantic_core import SchemaSerializer
+from pydantic_core._pydantic_core import SchemaSerializer
+
+
+class CloudpickleableSchemaSerializer: 
+    def __init__(self, *args):
+        self._args = args
+        print(args)
+        self._schema_serializer = SchemaSerializer(*args)
+
+    def __reduce__(self):
+        return CloudpickleableSchemaSerializer, self._args
+
+    def __getattr__(self, attr: str):
+        return getattr(self._schema_serializer, attr)
+
+pydantic._internal._model_construction.SchemaSerializer = CloudpickleableSchemaSerializer
+pydantic._internal._dataclasses.SchemaSerializer = CloudpickleableSchemaSerializer
+pydantic.type_adapter.SchemaSerializer = CloudpickleableSchemaSerializer
 
 import pickle
 from ray import cloudpickle
@@ -11,8 +31,8 @@ class User(BaseModel):
     id: int
     name: str = "Jane Doe"
 
-b = pickle.dumps(User)
-u2 = pickle.loads(b)
+b = cloudpickle.dumps(User)
+u2 = cloudpickle.loads(b)
 
 john = u2(id=1, name="John")
 print(john)
