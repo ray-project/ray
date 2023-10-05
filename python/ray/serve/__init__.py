4fba6abff960b00574244f859db89598e7a505bf
Sihan Wang <sihanwang41@gmail.com>
2023-10-05 15:20:14 -0700

    Update
    Signed-off-by: Sihan Wang <sihanwang41@gmail.com>

--- python/ray/serve/__init__.py
+++ python/ray/serve/__init__.py
@@ -21,6 +21,7 @@ try:
     )
     from ray.serve.batching import batch
     from ray.serve.config import HTTPOptions
+
 except ModuleNotFoundError as e:
     e.msg += (
         '. You can run `pip install "ray[serve]"` to install all Ray Serve'
