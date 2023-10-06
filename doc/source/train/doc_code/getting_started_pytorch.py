44972d3161d21c1b641d440c3020fca487552047
woshiyyya <xiaoyunxuan1998@gmail.com>
2023-10-06 14:41:49 -0700

    fix lint
    Signed-off-by: woshiyyya <xiaoyunxuan1998@gmail.com>

--- doc/source/train/doc_code/getting_started_pytorch.py
+++ doc/source/train/doc_code/getting_started_pytorch.py
@@ -11,11 +11,14 @@ from ray.train.torch import TorchTrainer
 from ray.train import ScalingConfig, Checkpoint
 import ray.train
 
+
 def train_func(config):
 
     # Model, Loss, Optimizer
     model = resnet18(num_classes=10)
-    model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+    model.conv1 = torch.nn.Conv2d(
+        1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False
+    )
     # [1] Prepare model.
     model = ray.train.torch.prepare_model(model)
     criterion = CrossEntropyLoss()
@@ -23,7 +26,9 @@ def train_func(config):
 
     # Data
     transform = Compose([ToTensor(), Normalize((0.5,), (0.5,))])
-    train_data = FashionMNIST(root='./data', train=True, download=True, transform=transform)
+    train_data = FashionMNIST(
+        root="./data", train=True, download=True, transform=transform
+    )
     train_loader = DataLoader(train_data, batch_size=128, shuffle=True)
     # [2] Prepare dataloader.
     train_loader = ray.train.torch.prepare_data_loader(train_loader)
@@ -36,12 +41,15 @@ def train_func(config):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-        
-        checkpoint_dir = tempfile.gettempdir() 
+
+        checkpoint_dir = tempfile.gettempdir()
         checkpoint_path = checkpoint_dir + "/model.checkpoint"
         torch.save(model.state_dict(), checkpoint_path)
         # [3] Report metrics and checkpoint.
-        ray.train.report({"loss": loss.item()}, checkpoint=Checkpoint.from_directory(checkpoint_dir))
+        ray.train.report(
+            {"loss": loss.item()}, checkpoint=Checkpoint.from_directory(checkpoint_dir)
+        )
+
 
 # [4] Configure scaling and resource requirements.
 scaling_config = ScalingConfig(num_workers=2, use_gpu=True)
