1325660ac1dfa107cd5363d1db55ae7e5d961a3d
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-10-11 10:43:24 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/job_submission/__init__.py
+++ python/ray/job_submission/__init__.py
@@ -1,18 +1,6 @@
-from ray._private.pydantic_compat import PYDANTIC_INSTALLED
 from ray.dashboard.modules.job.common import JobInfo, JobStatus
+from ray.dashboard.modules.job.pydantic_models import DriverInfo, JobDetails, JobType
 from ray.dashboard.modules.job.sdk import JobSubmissionClient
-
-if PYDANTIC_INSTALLED:
-    from ray.dashboard.modules.job.pydantic_models import (
-        DriverInfo,
-        JobDetails,
-        JobType,
-    )
-else:
-    DriverInfo = None
-    JobDetails = None
-    JobType = None
-
 
 __all__ = [
     "JobSubmissionClient",
