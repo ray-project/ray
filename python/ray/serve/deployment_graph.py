a92aa9e10f1310510facaba9408ca4a0f7425543
Cindy Zhang <cindyzyx9@gmail.com>
2023-10-12 16:15:17 -0700

    deprecate
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- python/ray/serve/deployment_graph.py
+++ python/ray/serve/deployment_graph.py
@@ -4,10 +4,10 @@ from ray.dag import DAGNode  # noqa: F401
 from ray.dag.class_node import ClassNode  # noqa: F401
 from ray.dag.function_node import FunctionNode  # noqa: F401
 from ray.dag.input_node import InputNode  # noqa: F401
-from ray.util.annotations import PublicAPI
+from ray.util.annotations import Deprecated
 
 
-@PublicAPI(stability="alpha")
+@Deprecated("The DAG API is deprecated.")
 class RayServeDAGHandle:
     """Resolved from a DeploymentNode at runtime.
 
