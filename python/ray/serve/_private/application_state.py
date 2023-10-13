ab4341c549869ccb3e8d8af2613bebe00852d571
Cindy Zhang <cindyzyx9@gmail.com>
2023-10-13 14:31:06 -0700

    try
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- python/ray/serve/_private/application_state.py
+++ python/ray/serve/_private/application_state.py
@@ -345,12 +345,12 @@ class ApplicationState:
                 self._check_routes(overrided_infos)
                 self._set_target_state_deployment_infos(overrided_infos)
             except (TypeError, ValueError, RayServeException):
-                self._set_target_state_deployment_infos(None)
+                # self._set_target_state_deployment_infos(None)
                 self._update_status(
                     ApplicationStatus.DEPLOY_FAILED, traceback.format_exc()
                 )
             except Exception:
-                self._set_target_state_deployment_infos(None)
+                # self._set_target_state_deployment_infos(None)
                 self._update_status(
                     ApplicationStatus.DEPLOY_FAILED,
                     (
