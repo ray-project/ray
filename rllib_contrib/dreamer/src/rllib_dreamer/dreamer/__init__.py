4d0afe8c0a49f6cba628b76a483e132376d6bbab
sven1977 <svenmika1977@gmail.com>
2023-10-14 13:02:25 +0200

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib_contrib/dreamer/src/rllib_dreamer/dreamer/__init__.py
+++
@@ -1,7 +0,0 @@
-from rllib_dreamer.dreamer.dreamer import Dreamer, DreamerConfig
-
-from ray.tune.registry import register_trainable
-
-__all__ = ["DreamerConfig", "Dreamer"]
-
-register_trainable("rllib-contrib-dreamer", Dreamer)
