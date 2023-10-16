e0f8236849e9708151f5b1126a2d7660ff7ea016
Chen Hui <huchen@vmware.com>
2023-10-16 01:53:19 +0000

    add comment for function external_ip
    Signed-off-by: Chen Hui <huchen@vmware.com>

--- python/ray/autoscaler/_private/vsphere/node_provider.py
+++ python/ray/autoscaler/_private/vsphere/node_provider.py
@@ -186,11 +186,12 @@ class VsphereNodeProvider(NodeProvider):
             return dict(d1)
 
     def external_ip(self, node_id):
+        # Return the external IP of the VM
         # Fetch vSphere VM object
         vm = self.pyvmomi_sdk_provider.get_pyvmomi_obj_by_moid(
             [vim.VirtualMachine], node_id
         )
-        # Get the ipaddress of this VM
+        # Get the external IP address of this VM
         for ipaddr in vm.guest.net[0].ipAddress:
             if is_ipv4(ipaddr):
                 logger.debug("Fetch IP {} for VM {}".format(ipaddr, vm))
