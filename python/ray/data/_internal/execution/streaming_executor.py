dff74165dd3c62512efdf16ef03d663ac04a22da
Hao Chen <chenh1024@gmail.com>
2023-10-17 16:28:16 -0700

    remove blank line
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- python/ray/data/_internal/execution/streaming_executor.py
+++ python/ray/data/_internal/execution/streaming_executor.py
@@ -243,7 +243,6 @@ class StreamingExecutor(Executor, threading.Thread):
         # Note: calling process_completed_tasks() is expensive since it incurs
         # ray.wait() overhead, so make sure to allow multiple dispatch per call for
         # greater parallelism.
-        #
         process_completed_tasks(topology)
 
         # Dispatch as many operators as we can for completed tasks.
