3c5d5608831626c0ef31250df4a037406e85034e
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-10-18 11:31:15 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/_private/pydantic_compat.py
+++ python/ray/_private/pydantic_compat.py
@@ -14,6 +14,55 @@ except ImportError:
     PYDANTIC_INSTALLED = False
 
 
+if not PYDANTIC_INSTALLED:
+    IS_PYDANTIC_2 = False
+    BaseModel = None
+    Extra = None
+    Field = None
+    NonNegativeFloat = None
+    NonNegativeInt = None
+    PositiveFloat = None
+    PositiveInt = None
+    ValidationError = None
+    root_validator = None
+    validator = None
+# In pydantic <1.9.0, __version__ attribute is missing, issue ref:
+# https://github.com/pydantic/pydantic/issues/2572, so we need to check
+# the existence prior to comparison.
+elif hasattr(pydantic, "__version__") and packaging.version.parse(
+    pydantic.__version__
+) < packaging.version.parse("2.0"):
+    IS_PYDANTIC_2 = False
+    from pydantic import (
+        BaseModel,
+        Extra,
+        Field,
+        NonNegativeFloat,
+        NonNegativeInt,
+        PositiveFloat,
+        PositiveInt,
+        ValidationError,
+        root_validator,
+        validator,
+    )
+else:
+    IS_PYDANTIC_2 = True
+    # TODO(edoakes): compare this against the version that has the fixes.
+    monkeypatch_pydantic_2_for_cloudpickle()
+    from pydantic.v1 import (
+        BaseModel,
+        Extra,
+        Field,
+        NonNegativeFloat,
+        NonNegativeInt,
+        PositiveFloat,
+        PositiveInt,
+        ValidationError,
+        root_validator,
+        validator,
+    )
+
+
 class CloudpickleableSchemaSerializer:
     def __init__(self, schema, core_config):
         self._schema = schema
@@ -63,50 +112,33 @@ def monkeypatch_pydantic_2_for_cloudpickle():
     pydantic._internal._model_construction._PydanticWeakRef = WeakRefWrapper
 
 
-if not PYDANTIC_INSTALLED:
-    IS_PYDANTIC_2 = False
-    BaseModel = None
-    Extra = None
-    Field = None
-    NonNegativeFloat = None
-    NonNegativeInt = None
-    PositiveFloat = None
-    PositiveInt = None
-    ValidationError = None
-    root_validator = None
-    validator = None
-# In pydantic <1.9.0, __version__ attribute is missing, issue ref:
-# https://github.com/pydantic/pydantic/issues/2572, so we need to check
-# the existence prior to comparison.
-elif hasattr(pydantic, "__version__") and packaging.version.parse(
-    pydantic.__version__
-) < packaging.version.parse("2.0"):
-    IS_PYDANTIC_2 = False
-    from pydantic import (
-        BaseModel,
-        Extra,
-        Field,
-        NonNegativeFloat,
-        NonNegativeInt,
-        PositiveFloat,
-        PositiveInt,
-        ValidationError,
-        root_validator,
-        validator,
-    )
-else:
-    IS_PYDANTIC_2 = True
-    # TODO(edoakes): compare this against the version that has the fixes.
-    monkeypatch_pydantic_2_for_cloudpickle()
-    from pydantic.v1 import (
-        BaseModel,
-        Extra,
-        Field,
-        NonNegativeFloat,
-        NonNegativeInt,
-        PositiveFloat,
-        PositiveInt,
-        ValidationError,
-        root_validator,
-        validator,
+def register_pydantic_serializers(serialization_context):
+    if not PYDANTIC_INSTALLED:
+        return
+
+    if IS_PYDANTIC_2:
+        monkeypatch_pydantic_2_for_cloudpickle()
+        from pydantic.v1.fields import ModelField
+    else:
+        from pydantic.fields import ModelField
+
+    # Pydantic's Cython validators are not serializable.
+    # https://github.com/cloudpipe/cloudpickle/issues/408
+    serialization_context._register_cloudpickle_serializer(
+        ModelField,
+        custom_serializer=lambda o: {
+            "name": o.name,
+            # outer_type_ is the original type for ModelFields,
+            # while type_ can be updated later with the nested type
+            # like int for List[int].
+            "type_": o.outer_type_,
+            "class_validators": o.class_validators,
+            "model_config": o.model_config,
+            "default": o.default,
+            "default_factory": o.default_factory,
+            "required": o.required,
+            "alias": o.alias,
+            "field_info": o.field_info,
+        },
+        custom_deserializer=lambda kwargs: ModelField(**kwargs),
     )
