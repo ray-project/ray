647066eae84d2bfa1d7f27815b2e336827f06a23
Glenn Moynihan <glennmoy@gmail.com>
2023-10-18 21:46:31 +0100

    Missed some JULIA language support

--- python/ray/_raylet.pyx
+++ python/ray/_raylet.pyx
@@ -108,6 +108,7 @@ from ray.includes.common cimport (
     LANGUAGE_CPP,
     LANGUAGE_JAVA,
     LANGUAGE_PYTHON,
+    LANGUAGE_JULIA,
     LocalMemoryBuffer,
     TASK_TYPE_NORMAL_TASK,
     TASK_TYPE_ACTOR_CREATION_TASK,
@@ -628,6 +629,8 @@ cdef class Language:
             return "CPP"
         elif <int32_t>self.lang == <int32_t>LANGUAGE_JAVA:
             return "JAVA"
+        elif <int32_t>self.lang == <int32_t>LANGUAGE_JULIA:
+            return "JULIA"
         else:
             raise Exception("Unexpected error")
 
@@ -637,6 +640,7 @@ cdef class Language:
     PYTHON = Language.from_native(LANGUAGE_PYTHON)
     CPP = Language.from_native(LANGUAGE_CPP)
     JAVA = Language.from_native(LANGUAGE_JAVA)
+    JULIA = Language.from_native(LANGUAGE_JULIA)
 
 
 cdef int prepare_resources(
