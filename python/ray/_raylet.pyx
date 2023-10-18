0c1b918768f0d224ff52bf1198eeac63b456525c
Jiajun Yao <jeromeyjj@gmail.com>
2023-10-18 12:52:59 -0700

    Only set visiable accelerator ids in the beginning of a task or actor
    Signed-off-by: Jiajun Yao <jeromeyjj@gmail.com>

--- python/ray/_raylet.pyx
+++ python/ray/_raylet.pyx
@@ -1854,7 +1854,8 @@ cdef execute_task_with_cancellation_handler(
 
     # Automatically restrict the GPUs (CUDA), neuron_core, TPU accelerator
     # runtime_ids to restrict availability to this task.
-    ray._private.utils.set_visible_accelerator_ids()
+    if (<int>task_type != <int>TASK_TYPE_ACTOR_TASK):
+        ray._private.utils.set_visible_accelerator_ids()
 
     # Automatically configure OMP_NUM_THREADS to the assigned CPU number.
     # It will be unset after the task execution if it was overwridden here.
