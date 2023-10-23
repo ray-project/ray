74f5be5c21be535a8c91234147fcf06c53b9aabb
Scott Lee <sjl@anyscale.com>
2023-10-23 09:50:20 -0700

    fix quotes
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- python/ray/data/read_api.py
+++ python/ray/data/read_api.py
@@ -1046,7 +1046,7 @@ def read_json(
         >>> import pyarrow.json as pajson
         >>> block_size = 10 << 20 # Set block size to 10MB
         >>> ray.data.read_json(
-        ...     s3://anonymous@ray-example-data/log.json,
+        ...     "s3://anonymous@ray-example-data/log.json",
         ...     read_options=pajson.ReadOptions(block_size=block_size)
         ... )
 
