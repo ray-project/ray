cecb6c0da364818159a9227d46fc323b75aa549e
Santosh Jha <santosh.m.jha@gmail.com>
2023-10-23 17:14:39 -0700

    Add a unit test for test_secret_env_vars_available
    Signed-off-by: Santosh Jha <santosh.m.jha@gmail.com>

--- python/ray/tests/test_runtime_env.py
+++ python/ray/tests/test_runtime_env.py
@@ -910,6 +910,20 @@ def test_runtime_env_interface():
     assert runtime_env.to_dict() == {}
 
 
+@pytest.mark.parametrize("runtime_env_class", [dict, RuntimeEnv])
+def test_secret_env_vars_available(start_cluster, runtime_env_class):
+    cluster, address = start_cluster
+    ray.init(address)
+
+    runtime_env = runtime_env_class(secret_env_vars={"foo": "bar"})
+
+    @ray.remote(runtime_env=runtime_env)
+    def f():
+        return os.environ.get("foo")
+
+    assert ray.get(f.remote()) == "bar"
+
+
 if __name__ == "__main__":
     import sys
 
