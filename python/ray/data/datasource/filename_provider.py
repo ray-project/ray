9f6f9f6dc590d41c42f787cde81466d4144f0484
Balaji Veeramani <balaji@anyscale.com>
2023-10-25 19:44:00 -0700

    Fix bug
    Signed-off-by: Balaji Veeramani <balaji@anyscale.com>

--- python/ray/data/datasource/filename_provider.py
+++ python/ray/data/datasource/filename_provider.py
@@ -95,7 +95,7 @@ class _DefaultFilenameProvider(FilenameProvider):
     def _generate_filename(self, file_id: str) -> str:
         filename = ""
         if self._dataset_uuid is not None:
-            filname += f"{self._dataset_uuid}_"
+            filename += f"{self._dataset_uuid}_"
         filename += file_id
         if self._file_format is not None:
             filename += f".{self._file_format}"
