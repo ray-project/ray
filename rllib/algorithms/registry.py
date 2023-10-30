14cb992090f5ffdbc0b775af10b52e692654e5dc
sven1977 <svenmika1977@gmail.com>
2023-10-30 19:27:59 +0100

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/algorithms/registry.py
+++ rllib/algorithms/registry.py
@@ -109,6 +109,31 @@ def get_algorithm_class(*args, **kwargs):
     pass
 
 
+def _get_algorithm_class(alg: str) -> type:
+    # This helps us get around a circular import (tune calls rllib._register_all when
+    # checking if a rllib Trainable is registered)
+    if alg in ALGORITHMS:
+        return ALGORITHMS[alg]()[0]
+    elif alg == "script":
+        from ray.tune import script_runner
+
+        return script_runner.ScriptRunner
+    elif alg == "__fake":
+        from ray.rllib.algorithms.mock import _MockTrainer
+
+        return _MockTrainer
+    elif alg == "__sigmoid_fake_data":
+        from ray.rllib.algorithms.mock import _SigmoidFakeData
+
+        return _SigmoidFakeData
+    elif alg == "__parameter_tuning":
+        from ray.rllib.algorithms.mock import _ParameterTuningTrainer
+
+        return _ParameterTuningTrainer
+    else:
+        raise Exception("Unknown algorithm {}.".format(alg))
+
+
 # Mapping from policy name to where it is located, relative to rllib.algorithms.
 # TODO(jungong) : Finish migrating all the policies to PolicyV2, so we can list
 # all the TF eager policies here.
