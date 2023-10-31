c228ad31cfd4d7feec7bc41323e7ffc655815ccb
Jonathan Nitisastro <jonathancn@anyscale.com>
2023-10-31 15:04:35 -0700

    move old head node detection to before on_done
    Signed-off-by: Jonathan Nitisastro <jonathancn@anyscale.com>

--- src/ray/gcs/gcs_server/gcs_node_manager.cc
+++ src/ray/gcs/gcs_server/gcs_node_manager.cc
@@ -53,23 +53,23 @@ void GcsNodeManager::HandleRegisterNode(rpc::RegisterNodeRequest request,
   RAY_LOG(INFO) << "Registering node info, node id = " << node_id
                 << ", address = " << request.node_info().node_manager_address()
                 << ", node name = " << request.node_info().node_name();
+  if (request.node_info().is_head_node()) {
+    // mark the old head node as dead
+    auto head_node_it =
+        std::find_if(alive_nodes_.begin(), alive_nodes_.end(), [](const auto &node) {
+          return node.second->is_head_node();
+        });
+    if (head_node_it != alive_nodes_.end()) {
+      NodeID old_head_node_id = head_node_it->first;
+      OnNodeFailure(old_head_node_id);
+    }
+  }
   auto on_done = [this, node_id, request, reply, send_reply_callback](
                      const Status &status) {
     RAY_CHECK_OK(status);
     RAY_LOG(INFO) << "Finished registering node info, node id = " << node_id
                   << ", address = " << request.node_info().node_manager_address()
                   << ", node name = " << request.node_info().node_name();
-    if (request.node_info().is_head_node()) {
-      // mark the old head node as dead
-      auto head_node_it =
-          std::find_if(alive_nodes_.begin(), alive_nodes_.end(), [](const auto &node) {
-            return node.second->is_head_node();
-          });
-      if (head_node_it != alive_nodes_.end()) {
-        NodeID old_head_node_id = head_node_it->first;
-        OnNodeFailure(old_head_node_id);
-      }
-    }
     RAY_CHECK_OK(gcs_publisher_->PublishNodeInfo(node_id, request.node_info(), nullptr));
     AddNode(std::make_shared<rpc::GcsNodeInfo>(request.node_info()));
     GCS_RPC_SEND_REPLY(send_reply_callback, reply, status);
