ca2e5da3a96985456736fd261311b6f716450cd7
Andrew Xue <andewzxue@gmail.com>
2023-10-31 14:44:20 -0700

    all to all api
    Signed-off-by: Andrew Xue <andewzxue@gmail.com>

--- python/ray/data/_internal/util.py
+++ python/ray/data/_internal/util.py
@@ -452,6 +452,28 @@ def ConsumptionAPI(*args, **kwargs):
     return _consumption_api(*args, **kwargs)
 
 
+def _all_to_all_api():
+    """Annotate the function with an indication that it's a all to all API, and that it
+    is an operation that requires all inputs to be materialized in-memory to execute.
+    """
+    def wrap(obj):
+        _insert_doc_at_pattern(
+            obj,
+            message="This operation requires all inputs to be materialized in object store to execute.",
+            pattern="Examples:",
+            insert_after=False,
+            directive="note",
+        )
+    return wrap
+
+
+def AllToAllAPI():
+    """Annotate the function with an indication that it's a all to all API, and that it
+    is an operation that requires all inputs to be materialized in-memory to execute.
+    """
+    return _all_to_all_api()
+
+
 def _split_list(arr: List[Any], num_splits: int) -> List[List[Any]]:
     """Split the list into `num_splits` lists.
 
