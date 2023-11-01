f5c59745d00982835feb145d14d1f9e0d4b0db6c
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2023-11-01 14:05:25 -0700

    [cd] tag anyscale image with all sort of aliases (#40862)
    Build anyscale images with all possible alias tags. The release test infra uses some of the alias and not the canonical tag. I'll remove this alias logic once we can get release test infra to use only the canonical tag (needs to completely deprecate its old version first).
    
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/anyscale_docker_container.py
+++ ci/ray_ci/anyscale_docker_container.py
@@ -17,12 +17,20 @@ class AnyscaleDockerContainer(DockerContainer):
         anyscale_image = f"{ecr}/anyscale/{self.image_type}:{tag}"
         requirement = self._get_requirement_file()
 
-        self.run_script(
-            [
-                f"./ci/build/build-anyscale-docker.sh "
-                f"{ray_image} {anyscale_image} {requirement} {ecr}",
+        cmds = [
+            f"./ci/build/build-anyscale-docker.sh "
+            f"{ray_image} {anyscale_image} {requirement} {ecr}",
+        ]
+        # TODO(can): remove the alias when release test infra uses only the canonical
+        # tag
+        for alias in self._get_image_tags():
+            alias_image = f"{ecr}/anyscale/{self.image_type}:{alias}"
+            cmds += [
+                f"docker tag {anyscale_image} {alias_image}",
+                f"docker push {alias_image}",
             ]
-        )
+
+        self.run_script(cmds)
 
     def _get_requirement_file(self) -> str:
         prefix = "requirements" if self.image_type == "ray" else "requirements_ml"
