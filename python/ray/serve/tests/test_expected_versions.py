0d0678cbc45d346fc78ef7b2ff483870dc2e0c0c
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-11-01 17:17:37 -0500

    Build nightly `pydantic` version for `Pydantic >= 2.0` test build (#40831)
    Removes the hacky monkeypatch workaround and instead fully tests Pydantic with the pickling fixes included.
    
    Upon the next Pydantic release, we can remove the logic to build from source and just install `2.5.0`.
    
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/tests/test_expected_versions.py
+++ python/ray/serve/tests/test_expected_versions.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+import pydantic
 import pytest
 
 
@@ -18,6 +19,22 @@ def test_expected_python_version():
     assert expected_python_version == actual_version, (
         f"expected_python_version={expected_python_version}, "
         f"actual_version={actual_version}"
+    )
+
+
+@pytest.mark.skipif(
+    "EXPECTED_PYDANTIC_VERSION" not in os.environ,
+    reason="EXPECTED_PYDANTIC_VERSION environment variable not set.",
+)
+def test_expected_pydantic_version():
+    """
+    Sanity check that tests are running using the expected Pydantic version.
+    """
+    expected_pydantic_version = os.environ["EXPECTED_PYDANTIC_VERSION"]
+    actual_pydantic_version = pydantic.__version__
+    assert expected_pydantic_version == actual_pydantic_version, (
+        f"expected_pydantic_version={expected_pydantic_version}, "
+        f"actual_pydantic_version={actual_pydantic_version}"
     )
 
 
