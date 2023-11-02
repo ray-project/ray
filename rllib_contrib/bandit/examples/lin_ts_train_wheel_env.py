99cc9fa41c2516bd4bf74234f4da5944e0beb4f2
Sven Mika <svenmika1977@gmail.com>
2023-11-02 19:47:49 +0100

    [RLlib, rllib_contrib] Issue errors (instead of warnings) when using `rllib_contrib` algos. (#40789)

--- rllib_contrib/bandit/examples/lin_ts_train_wheel_env.py
+++ rllib_contrib/bandit/examples/lin_ts_train_wheel_env.py
@@ -3,10 +3,11 @@
 """
 
 import argparse
+
 import numpy as np
 from matplotlib import pyplot as plt
+from rllib_bandit.bandit import BanditLinTSConfig
 
-from ray.rllib.algorithms.bandit.bandit import BanditLinTSConfig
 from ray.rllib.examples.env.bandit_envs_discrete import WheelBanditEnv
 from ray.rllib.utils.metrics.learner_info import LEARNER_INFO
 
