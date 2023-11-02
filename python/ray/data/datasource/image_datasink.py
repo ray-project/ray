088d3384d7366f0f7bc0c4dce5cd209e4fe8b10c
Balaji Veeramani <balaji@anyscale.com>
2023-11-02 02:08:47 -0700

    Appease lint
    Signed-off-by: Balaji Veeramani <balaji@anyscale.com>

--- python/ray/data/datasource/image_datasink.py
+++ python/ray/data/datasource/image_datasink.py
@@ -1,21 +1,19 @@
 import io
-from typing import Any, Dict, Optional
+from typing import Any, Dict
 
 import pyarrow
 
-from ray.data.datasource.block_path_provider import BlockWritePathProvider
 from ray.data.datasource.file_datasink import RowBasedFileDatasink
-from ray.data.datasource.filename_provider import FilenameProvider
 
 
 class _ImageDatasink(RowBasedFileDatasink):
     def __init__(
         self, path: str, column: str, file_format: str, **file_datasink_kwargs
     ):
+        super().__init__(path, **file_datasink_kwargs)
+
         self.column = column
         self.file_format = file_format
-
-        super().__init__(path, **file_datasink_kwargs)
 
     def write_row_to_file(self, row: Dict[str, Any], file: "pyarrow.NativeFile"):
         from PIL import Image
