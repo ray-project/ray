52362ddc3b1c491a1df9056328745c1d820da273
Stephanie Wang <swang@cs.berkeley.edu>
2023-11-07 15:31:39 -0800

    unit test
    Signed-off-by: Stephanie Wang <swang@cs.berkeley.edu>

--- python/ray/data/tests/test_block_sizing.py
+++ python/ray/data/tests/test_block_sizing.py
@@ -122,45 +122,91 @@ SHUFFLE_ALL_TO_ALL_OPS = [
     "shuffle_op",
     SHUFFLE_ALL_TO_ALL_OPS,
 )
-def test_shuffle(ray_start_regular_shared, restore_data_context, shuffle_op):
+def test_shuffle(shutdown_only, restore_data_context, shuffle_op):
+    ray.init(
+        _system_config={
+            "max_direct_call_object_size": 1000,
+        },
+        num_cpus=2,
+        object_store_memory=int(100e6),
+    )
+
     # Test AllToAll and Map -> AllToAll Datasets. Check that Map inherits
     # AllToAll's target block size.
     ctx = DataContext.get_current()
     ctx.min_parallelism = 1
     ctx.target_min_block_size = 1
-    mem_size = 8000
+    mem_size = 800_000
     shuffle_fn, kwargs, fusion_supported = shuffle_op
 
-    ctx.target_shuffle_max_block_size = 100 * 8
+    ctx.target_shuffle_max_block_size = 10_000 * 8
     num_blocks_expected = mem_size // ctx.target_shuffle_max_block_size
-    # ds = shuffle_fn(ray.data.range(1000), **kwargs)
-    # assert ds.materialize().num_blocks() == num_blocks_expected
-    ds = shuffle_fn(ray.data.range(1000).map(lambda x: x), **kwargs)
+    block_size_expected = ctx.target_shuffle_max_block_size
+    cursor = get_initial_core_execution_metrics_cursor()
+
+    ds = shuffle_fn(ray.data.range(100_000), **kwargs).materialize()
+    assert num_blocks_expected <= ds.num_blocks() <= num_blocks_expected * 1.5
+    cursor = assert_blocks_expected_in_plasma(
+        cursor,
+        # Dataset.sort produces some empty intermediate blocks because the
+        # input range is already partially sorted.
+        num_blocks_expected**2,
+        # Data is written out once before map phase if fusion is disabled, once
+        # during map phase, once during reduce phase.
+        total_bytes_expected=mem_size * 2 + (0 if fusion_supported else mem_size),
+    )
+
+    ds = shuffle_fn(ray.data.range(100_000).map(lambda x: x), **kwargs).materialize()
     if not fusion_supported:
         # TODO(swang): For some reason BlockBuilder's estimated
         # memory usage for range(1000)->map is 2x the actual memory usage.
         num_blocks_expected *= 2
-    assert ds.materialize().num_blocks() == num_blocks_expected
+        block_size_expected //= 2
+    assert num_blocks_expected <= ds.num_blocks() <= num_blocks_expected * 1.5
+    cursor = assert_blocks_expected_in_plasma(
+        cursor,
+        num_blocks_expected**2,
+        total_bytes_expected=mem_size * 2 + (0 if fusion_supported else mem_size),
+    )
 
-    ctx.target_shuffle_max_block_size /= 2
+    ctx.target_shuffle_max_block_size *= 2
     num_blocks_expected = mem_size // ctx.target_shuffle_max_block_size
-    ds = shuffle_fn(ray.data.range(1000), **kwargs)
-    assert ds.materialize().num_blocks() == num_blocks_expected
-    ds = shuffle_fn(ray.data.range(1000).map(lambda x: x), **kwargs)
+    block_size_expected = ctx.target_shuffle_max_block_size
+
+    ds = shuffle_fn(ray.data.range(100_000), **kwargs).materialize()
+    assert num_blocks_expected <= ds.num_blocks() <= num_blocks_expected * 1.5
+    cursor = assert_blocks_expected_in_plasma(
+        cursor,
+        num_blocks_expected**2,
+        total_bytes_expected=mem_size * 2 + (0 if fusion_supported else mem_size),
+    )
+
+    ds = shuffle_fn(ray.data.range(100_000).map(lambda x: x), **kwargs).materialize()
     if not fusion_supported:
         num_blocks_expected *= 2
-    assert ds.materialize().num_blocks() == num_blocks_expected
+        block_size_expected //= 2
+    assert num_blocks_expected <= ds.num_blocks() <= num_blocks_expected * 1.5
+    cursor = assert_blocks_expected_in_plasma(
+        cursor,
+        num_blocks_expected**2,
+        total_bytes_expected=mem_size * 2 + (0 if fusion_supported else mem_size),
+    )
 
     # Setting target max block size does not affect map ops when there is a
     # shuffle downstream.
-    ctx.target_max_block_size = 200 * 8
+    ctx.target_max_block_size = ctx.target_shuffle_max_block_size * 2
     num_blocks_expected = mem_size // ctx.target_shuffle_max_block_size
-    ds = shuffle_fn(ray.data.range(1000), **kwargs)
-    assert ds.materialize().num_blocks() == num_blocks_expected
+    block_size_expected = ctx.target_shuffle_max_block_size
     if not fusion_supported:
         num_blocks_expected *= 2
-    ds = shuffle_fn(ray.data.range(1000).map(lambda x: x), **kwargs)
-    assert ds.materialize().num_blocks() == num_blocks_expected
+        block_size_expected //= 2
+    ds = shuffle_fn(ray.data.range(100_000).map(lambda x: x), **kwargs).materialize()
+    assert num_blocks_expected <= ds.num_blocks() <= num_blocks_expected * 1.5
+    cursor = assert_blocks_expected_in_plasma(
+        cursor,
+        num_blocks_expected**2,
+        total_bytes_expected=mem_size * 2 + (0 if fusion_supported else mem_size),
+    )
 
 
 if __name__ == "__main__":
