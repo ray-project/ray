183dfbb65ce4ad50cb92f634f51d6bb0814c8dc2
Yi Cheng <74173148+iycheng@users.noreply.github.com>
2023-11-07 18:01:36 +0000

    format Signed-off-by: Yi Cheng <74173148+iycheng@users.noreply.github.com>

--- python/ray/_private/runtime_env/mpi.py
+++ python/ray/_private/runtime_env/mpi.py
@@ -42,7 +42,9 @@ class MPIPlugin(RuntimeEnvPlugin):
         # mpirun -n 10 python mpi.py worker_entry_func
 
         worker_entry = mpi_config.get("worker_entry", None)
-        assert worker_entry is not None, "`worker_entry` must be setup in the runtime env."
+        assert (
+            worker_entry is not None
+        ), "`worker_entry` must be setup in the runtime env."
         assertPath(worker_entry).is_file(), "`worker_entry` must be a file."
 
         cmds = (
