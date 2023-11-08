a77131a8c7ce1f34d495b5d45a6e9b18772a352f
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-11-08 15:39:06 -0600

    Add first integration tests
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

---
+++ python/ray/serve/tests/test_target_capacity.py
@@ -0,0 +1,194 @@
+import sys
+
+import pytest
+
+import ray
+import ray.actor
+from ray import serve
+from ray._private.test_utils import wait_for_condition
+from ray.serve._private.client import ServeControllerClient
+from ray.serve._private.common import ApplicationStatus
+from ray.serve._private.constants import SERVE_DEFAULT_APP_NAME
+from ray.serve.context import _get_global_client
+from ray.serve.schema import ServeApplicationSchema, ServeDeploySchema
+
+INGRESS_DEPLOYMENT_NAME = "ingress"
+INGRESS_DEPLOYMENT_NUM_REPLICAS = 6
+DOWNSTREAM_DEPLOYMENT_NAME = "downstream"
+DOWNSTREAM_DEPLOYMENT_NUM_REPLICAS = 2
+
+
+@pytest.fixture
+def shutdown_ray_and_serve():
+    serve.shutdown()
+    if ray.is_initialized():
+        ray.shutdown()
+    yield
+    serve.shutdown()
+    if ray.is_initialized():
+        ray.shutdown()
+
+
+@serve.deployment(ray_actor_options={"num_cpus": 0})
+class DummyDeployment:
+    def __init__(self, *args):
+        pass
+
+    def __call__(self, *args):
+        return "hi"
+
+
+test_app = DummyDeployment.options(
+    name=INGRESS_DEPLOYMENT_NAME,
+    num_replicas=INGRESS_DEPLOYMENT_NUM_REPLICAS,
+).bind(
+    DummyDeployment.options(
+        name=DOWNSTREAM_DEPLOYMENT_NAME,
+        num_replicas=DOWNSTREAM_DEPLOYMENT_NUM_REPLICAS,
+    ).bind()
+)
+
+
+def check_expected_num_replicas(
+    ingress_replicas: int, downstream_replicas: int
+) -> bool:
+    status = serve.status()
+    assert SERVE_DEFAULT_APP_NAME in status.applications
+
+    application = status.applications["default"]
+    assert application.status == ApplicationStatus.RUNNING
+
+    assert INGRESS_DEPLOYMENT_NAME in application.deployments
+    assert (
+        sum(application.deployments[INGRESS_DEPLOYMENT_NAME].replica_states.values())
+        == ingress_replicas
+    )
+
+    assert DOWNSTREAM_DEPLOYMENT_NAME in application.deployments
+    assert (
+        sum(application.deployments[DOWNSTREAM_DEPLOYMENT_NAME].replica_states.values())
+        == downstream_replicas
+    )
+
+    return True
+
+
+@pytest.fixture
+def client() -> ServeControllerClient:
+    serve.start()
+    yield _get_global_client()
+
+
+def test_incremental_scale_up(shutdown_ray_and_serve, client: ServeControllerClient):
+    config = ServeDeploySchema(
+        applications=[
+            ServeApplicationSchema(
+                import_path="ray.serve.tests.test_target_capacity:test_app"
+            )
+        ]
+    )
+
+    # Initially deploy at target_capacity 0, should have 1 replica of each.
+    config.target_capacity = 0.0
+    client.deploy_apps(config)
+    wait_for_condition(
+        check_expected_num_replicas,
+        ingress_replicas=1,
+        downstream_replicas=1,
+    )
+
+    # Increase target_capacity to 50, ingress deployment should scale up.
+    config.target_capacity = 50.0
+    client.deploy_apps(config)
+    wait_for_condition(
+        check_expected_num_replicas,
+        ingress_replicas=INGRESS_DEPLOYMENT_NUM_REPLICAS / 2,
+        downstream_replicas=DOWNSTREAM_DEPLOYMENT_NUM_REPLICAS / 2,
+    )
+
+    # Increase target_capacity to 100, both should fully scale up.
+    config.target_capacity = 100.0
+    client.deploy_apps(config)
+    wait_for_condition(
+        check_expected_num_replicas,
+        ingress_replicas=INGRESS_DEPLOYMENT_NUM_REPLICAS,
+        downstream_replicas=DOWNSTREAM_DEPLOYMENT_NUM_REPLICAS,
+    )
+
+    # Finish rollout (remove target_capacity), should have no effect.
+    config.target_capacity = None
+    client.deploy_apps(config)
+    assert check_expected_num_replicas(
+        ingress_replicas=INGRESS_DEPLOYMENT_NUM_REPLICAS,
+        downstream_replicas=DOWNSTREAM_DEPLOYMENT_NUM_REPLICAS,
+    )
+
+
+def test_incremental_scale_down(shutdown_ray_and_serve, client: ServeControllerClient):
+    config = ServeDeploySchema(
+        applications=[
+            ServeApplicationSchema(
+                import_path="ray.serve.tests.test_target_capacity:test_app"
+            )
+        ]
+    )
+
+    # Initially deploy with no target_capacity (full scale).
+    client.deploy_apps(config)
+    wait_for_condition(
+        check_expected_num_replicas,
+        ingress_replicas=INGRESS_DEPLOYMENT_NUM_REPLICAS,
+        downstream_replicas=DOWNSTREAM_DEPLOYMENT_NUM_REPLICAS,
+    )
+
+    # Decrease target_capacity to 50, both deployments should scale down.
+    config.target_capacity = 50.0
+    client.deploy_apps(config)
+    wait_for_condition(
+        check_expected_num_replicas,
+        ingress_replicas=INGRESS_DEPLOYMENT_NUM_REPLICAS / 2,
+        downstream_replicas=DOWNSTREAM_DEPLOYMENT_NUM_REPLICAS / 2,
+    )
+
+    # Decrease target_capacity to 0, both should fully scale down to zero.
+    config.target_capacity = 0.0
+    client.deploy_apps(config)
+    wait_for_condition(
+        check_expected_num_replicas,
+        ingress_replicas=1,
+        downstream_replicas=1,
+    )
+
+
+def test_controller_recover_target_capacity(
+    shutdown_ray_and_serve, client: ServeControllerClient
+):
+    config = ServeDeploySchema(
+        applications=[
+            ServeApplicationSchema(
+                import_path="ray.serve.tests.test_target_capacity:test_app"
+            )
+        ]
+    )
+
+    # Deploy with target_capacity 50, both deployments should be at half scale.
+    config.target_capacity = 50.0
+    client.deploy_apps(config)
+    wait_for_condition(
+        check_expected_num_replicas,
+        ingress_replicas=INGRESS_DEPLOYMENT_NUM_REPLICAS / 2,
+        downstream_replicas=DOWNSTREAM_DEPLOYMENT_NUM_REPLICAS / 2,
+    )
+
+    ray.kill(client._controller, no_restart=False)
+
+    # Verify that the target_capacity is recovered after the controller comes back.
+    wait_for_condition(lambda: serve.status().target_capacity == 50.0)
+    assert check_expected_num_replicas(
+        ingress_replicas=INGRESS_DEPLOYMENT_NUM_REPLICAS / 2,
+        downstream_replicas=DOWNSTREAM_DEPLOYMENT_NUM_REPLICAS / 2,
+    )
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main(["-v", "-s", __file__]))
