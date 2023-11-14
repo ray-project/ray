c888e5df9a1c5e6eeb427fe84e4ea2dec1ff9118
woshiyyya <xiaoyunxuan1998@gmail.com>
2023-11-14 11:54:25 -0800

    update multi-node release test
    Signed-off-by: woshiyyya <xiaoyunxuan1998@gmail.com>

--- release/ray_release/byod/byod_train_trainium.sh
+++ release/ray_release/byod/byod_train_trainium.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+# Configure EFA installer
+sudo apt-get install curl
+curl -O https://efa-installer.amazonaws.com/aws-efa-installer-latest.tar.gz
+wget https://efa-installer.amazonaws.com/aws-efa-installer.key && gpg --import aws-efa-installer.key
+cat aws-efa-installer.key | gpg --fingerprint
+wget https://efa-installer.amazonaws.com/aws-efa-installer-latest.tar.gz.sig && gpg --verify ./aws-efa-installer-latest.tar.gz.sig
+tar -xvf aws-efa-installer-latest.tar.gz
+cd aws-efa-installer && sudo bash efa_installer.sh --yes --skip-kmod
+cd
+sudo rm -rf aws-efa-installer-latest.tar.gz aws-efa-installer
 
 # Configure Linux for Neuron repository updates
-. /etc/os-release && \
-echo "deb https://apt.repos.neuron.amazonaws.com ${VERSION_CODENAME} main" | \
-sudo tee /etc/apt/sources.list.d/neuron.list > /dev/null
-
+. /etc/os-release
+sudo tee /etc/apt/sources.list.d/neuron.list > /dev/null <<EOF
+deb https://apt.repos.neuron.amazonaws.com ${VERSION_CODENAME} main
+EOF
 wget -qO - https://apt.repos.neuron.amazonaws.com/GPG-PUB-KEY-AMAZON-AWS-NEURON.PUB | sudo apt-key add -
 
 # Update OS packages 
@@ -17,6 +26,9 @@ sudo apt-get install aws-neuronx-runtime-lib=2.* -y
 # Install Neuron Tools 
 sudo apt-get install aws-neuronx-tools=2.* -y
 
-# Install neuronx and torch_xla
 pip config set global.extra-index-url https://pip.repos.neuron.amazonaws.com
 pip install neuronx-cc==2.* torch-neuronx torchvision
+
+export FI_PROVIDER=efa
+export FI_EFA_USE_DEVICE_RDMA=1
+export LD_LIBRARY_PATH=/opt/amazon/efa/lib:$LD_LIBRARY_PATH 
--- release/train_tests/trainium/test_trainium.py
+++ release/train_tests/trainium/test_trainium.py
@@ -1,56 +1,118 @@
+import os
+import time
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch_xla.core.xla_model as xm
-import torch_xla.distributed.xla_backend  # noqa: F401
+from torch import nn
 
 from ray.train import ScalingConfig
-from ray.train.torch import TorchTrainer, prepare_model
+from ray.train.torch import TorchTrainer
 from ray.train.torch.xla import TorchXLAConfig
 
+from torchvision.datasets import mnist
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
 
-class Model(nn.Module):
-    def __init__(self):
-        super(Model, self).__init__()
-        self.net1 = nn.Linear(10, 10)
-        self.relu = nn.ReLU()
-        self.net2 = nn.Linear(10, 5)
+# XLA imports
+import torch_xla.core.xla_model as xm
+
+# XLA imports for parallel loader and multi-processing
+import torch_xla.distributed.parallel_loader as pl
+from torch.utils.data.distributed import DistributedSampler
+
+# Global constants
+EPOCHS = 4
+WARMUP_STEPS = 2
+BATCH_SIZE = 32
+
+
+class MLP(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(28 * 28, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10),
+            nn.ReLU(),
+        )
 
     def forward(self, x):
-        return self.net2(self.relu(self.net1(x)))
+        return self.layers(x)
 
 
 def train_func():
-    device = xm.xla_device()
-    rank = xm.get_ordinal()
-
-    # Create the model and move to device
-    model = Model().to(device)
-    ddp_model = prepare_model(
-        model,
-        move_to_device=False,
-        parallel_strategy_kwargs={"gradient_as_bucket_view": True},
+    # Load MNIST train dataset
+    if not xm.is_master_ordinal():
+        xm.rendezvous("dataset_download")
+    train_dataset = mnist.MNIST(
+        root="/tmp/MNIST_DATA_train", train=True, download=True, transform=ToTensor()
     )
+    if xm.is_master_ordinal():
+        xm.rendezvous("dataset_download")
 
-    loss_fn = nn.MSELoss()
-    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
-    for step in range(5):
-        optimizer.zero_grad()
-        outputs = ddp_model(torch.randn(20, 10).to(device))
-        labels = torch.randn(20, 5).to(device)
-        loss = loss_fn(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        xm.mark_step()
-        if rank == 0:
-            print(f"Loss after step {step}: {loss.cpu()}")
+    # XLA MP: get world size
+    world_size = xm.xrt_world_size()
+    # multi-processing: ensure each worker has same initial weights
+    torch.manual_seed(0)
+
+    # Move model to device and declare optimizer and loss function
+    device = "xla"
+    model = MLP().to(device)
+    # For multiprocessing, scale up learning rate
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-5)
+    loss_fn = torch.nn.NLLLoss()
+
+    # Prepare data loader
+    train_sampler = None
+    if world_size > 1:
+        train_sampler = DistributedSampler(
+            train_dataset, num_replicas=world_size, rank=xm.get_ordinal(), shuffle=True
+        )
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=BATCH_SIZE,
+        sampler=train_sampler,
+        shuffle=False if train_sampler else True,
+    )
+    # XLA MP: use MpDeviceLoader from torch_xla.distributed
+    train_device_loader = pl.MpDeviceLoader(train_loader, device)
+
+    # Run the training loop
+    print("----------Training ---------------")
+    model.train()
+    for epoch in range(EPOCHS):
+        start = time.time()
+        for idx, (train_x, train_label) in enumerate(train_device_loader):
+            optimizer.zero_grad()
+            train_x = train_x.view(train_x.size(0), -1)
+            output = model(train_x)
+            loss = loss_fn(output, train_label)
+            loss.backward()
+            xm.optimizer_step(
+                optimizer
+            )  # XLA MP: performs grad allreduce and optimizer step
+            if idx < WARMUP_STEPS:  # skip warmup iterations
+                start = time.time()
+
+        # Compute statistics for the last epoch
+        interval = len(train_device_loader) - WARMUP_STEPS  # skip warmup iterations
+        throughput = interval / (time.time() - start)
+        print("Train throughput (iter/sec): {}".format(throughput))
+        print("Final loss is {:0.4f}".format(loss.detach().to("cpu")))
+
+    # Save checkpoint for evaluation (xm.save ensures only one process save)
+    os.makedirs("checkpoints", exist_ok=True)
+    checkpoint = {"state_dict": model.state_dict()}
+    xm.save(checkpoint, "checkpoints/checkpoint.pt")
+
+    print("----------End Training ---------------")
 
 
+# trn1.32xlarge -> 32 neuron_cores, 128 CPU
+# 2x trn1.32xlarge
 trainer = TorchTrainer(
     train_loop_per_worker=train_func,
     torch_config=TorchXLAConfig(),
     scaling_config=ScalingConfig(
-        num_workers=2, resources_per_worker={"neuron_cores": 1}
+        num_workers=64, resources_per_worker={"neuron_cores": 1}
     ),
 )
 result = trainer.fit()
