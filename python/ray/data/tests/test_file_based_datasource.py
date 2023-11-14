50f2403dffd25c6596d9651af9938b319bda0181
Balaji Veeramani <balaji@anyscale.com>
2023-11-13 22:36:40 -0800

    Add test
    Signed-off-by: Balaji Veeramani <balaji@anyscale.com>

--- python/ray/data/tests/test_file_based_datasource.py
+++ python/ray/data/tests/test_file_based_datasource.py
@@ -1,10 +1,38 @@
 import pytest
 
 import ray
+from ray.data._internal.delegating_block_builder import DelegatingBlockBuilder
+from ray.data.block import Block
 from ray.data.datasource.file_based_datasource import (
     OPEN_FILE_MAX_ATTEMPTS,
+    FileBasedDatasource,
     _open_file_with_retry,
 )
+
+
+class MockFileBasedDatasource(FileBasedDatasource):
+    def _read_file(self, f: "pyarrow.NativeFile", path: str) -> Block:
+        builder = DelegatingBlockBuilder()
+        builder.add({"data": f.readall()})
+        return builder.build()
+
+
+def test_file_extensions(ray_start_regular_shared, tmp_path):
+    csv_path = os.path.join(tmp_path, "file.csv")
+    with open(csv_path, "w") as file:
+        file.write("spam")
+
+    txt_path = os.path.join(tmp_path, "file.txt")
+    with open(txt_path, "w") as file:
+        file.write("ham")
+
+    datasource = MockFileBasedDatasource([csv_path, txt_path], file_extensions=None)
+    assert datasource.input_files() == [csv_path, txt_path]
+
+    datasource = MockFileBasedDatasource(
+        ["file.csv", "file.txt"], file_extensions=["csv"]
+    )
+    assert datasource.input_files() == [csv_path]
 
 
 def test_open_file_with_retry(ray_start_regular_shared):
