2d8426056b323bd0478a136b52ae04ea3adaed65
Shreyas Krishnaswamy <shrekris@anyscale.com>
2023-11-16 12:28:56 -0800

    Add unit test for initial_replicas handling
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- python/ray/serve/tests/test_target_capacity.py
+++ python/ray/serve/tests/test_target_capacity.py
@@ -1,5 +1,6 @@
 import asyncio
 import sys
+from copy import deepcopy
 from typing import Dict
 
 import pytest
@@ -7,6 +8,7 @@ import pytest
 import ray
 from ray import serve
 from ray._private.test_utils import wait_for_condition
+from ray.serve import Application
 from ray.serve._private.client import ServeControllerClient
 from ray.serve._private.common import ApplicationStatus
 from ray.serve._private.constants import SERVE_DEFAULT_APP_NAME
@@ -19,6 +21,10 @@ DOWNSTREAM_DEPLOYMENT_NAME = "downstream"
 DOWNSTREAM_DEPLOYMENT_NUM_REPLICAS = 2
 SCALE_TO_ZERO_DEPLOYMENT_NAME = "zero"
 SCALE_TO_ZERO_DEPLOYMENT_MAX_REPLICAS = 4
+START_AT_10_DEPLOYMENT_NAME = "start_at_ten"
+START_AT_10_DEPLOYMENT_INITIAL_REPLICAS = 10
+START_AT_10_DEPLOYMENT_MIN_REPLICAS = 0
+START_AT_10_DEPLOYMENT_MAX_REPLICAS = 20
 
 
 @pytest.fixture
@@ -52,11 +58,18 @@ test_app = DummyDeployment.options(
 )
 
 
-def check_expected_num_replicas(deployment_to_num_replicas: Dict[str, int]) -> bool:
-    status = serve.status()
-    assert SERVE_DEFAULT_APP_NAME in status.applications
+def check_expected_num_replicas(
+    deployment_to_num_replicas: Dict[str, int], app_name: str = SERVE_DEFAULT_APP_NAME
+) -> bool:
+    """Checks that the expected number of replicas for each deployment is running.
 
-    application = status.applications[SERVE_DEFAULT_APP_NAME]
+    Looks for the deployments in the app named app_name.
+    """
+
+    status = serve.status()
+    assert app_name in status.applications
+
+    application = status.applications[app_name]
     assert application.status == ApplicationStatus.RUNNING
 
     for name, num_replicas in deployment_to_num_replicas.items():
@@ -307,6 +320,227 @@ def test_autoscaling_scale_to_zero(
             SCALE_TO_ZERO_DEPLOYMENT_NAME: 0,
         },
     )
+
+
+@serve.deployment(
+    ray_actor_options={"num_cpus": 0},
+    max_concurrent_queries=2,
+    graceful_shutdown_timeout_s=0,
+)
+class HangDeployment:
+    async def __call__(self, *args):
+        await asyncio.sleep(10000)
+
+
+def create_hang_app(
+    name: str, min_replicas: int, initial_replicas: int, max_replicas: int
+) -> Application:
+    return HangDeployment.options(
+        name=name,
+        autoscaling_config={
+            "min_replicas": min_replicas,
+            "initial_replicas": initial_replicas,
+            "max_replicas": max_replicas,
+            "target_num_ongoing_requests_per_replicas": 1,
+        },
+    ).bind()
+
+
+class TestInitialReplicasHandling:
+    def deploy_config_and_wait_for_target_capacity(
+        self,
+        client: ServeControllerClient,
+        config: ServeDeploySchema,
+        target_capacity: float,
+    ):
+        config.target_capacity = target_capacity
+        client.deploy_apps(config)
+        wait_for_condition(lambda: serve.status().target_capacity == target_capacity)
+
+    def test_initial_replicas_scales_down(
+        self, shutdown_ray_and_serve, client: ServeControllerClient
+    ):
+        deployment_name = "start_at_ten"
+
+        config = ServeDeploySchema(
+            applications=[
+                ServeApplicationSchema(
+                    import_path="ray.serve.tests.test_target_capacity:create_hang_app",
+                    args={
+                        "name": deployment_name,
+                        "min_replicas": 0,
+                        "initial_replicas": 10,
+                        "max_replicas": 20,
+                    },
+                ),
+            ],
+        )
+
+        wait_for_condition(lambda: serve.status().target_capacity is None)
+
+        test_target_capacities = [100, 60, 20, 0]
+        expected_num_replicas = [1] * len(test_target_capacities)
+
+        for target_capacity, num_replicas in zip(
+            test_target_capacities, expected_num_replicas
+        ):
+            config.target_capacity = target_capacity
+            client.deploy_apps(config)
+            wait_for_condition(
+                lambda: serve.status().target_capacity == target_capacity
+            )
+            wait_for_condition(
+                check_expected_num_replicas({deployment_name: num_replicas})
+            )
+
+    def test_initial_replicas_scales_up_and_down(
+        self, shutdown_ray_and_serve, client: ServeControllerClient
+    ):
+        deployment_name = "start_at_ten"
+        min_replicas = 0
+        initial_replicas = 10
+
+        config = ServeDeploySchema(
+            target_capacity=0,
+            applications=[
+                ServeApplicationSchema(
+                    import_path="ray.serve.tests.test_target_capacity:create_hang_app",
+                    args={
+                        "name": deployment_name,
+                        "min_replicas": min_replicas,
+                        "initial_replicas": initial_replicas,
+                        "max_replicas": 2 * initial_replicas,
+                    },
+                ),
+            ],
+        )
+
+        test_target_capacities = [0, 20, 60, 40, 30, 100, None]
+        expected_num_replicas = [1, 2, 6, 1, 1, initial_replicas, min_replicas]
+
+        for target_capacity, num_replicas in zip(
+            test_target_capacities, expected_num_replicas
+        ):
+            config.target_capacity = target_capacity
+            client.deploy_apps(config)
+            wait_for_condition(
+                lambda: serve.status().target_capacity == target_capacity
+            )
+            wait_for_condition(
+                check_expected_num_replicas({deployment_name: num_replicas})
+            )
+
+    def test_initial_replicas_zero(
+        self, shutdown_ray_and_serve, client: ServeControllerClient
+    ):
+        deployment_name = "start_at_zero"
+        min_replicas = 0
+        initial_replicas = 0
+
+        config = ServeDeploySchema(
+            target_capacity=0,
+            applications=[
+                ServeApplicationSchema(
+                    import_path="ray.serve.tests.test_target_capacity:create_hang_app",
+                    args={
+                        "name": deployment_name,
+                        "min_replicas": min_replicas,
+                        "initial_replicas": initial_replicas,
+                        "max_replicas": 20,
+                    },
+                ),
+            ],
+        )
+
+        test_target_capacities = [0, 20, 60, 40, 30, 100, None]
+        expected_num_replicas = [0] * len(test_target_capacities)
+
+        for target_capacity, num_replicas in zip(
+            test_target_capacities, expected_num_replicas
+        ):
+            config.target_capacity = target_capacity
+            client.deploy_apps(config)
+            wait_for_condition(
+                lambda: serve.status().target_capacity == target_capacity
+            )
+            wait_for_condition(
+                check_expected_num_replicas({deployment_name: num_replicas})
+            )
+
+    def test_initial_replicas_new_configs(
+        self, shutdown_ray_and_serve, client: ServeControllerClient
+    ):
+        deployment_name = "start_at_ten"
+        min_replicas = 0
+        initial_replicas = 20
+        config_target_capacity = 0.4
+
+        config = ServeDeploySchema(
+            target_capacity=config_target_capacity,
+            applications=[
+                ServeApplicationSchema(
+                    name="app1",
+                    import_path="ray.serve.tests.test_target_capacity:create_hang_app",
+                    args={
+                        "name": deployment_name,
+                        "min_replicas": min_replicas,
+                        "initial_replicas": initial_replicas,
+                        "max_replicas": initial_replicas * 2,
+                    },
+                ),
+            ],
+        )
+
+        # When deploying first config, initial_replicas * target_capacity
+        # should be treated as floor
+        client.deploy_apps(config)
+        wait_for_condition(
+            lambda: serve.status().target_capacity == config_target_capacity
+        )
+        wait_for_condition(
+            check_expected_num_replicas(
+                {deployment_name: config_target_capacity * initial_replicas},
+                app_name="app1",
+            )
+        )
+
+        # When deploying a new config, initial_replicas * target_capacity
+        # should be treated as floor for all deployments, even if
+        # target_capacity is lower
+        new_deployment_name = f"new_{deployment_name}"
+        new_config_target_capacity = config_target_capacity / 2
+
+        new_config = deepcopy(config)
+        new_config.applications.append(
+            ServeApplicationSchema(
+                name="app2",
+                import_path="ray.serve.tests.test_target_capacity:create_hang_app",
+                args={
+                    "name": new_deployment_name,
+                    "min_replicas": min_replicas,
+                    "initial_replicas": initial_replicas,
+                    "max_replicas": initial_replicas * 2,
+                },
+            ),
+        )
+        new_config.target_capacity = new_config_target_capacity
+
+        client.deploy(new_config)
+        wait_for_condition(
+            lambda: serve.status().target_capacity == new_config_target_capacity
+        )
+        wait_for_condition(
+            check_expected_num_replicas(
+                {deployment_name: new_config_target_capacity * initial_replicas},
+                app_name="app1",
+            )
+        )
+        wait_for_condition(
+            check_expected_num_replicas(
+                {deployment_name: new_config_target_capacity * initial_replicas},
+                app_name="app2",
+            )
+        )
 
 
 if __name__ == "__main__":
