97ab4e60d8a78eb7fb338c9a82490dbe173555f3
Cheng Su <scnju13@gmail.com>
2023-11-20 15:37:36 -0800

    Skip doc test for read_json
    Signed-off-by: Cheng Su <scnju13@gmail.com>

--- python/ray/data/read_api.py
+++ python/ray/data/read_api.py
@@ -1040,15 +1040,10 @@ def read_json(
 
         >>> import pyarrow.json as pajson
         >>> block_size = 10 << 20 # Set block size to 10MB
-        >>> ray.data.read_json(
+        >>> ray.data.read_json( # doctest: +SKIP
         ...     "s3://anonymous@ray-example-data/log.json",
         ...     read_options=pajson.ReadOptions(block_size=block_size)
         ... )
-        Dataset(
-           num_blocks=...,
-           num_rows=1,
-           schema={timestamp: timestamp[s], size: int64}
-        )
 
     Args:
         paths: A single file or directory, or a list of file or directory paths.
