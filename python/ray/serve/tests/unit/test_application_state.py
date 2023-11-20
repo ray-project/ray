8ba01a1949ad954555b5a2e5ba00ef78e8155340
Shreyas Krishnaswamy <shrekris@anyscale.com>
2023-11-19 18:56:57 -0800

    Update deploy() method in MockDeploymentStateManager to match new API
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- python/ray/serve/tests/unit/test_application_state.py
+++ python/ray/serve/tests/unit/test_application_state.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Dict, List, Tuple
+from typing import Dict, List, Optional, Tuple
 from unittest.mock import Mock, PropertyMock, patch
 
 import pytest
@@ -16,6 +16,7 @@ from ray.serve._private.common import (
     DeploymentInfo,
     DeploymentStatus,
     DeploymentStatusInfo,
+    TargetCapacityScaleDirection,
 )
 from ray.serve._private.config import DeploymentConfig, ReplicaConfig
 from ray.serve._private.deploy_utils import deploy_args_to_deployment_info
@@ -54,7 +55,13 @@ class MockDeploymentStateManager:
                 self.deployment_statuses[name] = DeploymentStatus.UPDATING
                 self.deleting[name] = deleting
 
-    def deploy(self, deployment_id: DeploymentID, deployment_info: DeploymentInfo):
+    def deploy(
+        self,
+        deployment_id: DeploymentID,
+        deployment_info: DeploymentInfo,
+        target_capacity: Optional[float] = None,
+        target_capacity_scale_direction: Optional[TargetCapacityScaleDirection] = None,
+    ):
         existing_info = self.deployment_infos.get(deployment_id)
         self.deleting[deployment_id] = False
         self.deployment_infos[deployment_id] = deployment_info
