a7be4c74f425ed1f2df9910749a41b71847d1bfb
Xiaoshuang Liu <liuxiaoshuang4@huawei.com>
2023-11-21 15:13:59 +0800

    Add comments, enable the execution of NPU test cases in CI.
    Signed-off-by: Xiaoshuang Liu <liuxiaoshuang4@huawei.com>

--- python/ray/_private/accelerators/ascend_npu.py
+++ python/ray/_private/accelerators/ascend_npu.py
@@ -66,6 +66,11 @@ class AscendNPUAcceleratorManager(AcceleratorManager):
 
     @staticmethod
     def get_current_node_accelerator_type() -> Optional[str]:
+        '''Get the type of the Ascend NPU on the current node.
+
+        Returns:
+            A string of the type, such as "Ascend910A", "Ascend910B", "Ascend310P1".
+        '''
         try:
             import acl
 
@@ -78,6 +83,12 @@ class AscendNPUAcceleratorManager(AcceleratorManager):
     def validate_resource_request_quantity(
         quantity: float,
     ) -> Tuple[bool, Optional[str]]:
+        if quantity > 1:
+            logger.warning(
+                f"The task is requesting multiple Ascend NPUs. "
+                "If you need to build an HCCL network for NPU interconnection, "
+                "please refer to the HCCL User Manual."
+            )
         return (True, None)
 
     @staticmethod
