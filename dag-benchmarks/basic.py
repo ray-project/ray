af3a627721f730dd89a8e814d7a7e0af8dbeb7e5
Stephanie Wang <swang@cs.berkeley.edu>
2023-11-22 13:50:55 -0800

    Convert DAG basic benchmark to use OutputNode, Actor.method.bind

--- dag-benchmarks/basic.py
+++ dag-benchmarks/basic.py
@@ -3,6 +3,7 @@ import time
 
 import ray
 
+from ray.dag import InputNode, OutputNode
 
 @ray.remote
 class Actor:
@@ -12,41 +13,29 @@ class Actor:
 
     def inc(self, x):
         self.i += x
+        return self.i
 
     def get(self):
         return self.i
 
 
-@ray.remote
-class Gather:
-    def __init__(self):
-        pass
-
-    def gather(self, *vals):
-        return sum(vals)
-
-
 def run_benchmark(num_actors, num_trials):
     init_val = 10
-    # This required a change to cache actor handles in the DAG.
-    actors = [Actor.bind(init_val) for _ in range(num_actors)]
-    gather_actor = Gather.bind()
-    # 1 task for each actor.
-    vals = [a.get.bind() for a in actors]
-    # Gather results with another actor because Ray DAGs only support one
-    # return value right now.
-    ret = gather_actor.gather.bind(*vals)
+    actors = [Actor.remote(init_val) for _ in range(num_actors)]
+
+    with InputNode() as i:
+        out = [a.inc.bind(i) for a in actors]
+        dag = OutputNode(out)
 
     # Warmup.
-    for _ in range(3):
-        ref = ret.execute()
-        print(ref)
-        ray.get(ref) == init_val * num_actors
+    for i in range(3):
+        ref = dag.execute(1)
+        print(ray.get(ref))
 
     print("Starting...")
     start = time.time()
     for _ in range(num_trials):
-        ray.get(ret.execute()) == init_val * num_actors
+        ray.get(dag.execute(1))
     end = time.time()
     print(f"{num_trials} executed in {end - start}s.")
     print(f"Throughput: {num_trials / (end - start)} rounds/s.")
