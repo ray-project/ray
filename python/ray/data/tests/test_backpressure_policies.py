023f8f0ec49df0abeecd926293af4f6c7cadcd29
Hao Chen <chenh1024@gmail.com>
2023-11-21 17:43:39 -0800

    fix
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- python/ray/data/tests/test_backpressure_policies.py
+++ python/ray/data/tests/test_backpressure_policies.py
@@ -13,6 +13,13 @@ from ray.data._internal.execution.backpressure_policy import (
     ConcurrencyCapBackpressurePolicy,
     StreamingOutputBackpressurePolicy,
 )
+from ray.data.tests.conftest import (
+    CoreExecutionMetrics,
+    assert_core_execution_metrics_equals,
+    get_initial_core_execution_metrics_snapshot,
+    restore_data_context,
+)
+from ray.tests.conftest import shutdown_only
 
 
 class TestConcurrencyCapBackpressurePolicy(unittest.TestCase):
@@ -289,6 +296,88 @@ class TestStreamOutputBackpressurePolicy(unittest.TestCase):
             producer_timestamps,
             consumer_timestamps,
         )
+
+
+def test_large_e2e_backpressure(shutdown_only, restore_data_context):
+    """Test backpressure on a synthetic large-scale workload."""
+    # The cluster has 16 CPUs and 2GB object store memory.
+    # The dataset will have 2GB * 25% = 512MB memory budget.
+    #
+    # Each produce task generates 10 blocks, each of which has 0.1GB data.
+    #
+    # Without any backpressure, the producer tasks will output at most
+    # 16 * 10 * 0.1GB = 16GB data.
+    #
+    # With StreamingOutputBackpressurePolicy and the following configuration,
+    # the executor will still schedule 16 produce tasks, but only the first task is
+    # allowed to output all blocks. The total size of pending blocks will be
+    # (10 + 15 * 1 + 1) * 0.1GB = 2.6GB,
+    # where,
+    # - 10 is the number of blocks in the first task.
+    # - 15 * 1 is the number of blocks pending at the streaming generator level of
+    #   the other 15 tasks.
+    # - 1 is the number of blocks pending at the output queue.
+    max_pending_block_bytes = int(2.6 * 1024**3)
+
+    ray.init(num_cpus=16, object_store_memory=2 * 1024 * 1024 * 1024)
+
+    NUM_ROWS_PER_TASK = 10
+    NUM_TASKS = 32
+    NUM_ROWS_TOTAL = NUM_ROWS_PER_TASK * NUM_TASKS
+    BLOCK_SIZE = 100 * 1024 * 1024
+
+    def produce(batch):
+        print("Produce task started", batch["id"])
+        time.sleep(0.1)
+        for id in batch["id"]:
+            print("Producing", id)
+            yield {
+                "id": [id],
+                "image": [np.zeros(BLOCK_SIZE, dtype=np.uint8)],
+            }
+
+    def consume(batch):
+        print("Consume task started", batch["id"])
+        time.sleep(0.1)
+        return {"id": batch["id"], "result": [0 for _ in batch["id"]]}
+
+    data_context = ray.data.DataContext.get_current()
+    data_context.set_config(
+        ENABLED_BACKPRESSURE_POLICIES_CONFIG_KEY,
+        [
+            StreamingOutputBackpressurePolicy,
+        ],
+    )
+    data_context.set_config(
+        StreamingOutputBackpressurePolicy.MAX_BLOCKS_IN_OP_OUTPUT_QUEUE_CONFIG_KEY, 1
+    )
+    data_context.set_config(
+        StreamingOutputBackpressurePolicy.MAX_BLOCKS_IN_GENERATOR_BUFFER_CONFIG_KEY, 1
+    )
+    data_context.execution_options.verbose_progress = True
+    data_context.target_max_block_size = BLOCK_SIZE
+
+    last_snapshot = get_initial_core_execution_metrics_snapshot()
+
+    ds = ray.data.range(NUM_ROWS_TOTAL, parallelism=NUM_TASKS)
+    ds = ds.map_batches(produce, batch_size=NUM_ROWS_PER_TASK)
+    ds = ds.map_batches(consume, batch_size=None, num_cpus=0.9)
+    for _ in ds.iter_batches():
+        pass
+    max_spilled_bytes = int(2.0 * max_pending_block_bytes)
+    last_snapshot = assert_core_execution_metrics_equals(
+        CoreExecutionMetrics(
+            object_store_stats={
+                "spilled_bytes_total": lambda x: x < max_spilled_bytes,
+                "restored_bytes_total": lambda x: x < max_spilled_bytes,
+                "cumulative_created_plasma_bytes": lambda x: x
+                > NUM_ROWS_TOTAL * BLOCK_SIZE,
+                "cumulative_created_plasma_objects": lambda _: True,
+            },
+        ),
+        last_snapshot,
+    )
+    print("CoreExecutionMetrics", last_snapshot)
 
 
 if __name__ == "__main__":
