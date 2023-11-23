80e011ea3c5ef83bd6c6914d5ee6ad1d6163573f
sven1977 <svenmika1977@gmail.com>
2023-11-23 10:37:08 +0100

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/connectors/env_to_module/prev_action_prev_reward.py
+++ rllib/connectors/env_to_module/prev_action_prev_reward.py
@@ -1,3 +1,4 @@
+from functools import partial
 import numpy as np
 import tree  # pip install dm_tree
 
@@ -6,7 +7,8 @@ from ray.rllib.policy.sample_batch import SampleBatch
 from ray.rllib.utils.spaces.space_utils import batch, get_dummy_batch_for_space
 
 
-class PrevRewardPrevActionConnector(ConnectorV2):
+class _PrevRewardPrevActionConnector(ConnectorV2):
+    """A connector piece that adds previous rewards and actions to the input."""
     def __init__(
         self,
         *,
@@ -33,16 +35,6 @@ class PrevRewardPrevActionConnector(ConnectorV2):
         self.n_prev_rewards = n_prev_rewards
         self.as_learner_connector = as_learner_connector
 
-        self.action_space = ctx.rl_module.config.action_space
-
-        self.r0 = [0.0] * self.n_prev_rewards
-        self.a0 = get_dummy_batch_for_space(
-            self.action_space,
-            batch_size=self.n_prev_actions,
-            fill_value=0.0,
-            one_hot_discrete=True,
-        )
-
     def __call__(self, *, input_, episodes, ctx, **kwargs):
         # This is a data-in-data-out connector, so we expect `input_` to be a dict
         # with: key=column name, e.g. "obs" and value=[data to be processed by RLModule].
@@ -52,39 +44,49 @@ class PrevRewardPrevActionConnector(ConnectorV2):
         prev_a = []
         prev_r = []
         for episode in episodes:
-            len_ = len(episode) + episode._len_lookback_buffer
-            a0_idx = len_ - self.n_prev_actions
-            r0_idx = len_ - self.n_prev_rewards
             # Learner connector pipeline. Episodes have been finalized/numpy'ized.
             if self.as_learner_connector:
                 assert episode.is_numpy
-                prev_r.extend([
-                    (self.r0[idx:] if idx < 0 else [])
-                    + episode.rewards[max(idx, 0):idx + self.n_prev_rewards]
-                    for idx in range(-self.n_prev_rewards, len_ - self.n_prev_rewards)
-                ])
-                TODO: episodes.actions need to be one-hot'd
-                prev_a.append(
-                    tree.map_structure(
-                        lambda s0, s: np.concatenate((s0[idx:] if idx < 0 else [])
-                            + s[max(idx, 0):idx + self.n_prev_actions]),
-                        self.a0,
-                        episode.get_actions(),
-                    )
-                    for idx in range(-self.n_prev_actions, len_ - self.n_prev_actions)
-                )
+                # Loop through each timestep in the episode and add the previous n
+                # actions and previous m rewards (based on that timestep) to the batch.
+                for ts in range(len(episode)):
+                    prev_a.append(episode.get_actions(
+                        # Extract n actions from `ts - n` to `ts` (excluding `ts`).
+                        indices=slice(ts - self.n_prev_actions, ts),
+                        # Make sure negative indices are NOT interpreted as "counting
+                        # from the end".
+                        interpret_neg_idx_left_of_zero=True,
+                        # In case we are at the very beginning of the episode, e.g.
+                        # ts==0, fill the left side with zero-actions.
+                        fill=0.0,
+                        # Return one-hot arrays for those action components that are
+                        # discrete or multi-discrete.
+                        one_hot_discrete=True,
+                    ))
+                    # Do the same for rewards.
+                    prev_r.append(episode.get_rewards(
+                        indices=slice(ts - self.n_prev_rewards, ts),
+                        interpret_neg_idx_left_of_zero=True,
+                        fill=0.0,
+                    ))
             # Env-to-module pipeline. Episodes still operate on lists.
             else:
                 assert not episode.is_numpy
-                prev_r.append(
-                    episode.get_rewards(slice(-self.n_prev_rewards, None), fill=0.0)
-                )
-                TODO: episodes.actions need to be one-hot'd
-                prev_a.append(
-                    (self.a0[a0_idx:] if a0_idx < 0 else [])
-                    + episode.actions[-self.n_prev_actions:]
-                )
+                prev_a.append(episode.get_actions(
+                    indices=slice(-self.n_prev_actions, None),
+                    fill=0.0,
+                    one_hot_discrete=True,
+                ))
+                prev_r.append(episode.get_rewards(
+                    indices=slice(-self.n_prev_rewards, None),
+                    fill=0.0,
+                ))
 
         input_[SampleBatch.PREV_ACTIONS] = batch(prev_a)
         input_[SampleBatch.PREV_REWARDS] = np.array(prev_r)
         return input_
+
+
+PrevRewardPrevActionEnvToModule = partial(
+    _PrevRewardPrevActionConnector, as_learner_connector=False
+)
