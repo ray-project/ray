fdd07791cade8326b00c313f8ad7862ef7b847fb
SangBin Cho <sangcho@sangcho-LT93GQWG9C.local>
2023-11-27 20:17:31 +0900

    fix

--- a.py
+++ a.py
@@ -1,21 +1,27 @@
 import ray
+
 ray.init()
+
 
 @ray.remote
 class A:
     def kill(self):
         import os
+
         os._exit(1)
 
     def get(self):
         return 1
 
+
 a = A.remote()
+
 
 @ray.remote
 class B:
     def get_result(self, a):
         return ray.get(a.get.remote())
+
 
 b = B.remote()
 a.kill.remote()
