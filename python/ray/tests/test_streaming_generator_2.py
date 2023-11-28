8fe88ee0a2c0204b78f699527a5d00e91797d53c
Jonathan Nitisastro <jonathancn@anyscale.com>
2023-11-28 09:33:33 -0800

    revert streaming generator change
    Signed-off-by: Jonathan Nitisastro <jonathancn@anyscale.com>

--- python/ray/tests/test_streaming_generator_2.py
+++ python/ray/tests/test_streaming_generator_2.py
@@ -331,6 +331,7 @@ def test_python_object_leak(shutdown_only):
             return self.gc_garbage_len
 
         async def gen(self, fail=False):
+            gc.set_debug(gc.DEBUG_SAVEALL)
             gc.collect()
             self.gc_garbage_len = len(gc.garbage)
             print("Objects: ", self.gc_garbage_len)
@@ -340,6 +341,7 @@ def test_python_object_leak(shutdown_only):
             yield 1
 
         async def f(self, fail=False):
+            gc.set_debug(gc.DEBUG_SAVEALL)
             gc.collect()
             self.gc_garbage_len = len(gc.garbage)
             print("Objects: ", self.gc_garbage_len)
@@ -357,6 +359,7 @@ def test_python_object_leak(shutdown_only):
             return self.gc_garbage_len
 
         def f(self, fail=False):
+            gc.set_debug(gc.DEBUG_SAVEALL)
             gc.collect()
             self.gc_garbage_len = len(gc.garbage)
             print("Objects: ", self.gc_garbage_len)
@@ -366,6 +369,7 @@ def test_python_object_leak(shutdown_only):
             return 1
 
         def gen(self, fail=False):
+            gc.set_debug(gc.DEBUG_SAVEALL)
             gc.collect()
             self.gc_garbage_len = len(gc.garbage)
             print("Objects: ", self.gc_garbage_len)
