d8b198b90281904a81bc1a1fd446b521968d970f
Andrew Xue <andewzxue@gmail.com>
2023-11-29 08:40:25 -0800

    fix
    Signed-off-by: Andrew Xue <andewzxue@gmail.com>

--- release/nightly_tests/dataset/multi_node_train_benchmark.py
+++ release/nightly_tests/dataset/multi_node_train_benchmark.py
@@ -616,10 +616,10 @@ def benchmark_code(
 
     # Report the average of per-epoch throughput, excluding the first epoch.
     # Unless there is only one epoch.
-    skip_first_epoch = 0 if args.num_epochs == 1 else 1
+    start_epoch_tput = 0 if args.num_epochs == 1 else 1
     epoch_tputs = []
     num_rows_per_epoch = sum(result.metrics["num_rows"])
-    for i in range(skip_first_epoch, args.num_epochs):
+    for i in range(start_epoch_tput, args.num_epochs):
         time_start_epoch_i, time_end_epoch_i = zip(*result.metrics[f"epoch_{i}_times"])
         runtime_epoch_i = max(time_end_epoch_i) - min(time_start_epoch_i)
         tput_epoch_i = num_rows_per_epoch / runtime_epoch_i
