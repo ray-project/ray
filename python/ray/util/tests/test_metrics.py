b4bfe1e7552725895d29747eb64713ca5f2e9d25
Ujjawal <ujjawal.khare@dream11.com>
2023-11-29 12:12:09 +0530

    test cases added

---
+++ python/ray/util/tests/test_metrics.py
@@ -0,0 +1,18 @@
+import pytest
+
+from ray.util.metrics import Metric
+
+
+def test_invalid_metric_name(self):
+    with pytest.raises(ValueError) as e:
+        Metric("faulty-metric", "Test metric")
+    assert (
+        str(e.value)
+        == "Metric name faulty-metric is invalid. Please use metric names that match the regex ^[a-zA-Z_:][a-zA-Z0-9_:]*$"
+    )
+
+
+def test_empty_metric_name(self):
+    with pytest.raises(ValueError) as e:
+        Metric("", "Test metric")
+    assert str(e.value) == "Empty name is not allowed. Please provide a metric name."
