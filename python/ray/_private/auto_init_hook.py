53cfcbb8093fd873d9faf1a89a9ee8e53c8f506e
Eric Liang <ekhliang@gmail.com>
2023-11-29 18:24:08 -0800

    revert auto init hook
    Signed-off-by: Eric Liang <ekhliang@gmail.com>

--- python/ray/_private/auto_init_hook.py
+++ python/ray/_private/auto_init_hook.py
@@ -4,11 +4,13 @@ from functools import wraps
 import threading
 
 auto_init_lock = threading.Lock()
-enable_auto_connect = os.environ.get("RAY_ENABLE_AUTO_CONNECT", "") != "0"
 
 
 def auto_init_ray():
-    if enable_auto_connect and not ray.is_initialized():
+    if (
+        os.environ.get("RAY_ENABLE_AUTO_CONNECT", "") != "0"
+        and not ray.is_initialized()
+    ):
         auto_init_lock.acquire()
         if not ray.is_initialized():
             ray.init()
