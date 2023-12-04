775f0497df5866a784901c484f8683962a9bdf97
can <can@anyscale.com>
2023-12-04 04:27:26 +0000

    [ci] windows wanda
    Signed-off-by: can <can@anyscale.com>

---
+++ ci/ray_ci/build_base_windows.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -ex
+
+git config --global core.symlinks true
+git config --global core.autocrlf false
+mkdir -p /c/rayci
+git clone . /c/rayci
+cd /c/rayci
+
+powershell ci/pipeline/fix-windows-bazel.ps1
+
+{
+  echo "build --announce_rc";  
+  echo "build --config=ci";
+  echo "startup --output_user_root=c:/tmp";
+  echo "build --remote_cache=${BUILDKITE_BAZEL_CACHE_URL}";
+  echo "build --remote_upload_local_results=false" >> ~/.bazelrc;
+} >> ~/.bazelrc
+
+# Build ray
+conda init 
+pip install -U --ignore-installed  \
+  -c python/requirements_compiled.txt \
+  -r python/requirements.txt \
+  -r python/requirements/test-requirements.txt
+
+# Clean up temp files to speed up docker build
+pip cache purge
+bazel clean --expunge
+powershell ci/pipeline/fix-windows-recycle.ps1
