9b4a6767ce9d4fa74789531c19d048d6c50562f9
Shreyas Krishnaswamy <shrekris@anyscale.com>
2023-12-04 14:55:53 -0800

    Set target_capacity_direction to DOWN
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- python/ray/serve/_private/controller.py
+++ python/ray/serve/_private/controller.py
@@ -1105,10 +1105,9 @@ def calculate_target_capacity_direction(
 
         if curr_target_capacity == next_target_capacity:
             next_target_capacity_direction = curr_target_capacity_direction
-        elif curr_target_capacity is None:
-            # The current config has finished rolling out. The next config
-            # is now starting to roll out, so we set the scale direction to up.
-            next_target_capacity_direction = TargetCapacityDirection.UP
+        elif curr_target_capacity is None and next_target_capacity is not None:
+            # target_capacity is scaling down from None to a numeric value.
+            next_target_capacity_direction = TargetCapacityDirection.DOWN
         elif next_target_capacity is None:
             next_target_capacity_direction = None
         elif curr_target_capacity < next_target_capacity:
