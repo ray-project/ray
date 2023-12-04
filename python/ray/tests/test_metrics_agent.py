19f2297e75805fbb8bf626bdfa8ab8f9af57b6c7
rickyyx <rickyx@anyscale.com>
2023-12-04 22:12:38 +0000

    update
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- python/ray/tests/test_metrics_agent.py
+++ python/ray/tests/test_metrics_agent.py
@@ -589,7 +589,7 @@ def test_per_func_name_stats(shutdown_only):
             "ray::Actor",
             "ray::ActorB",
             "ray::IDLE",
-        } injl components
+        } in components
         return True
 
     wait_for_condition(verify_components, timeout=30)
