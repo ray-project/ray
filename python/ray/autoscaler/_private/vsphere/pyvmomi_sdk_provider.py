7fe451a8b83ea789d17fef3b8a954164008bf8a8
huchen2021 <85480625+huchen2021@users.noreply.github.com>
2023-12-06 01:28:03 +0800

    [Cluster launcher] [vSphere] Refactor: Add a cache in pyvmomi_sdk_provider (#41481)
    Pyvmomi API is based on SOAP protocol, and the object returned by get_pyvmomi_obj is just reference, not a new object created by deepcopy.
    We can cache the object which is returned by get_pyvmomi_obj, then use the cached one on next call of get_pyvmomi_obj. It can save a lot time.
    
    Signed-off-by: Chen Hui <huchen@vmware.com>

--- python/ray/autoscaler/_private/vsphere/pyvmomi_sdk_provider.py
+++ python/ray/autoscaler/_private/vsphere/pyvmomi_sdk_provider.py
@@ -1,10 +1,55 @@
 import atexit
+import logging
 import ssl
+from enum import Enum
 
 from pyVim.connect import Disconnect, SmartStubAdapter, VimSessionOrientedStub
-from pyVmomi import vim
+from pyVmomi import vim, vmodl
 
 from ray.autoscaler._private.vsphere.utils import Constants, singleton_client
+
+logger = logging.getLogger(__name__)
+
+
+class ObjectType(Enum):
+    # Enum for Object Type
+    ResourcePool = "ResourcePool"
+    VirtualMachine = "VirtualMachine"
+    Datastore = "Datastore"
+    ClusterComputeResource = "ClusterComputeResource"
+
+
+class KeyType(Enum):
+    # Enum for Key Type, name or object id
+    Name = "Name"
+    ObjectID = "ObjectID"
+
+
+def get_object_type(vimtype):
+    if vimtype == [vim.ResourcePool]:
+        return ObjectType.ResourcePool
+    elif vimtype == [vim.VirtualMachine]:
+        return ObjectType.VirtualMachine
+    elif vimtype == [vim.Datastore]:
+        return ObjectType.Datastore
+    elif vimtype == [vim.ClusterComputeResource]:
+        return ObjectType.ClusterComputeResource
+    else:
+        raise ValueError("Invalid Object Type")
+
+
+def check_obj_validness(obj):
+    if not obj:
+        return False
+    try:
+        # check the validness of the cached vmomi obj
+        _ = obj.name
+        return True
+    except vmodl.fault.ManagedObjectNotFound:
+        return False
+    except Exception as e:
+        logger.error(f"Got an exception during check the pyvmomi obj validness: {e}")
+        return False
 
 
 @singleton_client
@@ -26,6 +71,20 @@ class PyvmomiSdkProvider:
 
         # Instance parameters
         self.timeout = 0
+        self.cached = {
+            KeyType.Name: {
+                ObjectType.ResourcePool: {},
+                ObjectType.VirtualMachine: {},
+                ObjectType.Datastore: {},
+                ObjectType.ClusterComputeResource: {},
+            },
+            KeyType.ObjectID: {
+                ObjectType.ResourcePool: {},
+                ObjectType.VirtualMachine: {},
+                ObjectType.Datastore: {},
+                ObjectType.ClusterComputeResource: {},
+            },
+        }
 
         # Connect using a session oriented connection
         # Ref. https://github.com/vmware/pyvmomi/issues/347
@@ -52,6 +111,36 @@ class PyvmomiSdkProvider:
         session_stub = VimSessionOrientedStub(smart_stub, credentials)
         return vim.ServiceInstance("ServiceInstance", session_stub)
 
+    def get_obj_from_cache(self, vimtype, name, obj_id):
+        object_type = get_object_type(vimtype)
+        if name:
+            object_cache = self.cached[KeyType.Name][object_type]
+            obj = object_cache.get(name)
+            if check_obj_validness(obj):
+                if obj.name != name:
+                    # example: If someone has changed the VM name on the vSphere side,
+                    # then create another VM with the same name. Then this cache item
+                    # will be dirty because it still points to the previous VM obj.
+                    object_cache.pop(name)
+                    object_cache[obj.name] = obj
+                    return None
+                return obj
+            if obj:
+                object_cache.pop(name)
+        elif obj_id:
+            object_cache = self.cached[KeyType.ObjectID][object_type]
+            obj = object_cache.get(obj_id)
+            if check_obj_validness(obj):
+                return obj
+            if obj:
+                object_cache.pop(obj_id)
+        return None
+
+    def put_obj_in_cache(self, vimtype, obj):
+        object_type = get_object_type(vimtype)
+        self.cached[KeyType.Name][object_type][obj.name] = obj
+        self.cached[KeyType.ObjectID][object_type][obj._moId] = obj
+
     def get_pyvmomi_obj(self, vimtype, name=None, obj_id=None):
         """
         This function will return the vSphere object.
@@ -66,18 +155,26 @@ class PyvmomiSdkProvider:
         if self.pyvmomi_sdk_client is None:
             raise RuntimeError("Must init pyvmomi_sdk_client first")
 
+        cached_obj = self.get_obj_from_cache(vimtype, name, obj_id)
+        if cached_obj:
+            return cached_obj
+
         container = self.pyvmomi_sdk_client.content.viewManager.CreateContainerView(
             self.pyvmomi_sdk_client.content.rootFolder, vimtype, True
         )
+        obj = None
         # If both name and moid are provided we will prioritize name.
         if name:
-            for c in container.view:
-                if c.name == name:
-                    return c
+            for candidate in container.view:
+                if candidate.name == name:
+                    obj = candidate
         elif obj_id:
-            for c in container.view:
-                if obj_id in str(c):
-                    return c
+            for candidate in container.view:
+                if obj_id in str(candidate):
+                    obj = candidate
+        if obj:
+            self.put_obj_in_cache(vimtype, obj)
+            return obj
         raise ValueError(
             f"Cannot find the object with type {vimtype} on vSphere with"
             f"name={name} and obj_id={obj_id}"
@@ -88,5 +185,7 @@ class PyvmomiSdkProvider:
             _ = self.pyvmomi_sdk_client.RetrieveContent()
         except vim.fault.NotAuthenticated:
             self.pyvmomi_sdk_client = self.get_client()
+            self.cached.clear()
         except Exception as e:
+            self.cached.clear()
             raise RuntimeError(f"failed to ensure the connect, exception: {e}")
