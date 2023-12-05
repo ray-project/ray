e7f334f8dddf26a3e7a6ceaaa23a1cf7b0dd10de
Hao Chen <chenh1024@gmail.com>
2023-12-04 19:36:04 -0800

    fix
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- python/ray/train/data_parallel_trainer.py
+++ python/ray/train/data_parallel_trainer.py
@@ -266,8 +266,8 @@ class DataParallelTrainer(BaseTrainer):
 
         train_total_resources = self.scaling_config.total_resources
         self._data_config.set_train_total_resoruces(
-            train_total_resources["CPU"],
-            train_total_resources["GPU"],
+            train_total_resources.get("CPU", 0),
+            train_total_resources.get("GPU", 0),
         )
 
     @PublicAPI(stability="beta")
