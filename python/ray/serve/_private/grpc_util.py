b138607faa15cf19b0608addcd432d3d7a78ae3a
Gene Su <e870252314@gmail.com>
2023-12-06 12:24:39 -0800

    fix tests
    Signed-off-by: Gene Su <e870252314@gmail.com>

--- python/ray/serve/_private/grpc_util.py
+++ python/ray/serve/_private/grpc_util.py
@@ -141,14 +141,14 @@ class RayServegRPCContext:
         self._trailing_metadata += trailing_metadata
 
     def set_on_grpc_context(self, grpc_context: grpc._cython.cygrpc._ServicerContext):
-        if self._code is not None:
+        if self._code:
             grpc_context.set_code(self._code)
 
-        if self._compression is not None:
+        if self._compression:
             grpc_context.set_compression(self._compression)
 
-        if self._details is not None:
+        if self._details:
             grpc_context.set_details(self._details)
 
-        if self._trailing_metadata is not None:
+        if self._trailing_metadata:
             grpc_context.set_trailing_metadata(self._trailing_metadata)
