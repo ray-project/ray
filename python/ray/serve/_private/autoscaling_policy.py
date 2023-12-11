f7ba1fcebde690cef90fcfe17f71089424357df8
Gene Su <e870252314@gmail.com>
2023-12-11 12:12:30 -0800

    call autoscaling policy in the separate thread
    Signed-off-by: Gene Su <e870252314@gmail.com>

--- python/ray/serve/_private/autoscaling_policy.py
+++ python/ray/serve/_private/autoscaling_policy.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from threading import Thread
 from typing import Callable, List, Optional
 
@@ -11,102 +12,33 @@ from ray.serve.config import AutoscalingConfig
 logger = logging.getLogger(SERVE_LOGGER_NAME)
 
 
-class CustomScalingPolicy:
-    """A custom autoscaling policy to handle user specified scaling logic."""
-
-    def __init__(self, config: AutoscalingConfig):
-        self.config = config
-        self.custom_scaling_actor_handle = None
-        self.custom_scaling_remote_func = None
-        self.custom_scaling_ref = None
-
-    #     self._setup_remote_callable()
-    #
-    # def _setup_remote_callable(self):
-    #     """Set up the remote callable for the custom scaling policy.
-    #     If the custom scaling policy is a class, then create an actor handle for it.
-    #     Else, create a remote function for it.
-    #     """
-    #     autoscaling_policy_callable = self.config.get_autoscaling_policy()
-    #     if isclass(autoscaling_policy_callable):
-    #         assert issubclass(autoscaling_policy_callable, AutoscalingPolicy)
-    #
-    #         self.custom_scaling_actor_handle = ray.remote(
-    #             autoscaling_policy_callable
-    #         ).remote(self.config)
-    #     else:
-    #         self.custom_scaling_remote_func = ray.remote(autoscaling_policy_callable)
-
-    # def get_custom_scaling_ref(self, autoscaling_context: AutoscalingContext):
-    #     """Get the custom scaling reference.
-    #     If the custom scaling policy is a class, then call get_decision_num_replicas()
-    #     Else, call the remote function.
-    #     """
-    #     if self.custom_scaling_actor_handle:
-    #         return self.custom_scaling_actor_handle.get_decision_num_replicas.remote(
-    #             autoscaling_context
-    #         )
-    #
-    #     return self.custom_scaling_remote_func.remote(autoscaling_context)
-
-    # def get_decision_num_replicas(
-    #     self, autoscaling_context: AutoscalingContext
-    # ) -> Optional[int]:
-    #     """Make a decision to scale replicas.
-    #     Returns the new number of replicas to scale to. Or None if the custom scaling
-    #     function call is not finished yet, finished but not returning an integer or
-    #     None, or throw exception.
-    #     """
-    #     # TODO (genesu): reimplement this using threading.Event
-    #
-    #     if self.custom_scaling_ref is None:
-    #         # TODO (genesu): add a timeout for this
-    #         self.custom_scaling_ref = self.get_custom_scaling_ref(autoscaling_context)
-    #
-    #     finished, _ = ray.wait([self.custom_scaling_ref], timeout=0)
-    #     try:
-    #         if self.custom_scaling_ref in finished:
-    #             decision_num_replicas = ray.get(self.custom_scaling_ref)
-    #             self.custom_scaling_ref = None
-    #             if (
-    #                 isinstance(decision_num_replicas, int)
-    #                 and decision_num_replicas
-    #                 != autoscaling_context.curr_target_num_replicas
-    #             ):
-    #                 return decision_num_replicas
-    #             elif not isinstance(decision_num_replicas, (int, type(None))):
-    #                 logger.error(
-    #                     "Custom scaling policy must return an integer or None. "
-    #                     f"Received type {type(decision_num_replicas)}, "
-    #                     f"for {decision_num_replicas}."
-    #                 )
-    #     except Exception as e:
-    #         # TODO (genesu): add exponential backoff for this
-    #         logger.error(f"Error in custom scaling policy:\n{e}")
-    #         self.custom_scaling_ref = None
-    #
-    #     return None
-
-
 class ThreadManager:
     def __init__(self, func: Callable):
         self.func = func
-        self.get_decision_num_replicas = None
+        self.decision_num_replicas = None
         self.thread = None
 
     def __call__(self, context: AutoscalingContext):
-        self.get_decision_num_replicas = self.func(context)
+        self.start_time = time.time()
+        self.decision_num_replicas = self.func(context)
 
     def done(self):
         return self.thread is not None and not self.thread.is_alive()
 
     def get_decision_num_replicas(self, context: AutoscalingContext) -> Optional[int]:
-        if self.thread is None:
-            self.thread = Thread(target=self, kwargs={"context": context})
-            self.thread.start()
+        try:
+            if self.thread is None:
+                self.thread = Thread(target=self, kwargs={"context": context})
+                self.thread.start()
 
-        if self.done():
-            return self.get_decision_num_replicas
+            if self.done():
+                return self.decision_num_replicas
+
+        except Exception as e:
+            logger.warning(f"Autoscaling call throw exception: {e}")
+
+    def reset_thread(self):
+        self.thread = None
 
 
 class AutoscalingPolicyManager:
@@ -123,10 +55,7 @@ class AutoscalingPolicyManager:
         """Creates an autoscaling policy based on the given config."""
         if self.config:
             self.policy = self.config.get_policy()
-            # if self.config.get_policy() != BasicAutoscalingPolicy:
-            #     self.policy = CustomScalingPolicy(self.config)
-            # else:
-            #     self.policy = BasicAutoscalingPolicy(self.config)
+            self.thread_manager = ThreadManager(self.policy)
 
     def should_autoscale(self) -> bool:
         """Returns whether autoscaling should be performed."""
@@ -155,11 +84,22 @@ class AutoscalingPolicyManager:
             override_min_replicas=override_min_replicas,
             target_capacity=target_capacity,
         )
-        # self.ThreadManager.get_decision_num_replicas()
+        # Note: The thread manager will continuously return None until a decision is
+        # made. We do not currently force kill and restart the scaling call due to
+        # complexities. We will add docs to warn users not to make long-running calls
+        # here, and they can set timeout in their policies if needed.
+        decision_num_replicas = self.thread_manager.get_decision_num_replicas(
+            context=self.context
+        )
+        if self.thread_manager.done():
+            self.thread_manager.reset_thread()
 
-        decision_num_replicas = self.policy(self.context)
-
-        return decision_num_replicas
+        if decision_num_replicas is not None:
+            return self.apply_bounds(
+                curr_target_num_replicas=decision_num_replicas,
+                target_capacity=target_capacity,
+                target_capacity_direction=target_capacity_direction,
+            )
 
     def get_current_lower_bound(
         self,
