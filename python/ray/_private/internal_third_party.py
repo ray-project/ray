871abc0a22eaaee4f48bb552bbf6142fe517ba7d
Ruiyang Wang <rywang014@gmail.com>
2023-12-11 17:04:23 -0800

    WIP inspect caller
    Signed-off-by: Ruiyang Wang <rywang014@gmail.com>

--- python/ray/_private/internal_third_party.py
+++ python/ray/_private/internal_third_party.py
@@ -1,6 +1,14 @@
 # Anyone importing this file addes the internal third party files vendored in the Ray
 # pip package. Should only be used by the Dashboard and the Runtime Env Agent.
 
+def check_is_internal_process():
+    """
+    raises ImportError if this import is illegal. The only leagal importers are the Ray internal ones: the Dashboard and the Runtime Env Agent.
+    """
+    import inspect
+    print(inspect.stack())
+    # TODO: inspect the right layer (or the bottom layer?) of the stack, and see if the module is legal.
+    # Maybe bottom layer is better -> how you started the process at all
 
 def _configure_path():
     import sys
