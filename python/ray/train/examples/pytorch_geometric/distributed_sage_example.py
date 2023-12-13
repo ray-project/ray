b263f3379ca047a2ccd958226d1a7541eee47c1a
Matthew Deng <matt@anyscale.com>
2023-12-12 20:49:40 -0800

    block
    Signed-off-by: Matthew Deng <matt@anyscale.com>

--- python/ray/train/examples/pytorch_geometric/distributed_sage_example.py
+++ python/ray/train/examples/pytorch_geometric/distributed_sage_example.py
@@ -56,8 +56,8 @@ class SAGE(torch.nn.Module):
 
 
 def train_loop_per_worker(train_loop_config):
-    import os
-    os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+    # import os
+    # os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 
     dataset = train_loop_config["dataset_fn"]()
     batch_size = train_loop_config["batch_size"]
