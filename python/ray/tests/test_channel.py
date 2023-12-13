69f22c82a054432bde4275bd1ff173aa68a60265
Stephanie Wang <swang@cs.berkeley.edu>
2023-12-12 19:15:27 -0800

    fix
    Signed-off-by: Stephanie Wang <swang@cs.berkeley.edu>

--- python/ray/tests/test_channel.py
+++ python/ray/tests/test_channel.py
@@ -13,7 +13,7 @@ import ray.experimental.channel as ray_channel
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.skipif(sys.platform == "win32", reason="Requires POSIX.")
+@pytest.mark.skipif(sys.platform != "linux", reason="Requires Linux.")
 def test_put_local_get(ray_start_regular):
     chan = ray_channel.Channel(1000)
 
@@ -29,7 +29,7 @@ def test_put_local_get(ray_start_regular):
         chan.end_read()
 
 
-@pytest.mark.skipif(sys.platform == "win32", reason="Requires POSIX.")
+@pytest.mark.skipif(sys.platform != "linux", reason="Requires Linux.")
 def test_errors(ray_start_regular):
     @ray.remote
     class Actor:
@@ -74,7 +74,7 @@ def test_errors(ray_start_regular):
     assert "ray.exceptions.RaySystemError" in str(exc_info.value)
 
 
-@pytest.mark.skipif(sys.platform == "win32", reason="Requires POSIX.")
+@pytest.mark.skipif(sys.platform != "linux", reason="Requires Linux.")
 def test_put_different_meta(ray_start_regular):
     chan = ray_channel.Channel(1000)
 
@@ -100,7 +100,7 @@ def test_put_different_meta(ray_start_regular):
     _test(np.random.rand(1))
 
 
-@pytest.mark.skipif(sys.platform == "win32", reason="Requires POSIX.")
+@pytest.mark.skipif(sys.platform != "linux", reason="Requires Linux.")
 @pytest.mark.parametrize("num_readers", [1, 4])
 def test_put_remote_get(ray_start_regular, num_readers):
     chan = ray_channel.Channel(1000)
