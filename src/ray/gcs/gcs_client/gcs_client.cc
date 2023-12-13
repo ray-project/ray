79e350f941eb49c3d0f3eac539e223e892db3c3c
Jonathan Nitisastro <jonathancn@anyscale.com>
2023-12-13 11:57:42 -0800

    add client address

--- src/ray/gcs/gcs_client/gcs_client.cc
+++ src/ray/gcs/gcs_client/gcs_client.cc
@@ -167,10 +167,11 @@ Status PythonGcsClient::InstallFailureSignalHandler() {
 Status PythonGcsClient::Connect(const ClusterID &cluster_id,
                                 int64_t timeout_ms,
                                 size_t num_retries) {
+  RAY_LOG(INFO) << "Connect to gcs server: " << this;
   std::shared_ptr<grpc::Channel> channel_ =
       rpc::GcsRpcClient::CreateGcsChannel(options_.gcs_address_, options_.gcs_port_);
   node_info_stub_ = rpc::NodeInfoGcsService::NewStub(channel_);
-  RAY_LOG(INFO) << "channel_ count: " << channel_.use_count();
+  RAY_LOG(INFO) << "channel_ count: " << channel_.use_count() << "addresss: " << this;
   if (cluster_id.IsNil()) {
     size_t tries = num_retries + 1;
     RAY_CHECK(tries > 0) << "Expected positive retries, but got " << tries;
@@ -198,7 +199,7 @@ Status PythonGcsClient::Connect(const ClusterID &cluster_id,
       channel_ =
           rpc::GcsRpcClient::CreateGcsChannel(options_.gcs_address_, options_.gcs_port_);
       node_info_stub_ = rpc::NodeInfoGcsService::NewStub(channel_);
-      RAY_LOG(INFO) << "channel_ count: " << channel_.use_count();
+      RAY_LOG(INFO) << "channel_ count: " << channel_.use_count() << "addresss: " << this;
     }
     RAY_RETURN_NOT_OK(connect_status);
   } else {
@@ -212,7 +213,7 @@ Status PythonGcsClient::Connect(const ClusterID &cluster_id,
   job_info_stub_ = rpc::JobInfoGcsService::NewStub(channel_);
   node_resource_info_stub_ = rpc::NodeResourceInfoGcsService::NewStub(channel_);
   autoscaler_stub_ = rpc::autoscaler::AutoscalerStateService::NewStub(channel_);
-  RAY_LOG(INFO) << "channel_ count: " << channel_.use_count();
+  RAY_LOG(INFO) << "channel_ count: " << channel_.use_count() << "addresss: " << this;
   return Status::OK();
 }
 
