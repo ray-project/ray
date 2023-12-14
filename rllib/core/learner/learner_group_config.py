9e5ce8fe5893f613b489614b9e2ff278410d2b18
sven1977 <svenmika1977@gmail.com>
2023-12-14 14:17:01 +0100

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/core/learner/learner_group_config.py
+++ rllib/core/learner/learner_group_config.py
@@ -1,13 +1,14 @@
-from ray.rllib.utils.deprecation import deprecation_warning
+from dataclasses import dataclass
 
 
-deprecation_warning(
-    old="LearnerGroupConfig",
-    help="`LearnerGroupConfig` has been replaced by the `AlgorithmConfig` object of "
-    "your experiment. All information that used to be inside "
-    "`LearnerGroupConfig` is already available inside an `AlgorithmConfig` "
-    "object (e.g. `num_learner_workers` or `num_gpus_per_learner_worker`). "
-    "You can build a LearnerGroup directly using an `AlgorithmConfig` via: "
-    "`config.build_learner_group(env=..., spaces=..., rl_module_spec=...)`.",
-    error=True,
-)
+@dataclass
+class LearnerGroupConfig:
+    def __init_subclass__(cls, **kwargs):
+        raise ValueError(
+            "`LearnerGroupConfig` has been replaced by the `AlgorithmConfig` object of "
+            "your experiment. All information that used to be inside "
+            "`LearnerGroupConfig` is already available inside an `AlgorithmConfig` "
+            "object (e.g. `num_learner_workers` or `num_gpus_per_learner_worker`). "
+            "You can build a LearnerGroup directly using an `AlgorithmConfig` via: "
+            "`config.build_learner_group(env=..., spaces=..., rl_module_spec=...)`."
+        )
