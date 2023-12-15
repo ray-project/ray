b1d976142d53533baa7cded61363dd6df09fe942
SangBin Cho <sangcho@sangcho-LT93GQWG9C.local>
2023-12-15 09:20:33 +0900

    .

--- python/ray/_private/process_watcher.py
+++ python/ray/_private/process_watcher.py
@@ -61,11 +61,65 @@ def create_check_raylet_task(log_dir, gcs_address, parent_dead_callback, loop):
     raylet_pid = get_raylet_pid()
     return run_background_task(
         # _check_parent(raylet_pid, log_dir, gcs_address, parent_dead_callback)
-        check_parent_new_impl(loop, parent_dead_callback)
+        check_parent_via_pipe(log_dir, gcs_address, loop, parent_dead_callback)
     )
 
 
-async def check_parent_new_impl(loop, parent_dead_callback):
+def report_raylet_error_logs(log_dir: str, gcs_address: str):
+    log_path = os.path.join(log_dir, "raylet.out")
+    error = False
+    msg = "Raylet is terminated. "
+    try:
+        with open(log_path, "r", encoding="utf-8") as f:
+            # Seek to _RAYLET_LOG_MAX_TAIL_SIZE from the end if the
+            # file is larger than that.
+            f.seek(0, io.SEEK_END)
+            pos = max(0, f.tell() - _RAYLET_LOG_MAX_TAIL_SIZE)
+            f.seek(pos, io.SEEK_SET)
+            # Read remaining logs by lines.
+            raylet_logs = f.readlines()
+            # Assume the SIGTERM message must exist within the last
+            # _RAYLET_LOG_MAX_TAIL_SIZE of the log file.
+            if any(
+                "Raylet received SIGTERM" in line for line in raylet_logs
+            ):
+                msg += "Termination is graceful."
+                logger.info(msg)
+            else:
+                msg += (
+                    "Termination is unexpected. Possible reasons "
+                    "include: (1) SIGKILL by the user or system "
+                    "OOM killer, (2) Invalid memory access from "
+                    "Raylet causing SIGSEGV or SIGBUS, "
+                    "(3) Other termination signals. "
+                    f"Last {_RAYLET_LOG_MAX_PUBLISH_LINES} lines "
+                    "of the Raylet logs:\n"
+                )
+                msg += "    " + "    ".join(
+                    raylet_logs[-_RAYLET_LOG_MAX_PUBLISH_LINES:]
+                )
+                error = True
+    except Exception as e:
+        msg += f"Failed to read Raylet logs at {log_path}: {e}!"
+        logger.exception(msg)
+        error = True
+    if error:
+        logger.error(msg)
+        # TODO: switch to async if necessary.
+        ray._private.utils.publish_error_to_driver(
+            ray_constants.RAYLET_DIED_ERROR,
+            msg,
+            gcs_publisher=ray._raylet.GcsPublisher(address=gcs_address),
+        )
+    else:
+        logger.info(msg)
+
+
+async def check_parent_via_pipe(
+        log_dir: str,
+        gcs_address:str,
+        loop,
+        parent_dead_callback):
     while True:
 
         def read_pipe():
@@ -80,6 +134,7 @@ async def check_parent_new_impl(loop, parent_dead_callback):
         if len(input_data) == 0:
             # cannot read bytes from parent == parent is dead.
             parent_dead_callback("readpipe done.")
+            report_raylet_error_logs(log_dir, gcs_address)
             sys.exit(0)
 
 
@@ -118,55 +173,9 @@ async def _check_parent(raylet_pid, log_dir, gcs_address, parent_dead_callback):
                         dashboard_consts.DASHBOARD_AGENT_CHECK_PARENT_INTERVAL_S
                     )
                     continue
-
-                log_path = os.path.join(log_dir, "raylet.out")
-                error = False
+                
                 parent_dead_callback()
-                msg = "Raylet is terminated. "
-                try:
-                    with open(log_path, "r", encoding="utf-8") as f:
-                        # Seek to _RAYLET_LOG_MAX_TAIL_SIZE from the end if the
-                        # file is larger than that.
-                        f.seek(0, io.SEEK_END)
-                        pos = max(0, f.tell() - _RAYLET_LOG_MAX_TAIL_SIZE)
-                        f.seek(pos, io.SEEK_SET)
-                        # Read remaining logs by lines.
-                        raylet_logs = f.readlines()
-                        # Assume the SIGTERM message must exist within the last
-                        # _RAYLET_LOG_MAX_TAIL_SIZE of the log file.
-                        if any(
-                            "Raylet received SIGTERM" in line for line in raylet_logs
-                        ):
-                            msg += "Termination is graceful."
-                            logger.info(msg)
-                        else:
-                            msg += (
-                                "Termination is unexpected. Possible reasons "
-                                "include: (1) SIGKILL by the user or system "
-                                "OOM killer, (2) Invalid memory access from "
-                                "Raylet causing SIGSEGV or SIGBUS, "
-                                "(3) Other termination signals. "
-                                f"Last {_RAYLET_LOG_MAX_PUBLISH_LINES} lines "
-                                "of the Raylet logs:\n"
-                            )
-                            msg += "    " + "    ".join(
-                                raylet_logs[-_RAYLET_LOG_MAX_PUBLISH_LINES:]
-                            )
-                            error = True
-                except Exception as e:
-                    msg += f"Failed to read Raylet logs at {log_path}: {e}!"
-                    logger.exception(msg)
-                    error = True
-                if error:
-                    logger.error(msg)
-                    # TODO: switch to async if necessary.
-                    ray._private.utils.publish_error_to_driver(
-                        ray_constants.RAYLET_DIED_ERROR,
-                        msg,
-                        gcs_publisher=ray._raylet.GcsPublisher(address=gcs_address),
-                    )
-                else:
-                    logger.info(msg)
+                report_raylet_error_logs(log_dir, gcs_address)
                 sys.exit(0)
             else:
                 parent_death_cnt = 0
