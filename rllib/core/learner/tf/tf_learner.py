8eec98a98a96eac53155eeabf5f1894c80ad2ff4
Simon Zehnder <simon.zehnder@gmail.com>
2023-12-19 13:59:09 +0100

    The error in the logs are not very clear about what happened. I can produce an error on Linux with TF2.13. running 'action_masking.py' with framework TF2. This error is due to mutation of the batch via the ctor of 'NestedDict'. Using a copy as in this PR removes this error.
    Signed-off-by: Simon Zehnder <simon.zehnder@gmail.com>

--- rllib/core/learner/tf/tf_learner.py
+++ rllib/core/learner/tf/tf_learner.py
@@ -423,8 +423,8 @@ class TfLearner(Learner):
             #  constraint on forward_train and compute_loss APIs. This seems to be
             #  in-efficient. However, for tf>=2.12, it works also w/o this conversion
             #  so remove this after we upgrade officially to tf==2.12.
-            _batch = NestedDict(_batch)
-            with tf.GradientTape(persistent=True) as tape:
+            _batch = NestedDict(_batch.copy())
+            with tf.GradientTape(persistent=False) as tape:
                 fwd_out = self._module.forward_train(_batch)
                 loss_per_module = self.compute_loss(fwd_out=fwd_out, batch=_batch)
             gradients = self.compute_gradients(loss_per_module, gradient_tape=tape)
