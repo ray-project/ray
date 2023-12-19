ea33c9b909bca8da6704a01ce1d52afcfe2573b9
rickyyx <rickyx@anyscale.com>
2023-12-19 04:10:24 +0000

    update
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- python/ray/autoscaler/v2/tests/test_instance_manager.py
+++ python/ray/autoscaler/v2/tests/test_instance_manager.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import unittest
+from collections import defaultdict
 
 # coding: utf-8
 from typing import Dict, Set
@@ -8,9 +9,23 @@ from unittest.mock import patch
 
 import pytest
 
-from ray.autoscaler.v2.instance_manager.instance_manager import InstanceUtil
+from mock import MagicMock
+
+from ray.autoscaler.v2.instance_manager.instance_manager import (
+    DefaultInstanceManager,
+    InstanceUtil,
+)
+from ray.autoscaler.v2.instance_manager.instance_storage import InstanceStorage
+from ray.autoscaler.v2.instance_manager.storage import InMemoryStorage, StoreStatus
 from ray.autoscaler.v2.schema import InvalidInstanceStatusError
-from ray.core.generated.instance_manager_pb2 import Instance
+from ray.core.generated.instance_manager_pb2 import (
+    GetInstanceManagerStateRequest,
+    Instance,
+    InstanceUpdateEvent,
+    LaunchRequest,
+    StatusCode,
+    UpdateInstanceManagerStateRequest,
+)
 
 
 def exists_path(
@@ -182,6 +197,166 @@ class InstanceUtilTest(unittest.TestCase):
             assert not exists_path(
                 s, Instance.RAY_RUNNING, InstanceUtil.get_valid_transitions()
             )
+
+
+class DefaultInstanceManagerTest(unittest.TestCase):
+    def test_instances_version_mismatch(self):
+        ins_storage = MagicMock()
+        im = DefaultInstanceManager(ins_storage)
+        # Version mismatch on reading from the storage.
+        ins_storage.get_instances.return_value = ({}, 1)
+
+        launch_req = LaunchRequest(
+            instance_type="type-1",
+            count=1,
+            id="id-1",
+        )
+        reply = im.update_instance_manager_state(
+            UpdateInstanceManagerStateRequest(
+                expected_version=0,
+                launch_requests=[launch_req],
+            )
+        )
+        assert reply.status.code == StatusCode.VERSION_MISMATCH
+
+        # Version OK.
+        reply = im.update_instance_manager_state(
+            UpdateInstanceManagerStateRequest(
+                expected_version=1,
+                launch_requests=[launch_req],
+            )
+        )
+        assert reply.status.code == StatusCode.OK
+
+        # Version mismatch when writing to the storage (race happens)
+        ins_storage.batch_upsert_instances.return_value = StoreStatus(
+            False, 2  # No longer 1
+        )
+        reply = im.update_instance_manager_state(
+            UpdateInstanceManagerStateRequest(
+                expected_version=1,
+                launch_requests=[launch_req],
+            )
+        )
+        assert reply.status.code == StatusCode.VERSION_MISMATCH
+
+        # Non-version mismatch error.
+        ins_storage.batch_upsert_instances.return_value = StoreStatus(
+            False, 1  # Still 1
+        )
+        reply = im.update_instance_manager_state(
+            UpdateInstanceManagerStateRequest(
+                expected_version=1,
+                launch_requests=[launch_req],
+            )
+        )
+        assert reply.status.code == StatusCode.UNKNOWN_ERRORS
+
+    def test_get_and_updates(self):
+        ins_storage = InstanceStorage(
+            "cluster-id",
+            InMemoryStorage(),
+        )
+        im = DefaultInstanceManager(ins_storage)
+
+        # Empty storage.
+        reply = im.get_instance_manager_state(GetInstanceManagerStateRequest())
+        assert reply.status.code == StatusCode.OK
+        assert reply.state.instances == []
+
+        # Launch nodes.
+        reply = im.update_instance_manager_state(
+            UpdateInstanceManagerStateRequest(
+                expected_version=0,
+                launch_requests=[
+                    LaunchRequest(
+                        instance_type="type-1",
+                        count=1,
+                        id="id-1",
+                    ),
+                    LaunchRequest(
+                        instance_type="type-2",
+                        count=2,
+                        id="id-2",
+                    ),
+                ],
+            )
+        )
+        assert reply.status.code == StatusCode.OK
+
+        # Get launched nodes.
+        reply = im.get_instance_manager_state(GetInstanceManagerStateRequest())
+        assert reply.status.code == StatusCode.OK
+        assert len(reply.state.instances) == 3
+
+        types_count = defaultdict(int)
+        for ins in reply.state.instances:
+            types_count[ins.instance_type] += 1
+            assert ins.status == Instance.QUEUED
+            assert ins.launch_request_id in ["id-1", "id-2"]
+
+        assert types_count["type-1"] == 1
+        assert types_count["type-2"] == 2
+
+        # Update node status.
+        reply = im.update_instance_manager_state(
+            UpdateInstanceManagerStateRequest(
+                expected_version=1,
+                updates=[
+                    InstanceUpdateEvent(
+                        instance_id="0",
+                        new_instance_status=Instance.REQUESTED,
+                    ),
+                    InstanceUpdateEvent(
+                        instance_id="1",
+                        new_instance_status=Instance.REQUESTED,
+                    ),
+                ],
+            )
+        )
+
+        assert reply.status.code == StatusCode.OK
+
+        # Get updated nodes.
+        reply = im.get_instance_manager_state(GetInstanceManagerStateRequest())
+        assert reply.status.code == StatusCode.OK
+        assert len(reply.state.instances) == 3
+
+        types_count = defaultdict(int)
+        for ins in reply.state.instances:
+            types_count[ins.instance_type] += 1
+            if ins.instance_id in ["0", "1"]:
+                assert ins.status == Instance.REQUESTED
+            else:
+                assert ins.status == Instance.QUEUED
+
+        # Invalid instances status update.
+        reply = im.update_instance_manager_state(
+            UpdateInstanceManagerStateRequest(
+                expected_version=2,
+                updates=[
+                    InstanceUpdateEvent(
+                        instance_id="2",
+                        new_instance_status=Instance.RAY_RUNNING,  # Not requested yet.
+                    ),
+                ],
+            )
+        )
+        assert reply.status.code == StatusCode.INVALID_VALUE
+
+        # Invalid versions.
+        reply = im.update_instance_manager_state(
+            UpdateInstanceManagerStateRequest(
+                expected_version=0,  # Invalid version, outdated.
+                updates=[
+                    InstanceUpdateEvent(
+                        instance_id="2",
+                        new_instance_status=Instance.REQUESTED,
+                    ),
+                ],
+            )
+        )
+        assert reply.status.code == StatusCode.VERSION_MISMATCH
 
 
 if __name__ == "__main__":
