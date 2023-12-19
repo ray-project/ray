0b56b38cece55a4a6a232af842774c1542a8d7eb
root <vtsang@amd.com>
2023-12-19 21:58:10 +0000

    RayCore AMD GPU support
    Signed-off-by: root <vtsang@amd.com>

--- python/ray/_private/worker.py
+++ python/ray/_private/worker.py
@@ -426,8 +426,8 @@ class Worker:
         self.mode = None
         self.actors = {}
         # When the worker is constructed. Record the original value of the
-        # (CUDA_VISIBLE_DEVICES, ONEAPI_DEVICE_SELECTOR, NEURON_RT_VISIBLE_CORES,
-        # TPU_VISIBLE_CHIPS, ..) environment variables.
+        # (CUDA_VISIBLE_DEVICES, ONEAPI_DEVICE_SELECTOR, ROCR_VISIBLE_DEVICES,
+        # NEURON_RT_VISIBLE_CORES, TPU_VISIBLE_CHIPS, ..) environment variables.
         self.original_visible_accelerator_ids = (
             ray._private.utils.get_visible_accelerator_ids()
         )
@@ -959,7 +959,8 @@ class Worker:
         # (CUDA_VISIBLE_DEVICES, ONEAPI_DEVICE_SELECTOR, NEURON_RT_VISIBLE_CORES,
         # TPU_VISIBLE_CHIPS, ..) then respect that in the sense that only IDs
         # that appear in (CUDA_VISIBLE_DEVICES, ONEAPI_DEVICE_SELECTOR,
-        # NEURON_RT_VISIBLE_CORES, TPU_VISIBLE_CHIPS, ..) should be returned.
+        # ROCR_VISIBLE_DEVICES, NEURON_RT_VISIBLE_CORES, TPU_VISIBLE_CHIPS, ..)
+        # should be returned.
         if self.original_visible_accelerator_ids.get(resource_name, None) is not None:
             original_ids = self.original_visible_accelerator_ids[resource_name]
             assigned_ids = {str(original_ids[i]) for i in assigned_ids}
---
+++ python/ray/tests/accelerators/test_amd_gpu.py
@@ -0,0 +1,87 @@
+import os
+import sys
+import pytest
+from unittest.mock import patch
+
+import ray
+from ray._private.accelerators import AMDGPUAcceleratorManager as Accelerator
+
+
+def test_visible_amd_gpu_ids(shutdown_only):
+    with patch.object(Accelerator, "get_current_node_num_accelerators", return_value=4):
+        os.environ["ROCR_VISIBLE_DEVICES"] = "0,1,2"
+        ray.init()
+        manager = ray._private.accelerators.get_accelerator_manager_for_resource("GPU")
+        assert manager.get_current_node_num_accelerators() == 4
+        assert manager.__name__ == "AMDGPUAcceleratorManager"
+        assert ray.available_resources()["GPU"] == 3
+
+
+def test_visible_amd_gpu_type(shutdown_only):
+    with patch.object(
+        Accelerator,
+        "_get_amd_pci_ids",
+        return_value={
+            "card0": {"GPU ID": "0x740f"},
+            "card1": {"GPU ID": "0x740f"},
+            "card2": {"GPU ID": "0x740f"},
+            "card3": {"GPU ID": "0x740f"},
+        },
+    ):
+        ray.init()
+        manager = ray._private.accelerators.get_accelerator_manager_for_resource("GPU")
+        assert manager.get_current_node_accelerator_type() == "AMD Instinct MI210"
+
+    with patch.object(
+        Accelerator, "_get_amd_pci_ids", return_value={"card0": {"GPU ID": "0x640f"}}
+    ):
+        manager = None
+        manager = ray._private.accelerators.get_accelerator_manager_for_resource("GPU")
+        assert manager.get_current_node_accelerator_type() == None
+
+    with patch.object(Accelerator, "_get_amd_pci_ids", return_value=None):
+        manager = None
+        manager = ray._private.accelerators.get_accelerator_manager_for_resource("GPU")
+        assert manager.get_current_node_accelerator_type() == None
+
+
+def test_amd_gpu_accelerator_manager_api():
+    assert Accelerator.get_resource_name() == "GPU"
+    assert Accelerator.get_visible_accelerator_ids_env_var() == "ROCR_VISIBLE_DEVICES"
+    assert Accelerator.validate_resource_request_quantity(0.1) == (True, None)
+
+
+def test_get_current_process_visible_accelerator_ids():
+    os.environ["ROCR_VISIBLE_DEVICES"] = "0,1,2"
+    assert Accelerator.get_current_process_visible_accelerator_ids() == ["0", "1", "2"]
+
+    os.environ["ROCR_VISIBLE_DEVICES"] = "0,2,7"
+    assert Accelerator.get_current_process_visible_accelerator_ids() == ["0", "2", "7"]
+
+    del os.environ["ROCR_VISIBLE_DEVICES"]
+    assert Accelerator.get_current_process_visible_accelerator_ids() is None
+
+    os.environ["ROCR_VISIBLE_DEVICES"] = ""
+    assert Accelerator.get_current_process_visible_accelerator_ids() == []
+
+    del os.environ["ROCR_VISIBLE_DEVICES"]
+
+
+def test_set_current_process_visible_accelerator_ids():
+    Accelerator.set_current_process_visible_accelerator_ids(["0"])
+    assert os.environ["ROCR_VISIBLE_DEVICES"] == "0"
+
+    Accelerator.set_current_process_visible_accelerator_ids(["0", "1"])
+    assert os.environ["ROCR_VISIBLE_DEVICES"] == "0,1"
+
+    Accelerator.set_current_process_visible_accelerator_ids(["0", "1", "7"])
+    assert os.environ["ROCR_VISIBLE_DEVICES"] == "0,1,7"
+
+    del os.environ["ROCR_VISIBLE_DEVICES"]
+
+
+if __name__ == "__main__":
+    if os.environ.get("PARALLEL_CI"):
+        sys.exit(pytest.main(["-n", "auto", "--boxed", "-vs", __file__]))
+    else:
+        sys.exit(pytest.main(["-sv", __file__]))
