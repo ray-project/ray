16122ba9582574f334788a1a66930292383faf25
can <can@anyscale.com>
2023-12-20 21:14:44 +0000

    [ci] py311
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/test_ray_docker_container.py
+++ ci/ray_ci/test_ray_docker_container.py
@@ -24,16 +24,16 @@ class TestRayDockerContainer(RayCITestBase):
             "ci.ray_ci.docker_container.LinuxContainer.run_script",
             side_effect=_mock_run_script,
         ):
-            container = RayDockerContainer("3.8", "cu11.8.0", "ray")
+            container = RayDockerContainer("3.11", "cu11.8.0", "ray")
             container.run()
             cmd = self.cmds[-1]
             assert cmd == (
                 "./ci/build/build-ray-docker.sh "
-                f"ray-{RAY_VERSION}-cp38-cp38-manylinux2014_x86_64.whl "
-                f"{_DOCKER_ECR_REPO}:123-ray-py3.8-cu11.8.0-base "
+                f"ray-{RAY_VERSION}-cp311-cp311-manylinux2014_x86_64.whl "
+                f"{_DOCKER_ECR_REPO}:123-ray-py3.11-cu11.8.0-base "
                 "requirements_compiled.txt "
-                "rayproject/ray:123456-py38-cu118 "
-                "ray:123456-py38-cu118_pip-freeze.txt"
+                "rayproject/ray:123456-py311-cu118 "
+                "ray:123456-py311-cu118_pip-freeze.txt"
             )
 
             container = RayDockerContainer("3.9", "cpu", "ray-ml")
@@ -49,53 +49,53 @@ class TestRayDockerContainer(RayCITestBase):
             )
 
     def test_canonical_tag(self) -> None:
-        container = RayDockerContainer("3.8", "cpu", "ray", canonical_tag="abc")
+        container = RayDockerContainer("3.11", "cpu", "ray", canonical_tag="abc")
         assert container._get_canonical_tag() == "abc"
 
-        container = RayDockerContainer("3.8", "cpu", "ray")
-        assert container._get_canonical_tag() == "123456-py38-cpu"
+        container = RayDockerContainer("3.11", "cpu", "ray")
+        assert container._get_canonical_tag() == "123456-py311-cpu"
 
-        container = RayDockerContainer("3.8", "cpu", "ray", "aarch64")
-        assert container._get_canonical_tag() == "123456-py38-cpu-aarch64"
+        container = RayDockerContainer("3.11", "cpu", "ray", "aarch64")
+        assert container._get_canonical_tag() == "123456-py311-cpu-aarch64"
 
-        container = RayDockerContainer("3.8", "cu11.8.0", "ray-ml")
-        assert container._get_canonical_tag() == "123456-py38-cu118"
+        container = RayDockerContainer("3.11", "cu11.8.0", "ray-ml")
+        assert container._get_canonical_tag() == "123456-py311-cu118"
 
         with mock.patch.dict(os.environ, {"BUILDKITE_BRANCH": "releases/1.0.0"}):
-            container = RayDockerContainer("3.8", "cpu", "ray")
-            assert container._get_canonical_tag() == "1.0.0.123456-py38-cpu"
+            container = RayDockerContainer("3.11", "cpu", "ray")
+            assert container._get_canonical_tag() == "1.0.0.123456-py311-cpu"
 
         with mock.patch.dict(
             os.environ, {"BUILDKITE_BRANCH": "abc", "BUILDKITE_PULL_REQUEST": "123"}
         ):
-            container = RayDockerContainer("3.8", "cpu", "ray")
-            assert container._get_canonical_tag() == "pr-123.123456-py38-cpu"
+            container = RayDockerContainer("3.11", "cpu", "ray")
+            assert container._get_canonical_tag() == "pr-123.123456-py311-cpu"
 
     def test_get_image_tags(self) -> None:
         # bulk logic of _get_image_tags is tested in its callers (get_image_name and
         # get_canonical_tag), so we only test the basic cases here
-        container = RayDockerContainer("3.8", "cpu", "ray")
+        container = RayDockerContainer("3.11", "cpu", "ray")
         assert container._get_image_tags() == [
-            "123456-py38-cpu",
+            "123456-py311-cpu",
             "123456-cpu",
-            "123456-py38",
+            "123456-py311",
             "123456",
-            "nightly-py38-cpu",
+            "nightly-py311-cpu",
             "nightly-cpu",
-            "nightly-py38",
+            "nightly-py311",
             "nightly",
         ]
 
     def test_get_image_name(self) -> None:
-        container = RayDockerContainer("3.8", "cpu", "ray")
+        container = RayDockerContainer("3.11", "cpu", "ray")
         assert container._get_image_names() == [
-            "rayproject/ray:123456-py38-cpu",
+            "rayproject/ray:123456-py311-cpu",
             "rayproject/ray:123456-cpu",
-            "rayproject/ray:123456-py38",
+            "rayproject/ray:123456-py311",
             "rayproject/ray:123456",
-            "rayproject/ray:nightly-py38-cpu",
+            "rayproject/ray:nightly-py311-cpu",
             "rayproject/ray:nightly-cpu",
-            "rayproject/ray:nightly-py38",
+            "rayproject/ray:nightly-py311",
             "rayproject/ray:nightly",
         ]
 
@@ -110,23 +110,23 @@ class TestRayDockerContainer(RayCITestBase):
         ]
 
         with mock.patch.dict(os.environ, {"BUILDKITE_BRANCH": "releases/1.0.0"}):
-            container = RayDockerContainer("3.8", "cpu", "ray")
+            container = RayDockerContainer("3.11", "cpu", "ray")
             assert container._get_image_names() == [
-                "rayproject/ray:1.0.0.123456-py38-cpu",
+                "rayproject/ray:1.0.0.123456-py311-cpu",
                 "rayproject/ray:1.0.0.123456-cpu",
-                "rayproject/ray:1.0.0.123456-py38",
+                "rayproject/ray:1.0.0.123456-py311",
                 "rayproject/ray:1.0.0.123456",
             ]
 
     def test_get_python_version_tag(self) -> None:
-        container = RayDockerContainer("3.8", "cpu", "ray")
-        assert container.get_python_version_tag() == "-py38"
+        container = RayDockerContainer("3.11", "cpu", "ray")
+        assert container.get_python_version_tag() == "-py311"
 
     def test_get_platform_tag(self) -> None:
-        container = RayDockerContainer("3.8", "cpu", "ray")
+        container = RayDockerContainer("3.11", "cpu", "ray")
         assert container.get_platform_tag() == "-cpu"
 
-        container = RayDockerContainer("3.8", "cu11.8.0", "ray")
+        container = RayDockerContainer("3.11", "cu11.8.0", "ray")
         assert container.get_platform_tag() == "-cu118"
 
 
