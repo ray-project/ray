9969eaf5100ef1a4e525f11f0d57288fdccf2acf
Eric Liang <ekhliang@gmail.com>
2023-12-20 18:46:47 -0800

    [core] Fault tolerance for compiled DAGs (#41943)
    This adds fault tolerance and a teardown method for compiled DAGs.

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -1350,6 +1350,10 @@ Status CoreWorker::ExperimentalMutableObjectWriteRelease(const ObjectID &object_
   return plasma_store_provider_->ExperimentalMutableObjectWriteRelease(object_id);
 }
 
+Status CoreWorker::ExperimentalMutableObjectSetError(const ObjectID &object_id) {
+  return plasma_store_provider_->ExperimentalMutableObjectSetError(object_id);
+}
+
 Status CoreWorker::SealOwned(const ObjectID &object_id,
                              bool pin_object,
                              const std::unique_ptr<rpc::Address> &owner_address) {
