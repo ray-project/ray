4ba3345510fa0db20c0455abedc7f2011bd9b35d
akshay-anyscale <122416226+akshay-anyscale@users.noreply.github.com>
2024-01-01 13:30:49 +0530

    Update app.py
    Signed-off-by: akshay-anyscale <122416226+akshay-anyscale@users.noreply.github.com>

--- doc/source/templates/03_serving_stable_diffusion/app.py
+++ doc/source/templates/03_serving_stable_diffusion/app.py
@@ -5,9 +5,10 @@ from fastapi import FastAPI
 from fastapi.responses import Response
 import torch
 from diffusers import EulerDiscreteScheduler, StableDiffusionPipeline
+import logging
 
 app = FastAPI()
-
+logger = logging.getLogger("ray.serve")
 
 @serve.deployment(num_replicas=1)
 @serve.ingress(app)
@@ -24,7 +25,6 @@ class APIIngress:
         assert len(prompt), "prompt parameter cannot be empty"
 
         image = await self.handle.generate.remote(prompt, img_size=img_size)
-
         file_stream = BytesIO()
         image.save(file_stream, "PNG")
         return Response(content=file_stream.getvalue(), media_type="image/png")
@@ -49,9 +49,8 @@ class StableDiffusionV2:
 
     def generate(self, prompt: str, img_size: int = 512):
         assert len(prompt), "prompt parameter cannot be empty"
-
+        logger.info("Prompt: [%s]", prompt)
         image = self.pipe(prompt, height=img_size, width=img_size).images[0]
-
         return image
 
 
