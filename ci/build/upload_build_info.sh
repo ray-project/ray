d626e18585f6f2ce7b74c48e7301b84fcd57a867
can <can@anyscale.com>
2024-01-03 22:38:35 +0000

    [ci] move upload build info skipping logic into the script
    Signed-off-by: can <can@anyscale.com>

--- ci/build/upload_build_info.sh
+++ ci/build/upload_build_info.sh
@@ -7,9 +7,10 @@ readonly PIPELINE_POSTMERGE="0189e759-8c96-4302-b6b5-b4274406bf89"
 readonly PIPELINE_CIV1_BRANCH="0183465b-c6fb-479b-8577-4cfd743b545d"
 if [[
   "${BUILDKITE_PIPELINE_ID:-}" != "${PIPELINE_POSTMERGE}" && 
-  "${BUILDKITE_PIPELINE_ID:-}" != "${PIPELINE_CIV1_BRANCH}"
+  "${BUILDKITE_PIPELINE_ID:-}" != "${PIPELINE_CIV1_BRANCH}" &&
+  "${BUILDKITE_BRANCH:-}" != "master"
 ]]; then
-  echo "Skip build info uploading on non-postmerge pipeline."
+  echo "Skip build info uploading on master branch."
   exit 0
 fi
 
