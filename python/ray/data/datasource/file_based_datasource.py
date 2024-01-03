3cea2ed5d507c4a6cfdb11b4512c2fd1be7e1674
Scott Lee <sjl@anyscale.com>
2024-01-02 19:57:14 -0800

    add ACCESS_DENIED to retry type
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- python/ray/data/datasource/file_based_datasource.py
+++ python/ray/data/datasource/file_based_datasource.py
@@ -65,7 +65,7 @@ FILE_SIZE_FETCH_PARALLELIZATION_THRESHOLD = 16
 PATHS_PER_FILE_SIZE_FETCH_TASK = 16
 
 # The errors to retry for opening file.
-OPEN_FILE_RETRY_ON_ERRORS = ["AWS Error SLOW_DOWN"]
+OPEN_FILE_RETRY_ON_ERRORS = ["AWS Error SLOW_DOWN", "AWS Error ACCESS_DENIED"]
 
 # The max retry backoff in seconds for opening file.
 OPEN_FILE_RETRY_MAX_BACKOFF_SECONDS = 32
