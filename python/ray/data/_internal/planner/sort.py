b80305a26f2c43b09a3a6d779e40a343f677630e
Rony Lee <43735106+veryhannibal@users.noreply.github.com>
2024-01-09 23:18:19 +0800

    Add the boundary param for sort in ray.data.Dataset (#41269)
    User can specify the boundaries so the dataset will be divided into blocks according to the specified boundaries while sorting.
    
    Closes #41265
    
    ---------
    
    Signed-off-by: lile18 <lile18@jd.com>
    Signed-off-by: Rony Lee <43735106+veryhannibal@users.noreply.github.com>
    Signed-off-by: Stephanie Wang <swang@cs.berkeley.edu>
    Co-authored-by: lile18 <lile18@jd.com>
    Co-authored-by: Stephanie Wang <swang@cs.berkeley.edu>
    Co-authored-by: Wanqiang Ji <wanqiang.ji@gmail.com>

--- python/ray/data/_internal/planner/sort.py
+++ python/ray/data/_internal/planner/sort.py
@@ -45,7 +45,11 @@ def generate_sort_fn(
         num_outputs = num_mappers
 
         # Sample boundaries for sort key.
-        boundaries = SortTaskSpec.sample_boundaries(blocks, sort_key, num_outputs)
+        if not sort_key.boundaries:
+            boundaries = SortTaskSpec.sample_boundaries(blocks, sort_key, num_outputs)
+        else:
+            boundaries = [(b,) for b in sort_key.boundaries]
+            num_outputs = len(boundaries) + 1
         _, ascending = sort_key.to_pandas_sort_args()
         if not ascending:
             boundaries.reverse()
