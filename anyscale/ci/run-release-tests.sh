7d5d0a7c32eca052efbd6288068d57511ff371bf
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2024-01-09 14:36:03 -0800

    [ci] fix lint (#397)
    Fix a lint that I broke earlier due to merging too quickly. The code is
    copied directly from docker instruction page
    https://docs.docker.com/engine/install/ubuntu/
    
    
    ![](https://media4.giphy.com/media/tBb19eX26Hu0KxtNrgc/200.gif?cid=5a38a5a20ocz9vhoz9i6l99zhjixxpz467dwaus6vk2a2pdr&ep=v1_gifs_search&rid=200.gif&ct=g)
    
    Signed-off-by: can <can@anyscale.com>

--- anyscale/ci/run-release-tests.sh
+++ anyscale/ci/run-release-tests.sh
@@ -14,8 +14,8 @@ install_tools() {
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
     echo \
-        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-        "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
         sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     # Install packages
