57b96776647332a71d409c7b217f4f9630705f5c
Alexey Kudinkin <ak@anyscale.com>
2024-01-10 14:14:44 -0800

    Rebase onto `perf_counter` instead of `monotonic`
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/serve/_private/benchmarks/common.py
+++ python/ray/serve/_private/benchmarks/common.py
@@ -38,7 +38,6 @@ async def run_latency_benchmark(
 
     latencies = []
     for i in tqdm(range(num_requests + num_warmup_requests)):
-        # time.sleep(0.001)
         start = time.perf_counter()
         await to_call()
         end = time.perf_counter()
@@ -54,20 +53,20 @@ async def run_throughput_benchmark(
     fn: Callable, multiplier: int = 1, num_trials: int = 10, trial_runtime: float = 1
 ) -> Tuple[float, float]:
     """Returns (mean, stddev)."""
-    # warmup
+    # Warmup
     start = time.time()
     while time.time() - start < 0.1:
         await fn()
 
-    # real run
+    # Benchmark
     stats = []
     for _ in range(num_trials):
-        start = time.monotonic()
+        start = time.perf_counter()
         count = 0
-        while time.monotonic() - start < trial_runtime:
+        while time.perf_counter() - start < trial_runtime:
             await fn()
             count += 1
-        end = time.monotonic()
+        end = time.perf_counter()
         stats.append(multiplier * count / (end - start))
 
     return round(np.mean(stats), 2), round(np.std(stats), 2)
