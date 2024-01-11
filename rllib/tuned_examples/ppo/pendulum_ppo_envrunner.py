97dcb45e6ae03f81b273808cc6ab12edc70696cb
sven1977 <svenmika1977@gmail.com>
2024-01-11 11:43:14 +0100

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/tuned_examples/ppo/pendulum_ppo_envrunner.py
+++ rllib/tuned_examples/ppo/pendulum_ppo_envrunner.py
@@ -1,41 +1,47 @@
-from ray.rllib.algorithms.appo import APPOConfig
+from ray.rllib.algorithms.ppo import PPOConfig
 from ray.rllib.connectors.env_to_module.mean_std_filter import MeanStdFilter
 from ray.rllib.env.single_agent_env_runner import SingleAgentEnvRunner
 from ray import air, tune
 
 
 config = (
-    APPOConfig()
+    PPOConfig()
     # Enable new API stack and use EnvRunner.
     .experimental(_enable_new_api_stack=True)
-    .environment("CartPole-v1")
     .rollouts(
         env_runner_cls=SingleAgentEnvRunner,
-        num_rollout_workers=0,
-        num_envs_per_worker=1,
+        num_rollout_workers=1,
         # TODO (sven): Add MeanStd connector, once fully tested (should learn much
         #  better with it).
         #observation_filter="MeanStdFilter",
-        #env_to_module_connector=lambda env: MeanStdFilter(
-        #    input_observation_space=env.single_observation_space,
-        #    input_action_space=env.single_action_space,
-        #),
+        env_to_module_connector=lambda env: MeanStdFilter(
+            input_observation_space=env.single_observation_space,
+            input_action_space=env.single_action_space,
+        ),
     )
-    .resources(
-        num_learner_workers=1,
-        num_gpus_per_learner_worker=0,
-        num_gpus=0,
-        num_cpus_for_local_worker=0,
-    )
+    .environment("Pendulum-v1")
     .training(
-        num_sgd_iter=1,
+        train_batch_size_per_learner=512,
+        gamma=0.95,
+        lr=0.0001,
+        lambda_=0.1,
         vf_loss_coeff=0.01,
+        vf_clip_param=10.0,
+        mini_batch_size_per_learner=64,
+        #model={
+        #    "fcnet_activation": "relu",
+        #},
     )
+    #.evaluation(
+    #    evaluation_num_workers=1,
+    #    evaluation_interval=1,
+    #    enable_async_evaluation=True,
+    #)
 )
 
 stop = {
-    "timesteps_total": 10000000,
-    "sampler_results/episode_reward_mean": 400.0,
+    "timesteps_total": 400000,
+    "sampler_results/episode_reward_mean": -400.0,
 }
 
 
@@ -56,8 +62,9 @@ if __name__ == "__main__":
         param_space=config,
         run_config=air.RunConfig(
             stop=stop,
-            verbose=2,
+            verbose=1,
         ),
         tune_config=tune.TuneConfig(num_samples=5),
     )
     results = tuner.fit()
+
