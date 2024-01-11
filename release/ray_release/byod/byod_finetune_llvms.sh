169aa8a80d21888cbe5f8991173d8f5b520efa0d
Artur Niederfahrenhorst <attaismyname@googlemail.com>
2024-01-12 00:18:33 +0100

    Update release test deps

--- release/ray_release/byod/byod_finetune_llvms.sh
+++ release/ray_release/byod/byod_finetune_llvms.sh
@@ -11,13 +11,12 @@ pip3 install -U \
     datasets==2.14.4 \
     accelerate==0.21.0 \
     evaluate==0.4.0 \
-    bitsandbytes==0.41.1 \
     wandb==0.15.8 \
     pytorch-lightning==2.0.6 \
     "protobuf<3.21.0" \
     torchmetrics==1.0.3 \
     sentencepiece==0.1.99 \
     "urllib3<1.27" \
-    git+https://github.com/huggingface/transformers.git@d0c1aeb \
-    git+https://github.com/huggingface/peft.git@08368a1fba16de09756f067637ff326c71598fb3
+    transformers==4.31.0 \
+    peft==0.7.0
 pip3 install -U flash-attn==2.4.2 --no-build-isolation
