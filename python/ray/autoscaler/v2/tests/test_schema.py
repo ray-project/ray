3ab34a2fdbbcda563e400add5be96234047db3a0
rickyyx <rickyx@anyscale.com>
2024-01-15 18:18:46 +0000

    update
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- python/ray/autoscaler/v2/tests/test_schema.py
+++ python/ray/autoscaler/v2/tests/test_schema.py
@@ -11,34 +11,34 @@ from ray.core.generated.instance_manager_pb2 import Instance
 def test_autoscaler_instance():
 
     i = AutoscalerInstance()
-    assert not i.is_valid()[0], "Empty instance should be invalid"
+    assert not i.validate()[0], "Empty instance should be invalid"
 
     i = AutoscalerInstance(im_instance=Instance(status=Instance.QUEUED))
-    assert i.is_valid()[0], "Instance with only im_instance should be valid"
+    assert i.validate()[0], "Instance with only im_instance should be valid"
 
     i = AutoscalerInstance(
         ray_node=NodeState(status=NodeStatus.RUNNING, instance_id="i-123"),
         cloud_instance_id="i-123",
     )
-    assert i.is_valid()[0], i.is_valid()[1]
+    assert i.validate()[0], i.validate()[1]
 
     i = AutoscalerInstance(
         ray_node=NodeState(status=NodeStatus.RUNNING),
     )
-    assert not i.is_valid()[0], "Missing cloud node id."
+    assert not i.validate()[0], "Missing cloud node id."
 
     i = AutoscalerInstance(
         im_instance=Instance(status=Instance.QUEUED),
         ray_node=NodeState(status=NodeStatus.RUNNING),
     )
-    assert not i.is_valid()[
+    assert not i.validate()[
         0
     ], "cloud node id is required to link the ray node with im state"
 
     i = AutoscalerInstance(
         cloud_instance_id="i-123",
     )
-    assert not i.is_valid()[
+    assert not i.validate()[
         0
     ], "cloud instance id is not possible without im or ray node"
 
@@ -46,50 +46,50 @@ def test_autoscaler_instance():
         im_instance=Instance(status=Instance.ALLOCATED, cloud_instance_id="i-123"),
         cloud_instance_id="i-123",
     )
-    assert i.is_valid()[0]
+    assert i.validate()[0]
 
     i = AutoscalerInstance(
         im_instance=Instance(status=Instance.ALLOCATED, cloud_instance_id="i-123"),
         cloud_instance_id="i-124",  # mismatch.
     )
-    assert not i.is_valid()[0], "cloud instance id should match"
+    assert not i.validate()[0], "cloud instance id should match"
 
     i = AutoscalerInstance(
         im_instance=Instance(status=Instance.QUEUED, cloud_instance_id="i-123"),
         cloud_instance_id="i-123",
     )
-    assert not i.is_valid()[0], "cloud instance id is not possible with queued state"
+    assert not i.validate()[0], "cloud instance id is not possible with queued state"
 
     i = AutoscalerInstance(
         im_instance=Instance(status=Instance.ALLOCATED, cloud_instance_id="i-123"),
     )
-    assert not i.is_valid()[0], "cloud instance id should also be set"
+    assert not i.validate()[0], "cloud instance id should also be set"
 
     i = AutoscalerInstance(
         ray_node=NodeState(status=NodeStatus.RUNNING, instance_id="i-123"),
         cloud_instance_id="i-123",
     )
-    assert i.is_valid()[0]
+    assert i.validate()[0]
 
     i = AutoscalerInstance(
         ray_node=NodeState(status=NodeStatus.RUNNING, instance_id="i-123"),
         cloud_instance_id="i-124",  # mismatch.
     )
-    assert not i.is_valid()[0]
+    assert not i.validate()[0]
 
     i = AutoscalerInstance(
         im_instance=Instance(status=Instance.RAY_RUNNING, cloud_instance_id="i-123"),
         ray_node=NodeState(status=NodeStatus.RUNNING, instance_id="i-123"),
         cloud_instance_id="i-123",
     )
-    assert i.is_valid()[0]
+    assert i.validate()[0]
 
     i = AutoscalerInstance(
         im_instance=Instance(status=Instance.RAY_RUNNING, cloud_instance_id="i-123"),
         ray_node=NodeState(status=NodeStatus.RUNNING, instance_id="i-123"),
         cloud_instance_id="i-124",  # mismatch.
     )
-    assert not i.is_valid()[0]
+    assert not i.validate()[0]
 
 
 if __name__ == "__main__":
