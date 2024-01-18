ce3eda6de12bd41e207d1f9d82e5b6ad1eaeb686
Edward Oakes <ed.nmi.oakes@gmail.com>
2024-01-18 10:27:54 -0600

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/tests/unit/test_user_callable_wrapper.py
+++ python/ray/serve/tests/unit/test_user_callable_wrapper.py
@@ -631,7 +631,7 @@ async def test_http_handler(callable: Callable, monkeypatch):
 
 
 if __name__ == "__main__":
-    # Tests are timing out on Windows for an unknown reason. Given this is just a unit test,
-    # running on Linux and Mac should be sufficient.
+    # Tests are timing out on Windows for an unknown reason. Given this is just a unit
+    # test, running on Linux and Mac should be sufficient.
     if sys.platform != "win32":
         sys.exit(pytest.main(["-v", "-s", __file__]))
