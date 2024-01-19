838933bd88a9b829a02bcdcb58a1b9a9ed9d9c01
Matthew Owen <mowen@anyscale.com>
2024-01-19 13:56:56 -0800

    pr feedback
    Signed-off-by: Matthew Owen <mowen@anyscale.com>

--- python/ray/data/datasource/json_datasource.py
+++ python/ray/data/datasource/json_datasource.py
@@ -1,6 +1,6 @@
-import logging
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
 
+from ray.data._internal.dataset_logger import DatasetLogger
 from ray.data.context import DataContext
 from ray.data.datasource.file_based_datasource import FileBasedDatasource
 from ray.util.annotations import PublicAPI
@@ -8,7 +8,7 @@ from ray.util.annotations import PublicAPI
 if TYPE_CHECKING:
     import pyarrow
 
-logger = logging.getLogger(__name__)
+logger = DatasetLogger(__name__)
 
 
 @PublicAPI
@@ -42,32 +42,50 @@ class JSONDatasource(FileBasedDatasource):
 
         from pyarrow import ArrowInvalid, json
 
+        # When reading large files, the default block size configured in PyArrow can be
+        # too small, resulting in the following error: `pyarrow.lib.ArrowInvalid:
+        # straddling object straddles two block boundaries (try to increase block
+        # size?)`. The read will be retried with geometrically increasing block size
+        # until the size reaches `DataContext.get_current().target_max_block_size`.
+        # The initial block size will start at the PyArrow default block size or it can
+        # be manually set through the `read_options` parameter as follows.
+        #
+        # >>> import pyarrow.json as pajson
+        # >>> block_size = 10 << 20 # Set block size to 10MB
+        # >>> ray.data.read_json(  # doctest: +SKIP
+        # ...     "s3://anonymous@ray-example-data/log.json",
+        # ...     read_options=pajson.ReadOptions(block_size=block_size)
+        # ... )
+
         buffer = f.read_buffer()
-        block_size = self.read_options.block_size
-        use_threads = self.read_options.use_threads
+        init_block_size = self.read_options.block_size
         max_block_size = DataContext.get_current().target_max_block_size
         while True:
             try:
                 yield json.read_json(
                     BytesIO(buffer),
-                    read_options=json.ReadOptions(
-                        use_threads=use_threads, block_size=block_size
-                    ),
+                    read_options=self.read_options,
                     **self.arrow_json_args,
                 )
+                self.read_options.block_size = init_block_size
                 break
             except ArrowInvalid as e:
-                if (
-                    isinstance(e, ArrowInvalid)
-                    and "straddling" not in str(e)
-                    or block_size > max_block_size
-                ):
-                    raise e
+                if isinstance(e, ArrowInvalid) and "straddling" in str(e):
+                    if self.read_options.block_size < max_block_size:
+                        # Increase the block size in case it was too small.
+                        logger.get_logger(log_to_stdout=False).info(
+                            f"JSONDatasource read failed with "
+                            f"block_size={self.read_options.block_size}. Retrying with "
+                            f"block_size={self.read_options.block_size * 2}."
+                        )
+                        self.read_options.block_size *= 2
+                    else:
+                        raise ArrowInvalid(
+                            f"{e} - Auto-increasing block size to "
+                            f"{self.read_options.block_size}B failed. "
+                            f"More information on this issue can be found here: "
+                            f"https://github.com/apache/arrow/issues/25674"
+                        )
                 else:
-                    # Increase the block size in case it was too small.
-                    logger.info(
-                        f"JSONDatasource read failed with "
-                        f"block_size={block_size}. Retrying with "
-                        f"block_size={block_size * 2}."
-                    )
-                    block_size *= 2
+                    # unrelated error, simply reraise
+                    raise e
