eac2ed970ec818e2fa382f62a61110b894fdee82
mattip <matti.picus@gmail.com>
2024-01-22 21:53:50 +0200

    fixes from review
    Signed-off-by: mattip <matti.picus@gmail.com>

--- python/setup.py
+++ python/setup.py
@@ -49,7 +49,7 @@ CLEANABLE_SUBDIRS = [
 
 # In automated builds, we do a few adjustments before building. For instance,
 # the bazel environment is set up slightly differently, and symlinks are
-# replaced with junctions in Windows. This variable is set e.g. in our conda
+# replaced with junctions in Windows. This variable is set e.g. in our conda-forge
 # feedstock.
 is_automated_build = bool(int(os.environ.get("IS_AUTOMATED_BUILD", "0")))
 
@@ -561,17 +561,16 @@ def build(build_python, build_java, build_cpp):
         )
 
     if is_automated_build:
+        src_dir = os.environ.get("SRC_DIR", False) or os.getcwd()
+        src_dir = os.path.abspath(src_dir)
         if is_native_windows_or_msys():
-            root_dir = os.path.join(
-                os.path.splitdrive(os.environ["SRC_DIR"])[0], "bazel-root"
-            )
-            out_dir = os.path.join(os.path.splitdrive(os.environ["SRC_DIR"])[0], "b-o")
+            drive = os.path.splitdrive(src_dir)[0] + "\\"
+            root_dir = os.path.join(drive, "bazel-root")
+            out_dir = os.path.join(drive, "b-o")
             bazel_flags.append("--enable_runfiles=false")
         else:
-            root_dir = os.path.join(
-                os.path.abspath(os.environ["SRC_DIR"]), "..", "bazel-root"
-            )
-            out_dir = os.path.join(os.path.abspath(os.environ["SRC_DIR"]), "..", "b-o")
+            root_dir = os.path.join(src_dir, "..", "bazel-root")
+            out_dir = os.path.join(src_dir, "..", "b-o")
 
         for d in (root_dir, out_dir):
             if not os.path.exists(d):
