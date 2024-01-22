96033ce30baa8ca6f7fc329fc85e82a749b4ce81
can <can@anyscale.com>
2024-01-22 21:10:01 +0000

    [ci] script to collect weekly green metrics
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/automation/weekly_green_metric.py
+++ ci/ray_ci/automation/weekly_green_metric.py
@@ -1,19 +1,14 @@
 import click
 
 from ci.ray_ci.utils import logger
-from ci.ray_ci.tester_container import TesterContainer, PIPELINE_POSTMERGE
-from ray_release.configs.global_config import init_global_config
 from ray_release.test_automation.state_machine import TestStateMachine
 
 
 @click.command()
-@click.option(
-    "--production",
-    is_flag=True,
-    show_default=True,
-    default=False,
-    help=("Persist weekly green metric to S3"),
-)
-def main(production: bool) -> None:
-    ray_repo = TestStateMachine.get_ray_repo()
+def main() -> None:
+    blockers = TestStateMachine.get_release_blockers()
+    logger.info(f"Found {blockers.totalCount} release blockers")
 
+
+if __name__ == "__main__":
+    main()
