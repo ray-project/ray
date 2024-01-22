529518290a7b5aa53ee1211640136c106099d43c
can <can@anyscale.com>
2024-01-22 22:27:42 +0000

    [green][metric/2] group number of blockers by team
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/automation/weekly_green_metric.py
+++ ci/ray_ci/automation/weekly_green_metric.py
@@ -1,7 +1,4 @@
-from typing import List
-
 import click
-import github
 
 from ci.ray_ci.utils import logger
 from ray_release.test_automation.state_machine import TestStateMachine
@@ -11,6 +8,11 @@ from ray_release.test_automation.state_machine import TestStateMachine
 def main() -> None:
     blockers = TestStateMachine.get_release_blockers()
     logger.info(f"Found {blockers.totalCount} release blockers")
+
+    blocker_teams = [TestStateMachine.get_issue_owner(blocker) for blocker in blockers]
+    num_blocker_by_team = {team: blocker_teams.count(team) for team in blocker_teams}
+    for team, num_blocker in num_blocker_by_team.items():
+        logger.info(f"\t- Team {team} has {num_blocker} release blockers")
 
 
 if __name__ == "__main__":
