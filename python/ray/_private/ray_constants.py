5af4ae21955929f7479f67ac296b065c2a876e79
Ruiyang Wang <56065503+rynewang@users.noreply.github.com>
2024-01-25 11:37:23 -0800

    Update python/ray/_private/ray_constants.py
    Co-authored-by: angelinalg <122562471+angelinalg@users.noreply.github.com>
    Signed-off-by: Ruiyang Wang <56065503+rynewang@users.noreply.github.com>

--- python/ray/_private/ray_constants.py
+++ python/ray/_private/ray_constants.py
@@ -87,7 +87,7 @@ RAY_RUNTIME_ENV_URI_PIN_EXPIRATION_S_ENV_VAR = (
     "RAY_RUNTIME_ENV_TEMPORARY_REFERENCE_EXPIRATION_S"
 )
 # Ray populates this env var to the working dir in the creation of a runtime env.
-# e.g. `pip` and `conda` users can use this to find their requirements.txt.
+# For example, `pip` and `conda` users can use this environment variable to locate the `requirements.txt` file.
 RAY_RUNTIME_ENV_CREATE_WORKING_DIR_ENV_VAR = "RAY_RUNTIME_ENV_CREATE_WORKING_DIR"
 # Defaults to 10 minutes. This should be longer than the total time it takes for
 # the local working_dir and py_modules to be uploaded, or these files might get
