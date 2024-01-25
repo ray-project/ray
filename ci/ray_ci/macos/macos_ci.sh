2eae38508726f46d84b7cc773ad683d92d5ec90d
khluu <khluu000@gmail.com>
2024-01-24 17:17:14 -0800

    Query the list of flaky tests, exclude them from current test suites, then add a new test suite only for those flaky tests
    Signed-off-by: khluu <khluu000@gmail.com>

--- ci/ray_ci/macos/macos_ci.sh
+++ ci/ray_ci/macos/macos_ci.sh
@@ -11,31 +11,71 @@ export LANG="en_US.UTF-8"
 export BUILD="1"
 export DL="1"
 
+flaky_test_json_array=$(bazel run $(./ci/run/bazel_export_options) --config=ci \
+    ci/ray_ci/automation:query_flaky_macos_test)
+
+echo "Flaky tests: $flaky_test_json_array"
+
+flaky_tests=()
+
+# parse json array inot bash array
+while IFS= read -r line; do
+    flaky_tests+=("$line")
+done < <(echo "$flaky_test_json_array" | jq -r '.[]')
+
+# convert list of flaky tests into command to include/exclude from bazel test
+exclude_flaky_test_command=""
+include_flaky_test_command=""
+for item in "${flaky_tests[@]}"; do
+    exclude_flaky_test_command+="-${item} "
+    include_flaky_test_command+="${item} "
+done
+
+# strip newline char from the 
+exclude_flaky_test_command=${exclude_flaky_test_command%$'\n'}
+include_flaky_test_command=${include_flaky_test_command%$'\n'}
+
+run_flaky_test() {
+  # shellcheck disable=SC2046
+  bazel test $(./ci/run/bazel_export_options) --config=ci \
+    --test_env=CONDA_EXE --test_env=CONDA_PYTHON_EXE --test_env=CONDA_SHLVL --test_env=CONDA_PREFIX \
+    --test_env=CONDA_DEFAULT_ENV --test_env=CONDA_PROMPT_MODIFIER --test_env=CI \
+    $include_flaky_test_command
+}
+
 run_small_test() {
   # shellcheck disable=SC2046
   bazel test $(./ci/run/bazel_export_options) --config=ci \
     --test_env=CONDA_EXE --test_env=CONDA_PYTHON_EXE --test_env=CONDA_SHLVL --test_env=CONDA_PREFIX \
     --test_env=CONDA_DEFAULT_ENV --test_env=CONDA_PROMPT_MODIFIER --test_env=CI \
     --test_tag_filters=client_tests,small_size_python_tests \
-    -- python/ray/tests/...
+    -- python/ray/tests/...\
+    $exclude_flaky_test_command
 }
 
 run_medium_a_j_test() {
   # shellcheck disable=SC2046
   bazel test --config=ci $(./ci/run/bazel_export_options) --test_env=CI \
       --test_tag_filters=-kubernetes,medium_size_python_tests_a_to_j \
-      python/ray/tests/...
+      python/ray/tests/... \
+      $exclude_flaky_test_command
 }
 
 run_medium_k_z_test() {
   # shellcheck disable=SC2046
   bazel test --config=ci $(./ci/run/bazel_export_options) --test_env=CI \
       --test_tag_filters=-kubernetes,medium_size_python_tests_k_to_z \
-      python/ray/tests/...
+      python/ray/tests/... \
+      $exclude_flaky_test_command
 }
 
 run_large_test() {
-  ./ci/ci.sh test_large
+  # shellcheck disable=SC2046
+  bazel test --config=ci $(./ci/run/bazel_export_options) --test_env=CONDA_EXE --test_env=CONDA_PYTHON_EXE \
+      --test_env=CONDA_SHLVL --test_env=CONDA_PREFIX --test_env=CONDA_DEFAULT_ENV --test_env=CONDA_PROMPT_MODIFIER \
+      --test_env=CI --test_tag_filters="large_size_python_tests_shard_${BUILDKITE_PARALLEL_JOB}"  "$@" \
+      -- python/ray/tests/...
+      $exclude_flaky_test_command
 }
 
 run_core_dashboard_test() {
