97f2ffb5d7c3b04a8013e2c942a59aed2a2dea4d
Matti Picus <matti.picus@gmail.com>
2024-01-25 23:18:05 +0200

    shorten bazel output directories even more on windows (#42566)
    The AUTOMATED_BUILD environment variable is set on the conda-forge builds to solve various build problems when building there. One problem that came up in the 2.9.0 update is that the windows path is too long, which resulted in hard-to-debug build problems. This effectively does the fix for #42098 (setting up .bazelrc with startup --output_user_root=d:/tmp) programatically without involving .bazelrc.
    
    The code path will not be hit in the buildkite CI tests.
    
    ---------
    
    Signed-off-by: Matti Picus <matti.picus@gmail.com>
    Signed-off-by: mattip <matti.picus@gmail.com>
    
    Signed-off-by: wesley <rademakerwesley@gmail.com>

--- python/setup.py
+++ python/setup.py
@@ -49,7 +49,7 @@ CLEANABLE_SUBDIRS = [
 
 # In automated builds, we do a few adjustments before building. For instance,
 # the bazel environment is set up slightly differently, and symlinks are
-# replaced with junctions in Windows. This variable is set e.g. in our conda
+# replaced with junctions in Windows. This variable is set e.g. in our conda-forge
 # feedstock.
 is_automated_build = bool(int(os.environ.get("IS_AUTOMATED_BUILD", "0")))
 
@@ -560,13 +560,17 @@ def build(build_python, build_java, build_cpp):
             FutureWarning,
         )
 
-    if not is_automated_build:
-        bazel_precmd_flags = []
     if is_automated_build:
-        root_dir = os.path.join(
-            os.path.abspath(os.environ["SRC_DIR"]), "..", "bazel-root"
-        )
-        out_dir = os.path.join(os.path.abspath(os.environ["SRC_DIR"]), "..", "b-o")
+        src_dir = os.environ.get("SRC_DIR", False) or os.getcwd()
+        src_dir = os.path.abspath(src_dir)
+        if is_native_windows_or_msys():
+            drive = os.path.splitdrive(src_dir)[0] + "\\"
+            root_dir = os.path.join(drive, "bazel-root")
+            out_dir = os.path.join(drive, "b-o")
+            bazel_flags.append("--enable_runfiles=false")
+        else:
+            root_dir = os.path.join(src_dir, "..", "bazel-root")
+            out_dir = os.path.join(src_dir, "..", "b-o")
 
         for d in (root_dir, out_dir):
             if not os.path.exists(d):
@@ -576,9 +580,8 @@ def build(build_python, build_java, build_cpp):
             "--output_user_root=" + root_dir,
             "--output_base=" + out_dir,
         ]
-
-        if is_native_windows_or_msys():
-            bazel_flags.append("--enable_runfiles=false")
+    else:
+        bazel_precmd_flags = []
 
     bazel_targets = []
     bazel_targets += ["//:ray_pkg"] if build_python else []
