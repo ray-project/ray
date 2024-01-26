c162ba5d8d8b2b1048e27e3730c83c46a423289d
Shreyas Krishnaswamy <shrekris@anyscale.com>
2024-01-26 10:55:46 -0800

    Add object_ref_serialization.py
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

---
+++ doc/source/ray-core/doc_code/object_ref_serialization.py
@@ -0,0 +1,25 @@
+import ray
+from ray import cloudpickle
+
+FILE = "external_store.pickle"
+
+ray.init()
+
+my_dict = {"hello": "world"}
+
+obj_ref = ray.put(my_dict)
+with open(FILE, "wb+") as f:
+    cloudpickle.dump(obj_ref, f)
+
+# Object ref remains pinned in memory because
+# it was serialized with ray.cloudpickle
+del obj_ref
+
+with open(FILE, "rb") as f:
+    new_obj_ref = cloudpickle.load(f)
+
+# The deserialized object ref works as expected
+assert ray.get(new_obj_ref) == my_dict
+
+# Explicitly free the object
+ray._private.internal_api.free(new_obj_ref)
