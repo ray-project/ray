e01ec729c412bc1da6434c8d20ff97f1b3a6a09b
Shreyas Krishnaswamy <shrekris@anyscale.com>
2024-01-26 12:28:39 -0800

    Use ObjectRef capitalization in the code
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- doc/source/ray-core/doc_code/object_ref_serialization.py
+++ doc/source/ray-core/doc_code/object_ref_serialization.py
@@ -11,14 +11,14 @@ obj_ref = ray.put(my_dict)
 with open(FILE, "wb+") as f:
     cloudpickle.dump(obj_ref, f)
 
-# Object ref remains pinned in memory because
+# ObjectRef remains pinned in memory because
 # it was serialized with ray.cloudpickle.
 del obj_ref
 
 with open(FILE, "rb") as f:
     new_obj_ref = cloudpickle.load(f)
 
-# The deserialized object ref works as expected.
+# The deserialized ObjectRef works as expected.
 assert ray.get(new_obj_ref) == my_dict
 
 # Explicitly free the object.
