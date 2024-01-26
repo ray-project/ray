c0c6b1f90d56b3f58c2b407b7174811d3f3408b6
Shreyas Krishnaswamy <shrekris@anyscale.com>
2024-01-26 12:26:35 -0800

    Add periods
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- doc/source/ray-core/doc_code/object_ref_serialization.py
+++ doc/source/ray-core/doc_code/object_ref_serialization.py
@@ -12,14 +12,14 @@ with open(FILE, "wb+") as f:
     cloudpickle.dump(obj_ref, f)
 
 # Object ref remains pinned in memory because
-# it was serialized with ray.cloudpickle
+# it was serialized with ray.cloudpickle.
 del obj_ref
 
 with open(FILE, "rb") as f:
     new_obj_ref = cloudpickle.load(f)
 
-# The deserialized object ref works as expected
+# The deserialized object ref works as expected.
 assert ray.get(new_obj_ref) == my_dict
 
-# Explicitly free the object
+# Explicitly free the object.
 ray._private.internal_api.free(new_obj_ref)
