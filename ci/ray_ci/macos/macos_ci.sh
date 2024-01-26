602602811123310704c6be21a43b4a1c00f8543f
khluu <khluu000@gmail.com>
2024-01-25 16:52:05 -0800

    Filter flaky test for all test suite osx
    Signed-off-by: khluu <khluu000@gmail.com>

--- ci/ray_ci/macos/macos_ci.sh
+++ ci/ray_ci/macos/macos_ci.sh
@@ -12,11 +12,19 @@ export BUILD="1"
 export DL="1"
 
 filter_flaky_tests() {
+  # This function takes in a test targets list file, filters out flaky tests and
+  # overwrites the original test targets list file with the filtered list.
+
   test_targets_file="$1"
+  # temp file is needed because python script runs in bazel sandbox
   temp_path="$(bazel info bazel-genfiles)/temp_file.txt"
+  # copy test target file content to temp file
   cp "$test_targets_file" "$temp_path"
   bazel run ci/ray_ci/automation:filter_tests -- "$temp_path" "flaky"
+  # copy temp file content back to test target file
+  cp "$temp_path" "$test_targets_file"
 }
+
 query_small_test_targets() {
   bazel query 'attr(tags, "client_tests|small_size_python_tests", tests(//python/ray/tests/...))' > ci/ray_ci/macos/small_test_targets.txt
 }
@@ -29,7 +37,7 @@ run_small_test() {
     --test_env=CONDA_EXE --test_env=CONDA_PYTHON_EXE --test_env=CONDA_SHLVL --test_env=CONDA_PREFIX \
     --test_env=CONDA_DEFAULT_ENV --test_env=CONDA_PROMPT_MODIFIER --test_env=CI
 }
-run_small_test
+
 query_medium_a_j_test_targets() {
   bazel query 'attr(tags, "kubernetes|medium_size_python_tests_a_to_j", tests(//python/ray/tests/...))' > ci/ray_ci/macos/medium_a_j_test_targets.txt
 }
@@ -37,6 +45,7 @@ query_medium_a_j_test_targets() {
 run_medium_a_j_test() {
   # shellcheck disable=SC2046
   query_medium_a_j_test_targets
+  filter_flaky_tests ci/ray_ci/macos/medium_a_j_test_targets.txt
   cat ci/ray_ci/macos/medium_a_j_test_targets.txt | xargs bazel test --config=ci $(./ci/run/bazel_export_options) \
     --test_env=CI
 }
@@ -48,6 +57,7 @@ query_medium_k_z_test_targets() {
 run_medium_k_z_test() {
   # shellcheck disable=SC2046
   query_medium_k_z_test_targets
+  filter_flaky_tests ci/ray_ci/macos/medium_k_z_test_targets.txt
   cat ci/ray_ci/macos/medium_k_z_test_targets.txt | xargs bazel test --config=ci $(./ci/run/bazel_export_options) \
     --test_env=CI
 }
@@ -60,6 +70,7 @@ query_large_test() {
 run_large_test() {
   # shellcheck disable=SC2046
   query_large_test
+  filter_flaky_tests ci/ray_ci/macos/large_test_targets.txt
   cat ci/ray_ci/macos/large_test_targets.txt | xargs bazel test --config=ci $(./ci/run/bazel_export_options) \
     --test_env=CONDA_EXE --test_env=CONDA_PYTHON_EXE --test_env=CONDA_SHLVL --test_env=CONDA_PREFIX --test_env=CONDA_DEFAULT_ENV \
     --test_env=CONDA_PROMPT_MODIFIER --test_env=CI "$@"
