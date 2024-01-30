b8b348f7ca2df2530b0dd8f9d5557978d0c32015
rickyyx <rickyx@anyscale.com>
2024-01-30 22:22:10 +0000

    lint
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- python/ray/autoscaler/v2/tests/test_reconciler.py
+++ python/ray/autoscaler/v2/tests/test_reconciler.py
@@ -5,6 +5,11 @@ import sys
 import pytest  # noqa
 
 
+# TODO: add tests.
+def test_ok():
+    assert True
+
+
 if __name__ == "__main__":
     if os.environ.get("PARALLEL_CI"):
         sys.exit(pytest.main(["-n", "auto", "--boxed", "-vs", __file__]))
