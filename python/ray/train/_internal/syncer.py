974be7e387dba360ee01e290cc14eb1d059f815e
Justin Yu <justinvyu@anyscale.com>
2024-01-29 16:22:36 -0800

    mark syncer interface as developer api
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/train/_internal/syncer.py
+++ python/ray/train/_internal/syncer.py
@@ -3,14 +3,12 @@ import logging
 import threading
 import time
 import traceback
-import warnings
 from dataclasses import dataclass
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 from ray._private.thirdparty.tabulate.tabulate import tabulate
 from ray.train.constants import _DEPRECATED_VALUE
-from ray.util import log_once
-from ray.util.annotations import PublicAPI
+from ray.util.annotations import DeveloperAPI, PublicAPI
 from ray.widgets import Template
 
 logger = logging.getLogger(__name__)
@@ -180,6 +178,7 @@ class _BackgroundProcess:
         return result
 
 
+@DeveloperAPI
 class Syncer(abc.ABC):
     """Syncer class for synchronizing data between Ray nodes and remote (cloud) storage.
 
