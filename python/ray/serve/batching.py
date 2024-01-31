b6fc8d78dda8bedf782a30412f53d743814e61ec
Shreyas Krishnaswamy <shrekris@anyscale.com>
2024-01-31 15:25:18 -0800

    Deflake test
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- python/ray/serve/batching.py
+++ python/ray/serve/batching.py
@@ -107,8 +107,9 @@ class _BatchQueue:
         self.curr_iteration_start_time = time.time()
 
         self._handle_batch_task = None
+        self._loop = get_or_create_event_loop()
         if handle_batch_func is not None:
-            self._handle_batch_task = get_or_create_event_loop().create_task(
+            self._handle_batch_task = self._loop.create_task(
                 self._process_batches(handle_batch_func)
             )
 
@@ -250,14 +251,22 @@ class _BatchQueue:
     async def _process_batches(self, func: Callable) -> None:
         """Loops infinitely and processes queued request batches."""
 
-        while True:
+        while not self._loop.is_closed():
             try:
                 self.curr_iteration_start_time = time.time()
                 await self._process_batch(func)
             except Exception:
                 logger.exception(
-                    "_process_batches asyncio task ran into an " "unexpected exception."
+                    "_process_batches asyncio task ran into an unexpected exception."
                 )
+
+        logger.warning(
+            "The asyncio loop has closed. The @serve.batch handler is not "
+            "running anymore. All future requests sent to this handler will "
+            "fail. This should only occur if the Serve app is "
+            "being torn down. If it occurs any other time, please file an "
+            "issue on GitHub."
+        )
 
     async def _process_batch(self, func: Callable) -> None:
         """Processes queued request batch."""
