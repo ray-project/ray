1a748c8bfc2248468be5410250ae1ef5c12544b6
Justin Yu <justinvyu@anyscale.com>
2024-01-31 14:54:15 -0800

    update issue
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/train/mosaic/mosaic_trainer.py
+++ python/ray/train/mosaic/mosaic_trainer.py
@@ -3,16 +3,16 @@ from ray.util.annotations import Deprecated
 _DEPRECATION_MESSAGE = (
     "`ray.train.mosaic.MosaicTrainer` is deprecated. "
     "Use `ray.train.torch.TorchTrainer` instead. "
-    "See this issue for a migration example: "
-    "https://github.com/ray-project/ray/issues/42257"
+    "See this issue for more information: "
+    "https://github.com/ray-project/ray/issues/42893"
 )
 
 
 # TODO(justinvyu): [code_removal] Delete in Ray 2.11.
 @Deprecated
 class MosaicTrainer:
-    """Deprecated. See this issue for a migration example:
-    https://github.com/ray-project/ray/issues/42257
+    """Deprecated. See this issue for more information:
+    https://github.com/ray-project/ray/issues/42893
     """
 
     def __new__(cls, *args, **kwargs):
