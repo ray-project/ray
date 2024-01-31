110e52bdfd4610ea5f1c32fb58079cb2fa86564f
tterrysun <terry@anyscale.com>
2024-01-31 12:00:38 -0800

    better annotation
    Signed-off-by: tterrysun <terry@anyscale.com>

--- python/ray/dag/tests/test_accelerated_dag.py
+++ python/ray/dag/tests/test_accelerated_dag.py
@@ -98,7 +98,7 @@ def test_regular_args(ray_start_regular):
 
 
 def test_multi_args(ray_start_regular):
-    # Test passing multiple args to .bind in addition to DAGNode args.
+    # Test passing multiple args to .execute.
     a = Actor.remote(0)
     with InputNode() as i:
         dag = a.inc_two.bind(i[0], i[1])
@@ -116,7 +116,7 @@ def test_multi_args(ray_start_regular):
 
 
 def test_multi_kwargs(ray_start_regular):
-    # Test passing multiple kwargs to .bind in addition to DAGNode args.
+    # Test passing multiple kwargs to .execute.
     a = Actor.remote(0)
     with InputNode() as i:
         dag = a.inc_two.bind(i.x,  i.y)
@@ -134,7 +134,7 @@ def test_multi_kwargs(ray_start_regular):
 
 
 def test_multi_args_kwargs(ray_start_regular):
-    # Test passing multiple args and kwargs to .bind in addition to DAGNode args.
+    # Test passing multiple args and kwargs to .execute.
     a = Actor.remote(0)
     with InputNode() as i:
         dag = a.inc_four.bind(i[0],  i[1], i.x, i.y)
