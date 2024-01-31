420a67af9b00a97160f10774f76ec53a180e930c
Edward Oakes <ed.nmi.oakes@gmail.com>
2024-01-31 12:24:47 -0600

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

---
+++ python/ray/serve/_private/replica_scheduler/__init__.py
@@ -0,0 +1,8 @@
+from ray.serve._private.replica_scheduler.common import (  # noqa: F401
+    Query,
+    ReplicaScheduler,
+    ReplicaWrapper,
+)
+from ray.serve._private.replica_scheduler.pow_2_scheduler import (  # noqa: F401
+    PowerOfTwoChoicesReplicaScheduler,
+)
--- python/ray/serve/tests/unit/test_pow_2_replica_scheduler.py
+++ python/ray/serve/tests/unit/test_pow_2_replica_scheduler.py
@@ -10,7 +10,7 @@ import pytest
 import ray
 from ray._private.utils import get_or_create_event_loop
 from ray.serve._private.common import DeploymentID, RequestMetadata
-from ray.serve._private.router import (
+from ray.serve._private.replica_scheduler import (
     PowerOfTwoChoicesReplicaScheduler,
     Query,
     ReplicaWrapper,
@@ -123,7 +123,7 @@ def pow_2_scheduler(request) -> PowerOfTwoChoicesReplicaScheduler:
     # to 0.01s to speed up the test.
     os.environ.update({"RAY_SERVE_MULTIPLEXED_MODEL_ID_MATCHING_TIMEOUT_S": "0.01"})
     importlib.reload(ray.serve._private.constants)
-    importlib.reload(ray.serve._private.router)
+    importlib.reload(ray.serve._private.replica_scheduler.pow_2_scheduler)
 
     yield s
 
