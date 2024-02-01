fc15179b024176f1de0b93da136c76018b943c4f
khluu <khluu000@gmail.com>
2024-01-31 19:23:17 -0800

    lint fix
    Signed-off-by: khluu <khluu000@gmail.com>

--- ci/ray_ci/test_upgrade_version.py
+++ ci/ray_ci/test_upgrade_version.py
@@ -3,25 +3,38 @@ import sys
 import pytest
 import tempfile
 
-from ci.ray_ci.upgrade_version import list_java_files, get_current_version, upgrade_file_version
-from ci.ray_ci.utils import logger
+from ci.ray_ci.upgrade_version import (
+    list_java_files,
+    get_current_version,
+    upgrade_file_version,
+)
+
 
 @mock.patch("os.walk")
 def test_list_java_files(mock_os_walk):
     mock_os_walk.return_value = [
-        ("root/dir", ["subdir_1", "subdir_2", "subdir_3"], ["pom_template.xml", "not_pom.xml"]),
+        (
+            "root/dir",
+            ["subdir_1", "subdir_2", "subdir_3"],
+            ["pom_template.xml", "not_pom.xml"],
+        ),
         ("root/dir/subdir_1", [], ["pom.xml", "not_pom.xml", "not_pom_template.xml"]),
         ("root/dir/subdir_2", [], ["pom_template.xml", "pom.xml", "not_pom.py"]),
-        ("root/dir/subdir_3", [], ["pom.xml", "pom_template.xml", "not_pom_template.py"]),
+        (
+            "root/dir/subdir_3",
+            [],
+            ["pom.xml", "pom_template.xml", "not_pom_template.py"],
+        ),
     ]
-    assert list_java_files() == set([
+    assert list_java_files() == {
         "root/dir/pom_template.xml",
         "root/dir/subdir_1/pom.xml",
         "root/dir/subdir_2/pom_template.xml",
         "root/dir/subdir_2/pom.xml",
         "root/dir/subdir_3/pom.xml",
         "root/dir/subdir_3/pom_template.xml",
-    ])
+    }
+
 
 @mock.patch("subprocess.run")
 def test_get_current_version(mock_subprocess_run):
@@ -30,10 +43,17 @@ def test_get_current_version(mock_subprocess_run):
         stdout="1.1.1.default",
     )
     # Test when version is default
-    assert get_current_version("1.1.1.default", "2.0.0-SNAPSHOT") == ("1.1.1.default", "2.0.0-SNAPSHOT")
+    assert get_current_version("1.1.1.default", "2.0.0-SNAPSHOT") == (
+        "1.1.1.default",
+        "2.0.0-SNAPSHOT",
+    )
     # Test when version is different
-    assert get_current_version("1.1.1.dev1", "2.0.0-SNAPSHOT") == ("1.1.1.default", "1.1.1.default")
-    
+    assert get_current_version("1.1.1.dev1", "2.0.0-SNAPSHOT") == (
+        "1.1.1.default",
+        "1.1.1.default",
+    )
+
+
 def _prepare_tmp_file(java: bool = False):
     file = tempfile.NamedTemporaryFile()
     with open(file.name, "w") as f:
@@ -43,6 +63,7 @@ def _prepare_tmp_file(java: bool = False):
             f.write("version: 1.1.1.default")
         f.flush()
     return file
+
 
 def test_upgrade_file_version():
     non_java_version = "1.1.1.default"
@@ -55,23 +76,24 @@ def test_upgrade_file_version():
     java_file_1 = _prepare_tmp_file(java=True)
     java_file_2 = _prepare_tmp_file(java=True)
 
-    non_java_files = set([non_java_file_1.name, non_java_file_2.name])
-    java_files = set([java_file_1.name, java_file_2.name])
+    non_java_files = {non_java_file_1.name, non_java_file_2.name}
+    java_files = {java_file_1.name, java_file_2.name}
     upgrade_file_version(
-        non_java_files=non_java_files, 
-        java_files=java_files, 
-        non_java_version=non_java_version, 
-        java_version=java_version, 
-        new_version=new_version
+        non_java_files=non_java_files,
+        java_files=java_files,
+        non_java_version=non_java_version,
+        java_version=java_version,
+        new_version=new_version,
     )
 
     for file in non_java_files:
         with open(file, "r") as f:
             assert f.read() == f"version: {new_version}"
-    
+
     for file in java_files:
         with open(file, "r") as f:
             assert f.read() == f"<version>{new_version}</version>"
+
 
 if __name__ == "__main__":
     sys.exit(pytest.main(["-v", __file__]))
--- ci/ray_ci/upgrade_version.py
+++ ci/ray_ci/upgrade_version.py
@@ -5,10 +5,11 @@ import subprocess
 bazel_workspace_dir = os.environ.get("BUILD_WORKSPACE_DIRECTORY", "")
 
 non_java_files = {
-    'ci/ray_ci/utils.py',
-    'python/ray/_version.py',
-    'src/ray/common/constants.h',
+    "ci/ray_ci/utils.py",
+    "python/ray/_version.py",
+    "src/ray/common/constants.h",
 }
+
 
 def list_java_files():
     files = set()
@@ -24,29 +25,41 @@ def get_current_version(non_java_version: str, java_version: str):
     Scan for current Ray version and update the version global variables.
     """
     version_file_path = os.path.join(bazel_workspace_dir, "python/ray/_version.py")
-    output = subprocess.run(['python', version_file_path], stdout=subprocess.PIPE, text=True)
+    output = subprocess.run(
+        ["python", version_file_path], stdout=subprocess.PIPE, text=True
+    )
 
     if output.returncode != 0 or not output.stdout:
-        raise Exception('Failed to get current version')
-    version = output.stdout.split(' ')[0]
-    
+        raise Exception("Failed to get current version")
+    version = output.stdout.split(" ")[0]
+
     if version != non_java_version:
         non_java_version = version
         java_version = version
     return (non_java_version, java_version)
 
-def upgrade_file_version(non_java_files, java_files, non_java_version: str, java_version: str, new_version: str):
+
+def upgrade_file_version(
+    non_java_files,
+    java_files,
+    non_java_version: str,
+    java_version: str,
+    new_version: str,
+):
     """
     Modify the version in the files to the specified version.
     """
     for file_path in non_java_files | java_files:
-        with open(os.path.join(bazel_workspace_dir, file_path), 'r') as f:
+        with open(os.path.join(bazel_workspace_dir, file_path), "r") as f:
             content = f.read()
-        current_version = non_java_version if file_path in non_java_files else java_version
+        current_version = (
+            non_java_version if file_path in non_java_files else java_version
+        )
         content = content.replace(current_version, new_version)
 
-        with open(os.path.join(bazel_workspace_dir, file_path), 'w') as f:
+        with open(os.path.join(bazel_workspace_dir, file_path), "w") as f:
             f.write(content)
+
 
 @click.command()
 @click.option("--new_version", required=True, type=str)
@@ -59,7 +72,10 @@ def main(new_version: str):
     non_java_version, java_version = get_current_version(non_java_version, java_version)
 
     java_files = list_java_files()
-    upgrade_file_version(non_java_files, java_files, non_java_version, java_version, new_version)
+    upgrade_file_version(
+        non_java_files, java_files, non_java_version, java_version, new_version
+    )
+
 
 if __name__ == "__main__":
     main()
