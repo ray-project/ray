6b8e613fc6128c1d29ce9cf5bd374caf4f79204d
Balaji Veeramani <balaji@anyscale.com>
2024-02-01 17:09:07 -0800

    Appease lint
    Signed-off-by: Balaji Veeramani <balaji@anyscale.com>

--- python/ray/data/preprocessor.py
+++ python/ray/data/preprocessor.py
@@ -4,10 +4,10 @@ import collections
 import pickle
 import warnings
 from enum import Enum
-from typing import TYPE_CHECKING, Any, Dict, Optional, Union
+from typing import TYPE_CHECKING, Any, Dict, Union
 
 from ray.air.util.data_batch_conversion import BatchFormat
-from ray.util.annotations import Deprecated, DeveloperAPI, PublicAPI
+from ray.util.annotations import DeveloperAPI, PublicAPI
 
 if TYPE_CHECKING:
     import numpy as np
