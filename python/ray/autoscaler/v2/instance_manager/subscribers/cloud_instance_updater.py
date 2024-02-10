f95df6503f9cee132afab68bb425390c7fe740f1
Ricky Xu <xuchen727@hotmail.com>
2024-02-10 13:33:15 +0800

    update
    Signed-off-by: Ricky Xu <xuchen727@hotmail.com>

---
+++ python/ray/autoscaler/v2/instance_manager/subscribers/cloud_instance_updater.py
@@ -0,0 +1,88 @@
+import logging
+from collections import defaultdict
+from concurrent.futures import ThreadPoolExecutor
+import time
+from typing import List
+
+from ray.autoscaler.v2.instance_manager.instance_storage import (
+    InstanceUpdatedSubscriber,
+)
+from ray.autoscaler.v2.instance_manager.node_provider import ICloudInstanceProvider
+from ray.core.generated.instance_manager_pb2 import Instance, InstanceUpdateEvent
+
+logger = logging.getLogger(__name__)
+
+
+class CloudInstanceUpdater(InstanceUpdatedSubscriber):
+    """CloudInstanceUpdater is responsible for launching
+    new instances and terminating cloud instances
+
+    It requests the cloud instance provider to launch new instances when
+    there are new instance requests (with REQUESTED status change).
+    """
+
+    def __init__(
+        self,
+        cloud_provider: ICloudInstanceProvider,
+    ) -> None:
+        self._cloud_provider = cloud_provider
+        self._executor = ThreadPoolExecutor(max_workers=1)
+
+    def notify(self, events: List[InstanceUpdateEvent]) -> None:
+        new_requests = [
+            event for event in events if event.new_instance_status == Instance.REQUESTED
+        ]
+        new_terminations = [
+            event
+            for event in events
+            if event.new_instance_status == Instance.TERMINATING
+        ]
+        fut = self._executor.submit(self._launch_new_instances, new_requests)
+        fut.result()
+        fut = self._executor.submit(self._terminate_instances, new_terminations)
+        fut.result()
+
+    def _terminate_instances(self, new_terminations: List[InstanceUpdateEvent]):
+        """
+        Terminate cloud instances through cloud provider.
+
+        Args:
+            new_terminations: List of new instance terminations.
+        """
+        if not new_terminations:
+            logger.debug("No instances to terminate.")
+            return
+
+        # Terminate the instances.
+        instance_ids = [event.instance_id for event in new_terminations]
+        self._cloud_provider.terminate(ids=instance_ids, request_id=str(time.time_ns()))
+
+    def _launch_new_instances(self, new_requests: List[InstanceUpdateEvent]):
+        """
+        Launches new instances by requesting the cloud provider.
+
+        Args:
+            new_requests: List of new instance requests.
+
+        """
+        if not new_requests:
+            logger.debug("No instances to launch.")
+            return
+
+        # Group new requests by launch request id.
+        requests_by_launch_request_id = defaultdict(list)
+
+        for event in new_requests:
+            assert (
+                event.launch_request_id
+            ), "Launch request id should have been set by the reconciler"
+            requests_by_launch_request_id[event.launch_request_id].append(event)
+
+        for launch_request_id, events in requests_by_launch_request_id.items():
+            request_shape = defaultdict(int)
+            for event in events:
+                request_shape[event.instance_type] += 1
+            # Make requests to the cloud provider.
+            self._cloud_provider.launch(
+                shape=request_shape, request_id=launch_request_id
+            )
---
+++ python/ray/autoscaler/v2/tests/test_subscribers.py
@@ -0,0 +1,144 @@
+# coding: utf-8
+import os
+import sys
+
+import pytest
+
+import mock
+
+from ray._private.test_utils import wait_for_condition
+from ray.autoscaler.v2.instance_manager.subscribers.cloud_instance_updater import (
+    CloudInstanceUpdater,
+)
+from ray.core.generated.instance_manager_pb2 import Instance, InstanceUpdateEvent
+
+
+class TestCloudInstanceUpdater:
+    def test_launch_no_op(self):
+        mock_provider = mock.MagicMock()
+        launcher = CloudInstanceUpdater(mock_provider)
+        launcher.notify(
+            [
+                InstanceUpdateEvent(
+                    new_instance_status=Instance.RAY_RUNNING,
+                    launch_request_id="1",
+                    instance_type="type-1",
+                ),
+            ]
+        )
+        mock_provider.launch.assert_not_called()
+
+    def test_launch_new_instances(self):
+        mock_provider = mock.MagicMock()
+        launcher = CloudInstanceUpdater(mock_provider)
+        launcher.notify(
+            [
+                InstanceUpdateEvent(
+                    new_instance_status=Instance.REQUESTED,
+                    launch_request_id="1",
+                    instance_type="type-1",
+                ),
+                InstanceUpdateEvent(
+                    new_instance_status=Instance.REQUESTED,
+                    launch_request_id="1",
+                    instance_type="type-1",
+                ),
+                InstanceUpdateEvent(
+                    new_instance_status=Instance.REQUESTED,
+                    launch_request_id="2",
+                    instance_type="type-1",
+                ),
+                InstanceUpdateEvent(
+                    new_instance_status=Instance.REQUESTED,
+                    launch_request_id="2",
+                    instance_type="type-2",
+                ),
+            ]
+        )
+
+        mock_provider.launch.assert_has_calls(
+            [
+                mock.call(shape={"type-1": 2}, request_id="1"),
+                mock.call(shape={"type-1": 1, "type-2": 1}, request_id="2"),
+            ]
+        )
+
+    def test_multi_notify(self):
+        mock_provider = mock.MagicMock()
+        launcher = CloudInstanceUpdater(mock_provider)
+
+        launcher.notify(
+            [
+                InstanceUpdateEvent(
+                    new_instance_status=Instance.REQUESTED,
+                    launch_request_id="1",
+                    instance_type="type-1",
+                ),
+            ]
+        )
+
+        launcher.notify(
+            [
+                InstanceUpdateEvent(
+                    new_instance_status=Instance.REQUESTED,
+                    launch_request_id="2",
+                    instance_type="type-1",
+                ),
+            ]
+        )
+
+        def verify():
+            assert mock_provider.launch.call_count == 2
+            mock_provider.launch.assert_has_calls(
+                [
+                    mock.call(shape={"type-1": 1}, request_id="1"),
+                    mock.call(shape={"type-1": 1}, request_id="2"),
+                ]
+            )
+            return True
+
+        wait_for_condition(verify)
+
+    def test_terminate_no_op(self):
+        mock_provider = mock.MagicMock()
+        launcher = CloudInstanceUpdater(mock_provider)
+        launcher.notify(
+            [
+                InstanceUpdateEvent(
+                    new_instance_status=Instance.RAY_RUNNING,
+                    instance_id="1",
+                ),
+            ]
+        )
+        mock_provider.terminate.assert_not_called()
+
+    def test_terminate_instances(self):
+        mock_provider = mock.MagicMock()
+        launcher = CloudInstanceUpdater(mock_provider)
+        launcher.notify(
+            [
+                InstanceUpdateEvent(
+                    new_instance_status=Instance.TERMINATING,
+                    instance_id="1",
+                ),
+                InstanceUpdateEvent(
+                    new_instance_status=Instance.TERMINATING,
+                    instance_id="2",
+                ),
+                InstanceUpdateEvent(
+                    new_instance_status=Instance.TERMINATING,
+                    instance_id="3",
+                ),
+            ]
+        )
+
+        mock_provider.terminate.assert_called_once_with(
+            ids=["1", "2", "3"], request_id=mock.ANY
+        )
+
+
+if __name__ == "__main__":
+    if os.environ.get("PARALLEL_CI"):
+        sys.exit(pytest.main(["-n", "auto", "--boxed", "-vs", __file__]))
+    else:
+        sys.exit(pytest.main(["-sv", __file__]))
