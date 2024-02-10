36665b255c5a7cae1fe7a4feb13223139d9b1469
Ricky Xu <xuchen727@hotmail.com>
2024-02-10 19:58:27 +0800

    update
    Signed-off-by: Ricky Xu <xuchen727@hotmail.com>

--- python/ray/autoscaler/v2/instance_manager/subscribers/ray_stopper.py
+++ python/ray/autoscaler/v2/instance_manager/subscribers/ray_stopper.py
@@ -1,8 +1,10 @@
+from concurrent.futures import ThreadPoolExecutor
+import logging
 from typing import List
-from ray.autoscaler.v2.instance_manager import (
+from ray.autoscaler.v2.instance_manager.instance_manager import (
     InstanceUpdatedSubscriber,
 )
-from ray.autoscaler._private.gcs_client import GcsClient
+from ray._raylet import GcsClient
 from ray.core.generated.instance_manager_pb2 import (
     InstanceUpdateEvent,
     Instance,
@@ -10,17 +12,20 @@ from ray.core.generated.instance_manager_pb2 import (
 )
 from ray.core.generated.autoscaler_pb2 import DrainNodeRequest, DrainNodeReason
 
+logger = logging.getLogger(__name__)
+
 
 class RayStopper(InstanceUpdatedSubscriber):
     """RayStopper is responsible for stopping ray on instances."""
 
     def __init__(self, gcs_client: GcsClient) -> None:
         self._gcs_client = gcs_client
+        self._executor = ThreadPoolExecutor(max_workers=1)
 
     def notify(self, events: List[InstanceUpdateEvent]) -> None:
         for event in events:
             if event.new_instance_status == Instance.RAY_STOPPED:
-                self._stop_ray(event)
+                self._executor.submit(self._stop_ray, event)
 
     def _stop_ray(self, event: InstanceUpdateEvent) -> None:
         termination_request = event.termination_request
@@ -28,16 +33,33 @@ class RayStopper(InstanceUpdatedSubscriber):
 
         if termination_request.cause == TerminationRequest.Cause.IDLE:
             reason = DrainNodeReason.DRAIN_NODE_REASON_IDLE_TERMINATION
-            reason_str = "Idle termination for node for {} seconds.".format(
+            reason_str = "Idle termination of node for {} seconds.".format(
                 termination_request.idle_time_ms / 1000
             )
         elif (
             termination_request.cause == TerminationRequest.Cause.MAX_NUM_NODE_PER_TYPE
         ):
             reason = DrainNodeReason.DRAIN_NODE_REASON_PREEMPTION
-            reason_str
+            reason_str = "Preempted due to max number of nodes per type={}.".format(
+                termination_request.max_num_nodes_per_type
+            )
+        elif termination_request.cause == TerminationRequest.Cause.MAX_NUM_NODES:
+            reason = DrainNodeReason.DRAIN_NODE_REASON_PREEMPTION
+            reason_str = "Preempted due to max number of nodes={}.".format(
+                termination_request.max_num_nodes
+            )
 
-        drain_node_request = DrainNodeRequest(
+        reply = self._gcs_client.drain_node(
             node_id=ray_node_id.encode("utf-8"),
             reason=reason,
+            reason_message=reason_str,
+            # TODO: we could probably add a deadline here that's derived
+            # from the stuck instance reconcilation configs.
+            deadline_timestamp_ms=0,
         )
+        if reply.is_accepted:
+            logger.info("Stopping ray on {}: {}".format(ray_node_id, reason_str))
+        else:
+            logger.warning(
+                "Failed to stop ray on {}: {}".format(ray_node_id, reason_str)
+            )
---
+++ python/ray/autoscaler/v2/tests/test_subscribers.py
@@ -0,0 +1,125 @@
+# coding: utf-8
+import os
+import sys
+import mock
+
+import pytest
+
+from ray.autoscaler.v2.instance_manager.subscribers.ray_stopper import (
+    RayStopper,
+)  # noqa
+from ray.core.generated.instance_manager_pb2 import (
+    Instance,
+    InstanceUpdateEvent,
+    TerminationRequest,
+)
+from ray.core.generated.autoscaler_pb2 import DrainNodeReason, DrainNodeReply
+from ray._private.test_utils import wait_for_condition
+
+
+class TestRayStopper:
+    def test_no_op(self):
+        mock_gcs_client = mock.MagicMock()
+        ray_stopper = RayStopper(gcs_client=mock_gcs_client)
+
+        ray_stopper.notify(
+            [
+                InstanceUpdateEvent(
+                    instance_id="test_id",
+                    new_instance_status=Instance.REQUESTED,
+                )
+            ]
+        )
+        assert mock_gcs_client.drain_node.call_count == 0
+
+    def test_idle_termination(self):
+        mock_gcs_client = mock.MagicMock()
+        reply = DrainNodeReply(is_accepted=True)
+        mock_gcs_client.drain_node.return_value = reply
+        ray_stopper = RayStopper(gcs_client=mock_gcs_client)
+
+        ray_stopper.notify(
+            [
+                InstanceUpdateEvent(
+                    instance_id="test_id",
+                    new_instance_status=Instance.RAY_STOPPED,
+                    termination_request=TerminationRequest(
+                        cause=TerminationRequest.Cause.IDLE,
+                        idle_time_ms=1000,
+                        ray_node_id="id1",
+                    ),
+                )
+            ]
+        )
+
+        def verify():
+            mock_gcs_client.drain_node.assert_has_calls(
+                [
+                    mock.call(
+                        node_id=b"id1",
+                        reason=DrainNodeReason.DRAIN_NODE_REASON_IDLE_TERMINATION,
+                        reason_message="Idle termination of node for 1.0 seconds.",
+                        deadline_timestamp_ms=0,
+                    )
+                ]
+            )
+            return True
+
+        wait_for_condition(verify)
+
+    def test_preemption(self):
+        mock_gcs_client = mock.MagicMock()
+        reply = DrainNodeReply(is_accepted=True)
+        mock_gcs_client.drain_node.return_value = reply
+        ray_stopper = RayStopper(gcs_client=mock_gcs_client)
+
+        ray_stopper.notify(
+            [
+                InstanceUpdateEvent(
+                    instance_id="i-1",
+                    new_instance_status=Instance.RAY_STOPPED,
+                    termination_request=TerminationRequest(
+                        cause=TerminationRequest.Cause.MAX_NUM_NODE_PER_TYPE,
+                        max_num_nodes_per_type=10,
+                        ray_node_id="id1",
+                    ),
+                ),
+                InstanceUpdateEvent(
+                    instance_id="i-2",
+                    new_instance_status=Instance.RAY_STOPPED,
+                    termination_request=TerminationRequest(
+                        cause=TerminationRequest.Cause.MAX_NUM_NODES,
+                        max_num_nodes=100,
+                        ray_node_id="id2",
+                    ),
+                ),
+            ]
+        )
+
+        def verify():
+            mock_gcs_client.drain_node.assert_has_calls(
+                [
+                    mock.call(
+                        node_id=b"id1",
+                        reason=DrainNodeReason.DRAIN_NODE_REASON_PREEMPTION,
+                        reason_message="Preempted due to max number of nodes per type=10.",
+                        deadline_timestamp_ms=0,
+                    ),
+                    mock.call(
+                        node_id=b"id2",
+                        reason=DrainNodeReason.DRAIN_NODE_REASON_PREEMPTION,
+                        reason_message="Preempted due to max number of nodes=100.",
+                        deadline_timestamp_ms=0,
+                    ),
+                ]
+            )
+            return True
+
+        wait_for_condition(verify)
+
+
+if __name__ == "__main__":
+    if os.environ.get("PARALLEL_CI"):
+        sys.exit(pytest.main(["-n", "auto", "--boxed", "-vs", __file__]))
+    else:
+        sys.exit(pytest.main(["-sv", __file__]))
