fc7e9a7e0c45894965d1faee57b8a164c4363f8e
Ruiyang Wang <rywang014@gmail.com>
2024-02-12 12:51:15 -0800

    Bump version from 2.9.2 to 2.9.3
    Note: previous PR #42719 did not update doc/source/ray-overview/pip_freeze_ray-py39-cpu.txt and doc/source/ray-overview/pip_freeze_ray-ml-py39-cpu.txt. But I feel it worths updating.
    
    Signed-off-by: Ruiyang Wang <rywang014@gmail.com>

--- python/ray/_version.py
+++ python/ray/_version.py
@@ -1,6 +1,6 @@
 # Replaced with the current commit when building the wheels.
 commit = "{{RAY_COMMIT_SHA}}"
-version = "2.9.2"
+version = "2.9.3"
 
 if __name__ == "__main__":
     print("%s %s" % (version, commit))
