b8d86b005484b8f7ff9e50852f3c3f79fc378ecb
khluu <khluu000@gmail.com>
2024-02-12 22:41:06 -0800

    Clean up code
    Signed-off-by: khluu <khluu000@gmail.com>

--- .buildkite/release-automation/verify-macos-wheels.sh
+++ .buildkite/release-automation/verify-macos-wheels.sh
@@ -3,19 +3,32 @@
 set -euo pipefail
 
 set -x
-echo "Architecture: "$1""
+
+# Check for arguments
+if [ "$#" -ne 1 ]; then
+    echo "Missing argument to specify machine architecture."
+    echo "Use: x86_64 or arm64"
+    exit 1
+fi
+
+mac_architecture=$1 # First argument is the architecture of the machine, e.g. x86_64, arm64
+
 export PYTHON_VERSION="${PYTHON_VERSION:-3.8}"
 export RAY_VERSION="${RAY_VERSION:-2.9.1}"
 export RAY_HASH="${RAY_HASH:-cfbf98c315cfb2710c56039a3c96477d196de049}"
 
+# Install miniconda3 based on the architecture used
 mkdir -p ~/miniconda3
-curl https://repo.anaconda.com/miniconda/Miniconda3-py38_23.1.0-1-MacOSX-"$1".sh -o ~/miniconda3/miniconda.sh
+curl https://repo.anaconda.com/miniconda/Miniconda3-py38_23.1.0-1-MacOSX-"$mac_architecture".sh -o ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
-~/miniconda3/bin/conda init bash
 
+# Add miniconda3 to the PATH to use `conda` command.
 export PATH="$HOME/miniconda3/bin:$PATH"
-source $HOME/miniconda3/etc/profile.d/conda.sh # Initialize conda. Conda init requires a shell restart which should not be done on BK.
+
+# Initialize conda. This replaces calling `conda init bash`.
+# Conda init command requires a shell restart which should not be done on BK.
+source "$HOME/miniconda3/etc/profile.d/conda.sh"
 
 conda create -n rayio python="${PYTHON_VERSION}" -y
 
