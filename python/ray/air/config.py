cde842b9dc9c425ce00929712ecdb5ffe7d38771
woshiyyya <xiaoyunxuan1998@gmail.com>
2024-02-13 10:30:27 -0800

    update
    Signed-off-by: woshiyyya <xiaoyunxuan1998@gmail.com>

--- python/ray/air/config.py
+++ python/ray/air/config.py
@@ -263,10 +263,10 @@ class ScalingConfig:
     ) -> "ScalingConfig":
         """Create a ScalingConfig from a Tune's PlacementGroupFactory"""
         if pgf.head_bundle_is_empty:
-            trainer_resources = {}
+            trainer_bundle = {}
             worker_bundles = pgf.bundles
         else:
-            trainer_resources = pgf.bundles[0]
+            trainer_bundle = pgf.bundles[0]
             worker_bundles = pgf.bundles[1:]
 
         use_gpu = False
@@ -275,15 +275,19 @@ class ScalingConfig:
         num_workers = None
 
         if worker_bundles:
-            first_bundle = worker_bundles[0]
-            if not all(bundle == first_bundle for bundle in worker_bundles[1:]):
-                raise ValueError(
-                    "All worker bundles (any other than the first one) "
-                    "must be equal to each other."
-                )
-            use_gpu = bool(first_bundle.get("GPU"))
+            # In TrainTrainer, we merged the trainer resource into the first worker
+            # resources bundle. Need to extract trainer resource by calculating the
+            # resources diff.
+            # Note: If there's only one worker, we won't be able to calculate the diff.
+            # We'll have empty trainer bundle and assign all resources to the worker.
+            worker_bundle = worker_bundles[-1]
+            combined_bundle = worker_bundles[0]
+            trainer_resources = Counter(combined_bundle) - Counter(worker_bundle)
+            trainer_resources = dict(Counter(trainer_bundle) + trainer_resources)
+
+            use_gpu = bool(worker_bundle.get("GPU"))
             num_workers = len(worker_bundles)
-            resources_per_worker = first_bundle
+            resources_per_worker = worker_bundle
 
         return ScalingConfig(
             trainer_resources=trainer_resources,
