b8782e85512d384deb942701f37e9a0fe67fa5bd
Ruiyang Wang <56065503+rynewang@users.noreply.github.com>
2024-02-12 19:23:22 -0800

    Bump version from 2.9.2 to 2.9.3 (#43105)
    Signed-off-by: Ruiyang Wang <rywang014@gmail.com>

--- src/ray/common/constants.h
+++ src/ray/common/constants.h
@@ -61,7 +61,7 @@ constexpr int kMessagePackOffset = 9;
 constexpr char kSetupWorkerFilename[] = "setup_worker.py";
 
 /// The version of Ray
-constexpr char kRayVersion[] = "2.9.2";
+constexpr char kRayVersion[] = "2.9.3";
 
 /*****************************/
 /* ENV labels for autoscaler */
