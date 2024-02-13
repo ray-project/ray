2070005b8c02f1937d09415bd28d1e7e4a2467d1
khluu <khluu000@gmail.com>
2024-02-13 00:05:48 -0800

    Put conda back in and check bazel version
    Signed-off-by: khluu <khluu000@gmail.com>

--- .buildkite/release-automation/verify-macos-wheels.sh
+++ .buildkite/release-automation/verify-macos-wheels.sh
@@ -15,6 +15,21 @@ mac_architecture=$1 # First argument is the architecture of the machine, e.g. x8
 
 # Install dependencies (Bazel, Miniconda3, etc.)
 . ./ci/env/install-bazel.sh
+echo $PATH
+bazel --version
+
+# Install miniconda3 based on the architecture used
+mkdir -p ~/miniconda3
+curl https://repo.anaconda.com/miniconda/Miniconda3-py38_23.1.0-1-MacOSX-"$mac_architecture".sh -o ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+
+# Add miniconda3 to the PATH to use `conda` command.
+export PATH="$HOME/miniconda3/bin:$PATH"
+
+# Initialize conda. This replaces calling `conda init bash`.
+# Conda init command requires a shell restart which should not be done on BK.
+source "$HOME/miniconda3/etc/profile.d/conda.sh"
 
 export PYTHON_VERSION="${PYTHON_VERSION:-3.8}"
 export RAY_VERSION="${RAY_VERSION:-2.9.1}"
