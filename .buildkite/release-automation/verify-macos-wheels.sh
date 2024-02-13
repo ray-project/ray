00f5cf64cab791e750abc9ef9faee159fca5ea71
khluu <khluu000@gmail.com>
2024-02-12 23:21:47 -0800

    Try install_dependencies.sh
    Signed-off-by: khluu <khluu000@gmail.com>

--- .buildkite/release-automation/verify-macos-wheels.sh
+++ .buildkite/release-automation/verify-macos-wheels.sh
@@ -13,22 +13,12 @@ fi
 
 mac_architecture=$1 # First argument is the architecture of the machine, e.g. x86_64, arm64
 
+# Install dependencies (Bazel, Miniconda3, etc.)
+"${ROOT_DIR}/env/install-dependencies.sh"
+
 export PYTHON_VERSION="${PYTHON_VERSION:-3.8}"
 export RAY_VERSION="${RAY_VERSION:-2.9.1}"
 export RAY_HASH="${RAY_HASH:-cfbf98c315cfb2710c56039a3c96477d196de049}"
-
-# Install miniconda3 based on the architecture used
-mkdir -p ~/miniconda3
-curl https://repo.anaconda.com/miniconda/Miniconda3-py38_23.1.0-1-MacOSX-"$mac_architecture".sh -o ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
-
-# Add miniconda3 to the PATH to use `conda` command.
-export PATH="$HOME/miniconda3/bin:$PATH"
-
-# Initialize conda. This replaces calling `conda init bash`.
-# Conda init command requires a shell restart which should not be done on BK.
-source "$HOME/miniconda3/etc/profile.d/conda.sh"
 
 conda create -n rayio python="${PYTHON_VERSION}" -y
 
