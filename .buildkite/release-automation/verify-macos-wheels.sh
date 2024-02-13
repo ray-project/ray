f5fc16c912bdeaefb4f2e3d8972bd2bf1d1cdb3a
khluu <khluu000@gmail.com>
2024-02-12 17:25:48 -0800

    install conda for mac
    Signed-off-by: khluu <khluu000@gmail.com>

---
+++ .buildkite/release-automation/verify-macos-wheels.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euo pipefail
+
+set -x
+echo "Architecture: "$1""
+export PYTHON_VERSION="${PYTHON_VERSION:-3.8}"
+export RAY_VERSION="${RAY_VERSION:-2.9.1}"
+export RAY_HASH="${RAY_HASH:-cfbf98c315cfb2710c56039a3c96477d196de049}"
+
+mkdir -p ~/miniconda3
+curl https://repo.anaconda.com/miniconda/Miniconda3-py38_23.1.0-1-MacOSX-"$1".sh -o ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+
+export PATH="/root/miniconda3/bin:$PATH"
+
+conda create -n rayio python="${PYTHON_VERSION}" -y
+
+conda activate rayio
+
+pip install \
+    --index-url https://test.pypi.org/simple/ \
+    --extra-index-url https://pypi.org/simple \
+    "ray[cpp]==$RAY_VERSION"
+
+(
+    cd release/util
+    python sanity_check.py
+)
