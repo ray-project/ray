f9bd05bb01a8d9b6a9587c5148c7585211edfd58
khluu <khluu000@gmail.com>
2024-02-13 00:44:53 -0800

    Put code into functions
    Signed-off-by: khluu <khluu000@gmail.com>

--- .buildkite/release-automation/verify-macos-wheels.sh
+++ .buildkite/release-automation/verify-macos-wheels.sh
@@ -6,7 +6,7 @@ set -x
 
 python_versions=("3.8" "3.9" "3.10" "3.11")
 
-# Check for arguments
+# Check arguments
 if [ "$#" -ne 1 ]; then
     echo "Missing argument to specify machine architecture."
     echo "Use: x86_64 or arm64"
@@ -14,33 +14,26 @@ if [ "$#" -ne 1 ]; then
 fi
 
 mac_architecture=$1 # First argument is the architecture of the machine, e.g. x86_64, arm64
-
 export RAY_VERSION="${RAY_VERSION:-2.9.1}"
 export RAY_HASH="${RAY_HASH:-cfbf98c315cfb2710c56039a3c96477d196de049}"
 
-# Create tmp directory unique for the run
-TMP_DIR="$HOME/tmp/run-macos"
-mkdir -p "$TMP_DIR"
+install_miniconda() {
+    # Install miniconda3 based on the architecture used
+    mkdir -p "$TMP_DIR/miniconda3"
+    curl https://repo.anaconda.com/miniconda/Miniconda3-py38_23.1.0-1-MacOSX-"$mac_architecture".sh -o "$TMP_DIR/miniconda3/miniconda.sh"
+    bash "$TMP_DIR/miniconda3/miniconda.sh" -b -u -p "$TMP_DIR/miniconda3"
+    rm -rf "$TMP_DIR/miniconda3/miniconda.sh"
 
-# Install miniconda3 based on the architecture used
-mkdir -p "$TMP_DIR/miniconda3"
-curl https://repo.anaconda.com/miniconda/Miniconda3-py38_23.1.0-1-MacOSX-"$mac_architecture".sh -o "$TMP_DIR/miniconda3/miniconda.sh"
-bash "$TMP_DIR/miniconda3/miniconda.sh" -b -u -p "$TMP_DIR/miniconda3"
-rm -rf "$TMP_DIR/miniconda3/miniconda.sh"
+    # Add miniconda3 to the PATH to use `conda` command.
+    export PATH="$TMP_DIR/miniconda3/bin:$PATH"
 
-# Add miniconda3 to the PATH to use `conda` command.
-export PATH="$TMP_DIR/miniconda3/bin:$PATH"
+    # Initialize conda. This replaces calling `conda init bash`.
+    # Conda init command requires a shell restart which should not be done on BK.
+    source "$TMP_DIR/miniconda3/etc/profile.d/conda.sh"
+}
 
-# Initialize conda. This replaces calling `conda init bash`.
-# Conda init command requires a shell restart which should not be done on BK.
-source "$TMP_DIR/miniconda3/etc/profile.d/conda.sh"
-pip install \
-    --index-url https://test.pypi.org/simple/ \
-    --extra-index-url https://pypi.org/simple \
-    "ray[cpp]==$RAY_VERSION"
-
-# Run sanity checks for each python version
-for python_version in "${python_versions[@]}"; do
+run_sanity_check() {
+    python_version=$1
     conda create -n "rayio_${python_version}" python="${python_version}" -y
     conda activate "rayio_${python_version}"
     (
@@ -48,7 +41,22 @@ for python_version in "${python_versions[@]}"; do
         python sanity_check.py
     )
     conda deactivate
+}
 
+# Create tmp directory unique for the run
+TMP_DIR="$HOME/tmp/run-macos"
+mkdir -p "$TMP_DIR"
+
+install_miniconda
+
+pip install \
+    --index-url https://test.pypi.org/simple/ \
+    --extra-index-url https://pypi.org/simple \
+    "ray[cpp]==$RAY_VERSION"
+
+# Run sanity checks for each python version
+for python_version in "${python_versions[@]}"; do
+    run_sanity_check "$python_version"
 done
 
 rm -rf "$TMP_DIR"
