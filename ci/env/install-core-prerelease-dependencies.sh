59bbfef6c48bb939621756a3c40d9df4fc66713f
Lonnie Liu <lonnie@anyscale.com>
2024-02-14 07:01:19 +0000

    just to test if things can work

--- ci/env/install-core-prerelease-dependencies.sh
+++ ci/env/install-core-prerelease-dependencies.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 # install all unbounded dependencies in setup.py for ray core
 # TOOD(scv119) reenable grpcio once https://github.com/grpc/grpc/issues/31885 is fixed.
 # TOOD(scv119) reenable jsonschema once https://github.com/ray-project/ray/issues/33411 is fixed.
-for dependency in aiosignal frozenlist requests protobuf
-do
-    python -m pip install -U --pre --upgrade-strategy=eager $dependency
-done
+DEPS=(aiosignal frozenlist requests protobuf)
+
+python -m pip uninstall -y "${DEPS[@]}"
+python -m pip install -U --upgrade-strategy=eager "${DEPS[@]}"
