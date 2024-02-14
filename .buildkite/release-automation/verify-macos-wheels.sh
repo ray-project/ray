ff34af74bf9eede5a35fbf7576d9dbf7c8ea75ba
khluu <khluu000@gmail.com>
2024-02-13 17:13:43 -0800

    format fix
    Signed-off-by: khluu <khluu000@gmail.com>

--- .buildkite/release-automation/verify-macos-wheels.sh
+++ .buildkite/release-automation/verify-macos-wheels.sh
@@ -20,9 +20,9 @@ export RAY_HASH="${RAY_HASH:-cfbf98c315cfb2710c56039a3c96477d196de049}"
 export USE_BAZEL_VERSION="${USE_BAZEL_VERSION:-5.4.1}"
 
 install_bazel() {
-    if [ "${mac_architecture}" = "arm64" ]; then
+    if [[ "${mac_architecture}" = "arm64" ]]; then
       URL="https://github.com/bazelbuild/bazelisk/releases/download/${BAZELISK_VERSION}/bazelisk-darwin-arm64"
-    elif [ "${mac_architecture}" = "x86_64" ]; then
+    elif [[ "${mac_architecture}" = "x86_64" ]]; then
       URL="https://github.com/bazelbuild/bazelisk/releases/download/${BAZELISK_VERSION}/bazelisk-darwin-amd64"
     else
       echo "Could not find matching bazelisk URL for Mac ${mac_architecture}"
@@ -71,7 +71,7 @@ _clean_up() {
 }
 
 # Create tmp directory unique for the run
-TMP_DIR=$(mktemp -d "$HOME/tmp.XXXXXXXXXX")
+TMP_DIR="$(mktemp -d "$HOME/tmp.XXXXXXXXXX")"
 mkdir -p "$TMP_DIR/bin"
 # Add bin dir to the path.
 export PATH="$TMP_DIR/bin:$PATH"
