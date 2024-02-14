d5ca966c7fb7a252e1b59aac463ae28a66ee156d
Lonnie Liu <lonnie@anyscale.com>
2024-02-14 06:15:07 +0000

    debug min install

--- ci/ci_tags_from_change.sh
+++ ci/ci_tags_from_change.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+echo "lonnie"
+exit 0
+
 if [[ "${RAYCI_RUN_ALL_TESTS:-}" == "1" || "${BUILDKITE_BRANCH:-}" == "master" || "${BUILDKITE_BRANCH:-}" =~ ^releases/.* ]]; then
     echo '*'
     exit 0
