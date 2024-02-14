aed094f1bea2be387b62e7a22803fd6befe73302
Yicheng-Lu-llll <luyc58576@gmail.com>
2024-02-14 02:56:41 +0000

    estimated_finish_time may < current_time due to wrong estimated speed, skip this case
    Signed-off-by: Yicheng-Lu-llll <luyc58576@gmail.com>

--- python/ray/scheduler/custom_controller.py
+++ python/ray/scheduler/custom_controller.py
@@ -137,7 +137,8 @@ class Controller():
             available_memory = node[AVAILABLE_MEMORY]
 
             user_task_duration = user_task.spec[COMPLEXITY_SCORE] / node[SPEED]
-            user_task_estimated_finish_time =  int(time.time() * 1000) + user_task_duration
+            current_time = int(time.time() * 1000)
+            user_task_estimated_finish_time =  current_time + user_task_duration
             print("user_task_estimated_finish_time", node_id, user_task_estimated_finish_time)
             print("available_cpu", node_id, available_cpu)
             if available_cpu >= required_cpu and available_gpu >= required_gpu and available_memory >= required_memory:
@@ -155,6 +156,7 @@ class Controller():
                     start_time = task.status[USER_TASK_ESTIMATED_START_TIME]
 
                 estimated_finish_time = start_time + task.spec[COMPLEXITY_SCORE] / node[SPEED] + user_task_duration
+                print("start_time", start_time)
                 print("estimated_finish_time", node_id, estimated_finish_time)
 
                 available_cpu += task.spec[CPU] if CPU in task.spec else 0
@@ -162,7 +164,7 @@ class Controller():
                 available_memory += task.spec[MEMORY] if MEMORY in task.spec else 0
                 
                 if available_cpu >= required_cpu and available_gpu >= required_gpu and available_memory >= required_memory:
-                    if estimated_finish_time < earliest_time:
+                    if estimated_finish_time < earliest_time and estimated_finish_time > current_time:
                         earliest_time = estimated_finish_time
                         best_node = node_id
         
