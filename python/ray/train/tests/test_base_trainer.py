3c3e5a09f58c1437e858b7fa5c5ba91646d59562
woshiyyya <xiaoyunxuan1998@gmail.com>
2024-02-13 16:15:02 -0800

    fix 1
    Signed-off-by: woshiyyya <xiaoyunxuan1998@gmail.com>

--- python/ray/train/tests/test_base_trainer.py
+++ python/ray/train/tests/test_base_trainer.py
@@ -78,7 +78,7 @@ def test_arg_override(ray_start_4_cpus):
         assert self.custom_arg["outer"]["fixed"] == 1
 
         pg = get_current_placement_group()
-        assert len(pg.bundle_specs) == 2  # 1 trainer, 1 worker
+        assert len(pg.bundle_specs) == 1  # Merged trainer and worker
 
     scale_config = ScalingConfig(num_workers=4)
     trainer = DummyTrainer(
