b3be3a37369d83f6cd2dceb9903c3392a61c7a29
Ruiyang Wang <rywang014@gmail.com>
2024-02-14 10:57:42 -0800

    fix
    Signed-off-by: Ruiyang Wang <rywang014@gmail.com>

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -20,7 +20,7 @@ from ray._private.utils import split_address
 
 import aiosignal  # noqa: F401
 
-from ray._private.protobuf_compat import message_to_dict
+import ray._private.protobuf_compat
 from frozenlist import FrozenList  # noqa: F401
 
 from ray._private.utils import binary_to_hex, check_dashboard_dependencies_installed
@@ -217,12 +217,13 @@ def message_to_dict(message, decode_keys=None, **kwargs):
                     d[k] = v
         return d
 
+    d = ray._private.protobuf_compat.message_to_dict(
+        message, use_integers_for_enums=False, **kwargs
+    )
     if decode_keys:
-        return _decode_keys(
-            message_to_dict(message, use_integers_for_enums=False, **kwargs)
-        )
+        return _decode_keys(d)
     else:
-        return message_to_dict(message, use_integers_for_enums=False, **kwargs)
+        return d
 
 
 class SignalManager:
