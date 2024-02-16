3d411cbdcedbfc08f90179c4c4f12eaca6bf1a10
Justin Yu <justinvyu@anyscale.com>
2024-02-16 10:58:37 -0800

    pin commit
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- release/ray_release/byod/byod_xgboost_cuj_test.sh
+++ release/ray_release/byod/byod_xgboost_cuj_test.sh
@@ -4,4 +4,4 @@
 
 set -exo pipefail
 
-pip install -U "git+https://github.com/ray-project/xgboost_ray.git"
+pip install -U "git+https://github.com/ray-project/xgboost_ray@5a840af05d487171883dadbfdd37b138b607bed8#egg=xgboost_ray"
