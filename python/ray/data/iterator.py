c565d650f9f9d17587f3375bc737b81a670a193d
Justin Yu <justinvyu@anyscale.com>
2024-02-16 10:50:47 -0800

    update docstring
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/data/iterator.py
+++ python/ray/data/iterator.py
@@ -864,8 +864,9 @@ class DataIterator(abc.ABC):
     def materialize(self) -> "MaterializedDataset":
         """Execute and materialize this data iterator into object store memory.
 
-        This method will fully consume the iterator and return its contents as
-        a `MaterializedDataset` view.
+        NOTE: This method triggers the execution and materializes all blocks
+        of the iterator, returning its contents as a
+        :class:`~ray.data.dataset.MaterializedDataset` for further processing.
         """
 
         from ray.data.dataset import MaterializedDataset
