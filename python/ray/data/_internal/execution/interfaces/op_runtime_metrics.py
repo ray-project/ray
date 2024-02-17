b00f3473bc099625452d9ad408ce7cf91c2665a6
Hao Chen <chenh1024@gmail.com>
2024-02-16 17:06:03 -0800

    fix obj_store_mem_max_pending_output_per_task
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- python/ray/data/_internal/execution/interfaces/op_runtime_metrics.py
+++ python/ray/data/_internal/execution/interfaces/op_runtime_metrics.py
@@ -178,13 +178,15 @@ class OpRuntimeMetrics:
         if context._max_num_blocks_in_streaming_gen_buffer is None:
             return None
 
-        estimated_bytes_per_output = (
+        bytes_per_output = (
             self.average_bytes_per_output or context.target_max_block_size
         )
-        return (
-            estimated_bytes_per_output
-            * context._max_num_blocks_in_streaming_gen_buffer
-        )
+        num_pending_outputs = context._max_num_blocks_in_streaming_gen_buffer
+        if self.average_num_outputs_per_task is not None:
+            num_pending_outputs = min(
+                num_pending_outputs, self.average_num_outputs_per_task
+            )
+        return bytes_per_output * num_pending_outputs
 
     @property
     def obj_store_mem_pending_task_outputs(self) -> Optional[float]:
