4187c59546e053988f1c4c29614b1ba9cfeaad47
Edward Oakes <ed.nmi.oakes@gmail.com>
2024-02-21 08:35:42 -0600

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/tests/unit/test_router.py
+++ python/ray/serve/tests/unit/test_router.py
@@ -1,3 +1,4 @@
+import asyncio
 import sys
 from typing import Dict, List, Optional, Tuple, Union
 from unittest.mock import Mock
@@ -10,6 +11,7 @@ from ray.serve._private.common import (
     ReplicaQueueLengthInfo,
     RequestMetadata,
 )
+from ray.serve._private.config import DeploymentConfig
 from ray.serve._private.replica_scheduler import (
     PendingRequest,
     ReplicaScheduler,
@@ -17,6 +19,7 @@ from ray.serve._private.replica_scheduler import (
 )
 from ray.serve._private.replica_scheduler.pow_2_scheduler import ReplicaQueueLengthCache
 from ray.serve._private.router import Router
+from ray.serve.exceptions import BackPressureError
 
 
 class FakeObjectRefOrGen:
@@ -91,10 +94,14 @@ class FakeReplica(ReplicaWrapper):
 
 class FakeReplicaScheduler(ReplicaScheduler):
     def __init__(self, **kwargs):
-        self._last_pr: Optional[PendingRequest] = None
+        self._block_requests = False
+        self._blocked_requests: List[asyncio.Event] = []
         self._replica_to_return: Optional[FakeReplica] = None
         self._replica_to_return_on_retry: Optional[FakeReplica] = None
         self._replica_queue_len_cache = ReplicaQueueLengthCache()
+
+    def set_should_block_requests(self, block_requests: bool):
+        self._block_requests = block_requests
 
     @property
     def replica_queue_len_cache(self) -> ReplicaQueueLengthCache:
@@ -121,18 +128,25 @@ class FakeReplicaScheduler(ReplicaScheduler):
     def set_replica_to_return_on_retry(self, replica: FakeReplica):
         self._replica_to_return_on_retry = replica
 
+    def unblock_requests(self, num: int):
+        assert self._block_requests and len(self._blocked_requests) >= num
+        for _ in range(num):
+            self._blocked_requests.pop(0).set()
+
     async def choose_replica_for_request(
         self, pr: PendingRequest, *, is_retry: bool = False
     ) -> FakeReplica:
+        if self._block_requests:
+            event = asyncio.Event()
+            self._blocked_requests.append(event)
+            await event.wait()
+
         if is_retry:
-            assert pr == self._last_pr
             assert (
                 self._replica_to_return_on_retry is not None
             ), "Set a replica to return on retry."
             replica = self._replica_to_return_on_retry
         else:
-            assert self._last_pr is None
-            self._last_pr = pr
             assert self._replica_to_return is not None, "Set a replica to return."
             replica = self._replica_to_return
 
@@ -313,6 +327,180 @@ class TestAssignRequest:
         obj_ref = await router.assign_request(request_metadata)
         assert isinstance(obj_ref, FakeObjectRef)
         assert obj_ref.replica_id == "test-replica-1"
+
+    async def test_max_queued_requests_no_limit(
+        self, setup_router: Tuple[Router, FakeReplicaScheduler]
+    ):
+        router, fake_replica_scheduler = setup_router
+        fake_replica_scheduler.set_should_block_requests(True)
+        router.update_deployment_config(DeploymentConfig(max_queued_requests=-1))
+
+        replica = FakeReplica("test-replica-1")
+        fake_replica_scheduler.set_replica_to_return(replica)
+
+        request_metadata = RequestMetadata(
+            request_id="test-request-1",
+            endpoint="",
+        )
+
+        # Queued a bunch of tasks. None should error because there's no limit.
+        assign_request_tasks = [
+            asyncio.ensure_future(router.assign_request(request_metadata))
+            for _ in range(100)
+        ]
+
+        _, pending = await asyncio.wait(assign_request_tasks, timeout=0.01)
+        assert len(pending) == len(assign_request_tasks)
+
+        # Unblock the requests, now they should all get scheduled.
+        fake_replica_scheduler.unblock_requests(100)
+        assert all(
+            [
+                isinstance(obj_ref, FakeObjectRef)
+                and obj_ref.replica_id == "test-replica-1"
+                for obj_ref in await asyncio.gather(*assign_request_tasks)
+            ]
+        )
+
+    async def test_max_queued_requests_limited(
+        self, setup_router: Tuple[Router, FakeReplicaScheduler]
+    ):
+        router, fake_replica_scheduler = setup_router
+        fake_replica_scheduler.set_should_block_requests(True)
+        router.update_deployment_config(DeploymentConfig(max_queued_requests=5))
+
+        replica = FakeReplica("test-replica-1")
+        fake_replica_scheduler.set_replica_to_return(replica)
+
+        request_metadata = RequestMetadata(
+            request_id="test-request-1",
+            endpoint="",
+        )
+
+        # Queued `max_queued_requests` tasks. None should fail.
+        assign_request_tasks = [
+            asyncio.ensure_future(router.assign_request(request_metadata))
+            for _ in range(5)
+        ]
+
+        _, pending = await asyncio.wait(assign_request_tasks, timeout=0.01)
+        assert len(pending) == len(assign_request_tasks)
+
+        # Try to queue more tasks, they should fail immediately.
+        for _ in range(10):
+            with pytest.raises(BackPressureError):
+                await router.assign_request(request_metadata)
+
+        # Unblock a request.
+        fake_replica_scheduler.unblock_requests(1)
+        done, pending = await asyncio.wait(assign_request_tasks, timeout=0.01)
+        assert len(done) == 1
+        obj_ref = await done.pop()
+        assert isinstance(obj_ref, FakeObjectRef)
+        assert obj_ref.replica_id == "test-replica-1"
+
+        # One more task should be allowed to be queued.
+        assign_request_tasks = list(pending) + [
+            asyncio.ensure_future(router.assign_request(request_metadata))
+        ]
+
+        _, pending = await asyncio.wait(assign_request_tasks, timeout=0.01)
+        assert len(pending) == len(assign_request_tasks)
+
+        # Unblock the requests, now they should all get scheduled.
+        fake_replica_scheduler.unblock_requests(5)
+        assert all(
+            [
+                isinstance(obj_ref, FakeObjectRef)
+                and obj_ref.replica_id == "test-replica-1"
+                for obj_ref in await asyncio.gather(*assign_request_tasks)
+            ]
+        )
+
+    async def test_max_queued_requests_updated(
+        self, setup_router: Tuple[Router, FakeReplicaScheduler]
+    ):
+        router, fake_replica_scheduler = setup_router
+        fake_replica_scheduler.set_should_block_requests(True)
+        router.update_deployment_config(DeploymentConfig(max_queued_requests=5))
+
+        replica = FakeReplica("test-replica-1")
+        fake_replica_scheduler.set_replica_to_return(replica)
+
+        request_metadata = RequestMetadata(
+            request_id="test-request-1",
+            endpoint="",
+        )
+
+        # Queued `max_queued_requests` tasks. None should fail.
+        assign_request_tasks = [
+            asyncio.ensure_future(router.assign_request(request_metadata))
+            for _ in range(5)
+        ]
+
+        _, pending = await asyncio.wait(assign_request_tasks, timeout=0.01)
+        assert len(pending) == len(assign_request_tasks)
+
+        # Try to queue more tasks, they should fail immediately.
+        for _ in range(10):
+            with pytest.raises(BackPressureError):
+                await router.assign_request(request_metadata)
+
+        # Dynamically increase `max_queued_requests`, more tasks should be allowed to
+        # be queued.
+        router.update_deployment_config(DeploymentConfig(max_queued_requests=10))
+        assign_request_tasks.extend(
+            [
+                asyncio.ensure_future(router.assign_request(request_metadata))
+                for _ in range(5)
+            ]
+        )
+
+        _, pending = await asyncio.wait(assign_request_tasks, timeout=0.01)
+        assert len(pending) == len(assign_request_tasks)
+
+        # Try to queue more tasks, they should fail immediately.
+        for _ in range(10):
+            with pytest.raises(BackPressureError):
+                await router.assign_request(request_metadata)
+
+        # Dynamically decrease `max_queued_requests`, the existing tasks should remain
+        # queued but the limit should apply to new tasks.
+        router.update_deployment_config(DeploymentConfig(max_queued_requests=5))
+        _, pending = await asyncio.wait(assign_request_tasks, timeout=0.01)
+        assert len(pending) == len(assign_request_tasks)
+
+        for _ in range(10):
+            with pytest.raises(BackPressureError):
+                await router.assign_request(request_metadata)
+
+        fake_replica_scheduler.unblock_requests(5)
+        done, pending = await asyncio.wait(assign_request_tasks, timeout=0.01)
+        assert len(pending) == 5
+        assert all(
+            [
+                isinstance(obj_ref, FakeObjectRef)
+                and obj_ref.replica_id == "test-replica-1"
+                for obj_ref in await asyncio.gather(*done)
+            ]
+        )
+        assign_request_tasks = list(pending)
+
+        # Try to queue more tasks, they should fail immediately if the new limit is
+        # respected.
+        for _ in range(10):
+            with pytest.raises(BackPressureError):
+                await router.assign_request(request_metadata)
+
+        # Unblock the requests, now they should all get scheduled.
+        fake_replica_scheduler.unblock_requests(5)
+        assert all(
+            [
+                isinstance(obj_ref, FakeObjectRef)
+                and obj_ref.replica_id == "test-replica-1"
+                for obj_ref in await asyncio.gather(*assign_request_tasks)
+            ]
+        )
 
 
 if __name__ == "__main__":
