f5470ed7992f3a5032755631418753388893935a
khluu <51931015+khluu@users.noreply.github.com>
2024-02-21 00:27:37 -0800

    Split copy tag and filter func
    Signed-off-by: khluu <51931015+khluu@users.noreply.github.com>

--- ci/ray_ci/automation/test_docker_tags_lib.py
+++ ci/ray_ci/automation/test_docker_tags_lib.py
@@ -209,122 +209,6 @@ def test_delete_tag_failure_rate_limit_exceeded(mock_requests):
     }
 
 
-@mock.patch("ci.ray_ci.automation.docker_tags_lib.get_image_creation_time")
-def test_safe_to_delete_safe(mock_get_image_creation_time):
-    # Image creation time is more than 30 days ago
-    mock_get_image_creation_time.return_value = datetime.now(timezone.utc) - timedelta(
-        days=60
-    )
-    registry_tags = ["test_tag1", "test_tag2"]
-    is_safe = _safe_to_delete("test_tag1", 30, registry_tags)
-
-    mock_get_image_creation_time.assert_called_once_with(tag="test_tag1")
-    assert is_safe is True
-
-
-@mock.patch("ci.ray_ci.automation.docker_tags_lib.get_image_creation_time")
-def test_safe_to_delete_not_safe(mock_get_image_creation_time):
-    # Image creation time is less than 30 days ago
-    mock_get_image_creation_time.return_value = datetime.now(timezone.utc) - timedelta(
-        days=15
-    )
-    registry_tags = ["test_tag1", "test_tag2"]
-    is_safe = _safe_to_delete("test_tag1", 30, registry_tags)
-
-    mock_get_image_creation_time.assert_called_once_with(tag="test_tag1")
-    assert is_safe is False
-
-
-@mock.patch("ci.ray_ci.automation.docker_tags_lib.get_image_creation_time")
-def test_safe_to_delete_no_image_config(mock_get_image_creation_time):
-    # Image config is not found
-    # Tag is not in the registry
-    mock_get_image_creation_time.side_effect = RetrieveImageConfigException
-    registry_tags = ["test_tag1", "test_tag2"]
-    is_safe = _safe_to_delete("test_tag3", 30, registry_tags)
-
-    mock_get_image_creation_time.assert_called_once_with(tag="test_tag3")
-    assert is_safe is True
-
-
-@mock.patch("ci.ray_ci.automation.docker_tags_lib.get_image_creation_time")
-def test_safe_to_delete_no_image_config_tag_in_registry(mock_get_image_creation_time):
-    # Image config is not found
-    # Tag is in the registry
-    mock_get_image_creation_time.side_effect = RetrieveImageConfigException
-    registry_tags = ["test_tag1", "test_tag2"]
-    is_safe = _safe_to_delete("test_tag1", 30, registry_tags)
-
-    mock_get_image_creation_time.assert_called_once_with(tag="test_tag1")
-    assert is_safe is False
-
-
-@pytest.mark.parametrize(
-    ("tag", "recent_commit_shas", "expected_value"),
-    [
-        ("a1s2d3", {}, True),
-        ("a1s2d3-py38-cu123", {}, True),
-        ("2.7.0.a1s2d3-py37", {}, True),
-        ("2.7.0rc0.a1s2d3-py37", {}, True),
-        ("2.7.0.post1.a1s2d3-py37", {}, True),
-        ("a1s2d3", {"a1s2d3"}, False),
-        ("2.7.0rc0.a1s2d3-py37", {"a1s2d3"}, False),
-        ("2.7.0.a1s2d3-py37", {"q2w3e4"}, True),
-    ],
-)
-def test_is_old_commit_tag(tag, recent_commit_shas, expected_value):
-    assert _is_old_commit_tag(tag, recent_commit_shas) == expected_value
-
-
-@pytest.mark.parametrize(
-    ("tag", "release_versions", "expected_value"),
-    [
-        ("2.0.0", ["2.0.0"], True),
-        ("2.0.0rc0", ["2.0.0rc0"], True),
-        ("2.0.0-py38", ["2.0.0"], True),
-        ("2.0.0-py38-cu123", ["2.0.0"], True),
-        ("2.0.0.post1", ["2.0.0.post1"], True),
-        ("2.0.0.1", ["2.0.0"], False),
-        ("2.0.0.1r", ["2.0.0"], False),
-        ("a.1.c", ["2.0.0"], False),
-        ("1.a.b", ["2.0.0"], False),
-        ("2.0.0rc0", ["2.0.0"], False),
-        ("2.0.0", ["2.0.0rc0"], False),
-        ("2.0.0.a1s2d3", ["2.0.0"], False),
-        ("2.0.0.a1s2d3-py38-cu123", ["2.0.0"], False),
-        ("2.0.0", None, True),
-    ],
-)
-def test_is_release_tag(tag, release_versions, expected_value):
-    assert _is_release_tag(tag, release_versions) == expected_value
-
-
-@mock.patch("ci.ray_ci.automation.docker_tags_lib._call_crane_cp")
-def test_copy_tag_to_aws_ecr(mock_call_crane_cp):
-    tag = "test_namespace/test_repository:test_tag"
-    mock_call_crane_cp.return_value = (
-        "aws-ecr/name/repo:test_tag: digest: sha256:sample-sha256 size: 1788"
-    )
-
-    is_copied = copy_tag_to_aws_ecr(tag, "aws-ecr/name/repo")
-    mock_call_crane_cp.assert_called_once_with(
-        tag="test_tag", source=tag, aws_ecr_repo="aws-ecr/name/repo"
-    )
-    assert is_copied is True
-
-
-@mock.patch("ci.ray_ci.automation.docker_tags_lib._call_crane_cp")
-def test_copy_tag_to_aws_ecr_failure(mock_call_crane_cp):
-    tag = "test_namespace/test_repository:test_tag"
-    mock_call_crane_cp.return_value = "Error: Failed to copy tag."
-
-    is_copied = copy_tag_to_aws_ecr(tag, "aws-ecr/name/repo")
-    mock_call_crane_cp.assert_called_once_with(
-        tag="test_tag", source=tag, aws_ecr_repo="aws-ecr/name/repo"
-    )
-    assert is_copied is False
-
-
 def _make_docker_hub_response(
     tag: str, page_count: int, namespace: str, repository: str, page_limit: int
 ):
@@ -347,9 +231,9 @@ def _make_docker_hub_response(
     ("filter_func", "tags", "expected_tags"),
     [
         (
-            lambda t: _is_old_commit_tag(t, {}),
+            lambda t: t.startswith("a1s3d3"),
             ["a1s2d3-py38", "2.7.0.q2w3e4", "t5y678-py39-cu123"],
-            ["a1s2d3-py38", "2.7.0.q2w3e4", "t5y678-py39-cu123"],
+            ["a1s2d3-py38"],
         ),
         (
             lambda t: t.startswith("2.7.0"),
@@ -383,9 +267,9 @@ def test_query_tags_from_docker_hub(mock_requests, filter_func, tags, expected_t
     ("filter_func", "tags", "expected_tags"),
     [
         (
-            lambda t: _is_old_commit_tag(t, {}),
+            lambda t: t.startswith("a1s3d3"),
             ["a1s2d3-py38", "2.7.0.q2w3e4", "t5y678-py39-cu123"],
-            ["a1s2d3-py38", "2.7.0.q2w3e4", "t5y678-py39-cu123"],
+            ["a1s2d3-py38"],
         ),
     ],
 )
