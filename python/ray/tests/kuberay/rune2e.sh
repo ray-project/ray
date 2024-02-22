1e4f23e16a3a25e3dd5de283eed4e337e4bc95a6
rickyyx <rickyx@anyscale.com>
2024-02-22 07:26:39 +0000

    staging
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- python/ray/tests/kuberay/rune2e.sh
+++ python/ray/tests/kuberay/rune2e.sh
@@ -12,4 +12,4 @@ popd || exit
 kind create cluster || exit
 kind load docker-image $RAY_IMAGE || exit
 python setup/setup_kuberay.py
-RAY_IMAGE=$RAY_IMAGE python test_autoscaling_e2e.py
+RAY_IMAGE=$RAY_IMAGE pytest -vvs test_autoscaling_e2e.py
