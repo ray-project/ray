7f25fe893fdb3d15c737416e32391ee43deb4d09
Lonnie Liu <lonnie@anyscale.com>
2024-02-26 22:21:05 +0000

    .

--- .buildkite/release-automation/verify-windows-wheels.sh
+++ .buildkite/release-automation/verify-windows-wheels.sh
@@ -5,17 +5,17 @@ set -euo pipefail
 set -x
 
 PYTHON_VERSIONS=("3.8" "3.9" "3.10" "3.11")
-export RAY_VERSION="${RAY_VERSION:-2.9.2}"
-export RAY_HASH="${RAY_HASH:-fce7a361807580953364e2da964f9498f3123bf9}"
+export RAY_VERSION="${RAY_VERSION:-2.9.3}"
+export RAY_HASH="${RAY_HASH:-62655e11ed76509b78654b60be67bc59f8f3460a}"
 
 run_sanity_check() {
-    local python_version="$1"
-    conda create -n "rayio_${python_version}" python="${python_version}" -y
-    conda activate "rayio_${python_version}"
+    local PYTHON_VERSION="$1"
+    conda create -n "rayio_${PYTHON_VERSION}" python="${PYTHON_VERSION}" -y
+    conda activate "rayio_${PYTHON_VERSION}"
     pip install \
         --index-url https://test.pypi.org/simple/ \
         --extra-index-url https://pypi.org/simple \
-        "ray[cpp]==$RAY_VERSION"
+        "ray[cpp]==${RAY_VERSION}"
     (
         cd release/util
         which python
@@ -23,12 +23,12 @@ run_sanity_check() {
         python sanity_check.py
     )
     conda deactivate
-    conda env remove -n "rayio_${python_version}" -y
+    conda env remove -n "rayio_${PYTHON_VERSION}" -y
 }
 
 source /c/Miniconda3/etc/profile.d/conda.sh
 
 # Install Ray & run sanity checks for each python version
-for python_version in "${PYTHON_VERSIONS[@]}"; do
-    run_sanity_check "$python_version"
+for PYTHON_VERSION in "${PYTHON_VERSIONS[@]}"; do
+    run_sanity_check "${PYTHON_VERSION}"
 done
