eaba46737fe2f7050ad5b32c1246af396eea6b5c
khluu <51931015+khluu@users.noreply.github.com>
2024-02-27 11:50:41 -0800

    use branch runner queue
    Signed-off-by: khluu <51931015+khluu@users.noreply.github.com>

--- .buildkite/release-automation/check_release_blockers.sh
+++ .buildkite/release-automation/check_release_blockers.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 set -euo pipefail
-check_release_blockers_count() {
-    NUM_RELEASE_BLOCKERS=$(bazel run //ci/ray_ci/automation:weekly_green_metric)
-    if [ "$NUM_RELEASE_BLOCKERS" -gt 0 ]; then
-        echo "There are $NUM_RELEASE_BLOCKERS release blockers. Please fix them before releasing."
-        exit 1
-    fi
-}
-
-check_release_blockers_count
+NUM_RELEASE_BLOCKERS=$(bazel run //ci/ray_ci/automation:weekly_green_metric)
+if [ "$NUM_RELEASE_BLOCKERS" -gt 0 ]; then
+    echo "There are $NUM_RELEASE_BLOCKERS release blockers. Please fix them before releasing."
+    exit 1
+fi
