9ea60374c9ee9ed08ecc4d7dd0e8119187ecee2e
sven1977 <svenmika1977@gmail.com>
2024-02-28 19:10:23 +0100

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/examples/export/onnx_torch.py
+++ rllib/examples/export/onnx_torch.py
@@ -7,67 +7,102 @@ import os
 import shutil
 import torch
 
+from ray.rllib.utils.torch_utils import convert_to_torch_tensor
+
+
+class ONNXCompatibleWrapper(torch.nn.Module):
+    def __init__(self, original_model):
+        super(ONNXCompatibleWrapper, self).__init__()
+        self.original_model = original_model
+
+    def forward(self, a, b0, b1, c):
+        # Convert the separate tensor inputs back into the list format
+        # expected by the original model's forward method.
+        b = [b0, b1]
+        ret = self.original_model({"obs": a}, b, c)
+        # results, state_out_0, state_out_1
+        return ret[0], ret[1][0], ret[1][1]
+
+
 if __name__ == "__main__":
     # Configure our PPO Algorithm.
     config = (
         ppo.PPOConfig()
         # ONNX is not supported by RLModule API yet.
         .experimental(_enable_new_api_stack=False)
+        .environment("CartPole-v1")
         .rollouts(num_rollout_workers=1)
         .framework("torch")
+        .training(model={"use_lstm": True})
     )
 
-    outdir = "export_torch"
-    if os.path.exists(outdir):
-        shutil.rmtree(outdir)
+    B = 3
+    T = 5
+    LSTM_CELL = 256
 
-    np.random.seed(1234)
-
-    # We will run inference with this test batch
-    test_data = {
-        "obs": np.random.uniform(0, 1.0, size=(10, 4)).astype(np.float32),
-        "state_ins": np.array([0.0], dtype=np.float32),
+    # Input data for a python inference forward call.
+    test_data_python = {
+        "obs": np.random.uniform(0, 1.0, size=(B*T, 4)).astype(np.float32),
+        "state_ins": [
+            np.random.uniform(0, 1.0, size=(B, LSTM_CELL)).astype(np.float32),
+            np.random.uniform(0, 1.0, size=(B, LSTM_CELL)).astype(np.float32)
+        ],
+        "seq_lens": np.array([T] * B, np.float32),
     }
+    # Input data for the ONNX session.
+    test_data_onnx = {
+        "obs": test_data_python["obs"],
+        "state_in_0": test_data_python["state_ins"][0],
+        "state_in_1": test_data_python["state_ins"][1],
+        "seq_lens": test_data_python["seq_lens"],
+    }
+    # Input data for compiling the ONNX model.
+    test_data_onnx_input = convert_to_torch_tensor(test_data_onnx)
 
     # Start Ray and initialize a PPO Algorithm.
     ray.init()
-    algo = config.build(env="CartPole-v1")
-
-    # You could train the model here
-    # algo.train()
+    algo = config.build()
 
     # Let's run inference on the torch model
     policy = algo.get_policy()
     result_pytorch, _ = policy.model(
         {
-            "obs": torch.tensor(test_data["obs"]),
-        }
+            "obs": torch.tensor(test_data_python["obs"]),
+        },
+        [
+            torch.tensor(test_data_python["state_ins"][0]),
+            torch.tensor(test_data_python["state_ins"][1]),
+        ],
+        torch.tensor(test_data_python["seq_lens"]),
     )
 
     # Evaluate tensor to fetch numpy array
     result_pytorch = result_pytorch.detach().numpy()
 
     # This line will export the model to ONNX.
-    policy.export_model(outdir, onnx=11)
-    # Equivalent to:
-    # algo.export_policy_model(outdir, onnx=11)
-
-    # Import ONNX model.
-    exported_model_file = os.path.join(outdir, "model.onnx")
-
+    onnx_compatible = ONNXCompatibleWrapper(policy.model)
+    exported_model_file = "model.onnx"
+    torch.onnx.export(
+        onnx_compatible,
+        (test_data_onnx_input["obs"], test_data_onnx_input["state_in_0"], test_data_onnx_input["state_in_1"], test_data_onnx_input["seq_lens"]),
+        exported_model_file,
+        export_params=True,
+        opset_version=11,
+        do_constant_folding=True,
+        input_names=["obs", "state_in_0", "state_in_1", "seq_lens"],
+        output_names=["output", "state_out_0", "state_out_1"],
+        dynamic_axes={
+            k: {0: "batch_size"}
+            for k in ["obs", "state_in_0", "state_in_1", "seq_lens"]
+        },
+    )
     # Start an inference session for the ONNX model
     session = onnxruntime.InferenceSession(exported_model_file, None)
-
-    # Pass the same test batch to the ONNX model
-    if Version(torch.__version__) < Version("1.9.0"):
-        # In torch < 1.9.0 the second input/output name gets mixed up
-        test_data["state_outs"] = test_data.pop("state_ins")
-
-    result_onnx = session.run(["output"], test_data)
+    result_onnx = session.run(["output"], test_data_onnx)
 
     # These results should be equal!
     print("PYTORCH", result_pytorch)
-    print("ONNX", result_onnx)
+    print("ONNX", result_onnx[0])
 
     assert np.allclose(
         result_pytorch, result_onnx
