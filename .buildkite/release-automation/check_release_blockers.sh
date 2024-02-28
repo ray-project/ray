53c98cc6c01b3813f4f5219f703d2361f368d47d
khluu <51931015+khluu@users.noreply.github.com>
2024-02-27 16:09:55 -0800

    dont fail release blocker check
    Signed-off-by: khluu <51931015+khluu@users.noreply.github.com>

--- .buildkite/release-automation/check_release_blockers.sh
+++ .buildkite/release-automation/check_release_blockers.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 NUM_RELEASE_BLOCKERS=$(bazel run //ci/ray_ci/automation:weekly_green_metric)
 if [ "$NUM_RELEASE_BLOCKERS" -gt 0 ]; then
     echo "There are $NUM_RELEASE_BLOCKERS release blockers. Please fix them before releasing."
-    exit 1
 fi
