ray-project/ray

commit d6380d441dcd134c1ffd626ac621300438eca317

[data] Enable per-op resource reservation (#43171)
This PR enables per-op resource reservation by default. See docstring of `ReservationOpResourceAllocator` for the concrete protocol. 
Besides, this PR also:
* Increases the default object store memory limit from 25% to 50%, because now we have more precise control of memory. 
* Renames `OpResourceLimiter` to `OpResourceAllocator`, and adds 2 new APIs.
* Implements a bunch of perf optimizations for different scenarios in `ReservationOpResourceAllocator`. 
* Removes `StreamingOutputBackpressurePolicy`. as this feature is now implemented as `ReservationOpResourceAllocator.max_task_output_bytes_to_read`. 
* Removes `use_runtime_metrics_scheduling`.
* Fixes a bug and adds util functions in `ExecutionResources`. 
* Adds detailed resource usage in progress bars. 

---------

Signed-off-by: Hao Chen <chenh1024@gmail.com>
Signed-off-by: Balaji Veeramani <balaji@anyscale.com>
Co-authored-by: Balaji Veeramani <balaji@anyscale.com>

python/ray/data/BUILD
@@ -451,14 +451,6 @@ py_test(
 )
 
 py_test(
-    name = "test_runtime_metrics_scheduling",
-    size = "small",
-    srcs = ["tests/test_runtime_metrics_scheduling.py"],
-    tags = ["team:data", "exclusive"],
-    deps = ["//:ray_lib", ":conftest"],
-)
-
-py_test(
     name = "test_size_estimation",
     size = "medium",
     srcs = ["tests/test_size_estimation.py"],
@@ -523,14 +515,6 @@ py_test(
 )
 
 py_test(
-    name = "test_streaming_backpressure_edge_case",
-    size = "medium",
-    srcs = ["tests/test_streaming_backpressure_edge_case.py"],
-    tags = ["team:data", "exclusive"],
-    deps = ["//:ray_lib", ":conftest"],
-)
-
-py_test(
     name = "test_transform_pyarrow",
     size = "small",
     srcs = ["tests/test_transform_pyarrow.py"],
@@ -558,6 +542,14 @@ py_test(
     name = "test_backpressure_policies",
     size = "medium",
     srcs = ["tests/test_backpressure_policies.py"],
+    tags = ["team:data", "exclusive"],
+    deps = ["//:ray_lib", ":conftest"],
+)
+
+py_test(
+    name = "test_backpressure_e2e",
+    size = "medium",
+    srcs = ["tests/test_backpressure_e2e.py"],
     tags = ["team:data", "exclusive"],
     deps = ["//:ray_lib", ":conftest"],
 )

commit c61bbc0f944c4c64549d1b639243274d2581192b

[data] Fix bug in memory reservation (#43511)
Fix bugs in ReservationOpResourceAllocator (introduced by https://github.com/ray-project/ray/pull/43171)
* We treat a map op and its following non-map ops as the same group. `update_resources` already handles this properly . But `_should_unblock_streaming_output_backpressure` and ` _op_outputs_reserved_remaining` didn't consider this.
* Since we don't reserve any resources for `limit` and `streaming_split`, should set `num_cpus=0` for their tasks. 
* `_reserved_for_op_outputs` currently also includes op's internal output buffers. This is incorrect, because when `preserve_order=True`, task outputs will accumulate in op's internal output buffer, and use all the memory budget from `_reserved_for_op_outputs`. Then we still don't have memory budget to pull the blocks from the internal output buffer. Excluding the internal output buffer from `_reserved_for_op_outputs` fixes this issue.

Also deflake `test_backpressure_from_output` and `test_e2e_autoscaling_up`, as they depend on physical memory size of the node. 

Signed-off-by: Hao Chen <chenh1024@gmail.com>

python/ray/data/BUILD
@@ -548,7 +548,7 @@ py_test(
 
 py_test(
     name = "test_backpressure_e2e",
-    size = "medium",
+    size = "large",
     srcs = ["tests/test_backpressure_e2e.py"],
     tags = ["team:data", "exclusive"],
     deps = ["//:ray_lib", ":conftest"],

python/ray/data/_internal/execution/operators/limit_operator.py
@@ -58,7 +58,9 @@ class LimitOperator(OneToOneOperator):
                     metadata.size_bytes = BlockAccessor.for_block(block).size_bytes()
                     return block, metadata
 
-                block, metadata_ref = cached_remote_fn(slice_fn, num_returns=2).remote(
+                block, metadata_ref = cached_remote_fn(
+                    slice_fn, num_cpus=0, num_returns=2
+                ).remote(
                     block,
                     metadata,
                     self._limit - self._consumed_rows,

python/ray/data/_internal/execution/operators/output_splitter.py
@@ -287,7 +287,9 @@ def _split_block(
     b: ObjectRef[Block], left_size: int
 ) -> (ObjectRef[Block], ObjectRef[Block]):
     split_single_block = cached_remote_fn(_split_single_block)
-    left, right = split_single_block.options(num_returns=2).remote(b, left_size)
+    left, right = split_single_block.options(num_cpus=0, num_returns=2).remote(
+        b, left_size
+    )
     return left, right
 
 

python/ray/data/_internal/execution/resource_manager.py
@@ -27,6 +27,7 @@ if TYPE_CHECKING:
 
 
 logger = DatasetLogger(__name__)
+DEBUG_RESOURCE_MANAGER = os.environ.get("RAY_DATA_DEBUG_RESOURCE_MANAGER", "0") == "1"
 
 
 class ResourceManager:
@@ -55,13 +56,15 @@ class ResourceManager:
         self._global_limits_last_update_time = 0
         self._global_usage = ExecutionResources.zero()
         self._op_usages: Dict[PhysicalOperator, ExecutionResources] = {}
-        # Object store memory usage of the pending task outputs.
-        self._mem_pending_task_outputs: Dict[PhysicalOperator, int] = defaultdict(int)
-        # Object store memory usage of the internal/external output buffers, and
-        # the output buffers of the output dependency operators.
+        # Object store memory usage internal to the operator, including the
+        # pending task outputs and op's internal output buffers.
+        self._mem_op_internal: Dict[PhysicalOperator, int] = defaultdict(int)
+        # Object store memory usage of the blocks that have been taken out of
+        # the operator, including the external output buffer in OpState, and the
+        # input buffers of the downstream operators.
         self._mem_op_outputs: Dict[PhysicalOperator, int] = defaultdict(int)
         # Whether to print debug information.
-        self._debug = os.environ.get("RAY_DATA_DEBUG_RESOURCE_MANAGER", "0") == "1"
+        self._debug = DEBUG_RESOURCE_MANAGER
 
         self._downstream_fraction: Dict[PhysicalOperator, float] = {}
         self._downstream_object_store_memory: Dict[PhysicalOperator, int] = {}
@@ -86,22 +89,23 @@ class ResourceManager:
         if isinstance(op, InputDataBuffer):
             return 0
 
-        pending_task_outputs = op.metrics.obj_store_mem_pending_task_outputs or 0
+        # Pending task outputs.
+        mem_op_internal = op.metrics.obj_store_mem_pending_task_outputs or 0
+        # Op's internal output buffers.
+        mem_op_internal += op.metrics.obj_store_mem_internal_outqueue
 
-        output_buffers = op.metrics.obj_store_mem_internal_outqueue
-        output_buffers += state.outqueue_memory_usage()
-
-        next_op_input_buffers = 0
+        # Op's external output buffer.
+        mem_op_outputs = state.outqueue_memory_usage()
         for next_op in op.output_dependencies:
-            next_op_input_buffers += (
+            mem_op_outputs += (
                 next_op.metrics.obj_store_mem_internal_inqueue
                 + next_op.metrics.obj_store_mem_pending_task_inputs
             )
 
-        self._mem_pending_task_outputs[op] = pending_task_outputs
-        self._mem_op_outputs[op] = output_buffers + next_op_input_buffers
+        self._mem_op_internal[op] = mem_op_internal
+        self._mem_op_outputs[op] = mem_op_outputs
 
-        return pending_task_outputs + output_buffers + next_op_input_buffers
+        return mem_op_internal + mem_op_outputs
 
     def update_usages(self):
         """Recalculate resource usages."""
@@ -191,14 +195,17 @@ class ResourceManager:
         usage_str += f", objects: {self._op_usages[op].object_store_memory_str()}"
         if self._debug:
             usage_str += (
-                f" (task_pending: {memory_string(self._mem_pending_task_outputs[op])}, "  # noqa
-                f"op_outputs: {memory_string(self._mem_op_outputs[op])})"
+                f" (in={memory_string(self._mem_op_internal[op])},"
+                f"out={memory_string(self._mem_op_outputs[op])})"
             )
             if (
-                self.op_resource_allocator_enabled()
+                isinstance(self._op_resource_allocator, ReservationOpResourceAllocator)
                 and op in self._op_resource_allocator._op_budgets
             ):
-                usage_str += f", budget: {self._op_resource_allocator._op_budgets[op]}"
+                budget = self._op_resource_allocator._op_budgets[op]
+                usage_str += f", budget=(cpu={budget.cpu:.1f}"
+                usage_str += f",gpu={budget.gpu:.1f}"
+                usage_str += f",objects={budget.object_store_memory_str()})"
         return usage_str
 
     def get_downstream_fraction(self, op: PhysicalOperator) -> float:
@@ -255,7 +262,10 @@ class ReservationOpResourceAllocator(OpResourceAllocator):
     resource usages to limit the resources that each operator can use.
 
     It works in the following way:
-    1. Currently we only limit map operators. Non-map operators get unlimited resources.
+    1. Currently we only limit map operators. Non-map operators are not throttled, but
+       their usage will be accounted for their upstream map operators. E.g., for such
+       a dataset "map1->limit->map2->streaming_split", we'll treat "map1->limit" as
+       a group and "map2->streaming_split" as another group.
     2. For each map operator, we reserve `reservation_ratio * global_resources /
         num_map_ops` resources, half of which is reserved only for the operator outputs,
         excluding pending task outputs.
@@ -339,12 +349,13 @@ class ReservationOpResourceAllocator(OpResourceAllocator):
         # Per-op reserved resources, excluding `_reserved_for_op_outputs`.
         self._op_reserved: Dict[PhysicalOperator, ExecutionResources] = {}
         # Memory reserved exclusively for the outputs of each operator.
-        # "Op outputs" refer to the operator's internal and external output buffers
-        # and next operator's input buffers, but not including the pending task outputs.
+        # "Op outputs" refer to blocks that have been taken out of an operator,
+        # i.e., `RessourceManager._mem_op_outputs`.
         #
         # Note, if we don't reserve memory for op outputs, all the budget may be used by
-        # by the pending task outputs. Then we'll have no budget to pull the outputs
-        # from the running tasks.
+        # the pending task outputs, and/or op' internal output buffers (the latter can
+        # happen when `preserve_order=True`.
+        # Then we'll have no budget to pull blocks from the op.
         self._reserved_for_op_outputs: Dict[PhysicalOperator, int] = {}
         # Total shared resources.
         self._total_shared = ExecutionResources.zero()
@@ -397,8 +408,8 @@ class ReservationOpResourceAllocator(OpResourceAllocator):
         )
         for op in eligible_ops:
             # Reserve at least half of the default reserved resources for the outputs.
-            # This makes sure that we will have enough budget to pull the outputs from
-            # the running tasks.
+            # This makes sure that we will have enough budget to pull blocks from the
+            # op.
             self._reserved_for_op_outputs[op] = max(
                 default_reserved.object_store_memory // 2, 1
             )
@@ -462,7 +473,7 @@ class ReservationOpResourceAllocator(OpResourceAllocator):
         # launch tasks. Then we should temporarily unblock the streaming output
         # backpressure by allowing reading at least 1 block. So the current operator
         # can finish at least one task and yield resources to the downstream operators.
-        for next_op in op.output_dependencies:
+        for next_op in self._get_downstream_map_ops(op):
             if not self._reserved_min_resources[next_op]:
                 # Case 1: the downstream operator hasn't reserved the minimum resources
                 # to run at least one task.
@@ -476,30 +487,52 @@ class ReservationOpResourceAllocator(OpResourceAllocator):
         return False
 
     def _op_outputs_reserved_remaining(self, op: PhysicalOperator) -> int:
+        # Outputs usage of the current operator.
         outputs_usage = self._resource_manager._mem_op_outputs[op]
+        # Also account the downstream non-Map operators' memory usage.
+        outputs_usage += sum(
+            self._resource_manager.get_op_usage(next_op).object_store_memory
+            for next_op in self._get_downstream_non_map_ops(op)
+        )
         return max(self._reserved_for_op_outputs[op] - outputs_usage, 0)
 
     def max_task_output_bytes_to_read(self, op: PhysicalOperator) -> Optional[int]:
         if op not in self._op_budgets:
             return None
-        res = self._op_budgets[
-            op
-        ].object_store_memory + self._op_outputs_reserved_remaining(op)
+        res = self._op_budgets[op].object_store_memory
+        res += self._op_outputs_reserved_remaining(op)
         assert res >= 0
         if res == 0 and self._should_unblock_streaming_output_backpressure(op):
             res = 1
         return res
 
-    def _get_downstream_non_map_op_memory_usage(self, op: PhysicalOperator) -> int:
-        """Get the total memory usage of the downstream non-Map operators."""
-        usage = 0
+    def _get_downstream_non_map_ops(
+        self, op: PhysicalOperator
+    ) -> List[PhysicalOperator]:
+        """Get the downstream non-Map operators of the given operator.
+
+        E.g.,
+          - "cur_map->downstream_map" will return an empty list.
+          - "cur_map->limit1->limit2->downstream_map" will return [limit1, limit2].
+        """
         for next_op in op.output_dependencies:
             if not isinstance(next_op, MapOperator):
-                usage += self._resource_manager.get_op_usage(
-                    next_op
-                ).object_store_memory
-                usage += self._get_downstream_non_map_op_memory_usage(next_op)
-        return usage
+                yield next_op
+                yield from self._get_downstream_non_map_ops(next_op)
+
+    def _get_downstream_map_ops(self, op: PhysicalOperator) -> List[PhysicalOperator]:
+        """Get the downstream Map operators of the given operator, ignoring intermediate
+        non-Map operators.
+
+        E.g.,
+          - "cur_map->downstream_map" will return [downstream_map].
+          - "cur_map->limit1->limit2->downstream_map" will return [downstream_map].
+        """
+        for next_op in op.output_dependencies:
+            if isinstance(next_op, MapOperator):
+                yield next_op
+            else:
+                yield from self._get_downstream_map_ops(next_op)
 
     def update_usages(self):
         self._update_reservation()
@@ -516,7 +549,7 @@ class ReservationOpResourceAllocator(OpResourceAllocator):
             op_mem_usage = 0
             # Add the memory usage of the operator itself,
             # excluding `_reserved_for_op_outputs`.
-            op_mem_usage += self._resource_manager._mem_pending_task_outputs[op]
+            op_mem_usage += self._resource_manager._mem_op_internal[op]
             op_mem_usage += max(
                 self._resource_manager._mem_op_outputs[op]
                 - self._reserved_for_op_outputs[op],
@@ -527,7 +560,10 @@ class ReservationOpResourceAllocator(OpResourceAllocator):
             # This is because we don't directly throttle non-Map operators.
             # So if they are using too much memory, we should throttle their
             # upstream Map operator.
-            op_mem_usage += self._get_downstream_non_map_op_memory_usage(op)
+            op_mem_usage += sum(
+                self._resource_manager.get_op_usage(next_op).object_store_memory
+                for next_op in self._get_downstream_non_map_ops(op)
+            )
             op_usage = copy.deepcopy(self._resource_manager.get_op_usage(op))
             op_usage.object_store_memory = op_mem_usage
             op_reserved = self._op_reserved[op]

python/ray/data/tests/test_backpressure_e2e.py
@@ -81,6 +81,7 @@ def _build_dataset(
     consumer_num_cpus,
     num_blocks,
     block_size,
+    insert_limit_op=False,
 ):
     # Create a dataset with 2 operators:
     # - The producer op has only 1 task, which produces `num_blocks` blocks, each
@@ -107,7 +108,13 @@ def _build_dataset(
 
     ds = ray.data.range(1, parallelism=1).materialize()
     ds = ds.map_batches(producer, batch_size=None, num_cpus=producer_num_cpus)
+    # Add a limit op in the middle, to test that ReservationOpResourceAllocator
+    # will account limit op's resource usage to the previous producer map op.
+    if insert_limit_op:
+        ds = ds.limit(num_blocks)
     ds = ds.map_batches(consumer, batch_size=None, num_cpus=consumer_num_cpus)
+    if insert_limit_op:
+        ds = ds.limit(num_blocks)
     return ds
 
 
@@ -119,9 +126,11 @@ def _build_dataset(
         (3, 100),  # Both not enough
     ],
 )
+@pytest.mark.parametrize("insert_limit_op", [False, True])
 def test_no_deadlock_on_small_cluster_resources(
     cluster_cpus,
     cluster_obj_store_mem_mb,
+    insert_limit_op,
     shutdown_only,  # noqa: F811
     restore_data_context,  # noqa: F811
 ):
@@ -138,12 +147,14 @@ def test_no_deadlock_on_small_cluster_resources(
         consumer_num_cpus=1,
         num_blocks=num_blocks,
         block_size=block_size,
+        insert_limit_op=insert_limit_op,
     )
     assert len(ds.take_all()) == num_blocks
 
 
+@pytest.mark.parametrize("insert_limit_op", [False, True])
 def test_no_deadlock_on_resource_contention(
-    shutdown_only, restore_data_context  # noqa: F811
+    insert_limit_op, shutdown_only, restore_data_context  # noqa: F811
 ):
     """Test when resources are preempted by non-Data code, the execution can
     still proceed without deadlock."""
@@ -170,7 +181,37 @@ def test_no_deadlock_on_resource_contention(
         consumer_num_cpus=0.9,
         num_blocks=num_blocks,
         block_size=block_size,
+        insert_limit_op=insert_limit_op,
     )
+    assert len(ds.take_all()) == num_blocks
+
+
+def test_no_deadlock_with_preserve_order(
+    restore_data_context, shutdown_only  # noqa: F811
+):
+    """Test backpressure won't cause deadlocks when `preserve_order=True`."""
+    num_blocks = 20
+    block_size = 10 * 1024 * 1024
+    ray.init(num_cpus=num_blocks)
+    data_context = ray.data.DataContext.get_current()
+    data_context.target_max_block_size = block_size
+    data_context._max_num_blocks_in_streaming_gen_buffer = 1
+    data_context.execution_options.preserve_order = True
+    data_context.execution_options.resource_limits.object_store_memory = 5 * block_size
+
+    # Some tasks are slower than others.
+    # The faster tasks will finish first and occupy Map op's internal output buffer.
+    # Test that we won't backpressure the operator in this case.
+    def map_fn(batch):
+        idx = batch["id"][0]
+        print("map_fn", idx, time.time())
+        if idx % 2 == 0:
+            time.sleep(3)
+        batch["data"] = [np.zeros(block_size, dtype=np.uint8)]
+        return batch
+
+    ds = ray.data.range(num_blocks, parallelism=num_blocks)
+    ds = ds.map_batches(map_fn, batch_size=None, num_cpus=1)
     assert len(ds.take_all()) == num_blocks
 
 

python/ray/data/tests/test_streaming_integration.py
@@ -4,6 +4,7 @@ import threading
 import time
 from typing import Any, List
 
+import numpy as np
 import pandas as pd
 import pytest
 
@@ -420,6 +421,7 @@ def test_scheduling_progress_when_output_blocked(
 def test_backpressure_from_output(ray_start_10_cpus_shared, restore_data_context):
     # Here we set the memory limit low enough so the output getting blocked will
     # actually stall execution.
+    block_size = 10 * 1024 * 1024
 
     @ray.remote
     class Counter:
@@ -436,14 +438,17 @@ def test_backpressure_from_output(ray_start_10_cpus_shared, restore_data_context
 
     def func(x):
         ray.get(counter.inc.remote())
-        return x
+        return {
+            "data": [np.ones(block_size, dtype=np.uint8)],
+        }
 
     ctx = DataContext.get_current()
-    ctx.execution_options.resource_limits.object_store_memory = 10000
+    ctx.target_max_block_size = block_size
+    ctx.execution_options.resource_limits.object_store_memory = block_size
 
     # Only take the first item from the iterator.
-    ds = ray.data.range(100000, parallelism=100).map_batches(func, batch_size=None)
-    it = iter(ds.iter_batches(batch_size=None))
+    ds = ray.data.range(100, parallelism=100).map_batches(func, batch_size=None)
+    it = iter(ds.iter_batches(batch_size=None, prefetch_batches=0))
     next(it)
     time.sleep(3)  # Pause a little so anything that would be executed runs.
     num_finished = ray.get(counter.get.remote())
@@ -475,6 +480,10 @@ def test_e2e_liveness_with_output_backpressure_edge_case(
 
 
 def test_e2e_autoscaling_up(ray_start_10_cpus_shared, restore_data_context):
+    ctx = ray.data.DataContext.get_current()
+    ctx.execution_options.resource_limits.object_store_memory = 100 * 1024**2
+    ctx.target_max_block_size = 1 * 1024**2
+
     @ray.remote(max_concurrency=10)
     class Barrier:
         def __init__(self, n, delay=0):