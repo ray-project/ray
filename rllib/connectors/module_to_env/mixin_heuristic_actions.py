34217b3d922aa1072e40ac188e738ce9f49bf4df
sven1977 <svenmika1977@gmail.com>
2024-03-03 13:07:19 +0100

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

---
+++ rllib/connectors/module_to_env/mixin_heuristic_actions.py
@@ -0,0 +1,89 @@
+from typing import Any, Callable, List, Optional
+
+import gymnasium as gym
+
+from ray.rllib.connectors.connector_v2 import ConnectorV2
+from ray.rllib.core.rl_module.rl_module import RLModule
+from ray.rllib.policy.sample_batch import SampleBatch
+from ray.rllib.utils.annotations import override
+from ray.rllib.utils.typing import EpisodeType, TensorType
+
+
+class MixinHeuristicActions(ConnectorV2):
+    """Connector mixing in heuristic action (logits) into the RLModule computes ones.
+
+    This connector should be used for a) inference only or b) off-policy algorithms,
+    which do NOT require the current RLModule to produce the actions sent to the env.
+
+    - This connector should be placed into the module-to-env pipeline.
+    - If the mixin weight is > 0.0, the connector will compute heuristic action logits
+    using a provided (heuristic) function and mixin these heuristic logits into the
+    RLModule computes ones.
+    - The connector will rely on the RLlib default connector pieces in the module-to-env
+    pipeline to create the action distribution object (whose class is defined by the
+    RLModule), sample from this distribution (using the mixin logits), and push the
+    sampled action(s) into the final batch sent to the env for stepping.
+    """
+
+    def __init__(
+        self,
+        input_observation_space: Optional[gym.Space] = None,
+        input_action_space: Optional[gym.Space] = None,
+        *,
+        compute_heuristic_actions: Callable[[Any], TensorType],
+        mixin_weight: float = 0.5,
+    ):
+        """Initializes a MixinHeuristicActions instance.
+
+        Args:
+            compute_heuristic_actions: A callable taking a current observation and
+                returning a tensor with action logits. This may be a one-hot tensor
+                if the action choice is unanimous/greedy.
+            mixin_weight: The weight from 0.0 to 1.0, with which to mixin the heuristic
+                logits into the RLModule computed ones.
+        """
+        super().__init__(input_observation_space, input_action_space)
+        self.compute_heuristic_actions = compute_heuristic_actions
+        self.mixin_weight = mixin_weight
+
+    @override(ConnectorV2)
+    def __call__(
+        self,
+        *,
+        rl_module: RLModule,
+        data: Any,
+        episodes: List[EpisodeType],
+        explore: Optional[bool] = None,
+        shared_data: Optional[dict] = None,
+        **kwargs,
+    ) -> Any:
+        # Early out if no mixin required -> Just use RLModule logits as always.
+        if self.mixin_weight == 0.0:
+            return data
+
+        action_dist_inputs = data.get(SampleBatch.ACTION_DIST_INPUTS)
+        if action_dist_inputs is None:
+            raise ValueError(
+                "`data` must already have a column named "
+                f"{SampleBatch.ACTION_DIST_INPUTS} in it for this connector to work!"
+            )
+
+        def _wrapped_compute_heuristic_actions(
+            obs_and_logits, eps_id, agent_id, module_id
+        ):
+            obs, rl_module_logits = obs_and_logits
+            # Compute heuristic logits.
+            heuristic_logits = self.compute_heuristic_actions(obs)
+            # Mixin heuristics with RLModule computed action logits.
+            return (
+                heuristic_logits * self.mixin_weight
+                + rl_module_logits * (1.0 - self.mixin_weight)
+            )
+
+        self.foreach_batch_item_change_in_place(
+            batch=data,
+            column=[SampleBatch.OBS, SampleBatch.ACTION_DIST_INPUTS],
+            func=_wrapped_compute_heuristic_actions,
+        )
+
+        return data
---
+++ rllib/examples/connectors/connector_v2_mixin_heuristic_actions.py
@@ -0,0 +1,129 @@
+import numpy as np
+
+from ray.rllib.connectors.module_to_env.mixin_heuristic_actions import (
+    MixinHeuristicActions
+)
+from ray.rllib.env.multi_agent_env_runner import MultiAgentEnvRunner
+from ray.rllib.env.single_agent_env_runner import SingleAgentEnvRunner
+from ray.rllib.examples.env.multi_agent import MultiAgentCartPole
+from ray.rllib.utils.test_utils import (
+    add_rllib_example_script_args,
+    run_rllib_example_script_experiment,
+)
+from ray.tune.registry import get_trainable_cls
+
+
+# Read in common example script command line arguments.
+parser = add_rllib_example_script_args(
+    default_timesteps=20000, default_reward=500.0, default_iters=100
+)
+parser.add_argument(
+    "--mixin-weight",
+    type=float,
+    default=0.5,
+    help="The weight between 0.0 and 1.0 of the heuristic action mixin (over the "
+    "RLModule computed one). 0.0 means only use our trained RLModule, 1.0 means only "
+    "use the heuristic policy.",
+)
+
+
+def cartpole_perfect_actions(obs):
+    cart_pos, cart_vel, pole_angle, pole_vel = obs
+
+    # The following tuning parameters should provide almost perfect control that can
+    # reliably reach a 500.0 return in CartPole-v1.
+    k_angle = 0.75  # Weight for how much we care about the pole angle.
+    k_pos = 0.5  # Weight for how much we care about the cart position.
+    k_vel = 0.5  # Weight for how much we care about the cart velocity.
+    k_pole_vel = 0.5  # Weight for how much we care about the pole velocity at its tip.
+    # Calculate a simple score that represents the desirability of moving right
+    move_right_score = (
+        pole_angle * k_angle
+        + pole_vel * k_pole_vel
+        + cart_pos * k_pos
+        + cart_vel * k_vel
+    )
+    # Decide on action based on the score.
+    if move_right_score > 0:
+        return np.array([0.0, 1.0])
+    else:
+        return np.array([1.0, 0.0])
+
+
+if __name__ == "__main__":
+    from ray import tune
+
+    args = parser.parse_args()
+
+    if args.num_agents > 0:
+        tune.register_env(
+            "env",
+            lambda cfg: MultiAgentCartPole(
+                dict(cfg, **{"num_agents": args.num_agents})
+            ),
+        )
+
+    config = (
+        get_trainable_cls(args.algo)
+        .get_default_config()
+        # Use new API stack ...
+        .experimental(_enable_new_api_stack=args.enable_new_api_stack)
+        .framework(args.framework)
+        .environment("env" if args.num_agents > 0 else "CartPole-v1")
+        .rollouts(
+            num_rollout_workers=args.num_env_runners,
+            # Set up the correct env-runner to use depending on
+            # old-stack/new-stack and multi-agent settings.
+            env_runner_cls=(
+                None
+                if not args.enable_new_api_stack
+                else SingleAgentEnvRunner
+                if args.num_agents == 0
+                else MultiAgentEnvRunner
+            ),
+        )
+        .resources(
+            num_gpus=args.num_gpus,  # old stack
+            num_learner_workers=args.num_gpus,  # new stack
+            num_gpus_per_learner_worker=1 if args.num_gpus else 0,
+            num_cpus_for_local_worker=1,
+        )
+        .training(
+            gamma=0.99,
+            lr=0.0003,
+            model=dict(
+                {
+                    "vf_share_layers": True,
+                    "fcnet_hiddens": [32],
+                    "fcnet_activation": "linear",
+                },
+                **({"uses_new_env_runners": True} if args.enable_new_api_stack else {}),
+            ),
+        )
+        .evaluation(
+            evaluation_interval=1,
+            evaluation_num_workers=2,
+            enable_async_evaluation=True,
+            evaluation_duration=10,
+            evaluation_config={
+                "explore": False,
+                "_module_to_env_connector": lambda env: MixinHeuristicActions(
+                    compute_heuristic_actions=cartpole_perfect_actions,
+                    mixin_weight=args.mixin_weight,
+                ),
+            },
+        )
+    )
+
+    # Add a simple multi-agent setup.
+    if args.num_agents > 0:
+        config.multi_agent(
+            policies={f"p{i}" for i in range(args.num_agents)},
+            policy_mapping_fn=lambda aid, *a, **kw: f"p{aid}",
+        )
+
+    # Run everything as configured.
+    stop = {
+        "evaluation/sampler_results/episode_reward_mean": 500.0,
+    }
+    run_rllib_example_script_experiment(config, args, stop=stop)
