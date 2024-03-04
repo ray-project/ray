05c6f6847c4106d04e7d8a8a39626f1e5950b8d7
rickyyx <rickyx@anyscale.com>
2024-03-04 05:24:12 +0000

    up
    Signed-off-by: rickyyx <rickyx@anyscale.com>

--- python/ray/tests/kuberay/scripts/scale_up.py
+++ python/ray/tests/kuberay/scripts/scale_up.py
@@ -5,9 +5,14 @@ from ray._private.test_utils import wait_for_condition
 def main():
     """Submits CPU request"""
     ray.autoscaler.sdk.request_resources(num_cpus=2)
+    from ray.autoscaler.v2.sdk import get_cluster_status
+    from ray.autoscaler.v2.utils import ClusterStatusFormatter
 
     def verify():
         cluster_resources = ray.cluster_resources()
+
+        cluster_status = get_cluster_status()
+        print(ClusterStatusFormatter.format(cluster_status))
         assert cluster_resources.get("CPU", 0) == 2, cluster_resources
 
         return True
