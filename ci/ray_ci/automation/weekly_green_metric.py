5bb8d033e7aa1d77c101262f7dafef5a7d942564
khluu <51931015+khluu@users.noreply.github.com>
2024-03-05 15:38:08 -0800

    use sys exit
    Signed-off-by: khluu <51931015+khluu@users.noreply.github.com>

--- ci/ray_ci/automation/weekly_green_metric.py
+++ ci/ray_ci/automation/weekly_green_metric.py
@@ -1,6 +1,6 @@
 import json
 import time
-import os
+import sys
 
 import boto3
 import click
@@ -49,7 +49,7 @@ def main(production: bool, check: bool) -> None:
         logger.info("Weekly green metric updated successfully")
 
     if check and blockers.totalCount != 0:
-        os.exit(42)  # Not retrying the check on Buildkite jobs
+        sys.exit(42)  # Not retrying the check on Buildkite jobs
 
 
 if __name__ == "__main__":
