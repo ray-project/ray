b966dc5250da46e6c21f88a1962595a819eb5d96
Edward Oakes <ed.nmi.oakes@gmail.com>
2024-03-05 09:22:17 -0600

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/_private/deployment_state.py
+++ python/ray/serve/_private/deployment_state.py
@@ -1632,9 +1632,9 @@ class DeploymentState:
             )
 
         logger.info(
-            f"Deploying new version of deployment {self.deployment_name} in "
-            f"application '{self.app_name}'. Setting initial target number of "
-            f"replicas to {target_num_replicas}."
+            f"Deploying new version of deployment '{self.deployment_name}' in "
+            f"application '{self.app_name}' "
+            f"(initial target replicas: {target_num_replicas})."
         )
         self._replica_constructor_retry_counter = 0
         self._backoff_time_s = 1
