e0e2fe10ab1ca849c6003da7b03d892179eab12e
khluu <51931015+khluu@users.noreply.github.com>
2024-03-05 16:51:33 -0800

    push
    Signed-off-by: khluu <51931015+khluu@users.noreply.github.com>

---
+++ .buildkite/release-automation/verify-windows-wheels.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -euo pipefail
+
+set -x
+
+PYTHON_VERSIONS=("3.8" "3.9" "3.10" "3.11")
+export RAY_VERSION="${RAY_VERSION:-2.9.2}"
+export RAY_HASH="${RAY_HASH:-fce7a361807580953364e2da964f9498f3123bf9}"
+
+run_sanity_check() {
+    local python_version="$1"
+    conda create -n "rayio_${python_version}" python="${python_version}" -y
+    conda activate "rayio_${python_version}"
+    pip install \
+        --index-url https://test.pypi.org/simple/ \
+        --extra-index-url https://pypi.org/simple \
+        "ray[cpp]==$RAY_VERSION"
+    (
+        cd release/util
+        which python
+        python --version
+        python sanity_check.py
+    )
+    conda deactivate
+    conda env remove -n "rayio_${python_version}" -y
+}
+
+source /c/Miniconda3/etc/profile.d/conda.sh
+
+# Install Ray & run sanity checks for each python version
+for python_version in "${PYTHON_VERSIONS[@]}"; do
+    run_sanity_check "$python_version"
+done
