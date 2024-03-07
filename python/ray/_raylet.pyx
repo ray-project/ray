505d14143cfcef8081f4f67b7eacdeb706f12580
Alexey Kudinkin <ak@anyscale.com>
2024-03-07 14:21:21 -0800

    Wait for executor shutdown in `stop_and_join_asyncio_threads_if_exist`
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/_raylet.pyx
+++ python/ray/_raylet.pyx
@@ -4758,7 +4758,7 @@ cdef class CoreWorker:
         threads = []
         if self.event_loop_executor:
             self.event_loop_executor.shutdown(
-                wait=False, cancel_futures=True)
+                wait=True, cancel_futures=True)
         if self.eventloop_for_default_cg is not None:
             event_loops.append(self.eventloop_for_default_cg)
         if self.thread_for_default_cg is not None:
