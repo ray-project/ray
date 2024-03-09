92e55895e72308db9d550d958b1d9fb65b912cff
khluu <51931015+khluu@users.noreply.github.com>
2024-03-08 17:22:23 -0800

    push
    Signed-off-by: khluu <51931015+khluu@users.noreply.github.com>

--- ci/ray_ci/utils.py
+++ ci/ray_ci/utils.py
@@ -14,7 +14,7 @@ from ray_release.test import Test, TestState
 
 
 POSTMERGE_PIPELINE = "0189e759-8c96-4302-b6b5-b4274406bf89"
-RAY_VERSION = "3.0.0.dev0"
+RAY_VERSION = "2.10.0"
 
 
 def chunk_into_n(list: List[str], n: int) -> List[List[str]]:
