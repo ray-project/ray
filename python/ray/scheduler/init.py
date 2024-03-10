a9edad691b8a8f6913d3fa76832ff822e0545a43
zzb66666666x <18767066589@163.com>
2024-03-10 08:14:14 +0000

    set abs path to ray runtime env workdir

--- python/ray/scheduler/init.py
+++ python/ray/scheduler/init.py
@@ -2,8 +2,13 @@ import ray
 from ray import serve
 from api_server import Apiserver
 from custom_controller import Controller
+import os
 
-ray.init(address="auto", runtime_env={"working_dir": "."})
+cur_file_path = os.path.dirname(os.path.realpath(__file__))
+
+print(cur_file_path)
+
+ray.init(address="auto", runtime_env={"working_dir": cur_file_path})
 
 serve.start(http_options={"host": "0.0.0.0"})
 serve.run(Apiserver.bind())
