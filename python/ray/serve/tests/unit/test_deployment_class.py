2318944be780ce7ed5f551f6fed872e8d44e63f9
Shreyas Krishnaswamy <shrekris@anyscale.com>
2024-03-12 14:06:05 -0700

    Add a comment
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- python/ray/serve/tests/unit/test_deployment_class.py
+++ python/ray/serve/tests/unit/test_deployment_class.py
@@ -223,6 +223,7 @@ class TestDeploymentOptions:
 
         with pytest.raises(ValueError):
 
+            # PG bundle with empty resources is invalid.
             @serve.deployment(
                 placement_group_bundles=[{"CPU": 0, "GPU": 0}],
                 ray_actor_options={"num_cpus": 0, "num_gpus": 0},
