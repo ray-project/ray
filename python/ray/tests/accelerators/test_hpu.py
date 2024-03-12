07c3553f64726f6ac5419090d7cc2347e2a6f0c8
Zhi Lin <zhi.lin@intel.com>
2024-03-12 09:57:45 +0000

    merge to test_hpu
    Signed-off-by: Zhi Lin <zhi.lin@intel.com>

--- python/ray/tests/accelerators/test_hpu.py
+++ python/ray/tests/accelerators/test_hpu.py
@@ -4,9 +4,10 @@ import pytest
 from unittest.mock import patch
 
 import ray
-from ray._private.accelerators import HPUAcceleratorManager
-from ray._private.accelerators import hpu
-
+from ray._private.accelerators import hpu, HPUAcceleratorManager
+from ray._private.test_utils import RayTestTimeoutException
+from ray.util.placement_group import placement_group
+from ray.util.scheduling_strategies import PlacementGroupSchedulingStrategy
 
 def test_user_configured_more_than_visible(monkeypatch, call_ray_stop_only):
     # Test more hpus are configured than visible.
@@ -111,6 +112,308 @@ def test_check_accelerator_info():
         assert HPUAcceleratorManager.get_current_node_accelerator_type() is None
 
     assert HPUAcceleratorManager.get_resource_name() == "HPU"
+
+
+def test_decorator_args(ray_start_regular):
+    
+    # This is a valid way of using the decorator.
+    @ray.remote(resources={'HPU': 1})  # noqa: F811
+    class Actor:  # noqa: F811
+        def __init__(self):
+            pass
+    
+    # This is a valid way of using the decorator.
+    @ray.remote(num_cpus=1, resources={'HPU': 1})  # noqa: F811
+    class Actor:  # noqa: F811
+        def __init__(self):
+            pass
+
+
+def test_actor_deletion_with_hpus(shutdown_only):
+    ray.init(num_cpus=1, resources={'HPU': 1}, object_store_memory=int(150 * 1024 * 1024))
+
+    # When an actor that uses an HPU exits, make sure that the HPU resources
+    # are released.
+
+    @ray.remote(resources={'HPU': 1})
+    class Actor:
+        def getpid(self):
+            return os.getpid()
+
+    for _ in range(5):
+        # If we can successfully create an actor, that means that enough
+        # HPU resources are available.
+        a = Actor.remote()
+        ray.get(a.getpid.remote())
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="Failing on Windows.")
+def test_actor_hpus(ray_start_cluster):
+    cluster = ray_start_cluster
+    num_nodes = 2
+    num_hpus_per_raylet = 2
+    for i in range(num_nodes):
+        cluster.add_node(
+            num_cpus=10 * 2, resources={"HPU": num_hpus_per_raylet}
+        )
+    ray.init(address=cluster.address)
+
+    @ray.remote(resources={'HPU': 1})
+    class Actor1:
+        def __init__(self):
+            resource_ids = ray.get_runtime_context().get_resource_ids()
+            self.hpu_ids = resource_ids.get('HPU')
+
+        def get_location_and_ids(self):
+            return (
+                ray._private.worker.global_worker.node.unique_id,
+                tuple(self.hpu_ids),
+            )
+
+    # Create one actor per HPU.
+    actors = [Actor1.remote() for _ in range(num_nodes * num_hpus_per_raylet)]
+    # Make sure that no two actors are assigned to the same HPU.
+    locations_and_ids = ray.get(
+        [actor.get_location_and_ids.remote() for actor in actors]
+    )
+    node_names = {location for location, hpu_id in locations_and_ids}
+    assert len(node_names) == num_nodes
+    location_actor_combinations = []
+    for node_name in node_names:
+        for hpu_id in range(num_hpus_per_raylet):
+            location_actor_combinations.append((node_name, (f'{hpu_id}',)))
+
+    assert set(locations_and_ids) == set(location_actor_combinations)
+
+    # Creating a new actor should fail because all of the HPUs are being
+    # used.
+    a = Actor1.remote()
+    ready_ids, _ = ray.wait([a.get_location_and_ids.remote()], timeout=0.01)
+    assert ready_ids == []
+
+
+def test_blocking_actor_task(shutdown_only):
+    ray.init(num_cpus=1, resources={"HPU": 1}, object_store_memory=int(150 * 1024 * 1024))
+
+    @ray.remote(resources={"HPU": 1})
+    def f():
+        return 1
+
+    @ray.remote
+    class Foo:
+        def __init__(self):
+            pass
+
+        def blocking_method(self):
+            ray.get(f.remote())
+
+    # Make sure we can execute a blocking actor method even if there is
+    # only one CPU.
+    actor = Foo.remote()
+    ray.get(actor.blocking_method.remote())
+
+    @ray.remote(num_cpus=1)
+    class CPUFoo:
+        def __init__(self):
+            pass
+
+        def blocking_method(self):
+            ray.get(f.remote())
+
+    # Make sure that lifetime CPU resources are not released when actors
+    # block.
+    actor = CPUFoo.remote()
+    x_id = actor.blocking_method.remote()
+    ready_ids, remaining_ids = ray.wait([x_id], timeout=1.0)
+    assert ready_ids == []
+    assert remaining_ids == [x_id]
+
+    @ray.remote(resources={"HPU": 1})
+    class HPUFoo:
+        def __init__(self):
+            pass
+
+        def blocking_method(self):
+            ray.get(f.remote())
+
+    # Make sure that HPU resources are not released when actors block.
+    actor = HPUFoo.remote()
+    x_id = actor.blocking_method.remote()
+    ready_ids, remaining_ids = ray.wait([x_id], timeout=1.0)
+    assert ready_ids == []
+    assert remaining_ids == [x_id]
+
+
+def test_actor_habana_visible_devices(shutdown_only):
+    """Test user can overwrite HABANA_VISIBLE_MODULES
+    after the actor is created."""
+    ray.init(resources={'HPU': 1})
+
+    @ray.remote(resources={'HPU': 1})
+    class Actor:
+        def set_habana_visible_devices(self, habana_visible_devices):
+            os.environ["HABANA_VISIBLE_MODULES"] = habana_visible_devices
+
+        def get_habana_visible_devices(self):
+            return os.environ["HABANA_VISIBLE_MODULES"]
+
+    actor = Actor.remote()
+    assert ray.get(actor.get_habana_visible_devices.remote()) == "0"
+    ray.get(actor.set_habana_visible_devices.remote("0,1"))
+    assert ray.get(actor.get_habana_visible_devices.remote()) == "0,1"
+
+
+def test_hpu_ids(shutdown_only):
+    num_hpus = 3
+    ray.init(num_cpus=num_hpus, resources={"HPU": num_hpus})
+
+    def get_hpu_ids(hpus_per_worker):
+        hpu_ids = ray.get_runtime_context().get_resource_ids()["HPU"]
+        assert len(hpu_ids) == hpus_per_worker
+        modules = os.environ.get("HABANA_VISIBLE_MODULES")
+        if modules is not None:
+            assert modules == ",".join([str(i) for i in hpu_ids])  # noqa
+        for hpu_id in hpu_ids:
+            assert hpu_id in [str(i) for i in range(num_hpus)]
+        return hpu_ids
+
+    f0 = ray.remote(resources={"HPU": 0})(lambda: get_hpu_ids(0))
+    f1 = ray.remote(resources={"HPU": 1})(lambda: get_hpu_ids(1))
+    f2 = ray.remote(resources={"HPU": 2})(lambda: get_hpu_ids(2))
+
+    # Wait for all workers to start up.
+    @ray.remote
+    def g():
+        time.sleep(0.2)
+        return os.getpid()
+
+    start_time = time.time()
+    while True:
+        num_workers_started = len(set(ray.get([g.remote() for _ in range(num_hpus)])))
+        if num_workers_started == num_hpus:
+            break
+        if time.time() > start_time + 10:
+            raise RayTestTimeoutException(
+                "Timed out while waiting for workers to start up."
+            )
+
+    list_of_ids = ray.get([f0.remote() for _ in range(10)])
+    assert list_of_ids == 10 * [[]]
+    ray.get([f1.remote() for _ in range(10)])
+    ray.get([f2.remote() for _ in range(10)])
+
+    # Test that actors have NEURON_RT_VISIBLE_CORES set properly.
+
+    @ray.remote
+    class Actor0:
+        def __init__(self):
+            hpu_ids = ray.get_runtime_context().get_resource_ids()[
+                "HPU"
+            ]
+            assert len(hpu_ids) == 0
+            assert os.environ["HABANA_VISIBLE_MODULES"] == ",".join(
+                [str(i) for i in hpu_ids]  # noqa
+            )
+            # Set self.x to make sure that we got here.
+            self.x = 0
+
+        def test(self):
+            hpu_ids = ray.get_runtime_context().get_resource_ids()[
+                "HPU"
+            ]
+            assert len(hpu_ids) == 0
+            assert os.environ["HABANA_VISIBLE_MODULES"] == ",".join(
+                [str(i) for i in hpu_ids]  # noqa
+            )
+            return self.x
+
+    @ray.remote(resources={"HPU": 1})
+    class Actor1:
+        def __init__(self):
+            hpu_ids = ray.get_runtime_context().get_resource_ids()[
+                "HPU"
+            ]
+            assert len(hpu_ids) == 1
+            assert os.environ["HABANA_VISIBLE_MODULES"] == ",".join(
+                [str(i) for i in hpu_ids]  # noqa
+            )
+            # Set self.x to make sure that we got here.
+            self.x = 1
+
+        def test(self):
+            hpu_ids = ray.get_runtime_context().get_resource_ids()[
+                "HPU"
+            ]
+            assert len(hpu_ids) == 1
+            assert os.environ["HABANA_VISIBLE_MODULES"] == ",".join(
+                [str(i) for i in hpu_ids]
+            )
+            return self.x
+
+    @ray.remote(resources={"HPU": 2})
+    class Actor2:
+        def __init__(self):
+            hpu_ids = ray.get_runtime_context().get_resource_ids()[
+                "HPU"
+            ]
+            assert len(hpu_ids) == 2
+            assert os.environ["HABANA_VISIBLE_MODULES"] == ",".join(
+                [str(i) for i in hpu_ids]
+            )
+            # Set self.x to make sure that we got here.
+            self.x = 2
+
+        def test(self):
+            hpu_ids = ray.get_runtime_context().get_resource_ids()[
+                "HPU"
+            ]
+            assert len(hpu_ids) == 2
+            assert os.environ["HABANA_VISIBLE_MODULES"] == ",".join(
+                [str(i) for i in hpu_ids]
+            )
+            return self.x
+
+    a0 = Actor0.remote()
+    assert ray.get(a0.test.remote()) == 0
+
+    a1 = Actor1.remote()
+    assert ray.get(a1.test.remote()) == 1
+
+    a2 = Actor2.remote()
+    assert ray.get(a2.test.remote()) == 2
+
+
+def test_hpu_with_placement_group(shutdown_only):
+    num_hpus = 2
+    ray.init(num_cpus=1, resources={"HPU": num_hpus})
+
+    @ray.remote(resources={"HPU": num_hpus})
+    class HPUActor:
+        def __init__(self):
+            pass
+
+        def ready(self):
+            hpu_ids = ray.get_runtime_context().get_resource_ids()[
+                "HPU"
+            ]
+            assert len(hpu_ids) == num_hpus
+            assert os.environ["HABANA_VISIBLE_MODULES"] == ",".join(
+                [str(i) for i in hpu_ids]  # noqa
+            )
+
+    # Reserve a placement group of 1 bundle that reserves 1 CPU and 2 HPU.
+    pg = placement_group([{"CPU": 1, "HPU": num_hpus}])
+
+    # Wait until placement group is created.
+    ray.get(pg.ready(), timeout=10)
+
+    actor = HPUActor.options(
+        scheduling_strategy=PlacementGroupSchedulingStrategy(
+            placement_group=pg,
+        )
+    ).remote()
+
+    ray.get(actor.ready.remote(), timeout=10)
 
 
 if __name__ == "__main__":
