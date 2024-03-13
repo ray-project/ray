706b96871e00be0071f00e1f48b565eeaee81cf6
can <can@anyscale.com>
2024-03-13 18:17:08 +0000

    [ci] migrate the rest of rllib contrib
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/rllib_contrib/rllib_contrib_ci.sh
+++ ci/ray_ci/rllib_contrib/rllib_contrib_ci.sh
@@ -140,4 +140,62 @@ test_td3() {
   bazel test --config=ci $(./ci/run/bazel_export_options) --build_tests_only --test_tag_filters=-flaky rllib_contrib/td3/...
 }
 
+test_a3c() {
+  build "a3c"
+  # BAZEL (learning and compilation) tests:
+  bazel test --config=ci $(./ci/run/bazel_export_options) --build_tests_only --test_tag_filters=-flaky rllib_contrib/a3c/...
+  bazel test --config=ci $(./ci/run/bazel_export_options) --build_tests_only --test_tag_filters=-flaky,learning_tests --test_arg=--framework=torch rllib_contrib/a3c/...
+  bazel test --config=ci $(./ci/run/bazel_export_options) --build_tests_only --test_tag_filters=-flaky,learning_tests --test_arg=--framework=tf2 rllib_contrib/a3c/...
+}
+
+test_crr() {
+  build "crr"
+  wget https://raw.githubusercontent.com/ray-project/ray/master/rllib/tests/data/pendulum/pendulum_replay_v1.1.0.zip -P rllib_contrib/crr/tuned_examples/
+  # BAZEL (learning and compilation) tests:
+  bazel test --config=ci $(./ci/run/bazel_export_options) --build_tests_only --test_tag_filters=-flaky,-learning_tests rllib_contrib/crr/...
+}
+
+test_ddppo() {
+  build "ddppo"
+  # BAZEL (learning and compilation) tests:
+  bazel test --config=ci $(./ci/run/bazel_export_options) --build_tests_only --test_tag_filters=-flaky,-learning_tests rllib_contrib/ddppo/...
+  bazel test --config=ci $(./ci/run/bazel_export_options) --build_tests_only --test_tag_filters=-flaky,learning_tests --test_arg=--framework=torch rllib_contrib/ddppo/...
+}
+
+test_dt() {
+  build "dt"
+  wget https://github.com/ray-project/ray/raw/releases/2.5.1/rllib/tests/data/pendulum/pendulum_expert_sac_50eps.zip -P rllib_contrib/dt/tuned_examples/
+  wget https://github.com/ray-project/ray/raw/releases/2.5.1/rllib/tests/data/pendulum/pendulum_medium_sac_50eps.zip -P rllib_contrib/dt/tuned_examples/
+  # BAZEL (learning and compilation) tests:
+  bazel test --config=ci $(./ci/run/bazel_export_options) --build_tests_only --test_tag_filters=-flaky,-learning_tests rllib_contrib/dt/...
+}
+
+test_leela_chess_zero() {
+  build "leela_chess_zero"
+  # BAZEL (learning and compilation) tests:
+  bazel test --config=ci $(./ci/run/bazel_export_options) --build_tests_only --test_tag_filters=-flaky rllib_contrib/leela_chess_zero/...
+}
+
+test_mbmpo() {
+  # Install mujoco necessary for the testing environments
+  sudo apt install libosmesa6-dev libgl1-mesa-glx libglfw3 patchelf -y
+  mkdir -p /root/.mujoco
+  wget https://github.com/google-deepmind/mujoco/releases/download/2.1.1/mujoco-2.1.1-linux-x86_64.tar.gz
+  mv mujoco-2.1.1-linux-x86_64.tar.gz /root/.mujoco/.
+  (cd /root/.mujoco && tar -xf /root/.mujoco/mujoco-2.1.1-linux-x86_64.tar.gz)
+  # shellcheck disable=SC2016
+  echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mujoco-2.1.1/bin' >> /root/.bashrc
+  source /root/.bashrc
+  # build
+  build "mbmpo"
+  # BAZEL (learning and compilation) tests:
+  bazel test --config=ci $(./ci/run/bazel_export_options) --build_tests_only --test_tag_filters=-flaky rllib_contrib/mbmpo/...
+}
+
+test_td3() {
+  build "td3"
+  # BAZEL (learning and compilation) tests:
+  bazel test --config=ci $(./ci/run/bazel_export_options) --build_tests_only --test_tag_filters=-flaky rllib_contrib/td3/...
+}
+
 "$@"
