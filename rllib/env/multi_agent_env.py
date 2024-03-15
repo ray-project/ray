ba72dd437f97f78ac149648ee2723a48877d6d8f
sven1977 <svenmika1977@gmail.com>
2024-03-15 12:28:00 +0100

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/env/multi_agent_env.py
+++ rllib/env/multi_agent_env.py
@@ -699,8 +699,11 @@ class MultiAgentEnvWrapper(BaseEnv):
 
             if isinstance(obs, Exception):
                 if self.restart_failed_sub_environments:
-                    del self.env_states[idx]
-                    del self.envs[idx]
+                    env_state_to_del = self.env_states[idx]
+                    env_to_del = self.envs[idx]
+                    self.env_states[idx] = self.envs[idx] = None
+                    del env_state_to_del
+                    del env_to_del
                     # Re-init env_state.
                     self._init_env_state(idx=idx)
                     # Try recreating the sub-env.
@@ -738,8 +741,11 @@ class MultiAgentEnvWrapper(BaseEnv):
                         "Trying to close old and replaced sub-environment (at vector "
                         f"index={idx}), but closing resulted in error:\n{e}"
                     )
-            del self.env_states[idx]
-            del self.envs[idx]
+            env_state_to_del = self.env_states[idx]
+            env_to_del = self.envs[idx]
+            self.env_states[idx] = self.envs[idx] = None
+            del env_state_to_del
+            del env_to_del
             # Re-init env_state.
             self._init_env_state(idx=idx)
             # Try recreating the sub-env.
