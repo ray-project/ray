66f363696db3b9735493e8f10b26016b0ed874a2
Shreyas Krishnaswamy <shrekris@anyscale.com>
2024-03-20 11:57:11 -0700

    Apply linter
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- python/ray/serve/tests/unit/test_serve_utils.py
+++ python/ray/serve/tests/unit/test_serve_utils.py
@@ -1,8 +1,9 @@
 import sys
+
 import pytest
 
-from ray.exceptions import RayActorError
 from ray.core.generated.common_pb2 import ActorDiedErrorContext
+from ray.exceptions import RayActorError
 from ray.serve._private.utils import is_actor_dead
 
 
@@ -13,13 +14,13 @@ class TestIsActorDead:
         context = ActorDiedErrorContext(
             error_message="Fake error message",
             actor_id=("a" * 16).encode("utf-8"),
-            class_name="Fake class name"
+            class_name="Fake class name",
         )
 
         # If a RayActorError has a cause, then the actor has died.
         ray_actor_error_with_cause = RayActorError(cause=context)
         assert is_actor_dead(ray_actor_error_with_cause) is True
-    
+
     def test_actor_died_empty_error_message(self):
         """Check that is_actor_dead returns True when the context is empty."""
 
@@ -40,33 +41,33 @@ class TestIsActorDead:
         # A slow network returns a RayActorError with no cause.
         empty_ray_actor_error = RayActorError()
         assert is_actor_dead(empty_ray_actor_error) is False
-    
+
     def test_input_validation(self):
         """Check that is_actor_dead validates the error is a RayActorError."""
-        
+
         runtime_error = RuntimeError("This is not a RayActorError!")
-        
+
         # Only a RayActorError indicates that the actor has died.
         assert is_actor_dead(runtime_error) is False
-    
+
     def test_unexpected_error(self):
         """Check that an unexpected error in is_actor_dead doesn't cause a crash."""
 
         class BrokenRayActorError(RayActorError):
             """This custom RayActorError removes its own error messages."""
-            
+
             def __init__(self, *args, **kwargs):
                 super().__init__(*args, **kwargs)
                 del self.base_error_msg
-        
+
         context = ActorDiedErrorContext(
             error_message="Fake error message",
             actor_id=("a" * 16).encode("utf-8"),
-            class_name="Fake class name"
+            class_name="Fake class name",
         )
 
         broken_ray_actor_error = BrokenRayActorError(cause=context)
-        
+
         # is_actor_dead should return False since we don't know whether the
         # actor has died for sure.
         assert is_actor_dead(broken_ray_actor_error) is False
