44eea6ee5931380db10b746ce247cd37b2e88400
Alexey Kudinkin <ak@anyscale.com>
2024-03-21 19:29:26 -0700

    Expose keep-alive protocol configuration for gRPC clients
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- src/ray/common/grpc_util.h
+++ src/ray/common/grpc_util.h
@@ -165,6 +165,11 @@ inline grpc::ChannelArguments CreateDefaultChannelArguments(
                      ::RayConfig::instance().grpc_client_keepalive_timeout_ms());
   }
 
+  arguments.SetInt(GRPC_ARG_HTTP2_MAX_PINGS_WITHOUT_DATA,
+                    ::RayConfig::instance().grpc_http2_max_pings_without_data());
+  arguments.SetInt(GRPC_ARG_KEEPALIVE_PERMIT_WITHOUT_CALLS,
+                      ::RayConfig::instance().grpc_keepalive_permit_without_calls());
+
   arguments.SetInt(GRPC_ARG_CLIENT_IDLE_TIMEOUT_MS,
                    ::RayConfig::instance().grpc_client_idle_timeout_ms());
 
