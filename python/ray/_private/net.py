6486ebfcd5066c762f58e0e0fc446739012d81b1
Sam Gleske <sam.mxracer@gmail.com>
2024-03-23 13:27:35 -0400

    Fix net.py
    Misuse of dualstack.  It should only be used when listening for a port on
    all interfaces.
    
    flake8 fixes
    
    Signed-off-by: Sam Gleske <sam.mxracer@gmail.com>

--- python/ray/_private/net.py
+++ python/ray/_private/net.py
@@ -4,107 +4,138 @@
 #
 import re
 import socket
+from contextlib import closing
+
 
 @staticmethod
 def _get_addrinfo_from_sock_kind(address, kind, family=0):
     """
     Get a list of valid IP addresses compatible with requested filters.
 
-    kind is socket kind such as socket.SOCK_STREAM or socket.SOCK_DGRAM
-    family is INET address family.  0 is both IPv4 and IPv6.  socket.AF_INET is IPv4.  socket.AF_INET6 is IPv6
+    kind is socket kind such as socket.SOCK_STREAM or socket.SOCK_DGRAM family
+    is INET address family.  0 is both IPv4 and IPv6.  socket.AF_INET is IPv4.
+    socket.AF_INET6 is IPv6
 
     Returns a List of tuples with (family, ip) as items.
 
-    Example return value for _get_addrinfo_from_sock_kind("localhost", socket.SOCK_STREAM):
+    Example return value for
+    _get_addrinfo_from_sock_kind("localhost", socket.SOCK_STREAM):
 
         [(socket.AF_INET6, "::1"), (socket.AF_INET, "127.0.0.1")]
     """
-    return [(x[0], x[-1][0]) for x in socket.getaddrinfo(host, None, family) if x[1] == kind]
+    return [
+        (x[0], x[-1][0]) for x in socket.getaddrinfo(address, None, family)
+        if x[1] == kind
+    ]
+
 
 @staticmethod
 def _get_addrinfo_from_sock_kind_ipv4_fallback_ipv6(address, kind):
-    """Same as _get_sock_kind_from_addrinfo() but favors IPv4 if it is available.
+    """
+    Same as _get_sock_kind_from_addrinfo() but favors IPv4 if it is available.
 
-    If IPv4 is not available it will fall back to searching for IPv6."""
+    If IPv4 is not available it will fall back to searching for IPv6.
+    """
     inet_addresses = []
     try:
-        inet_addresses = _get_addrinfo_from_sock_kind(host, kind, socket.AF_INET)
-    except:
+        inet_addresses = _get_addrinfo_from_sock_kind(address, kind, socket.AF_INET)
+    except Exception as ignored:  # noqa: F841
         pass
     if len(inet_addresses) > 0:
         return inet_addresses
     else:
         # this call expands to both IPv4 and IPv6 addresses for the given host
-        return _get_addrinfo_from_sock_kind(host, kind)
+        return _get_addrinfo_from_sock_kind(address, kind)
+
 
 @staticmethod
 def _get_sock_from_host(address, kind):
-    """Get a socket.socket for a provided inet address.
+    """
+    Get a socket.socket for a provided inet address.
 
-    Favor IPv4 but expand to IPv6 if IPv4 not available."""
+    Favor IPv4 but expand to IPv6 if IPv4 not available.
+    """
     # obtain the first valid address for use
     # inet_address is a tuple with (socket.AF_INET or socket.AF_INET6, ip_address)
-    inet_address = _get_addrinfo_from_sock_kind_ipv4_fallback_ipv6(host, kind)[0]
+    inet_address = _get_addrinfo_from_sock_kind_ipv4_fallback_ipv6(address, kind)[0]
     return socket.socket(inet_address[0], kind)
+
 
 @staticmethod
 def _get_sock_stream_from_host(address):
-    """Returns a socket.socket which can either be IPv4 or IPv6 with socket.SOCK_STREAM.
+    """
+    Returns a socket.socket which can either be IPv4 or IPv6 with
+    socket.SOCK_STREAM.
 
-    Favor IPv4 but expand to IPv6 if IPv4 not available."""
+    Favor IPv4 but expand to IPv6 if IPv4 not available.
+    """
     return _get_sock_from_host(address, socket.SOCK_STREAM)
+
 
 @staticmethod
 def _get_sock_dgram_from_host(address):
-    """Returns a socket.socket which can either be IPv4 or IPv6 with socket.SOCK_DGRAM.
+    """
+    Returns a socket.socket which can either be IPv4 or IPv6 with
+    socket.SOCK_DGRAM.
 
-    Favor IPv4 but expand to IPv6 if IPv4 not available."""
+    Favor IPv4 but expand to IPv6 if IPv4 not available.
+    """
     return _get_sock_from_host(address, socket.SOCK_DGRAM)
+
 
 @staticmethod
 def _get_private_ip_addresses():
-    """Connects to Google DNS over IPv4, IPv6, or both to discover private IP addresses."""
+    """
+    Connects to Google DNS over IPv4, IPv6, or both to discover private IP
+    addresses.
+    """
     private_ip_addresses = []
     for public_host in ["2001:4860:4860::8888", "8.8.8.8"]:
+        if ":" in public_host and not socket.has_ipv6:
+            continue
         try:
-            s = _get_socket_dualstack_fallback_single_stack_laddr(socket.SOCK_DGRAM)
-            if not socket.has_dualstack_ipv6():
-                # be aware we're single stack ip networking
-                # we should skip addresses that are not compatible with our supported stack
-                if ":" in public_host and not s.family == socket.AF_INET6:
+            with closing(_get_sock_dgram_from_host(public_host)) as s:
+                # IPv4 may not be available so skip
+                if ":" not in public_host and not s.family == socket.AF_INET:
                     continue
-                if not (":" in public_host) and not s.family == socket.AF_INET:
-                    continue
-            s.connect((public_host, 80))
-            # get the local iface IP used to connect against the public_host
-            private_ip_addresses.append(s.getsockname()[0])
-        except:
+                s.connect((public_host, 80))
+                # get the local iface IP used to connect against the public_host
+                private_ip_addresses.append(s.getsockname()[0])
+        except Exception as ignored:  # noqa: F841
             pass
-        finally:
-            s.close()
     return private_ip_addresses
+
 
 @staticmethod
 def _get_socket_dualstack_fallback_single_stack_laddr(kind=socket.SOCK_STREAM):
-    """Similar to socket.socket() except that if networking is dualstack it will listen on all address family interfaces.
+    """
+    Similar to socket.socket() except that if networking is dualstack it will
+    listen on all address family interfaces.
 
-    If single-stack, then the loopback interface is queried for which stack is allowed (IPv4 or IPv6)"""
+    If single-stack, then the loopback interface is queried for which stack is
+    allowed (IPv4 or IPv6).
+    """
     if socket.has_dualstack_ipv6():
         sock = socket.socket(socket.AF_INET6, kind)
         sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
         # set up to listen on all IPv4 and IPv6 interfaces with a single socket
         return sock
     else:
-        # fall back to supporting the host single network stack (IPv4 or IPv6 only)
+        # fall back to supporting the host single network stack.i
+        # (IPv4 or IPv6 only)
         return _get_sock_from_host("localhost", kind)
+
 
 @staticmethod
 def _parse_ip_port(address: str) -> list[str]:
-    """Parses a str of ip:port and returns a List with (ip, port).
+    """
+    Parses a str of ip:port and returns a List with (ip, port).
 
-    This uses str.rsplit(":", 1) to support DNS, IPv6, and IPv4 parsing."""
+    This uses str.rsplit(":", 1) to support DNS, IPv6, and IPv4 parsing.
+    """
 
-    # if no colon is in the address then assume a List with the original value is desired.
+    # If no colon is in the address then assume a List with the original value
+    # is desired.
     #
     # This is necessary, for example, in
     # ray._private.services.resolve_ip_for_localhost()
@@ -113,14 +144,14 @@ def _parse_ip_port(address: str) -> list[str]:
     #       fail with ::1 instead of 127.0.0.1.  For now, I don't think this
     #       needs to be handled.  From after this conditional, IPv6 will always
     #       be required to have ip:port or it will raise a ValueError.
-    if not ":" in address:
+    if ":" not in address:
         return [address]
 
     # do not attempt to parse if not ip:port or url-ish (*://ip:port/*) with
     # ip:port for DNS, IPv4, and IPv6 supported
     pattern = re.compile("^(.*/)?.*[^:]:[0-9]+(/.*)?$")
     if not pattern.match(address):
-        raise ValueError(f"Malformed address (expected address to include IP or DNS host and port e.g. ip:port): {address}")
+        raise ValueError(f"Malformed address (expected address to include IP or DNS host and port e.g. ip:port): {address}")  # noqa: E501
     # use rsplit for IPv6 or IPv4
     ip, port = address.rsplit(":", 1)
     # check for url-ish
