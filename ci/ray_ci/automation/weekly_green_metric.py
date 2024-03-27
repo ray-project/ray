0de226fa58750cd3333f23b263757948588f2c68
can <can@anyscale.com>
2024-03-27 16:39:52 +0000

    [ci] create test db bucket for pr
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/automation/weekly_green_metric.py
+++ ci/ray_ci/automation/weekly_green_metric.py
@@ -5,7 +5,7 @@ import sys
 import boto3
 import click
 
-from ci.ray_ci.utils import logger, ci_init
+from ci.ray_ci.utils import logger, ci_init, get_state_machine_aws_bucket
 from ray_release.test_automation.state_machine import TestStateMachine
 from ray_release.configs.global_config import get_global_config
 
@@ -45,7 +45,7 @@ def main(production: bool, check: bool) -> None:
             logger.info(f"\t- Team {team} has {num_blocker} release blockers")
 
         boto3.client("s3").put_object(
-            Bucket=get_global_config()["state_machine_master_aws_bucket"],
+            Bucket=get_state_machine_aws_bucket(),
             Key=f"{AWS_WEEKLY_GREEN_METRIC}/blocker_{int(time.time() * 1000)}.json",
             Body=json.dumps(num_blocker_by_team),
         )
