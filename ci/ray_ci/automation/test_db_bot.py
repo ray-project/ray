09aa636fd8fe1998b8beeb8d30fbfbd86fddff54
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2024-03-28 12:30:42 -0700

    [ci][config/2] move ci to use config under ci directory (#44313)
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/automation/test_db_bot.py
+++ ci/ray_ci/automation/test_db_bot.py
@@ -2,18 +2,16 @@ import os
 
 import click
 
-from ci.ray_ci.utils import logger
+from ci.ray_ci.utils import logger, ci_init
 from ci.ray_ci.tester_container import TesterContainer
-from ray_release.configs.global_config import init_global_config
 from release.ray_release.configs.global_config import BRANCH_PIPELINES
-from ray_release.bazel import bazel_runfile
 
 
 @click.command()
 @click.argument("team", required=True, type=str)
 @click.argument("bazel_log_dir", required=True, type=str)
 def main(team: str, bazel_log_dir: str) -> None:
-    init_global_config(bazel_runfile("release/ray_release/configs/oss_config.yaml"))
+    ci_init()
 
     if os.environ.get("BUILDKITE_BRANCH") != "master":
         logger.info("Skip upload test results. We only upload on master branch.")
