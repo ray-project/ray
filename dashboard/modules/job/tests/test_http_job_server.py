088c8754b2f3643254839da1c255f1395501f42f
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2024-04-01 08:43:27 -0700

    Update log assertion in test

--- dashboard/modules/job/tests/test_http_job_server.py
+++ dashboard/modules/job/tests/test_http_job_server.py
@@ -377,7 +377,7 @@ def test_timeout(job_sdk_client):
     data = client.get_job_info(job_id)
     assert "Failed to set up runtime environment" in data.message
     assert "Timeout" in data.message
-    assert "consider increasing `setup_timeout_seconds`" in data.message
+    assert "setup_timeout_seconds" in data.message
 
 
 def test_per_task_runtime_env(job_sdk_client: JobSubmissionClient):
