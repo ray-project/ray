a417708b595a7fa138a494dc7142a911fd6e48f3
Alexey Kudinkin <ak@anyscale.com>
2024-04-01 18:14:10 -0700

    Cleaning up
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/_private/async_compat.py
+++ python/ray/_private/async_compat.py
@@ -31,14 +31,3 @@ def is_async_func(func):
     """Return True if the function is an async or async generator method."""
     return inspect.iscoroutinefunction(func) or inspect.isasyncgenfunction(func)
 
-
-def sync_to_async(func):
-    """Convert a blocking function to async function"""
-
-    if is_async_func(func):
-        return func
-
-    async def wrapper(*args, **kwargs):
-        return func(*args, **kwargs)
-
-    return wrapper
