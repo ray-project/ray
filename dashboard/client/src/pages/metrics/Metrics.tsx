ray-project/ray

commit 2f7d191bad184730a3c02efe91d8358b9402d331

[Dashboard] Upgrade to MUIv5 (#42454)
I followed the guide here: https://mui.com/material-ui/migration/migration-v4/
This will unblock us from upgrading to React 18

Mostly to resolve tech debt.

There are some visual changes:

Links are underlined by default
Input boxes are "outlined" variant by default

---------

Signed-off-by: Alan Guo <aguo@anyscale.com>

dashboard/client/src/pages/metrics/Metrics.tsx
@@ -1,14 +1,16 @@
 import {
+  Alert,
+  AlertProps,
   Button,
-  createStyles,
-  makeStyles,
+  Link,
   Menu,
   MenuItem,
   Paper,
   TextField,
   Tooltip,
-} from "@material-ui/core";
-import { Alert, AlertProps } from "@material-ui/lab";
+} from "@mui/material";
+import createStyles from "@mui/styles/createStyles";
+import makeStyles from "@mui/styles/makeStyles";
 import React, { useContext, useEffect, useState } from "react";
 import { RiExternalLinkLine } from "react-icons/ri";
 
@@ -438,6 +440,7 @@ export const Metrics = () => {
               onChange={({ target: { value } }) => {
                 setTimeRangeOption(value as TimeRangeOptions);
               }}
+              variant="standard"
             >
               {Object.entries(TimeRangeOptions).map(([key, value]) => (
                 <MenuItem key={key} value={value}>
@@ -496,7 +499,7 @@ const useMetricsSectionStyles = makeStyles((theme) =>
       overflow: "hidden",
       [theme.breakpoints.up("md")]: {
         // Calculate max width based on 1/3 of the total width minus padding between cards
-        width: `calc((100% - ${theme.spacing(3)}px * 2) / 3)`,
+        width: `calc((100% - ${theme.spacing(3)} * 2) / 3)`,
       },
     },
     grafanaEmbed: {
@@ -540,7 +543,6 @@ const MetricsSection = ({
             <Paper
               key={pathParams}
               className={classes.chart}
-              elevation={1}
               variant="outlined"
             >
               <iframe
@@ -586,13 +588,13 @@ export const GrafanaNotRunningAlert = ({
       <br />
       Time-series charts are hidden because either Prometheus or Grafana server
       is not detected. Follow{" "}
-      <a
+      <Link
         href="https://docs.ray.io/en/latest/cluster/metrics.html"
         target="_blank"
         rel="noreferrer"
       >
         these instructions
-      </a>{" "}
+      </Link>{" "}
       to set them up and refresh this page.
     </Alert>
   ) : null;

dashboard/client/src/pages/serve/ServeMetricsSection.tsx
@@ -1,11 +1,6 @@
-import {
-  Button,
-  createStyles,
-  makeStyles,
-  MenuItem,
-  Paper,
-  TextField,
-} from "@material-ui/core";
+import { Button, MenuItem, Paper, TextField } from "@mui/material";
+import createStyles from "@mui/styles/createStyles";
+import makeStyles from "@mui/styles/makeStyles";
 import React, { useContext, useEffect, useState } from "react";
 import { RiExternalLinkLine } from "react-icons/ri";
 import { GlobalContext } from "../../App";
@@ -33,7 +28,7 @@ const useStyles = makeStyles((theme) =>
       overflow: "hidden",
       [theme.breakpoints.up("md")]: {
         // Calculate max width based on 1/3 of the total width minus padding between cards
-        width: `calc((100% - ${theme.spacing(3)}px * 2) / 3)`,
+        width: `calc((100% - ${theme.spacing(3)} * 2) / 3)`,
       },
     },
     grafanaEmbed: {
@@ -170,7 +165,6 @@ export const ServeMetricsSection = ({
               <Paper
                 key={pathParams}
                 className={classes.chart}
-                elevation={1}
                 variant="outlined"
               >
                 <iframe

commit 6e10ca79bb623a0004c8ad5300a3f5d8aa2c4eb3

[Dashboard] fix some visual bugs from the MUI v5 upgrade (#44462)
Fixes some bugs introduced from #42454

Signed-off-by: Alan Guo <aguo@anyscale.com>

dashboard/client/src/pages/metrics/Metrics.tsx
@@ -544,6 +544,7 @@ const MetricsSection = ({
               key={pathParams}
               className={classes.chart}
               variant="outlined"
+              elevation={0}
             >
               <iframe
                 key={title}

dashboard/client/src/pages/serve/ServeMetricsSection.tsx
@@ -1,4 +1,4 @@
-import { Button, MenuItem, Paper, TextField } from "@mui/material";
+import { Box, Button, MenuItem, Paper, TextField } from "@mui/material";
 import createStyles from "@mui/styles/createStyles";
 import makeStyles from "@mui/styles/makeStyles";
 import React, { useContext, useEffect, useState } from "react";
@@ -130,7 +130,7 @@ export const ServeMetricsSection = ({
   return grafanaHost === undefined || !prometheusHealth ? null : (
     <CollapsibleSection className={className} title="Metrics" startExpanded>
       <div>
-        <Paper className={classes.topBar}>
+        <Box className={classes.topBar}>
           <Button
             href={`${grafanaHost}/d/${grafanaServeDashboardUid}?var-datasource=${dashboardDatasource}`}
             target="_blank"
@@ -155,7 +155,7 @@ export const ServeMetricsSection = ({
               </MenuItem>
             ))}
           </TextField>
-        </Paper>
+        </Box>
         <div className={classes.grafanaEmbedsContainer}>
           {metricsConfig.map(({ title, pathParams }) => {
             const path =
@@ -166,6 +166,7 @@ export const ServeMetricsSection = ({
                 key={pathParams}
                 className={classes.chart}
                 variant="outlined"
+                elevation={0}
               >
                 <iframe
                   key={title}