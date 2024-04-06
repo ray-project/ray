5158ad4e33ae9f87d8b810e59a76d6bf14727db6
Balaji Veeramani <balaji@anyscale.com>
2024-04-05 17:39:20 -0700

    Add docs
    Signed-off-by: Balaji Veeramani <balaji@anyscale.com>

--- python/ray/data/_internal/logging.py
+++ python/ray/data/_internal/logging.py
@@ -5,12 +5,22 @@ from typing import Optional
 import ray
 from ray._private.ray_constants import LOGGER_FORMAT
 
-_default_log_handler: logging.Handler = None
+_default_file_handler: logging.Handler = None
 
 DEFAULT_DATASET_LOG_FILENAME = "ray-data.log"
 
 
-class LogHandler(logging.Handler):
+class SessionFileHandler(logging.Handler):
+    """A handler that writes to a log file in the Ray session directory.
+
+    The Ray session directory isn't available until Ray is initialized, so this handler
+    lazily creates the file handler when you emit a log record.
+
+    Args:
+        filename: The name of the log file. The file is created in the 'logs' directory
+            of the Ray session directory.
+    """
+
     def __init__(self, filename: str):
         super().__init__()
         self._filename = filename
@@ -42,23 +52,32 @@ class LogHandler(logging.Handler):
 
     @property
     def path(self) -> Optional[str]:
+        """Path to the log file or ``None`` if the file hasn't been created yet."""
         return self._path
 
 
-def configure_logging():
-    global _default_log_handler
-    assert _default_log_handler is None, "Logging already configured."
+def configure_logging() -> None:
+    """Configure the Python logger named 'ray.data'.
+
+    This function configures the logger to write Ray Data logs to a file in the Ray
+    session directory.
+
+    Only call this function once. It'll fail if you call it twice.
+    """
+    # Save the file handler in a global variable so that we can get the log path later.
+    global _default_file_handler
+    assert _default_file_handler is None, "Logging already configured."
 
     logger = logging.getLogger("ray.data")
     logger.setLevel(logging.INFO)
 
     formatter = logging.Formatter(fmt=LOGGER_FORMAT)
-    log_handler = LogHandler(DEFAULT_DATASET_LOG_FILENAME)
-    log_handler.setFormatter(formatter)
-    logger.addHandler(log_handler)
+    file_handler = SessionFileHandler(DEFAULT_DATASET_LOG_FILENAME)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
-    _default_log_handler = log_handler
+    _default_file_handler = file_handler
 
 
 def get_log_path() -> Optional[str]:
-    return _default_log_handler.path
+    return _default_file_handler.path
