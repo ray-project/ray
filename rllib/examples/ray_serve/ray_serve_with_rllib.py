683bc4b56686962d85174d16276b0d4027a86067
sven1977 <svenmika1977@gmail.com>
2024-04-08 17:07:40 +0200

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/examples/ray_serve/ray_serve_with_rllib.py
+++ rllib/examples/ray_serve/ray_serve_with_rllib.py
@@ -1,119 +1,125 @@
-"""This example script shows how one can use Ray Serve to serve an already
-trained RLlib Policy (and its model) to serve action computations.
+"""This example script shows how one can use Ray Serve in combination with RLlib.
 
+Here, we serve an already trained PyTorch RLModule to provide action computations
+to a Ray Serve client.
 """
 
 import argparse
-import gymnasium as gym
+import atexit
 import requests
-from starlette.requests import Request
+import subprocess
+import time
 
-import ray
-import ray.rllib.algorithms.algorithm as Algorithm
-import ray.rllib.algorithms.algorithm_config as AlgorithmConfig
-from ray.rllib.algorithms.dqn import DQNConfig
-from ray.rllib.env.wrappers.atari_wrappers import FrameStack, WarpFrame
-from ray import serve
+import gymnasium as gym
+
+from ray.rllib.algorithms.algorithm import AlgorithmConfig
+from ray.rllib.algorithms.ppo import PPOConfig
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--framework",
-    choices=["tf", "tf2", "torch"],
-    default="torch",
-    help="The DL framework specifier.",
-)
-parser.add_argument("--train-iters", type=int, default=1)
+parser.add_argument("--train-iters", type=int, default=3)
+parser.add_argument("--serve-episodes", type=int, default=2)
 parser.add_argument("--no-render", action="store_true")
 
-args = parser.parse_args()
 
-
-class ServeRLlibPolicy:
-    """Callable class used by Ray Serve to handle async requests.
-
-    All the necessary serving logic is implemented in here:
-    - Creation and restoring of the (already trained) RLlib Algorithm.
-    - Calls to algo.compute_action upon receiving an action request
-      (with a current observation).
-    """
-
-    def __init__(self, checkpoint_path):
-        # Create the Algorithm from the given checkpoint.
-        self.algo = Algorithm.from_checkpoint(checkpoint_path)
-
-    async def __call__(self, request: Request):
-        json_input = await request.json()
-
-        # Compute and return the action for the given observation.
-        obs = json_input["observation"]
-        action = self.algo.compute_single_action(obs)
-
-        return {"action": int(action)}
-
-
-def train_rllib_policy(config: AlgorithmConfig):
-    """Trains a DQN on ALE/MsPacman-v5 for n iterations.
+def train_rllib_rl_module(config: AlgorithmConfig, train_iters: int = 1):
+    """Trains a PPO (RLModule) on ALE/MsPacman-v5 for n iterations.
 
     Saves the trained Algorithm to disk and returns the checkpoint path.
 
     Args:
         config: The algo config object for the Algorithm.
+        train_iters: For how many iterations to train the Algorithm.
 
     Returns:
-        str: The saved checkpoint to restore DQN from.
+        str: The saved checkpoint to restore the RLModule from.
     """
     # Create algorithm from config.
     algo = config.build()
 
     # Train for n iterations, then save, stop, and return the checkpoint path.
-    for _ in range(args.train_iters):
+    for _ in range(train_iters):
         print(algo.train())
+
+    # TODO (sven): Change this example to only storing the RLModule checkpoint, NOT
+    #  the entire Algorithm.
     checkpoint_result = algo.save()
+
     algo.stop()
-    return checkpoint_result
+
+    return checkpoint_result.checkpoint
+
+
+def kill_proc(proc):
+    try:
+        proc.terminate()  # Send SIGTERM
+        proc.wait(timeout=5)  # Wait for process to terminate
+    except subprocess.TimeoutExpired:
+        proc.kill()  # Send SIGKILL
+        proc.wait()  # Ensure process is dead
 
 
 if __name__ == "__main__":
+    args = parser.parse_args()
 
-    # Config for the served RLlib Policy/Algorithm.
-    config = DQNConfig().environment("ALE/MsPacman-v5").framework(args.framework)
-
-    # Train the Algorithm for some time, then save it and get the checkpoint path.
-    checkpoint_result = train_rllib_policy(config)
-
-    ray.init(num_cpus=8)
-
-    # Start Ray serve (create the RLlib Policy service defined by
-    # our `ServeRLlibPolicy` class above).
-    client = serve.start()
-    client.create_backend("backend", ServeRLlibPolicy, config, checkpoint_result)
-    client.create_endpoint(
-        "endpoint", backend="backend", route="/mspacman-rllib-policy"
+    # Config for the served RLlib RLModule/Algorithm.
+    config = (
+        PPOConfig().experimental(_enable_new_api_stack=True).environment("CartPole-v1")
     )
 
-    # Create the environment that we would like to receive
-    # served actions for.
-    env = FrameStack(WarpFrame(gym.make("ALE/MsPacman-v5"), 84), 4)
-    obs, info = env.reset()
+    # Train the Algorithm for some time, then save it and get the checkpoint path.
+    checkpoint = train_rllib_rl_module(config, train_iters=args.train_iters)
 
-    while True:
-        print("-> Requesting action for obs ...")
-        # Send a request to serve.
-        resp = requests.get(
-            "http://localhost:8000/mspacman-rllib-policy",
-            json={"observation": obs.tolist()},
-        )
-        response = resp.json()
-        print("<- Received response {}".format(response))
+    # Start the serve app with the trained checkpoint.
+    serve_proc = subprocess.Popen(
+        [
+            "serve",
+            "run",
+            "classes.cartpole_deployment:rl_module",
+            f"checkpoint={checkpoint.path}",
+        ]
+    )
+    # Register our `kill_proc` function to be called on exit to stop Ray Serve again.
+    atexit.register(kill_proc, serve_proc)
+    # Wait a while to make sure the app is ready to serve.
+    time.sleep(20)
+    print(f"Started Ray Serve with PID: {serve_proc.pid}")
 
-        # Apply the action in the env.
-        action = response["action"]
-        obs, reward, done, _, _ = env.step(action)
+    try:
+        # Create the environment that we would like to receive
+        # served actions for.
+        env = gym.make("CartPole-v1", render_mode="human")
+        obs, info = env.reset()
 
-        # If episode done -> reset to get initial observation of new episode.
-        if done:
-            obs, info = env.reset()
+        num_episodes = 0
+        episode_return = 0.0
 
-        # Render if necessary.
-        if not args.no_render:
-            env.render()
+        while num_episodes < args.serve_episodes:
+            # Render env if necessary.
+            if not args.no_render:
+                env.render()
+
+            # print("-> Requesting action for obs ...")
+            # Send a request to serve.
+            resp = requests.get(
+                "http://localhost:8000/rllib-rlmodule",
+                json={"observation": obs.tolist()},
+                # timeout=5.0,
+            )
+            response = resp.json()
+            # print("<- Received response {}".format(response))
+
+            # Apply the action in the env.
+            action = response["action"]
+            obs, reward, done, _, _ = env.step(action)
+            episode_return += reward
+
+            # If episode done -> reset to get initial observation of new episode.
+            if done:
+                print(f"Episode R={episode_return}")
+                obs, info = env.reset()
+                num_episodes += 1
+                episode_return = 0.0
+
+    finally:
+        # Make sure to kill the process on script termination
+        kill_proc(serve_proc)
