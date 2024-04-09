3f48f50c061b1e2fe27fbfaab5dd81cb62c1f4bc
woshiyyya <xiaoyunxuan1998@gmail.com>
2024-04-08 19:03:06 -0700

    fix
    Signed-off-by: woshiyyya <xiaoyunxuan1998@gmail.com>

--- python/ray/train/_internal/schema.py
+++ python/ray/train/_internal/schema.py
@@ -1,16 +1,19 @@
-from pydantic import BaseModel
-from typing import Dict, List, Optional
-from ray._private.pydantic_compat import BaseModel
+from typing import List, Optional
+
+from ray._private.pydantic_compat import BaseModel, Field
+from ray.util.annotations import DeveloperAPI
+
 
 @DeveloperAPI
 class TrainWorkerInfo(BaseModel):
     """Metadata of a Ray Train worker."""
+
     actor_id: str = Field(description="Actor ID of the worker.")
     world_rank: int = Field(description="World rank.")
     local_rank: int = Field(description="Local rank.")
     node_rank: int = Field(description="Node rank.")
     gpu_ids: Optional[List[str]] = Field(
-        description= "A list of GPU ids allocated to that worker."
+        description="A list of GPU ids allocated to that worker."
     )
     node_id: Optional[str] = Field(
         description="ID of the node that the worker is running on."
@@ -24,11 +27,12 @@ class TrainWorkerInfo(BaseModel):
 @DeveloperAPI
 class TrainRunInfo(BaseModel):
     """Metadata for a Ray Train run and information about its workers."""
+
     name: str = Field(description="The name of the Train run.")
     id: str = Field(description="The unique identifier for each Train run.")
     job_id: str = Field(description="Ray Job ID.")
     trial_name: str = Field(
-        description= (
+        description=(
             "Trial name. It should be different among different Train runs, "
             "except for those that are restored from checkpoints."
         )
