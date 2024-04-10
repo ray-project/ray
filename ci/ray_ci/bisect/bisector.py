f922000962df9fe758fb81c6a55d1f7064c8c447
can <can@anyscale.com>
2024-04-10 03:01:31 +0000

    [ci] inferface for bisector
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/bisect/bisector.py
+++ ci/ray_ci/bisect/bisector.py
@@ -1,21 +1,25 @@
-import abc
+import os
+import subprocess
+from typing import List, Optional
+
+from ci.ray_ci.utils import logger
+from ci.ray_ci.bisect.validator import Validator
 
 
-from  ci.utils import logger
-
-
-class Bisector(abc.ABC):
+class Bisector:
     def __init__(
         self,
         test: str,
         passing_revision: str,
         failing_revision: str,
+        validator: Validator,
     ) -> None:
         self.test = test
         self.passing_revision = passing_revision
         self.failing_revision = failing_revision
+        self.validator = validator
 
-    def run() -> Optional[str]:
+    def run(self) -> Optional[str]:
         """
         Find the blame revision for the test given the range of passing and failing
         revision. If a blame cannot be found, return None
@@ -29,28 +33,38 @@ class Bisector(abc.ABC):
                 f"{revisions[0]} to {revisions[-1]}"
             )
             mid = len(revisions) // 2
-            if self.validate(revisions[mid]):
+            if self._checkout_and_validate(revisions[mid]):
                 revisions = revisions[mid:]
             else:
                 revisions = revisions[:mid]
 
         return revisions[-1]
 
-    def _get_revision_lists() -> List[str]:
+    def _get_revision_lists(self) -> List[str]:
         return (
             subprocess.check_output(
-                f"git rev-list --reverse "
-                f"^{self.passing_revision}~ {self.failing_revision}",
-                shell=True,
+                [
+                    "git",
+                    "rev-list",
+                    "--reverse",
+                    f"^{self.passing_revision}~",
+                    self.failing_revision,
+                ],
+                cwd=os.environ["RAYCI_CHECKOUT_DIR"],
             )
             .decode("utf-8")
             .strip()
             .split("\n")
         )
 
-    @abc.abstractmethod
-    def validate(self, revision: str) -> bool:
+    def _checkout_and_validate(self, revision: str) -> bool:
         """
         Validate whether the test is passing or failing on the given revision
         """
-        pass
+        subprocess.check_call(
+            ["git", "clean", "-df"], cwd=os.environ["RAYCI_CHECKOUT_DIR"]
+        )
+        subprocess.check_call(
+            ["git", "checkout", revision], cwd=os.environ["RAYCI_CHECKOUT_DIR"]
+        )
+        self.validator.run(self.test)
