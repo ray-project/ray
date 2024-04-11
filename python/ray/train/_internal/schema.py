c273413eeb10f7d0e7f5118428592b1fbeaa03ff
woshiyyya <xiaoyunxuan1998@gmail.com>
2024-04-10 18:44:15 -0700

    update
    Signed-off-by: woshiyyya <xiaoyunxuan1998@gmail.com>

--- python/ray/train/_internal/schema.py
+++ python/ray/train/_internal/schema.py
@@ -12,31 +12,29 @@ class TrainWorkerInfo(BaseModel):
     world_rank: int = Field(description="World rank.")
     local_rank: int = Field(description="Local rank.")
     node_rank: int = Field(description="Node rank.")
+    node_id: str = Field(description="ID of the node that the worker is running on.")
+    node_ip: str = Field(
+        description="IP address of the node that the worker is running on."
+    )
+    pid: str = Field(description="PID of the worker.")
     gpu_ids: Optional[List[str]] = Field(
         description="A list of GPU ids allocated to that worker."
     )
-    node_id: Optional[str] = Field(
-        description="ID of the node that the worker is running on."
-    )
-    node_ip: Optional[str] = Field(
-        description="IP address of the node that the worker is running on."
-    )
-    pid: Optional[str] = Field(description="PID of the worker.")
 
 
 @DeveloperAPI
 class TrainRunInfo(BaseModel):
     """Metadata for a Ray Train run and information about its workers."""
 
-    name: str = Field(description="The name of the Train run.")
     id: str = Field(description="The unique identifier for each Train run.")
-    job_id: str = Field(description="Ray Job ID.")
-    trial_name: str = Field(
+    name: str = Field(description="The name of the Train run.")
+    trial_name: Optional[str] = Field(
         description=(
             "Trial name. It should be different among different Train runs, "
             "except for those that are restored from checkpoints."
         )
     )
+    job_id: str = Field(description="The Ray Job ID.")
     trainer_actor_id: str = Field(description="Actor Id of the Trainer.")
     workers: List[TrainWorkerInfo] = Field(
         description="A List of Train workers sorted by global ranks."
