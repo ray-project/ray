4f13fcedfa6eeb0fc82be1bb00c699c85c280ac6
Raymond Lee <lee1258561@gmail.com>
2024-04-12 11:38:15 -0700

    slight modification of version

--- python/ray/_version.py
+++ python/ray/_version.py
@@ -1,6 +1,6 @@
 # Replaced with the current commit when building the wheels.
 commit = "{{RAY_COMMIT_SHA}}"
-version = "2.9.3pinterest1"
+version = "2.9.3+pinterest1"
 
 if __name__ == "__main__":
     print("%s %s" % (version, commit))
