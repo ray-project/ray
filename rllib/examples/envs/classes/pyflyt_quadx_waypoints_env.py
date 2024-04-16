4c13d0535e48df9e853cb87675eb396fff5cebc7
peterghaddad <peter.g.haddad@lmco.com>
2024-04-16 17:51:36 -0600

    lint

--- rllib/examples/envs/classes/pyflyt_quadx_waypoints_env.py
+++ rllib/examples/envs/classes/pyflyt_quadx_waypoints_env.py
@@ -1,10 +1,5 @@
-from ray.tune.registry import register_env
-from gymnasium.wrappers import RecordVideo
 from PyFlyt.gym_envs import FlattenWaypointEnv
-from gymnasium.wrappers import TransformReward
-
 import gymnasium as gym
-import PyFlyt.gym_envs  # noqa
 
 
 class RewardWrapper(gym.RewardWrapper):
@@ -19,10 +14,7 @@ class RewardWrapper(gym.RewardWrapper):
 
 
 class QuadXWayPointsEnv(gym.Env):
-    from gymnasium.wrappers import RecordVideo
-    import PyFlyt.gym_envs  # Must be here
-    from PyFlyt.gym_envs import FlattenWaypointEnv
-    from gymnasium.wrappers import TransformReward
+    import PyFlyt.gym_envs  # noqa
 
     def __init__(self, config=None):
         env = gym.make("PyFlyt/QuadX-Waypoints-v1")
