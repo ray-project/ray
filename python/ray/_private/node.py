65e580a89855403699ff3385cbde11f526addf70
Rui Qiao <ruisearch42@gmail.com>
2024-04-16 11:43:14 -0700

    format
    Signed-off-by: Rui Qiao <ruisearch42@gmail.com>

--- python/ray/_private/node.py
+++ python/ray/_private/node.py
@@ -286,9 +286,7 @@ class Node:
                 self._node_id = node_id
             elif os.environ.get("RAY_OVERRIDE_NODE_ID_FOR_TESTING"):
                 node_id = os.environ["RAY_OVERRIDE_NODE_ID_FOR_TESTING"]
-                logger.debug(
-                    f"Setting node ID to {node_id} based on env override"
-                )
+                logger.debug(f"Setting node ID to {node_id} based on env override")
                 self._node_id = self._ray_params.node_id
             else:
                 node_id = ray.NodeID.from_random().hex()
