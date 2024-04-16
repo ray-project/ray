df178fc697808349daee81f546c8da3d4aa6b1d7
can <can@anyscale.com>
2024-04-16 20:04:24 +0000

    [ci]
    Signed-off-by: can <can@anyscale.com>

--- release/ray_release/test_automation/release_state_machine.py
+++ release/ray_release/test_automation/release_state_machine.py
@@ -1,6 +1,8 @@
 from ray_release.test_automation.state_machine import (
     TestStateMachine,
     WEEKLY_RELEASE_BLOCKER_TAG,
+    BUILDKITE_BISECT_PIPELINE,
+    BUILDKITE_ORGANIZATION,
 )
 from ray_release.test import Test, TestState
 
