0a3a40f6ddb3485e6b13e07ebba326573a992ced
can <can@anyscale.com>
2024-04-17 03:15:31 +0000

    [ci][bisect/2] bisect pipeline for macos tests
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/bisect/macos_validator.py
+++ ci/ray_ci/bisect/macos_validator.py
@@ -10,10 +10,15 @@ TEST_SCRIPT = "ci/ray_ci/bisect/macos_validator.sh"
 
 class MacOSValidator(Validator):
     def run(self, test: str) -> bool:
+        env = os.environ.copy()
+        # We need to unset PYTHONPATH to avoid conflicts with the Python from the
+        # Bazel runfiles.
+        env.update({"RAYCI_BISECT_RUN": "1", "PYTHONPATH": ""})
         return (
             subprocess.run(
                 [f"{bazel_runfile(TEST_SCRIPT)}", "run_tests", test],
                 cwd=os.environ["RAYCI_CHECKOUT_DIR"],
+                env=env,
             ).returncode
             == 0
         )
