2185fa9cbd633a5ae20f798e7da044db34335d3b
WeichenXu <weichen.xu@databricks.com>
2024-04-19 01:53:28 +0800

    [spark] Fix Ray-on-Spark ray child process shebang issue (#44827)
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- python/ray/util/spark/start_ray_node.py
+++ python/ray/util/spark/start_ray_node.py
@@ -62,7 +62,16 @@ if __name__ == "__main__":
     # same temp directory, adding a shared lock representing current ray node is
     # using the temp directory.
     fcntl.flock(lock_fd, fcntl.LOCK_SH)
-    process = subprocess.Popen([ray_cli_cmd, "start", *arg_list], text=True)
+    process = subprocess.Popen(
+        # 'ray start ...' command uses python that is set by
+        # Shebang #! ..., the Shebang line is hardcoded in ray script,
+        # it can't be changed to other python executable path.
+        # to enforce using current python executable,
+        # turn the subprocess command to
+        # '`sys.executable` `which ray` start ...'
+        [sys.executable, shutil.which(ray_cli_cmd), "start", *arg_list],
+        text=True,
+    )
 
     def try_clean_temp_dir_at_exit():
         try:
