ray-project/ray

commit d015cf2adb43386cdd03a46e57b050f3a13d750b

Revert "Add proprietary tracing " (#561)
Reverts anyscale/runtime#497

Broke a template test:
https://buildkite.com/anyscale/runtime-release/builds/639. Confirm the
revert fixes the tests:
https://buildkite.com/anyscale/runtime-release/builds/645

python/ray/anyscale/serve/BUILD
@@ -21,7 +21,6 @@ py_test_module_list(
     "tests/test_draining.py",
     "tests/test_az_routing.py",
     "tests/test_centralized_scheduler.py",
-    "tests/test_tracing_utils.py",
   ],
   size = "medium",
   tags = ["team:serve", "exclusive"],

python/ray/anyscale/serve/_private/constants.py
@@ -15,13 +15,3 @@ ANYSCALE_RAY_SERVE_DEFAULT_DRAINING_TIMEOUT_S = float(
 ANYSCALE_RAY_SERVE_COMPACTION_TIMEOUT_S = float(
     os.environ.get("ANYSCALE_RAY_SERVE_COMPACTION_TIMEOUT_S", 1800.0)
 )
-
-DEFAULT_TRACING_EXPORTER_IMPORT_PATH = (
-    "ray.anyscale.serve._private.tracing_utils:default_tracing_exporter"
-)
-# Path to tracing exporter function
-# If None, then use default tracing exporter
-# If empty string, then tracing is disabled
-ANYSCALE_TRACING_EXPORTER_IMPORT_PATH = os.environ.get(
-    "ANYSCALE_TRACING_EXPORTER_IMPORT_PATH", DEFAULT_TRACING_EXPORTER_IMPORT_PATH
-)

python/ray/anyscale/serve/_private/tracing_utils.py
@@ -1,107 +1,0 @@
-import inspect
-import os
-from typing import Any, Callable, List
-
-from ray._private.utils import import_attr
-from ray.anyscale.serve._private.constants import (
-    ANYSCALE_TRACING_EXPORTER_IMPORT_PATH,
-    DEFAULT_TRACING_EXPORTER_IMPORT_PATH,
-)
-from ray.serve._private.logging_utils import get_serve_logs_dir
-
-try:
-    from opentelemetry import trace
-    from opentelemetry.sdk.trace import TracerProvider
-    from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
-except ImportError:
-    ConsoleSpanExporter = None
-    SimpleSpanProcessor = None
-    trace = None
-    TracerProvider = None
-
-
-# Default tracing exporter needs to map to DEFAULT_TRACING_EXPORTER_IMPORT_PATH
-# defined in "python/ray/anyscale/serve/_private/constants.py"
-def default_tracing_exporter(tracing_file_name):
-    serve_logs_dir = get_serve_logs_dir()
-    spans_dir = os.path.join(serve_logs_dir, "spans")
-    os.makedirs(spans_dir, exist_ok=True)
-    spans_file = os.path.join(spans_dir, tracing_file_name)
-
-    return [SimpleSpanProcessor(ConsoleSpanExporter(out=open(spans_file, "a")))]
-
-
-def _validate_tracing_exporter(func: Callable) -> None:
-    if inspect.isfunction(func) is False:
-        raise TypeError("Tracing exporter must be a function.")
-
-    signature = inspect.signature(func)
-
-    if len(signature.parameters) != 0:
-        raise TypeError("Tracing exporter cannot take any arguments.")
-
-
-def _validate_tracing_exporter_processors(span_processors: List[Any]):
-    if not isinstance(span_processors, list):
-        raise TypeError(
-            "Output of tracing exporter needs to be of type "
-            f"List[SimpleSpanProcessor], but received type {type(span_processors)}."
-        )
-
-    for span_processor in span_processors:
-        if not isinstance(span_processor, SimpleSpanProcessor):
-            raise TypeError(
-                "Output of tracing exporter needs to be of "
-                "type List[SimpleSpanProcessor], "
-                f"but received type {type(span_processor)}."
-            )
-
-
-def _load_span_processors(
-    tracing_exporter_import_path: str,
-    tracing_file_name: str,
-):
-    tracing_exporter_def = import_attr(tracing_exporter_import_path)
-
-    if tracing_exporter_import_path == DEFAULT_TRACING_EXPORTER_IMPORT_PATH:
-        return tracing_exporter_def(tracing_file_name)
-    else:
-        # Validate tracing exporter function
-        _validate_tracing_exporter(tracing_exporter_def)
-        # Validate tracing exporter processors
-        span_processors = tracing_exporter_def()
-        _validate_tracing_exporter_processors(span_processors)
-
-    return span_processors
-
-
-def setup_tracing(tracing_file_name: str) -> None:
-    # If exporter import path is empty, then shortcircuit
-    tracing_exporter_import_path = ANYSCALE_TRACING_EXPORTER_IMPORT_PATH
-
-    if tracing_exporter_import_path == "":
-        return
-
-    # Check dependencies
-    if (
-        not ConsoleSpanExporter
-        or not SimpleSpanProcessor
-        or not trace
-        or not TracerProvider
-    ):
-        raise ImportError(
-            "You must `pip install opentelemetry` and `pip install opentelemetry-sdk`"
-            "to enable tracing on Ray Serve."
-        )
-
-    span_processors = _load_span_processors(
-        tracing_exporter_import_path, tracing_file_name
-    )
-
-    # Intialize tracing
-    # Sets the tracer_provider. This is only allowed once per execution
-    # context and will log a warning if attempted multiple times.
-    trace.set_tracer_provider(TracerProvider())
-
-    for span_processor in span_processors:
-        trace.get_tracer_provider().add_span_processor(span_processor)

python/ray/serve/_private/replica.py
@@ -17,11 +17,6 @@ import ray
 from ray import cloudpickle
 from ray._private.utils import get_or_create_event_loop
 from ray.actor import ActorClass
-
-# ===== Begin Anyscale proprietary code ======
-from ray.anyscale.serve._private.tracing_utils import setup_tracing
-
-# ===== End Anyscale proprietary code ======
 from ray.remote_function import RemoteFunction
 from ray.serve import metrics
 from ray.serve._private.common import (
@@ -292,24 +287,6 @@ class ReplicaActor:
             self._event_loop,
             self._deployment_config.autoscaling_config,
         )
-
-        # ===== Begin Anyscale proprietary code ======
-        try:
-            tracing_file_name = get_component_file_name(
-                component_type=ServeComponentType.REPLICA,
-                component_name=self._component_name,
-                component_id=self._component_id,
-                suffix="_tracing.json",
-            )
-            setup_tracing(
-                tracing_file_name=tracing_file_name,
-            )
-        except Exception as e:
-            logger.warning(
-                f"Failed to set up tracing: {e}. "
-                "The replica will continue running, but traces will not be exported."
-            )
-        # ===== End Anyscale proprietary code ======
 
     def _set_internal_replica_context(self, *, servable_object: Callable = None):
         ray.serve.context._set_internal_replica_context(

python/ray/anyscale/serve/tests/test_tracing_utils.py
@@ -1,170 +1,0 @@
-import json
-import os
-import re
-import shutil
-from unittest.mock import patch
-
-import pytest
-import requests
-import starlette
-
-import ray
-from ray import serve
-from ray.anyscale.serve._private.tracing_utils import (
-    DEFAULT_TRACING_EXPORTER_IMPORT_PATH,
-    _load_span_processors,
-    _validate_tracing_exporter,
-    _validate_tracing_exporter_processors,
-    setup_tracing,
-)
-from ray.serve._private.logging_utils import get_serve_logs_dir
-from ray.serve.tests.conftest import *  # noqa
-from ray.tests.conftest import *  # noqa
-
-try:
-    from opentelemetry import trace
-    from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
-except ImportError:
-    raise ModuleNotFoundError(
-        "`opentelemetry` or `opentelemetry.sdk.trace.export` not found"
-    )
-
-
-def test_default_tracing_exporter(ray_start_cluster):
-
-    cluster = ray_start_cluster
-    cluster.add_node(num_cpus=1)
-    cluster.wait_for_nodes()
-
-    ray.init(address=cluster.address)
-
-    span_processors = _load_span_processors(
-        DEFAULT_TRACING_EXPORTER_IMPORT_PATH, "mock_file.json"
-    )
-
-    assert isinstance(span_processors, list)
-
-    for span_processor in span_processors:
-        assert isinstance(span_processor, SimpleSpanProcessor)
-
-
-def custom_tracing_exporter():
-    return [SimpleSpanProcessor(ConsoleSpanExporter(out=open("spans", "a")))]
-
-
-def test_custom_tracing_exporter():
-    custom_tracing_exporter = (
-        "ray.anyscale.serve.tests.test_tracing_utils:custom_tracing_exporter"
-    )
-
-    span_processors = _load_span_processors(custom_tracing_exporter, "mock_file.json")
-
-    assert isinstance(span_processors, list)
-
-    for span_processor in span_processors:
-        assert isinstance(span_processor, SimpleSpanProcessor)
-
-
-def test_validate_tracing_exporter_with_string():
-    invalid_exporters = [1, "string", []]
-    expected_exception = "Tracing exporter must be a function."
-
-    for invalid_exporter in invalid_exporters:
-        with pytest.raises(TypeError, match=expected_exception):
-            _validate_tracing_exporter(invalid_exporter)
-
-
-def test_validate_tracing_exporter_with_args():
-    def test_exporter(arg):
-        return arg
-
-    expected_exception = "Tracing exporter cannot take any arguments."
-
-    with pytest.raises(TypeError, match=expected_exception):
-        _validate_tracing_exporter(test_exporter)
-
-
-def test_validate_tracing_exporter_processors_list():
-    invalid_span_processors = [1, "string"]
-    for invalid_span_processor in invalid_span_processors:
-        expected_exception = re.escape(
-            "Output of tracing exporter needs to be of type "
-            "List[SimpleSpanProcessor], but received type "
-            f"{type(invalid_span_processor)}."
-        )
-        with pytest.raises(TypeError, match=expected_exception):
-            _validate_tracing_exporter_processors(invalid_span_processor)
-
-
-def test_validate_tracing_exporter_processors_full_output():
-    invalid_span_processors = [[1, 2], ["1", "2"]]
-    for invalid_span_processor in invalid_span_processors:
-        expected_exception = re.escape(
-            "Output of tracing exporter needs to be of "
-            "type List[SimpleSpanProcessor], "
-            f"but received type {type(invalid_span_processor[0])}."
-        )
-        with pytest.raises(TypeError, match=expected_exception):
-            _validate_tracing_exporter_processors(invalid_span_processor)
-
-
-def test_missing_dependencies():
-    expected_exception = (
-        "You must `pip install opentelemetry` and "
-        "`pip install opentelemetry-sdk`"
-        "to enable tracing on Ray Serve."
-    )
-    with patch(
-        "ray.anyscale.serve._private.tracing_utils.ConsoleSpanExporter", new=None
-    ):
-        with pytest.raises(ImportError, match=expected_exception):
-            setup_tracing("mock_file.json")
-
-
-@pytest.fixture
-def serve_and_ray_shutdown():
-    serve.shutdown()
-    ray.shutdown()
-    yield
-    serve.shutdown()
-
-
-def test_tracing_e2e(serve_and_ray_shutdown):
-    @serve.deployment
-    class Model:
-        def __call__(self, req: starlette.requests.Request):
-            replica_context = serve.get_replica_context()
-            tracer = trace.get_tracer(__name__)
-            with tracer.start_as_current_span("example_span") as span:
-                span.set_attribute("deployment_name", replica_context.deployment)
-                span.set_attribute("replica_id", replica_context.replica_id.unique_id)
-
-    serve.run(Model.bind())
-    requests.post("http://127.0.0.1:8000/")
-
-    serve_logs_dir = get_serve_logs_dir()
-    spans_dir = os.path.join(serve_logs_dir, "spans")
-
-    files = os.listdir(spans_dir)
-
-    assert len(files) == 1
-
-    tracing_file = files[0]
-
-    with open(os.path.join(spans_dir, tracing_file), "r") as file:
-        data = json.load(file)
-
-    assert "attributes" in data
-
-    attributes = data["attributes"]
-
-    assert "deployment_name" in attributes and attributes["deployment_name"] == "Model"
-    assert "replica_id" in attributes
-
-    shutil.rmtree(spans_dir)
-
-
-if __name__ == "__main__":
-    import sys
-
-    sys.exit(pytest.main(["-v", "-s", __file__]))

python/ray/serve/tests/test_request_timeout.py
@@ -196,7 +196,7 @@ def test_request_hangs_in_assignment(ray_instance, shutdown_serve):
 @pytest.mark.parametrize(
     "ray_instance",
     [
-        {"RAY_SERVE_REQUEST_PROCESSING_TIMEOUT_S": "5"},
+        {"RAY_SERVE_REQUEST_PROCESSING_TIMEOUT_S": "0.1"},
     ],
     indirect=True,
 )

commit 9f3196d2ecd27943221f54d1dfcba3fe76bdcdfd

Add proprietary tracing (#592)
Initialize tracing in Serve replica based on environment variables. 

Re-merging this PR https://github.com/anyscale/runtime/pull/497

Fixed requests latency regression from #561 by importing Opentelemetry
libraries lazily.

---------

Signed-off-by: Kamen Shah <kamenshah@gmail.com>

python/ray/anyscale/serve/BUILD
@@ -23,6 +23,7 @@ py_test_module_list(
     "tests/test_centralized_scheduler.py",
     "tests/test_telemetry.py",
     "tests/test_metrics.py",
+    "tests/test_tracing_utils.py",
   ],
   size = "medium",
   tags = ["team:serve", "exclusive"],

python/ray/anyscale/serve/_private/constants.py
@@ -15,3 +15,13 @@ ANYSCALE_RAY_SERVE_DEFAULT_DRAINING_TIMEOUT_S = float(
 ANYSCALE_RAY_SERVE_COMPACTION_TIMEOUT_S = float(
     os.environ.get("ANYSCALE_RAY_SERVE_COMPACTION_TIMEOUT_S", 1800.0)
 )
+
+DEFAULT_TRACING_EXPORTER_IMPORT_PATH = (
+    "ray.anyscale.serve._private.tracing_utils:default_tracing_exporter"
+)
+# Path to tracing exporter function
+# If None, then use default tracing exporter
+# If empty string, then tracing is disabled
+ANYSCALE_TRACING_EXPORTER_IMPORT_PATH = os.environ.get(
+    "ANYSCALE_TRACING_EXPORTER_IMPORT_PATH", DEFAULT_TRACING_EXPORTER_IMPORT_PATH
+)

python/ray/anyscale/serve/_private/tracing_utils.py
@@ -1,0 +1,120 @@
+import inspect
+import os
+from typing import Any, Callable, List
+
+from ray._private.utils import import_attr
+from ray.anyscale.serve._private.constants import (
+    ANYSCALE_TRACING_EXPORTER_IMPORT_PATH,
+    DEFAULT_TRACING_EXPORTER_IMPORT_PATH,
+)
+from ray.serve._private.common import ServeComponentType
+from ray.serve._private.logging_utils import get_serve_logs_dir
+from ray.serve._private.utils import get_component_file_name
+
+try:
+    from opentelemetry import trace
+    from opentelemetry.sdk.trace import TracerProvider
+    from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+except ImportError:
+    ConsoleSpanExporter = None
+    SimpleSpanProcessor = None
+    trace = None
+    TracerProvider = None
+
+
+# Default tracing exporter needs to map to DEFAULT_TRACING_EXPORTER_IMPORT_PATH
+# defined in "python/ray/anyscale/serve/_private/constants.py"
+def default_tracing_exporter(tracing_file_name):
+    serve_logs_dir = get_serve_logs_dir()
+    spans_dir = os.path.join(serve_logs_dir, "spans")
+    os.makedirs(spans_dir, exist_ok=True)
+    spans_file = os.path.join(spans_dir, tracing_file_name)
+
+    return [SimpleSpanProcessor(ConsoleSpanExporter(out=open(spans_file, "a")))]
+
+
+def _validate_tracing_exporter(func: Callable) -> None:
+    if inspect.isfunction(func) is False:
+        raise TypeError("Tracing exporter must be a function.")
+
+    signature = inspect.signature(func)
+
+    if len(signature.parameters) != 0:
+        raise TypeError("Tracing exporter cannot take any arguments.")
+
+
+def _validate_tracing_exporter_processors(span_processors: List[Any]):
+    if not isinstance(span_processors, list):
+        raise TypeError(
+            "Output of tracing exporter needs to be of type "
+            f"List[SimpleSpanProcessor], but received type {type(span_processors)}."
+        )
+
+    for span_processor in span_processors:
+        if not isinstance(span_processor, SimpleSpanProcessor):
+            raise TypeError(
+                "Output of tracing exporter needs to be of "
+                "type List[SimpleSpanProcessor], "
+                f"but received type {type(span_processor)}."
+            )
+
+
+def _load_span_processors(
+    tracing_exporter_import_path: str,
+    tracing_file_name: str,
+):
+    tracing_exporter_def = import_attr(tracing_exporter_import_path)
+
+    if tracing_exporter_import_path == DEFAULT_TRACING_EXPORTER_IMPORT_PATH:
+        return tracing_exporter_def(tracing_file_name)
+    else:
+        # Validate tracing exporter function
+        _validate_tracing_exporter(tracing_exporter_def)
+        # Validate tracing exporter processors
+        span_processors = tracing_exporter_def()
+        _validate_tracing_exporter_processors(span_processors)
+
+    return span_processors
+
+
+def setup_tracing(
+    component_type: ServeComponentType,
+    component_name: str,
+    component_id: str,
+) -> None:
+    # If exporter import path is empty, then shortcircuit
+    tracing_exporter_import_path = ANYSCALE_TRACING_EXPORTER_IMPORT_PATH
+
+    if tracing_exporter_import_path == "":
+        return
+
+    # Check dependencies
+    if (
+        not ConsoleSpanExporter
+        or not SimpleSpanProcessor
+        or not trace
+        or not TracerProvider
+    ):
+        raise ImportError(
+            "You must `pip install opentelemetry` and `pip install opentelemetry-sdk`"
+            "to enable tracing on Ray Serve."
+        )
+
+    tracing_file_name = get_component_file_name(
+        component_type=component_type,
+        component_name=component_name,
+        component_id=component_id,
+        suffix="_tracing.json",
+    )
+
+    span_processors = _load_span_processors(
+        tracing_exporter_import_path, tracing_file_name
+    )
+
+    # Intialize tracing
+    # Sets the tracer_provider. This is only allowed once per execution
+    # context and will log a warning if attempted multiple times.
+    trace.set_tracer_provider(TracerProvider())
+
+    for span_processor in span_processors:
+        trace.get_tracer_provider().add_span_processor(span_processor)

python/ray/serve/_private/replica.py
@@ -299,6 +299,27 @@ class ReplicaActor:
     def _configure_logger_and_profilers(
         self, logging_config: Union[None, Dict, LoggingConfig]
     ):
+
+        # ===== Begin Anyscale proprietary code ======
+        # Load the setup_tracing function lazily
+        # as Opentelemetry imports are time-consuming.
+        from ray.anyscale.serve._private.tracing_utils import setup_tracing
+
+        try:
+            setup_tracing(
+                component_type=ServeComponentType.REPLICA,
+                component_name=self._component_name,
+                component_id=self._component_id,
+            )
+            logger.info("Successfully setup tracing for replica")
+
+        except Exception as e:
+            logger.warning(
+                f"Failed to set up tracing: {e}. "
+                "The replica will continue running, but traces will not be exported."
+            )
+        # ===== End Anyscale proprietary code ======
+
         if logging_config is None:
             logging_config = {}
         if isinstance(logging_config, dict):

python/ray/anyscale/serve/tests/test_tracing_utils.py
@@ -1,0 +1,179 @@
+import json
+import os
+import re
+import shutil
+from unittest.mock import patch
+
+import pytest
+import requests
+import starlette
+
+import ray
+from ray import serve
+from ray.anyscale.serve._private.tracing_utils import (
+    DEFAULT_TRACING_EXPORTER_IMPORT_PATH,
+    _load_span_processors,
+    _validate_tracing_exporter,
+    _validate_tracing_exporter_processors,
+    setup_tracing,
+)
+from ray.serve._private.common import ServeComponentType
+from ray.serve._private.logging_utils import get_serve_logs_dir
+from ray.serve.tests.conftest import *  # noqa
+from ray.tests.conftest import *  # noqa
+
+try:
+    from opentelemetry import trace
+    from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+except ImportError:
+    raise ModuleNotFoundError(
+        "`opentelemetry` or `opentelemetry.sdk.trace.export` not found"
+    )
+
+
+def test_default_tracing_exporter(ray_start_cluster):
+
+    cluster = ray_start_cluster
+    cluster.add_node(num_cpus=1)
+    cluster.wait_for_nodes()
+
+    ray.init(address=cluster.address)
+
+    span_processors = _load_span_processors(
+        DEFAULT_TRACING_EXPORTER_IMPORT_PATH, "mock_file.json"
+    )
+
+    assert isinstance(span_processors, list)
+
+    for span_processor in span_processors:
+        assert isinstance(span_processor, SimpleSpanProcessor)
+
+
+def custom_tracing_exporter():
+    return [SimpleSpanProcessor(ConsoleSpanExporter(out=open("spans.txt", "a")))]
+
+
+def test_custom_tracing_exporter():
+    custom_tracing_exporter = (
+        "ray.anyscale.serve.tests.test_tracing_utils:custom_tracing_exporter"
+    )
+
+    span_processors = _load_span_processors(custom_tracing_exporter, "mock_file.json")
+
+    assert isinstance(span_processors, list)
+
+    for span_processor in span_processors:
+        assert isinstance(span_processor, SimpleSpanProcessor)
+
+    assert os.path.exists("spans.txt")
+
+    os.remove("spans.txt")
+
+
+def test_validate_tracing_exporter_with_string():
+    invalid_exporters = [1, "string", []]
+    expected_exception = "Tracing exporter must be a function."
+
+    for invalid_exporter in invalid_exporters:
+        with pytest.raises(TypeError, match=expected_exception):
+            _validate_tracing_exporter(invalid_exporter)
+
+
+def test_validate_tracing_exporter_with_args():
+    def test_exporter(arg):
+        return arg
+
+    expected_exception = "Tracing exporter cannot take any arguments."
+
+    with pytest.raises(TypeError, match=expected_exception):
+        _validate_tracing_exporter(test_exporter)
+
+
+def test_validate_tracing_exporter_processors_list():
+    invalid_span_processors = [1, "string"]
+    for invalid_span_processor in invalid_span_processors:
+        expected_exception = re.escape(
+            "Output of tracing exporter needs to be of type "
+            "List[SimpleSpanProcessor], but received type "
+            f"{type(invalid_span_processor)}."
+        )
+        with pytest.raises(TypeError, match=expected_exception):
+            _validate_tracing_exporter_processors(invalid_span_processor)
+
+
+def test_validate_tracing_exporter_processors_full_output():
+    invalid_span_processors = [[1, 2], ["1", "2"]]
+    for invalid_span_processor in invalid_span_processors:
+        expected_exception = re.escape(
+            "Output of tracing exporter needs to be of "
+            "type List[SimpleSpanProcessor], "
+            f"but received type {type(invalid_span_processor[0])}."
+        )
+        with pytest.raises(TypeError, match=expected_exception):
+            _validate_tracing_exporter_processors(invalid_span_processor)
+
+
+def test_missing_dependencies():
+    expected_exception = (
+        "You must `pip install opentelemetry` and "
+        "`pip install opentelemetry-sdk`"
+        "to enable tracing on Ray Serve."
+    )
+    with patch(
+        "ray.anyscale.serve._private.tracing_utils.ConsoleSpanExporter", new=None
+    ):
+        with pytest.raises(ImportError, match=expected_exception):
+            setup_tracing(
+                component_type=ServeComponentType.REPLICA,
+                component_name="component_name",
+                component_id="component_id",
+            )
+
+
+@pytest.fixture
+def serve_and_ray_shutdown():
+    serve.shutdown()
+    ray.shutdown()
+    yield
+    serve.shutdown()
+
+
+def test_tracing_e2e(serve_and_ray_shutdown):
+    @serve.deployment
+    class Model:
+        def __call__(self, req: starlette.requests.Request):
+            replica_context = serve.get_replica_context()
+            tracer = trace.get_tracer(__name__)
+            with tracer.start_as_current_span("example_span") as span:
+                span.set_attribute("deployment_name", replica_context.deployment)
+                span.set_attribute("replica_id", replica_context.replica_id.unique_id)
+
+    serve.run(Model.bind())
+    requests.post("http://127.0.0.1:8000/")
+
+    serve_logs_dir = get_serve_logs_dir()
+    spans_dir = os.path.join(serve_logs_dir, "spans")
+
+    files = os.listdir(spans_dir)
+
+    assert len(files) == 1
+
+    tracing_file = files[0]
+
+    with open(os.path.join(spans_dir, tracing_file), "r") as file:
+        data = json.load(file)
+
+    assert "attributes" in data
+
+    attributes = data["attributes"]
+
+    assert "deployment_name" in attributes and attributes["deployment_name"] == "Model"
+    assert "replica_id" in attributes
+
+    shutil.rmtree(spans_dir)
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(pytest.main(["-v", "-s", __file__]))