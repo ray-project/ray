04581aab0bb1a7fc7c52391390c48503d291b219
Simon Zehnder <simon.zehnder@gmail.com>
2024-04-29 11:23:18 +0200

    Changed episode states from list to dict.
    Signed-off-by: Simon Zehnder <simon.zehnder@gmail.com>

--- rllib/env/multi_agent_episode.py
+++ rllib/env/multi_agent_episode.py
@@ -1697,48 +1697,43 @@ class MultiAgentEpisode:
         Note that from an episode's state the episode itself can
         be recreated.
 
-        Returns: A dicitonary containing pickable data fro a
+        Returns: A dicitonary containing pickable data for a
             `MultiAgentEpisode`.
         """
-        # TODO (simon): Add the agent caches.
-        return list(
-            {
-                "id_": self.id_,
-                "agent_to_module_mapping_fn": self.agent_to_module_mapping_fn,
-                "_agent_to_module_mapping": self._agent_to_module_mapping,
-                "observation_space": self.observation_space,
-                "action_space": self.action_space,
-                "env_t_started": self.env_t_started,
-                "env_t": self.env_t,
-                "agent_t_started": self.agent_t_started,
-                # TODO (simon): Check, if we can store the `InfiniteLookbackBuffer`
-                "env_t_to_agent_t": self.env_t_to_agent_t,
-                "_hanging_actions_end": self._hanging_actions_end,
-                "_hanging_extra_model_outputs_end": (
-                    self._hanging_extra_model_outputs_end
-                ),
-                "_hanging_rewards_end": self._hanging_rewards_end,
-                "_hanging_actions_begin": self._hanging_actions_begin,
-                "_hanging_extra_model_outputs_begin": (
-                    self._hanging_extra_model_outputs_begin
-                ),
-                "_hanging_rewards_begin": self._hanging_rewards_begin,
-                "is_terminated": self.is_terminated,
-                "is_truncated": self.is_truncated,
-                "agent_episodes": list(
-                    {
-                        agent_id: agent_eps.get_state()
-                        for agent_id, agent_eps in self.agent_episodes.items()
-                    }.items()
-                ),
-                "render_images": self.render_images,
-                "_start_time": self._start_time,
-                "_last_step_time": self._last_step_time,
-            }.items()
-        )
+        return {
+            "id_": self.id_,
+            "agent_to_module_mapping_fn": self.agent_to_module_mapping_fn,
+            "_agent_to_module_mapping": self._agent_to_module_mapping,
+            "observation_space": self.observation_space,
+            "action_space": self.action_space,
+            "env_t_started": self.env_t_started,
+            "env_t": self.env_t,
+            "agent_t_started": self.agent_t_started,
+            # TODO (simon): Check, if we can store the `InfiniteLookbackBuffer`
+            "env_t_to_agent_t": self.env_t_to_agent_t,
+            "_hanging_actions_end": self._hanging_actions_end,
+            "_hanging_extra_model_outputs_end": (self._hanging_extra_model_outputs_end),
+            "_hanging_rewards_end": self._hanging_rewards_end,
+            "_hanging_actions_begin": self._hanging_actions_begin,
+            "_hanging_extra_model_outputs_begin": (
+                self._hanging_extra_model_outputs_begin
+            ),
+            "_hanging_rewards_begin": self._hanging_rewards_begin,
+            "is_terminated": self.is_terminated,
+            "is_truncated": self.is_truncated,
+            "agent_episodes": list(
+                {
+                    agent_id: agent_eps.get_state()
+                    for agent_id, agent_eps in self.agent_episodes.items()
+                }.items()
+            ),
+            "render_images": self.render_images,
+            "_start_time": self._start_time,
+            "_last_step_time": self._last_step_time,
+        }
 
     @staticmethod
-    def from_state(state) -> "MultiAgentEpisode":
+    def from_state(state: Dict[str, Any]) -> "MultiAgentEpisode":
         """Creates a multi-agent episode from a state dictionary.
 
         See `MultiAgentEpisode.get_state()` for creating a state for
@@ -1747,38 +1742,43 @@ class MultiAgentEpisode:
         i.e. all data must have been stored in the state.
 
         Args:
-            state: A list of tuples containing all data required to recreate
-                a `MultiAgentEpisode`. See `MultiAgentEpisode.get_state()`.
+            state: A dict containing all data required to recreate a MultiAgentEpisode`.
+                See `MultiAgentEpisode.get_state()`.
 
         Returns:
             A `MultiAgentEpisode` instance created from the state data.
         """
         # Create an empty `MultiAgentEpisode` instance.
-        episode = MultiAgentEpisode(id_=state[0][1])
+        episode = MultiAgentEpisode(id_=state["id_"])
         # Fill the instance with the state data.
-        episode.agent_to_module_mapping_fn = state[1][1]
-        episode._agent_to_module_mapping = state[2][1]
-        episode.observation_space = state[3][1]
-        episode.action_space = state[4][1]
-        episode.env_t_started = state[5][1]
-        episode.env_t = state[6][1]
-        episode.agent_t_started = state[7][1]
-        episode.env_t_to_agent_t = state[8][1]
-        episode._hanging_actions_end = state[9][1]
-        episode._hanging_extra_model_outputs_end = state[10][1]
-        episode._hanging_rewards_end = state[11][1]
-        episode._hanging_actions_begin = state[12][1]
-        episode._hanging_extra_model_outputs_begin = state[13][1]
-        episode._hanging_rewards_begin = state[14][1]
-        episode.is_terminated = state[15][1]
-        episode.is_truncated = state[16][1]
+        episode.agent_to_module_mapping_fn = state["agent_to_module_mapping_fn"]
+        episode._agent_to_module_mapping = state["_agent_to_module_mapping"]
+        episode.observation_space = state["observation_space"]
+        episode.action_space = state["action_space"]
+        episode.env_t_started = state["env_t_started"]
+        episode.env_t = state["env_t"]
+        episode.agent_t_started = state["agent_t_started"]
+        episode.env_t_to_agent_t = state["env_t_to_agent_t"]
+        episode._hanging_actions_end = state["_hanging_actions_end"]
+        episode._hanging_extra_model_outputs_end = state[
+            "_hanging_extra_model_outputs_end"
+        ]
+        episode._hanging_rewards_end = state["_hanging_rewards_end"]
+        episode._hanging_actions_begin = state["_hanging_actions_begin"]
+        episode._hanging_extra_model_outputs_begin = state[
+            "_hanging_extra_model_outputs_begin"
+        ]
+        episode._hanging_rewards_begin = state["_hanging_rewards_begin"]
+        episode.is_terminated = state["is_terminated"]
+        episode.is_truncated = state["is_truncated"]
         episode.agent_episodes = {
             agent_id: SingleAgentEpisode.from_state(agent_state)
-            for agent_id, agent_state in state[17][1]
+            for agent_id, agent_state in state["agent_episodes"]
         }
-        episode.render_images = state[18][1]
-        episode._start_time = state[19][1]
-        episode._last_step_time = state[20][1]
+        episode.render_images = state["render_images"]
+        episode._start_time = state["_start_time"]
+        episode._last_step_time = state["_last_step_time"]
+
         # Validate the episode.
         episode.validate()
 
