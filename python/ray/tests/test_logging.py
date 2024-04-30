82e1018623f898a8955cc91b2de8265fd90558c4
kaihsun <kaihsun@anyscale.com>
2024-04-30 04:42:01 +0000

    add a unit test
    Signed-off-by: kaihsun <kaihsun@anyscale.com>

--- python/ray/tests/test_logging.py
+++ python/ray/tests/test_logging.py
@@ -459,6 +459,13 @@ assert set(log_component_names).isdisjoint(set(paths)), paths
         # unique ID suffix.
         assert f"({component}" in stderr, stderr
 
+def test_custom_logging_format(shutdown_only):
+    script = """
+import ray
+ray.init(logging_format='custom logging format - %(message)s')
+"""
+    stderr = run_string_as_driver(script)
+    assert "custom logging format - " in stderr
 
 def test_segfault_stack_trace(ray_start_cluster, capsys):
     @ray.remote
