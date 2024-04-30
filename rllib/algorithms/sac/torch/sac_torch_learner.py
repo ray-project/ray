769ca5224d73935463f2c1a41983a67137b5f513
sven1977 <svenmika1977@gmail.com>
2024-04-30 15:00:09 +0200

    LINT
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/algorithms/sac/torch/sac_torch_learner.py
+++ rllib/algorithms/sac/torch/sac_torch_learner.py
@@ -15,7 +15,6 @@ from ray.rllib.algorithms.sac.sac_learner import (
     TD_ERROR_KEY,
     SACLearner,
 )
-from ray.rllib.core import DEFAULT_MODULE_ID
 from ray.rllib.core.learner.learner import (
     POLICY_LOSS_KEY,
 )
