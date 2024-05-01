1b6bf5f694f40e77cff457eb34f5cf29257ed44f
Hao Chen <chenh1024@gmail.com>
2024-05-01 11:46:53 -0700

    fix resource_manager
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- python/ray/data/_internal/execution/resource_manager.py
+++ python/ray/data/_internal/execution/resource_manager.py
@@ -11,9 +11,6 @@ from ray.data._internal.execution.interfaces.execution_options import (
     ExecutionResources,
 )
 from ray.data._internal.execution.interfaces.physical_operator import PhysicalOperator
-from ray.data._internal.execution.operators.actor_pool_map_operator import (
-    ActorPoolMapOperator,
-)
 from ray.data._internal.execution.operators.input_data_buffer import InputDataBuffer
 from ray.data._internal.execution.operators.limit_operator import LimitOperator
 from ray.data._internal.execution.operators.map_operator import MapOperator
@@ -139,9 +136,9 @@ class ResourceManager:
             f = (1.0 + num_ops_so_far) / max(1.0, num_ops_total - 1.0)
             num_ops_so_far += 1
             self._downstream_fraction[op] = min(1.0, f)
-            self._downstream_object_store_memory[
-                op
-            ] = self._global_usage.object_store_memory
+            self._downstream_object_store_memory[op] = (
+                self._global_usage.object_store_memory
+            )
 
             # Update operator's object store usage, which is used by
             # DatasetStats and updated on the Ray Data dashboard.
@@ -422,11 +419,11 @@ class ReservationOpResourceAllocator(OpResourceAllocator):
             min_reserved = op.incremental_resource_usage().copy()
             # 2. To ensure that all GPUs are utilized, reserve enough resource budget
             # to launch one task for each worker.
-            if (
-                isinstance(op, ActorPoolMapOperator)
-                and op.base_resource_usage().gpu > 0
-            ):
-                min_reserved.object_store_memory *= op._autoscaling_policy.min_workers
+            if op.base_resource_usage().gpu > 0:
+                min_workers = sum(
+                    pool.min_size() for pool in op.get_autoscaling_actor_pools()
+                )
+                min_reserved.object_store_memory *= min_workers
             # Also include `reserved_for_op_outputs`.
             min_reserved.object_store_memory += self._reserved_for_op_outputs[op]
             # Total resources we want to reserve for this operator.
