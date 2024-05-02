9598fa2666cb9d775595f2211b498a3fd71a5ded
root <vtsang@amd.com>
2024-05-02 21:12:42 +0000

    integrate rsmi python bindings
    Signed-off-by: root <vtsang@amd.com>

--- python/ray/_private/accelerators/amd_gpu.py
+++ python/ray/_private/accelerators/amd_gpu.py
@@ -6,18 +6,6 @@ from typing import Optional, List, Tuple
 
 from ray._private.accelerators.accelerator import AcceleratorManager
 
-try:
-    from amdsmi import (
-        amdsmi_init,
-        amdsmi_shut_down,
-        amdsmi_get_processor_handles,
-        amdsmi_get_gpu_asic_info,
-        AmdSmiException,
-    )
-except Exception:
-    pass
-
-
 logger = logging.getLogger(__name__)
 
 ROCR_VISIBLE_DEVICES_ENV_VAR = "ROCR_VISIBLE_DEVICES"
@@ -66,41 +54,23 @@ class AMDGPUAcceleratorManager(AcceleratorManager):
 
     @staticmethod
     def get_current_node_num_accelerators() -> int:
+        import ray._private.thirdparty.pyamdsmi as pyamdsmi
         num_gpus = 0
-        major_version = 0
 
         try:
-            major_version = AMDGPUAcceleratorManager._get_rocm_major_version()
-        except Exception:
-            return 0
-
-        if major_version >= 6:
+            pyamdsmi.smi_initialize()
             try:
-                amdsmi_init()
-                try:
-                    num_gpus = len(amdsmi_get_processor_handles())
-                except AmdSmiException:
-                    return 0
-            except AmdSmiException:
-                return 0
-            finally:
-                try:
-                    amdsmi_shut_down()
-                except AmdSmiException:
-                    pass
-        else:
-            try:
-                num_gpus = (
-                    len(
-                        subprocess.check_output(["rocm-smi", "-i", "--csv"])
-                        .decode("utf-8")
-                        .strip()
-                        .split("\n")
-                    )
-                    - 1
-                )
+                num_gpus = pyamdsmi.smi_get_device_count()
             except Exception:
                 pass
+        except Exception:
+            pass
+        finally:
+            try:
+                pyamdsmi.smi_shutdown()
+            except Exception:
+                pass
+
         return num_gpus
 
     @staticmethod
@@ -148,81 +118,28 @@ class AMDGPUAcceleratorManager(AcceleratorManager):
         """Get the list of GPUs IDs
         Example:
             On a node with 2x MI210 GPUs
-            For ROCm version < 6.0
-                Uses rocm-smi subprocess call
-            For ROCm version >= 6.0
-                Uses amismi library python binding
+            pyamdsmi library python bindings
             return: ['0x740f', '0x740f']
         Returns:
             A list of strings containing GPU IDs
         """
+        import ray._private.thirdparty.pyamdsmi as pyamdsmi
+        
+        device_ids = []
         try:
-            major_version = AMDGPUAcceleratorManager._get_rocm_major_version()
+            pyamdsmi.smi_initialize()
+            num_devices = pyamdsmi.smi_get_device_count()
+            for i in range(num_devices):
+                did = pyamdsmi.smi_get_device_id(i)
+                if did >= 0:
+                    device_ids.append(hex(did))
         except Exception:
             return None
-
-        device_ids = []
-        if major_version >= 6:
+        finally:
             try:
-                amdsmi_init()
-                devices = amdsmi_get_processor_handles()
-                if len(devices) > 0:
-                    for device in devices:
-                        asic_info = amdsmi_get_gpu_asic_info(device)
-                        device_ids.append(hex(asic_info["device_id"]))
-            except AmdSmiException:
-                return None
-            finally:
-                try:
-                    amdsmi_shut_down()
-                except AmdSmiException:
-                    pass
-        else:
-            try:
-                amd_ids = eval(
-                    subprocess.check_output(["rocm-smi", "--showid", "--json"]).decode(
-                        "utf-8"
-                    )
-                )
-                keys = amd_ids.keys()
-                for k in keys:
-                    device_ids.append(amd_ids[k]["GPU ID"])
+                pyamdsmi.pyamdsmi_shutdown()
             except Exception:
-                return None
+                pass
 
         return device_ids
 
-    @staticmethod
-    def _get_rocm_major_version() -> int:
-        """Get ROCm version from hipconfig
-        Example:
-        ['HIPversion:6.0.32830-d62f6a171', '', '==hipconfig',
-        'HIP_PATH:/opt/rocm-6.0.0', 'ROCM_PATH:/opt/rocm-6.0.0',
-        'HIP_COMPILER:clang', 'HIP_PLATFORM:amd', 'HIP_RUNTIME:rocclr',
-        'CPP_CONFIG:-D__HIP_PLATFORM_HCC__=-D__HIP_PLATFORM_AMD__=-I/opt/rocm-6.0.0/include',
-        '', '', '==hip-clang', 'HIP_CLANG_PATH:/opt/rocm-6.0.0/llvm/bin']
-        return: '6'
-
-        Returns:
-        ROCm major version string
-        """
-        major_version = 0
-
-        if sys.platform.startswith("linux"):
-            try:
-                hipconfig = (
-                    subprocess.check_output(["hipconfig"])
-                    .decode("utf-8")
-                    .replace(" ", "")
-                    .split("\n")
-                )
-                subs = "HIPversion"
-                res = [i for i in hipconfig if subs in i]
-
-                if len(res) > 0:
-                    version = res[0].split(":")[1]
-                    major_version = int(version.split(".")[0])
-            except Exception:
-                raise Exception("Unable to find ROCm version.")
-
-        return major_version
