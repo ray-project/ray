f6dc9b4cd59a257fd1e4ff304a09eaa49ac8cf58
kaihsun <kaihsun@anyscale.com>
2024-05-03 00:16:29 +0000

    add tests
    Signed-off-by: kaihsun <kaihsun@anyscale.com>

--- python/ray/tests/test_structured_logging.py
+++ python/ray/tests/test_structured_logging.py
@@ -6,23 +6,17 @@ import sys
 import json
 from ray._private.logging.structured_logging import CoreContextFilter, JSONFormatter
 
+
 class TestCoreContextFilter:
     def test_driver_process(self, shutdown_only):
         filter = CoreContextFilter()
         record = logging.makeLogRecord({})
         assert filter.filter(record)
-        should_exist = [
-            "job_id",
-            "worker_id",
-            "node_id"
-        ]
+        should_exist = ["job_id", "worker_id", "node_id"]
         for attr in should_exist:
             assert hasattr(record, attr)
         # This is not a worker process, so actor_id and task_id should not exist.
-        should_not_exist = [
-            "actor_id",
-            "task_id"
-        ]
+        should_not_exist = ["actor_id", "task_id"]
         for attr in should_not_exist:
             assert not hasattr(record, attr)
 
@@ -32,15 +26,11 @@ class TestCoreContextFilter:
             filter = CoreContextFilter()
             record = logging.makeLogRecord({})
             assert filter.filter(record)
-            should_exist = [
-                "job_id",
-                "worker_id",
-                "node_id",
-                "task_id"
-            ]
+            should_exist = ["job_id", "worker_id", "node_id", "task_id"]
             for attr in should_exist:
                 assert hasattr(record, attr)
             assert not hasattr(record, "actor_id")
+
         obj_ref = f.remote()
         ray.get(obj_ref)
 
@@ -51,17 +41,13 @@ class TestCoreContextFilter:
                 filter = CoreContextFilter()
                 record = logging.makeLogRecord({})
                 assert filter.filter(record)
-                should_exist = [
-                    "job_id",
-                    "worker_id",
-                    "node_id",
-                    "actor_id",
-                    "task_id"
-                ]
+                should_exist = ["job_id", "worker_id", "node_id", "actor_id", "task_id"]
                 for attr in should_exist:
                     assert hasattr(record, attr)
+
         actor = A.remote()
         ray.get(actor.f.remote())
+
 
 class TestJSONFormatter:
     def test_empty_record(self, shutdown_only):
@@ -69,21 +55,11 @@ class TestJSONFormatter:
         record = logging.makeLogRecord({})
         formatted = formatter.format(record)
         record_dict = json.loads(formatted)
-        should_exist = [
-            "ts",
-            "level",
-            "msg",
-            "filename",
-            "lineno"
-        ]
+        should_exist = ["ts", "level", "msg", "filename", "lineno"]
         for key in should_exist:
             assert key in record_dict
         assert len(record_dict) == len(should_exist)
-        should_not_exist = [
-            "exc_text"
-        ]
-        for key in should_not_exist:
-            assert key not in record_dict
+        assert "exc_text" not in record_dict
 
     def test_record_with_exception(self, shutdown_only):
         formatter = JSONFormatter()
@@ -94,14 +70,7 @@ class TestJSONFormatter:
             record.exc_info = sys.exc_info()
         formatted = formatter.format(record)
         record_dict = json.loads(formatted)
-        should_exist = [
-            "ts",
-            "level",
-            "msg",
-            "filename",
-            "lineno",
-            "exc_text"
-        ]
+        should_exist = ["ts", "level", "msg", "filename", "lineno", "exc_text"]
         for key in should_exist:
             assert key in record_dict
         assert len(record_dict) == len(should_exist)
@@ -111,17 +80,11 @@ class TestJSONFormatter:
         record = logging.makeLogRecord({"user": "ray"})
         formatted = formatter.format(record)
         record_dict = json.loads(formatted)
-        should_exist = [
-            "ts",
-            "level",
-            "msg",
-            "filename",
-            "lineno",
-            "user"
-        ]
+        should_exist = ["ts", "level", "msg", "filename", "lineno", "user"]
         for key in should_exist:
             assert key in record_dict
         assert len(record_dict) == len(should_exist)
+
 
 if __name__ == "__main__":
     if os.environ.get("PARALLEL_CI"):
