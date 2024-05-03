c23e05e0865de512d68aeb128c8e179ea3b166ed
kaihsun <kaihsun@anyscale.com>
2024-05-03 22:31:01 +0000

    update
    Signed-off-by: kaihsun <kaihsun@anyscale.com>

--- python/ray/_private/structured_logging/filters.py
+++ python/ray/_private/structured_logging/filters.py
@@ -1,18 +1,28 @@
 import logging
 import ray
 
+from enum import Enum
+
+
+class CoreContextEnum(str, Enum):
+    JOB_ID = "job_id"
+    WORKER_ID = "worker_id"
+    NODE_ID = "node_id"
+    ACTOR_ID = "actor_id"
+    TASK_ID = "task_id"
+
 
 class CoreContextFilter(logging.Filter):
     def filter(self, record):
         runtime_context = ray.get_runtime_context()
-        record.job_id = runtime_context.get_job_id()
-        record.worker_id = runtime_context.get_worker_id()
-        record.node_id = runtime_context.get_node_id()
+        setattr(record, CoreContextEnum.JOB_ID, runtime_context.get_job_id())
+        setattr(record, CoreContextEnum.WORKER_ID, runtime_context.get_worker_id())
+        setattr(record, CoreContextEnum.NODE_ID, runtime_context.get_node_id())
         if runtime_context.worker.mode == ray.WORKER_MODE:
             actor_id = runtime_context.get_actor_id()
             if actor_id is not None:
-                record.actor_id = actor_id
+                setattr(record, CoreContextEnum.ACTOR_ID.value, actor_id)
             task_id = runtime_context.get_task_id()
             if task_id is not None:
-                record.task_id = task_id
+                setattr(record, CoreContextEnum.TASK_ID.value, task_id)
         return True
