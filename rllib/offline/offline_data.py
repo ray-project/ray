801911db7bbc405c88c513bcd45ab232444219ec
Simon Zehnder <simon.zehnder@gmail.com>
2024-05-06 13:51:06 +0200

    Implemented test for 'OfflineData' class and stored intermediate work.
    Signed-off-by: Simon Zehnder <simon.zehnder@gmail.com>

--- rllib/offline/offline_data.py
+++ rllib/offline/offline_data.py
@@ -1,8 +1,11 @@
 import logging
+import numpy as np
 from pathlib import Path
 import ray
 from typing import Any, Dict
 
+from ray.rllib.env.single_agent_episode import SingleAgentEpisode
+from ray.rllib.utils.typing import EpisodeType
 logger = logging.getLogger(__name__)
 
 
@@ -36,3 +39,16 @@ class OfflineData:
         else:
             # Return a single batch
             return self.data.take_batch(batch_size=num_samples)
+        
+
+    def _convert_to_episodes(self, batch: Dict[str, np.ndarray]):
+        """Converts a batch of data to episodes."""
+
+        for i, obs in enumerate(batch["obs"]):
+            episode = SingleAgentEpisode(
+                episode_id=batch["eps_id"][0],
+                agent_id=batch["agent_index"][0]
+            )
+
+        
+
---
+++ rllib/offline/tests/test_offline_data.py
@@ -0,0 +1,31 @@
+import ray
+import unittest
+
+from pathlib import Path
+
+from ray.rllib.offline.offline_data import OfflineData
+
+
+class TestOfflineData(unittest.TestCase):
+    def setUp(self) -> None:
+        ray.init()
+
+    def tearDown(self) -> None:
+        ray.shutdown()
+
+    def test_offline_data_load(self):
+        
+        data_path = "tests/data/pendulum/large.json"
+        base_path = Path(__file__).parents[2]
+        data_path = "local://" + base_path.joinpath(data_path).as_posix()
+
+        offline_data = OfflineData(config={"input_": data_path})
+
+        single_row = offline_data.data.take_batch(batch_size=1)
+
+
+if __name__ == "__main__":
+    import sys
+    import pytest
+
+    sys.exit(pytest.main(["-v", __file__]))
