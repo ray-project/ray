5f2d15f1ac4a0e52142d7649b105a9987be759f8
can <can@anyscale.com>
2024-05-07 03:52:08 +0000

    [ci] add script to determine step ids for microcheck
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/automation/determine_microcheck_step_ids.py
+++ ci/ray_ci/automation/determine_microcheck_step_ids.py
@@ -1,14 +1,8 @@
 import click
-from typing import List, Set, Dict
 
-from ci.ray_ci.utils import logger, ci_init
-from ray_release.configs.global_config import get_global_config
+from ci.ray_ci.utils import ci_init
+from ci.ray_ci.automation.determine_microcheck_tests import LINUX_TEST_PREFIX
 from ray_release.test import Test
-from ray_release.result import ResultStatus
-
-# The s3 prefix for the tests that run on Linux. It comes from the bazel prefix rule
-# linux:// with the character "/" replaced by "_" for s3 compatibility
-LINUX_TEST_PREFIX = "linux:__"
 
 
 @click.command()
@@ -17,8 +11,8 @@ def main() -> None:
     This script determines the rayci step ids to run microcheck tests.
     """
     ci_init()
-    high_impact_tests = [
-        test for test in Test.gen_from_s3(LINUX_TEST_PREFIX) test.is_high_impact()
-    ]
-    step_ids = [test.get_test_result(limit=1)[0].rayci_step_id for test in high_impact_tests]
-    print(" ".join(step_ids))
+    print(",".join(Test.gen_high_impact_tests(LINUX_TEST_PREFIX).keys()))
+
+
+if __name__ == "__main__":
+    main()
