dab6229473e94afabb49f1a59c60730eb05ccc72
Brandon Shelley <brandon@pacificaviator.co>
2024-05-07 08:39:02 -0700

    Add descriptive error message when deployment name not found
    If the named deployment isn't found, it's difficult to troubleshoot the names of deployments that are available. This error lists the known ones out in the error message instead.
    
    Signed-off-by: Brandon Shelley <brandon@pacificaviator.co>

--- python/ray/serve/_private/application_state.py
+++ python/ray/serve/_private/application_state.py
@@ -1236,14 +1236,13 @@ def override_deployment_info(
             options["max_ongoing_requests"] = options.get("max_ongoing_requests")
 
         deployment_name = options["name"]
-        try:
-            info = deployment_infos[deployment_name]
-        except KeyError:
-            raise RayServeException(
-                f"Deployment '{deployment_name}' for application "
-                f"'{app_name}' does not exist. Available: "
-                f"{list(deployment_infos.keys())}"
+        if deployment_name not in deployment_infos:
+            raise ValueError(
+                f"Deployment '{deployment_name}' does not exist. "
+                f"Available: {list(deployment_infos.keys())}"
             )
+
+        info = deployment_infos[deployment_name]
         original_options = info.deployment_config.dict()
         original_options["user_configured_option_names"].update(set(options))
 
