51f335e27089c54c8eb179e2f1313b4e9ca4d01c
Peyton Murray <peynmurray@gmail.com>
2024-05-06 20:16:54 -0700

    Update python/ray/util/dask/callbacks.py
    Co-authored-by: angelinalg <122562471+angelinalg@users.noreply.github.com>
    Signed-off-by: Peyton Murray <peynmurray@gmail.com>

--- python/ray/util/dask/callbacks.py
+++ python/ray/util/dask/callbacks.py
@@ -110,7 +110,7 @@ class RayDaskCallback(Callback):
                 the task function, and the remaining tuple items are
                 the task arguments, which are either the actual argument values,
                 or Dask keys into the deps dictionary whose
-                corresponding values are the argument values).
+                corresponding values are the argument values.
             key: The Dask graph key for the given task.
             deps: The dependencies of this task.
             object_ref (ray.ObjectRef): The object reference for the
