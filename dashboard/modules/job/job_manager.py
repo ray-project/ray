a97e5a9757c966244b3a843eb087f9e602ee83f6
Alexey Kudinkin <ak@anyscale.com>
2024-05-06 22:54:30 -0700

    Tidiyng up
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- dashboard/modules/job/job_manager.py
+++ dashboard/modules/job/job_manager.py
@@ -247,6 +247,7 @@ class JobManager:
                 await self._get_head_node_scheduling_strategy()
             )
 
+            # TODO consider restarting JS actor
             supervisor = self._supervisor_actor_cls.options(
                 lifetime="detached",
                 name=JOB_ACTOR_NAME_TEMPLATE.format(job_id=submission_id),
