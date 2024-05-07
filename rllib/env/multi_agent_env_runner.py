minor fixes: - more metrics (3 learner queue sizes) - make weight synching abide to broadcast interval setting and behave the exact same way as on old API stack - Bring back nan counts for episode_return_mean just in case it's used as a stopping criterium in tune (single- and multi-agent episodes) - Pong IMPALA improvements: broadcast interval=5 vf_coeff=0.1 envs_per_runner=5
Signed-off-by: sven1977 <svenmika1977@gmail.com>

rllib/env/multi_agent_env_runner.py
@@ -601,6 +601,13 @@ class MultiAgentEnvRunner(EnvRunner):
                 dict(agent_steps),
             )
 
+        # If no episodes at all, log NaN stats.
+        if (
+            len(self._done_episodes_for_metrics) == 0
+            and self.metrics.peek("episode_return_mean", default=np.nan) is np.nan
+        ):
+            self._log_episode_metrics(np.nan, np.nan, np.nan)
+
         # Log num episodes counter for this iteration.
         self.metrics.log_value(
             NUM_EPISODES,
@@ -839,7 +846,15 @@ class MultiAgentEnvRunner(EnvRunner):
         )
         return num_steps
 
-    def _log_episode_metrics(self, length, ret, sec, agents=None, modules=None, agent_steps=None):
+    def _log_episode_metrics(
+        self,
+        length,
+        ret,
+        sec,
+        agents=None,
+        modules=None,
+        agent_steps=None,
+    ):
         # Log general episode metrics.
         self.metrics.log_dict(
             {