ray-project/ray

commit 2ad4e33e9325b1794d61156ffa59cb492d2a6a41

[data] introduce abstract interface for data autoscaling (#45002)
* Introduce an abstract interface for data autoscaling, making
autoscaling behavior easier to customize and extend. Main components:
* `Autoscaler`: the abstract interface responsible for all autoscaling
decisions, including cluster and actor pool autoscaling.
* `AutoscalingActorPool`: abstract interface that represents an actor
pool that can autoscale.
  * `DefaultAutoscaler`: default implementation. 
* No major code logic changes in this PR, except
* fixing a small bug of calculating actor pool util (should be
`num_active_actors/current_size` instead of
`num_running_actors/current_size`).
* `ActorPoolMapOperator.incremental_resource_usage` now doesn't consider
autoscaling, as we are abstracting autoscaling out of the op. Previously
the info wasn't useful either.
  *  Removed actor pool autoscaling logic for bulk executor. 

---------

Signed-off-by: Hao Chen <chenh1024@gmail.com>

python/ray/data/_internal/execution/autoscaler/default_autoscaler.py
@@ -1,0 +1,184 @@
+import math
+import time
+from typing import TYPE_CHECKING, Dict
+
+from .autoscaler import Autoscaler
+from .autoscaling_actor_pool import AutoscalingActorPool
+from ray.data._internal.execution.autoscaling_requester import (
+    get_or_create_autoscaling_requester_actor,
+)
+from ray.data._internal.execution.interfaces.execution_options import ExecutionResources
+
+if TYPE_CHECKING:
+    from ray.data._internal.execution.interfaces import PhysicalOperator
+    from ray.data._internal.execution.resource_manager import ResourceManager
+    from ray.data._internal.execution.streaming_executor_state import OpState, Topology
+
+
+class DefaultAutoscaler(Autoscaler):
+
+    # Default threshold of actor pool utilization to trigger scaling up.
+    DEFAULT_ACTOR_POOL_SCALING_UP_THRESHOLD: float = 0.8
+    # Default threshold of actor pool utilization to trigger scaling down.
+    DEFAULT_ACTOR_POOL_SCALING_DOWN_THRESHOLD: float = 0.5
+
+    # Min number of seconds between two autoscaling requests.
+    MIN_GAP_BETWEEN_AUTOSCALING_REQUESTS = 20
+
+    def __init__(
+        self,
+        topology: "Topology",
+        resource_manager: "ResourceManager",
+        execution_id: str,
+        actor_pool_scaling_up_threshold: float = DEFAULT_ACTOR_POOL_SCALING_UP_THRESHOLD,  # noqa: E501
+        actor_pool_scaling_down_threshold: float = DEFAULT_ACTOR_POOL_SCALING_DOWN_THRESHOLD,  # noqa: E501
+    ):
+        self._actor_pool_scaling_up_threshold = actor_pool_scaling_up_threshold
+        self._actor_pool_scaling_down_threshold = actor_pool_scaling_down_threshold
+        # Last time when a request was sent to Ray's autoscaler.
+        self._last_request_time = 0
+        super().__init__(topology, resource_manager, execution_id)
+
+    def try_trigger_scaling(self):
+        self._try_scale_up_cluster()
+        self._try_scale_up_or_down_actor_pool()
+
+    def _calculate_actor_pool_util(self, actor_pool: AutoscalingActorPool):
+        """Calculate the utilization of the given actor pool."""
+        if actor_pool.current_size() == 0:
+            return 0
+        else:
+            return actor_pool.num_active_actors() / actor_pool.current_size()
+
+    def _actor_pool_should_scale_up(
+        self,
+        actor_pool: AutoscalingActorPool,
+        op: "PhysicalOperator",
+        op_state: "OpState",
+    ):
+        # Do not scale up, if the op is completed or no more inputs are coming.
+        if op.completed() or (op._inputs_complete and op.internal_queue_size() == 0):
+            return False
+        if actor_pool.current_size() < actor_pool.min_size():
+            # Scale up, if the actor pool is below min size.
+            return True
+        elif actor_pool.current_size() >= actor_pool.max_size():
+            # Do not scale up, if the actor pool is already at max size.
+            return False
+        # Do not scale up, if the op still has enough resources to run.
+        if op_state._scheduling_status.under_resource_limits:
+            return False
+        # Do not scale up, if the op has enough free slots for the existing inputs.
+        if op_state.num_queued() <= actor_pool.num_free_task_slots():
+            return False
+        # Determine whether to scale up based on the actor pool utilization.
+        util = self._calculate_actor_pool_util(actor_pool)
+        return util > self._actor_pool_scaling_up_threshold
+
+    def _actor_pool_should_scale_down(
+        self,
+        actor_pool: AutoscalingActorPool,
+        op: "PhysicalOperator",
+    ):
+        # Scale down, if the op is completed or no more inputs are coming.
+        if op.completed() or (op._inputs_complete and op.internal_queue_size() == 0):
+            return True
+        if actor_pool.current_size() > actor_pool.max_size():
+            # Scale down, if the actor pool is above max size.
+            return True
+        elif actor_pool.current_size() <= actor_pool.min_size():
+            # Do not scale down, if the actor pool is already at min size.
+            return False
+        # Determine whether to scale down based on the actor pool utilization.
+        util = self._calculate_actor_pool_util(actor_pool)
+        return util < self._actor_pool_scaling_down_threshold
+
+    def _try_scale_up_or_down_actor_pool(self):
+        for op, state in self._topology.items():
+            actor_pools = op.get_autoscaling_actor_pools()
+            for actor_pool in actor_pools:
+                while True:
+                    # Try to scale up or down the actor pool.
+                    should_scale_up = self._actor_pool_should_scale_up(
+                        actor_pool,
+                        op,
+                        state,
+                    )
+                    should_scale_down = self._actor_pool_should_scale_down(
+                        actor_pool, op
+                    )
+                    if should_scale_up and not should_scale_down:
+                        if actor_pool.scale_up(1) == 0:
+                            break
+                    elif should_scale_down and not should_scale_up:
+                        if actor_pool.scale_down(1) == 0:
+                            break
+                    else:
+                        break
+
+    def _try_scale_up_cluster(self):
+        """Try to scale up the cluster to accomodate the provided in-progress workload.
+
+        This makes a resource request to Ray's autoscaler consisting of the current,
+        aggregate usage of all operators in the DAG + the incremental usage of all
+        operators that are ready for dispatch (i.e. that have inputs queued). If the
+        autoscaler were to grant this resource request, it would allow us to dispatch
+        one task for every ready operator.
+
+        Note that this resource request does not take the global resource limits or the
+        liveness policy into account; it only tries to make the existing resource usage
+        + one more task per ready operator feasible in the cluster.
+        """
+        # Limit the frequency of autoscaling requests.
+        now = time.time()
+        if now - self._last_request_time < self.MIN_GAP_BETWEEN_AUTOSCALING_REQUESTS:
+            return
+
+        # Scale up the cluster, if no ops are allowed to run, but there are still data
+        # in the input queues.
+        no_runnable_op = all(
+            op_state._scheduling_status.runnable is False
+            for _, op_state in self._topology.items()
+        )
+        any_has_input = any(
+            op_state.num_queued() > 0 for _, op_state in self._topology.items()
+        )
+        if not (no_runnable_op and any_has_input):
+            return
+
+        self._last_request_time = now
+
+        # Get resource usage for all ops + additional resources needed to launch one
+        # more task for each ready op.
+        resource_request = []
+
+        def to_bundle(resource: ExecutionResources) -> Dict:
+            req = {}
+            if resource.cpu:
+                req["CPU"] = math.ceil(resource.cpu)
+            if resource.gpu:
+                req["GPU"] = math.ceil(resource.gpu)
+            return req
+
+        for op, state in self._topology.items():
+            per_task_resource = op.incremental_resource_usage()
+            task_bundle = to_bundle(per_task_resource)
+            resource_request.extend([task_bundle] * op.num_active_tasks())
+            # Only include incremental resource usage for ops that are ready for
+            # dispatch.
+            if state.num_queued() > 0:
+                # TODO(Clark): Scale up more aggressively by adding incremental resource
+                # usage for more than one bundle in the queue for this op?
+                resource_request.append(task_bundle)
+
+        self._send_resource_request(resource_request)
+
+    def _send_resource_request(self, resource_request):
+        # Make autoscaler resource request.
+        actor = get_or_create_autoscaling_requester_actor()
+        actor.request_resources.remote(resource_request, self._execution_id)
+
+    def on_executor_shutdown(self):
+        # Make request for zero resources to autoscaler for this execution.
+        actor = get_or_create_autoscaling_requester_actor()
+        actor.request_resources.remote({}, self._execution_id)

python/ray/data/_internal/execution/streaming_executor_state.py
@@ -12,9 +12,7 @@ from dataclasses import dataclass
 from typing import Dict, List, Optional, Tuple
 
 import ray
-from ray.data._internal.execution.autoscaling_requester import (
-    get_or_create_autoscaling_requester_actor,
-)
+from ray.data._internal.execution.autoscaler import Autoscaler
 from ray.data._internal.execution.backpressure_policy import BackpressurePolicy
 from ray.data._internal.execution.interfaces import (
     ExecutionOptions,
@@ -41,18 +39,6 @@ logger = logging.getLogger(__name__)
 # Holds the full execution state of the streaming topology. It's a dict mapping each
 # operator to tracked streaming exec state.
 Topology = Dict[PhysicalOperator, "OpState"]
-
-# Min number of seconds between two autoscaling requests.
-MIN_GAP_BETWEEN_AUTOSCALING_REQUESTS = 20
-
-
-@dataclass
-class AutoscalingState:
-    """State of the interaction between an executor and Ray autoscaler."""
-
-    # The timestamp of the latest resource request made to Ray autoscaler
-    # by an executor.
-    last_request_ts: int = 0
 
 
 class OpBufferQueue:
@@ -156,6 +142,26 @@ class OpBufferQueue:
             self._num_per_split.clear()
 
 
+@dataclass
+class OpSchedulingStatus:
+    """The scheduling status of an operator.
+
+    This will be updated each time when StreamingExecutor makes
+    a scheduling decision, i.e., in each `select_operator_to_run`
+    call.
+    """
+
+    # Whether the op was selected to run in the last scheduling
+    # decision.
+    selected: bool = False
+    # Whether the op was considered runnable in the last scheduling
+    # decision.
+    runnable: bool = False
+    # Whether the resources were sufficient for the operator to run
+    # in the last scheduling decision.
+    under_resource_limits: bool = False
+
+
 class OpState:
     """The execution state tracked for each PhysicalOperator.
 
@@ -186,6 +192,7 @@ class OpState:
         # Used for StreamingExecutor to signal exception or end of execution
         self._finished: bool = False
         self._exception: Optional[Exception] = None
+        self._scheduling_status = OpSchedulingStatus()
 
     def __repr__(self):
         return f"OpState({self.op.name})"
@@ -497,9 +504,8 @@ def select_operator_to_run(
     topology: Topology,
     resource_manager: ResourceManager,
     backpressure_policies: List[BackpressurePolicy],
+    autoscaler: Autoscaler,
     ensure_at_least_one_running: bool,
-    execution_id: str,
-    autoscaling_state: AutoscalingState,
 ) -> Optional[PhysicalOperator]:
     """Select an operator to run, if possible.
 
@@ -526,6 +532,7 @@ def select_operator_to_run(
         in_backpressure = not under_resource_limits or any(
             not p.can_add_input(op) for p in backpressure_policies
         )
+        op_runnable = False
         if (
             not in_backpressure
             and not op.completed()
@@ -533,21 +540,15 @@ def select_operator_to_run(
             and op.should_add_input()
         ):
             ops.append(op)
+            op_runnable = True
+        state._scheduling_status = OpSchedulingStatus(
+            selected=False,
+            runnable=op_runnable,
+            under_resource_limits=under_resource_limits,
+        )
+
         # Signal whether op in backpressure for stats collections
         op.notify_in_task_submission_backpressure(in_backpressure)
-        # Update the op in all cases to enable internal autoscaling, etc.
-        op.notify_resource_usage(state.num_queued(), under_resource_limits)
-
-    # If no ops are allowed to execute due to resource constraints, try to trigger
-    # cluster scale-up.
-    if not ops and any(state.num_queued() > 0 for state in topology.values()):
-        now = time.time()
-        if (
-            now
-            > autoscaling_state.last_request_ts + MIN_GAP_BETWEEN_AUTOSCALING_REQUESTS
-        ):
-            autoscaling_state.last_request_ts = now
-            _try_to_scale_up_cluster(topology, execution_id)
 
     # To ensure liveness, allow at least 1 op to run regardless of limits. This is
     # gated on `ensure_at_least_one_running`, which is set if the consumer is blocked.
@@ -569,58 +570,16 @@ def select_operator_to_run(
 
     # Run metadata-only operators first. After that, choose the operator with the least
     # memory usage.
-    return min(
+    selected_op = min(
         ops,
         key=lambda op: (
             not op.throttling_disabled(),
             resource_manager.get_op_usage(op).object_store_memory,
         ),
     )
-
-
-def _try_to_scale_up_cluster(topology: Topology, execution_id: str):
-    """Try to scale up the cluster to accomodate the provided in-progress workload.
-
-    This makes a resource request to Ray's autoscaler consisting of the current,
-    aggregate usage of all operators in the DAG + the incremental usage of all operators
-    that are ready for dispatch (i.e. that have inputs queued). If the autoscaler were
-    to grant this resource request, it would allow us to dispatch one task for every
-    ready operator.
-
-    Note that this resource request does not take the global resource limits or the
-    liveness policy into account; it only tries to make the existing resource usage +
-    one more task per ready operator feasible in the cluster.
-
-    Args:
-        topology: The execution state of the in-progress workload for which we wish to
-            request more resources.
-    """
-    # Get resource usage for all ops + additional resources needed to launch one more
-    # task for each ready op.
-    resource_request = []
-
-    def to_bundle(resource: ExecutionResources) -> Dict:
-        req = {}
-        if resource.cpu:
-            req["CPU"] = math.ceil(resource.cpu)
-        if resource.gpu:
-            req["GPU"] = math.ceil(resource.gpu)
-        return req
-
-    for op, state in topology.items():
-        per_task_resource = op.incremental_resource_usage()
-        task_bundle = to_bundle(per_task_resource)
-        resource_request.extend([task_bundle] * op.num_active_tasks())
-        # Only include incremental resource usage for ops that are ready for
-        # dispatch.
-        if state.num_queued() > 0:
-            # TODO(Clark): Scale up more aggressively by adding incremental resource
-            # usage for more than one bundle in the queue for this op?
-            resource_request.append(task_bundle)
-
-    # Make autoscaler resource request.
-    actor = get_or_create_autoscaling_requester_actor()
-    actor.request_resources.remote(resource_request, execution_id)
+    topology[selected_op]._scheduling_status.selected = True
+    autoscaler.try_trigger_scaling()
+    return selected_op
 
 
 def _execution_allowed(op: PhysicalOperator, resource_manager: ResourceManager) -> bool:

python/ray/data/tests/test_autoscaler.py
@@ -1,0 +1,189 @@
+from contextlib import contextmanager
+from unittest.mock import MagicMock
+
+from pytest_shutil.workspace import pytest
+
+from ray.data import ExecutionResources
+from ray.data._internal.execution.autoscaler.default_autoscaler import DefaultAutoscaler
+
+
+def test_actor_pool_scaling():
+    """Test `_actor_pool_should_scale_up` and `_actor_pool_should_scale_down`
+    in `DefaultAutoscaler`"""
+
+    autoscaler = DefaultAutoscaler(
+        topology=MagicMock(),
+        resource_manager=MagicMock(),
+        execution_id="execution_id",
+        actor_pool_scaling_up_threshold=0.8,
+        actor_pool_scaling_down_threshold=0.5,
+    )
+
+    # Current actor pool utilization is 0.9, which is above the threshold.
+    actor_pool = MagicMock(
+        min_size=MagicMock(return_value=5),
+        max_size=MagicMock(return_value=15),
+        current_size=MagicMock(return_value=10),
+        num_active_actors=MagicMock(return_value=9),
+        num_free_task_slots=MagicMock(return_value=5),
+    )
+
+    op = MagicMock(
+        completed=MagicMock(return_value=False),
+        _inputs_complete=False,
+        internal_queue_size=MagicMock(return_value=1),
+    )
+    op_state = MagicMock(num_queued=MagicMock(return_value=10))
+    op_scheduling_status = MagicMock(under_resource_limits=False)
+    op_state._scheduling_status = op_scheduling_status
+
+    @contextmanager
+    def patch(mock, attr, value, is_method=True):
+        original = getattr(mock, attr)
+        if is_method:
+            value = MagicMock(return_value=value)
+        setattr(mock, attr, value)
+        yield
+        setattr(mock, attr, original)
+
+    # === Test scaling up ===
+
+    def assert_should_scale_up(expected):
+        nonlocal actor_pool, op, op_state
+
+        assert (
+            autoscaler._actor_pool_should_scale_up(
+                actor_pool=actor_pool,
+                op=op,
+                op_state=op_state,
+            )
+            == expected
+        )
+
+    # Should scale up since the util above the threshold.
+    assert autoscaler._calculate_actor_pool_util(actor_pool) == 0.9
+    assert_should_scale_up(True)
+
+    # Shouldn't scale up since the util is below the threshold.
+    with patch(actor_pool, "num_active_actors", 7):
+        assert autoscaler._calculate_actor_pool_util(actor_pool) == 0.7
+        assert_should_scale_up(False)
+
+    # Shouldn't scale up since we have reached the max size.
+    with patch(actor_pool, "current_size", 15):
+        assert_should_scale_up(False)
+
+    # Should scale up since the pool is below the min size.
+    with patch(actor_pool, "current_size", 4):
+        assert_should_scale_up(True)
+
+    # Shouldn't scale up since if the op is completed, or
+    # the op has no more inputs.
+    with patch(op, "completed", True):
+        assert_should_scale_up(False)
+    with patch(op, "_inputs_complete", True, is_method=False):
+        with patch(op, "internal_queue_size", 0):
+            assert_should_scale_up(False)
+
+    # Shouldn't scale up since the op is under resource limits.
+    with patch(
+        op_scheduling_status,
+        "under_resource_limits",
+        True,
+        is_method=False,
+    ):
+        assert_should_scale_up(False)
+
+    # Shouldn't scale up since the op has enough free slots for
+    # the existing inputs.
+    with patch(op_state, "num_queued", 5):
+        assert_should_scale_up(False)
+
+    # === Test scaling down ===
+
+    def assert_should_scale_down(expected):
+        assert (
+            autoscaler._actor_pool_should_scale_down(
+                actor_pool=actor_pool,
+                op=op,
+            )
+            == expected
+        )
+
+    # Shouldn't scale down since the util above the threshold.
+    assert autoscaler._calculate_actor_pool_util(actor_pool) == 0.9
+    assert_should_scale_down(False)
+
+    # Should scale down since the util is below the threshold.
+    with patch(actor_pool, "num_active_actors", 4):
+        assert autoscaler._calculate_actor_pool_util(actor_pool) == 0.4
+        assert_should_scale_down(True)
+
+    # Should scale down since the pool is above the max size.
+    with patch(actor_pool, "current_size", 16):
+        assert_should_scale_down(True)
+
+    # Shouldn't scale down since we have reached the min size.
+    with patch(actor_pool, "current_size", 5):
+        assert_should_scale_down(False)
+
+    # Should scale down since if the op is completed, or
+    # the op has no more inputs.
+    with patch(op, "completed", True):
+        assert_should_scale_down(True)
+    with patch(op, "_inputs_complete", True, is_method=False):
+        with patch(op, "internal_queue_size", 0):
+            assert_should_scale_down(True)
+
+
+def test_cluster_scaling():
+    """Test `_try_scale_up_cluster` in `DefaultAutoscaler`"""
+    op1 = MagicMock(
+        input_dependencies=[],
+        incremental_resource_usage=MagicMock(
+            return_value=ExecutionResources(cpu=1, gpu=0, object_store_memory=0)
+        ),
+        num_active_tasks=MagicMock(return_value=1),
+    )
+    op_state1 = MagicMock(
+        num_queued=MagicMock(return_value=0),
+        _scheduling_status=MagicMock(
+            runnable=False,
+        ),
+    )
+    op2 = MagicMock(
+        input_dependencies=[op1],
+        incremental_resource_usage=MagicMock(
+            return_value=ExecutionResources(cpu=2, gpu=0, object_store_memory=0)
+        ),
+        num_active_tasks=MagicMock(return_value=1),
+    )
+    op_state2 = MagicMock(
+        num_queued=MagicMock(return_value=1),
+        _scheduling_status=MagicMock(
+            runnable=False,
+        ),
+    )
+    topology = {
+        op1: op_state1,
+        op2: op_state2,
+    }
+
+    autoscaler = DefaultAutoscaler(
+        topology=topology,
+        resource_manager=MagicMock(),
+        execution_id="execution_id",
+    )
+
+    autoscaler._send_resource_request = MagicMock()
+    autoscaler._try_scale_up_cluster()
+
+    autoscaler._send_resource_request.assert_called_once_with(
+        [{"CPU": 1}, {"CPU": 2}, {"CPU": 2}]
+    )
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(pytest.main(["-v", __file__]))

python/ray/data/BUILD
@@ -580,6 +580,14 @@ py_test(
 )
 
 py_test(
+    name = "test_autoscaler",
+    size = "small",
+    srcs = ["tests/test_autoscaler.py"],
+    tags = ["team:data", "exclusive"],
+    deps = ["//:ray_lib", ":conftest"],
+)
+
+py_test(
     name = "test_lance",
     size = "small",
     srcs = ["tests/test_lance.py"],

python/ray/data/_internal/execution/autoscaler/__init__.py
@@ -1,0 +1,15 @@
+from .autoscaler import Autoscaler
+from .autoscaling_actor_pool import AutoscalingActorPool
+from .default_autoscaler import DefaultAutoscaler
+
+
+def create_autoscaler(topology, resource_manager, execution_id):
+    return DefaultAutoscaler(topology, resource_manager, execution_id)
+
+
+__all__ = [
+    "Autoscaler",
+    "DefaultAutoscaler",
+    "create_autoscaler",
+    "AutoscalingActorPool",
+]

python/ray/data/_internal/execution/autoscaler/autoscaler.py
@@ -1,0 +1,38 @@
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+from ray.util.annotations import DeveloperAPI
+
+if TYPE_CHECKING:
+    from ray.data._internal.execution.resource_manager import ResourceManager
+    from ray.data._internal.execution.streaming_executor_state import Topology
+
+
+@DeveloperAPI
+class Autoscaler(ABC):
+    """Abstract interface for Ray Data autoscaler."""
+
+    def __init__(
+        self,
+        topology: "Topology",
+        resource_manager: "ResourceManager",
+        execution_id: str,
+    ):
+        self._topology = topology
+        self._resource_manager = resource_manager
+        self._execution_id = execution_id
+
+    @abstractmethod
+    def try_trigger_scaling(self):
+        """Try trigger autoscaling.
+
+        This method will be called each time when StreamingExecutor makes
+        a scheduling decision. A subclass should override this method to
+        handle the autoscaling of both the cluster and `AutoscalingActorPool`s.
+        """
+        ...
+
+    @abstractmethod
+    def on_executor_shutdown(self):
+        """Callback when the StreamingExecutor is shutting down."""
+        ...

commit c13fb228f1db51c0a2f267f36d16cac706b2986e

[data] fix bugs introduced by autoscaler refactor (#45200)
<!-- Thank you for your contribution! Please review
https://github.com/ray-project/ray/blob/master/CONTRIBUTING.rst before
opening a pull request. -->

<!-- Please add a reviewer to the assignee section when you create a PR.
If you don't have the access to it, we will shortly find a reviewer and
assign them to your PR. -->

## Why are these changes needed?

Fix following bugs introduced by #45002:
* `autoscaler.try_trigger_scaling` not called when `select_op_to_run`
returns None.
* scaling up condition on `under_resource_limits`.


`python/ray/data/tests/test_streaming_integration.py::test_e2e_autoscaling_up`
should pass after this fix.

## Related issue number

Closes https://github.com/ray-project/ray/issues/43481

## Checks

- [ ] I've signed off every commit(by using the -s flag, i.e., `git
commit -s`) in this PR.
- [ ] I've run `scripts/format.sh` to lint the changes in this PR.
- [ ] I've included any doc changes needed for
https://docs.ray.io/en/master/.
- [ ] I've added any new APIs to the API Reference. For example, if I
added a
method in Tune, I've added it in `doc/source/tune/api/` under the
           corresponding `.rst` file.
- [ ] I've made sure the tests are passing. Note that there might be a
few flaky tests, see the recent failures at https://flakey-tests.ray.io/
- Testing Strategy
   - [ ] Unit tests
   - [ ] Release tests
   - [ ] This PR is not tested :(

Signed-off-by: Hao Chen <chenh1024@gmail.com>

python/ray/data/_internal/execution/autoscaler/default_autoscaler.py
@@ -65,8 +65,8 @@ class DefaultAutoscaler(Autoscaler):
         elif actor_pool.current_size() >= actor_pool.max_size():
             # Do not scale up, if the actor pool is already at max size.
             return False
-        # Do not scale up, if the op still has enough resources to run.
-        if op_state._scheduling_status.under_resource_limits:
+        # Do not scale up, if the op does not have more resources.
+        if not op_state._scheduling_status.under_resource_limits:
             return False
         # Do not scale up, if the op has enough free slots for the existing inputs.
         if op_state.num_queued() <= actor_pool.num_free_task_slots():

python/ray/data/_internal/execution/streaming_executor_state.py
@@ -541,6 +541,7 @@ def select_operator_to_run(
         ):
             ops.append(op)
             op_runnable = True
+        # Update scheduling status
         state._scheduling_status = OpSchedulingStatus(
             selected=False,
             runnable=op_runnable,
@@ -564,20 +565,18 @@ def select_operator_to_run(
             if state.num_queued() > 0 and not op.completed()
         ]
 
-    # Nothing to run.
-    if not ops:
-        return None
-
-    # Run metadata-only operators first. After that, choose the operator with the least
-    # memory usage.
-    selected_op = min(
-        ops,
-        key=lambda op: (
-            not op.throttling_disabled(),
-            resource_manager.get_op_usage(op).object_store_memory,
-        ),
-    )
-    topology[selected_op]._scheduling_status.selected = True
+    selected_op = None
+    if ops:
+        # Run metadata-only operators first. After that, choose the operator with the
+        # least memory usage.
+        selected_op = min(
+            ops,
+            key=lambda op: (
+                not op.throttling_disabled(),
+                resource_manager.get_op_usage(op).object_store_memory,
+            ),
+        )
+        topology[selected_op]._scheduling_status.selected = True
     autoscaler.try_trigger_scaling()
     return selected_op
 

python/ray/data/tests/test_autoscaler.py
@@ -34,7 +34,7 @@ def test_actor_pool_scaling():
         internal_queue_size=MagicMock(return_value=1),
     )
     op_state = MagicMock(num_queued=MagicMock(return_value=10))
-    op_scheduling_status = MagicMock(under_resource_limits=False)
+    op_scheduling_status = MagicMock(under_resource_limits=True)
     op_state._scheduling_status = op_scheduling_status
 
     @contextmanager
@@ -85,11 +85,11 @@ def test_actor_pool_scaling():
         with patch(op, "internal_queue_size", 0):
             assert_should_scale_up(False)
 
-    # Shouldn't scale up since the op is under resource limits.
+    # Shouldn't scale up since the op doesn't have enough resources.
     with patch(
         op_scheduling_status,
         "under_resource_limits",
-        True,
+        False,
         is_method=False,
     ):
         assert_should_scale_up(False)