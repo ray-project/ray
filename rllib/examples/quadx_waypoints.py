91e6f4fd2adc5bea30f5243f4f53dae6c4c2a32a
peterghaddad <peter.g.haddad@lmco.com>
2024-05-14 15:21:07 -0600

    Address feedback

--- rllib/examples/quadx_waypoints.py
+++ rllib/examples/quadx_waypoints.py
@@ -9,26 +9,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     "--run", type=str, default="PPO", help="The RLlib-registered algorithm to use."
 )
+parser.add_argument('--env-name', type=str, default="quadx_waypoints")
 parser.add_argument("--num-cpus", type=int, default=4)
+parser.add_argument("--num-envs-per-worker", type=int, default=4)
 parser.add_argument(
     "--framework",
     choices=["tf2", "torch"],
     default="torch",
     help="The DL framework specifier.",
 )
-parser.add_argument("--use-prev-action", action="store_true")
-parser.add_argument("--use-prev-reward", action="store_true")
-parser.add_argument(
-    "--as-test",
-    action="store_true",
-    help="Whether this script should be run as a test: --stop-reward must "
-    "be achieved within --stop-timesteps AND --stop-iters.",
-)
+
 parser.add_argument(
     "--stop-iters", type=int, default=500, help="Number of iterations to train."
-)
-parser.add_argument(
-    "--stop-timesteps", type=int, default=1000000, help="Number of timesteps to train."
 )
 parser.add_argument(
     "--stop-reward", type=float, default=90.0, help="Reward at which we stop training."
@@ -65,34 +57,39 @@ if __name__ == "__main__":
 
     ray.init()
 
-    register_env("quadx_waypoints", env_creator=create_quadx_waypoints_env)
+    register_env(args.env_name, env_creator=create_quadx_waypoints_env)
 
     algo_cls = get_trainable_cls(args.run)
     config = algo_cls.get_default_config()
 
     config.environment(
-        env="quadx_waypoints"
+        env=args.env_name
     ).resources(
         num_gpus=int(os.environ.get("RLLIB_NUM_GPUS", "0"))
+    ).rollouts(
+        num_rollout_workers=args.num_cpus,
+        num_envs_per_worker=args.num_envs_per_worker,
     ).framework(
         args.framework
     ).api_stack(
             enable_rl_module_and_learner=True,
             enable_env_runner_and_connector_v2=True,
-    ).rl_module(
-        model_config_dict={
-            "use_lstm": True,
-            "lstm_cell_size": 32,
-            "lstm_use_prev_action": args.use_prev_action,
-            "lstm_use_prev_reward": args.use_prev_reward,
-        }
     ).reporting(
         min_time_s_per_iteration=0.1
     )
 
     if args.run == "PPO":
-        config.training(num_sgd_iter=5, vf_loss_coeff=0.0001, train_batch_size=512)
-        config.model["vf_share_layers"] = True
+        config.rl_module(
+            model_config_dict={
+                "fcnet_hiddens": [32],
+                "fcnet_activation": "linear",
+                "vf_share_layers": True,
+            }
+        )  
+        config.training(
+            sgd_minibatch_size=128,
+            train_batch_size=10000,
+        )
     elif args.run == "IMPALA":
         config.rollouts(num_rollout_workers=2)
         config.resources(num_gpus=0)
@@ -100,8 +97,7 @@ if __name__ == "__main__":
 
     stop = {
         "training_iteration": args.stop_iters,
-        "timesteps_total": args.stop_timesteps,
-        "episode_return_mean": args.stop_reward,
+        "env_runner_results/episode_return_mean": args.stop_reward,
     }
 
     tuner = tune.Tuner(
