1631d61199ddb27b4b78328ee23f036b1656cc2c
khluu <51931015+khluu@users.noreply.github.com>
2024-05-15 03:07:39 -0700

    p
    Signed-off-by: khluu <51931015+khluu@users.noreply.github.com>

--- .buildkite/release-automation/test.sh
+++ .buildkite/release-automation/test.sh
@@ -1,3 +1,3 @@
-OUTPUT_DIR="$HOME/.buildkite/ray/ci/ray_ci/automation/"
+OUTPUT_DIR="/artifact-mount/.image-info/"
 BUILD_OUTPUT="bazel-bin/ci/ray_ci/automation/update_version.zip"
 cp $BUILD_OUTPUT $OUTPUT_DIR/
