9b2a208f51fd86ccd5178d6c9642abea5de76549
Rob Cheung <kousun12@gmail.com>
2024-05-16 16:10:11 -0400

    allow more than one max_calls for build_serve_application

--- python/ray/serve/_private/application_state.py
+++ python/ray/serve/_private/application_state.py
@@ -1003,7 +1003,7 @@ class ApplicationStateManager:
         )
 
 
-@ray.remote(num_cpus=0, max_calls=1)
+@ray.remote(num_cpus=0)
 def build_serve_application(
     import_path: str,
     config_deployments: List[str],
