0526571636a58a46f81b29ad5c1258185d26131f
Rui Qiao <ruisearch42@gmail.com>
2024-05-21 17:08:32 -0700

    cleanup
    Signed-off-by: Rui Qiao <ruisearch42@gmail.com>

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -668,7 +668,7 @@ def compose_state_message(death_info_dict: dict) -> Optional[str]:
     elif death_reason == "UNEXPECTED_TERMINATION":
         state_message = "Unexpected termination"
     elif death_reason == "AUTOSCALER_DRAIN_PREEMPTED":
-        state_message = "Terminated due to preemption from cloud provider"
+        state_message = "Terminated due to preemption"
     elif death_reason == "AUTOSCALER_DRAIN_IDLE":
         state_message = "Terminated due to idle (no Ray activity)"
     else:
