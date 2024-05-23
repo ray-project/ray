00576011f13d889123020af9186ee32f67a43c8c
can <can@anyscale.com>
2024-05-23 23:51:14 +0000

    [ci][doc/3] add a cmd to query and check apis for ray packages
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/doc/cmd_check_api_discrepancy.py
+++ ci/ray_ci/doc/cmd_check_api_discrepancy.py
@@ -1,15 +1,23 @@
 import click
 
 from ci.ray_ci.doc.module import Module
-from ci.ray_ci.utils import logger
+
 
 @click.command()
 @click.argument("module", required=True, type=str)
-def main() -> None:
+def main(module: str) -> None:
     """
     This script checks for annotated classes and functions in a module, and finds
     discrepancies between the annotations and the documentation.
     """
     module = Module(module)
     for api in module.get_apis():
-        logger.info(f"API: {api.name}, Annotation Type: {api.annotation_type}, Code Type: {api.code_type}")
+        print(
+            f"API: {api.name}, "
+            f"Annotation Type: {api.annotation_type}, "
+            f"Code Type: {api.code_type}"
+        )
+
+
+if __name__ == "__main__":
+    main()
