748a9fdfd03ce3d207960aaf5ebabbefc5c35300
Jiajun Yao <jeromeyjj@gmail.com>
2024-05-24 13:00:20 -0700

    up
    Signed-off-by: Jiajun Yao <jeromeyjj@gmail.com>

--- src/ray/util/logging.cc
+++ src/ray/util/logging.cc
@@ -125,6 +125,104 @@ inline const char *ConstBasename(const char *filepath) {
   return base ? (base + 1) : filepath;
 }
 
+std::size_t json_extra_space(const std::string &s) {
+  std::size_t result = 0;
+
+  for (const auto &c : s) {
+    switch (c) {
+    case '"':
+    case '\\':
+    case '\b':
+    case '\f':
+    case '\n':
+    case '\r':
+    case '\t': {
+      // from c (1 byte) to \x (2 bytes)
+      result += 1;
+      break;
+    }
+
+    default:
+      break;
+    }
+  }
+
+  return result;
+}
+
+// Adapted from nlohmann/json
+std::string json_escape_string(const std::string &s) noexcept {
+  const auto space = json_extra_space(s);
+  if (space == 0) {
+    return s;
+  }
+
+  // create a result string of necessary size
+  std::string result(s.size() + space, '\\');
+  std::size_t pos = 0;
+
+  for (const auto &c : s) {
+    switch (c) {
+    // quotation mark (0x22)
+    case '"': {
+      result[pos + 1] = '"';
+      pos += 2;
+      break;
+    }
+
+    // reverse solidus (0x5c)
+    case '\\': {
+      // nothing to change
+      pos += 2;
+      break;
+    }
+
+    // backspace (0x08)
+    case '\b': {
+      result[pos + 1] = 'b';
+      pos += 2;
+      break;
+    }
+
+    // formfeed (0x0c)
+    case '\f': {
+      result[pos + 1] = 'f';
+      pos += 2;
+      break;
+    }
+
+    // newline (0x0a)
+    case '\n': {
+      result[pos + 1] = 'n';
+      pos += 2;
+      break;
+    }
+
+    // carriage return (0x0d)
+    case '\r': {
+      result[pos + 1] = 'r';
+      pos += 2;
+      break;
+    }
+
+    // horizontal tab (0x09)
+    case '\t': {
+      result[pos + 1] = 't';
+      pos += 2;
+      break;
+    }
+
+    default: {
+      // all other characters are added as-is
+      result[pos++] = c;
+      break;
+    }
+    }
+  }
+
+  return result;
+}
+
 /// A logger that prints logs to stderr.
 /// This is the default logger if logging is not initialized.
 /// NOTE(lingxuan.zlx): Default stderr logger must be singleton and global
@@ -454,7 +552,7 @@ RayLog::~RayLog() {
     logger->log(GetMappedSeverity(severity_),
                 /*fmt*/ ",\"{}\":\"{}\"{}",
                 kLogKeyMessage,
-                msg_osstream_.str(),
+                json_escape_string(msg_osstream_.str()),
                 context_osstream_.str());
   } else {
     logger->log(GetMappedSeverity(severity_),
