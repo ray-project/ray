51c64e58c730c5242de798955315d7a70457b07a
votrou <connorvotroubek@gmail.com>
2024-05-30 09:47:35 -0700

    fix another small bug

--- python/ray/data/_internal/execution/operators/actor_pool_map_operator.py
+++ python/ray/data/_internal/execution/operators/actor_pool_map_operator.py
@@ -146,7 +146,7 @@ class ActorPoolMapOperator(MapOperator):
                 "enough resources for the requested actor pool."
             )
         total_latency = time.time() - start
-        logger.get_logger.info(f"{self._name}: Took {total_latency} seconds to provision actors")
+        logger.get_logger().info(f"{self._name}: Took {total_latency} seconds to provision actors")
         self.actor_pool_creation_latency.observe(total_latency * 1000)
 
     def should_add_input(self) -> bool:
