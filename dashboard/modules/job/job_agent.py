21a04d56f5889ac78fcc171def426a1a4bde8f6c
Alexey Kudinkin <ak@anyscale.com>
2024-05-31 16:56:17 -0700

    Split up `JobAgent` onto  - `JobManagerAgent`: managing job's lifecycle (t/h `JobManager`)  - `JobObservabilityAgent`: providing ways to access job's logs
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- dashboard/modules/job/job_agent.py
+++ dashboard/modules/job/job_agent.py
@@ -1,9 +1,13 @@
+import asyncio
 import aiohttp
-from aiohttp.web import Request, Response
 import dataclasses
 import json
 import logging
 import traceback
+
+from aiohttp.web import Request, Response
+from typing import AsyncIterator
+
 import ray
 import ray.dashboard.optional_utils as optional_utils
 import ray.dashboard.utils as dashboard_utils
@@ -13,20 +17,27 @@ from ray.dashboard.modules.job.common import (
     JobSubmitResponse,
     JobStopResponse,
     JobLogsResponse,
+    JobInfoStorageClient,
 )
+from ray.dashboard.modules.job.job_log_storage_client import JobLogStorageClient
 from ray.dashboard.modules.job.job_manager import JobManager
 from ray.dashboard.modules.job.pydantic_models import JobType
 from ray.dashboard.modules.job.utils import parse_and_validate_request, find_job_by_ids
-
 
 routes = optional_utils.DashboardAgentRouteTable
 logger = logging.getLogger(__name__)
 
 
-class JobAgent(dashboard_utils.DashboardAgentModule):
+class JobManagerAgent(dashboard_utils.DashboardAgentModule):
+    """TODO"""
+
     def __init__(self, dashboard_agent):
         super().__init__(dashboard_agent)
+
         self._job_manager = None
+
+        self._job_info_client = None
+        self._job_log_client = None
 
     @routes.post("/api/job_agent/jobs/")
     @optional_utils.init_ray_and_catch_exceptions()
@@ -76,7 +87,7 @@ class JobAgent(dashboard_utils.DashboardAgentModule):
         job_or_submission_id = req.match_info["job_or_submission_id"]
         job = await find_job_by_ids(
             self._dashboard_agent.gcs_aio_client,
-            self.get_job_manager().job_info_client(),
+            self._get_job_info_client(),
             job_or_submission_id,
         )
         if not job:
@@ -109,7 +120,7 @@ class JobAgent(dashboard_utils.DashboardAgentModule):
         job_or_submission_id = req.match_info["job_or_submission_id"]
         job = await find_job_by_ids(
             self._dashboard_agent.gcs_aio_client,
-            self.get_job_manager().job_info_client(),
+            self._get_job_info_client(),
             job_or_submission_id,
         )
         if not job:
@@ -136,13 +147,42 @@ class JobAgent(dashboard_utils.DashboardAgentModule):
             text=json.dumps(dataclasses.asdict(resp)), content_type="application/json"
         )
 
+    def get_job_manager(self):
+        if not self._job_manager:
+            self._job_manager = JobManager(
+                self._dashboard_agent.gcs_aio_client, self._dashboard_agent.log_dir
+            )
+
+        return self._job_manager
+
+    async def run(self, server):
+        pass
+
+    @staticmethod
+    def is_minimal_module():
+        return False
+
+
+class JobObservabilityAgent(dashboard_utils.DashboardAgentModule):
+    """TODO"""
+
+    # Time that we will sleep while tailing logs if no new log line is
+    # available.
+    LOG_TAIL_SLEEP_S = 1
+
+    def __init__(self, dashboard_agent):
+        super().__init__(dashboard_agent)
+
+        self._job_log_client = JobLogStorageClient()
+        self._job_info_client = None
+
     @routes.get("/api/job_agent/jobs/{job_or_submission_id}/logs")
     @optional_utils.init_ray_and_catch_exceptions()
     async def get_job_logs(self, req: Request) -> Response:
         job_or_submission_id = req.match_info["job_or_submission_id"]
         job = await find_job_by_ids(
             self._dashboard_agent.gcs_aio_client,
-            self.get_job_manager().job_info_client(),
+            self._get_job_info_client(),
             job_or_submission_id,
         )
         if not job:
@@ -158,7 +198,7 @@ class JobAgent(dashboard_utils.DashboardAgentModule):
             )
 
         resp = JobLogsResponse(
-            logs=self.get_job_manager().get_job_logs(job.submission_id)
+            logs=self._job_log_client.get_logs(job.submission_id)
         )
         return Response(
             text=json.dumps(dataclasses.asdict(resp)), content_type="application/json"
@@ -170,7 +210,7 @@ class JobAgent(dashboard_utils.DashboardAgentModule):
         job_or_submission_id = req.match_info["job_or_submission_id"]
         job = await find_job_by_ids(
             self._dashboard_agent.gcs_aio_client,
-            self.get_job_manager().job_info_client(),
+            self._get_job_info_client(),
             job_or_submission_id,
         )
         if not job:
@@ -188,17 +228,31 @@ class JobAgent(dashboard_utils.DashboardAgentModule):
         ws = aiohttp.web.WebSocketResponse()
         await ws.prepare(req)
 
-        async for lines in self._job_manager.tail_job_logs(job.submission_id):
+        async for lines in self._tail_job_logs(job.submission_id):
             await ws.send_str(lines)
 
         return ws
 
-    def get_job_manager(self):
-        if not self._job_manager:
-            self._job_manager = JobManager(
-                self._dashboard_agent.gcs_aio_client, self._dashboard_agent.log_dir
-            )
-        return self._job_manager
+    async def _tail_job_logs(self, job_id: str) -> AsyncIterator[str]:
+        if await self._job_info_client.get_job_status(job_id) is None:
+            raise RuntimeError(f"Job '{job_id}' does not exist.")
+
+        for lines in self._job_log_client.tail_logs(job_id):
+            if lines is None:
+                # Return if the job has exited and there are no new log lines.
+                status = await self._job_info_client.get_job_status(job_id)
+                if status.is_terminal():
+                    return
+
+                await asyncio.sleep(self.LOG_TAIL_SLEEP_S)
+            else:
+                yield "".join(lines)
+
+    def _get_job_info_client(self):
+        if not self._job_info_client:
+            self._job_info_client = JobInfoStorageClient(self._dashboard_agent.gcs_aio_client)
+
+        return self._job_info_client
 
     async def run(self, server):
         pass
