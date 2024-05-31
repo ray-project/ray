60fec3f3b9ff716bf96d9daf45a6819e7223dff4
sven1977 <svenmika1977@gmail.com>
2024-05-31 12:03:30 +0200

    minor cleanups
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/algorithms/dreamerv3/dreamerv3_learner.py
+++ rllib/algorithms/dreamerv3/dreamerv3_learner.py
@@ -8,20 +8,23 @@ D. Hafner, T. Lillicrap, M. Norouzi, J. Ba
 https://arxiv.org/pdf/2010.02193.pdf
 """
 from ray.rllib.core.learner.learner import Learner
-from ray.rllib.utils.annotations import override
+from ray.rllib.utils.annotations import (
+    override,
+    OverrideToImplementCustomLogic_CallToSuperRecommended,
+)
 
 
 class DreamerV3Learner(Learner):
     """DreamerV3 specific Learner class.
 
-    Only implements the `additional_update_for_module()` method to define the logic
+    Only implements the `_after_gradient_based_update()` method to define the logic
     for updating the critic EMA-copy after each training step.
     """
 
+    @OverrideToImplementCustomLogic_CallToSuperRecommended
     @override(Learner)
-    def _after_update(self, timesteps):
-
-        super()._after_update(timesteps)
+    def _after_gradient_based_update(self, timesteps):
+        super()._after_gradient_based_update(timesteps)
 
         # Update EMA weights of the critic.
         for module_id, module in self.module._rl_modules.items():
