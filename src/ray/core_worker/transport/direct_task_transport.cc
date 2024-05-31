03367ff5d8d5a316fdda84c961b95810afb6d2fa
Rui Qiao <ruisearch42@gmail.com>
2024-05-30 23:22:57 -0700

    comments
    Signed-off-by: Rui Qiao <ruisearch42@gmail.com>

--- src/ray/core_worker/transport/direct_task_transport.cc
+++ src/ray/core_worker/transport/direct_task_transport.cc
@@ -729,13 +729,15 @@ void CoreWorkerDirectTaskSubmitter::HandleGetTaskFailureCause(
     task_error_type = rpc::ErrorType::NODE_DIED;
     std::stringstream buffer;
     buffer << "Task failed due to the node (where this task was running) "
-           << " was dead or unreachable.\n\nThe node IP: " << addr.ip_address()
-           << ", node ID: " << NodeID::FromBinary(addr.raylet_id())
+           << " was dead or unavailable.\n\nThe node IP: " << addr.ip_address()
+           << ", node ID: " << NodeID::FromBinary(addr.raylet_id()) << "\n\n"
            << "This can happen if the instance where the node was running failed, "
            << "the node was preempted, or raylet crashed unexpectedly "
            << "(e.g., due to OOM) etc.\n\n"
-           << "To see node death information, use `ray list nodes`, "
-           << "or check Ray dashboard cluster page, or search the node ID in GCS log.";
+           << "To see node death information, use `ray list nodes --filter \"node_id="
+           << NodeID::FromBinary(addr.raylet_id()) << "\"`, "
+           << "or check Ray dashboard cluster page, or search the node ID in GCS log, "
+           << "or use `ray logs raylet.out -ip " << addr.ip_address() << "`";
     error_info = std::make_unique<rpc::RayErrorInfo>();
     error_info->set_error_message(buffer.str());
     error_info->set_error_type(rpc::ErrorType::NODE_DIED);
