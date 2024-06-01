54c90aa5f6565f403539f813867eb91d56ba74b6
Kit Lee <7000003+wingkitlee0@users.noreply.github.com>
2024-06-01 12:16:18 -0400

    [Data] Optimizing the multi column groupby
    Changed from a custom class implementation to a purely numpy implementation
    
    Signed-off-by: Kit Lee <7000003+wingkitlee0@users.noreply.github.com>

---
+++ python/ray/data/_internal/boundaries.py
@@ -0,0 +1,26 @@
+from typing import Dict, Union
+
+import numpy as np
+
+
+def get_block_boundaries(
+    block: Union[np.ndarray, Dict[str, np.ndarray]],
+) -> np.ndarray:
+    """Compute block boundaries based on the key(s), that is, a list of
+    starting indices of each group and an end index of the last group.
+
+    Args:
+        block: numpy arrays of the group key(s). This is generally coming from
+            ``BlockAccessor.to_numpy()``.
+
+    Returns:
+        A list of starting indices of each group. The first entry is 0 and
+        the last entry is ``len(array)``.
+    """
+
+    if isinstance(block, dict):
+        # For multiple keys, we create a numpy record array
+        dtype = [(k, v.dtype) for k, v in block.items()]
+        block = np.array(list(zip(*block.values())), dtype=dtype)
+
+    return np.hstack([[0], np.where(block[1:] != block[:-1])[0] + 1, [len(block)]])
