a8a61a0e8836eeaf3e04f02fa4bc9c263622256d
Lonnie Liu <lonnie@anyscale.com>
2024-06-01 17:54:19 +0000

    replace bazel in image on jobs

--- ci/ray_ci/windows/install_tools.sh
+++ ci/ray_ci/windows/install_tools.sh
@@ -2,5 +2,8 @@
 
 set -ex
 
+# Delete the existing bazel and download bazelisk
+powershell ci/ray_ci/windows/install_bazelisk.ps1
+
 powershell ci/pipeline/fix-windows-container-networking.ps1
 pip install awscli
