50b9c58f1914040f38744509b85530b59089dbf6
Hao Chen <chenh1024@gmail.com>
2024-06-03 17:00:23 -0700

    simplify
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- python/ray/data/_internal/planner/planner.py
+++ python/ray/data/_internal/planner/planner.py
@@ -1,77 +1,76 @@
-from typing import Callable, ClassVar, Dict, Generic, List, Tuple, Type, TypeVar
+from typing import Callable, Dict, List, Tuple, Type, TypeVar
 
 from ray.data._internal.execution.interfaces import PhysicalOperator
-from ray.data._internal.execution.operators.limit_operator import LimitOperator
-from ray.data._internal.execution.operators.union_operator import UnionOperator
-from ray.data._internal.execution.operators.zip_operator import ZipOperator
 from ray.data._internal.logical.interfaces import (
     LogicalOperator,
     LogicalPlan,
     PhysicalPlan,
 )
-from ray.data._internal.logical.operators.all_to_all_operator import AbstractAllToAll
-from ray.data._internal.logical.operators.from_operators import AbstractFrom
-from ray.data._internal.logical.operators.input_data_operator import InputData
-from ray.data._internal.logical.operators.map_operator import AbstractUDFMap
-from ray.data._internal.logical.operators.n_ary_operator import Union, Zip
-from ray.data._internal.logical.operators.one_to_one_operator import Limit
-from ray.data._internal.logical.operators.read_operator import Read
-from ray.data._internal.logical.operators.write_operator import Write
-from ray.data._internal.planner.plan_all_to_all_op import plan_all_to_all_op
-from ray.data._internal.planner.plan_from_op import plan_from_op
-from ray.data._internal.planner.plan_input_data_op import plan_input_data_op
-from ray.data._internal.planner.plan_read_op import plan_read_op
-from ray.data._internal.planner.plan_udf_map_op import plan_udf_map_op
-from ray.data._internal.planner.plan_write_op import plan_write_op
+from ray.util.annotations import DeveloperAPI
 
 LogicalOperatorType = TypeVar("LogicalOperatorType", bound=LogicalOperator)
 
-CallbackFn = Callable[[LogicalOperatorType, List[PhysicalOperator]], PhysicalOperator]
+PlanLogicalOpFn = Callable[
+    [LogicalOperatorType, List[PhysicalOperator]], PhysicalOperator
+]
+
+# A list of registered plan functions for logical operators.
+PLAN_LOGICAL_OP_FNS: List[Tuple[Type[LogicalOperator], PlanLogicalOpFn]] = []
 
 
-class PlanLogicalOpFn(Generic[LogicalOperatorType]):
-    def __init__(
-        self, logical_op_type: Type[LogicalOperatorType], callback: CallbackFn
-    ):
-        self._logical_op_type = logical_op_type
-        self._callback = callback
-
-    def should_handle(self, logical_op: LogicalOperator):
-        return isinstance(logical_op, self._logical_op_type)
-
-    def __call__(
-        self, logical_op: LogicalOperator, physical_children: List[PhysicalOperator]
-    ) -> PhysicalOperator:
-        return self._callback(logical_op, physical_children)
-
-
-PLAN_LOGICAL_OP_FNS: List[PlanLogicalOpFn] = []
+@DeveloperAPI
+def register_plan_logical_op_fn(
+    logical_op_type: Type[LogicalOperator],
+    plan_fn: PlanLogicalOpFn,
+):
+    """Register a plan function for a logical operator type."""
+    PLAN_LOGICAL_OP_FNS.append((logical_op_type, plan_fn))
 
 
 def init_plan_logical_op_fns():
-    PLAN_LOGICAL_OP_FNS.append(PlanLogicalOpFn(Read, plan_read_op))
-    PLAN_LOGICAL_OP_FNS.append(PlanLogicalOpFn(InputData, plan_input_data_op))
-    PLAN_LOGICAL_OP_FNS.append(PlanLogicalOpFn(Write, plan_write_op))
-    PLAN_LOGICAL_OP_FNS.append(PlanLogicalOpFn(AbstractFrom, plan_from_op))
-    PLAN_LOGICAL_OP_FNS.append(PlanLogicalOpFn(AbstractUDFMap, plan_udf_map_op))
-    PLAN_LOGICAL_OP_FNS.append(PlanLogicalOpFn(AbstractAllToAll, plan_all_to_all_op))
+    from ray.data._internal.execution.operators.limit_operator import LimitOperator
+    from ray.data._internal.execution.operators.union_operator import UnionOperator
+    from ray.data._internal.execution.operators.zip_operator import ZipOperator
+    from ray.data._internal.logical.operators.all_to_all_operator import (
+        AbstractAllToAll,
+    )
+    from ray.data._internal.logical.operators.from_operators import AbstractFrom
+    from ray.data._internal.logical.operators.input_data_operator import InputData
+    from ray.data._internal.logical.operators.map_operator import AbstractUDFMap
+    from ray.data._internal.logical.operators.n_ary_operator import Union, Zip
+    from ray.data._internal.logical.operators.one_to_one_operator import Limit
+    from ray.data._internal.logical.operators.read_operator import Read
+    from ray.data._internal.logical.operators.write_operator import Write
+    from ray.data._internal.planner.plan_all_to_all_op import plan_all_to_all_op
+    from ray.data._internal.planner.plan_from_op import plan_from_op
+    from ray.data._internal.planner.plan_input_data_op import plan_input_data_op
+    from ray.data._internal.planner.plan_read_op import plan_read_op
+    from ray.data._internal.planner.plan_udf_map_op import plan_udf_map_op
+    from ray.data._internal.planner.plan_write_op import plan_write_op
+
+    register_plan_logical_op_fn(Read, plan_read_op)
+    register_plan_logical_op_fn(InputData, plan_input_data_op)
+    register_plan_logical_op_fn(Write, plan_write_op)
+    register_plan_logical_op_fn(AbstractFrom, plan_from_op)
+    register_plan_logical_op_fn(AbstractUDFMap, plan_udf_map_op)
+    register_plan_logical_op_fn(AbstractAllToAll, plan_all_to_all_op)
 
     def plan_zip_op(_, physical_children):
         assert len(physical_children) == 2
         return ZipOperator(physical_children[0], physical_children[1])
 
-    PLAN_LOGICAL_OP_FNS.append(PlanLogicalOpFn(Zip, plan_zip_op))
+    register_plan_logical_op_fn(Zip, plan_zip_op)
 
     def plan_union_op(_, physical_children):
         assert len(physical_children) >= 2
         return UnionOperator(*physical_children)
 
-    PLAN_LOGICAL_OP_FNS.append(PlanLogicalOpFn(Union, plan_union_op))
+    register_plan_logical_op_fn(Union, plan_union_op)
 
     def plan_limit_op(logical_op, physical_children):
         return LimitOperator(logical_op._limit, physical_children[0])
 
-    PLAN_LOGICAL_OP_FNS.append(PlanLogicalOpFn(Limit, plan_limit_op))
+    register_plan_logical_op_fn(Limit, plan_limit_op)
 
 
 init_plan_logical_op_fns()
@@ -99,8 +98,8 @@ class Planner:
             physical_children.append(self._plan(child))
 
         physical_op = None
-        for plan_fn in PLAN_LOGICAL_OP_FNS:
-            if plan_fn.should_handle(logical_op):
+        for op_type, plan_fn in PLAN_LOGICAL_OP_FNS:
+            if isinstance(logical_op, op_type):
                 physical_op = plan_fn(logical_op, physical_children)
                 break
 
