94bf558c6c7c14ed5555d7f05b5a10e35cd5c4c1
sven1977 <svenmika1977@gmail.com>
2024-06-04 09:55:19 +0200

    torch Stats just like tf Stats to try whether the torch.nanmean() on the Stats.values list is the problem ... to be checked
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/utils/metrics/stats.py
+++ rllib/utils/metrics/stats.py
@@ -608,11 +608,23 @@ class Stats:
         else:
             # Use the numpy/torch "nan"-prefix to ignore NaN's in our value lists.
             if torch and torch.is_tensor(values[0]):
-                reduce_meth = getattr(torch, "nan" + self._reduce_method)
-                reduce_in = torch.stack(values)
-                if self._reduce_method == "mean":
-                    reduce_in = reduce_in.float()
-                reduced = reduce_meth(reduce_in)
+                # TODO (sven): Currently, tensor metrics only work with window=1.
+                #  We might want o enforce it more formally, b/c it's probably not a
+                #  good idea to have MetricsLogger or Stats tinker with the actual
+                #  computation graph that users are trying to build in their loss
+                #  functions.
+                assert len(values) == 1
+                # TODO (sven) Try the same tactic as for tf (which helped make tf
+                #  learn vs completely(!) fail to learn).
+                if len(values[0].shape) == 0:
+                    reduced = values[0]
+                else:
+                    reduce_meth = getattr(torch, "nan" + self._reduce_method)
+                    reduce_in = torch.stack(values)
+                    if self._reduce_method == "mean":
+                        reduce_in = reduce_in.float()
+                    reduced = reduce_meth(reduce_in)
+
             elif tf and tf.is_tensor(values[0]):
                 # TODO (sven): Currently, tensor metrics only work with window=1.
                 #  We might want o enforce it more formally, b/c it's probably not a
