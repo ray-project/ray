30d5a1938aa92c85a4d1239b50c4b2df3d4b2bdb
Alan Guo <aguo@anyscale.com>
2024-06-04 12:13:03 -0700

    fixup
    Signed-off-by: Alan Guo <aguo@anyscale.com>

--- python/ray/train/tests/test_train_head.py
+++ python/ray/train/tests/test_train_head.py
@@ -16,36 +16,33 @@ from ray.train.torch import TorchTrainer
 )
 def test_get_train_runs(monkeypatch, shutdown_only):
     monkeypatch.setenv("RAY_TRAIN_ENABLE_STATE_TRACKING", "1")
-    try:
-        ray.init(num_cpus=8)
 
-        def train_func():
-            print("Training Starts")
-            time.sleep(0.5)
+    ray.init(num_cpus=8)
 
-        datasets = {"train": ray.data.range(100), "val": ray.data.range(100)}
+    def train_func():
+        print("Training Starts")
+        time.sleep(0.5)
 
-        trainer = TorchTrainer(
-            train_func,
-            run_config=RunConfig(
-                name="my_train_run", storage_path="/tmp/cluster_storage"
-            ),
-            scaling_config=ScalingConfig(num_workers=4, use_gpu=False),
-            datasets=datasets,
-        )
-        trainer.fit()
+    datasets = {"train": ray.data.range(100), "val": ray.data.range(100)}
 
-        # Call the train run api
-        url = ray._private.worker.get_dashboard_url()
-        resp = requests.get("http://" + url + "/api/train/runs")
-        assert resp.status_code == 200
-        body = resp.json()
-        assert len(body["train_runs"]) == 1
-        assert body["train_runs"][0]["name"] == "my_train_run"
-        assert len(body["train_runs"][0]["workers"]) == 4
+    trainer = TorchTrainer(
+        train_func,
+        run_config=RunConfig(
+            name="my_train_run", storage_path="/tmp/cluster_storage"
+        ),
+        scaling_config=ScalingConfig(num_workers=4, use_gpu=False),
+        datasets=datasets,
+    )
+    trainer.fit()
 
-    finally:
-        del os.environ["RAY_TRAIN_ENABLE_STATE_TRACKING"]
+    # Call the train run api
+    url = ray._private.worker.get_dashboard_url()
+    resp = requests.get("http://" + url + "/api/train/runs")
+    assert resp.status_code == 200
+    body = resp.json()
+    assert len(body["train_runs"]) == 1
+    assert body["train_runs"][0]["name"] == "my_train_run"
+    assert len(body["train_runs"][0]["workers"]) == 4
 
 
 if __name__ == "__main__":
