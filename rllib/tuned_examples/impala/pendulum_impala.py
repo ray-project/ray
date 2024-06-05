6137312a83c3338e8321e67696fd35ded8a16fdd
sven1977 <svenmika1977@gmail.com>
2024-06-05 20:52:17 +0200

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/tuned_examples/impala/pendulum_impala.py
+++ rllib/tuned_examples/impala/pendulum_impala.py
@@ -9,7 +9,6 @@ from ray.rllib.utils.test_utils import add_rllib_example_script_args
 parser = add_rllib_example_script_args()
 args = parser.parse_args()
 
-
 config = (
     ImpalaConfig()
     # Enable new API stack and use EnvRunner.
@@ -17,28 +16,28 @@ config = (
         enable_rl_module_and_learner=True,
         enable_env_runner_and_connector_v2=True,
     )
-    .environment("CartPole-v1")
+    .env_runners(num_envs_per_env_runner=5)
+    .environment("Pendulum-v1")
     .training(
-        train_batch_size_per_learner=500,
+        train_batch_size_per_learner=1024,
         grad_clip=40.0,
         grad_clip_by="global_norm",
-        lr=0.0005 * (args.num_gpus**0.5),
+        lr=0.0003 * (args.num_gpus ** 0.5),
         vf_loss_coeff=0.05,
-        entropy_coeff=0.0,
+        entropy_coeff=[[0, 0.1], [2000000, 0.0]],
     )
     .rl_module(
         model_config_dict={
-            "vf_share_layers": True,
+            # "vf_share_layers": True,
             "uses_new_env_runners": True,
         },
     )
 )
 
 stop = {
-    f"{ENV_RUNNER_RESULTS}/{EPISODE_RETURN_MEAN}": 450.0,
-    #NUM_ENV_STEPS_SAMPLED_LIFETIME: 2000000,
+    f"{ENV_RUNNER_RESULTS}/{EPISODE_RETURN_MEAN}": -200.0,
+    NUM_ENV_STEPS_SAMPLED_LIFETIME: 5000000,
 }
-
 
 if __name__ == "__main__":
     from ray.rllib.utils.test_utils import run_rllib_example_script_experiment
