25683c5bde96ee445ab939685e232af939a108f2
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2024-06-07 14:46:00 -0700

    Revert "[cpp] use bazelisk in template example (#45805)"
    This reverts commit 2c6a69ad35ff37239c8c8f3c76771d982161239a.

--- cpp/example/run.sh
+++ cpp/example/run.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
-# Cause the script to exit if a single command fails.
+#Cause the script to exit if a single command fails.
 set -e
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")" || exit; pwd)"
 
-if [[ "${USE_BAZEL_VERSION:-}" == "" ]]; then
-    export USE_BAZEL_VERSION=6.5.0
-fi
-
-bazelisk --nosystem_rc --nohome_rc build //:example
-
+bazel --nosystem_rc --nohome_rc build //:example
 if [[ "$OSTYPE" == "darwin"* ]]; then
     DYLD_LIBRARY_PATH="$ROOT_DIR/thirdparty/lib" "${ROOT_DIR}"/bazel-bin/example
 else
