de9f147ea17948d2c43d830436aa32da14b92e45
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2024-06-07 12:28:48 -0700

    [cpp] use bazelisk in template example (#45805)
    and use bazel 6.5.0 for recommendation
    
    the example that we generates does not work with bazel 7.x
    
    Signed-off-by: Lonnie Liu <lonnie@anyscale.com>
    Signed-off-by: Richard Liu <ricliu@google.com>

--- cpp/example/run.sh
+++ cpp/example/run.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-#Cause the script to exit if a single command fails.
+# Cause the script to exit if a single command fails.
 set -e
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")" || exit; pwd)"
 
-bazel --nosystem_rc --nohome_rc build //:example
+if [[ "${USE_BAZEL_VERSION:-}" == "" ]]; then
+    export USE_BAZEL_VERSION=6.5.0
+fi
+
+bazelisk --nosystem_rc --nohome_rc build //:example
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
     DYLD_LIBRARY_PATH="$ROOT_DIR/thirdparty/lib" "${ROOT_DIR}"/bazel-bin/example
 else
