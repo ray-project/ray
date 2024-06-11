db94e46ffb13235894dae52a4c47d47643115b1b
sven1977 <svenmika1977@gmail.com>
2024-06-11 17:44:04 +0200

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/examples/connectors/prev_actions_prev_rewards.py
+++ rllib/examples/connectors/prev_actions_prev_rewards.py
@@ -1,4 +1,4 @@
-"""Example using connectors (V2) for observation frame-stacking in Atari environments.
+"""Example using a ConnectorV2 to add previous rewards/actions to an RLModule's input.
 
 An RLlib Algorithm has 3 distinct connector pipelines:
 - An env-to-module pipeline in an EnvRunner accepting a list of episodes and producing
@@ -20,10 +20,11 @@ pieces (or use the ones available already in RLlib) and add them to one of the 3
 different pipelines described above, as required.
 
 This example:
-    - shows how the `FrameStackingEnvToModule` ConnectorV2 piece can be added to the
-    env-to-module pipeline.
-    - shows how the `FrameStackingLearner` ConnectorV2 piece can be added to the
-    learner connector pipeline.
+    - shows how the `PrevActionsPrevRewardsConnector` piece can be added to the
+    env-to-module pipeline to extract previous rewards and/or actions from the ongoing
+    episodes.
+    - shows how this connector creates  and wraps this information together to the RLModule's
+    original observation and .
     - demonstrates that using these two pieces (rather than performing framestacking
     already inside the environment using a gymnasium wrapper) increases overall
     performance by about 5%.
@@ -146,10 +147,7 @@ if __name__ == "__main__":
             train_batch_size=4000,
             vf_loss_coeff=0.01,
         )
-    )
-
-    if args.enable_new_api_stack:
-        config = config.rl_module(
+        .rl_module(
             model_config_dict={
                 "use_lstm": True,
                 "max_seq_len": 50,
@@ -161,20 +159,7 @@ if __name__ == "__main__":
                 "uses_new_env_runners": True,
             }
         )
-    else:
-        config = config.training(
-            model=dict(
-                {
-                    "use_lstm": True,
-                    "max_seq_len": 50,
-                    "fcnet_hiddens": [32],
-                    "fcnet_activation": "linear",
-                    "vf_share_layers": True,
-                    "fcnet_weights_initializer": nn.init.xavier_uniform_,
-                    "fcnet_bias_initializer": functools.partial(nn.init.constant_, 0.0),
-                }
-            )
-        )
+    )
 
     # Add a simple multi-agent setup.
     if args.num_agents > 0:
