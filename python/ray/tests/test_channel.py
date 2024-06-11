31b08ec570dbb49180a9e005de09c7039dd2a85f
jackhumphries <1645405+jackhumphries@users.noreply.github.com>
2024-06-11 09:51:47 -0700

    [core][experimental] Driver on different node can read from accelerated DAG output (#45786)
    This PR adds support for the driver reading from the accelerated DAG
    output. Previously, this was not possible unless the output writer
    (i.e., the last task in the DAG) was on the same node as the driver,
    which may not always be the case.
    
    To support this, the output writer needs to be able to invoke remote
    functions on the driver. This is necessary so that the output writer can
    create a reader ref on the driver node, and later potentially create a
    larger reader ref on the driver node if the channel backing store needs
    to be resized. However, remote functions cannot be invoked on the
    driver.
    
    This PR adds a DriverActor that an accelerated DAG creates when it is
    initialized. This DriverActor has an empty implementation, though it
    serves as a way for the output writer to invoke remote functions on the
    driver node. The workflow for creating a channel that has the driver as
    a reader is this:
    1. Output writer creates a writer ref on its own node.
    2. Output writer invokes a remote function on DriverActor to create a
    reader ref on the driver node.
    3. Driver gets the channel from the output writer, which already
    includes the reader ref that the DriverActor created.
    
    The workflow for resizing a channel backing store is similar:
    1. Output writer detects that the channel backing store must be resized,
    so it creates a new writer ref on its own node.
    2. Output writer invokes a remote function on DriverActor to create a
    new reader ref on the driver node.
    3. Output writer sends a `_ResizeChannel` message on the channel via the
    old writer-ref/reader-ref pair. The `_ResizeChannel` message includes
    the new reader ref.
    4. Driver gets `_ResizeChannel` message from the output writer on the
    old reader ref. The driver then updates its channel to use the new
    reader ref, which is included in the `_ResizeChannel` message. Note that
    the DriverActor does not read from the channel, so the driver receives
    this message.
    
    ---------
    
    Signed-off-by: Jack Humphries <1645405+jackhumphries@users.noreply.github.com>
    Signed-off-by: Richard Liu <ricliu@google.com>

--- python/ray/tests/test_channel.py
+++ python/ray/tests/test_channel.py
@@ -10,8 +10,18 @@ import pytest
 import ray
 import ray.cluster_utils
 import ray.experimental.channel as ray_channel
+from ray.util.scheduling_strategies import NodeAffinitySchedulingStrategy
+from ray.dag.compiled_dag_node import CompiledDAG
 
 logger = logging.getLogger(__name__)
+
+
+def create_driver_actor():
+    return CompiledDAG.DAGDriverProxyActor.options(
+        scheduling_strategy=NodeAffinitySchedulingStrategy(
+            ray.get_runtime_context().get_node_id(), soft=False
+        )
+    ).remote()
 
 
 @pytest.mark.skipif(
@@ -19,10 +29,7 @@ logger = logging.getLogger(__name__)
     reason="Requires Linux or Mac.",
 )
 def test_put_local_get(ray_start_regular):
-    """
-    Tests that the driver can write and read from a channel.
-    """
-    chan = ray_channel.Channel(None, [None], 1000)
+    chan = ray_channel.Channel(None, [create_driver_actor()], 1000)
 
     num_writes = 1000
     for i in range(num_writes):
@@ -30,6 +37,97 @@ def test_put_local_get(ray_start_regular):
         chan.write(val)
         assert chan.begin_read() == val
         chan.end_read()
+
+
+@pytest.mark.skipif(
+    sys.platform != "linux" and sys.platform != "darwin",
+    reason="Requires Linux or Mac.",
+)
+@pytest.mark.parametrize("remote", [True, False])
+def test_driver_as_reader(ray_start_cluster, remote):
+    cluster = ray_start_cluster
+    if remote:
+        # This node is for the driver. num_cpus is 1 because the
+        # CompiledDAG.DAGDriverProxyActor needs a place to run.
+        cluster.add_node(num_cpus=1)
+        ray.init(address=cluster.address)
+        # This node is for the writer actor.
+        cluster.add_node(num_cpus=1)
+    else:
+        # This node is for both the driver (including the DriverHelperActor) and the
+        # writer actor.
+        cluster.add_node(num_cpus=2)
+        ray.init(address=cluster.address)
+
+    @ray.remote(num_cpus=1)
+    class Actor:
+        def setup(self, driver_actor):
+            self._channel = ray_channel.Channel(
+                ray.get_runtime_context().current_actor,
+                [driver_actor],
+                1000,
+            )
+
+        def get_channel(self):
+            return self._channel
+
+        def write(self):
+            self._channel.write(b"x")
+
+    a = Actor.remote()
+    ray.get(a.setup.remote(create_driver_actor()))
+    chan = ray.get(a.get_channel.remote())
+
+    ray.get(a.write.remote())
+    assert chan.begin_read() == b"x"
+    chan.end_read()
+
+
+@pytest.mark.parametrize("remote", [True, False])
+def test_driver_as_reader_with_resize(ray_start_cluster, remote):
+    cluster = ray_start_cluster
+    if remote:
+        # This node is for the driver. num_cpus is 1 because the
+        # CompiledDAG.DAGDriverProxyActor needs a place to run.
+        cluster.add_node(num_cpus=1)
+        ray.init(address=cluster.address)
+        # This node is for the writer actor.
+        cluster.add_node(num_cpus=1)
+    else:
+        # This node is for both the driver (including the DriverHelperActor) and the
+        # writer actor.
+        cluster.add_node(num_cpus=2)
+        ray.init(address=cluster.address)
+
+    @ray.remote(num_cpus=1)
+    class Actor:
+        def setup(self, driver_actor):
+            self._channel = ray_channel.Channel(
+                ray.get_runtime_context().current_actor,
+                [driver_actor],
+                1000,
+            )
+
+        def get_channel(self):
+            return self._channel
+
+        def write(self):
+            self._channel.write(b"x")
+
+        def write_large(self):
+            self._channel.write(b"x" * 2000)
+
+    a = Actor.remote()
+    ray.get(a.setup.remote(create_driver_actor()))
+    chan = ray.get(a.get_channel.remote())
+
+    ray.get(a.write.remote())
+    assert chan.begin_read() == b"x"
+    chan.end_read()
+
+    ray.get(a.write_large.remote())
+    assert chan.begin_read() == b"x" * 2000
+    chan.end_read()
 
 
 @pytest.mark.skipif(
@@ -110,7 +208,7 @@ def test_errors(ray_start_regular):
 
     a = Actor.remote()
     # Multiple consecutive reads from the same process are fine.
-    chan = ray.get(a.make_chan.remote([None], do_write=True))
+    chan = ray.get(a.make_chan.remote([create_driver_actor()], do_write=True))
     assert chan.begin_read() == b"hello"
     chan.end_read()
 
@@ -137,10 +235,7 @@ def test_errors(ray_start_regular):
     reason="Requires Linux or Mac.",
 )
 def test_put_different_meta(ray_start_regular):
-    """
-    Tests that different object/primitive types can be passed through the same channel.
-    """
-    chan = ray_channel.Channel(None, [None], 1000)
+    chan = ray_channel.Channel(None, [create_driver_actor()], 1000)
 
     def _test(val):
         chan.write(val)
@@ -204,7 +299,7 @@ def test_resize_channel_on_same_node(ray_start_regular):
     Tests that the channel backing store is automatically increased when a large object
     is written to it. The writer and reader are on the same node.
     """
-    chan = ray_channel.Channel(None, [None], 1000)
+    chan = ray_channel.Channel(None, [create_driver_actor()], 1000)
 
     def _test(val):
         chan.write(val)
