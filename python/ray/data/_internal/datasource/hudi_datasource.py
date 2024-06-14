19d1df1933abb72994971b103bb6ff69aafdb310
Shiyan Xu <2701446+xushiyan@users.noreply.github.com>
2024-06-14 12:01:49 -0500

    [data][api] implement `HudiDataSource`
    @microcheck //python:ray/data/tests/test_hudi
    
    Signed-off-by: Shiyan Xu <2701446+xushiyan@users.noreply.github.com>

---
+++ python/ray/data/_internal/datasource/hudi_datasource.py
@@ -0,0 +1,87 @@
+import logging
+from functools import partial
+from pathlib import PurePosixPath
+from typing import Dict, Iterator, List, Optional
+from urllib.parse import urljoin
+
+from ray.data._internal.util import _check_import
+from ray.data.block import BlockMetadata
+from ray.data.datasource.datasource import Datasource, ReadTask
+
+logger = logging.getLogger(__name__)
+
+
+class HudiDatasource(Datasource):
+    """Hudi datasource, for reading Apache Hudi table."""
+
+    def __init__(
+        self,
+        table_uri: str,
+        storage_options: Optional[Dict[str, str]] = None,
+    ):
+        _check_import(self, module="hudi", package="hudi-python")
+
+        self._table_uri = table_uri
+        self._storage_options = storage_options
+
+    def get_read_tasks(self, parallelism: int) -> List["ReadTask"]:
+        import pyarrow
+        from hudi import HudiFileGroupReader, HudiTable
+
+        def _perform_read(
+            table_uri: str,
+            base_file_paths: List[str],
+            options: Dict[str, str],
+        ) -> Iterator["pyarrow.Table"]:
+            for p in base_file_paths:
+                fg_reader = HudiFileGroupReader(table_uri, options)
+                batches = fg_reader.read_file_slice_by_base_file_path(p)
+                yield pyarrow.Table.from_batches(batches)
+
+        hudi_table = HudiTable(self._table_uri, self._storage_options)
+
+        reader_options = {}
+        reader_options.update(hudi_table.storage_options())
+        reader_options.update(hudi_table.hudi_options())
+
+        schema = hudi_table.get_schema()
+        read_tasks = []
+        for file_slices in hudi_table.split_file_slices(parallelism):
+            if len(file_slices) <= 0:
+                continue
+
+            num_rows = 0
+            relative_paths = []
+            input_files = []
+            size_bytes = 0
+            for f in file_slices:
+                num_rows += f.num_records
+                relative_path = f.base_file_relative_path()
+                relative_paths.append(relative_path)
+                full_path = urljoin(
+                    self._table_uri, PurePosixPath(relative_path).as_posix()
+                )
+                input_files.append(full_path)
+                size_bytes += f.base_file_size
+
+            metadata = BlockMetadata(
+                num_rows=num_rows,
+                schema=schema,
+                input_files=input_files,
+                size_bytes=size_bytes,
+                exec_stats=None,
+            )
+
+            read_task = ReadTask(
+                read_fn=partial(
+                    _perform_read, self._table_uri, relative_paths, reader_options
+                ),
+                metadata=metadata,
+            )
+            read_tasks.append(read_task)
+
+        return read_tasks
+
+    def estimate_inmemory_data_size(self) -> Optional[int]:
+        # TODO(xushiyan) add APIs to provide estimated in-memory size
+        return None
