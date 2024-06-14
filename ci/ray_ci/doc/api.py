d844d63d9ad932c1311cf0e393f9233ac0346c1b
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2024-06-13 18:02:51 -0700

    [doc][api/7] handle ~ character in autodoc (#45858)
    `~` is a command in autodoc to truncate the fullname into the attribute
    name in autodoc. For example, in
    https://github.com/ray-project/ray/blob/master/doc/source/data/api/preprocessor.rst,
    `~preprocessor.Preprocessor.fit` tell autodoc to render the api by only
    it's attribute name `fit` instead of its fullname.
    
    For the purpose of our analysis, we should ignore this `~` character.
    
    Test:
    - CI
    
    ---------
    
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/doc/api.py
+++ ci/ray_ci/doc/api.py
@@ -8,6 +8,9 @@ from typing import Optional, List
 
 _SPHINX_AUTOSUMMARY_HEADER = ".. autosummary::"
 _SPHINX_AUTOCLASS_HEADER = ".. autoclass::"
+# This is a special character used in autosummary to render only the api shortname, for
+# example ~module.api_name will render only api_name
+_SPHINX_AUTODOC_SHORTNAME = "~"
 
 
 class AnnotationType(Enum):
@@ -61,9 +64,8 @@ class API:
                 # end of autosummary, \s means empty space, this line is checking if
                 # the line is not empty and not starting with empty space
                 break
-            api_name = (
-                f"{current_module}.{line.strip()}" if current_module else line.strip()
-            )
+            attribute = line.strip().removeprefix(_SPHINX_AUTODOC_SHORTNAME)
+            api_name = f"{current_module}.{attribute}" if current_module else attribute
             apis.append(
                 API(
                     name=api_name,
@@ -86,7 +88,11 @@ class API:
         doc = doc.strip()
         if not doc.startswith(_SPHINX_AUTOCLASS_HEADER):
             return None
-        cls = doc[len(_SPHINX_AUTOCLASS_HEADER) :].strip()
+        cls = (
+            doc[len(_SPHINX_AUTOCLASS_HEADER) :]
+            .strip()
+            .removeprefix(_SPHINX_AUTODOC_SHORTNAME)
+        )
         api_name = f"{current_module}.{cls}" if current_module else cls
 
         return API(
