898d051e98e442754e198f00b6e076d17574f62c
Kai-Hsun Chen <kaihsun@anyscale.com>
2024-06-14 09:26:36 -0700

    [core][experimental] Fix CI errors and improve __repr__ (#45950)
    This PR fixes the incompatibility between #45687 and #45230.
    
    * Use `ray.get(...)` instead of `begin_read` and `end_read`.
    * Update the `__repr__` function of the `CompiledDAGRef`.
    * Only the shared memory channel has `_reader_ref`, so it will have
    issues when the channel is `CompositeChannel`. We should not have
    channel-specific logic in this file. If we include channel-specific
    logic and import some channels, it is highly likely to cause a circular
    import. Instead, a better approach is to print the channels, and the
    channels should define what is useful for debugging by overwriting
    `__repr__` by themselves.
    
    After the update, when we print `CompiledDAGRef`, it will look like
    this:
    
    ```
    CompiledDAGRef(_dag=<ray.dag.compiled_dag_node.CompiledDAG object at 0x7f483c12b820>, _execution_index=0, _ray_get_called=False, _dag_output_channels=CompositeChannel(_channels={Channel(_reader_ref=ObjectRef(001b78d7db9a6593d5a10ceecdc746e06962d0e40100000002e1f505))}))
    ```
    
    ---------
    
    Signed-off-by: kaihsun <kaihsun@anyscale.com>

--- python/ray/experimental/channel/shared_memory_channel.py
+++ python/ray/experimental/channel/shared_memory_channel.py
@@ -361,6 +361,9 @@ class Channel(ChannelInterface):
             self._reader_ref,
         )
 
+    def __repr__(self) -> str:
+        return f"Channel(_reader_ref={self._reader_ref})"
+
     def _resize_channel_if_needed(self, serialized_value: str):
         # serialized_value.total_bytes *only* includes the size of the data. It does not
         # include the size of the metadata, so we must account for the size of the
@@ -543,10 +546,16 @@ class CompositeChannel(ChannelInterface):
             self._channels,
         )
 
+    def __str__(self) -> str:
+        return f"CompositeChannel(_channels={self._channels})"
+
     def write(self, value: Any):
         self.ensure_registered_as_writer()
         for channel in self._channels:
             channel.write(value)
+
+    def read(self) -> Any:
+        return self.begin_read()
 
     def begin_read(self) -> Any:
         self.ensure_registered_as_reader()
