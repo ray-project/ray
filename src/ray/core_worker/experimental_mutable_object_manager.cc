86f2888d80c536c88ffa8682014811bee77c318f
Stephanie Wang <swang@cs.berkeley.edu>
2024-06-20 14:20:23 -0700

    x
    Signed-off-by: Stephanie Wang <swang@cs.berkeley.edu>

--- src/ray/core_worker/experimental_mutable_object_manager.cc
+++ src/ray/core_worker/experimental_mutable_object_manager.cc
@@ -275,8 +275,7 @@ Status MutableObjectManager::ReadAcquire(const ObjectID &object_id,
   }
   PlasmaObjectHeader::Semaphores sem;
   if (!GetSemaphores(object_id, sem)) {
-    return Status::ChannelError(
-        "Channel has not been registered (cannot get semaphores)");
+    return Status::IOError("Channel has not been registered (cannot get semaphores)");
   }
 
   // Check whether the channel has an error set before checking that we are the only
