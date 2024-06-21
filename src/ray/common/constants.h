842b60d167c0e1589ee2a6698bd6f761f1db0199
votrou <connorvotroubek@gmail.com>
2024-06-20 23:06:21 -0700

    change versions

--- src/ray/common/constants.h
+++ src/ray/common/constants.h
@@ -61,7 +61,7 @@ constexpr int kMessagePackOffset = 9;
 constexpr char kSetupWorkerFilename[] = "setup_worker.py";
 
 /// The version of Ray
-constexpr char kRayVersion[] = "2.9.3";
+constexpr char kRayVersion[] = "2.9.3+pinterest5";
 
 /*****************************/
 /* ENV labels for autoscaler */
