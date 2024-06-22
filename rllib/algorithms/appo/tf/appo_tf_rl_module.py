6fdebca288f3a2475bf95ba7bbbb59f5c0ae6a7e
sven1977 <svenmika1977@gmail.com>
2024-06-22 18:17:08 +0200

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/algorithms/appo/tf/appo_tf_rl_module.py
+++ rllib/algorithms/appo/tf/appo_tf_rl_module.py
@@ -16,7 +16,7 @@ from ray.rllib.utils.nested_dict import NestedDict
 _, tf, _ = try_import_tf()
 
 
-class APPOTfRLModule(PPOTfRLModule, APPORLModule, RLModuleWithTargetNetworksInterface):
+class APPOTfRLModule(PPOTfRLModule, APPORLModule):
     @override(PPOTfRLModule)
     def setup(self):
         super().setup()
