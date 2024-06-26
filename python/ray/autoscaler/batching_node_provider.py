45c5b230f232b0e5a3554e8b409a6dbce3b6a3ae
Ryan O'Leary <ryanaoleary@google.com>
2024-06-26 22:15:09 +0000

    Add logger info statement for multi-host scaling
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>

--- python/ray/autoscaler/batching_node_provider.py
+++ python/ray/autoscaler/batching_node_provider.py
@@ -253,4 +253,8 @@ class BatchingNodeProvider(NodeProvider):
                 # Check if worker has already been scheduled to delete
                 if worker_id not in self.scale_request.workers_to_delete:
                     self.scale_request.workers_to_delete.add(worker_id)
+                    logger.info(
+                        f"Autoscaler terminating node {node_id}"
+                        f"in multi-host replica {node_replica_index}."
+                    )
         self.scale_change_needed = True
