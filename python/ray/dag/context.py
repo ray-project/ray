bb49041ea4bcdc77636334e466fa3d6ac102826d
kaihsun <kaihsun@anyscale.com>
2024-06-26 01:40:50 +0000

    add comments
    Signed-off-by: kaihsun <kaihsun@anyscale.com>

--- python/ray/dag/context.py
+++ python/ray/dag/context.py
@@ -19,12 +19,32 @@ DEFAULT_MAX_BUFFERED_RESULTS = int(os.environ.get("RAY_DAG_max_buffered_results"
 @DeveloperAPI
 @dataclass
 class DAGContext:
+    """Global settings for Ray DAG.
+
+    Args:
+        buffer_size_bytes: The maximum size of messages that can be passed
+            between tasks in the DAG.
+        async_max_queue_size: The max queue size for the async execution.
+        max_buffered_results: The maximum number of execution results that
+            are allowed to be buffered. Setting a higher value allows more
+            DAGs to be executed before `ray.get()` must be called but also
+            increases the memory usage. Note that if the number of ongoing
+            executions is beyond the DAG capacity, the new execution would
+            be blocked in the first place; therefore, this limit is only
+            enforced when it is smaller than the DAG capacity.
+    """
+
     buffer_size_bytes: int = DEFAULT_BUFFER_SIZE_BYTES
     async_max_queue_size: int = DEFAULT_ASYNC_MAX_QUEUE_SIZE
     max_buffered_results: int = DEFAULT_MAX_BUFFERED_RESULTS
 
     @staticmethod
     def get_current() -> "DAGContext":
+        """Get or create a singleton context.
+
+        If the context has not yet been created in this process, it will be
+        initialized with default settings.
+        """
         global _default_context
 
         with _context_lock:
