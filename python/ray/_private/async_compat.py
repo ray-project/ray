7d6fe9613dad486e9b464563d3f249529c70bf9e
Josh Karpel <josh.karpel@gmail.com>
2024-06-26 11:26:46 -0500

    drop tricky cache
    Signed-off-by: Josh Karpel <josh.karpel@gmail.com>

--- python/ray/_private/async_compat.py
+++ python/ray/_private/async_compat.py
@@ -28,7 +28,6 @@ def try_install_uvloop():
         pass
 
 
-@lru_cache(maxsize=2**10)
 def is_async_func(func) -> bool:
     """Return True if the function is an async or async generator method."""
     return inspect.iscoroutinefunction(func) or inspect.isasyncgenfunction(func)
@@ -37,16 +36,7 @@ def is_async_func(func) -> bool:
 @lru_cache(maxsize=2**10)
 def has_async_methods(cls: object) -> bool:
     """Return True if the class has any async methods."""
-    return bool(  # a non-empty result becomes True
-        inspect.getmembers(
-            # We need to check whether the member is a method first,
-            # because the @lru_cache on is_async_func can't handle
-            # non-hashable types, and any given class might have some
-            # non-hashable members.
-            cls,
-            predicate=lambda m: inspect.ismethod(m) and is_async_func(m),
-        )
-    )
+    return len(inspect.getmembers(cls, predicate=is_async_func)) > 0
 
 
 @lru_cache(maxsize=2**10)
