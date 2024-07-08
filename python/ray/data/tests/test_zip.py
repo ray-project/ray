b6df226a3c8b9fc5d8d4e7d078b2638561442ae0
Scott Lee <sjl@anyscale.com>
2024-07-08 14:58:38 -0700

    snapshot metadata only
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- python/ray/data/tests/test_zip.py
+++ python/ray/data/tests/test_zip.py
@@ -82,24 +82,16 @@ def test_zip_pandas(ray_start_regular_shared):
     ds2 = ray.data.from_pandas(pd.DataFrame({"col3": ["a", "b"], "col4": ["d", "e"]}))
     ds = ds1.zip(ds2)
     assert ds.count() == 2
-
+    assert "{col1: int64, col2: int64, col3: object, col4: object}" in str(ds)
     result = list(ds.take())
     assert result[0] == {"col1": 1, "col2": 4, "col3": "a", "col4": "d"}
-
-    # Execute the dataset to get full schema.
-    ds = ds.materialize()
-    assert "{col1: int64, col2: int64, col3: object, col4: object}" in str(ds)
 
     ds3 = ray.data.from_pandas(pd.DataFrame({"col2": ["a", "b"], "col4": ["d", "e"]}))
     ds = ds1.zip(ds3)
     assert ds.count() == 2
-
+    assert "{col1: int64, col2: int64, col2_1: object, col4: object}" in str(ds)
     result = list(ds.take())
     assert result[0] == {"col1": 1, "col2": 4, "col2_1": "a", "col4": "d"}
-
-    # Execute the dataset to get full schema.
-    ds = ds.materialize()
-    assert "{col1: int64, col2: int64, col2_1: object, col4: object}" in str(ds)
 
 
 def test_zip_arrow(ray_start_regular_shared):
@@ -107,24 +99,16 @@ def test_zip_arrow(ray_start_regular_shared):
     ds2 = ray.data.range(5).map(lambda r: {"a": r["id"] + 1, "b": r["id"] + 2})
     ds = ds1.zip(ds2)
     assert ds.count() == 5
-
+    assert "{id: int64, a: int64, b: int64}" in str(ds)
     result = list(ds.take())
     assert result[0] == {"id": 0, "a": 1, "b": 2}
-
-    # Execute the dataset to get full schema.
-    ds = ds.materialize()
-    assert "{id: int64, a: int64, b: int64}" in str(ds)
 
     # Test duplicate column names.
     ds = ds1.zip(ds1).zip(ds1)
     assert ds.count() == 5
-
+    assert "{id: int64, id_1: int64, id_2: int64}" in str(ds)
     result = list(ds.take())
     assert result[0] == {"id": 0, "id_1": 0, "id_2": 0}
-
-    # Execute the dataset to get full schema.
-    ds = ds.materialize()
-    assert "{id: int64, id_1: int64, id_2: int64}" in str(ds)
 
 
 def test_zip_multiple_block_types(ray_start_regular_shared):
