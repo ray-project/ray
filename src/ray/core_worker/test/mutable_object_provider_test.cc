3d1af943464782fa058b74fe3238050ed97883fc
kaihsun <kaihsun@anyscale.com>
2024-07-12 16:27:24 +0000

    fix CI error
    Signed-off-by: kaihsun <kaihsun@anyscale.com>

--- src/ray/core_worker/test/mutable_object_provider_test.cc
+++ src/ray/core_worker/test/mutable_object_provider_test.cc
@@ -119,8 +119,7 @@ class TestInterface : public MutableObjectReaderInterface {
       const ObjectID &object_id,
       uint64_t data_size,
       uint64_t metadata_size,
-      void *data,
-      const rpc::ClientCallback<rpc::PushMutableObjectReply> &callback) override {
+      void *data) override {
     absl::MutexLock guard(&lock_);
     pushed_objects_.push_back(object_id);
   }
