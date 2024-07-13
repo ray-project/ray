1a369e17d18698fd6c81f72e496e4c1bb535cef7
Yunxuan Xiao <yunxuanx@anyscale.com>
2024-07-12 20:55:30 -0700

    [Train] Update run status and actor status for train runs. (#46395)
    Added two entries for `TrainRunInfo`:
    - TrainRunInfo.run_status: `STARTED`, `FINISHED`, `ERRORED`, `ABORTED`
    - TrainRunInfo.controller_actor_status: `ALIVE`, `DEAD`
    - WorkerInfo.status: `ALIVE`, `DEAD`
    
    Signed-off-by: yunxuanx <yunxuanx@anyscale.com>
    Signed-off-by: Yunxuan Xiao <xiaoyunxuan1998@gmail.com>
    Co-authored-by: Alan Guo <aguo@aguo.software>

--- python/ray/dashboard/modules/train/train_head.py
+++ python/ray/dashboard/modules/train/train_head.py
@@ -5,6 +5,8 @@ from aiohttp.web import Request, Response
 import ray
 import ray.dashboard.optional_utils as dashboard_optional_utils
 import ray.dashboard.utils as dashboard_utils
+from ray.core.generated import gcs_service_pb2, gcs_service_pb2_grpc
+from ray.dashboard.modules.actor.actor_head import actor_table_data_to_dict
 from ray.dashboard.modules.job.common import JobInfoStorageClient
 from ray.dashboard.modules.job.utils import find_jobs_by_job_ids
 from ray.util.annotations import DeveloperAPI
@@ -20,10 +22,9 @@ class TrainHead(dashboard_utils.DashboardHeadModule):
         super().__init__(dashboard_head)
         self._train_stats_actor = None
         self._job_info_client = None
+        self._gcs_actor_info_stub = None
 
-    # TODO(aguo): Update this to a "v2" path since I made a backwards-incompatible
-    # change. Will do so after the API is more stable.
-    @routes.get("/api/train/runs")
+    @routes.get("/api/train/v2/runs")
     @dashboard_optional_utils.init_ray_and_catch_exceptions()
     @DeveloperAPI
     async def get_train_runs(self, req: Request) -> Response:
@@ -57,6 +58,7 @@ class TrainHead(dashboard_utils.DashboardHeadModule):
         else:
             try:
                 train_runs = await stats_actor.get_all_train_runs.remote()
+                await self._add_actor_status_and_update_run_status(train_runs)
                 # Sort train runs in reverse chronological order
                 train_runs = sorted(
                     train_runs.values(),
@@ -92,6 +94,44 @@ class TrainHead(dashboard_utils.DashboardHeadModule):
             content_type="application/json",
         )
 
+    async def _add_actor_status_and_update_run_status(self, train_runs):
+        from ray.train._internal.state.schema import ActorStatusEnum, RunStatusEnum
+
+        actor_status_table = {}
+        try:
+            logger.info("Getting all actor info from GCS.")
+            request = gcs_service_pb2.GetAllActorInfoRequest()
+            reply = await self._gcs_actor_info_stub.GetAllActorInfo(request, timeout=5)
+            if reply.status.code == 0:
+                for message in reply.actor_table_data:
+                    actor_table_data = actor_table_data_to_dict(message)
+                    actor_status_table[actor_table_data["actorId"]] = actor_table_data[
+                        "state"
+                    ]
+        except Exception:
+            logger.exception("Error Getting all actor info from GCS.")
+
+        for train_run in train_runs.values():
+            for worker_info in train_run.workers:
+                worker_info.status = actor_status_table.get(worker_info.actor_id, None)
+
+            # The train run can be unexpectedly terminated before the final run
+            # status was updated. This could be due to errors outside of the training
+            # function (e.g., system failure or user interruption) that crashed the
+            # train controller.
+            # We need to detect this case and mark the train run as ABORTED.
+            controller_actor_status = actor_status_table.get(
+                train_run.controller_actor_id, None
+            )
+            if (
+                controller_actor_status == ActorStatusEnum.DEAD
+                and train_run.run_status == RunStatusEnum.STARTED
+            ):
+                train_run.run_status = RunStatusEnum.ABORTED
+                train_run.status_detail = (
+                    "Unexpectedly terminated due to system errors."
+                )
+
     @staticmethod
     def is_minimal_module():
         return False
@@ -101,6 +141,11 @@ class TrainHead(dashboard_utils.DashboardHeadModule):
             self._job_info_client = JobInfoStorageClient(
                 self._dashboard_head.gcs_aio_client
             )
+
+        gcs_channel = self._dashboard_head.aiogrpc_gcs_channel
+        self._gcs_actor_info_stub = gcs_service_pb2_grpc.ActorInfoGcsServiceStub(
+            gcs_channel
+        )
 
     async def get_train_stats_actor(self):
         """
