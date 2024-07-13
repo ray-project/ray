1b64f6e588b240b0e029015053ee180b5876a8e9
Lonnie Liu <lonnie@anyscale.com>
2024-07-12 20:40:24 -0700

    try the other one..

--- .buildkite/release-automation/verify-windows-wheels-in-docker.sh
+++ .buildkite/release-automation/verify-windows-wheels-in-docker.sh
@@ -10,7 +10,7 @@ RAY_COMMIT="${RAY_COMMIT:-607f2f30f5f21543b6a5568ee77ea779eeba30a8}"
 # Download winpty
 mkdir -p /c/tmp/winpty
 which curl
-wget -q https://github.com/rprichard/winpty/releases/download/0.4.3/winpty-0.4.3-cygwin-2.8.0-x64.tar.gz -O /c/tmp/winpty/winpty.tar.gz
+wget -q https://github.com/rprichard/winpty/releases/download/0.4.3/winpty-0.4.3-msys2-2.7.0-x64.tar.gz -O /c/tmp/winpty/winpty.tar.gz
 tar -xzf /c/tmp/winpty/winpty.tar.gz -C /c/tmp/winpty
 
 # Setup verify context
@@ -23,6 +23,6 @@ cp /c/workdir/release/util/sanity_check.py /c/tmp/verify/release/util/sanity_che
 
 docker build -t rayproject/win-verifier /c/tmp/verify
 
-/c/tmp/winpty/winpty-0.4.3-cygwin-2.8.0-x64/bin/winpty.exe docker run -it --rm --env RAY_VERSION --env RAY_COMMIT rayproject/win-verifier
+/c/tmp/winpty/winpty-0.4.3-msys2-2.7.0-x64/bin/winpty.exe docker run -it --rm --env RAY_VERSION --env RAY_COMMIT rayproject/win-verifier
 
 # python -c "import time; time.sleep(1000000)"
