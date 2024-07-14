68e61533cbc99a264caa6dd98e16e135a38398c1
MJ <m.jovanovic9999@gmail.com>
2024-07-14 14:38:30 +0200

    formatting step
    Signed-off-by: MJ <m.jovanovic9999@gmail.com>

--- python/ray/train/examples/tf/tune_tensorflow_autoencoder_example.py
+++ python/ray/train/examples/tf/tune_tensorflow_autoencoder_example.py
@@ -2,11 +2,11 @@ import argparse
 
 import ray
 from ray import tune
+from ray.train import ScalingConfig
 from ray.train.examples.tf.tensorflow_mnist_example import train_func
 from ray.train.tensorflow import TensorflowTrainer
 from ray.tune.tune_config import TuneConfig
 from ray.tune.tuner import Tuner
-from ray.train import ScalingConfig
 
 
 def tune_tensorflow_mnist(
