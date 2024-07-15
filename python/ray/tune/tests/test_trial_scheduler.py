fe80ba15fd5849ce9873529d79699609a21ed62a
Justin Yu <justinvyu@anyscale.com>
2024-07-15 16:47:10 -0700

    skip bohb tests
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/tune/tests/test_trial_scheduler.py
+++ python/ray/tune/tests/test_trial_scheduler.py
@@ -932,6 +932,7 @@ class BOHBSuite(unittest.TestCase):
             [t.status for t in trials], [Trial.PAUSED, Trial.PAUSED, Trial.PAUSED]
         )
 
+    @pytest.mark.skipif(sys.version_info >= (3, 12))
     def testNonstopBOHB(self):
         from ray.tune.search.bohb import TuneBOHB
 
@@ -2551,6 +2552,7 @@ class AsyncHyperBandSuite(unittest.TestCase):
     def testAnonymousMetricEndToEndASHA(self):
         self._testAnonymousMetricEndToEnd(AsyncHyperBandScheduler)
 
+    @pytest.mark.skipif(sys.version_info >= (3, 12))
     def testAnonymousMetricEndToEndBOHB(self):
         from ray.tune.search.bohb import TuneBOHB
 
