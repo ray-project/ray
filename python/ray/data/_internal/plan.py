b35dfdc50be8fa612cdea26322e9b5a947f1c3f4
Balaji Veeramani <balaji@anyscale.com>
2024-07-19 11:32:40 -0700

    Appease lint
    Signed-off-by: Balaji Veeramani <balaji@anyscale.com>

--- python/ray/data/_internal/plan.py
+++ python/ray/data/_internal/plan.py
@@ -13,7 +13,7 @@ from ray.data._internal.logical.interfaces.logical_plan import LogicalPlan
 from ray.data._internal.logical.operators.from_operators import AbstractFrom
 from ray.data._internal.logical.operators.input_data_operator import InputData
 from ray.data._internal.logical.operators.read_operator import Read
-from ray.data._internal.stats import DatasetStats, DatasetStatsSummary
+from ray.data._internal.stats import DatasetStats
 from ray.data._internal.util import create_dataset_tag, unify_block_metadata_schema
 from ray.data.block import BlockMetadata
 from ray.data.context import DataContext
