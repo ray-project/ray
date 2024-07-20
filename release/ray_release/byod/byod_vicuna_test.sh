28c44f9e6f574211305c7ef7116eece4a980eccb
can <can@anyscale.com>
2024-07-20 05:29:46 +0000

    fix air_example_vicuna_13b_lightning_deepspeed_finetuning
    Signed-off-by: can <can@anyscale.com>

--- release/ray_release/byod/byod_vicuna_test.sh
+++ release/ray_release/byod/byod_vicuna_test.sh
@@ -13,4 +13,6 @@ sudo mount /dev/nvme1n1 /mnt/local_storage || true
 EOF
 pip3 uninstall -y pytorch-lightning
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip3 install lightning==2.0.3  transformers==4.30.2 accelerate==0.20.3 deepspeed==0.12.3
+pip3 install lightning==2.0.3  transformers==4.30.2 accelerate==0.20.3 \
+  deepspeed==0.12.3 myst-parser==0.15.2 myst-nb==0.13.1 \
+  wandb==0.13.4
