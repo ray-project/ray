bd4ef65e3d3a7153b2ed44eeb083de604ff0d4e4
can <can@anyscale.com>
2024-07-24 20:39:19 +0000

    [doc][build] BuildCache api class
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/doc/cmd_build.py
+++ ci/ray_ci/doc/cmd_build.py
@@ -1,15 +1,12 @@
 import subprocess
 import os
 
-import boto3
 import click
 
 from ci.ray_ci.utils import logger, ci_init
-from ray_release.util import get_write_state_machine_aws_bucket
+from ci.ray_ci.doc.build_cache import BuildCache
+
 from ray_release.configs.global_config import get_global_config
-
-
-AWS_CACHE_KEY = "doc_build"
 
 
 @click.command()
@@ -22,6 +19,11 @@ def main(ray_checkout_dir: str) -> None:
     This script builds ray doc and upload build artifacts to S3.
     """
     ci_init()
+    # Add the safe.directory config to the global git config so that the doc build
+    subprocess.run(
+        ["git", "config", "--global", "--add", "safe.directory", ray_checkout_dir],
+        check=True,
+    )
 
     logger.info("Building ray doc.")
     _build(ray_checkout_dir)
@@ -42,7 +44,7 @@ def main(ray_checkout_dir: str) -> None:
         return
 
     logger.info("Uploading build artifacts to S3.")
-    _upload_build_artifacts(ray_checkout_dir)
+    BuildCache(os.path.join(ray_checkout_dir, "doc")).upload()
 
     return
 
@@ -58,38 +60,6 @@ def _build(ray_checkout_dir):
         env=env,
         check=True,
     )
-
-
-def _upload_build_artifacts(ray_checkout_dir):
-    """
-    Upload the build artifacts to S3.
-    """
-    # Get the list of the doc-generated files
-    subprocess.run(
-        ["git", "config", "--global", "--add", "safe.directory", ray_checkout_dir],
-        check=True,
-    )
-    doc_generated_files = subprocess.check_output(
-        ["git", "ls-files", "doc", "--others", "-z"],
-        cwd=ray_checkout_dir,
-    )
-
-    # Create a tarball of the doc-generated files
-    doc_tarball = f'{os.environ["BUILDKITE_COMMIT"]}.tgz'
-    with subprocess.Popen(
-        ["tar", "-cvzf", doc_tarball, "--null", "-T", "-"],
-        stdin=subprocess.PIPE,
-        cwd=ray_checkout_dir,
-    ) as proc:
-        proc.communicate(input=doc_generated_files)
-
-    # Upload the tarball to S3
-    boto3.client("s3").upload_file(
-        os.path.join(ray_checkout_dir, doc_tarball),
-        get_write_state_machine_aws_bucket(),
-        f"{AWS_CACHE_KEY}/{doc_tarball}",
-    )
-    logger.info(f"Successfully uploaded {doc_tarball} to S3.")
 
 
 if __name__ == "__main__":
