76365e9d98bc026a01d7034b3159723ce476d3b0
khluu <51931015+khluu@users.noreply.github.com>
2024-07-28 22:42:19 -0700

    p
    Signed-off-by: khluu <51931015+khluu@users.noreply.github.com>

--- ci/ray_ci/doc/load_doc_cache.py
+++ ci/ray_ci/doc/load_doc_cache.py
@@ -3,12 +3,13 @@ import botocore
 import subprocess
 import tarfile
 import os
+import click
 
-ray_dir = "/Users/kevin/ray"
 S3_BUCKET = "ray-ci-results"
 DOC_BUILD_DIR_S3= "doc_build"
+
 def find_latest_commit():
-    """Fetch latest commit that was pushed to origin/master."""
+    """Find latest commit that was pushed to origin/master that is also on local env."""
     latest_commit = subprocess.check_output(["git", "log", "--pretty=format:%H", "--first-parent", "origin/master", "-n", "1"]).decode("utf-8")
     return latest_commit
 
@@ -21,30 +22,32 @@ def fetch_cache_from_s3(commit, target_file_path):
         target_file_path (str): The file path to save the doc cache archive
     """
     # Create an S3 client
-    s3 = boto3.client('s3')
+    s3 = boto3.client("s3")
     s3_file_path = f"{DOC_BUILD_DIR_S3}/{commit}.tgz"
     try:
         print(f"Downloading {commit}.tgz from S3...")
         s3.download_file(S3_BUCKET, s3_file_path, target_file_path)
         print(f"Successfully downloaded {s3_file_path} to {target_file_path}")
     except botocore.exceptions.ClientError as e:
-        if e.response["Error"]["Code"] == "404":
-            print("The object does not exist.")
-        else:
-            raise
+        print(f"Failed to download {s3_file_path} from S3: {str(e)}")
+        raise e
 
-def extract_cache(file_path):
+def extract_cache(cache_path: str, doc_dir: str):
     """
-    Extract the doc cache archive to the ray/doc directory
+    Extract the doc cache archive to overwrite the ray/doc directory
 
     Args:
         file_path (str): The file path of the doc cache archive
     """
-    with tarfile.open(file_path, "r:gz") as tar:
-        tar.extractall(f"{ray_dir}/doc")
-    print(f"Extracted {file_path} to /Users/kevin/ray/doc")
+    with tarfile.open(cache_path, "r:gz") as tar:
+        tar.extractall(doc_dir)
+    print(f"Extracted {cache_path} to {doc_dir}")
 
-def list_changed_and_added_files():
+def list_changed_and_added_files(ray_dir: str):
+    """
+    List all changed and added untracked files in the repo. 
+    This is to prevent cache environment from updating timestamp of these files.
+    """
     untracked_files = (
         subprocess.check_output(
             ["git", "ls-files", "--others"],
@@ -63,23 +66,25 @@ def list_changed_and_added_files():
     )
     return untracked_files + modified_files
 
-def main() -> None:
-    # files = list_changed_and_added_files()
-    # with open(f"{ray_dir}/pending_files.txt", "w") as f:
-    #     for file in files:
-    #         filename = file.split(".")[0]
-    #         if filename.startswith("doc/"):
-    #             filename = filename.replace("doc/", "")
-    #         f.write(filename + "\n")
+@click.command()
+@click.option("--ray-dir", default="/ray", help="Path to Ray repo")
+def main(ray_dir: str) -> None:
+    # List all changed and added files in the repo
+    files = list_changed_and_added_files(ray_dir)
+    with open(f"{ray_dir}/pending_files.txt", "w") as f:
+        for file in files:
+            # Only keep filename without extension and remove "doc/" prefix
+            filename = file.split(".")[0]
+            if filename.startswith("doc/"):
+                filename = filename.replace("doc/", "")
+            f.write(filename + "\n")
+
     commit = find_latest_commit()
-    print(commit)
-    target_file_path = f"{ray_dir}/doc.tgz"
-    fetch_cache_from_s3(commit, target_file_path)
-    extract_cache(target_file_path)
+    cache_path = f"{ray_dir}/doc.tgz"
+    # Fetch cache of that commit from S3 to cache_path
+    fetch_cache_from_s3(commit, cache_path)
+    # Extract cache to override ray/doc directory
+    extract_cache(cache_path, f"{ray_dir}/doc")
 
 if __name__ == "__main__":
     main()
-# object_name = f"doc_build/{commit}.tgz"
-# file_name = f"{ray_dir}/doc.tgz"
-# fetch_cache_from_s3(object_name, file_name)
-#extract_cache(f"{ray_dir}/doc.tgz")
