744120dbe07bb2a236bad673dc4c3bb03f0ec51c
khluu <51931015+khluu@users.noreply.github.com>
2024-07-29 13:47:02 -0700

    p
    Signed-off-by: khluu <51931015+khluu@users.noreply.github.com>

--- ci/ray_ci/doc/load_doc_cache.py
+++ ci/ray_ci/doc/load_doc_cache.py
@@ -9,7 +9,7 @@ S3_BUCKET = "ray-ci-results"
 DOC_BUILD_DIR_S3 = "doc_build"
 
 
-def find_latest_commit():
+def find_latest_master_commit():
     """Find latest commit that was pushed to origin/master that is also on local env."""
     latest_commit = subprocess.check_output(
         [
@@ -17,7 +17,7 @@ def find_latest_commit():
             "log",
             "--pretty=format:%H",
             "--first-parent",
-            "origin/master",
+            "master",
             "-n",
             "1",
         ]
@@ -57,7 +57,7 @@ def extract_cache(cache_path: str, doc_dir: str):
     print(f"Extracted {cache_path} to {doc_dir}")
 
 
-def list_changed_and_added_files(ray_dir: str):
+def list_changed_and_added_files(ray_dir: str, latest_master_commit: str):
     """
     List all changed and added untracked files in the repo.
     This is to prevent cache environment from updating timestamp of these files.
@@ -78,8 +78,16 @@ def list_changed_and_added_files(ray_dir: str):
         .decode("utf-8")
         .split(os.linesep)
     )
+    diff_files_with_master = (
+        subprocess.check_output(
+            ["git", "diff", "--name-only", latest_master_commit],
+            cwd=ray_dir,
+        )
+        .decode("utf-8")
+        .split(os.linesep)
+    )
     filenames = []
-    for file in untracked_files + modified_files:
+    for file in untracked_files + modified_files + diff_files_with_master:
         filename = file.split(".")[0]  # Remove extension
         if filename.startswith("doc/"):  # Remove "doc/" prefix
             filename = filename.replace("doc/", "")
@@ -90,17 +98,17 @@ def list_changed_and_added_files(ray_dir: str):
 @click.command()
 @click.option("--ray-dir", default="/ray", help="Path to Ray repo")
 def main(ray_dir: str) -> None:
+    latest_master_commit = find_latest_master_commit()
     # List all changed and added files in the repo
-    filenames = list_changed_and_added_files(ray_dir)
+    filenames = list_changed_and_added_files(ray_dir, latest_master_commit)
     with open(
         f"{ray_dir}/pending_files.txt", "w"
     ) as f:  # Save to file to be used when updating cache environment
         f.write("\n".join(filenames))
 
-    commit = find_latest_commit()
     cache_path = f"{ray_dir}/doc.tgz"
     # Fetch cache of that commit from S3 to cache_path
-    fetch_cache_from_s3(commit, cache_path)
+    fetch_cache_from_s3(latest_master_commit, cache_path)
     # Extract cache to override ray/doc directory
     extract_cache(cache_path, f"{ray_dir}/doc")
 
