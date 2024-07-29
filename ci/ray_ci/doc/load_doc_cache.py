fd10af0eeb687d745c3106c983c53295ae538e56
khluu <51931015+khluu@users.noreply.github.com>
2024-07-28 22:45:16 -0700

    format
    Signed-off-by: khluu <51931015+khluu@users.noreply.github.com>

--- ci/ray_ci/doc/load_doc_cache.py
+++ ci/ray_ci/doc/load_doc_cache.py
@@ -6,17 +6,29 @@ import os
 import click
 
 S3_BUCKET = "ray-ci-results"
-DOC_BUILD_DIR_S3= "doc_build"
+DOC_BUILD_DIR_S3 = "doc_build"
+
 
 def find_latest_commit():
     """Find latest commit that was pushed to origin/master that is also on local env."""
-    latest_commit = subprocess.check_output(["git", "log", "--pretty=format:%H", "--first-parent", "origin/master", "-n", "1"]).decode("utf-8")
+    latest_commit = subprocess.check_output(
+        [
+            "git",
+            "log",
+            "--pretty=format:%H",
+            "--first-parent",
+            "origin/master",
+            "-n",
+            "1",
+        ]
+    ).decode("utf-8")
     return latest_commit
+
 
 def fetch_cache_from_s3(commit, target_file_path):
     """
     Fetch doc cache archive from ray-ci-results S3 bucket
-    
+
     Args:
         commit (str): The commit hash of the doc cache to fetch
         target_file_path (str): The file path to save the doc cache archive
@@ -32,6 +44,7 @@ def fetch_cache_from_s3(commit, target_file_path):
         print(f"Failed to download {s3_file_path} from S3: {str(e)}")
         raise e
 
+
 def extract_cache(cache_path: str, doc_dir: str):
     """
     Extract the doc cache archive to overwrite the ray/doc directory
@@ -43,9 +56,10 @@ def extract_cache(cache_path: str, doc_dir: str):
         tar.extractall(doc_dir)
     print(f"Extracted {cache_path} to {doc_dir}")
 
+
 def list_changed_and_added_files(ray_dir: str):
     """
-    List all changed and added untracked files in the repo. 
+    List all changed and added untracked files in the repo.
     This is to prevent cache environment from updating timestamp of these files.
     """
     untracked_files = (
@@ -66,6 +80,7 @@ def list_changed_and_added_files(ray_dir: str):
     )
     return untracked_files + modified_files
 
+
 @click.command()
 @click.option("--ray-dir", default="/ray", help="Path to Ray repo")
 def main(ray_dir: str) -> None:
@@ -85,6 +100,7 @@ def main(ray_dir: str) -> None:
     fetch_cache_from_s3(commit, cache_path)
     # Extract cache to override ray/doc directory
     extract_cache(cache_path, f"{ray_dir}/doc")
+
 
 if __name__ == "__main__":
     main()
