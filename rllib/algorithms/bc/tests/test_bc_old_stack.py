5afc3ab94aaa28f99d183e772973b94984012899
simonsays1980 <simon.zehnder@gmail.com>
2024-07-30 15:37:01 +0200

    Fixed old stack test by removing hybrid stack.
    Signed-off-by: simonsays1980 <simon.zehnder@gmail.com>

--- rllib/algorithms/bc/tests/test_bc_old_stack.py
+++ rllib/algorithms/bc/tests/test_bc_old_stack.py
@@ -50,54 +50,48 @@ class TestBC(unittest.TestCase):
         num_iterations = 350
         min_return_to_reach = 75.0
 
-        # Test for RLModule API and ModelV2.
-        for rl_modules in [True, False]:
-            config.api_stack(enable_rl_module_and_learner=rl_modules)
-            # Old and new stack support different frameworks
-            if rl_modules:
-                frameworks_to_test = ("torch", "tf2")
-            else:
-                frameworks_to_test = ("torch", "tf")
+        # Test for the following frameworks.
+        frameworks_to_test = ("torch", "tf")
 
-            for _ in framework_iterator(config, frameworks=frameworks_to_test):
-                for recurrent in [True, False]:
-                    # We only test recurrent networks with RLModules.
-                    if recurrent:
-                        # TODO (Artur): We read input data without a time-dimensions.
-                        #  In order for a recurrent offline learning RL Module to
-                        #  work, the input data needs to be transformed do add a
-                        #  time-dimension.
-                        continue
+        for _ in framework_iterator(config, frameworks=frameworks_to_test):
+            for recurrent in [True, False]:
+                # We only test recurrent networks with RLModules.
+                if recurrent:
+                    # TODO (Artur): We read input data without a time-dimensions.
+                    #  In order for a recurrent offline learning RL Module to
+                    #  work, the input data needs to be transformed do add a
+                    #  time-dimension.
+                    continue
 
-                    config.training(model={"use_lstm": recurrent})
-                    algo = config.build(env="CartPole-v1")
-                    learnt = False
-                    for i in range(num_iterations):
-                        results = algo.train()
-                        check_train_results(results)
-                        print(results)
+                config.training(model={"use_lstm": recurrent})
+                algo = config.build(env="CartPole-v1")
+                learnt = False
+                for i in range(num_iterations):
+                    results = algo.train()
+                    check_train_results(results)
+                    print(results)
 
-                        eval_results = results.get("evaluation")
-                        if eval_results:
-                            mean_return = eval_results[ENV_RUNNER_RESULTS][
-                                EPISODE_RETURN_MEAN
-                            ]
-                            print("iter={} R={}".format(i, mean_return))
-                            # Learn until good reward is reached in the actual env.
-                            if mean_return > min_return_to_reach:
-                                print("learnt!")
-                                learnt = True
-                                break
+                    eval_results = results.get("evaluation")
+                    if eval_results:
+                        mean_return = eval_results[ENV_RUNNER_RESULTS][
+                            EPISODE_RETURN_MEAN
+                        ]
+                        print("iter={} R={}".format(i, mean_return))
+                        # Learn until good reward is reached in the actual env.
+                        if mean_return > min_return_to_reach:
+                            print("learnt!")
+                            learnt = True
+                            break
 
-                    if not learnt:
-                        raise ValueError(
-                            "`BC` did not reach {} reward from expert offline "
-                            "data!".format(min_return_to_reach)
-                        )
+                if not learnt:
+                    raise ValueError(
+                        "`BC` did not reach {} reward from expert offline "
+                        "data!".format(min_return_to_reach)
+                    )
 
-                    check_compute_single_action(algo, include_prev_action_reward=True)
+                check_compute_single_action(algo, include_prev_action_reward=True)
 
-                    algo.stop()
+                algo.stop()
 
 
 if __name__ == "__main__":
