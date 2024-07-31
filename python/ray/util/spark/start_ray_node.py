cac1b7f7b94a65107eb387962e0f92eda83c6a40
Weichen Xu <weichen.xu@databricks.com>
2024-07-31 18:55:04 +0800

    init
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- python/ray/util/spark/start_ray_node.py
+++ python/ray/util/spark/start_ray_node.py
@@ -62,6 +62,15 @@ if __name__ == "__main__":
     # same temp directory, adding a shared lock representing current ray node is
     # using the temp directory.
     fcntl.flock(lock_fd, fcntl.LOCK_SH)
+
+    def preexec_function():
+        # Make Ray node process runs in a separate group,
+        # otherwise Ray node will be in the same group of parent process,
+        # if parent process is a Jupyter notebook kernel, when user
+        # clicks interrupt cell button, SIGINT signal is sent, then Ray node will
+        # receive SIGINT signal and it causes Ray node process dies.
+        os.setpgrp()
+
     process = subprocess.Popen(
         # 'ray start ...' command uses python that is set by
         # Shebang #! ..., the Shebang line is hardcoded in ray script,
@@ -71,6 +80,7 @@ if __name__ == "__main__":
         # '`sys.executable` `which ray` start ...'
         [sys.executable, shutil.which(ray_cli_cmd), "start", *arg_list],
         text=True,
+        preexec_fn=preexec_function,
     )
 
     def try_clean_temp_dir_at_exit():
