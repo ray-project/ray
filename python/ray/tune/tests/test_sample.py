27786323e94a196c90aa22383892e91590cab1f5
can <can@anyscale.com>
2024-07-31 21:06:30 +0000

    [py312] ml cpu tests
    Signed-off-by: can <can@anyscale.com>

--- python/ray/tune/tests/test_sample.py
+++ python/ray/tune/tests/test_sample.py
@@ -1551,6 +1551,10 @@ class SearchSpaceTest(unittest.TestCase):
 
         return self._testPointsToEvaluate(TuneBOHB, config)
 
+    @pytest.mark.skipif(
+        sys.version_info >= (3, 12),
+        reason="hyperoprt not yet supported for python 3.12+",
+    )
     def testPointsToEvaluateHyperOpt(self):
         config = {
             "metric": ray.tune.search.sample.Categorical([1, 2, 3, 4]).uniform(),
@@ -1581,6 +1585,10 @@ class SearchSpaceTest(unittest.TestCase):
 
         return self._testPointsToEvaluate(HyperOptSearch, config)
 
+    @pytest.mark.skipif(
+        sys.version_info >= (3, 12),
+        reason="hyperoprt not yet supported for python 3.12+",
+    )
     def testPointsToEvaluateHyperOptNested(self):
         space = {
             "nested": [
