e0f8eec6628d4135e587aca65b61c00d695c75f5
Weichen Xu <weichen.xu@databricks.com>
2024-08-02 21:13:52 +0800

    update test
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- python/ray/tests/spark/test_basic.py
+++ python/ray/tests/spark/test_basic.py
@@ -9,6 +9,7 @@ import pytest
 import sys
 from unittest import mock
 from abc import ABC
+import subprocess
 
 import ray
 
@@ -508,39 +509,45 @@ class TestSparkLocalCluster:
         assert config["provider"]["extra_aa"] == "abc"
         assert config["provider"]["extra_bb"] == 789
 
-    def test_sigint_signal(self):
-        local_addr, remote_addr = setup_ray_cluster(
-            max_worker_nodes=1,
-            head_node_options={"include_dashboard": False},
-            collect_log_to_path="/tmp/ray_log_collect",
+    def test_start_ray_node_in_new_process_group(self):
+        def preexec_function():
+            os.setpgrp()
+
+        proc = subprocess.Popen(
+            [
+                sys.executable,
+                "-m",
+                "ray.util.spark.start_ray_node",
+                "--head",
+                "--block",
+                "--port=44335",
+            ],
+            preexec_fn=preexec_function,
+            env={
+                **os.environ,
+                "RAY_ON_SPARK_COLLECT_LOG_TO_PATH": "",
+                "RAY_ON_SPARK_START_RAY_PARENT_PID": str(os.getpid()),
+            },
         )
+        time.sleep(10)
 
-        ray.init(address=local_addr)
-
-        def sigint_noop_handler(signum, frame):
-            pass
-
-        default_sigint_handler = signal.signal(signal.SIGINT, sigint_noop_handler)
+        ray.init("127.0.0.1:44335")
 
         # send a SIGINT signal to head process (i.e. the `start_ray_node` process),
         # then test the Ray node is not killed by the SIGINT signal.
         # See https://github.com/ray-project/ray/pull/46899 for details
-        os.killpg(os.getpgid(0), signal.SIGINT)
-
-        time.sleep(1)
-        signal.signal(signal.SIGINT, default_sigint_handler)
+        os.killpg(os.getpgid(proc.pid), signal.SIGINT)
+        time.sleep(3)
 
         @ray.remote
         def f(x):
             return x * x
 
-        futures = [f.remote(i) for i in range(32)]
+        futures = [f.remote(i) for i in range(4)]
         results = ray.get(futures)
-        assert results == [i * i for i in range(32)]
+        assert results == [i * i for i in range(4)]
 
-        ray.shutdown()
-
-        shutdown_ray_cluster()
+        proc.terminate()
 
 
 if __name__ == "__main__":
