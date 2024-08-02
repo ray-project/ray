8944ca1985b3ed6304bd7c032e33d0f3dde7d6ab
cristianjd <cristian.j.derr@gmail.com>
2024-08-01 18:04:05 -0700

    Generate Versions JSON for new Version Switcher (#46901)
    ## Why are these changes needed?
    
    In order to move away from the "Read the Docs" version switcher and to
    the one provided by the PyData theme we must first have a stable JSON of
    the available versions as described here:
    https://github.com/google/google-java-format/releases/download/google-java-format-1.7/google-java-format-1.7-all-deps.jar
    
    The adds helpers to custom directives in order to get the list of git
    tags starting from the first one we display in the switcher then sort in
    semver order. "Latest" and "master" are added to the top. This is run in
    the setup fn to generate the JSON in the static folder. After this is
    deployed we can reference it, add the new picker, and hide the previous
    one.
    
    ## Related issue number
    
    This is part one of a fix for
    https://github.com/ray-project/ray/issues/46382 and
    https://github.com/ray-project/ray/issues/46189. The follow up will
    close these out.
    
    
    Signed-off-by: cristianjd <cristian.j.derr@gmail.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,6 +24,7 @@ from custom_directives import (  # noqa
     parse_navbar_config,
     setup_context,
     pregenerate_example_rsts,
+    generate_versions_json,
 )
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -480,6 +481,10 @@ def _autogen_apis(app: sphinx.application.Sphinx):
 
 
 def setup(app):
+    # Only generate versions JSON during RTD build
+    if os.getenv("READTHEDOCS") == "True":
+        generate_versions_json()
+
     pregenerate_example_rsts(app)
 
     # NOTE: 'MOCK' is a custom option we introduced to illustrate mock outputs. Since
