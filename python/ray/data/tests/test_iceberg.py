1ee28b8930111ea102809689223f2a1a6ebee231
Dev <dev.goyal@hinge.co>
2024-08-07 02:18:35 -0400

    Intermediate commit with updated tests
    Signed-off-by: Dev <dev.goyal@hinge.co>

--- python/ray/data/tests/test_iceberg.py
+++ python/ray/data/tests/test_iceberg.py
@@ -1,10 +1,18 @@
 import os
+import random
 
-import numpy as np
 import pyarrow as pa
 import pytest
 from pyiceberg import catalog as pyi_catalog
-from ray.data.datasource.iceberg_datasource import IcebergDatasource
+from pyiceberg import expressions as pyi_expr
+from pyiceberg import schema as pyi_schema
+from pyiceberg import types as pyi_types
+from pyiceberg.partitioning import PartitionField, PartitionSpec
+from pyiceberg.transforms import IdentityTransform
+
+import ray
+from ray.data import read_iceberg
+from ray.data._internal.datasource.iceberg_datasource import IcebergDatasource
 
 _CATALOG_NAME = "ray_catalog"
 _DB_NAME = "ray_db"
@@ -14,7 +22,6 @@ _TABLE_NAME = "ray_test"
 @pytest.fixture(autouse=True)
 def pyiceberg_full_mock(monkeypatch):
     from pyiceberg.catalog.sql import SqlCatalog
-    from pyiceberg.table import DataScan
 
     warehouse_path = "/tmp/warehouse"
     if not os.path.exists(warehouse_path):
@@ -27,24 +34,60 @@ def pyiceberg_full_mock(monkeypatch):
         },
     )
 
+    schema = pa.schema(
+        [
+            pa.field("col_a", pa.int32()),
+            pa.field("col_b", pa.string()),
+            pa.field("col_c", pa.int16()),
+        ]
+    )
     pya_table = pa.Table.from_pydict(
         mapping={
-            "col_a": list(range(100)),
-            "col_b": np.random.choice(["a", "b", "c", "d"], size=100, replace=True),
+            "col_a": list(range(120)),
+            "col_b": random.choices(["a", "b", "c", "d"], k=120),
+            "col_c": random.choices(list(range(10)), k=120),
         },
+        schema=schema,
     )
 
     if (_DB_NAME,) not in catalog.list_namespaces():
         catalog.create_namespace(_DB_NAME)
     if (_DB_NAME, _TABLE_NAME) in catalog.list_tables(_DB_NAME):
         catalog.drop_table(f"{_DB_NAME}.{_TABLE_NAME}")
+
+    # Create the table, and add data to it
     table = catalog.create_table(
         f"{_DB_NAME}.{_TABLE_NAME}",
-        schema=pya_table.schema,
+        schema=pyi_schema.Schema(
+            pyi_types.NestedField(
+                field_id=1,
+                name="col_a",
+                field_type=pyi_types.IntegerType(),
+                required=False,
+            ),
+            pyi_types.NestedField(
+                field_id=2,
+                name="col_b",
+                field_type=pyi_types.StringType(),
+                required=False,
+            ),
+            pyi_types.NestedField(
+                field_id=3,
+                name="col_c",
+                field_type=pyi_types.IntegerType(),
+                required=False,
+            ),
+        ),
+        partition_spec=PartitionSpec(
+            PartitionField(
+                source_id=3, field_id=3, transform=IdentityTransform(), name="col_c"
+            )
+        ),
     )
     table.append(pya_table)
 
-    plan_files = table.scan().plan_files()
+    # Delete some data so there are delete file(s)
+    table.delete(delete_filter=pyi_expr.GreaterThanOrEqual("col_a", 101))
 
     def pyi_get_catalog_mock(catalog_name, *args, **kwargs):
         if catalog_name != _CATALOG_NAME:
@@ -52,7 +95,7 @@ def pyiceberg_full_mock(monkeypatch):
         return catalog
 
     monkeypatch.setattr(pyi_catalog, "load_catalog", pyi_get_catalog_mock)
-    monkeypatch.setattr(DataScan, "plan_files", lambda self: list(plan_files) * 10)
+    # monkeypatch.setattr(DataScan, "plan_files", lambda self: list(plan_files) * 10)
 
 
 class TestReadIceberg:
@@ -78,11 +121,18 @@ class TestReadIceberg:
             catalog_kwargs={"name": _CATALOG_NAME},
         )
 
-        chunks = iceberg_ds._distribute_tasks_into_equal_chunks(iceberg_ds.plan_files, 5)
+        chunks = iceberg_ds._distribute_tasks_into_equal_chunks(
+            iceberg_ds.plan_files, 5
+        )
         assert (len(c) == 2 for c in chunks)
 
-        chunks = iceberg_ds._distribute_tasks_into_equal_chunks(iceberg_ds.plan_files, 20)
-        assert sum(len(c) == 1 for c in chunks) == 10 and sum(len(c) == 0 for c in chunks) == 10
+        chunks = iceberg_ds._distribute_tasks_into_equal_chunks(
+            iceberg_ds.plan_files, 20
+        )
+        assert (
+            sum(len(c) == 1 for c in chunks) == 10
+            and sum(len(c) == 0 for c in chunks) == 10
+        )
 
     def test_get_read_tasks(self):
         iceberg_ds = IcebergDatasource(
@@ -94,23 +144,29 @@ class TestReadIceberg:
         assert all(len(rt.metadata.input_files) == 2 for rt in read_tasks)
 
     def test_filtered_read(self):
-        import pyarrow as pa
         from pyiceberg import expressions as pyi_expr
-
-        pass
 
         iceberg_ds = IcebergDatasource(
             table_identifier=f"{_DB_NAME}.{_TABLE_NAME}",
-            row_filter=pyi_expr.EqualTo("col_a", 1),
+            row_filter=pyi_expr.In("col_c", {1, 2, 3, 4}),
             selected_fields=("col_b",),
             catalog_kwargs={"name": _CATALOG_NAME, "type": "sql"},
         )
         read_tasks = iceberg_ds.get_read_tasks(5)
-        assert len(read_tasks) == 5
-        assert all(len(rt.metadata.input_files) == 2 for rt in read_tasks)
+        # Should be capped to 4, as there will be only 4 files
+        assert len(read_tasks) == 4, read_tasks
+        assert all(len(rt.metadata.input_files) == 1 for rt in read_tasks)
 
-        expected_schema = pa.schema([pa.field("col_b", pa.large_string())])
-        assert all(rt.metadata.schema.equals(expected_schema) for rt in read_tasks), (
-            [rt.metadata.schema for rt in read_tasks],
-            expected_schema,
+    def test_read_basic(self):
+        ray_ds = read_iceberg(
+            table_identifier=f"{_DB_NAME}.{_TABLE_NAME}",
+            row_filter=pyi_expr.In("col_c", {1, 2, 3, 4}),
+            selected_fields=("col_a", "col_b", "col_c",),
+            catalog_kwargs={"name": _CATALOG_NAME, "type": "sql"},
         )
+        table = pa.concat_tables([ray.get(ar) for ar in ray_ds.to_arrow_refs()])
+        # expected_schema = pa.schema([pa.field("col_b", pa.string())])
+        # assert all(rt.metadata.schema.equals(expected_schema) for rt in read_tasks), (
+        #     [rt.metadata.schema for rt in read_tasks],
+        #     expected_schema,
+        # )
