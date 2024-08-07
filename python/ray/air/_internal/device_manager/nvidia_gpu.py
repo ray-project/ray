9c5a2965ed65de119c94c3d9645774e01bd67d43
liuxsh9 <liuxiaoshuang4@huawei.com>
2024-08-07 18:23:23 +0800

    delete fall back logic in CUDATorchDevicaManager
    Signed-off-by: liuxsh9 <liuxiaoshuang4@huawei.com>

--- python/ray/air/_internal/device_manager/nvidia_gpu.py
+++ python/ray/air/_internal/device_manager/nvidia_gpu.py
@@ -22,46 +22,42 @@ class CUDATorchDeviceManager(TorchDeviceManager):
         Assumes that `CUDA_VISIBLE_DEVICES` is set and is a
         superset of the `ray.get_gpu_ids()`.
         """
-        if torch.cuda.is_available():
-            # GPU IDs are assigned by Ray after you specify "use_gpu"
-            # GPU `ray.get_gpu_ids()` may return ints or may return strings.
-            # We should always convert to strings.
-            gpu_ids = [str(id) for id in ray.get_gpu_ids()]
 
-            device_ids = []
+        # GPU IDs are assigned by Ray after you specify "use_gpu"
+        # GPU `ray.get_gpu_ids()` may return ints or may return strings.
+        # We should always convert to strings.
+        gpu_ids = [str(id) for id in ray.get_gpu_ids()]
 
-            if len(gpu_ids) > 0:
-                cuda_visible_str = os.environ.get("CUDA_VISIBLE_DEVICES", "")
-                if cuda_visible_str and cuda_visible_str != "NoDevFiles":
-                    cuda_visible_list = cuda_visible_str.split(",")
-                else:
-                    cuda_visible_list = []
+        device_ids = []
 
-                # By default, there should only be one GPU ID if `use_gpu=True`.
-                # If there are multiple GPUs, return a list of devices.
-                # If using fractional GPUs, these IDs are not guaranteed
-                # to be unique across different processes.
-                for gpu_id in gpu_ids:
-                    try:
-                        device_ids.append(cuda_visible_list.index(gpu_id))
-                    except IndexError:
-                        raise RuntimeError(
-                            "CUDA_VISIBLE_DEVICES set incorrectly. "
-                            f"Got {cuda_visible_str}, expected to include {gpu_id}. "
-                            "Did you override the `CUDA_VISIBLE_DEVICES` environment"
-                            " variable? If not, please help file an issue on Github."
-                        )
-
+        if len(gpu_ids) > 0:
+            cuda_visible_str = os.environ.get("CUDA_VISIBLE_DEVICES", "")
+            if cuda_visible_str and cuda_visible_str != "NoDevFiles":
+                cuda_visible_list = cuda_visible_str.split(",")
             else:
-                # If called on the driver or outside of Ray Train, return the
-                # 0th device.
-                device_ids.append(0)
+                cuda_visible_list = []
 
-            devices = [torch.device(f"cuda:{device_id}") for device_id in device_ids]
+            # By default, there should only be one GPU ID if `use_gpu=True`.
+            # If there are multiple GPUs, return a list of devices.
+            # If using fractional GPUs, these IDs are not guaranteed
+            # to be unique across different processes.
+            for gpu_id in gpu_ids:
+                try:
+                    device_ids.append(cuda_visible_list.index(gpu_id))
+                except IndexError:
+                    raise RuntimeError(
+                        "CUDA_VISIBLE_DEVICES set incorrectly. "
+                        f"Got {cuda_visible_str}, expected to include {gpu_id}. "
+                        "Did you override the `CUDA_VISIBLE_DEVICES` environment"
+                        " variable? If not, please help file an issue on Github."
+                    )
+
         else:
-            devices = [torch.device("cpu")]
+            # If called on the driver or outside of Ray Train, return the
+            # 0th device.
+            device_ids.append(0)
 
-        return devices
+        return [torch.device(f"cuda:{device_id}") for device_id in device_ids]
 
     def set_device(self, device: Union[torch.device, int, str, None]):
         torch.cuda.set_device(device)
