7b17a81c4873f0e516c2a677a8879932dc52ab9a
Matthew Owen <mowen@anyscale.com>
2024-08-08 10:35:28 -0700

    [data] Only dump state once on progress bar close (#46928)
    Close https://github.com/ray-project/ray/issues/44979. This also
    addresses an issue where the bars were misordered in jupyter notebooks
    (the overall dataset bar showed up last). 
    
    ---------
    
    Signed-off-by: Matthew Owen <mowen@anyscale.com>

--- python/ray/experimental/tqdm_ray.py
+++ python/ray/experimental/tqdm_ray.py
@@ -242,8 +242,12 @@ class _BarGroup:
     def close_bar(self, state: ProgressBarState) -> None:
         """Remove a bar from this group."""
         bar = self.bars_by_uuid[state["uuid"]]
+        # Note: Hide and then unhide bars to prevent flashing of the
+        # last bar when we are closing multiple bars sequentially.
+        instance().hide_bars()
         bar.close()
         del self.bars_by_uuid[state["uuid"]]
+        instance().unhide_bars()
 
     def slots_required(self):
         """Return the number of pos slots we need to accomodate bars in this group."""
