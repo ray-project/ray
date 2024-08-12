e8a70b1ec109c0206009cd8f32ba8b98c75276ea
Nikita Vemuri <nikitavemuri@anyscale.com>
2024-08-12 11:39:27 -0700

    wip
    Signed-off-by: Nikita Vemuri <nikitavemuri@anyscale.com>

--- src/ray/core_worker/test/actor_manager_test.cc
+++ src/ray/core_worker/test/actor_manager_test.cc
@@ -480,23 +480,27 @@ TEST_F(ActorManagerTest, TestActorDeathCauseBackwardCompatibility) {
   runtime_env_failed_context.set_error_message("error message string");
   actor_death_cause2.mutable_runtime_env_failed_context()->CopyFrom(runtime_env_failed_context);
 
-  rpc::ActorDiedErrorContext actor_died_context;
-  rpc::NodeDeathInfo node_death_info;
-  rpc::ActorDeathCause actor_death_cause3;
-  node_death_info.set_reason(rpc::NodeDeathInfo::UNEXPECTED_TERMINATION);
-  node_death_info.set_reason_message("reason message string");
-  actor_died_context.set_error_message("error message string");
-  actor_died_context.set_owner_id("ownerId1");
-  actor_died_context.set_owner_ip_address("127.0.0.1");
-  actor_died_context.set_node_ip_address("127.0.0.1");
-  actor_died_context.set_pid(123);
-  actor_died_context.set_name("actor_name");
-  actor_died_context.set_ray_namespace("actor_ray_namespace");
-  actor_died_context.set_class_name("actor_class_name");
-  actor_died_context.set_actor_id("actorId1");
-  actor_died_context.set_never_started(False);
-  actor_died_context.mutable_node_death_info()->CopyFrom(node_death_info);
-  actor_death_cause3.mutable_actor_died_error_context()->CopyFrom(actor_died_context);
+  rpc::NodeDeathInfo::Reason node_death_info_reason_arr[5] = {rpc::NodeDeathInfo::UNSPECIFIED, rpc::NodeDeathInfo::EXPECTED_TERMINATION, rpc::NodeDeathInfo::UNEXPECTED_TERMINATION, rpc::NodeDeathInfo::AUTOSCALER_DRAIN_PREEMPTED, rpc::NodeDeathInfo::AUTOSCALER_DRAIN_IDLE};
+  for (rpc::NodeDeathInfo::Reason node_death_info_reason : node_death_info_reason_arr) {
+    rpc::ActorDiedErrorContext actor_died_context;
+    rpc::NodeDeathInfo node_death_info;
+    rpc::ActorDeathCause actor_death_cause3;
+    node_death_info.set_reason(node_death_info_reason);
+    node_death_info.set_reason_message("reason message string");
+    actor_died_context.set_error_message("error message string");
+    actor_died_context.set_owner_id("ownerId1");
+    actor_died_context.set_owner_ip_address("127.0.0.1");
+    actor_died_context.set_node_ip_address("127.0.0.1");
+    actor_died_context.set_pid(123);
+    actor_died_context.set_name("actor_name");
+    actor_died_context.set_ray_namespace("actor_ray_namespace");
+    actor_died_context.set_class_name("actor_class_name");
+    actor_died_context.set_actor_id("actorId1");
+    actor_died_context.set_never_started(false);
+    actor_died_context.mutable_node_death_info()->CopyFrom(node_death_info);
+    actor_death_cause3.mutable_actor_died_error_context()->CopyFrom(actor_died_context);
+  }
+  
 
 
 
