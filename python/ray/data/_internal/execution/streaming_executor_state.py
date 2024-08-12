9ffb3b47aa63c6f35bebae54b877f3638a79a09e
Matthew Owen <mowen@anyscale.com>
2024-08-12 14:01:16 -0700

    adding in backpressure label
    Signed-off-by: Matthew Owen <mowen@anyscale.com>

--- python/ray/data/_internal/execution/streaming_executor_state.py
+++ python/ray/data/_internal/execution/streaming_executor_state.py
@@ -260,6 +260,8 @@ class OpState:
         queued = self.num_queued() + self.op.internal_queue_size()
         active = self.op.num_active_tasks()
         desc = f"- {self.op.name}: {active} active, {queued} queued"
+        if self.op._in_task_submission_backpressure:
+            desc += ", BACKPRESSURED"
         desc += f", [{resource_manager.get_op_usage_str(self.op)}]"
         suffix = self.op.progress_str()
         if suffix:
