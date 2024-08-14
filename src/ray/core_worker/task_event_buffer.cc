5d47277aadb23724c4efaf1c8d89e9e3e1d93f92
Nikita Vemuri <nikitavemuri@anyscale.com>
2024-08-13 23:56:31 -0700

    wip - aggregate events
    Signed-off-by: Nikita Vemuri <nikitavemuri@anyscale.com>

--- src/ray/core_worker/task_event_buffer.cc
+++ src/ray/core_worker/task_event_buffer.cc
@@ -409,9 +409,8 @@ void TaskEventBufferImpl::WriteExportData(
     std::vector<std::unique_ptr<TaskEvent>> &&profile_events_to_send,
     absl::flat_hash_set<TaskAttempt> &&dropped_task_attempts_to_send) {
   // Aggregate the task events by TaskAttempt.
-  // absl::flat_hash_map<TaskAttempt, rpc::ExportTaskEventData> agg_task_events;
-  // std::vector<std::shared_ptr<rpc::ExportTaskEventData>> rpc_export_task_events;
-  auto to_rpc_event_fn = [this, &dropped_task_attempts_to_send](
+  absl::flat_hash_map<TaskAttempt, std::shared_ptr<rpc::ExportTaskEventData>> agg_task_events;
+  auto to_rpc_event_fn = [this, &agg_task_events, &dropped_task_attempts_to_send](
                              std::unique_ptr<TaskEvent> &event) {
     if (dropped_task_attempts_to_send.count(event->GetTaskAttempt())) {
       // We are marking this as data loss due to some missing task status updates.
@@ -421,24 +420,25 @@ void TaskEventBufferImpl::WriteExportData(
       return;
     }
 
-    std::shared_ptr<rpc::ExportTaskEventData> rpc_event_data_ptr = std::make_shared<rpc::ExportTaskEventData>();
+    if (!agg_task_events.count(event->GetTaskAttempt())) {
+      auto inserted =
+          agg_task_events.insert({event->GetTaskAttempt(), std::make_shared<rpc::ExportTaskEventData>()});
+      RAY_CHECK(inserted.second);
+    }
 
-    // if (!agg_task_events.count(event->GetTaskAttempt())) {
-    //   auto inserted =
-    //       agg_task_events.insert({event->GetTaskAttempt(), rpc::TaskEvents()});
-    //   RAY_CHECK(inserted.second);
-    // }
+    auto itr = agg_task_events.find(event->GetTaskAttempt());
 
-    // auto itr = agg_task_events.find(event->GetTaskAttempt());
-
-    event->ToRpcTaskExportEvents(rpc_event_data_ptr);
-    RayExportEvent(rpc_event_data_ptr).SendEvent();
+    event->ToRpcTaskExportEvents(itr->second);
   };
 
   std::for_each(
       status_events_to_send.begin(), status_events_to_send.end(), to_rpc_event_fn);
   std::for_each(
       profile_events_to_send.begin(), profile_events_to_send.end(), to_rpc_event_fn);
+  
+  for (auto &[_task_attempt, task_event_ptr] : agg_task_events) {
+    RayExportEvent(task_event_ptr).SendEvent();
+  };
 }
 
 void TaskEventBufferImpl::FlushEvents(bool forced) {
