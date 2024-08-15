27dccf0278f9c30b086cb43e3dfe762d304162ed
Nikita Vemuri <nikitavemuri@anyscale.com>
2024-08-15 09:36:21 -0700

    add proto
    Signed-off-by: Nikita Vemuri <nikitavemuri@anyscale.com>

--- src/ray/util/event.cc
+++ src/ray/util/event.cc
@@ -406,6 +406,11 @@ void RayExportEvent::SendEvent() {
     export_event.mutable_task_event_data()->CopyFrom(*(*ptr_to_task_event_data_ptr));
     export_event.set_source_type(
         rpc::ExportEvent_SourceType::ExportEvent_SourceType_EXPORT_TASK);
+  } else if (auto ptr_to_actor_event_data_ptr =
+                 std::get_if<std::shared_ptr<rpc::ExportActorData>>(&event_data_ptr_)) {
+    export_event.mutable_actor_event_data()->CopyFrom(*(*ptr_to_actor_event_data_ptr));
+    export_event.set_source_type(
+        rpc::ExportEvent_SourceType::ExportEvent_SourceType_EXPORT_ACTOR);
   } else {
     // This shouldn't be possible because event_data_ptr_ is typed as ExportEventDataPtr
     RAY_LOG(FATAL) << "Invalid event_data type.";
