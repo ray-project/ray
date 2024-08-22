1c6760689ab33ca9cd6a071548f7664a4aba6270
SangBin Cho <sangcho@sangcho-LT93GQWG9C.local>
2024-08-22 08:49:14 -0700

    .

--- a.py
+++ a.py
@@ -1,35 +1,26 @@
-import ray
-from ray.dag.input_node import InputNode
-from ray.dag.output_node import MultiOutputNode
+from queue import Queue
+import threading
+
 import time
 
-@ray.remote
-class Actor1:
-    def fwd(self, x):
-        time.sleep(1)
+q = Queue()
+d = b"1" * 1024 * 100 # 100kb
+done = []
+def run():
+    i = 0
+    while True:
+        result = q.get()
+        i += 1
+        if i == 100:
+            break
+    print("done", time.time())
 
-@ray.remote
-class Actor2:
-    def fwd(self, x):
-        time.sleep(0.1)
+t = threading.Thread(target=run)
+t.start()
+st = time.time()
+for _ in range(100):
+    q.put(d)
+el = time.time() - st
 
-    def bwd(self, x, y):
-        time.sleep(1)
-
-actor1 = Actor1.remote()
-actor2 = Actor2.remote()
-
-with InputNode() as input_node:
-    ref1 = actor1.fwd.bind(input_node)
-    ref2 = actor2.fwd.bind(input_node)
-    dag = actor2.bwd.bind(ref1, ref2)
-dag = dag.experimental_compile()
-
-output_refs = []
-
-s = time.time()
-for i in range(10):
-    output_refs.append(dag.execute(1))
-ray.get(output_refs)
-e = time.time()
-print(f"Total run time: {e - s}")
+print(el / 100 * 1000 * 1000, "put_and_get/us")
+breakpoint()
