83549077babb6a28778d0ef9ea7ddfc16731e50e
Nikita Vemuri <nikitavemuri@anyscale.com>
2024-08-23 18:03:00 -0700

    formatting
    Signed-off-by: Nikita Vemuri <nikitavemuri@anyscale.com>

--- src/ray/util/event.cc
+++ src/ray/util/event.cc
@@ -165,8 +165,8 @@ void LogEventReporter::Report(const rpc::Event &event, const json &custom_fields
 void LogEventReporter::ReportExportEvent(const rpc::ExportEvent &export_event) {
   RAY_CHECK(ExportEvent_SourceType_IsValid(export_event.source_type()));
   std::string result = ExportEventToString(export_event);
-  log_sink_->info(result);
 
+  log_sink_->info(result);
   if (force_flush_) {
     Flush();
   }
