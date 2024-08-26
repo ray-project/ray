e8591145774d5d3afdca0b75e74e6af71b67d0a1
Nikita Vemuri <nikitavemuri@anyscale.com>
2024-08-26 14:31:24 -0700

    update
    Signed-off-by: Nikita Vemuri <nikitavemuri@anyscale.com>

--- src/ray/gcs/gcs_server/gcs_server_main.cc
+++ src/ray/gcs/gcs_server/gcs_server_main.cc
@@ -88,6 +88,9 @@ int main(int argc, char *argv[]) {
 
   // Initialize event framework.
   if (RayConfig::instance().event_log_reporter_enabled() && !log_dir.empty()) {
+    // This GCS server process emits GCS standard events, and Actor export events
+    // so the various source types are passed to RayEventInit. The type of an
+    // event is determined by the schema of its event data.
     const std::vector<ray::SourceTypeVariant> source_types = {
         ray::rpc::Event_SourceType::Event_SourceType_GCS,
         ray::rpc::ExportEvent_SourceType_EXPORT_ACTOR};
