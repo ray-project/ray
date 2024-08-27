180aac3cf07935dcca4262987b1d8292cc6344ce
Jiajun Yao <jeromeyjj@gmail.com>
2024-08-27 09:40:46 -0700

    up
    Signed-off-by: Jiajun Yao <jeromeyjj@gmail.com>

--- src/ray/core_worker/transport/actor_task_submitter.cc
+++ src/ray/core_worker/transport/actor_task_submitter.cc
@@ -97,6 +97,9 @@ Status ActorTaskSubmitter::SubmitActorCreationTask(TaskSpecification task_spec) 
               RAY_LOG(INFO).WithField(actor_id)
                   << "Failed to create actor with status: " << status.ToString();
             }
+            // Actor creation task retry happens in GCS
+            // and transient rpc errors are retried in gcs client
+            // so we don't need to retry here.
             RAY_UNUSED(task_finisher_.FailPendingTask(
                 task_id,
                 rpc::ErrorType::ACTOR_CREATION_FAILED,
