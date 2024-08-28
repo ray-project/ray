0408a65dab36526a62bd947a271f267ea4bc2eee
Jiajun Yao <jeromeyjj@gmail.com>
2024-08-27 22:58:34 -0700

    up
    Signed-off-by: Jiajun Yao <jeromeyjj@gmail.com>

--- python/ray/tests/test_system_metrics.py
+++ python/ray/tests/test_system_metrics.py
@@ -1,5 +1,8 @@
 import os
 import time
+
+import pytest
+
 import ray
 from ray._private.test_utils import (
     raw_metrics,
@@ -37,3 +40,12 @@ def test_unintentional_worker_failures_metric(shutdown_only):
     # Wait for metrics to be reported
     time.sleep(1)
     wait_for_condition(lambda: verify())
+
+
+if __name__ == "__main__":
+    import sys
+
+    if os.environ.get("PARALLEL_CI"):
+        sys.exit(pytest.main(["-n", "auto", "--boxed", "-vs", __file__]))
+    else:
+        sys.exit(pytest.main(["-sv", __file__]))
