bdcfff8f8716fa70f2d53285591c2c8e08f26b46
Nikita Vemuri <nikitavemuri@anyscale.com>
2024-09-03 14:51:55 -0700

    add test
    Signed-off-by: Nikita Vemuri <nikitavemuri@anyscale.com>

--- python/ray/dashboard/modules/job/tests/test_job_manager.py
+++ python/ray/dashboard/modules/job/tests/test_job_manager.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 import os
 import signal
 import sys
@@ -178,6 +179,49 @@ async def test_get_all_job_info(call_ray_start, tmp_path):  # noqa: F811
             assert job_info.driver_node_id != ""
 
     assert found
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "call_ray_start",
+    ["ray start --head"],
+    indirect=True,
+)
+async def test_submission_job_export_events(call_ray_start, tmp_path):  # noqa: F811
+    """Submission job export events are correctly written"""
+    address_info = ray.init(address=call_ray_start)
+    gcs_aio_client = GcsAioClient(
+        address=address_info["gcs_address"], nums_reconnect_retry=0
+    )
+    log_dir = ray._private.worker._global_node.get_logs_dir_path()
+    job_manager = JobManager(gcs_aio_client, log_dir)
+
+    # Submit a job.
+    submission_id = await job_manager.submit_job(
+        entrypoint="python -c 'import ray; ray.init()'",
+    )
+
+    # Wait for the job to be finished.
+    await async_wait_for_condition_async_predicate(
+        check_job_succeeded, job_manager=job_manager, job_id=submission_id
+    )
+
+    # Verify export events are written
+    event_dir = f"{log_dir}/events"
+    assert os.path.isdir(event_dir)
+    event_file = f"{event_dir}/event_EXPORT_SUBMISSION_JOB.log"
+    assert os.path.isfile(event_file)
+
+    with open(event_file, "r") as f:
+        lines = f.readlines()
+        assert len(lines) == 3
+        expected_status_values = ["PENDING", "RUNNING", "SUCCEEDED"]
+
+        for line, expected_status in zip(lines, expected_status_values):
+            data = json.loads(line)
+            assert data["source_type"] == "EXPORT_SUBMISSION_JOB"
+            assert data["event_data"]["submission_job_id"] == submission_id
+            assert data["event_data"]["status"] == expected_status
 
 
 @pytest.mark.asyncio
