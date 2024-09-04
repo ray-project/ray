Fixed a small bug that was caused by passing in a gradient dict instead of a list.
Signed-off-by: simonsays1980 <simon.zehnder@gmail.com>

rllib/core/learner/learner.py
@@ -575,7 +575,8 @@ class Learner(Checkpointable):
             elif config.log_gradients:
                 # Compute the global norm of gradients and log it.
                 global_norm = self._get_global_norm_function()(
-                    grad_dict_to_clip,
+                    # Note, `tf.linalg.global_norm` needs a list of tensors.
+                    list(grad_dict_to_clip.values()),
                 )
                 self.metrics.log_value(
                     key=(module_id, f"gradients_{optimizer_name}_global_norm"),