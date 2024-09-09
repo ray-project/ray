a73d60478feba8b8f6963124a3311fd75c34a280
simonsays1980 <simon.zehnder@gmail.com>
2024-09-09 20:06:58 +0200

    [RLlib; Offline RL] - Replace GAE in `MARWILOfflinePreLearner` with `GeneralAdvantageEstimation` connector in learner pipeline. (#47532)
    Signed-off-by: ujjawal-khare <ujjawal.khare@dream11.com>

--- rllib/tuned_examples/marwil/cartpole_marwil.py
+++ rllib/tuned_examples/marwil/cartpole_marwil.py
@@ -49,21 +49,10 @@ config = (
     # as remote learners.
     .offline_data(
         input_=[data_path.as_posix()],
-        # The `kwargs` for the `input_read_method`. We override the
-        # the number of blocks to pull at once b/c our dataset is
-        # small.
-        input_read_method_kwargs={"override_num_blocks": max(args.num_gpus * 2, 2)},
-        # The `kwargs` for the `map_batches` method in which our
-        # `OfflinePreLearner` is run. 2 data workers should be run
-        # concurrently.
-        map_batches_kwargs={"concurrency": 2, "num_cpus": 2},
-        # The `kwargs` for the `iter_batches` method. Due to the small
-        # dataset we choose only a single batch to prefetch.
-        iter_batches_kwargs={"prefetch_batches": 1},
-        # The number of iterations to be run per learner when in multi-learner
-        # mode in a single RLlib training iteration. Leave this to `None` to
-        # run an entire epoch on the dataset during a single RLlib training
-        # iteration. For single-learner mode 1 is the only option.
+        # Note, we want to have at leat 2 data blocks to read from such that
+        # concurrency in `map_batches` works.
+        input_read_method_kwargs={"override_num_blocks": max(args.num_gpus, 2)},
+        prelearner_module_synch_period=20,
         dataset_num_iters_per_learner=1 if args.num_gpus == 0 else None,
     )
     .training(
