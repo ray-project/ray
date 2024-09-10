d3464dce11a7e578c9c8d7d418ab2dccb94a7b9a
Balaji Veeramani <balaji@anyscale.com>
2024-09-11 07:39:46 +0900

    [Data] Increase number of blocks per read files task (#995)
    See https://github.com/anyscale/runtime/issues/927 for context.
    
    Fixes #927.
    
    Signed-off-by: Balaji Veeramani <bveeramani@berkeley.edu>

--- python/ray/anyscale/data/_internal/planner/plan_expand_paths_op.py
+++ python/ray/anyscale/data/_internal/planner/plan_expand_paths_op.py
@@ -31,6 +31,10 @@ logger = logging.getLogger(__name__)
 # TODO(@bveeramani): 200 is arbitrary.
 MAX_NUM_INPUT_BLOCKS = 200
 
+# To amortize overheads associated with launching Ray tasks and using multi-threading,
+# produce multiple blocks in each read task.
+NUM_NUM_BLOCKS_PER_READ_TASK = 8
+
 
 def plan_expand_paths_op(
     op: ExpandPaths, physical_children: List[PhysicalOperator]
@@ -108,6 +112,7 @@ def create_expand_paths_operator(
                     if (
                         running_in_memory_size
                         > ray.data.DataContext.get_current().target_max_block_size
+                        * NUM_NUM_BLOCKS_PER_READ_TASK
                     ):
                         block = pa.Table.from_pydict({"path": file_paths})
                         yield block
