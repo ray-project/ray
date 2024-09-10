7430d396669a02e2d6d306d6f11d89aed353fd57
Weichen Xu <weichen.xu@databricks.com>
2024-09-10 20:35:30 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- python/ray/tests/spark/test_basic.py
+++ python/ray/tests/spark/test_basic.py
@@ -341,8 +341,7 @@ class TestSparkLocalCluster:
 
         shutdown_ray_cluster()
 
-    @pytest.mark.parametrize("autoscale", [False, True])
-    def test_setup_global_ray_cluster(self, autoscale):
+    def test_setup_global_ray_cluster(self):
         shutil.rmtree("/tmp/ray", ignore_errors=True)
 
         assert ray.util.spark.cluster_init._global_ray_cluster_cancel_event is None
@@ -356,7 +355,7 @@ class TestSparkLocalCluster:
                     ):
                         setup_global_ray_cluster(
                             max_worker_nodes=1,
-                            min_worker_nodes=(0 if autoscale else 1),
+                            min_worker_nodes=1,
                         )
                 except BaseException:
                     # For debugging testing failure.
