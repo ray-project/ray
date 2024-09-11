95e078ee8e8206ca3468ccc3941e5a7d762d623f
Xiaoshuang Liu <liuxiaoshuang4@huawei.com>
2024-09-12 02:58:43 +0800

    [GCS] Optimize `GetAllJobInfo` API for performance (#47530)
    Signed-off-by: liuxsh9 <liuxiaoshuang4@huawei.com>
    Signed-off-by: ujjawal-khare <ujjawal.khare@dream11.com>

--- src/ray/gcs/gcs_client/global_state_accessor.cc
+++ src/ray/gcs/gcs_client/global_state_accessor.cc
@@ -69,6 +69,8 @@ std::vector<std::string> GlobalStateAccessor::GetAllJobInfo(
     absl::ReaderMutexLock lock(&mutex_);
     RAY_CHECK_OK(gcs_client_->Jobs().AsyncGetAll(
         /*job_or_submission_id=*/std::nullopt,
+        skip_submission_job_info_field,
+        skip_is_running_tasks_field,
         TransformForMultiItemCallback<rpc::JobTableData>(job_table_data, promise),
         /*timeout_ms=*/-1));
   }
