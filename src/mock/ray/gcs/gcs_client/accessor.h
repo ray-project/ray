1528145ce8debdb2d691b21db82e82a7c9b24d3e
Xiaoshuang Liu <liuxiaoshuang4@huawei.com>
2024-09-12 02:58:43 +0800

    [GCS] Optimize `GetAllJobInfo` API for performance (#47530)
    Signed-off-by: liuxsh9 <liuxiaoshuang4@huawei.com>
    Signed-off-by: ujjawal-khare <ujjawal.khare@dream11.com>

--- src/mock/ray/gcs/gcs_client/accessor.h
+++ src/mock/ray/gcs/gcs_client/accessor.h
@@ -106,6 +106,8 @@ class MockJobInfoAccessor : public JobInfoAccessor {
   MOCK_METHOD(Status,
               AsyncGetAll,
               (const std::optional<std::string> &job_or_submission_id,
+               bool skip_submission_job_info_field,
+               bool skip_is_running_tasks_field,
                const MultiItemCallback<rpc::JobTableData> &callback,
                int64_t timeout_ms),
               (override));
