39ec5de1cf63df9976995ca08bffbab62ecb2a6e
Balaji Veeramani <balaji@anyscale.com>
2024-09-13 06:18:24 +0900

    [Data] Refactor custom optimizer rules (#1011)
    This PR refactors the way we add custom optimizer rules so it's easier
    to add Anyscale-specific optimization rules.
    
    For more context, see https://github.com/ray-project/ray/pull/47605.
    
    Signed-off-by: Balaji Veeramani <bveeramani@berkeley.edu>

--- python/ray/anyscale/data/apply_anyscale_patches.py
+++ python/ray/anyscale/data/apply_anyscale_patches.py
@@ -1,6 +1,13 @@
 import ray
+from ray._private.ray_constants import env_bool
 from ray.anyscale.data.api.dataset_mixin import DatasetMixin
+from ray.anyscale.data.local_limit import ApplyLocalLimitRule
 from ray.anyscale.data.planner import _register_anyscale_plan_logical_op_fns
+from ray.data._internal.logical.optimizers import register_physical_rule
+
+ANYSCALE_LOCAL_LIMIT_MAP_OPERATOR_ENABLED = env_bool(
+    "ANYSCALE_LOCAL_LIMIT_MAP_OPERATOR_ENABLED", False
+)
 
 
 def _patch_class_with_mixin(original_cls, mixin_cls):
@@ -15,3 +22,6 @@ def apply_anyscale_patches():
     _patch_class_with_mixin(ray.data.Dataset, DatasetMixin)
 
     _register_anyscale_plan_logical_op_fns()
+
+    if ANYSCALE_LOCAL_LIMIT_MAP_OPERATOR_ENABLED:
+        register_physical_rule(ApplyLocalLimitRule)
