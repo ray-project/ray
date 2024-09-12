989f51934056da8d6a4dacba8c175bd991be5561
Hao Chen <chenh1024@gmail.com>
2024-09-11 17:06:13 -0700

    [data] refine test_removed_nodes_not_added_back (#1015)
    Make test_removed_nodes_not_added_back more robust by making sure the
    actors are running tasks when removing the nodes.
    
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- python/ray/anyscale/data/tests/test_advanced_fault_tolerance.py
+++ python/ray/anyscale/data/tests/test_advanced_fault_tolerance.py
@@ -15,6 +15,29 @@ def test_removed_nodes_not_added_back(ray_start_cluster):
     cluster.add_node(num_cpus=0)
     ray.init()
 
+    @ray.remote(num_cpus=0)
+    class Signal:
+        def __init__(self):
+            self._num_alive_actors = 0
+            self._nodes_removed = False
+
+        async def notify_actor_alive(self):
+            self._num_alive_actors += 1
+
+        async def wait_for_actors_alive(self, value):
+            while self._num_alive_actors != value:
+                await asyncio.sleep(0.01)
+
+        async def notify_nodes_removed(self):
+            self._nodes_removed = True
+
+        async def wait_for_nodes_removed(self):
+            while not self._nodes_removed:
+                await asyncio.sleep(0.01)
+
+    # Create the signal actor on the head node.
+    signal_actor = Signal.remote()
+
     num_nodes = 4
     nodes = []
     for _ in range(num_nodes):
@@ -23,20 +46,6 @@ def test_removed_nodes_not_added_back(ray_start_cluster):
 
     num_items = 100
 
-    @ray.remote(num_cpus=0)
-    class Signal:
-        def __init__(self):
-            self._counter = 0
-
-        async def incr(self):
-            self._counter += 1
-
-        async def wait(self, value):
-            while self._counter != value:
-                await asyncio.sleep(0.1)
-
-    signal_actor = Signal.remote()
-
     class MyUDF:
         def __init__(self, signal_actor):
             self._signal_actor = signal_actor
@@ -44,9 +53,12 @@ def test_removed_nodes_not_added_back(ray_start_cluster):
 
         def __call__(self, batch):
             if not self._signal_sent:
-                self._signal_actor.incr.remote()
+                self._signal_actor.notify_actor_alive.remote()
+                # Wait for the driver to remove nodes. This makes sure all
+                # actors are running tasks when removing nodes.
+                ray.get(self._signal_actor.wait_for_nodes_removed.remote())
                 self._signal_sent = True
-            time.sleep(0.1)
+            time.sleep(0.01)
             return batch
 
     res = []
@@ -68,11 +80,12 @@ def test_removed_nodes_not_added_back(ray_start_cluster):
     thread.start()
 
     # Wait for all actors to start, then remove some nodes.
-    ray.get(signal_actor.wait.remote(num_nodes))
+    ray.get(signal_actor.wait_for_actors_alive.remote(num_nodes))
     print("Removing nodes")
     nodes_to_remove = nodes[-num_nodes // 2 :]
     for node in nodes_to_remove:
         cluster.remove_node(node)
+    ray.get(signal_actor.notify_nodes_removed.remote())
 
     thread.join()
     assert sorted(res, key=lambda x: x["id"]) == [{"id": i} for i in range(num_items)]
