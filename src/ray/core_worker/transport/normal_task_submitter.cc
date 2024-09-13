b083fe1131f68dc507061be27cc98cafd9c8bde5
hejialing.hjl <hejialing.hjl@bytedance.com>
2024-09-13 19:34:38 +0800

    save

--- src/ray/core_worker/transport/normal_task_submitter.cc
+++ src/ray/core_worker/transport/normal_task_submitter.cc
@@ -714,10 +714,15 @@ Status NormalTaskSubmitter::CancelTask(TaskSpecification task_spec,
   std::shared_ptr<rpc::CoreWorkerClientInterface> client = nullptr;
   {
     absl::MutexLock lock(&mu_);
-    if (cancelled_tasks_.find(task_spec.TaskId()) != cancelled_tasks_.end() ||
-        !task_finisher_->MarkTaskCanceled(task_spec.TaskId())) {
+    if (cancelled_tasks_.find(task_spec.TaskId()) != cancelled_tasks_.end()) {
       return Status::OK();
     }
+  }
+  if (!task_finisher_->MarkTaskCanceled(task_spec.TaskId())) {
+    return Status::OK();
+  }
+  {
+    absl::ReleasableMutexLock lock(&mu_);
 
     auto &scheduling_key_entry = scheduling_key_entries_[scheduling_key];
     auto &scheduling_tasks = scheduling_key_entry.task_queue;
@@ -731,6 +736,7 @@ Status NormalTaskSubmitter::CancelTask(TaskSpecification task_spec,
           if (scheduling_tasks.empty()) {
             CancelWorkerLeaseIfNeeded(scheduling_key);
           }
+          lock.Release();
           RAY_UNUSED(task_finisher_->FailPendingTask(task_spec.TaskId(),
                                                      rpc::ErrorType::TASK_CANCELLED));
           return Status::OK();
