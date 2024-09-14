5faa96d2445e36abf089cddbec816f204d0fef5e
SangBin Cho <rkooo567@gmail.com>
2024-09-13 23:10:34 -0700

    wip

--- python/ray/tests/test_serialization.py
+++ python/ray/tests/test_serialization.py
@@ -733,6 +733,35 @@ def test_usage_with_dataclass(ray_start_regular):
     assert dataclasses.asdict(new_y) == expect_dict
 
 
+def test_pytorch_tensor_zerocopy_serialization(shutdown_only):
+    ray.init()
+    import torch
+
+    # from ray.util import serialization_addons
+    # ctx = ray._private.worker.global_worker.get_serialization_context()
+    # serialization_addons.register_torch_zero_copy_serializer(ctx)
+    # test dense tensor
+    tensor = torch.rand(32, 3, 64, 64)
+    ref = ray.put(tensor)
+    tensor_1, tensor_2 = ray.get([ref] * 2)
+    assert tensor_1.data_ptr() == tensor_2.data_ptr()
+
+    # test sparse tensor
+    i = torch.arange(0, 1024 * 1024, 4).view(1, -1)
+    v = torch.rand(1024 * 1024 // 4)
+    k = torch.sparse_coo_tensor(i, v, size=(1024 * 1024,))
+    ref = ray.put(k)
+    tensor_1, tensor_2 = ray.get([ref] * 2)
+    assert tensor_1._indices().data_ptr() == tensor_2._indices().data_ptr()
+    assert tensor_1._values().data_ptr() == tensor_2._values().data_ptr()
+
+    # test attributes
+    tensor = torch.rand(4).requires_grad_(True)
+    ref = ray.put(tensor)
+    tensor = ray.get(ref)
+    assert tensor.requires_grad
+
+
 if __name__ == "__main__":
     import os
     import pytest
--- python/ray/util/serialization_addons.py
+++ python/ray/util/serialization_addons.py
@@ -7,6 +7,80 @@ import sys
 
 from ray.util.annotations import DeveloperAPI
 
+import warnings
+
+_TORCH_WARNING_FILTER_ACTIVATE = True
+
+
+class _TorchTensorReducingHelper:
+    def __init__(self, tensor: "torch.Tensor"):
+        self.tensor = tensor
+
+    @classmethod
+    def rebuild_tensor(cls, rebuild_func, device, ndarray, params):
+        import torch
+
+        global _TORCH_WARNING_FILTER_ACTIVATE
+        # filtering warning messages would be the bottleneck for
+        # deserializing torch tensors. Since the warning only prompts once,
+        # we would only deal with it for the first time.
+        if _TORCH_WARNING_FILTER_ACTIVATE:
+            with warnings.catch_warnings():
+                warnings.filterwarnings(
+                    "ignore",
+                    category=UserWarning,
+                    message="The given NumPy array is not writeable",
+                )
+                _tensor = torch.from_numpy(ndarray)
+            _TORCH_WARNING_FILTER_ACTIVATE = False
+        else:
+            _tensor = torch.from_numpy(ndarray)
+        if device != torch.device("cpu"):
+            _tensor = _tensor.to(device)
+        tensor = rebuild_func(_tensor.storage(), *params)
+        return cls(tensor)
+
+    @classmethod
+    def rebuild_sparse_tensor(cls, rebuild_func, content):
+        tensor = rebuild_func(*content)
+        return cls(tensor)
+
+    def __reduce_ex__(self, protocol):
+        _rebuild_func, content = self.tensor.__reduce_ex__(protocol)
+        if self.tensor.is_sparse:
+            # Torch will help us reduce the sparse tensor into
+            # several continuous tensors.
+            return self.rebuild_sparse_tensor, (_rebuild_func, content)
+        # By only replacing the storage with a numpy array, we can reuse
+        # zero-copy serialization while keeping all other params of the
+        # torch tensor.
+        return self.rebuild_tensor, (
+            _rebuild_func,
+            self.tensor.device,
+            self.tensor.detach().cpu().numpy(),
+            content[1:],
+        )
+
+
+def _unwrap_tensor(s: "torch.Tensor"):
+    return s.tensor
+
+
+def torch_tensor_reducer(tensor: "torch.Tensor"):
+    return _unwrap_tensor, (_TorchTensorReducingHelper(tensor),)
+
+
+@DeveloperAPI
+def register_torch_zero_copy_serializer(serialization_context):
+    """Register zero copy torch serializer to Ray."""
+    try:
+        import torch
+    except ImportError:
+        return
+    serialization_context._register_cloudpickle_reducer(
+        torch.Tensor, torch_tensor_reducer
+    )
+
 
 @DeveloperAPI
 def register_starlette_serializer(serialization_context):
@@ -30,6 +104,7 @@ def apply(serialization_context):
 
     register_pydantic_serializers(serialization_context)
     register_starlette_serializer(serialization_context)
+    register_torch_zero_copy_serializer(serialization_context)
 
     if sys.platform != "win32":
         from ray._private.arrow_serialization import (
