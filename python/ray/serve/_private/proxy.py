f9d264a9f9a068d7c0e06193235b5ff95773acce
Gene Su <e870252314@gmail.com>
2024-09-17 13:20:12 -0700

    [Serve] Refactor helper function to add grpc address
    Signed-off-by: Gene Su <e870252314@gmail.com>

--- python/ray/serve/_private/proxy.py
+++ python/ray/serve/_private/proxy.py
@@ -40,7 +40,11 @@ from ray.serve._private.constants import (
     SERVE_MULTIPLEXED_MODEL_ID,
     SERVE_NAMESPACE,
 )
-from ray.serve._private.grpc_util import DummyServicer, create_serve_grpc_server
+from ray.serve._private.grpc_util import (
+    DummyServicer,
+    add_grpc_address,
+    create_serve_grpc_server,
+)
 from ray.serve._private.http_util import (
     MessageQueue,
     convert_object_to_asgi_messages,
