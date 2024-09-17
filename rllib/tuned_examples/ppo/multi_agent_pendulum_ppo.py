c26ae5d43cd37ccb4e3b1a5eff5714c7538241a4
sven1977 <svenmika1977@gmail.com>
2024-09-17 12:45:18 +0200

    fix
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/tuned_examples/ppo/multi_agent_pendulum_ppo.py
+++ rllib/tuned_examples/ppo/multi_agent_pendulum_ppo.py
@@ -28,7 +28,7 @@ config = (
     )
     .environment("multi_agent_pendulum", env_config={"num_agents": args.num_agents})
     .env_runners(
-        env_to_module_connector=lambda env: MeanStdFilter(),
+        env_to_module_connector=lambda env: MeanStdFilter(multi_agent=True),
     )
     .training(
         train_batch_size_per_learner=1024,
