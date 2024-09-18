cb31d432094f8869fc83f58267b8e32dc33c6549
Josh Karpel <josh.karpel@gmail.com>
2024-09-18 11:44:03 -0500

    forgot to actually run the deploys...
    Signed-off-by: Josh Karpel <josh.karpel@gmail.com>

--- python/ray/dashboard/modules/serve/tests/deploy_imperative_serve_apps.py
+++ python/ray/dashboard/modules/serve/tests/deploy_imperative_serve_apps.py
@@ -6,4 +6,11 @@ from ray.serve.tests.test_config_files import world
 
 def submit_imperative_apps() -> None:
     serve.run(world.DagNode, name="app1", route_prefix="/apple")
+    print("Deployed app1")
+
     serve.run(world.DagNode, name="app2", route_prefix="/banana")
+    print("Deployed app2")
+
+
+if __name__ == "__main__":
+    submit_imperative_apps()
