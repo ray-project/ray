6ddd15de75f024a21820500ac1541b2ad62ae7fc
simonsays1980 <simon.zehnder@gmail.com>
2024-09-19 15:49:51 +0200

    Fixed a bug with the filesystem in 'OffineData.
    Signed-off-by: simonsays1980 <simon.zehnder@gmail.com>

--- rllib/offline/offline_data.py
+++ rllib/offline/offline_data.py
@@ -1,5 +1,6 @@
 import logging
 from pathlib import Path
+import pyarrow.fs
 import ray
 import time
 import types
@@ -45,8 +46,8 @@ class OfflineData:
         self.data_is_mapped = False
 
         # Set the filesystem.
-        self.filesystem = self.config.output_filesystem
-        self.filesystem_kwargs = self.config.output_filesystem_kwargs
+        self.filesystem = self.config.input_filesystem
+        self.filesystem_kwargs = self.config.input_filesystem_kwargs
         self.filesystem_object = None
 
         # If a specific filesystem is given, set it up. Note, this could
@@ -58,13 +59,13 @@ class OfflineData:
 
             self.filesystem_object = gcsfs.GCSFileSystem(**self.filesystem_kwargs)
         elif self.filesystem == "s3":
-            from pyarrow import fs
-
-            self.filesystem_object = fs.S3FileSystem(**self.filesystem_kwargs)
+            self.filesystem_object = pyarrow.fs.S3FileSystem(**self.filesystem_kwargs)
         elif self.filesystem == "abs":
             import adlfs
 
             self.filesystem_object = adlfs.AzureBlobFileSystem(**self.filesystem_kwargs)
+        elif isinstance(self.filesystem, pyarrow.fs.FileSystem):
+            self.filesystem_object = self.filesystem
         elif self.filesystem is not None:
             raise ValueError(
                 f"Unknown filesystem: {self.filesystem}. Filesystems can be "
