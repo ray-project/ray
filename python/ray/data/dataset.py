1cbc30c4dbb52685587a673d0664913878646e8c
Yun Tang <myasuka@live.com>
2024-09-19 14:59:29 +0800

    [Docs][hotfix] Correct the desc of  nums of blocks
    Signed-off-by: Yun Tang <myasuka@live.com>

--- python/ray/data/dataset.py
+++ python/ray/data/dataset.py
@@ -2700,7 +2700,7 @@ class Dataset:
         """Writes the :class:`~ray.data.Dataset` to parquet files under the provided ``path``.
 
         The number of files is determined by the number of blocks in the dataset.
-        To control the number of number of blocks, call
+        To control the number of blocks, call
         :meth:`~ray.data.Dataset.repartition`.
 
         If pyarrow can't represent your data, this method errors.
@@ -2802,7 +2802,7 @@ class Dataset:
         """Writes the :class:`~ray.data.Dataset` to JSON and JSONL files.
 
         The number of files is determined by the number of blocks in the dataset.
-        To control the number of number of blocks, call
+        To control the number of blocks, call
         :meth:`~ray.data.Dataset.repartition`.
 
         This method is only supported for datasets with records that are convertible to
@@ -2986,7 +2986,7 @@ class Dataset:
         """Writes the :class:`~ray.data.Dataset` to CSV files.
 
         The number of files is determined by the number of blocks in the dataset.
-        To control the number of number of blocks, call
+        To control the number of blocks, call
         :meth:`~ray.data.Dataset.repartition`.
 
         This method is only supported for datasets with records that are convertible to
@@ -3106,7 +3106,7 @@ class Dataset:
             and will error if the dataset contains unsupported types.
 
         The number of files is determined by the number of blocks in the dataset.
-        To control the number of number of blocks, call
+        To control the number of blocks, call
         :meth:`~ray.data.Dataset.repartition`.
 
         This method is only supported for datasets with records that are convertible to
@@ -3279,7 +3279,7 @@ class Dataset:
         NumPy arrays.
 
         The number of files is determined by the number of blocks in the dataset.
-        To control the number of number of blocks, call
+        To control the number of blocks, call
         :meth:`~ray.data.Dataset.repartition`.
 
 
@@ -3429,7 +3429,7 @@ class Dataset:
         This method is only supported for datasets convertible to pyarrow tables.
 
         The number of parallel writes is determined by the number of blocks in the
-        dataset. To control the number of number of blocks, call
+        dataset. To control the number of blocks, call
         :meth:`~ray.data.Dataset.repartition`.
 
         .. warning::
