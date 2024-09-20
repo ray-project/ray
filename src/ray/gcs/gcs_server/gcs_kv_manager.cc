f8dec041056e1366e7bf788066abb74473a0b2b6
Ruiyang Wang <rywang014@gmail.com>
2024-09-20 09:25:22 -0700

    fix
    Signed-off-by: Ruiyang Wang <rywang014@gmail.com>

--- src/ray/gcs/gcs_server/gcs_kv_manager.cc
+++ src/ray/gcs/gcs_server/gcs_kv_manager.cc
@@ -145,7 +145,7 @@ void GcsInternalKVManager::HandleGetInternalConfig(
     rpc::GetInternalConfigRequest request,
     rpc::GetInternalConfigReply *reply,
     rpc::SendReplyCallback send_reply_callback) {
-  reply->set_config(stored_config_.config());
+  reply->set_config(raylet_config_list_);
   GCS_RPC_SEND_REPLY(send_reply_callback, reply, Status::OK());
   ++counts_[CountType::GET_INTERNAL_CONFIG_REQUEST];
 }
