931c6e124e96216a3215a51ddadc252c84b7c0c9
sven1977 <svenmika1977@gmail.com>
2024-09-21 22:41:43 +0200

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/core/rl_module/torch/torch_rl_module.py
+++ rllib/core/rl_module/torch/torch_rl_module.py
@@ -8,6 +8,7 @@ from ray.rllib.core.rl_module.rl_module import RLModule
 from ray.rllib.core.rl_module.torch.torch_compile_config import TorchCompileConfig
 from ray.rllib.models.torch.torch_distributions import (
     TorchCategorical,
+    TorchDiagGaussian,
     TorchDistribution,
 )
 from ray.rllib.utils.annotations import override, OverrideToImplementCustomLogic
@@ -127,20 +128,33 @@ class TorchRLModule(nn.Module, RLModule):
     @OverrideToImplementCustomLogic
     @override(RLModule)
     def get_inference_action_dist_cls(self) -> Type[TorchDistribution]:
-        if isinstance(self.config.action_space, gym.spaces.Discrete):
+        if self.action_dist_cls is not None:
+            return self.action_dist_cls
+        elif isinstance(self.config.action_space, gym.spaces.Discrete):
             return TorchCategorical
+        elif isinstance(self.config.action_space, gym.spaces.Box):
+            return TorchDiagGaussian
+        else:
+            raise ValueError(
+                f"Default action distribution for action space "
+                f"{self.config.action_space} not supported! Either set the "
+                f"`self.action_dist_cls` property in your RLModule's `setup()` method "
+                f"to a subclass of `ray.rllib.models.torch.torch_distributions."
+                f"TorchDistribution` or - if you need different distributions for "
+                f"inference and training - override the three methods: "
+                f"`get_inference_action_dist_cls`, `get_exploration_action_dist_cls`, "
+                f"and `get_train_action_dist_cls` in your RLModule."
+            )
 
     @OverrideToImplementCustomLogic
     @override(RLModule)
     def get_exploration_action_dist_cls(self) -> Type[TorchDistribution]:
-        if isinstance(self.config.action_space, gym.spaces.Discrete):
-            return TorchCategorical
+        return self.get_inference_action_dist_cls()
 
     @OverrideToImplementCustomLogic
     @override(RLModule)
     def get_train_action_dist_cls(self) -> Type[TorchDistribution]:
-        if isinstance(self.config.action_space, gym.spaces.Discrete):
-            return TorchCategorical
+        return self.get_inference_action_dist_cls()
 
 
 class TorchDDPRLModule(RLModule, nn.parallel.DistributedDataParallel):
