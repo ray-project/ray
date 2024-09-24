49cea08ff4836b65d216e87822a7045a14bc2103
Jiajun Yao <jeromeyjj@gmail.com>
2024-09-24 00:11:44 -0700

    fix
    Signed-off-by: Jiajun Yao <jeromeyjj@gmail.com>

--- python/ray/tests/test_task_metrics.py
+++ python/ray/tests/test_task_metrics.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 import sys
 import os
 import copy
+import multiprocessing
 
 import pytest
 
@@ -202,18 +203,8 @@ ray.get(w)
     proc.kill()
 
 
-def test_task_fetch_args(ray_start_cluster):
-    cluster = ray_start_cluster
-    cluster.add_node(
-        resources={"head": 1},
-        _system_config={
-            "metrics_report_interval_ms": 100,
-            "testing_asio_delay_us": "ObjectManagerService.grpc_server.Pull=5000000000:5000000000",  # noqa: E501
-        },
-    )
-    info = ray.init(address=cluster.address)
-    cluster.add_node(resources={"worker": 1})
-    cluster.wait_for_nodes()
+def driver_for_test_task_fetch_args(head_info):
+    ray.init("auto")
 
     @ray.remote(resources={"worker": 1})
     def task1():
@@ -227,14 +218,36 @@ def test_task_fetch_args(ray_start_cluster):
     o2 = task2.remote(o1)
 
     wait_for_condition(
-        lambda: tasks_by_state(info).get("PENDING_ARGS_FETCH", 0.0) == 1.0
+        lambda: tasks_by_state(head_info).get("PENDING_ARGS_FETCH", 0.0) == 1.0
     )
 
     ray.cancel(o2)
 
     wait_for_condition(
-        lambda: tasks_by_state(info).get("PENDING_ARGS_FETCH", 0.0) == 0.0
+        lambda: tasks_by_state(head_info).get("PENDING_ARGS_FETCH", 0.0) == 0.0
     )
+
+
+def test_task_fetch_args(ray_start_cluster):
+    cluster = ray_start_cluster
+    cluster.add_node(
+        resources={"head": 1},
+        _system_config={
+            "metrics_report_interval_ms": 100,
+            "testing_asio_delay_us": "ObjectManagerService.grpc_server.Pull=5000000000:5000000000",  # noqa: E501
+        },
+    )
+    head_info = ray.init(address=cluster.address)
+    cluster.add_node(resources={"worker": 1})
+    cluster.wait_for_nodes()
+
+    multiprocessing.set_start_method("spawn")
+    p = multiprocessing.Process(
+        target=driver_for_test_task_fetch_args, args=(head_info,)
+    )
+    p.start()
+    p.join()
+    assert p.exitcode == 0
 
 
 def test_task_wait_on_deps(shutdown_only):
