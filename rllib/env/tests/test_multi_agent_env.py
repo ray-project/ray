5f039eeb31b7a8bca12d4dff09ee2f9dcf9498f7
sven1977 <svenmika1977@gmail.com>
2024-09-26 14:34:31 +0200

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/env/tests/test_multi_agent_env.py
+++ rllib/env/tests/test_multi_agent_env.py
@@ -914,33 +914,6 @@ class TestMultiAgentEnv(unittest.TestCase):
             obs_space_in_preferred_format
         ), "Observation space should be in preferred format but isn't."
 
-    def test_spaces_sample_contain_in_preferred_format(self):
-        env = NestedMultiAgentEnv()
-        # this environment has spaces that are in the preferred format
-        # for multi-agent environments where the spaces are dict spaces
-        # mapping agent-ids to sub-spaces
-        obs = env.observation_space_sample()
-        assert env.observation_space_contains(
-            obs
-        ), "Observation space does not contain obs"
-
-        action = env.action_space_sample()
-        assert env.action_space_contains(action), "Action space does not contain action"
-
-    def test_spaces_sample_contain_not_in_preferred_format(self):
-        env = make_multi_agent("CartPole-v1")({"num_agents": 2})
-        # this environment has spaces that are not in the preferred format
-        # for multi-agent environments where the spaces not in the preferred
-        # format, users must override the observation_space_contains,
-        # action_space_contains observation_space_sample,
-        # and action_space_sample methods in order to do proper checks
-        obs = env.observation_space_sample()
-        assert env.observation_space_contains(
-            obs
-        ), "Observation space does not contain obs"
-        action = env.action_space_sample()
-        assert env.action_space_contains(action), "Action space does not contain action"
-
 
 if __name__ == "__main__":
     import pytest
