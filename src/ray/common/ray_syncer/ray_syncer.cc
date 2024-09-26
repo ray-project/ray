f2efd56f6395cb9b69baae976ced2c896710982b
Jiajun Yao <jeromeyjj@gmail.com>
2024-09-25 18:06:14 -0700

    up
    Signed-off-by: Jiajun Yao <jeromeyjj@gmail.com>

--- src/ray/common/ray_syncer/ray_syncer.cc
+++ src/ray/common/ray_syncer/ray_syncer.cc
@@ -354,6 +354,7 @@ ServerBidiReactor *RaySyncerService::StartSync(grpc::CallbackServerContext *cont
       [this](const std::string &node_id, bool reconnect) mutable {
         // No need to reconnect for server side.
         RAY_CHECK(!reconnect);
+        RAY_LOG(INFO) << "Connection is broken." << NodeID::FromBinary(node_id);
         syncer_.sync_reactors_.erase(node_id);
         syncer_.node_state_->RemoveNode(node_id);
       });
