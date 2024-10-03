ed960ea3ef626b7262f402042d864698fe2dd49c
sven1977 <svenmika1977@gmail.com>
2024-10-03 21:36:36 +0200

    wip
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/core/rl_module/marl_module.py
+++ rllib/core/rl_module/marl_module.py
@@ -4,5 +4,5 @@ from ray.rllib.utils.deprecation import deprecation_warning
 deprecation_warning(
     old="ray.rllib.core.rl_module.marl_module",
     new="ray.rllib.core.rl_module.multi_rl_module",
-    error=True,
+    error=False,
 )
