15de03d6169c3a37728bd7e69867baebc0d2e344
Sven Mika <sven@anyscale.io>
2024-10-08 19:41:52 +0200

    Apply suggestions from code review
    Signed-off-by: Sven Mika <sven@anyscale.io>

--- rllib/env/utils/infinite_lookback_buffer.py
+++ rllib/env/utils/infinite_lookback_buffer.py
@@ -78,9 +78,6 @@ class InfiniteLookbackBuffer:
             else self.data,
             "lookback": self.lookback,
             "finalized": self.finalized,
-            # "space_struct": gym_space_to_dict(self.space_struct)
-            # if self.space_struct
-            # else self.space_struct,
             "space": gym_space_to_dict(self.space) if self.space else self.space,
         }
 
@@ -99,10 +96,10 @@ class InfiniteLookbackBuffer:
         buffer.lookback = state["lookback"]
         buffer.finalized = state["finalized"]
         buffer.space = (
-            gym_space_from_dict(state["space"]) if state["space"] else state["space"]
+            gym_space_from_dict(state["space"]) if state["space"] else None
         )
         buffer.space_struct = (
-            get_base_struct_from_space(buffer.space) if buffer.space else buffer.space
+            get_base_struct_from_space(buffer.space) if buffer.space else None
         )
         buffer.data = (
             from_jsonable_if_needed(state["data"], buffer.space)
