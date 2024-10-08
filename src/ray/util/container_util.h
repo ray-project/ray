815a9e4a4752278f26dd731fc53347e5a03811e0
Ruiyang Wang <56065503+rynewang@users.noreply.github.com>
2024-10-07 17:27:45 -0700

    [core] Change debug_string from returning a string to streaming to an ostream. (#47893)
    We have a convenience function `debug_string` used in Ray logs: it
    prints printables (operator<<), containers, pairs. However it returns a
    std::string which is feed into RAY_LOG(). This makes a copy.
    
    Changes the signature to return a `DebugStringWrapper` which holds const
    reference to the argument, and is printable for all already supported
    types. Additionally supports std::tuple.
    
    This should only have marginal perf benefits since we typically don't
    debug_string a very big data structure.
    
    Signed-off-by: Ruiyang Wang <rywang014@gmail.com>
    Signed-off-by: ujjawal-khare <ujjawal.khare@dream11.com>

--- src/ray/util/container_util.h
+++ src/ray/util/container_util.h
@@ -21,7 +21,6 @@
 #include <set>
 #include <sstream>
 #include <type_traits>
-#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
@@ -70,13 +69,6 @@ std::ostream &operator<<(std::ostream &os, DebugStringWrapper<T> wrapper) {
   return os << wrapper.obj_;
 }
 
-// TODO(hjiang): Implement debug string for `std::variant`.
-template <>
-inline std::ostream &operator<<(std::ostream &os,
-                                DebugStringWrapper<std::nullopt_t> wrapper) {
-  return os << "(nullopt)";
-}
-
 template <typename... Ts>
 std::ostream &operator<<(std::ostream &os, DebugStringWrapper<std::pair<Ts...>> pair) {
   return os << "(" << debug_string(pair.obj_.first) << ", "
@@ -103,10 +95,6 @@ std::ostream &operator<<(std::ostream &os, DebugStringWrapper<std::tuple<Ts...>>
   return os;
 }
 
-template <typename T, std::size_t N>
-std::ostream &operator<<(std::ostream &os, DebugStringWrapper<std::array<T, N>> c) {
-  return c.StringifyContainer(os);
-}
 template <typename... Ts>
 std::ostream &operator<<(std::ostream &os, DebugStringWrapper<std::vector<Ts...>> c) {
   return c.StringifyContainer(os);
@@ -133,19 +121,6 @@ template <typename... Ts>
 std::ostream &operator<<(std::ostream &os,
                          DebugStringWrapper<absl::flat_hash_map<Ts...>> c) {
   return c.StringifyContainer(os);
-}
-template <typename... Ts>
-std::ostream &operator<<(std::ostream &os,
-                         DebugStringWrapper<std::unordered_map<Ts...>> c) {
-  return c.StringifyContainer(os);
-}
-
-template <typename T>
-std::ostream &operator<<(std::ostream &os, DebugStringWrapper<std::optional<T>> c) {
-  if (!c.obj_.has_value()) {
-    return os << debug_string(std::nullopt);
-  }
-  return os << debug_string(c.obj_.value());
 }
 
 template <typename C>
