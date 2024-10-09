1c6ad9604b4a81394281bfc0904c7477dfdb7e8d
sven1977 <svenmika1977@gmail.com>
2024-10-09 11:55:01 +0200

    fixes
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/utils/test_utils.py
+++ rllib/utils/test_utils.py
@@ -32,7 +32,7 @@ from ray import air, tune
 from ray.air.constants import TRAINING_ITERATION
 from ray.air.integrations.wandb import WandbLoggerCallback, WANDB_ENV_VAR
 from ray.rllib.common import SupportedFileType
-from ray.rllib.core import DEFAULT_MODULE_ID
+from ray.rllib.core import DEFAULT_MODULE_ID, Columns
 from ray.rllib.env.wrappers.atari_wrappers import is_atari, wrap_deepmind
 from ray.rllib.train import load_experiments_from_file
 from ray.rllib.utils.annotations import OldAPIStack
@@ -1833,12 +1833,26 @@ class ModelChecker:
         # Dict of models to check against each other.
         self.models = {}
 
-    def add(self, framework: str = "torch") -> Any:
+    def add(self, framework: str = "torch", obs=True, state=False) -> Any:
         """Builds a new Model for the given framework."""
         model = self.models[framework] = self.config.build(framework=framework)
 
         # Pass a B=1 observation through the model.
-        inputs = model.input_specs.fill(self.random_fill_input_value)
+        inputs = np.full(
+            [1] + ([1] if state else []) + list(self.config.input_dims),
+            self.random_fill_input_value,
+        )
+        if obs:
+            inputs = {Columns.OBS: inputs}
+        if state:
+            inputs[Columns.STATE_IN] = tree.map_structure(
+                lambda s: np.zeros(shape=[1] + list(s)), state
+            )
+        if framework == "torch":
+            from ray.rllib.utils.torch_utils import convert_to_torch_tensor
+
+            inputs = convert_to_torch_tensor(inputs)
+        # w/ old specs: inputs = model.input_specs.fill(self.random_fill_input_value)
 
         outputs = model(inputs)
 
