ead78494e030c300f37ce9c8464d6edd0f51e684
gorloffslava <gorloff.slava@gmail.com>
2024-10-09 19:47:15 +0500

    + checkpoint

--- meson_setup.sh
+++ meson_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export CFLAGS="-Wno-deprecated-declarations"
-export CXXFLAGS="-Wno-deprecated-declarations"
+export CFLAGS="-Wno-deprecated-declarations -Wno-deprecated-this-capture"
+export CXXFLAGS="-Wno-deprecated-declarations -Wno-deprecated-this-capture"
 
 meson setup --reconfigure eugo_build
