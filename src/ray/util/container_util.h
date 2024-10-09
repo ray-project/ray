4cb79557d33a05df42939e108274fc82585b23ad
dentiny <dentinyhao@gmail.com>
2024-10-09 10:49:19 +0000

    Avoid nullopt code duplication
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- src/ray/util/container_util.h
+++ src/ray/util/container_util.h
@@ -148,7 +148,7 @@ std::ostream &operator<<(std::ostream &os,
 template <typename T>
 std::ostream &operator<<(std::ostream &os, DebugStringWrapper<std::optional<T>> c) {
   if (!c.obj_.has_value()) {
-    return os << "(nullopt)";
+    return os << debug_string(std::nullopt);
   }
   return os << debug_string(c.obj_.value());
 }
