b83e7addaceb5633ce217d3e56cacc9c59d26a5d
ujjawal-khare <ujjawal.khare@dream11.com>
2024-10-15 13:34:02 +0530

    lint error fixed
    Signed-off-by: ujjawal-khare <ujjawal.khare@dream11.com>

--- python/ray/dashboard/tests/test_utils.py
+++ python/ray/dashboard/tests/test_utils.py
@@ -1,12 +1,15 @@
+import logging
+
 from ray.dashboard.utils import close_logger_file_descriptor
 
 
 def test_close_logger_file_descriptor():
+    logger_format = "%(message)s"
     logger = logging.getLogger("test_job_id")
 
     job_driver_log_path = "/tmp/ray.log"
     job_driver_handler = logging.FileHandler(job_driver_log_path)
-    job_driver_formatter = logging.Formatter(LOGGER_FORMAT)
+    job_driver_formatter = logging.Formatter(logger_format)
     job_driver_handler.setFormatter(job_driver_formatter)
     logger.addHandler(job_driver_handler)
 
