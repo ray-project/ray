2b1680a72ae10e9df9284f8b8f3b5a39a28f5f09
ujjawal-khare <ujjawal.khare@dream11.com>
2024-10-15 10:41:05 +0530

    unneccessary files removed
    Signed-off-by: ujjawal-khare <ujjawal.khare@dream11.com>

--- myenv/lib/python3.9/site-packages/isort/stdlibs/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import all as _all
-from . import py2, py3, py27, py35, py36, py37, py38, py39, py310
--- myenv/lib/python3.9/site-packages/isort/stdlibs/py3.py
+++
@@ -1,3 +0,0 @@
-from . import py35, py36, py37, py38, py39, py310
-
-stdlib = py35.stdlib | py36.stdlib | py37.stdlib | py38.stdlib | py39.stdlib | py310.stdlib
