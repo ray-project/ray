1ae6690c961d21a526c110f916d945bff5bc7eab
ujjawal-khare <ujjawal.khare@dream11.com>
2024-10-15 10:41:05 +0530

    unneccessary files removed
    Signed-off-by: ujjawal-khare <ujjawal.khare@dream11.com>

--- myenv/lib/python3.9/site-packages/isort/sections.py
+++
@@ -1,9 +0,0 @@
-"""Defines all sections isort uses by default"""
-from typing import Tuple
-
-FUTURE: str = "FUTURE"
-STDLIB: str = "STDLIB"
-THIRDPARTY: str = "THIRDPARTY"
-FIRSTPARTY: str = "FIRSTPARTY"
-LOCALFOLDER: str = "LOCALFOLDER"
-DEFAULT: Tuple[str, ...] = (FUTURE, STDLIB, THIRDPARTY, FIRSTPARTY, LOCALFOLDER)
