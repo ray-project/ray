2b18b33258861428a4bdf4291652aec7239f1c67
gorloffslava <gorloff.slava@gmail.com>
2024-10-17 18:17:13 +0500

    + IMPORTANT checkpoint

--- eugo_bootstrap.sh
+++ eugo_bootstrap.sh
@@ -2,15 +2,23 @@
 set -euo pipefail
 
 
+export LD_PRELOAD=""
+
+
 # MARK: - 1. NodeJS
 dnf install nodejs20
 
 update-alternatives --install /usr/bin/node node /usr/bin/node-20 2
-update-alternatives --install /usr/bin/npm npm /usr/bin/node-20 2
+update-alternatives --install /usr/bin/npm npm /usr/bin/npm-20 2
 
 
 # MARK: - 2. Older Cython (v0.x)
 export EUGO_CYTHON_OLD_PATH="${EUGO_STAGING_PATH_BASE}/_cython_old"
+
+eugo_install_package \
+  -m="eugo/cython_old/meta.json" \
+  -s="eugo/cython_old/setup" \
+  -i="${EUGO_CYTHON_OLD_PATH}"
 
 export PYTHONPATH="${EUGO_CYTHON_OLD_PATH}${PYTHONPATH:+:$PYTHONPATH}"
 export PATH="${EUGO_CYTHON_OLD_PATH}/bin:${PATH}"
