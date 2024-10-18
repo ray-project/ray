ccbf68bc2004db4cfac307e541f336bf786a07c8
Weixin Deng <weixin@cs.washington.edu>
2024-10-18 10:37:17 -0700

    refactor: Use separate types by if-else
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- python/ray/dag/collective_node.py
+++ python/ray/dag/collective_node.py
@@ -13,9 +13,11 @@ from ray.experimental.channel import ChannelContext
 from ray.experimental.channel.torch_tensor_nccl_channel import _init_nccl_group
 from ray.experimental.channel.torch_tensor_type import GPUCommunicator, TorchTensorType
 from ray.util.annotations import DeveloperAPI
-from ray.util.collective.types import _CollectiveOp, ReduceOp
 
-# from ray.experimental.util.types import _CollectiveOp, ReduceOp
+if False:
+    from ray.util.collective.types import _CollectiveOp, ReduceOp
+else:
+    from ray.experimental.util.types import _CollectiveOp, ReduceOp
 
 
 class _CollectiveOperation:
