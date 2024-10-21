27984dcb78ebaea8de39027c217e1b9160a2710e
Balaji Veeramani <bveeramani@berkeley.edu>
2024-10-21 10:58:06 -0700

    Fix typo
    Signed-off-by: Balaji Veeramani <bveeramani@berkeley.edu>

--- python/ray/data/_internal/logical/operators/count_operator.py
+++ python/ray/data/_internal/logical/operators/count_operator.py
@@ -6,9 +6,9 @@ from ray.data._internal.logical.interfaces import LogicalOperator
 class Count(LogicalOperator):
     """Logical operator that represents counting the number of rows in inputs.
 
-    Physical operators that implement this logical operator should produce one more rows
-    with a single column named by `Count.COLUMN_NAME`. When you sum the values in this
-    column, you should get the total number of rows in the dataset.
+    Physical operators that implement this logical operator should produce one or more
+    rows with a single column named `Count.COLUMN_NAME`. When you sum the values in
+    this column, you should get the total number of rows in the dataset.
     """
 
     COLUMN_NAME = "__num_rows"
