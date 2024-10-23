4eb8cc597711406a779cac1a4d4ac147703bde5d
Artur Niederfahrenhorst <attaismyname@googlemail.com>
2024-10-23 15:48:53 +0200

    fix doctests

--- python/ray/data/iterator.py
+++ python/ray/data/iterator.py
@@ -734,7 +734,7 @@ class DataIterator(abc.ABC):
             If your model accepts additional metadata aside from features and label, specify a single additional column or a list of additional columns.
             A common use case is to include sample weights in the data samples and train a ``tf.keras.Model`` with ``tf.keras.Model.fit``.
 
-            >>> ds = ds.add_column("sample weights", lambda df: 1)
+            >>> ds = ds.add_column("sample weights", lambda x: [1] * x.num_rows)
             >>> it = ds.iterator()
             >>> it.to_tf(feature_columns="sepal length (cm)", label_columns="target", additional_columns="sample weights")
             <_OptionsDataset element_spec=(TensorSpec(shape=(None,), dtype=tf.float64, name='sepal length (cm)'), TensorSpec(shape=(None,), dtype=tf.int64, name='target'), TensorSpec(shape=(None,), dtype=tf.int64, name='sample weights'))>
