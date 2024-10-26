9cfb1a79e8a2e4f1d22afbf8b28c8b3b848461dd
Edward Oakes <ed.nmi.oakes@gmail.com>
2024-10-26 18:32:07 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/tests/test_deploy_2.py
+++ python/ray/serve/tests/test_deploy_2.py
@@ -11,7 +11,6 @@ import requests
 
 import ray
 from ray import serve
-from ray._private.pydantic_compat import ValidationError
 from ray._private.test_utils import SignalActor, wait_for_condition
 from ray.serve._private.common import DeploymentStatus
 from ray.serve._private.logging_utils import get_serve_logs_dir
