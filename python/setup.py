d7f7e0f58248b9de145949883989ab597f97a2da
Scott Lee <sjl@anyscale.com>
2024-10-29 16:34:13 -0700

    update setup.py
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- python/setup.py
+++ python/setup.py
@@ -227,7 +227,9 @@ ray_files += [
 if setup_spec.type == SetupType.RAY:
     pandas_dep = "pandas >= 1.3"
     numpy_dep = "numpy >= 1.20"
-    pyarrow_dep = "pyarrow >= 6.0.1"
+    pyarrow_dep = (
+        "pyarrow >= 6.0.1,<18" if sys.platform == "darwin" else "pyarrow >= 6.0.1"
+    )
     setup_spec.extras = {
         "adag": [
             "cupy-cuda12x; sys_platform != 'darwin'",
