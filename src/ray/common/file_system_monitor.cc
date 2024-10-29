867cae14c8f5afedc794504bb0dd4c17ab742627
zhilong <121425509+Bye-legumes@users.noreply.github.com>
2024-10-29 13:30:23 -0400

    Update src/ray/common/file_system_monitor.cc
    Co-authored-by: Jiajun Yao <jeromeyjj@gmail.com>
    Signed-off-by: zhilong <121425509+Bye-legumes@users.noreply.github.com>

--- src/ray/common/file_system_monitor.cc
+++ src/ray/common/file_system_monitor.cc
@@ -103,7 +103,7 @@ bool FileSystemMonitor::OverCapacityImpl(
   }
 
   // Convert bytes to GB
-  double available_gb = static_cast<double>(space_info->available) / (1024 * 1024 * 1024);
+  const double available_gb = static_cast<double>(space_info->available) / (1024 * 1024 * 1024);
   double capacity_gb = static_cast<double>(space_info->capacity) / (1024 * 1024 * 1024);
 
   std::ostringstream ostr;
