500b3b6155cd9a1d09930cb801701c36688dec8b
Ruiyang Wang <56065503+rynewang@users.noreply.github.com>
2024-10-30 10:48:13 -0700

    [core] Posts CoreWorkerMemoryStore callbacks onto io_context. (#47833)
    CoreWorkerMemoryStore has GetAsync method which calls callbacks when the
    object is ready, immediately or on a Put() call. However despite the
    name, the call is not `Async`: it just calls on the GetAsync() or Put()
    call stack. If a mutex is held by any call frame in the callstack, it
    remains held in the callback, ripe for deadlocks.
    
    Adds another ctor argument io_context and posts any callbacks to the
    context to avoid those deadlocks. Note many cpp tests creates
    CoreWorkerMemoryStore without a io_context so I kept the option of
    creating an owned io_context with thread.
    
    Signed-off-by: Ruiyang Wang <rywang014@gmail.com>
    Signed-off-by: Ruiyang Wang <56065503+rynewang@users.noreply.github.com>
    Co-authored-by: Jiajun Yao <jeromeyjj@gmail.com>

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -370,7 +370,8 @@ CoreWorker::CoreWorker(const CoreWorkerOptions &options, const WorkerID &worker_
       (options_.worker_type != WorkerType::SPILL_WORKER &&
        options_.worker_type != WorkerType::RESTORE_WORKER),
       /*get_current_call_site=*/boost::bind(&CoreWorker::CurrentCallSite, this)));
-  memory_store_.reset(new CoreWorkerMemoryStore(
+  memory_store_ = std::make_shared<CoreWorkerMemoryStore>(
+      io_service_,
       reference_counter_,
       local_raylet_client_,
       options_.check_signals,
@@ -391,7 +392,7 @@ CoreWorker::CoreWorker(const CoreWorkerOptions &options, const WorkerID &worker_
               }
             },
             "CoreWorker.HandleException");
-      }));
+      });
 
 #if defined(__APPLE__) || defined(__linux__)
   // TODO(jhumphri): Combine with implementation in NodeManager.
@@ -4479,29 +4480,17 @@ void CoreWorker::GetAsync(const ObjectID &object_id,
 
   memory_store_->GetAsync(
       object_id,
-      [this,
-       object_id,
+      // This callback is posted to io_service_ by memory store.
+      [object_id,
        python_user_callback,
        success_callback = std::move(success_callback),
        fallback_callback =
            std::move(fallback_callback)](std::shared_ptr<RayObject> ray_object) {
-        // Post the callback to the io_service_ to avoid deadlocks.
-        // The user callback can make arbitrary Ray API calls and will be called
-        // immediately when the object is `Put` into the in-memory store. This can
-        // cause deadlocks if the callers of `Put` is holding a lock.
-        io_service_.post(
-            [object_id,
-             python_user_callback,
-             success_callback = std::move(success_callback),
-             fallback_callback = std::move(fallback_callback),
-             ray_object = std::move(ray_object)]() {
-              if (ray_object->IsInPlasmaError()) {
-                fallback_callback(ray_object, object_id, python_user_callback);
-              } else {
-                success_callback(ray_object, object_id, python_user_callback);
-              }
-            },
-            "CoreWorker.GetAsync.Callback");
+        if (ray_object->IsInPlasmaError()) {
+          fallback_callback(ray_object, object_id, python_user_callback);
+        } else {
+          success_callback(ray_object, object_id, python_user_callback);
+        }
       });
 }
 
