0c58e8732b6c6e59b0614dcac9d0199962bad760
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2024-10-29 19:46:14 -0700

    Revert "Revert "Revert "[RLlib] Upgrade to gymnasium 1.0.0 (ale_py 0.10.1, mujoco 3.2.4, pettingzoo 1.24.3 supersuit 3.9.3).""" (#48435)
    Reverts ray-project/ray#48308

--- release/long_running_tests/workloads/apex.py
+++ release/long_running_tests/workloads/apex.py
@@ -39,7 +39,7 @@ run_experiments(
     {
         "apex": {
             "run": "APEX",
-            "env": "ale_py:ALE/Pong-v5",
+            "env": "ALE/Pong-v5",
             "config": {
                 "num_workers": 3,
                 "num_gpus": 0,
