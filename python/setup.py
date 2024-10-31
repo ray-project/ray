d55fa8f3263a8c77edcb2299096f4a22bbebb954
Scott Lee <sjl@anyscale.com>
2024-10-31 10:04:03 -0700

    limit to x86
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- python/setup.py
+++ python/setup.py
@@ -228,8 +228,8 @@ if setup_spec.type == SetupType.RAY:
     pandas_dep = "pandas >= 1.3"
     numpy_dep = "numpy >= 1.20"
     pyarrow_deps = [
-        "pyarrow >= 6.0.1,<18; sys_platform == 'darwin'",
-        "pyarrow >= 6.0.1; sys_platform != 'darwin'",
+        "pyarrow >= 6.0.1",
+        "pyarrow <18; sys_platform == 'darwin' and platform_machine == 'x86_64'",
     ]
     setup_spec.extras = {
         "adag": [
