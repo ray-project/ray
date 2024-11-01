199b5826fc8222ba37f235d9dbcb3ab52cef25c7
Edward Oakes <ed.nmi.oakes@gmail.com>
2024-11-01 13:58:42 -0500

    [serve] Return "Internal Server Error" instead of traceback on user exception (#48491)
    In the non-fastapi HTTP handling codepath, we currently format the
    internal stack trace and return it in the response. This is unexpected
    behavior compared to other HTTP servers because it leaks internal
    implementation details.
    
    This change matches the FastAPI behavior: returning a `500` status with
    "Internal Server Error."
    
    ---------
    
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/tests/unit/test_user_callable_wrapper.py
+++ python/ray/serve/tests/unit/test_user_callable_wrapper.py
@@ -12,7 +12,6 @@ from starlette.requests import Request
 from starlette.responses import PlainTextResponse
 
 from ray import serve
-from ray.exceptions import RayTaskError
 from ray.serve._private.common import (
     DeploymentID,
     RequestMetadata,
@@ -152,7 +151,7 @@ def test_basic_class_callable():
 
     # Call non-generator method with is_streaming.
     request_metadata = _make_request_metadata(is_streaming=True)
-    with pytest.raises(RayTaskError, match="did not return a generator."):
+    with pytest.raises(TypeError, match="did not return a generator."):
         user_callable_wrapper.call_user_method(
             request_metadata, tuple(), dict()
         ).result()
@@ -176,7 +175,7 @@ def test_basic_class_callable():
         ).result()
         == "hi-kwarg"
     )
-    with pytest.raises(RayTaskError, match="uh-oh"):
+    with pytest.raises(RuntimeError, match="uh-oh"):
         user_callable_wrapper.call_user_method(
             request_metadata, tuple(), {"raise_exception": True}
         ).result()
@@ -185,7 +184,7 @@ def test_basic_class_callable():
     request_metadata = _make_request_metadata(
         call_method="call_async", is_streaming=True
     )
-    with pytest.raises(RayTaskError, match="did not return a generator."):
+    with pytest.raises(TypeError, match="did not return a generator."):
         user_callable_wrapper.call_user_method(
             request_metadata, tuple(), dict()
         ).result()
@@ -210,7 +209,7 @@ def test_basic_class_callable():
         ).result()
         == "hi-kwarg"
     )
-    with pytest.raises(RayTaskError, match="uh-oh"):
+    with pytest.raises(RuntimeError, match="uh-oh"):
         user_callable_wrapper.call_user_method(
             request_metadata, tuple(), {"raise_exception": True}
         ).result()
@@ -230,7 +229,7 @@ def test_basic_class_callable_generators():
         call_method="call_generator", is_streaming=False
     )
     with pytest.raises(
-        RayTaskError, match="Method 'call_generator' returned a generator."
+        TypeError, match="Method 'call_generator' returned a generator."
     ):
         user_callable_wrapper.call_user_method(
             request_metadata, (10,), dict(), generator_result_callback=append_to_list
@@ -247,7 +246,7 @@ def test_basic_class_callable_generators():
     result_list.clear()
 
     # Call sync generator raising exception.
-    with pytest.raises(RayTaskError, match="uh-oh"):
+    with pytest.raises(RuntimeError, match="uh-oh"):
         user_callable_wrapper.call_user_method(
             request_metadata,
             (10,),
@@ -262,7 +261,7 @@ def test_basic_class_callable_generators():
         call_method="call_async_generator", is_streaming=False
     )
     with pytest.raises(
-        RayTaskError, match="Method 'call_async_generator' returned a generator."
+        TypeError, match="Method 'call_async_generator' returned a generator."
     ):
         user_callable_wrapper.call_user_method(
             request_metadata, (10,), dict(), generator_result_callback=append_to_list
@@ -279,7 +278,7 @@ def test_basic_class_callable_generators():
     result_list.clear()
 
     # Call async generator raising exception.
-    with pytest.raises(RayTaskError, match="uh-oh"):
+    with pytest.raises(RuntimeError, match="uh-oh"):
         user_callable_wrapper.call_user_method(
             request_metadata,
             (10,),
@@ -296,7 +295,7 @@ def test_basic_function_callable(fn: Callable):
 
     # Call non-generator function with is_streaming.
     request_metadata = _make_request_metadata(is_streaming=True)
-    with pytest.raises(RayTaskError, match="did not return a generator."):
+    with pytest.raises(TypeError, match="did not return a generator."):
         user_callable_wrapper.call_user_method(
             request_metadata, tuple(), dict()
         ).result()
@@ -317,7 +316,7 @@ def test_basic_function_callable(fn: Callable):
             request_metadata, tuple(), {"suffix": "-kwarg"}
         ).result()
     ) == "hi-kwarg"
-    with pytest.raises(RayTaskError, match="uh-oh"):
+    with pytest.raises(RuntimeError, match="uh-oh"):
         user_callable_wrapper.call_user_method(
             request_metadata, tuple(), {"raise_exception": True}
         ).result()
@@ -336,7 +335,7 @@ def test_basic_function_callable_generators(fn: Callable):
     # Call generator function without is_streaming.
     request_metadata = _make_request_metadata(is_streaming=False)
     with pytest.raises(
-        RayTaskError, match=f"Method '{fn.__name__}' returned a generator."
+        TypeError, match=f"Method '{fn.__name__}' returned a generator."
     ):
         user_callable_wrapper.call_user_method(
             request_metadata, (10,), dict(), generator_result_callback=append_to_list
@@ -353,7 +352,7 @@ def test_basic_function_callable_generators(fn: Callable):
     result_list.clear()
 
     # Call generator function raising exception.
-    with pytest.raises(RayTaskError, match="uh-oh"):
+    with pytest.raises(RuntimeError, match="uh-oh"):
         user_callable_wrapper.call_user_method(
             request_metadata,
             (10,),
