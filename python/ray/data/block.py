2e58973f897a5ec6e825a3480c137524f5c7b3ff
Kit Lee <7000003+wingkitlee0@users.noreply.github.com>
2024-11-01 20:54:38 -0400

    [Data] Optimizing the multi column groupby
    Changed from a custom class implementation to a purely numpy implementation
    
    Signed-off-by: Kit Lee <7000003+wingkitlee0@users.noreply.github.com>

--- python/ray/data/block.py
+++ python/ray/data/block.py
@@ -243,8 +243,8 @@ class BlockAccessor:
         """Return a slice of this block.
 
         Args:
-            start: The starting index of the slice.
-            end: The ending index of the slice.
+            start: The starting index of the slice (inclusive).
+            end: The ending index of the slice (exclusive).
             copy: Whether to perform a data copy for the slice.
 
         Returns:
@@ -458,3 +458,25 @@ class BlockAccessor:
     def block_type(self) -> BlockType:
         """Return the block type of this block."""
         raise NotImplementedError
+
+
+def _get_block_boundaries(
+    block: Union[np.ndarray, Dict[str, np.ndarray]]
+) -> np.ndarray:
+    """Compute block boundaries based on the key(s), that is, a list of
+    starting indices of each group and an end index of the last group.
+
+    Args:
+        block: numpy arrays of the group key(s). This is generally coming from
+            ``BlockAccessor.to_numpy()``.
+
+    Returns:
+        A list of starting indices of each group. The first entry is 0 and
+        the last entry is ``len(array)``.
+    """
+    if isinstance(block, dict):
+        # For multiple keys, we create a numpy record array
+        dtype = [(k, v.dtype) for k, v in block.items()]
+        block = np.array(list(zip(*block.values())), dtype=dtype)
+
+    return np.hstack([[0], np.where(block[1:] != block[:-1])[0] + 1, [len(block)]])
