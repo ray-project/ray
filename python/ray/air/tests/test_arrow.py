6f612fe658f6a6d2b140708e0b77c0054403dbf1
Alexey Kudinkin <ak@anyscale.com>
2024-11-06 13:14:31 -0800

    Added an explicit test for Arrow Object extension type fallback
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/air/tests/test_arrow.py
+++ python/ray/air/tests/test_arrow.py
@@ -1,18 +1,40 @@
-import pyarrow as pa
+from dataclasses import dataclass, field
 
-from ray.air.util.tensor_extensions.arrow import _infer_pyarrow_type
+import pyarrow as pa
+import pytest
+
+from ray.air.util.tensor_extensions.arrow import _infer_pyarrow_type, _convert_to_pyarrow_native_array, \
+    ArrowConversionError, convert_to_pyarrow_array
 from ray.air.util.tensor_extensions.utils import create_ragged_ndarray
 
 
-def test_pa_infer_type():
-    class UserObj:
-        pass
+@dataclass
+class UserObj:
+    i: int = field()
 
+
+def test_pa_infer_type_failing_to_infer():
     # Represent a single column that will be using `ArrowPythonObjectExtension` type
     # to ser/de native Python objects into bytes
-    column_vals = create_ragged_ndarray(["hi", 1, None, [[[[]]]], {"a": [[{"b": 2, "c": UserObj()}]]}, UserObj()])
+    column_vals = create_ragged_ndarray(["hi", 1, None, [[[[]]]], {"a": [[{"b": 2, "c": UserObj(i=123)}]]}, UserObj(i=456)])
 
     inferred_dtype = _infer_pyarrow_type(column_vals)
 
     # Arrow (17.0) seem to fallback to assume the dtype of the first element
     assert pa.string().equals(inferred_dtype)
+
+
+def test_convert_to_pyarrow_array_object_ext_type_fallback():
+    column_values = create_ragged_ndarray(["hi", 1, None, [[[[]]]], {"a": [[{"b": 2, "c": UserObj(i=123)}]]}, UserObj(i=456)])
+    column_name = "py_object_column"
+
+    # First, assert that straightforward conversion into Arrow native types fails
+    with pytest.raises(ArrowConversionError) as exc_info:
+        _convert_to_pyarrow_native_array(column_values, column_name)
+
+    assert str(exc_info.value) == "Error converting data to Arrow: ['hi' 1 None list([[[[]]]]) {'a': [[{'b': 2, 'c': UserObj(i=123)}]]}\n UserObj(i=456)]"
+
+    # Subsequently, assert that fallback to `ArrowObjectExtensionType` succeeds
+    pa_array = convert_to_pyarrow_array(column_values, column_name)
+
+    assert pa_array.to_pylist() == column_values.tolist()
