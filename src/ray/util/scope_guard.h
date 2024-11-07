e49ca35a0da60d1887cdc38d6cc0d00bd49e17f8
dentiny <dentinyhao@gmail.com>
2024-11-07 08:00:39 +0000

    copyright
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- src/ray/util/scope_guard.h
+++ src/ray/util/scope_guard.h
@@ -1,3 +1,17 @@
+// Copyright 2024 The Ray Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // SCOPE_EXIT is used to execute a series of registered functions when it goes
 // out of scope.
 // For details, refer to Andrei Alexandrescu's CppCon 2015 talk "Declarative
