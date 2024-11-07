e43226894a7d304cbb9f8191c7d73e9b14d31d37
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2024-11-07 05:31:07 -0800

    [ci] fix osx wheel matching logic (#48615)
    and remove linux wheel testing logic. linux is not using this script
    anymore.
    
    Signed-off-by: Lonnie Liu <lonnie@anyscale.com>

--- ci/build/test-wheels.sh
+++ ci/build/test-wheels.sh
@@ -25,7 +25,6 @@ if [ -n "${BUILDKITE-}" ]; then
 fi
 TEST_DIR="${BUILD_DIR}/python/ray/tests"
 TEST_SCRIPTS=("$TEST_DIR/test_microbenchmarks.py" "$TEST_DIR/test_basic.py")
-DASHBOARD_TEST_SCRIPT="${BUILD_DIR}/python/ray/tests/test_dashboard.py"
 
 
 function retry {
@@ -46,51 +45,7 @@ function retry {
   done
 }
 
-if [[ "$platform" == "linux" ]]; then
-  # Install miniconda.
-  PY_WHEEL_VERSIONS=("39")
-  PY_MMS=("3.9.5")
-  wget --quiet "https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh" -O miniconda3.sh
-  "${ROOT_DIR}"/../suppress_output bash miniconda3.sh -b -p "$HOME/miniconda3"
-  export PATH="$HOME/miniconda3/bin:$PATH"
-
-  for ((i=0; i<${#PY_MMS[@]}; ++i)); do
-    PY_MM="${PY_MMS[i]}"
-    PY_WHEEL_VERSION="${PY_WHEEL_VERSIONS[i]}"
-
-    conda install -y python="${PY_MM}"
-
-    PYTHON_EXE="$HOME/miniconda3/bin/python"
-    PIP_CMD="$HOME/miniconda3/bin/pip"
-
-    # Find the right wheel by grepping for the Python version.
-    PYTHON_WHEEL="$(printf "%s\n" "$ROOT_DIR"/../../.whl/*"$PY_WHEEL_VERSION"* | head -n 1)"
-
-    # Install the wheel.
-    "$PIP_CMD" install -q "$PYTHON_WHEEL"
-
-    # Check that ray.__commit__ was set properly.
-    "$PYTHON_EXE" -u -c "import ray; print(ray.__commit__)" | grep "$TRAVIS_COMMIT" || (echo "ray.__commit__ not set properly!" && exit 1)
-
-    # Install the dependencies to run the tests.
-    "$PIP_CMD" install -q aiohttp aiosignal frozenlist grpcio 'pytest==7.0.1' requests proxy.py
-
-    # Run a simple test script to make sure that the wheel works.
-    for SCRIPT in "${TEST_SCRIPTS[@]}"; do
-        retry "$PYTHON_EXE" "$SCRIPT"
-    done
-    retry "$PYTHON_EXE" "$DASHBOARD_TEST_SCRIPT"
-  done
-
-  # Check that the other wheels are present.
-  NUMBER_OF_WHEELS="$(find "$ROOT_DIR"/../../.whl/ -mindepth 1 -maxdepth 1 -name "*.whl" | wc -l)"
-  if [[ "$NUMBER_OF_WHEELS" != "5" ]]; then
-    echo "Wrong number of wheels found."
-    ls -l "$ROOT_DIR/../.whl/"
-    exit 2
-  fi
-
-elif [[ "$platform" == "macosx" ]]; then
+if [[ "$platform" == "macosx" ]]; then
   MACPYTHON_PY_PREFIX=/Library/Frameworks/Python.framework/Versions
 
   PY_WHEEL_VERSIONS=("39" "310")
@@ -125,7 +80,7 @@ elif [[ "$platform" == "macosx" ]]; then
     fi
 
     # Find the appropriate wheel by grepping for the Python version.
-    PYTHON_WHEEL="$(printf "%s\n" "$ROOT_DIR"/../../.whl/*"$PY_WHEEL_VERSION"* | head -n 1)"
+    PYTHON_WHEEL="$(printf "%s\n" "$ROOT_DIR"/../../.whl/*"cp$PY_WHEEL_VERSION-cp$PY_WHEEL_VERSION"* | head -n 1)"
 
     # Print some env info
     "$PYTHON_EXE" --version
