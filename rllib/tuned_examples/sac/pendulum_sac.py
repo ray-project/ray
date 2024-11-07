359f40ae81e34e3094a036d1bf6e0b482bf85e92
Sven Mika <svenmika1977@gmail.com>
2024-11-07 13:59:40 +0100

    [RLlib] Fix IMPALA/APPO learning behavior: Fix EnvRunner sync bug, GPU loader thread, enable local learner w/ GPU. (#48314)

--- rllib/tuned_examples/sac/pendulum_sac.py
+++ rllib/tuned_examples/sac/pendulum_sac.py
@@ -19,9 +19,9 @@ config = (
     .training(
         initial_alpha=1.001,
         # Use a smaller learning rate for the policy.
-        actor_lr=2e-4 * (args.num_gpus or 1) ** 0.5,
-        critic_lr=8e-4 * (args.num_gpus or 1) ** 0.5,
-        alpha_lr=9e-4 * (args.num_gpus or 1) ** 0.5,
+        actor_lr=2e-4 * (args.num_learners or 1) ** 0.5,
+        critic_lr=8e-4 * (args.num_learners or 1) ** 0.5,
+        alpha_lr=9e-4 * (args.num_learners or 1) ** 0.5,
         lr=None,
         target_entropy="auto",
         n_step=(2, 5),
@@ -34,7 +34,7 @@ config = (
             "alpha": 1.0,
             "beta": 0.0,
         },
-        num_steps_sampled_before_learning_starts=256 * (args.num_gpus or 1),
+        num_steps_sampled_before_learning_starts=256 * (args.num_learners or 1),
     )
     .rl_module(
         model_config=DefaultModelConfig(
