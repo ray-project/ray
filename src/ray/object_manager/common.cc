0ab63fad9267da5dc5bc0bb45d629a2f6b406538
Slava Gorlov <31761951+gorloffslava@users.noreply.github.com>
2024-11-14 21:40:02 +0500

    Fixed linter error and synced with upstream
    Signed-off-by: Slava Gorlov <31761951+gorloffslava@users.noreply.github.com>

--- src/ray/object_manager/common.cc
+++ src/ray/object_manager/common.cc
@@ -15,8 +15,8 @@
 #include "ray/object_manager/common.h"
 
 #include "absl/functional/bind_front.h"
-#include "absl/strings/str_format.h"
 #include "absl/strings/str_cat.h"
+#include "absl/strings/str_format.h"
 
 namespace ray {
 
