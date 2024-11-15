fbe0ebfa97b94de2e7af327036f8800f5b16b349
Scott Lee <scottjlee@users.noreply.github.com>
2024-11-15 14:06:02 -0800

    [Data] [Dashboard] Fix `Rows Outputted` being zero on Ray Data Dashboard (#48745)
    ## Why are these changes needed?
    
    Currently, there are some cases where the `Rows Outputted` value on the
    Ray Job page's `Ray Data Overview` section says "0", even after the
    dataset execution completes. The root cause of the bug is that we clear
    iteration/execution metrics after the dataset completes. This was
    previously used to "reset" the metrics to 0 after dataset completion, so
    that the last emitted value would not persist on the dashboard, even
    after the job finishes. Now that we display rates on the dashboard, this
    hack is no longer needed, and we can skip the metrics clearing.
    
    Fixed result:
    <img width="1860" alt="Screenshot at Nov 14 12-11-24"
    src="https://github.com/user-attachments/assets/35061b3f-9359-412b-8ab2-f4bcce412994">
    
    ## Related issue number
    
    Closes https://github.com/ray-project/ray/issues/44635
    
    ## Checks
    
    - [x] I've signed off every commit(by using the -s flag, i.e., `git
    commit -s`) in this PR.
    - [x] I've run `scripts/format.sh` to lint the changes in this PR.
    - [ ] I've included any doc changes needed for
    https://docs.ray.io/en/master/.
    - [ ] I've added any new APIs to the API Reference. For example, if I
    added a
    method in Tune, I've added it in `doc/source/tune/api/` under the
               corresponding `.rst` file.
    - [x] I've made sure the tests are passing. Note that there might be a
    few flaky tests, see the recent failures at https://flakey-tests.ray.io/
    - Testing Strategy
       - [x] Unit tests
       - [ ] Release tests
       - [ ] This PR is not tested :(
    
    ---------
    
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- python/ray/data/tests/test_stats.py
+++ python/ray/data/tests/test_stats.py
@@ -1663,8 +1663,9 @@ def test_stats_manager(shutdown_only):
     datasets = [None] * num_threads
     # Mock clear methods so that _last_execution_stats and _last_iteration_stats
     # are not cleared. We will assert on them afterwards.
-    with patch.object(StatsManager, "clear_execution_metrics"), patch.object(
-        StatsManager, "clear_iteration_metrics"
+    with (
+        patch.object(StatsManager, "clear_last_execution_stats"),
+        patch.object(StatsManager, "clear_iteration_metrics"),
     ):
 
         def update_stats_manager(i):
@@ -1689,9 +1690,7 @@ def test_stats_manager(shutdown_only):
         dataset_tag = create_dataset_tag(dataset._name, dataset._uuid)
         assert dataset_tag in StatsManager._last_execution_stats
         assert dataset_tag in StatsManager._last_iteration_stats
-        StatsManager.clear_execution_metrics(
-            dataset_tag, ["Input0", "ReadRange->MapBatches(<lambda>)1"]
-        )
+        StatsManager.clear_last_execution_stats(dataset_tag)
         StatsManager.clear_iteration_metrics(dataset_tag)
 
     wait_for_condition(lambda: not StatsManager._update_thread.is_alive())
