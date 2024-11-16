552e63a46188d48ac4962690bffa0a7c32062017
Alexey Kudinkin <ak@anyscale.com>
2024-11-15 23:53:53 -0800

    Merged test files
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/data/tests/test_arrow_block.py
+++ python/ray/data/tests/test_arrow_block.py
@@ -1,13 +1,97 @@
+import os
 import types
+from tempfile import TemporaryDirectory
 
 import numpy as np
 import pyarrow as pa
 import pytest
-
 import ray
+import sys
+
+from pyarrow import parquet as pq
 from ray._private.test_utils import run_string_as_driver
+from ray.data import DataContext
 from ray.data._internal.arrow_block import ArrowBlockAccessor
+from ray.data._internal.util import MiB, GiB
 from ray.data.extensions.object_extension import _object_extension_type_allowed
+
+
+@pytest.fixture(scope="module")
+def parquet_dataset_single_column_gt_2gb():
+    chunk_size = 256 * MiB
+    num_chunks = 10
+
+    total_column_size = chunk_size * 10 # ~2.5 GiB
+
+    with TemporaryDirectory() as tmp_dir:
+        dataset_path = f"{tmp_dir}/large_parquet_chunk_{chunk_size}"
+
+        # Create directory
+        os.mkdir(dataset_path)
+
+        for i in range(num_chunks):
+            chunk = b"a" * chunk_size
+
+            d = {"id": [i], "bin": [chunk]}
+            t = pa.Table.from_pydict(d)
+
+            print(f">>> Table schema: {t.schema} (size={sys.getsizeof(t)})")
+
+            filepath = f"{dataset_path}/chunk_{i}.parquet"
+            pq.write_table(t, filepath)
+
+            print(f">>> Created a chunk #{i}")
+
+        print(f">>> Created dataset at {dataset_path}")
+
+        yield dataset_path, num_chunks, total_column_size
+
+        print(f">>> Cleaning up dataset at {dataset_path}")
+
+
+@pytest.mark.parametrize(
+    "op", ["map", "map_batches"]
+)
+def test_arrow_batch_gt_2gb(parquet_dataset_single_column_gt_2gb, restore_data_context, op):
+    # Disable (automatic) fallback to `ArrowPythonObjectType` extension type
+    DataContext.get_current().enable_fallback_to_arrow_object_ext_type = False
+
+    dataset_path, num_rows, total_column_size = parquet_dataset_single_column_gt_2gb
+
+    def _id(x):
+        print(f">>> [DBG] _id: {len(str(x)) / GiB:.2f} GiB, {str(x)[:128]}")
+        return x
+
+    ds = ray.data.read_parquet(dataset_path).materialize()
+
+    if op == "map":
+        ds = ds.map(_id)
+    elif op == "map_batches":
+        # Combine all rows into a single batch using `map_batches` coercing to
+        # numpy format
+        ds = ds.map_batches(
+            _id,
+            batch_format="numpy",
+            batch_size=num_rows,
+            zero_copy_batch=False,
+        )
+
+    batch = ds.take_batch()
+
+    total_binary_column_size = sum([len(b) for b in batch['bin']])
+
+    print(
+        f">>> Batch:\n"
+        f"------\n"
+        "Column: 'id'"
+        f"Values: {batch['id']}\n"
+        f"------\n"
+        "Column: 'bin'"
+        f"Total: {total_binary_column_size / GiB} GiB\n"
+        f"Values: {[str(v)[:3] + ' x ' + str(len(v)) for v in batch['bin']]}\n"
+    )
+
+    assert total_binary_column_size == total_column_size
 
 
 def test_append_column(ray_start_regular_shared):
