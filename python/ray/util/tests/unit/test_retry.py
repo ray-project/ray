06c66d98b87012d45600a7b10aab6d5a0709869b
dentiny <dentinyhao@gmail.com>
2024-11-17 01:16:22 +0000

    add test on retry failure
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- python/ray/util/tests/unit/test_retry.py
+++ python/ray/util/tests/unit/test_retry.py
@@ -37,5 +37,21 @@ def test_retry_with_limited_error():
     assert invocation_count == 2
 
 
+# Test senario: exception count exceeds retry count.
+def test_retry_with_unlimited_error():
+    invocation_count = 0
+
+    @retry.retry(init_delay_sec=1, jitter_sec=1, backoff=1, max_retry_count=3)
+    def unlimited_error() -> int:
+        nonlocal invocation_count
+
+        invocation_count += 1
+        raise Exception("Manual exception")
+
+    with pytest.raises(Exception, match="Manual exception"):
+        unlimited_error()
+    assert invocation_count == 3
+
+
 if __name__ == "__main__":
     sys.exit(pytest.main(["-sv", __file__]))
