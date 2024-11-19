89ff58462cbf50c1fea1195f826cf2f8563a82ca
Alexey Kudinkin <ak@anyscale.com>
2024-11-18 16:52:38 -0800

    Added missing `pa.Table` to `DataBatchType` def
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/air/data_batch_type.py
+++ python/ray/air/data_batch_type.py
@@ -3,5 +3,7 @@ from typing import TYPE_CHECKING, Dict, Union
 if TYPE_CHECKING:
     import numpy
     import pandas
+    import pyarrow
 
-DataBatchType = Union["numpy.ndarray", "pandas.DataFrame", Dict[str, "numpy.ndarray"]]
+# TODO de-dup with ray.data.block.DataBatch
+DataBatchType = Union["numpy.ndarray", "pyarrow.Table" "pandas.DataFrame", Dict[str, "numpy.ndarray"]]
