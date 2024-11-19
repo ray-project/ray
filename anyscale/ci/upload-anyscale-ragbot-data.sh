8d617c6684c6d54bdc0915e4f75125d946d88229
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2024-11-18 16:19:05 -0800

    [ragbot] upload doc source without path (#1221)
    Currently when we upload the doc build artifacts, we also upload its
    absolute path `ray/doc/...`. This PR changes to upload only the html
    content. This simplifies the data post-processing on the ragbot training
    part.
    
    Test:
    
    
    ![](https://media3.giphy.com/media/TejmLnMKgnmPInMQjV/200.gif?cid=5a38a5a2nx7g7zvvv04n1wbhhajsxhc1t4dk3wm7wueteyi1&ep=v1_gifs_search&rid=200.gif&ct=g)
    
    Signed-off-by: can <can@anyscale.com>

--- anyscale/ci/upload-anyscale-ragbot-data.sh
+++ anyscale/ci/upload-anyscale-ragbot-data.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 bazel run //ci/ray_ci/doc:cmd_build
-tar -cvzf /tmp/"${BUILDKITE_COMMIT}".tgz doc/_build/html
+tar -cvzf /tmp/"${BUILDKITE_COMMIT}".tgz -C doc/_build/html .
 pip install awscli==1.35.21
 aws s3 cp /tmp/"${BUILDKITE_COMMIT}".tgz s3://anyscale-rag-bot/ray_doc/"${BUILDKITE_COMMIT}".tgz
