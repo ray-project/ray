5b26e2de44f141ddd6d78d1ec33f7a1dcff39bab
Alexey Kudinkin <ak@anyscale.com>
2024-11-20 12:31:24 -0800

    Cleaned up & streamlined boundary sampling sequence to avoid conversion from Numpy to Python objects
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/data/_internal/planner/exchange/sort_task_spec.py
+++ python/ray/data/_internal/planner/exchange/sort_task_spec.py
@@ -7,7 +7,6 @@ from ray.data._internal.planner.exchange.interfaces import ExchangeTaskSpec
 from ray.data._internal.progress_bar import ProgressBar
 from ray.data._internal.remote_fn import cached_remote_fn
 from ray.data._internal.table_block import TableBlockAccessor
-from ray.data._internal.util import NULL_SENTINEL
 from ray.data.block import Block, BlockAccessor, BlockExecStats, BlockMetadata
 from ray.types import ObjectRef
 
@@ -195,32 +194,18 @@ class SortTaskSpec(ExchangeTaskSpec):
             builder.add_block(sample)
         samples_table = builder.build()
         samples_dict = BlockAccessor.for_block(samples_table).to_numpy(columns=columns)
-        # This zip does the transposition from list of column values to list of tuples.
-        samples_list = list(zip(*samples_dict.values()))
-
-        def is_na(x):
-            # Check if x is None or NaN. Type casting to np.array first to avoid
-            # isnan failing on strings and other types.
-            if x is None:
-                return True
-            x = np.asarray(x)
-            if np.issubdtype(x.dtype, np.number):
-                return np.isnan(x)
-            return False
-
-        def key_fn_with_nones(sample):
-            return tuple(NULL_SENTINEL if is_na(x) else x for x in sample)
-
-        # Sort the list, but Nones should be NULL_SENTINEL to ensure safe sorting.
-        samples_list = sorted(samples_list, key=key_fn_with_nones)
+        # Stack columns to transpose from list of columns to an array of row-like arrays
+        sampled_rows_array = np.column_stack(list(samples_dict.values()))
+        # Sort rows
+        sampled_rows_array.sort(axis=0)
 
         # Each boundary corresponds to a quantile of the data.
         quantile_indices = [
-            int(q * (len(samples_list) - 1))
+            int(q * (len(sampled_rows_array) - 1))
             for q in np.linspace(0, 1, num_reducers + 1)
         ]
         # Exclude the first and last quantiles because they're 0 and 1.
-        return [samples_list[i] for i in quantile_indices[1:-1]]
+        return [sampled_rows_array[i] for i in quantile_indices[1:-1]]
 
 
 def _sample_block(block: Block, n_samples: int, sort_key: SortKey) -> Block:
