no longer depend on mock nccl, test on wrong shape not passing

python/ray/dag/collective_node.py
@@ -10,8 +10,12 @@ from ray.dag import (
 )
 from ray.dag.constants import COLLECTIVE_OPERATION_KEY
 from ray.experimental.channel import ChannelContext
-from ray.experimental.channel.torch_tensor_nccl_channel import _init_nccl_group
+from ray.experimental.channel.torch_tensor_nccl_channel import (
+    _init_nccl_group,
+    _init_cpu_group,
+)
 from ray.experimental.channel.torch_tensor_type import GPUCommunicator, TorchTensorType
+from ray.experimental.channel.cpu_communicator import CPUCommunicator
 from ray.experimental.util.types import _CollectiveOp, ReduceOp
 from ray.util.annotations import DeveloperAPI
 
@@ -71,6 +75,9 @@ class _CollectiveOperation:
                 raise ValueError(
                     "Expected actor handles to match the custom NCCL group"
                 )
+        if isinstance(transport, CPUCommunicator):
+            if set(transport.get_actor_handles()) != set(self._actor_handles):
+                raise ValueError("Expected actor handles to match the custom CPU group")
 
     def __str__(self) -> str:
         return (
@@ -103,6 +110,21 @@ class _CollectiveOperation:
         type_hint.set_nccl_group_id(nccl_group_id)
         return nccl_group_id
 
+    def init_cpu_group(self, cpu_group_id: Optional[str] = None) -> str:
+        """
+        Initialize the CPU group if it has not been initialized yet. If `cpu_group_id`
+        is provided, it means the CPU group has already been initialized.
+        """
+        type_hint = self._type_hint
+        if type_hint.cpu_group_id is not None:
+            return type_hint.cpu_group_id
+        if cpu_group_id is None:
+            cpu_group_id = _init_cpu_group(
+                self._actor_handles, type_hint.get_custom_cpu_group()
+            )
+        type_hint.set_cpu_group_id(cpu_group_id)
+        return cpu_group_id
+
     def get_nccl_group(self) -> GPUCommunicator:
         if self._type_hint.nccl_group_id is not None:
             ctx = ChannelContext.get_current()
@@ -113,6 +135,16 @@ class _CollectiveOperation:
             raise ValueError("Expected a NCCL group")
         return nccl_group
 
+    def get_cpu_group(self) -> CPUCommunicator:
+        if self._type_hint.cpu_group_id is not None:
+            ctx = ChannelContext.get_current()
+            cpu_group = ctx.cpu_groups[self._type_hint.cpu_group_id]
+        elif self._type_hint.get_custom_cpu_group() is not None:
+            nccl_group = self._type_hint.get_custom_cpu_group()
+        else:
+            raise ValueError("Expected a CPU group")
+        return cpu_group
+
     def execute(self, send_buf: "torch.Tensor") -> "torch.Tensor":
         """
         Call the collective operation on the input tensor. An output tensor is
@@ -120,11 +152,20 @@ class _CollectiveOperation:
         """
         import torch
 
-        if not isinstance(send_buf, torch.Tensor):
-            raise ValueError("Expected a torch tensor")
-        nccl_group = self.get_nccl_group()
-        recv_buf = torch.empty_like(send_buf)
-        nccl_group.allreduce(send_buf, recv_buf, self._op)
+        ctx = ChannelContext.get_current()
+        if ctx.nccl_groups:
+            if not isinstance(send_buf, torch.Tensor):
+                raise ValueError("Expected a torch tensor")
+            nccl_group = self.get_nccl_group()
+            recv_buf = torch.empty_like(send_buf)
+            nccl_group.allreduce(send_buf, recv_buf, self._op)
+        if ctx.cpu_groups:
+            if not isinstance(send_buf, torch.Tensor):
+                raise ValueError("Expected a torch tensor")
+            cpu_group = self.get_cpu_group()
+            recv_buf = torch.empty_like(send_buf)
+            cpu_group.allreduce(send_buf, recv_buf, self._op)
+
         return recv_buf
 
 

python/ray/dag/tests/experimental/test_cpu_communicator_dag.py
@@ -8,7 +8,7 @@ import ray
 import ray.cluster_utils
 from ray.exceptions import RayChannelError
 from ray.experimental.channel.torch_tensor_type import TorchTensorType
-from ray.experimental.channel.cpu_nccl_group import CPUNcclGroup, start_nccl_mock
+from ray.experimental.channel.cpu_communicator import CPUCommunicator
 from ray.dag import InputNode
 import ray.experimental.collective as collective
 from ray.dag.output_node import MultiOutputNode
@@ -17,8 +17,8 @@ from ray.tests.conftest import *  # noqa
 
 @ray.remote
 class CPUTorchTensorWorker:
-    def start_cuda_and_torch_mock(self):
-        start_nccl_mock()
+    def __init__(self):
+        self.device = torch.device(type="cpu")
 
     def send(self, shape, dtype, value: int, send_tensor=True):
         if not send_tensor:
@@ -38,12 +38,13 @@ class CPUTorchTensorWorker:
         return torch.ones(shape, dtype=dtype) * value
 
     def recv(self, tensor):
-        # assert tensor.device == self.device
+        assert tensor.device == self.device
         return (tensor[0].item(), tensor.shape, tensor.dtype)
 
     def recv_dict(self, tensor_dict):
         vals = {}
         for i, tensor in tensor_dict.items():
+            assert tensor.device == self.device
             vals[i] = self.recv(tensor)
         return vals
 
@@ -53,6 +54,7 @@ class CPUTorchTensorWorker:
         return tensor
 
     def recv_tensor(self, tensor):
+        assert tensor.device == self.device
         return tensor
 
     def return_tensor(self, size: int) -> torch.Tensor:
@@ -74,13 +76,7 @@ def test_p2p_basic(ray_start_cluster):
     sender = CPUTorchTensorWorker.remote()
     receiver = CPUTorchTensorWorker.remote()
 
-    cpu_group = CPUNcclGroup(2, [sender, receiver])
-    ray.get(
-        [
-            sender.start_cuda_and_torch_mock.remote(),
-            receiver.start_cuda_and_torch_mock.remote(),
-        ]
-    )
+    cpu_group = CPUCommunicator(2, [sender, receiver])
 
     shape = (10,)
     dtype = torch.float16
@@ -110,9 +106,8 @@ def test_p2p_basic(ray_start_cluster):
 def test_allreduce_basic(ray_start_cluster):
     num_workers = 2
     workers = [CPUTorchTensorWorker.remote() for _ in range(num_workers)]
-    ray.get([worker.start_cuda_and_torch_mock.remote() for worker in workers])
 
-    cpu_group = CPUNcclGroup(num_workers, workers)
+    cpu_group = CPUCommunicator(num_workers, workers)
 
     shape = (10,)
     dtype = torch.float16
@@ -157,9 +152,8 @@ def test_allreduce_basic(ray_start_cluster):
 def test_allreduce_get_partial(ray_start_cluster):
     num_workers = 2
     workers = [CPUTorchTensorWorker.remote() for _ in range(num_workers)]
-    ray.get([worker.start_cuda_and_torch_mock.remote() for worker in workers])
 
-    cpu_group = CPUNcclGroup(num_workers, workers)
+    cpu_group = CPUCommunicator(num_workers, workers)
 
     shape = (10,)
     dtype = torch.float16
@@ -202,9 +196,8 @@ def test_allreduce_get_partial(ray_start_cluster):
 def test_allreduce_wrong_shape(ray_start_cluster):
     num_workers = 2
     workers = [CPUTorchTensorWorker.remote() for _ in range(num_workers)]
-    ray.get([worker.start_cuda_and_torch_mock.remote() for worker in workers])
 
-    cpu_group = CPUNcclGroup(num_workers, workers)
+    cpu_group = CPUCommunicator(num_workers, workers)
 
     dtype = torch.float16
 
@@ -269,9 +262,8 @@ def test_allreduce_scheduling(ray_start_cluster):
     """
     num_workers = 2
     workers = [CPUTorchTensorWorker.remote() for _ in range(num_workers)]
-    ray.get([worker.start_cuda_and_torch_mock.remote() for worker in workers])
 
-    cpu_group = CPUNcclGroup(num_workers, workers)
+    cpu_group = CPUCommunicator(num_workers, workers)
 
     shape = (10,)
     dtype = torch.float16
@@ -285,7 +277,7 @@ def test_allreduce_scheduling(ray_start_cluster):
         t = workers[0].send.bind(shape, dtype, inp)
         t.with_type_hint(TorchTensorType(transport=cpu_group))
 
-        collectives = collective.allreduce.bind([x, y])
+        collectives = collective.allreduce.bind([x, y], transport=cpu_group)
         recv = workers[1].recv.bind(t)
         dag = MultiOutputNode([collectives[0], collectives[1], recv])
 
@@ -317,8 +309,10 @@ def test_allreduce_duplicate_actors(ray_start_cluster):
     Test an error is thrown when two input nodes from the same actor bind to
     an all-reduce.
     """
+    num_workers = 2
     worker = CPUTorchTensorWorker.remote()
-    ray.get(worker.start_cuda_and_torch_mock.remote())
+
+    cpu_group = CPUCommunicator(num_workers, [worker, worker])
 
     with InputNode() as inp:
         computes = [worker.return_tensor.bind(inp) for _ in range(2)]
@@ -326,7 +320,7 @@ def test_allreduce_duplicate_actors(ray_start_cluster):
             ValueError,
             match="Expected unique actor handles for a collective operation",
         ):
-            collective.allreduce.bind(computes)
+            collective.allreduce.bind(computes, transport=cpu_group)
 
     with InputNode() as inp:
         compute = worker.return_tensor.bind(inp)
@@ -335,7 +329,7 @@ def test_allreduce_duplicate_actors(ray_start_cluster):
             ValueError,
             match="Expected unique input nodes for a collective operation",
         ):
-            collective.allreduce.bind(computes)
+            collective.allreduce.bind(computes, transport=cpu_group)
 
 
 @pytest.mark.parametrize(
@@ -355,15 +349,14 @@ def test_allreduce_wrong_actors(ray_start_cluster):
     """
     num_workers = 2
     workers = [CPUTorchTensorWorker.remote() for _ in range(num_workers * 2)]
-    ray.get([worker.start_cuda_and_torch_mock.remote() for worker in workers])
 
-    cpu_group = CPUNcclGroup(num_workers, workers[:2])
+    cpu_group = CPUCommunicator(num_workers, workers[:2])
 
     with InputNode() as inp:
         computes = [worker.return_tensor.bind(inp) for worker in workers[2:]]
         with pytest.raises(
             ValueError,
-            match="Expected actor handles to match the custom NCCL group",
+            match="Expected actor handles to match the custom CPU group",
         ):
             collective.allreduce.bind(computes, transport=cpu_group)
 

python/ray/experimental/channel/torch_tensor_nccl_channel.py
@@ -9,7 +9,7 @@ import ray
 import ray.util.serialization
 from ray.experimental.channel import ChannelContext
 from ray.experimental.channel.common import ChannelInterface
-from ray.experimental.channel.cpu_nccl_group import CPUNcclGroup
+from ray.experimental.channel.cpu_communicator import CPUCommunicator
 from ray.experimental.channel.gpu_communicator import GPUCommunicator
 from ray.experimental.channel.nccl_group import _NcclGroup
 from ray.experimental.channel.shared_memory_channel import SharedMemoryType
@@ -554,7 +554,7 @@ def _do_init_nccl_group(
 ):
     import torch
 
-    if not custom_nccl_group or not isinstance(custom_nccl_group, CPUNcclGroup):
+    if not custom_nccl_group:
         assert (
             ray.get_gpu_ids()
         ), "Actors participating in NCCL group must have at least one GPU assigned"
@@ -571,6 +571,26 @@ def _do_init_nccl_group(
             actor_handles,
             torch.cuda.current_stream().cuda_stream,
             use_communication_streams,
+        )
+
+
+def _do_init_cpu_group(
+    self,
+    group_id,
+    world_size,
+    rank,
+    actor_handles,
+    custom_cpu_group: Optional[CPUCommunicator] = None,
+):
+    ctx = ChannelContext.get_current()
+    if custom_cpu_group is not None:
+        custom_cpu_group.initialize(rank)
+        ctx.cpu_groups[group_id] = custom_cpu_group
+    else:
+        ctx.cpu_groups[group_id] = CPUCommunicator(
+            world_size,
+            rank,
+            actor_handles,
         )
 
 
@@ -650,7 +670,7 @@ def _init_nccl_group(
         [actor.__ray_call__.remote(_do_check_has_gpu) for actor in actors]
     )
     for has_gpu, actor in zip(has_gpus, actors):
-        if not has_gpu and not isinstance(custom_nccl_group, CPUNcclGroup):
+        if not has_gpu:
             raise ValueError(
                 f"Actor {actor} returns a tensor with type hint "
                 'TorchTensor(transport="nccl") or '
@@ -707,6 +727,72 @@ def _init_nccl_group(
             rank=None,
             actor_handles=actors,
             cuda_stream=None,
+        )
+    return group_id
+
+
+def _init_cpu_group(
+    actors: List[ray.actor.ActorHandle],
+    custom_cpu_group: Optional[CPUCommunicator] = None,
+    use_communication_streams: bool = False,
+) -> str:
+    """
+    Initialize a NCCL group with the given actors. If a custom NCCL group is
+    provided, then it will be used, otherwise a new NCCL group will be created.
+
+    Args:
+        actors: A list of actors that participate in the NCCL group.
+        custom_nccl_group: A custom NCCL group to initialize.
+        use_communication_streams: Whether to use dedicated send and recv
+                streams for communication. If True, communication and computation
+                can be overlapped to improve perfomrance.
+    """
+    assert (
+        use_communication_streams == False
+    ), "Overlapping communication and computation is not implemented for CPUCommunicator"
+
+    ctx = ChannelContext.get_current()
+
+    actor_ids = {actor._ray_actor_id for actor in actors}
+    assert len(actor_ids) == len(actors), "Actors must be unique"
+
+    # Used to uniquely identify this NCCL group.
+    group_id = str(uuid.uuid4())
+
+    if custom_cpu_group is not None:
+        logger.info(f"Initializing custom CPU group {group_id} on actors: {actors}")
+    else:
+        logger.info(f"Creating CPU group {group_id} on actors: {actors}")
+
+    world_size = len(actors)
+    ranks = _get_ranks(actors, custom_cpu_group)
+    init_tasks = [
+        actor.__ray_call__.remote(
+            _do_init_cpu_group,
+            group_id,
+            world_size,
+            rank,
+            actors,
+            custom_cpu_group,
+        )
+        for rank, actor in zip(ranks, actors)
+    ]
+    try:
+        ray.get(init_tasks, timeout=30)
+    except ray.exceptions.GetTimeoutError:
+        logger.warning(
+            "NCCL group creation not done after 30s. NCCL group creation may be hung."
+        )
+        ray.get(init_tasks)
+
+    logger.info("CPU group initialized.")
+
+    if custom_cpu_group is not None:
+        ctx.cpu_groups[group_id] = custom_cpu_group
+    else:
+        ctx.cpu_groups[group_id] = CPUCommunicator(
+            world_size,
+            actor_handles=actors,
         )
     return group_id
 