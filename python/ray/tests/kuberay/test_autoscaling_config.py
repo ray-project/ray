37b7df004b6e7f05a0c311b578a06200c5e13a14
wa101 <wadhah.mahroug15@gmail.com>
2024-12-03 15:51:35 +0100

    add ray cr path to test class
    Signed-off-by: wa101 <wadhah.mahroug15@gmail.com>

--- python/ray/tests/kuberay/test_autoscaling_config.py
+++ python/ray/tests/kuberay/test_autoscaling_config.py
@@ -378,13 +378,14 @@ def test_autoscaling_config_fetch_retries(exception, num_exceptions):
 
         def get(self, *args, **kwargs):
             self.exception_counter += 1
-            if self.exception_counter <= num_exceptions:
+            if self.exception_counter < num_exceptions:
                 raise requests.HTTPError("Mock HTTP Error")
             return {"ok-key": "ok-value"}
 
     class MockAutoscalingConfigProducer(AutoscalingConfigProducer):
         def __init__(self, *args, **kwargs):
             self.kubernetes_api_client = MockKubernetesHttpApiClient()
+            self._ray_cr_path = "rayclusters/mock"
 
     config_producer = MockAutoscalingConfigProducer()
     # Patch retry backoff period.
