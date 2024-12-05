0c44560b2fc34813d350193e8eb41cbc399fa24f
Ruiyang Wang <56065503+rynewang@users.noreply.github.com>
2024-12-04 16:39:27 -0800

    [core] Fix broken includes (#49077)
    so cpp/ has #include "src/ray/..." but we don't want any of them in
    src/.
    
    In src/ we still have #include "src/ray/protobuf/..." and they are OK.
    
    Changing all occurances in src/, that has #include "src/ray/..." but not
    in protobuf/ by removing the `src/` part.
    
    ---------
    
    Signed-off-by: Ruiyang Wang <rywang014@gmail.com>

--- src/ray/util/tests/shared_lru_test.cc
+++ src/ray/util/tests/shared_lru_test.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "src/ray/util/shared_lru.h"
+#include "ray/util/shared_lru.h"
 
 #include <gtest/gtest.h>
 
