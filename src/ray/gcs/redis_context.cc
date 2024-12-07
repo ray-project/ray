5af0ab825f4965041d3f9f6f56d21929dab3d1f8
Ruiyang Wang <rywang014@gmail.com>
2024-12-06 16:38:15 -0800

    remote useless dispatch
    Signed-off-by: Ruiyang Wang <rywang014@gmail.com>

--- src/ray/gcs/redis_context.cc
+++ src/ray/gcs/redis_context.cc
@@ -673,9 +673,8 @@ void RedisContext::RunArgvAsync(std::vector<std::string> args,
                                                  std::move(redis_callback),
                                                  redis_async_context_.get(),
                                                  std::move(args));
-  // If we are already on the io_service thread, we can run the request immediately.
-  io_service_.dispatch([request_context]() { request_context->Run(); },
-                       "RedisContext::RunArgvAsync");
+  // RedisRequestContext is thread safe.
+  request_context->Run();
 }
 
 }  // namespace gcs
