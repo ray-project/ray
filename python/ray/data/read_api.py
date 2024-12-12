d033d2964fe5d5840434299d31015cd99cb84ce5
Connor Sanders <connor@elastiflow.com>
2024-12-11 18:48:31 -0800

    [Data] Add read_clickhouse API to read ClickHouse Dataset (#49060)
    Greetings from ElastiFlow!
    
    This PR introduces a new ClickHouseDatasource connector for Ray, which
    provides a convenient way to read data from ClickHouse into Ray
    Datasets. The ClickHouseDatasource is particularly useful for users who
    are working with large datasets stored in ClickHouse and want to
    leverage Ray's distributed computing capabilities for AI and ML
    use-cases. We found this functionality useful while evaluating ML
    technologies and wanted to contribute this back.
    
    Key Features and Benefits:
    1. **Seamless Integration**: The ClickHouseDatasource allows for
    seamless integration of ClickHouse data into Ray workflows, enabling
    users to easily access their data and apply Ray's powerful parallel
    computation.
    2. **Custom Query Support**: Users can specify custom columns, and
    orderings, allowing for flexible query generation directly from the Ray
    interface, which helps in reading only the necessary data, thereby
    improving performance.
    3. **User-Friendly API**: The connector abstracts the complexity of
    setting up and querying ClickHouse, providing a simple API that allows
    users to focus on data analysis rather than data extraction.
    
    Tested locally with a ClickHouse table containing ~12m records.
    
    <img width="1340" alt="Screenshot 2024-11-20 at 3 52 42 AM"
    src="https://github.com/user-attachments/assets/2421e48a-7169-4a9e-bb4d-b6b96f7e502b">
    
    PLEASE NOTE: This PR is a continuation of
    https://github.com/ray-project/ray/pull/48817, which was closed without
    merging.
    
    ---------
    
    Signed-off-by: Connor Sanders <connor@elastiflow.com>
    Co-authored-by: Alexey Kudinkin <alexey.kudinkin@gmail.com>

--- python/ray/data/read_api.py
+++ python/ray/data/read_api.py
@@ -23,6 +23,7 @@ from ray.air.util.tensor_extensions.utils import _create_possibly_ragged_ndarray
 from ray.data._internal.datasource.avro_datasource import AvroDatasource
 from ray.data._internal.datasource.bigquery_datasource import BigQueryDatasource
 from ray.data._internal.datasource.binary_datasource import BinaryDatasource
+from ray.data._internal.datasource.clickhouse_datasource import ClickHouseDatasource
 from ray.data._internal.datasource.csv_datasource import CSVDatasource
 from ray.data._internal.datasource.delta_sharing_datasource import (
     DeltaSharingDatasource,
@@ -3243,6 +3244,79 @@ def read_lance(
         filter=filter,
         storage_options=storage_options,
         scanner_options=scanner_options,
+    )
+
+    return read_datasource(
+        datasource=datasource,
+        ray_remote_args=ray_remote_args,
+        concurrency=concurrency,
+        override_num_blocks=override_num_blocks,
+    )
+
+
+@PublicAPI(stability="alpha")
+def read_clickhouse(
+    *,
+    table: str,
+    dsn: str,
+    columns: Optional[List[str]] = None,
+    order_by: Optional[Tuple[List[str], bool]] = None,
+    client_settings: Optional[Dict[str, Any]] = None,
+    client_kwargs: Optional[Dict[str, Any]] = None,
+    ray_remote_args: Optional[Dict[str, Any]] = None,
+    concurrency: Optional[int] = None,
+    override_num_blocks: Optional[int] = None,
+) -> Dataset:
+    """
+    Create a :class:`~ray.data.Dataset` from a ClickHouse table or view.
+
+    Examples:
+        >>> import ray
+        >>> ds = ray.data.read_clickhouse( # doctest: +SKIP
+        ...     table="default.table",
+        ...     dsn="clickhouse+http://username:password@host:8124/default",
+        ...     columns=["timestamp", "age", "status", "text", "label"],
+        ...     order_by=(["timestamp"], False),
+        ... )
+
+    Args:
+        table: Fully qualified table or view identifier (e.g.,
+            "default.table_name").
+        dsn: A string in standard DSN (Data Source Name) HTTP format (e.g.,
+            "clickhouse+http://username:password@host:8124/default").
+            For more information, see `ClickHouse Connection String doc
+            <https://clickhouse.com/docs/en/integrations/sql-clients/cli#connection_string>`_.
+        columns: Optional list of columns to select from the data source.
+            If no columns are specified, all columns will be selected by default.
+        order_by: Optional tuple containing a list of columns to order by and a boolean indicating whether the order
+            should be descending (True for DESC, False for ASC). Please Note: order_by is required to support
+            parallelism. If not provided, the data will be read in a single task. This is to ensure
+            that the data is read in a consistent order across all tasks.
+        client_settings: Optional ClickHouse server settings to be used with the session/every request.
+            For more information, see `ClickHouse Client Settings
+            <https://clickhouse.com/docs/en/integrations/python#settings-argument>`_.
+        client_kwargs: Optional additional arguments to pass to the ClickHouse client. For more information,
+            see `ClickHouse Core Settings <https://clickhouse.com/docs/en/integrations/python#additional-options>`_.
+        ray_remote_args: kwargs passed to :meth:`~ray.remote` in the read tasks.
+        concurrency: The maximum number of Ray tasks to run concurrently. Set this
+            to control number of tasks to run concurrently. This doesn't change the
+            total number of tasks run or the total number of output blocks. By default,
+            concurrency is dynamically decided based on the available resources.
+        override_num_blocks: Override the number of output blocks from all read tasks.
+            By default, the number of output blocks is dynamically decided based on
+            input data size and available resources. You shouldn't manually set this
+            value in most cases.
+
+    Returns:
+        A :class:`~ray.data.Dataset` producing records read from the ClickHouse table or view.
+    """  # noqa: E501
+    datasource = ClickHouseDatasource(
+        table=table,
+        dsn=dsn,
+        columns=columns,
+        order_by=order_by,
+        client_settings=client_settings,
+        client_kwargs=client_kwargs,
     )
 
     return read_datasource(
