dc8f1d92d5a97aa8e1d5a5f82ff4ac02deb198d3
dentiny <dentinyhao@gmail.com>
2024-12-16 08:00:50 +0000

    comment on ray log filepath for gcs
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- src/ray/gcs/gcs_server/gcs_server_main.cc
+++ src/ray/gcs/gcs_server/gcs_server_main.cc
@@ -34,7 +34,7 @@ DEFINE_string(event_log_dir,
 DEFINE_string(
     ray_log_filepath,
     "",
-    "The filename to dump gcs server log on stdout, which is written via `RAY_LOG`.");
+    "The log filepath to dump gcs server log, which is written via `RAY_LOG`.");
 DEFINE_int32(gcs_server_port, 0, "The port of gcs server.");
 DEFINE_int32(metrics_agent_port, -1, "The port of metrics agent.");
 DEFINE_string(config_list, "", "The config list of raylet.");
