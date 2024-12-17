9777b29c3046d5efc4602952dc224c96f18b1a1d
Balaji Veeramani <bveeramani@berkeley.edu>
2024-12-16 19:13:46 -0800

    Fix example
    Signed-off-by: Balaji Veeramani <bveeramani@berkeley.edu>

--- python/ray/data/datasource/file_based_datasource.py
+++ python/ray/data/datasource/file_based_datasource.py
@@ -84,7 +84,7 @@ class FileShuffleConfig:
         >>> import ray
         >>> from ray.data import FileShuffleConfig
         >>> shuffle = FileShuffleConfig(seed=42)
-        >>> ray.data.read_images("s3://anonymous@ray-example-data/batoidea", shuffle=shuffle)
+        >>> ds = ray.data.read_images("s3://anonymous@ray-example-data/batoidea", shuffle=shuffle)
     """  # noqa: E501
 
     seed: Optional[int] = None
