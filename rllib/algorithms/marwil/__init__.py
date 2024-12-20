4a249ed2156d18e175d37b944999050978e6638a
sven1977 <svenmika1977@gmail.com>
2024-12-20 09:44:58 +0100

    wip

--- rllib/algorithms/marwil/__init__.py
+++ rllib/algorithms/marwil/__init__.py
@@ -2,8 +2,6 @@ from ray.rllib.algorithms.marwil.marwil import (
     MARWIL,
     MARWILConfig,
 )
-from ray.rllib.algorithms.marwil.marwil_learner import MARWILLearner
-from ray.rllib.algorithms.marwil.torch.marwil_torch_learner import MARWILTorchLearner
 from ray.rllib.algorithms.marwil.marwil_tf_policy import (
     MARWILTF1Policy,
     MARWILTF2Policy,
@@ -13,8 +11,6 @@ from ray.rllib.algorithms.marwil.marwil_torch_policy import MARWILTorchPolicy
 __all__ = [
     "MARWIL",
     "MARWILConfig",
-    "MARWILLearner",
-    "MARWILTorchLearner",
     # @OldAPIStack
     "MARWILTF1Policy",
     "MARWILTF2Policy",
