4e8f9df093a4115ec10594426e346bc0d1a355ed
Balaji Veeramani <bveeramani@berkeley.edu>
2024-12-19 17:45:58 -0800

    Initial commit
    Signed-off-by: Balaji Veeramani <bveeramani@berkeley.edu>

---
+++ release/nightly_tests/dataset/map_benchmark.py
@@ -0,0 +1,83 @@
+import argparse
+import zlib
+
+import pyarrow as pa
+import pyarrow.compute as pc
+import pandas as pd
+import ray
+
+from benchmark import Benchmark
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Map benchmark")
+    parser.add_argument(
+        "--api", choices=["map", "map_batches", "flat_map"], required=True
+    )
+    parser.add_argument(
+        "--sf", choices=["1", "10", "100", "1000", "10000"], default="1"
+    )
+    parser.add_argument("--batch-format", choices=["numpy", "pandas", "pyarrow"])
+    parser.add_argument("--compute", choices=["tasks", "actors"])
+    return parser.parse_args()
+
+
+def main(args: argparse.Namespace) -> None:
+    benchmark = Benchmark("map")
+
+    path = f"s3://ray-benchmark-data/tpch/parquet/sf{args.sf}/lineitem"
+    ds = ray.data.read_parquet(path)
+
+    if args.api == "map":
+        ds = ds.map(increment_row)
+    elif args.api == "map_batches":
+        if not args.compute or args.compute == "tasks":
+            ds = ds.map_batches(increment_batch, batch_format=args.batch_format)
+        else:
+            ds = ds.map_batches(
+                IncrementBatch, batch_format=args.batch_format, concurrency=(1, 1024)
+            )
+    elif args.api == "flat_map":
+        ds = ds.flat_map(flat_increment_row)
+    else:
+        assert False, f"Invalid API argument: {args}"
+
+    def benchmark_fn():
+        for _ in ds.iter_internal_ref_bundles():
+            pass
+
+    benchmark.run_fn(str(vars(args)), benchmark_fn)
+    benchmark.write_result()
+
+
+def increment_row(row):
+    row["column00"] += 1
+    return row
+
+
+def flat_increment_row(row):
+    row["column00"] += 1
+    return [row]
+
+
+def increment_batch(batch):
+    if isinstance(batch, (dict, pd.DataFrame)):
+        batch["column00"] += 1
+    elif isinstance(batch, pa.Table):
+        column00_incremented = pc.add(batch["column00"], 1)
+        batch = batch.set_column(
+            batch.column_names.index("column00"), "column00", column00_incremented
+        )
+    else:
+        assert False, f"Invalid batch format: {type(batch)}"
+    return batch
+
+
+class IncrementBatch:
+    def __call__(self, batch):
+        return increment_batch(batch)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
