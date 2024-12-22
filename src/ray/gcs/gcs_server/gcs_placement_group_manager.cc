e27b1265d6dd65bdce4f8d43f7ddbf185b10c6d3
dentiny <dentinyhao@gmail.com>
2024-12-22 11:58:22 -0800

    [core] Use redis error as callback arg (#49357)
    As of now, the callback for getting job status from storage takes two
    arguments,
    - one for `status`, which means the status of getting entry from storage
    - another for optional result, which indicates the extracted value
    
    For all storage client implementations as of now, I'm not sure why
    `status` argument is not used;
    IMO either we propagate the status to caller; or we check failure and
    remove the `status` argument.
    This PR does (1).
    
    ---------
    
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- src/ray/gcs/gcs_server/gcs_placement_group_manager.cc
+++ src/ray/gcs/gcs_server/gcs_placement_group_manager.cc
@@ -593,7 +593,7 @@ void GcsPlacementGroupManager::HandleGetPlacementGroup(
     }
     RAY_LOG(DEBUG) << "Finished getting placement group info, placement group id = "
                    << placement_group_id;
-    GCS_RPC_SEND_REPLY(send_reply_callback, reply, Status::OK());
+    GCS_RPC_SEND_REPLY(send_reply_callback, reply, status);
   };
 
   auto it = registered_placement_groups_.find(placement_group_id);
@@ -715,6 +715,10 @@ void GcsPlacementGroupManager::WaitPlacementGroup(
     auto on_done = [this, placement_group_id, callback](
                        const Status &status,
                        const std::optional<PlacementGroupTableData> &result) {
+      if (!status.ok()) {
+        callback(status);
+        return;
+      }
       if (result) {
         RAY_LOG(DEBUG) << "Placement group is removed, placement group id = "
                        << placement_group_id;
