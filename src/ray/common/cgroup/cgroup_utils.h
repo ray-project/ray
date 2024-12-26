12f0cfe2ca87299f6fae3419b1e8a872e434d24d
dentiny <dentinyhao@gmail.com>
2024-12-26 23:46:46 +0000

    fix comment
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- src/ray/common/cgroup/cgroup_utils.h
+++ src/ray/common/cgroup/cgroup_utils.h
@@ -25,6 +25,9 @@
 namespace ray {
 
 // A util class which sets up cgroup at construction, and cleans up at destruction.
+// On ctor, creates a cgroup v2 if necessary based on the context. Then puts `ctx.pid`
+// into this cgroup. On dtor, puts `ctx.pid` into the default cgroup, and remove this
+// cgroup v2 if any.
 class CgroupV2Setup {
  public:
   // A failed construction returns nullptr.
