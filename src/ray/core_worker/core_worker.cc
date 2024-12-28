9b3cf24c3035cc68384eaf6bc6ccf35639dec8a5
dentiny <dentinyhao@gmail.com>
2024-12-28 00:59:20 +0000

    print serialized runtime env submit task
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -2380,6 +2380,8 @@ std::vector<rpc::ObjectReference> CoreWorker::SubmitTask(
   int64_t depth = worker_context_.GetTaskDepth() + 1;
   // TODO(ekl) offload task building onto a thread pool for performance
 
+  RAY_LOG(ERROR) << "hjiang " << task_options.serialized_runtime_env_info;
+
   BuildCommonTaskSpec(builder,
                       worker_context_.GetCurrentJobID(),
                       task_id,
