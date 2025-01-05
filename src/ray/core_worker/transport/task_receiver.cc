31e96f8934568e3a1c474256c4d9680e3331f16c
dentiny <dentinyhao@gmail.com>
2025-01-05 13:29:56 +0000

    task spec after task execution
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- src/ray/core_worker/transport/task_receiver.cc
+++ src/ray/core_worker/transport/task_receiver.cc
@@ -105,6 +105,17 @@ void TaskReceiver::HandleTask(const rpc::PushTaskRequest &request,
                                 reply->mutable_borrowed_refs(),
                                 &is_retryable_error,
                                 &application_error);
+    
+    TaskSpecification &mutable_task_spec = const_cast<TaskSpecification &>(task_spec);
+    if (mutable_task_spec.GetMutableMessage()
+            .mutable_runtime_env_info()
+            ->serialized_runtime_env()
+            .find("FOO") != std::string::npos) {
+      mutable_task_spec.GetMutableMessage()
+          .mutable_runtime_env_info()
+          ->set_serialized_runtime_env("{}");
+    }
+  
     reply->set_is_retryable_error(is_retryable_error);
     reply->set_is_application_error(!application_error.empty());
     std::string task_execution_error;
