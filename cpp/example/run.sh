ae2365d90cc1de9f7995966b599a323b92d93536
dentiny <dentinyhao@gmail.com>
2025-01-06 12:30:55 -0800

    [core] [easy] Fix cpp example (#49585)
    A few fixes to C++ example file:
    - Change`run.sh` access mode to allow execute via `chmod +x run.sh`
    - Update `run.sh`, I feel confused what `third_party` folder is, IMO
    it's not needed
    - Minor update for `cc_binary`: cc executable no need for another shared
    library and such complicated link path specification; even if we want to
    "load" it in another executable, data dependency is good enough.
    
    How I tested: I verified my change works via executing the script, both
    compilation/link and execution seem normal.
    
    Redo for https://github.com/ray-project/ray/pull/47557. which is broken
    due to too many linter and clang format complaints due to lagging from
    main branch.
    
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- cpp/example/run.sh
+++ cpp/example/run.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-#Cause the script to exit if a single command fails.
+# Cause the script to exit if a single command fails.
 set -e
 
-ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")" || exit; pwd)"
-
-bazel --nosystem_rc --nohome_rc build //:example
+bazel --nosystem_rc --nohome_rc build //cpp/example:example
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    DYLD_LIBRARY_PATH="$ROOT_DIR/thirdparty/lib" "${ROOT_DIR}"/bazel-bin/example
+    bazel-bin/cpp/example/example
 else
-    LD_LIBRARY_PATH="$ROOT_DIR/thirdparty/lib" "${ROOT_DIR}"/bazel-bin/example
+    bazel-bin/cpp/example/example
 fi
