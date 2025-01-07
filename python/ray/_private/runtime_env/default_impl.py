54b237694716bd44545e4ef237db0215d616d113
Philipp Moritz <pcmoritz@gmail.com>
2025-01-07 11:14:00 -0800

    [core] Introduce get_compatible_keys for ImageURIPlugin (#49612)
    <!-- Thank you for your contribution! Please review
    https://github.com/ray-project/ray/blob/master/CONTRIBUTING.rst before
    opening a pull request. -->
    
    <!-- Please add a reviewer to the assignee section when you create a PR.
    If you don't have the access to it, we will shortly find a reviewer and
    assign them to your PR. -->
    
    ## Why are these changes needed?
    
    Refactor the code so it can be overwritten in the ImageURIPlugin
    
    ## Related issue number
    
    <!-- For example: "Closes #1234" -->
    
    ## Checks
    
    - [ ] I've signed off every commit(by using the -s flag, i.e., `git
    commit -s`) in this PR.
    - [ ] I've run `scripts/format.sh` to lint the changes in this PR.
    - [ ] I've included any doc changes needed for
    https://docs.ray.io/en/master/.
    - [ ] I've added any new APIs to the API Reference. For example, if I
    added a
    method in Tune, I've added it in `doc/source/tune/api/` under the
               corresponding `.rst` file.
    - [ ] I've made sure the tests are passing. Note that there might be a
    few flaky tests, see the recent failures at https://flakey-tests.ray.io/
    - Testing Strategy
       - [ ] Unit tests
       - [ ] Release tests
       - [ ] This PR is not tested :(

--- python/ray/_private/runtime_env/default_impl.py
+++ python/ray/_private/runtime_env/default_impl.py
@@ -1,8 +1,8 @@
 from ray._private.runtime_env.image_uri import ImageURIPlugin
 
 
-def get_image_uri_plugin(ray_tmp_dir: str):
-    return ImageURIPlugin(ray_tmp_dir)
+def get_image_uri_plugin_cls():
+    return ImageURIPlugin
 
 
 def get_protocols_provider():
