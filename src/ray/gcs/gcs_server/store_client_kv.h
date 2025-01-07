fdb7d75305ef4c54a3cb1ba4c3f35896296188f2
Ruiyang Wang <rywang014@gmail.com>
2025-01-07 14:46:04 -0800

    lint
    Signed-off-by: Ruiyang Wang <rywang014@gmail.com>

--- src/ray/gcs/gcs_server/store_client_kv.h
+++ src/ray/gcs/gcs_server/store_client_kv.h
@@ -33,10 +33,10 @@ class StoreClientInternalKV : public InternalKVInterface {
            const std::string &key,
            Postable<void(std::optional<std::string>)> callback) override;
 
-  void MultiGet(const std::string &ns,
-                const std::vector<std::string> &keys,
-                Postable<void(absl::flat_hash_map<std::string, std::string>)>
-                    callback) override;
+  void MultiGet(
+      const std::string &ns,
+      const std::vector<std::string> &keys,
+      Postable<void(absl::flat_hash_map<std::string, std::string>)> callback) override;
 
   void Put(const std::string &ns,
            const std::string &key,
