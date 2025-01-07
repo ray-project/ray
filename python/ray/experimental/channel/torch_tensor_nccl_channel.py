b1044a848ca7205dcbc1690ad216bd625bbd0114
Rui Qiao <161574667+ruisearch42@users.noreply.github.com>
2025-01-07 14:11:31 -0800

    [core][compiled graphs] Throw unrecoverable actor exceptions at ray.get() (#49461)
    Signed-off-by: Rui Qiao <ruisearch42@gmail.com>
    Signed-off-by: lielin.hyl <lielin.hyl@alibaba-inc.com>

--- python/ray/experimental/channel/torch_tensor_nccl_channel.py
+++ python/ray/experimental/channel/torch_tensor_nccl_channel.py
@@ -227,8 +227,11 @@ class TorchTensorNcclChannel(ChannelInterface):
         if isinstance(value, ray.exceptions.RayTaskError):
             if self._typ.static_shape or self._typ.direct_return:
                 # Raise a fatal error to teardown the DAG.
+                # This error will also be caught from `CompiledDAGRef.get()`
+                # and raised to the user
                 # TODO(swang): Write exceptions to the tensor metadata or
-                # non-tensor data channel if it is available.
+                # non-tensor data channel if it is available to make these
+                # exceptions recoverable.
                 raise value
 
         if self._cpu_data_channel is None:
@@ -237,12 +240,12 @@ class TorchTensorNcclChannel(ChannelInterface):
             # directly without trying to serialize it first.
             import torch
 
+            # These ValueErrors will also be caught from `CompiledDAGRef.get()`
+            # and raised to the user
             if not isinstance(value, torch.Tensor):
-                # TODO(swang): These errors are currently fatal for the DAG
-                # because there is no way for the receiver to receive the
-                # exception. This could be improved by sending the exception
-                # through the gpu_data_channel's CPU-based metadata channel,
-                # if one exists.
+                # TODO(swang): These errors are currently fatal for the DAG.
+                # This could be improved by sending the exception through the
+                # gpu_data_channel's CPU-based metadata channel, if one exists.
                 raise ValueError(
                     "Task annotated with _direct_return=True must "
                     "return a CUDA torch.Tensor, instead found value "
