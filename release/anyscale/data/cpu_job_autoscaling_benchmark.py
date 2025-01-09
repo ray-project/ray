90a2c216a66dac7b551b7930e554f920ae60016f
Balaji Veeramani <balaji@anyscale.com>
2025-01-09 11:55:39 -0800

    [Data] Fix broken CPU autoscaling benchmark (#1331)
    https://github.com/ray-project/ray/pull/49549 removed the `name`
    parameter from the `Benchmark` constructor. This PR updates this release
    test accordingly.
    
    ---------
    
    Signed-off-by: Balaji Veeramani <bveeramani@berkeley.edu>

--- release/anyscale/data/cpu_job_autoscaling_benchmark.py
+++ release/anyscale/data/cpu_job_autoscaling_benchmark.py
@@ -4,7 +4,6 @@ from typing import Dict
 import numpy as np
 import ray
 from torchvision import transforms
-from ray.anyscale.data import VideoDatasource
 
 from benchmark import Benchmark
 
@@ -20,12 +19,12 @@ def main():
     """
     ray.init()
     paths = [f"{DATA_URI}/000.mp4" for _ in range(NUM_FILES)]
-    dataset = ray.data.read_datasource(VideoDatasource(paths=paths, include_paths=True))
+    dataset = ray.data.read_videos(paths=paths, include_paths=True)
     dataset = dataset.map(transform_frame)
     dataset = dataset.select_columns(["path", "frame_index"])
     dataset_iter = dataset.iter_batches(batch_size=None)
 
-    benchmark = Benchmark("cpu_job_autoscaling_benchmark")
+    benchmark = Benchmark()
     benchmark.run_iterate_ds("main", dataset_iter)
     test_output_json = os.environ.get("TEST_OUTPUT_JSON", "/tmp/release_test_out.json")
     benchmark.write_result(test_output_json)
