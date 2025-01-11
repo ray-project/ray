3170c2bb3cacf13fefebf01549d190fa7f80162a
dentiny <dentinyhao@gmail.com>
2025-01-10 18:33:37 -0800

    [core] pipe logging in C++ (#49733)
    This PR implements the pipe logging in C++, instead of python.
    The main motivation for reuse for other languages in the future.
    
    One thing worth noticing is I use two backgrounds for read from pipe and
    dump to spdlog:
    - The motivation is to avoid any IO operation on read thread, thus
    blocking write thread due to limited pipe buffer size
    - The overhead of which is extra copy of logging message; we should
    merge into one thread if performance issue reported
    
    Usage in core worker:
    ```
    class CoreWorker {
      // Only assigned when rotation is requested; otherwise empty;
      // All handles' termination functors are invoked at core workers' destruction.
      std::vector<RotationFileHandle> rotation_file_handles_;
    };
    ```
    
    ---------
    
    Signed-off-by: dentiny <dentinyhao@gmail.com>

---
+++ src/ray/util/pipe_logger.h
@@ -0,0 +1,127 @@
+// Copyright 2025 The Ray Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Util on logging with pipe.
+
+#pragma once
+
+#include <functional>
+#include <limits>
+#include <string>
+
+namespace ray {
+
+// Environmenr variable, which indicates the pipe size of read.
+//
+// TODO(hjiang): Should document the env variable after end-to-end integration has
+// finished.
+inline constexpr std::string_view kPipeLogReadBufSizeEnv = "RAY_PIPE_LOG_READ_BUF_SIZE";
+
+// Configuration for log rotation. By default no rotation enabled.
+struct LogRotationOption {
+  // Max number of bytes in a rotated file.
+  size_t rotation_max_size = std::numeric_limits<size_t>::max();
+  // Max number of files for all rotated files.
+  size_t rotation_max_file_count = 1;
+};
+
+// TODO(hjiang): Use `MEMFD_TYPE_NON_UNIQUE` after we split `plasma/compat.h` into a
+// separate target, otherwise we're introducing too many unnecessary dependencies.
+class RotationFileHandle {
+ public:
+  RotationFileHandle(int write_fd, std::function<void()> termination_caller)
+      : write_fd_(write_fd), termination_caller_(std::move(termination_caller)) {}
+  RotationFileHandle(const RotationFileHandle &) = delete;
+  RotationFileHandle &operator=(const RotationFileHandle &) = delete;
+
+  // Used to write to.
+  //
+  // TODO(hjiang): I will followup with another PR to make a `FD` class, which is not
+  // copiable to avoid manual `dup`.
+#if defined(__APPLE__) || defined(__linux__)
+  RotationFileHandle(RotationFileHandle &&rhs) {
+    write_fd_ = rhs.write_fd_;
+    rhs.write_fd_ = -1;
+    termination_caller_ = std::move(rhs.termination_caller_);
+  }
+  RotationFileHandle &operator=(RotationFileHandle &&rhs) {
+    if (this == &rhs) {
+      return *this;
+    }
+    write_fd_ = rhs.write_fd_;
+    rhs.write_fd_ = -1;
+    termination_caller_ = std::move(rhs.termination_caller_);
+    return *this;
+  }
+  ~RotationFileHandle() {
+    // Only invoke termination functor when handler at a valid state.
+    if (write_fd_ != -1) {
+      termination_caller_();
+    }
+  }
+
+  int GetWriteHandle() const { return write_fd_; }
+
+ private:
+  int write_fd_;
+#elif defined(_WIN32)
+  RotationFileHandle(RotationFileHandle &&rhs) {
+    write_fd_ = rhs.write_fd_;
+    rhs.write_fd_ = nullptr;
+    termination_caller_ = std::move(rhs.termination_caller_);
+  }
+  RotationFileHandle &operator=(RotationFileHandle &&rhs) {
+    if (this == &rhs) {
+      return *this;
+    }
+    write_fd_ = rhs.write_fd_;
+    rhs.write_fd_ = nullptr;
+    termination_caller_ = std::move(rhs.termination_caller_);
+    return *this;
+  }
+  ~RotationFileHandle() {
+    // Only invoke termination functor when handler at a valid state.
+    if (write_fd_ != nullptr) {
+      termination_caller_();
+    }
+  }
+
+  HANDLE GetWriteHandle() const { return write_handle_; }
+
+ private:
+  HANDLE write_handle_;
+#endif
+
+  // Termination hook, used to flush and call completion.
+  std::function<void()> termination_caller_;
+};
+
+// This function creates a pipe so applications could write to.
+// There're will be two threads spawned in the background, one thread continuously reads
+// from the pipe, another one dumps whatever read from pipe to the persistent file.
+//
+// The returned [RotationFileHandle] owns the lifecycle for pipe file descriptors and
+// background threads, the resource release happens at token's destruction.
+//
+// @param on_completion: called after all content has been persisted.
+// @return pipe stream token, so application could stream content into, and synchronize on
+// the destruction.
+//
+// Notice caller side should _NOT_ close the given file handle, it will be handled
+// internally.
+RotationFileHandle CreatePipeAndStreamOutput(const std::string &fname,
+                                             const LogRotationOption &log_rotate_opt,
+                                             std::function<void()> on_completion);
+
+}  // namespace ray
---
+++ src/ray/util/tests/pipe_logger_test.cc
@@ -0,0 +1,83 @@
+// Copyright 2025 The Ray Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#if defined(__APPLE__) || defined(__linux__)
+
+#include "ray/util/pipe_logger.h"
+
+#include <gtest/gtest.h>
+
+#include <cstdint>
+#include <future>
+#include <string_view>
+
+#include "ray/util/tests/unix_test_utils.h"
+#include "ray/util/util.h"
+
+namespace ray {
+
+namespace {
+
+constexpr std::string_view kLogLine1 = "hello\n";
+constexpr std::string_view kLogLine2 = "world\n";
+
+class PipeLoggerTest : public ::testing::TestWithParam<size_t> {};
+
+TEST_P(PipeLoggerTest, LogWriteAndPersistence) {
+  const size_t pipe_buffer_size = GetParam();
+  setenv(kPipeLogReadBufSizeEnv.data(),
+         absl::StrFormat("%d", pipe_buffer_size).data(),
+         /*overwrite=*/1);
+
+  // TODO(core): We should have a better test util, which allows us to create a temporary
+  // testing directory.
+  const std::string test_fname = absl::StrFormat("%s.out", GenerateUUIDV4());
+
+  std::promise<void> promise{};
+  auto on_completion = [&promise]() { promise.set_value(); };
+
+  // Take the default option, which doesn't have rotation enabled.
+  LogRotationOption logging_option{};
+  {
+    auto log_token =
+        CreatePipeAndStreamOutput(test_fname, logging_option, std::move(on_completion));
+
+    ASSERT_EQ(write(log_token.GetWriteHandle(), kLogLine1.data(), kLogLine1.length()),
+              kLogLine1.length());
+    ASSERT_EQ(write(log_token.GetWriteHandle(), kLogLine2.data(), kLogLine2.length()),
+              kLogLine2.length());
+
+    // Write empty line, which is not expected to appear.
+    ASSERT_EQ(write(log_token.GetWriteHandle(), "\n", /*count=*/1), 1);
+  }
+
+  // Synchronize on log flush completion.
+  promise.get_future().get();
+
+  // Check log content after completion.
+  const auto actual_content = CompleteReadFile(test_fname);
+  const std::string expected_content = absl::StrFormat("%s%s", kLogLine1, kLogLine2);
+  EXPECT_EQ(actual_content, expected_content);
+
+  // Delete temporary file.
+  EXPECT_EQ(unlink(test_fname.data()), 0);
+}
+
+INSTANTIATE_TEST_SUITE_P(PipeLoggerTest, PipeLoggerTest, testing::Values(1024, 3));
+
+}  // namespace
+
+}  // namespace ray
+
+#endif
