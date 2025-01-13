197a1ef680d8e68efc84c9b369e54f7d60581476
Chi-Sheng Liu <chishengliu@chishengliu.com>
2025-01-14 02:24:14 +0800

    [CI][Linter] Replace `flake8` and `isort` with `ruff` (#49435)
    and use ruff inside pre-commit.

--- python/ray/train/examples/experiment_tracking/torch_exp_tracking_mlflow.py
+++ python/ray/train/examples/experiment_tracking/torch_exp_tracking_mlflow.py
@@ -1,4 +1,4 @@
-# flake8: noqa
+# ruff: noqa
 # isort: skip_file
 from filelock import FileLock
 import os
--- python/ray/tune/tests/tutorial.py
+++ python/ray/tune/tests/tutorial.py
@@ -1,4 +1,4 @@
-# flake8: noqa
+# ruff: noqa
 # isort: skip_file
 # Original Code: https://github.com/pytorch/examples/blob/master/mnist/main.py
 
