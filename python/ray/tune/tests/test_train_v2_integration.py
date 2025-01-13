04b37e91ef80d5ef6c489f48e6f3e786587aa024
Justin Yu <justinvyu@anyscale.com>
2025-01-13 12:19:54 -0800

    update test to search over num_workers
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/tune/tests/test_train_v2_integration.py
+++ python/ray/tune/tests/test_train_v2_integration.py
@@ -10,14 +10,14 @@ from ray.train.v2._internal.constants import HEALTH_CHECK_INTERVAL_S_ENV_VAR
 from ray.train.v2.api.data_parallel_trainer import DataParallelTrainer
 
 TRAIN_DRIVER_RESOURCE_NAME = "train_driver_resource"
-NUM_GPUS = 4
+NUM_GPUS_IN_CLUSTER = 4
 
 
 @pytest.fixture
 def ray_cpu_head_gpu_worker():
     cluster = Cluster()
     cluster.add_node(resources={TRAIN_DRIVER_RESOURCE_NAME: 1})
-    cluster.add_node(num_cpus=0, num_gpus=NUM_GPUS)
+    cluster.add_node(num_cpus=0, num_gpus=NUM_GPUS_IN_CLUSTER)
 
     ray.init(address=cluster.address)
 
@@ -40,20 +40,22 @@ def test_e2e(
     num_trials,
     limit_concurrency,
 ):
-    num_workers_per_trial = 2
+    num_workers_per_trial = [1, 2]
 
     def train_fn_per_worker(train_fn_config):
+        world_size = ray.train.get_context().get_world_size()
         with create_dict_checkpoint({"model": "dummy"}) as checkpoint:
-            ray.train.report({"loss": 0.1}, checkpoint=checkpoint)
+            ray.train.report(
+                {"loss": 0.1, "world_size": world_size}, checkpoint=checkpoint
+            )
 
     def launch_training(tune_config):
         # TODO: Add TuneReportCallback to report intermediate metrics
-
         trainer = DataParallelTrainer(
             train_loop_per_worker=train_fn_per_worker,
             train_loop_config=tune_config["train_loop_config"],
             scaling_config=ray.train.ScalingConfig(
-                num_workers=num_workers_per_trial, use_gpu=True
+                num_workers=tune_config["num_workers"], use_gpu=True
             ),
             run_config=ray.tune.RunConfig(
                 storage_path=tmp_path,
@@ -69,22 +71,29 @@ def test_e2e(
     tuner = ray.tune.Tuner(
         ray.tune.with_resources(launch_training, {TRAIN_DRIVER_RESOURCE_NAME: 0.01}),
         param_space={
-            "train_loop_config": {
-                "trial_idx": ray.tune.grid_search(list(range(num_trials)))
-            }
+            # Search over parameters passed into each train worker.
+            "train_loop_config": {"lr": ray.tune.choice([0.01, 0.001])},
+            # Search over Train "run level" parameters.
+            "num_workers": ray.tune.grid_search(num_workers_per_trial),
         },
         tune_config=ray.tune.TuneConfig(
             max_concurrent_trials=(
-                NUM_GPUS // num_workers_per_trial if limit_concurrency else None
+                NUM_GPUS_IN_CLUSTER // max(num_workers_per_trial)
+                if limit_concurrency
+                else None
             )
         ),
         run_config=ray.tune.RunConfig(storage_path=tmp_path, name="tune"),
     )
     result_grid = tuner.fit()
     assert len(result_grid) == num_trials
+
+    world_sizes = set()
     for result in result_grid:
         assert "loss" in result.metrics
         assert "checkpoint_path" in result.metrics
+        world_sizes.add(result.metrics["world_size"])
+    assert world_sizes == {1, 2}
 
 
 if __name__ == "__main__":
