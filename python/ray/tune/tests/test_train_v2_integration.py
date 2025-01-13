f6c451208f8a5d92d6f4218fc01edd2c19df7455
Justin Yu <justinvyu@anyscale.com>
2025-01-13 12:42:30 -0800

    propagate more env vars properly
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/tune/tests/test_train_v2_integration.py
+++ python/ray/tune/tests/test_train_v2_integration.py
@@ -13,7 +13,7 @@ TRAIN_DRIVER_RESOURCE_NAME = "train_driver_resource"
 NUM_GPUS_IN_CLUSTER = 4
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def ray_cpu_head_gpu_worker():
     cluster = Cluster()
     cluster.add_node(resources={TRAIN_DRIVER_RESOURCE_NAME: 1})
@@ -32,16 +32,14 @@ def speed_up_tests(monkeypatch):
     monkeypatch.setenv(HEALTH_CHECK_INTERVAL_S_ENV_VAR, "0.1")
 
 
-@pytest.mark.parametrize("num_trials", [1, 4])
+@pytest.mark.parametrize("num_workers_grid_search", [[1], [1, 2, 4]])
 @pytest.mark.parametrize("limit_concurrency", [True, False])
 def test_e2e(
     ray_cpu_head_gpu_worker,
     tmp_path,
-    num_trials,
+    num_workers_grid_search,
     limit_concurrency,
 ):
-    num_workers_per_trial = [1, 2]
-
     def train_fn_per_worker(train_fn_config):
         world_size = ray.train.get_context().get_world_size()
         with create_dict_checkpoint({"model": "dummy"}) as checkpoint:
@@ -74,11 +72,11 @@ def test_e2e(
             # Search over parameters passed into each train worker.
             "train_loop_config": {"lr": ray.tune.choice([0.01, 0.001])},
             # Search over Train "run level" parameters.
-            "num_workers": ray.tune.grid_search(num_workers_per_trial),
+            "num_workers": ray.tune.grid_search(num_workers_grid_search),
         },
         tune_config=ray.tune.TuneConfig(
             max_concurrent_trials=(
-                NUM_GPUS_IN_CLUSTER // max(num_workers_per_trial)
+                NUM_GPUS_IN_CLUSTER // max(num_workers_grid_search)
                 if limit_concurrency
                 else None
             )
@@ -86,14 +84,14 @@ def test_e2e(
         run_config=ray.tune.RunConfig(storage_path=tmp_path, name="tune"),
     )
     result_grid = tuner.fit()
-    assert len(result_grid) == num_trials
+    assert len(result_grid) == len(num_workers_grid_search)
 
     world_sizes = set()
     for result in result_grid:
         assert "loss" in result.metrics
         assert "checkpoint_path" in result.metrics
         world_sizes.add(result.metrics["world_size"])
-    assert world_sizes == {1, 2}
+    assert world_sizes == set(num_workers_grid_search)
 
 
 if __name__ == "__main__":
