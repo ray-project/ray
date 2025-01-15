7d302ac2fbfdee77b135931b3d2f5290916c3da7
Matthew Deng <matt@anyscale.com>
2025-01-15 15:34:49 -0800

    [train] update mnist example
    Signed-off-by: Matthew Deng <matt@anyscale.com>

--- python/ray/tune/examples/mnist_ptl_mini.py
+++ python/ray/tune/examples/mnist_ptl_mini.py
@@ -3,12 +3,12 @@ import os
 
 import pytorch_lightning as pl
 import torch
+from datasets import load_dataset
 from filelock import FileLock
 from torch.nn import functional as F
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader
 from torchmetrics import Accuracy
 from torchvision import transforms
-from torchvision.datasets import MNIST
 
 from ray import train, tune
 from ray.tune.integration.pytorch_lightning import TuneReportCheckpointCallback
@@ -19,43 +19,46 @@ PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
 class MNISTDataModule(pl.LightningDataModule):
     def __init__(self, batch_size: int, data_dir: str = PATH_DATASETS):
         super().__init__()
+        self.batch_size = batch_size
+        self.dims = (1, 28, 28)
+        self.num_classes = 10
         self.data_dir = data_dir
-        self.transform = transforms.Compose(
+
+    def prepare_data(self):
+        # Download the MNIST dataset if not already present
+        with FileLock(os.path.expanduser("~/.data.lock")):
+            load_dataset("ylecun/mnist", cache_dir=self.data_dir)
+
+    def setup(self, stage=None):
+        dataset = load_dataset("ylecun/mnist", cache_dir=self.data_dir)
+
+        transform = transforms.Compose(
             [
                 transforms.ToTensor(),
                 transforms.Normalize((0.1307,), (0.3081,)),
             ]
         )
 
-        self.batch_size = batch_size
-        self.dims = (1, 28, 28)
-        self.num_classes = 10
+        train_data = [
+            (transform(sample["image"]), sample["label"]) for sample in dataset["train"]
+        ]
+        test_data = [
+            (transform(sample["image"]), sample["label"]) for sample in dataset["test"]
+        ]
 
-    def prepare_data(self):
-        # download
-        MNIST(self.data_dir, train=True, download=True)
-        MNIST(self.data_dir, train=False, download=True)
-
-    def setup(self, stage=None):
-        # Assign train/val datasets for use in dataloaders
-        if stage == "fit" or stage is None:
-            mnist_full = MNIST(self.data_dir, train=True, transform=self.transform)
-            self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
-
-        # Assign test dataset for use in dataloader(s)
-        if stage == "test" or stage is None:
-            self.mnist_test = MNIST(
-                self.data_dir, train=False, transform=self.transform
-            )
+        self.dataset = {"train": train_data, "test": test_data}
 
     def train_dataloader(self):
-        return DataLoader(self.mnist_train, batch_size=self.batch_size)
+        return DataLoader(
+            self.dataset["train"],
+            batch_size=self.batch_size,
+        )
 
     def val_dataloader(self):
-        return DataLoader(self.mnist_val, batch_size=self.batch_size)
-
-    def test_dataloader(self):
-        return DataLoader(self.mnist_test, batch_size=self.batch_size)
+        return DataLoader(
+            self.dataset["test"],  # Using test set as validation
+            batch_size=self.batch_size,
+        )
 
 
 class LightningMNISTClassifier(pl.LightningModule):
