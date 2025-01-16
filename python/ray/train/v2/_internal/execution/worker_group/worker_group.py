0b8a8db7b7ce4b95e725f35c0687220878cfb557
Justin Yu <justinvyu@anyscale.com>
2025-01-16 09:49:55 -0800

    [train v2+tune] Add Train v2 + Tune integration test (#49601)
    Add an integration test for Train v2 + Tune.
    
    This PR also contains sets the environment variable introduced in
    https://github.com/ray-project/ray/pull/49522 to not run
    `TrainController` as an actor when running in a Tune trainable actor.
    
    ---------
    
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/train/v2/_internal/execution/worker_group/worker_group.py
+++ python/ray/train/v2/_internal/execution/worker_group/worker_group.py
@@ -440,7 +440,9 @@ class WorkerGroup:
             else:
                 done_refs = [w.actor.__ray_terminate__.remote() for w in self._workers]
                 # Wait for actors to die gracefully.
-                _, not_done = ray.wait(done_refs, timeout=patience_s)
+                _, not_done = ray.wait(
+                    done_refs, num_returns=len(done_refs), timeout=patience_s
+                )
                 if not_done:
                     logger.debug(
                         "Graceful termination failed. Falling back to force kill."
@@ -449,11 +451,11 @@ class WorkerGroup:
                     for worker in self._workers:
                         ray.kill(worker.actor)
 
-            if self._pg:
-                remove_placement_group(self._pg)
-
             if self._sync_actor:
                 ray.kill(self._sync_actor)
+
+            if self._pg:
+                remove_placement_group(self._pg)
 
             self._clear_state()
 
