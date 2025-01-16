f3739efd52535b7df7a8c5797cb75ff8aca631b4
matthewdeng <matt@anyscale.com>
2025-01-15 18:44:19 -0800

    [tune] fix mnist example (#49874)
    Fixes timeout issue caused by reading from `torchvision`'s `MNIST` by
    reading from `datasets` instead.
    
    Signed-off-by: Matthew Deng <matt@anyscale.com>

--- python/ray/tune/examples/mnist_ptl_mini.py
+++ python/ray/tune/examples/mnist_ptl_mini.py
@@ -3,12 +3,12 @@ import os
 
 import pytorch_lightning as pl
 import torch
+from datasets import load_dataset
 from filelock import FileLock
 from torch.nn import functional as F
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader
 from torchmetrics import Accuracy
 from torchvision import transforms
-from torchvision.datasets import MNIST
 
 from ray import train, tune
 from ray.tune.integration.pytorch_lightning import TuneReportCheckpointCallback
@@ -26,36 +26,29 @@ class MNISTDataModule(pl.LightningDataModule):
                 transforms.Normalize((0.1307,), (0.3081,)),
             ]
         )
-
         self.batch_size = batch_size
         self.dims = (1, 28, 28)
         self.num_classes = 10
 
     def prepare_data(self):
         # download
-        MNIST(self.data_dir, train=True, download=True)
-        MNIST(self.data_dir, train=False, download=True)
+        with FileLock(os.path.expanduser("~/.data.lock")):
+            load_dataset("ylecun/mnist", cache_dir=self.data_dir)
 
     def setup(self, stage=None):
-        # Assign train/val datasets for use in dataloaders
-        if stage == "fit" or stage is None:
-            mnist_full = MNIST(self.data_dir, train=True, transform=self.transform)
-            self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
+        dataset = load_dataset("ylecun/mnist", cache_dir=self.data_dir)
 
-        # Assign test dataset for use in dataloader(s)
-        if stage == "test" or stage is None:
-            self.mnist_test = MNIST(
-                self.data_dir, train=False, transform=self.transform
-            )
+        def transform_fn(sample):
+            return (self.transform(sample["image"]), sample["label"])
+
+        self.mnist_train = [transform_fn(sample) for sample in dataset["train"]]
+        self.mnist_val = [transform_fn(sample) for sample in dataset["test"]]
 
     def train_dataloader(self):
         return DataLoader(self.mnist_train, batch_size=self.batch_size)
 
     def val_dataloader(self):
         return DataLoader(self.mnist_val, batch_size=self.batch_size)
-
-    def test_dataloader(self):
-        return DataLoader(self.mnist_test, batch_size=self.batch_size)
 
 
 class LightningMNISTClassifier(pl.LightningModule):
