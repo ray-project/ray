4818581867edf247da3753a4577c4d5ec0811c37
tlaurie <tycoonoilandgas@gmail.com>
2025-01-18 11:33:21 -0500

    Trigger test rerun for CI pipeline
    Signed-off-by: tlaurie <tycoonoilandgas@gmail.com>

---
+++ rllib/examples/learners/mog_learner.py
@@ -0,0 +1,134 @@
+"""Example of how to write a custom loss function, custom rl_module and custom config.
+
+This example shows:
+    - how to subclass an existing (torch) Learner and override its `compute_loss_for_module()` method
+    - how you can add your own loss to a custom (or non-custom) rl_module using compute_loss_for_module
+    - how to add add custom pipeline components to a custom config class using build_learner_connector
+    - how to plug in a custom Learner, rl_module, its loss, and use a custom Config to train with
+
+See the :py:class:`~ray.rllib.examples.learners.classes.mog_loss.PPOTorchLearnerCustomMOGLoss`  # noqa
+class for details on how to override the main (PPO) loss function.
+
+This script will create a new critic network that will be a distribution of gaussians to allow for more expressivity
+of the value function. The param --num_gaussians specifies how many gaussians to use for the value function. 
+Normally between 3-5 generally works well.
+
+
+How to run this script
+----------------------
+`python mog_learner.py --enable-new-api-stack --num-gaussians=3 --lr=0.001`
+
+
+Results to expect
+-----------------
+In general with environments, the distributional critic should perform better than a normal critic given
+the same hyperparameters. Some hiccups have been the alphas going too small which causes nans in the loss equation.
+This has been fixed by using the log_softmax(alphas) instead of using the softmax on the alphas once out of
+the module.
+
+With --num_guassians=2 and --lr=0.0001
+(trying to reach 250.0 return on CartPole in 100k env steps):
++-----------------------------+------------+-----------------+--------+
+| Trial name                  | status     | loc             |   iter |
+|                             |            |                 |        |
+|-----------------------------+------------+-----------------+--------+
+| PPO_CartPole-v1_4a3a0_00000 | TERMINATED | 127.0.0.1:54584 |     10 |
++-----------------------------+------------+-----------------+--------+
++------------------+------------------------+---------------------+
+|   total time (s) | num_env_steps_sampled_ | episode_return_mean |
+|                  |              _lifetime |                     |
+|------------------+------------------------+---------------------+
+|          42.9956 |                 40_000 |              271.53 |
++------------------+------------------------+---------------------+
+"""
+
+from pprint import pprint
+from ray.rllib.core.rl_module.rl_module import RLModuleSpec
+from ray.rllib.examples.learners.custom_ppo_config import CustomPPOConfig
+from ray.rllib.examples.rl_modules.classes.custom_mog import MOGModule
+from ray.rllib.examples.learners.classes.mog_loss import PPOTorchLearnerCustomMOGLoss
+from ray.rllib.utils.framework import try_import_torch
+from ray.rllib.utils.test_utils import (
+    add_rllib_example_script_args,
+)
+
+torch, _ = try_import_torch()
+
+
+parser = add_rllib_example_script_args(
+    default_reward=250.0,
+    default_timesteps=200_000,
+)
+parser.set_defaults(enable_new_api_stack=True)
+parser.add_argument(
+    "--num_gaussians",
+    type=int,
+    default=3,
+    help="Number of gaussians to represent the value function",
+)
+parser.add_argument(
+    "--lr",
+    type=float,
+    default=0.0003,
+    help="Learning rate",
+)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    custom_config = {
+        "vf_share_layers": False,
+        "fcnet_hiddens": [128, 128],
+        "fcnet_activation": "LeakyReLU",
+        "num_mixture_components": args.num_gaussians,
+    }
+
+    module_to_load_spec = RLModuleSpec(
+        module_class=MOGModule,
+        model_config=custom_config,
+    )
+
+    assert (
+        args.enable_new_api_stack
+    ), "Must set --enable-new-api-stack when running this script!"
+    assert args.algo == "PPO", "Must set --algo=PPO when running this script!"
+
+    base_config = (
+        CustomPPOConfig()
+        .environment("CartPole-v1")
+        .training(
+            # set the learner class which will use the custom loss function we want
+            learner_class=PPOTorchLearnerCustomMOGLoss,
+            # can pass args through the learner_config_dict to have access in the custom loss config
+            lr=args.lr,
+        )
+        .rl_module(
+            rl_module_spec=module_to_load_spec,
+        )
+    )
+
+algo = base_config.build()
+total_timesteps = 0
+default_timesteps = 200_000
+default_reward = 250
+for iteration in range(1000):
+    result = algo.train()
+
+    pprint(result)
+
+    episode_return_mean = result["env_runners"]["episode_return_mean"]
+    total_timesteps = result["env_runners"]["num_env_steps_sampled_lifetime"]
+
+    print(
+        f"Iteration {iteration}: Episode Return Mean: {episode_return_mean}, Total Timesteps: {total_timesteps}"
+    )
+    if episode_return_mean >= default_reward:
+        print(
+            f"Stopping: Reached target reward of {default_reward} at iteration {iteration}"
+        )
+        break
+    if total_timesteps >= default_timesteps:
+        print(
+            f"Stopping: Reached maximum timesteps of {default_timesteps} at iteration {iteration}"
+        )
+        break
---
+++ rllib/examples/rl_modules/classes/custom_mog.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+import gymnasium as gym
+from typing import Any, Dict, Optional
+from ray.rllib.utils.annotations import override
+from ray.rllib.core.rl_module.rl_module import RLModule
+from ray.rllib.core.rl_module.torch.torch_rl_module import TorchRLModule
+from ray.rllib.core.rl_module.apis import ValueFunctionAPI
+
+
+"""
+Custom rl_module that demonstrates setting up, definding the necessary _forward methods,
+and overriding the ValueFunctionAPI to compute values using mixture of gaussian components.
+
+This also uses a custom_config from the module_to_load_spec to get the fcnet_hiddens
+as well as custom arg num_gaussians
+"""
+
+
+class MOGModule(TorchRLModule, ValueFunctionAPI):
+    def __init__(
+        self,
+        observation_space: gym.spaces.Space,
+        action_space: gym.spaces.Space,
+        inference_only: bool = False,
+        learner_only: bool = False,
+        model_config: Optional[dict] = None,
+        catalog_class: Optional[type] = None,
+    ) -> None:
+        super().__init__(
+            observation_space=observation_space,
+            action_space=action_space,
+            inference_only=inference_only,
+            learner_only=learner_only,
+            model_config=model_config,
+        )
+
+    def setup(self):
+        input_dim = self.observation_space.shape[0]
+        hidden_dim = self.model_config["fcnet_hiddens"][0]
+
+        # set failsafe for action space dim
+        if isinstance(self.action_space, gym.spaces.Box):
+            output_dim = self.action_space.shape[0]
+        elif isinstance(self.action_space, gym.spaces.Discrete):
+            output_dim = self.action_space.n
+        else:
+            raise ValueError(
+                f"Unsupported action space type: {type(self.action_space)}"
+            )
+
+        self.num_gaussians = self.model_config.get(
+            "num_mixture_components", 1
+        )  # create a base scalar as a value (normal critic)
+
+        self.policy = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.LeakyReLU(),
+            nn.Linear(hidden_dim, output_dim),
+            nn.Tanh(),
+        )
+
+        self.mog_critic = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.LeakyReLU(),
+            nn.Linear(hidden_dim, self.num_gaussians * 3),
+        )
+
+    @override(RLModule)
+    def _forward_inference(self, batch: Dict[str, Any]) -> Dict[str, Any]:
+        with torch.no_grad():
+            return self._forward_train(batch)
+
+    @override(RLModule)
+    def _forward_exploration(self, batch: Dict[str, Any], **kwargs) -> Dict[str, Any]:
+        with torch.no_grad():
+            return self._forward_train(batch)
+
+    @override(ValueFunctionAPI)
+    def compute_values(self, batch: Dict[str, Any], embeddings: Optional[Any] = None):
+        obs = batch["obs"]
+        mog_output = self._compute_mog_components(obs)
+        return torch.sum(
+            mog_output["means"]
+            * torch.clamp(
+                nn.functional.softmax(mog_output["alphas"], dim=-1), 1e-6, None
+            ),
+            dim=-1,
+        )
+
+    def _compute_mog_components(self, obs: torch.Tensor) -> Dict[str, torch.Tensor]:
+        mog_output = self.mog_critic(obs)
+        means = mog_output[:, : self.num_gaussians]
+        sigmas_prev = mog_output[:, self.num_gaussians : self.num_gaussians * 2]
+        sigmas = nn.functional.softplus(sigmas_prev) + 1e-6
+        alphas = mog_output[:, self.num_gaussians * 2 :]
+        return {
+            "means": means,
+            "sigmas": sigmas,
+            "alphas": alphas,
+        }
+
+    @override(RLModule)
+    def _forward_train(self, batch: Dict[str, Any]) -> Dict[str, Any]:
+        obs = batch["obs"]
+        action_logits = self.policy(obs)
+        value_function_out = self.compute_values(batch)
+
+        return {
+            "action_dist_inputs": action_logits,
+            "value_function_out": value_function_out,
+            "mog_components": self._compute_mog_components(obs),
+        }
