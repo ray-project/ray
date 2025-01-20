aec5cb9085e776747b9a42c3da2d3c5330787c19
fscnick <fscnick.dev@gmail.com>
2025-01-20 20:04:49 +0800

    [Refector] Fix ruff rule F841:unused-variable conti
    Signed-off-by: fscnick <fscnick.dev@gmail.com>

--- python/ray/dag/tests/experimental/test_accelerated_dag.py
+++ python/ray/dag/tests/experimental/test_accelerated_dag.py
@@ -223,7 +223,7 @@ class TestDAGRefDestruction:
         with InputNode() as inp:
             dag = a.inc.bind(inp)
         compiled_dag = dag.experimental_compile()
-        ref = compiled_dag.execute(2)
+        compiled_dag.execute(2)
         ref2 = compiled_dag.execute(2)
         ref3 = compiled_dag.execute(2)
         del ref2
@@ -241,7 +241,7 @@ class TestDAGRefDestruction:
         del ref2
         del ref3
         ray.get(ref)
-        ref4 = compiled_dag.execute(3)
+        compiled_dag.execute(3)
         # Test that max_inflight error is not raised as ref2 and ref3
         # should be destructed and not counted in the inflight executions
         ref5 = compiled_dag.execute(3)
