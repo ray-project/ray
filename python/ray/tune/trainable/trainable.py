2d288de73df3740b7ab59c38f827e7dde1a32513
Justin Yu <justinvyu@anyscale.com>
2025-01-22 13:40:20 -0800

    tune class trainables should save tune.Checkpoint
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/tune/trainable/trainable.py
+++ python/ray/tune/trainable/trainable.py
@@ -15,10 +15,10 @@ import ray
 import ray.cloudpickle as ray_pickle
 from ray.air._internal.util import exception_cause, skip_exceptions
 from ray.air.constants import TIME_THIS_ITER_S, TIMESTAMP, TRAINING_ITERATION
-from ray.train import Checkpoint
 from ray.train._internal.checkpoint_manager import _TrainingResult
 from ray.train._internal.storage import StorageContext, _exists_at_fs_path
 from ray.train.constants import DEFAULT_STORAGE_PATH
+from ray.tune import Checkpoint
 from ray.tune.execution.placement_groups import PlacementGroupFactory
 from ray.tune.result import (
     DEBUG_METRICS,
