1e832dff3b396f8005b6929d15544615bacef922
Richard Liaw <rliaw@berkeley.edu>
2025-01-22 11:56:05 -0800

    lint
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>

--- python/ray/data/_internal/datasource/sql_datasource.py
+++ python/ray/data/_internal/datasource/sql_datasource.py
@@ -3,7 +3,7 @@ import math
 from contextlib import contextmanager
 from typing import Any, Callable, Iterable, Iterator, List, Optional
 
-from ray.data.block import Block, BlockAccessor, BlockMetadata
+from ray.data.block import Block, BlockMetadata
 from ray.data.datasource.datasource import Datasource, ReadTask
 
 Connection = Any  # A Python DB API2-compliant `Connection` object.
