52b7e93cfd60a2cc37310628d215d79b84395eec
Justin Yu <justinvyu@anyscale.com>
2025-01-23 00:29:09 -0800

    add imagenet utils
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- release/train_tests/torch/image_classification_factory.py
+++ release/train_tests/torch/image_classification_factory.py
@@ -29,7 +29,9 @@ class ImageClassificationFactory(BenchmarkFactory):
         if self.benchmark_config.dataloader_type == DataloaderType.RAY_DATA:
             ds_iterator = ray.train.get_dataset_shard("train")
             # TODO: configure this
-            return ds_iterator.iter_torch_batches(batch_size=32)
+            return ds_iterator.iter_torch_batches(
+                batch_size=32, local_shuffle_buffer_size=32 * 8
+            )
         elif self.benchmark_config.dataloader_type == DataloaderType.MOCK:
             return mock_dataloader(num_batches=64, batch_size=32)
         else:
@@ -52,9 +54,23 @@ class ImageClassificationFactory(BenchmarkFactory):
         if self.benchmark_config.dataloader_type != DataloaderType.RAY_DATA:
             return {}
 
-        # TODO: for imagenet
-        train_ds = ray.data.read_parquet()
-        return {"train": train_ds}
+        from imagenet import get_preprocess_map_fn, IMAGENET_PARQUET_SPLIT_S3_DIRS
+
+        train_ds = ray.data.read_parquet(
+            IMAGENET_PARQUET_SPLIT_S3_DIRS["train"], columns=["image", "label"]
+        ).map(get_preprocess_map_fn(decode_image=True, random_transforms=True))
+
+        # TODO: The validation parquet files do not have labels,
+        # so just use a subset of the train dataset for now.
+        val_ds = (
+            ray.data.read_parquet(
+                IMAGENET_PARQUET_SPLIT_S3_DIRS["val"], columns=["image"]
+            )
+            .limit(50000)
+            .map(get_preprocess_map_fn(decode_image=True, random_transforms=False))
+        )
+
+        return {"train": train_ds, "val": val_ds}
 
     def get_loss_fn(self):
         return torch.nn.CrossEntropyLoss()
