7a263d41f1001bc9809e0ca977641d6730f00be8
Cody Yu <hao.yu.cody@gmail.com>
2025-01-28 11:57:23 -0800

    done
    Signed-off-by: Cody Yu <hao.yu.cody@gmail.com>

---
+++ python/ray/llm/_internal/batch/stages/chat_template_stage.py
@@ -0,0 +1,59 @@
+"""Apply chat template stage"""
+
+from typing import Any, Dict, AsyncIterator, List
+
+from ray.llm._internal.batch.stages.base import (
+    StatefulStage,
+    StatefulStageUDF,
+)
+from ray.llm._internal.batch.utils import get_cached_tokenizer
+
+
+class ChatTemplateUDF(StatefulStageUDF):
+    def __init__(
+        self,
+        data_column: str,
+        model: str,
+    ):
+        """
+        Initialize the HttpRequestUDF.
+        Args:
+            data_column: The data column name.
+            model: The model to use for the chat template.
+        """
+        from transformers import AutoTokenizer
+
+        super().__init__(data_column)
+        self.tokenizer = get_cached_tokenizer(AutoTokenizer.from_pretrained(model))
+
+    async def udf(self, batch: List[Dict[str, Any]]) -> AsyncIterator[Dict[str, Any]]:
+        """
+        Apply chat template to the given batch.
+        Args:
+            batch: A list of rows to send.
+        Yields:
+            A generator of rows with the chat template applied.
+        """
+        for prompt in self.tokenizer.apply_chat_template(
+            [row["messages"].tolist() for row in batch],
+            tokenize=False,
+            add_generation_prompt=True,
+        ):
+            yield {"prompt": prompt}
+
+    @property
+    def expected_input_keys(self) -> List[str]:
+        """The expected input keys."""
+        return ["messages"]
+
+
+class ChatTemplateStage(StatefulStage):
+    """
+    A stage that applies chat template.
+    """
+
+    fn: StatefulStageUDF = ChatTemplateUDF
+    fn_constructor_kwargs: Dict[str, Any]
+    map_batches_kwargs: Dict[str, Any] = dict(
+        concurrency=1,
+    )
