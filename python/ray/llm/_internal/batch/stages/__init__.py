a0aff9726827549cf6e7386d431aaf83bc2870bb
Cody Yu <hao.yu.cody@gmail.com>
2025-01-28 16:11:10 -0800

    init
    Signed-off-by: Cody Yu <hao.yu.cody@gmail.com>

--- python/ray/llm/_internal/batch/stages/__init__.py
+++ python/ray/llm/_internal/batch/stages/__init__.py
@@ -1,9 +1,11 @@
 from .base import StatefulStage, wrap_preprocess, wrap_postprocess
 from .http_request_stage import HttpRequestStage
+from .prepare_image_stage import PrepareImageStage
 
 __all__ = [
     "StatefulStage",
     "HttpRequestStage",
     "wrap_preprocess",
     "wrap_postprocess",
+    "PrepareImageStage",
 ]
