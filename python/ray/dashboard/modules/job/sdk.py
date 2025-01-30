4e149cdd448082a92d4ac1d6a7da4f4a47ea855f
Philipp Moritz <pcmoritz@gmail.com>
2025-01-30 14:48:06 -0800

    Update sdk.py
    Signed-off-by: Philipp Moritz <pcmoritz@gmail.com>

--- python/ray/dashboard/modules/job/sdk.py
+++ python/ray/dashboard/modules/job/sdk.py
@@ -381,7 +381,7 @@ class JobSubmissionClient(SubmissionClient):
             start_time=1647454832, end_time=None, metadata={}, runtime_env={})]
 
         Returns:
-            List all jobs along with their status and other information.
+            A list of JobDetails containing the job status and other information.
 
         Raises:
             RuntimeError: If the request to the job server fails.
