af728f2365625cde667a4ef9faa5dd36fca50998
Ruiyang Wang <rywang014@gmail.com>
2025-01-30 16:50:25 -0800

    WIP ServeHead. Problem is the dynamically created class is not pickleable.
    Signed-off-by: Ruiyang Wang <rywang014@gmail.com>

--- python/ray/dashboard/modules/serve/serve_head.py
+++ python/ray/dashboard/modules/serve/serve_head.py
@@ -1,10 +1,8 @@
-import ray.dashboard.optional_utils as optional_utils
-import ray.dashboard.utils as dashboard_utils
+from ray.dashboard.subprocesses.routes import SubprocessRouteTable
+from ray.dashboard.subprocesses.module import SubprocessModule
 from ray.dashboard.modules.serve.serve_rest_api_impl import create_serve_rest_api
 
-dashboard_head_route_table = optional_utils.DashboardHeadRouteTable
-
 ServeHead = create_serve_rest_api(
-    dashboard_module_superclass=dashboard_utils.DashboardHeadModule,
-    dashboard_route_table=dashboard_head_route_table,
+    dashboard_module_superclass=SubprocessModule,
+    dashboard_route_table=SubprocessRouteTable,
 )
