1db769ebfd4dea012055ee318bd900e188f124a1
dentiny <dentinyhao@gmail.com>
2025-01-31 00:35:16 +0000

    remove python preload module
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- src/ray/raylet/worker_pool.cc
+++ src/ray/raylet/worker_pool.cc
@@ -417,15 +417,15 @@ WorkerPool::BuildProcessCommandArgs(const Language &language,
     }
   }
 
-  if (language == Language::PYTHON && worker_type == rpc::WorkerType::WORKER &&
-      RayConfig::instance().preload_python_modules().size() > 0) {
-    std::string serialized_preload_python_modules =
-        absl::StrJoin(RayConfig::instance().preload_python_modules(), ",");
-    RAY_LOG(DEBUG) << "Starting worker with preload_python_modules "
-                   << serialized_preload_python_modules;
-    worker_command_args.push_back("--worker-preload-modules=" +
-                                  serialized_preload_python_modules);
-  }
+  // if (language == Language::PYTHON && worker_type == rpc::WorkerType::WORKER &&
+  //     RayConfig::instance().preload_python_modules().size() > 0) {
+  //   std::string serialized_preload_python_modules =
+  //       absl::StrJoin(RayConfig::instance().preload_python_modules(), ",");
+  //   RAY_LOG(DEBUG) << "Starting worker with preload_python_modules "
+  //                  << serialized_preload_python_modules;
+  //   worker_command_args.push_back("--worker-preload-modules=" +
+  //                                 serialized_preload_python_modules);
+  // }
 
   // We use setproctitle to change python worker process title,
   // causing the process's /proc/PID/environ being empty.
