8e10a6796b3e7a671c70836de7656c4478488c33
dentiny <dentinyhao@gmail.com>
2025-01-31 20:41:19 -0800

    [core] Use boost iostream in pipe logger for cross-platform (#50044)
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- src/ray/util/tests/stream_redirection_utils_test.cc
+++ src/ray/util/tests/stream_redirection_utils_test.cc
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#if defined(__APPLE__) || defined(__linux__)
-
 #include "ray/util/stream_redirection_utils.h"
 
 #include <gtest/gtest.h>
@@ -31,15 +29,33 @@ namespace ray {
 namespace {
 constexpr std::string_view kLogLine1 = "hello\n";
 constexpr std::string_view kLogLine2 = "world\n";
+
+// Output logging files to cleanup at process termination.
+std::vector<std::string> log_files;
+void CleanupOutputLogFiles() {
+  for (const auto &cur_log : log_files) {
+    EXPECT_TRUE(std::filesystem::remove(cur_log));
+  }
+}
+
 }  // namespace
 
 TEST(LoggingUtilTest, RedirectStderr) {
+  const std::string test_file_path = absl::StrFormat("%s.err", GenerateUUIDV4());
+  const std::string log_file_path1 = test_file_path;
+  const std::string log_file_path2 = absl::StrFormat("%s.1", test_file_path);
+  log_files.emplace_back(log_file_path1);
+  log_files.emplace_back(log_file_path2);
+
+  // Cleanup generated log files at test completion; because loggers are closed at process
+  // termination via exit hook, and hooked functions are executed at the reverse order of
+  // their registration, so register cleanup hook before logger close hook.
+  ASSERT_EQ(std::atexit(CleanupOutputLogFiles), 0);
+
   // Works via `dup`, so have to execute before we redirect via `dup2` and close stderr.
   testing::internal::CaptureStderr();
 
   // Redirect stderr for testing, so we could have stdout for debugging.
-  const std::string test_file_path = absl::StrFormat("%s.err", GenerateUUIDV4());
-
   StreamRedirectionOption opts;
   opts.file_path = test_file_path;
   opts.tee_to_stderr = true;
@@ -56,12 +72,10 @@ TEST(LoggingUtilTest, RedirectStderr) {
   FlushOnRedirectedStderr();
 
   // Check log content after completion.
-  const std::string log_file_path1 = test_file_path;
   const auto actual_content1 = ReadEntireFile(log_file_path1);
   RAY_ASSERT_OK(actual_content1);
   EXPECT_EQ(*actual_content1, kLogLine2);
 
-  const std::string log_file_path2 = absl::StrFormat("%s.1", test_file_path);
   const auto actual_content2 = ReadEntireFile(log_file_path2);
   RAY_ASSERT_OK(actual_content2);
   EXPECT_EQ(*actual_content2, kLogLine1);
@@ -70,13 +84,7 @@ TEST(LoggingUtilTest, RedirectStderr) {
   std::string stderr_content = testing::internal::GetCapturedStderr();
   EXPECT_EQ(stderr_content, absl::StrFormat("%s%s", kLogLine1, kLogLine2));
 
-  // Delete temporary file.
-  EXPECT_EQ(unlink(log_file_path1.data()), 0);
-  EXPECT_EQ(unlink(log_file_path2.data()), 0);
-
   // Make sure flush hook works fine and process terminates with no problem.
 }
 
 }  // namespace ray
-
-#endif
