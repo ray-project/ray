2ae861eec1cdea49f87b12db078ffd05309f1ba5
Balaji Veeramani <balaji@anyscale.com>
2025-02-03 16:59:55 -0800

    [Data] Mark aggregation functions as public APIs (#50202)
    https://github.com/ray-project/ray/pull/46193 moved `AggregateFn`
    subclasses like `Count` to the `_internal` subpackage. The rationale was
    that they're implementation details of methods like `Dataset.count`.
    
    However, if you want to perform multiple aggregations simultaneously,
    then you need to use the `AggregateFn` subclasses. So, this PR makes the
    subclasses public.
    
    ---------
    
    Signed-off-by: Balaji Veeramani <bveeramani@berkeley.edu>

--- python/ray/data/tests/test_execution_optimizer.py
+++ python/ray/data/tests/test_execution_optimizer.py
@@ -8,7 +8,6 @@ import pyarrow as pa
 import pytest
 
 import ray
-from ray.data._internal.aggregate import Count
 from ray.data._internal.datasource.parquet_datasink import ParquetDatasink
 from ray.data._internal.execution.interfaces import ExecutionOptions
 from ray.data._internal.execution.operators.base_physical_operator import (
@@ -64,6 +63,7 @@ from ray.data._internal.logical.util import (
 from ray.data._internal.planner.exchange.sort_task_spec import SortKey
 from ray.data._internal.planner.planner import Planner
 from ray.data._internal.stats import DatasetStats
+from ray.data.aggregate import Count
 from ray.data.block import BlockMetadata
 from ray.data.context import DataContext
 from ray.data.datasource import Datasource
