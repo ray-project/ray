c1614e3700309a92a32351f0e59e574cf8afb8f7
Philipp Moritz <pcmoritz@gmail.com>
2025-02-04 14:03:58 -0800

    windows

--- python/ray/tests/test_runtime_env_py_executable.py
+++ python/ray/tests/test_runtime_env_py_executable.py
@@ -20,6 +20,7 @@ def tmp_working_dir():
         yield tmp_dir
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="Not ported to Windows yet.")
 def test_simple_py_executable(shutdown_only):
     runtime_env = {
         "py_executable": "env RAY_TEST_PY_EXECUTABLE_ENV_EXAMPLE=1 " + sys.executable
@@ -33,6 +34,7 @@ def test_simple_py_executable(shutdown_only):
     assert ray.get(f.remote()) == "1"
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="Not ported to Windows yet.")
 def test_py_executable_with_working_dir(shutdown_only, tmp_working_dir):
     tmp_dir = tmp_working_dir
 
