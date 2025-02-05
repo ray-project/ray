4fae7f2df167755431a21426178910c1b5433b13
Alexey Kudinkin <ak@anyscale.com>
2025-02-05 10:32:14 -0800

    [Data] Added support for type promotions  (#50231)
    - Adds support for type promotions when unifying schemas b/w the blocks (not supported for PA < 14.0)
    - Fixes semantic around np.nan handling for Pandas/Arrow blocks
    - Enables all tests to run against every implementation of shuffling
    - Makes sure tasks actual resource bundles are captured by PhysicalOperator
    - Fixed tests
    
    ---------
    
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/_private/utils.py
+++ python/ray/_private/utils.py
@@ -1,5 +1,7 @@
 import asyncio
 import binascii
+import random
+import string
 from collections import defaultdict
 import contextlib
 import errno
@@ -75,6 +77,18 @@ PLACEMENT_GROUP_INDEXED_BUNDLED_RESOURCE_PATTERN = re.compile(
     r"(.+)_group_(\d+)_([0-9a-zA-Z]+)"
 )
 PLACEMENT_GROUP_WILDCARD_RESOURCE_PATTERN = re.compile(r"(.+)_group_([0-9a-zA-Z]+)")
+
+
+# Match the standard alphabet used for UUIDs.
+RANDOM_STRING_ALPHABET = string.ascii_lowercase + string.digits
+
+
+def get_random_alphanumeric_string(length: int):
+    """Generates random string of length consisting exclusively of
+    - Lower-case ASCII chars
+    - Digits
+    """
+    return "".join(random.choices(RANDOM_STRING_ALPHABET, k=length))
 
 
 def get_user_temp_dir():
