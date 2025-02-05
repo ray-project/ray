18ffd9758c9b86613509d45fb12a95fe657e5a1b
Sven Mika <svenmika1977@gmail.com>
2025-02-05 16:00:33 +0100

    [RLlib] APPO/IMPALA, allow CPUs AND GPUs per Learner at same time and fix aggr actor GPUs. (#1396)

--- rllib/extensions/algorithm_base.py
+++ rllib/extensions/algorithm_base.py
@@ -17,17 +17,20 @@ class AlgorithmBase:
         # TODO (sven): Add this assert, once rayturbo uses the latest master,
         #  which does not call this method anymore when `num_learners=0`.
         # assert config.num_learners == 0
-        _num_agg = getattr(config, "num_aggregator_actors_per_learner", 0)
+        num_cpus_per_learner = (
+            config.num_cpus_per_learner
+            if config.num_cpus_per_learner != "auto"
+            else 0
+            if config.num_gpus_per_learner > 0
+            else 1
+        )
 
         if config.num_learners > 0:
             per_learner = {
                 "CPU": (
-                    config.num_cpus_per_learner
-                    if config.num_gpus_per_learner == 0
-                    else 0
-                )
-                + config.num_aggregator_actors_per_learner,
-                "GPU": max(0, config.num_gpus_per_learner),
+                    num_cpus_per_learner + config.num_aggregator_actors_per_learner
+                ),
+                "GPU": config.num_gpus_per_learner,
             }
 
             return [per_learner] * config.num_learners
@@ -40,7 +43,7 @@ class AlgorithmBase:
                     # sampling and training is not done concurrently when local is
                     # used, so pick the max.
                     "CPU": max(
-                        config.num_cpus_per_learner,
+                        num_cpus_per_learner,
                         config.num_cpus_for_main_process,
                     )
                     + config.num_aggregator_actors_per_learner,
