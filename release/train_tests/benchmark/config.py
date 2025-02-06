b819ab80de73f4378a8d57d483487e4a26ba3647
Justin Yu <justinvyu@anyscale.com>
2025-02-05 16:37:48 -0800

    [train] Add training + data ingestion benchmark fixtures + image classification example (#50019)
    Add a release Ray Train + Ray Data training ingest benchmark release
    test with 2 variants, one that skips training and just consumes batches
    as quickly as possible, and another that performs training and
    validation.
    
    The difference between this release test and the existing Ray Data
    release tests is that these training benchmarks are aimed to reflect
    real world usage with common, realistic training tasks and their
    relevant preprocessing steps.
    
    This first PR adds an image classification training task with `resnet50`
    (~25M params) and Imagenet (~150GB) that is similar to the existing
    `multi_node_train_benchmark.py` script. This PR makes a few
    simplifications and packages the training script and perf metric
    collection in a few pluggable interfaces. Followups will continue
    building on this release test fixture and add more training benchmarks
    such as recsys model training and xgboost training.
    
    ---------
    
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

---
+++ release/train_tests/benchmark/config.py
@@ -0,0 +1,32 @@
+import enum
+
+import pydantic
+
+
+class DataloaderType(enum.Enum):
+    RAY_DATA = "ray_data"
+    MOCK = "mock"
+
+
+class BenchmarkConfig(pydantic.BaseModel):
+    # ScalingConfig
+    num_workers: int = 1
+
+    # Model
+    task: str = "image_classification"
+
+    # Data
+    dataloader_type: DataloaderType = DataloaderType.RAY_DATA
+
+    # Training
+    num_epochs: int = 1
+    skip_train_step: bool = False
+    train_batch_size: int = 32
+
+    # Validation
+    validate_every_n_steps: int = -1
+    skip_validation_at_epoch_end: bool = False
+    validation_batch_size: int = 256
+
+    # Logging
+    log_metrics_every_n_steps: int = 512
