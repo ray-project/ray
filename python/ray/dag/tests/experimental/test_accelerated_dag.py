3da9ce4d6e566536ca404aadddd7a2eced9b546a
Rui Qiao <ruisearch42@gmail.com>
2025-02-11 12:14:09 -0800

    up
    Signed-off-by: Rui Qiao <ruisearch42@gmail.com>

--- python/ray/dag/tests/experimental/test_accelerated_dag.py
+++ python/ray/dag/tests/experimental/test_accelerated_dag.py
@@ -1130,16 +1130,29 @@ def test_get_with_zero_timeout(ray_start_regular):
 def test_submit_timeout(ray_start_regular):
     a = Actor.remote(0)
     with InputNode() as inp:
+        # By default, shared memory channel is buffered
+        # To test submission timeout, use with_tensor_transport()
+        # for a non-buffered channel
+        inp = inp.with_tensor_transport()
         dag = a.sleep.bind(inp)
 
-    compiled_dag = dag.experimental_compile(_max_inflight_executions=1)
-    ref = compiled_dag.execute(3, _submit_timeout=5)
-    ray.get(ref)
+    refs = []
+    compiled_dag = dag.experimental_compile()
+    # No timeout, the object will stay at actor.sleep() for 5 seconds
+    refs.append(compiled_dag.execute(5, _submit_timeout=1))
+    # No timeout, the object will immediately enter DAG input channel
+    refs.append(compiled_dag.execute(3, _submit_timeout=1))
 
-    # with pytest.raises(RayChannelTimeoutError, match="submit timed out"):
-    ref = compiled_dag.execute(3, _submit_timeout=1)
-    # compiled_dag.execute(3, _submit_timeout=1)
-    # compiled_dag.execute(3, _submit_timeout=1)
+    with pytest.raises(
+        RayChannelTimeoutError,
+        match="Timed out after 1 seconds. "
+        "If the execution is expected to take a long time, "
+        "increase _submit_timeout or RAY_CGRAPH_submit_timeout. "
+        "Otherwise, this may indicate that execution is hanging.",
+    ):
+        # Timeout, the object submission is blocked
+        ref = compiled_dag.execute(3, _submit_timeout=1)
+        refs.append(ref)
 
 
 def test_dag_exception_basic(ray_start_regular, capsys):
