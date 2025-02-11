e5249e2392393ace6f2571cae2441248855f1ba7
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-02-11 17:45:20 -0600

    WIP
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/tests/test_telemetry_serve_used.py
+++ python/ray/serve/tests/test_telemetry_serve_used.py
@@ -6,10 +6,12 @@ import ray
 import ray._private.usage.usage_lib as ray_usage_lib
 from ray import serve
 
+
 def _is_serve_used() -> bool:
     return "serve" in ray_usage_lib.get_library_usages_to_report(
         ray.experimental.internal_kv.internal_kv_get_gcs_client()
     )
+
 
 @pytest.fixture
 def reset_usage_lib(ray_shutdown):
@@ -20,6 +22,7 @@ def test_serve_start(reset_usage_lib):
     assert not _is_serve_used()
     serve.start()
     assert _is_serve_used()
+
 
 def test_serve_run(reset_usage_lib):
     assert not _is_serve_used()
