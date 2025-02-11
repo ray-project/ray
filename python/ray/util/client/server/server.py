bbbe729099b59eb7b3ba0a97d4f3b0c0f6e239f2
Philipp Moritz <pcmoritz@gmail.com>
2025-02-11 13:49:29 -0800

    lint
    Signed-off-by: Philipp Moritz <pcmoritz@gmail.com>

--- python/ray/util/client/server/server.py
+++ python/ray/util/client/server/server.py
@@ -892,7 +892,11 @@ def main():
     )
 
     hostport = "%s:%d" % (args.host, args.port)
-    args_str = str(args).replace(args.redis_password, "****") if args.redis_password else args_str
+    args_str = (
+        str(args).replace(args.redis_password, "****")
+        if args.redis_password
+        else args_str
+    )
     logger.info(f"Starting Ray Client server on {hostport}, args {args_str}")
     if args.mode == "proxy":
         server = serve_proxier(
