699d88ac477ba429e2a9d24644c7d76fa29730e6
Ryan O'Leary <ryanaoleary@google.com>
2025-02-11 06:42:24 +0000

    Use running instances to rate-limit upscaling
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>

--- python/ray/autoscaler/_private/resource_demand_scheduler.py
+++ python/ray/autoscaler/_private/resource_demand_scheduler.py
@@ -45,9 +45,6 @@ from ray.core.generated.common_pb2 import PlacementStrategy
 
 logger = logging.getLogger(__name__)
 
-# The minimum number of nodes to launch concurrently.
-UPSCALING_INITIAL_NUM_NODES = 5
-
 NodeResources = ResourceDict
 ResourceDemands = List[ResourceDict]
 
@@ -433,7 +430,7 @@ class ResourceDemandScheduler:
             # Enforce here max allowed pending nodes to be frac of total
             # running nodes.
             max_allowed_pending_nodes = max(
-                UPSCALING_INITIAL_NUM_NODES,
+                AUTOSCALER_UPSCALING_INITIAL_NUM_NODES,
                 int(self.upscaling_speed * max(running_nodes[node_type], 1)),
             )
             total_pending_nodes = (
