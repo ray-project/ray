0c7bdefec32ad411cfc24c28f7b9f41d85c43ec9
dentiny <dentinyhao@gmail.com>
2025-02-12 23:40:01 +0000

    create env with nil job
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- src/ray/raylet/worker_pool.cc
+++ src/ray/raylet/worker_pool.cc
@@ -1498,7 +1498,8 @@ void WorkerPool::PrestartWorkersInternal(const TaskSpecification &task_spec,
     GetOrCreateRuntimeEnv(
         task_spec.SerializedRuntimeEnv(),
         task_spec.RuntimeEnvConfig(),
-        task_spec.JobId(),
+        // task_spec.JobId(),
+        JobID::Nil(),
         [this, task_spec = task_spec](bool successful,
                                       const std::string &serialized_runtime_env_context,
                                       const std::string &setup_error_message) {
