efa3c086389bb364ea2aef7c4dbe4ea04b78e41b
Alexey Kudinkin <ak@anyscale.com>
2025-02-12 16:34:21 -0800

    `lint`
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/data/block.py
+++ python/ray/data/block.py
@@ -512,7 +512,9 @@ class BlockAccessor:
         raise NotImplementedError
 
 
-def _get_group_boundaries_sorted(block: Block, key: Union[str, List[str]]) -> np.ndarray:
+def _get_group_boundaries_sorted(
+    block: Block, key: Union[str, List[str]]
+) -> np.ndarray:
     """
     NOTE: THIS METHOD ASSUMES THAT PROVIDED BLOCK IS ALREADY SORTED
 
