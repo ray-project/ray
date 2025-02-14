4fc888e41a828ffca1f52edbef7b854970b64fd8
Jimmy Xie <rxie@figma.com>
2025-02-14 09:42:21 -0500

    improve transaction support
    Signed-off-by: Jimmy Xie <rxie@figma.com>
    
    improve transaction support
    
    Signed-off-by: Jimmy Xie <rxie@figma.com>
    
    improve transaction support
    
    Signed-off-by: Jimmy Xie <rxie@figma.com>
    
    improve transaction support
    
    Signed-off-by: Jimmy Xie <rxie@figma.com>
    
    improve transaction support
    
    Signed-off-by: Jimmy Xie <rxie@figma.com>

--- python/ray/data/_internal/datasource/iceberg_datasink.py
+++ python/ray/data/_internal/datasource/iceberg_datasink.py
@@ -14,6 +14,7 @@ import uuid
 
 if TYPE_CHECKING:
     from pyiceberg.catalog import Catalog
+    from pyiceberg.io import FileIO
     from pyiceberg.manifest import DataFile
     from pyiceberg.table import Table
 
@@ -56,48 +57,69 @@ class IcebergDatasink(Datasink[List["DataFile"]]):
         else:
             self._catalog_name = "default"
         self._uuid = None
-        self._table = None
-        self._datasink = None
+
+        self._io = None
+        self._table_metadata = None
 
     def _get_catalog(self) -> "Catalog":
         from pyiceberg import catalog
 
         return catalog.load_catalog(self._catalog_name, **self._catalog_kwargs)
 
-    @property
-    def table(self) -> "Table":
-        """
-        Return the table reference from the catalog
-        """
-        if self._table is None:
-            catalog = self._get_catalog()
-            self._table = catalog.load_table(self.table_identifier)
-        return self._table
-
     def on_write_start(self) -> None:
         """Prepare for the transaction"""
+        catalog = self._get_catalog()
+        table = catalog.load_table(self.table_identifier)
+        self._txn = table.transaction()
+        self._io = table.io
+        self._table_metadata = self._txn.table_metadata
         self._uuid = uuid.uuid4()
 
+        if unsupported_partitions := [
+            field
+            for field in self._txn.table_metadata.spec().fields
+            if not field.transform.supports_pyarrow_transform
+        ]:
+            raise ValueError(
+                f"Not all partition types are supported for writes. Following partitions cannot be written using pyarrow: {unsupported_partitions}."
+            )
+
     def write(self, blocks: Iterable[Block], ctx: TaskContext):
-        from pyiceberg.io.pyarrow import _dataframe_to_data_files
+        from pyiceberg.io.pyarrow import (
+            _check_pyarrow_schema_compatible,
+            _dataframe_to_data_files,
+        )
+        from pyiceberg.table import DOWNCAST_NS_TIMESTAMP_TO_US_ON_WRITE
+        from pyiceberg.utils.config import Config
 
         data_files_list = []
         for block in blocks:
             pa_table = BlockAccessor.for_block(block).to_arrow()
+
+            downcast_ns_timestamp_to_us = (
+                Config().get_bool(DOWNCAST_NS_TIMESTAMP_TO_US_ON_WRITE) or False
+            )
+            _check_pyarrow_schema_compatible(
+                self._table_metadata.schema(),
+                provided_schema=pa_table.schema,
+                downcast_ns_timestamp_to_us=downcast_ns_timestamp_to_us,
+            )
+
             data_files = _dataframe_to_data_files(
-                self.table.metadata, pa_table, self.table.io, self._uuid
+                self._table_metadata, pa_table, self._io, self._uuid
             )
             data_files_list.extend(data_files)
 
         return data_files_list
 
     def on_write_complete(self, write_result: WriteResult[List["DataFile"]]):
-        from pyiceberg.io.pyarrow import _FastAppendFiles, Operation
+        from pyiceberg.table.update.snapshot import _FastAppendFiles
+        from pyiceberg.table.snapshots import Operation
 
         fast_append = _FastAppendFiles(
             operation=Operation.APPEND,
-            transaction=self._table._transaction,
-            io=self._table.io,
+            transaction=self._txn,
+            io=self._io,
             snapshot_properties=self._snapshot_properties,
         )
 
