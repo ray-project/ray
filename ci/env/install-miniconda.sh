3c767637515b131dfd3bed079a861b6199cf561b
Philipp Moritz <pcmoritz@gmail.com>
2025-02-15 15:05:18 -0800

    update

--- ci/env/install-miniconda.sh
+++ ci/env/install-miniconda.sh
@@ -87,7 +87,7 @@ install_miniconda() {
       set +x
       echo "Updating Anaconda Python ${python_version} to ${PYTHON}..."
       "${WORKSPACE_DIR}"/ci/suppress_output conda create -n py python="${PYTHON}" -c conda-forge -q -y
-      "${WORKSPACE_DIR}"/ci/suppress_output conda init
+      "${WORKSPACE_DIR}"/ci/suppress_output conda init --all
       "${WORKSPACE_DIR}"/ci/suppress_output conda activate py
     )
   elif [ "${MINIMAL_INSTALL-}" = "1" ]; then  # Reset environment
