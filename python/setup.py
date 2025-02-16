df87d16176c0a5d9edd85c6c95b60765e1bccbfa
Gene Su <e870252314@gmail.com>
2025-02-16 00:34:18 -0800

    add asyncache dependency
    Signed-off-by: Gene Su <e870252314@gmail.com>

--- python/setup.py
+++ python/setup.py
@@ -361,7 +361,7 @@ if setup_spec.type == SetupType.RAY:
     #
     # Keep this in sync with python/requirements/llm/llm-requirements.txt
     #
-    setup_spec.extras["llm"] = list(set(["vllm>=0.7.2"] + setup_spec.extras["data"]))
+    setup_spec.extras["llm"] = list(set(["vllm>=0.7.2", "asyncache"] + setup_spec.extras["data"]))
 
 # These are the main dependencies for users of ray. This list
 # should be carefully curated. If you change it, please reflect
