0ae54ad4895e2b3d7aa8b717c6f356d09e86aa5f
Gene Su <e870252314@gmail.com>
2025-02-16 20:40:20 -0800

    add test for vllm engine
    Signed-off-by: Gene Su <e870252314@gmail.com>

--- python/ray/llm/tests/serve/builders/test_application_builders.py
+++ python/ray/llm/tests/serve/builders/test_application_builders.py
@@ -2,10 +2,7 @@ import pytest
 from ray import serve
 
 from ray.llm._internal.serve.configs.server_models import (
-    LLMConfig,
-    ModelLoadingConfig,
     LLMServingArgs,
-    DeploymentConfig,
 )
 from ray.llm._internal.serve.builders.application_builders import (
     build_openai_app,
@@ -18,29 +15,6 @@ import tempfile
 import signal
 
 from ray._private.test_utils import wait_for_condition
-from ray.llm._internal.serve.configs.constants import RAYLLM_VLLM_ENGINE_CLS_ENV
-
-
-@pytest.fixture
-def use_mock_vllm_engine(monkeypatch):
-    monkeypatch.setenv(
-        RAYLLM_VLLM_ENGINE_CLS_ENV,
-        "ray.llm._internal.serve.deployments.llm.vllm.vllm_engine.MockVLLMEngine",
-    )
-    yield
-
-
-@pytest.fixture
-def llm_config(download_model_ckpt):
-    yield LLMConfig(
-        model_loading_config=ModelLoadingConfig(
-            model_id=download_model_ckpt,
-        ),
-        accelerator_type="L4",
-        deployment_config=DeploymentConfig(
-            ray_actor_options={"resources": {"mock_resource": 0}},
-        ),
-    )
 
 
 @pytest.fixture
--- python/ray/llm/tests/serve/conftest.py
+++ python/ray/llm/tests/serve/conftest.py
@@ -1,6 +1,12 @@
 import ray
 from ray import serve
 import pytest
+from ray.llm._internal.serve.configs.constants import RAYLLM_VLLM_ENGINE_CLS_ENV
+from ray.llm._internal.serve.configs.server_models import (
+    LLMConfig,
+    ModelLoadingConfig,
+    DeploymentConfig,
+)
 
 
 @pytest.fixture
@@ -12,3 +18,25 @@ def shutdown_ray_and_serve():
     serve.shutdown()
     if ray.is_initialized():
         ray.shutdown()
+
+
+@pytest.fixture
+def use_mock_vllm_engine(monkeypatch):
+    monkeypatch.setenv(
+        RAYLLM_VLLM_ENGINE_CLS_ENV,
+        "ray.llm._internal.serve.deployments.llm.vllm.vllm_engine.MockVLLMEngine",
+    )
+    yield
+
+
+@pytest.fixture
+def llm_config(download_model_ckpt):
+    yield LLMConfig(
+        model_loading_config=ModelLoadingConfig(
+            model_id=download_model_ckpt,
+        ),
+        accelerator_type="L4",
+        deployment_config=DeploymentConfig(
+            ray_actor_options={"resources": {"mock_resource": 0}},
+        ),
+    )
