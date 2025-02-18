b2a970b44326076a003475a41f87174cf299c7b3
Philipp Moritz <pcmoritz@gmail.com>
2025-02-18 13:42:04 -0800

    update

--- ci/env/install-uv.sh
+++ ci/env/install-uv.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 curl -LsSf https://astral.sh/uv/0.6.1/install.sh | env UV_UNMANAGED_INSTALL="$HOME/.local/bin" sh
-$HOME/.local/bin/uv python install "${PYTHON}" --default --preview
-$HOME/.local/bin/uv tool update-shell
+$HOME/.local/bin/uv python install "${PYTHON_VERSION}" --default --preview --bin "/usr/local/bin"
