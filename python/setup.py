8e4df550b40fe098cd62cfac6c9efe28e0192e85
Gene Su <e870252314@gmail.com>
2025-02-17 21:59:06 -0800

    add boto3
    Signed-off-by: Gene Su <e870252314@gmail.com>

--- python/setup.py
+++ python/setup.py
@@ -363,7 +363,13 @@ if setup_spec.type == SetupType.RAY:
     #
     setup_spec.extras["llm"] = list(
         set(
-            ["vllm>=0.7.2", "asyncache==0.3.1", "jsonref==1.1.0", "botocore"]
+            [
+                "vllm>=0.7.2",
+                "asyncache==0.3.1",
+                "jsonref==1.1.0",
+                "aiobotocore",
+                "boto3",
+            ]
             + setup_spec.extras["data"]
         )
     )
