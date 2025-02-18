dd45bd3ce6f7f30fd6763710ef51c7df81b60a85
dayshah <dhyey2019@gmail.com>
2025-02-17 20:45:26 -0800

    build cpp if java or cpp
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- python/setup.py
+++ python/setup.py
@@ -700,7 +700,7 @@ def pip_run(build_ext):
     if SKIP_BAZEL_BUILD:
         build(False, False, False)
     else:
-        build(True, BUILD_JAVA, BUILD_CPP)
+        build(True, BUILD_JAVA, BUILD_CPP or BUILD_JAVA)
 
     if setup_spec.type == SetupType.RAY:
         setup_spec.files_to_include += ray_files
