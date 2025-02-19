361fd5a2f21e13feb3cc2c07ddd7d090231316d0
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2025-02-19 10:02:36 -0800

    lint
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- python/ray/llm/_internal/serve/configs/server_models.py
+++ python/ray/llm/_internal/serve/configs/server_models.py
@@ -434,7 +434,6 @@ class LLMConfig(BaseModelExtended):
             raise ValueError(f"Invalid deployment config: {value}") from e
         return value
 
-
     def ray_accelerator_type(self) -> str:
         """Converts the accelerator type to the Ray Core format."""
 
