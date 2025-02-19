179af9728165775c21585e519df54536c35919a8
Gene Der Su <e870252314@gmail.com>
2025-02-19 00:30:28 -0800

    [ray.serve.llm] Fix setting up AutoProcessor (#50715)
    Signed-off-by: Gene Su <e870252314@gmail.com>

--- python/ray/llm/_internal/serve/deployments/utils/node_initialization_utils.py
+++ python/ray/llm/_internal/serve/deployments/utils/node_initialization_utils.py
@@ -233,7 +233,10 @@ async def initialize_node(llm_config: LLMConfig) -> InitializeNodeOutput:
             download_extra_files=True,
         )
 
-    llm_config.apply_checkpoint_info(engine_config.actual_hf_model_id)
+    llm_config.apply_checkpoint_info(
+        engine_config.actual_hf_model_id,
+        trust_remote_code=engine_config.trust_remote_code,
+    )
 
     return InitializeNodeOutput(
         placement_group=pg, runtime_env=runtime_env, extra_init_kwargs=extra_init_kwargs
@@ -264,12 +267,6 @@ def _initialize_local_node(
         logger.info(f"Downloading the tokenizer for {engine_config.actual_hf_model_id}")
 
     _ = AutoTokenizer.from_pretrained(
-        engine_config.actual_hf_model_id,
-        trust_remote_code=engine_config.trust_remote_code,
-    )
-    prompt_format = engine_config.prompt_format
-    # Note (genesu): The prompt format is always loaded from HF.
-    prompt_format.set_processor(
         engine_config.actual_hf_model_id,
         trust_remote_code=engine_config.trust_remote_code,
     )
