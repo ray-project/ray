6b373e99e1dcbe28629e064d8432519d07ebe51c
Richard Liaw <rliaw@berkeley.edu>
2025-02-18 18:49:51 -0800

    [data/llm/docs] Initial draft of user guide for Data LLM APIs (#50674)
    ## Why are these changes needed?
    
    Adds user guide and link-ins for Ray Data documentation.
    
    This is part of the #50639 thread of work.
    
    This is based on #50494 
    
    cc @comaniac @gvspraveen @kouroshHakha  
    
    ## Related issue number
    
    <!-- For example: "Closes #1234" -->
    
    ## Checks
    
    - [ ] I've signed off every commit(by using the -s flag, i.e., `git
    commit -s`) in this PR.
    - [ ] I've run `scripts/format.sh` to lint the changes in this PR.
    - [ ] I've included any doc changes needed for
    https://docs.ray.io/en/master/.
    - [ ] I've added any new APIs to the API Reference. For example, if I
    added a
    method in Tune, I've added it in `doc/source/tune/api/` under the
               corresponding `.rst` file.
    - [ ] I've made sure the tests are passing. Note that there might be a
    few flaky tests, see the recent failures at https://flakey-tests.ray.io/
    - Testing Strategy
       - [ ] Unit tests
       - [ ] Release tests
       - [ ] This PR is not tested :(
    
    ---------
    
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>
    Co-authored-by: Cody Yu <hao.yu.cody@gmail.com>

--- python/ray/data/llm.py
+++ python/ray/data/llm.py
@@ -20,6 +20,12 @@ class ProcessorConfig(_ProcessorConfig):
 class HttpRequestProcessorConfig(_HttpRequestProcessorConfig):
     """The configuration for the HTTP request processor.
 
+    Args:
+        batch_size: The batch size to send to the HTTP request.
+        url: The URL to send the HTTP request to.
+        headers: The headers to send with the HTTP request.
+        concurrency: The number of concurrent requests to send.
+
     Examples:
         .. testcode::
             :skipif: True
@@ -61,7 +67,28 @@ class HttpRequestProcessorConfig(_HttpRequestProcessorConfig):
 class vLLMEngineProcessorConfig(_vLLMEngineProcessorConfig):
     """The configuration for the vLLM engine processor.
 
+    Args:
+        model: The model to use for the vLLM engine.
+        engine_kwargs: The kwargs to pass to the vLLM engine.
+        task_type: The task type to use. If not specified, will use 'generate' by default.
+        runtime_env: The runtime environment to use for the vLLM engine.
+        max_pending_requests: The maximum number of pending requests. If not specified,
+            will use the default value from the vLLM engine.
+        max_concurrent_batches: The maximum number of concurrent batches in the engine.
+            This is to overlap the batch processing to avoid the tail latency of
+            each batch. The default value may not be optimal when the batch size
+            or the batch processing latency is too small, but it should be good
+            enough for batch size >= 64.
+        apply_chat_template: Whether to apply chat template.
+        chat_template: The chat template to use. This is usually not needed if the
+            model checkpoint already contains the chat template.
+        tokenize: Whether to tokenize the input before passing it to the vLLM engine.
+            If not, vLLM will tokenize the prompt in the engine.
+        detokenize: Whether to detokenize the output.
+        has_image: Whether the input messages have images.
+
     Examples:
+
         .. testcode::
             :skipif: True
 
@@ -75,7 +102,6 @@ class vLLMEngineProcessorConfig(_vLLMEngineProcessorConfig):
                     enable_chunked_prefill=True,
                     max_num_batched_tokens=4096,
                 ),
-                accelerator_type="L4",
                 concurrency=1,
                 batch_size=64,
             )
@@ -124,6 +150,47 @@ def build_llm_processor(
 
     Returns:
         The built processor.
+
+    Example:
+        .. testcode::
+            :skipif: True
+
+            import ray
+            from ray.data.llm import vLLMEngineProcessorConfig, build_llm_processor
+
+            config = vLLMEngineProcessorConfig(
+                model="meta-llama/Meta-Llama-3.1-8B-Instruct",
+                engine_kwargs=dict(
+                    enable_prefix_caching=True,
+                    enable_chunked_prefill=True,
+                    max_num_batched_tokens=4096,
+                ),
+                concurrency=1,
+                batch_size=64,
+            )
+
+            processor = build_llm_processor(
+                config,
+                preprocess=lambda row: dict(
+                    messages=[
+                        {"role": "system", "content": "You are a calculator"},
+                        {"role": "user", "content": f"{row['id']} ** 3 = ?"},
+                    ],
+                    sampling_params=dict(
+                        temperature=0.3,
+                        max_tokens=20,
+                        detokenize=False,
+                    ),
+                ),
+                postprocess=lambda row: dict(
+                    resp=row["generated_text"],
+                ),
+            )
+
+            ds = ray.data.range(300)
+            ds = processor(ds)
+            for row in ds.take_all():
+                print(row)
     """
     from ray.llm._internal.batch.processor import ProcessorBuilder
 
