5b48b6f17e263bb34a422f7e8251cd6264a3a691
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2025-02-19 16:37:53 -0800

    testing adding them to __init__.py
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

---
+++ python/ray/serve/llm/__init__.py
@@ -0,0 +1,11 @@
+
+from ray.serve.llm.deployments import (
+    VLLMDeploymentImpl,
+    LLMModelRouterDeploymentImpl,
+)
+
+
+__all__ = [
+    "VLLMDeploymentImpl",
+    "LLMModelRouterDeploymentImpl",
+]
