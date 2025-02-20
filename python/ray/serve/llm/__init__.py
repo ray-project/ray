bfcbe8ccbe3b782973932913fb106ac882242f57
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2025-02-19 16:43:12 -0800

    adding more
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- python/ray/serve/llm/__init__.py
+++ python/ray/serve/llm/__init__.py
@@ -3,8 +3,31 @@ from ray.serve.llm.deployments import (
     LLMModelRouterDeploymentImpl,
 )
 
+from ray.serve.llm.configs import (
+    LLMConfig,
+    LLMServingArgs,
+    ModelLoadingConfig,
+    S3MirrorConfig,
+    S3AWSCredentials,
+    GCSMirrorConfig,
+    LoraConfig,
+)
+
+from ray.serve.llm.builders import (
+    build_vllm_deployment,
+    build_openai_app,
+)
 
 __all__ = [
     "VLLMDeploymentImpl",
     "LLMModelRouterDeploymentImpl",
+    "LLMConfig",
+    "LLMServingArgs",
+    "ModelLoadingConfig",
+    "S3MirrorConfig",
+    "S3AWSCredentials",
+    "GCSMirrorConfig",
+    "LoraConfig",
+    "build_vllm_deployment",
+    "build_openai_app",
 ]
