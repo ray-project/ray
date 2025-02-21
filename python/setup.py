c41bf89620967817456e098b9209aa909b2742e1
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2025-02-21 11:50:50 -0800

    [llm] removes unused aiobotocore dependency (#50797)
    not really used anywhere in the code
    
    Signed-off-by: Lonnie Liu <lonnie@anyscale.com>

--- python/setup.py
+++ python/setup.py
@@ -367,7 +367,6 @@ if setup_spec.type == SetupType.RAY:
                 "vllm>=0.7.2",
                 "asyncache>=0.3.1",
                 "jsonref>=1.1.0",
-                "aiobotocore",
                 "boto3",
                 "async_timeout",
             ]
