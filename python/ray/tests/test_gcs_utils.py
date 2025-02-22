3e850a723961f1d6e56dcc6c428ab66738009a0f
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2025-02-21 16:13:36 -0800

    remove asyncio_timeout from dependencies
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- python/ray/tests/test_gcs_utils.py
+++ python/ray/tests/test_gcs_utils.py
@@ -4,7 +4,6 @@ import time
 import signal
 import sys
 import asyncio
-import async_timeout
 
 import pytest
 import ray
@@ -267,7 +266,7 @@ async def test_gcs_aio_client_is_async(ray_start_regular):
     gcs_client = gcs_utils.GcsAioClient(address=gcs_address, nums_reconnect_retry=0)
 
     await gcs_client.internal_kv_put(b"A", b"B", False, b"NS", timeout=2)
-    async with async_timeout.timeout(3):
+    async with asyncio.timeout(3):
         none, result = await asyncio.gather(
             asyncio.sleep(2), gcs_client.internal_kv_get(b"A", b"NS", timeout=2)
         )
