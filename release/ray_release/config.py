abaee9825a4ed1cfa8b0219a68bf99f73d6efac1
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2025-02-22 22:29:47 -0800

    [ci] allow specifying project id for release test (#50836)
    required for running tests now
    
    Signed-off-by: Lonnie Liu <lonnie@anyscale.com>

--- release/ray_release/config.py
+++ release/ray_release/config.py
@@ -31,7 +31,7 @@ DEFAULT_CLOUD_ID = DeferredEnvVar(
 )
 DEFAULT_ANYSCALE_PROJECT = DeferredEnvVar(
     "RELEASE_DEFAULT_PROJECT",
-    "prj_FKRmeV5pA6X72aVscFALNC32",
+    "prj_rw3kzbqsehh4a6klka7j616fgb",
 )
 
 RELEASE_PACKAGE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
@@ -253,3 +253,8 @@ def get_test_cloud_id(test: Test) -> str:
     else:
         cloud_id = cloud_id or str(DEFAULT_CLOUD_ID)
     return cloud_id
+
+
+def get_test_project_id(test: Test) -> str:
+    default_project_id = str(DEFAULT_ANYSCALE_PROJECT)
+    return test.get("cluster", {}).get("project_id", default_project_id)
