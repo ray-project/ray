64b3e8538867ee1fa5f15e88eb599141caf97812
Alexey Kudinkin <ak@anyscale.com>
2025-02-24 17:55:09 -0800

    Revisited `InputDataBuffer` to support blocks fed in via iterator
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/data/_internal/execution/operators/input_data_buffer.py
+++ python/ray/data/_internal/execution/operators/input_data_buffer.py
@@ -1,4 +1,4 @@
-from typing import Callable, List, Optional
+from typing import Callable, List, Optional, Iterator
 
 from ray.data._internal.execution.interfaces import (
     ExecutionOptions,
@@ -6,6 +6,7 @@ from ray.data._internal.execution.interfaces import (
     RefBundle,
 )
 from ray.data._internal.stats import StatsDict
+from ray.data.block import to_stats, BlockStats
 from ray.data.context import DataContext
 
 
@@ -20,12 +21,14 @@ class InputDataBuffer(PhysicalOperator):
         self,
         data_context: DataContext,
         input_data: Optional[List[RefBundle]] = None,
+        input_data_iter: Optional[Iterator[RefBundle]] = None,
         input_data_factory: Optional[Callable[[int], List[RefBundle]]] = None,
         num_output_blocks: Optional[int] = None,
     ):
-        """Create an InputDataBuffer.
+        """Create an InputDataBuffer Operator.
 
         Args:
+            # TODO update
             input_data: The list of bundles to output from this operator.
             input_data_factory: The factory to get input data, if input_data is None.
             num_output_blocks: The number of output blocks. If not specified, progress
@@ -34,39 +37,61 @@ class InputDataBuffer(PhysicalOperator):
         super().__init__("Input", [], data_context, target_max_block_size=None)
         if input_data is not None:
             assert input_data_factory is None
+            assert input_data_iter is None
+
             # Copy the input data to avoid mutating the original list.
             self._input_data = input_data[:]
-            self._is_input_initialized = True
-            self._initialize_metadata()
-        else:
+            self._pre_initialize_metadata()
+
+        elif input_data_iter is not None:
             # Initialize input lazily when execution is started.
-            assert input_data_factory is not None
+            assert input_data is None
+            assert input_data_factory is None
+
+            self._input_data_iter = input_data_iter
+
+        elif input_data_factory is not None:
+            assert input_data is None
+            assert input_data_iter is None
+
             self._input_data_factory = input_data_factory
-            self._is_input_initialized = False
-        self._input_data_index = 0
+
+        self._next_bundle: Optional[RefBundle] = None
+        self._block_stats: List[BlockStats] = []
+
 
     def start(self, options: ExecutionOptions) -> None:
-        if not self._is_input_initialized:
+        if self._input_data_factory is not None:
             self._input_data = self._input_data_factory(
                 self.actual_target_max_block_size
             )
-            self._is_input_initialized = True
-            self._initialize_metadata()
-        # InputDataBuffer does not take inputs from other operators,
-        # so we record input metrics here
-        for bundle in self._input_data:
-            self._metrics.on_input_received(bundle)
+            self._pre_initialize_metadata()
+
+        if self._input_data:
+
+            self._input_data_iter = iter(self._input_data)
+            self._input_data = None
+
         super().start(options)
 
     def has_next(self) -> bool:
-        return self._input_data_index < len(self._input_data)
+        if self._next_bundle is not None:
+            return True
+
+        try:
+            self._next_bundle = next(self._input_data_iter)
+        except StopIteration:
+            self._next_bundle = None
+            return False
+
+        self._metrics.on_input_received(self._next_bundle)
+        # Append fetched block stats
+        self._block_stats.extend(to_stats(self._next_bundle.metadata))
+
+        return True
 
     def _get_next_inner(self) -> RefBundle:
-        # We can't pop the input data. If we do, Ray might garbage collect the block
-        # references, and Ray won't be able to reconstruct downstream objects.
-        bundle = self._input_data[self._input_data_index]
-        self._input_data_index += 1
-        return bundle
+        return self._next_bundle
 
     def get_stats(self) -> StatsDict:
         return {}
@@ -74,16 +99,8 @@ class InputDataBuffer(PhysicalOperator):
     def _add_input_inner(self, refs, input_index) -> None:
         raise ValueError("Inputs are not allowed for this operator.")
 
-    def _initialize_metadata(self):
-        assert self._input_data is not None and self._is_input_initialized
+    def _pre_initialize_metadata(self):
         self._estimated_num_output_bundles = len(self._input_data)
-
-        block_metadata = []
-        for bundle in self._input_data:
-            block_metadata.extend(bundle.metadata)
-        self._stats = {
-            "input": block_metadata,
-        }
 
     def implements_accurate_memory_accounting(self) -> bool:
         return True
