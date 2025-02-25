09961dddb97b9df90c9bfac791e125bfba0265e3
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2025-02-24 20:57:51 -0800

    [ci] convert the if-else change in cond testing script into a config file (#50776)
    and adding tests for parsing.
    
    Signed-off-by: Lonnie Liu <lonnie@anyscale.com>

--- ci/pipeline/test_conditional_testing.py
+++ ci/pipeline/test_conditional_testing.py
@@ -8,6 +8,8 @@ import runfiles
 import pytest
 import yaml
 
+from ci.pipeline.determine_tests_to_run import TagRule, TagRuleSet
+
 _REPO_NAME = "com_github_ray_project_ray"
 _runfiles = runfiles.Create()
 
@@ -154,6 +156,46 @@ def test_conditional_testing_pull_request():
 
             want = test_case.tags
             assert want == set(tags), f"file {test_case.file}, want {want}, got {tags}"
+
+
+def test_tag_rule():
+    rule = TagRule(
+        tags=["hit"],
+        dirs=["fancy"],
+        files=["file.txt"],
+        patterns=["python/*.py"],
+    )
+
+    assert rule.match("fancy")
+    assert rule.match("fancy/a.md")
+    assert rule.match("python/a.py")
+    assert rule.match("python/subdir/a.py")
+    assert rule.match("file.txt")
+    assert not rule.match("fancy_file.txt")
+    assert not rule.match("python/a.txt")
+
+    assert rule.match_tags("fancy") == ({"hit"}, True)
+    assert rule.match_tags("not_match") == (set(), False)
+
+    skip_rule = TagRule(tags=[], files=["skip.txt"])
+    assert skip_rule.match("skip.txt")
+    assert skip_rule.match_tags("skip.txt") == (set(), True)
+    assert skip_rule.match_tags("not_match") == (set(), False)
+
+
+def test_tag_rule_set():
+    rule_set = TagRuleSet("\n".join(["#comment", "fancy/ # a dir", "@fancy"]))
+    assert rule_set.match_tags("fancy/file.txt") == ({"fancy"}, True)
+
+    rule_set = TagRuleSet(
+        "\n".join(["fancy/ #dir", "@fancy", ";", "\t\t  ", "foobar.txt", "@foobar"])
+    )
+    assert rule_set.match_tags("fancy/file.txt") == ({"fancy"}, True)
+    assert rule_set.match_tags("foobar.txt") == ({"foobar"}, True)
+    assert rule_set.match_tags("not_a_match") == (set(), False)
+
+    rule_set = TagRuleSet("")
+    assert rule_set.match_tags("anything") == (set(), False)
 
 
 if __name__ == "__main__":
