6903a0df28d329bc0523db7daa3800246e919cbd
Matthew Deng <matt@anyscale.com>
2025-02-27 15:15:32 -0800

    [train] fix serialize import test for py3.12
    Signed-off-by: Matthew Deng <matt@anyscale.com>

--- python/ray/train/v2/tests/test_v2_api.py
+++ python/ray/train/v2/tests/test_v2_api.py
@@ -58,9 +58,13 @@ def test_serialized_imports(ray_start_4_cpus):
     """Check that captured imports are deserialized properly without circular imports."""
 
     from ray.train.torch import TorchTrainer
-    from ray.train.tensorflow import TensorflowTrainer
     from ray.train.xgboost import XGBoostTrainer
     from ray.train.lightgbm import LightGBMTrainer
+
+    if sys.version_info < (3, 12):
+        from ray.train.tensorflow import TensorflowTrainer
+    else:
+        TensorflowTrainer = None
 
     @ray.remote
     def dummy_task():
