e50607a02cc58be465041c0d68c6453de421bb95
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2025-02-28 12:21:50 -0800

    [ci] enable conditional testing on rayturbo CI (#1509)
    only running related tests
    
    ---------
    
    Signed-off-by: Lonnie Liu <lonnie@anyscale.com>

--- anyscale/ci/buildkite/boot.sh
+++ anyscale/ci/buildkite/boot.sh
@@ -3,8 +3,9 @@
 set -euo pipefail
 
 curl -sfL "https://raw.githubusercontent.com/ray-project/rayci/stable/run_rayci.sh" > /tmp/run_rayci.sh
-if [[ "$BUILDKITE_PIPELINE_ID" == "019490b9-2fc3-4200-81f8-9bfa7fc383ab" ]]; then # new rayturbo pipeline
-    /bin/bash /tmp/run_rayci.sh -upload -config anyscale/ci/config_2025.yaml
-else # old rayturbo pipeline
-    /bin/bash /tmp/run_rayci.sh -upload -config anyscale/ci/config.yaml
+
+if [[ "${BUILDKITE_BRANCH:-}" == "master" || "${BUILDKITE_BRANCH:-}" =~ ^releases/.* ]]; then
+    /bin/bash /tmp/run_rayci.sh -upload -config anyscale/ci/config-postmerge.yaml
+else
+    /bin/bash /tmp/run_rayci.sh -upload -config anyscale/ci/config-premerge.yaml
 fi
