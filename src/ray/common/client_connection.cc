96251f8c201766b1d6f170afb96db0e3e7438232
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-02-28 12:35:18 -0600

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/common/client_connection.cc
+++ src/ray/common/client_connection.cc
@@ -620,7 +620,7 @@ std::vector<bool> CheckForClientDisconnects(
     poll_fds[i] = {conns[i]->GetNativeHandle(), /*events=*/0, /*revents=*/0};
   }
 
-  int errno = poll(poll_fds.data(), poll_fds.size(), /*timeout=*/ 0);
+  int errno = poll(poll_fds.data(), poll_fds.size(), /*timeout=*/0);
   if (errno > 0) {
     for (size_t i = 0; i < conns.size(); ++i) {
       // Check if a POLLHUP event occurred on the FD.
