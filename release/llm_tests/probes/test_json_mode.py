3b08aa52bf82ea900cbd2d99f83af72bb3055167
Gene Su <e870252314@gmail.com>
2025-02-28 09:43:10 -0800

    add test for tp2 and lora
    Signed-off-by: Gene Su <e870252314@gmail.com>

--- release/llm_tests/probes/test_json_mode.py
+++ release/llm_tests/probes/test_json_mode.py
@@ -144,7 +144,6 @@ async def test_json_mode(
     stream: bool,
     openai_async_client,
 ):
-    assert False, "test failure case"
     print(
         f"Sending json mode request to {model} ({test_id}) with {n_concurrent_requests} concurrent requests"
     )
