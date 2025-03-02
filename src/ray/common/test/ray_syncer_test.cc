45b16a30dbeeb6f874ac76091cafcbbd324230d9
Drice1999 <97437268+Drice1999@users.noreply.github.com>
2025-03-02 21:05:16 +0800

    Fix mock dependency for mock/ray/common/ray_syncer/ray_syncer.h (#50930)
    https://github.com/ray-project/ray/blob/master/src/mock/ray/common/ray_syncer/ray_syncer.h
    should have the dependency of and `ray_syncer`,
    `ray_syncer_bidi_reactor` and `ray_syncer_bidi_reactor_base` but somehow
    it doesn't...
    
    As a result, we have to include ray_syncer.h before mock_ray_syncer.h
    and avoid linter reorder via clang-format off, otherwise build will
    break.
    
    Signed-off-by: Drice1999 <chenxh267@gmail.com>

--- src/ray/common/test/ray_syncer_test.cc
+++ src/ray/common/test/ray_syncer_test.cc
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include "mock/ray/common/ray_syncer/ray_syncer.h"
 
 #include <gmock/gmock.h>
 #include <google/protobuf/util/json_util.h>
@@ -26,14 +27,11 @@
 #include <chrono>
 #include <sstream>
 
-// clang-format off
 #include "ray/common/ray_syncer/node_state.h"
 #include "ray/common/ray_syncer/ray_syncer.h"
 #include "ray/common/ray_syncer/ray_syncer_client.h"
 #include "ray/common/ray_syncer/ray_syncer_server.h"
 #include "ray/rpc/grpc_server.h"
-#include "mock/ray/common/ray_syncer/ray_syncer.h"
-// clang-format on
 
 using namespace std::chrono;
 using namespace ray::syncer;
