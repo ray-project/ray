f3558db0804b2bfd819a65f6d3f3aebd84ba24bd
Mengjin Yan <myan@anyscale.com>
2025-03-03 13:11:30 -0800

    [Core] Remove the Anyscale Logging Configurator from Rayturbo (#1514)
    This is a cleanup PR for the effort of open sourcing JSON format
    logging.
    
    As the change from OSS is
    merged:https://github.com/anyscale/rayturbo/commit/02d3f3ca516e6b3f68df9f617589a90e375bc9cb,
    we should be able to remove the Anyscale's logging configurator from
    RayTurbo.
    
    This PR removes the customization from RayTurbo for the cleanup.
    
    Signed-off-by: Mengjin Yan <mengjinyan3@gmail.com>

--- python/ray/_private/ray_logging/default_impl.py
+++ python/ray/_private/ray_logging/default_impl.py
@@ -2,14 +2,3 @@ def get_logging_configurator():
     from ray._private.ray_logging.logging_config import DefaultLoggingConfigurator
 
     return DefaultLoggingConfigurator()
-
-
-# Anyscale overrides
-
-
-def get_logging_configurator():  # noqa: F811
-    from ray.anyscale._private.ray_logging.logging_config import (
-        AnyscaleLoggingConfigurator,
-    )
-
-    return AnyscaleLoggingConfigurator()
