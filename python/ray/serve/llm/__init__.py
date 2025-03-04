62ce06b4ce614384b8945e4705099f3e27a4cf35
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2025-03-03 22:43:34 -0800

    wip
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- python/ray/serve/llm/__init__.py
+++ python/ray/serve/llm/__init__.py
@@ -23,6 +23,46 @@ if TYPE_CHECKING:
 
 
 ##########
+# Models
+##########
+
+
+@PublicAPI(stability="alpha")
+class LLMConfig(_LLMConfig):
+    """The configuration for starting an LLM deployment."""
+
+    pass
+
+
+@PublicAPI(stability="alpha")
+class LLMServingArgs(_LLMServingArgs):
+    """The configuration for starting an LLM deployment application."""
+
+    pass
+
+
+@PublicAPI(stability="alpha")
+class ModelLoadingConfig(_ModelLoadingConfig):
+    """The configuration for loading an LLM model."""
+
+    pass
+
+
+@PublicAPI(stability="alpha")
+class CloudMirrorConfig(_CloudMirrorConfig):
+    """The configuration for mirroring an LLM model from cloud storage."""
+
+    pass
+
+
+@PublicAPI(stability="alpha")
+class LoraConfig(_LoraConfig):
+    """The configuration for loading an LLM model with LoRA."""
+
+    pass
+
+
+##########
 # Builders
 ##########
 
@@ -161,46 +201,6 @@ def build_openai_app(llm_serving_args: "LLMServingArgs") -> "Application":
     from ray.llm._internal.serve.builders import build_openai_app
 
     return build_openai_app(llm_serving_args=llm_serving_args)
-
-
-##########
-# Models
-##########
-
-
-@PublicAPI(stability="alpha")
-class LLMConfig(_LLMConfig):
-    """The configuration for starting an LLM deployment."""
-
-    pass
-
-
-@PublicAPI(stability="alpha")
-class LLMServingArgs(_LLMServingArgs):
-    """The configuration for starting an LLM deployment application."""
-
-    pass
-
-
-@PublicAPI(stability="alpha")
-class ModelLoadingConfig(_ModelLoadingConfig):
-    """The configuration for loading an LLM model."""
-
-    pass
-
-
-@PublicAPI(stability="alpha")
-class CloudMirrorConfig(_CloudMirrorConfig):
-    """The configuration for mirroring an LLM model from cloud storage."""
-
-    pass
-
-
-@PublicAPI(stability="alpha")
-class LoraConfig(_LoraConfig):
-    """The configuration for loading an LLM model with LoRA."""
-
-    pass
 
 
 #############
