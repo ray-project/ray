bec8db8cf75ca2d0e1ea835c73f097dc113bd278
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-03-05 15:41:08 -0600

    [core] Enable `make_shared` in plasma client (#51100)
    Using the same trick we do for `ClientConnection`.
    
    Renamed `Tag` -> `PrivateTag` to make it a little more obvious (I had to
    figure out what it was again and I reviewed the original PR).
    
    ---------
    
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/common/client_connection.cc
+++ src/ray/common/client_connection.cc
@@ -115,10 +115,10 @@ Status ConnectSocketRetry(local_stream_socket &socket,
 }
 
 std::shared_ptr<ServerConnection> ServerConnection::Create(local_stream_socket &&socket) {
-  return std::make_shared<ServerConnection>(Tag{}, std::move(socket));
+  return std::make_shared<ServerConnection>(PrivateTag{}, std::move(socket));
 }
 
-ServerConnection::ServerConnection(Tag, local_stream_socket &&socket)
+ServerConnection::ServerConnection(PrivateTag, local_stream_socket &&socket)
     : socket_(std::move(socket)),
       async_write_max_messages_(1),
       async_write_queue_(),
@@ -415,7 +415,7 @@ std::shared_ptr<ClientConnection> ClientConnection::Create(
     const std::string &debug_label,
     const std::vector<std::string> &message_type_enum_names,
     int64_t error_message_type) {
-  return std::make_shared<ClientConnection>(Tag{},
+  return std::make_shared<ClientConnection>(PrivateTag{},
                                             message_handler,
                                             std::move(socket),
                                             debug_label,
@@ -424,7 +424,7 @@ std::shared_ptr<ClientConnection> ClientConnection::Create(
 }
 
 ClientConnection::ClientConnection(
-    Tag,
+    PrivateTag,
     MessageHandler &message_handler,
     local_stream_socket &&socket,
     const std::string &debug_label,
