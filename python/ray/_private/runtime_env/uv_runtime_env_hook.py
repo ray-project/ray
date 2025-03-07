4068735764da1a7d86558d4a721b6823f72454f1
Philipp Moritz <pcmoritz@gmail.com>
2025-03-06 22:10:55 -0800

    Fix UV hook to support job submission

--- python/ray/_private/runtime_env/uv_runtime_env_hook.py
+++ python/ray/_private/runtime_env/uv_runtime_env_hook.py
@@ -7,6 +7,59 @@ from typing import Any, Dict, Optional
 import psutil
 
 
+def _check_working_dir_files(uv_run_args, runtime_env):
+    # Do error checking that should catch the most common cases of how
+    # things are different in Ray, i.e. not the whole file system will
+    # be available on the workers, only the working_dir.
+
+    # First parse the arguments we need to check
+    uv_run_parser = argparse.ArgumentParser()
+    uv_run_parser.add_argument("--with-requirements", nargs="?")
+    uv_run_parser.add_argument("--project", nargs="?")
+    uv_run_parser.add_argument("--no-project", action="store_true")
+    known_args, _ = uv_run_parser.parse_known_args(uv_run_args)
+
+    working_dir = Path(runtime_env["working_dir"]).resolve()
+
+    # Check if the requirements.txt file is in the working_dir
+    if known_args.with_requirements and not Path(
+        known_args.with_requirements
+    ).resolve().is_relative_to(working_dir):
+        raise RuntimeError(
+            f"You specified --with-requirements={known_args.with_requirements} but "
+            f"the requirements file is not in the working_dir {runtime_env["working_dir"]}, "
+            "so the workers will not have access to the file. Make sure "
+            "the requirements file is in the working directory. "
+            "You can do so by specifying --directory in 'uv run', by changing the current "
+            "working directory before running 'uv run', or by using the 'working_dir' "
+            "parameter of the runtime_environment."
+        )
+
+    # Check if the pyproject.toml file is in the working_dir
+    pyproject = None
+    if known_args.no_project:
+        pyproject = None
+    elif known_args.project:
+        pyproject = Path(known_args.project)
+    else:
+        # Walk up the directory tree until pyproject.toml is found
+        current_path = Path.cwd().resolve()
+        while current_path != current_path.parent:
+            if (current_path / "pyproject.toml").exists():
+                pyproject = Path(current_path / "pyproject.toml")
+                break
+            current_path = current_path.parent
+
+    if pyproject and not pyproject.resolve().is_relative_to(working_dir):
+        raise RuntimeError(
+            f"Your {pyproject.resolve()} is not in the working_dir {runtime_env['working_dir']}, "
+            "so the workers will not have access to the file. Make sure "
+            "the pyproject.toml file is in the working directory. "
+            "You can do so by specifying --directory in 'uv run', by changing the current "
+            "working directory before running 'uv run', or by using the 'working_dir' "
+            "parameter of the runtime_environment."
+        )
+
 def hook(runtime_env: Optional[Dict[str, Any]]) -> Dict[str, Any]:
     """Hook that detects if the driver is run in 'uv run' and sets the runtime environment accordingly."""
 
@@ -39,58 +92,7 @@ def hook(runtime_env: Optional[Dict[str, Any]]) -> Dict[str, Any]:
     # use the same working_dir that uv run would use
     if "working_dir" not in runtime_env:
         runtime_env["working_dir"] = os.getcwd()
-
-    # In the last part of the function we do some error checking that should catch
-    # the most common cases of how things are different in Ray, i.e. not the whole
-    # file system will be available on the workers, only the working_dir.
-
-    # First parse the arguments we need to check
-    uv_run_parser = argparse.ArgumentParser()
-    uv_run_parser.add_argument("--with-requirements", nargs="?")
-    uv_run_parser.add_argument("--project", nargs="?")
-    uv_run_parser.add_argument("--no-project", action="store_true")
-    known_args, _ = uv_run_parser.parse_known_args(uv_run_args)
-
-    working_dir = Path(runtime_env["working_dir"]).resolve()
-
-    # Check if the requirements.txt file is in the working_dir
-    if known_args.with_requirements and not Path(
-        known_args.with_requirements
-    ).resolve().is_relative_to(working_dir):
-        raise RuntimeError(
-            f"You specified --with-requirements={known_args.with_requirements} but "
-            f"the requirements file is not in the working_dir {runtime_env['working_dir']}, "
-            "so the workers will not have access to the file. Make sure "
-            "the requirements file is in the working directory. "
-            "You can do so by specifying --directory in 'uv run', by changing the current "
-            "working directory before running 'uv run', or by using the 'working_dir' "
-            "parameter of the runtime_environment."
-        )
-
-    # Check if the pyproject.toml file is in the working_dir
-    pyproject = None
-    if known_args.no_project:
-        pyproject = None
-    elif known_args.project:
-        pyproject = Path(known_args.project)
-    else:
-        # Walk up the directory tree until pyproject.toml is found
-        current_path = Path.cwd().resolve()
-        while current_path != current_path.parent:
-            if (current_path / "pyproject.toml").exists():
-                pyproject = Path(current_path / "pyproject.toml")
-                break
-            current_path = current_path.parent
-
-    if pyproject and not pyproject.resolve().is_relative_to(working_dir):
-        raise RuntimeError(
-            f"Your {pyproject.resolve()} is not in the working_dir {runtime_env['working_dir']}, "
-            "so the workers will not have access to the file. Make sure "
-            "the pyproject.toml file is in the working directory. "
-            "You can do so by specifying --directory in 'uv run', by changing the current "
-            "working directory before running 'uv run', or by using the 'working_dir' "
-            "parameter of the runtime_environment."
-        )
+        _check_working_dir_files(uv_run_args, runtime_env)
 
     return runtime_env
 
