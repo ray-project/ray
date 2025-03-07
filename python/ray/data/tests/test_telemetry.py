ebac4328e961156aa0f164a84a0df20991ce1331
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-03-07 11:13:52 -0600

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/data/tests/test_telemetry.py
+++ python/ray/data/tests/test_telemetry.py
@@ -1,10 +1,11 @@
 import sys
-from typing import Set
 
 import pytest
 
 import ray
 import ray._private.usage.usage_lib as ray_usage_lib
+from ray._private.test_utils import check_library_usage_telemetry, TelemetryCallsite
+
 from ray import data
 
 
@@ -15,39 +16,26 @@ def reset_usage_lib():
     ray_usage_lib.reset_global_state()
 
 
-def _get_library_usages() -> Set[str]:
-    return set(
-        ray_usage_lib.get_library_usages_to_report(
-            ray.experimental.internal_kv.internal_kv_get_gcs_client()
-        )
+@pytest.mark.parametrize("callsite", list(TelemetryCallsite))
+def test_not_used_on_import(reset_usage_lib, callsite: TelemetryCallsite):
+    def _import_ray_data():
+        from ray import data  # noqa: F401
+
+    check_library_usage_telemetry(
+        _import_ray_data, callsite=callsite, expected_library_usages=[set(), {"core"}]
     )
 
 
-@pytest.mark.parametrize("callsite", ["driver", "actor", "task"])
-def test_data_range(reset_usage_lib, callsite: str):
-    assert len(_get_library_usages()) == 0
-
-    if callsite == "driver":
+@pytest.mark.parametrize("callsite", list(TelemetryCallsite))
+def test_used_on_data_range(reset_usage_lib, callsite: TelemetryCallsite):
+    def _call_data_range():
         data.range(10)
-    elif callsite == "actor":
 
-        @ray.remote
-        class A:
-            def __init__(self):
-                data.range(10)
-
-        a = A.remote()
-        ray.get(a.__ray_ready__.remote())
-
-    elif callsite == "task":
-
-        @ray.remote
-        def f():
-            data.range(10)
-
-        ray.get(f.remote())
-
-    assert _get_library_usages() == {"core", "dataset"}
+    check_library_usage_telemetry(
+        _call_data_range,
+        callsite=callsite,
+        expected_library_usages=[{"core", "dataset"}],
+    )
 
 
 if __name__ == "__main__":
