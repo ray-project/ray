11e886c673e7b15a9a0c9aad5deddbf0373ad090
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-03-07 19:43:38 -0600

    [telemetry] Move library usage tests out of core (#51161)
    Splits the tests for library-specific telemetry data into their own test
    files. Each one has a similarly-structured test with a shared utility
    for common logic.
    
    This exercise many issues in the telemetry that we need to address in
    follow ups:
    
    - https://github.com/ray-project/ray/issues/51163
    - https://github.com/ray-project/ray/issues/51164
    - https://github.com/ray-project/ray/issues/51165
    - https://github.com/ray-project/ray/issues/51166
    - https://github.com/ray-project/ray/issues/51167
    - https://github.com/ray-project/ray/issues/51168
    
    ---------
    
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

---
+++ python/ray/tune/tests/test_telemetry.py
@@ -0,0 +1,55 @@
+import sys
+
+import pytest
+
+import ray
+import ray._private.usage.usage_lib as ray_usage_lib
+from ray._private.test_utils import check_library_usage_telemetry, TelemetryCallsite
+
+from ray import tune
+
+
+@pytest.fixture
+def reset_usage_lib():
+    yield
+    ray.shutdown()
+    ray_usage_lib.reset_global_state()
+
+
+@pytest.mark.parametrize("callsite", list(TelemetryCallsite))
+def test_not_used_on_import(reset_usage_lib, callsite: TelemetryCallsite):
+    if callsite in {TelemetryCallsite.ACTOR, TelemetryCallsite.TASK}:
+        pytest.skip("TODO: train usage is exported when importing in an actor or task.")
+
+    def _import_ray_tune():
+        from ray import tune  # noqa: F401
+
+    check_library_usage_telemetry(
+        _import_ray_tune, callsite=callsite, expected_library_usages=[set(), {"core"}]
+    )
+
+
+@pytest.mark.parametrize("callsite", list(TelemetryCallsite))
+def test_used_on_tuner_fit(reset_usage_lib, callsite: TelemetryCallsite):
+    def _call_tuner_fit():
+        def objective(*args):
+            pass
+
+        tuner = tune.Tuner(objective)
+        tuner.fit()
+
+    check_library_usage_telemetry(
+        _call_tuner_fit,
+        callsite=callsite,
+        expected_library_usages=[{"train", "tune"}, {"core", "train", "tune"}],
+        expected_extra_usage_tags={
+            "tune_scheduler": "FIFOScheduler",
+            "tune_searcher": "BasicVariantGenerator",
+            "air_entrypoint": "Tuner.fit",
+            "air_storage_configuration": "local",
+        },
+    )
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main(["-v", "-s", __file__]))
