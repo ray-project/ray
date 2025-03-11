a7131274bd98ab3430eab74d47ff94532745701b
Chi-Sheng Liu <chishengliu@chishengliu.com>
2025-03-12 06:03:41 +0800

    Install python dependencies
    Signed-off-by: Chi-Sheng Liu <chishengliu@chishengliu.com>

--- ci/k8s/run-kuberay-doc-tests.sh
+++ ci/k8s/run-kuberay-doc-tests.sh
@@ -5,6 +5,11 @@ set -euo pipefail
 echo "--- Setup k8s environment"
 SKIP_CREATE_KIND_CLUSTER=1 bash ci/k8s/prep-k8s-environment.sh
 
+echo "--- Install Python dependencies"
+pip install -c python/requirements_compiled.txt pytest nbval bash_kernel
+python -m bash_kernel.install
+pip install "ray[default]==2.43.0"
+
 echo "--- Run doc tests"
 cd doc/source/cluster/kubernetes
 py.test --nbval getting-started/raycluster-quick-start.ipynb --nbval-kernel-name bash --sanitize-with doc_sanitize.cfg
