0151063e197bc6c81042359108ce6dc56f9c710f
simonsays1980 <simon.zehnder@gmail.com>
2025-03-11 11:06:24 +0100

    Removed API tag from 'Stats' because linter requests documentation.
    Signed-off-by: simonsays1980 <simon.zehnder@gmail.com>

--- rllib/utils/metrics/stats.py
+++ rllib/utils/metrics/stats.py
@@ -9,13 +9,11 @@ import numpy as np
 from ray.rllib.utils import force_list
 from ray.rllib.utils.framework import try_import_tf, try_import_torch
 from ray.rllib.utils.numpy import convert_to_numpy
-from ray.util.annotations import PublicAPI
 
 _, tf, _ = try_import_tf()
 torch, _ = try_import_torch()
 
 
-@PublicAPI(stability="alpha")
 class Stats:
     """A container class holding a number of values and executing reductions over them.
 
