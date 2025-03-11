ccd828122e653baeff867ec82e664a02420f711d
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2025-03-10 18:25:15 -0700

    wip
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- python/ray/llm/_internal/serve/config_generator/utils/gpu.py
+++ python/ray/llm/_internal/serve/config_generator/utils/gpu.py
@@ -4,6 +4,13 @@ from typing import List, Dict
 import yaml
 from ray.llm._internal.serve.configs.server_models import GPUType
 
+
+from ray.llm._internal.serve.config_generator.utils.constants import (
+    DEFAULT_DEPLOYMENT_CONFIGS_FILE,
+    TEMPLATE_DIR,
+)
+
+
 # All practical GPUs
 ALL_GPU_TYPES = [
     GPUType.NVIDIA_L4,
@@ -14,18 +21,16 @@ ALL_GPU_TYPES = [
     GPUType.NVIDIA_H100,
 ]
 
-from ray.llm._internal.serve.config_generator.utils.constants import (
-    DEFAULT_DEPLOYMENT_CONFIGS_FILE,
-    TEMPLATE_DIR,
-)
 
-
-def read_model_id_to_gpu_mapping() -> Dict[str, str]:
+def read_model_id_to_gpu_mapping() -> Dict[str, List[str]]:
     file_path = os.path.join(TEMPLATE_DIR, DEFAULT_DEPLOYMENT_CONFIGS_FILE)
     with open(file_path, "r") as stream:
         configs = yaml.safe_load(stream)
     model_id_configs = configs["model_id_to_gpu_deployment_configs"]
-    return {model_id: gpu_type for model_id, gpu_type in model_id_configs.items()}
+    return {
+        model_id: list(model_config.keys())
+        for model_id, model_config in model_id_configs.items()
+    }
 
 
 DEFAULT_MODEL_ID_TO_GPU: Dict[str, str] = read_model_id_to_gpu_mapping()
