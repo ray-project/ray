6839f784c5a6f7c7afe08a2bbfe94aea6af11d74
Linkun Chen <github@lkchen.net>
2025-03-12 10:01:36 -0700

    remove "serve" related stuff from logging
    Signed-off-by: Linkun Chen <github@lkchen.net>

--- python/ray/llm/_internal/batch/observability/logging/__init__.py
+++ python/ray/llm/_internal/batch/observability/logging/__init__.py
@@ -2,14 +2,13 @@ import logging
 from typing import Optional
 
 from ray._private.ray_logging.filters import CoreContextFilter
-from ray.serve._private.logging_utils import ServeContextFilter
 
 
 def _setup_logger(logger_name: str):
     """Setup logger given the logger name.
 
     This function is idempotent and won't set up the same logger multiple times. It will
-    Also skip the setup if Serve logger is already setup and has handlers.
+    also skip the setup if Data logger is already setup and has handlers.
 
     Args:
         logger_name: logger name used to get the logger.
@@ -17,7 +16,7 @@ def _setup_logger(logger_name: str):
     logger = logging.getLogger(logger_name)
     llm_logger = logging.getLogger("ray.data.llm")
 
-    # Skip setup if the logger already has handlers setup or if the parent (Serve
+    # Skip setup if the logger already has handlers setup or if the parent (Data
     # logger) has handlers.
     if logger.handlers or llm_logger.handlers:
         return
@@ -25,14 +24,13 @@ def _setup_logger(logger_name: str):
     # Set up stream handler, which logs to console as plaintext.
     stream_handler = logging.StreamHandler()
     stream_handler.addFilter(CoreContextFilter())
-    stream_handler.addFilter(ServeContextFilter())
     logger.addHandler(stream_handler)
     logger.setLevel(logging.INFO)
     logger.propagate = False
 
 
 def get_logger(name: Optional[str] = None):
-    """Get a structured logger inherited from the Ray Serve logger.
+    """Get a structured logger inherited from the Ray Data logger.
 
     Loggers by default are logging to stdout, and are expected to be scraped by an
     external process.
