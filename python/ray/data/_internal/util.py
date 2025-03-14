3dbf8ac6daf5c8c1d2a6b2b71cd978646e09d687
srinathk10 <68668616+srinathk10@users.noreply.github.com>
2025-03-14 15:40:07 -0700

    Remove redundant sort in find_partition_index (#1539)
    Signed-off-by: Srinath Krishnamachari <srinath.krishnamachari@anyscale.com>

--- python/ray/data/_internal/util.py
+++ python/ray/data/_internal/util.py
@@ -777,13 +777,14 @@ def find_partition_index(
             # is an index into the ascending order of ``col_vals``, so we need
             # to subtract it from ``len(col_vals)`` to get the index in the
             # original descending order of ``col_vals``.
+            sorter = np.arange(len(col_vals) - 1, -1, -1)
             left = prevleft + (
                 len(col_vals)
                 - np.searchsorted(
                     col_vals,
                     desired_val,
                     side="right",
-                    sorter=np.arange(len(col_vals) - 1, -1, -1),
+                    sorter=sorter,
                 )
             )
             right = prevleft + (
@@ -792,7 +793,7 @@ def find_partition_index(
                     col_vals,
                     desired_val,
                     side="left",
-                    sorter=np.arange(len(col_vals) - 1, -1, -1),
+                    sorter=sorter,
                 )
             )
         else:
