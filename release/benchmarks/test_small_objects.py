0488347e075cff59fb40e34ce4108d6b428e8300
dentiny <dentinyhao@gmail.com>
2025-03-14 23:10:52 +0000

    update size
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- release/benchmarks/test_small_objects.py
+++ release/benchmarks/test_small_objects.py
@@ -10,7 +10,7 @@ def test_small_objects():
     @ray.remote(num_cpus=1)
     class Actor:
         def send(self, _, actor_idx):
-            numpy_arr = np.ones((5, 1024))
+            numpy_arr = np.ones((20, 1024))
             return pickle.dumps((numpy_arr, actor_idx))
 
     actors = [Actor.remote() for _ in range(64)]
