6d4b0a39430324cb68a8ad745c5a566c2388959d
400Ping <43886578+400Ping@users.noreply.github.com>
2025-03-15 23:03:07 +0800

    [Fix] Cover cpplint in test
    Signed-off-by: 400Ping <43886578+400Ping@users.noreply.github.com>

--- src/ray/raylet/test/util.h
+++ src/ray/raylet/test/util.h
@@ -12,6 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifndef SRC_RAY_RAYLET_TEST_UTIL_H_
+#define SRC_RAY_RAYLET_TEST_UTIL_H_
+#endif  // SRC_RAY_RAYLET_TEST_UTIL_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
 #include "ray/raylet/worker.h"
 
 namespace ray {
