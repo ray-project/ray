6268a2d6e2f0a4bf96c995493d61ec4197408d06
Kai-Hsun Chen <kaihsun@anyscale.com>
2025-03-17 14:54:42 -0700

    [core][dashboard] Return status code other than 200 and 500 (#51417)
    The utility function `rest_response` takes a boolean parameter,
    `success`, to generate an HTTP response. It returns 200 when `success`
    is true, and 500 otherwise. However, it's not enough.
    
    For example, if users try to kill a non-existent actor via
    `/api/actors/kill`, 404 should be returned. Revisiting status code of
    each API endpoint can be good first issues.
    
    ---------
    
    Signed-off-by: kaihsun <kaihsun@anyscale.com>

--- python/ray/dashboard/modules/usage_stats/usage_stats_head.py
+++ python/ray/dashboard/modules/usage_stats/usage_stats_head.py
@@ -45,7 +45,7 @@ class UsageStatsHead(dashboard_utils.DashboardHeadModule):
         @routes.get("/usage_stats_enabled")
         async def get_usage_stats_enabled(self, req) -> aiohttp.web.Response:
             return ray.dashboard.optional_utils.rest_response(
-                success=True,
+                status_code=dashboard_utils.HTTPStatusCode.OK,
                 message="Fetched usage stats enabled",
                 usage_stats_enabled=self.usage_stats_enabled,
                 usage_stats_prompt_enabled=self.usage_stats_prompt_enabled,
@@ -54,7 +54,7 @@ class UsageStatsHead(dashboard_utils.DashboardHeadModule):
         @routes.get("/cluster_id")
         async def get_cluster_id(self, req) -> aiohttp.web.Response:
             return ray.dashboard.optional_utils.rest_response(
-                success=True,
+                status_code=dashboard_utils.HTTPStatusCode.OK,
                 message="Fetched cluster id",
                 cluster_id=self.gcs_client.cluster_id.hex(),
             )
