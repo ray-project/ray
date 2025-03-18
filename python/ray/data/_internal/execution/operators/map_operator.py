47147f41851f622e6e23f18f865b67955f9df656
Balaji Veeramani <bveeramani@berkeley.edu>
2025-03-18 12:48:47 -0700

    Revert "[Data] Poll memory usage per map task (#51324)"
    This reverts commit d7601c1531e38e07fc3220451c577a3c20205dd2.

--- python/ray/data/_internal/execution/operators/map_operator.py
+++ python/ray/data/_internal/execution/operators/map_operator.py
@@ -49,9 +49,9 @@ from ray.data._internal.stats import StatsDict
 from ray.data.block import (
     Block,
     BlockAccessor,
+    BlockExecStats,
     BlockMetadata,
     BlockStats,
-    _BlockExecStatsBuilder,
     to_stats,
 )
 from ray.data.context import DataContext
@@ -530,17 +530,17 @@ def _map_task(
     """
     DataContext._set_current(data_context)
     ctx.kwargs.update(kwargs)
+    stats = BlockExecStats.builder()
     map_transformer.set_target_max_block_size(ctx.target_max_block_size)
-    with _BlockExecStatsBuilder(data_context.memory_poll_interval_s) as stats:
-        for b_out in map_transformer.apply_transform(iter(blocks), ctx):
-            # TODO(Clark): Add input file propagation from input blocks.
-            m_out = BlockAccessor.for_block(b_out).get_metadata()
-            m_out.exec_stats = stats.build()
-            m_out.exec_stats.udf_time_s = map_transformer.udf_time()
-            m_out.exec_stats.task_idx = ctx.task_idx
-            yield b_out
-            yield m_out
-            stats.reset()
+    for b_out in map_transformer.apply_transform(iter(blocks), ctx):
+        # TODO(Clark): Add input file propagation from input blocks.
+        m_out = BlockAccessor.for_block(b_out).get_metadata()
+        m_out.exec_stats = stats.build()
+        m_out.exec_stats.udf_time_s = map_transformer.udf_time()
+        m_out.exec_stats.task_idx = ctx.task_idx
+        yield b_out
+        yield m_out
+        stats = BlockExecStats.builder()
 
 
 class _BlockRefBundler:
--- python/ray/data/context.py
+++ python/ray/data/context.py
@@ -323,8 +323,6 @@ class DataContext:
             transient errors when reading from remote storage systems.
         enable_per_node_metrics: Enable per node metrics reporting for Ray Data,
             disabled by default.
-        memory_poll_interval_s: The interval to poll the USS of map tasks. If `None`,
-            map tasks won't record memory stats.
     """
 
     target_max_block_size: int = DEFAULT_TARGET_MAX_BLOCK_SIZE
@@ -397,7 +395,6 @@ class DataContext:
     )
     enable_per_node_metrics: bool = DEFAULT_ENABLE_PER_NODE_METRICS
     override_object_store_memory_limit_fraction: float = None
-    memory_poll_interval_s: Optional[float] = 1
 
     def __post_init__(self):
         # The additonal ray remote args that should be added to
