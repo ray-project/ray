9bd00da0215b4cf66fb76c249e475e7cb2d8ca29
Lonnie Liu <lonnie@anyscale.com>
2025-03-18 20:00:21 +0000

    version change for 2.44.1
    Signed-off-by: Lonnie Liu <lonnie@anyscale.com>

--- python/ray/_version.py
+++ python/ray/_version.py
@@ -1,6 +1,6 @@
 # Replaced with the current commit when building the wheels.
 commit = "{{RAY_COMMIT_SHA}}"
-version = "2.44.0"
+version = "2.44.1"
 
 if __name__ == "__main__":
     print("%s %s" % (version, commit))
