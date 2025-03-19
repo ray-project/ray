10a5b30f51b029e28995e2209f138ae3d3f4c099
Rui Qiao <161574667+ruisearch42@users.noreply.github.com>
2025-03-18 20:53:09 -0700

    Revert "[Feat][Core/Dashboard] Add SubprocessModules to the Dashboard routes, and convert HealthzHead (#51282)" (#51512)
    Signed-off-by: Rui Qiao <ruisearch42@gmail.com>
    Signed-off-by: Dhakshin Suriakannu <d_suriakannu@apple.com>

--- python/ray/dashboard/subprocesses/utils.py
+++ python/ray/dashboard/subprocesses/utils.py
@@ -1,7 +1,6 @@
 import os
 import enum
 from typing import TypeVar
-from ray._private.utils import validate_socket_filepath
 
 K = TypeVar("K")
 V = TypeVar("V")
@@ -16,24 +15,14 @@ class ResponseType(enum.Enum):
 def module_logging_filename(module_name: str, logging_filename: str) -> str:
     """
     Parse logging_filename = STEM EXTENSION,
-    return STEM _ MODULE_NAME _ EXTENSION
-
-    If logging_filename is empty, return "stderr"
+    return STEM _ MODULE_NAME EXTENSION
 
     Example:
     module_name = "TestModule"
     logging_filename = "dashboard.log"
     STEM = "dashboard"
     EXTENSION = ".log"
-    return "dashboard_TestModule.log"
+    return "dashboard-TestModule.log"
     """
-    if not logging_filename:
-        return "stderr"
     stem, extension = os.path.splitext(logging_filename)
     return f"{stem}_{module_name}{extension}"
-
-
-def get_socket_path(socket_dir: str, module_name: str) -> str:
-    socket_path = os.path.join(socket_dir, "dash_" + module_name)
-    validate_socket_filepath(socket_path)
-    return socket_path
