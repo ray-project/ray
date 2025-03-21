e1f515b6f72e1feb594dbe6168bec71b7e4c46fa
Richard Liaw <rliaw@berkeley.edu>
2025-03-21 13:18:57 -0700

    Update python/ray/data/_internal/datasource/databricks_uc_datasource.py
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>

--- python/ray/data/_internal/datasource/databricks_uc_datasource.py
+++ python/ray/data/_internal/datasource/databricks_uc_datasource.py
@@ -37,8 +37,8 @@ class DatabricksUCDatasource(Datasource):
         self.schema = schema
         self.query = query
 
-        if not host.startswith(('http://', 'https://')):
-            self.host = f'https://{host}'
+        if not host.startswith(("http://", "https://")):
+            self.host = f"https://{host}"
 
         url_base = f"{self.host}/api/2.0/sql/statements/"
 
