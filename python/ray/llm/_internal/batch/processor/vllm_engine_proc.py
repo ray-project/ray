360ede3d5b7125467ebc93a13e66ddd5873be7b3
Xiaoshuang Liu <liuxiaoshuang4@huawei.com>
2025-03-22 03:20:59 +0800

    [llm] ray.llm support custom accelerators (#51359)
    Signed-off-by: liuxsh9 <liuxiaoshuang4@huawei.com>
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>
    Co-authored-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- python/ray/llm/_internal/batch/processor/vllm_engine_proc.py
+++ python/ray/llm/_internal/batch/processor/vllm_engine_proc.py
@@ -193,6 +193,7 @@ def build_vllm_engine_processor(
                 # This is used to make sure we overlap batches to avoid the tail
                 # latency of each batch.
                 max_concurrency=config.max_concurrent_batches,
+                resources=config.resources_per_bundle,
                 accelerator_type=config.accelerator_type,
                 runtime_env=config.runtime_env,
             ),
