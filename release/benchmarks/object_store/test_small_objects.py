9339cabe3036a520262edfa44a6819cd339dc188
dayshah <dhyey2019@gmail.com>
2025-03-25 09:58:54 -0700

    update wording
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- release/benchmarks/object_store/test_small_objects.py
+++ release/benchmarks/object_store/test_small_objects.py
@@ -5,7 +5,7 @@ import os
 import json
 
 
-def test_small_objects_ingest():
+def test_small_objects_many_to_one():
     @ray.remote(num_cpus=1)
     class Actor:
         def send(self, _, actor_idx):
@@ -28,7 +28,7 @@ def test_small_objects_ingest():
     return num_messages / 60
 
 
-def test_small_objects_broadcast():
+def test_small_objects_one_to_many():
     @ray.remote(num_cpus=1)
     class Actor:
         def receive(self, numpy_arr, actor_idx):
@@ -52,28 +52,28 @@ def test_small_objects_broadcast():
 
 
 ray.init(address="auto")
-ingest_throughput = test_small_objects_ingest()
-print(f"Number of messages per second ingested: {ingest_throughput}")
-broadcast_throughput = test_small_objects_broadcast()
-print(f"Number of messages per second broadcasted: {broadcast_throughput}")
+many_to_one_throughput = test_small_objects_many_to_one()
+print(f"Number of messages per second many_to_one: {many_to_one_throughput}")
+one_to_many_throughput = test_small_objects_one_to_many()
+print(f"Number of messages per second one_to_many: {one_to_many_throughput}")
 
 
 if "TEST_OUTPUT_JSON" in os.environ:
     with open(os.environ["TEST_OUTPUT_JSON"], "w") as out_file:
         results = {
-            "num_messages_ingested": ingest_throughput,
-            "num_messages_broadcasted": broadcast_throughput,
+            "num_messages_many_to_one": many_to_one_throughput,
+            "num_messages_one_to_many": one_to_many_throughput,
             "success": "1",
         }
         results["perf_metrics"] = [
             {
-                "perf_metric_name": "num_small_objects_ingested_per_second",
-                "perf_metric_value": ingest_throughput,
+                "perf_metric_name": "num_small_objects_many_to_one",
+                "perf_metric_value": many_to_one_throughput,
                 "perf_metric_type": "THROUGHPUT",
             },
             {
-                "perf_metric_name": "num_small_objects_broadcasted_per_second",
-                "perf_metric_value": broadcast_throughput,
+                "perf_metric_name": "num_small_objects_one_to_many_per_second",
+                "perf_metric_value": one_to_many_throughput,
                 "perf_metric_type": "THROUGHPUT",
             },
         ]
