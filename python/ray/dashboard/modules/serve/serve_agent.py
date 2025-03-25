bc289d8fcbe7de059d4d34a01755ef13172e2ca1
Chi-Sheng Liu <chishengliu@chishengliu.com>
2025-03-26 01:38:19 +0800

    [Refactor][Core/Dashboard] Extract shared implementation from ServeHead and ServeAgent to a separate class
    Signed-off-by: Chi-Sheng Liu <chishengliu@chishengliu.com>

--- python/ray/dashboard/modules/serve/serve_agent.py
+++ python/ray/dashboard/modules/serve/serve_agent.py
@@ -1,11 +1,96 @@
+import dataclasses
+import json
+import logging
+
+import aiohttp
+from aiohttp.web import Request, Response
+
+import ray
 import ray.dashboard.optional_utils as optional_utils
 import ray.dashboard.utils as dashboard_utils
-from ray.dashboard.modules.serve.serve_rest_api_impl import create_serve_rest_api
+from ray._private.pydantic_compat import ValidationError
+from ray.exceptions import RayTaskError
+from ray.dashboard.modules.serve.serve_api_delegate import ServeAPIDelegate
+from ray.dashboard.modules.serve.utils import validate_endpoint
 
-dashboard_agent_route_table = optional_utils.DashboardAgentRouteTable
 
-ServeAgent = create_serve_rest_api(
-    dashboard_module_superclass=dashboard_utils.DashboardAgentModule,
-    dashboard_route_table=dashboard_agent_route_table,
-    log_deprecation_warning=True,
-)
+logger = logging.getLogger(__name__)
+routes = optional_utils.DashboardAgentRouteTable
+
+
+class ServeAgent(dashboard_utils.DashboardAgentModule):
+    def __init__(
+        self,
+        dashboard_agent: dashboard_utils.DashboardAgentModule,
+    ):
+        super().__init__(dashboard_agent)
+        self._api_delegate = ServeAPIDelegate()
+
+    # TODO: It's better to use `/api/version`.
+    @routes.get("/api/ray/version")
+    async def get_version(self, req: Request) -> Response:
+        version_res = self._api_delegate.get_version(self.session_name)
+        return Response(
+            text=json.dumps(dataclasses.asdict(version_res)),
+            content_type="application/json",
+            status=aiohttp.web.HTTPOk.status_code,
+        )
+
+    @routes.get("/api/serve/applications/")
+    @optional_utils.init_ray_and_catch_exceptions()
+    @validate_endpoint(log_deprecation_warning=True)
+    async def get_serve_instance_details(self, req: Request) -> Response:
+        try:
+            details = await self._api_delegate.get_serve_instance_details()
+        except ray.exceptions.RayTaskError as e:
+            # Task failure sometimes are due to GCS
+            # failure. When GCS failed, we expect a longer time
+            # to recover.
+            return Response(
+                status=503,
+                text=(
+                    "Failed to get a response from the controller. "
+                    f"The GCS may be down, please retry later: {e}"
+                ),
+            )
+
+        return Response(
+            text=json.dumps(details),
+            content_type="application/json",
+        )
+
+    @routes.delete("/api/serve/applications/")
+    @optional_utils.init_ray_and_catch_exceptions()
+    async def delete_serve_applications(self, req: Request) -> Response:
+        await self._api_delegate.delete_serve_applications()
+        return Response()
+
+    @routes.put("/api/serve/applications/")
+    @optional_utils.init_ray_and_catch_exceptions()
+    @validate_endpoint(log_deprecation_warning=True)
+    async def put_all_applications(self, req: Request) -> Response:
+        from ray.serve.schema import ServeDeploySchema
+
+        try:
+            config: ServeDeploySchema = ServeDeploySchema.parse_obj(await req.json())
+        except ValidationError as e:
+            return Response(
+                status=400,
+                text=repr(e),
+            )
+
+        try:
+            await self._api_delegate.put_all_applications(config)
+        except RayTaskError as e:
+            return Response(
+                status=400,
+                text=str(e),
+            )
+        return Response()
+
+    async def run(self, server):
+        pass
+
+    @staticmethod
+    def is_minimal_module():
+        return False
--- python/ray/dashboard/modules/serve/serve_head.py
+++ python/ray/dashboard/modules/serve/serve_head.py
@@ -1,10 +1,93 @@
+import dataclasses
+import json
+import logging
+
+import aiohttp
+from aiohttp.web import Request, Response
+
+import ray
 import ray.dashboard.optional_utils as optional_utils
 import ray.dashboard.utils as dashboard_utils
-from ray.dashboard.modules.serve.serve_rest_api_impl import create_serve_rest_api
+from ray._private.pydantic_compat import ValidationError
+from ray.exceptions import RayTaskError
+from ray.dashboard.modules.serve.serve_api_delegate import ServeAPIDelegate
+from ray.dashboard.modules.serve.utils import validate_endpoint
 
-dashboard_head_route_table = optional_utils.DashboardHeadRouteTable
 
-ServeHead = create_serve_rest_api(
-    dashboard_module_superclass=dashboard_utils.DashboardHeadModule,
-    dashboard_route_table=dashboard_head_route_table,
-)
+logger = logging.getLogger(__name__)
+routes = optional_utils.DashboardHeadRouteTable
+
+
+class ServeHead(dashboard_utils.DashboardHeadModule):
+    def __init__(self, config: dashboard_utils.DashboardHeadModuleConfig):
+        super().__init__(config)
+        self._api_delegate = ServeAPIDelegate()
+
+    # TODO: It's better to use `/api/version`.
+    @routes.get("/api/ray/version")
+    async def get_version(self, req: Request) -> Response:
+        version_res = self._api_delegate.get_version(self.session_name)
+        return Response(
+            text=json.dumps(dataclasses.asdict(version_res)),
+            content_type="application/json",
+            status=aiohttp.web.HTTPOk.status_code,
+        )
+
+    @routes.get("/api/serve/applications/")
+    @optional_utils.init_ray_and_catch_exceptions()
+    @validate_endpoint(log_deprecation_warning=False)
+    async def get_serve_instance_details(self, req: Request) -> Response:
+        try:
+            details = await self._api_delegate.get_serve_instance_details()
+        except ray.exceptions.RayTaskError as e:
+            # Task failure sometimes are due to GCS
+            # failure. When GCS failed, we expect a longer time
+            # to recover.
+            return Response(
+                status=503,
+                text=(
+                    "Failed to get a response from the controller. "
+                    f"The GCS may be down, please retry later: {e}"
+                ),
+            )
+
+        return Response(
+            text=json.dumps(details),
+            content_type="application/json",
+        )
+
+    @routes.delete("/api/serve/applications/")
+    @optional_utils.init_ray_and_catch_exceptions()
+    async def delete_serve_applications(self, req: Request) -> Response:
+        await self._api_delegate.delete_serve_applications()
+        return Response()
+
+    @routes.put("/api/serve/applications/")
+    @optional_utils.init_ray_and_catch_exceptions()
+    @validate_endpoint(log_deprecation_warning=False)
+    async def put_all_applications(self, req: Request) -> Response:
+        from ray.serve.schema import ServeDeploySchema
+
+        try:
+            config: ServeDeploySchema = ServeDeploySchema.parse_obj(await req.json())
+        except ValidationError as e:
+            return Response(
+                status=400,
+                text=repr(e),
+            )
+
+        try:
+            await self._api_delegate.put_all_applications(config)
+        except RayTaskError as e:
+            return Response(
+                status=400,
+                text=str(e),
+            )
+        return Response()
+
+    async def run(self, server):
+        pass
+
+    @staticmethod
+    def is_minimal_module():
+        return False
