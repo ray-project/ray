14c43223d47ada4eefe29f51c1b2f5a1823c0638
dentiny <dentinyhao@gmail.com>
2025-03-27 04:50:30 +0000

    fix root variable shadow
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- src/ray/gcs/gcs_server/test/gcs_task_manager_test.cc
+++ src/ray/gcs/gcs_server/test/gcs_task_manager_test.cc
@@ -1351,7 +1351,7 @@ TEST_F(GcsTaskManagerMemoryLimitedTest, TestIndexNoLeak) {
 
   // Evict all of them with tasks with single attempt, no parent, same job, no worker id.
   {
-    auto task_ids = GenTaskIDs(num_limit);
+    task_ids = GenTaskIDs(num_limit);
     auto job_id = 0;
     auto attempt_number = 0;
     for (size_t i = 0; i < num_limit; i++) {
