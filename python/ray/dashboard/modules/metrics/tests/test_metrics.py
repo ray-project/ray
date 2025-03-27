209fd0f8c44de64dd972d02eada1b218ea20f266
400Ping <43886578+400Ping@users.noreply.github.com>
2025-03-28 00:27:00 +0800

    [Fix] Fix scope mismatch problem
    Signed-off-by: 400Ping <43886578+400Ping@users.noreply.github.com>

--- python/ray/dashboard/modules/metrics/tests/test_metrics.py
+++ python/ray/dashboard/modules/metrics/tests/test_metrics.py
@@ -2,52 +2,21 @@ import sys
 import asyncio
 import requests
 import pytest
-import ray._private.ray_constants as ray_constants
-from ray._private.test_utils import find_free_port, wait_for_condition
+from ray._private.test_utils import wait_for_condition, wait_until_server_available
 from ray.dashboard.tests.conftest import *  # noqa
 
 
-@pytest.fixture(scope="module")
-def ray_dashboard(ray_start_cluster):
-    """
-    Starts Ray cluster with the dashboard and returns the dashboard URL.
-    """
-    dashboard_port = find_free_port()
-    h = ray_start_cluster.add_node(dashboard_port=dashboard_port)
-    uri = f"http://localhost:{dashboard_port}"
-
-    wait_for_condition(
-        lambda: requests.get(f"{uri}/api/grafana_health").status_code == 200
-    )
-
-    return {"webui_url": uri, "node": h}
-
-
-@pytest.fixture(scope="module")
-def webui_url(ray_dashboard):
-    """
-    Extracts the Web UI URL from the Ray dashboard fixture.
-    """
-    return ray_dashboard["webui_url"]
-
-
-def is_service_ready(url):
-    """
-    Checks if the given API endpoint is available.
-    """
-    try:
-        resp = requests.get(url, timeout=3)
-        return resp.status_code == 200
-    except requests.exceptions.RequestException:
-        return False
-
-
-def test_grafana_health(webui_url):
+def test_grafana_health(ray_start_with_dashboard):
     """
     Tests the /api/grafana_health endpoint from MetricsHead module.
     """
+    assert wait_until_server_available(ray_start_with_dashboard["webui_url"])
+    webui_url = ray_start_with_dashboard["webui_url"]
+
     url = f"{webui_url}/api/grafana_health"
-    assert wait_for_condition(lambda: is_service_ready(url), timeout=10)
+    assert wait_for_condition(
+        lambda: requests.get(url, timeout=3).status_code == 200, timeout=10
+    )
 
     resp = requests.get(url)
     assert resp.status_code == 200, resp.text
@@ -56,12 +25,17 @@ def test_grafana_health(webui_url):
     assert "grafana_host" in data["data"]
 
 
-def test_prometheus_health(webui_url):
+def test_prometheus_health(ray_start_with_dashboard):
     """
     Tests the /api/prometheus_health endpoint from MetricsHead module.
     """
+    assert wait_until_server_available(ray_start_with_dashboard["webui_url"])
+    webui_url = ray_start_with_dashboard["webui_url"]
+
     url = f"{webui_url}/api/prometheus_health"
-    assert wait_for_condition(lambda: is_service_ready(url), timeout=10)
+    assert wait_for_condition(
+        lambda: requests.get(url, timeout=3).status_code == 200, timeout=10
+    )
 
     resp = requests.get(url)
     assert resp.status_code == 200, resp.text
@@ -69,47 +43,14 @@ def test_prometheus_health(webui_url):
     assert data["result"] is True
 
 
-def test_grafana_health_fail(webui_url, ray_dashboard):
-    """
-    Tests /api/grafana_health when MetricsHead (Grafana) is not running.
-    """
-    url = f"{webui_url}/api/grafana_health"
-
-    ray_dashboard["node"].all_processes[ray_constants.PROCESS_TYPE_METRICS_HEAD][
-        0
-    ].process.kill()
-
-    try:
-        wait_for_condition(
-            lambda: requests.get(url, timeout=1).status_code != 200, timeout=4
-        )
-    except RuntimeError as e:
-        assert "Read timed out" in str(e)
-
-
-def test_prometheus_health_fail(webui_url, ray_dashboard):
-    """
-    Tests /api/prometheus_health when MetricsHead (Prometheus) is not running.
-    """
-    url = f"{webui_url}/api/prometheus_health"
-
-    ray_dashboard["node"].all_processes[ray_constants.PROCESS_TYPE_METRICS_HEAD][
-        0
-    ].process.kill()
-
-    try:
-        wait_for_condition(
-            lambda: requests.get(url, timeout=1).status_code != 200, timeout=4
-        )
-    except RuntimeError as e:
-        assert "Read timed out" in str(e)
-
-
 @pytest.mark.asyncio
-async def test_async_health_check(webui_url):
+async def test_async_health_check(ray_start_with_dashboard):
     """
     Tests asynchronous API health check for robustness.
     """
+    assert wait_until_server_available(ray_start_with_dashboard["webui_url"])
+    webui_url = ray_start_with_dashboard["webui_url"]
+
     url = f"{webui_url}/api/grafana_health"
     for _ in range(5):
         resp = await asyncio.to_thread(requests.get, url)
