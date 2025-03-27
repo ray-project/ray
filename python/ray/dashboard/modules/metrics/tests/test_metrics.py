56a57306b275ad0cb8adf7ad083a40075cce5473
400Ping <43886578+400Ping@users.noreply.github.com>
2025-03-28 01:49:13 +0800

    [Fix] Fix url literal
    Signed-off-by: 400Ping <43886578+400Ping@users.noreply.github.com>

--- python/ray/dashboard/modules/metrics/tests/test_metrics.py
+++ python/ray/dashboard/modules/metrics/tests/test_metrics.py
@@ -1,6 +1,7 @@
 import sys
 import asyncio
 import requests
+import urllib.parse
 import pytest
 from ray._private.test_utils import wait_for_condition, wait_until_server_available
 from ray.dashboard.tests.conftest import *  # noqa
@@ -14,7 +15,10 @@ def test_grafana_health(ray_start_with_dashboard):
     if not webui_url.startswith("http"):
         webui_url = "http://" + webui_url
 
-    assert wait_until_server_available(webui_url)
+    parsed = urllib.parse.urlparse(webui_url)
+    host_port = f"{parsed.hostname}:{parsed.port}"
+    assert wait_until_server_available(host_port)
+
     url = f"{webui_url}/api/grafana_health"
     assert wait_for_condition(
         lambda: requests.get(url, timeout=3).status_code == 200, timeout=10
@@ -35,7 +39,10 @@ def test_prometheus_health(ray_start_with_dashboard):
     if not webui_url.startswith("http"):
         webui_url = "http://" + webui_url
 
-    assert wait_until_server_available(webui_url)
+    parsed = urllib.parse.urlparse(webui_url)
+    host_port = f"{parsed.hostname}:{parsed.port}"
+    assert wait_until_server_available(host_port)
+
     url = f"{webui_url}/api/prometheus_health"
     assert wait_for_condition(
         lambda: requests.get(url, timeout=3).status_code == 200, timeout=10
@@ -56,7 +63,10 @@ async def test_async_health_check(ray_start_with_dashboard):
     if not webui_url.startswith("http"):
         webui_url = "http://" + webui_url
 
-    assert wait_until_server_available(webui_url)
+    parsed = urllib.parse.urlparse(webui_url)
+    host_port = f"{parsed.hostname}:{parsed.port}"
+    assert wait_until_server_available(host_port)
+
     url = f"{webui_url}/api/grafana_health"
     for _ in range(5):
         resp = await asyncio.to_thread(requests.get, url)
