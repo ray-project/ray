8b0aebf7d88b4a647828f2042dc2c28e69c51285
dentiny <dentinyhao@gmail.com>
2025-03-29 00:19:26 +0000

    stdout to a separate file
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- python/ray/_private/node.py
+++ python/ray/_private/node.py
@@ -1156,8 +1156,8 @@ class Node:
         """
         # Only redirect logs to .err. .err file is only useful when the
         # component has an unexpected output to stdout/stderr.
-        _, stderr_log_fname = self.get_log_file_names(
-            "dashboard", unique=True, create_out=False, create_err=True
+        stdout_log_fname, stderr_log_fname = self.get_log_file_names(
+            "dashboard", unique=True, create_out=True, create_err=True
         )
         self._webui_url, process_info = ray._private.services.start_api_server(
             include_dashboard,
@@ -1174,7 +1174,7 @@ class Node:
             fate_share=self.kernel_fate_share,
             max_bytes=self.max_bytes,
             backup_count=self.backup_count,
-            stdout_filepath=None,
+            stdout_filepath=stdout_log_fname,
             stderr_filepath=stderr_log_fname,
         )
         assert ray_constants.PROCESS_TYPE_DASHBOARD not in self.all_processes
