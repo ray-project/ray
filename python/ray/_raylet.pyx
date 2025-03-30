0e1a4fbde76c0e5bc8e5b23ed169c35efa2de09e
Justin Miller <justinrmiller@gmail.com>
2025-03-29 20:59:44 -0700

    Removing replace as the method isn't available.

--- python/ray/_raylet.pyx
+++ python/ray/_raylet.pyx
@@ -4389,6 +4389,7 @@ cdef class CoreWorker:
                     "Task {} has num_returns=0 but returned a non-None value {}. "
                     "The return value will be ignored.\n"
                 ).format(task_name.replace("()", ""), repr(num_outputs_stored))
+                ).format(task_name, repr(num_outputs_stored))
 
                 warnings.warn(task_num_returns_warning)
 
