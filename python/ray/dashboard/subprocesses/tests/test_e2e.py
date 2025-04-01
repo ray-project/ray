0eab79d3efd26962005b66c0d34bbd0c17173f54
Chi-Sheng Liu <chishengliu@chishengliu.com>
2025-04-02 07:10:58 +0800

    Fix temp_dir error
    Signed-off-by: Chi-Sheng Liu <chishengliu@chishengliu.com>

--- python/ray/dashboard/subprocesses/tests/test_e2e.py
+++ python/ray/dashboard/subprocesses/tests/test_e2e.py
@@ -27,15 +27,15 @@ def default_module_config(tmp_path) -> SubprocessModuleConfig:
         cluster_id_hex="test_cluster_id",
         gcs_address="",
         session_name="test_session",
-        temp_dir=str(tmp_path / "temp"),
-        session_dir=str(tmp_path / "session"),
+        temp_dir=str(tmp_path),
+        session_dir=str(tmp_path),
         logging_level=ray_constants.LOGGER_LEVEL,
         logging_format=ray_constants.LOGGER_FORMAT,
-        log_dir=str(tmp_path / "logs"),
+        log_dir=str(tmp_path),
         logging_filename=dashboard_consts.DASHBOARD_LOG_FILENAME,
         logging_rotate_bytes=ray_constants.LOGGING_ROTATE_BYTES,
         logging_rotate_backup_count=ray_constants.LOGGING_ROTATE_BACKUP_COUNT,
-        socket_dir=str(tmp_path / "socket"),
+        socket_dir=str(tmp_path),
     )
 
 
