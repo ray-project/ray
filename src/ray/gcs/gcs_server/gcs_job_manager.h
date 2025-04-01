c61b50de6faa1ccb6bc4816c4811cf737f7d8dcf
Dongjun Na <kmu5544616@gmail.com>
2025-04-01 09:53:14 +0900

    Refactor rename running_job_infos_ to running_job_start_times_
    Signed-off-by: Dongjun Na <kmu5544616@gmail.com>

--- src/ray/gcs/gcs_server/gcs_job_manager.h
+++ src/ray/gcs/gcs_server/gcs_job_manager.h
@@ -125,8 +125,9 @@ class GcsJobManager : public rpc::JobInfoHandler {
   // the same thread.
   ThreadChecker thread_checker_;
 
-  // Running Job Infos, used to report metrics.
-  absl::flat_hash_map<JobID, int64_t> running_job_infos_;
+  // Running Job Start Times, used to report metrics.
+  // Maps JobID to job start time in milliseconds since epoch.
+  absl::flat_hash_map<JobID, int64_t> running_job_start_times_;
 
   // Number of finished jobs since start of this GCS Server, used to report metrics.
   int64_t finished_jobs_count_ = 0;
