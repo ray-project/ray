f8dcd83c71b870e23b2b94bfea333a7a1219b21d
Cheng-Yeh Chung <kenchung285@gmail.com>
2025-04-02 00:32:55 +0900

    update
    Signed-off-by: Cheng-Yeh Chung <kenchung285@gmail.com>

--- ci/k8s/run-kuberay-doc-tests.sh
+++ ci/k8s/run-kuberay-doc-tests.sh
@@ -10,9 +10,6 @@ pip install -c python/requirements_compiled.txt pytest nbval bash_kernel
 python -m bash_kernel.install
 pip install "ray[default]==2.41.0"
 
-echo "--- Install tools for doctest"
-sudo apt-get install jq
-
 echo "--- Run a deliberate failure test to ensure the test script fails on error"
 # The following Jupyter notebook only contains a single cell that runs the `date` command.
 # The test script should fail because the output of the `date` command is different everytime.
