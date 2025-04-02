8514daeb9bc45eb78de963d176ed3200be5f16bd
Ziyi Tan <ajb459684460@gmail.com>
2025-04-02 22:57:51 +0800

    [core] replace :ray_common with sub-dependencies (#51814)
    Signed-off-by: Ziy1-Tan <ajb459684460@gmail.com>

--- src/ray/gcs/gcs_client/test/gcs_client_reconnection_test.cc
+++ src/ray/gcs/gcs_client/test/gcs_client_reconnection_test.cc
@@ -22,7 +22,6 @@
 #include "absl/strings/substitute.h"
 #include "gtest/gtest.h"
 #include "ray/common/asio/instrumented_io_context.h"
-#include "ray/common/test_util.h"
 #include "ray/gcs/gcs_client/accessor.h"
 #include "ray/gcs/gcs_client/gcs_client.h"
 #include "ray/gcs/gcs_server/gcs_server.h"
