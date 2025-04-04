f16a18df34c00e081389e7f738d0c46573f2edb2
Ryan O'Leary <ryanaoleary@google.com>
2025-04-04 23:57:09 +0000

    split up validation into helper functions
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>

--- python/ray/_private/label_utils.py
+++ python/ray/_private/label_utils.py
@@ -1,6 +1,9 @@
 import re
 import yaml
-from typing import Dict
+from typing import (
+    Dict,
+    Optional,
+)
 
 
 # Regex patterns used to validate that labels conform to Kubernetes label syntax rules.
@@ -79,28 +82,56 @@ def parse_node_labels_from_yaml_file(path: str) -> Dict[str, str]:
 def validate_node_labels(labels: Dict[str, str]):
     if labels is None:
         return
-    for key in labels.keys():
-        if "/" in key:
-            prefix, name = key.rsplit("/")
-            if len(prefix) > 253 or not re.match(LABEL_PREFIX_REGEX, prefix):
-                raise ValueError(
-                    f"Invalid label key prefix `{prefix}`. Prefix must be a series of DNS labels "
-                    f"separated by dots (.),not longer than 253 characters in total."
-                )
-        else:
-            name = key
-        if len(name) > 63 or not re.match(LABEL_REGEX, name):
-            raise ValueError(
-                f"Invalid label key name `{name}`. Name must be 63 chars or less beginning and ending "
-                f"with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_),"
-                f"dots (.), and alphanumerics between."
+    for key, value in labels.items():
+        possible_error_message = validate_label_key(key)
+        if possible_error_message:
+            raise ValueError(possible_error_message)
+        if value is not None:
+            possible_error_message = validate_label_value(value)
+            if possible_error_message:
+                raise ValueError(possible_error_message)
+
+
+def validate_label_key(key: str) -> Optional[str]:
+    if "/" in key:
+        prefix, name = key.rsplit("/", 1)
+        if len(prefix) > 253 or not re.match(LABEL_PREFIX_REGEX, prefix):
+            return str(
+                f"Invalid label key prefix `{prefix}`. Prefix must be a series of DNS labels "
+                f"separated by dots (.), not longer than 253 characters in total."
             )
-        value = labels.get(key)
-        if value is None or value == "":
-            return
-        if len(value) > 63 or not re.match(LABEL_REGEX, value):
-            raise ValueError(
-                f"Invalid label key value `{value}`. Value must be 63 chars or less beginning and ending "
-                f"with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_),"
-                f"dots (.), and alphanumerics between."
-            )
+    else:
+        name = key
+    if len(name) > 63 or not re.match(LABEL_REGEX, name):
+        return str(
+            f"Invalid label key name `{name}`. Name must be 63 chars or less beginning and ending "
+            f"with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_),"
+            f"dots (.), and alphanumerics between."
+        )
+    return None
+
+
+def validate_label_value(value: str) -> Optional[str]:
+    if value == "":
+        return None
+    if len(value) > 63 or not re.match(LABEL_REGEX, value):
+        return str(
+            f"Invalid label key value `{value}`. Value must be 63 chars or less beginning and ending "
+            f"with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_),"
+            f"dots (.), and alphanumerics between."
+        )
+    return None
+
+
+def validate_label_selector_value(selector: str) -> Optional[str]:
+    if selector == "":
+        return None
+    if not re.match(LABEL_SELECTOR_REGEX, selector):
+        return str(
+            f"Invalid label selector value `{selector}`. The label selector value should contain optional operators and a label value. Supported operators are: ! and {LABEL_OPERATORS}. "
+            f"Value must be 63 chars or less beginning and ending "
+            f"with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_),"
+            f"dots (.), and alphanumerics between."
+        )
+
+    return None
