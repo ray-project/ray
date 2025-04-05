71c5681fd31f4d987799fef3a2762751b386d8ea
Ryan O'Leary <ryanaoleary@google.com>
2025-04-05 00:16:19 +0000

    Parametrize functions where appropriate
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>

--- python/ray/tests/test_label_utils.py
+++ python/ray/tests/test_label_utils.py
@@ -8,27 +8,39 @@ import sys
 import tempfile
 
 
-def test_parse_node_labels_from_string():
-    # Empty label argument passed
-    labels_string = ""
-    labels_dict = parse_node_labels_string(labels_string)
-    assert labels_dict == {}
-
-    # Valid label key with empty value
-    labels_string = "region="
-    labels_dict = parse_node_labels_string(labels_string)
-    assert labels_dict == {"region": ""}
-
-    # Multiple valid label keys and values
-    labels_string = "ray.io/accelerator-type=A100,region=us-west4"
-    labels_dict = parse_node_labels_string(labels_string)
-    assert labels_dict == {"ray.io/accelerator-type": "A100", "region": "us-west4"}
-
-    # Invalid label
-    labels_string = "ray.io/accelerator-type=type=A100"
-    with pytest.raises(ValueError) as e:
-        parse_node_labels_string(labels_string)
-    assert "Label value is not a key-value pair" in str(e)
+@pytest.mark.parametrize(
+    "labels_string, should_raise, expected_result, expected_error_msg",
+    [
+        # Valid - Empty label string
+        ("", False, {}, None),
+        # Valid - Empty label value
+        ("region=", False, {"region": ""}, None),
+        # Valid - multiple label key-value pairs
+        (
+            "ray.io/accelerator-type=A100,region=us-west4",
+            False,
+            {"ray.io/accelerator-type": "A100", "region": "us-west4"},
+            None,
+        ),
+        # Invalid - label is not a key-value pair
+        (
+            "ray.io/accelerator-type=type=A100",
+            True,
+            None,
+            "Label value is not a key-value pair",
+        ),
+    ],
+)
+def test_parse_node_labels_from_string(
+    labels_string, should_raise, expected_result, expected_error_msg
+):
+    if should_raise:
+        with pytest.raises(ValueError) as e:
+            parse_node_labels_string(labels_string)
+        assert expected_error_msg in str(e.value)
+    else:
+        labels_dict = parse_node_labels_string(labels_string)
+        assert labels_dict == expected_result
 
 
 def test_parse_node_labels_from_yaml_file():
@@ -81,28 +93,38 @@ def test_parse_node_labels_from_yaml_file():
     assert 'The value of "gpu" is not string type' in str(e)
 
 
-def test_validate_node_labels():
-    # Invalid key prefix syntax
-    labels_dict = {"!invalidPrefix/accelerator-type": "A100"}
-    with pytest.raises(ValueError) as e:
+@pytest.mark.parametrize(
+    "labels_dict, expected_error, expected_message",
+    [
+        (  # Invalid key prefix syntax
+            {"!invalidPrefix/accelerator-type": "A100"},
+            ValueError,
+            "Invalid label key prefix",
+        ),
+        (  # Invalid key name syntax
+            {"!!accelerator-type?": "A100"},
+            ValueError,
+            "Invalid label key name",
+        ),
+        (  # Invalid value syntax
+            {"accelerator-type": "??"},
+            ValueError,
+            "Invalid label key value",
+        ),
+        (  # Valid label
+            {"accelerator-type": "A100"},
+            None,
+            None,
+        ),
+    ],
+)
+def test_validate_node_labels(labels_dict, expected_error, expected_message):
+    if expected_error:
+        with pytest.raises(expected_error) as e:
+            validate_node_labels(labels_dict)
+        assert expected_message in str(e.value)
+    else:
         validate_node_labels(labels_dict)
-    assert "Invalid label key prefix" in str(e)
-
-    # Invalid key name syntax
-    labels_dict = {"!!accelerator-type?": "A100"}
-    with pytest.raises(ValueError) as e:
-        validate_node_labels(labels_dict)
-    assert "Invalid label key name" in str(e)
-
-    # Invalid key value syntax
-    labels_dict = {"accelerator-type": "??"}
-    with pytest.raises(ValueError) as e:
-        validate_node_labels(labels_dict)
-    assert "Invalid label key value" in str(e)
-
-    # Valid node label
-    labels_dict = {"accelerator-type": "A100"}
-    validate_node_labels(labels_dict)
 
 
 if __name__ == "__main__":
