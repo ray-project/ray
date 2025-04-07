8b1d10380aaeb4b7a222d8c42cf137ffeb3ef0dc
dayshah <dhyey2019@gmail.com>
2025-04-07 10:21:52 -0700

    fix GetActorID
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- src/ray/core_worker/core_worker.h
+++ src/ray/core_worker/core_worker.h
@@ -1010,8 +1010,7 @@ class CoreWorker : public rpc::CoreWorkerServiceHandler {
   ///
 
   ActorID GetActorId() const {
-    // TODO(dayshah): Figure out why Java tests fail if we lock here.
-    // absl::MutexLock lock(&mutex_);
+    absl::MutexLock lock(&mutex_);
     return actor_id_;
   }
 
@@ -1832,8 +1831,7 @@ class CoreWorker : public rpc::CoreWorkerServiceHandler {
   mutable absl::Mutex mutex_;
 
   /// Our actor ID. If this is nil, then we execute only stateless tasks.
-  /// TODO(dayshah): Java tests fail if we access this without a mutex.
-  ActorID actor_id_;
+  ActorID actor_id_ ABSL_GUARDED_BY(mutex_);
 
   /// The currently executing task spec. We have to track this separately since
   /// we cannot access the thread-local worker contexts from GetCoreWorkerStats()
