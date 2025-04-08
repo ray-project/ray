58493a05780ad3b21e3e78896abbbb2704e371a1
Justin Yu <justinvyu@anyscale.com>
2025-04-08 16:59:49 -0700

    add worker initialized message
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/train/v2/_internal/execution/worker_group/worker_group.py
+++ python/ray/train/v2/_internal/execution/worker_group/worker_group.py
@@ -325,6 +325,17 @@ class WorkerGroup:
             ]
         )
 
+        workers_info = "\n".join(
+            [
+                f"- (node_id={w.metadata.node_id}, ip={w.metadata.node_ip}, "
+                f"pid={w.metadata.pid}) world_rank={w.distributed_context.world_rank}, "
+                f"local_rank={w.distributed_context.local_rank}, "
+                f"node_rank={w.distributed_context.node_rank}"
+                for w in workers
+            ]
+        )
+        logger.info(f"Started training worker processes: \n{workers_info}")
+
         for callback in self._callbacks:
             callback.after_worker_group_training_start(self)
 
