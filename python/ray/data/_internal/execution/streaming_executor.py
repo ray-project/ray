d473d34d5aa0e26e906fc32162c193014e2f529c
can <can@anyscale.com>
2025-04-09 21:22:03 +0000

    [data.dashboard.x/1] fix operator state
    Signed-off-by: can <can@anyscale.com>

--- python/ray/data/_internal/execution/streaming_executor.py
+++ python/ray/data/_internal/execution/streaming_executor.py
@@ -435,7 +435,8 @@ class StreamingExecutor(Executor, threading.Thread):
                     "progress": op_state.num_completed_tasks,
                     "total": op.num_outputs_total(),
                     "total_rows": op.num_output_rows_total(),
-                    "state": state,
+                    "state": 
+                        DatasetState.FINISHED.name if op_state.num_completed_tasks == op.num_outputs_total() and op.num_outputs_total() > 0 else state,
                 }
                 for i, (op, op_state) in enumerate(self._topology.items())
             },
