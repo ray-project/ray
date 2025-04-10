07a3b75a0e62ee39f400f7d5c097d2d34fe6f1c8
sharkdtu <sharkdtu@tencent.com>
2025-04-10 14:33:10 +0800

    [data] Add overwrite options for iceberg sink
    Signed-off-by: sharkdtu <sharkdtu@tencent.com>

--- python/ray/data/dataset.py
+++ python/ray/data/dataset.py
@@ -3565,6 +3565,7 @@ class Dataset:
         table_identifier: str,
         catalog_kwargs: Optional[Dict[str, Any]] = None,
         snapshot_properties: Optional[Dict[str, str]] = None,
+        overwrite_filter: Union["BooleanExpression", str] = None,
         ray_remote_args: Dict[str, Any] = None,
         concurrency: Optional[int] = None,
     ) -> None:
@@ -3596,6 +3597,8 @@ class Dataset:
                 #pyiceberg.catalog.load_catalog>`_.
             snapshot_properties: custom properties write to snapshot when committing
                 to an iceberg table.
+            overwrite_filter: A boolean expression in case of a partial overwrite,
+                e.g. EqualTo("day", "2025-05-13"). default is None, which means append.
             ray_remote_args: kwargs passed to :func:`ray.remote` in the write tasks.
             concurrency: The maximum number of Ray tasks to run concurrently. Set this
                 to control number of tasks to run concurrently. This doesn't change the
@@ -3604,7 +3607,10 @@ class Dataset:
         """
 
         datasink = IcebergDatasink(
-            table_identifier, catalog_kwargs, snapshot_properties
+            table_identifier,
+            catalog_kwargs,
+            snapshot_properties,
+            overwrite_filter,
         )
 
         self.write_datasink(
@@ -5546,9 +5552,9 @@ class Dataset:
         import pyarrow as pa
 
         ref_bundles: Iterator[RefBundle] = self.iter_internal_ref_bundles()
-        block_refs: List[
-            ObjectRef["pyarrow.Table"]
-        ] = _ref_bundles_iterator_to_block_refs_list(ref_bundles)
+        block_refs: List[ObjectRef["pyarrow.Table"]] = (
+            _ref_bundles_iterator_to_block_refs_list(ref_bundles)
+        )
         # Schema is safe to call since we have already triggered execution with
         # iter_internal_ref_bundles.
         schema = self.schema(fetch_if_missing=True)
