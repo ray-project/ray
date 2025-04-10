eb05678c7f265759df775e4e5bddb9e35663ace0
dayshah <dhyey2019@gmail.com>
2025-04-10 16:37:56 -0700

    to debug
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- src/ray/raylet/scheduling/local_resource_manager.cc
+++ src/ray/raylet/scheduling/local_resource_manager.cc
@@ -289,7 +289,7 @@ void LocalResourceManager::UpdateAvailableObjectStoreMemResource() {
       last_idle_times_[ResourceID::ObjectStoreMemory()] = absl::Now();
     } else {
       // Clear the idle info since we know it's being used.
-      RAY_LOG_EVERY_MS(INFO, 5000) << "Object store memory is not idle.";
+      RAY_LOG(DEBUG) << "Object store memory is not idle.";
       last_idle_times_[ResourceID::ObjectStoreMemory()] = absl::nullopt;
     }
 
