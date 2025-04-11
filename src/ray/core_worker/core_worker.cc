6bd7e398350811153f22a87c4e8e38ce0f340b68
Kai-Hsun Chen <kaihsun@apache.org>
2025-04-11 00:19:59 -0700

    Update src/ray/core_worker/core_worker.cc
    Co-authored-by: Jiajun Yao <jeromeyjj@gmail.com>
    Signed-off-by: Kai-Hsun Chen <kaihsun@apache.org>

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -1371,7 +1371,7 @@ void CoreWorker::InternalHeartbeat() {
   std::vector<TaskToRetry> tasks_to_resubmit;
   {
     absl::MutexLock lock(&mutex_);
-    auto current_time = current_time_ms();
+    const auto current_time = current_time_ms();
     while (!to_resubmit_.empty() && current_time > to_resubmit_.top().execution_time_ms) {
       tasks_to_resubmit.emplace_back(to_resubmit_.top());
       to_resubmit_.pop();
