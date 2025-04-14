6aa898e633312970c48c21c7afb8dc6524728e47
Justin Yu <justinvyu@anyscale.com>
2025-04-14 16:14:57 -0700

    fix torch install
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- release/ray_release/byod/byod_install_torchrec.sh
+++ release/ray_release/byod/byod_install_torchrec.sh
@@ -3,7 +3,7 @@
 
 set -exo pipefail
 
-pip3 install torch==2.6.0+cu121 \
+pip3 install torch==2.6.0 \
   torchrec==1.1.0 \
   fbgemm-gpu==1.1.0 \
   --extra-index-url https://download.pytorch.org/whl/cu121
