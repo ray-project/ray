9022599e3c3ebd01e26a8ba5f7f97d21e046f096
GokuMohandas <gokumd@gmail.com>
2025-04-15 04:03:50 +0000

    removed ray updating deps and yes will use lockfile later

--- release/ray_release/byod/byod_llm_ner.sh
+++ release/ray_release/byod/byod_llm_ner.sh
@@ -3,14 +3,12 @@
 set -exo pipefail
 
 # Install Python dependencies
-pip3 install --no-cache-dir --upgrade \
-    "ray[serve,llm]>=2.44.0" \
-    "vllm>=0.7.2" \
+pip3 install --no-cache-dir \
     "xgrammar==0.1.11" \
     "pynvml==12.0.0" \
     "hf_transfer==0.1.9" \
-    "tensorboard" \
-    "git+https://github.com/hiyouga/LLaMA-Factory.git#egg=llamafactory"
+    "tensorboard==2.19.0" \
+    "git+https://github.com/hiyouga/LLaMA-Factory.git@ac8c6fdd3ab7fb6372f231f238e6b8ba6a17eb16#egg=llamafactory"
 
 # Env vars
 export HF_HUB_ENABLE_HF_TRANSFER=1
