b4f6465bea87ad5f731411ca50c0a8912942160f
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2025-04-16 14:41:30 -0700

    [release] remove unused champ image building (#52358)
    not used anymore, and does not really work now.
    
    Signed-off-by: Lonnie Liu <lonnie@anyscale.com>

--- release/ray_release/tests/test_byod_build.py
+++ release/ray_release/tests/test_byod_build.py
@@ -10,8 +10,6 @@ from ray_release.test import Test
 from ray_release.byod.build import (
     build_anyscale_custom_byod_image,
     build_anyscale_base_byod_images,
-    build_champagne_image,
-    DATAPLANE_FILENAME,
     _get_ray_commit,
 )
 
@@ -39,37 +37,6 @@ def test_get_ray_commit() -> None:
     )
     assert _get_ray_commit({"BUILDKITE_COMMIT": "987789"}) == "987789"
     assert _get_ray_commit({"PATH": "/usr/bin"}) == ""
-
-
-def test_build_anyscale_champagne_image() -> None:
-    cmds = []
-
-    def _mock_check_call(
-        cmd: List[str],
-        *args,
-        **kwargs,
-    ) -> None:
-        cmds.append(cmd)
-
-    with patch.dict(
-        "os.environ",
-        {"BUILDKITE_COMMIT": "abc123", "BUILDKITE_BRANCH": "master"},
-    ), patch(
-        "ray_release.byod.build._download_dataplane_build_file",
-        return_value=None,
-    ), patch(
-        "subprocess.check_call",
-        side_effect=_mock_check_call,
-    ), patch(
-        "subprocess.check_output",
-        return_value=b"abc123",
-    ), open(
-        DATAPLANE_FILENAME, "wb"
-    ) as _:
-        build_champagne_image("2.5.1", "py37", "cpu")
-        assert "docker build --build-arg BASE_IMAGE=rayproject/ray:2.5.1-py37 -t "
-        "029272617770.dkr.ecr.us-west-2.amazonaws.com/"
-        "anyscale/ray:champagne-2.5.1 -" == " ".join(cmds[0])
 
 
 init_global_config(bazel_runfile("release/ray_release/configs/oss_config.yaml"))
