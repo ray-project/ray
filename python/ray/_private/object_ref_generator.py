d89448600dd3bd75b6da3a5094734db890659ffd
machichima <nary12321@gmail.com>
2025-04-17 22:01:25 +0800

    refactor: add type hints -e Signed-off-by: machichima <nary12321@gmail.com>

--- python/ray/_private/object_ref_generator.py
+++ python/ray/_private/object_ref_generator.py
@@ -1,17 +1,19 @@
+from __future__ import annotations
 from ray.util.annotations import DeveloperAPI
+from typing import Iterator, List
 
 
 @DeveloperAPI
 class DynamicObjectRefGenerator:
-    def __init__(self, refs):
+    def __init__(self, refs: List["ray.ObjectRef"]):
         # TODO(swang): As an optimization, can also store the generator
         # ObjectID so that we don't need to keep individual ref counts for the
         # inner ObjectRefs.
-        self._refs = refs
+        self._refs: List["ray.ObjectRef"] = refs
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator("ray.ObjectRef"):
         for ref in self._refs:
             yield ref
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._refs)
