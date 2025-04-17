797e4ead7c14c9518543733ab5d9d8ea8b6af575
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-04-17 09:25:32 -0500

    Update src/ray/core_worker/core_worker.h
    Co-authored-by: Ibrahim Rabbani <israbbani@gmail.com>
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/core_worker/core_worker.h
+++ src/ray/core_worker/core_worker.h
@@ -1893,7 +1893,7 @@ class CoreWorker : public rpc::CoreWorkerServiceHandler {
   /// `Shutdown` should be unified to avoid mistakes due to complex dependent semantics.
   /// See https://github.com/ray-project/ray/issues/51642.
 
-  /// Used to ensure that the `Exit` method is called at most once.
+  /// Used to ensure that the `CoreWorker::Exit` method is called at most once.
   std::atomic<bool> is_exited_ = false;
   /// Used to ensure that the `Shutdown` method is called at most once.
   std::atomic<bool> is_shutdown_ = false;
