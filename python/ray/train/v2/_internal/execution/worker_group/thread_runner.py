f98abeee185408e7a55c6254f76aab614c19e82e
Justin Yu <justinvyu@anyscale.com>
2025-04-21 16:20:03 -0700

    add a util for getting callable name
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/train/v2/_internal/execution/worker_group/thread_runner.py
+++ python/ray/train/v2/_internal/execution/worker_group/thread_runner.py
@@ -28,6 +28,10 @@ class ThreadRunner:
         if self._thread is not None:
             raise RuntimeError("Thread is already running.")
 
+        import ipdb
+
+        ipdb.set_trace()
+
         def _run_target():
             with self._lock:
                 self._is_running = True
