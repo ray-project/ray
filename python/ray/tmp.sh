4e84bf5e8fadc04bcaa772552c7312caf2283e64
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-04-23 06:58:38 -0700

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/tmp.sh
+++
@@ -1,9 +0,0 @@
-for region in $(aws ec2 describe-regions --query "Regions[].RegionName" --output text); do
-  echo "Region: $region";
-  aws ec2 describe-images \
-    --region $region \
-    --filters "Name=name,Values=Deep Learning AMI (Ubuntu 18.04) Version 71.0*" \
-    --query "Images[].[ImageId, Name]" \
-    --output text;
-  echo "--------------------------";
-done
