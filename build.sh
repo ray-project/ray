674a04fe644025f61bb02638a198c096a7868b59
Omkar Kulkarni <omkar@omkar-JKJHCX74L6.local>
2025-04-23 18:33:22 -0700

    feat: simple commit

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ if [ "${OSTYPE}" = msys ]; then
   echo "WARNING: ${0##*/} is not recommended on MSYS2, as MSYS2 alters the build environment."
 fi
 
-# test commit 
+# test commit2
 if [ -z "${PYTHON3_BIN_PATH-}" ]; then
   PYTHON3_BIN_PATH="$(command -v python3 || command -v python || echo python)"
 fi
