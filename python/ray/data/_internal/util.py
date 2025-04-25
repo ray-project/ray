d367cd50426c59ce8e27df598267dbc2ef072e17
jhsu <jhsu@anyscale.com>
2025-04-25 15:17:57 -0700

    Update from_numpy and from_torch
    Signed-off-by: jhsu <jhsu@anyscale.com>

--- python/ray/data/_internal/util.py
+++ python/ray/data/_internal/util.py
@@ -686,13 +686,15 @@ def pandas_df_to_arrow_block(df: "pandas.DataFrame") -> "Block":
     )
 
 
-def ndarray_to_block(ndarray: np.ndarray, ctx: DataContext) -> "Block":
+def ndarray_to_block(
+    ndarray: np.ndarray, ctx: DataContext, column_name: str = "data"
+) -> "Block":
     from ray.data.block import BlockAccessor, BlockExecStats
 
     DataContext._set_current(ctx)
 
     stats = BlockExecStats.builder()
-    block = BlockAccessor.batch_to_block({"data": ndarray})
+    block = BlockAccessor.batch_to_block({column_name: ndarray})
     metadata = BlockAccessor.for_block(block).get_metadata(exec_stats=stats.build())
     return block, metadata
 
