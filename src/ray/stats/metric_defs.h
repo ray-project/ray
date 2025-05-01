a90a8184f990a18b031cf95cc2ecf44e0474f6fe
Dhyey Shah <dhyey2019@gmail.com>
2025-05-01 11:06:06 -0400

    [core] Add grpc server success and fail count metric (#52711)
    Add a grpc server success and failure count, so we know which + how many
    replies failed to make it.
    
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- src/ray/stats/metric_defs.h
+++ src/ray/stats/metric_defs.h
@@ -70,6 +70,8 @@ DECLARE_stats(grpc_server_req_process_time_ms);
 DECLARE_stats(grpc_server_req_new);
 DECLARE_stats(grpc_server_req_handling);
 DECLARE_stats(grpc_server_req_finished);
+DECLARE_stats(grpc_server_req_succeeded);
+DECLARE_stats(grpc_server_req_failed);
 
 /// Object Manager.
 DECLARE_stats(object_manager_bytes);
