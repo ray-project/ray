341694a27453c8454554f11900a6f9b2618c010d
Gene Der Su <e870252314@gmail.com>
2025-05-01 13:42:00 -0700

    [Serve.llm][CI] test Ray Serve LLM with vLLM v1 (#52668)
    Signed-off-by: Gene Su <e870252314@gmail.com>

--- python/ray/llm/_internal/serve/deployments/llm/vllm/vllm_engine.py
+++ python/ray/llm/_internal/serve/deployments/llm/vllm/vllm_engine.py
@@ -4,6 +4,7 @@ import time
 from typing import Any, AsyncGenerator, Dict, List, Optional, Tuple, TYPE_CHECKING
 
 import ray
+import re
 from ray.util import metrics
 from ray.util.placement_group import PlacementGroup
 from ray.util.scheduling_strategies import PlacementGroupSchedulingStrategy
@@ -73,6 +74,10 @@ time_in_queue_histogram = metrics.Histogram(
     boundaries=LONG_RANGE_LATENCY_HISTOGRAM_BUCKETS_MS,
 )
 
+V1_TOO_LONG_PATTERN = re.compile(
+    r".* (\d+).* is longer than the maximum model length of (\d+).*"
+)
+
 
 def _get_async_engine_args(llm_config: LLMConfig) -> "AsyncEngineArgs":
     engine_config = llm_config.get_engine_config()
@@ -90,6 +95,7 @@ def _get_async_engine_args(llm_config: LLMConfig) -> "AsyncEngineArgs":
         **{
             "model": model,
             "distributed_executor_backend": "ray",
+            "guided_decoding_backend": RAYLLM_GUIDED_DECODING_BACKEND,
             "disable_log_stats": False,
             **engine_config.get_initialization_kwargs(),
         }
@@ -646,6 +652,11 @@ class VLLMEngine(LLMEngine):
             if len(error_args) == 3 and "Input too long." == error_args[0]:
                 _, input_length, max_input_length = error_args
                 raise InputTooLong(input_length, max_input_length).exception from None
+            elif len(error_args) == 1 and V1_TOO_LONG_PATTERN.match(error_args[0]):
+                parsed_error = V1_TOO_LONG_PATTERN.match(error_args[0])
+                raise InputTooLong(
+                    int(parsed_error[1]), int(parsed_error[2])
+                ).exception from None
             else:
                 raise e from None
         finally:
