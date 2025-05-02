28a4b442981fddf58d0d3fdf85bb41ff7b3526e9
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-05-02 14:02:14 -0500

    WIP
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/tests/test_logging.py
+++ python/ray/tests/test_logging.py
@@ -1113,7 +1113,8 @@ def test_log_monitor_ip_correct(ray_start_cluster):
     cluster = ray_start_cluster
     # add first node
     cluster.add_node(
-        node_ip_address="127.0.0.2", resources={"foobar": 1},
+        node_ip_address="127.0.0.2",
+        resources={"foobar": 1},
     )
     address = cluster.address
     ray.init(address)
