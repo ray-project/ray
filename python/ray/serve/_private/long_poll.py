96a632d8bbc5883f5ac5dac5fd100d3ee6b952ed
Chi-Sheng Liu <chishengliu@chishengliu.com>
2025-05-03 13:46:16 +0800

    [CI] Combine "as" imports for isort (#52742)
    isort cleanup

--- python/ray/serve/_private/long_poll.py
+++ python/ray/serve/_private/long_poll.py
@@ -15,12 +15,12 @@ from ray._common.utils import get_or_create_event_loop
 from ray.serve._private.constants import SERVE_LOGGER_NAME
 from ray.serve.generated.serve_pb2 import (
     DeploymentTargetInfo,
+    EndpointInfo as EndpointInfoProto,
     EndpointSet,
     LongPollRequest,
     LongPollResult,
+    UpdatedObject as UpdatedObjectProto,
 )
-from ray.serve.generated.serve_pb2 import EndpointInfo as EndpointInfoProto
-from ray.serve.generated.serve_pb2 import UpdatedObject as UpdatedObjectProto
 from ray.util import metrics
 
 logger = logging.getLogger(SERVE_LOGGER_NAME)
