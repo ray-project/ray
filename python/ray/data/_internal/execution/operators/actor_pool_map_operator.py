9c2845f8d2051763ebf96b89703437b7c6c76261
Alexey Kudinkin <ak@anyscale.com>
2025-05-02 22:05:53 -0700

    Override `ActorPoolMapOperator.completed`
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/data/_internal/execution/operators/actor_pool_map_operator.py
+++ python/ray/data/_internal/execution/operators/actor_pool_map_operator.py
@@ -136,6 +136,10 @@ class ActorPoolMapOperator(MapOperator):
     def internal_queue_size(self) -> int:
         return len(self._bundle_queue)
 
+    def completed(self) -> bool:
+        # TODO separate marking as completed from the check
+        return self._inputs_complete and self._bundle_queue.is_empty() and super().completed()
+
     def start(self, options: ExecutionOptions):
         self._actor_locality_enabled = options.actor_locality_enabled
         super().start(options)
