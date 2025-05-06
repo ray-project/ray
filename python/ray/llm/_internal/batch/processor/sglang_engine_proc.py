31fbddd4f492b43efbf8bc2af4021e3f71a3e8c9
Linkun Chen <github@lkchen.net>
2025-05-06 13:17:55 -0700

    unify config
    Signed-off-by: Linkun Chen <github@lkchen.net>

--- python/ray/llm/_internal/batch/processor/sglang_engine_proc.py
+++ python/ray/llm/_internal/batch/processor/sglang_engine_proc.py
@@ -9,7 +9,7 @@ from ray.data.block import UserDefinedFunction
 import ray
 from ray.llm._internal.batch.processor.base import (
     Processor,
-    ProcessorConfig,
+    OfflineProcessorConfig,
     ProcessorBuilder,
 )
 from ray.llm._internal.batch.stages import (
@@ -33,13 +33,10 @@ import transformers
 DEFAULT_MODEL_ARCHITECTURE = "UNKNOWN_MODEL_ARCHITECTURE"
 
 
-class SGLangEngineProcessorConfig(ProcessorConfig):
+class SGLangEngineProcessorConfig(OfflineProcessorConfig):
     """The configuration for the SGLang engine processor."""
 
     # SGLang stage configurations.
-    model_source: str = Field(
-        description="The model source to use for the SGLang engine.",
-    )
     engine_kwargs: Dict[str, Any] = Field(
         default_factory=dict,
         description="The kwargs to pass to the SGLang engine. See "
@@ -50,42 +47,6 @@ class SGLangEngineProcessorConfig(ProcessorConfig):
         default=SGLangTaskType.GENERATE,
         description="The task type to use. If not specified, will use "
         "'generate' by default.",
-    )
-    runtime_env: Optional[Dict[str, Any]] = Field(
-        default=None,
-        description="The runtime environment to use for the SGLang engine.",
-    )
-    max_pending_requests: Optional[int] = Field(
-        default=None,
-        description="The maximum number of pending requests. If not specified, "
-        "will use the default value from the SGLang engine.",
-    )
-    max_concurrent_batches: int = Field(
-        default=4,
-        description="The maximum number of concurrent batches in the engine. "
-        "This is to overlap the batch processing to avoid the tail latency of "
-        "each batch. The default value may not be optimal when the batch size "
-        "or the batch processing latency is too small, but it should be good "
-        "enough for batch size >= 64.",
-    )
-
-    # Processor stage configurations.
-    apply_chat_template: bool = Field(
-        default=True, description="Whether to apply chat template."
-    )
-    chat_template: Optional[str] = Field(
-        default=None,
-        description="The chat template to use. This is usually not needed if the "
-        "model checkpoint already contains the chat template.",
-    )
-    tokenize: bool = Field(
-        default=True,
-        description="Whether to tokenize the input before passing it to the "
-        "SGLang engine. If not, SGLang will tokenize the prompt in the engine.",
-    )
-    detokenize: bool = Field(
-        default=True,
-        description="Whether to detokenize the output.",
     )
 
     @root_validator(pre=True)
