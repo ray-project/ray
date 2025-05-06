b417883432899d5900007dd93ae2d15324c5df32
Alexey Kudinkin <ak@anyscale.com>
2025-05-06 15:38:16 -0700

    `lint`
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/data/tests/test_json.py
+++ python/ray/data/tests/test_json.py
@@ -740,11 +740,7 @@ def test_mixed_gzipped_json_files(ray_start_regular_shared, tmp_path):
 
 @pytest.mark.parametrize(
     "ray_start_10_cpus_shared",
-    [
-        {
-            "object_store_memory": 1 * GiB
-        }
-    ],
+    [{"object_store_memory": 1 * GiB}],
     indirect=True,
 )
 def test_json_with_http_path_parallelization(ray_start_10_cpus_shared):
