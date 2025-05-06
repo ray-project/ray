771e7c731ecc71ec2c9b6afcbe982f9a1140cba6
dayshah <dhyey2019@gmail.com>
2025-05-06 15:03:15 -0700

    fix
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- src/ray/rpc/metrics_agent_client.h
+++ src/ray/rpc/metrics_agent_client.h
@@ -51,7 +51,7 @@ class MetricsAgentClientImpl : public MetricsAgentClient {
   MetricsAgentClientImpl(const std::string &address,
                          const int port,
                          instrumented_io_context &io_service)
-      : client_call_manager_(io_service) {
+      : client_call_manager_(io_service, true) {
     RAY_LOG(DEBUG) << "Initiate the metrics client of address:" << address
                    << " port:" << port;
     grpc_client_ = std::make_unique<GrpcClient<ReporterService>>(
