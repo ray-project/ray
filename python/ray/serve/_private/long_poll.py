444ba86acf761ed558effbd5a04e03df8b5a2b97
Josh Karpel <josh.karpel@gmail.com>
2025-05-06 11:50:37 -0500

    use call_soon_threadsafe instead
    Signed-off-by: Josh Karpel <josh.karpel@gmail.com>

--- python/ray/serve/_private/long_poll.py
+++ python/ray/serve/_private/long_poll.py
@@ -118,6 +118,20 @@ class LongPollClient:
         but the snapshot ID will be preserved, so the new listener will only be called
         on the *next* update to that key.
         """
+        # We need to run the underlying method in the same event loop that runs
+        # the long poll loop, because we need to mutate the mapping of snapshot IDs,
+        # which also needs to be serialized by the long poll's RPC to the
+        # Serve Controller. If those happened concurrently in different threads,
+        # we could get a `RuntimeError: dictionary changed size during iteration`.
+        # See https://github.com/ray-project/ray/pull/52793 for more details.
+        self.event_loop.call_soon_threadsafe(self._add_key_listeners, key_listeners)
+
+    def _add_key_listeners(
+        self, key_listeners: Dict[KeyType, UpdateStateCallable]
+    ) -> None:
+        """Inner method that actually adds the key listeners, to be called
+        via call_soon_threadsafe for thread safety.
+        """
         # Only initialize snapshot ids for *new* keys.
         self.snapshot_ids.update(
             {key: -1 for key in key_listeners.keys() if key not in self.key_listeners}
@@ -144,13 +158,7 @@ class LongPollClient:
             return
 
         self._callbacks_processed_count = 0
-        self._current_ref = self.host_actor.listen_for_change.remote(
-            # Copy the snapshot IDs before serializing
-            # because they might be mutated from a different thread concurrently
-            # (from the SharedRouterLongPollClient registering a new router).
-            # See https://github.com/ray-project/ray/pull/52793 for more details.
-            self.snapshot_ids.copy()
-        )
+        self._current_ref = self.host_actor.listen_for_change.remote(self.snapshot_ids)
         self._current_ref._on_completed(lambda update: self._process_update(update))
 
     def _schedule_to_event_loop(self, callback):
