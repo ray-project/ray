c466030fa00e11eb7a0c07b0b2a77132abc84f2e
dayshah <dhyey2019@gmail.com>
2025-05-06 19:02:15 -0700

    fix cpp test build
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- src/ray/rpc/test/grpc_server_client_test.cc
+++ src/ray/rpc/test/grpc_server_client_test.cc
@@ -217,6 +217,7 @@ TEST_F(TestGrpcServerClientFixture, TestClientCallManagerTimeout) {
   grpc_client_.reset();
   client_call_manager_.reset();
   client_call_manager_.reset(new ClientCallManager(client_io_service_,
+                                                   false,
                                                    ClusterID::Nil(),
                                                    /*num_thread=*/1,
                                                    /*call_timeout_ms=*/100));
@@ -251,6 +252,7 @@ TEST_F(TestGrpcServerClientFixture, TestClientDiedBeforeReply) {
   grpc_client_.reset();
   client_call_manager_.reset();
   client_call_manager_.reset(new ClientCallManager(client_io_service_,
+                                                   false,
                                                    ClusterID::Nil(),
                                                    /*num_thread=*/1,
                                                    /*call_timeout_ms=*/100));
@@ -282,7 +284,7 @@ TEST_F(TestGrpcServerClientFixture, TestClientDiedBeforeReply) {
   }
   // Reinit client with infinite timeout.
   client_call_manager_.reset(
-      new ClientCallManager(client_io_service_, ClusterID::FromRandom()));
+      new ClientCallManager(client_io_service_, false, ClusterID::FromRandom()));
   grpc_client_.reset(new GrpcClient<TestService>(
       "127.0.0.1", grpc_server_->GetPort(), *client_call_manager_));
   // Send again, this request should be replied. If any leaking happened, this call won't
