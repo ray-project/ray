22bb0b5d6f6cd4256c4af5f092fc37b09206273e
can <can@anyscale.com>
2025-05-07 19:43:31 +0000

    [core][obs/1] aggregate ray tasks + actors at node level
    Signed-off-by: can <can@anyscale.com>

--- python/ray/_private/ray_constants.py
+++ python/ray/_private/ray_constants.py
@@ -565,3 +565,11 @@ RAY_EXPORT_EVENT_MAX_FILE_SIZE_BYTES = env_bool(
 )
 
 RAY_EXPORT_EVENT_MAX_BACKUP_COUNT = env_bool("RAY_EXPORT_EVENT_MAX_BACKUP_COUNT", 20)
+
+# Prometheus metric cardinality level setting, either "legacy" or "recommended".
+#
+# Legacy: report all metrics to prometheus with the set of labels that are reported by
+#   the component, including WorkerId, (task or actor) Name, etc. This is the default.
+# Recommended: report only the node level metrics to prometheus. This means that the
+#   WorkerId will be removed from all metrics.
+RAY_METRIC_CARDINALITY_LEVEL = os.environ.get("RAY_metric_cardinality_level", "legacy")
---
+++ python/ray/tests/test_aggregated_prometheus_metrics.py
@@ -0,0 +1,124 @@
+# isort: skip_file
+# ruff: noqa: E402
+import sys
+import requests
+import os
+
+import pytest
+
+import ray
+from ray._private.test_utils import (
+    fetch_prometheus_metrics,
+    wait_for_assertion,
+)
+from ray._private.metrics_agent import WORKER_ID_TAG_KEY
+
+
+try:
+    import prometheus_client
+except ImportError:
+    prometheus_client = None
+
+
+_TO_TEST_METRICS = ["ray_tasks", "ray_actors"]
+
+
+@pytest.fixture
+def _setup_cluster_for_test(request, ray_start_cluster):
+    core_metric_cardinality_level = request.param
+    os.environ["RAY_metric_cardinality_level"] = core_metric_cardinality_level
+    cluster = ray_start_cluster
+    cluster.add_node(
+        _system_config={
+            "metrics_report_interval_ms": 1000,
+            "enable_metrics_collection": True,
+            "metric_cardinality_level": core_metric_cardinality_level,
+        }
+    )
+    cluster.wait_for_nodes()
+    ray_context = ray.init(
+        address=cluster.address,
+    )
+
+    @ray.remote
+    def t():
+        print("task")
+
+    @ray.remote
+    class A:
+        async def run(self):
+            print("actor")
+
+    a = A.remote()
+    obj_refs = [t.remote(), a.run.remote()]
+
+    # Make a request to the dashboard to produce some dashboard metrics
+    requests.get(f"http://{ray_context.dashboard_url}/nodes")
+
+    node_info_list = ray.nodes()
+    prom_addresses = []
+    for node_info in node_info_list:
+        prom_addresses.append(
+            f"{node_info['NodeManagerAddress']}:{node_info['MetricsExportPort']}"
+        )
+    yield prom_addresses
+
+    ray.get(obj_refs)
+
+
+@pytest.mark.skipif(prometheus_client is None, reason="Prometheus not installed")
+@pytest.mark.parametrize("_setup_cluster_for_test", ["recommended"], indirect=True)
+def test_recommended_cardinality(_setup_cluster_for_test):
+    """
+    Test that the ray_tasks and ray_actors metric are reported without the WorkerId tag
+    """
+    TEST_TIMEOUT_S = 30
+    prom_addresses = _setup_cluster_for_test
+
+    def _validate():
+        metric_samples = fetch_prometheus_metrics(prom_addresses)
+        for metric in _TO_TEST_METRICS:
+            samples = metric_samples.get(metric)
+            assert samples, f"Metric {metric} not found in samples"
+            for sample in samples:
+                assert (
+                    sample.labels.get(WORKER_ID_TAG_KEY) is None
+                ), f"Sample {sample} contains WorkerId tag"
+
+    wait_for_assertion(
+        _validate,
+        timeout=TEST_TIMEOUT_S,
+        retry_interval_ms=1000,  # Yield resource for other processes
+    )
+
+
+@pytest.mark.skipif(prometheus_client is None, reason="Prometheus not installed")
+@pytest.mark.parametrize("_setup_cluster_for_test", ["legacy"], indirect=True)
+def test_legacy_cardinality(_setup_cluster_for_test):
+    """
+    Test that the ray_tasks and ray_actors metric are reported without the WorkerId tag
+    """
+    TEST_TIMEOUT_S = 30
+    prom_addresses = _setup_cluster_for_test
+
+    def _validate():
+        metric_samples = fetch_prometheus_metrics(prom_addresses)
+        for metric in _TO_TEST_METRICS:
+            samples = metric_samples.get(metric)
+            assert samples, f"Metric {metric} not found in samples"
+            for sample in samples:
+                assert (
+                    sample.labels.get(WORKER_ID_TAG_KEY) is not None
+                ), f"Sample {sample} does not contain WorkerId tag"
+
+    wait_for_assertion(
+        _validate,
+        timeout=TEST_TIMEOUT_S,
+        retry_interval_ms=1000,  # Yield resource for other processes
+    )
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(pytest.main(["-sv", __file__]))
