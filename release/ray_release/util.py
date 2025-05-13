ad8bde606066289b51b1668e7f22365abb25b816
kevin <kevin@anyscale.com>
2025-05-13 09:01:28 +0000

    p
    Signed-off-by: kevin <kevin@anyscale.com>

--- release/ray_release/util.py
+++ release/ray_release/util.py
@@ -11,6 +11,7 @@ from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple
 import requests
 from ray_release.logger import logger
 from ray_release.configs.global_config import get_global_config
+from ray_release.exception import ClusterEnvCreateError
 
 if TYPE_CHECKING:
     from anyscale.sdk.anyscale_client.sdk import AnyscaleSDK
@@ -206,3 +207,63 @@ def join_cloud_storage_paths(*paths: str):
     while joined_path[-1] == "/":
         joined_path = joined_path[:-1]
     return joined_path
+
+
+def get_custom_cluster_env_name(image: str, test_name: str) -> str:
+    image_normalized = image.replace("/", "_").replace(":", "_").replace(".", "_")
+    return f"test_env_{image_normalized}_{test_name}"
+
+
+def create_cluster_env_from_image(
+    image: str, test_name: str, runtime_env: Dict[str, Any]
+) -> str:
+    anyscale_sdk = get_anyscale_sdk()
+    cluster_env_name = get_custom_cluster_env_name(image, test_name)
+
+    # Find whether there is identical cluster env
+    paging_token = None
+    cluster_env_id = None
+    while not cluster_env_id:
+        result = anyscale_sdk.search_cluster_environments(
+            dict(
+                name=dict(equals=cluster_env_name),
+                paging=dict(count=50, paging_token=paging_token),
+                project_id=None,
+            )
+        )
+        paging_token = result.metadata.next_paging_token
+
+        for res in result.results:
+            if res.name == cluster_env_name:
+                cluster_env_id = res.id
+                logger.info(f"Cluster env already exists with ID " f"{cluster_env_id}")
+                break
+
+        if not paging_token or cluster_env_id:
+            break
+
+    if not cluster_env_id:
+        logger.info("Cluster env not found. Creating new one.")
+        try:
+            result = anyscale_sdk.create_byod_cluster_environment(
+                dict(
+                    name=cluster_env_name,
+                    config_json=dict(
+                        docker_image=image,
+                        ray_version="nightly",
+                        env_vars=runtime_env,
+                    ),
+                )
+            )
+            cluster_env_id = result.result.id
+        except Exception as e:
+            logger.warning(
+                f"Got exception when trying to create cluster "
+                f"env: {e}. Sleeping for 10 seconds with jitter and then "
+                f"try again..."
+            )
+            raise ClusterEnvCreateError("Could not create cluster env.") from e
+
+        logger.info(f"Cluster env created with ID {cluster_env_id}")
+
+    return cluster_env_id
