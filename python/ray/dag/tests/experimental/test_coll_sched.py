a033968d395164a007169e66978b2814894894d3
Weixin Deng <weixin@cs.washington.edu>
2025-05-13 10:59:55 -0700

    fix: test infra
    Signed-off-by: Weixin Deng <weixin@cs.washington.edu>

--- python/ray/dag/tests/experimental/test_coll_sched.py
+++ python/ray/dag/tests/experimental/test_coll_sched.py
@@ -1,38 +1,37 @@
-import logging
+# coding: utf-8
 import os
-import time
+import sys
+import pytest
+import torch
 
-os.environ["RAY_DEDUP_LOGS"] = "0"
-
-import fire
 import ray
 import ray.experimental.collective as collective
 from ray.dag import InputNode, MultiOutputNode
+from ray.tests.conftest import *  # noqa
 
+USE_GPU = bool(os.environ.get("RAY_PYTEST_USE_GPU", 0))
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(levelname)s %(filename)s:%(lineno)d %(funcName)s] %(message)s",
-)
 
 
 @ray.remote(num_gpus=1)
 class Actor:
     def __init__(self):
-        pass
+        return
 
     def compute(self, _):
-        return
+        return torch.ones(1, 1)
 
     def update(self, _):
         return
 
 
-def run_ddp():
-    ray.init()
+@pytest.mark.skipif(not USE_GPU, reason="Skipping GPU Test")
+@pytest.mark.parametrize("ray_start_regular", [{"num_cpus": 4}], indirect=True)
+@pytest.mark.parametrize("num_gpus", [2])
+def test_simple(ray_start_regular, num_gpus):
+    actors = [Actor.remote() for _ in range(num_gpus)]
 
-    actors = [Actor.remote() for _ in range(2)]
     upds = []
     with InputNode() as inp:
         grads = [actor.compute.bind(inp) for actor in actors]
@@ -48,17 +47,11 @@ def run_ddp():
         dag = MultiOutputNode(upds)
 
     compiled_dag = dag.experimental_compile()
-    logger.info(compiled_dag.actor_to_execution_schedule)
-
-    ray.shutdown()
-
-
-def main(name: str):
-    if name == "ddp":
-        run_ddp()
-    else:
-        logger.error(f"Unknown name: {name}")
+    print(compiled_dag.actor_to_execution_schedule)
 
 
 if __name__ == "__main__":
-    fire.Fire(main)
+    if os.environ.get("PARALLEL_CI"):
+        sys.exit(pytest.main(["-n", "auto", "--boxed", "-vs", __file__]))
+    else:
+        sys.exit(pytest.main(["-sv", __file__]))
