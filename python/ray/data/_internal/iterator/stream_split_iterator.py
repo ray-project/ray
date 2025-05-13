6c693269494ddb0a0878d793b152c290831d2fdf
Ricardo Decal <richard.decal@gmail.com>
2025-05-12 17:11:23 -0700

    push checks into the DataIterator parent class
    Signed-off-by: Ricardo Decal <richard.decal@gmail.com>

--- python/ray/data/_internal/iterator/stream_split_iterator.py
+++ python/ray/data/_internal/iterator/stream_split_iterator.py
@@ -73,44 +73,6 @@ class StreamSplitDataIterator(DataIterator):
         self._world_size = world_size
         self._iter_stats = DatasetStats(metadata={}, parent=None)
 
-    def _raise_if_stream_split(self, method_name: str):
-        raise AttributeError(
-            f"Cannot call `{method_name}` on a StreamSplitDataIterator. This error "
-            "typically occurs when trying to apply an operation (e.g. preprocessor "
-            "transform, format conversion) to a dataset shard inside a train worker. "
-            "Instead, you should apply the operation to the Ray Dataset before splitting "
-            "or sharding it across workers."
-        )
-
-    def iter_batches(
-        self,
-        *args,
-        **kwargs,
-    ):
-        self._raise_if_stream_split("iter_batches")
-
-    def iter_rows(self, *args, **kwargs):
-        self._raise_if_stream_split("iter_rows")
-
-    def iter_torch_batches(self, *args, **kwargs):
-        self._raise_if_stream_split("iter_torch_batches")
-
-    def iter_tf_batches(
-        self,
-        *args,
-        **kwargs,
-    ):
-        self._raise_if_stream_split("iter_tf_batches")
-
-    def to_torch(self, *args, **kwargs):
-        self._raise_if_stream_split("to_torch")
-
-    def to_tf(self, *args, **kwargs):
-        self._raise_if_stream_split("to_tf")
-
-    def materialize(self):
-        self._raise_if_stream_split("materialize")
-
     def _to_ref_bundle_iterator(
         self,
     ) -> Tuple[Iterator[RefBundle], Optional[DatasetStats], bool]:
--- python/ray/data/iterator.py
+++ python/ray/data/iterator.py
@@ -242,8 +242,7 @@ class DataIterator(abc.ABC):
         ...
 
     @abc.abstractmethod
-    def get_context(self) -> DataContext:
-        ...
+    def get_context(self) -> DataContext: ...
 
     @PublicAPI
     def iter_torch_batches(
@@ -967,6 +966,15 @@ class DataIterator(abc.ABC):
     def __del__(self):
         # Clear metrics on deletion in case the iterator was not fully consumed.
         StatsManager.clear_iteration_metrics(self._get_dataset_tag())
+
+    def map_batches(self, *args, **kwargs):
+        raise AttributeError(
+            "Cannot call `map_batches` on a DataIterator. This error "
+            "typically occurs when trying to apply an operation (e.g. preprocessor "
+            "transform, format conversion) to a dataset shard inside a train worker. "
+            "Instead, you should apply the operation to the Ray Dataset before splitting "
+            "or sharding it across workers."
+        )
 
 
 # Backwards compatibility alias.
