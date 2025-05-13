93dc3f9cf75d03b0affb5ea8597c36ce243b495c
Ryan O'Leary <ryanaoleary@google.com>
2025-05-13 00:18:38 +0000

    Remove verbose logging again
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>

--- python/ray/autoscaler/v2/tests/test_node_provider.py
+++ python/ray/autoscaler/v2/tests/test_node_provider.py
@@ -721,4 +721,4 @@ if __name__ == "__main__":
     if os.environ.get("PARALLEL_CI"):
         sys.exit(pytest.main(["-n", "auto", "--boxed", "-vs", __file__]))
     else:
-        sys.exit(pytest.main(["-sv", "-vv", __file__]))
+        sys.exit(pytest.main(["-sv", __file__]))
