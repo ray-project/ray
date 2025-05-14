e6951cc35d9e7a5833908c4f1929c83da1048a9f
GokuMohandas <gokumd@gmail.com>
2025-05-14 12:44:10 -0700

    lint fixes

--- doc/source/ray-overview/examples/entity-recognition-with-llms/ci/tests.sh
+++ doc/source/ray-overview/examples/entity-recognition-with-llms/ci/tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python ci/nb2py.py README.ipynb README.py  # jupyter will convert even non-python code logic
+python ci/nb2py.py README.ipynb README.py  # convert notebook to py script
 python README.py  # be sure to use ipython to ensure even non-python cells are executed properly
 rm README.py  # remove the generated script
