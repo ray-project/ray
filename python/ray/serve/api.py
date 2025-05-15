0b6ce781ae9b1feb2b36ea0769b669b3590b6801
Abrar Sheikh <abrar@anyscale.com>
2025-05-15 14:31:17 -0700

    ASGI ingress api can be passed a builder function (#52892)
    This PR enhances Ray Serve's ingress API to accept builder functions
    that return ASGI applications, providing more flexibility in application
    initialization:
    
    1. Added support for passing builder functions to serve.ingress() that
    are evaluated when replicas initialize
    2. Modified ASGIAppReplicaWrapper to handle both direct ASGI apps and
    builder functions
    3. Updated type hints to include function callables as valid inputs
    4. Added tests for all new usage patterns
    5. Expanded documentation with examples of the new functionality
    6. These changes allow lazy initialization of ASGI applications,
    enabling more complex startup patterns
    7. since docs_path wont be available until builder function is run on
    replica, I made some code change to update the docs_path on application
    state.
    
    ---------
    
    Signed-off-by: abrar <abrar@anyscale.com>

--- python/ray/serve/api.py
+++ python/ray/serve/api.py
@@ -2,10 +2,11 @@ import collections
 import inspect
 import logging
 from functools import wraps
-from typing import Any, Callable, Dict, List, Optional, Sequence, Union
+from typing import Any, Callable, Dict, List, Optional, Sequence, Type, Union
 
 from attr import dataclass
 from fastapi import APIRouter, FastAPI
+from starlette.types import ASGIApp
 
 import ray
 from ray import cloudpickle
@@ -169,10 +170,13 @@ def get_replica_context() -> ReplicaContext:
 
 
 @PublicAPI(stability="stable")
-def ingress(app: Union["FastAPI", "APIRouter", Callable]) -> Callable:
-    """Wrap a deployment class with a FastAPI application for HTTP request parsing.
+def ingress(app: Union[ASGIApp, Callable]) -> Callable:
+    """Wrap a deployment class with an ASGI application for HTTP request parsing.
+    There are a few different ways to use this functionality.
 
     Example:
+
+    FastAPI app routes are defined inside the deployment class.
 
         .. code-block:: python
 
@@ -190,15 +194,72 @@ def ingress(app: Union["FastAPI", "APIRouter", Callable]) -> Callable:
 
             app = MyFastAPIDeployment.bind()
 
+    You can also use a standalone FastAPI app without registering
+    routes inside the deployment.
+
+    .. code-block:: python
+
+        from ray import serve
+        from fastapi import FastAPI
+
+        app = FastAPI()
+
+        @app.get("/hi")
+        def say_hi():
+            return "Hello world!"
+
+        deployment = serve.deployment(serve.ingress(app)())
+        app = deployment.bind()
+
+    You can also pass in a builder function that returns an ASGI app.
+    The builder function is evaluated when the deployment is initialized on
+    replicas. This example shows how to use a sub-deployment inside the routes
+    defined outside the deployment class.
+
+    .. code-block:: python
+
+        from ray import serve
+
+        @serve.deployment
+        class SubDeployment:
+            def __call__(self):
+                return "Hello world!"
+
+        def build_asgi_app():
+            from fastapi import FastAPI
+
+            app = FastAPI()
+
+            def get_sub_deployment_handle():
+                return serve.get_deployment_handle(SubDeployment.name, app_name="my_app")
+
+            @app.get("/hi")
+            async def say_hi(handle: Depends(get_sub_deployment_handle)):
+                return await handle.remote()
+
+            return app
+
+        deployment = serve.deployment(serve.ingress(build_asgi_app)())
+        app = deployment.bind(SubDeployment.bind(), name="my_app", route_prefix="/")
+
     Args:
-        app: the FastAPI app or router object to wrap this class with.
+        app: the FastAPI app to wrap this class with.
             Can be any ASGI-compatible callable.
+            You can also pass in a builder function that returns an ASGI app.
     """
 
-    def decorator(cls):
+    def decorator(cls: Optional[Type[Any]] = None) -> Callable:
+        if cls is None:
+
+            class ASGIIngressDeployment:
+                def __init__(self, *args, **kwargs):
+                    self.args = args
+                    self.kwargs = kwargs
+
+            cls = ASGIIngressDeployment
+
         if not inspect.isclass(cls):
             raise ValueError("@serve.ingress must be used with a class.")
-
         if issubclass(cls, collections.abc.Callable):
             raise ValueError(
                 "Classes passed to @serve.ingress may not have __call__ method."
@@ -209,13 +270,18 @@ def ingress(app: Union["FastAPI", "APIRouter", Callable]) -> Callable:
         if isinstance(app, (FastAPI, APIRouter)):
             make_fastapi_class_based_view(app, cls)
 
-        # Free the state of the app so subsequent modification won't affect
-        # this ingress deployment. We don't use copy.copy here to avoid
-        # recursion issue.
-        ensure_serialization_context()
-        frozen_app = cloudpickle.loads(
-            pickle_dumps(app, error_msg="Failed to serialize the FastAPI app.")
-        )
+        frozen_app_or_func: Union[ASGIApp, Callable] = None
+
+        if inspect.isfunction(app):
+            frozen_app_or_func = app
+        else:
+            # Free the state of the app so subsequent modification won't affect
+            # this ingress deployment. We don't use copy.copy here to avoid
+            # recursion issue.
+            ensure_serialization_context()
+            frozen_app_or_func = cloudpickle.loads(
+                pickle_dumps(app, error_msg="Failed to serialize the ASGI app.")
+            )
 
         class ASGIIngressWrapper(cls, ASGIAppReplicaWrapper):
             def __init__(self, *args, **kwargs):
@@ -223,7 +289,7 @@ def ingress(app: Union["FastAPI", "APIRouter", Callable]) -> Callable:
                 cls.__init__(self, *args, **kwargs)
 
                 ServeUsageTag.FASTAPI_USED.record("1")
-                ASGIAppReplicaWrapper.__init__(self, frozen_app)
+                ASGIAppReplicaWrapper.__init__(self, frozen_app_or_func)
 
             async def __del__(self):
                 await ASGIAppReplicaWrapper.__del__(self)
@@ -236,8 +302,11 @@ def ingress(app: Union["FastAPI", "APIRouter", Callable]) -> Callable:
                         cls.__del__(self)
 
         ASGIIngressWrapper.__name__ = cls.__name__
-        if hasattr(frozen_app, "docs_url"):
-            ASGIIngressWrapper.__fastapi_docs_path__ = frozen_app.docs_url
+        if hasattr(frozen_app_or_func, "docs_url"):
+            # TODO (abrar): fastapi apps instantiated by builder function will set
+            # the docs path on application state via the replica.
+            # This split in logic is not desirable, we should consolidate the two.
+            ASGIIngressWrapper.__fastapi_docs_path__ = frozen_app_or_func.docs_url
 
         return ASGIIngressWrapper
 
