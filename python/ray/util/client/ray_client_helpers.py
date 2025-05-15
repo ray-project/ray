c3f4489852dda1bae11e0812fc62a2fd7edc899b
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-05-15 08:40:17 -0500

    WIP
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/util/client/ray_client_helpers.py
+++ python/ray/util/client/ray_client_helpers.py
@@ -80,36 +80,3 @@ def ray_start_cluster_client_server_pair(address):
         ray._inside_client_test = False
         ray.disconnect()
         server.stop(0)
-
-
-@contextmanager
-def connect_to_client_or_not(connect_to_client: bool):
-    """Utility for running test logic with and without a Ray client connection.
-
-    If client_connect is True, will connect to Ray client in context.
-    If client_connect is False, does nothing.
-
-    How to use:
-    Given a test of the following form:
-
-    def test_<name>(args):
-        <initialize a ray cluster>
-        <use the ray cluster>
-
-    Modify the test to
-
-    @pytest.mark.parametrize("connect_to_client", [False, True])
-    def test_<name>(args, connect_to_client)
-    <initialize a ray cluster>
-    with connect_to_client_or_not(connect_to_client):
-        <use the ray cluster>
-
-    Parameterize the argument connect over True, False to run the test with and
-    without a Ray client connection.
-    """
-
-    if connect_to_client:
-        with ray_start_client_server(namespace=""), enable_client_mode():
-            yield
-    else:
-        yield
