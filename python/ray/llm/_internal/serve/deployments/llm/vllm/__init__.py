3db8f170a49189f1f1e2f94a9bd0e554c96b1971
Linkun Chen <github@lkchen.net>
2025-05-17 01:48:47 -0700

    set kv stuff in server
    Signed-off-by: Linkun Chen <github@lkchen.net>

--- python/ray/llm/_internal/serve/deployments/llm/vllm/__init__.py
+++ python/ray/llm/_internal/serve/deployments/llm/vllm/__init__.py
@@ -1,1 +1,2 @@
+# The key for the kv_transfer_params in the internal_parameters.
 KV_TRANSFER_PARAMS_KEY = "kv_transfer_params"
--- python/ray/llm/_internal/serve/deployments/routers/router.py
+++ python/ray/llm/_internal/serve/deployments/routers/router.py
@@ -10,7 +10,6 @@ from typing import (
     Dict,
     List,
     Optional,
-    Tuple,
     TypeVar,
     Union,
 )
