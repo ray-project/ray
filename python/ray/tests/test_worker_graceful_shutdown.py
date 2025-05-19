090975decf7bd9f3c81a64193d756d2ddb439eef
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-05-19 15:14:50 -0400

    WIP
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/tests/test_worker_graceful_shutdown.py
+++ python/ray/tests/test_worker_graceful_shutdown.py
@@ -11,6 +11,11 @@ from ray._private.test_utils import SignalActor, wait_for_condition
 
 @pytest.mark.parametrize("actor_type", ["asyncio", "threaded"])
 def test_ray_get_during_graceful_shutdown(ray_start_regular_shared, actor_type: str):
+    """Test that ray.get works as expected when draining tasks during shutdown.
+
+    This currently only applies to concurrent actors, because single-threaded actors do
+    not allow tasks to finish exiting after SIGTERM.
+    """
     signal_actor = SignalActor.remote()
 
     assert actor_type in {"asyncio", "threaded"}
@@ -18,32 +23,35 @@ def test_ray_get_during_graceful_shutdown(ray_start_regular_shared, actor_type: 
 
         @ray.remote
         class A:
-            def getpid(self) -> int:
-                return os.getpid()
+            def exit(self):
+                os.kill(os.getpid(), signal.SIGTERM)
 
             async def wait_then_get(self, nested_ref: List[ray.ObjectRef]) -> str:
                 print("Waiting for signal...")
                 await signal_actor.wait.remote()
-                return ray.get(nested_ref[0])
+                print("Got signal, calling ray.get")
+                return await nested_ref[0]
 
     elif actor_type == "threaded":
 
         @ray.remote(max_concurrency=2)
         class A:
-            def getpid(self) -> int:
-                return os.getpid()
+            def exit(self):
+                os.kill(os.getpid(), signal.SIGTERM)
 
             def wait_then_get(self, nested_ref: List[ray.ObjectRef]):
                 print("Waiting for signal...")
                 ray.get(signal_actor.wait.remote())
+                print("Got signal, calling ray.get")
                 return ray.get(nested_ref[0])
 
     actor = A.remote()
     wait_ref = actor.wait_then_get.remote([ray.put("hi")])
     wait_for_condition(lambda: ray.get(signal_actor.cur_num_waiters.remote()) == 1)
 
-    os.kill(ray.get(actor.getpid.remote()), signal.SIGTERM)
+    ray.get(actor.exit.remote())
     ray.get(signal_actor.send.remote())
+
     assert ray.get(wait_ref) == "hi"
 
 
