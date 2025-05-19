492d6c9313b0265c36f2c8841ae8563dcb57a94e
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-05-19 14:51:40 -0400

    WIP
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/tests/test_worker_graceful_shutdown.py
+++ python/ray/tests/test_worker_graceful_shutdown.py
@@ -7,12 +7,14 @@ import pytest
 import ray
 from ray._private.test_utils import SignalActor, wait_for_condition
 
+
 @pytest.mark.parametrize("actor_type", ["asyncio", "threaded"])
 def test_ray_get_during_graceful_shutdown(ray_start_regular_shared, actor_type: str):
     signal_actor = SignalActor.remote()
 
     assert actor_type in {"asyncio", "threaded"}
     if actor_type == "asyncio":
+
         @ray.remote
         class A:
             def getpid(self) -> int:
@@ -24,6 +26,7 @@ def test_ray_get_during_graceful_shutdown(ray_start_regular_shared, actor_type: 
                 return ray.get(nested_ref[0])
 
     elif actor_type == "threaded":
+
         @ray.remote(max_concurrency=2)
         class A:
             def getpid(self) -> int:
@@ -36,9 +39,7 @@ def test_ray_get_during_graceful_shutdown(ray_start_regular_shared, actor_type: 
 
     actor = A.remote()
     wait_ref = actor.wait_then_get.remote([ray.put("hi")])
-    wait_for_condition(
-        lambda: ray.get(signal_actor.cur_num_waiters.remote()) == 1
-    )
+    wait_for_condition(lambda: ray.get(signal_actor.cur_num_waiters.remote()) == 1)
 
     os.kill(ray.get(actor.getpid.remote()), signal.SIGTERM)
     ray.get(signal_actor.send.remote())
