362080126d22ec0d232235e63dd6d6bf76761f0b
Kai-Hsun Chen <kaihsun@anyscale.com>
2025-05-19 21:03:43 +0000

    fix test_actor_autocomplete
    Signed-off-by: Kai-Hsun Chen <kaihsun@anyscale.com>

--- python/ray/tests/test_actor.py
+++ python/ray/tests/test_actor.py
@@ -1303,7 +1303,7 @@ def test_actor_autocomplete(ray_start_regular_shared):
     method_options = [fn for fn in dir(f.method_one) if not fn.startswith("_")]
 
     if client_test_enabled():
-        assert set(method_options) == {"options", "remote", "valid_transports"}
+        assert set(method_options) == {"options", "remote"}
     else:
         assert set(method_options) == {"options", "remote", "bind", "valid_transports"}
 
