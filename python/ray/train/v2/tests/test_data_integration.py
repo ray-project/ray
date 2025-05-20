e73b292b310691d04c1ce4f835a284c8a689d46d
Ricardo Decal <rdecal@anyscale.com>
2025-05-20 14:50:28 -0700

    lint
    Signed-off-by: Ricardo Decal <rdecal@anyscale.com>

--- python/ray/train/v2/tests/test_data_integration.py
+++ python/ray/train/v2/tests/test_data_integration.py
@@ -15,6 +15,7 @@ from ray.train.v2._internal.execution.worker_group.worker_group import (
 from ray.train.v2.api.data_parallel_trainer import DataParallelTrainer
 from ray.train.v2.tests.util import DummyObjectRefWrapper, DummyWorkerGroup
 from ray.train.v2.exceptions import TrainingFailedError
+
 # TODO(justinvyu): Bring over more tests from ray/air/tests/test_new_dataset_config.py
 
 
@@ -125,7 +126,7 @@ def test_transform_on_split_iterator_raises(ray_start_4_cpus):
 
     with pytest.raises(TrainingFailedError) as exc_info:
         trainer.fit()
-        
+
     # Check that the worker failures contain an AttributeError with the expected message
     worker_errors = list(exc_info.value.worker_failures.values())
     assert any(
