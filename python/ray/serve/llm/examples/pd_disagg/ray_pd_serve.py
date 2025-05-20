c235f797dd4f8a910bf832734def9eed0066ce39
Linkun Chen <github@lkchen.net>
2025-05-19 20:52:13 -0700

    use yaml
    Signed-off-by: Linkun Chen <github@lkchen.net>

--- python/ray/serve/llm/examples/pd_disagg/ray_pd_serve.py
+++ python/ray/serve/llm/examples/pd_disagg/ray_pd_serve.py
@@ -1,28 +1,24 @@
-"""Using Ray Serve to proxy between P/D LLM servers.
-
-Usage:
->>> serve run ray.serve.llm.examples.pd_disagg.ray_pd_serve:example_disagg_1p1d
-or
->>> python -m ray.serve.llm.examples.pd_disagg.ray_pd_serve
+"""Using Ray Serve to deploy LLM models with P/D disaggregation.
 """
 
 import os
 import asyncio
 import sys
 import logging
-from typing import List, AsyncGenerator
+from typing import List, AsyncGenerator, Union, cast
 
 if sys.version_info >= (3, 11):
     from asyncio import timeout
 else:
     from async_timeout import timeout
 
+from pydantic import BaseModel
 from vllm.config import KVTransferConfig
 
 from ray import serve
 from ray.serve.deployment import Application
 from ray.serve.handle import DeploymentHandle
-from ray.serve.llm import LLMRouter, LLMConfig, ModelLoadingConfig
+from ray.serve.llm import LLMRouter, LLMConfig, ModelLoadingConfig, build_llm_deployment
 
 from ray.llm._internal.serve.configs.prompt_formats import Prompt
 from ray.llm._internal.serve.deployments.llm.llm_server import LLMServer
@@ -33,15 +29,41 @@ from ray.llm._internal.serve.configs.constants import (
     RAYLLM_ROUTER_HTTP_TIMEOUT,
 )
 from ray.llm._internal.serve.configs.server_models import (
-    LLMServingArgs,
-    LLMConfig,
+    parse_args as parse_llm_configs,
     LLMRawResponse,
 )
 
-# TODO(lk-chen): public api does not have name_prefix, shall we add it to LLMConfig?
-from ray.llm._internal.serve.builders.application_builders import build_llm_deployment
-
 logger = logging.getLogger(__name__)
+
+
+class PDServingArgs(BaseModel):
+    """Schema for P/D serving args."""
+
+    prefill_configs: List[Union[str, LLMConfig]]
+    decode_configs: List[Union[str, LLMConfig]]
+
+    def parse_args(self) -> "PDServingArgs":
+        """Converts this LLMServingArgs object into an DeployArgs object."""
+
+        if len(self.prefill_configs) != len(self.decode_configs):
+            raise ValueError(
+                f"Prefill and decode configs must have the same length, got {len(self.prefill_configs)} prefill configs and {len(self.decode_configs)} decode configs."
+            )
+
+        def parse_configs_and_cast_type(
+            configs: List[Union[str, LLMConfig]]
+        ) -> List[LLMConfig]:
+            # ray.serve.llm.__init__ imports internal LLMConfig, and extends it to external-facing LLMConfig.
+            # parse_llm_configs returns internal LLMConfig, while {prefill, decode}_configs expect external-facing LLMConfig.
+            # So the model_dump() here is to convert the type, to satisfy pydantic.
+            configs = parse_llm_configs(configs)
+            return [LLMConfig(**config.model_dump()) for config in configs]
+
+        return PDServingArgs(
+            # Parse string file path into LLMConfig
+            prefill_configs=parse_configs_and_cast_type(self.prefill_configs),
+            decode_configs=parse_configs_and_cast_type(self.decode_configs),
+        )
 
 
 class PDProxyServer(LLMServer):
@@ -57,18 +79,22 @@ class PDProxyServer(LLMServer):
 
     async def __init__(
         self,
-        llm_config: LLMConfig,
         prefill_server: DeploymentHandle,
         decode_server: DeploymentHandle,
     ):
         class FakeEngine:
+            """Provide a fake engine such that proxy don't really start any engine."""
+
             def __init__(self, *args, **kwargs):
                 pass
 
             async def start(self, *args, **kwargs):
                 pass
 
-        await super().__init__(llm_config, engine_cls=FakeEngine)
+        await super().__init__(
+            LLMConfig(model_loading_config=ModelLoadingConfig(model_id="")),
+            engine_cls=FakeEngine,
+        )
 
         self.prefill_server = prefill_server
         self.decode_server = decode_server
@@ -150,7 +176,6 @@ class PDProxyServer(LLMServer):
         >>> prefill_deploy = ray.serve.llm.build_llm_deployment(prefill_config)
         >>> decode_deploy = ray.serve.llm.build_llm_deployment(decode_config)
         >>> PDProxyServer.as_deployment().bind(
-        ...     llm_config=llm_config,
         ...     prefill_server=prefill_deploy,
         ...     decode_server=decode_deploy,
         ... )
@@ -176,99 +201,56 @@ class PDProxyServer(LLMServer):
         )(cls)
 
 
-def build_PD_disagg_app(llm_serving_args: LLMServingArgs) -> Application:
-    rayllm_args = LLMServingArgs.model_validate(llm_serving_args).parse_args()
-
-    prefill_configs = rayllm_args.llm_configs
-    model_ids = {m.model_id for m in prefill_configs}
-    if len(model_ids) != len(prefill_configs):
-        raise ValueError("Duplicate models found. Make sure model ids are unique.")
-
-    if len(prefill_configs) == 0:
+def _validate_llm_configs(llm_configs: List[LLMConfig], config_name: str) -> None:
+    model_ids = {m.model_id for m in llm_configs}
+    if len(model_ids) != len(llm_configs):
+        raise ValueError(
+            f"Duplicate models found. Make sure model ids are unique for {config_name}."
+        )
+    if len(llm_configs) == 0:
         logger.error(
-            "List of models is empty. Maybe some parameters cannot be parsed into the LLMConfig config."
+            f"List of models is empty. Maybe some parameters cannot be parsed into the LLMConfig config for {config_name}."
         )
 
-    for config in prefill_configs:
-        config.engine_kwargs.update(
-            {
-                "kv_transfer_config": KVTransferConfig(
-                    kv_connector="NixlConnector", kv_role="kv_both"
-                )
-            }
-        )
 
-    # build_llm_deployment() could change config, so deep copy configs for P/D.
-    # llm_configs should be list[`LLMConfig`] at this point.
-    decode_configs = [c.model_copy(deep=True) for c in prefill_configs]
+def build_PD_disagg_app(llm_serving_args: dict) -> Application:
+    """Build a deployable application utilizing P/D disaggregation."""
 
-    def _get_llm_deployments(
-        llm_configs: List[LLMConfig], name_prefix: str
-    ) -> List[DeploymentHandle]:
-        return [
-            build_llm_deployment(llm_config, name_prefix=name_prefix)
-            for llm_config in llm_configs
-        ]
+    rayllm_args = PDServingArgs.model_validate(llm_serving_args).parse_args()
 
-    prefill_deployments = _get_llm_deployments(prefill_configs, "PrefillDeployment")
-    decode_deployments = _get_llm_deployments(decode_configs, "DecodeDeployment")
+    _validate_llm_configs(rayllm_args.prefill_configs, "prefill_configs")
+    _validate_llm_configs(rayllm_args.decode_configs, "decode_configs")
+
+    for config in rayllm_args.prefill_configs + rayllm_args.decode_configs:
+        if "kv_transfer_config" not in config.engine_kwargs:
+            config.engine_kwargs.update(
+                {
+                    "kv_transfer_config": KVTransferConfig(
+                        kv_connector="NixlConnector", kv_role="kv_both"
+                    )
+                }
+            )
+
+    def _get_llm_deployments(llm_configs: List[LLMConfig]) -> List[DeploymentHandle]:
+        return [build_llm_deployment(llm_config) for llm_config in llm_configs]
+
+    prefill_deployments = _get_llm_deployments(rayllm_args.prefill_configs)
+    decode_deployments = _get_llm_deployments(rayllm_args.decode_configs)
     if len(prefill_deployments) != len(decode_deployments):
         raise ValueError("Prefill and decode deployments must have the same length.")
 
     proxy_server_deployments = [
         PDProxyServer.as_deployment().bind(
-            llm_config=llm_config,
             prefill_server=prefill_deploy,
             decode_server=decode_deploy,
         )
-        for llm_config, prefill_deploy, decode_deploy in zip(
-            prefill_configs, prefill_deployments, decode_deployments
+        for prefill_deploy, decode_deploy in zip(
+            prefill_deployments, decode_deployments
         )
     ]
 
     # TODO(lk-chen): Need to re-think about configs here, in non-PD case LLMRouter use 2x replica
     # as LLM, here in PD case what shall we do?
-    return LLMRouter.as_deployment(llm_configs=decode_configs).bind(
+    return LLMRouter.as_deployment(llm_configs=rayllm_args.decode_configs).bind(
         llm_deployments=proxy_server_deployments
     )
-
-
-def _default_example_config_1p1d() -> LLMServingArgs:
-    """Example config for 1P1D, hosting Qwen3-0.6B."""
-    maybe_hf_token = os.environ.get("HF_TOKEN", None)
-    env_vars = dict(
-        VLLM_USE_V1="1",
-    )
-    if maybe_hf_token:
-        env_vars["HF_TOKEN"] = maybe_hf_token
-    return LLMServingArgs(
-        llm_configs=[
-            LLMConfig(
-                model_loading_config=ModelLoadingConfig(
-                    model_id="Qwen/Qwen3-0.6B",
-                ),
-                engine_kwargs=dict(
-                    tensor_parallel_size=1,
-                    data_parallel_size=1,
-                    gpu_memory_utilization=0.2,
-                ),
-                runtime_env=dict(
-                    env_vars=env_vars,
-                ),
-                resources_per_bundle=dict(
-                    # Qwen3-0.6B is small, we use GPU=0.25 here so people can deploy on even
-                    # single node with 1 GPU, smoothen the first-time-user experience.
-                    GPU=0.25,
-                ),
-            )
-        ]
-    )
-
-
-example_disagg_1p1d = build_PD_disagg_app(_default_example_config_1p1d())
-
-if __name__ == "__main__":
-    import ray
-
-    ray.init(ignore_reinit_error=True)
-    ray.serve.run(example_disagg_1p1d)
