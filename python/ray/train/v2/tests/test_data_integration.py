677890e9694c2e71d787a380e9c62f48a19d2266
Ricardo Decal <rdecal@anyscale.com>
2025-05-20 15:06:23 -0700

    lint
    Signed-off-by: Ricardo Decal <rdecal@anyscale.com>

--- python/ray/train/v2/tests/test_data_integration.py
+++ python/ray/train/v2/tests/test_data_integration.py
@@ -13,8 +13,8 @@ from ray.train.v2._internal.execution.worker_group.worker_group import (
     WorkerGroupContext,
 )
 from ray.train.v2.api.data_parallel_trainer import DataParallelTrainer
-from ray.train.v2.tests.util import DummyObjectRefWrapper, DummyWorkerGroup
 from ray.train.v2.exceptions import TrainingFailedError
+from ray.train.v2.tests.util import DummyObjectRefWrapper, DummyWorkerGroup
 
 # TODO(justinvyu): Bring over more tests from ray/air/tests/test_new_dataset_config.py
 
