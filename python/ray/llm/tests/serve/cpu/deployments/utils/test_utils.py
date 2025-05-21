c7a6bfa86aca9ecd8c76f61957f3cf8fcdebf63d
Linkun Chen <github@lkchen.net>
2025-05-20 18:48:51 -0700

    resolve comment
    Signed-off-by: Linkun Chen <github@lkchen.net>

--- python/ray/llm/tests/serve/cpu/deployments/utils/test_utils.py
+++ python/ray/llm/tests/serve/cpu/deployments/utils/test_utils.py
@@ -2,10 +2,10 @@ from ray.serve.llm import LLMServer
 
 
 async def create_server(*args, **kwargs):
-    """Asynchronously create an LLMServer instance.
+    """Asynchronously create an LLMServer instance."""
 
-    _ = LLMServer(...) will raise TypeError("__init__() should return None")
-    """
+    # _ = LLMServer(...) will raise TypeError("__init__() should return None")
+    # so we do __new__ then __init__
     server = LLMServer.__new__(LLMServer)
     await server.__init__(*args, **kwargs)
     return server
