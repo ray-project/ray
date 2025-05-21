4bd46599ca8cc1c32e4f02f89afae6082d5339e7
Artur Niederfahrenhorst <attaismyname@googlemail.com>
2025-05-21 18:32:02 +0700

    Add API annotation

--- rllib/utils/metrics/stats.py
+++ rllib/utils/metrics/stats.py
@@ -835,6 +835,7 @@ class Stats:
                 return [reduced], values
 
 
+@DeveloperAPI
 def merge_stats(base_stats: Optional[Stats], incoming_stats: List[Stats]) -> Stats:
     """Merges Stats objects.
 
