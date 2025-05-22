ff21d27460354efc1c18308bfa8309f03d50b73f
Ricardo Decal <rdecal@anyscale.com>
2025-05-22 14:46:24 -0700

    rewrite tests to use pure python instead of pytest to avoid sys path problems
    Signed-off-by: Ricardo Decal <rdecal@anyscale.com>

--- release/ray_release/byod/byod_e2e_xgboost.sh
+++ release/ray_release/byod/byod_e2e_xgboost.sh
@@ -2,4 +2,4 @@
 
 set -exo pipefail
 
-pip3 install --no-cache-dir mlflow==2.19.0 scikit-learn==1.6.0 xgboost>=3.0.0 pytest==8.3.4
+pip3 install --no-cache-dir mlflow==2.19.0 scikit-learn==1.6.0 xgboost>=3.0.0
