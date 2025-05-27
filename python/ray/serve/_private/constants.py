e0a1473f7c7e015f4c40a5668838fee81ba17ae7
Gene Der Su <e870252314@gmail.com>
2025-05-27 15:12:18 -0700

    [Serve] Configure request router through the deployment decorator  (#53108)
    ## Why are these changes needed?
    
    Adding `request_router_class` and `serialized_request_router_cls` to
    deployment decorator.
    
    ## Related issue number
    
    Closes
    https://anyscale1.atlassian.net/browse/LLM-1903?atlOrigin=eyJpIjoiMDE4YTk5Y2ZiYmQ1NDdjZDhhNThiYjA2MjIyM2ZlZjIiLCJwIjoiaiJ9
    
    ## Checks
    
    - [ ] I've signed off every commit(by using the -s flag, i.e., `git
    commit -s`) in this PR.
    - [ ] I've run `scripts/format.sh` to lint the changes in this PR.
    - [ ] I've included any doc changes needed for
    https://docs.ray.io/en/master/.
    - [ ] I've added any new APIs to the API Reference. For example, if I
    added a
    method in Tune, I've added it in `doc/source/tune/api/` under the
               corresponding `.rst` file.
    - [ ] I've made sure the tests are passing. Note that there might be a
    few flaky tests, see the recent failures at https://flakey-tests.ray.io/
    - Testing Strategy
       - [ ] Unit tests
       - [ ] Release tests
       - [ ] This PR is not tested :(
    
    ---------
    
    Signed-off-by: Gene Su <e870252314@gmail.com>
    Signed-off-by: Gene Der Su <gdsu@ucdavis.edu>
    Signed-off-by: Gene Der Su <e870252314@gmail.com>
    Co-authored-by: Cindy Zhang <cindyzyx9@gmail.com>

--- python/ray/serve/_private/constants.py
+++ python/ray/serve/_private/constants.py
@@ -428,3 +428,8 @@ RAY_SERVE_PROXY_GC_THRESHOLD = int(
 RAY_SERVE_METRICS_EXPORT_INTERVAL_MS = int(
     os.environ.get("RAY_SERVE_METRICS_EXPORT_INTERVAL_MS", "100")
 )
+
+# The default request router class to use if none is specified.
+DEFAULT_REQUEST_ROUTER_PATH = (
+    "ray.serve._private.replica_scheduler:PowerOfTwoChoicesReplicaScheduler"
+)
