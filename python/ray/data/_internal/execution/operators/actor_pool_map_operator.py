58fd052b6304f1bd29a67dfbf21ff3f94de871a3
Srinath Krishnamachari <srinath.krishnamachari@anyscale.com>
2025-05-27 21:14:24 +0000

    Addresses review comments
    Signed-off-by: Srinath Krishnamachari <srinath.krishnamachari@anyscale.com>

--- python/ray/data/_internal/execution/operators/actor_pool_map_operator.py
+++ python/ray/data/_internal/execution/operators/actor_pool_map_operator.py
@@ -422,11 +422,12 @@ class ActorPoolMapOperator(MapOperator):
         """Updates resources usage."""
         for actor in self._actor_pool.get_running_actor_refs():
             actor_state = actor._get_local_state()
-            if actor_state is None:
+            if (
+                actor_state is None
+                or actor_state == gcs_pb2.ActorTableData.ActorState.DEAD
+            ):
                 # actor._get_local_state can return None if the state is Unknown
-                continue
-            elif actor_state == gcs_pb2.ActorTableData.ActorState.DEAD:
-                # The actor is dead, so nothing to do.
+                # If actor_state is None or dead, there is nothing to do.
                 continue
             elif actor_state != gcs_pb2.ActorTableData.ActorState.ALIVE:
                 # The actors can be either ALIVE or RESTARTING here because they will
