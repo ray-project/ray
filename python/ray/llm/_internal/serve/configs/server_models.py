7505b86dcddcf70d9f10031704b3ff3a26594578
Linkun Chen <github@lkchen.net>
2025-05-27 14:27:53 -0700

    not optional annot.
    Signed-off-by: Linkun Chen <github@lkchen.net>

--- python/ray/llm/_internal/serve/configs/server_models.py
+++ python/ray/llm/_internal/serve/configs/server_models.py
@@ -463,7 +463,7 @@ class LLMConfig(BaseModelExtended):
                 serve.run(llm_app)
 
         Keyword Args:
-            name_prefix (Optional[str]): Optional prefix to be used for the deployment name.
+            name_prefix: Optional prefix to be used for the deployment name.
 
         Returns:
             The dictionary to use in .options() when creating the deployment.
