0010bc613968fc746e7100705af0aa8150a64fd6
Gene Su <e870252314@gmail.com>
2025-05-27 22:13:32 -0700

    [Serve] Implement recording routing stats periodically
    Signed-off-by: Gene Su <e870252314@gmail.com>

--- python/ray/serve/tests/test_multiplex.py
+++ python/ray/serve/tests/test_multiplex.py
@@ -303,8 +303,8 @@ class TestBasicAPI:
         assert serve.get_multiplexed_model_id() == "1"
 
 
-def test_multiplexed_replica_info(serve_instance):
-    """Test MultiplexedReplicaInfo is passed to the controller & router"""
+def test_request_routing_info(serve_instance):
+    """Test RequestRoutingInfo is passed to the controller & router"""
 
     @serve.deployment
     class MyModel:
