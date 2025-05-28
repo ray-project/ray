ec2972022db63b5c22b10aa63c9acd0f25ac43ec
Colin Ho <colin.ho99@gmail.com>
2025-05-28 04:31:18 +0000

    remove prints
    @microcheck //python/ray/data/tests:test_daft
    
    Signed-off-by: Colin Ho <colin.ho99@gmail.com>

--- python/ray/data/tests/test_daft.py
+++ python/ray/data/tests/test_daft.py
@@ -17,27 +17,19 @@ def ray_start(request):
 
 
 def test_daft_round_trip(ray_start):
-    print("test_daft_round_trip")
     data = {
         "int_col": list(range(128)),
         "str_col": [str(i) for i in range(128)],
         "nested_list_col": [[i] * 3 for i in range(128)],
         "tensor_col": [np.array([[i] * 3] * 3) for i in range(128)],
     }
-    print("data", data)
     df = daft.from_pydict(data)
-    print("df", df)
     ds = ray.data.from_daft(df)
-    print("ds", ds)
     pd.testing.assert_frame_equal(ds.to_pandas(), df.to_pandas())
 
-    print("ds.to_daft()")
     df2 = ds.to_daft()
-    print("df2", df2)
     df_pandas = df.to_pandas()
-    print("df_pandas", df_pandas)
     df2_pandas = df2.to_pandas()
-    print("df2_pandas", df2_pandas)
 
     for c in data.keys():
         # NOTE: tensor behavior on round-trip is different because Ray Data provides
@@ -51,7 +43,6 @@ def test_daft_round_trip(ray_start):
             )
         else:
             pd.testing.assert_series_equal(df_pandas[c], df2_pandas[c])
-    print("test_daft_round_trip passed")
 
 
 if __name__ == "__main__":
