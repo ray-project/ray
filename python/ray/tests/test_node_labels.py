0c7a18f01a06c3ce9b8756a1f667081354ac6b1c
Ryan O'Leary <ryanaoleary@google.com>
2025-05-28 07:29:05 +0000

    Add default Ray Node labels at Node init
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>
    
    Add default Ray Node labels at Node init
    
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>
    
    Add default Ray Node labels at Node init
    
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>

--- python/ray/tests/test_node_labels.py
+++ python/ray/tests/test_node_labels.py
@@ -13,7 +13,7 @@ def check_cmd_stderr(cmd):
     return subprocess.run(cmd, stderr=subprocess.PIPE).stderr.decode("utf-8")
 
 
-def add_default_labels(node_info, labels):
+def add_default_labels_for_test(node_info, labels):
     labels["ray.io/node_id"] = node_info["NodeID"]
     return labels
 
@@ -26,7 +26,7 @@ def add_default_labels(node_info, labels):
 def test_ray_start_set_node_labels_from_json(call_ray_start):
     ray.init(address=call_ray_start)
     node_info = ray.nodes()[0]
-    assert node_info["Labels"] == add_default_labels(
+    assert node_info["Labels"] == add_default_labels_for_test(
         node_info, {"gpu_type": "A100", "region": "us"}
     )
 
@@ -39,7 +39,7 @@ def test_ray_start_set_node_labels_from_json(call_ray_start):
 def test_ray_start_set_node_labels_from_string(call_ray_start):
     ray.init(address=call_ray_start)
     node_info = ray.nodes()[0]
-    assert node_info["Labels"] == add_default_labels(
+    assert node_info["Labels"] == add_default_labels_for_test(
         node_info, {"gpu_type": "A100", "region": "us"}
     )
 
@@ -54,18 +54,18 @@ def test_ray_start_set_node_labels_from_string(call_ray_start):
 def test_ray_start_set_empty_node_labels(call_ray_start):
     ray.init(address=call_ray_start)
     node_info = ray.nodes()[0]
-    assert node_info["Labels"] == add_default_labels(node_info, {})
+    assert node_info["Labels"] == add_default_labels_for_test(node_info, {})
 
 
 def test_ray_init_set_node_labels(shutdown_only):
     labels = {"gpu_type": "A100", "region": "us"}
     ray.init(labels=labels)
     node_info = ray.nodes()[0]
-    assert node_info["Labels"] == add_default_labels(node_info, labels)
+    assert node_info["Labels"] == add_default_labels_for_test(node_info, labels)
     ray.shutdown()
     ray.init(labels={})
     node_info = ray.nodes()[0]
-    assert node_info["Labels"] == add_default_labels(node_info, {})
+    assert node_info["Labels"] == add_default_labels_for_test(node_info, {})
 
 
 def test_ray_init_set_node_labels_value_error(ray_start_cluster):
@@ -104,14 +104,14 @@ def test_cluster_add_node_with_labels(ray_start_cluster):
     cluster.wait_for_nodes()
     ray.init(address=cluster.address)
     node_info = ray.nodes()[0]
-    assert node_info["Labels"] == add_default_labels(node_info, labels)
+    assert node_info["Labels"] == add_default_labels_for_test(node_info, labels)
     head_node_id = ray.nodes()[0]["NodeID"]
 
     cluster.add_node(num_cpus=1, labels={})
     cluster.wait_for_nodes()
     for node in ray.nodes():
         if node["NodeID"] != head_node_id:
-            assert node["Labels"] == add_default_labels(node, {})
+            assert node["Labels"] == add_default_labels_for_test(node, {})
 
 
 @pytest.mark.parametrize("autoscaler_v2", [False, True], ids=["v1", "v2"])
@@ -137,7 +137,9 @@ def test_autoscaler_set_node_labels(autoscaler_v2, shutdown_only):
 
         for node in ray.nodes():
             if node["Resources"].get("CPU", 0) == 1:
-                assert node["Labels"] == add_default_labels(node, {"region": "us"})
+                assert node["Labels"] == add_default_labels_for_test(
+                    node, {"region": "us"}
+                )
     finally:
         cluster.shutdown()
 
@@ -152,12 +154,46 @@ def test_ray_start_set_node_labels_from_file():
         subprocess.check_call(cmd)
         ray.init(address="auto")
         node_info = ray.nodes()[0]
-        assert node_info["Labels"] == add_default_labels(
+        assert node_info["Labels"] == add_default_labels_for_test(
             node_info, {"gpu_type": "A100", "region": "us", "market-type": "spot"}
         )
     finally:
         subprocess.check_call(["ray", "stop", "--force"])
         os.remove(test_file_path)
+
+
+# def test_add_default_ray_node_labels_sets_expected_labels(shutdown_only):
+#     # Set environment variables for test
+#     os.environ["RAY_NODE_MARKET_TYPE"] = "spot"
+#     os.environ["RAY_NODE_GROUP"] = "worker-group-1"
+#     os.environ["RAY_NODE_REGION"] = "us-central2"
+#     os.environ["RAY_NODE_ZONE"] = "us-central2-b"
+
+#     # Patch accelerator manager to return a known type since test
+#     # is not actually scheduled on accelerator node.
+#     with patch.object(
+#         get_all_accelerator_managers()[0],
+#         "get_current_node_accelerator_type",
+#         return_value="A100",
+#     ):
+#         ray.init()
+#         node_info = ray.nodes()[0]
+#         labels = node_info["Labels"]
+
+#         assert labels.get("ray.io/market-type") == "spot"
+#         assert labels.get("ray.io/node-group") == "worker-group-1"
+#         assert labels.get("ray.io/availability-region") == "us-central2"
+#         assert labels.get("ray.io/availability-zone") == "us-central2-b"
+#         assert labels.get("ray.io/accelerator-type") == "A100"
+
+#     # Unset env vars
+#     for k in [
+#         "RAY_NODE_MARKET_TYPE",
+#         "RAY_NODE_GROUP",
+#         "RAY_NODE_ZONE",
+#         "RAY_NODE_REGION",
+#     ]:
+#         del os.environ[k]
 
 
 if __name__ == "__main__":
