f9f0129b8930163d958dd00abb75b9d65f7732f6
lkchen <github@lkchen.net>
2025-05-28 16:10:28 -0700

    [serve.llm] release tests for 1p1d (#53190)
    Signed-off-by: Linkun Chen <github@lkchen.net>

--- python/ray/llm/_internal/serve/deployments/llm/vllm/vllm_engine.py
+++ python/ray/llm/_internal/serve/deployments/llm/vllm/vllm_engine.py
@@ -213,12 +213,12 @@ class VLLMEngine(LLMEngine):
 
             # We need to overwrite the engine_id to make it unique across replicas.
             # "engine_id" is added in vllm 0.9.0, so do existance check.
-            if "engine_id" in kv_transfer_config.model_fields:
-                engine_id = getattr(kv_transfer_config, "engine_id", uuid.uuid4())
-                host = vllm.envs.NIXL_SIDE_CHANNEL_HOST
-                port = vllm.envs.NIXL_SIDE_CHANNEL_PORT
-                kv_transfer_config.engine_id = "-".join([engine_id, host, port])
-            else:
+            try:
+                engine_id = getattr(kv_transfer_config, "engine_id", str(uuid.uuid4()))
+                host = getattr(vllm.envs, "VLLM_NIXL_SIDE_CHANNEL_HOST", "localhost")
+                port = getattr(vllm.envs, "VLLM_NIXL_SIDE_CHANNEL_PORT", 5557)
+                kv_transfer_config.engine_id = "-".join([engine_id, host, str(port)])
+            except ValueError:
                 # TODO(lk-chen): Raise error once vllm 0.9.0 is pinned to rayllm
                 logger.warning(
                     "engine_id is not supported in vllm < 0.9.0, NIXL-backed kv transfer "
