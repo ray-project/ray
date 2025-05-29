2df0852593c8c1ec65464c03e6bd6761a94a8e6a
Alex Wang <alexwang@pinterest.com>
2025-05-29 20:59:54 +0000

    changes to build python 3.8 whl

--- python/ray/_version.py
+++ python/ray/_version.py
@@ -1,5 +1,5 @@
 # Replaced with the current commit when building the wheels.
-commit = "{{RAY_COMMIT_SHA}}"
+commit = "ad9cb98e40e877c224007d43a6da10f55312a446"
 version = "3.0.0.dev0"
 
 if __name__ == "__main__":
