2884f4f8944cca09d8f577f43f654e2c3d3a2846
Kai-Hsun Chen <kaihsun@anyscale.com>
2025-05-29 00:40:32 +0000

    add tests for GPUObjectManager
    Signed-off-by: Kai-Hsun Chen <kaihsun@anyscale.com>

--- python/ray/tests/test_gpu_objects.py
+++ python/ray/tests/test_gpu_objects.py
@@ -28,6 +28,14 @@ class GPUTestActor:
             return [d * 2 for d in data]
         return data * 2
 
+    def get_gpu_object(self, obj_id: str):
+        gpu_object_manager = ray._private.worker.global_worker.gpu_object_manager
+        if gpu_object_manager.has_gpu_object(obj_id):
+            gpu_object = gpu_object_manager.get_gpu_object(obj_id)
+            print(f"gpu_object: {gpu_object}")
+            return gpu_object
+        return None
+
 
 def init_process_group(actors):
     world_size = len(actors)
@@ -95,6 +103,40 @@ def test_multiple_tensors(ray_start_regular):
     assert result[0] == pytest.approx(tensor1 * 2)
     assert result[1] == pytest.approx(tensor2 * 2)
     assert result[2] == cpu_data * 2
+
+
+def test_trigger_out_of_band_tensor_transfer(ray_start_regular):
+    world_size = 2
+    actors = [GPUTestActor.remote() for _ in range(world_size)]
+    init_process_group(actors)
+
+    src_actor, dst_actor = actors[0], actors[1]
+
+    tensor = torch.tensor([1, 2, 3])
+    gpu_ref = src_actor.echo.remote(tensor)
+
+    # Check src_actor has the GPU object
+    ret_val_src = ray.get(src_actor.get_gpu_object.remote(gpu_ref.hex()))
+    assert ret_val_src is not None
+    assert len(ret_val_src) == 1
+    assert torch.equal(ret_val_src[0], tensor)
+
+    gpu_object_manager = ray._private.worker.global_worker.gpu_object_manager
+    gpu_object_manager.add_gpu_object_ref(gpu_ref, src_actor)
+
+    # Trigger out-of-band tensor transfer from src_actor to dst_actor.
+    # The GPU object will be removed from src_actor's GPU object store
+    # because the current GC implementation garbage collects GPU objects
+    # whenever they are consumed once.
+    task_args = (gpu_ref,)
+    gpu_object_manager.trigger_out_of_band_tensor_transfer(dst_actor, task_args)
+    assert ray.get(src_actor.get_gpu_object.remote(gpu_ref.hex())) is None
+
+    # Check dst_actor has the GPU object
+    ret_val_dst = ray.get(dst_actor.get_gpu_object.remote(gpu_ref.hex()))
+    assert ret_val_dst is not None
+    assert len(ret_val_dst) == 1
+    assert torch.equal(ret_val_dst[0], tensor)
 
 
 def test_invalid_tensor_transport(ray_start_regular):
