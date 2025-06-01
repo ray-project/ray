23f7d7565aa98dc391ea8d7337ae2516c5eab560
Kunling Geng <kunling@kunling-LGW6TWQYVT.local>
2025-05-31 18:01:36 -0700

    add tests for all noteboooks
    Signed-off-by: Kunling Geng <kunling@kunling-LGW6TWQYVT.local>

--- doc/source/ray-overview/examples/object-detection/ci/tests.sh
+++ doc/source/ray-overview/examples/object-detection/ci/tests.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 pip install jupytext
 python ci/test_myst_doc.py 1.object_detection_train.ipynb
+python ci/test_myst_doc.py 2.object_detection_batch_inference_eval.ipynb
+python ci/test_myst_doc.py 3.video_processing_batch_inference.ipynb
+python ci/test_myst_doc.py 4.object_detection_serve.ipynb
