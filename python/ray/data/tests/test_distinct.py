66e85d457739d2898334b429c52ec6b972d24875
soffer-anyscale <stephen.offer@anyscale.com>
2025-05-31 17:58:02 -0700

    fixed unit test
    Signed-off-by: soffer-anyscale <stephen.offer@anyscale.com>

--- python/ray/data/tests/test_distinct.py
+++ python/ray/data/tests/test_distinct.py
@@ -4,7 +4,7 @@ import pytest
 import ray
 
 
-def test_distinct_all_columns_local(ray_start):
+def test_distinct_all_columns_local():
     ds = ray.data.from_arrow(
         pa.table({"a": [1, 2, 2, 1, 3], "b": ["x", "y", "y", "x", "z"]})
     )
@@ -12,7 +12,7 @@ def test_distinct_all_columns_local(ray_start):
     assert out == [{"a": 1, "b": "x"}, {"a": 2, "b": "y"}, {"a": 3, "b": "z"}]
 
 
-def test_distinct_across_blocks(ray_start):
+def test_distinct_across_blocks():
     table = pa.table({"a": [1, 2, 2, 3, 1, 3], "b": ["x", "y", "y", "z", "x", "z"]})
     ds = ray.data.from_arrow(table).repartition(3)
     # Should still dedupe globally
@@ -20,13 +20,13 @@ def test_distinct_across_blocks(ray_start):
     assert out == [{"a": 1, "b": "x"}, {"a": 2, "b": "y"}, {"a": 3, "b": "z"}]
 
 
-def test_distinct_all_unique(ray_start):
+def test_distinct_all_unique():
     ds = ray.data.from_arrow(pa.table({"a": [1, 2, 3], "b": ["x", "y", "z"]}))
     out = ds.distinct().sort(key=["a", "b"]).take_all()
     assert out == [{"a": 1, "b": "x"}, {"a": 2, "b": "y"}, {"a": 3, "b": "z"}]
 
 
-def test_distinct_all_duplicate(ray_start):
+def test_distinct_all_duplicate():
     ds = ray.data.from_arrow(
         pa.table({"a": [5, 5, 5, 5], "b": ["foo", "foo", "foo", "foo"]})
     )
@@ -34,7 +34,7 @@ def test_distinct_all_duplicate(ray_start):
     assert out == [{"a": 5, "b": "foo"}]
 
 
-def test_distinct_empty(ray_start):
+def test_distinct_empty():
     ds = ray.data.from_arrow(pa.table({"a": [], "b": []}))
     out = ds.distinct().take_all()
     assert out == []
