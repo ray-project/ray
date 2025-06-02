e87dc4226854639001cd2e9751fbf74b13abc714
soffer-anyscale <stephen.offer@anyscale.com>
2025-06-02 15:12:22 -0700

    updated unit test
    Signed-off-by: soffer-anyscale <stephen.offer@anyscale.com>

--- python/ray/data/tests/test_compact.py
+++ python/ray/data/tests/test_compact.py
@@ -3,6 +3,7 @@ import numpy as np
 
 import ray
 
+
 def make_dataset(sizes_mib):
     # Creates a Ray Dataset where each block is ~size MiB as a numpy array
     # One block per size entry
@@ -10,6 +11,7 @@ def make_dataset(sizes_mib):
     # Make sure each block is constructed as a Ray Dataset from an explicit block
     ds = ray.data.from_numpy_refs([ray.put(b) for b in blocks])
     return ds
+
 
 def test_compact_merges_blocks():
     ds = make_dataset([5] * 8)  # 8x5MiB
@@ -21,10 +23,12 @@ def test_compact_merges_blocks():
     assert out.num_blocks() == 2
     assert out.count() == ds.count()
 
+
 def test_compact_noop_if_already_at_target():
     ds = make_dataset([16] * 2)
     out = ds.compact("16MiB")
     assert out is ds
+
 
 def test_compact_empty_dataset():
     ds = ray.data.from_items([])
@@ -32,7 +36,8 @@ def test_compact_empty_dataset():
     assert out.count() == 0
     assert out is ds
 
-@pytest.mark.parametrize("val", ["1MiB", 1024*1024, "2MiB"])
+
+@pytest.mark.parametrize("val", ["1MiB", 1024 * 1024, "2MiB"])
 def test_compact_accepts_int_and_str(val):
     ds = make_dataset([1, 1])
     out = ds.compact(val)
