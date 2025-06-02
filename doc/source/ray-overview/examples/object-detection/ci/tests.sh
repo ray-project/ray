6d24fb697e1943a2e7ca18e296888d7c91c8f431
Kunling Geng <kunling@kunling-LGW6TWQYVT.local>
2025-06-02 10:04:53 -0700

    fix release tests.sh
    Signed-off-by: Kunling Geng <kunling@kunling-LGW6TWQYVT.local>

--- doc/source/ray-overview/examples/object-detection/ci/tests.sh
+++ doc/source/ray-overview/examples/object-detection/ci/tests.sh
@@ -4,7 +4,7 @@
 # to check each subprocess unit and validate that errors
 # aren't being consumed/hidden
 
-for nb in object_detection_train 2.object_detection_batch_inference_eval 3.video_processing_batch_inference 4.object_detection_serve; do
+for nb in 1.object_detection_train 2.object_detection_batch_inference_eval 3.video_processing_batch_inference 4.object_detection_serve; do
   # Convert .ipynb → .py (in the current dir)
   python ci/nb2py.py "${nb}.ipynb" "${nb}.py"
   # Run the generated script (also in the current dir)
