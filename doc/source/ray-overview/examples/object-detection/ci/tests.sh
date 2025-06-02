0b94a13b6ed6fd6811117484d8f93a279bd96610
Kunling Geng <kunling@kunling-LGW6TWQYVT.local>
2025-06-01 20:55:32 -0700

    use nb2py for ci tests
    Signed-off-by: Kunling Geng <kunling@kunling-LGW6TWQYVT.local>

--- doc/source/ray-overview/examples/object-detection/ci/tests.sh
+++ doc/source/ray-overview/examples/object-detection/ci/tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-pip install jupytext
-python ci/test_myst_doc.py --path 1.object_detection_train.ipynb
-python ci/test_myst_doc.py --path 2.object_detection_batch_inference_eval.ipynb
-python ci/test_myst_doc.py --path 3.video_processing_batch_inference.ipynb
-python ci/test_myst_doc.py --path 4.object_detection_serve.ipynb
+
+# don't use nbcovert or jupytext unless you're willing
+# to check each subprocess unit and validate that errors
+# aren't being consumed/hidden
+
+for nb in object_detection_train 2.object_detection_batch_inference_eval 3.video_processing_batch_inference 4.object_detection_serve; do
+  python ci/nb2py.py notebooks/${nb}.ipynb notebooks/${nb}.py  # convert notebook to script
+  (cd notebooks && python ${nb}.py)  # run generated script
+  (cd notebooks && rm ${nb}.py)  # remove the generated script
+done
