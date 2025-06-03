f7be0e8fb6a4100bdb13bbe61d896877c7013807
can <can@anyscale.com>
2025-06-03 04:44:38 +0000

    [core][telemetry/09] record sum metric e2e
    Signed-off-by: can <can@anyscale.com>

--- src/ray/stats/tests/metric_with_open_telemetry_test.cc
+++ src/ray/stats/tests/metric_with_open_telemetry_test.cc
@@ -114,5 +114,14 @@ TEST_F(MetricTest, TestSumMetric) {
   STATS_metric_sum_test.Record(200.0, {{"Tag1", "Value1"}, {"Tag2", "Value2"}});
 }
 
+TEST_F(MetricTest, TestSumMetric) {
+  ASSERT_TRUE(
+      OpenTelemetryMetricRecorder::GetInstance().IsMetricRegistered("metric_sum_test"));
+  // We only test that recording is not crashing. The actual value is not checked
+  // because open telemetry does not provide a way to retrieve the value of a counter.
+  // Checking value is performed via e2e tests instead (e.g., in test_metrics_agent.py).
+  STATS_metric_sum_test.Record(200.0, {{"Tag1", "Value1"}, {"Tag2", "Value2"}});
+}
+
 }  // namespace telemetry
 }  // namespace ray
