8c054a967f8e40f4d637b3e5d572ea216d762a79
Matthew <user@LAPTOP-8PF05T9K>
2025-06-03 09:48:35 -0500

    Ran some testing, updated the example script to better match newer additions.
    Signed-off-by: Matthew <mweston3@illinois.edu>

--- rllib/examples/catalogs/attention_encoder.py
+++ rllib/examples/catalogs/attention_encoder.py
@@ -5,10 +5,21 @@ It demonstrates:
 
  - How to write a custom catalog and pytorch Encoder
  - How to operate upon a Repeated observation space when training a model
+ 
+How to run this script
+----------------------
+`python [script file name].py
 
+For debugging, use the following additional command line options
+`--no-tune --num-env-runners=0`
+which should allow you to set breakpoints anywhere in the RLlib code and
+have the execution stop there for inspection and debugging.
+
+For logging to your WandB account, use:
+`--wandb-key=[your WandB API key] --wandb-project=[some project name]
+--wandb-run-name=[optional: WandB run name (within the defined project)]`
 """
 
-# __sphinx_doc_begin__
 import gymnasium as gym
 import numpy as np
 
@@ -16,23 +27,34 @@ from ray.rllib.algorithms.ppo.ppo import PPOConfig
 from ray.rllib.algorithms.ppo.ppo_catalog import PPOCatalog
 from ray.rllib.core.rl_module.rl_module import RLModuleSpec
 from ray.tune.registry import register_env
+from ray.rllib.utils.test_utils import (
+    add_rllib_example_script_args,
+    run_rllib_example_script_experiment,
+)
 
 from ray.rllib.env.wrappers.repeated_wrapper import ObsVectorizationWrapper
 from ray.rllib.examples.catalogs.models.attention_encoder import AttentionEncoderConfig
 from ray.rllib.examples.envs.classes.repeated_obs_env import RepeatedObsEnv
 
-# TODO: Remove
+
 from ray.rllib.utils.metrics import (
-    DIFF_NUM_GRAD_UPDATES_VS_SAMPLER_POLICY,
     ENV_RUNNER_RESULTS,
     EPISODE_RETURN_MEAN,
-    EVALUATION_RESULTS,
-    NUM_ENV_STEPS_TRAINED,
-    NUM_ENV_STEPS_SAMPLED_LIFETIME,
+    TRAINING_ITERATION_TIMER,
 )
 
-# Define a PPO Catalog that we can use to inject our attention-based Encoder into RLlib's
-# decision tree of what model to choose
+# Handle command line args
+parser = add_rllib_example_script_args(
+    default_reward=0.8, default_iters=50
+)
+parser.set_defaults(
+    enable_new_api_stack=True,
+)
+parser.add_argument("--max-voter-pairs",type=int,default=5)
+parser.add_argument("--num-values",type=int,default=2)
+parser.add_argument("--attn-dim",type=int,default=128)
+
+# Define a PPO Catalog that tells Rllib to use our custom encoder
 class AttentionPPOCatalog(PPOCatalog):
     '''
       A special PPO catalog producing an encoder that handles dictionaries of (potentially Repeated) action spaces in the same manner as https://arxiv.org/abs/1909.07528.
@@ -41,48 +63,49 @@ class AttentionPPOCatalog(PPOCatalog):
     def _get_encoder_config(cls,observation_space: gym.Space,**kwargs,):
       return AttentionEncoderConfig(observation_space, **kwargs)
 
-register_env("env", lambda cfg: ObsVectorizationWrapper(RepeatedObsEnv(cfg)))
+if __name__ == "__main__":
+    args = parser.parse_args()
+    # The wrapper allows Repeated observations to be batched successfully by Rllib.
+    register_env("env", lambda cfg: ObsVectorizationWrapper(RepeatedObsEnv(cfg)))
 
-# Create a generic config with our enhanced Catalog
-ppo_config = (
-    PPOConfig()
-    .environment(
-        env="env",
-        env_config={
-            'max_voter_pairs': 5,
-            'num_values': 2,
-            'random_seed': 0
+    # Define our config
+    config = (
+        PPOConfig()
+        .environment(
+            env="env",
+            env_config={
+                'max_voter_pairs': args.max_voter_pairs,
+                'num_values': args.num_values,
+                'random_seed': 0
+            },
+        )
+        .framework("torch")
+        .training(
+            train_batch_size=256,
+        )
+        .rl_module(rl_module_spec=RLModuleSpec(
+              catalog_class=AttentionPPOCatalog,
+              model_config={
+                  "attention_emb_dim": args.attn_dim,
+                  "head_fcnet_hiddens": (256,256),
+                  'vf_share_layers': False,
+              }
+            ),
+        )
+    )
+
+    # Set the stopping arguments.
+    EPISODE_RETURN_MEAN_KEY = f"{ENV_RUNNER_RESULTS}/{EPISODE_RETURN_MEAN}"
+    stop = {
+        TRAINING_ITERATION_TIMER: args.stop_iters,
+        EPISODE_RETURN_MEAN_KEY: args.stop_reward,
+    }
+
+    # Run the experiment.
+    run_rllib_example_script_experiment(config,
+        args,
+        stop=stop,
+        success_metric={
+            f"{ENV_RUNNER_RESULTS}/{EPISODE_RETURN_MEAN}": args.stop_reward,
         },
     )
-    .framework("torch")
-    .env_runners(
-        num_env_runners=0,
-        num_envs_per_env_runner=1,
-    )
-    .training(
-        train_batch_size=256,
-    )
-    .rl_module(rl_module_spec=RLModuleSpec(
-          catalog_class=AttentionPPOCatalog,
-          model_config={
-              "attention_emb_dim": 128,
-              "head_fcnet_hiddens": (256,256),
-              'vf_share_layers': False,
-          }
-        ),
-    )
-)
-
-algo = ppo_config.build_algo()
-
-# TODO: Switch this to the experiment setup used by everything else.
-num_iters = 100
-
-for i in range(num_iters):
-  results = algo.train()
-  if ENV_RUNNER_RESULTS in results:
-      mean_return = results[ENV_RUNNER_RESULTS].get(
-          EPISODE_RETURN_MEAN, np.nan
-      )
-      print(f"iter={i} R={mean_return}")
-# __sphinx_doc_end__
