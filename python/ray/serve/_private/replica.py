50975319a6851a3a6e0172ce2dae109dd15b2d7e
Ziy1-Tan <ajb459684460@gmail.com>
2025-06-04 20:04:55 +0800

    Remove unused docs_path
    Signed-off-by: Ziy1-Tan <ajb459684460@gmail.com>

--- python/ray/serve/_private/replica.py
+++ python/ray/serve/_private/replica.py
@@ -999,7 +999,7 @@ class ReplicaActor:
             2. DeploymentVersion of the replica
             3. Initialization duration in seconds
             4. Port
-            5. Serve docs_path
+            5. FastAPI `docs_path`, if relevant (i.e. this is an ingress deployment integrated with FastAPI).
         """
         # Unused `_after` argument is for scheduling: passing an ObjectRef
         # allows delaying this call until after the `_after` call has returned.
