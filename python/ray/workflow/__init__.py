5238152211443f8940e72497292c02175383490a
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-06-06 13:58:58 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/workflow/__init__.py
+++ python/ray/workflow/__init__.py
@@ -1,4 +1,4 @@
 raise RuntimeError(
-    "The experimental Ray Workflows library has been removed. "
-    "The last Ray release containing ray.workflows is `ray==2.47`."
+    "The experimental Ray Workflows library was deprecated in Ray 2.44 and has been "
+    "removed. The last Ray release containing ray.workflows is `ray==2.47`."
 )
