11c70ddaa96469d685c8ac5fbfa24402e826514b
Ricardo Decal <crypdick@users.noreply.github.com>
2025-06-09 17:06:11 -0700

    [Docs] Adds second notebook to timeseries tutorial (#53561)
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- doc/source/ray-overview/examples/e2e-timeseries/index.rst
+++ doc/source/ray-overview/examples/e2e-timeseries/index.rst
@@ -9,3 +9,4 @@ An end-to-end tutorial to train a time-series model.
 
     ./README.ipynb
     ./e2e_timeseries/01-Distributed-Training.ipynb
+    ./e2e_timeseries/02-Validation.ipynb
--- release/ray_release/byod/byod_e2e_timeseries.sh
+++ release/ray_release/byod/byod_e2e_timeseries.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set bash options for safer script execution:
+# -e: Exit immediately if any command fails
+# -x: Print each command before executing it (for debugging)
+# -o pipefail: Fail if any command in a pipeline fails (not just the last one)
 set -exo pipefail
 
 # Install Python dependencies.
