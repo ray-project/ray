d87b2ba17f270a3a85ff652265ae4f4cee5a0578
Srinath Krishnamachari <srinath.krishnamachari@anyscale.com>
2025-06-10 04:43:13 +0000

    Misc fixes
    Signed-off-by: Srinath Krishnamachari <srinath.krishnamachari@anyscale.com>

--- release/train_tests/benchmark/image_classification/run_benchmark.sh
+++ release/train_tests/benchmark/image_classification/run_benchmark.sh
@@ -37,28 +37,32 @@ fi
 echo "Running command: ${ARGS[*]}"
 echo "Number of runs: $NUM_RUNS"
 
-TOTAL_TIME=0
+TOTAL_NANO=0
 
 for i in $(seq 1 $NUM_RUNS); do
     echo "Run $i/$NUM_RUNS"
 
-    # Time command execution and capture output
-    START_TIME=$(date +%s.%N)
+    START_TIME_NS=$(date +%s%N)
     "${ARGS[@]}" > "run_${i}.log" 2>&1
     EXIT_CODE=$?
-    END_TIME=$(date +%s.%N)
+    END_TIME_NS=$(date +%s%N)
 
-    # Calculate duration
-    DURATION=$(echo "$END_TIME - $START_TIME" | bc)
-    TOTAL_TIME=$(echo "$TOTAL_TIME + $DURATION" | bc)
+    DURATION_NS=$((END_TIME_NS - START_TIME_NS))
+    TOTAL_NANO=$((TOTAL_NANO + DURATION_NS))
 
-    echo "Duration: ${DURATION}s"
+    DURATION_SEC=$((DURATION_NS / 1000000000))
+    DURATION_MS=$(((DURATION_NS / 1000000) % 1000))
+    echo "Duration: ${DURATION_SEC}.${DURATION_MS}s"
     echo "Exit Code: $EXIT_CODE"
     echo "Log: run_${i}.log"
     echo "---"
 done
 
-# Print summary
-AVG_TIME=$(echo "$TOTAL_TIME / $NUM_RUNS" | bc -l)
-printf "Total time: %.3fs\n" "$TOTAL_TIME"
-printf "Average time per run: %.3fs\n" "$AVG_TIME"
+AVG_NANO=$((TOTAL_NANO / NUM_RUNS))
+AVG_SEC=$((AVG_NANO / 1000000000))
+AVG_MS=$(((AVG_NANO / 1000000) % 1000))
+TOTAL_SEC=$((TOTAL_NANO / 1000000000))
+TOTAL_MS=$(((TOTAL_NANO / 1000000) % 1000))
+
+printf "Total time: %d.%03ds\n" "$TOTAL_SEC" "$TOTAL_MS"
+printf "Average time per run: %d.%03ds\n" "$AVG_SEC" "$AVG_MS"
