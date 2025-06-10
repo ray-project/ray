c69a1104972307a513c3ad23c6ffe5ac995b5282
akyang-anyscale <alexyang@anyscale.com>
2025-06-10 14:39:50 -0700

    remove timeout
    Signed-off-by: akyang-anyscale <alexyang@anyscale.com>

--- python/ray/serve/tests/test_backpressure.py
+++ python/ray/serve/tests/test_backpressure.py
@@ -67,7 +67,7 @@ def test_http_backpressure(serve_instance):
 
     @ray.remote(num_cpus=0)
     def do_request(msg: str) -> Tuple[int, str]:
-        r = httpx.request("GET", "http://localhost:8000/", json={"msg": msg})
+        r = httpx.request("GET", "http://localhost:8000/", json={"msg": msg}, timeout=None)
         return r.status_code, r.text
 
     # First response should block. Until the signal is sent, all subsequent requests
