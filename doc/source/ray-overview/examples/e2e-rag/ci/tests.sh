4a28e9e2a158078bf867117a8ace3152556dd507
GokuMohandas <gokumd@gmail.com>
2025-06-10 08:54:46 -0700

    added tests

--- doc/source/ray-overview/examples/e2e-rag/ci/tests.sh
+++ doc/source/ray-overview/examples/e2e-rag/ci/tests.sh
@@ -4,7 +4,17 @@
 # to check each subprocess unit and validate that errors
 # aren't being consumed/hidden
 
-for nb in 01-Batch-Inference 02-Distributed-Training 03-Online-Serving; do
+ordered_notebook_names=(
+  "01_(Optional)_Regular_Document_Processing_Pipeline"
+  "02_Scalable_RAG_Data_Ingestion_with_Ray_Data"
+  "03_Deploy_LLM_with_Ray_Serve"
+  # "04_Build_Basic_RAG_Chatbot"                         # skipped - requires bespoke Anyscale service
+  # "05_Improve_RAG_with_Prompt_Engineering"             # skipped - requires bespoke Anyscale service
+  # "06_(Optional)_Evaluate_RAG_with_Online_Inference"   # skipped - requires bespoke Anyscale service
+  "07_Evaluate_RAG_with_Ray_Data_LLM_Batch_inference"
+)
+
+for nb in "${ordered_notebook_names[@]}"; do
   python ci/nb2py.py notebooks/${nb}.ipynb notebooks/${nb}.py  # convert notebook to script
   (cd notebooks && python ${nb}.py)  # run generated script
   (cd notebooks && rm ${nb}.py)  # remove the generated script
