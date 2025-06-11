d745acc196f90cad2fdc226700dfbfe3fe199588
can <can@anyscale.com>
2025-06-11 14:11:26 +0000

    [core][telemetry/10] support custom gauge+counter+sum metrics
    Signed-off-by: can <can@anyscale.com>

--- src/ray/stats/metric.cc
+++ src/ray/stats/metric.cc
@@ -203,6 +203,11 @@ void Gauge::RegisterOpenTelemetryMetric() {
 }
 
 void Gauge::RegisterView() {
+  if (::RayConfig::instance().experimental_enable_open_telemetry_on_core()) {
+    // Register the metric in OpenTelemetry.
+    OpenTelemetryMetricRecorder::GetInstance().RegisterGaugeMetric(name_, description_);
+    return;
+  }
   opencensus::stats::ViewDescriptor view_descriptor =
       opencensus::stats::ViewDescriptor()
           .set_name(name_)
@@ -237,6 +242,11 @@ void Count::RegisterOpenTelemetryMetric() {
 }
 
 void Count::RegisterView() {
+  if (::RayConfig::instance().experimental_enable_open_telemetry_on_core()) {
+    // Register the metric in OpenTelemetry.
+    OpenTelemetryMetricRecorder::GetInstance().RegisterCounterMetric(name_, description_);
+    return;
+  }
   opencensus::stats::ViewDescriptor view_descriptor =
       opencensus::stats::ViewDescriptor()
           .set_name(name_)
@@ -252,6 +262,11 @@ void Sum::RegisterOpenTelemetryMetric() {
 }
 
 void Sum::RegisterView() {
+  if (::RayConfig::instance().experimental_enable_open_telemetry_on_core()) {
+    // Register the metric in OpenTelemetry.
+    OpenTelemetryMetricRecorder::GetInstance().RegisterSumMetric(name_, description_);
+    return;
+  }
   opencensus::stats::ViewDescriptor view_descriptor =
       opencensus::stats::ViewDescriptor()
           .set_name(name_)
