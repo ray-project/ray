12122d3040bbbd5cf060931404fcd72271efba06
can <can@anyscale.com>
2025-06-11 14:11:26 +0000

    [core][telemetry/10] support custom gauge+counter+sum metrics
    Signed-off-by: can <can@anyscale.com>
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- src/ray/stats/metric.cc
+++ src/ray/stats/metric.cc
@@ -113,6 +113,37 @@ void Metric::Record(double value, TagsType tags) {
     return;
   }
 
+  if (::RayConfig::instance().experimental_enable_open_telemetry_on_core()) {
+    // Register the metric if it is not registered yet. Otherwise this will be a no-op.
+    // We register the metric at the first time we record it instead of at the
+    // construction time to avoid static initialization order issues. This function
+    // is thread-safe.
+    RegisterOpenTelemetryMetric();
+    if (OpenTelemetryMetricRecorder::GetInstance().IsMetricRegistered(name_)) {
+      // Collect tags from both the metric-specific tags and the global tags.
+      absl::flat_hash_map<std::string, std::string> open_telemetry_tags;
+      std::unordered_set<std::string> tag_keys_set;
+      for (const auto &tag_key : tag_keys_) {
+        tag_keys_set.insert(tag_key.name());
+      }
+      // Insert metric-specific tags that match the expected keys.
+      for (const auto &tag : tags) {
+        const std::string &key = tag.first.name();
+        if (tag_keys_set.count(key)) {
+          open_telemetry_tags[key] = tag.second;
+        }
+      }
+      // Add global tags, overwriting any existing tag keys.
+      for (const auto &tag : StatsConfig::instance().GetGlobalTags()) {
+        open_telemetry_tags[tag.first.name()] = tag.second;
+      }
+      OpenTelemetryMetricRecorder::GetInstance().SetMetricValue(
+          name_, std::move(open_telemetry_tags), value);
+
+      return;
+    }
+  }
+
   absl::MutexLock lock(&registration_mutex_);
   if (measure_ == nullptr) {
     // Measure could be registered before, so we try to get it first.
@@ -159,6 +190,11 @@ void Metric::Record(double value, std::unordered_map<std::string, std::string> t
 
 Metric::~Metric() { opencensus::stats::StatsExporter::RemoveView(name_); }
 
+void Gauge::RegisterOpenTelemetryMetric() {
+  // Register the metric in OpenTelemetry.
+  OpenTelemetryMetricRecorder::GetInstance().RegisterGaugeMetric(name_, description_);
+}
+
 void Gauge::RegisterView() {
   opencensus::stats::ViewDescriptor view_descriptor =
       opencensus::stats::ViewDescriptor()
@@ -167,6 +203,11 @@ void Gauge::RegisterView() {
           .set_measure(name_)
           .set_aggregation(opencensus::stats::Aggregation::LastValue());
   internal::RegisterAsView(view_descriptor, tag_keys_);
+}
+
+void Histogram::RegisterOpenTelemetryMetric() {
+  // Histogram is not supported in OpenTelemetry.
+  return;
 }
 
 void Histogram::RegisterView() {
@@ -181,6 +222,10 @@ void Histogram::RegisterView() {
   internal::RegisterAsView(view_descriptor, tag_keys_);
 }
 
+void Count::RegisterOpenTelemetryMetric() {
+  OpenTelemetryMetricRecorder::GetInstance().RegisterCounterMetric(name_, description_);
+}
+
 void Count::RegisterView() {
   opencensus::stats::ViewDescriptor view_descriptor =
       opencensus::stats::ViewDescriptor()
@@ -190,6 +235,10 @@ void Count::RegisterView() {
           .set_aggregation(opencensus::stats::Aggregation::Count());
 
   internal::RegisterAsView(view_descriptor, tag_keys_);
+}
+
+void Sum::RegisterOpenTelemetryMetric() {
+  OpenTelemetryMetricRecorder::GetInstance().RegisterSumMetric(name_, description_);
 }
 
 void Sum::RegisterView() {
