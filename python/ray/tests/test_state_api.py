29dfe3e671ed5958d379284f67d9f5f855cf540c
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-06-13 10:00:45 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/tests/test_state_api.py
+++ python/ray/tests/test_state_api.py
@@ -7,7 +7,9 @@ from collections import Counter
 from concurrent.futures import ThreadPoolExecutor
 from typing import List
 from unittest.mock import MagicMock, AsyncMock, patch
+import yaml
 
+from click.testing import CliRunner
 import pytest
 import pytest_asyncio
 from ray._private.state_api_test_utils import (
@@ -18,8 +20,6 @@ from ray._private.state_api_test_utils import (
 from ray.util.state import get_job
 from ray.dashboard.modules.job.pydantic_models import JobDetails
 from ray.util.state.common import Humanify
-import yaml
-from click.testing import CliRunner
 
 from ray.util.scheduling_strategies import PlacementGroupSchedulingStrategy
 import ray
@@ -1767,17 +1767,12 @@ def test_humanify():
 async def test_state_data_source_client_limit_distributed_sources(ray_start_cluster):
     cluster = ray_start_cluster
     # head
-    cluster.add_node(num_cpus=8)
+    cluster.add_node(num_cpus=8, dashboard_agent_listen_port=find_free_port())
     ray.init(address=cluster.address)
     client = state_source_client(cluster.address)
 
-    nodes = ray.nodes()
-    assert len(nodes) == 1
-    ip = nodes[0]["NodeManagerAddress"]
-    port = int(nodes[0]["NodeManagerPort"])
-    """
-    Test objects
-    """
+    [node] = ray.nodes()
+    ip, port = node["NodeManagerAddress"], int(node["NodeManagerPort"])
 
     @ray.remote
     def long_running_task(obj):  # noqa
@@ -1810,8 +1805,7 @@ async def test_state_data_source_client_limit_distributed_sources(ray_start_clus
         return True
 
     await async_wait_for_condition(verify)
-    for ref in refs:
-        ray.cancel(ref, force=True, recursive=True)
+    [ray.cancel(ref, force=True) for ref in refs]
     del refs
 
     """
@@ -1865,7 +1859,7 @@ def test_cli_apis_sanity_check(ray_start_cluster):
     cluster.add_node(num_cpus=2)
     ray.init(address=cluster.address)
     for _ in range(NUM_NODES - 1):
-        cluster.add_node(num_cpus=2)
+        cluster.add_node(num_cpus=2, dashboard_agent_listen_port=find_free_port())
     runner = CliRunner()
 
     client = JobSubmissionClient(
@@ -2159,7 +2153,7 @@ async def test_cloud_envs(ray_start_cluster, monkeypatch):
             "test_cloud_id",
         )
         m.setenv("RAY_NODE_TYPE_NAME", "test-node-type")
-        cluster.add_node(num_cpus=1, node_name="worker_node")
+        cluster.add_node(num_cpus=1, node_name="worker_node", dashboard_agent_listen_port=find_free_port())
     client = state_source_client(cluster.address)
 
     async def verify():
@@ -2187,7 +2181,7 @@ def test_list_get_nodes(ray_start_cluster):
     cluster = ray_start_cluster
     cluster.add_node(num_cpus=1, node_name="head_node")
     ray.init(address=cluster.address)
-    worker_node = cluster.add_node(num_cpus=1, node_name="worker_node")
+    worker_node = cluster.add_node(num_cpus=1, node_name="worker_node", dashboard_agent_listen_port=find_free_port())
 
     cluster.remove_node(worker_node)
 
