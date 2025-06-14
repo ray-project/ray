0b02f91b605711935fa6834572184eb92ffc31b1
Dhakshin Suriakannu <dhakshin01@gmail.com>
2025-06-14 14:00:42 -0700

    fix format

--- python/ray/serve/tests/test_deploy_2.py
+++ python/ray/serve/tests/test_deploy_2.py
@@ -12,7 +12,7 @@ import pytest
 import ray
 from ray import serve
 from ray._private.test_utils import SignalActor, wait_for_condition
-from ray.serve._private.common import DeploymentStatus, DeploymentID
+from ray.serve._private.common import DeploymentID, DeploymentStatus
 from ray.serve._private.logging_utils import get_serve_logs_dir
 from ray.serve._private.test_utils import (
     check_deployment_status,
