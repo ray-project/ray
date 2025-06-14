37a6c15e552c5e90cf1c23321f913b5cbb4f363c
Timothy Seah <tseah@anyscale.com>
2025-06-13 18:33:18 -0700

    use state correctly
    Signed-off-by: Timothy Seah <tseah@anyscale.com>

--- python/ray/train/v2/_internal/state/state_actor.py
+++ python/ray/train/v2/_internal/state/state_actor.py
@@ -28,7 +28,6 @@ from ray.train.v2._internal.state.util import (
 )
 from ray.train.v2._internal.util import time_monotonic
 from ray.util.state import get_actor
-from ray.util.state.common import ActorState
 
 logger = logging.getLogger(__name__)
 
@@ -70,7 +69,7 @@ class TrainStateActor:
                     if is_terminal_run_status(run.status):
                         continue
                     actor_state = get_actor(run.controller_actor_id)
-                    if not actor_state or actor_state.state == ActorState.DEAD:
+                    if not actor_state or actor_state.state == "DEAD":
                         update_train_run_aborted(run)
                         self.create_or_update_train_run(run)
                         for run_attempt in self._run_attempts.get(run_id, {}).values():
