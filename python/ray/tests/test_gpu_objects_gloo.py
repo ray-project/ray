d2c1bf0a535ff9a75053781ae72ac5e9620c0c39
Stephanie Wang <smwang@cs.washington.edu>
2025-06-16 16:52:35 -0700

    [core][gpu objects] Integrate single-controller collective APIs with GPU objects (#53720)
    Adds integration between the single-controller collective APIs
    introduced in #53319 and the GPU objects feature prototyped in #52938.
    Actor collectives created through
    `ray.experimental.collective.create_collective_group` will now be
    automatically used if a task declares a tensor transport other than the
    default OBJECT_STORE. This also adds support for allocating the torch
    tensors on the correct device (GPU for NCCL and CPU for GLOO).
    
    See updates in test_gpu_objects.py for examples.
    ---------
    
    Signed-off-by: Stephanie wang <smwang@cs.washington.edu>
    Signed-off-by: Stephanie Wang <smwang@cs.washington.edu>
    Co-authored-by: Kai-Hsun Chen <kaihsun@apache.org>
    Co-authored-by: Edward Oakes <ed.nmi.oakes@gmail.com>
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- python/ray/tests/test_gpu_objects_gloo.py
+++ python/ray/tests/test_gpu_objects_gloo.py
@@ -3,22 +3,12 @@ import random
 import torch
 import pytest
 import ray
-import torch.distributed as dist
-from ray.experimental.channel.torch_tensor_type import TorchTensorType
-from ray.experimental.channel import ChannelContext
+from ray.experimental.collective import create_collective_group
+from ray._private.custom_types import TensorTransportEnum
 
 
 @ray.remote
 class GPUTestActor:
-    def register_custom_serializer(self):
-        TorchTensorType().register_custom_serializer()
-
-    def setup(self, world_size, rank):
-        init_method = "tcp://localhost:8889"
-        dist.init_process_group(
-            backend="gloo", world_size=world_size, rank=rank, init_method=init_method
-        )
-
     @ray.method(tensor_transport="gloo")
     def echo(self, data):
         return data
@@ -37,21 +27,10 @@ class GPUTestActor:
         return None
 
 
-def init_process_group(actors):
-    world_size = len(actors)
-    ray.get([actor.setup.remote(world_size, i) for i, actor in enumerate(actors)])
-    # Set up communicator so that the driver knows the actor-to-rank mapping.
-    ctx = ChannelContext.get_current()
-    ctx.communicators[0] = actors
-    # Register custom serializer so that the serializer can retrieve tensors from
-    # return values of actor methods.
-    ray.get([actor.register_custom_serializer.remote() for actor in actors])
-
-
 def test_inter_actor_gpu_tensor_transfer(ray_start_regular):
     world_size = 2
     actors = [GPUTestActor.remote() for _ in range(world_size)]
-    init_process_group(actors)
+    create_collective_group(actors, backend="torch_gloo")
 
     small_tensor = torch.randn((1,))
     sender = actors[0]
@@ -69,7 +48,7 @@ def test_inter_actor_gpu_tensor_transfer(ray_start_regular):
 
 def test_intra_gpu_tensor_transfer(ray_start_regular):
     actor = GPUTestActor.remote()
-    init_process_group([actor])
+    create_collective_group([actor], backend="torch_gloo")
 
     small_tensor = torch.randn((1,))
 
@@ -101,7 +80,7 @@ def test_intra_gpu_tensor_transfer(ray_start_regular):
 def test_mix_cpu_gpu_data(ray_start_regular):
     world_size = 2
     actors = [GPUTestActor.remote() for _ in range(world_size)]
-    init_process_group(actors)
+    create_collective_group(actors, backend="torch_gloo")
 
     tensor = torch.randn((1,))
     cpu_data = random.randint(0, 100)
@@ -119,7 +98,7 @@ def test_mix_cpu_gpu_data(ray_start_regular):
 def test_multiple_tensors(ray_start_regular):
     world_size = 2
     actors = [GPUTestActor.remote() for _ in range(world_size)]
-    init_process_group(actors)
+    create_collective_group(actors, backend="torch_gloo")
 
     tensor1 = torch.randn((1,))
     tensor2 = torch.randn((2,))
@@ -139,7 +118,7 @@ def test_multiple_tensors(ray_start_regular):
 def test_trigger_out_of_band_tensor_transfer(ray_start_regular):
     world_size = 2
     actors = [GPUTestActor.remote() for _ in range(world_size)]
-    init_process_group(actors)
+    create_collective_group(actors, backend="torch_gloo")
 
     src_actor, dst_actor = actors[0], actors[1]
 
@@ -153,7 +132,7 @@ def test_trigger_out_of_band_tensor_transfer(ray_start_regular):
     assert torch.equal(ret_val_src[0], tensor)
 
     gpu_object_manager = ray._private.worker.global_worker.gpu_object_manager
-    gpu_object_manager.add_gpu_object_ref(gpu_ref, src_actor)
+    gpu_object_manager.add_gpu_object_ref(gpu_ref, src_actor, TensorTransportEnum.GLOO)
 
     # Trigger out-of-band tensor transfer from src_actor to dst_actor.
     # The GPU object will be removed from src_actor's GPU object store
