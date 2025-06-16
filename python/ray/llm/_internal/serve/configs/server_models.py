79763707b8993efa1508721553f4c2e4ffb802e0
Seiji Eicher <seiji@anyscale.com>
2025-06-16 10:01:41 -0700

    Upgrade workaround to fix
    Signed-off-by: Seiji Eicher <seiji@anyscale.com>

--- python/ray/llm/_internal/serve/configs/server_models.py
+++ python/ray/llm/_internal/serve/configs/server_models.py
@@ -275,17 +275,6 @@ class LLMConfig(BaseModelExtended):
         self._infer_supports_vision(model_id_or_path)
         self._set_model_architecture(model_id_or_path)
 
-        # TODO(seiji): remove this once https://github.com/vllm-project/vllm/pull/19510 is merged
-        # It prevents a pickle error since this loads transformers_modules
-        try:
-            transformers.AutoProcessor.from_pretrained(
-                model_id_or_path,
-                trust_remote_code=trust_remote_code,
-            )
-        except Exception:
-            # Some models don't have processors, which is fine
-            pass
-
     @property
     def supports_vision(self) -> bool:
         return self._supports_vision
