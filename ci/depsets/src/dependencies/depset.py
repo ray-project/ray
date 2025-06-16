aff447b077a3ca626bd1a40efa001bf1af492a02
elliot-barn <elliot.barnwell@anyscale.com>
2025-06-16 12:27:11 -0700

    adding dependencyDAG to depsets
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/depsets/src/dependencies/depset.py
+++ ci/depsets/src/dependencies/depset.py
@@ -1,5 +1,5 @@
 from typing import List, Dict, Optional
-
+from collections import defaultdict
 import os
 
 class Dep:
@@ -101,3 +101,79 @@ class DepGraph:
     def add_dep(self, dep: Dep):
         self.graph[dep.name] = dep
 
+class DependencyDAG:
+    def __init__(self):
+        self.adj_list = defaultdict(set)      # parent -> set of children
+        self.reverse_adj = defaultdict(set)   # child -> set of parents
+
+    def add_dep(self, dep):
+        self.adj_list[dep]  # Ensures the node is in the graph
+        self.reverse_adj[dep]
+
+    def add_edge(self, parent, child):
+        self.add_dep(parent)
+        self.add_dep(child)
+        self.adj_list[parent].add(child)
+        self.reverse_adj[child].add(parent)
+
+    def get_nodes(self):
+        return set(self.adj_list.keys())
+
+    def get_edges(self):
+        return [(src, dst) for src in self.adj_list for dst in self.adj_list[src]]
+
+    def get_children(self, dep):
+        return self.adj_list.get(dep, set())
+
+    def get_parents(self, dep):
+        return self.reverse_adj.get(dep, set())
+
+    def get_root_nodes(self):
+        # Nodes with no parents (in-degree = 0)
+        return {dep for dep in self.adj_list if not self.reverse_adj[dep]}
+
+    def detect_cycles(self):
+        visited = set()
+        rec_stack = set()
+
+        def dfs(node):
+            if node in rec_stack:
+                return True  # Cycle detected
+            if node in visited:
+                return False
+            visited.add(node)
+            rec_stack.add(node)
+            for neighbor in self.adj_list[node]:
+                if dfs(neighbor):
+                    return True
+            rec_stack.remove(node)
+            return False
+
+        return any(dfs(node) for node in self.adj_list)
+
+    def get_descendants(self, start_node, max_depth=1):
+        """Return all downstream nodes within max_depth from start_node"""
+        visited = set()
+        current_level = {start_node}
+
+        for _ in range(max_depth):
+            next_level = set()
+            for node in current_level:
+                for child in self.get_children(node):
+                    if child not in visited:
+                        visited.add(child)
+                        next_level.add(child)
+            current_level = next_level
+
+        return visited
+
+    def get_n_degree_dependencies_from_roots(self, max_depth=1):
+        """Return a mapping of root -> set of dependencies within max_depth"""
+        root_deps = {}
+        roots = self.get_root_nodes()
+
+        for root in roots:
+            deps = self.get_descendants(root, max_depth)
+            root_deps[root] = deps
+
+        return root_deps
