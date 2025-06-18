b6000c989a6e27156c97c8c45031310eb70c2e30
Alexey Kudinkin <ak@anyscale.com>
2025-06-18 10:24:07 -0700

    Fixing test
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/data/tests/test_parquet.py
+++ python/ray/data/tests/test_parquet.py
@@ -321,8 +321,8 @@ def test_parquet_read_meta_provider(ray_start_regular_shared, fs, data_path):
     #   - Passed to ReadParquet hold metadata matching actual bundle
     #   - Produced by ReadParquet reflects actual amount of bytes read
     assert read_stats.base_name == "ReadParquet"
-    assert read_stats.extra_metrics.average_bytes_inputs_per_task < 5_000
-    assert read_stats.extra_metrics.bytes_task_outputs_generated == expected_byte_size
+    assert read_stats.extra_metrics['average_bytes_inputs_per_task'] < 5_000
+    assert read_stats.extra_metrics['bytes_task_outputs_generated'] == expected_byte_size
 
     assert sorted(values) == expected_values
 
