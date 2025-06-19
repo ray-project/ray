8b962e01c19ca38a81d280cbe9329279b66dc859
Ricardo Decal <rdecal@anyscale.com>
2025-06-18 18:19:55 -0700

    typo
    Signed-off-by: Ricardo Decal <rdecal@anyscale.com>

--- doc/source/ray-overview/examples/e2e-timeseries/ci/run_tests.sh
+++ doc/source/ray-overview/examples/e2e-timeseries/ci/run_tests.sh
@@ -1,1 +1,1 @@
-jupyter execute e2e_timeseries/01-Distribute-Training.ipynb e2e_timeseries/02-Validation.ipynb e2e_timeseries/03-Serving.ipynb
+jupyter execute e2e_timeseries/01-Distributed-Training.ipynb e2e_timeseries/02-Validation.ipynb e2e_timeseries/03-Serving.ipynb
