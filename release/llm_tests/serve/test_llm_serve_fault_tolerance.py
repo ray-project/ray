7c1a5f091e0fa0590e454ce0cf778aa60eb09ca2
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2025-06-18 19:48:26 -0700

    wip
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- release/llm_tests/serve/test_llm_serve_fault_tolerance.py
+++ release/llm_tests/serve/test_llm_serve_fault_tolerance.py
@@ -1,10 +1,10 @@
 import time
-from typing import Literal, List
+from typing import Literal, List, Generator
 
 import pytest
 import ray
 from ray import serve
-from ray.serve.llm import LLMConfig, build_openai_app, ModelLoadingConfig
+from ray.serve.llm import LLMConfig, ModelLoadingConfig, build_llm_deployment
 
 MODEL_ID = "Qwen/Qwen2.5-0.5B-Instruct"
 RAY_MODEL_ID = "qwen-0.5b"
@@ -20,7 +20,7 @@ def get_llm_config(
             model_source=MODEL_ID,
         ),
         deployment_config=dict(
-            name="llm_deployment",
+            name="test",
             num_replicas=2,
         ),
         engine_kwargs=dict(
@@ -48,11 +48,10 @@ def kill_replica(replica_id: str) -> None:
 @pytest.fixture(name="app", scope="function")
 def start_ray_serve(
     tensor_parallel_size: int = 1,
-) -> str:
+) -> Generator:
     """Start Ray Serve with specified parallelism parameters."""
-    ray_url = "http://localhost:8000"
     llm_config: LLMConfig = get_llm_config(tensor_parallel_size)
-    app = build_openai_app(dict(llm_configs=[llm_config]))
+    app = build_llm_deployment(llm_config, name_prefix="LLM:")
     serve.run(app, blocking=False)
     yield app
     serve.shutdown()
@@ -62,8 +61,8 @@ def wait_for_deployment_status(
     deployment_name: str, status: Literal["HEALTHY", "UNHEALTHY"], timeout_s: int = 120
 ) -> None:
     s = time.time()
-    print(f"Waiting for deployment {deployment_name} to become {status}")
     while time.time() - s < timeout_s:
+        print(f"Waiting for deployment {deployment_name} to become {status}")
         state = serve.status()
         if state.applications["default"].deployments[deployment_name].status == status:
             return
@@ -76,21 +75,20 @@ def wait_for_deployment_status(
 
 def test_recovery_from_replica_failure(app) -> None:
     """Tests that the deployment recovers from replica failure."""
-
-    start_ray_serve(tensor_parallel_size=1)
-    wait_for_deployment_status("llm_deployment", "HEALTHY", timeout_s=60)
+    dname = "LLM:test"
+    wait_for_deployment_status(dname, "HEALTHY", timeout_s=60)
 
     # Kill both replicas
-    replica_ids = find_replica_ids("llm_deployment")
+    replica_ids = find_replica_ids(dname)
     for replica_id in replica_ids:
         print(f"Killing replica {replica_id}")
         kill_replica(replica_id)
 
     # wait for deployment to get unhealthy
-    wait_for_deployment_status("llm_deployment", "UNHEALTHY", timeout_s=60)
+    wait_for_deployment_status(dname, "UNHEALTHY", timeout_s=60)
 
     # Wait again for deployment to get healthy
-    wait_for_deployment_status("llm_deployment", "HEALTHY", timeout_s=60)
+    wait_for_deployment_status(dname, "HEALTHY", timeout_s=60)
 
 
 if __name__ == "__main__":
