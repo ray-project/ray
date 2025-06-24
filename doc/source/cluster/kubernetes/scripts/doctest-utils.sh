943ff7365b7c0c62f95906ac6d679dadcc2def3a
Chi-Sheng Liu <chishengliu@chishengliu.com>
2025-06-24 23:00:36 +0800

    Disable color output for jq
    Signed-off-by: Chi-Sheng Liu <chishengliu@chishengliu.com>

--- doc/source/cluster/kubernetes/scripts/doctest-utils.sh
+++ doc/source/cluster/kubernetes/scripts/doctest-utils.sh
@@ -2,16 +2,7 @@
 
 KUBERAY_VERSION="1.4.0"
 
-script_name=$(basename "$0")
-
-if [ "$#" -eq 0 ]; then
-  echo "Usage: ${script_name} <command>" >&2
-  exit 1
-fi
-
-kuberay_version() {
-  echo "${KUBERAY_VERSION}"
-}
+alias jq='jq --monochrome-output'
 
 install_kuberay_operator() {
   if ! helm repo list | grep -q '^kuberay\s'; then
@@ -21,16 +12,3 @@ install_kuberay_operator() {
   helm install kuberay-operator kuberay/kuberay-operator --version "${KUBERAY_VERSION}"
   kubectl wait --for=condition=available deployment/kuberay-operator --timeout=120s
 }
-
-case "$1" in
-  kuberay_version)
-    kuberay_version
-    ;;
-  install_kuberay_operator)
-    install_kuberay_operator
-    ;;
-  *)
-    echo "Error: No such command: '$1'" >&2
-    exit 1
-    ;;
-esac
