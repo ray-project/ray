b6542314a070b97200ad75afdb36e94fdd90d1a0
goutamvenkat-anyscale <goutam@anyscale.com>
2025-06-26 15:25:31 -0700

    Update python/ray/data/_internal/datasource/parquet_datasink.py
    Co-authored-by: Alexey Kudinkin <alexey.kudinkin@gmail.com>
    Signed-off-by: goutamvenkat-anyscale <goutam@anyscale.com>

--- python/ray/data/_internal/datasource/parquet_datasink.py
+++ python/ray/data/_internal/datasource/parquet_datasink.py
@@ -160,9 +160,9 @@ class ParquetDatasink(_FileDatasink):
         else:
             # Has extension but not templatized, add template while preserving extension
             logger.warning(
-                "FilenameProvider have to providing proper filename base template including '{{i}}' "
+                "FilenameProvider have to provide proper filename template including '{{i}}' "
                 "macro to ensure unique filenames when writing multiple files. Appending '{{i}}' "
-                "macros to the end of the file. For more details on the expected filename template checkout "
+                "macro to the end of the file. For more details on the expected filename template checkout "
                 "PyArrow's `write_to_dataset` API"
             )
             # Use pathlib.Path to properly handle filenames with dots
