8f0b4e7aee2139bb796581d1e674e91138969d9a
Jun-Hao Wan <ken89@kimo.com>
2025-06-27 02:05:02 +0800

    [Doc][KubeRay] Convert rayjob-quick-start.ipynb back to markdown docs (#54093)
    Signed-off-by: win5923 <ken89@kimo.com>
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/k8s/run-kuberay-doc-tests.sh
+++ ci/k8s/run-kuberay-doc-tests.sh
@@ -63,7 +63,6 @@ echo "--- Run doc tests"
 cd doc/source/cluster/kubernetes
 TESTS=(
   "getting-started/raycluster-quick-start.ipynb"
-  "getting-started/rayjob-quick-start.ipynb"
   "getting-started/rayservice-quick-start.ipynb"
   "user-guides/kuberay-gcs-ft.ipynb"
 )
