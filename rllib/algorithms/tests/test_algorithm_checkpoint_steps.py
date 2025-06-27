117cbe08ebc0a84581779406959aae9e7c5ba3e4
Daraan <github.blurry@9ox.net>
2025-06-27 10:26:51 +0200

    Correct count after step 3
    @microcheck //rllib:algorithms/tests/test_algorithm_checkpoint_steps
    
    Signed-off-by: Daraan <github.blurry@9ox.net>

--- rllib/algorithms/tests/test_algorithm_checkpoint_steps.py
+++ rllib/algorithms/tests/test_algorithm_checkpoint_steps.py
@@ -18,27 +18,33 @@ class TestAlgorithmCheckpointStepsAfterRestore(unittest.TestCase):
         ray.shutdown()
 
     def test_correct_steps_after_restore(self):
+        ENV_STEPS_PER_ITERATION = 10
         config = PPOConfig()
         config.training(
-            train_batch_size_per_learner=100, num_epochs=2, minibatch_size=50
+            train_batch_size_per_learner=ENV_STEPS_PER_ITERATION,
+            num_epochs=1,
+            minibatch_size=ENV_STEPS_PER_ITERATION,
         )
-        config.debugging(
-            seed=11,
-            log_sys_usage=False,
-        )
+        config.debugging(log_sys_usage=False)
         config.reporting(metrics_num_episodes_for_smoothing=1)
         config.environment(env="CartPole-v1")
-        algo0 = config.env_runners(
+        algo_0_runner = config.env_runners(
             num_env_runners=0,
         ).build_algo()
-        algo1 = config.env_runners(
+        algo_1_runner = config.env_runners(
             num_env_runners=1,
         ).build_algo()
-        assert algo0.config and algo1.config
-        self.assertEqual(algo0.config.num_env_runners, 0)
-        self.assertEqual(algo1.config.num_env_runners, 1)
-        _result0a = algo0.step()  # Step 1
-        _result1a = algo1.step()  # Step 1
+        assert (
+            algo_0_runner.config
+            and algo_1_runner.config
+            and algo_0_runner.metrics
+            and algo_1_runner.metrics
+        )
+        self.assertEqual(algo_0_runner.config.num_env_runners, 0)
+        self.assertEqual(algo_1_runner.config.num_env_runners, 1)
+        # --- Step 1 ---
+        _result_0 = algo_0_runner.step()
+        _result_1 = algo_1_runner.step()
         with (
             tempfile.TemporaryDirectory(prefix=".ckpt_a0_") as checkpoint_0_step1,
             tempfile.TemporaryDirectory(prefix=".ckpt_a1_") as checkpoint_1_step1,
@@ -52,107 +58,151 @@ class TestAlgorithmCheckpointStepsAfterRestore(unittest.TestCase):
             ) as checkpoint_1_step2_restored,
         ):
             # Save Step 1
-            algo0.save_checkpoint(checkpoint_0_step1)
-            algo1.save_checkpoint(checkpoint_1_step1)
-            # Step 2
-            result0_step2 = algo0.step()
-            result1_step2 = algo1.step()
+            algo_0_runner.save_checkpoint(checkpoint_0_step1)
+            algo_1_runner.save_checkpoint(checkpoint_1_step1)
+            # --- Step 2 ---
+            result_algo_0_step2 = algo_0_runner.step()
+            result_algo_1_step2 = algo_1_runner.step()
             self.assertEqual(
-                result0_step2[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
-                result1_step2[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
+                result_algo_0_step2[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
+                result_algo_1_step2[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
             )
             self.assertEqual(
-                result0_step2[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
-                200,
+                result_algo_0_step2[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
+                ENV_STEPS_PER_ITERATION * 2,
             )
             # Save Step 2
-            algo0.save_checkpoint(checkpoint_0_step2)
-            algo1.save_checkpoint(checkpoint_1_step2)
-            # Step 3
-            result0_step3 = algo0.step()
-            result1_step3 = algo1.step()
+            algo_0_runner.save_checkpoint(checkpoint_0_step2)
+            algo_1_runner.save_checkpoint(checkpoint_1_step2)
             self.assertEqual(
-                result0_step3[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
-                result1_step3[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
+                algo_0_runner.metrics.peek(
+                    (ENV_RUNNER_RESULTS, NUM_ENV_STEPS_SAMPLED_LIFETIME)
+                ),
+                ENV_STEPS_PER_ITERATION * 2,
             )
             self.assertEqual(
-                result0_step3[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
-                300,
+                algo_1_runner.metrics.peek(
+                    (ENV_RUNNER_RESULTS, NUM_ENV_STEPS_SAMPLED_LIFETIME)
+                ),
+                ENV_STEPS_PER_ITERATION * 2,
+            )
+
+            # --- Step 3 ---
+            result_algo_0_step3 = algo_0_runner.step()
+            result_algo_1_step3 = algo_1_runner.step()
+            self.assertEqual(
+                result_algo_0_step3[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
+                result_algo_1_step3[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
+            )
+            self.assertEqual(
+                result_algo_0_step3[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
+                ENV_STEPS_PER_ITERATION * 3,
             )
             # Load Step 1
-            algo0_restored = PPO.from_checkpoint(checkpoint_0_step1)
-            algo1_restored = PPO.from_checkpoint(checkpoint_1_step1)
-            assert algo0_restored.metrics and algo1_restored.metrics
+            algo_0_runner_restored = PPO.from_checkpoint(checkpoint_0_step1)
+            algo_1_runner_restored = PPO.from_checkpoint(checkpoint_1_step1)
+            assert algo_0_runner_restored.metrics and algo_1_runner_restored.metrics
             # Check loaded metric
             self.assertEqual(
-                algo0_restored.metrics.peek(
+                algo_0_runner_restored.metrics.peek(
                     (ENV_RUNNER_RESULTS, NUM_ENV_STEPS_SAMPLED_LIFETIME)
                 ),
-                100,
+                ENV_STEPS_PER_ITERATION,
             )
             self.assertEqual(
-                algo0_restored.metrics.peek(
+                algo_0_runner_restored.metrics.peek(
                     (ENV_RUNNER_RESULTS, NUM_ENV_STEPS_SAMPLED_LIFETIME)
                 ),
-                algo1_restored.metrics.peek(
+                algo_1_runner_restored.metrics.peek(
                     (ENV_RUNNER_RESULTS, NUM_ENV_STEPS_SAMPLED_LIFETIME)
                 ),
             )
-            tree.assert_same_structure(algo0_restored.metrics, algo1_restored.metrics)
-            # Step 2 from restored & checkpoint
-            result_algo0_step2_restored = algo0_restored.step()
-            result_algo1_step2_restored = algo1_restored.step()
-            algo0.save_checkpoint(checkpoint_0_step2_restored)
-            algo1.save_checkpoint(checkpoint_1_step2_restored)
+            tree.assert_same_structure(
+                algo_0_runner_restored.metrics, algo_1_runner_restored.metrics
+            )
+
+            # --- Step 2 from restored & checkpoint ---
+            result_algo0_step2_restored = algo_0_runner_restored.step()
+            result_algo1_step2_restored = algo_1_runner_restored.step()
+            algo_0_runner_restored.save_checkpoint(checkpoint_0_step2_restored)
+            algo_1_runner_restored.save_checkpoint(checkpoint_1_step2_restored)
 
             with self.subTest("From checkpoint: env_runners=0 - Step 2"):
                 self.assertEqual(
-                    result0_step2[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
+                    result_algo_0_step2[ENV_RUNNER_RESULTS][
+                        NUM_ENV_STEPS_SAMPLED_LIFETIME
+                    ],
                     result_algo0_step2_restored[ENV_RUNNER_RESULTS][
                         NUM_ENV_STEPS_SAMPLED_LIFETIME
                     ],
                 )
             with self.subTest("From checkpoint: env_runners=1 - Step 2"):
                 self.assertEqual(
-                    result1_step2[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
+                    result_algo_1_step2[ENV_RUNNER_RESULTS][
+                        NUM_ENV_STEPS_SAMPLED_LIFETIME
+                    ],
                     result_algo1_step2_restored[ENV_RUNNER_RESULTS][
                         NUM_ENV_STEPS_SAMPLED_LIFETIME
                     ],
                 )
-
+            # Test after restoring a second time
             # Load Restored from step 2
-            algo0_restored_x2 = PPO.from_checkpoint(checkpoint_0_step2_restored)
-            algo1_restored_x2 = PPO.from_checkpoint(checkpoint_1_step2_restored)
+            algo_0_restored_x2 = PPO.from_checkpoint(checkpoint_0_step2_restored)
+            algo_1_restored_x2 = PPO.from_checkpoint(checkpoint_1_step2_restored)
+            assert algo_0_restored_x2.metrics and algo_1_restored_x2.metrics
+            self.assertEqual(
+                algo_0_restored_x2.metrics.peek(
+                    (ENV_RUNNER_RESULTS, NUM_ENV_STEPS_SAMPLED_LIFETIME)
+                ),
+                ENV_STEPS_PER_ITERATION * 2,
+                "metrics not loaded as expected",
+            )
+            self.assertEqual(
+                algo_1_restored_x2.metrics.peek(
+                    (ENV_RUNNER_RESULTS, NUM_ENV_STEPS_SAMPLED_LIFETIME)
+                ),
+                ENV_STEPS_PER_ITERATION * 2,
+                "metrics not loaded as expected",
+            )
             # Step 3 from restored
-            result_algo0_step3_restored = algo0_restored.step()
-            result_algo1_step3_restored = algo1_restored.step()
-            result_algo0_step3_restored_x2 = algo0_restored_x2.step()
-            result_algo1_step3_restored_x2 = algo1_restored_x2.step()
+            result_algo0_step3_restored = algo_0_runner_restored.step()
+            result_algo1_step3_restored = algo_1_runner_restored.step()
+            result_algo0_step3_restored_x2 = algo_0_restored_x2.step()
+            result_algo1_step3_restored_x2 = algo_1_restored_x2.step()
 
+            # Test that all results after step 3 are have 300 steps
             with self.subTest("From checkpoint: env_runners=0 - Step 3"):
                 self.assertEqual(
-                    result0_step3[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
+                    result_algo_0_step3[ENV_RUNNER_RESULTS][
+                        NUM_ENV_STEPS_SAMPLED_LIFETIME
+                    ],
                     result_algo0_step3_restored[ENV_RUNNER_RESULTS][
                         NUM_ENV_STEPS_SAMPLED_LIFETIME
                     ],
                 )
             with self.subTest("From checkpoint: env_runners=0 - Step 3 (restored x2)"):
                 self.assertEqual(
-                    result0_step3[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
+                    result_algo_0_step3[ENV_RUNNER_RESULTS][
+                        NUM_ENV_STEPS_SAMPLED_LIFETIME
+                    ],
                     result_algo0_step3_restored_x2[ENV_RUNNER_RESULTS][
                         NUM_ENV_STEPS_SAMPLED_LIFETIME
                     ],
                 )
             with self.subTest("From checkpoint: env_runners=1 - Step 3"):
                 self.assertEqual(
-                    result1_step3[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
+                    result_algo_1_step3[ENV_RUNNER_RESULTS][
+                        NUM_ENV_STEPS_SAMPLED_LIFETIME
+                    ],
                     result_algo1_step3_restored[ENV_RUNNER_RESULTS][
                         NUM_ENV_STEPS_SAMPLED_LIFETIME
                     ],
                 )
             with self.subTest("From checkpoint: env_runners=1 - Step 3 (restored x2)"):
                 self.assertEqual(
-                    result1_step3[ENV_RUNNER_RESULTS][NUM_ENV_STEPS_SAMPLED_LIFETIME],
+                    result_algo_1_step3[ENV_RUNNER_RESULTS][
+                        NUM_ENV_STEPS_SAMPLED_LIFETIME
+                    ],
                     result_algo1_step3_restored_x2[ENV_RUNNER_RESULTS][
                         NUM_ENV_STEPS_SAMPLED_LIFETIME
                     ],
