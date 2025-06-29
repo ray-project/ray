b169f3184e397d2d84b7819c5acd9562580c033f
harshit <harshit@anyscale.com>
2025-06-29 06:02:26 +0000

    fix PR checks

--- python/ray/_common/tests/test_collector.py
+++ python/ray/_common/tests/test_collector.py
@@ -5,7 +5,7 @@ from ray._common.test_utils import Collector
 def test_collector_class():
     collector = Collector.remote()
 
-    random_items = ["this", "is", 1 , "demo", "string"]
+    random_items = ["this", "is", 1, "demo", "string"]
 
     for item in random_items:
         collector.add.remote(item)
@@ -16,3 +16,9 @@ def test_collector_class():
 
     for i in range(0, len(result)):
         assert result[i] == random_items[i]
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(pytest.main(["-v", __file__]))
