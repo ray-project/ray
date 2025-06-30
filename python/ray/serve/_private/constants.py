4db13513cd0e6b9fcd7036182df02608c7cb2271
Cindy Zhang <cindyzyx9@gmail.com>
2025-06-30 14:36:04 -0700

    fix default
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- python/ray/serve/_private/constants.py
+++ python/ray/serve/_private/constants.py
@@ -458,5 +458,5 @@ REQUEST_ROUTING_STATS_METHOD = "record_routing_stats"
 
 # Feature flag to run user code in a separate event loop.
 RAY_SERVE_RUN_USER_CODE_IN_SEPARATE_THREAD = (
-    os.environ.get("RAY_SERVE_RUN_USER_CODE_IN_SEPARATE_THREAD", "0") == "1"
+    os.environ.get("RAY_SERVE_RUN_USER_CODE_IN_SEPARATE_THREAD", "1") == "1"
 )
