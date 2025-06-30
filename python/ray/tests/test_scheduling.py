8eaca8e546b2ccabfcd2c3d9173a86d532297b1d
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-06-30 11:36:05 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/tests/test_scheduling.py
+++ python/ray/tests/test_scheduling.py
@@ -428,12 +428,8 @@ def test_locality_aware_leasing_borrowed_objects(ray_start_cluster):
         ),
     ).remote()
 
-    # Ensure the owner has object info prior to scheduling the task so the object info
-    # will be inlined to the borrower.
-    ray.wait([worker_node_ref], fetch_local=False)
-
     # Run a borrower task on the head node. From within the borrower task, we launch
-    # another task. That inner task should run on the worker node based on locality.
+    # another task. The inner task should run on the worker node based on locality.
     assert (
         ray.get(
             borrower.options(
