ray-project/ray

commit 79c7c181f36e63035e82d883c1af2f7f04873fc9

[dask-on-ray] Add multiple return DataFrame shuffle optimization. (#13951)

python/ray/util/dask/__init__.py
@@ -4,11 +4,16 @@ from .callbacks import (
     local_ray_callbacks,
     unpack_ray_callbacks,
 )
+from .optimizations import dataframe_optimize
 
 __all__ = [
+    # Schedulers
     "ray_dask_get",
     "ray_dask_get_sync",
+    # Callbacks
     "RayDaskCallback",
     "local_ray_callbacks",
     "unpack_ray_callbacks",
+    # Optimizations
+    "dataframe_optimize",
 ]

python/ray/util/dask/optimizations.py
@@ -1,0 +1,160 @@
+import operator
+import warnings
+
+import dask
+from dask import core
+from dask.core import istask
+from dask.dataframe.core import _concat
+from dask.dataframe.optimize import optimize
+from dask.dataframe.shuffle import shuffle_group
+from dask.highlevelgraph import HighLevelGraph
+
+from .scheduler import MultipleReturnFunc, multiple_return_get
+
+try:
+    from dask.dataframe.shuffle import SimpleShuffleLayer
+except ImportError:
+    # SimpleShuffleLayer doesn't exist in this version of Dask.
+    SimpleShuffleLayer = None
+
+if SimpleShuffleLayer is not None:
+
+    class MultipleReturnSimpleShuffleLayer(SimpleShuffleLayer):
+        @classmethod
+        def clone(cls, layer: SimpleShuffleLayer):
+            # TODO(Clark): Probably don't need this since SimpleShuffleLayer
+            # implements __copy__() and the shallow clone should be enough?
+            return cls(
+                name=layer.name,
+                column=layer.column,
+                npartitions=layer.npartitions,
+                npartitions_input=layer.npartitions_input,
+                ignore_index=layer.ignore_index,
+                name_input=layer.name_input,
+                meta_input=layer.meta_input,
+                parts_out=layer.parts_out,
+                annotations=layer.annotations,
+            )
+
+        def __repr__(self):
+            return (f"MultipleReturnSimpleShuffleLayer<name='{self.name}', "
+                    f"npartitions={self.npartitions}>")
+
+        def __reduce__(self):
+            attrs = [
+                "name",
+                "column",
+                "npartitions",
+                "npartitions_input",
+                "ignore_index",
+                "name_input",
+                "meta_input",
+                "parts_out",
+                "annotations",
+            ]
+            return (MultipleReturnSimpleShuffleLayer,
+                    tuple(getattr(self, attr) for attr in attrs))
+
+        def _cull(self, parts_out):
+            return MultipleReturnSimpleShuffleLayer(
+                self.name,
+                self.column,
+                self.npartitions,
+                self.npartitions_input,
+                self.ignore_index,
+                self.name_input,
+                self.meta_input,
+                parts_out=parts_out,
+            )
+
+        def _construct_graph(self):
+            """Construct graph for a simple shuffle operation."""
+
+            shuffle_group_name = "group-" + self.name
+            shuffle_split_name = "split-" + self.name
+
+            dsk = {}
+            n_parts_out = len(self.parts_out)
+            for part_out in self.parts_out:
+                # TODO(Clark): Find better pattern than in-scheduler concat.
+                _concat_list = [(shuffle_split_name, part_out, part_in)
+                                for part_in in range(self.npartitions_input)]
+                dsk[(self.name, part_out)] = (_concat, _concat_list,
+                                              self.ignore_index)
+                for _, _part_out, _part_in in _concat_list:
+                    dsk[(shuffle_split_name, _part_out, _part_in)] = (
+                        multiple_return_get,
+                        (shuffle_group_name, _part_in),
+                        _part_out,
+                    )
+                    if (shuffle_group_name, _part_in) not in dsk:
+                        dsk[(shuffle_group_name, _part_in)] = (
+                            MultipleReturnFunc(
+                                shuffle_group,
+                                n_parts_out,
+                            ),
+                            (self.name_input, _part_in),
+                            self.column,
+                            0,
+                            self.npartitions,
+                            self.npartitions,
+                            self.ignore_index,
+                            self.npartitions,
+                        )
+
+            return dsk
+
+    def rewrite_simple_shuffle_layer(dsk, keys):
+        if not isinstance(dsk, HighLevelGraph):
+            dsk = HighLevelGraph.from_collections(
+                id(dsk), dsk, dependencies=())
+        else:
+            dsk = dsk.copy()
+
+        layers = dsk.layers.copy()
+        for key, layer in layers.items():
+            if type(layer) is SimpleShuffleLayer:
+                dsk.layers[key] = MultipleReturnSimpleShuffleLayer.clone(layer)
+        return dsk
+
+    def dataframe_optimize(dsk, keys, **kwargs):
+        if not isinstance(keys, (list, set)):
+            keys = [keys]
+        keys = list(core.flatten(keys))
+
+        if not isinstance(dsk, HighLevelGraph):
+            dsk = HighLevelGraph.from_collections(
+                id(dsk), dsk, dependencies=())
+
+        dsk = rewrite_simple_shuffle_layer(dsk, keys=keys)
+        return optimize(dsk, keys, **kwargs)
+else:
+
+    def dataframe_optimize(dsk, keys, **kwargs):
+        warnings.warn("Custom dataframe shuffle optimization only works on "
+                      "dask>=2020.12.0, you are on version "
+                      f"{dask.__version__}, please upgrade Dask."
+                      "Falling back to default dataframe optimizer.")
+        return optimize(dsk, keys, **kwargs)
+
+
+# Stale approaches below.
+
+
+def fuse_splits_into_multiple_return(dsk, keys):
+    if not isinstance(dsk, HighLevelGraph):
+        dsk = HighLevelGraph.from_collections(id(dsk), dsk, dependencies=())
+    else:
+        dsk = dsk.copy()
+    dependencies = dsk.dependencies.copy()
+    for k, v in dsk.items():
+        if istask(v) and v[0] == shuffle_group:
+            task_deps = dependencies[k]
+            # Only rewrite shuffle group split if all downstream dependencies
+            # are splits.
+            if all(
+                    istask(dsk[dep]) and dsk[dep][0] == operator.getitem
+                    for dep in task_deps):
+                for dep in task_deps:
+                    # Rewrite split
+                    pass

python/ray/util/dask/scheduler.py
@@ -1,6 +1,7 @@
 import atexit
 from collections import defaultdict
 from multiprocessing.pool import ThreadPool
+from dataclasses import dataclass
 import threading
 
 import ray
@@ -270,19 +271,31 @@ def _rayify_task(
                     return alternate_return
 
         func, args = task[0], task[1:]
+        if func is multiple_return_get:
+            return _execute_task(task, deps)
         # If the function's arguments contain nested object references, we must
         # unpack said object references into a flat set of arguments so that
         # Ray properly tracks the object dependencies between Ray tasks.
-        object_refs, repack = unpack_object_refs(args, deps)
+        arg_object_refs, repack = unpack_object_refs(args, deps)
         # Submit the task using a wrapper function.
-        object_ref = dask_task_wrapper.options(name=f"dask:{key!s}").remote(
-            func, repack, key, ray_pretask_cbs, ray_posttask_cbs, *object_refs)
+        object_refs = dask_task_wrapper.options(
+            name=f"dask:{key!s}",
+            num_returns=(1 if not isinstance(func, MultipleReturnFunc) else
+                         func.num_returns),
+        ).remote(
+            func,
+            repack,
+            key,
+            ray_pretask_cbs,
+            ray_posttask_cbs,
+            *arg_object_refs,
+        )
 
         if ray_postsubmit_cbs is not None:
             for cb in ray_postsubmit_cbs:
-                cb(task, key, deps, object_ref)
+                cb(task, key, deps, object_refs)
 
-        return object_ref
+        return object_refs
     elif not ishashable(task):
         return task
     elif task in deps:
@@ -434,3 +447,16 @@ def ray_dask_get_sync(dsk, keys, **kwargs):
                 cb(result)
 
         return result
+
+
+@dataclass
+class MultipleReturnFunc:
+    func: callable
+    num_returns: int
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+
+def multiple_return_get(multiple_returns, idx):
+    return multiple_returns[idx]

commit 6adb7c49d7f7a3f95a6489d99b3b015d4766654e

[data] adapt dask on ray to the new dask task class  (#54108)
"Dask on Ray" (DoR) is broken in dask==2024.11.0 or later as reported in
https://github.com/ray-project/ray/issues/48689 because Dask removed a
private function in https://github.com/dask/dask/pull/11378 that DoR has
been relying on. Not only https://github.com/dask/dask/pull/11378, Dask
has migrated their task data structure to a new format (the high-level
motivation is described in https://github.com/dask/dask/issues/9969).
Since this migration spans across a series of changes between 2024.11.0 and
2025.1.0, it's not realistic to copy what's been removed and paste them
in Ray.

This change adapts Dask on Ray to the change to keep its functionality.
The change is compatible only with
`dask>=2024.11.0,<2025.1.0` because Dask made another major change in
2025.1.0, breaking the shuffle optimization introduced in
https://github.com/ray-project/ray/pull/13951 

Signed-off-by: Lonnie Liu <lonnie@anyscale.com>
Co-authored-by: Hiromu Hota <hiromu.hota@gmail.com>

.buildkite/core.rayci.yml
@@ -181,9 +181,9 @@ steps:
       - bazel run //ci/ray_ci:test_in_docker --
         python/ray/util/dask/... core
         --install-mask all-ray-libraries
-        --build-name datalbuild
+        --build-name databuild-py3.12
     depends_on:
-      - datalbuild
+      - databuild-multipy
       - forge
 
   - label: ":ray: core: modin tests"

.buildkite/data.rayci.yml
@@ -152,9 +152,9 @@ steps:
     instance_type: medium
     commands:
       - bazel run //ci/ray_ci:test_in_docker -- //python/ray/data/... data
-        --build-name datalbuild
+        --build-name databuild-py3.12
         --only-tags dask
-    depends_on: datalbuild
+    depends_on: databuild-multipy
 
   - label: ":database: data: TFRecords (tfx-bsl) tests"
     tags:
@@ -193,10 +193,10 @@ steps:
     instance_type: medium
     commands:
       - bazel run //ci/ray_ci:test_in_docker -- //doc/... data
-        --build-name datalbuild
+        --build-name databuild-py3.12
         --only-tags dask
         --parallelism-per-worker 2
-    depends_on: datalbuild
+    depends_on: databuild-multipy
 
   - label: ":database: data: doc gpu tests"
     tags:

doc/source/ray-more-libs/doc_code/dask_on_ray_persist_example.py
@@ -12,10 +12,12 @@ ray.init()
 enable_dask_on_ray()
 
 d_arr = da.ones(100)
-print(dask.base.collections_to_dsk([d_arr]))
-# {('ones-c345e6f8436ff9bcd68ddf25287d27f3',
-#   0): (functools.partial(<function _broadcast_trick_inner at 0x7f27f1a71f80>,
-#   dtype=dtype('float64')), (5,))}
+
+# Print the internal Dask graph. Replace this with `print(dask.base.collections_to_dsk([d_arr]))` when dask>=2024.11.0,<2025.4.0.
+print(dask.base.collections_to_expr([d_arr]).dask)
+# {('ones_like-5902a58f37d3b639948dee893f5c4f4a', 0):
+# <Task ('ones_like-5902a58f37d3b639948dee893f5c4f4a', 0)
+# ones_like(...)>}
 
 # This submits all underlying Ray tasks to the cluster and returns
 # a Dask array with the Ray futures inlined.
@@ -23,9 +25,10 @@ d_arr_p = d_arr.persist()
 
 # Notice that the Ray ObjectRef is inlined. The dask.ones() task has
 # been submitted to and is running on the Ray cluster.
-dask.base.collections_to_dsk([d_arr_p])
-# {('ones-c345e6f8436ff9bcd68ddf25287d27f3',
-#   0): ObjectRef(8b4e50dc1ddac855ffffffffffffffffffffffff0100000001000000)}
+# Replace this in a similar way when dask>=2024.11.0,<2025.4.0.
+print(dask.base.collections_to_expr([d_arr_p]).dask)
+# {('ones_like-5902a58f37d3b639948dee893f5c4f4a', 0):
+# DataNode(ObjectRef(2c329aa28fcae64affffffffffffffffffffffff2c00000001000000))}
 
 # Future computations on this persisted Dask Array will be fast since we
 # already started computing d_arr_p in the background.

python/ray/util/dask/__init__.py
@@ -1,4 +1,16 @@
 import dask
+from packaging.version import Version
+
+# Version(dask.__version__) becomes "0" during doc builds.
+if Version(dask.__version__) != Version("0") and Version(dask.__version__) < Version(
+    "2024.11.0"
+):
+    # Dask on Ray doesn't work if Dask version is less than 2024.11.0.
+    raise ImportError(
+        "Dask on Ray requires Dask version 2024.11.0 or later. "
+        "Please upgrade your Dask installation."
+    )
+
 from .scheduler import (
     ray_dask_get,
     ray_dask_get_sync,

python/ray/util/dask/common.py
@@ -5,7 +5,7 @@ import uuid
 
 import ray
 
-from dask.base import quote
+from dask.core import quote
 from dask.core import get as get_sync
 from dask.utils import apply
 

python/ray/util/dask/optimizations.py
@@ -1,21 +1,27 @@
-import operator
 import warnings
 
 import dask
 from dask import core
-from dask.core import istask
 from dask.dataframe.core import _concat
-from dask.dataframe.optimize import optimize
-from dask.dataframe.shuffle import shuffle_group
 from dask.highlevelgraph import HighLevelGraph
 
 from .scheduler import MultipleReturnFunc, multiple_return_get
 
 try:
     from dask.dataframe.shuffle import SimpleShuffleLayer
+    from dask.dataframe.optimize import optimize
+    from dask.dataframe.shuffle import shuffle_group
 except ImportError:
     # SimpleShuffleLayer doesn't exist in this version of Dask.
+    # This is the case for dask>=2025.1.0.
     SimpleShuffleLayer = None
+try:
+    import dask_expr  # noqa: F401
+
+    SimpleShuffleLayer = None
+except ImportError:
+    pass
+
 
 if SimpleShuffleLayer is not None:
 
@@ -137,31 +143,8 @@ else:
     def dataframe_optimize(dsk, keys, **kwargs):
         warnings.warn(
             "Custom dataframe shuffle optimization only works on "
-            "dask>=2020.12.0, you are on version "
-            f"{dask.__version__}, please upgrade Dask."
-            "Falling back to default dataframe optimizer."
+            "dask>=2024.11.0,<2025.1.0, you are on version "
+            f"{dask.__version__}."
+            "Doing no additional optimization aside from the default one."
         )
-        return optimize(dsk, keys, **kwargs)
-
-
-# Stale approaches below.
-
-
-def fuse_splits_into_multiple_return(dsk, keys):
-    if not isinstance(dsk, HighLevelGraph):
-        dsk = HighLevelGraph.from_collections(id(dsk), dsk, dependencies=())
-    else:
-        dsk = dsk.copy()
-    dependencies = dsk.dependencies.copy()
-    for k, v in dsk.items():
-        if istask(v) and v[0] == shuffle_group:
-            task_deps = dependencies[k]
-            # Only rewrite shuffle group split if all downstream dependencies
-            # are splits.
-            if all(
-                istask(dsk[dep]) and dsk[dep][0] == operator.getitem
-                for dep in task_deps
-            ):
-                for dep in task_deps:
-                    # Rewrite split
-                    pass
+        return None

python/ray/util/dask/scheduler.py
@@ -1,15 +1,26 @@
+import warnings
+
 import atexit
 import threading
 import time
 from collections import defaultdict
 from collections import OrderedDict
+from collections.abc import Mapping
 from dataclasses import dataclass
 from multiprocessing.pool import ThreadPool
 from pprint import pprint
 from typing import Optional
 
 import dask
-from dask.core import istask, ishashable, _execute_task
+from dask.core import istask, ishashable
+
+try:
+    from dask._task_spec import Task, Alias, DataNode, TaskRef, convert_legacy_graph
+except ImportError:
+    warnings.warn(
+        "Dask on Ray is available only on dask>=2024.11.0, "
+        f"you are on version {dask.__version__}."
+    )
 from dask.system import CPU_COUNT
 from dask.threaded import pack_exception, _thread_get_id
 
@@ -147,13 +158,18 @@ def ray_dask_get(dsk, keys, **kwargs):
     if "resources" in kwargs:
         raise ValueError(TOP_LEVEL_RESOURCES_ERR_MSG)
     ray_remote_args = kwargs.pop("ray_remote_args", {})
-    try:
-        annotations = dask.config.get("annotations")
-    except KeyError:
-        annotations = {}
+    annotations = dask.get_annotations()
     if "resources" in annotations:
         raise ValueError(TOP_LEVEL_RESOURCES_ERR_MSG)
 
+    # Take out the dask graph if it is an Expr for dask>=2025.4.0.
+    if not isinstance(dsk, Mapping):
+        if hasattr(dsk, "_optimized_dsk"):
+            # For Expr with this property
+            dsk = dsk._optimized_dsk
+        else:
+            # For any other Expr
+            dsk = dsk.__dask_graph__()
     scoped_ray_remote_args = _build_key_scoped_ray_remote_args(
         dsk, annotations, ray_remote_args
     )
@@ -168,6 +184,8 @@ def ray_dask_get(dsk, keys, **kwargs):
             ray_postsubmit_all_cbs,
             ray_finish_cbs,
         ) = unpack_ray_callbacks(ray_callbacks)
+        # Make sure the graph is in the new format
+        dsk = convert_legacy_graph(dsk)
         # NOTE: We hijack Dask's `get_async` function, injecting a different
         # task executor.
         object_refs = get_async(
@@ -362,13 +380,23 @@ def _rayify_task(
                 if alternate_return is not None:
                     return alternate_return
 
-        func, args = task[0], task[1:]
-        if func is multiple_return_get:
-            return _execute_task(task, deps)
+        if isinstance(task, Alias):
+            target = task.target
+            if isinstance(target, TaskRef):
+                # for 2024.12.0
+                return deps[target.key]
+            else:
+                # for 2024.12.1+
+                return deps[target]
+        elif isinstance(task, Task):
+            func = task.func
+        else:
+            raise ValueError("Invalid task type: %s" % type(task))
+
         # If the function's arguments contain nested object references, we must
         # unpack said object references into a flat set of arguments so that
         # Ray properly tracks the object dependencies between Ray tasks.
-        arg_object_refs, repack = unpack_object_refs(args, deps)
+        arg_object_refs, repack = unpack_object_refs(deps)
         # Submit the task using a wrapper function.
         object_refs = dask_task_wrapper.options(
             name=f"dask:{key!s}",
@@ -377,7 +405,7 @@ def _rayify_task(
             ),
             **ray_remote_args,
         ).remote(
-            func,
+            task,
             repack,
             key,
             ray_pretask_cbs,
@@ -399,23 +427,23 @@ def _rayify_task(
 
 
 @ray.remote
-def dask_task_wrapper(func, repack, key, ray_pretask_cbs, ray_posttask_cbs, *args):
+def dask_task_wrapper(
+    task, repack, key, ray_pretask_cbs, ray_posttask_cbs, *arg_object_refs
+):
     """
     A Ray remote function acting as a Dask task wrapper. This function will
-    repackage the given flat `args` into its original data structures using
-    `repack`, execute any Dask subtasks within the repackaged arguments
-    (inlined by Dask's optimization pass), and then pass the concrete task
-    arguments to the provide Dask task function, `func`.
+    repackage the given `arg_object_refs` into its original `deps` using
+    `repack`, and then pass it to the provided Dask Task object , `task`.
 
     Args:
-        func: The Dask task function to execute.
+        task: The Dask Task class object to execute.
         repack: A function that repackages the provided args into
             the original (possibly nested) Python objects.
         key: The Dask key for this task.
         ray_pretask_cbs: Pre-task execution callbacks.
         ray_posttask_cbs: Post-task execution callback.
-        *args (ObjectRef): Ray object references representing the Dask task's
-            arguments.
+        *arg_object_refs (ObjectRef): Ray object references representing the dependencies'
+            results.
 
     Returns:
         The output of the Dask task. In the context of Ray, a
@@ -424,13 +452,31 @@ def dask_task_wrapper(func, repack, key, ray_pretask_cbs, ray_posttask_cbs, *arg
     """
     if ray_pretask_cbs is not None:
         pre_states = [
-            cb(key, args) if cb is not None else None for cb in ray_pretask_cbs
+            cb(key, arg_object_refs) if cb is not None else None
+            for cb in ray_pretask_cbs
         ]
-    repacked_args, repacked_deps = repack(args)
-    # Recursively execute Dask-inlined tasks.
-    actual_args = [_execute_task(a, repacked_deps) for a in repacked_args]
-    # Execute the actual underlying Dask task.
-    result = func(*actual_args)
+    (repacked_deps,) = repack(arg_object_refs)
+    # De-reference the potentially nested arguments recursively.
+    def _dereference_args(x):
+        if isinstance(x, Task):
+            x.args = _dereference_args(x.args)
+            return x
+        elif isinstance(x, Mapping):
+            return {k: _dereference_args(v) for k, v in x.items()}
+        elif isinstance(x, tuple):
+            return tuple(_dereference_args(x) for x in x)
+        elif isinstance(x, ray.ObjectRef):
+            return ray.get(x)
+        elif isinstance(x, DataNode):
+            if isinstance(x.value, ray.ObjectRef):
+                value = ray.get(x.value)
+                return DataNode(key=x.key, value=value)
+            return x
+        else:
+            return x
+
+    task = _dereference_args(task)
+    result = task(repacked_deps)
 
     if ray_posttask_cbs is not None:
         for cb, pre_state in zip(ray_posttask_cbs, pre_states):
@@ -548,6 +594,8 @@ def ray_dask_get_sync(dsk, keys, **kwargs):
             ray_postsubmit_all_cbs,
             ray_finish_cbs,
         ) = unpack_ray_callbacks(ray_callbacks)
+        # Make sure the graph is in the new format
+        dsk = convert_legacy_graph(dsk)
         # NOTE: We hijack Dask's `get_async` function, injecting a different
         # task executor.
         object_refs = get_async(

python/ray/util/dask/scheduler_utils.py
@@ -4,11 +4,21 @@ The following is adapted from Dask release 2021.03.1:
 """
 
 import os
+import warnings
 from queue import Queue, Empty
 
+import dask
 from dask import config
+
+try:
+    from dask._task_spec import DataNode, DependenciesMapping
+except ImportError:
+    warnings.warn(
+        "Dask on Ray is available only on dask>=2024.11.0, "
+        f"you are on version {dask.__version__}."
+    )
 from dask.callbacks import local_callbacks, unpack_callbacks
-from dask.core import _execute_task, flatten, get_dependencies, has_tasks, reverse_dict
+from dask.core import flatten, get_dependencies, reverse_dict
 from dask.order import order
 
 if os.name == "nt":
@@ -56,17 +66,18 @@ def start_state_from_dask(dsk, cache=None, sortkey=None):
         cache = config.get("cache", None)
     if cache is None:
         cache = dict()
+
     data_keys = set()
     for k, v in dsk.items():
-        if not has_tasks(dsk, v):
-            cache[k] = v
+        if isinstance(v, DataNode):
+            cache[k] = v()
             data_keys.add(k)
 
     dsk2 = dsk.copy()
     dsk2.update(cache)
 
-    dependencies = {k: get_dependencies(dsk2, k) for k in dsk}
-    waiting = {k: v.copy() for k, v in dependencies.items() if k not in data_keys}
+    dependencies = DependenciesMapping(dsk)
+    waiting = {k: set(v) for k, v in dependencies.items() if k not in data_keys}
 
     dependents = reverse_dict(dependencies)
     for a in cache:
@@ -102,7 +113,7 @@ def execute_task(key, task_info, dumps, loads, get_id, pack_exception):
     """
     try:
         task, data = loads(task_info)
-        result = _execute_task(task, data)
+        result = task(data)
         id = get_id()
         result = dumps((result, id))
         failed = False
@@ -220,7 +231,7 @@ def get_async(
     callbacks=None,
     dumps=identity,
     loads=identity,
-    **kwargs
+    **kwargs,
 ):
     """Asynchronous get function
     This is a general version of various asynchronous schedulers for dask.  It
@@ -339,7 +350,7 @@ def get_async(
                             for dep in get_dependencies(dsk, key)
                         }
                         task = dsk[key]
-                        _execute_task(task, data)  # Re-execute locally
+                        task(data)  # Re-execute locally
                     else:
                         raise_exception(exc, tb)
                 res, worker_id = loads(res_info)

python/ray/data/tests/test_ecosystem_dask.py
@@ -54,6 +54,12 @@ def test_to_dask_simple(ray_start_regular_shared):
 
 @pytest.mark.parametrize("ds_format", ["pandas", "arrow"])
 def test_to_dask(ray_start_regular_shared, ds_format):
+    # Since 2023.7.1, Dask DataFrame automatically converts text data using object data types to string[pyarrow]
+    # For the purpose of this test, we need to disable this behavior.
+    import dask
+
+    dask.config.set({"dataframe.convert-string": False})
+
     from ray.util.dask import ray_dask_get
 
     df1 = pd.DataFrame({"one": [1, 2, 3], "two": ["a", "b", "c"]})

python/ray/util/dask/tests/test_dask_callback.py
@@ -8,10 +8,6 @@ import ray
 from ray.tests.conftest import *  # noqa: F403, F401
 from ray.util.dask import ray_dask_get, RayDaskCallback
 
-pytestmark = pytest.mark.skipif(
-    sys.version_info >= (3, 12), reason="Skip dask tests for Python version 3.12+"
-)
-
 
 @dask.delayed
 def add(x, y):

python/ray/util/dask/tests/test_dask_multi_node.py
@@ -2,14 +2,13 @@ import sys
 
 import dask
 import pytest
+import dask.dataframe as dd
+import numpy as np
+import pandas as pd
 
 import ray
 from ray.tests.conftest import *  # noqa: F403, F401
 from ray.util.dask import enable_dask_on_ray
-
-pytestmark = pytest.mark.skipif(
-    sys.version_info >= (3, 12), reason="Skip dask tests for Python version 3.12+"
-)
 
 
 @pytest.fixture
@@ -66,6 +65,26 @@ def test_ray_dask_resources(ray_start_cluster, ray_enable_dask_on_ray):
     with pytest.raises(ValueError):
         c = dask.delayed(get_node_id)
         result = c().compute(resources={"pin": 0.01})
+
+    def get_node_id(row):
+        return pd.Series(ray._private.worker.global_worker.node.unique_id)
+
+    # Test annotations on compute.
+    df = dd.from_pandas(
+        pd.DataFrame(np.random.randint(0, 2, size=(2, 2)), columns=["age", "grade"]),
+        npartitions=2,
+    )
+    c = df.apply(get_node_id, axis=1, meta={0: str})
+    with dask.annotate(ray_remote_args=dict(num_gpus=1, resources={"pin": 0.01})):
+        result = c.compute(optimize_graph=False)
+    assert result[0].iloc[0] == pinned_node.unique_id
+
+    # Test compute global Ray remote args.
+    c = df.apply(get_node_id, axis=1, meta={0: str})
+    result = c.compute(
+        ray_remote_args={"resources": {"pin": 0.01}}, optimize_graph=False
+    )
+    assert result[0].iloc[0] == pinned_node.unique_id
 
 
 if __name__ == "__main__":

python/ray/util/dask/tests/test_dask_optimization.py
@@ -2,21 +2,33 @@ import sys
 
 import dask
 import dask.dataframe as dd
-from dask.dataframe.shuffle import SimpleShuffleLayer
 from unittest import mock
 import numpy as np
 import pandas as pd
 import pytest
+from packaging.version import Version
 
 from ray.tests.conftest import *  # noqa
 from ray.util.dask import dataframe_optimize
-from ray.util.dask.optimizations import (
-    rewrite_simple_shuffle_layer,
-    MultipleReturnSimpleShuffleLayer,
-)
+
+try:
+    import dask_expr  # noqa: F401
+
+    DASK_EXPR_INSTALLED = True
+except ImportError:
+    DASK_EXPR_INSTALLED = False
+    pass
+
+if Version(dask.__version__) < Version("2025.1") and not DASK_EXPR_INSTALLED:
+    from dask.dataframe.shuffle import SimpleShuffleLayer
+    from ray.util.dask.optimizations import (
+        rewrite_simple_shuffle_layer,
+        MultipleReturnSimpleShuffleLayer,
+    )
 
 pytestmark = pytest.mark.skipif(
-    sys.version_info >= (3, 12), reason="Skip dask tests for Python version 3.12+"
+    Version(dask.__version__) >= Version("2025.1") or DASK_EXPR_INSTALLED,
+    reason="Skip dask tests for Dask 2025.1+",
 )
 
 

python/ray/util/dask/tests/test_dask_scheduler.py
@@ -13,10 +13,6 @@ from ray.util.client.common import ClientObjectRef
 from ray.util.dask import disable_dask_on_ray, enable_dask_on_ray, ray_dask_get
 from ray.util.dask.callbacks import ProgressBarCallback
 
-pytestmark = pytest.mark.skipif(
-    sys.version_info >= (3, 12), reason="Skip dask tests for Python version 3.12+"
-)
-
 
 @pytest.fixture
 def ray_enable_dask_on_ray():
@@ -90,10 +86,10 @@ def test_sort_with_progress_bar(ray_start_regular_shared):
     sorted_without_pb = None
     with ProgressBarCallback():
         sorted_with_pb = df.set_index(
-            ["age"], shuffle="tasks", max_branch=npartitions
+            ["age"], shuffle_method="tasks", max_branch=npartitions
         ).compute(scheduler=ray_dask_get, _ray_enable_progress_bar=True)
     sorted_without_pb = df.set_index(
-        ["age"], shuffle="tasks", max_branch=npartitions
+        ["age"], shuffle_method="tasks", max_branch=npartitions
     ).compute(scheduler=ray_dask_get)
     assert sorted_with_pb.equals(sorted_without_pb)
 