59f6eaf4f49fa984e5bda42de43e06abcfcb42ab
dayshah <dhyey2019@gmail.com>
2025-07-03 01:06:24 -0700

    i have sinned
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- src/ray/gcs/gcs_client/accessor.cc
+++ src/ray/gcs/gcs_client/accessor.cc
@@ -326,6 +326,22 @@ Status ActorInfoAccessor::AsyncRestartActorForLineageReconstruction(
   return Status::OK();
 }
 
+namespace {
+
+// TODO(dayshah): Yes this is temporary.
+Status ComputeGcsStatus(const Status &grpc_status, const rpc::GcsStatus &gcs_status) {
+  // If gRPC status is ok return the GCS status, otherwise return the gRPC status.
+  if (grpc_status.ok()) {
+    return gcs_status.code() == static_cast<int>(StatusCode::OK)
+               ? Status::OK()
+               : Status(StatusCode(gcs_status.code()), gcs_status.message());
+  } else {
+    return grpc_status;
+  }
+}
+
+}  // namespace
+
 Status ActorInfoAccessor::AsyncRegisterActor(const ray::TaskSpecification &task_spec,
                                              const ray::gcs::StatusCallback &callback,
                                              int64_t timeout_ms) {
@@ -335,10 +351,7 @@ Status ActorInfoAccessor::AsyncRegisterActor(const ray::TaskSpecification &task_
   client_impl_->GetGcsRpcClient().RegisterActor(
       request,
       [callback](const Status &status, rpc::RegisterActorReply &&reply) {
-        // If gRPC status is ok return the GCS status, otherwise return the gRPC status.
-        callback(status.ok()
-                     ? Status(StatusCode(reply.status().code()), reply.status().message())
-                     : status);
+        callback(ComputeGcsStatus(status, reply.status()));
       },
       timeout_ms);
   return Status::OK();
@@ -351,9 +364,7 @@ Status ActorInfoAccessor::SyncRegisterActor(const ray::TaskSpecification &task_s
   request.mutable_task_spec()->CopyFrom(task_spec.GetMessage());
   auto status = client_impl_->GetGcsRpcClient().SyncRegisterActor(
       request, &reply, GetGcsTimeoutMs());
-  // If gRPC status is ok return the GCS status, otherwise return the gRPC status.
-  return status.ok() ? Status(StatusCode(reply.status().code()), reply.status().message())
-                     : status;
+  return ComputeGcsStatus(status, reply.status());
 }
 
 Status ActorInfoAccessor::AsyncKillActor(const ActorID &actor_id,
