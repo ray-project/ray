88ee02ce3f20f5aa013fc6a5827bf9599ba4c16a
Timothy Seah <tseah@anyscale.com>
2025-07-03 15:46:10 -0700

    raise importerror
    Signed-off-by: Timothy Seah <tseah@anyscale.com>

--- python/ray/train/collective/__init__.py
+++ python/ray/train/collective/__init__.py
@@ -10,5 +10,10 @@ if is_v2_enabled():
 
     broadcast_from_rank_zero.__module__ = "ray.train.collective"
     barrier.__module__ = "ray.train.collective"
+else:
+    raise ImportError(
+        "`ray.train.collective` is only available in Ray Train v2. "
+        "To enable it, please set `RAY_TRAIN_V2_ENABLED=1`."
+    )
 
 # DO NOT ADD ANYTHING AFTER THIS LINE.
