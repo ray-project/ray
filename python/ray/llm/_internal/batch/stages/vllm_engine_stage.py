e9e221c4c975cdeca2ec9dfc69311340bd032408
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2025-07-06 15:26:33 -0700

    wip
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- python/ray/llm/_internal/batch/stages/vllm_engine_stage.py
+++ python/ray/llm/_internal/batch/stages/vllm_engine_stage.py
@@ -475,7 +475,6 @@ class vLLMEngineStageUDF(StatefulStageUDF):
             model=self.model,
             model_source=model_source,
             idx_in_batch_column=self.IDX_IN_BATCH_COLUMN,
-            disable_log_stats=False,
             disable_log_requests=True,
             max_pending_requests=self.max_pending_requests,
             dynamic_lora_loading_path=dynamic_lora_loading_path,
