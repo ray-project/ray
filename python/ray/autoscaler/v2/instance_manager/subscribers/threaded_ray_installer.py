c381d5d7e9bc458b08a5daf9218678665bcfd9b0
Rueian <rueian@anyscale.com>
2025-07-07 14:17:44 -0700

    [core][autoscaler] make the autoscaler v2 work with the cluster launcher (#54230)
    ## Why are these changes needed?
    
    This PR wires up the `NodeProviderAdapter` and `ThreadedRayInstaller` to
    make autoscaler v2 work with the cluster launcher.
    
    An autoscaler v2 enabled cluster can be launched by the following
    command:
    
    ```sh
    RAY_enable_autoscaler_v2=1 ray up aws.yaml -y
    ```
    
    and this `aws.yaml`:
    
    ```yaml
    cluster_name: default
    max_workers: 2
    upscaling_speed: 1.0
    docker:
        image: "rayproject/ray:nightly-py312"
        container_name: "ray_container"
        pull_before_run: True
        run_options:
            - --ulimit nofile=65536:65536
    idle_timeout_minutes: 5
    provider:
        type: aws
        region: us-west-2
        availability_zone: us-west-2a,us-west-2b
        cache_stopped_nodes: True
    auth:
        ssh_user: ubuntu
    available_node_types:
        ray.head.default:
            resources: {}
            node_config:
                InstanceType: m5.large
                ImageId: ami-0387d929287ab193e
                BlockDeviceMappings:
                    - DeviceName: /dev/sda1
                      Ebs:
                          VolumeSize: 140
                          VolumeType: gp3
        ray.worker.default:
            min_workers: 1
            max_workers: 2
            resources: {}
            node_config:
                InstanceType: m5.large
                ImageId: ami-0387d929287ab193e
                InstanceMarketOptions:
                    MarketType: spot
    head_node_type: ray.head.default
    
    file_mounts: {
        "/home/ray/anaconda3/lib/python3.12/site-packages/ray/autoscaler/v2": "python/ray/autoscaler/v2",
    }
    
    cluster_synced_files: []
    file_mounts_sync_continuously: False
    rsync_exclude:
        - "**/.git"
        - "**/.git/**"
    rsync_filter:
        - ".gitignore"
    initialization_commands: []
    setup_commands: []
    head_setup_commands: []
    worker_setup_commands: []
    head_start_ray_commands:
        - ray stop
        - ray start --head --port=6379 --object-manager-port=8076 --autoscaling-config=~/ray_bootstrap_config.yaml --dashboard-host=0.0.0.0
    worker_start_ray_commands:
        - ray stop
        - ray start --address=$RAY_HEAD_IP:6379 --object-manager-port=8076
    ```
    
    The equivalent setup for gcp is also tested.
    
    Note that the `image: "rayproject/ray:nightly-py312"` and `file_mounts`
    are **required**, and the `python/ray/autoscaler/v2` local path should
    point to this PR since we need to patch the autoscaler in the docker
    image.
    
    ## Summary of Changes
    
    * Make `ray up` accept the `RAY_enable_autoscaler_v2` env.
    * Enable the `ThreadedRayInstaller` in the autoscaler v2 if the node
    provider is a `NodeProviderAdapter`.
    * Fix the `ThreadedRayInstaller` for not conflict with the autoscaler
    v2.
    
    ## Checks
    
    - [x] I've signed off every commit(by using the -s flag, i.e., `git
    commit -s`) in this PR.
    - [x] I've run `scripts/format.sh` to lint the changes in this PR.
    - [ ] I've included any doc changes needed for
    https://docs.ray.io/en/master/.
    - [ ] I've added any new APIs to the API Reference. For example, if I
    added a
    method in Tune, I've added it in `doc/source/tune/api/` under the
               corresponding `.rst` file.
    - [ ] I've made sure the tests are passing. Note that there might be a
    few flaky tests, see the recent failures at https://flakey-tests.ray.io/
    - Testing Strategy
       - [x] Unit tests
       - [ ] Release tests
       - [ ] This PR is not tested :(
    
    ---------
    
    Signed-off-by: Rueian <rueian@anyscale.com>
    Signed-off-by: Rueian <rueiancsie@gmail.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- python/ray/autoscaler/v2/instance_manager/subscribers/threaded_ray_installer.py
+++ python/ray/autoscaler/v2/instance_manager/subscribers/threaded_ray_installer.py
@@ -1,16 +1,30 @@
+import dataclasses
 import logging
 import time
 from concurrent.futures import ThreadPoolExecutor
 from typing import List
+from queue import Queue
 
 from ray.autoscaler.v2.instance_manager.instance_manager import (
     InstanceUpdatedSubscriber,
 )
 from ray.autoscaler.v2.instance_manager.instance_storage import InstanceStorage
 from ray.autoscaler.v2.instance_manager.ray_installer import RayInstaller
-from ray.core.generated.instance_manager_pb2 import Instance, InstanceUpdateEvent
+from ray.core.generated.instance_manager_pb2 import (
+    NodeKind,
+    Instance,
+    InstanceUpdateEvent,
+)
 
 logger = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass(frozen=True)
+class RayInstallError:
+    # Instance manager's instance id.
+    im_instance_id: str
+    # Error details.
+    details: str
 
 
 class ThreadedRayInstaller(InstanceUpdatedSubscriber):
@@ -21,6 +35,7 @@ class ThreadedRayInstaller(InstanceUpdatedSubscriber):
         head_node_ip: str,
         instance_storage: InstanceStorage,
         ray_installer: RayInstaller,
+        error_queue: Queue,
         max_install_attempts: int = 3,
         install_retry_interval: int = 10,
         max_concurrent_installs: int = 50,
@@ -31,65 +46,50 @@ class ThreadedRayInstaller(InstanceUpdatedSubscriber):
         self._max_concurrent_installs = max_concurrent_installs
         self._max_install_attempts = max_install_attempts
         self._install_retry_interval = install_retry_interval
+        self._error_queue = error_queue
         self._ray_installation_executor = ThreadPoolExecutor(
             max_workers=self._max_concurrent_installs
         )
 
     def notify(self, events: List[InstanceUpdateEvent]) -> None:
         for event in events:
-            if event.new_instance_status == Instance.ALLOCATED:
+            if event.new_instance_status == Instance.RAY_INSTALLING:
                 self._install_ray_on_new_nodes(event.instance_id)
 
     def _install_ray_on_new_nodes(self, instance_id: str) -> None:
         allocated_instance, _ = self._instance_storage.get_instances(
             instance_ids={instance_id},
-            status_filter={Instance.ALLOCATED},
+            status_filter={Instance.RAY_INSTALLING},
         )
         for instance in allocated_instance.values():
+            assert instance.node_kind == NodeKind.WORKER
             self._ray_installation_executor.submit(
                 self._install_ray_on_single_node, instance
             )
 
     def _install_ray_on_single_node(self, instance: Instance) -> None:
-        assert instance.status == Instance.ALLOCATED
-        success, version = self._instance_storage.upsert_instance(
-            instance, expected_instance_version=instance.version
-        )
-        if not success:
-            logger.warning(
-                f"Failed to update instance {instance.instance_id} to RAY_INSTALLING"
-            )
-            # Do not need to handle failures, it will be covered by
-            # garbage collection.
-            return
+        assert instance.status == Instance.RAY_INSTALLING
 
         # install with exponential backoff
-        installed = False
         backoff_factor = 1
+        last_exception = None
         for _ in range(self._max_install_attempts):
-            installed = self._ray_installer.install_ray(instance, self._head_node_ip)
-            if installed:
-                break
+            try:
+                self._ray_installer.install_ray(instance, self._head_node_ip)
+                return
+            except Exception as e:
+                logger.info(
+                    f"Ray installation failed on instance {instance.cloud_instance_id}: {e}"
+                )
+                last_exception = e
+
             logger.warning("Failed to install ray, retrying...")
             time.sleep(self._install_retry_interval * backoff_factor)
             backoff_factor *= 2
 
-        if not installed:
-            instance.status = Instance.RAY_INSTALL_FAILED
-            success, version = self._instance_storage.upsert_instance(
-                instance,
-                expected_instance_version=version,
+        self._error_queue.put_nowait(
+            RayInstallError(
+                im_instance_id=instance.instance_id,
+                details=str(last_exception),
             )
-        else:
-            instance.status = Instance.RAY_RUNNING
-            success, version = self._instance_storage.upsert_instance(
-                instance,
-                expected_instance_version=version,
-            )
-        if not success:
-            logger.warning(
-                f"Failed to update instance {instance.instance_id} to {instance.status}"
-            )
-            # Do not need to handle failures, it will be covered by
-            # garbage collection.
-            return
+        )
