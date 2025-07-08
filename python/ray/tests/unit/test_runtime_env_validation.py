152fea69c8c14960139dfca3a1659916f1ac8e46
Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>
2025-07-08 10:36:02 +0100

    [core][runtime env] unit-test validating pip_install_options with pip
    Signed-off-by: Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>

--- python/ray/tests/unit/test_runtime_env_validation.py
+++ python/ray/tests/unit/test_runtime_env_validation.py
@@ -500,6 +500,21 @@ class TestValidatePip:
         assert not result["pip_check"]
         assert "pip_version" not in result
 
+    def test_validate_pip_install_options(self):
+        opts = [
+            "--no-cache-dir",
+            "--no-build-isolation",
+            "--disable-pip-version-check"
+        ]
+        result = validation.parse_and_validate_pip({
+            "packages": ["pkg1", "ray", "pkg2"],
+            "pip_install_options": list(opts),
+        })
+        assert result["packages"] == ["pkg1", "ray", "pkg2"]
+        assert not result["pip_check"]
+        assert "pip_version" not in result
+        assert result["pip_install_options"] == opts
+
 
 class TestValidateEnvVars:
     def test_type_validation(self):
