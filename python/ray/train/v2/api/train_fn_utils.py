0fe317ee4feec53509898b27dd450a01773d868c
Timothy Seah <timothy.seah777@yahoo.com>
2025-07-08 13:57:47 -0700

    Update python/ray/train/v2/api/train_fn_utils.py
    Co-authored-by: Justin Yu <justinvyu@anyscale.com>
    Signed-off-by: Timothy Seah <timothy.seah777@yahoo.com>

--- python/ray/train/v2/api/train_fn_utils.py
+++ python/ray/train/v2/api/train_fn_utils.py
@@ -81,9 +81,10 @@ def report(
     Args:
         metrics: The metrics you want to report.
         checkpoint: The optional checkpoint you want to report.
-        checkpoint_dir_name: The optional directory to store the checkpoint.
-            If not set, the checkpoint will be stored in the default storage path.
-            If set, make sure this value is unique for each iteration.
+        checkpoint_dir_name: Custom name for the checkpoint directory. If not provided, a unique directory
+    name will be automatically generated. If provided, it must be unique
+    across all checkpoints to avoid naming collisions. Consider including identifiers
+    such as the epoch or batch index in the name.
     """
 
     get_train_context().report(
