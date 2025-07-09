d987896bcef1360ab1596b9289b8b410419de2f1
Cindy Zhang <cindyzyx9@gmail.com>
2025-07-09 13:51:32 -0700

    update comment
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- python/ray/serve/tests/test_deployment_scheduler.py
+++ python/ray/serve/tests/test_deployment_scheduler.py
@@ -207,7 +207,7 @@ class TestCompactScheduling:
     @pytest.mark.parametrize(
         "app_resources,expected_worker_nodes",
         [
-            # [2, 5, 3, 3, 7, 2, 6, 2] -> 3 nodes
+            # [2, 5, 3, 3, 7, 6, 4] -> 3 nodes
             ({5: 1, 3: 2, 7: 1, 2: 1, 6: 1, 4: 1}, 3),
             # [1, 7, 7, 3, 2] -> 2 nodes
             ({1: 1, 7: 2, 3: 1, 2: 1}, 2),
