d13e9020e4a9575b59089efebb352c02c53d9129
Philipp Moritz <pcmoritz@gmail.com>
2025-07-09 18:18:41 -0700

    update test

--- python/ray/tests/test_runtime_env_uv_run.py
+++ python/ray/tests/test_runtime_env_uv_run.py
@@ -323,15 +323,13 @@ def test_uv_run_runtime_env_hook(with_uv):
             "run",
             "--no-project",
             "--isolated",
-            "--extra",
-            "vllm",
             "-m",
             "ray._private.runtime_env.uv_runtime_env_hook",
             "--extra-args",
         ],
         runtime_env={},
         expected_output={
-            "py_executable": f"{uv} run --no-project --isolated --extra vllm",
+            "py_executable": f"{uv} run --no-project --isolated",
             "working_dir": os.getcwd(),
         },
     )
