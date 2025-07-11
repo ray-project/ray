9fda65338cbc981cffbfa643067c44cf35c3f426
doyoung <doyoung@anyscale.com>
2025-07-11 23:41:46 +0000

    format
    Signed-off-by: doyoung <doyoung@anyscale.com>

--- python/ray/serve/tests/test_api_2.py
+++ python/ray/serve/tests/test_api_2.py
@@ -16,8 +16,12 @@ def test_get_application_urls(serve_instance):
     node_ip = controller_details.node_ip
     assert get_application_urls(use_localhost=False) == [f"http://{node_ip}:8000"]
     assert get_application_urls("gRPC", use_localhost=False) == [f"{node_ip}:9000"]
-    assert get_application_urls(RequestProtocol.HTTP, use_localhost=False) == [f"http://{node_ip}:8000"]
-    assert get_application_urls(RequestProtocol.GRPC, use_localhost=False) == [f"{node_ip}:9000"]
+    assert get_application_urls(RequestProtocol.HTTP, use_localhost=False) == [
+        f"http://{node_ip}:8000"
+    ]
+    assert get_application_urls(RequestProtocol.GRPC, use_localhost=False) == [
+        f"{node_ip}:9000"
+    ]
 
 
 def test_get_application_urls_with_app_name(serve_instance):
@@ -28,8 +32,12 @@ def test_get_application_urls_with_app_name(serve_instance):
     serve.run(f.bind(), name="app1", route_prefix="/")
     controller_details = ray.get(serve_instance._controller.get_actor_details.remote())
     node_ip = controller_details.node_ip
-    assert get_application_urls("HTTP", app_name="app1", use_localhost=False) == [f"http://{node_ip}:8000"]
-    assert get_application_urls("gRPC", app_name="app1", use_localhost=False) == [f"{node_ip}:9000"]
+    assert get_application_urls("HTTP", app_name="app1", use_localhost=False) == [
+        f"http://{node_ip}:8000"
+    ]
+    assert get_application_urls("gRPC", app_name="app1", use_localhost=False) == [
+        f"{node_ip}:9000"
+    ]
 
 
 def test_get_application_urls_with_route_prefix(serve_instance):
@@ -43,7 +51,9 @@ def test_get_application_urls_with_route_prefix(serve_instance):
     assert get_application_urls("HTTP", app_name="app1", use_localhost=False) == [
         f"http://{node_ip}:8000/app1"
     ]
-    assert get_application_urls("gRPC", app_name="app1", use_localhost=False) == [f"{node_ip}:9000"]
+    assert get_application_urls("gRPC", app_name="app1", use_localhost=False) == [
+        f"{node_ip}:9000"
+    ]
 
 
 if __name__ == "__main__":
