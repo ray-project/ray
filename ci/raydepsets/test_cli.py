d9462acf1fe75b45888e549e5273cbb44aa3a77e
elliot-barn <elliot.barnwell@anyscale.com>
2025-07-14 21:46:50 +0000

    uncommenting unit tests
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/raydepsets/test_cli.py
+++ ci/raydepsets/test_cli.py
@@ -4,11 +4,12 @@ import unittest
 import tempfile
 import runfiles
 import shutil
-from ci.raydepsets.cli import load
+from ci.raydepsets.cli import load, DependencySetManager, Workspace
 from click.testing import CliRunner
 from pathlib import Path
 import os
 from ci.raydepsets.cli import uv_binary
+import subprocess
 
 _REPO_NAME = "com_github_ray_project_ray"
 _runfiles = runfiles.Create()
@@ -23,85 +24,85 @@ class TestCli(unittest.TestCase):
             if uv_dir not in current_path:
                 os.environ["PATH"] = f"{uv_dir}:{current_path}"
 
-    # def test_workspace_init(self):
-    #     with tempfile.TemporaryDirectory() as tmpdir:
-    #         workspace = Workspace(tmpdir)
-    #         assert workspace.dir is not None
+    def test_workspace_init(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            workspace = Workspace(tmpdir)
+            assert workspace.dir is not None
 
-    # def test_cli_load_fail_no_config(self):
-    #     result = CliRunner().invoke(
-    #         load,
-    #         [
-    #             "fake_path/test.config.yaml",
-    #             "--workspace-dir",
-    #             "/ci/raydepsets/test_data",
-    #         ],
-    #     )
-    #     assert result.exit_code == 1
-    #     assert isinstance(result.exception, FileNotFoundError)
-    #     assert "No such file or directory" in str(result.exception)
+    def test_cli_load_fail_no_config(self):
+        result = CliRunner().invoke(
+            load,
+            [
+                "fake_path/test.config.yaml",
+                "--workspace-dir",
+                "/ci/raydepsets/test_data",
+            ],
+        )
+        assert result.exit_code == 1
+        assert isinstance(result.exception, FileNotFoundError)
+        assert "No such file or directory" in str(result.exception)
 
-    # def test_dependency_set_manager_init(self):
-    #     with tempfile.TemporaryDirectory() as tmpdir:
-    #         _copy_data_to_tmpdir(tmpdir)
-    #         manager = DependencySetManager(
-    #             config_path="test.config.yaml",
-    #             workspace_dir=tmpdir,
-    #         )
-    #         assert manager is not None
-    #         assert manager.workspace.dir == tmpdir
-    #         assert manager.config.depsets[0].name == "ray_base_test_depset"
-    #         assert manager.config.depsets[0].operation == "compile"
-    #         assert manager.config.depsets[0].requirements == ["requirements_test.txt"]
-    #         assert manager.config.depsets[0].constraints == [
-    #             "requirement_constraints_test.txt"
-    #         ]
-    #         assert manager.config.depsets[0].output == "requirements_compiled.txt"
+    def test_dependency_set_manager_init(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            _copy_data_to_tmpdir(tmpdir)
+            manager = DependencySetManager(
+                config_path="test.config.yaml",
+                workspace_dir=tmpdir,
+            )
+            assert manager is not None
+            assert manager.workspace.dir == tmpdir
+            assert manager.config.depsets[0].name == "ray_base_test_depset"
+            assert manager.config.depsets[0].operation == "compile"
+            assert manager.config.depsets[0].requirements == ["requirements_test.txt"]
+            assert manager.config.depsets[0].constraints == [
+                "requirement_constraints_test.txt"
+            ]
+            assert manager.config.depsets[0].output == "requirements_compiled.txt"
 
-    # def test_dependency_set_manager_get_depset(self):
-    #     with tempfile.TemporaryDirectory() as tmpdir:
-    #         _copy_data_to_tmpdir(tmpdir)
-    #         manager = DependencySetManager(
-    #             config_path="test.config.yaml",
-    #             workspace_dir=tmpdir,
-    #         )
-    #         with self.assertRaises(KeyError):
-    #             manager.get_depset("fake_depset")
+    def test_dependency_set_manager_get_depset(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            _copy_data_to_tmpdir(tmpdir)
+            manager = DependencySetManager(
+                config_path="test.config.yaml",
+                workspace_dir=tmpdir,
+            )
+            with self.assertRaises(KeyError):
+                manager.get_depset("fake_depset")
 
-    # def test_uv_binary_exists(self):
-    #     assert uv_binary() is not None
+    def test_uv_binary_exists(self):
+        assert uv_binary() is not None
 
-    # def test_uv_version(self):
-    #     result = subprocess.run(
-    #         [uv_binary(), "--version"],
-    #         stdout=subprocess.PIPE,
-    #         stderr=subprocess.PIPE,
-    #     )
-    #     assert result.returncode == 0
-    #     assert "uv 0.7.20" in result.stdout.decode("utf-8")
-    #     assert result.stderr.decode("utf-8") == ""
+    def test_uv_version(self):
+        result = subprocess.run(
+            [uv_binary(), "--version"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        assert result.returncode == 0
+        assert "uv 0.7.20" in result.stdout.decode("utf-8")
+        assert result.stderr.decode("utf-8") == ""
 
-    # def test_compile_by_depset_name(self):
-    #     with tempfile.TemporaryDirectory() as tmpdir:
-    #         _copy_data_to_tmpdir(tmpdir)
-    #         result = CliRunner().invoke(
-    #             load,
-    #             [
-    #                 "test.config.yaml",
-    #                 "--workspace-dir",
-    #                 tmpdir,
-    #                 "--name",
-    #                 "ray_base_test_depset",
-    #             ],
-    #         )
+    def test_compile_by_depset_name(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            _copy_data_to_tmpdir(tmpdir)
+            result = CliRunner().invoke(
+                load,
+                [
+                    "test.config.yaml",
+                    "--workspace-dir",
+                    tmpdir,
+                    "--name",
+                    "ray_base_test_depset",
+                ],
+            )
 
-    #         output_fp = Path(tmpdir) / "requirements_compiled.txt"
-    #         assert result.exit_code == 0
-    #         assert Path(output_fp).is_file()
-    #         assert (
-    #             "Dependency set ray_base_test_depset compiled successfully"
-    #             in result.output
-    #         )
+            output_fp = Path(tmpdir) / "requirements_compiled.txt"
+            assert result.exit_code == 0
+            assert Path(output_fp).is_file()
+            assert (
+                "Dependency set ray_base_test_depset compiled successfully"
+                in result.output
+            )
 
     def test_subset_by_depset_name(self):
         with tempfile.TemporaryDirectory() as tmpdir:
@@ -151,4 +152,4 @@ def _copy_data_to_tmpdir(tmpdir):
 
 
 if __name__ == "__main__":
-    sys.exit(pytest.main(["-vv", __file__]))
+    sys.exit(pytest.main(["-v", __file__]))
