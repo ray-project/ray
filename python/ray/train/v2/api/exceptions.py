048388f8be5b1f284c44bad34f4f543594d81bed
xgui <xgui@anyscale.com>
2025-07-15 19:03:17 +0000

    add WorkerTrainingFailedError and SchedulingTrainingFailedError
    Signed-off-by: xgui <xgui@anyscale.com>

--- python/ray/train/v2/api/exceptions.py
+++ python/ray/train/v2/api/exceptions.py
@@ -6,12 +6,32 @@ from ray.util.annotations import PublicAPI
 
 @PublicAPI(stability="alpha")
 class TrainingFailedError(RayTrainError):
-    """Exception raised by `<Framework>Trainer.fit()` when training fails."""
+    """Base exception raised by `<Framework>Trainer.fit()` when training fails."""
+
+    def __init__(self, error_message: str):
+        super().__init__(error_message)
+        self._error_message = error_message
+
+
+@PublicAPI(stability="alpha")
+class WorkerTrainingFailedError(TrainingFailedError):
+    """Exception raised when training fails due to worker errors."""
 
     def __init__(self, error_message: str, worker_failures: Dict[int, Exception]):
         super().__init__("Training failed due to worker errors:\n" + error_message)
-        self._error_message = error_message
         self.worker_failures = worker_failures
 
     def __reduce__(self):
         return (self.__class__, (self._error_message, self.worker_failures))
+
+
+@PublicAPI(stability="alpha")
+class SchedulingTrainingFailedError(TrainingFailedError):
+    """Exception raised when training fails due to scheduling errors."""
+
+    def __init__(self, error_message: str, scheduling_error: Exception):
+        super().__init__("Training failed due to scheduling error:\n" + error_message)
+        self.scheduling_error = scheduling_error
+
+    def __reduce__(self):
+        return (self.__class__, (self._error_message, self.scheduling_error))
