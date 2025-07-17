189726d324e018f80986730e1dd45e6a6c273cb4
Mengjin Yan <mengjinyan3@gmail.com>
2025-07-16 23:16:13 -0700

    fix test failure
    Signed-off-by: Mengjin Yan <mengjinyan3@gmail.com>

--- src/ray/core_worker/task_event_buffer.cc
+++ src/ray/core_worker/task_event_buffer.cc
@@ -382,7 +382,8 @@ bool TaskEventBuffer::RecordTaskStatusEventIfNeeded(
 TaskEventBufferImpl::TaskEventBufferImpl(
     std::unique_ptr<gcs::GcsClient> gcs_client,
     std::unique_ptr<rpc::EventAggregatorClient> event_aggregator_client)
-    : periodical_runner_(PeriodicalRunner::Create(io_service_)),
+    : work_guard_(boost::asio::make_work_guard(io_service_)),
+      periodical_runner_(PeriodicalRunner::Create(io_service_)),
       gcs_client_(std::move(gcs_client)),
       event_aggregator_client_(std::move(event_aggregator_client)) {}
 
