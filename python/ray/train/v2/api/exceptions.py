d9aa3ba8aac52b3f5e3b1b38599ef88cb0224c17
xgui <xgui@anyscale.com>
2025-07-18 17:57:20 +0000

    resolve comments
    Signed-off-by: xgui <xgui@anyscale.com>

--- python/ray/train/v2/api/exceptions.py
+++ python/ray/train/v2/api/exceptions.py
@@ -6,19 +6,11 @@ from ray.util.annotations import PublicAPI
 
 @PublicAPI(stability="alpha")
 class TrainingFailedError(RayTrainError):
-    """Base exception raised by `<Framework>Trainer.fit()` when training fails."""
-
-    def __init__(self, error_message: str):
-        super().__init__(error_message)
-        self._error_message = error_message
-
-
-@PublicAPI(stability="alpha")
-class WorkerTrainingFailedError(TrainingFailedError):
-    """Exception raised when training fails due to worker errors."""
+    """Exception raised after the controller finishes and training starts."""
 
     def __init__(self, error_message: str, worker_failures: Dict[int, Exception]):
         super().__init__("Training failed due to worker errors:\n" + error_message)
+        self._error_message = error_message
         self.worker_failures = worker_failures
 
     def __reduce__(self):
@@ -26,12 +18,13 @@ class WorkerTrainingFailedError(TrainingFailedError):
 
 
 @PublicAPI(stability="alpha")
-class SchedulingTrainingFailedError(TrainingFailedError):
-    """Exception raised when training fails due to scheduling errors."""
+class ControllerError(RayTrainError):
+    """Exception raised when training fails due to controller errors."""
 
-    def __init__(self, error_message: str, scheduling_error: Exception):
-        super().__init__("Training failed due to scheduling error:\n" + error_message)
-        self.scheduling_error = scheduling_error
+    def __init__(self, error_message: str, controller_failure: Exception):
+        super().__init__("Training failed due to controller errors:\n" + error_message)
+        self._error_message = error_message
+        self.controller_failure = controller_failure
 
     def __reduce__(self):
-        return (self.__class__, (self._error_message, self.scheduling_error))
+        return (self.__class__, (self._error_message, self.controller_failure))
