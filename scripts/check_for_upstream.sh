68e957fdad633de4fd2301874064d42759164d1e
elliot-barn <elliot.barnwell@anyscale.com>
2025-07-22 20:13:50 +0000

    updating script
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- scripts/check_for_upstream.sh
+++ scripts/check_for_upstream.sh
@@ -3,12 +3,10 @@
 FILE=".UPSTREAM"
 
 check_for_upstream() {
-if [ -f "$FILE" ]; then
-  echo "Error: The file '$FILE' exists. You are attempting to push ray turbo to ray."
-  exit 1
-  fi
-
-  exit 0
+if find . -type f -name "$FILE" -quit | grep -q .; then
+    echo "Error: The file '$FILE' exists. You may be attempting to push ray turbo to ray."
+    exit 1
+fi
 }
 
 check_for_upstream "$@"
