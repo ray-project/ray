fb21193ee15b937df3733ebf06d0d4f844e1e63e
Mengjin Yan <mengjinyan3@gmail.com>
2025-07-21 23:08:14 -0700

    use free port in tests
    Signed-off-by: Mengjin Yan <mengjinyan3@gmail.com>

--- python/ray/tests/test_metrics_agent.py
+++ python/ray/tests/test_metrics_agent.py
@@ -524,13 +524,15 @@ def test_metrics_export_event_aggregator_agent(
             "ray_event_aggregator_agent_events_published_total": 1.0,
             "ray_event_aggregator_agent_events_filtered_out_total": 1.0,
         }
+        result = True
         for descriptor, expected_value in expected_metrics_values.items():
             samples = [m for m in metric_samples if m.name == descriptor]
+            print(f"samples: {samples}")
             if not samples:
-                return False
+                result = False
             if samples[0].value != expected_value:
-                return False
-        return True
+                result = False
+        return result
 
     wait_for_condition(test_case_stats_exist, timeout=30, retry_interval_ms=1000)
 
