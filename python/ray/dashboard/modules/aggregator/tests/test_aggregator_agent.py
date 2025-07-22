dcfee9f595711553131296d6ba5c0a6e753be228
Mengjin Yan <mengjinyan3@gmail.com>
2025-07-21 23:04:24 -0700

    merge the addr and port env var
    Signed-off-by: Mengjin Yan <mengjinyan3@gmail.com>

--- python/ray/dashboard/modules/aggregator/tests/test_aggregator_agent.py
+++ python/ray/dashboard/modules/aggregator/tests/test_aggregator_agent.py
@@ -35,11 +35,18 @@ from ray.dashboard.modules.aggregator.aggregator_agent import AggregatorAgent
 
 
 _EVENT_AGGREGATOR_AGENT_TARGET_PORT = find_free_port()
+_EVENT_AGGREGATOR_AGENT_TARGET_IP = "127.0.0.1"
+_EVENT_AGGREGATOR_AGENT_TARGET_ADDR = (
+    "http://"
+    + _EVENT_AGGREGATOR_AGENT_TARGET_IP
+    + ":"
+    + str(_EVENT_AGGREGATOR_AGENT_TARGET_PORT)
+)
 
 
 @pytest.fixture(scope="module")
 def httpserver_listen_address():
-    return ("127.0.0.1", _EVENT_AGGREGATOR_AGENT_TARGET_PORT)
+    return (_EVENT_AGGREGATOR_AGENT_TARGET_IP, _EVENT_AGGREGATOR_AGENT_TARGET_PORT)
 
 
 @pytest.fixture
@@ -57,10 +64,7 @@ _with_aggregator_port = pytest.mark.parametrize(
     [
         {
             "env_vars": {
-                "RAY_DASHBOARD_AGGREGATOR_AGENT_EVENT_EXPORT_PORT": str(
-                    _EVENT_AGGREGATOR_AGENT_TARGET_PORT
-                ),
-                "RAY_DASHBOARD_AGGREGATOR_AGENT_EVENT_EXPORT_ADDR": "http://127.0.0.1",
+                "RAY_DASHBOARD_AGGREGATOR_AGENT_EVENT_EXPORT_ADDR": _EVENT_AGGREGATOR_AGENT_TARGET_ADDR,
             },
         },
     ],
@@ -93,26 +97,21 @@ def get_event_aggregator_grpc_stub(webui_url, gcs_address, head_node_id):
 @pytest.mark.parametrize(
     (
         "export_addr",
-        "export_port",
         "expected_http_target_enabled",
         "expected_event_processing_enabled",
     ),
     [
-        ("", -1, False, False),
-        ("http://127.0.0.1", -1, False, False),
-        ("", _EVENT_AGGREGATOR_AGENT_TARGET_PORT, False, False),
-        ("http://127.0.0.1", _EVENT_AGGREGATOR_AGENT_TARGET_PORT, True, True),
+        ("", False, False),
+        ("http://127.0.0.1:" + str(_EVENT_AGGREGATOR_AGENT_TARGET_PORT), True, True),
     ],
 )
 def test_aggregator_agent_http_target_not_enabled(
     export_addr,
-    export_port,
     expected_http_target_enabled,
     expected_event_processing_enabled,
 ):
     dashboard_agent = MagicMock()
     dashboard_agent.events_export_addr = export_addr
-    dashboard_agent.events_export_port = export_port
     agent = AggregatorAgent(dashboard_agent)
     assert agent._event_http_target_enabled == expected_http_target_enabled
     assert agent._event_processing_enabled == expected_event_processing_enabled
@@ -168,14 +167,7 @@ def test_aggregator_agent_receive_publish_events_normally(
         {
             "env_vars": {
                 "RAY_DASHBOARD_AGGREGATOR_AGENT_MAX_EVENT_BUFFER_SIZE": 1,
-<<<<<<< HEAD
-                "RAY_DASHBOARD_AGGREGATOR_AGENT_EVENT_SEND_PORT": str(
-                    _EVENT_AGGREGATOR_AGENT_TARGET_PORT
-                ),
-=======
-                "RAY_DASHBOARD_AGGREGATOR_AGENT_EVENT_EXPORT_PORT": _EVENT_AGGREGATOR_AGENT_TARGET_PORT,
-                "RAY_DASHBOARD_AGGREGATOR_AGENT_EVENT_EXPORT_ADDR": "http://127.0.0.1",
->>>>>>> 646884b0c2 (update the configuration in aggregation agent)
+                "RAY_DASHBOARD_AGGREGATOR_AGENT_EVENT_EXPORT_ADDR": _EVENT_AGGREGATOR_AGENT_TARGET_ADDR,
             },
         },
     ],
@@ -287,6 +279,7 @@ def test_aggregator_agent_receive_multiple_events(
             "env_vars": {
                 "RAY_DASHBOARD_AGGREGATOR_AGENT_MAX_EVENT_BUFFER_SIZE": 1,
 <<<<<<< HEAD
+<<<<<<< HEAD
                 "RAY_DASHBOARD_AGGREGATOR_AGENT_EVENT_SEND_PORT": str(
                     _EVENT_AGGREGATOR_AGENT_TARGET_PORT
                 ),
@@ -294,6 +287,9 @@ def test_aggregator_agent_receive_multiple_events(
                 "RAY_DASHBOARD_AGGREGATOR_AGENT_EVENT_EXPORT_ADDR": "http://127.0.0.1",
                 "RAY_DASHBOARD_AGGREGATOR_AGENT_EVENT_EXPORT_PORT": _EVENT_AGGREGATOR_AGENT_TARGET_PORT,
 >>>>>>> 646884b0c2 (update the configuration in aggregation agent)
+=======
+                "RAY_DASHBOARD_AGGREGATOR_AGENT_EVENT_EXPORT_ADDR": _EVENT_AGGREGATOR_AGENT_TARGET_ADDR,
+>>>>>>> 3a08e9b21f (merge the addr and port env var)
             },
         },
     ],
