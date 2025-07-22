0b0f647f2986b739d35a38efaf140c915058ea26
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2025-07-21 23:27:55 -0700

    add lmcache installation checker
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- python/ray/llm/_internal/serve/deployments/llm/vllm/kv_transfer_backends/lmcache_connector_v1.py
+++ python/ray/llm/_internal/serve/deployments/llm/vllm/kv_transfer_backends/lmcache_connector_v1.py
@@ -3,10 +3,17 @@ from ray.llm._internal.serve.deployments.llm.vllm.kv_transfer_backends.base impo
 )
 
 
+def _check_lmcache_installed():
+    try:
+        import lmcache
+    except ImportError:
+        raise ImportError("LMCache is not installed. Please install it with `pip install lmcache`.")
+    
+
 class LMCacheConnectorV1Backend(BaseConnectorBackend):
     def setup(self) -> None:
         """
         No manipulations with LMCacheConnectorV1 is needed.
         vLLM will configure the LMCache connector.
         """
-        pass
+        _check_lmcache_installed()
