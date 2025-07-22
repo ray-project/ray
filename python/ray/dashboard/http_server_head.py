031fca594fdd2ce1a7864ee9f56a426adebf32d7
Lester Wang <38121764+LeslieWongCV@users.noreply.github.com>
2025-07-22 13:19:10 +0800

    [Core] Fix bug in dashboard by replacing invalid pathlib.posixpath.realpath with posixpath.realpath (#54523)
    Signed-off-by: Lester Wang <38121764+LeslieWongCV@users.noreply.github.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- python/ray/dashboard/http_server_head.py
+++ python/ray/dashboard/http_server_head.py
@@ -4,6 +4,7 @@ import ipaddress
 import logging
 import os
 import pathlib
+import posixpath
 import sys
 import time
 from math import floor
@@ -170,9 +171,7 @@ class HttpServerDashboardHead:
 
             # If the destination is not relative to the expected directory,
             # then the user is attempting path traversal, so deny the request.
-            request_path = pathlib.PurePosixPath(
-                pathlib.posixpath.realpath(request.path)
-            )
+            request_path = pathlib.PurePosixPath(posixpath.realpath(request.path))
             if request_path != parent and parent not in request_path.parents:
                 logger.info(
                     f"Rejecting {request_path=} because it is not relative to {parent=}"
