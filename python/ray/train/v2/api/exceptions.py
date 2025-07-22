f6dc62608704b6cf75ba1e7b355ae51ee0396e97
Xinyuan <43737116+xinyuangui2@users.noreply.github.com>
2025-07-21 17:56:45 -0700

    [Train] Add ControllerError for the errors thrown from the controller (#54801)
    The controller can raise a variety of errors. We distinguish these through two subclasses of `RayTrainError`:
    * `TrainingFailedError` captures Train worker failures. If any of the workers failed, then this error is populated with a dict mapping worker rank to the error.
    * `ControllerError` captures Train driver errors (the `TrainController`). For example, if there are too many worker group startup attempts that fail (see https://github.com/ray-project/ray/pull/54257), then the controller can error out.
    ---------
    
    Signed-off-by: xgui <xgui@anyscale.com>
    Signed-off-by: jugalshah291 <shah.jugal291@gmail.com>

--- python/ray/train/v2/api/exceptions.py
+++ python/ray/train/v2/api/exceptions.py
@@ -6,7 +6,13 @@ from ray.util.annotations import PublicAPI
 
 @PublicAPI(stability="alpha")
 class TrainingFailedError(RayTrainError):
-    """Exception raised by `<Framework>Trainer.fit()` when training fails."""
+    """Exception raised from the training workers.
+
+    Args:
+        error_message: A human-readable error message describing the training worker failures.
+        worker_failures: A mapping from worker rank to the exception that
+            occurred on that worker during training.
+    """
 
     def __init__(self, error_message: str, worker_failures: Dict[int, Exception]):
         super().__init__("Training failed due to worker errors:\n" + error_message)
@@ -15,3 +21,21 @@ class TrainingFailedError(RayTrainError):
 
     def __reduce__(self):
         return (self.__class__, (self._error_message, self.worker_failures))
+
+
+@PublicAPI(stability="alpha")
+class ControllerError(RayTrainError):
+    """Exception raised when training fails due to a controller error.
+
+    Args:
+        controller_failure: The exception that occurred on the controller.
+    """
+
+    def __init__(self, controller_failure: Exception):
+        super().__init__(
+            "Training failed due to controller error:\n" + str(controller_failure)
+        )
+        self.controller_failure = controller_failure
+
+    def __reduce__(self):
+        return (self.__class__, (self.controller_failure,))
