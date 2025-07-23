c7fa3f7d97c668750d068f4de1effd6ddaa751ee
Gagandeep Singh <gdp.1807@gmail.com>
2025-07-23 14:51:14 +0530

    ENH: Remove --target //:ray_pkg
    Signed-off-by: Gagandeep Singh <gdp.1807@gmail.com>

--- ci/fossa/analyze.sh
+++ ci/fossa/analyze.sh
@@ -20,5 +20,4 @@ git clone https://"$GITHUB_TOKEN"@github.com/ray-project/ray-cpp-wheel-analyzer.
 python "$HOME"/ray-cpp-wheel-analyzer/run_fossa_analysis_with_bazel.py \
         --run-fossa --fossa-api-key "$FOSSA_API_KEY" \
         --output-dir "$HOME"/fossa-scan-dir \
-        --fossa-exec "$FOSSA_BIN" \
-        --targets //:ray_pkg
+        --fossa-exec "$FOSSA_BIN"
