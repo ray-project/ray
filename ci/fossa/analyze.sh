6729547506b80b0407aff1859157854c15b763ac
Gagandeep Singh <gdp.1807@gmail.com>
2025-07-23 11:57:51 +0530

    ENH: Run ray-cpp-wheel-analyzer on CI
    Signed-off-by: Gagandeep Singh <gdp.1807@gmail.com>

--- ci/fossa/analyze.sh
+++ ci/fossa/analyze.sh
@@ -12,3 +12,10 @@ FOSSA_API_KEY="$(
 export FOSSA_API_KEY
 
 "$FOSSA_BIN" analyze
+
+git clone https://"$GITHUB_TOKEN"@github.com/ray-project/ray-cpp-wheel-analyzer.git "$HOME"/ray-cpp-wheel-analyzer
+
+python "$HOME"/ray-cpp-wheel-analyzer/run_fossa_analysis_with_bazel.py \
+        --run-fossa --fossa-api-key "$FOSSA_API_KEY" \
+        --output-dir ../fossa-scan-dir \
+        --fossa-exec "$FOSSA_BIN"
