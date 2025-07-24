819b6e77afe8e6de3fcfe00da36a82c8abf9f9a0
ℍ𝕠𝕝𝕝𝕠𝕨 𝕄𝕒𝕟 <hollowman@opensuse.org>
2025-07-24 17:11:49 +0300

    [Dashboard] Refactor and support reporting AMD GPU usage & NVIDIA MIG mode memory usage (#51345)
    To add support for reporting AMD GPU usage and NVIDIA MIG mode memory
    usage in the dashboard:
    
    ![QQ20250313-221220](https://github.com/user-attachments/assets/67e6dbfd-5876-4d72-9524-6d770938ae8f)
    
    ---------
    
    Signed-off-by: Hollow Man <hollowman@opensuse.org>
    Signed-off-by: jugalshah291 <shah.jugal291@gmail.com>

--- python/ray/dashboard/modules/reporter/reporter_agent.py
+++ python/ray/dashboard/modules/reporter/reporter_agent.py
@@ -9,7 +9,7 @@ import sys
 import traceback
 from collections import defaultdict
 from concurrent.futures import ThreadPoolExecutor
-from typing import List, Optional, Tuple, TypedDict, Union
+from typing import List, Optional, Tuple
 
 from opencensus.stats import stats as stats_module
 from prometheus_client.core import REGISTRY
@@ -28,8 +28,12 @@ import ray.dashboard.modules.reporter.reporter_consts as reporter_consts
 import ray.dashboard.utils as dashboard_utils
 from ray._common.utils import (
     get_or_create_event_loop,
-    get_system_memory,
     get_user_temp_dir,
+)
+from ray._private.utils import get_system_memory
+from ray.dashboard.modules.reporter.gpu_providers import (
+    GpuMetricProvider,
+    TpuUtilizationInfo,
 )
 from ray._private import utils
 from ray._private.metrics_agent import Gauge, MetricsAgent, Record
@@ -62,8 +66,6 @@ from ray.dashboard.modules.reporter.profile_manager import (
 import psutil
 
 logger = logging.getLogger(__name__)
-
-enable_gpu_usage_check = True
 
 enable_tpu_usage_check = True
 
@@ -367,43 +369,6 @@ PSUTIL_PROCESS_ATTRS = (
     else []
 )
 
-MB = 1024 * 1024
-
-# Types
-Percentage = int
-Megabytes = int
-Bytes = int
-
-
-# gpu utilization for nvidia gpu from a single process
-class ProcessGPUInfo(TypedDict):
-    pid: int
-    gpu_memory_usage: Megabytes
-
-
-# gpu utilization for nvidia gpu
-class GpuUtilizationInfo(TypedDict):
-    index: int
-    name: str
-    uuid: str
-    utilization_gpu: Optional[Percentage]
-    memory_used: Megabytes
-    memory_total: Megabytes
-    processes_pids: Optional[List[ProcessGPUInfo]]
-
-
-# tpu utilization for google tpu
-class TpuUtilizationInfo(TypedDict):
-    index: int
-    name: str
-    tpu_type: str
-    tpu_topology: str
-    tensorcore_utilization: Percentage
-    hbm_utilization: Percentage
-    duty_cycle: Percentage
-    memory_used: Bytes
-    memory_total: Bytes
-
 
 class ReporterAgent(
     dashboard_utils.DashboardAgentModule,
@@ -499,6 +464,9 @@ class ReporterAgent(
             profile_dir_path=self._log_dir, ip_address=self._ip
         )
         self._gpu_profiling_manager.start_monitoring_daemon()
+
+        # Create GPU metric provider instance
+        self._gpu_metric_provider = GpuMetricProvider()
 
     async def GetTraceback(self, request, context):
         pid = request.pid
@@ -681,82 +649,9 @@ class ReporterAgent(
         else:
             return psutil.cpu_percent()
 
-    @staticmethod
-    def _get_gpu_usage():
-        import ray._private.thirdparty.pynvml as pynvml
-
-        global enable_gpu_usage_check
-        if not enable_gpu_usage_check:
-            return []
-        gpu_utilizations = []
-
-        def decode(b: Union[str, bytes]) -> str:
-            if isinstance(b, bytes):
-                return b.decode("utf-8")  # for python3, to unicode
-            return b
-
-        try:
-            pynvml.nvmlInit()
-        except Exception as e:
-            logger.debug(f"pynvml failed to retrieve GPU information: {e}")
-
-            # On machines without GPUs, pynvml.nvmlInit() can run subprocesses that
-            # spew to stderr. Then with log_to_driver=True, we get log spew from every
-            # single raylet. To avoid this, disable the GPU usage check on
-            # certain errors.
-            # https://github.com/ray-project/ray/issues/14305
-            # https://github.com/ray-project/ray/pull/21686
-            if type(e).__name__ == "NVMLError_DriverNotLoaded":
-                enable_gpu_usage_check = False
-            return gpu_utilizations
-
-        num_gpus = pynvml.nvmlDeviceGetCount()
-        for i in range(num_gpus):
-            gpu_handle = pynvml.nvmlDeviceGetHandleByIndex(i)
-            memory_info = pynvml.nvmlDeviceGetMemoryInfo(gpu_handle)
-            utilization = None
-            try:
-                utilization_info = pynvml.nvmlDeviceGetUtilizationRates(gpu_handle)
-                utilization = int(utilization_info.gpu)
-            except pynvml.NVMLError as e:
-                logger.debug(f"pynvml failed to retrieve GPU utilization: {e}")
-
-            # processes pids
-            processes_pids = None
-            try:
-                nv_comp_processes = pynvml.nvmlDeviceGetComputeRunningProcesses(
-                    gpu_handle
-                )
-                nv_graphics_processes = pynvml.nvmlDeviceGetGraphicsRunningProcesses(
-                    gpu_handle
-                )
-                processes_pids = [
-                    ProcessGPUInfo(
-                        pid=int(nv_process.pid),
-                        gpu_memory_usage=(
-                            int(nv_process.usedGpuMemory) // MB
-                            if nv_process.usedGpuMemory
-                            else 0
-                        ),
-                    )
-                    for nv_process in (nv_comp_processes + nv_graphics_processes)
-                ]
-            except pynvml.NVMLError as e:
-                logger.debug(f"pynvml failed to retrieve GPU processes: {e}")
-
-            info = GpuUtilizationInfo(
-                index=i,
-                name=decode(pynvml.nvmlDeviceGetName(gpu_handle)),
-                uuid=decode(pynvml.nvmlDeviceGetUUID(gpu_handle)),
-                utilization_gpu=utilization,
-                memory_used=int(memory_info.used) // MB,
-                memory_total=int(memory_info.total) // MB,
-                processes_pids=processes_pids,
-            )
-            gpu_utilizations.append(info)
-        pynvml.nvmlShutdown()
-
-        return gpu_utilizations
+    def _get_gpu_usage(self):
+        """Get GPU usage information using the GPU metric provider."""
+        return self._gpu_metric_provider.get_gpu_usage()
 
     @staticmethod
     def _get_tpu_usage() -> List[TpuUtilizationInfo]:
@@ -1743,6 +1638,9 @@ class ReporterAgent(
                 metrics_service_pb2_grpc.add_MetricsServiceServicer_to_server(
                     self, server
                 )
+
+        # Initialize GPU metric provider when the agent starts
+        self._gpu_metric_provider.initialize()
 
         await self._run_loop()
 
