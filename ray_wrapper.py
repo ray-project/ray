e53ad994f7d59d3ceba031b095debbbedf7c32d7
omkar <omkar@anyscale.com>
2025-07-24 23:21:53 +0000

    feat: reverted fixes Signed-off-by: Omkar omkar@anyscale.com

--- ray_wrapper.py
+++
@@ -1,9 +0,0 @@
-#!/home/ubuntu/.conda/envs/rayturbo/bin/python3.9
-# -*- coding: utf-8 -*-
-import re
-import sys
-from ray.scripts.scripts import main
-
-if __name__ == "__main__":
-    sys.argv[0] = re.sub(r"(-script\.pyw|\.exe)?$", "", sys.argv[0])
-    sys.exit(main())
