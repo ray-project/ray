8a6c66221ae684c2ec139b342efa1c42065e6cba
tianyi-ge <tianyig@outlook.com>
2025-07-25 17:03:03 +0800

    [core] fix new shadow
    Signed-off-by: tianyi-ge <tianyig@outlook.com>

--- src/ray/raylet/main.cc
+++ src/ray/raylet/main.cc
@@ -767,10 +767,10 @@ int main(int argc, char *argv[]) {
                                                           announce_infeasible_task,
                                                           *local_task_manager);
 
-    auto raylet_client_factory = [&](const NodeID &node_id,
+    auto raylet_client_factory = [&](const NodeID &id,
                                      ray::rpc::ClientCallManager &client_call_mgr) {
-      const ray::rpc::GcsNodeInfo *node_info = gcs_client->Nodes().Get(node_id);
-      RAY_CHECK(node_info) << "No GCS info for node " << node_id;
+      const ray::rpc::GcsNodeInfo *node_info = gcs_client->Nodes().Get(id);
+      RAY_CHECK(node_info) << "No GCS info for node " << id;
       return std::make_shared<ray::raylet::RayletClient>(
           node_info->node_manager_address(),
           node_info->node_manager_port(),
