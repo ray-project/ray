f3e82c1f57c3deb40c5a26abbbc034f1c1bac4ed
Kourosh Hakhamaneshi <kourosh@anyscale.com>
2025-07-25 09:14:49 -0700

    wip
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>

--- python/ray/serve/llm/request_router.py
+++ python/ray/serve/llm/request_router.py
@@ -1,4 +1,3 @@
-# TODO (Kourosh): Internally rename PrefixCacheAffinityRouter to something else.
 from ray.llm._internal.serve.request_router.prefix_aware.prefix_aware_router import (
     PrefixCacheAffinityRouter as _PrefixCacheAffinityRouter,
 )
