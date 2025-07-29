fe0d66183a302edf83570dd6c7eea143f01e4ddc
Linkun Chen <github@lkchen.net>
2025-07-28 17:27:08 -0700

    wip
    Signed-off-by: Linkun Chen <github@lkchen.net>

---
+++ python/ray/llm/tests/serve/gpu/deployments/llm/vllm/test_vllm_engine_gpu.py
@@ -0,0 +1,37 @@
+
+import asyncio
+import pytest
+
+from ray.serve.llm import LLMConfig, ModelLoadingConfig
+from ray.llm._internal.serve.deployments.llm.vllm.vllm_engine import VLLMEngine
+
+
+@pytest.mark.asyncio
+def test_vllm_engine_start_with_custom_resource_bundle(
+    # defined in conftest.py
+    model_smolvlm_256m
+):
+    """Test vLLM engine start with custom resource bundle."""
+    llm_config = LLMConfig(
+        model_loading_config=ModelLoadingConfig(
+            model_id="smolvlm-256m",
+            model_source=model_smolvlm_256m,
+        ),
+        engine_kwargs=dict(
+            gpu_memory_utilization=0.4,
+            use_tqdm_on_load=False,
+            enforce_eager=True,
+        ),
+        resources_per_bundle=dict(GPU=0.49),
+        runtime_env=dict(
+            env_vars={
+                "VLLM_RAY_PER_WORKER_GPUS": "0.49",
+                "VLLM_DISABLE_COMPILE_CACHE": "1",
+            },
+        ),
+    )
+
+    engine = VLLMEngine(llm_config)
+    asyncio.run(engine.start())
+    asyncio.run(engine.check_health())
+    engine.shutdown()
