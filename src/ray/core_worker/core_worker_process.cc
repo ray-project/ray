849494eb7c683c2a5049c74f02079f0205c67c34
joshlee <joshlee@anyscale.com>
2025-07-30 16:47:59 +0000

    Fixing windows build error
    Signed-off-by: joshlee <joshlee@anyscale.com>

--- src/ray/core_worker/core_worker_process.cc
+++ src/ray/core_worker/core_worker_process.cc
@@ -375,6 +375,8 @@ std::shared_ptr<CoreWorker> CoreWorkerProcessImpl::CreateCoreWorker(
             "CoreWorker.HandleException");
       });
 
+  std::shared_ptr<experimental::MutableObjectProvider> experimental_mutable_object_provider;
+
 #if defined(__APPLE__) || defined(__linux__)
   auto raylet_channel_client_factory = [this](const NodeID &node_id) {
     auto core_worker = GetCoreWorker();
