bc46074b4af4270a2f103b6fabdf5d21991733c4
Joshua Lee <73967497+Sparks0219@users.noreply.github.com>
2025-07-30 12:44:09 -0700

    [core] Fixing windows build error (#55053)
    Introduced a windows build error in
    https://github.com/ray-project/ray/pull/54759 since
    experimental_mutable_object_provider is not defined for windows.
    Updating the initialization of the experimental_mutable_object_provider
    pointer to be outside of the guards for linux/apple.
    
    ---------
    
    Signed-off-by: joshlee <joshlee@anyscale.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- src/ray/core_worker/core_worker_process.cc
+++ src/ray/core_worker/core_worker_process.cc
@@ -380,6 +380,9 @@ std::shared_ptr<CoreWorker> CoreWorkerProcessImpl::CreateCoreWorker(
             "CoreWorker.HandleException");
       });
 
+  std::shared_ptr<experimental::MutableObjectProvider>
+      experimental_mutable_object_provider;
+
 #if defined(__APPLE__) || defined(__linux__)
   auto raylet_channel_client_factory = [this](const NodeID &node_id) {
     auto core_worker = GetCoreWorker();
@@ -390,7 +393,7 @@ std::shared_ptr<CoreWorker> CoreWorkerProcessImpl::CreateCoreWorker(
     return core_worker->raylet_client_pool_->GetOrConnectByAddress(std::move(addr));
   };
 
-  auto experimental_mutable_object_provider =
+  experimental_mutable_object_provider =
       std::make_shared<experimental::MutableObjectProvider>(
           *plasma_store_provider->store_client(),
           raylet_channel_client_factory,
