439e7945f8edc671a81d8256e9d019dd96019fa3
Goku Mohandas <gokumd@gmail.com>
2025-07-29 21:27:39 -0700

    Multimodal ai (#54029)
    Signed-off-by: Goku Mohandas <gokumd@gmail.com>
    Co-authored-by: angelinalg <122562471+angelinalg@users.noreply.github.com>
    Co-authored-by: Masoud <masoudcharkhabi@gmail.com>
    Signed-off-by: jugalshah291 <shah.jugal291@gmail.com>

--- doc/source/ray-overview/examples/e2e-multimodal-ai-workloads/doggos/serve.py
+++ doc/source/ray-overview/examples/e2e-multimodal-ai-workloads/doggos/serve.py
@@ -1,15 +1,17 @@
-import os
 from urllib.parse import urlparse
 
 import mlflow
-import ray
-from fastapi import FastAPI
-from ray import serve
-from starlette.requests import Request
-
-from doggos.embed import EmbeddingGenerator, get_top_matches
+import numpy as np
+import torch
 from doggos.infer import TorchPredictor
+from doggos.model import collate_fn
 from doggos.utils import url_to_array
+from fastapi import FastAPI
+from PIL import Image
+from starlette.requests import Request
+from transformers import CLIPModel, CLIPProcessor
+
+from ray import serve
 
 # Define app
 api = FastAPI(
@@ -22,92 +24,49 @@ api = FastAPI(
 @serve.deployment(
     num_replicas="1",
     ray_actor_options={
-        "num_cpus": 2,
+        "num_cpus": 4,
         "num_gpus": 1,
         "accelerator_type": "L4",
     },
 )
 class ClassPredictor:
-    def __init__(self, artifacts_dir):
+    def __init__(self, model_id, artifacts_dir, device="cuda"):
         """Initialize the model."""
+        # Embdding model
+        self.processor = CLIPProcessor.from_pretrained(model_id)
+        self.model = CLIPModel.from_pretrained(model_id)
+        self.model.to(device=device)
+        self.device = device
+
+        # Trained classifier
         self.predictor = TorchPredictor.from_artifacts_dir(artifacts_dir=artifacts_dir)
         self.preprocessor = self.predictor.preprocessor
 
     def get_probabilities(self, url):
-        image = url_to_array(url=url)
-        ds = ray.data.from_items([{"image": image, "url": url}])
-        ds = self.preprocessor.transform(
-            ds=ds,
-            concurrency=1,
-            batch_size=1,
-            num_gpus=1,
+        image = Image.fromarray(np.uint8(url_to_array(url=url))).convert("RGB")
+        inputs = self.processor(images=[image], return_tensors="pt", padding=True).to(
+            self.device
         )
-        ds = ds.map_batches(
-            self.predictor.predict_probabilities,
-            fn_kwargs={"device": "cuda"},
-            concurrency=1,
-            batch_size=1,
-            num_gpus=1,
+        with torch.inference_mode():
+            embedding = self.model.get_image_features(**inputs).cpu().numpy()
+        probabilities = self.predictor.predict_probabilities(
+            collate_fn({"embedding": embedding})
         )
-        probabilities = ds.take_all()[0]["probabilities"]
         return probabilities
-
-
-@serve.deployment(
-    num_replicas="1",
-    ray_actor_options={
-        "num_cpus": 2,
-        "num_gpus": 1,
-        "accelerator_type": "L4",
-    },
-)
-class EmbeddingSimilarity:
-    def __init__(self, embeddings_path):
-        self.embedding_generator = EmbeddingGenerator(
-            model_id="openai/clip-vit-base-patch32"
-        )
-        self.embeddings_ds = ray.data.read_parquet(embeddings_path)  # use vector DB
-
-    def get_top_matches(self, url, probabilities, k):
-        # Top k class predictions
-        sorted_probabilities = sorted(
-            probabilities.items(), key=lambda x: x[1], reverse=True
-        )
-        top_k = [item[0] for item in sorted_probabilities[0:k]]
-
-        # Generate embedding
-        image = url_to_array(url=url)
-        embedding = self.embedding_generator({"image": [image]})["embedding"][0]
-
-        # Filter for top matches
-        top_matches = get_top_matches(
-            query_embedding=embedding,
-            embeddings_ds=self.embeddings_ds,
-            class_filters=top_k,
-            n=5,
-        )
-        return top_matches
 
 
 @serve.deployment(num_replicas="1", ray_actor_options={"num_cpus": 2})
 @serve.ingress(api)
 class Doggos:
-    def __init__(self, classifier, embedder):
+    def __init__(self, classifier):
         self.classifier = classifier
-        self.embedder = embedder
 
     @api.post("/predict/")
     async def predict(self, request: Request):
         data = await request.json()
         probabilities = await self.classifier.get_probabilities.remote(url=data["url"])
-        top_matches = await self.embedder.get_top_matches.remote(
-            url=data["url"],
-            probabilities=probabilities,
-            k=data["k"],
-        )
         return {
             "probabilities": probabilities,
-            "top_matches": top_matches,
         }
 
 
@@ -123,15 +82,12 @@ sorted_runs = mlflow.search_runs(
 )
 best_run = sorted_runs.iloc[0]
 artifacts_dir = urlparse(best_run.artifact_uri).path
-embeddings_path = os.path.join("/mnt/user_storage", "doggos/embeddings")
 
 # Define app
 app = Doggos.bind(
     classifier=ClassPredictor.bind(artifacts_dir=artifacts_dir),
-    embedder=EmbeddingSimilarity.bind(embeddings_path=embeddings_path),
 )
 
 if __name__ == "__main__":
-
     # Run service locally
     serve.run(app, route_prefix="/")
