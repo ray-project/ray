151d7dc878bdff61c32889b92709377dc437b32e
Gagandeep Singh <gdp.1807@gmail.com>
2025-07-31 16:41:05 +0530

    CI: Build dashboard by switching to current user
    Signed-off-by: Gagandeep Singh <gdp.1807@gmail.com>

--- ci/build/build-dashboard.sh
+++ ci/build/build-dashboard.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 set -euxo pipefail
 
+# Host user UID/GID
+HOST_UID=${HOST_UID:-$(id -u)}
+HOST_GID=${HOST_GID:-$(id -g)}
+
+if [ "$EUID" -eq 0 ]; then
+
+  # Install sudo
+  yum -y install sudo
+
+  # Create group and user
+  groupadd -g "$HOST_GID" builduser
+  useradd -m -u "$HOST_UID" -g "$HOST_GID" -d /ray builduser
+
+  # Give sudo access
+  echo "builduser ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+  exec sudo -E -u builduser HOME="$HOME" bash "$0" "$@"
+
+  exit 0
+
+fi
+
 # Build the dashboard using Node
 cd "$(dirname "$0")/../../python/ray/dashboard/client"
 
