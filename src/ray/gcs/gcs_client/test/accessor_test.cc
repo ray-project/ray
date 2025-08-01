793d97ab1226e9ad357b6380702e25460d5a226d
Chi-Sheng Liu <chishengliu@chishengliu.com>
2025-08-02 04:33:25 +0800

    [Feat][Core] Don't count task retries due to node preemption (#54182)
    We decided that task retries due to node preemption should not count
    toward `max_retries`.
    ---------
    
    Signed-off-by: Rueian <rueian@anyscale.com>
    Signed-off-by: Rueian <rueiancsie@gmail.com>
    Co-authored-by: Rueian <rueian@anyscale.com>
    Co-authored-by: Rueian <rueiancsie@gmail.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- src/ray/gcs/gcs_client/test/accessor_test.cc
+++ src/ray/gcs/gcs_client/test/accessor_test.cc
@@ -55,6 +55,33 @@ TEST(NodeInfoAccessorTest, TestHandleNotification) {
   ASSERT_EQ(num_notifications, 2);
 }
 
+TEST(NodeInfoAccessorTest, TestHandleNotificationDeathInfo) {
+  NodeInfoAccessor accessor;
+  rpc::GcsNodeInfo node_info;
+  node_info.set_state(rpc::GcsNodeInfo_GcsNodeState::GcsNodeInfo_GcsNodeState_DEAD);
+  NodeID node_id = NodeID::FromRandom();
+  node_info.set_node_id(node_id.Binary());
+
+  auto death_info = node_info.mutable_death_info();
+  death_info->set_reason(rpc::NodeDeathInfo::EXPECTED_TERMINATION);
+  death_info->set_reason_message("Test termination reason");
+
+  node_info.set_end_time_ms(12345678);
+
+  accessor.HandleNotification(std::move(node_info));
+
+  auto cached_node = accessor.Get(node_id, false);
+  ASSERT_NE(cached_node, nullptr);
+  ASSERT_EQ(cached_node->node_id(), node_id.Binary());
+  ASSERT_EQ(cached_node->state(),
+            rpc::GcsNodeInfo_GcsNodeState::GcsNodeInfo_GcsNodeState_DEAD);
+
+  ASSERT_TRUE(cached_node->has_death_info());
+  ASSERT_EQ(cached_node->death_info().reason(), rpc::NodeDeathInfo::EXPECTED_TERMINATION);
+  ASSERT_EQ(cached_node->death_info().reason_message(), "Test termination reason");
+  ASSERT_EQ(cached_node->end_time_ms(), 12345678);
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
