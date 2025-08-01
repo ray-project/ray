4a992cb62cf7b23f458e24fc8bf87e59bc1ebcb3
harshit <harshit@anyscale.com>
2025-08-01 05:11:56 +0000

    change default batch timeout
    Signed-off-by: harshit <harshit@anyscale.com>

--- python/ray/serve/batching.py
+++ python/ray/serve/batching.py
@@ -588,7 +588,7 @@ def batch(
     _func: Optional[Callable] = None,
     /,
     max_batch_size: int = 10,
-    batch_wait_timeout_s: float = 0.0,
+    batch_wait_timeout_s: float = 0.01,
     max_concurrent_batches: int = 1,
 ) -> Callable:
     """Converts a function to asynchronously handle batches.
