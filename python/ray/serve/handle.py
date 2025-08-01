c866124dae2181afb7556722de7b31e906d1fda2
Abrar Sheikh <abrar@anyscale.com>
2025-08-01 07:20:58 -0700

    introduce same event loop router in serve (#55030)
    ```
    from ray import serve
    
    @serve.deployment(max_ongoing_requests=10000)
    class MyDeployment:
        async def __call__(self):
            return "Hello, world!"
    
    app = MyDeployment.bind()
    
    ```
    
    `ab -n 2000 -c 100 "http://localhost:8000/"`
    
    `RAY_SERVE_RUN_ROUTER_IN_SEPARATE_LOOP=0 serve run app:app`
    
    Requests per second:    483.46 [#/sec] (mean)
    Time per request:       206.843 [ms] (mean)
    
    `RAY_SERVE_RUN_ROUTER_IN_SEPARATE_LOOP=1 serve run app:app`
    
    Requests per second:    412.05 [#/sec] (mean)
    Time per request:       242.688 [ms] (mean)
    
    ~17% improvement in performance with running router in the same event
    loop as proxy
    
    ---------
    
    Signed-off-by: abrar <abrar@anyscale.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- python/ray/serve/handle.py
+++ python/ray/serve/handle.py
@@ -157,6 +157,9 @@ class _DeploymentHandleBase:
         ):
             ServeUsageTag.DEPLOYMENT_HANDLE_API_USED.record("1")
 
+    def _is_router_running_in_separate_loop(self) -> bool:
+        return self.init_options._run_router_in_separate_loop
+
     def _options(self, _prefer_local_routing=DEFAULT.VALUE, **kwargs):
         if kwargs.get("stream") is True and inside_ray_client_context():
             raise RuntimeError(
@@ -208,15 +211,27 @@ class _DeploymentHandleBase:
     def __getattr__(self, name):
         return self.options(method_name=name)
 
-    def shutdown(self):
+    def shutdown(self, _skip_asyncio_check: bool = False):
         if self._router:
             shutdown_future = self._router.shutdown()
-            shutdown_future.result()
+            if self._is_router_running_in_separate_loop():
+                shutdown_future.result()
+            else:
+                if not _skip_asyncio_check:
+                    raise RuntimeError(
+                        "Sync methods should not be called from within an `asyncio` event "
+                        "loop. Use `await handle.shutdown_async()` instead."
+                    )
 
     async def shutdown_async(self):
         if self._router:
-            shutdown_future = self._router.shutdown()
-            await asyncio.wrap_future(shutdown_future)
+            shutdown_future: Union[
+                asyncio.Future, concurrent.futures.Future
+            ] = self._router.shutdown()
+            if self._is_router_running_in_separate_loop:
+                await asyncio.wrap_future(shutdown_future)
+            else:
+                await shutdown_future
 
     def __repr__(self):
         return f"{self.__class__.__name__}" f"(deployment='{self.deployment_name}')"
@@ -238,13 +253,17 @@ class _DeploymentHandleBase:
 class _DeploymentResponseBase:
     def __init__(
         self,
-        replica_result_future: concurrent.futures.Future[ReplicaResult],
+        replica_result_future: Union[
+            concurrent.futures.Future[ReplicaResult], asyncio.Future[ReplicaResult]
+        ],
         request_metadata: RequestMetadata,
+        _is_router_running_in_separate_loop: bool = True,
     ):
         self._cancelled = False
         self._replica_result_future = replica_result_future
         self._replica_result: Optional[ReplicaResult] = None
         self._request_metadata: RequestMetadata = request_metadata
+        self._is_router_running_in_separate_loop = _is_router_running_in_separate_loop
 
     @property
     def request_id(self) -> str:
@@ -259,10 +278,16 @@ class _DeploymentResponseBase:
         """
 
         if self._replica_result is None:
+            if not self._is_router_running_in_separate_loop:
+                raise RuntimeError(
+                    "Sync methods should not be called from within an `asyncio` event "
+                    "loop. Use `await response` instead."
+                )
             try:
                 self._replica_result = self._replica_result_future.result(
                     timeout=_timeout_s
                 )
+
             except concurrent.futures.TimeoutError:
                 raise TimeoutError("Timed out resolving to ObjectRef.") from None
             except concurrent.futures.CancelledError:
@@ -277,11 +302,16 @@ class _DeploymentResponseBase:
         """
 
         if self._replica_result is None:
-            # Use `asyncio.wrap_future` so `self._replica_result_future` can be awaited
-            # safely from any asyncio loop.
-            self._replica_result = await asyncio.wrap_future(
-                self._replica_result_future
-            )
+            if self._is_router_running_in_separate_loop:
+                # Use `asyncio.wrap_future` so `self._replica_result_future` can be awaited
+                # safely from any asyncio loop.
+                # self._replica_result_future is a object of type concurrent.futures.Future
+                self._replica_result = await asyncio.wrap_future(
+                    self._replica_result_future
+                )
+            else:
+                # self._replica_result_future is a object of type asyncio.Future
+                self._replica_result = await self._replica_result_future
 
         return self._replica_result
 
@@ -310,6 +340,12 @@ class _DeploymentResponseBase:
 
         self._cancelled = True
         self._replica_result_future.cancel()
+        if not self._is_router_running_in_separate_loop:
+            # Given that there is a event loop running, we can't call sync methods.
+            # Hence optimistically cancel the replica result future and replica result.
+            if self._replica_result:
+                self._replica_result.cancel()
+            return
         try:
             # try to fetch the results synchronously. if it succeeds,
             # we will explicitly cancel the replica result. if it fails,
@@ -749,4 +785,8 @@ class DeploymentHandle(_DeploymentHandleBase):
         else:
             response_cls = DeploymentResponse
 
-        return response_cls(future, request_metadata)
+        return response_cls(
+            future,
+            request_metadata,
+            _is_router_running_in_separate_loop=self._is_router_running_in_separate_loop(),
+        )
