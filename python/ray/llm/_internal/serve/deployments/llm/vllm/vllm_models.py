85e226212aa7e312b124a857f2cabd38fe06d142
Seiji Eicher <58963096+eicherseiji@users.noreply.github.com>
2025-08-04 12:11:33 -0700

    [llm] bump vLLM to version 0.10.0 (#55067)
    - Use upstream `RayPrometheusStatLogger` to close spec. decode + lora
    errors
    - Include fix for https://github.com/vllm-project/vllm/issues/20647
      - Restore PP=2 to DeepSeek-V2-Lite release test
      - Remove copy of `FrontendArgs` upstreamed with https://github.com/vllm-project/vllm/pull/20206/
    
    Closes https://github.com/ray-project/ray/issues/54952
    
    Includes fix for https://github.com/ray-project/ray/issues/54812
    
    ---------
    
    Signed-off-by: Seiji Eicher <seiji@anyscale.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- python/ray/llm/_internal/serve/deployments/llm/vllm/vllm_models.py
+++ python/ray/llm/_internal/serve/deployments/llm/vllm/vllm_models.py
@@ -1,10 +1,10 @@
 import dataclasses
 import os
-from dataclasses import dataclass, field
 from typing import Any, Dict, List, Optional
 
 from pydantic import ConfigDict, Field
 from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.entrypoints.openai.cli_args import FrontendArgs
 
 from ray.llm._internal.common.base_pydantic import BaseModelExtended
 from ray.llm._internal.common.utils.cloud_utils import CloudMirrorConfig
@@ -24,49 +24,6 @@ from ray.util.placement_group import (
     placement_group,
     placement_group_table,
 )
-
-
-# TODO (Kourosh): Temprary until this abstraction lands in vllm upstream.
-# https://github.com/vllm-project/vllm/pull/20206
-@dataclass
-class FrontendArgs:
-    """Mirror of default values for FrontendArgs in vllm."""
-
-    host: Optional[str] = None
-    port: int = 8000
-    uvicorn_log_level: str = "info"
-    disable_uvicorn_access_log: bool = False
-    allow_credentials: bool = False
-    allowed_origins: list[str] = field(default_factory=lambda: ["*"])
-    allowed_methods: list[str] = field(default_factory=lambda: ["*"])
-    allowed_headers: list[str] = field(default_factory=lambda: ["*"])
-    api_key: Optional[str] = None
-    lora_modules: Optional[list[str]] = None
-    prompt_adapters: Optional[list[str]] = None
-    chat_template: Optional[str] = None
-    chat_template_content_format: str = "auto"
-    response_role: str = "assistant"
-    ssl_keyfile: Optional[str] = None
-    ssl_certfile: Optional[str] = None
-    ssl_ca_certs: Optional[str] = None
-    enable_ssl_refresh: bool = False
-    ssl_cert_reqs: int = 0
-    root_path: Optional[str] = None
-    middleware: list[str] = field(default_factory=lambda: [])
-    return_tokens_as_token_ids: bool = False
-    disable_frontend_multiprocessing: bool = False
-    enable_request_id_headers: bool = False
-    enable_auto_tool_choice: bool = False
-    tool_call_parser: Optional[str] = None
-    tool_parser_plugin: str = ""
-    log_config_file: Optional[str] = None
-    max_log_len: Optional[int] = None
-    disable_fastapi_docs: bool = False
-    enable_prompt_tokens_details: bool = False
-    enable_server_load_tracking: bool = False
-    enable_force_include_usage: bool = False
-    expand_tools_even_if_tool_choice_none: bool = False
-
 
 # The key for the kv_transfer_params in the internal metadata.
 KV_TRANSFER_PARAMS_KEY = "kv_transfer_params"
