d32a8ab67dd71ee0b31ecb8a1dd52989fef88e09
omkar <omkar@anyscale.com>
2025-08-04 16:56:28 +0000

    feat: updated test suite for DI
    Signed-off-by: omkar <omkar@anyscale.com>

--- python/ray/serve/tests/test_metrics.py
+++ python/ray/serve/tests/test_metrics.py
@@ -379,7 +379,7 @@ def test_proxy_metrics_internal_error(metrics_start_shutdown):
 
     def verify_metrics(_expected_metrics, do_assert=False):
         try:
-            ip = "0.0.0.0" if sys.platform == "win32" else "127.0.0.1"
+            ip = "127.0.0.1" if sys.platform == "win32" else "0.0.0.0"
             resp = httpx.get(f"http://{ip}:9999", timeout=None).text
         # Requests will fail if we are crashing the controller
         except httpx.HTTPError:
