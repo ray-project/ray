2cc5d0b36f10116f318eba77fdc28f3fb66a8b1b
Justin Yu <justinvyu@anyscale.com>
2025-08-04 15:28:12 -0700

    add more boundary operators in randomize blocks rule
    Signed-off-by: Justin Yu <justinvyu@anyscale.com>

--- python/ray/data/_internal/logical/rules/randomize_blocks.py
+++ python/ray/data/_internal/logical/rules/randomize_blocks.py
@@ -6,6 +6,8 @@ from ray.data._internal.logical.operators.all_to_all_operator import (
     AbstractAllToAll,
     RandomizeBlocks,
 )
+from ray.data._internal.logical.operators.streaming_split_operator import StreamingSplit
+from ray.data._internal.logical.operators.write_operator import Write
 
 
 class ReorderRandomizeBlocksRule(Rule):
@@ -16,14 +18,39 @@ class ReorderRandomizeBlocksRule(Rule):
 
     1. Dedupes multiple RandomizeBlocks operators if they are not seeded.
     2. Moves RandomizeBlocks operator to the end of a sequence of AbstractUDFMap
-    operators. RandomizeBlocks operators are not moved across AbstractAllToAll operator
-    boundaries.
+    operators. RandomizeBlocks operators are not moved across boundary operators.
+
+    A boundary operator is one that RandomizeBlocks is not allowed to be pushed
+    past to retain an equivalent logical plan.
+    This includes AllToAll, Write, and StreamingSplit operators.
+
+    Example 1: RandomizeBlocks is not allowed to be pushed past Write/StreamingSplit.
+
+    Input: Read -> Map1 -> RandomizeBlocks -> Map2 -> Write/StreamingSplit
+    Optimized: (Read->Map1->Map2) -> RandomizeBlocks -> Write/StreamingSplit
+
+    Example 2: RandomizeBlocks is not allowed to be pushed past Sort (AllToAll).
+
+    Input: Read -> Map1 -> RandomizeBlocks -> Sort -> Map2 -> Write
+    Optimized: (Read->Map1) -> RandomizeBlocks -> Sort -> Map2 -> Write
     """
 
     def apply(self, plan: LogicalPlan) -> LogicalPlan:
         optimized_dag: LogicalOperator = self._apply(plan.dag)
         new_plan = LogicalPlan(dag=optimized_dag, context=plan.context)
         return new_plan
+
+    @staticmethod
+    def _is_boundary_operator(op: LogicalOperator) -> bool:
+        """Returns True if the operator is a boundary operator."""
+        # Only consider AllToAll operators that are not RandomizeBlocks,
+        # since we will already deduplicate RandomizeBlocks operators.
+        is_all_to_all = isinstance(op, AbstractAllToAll) and not isinstance(
+            op, RandomizeBlocks
+        )
+        is_write = isinstance(op, Write)
+        is_streaming_split = isinstance(op, StreamingSplit)
+        return is_all_to_all or is_write or is_streaming_split
 
     def _apply(self, op: LogicalOperator) -> LogicalOperator:
         operators = []
@@ -54,13 +81,11 @@ class ReorderRandomizeBlocksRule(Rule):
                     # dependencies.
                     assert len(upstream_ops[i].input_dependencies) == 1
                     upstream_ops[i] = upstream_ops[i].input_dependencies[0]
-            if isinstance(current_op, AbstractAllToAll) and not isinstance(
-                current_op, RandomizeBlocks
-            ):
-                # If this operator is a an AllToAll Operator, then insert
-                # RandomizeBlocks right before this operator rather than the end of the
-                # DAG.
-                # All-to-all operators can have only 1 input operator.
+
+            if self._is_boundary_operator(current_op):
+                # If this operator is a boundary operator, then insert RandomizeBlocks
+                # right before this operator rather than the end of the DAG.
+                # Boundary operators can have only 1 input operator.
                 assert len(upstream_ops) == 1
                 input_op = upstream_ops[0]
                 for random_op in operators:
