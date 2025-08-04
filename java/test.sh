04fd9be92463f1a9988e2fc297f5ec3955d2821a
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2025-08-04 12:09:16 -0700

    [wheel] do not depend on `:ray_pkg` from java (#55181)
    depends on `//:ray_pkg_zip` instead, with comment asking user to
    manually run `//:gen_ray_pkg`
    
    the java test rules are not conventional, hermetic ones, but "local"
    ones that run in the source code directory. the ray core rules are all
    converted to non-local, hermetic build rules, and we are deprecating the
    local `ray_pkg` one. as a result, for the java rules to run, one has to
    manually run the `bazel run //:gen_ray_pkg` to copy the ray core bits
    back to the source directory.
    
    Signed-off-by: Lonnie Liu <lonnie@anyscale.com>
    Signed-off-by: jugalshah291 <shah.jugal291@gmail.com>

--- java/test.sh
+++ java/test.sh
@@ -68,6 +68,9 @@ bazel build //java:copy_pom_files
 bazel build //java:cp_java_generated
 bazel build //java:gen_maven_deps
 
+echo "Build ray core."
+bazel run //:gen_ray_pkg
+
 echo "Build test jar."
 bazel build //java:all_tests_shaded.jar
 
