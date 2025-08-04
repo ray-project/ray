3ff51907309cb282be6ab5a21b56ab055bad7f4d
goutamvenkat-anyscale <goutam@anyscale.com>
2025-08-04 10:51:12 -0700

    [Data] Issue Detection Framework (#55155)
    <!-- Thank you for your contribution! Please review
    https://github.com/ray-project/ray/blob/master/CONTRIBUTING.rst before
    opening a pull request. -->
    
    <!-- Please add a reviewer to the assignee section when you create a PR.
    If you don't have the access to it, we will shortly find a reviewer and
    assign them to your PR. -->
    
    ## Why are these changes needed?
    
    This is a framework that enables tracking issues such as hanging
    operators, high memory usage etc. and embedding them into the streaming
    executor.
    
    ## Related issue number
    
    <!-- For example: "Closes #1234" -->
    
    ## Checks
    
    - [x] I've signed off every commit(by using the -s flag, i.e., `git
    commit -s`) in this PR.
    - [x] I've run `scripts/format.sh` to lint the changes in this PR.
    - [ ] I've included any doc changes needed for
    https://docs.ray.io/en/master/.
    - [ ] I've added any new APIs to the API Reference. For example, if I
    added a
    method in Tune, I've added it in `doc/source/tune/api/` under the
               corresponding `.rst` file.
    - [ ] I've made sure the tests are passing. Note that there might be a
    few flaky tests, see the recent failures at https://flakey-tests.ray.io/
    - Testing Strategy
       - [x] Unit tests
       - [ ] Release tests
       - [ ] This PR is not tested :(
    
    ---------
    
    Signed-off-by: Goutam V <goutam@anyscale.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

---
+++ python/ray/data/_internal/issue_detection/detectors/__init__.py
@@ -0,0 +1,15 @@
+from ray.data._internal.issue_detection.detectors.hanging_detector import (
+    HangingExecutionIssueDetector,
+    HangingExecutionIssueDetectorConfig,
+)
+from ray.data._internal.issue_detection.detectors.high_memory_detector import (
+    HighMemoryIssueDetector,
+    HighMemoryIssueDetectorConfig,
+)
+
+__all__ = [
+    "HangingExecutionIssueDetector",
+    "HangingExecutionIssueDetectorConfig",
+    "HighMemoryIssueDetector",
+    "HighMemoryIssueDetectorConfig",
+]
--- python/ray/data/context.py
+++ python/ray/data/context.py
@@ -17,6 +17,9 @@ from ray.util.scheduling_strategies import SchedulingStrategyT
 
 if TYPE_CHECKING:
     from ray.data._internal.execution.interfaces import ExecutionOptions
+    from ray.data._internal.issue_detection.issue_detector_configuration import (
+        IssueDetectorsConfiguration,
+    )
 
 logger = logging.getLogger(__name__)
 
@@ -280,6 +283,15 @@ def _deduce_default_shuffle_algorithm() -> ShuffleStrategy:
         return DEFAULT_SHUFFLE_STRATEGY
 
 
+def _issue_detectors_config_factory() -> "IssueDetectorsConfiguration":
+    # Lazily import to avoid circular dependencies.
+    from ray.data._internal.issue_detection.issue_detector_configuration import (
+        IssueDetectorsConfiguration,
+    )
+
+    return IssueDetectorsConfiguration()
+
+
 @DeveloperAPI
 @dataclass
 class DataContext:
@@ -519,6 +531,10 @@ class DataContext:
     # I.E., after the hook is triggered and the UDF is deleted, another
     # retry task may still be scheduled to this actor and it will fail.
     _enable_actor_pool_on_exit_hook: bool = False
+
+    issue_detectors_config: "IssueDetectorsConfiguration" = field(
+        default_factory=_issue_detectors_config_factory
+    )
 
     def __post_init__(self):
         # The additonal ray remote args that should be added to
