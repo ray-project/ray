3b904e219a31a055a4ce9a8b5e1f87e229da8345
avigyabb <avigyabb@stanford.edu>
2025-08-05 23:44:26 +0000

    working on test- unfinished
    Signed-off-by: avigyabb <avigyabb@stanford.edu>

--- python/ray/tests/test_gpu_objects_nccl.py
+++ python/ray/tests/test_gpu_objects_nccl.py
@@ -14,6 +14,9 @@ class GPUTestActor:
     def sum(self, data):
         return data.sum().item()
 
+    def increment(self, data):
+        return data + 1
+
 
 @pytest.mark.parametrize("ray_start_regular", [{"num_gpus": 2}], indirect=True)
 def test_p2p(ray_start_regular):
@@ -31,6 +34,25 @@ def test_p2p(ray_start_regular):
     # Trigger tensor transfer from src to dst actor
     remote_sum = ray.get(dst_actor.sum.remote(gpu_ref))
     assert tensor.sum().item() == remote_sum
+
+
+def test_duplicate_objectref_transfer(ray_start_regular):
+    """
+    This test checks that passes a GPU object ref to the same actor and a different actor.
+    """
+    world_size = 2
+    actors = [GPUTestActor.remote() for _ in range(world_size)]
+    create_collective_group(actors, backend="nccl")
+
+    actor0, actor1 = actors[0], actors[1]
+
+    small_tensor = torch.randn((1,))
+
+    ref = actor0.echo.remote(small_tensor)
+    result1 = actor0.increment.remote(ref)
+    result2 = actor1.increment.remote(ref)
+    assert ray.get(result1) == pytest.approx(small_tensor + 2)
+    assert ray.get(result2) == pytest.approx(small_tensor + 2)
 
 
 if __name__ == "__main__":
