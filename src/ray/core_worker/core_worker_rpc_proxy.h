326b37a757d580800509f6753af90df5fe17e6f4
joshlee <joshlee@anyscale.com>
2025-08-05 07:41:47 +0000

    Fixing error check
    Signed-off-by: joshlee <joshlee@anyscale.com>

--- src/ray/core_worker/core_worker_rpc_proxy.h
+++ src/ray/core_worker/core_worker_rpc_proxy.h
@@ -72,27 +72,24 @@ class CoreWorkerServiceHandlerProxy : public rpc::CoreWorkerServiceHandler {
 
   /// Wait until the worker is initialized.
   void WaitUntilInitialized() override {
-    absl::MutexLock lock(&core_worker_mutex_);
-    while (core_worker_ == nullptr) {
-      core_worker_cv_.WaitWithTimeout(&core_worker_mutex_, absl::Seconds(60));
-    }
-    // If the core worker initialization is hanging for 60 seconds, we can assume that
-    // something is wrong and we should exit the program.
-    if (core_worker_ == nullptr) {
+    auto is_initialized = [this]() { return this->core_worker_ != nullptr; };
+    if (!core_worker_mutex_.LockWhenWithTimeout(absl::Condition(&is_initialized),
+                                                absl::Seconds(60))) {
+      // If the core worker initialization is hanging for 60 seconds, we can assume that
+      // something is wrong and we should exit the program.
       RAY_LOG(FATAL) << "Core worker is not initialized after 60 seconds";
     }
+    core_worker_mutex_.Unlock();
   }
 
   void SetCoreWorker(CoreWorker *core_worker) {
     absl::MutexLock lock(&core_worker_mutex_);
     core_worker_ = core_worker;
-    core_worker_cv_.SignalAll();
   }
 
  private:
   absl::Mutex core_worker_mutex_;
-  absl::CondVar core_worker_cv_;
-  CoreWorker *core_worker_ = nullptr;
+  CoreWorker *core_worker_ ABSL_GUARDED_BY(core_worker_mutex_) = nullptr;
 };
 
 }  // namespace core
