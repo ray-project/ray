64f7f4d8c2951a21de0ebe39d98458775d8d5a38
zac <zac@anyscale.com>
2025-08-05 22:56:14 +0000

    align test env variables to what's used in docker CI

--- python/ray/tests/test_basic.py
+++ python/ray/tests/test_basic.py
@@ -249,8 +249,9 @@ def test_worker_thread_count(monkeypatch, shutdown_only):
             except ImportError:
                 return None
 
-    # Set the environment variable used by the raylet
+    # Set the environment variables used by the raylet and worker
     monkeypatch.setenv("RAY_worker_num_grpc_internal_threads", "1")
+    monkeypatch.setenv("RAY_num_server_call_thread", "1")
 
     # TODO: The for loop and the 'assert ... in {..,..}' complicates this
     # test unnecessarily. We should only need to call the assert after
@@ -262,7 +263,7 @@ def test_worker_thread_count(monkeypatch, shutdown_only):
         ray.get(actor.get_thread_count.remote())
     # Lowering these numbers in this assert should be celebrated,
     # increasing these numbers should be scrutinized
-    assert ray.get(actor.get_thread_count.remote()) in {27, 28}
+    assert ray.get(actor.get_thread_count.remote()) in {24, 25}
 
 
 # https://github.com/ray-project/ray/issues/7287
