53e3586f6fcf88dd5a3a046de2df29f640579e70
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-08-06 06:59:54 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/raylet_client/raylet_connection.h
+++ src/ray/raylet_client/raylet_connection.h
@@ -21,8 +21,8 @@
 #include <vector>
 
 #include "ray/common/asio/instrumented_io_context.h"
-#include "ray/ipc/client_connection.h"
 #include "ray/flatbuffers/node_manager_generated.h"
+#include "ray/ipc/client_connection.h"
 
 namespace ray::raylet {
 
