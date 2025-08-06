9123c45688395f102957730721db5b94d93294d5
Seiji Eicher <seiji@anyscale.com>
2025-08-06 11:58:10 -0700

    Remove constraint in setup.py
    Signed-off-by: Seiji Eicher <seiji@anyscale.com>

--- python/setup.py
+++ python/setup.py
@@ -363,7 +363,6 @@ if setup_spec.type == SetupType.RAY:
                 "async-timeout; python_version < '3.11'",
                 "typer",
                 "hf_transfer",
-                "transformers<4.54.0",  # Due to https://github.com/vllm-project/vllm-ascend/issues/2046
             ]
             + setup_spec.extras["data"]
             + setup_spec.extras["serve"]
