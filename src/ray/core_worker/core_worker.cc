e13b6bfd0fba716436a7000b017202a8dea80f21
Dhyey Shah <dhyey2019@gmail.com>
2025-08-06 13:48:28 -0700

    [core] Don't make an ipc on get if nothing to get from plasma (#55270)
    We'll make this call to the plasma store provider on a ray.get
    regardless of the size of plasma_object_ids.
    
    https://github.com/ray-project/ray/blob/f9d3ae612b370e5c8f92a446e3647679638ab829/src/ray/core_worker/core_worker.cc#L1462
    
    And then the plasma store provider will call this if it's empty. And
    UnblockIfNeeded could make an unnecessary ipc.
    
    https://github.com/ray-project/ray/blob/7c7222ef944078bf6c3a45519178a902b614e55b/src/ray/core_worker/store_provider/plasma_store_provider.cc#L312
    
    Signed-off-by: dayshah <dhyey2019@gmail.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -1448,7 +1448,7 @@ Status CoreWorker::GetObjects(const std::vector<ObjectID> &ids,
     }
   }
 
-  if (!got_exception) {
+  if (!got_exception && !plasma_object_ids.empty()) {
     // If any of the objects have been promoted to plasma, then we retry their
     // gets at the provider plasma. Once we get the objects from plasma, we flip
     // the transport type again and return them for the original direct call ids.
