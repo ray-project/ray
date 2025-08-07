c33205d1020e5cb2d3c5f70b1c7baa7ba39428f9
xgui <xgui@anyscale.com>
2025-08-07 20:11:28 +0000

    fix unittest
    Signed-off-by: xgui <xgui@anyscale.com>

--- python/ray/train/v2/tests/test_persistence.py
+++ python/ray/train/v2/tests/test_persistence.py
@@ -25,6 +25,7 @@ from ray.train import (
 )
 from ray.train.v2._internal.constants import HEALTH_CHECK_INTERVAL_S_ENV_VAR
 from ray.train.v2._internal.execution.storage import _download_from_fs_path
+from ray.train.v2._internal.execution.train_fn_utils import get_train_fn_utils
 from ray.train.v2.api.data_parallel_trainer import DataParallelTrainer
 
 
@@ -211,8 +212,8 @@ def train_fn(config):
         # which will cause the test assertions to fail.
         # This should be fixed by forcing a queue flush on all workers before
         # executing the failure decisions.
-        train_context = ray.train.get_context()
-        sync_actor = train_context.get_synchronization_actor()
+        sync_actor = get_train_fn_utils().get_synchronization_actor()
+        train_context = get_train_fn_utils().get_context()
         ray.get(
             sync_actor.broadcast_from_rank_zero.remote(
                 world_rank=train_context.get_world_rank(),
