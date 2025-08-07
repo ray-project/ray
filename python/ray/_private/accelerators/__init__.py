d1b7fce4f0be3403bfef1d4f902b6808c4125460
rebel-scottlee <scott.lee@rebellions.ai>
2025-08-07 23:49:41 +0900

    [Core] Add AcceleratorManager implementation for Rebellions NPU (#53985)
    Signed-off-by: Scott Lee <scott.lee@rebellions.ai>
    Signed-off-by: rebel-scottlee <scott.lee@rebellions.ai>
    Co-authored-by: Dhyey Shah <dhyey2019@gmail.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- python/ray/_private/accelerators/__init__.py
+++ python/ray/_private/accelerators/__init__.py
@@ -7,6 +7,7 @@ from ray._private.accelerators.intel_gpu import IntelGPUAcceleratorManager
 from ray._private.accelerators.neuron import NeuronAcceleratorManager
 from ray._private.accelerators.npu import NPUAcceleratorManager
 from ray._private.accelerators.nvidia_gpu import NvidiaGPUAcceleratorManager
+from ray._private.accelerators.rbln import RBLNAcceleratorManager
 from ray._private.accelerators.tpu import TPUAcceleratorManager
 
 
@@ -20,6 +21,7 @@ def get_all_accelerator_managers() -> Set[AcceleratorManager]:
         NeuronAcceleratorManager,
         HPUAcceleratorManager,
         NPUAcceleratorManager,
+        RBLNAcceleratorManager,
     }
 
 
@@ -71,6 +73,7 @@ __all__ = [
     "NeuronAcceleratorManager",
     "HPUAcceleratorManager",
     "NPUAcceleratorManager",
+    "RBLNAcceleratorManager",
     "get_all_accelerator_managers",
     "get_all_accelerator_resource_names",
     "get_accelerator_manager_for_resource",
