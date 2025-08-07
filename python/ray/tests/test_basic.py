923d43f999ea18dbf5472b967e5fc9359a3225df
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-08-07 10:23:54 -0500

    [core] Skip `test_worker_thread_count` on Windows (#55360)
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- python/ray/tests/test_basic.py
+++ python/ray/tests/test_basic.py
@@ -233,6 +233,7 @@ def test_default_worker_import_dependency(shutdown_only):
     ray.get(f.remote())
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="Windows thread count not policed.")
 def test_worker_thread_count(monkeypatch, shutdown_only):
     """This test will fail if the number of threads spawned by a worker process
     increases. If you find that a patch is now causing this test to fail,
