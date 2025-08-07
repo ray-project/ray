f1a92fe36ff98c7cb2d9bdc65c5fca51e7a50b92
Zac Policzer <zac@anyscale.com>
2025-08-06 21:10:53 -0700

    [core] Patch grpc with RAY_num_grpc_threads to control grpc thread count (#54988)
    Signed-off-by: Zac Policzer <zacattackftw@gmail.com>
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>
    Signed-off-by: zac <zac@anyscale.com>
    Co-authored-by: Jiajun Yao <jeromeyjj@gmail.com>
    Co-authored-by: Edward Oakes <ed.nmi.oakes@gmail.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- src/ray/common/ray_config_def.h
+++ src/ray/common/ray_config_def.h
@@ -946,10 +946,16 @@ RAY_CONFIG(bool, enable_infeasible_task_early_exit, false);
 // disconnects.
 RAY_CONFIG(int64_t, raylet_check_for_unexpected_worker_disconnect_interval_ms, 1000)
 
-/// The maximum time in seconds that an actor task can wait in the scheduling queue
-/// for tasks with smaller sequence numbers to show up. If timed out, the task will
-/// be cancelled.
+// The maximum time in seconds that an actor task can wait in the scheduling queue
+// for tasks with smaller sequence numbers to show up. If timed out, the task will
+// be cancelled.
 RAY_CONFIG(int64_t, actor_scheduling_queue_max_reorder_wait_seconds, 30)
 
 /// Timeout for raylet grpc server reconnection in seconds.
 RAY_CONFIG(int32_t, raylet_rpc_server_reconnect_timeout_s, 60)
+
+// The number of grpc threads spun up on the worker process. This config is consumed
+// by the raylet and then broadcast to the worker process at time of the worker
+// process getting spawned.  Setting to zero or less maintains the default
+// number of threads grpc will spawn.
+RAY_CONFIG(int64_t, worker_num_grpc_internal_threads, 0)
