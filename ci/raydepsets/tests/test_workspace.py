a3d624493a6c09408b650e0a4fa8a801674b479c
elliot-barn <elliot.barnwell@anyscale.com>
2025-08-07 23:59:26 +0000

    refactoring tests
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

---
+++ ci/raydepsets/tests/test_workspace.py
@@ -0,0 +1,33 @@
+import sys
+import pytest
+import tempfile
+from pathlib import Path
+from ci.raydepsets.workspace import Workspace
+from ci.raydepsets.tests.test_utils import copy_data_to_tmpdir
+
+
+def test_workspace_init():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        workspace = Workspace(tmpdir)
+        assert workspace.dir is not None
+
+
+def test_parse_build_arg_sets():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        copy_data_to_tmpdir(tmpdir)
+        workspace = Workspace(dir=tmpdir)
+        config = workspace.load_config(path=Path(tmpdir) / "test.depsets.yaml")
+        assert config.build_arg_sets[0].name == "py311_cpu"
+        assert config.build_arg_sets[0].build_args == {
+            "CUDA_VERSION": "cpu",
+            "PYTHON_VERSION": "py311",
+        }
+        assert config.build_arg_sets[1].name == "py311_cuda128"
+        assert config.build_arg_sets[1].build_args == {
+            "CUDA_VERSION": 128,
+            "PYTHON_VERSION": "py311",
+        }
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main(["-v", __file__]))
