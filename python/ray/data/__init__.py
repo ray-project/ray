6437c4566d37628e811bb4d75cdfde2a690c0f0d
Alexey Kudinkin <ak@anyscale.com>
2025-08-08 11:21:36 -0700

    `lint`
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/data/__init__.py
+++ python/ray/data/__init__.py
@@ -1,9 +1,6 @@
 # Short term workaround for https://github.com/ray-project/ray/issues/32435
 # Dataset has a hard dependency on pandas, so it doesn't need to be delayed.
 import pandas  # noqa
-from packaging.version import parse as parse_version
-
-from ray._private.arrow_utils import get_pyarrow_version
 
 from ray.data._internal.compute import ActorPoolStrategy
 from ray.data._internal.datasource.tfrecords_datasource import TFXReadOptions
