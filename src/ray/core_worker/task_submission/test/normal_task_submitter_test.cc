d54c8bbfd9a8513c9152c21899b1dcf662edd665
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-08-09 09:08:14 -0500

    compiling
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/core_worker/task_submission/test/normal_task_submitter_test.cc
+++ src/ray/core_worker/task_submission/test/normal_task_submitter_test.cc
@@ -30,7 +30,6 @@
 #include "ray/common/task/task_util.h"
 #include "ray/common/test_util.h"
 #include "ray/core_worker/store_provider/memory_store/memory_store.h"
-#include "ray/core_worker/store_provider/memory_store/memory_store.h"
 #include "ray/raylet_client/raylet_client.h"
 #include "ray/rpc/worker/core_worker_client.h"
 
