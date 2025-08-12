b360d93159e0ced58d25352ad75eff06529836d0
Cuong Nguyen <can@anyscale.com>
2025-08-12 10:46:39 -0700

    [core][obsclean/02] de-static all internal ray metrics
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- src/ray/object_manager/ownership_object_directory.h
+++ src/ray/object_manager/ownership_object_directory.h
@@ -28,6 +28,7 @@
 #include "ray/pubsub/subscriber.h"
 #include "ray/rpc/worker/core_worker_client.h"
 #include "ray/rpc/worker/core_worker_client_pool.h"
+#include "ray/stats/metric.h"
 
 namespace ray {
 
@@ -173,6 +174,45 @@ class OwnershipBasedObjectDirectory : public IObjectDirectory {
   double metrics_num_object_location_updates_per_second_ = 0;
 
   uint64_t cum_metrics_num_object_location_updates_ = 0;
+
+  /// Ray metrics
+  ray::stats::Gauge ray_metric_object_directory_location_subscriptions_{
+      /*name=*/"object_directory_subscriptions",
+      /*description=*/
+      "Number of object location subscriptions. If this is high, the raylet is "
+      "attempting "
+      "to pull a lot of objects.",
+      /*unit=*/"subscriptions"};
+
+  ray::stats::Gauge ray_metric_object_directory_location_updates_{
+      /*name=*/"object_directory_updates",
+      /*description=*/
+      "Number of object location updates per second., If this is high, the raylet is "
+      "attempting to pull a lot of objects and/or the locations for objects are "
+      "frequently "
+      "changing (e.g. due to many object copies or evictions).",
+      /*unit=*/"updates"};
+
+  ray::stats::Gauge ray_metric_object_directory_location_lookups_{
+      /*name=*/"object_directory_lookups",
+      /*description=*/
+      "Number of object location lookups per second. If this is high, the raylet is "
+      "waiting on a lot of objects.",
+      /*unit=*/"lookups"};
+
+  ray::stats::Gauge ray_metric_object_directory_location_added_{
+      /*name=*/"object_directory_added_locations",
+      /*description=*/
+      "Number of object locations added per second., If this is high, a lot of objects "
+      "have been added on this node.",
+      /*unit=*/"additions"};
+
+  ray::stats::Gauge ray_metric_object_directory_location_removed_{
+      /*name=*/"object_directory_removed_locations",
+      /*description=*/
+      "Number of object locations removed per second. If this is high, a lot of objects "
+      "have been removed from this node.",
+      /*unit=*/"removals"};
 
   friend class OwnershipBasedObjectDirectoryTest;
 };
