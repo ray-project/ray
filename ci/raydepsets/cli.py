0e833cceffbff18f4dbb1a0e34ab853322baa708
elliot-barn <elliot.barnwell@anyscale.com>
2025-08-12 17:32:29 +0000

    moving doc string to option descriptions
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/raydepsets/cli.py
+++ ci/raydepsets/cli.py
@@ -32,10 +32,24 @@ def cli():
 
 @cli.command()
 @click.argument("config_path", default="ci/raydepsets/ray.depsets.yaml")
-@click.option("--workspace-dir", default=None)
-@click.option("--name", default=None)
-@click.option("--build-arg-set", default=None)
-@click.option("--uv-cache-dir", default=None)
+@click.option(
+    "--workspace-dir",
+    default=None,
+    help="The path to the workspace directory. If not specified, $BUILD_WORKSPACE_DIRECTORY will be used.",
+)
+@click.option(
+    "--name",
+    default=None,
+    help="The name of the dependency set to load. If not specified, all dependency sets will be loaded.",
+)
+@click.option(
+    "--build-arg-set",
+    default=None,
+    help="The name of the build arg set to use. If not specified, a depset matching the name with no build arg set will be loaded.",
+)
+@click.option(
+    "--uv-cache-dir", default=None, help="The directory to cache uv dependencies"
+)
 def load(
     config_path: str,
     workspace_dir: Optional[str],
@@ -44,17 +58,9 @@ def load(
     uv_cache_dir: Optional[str],
 ):
     """
-    Load a dependency sets from a config file.
+    Load dependency sets from a config file.
     Args:
-        config_path: The path to the config file.
-        workspace_dir: The path to the workspace directory.
-        name: The name of the dependency set to load.
-        build_arg_set: The name of the build arg set to use.
-    User can specify a name and build arg set to load a single dependency set.
-    If no name is specified, all dependency sets will be loaded.
-    If no build arg set is specified, a depset matching the name with no build arg set will be loaded.
-    If no workspace directory is specified, $BUILD_WORKSPACE_DIRECTORY will be used.
-    If no uv cache directory is specified, the default uv cache directory will be used.
+        config_path: The path to the config file. If not specified, ci/raydepsets/ray.depsets.yaml will be used.
     """
     manager = DependencySetManager(
         config_path=config_path,
