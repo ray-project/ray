6a6e494fc6979e6b821ba3c2e3fac905bbc81890
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2025-08-13 13:21:41 -0700

    [release test] remove "multi" test frequency (#55561)
    not used anywhere any more
    
    Signed-off-by: Lonnie Liu <lonnie@anyscale.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- release/ray_release/buildkite/settings.py
+++ release/ray_release/buildkite/settings.py
@@ -11,7 +11,6 @@ from ray_release.wheels import DEFAULT_BRANCH, get_buildkite_repo_branch
 class Frequency(enum.Enum):
     MANUAL = enum.auto()
     ANY = enum.auto()
-    MULTI = enum.auto()
     NIGHTLY = enum.auto()
     NIGHTLY_3x = enum.auto()
     WEEKLY = enum.auto()
@@ -22,7 +21,6 @@ frequency_str_to_enum = {
     "manual": Frequency.MANUAL,
     "any": Frequency.ANY,
     "any-smoke": Frequency.ANY,
-    "multi": Frequency.MULTI,
     "nightly": Frequency.NIGHTLY,
     "nightly-3x": Frequency.NIGHTLY_3x,
     "weekly": Frequency.WEEKLY,
