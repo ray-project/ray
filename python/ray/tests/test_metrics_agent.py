a840a16d517be96b691f9dfb1d28d956ab98cb49
Mengjin Yan <mengjinyan3@gmail.com>
2025-08-13 11:57:54 -0700

    [Core][TaskEventFollowup/03] Improve the Target Http Endpoint in Aggregator Agent (#55529)
    This PR improves the target http endpoint in the aggregator_agent.py:
    
    Merge the address and port as one env var to specify the target http endpoint
    Set the default value of the endpoint to be empty. And only when the endpoint is specified, we send the events out to the endpoint
    Update corresponding tests
    -----------
    
    Signed-off-by: Mengjin Yan <mengjinyan3@gmail.com>
    Signed-off-by: myan <myan@anyscale.com>
    Signed-off-by: jugalshah291 <shah.jugal291@gmail.com>

--- python/ray/tests/test_metrics_agent.py
+++ python/ray/tests/test_metrics_agent.py
@@ -144,6 +144,7 @@ _EVENT_AGGREGATOR_METRICS = [
     "ray_event_aggregator_agent_events_failed_to_add_to_aggregator_total",
     "ray_event_aggregator_agent_events_dropped_at_event_aggregator_total",
     "ray_event_aggregator_agent_events_published_total",
+    "ray_event_aggregator_agent_events_filtered_out_total",
 ]
 
 _NODE_METRICS = [
@@ -465,6 +466,10 @@ def test_metrics_export_node_metrics(shutdown_only):
 
 
 _EVENT_AGGREGATOR_AGENT_TARGET_PORT = find_free_port()
+_EVENT_AGGREGATOR_AGENT_TARGET_IP = "127.0.0.1"
+_EVENT_AGGREGATOR_AGENT_TARGET_ADDR = (
+    f"http://{_EVENT_AGGREGATOR_AGENT_TARGET_IP}:{_EVENT_AGGREGATOR_AGENT_TARGET_PORT}"
+)
 
 
 @pytest.fixture(scope="module")
@@ -477,8 +482,11 @@ def httpserver_listen_address():
     [
         {
             "env_vars": {
-                "RAY_DASHBOARD_AGGREGATOR_AGENT_MAX_EVENT_BUFFER_SIZE": 1,
-                "RAY_DASHBOARD_AGGREGATOR_AGENT_EVENT_SEND_PORT": _EVENT_AGGREGATOR_AGENT_TARGET_PORT,
+                "RAY_DASHBOARD_AGGREGATOR_AGENT_MAX_EVENT_BUFFER_SIZE": 2,
+                "RAY_DASHBOARD_AGGREGATOR_AGENT_EVENTS_EXPORT_ADDR": _EVENT_AGGREGATOR_AGENT_TARGET_ADDR,
+                # Turn off task events generation to avoid the task events from the
+                # cluster impacting the test result
+                "RAY_task_events_report_interval_ms": 0,
             },
         },
     ],
@@ -505,16 +513,18 @@ def test_metrics_export_event_aggregator_agent(
             "ray_event_aggregator_agent_events_failed_to_add_to_aggregator_total",
             "ray_event_aggregator_agent_events_dropped_at_event_aggregator_total",
             "ray_event_aggregator_agent_events_published_total",
+            "ray_event_aggregator_agent_events_filtered_out_total",
         ]
         return all(metric in metrics_names for metric in event_aggregator_metrics)
 
     def test_case_value_correct():
         _, _, metric_samples = fetch_prometheus(prom_addresses)
         expected_metrics_values = {
-            "ray_event_aggregator_agent_events_received_total": 2.0,
+            "ray_event_aggregator_agent_events_received_total": 3.0,
             "ray_event_aggregator_agent_events_failed_to_add_to_aggregator_total": 0.0,
             "ray_event_aggregator_agent_events_dropped_at_event_aggregator_total": 1.0,
             "ray_event_aggregator_agent_events_published_total": 1.0,
+            "ray_event_aggregator_agent_events_filtered_out_total": 1.0,
         }
         for descriptor, expected_value in expected_metrics_values.items():
             samples = [m for m in metric_samples if m.name == descriptor]
@@ -543,10 +553,18 @@ def test_metrics_export_event_aggregator_agent(
                 RayEvent(
                     event_id=b"2",
                     source_type=RayEvent.SourceType.CORE_WORKER,
-                    event_type=RayEvent.EventType.TASK_DEFINITION_EVENT,
+                    event_type=RayEvent.EventType.TASK_PROFILE_EVENT,
                     timestamp=timestamp,
                     severity=RayEvent.Severity.INFO,
                     message="hello 2",
+                ),
+                RayEvent(
+                    event_id=b"3",
+                    source_type=RayEvent.SourceType.CORE_WORKER,
+                    event_type=RayEvent.EventType.TASK_DEFINITION_EVENT,
+                    timestamp=timestamp,
+                    severity=RayEvent.Severity.INFO,
+                    message="hello 3",
                 ),
             ],
             task_events_metadata=TaskEventsMetadata(
