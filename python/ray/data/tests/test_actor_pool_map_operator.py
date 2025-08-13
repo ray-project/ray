7423301effc8ab518c8556c86ca0f381cf503d20
Alexey Kudinkin <ak@anyscale.com>
2025-08-13 14:32:25 -0400

    [Data] Fixing `AutoscalingActorPool` to properly downscale upon completion of the execution (#55565)
    <!-- Thank you for your contribution! Please review
    https://github.com/ray-project/ray/blob/master/CONTRIBUTING.rst before
    opening a pull request. -->
    
    <!-- Please add a reviewer to the assignee section when you create a PR.
    If you don't have the access to it, we will shortly find a reviewer and
    assign them to your PR. -->
    
    ## Why are these changes needed?
    
    In 2.48 change introduced debouncing handling that disallows downscaling
    for Actor Pool for 30s after latest upscaling to give AP Operator enough
    time to start utilizing upscaled actor.
    
    However, that affected ability of the Actor Pool to downscale upon
    completion of the execution: when operator completes execution it should
    start downscaling immediately. This change addresses that.
    
    ## Related issue number
    
    <!-- For example: "Closes #1234" -->
    
    ## Checks
    
    - [ ] I've signed off every commit(by using the -s flag, i.e., `git
    commit -s`) in this PR.
    - [ ] I've run `scripts/format.sh` to lint the changes in this PR.
    - [ ] I've included any doc changes needed for
    https://docs.ray.io/en/master/.
    - [ ] I've added any new APIs to the API Reference. For example, if I
    added a
    method in Tune, I've added it in `doc/source/tune/api/` under the
               corresponding `.rst` file.
    - [ ] I've made sure the tests are passing. Note that there might be a
    few flaky tests, see the recent failures at https://flakey-tests.ray.io/
    - Testing Strategy
       - [ ] Unit tests
       - [ ] Release tests
       - [ ] This PR is not tested :(
    
    ---------
    
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- python/ray/data/tests/test_actor_pool_map_operator.py
+++ python/ray/data/tests/test_actor_pool_map_operator.py
@@ -4,6 +4,7 @@ import datetime
 import threading
 import time
 import unittest
+from dataclasses import replace
 from typing import Any, Dict, Optional, Tuple
 from unittest.mock import MagicMock
 
@@ -161,7 +162,11 @@ class TestActorPool(unittest.TestCase):
             pool.scale(ActorPoolScalingRequest(delta=1, reason="scaling up"))
             # Assert we can't scale down immediately after scale up
             assert not pool._can_apply(downscaling_request)
-            assert pool._last_upscaling_ts == time.time()
+            assert pool._last_upscaled_at == time.time()
+
+            # Check that we can still scale down if downscaling request
+            # is a forced one
+            assert pool._can_apply(replace(downscaling_request, force=True))
 
             # Advance clock
             f.tick(
