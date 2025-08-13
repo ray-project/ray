d89360353b7ef913d1cee684816a3bca8fe3da0b
Divyam Raj <divyamraj@MBP-FNJP4QXHF5.local>
2025-08-13 23:45:34 +0530

    [core][gpu-objects] Fail fetch after 600s not 60s #55512
    Signed-off-by: Divyam Raj <divyamraj18@gmail.com>

--- python/ray/_private/ray_constants.py
+++ python/ray/_private/ray_constants.py
@@ -593,10 +593,18 @@ RAY_EXPERIMENTAL_ENABLE_OPEN_TELEMETRY_ON_CORE = env_bool(
     "RAY_experimental_enable_open_telemetry_on_core", False
 )
 
+# How long to wait for a fetch to complete during ray.get before warning the user.
+#
+# NOTE: This must be kept in sync with the C++ definition of
+# `RayConfig::fetch_warn_timeout_milliseconds`.
+FETCH_WARN_TIMEOUT_SECONDS = (
+    env_integer("RAY_fetch_warn_timeout_milliseconds", 60000) / 1000
+)
+
 # How long to wait for a fetch to complete during ray.get before timing out and raising an exception to the user.
 #
 # NOTE: This must be kept in sync with the C++ definition of
 # `RayConfig::fetch_fail_timeout_milliseconds`.
 FETCH_FAIL_TIMEOUT_SECONDS = (
-    env_integer("RAY_fetch_fail_timeout_milliseconds", 60000) / 1000
+    env_integer("RAY_fetch_fail_timeout_milliseconds", 600000) / 1000
 )
