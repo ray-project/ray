c8bad40c306dff71ff3d65dd96c53b69b2324399
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-08-14 14:00:14 -0500

    WIP
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/core_worker/test/core_worker_test.cc
+++ src/ray/core_worker/test/core_worker_test.cc
@@ -37,7 +37,7 @@
 #include "ray/core_worker/future_resolver.h"
 #include "ray/core_worker/object_recovery_manager.h"
 #include "ray/core_worker/reference_count.h"
-#include "ray/core_worker/store_provider/memory_store/memory_store.h"
+#include "ray/core_worker/store_provider/memory_store.h"
 #include "ray/core_worker/task_submission/actor_task_submitter.h"
 #include "ray/core_worker/task_submission/normal_task_submitter.h"
 #include "ray/rpc/worker/core_worker_client_pool.h"
