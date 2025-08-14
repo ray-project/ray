799ccb72345b4665eefca99996da1580bbd21cf7
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-08-14 11:05:28 -0400

    abs path
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/core_worker/task_submission/actor_task_submitter.h
+++ src/ray/core_worker/task_submission/actor_task_submitter.h
@@ -30,6 +30,7 @@
 #include "ray/common/asio/asio_util.h"
 #include "ray/common/id.h"
 #include "ray/core_worker/actor_creator.h"
+#include "ray/core_worker/reference_count.h"
 #include "ray/core_worker/store_provider/memory_store.h"
 #include "ray/core_worker/task_submission/actor_submit_queue.h"
 #include "ray/core_worker/task_submission/dependency_resolver.h"
