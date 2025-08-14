3e926c4b0f1e2e5847fdaa234a0a301f39807bd3
mattip <matti.picus@gmail.com>
2025-08-14 11:11:06 +0300

    typo
    Signed-off-by: mattip <matti.picus@gmail.com>

--- python/ray/tests/test_basic.py
+++ python/ray/tests/test_basic.py
@@ -266,7 +266,7 @@ def test_worker_thread_count(monkeypatch, shutdown_only):
         ray.get(actor.get_thread_count.remote())
     # Lowering these numbers in this assert should be celebrated,
     # increasing these numbers should be scrutinized
-    assert ray.get(actor.get_thread_count.remote()) in {24, 25,, 26}
+    assert ray.get(actor.get_thread_count.remote()) in {24, 25, 26}
 
 
 # https://github.com/ray-project/ray/issues/7287
