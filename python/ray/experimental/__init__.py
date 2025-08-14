964b689d2f2ec7a166b580dfafe7175249f8116a
Stephanie Wang <smwang@cs.washington.edu>
2025-08-14 10:02:16 -0700

    [core][gpu-object] Add a user-facing call to wait for tensor to be freed (#55076)
    This adds a call `ray.experimental.wait_tensor_freed` that allows user
    code to check when a tensor that it put into Ray's GPU object store has
    been freed. Unlike the normal Ray object store, the GPU object store is
    just a Python data structure on the actor, which allows us to avoid
    copying. This means that the actor can keep a reference to an object in
    its store. The API call allows the actor to check when the object has
    been freed from the store, so that it can safely write to the tensor
    again.
    
    Closes #52341.
    
    ---------
    
    Signed-off-by: Stephanie wang <smwang@cs.washington.edu>
    Signed-off-by: Stephanie Wang <smwang@cs.washington.edu>
    Co-authored-by: Kai-Hsun Chen <kaihsun@apache.org>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- python/ray/experimental/__init__.py
+++ python/ray/experimental/__init__.py
@@ -1,10 +1,11 @@
 from ray.experimental.dynamic_resources import set_resource
 from ray.experimental.locations import get_local_object_locations, get_object_locations
-from ray.experimental.gpu_object_manager import GPUObjectManager
+from ray.experimental.gpu_object_manager import GPUObjectManager, wait_tensor_freed
 
 __all__ = [
     "get_object_locations",
     "get_local_object_locations",
     "set_resource",
     "GPUObjectManager",
+    "wait_tensor_freed",
 ]
