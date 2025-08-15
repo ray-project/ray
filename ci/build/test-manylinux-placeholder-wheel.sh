9a5d5a1375a5496c974ff7cde636b740305b5dcd
elliot-barn <elliot.barnwell@anyscale.com>
2025-08-15 21:05:03 +0000

    outputing python version
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/build/test-manylinux-placeholder-wheel.sh
+++ ci/build/test-manylinux-placeholder-wheel.sh
@@ -6,6 +6,7 @@ set -e
 # Show explicitly which commands are currently running.
 set -x
 
+which python
 ROOT_DIR=$(cd "$(dirname "$0")/$(dirname "$(test -L "$0" && readlink "$0" || echo "/")")"; pwd)
 
 echo "ROOT_DIR: $ROOT_DIR"
@@ -23,11 +24,11 @@ for ((i=0; i<${#PY_MMS[@]}; ++i)); do
 
   PY_WHEEL_VERSION="${PY_WHEEL_VERSIONS[i]}"
 
-  PYTHON_EXE="$MACPYTHON_PY_PREFIX/$PY_MM/bin/python$PY_MM"
+  PYTHON_EXE="opt/python/$PY_MM/bin/python$PY_MM"
   PIP_CMD="$(dirname "$PYTHON_EXE")/pip$PY_MM"
 
   # Find the appropriate wheel by grepping for the Python version.
-  PYTHON_WHEEL="$(printf "%s\n" "$ROOT_DIR"/../../dist/*"cp$PY_WHEEL_VERSION-cp$PY_WHEEL_VERSION"* | head -n 1)"
+  PYTHON_WHEEL="$(printf "%s\n" "$ROOT_DIR"/../../.whl/*"cp$PY_WHEEL_VERSION-cp$PY_WHEEL_VERSION"* | head -n 1)"
   echo "PYTHON_WHEEL: $PYTHON_WHEEL"
 
   # Print some env info
