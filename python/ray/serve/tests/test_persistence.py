1fbb439e3738d2b6a635bcf45c0c50c7cdf08c26
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-08-15 13:37:38 -0500

    easy
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/serve/tests/test_persistence.py
+++ python/ray/serve/tests/test_persistence.py
@@ -4,7 +4,8 @@ from ray._private.test_utils import run_string_as_driver
 
 
 def test_new_driver(serve_instance):
-    run_string_as_driver("""
+    run_string_as_driver(
+        """
 import ray
 ray.init(address="{}", namespace="default_test_namespace")
 
@@ -16,8 +17,9 @@ def driver():
 
 serve.run(driver.bind(), name="app")
 """.format(
-        ray.get_runtime_context().gcs_address,
-    ))
+            ray.get_runtime_context().gcs_address,
+        )
+    )
 
     handle = serve.get_app_handle("app")
     assert handle.remote().result() == "OK!"
