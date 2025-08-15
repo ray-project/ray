aaad1d13f095c31b786649b674bb136724a82e9f
Linkun <github@lkchen.net>
2025-08-15 15:03:47 -0700

    adapt master
    Signed-off-by: Linkun <github@lkchen.net>

--- ci/compile_llm_requirements.sh
+++ ci/compile_llm_requirements.sh
@@ -2,17 +2,6 @@
 
 set -euo pipefail
 
-SED_CMD=sed
-# On macos, use gnu-sed because 'sed -i' may not be used with stdin
-if [[ "$(uname)" == "Darwin" ]]; then
-	# check if gsed is available
-	if ! command -v gsed &> /dev/null; then
-		echo "--- gsed is not installed. Install via `brew install gnu-sed`"
-		exit 1
-	fi
-	SED_CMD=gsed
-fi
-
 PYTHON_CODE="$(python -c "import sys; v=sys.version_info; print(f'py{v.major}{v.minor}')")"
 if [[ "${PYTHON_CODE}" != "py311" ]]; then
 	echo "--- Python version is not 3.11"
@@ -24,8 +13,8 @@ mkdir -p /tmp/ray-deps
 
 # Remove the GPU constraints
 cp python/requirements_compiled.txt /tmp/ray-deps/requirements_compiled.txt
-sed -i '/^--extra-index-url /d' /tmp/ray-deps/requirements_compiled.txt
-sed -i '/^--find-links /d' /tmp/ray-deps/requirements_compiled.txt
+sed -e '/^--extra-index-url /d' -e '/^--find-links /d' /tmp/ray-deps/requirements_compiled.txt > /tmp/ray-deps/requirements_compiled.txt.tmp
+mv /tmp/ray-deps/requirements_compiled.txt.tmp /tmp/ray-deps/requirements_compiled.txt
 
 bazel run //ci/raydepsets:raydepsets -- build ci/raydepsets/rayllm.depsets.yaml
 
