7e0f391100a1222ddd41484d6be417af78854ff1
kourosh hakhamaneshi <31483498+kouroshHakha@users.noreply.github.com>
2025-08-16 03:19:20 +0200

    [serve.llm] Add reset_prefix_cache remote method to llm server (#55658)
    Signed-off-by: Kourosh Hakhamaneshi <kourosh@anyscale.com>
    Signed-off-by: jugalshah291 <shah.jugal291@gmail.com>

--- python/ray/llm/_internal/serve/deployments/llm/vllm/vllm_engine.py
+++ python/ray/llm/_internal/serve/deployments/llm/vllm/vllm_engine.py
@@ -450,3 +450,7 @@ class VLLMEngine(LLMEngine):
         except BaseException as e:
             logger.error("Healthcheck failed. The replica will be restarted")
             raise e from None
+
+    async def reset_prefix_cache(self) -> None:
+        assert self._engine_client is not None, "engine_client is not initialized"
+        await self._engine_client.reset_prefix_cache()
