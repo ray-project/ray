b1379bb354714a0e88bcf2699eebd51c17b56953
omkar <omkar@anyscale.com>
2025-08-16 21:47:12 +0000

    fix: used deployment mode to detect if DI or standalone
    Signed-off-by: omkar <omkar@anyscale.com>

--- python/ray/serve/tests/test_standalone_3.py
+++ python/ray/serve/tests/test_standalone_3.py
@@ -22,6 +22,13 @@ from ray.tests.conftest import call_ray_stop_only  # noqa: F401
 from ray.util.state import list_actors
 
 
+# Some tests are not possible to run if proxy is not available on every node.
+# We skip them if proxy is not available.
+def is_proxy_on_every_node() -> bool:
+    client = _get_global_client()
+    return client._http_config.location == "EveryNode"
+
+
 @pytest.fixture
 def shutdown_ray():
     if ray.is_initialized():
@@ -286,8 +293,8 @@ def test_autoscaler_shutdown_node_http_everynode(
 
     serve.run(A.bind(), name="app_f")
 
-    # 2 proxies, 1 controller, 2 replicas.
-    wait_for_condition(lambda: len(list_actors()) == 5)
+    expected_actors = 4 if not is_proxy_on_every_node() else 5
+    wait_for_condition(lambda: len(list_actors()) == expected_actors)
     assert len(ray.nodes()) == 2
 
     # Stop all deployment replicas.
@@ -347,8 +354,10 @@ def test_drain_and_undrain_http_proxy_actors(
 
     serve.run(HelloModel.options(num_replicas=2).bind())
 
+    expected_actor_count = 4 if not is_proxy_on_every_node() else 6
+
     # 3 proxies, 1 controller, 2 replicas.
-    wait_for_condition(lambda: len(list_actors()) == 6)
+    wait_for_condition(lambda: len(list_actors()) == expected_actor_count)
     assert len(ray.nodes()) == 3
 
     client = _get_global_client()
@@ -356,7 +365,8 @@ def test_drain_and_undrain_http_proxy_actors(
         **ray.get(client._controller.get_serve_instance_details.remote())
     )
     proxy_actor_ids = {proxy.actor_id for _, proxy in serve_details.proxies.items()}
-    assert len(proxy_actor_ids) == 3
+    expected_proxy_actor_count = 3 if is_proxy_on_every_node() else 1
+    assert len(proxy_actor_ids) == expected_proxy_actor_count
 
     serve.run(HelloModel.options(num_replicas=1).bind())
     # 1 proxy should be draining
@@ -426,8 +436,8 @@ def test_controller_shutdown_gracefully(
     model = HelloModel.bind()
     serve.run(target=model)
 
-    # Ensure total actors of 2 proxies, 1 controller, and 2 replicas
-    wait_for_condition(lambda: len(list_actors()) == 5)
+    expected_actors = 4 if not is_proxy_on_every_node() else 5
+    wait_for_condition(lambda: len(list_actors()) == expected_actors)
     assert len(ray.nodes()) == 2
 
     # Call `graceful_shutdown()` on the controller, so it will start shutdown.
@@ -485,8 +495,9 @@ def test_client_shutdown_gracefully_when_timeout(
     model = HelloModel.bind()
     serve.run(target=model)
 
-    # Ensure total actors of 2 proxies, 1 controller, and 2 replicas
-    wait_for_condition(lambda: len(list_actors()) == 5)
+    # Check expected actors based on mode
+    expected_actors = 5 if is_proxy_on_every_node() else 4
+    wait_for_condition(lambda: len(list_actors()) == expected_actors)
     assert len(ray.nodes()) == 2
 
     # Ensure client times out if the controller does not shutdown within timeout.
