271ee51d987a7eafb2f047cb45dd651d1e893f30
JasonLi1909 <jasli1909@gmail.com>
2025-08-15 17:46:36 -0700

    cleaned imports to use import ray.train
    Signed-off-by: JasonLi1909 <jasli1909@gmail.com>

--- python/ray/train/lightning/_lightning_utils.py
+++ python/ray/train/lightning/_lightning_utils.py
@@ -9,7 +9,7 @@ import torch
 from packaging.version import Version
 
 import ray
-import ray.train as train
+import ray.train
 from ray._common.usage.usage_lib import TagKey, record_extra_usage_tag
 from ray.train import Checkpoint
 from ray.util import PublicAPI
@@ -182,16 +182,16 @@ class RayLightningEnvironment(LightningEnvironment):  # noqa: F821
         record_extra_usage_tag(TagKey.TRAIN_LIGHTNING_RAYLIGHTNINGENVIRONMENT, "1")
 
     def world_size(self) -> int:
-        return train.get_context().get_world_size()
+        return ray.train.get_context().get_world_size()
 
     def global_rank(self) -> int:
-        return train.get_context().get_world_rank()
+        return ray.train.get_context().get_world_rank()
 
     def local_rank(self) -> int:
-        return train.get_context().get_local_rank()
+        return ray.train.get_context().get_local_rank()
 
     def node_rank(self) -> int:
-        return train.get_context().get_node_rank()
+        return ray.train.get_context().get_node_rank()
 
     def set_world_size(self, size: int) -> None:
         # Disable it since `world_size()` directly returns data from Train context.
@@ -260,8 +260,8 @@ class RayTrainReportCallback(pl.callbacks.Callback):
     def __init__(self) -> None:
         super().__init__()
         job_id = ray.get_runtime_context().get_job_id()
-        experiment_name = train.get_context().get_experiment_name()
-        self.local_rank = train.get_context().get_local_rank()
+        experiment_name = ray.train.get_context().get_experiment_name()
+        self.local_rank = ray.train.get_context().get_local_rank()
 
         self.tmpdir_prefix = Path(
             tempfile.gettempdir(),
@@ -291,7 +291,7 @@ class RayTrainReportCallback(pl.callbacks.Callback):
 
         # Report to train session
         checkpoint = Checkpoint.from_directory(tmpdir)
-        train.report(metrics=metrics, checkpoint=checkpoint)
+        ray.train.report(metrics=metrics, checkpoint=checkpoint)
 
         # Add a barrier to ensure all workers finished reporting here
         trainer.strategy.barrier()
