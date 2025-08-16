f6045033adc9b94d3264eb739718928c6d694e4d
dayshah <dhyey2019@gmail.com>
2025-08-16 09:49:14 -0700

    [core] Update base exception group tests
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- python/ray/tests/test_exceptiongroup.py
+++ python/ray/tests/test_exceptiongroup.py
@@ -21,7 +21,7 @@ def test_baseexceptiongroup_task(ray_start_regular):
     def task():
         raise baseexceptiongroup
 
-    with pytest.raises(ray.exceptions.WorkerCrashedError):
+    with pytest.raises(ray.exceptions.BaseExceptionGroup):
         ray.get(task.remote())
 
 
@@ -35,7 +35,7 @@ def test_baseexceptiongroup_actor(ray_start_regular):
         def f(self):
             raise baseexceptiongroup
 
-    with pytest.raises(ray.exceptions.ActorDiedError):
+    with pytest.raises(ray.exceptions.BaseExceptionGroup):
         a = Actor.remote()
         ray.get(a.f.remote())
 
