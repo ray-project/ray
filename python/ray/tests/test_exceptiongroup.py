e1dcf653452fa3a82b604ed69042ab4e35c1566a
dayshah <dhyey2019@gmail.com>
2025-08-16 17:09:20 +0000

    fix
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- python/ray/tests/test_exceptiongroup.py
+++ python/ray/tests/test_exceptiongroup.py
@@ -21,7 +21,7 @@ def test_baseexceptiongroup_task(ray_start_regular):
     def task():
         raise baseexceptiongroup
 
-    with pytest.raises(BaseExceptionGroup):  # noqa: F821
+    with pytest.raises(ray.exceptions.RayTaskError):  # noqa: F821
         ray.get(task.remote())
 
 
@@ -35,7 +35,7 @@ def test_baseexceptiongroup_actor(ray_start_regular):
         def f(self):
             raise baseexceptiongroup
 
-    with pytest.raises(BaseExceptionGroup):  # noqa: F821
+    with pytest.raises(ray.exceptions.RayTaskError):  # noqa: F821
         a = Actor.remote()
         ray.get(a.f.remote())
 
