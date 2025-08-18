23a52a08bef21c173bbfdba68a75920940185a26
elliot-barn <elliot.barnwell@anyscale.com>
2025-08-18 19:48:16 +0000

    compiling ray img lock files
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

---
+++ ci/compile_ray_img_requirements.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+PYTHON_CODE="$(python -c "import sys; v=sys.version_info; print(f'py{v.major}{v.minor}')")"
+if [[ "${PYTHON_CODE}" != "py311" ]]; then
+	echo "--- Python version is not 3.11"
+	echo "--- Current Python version: ${PYTHON_CODE}"
+	exit 1
+fi
+
+mkdir -p /tmp/ray-deps
+
+echo "ray==3.0.0.dev0" > /tmp/ray-deps/requirements.txt
+
+# Remove the GPU constraints
+cp python/requirements_compiled.txt /tmp/ray-deps/requirements_compiled.txt
+sed -i '/^--extra-index-url /d' /tmp/ray-deps/requirements_compiled.txt
+sed -i '/^--find-links /d' /tmp/ray-deps/requirements_compiled.txt
+
+bazel run //ci/raydepsets:raydepsets -- build ci/raydepsets/rayimg.depsets.yaml
+
+echo "--- Done"
