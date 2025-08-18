3a4a6df305fc4f9d8f743832a2bfec76ba7c389d
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-08-18 15:17:22 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/gcs/gcs_server/gcs_server.cc
+++ src/ray/gcs/gcs_server/gcs_server.cc
@@ -129,7 +129,7 @@ GcsServer::GcsServer(const ray::gcs::GcsServerConfig &config,
         std::make_unique<ObservableStoreClient>(std::make_unique<InMemoryStoreClient>());
     break;
   case StorageType::REDIS_PERSIST: {
-    store_client = std::make_unique<RedisStoreClient>(CreateRedisClient(io_context));
+    store_client = std::make_unique<RedisStoreClient>(CreateRedisClient(io_context), io_context);
     break;
   }
   default:
@@ -586,7 +586,7 @@ void GcsServer::InitKVManager() {
   switch (storage_type_) {
   case (StorageType::REDIS_PERSIST):
     instance = std::make_unique<StoreClientInternalKV>(
-        std::make_unique<RedisStoreClient>(CreateRedisClient(io_context)));
+        std::make_unique<RedisStoreClient>(CreateRedisClient(io_context), io_context));
     break;
   case (StorageType::IN_MEMORY):
     instance = std::make_unique<StoreClientInternalKV>(
