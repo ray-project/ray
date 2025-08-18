c87f5a988485585657474373cf53d0ac40513237
cong.qian <cong.qian@anyscale.com>
2025-08-18 23:17:05 +0000

    [Data] Emit events rather than just logs for detected issues
    Signed-off-by: cong.qian <cong.qian@anyscale.com>

--- python/ray/data/tests/test_issue_detection_manager.py
+++ python/ray/data/tests/test_issue_detection_manager.py
@@ -1,25 +1,46 @@
+import json
+import os
 import sys
 from unittest.mock import MagicMock
 
 import pytest
 
+import ray
 from ray.data._internal.execution.operators.input_data_buffer import (
     InputDataBuffer,
 )
 from ray.data._internal.execution.operators.task_pool_map_operator import (
     MapOperator,
 )
+from ray.data._internal.execution.streaming_executor import StreamingExecutor
 from ray.data._internal.issue_detection.issue_detector import (
     Issue,
     IssueType,
 )
 from ray.data._internal.issue_detection.issue_detector_manager import (
+    ISSUE_TYPE_TO_EXPORT_OPERATOR_EVENT_TYPE,
     IssueDetectorManager,
 )
 from ray.data.context import DataContext
 
 
+def _get_exported_data():
+    exported_file = os.path.join(
+        ray._private.worker._global_node.get_session_dir_path(),
+        "logs",
+        "export_events",
+        "event_EXPORT_DATASET_OPERATOR_EVENT.log",
+    )
+    assert os.path.isfile(exported_file)
+
+    with open(exported_file, "r") as f:
+        data = f.readlines()
+
+    return [json.loads(line) for line in data]
+
+
 def test_report_issues():
+    ray.init()
     ctx = DataContext.get_current()
     input_operator = InputDataBuffer(ctx, input_data=[])
     map_operator = MapOperator.create(
@@ -29,7 +50,8 @@ def test_report_issues():
         ray_remote_args={},
     )
     topology = {input_operator: MagicMock(), map_operator: MagicMock()}
-    executor = MagicMock(_topology=topology)
+    executor = StreamingExecutor(ctx)
+    executor._topology = topology
     detector = IssueDetectorManager(executor)
 
     detector._report_issues(
@@ -52,6 +74,25 @@ def test_report_issues():
     assert input_operator.metrics.issue_detector_high_memory == 0
     assert map_operator.metrics.issue_detector_hanging == 0
     assert map_operator.metrics.issue_detector_high_memory == 1
+
+    data = _get_exported_data()
+    assert len(data) == 2
+    assert data[0]["event_data"]["dataset_id"] == "dataset"
+    assert data[0]["event_data"]["operator_id"] == f"{input_operator.name}_0"
+    assert data[0]["event_data"]["operator_name"] == input_operator.name
+    assert (
+        data[0]["event_data"]["event_type"]
+        == ISSUE_TYPE_TO_EXPORT_OPERATOR_EVENT_TYPE[IssueType.HANGING]
+    )
+    assert data[0]["event_data"]["message"] == "Hanging detected"
+    assert data[1]["event_data"]["dataset_id"] == "dataset"
+    assert data[1]["event_data"]["operator_id"] == f"{map_operator.name}_1"
+    assert data[1]["event_data"]["operator_name"] == map_operator.name
+    assert (
+        data[1]["event_data"]["event_type"]
+        == ISSUE_TYPE_TO_EXPORT_OPERATOR_EVENT_TYPE[IssueType.HIGH_MEMORY]
+    )
+    assert data[1]["event_data"]["message"] == "High memory usage detected"
 
 
 if __name__ == "__main__":
