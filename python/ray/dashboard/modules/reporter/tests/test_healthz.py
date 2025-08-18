48e7190e2d592fe2abc090797d5385370e579de3
avigyabb <avigyabb@stanford.edu>
2025-08-18 17:31:22 +0000

    changed test_healthz to use node_ip instead of localhost
    Signed-off-by: avigyabb <avigyabb@stanford.edu>

--- python/ray/dashboard/modules/reporter/tests/test_healthz.py
+++ python/ray/dashboard/modules/reporter/tests/test_healthz.py
@@ -25,7 +25,8 @@ def test_healthz_head(monkeypatch, ray_start_cluster):
 def test_healthz_agent_1(monkeypatch, ray_start_cluster):
     agent_port = find_free_port()
     h = ray_start_cluster.add_node(dashboard_agent_listen_port=agent_port)
-    uri = f"http://localhost:{agent_port}/api/local_raylet_healthz"
+    # Use the node's actual IP address instead of localhost
+    uri = f"http://{h.node_ip_address}:{agent_port}/api/local_raylet_healthz"
 
     wait_for_condition(lambda: requests.get(uri).status_code == 200)
 
@@ -43,7 +44,8 @@ def test_healthz_agent_2(monkeypatch, ray_start_cluster):
 
     agent_port = find_free_port()
     h = ray_start_cluster.add_node(dashboard_agent_listen_port=agent_port)
-    uri = f"http://localhost:{agent_port}/api/local_raylet_healthz"
+    # Use the node's actual IP address instead of localhost
+    uri = f"http://{h.node_ip_address}:{agent_port}/api/local_raylet_healthz"
 
     wait_for_condition(lambda: requests.get(uri).status_code == 200)
 
