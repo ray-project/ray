973dae8938f869be1bbbb8dd8483a37cf830f9e8
Omkar Kulkarni <omkar@anyscale.com>
2025-08-19 09:45:26 -0700

    Update python/ray/serve/tests/test_standalone_3.py
    Co-authored-by: Cindy Zhang <cindyzyx9@gmail.com>
    Signed-off-by: Omkar Kulkarni <omkar@anyscale.com>

--- python/ray/serve/tests/test_standalone_3.py
+++ python/ray/serve/tests/test_standalone_3.py
@@ -498,6 +498,8 @@ def test_client_shutdown_gracefully_when_timeout(
     serve.run(target=model)
 
     # Check expected actors based on mode
+    # If proxy is on every node, total actors are 2 proxies, 1 controller, and 2 replicas
+    # Otherwise, total actors are 1 proxy, 1 controller, and 2 replicas
     expected_actors = 5 if is_proxy_on_every_node() else 4
     wait_for_condition(lambda: len(list_actors()) == expected_actors)
     assert len(ray.nodes()) == 2
