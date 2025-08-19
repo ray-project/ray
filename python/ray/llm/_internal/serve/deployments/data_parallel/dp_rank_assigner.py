0f6f9ffad8068d564e7bc964302cd435a7b59eb7
Rui Qiao <ruisearch42@gmail.com>
2025-08-19 01:45:27 +0000

    up
    Signed-off-by: Rui Qiao <ruisearch42@gmail.com>

--- python/ray/llm/_internal/serve/deployments/data_parallel/dp_rank_assigner.py
+++ python/ray/llm/_internal/serve/deployments/data_parallel/dp_rank_assigner.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from typing import Optional
+from typing import Dict, List, Optional
 
 from ray import serve
 
@@ -16,36 +16,68 @@ class DPRankAssigner:
     deployment.
     """
 
-    def __init__(self, dp_size: int, dp_size_local: Optional[int] = None):
-        self.dp_size = dp_size
-        self.dp_size_local = dp_size_local
-        self.lock = asyncio.Lock()
-        self.dp_address = None
-        self.dp_rpc_port = None
-        self.master_info_event = asyncio.Event()
-        if dp_size_local is None:
+    def __init__(self, dp_size: int, dp_size_per_node: Optional[int] = None):
+        self.dp_size: int = dp_size
+        self.dp_size_per_node: Optional[int] = dp_size_per_node
+        self.lock: asyncio.Lock = asyncio.Lock()
+        self.dp_address: Optional[str] = None
+        self.dp_rpc_port: Optional[int] = None
+        self.master_info_event: asyncio.Event = asyncio.Event()
+
+        # Fields for _register_random_placement():
+        # Next rank to assign
+        self.next_rank: Optional[int] = None
+
+        # Fields for _register_node_pack_placement():
+        # Number of nodes to assign to
+        self.num_nodes: Optional[int] = None
+        # Map from node id to available ranks
+        self.node_to_avail_ranks: Dict[str, List[int]] = {}
+
+        if dp_size_per_node is None:
             self.next_rank = 0
-            logger.info(f"Using naive rank assigner for DP size {self.dp_size}")
-        else:
-            assert (
-                self.dp_size % self.dp_size_local == 0
-            ), f"dp_size {self.dp_size} must be divisible by dp_size_local {self.dp_size_local}"
-            self.num_nodes = self.dp_size // self.dp_size_local
             logger.info(
-                f"Using node pack rank assigner for DP size {self.dp_size} with dp_size_local {self.dp_size_local}"
+                f"Using random placement rank assigner for DP size {self.dp_size}"
+            )
+        else:
+            if self.dp_size_per_node <= 0:
+                raise ValueError(
+                    f"dp_size_per_node {self.dp_size_per_node} must be greater than 0"
+                )
+            if self.dp_size % self.dp_size_per_node != 0:
+                raise ValueError(
+                    f"dp_size {self.dp_size} must be divisible by dp_size_per_node {self.dp_size_per_node}"
+                )
+            self.num_nodes = self.dp_size // self.dp_size_per_node
+            logger.info(
+                f"Using node pack placement rank assigner for DP size {self.dp_size}"
+                f"with dp_size_per_node {self.dp_size_per_node}"
             )
 
-        self.node_to_avail_ranks = {}
-
     async def register(self, replica_ctx: "serve.context.ReplicaContext", node_id: str):
-        if self.dp_size_local is None:
-            return await self.register_naive(replica_ctx, node_id)
-        else:
-            return await self.register_node_pack(replica_ctx, node_id)
+        """
+        Register a replica and assign a rank to it.
 
-    async def register_naive(
+        Args:
+            replica_ctx: The replica context.
+            node_id: The node id of the replica.
+
+        Returns:
+            The rank of the replica.
+        """
+        if self.dp_size_per_node is None:
+            return await self._register_random_placement(replica_ctx, node_id)
+        else:
+            return await self._register_node_pack_placement(replica_ctx, node_id)
+
+    async def _register_random_placement(
         self, replica_ctx: "serve.context.ReplicaContext", node_id: str
     ):
+        """
+        Assign a rank based on random placement.
+
+        The ranks are assigned in a random order, regardless of its node id.
+        """
         async with self.lock:
             if self.next_rank >= self.dp_size:
                 raise ValueError(
@@ -57,19 +89,32 @@ class DPRankAssigner:
             self.next_rank += 1
             return rank
 
-    async def register_node_pack(
+    async def _register_node_pack_placement(
         self, replica_ctx: "serve.context.ReplicaContext", node_id: str
     ):
+        """
+        Assign a rank based on node pack placement.
+
+        This should be used for DeepEP which assumes that the ranks ranging from
+        [dp_rank_per_node * node_rank, dp_rank_per_node * (node_rank + 1) - 1] are
+        assigned to the same node.
+
+        For example, if dp_size_per_node is 8, and there are 16 ranks in total, then
+        the ranks [0, 7] should be assigned to one node, and ranks [8, 15] should be
+        assigned to another node.
+        """
         async with self.lock:
             if not self.node_to_avail_ranks:
-                self.node_to_avail_ranks[node_id] = list(range(1, self.dp_size_local))
+                self.node_to_avail_ranks[node_id] = list(
+                    range(1, self.dp_size_per_node)
+                )
                 return 0
             elif node_id not in self.node_to_avail_ranks:
                 node_rank = len(self.node_to_avail_ranks)
                 assert node_rank < self.num_nodes
-                rank = node_rank * self.dp_size_local
+                rank = node_rank * self.dp_size_per_node
                 self.node_to_avail_ranks[node_id] = list(
-                    range(rank + 1, rank + self.dp_size_local)
+                    range(rank + 1, rank + self.dp_size_per_node)
                 )
                 return rank
             else:
--- python/ray/llm/_internal/serve/deployments/data_parallel/dp_server.py
+++ python/ray/llm/_internal/serve/deployments/data_parallel/dp_server.py
@@ -77,11 +77,14 @@ def build_dp_deployment(
         raise ValueError(
             "data_parallel_size should be greater than 1 for DP deployment."
         )
-    # NOTE: we cannot use engine_kwargs.data_parallel_size_local because
-    # that has special semantics in vLLM.
-    # TODO(rui): figure out a better way to pass in dp_size_local.
-    dp_size_local = llm_config.experimental_configs.get("dp_size_local", None)
-    dp_rank_assigner = DPRankAssigner.bind(dp_size=dp_size, dp_size_local=dp_size_local)
+
+    # TODO(rui): figure out a better way to pass in dp_size_per_node.
+    # NOTE: we cannot use engine_kwargs.data_parallel_size_local to specify
+    # the number of ranks per node because that has special semantics in vLLM.
+    dp_size_per_node = llm_config.experimental_configs.get("dp_size_per_node", None)
+    dp_rank_assigner = DPRankAssigner.bind(
+        dp_size=dp_size, dp_size_per_node=dp_size_per_node
+    )
     name_prefix = name_prefix or "DPLLMDeployment:"
     name = name_prefix + llm_config._get_deployment_name()
     if "num_replicas" in llm_config.deployment_config:
