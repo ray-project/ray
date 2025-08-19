26a3ff8c90dd4a455f4f5699c0d7858afcfc3eb9
Qiaolin Yu <liin1211@outlook.com>
2025-08-19 13:57:58 -0700

    [core][GPU Objects] Support nixl as tensor transport backend (#54459)
    <!-- Thank you for your contribution! Please review
    https://github.com/ray-project/ray/blob/master/CONTRIBUTING.rst before
    opening a pull request. -->
    Support nixl as tensor transport backend when using gpu objects. The
    code is tested manually. Related tests may be put in next pr since
    integrating nixl in ci needs some work to do.
    
    ---------
    
    Signed-off-by: Stephanie Wang <smwang@cs.washington.edu>
    Signed-off-by: kaihsun <kaihsun@anyscale.com>
    Co-authored-by: Stephanie Wang <smwang@cs.washington.edu>
    Co-authored-by: kaihsun <kaihsun@anyscale.com>

--- python/ray/experimental/collective/util.py
+++ python/ray/experimental/collective/util.py
@@ -4,6 +4,42 @@ import socket
 
 import ray
 
+from ray.util.collective.types import Backend
+from ray.experimental.collective.tensor_transport_manager import TensorTransportManager
+from ray.experimental.collective.nixl_tensor_transport import NixlTensorTransport
+from ray.experimental.collective.collective_tensor_transport import (
+    CollectiveTensorTransport,
+)
+
+# Singleton instances for tensor transport managers
+_nixl_tensor_transport_manager = None
+_collective_tensor_transport_manager = None
+
+
+def get_tensor_transport_manager(
+    tensor_transport: Backend,
+) -> "TensorTransportManager":
+    """Get the tensor transport manager for the given tensor transport protocol.
+
+    Args:
+        tensor_transport: The tensor transport protocol to use for the GPU object.
+
+    Returns:
+        TensorTransportManager: The tensor transport manager for the given tensor transport protocol.
+    """
+    if tensor_transport == Backend.NIXL:
+        global _nixl_tensor_transport_manager
+        if _nixl_tensor_transport_manager is None:
+            _nixl_tensor_transport_manager = NixlTensorTransport()
+        return _nixl_tensor_transport_manager
+    elif tensor_transport == Backend.TORCH_GLOO or tensor_transport == Backend.NCCL:
+        global _collective_tensor_transport_manager
+        if _collective_tensor_transport_manager is None:
+            _collective_tensor_transport_manager = CollectiveTensorTransport()
+        return _collective_tensor_transport_manager
+    else:
+        raise ValueError(f"Unsupported tensor transport protocol: {tensor_transport}")
+
 
 def find_free_port() -> int:
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
