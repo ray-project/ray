870e202a9e95d31b5e71f1a1ca7085e6bffc6f53
Doyoung Kim <34902420+landscapepainter@users.noreply.github.com>
2025-08-19 15:51:13 -0700

    [Serve] Updates test_deploy_2.py to hardcode route_prefix for test_change_route_prefix (#55700)
    This test specifies the route_prefix value for one of the urls for
    better readability.
    
    ---------
    
    Signed-off-by: doyoung <doyoung@anyscale.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- python/ray/serve/tests/test_deploy_app_2.py
+++ python/ray/serve/tests/test_deploy_app_2.py
@@ -575,8 +575,8 @@ def test_change_route_prefix(serve_instance):
         assert "Path '/old' not found." in resp.text
 
         # Response from new route should be same PID
-        url = get_application_url()
-        pid2 = httpx.get(url).json()[0]
+        url = get_application_url(exclude_route_prefix=True)
+        pid2 = httpx.get(f"{url}/new").json()[0]
         assert pid2 == pid1
         return True
 
