0f73172f2e473f01e95336694ae3b49bcf707760
elliot-barn <elliot.barnwell@anyscale.com>
2025-08-21 21:13:23 +0000

    making tmp path releative to ray
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/compile_llm_requirements.sh
+++ ci/compile_llm_requirements.sh
@@ -11,12 +11,12 @@ if [[ "${PYTHON_CODE}" != "py311" ]]; then
 	exit 1
 fi
 
-mkdir -p /tmp/ray-deps
+mkdir -p tmp-raydepsets/llm/
 
 # Remove the GPU constraints
-cp python/requirements_compiled.txt /tmp/ray-deps/requirements_compiled.txt
-sed -e '/^--extra-index-url /d' -e '/^--find-links /d' /tmp/ray-deps/requirements_compiled.txt > /tmp/ray-deps/requirements_compiled.txt.tmp
-mv /tmp/ray-deps/requirements_compiled.txt.tmp /tmp/ray-deps/requirements_compiled.txt
+cp python/requirements_compiled.txt tmp-raydepsets/llm/requirements_compiled.txt
+sed -e '/^--extra-index-url /d' -e '/^--find-links /d' tmp-raydepsets/llm/requirements_compiled.txt > tmp-raydepsets/llm/requirements_compiled.txt.tmp
+mv tmp-raydepsets/llm/requirements_compiled.txt.tmp tmp-raydepsets/llm/requirements_compiled.txt
 
 bazel run //ci/raydepsets:raydepsets -- build "${CONFIG_PATH}"
 
