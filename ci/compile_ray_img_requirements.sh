9274700809781138e68262838c517dc44e152e86
elliot-barn <elliot.barnwell@anyscale.com>
2025-08-22 00:12:24 +0000

    renaming lock files
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/compile_ray_img_requirements.sh
+++ ci/compile_ray_img_requirements.sh
@@ -5,15 +5,15 @@ set -euo pipefail
 PYTHON_CODE="$(python -c "import sys; v=sys.version_info; print(f'py{v.major}{v.minor}')")"
 echo "--- Python version: ${PYTHON_CODE}"
 
-mkdir -p /tmp/ray-deps
-mkdir -p /tmp/ray-deps/python/lock_files/ray_img
+mkdir -p tmp-raydeps
+mkdir -p tmp-raydeps/python/lock_files/ray_img
 
-echo "ray==3.0.0.dev0" > /tmp/ray-deps/requirements.txt
+echo "ray[all]==3.0.0.dev0" > tmp-raydeps/requirements.txt
 
 # Remove the GPU constraints
-cp python/requirements_compiled.txt /tmp/ray-deps/requirements_compiled.txt
-sed -i '/^--extra-index-url /d' /tmp/ray-deps/requirements_compiled.txt
-sed -i '/^--find-links /d' /tmp/ray-deps/requirements_compiled.txt
+cp python/requirements_compiled.txt tmp-raydeps/requirements_compiled.txt
+sed -i '/^--extra-index-url /d' tmp-raydeps/requirements_compiled.txt
+sed -i '/^--find-links /d' tmp-raydeps/requirements_compiled.txt
 
 bazel run //ci/raydepsets:raydepsets -- build ci/raydepsets/rayimg.depsets.yaml
 
