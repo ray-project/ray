93e771469c9834bcded0efd79fbc380a3c414241
Aydin Abiar <aydin@anyscale.com>
2025-08-22 11:48:37 -0700

    fixed name to README
    Signed-off-by: Aydin Abiar <aydin@anyscale.com>

--- doc/source/ray-overview/examples/deployment-serve-llm/ci/tests.sh
+++ doc/source/ray-overview/examples/deployment-serve-llm/ci/tests.sh
@@ -7,11 +7,11 @@
 set -exo pipefail
 
 for nb in \
-  "small-size-llm/notebook" \
-  "medium-size-llm/notebook" \
-  "vision-llm/notebook" \
-  "reasoning-llm/notebook" \
-  "hybrid-reasoning-llm/notebook"
+  "small-size-llm/README" \
+  "medium-size-llm/README" \
+  "vision-llm/README" \
+  "reasoning-llm/README" \
+  "hybrid-reasoning-llm/README"
 do
   python ci/nb2py.py "${nb}.ipynb" "${nb}.py" --ignore-cmds
   python "${nb}.py"
