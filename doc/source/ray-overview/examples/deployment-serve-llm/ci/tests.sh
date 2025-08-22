39753e434b3b1f8f9b5d125bb1edde4d4476d07a
Aydin Abiar <aydin@anyscale.com>
2025-08-22 14:57:26 -0700

    add large size llm tutorial test
    Signed-off-by: Aydin Abiar <aydin@anyscale.com>

--- doc/source/ray-overview/examples/deployment-serve-llm/ci/tests.sh
+++ doc/source/ray-overview/examples/deployment-serve-llm/ci/tests.sh
@@ -9,6 +9,7 @@ set -exo pipefail
 for nb in \
   "small-size-llm/README" \
   "medium-size-llm/README" \
+  "large-size-llm/README" \
   "vision-llm/README" \
   "reasoning-llm/README" \
   "hybrid-reasoning-llm/README"
