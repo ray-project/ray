d9f59a3657d8e50b7e136d45d59b81702d8f7219
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-08-22 13:41:08 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/gcs/gcs_server/tests/gcs_server_test_util.h
+++ src/ray/gcs/gcs_server/tests/gcs_server_test_util.h
@@ -30,10 +30,10 @@
 #include "ray/gcs/gcs_client/accessor.h"
 #include "ray/gcs/gcs_server/gcs_actor_manager.h"
 #include "ray/gcs/gcs_server/gcs_actor_scheduler.h"
+#include "ray/gcs/gcs_server/gcs_node_manager.h"
 #include "ray/gcs/gcs_server/gcs_placement_group_mgr.h"
 #include "ray/gcs/gcs_server/gcs_placement_group_scheduler.h"
 #include "ray/gcs/gcs_server/gcs_resource_manager.h"
-#include "ray/gcs/gcs_server/gcs_node_manager.h"
 #include "ray/gcs/store_client/in_memory_store_client.h"
 
 namespace ray {
