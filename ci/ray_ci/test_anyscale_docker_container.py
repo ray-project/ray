d3635826f85e84d7dd405e75a7bfe624c69794de
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2025-08-23 15:50:44 -0700

    [image] add extra-test stage in image building (#55725)
    this removes the requirement to build the extra test layer after release
    image, and allows caching of everything other than the ray wheel and ray
    wheel installation.
    
    Signed-off-by: Lonnie Liu <lonnie@anyscale.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- ci/ray_ci/test_anyscale_docker_container.py
+++ ci/ray_ci/test_anyscale_docker_container.py
@@ -58,28 +58,12 @@ class TestAnyscaleDockerContainer(RayCITestBase):
                 == [
                     "./ci/build/build-anyscale-docker.sh "
                     f"rayproject/ray-ml:123456-{pv}-cu121 "
-                    f"{aws_prj}:123456-{pv}-cu121 requirements_ml_byod_{v}.txt {aws_ecr}",
+                    f"{aws_prj}:123456-{pv}-cu121 {aws_ecr}",
                     f"./release/gcloud_docker_login.sh {gce_credentials}",
                     "export PATH=$(pwd)/google-cloud-sdk/bin:$PATH",
                 ]
                 + push_cmds_want
             )
-
-    def test_requirements_file(self) -> None:
-        container = AnyscaleDockerContainer("3.11", "cu12.1.1-cudnn8", "ray-ml")
-        assert container._get_requirement_file() == "requirements_ml_byod_3.11.txt"
-
-        container = AnyscaleDockerContainer("3.9", "cu12.1.1-cudnn8", "ray-ml")
-        assert container._get_requirement_file() == "requirements_ml_byod_3.9.txt"
-
-        container = AnyscaleDockerContainer("3.11", "cu12.4.1-cudnn", "ray-llm")
-        assert container._get_requirement_file() == "requirements_llm_byod_3.11.txt"
-
-        container = AnyscaleDockerContainer("3.9", "cpu", "ray")
-        assert container._get_requirement_file() == "requirements_byod_3.9.txt"
-
-        container = AnyscaleDockerContainer("3.12", "cpu", "ray")
-        assert container._get_requirement_file() == "requirements_byod_3.12.txt"
 
 
 if __name__ == "__main__":
