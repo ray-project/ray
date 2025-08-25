f51bc5aca18bcb34251cb91636fc8150d9f830c8
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-08-25 13:37:16 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/gcs/gcs_server/grpc_services.h
+++ src/ray/gcs/gcs_server/grpc_services.h
@@ -69,7 +69,7 @@ class NodeResourceInfoGrpcService : public GrpcService {
   void InitServerCallFactories(
       const std::unique_ptr<grpc::ServerCompletionQueue> &cq,
       std::vector<std::unique_ptr<ServerCallFactory>> *server_call_factories,
-      const ClusterID &cluster_id) override {}
+      const ClusterID &cluster_id) override;
 
  private:
   /// The grpc async service object.
