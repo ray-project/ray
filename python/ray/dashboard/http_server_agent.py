03e491b0bb3303ccabe1658fc62b9f098055749a
Jiajun Yao <jeromeyjj@gmail.com>
2025-08-25 15:40:32 -0700

    Bind dashboard agent http server to localhost in addition to the node ip
    Signed-off-by: Jiajun Yao <jeromeyjj@gmail.com>

--- python/ray/dashboard/http_server_agent.py
+++ python/ray/dashboard/http_server_agent.py
@@ -7,7 +7,7 @@ from packaging.version import Version
 
 import ray.dashboard.optional_utils as dashboard_optional_utils
 from ray._common.utils import get_or_create_event_loop
-from ray._common.network_utils import build_address
+from ray._common.network_utils import build_address, is_localhost
 from ray.dashboard.optional_deps import aiohttp, aiohttp_cors, hdrs
 
 logger = logging.getLogger(__name__)
@@ -48,6 +48,13 @@ class HttpServerAgent:
                     self.listen_port,
                 )
                 await site.start()
+                if not is_localhost(self.ip):
+                    local_site = aiohttp.web.TCPSite(
+                        self.runner,
+                        "localhost",
+                        self.listen_port,
+                    )
+                    await local_site.start()
                 if attempt > 0:
                     logger.info(
                         f"Successfully started agent on port {self.listen_port} "
