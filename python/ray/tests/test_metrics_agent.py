e3ab78c4bfafa513a4af02645a9d3524ffa2d17a
Carolyn Wang <carolynw825@gmail.com>
2025-08-25 13:25:08 -0700

    [observability] Prometheus http service discovery API (#55656)
    ## Why are these changes needed?
    
    Adds an http endpoint for prometheus service discovery for metrics
    targets.
    
    Manual testing:
    Started local prometheus with config
    ```
    global:
      scrape_interval: 10s # Set the scrape interval to every 10 seconds. Default is every 1 minute.
      evaluation_interval: 10s # Evaluate rules every 10 seconds. The default is every 1 minute.
    
    scrape_configs:
    - job_name: 'ray'
      http_sd_configs:
      - url: 'http://127.0.0.1:8265/api/prometheus/sd'
        refresh_interval: 60s
    ```
    
    and local ray cluster with `ray start --head
    --metrics-export-port=8080`.
    
    check api response:
    ```
    % curl http://127.0.0.1:8265/api/prometheus/sd
    [{"labels": {"job": "ray"}, "targets": ["127.0.0.1:8080", "127.0.0.1:44217", "127.0.0.1:44227"]}]
    ```
    
    ensure prometheus can scrape for these targets:
    <img width="1029" height="403" alt="Screenshot 2025-08-15 at 2 25 34 PM"
    src="https://github.com/user-attachments/assets/aedf5bd0-77f3-4288-9e9f-50d8e459bbe7"
    />
    
    ## Related issue number
    
    ## Checks
    
    - [x] I've signed off every commit(by using the -s flag, i.e., `git
    commit -s`) in this PR.
    - [x] I've run `scripts/format.sh` to lint the changes in this PR.
    - [x] I've included any doc changes needed for
    https://docs.ray.io/en/master/.
    - [x] I've added any new APIs to the API Reference. For example, if I
    added a
    method in Tune, I've added it in `doc/source/tune/api/` under the
               corresponding `.rst` file.
    - [x] I've made sure the tests are passing. Note that there might be a
    few flaky tests, see the recent failures at https://flakey-tests.ray.io/
    - Testing Strategy
       - [ ] Unit tests
       - [ ] Release tests
       - [ ] This PR is not tested :(
    
    ---------
    
    Signed-off-by: carolynwang <carolyn@anyscale.com>
    Signed-off-by: jugalshah291 <shah.jugal291@gmail.com>

--- python/ray/tests/test_metrics_agent.py
+++ python/ray/tests/test_metrics_agent.py
@@ -971,9 +971,10 @@ def test_prometheus_file_based_service_discovery(ray_start_cluster):
         )
         return node_export_addrs + [autoscaler_export_addr, dashboard_export_addr]
 
-    loaded_json_data = json.loads(writer.get_file_discovery_content())[0]
+    loaded_json_data = json.loads(writer.get_file_discovery_content())
+    assert loaded_json_data == writer.get_latest_service_discovery_content()
     assert set(get_metrics_export_address_from_node(nodes)) == set(
-        loaded_json_data["targets"]
+        loaded_json_data[0]["targets"]
     )
 
     # Let's update nodes.
@@ -981,9 +982,10 @@ def test_prometheus_file_based_service_discovery(ray_start_cluster):
         nodes.append(cluster.add_node())
 
     # Make sure service discovery file content is correctly updated.
-    loaded_json_data = json.loads(writer.get_file_discovery_content())[0]
+    loaded_json_data = json.loads(writer.get_file_discovery_content())
+    assert loaded_json_data == writer.get_latest_service_discovery_content()
     assert set(get_metrics_export_address_from_node(nodes)) == set(
-        loaded_json_data["targets"]
+        loaded_json_data[0]["targets"]
     )
 
 
