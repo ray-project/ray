ca9ed9008793d723c361f2229c01f8edbc624c4d
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-08-26 15:18:55 -0500

    [core] Remove `gcs_rpc_server.h` dependency from `InternalPubSubHandler` (#55952)
    Splitting gRPC service interface from implementation.
    
    ---------
    
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/gcs/gcs_server/gcs_server.cc
+++ src/ray/gcs/gcs_server/gcs_server.cc
@@ -621,8 +621,10 @@ void GcsServer::InitKVService() {
 void GcsServer::InitPubSubHandler() {
   auto &io_context = io_context_provider_.GetIOContext<GcsPublisher>();
   pubsub_handler_ = std::make_unique<InternalPubSubHandler>(io_context, *gcs_publisher_);
-  rpc_server_.RegisterService(
-      std::make_unique<rpc::InternalPubSubGrpcService>(io_context, *pubsub_handler_));
+
+  // This service is used to handle long poll requests, so we don't limit active RPCs.
+  rpc_server_.RegisterService(std::make_unique<rpc::InternalPubSubGrpcService>(
+      io_context, *pubsub_handler_, /*max_active_rpcs_per_handler_=*/-1));
 }
 
 void GcsServer::InitRuntimeEnvManager() {
