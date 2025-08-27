078dc75bd9c7290abb94ec2be5b9fb467da07344
kevin <kevin@anyscale.com>
2025-08-27 23:59:58 +0000

    p
    Signed-off-by: kevin <kevin@anyscale.com>

--- release/hello_world_tests/hello_world.py
+++ release/hello_world_tests/hello_world.py
@@ -1,13 +1,16 @@
 import ray
 
+
 @ray.remote
 def hello_world():
     print("Hello, world!")
+
 
 def main():
     ray.init()
     ray.get(hello_world.remote())
     ray.shutdown()
+
 
 if __name__ == "__main__":
     main()
