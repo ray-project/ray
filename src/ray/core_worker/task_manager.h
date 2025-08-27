fa4a4e9e92b26bbb70c6591fbbf553ff5caabfb1
Cuong Nguyen <can@anyscale.com>
2025-08-27 13:40:38 -0700

    w00t
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- src/ray/core_worker/task_manager.h
+++ src/ray/core_worker/task_manager.h
@@ -37,6 +37,7 @@
 #include "src/ray/protobuf/common.pb.h"
 #include "src/ray/protobuf/core_worker.pb.h"
 #include "src/ray/protobuf/gcs.pb.h"
+#include "ray/observability/metric_interface.h"
 
 namespace ray {
 namespace core {
