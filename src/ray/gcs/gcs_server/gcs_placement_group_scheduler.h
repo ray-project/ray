107b3948075245700e94b8aac1a8b6ddcc45949d
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-08-28 08:35:34 -0500

    [core] Split `gcs_placement_group_scheduler` out from `gcs_server_lib` (#55955)
    Involved splitting `GcsPlacementGroup` into its own file to break
    circular dependency.
    
    Also eliminated `gcs_server_test_util.h` by:
    
    1. Updating the global fakes with the implementation that was defined in
    the file.
    2. Moving other utilities into the single file that depended on them.
    
    ---------
    
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>
    Signed-off-by: jugalshah291 <shah.jugal291@gmail.com>

--- src/ray/gcs/gcs_server/gcs_placement_group_scheduler.h
+++ src/ray/gcs/gcs_server/gcs_placement_group_scheduler.h
@@ -26,6 +26,7 @@
 #include "ray/common/id.h"
 #include "ray/common/scheduling/scheduling_ids.h"
 #include "ray/gcs/gcs_server/gcs_node_manager.h"
+#include "ray/gcs/gcs_server/gcs_placement_group.h"
 #include "ray/gcs/gcs_server/gcs_table_storage.h"
 #include "ray/raylet/scheduling/cluster_resource_scheduler.h"
 #include "ray/raylet/scheduling/policy/scheduling_context.h"
@@ -37,8 +38,6 @@
 
 namespace ray {
 namespace gcs {
-
-class GcsPlacementGroup;
 
 using PGSchedulingFailureCallback =
     std::function<void(std::shared_ptr<GcsPlacementGroup>, bool)>;
@@ -513,6 +512,11 @@ class GcsPlacementGroupScheduler : public GcsPlacementGroupSchedulerInterface {
   /// The bundles that waiting to be destroyed and release resources.
   std::list<std::pair<NodeID, std::shared_ptr<const BundleSpecification>>>
       waiting_removed_bundles_;
+
+  friend class GcsPlacementGroupSchedulerTest;
+  FRIEND_TEST(GcsPlacementGroupSchedulerTest, TestCheckingWildcardResource);
+  FRIEND_TEST(GcsPlacementGroupSchedulerTest, TestWaitingRemovedBundles);
+  FRIEND_TEST(GcsPlacementGroupSchedulerTest, TestBundlesRemovedWhenNodeDead);
 };
 
 }  // namespace gcs
