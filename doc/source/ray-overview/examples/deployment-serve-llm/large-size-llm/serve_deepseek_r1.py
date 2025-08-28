3765053c5754751798141f0e7ebc7eee32878e5d
Aydin Abiar <aydin@anyscale.com>
2025-08-27 17:56:04 -0700

    black compliant..
    Signed-off-by: Aydin Abiar <aydin@anyscale.com>

--- doc/source/ray-overview/examples/deployment-serve-llm/large-size-llm/serve_deepseek_r1.py
+++ doc/source/ray-overview/examples/deployment-serve-llm/large-size-llm/serve_deepseek_r1.py
@@ -1,4 +1,4 @@
-#serve_deepseek_r1.py
+# serve_deepseek_r1.py
 from ray.serve.llm import LLMConfig, build_openai_app
 
 llm_config = LLMConfig(
@@ -9,21 +9,22 @@ llm_config = LLMConfig(
     accelerator_type="H100",
     deployment_config=dict(
         autoscaling_config=dict(
-            min_replicas=1, max_replicas=1,
+            min_replicas=1,
+            max_replicas=1,
         )
     ),
     ### Uncomment if your model is gated and need your Huggingface Token to access it
-    #runtime_env=dict(
+    # runtime_env=dict(
     #    env_vars={
     #        "HF_TOKEN": os.environ.get("HF_TOKEN")
     #    }
-    #),
+    # ),
     engine_kwargs=dict(
         max_model_len=16384,
         # Split weights among 8 GPUs in the node
         tensor_parallel_size=8,
-        pipeline_parallel_size=2
-    )
+        pipeline_parallel_size=2,
+    ),
 )
 
 app = build_openai_app({"llm_configs": [llm_config]})
--- doc/source/ray-overview/examples/deployment-serve-llm/medium-size-llm/serve_llama_3_1_70b.py
+++ doc/source/ray-overview/examples/deployment-serve-llm/medium-size-llm/serve_llama_3_1_70b.py
@@ -1,4 +1,4 @@
-#serve_llama_3_1_70b.py
+# serve_llama_3_1_70b.py
 from ray.serve.llm import LLMConfig, build_openai_app
 import os
 
@@ -11,22 +11,19 @@ llm_config = LLMConfig(
     accelerator_type="A100-40G",
     deployment_config=dict(
         autoscaling_config=dict(
-            min_replicas=1, max_replicas=4,
+            min_replicas=1,
+            max_replicas=4,
         )
     ),
     ### If your model is not gated, you can skip `hf_token`
     # Share your Hugging Face Token to the vllm engine so it can access the gated Llama 3
     # Type `export HF_TOKEN=<YOUR-HUGGINGFACE-TOKEN>` in a terminal
-    runtime_env=dict(
-        env_vars={
-            "HF_TOKEN": os.environ.get("HF_TOKEN")
-        }
-    ),
+    runtime_env=dict(env_vars={"HF_TOKEN": os.environ.get("HF_TOKEN")}),
     engine_kwargs=dict(
         max_model_len=32768,
         # Split weights among 8 GPUs in the node
-        tensor_parallel_size=8
-    )
+        tensor_parallel_size=8,
+    ),
 )
 
 app = build_openai_app({"llm_configs": [llm_config]})
