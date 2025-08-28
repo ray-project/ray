379b8b7a2e04e316a9fc12889b2030f9327ceff6
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-08-28 14:24:56 -0500

    [core] Split `gcs_placement_group_manager` out from `gcs_server_lib` (#56048)
    Also renamed it for consistency.
    
    I think previously this was an issue due to the test path length being
    too long on Windows, but I mitigated that recently.
    
    ---------
    
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- src/mock/ray/gcs/gcs_server/gcs_actor_scheduler.h
+++ src/mock/ray/gcs/gcs_server/gcs_actor_scheduler.h
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#pragma once
+
+#include <gmock/gmock.h>
+
+#include "ray/gcs/gcs_server/gcs_actor_scheduler.h"
+
 namespace ray {
 namespace gcs {
 
