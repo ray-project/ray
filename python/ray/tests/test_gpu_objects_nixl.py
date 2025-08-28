068f430e9f65d6a1f5ead21d20c0e496bb9e1518
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2025-08-27 17:21:00 -0700

    Revert "Support cpu tensor transfer with NIXL in GPU Objects (#55793)"
    This reverts commit 028965089883cd56dcb0cc5a0e2e683e9f684fe1.

--- python/ray/tests/test_gpu_objects_nixl.py
+++ python/ray/tests/test_gpu_objects_nixl.py
@@ -7,11 +7,10 @@ import ray
 @ray.remote(num_gpus=1, num_cpus=0, enable_tensor_transport=True)
 class GPUTestActor:
     @ray.method(tensor_transport="nixl")
-    def echo(self, data, device):
-        return data.to(device)
+    def echo(self, data):
+        return data.to("cuda")
 
-    def sum(self, data, device):
-        assert data.device.type == device
+    def sum(self, data):
         return data.sum().item()
 
 
@@ -24,20 +23,11 @@ def test_p2p(ray_start_regular):
 
     # Create test tensor
     tensor = torch.tensor([1, 2, 3])
-
-    tensor1 = torch.tensor([4, 5, 6])
-
-    # Test GPU to GPU transfer
-    ref = src_actor.echo.remote(tensor, "cuda")
+    ref = src_actor.echo.remote(tensor)
 
     # Trigger tensor transfer from src to dst actor
-    result = dst_actor.sum.remote(ref, "cuda")
+    result = dst_actor.sum.remote(ref)
     assert tensor.sum().item() == ray.get(result)
-
-    # Test CPU to CPU transfer
-    ref1 = src_actor.echo.remote(tensor1, "cpu")
-    result1 = dst_actor.sum.remote(ref1, "cpu")
-    assert tensor1.sum().item() == ray.get(result1)
 
 
 @pytest.mark.parametrize("ray_start_regular", [{"num_gpus": 1}], indirect=True)
@@ -47,8 +37,8 @@ def test_intra_gpu_tensor_transfer(ray_start_regular):
     tensor = torch.tensor([1, 2, 3])
 
     # Intra-actor communication for pure GPU tensors
-    ref = actor.echo.remote(tensor, "cuda")
-    result = actor.sum.remote(ref, "cuda")
+    ref = actor.echo.remote(tensor)
+    result = actor.sum.remote(ref)
     assert tensor.sum().item() == ray.get(result)
 
 
