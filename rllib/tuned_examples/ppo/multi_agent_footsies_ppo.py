ad34a6a6bb44f7f489f4f84d2422372f82258b32
Kamil Kaczmarek <kamil@anyscale.com>
2025-08-28 13:56:30 -0700

    only run_rllib_example_script_experiment in the main
    Signed-off-by: Kamil Kaczmarek <kamil@anyscale.com>

--- rllib/tuned_examples/ppo/multi_agent_footsies_ppo.py
+++ rllib/tuned_examples/ppo/multi_agent_footsies_ppo.py
@@ -51,7 +51,6 @@ from ray.rllib.examples.rl_modules.classes.random_rlm import RandomRLModule
 from ray.rllib.utils.metrics import NUM_ENV_STEPS_SAMPLED_LIFETIME
 from ray.rllib.utils.test_utils import (
     add_rllib_example_script_args,
-    run_rllib_example_script_experiment,
 )
 from ray.tune.registry import register_env
 from ray.tune.result import TRAINING_ITERATION
@@ -59,7 +58,8 @@ from ray.tune.result import TRAINING_ITERATION
 # setting two default stopping criteria:
 #    1. training_iteration (via "stop_iters")
 #    2. num_env_steps_sampled_lifetime (via "default_timesteps")
-# ...values very high to make sure that the test passes by adding all required policies to the mix, not by hitting the iteration limit.
+# ...values very high to make sure that the test passes by adding
+# all required policies to the mix, not by hitting the iteration limit.
 # Our main stopping criterion is "target_mix_size" (see an argument below).
 parser = add_rllib_example_script_args(
     default_iters=500,
@@ -125,138 +125,140 @@ parser.add_argument(
     help="The length of each rollout fragment to be collected by the EnvRunners when sampling.",
 )
 
+main_policy = "lstm"
+args = parser.parse_args()
+register_env(name="FootsiesEnv", env_creator=env_creator)
 
-if __name__ == "__main__":
-    main_policy = "lstm"
-    args = parser.parse_args()
-
-    register_env(name="FootsiesEnv", env_creator=env_creator)
-
-    config = (
-        PPOConfig()
-        .reporting(
-            min_time_s_per_iteration=30,
-        )
-        .environment(
-            env="FootsiesEnv",
-            env_config={
-                "max_t": 1000,
-                "frame_skip": 4,
-                "observation_delay": 16,
-                "train_start_port": args.train_start_port,
-                "eval_start_port": args.eval_start_port,
-                "host": "localhost",
-                "binary_download_dir": args.binary_download_dir,
-                "binary_extract_dir": args.binary_extract_dir,
-                "binary_to_download": args.binary_to_download,
-            },
-        )
-        .learners(
-            num_learners=1,
-            num_cpus_per_learner=1,
-            num_gpus_per_learner=0,
-            num_aggregator_actors_per_learner=0,
-        )
-        .env_runners(
-            env_runner_cls=MultiAgentEnvRunner,
-            num_env_runners=args.num_env_runners or 1,
-            num_cpus_per_env_runner=0.5,
-            num_envs_per_env_runner=1,
-            batch_mode="truncate_episodes",
-            rollout_fragment_length=args.rollout_fragment_length,
-            episodes_to_numpy=False,
-            create_env_on_local_worker=True,
-        )
-        .training(
-            train_batch_size_per_learner=args.rollout_fragment_length
-            * (args.num_env_runners or 1),
-            lr=1e-4,
-            entropy_coeff=0.01,
-            num_epochs=10,
-            minibatch_size=128,
-        )
-        .multi_agent(
-            policies={
-                main_policy,
-                "noop",
-                "back",
-                "attack",
-                "random",
-            },
-            # this is a starting policy_mapping_fn. It will be updated by the MixManagerCallback during training.
-            policy_mapping_fn=Matchmaker(
-                [Matchup(main_policy, "noop", 1.0)]
-            ).agent_to_module_mapping_fn,
-            # we only train the main policy, this doesn't change during training.
-            policies_to_train=[main_policy],
-        )
-        .rl_module(
-            rl_module_spec=MultiRLModuleSpec(
-                rl_module_specs={
-                    main_policy: RLModuleSpec(
-                        module_class=LSTMContainingRLModule,
-                        model_config={
-                            "lstm_cell_size": 128,
-                            "dense_layers": [128, 128],
-                            "max_seq_len": 64,
-                        },
-                    ),
-                    # for simplicity, all fixed RLModules are added to the config at the start.
-                    # However, only "noop" is used at the start of training,
-                    # the others are added to the mix later by the MixManagerCallback.
-                    "noop": RLModuleSpec(module_class=NoopFixedRLModule),
-                    "back": RLModuleSpec(module_class=BackFixedRLModule),
-                    "attack": RLModuleSpec(module_class=AttackFixedRLModule),
-                    "random": RLModuleSpec(module_class=RandomRLModule),
-                },
-            )
-        )
-        .evaluation(
-            evaluation_num_env_runners=2,
-            evaluation_sample_timeout_s=180,
-            evaluation_interval=1,
-            evaluation_duration=20,  # 20 episodes is enough to get a good win rate estimate
-            evaluation_duration_unit="episodes",
-            evaluation_parallel_to_training=False,  # we may add new RLModules to the mix at the end of the evaluation stage. Running evaluation in parallel may result in training for one more iteration on the old mix.
-            evaluation_force_reset_envs_before_iteration=True,
-            evaluation_config={
-                "env_config": {"env-for-evaluation": True},
-            },  # evaluation_config is used to add an argument to the env creator.
-        )
-        .callbacks(
-            [
-                functools.partial(
-                    MetricsLoggerCallback,
-                    main_policy=main_policy,
+config = (
+    PPOConfig()
+    .reporting(
+        min_time_s_per_iteration=30,
+    )
+    .environment(
+        env="FootsiesEnv",
+        env_config={
+            "max_t": 1000,
+            "frame_skip": 4,
+            "observation_delay": 16,
+            "train_start_port": args.train_start_port,
+            "eval_start_port": args.eval_start_port,
+            "host": "localhost",
+            "binary_download_dir": args.binary_download_dir,
+            "binary_extract_dir": args.binary_extract_dir,
+            "binary_to_download": args.binary_to_download,
+        },
+    )
+    .learners(
+        num_learners=1,
+        num_cpus_per_learner=1,
+        num_gpus_per_learner=0,
+        num_aggregator_actors_per_learner=0,
+    )
+    .env_runners(
+        env_runner_cls=MultiAgentEnvRunner,
+        num_env_runners=args.num_env_runners or 1,
+        num_cpus_per_env_runner=0.5,
+        num_envs_per_env_runner=1,
+        batch_mode="truncate_episodes",
+        rollout_fragment_length=args.rollout_fragment_length,
+        episodes_to_numpy=False,
+        create_env_on_local_worker=True,
+    )
+    .training(
+        train_batch_size_per_learner=args.rollout_fragment_length
+                                     * (args.num_env_runners or 1),
+        lr=1e-4,
+        entropy_coeff=0.01,
+        num_epochs=10,
+        minibatch_size=128,
+    )
+    .multi_agent(
+        policies={
+            main_policy,
+            "noop",
+            "back",
+            "attack",
+            "random",
+        },
+        # this is a starting policy_mapping_fn
+        # It will be updated by the MixManagerCallback during training.
+        policy_mapping_fn=Matchmaker(
+            [Matchup(main_policy, "noop", 1.0)]
+        ).agent_to_module_mapping_fn,
+        # we only train the main policy, this doesn't change during training.
+        policies_to_train=[main_policy],
+    )
+    .rl_module(
+        rl_module_spec=MultiRLModuleSpec(
+            rl_module_specs={
+                main_policy: RLModuleSpec(
+                    module_class=LSTMContainingRLModule,
+                    model_config={
+                        "lstm_cell_size": 128,
+                        "dense_layers": [128, 128],
+                        "max_seq_len": 64,
+                    },
                 ),
-                functools.partial(
-                    MixManagerCallback,
-                    win_rate_threshold=args.win_rate_threshold,
-                    main_policy=main_policy,
-                    target_mix_size=args.target_mix_size,
-                    starting_modules=[main_policy, "noop"],
-                    fixed_modules_progression_sequence=(
-                        "noop",
-                        "back",
-                        "attack",
-                        "random",
-                    ),
-                ),
-            ]
+                # for simplicity, all fixed RLModules are added to the config at the start.
+                # However, only "noop" is used at the start of training,
+                # the others are added to the mix later by the MixManagerCallback.
+                "noop": RLModuleSpec(module_class=NoopFixedRLModule),
+                "back": RLModuleSpec(module_class=BackFixedRLModule),
+                "attack": RLModuleSpec(module_class=AttackFixedRLModule),
+                "random": RLModuleSpec(module_class=RandomRLModule),
+            },
         )
     )
+    .evaluation(
+        evaluation_num_env_runners=2,
+        evaluation_sample_timeout_s=180,
+        evaluation_interval=1,
+        evaluation_duration=20,  # 20 episodes is enough to get a good win rate estimate
+        evaluation_duration_unit="episodes",
+        evaluation_parallel_to_training=False,
+        # we may add new RLModules to the mix at the end of the evaluation stage.
+        # Running evaluation in parallel may result in training for one more iteration on the old mix.
+        evaluation_force_reset_envs_before_iteration=True,
+        evaluation_config={
+            "env_config": {"env-for-evaluation": True},
+        },  # evaluation_config is used to add an argument to the env creator.
+    )
+    .callbacks(
+        [
+            functools.partial(
+                MetricsLoggerCallback,
+                main_policy=main_policy,
+            ),
+            functools.partial(
+                MixManagerCallback,
+                win_rate_threshold=args.win_rate_threshold,
+                main_policy=main_policy,
+                target_mix_size=args.target_mix_size,
+                starting_modules=[main_policy, "noop"],
+                fixed_modules_progression_sequence=(
+                    "noop",
+                    "back",
+                    "attack",
+                    "random",
+                ),
+            ),
+        ]
+    )
+)
 
-    # creating stopping criteria to be passed to Ray Tune. The main stopping criterion is "mix_size".
-    # "mix_size" is reported at the end of each training iteration by the MixManagerCallback.
-    stop = {
-        NUM_ENV_STEPS_SAMPLED_LIFETIME: args.stop_timesteps,
-        TRAINING_ITERATION: args.stop_iters,
-        "mix_size": args.target_mix_size,
-    }
+# stopping criteria to be passed to Ray Tune. The main stopping criterion is "mix_size".
+# "mix_size" is reported at the end of each training iteration by the MixManagerCallback.
+stop = {
+    NUM_ENV_STEPS_SAMPLED_LIFETIME: args.stop_timesteps,
+    TRAINING_ITERATION: args.stop_iters,
+    "mix_size": args.target_mix_size,
+}
 
-    # Run the experiment
+if __name__ == "__main__":
+    from ray.rllib.utils.test_utils import run_rllib_example_script_experiment
+
     results = run_rllib_example_script_experiment(
-        config,
-        args,
+        base_config=config,
+        args=args,
         stop=stop,
     )
