d31f4886f2042e7f69fe796bd98392f612404333
czgdp1807 <gdp.1807@gmail.com>
2025-08-28 04:25:38 +0000

    MAINT: Changes due to ruff in python/ray/dag/*.py
    Signed-off-by: czgdp1807 <gdp.1807@gmail.com>

--- python/ray/dag/__init__.py
+++ python/ray/dag/__init__.py
@@ -1,27 +1,27 @@
-from ray.dag.dag_node import DAGNode
-from ray.dag.function_node import FunctionNode
 from ray.dag.class_node import (
-    ClassNode,
     ClassMethodNode,
+    ClassNode,
 )
 from ray.dag.collective_node import CollectiveOutputNode
-from ray.dag.input_node import (
-    InputNode,
-    InputAttributeNode,
-    DAGInputData,
-)
-from ray.dag.output_node import MultiOutputNode
-from ray.dag.dag_operation_future import DAGOperationFuture, GPUFuture
 from ray.dag.constants import (
-    PARENT_CLASS_NODE_KEY,
-    PREV_CLASS_METHOD_CALL_KEY,
     BIND_INDEX_KEY,
-    IS_CLASS_METHOD_OUTPUT_KEY,
     COLLECTIVE_OPERATION_KEY,
     DAGNODE_TYPE_KEY,
+    IS_CLASS_METHOD_OUTPUT_KEY,
+    PARENT_CLASS_NODE_KEY,
+    PREV_CLASS_METHOD_CALL_KEY,
 )
-from ray.dag.vis_utils import plot
 from ray.dag.context import DAGContext
+from ray.dag.dag_node import DAGNode
+from ray.dag.dag_operation_future import DAGOperationFuture, GPUFuture
+from ray.dag.function_node import FunctionNode
+from ray.dag.input_node import (
+    DAGInputData,
+    InputAttributeNode,
+    InputNode,
+)
+from ray.dag.output_node import MultiOutputNode
+from ray.dag.vis_utils import plot
 
 __all__ = [
     "ClassNode",
