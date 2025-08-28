f7ee0fcc5404b5eef74a824be9c572fe8f5af482
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-08-28 08:35:34 -0500

    [core] Split `gcs_placement_group_scheduler` out from `gcs_server_lib` (#55955)
    Involved splitting `GcsPlacementGroup` into its own file to break
    circular dependency.
    
    Also eliminated `gcs_server_test_util.h` by:
    
    1. Updating the global fakes with the implementation that was defined in
    the file.
    2. Moving other utilities into the single file that depended on them.
    
    ---------
    
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/gcs/gcs_server/tests/export_api/gcs_actor_manager_export_event_test.cc
+++ src/ray/gcs/gcs_server/tests/export_api/gcs_actor_manager_export_event_test.cc
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 #include <gtest/gtest.h>
 
 #include <chrono>
@@ -25,15 +24,19 @@
 #include "mock/ray/gcs/gcs_server/gcs_kv_manager.h"
 #include "mock/ray/gcs/gcs_server/gcs_node_manager.h"
 #include "ray/common/asio/instrumented_io_context.h"
+#include "ray/common/runtime_env_manager.h"
+#include "ray/gcs/gcs_server/gcs_actor.h"
+#include "ray/gcs/gcs_server/gcs_actor_manager.h"
+#include "ray/gcs/gcs_server/gcs_function_manager.h"
 #include "ray/gcs/gcs_server/gcs_kv_manager.h"
-#include "ray/gcs/gcs_server/tests/gcs_server_test_util.h"
 #include "ray/gcs/store_client/in_memory_store_client.h"
 #include "ray/gcs/tests/gcs_test_util.h"
 #include "ray/pubsub/publisher.h"
+#include "ray/rpc/worker/core_worker_client.h"
+#include "ray/rpc/worker/core_worker_client_pool.h"
 #include "ray/util/event.h"
 
 namespace ray {
-
 namespace gcs {
 
 using ::testing::_;
@@ -351,5 +354,4 @@ TEST_F(GcsActorManagerTest, TestBasic) {
 }
 
 }  // namespace gcs
-
 }  // namespace ray
