d80cba588e36312da579307a6107ca7cf1fb2d9b
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-08-28 11:43:45 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/gcs/gcs_server/gcs_server.cc
+++ src/ray/gcs/gcs_server/gcs_server.cc
@@ -731,7 +731,9 @@ void GcsServer::InitGcsAutoscalerStateManager(const GcsInitData &gcs_init_data) 
   gcs_autoscaler_state_manager_->Initialize(gcs_init_data);
   rpc_server_.RegisterService(
       std::make_unique<rpc::autoscaler::AutoscalerStateGrpcService>(
-          io_context_provider_.GetDefaultIOContext(), *gcs_autoscaler_state_manager_));
+          io_context_provider_.GetDefaultIOContext(),
+          *gcs_autoscaler_state_manager_,
+          RayConfig::instance().gcs_max_active_rpcs_per_handler()));
 }
 
 void GcsServer::InitGcsTaskManager() {
