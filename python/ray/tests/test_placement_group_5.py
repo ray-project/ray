f1f2581cab944851a536eaad3bdd0d6cdff9dc86
Joshua Lee <73967497+Sparks0219@users.noreply.github.com>
2025-08-28 20:59:39 -0700

    [core] Create LeaseID (#55469)
    Signed-off-by: joshlee <joshlee@anyscale.com>
    Signed-off-by: yenhong.wong <yenhong.wong@grabtaxi.com>

--- python/ray/tests/test_placement_group_5.py
+++ python/ray/tests/test_placement_group_5.py
@@ -510,7 +510,7 @@ def test_remove_placement_group_with_pending_worker_lease_waiting_for_pg_resourc
     Specific test steps:
       1. Create a placement group with only 1 bundle.
       2. Create two actors using the aforementioned pg. At this point,
-         the latter actor lease request will definitely be pending in local task manager dispatch queue due to
+         the latter actor lease request will definitely be pending in local lease manager leases_to_grant queue due to
          unavailable pg bundle resources.
       3. Remove the pg while the latter actor lease request is pending.
       4. Verify that the pending actor lease request is cancelled and the pg
@@ -549,7 +549,7 @@ def test_remove_placement_group_with_pending_worker_lease_waiting_for_pg_resourc
             return False
         for sample in samples:
             if sample.labels["State"] == "Dispatched" and sample.value == 1:
-                # actor2 is in the local task manager dispatch queue
+                # actor2 is in the local lease manager leases_to_grant queue
                 return True
         return False
 
