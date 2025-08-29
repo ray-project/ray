62bd9da5ca9d8af2efa534eca421da5ffaaaae99
Aydin Abiar <aydin@anyscale.com>
2025-08-29 10:41:05 -0700

    remove orphan from notebook and add them to excluded patterns in build config
    Signed-off-by: Aydin Abiar <aydin@anyscale.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -228,6 +228,7 @@ exclude_patterns = [
     "data/api/ray.data.*.rst",
     "ray-overview/examples/**/README.md",  # Exclude .md files in examples subfolders
     "train/examples/**/README.md",
+    "serve/tutorials/deployment-serve-llm/**/notebook.ipynb",
 ] + autogen_files
 
 # If "DOC_LIB" is found, only build that top-level navigation item.
