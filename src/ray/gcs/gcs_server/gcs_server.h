2f359b1fa589a723c71fb4a83dbd747c656c1474
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-08-28 19:18:42 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/gcs/gcs_server/gcs_server.h
+++ src/ray/gcs/gcs_server/gcs_server.h
@@ -40,7 +40,6 @@
 #include "ray/raylet/scheduling/cluster_resource_scheduler.h"
 #include "ray/raylet/scheduling/cluster_task_manager.h"
 #include "ray/rpc/client_call.h"
-#include "ray/rpc/gcs/gcs_rpc_server.h"
 #include "ray/rpc/metrics_agent_client.h"
 #include "ray/rpc/node_manager/raylet_client_pool.h"
 #include "ray/rpc/worker/core_worker_client_pool.h"
