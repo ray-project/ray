594f5a0ddc2f517da3c9115cf62638a18675a76f
Lonnie Liu <lonnie@anyscale.com>
2025-08-30 04:10:19 +0000

    doc build test

---
+++ python/ray/train/v2/api/reported_checkpoint.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import Any, Dict
+
+from ray.train import Checkpoint
+from ray.util.annotations import PublicAPI
+
+
+@dataclass
+@PublicAPI(stability="alpha")
+class ReportedCheckpoint:
+    """A user-reported checkpoint and its associated metrics.
+    Attributes:
+        checkpoint: The checkpoint reported by the user.
+        metrics: The metrics associated with that checkpoint.
+    """
+
+    checkpoint: Checkpoint
+    metrics: Dict[str, Any]
