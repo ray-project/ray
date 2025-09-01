6b8f78322b2cf13ecd56815ea393308cf8ec396e
joshlee <joshlee@anyscale.com>
2025-09-01 22:08:34 +0000

    Addressing comments
    Signed-off-by: joshlee <joshlee@anyscale.com>

--- python/ray/tests/test_raylet_fault_tolerance.py
+++ python/ray/tests/test_raylet_fault_tolerance.py
@@ -1,29 +1,34 @@
 import ray
-import time
 import pytest
 import sys
 
+# Not testing response as HandleReturnWorkerLease is clearly idempotent and
+# testing the edge case mentioned in #55469 makes the test brittle since a
+# minor change in the implementation/config variables means the edge case is not tested
 
-@pytest.mark.parametrize("deterministic_failure", ["request", "response"])
-def test_return_worker_lease_retry_and_idempotency(
-    monkeypatch, shutdown_only, deterministic_failure
-):
+
+def test_return_worker_lease_retry_and_idempotency(monkeypatch, shutdown_only):
     monkeypatch.setenv(
         "RAY_testing_rpc_failure",
-        "NodeManagerService.grpc_client.ReturnWorkerLease=1:"
-        + ("100:0" if deterministic_failure == "request" else "0:100"),
+        "NodeManagerService.grpc_client.ReturnWorkerLease=1:100:0",
     )
-    ray.init(num_cpus=1)
+    ray.init(
+        num_cpus=1,
+    )
 
+    # Create two functions with different names to ensure the scheduler class changes
+    # and a new lease request is sent for each call
     @ray.remote
-    def simple_task():
-        time.sleep(1)
+    def simple_task_1():
         return 0
 
-    _ = simple_task.remote()
-    _ = simple_task.remote()
-    result_ref3 = simple_task.remote()
-    assert ray.get(result_ref3) == 0
+    @ray.remote
+    def simple_task_2():
+        return 0
+
+    _ = simple_task_1.remote()
+    result_ref2 = simple_task_2.remote()
+    assert ray.get(result_ref2) == 0
 
 
 if __name__ == "__main__":
