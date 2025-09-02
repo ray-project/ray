2d2a948885621c6d94f79fc6f660d3019da6bfb7
zac <zac@anyscale.com>
2025-09-02 16:13:38 +0000

    updated test file
    Signed-off-by: zac <zac@anyscale.com>

--- src/ray/common/tests/event_stats_test.cc
+++ src/ray/common/tests/event_stats_test.cc
@@ -26,7 +26,7 @@ TEST(EventStatsTest, TestRecordEnd) {
   ASSERT_EQ(event_stats.cum_count, 1);
   ASSERT_EQ(event_stats.curr_count, 1);
   std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  event_tracker.RecordEnd(std::move(handle), false);
+  event_tracker.RecordEnd(std::move(handle));
   event_stats = event_tracker.get_event_stats("method").value();
   ASSERT_EQ(event_stats.cum_count, 1);
   ASSERT_EQ(event_stats.curr_count, 0);
@@ -48,8 +48,7 @@ TEST(EventStatsTest, TestRecordExecution) {
         event_stats = event_tracker.get_event_stats("method").value();
         ASSERT_EQ(event_stats.running_count, 1);
       },
-      std::move(handle),
-      false);
+      std::move(handle));
   event_stats = event_tracker.get_event_stats("method").value();
   ASSERT_EQ(event_stats.cum_count, 1);
   ASSERT_EQ(event_stats.curr_count, 0);
