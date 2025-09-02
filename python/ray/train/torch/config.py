c88a6baa5528f458a72487c2b3c7218fb5787e1b
Lehui Liu <lehui@anyscale.com>
2025-09-02 16:12:18 -0700

    fix a haluci
    Signed-off-by: Lehui Liu <lehui@anyscale.com>

--- python/ray/train/torch/config.py
+++ python/ray/train/torch/config.py
@@ -141,12 +141,13 @@ def _setup_torch_process_group(
         register_custom_torch_dist_backend(backend)
     elif backend == "xla":
         import torch_xla.distributed.xla_backend
-        import torch_xla.runtime as xr
-        assert xr.using_spmd(), "XLA_USE_SPMD must be 1 before PG init"
+        # import torch_xla.runtime as xr
+        # assert xr.using_spmd(), "XLA_USE_SPMD must be 1 before PG init"
         # General backend setup for other backends (gloo, nccl, etc.)
         if dist.is_initialized():
             logger.warning("Process group already initialized. Skipping.")
             return
+        print(">>> os.environ[\"XLA_USE_SPMD\"]", os.environ["XLA_USE_SPMD"])
         dist.init_process_group(
             backend='xla',
             init_method='xla://',
