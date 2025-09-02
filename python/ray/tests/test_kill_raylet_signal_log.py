b430dad3d37ddd71504bba75d6a667e64a72e193
dayshah <dhyey2019@gmail.com>
2025-09-01 23:12:46 -0700

    [core] Fix test_kill_raylet_signal_log on mac
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- python/ray/tests/test_kill_raylet_signal_log.py
+++ python/ray/tests/test_kill_raylet_signal_log.py
@@ -1,7 +1,5 @@
 import signal
 import sys
-
-# Import psutil after ray so the packaged version is used.
 import psutil
 import pytest
 
@@ -14,31 +12,25 @@ def get_pid(name):
     for pid in pids:
         if name in pid.name():
             return pid.pid
-
     return -1
 
 
-def check_result(filename, num_signal, check_key):
-    ray.init(num_cpus=1)
+@pytest.mark.skipif(sys.platform == "win32", reason="Not support on Windows.")
+def test_kill_raylet_signal_log(ray_start_regular):
     session_dir = ray._private.worker._global_node.get_session_dir_path()
-    raylet_out_path = filename.format(session_dir)
+    raylet_out_path = "{}/logs/raylet.err".format(session_dir)
     pid = get_pid("raylet")
     assert pid > 0
     p = psutil.Process(pid)
-    p.send_signal(num_signal)
+    p.send_signal(signal.SIGABRT)
     p.wait(timeout=15)
 
-    def check_file():
+    def check_for_sigabrt_in_log():
         with open(raylet_out_path) as f:
             s = f.read()
-            return check_key in s
+            return "SIGABRT" in s
 
-    wait_for_condition(check_file)
-
-
-@pytest.mark.skipif(sys.platform == "win32", reason="Not support on Windows.")
-def test_kill_raylet_signal_log(shutdown_only):
-    check_result("{}/logs/raylet.err", signal.SIGABRT, "SIGABRT")
+    wait_for_condition(check_for_sigabrt_in_log)
 
 
 if __name__ == "__main__":
