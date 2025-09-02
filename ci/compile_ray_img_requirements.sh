0736aed671b5ec1b2aeba62ad8979592d24daa35
elliot-barn <elliot.barnwell@anyscale.com>
2025-09-02 18:15:25 +0000

    updating rayimg compile job
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/compile_ray_img_requirements.sh
+++ ci/compile_ray_img_requirements.sh
@@ -5,16 +5,16 @@ set -euo pipefail
 PYTHON_CODE="$(python -c "import sys; v=sys.version_info; print(f'py{v.major}{v.minor}')")"
 echo "--- Python version: ${PYTHON_CODE}"
 
-mkdir -p tmp-raydeps
+# mkdir -p tmp-raydeps
 
-echo "ray[all]==3.0.0.dev0" > tmp-raydeps/requirements.txt
+echo "ray[all]==100.0.0-dev" > python/ray-requirement.txt
 
 # Remove the GPU constraints
-cp python/requirements_compiled.txt tmp-raydeps/requirements_compiled.txt
-sed -i '/^--extra-index-url /d' tmp-raydeps/requirements_compiled.txt
-sed -i '/^--find-links /d' tmp-raydeps/requirements_compiled.txt
+# cp python/requirements_compiled.txt tmp-raydeps/requirements_compiled.txt
+# sed -i '/^--extra-index-url /d' tmp-raydeps/requirements_compiled.txt
+# sed -i '/^--find-links /d' tmp-raydeps/requirements_compiled.txt
 
 bazel run //ci/raydepsets:raydepsets -- build ci/raydepsets/rayimg.depsets.yaml
 
-rm -rf tmp-raydeps
+rm python/ray-requirement.txt
 echo "--- Done"
