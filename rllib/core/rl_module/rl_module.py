55eb2e0899e30ed08bc1550ad731fdc1f6e5b685
Daniel Sperber <github.blurry@9ox.net>
2025-09-04 19:18:04 +0200

    [doc] fix: Typo and missing import in doc (#48311)
    <!-- Thank you for your contribution! Please review
    https://github.com/ray-project/ray/blob/master/CONTRIBUTING.rst before
    opening a pull request. -->
    
    <!-- Please add a reviewer to the assignee section when you create a PR.
    If you don't have the access to it, we will shortly find a reviewer and
    assign them to your PR. -->
    
    ## Why are these changes needed?
    
    I realized that the code samples of
    [`RLModule`](https://docs.ray.io/en/latest/rllib/package_ref/doc/ray.rllib.core.rl_module.rl_module.RLModule.html#ray.rllib.core.rl_module.rl_module.RLModule)
    were incomplete and missed some imports.
    
    Another code sample lacked a comma for correct syntax.
    
    ## Related issue number
    
    NA/
    
    ## Checks
    
    - [x] I've signed off every commit(by using the -s flag, i.e., `git
    commit -s`) in this PR.
    - [x] I've run `scripts/format.sh` to lint the changes in this PR.
    - [x] I've included any doc changes needed for
    https://docs.ray.io/en/master/.
    - [x] I've added any new APIs to the API Reference. For example, if I
    added a
    method in Tune, I've added it in `doc/source/tune/api/` under the
               corresponding `.rst` file.
    - [x] I've made sure the tests are passing. Note that there might be a
    few flaky tests, see the recent failures at https://flakey-tests.ray.io/
    - Testing Strategy
       - [ ] Unit tests
       - [ ] Release tests
       - [ ] This PR is not tested :(
    
    ---------
    
    Signed-off-by: Daraan <github.blurry@9ox.net>
    Signed-off-by: Daniel <github.blurry@9ox.net>
    Signed-off-by: Douglas Strodtman <douglas@anyscale.com>

--- rllib/core/rl_module/rl_module.py
+++ rllib/core/rl_module/rl_module.py
@@ -269,6 +269,8 @@ class RLModule(Checkpointable, abc.ABC):
             DefaultPPOTorchRLModule
         )
         from ray.rllib.algorithms.ppo.ppo_catalog import PPOCatalog
+        from ray.rllib.core.rl_module.rl_module import RLModuleSpec
+        from ray.rllib.core.rl_module.default_model_config import DefaultModelConfig
         import gymnasium as gym
         import torch
 
@@ -300,6 +302,12 @@ class RLModule(Checkpointable, abc.ABC):
 
     .. testcode::
 
+        from ray.rllib.algorithms.ppo.torch.ppo_torch_rl_module import (
+            PPOTorchRLModule
+        )
+        from ray.rllib.algorithms.ppo.ppo_catalog import PPOCatalog
+        from ray.rllib.core.rl_module.rl_module import RLModuleSpec
+        from ray.rllib.core.rl_module.default_model_config import DefaultModelConfig
         import gymnasium as gym
         import torch
 
@@ -327,6 +335,12 @@ class RLModule(Checkpointable, abc.ABC):
 
     .. testcode::
 
+        from ray.rllib.algorithms.ppo.torch.ppo_torch_rl_module import (
+            PPOTorchRLModule
+        )
+        from ray.rllib.algorithms.ppo.ppo_catalog import PPOCatalog
+        from ray.rllib.core.rl_module.rl_module import RLModuleSpec
+        from ray.rllib.core.rl_module.default_model_config import DefaultModelConfig
         import gymnasium as gym
         import torch
 
