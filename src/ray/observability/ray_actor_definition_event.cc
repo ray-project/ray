9caff21cd7856e355770365523744dd2a81aca34
sampan <sampan@anyscale.com>
2025-09-05 06:41:30 +0000

    add implementations for actor definition and execution events
    Signed-off-by: sampan <sampan@anyscale.com>

---
+++ src/ray/observability/ray_actor_definition_event.cc
@@ -0,0 +1,59 @@
+// Copyright 2025 The Ray Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "ray/observability/ray_actor_definition_event.h"
+
+namespace ray {
+namespace observability {
+
+RayActorDefinitionEvent::RayActorDefinitionEvent(const rpc::ActorTableData &data,
+                                                 const std::map<std::string, std::string> &labels,
+                                                 const std::string &session_name)
+    : RayEvent<rpc::ActorDefinitionEvent>(session_name) {
+  event_type_ = rpc::events::RayEvent::ACTOR_DEFINITION_EVENT;
+
+  data_.set_actor_id(data.actor_id());
+  data_.set_job_id(data.job_id());
+  data_.set_is_detached(data.is_detached());
+  data_.set_name(data.name());
+  data_.set_ray_namespace(data.ray_namespace());
+  data_.set_serialized_runtime_env(data.serialized_runtime_env());
+  data_.set_class_name(data.class_name());
+  data_.mutable_required_resources()->insert(data.required_resources().begin(),
+                                             data.required_resources().end());
+  if (data.has_placement_group_id()) {
+    data_.set_placement_group_id(data.placement_group_id());
+  }
+  data_.mutable_labels()->insert(labels.begin(), labels.end());
+  data_.mutable_label_selector()->insert(data.label_selector().begin(), data.label_selector().end());
+}
+
+std::string RayActorDefinitionEvent::GetResourceId() const { return data_.actor_id(); }
+
+void RayActorDefinitionEvent::Merge(
+    RayEvent<rpc::ActorDefinitionEvent> &&other) {
+  // Definition events are static. Merging does not change the event.
+  return;
+}
+
+ray::rpc::events::RayEvent RayActorDefinitionEvent::SerializeData() const {
+  ray::rpc::events::RayEvent event;
+  event.set_source_type(rpc::events::RayEvent::GCS);
+  event.set_severity(rpc::events::RayEvent::INFO);
+  *event.mutable_actor_definition_event() = data_;
+  return event;
+}
+
+}  // namespace observability
+}  // namespace ray
---
+++ src/ray/observability/ray_actor_execution_event.cc
@@ -0,0 +1,69 @@
+// Copyright 2025 The Ray Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "ray/observability/ray_actor_execution_event.h"
+
+namespace ray {
+namespace observability {
+
+RayActorExecutionEvent::RayActorExecutionEvent(
+    const rpc::ActorTableData &data,
+    rpc::ActorExecutionEvent::State state,
+    const std::string &session_name)
+    : RayEvent<rpc::ActorExecutionEvent>(session_name) {
+  event_type_ = rpc::events::RayEvent::ACTOR_EXECUTION_EVENT;
+  ray::rpc::ActorExecutionEvent::StateTransition state_transition;
+  state_transition.set_state(state);
+  state_transition.mutable_timestamp()->CopyFrom(AbslTimeNanosToProtoTimestamp(
+      absl::ToInt64Nanoseconds(absl::Now() - absl::UnixEpoch())));
+
+  // Set state specific fields
+  if (state == rpc::ActorExecutionEvent::ALIVE) {
+    if (data.has_node_id()) {
+      state_transition.set_node_id(data.node_id());
+    }
+    state_transition.set_pid(data.pid());
+    state_transition.set_repr_name(data.repr_name());
+  }
+
+  if (state == rpc::ActorExecutionEvent::DEAD) {
+    if (data.has_death_cause()) {
+      *state_transition.mutable_death_cause() = data.death_cause();
+    }
+  }
+
+  data_.set_actor_id(data.actor_id());
+  data_.mutable_states()->Add(std::move(state_transition));
+}
+
+std::string RayActorExecutionEvent::GetResourceId() const { return data_.actor_id(); }
+
+void RayActorExecutionEvent::Merge(
+    RayEvent<rpc::ActorExecutionEvent> &&other) {
+  auto &&other_event = static_cast<RayActorExecutionEvent &&>(other);
+  for (auto &state : *other_event.data_.mutable_states()) {
+    data_.mutable_states()->Add(std::move(state));
+  }
+}
+
+ray::rpc::events::RayEvent RayActorExecutionEvent::SerializeData() const {
+  ray::rpc::events::RayEvent event;
+  event.set_source_type(rpc::events::RayEvent::GCS);
+  event.set_severity(rpc::events::RayEvent::INFO);
+  *event.mutable_actor_execution_event() = data_;
+  return event;
+}
+
+}  // namespace observability
+}  // namespace ray
