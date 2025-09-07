5070b06395f707897989248d08b9f1f39d08c9a4
Rueian <rueian@anyscale.com>
2025-09-07 11:14:32 -0700

    [core] replace node_manager_client with raylet_client_lib (#56261)
    Signed-off-by: Rueian <rueian@anyscale.com>

--- src/ray/core_worker/core_worker.h
+++ src/ray/core_worker/core_worker.h
@@ -52,7 +52,7 @@
 #include "ray/ipc/raylet_ipc_client_interface.h"
 #include "ray/pubsub/publisher.h"
 #include "ray/pubsub/subscriber.h"
-#include "ray/raylet_client/raylet_client_interface.h"
+#include "ray/rpc/raylet/raylet_client_interface.h"
 #include "ray/util/process.h"
 #include "ray/util/shared_lru.h"
 #include "src/ray/protobuf/pubsub.pb.h"
