3bb6311920a5363a292e898e518ed3572ead71cd
abrar <abrar@anyscale.com>
2025-09-08 22:49:13 +0000

    fix tests
    Signed-off-by: abrar <abrar@anyscale.com>

--- python/ray/serve/tests/test_deploy_app_2.py
+++ python/ray/serve/tests/test_deploy_app_2.py
@@ -668,6 +668,7 @@ def test_num_replicas_auto_basic(serve_instance):
         "downscaling_factor": None,
         "smoothing_factor": 1.0,
         "initial_replicas": None,
+        "policy": {"name": "ray.serve.autoscaling_policy:default_autoscaling_policy"},
     }
 
     h = serve.get_app_handle(SERVE_DEFAULT_APP_NAME)
