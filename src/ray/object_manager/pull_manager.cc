a9634b7601f225a0c9acc6f423349dd9158a7ecb
Rueian <rueian@anyscale.com>
2025-09-09 16:15:41 -0700

    [core] Fix idle pull manager when it closes to the quota
    Signed-off-by: Rueian <rueian@anyscale.com>

--- src/ray/object_manager/pull_manager.cc
+++ src/ray/object_manager/pull_manager.cc
@@ -298,6 +298,12 @@ void PullManager::UpdatePullsBasedOnAvailableMemory(int64_t num_bytes_available)
                                  /*quota_margin=*/0L,
                                  &object_ids_to_cancel);
 
+  // Remove objects from cancellation set if they are also in the pull set,
+  // since we still want to pull these objects.
+  for (const auto &obj_id : objects_to_pull) {
+    object_ids_to_cancel.erase(obj_id);
+  }
+
   // Call the cancellation callbacks outside of the lock.
   for (const auto &obj_id : object_ids_to_cancel) {
     RAY_LOG(DEBUG) << "Not enough memory to create requested object " << obj_id
@@ -308,9 +314,7 @@ void PullManager::UpdatePullsBasedOnAvailableMemory(int64_t num_bytes_available)
   {
     absl::MutexLock lock(&active_objects_mu_);
     for (const auto &obj_id : objects_to_pull) {
-      if (object_ids_to_cancel.count(obj_id) == 0) {
-        TryToMakeObjectLocal(obj_id);
-      }
+      TryToMakeObjectLocal(obj_id);
     }
   }
 }
