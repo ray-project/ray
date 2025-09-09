b10c27b973a7cefc41dc68b83239114ba1522751
dayshah <dhyey2019@gmail.com>
2025-09-09 11:34:14 -0700

    fix
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- src/ray/core_worker/task_manager.cc
+++ src/ray/core_worker/task_manager.cc
@@ -391,7 +391,7 @@ std::optional<rpc::ErrorType> TaskManager::ResubmitTask(
   // issue #54260.
   RAY_LOG(INFO) << "Resubmitting task that produced lost plasma object, attempt #"
                 << spec.AttemptNumber() << ": " << spec.DebugString();
-  queue_retry_callback_(spec, /*delay_ms=*/0);
+  async_retry_task_callback_(spec, /*delay_ms=*/0);
 
   return std::nullopt;
 }
@@ -399,8 +399,8 @@ std::optional<rpc::ErrorType> TaskManager::ResubmitTask(
 void TaskManager::SetupTaskEntryForResubmit(TaskEntry &task_entry) {
   task_entry.MarkRetry();
   // NOTE(rickyx): We only increment the AttemptNumber on the task spec when
-  // `queue_retry_callback_` is invoked. In order to record the correct status change for
-  // the new task attempt, we pass the attempt number explicitly.
+  // `async_retry_task_callback_` is invoked. In order to record the correct status change
+  // for the new task attempt, we pass the attempt number explicitly.
   SetTaskStatus(task_entry,
                 rpc::TaskStatus::PENDING_ARGS_AVAIL,
                 /* state_update */ std::nullopt,
@@ -478,7 +478,7 @@ void TaskManager::MarkGeneratorFailedAndResubmit(const TaskID &task_id) {
   // Note: Don't need to call UpdateReferencesForResubmit because CompletePendingTask or
   // FailPendingTask are not called when this is. Therefore, RemoveFinishedTaskReferences
   // never happened for this task.
-  queue_retry_callback_(spec, /*delay_ms*/ 0);
+  async_retry_task_callback_(spec, /*delay_ms*/ 0);
 }
 
 void TaskManager::DrainAndShutdown(std::function<void()> shutdown) {
@@ -1194,7 +1194,7 @@ bool TaskManager::RetryTaskIfPossible(const TaskID &task_id,
                                  spec.AttemptNumber(),
                                  RayConfig::instance().task_oom_retry_delay_base_ms())
                            : RayConfig::instance().task_retry_delay_ms();
-    queue_retry_callback_(spec, delay_ms);
+    async_retry_task_callback_(spec, delay_ms);
     return true;
   } else {
     RAY_LOG(INFO) << "No retries left for task " << spec.TaskId()
