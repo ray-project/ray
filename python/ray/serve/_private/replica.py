f8aa57bd5fb8b982fd7aeb99399a8db97ef4f065
Doyoung Kim <34902420+landscapepainter@users.noreply.github.com>
2025-09-11 10:52:06 -0700

    [Serve] Refactor test_deploy_app_2.py and add port attributes (#55748)
    This PR refactors test_deploy_app_2.py and adds additional attributes to
    `deployment_state.py` and `replica.py`
    
    ---------
    
    Signed-off-by: doyoung <doyoung@anyscale.com>
    Signed-off-by: Doyoung Kim <34902420+landscapepainter@users.noreply.github.com>
    Signed-off-by: zac <zac@anyscale.com>

--- python/ray/serve/_private/replica.py
+++ python/ray/serve/_private/replica.py
@@ -425,8 +425,10 @@ class ReplicaBase(ABC):
             ingress=ingress,
         )
 
-        self._port: Optional[int] = None
+        self._internal_grpc_port: Optional[int] = None
         self._docs_path: Optional[str] = None
+        self._http_port: Optional[int] = None
+        self._grpc_port: Optional[int] = None
 
     @property
     def max_ongoing_requests(self) -> int:
@@ -441,8 +443,10 @@ class ReplicaBase(ABC):
             self._version.deployment_config,
             self._version,
             self._initialization_latency,
-            self._port,
+            self._internal_grpc_port,
             self._docs_path,
+            self._http_port,
+            self._grpc_port,
             current_rank,
         )
 
