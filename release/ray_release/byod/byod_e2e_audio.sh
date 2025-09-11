ab9fac34e7be53366dd696a3128a7476d42b9303
elliot-barn <elliot.barnwell@anyscale.com>
2025-09-11 19:43:11 +0000

    updating lock file name
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- release/ray_release/byod/byod_e2e_audio.sh
+++ release/ray_release/byod/byod_e2e_audio.sh
@@ -3,4 +3,4 @@
 set -exo pipefail
 
 # Install Python dependencies
-uv pip sync e2e_audio_py311_cu128.lock --system --index-strategy unsafe-best-match
+uv pip sync llm_example_py311_cu128.lock --system --index-strategy unsafe-best-match
