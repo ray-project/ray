2fae70996edbf06acc348c39101fec0b36d8446c
Qiaolin-Yu <liin1211@outlook.com>
2025-09-12 19:24:18 +0000

    upd

--- src/ray/core_worker/task_manager.cc
+++ src/ray/core_worker/task_manager.cc
@@ -1170,10 +1170,13 @@ bool TaskManager::RetryTaskIfPossible(const TaskID &task_id,
                     worker::TaskStatusEvent::TaskStateUpdate(error_info));
       task_entry.MarkRetry();
       // Push the error to the driver if the task will still retry.
-      push_error_callback_(task_entry.spec_.JobId(),
+      Status push_error_status = push_error_callback_(task_entry.spec_.JobId(),
                            rpc::ErrorType_Name(error_info.error_type()),
                            error_info.error_message(),
                            current_time_ms());
+      if (!push_error_status.ok()) {
+        RAY_LOG(INFO) << "Failed to push error to driver for task " << spec.TaskId();
+      }
 
       // Mark the new status and also include task spec info for the new attempt.
       SetTaskStatus(task_entry,
