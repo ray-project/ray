4477fc74d181d387bb535034a689019b38cd1a48
elliot-barn <elliot.barnwell@anyscale.com>
2025-09-12 23:17:18 +0000

    uv pip sync on root directory
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- release/ray_release/byod/byod_e2e_audio.sh
+++ release/ray_release/byod/byod_e2e_audio.sh
@@ -3,4 +3,4 @@
 set -exo pipefail
 
 # Install Python dependencies
-uv pip sync /home/ray/lock_file.lock --system --index-strategy unsafe-best-match
+# uv pip sync /home/ray/lock_file.lock --system --index-strategy unsafe-best-match
