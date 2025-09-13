c6dd3a38be8a17a6cf567f779dd0dd88811f5a07
Timothy Seah <tseah@anyscale.com>
2025-09-12 17:53:48 -0700

    comment for v2 migration
    Signed-off-by: Timothy Seah <tseah@anyscale.com>

--- release/air_tests/air_benchmarks/workloads/torch_benchmark.py
+++ release/air_tests/air_benchmarks/workloads/torch_benchmark.py
@@ -320,6 +320,7 @@ def train_torch_ray_air(
         train_loop_per_worker=train_loop,
         train_loop_config=config,
         scaling_config=ScalingConfig(
+            # TODO: remove this for v2 migration
             trainer_resources={"CPU": 0},
             num_workers=num_workers,
             resources_per_worker={"CPU": cpus_per_worker},
