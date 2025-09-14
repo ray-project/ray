fc4bad54897c375e53808b2d840526d1de303fe7
Balaji Veeramani <bveeramani@berkeley.edu>
2025-09-14 09:01:11 -0700

    Fix tests
    Signed-off-by: Balaji Veeramani <bveeramani@berkeley.edu>

--- python/ray/data/tests/test_parquet.py
+++ python/ray/data/tests/test_parquet.py
@@ -100,7 +100,7 @@ def test_write_parquet_partition_cols(
 def test_include_paths(
     ray_start_regular_shared, tmp_path, target_max_block_size_infinite_or_default
 ):
-    path = os.path.join(tmp_path, "test.txt")
+    path = os.path.join(tmp_path, "test.parquet")
     table = pa.Table.from_pydict({"animals": ["cat", "dog"]})
     pq.write_table(table, path)
 
@@ -1406,8 +1406,12 @@ def test_read_null_data_in_first_file(
     # The `read_parquet` implementation might infer the schema from the first file.
     # This test ensures that implementation handles the case where the first file has no
     # data and the inferred type is `null`.
-    pq.write_table(pa.Table.from_pydict({"data": [None, None, None]}), tmp_path / "1")
-    pq.write_table(pa.Table.from_pydict({"data": ["spam", "ham"]}), tmp_path / "2")
+    pq.write_table(
+        pa.Table.from_pydict({"data": [None, None, None]}), tmp_path / "1.parquet"
+    )
+    pq.write_table(
+        pa.Table.from_pydict({"data": ["spam", "ham"]}), tmp_path / "2.parquet"
+    )
 
     ds = ray.data.read_parquet(tmp_path)
 
@@ -1419,16 +1423,6 @@ def test_read_null_data_in_first_file(
         {"data": "ham"},
         {"data": "spam"},
     ]
-
-
-def test_read_invalid_file_extensions_emits_warning(
-    tmp_path, ray_start_regular_shared, target_max_block_size_infinite_or_default
-):
-    table = pa.Table.from_pydict({})
-    pq.write_table(table, tmp_path / "no_extension")
-
-    with pytest.warns(FutureWarning, match="file_extensions"):
-        ray.data.read_parquet(tmp_path)
 
 
 def test_parquet_row_group_size_001(ray_start_regular_shared, tmp_path):
