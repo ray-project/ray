5d1436c5b0f77935ea7955891aad32981f449c8b
Cuong Nguyen <can@anyscale.com>
2025-09-15 15:34:59 -0700

    [core][1ev-debt/02] implement event merge logic
    Signed-off-by: Cuong Nguyen <can@anyscale.com>

--- src/ray/observability/ray_event_recorder.cc
+++ src/ray/observability/ray_event_recorder.cc
@@ -42,10 +42,19 @@ void RayEventRecorder::ExportEvents() {
   }
   rpc::events::AddEventsRequest request;
   rpc::events::RayEventsData ray_event_data;
-  // TODO(#56391): To further optimize the performance, we can merge multiple
-  // events with the same resource ID into a single event.
+  // group the event in the buffer_ by their entity id and type; then for each group, 
+  // merge the events into a single event.
+  absl::flat_hash_map<std::string, std::vector<std::unique_ptr<RayEventInterface>>> event_groups;
   for (auto &event : buffer_) {
-    rpc::events::RayEvent ray_event = std::move(*event).Serialize();
+    event_groups[event->GetEntityId() + "_" + std::to_string(event->GetEventType())].push_back(std::move(event));
+  }
+  for (auto &[entity_id_type, events] : event_groups) {
+    // merge the later event in the group into the first event, then add the merged 
+    // event to the request.
+    for (size_t i = 1; i < events.size(); i++) {
+      events[0]->Merge(std::move(*events[i]));
+    }
+    rpc::events::RayEvent ray_event = std::move(*events[0]).Serialize();
     *ray_event_data.mutable_events()->Add() = std::move(ray_event);
   }
   *request.mutable_events_data() = std::move(ray_event_data);
