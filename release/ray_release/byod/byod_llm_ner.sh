279c41832b7e9cc75cbef39c69920858e0e2a5cb
elliot-barn <elliot.barnwell@anyscale.com>
2025-09-17 15:56:07 +0000

    reverting byod_llm_ner.sh
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- release/ray_release/byod/byod_llm_ner.sh
+++ release/ray_release/byod/byod_llm_ner.sh
@@ -6,7 +6,13 @@ set -exo pipefail
 # pip3 install --no-cache-dir -r https://raw.githubusercontent.com/anyscale/e2e-llm-workflows/refs/heads/main/lockfile.txt
 
 # Install Python dependencies
-# uv pip sync llm_example_py311_cu128.lock --system --index-strategy unsafe-best-match
+pip3 install --no-cache-dir \
+    "xgrammar==0.1.11" \
+    "pynvml==12.0.0" \
+    "hf_transfer==0.1.9" \
+    "tensorboard==2.19.0" \
+    "git+https://github.com/hiyouga/LLaMA-Factory.git@ac8c6fdd3ab7fb6372f231f238e6b8ba6a17eb16#egg=llamafactory"
+
 
 # Env vars
 export HF_HUB_ENABLE_HF_TRANSFER=1
