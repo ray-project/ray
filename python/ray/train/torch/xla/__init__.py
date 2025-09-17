7201cce46939182edaf151f59b2c339fdd745aed
Lehui Liu <lehui@anyscale.com>
2025-09-17 15:54:10 -0700

    remove is_xla_mesh etc
    Signed-off-by: Lehui Liu <lehui@anyscale.com>

--- python/ray/train/torch/xla/__init__.py
+++ python/ray/train/torch/xla/__init__.py
@@ -1,8 +1,8 @@
 from ray.train.torch.xla.config import TorchXLAConfig
-from ray.train.torch.xla.utils import get_xla_mesh, is_xla_backend
+# from ray.train.torch.xla.utils import get_xla_mesh, is_xla_backend
 
 __all__ = [
     "TorchXLAConfig",
-    "get_xla_mesh",
-    "is_xla_backend",
+    # "get_xla_mesh",
+    # "is_xla_backend",
 ]
