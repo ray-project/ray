b23c64ffdcbde632a965fc4a008887c3297123f3
Srinath Krishnamachari <68668616+srinathk10@users.noreply.github.com>
2025-09-18 13:49:21 -0700

    [Train] Enable debug logging; fix default actor_locality_enabled (#56632)
    - Enable RAY_DATA_DEBUG_RESOURCE_MANAGER for debugging resource manager.
    - Enable Ray Data progress bar.
    - Set actor_locality_enabled to True
    
    ---------
    
    Signed-off-by: Srinath Krishnamachari <srinath.krishnamachari@anyscale.com>
    Signed-off-by: Srinath Krishnamachari <68668616+srinathk10@users.noreply.github.com>
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- release/train_tests/benchmark/config.py
+++ release/train_tests/benchmark/config.py
@@ -41,11 +41,11 @@ class RecsysConfig(TaskConfig):
 class RayDataConfig(DataLoaderConfig):
     # NOTE: Optional[int] doesn't play well with argparse.
     local_buffer_shuffle_size: int = -1
-    enable_operator_progress_bars: bool = False
+    enable_operator_progress_bars: bool = True
     ray_data_prefetch_batches: int = 4
     ray_data_override_num_blocks: int = -1
     locality_with_output: bool = False
-    actor_locality_enabled: bool = False
+    actor_locality_enabled: bool = True
     enable_shard_locality: bool = True
     preserve_order: bool = False
     ray_data_pin_memory: bool = False
