9ffd52c39d49080edcd12170c84e71d646e59c83
Alexey Kudinkin <ak@anyscale.com>
2025-09-19 14:24:10 -0700

    Revisited test to make it E2E
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/data/tests/test_hash_shuffle.py
+++ python/ray/data/tests/test_hash_shuffle.py
@@ -1,15 +1,62 @@
+from typing import Optional
 from unittest.mock import MagicMock
+
+import pytest
 
 from ray.data import DataContext, ExecutionResources
 from ray.data._internal.execution.interfaces import PhysicalOperator
 from ray.data._internal.execution.operators.join import JoinOperator
+from ray.data._internal.logical.interfaces import LogicalOperator
 from ray.data._internal.logical.operators.join_operator import JoinType
 from ray.data._internal.util import GiB
+from ray.data.block import BlockMetadata
 
 
-def test_default_shuffle_aggregator_args(ray_start_regular):
+@pytest.mark.parametrize(
+    "target_num_partitions,expected_num_aggregators,expected_ray_remote_args", [
+        # User-configured 16 partitions
+        (16, 1, {
+            "max_concurrency": 8,   # 1 aggregator, 16 partitions (capped at 8)
+            "num_cpus": 0.25,       # 1 CPU * 25%
+            "memory": 4966055965,   # ~4.7Gb
+            "scheduling_strategy": "SPREAD",
+        }),
+        # User-configured 1 partition (substantially more joining overhead)
+        (1, 1, {
+            "max_concurrency": 1,   # 1 aggregator
+            "num_cpus": 0.25,       # 1 CPU * 25%
+            "memory": 15032385620,  # ~15Gb
+            "scheduling_strategy": "SPREAD",
+        }),
+        # Derived 10 partitions
+        (None, 1, {
+            "max_concurrency": 8,   # 1 aggregator, 10 partitions (capped at 8
+            "num_cpus": 0.25,       # 1 CPU * 25%
+            "memory": 5368709150,   # ~5Gb
+            "scheduling_strategy": "SPREAD",
+        }),
+    ]
+)
+def test_default_shuffle_aggregator_args(
+    ray_start_regular,
+    # input_num_blocks: int,
+    expected_num_aggregators,
+    target_num_partitions: Optional[int],
+    expected_ray_remote_args,
+):
+    parent_logical_op_mock = MagicMock(LogicalOperator)
+    parent_logical_op_mock.infer_metadata.return_value = BlockMetadata(
+        num_rows=None,
+        size_bytes=1 * GiB,
+        exec_stats=None,
+        input_files=None,
+    )
+    parent_logical_op_mock.estimated_num_outputs.return_value = 10
+
     parent_op_mock = MagicMock(PhysicalOperator)
+
     parent_op_mock._output_dependencies = []
+    parent_op_mock._logical_operators = [parent_logical_op_mock]
 
     op = JoinOperator(
         left_input_op=parent_op_mock,
@@ -18,50 +65,8 @@ def test_default_shuffle_aggregator_args(ray_start_regular):
         left_key_columns=("id",),
         right_key_columns=("id",),
         join_type=JoinType.INNER,
-        num_partitions=16,
+        num_partitions=target_num_partitions,
     )
 
-    # - 1 partition per aggregator
-    # - No partition size hint
-    args = op._get_default_aggregator_ray_remote_args(
-        num_partitions=16,
-        num_aggregators=16,
-        total_available_cluster_resources=ExecutionResources(cpu=4),
-        partition_size_hint=None,
-    )
-
-    assert {
-        "num_cpus": 0.025,  # 4 cores * 10% / 16
-        "memory": 939524096,
-        "scheduling_strategy": "SPREAD",
-    } == args
-
-    # - 4 partitions per aggregator
-    # - No partition size hint
-    args = op._get_default_aggregator_ray_remote_args(
-        num_partitions=64,
-        num_aggregators=16,
-        total_available_cluster_resources=ExecutionResources(cpu=8),
-        partition_size_hint=None,
-    )
-
-    assert {
-        "num_cpus": 0.05,  # 8 cores * 10% / 16
-        "memory": 1744830464,
-        "scheduling_strategy": "SPREAD",
-    } == args
-
-    # - 4 partitions per aggregator
-    # - No partition size hint
-    args = op._get_default_aggregator_ray_remote_args(
-        num_partitions=64,
-        num_aggregators=16,
-        total_available_cluster_resources=ExecutionResources(cpu=1),
-        partition_size_hint=1 * GiB,
-    )
-
-    assert {
-        "num_cpus": 0.00625,  # 1 cores * 10% / 16
-        "memory": 13958643712,
-        "scheduling_strategy": "SPREAD",
-    } == args
+    assert op._aggregator_pool._aggregator_ray_remote_args == expected_ray_remote_args
+    assert op._aggregator_pool.num_aggregators == expected_num_aggregators
