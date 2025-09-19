6b03c4a346aa652c87b6be91b8bd0a251c80030f
avigyabb <98926738+avigyabb@users.noreply.github.com>
2025-09-19 01:14:08 -0700

    Update nixl_tensor_transport.py
    Signed-off-by: avigyabb <98926738+avigyabb@users.noreply.github.com>

--- python/ray/experimental/collective/nixl_tensor_transport.py
+++ python/ray/experimental/collective/nixl_tensor_transport.py
@@ -30,7 +30,7 @@ class NixlTensorTransport(TensorTransportManager):
         ) -> bool:
             try:
                 from ray.util.collective.collective import get_group_handle
-                
+
                 nixl_backend = get_group_handle(NIXL_GROUP_NAME)
                 return nixl_backend is not None
             except Exception:
