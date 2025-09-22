7d955e7eddb5dfdd48cde6d608a9c7a7aeb06b13
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-09-22 08:56:18 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- doc/source/ray-core/doc_code/nested-tasks.py
+++ doc/source/ray-core/doc_code/nested-tasks.py
@@ -20,6 +20,8 @@ def h():
     # Call f 4 times, block until those 4 tasks finish,
     # retrieve the results, and return the values.
     return ray.get([f.remote() for _ in range(4)])
+
+
 # __nested_end__
 
 ray.init(num_cpus=4, num_gpus=1)
@@ -33,6 +35,8 @@ assert all(isinstance(o, ray.ObjectRef) for o in obj_refs)
 @ray.remote(num_cpus=1, num_gpus=1)
 def g():
     return ray.get(f.remote())
+
+
 # __yield_end__
 
 assert ray.get(g.remote()) == 1
