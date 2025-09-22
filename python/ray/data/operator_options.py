d47aba3928be8d02e5bc5d39c7da7ab6d7b1740c
Praveen Gorthy <praveeng@anyscale.com>
2025-09-21 20:04:40 -0700

    Lint and test failures
    Signed-off-by: Praveen Gorthy <praveeng@anyscale.com>

---
+++ python/ray/data/operator_options.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+from ray.util.annotations import PublicAPI
+
+@PublicAPI(stability="alpha")
+@dataclass
+class OperatorOptions:
+    """Options for configuring individual operators."""
+
+    # Whether to disable fusion for this operator. By default, fusion is enabled.
+    disable_fusion: bool = False
