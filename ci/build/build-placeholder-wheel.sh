35b635c43f665593720c0c8b7996e2055f589c05
elliot-barn <elliot.barnwell@anyscale.com>
2025-09-22 19:01:35 +0000

    removing constraint from requirements file
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/build/build-placeholder-wheel.sh
+++ ci/build/build-placeholder-wheel.sh
@@ -7,5 +7,5 @@ export RAY_DEBUG_BUILD=deps-only
 PYTHON_VERSIONS=("3.9" "3.10" "3.11" "3.12")
 
 for PYTHON_VERSION in "${PYTHON_VERSIONS[@]}"; do
-  uv build --wheel --directory python/ -o ../.whl/ --force-pep517 --python "$PYTHON_VERSION"
+  uv build --wheel --directory python/ -o ../.whl/ --python "$PYTHON_VERSION"
 done
