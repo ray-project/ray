4b9a7feadd6108b67c04beb436bf406330d9663d
iamjustinhsu <jhsu@anyscale.com>
2025-09-23 10:33:05 -0700

    add more ops
    Signed-off-by: iamjustinhsu <jhsu@anyscale.com>

--- python/ray/data/_internal/logical/rules/shuffle_fusion.py
+++ python/ray/data/_internal/logical/rules/shuffle_fusion.py
@@ -1,20 +1,19 @@
-
-
 import copy
 import logging
-from dataclasses import dataclass
-from typing import Dict, List, Optional, Union
 
 from ray.data._internal.logical.interfaces import (
     LogicalOperator,
-    LogicalOperatorSupportsProjectionPushdown,
     LogicalPlan,
+    Operator,
     Rule,
 )
-from ray.data._internal.logical.operators.all_to_all_operator import Repartition
-from ray.data._internal.logical.operators.map_operator import Project
-from ray.data._internal.logical.operators.read_operator import Read
-from ray.data.expressions import Expr
+from ray.data._internal.logical.operators.all_to_all_operator import (
+    RandomShuffle,
+    Repartition,
+)
+from ray.data._internal.logical.operators.map_operator import (
+    StreamingRepartition,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -39,24 +38,48 @@ class ShuffleFusion(Rule):
         if len(parent_ops) == 1:
             parent_op = parent_ops[0]
             if isinstance(parent_op, Repartition) and isinstance(child_op, Repartition):
-                # TODO(justin): make this whole process a helper function, I see it a lot.
-
-                # 1. Create a new operator
-                twin_op = copy.copy(child_op)
-
-                # 2. Rewire parent.inputs (aka grandparent) -> twin_op
-                grandparent_ops = parent_op.input_dependencies
-                twin_op.input_dependencies = grandparent_ops
-
-                # 3. Rewire grandparent -> twin_op
-                assert len(grandparent_ops) == 1
-                grandparent_op = grandparent_ops[0]
-                grandparent_op.output_dependencies = [twin_op]
-                return twin_op
+                return _disown_op(parent_op)
+            elif isinstance(parent_op, StreamingRepartition) and isinstance(
+                child_op, Repartition
+            ):
+                return _disown_op(parent_op)
+            elif isinstance(parent_op, RandomShuffle) and isinstance(
+                child_op, RandomShuffle
+            ):
+                return _disown_op(parent_op)
+            elif isinstance(parent_op, RandomShuffle) and isinstance(
+                child_op, Repartition
+            ):
+                twin_op: Repartition = _disown_op(parent_op)
+                twin_op._shuffle = True
             else:
-                # TODO(justin): case for other rules
                 ...
-                
 
         return op
 
+
+def _disown_op(child_op: Operator) -> Operator:
+    """Visually is does this:
+
+        Before: parent -> child -> grandchild
+        After: parent -> grandchild
+
+    This method assumes that the all of the operators are 1-to-1
+
+    Returns: A new(copy) grandchild operator
+    """
+
+    # TODO(justin): check for boundary conditions
+    parent_op = []
+    if child_op.input_dependencies:
+        parent_op = child_op.input_dependencies[0]
+
+    grandchild_op = []
+    if child_op.output_dependencies:
+        grandchild_op = child_op.output_dependencies[0]
+
+    grandtwin_op = copy.copy(grandchild_op)
+    grandtwin_op.input_dependencies = [parent_op]
+    parent_op.output_dependencies = [grandtwin_op]
+
+    return grandtwin_op
