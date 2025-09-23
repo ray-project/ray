b696d2e59605d352654b7eb0d95b1613a3d84438
elliot-barn <elliot.barnwell@anyscale.com>
2025-09-23 23:05:42 +0000

    creating depset for e2e_xgboost
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- release/ray_release/byod/byod_e2e_xgboost.sh
+++ release/ray_release/byod/byod_e2e_xgboost.sh
@@ -2,4 +2,4 @@
 
 set -exo pipefail
 
-pip3 install --no-cache-dir mlflow==2.19.0 scikit-learn==1.6.0 xgboost>=3.0.0
+uv pip install -r python_depset.lock --system --no-deps --index-strategy unsafe-best-match
