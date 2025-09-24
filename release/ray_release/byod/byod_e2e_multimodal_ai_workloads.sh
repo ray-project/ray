293ababe0b173f3fa284c17898967b6aa8df7082
elliot-barn <elliot.barnwell@anyscale.com>
2025-09-24 19:58:38 +0000

    adding lock file
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- release/ray_release/byod/byod_e2e_multimodal_ai_workloads.sh
+++ release/ray_release/byod/byod_e2e_multimodal_ai_workloads.sh
@@ -3,10 +3,4 @@
 set -exo pipefail
 
 # Install Python dependencies
-pip3 install --no-cache-dir \
-    "matplotlib==3.10.0" \
-    "torch==2.7.1" \
-    "transformers==4.52.3" \
-    "scikit-learn==1.6.0" \
-    "mlflow==2.19.0" \
-    "ipywidgets==8.1.3"
+uv pip install -r python_depset.lock --system --no-deps --index-strategy unsafe-best-match
