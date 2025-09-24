62d3b107d2a560f6ad8184bc107c5ee2a37bf4e9
elliot-barn <elliot.barnwell@anyscale.com>
2025-09-24 00:28:06 +0000

    installing deps for example
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- doc/source/train/examples/pytorch/distributing-pytorch/ci/tests.sh
+++ doc/source/train/examples/pytorch/distributing-pytorch/ci/tests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -euxo pipefail
+
+uv pip install -r python_depset.lock --index-strategy unsafe-best-match --system --no-deps
 python ci/nb2py.py README.ipynb README.py  # convert notebook to py script
 python README.py  # be sure to use ipython to ensure even non-python cells are executed properly
 rm README.py  # remove the generated script
