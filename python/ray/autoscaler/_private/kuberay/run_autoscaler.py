ffaa7084ab6b6bdc2c798ed39c9cdc306b9a5a62
Mengjin Yan <mengjinyan3@gmail.com>
2025-09-23 17:23:21 -0700

    update autoscaler logging to honor the user specified temp dif
    Signed-off-by: Mengjin Yan <mengjinyan3@gmail.com>

--- python/ray/autoscaler/_private/kuberay/run_autoscaler.py
+++ python/ray/autoscaler/_private/kuberay/run_autoscaler.py
@@ -24,9 +24,16 @@ logger = logging.getLogger(__name__)
 BACKOFF_S = 5
 
 
-def _get_log_dir() -> str:
+def _get_log_dir(gcs_client: GcsClient) -> str:
+    temp_dir = ray._private.utils.internal_kv_get_with_retry(
+        gcs_client,
+        "temp_dir",
+        ray_constants.KV_NAMESPACE_SESSION,
+        num_retries=ray_constants.NUM_REDIS_GET_RETRIES,
+    )
+
     return os.path.join(
-        ray._common.utils.get_ray_temp_dir(),
+        ray._common.utils.decode(temp_dir),
         ray._private.ray_constants.SESSION_LATEST,
         "logs",
     )
@@ -57,9 +64,11 @@ def run_kuberay_autoscaler(cluster_name: str, cluster_namespace: str):
             )
             time.sleep(BACKOFF_S)
 
+    gcs_client = GcsClient(ray_address)
+
     # The Ray head container sets up the log directory. Thus, we set up logging
     # only after the Ray head is ready.
-    _setup_logging()
+    _setup_logging(gcs_client)
 
     # autoscaling_config_producer reads the RayCluster CR from K8s and uses the CR
     # to output an autoscaling config.
@@ -67,14 +76,13 @@ def run_kuberay_autoscaler(cluster_name: str, cluster_namespace: str):
         cluster_name, cluster_namespace
     )
 
-    gcs_client = GcsClient(ray_address)
     if is_autoscaler_v2(fetch_from_server=True, gcs_client=gcs_client):
         from ray.autoscaler.v2.monitor import AutoscalerMonitor as MonitorV2
 
         MonitorV2(
             address=gcs_client.address,
             config_reader=KubeRayConfigReader(autoscaling_config_producer),
-            log_dir=_get_log_dir(),
+            log_dir=_get_log_dir(gcs_client),
             monitor_ip=head_ip,
         ).run()
     else:
