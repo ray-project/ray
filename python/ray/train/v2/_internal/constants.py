5ab46e0413a29eb190b717aa03e6da169f327ce7
Timothy Seah <tseah@anyscale.com>
2025-09-24 14:58:22 -0700

    [core][train] Ray Train disables blocking get inside async warning (#56757)
    # Summary
    
    Ray Train essentially has three parts: the driver, the controller actor,
    and the worker actors. We turned the controller into an async actor so
    that users can abort or get reported checkpoints from the controller
    while it is running.
    
    However, Ray Train currently calls `ray.get` several times within the
    `Controller` async actor e.g. [when waiting for the placement group to
    be
    ready](https://github.com/ray-project/ray/blob/master/python/ray/train/v2/_internal/execution/worker_group/worker_group.py#L293).
    I tried replacing all of these calls with `awaits` but ultimately
    decided against it because doing so would be a large effort (see
    https://github.com/ray-project/ray/pull/54181 for some examples,
    including changing all our callbacks to be asyncio compatible) and
    require us to handle complex corner cases like controller abortion
    cleaning up an in-progress placement group.
    
    Ultimately we decided that this was fine because it enables the
    aforementioned operations (abort and get reported checkpoints) without
    introducing any behavior regressions (the ray.get's were already
    blocking before we made everything asyncio) other than showing Ray train
    users the warning below, which has caused confusion:
    
    ```
    "Using blocking ray.get inside async actor. "
    "This blocks the event loop. Please use `await` "
    "on object ref with asyncio.gather if you want to "
    "yield execution to the event loop instead."
    ```
    
    This PR
    * introduces a new `WARN_BLOCKING_GET_INSIDE_ASYNC` env var that toggles
    whether we `logger.warning` or `logger.debug`. This warns by default so
    it is a no-op for all non Ray Train use cases.
    * Ray Train sets this env var to "0" if it is not already set. Users can
    still flip the env var if they want.
    
    # Testing
    
    Unit tests
    
    ---------
    
    Signed-off-by: Timothy Seah <tseah@anyscale.com>
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- python/ray/train/v2/_internal/constants.py
+++ python/ray/train/v2/_internal/constants.py
@@ -1,6 +1,7 @@
 import os
 from typing import Dict
 
+from ray._common.constants import RAY_WARN_BLOCKING_GET_INSIDE_ASYNC_ENV_VAR
 from ray._private.ray_constants import env_bool, env_set_by_user
 
 # Unsupported configs can use this value to detect if the user has set it.
@@ -83,8 +84,12 @@ GET_ACTOR_TIMEOUT_S: int = 2
 # GET_ACTOR_TIMEOUT_S_ENV_VAR * CONTROLLERS_TO_POLL_PER_ITERATION_ENV_VAR should be
 # way less than STATE_ACTOR_RECONCILIATION_INTERVAL_S_ENV_VAR.
 CONTROLLERS_TO_POLL_PER_ITERATION: int = 5
+
 # Environment variable for Train execution callbacks
 RAY_TRAIN_CALLBACKS_ENV_VAR = "RAY_TRAIN_CALLBACKS"
+
+# Ray Train does not warn by default when using blocking ray.get inside async actor.
+DEFAULT_RAY_WARN_BLOCKING_GET_INSIDE_ASYNC_VALUE = "0"
 
 # Environment variables to propagate from the driver to the controller,
 # and then from the controller to the workers.
@@ -100,6 +105,7 @@ ENV_VARS_TO_PROPAGATE = {
     ENABLE_WORKER_STRUCTURED_LOGGING_ENV_VAR,
     ENABLE_STATE_ACTOR_RECONCILIATION_ENV_VAR,
     STATE_ACTOR_RECONCILIATION_INTERVAL_S_ENV_VAR,
+    RAY_WARN_BLOCKING_GET_INSIDE_ASYNC_ENV_VAR,
 }
 
 
