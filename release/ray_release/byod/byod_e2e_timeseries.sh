1b8f87643127a4c7030be5e4eed0bb3ea87d77ac
elliot-barn <elliot.barnwell@anyscale.com>
2025-09-25 17:57:57 +0000

    installing deps in notebook
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- release/ray_release/byod/byod_e2e_timeseries.sh
+++ release/ray_release/byod/byod_e2e_timeseries.sh
@@ -5,7 +5,3 @@
 # -x: Print each command before executing it (for debugging)
 # -o pipefail: Fail if any command in a pipeline fails (not just the last one)
 set -exo pipefail
-
-# Install Python dependencies.
-
-uv pip install -r python_depset.lock --system --no-deps --index-strategy unsafe-best-match
