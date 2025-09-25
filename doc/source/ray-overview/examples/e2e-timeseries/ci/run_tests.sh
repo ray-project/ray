057a65987223a0789f014049c0325a553d329398
elliot-barn <elliot.barnwell@anyscale.com>
2025-09-25 18:32:00 +0000

    updating install place
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- doc/source/ray-overview/examples/e2e-timeseries/ci/run_tests.sh
+++ doc/source/ray-overview/examples/e2e-timeseries/ci/run_tests.sh
@@ -2,4 +2,6 @@
 
 set -euxo pipefail
 
+uv pip install -r python_depset.lock --system --no-deps --index-strategy unsafe-best-match
+
 jupyter execute e2e_timeseries/01-Distributed-Training.ipynb e2e_timeseries/02-Validation.ipynb e2e_timeseries/03-Serving.ipynb
