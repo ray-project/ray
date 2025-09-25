ef342aab30fc6189b57d8f8d93523dd94dc322b3
Kevin H. Luu <kevin@anyscale.com>
2025-09-24 22:21:30 -0700

    [release] Fix release launching script (#56916)
    The additional args and a logic to convert frequency into `Frequency`
    was missing from https://github.com/ray-project/ray/pull/56650
    
    ---------
    
    Signed-off-by: kevin <kevin@anyscale.com>

--- release/ray_release/scripts/custom_image_build_and_test_init.py
+++ release/ray_release/scripts/custom_image_build_and_test_init.py
@@ -103,7 +103,7 @@ def main(
     settings = get_pipeline_settings()
     env = {}
 
-    frequency = get_frequency(frequency) or settings["frequency"]
+    frequency = get_frequency(frequency) if frequency else settings["frequency"]
     prefer_smoke_tests = settings["prefer_smoke_tests"]
     test_filters = get_test_filters(test_filters) or settings["test_filters"]
     priority = settings["priority"]
