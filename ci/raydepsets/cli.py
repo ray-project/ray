c93bf36f2e2438c9fee2b7a150f11dafe1553c68
Elliot Barnwell <elliot.barnwell@anyscale.com>
2025-09-25 17:43:21 -0700

    [deps] removing default flag & updating ray img lock file (#56859)
    removing `--extra-index-url https://download.pytorch.org/whl/cpu` as
    default flag
    
    removing headers from requirements compiled before compiling ray lock
    files
    
    Updating unit tests
    
    ---------
    
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- ci/raydepsets/cli.py
+++ ci/raydepsets/cli.py
@@ -19,7 +19,6 @@ DEFAULT_UV_FLAGS = """
     --strip-extras
     --unsafe-package setuptools
     --index-url https://pypi.org/simple
-    --extra-index-url https://download.pytorch.org/whl/cpu
     --index-strategy unsafe-best-match
     --no-strip-markers
     --emit-index-url
