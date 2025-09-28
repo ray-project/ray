8a4ad88ba7a242c8dd110a13516060cfcfcdd640
elliot-barn <elliot.barnwell@anyscale.com>
2025-09-28 22:15:08 +0000

    pip install in ci/tests.sh
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- doc/source/train/examples/pytorch/distributing-pytorch/ci/tests.sh
+++ doc/source/train/examples/pytorch/distributing-pytorch/ci/tests.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -exuo pipefail
+
+uv pip install -r requirements.in -c python_depset.lock --system --no-deps --index-strategy unsafe-best-match
+
 python ci/nb2py.py README.ipynb README.py  # convert notebook to py script
 python README.py  # be sure to use ipython to ensure even non-python cells are executed properly
 rm README.py  # remove the generated script
