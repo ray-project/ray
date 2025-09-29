12df10eed79af37f64c012518814e925a493182b
elliot-barn <elliot.barnwell@anyscale.com>
2025-09-29 20:15:14 +0000

    updating path to python_depset.lock
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- doc/source/train/examples/pytorch/distributing-pytorch/ci/tests.sh
+++ doc/source/train/examples/pytorch/distributing-pytorch/ci/tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -exuo pipefail
 
-uv pip install -r requirements.in -c python_depset.lock --system --no-deps --index-strategy unsafe-best-match
+ls -a
+
+uv pip install -r requirements.in -c $HOME/python_depset.lock --system --no-deps --index-strategy unsafe-best-match
 
 python ci/nb2py.py README.ipynb README.py  # convert notebook to py script
 python README.py  # be sure to use ipython to ensure even non-python cells are executed properly
