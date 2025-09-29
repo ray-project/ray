ray-project/ray

commit 2330b6204606b86fd9f108105976c86247d72615

[core] Cleanup metrics stuff + fix gcc >10 build (#56514)
## Why are these changes needed?
Fixing the build for my gcc devbox, was struggling to disambiguate
between map with string_view vs. map with string. Changed Record to
always take string_view. The string overload is only used in Cython so
just have a separate RecordForCython that's not part of the
MetricInterface. We can't use string_view in Cython because our Cython
version isn't new enough.

Also killing tag_defs.cc, it just had one line tag registrations that
could go in the headers. There were some tags that were unused so killed
them.
There were also some string constants that could just be put in the one
spot they were used so the files that use it don't need the dependency.

The biggest change is changing the record to take a vector of pairs
instead of an unordered map. We didn't use the map aspect of it, so a
vector is more efficient.

Record was also taking by const ref before and then trying to move, so
the move wasn't actually happening. Taking by value now so that it
actually moves.

---------

Signed-off-by: dayshah <dhyey2019@gmail.com>

src/ray/raylet/local_object_manager.cc
@@ -22,6 +22,7 @@
 
 #include "ray/common/asio/instrumented_io_context.h"
 #include "ray/stats/metric_defs.h"
+#include "ray/stats/tag_defs.h"
 
 namespace ray {
 
@@ -646,9 +647,8 @@ void LocalObjectManager::RecordMetrics() const {
   ray::stats::STATS_spill_manager_request_total.Record(restored_objects_total_,
                                                        "Restored");
 
-  ray::stats::STATS_object_store_memory.Record(
-      spilled_bytes_current_,
-      {{ray::stats::LocationKey.name(), ray::stats::kObjectLocSpilled}});
+  stats::STATS_object_store_memory.Record(
+      spilled_bytes_current_, {{std::string_view(stats::LocationKey.name()), "SPILLED"}});
 
   ray::stats::STATS_spill_manager_request_total.Record(num_failed_deletion_requests_,
                                                        "FailedDeletion");

src/ray/stats/BUILD.bazel
@@ -5,7 +5,6 @@ ray_cc_library(
     srcs = [
         "metric.cc",
         "metric_defs.cc",
-        "tag_defs.cc",
     ],
     hdrs = [
         "metric.h",
@@ -59,7 +58,6 @@ ray_cc_library(
 
 ray_cc_library(
     name = "tag_defs",
-    srcs = ["tag_defs.cc"],
     hdrs = ["tag_defs.h"],
     deps = [
         "//src/ray/observability:metric_interface",

src/ray/stats/metric_defs.cc
@@ -14,6 +14,7 @@
 
 #include "ray/stats/metric_defs.h"
 
+#include "ray/stats/tag_defs.h"
 #include "ray/util/size_literals.h"
 
 using namespace ray::literals;

src/ray/stats/tag_defs.cc
@@ -1,51 +1,0 @@
-// Copyright 2017 The Ray Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//  http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-#include "ray/stats/tag_defs.h"
-
-namespace ray {
-namespace stats {
-const TagKeyType ComponentKey = TagKeyType::Register("Component");
-
-const TagKeyType JobNameKey = TagKeyType::Register("JobName");
-
-const TagKeyType NodeAddressKey = TagKeyType::Register("NodeAddress");
-
-const TagKeyType VersionKey = TagKeyType::Register("Version");
-
-const TagKeyType LanguageKey = TagKeyType::Register("Language");
-
-const TagKeyType WorkerPidKey = TagKeyType::Register("WorkerPid");
-
-const TagKeyType DriverPidKey = TagKeyType::Register("DriverPid");
-
-const TagKeyType ActorIdKey = TagKeyType::Register("ActorId");
-
-// Keep in sync with the WORKER_ID_TAG_KEY in
-// python/ray/_private/telemetry/metric_cardinality.py
-const TagKeyType WorkerIdKey = TagKeyType::Register("WorkerId");
-
-const TagKeyType JobIdKey = TagKeyType::Register("JobId");
-
-const TagKeyType SessionNameKey = TagKeyType::Register("SessionName");
-
-const TagKeyType NameKey = TagKeyType::Register("Name");
-
-const TagKeyType LocationKey = TagKeyType::Register("Location");
-
-const TagKeyType ObjectStateKey = TagKeyType::Register("ObjectState");
-
-const TagKeyType SourceKey = TagKeyType::Register("Source");
-}  // namespace stats
-}  // namespace ray

src/ray/stats/tag_defs.h
@@ -22,52 +22,29 @@
 namespace ray {
 namespace stats {
 
-extern const TagKeyType ComponentKey;
+inline const TagKeyType ComponentKey = TagKeyType::Register("Component");
 
-extern const TagKeyType JobNameKey;
+inline const TagKeyType NodeAddressKey = TagKeyType::Register("NodeAddress");
 
-extern const TagKeyType NodeAddressKey;
+inline const TagKeyType VersionKey = TagKeyType::Register("Version");
 
-extern const TagKeyType VersionKey;
+inline const TagKeyType LanguageKey = TagKeyType::Register("Language");
 
-extern const TagKeyType LanguageKey;
+// Keep in sync with the WORKER_ID_TAG_KEY in
+// python/ray/_private/telemetry/metric_cardinality.py
+inline const TagKeyType WorkerIdKey = TagKeyType::Register("WorkerId");
 
-extern const TagKeyType WorkerPidKey;
+inline const TagKeyType SessionNameKey = TagKeyType::Register("SessionName");
 
-extern const TagKeyType DriverPidKey;
-
-extern const TagKeyType ActorIdKey;
-
-extern const TagKeyType WorkerIdKey;
-
-extern const TagKeyType JobIdKey;
-
-extern const TagKeyType SessionNameKey;
-
-extern const TagKeyType NameKey;
-
-extern const TagKeyType SourceKey;
+inline const TagKeyType NameKey = TagKeyType::Register("Name");
 
 // Object store memory location tag constants
-extern const TagKeyType LocationKey;
-
-constexpr char kResourceNameKey[] = "ResourceName";
-
-constexpr char kCustomKey[] = "CustomKey";
-
-constexpr char kObjectLocMmapShm[] = "MMAP_SHM";
-constexpr char kObjectLocMmapDisk[] = "MMAP_DISK";
-constexpr char kObjectLocSpilled[] = "SPILLED";
-constexpr char kObjectLocWorkerHeap[] = "WORKER_HEAP";
+inline const TagKeyType LocationKey = TagKeyType::Register("Location");
 
 // Object store memory sealed/unsealed tag
-extern const TagKeyType ObjectStateKey;
-constexpr char kObjectSealed[] = "SEALED";
-constexpr char kObjectUnsealed[] = "UNSEALED";
+inline const TagKeyType ObjectStateKey = TagKeyType::Register("ObjectState");
 
-// GCS task manager tags
-constexpr char kGcsTaskStatusEventDropped[] = "STATUS_EVENT";
-constexpr char kGcsProfileEventDropped[] = "PROFILE_EVENT";
+inline const TagKeyType SourceKey = TagKeyType::Register("Source");
 
 }  // namespace stats
 }  // namespace ray

cpp/include/ray/api/metric.h
@@ -90,7 +90,7 @@ class Histogram : public Metric {
   ///
   /// \param[in] value The value that we record.
   /// \param[in] tags The map tag values that we want to record
-  void Observe(double value, const std::unordered_map<std::string, std::string> &Tags);
+  void Observe(double value, const std::unordered_map<std::string, std::string> &tags);
 };  // class Histogram
 
 class Counter : public Metric {

cpp/src/ray/runtime/metric/metric.cc
@@ -37,7 +37,12 @@ void Metric::Record(double value,
                     const std::unordered_map<std::string, std::string> &tags) {
   RAY_CHECK(metric_ != nullptr) << "The metric_ must not be nullptr.";
   stats::Metric *metric = reinterpret_cast<stats::Metric *>(metric_);
-  metric->Record(value, tags);
+  std::vector<std::pair<std::string_view, std::string>> tags_pair_vec;
+  tags_pair_vec.reserve(tags.size());
+  for (const auto &tag : tags) {
+    tags_pair_vec.emplace_back(std::string_view(tag.first), tag.second);
+  }
+  metric->Record(value, std::move(tags_pair_vec));
 }
 
 Gauge::Gauge(const std::string &name,

python/ray/includes/metric.pxd
@@ -1,6 +1,6 @@
 from libcpp.string cimport string as c_string
-from libcpp.unordered_map cimport unordered_map
 from libcpp.vector cimport vector as c_vector
+from libcpp.pair cimport pair as c_pair
 
 cdef extern from "opencensus/tags/tag_key.h" nogil:
     cdef cppclass CTagKey "opencensus::tags::TagKey":
@@ -16,8 +16,8 @@ cdef extern from "ray/stats/metric.h" nogil:
                 const c_vector[c_string] &tag_keys)
         c_string GetName() const
         void Record(double value)
-        void Record(double value,
-                    unordered_map[c_string, c_string] &tags)
+        void RecordForCython(double value,
+                    c_vector[c_pair[c_string, c_string]] tags)
 
     cdef cppclass CGauge "ray::stats::Gauge":
         CGauge(const c_string &name,

commit 0cc03f87af10dddc05cba98c6906e167543925fd

[core] Fix cpp api mac breakage from #56514 (#56915)
https://buildkite.com/ray-project/postmerge-macos/builds/8257#01997de0-1e13-4954-a370-6255f650ca17
mac c++ / java tests broke after #56514.

Fixing by reverting to have tag_defs.cc define the vars in a separate
file and not just in the header.

Why does this fix it - I have no idea... for some reason the c++ api
dynamic linking madness doesn't like the inlined vars on the mac build
specifically???

---------

Signed-off-by: dayshah <dhyey2019@gmail.com>

src/ray/raylet/local_object_manager.cc
@@ -647,8 +647,8 @@ void LocalObjectManager::RecordMetrics() const {
   ray::stats::STATS_spill_manager_request_total.Record(restored_objects_total_,
                                                        "Restored");
 
-  stats::STATS_object_store_memory.Record(
-      spilled_bytes_current_, {{std::string_view(stats::LocationKey.name()), "SPILLED"}});
+  stats::STATS_object_store_memory.Record(spilled_bytes_current_,
+                                          {{stats::LocationKey, "SPILLED"}});
 
   ray::stats::STATS_spill_manager_request_total.Record(num_failed_deletion_requests_,
                                                        "FailedDeletion");

src/ray/stats/BUILD.bazel
@@ -58,6 +58,7 @@ ray_cc_library(
 
 ray_cc_library(
     name = "tag_defs",
+    srcs = ["tag_defs.cc"],
     hdrs = ["tag_defs.h"],
     deps = [
         "//src/ray/observability:metric_interface",

src/ray/stats/metric_defs.cc
@@ -105,7 +105,7 @@ DEFINE_stats(
     /// ObjectState:
     ///    - SEALED: sealed objects bytes (could be MMAP_SHM or MMAP_DISK)
     ///    - UNSEALED: unsealed objects bytes (could be MMAP_SHM or MMAP_DISK)
-    (ray::stats::LocationKey.name(), ray::stats::ObjectStateKey.name()),
+    ("Location", "ObjectState"),
     (),
     ray::stats::GAUGE);
 

src/ray/stats/tag_defs.cc
@@ -1,0 +1,37 @@
+// Copyright 2017 The Ray Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "ray/stats/tag_defs.h"
+
+namespace ray {
+namespace stats {
+const TagKeyType ComponentKey = TagKeyType::Register("Component");
+
+const TagKeyType NodeAddressKey = TagKeyType::Register("NodeAddress");
+
+const TagKeyType VersionKey = TagKeyType::Register("Version");
+
+const TagKeyType LanguageKey = TagKeyType::Register("Language");
+
+// Keep in sync with the WORKER_ID_TAG_KEY in
+// python/ray/_private/telemetry/metric_cardinality.py
+const TagKeyType WorkerIdKey = TagKeyType::Register("WorkerId");
+
+const TagKeyType SessionNameKey = TagKeyType::Register("SessionName");
+
+const TagKeyType NameKey = TagKeyType::Register("Name");
+
+const TagKeyType SourceKey = TagKeyType::Register("Source");
+}  // namespace stats
+}  // namespace ray

src/ray/stats/tag_defs.h
@@ -22,29 +22,27 @@
 namespace ray {
 namespace stats {
 
-inline const TagKeyType ComponentKey = TagKeyType::Register("Component");
+extern const TagKeyType ComponentKey;
 
-inline const TagKeyType NodeAddressKey = TagKeyType::Register("NodeAddress");
+extern const TagKeyType NodeAddressKey;
 
-inline const TagKeyType VersionKey = TagKeyType::Register("Version");
+extern const TagKeyType VersionKey;
 
-inline const TagKeyType LanguageKey = TagKeyType::Register("Language");
+extern const TagKeyType LanguageKey;
 
-// Keep in sync with the WORKER_ID_TAG_KEY in
-// python/ray/_private/telemetry/metric_cardinality.py
-inline const TagKeyType WorkerIdKey = TagKeyType::Register("WorkerId");
+extern const TagKeyType WorkerIdKey;
 
-inline const TagKeyType SessionNameKey = TagKeyType::Register("SessionName");
+extern const TagKeyType SessionNameKey;
 
-inline const TagKeyType NameKey = TagKeyType::Register("Name");
+extern const TagKeyType NameKey;
+
+extern const TagKeyType SourceKey;
 
 // Object store memory location tag constants
-inline const TagKeyType LocationKey = TagKeyType::Register("Location");
+constexpr std::string_view LocationKey = "Location";
 
 // Object store memory sealed/unsealed tag
-inline const TagKeyType ObjectStateKey = TagKeyType::Register("ObjectState");
-
-inline const TagKeyType SourceKey = TagKeyType::Register("Source");
+constexpr std::string_view ObjectStateKey = "ObjectState";
 
 }  // namespace stats
 }  // namespace ray