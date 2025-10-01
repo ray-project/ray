7bb810c1bd78f87c4a81e49cf8363557fa9b052e
joshlee <joshlee@anyscale.com>
2025-10-01 00:37:17 +0000

    Fixing bug
    Signed-off-by: joshlee <joshlee@anyscale.com>

--- src/ray/gcs/gcs_server.cc
+++ src/ray/gcs/gcs_server.cc
@@ -77,15 +77,22 @@ GcsServer::GcsServer(
                            RayConfig::instance().gcs_server_rpc_client_thread_num()),
       // NOTE: The raylet client server_unavailable_timeout_seconds is set to -1 because
       // the gcs is notified when any node has died from the gcs node manager.
-      // Hence we don't ever need to invoke the unavailable timeout callback.
+      // We still need to invoke the unavailable timeout callback immediately in case we
+      // make a request to the raylet after receiving the node death notification.
       raylet_client_pool_([this](const rpc::Address &addr) {
         return std::make_shared<ray::rpc::RayletClient>(
             addr,
             this->client_call_manager_,
             /*raylet_unavailable_timeout_callback=*/
-            []() {},
+            [this, addr]() {
+              const NodeID node_id = NodeID::FromBinary(addr.node_id());
+              auto alive_node = this->gcs_node_manager_->GetAliveNode(node_id);
+              if (!alive_node.has_value()) {
+                this->raylet_client_pool_.Disconnect(node_id);
+              }
+            },
             /*server_unavailable_timeout_seconds=*/-1,
-            /*server_call_unavailable_timeout_immediately=*/false);
+            /*server_call_unavailable_timeout_immediately=*/true);
       }),
       worker_client_pool_([this](const rpc::Address &addr) {
         return std::make_shared<rpc::CoreWorkerClient>(
