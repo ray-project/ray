b729b7275e8c9539ae20648fb8ede4d541415986
Joshua Lee <73967497+Sparks0219@users.noreply.github.com>
2025-10-01 06:25:11 -0700

    [core] Modify test expectations for test_actor_failures.py::test_exit_actor_queued  (#57083)
    Seems like test_actor_failures.py::test_exit_actor_queued need's to have
    it's test expectation adjusted after #56159 since pending actor tasks
    are explicilty cancelled with a TaskCancelledError now. RayActorError
    can still pop up for the ongoing task that was cancelled so need to
    account for both.
    
    Signed-off-by: joshlee <joshlee@anyscale.com>
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- release/nightly_tests/stress_tests/test_parallel_tasks_memory_pressure.py
+++ release/nightly_tests/stress_tests/test_parallel_tasks_memory_pressure.py
@@ -1,75 +1,66 @@
-from math import ceil
-import time
+import argparse
 import random
+import time
+from math import ceil
+from typing import Dict
+
+import numpy as np
 
 import ray
 from ray._private.utils import get_system_memory, get_used_memory
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--num-tasks",
+    help="number of tasks to process in total",
+    default="20",
+    type=int,
+)
 
-@ray.remote
+parser.add_argument(
+    "--mem-pct-per-task",
+    help="memory to allocate per task as a fraction of the node's available memory",
+    default="0.45",
+    type=float,
+)
+
+@ray.remote(max_retries=-1)
 def allocate_memory(
-    total_allocate_bytes: int,
+    target_bytes: int,
+    *,
     num_chunks: int = 10,
-    allocate_interval_s: float = 0,
-) -> int:
+    allocate_interval_s: float = 1,
+):
     chunks = []
-    # divide by 8 as each element in the array occupies 8 bytes
-    bytes_per_chunk = total_allocate_bytes / 8 / num_chunks
     for _ in range(num_chunks):
-        chunks.append([0] * ceil(bytes_per_chunk))
+        chunk = np.empty(ceil(target_bytes / num_chunks), dtype=np.uint8)
+        chunk.fill(1)
+        chunks.append(chunk)
 
         # If all tasks try to allocate memory at the same time,
         # the memory monitor might not be able to kill them in time.
-        # To avoid this, we introduce a random sleep interval.
-        r = 1 + 5 * random.random()
-        time.sleep(allocate_interval_s * r)
-    return 1
+        # To avoid this, we introduce jitter in the sleep interval.
+        time.sleep(allocate_interval_s * random.random())
 
 
-def get_additional_bytes_to_reach_memory_usage_pct(pct: float) -> int:
-    total = get_system_memory()
-    used = get_used_memory()
-    bytes_needed = total * pct - used
-    assert bytes_needed > 0, "node has less memory than what is requested"
-    return int(bytes_needed)
+def main(*, num_tasks: int, mem_pct_per_task: float):
+    # First run some warmup tasks before estimating the steady state memory consumption.
+    print(f"Running {num_tasks} warm up tasks, each allocating 100 MiB.")
+    # ray.get([allocate_memory.remote(100 * 1024**2, num_chunks=1) for _ in range(num_tasks)])
+    print("Warm up tasks finished.")
 
+    total_bytes, used_bytes = get_system_memory(), get_used_memory()
+    bytes_per_task = int((total_bytes - used_bytes) * mem_pct_per_task)
+    gib_per_task = bytes_per_task / 1024**3
 
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--num-tasks",
-        help="number of tasks to process in total",
-        default="20",
-        type=int,
-    )
-
-    parser.add_argument(
-        "--mem-pct-per-task",
-        help="memory to allocate per task as a fraction of the node's available memory",
-        default="0.45",
-        type=float,
-    )
-
-    args = parser.parse_args()
-
-    cpu_per_task = 1
-
-    bytes_per_task = get_additional_bytes_to_reach_memory_usage_pct(
-        args.mem_pct_per_task
-    )
-
-    start = time.time()
-    task_refs = [
-        allocate_memory.options(num_cpus=cpu_per_task).remote(
-            total_allocate_bytes=bytes_per_task, allocate_interval_s=1
-        )
-        for _ in range(args.num_tasks)
-    ]
     # When a task or actor is killed by the memory monitor
     # it will be retried with exponential backoff.
-    results = [ray.get(ref) for ref in task_refs]
+    start = time.time()
+    print(f"Running {num_tasks} tasks, each allocating {gib_per_task:.2f} GiB.")
+    ray.get([allocate_memory.remote(bytes_per_task) for _ in range(num_tasks)])
     end = time.time()
 
-    print(f"processed {args.num_tasks} tasks in {end-start} seconds")
+    print(f"Tasks completed in {end-start:.2f} seconds.")
+
+if __name__ == "__main__":
+    main(**vars(parser.parse_args()))
