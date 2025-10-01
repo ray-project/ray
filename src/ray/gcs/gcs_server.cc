0dd0cfeeb91788e16300176975bbcff5b7e5c464
Dhyey Shah <dhyey2019@gmail.com>
2025-09-30 23:59:56 -0700

    [core] Add config to not inject rpc failures if rpc's are local (#57034)
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- src/ray/gcs/gcs_server.cc
+++ src/ray/gcs/gcs_server.cc
@@ -73,6 +73,7 @@ GcsServer::GcsServer(
                   /*keepalive_time_ms=*/RayConfig::instance().grpc_keepalive_time_ms()),
       client_call_manager_(main_service,
                            /*record_stats=*/true,
+                           config.node_ip_address,
                            ClusterID::Nil(),
                            RayConfig::instance().gcs_server_rpc_client_thread_num()),
       raylet_client_pool_([this](const rpc::Address &addr) {
@@ -121,6 +122,7 @@ GcsServer::GcsServer(
       event_aggregator_client_call_manager_(
           io_context_provider_.GetIOContext<observability::RayEventRecorder>(),
           /*record_stats=*/true,
+          config.node_ip_address,
           ClusterID::Nil(),
           RayConfig::instance().gcs_server_rpc_client_thread_num()),
       event_aggregator_client_(std::make_unique<rpc::EventAggregatorClientImpl>(
