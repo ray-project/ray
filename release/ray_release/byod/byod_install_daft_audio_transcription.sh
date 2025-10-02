0f59a0a921295af246a165620db388d6368e3c94
elliot-barn <elliot.barnwell@anyscale.com>
2025-10-02 06:16:31 +0000

    updating lock file name
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- release/ray_release/byod/byod_install_daft_audio_transcription.sh
+++ release/ray_release/byod/byod_install_daft_audio_transcription.sh
@@ -4,4 +4,4 @@
 set -exo pipefail
 
 # TODO: Pin these versions.
-uv pip install -r audio_transcription_py3.10.lock --system --no-deps --index-strategy unsafe-best-match
+uv pip install -r python_depset.lock --system --no-deps --index-strategy unsafe-best-match
