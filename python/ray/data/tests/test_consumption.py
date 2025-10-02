e98bbc7623274aed3ac9cf3e00dc1ed18ca54ec1
Alexey Kudinkin <ak@anyscale.com>
2025-10-01 18:53:23 -0700

    Fixed version check test
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/data/tests/test_consumption.py
+++ python/ray/data/tests/test_consumption.py
@@ -1584,7 +1584,7 @@ def test_unsupported_pyarrow_versions_check(shutdown_only):
 
     with pytest.raises(
         Exception,
-        match=r".*Dataset requires pyarrow >= 9.0.0, but 5.0.0 is installed.*",
+        match=r".*Dataset requires pyarrow >= 9.0.0, but 8.0.0 is installed.*",
     ):
         ray.get(should_error.remote())
 
