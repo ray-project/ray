dc95315dabc37b3da06865f840d6ae97b2ce5ca0
Goutam V. <goutam@anyscale.com>
2025-10-01 19:08:24 -0700

    Add comment explaining
    Signed-off-by: Goutam V. <goutam@anyscale.com>

--- python/ray/data/_internal/execution/operators/actor_pool_map_operator.py
+++ python/ray/data/_internal/execution/operators/actor_pool_map_operator.py
@@ -531,6 +531,8 @@ class _MapWorker:
     ):
         self.src_fn_name: str = src_fn_name
         self._map_transformer = map_transformer
+        # Initialize the data context for this actor after setting the src_fn_name in order to not
+        # break __repr__. It's possible that logging setup fails.
         DataContext._set_current(ctx)
         # Initialize state for this actor.
         self._map_transformer.init()
