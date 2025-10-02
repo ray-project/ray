2c7ef60cb1db8641882f96a77014687d6bd31e1c
Lehui Liu <lehui@anyscale.com>
2025-10-02 10:20:34 -0700

    formatting
    Signed-off-by: Lehui Liu <lehui@anyscale.com>

--- python/ray/tune/integration/keras.py
+++ python/ray/tune/integration/keras.py
@@ -29,61 +29,42 @@ class _TuneCheckpointCallback:
 
 @PublicAPI(stability="beta")
 class TuneReportCheckpointCallback(RayReportCallback):
-    """Keras callback to save checkpoints and report metrics for Ray Tune.
+    """Keras callback for Ray Tune reporting and checkpointing.
+
+    .. note::
+        Metrics are always reported with checkpoints, even if the event isn't specified
+        in ``report_metrics_on``.
+
+    Example:
+        .. code-block:: python
+
+            ############# Using it in TrainSession ###############
+            from ray.tune.integrations.keras import TuneReportCheckpointCallback
+            def train_loop_per_worker():
+                strategy = tf.distribute.MultiWorkerMirroredStrategy()
+                with strategy.scope():
+                    model = build_model()
+
+                model.fit(dataset_shard, callbacks=[TuneReportCheckpointCallback()])
 
     Args:
-        metrics: Metrics to report. If this is a list,
-            each item describes the metric key reported to XGBoost,
-            and it will be reported under the same name.
-            This can also be a dict of {<key-to-report>: <keras-metric-key>},
-            which can be used to rename xgboost default metrics.
-        filename: Customize the saved checkpoint file type by passing
-            a filename. Defaults to "model.ubj".
-        frequency: How often to save checkpoints, in terms of iterations.
-            Defaults to 0 (no checkpoints are saved during training).
-        checkpoint_at_end: Whether or not to save a checkpoint at the end of training.
-        results_postprocessing_fn: An optional Callable that takes in
-            the metrics dict that will be reported (after it has been flattened)
-            and returns a modified dict. For example, this can be used to
-            average results across CV fold when using ``xgboost.cv``.
+        metrics: Metrics to report. If this is a list, each item describes
+            the metric key reported to Keras, and it's reported under the
+            same name. If this is a dict, each key is the name reported
+            and the respective value is the metric key reported to Keras.
+            If this is None, all Keras logs are reported.
+        report_metrics_on: When to report metrics. Must be one of
+            the Keras event hooks (less the ``on_``), e.g.
+            "train_start" or "predict_end". Defaults to "epoch_end".
+        checkpoint_on: When to save checkpoints. Must be one of the Keras event hooks
+            (less the ``on_``), e.g. "train_start" or "predict_end". Defaults to
+            "epoch_end".
 
-    Examples
-    --------
-
-    Reporting checkpoints and metrics to Ray Tune when running many
-    independent xgboost trials (without data parallelism within a trial).
-
-    .. testcode::
-        :skipif: True
-
-        import xgboost
-
-        from ray.tune import Tuner
-        from ray.tune.integration.xgboost import TuneReportCheckpointCallback
-
-        def train_fn(config):
-            # Report log loss to Ray Tune after each validation epoch.
-            bst = xgboost.train(
-                ...,
-                callbacks=[
-                    TuneReportCheckpointCallback(
-                        metrics={"loss": "eval-logloss"}, frequency=1
-                    )
-                ],
-            )
-
-        tuner = Tuner(train_fn)
-        results = tuner.fit()
     """
 
-
-    # @contextmanager
-    # def _get_checkpoint(self, model: Booster) -> Optional[Checkpoint]:
-    #     with tempfile.TemporaryDirectory() as temp_checkpoint_dir:
-    #         model.save_model(Path(temp_checkpoint_dir, self._filename).as_posix())
-    #         yield Checkpoint(temp_checkpoint_dir)
-
-    def _save_and_report_checkpoint(self, report_dict: Dict, model: TensorflowCheckpoint):
+    def _save_and_report_checkpoint(
+        self, report_dict: Dict, model: TensorflowCheckpoint
+    ):
         ray.tune.report(report_dict, checkpoint=model)
 
     def _report_metrics(self, report_dict: Dict):
