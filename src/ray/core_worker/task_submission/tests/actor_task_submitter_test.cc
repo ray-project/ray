fac05eaff46aa0b5a03cab979fab391dd2d3a2fd
Kunchen Dai <54918178+Kunchd@users.noreply.github.com>
2025-10-02 09:05:54 -0700

    [Core] Rename reference_count files to reference_counter files (#57102)
    Signed-off-by: davik <davik@anyscale.com>
    Co-authored-by: davik <davik@anyscale.com>

--- src/ray/core_worker/task_submission/tests/actor_task_submitter_test.cc
+++ src/ray/core_worker/task_submission/tests/actor_task_submitter_test.cc
@@ -19,7 +19,7 @@
 #include <vector>
 
 #include "gtest/gtest.h"
-#include "mock/ray/core_worker/reference_count.h"
+#include "mock/ray/core_worker/reference_counter.h"
 #include "mock/ray/core_worker/task_manager_interface.h"
 #include "ray/common/test_utils.h"
 #include "ray/core_worker/fake_actor_creator.h"
