7a723b1c673c96f931af94e51de848afb4f4ed33
elliot-barn <elliot.barnwell@anyscale.com>
2025-10-03 00:34:50 +0000

    adding home reference for python depset
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- release/hello_world_tests/test.sh
+++ release/hello_world_tests/test.sh
@@ -3,7 +3,7 @@
 set -exo pipefail
 
 # Install Python dependencies
-uv pip install -r python_depset.lock --system --no-deps --index-strategy unsafe-best-match
+uv pip install -r "$HOME"/python_depset.lock --system --no-deps --index-strategy unsafe-best-match
 
 # Run the test
 python hello_world.py
