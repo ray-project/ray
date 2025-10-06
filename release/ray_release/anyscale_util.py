7080901f38705e609d6af8e1232dc777844519ce
kevin <kevin@anyscale.com>
2025-10-06 08:34:06 +0000

    p
    Signed-off-by: kevin <kevin@anyscale.com>

--- release/ray_release/anyscale_util.py
+++ release/ray_release/anyscale_util.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Optional, Dict, Any
 
 from ray_release.logger import logger
 from ray_release.util import get_anyscale_sdk
@@ -6,6 +6,13 @@ from ray_release.util import get_anyscale_sdk
 if TYPE_CHECKING:
     from anyscale.sdk.anyscale_client.sdk import AnyscaleSDK
 
+from anyscale.compute_config.models import (
+    ComputeConfig,
+    HeadNodeConfig,
+    WorkerNodeGroupConfig,
+    MarketType,
+)
+from ray_release.test import Test
 
 LAST_LOGS_LENGTH = 100
 
@@ -50,3 +57,71 @@ def get_cluster_name(cluster_id: str, sdk: Optional["AnyscaleSDK"] = None) -> st
 
     result = sdk.get_cluster(cluster_id)
     return result.result.name
+
+
+def convert_cluster_compute_to_anyscale_compute_config(
+    cluster_compute: Dict[str, Any]
+) -> ComputeConfig:
+    head_node = cluster_compute["head_node_type"]
+    worker_node_types = cluster_compute["worker_node_types"]
+    cloud_id = cluster_compute["cloud_id"]
+
+    head_node_config = HeadNodeConfig(
+        instance_type=head_node["instance_type"],
+    )
+    worker_node_configs = []
+    for worker_node_type in worker_node_types:
+        worker_node_config = WorkerNodeGroupConfig(
+            instance_type=worker_node_type["instance_type"],
+            min_workers=worker_node_type["min_workers"],
+            max_workers=worker_node_type["max_workers"],
+        )
+        if worker_node_type.get("use_spot", False):
+            worker_node_config.market_type = MarketType.SPOT
+        worker_node_configs.append(worker_node_config)
+
+    compute_config = ComputeConfig(
+        cloud_id=cloud_id,
+        head_node=head_node_config,
+        worker_node_groups=worker_node_configs,
+    )
+    return compute_config
+
+
+def get_entrypoint_command(
+    test: Test,
+    timeout: int,
+    smoke_test: bool,
+):
+    command = test["run"]["script"]
+    command_env = test.get_byod_runtime_env()
+
+    raise_on_timeout = not test["run"].get("long_running", False)
+    no_raise_on_timeout_str = "--test-no-raise-on-timeout" if raise_on_timeout else ""
+    if smoke_test:
+        command = f"{command} --smoke-test"
+        command_env["IS_SMOKE_TEST"] = "1"
+
+    full_command = (
+        f"python anyscale_job_wrapper.py '{command}'"
+        f"--test-workload-timeout {timeout}{no_raise_on_timeout_str} "
+    )
+
+    return full_command
+
+
+"""
+cloud_id: {{env["ANYSCALE_CLOUD_ID"]}}
+region: us-west-2
+
+head_node_type:
+    name: head_node
+    instance_type: m5.xlarge  # 4 CPUs
+
+worker_node_types:
+    - name: worker_node
+      instance_type: m5.xlarge
+      min_workers: 0
+      max_workers: 2
+      use_spot: false
+"""
