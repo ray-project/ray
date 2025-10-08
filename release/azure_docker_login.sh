6cfc4d28eb1528def40b027abce3cc23f9adad00
kevin <kevin@anyscale.com>
2025-10-08 00:55:31 +0000

    p
    Signed-off-by: kevin <kevin@anyscale.com>

--- release/azure_docker_login.sh
+++ release/azure_docker_login.sh
@@ -4,11 +4,11 @@
 set -euo pipefail
 
 # Retrieve credentials from Secrets Manager
-SECRET=$(aws secretsmanager get-secret-value \
+SECRET="$(aws secretsmanager get-secret-value \
   --secret-id azure-service-principal-oss-release \
   --query SecretString \
   --region us-west-2 \
-  --output text)
+  --output text)"
 
 CLIENT_ID="$(echo "$SECRET" | jq -r '.client_id')"
 TENANT_ID="$(echo "$SECRET" | jq -r '.tenant_id')"
