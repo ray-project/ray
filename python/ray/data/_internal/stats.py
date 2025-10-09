2efdecd8ede5c681a58d602f76bd6bf468a6a627
Alexey Kudinkin <ak@anyscale.com>
2025-10-09 14:33:48 -0700

    `lint`
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/data/_internal/stats.py
+++ python/ray/data/_internal/stats.py
@@ -282,7 +282,7 @@ class _StatsActor:
         self.time_to_first_batch_s = Gauge(
             "data_iter_time_to_first_batch_seconds",
             description="Total time spent waiting for the first batch after starting iteration. "
-                        "This includes the dataset pipeline warmup time. This metric is accumulated across different epochs.",
+            "This includes the dataset pipeline warmup time. This metric is accumulated across different epochs.",
             tag_keys=iter_tag_keys,
         )
 
@@ -518,7 +518,6 @@ class _StatsActor:
 
         self.iter_total_blocked_s.set(stats.iter_total_blocked_s.get(), tags)
         self.iter_user_s.set(stats.iter_user_s.get(), tags)
-
 
     def register_dataset(
         self,
