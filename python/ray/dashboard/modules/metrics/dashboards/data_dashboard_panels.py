730004c6fa9eb5a63a538c9a2e62abdb1798b8b7
Alan Guo <aguo@anyscale.com>
2025-10-09 11:49:34 -0700

    fix duplicate panel id
    Signed-off-by: Alan Guo <aguo@anyscale.com>

--- python/ray/dashboard/modules/metrics/dashboards/data_dashboard_panels.py
+++ python/ray/dashboard/modules/metrics/dashboards/data_dashboard_panels.py
@@ -495,7 +495,7 @@ TASK_COMPLETION_TIME_PANEL = Panel(
 )
 
 BLOCK_COMPLETION_TIME_PANEL = Panel(
-    id=58,
+    id=59,
     title="Block Completion Time Histogram (s)",
     description="Time (in seconds) spent processing blocks to completion. If multiple blocks are generated per task, this is approximated by assuming each block took an equal amount of time to process. Larger bars means more blocks finished within that duration range.",
     targets=[
@@ -512,7 +512,7 @@ BLOCK_COMPLETION_TIME_PANEL = Panel(
 )
 
 BLOCK_SIZE_BYTES_PANEL = Panel(
-    id=59,
+    id=60,
     title="Block Size (Bytes) Histogram",
     description="Size (in bytes) per block. Larger bars means more blocks are within that size range.",
     targets=[
@@ -529,7 +529,7 @@ BLOCK_SIZE_BYTES_PANEL = Panel(
 )
 
 BLOCK_SIZE_ROWS_PANEL = Panel(
-    id=60,
+    id=61,
     title="Block Size (Rows) Histogram",
     description="Number of rows per block. Larger bars means more blocks are within that number of rows range.",
     targets=[
