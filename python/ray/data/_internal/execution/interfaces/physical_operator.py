c6930de5871d8601914303fc34f62d3f839afc49
Balaji Veeramani <bveeramani@berkeley.edu>
2025-10-09 09:51:57 -0700

    Refactor
    Signed-off-by: Balaji Veeramani <bveeramani@berkeley.edu>

--- python/ray/data/_internal/execution/interfaces/physical_operator.py
+++ python/ray/data/_internal/execution/interfaces/physical_operator.py
@@ -30,6 +30,7 @@ from ray.data._internal.logical.interfaces import LogicalOperator, Operator
 from ray.data._internal.output_buffer import OutputBlockSizeOption
 from ray.data._internal.progress_bar import ProgressBar
 from ray.data._internal.stats import StatsDict, Timer
+from ray.data.block import Block, BlockMetadata
 from ray.data.context import DataContext
 
 if TYPE_CHECKING:
@@ -38,11 +39,6 @@ if TYPE_CHECKING:
 
 logger = logging.getLogger(__name__)
 
-# Timeout for waiting for metadata reference to become available (in seconds)
-METADATA_REF_WAIT_TIMEOUT_SECONDS = 0.1
-
-# Timeout for getting metadata from Ray object references (in seconds)
-METADATA_GET_TIMEOUT_SECONDS = 1
 
 # TODO(hchen): Ray Core should have a common interface for these two types.
 Waitable = Union[ray.ObjectRef, ObjectRefGenerator]
@@ -119,7 +115,13 @@ class DataOpTask(OpTask):
         self._streaming_gen = streaming_gen
         self._output_ready_callback = output_ready_callback
         self._task_done_callback = task_done_callback
-        self._pending_block_pair = None
+
+        # If the generator hasn't produced block metadata yet, or if the block metadata
+        # object isn't available after we get a reference, we need store the pending
+        # references and wait until Ray (re)constructs the block metadata. Either case
+        # can happen if a node dies after producing a block.
+        self._pending_block_ref: ray.ObjectRef[Block] = ray.ObjectRef.nil()
+        self._pending_meta_ref: ray.ObjectRef[BlockMetadata] = ray.ObjectRef.nil()
 
     def get_waitable(self) -> ObjectRefGenerator:
         return self._streaming_gen
@@ -134,64 +136,68 @@ class DataOpTask(OpTask):
         """
         bytes_read = 0
         while max_bytes_to_read is None or bytes_read < max_bytes_to_read:
-            block_ref, meta_ref = self._pending_block_pair or (
-                None,
-                None,
-            )
-            try:
-                if block_ref is None:
-                    block_ref = self._streaming_gen._next_sync(0)
-                if block_ref.is_nil():
-                    # The generator currently doesn't have new output.
-                    # And it's not stopped yet.
+            if self._pending_block_ref.is_nil():
+                assert self._pending_meta_ref.is_nil()
+
+                try:
+                    self._pending_block_ref = self._streaming_gen._next_sync(0)
+                except StopIteration:
+                    self._task_done_callback(None)
                     break
-            except StopIteration:
-                self._task_done_callback(None)
+
+            if self._pending_block_ref.is_nil():
+                # The generator currently doesn't have new output.
+                # And it's not stopped yet.
+                break
+
+            if self._pending_meta_ref.is_nil():
+                try:
+                    self._pending_meta_ref = self._streaming_gen._next_sync(0)
+                except StopIteration:
+                    # The generator should always yield 2 values (block and metadata)
+                    # each time. If we get a StopIteration here, it means an error
+                    # happened in the task.
+                    # And in this case, the block_ref is the exception object.
+                    # TODO(hchen): Ray Core should have a better interface for
+                    # detecting and obtaining the exception.
+                    try:
+                        ray.get(self._pending_block_ref)
+                        assert False, "Above ray.get should raise an exception."
+                    except Exception as ex:
+                        self._task_done_callback(ex)
+                        raise ex from None
+
+            if self._pending_meta_ref.is_nil():
+                # We have a reference to the block but the metadata isn't ready yet.
                 break
 
             try:
-                if meta_ref is None:
-                    meta_ref = self._streaming_gen._next_sync(
-                        METADATA_REF_WAIT_TIMEOUT_SECONDS
-                    )
-                if meta_ref.is_nil():
-                    self._pending_block_pair = (block_ref, None)
-                    break
                 meta_with_schema: "BlockMetadataWithSchema" = ray.get(
-                    meta_ref, timeout=METADATA_GET_TIMEOUT_SECONDS
+                    self._pending_meta_ref, timeout=0
                 )
             except ray.exceptions.GetTimeoutError:
+                # We have a reference to the block and its metadata, but the metadata
+                # object isn't available. This can happen if the node dies.
                 logger.warning(
-                    f"Metadata object not ready for block_ref={block_ref.hex()[:8]}... "
+                    f"Metadata object not ready for "
+                    f"block_ref={self._pending_block_ref.block_ref.hex()[:8]}... "
                     f"(operator={self.__class__.__name__}). "
-                    f"Metadata may still be computing or worker may have failed and object is being reconstructed. "
-                    f"Will retry in next iteration."
+                    f"Metadata may still be computing or worker may have failed and "
+                    f"object is being reconstructed. Will retry in next iteration."
                 )
-                self._pending_block_pair = (block_ref, meta_ref)
                 break
-            except StopIteration:
-                # The generator should always yield 2 values (block and metadata)
-                # each time. If we get a StopIteration here, it means an error
-                # happened in the task.
-                # And in this case, the block_ref is the exception object.
-                # TODO(hchen): Ray Core should have a better interface for
-                # detecting and obtaining the exception.
-                try:
-                    ray.get(block_ref)
-                    assert False, "Above ray.get should raise an exception."
-                except Exception as ex:
-                    self._task_done_callback(ex)
-                    raise ex from None
 
             meta = meta_with_schema.metadata
             self._output_ready_callback(
                 RefBundle(
-                    [(block_ref, meta)],
+                    [(self._pending_block_ref, meta)],
                     owns_blocks=True,
                     schema=meta_with_schema.schema,
                 ),
             )
-            self._pending_block_pair = None
+            self._pending_block_ref = None
+            self._pending_meta_ref = None
+
             bytes_read += meta.size_bytes
 
         return bytes_read
