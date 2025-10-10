17b67eef9683aa34190c05247a248203c1f51306
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-10-10 09:53:22 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/_private/worker.py
+++ python/ray/_private/worker.py
@@ -1835,11 +1835,10 @@ def init(
     if local_mode:
         driver_mode = LOCAL_MODE
         warnings.warn(
-            "DeprecationWarning: local mode is an experimental feature that is no "
-            "longer maintained and will be removed in the future."
-            " For debugging consider using Ray Distributed Debugger. ",
-            DeprecationWarning,
-            stacklevel=2,
+            "`local_mode` is an experimental feature that is no "
+            "longer maintained and will be removed in the near future. "
+            "For debugging consider using the Ray distributed debugger.",
+            FutureWarning, stacklevel=2,
         )
     else:
         driver_mode = SCRIPT_MODE
