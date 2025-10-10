2be6656d98fe7c33d0881d3a47ce9201bb8add37
Elliot Barnwell <elliot.barnwell@anyscale.com>
2025-10-10 14:18:27 -0700

    [deps] raydepsets import all config (2/3) (#57584)
    Adding workspace funcs to parse all configs
    not currently used in raydepsets
    
    ---------
    
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>
    Co-authored-by: Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- ci/raydepsets/tests/test_workspace.py
+++ ci/raydepsets/tests/test_workspace.py
@@ -18,15 +18,14 @@ def test_parse_build_arg_sets():
     with tempfile.TemporaryDirectory() as tmpdir:
         copy_data_to_tmpdir(tmpdir)
         workspace = Workspace(dir=tmpdir)
-        config = workspace.load_config(path=Path(tmpdir) / "test.depsets.yaml")
-        assert config.build_arg_sets["py311_cpu"].build_args == {
-            "CUDA_VERSION": "cpu",
-            "PYTHON_VERSION": "py311",
-        }
-        assert config.build_arg_sets["py311_cuda128"].build_args == {
-            "CUDA_VERSION": 128,
-            "PYTHON_VERSION": "py311",
-        }
+        config = workspace.load_config(config_path=Path(tmpdir) / "test.depsets.yaml")
+        assert "general_depset__py311_cpu" in [depset.name for depset in config.depsets]
+        assert "build_args_test_depset__py311_cpu" in [
+            depset.name for depset in config.depsets
+        ]
+        assert "expanded_depset__py311_cpu" in [
+            depset.name for depset in config.depsets
+        ]
 
 
 def test_substitute_build_args():
@@ -65,16 +64,77 @@ depsets:
             )
         with pytest.raises(KeyError):
             workspace = Workspace(dir=tmpdir)
-            workspace.load_config(path=Path(tmpdir) / "test.depsets.yaml")
+            workspace.load_config(config_path=Path(tmpdir) / "test.depsets.yaml")
 
 
 def test_parse_pre_hooks():
     with tempfile.TemporaryDirectory() as tmpdir:
         copy_data_to_tmpdir(tmpdir)
         workspace = Workspace(dir=tmpdir)
-        config = workspace.load_config(path=Path(tmpdir) / "test.depsets.yaml")
+        config = workspace.load_config(config_path=Path(tmpdir) / "test2.depsets.yaml")
         pre_hook_depset = get_depset_by_name(config.depsets, "pre_hook_test_depset")
         assert pre_hook_depset.pre_hooks == ["pre-hook-test.sh"]
+
+
+def test_load_first_config():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        copy_data_to_tmpdir(tmpdir)
+        workspace = Workspace(dir=tmpdir)
+        config = workspace.load_config(config_path=Path(tmpdir) / "test.depsets.yaml")
+        assert config.depsets is not None
+        assert len(config.depsets) == 8
+
+
+def test_load_second_config():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        copy_data_to_tmpdir(tmpdir)
+        workspace = Workspace(dir=tmpdir)
+        config = workspace.load_config(config_path=Path(tmpdir) / "test2.depsets.yaml")
+        assert config.depsets is not None
+        assert len(config.depsets) == 3
+
+
+# load all configs should always load all depsets
+def test_load_all_configs_first_config():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        copy_data_to_tmpdir(tmpdir)
+        workspace = Workspace(dir=tmpdir)
+        config = workspace.load_configs(config_path=Path(tmpdir) / "test.depsets.yaml")
+        assert config.depsets is not None
+        assert len(config.depsets) == 11
+
+
+# load all configs should always load all depsets
+def test_load_all_configs_second_config():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        copy_data_to_tmpdir(tmpdir)
+        workspace = Workspace(dir=tmpdir)
+        config = workspace.load_configs(config_path=Path(tmpdir) / "test2.depsets.yaml")
+        assert config.depsets is not None
+        assert len(config.depsets) == 11
+
+
+def test_merge_configs():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        copy_data_to_tmpdir(tmpdir)
+        workspace = Workspace(dir=tmpdir)
+        config = workspace.load_config(config_path=Path(tmpdir) / "test.depsets.yaml")
+        config2 = workspace.load_config(config_path=Path(tmpdir) / "test2.depsets.yaml")
+        merged_config = workspace.merge_configs([config, config2])
+        assert merged_config.depsets is not None
+        assert len(merged_config.depsets) == 11
+
+
+def test_get_configs_dir():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        copy_data_to_tmpdir(tmpdir)
+        workspace = Workspace(dir=tmpdir)
+        configs_dir = workspace.get_configs_dir(
+            configs_path=Path(tmpdir) / "test.depsets.yaml"
+        )
+        assert len(configs_dir) == 2
+        assert f"{tmpdir}/test.depsets.yaml" in configs_dir
+        assert f"{tmpdir}/test2.depsets.yaml" in configs_dir
 
 
 if __name__ == "__main__":
