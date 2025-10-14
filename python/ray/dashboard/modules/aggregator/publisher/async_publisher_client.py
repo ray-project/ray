921e52ee92e37492089f1ce19bd060db029b80b9
Mengjin Yan <mengjinyan3@gmail.com>
2025-10-14 11:37:20 -0700

    turn off json property name conversion
    Signed-off-by: Mengjin Yan <mengjinyan3@gmail.com>

--- python/ray/dashboard/modules/aggregator/publisher/async_publisher_client.py
+++ python/ray/dashboard/modules/aggregator/publisher/async_publisher_client.py
@@ -89,7 +89,11 @@ class AsyncHttpPublisherClient(PublisherClientInterface):
             self._executor,
             lambda: [
                 json.loads(
-                    message_to_json(e, always_print_fields_with_no_presence=True)
+                    message_to_json(
+                        e,
+                        always_print_fields_with_no_presence=True,
+                        preserving_proto_field_name=True,
+                    )
                 )
                 for e in filtered
             ],
