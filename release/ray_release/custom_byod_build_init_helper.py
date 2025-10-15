b731ac73077336184f9c386885a84dbe5a596136
kevin <kevin@anyscale.com>
2025-10-15 22:41:46 +0000

    p
    Signed-off-by: kevin <kevin@anyscale.com>

--- release/ray_release/custom_byod_build_init_helper.py
+++ release/ray_release/custom_byod_build_init_helper.py
@@ -93,7 +93,7 @@ def get_prerequisite_step(image: str, base_image: str) -> Optional[str]:
     image_repository, _ = image.split(":")
     image_name = image_repository.split("/")[-1]
     if base_image.startswith("rayproject/ray:"):
-        return None
+        return "[]"
     if image_name == "ray-ml":
         return config["release_image_step_ray_ml"]
     elif image_name == "ray-llm":
