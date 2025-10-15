d3162265172b9d817f8a6c428b80a74464351b01
Mengjin Yan <mengjinyan3@gmail.com>
2025-10-14 17:25:56 -0700

    turn off task dashboard baseline
    Signed-off-by: Mengjin Yan <mengjinyan3@gmail.com>

--- src/ray/common/ray_config_def.h
+++ src/ray/common/ray_config_def.h
@@ -951,7 +951,7 @@ RAY_CONFIG(bool, enable_core_worker_task_event_to_gcs, true)
 // Currently, only task events are supported.
 // TODO(myan): #54515 Remove this flag after the task events are fully migrated to the
 // event aggregator.
-RAY_CONFIG(bool, enable_core_worker_ray_event_to_aggregator, true)
+RAY_CONFIG(bool, enable_core_worker_ray_event_to_aggregator, false)
 
 // Configuration for pipe logger buffer size.
 RAY_CONFIG(uint64_t, pipe_logger_read_buf_size, 1024)
