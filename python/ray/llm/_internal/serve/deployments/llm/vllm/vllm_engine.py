28252ed7c8a6e136455372afcc22fd293a3bf1be
ahao-anyscale <ahao@anyscale.com>
2025-10-15 16:43:46 -0700

    fix comment
    Signed-off-by: ahao-anyscale <ahao@anyscale.com>

--- python/ray/llm/_internal/serve/deployments/llm/vllm/vllm_engine.py
+++ python/ray/llm/_internal/serve/deployments/llm/vllm/vllm_engine.py
@@ -184,7 +184,7 @@ class VLLMEngine(LLMEngine):
 
         await init_app_state(
             engine_client=self._engine_client,
-            # [ahao] remove vllm_config for vllm v1.12
+            # TODO (ahao): remove vllm_config for vllm v1.12
             vllm_config=vllm_engine_config,
             state=state,
             args=args,
