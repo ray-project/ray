1cf877e836b849584ae07620522a75cf547dfc37
omkar <omkar@anyscale.com>
2025-10-16 19:55:52 +0000

    cleanup

--- python/ray/serve/_private/controller.py
+++ python/ray/serve/_private/controller.py
@@ -1035,6 +1035,7 @@ class ServeController:
                     protocol=RequestProtocol.HTTP,
                     route_prefix="/",
                     targets=self.proxy_state_manager.get_targets(RequestProtocol.HTTP),
+                    app_name="",
                 )
             )
             if is_grpc_enabled(self.get_grpc_config()):
@@ -1045,6 +1046,7 @@ class ServeController:
                         targets=self.proxy_state_manager.get_targets(
                             RequestProtocol.GRPC
                         ),
+                        app_name="",
                     )
                 )
         return target_groups
