3ea3f02064256b7e3b95f4fc72fe3229369c14a3
elliot-barn <elliot.barnwell@anyscale.com>
2025-10-17 20:06:08 +0000

    trigger eslint
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- python/ray/dashboard/client/src/App.tsx
+++ python/ray/dashboard/client/src/App.tsx
@@ -61,7 +61,6 @@ import { lightTheme } from "./theme";
 dayjs.extend(duration);
 const test
 console.log(test);
-
 // lazy loading fro prevent loading too much code at once
 const Actors = React.lazy(() => import("./pages/actor"));
 const CMDResult = React.lazy(() => import("./pages/cmd/CMDResult"));
