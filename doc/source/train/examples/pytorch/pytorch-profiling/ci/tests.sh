59718a5de4995b968809ebb4f0b7d333209198bd
Lehui Liu <lehui@anyscale.com>
2025-10-17 16:27:23 -0700

    [doc][release] Add PyTorch Profiler and Ray Train template (#57133)
    This PR adds a workspace template that walks users through how to
    integrate PyTorch Profiler with Ray Train. The purpose of this template
    is to walk user through how to generate trace/memory profile with
    Pytorch Profiler in the TorchTrainer.
    
    For a high level overview, this template covers:
    
    1. A hands-on example of training an image classification model
    2. A simple torch profiler integration script
    3. Some more advanced use cases including `record_function` to customize
    the profiling experience.
    4. a successful release test run:
    https://buildkite.com/ray-project/release/builds/63492#0199e3e4-fa8c-4f4f-a92c-f6d47a415c53
    
    Testing:
    testing in Anyscale workspace
    
    ---------
    
    Signed-off-by: Lehui Liu <lehui@anyscale.com>

---
+++ doc/source/train/examples/pytorch/pytorch-profiling/ci/tests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python ci/nb2py.py README.ipynb README.py  # convert notebook to py script
+python README.py  # be sure to use ipython to ensure even non-python cells are executed properly
+rm README.py  # remove the generated script
