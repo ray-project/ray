5ddd468ee30ed4577fbc4d0622a911c899f0824e
peterxcli <peterxcli@gmail.com>
2025-10-20 04:43:41 +0000

    chore: move new test into test_operators
    Signed-off-by: peterxcli <peterxcli@gmail.com>

--- python/ray/data/tests/test_operators.py
+++ python/ray/data/tests/test_operators.py
@@ -1549,6 +1549,31 @@ def test_per_block_limit_fn(blocks_data, per_block_limit, expected_output):
     assert result_data == expected_output
 
 
+def test_apply_transform_dag_consistency():
+    from ray.data._internal.logical.interfaces.operator import Operator
+
+    a = Operator("A", [])
+    b = Operator("B", [])
+    c = Operator("C", [a, b])
+
+    def transform_b(op: Operator) -> Operator:
+        """Transform only operator B, leaving A unchanged."""
+        if op.name == "B":
+            return Operator("Transformed B", op.input_dependencies)
+        return op
+
+    c_transformed = c._apply_transform(transform_b)
+
+    # This should create the DAG:
+    #               A --|
+    #                   |--> C
+    #   B transformed --|
+
+    assert c_transformed.name == "C"
+    assert c_transformed.input_dependencies[0].output_dependencies[0].name == "C"
+    assert c_transformed is c_transformed.input_dependencies[0].output_dependencies[0]
+
+
 if __name__ == "__main__":
     import sys
 
