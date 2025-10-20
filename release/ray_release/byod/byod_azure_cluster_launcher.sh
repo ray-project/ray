be234761df428d00e4ba6bec873552f5656e467c
kevin <kevin@anyscale.com>
2025-10-20 23:06:09 +0000

    p
    Signed-off-by: kevin <kevin@anyscale.com>

--- release/ray_release/byod/byod_azure_cluster_launcher.sh
+++ release/ray_release/byod/byod_azure_cluster_launcher.sh
@@ -3,4 +3,4 @@
 
 set -exo pipefail
 
-pip3 install azure-identity azure-mgmt-network azure-mgmt-compute azure-common msrestazure
+pip3 install azure-core==1.35.0 azure-identity==1.23.1 azure-mgmt-compute==35.0.0 azure-mgmt-network==29.0.0 azure-mgmt-resource==24.0.0 azure-common==1.1.28 msrest==0.7.1 msrestazure==0.6.4.post1
