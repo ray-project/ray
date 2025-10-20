b5a5c820ce3becdbadab86e71efad8e3aeadf302
elliot-barn <elliot.barnwell@anyscale.com>
2025-10-20 22:33:54 +0000

    creating depset for e2e rag
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- release/ray_release/byod/byod_e2e_rag.sh
+++ release/ray_release/byod/byod_e2e_rag.sh
@@ -8,10 +8,4 @@ sudo apt-get update && \
     sudo rm -rf /var/lib/apt/lists/*
 
 # Install python dependencies
-pip3 install --no-cache-dir \
-    "unstructured[all-docs]==0.16.23" \
-    "sentence-transformers==3.4.1" \
-    "chromadb==0.6.3" \
-    "langchain_text_splitters==0.3.6" \
-    "pandas==2.2.3" \
-    "tiktoken==0.9.0"
+pip3 install -r python_depset.lock --system --no-deps --index-strategy unsafe-best-match
