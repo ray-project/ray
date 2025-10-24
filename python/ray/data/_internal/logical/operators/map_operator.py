9d277a5282fa40c76f6232021e1ac6fc4f89cc9d
You-Cheng Lin <106612301+owenowenisme@users.noreply.github.com>
2025-10-25 03:28:10 +0800

    [Data] `map_batches` support limit_pushdown (#57880)
    ## Description
    
    This PR adds a ‎`preserve_row` option to ‎`map_batches`. When
    ‎`preserve_row` is true, the limit operator can be pushed down through
    this ‎`map_batches` call for optimization.
    
    Note: ‎`map_group` is built on ‎`map_batches`, but limit pushdown
    support for ‎`map_group` is out of scope for this PR, so
    ‎`preserve_row_count` is set to false for it.
    
    
    ## Related issues
    
    ## Additional information
    
    ---------
    
    Signed-off-by: You-Cheng Lin <youchenglin@youchenglin-L3DPGF50JG.local>
    Signed-off-by: You-Cheng Lin <mses010108@gmail.com>
    Co-authored-by: You-Cheng Lin <youchenglin@youchenglin-L3DPGF50JG.local>

--- python/ray/data/_internal/logical/operators/map_operator.py
+++ python/ray/data/_internal/logical/operators/map_operator.py
@@ -169,6 +169,7 @@ class MapBatches(AbstractUDFMap):
         fn_constructor_kwargs: Optional[Dict[str, Any]] = None,
         min_rows_per_bundled_input: Optional[int] = None,
         compute: Optional[ComputeStrategy] = None,
+        udf_modifying_row_count: bool = True,
         ray_remote_args_fn: Optional[Callable[[], Dict[str, Any]]] = None,
         ray_remote_args: Optional[Dict[str, Any]] = None,
     ):
@@ -188,9 +189,10 @@ class MapBatches(AbstractUDFMap):
         self._batch_size = batch_size
         self._batch_format = batch_format
         self._zero_copy_batch = zero_copy_batch
+        self._udf_modifying_row_count = udf_modifying_row_count
 
     def can_modify_num_rows(self) -> bool:
-        return False
+        return self._udf_modifying_row_count
 
 
 class MapRows(AbstractUDFMap):
