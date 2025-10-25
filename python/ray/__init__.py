2af602c8cefad82b30d0e9bf5976c12b6b497a25
YoussefEssDS <oyoussefesseddiq@gmail.com>
2025-10-24 23:58:10 -0400

    Revert init
    Signed-off-by: YoussefEssDS <oyoussefesseddiq@gmail.com>

--- python/ray/__init__.py
+++ python/ray/__init__.py
@@ -3,8 +3,6 @@ from ray._private import log  # isort: skip # noqa: F401
 import logging
 import os
 import sys
-from types import ModuleType
-import importlib
 from typing import TYPE_CHECKING
 
 log.generate_logging_config()
@@ -79,100 +77,12 @@ _configure_system()
 # Delete configuration function.
 del _configure_system
 
-_IS_DOC_BUILD = os.environ.get("RAY_DOC_BUILD") == "1"
-
-
-class _RayletStubModule(ModuleType):
-    """Fallback stub module used when native ray._raylet bindings are unavailable."""
-
-    def __getattr__(self, name: str):
-        placeholder = type(name, (), {})
-        setattr(self, name, placeholder)
-        return placeholder
-
-
-def _install_raylet_stub() -> ModuleType:
-    stub = _RayletStubModule("ray._raylet")
-
-    # Known classes/identifiers expected during import.
-    def _make_id_type(name: str):
-        def _repr(self):
-            return f"<{name} stub>"
-
-        cls_dict = {
-            "__repr__": _repr,
-            "from_random": classmethod(lambda cls, *a, **k: cls()),
-            "from_hex": classmethod(lambda cls, *a, **k: cls()),
-        }
-        return type(name, (), cls_dict)
-
-    for attr in [
-        "ActorClassID",
-        "ActorID",
-        "NodeID",
-        "JobID",
-        "WorkerID",
-        "FunctionID",
-        "ObjectID",
-        "ObjectRef",
-        "ObjectRefGenerator",
-        "DynamicObjectRefGenerator",
-        "TaskID",
-        "UniqueID",
-        "Language",
-        "PlacementGroupID",
-        "ClusterID",
-    ]:
-        setattr(stub, attr, _make_id_type(attr))
-
-    class _ConfigStub:
-        def __init__(self, *args, **kwargs):
-            pass
-
-        def __getattr__(self, name: str):
-            def _stub(*a, **k):
-                return 0
-
-            return _stub
-
-        def __getattr__(self, name):
-            def _stub(*args, **kwargs):
-                return 0
-
-            return _stub
-
-    stub.Config = _ConfigStub
-    stub.STREAMING_GENERATOR_RETURN = object()
-    stub.PythonFunctionDescriptor = type("PythonFunctionDescriptor", (), {})
-
-    def _stub_function(*args, **kwargs):
-        raise RuntimeError(
-            "ray._raylet functionality is unavailable during documentation builds."
-        )
-
-    stub.raise_sys_exit_with_custom_error_message = _stub_function
-
-    sys.modules["ray._raylet"] = stub
-    return stub
-
-
-if _IS_DOC_BUILD and "ray._raylet" not in sys.modules:
-    _install_raylet_stub()
-
 from ray import _version  # noqa: E402
 
 __commit__ = _version.commit
 __version__ = _version.version
 
-try:
-    import ray._raylet  # noqa: E402
-except ModuleNotFoundError:
-    if _IS_DOC_BUILD:
-        _install_raylet_stub()
-        importlib.invalidate_caches()
-        import ray._raylet  # noqa: E402
-    else:
-        raise
+import ray._raylet  # noqa: E402
 
 from ray._raylet import (  # noqa: E402,F401
     ActorClassID,
