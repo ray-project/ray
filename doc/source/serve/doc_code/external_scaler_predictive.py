1a7dcd4caf3cc435f7ff813fd07eb4822b39d45b
harshit <harshit@anyscale.com>
2025-10-27 05:26:07 +0000

    review changes
    Signed-off-by: harshit <harshit@anyscale.com>

--- doc/source/serve/doc_code/external_scaler_predictive.py
+++ doc/source/serve/doc_code/external_scaler_predictive.py
@@ -1,13 +1,11 @@
 # __serve_example_begin__
 import time
-
 from ray import serve
 
 
 @serve.deployment(num_replicas=3, external_scaler_enabled=True)
 class TextProcessor:
     """A simple text processing deployment that can be scaled externally."""
-
     def __init__(self):
         self.request_count = 0
 
@@ -15,7 +13,6 @@ class TextProcessor:
         # Simulate text processing work
         time.sleep(0.1)
         self.request_count += 1
-
         return {
             "processed_text": text.upper(),
             "length": len(text),
