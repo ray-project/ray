9d158a4ccdc43b8afc044ab08e53430d89edcd60
Srinath Krishnamachari <srinath.krishnamachari@anyscale.com>
2025-10-27 19:30:21 +0000

    [Train] Add streaming repartition to Training Injest
    Signed-off-by: Srinath Krishnamachari <srinath.krishnamachari@anyscale.com>

--- release/train_tests/benchmark/image_classification/parquet/factory.py
+++ release/train_tests/benchmark/image_classification/parquet/factory.py
@@ -53,6 +53,7 @@ class ImageClassificationParquetRayDataLoaderFactory(
             self._data_dirs[DatasetKey.TRAIN],
             columns=["image", "label"],
         ).map(get_preprocess_map_fn(decode_image=True, random_transforms=True))
+        train_ds = train_ds.repartition(target_num_rows_per_block=self.get_dataloader_config().train_batch_size)
 
         if self.get_dataloader_config().limit_training_rows > 0:
             train_ds = train_ds.limit(self.get_dataloader_config().limit_training_rows)
@@ -62,6 +63,7 @@ class ImageClassificationParquetRayDataLoaderFactory(
             self._data_dirs[DatasetKey.TRAIN],
             columns=["image", "label"],
         ).map(get_preprocess_map_fn(decode_image=True, random_transforms=False))
+        val_ds = val_ds.repartition(target_num_rows_per_block=self.get_dataloader_config().validation_batch_size)
 
         if self.get_dataloader_config().limit_validation_rows > 0:
             val_ds = val_ds.limit(self.get_dataloader_config().limit_validation_rows)
