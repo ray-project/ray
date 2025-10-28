c92bb27b8778fc7680918c7eba6993e489af8f68
harshit <harshit@anyscale.com>
2025-10-28 11:04:28 +0000

    fix tests
    Signed-off-by: harshit <harshit@anyscale.com>

--- python/ray/serve/tests/unit/test_application_state.py
+++ python/ray/serve/tests/unit/test_application_state.py
@@ -244,6 +244,7 @@ def mocked_application_state() -> Tuple[ApplicationState, MockDeploymentStateMan
         autoscaling_state_manager=AutoscalingStateManager(),
         endpoint_state=MockEndpointState(),
         logging_config=LoggingConfig(),
+        external_scaler_enabled=False,
     )
     yield application_state, deployment_state_manager
 
@@ -1480,6 +1481,7 @@ class TestAutoscale:
             autoscaling_state_manager=AutoscalingStateManager(),
             endpoint_state=MockEndpointState(),
             logging_config=LoggingConfig(),
+            external_scaler_enabled=False,
         )
 
         # Verify autoscale returns False
