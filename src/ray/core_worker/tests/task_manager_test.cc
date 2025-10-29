608f6ba6c69389b71902688136c3035d8f9362aa
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2025-10-29 14:26:01 -0700

    [core][stats-die/03] kill STATS in core worker component (#58060)
    This PR replace STATS with Metric as a way to define metric inside ray
    (as a unification effort) in all core worker components. For the most
    parts, metrics are defined as the top level component
    (core_worker_process.cc) and pass down as an interface to the
    sub-components.
    
    **Details**
    Full context of this refactoring work.
    - Each component (e.g., gcs, raylet, core_worker, etc.) now has a
    metrics.h file located in its top-level directory. This file defines all
    metrics for that component.
    - In most cases, metrics are defined once in the main entry point of
    each component (gcs/gcs_server_main.cc for GCS, raylet/main.cc for
    Raylet, core_worker/core_worker_process.cc for the Core Worker, etc.).
    These metrics are then passed down to subcomponents via the
    ray::observability::MetricInterface.
    - This approach significantly reduces rebuild time when metric
    infrastructure changes. Previously, a change would trigger a full Ray
    rebuild; now, only the top-level entry points of each component need
    rebuilding.
    - There are a few exceptions where metrics are tracked inside object
    libraries (e.g., task_specification). In these cases, metrics are
    defined within the library itself, since there is no corresponding
    top-level entry point.
    - Finally, the obsolete metric_defs.h and metric_defs.cc files can now
    be completely removed. This paves the way for further dead code cleanup
    in a future PR.
    
    Test:
    - CI
    
    Signed-off-by: Cuong Nguyen <can@anyscale.com>
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- src/ray/core_worker/tests/task_manager_test.cc
+++ src/ray/core_worker/tests/task_manager_test.cc
@@ -183,6 +183,7 @@ class TaskManagerTest : public ::testing::Test {
             },
             mock_gcs_client_,
             fake_task_by_state_counter_,
+            fake_total_lineage_bytes_gauge_,
             /*free_actor_object_callback=*/[](const ObjectID &object_id) {}) {}
 
   virtual void TearDown() { AssertNoLeaks(); }
@@ -230,6 +231,7 @@ class TaskManagerTest : public ::testing::Test {
   uint32_t last_delay_ms_ = 0;
   std::unordered_set<ObjectID> stored_in_plasma;
   ray::observability::FakeGauge fake_task_by_state_counter_;
+  ray::observability::FakeGauge fake_total_lineage_bytes_gauge_;
 };
 
 class TaskManagerLineageTest : public TaskManagerTest {
@@ -1404,6 +1406,7 @@ TEST_F(TaskManagerTest, PlasmaPut_ObjectStoreFull_FailsTaskAndWritesError) {
       },
       mock_gcs_client_,
       fake_task_by_state_counter_,
+      fake_total_lineage_bytes_gauge_,
       /*free_actor_object_callback=*/[](const ObjectID &object_id) {});
 
   rpc::Address caller_address;
@@ -1467,6 +1470,7 @@ TEST_F(TaskManagerTest, PlasmaPut_TransientFull_RetriesThenSucceeds) {
       },
       mock_gcs_client_,
       fake_task_by_state_counter_,
+      fake_total_lineage_bytes_gauge_,
       /*free_actor_object_callback=*/[](const ObjectID &object_id) {});
 
   rpc::Address caller_address;
@@ -1528,6 +1532,7 @@ TEST_F(TaskManagerTest, DynamicReturn_PlasmaPutFailure_FailsTaskImmediately) {
       },
       mock_gcs_client_,
       fake_task_by_state_counter_,
+      fake_total_lineage_bytes_gauge_,
       /*free_actor_object_callback=*/[](const ObjectID &object_id) {});
 
   auto spec = CreateTaskHelper(1, {}, /*dynamic_returns=*/true);
@@ -3016,6 +3021,7 @@ TEST_F(TaskManagerTest, TestRetryErrorMessageSentToCallback) {
           -> std::shared_ptr<ray::rpc::CoreWorkerClientInterface> { return nullptr; },
       mock_gcs_client_,
       fake_task_by_state_counter_,
+      fake_total_lineage_bytes_gauge_,
       /*free_actor_object_callback=*/[](const ObjectID &object_id) {});
 
   // Create a task with retries enabled
@@ -3096,6 +3102,7 @@ TEST_F(TaskManagerTest, TestErrorLogWhenPushErrorCallbackFails) {
           -> std::shared_ptr<ray::rpc::CoreWorkerClientInterface> { return nullptr; },
       mock_gcs_client_,
       fake_task_by_state_counter_,
+      fake_total_lineage_bytes_gauge_,
       /*free_actor_object_callback=*/[](const ObjectID &object_id) {});
 
   // Create a task that will be retried
