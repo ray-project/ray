2220b6b44bd3a170ab924a6e8941479e247dc68a
soffer-anyscale <stephen.offer@anyscale.com>
2025-10-31 13:19:33 -0600

    Simplify size estimation logic
    - Remove complex block overhead calculations
    - Remove unnecessary ObjectRef size estimation
    - Use sys.getsizeof() for all objects (simple and correct)
    - Remove unused constants (RAY_OBJECTREF_SIZE_BYTES, etc.)
    - Most cached results are simple Python types (int, list, schema)
    
    Signed-off-by: soffer-anyscale <stephen.offer@anyscale.com>

--- python/ray/data/_internal/cache/size_utils.py
+++ python/ray/data/_internal/cache/size_utils.py
@@ -1,36 +1,9 @@
-"""Object size calculation for Ray Data caching.
-
-For Ray objects (ObjectRef, MaterializedDataset), only measures pointer/metadata
-size, not actual data size, since data lives in the object store.
-"""
+"""Object size calculation for Ray Data caching."""
 
 import sys
 from typing import Any
 
-from .constants import (
-    RAY_BLOCK_OVERHEAD_BYTES,
-    RAY_DATASET_METADATA_SIZE_BYTES,
-    RAY_OBJECTREF_SIZE_BYTES,
-)
-
 
 def get_object_size(obj: Any) -> int:
     """Get approximate size of an object in bytes."""
-    try:
-        import ray
-
-        if isinstance(obj, ray.ObjectRef):
-            return RAY_OBJECTREF_SIZE_BYTES
-    except ImportError:
-        pass
-
-    if hasattr(obj, "num_blocks"):
-        try:
-            num_blocks = obj.num_blocks()
-            return (
-                num_blocks * RAY_BLOCK_OVERHEAD_BYTES + RAY_DATASET_METADATA_SIZE_BYTES
-            )
-        except (AttributeError, TypeError):
-            pass
-
     return sys.getsizeof(obj)
