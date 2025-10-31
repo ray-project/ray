02f639875a7a4c747ece5590b0285723cb8c3576
Mark Towers <mark@anyscale.com>
2025-10-31 14:04:41 +0000

    remove all redundant ruff isort configures and run pre-commit
    Signed-off-by: Mark Towers <mark@anyscale.com>

--- rllib/utils/replay_buffers/__init__.py
+++ rllib/utils/replay_buffers/__init__.py
@@ -1,10 +1,11 @@
+from ray.rllib.utils.replay_buffers import utils
 from ray.rllib.utils.replay_buffers.episode_replay_buffer import EpisodeReplayBuffer
 from ray.rllib.utils.replay_buffers.fifo_replay_buffer import FifoReplayBuffer
-from ray.rllib.utils.replay_buffers.multi_agent_mixin_replay_buffer import (
-    MultiAgentMixInReplayBuffer,
-)
 from ray.rllib.utils.replay_buffers.multi_agent_episode_buffer import (
     MultiAgentEpisodeReplayBuffer,
+)
+from ray.rllib.utils.replay_buffers.multi_agent_mixin_replay_buffer import (
+    MultiAgentMixInReplayBuffer,
 )
 from ray.rllib.utils.replay_buffers.multi_agent_prioritized_episode_buffer import (
     MultiAgentPrioritizedEpisodeReplayBuffer,
@@ -24,7 +25,6 @@ from ray.rllib.utils.replay_buffers.prioritized_replay_buffer import (
 )
 from ray.rllib.utils.replay_buffers.replay_buffer import ReplayBuffer, StorageUnit
 from ray.rllib.utils.replay_buffers.reservoir_replay_buffer import ReservoirReplayBuffer
-from ray.rllib.utils.replay_buffers import utils
 
 __all__ = [
     "EpisodeReplayBuffer",
