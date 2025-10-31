f69992a04b81339d28fadc7a7dbf612c7c4e23f6
Mark Towers <mark@anyscale.com>
2025-10-31 15:44:21 +0000

    Fix isort for replay-buffer
    Signed-off-by: Mark Towers <mark@anyscale.com>

--- rllib/utils/replay_buffers/__init__.py
+++ rllib/utils/replay_buffers/__init__.py
@@ -1,4 +1,3 @@
-from ray.rllib.utils.replay_buffers import utils
 from ray.rllib.utils.replay_buffers.episode_replay_buffer import EpisodeReplayBuffer
 from ray.rllib.utils.replay_buffers.fifo_replay_buffer import FifoReplayBuffer
 from ray.rllib.utils.replay_buffers.multi_agent_episode_buffer import (
@@ -25,6 +24,8 @@ from ray.rllib.utils.replay_buffers.prioritized_replay_buffer import (
 )
 from ray.rllib.utils.replay_buffers.replay_buffer import ReplayBuffer, StorageUnit
 from ray.rllib.utils.replay_buffers.reservoir_replay_buffer import ReservoirReplayBuffer
+
+from ray.rllib.utils.replay_buffers import utils  # isort: skip
 
 __all__ = [
     "EpisodeReplayBuffer",
