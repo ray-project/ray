5c361f290bfbdc4f57eabc1d05acb4c96ff05005
sampan <sampan@anyscale.com>
2025-11-01 14:58:50 +0000

    fix lint
    Signed-off-by: sampan <sampan@anyscale.com>

--- python/ray/dashboard/modules/dashboard_sdk.py
+++ python/ray/dashboard/modules/dashboard_sdk.py
@@ -24,9 +24,7 @@ from ray._private.runtime_env.packaging import (
 from ray._private.runtime_env.py_modules import upload_py_modules_if_needed
 from ray._private.runtime_env.working_dir import upload_working_dir_if_needed
 from ray._private.utils import split_address
-from ray._raylet import AuthenticationTokenLoader
 from ray.autoscaler._private.cli_logger import cli_logger
-from ray.dashboard.authentication_utils import is_token_auth_enabled
 from ray.dashboard.modules.job.common import uri_to_http_components
 from ray.util.annotations import DeveloperAPI, PublicAPI
 
