9f8ee5ebc982a43d273e4d7182c895730d0c041b
Daniel Shin <kyuseung1016@gmail.com>
2025-11-07 20:57:22 +0900

    finish label, categorizer
    Signed-off-by: Daniel Shin <kyuseung1016@gmail.com>

--- python/ray/data/preprocessors/encoder.py
+++ python/ray/data/preprocessors/encoder.py
@@ -530,14 +530,11 @@ class LabelEncoder(Preprocessor):
         self.output_column = output_column or label_column
 
     def _fit(self, dataset: "Dataset") -> Preprocessor:
-        self.stat_computation_plan.add_callable_stat(
-            stat_fn=lambda key_gen: compute_unique_value_indices(
-                dataset=dataset,
-                columns=[self.label_column],
-                key_gen=key_gen,
+        self.stat_computation_plan.add_aggregator(
+            aggregator_fn=lambda col: Unique(
+                col, ignore_nulls=False, encode_lists=True
             ),
             post_process_fn=unique_post_fn(),
-            stat_key_fn=lambda col: f"unique({col})",
             post_key_fn=lambda col: f"unique_values({col})",
             columns=[self.label_column],
         )
@@ -687,17 +684,14 @@ class Categorizer(Preprocessor):
         def callback(unique_indices: Dict[str, Dict]) -> pd.CategoricalDtype:
             return pd.CategoricalDtype(unique_indices.keys())
 
-        self.stat_computation_plan.add_callable_stat(
-            stat_fn=lambda key_gen: compute_unique_value_indices(
-                dataset=dataset,
-                columns=columns_to_get,
-                key_gen=key_gen,
+        self.stat_computation_plan.add_aggregator(
+            aggregator_fn=lambda col: Unique(
+                col, ignore_nulls=False, encode_lists=True
             ),
             post_process_fn=make_post_processor(
                 base_fn=unique_post_fn(drop_na_values=True),
                 callbacks=[callback],
             ),
-            stat_key_fn=lambda col: f"unique({col})",
             post_key_fn=lambda col: col,
             columns=columns_to_get,
         )
@@ -712,72 +706,6 @@ class Categorizer(Preprocessor):
             f"{self.__class__.__name__}(columns={self.columns!r}, "
             f"dtypes={self.dtypes!r}, output_columns={self.output_columns!r})"
         )
-
-
-def compute_unique_value_indices(
-    *,
-    dataset: "Dataset",
-    columns: List[str],
-    key_gen: Callable,
-    encode_lists: bool = True,
-    max_categories: Optional[Dict[str, int]] = None,
-):
-    if max_categories is None:
-        max_categories = {}
-    columns_set = set(columns)
-    for column in max_categories:
-        if column not in columns_set:
-            raise ValueError(
-                f"You set `max_categories` for {column}, which is not present in "
-                f"{columns}."
-            )
-
-    def get_pd_value_counts_per_column(col: pd.Series) -> Dict:
-        # special handling for lists
-        if _is_series_composed_of_lists(col):
-            if encode_lists:
-                counter = Counter()
-
-                def update_counter(element):
-                    counter.update(element)
-                    return element
-
-                col.map(update_counter)
-                return counter
-            else:
-                # convert to tuples to make lists hashable
-                col = col.map(lambda x: tuple(x))
-        return Counter(col.value_counts(dropna=False).to_dict())
-
-    def get_pd_value_counts(df: pd.DataFrame) -> Dict[str, List[Dict]]:
-
-        df_columns = df.columns.tolist()
-        result = {}
-        for col in columns:
-            if col in df_columns:
-                result[col] = [get_pd_value_counts_per_column(df[col])]
-            else:
-                raise ValueError(
-                    f"Column '{col}' does not exist in DataFrame, which has columns: {df_columns}"  # noqa: E501
-                )
-        return result
-
-    value_counts_ds = dataset.map_batches(get_pd_value_counts, batch_format="pandas")
-    unique_values_by_col: Dict[str, Set] = {key_gen(col): set() for col in columns}
-    for batch in value_counts_ds.iter_batches(batch_size=None):
-        for col, counters in batch.items():
-            for counter in counters:
-                counter: Dict[Any, int] = {
-                    k: v for k, v in counter.items() if v is not None
-                }
-                if col in max_categories:
-                    counter: Dict[Any, int] = dict(
-                        Counter(counter).most_common(max_categories[col])
-                    )
-                # add only column values since frequencies are needed beyond this point
-                unique_values_by_col[key_gen(col)].update(counter.keys())
-
-    return unique_values_by_col
 
 
 def fit_hot_encoders(
