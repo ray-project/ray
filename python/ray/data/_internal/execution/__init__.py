4bbcbbd29aff9637b038b13b4fbb951ce19798ab
iamjustinhsu <jhsu@anyscale.com>
2025-11-10 14:20:06 -0800

    clean
    Signed-off-by: iamjustinhsu <jhsu@anyscale.com>

--- python/ray/data/_internal/execution/__init__.py
+++ python/ray/data/_internal/execution/__init__.py
@@ -3,6 +3,4 @@ from .ranker import DefaultRanker, Ranker
 
 def create_ranker() -> Ranker:
     """Create a ranker instance based on environment and configuration."""
-    from ray._private.ray_constants import env_bool
-
     return DefaultRanker()
