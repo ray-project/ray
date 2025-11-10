9ab06d34f307a04ec089b409e3ebba308eea0d1c
iamjustinhsu <jhsu@anyscale.com>
2025-11-10 14:15:36 -0800

    clean
    Signed-off-by: iamjustinhsu <jhsu@anyscale.com>

--- python/ray/data/_internal/execution/__init__.py
+++ python/ray/data/_internal/execution/__init__.py
@@ -5,10 +5,4 @@ def create_ranker() -> Ranker:
     """Create a ranker instance based on environment and configuration."""
     from ray._private.ray_constants import env_bool
 
-    # Check if RayTurbo ranker should be used
-    if env_bool("RAY_DATA_USE_TURBO_RANKER", True):
-        from ray.anyscale.data._internal.execution.ranker import LocationAwareRanker
-
-        return LocationAwareRanker()
-    else:
-        return DefaultRanker()
+    return DefaultRanker()
