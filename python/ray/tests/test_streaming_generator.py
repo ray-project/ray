5d6d63609e93f95d262ad15fb79546bd56461258
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-11-11 15:36:36 -0600

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/tests/test_streaming_generator.py
+++ python/ray/tests/test_streaming_generator.py
@@ -96,13 +96,7 @@ def test_streaming_object_ref_generator_basic_unit(mocked_worker):
             with pytest.raises(StopIteration):
                 generator._next_sync(timeout_s=0)
 
-            # Clear frames held by the StopIteration traceback so generator frames can be freed.
-            tb = sys.exc_info()[2]
-            if tb is not None:
-                traceback.clear_frames(tb)
-
-            del generator
-            gc.collect()
+            generator.__del__()
             c.async_delete_object_ref_stream.assert_called()
 
 
