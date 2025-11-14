86df93040707d5f5b9f03c96bc8867624d3c7335
行筠 <wxw403883@alibaba-inc.com>
2025-11-14 14:43:24 +0800

    autoscaler for spot resources

--- python/ray/autoscaler/v2/tests/test_reconciler.py
+++ python/ray/autoscaler/v2/tests/test_reconciler.py
@@ -21,8 +21,9 @@ from ray.autoscaler.v2.instance_manager.node_provider import (  # noqa
 )
 from ray.autoscaler.v2.instance_manager.reconciler import Reconciler, logger
 from ray.autoscaler.v2.instance_manager.storage import InMemoryStorage
-from ray.autoscaler.v2.instance_manager.subscribers.cloud_resource_monitor import \
+from ray.autoscaler.v2.instance_manager.subscribers.cloud_resource_monitor import (
     CloudResourceMonitor
+)
 from ray.autoscaler.v2.instance_manager.subscribers.ray_stopper import RayStopError
 from ray.autoscaler.v2.instance_manager.subscribers.threaded_ray_installer import (
     RayInstallError,
