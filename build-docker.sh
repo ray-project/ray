3c3b6fec20850dea2afc683a2aa90291966c8cd1
elliot-barn <elliot.barnwell@anyscale.com>
2025-11-15 19:49:10 +0000

    updating lock file location
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- build-docker.sh
+++ build-docker.sh
@@ -64,12 +64,14 @@ RAY_DEPS_BUILD_DIR="$(mktemp -d)"
 cp docker/base-deps/Dockerfile "${RAY_DEPS_BUILD_DIR}/."
 mkdir -p "${RAY_DEPS_BUILD_DIR}/python"
 cp python/requirements_compiled.txt "${RAY_DEPS_BUILD_DIR}/python/requirements_compiled.txt"
-REQUIREMENTS_FILE_BASE_DEPS="ray_base_deps_py${PYTHON_VERSION}.lock"
+REQUIREMENTS_FILE_BASE_DEPS="python/deplocks/base_deps/ray_base_deps_py${PYTHON_VERSION}.lock"
+cp "${REQUIREMENTS_FILE_BASE_DEPS}" "${RAY_DEPS_BUILD_DIR}/${REQUIREMENTS_FILE_BASE_DEPS}"
+
 BUILD_CMD=(
     docker build "${BUILD_ARGS[@]}"
     --build-arg BASE_IMAG="$BASE_IMAGE"
     --build-arg PYTHON_VERSION="${PYTHON_VERSION}"
-    --build-arg PYTHON_DEPSET="python/deplocks/base_deps/${REQUIREMENTS_FILE_BASE_DEPS}"
+    --build-arg PYTHON_DEPSET="${RAY_DEPS_BUILD_DIR}/${REQUIREMENTS_FILE_BASE_DEPS}"
     -t "rayproject/base-deps:dev$GPU" "${RAY_DEPS_BUILD_DIR}"
 )
 
