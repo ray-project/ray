c528543686e92b8dd8bb11f896ad7452b61eb5e3
Lonnie Liu <lonnie@anyscale.com>
2025-11-16 19:14:31 +0000

    .

--- release/ray_release/buildkite/step.py
+++ release/ray_release/buildkite/step.py
@@ -159,7 +159,7 @@ def get_step(
     env_dict["ANYSCALE_PROJECT"] = get_test_project_id(test, default_project_id)
 
     step["env"].update(env_dict)
-    step["plugins"][0][DOCKER_PLUGIN_KEY]["image"] = "python:3.10"
+    step["plugins"][0][DOCKER_PLUGIN_KEY]["image"] = "python:3.9"
 
     commit = get_test_env_var("RAY_COMMIT")
     branch = get_test_env_var("RAY_BRANCH")
