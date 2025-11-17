8e96ab4d93eaba04b4d1d5703f9f34afd6b4bd5a
Goutam <goutam@anyscale.com>
2025-11-16 21:19:25 -0800

    Fix tests
    Signed-off-by: Goutam <goutam@anyscale.com>

--- python/ray/data/tests/test_predicate_pushdown.py
+++ python/ray/data/tests/test_predicate_pushdown.py
@@ -161,7 +161,7 @@ def test_chained_filter_with_expressions(parquet_ds):
         ),
         (
             lambda ds: ds.filter(expr=col("sepal.length") > 5.0),
-            "Filter[Filter(<expression>)]",  # CSV doesn't support predicate pushdown
+            "Filter[Filter(col('sepal.length') > 5.0)]",  # CSV doesn't support predicate pushdown
         ),
     ],
 )
@@ -194,8 +194,8 @@ def test_filter_mixed(csv_ds):
     # CSV doesn't support predicate pushdown, so filters stay after Read
     _check_plan_with_flexible_read(
         csv_ds,
-        "Filter[Filter(<lambda>)] -> Filter[Filter(<expression>)] -> "
-        "MapRows[Map(<lambda>)] -> Filter[Filter(<expression>)]",
+        "Filter[Filter(<lambda>)] -> Filter[Filter((col('sepal.length') > 4.0) & (col('sepal.length') > 3.0))] -> "
+        "MapRows[Map(<lambda>)] -> Filter[Filter((col('sepal.length') > 1.0) & (col('sepal.length') > 2.0))]",
         filtered_expr_data,
     )
 
@@ -230,7 +230,7 @@ def test_filter_mixed_expression_first_csv(ray_start_regular_shared):
     # CSV doesn't support predicate pushdown, so expression filters get fused but not pushed down
     _check_plan_with_flexible_read(
         ds,
-        "Filter[Filter(<expression>)] -> Filter[Filter(<lambda>)]",
+        "Filter[Filter((col('sepal.length') > 4.0) & (col('sepal.length') > 3.0))] -> Filter[Filter(<lambda>)]",
         filtered_expr_data,
     )
 
@@ -246,7 +246,7 @@ def test_filter_mixed_expression_not_readfiles(ray_start_regular_shared):
     assert all(record["id"] > 2.0 for record in filtered_expr_data)
     _check_valid_plan_and_result(
         ds,
-        "Read[ReadRange] -> Filter[Filter(<expression>)] -> "
+        "Read[ReadRange] -> Filter[Filter((col('id') > 2.0) & (col('id') > 1.0))] -> "
         "Filter[Filter(<lambda>)]",
         filtered_expr_data,
     )
