b894afd96bcfc6eca50c9ea11c6c14c3ca7a96f4
Goutam <goutam@anyscale.com>
2025-11-17 21:17:39 -0800

    [Data] - Change MapWorker repr to not be reliant on src_fn_name
    Signed-off-by: Goutam <goutam@anyscale.com>

--- python/ray/data/_internal/execution/operators/actor_pool_map_operator.py
+++ python/ray/data/_internal/execution/operators/actor_pool_map_operator.py
@@ -166,7 +166,7 @@ class ActorPoolMapOperator(MapOperator):
         self._actor_task_selector = self._create_task_selector(self._actor_pool)
         # A queue of bundles awaiting dispatch to actors.
         self._bundle_queue = create_bundle_queue()
-        # HACK: Without this, all actors show up as `_MapWorker` in Grafana, so we can’t
+        # HACK: Without this, all actors show up as `_MapWorker` in Grafana, so we can't
         # tell which operator they belong to. To fix that, we dynamically create a new
         # class per operator with a unique name.
         self._map_worker_cls = type(f"MapWorker({self.name})", (_MapWorker,), {})
@@ -593,7 +593,9 @@ class _MapWorker:
         )
 
     def __repr__(self):
-        return f"MapWorker({self.src_fn_name})"
+        # Use getattr to handle case where __init__ failed before src_fn_name was set.
+        # This can happen during actor restarts or initialization failures.
+        return f"MapWorker({getattr(self, 'src_fn_name', '<initializing>')})"
 
     def on_exit(self):
         """Called when the actor is about to exist.
@@ -1027,8 +1029,8 @@ class _ActorPool(AutoscalingActorPool):
     def get_logical_ids(self) -> List[str]:
         """Get the logical IDs for pending and running actors in the actor pool.
 
-        We can’t use Ray Core actor IDs because we need to identify actors by labels,
-        but labels must be set before creation, and actor IDs aren’t available until
+        We can't use Ray Core actor IDs because we need to identify actors by labels,
+        but labels must be set before creation, and actor IDs aren't available until
         after.
         """
         return list(self._actor_to_logical_id.values())
