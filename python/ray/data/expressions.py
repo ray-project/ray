950ef28c0f1b128313853d17bd4a7e4aa69fbf00
400Ping <fourhundredping@gmail.com>
2025-11-19 00:29:25 +0800

    add type-checking
    Signed-off-by: 400Ping <fourhundredping@gmail.com>

--- python/ray/data/expressions.py
+++ python/ray/data/expressions.py
@@ -23,6 +23,7 @@ from ray.data.datatype import DataType
 from ray.util.annotations import DeveloperAPI, PublicAPI
 
 if TYPE_CHECKING:
+    from ray.data.namespace_expressions.list_namespace import _ArrayNamespace
     from ray.data.namespace_expressions.list_namespace import _ListNamespace
     from ray.data.namespace_expressions.string_namespace import _StringNamespace
     from ray.data.namespace_expressions.struct_namespace import _StructNamespace
