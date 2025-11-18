dd56025339f9a3116ce5f3d3b2d890b7e3298f46
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-11-18 10:08:26 -0600

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/raylet_rpc_client/raylet_client.h
+++ src/ray/raylet_rpc_client/raylet_client.h
@@ -39,8 +39,7 @@ namespace rpc {
 
 /// Raylet client is responsible for communication with raylet. It implements
 /// [RayletClientInterface] and works on worker registration, lease management, etc.
-class RayletClient : public RayletClientInterface,
-                     public std::enable_shared_from_this<RayletClient> {
+class RayletClient : public RayletClientInterface {
  public:
   /// Connect to the raylet.
   ///
