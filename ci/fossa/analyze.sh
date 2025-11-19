eb7e801d07d25a737ecbb66569ee67313fcf13e8
Rajesh G <rajesh@anyscale.com>
2025-11-19 20:30:19 +0530

    fix linting issues
    Signed-off-by: Rajesh G <rajesh@anyscale.com>

--- ci/fossa/analyze.sh
+++ ci/fossa/analyze.sh
@@ -12,14 +12,14 @@ FOSSA_API_KEY="$(
 )"
 export FOSSA_API_KEY
 
-mkdir -p $OUTPUT_FOLDER
+mkdir -p "$OUTPUT_FOLDER"
 
-source $HOME/venv/bin/activate
-python ci/fossa/ray_oss_analysis.py -cmd bazelisk -p //:gen_ray_pkg -o $OUTPUT_FOLDER --log-file "$OUTPUT_FOLDER/package_license_analysis.log"
-cd $OUTPUT_FOLDER; "$FOSSA_BIN" analyze -p ray --fossa-deps-file fossa_deps.yaml
+source "$HOME/venv/bin/activate"
+python ci/fossa/ray_oss_analysis.py -cmd bazelisk -p //:gen_ray_pkg -o "$OUTPUT_FOLDER" --log-file "$OUTPUT_FOLDER/package_license_analysis.log"
+cd "$OUTPUT_FOLDER"; "$FOSSA_BIN" analyze -p ray --fossa-deps-file fossa_deps.yaml
 
 # copy to artifacts folder for upload
-cp $OUTPUT_FOLDER/askalono_results.json /artifact-mount/
-cp $OUTPUT_FOLDER/package_license_analysis.log /artifact-mount/
-cp $OUTPUT_FOLDER/askalono_results.xlsx /artifact-mount/
-cp $OUTPUT_FOLDER/fossa_deps.yaml /artifact-mount/
+cp "$OUTPUT_FOLDER"/askalono_results.json /artifact-mount/
+cp "$OUTPUT_FOLDER"/package_license_analysis.log /artifact-mount/
+cp "$OUTPUT_FOLDER"/askalono_results.xlsx /artifact-mount/
+cp "$OUTPUT_FOLDER"/fossa_deps.yaml /artifact-mount/
