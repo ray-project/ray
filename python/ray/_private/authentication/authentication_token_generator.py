500419e94af92b6ef6ed922c6c16b135f8a63fd4
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-11-19 09:13:53 -0600

    use secrets
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/_private/authentication/authentication_token_generator.py
+++ python/ray/_private/authentication/authentication_token_generator.py
@@ -5,5 +5,5 @@ def generate_new_authentication_token() -> str:
     """Generate an authentication token for the cluster.
 
     256 bits of entropy is considered sufficient to be durable to brute force attacks.
-	"""
+    """
     return secrets.token_hex(32)
--- python/ray/tests/authentication/test_async_grpc_interceptors.py
+++ python/ray/tests/authentication/test_async_grpc_interceptors.py
@@ -2,14 +2,14 @@ import grpc
 import pytest
 from grpc import aio as aiogrpc
 
+from ray._private.authentication.authentication_token_generator import (
+    generate_new_authentication_token,
+)
 from ray._private.authentication_test_utils import (
     authentication_env_guard,
     reset_auth_token_state,
     set_auth_mode,
     set_env_auth_token,
-)
-from ray._private.authentication.authentication_token_generator import (
-    generate_new_authentication_token,
 )
 from ray._private.grpc_utils import init_grpc_channel
 from ray.core.generated import reporter_pb2, reporter_pb2_grpc
