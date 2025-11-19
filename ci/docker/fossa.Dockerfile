2e001daa79dd76681fc3d66d0f3d11b84a78d17e
Rajesh G <rajesh@anyscale.com>
2025-11-19 19:57:22 +0530

    run askalono and push the identified licenses to fossa for tracking
    Signed-off-by: Rajesh G <rajesh@anyscale.com>

--- ci/docker/fossa.Dockerfile
+++ ci/docker/fossa.Dockerfile
@@ -12,6 +12,13 @@ mkdir -p "$HOME/fossa"
 curl -sSfL https://github.com/fossas/fossa-cli/releases/download/v3.10.6/fossa_3.10.6_linux_amd64.tar.gz \
     | tar -xvzf - -C "$HOME/fossa"
 
+uv venv --no-project $HOME/venv && source $HOME/venv/bin/activate && uv pip install pandas==2.3.3 openpyxl==3.1.5 pyyaml==6.0.3
+
+wget -O /tmp/askalono.zip https://github.com/jpeddicord/askalono/releases/download/0.5.0/askalono-Linux.zip 
+unzip -d /tmp/ /tmp/askalono.zip
+mkdir -p $HOME/.local/bin/
+mv /tmp/askalono $HOME/.local/bin/
+
 EOF
 
 CMD ["echo", "ray fossa"]
--- ci/fossa/analyze.sh
+++ ci/fossa/analyze.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 FOSSA_BIN="$HOME/fossa/fossa"
+OUTPUT_FOLDER="$HOME/fossa_output_folder"
 
 FOSSA_API_KEY="$(
     aws secretsmanager get-secret-value --region us-west-2 \
@@ -11,4 +12,14 @@ FOSSA_API_KEY="$(
 )"
 export FOSSA_API_KEY
 
-"$FOSSA_BIN" analyze
+mkdir -p $OUTPUT_FOLDER
+
+source $HOME/venv/bin/activate
+python ci/fossa/ray_oss_analysis.py -cmd bazelisk -p //:gen_ray_pkg -o $OUTPUT_FOLDER --log-file "$OUTPUT_FOLDER/package_license_analysis.log"
+cd $OUTPUT_FOLDER; "$FOSSA_BIN" analyze -p ray --fossa-deps-file fossa_deps.yaml
+
+# copy to artifacts folder for upload
+cp $OUTPUT_FOLDER/askalono_results.json /artifact-mount/
+cp $OUTPUT_FOLDER/package_license_analysis.log /artifact-mount/
+cp $OUTPUT_FOLDER/askalono_results.xlsx /artifact-mount/
+cp $OUTPUT_FOLDER/fossa_deps.yaml /artifact-mount/
