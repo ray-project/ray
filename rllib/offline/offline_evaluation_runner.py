4877b02d7a59503edbee4588df861856657b632f
Daniel Sperber <github.blurry@9ox.net>
2025-11-20 14:21:35 +0100

    Align with https://github.com/ray-project/ray/pull/57215
    Signed-off-by: Daniel Sperber <github.blurry@9ox.net>

--- rllib/offline/offline_evaluation_runner.py
+++ rllib/offline/offline_evaluation_runner.py
@@ -331,6 +331,9 @@ class OfflineEvaluationRunner(Runner, Checkpointable):
             )
 
     def _log_steps_evaluated_metrics(self, batch: MultiAgentBatch) -> None:
+        # Collect all module steps and add them for `ALL_MODULES` to avoid
+        # biasing the throughput by looping through modules.
+        total_module_steps = 0
         for mid, module_batch in batch.policy_batches.items():
             # Log weights seq no for this batch.
             self.metrics.log_value(
@@ -357,31 +360,34 @@ class OfflineEvaluationRunner(Runner, Checkpointable):
                 value=module_batch_size,
                 reduce="sum",
             )
-            # Log module steps (sum of all modules).
-            self.metrics.log_value(
-                key=(ALL_MODULES, NUM_MODULE_STEPS_SAMPLED),
-                value=module_batch_size,
-                reduce="sum",
-                clear_on_reduce=True,
-            )
-            self.metrics.log_value(
-                key=(ALL_MODULES, NUM_MODULE_STEPS_SAMPLED_LIFETIME),
-                value=module_batch_size,
-                reduce="sum",
-            )
-            # Log env steps (all modules).
-            self.metrics.log_value(
-                (ALL_MODULES, NUM_ENV_STEPS_SAMPLED),
-                module_batch_size,
-                reduce="sum",
-                clear_on_reduce=True,
-            )
-            self.metrics.log_value(
-                (ALL_MODULES, NUM_ENV_STEPS_SAMPLED_LIFETIME),
-                module_batch_size,
-                reduce="sum",
-                with_throughput=True,
-            )
+            total_module_steps += module_batch_size
+
+        # Log module steps (sum of all modules).
+        self.metrics.log_value(
+            key=(ALL_MODULES, NUM_MODULE_STEPS_SAMPLED),
+            value=total_module_steps,
+            reduce="sum",
+            clear_on_reduce=True,
+        )
+        self.metrics.log_value(
+            key=(ALL_MODULES, NUM_MODULE_STEPS_SAMPLED_LIFETIME),
+            value=total_module_steps,
+            reduce="sum",
+        )
+            
+        # Log env steps (all modules).
+        self.metrics.log_value(
+            (ALL_MODULES, NUM_ENV_STEPS_SAMPLED),
+            total_module_steps,
+            reduce="sum",
+            clear_on_reduce=True,
+        )
+        self.metrics.log_value(
+            (ALL_MODULES, NUM_ENV_STEPS_SAMPLED_LIFETIME),
+            total_module_steps,
+            reduce="sum",
+            with_throughput=True,
+        )
 
     @override(Runner)
     def set_device(self):
