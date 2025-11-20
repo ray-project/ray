096050cdec1e95ec3e4a97edc1bef5ae14481b9d
iamjustinhsu <jhsu@anyscale.com>
2025-11-20 09:48:09 -0800

    fix
    Signed-off-by: iamjustinhsu <jhsu@anyscale.com>

--- python/ray/data/_internal/planner/repartition.py
+++ python/ray/data/_internal/planner/repartition.py
@@ -79,7 +79,7 @@ def generate_repartition_fn(
             target_shuffle_max_block_size=(
                 ctx.target_max_block_size_override or data_context.target_max_block_size
             ),
-            random_shuffle=False,
+            random_shuffle=random_permute,
         )
         scheduler = SplitRepartitionTaskScheduler(shuffle_spec)
         return scheduler.execute(refs, num_outputs, ctx)
