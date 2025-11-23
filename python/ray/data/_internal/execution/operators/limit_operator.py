cac1505490fccad0c94e33539c09eb6d9393b11a
Simeet Nayan <simeetnayan.8100@gmail.com>
2025-11-23 21:37:27 +0530

    Rename _execution_finished property access to _is_execution_marked_finished in limit_operator
    Signed-off-by: Simeet Nayan <simeetnayan.8100@gmail.com>

--- python/ray/data/_internal/execution/operators/limit_operator.py
+++ python/ray/data/_internal/execution/operators/limit_operator.py
@@ -117,7 +117,7 @@ class LimitOperator(OneToOneOperator):
     def num_outputs_total(self) -> Optional[int]:
         # Before execution is completed, we don't know how many output
         # bundles we will have. We estimate based off the consumption so far.
-        if self._execution_finished:
+        if self._is_execution_marked_finished:
             return self._cur_output_bundles
         return self._estimated_num_output_bundles
 
