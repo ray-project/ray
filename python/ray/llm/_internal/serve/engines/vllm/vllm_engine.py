cc125f6b1fa2a2b5d48c88584c36ef80ab6af1f6
Qidong Su <qidongs@nvidia.com>
2025-11-24 20:06:04 +0000

    fmt
    Signed-off-by: Qidong Su <qidongs@nvidia.com>

--- python/ray/llm/_internal/serve/engines/vllm/vllm_engine.py
+++ python/ray/llm/_internal/serve/engines/vllm/vllm_engine.py
@@ -1,7 +1,7 @@
 import argparse
+import inspect
 import os
 from typing import TYPE_CHECKING, AsyncGenerator, Optional, Tuple, Union
-import inspect
 
 from starlette.datastructures import State
 from starlette.requests import Request
