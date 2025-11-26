e3a8274f086463d8876dee72655c7895f1d1192f
Balaji Veeramani <bveeramani@berkeley.edu>
2025-11-25 23:14:45 -0800

    Fix bug
    Signed-off-by: Balaji Veeramani <bveeramani@berkeley.edu>

--- python/ray/data/_internal/execution/operators/map_operator.py
+++ python/ray/data/_internal/execution/operators/map_operator.py
@@ -213,6 +213,20 @@ class MapOperator(InternalQueueOperatorMixin, OneToOneOperator, ABC):
             name += f"->SplitBlocks({self._additional_split_factor})"
         return name
 
+    def clear_internal_input_queue(self) -> None:
+        """Clear internal input queue (block ref bundler)."""
+        self._block_ref_bundler.done_adding_bundles()
+        while self._block_ref_bundler.has_bundle():
+            (input_bundles, _) = self._block_ref_bundler.get_next_bundle()
+            for input_bundle in input_bundles:
+                self._metrics.on_input_dequeued(input_bundle)
+
+    def clear_internal_output_queue(self) -> None:
+        """Clear internal output queue."""
+        while self._output_queue.has_next():
+            bundle = self._output_queue.get_next()
+            self._metrics.on_output_dequeued(bundle)
+
     @classmethod
     def create(
         cls,
