c95c9e2e4e6c13f60652b6094bad5a26ab5513cf
Edward Oakes <ed.nmi.oakes@gmail.com>
2025-12-01 08:30:53 -0600

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/exceptions.py
+++ python/ray/exceptions.py
@@ -987,6 +987,7 @@ class ActorAlreadyExistsError(ValueError, RayError):
     """
 
     def __init__(self, error_message: str):
+        super().__init__(error_message)
         self.error_message = error_message
 
     def __str__(self):
