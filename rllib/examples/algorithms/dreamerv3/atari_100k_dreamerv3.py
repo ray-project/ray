141ae42dcc98576b44c55c9686618416adbe80a3
Mark Towers <mark@anyscale.com>
2025-12-03 16:35:54 -0800

    Update the remaining files
    Signed-off-by: Mark Towers <mark@anyscale.com>

--- rllib/examples/algorithms/dreamerv3/atari_100k_dreamerv3.py
+++ rllib/examples/algorithms/dreamerv3/atari_100k_dreamerv3.py
@@ -19,7 +19,7 @@ import gymnasium as gym
 from ray import tune
 from ray.rllib.algorithms.dreamerv3.dreamerv3 import DreamerV3Config
 from ray.rllib.env.wrappers.atari_wrappers import wrap_atari_for_new_api_stack
-from ray.rllib.utils.test_utils import (
+from ray.rllib.examples.utils import (
     add_rllib_example_script_args,
     run_rllib_example_script_experiment,
 )
