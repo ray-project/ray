0ac89ad77c7c665387b62cafc6a5255aca462b62
Artur Niederfahrenhorst <attaismyname@googlemail.com>
2025-12-05 19:46:33 +0100

    fix offline prelearner test

--- rllib/algorithms/algorithm.py
+++ rllib/algorithms/algorithm.py
@@ -3890,12 +3890,11 @@ class Algorithm(Checkpointable, Trainable):
 
             # Return dict (shallow copy of `train_results`).
             results: ResultDict = train_results.copy()
-            if results.get(NUM_ENV_STEPS_SAMPLED_LIFETIME) is None:
-                # Backward compatibility `NUM_ENV_STEPS_SAMPLED_LIFETIME` is now:
-                # `ENV_RUNNER_RESULTS/NUM_ENV_STEPS_SAMPLED_LIFETIME`.
-                results[NUM_ENV_STEPS_SAMPLED_LIFETIME] = results.get(
-                    ENV_RUNNER_RESULTS, {}
-                ).get(NUM_ENV_STEPS_SAMPLED_LIFETIME, 0)
+            # Backward compatibility `NUM_ENV_STEPS_SAMPLED_LIFETIME` is now:
+            # `ENV_RUNNER_RESULTS/NUM_ENV_STEPS_SAMPLED_LIFETIME`.
+            results[NUM_ENV_STEPS_SAMPLED_LIFETIME] = results.get(
+                ENV_RUNNER_RESULTS, {}
+            ).get(NUM_ENV_STEPS_SAMPLED_LIFETIME, 0)
 
             # Evaluation results.
             if eval_results:
