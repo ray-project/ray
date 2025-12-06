42e1f85e41699f8c760af1326d651cc80b680159
dayshah <dhyey2019@gmail.com>
2025-12-06 02:33:08 +0000

    [core][rdt] Rework passing transport through for bring your own transport
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- python/ray/experimental/gpu_object_manager/nixl_tensor_transport.py
+++ python/ray/experimental/gpu_object_manager/nixl_tensor_transport.py
@@ -47,7 +47,7 @@ class NixlTensorTransport(TensorTransportManager):
 
     @property
     def tensor_transport_backend(self) -> str:
-        return "nixl"
+        return "NIXL"
 
     @staticmethod
     def is_one_sided() -> bool:
@@ -79,6 +79,7 @@ class NixlTensorTransport(TensorTransportManager):
 
     def actor_has_tensor_transport(self, actor: "ray.actor.ActorHandle") -> bool:
         # TODO(dayshah): This is called on a .remote RDT call, so it's quite expensive.
+        print("checking nixl tensor transport")
 
         def __ray_actor_has_tensor_transport__(
             self: "ray.actor.ActorHandle",
@@ -89,9 +90,11 @@ class NixlTensorTransport(TensorTransportManager):
                     get_tensor_transport_manager,
                 )
 
-                get_tensor_transport_manager("nixl").get_nixl_agent()
+                get_tensor_transport_manager("NIXL").get_nixl_agent()
+                print("nixl found")
                 return True
             except Exception:
+                print("nixl not found")
                 return False
 
         return ray.get(
