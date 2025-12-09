ea424685ac0fc99c61a7ab945d96588a663bda5a
Rushikesh Adhav <adhavrushikesh6@gmail.com>
2025-12-09 22:25:48 +0530

    fix: use DefaultFileMetadataProvider
    Signed-off-by: Rushikesh Adhav <adhavrushikesh6@gmail.com>

--- python/ray/data/tests/test_json.py
+++ python/ray/data/tests/test_json.py
@@ -21,6 +21,9 @@ from ray.data.datasource import (
 from ray.data.datasource.file_based_datasource import (
     FILE_SIZE_FETCH_PARALLELIZATION_THRESHOLD,
 )
+from ray.data.datasource.file_meta_provider import (
+    DefaultFileMetadataProvider,
+)
 from ray.data.tests.conftest import *  # noqa
 from ray.tests.conftest import *  # noqa
 
@@ -211,6 +214,15 @@ def test_json_read_meta_provider(
     df1 = pd.DataFrame({"one": [1, 2, 3], "two": ["a", "b", "c"]})
     path1 = os.path.join(tmp_path, "test1.json")
     df1.to_json(path1, orient="records", lines=True)
+    ds = ray.data.read_json(
+        path1,
+        meta_provider=DefaultFileMetadataProvider(),
+    )
+
+    # Expect to lazily compute all metadata correctly.
+    assert ds.count() == 3
+    assert ds.input_files() == [path1]
+    assert ds.schema() == Schema(pa.schema([("one", pa.int64()), ("two", pa.string())]))
 
     with pytest.raises(NotImplementedError):
         ray.data.read_json(
