70a90edacb02060dd26fb851584e9613c3e62408
Balaji Veeramani <bveeramani@berkeley.edu>
2025-12-10 10:22:50 -0800

    Update release test
    Signed-off-by: Balaji Veeramani <bveeramani@berkeley.edu>

--- release/nightly_tests/dataset/autoscaling/does_not_over_provision.py
+++ release/nightly_tests/dataset/autoscaling/does_not_over_provision.py
@@ -11,17 +11,11 @@ def main():
     ray.init()
 
     def sleep_task(row):
-        # Only sleep for the first task so that some tasks finish.
-        if row["id"] == 0:
-            # By sleeping for 5 mins, we're able to test whether the autoscaler scales
-            # up unnecessarily. This pipeline can use at most 2 CPUs, so the peak num of
-            # CPUs should be at most 8 CPUs (1 node).
-            time.sleep(300)
-
+        time.sleep(1)
         return row
 
     with ClusterResourceMonitor() as monitor:
-        ray.data.range(2, override_num_blocks=2).map(sleep_task).materialize()
+        ray.data.range(300, override_num_blocks=300).map(sleep_task).materialize()
 
         peak_resources = monitor.get_peak_cluster_resources()
         assert peak_resources.cpu == 8, f"Expected 8 CPUs, got {peak_resources.cpu}"
