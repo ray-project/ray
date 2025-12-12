8318c29e8dd31ef5d5af944e88430dd7d34c35f8
yicheng <yicheng@anyscale.com>
2025-12-12 18:08:06 +0000

    fix node_manager_test
    Signed-off-by: yicheng <yicheng@anyscale.com>

--- src/ray/raylet/tests/node_manager_test.cc
+++ src/ray/raylet/tests/node_manager_test.cc
@@ -318,6 +318,12 @@ class NodeManagerTest : public ::testing::Test {
     node_manager_config.store_socket_name = "test_store_socket";
     node_manager_config.resource_config = ResourceSet(
         absl::flat_hash_map<std::string, double>{{"CPU", kTestTotalCpuResource}});
+    // Set non-zero ports to skip waiting for agents reporting ports back.
+    // we don't actually start the agents in this test.
+    node_manager_config.metrics_agent_port = 44386;
+    node_manager_config.metrics_export_port = 63802;
+    node_manager_config.dashboard_agent_listen_port = 52365;
+    node_manager_config.runtime_env_agent_port = 37429;
 
     core_worker_subscriber_ = std::make_unique<pubsub::FakeSubscriber>();
     mock_object_directory_ = std::make_unique<MockObjectDirectory>();
