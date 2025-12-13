c9d2a2151dd5121c3f24f77bf4f2167888717c0c
elliot-barn <elliot.barnwell@anyscale.com>
2025-12-13 02:04:24 +0000

    adding test for parser
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/raydepsets/parser.py
+++ ci/raydepsets/parser.py
@@ -13,39 +13,25 @@ class Dep:
 class Parser:
     def __init__(self, depset_path: str):
         self.depset_path = depset_path
-        # self.depset_path = "ci/raydepsets/tests/test_data/test_lock_file.lock"
 
     def parse(self) -> List[Dep]:
         deps = []
         with open(self.depset_path, "r") as f:
+            deps_arr = []
+            name = None
+            version = None
             for line in f.readlines():
                 package_line_match = re.findall(
                     r"([A-Za-z0-9_\-]+)==([A-Za-z0-9\.\-]+)", line
                 )
-                dependency_line_match = re.findall(r"(#\s{3}(.*)$)", line)
-                if package_line_match:
-                    print("package line match: ", package_line_match[0])
-                    name, version = package_line_match[0]
-                if dependency_line_match:
-                    print("dependency line match: ", dependency_line_match[0])
-                    deps.append(
-                        Dep(
-                            name=name,
-                            version=version,
-                            dependencies=dependency_line_match[0],
-                        )
-                    )
+                if len(deps_arr) > 0 and package_line_match:
+                    deps.append(Dep(name=name, version=version, dependencies=deps_arr))
+                    deps_arr = []
 
-        # matches = re.findall(r"([A-Za-z0-9_\-]+)==([A-Za-z0-9\.\-]+)", data)
-        # # need to handle dependencies
-        # if matches is None:
-        #     raise ValueError("No matches found")
-        # for name, version in matches:
-        #     deps.append(Dep(name=name, version=version, dependencies=[]))
+                dependency_line_match = re.search(r"^\s*#\s+(.*)$", line)
+                if package_line_match:
+                    name, version = package_line_match[0]
+                elif dependency_line_match and dependency_line_match.group(1) != "via":
+                    deps_arr.append(dependency_line_match.group(1).replace("via ", ""))
 
         return deps
-
-
-if __name__ == "__main__":
-    parser = Parser("ci/raydepsets/tests/test_data/test_lock_file.lock")
-    print(parser.parse())
---
+++ ci/raydepsets/tests/test_parser.py
@@ -0,0 +1,23 @@
+import sys
+import tempfile
+from pathlib import Path
+
+import pytest
+
+from ci.raydepsets.parser import Parser
+from ci.raydepsets.tests.utils import copy_data_to_tmpdir
+
+
+def test_parser():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        copy_data_to_tmpdir(tmpdir)
+        parser = Parser(Path(tmpdir) / "test_python_depset.lock")
+        parsed_deps = parser.parse()
+        assert len(parsed_deps) == 10
+        assert parsed_deps[0].name == "aiohappyeyeballs"
+        assert parsed_deps[0].version == "2.6.1"
+        assert parsed_deps[0].dependencies == ["aiohttp"]
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main(["-v", __file__]))
