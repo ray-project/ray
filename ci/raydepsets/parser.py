6cc2f650e24ef731096d052ce4f7092a1b0812d1
elliot-barn <elliot.barnwell@anyscale.com>
2025-12-16 19:31:40 +0000

    updating parser
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/raydepsets/parser.py
+++ ci/raydepsets/parser.py
@@ -7,7 +7,7 @@ from typing import List
 class Dep:
     name: str
     version: str
-    dependencies: List[str]
+    required_by: List[str]
 
 
 class Parser:
@@ -24,14 +24,29 @@ class Parser:
                 package_line_match = re.findall(
                     r"([A-Za-z0-9_\-]+)==([A-Za-z0-9\.\-]+)", line
                 )
-                if len(deps_arr) > 0 and package_line_match:
-                    deps.append(Dep(name=name, version=version, dependencies=deps_arr))
+                if name and version and package_line_match:
+                    deps.append(Dep(name=name, version=version, required_by=deps_arr))
                     deps_arr = []
-
-                dependency_line_match = re.search(r"^\s*#\s+(.*)$", line)
+                dependency_line_match = re.search(
+                    r"^\s{4}#\s{3}(.*)$|^\s{4}#\svia\s(.*)$", line
+                )
+                # ignore requirements and constraints
+                req_line_match = re.search(r"-r\s+(.*)$", line)
+                constraint_line_match = re.search(r"-c\s+(.*)$", line)
                 if package_line_match:
                     name, version = package_line_match[0]
-                elif dependency_line_match and dependency_line_match.group(1) != "via":
-                    deps_arr.append(dependency_line_match.group(1).replace("via ", ""))
-
+                elif (
+                    dependency_line_match
+                    and not req_line_match
+                    and not constraint_line_match
+                    and dependency_line_match.group(1) != ""
+                    and dependency_line_match.group(2) != ""
+                ):
+                    # match either "# via <package>" or "#   <package>"
+                    dep = dependency_line_match.group(1) or dependency_line_match.group(
+                        2
+                    )
+                    deps_arr.append(dep)
+            # handle last dependency
+            deps.append(Dep(name=name, version=version, required_by=deps_arr))
         return deps
