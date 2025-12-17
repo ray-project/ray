0922e0ef73c2ec432e0ac98ac4337b5e9c9387f4
Christina Zhu <christina@anyscale.com>
2025-12-16 17:21:23 -0800

    Removed commented out code
    Signed-off-by: Christina Zhu <christina@anyscale.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -574,10 +574,6 @@ def setup(app):
     app.add_js_file("js/csat.js", defer="defer")
     app.add_css_file("css/csat.css")
 
-    # Disabled: Custom "Ask AI" widget (replaced with runLLM widget)
-    # app.add_js_file("js/assistant.js", defer="defer")
-    # app.add_css_file("css/assistant.css")
-
     app.add_js_file("js/dismissable-banner.js", defer="defer")
     app.add_css_file("css/dismissable-banner.css")
 
