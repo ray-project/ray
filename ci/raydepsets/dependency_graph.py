ac8520f5d924e763daa66c9db6fb54e21745c27a
elliot-barn <elliot.barnwell@anyscale.com>
2025-12-18 14:37:42 -0800

    adding doc strings
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/raydepsets/dependency_graph.py
+++ ci/raydepsets/dependency_graph.py
@@ -13,6 +13,8 @@ class Dep:
 
 
 class DependencyGraph:
+    """Manages a dependency graph of Python packages."""
+
     def __init__(self):
         self.graph = DiGraph()
 
@@ -28,7 +30,6 @@ class DependencyGraph:
             for dependent in dep.required_by:
                 if not self.graph.has_node(dependent):
                     raise ValueError(f"Dependency {dependent} not found in deps")
-                # Edge: dependent -> dep (the package that requires points to what it requires)
                 self.graph.add_edge(dependent, dep.name)
 
     def build_dependency_graph(self, deps: List[Dep]):
@@ -36,13 +37,14 @@ class DependencyGraph:
         self.add_edges(deps)
 
     def remove_dropped_dependencies(self, dropped_dependencies: List[str]):
+        """Remove dependencies and their children from the graph"""
         for dep in dropped_dependencies:
             if not self.graph.has_node(dep):
                 raise ValueError(f"Dependency {dep} not found in deps")
 
         to_remove = set(dropped_dependencies)
 
-        # Cascade: find nodes that should be removed because nothing else requires them
+        # Find nodes that should be removed because nothing else requires them
         # A node should be removed if ALL its predecessors (dependents) are being removed
         changed = True
         while changed:
@@ -50,7 +52,7 @@ class DependencyGraph:
             for node in list(self.graph.nodes()):
                 if node in to_remove:
                     continue
-                # Get all packages that require this node (predecessors point TO this node)
+                # Get all packages that require this node
                 dependents = set(self.graph.predecessors(node))
                 # If this node has dependents and ALL of them are being removed,
                 # then this node is no longer needed
