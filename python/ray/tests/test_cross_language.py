f9bab8918fd20ed7df7aaff673b8c016e09f4950
JianZhang <keepromise@apache.org>
2025-12-22 11:22:43 +0800

    fix ut
    Signed-off-by: JianZhang <keepromise@apache.org>

--- python/ray/tests/test_cross_language.py
+++ python/ray/tests/test_cross_language.py
@@ -15,6 +15,12 @@ def test_cross_language_raise_kwargs(shutdown_only):
     with pytest.raises(Exception, match="kwargs"):
         ray.cross_language.java_actor_class("a").remote(x="arg1")
 
+    with pytest.raises(Exception, match="kwargs"):
+        ray.cross_language.cpp_function("a").remote(x="arg1")
+
+    with pytest.raises(Exception, match="kwargs"):
+        ray.cross_language.cpp_actor_class("a", "b").remote(x="arg1")
+
 
 def test_cross_language_raise_exception(shutdown_only):
     ray.init(job_config=ray.job_config.JobConfig(code_search_path=sys.path))
@@ -25,23 +31,30 @@ def test_cross_language_raise_exception(shutdown_only):
     with pytest.raises(Exception, match="transfer"):
         ray.cross_language.java_function("a", "b").remote(PythonObject())
 
+    with pytest.raises(Exception, match="transfer"):
+        ray.cross_language.cpp_function("a").remote(PythonObject())
 
-def test_cpp_descriptor_resolves_python_actor_method(shutdown_only):
+
+def test_function_id(shutdown_only):
     ray.init(job_config=ray.job_config.JobConfig(code_search_path=sys.path))
 
-    @ray.remote
-    class SampleActor:
-        def check_descriptor(self):
-            worker = ray._private.worker.global_worker
-            fm = worker.function_actor_manager
-            fd = ray._raylet.CppFunctionDescriptor(
-                "check_descriptor", "PYTHON", self.__class__.__qualname__
-            )
-            info = fm.get_execution_info(worker.current_job_id, fd)
-            return info.function_name
+    cpp_func = ray.cross_language.cpp_function("TestFunctionID")
+    func_id = cpp_func._function_descriptor.function_id
+    print(func_id)
+    assert func_id is not None
 
-    actor = SampleActor.remote()
-    assert ray.get(actor.check_descriptor.remote()) == "check_descriptor"
+
+def test_actor_function_id(shutdown_only):
+    ray.init(job_config=ray.job_config.JobConfig(code_search_path=sys.path))
+
+    cpp_actor_class = ray.cross_language.cpp_actor_class(
+        "TestActorFunction", "TestActor"
+    )
+    func_id = (
+        cpp_actor_class.__ray_metadata__.actor_creation_function_descriptor.function_id
+    )
+    print(func_id)
+    assert func_id is not None
 
 
 if __name__ == "__main__":
