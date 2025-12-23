650c78434a24c578f29c2c42d8a41fe8cf3a77f7
xiaowen.wxw <wxw403883@alibaba-inc.com>
2025-12-23 16:20:49 +0800

    add ut Signed-off-by: xiaowen.wxw <wxw403883@alibaba-inc.com>

--- python/ray/_common/filters.py
+++ python/ray/_common/filters.py
@@ -50,6 +50,6 @@ class CoreContextFilter(logging.Filter):
     def filter(self, record):
         context = self.get_ray_core_logging_context()
         for key, value in context.items():
-            if value:
+            if value is not None and value != "":
                 setattr(record, key, value)
         return True
