f3f358031070bda9cb93d26721044eda18bea9bf
Kit Lee <7000003+wingkitlee0@users.noreply.github.com>
2025-12-24 16:35:14 -0500

    [Doc] Improve docs for ray.data.Datasource
    Signed-off-by: Kit Lee <7000003+wingkitlee0@users.noreply.github.com>

--- python/ray/data/datasource/datasource.py
+++ python/ray/data/datasource/datasource.py
@@ -225,7 +225,21 @@ class _DatasourcePredicatePushdownMixin:
 class Datasource(_DatasourceProjectionPushdownMixin, _DatasourcePredicatePushdownMixin):
     """Interface for defining a custom :class:`~ray.data.Dataset` datasource.
 
+    User may subclass this class to implement a custom datasource. The subclass should
+    implement :meth:`.get_read_tasks` and
+    :meth:`.estimate_inmemory_data_size` to read the data and estimate the in-memory data size, respectively.
+
     To read a datasource into a dataset, use :meth:`~ray.data.read_datasource`.
+
+    Example:
+        >>> class MyDatasource(Datasource): # doctest: +SKIP
+        ...     def get_read_tasks(self, parallelism: int, **read_args) -> List["ReadTask"]: # doctest: +SKIP
+        ...         return [ReadTask(lambda: [pa.Table.from_pydict({"data": range(10)})], BlockMetadata(10, 80, None, None))] # doctest: +SKIP
+        ...     def estimate_inmemory_data_size(self) -> Optional[int]: # doctest: +SKIP
+        ...         return 80
+        >>> ds = MyDatasource() # doctest: +SKIP
+        >>> tasks = ds.get_read_tasks(parallelism=1) # doctest: +SKIP
+        >>> assert len(tasks) == 1 and tasks[0].metadata.num_rows == 10 # doctest: +SKIP
     """  # noqa: E501
 
     def __init__(self):
@@ -286,6 +300,8 @@ class Datasource(_DatasourceProjectionPushdownMixin, _DatasourcePredicatePushdow
 
     @property
     def should_create_reader(self) -> bool:
+        """Return True if the datasource should create a legacy reader"""
+
         has_implemented_get_read_tasks = (
             type(self).get_read_tasks is not Datasource.get_read_tasks
         )
@@ -293,9 +309,10 @@ class Datasource(_DatasourceProjectionPushdownMixin, _DatasourcePredicatePushdow
             type(self).estimate_inmemory_data_size
             is not Datasource.estimate_inmemory_data_size
         )
-        return (
-            not has_implemented_get_read_tasks
-            or not has_implemented_estimate_inmemory_data_size
+        # False when both get_read_tasks and estimate_inmemory_data_size are implemented
+        return not (
+            has_implemented_get_read_tasks
+            and has_implemented_estimate_inmemory_data_size
         )
 
     @property
