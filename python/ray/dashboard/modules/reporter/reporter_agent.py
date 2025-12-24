45fa1a2f30d49aed4e09438af66cc3e0c29db133
zac <zac@anyscale.com>
2025-12-24 17:05:17 +0000

    psutil._common.sdiskusage is deprecated in newer versions (namely, the one in python 3.14). And it's marked as an internal api.  So pulled it out to it's own variable so keep version compatibility.
    Signed-off-by: zac <zac@anyscale.com>

--- python/ray/dashboard/modules/reporter/reporter_agent.py
+++ python/ray/dashboard/modules/reporter/reporter_agent.py
@@ -6,7 +6,7 @@ import os
 import socket
 import sys
 import traceback
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from concurrent.futures import ThreadPoolExecutor
 from typing import List, Optional, Tuple
 
@@ -71,6 +71,10 @@ from ray.exceptions import (
 )
 
 import psutil
+
+# Define sdiskusage locally to avoid relying on psutil's private API
+# (psutil._common.sdiskusage may not exist in all versions)
+sdiskusage = namedtuple("sdiskusage", ["total", "used", "free", "percent"])
 
 logger = logging.getLogger(__name__)
 
@@ -869,9 +873,7 @@ class ReporterAgent(
     def _get_disk_usage():
         if IN_KUBERNETES_POD and not ENABLE_K8S_DISK_USAGE:
             # If in a K8s pod, disable disk display by passing in dummy values.
-            return {
-                "/": psutil._common.sdiskusage(total=1, used=0, free=1, percent=0.0)
-            }
+            return {"/": sdiskusage(total=1, used=0, free=1, percent=0.0)}
         if sys.platform == "win32":
             root = psutil.disk_partitions()[0].mountpoint
         else:
