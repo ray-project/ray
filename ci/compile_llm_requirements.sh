292242c4360e285b1671aaa31609f192b335e22f
abrar <abrar@anyscale.com>
2026-01-05 22:23:05 +0000

    use bazel
    Signed-off-by: abrar <abrar@anyscale.com>

--- ci/compile_llm_requirements.sh
+++ ci/compile_llm_requirements.sh
@@ -11,6 +11,6 @@ cp python/requirements_compiled.txt /tmp/ray-deps/requirements_compiled.txt
 sed -e '/^--extra-index-url /d' -e '/^--find-links /d' /tmp/ray-deps/requirements_compiled.txt > /tmp/ray-deps/requirements_compiled.txt.tmp
 mv /tmp/ray-deps/requirements_compiled.txt.tmp /tmp/ray-deps/requirements_compiled.txt
 
-bazelisk run //ci/raydepsets:raydepsets -- build "${CONFIG_PATH}"
+bazel run //ci/raydepsets:raydepsets -- build "${CONFIG_PATH}"
 
 echo "--- Done"
