285e9d70877e7177fb3f3c3c81095347d91ae9c3
Tom Liu (LIU HSIU-CHI) <aa123593465@gmail.com>
2026-01-06 15:11:24 +0800

    [Core] Fix the conflict with latest master branch
    Signed-off-by: Tom Liu (LIU HSIU-CHI) <aa123593465@gmail.com>

--- python/ray/_private/test_utils.py
+++ python/ray/_private/test_utils.py
@@ -39,7 +39,8 @@ from ray._private import (
 from ray._private.internal_api import memory_summary
 from ray._private.tls_utils import generate_self_signed_tls_certs
 from ray._private.worker import RayContext
-from ray._raylet import Config, GcsClientOptions, GlobalStateAccessor
+import ray.dashboard.consts as dashboard_consts
+from ray._raylet import Config, GcsClient, GcsClientOptions, GlobalStateAccessor
 from ray.core.generated import (
     gcs_pb2,
     gcs_service_pb2,
@@ -398,16 +399,23 @@ def check_call_ray(args, capture_stdout=False, capture_stderr=False):
 
 
 def get_dashboard_agent_address(gcs_client: GcsClient, node_id: str):
-    result = gcs_client.internal_kv_get(
-        f"{dashboard_consts.DASHBOARD_AGENT_ADDR_NODE_ID_PREFIX}{node_id}".encode(),
-        namespace=ray_constants.KV_NAMESPACE_DASHBOARD,
+    """Get dashboard agent address from GcsNodeInfo.
+
+    Returns the agent address string in format "{ip}:{grpc_port}" or None if not found.
+    """
+    from ray import NodeID
+
+    node_id_obj = NodeID.from_hex(node_id)
+    node_info_dict = gcs_client.get_all_node_info(
         timeout=dashboard_consts.GCS_RPC_TIMEOUT_SECONDS,
+        state_filter=gcs_pb2.GcsNodeInfo.GcsNodeState.ALIVE,
     )
-    if result:
-        # Returns [ip, http_port, grpc_port]
-        ip, _, grpc_port = json.loads(result)
-        return f"{ip}:{grpc_port}"
-    return None
+    if node_id_obj not in node_info_dict:
+        return None
+    node_info = node_info_dict[node_id_obj]
+    if node_info.metrics_agent_port <= 0:
+        return None
+    return f"{node_info.node_manager_address}:{node_info.metrics_agent_port}"
 
 
 def wait_for_dashboard_agent_available(cluster):
