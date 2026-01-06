9545f208975fd2c21628cd37f0f3283327179011
Balaji Veeramani <bveeramani@berkeley.edu>
2026-01-06 00:02:04 -0800

    Initial commit
    Signed-off-by: Balaji Veeramani <bveeramani@berkeley.edu>

--- python/ray/data/tests/test_autoscaler.py
+++ python/ray/data/tests/test_autoscaler.py
@@ -67,7 +67,7 @@ def test_actor_pool_scaling():
         _inputs_complete=False,
         input_dependencies=[MagicMock()],
         internal_input_queue_num_blocks=MagicMock(return_value=1),
-        metrics=MagicMock(average_num_inputs_per_task=1),
+        metrics=MagicMock(average_num_inputs_per_task=1, num_inputs_received=1),
     )
     op_state = OpState(
         op, inqueues=[MagicMock(__len__=MagicMock(return_value=10), num_blocks=10)]
@@ -215,6 +215,13 @@ def test_actor_pool_scaling():
         assert_autoscaling_action(
             delta=0,
             expected_reason="exceeded resource limits",
+        )
+
+    # Should no-op because the op has no budget.
+    with patch(op.metrics, "num_inputs_received", 0, is_method=False):
+        assert_autoscaling_action(
+            delta=0,
+            expected_reason="no inputs received",
         )
 
 
