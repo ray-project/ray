aff471db6e0ff591ba4fd805cfdbfe82495e37e3
elliot-barn <elliot.barnwell@anyscale.com>
2026-01-07 21:15:54 +0000

    updating dependency sets
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/raydepsets/pre_hooks/remove-compiled-headers.sh
+++ ci/raydepsets/pre_hooks/remove-compiled-headers.sh
@@ -2,9 +2,11 @@
 
 set -euo pipefail
 
+PYTHON_VERSION=${1:-3.10}
+
 mkdir -p /tmp/ray-deps
 
 # Remove the GPU constraints
-cp python/requirements_compiled.txt /tmp/ray-deps/requirements_compiled.txt
-sed -e '/^--extra-index-url /d' -e '/^--find-links /d' /tmp/ray-deps/requirements_compiled.txt > /tmp/ray-deps/requirements_compiled.txt.tmp
-mv /tmp/ray-deps/requirements_compiled.txt.tmp /tmp/ray-deps/requirements_compiled.txt
+cp python/requirements_compiled_py"${PYTHON_VERSION}".txt /tmp/ray-deps/requirements_compiled_py"${PYTHON_VERSION}".txt
+sed -e '/^--extra-index-url /d' -e '/^--find-links /d' /tmp/ray-deps/requirements_compiled_py"${PYTHON_VERSION}".txt > /tmp/ray-deps/requirements_compiled_py"${PYTHON_VERSION}".txt.tmp
+mv /tmp/ray-deps/requirements_compiled_py"${PYTHON_VERSION}".txt.tmp /tmp/ray-deps/requirements_compiled_py"${PYTHON_VERSION}".txt
