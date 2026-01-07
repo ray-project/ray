43a2ba21b7c9b7b5c9791df8c336f0cbb834c728
Damodar Yekkuluri <damodar3sachin@gmail.com>
2026-01-07 15:33:39 +0530

    minor - Fix formatting
    Signed-off-by: Damodar Yekkuluri <damodar3sachin@gmail.com>

--- python/ray/serve/deployment.py
+++ python/ray/serve/deployment.py
@@ -73,10 +73,10 @@ class Deployment:
 
     User-defined Methods:
         check_health():
-            [Optional] Define this method to implement custom application-level 
-            health checks. Ray Serve calls this periodically (default 10s). 
-            If it raises an exception, the replica is marked as UNHEALTHY 
-            and restarted. Useful for detecting internal engine crashes 
+            [Optional] Define this method to implement custom application-level
+            health checks. Ray Serve calls this periodically (default 10s).
+            If it raises an exception, the replica is marked as UNHEALTHY
+            and restarted. Useful for detecting internal engine crashes
             (like vLLM AsyncEngineDeadError) that leave the actor process alive.
 
     Example:
