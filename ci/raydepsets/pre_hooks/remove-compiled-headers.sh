9d2b911577e9dec9bc44121fb4a7b47bc4697c41
elliot-barn <elliot.barnwell@anyscale.com>
2026-01-07 22:36:46 +0000

    recompiling depsets
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/raydepsets/pre_hooks/remove-compiled-headers.sh
+++ ci/raydepsets/pre_hooks/remove-compiled-headers.sh
@@ -2,11 +2,17 @@
 
 set -euo pipefail
 
-PYTHON_VERSION=${1:-3.10}
+PYTHON_VERSION=${1:-}
+
+if [[ -z "${PYTHON_VERSION}" ]]; then
+    FILENAME="requirements_compiled.txt"
+else
+    FILENAME="requirements_compiled_py${PYTHON_VERSION}.txt"
+fi
 
 mkdir -p /tmp/ray-deps
 
 # Remove the GPU constraints
-cp python/requirements_compiled_py"${PYTHON_VERSION}".txt /tmp/ray-deps/requirements_compiled_py"${PYTHON_VERSION}".txt
-sed -e '/^--extra-index-url /d' -e '/^--find-links /d' /tmp/ray-deps/requirements_compiled_py"${PYTHON_VERSION}".txt > /tmp/ray-deps/requirements_compiled_py"${PYTHON_VERSION}".txt.tmp
-mv /tmp/ray-deps/requirements_compiled_py"${PYTHON_VERSION}".txt.tmp /tmp/ray-deps/requirements_compiled_py"${PYTHON_VERSION}".txt
+cp "python/${FILENAME}" "/tmp/ray-deps/${FILENAME}"
+sed -e '/^--extra-index-url /d' -e '/^--find-links /d' "/tmp/ray-deps/${FILENAME}" > "/tmp/ray-deps/${FILENAME}.tmp"
+mv "/tmp/ray-deps/${FILENAME}.tmp" "/tmp/ray-deps/${FILENAME}"
