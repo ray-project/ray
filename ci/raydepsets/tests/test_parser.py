162bab709f4001a74391f0cbb84d05605d3f9236
elliot-barn <elliot.barnwell@anyscale.com>
2026-01-08 23:03:33 +0000

    updating unit tests
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/raydepsets/tests/test_parser.py
+++ ci/raydepsets/tests/test_parser.py
@@ -6,24 +6,26 @@ import pytest
 
 from ci.raydepsets.parser import Parser
 from ci.raydepsets.tests.utils import (
+    _REPO_NAME,
+    _runfiles,
     copy_data_to_tmpdir,
     replace_in_file,
-    write_to_file,
 )
 
 
 def test_parser():
-    with tempfile.TemporaryDirectory() as tmpdir:
-        copy_data_to_tmpdir(tmpdir)
-        parser = Parser(Path(tmpdir) / "test_python_depset.lock")
-        parsed_deps = parser.parse()
-        assert len(parsed_deps) == 11
-        assert parsed_deps[0].name == "aiohappyeyeballs"
-        assert parsed_deps[0].version == "2.6.1"
-        assert parsed_deps[0].required_by == ["aiohttp"]
-        assert parsed_deps[1].name == "aiohttp"
-        assert parsed_deps[1].version == "3.11.16"
-        assert parsed_deps[1].required_by == []
+    lock_file_path = _runfiles.Rlocation(
+        f"{_REPO_NAME}/ci/raydepsets/tests/test_data/test_python_depset.lock"
+    )
+    parser = Parser(lock_file_path)
+    parsed_deps = parser.parse()
+    assert len(parsed_deps) == 11
+    assert parsed_deps[0].name == "aiohappyeyeballs"
+    assert parsed_deps[0].version == "2.6.1"
+    assert parsed_deps[0].required_by == ["aiohttp"]
+    assert parsed_deps[1].name == "aiohttp"
+    assert parsed_deps[1].version == "3.11.16"
+    assert parsed_deps[1].required_by == []
 
 
 def test_parser_w_annotations():
@@ -31,8 +33,8 @@ def test_parser_w_annotations():
         copy_data_to_tmpdir(tmpdir)
         replace_in_file(
             Path(tmpdir) / "test_python_depset.lock",
-            "frozenlist==1.8.0 \n",
-            'frozenlist==1.8.0 ; python_version >= "3.11"\n',
+            "frozenlist==1.8.0 \\\n",
+            'frozenlist==1.8.0 ; python_version >= "3.11" \\\n',
         )
         parser = Parser(Path(tmpdir) / "test_python_depset.lock")
         parsed_deps = parser.parse()
@@ -49,8 +51,10 @@ def test_parser_w_annotations():
 
 def test_parser_empty_file():
     with tempfile.TemporaryDirectory() as tmpdir:
-        write_to_file(Path(tmpdir) / "empty.lock", "")
-        parser = Parser(Path(tmpdir) / "empty.lock")
+        filepath = Path(tmpdir) / "empty.lock"
+        with open(filepath, "w") as f:
+            f.write("")
+        parser = Parser(filepath)
         parsed_deps = parser.parse()
         assert len(parsed_deps) == 0
 
