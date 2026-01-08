477a02808600df2a1a53b9f825319e1c151a0c72
abrar <abrar@anyscale.com>
2026-01-08 01:20:21 +0000

    move files
    Signed-off-by: abrar <abrar@anyscale.com>

--- doc/source/serve/tutorials/video-analysis/ci/tests.sh
+++ doc/source/serve/tutorials/video-analysis/ci/tests.sh
@@ -6,7 +6,6 @@ S3_BUCKET=anyscale-example-video-analysis-test-bucket
 
 export S3_BUCKET
 
-python ci/nb2py.py content/video_analysis_pipeline.ipynb content/video_analysis_pipeline.py # convert notebook to py script
-cd content && python video_analysis_pipeline.py  # be sure to use ipython to ensure even non-python cells are executed properly
-rm video_analysis_pipeline.py  # remove the generated script
-cd ..
+python ci/nb2py.py README.ipynb README.py # convert notebook to py script
+python README.py  # be sure to use ipython to ensure even non-python cells are executed properly
+rm README.py  # remove the generated script
