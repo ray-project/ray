Fixed edge case with truncation and added a more direct test for value target calculation
Signed-off-by: Matthew <mweston3@illinois.edu>

rllib/algorithms/ppo/tests/test_ppo_value_bootstrapping.py
@@ -6,8 +6,66 @@ from ray.rllib.connectors.env_to_module import FlattenObservations
 from ray.rllib.core.columns import Columns
 from ray.rllib.policy.sample_batch import DEFAULT_POLICY_ID
 from ray.rllib.utils.metrics import ENV_RUNNER_RESULTS, EPISODE_RETURN_MEAN
-import torch
+from ray.rllib.utils.postprocessing.value_predictions import compute_value_targets
+from ray.rllib.utils.postprocessing.zero_padding import unpad_data_if_necessary
 
+from ray.rllib.env.single_agent_episode import SingleAgentEpisode
+from ray.rllib.connectors.learner import AddOneTsToEpisodesAndTruncate
+
+import torch
+import numpy as np
+
+from ray.rllib.connectors.learner.learner_connector_pipeline import (
+    LearnerConnectorPipeline,
+)
+from ray.rllib.connectors.learner import (
+    AddColumnsFromEpisodesToTrainBatch,
+    BatchIndividualItems,
+    LearnerConnectorPipeline,
+)
+
+def simulate_vt_calculation(vfps, rewards, terminateds, truncateds, gamma, lambda_):
+    # Formatting
+    episodes = []
+    for vfp, r, term, trunc in zip(vfps, rewards, terminateds, truncateds):
+        episodes.append(SingleAgentEpisode(
+            observations=[0]*len(vfp), # Include observation after last action
+            actions=[0]*len(r),
+            rewards=r,
+            terminated=term,
+            truncated=trunc,
+            len_lookback_buffer=0,
+        ))
+    episode_lens = [len(e) for e in episodes]
+    # Call AddOneTsToEpisodesAndTruncate
+    pipe = LearnerConnectorPipeline(connectors=[AddOneTsToEpisodesAndTruncate(), AddColumnsFromEpisodesToTrainBatch(), BatchIndividualItems()])
+    batch = pipe(
+        episodes=episodes,
+        batch={},
+        rl_module=None,
+        explore=False,
+        shared_data={},
+    )
+    # Add the last episode's terminated/truncated flags to `terminateds` and `truncateds`
+    vfps = [v for vfpl in vfps for v in vfpl]
+    # Compute the value targets
+    return compute_value_targets(
+        values=vfps,
+        rewards=unpad_data_if_necessary(
+          episode_lens,
+          np.array(batch[Columns.REWARDS]),
+        ),
+        terminateds=unpad_data_if_necessary(
+          episode_lens,
+          np.array(batch[Columns.TERMINATEDS]),
+        ),
+        truncateds=unpad_data_if_necessary(
+          episode_lens,
+          np.array(batch[Columns.TRUNCATEDS]),
+        ),
+        gamma=gamma,
+        lambda_=lambda_,
+    )
 
 class TestPPO(unittest.TestCase):
     @classmethod
@@ -17,6 +75,31 @@ class TestPPO(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         ray.shutdown()
+        
+    def test_value_computation(self):
+        correct = [0.9405, 1., None,     0.9405, 1., None]
+        two_term = simulate_vt_calculation(
+            [[0.0, 0.95, 0.95], [0.0, 0.95, 0.95]], # Value head outputs
+            [[0.0, 1.0],  [0.0,  1.0]], # Environment rewards
+            [True, True], # Terminated flags
+            [False, False], # Truncated flags
+            gamma=0.99, lambda_=0.0,
+        )
+        for pred, gt in zip(two_term, correct):
+            if (gt is not None):
+                self.assertEqual(pred, gt)
+        # Test case where an episode is truncated (state value should be included)
+        correct = [0.9405, 1., None, 0.9405, 1.9405, None]
+        term_trunc = simulate_vt_calculation(
+            [[0.0, 0.95, 0.95], [0.0, 0.95, 0.95]], # Value head outputs
+            [[0.0, 1.0], [0.0,  1.0]], # Environment rewards
+            [True, False], # Terminated flags
+            [False, True], # Truncated flags
+            gamma=0.99, lambda_=0.0,
+        )
+        for pred, gt in zip(term_trunc, correct):
+            if (gt is not None):
+                self.assertEqual(pred, gt)
 
     def test_ppo_value_bootstrapping(self):
         """Test whether PPO's value bootstrapping works properly."""