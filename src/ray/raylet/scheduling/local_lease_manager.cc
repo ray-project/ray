afd39c877caa529276540efa300ffbfb06c334e0
irabbani <irabbani@anyscale.com>
2026-01-13 22:56:12 +0000

    [core] (4/N) Removing GCS Centralized Scheduling
    Removes the GCS's dependency on LocalLeaseManager.
    Moves the LocalLeaseManager into the raylet/scheduling package.
    The LocalLeaseManager is now private to the raylet.
    
    Signed-off-by: irabbani <irabbani@anyscale.com>

--- src/ray/raylet/scheduling/local_lease_manager.cc
+++ src/ray/raylet/scheduling/local_lease_manager.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "ray/raylet/local_lease_manager.h"
+#include "ray/raylet/scheduling/local_lease_manager.h"
 
 #include <google/protobuf/map.h>
 
