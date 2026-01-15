20d10ddcc151bd7d19e6ede3da04cff44d333ec2
Ibrahim Rabbani <irabbani@anyscale.com>
2026-01-15 11:39:31 -0800

    [core] (1/n) [Removing GCS Centralized Scheduling] Removing the feature flag, GCS, and Raylet code. (#59979)
    This is 1/N in a series of PRs to remove Centralized Actor Scheduling by
    the GCS (introduced in #15943). The feature is off by default and no
    longer in use or supported.
    
    In this PR, I remove the feature flag to turn the feature on and remove
    related code and tests in the GCS and the Raylet.
    
    ---------
    
    Signed-off-by: irabbani <irabbani@anyscale.com>
    Signed-off-by: Ibrahim Rabbani <irabbani@anyscale.com>

--- src/ray/gcs/gcs_actor_manager.cc
+++ src/ray/gcs/gcs_actor_manager.cc
@@ -26,6 +26,7 @@
 #include "ray/common/protobuf_utils.h"
 #include "ray/common/ray_config.h"
 #include "ray/common/task/task_spec.h"
+#include "ray/util/container_util.h"
 #include "ray/util/logging.h"
 #include "ray/util/time.h"
 
@@ -1948,7 +1949,7 @@ const GcsActor *GcsActorManager::GetActor(const ActorID &actor_id) const {
 }
 
 bool GcsActorManager::RemovePendingActor(std::shared_ptr<GcsActor> actor) {
-  const auto &actor_id = actor->GetActorID();
+  const ActorID &actor_id = actor->GetActorID();
   auto pending_it = std::find_if(pending_actors_.begin(),
                                  pending_actors_.end(),
                                  [actor_id](const std::shared_ptr<GcsActor> &this_actor) {
@@ -1960,7 +1961,7 @@ bool GcsActorManager::RemovePendingActor(std::shared_ptr<GcsActor> actor) {
     pending_actors_.erase(pending_it);
     return true;
   }
-  return gcs_actor_scheduler_->CancelInFlightActorScheduling(actor);
+  return false;
 }
 
 void GcsActorManager::RunAndClearActorCreationCallbacks(
@@ -1976,9 +1977,7 @@ void GcsActorManager::RunAndClearActorCreationCallbacks(
   }
 }
 
-size_t GcsActorManager::GetPendingActorsCount() const {
-  return gcs_actor_scheduler_->GetPendingActorsCount() + pending_actors_.size();
-}
+size_t GcsActorManager::GetPendingActorsCount() const { return pending_actors_.size(); }
 
 std::string GcsActorManager::DebugString() const {
   uint64_t num_named_actors = 0;
