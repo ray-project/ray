5c21aa58181fec96ce591dd7a99678329c5699e8
Nikhil Ghosh <nikhil@anyscale.com>
2026-01-16 15:52:16 -0800

    fix typos / grammar
    Signed-off-by: Nikhil Ghosh <nikhil@anyscale.com>

--- python/ray/data/preprocessor.py
+++ python/ray/data/preprocessor.py
@@ -77,7 +77,7 @@ class Preprocessor(abc.ABC):
     def _check_has_fitted_state(self):
         """Checks if the Preprocessor has fitted state.
 
-        This is also used as an indiciation if the Preprocessor has been fit, following
+        This is also used as an indication if the Preprocessor has been fit, following
         convention from Ray versions prior to 2.6.
         This allows preprocessors that have been fit in older versions of Ray to be
         used to transform data in newer versions.
