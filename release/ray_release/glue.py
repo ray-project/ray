70a6f627385d8f34596e74393875dd0ddad3d763
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2026-01-18 08:35:42 -0800

    .
    Signed-off-by: Lonnie Liu <95255098+aslonnie@users.noreply.github.com>

--- release/ray_release/glue.py
+++ release/ray_release/glue.py
@@ -353,17 +353,7 @@ def _fetching_results(
         )
 
     try:
-<<<<<<< HEAD
-        metrics = command_runner.fetch_metrics()
-=======
-        # Logic duplicated in ray_release/command_runner/_anyscale_job_wrapper.py
-        # Timeout is the time the test took divided by 200
-        # (~7 minutes for a 24h test) but no less than 30s
-        # and no more than 900s
-        metrics_timeout = max(30, min((time.time() - start_time_unix) / 200, 900))
-        runner.save_metrics(start_time_unix, timeout=metrics_timeout)
         metrics = runner.fetch_metrics()
->>>>>>> lonnie-260117-jobrunner
     except Exception as e:
         logger.exception(f"Could not fetch metrics for test command: {e}")
         metrics = {}
