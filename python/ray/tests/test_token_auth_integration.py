ffc98260b97b53d79fedbe3b68caad6a3e8b3625
Tom Liu (Liu Hsiu-Chi) <aa123593465@gmail.com>
2026-01-19 13:22:40 +0800

    [Core] Refactor dashboard agent address retrieval in tests and remove unused constants
    Signed-off-by: Tom Liu (Liu Hsiu-Chi) <aa123593465@gmail.com>

--- python/ray/tests/test_token_auth_integration.py
+++ python/ray/tests/test_token_auth_integration.py
@@ -9,7 +9,6 @@ from typing import Optional
 import pytest
 
 import ray
-import ray.dashboard.consts as dashboard_consts
 from ray._common.network_utils import build_address
 from ray._private.test_utils import (
     PrometheusTimeseries,
@@ -712,17 +711,20 @@ def test_opentelemetry_metrics_with_token_auth(setup_cluster_with_token_auth):
 
 def _get_dashboard_agent_address(cluster_info):
     """Get the dashboard agent HTTP address from a running cluster."""
-    import json
+    from ray import NodeID
+    from ray._raylet import GcsClient
 
-    # Get agent address from internal KV
-    node_id = ray.nodes()[0]["NodeID"]
-    key = f"{dashboard_consts.DASHBOARD_AGENT_ADDR_NODE_ID_PREFIX}{node_id}"
-    agent_addr = ray.experimental.internal_kv._internal_kv_get(
-        key, namespace=ray._private.ray_constants.KV_NAMESPACE_DASHBOARD
-    )
-    if agent_addr:
-        ip, http_port, grpc_port = json.loads(agent_addr)
-        return f"http://{ip}:{http_port}"
+    # Get agent address from GcsNodeInfo
+    node_id_hex = ray.nodes()[0]["NodeID"]
+    node_id = NodeID.from_hex(node_id_hex)
+    gcs_client = GcsClient(address=cluster_info["gcs_address"])
+    node_info_dict = gcs_client.get_all_node_info(node_id=node_id)
+    if node_info_dict and node_id in node_info_dict:
+        node_info = node_info_dict[node_id]
+        ip = node_info.node_manager_address
+        http_port = node_info.dashboard_agent_listen_port
+        if http_port > 0:
+            return f"http://{ip}:{http_port}"
     return None
 
 
