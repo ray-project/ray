764577bec9d93ee1c707cef8dfbc9144e36fb964
Hyunoh-Yeo <hyunoh.yeo@gmail.com>
2026-01-23 13:50:18 -0800

    some fixes
    Signed-off-by: Hyunoh-Yeo <hyunoh.yeo@gmail.com>

--- python/ray/data/read_api.py
+++ python/ray/data/read_api.py
@@ -79,7 +79,6 @@ from ray.data._internal.util import (
     ndarray_to_block,
     pandas_df_to_arrow_block,
 )
-from ray.data.arrow_utils import get_pyarrow_version
 from ray.data.block import (
     Block,
     BlockExecStats,
@@ -101,6 +100,7 @@ from ray.data.datasource.file_meta_provider import (
     DefaultFileMetadataProvider,
 )
 from ray.data.datasource.partitioning import Partitioning
+from ray.data.util.arrow_utils import get_pyarrow_version
 from ray.types import ObjectRef
 from ray.util.annotations import DeveloperAPI, PublicAPI
 from ray.util.scheduling_strategies import NodeAffinitySchedulingStrategy
