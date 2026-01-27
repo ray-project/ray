a09b67ca7e6b9aecab8a443f7549648f6bba9e38
ryanaoleary <ryanaoleary@google.com>
2026-01-27 20:29:58 +0000

    run lint
    Signed-off-by: ryanaoleary <ryanaoleary@google.com>

--- src/ray/raylet/scheduling/policy/bundle_scheduling_policy.h
+++ src/ray/raylet/scheduling/policy/bundle_scheduling_policy.h
@@ -54,9 +54,9 @@ class BundleSchedulingPolicy : public IBundleSchedulingPolicy {
   /// Checks if every bundle is individually feasible on at least one node in the cluster.
   ///
   /// \param resource_request_list The list of resource and label constraints for each
-  /// bundle. 
+  /// bundle.
   /// \param candidate_nodes The candidate nodes in the cluster available for
-  /// scheduling. 
+  /// scheduling.
   /// \return True if all bundles are feasible on at least one node, false
   /// otherwise.
   bool IsRequestFeasible(
