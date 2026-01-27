2b19b36039860e50bf3cf39ea7f01cd7cbf940e1
Alexey Kudinkin <ak@anyscale.com>
2026-01-27 14:58:59 -0800

    Revisited `ReorderingBundleQueue` to implement correct semantic
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/data/_internal/execution/bundle_queue/ordered.py
+++ python/ray/data/_internal/execution/bundle_queue/ordered.py
@@ -12,30 +12,30 @@ if TYPE_CHECKING:
 
 
 class ReorderingBundleQueue(BaseBundleQueue):
-    """A queue that supports adding in index order."""
+    """A queue that iterates over the bundles in the key order rather than
+    insertion order.
+
+    For bundles inserted with the same key, insertion order is used.
+    """
 
     def __init__(self):
         super().__init__()
 
         self._inner: DefaultDict[int, Deque[RefBundle]] = defaultdict(lambda: deque())
         self._current_index: int = 0
-        self._completed_keys: Set[int] = set()
+        self._finalized_keys: Set[int] = set()
         self._finished: bool = False
 
-    def _move_to_next_index(self):
+    def _move_to_next_key(self):
         """Move the output index to the next task.
 
         This method should only be called when the current task is complete and all
         outputs have been taken.
         """
         assert len(self._inner[self._current_index]) == 0
-        assert self._current_index in self._completed_keys
+        assert self._current_index in self._finalized_keys
 
-        del self._inner[self._current_index]
-
-        self._completed_keys.remove(self._current_index)
-
-        if self._inner:
+        if self._current_index < len(self._inner) - 1:
             self._current_index += 1
         else:
             self._current_index = -1
@@ -48,16 +48,13 @@ class ReorderingBundleQueue(BaseBundleQueue):
 
     @override
     def has_next(self) -> bool:
-        if self._finished:
-            return False
-
         if (
-            len(self._inner[self._current_index]) == 0 and
-            self._current_index in self._completed_keys
+            self._current_index in self._finalized_keys and
+            len(self._inner[self._current_index]) == 0
         ):
-            self._move_to_next_index()
+            self._move_to_next_key()
 
-        return True
+        return len(self._inner[self._current_index]) > 0
 
     @override
     def _get_next_inner(self) -> RefBundle:
@@ -68,21 +65,20 @@ class ReorderingBundleQueue(BaseBundleQueue):
 
     @override
     def peek_next(self) -> Optional[RefBundle]:
-        if not self._inner[self._current_index]:
-            return None
-        return self._inner[self._current_index][0]
+        return (
+            self._inner[self._current_index][0]
+            if self._inner[self._current_index]
+            else None
+        )
 
     @override
     def finalize(self, key: int):
         assert key is not None and key >= self._current_index
-        self._completed_keys.add(key)
-        if key == self._current_index:
-            if len(self._inner[key]) == 0:
-                self._move_to_next_index()
+        self._finalized_keys.add(key)
 
     @override
     def clear(self):
         self._reset_metrics()
         self._inner.clear()
-        self._completed_keys.clear()
+        self._finalized_keys.clear()
         self._current_index = 0
