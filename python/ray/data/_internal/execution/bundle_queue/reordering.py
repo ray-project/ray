9a44f89cdd09c47221f12c30fc61f3e7b4e3d74b
Alexey Kudinkin <ak@anyscale.com>
2026-01-27 15:57:30 -0800

    Tidying up
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/data/_internal/execution/bundle_queue/reordering.py
+++ python/ray/data/_internal/execution/bundle_queue/reordering.py
@@ -27,7 +27,7 @@ class ReorderingBundleQueue(BaseBundleQueue):
         super().__init__()
 
         self._inner: DefaultDict[int, Deque[RefBundle]] = defaultdict(lambda: deque())
-        self._current_index: int = 0
+        self._current_key: int = 0
         self._finalized_keys: Set[int] = set()
 
     def _move_to_next_key(self):
@@ -36,10 +36,10 @@ class ReorderingBundleQueue(BaseBundleQueue):
         This method should only be called when the current task is complete and all
         outputs have been taken.
         """
-        assert len(self._inner[self._current_index]) == 0
-        assert self._current_index in self._finalized_keys
+        assert len(self._inner[self._current_key]) == 0
+        assert self._current_key in self._finalized_keys
 
-        self._current_index += 1
+        self._current_key += 1
 
     @override
     def _add_inner(self, bundle: RefBundle, key: int) -> None:
@@ -49,12 +49,12 @@ class ReorderingBundleQueue(BaseBundleQueue):
     @override
     def has_next(self) -> bool:
         if (
-            self._current_index in self._finalized_keys
-            and len(self._inner[self._current_index]) == 0
+            self._current_key in self._finalized_keys
+            and len(self._inner[self._current_key]) == 0
         ):
             self._move_to_next_key()
 
-        return len(self._inner[self._current_index]) > 0
+        return len(self._inner[self._current_key]) > 0
 
     @override
     def _get_next_inner(self) -> RefBundle:
@@ -63,19 +63,19 @@ class ReorderingBundleQueue(BaseBundleQueue):
         if not self.has_next():
             raise ValueError("Cannot pop from empty queue.")
 
-        return self._inner[self._current_index].popleft()
+        return self._inner[self._current_key].popleft()
 
     @override
     def peek_next(self) -> Optional[RefBundle]:
         return (
-            self._inner[self._current_index][0]
-            if self._inner[self._current_index]
+            self._inner[self._current_key][0]
+            if self._inner[self._current_key]
             else None
         )
 
     @override
     def finalize(self, key: int):
-        assert key is not None and key >= self._current_index
+        assert key is not None and key >= self._current_key
         self._finalized_keys.add(key)
 
     @override
@@ -83,4 +83,4 @@ class ReorderingBundleQueue(BaseBundleQueue):
         self._reset_metrics()
         self._inner.clear()
         self._finalized_keys.clear()
-        self._current_index = 0
+        self._current_key = 0
