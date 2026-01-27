a9a28d177635038c602912ef2105fa488d2645c7
yaommen <myanstu@163.com>
2026-01-27 15:17:05 +0800

    [Data] Drop output_dependencies from logical operators
    Signed-off-by: yaommen <myanstu@163.com>

--- python/ray/data/_internal/logical/rules/predicate_pushdown.py
+++ python/ray/data/_internal/logical/rules/predicate_pushdown.py
@@ -319,6 +319,4 @@ class PredicatePushdown(Rule):
         """
         new_op = copy.copy(op)
         new_op.input_dependencies = new_inputs
-        new_op._output_dependencies = []
-        new_op._wire_output_deps(new_inputs)
         return new_op
