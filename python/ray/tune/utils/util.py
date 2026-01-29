42042dde87ae840d0107b377b70b038fa8aa6169
Timothy Seah <tseah@anyscale.com>
2026-01-29 15:51:41 -0800

    ensure file name uniqueness within the same session
    Signed-off-by: Timothy Seah <tseah@anyscale.com>

--- python/ray/tune/utils/util.py
+++ python/ray/tune/utils/util.py
@@ -5,6 +5,7 @@ import logging
 import os
 import threading
 import time
+import uuid
 from collections import defaultdict
 from datetime import datetime
 from numbers import Number
@@ -408,7 +409,9 @@ def _atomic_save(state: Dict, checkpoint_dir: str, file_name: str, tmp_file_name
     """
     import ray.cloudpickle as cloudpickle
 
-    tmp_search_ckpt_path = os.path.join(checkpoint_dir, tmp_file_name)
+    tmp_search_ckpt_path = os.path.join(
+        checkpoint_dir, tmp_file_name + str(uuid.uuid4())
+    )
     with open(tmp_search_ckpt_path, "wb") as f:
         cloudpickle.dump(state, f)
 
