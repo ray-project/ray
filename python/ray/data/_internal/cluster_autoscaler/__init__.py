06af738bcb3089915e5ab9d8d1c3ea9a2eb9b339
Balaji Veeramani <balaji@anyscale.com>
2026-01-28 17:39:46 -0800

    [Data] Info log cluster scale up decisions (#60357)
    > I am thinking there is very limited visibility into the autoscaling
    decisions, currently have to look through DEBUG logs FWICT. Adding some
    visibility in terms of metrics and events would be nice, and promoting
    key action logs to INFO would be my high-level suggestion
    
    This PR logs a message to STDOUT whenever the autoscaler decides to
    scale up the cluster.
    
    ---------
    
    Signed-off-by: Balaji Veeramani <bveeramani@berkeley.edu>
    Signed-off-by: Adel Nour <ans9868@nyu.edu>

--- python/ray/data/_internal/cluster_autoscaler/__init__.py
+++ python/ray/data/_internal/cluster_autoscaler/__init__.py
@@ -40,8 +40,8 @@ def create_cluster_autoscaler(
     if selected_autoscaler == ClusterAutoscalerVersion.V2:
         return DefaultClusterAutoscalerV2(
             resource_manager,
-            resource_limits=resource_limits,
             execution_id=execution_id,
+            resource_limits=resource_limits,
         )
 
     elif selected_autoscaler == ClusterAutoscalerVersion.V1:
