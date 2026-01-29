82ca953df39f2d404cd41e38a3a1927bf22ce3b0
Edward Oakes <ed.nmi.oakes@gmail.com>
2026-01-29 08:06:46 -0800

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/core_worker/core_worker.cc
+++ src/ray/core_worker/core_worker.cc
@@ -2665,9 +2665,9 @@ Status CoreWorker::AllocateReturnObject(const ObjectID &object_id,
 
     // Allocate a buffer for the return object.
     if (static_cast<int64_t>(data_size) < max_direct_call_object_size_ &&
-         // ensure we don't exceed the limit if we allocate this object inline.
-         (*task_output_inlined_bytes + static_cast<int64_t>(data_size) <=
-          RayConfig::instance().task_rpc_inlined_bytes_limit())) {
+        // ensure we don't exceed the limit if we allocate this object inline.
+        (*task_output_inlined_bytes + static_cast<int64_t>(data_size) <=
+         RayConfig::instance().task_rpc_inlined_bytes_limit())) {
       data_buffer = std::make_shared<LocalMemoryBuffer>(data_size);
       *task_output_inlined_bytes += static_cast<int64_t>(data_size);
     } else {
