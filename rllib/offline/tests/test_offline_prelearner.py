c42df8c6a57d767b4a64b7d15262009ed328e6cb
simonsays1980 <simon.zehnder@gmail.com>
2026-02-03 18:22:43 +0100

    Uncommented offline prelearner tests.
    Signed-off-by: simonsays1980 <simon.zehnder@gmail.com>

--- rllib/offline/tests/test_offline_prelearner.py
+++ rllib/offline/tests/test_offline_prelearner.py
@@ -9,6 +9,7 @@ from ray.rllib.algorithms.bc import BCConfig
 from ray.rllib.algorithms.ppo import PPOConfig
 from ray.rllib.core import COMPONENT_RL_MODULE, Columns
 from ray.rllib.env import INPUT_ENV_SPACES
+from ray.rllib.env.single_agent_episode import SingleAgentEpisode
 from ray.rllib.offline.offline_prelearner import OfflinePreLearner
 from ray.rllib.policy.sample_batch import DEFAULT_POLICY_ID
 from ray.rllib.utils import unflatten_dict
@@ -65,173 +66,173 @@ class TestOfflinePreLearner(unittest.TestCase):
             )
         )
 
-    # def test_offline_prelearner_buffer_class(self):
-    #     """Tests using a user-defined buffer class with kwargs."""
+    def test_offline_prelearner_buffer_class(self):
+        """Tests using a user-defined buffer class with kwargs."""
 
-    #     from ray.rllib.utils.replay_buffers.prioritized_episode_buffer import (
-    #         PrioritizedEpisodeReplayBuffer,
-    #     )
+        from ray.rllib.utils.replay_buffers.prioritized_episode_buffer import (
+            PrioritizedEpisodeReplayBuffer,
+        )
 
-    #     sample_batch_data_path = (
-    #         self.base_path / "offline/tests/data/cartpole/large.json"
-    #     )
+        sample_batch_data_path = (
+            self.base_path / "offline/tests/data/cartpole/large.json"
+        )
 
-    #     self.config.offline_data(
-    #         input_=["local://" + sample_batch_data_path.as_posix()],
-    #         # Note, for the data we need to read a JSON file.
-    #         input_read_method="read_json",
-    #         # Note, this has to be set to `True`.
-    #         input_read_sample_batches=True,
-    #         # Use a user-defined `PreLearner` class and kwargs.
-    #         prelearner_buffer_class=PrioritizedEpisodeReplayBuffer,
-    #         prelearner_buffer_kwargs={
-    #             "capacity": 2000,
-    #             "alpha": 0.8,
-    #         },
-    #     )
+        self.config.offline_data(
+            input_=["local://" + sample_batch_data_path.as_posix()],
+            # Note, for the data we need to read a JSON file.
+            input_read_method="read_json",
+            # Note, this has to be set to `True`.
+            input_read_sample_batches=True,
+            # Use a user-defined `PreLearner` class and kwargs.
+            prelearner_buffer_class=PrioritizedEpisodeReplayBuffer,
+            prelearner_buffer_kwargs={
+                "capacity": 2000,
+                "alpha": 0.8,
+            },
+        )
 
-    #     # Build the algorithm to get the learner.
-    #     algo = self.config.build()
-    #     # Get the module state from the `Learner`(s).
-    #     module_state = algo.offline_data.learner_handles[0].get_state(
-    #         component=COMPONENT_RL_MODULE,
-    #     )[COMPONENT_RL_MODULE]
-    #     # Set up an `OfflinePreLearner` instance.
-    #     oplr = OfflinePreLearner(
-    #         config=self.config,
-    #         module_spec=algo.offline_data.module_spec,
-    #         module_state=module_state,
-    #     )
+        # Build the algorithm to get the learner.
+        algo = self.config.build()
+        # Get the module state from the `Learner`(s).
+        module_state = algo.offline_data.learner_handles[0].get_state(
+            component=COMPONENT_RL_MODULE,
+        )[COMPONENT_RL_MODULE]
+        # Set up an `OfflinePreLearner` instance.
+        oplr = OfflinePreLearner(
+            config=self.config,
+            module_spec=algo.offline_data.module_spec,
+            module_state=module_state,
+        )
 
-    #     # Ensure we have indeed a `PrioritizedEpisodeReplayBuffer` in the `PreLearner`
-    #     # with the `kwargs` we set.
-    #     self.assertIsInstance(oplr.episode_buffer, PrioritizedEpisodeReplayBuffer)
-    #     self.assertEqual(oplr.episode_buffer.capacity, 2000)
-    #     self.assertEqual(oplr.episode_buffer._alpha, 0.8)
+        # Ensure we have indeed a `PrioritizedEpisodeReplayBuffer` in the `PreLearner`
+        # with the `kwargs` we set.
+        self.assertIsInstance(oplr.episode_buffer, PrioritizedEpisodeReplayBuffer)
+        self.assertEqual(oplr.episode_buffer.capacity, 2000)
+        self.assertEqual(oplr.episode_buffer._alpha, 0.8)
 
-    #     # Now sample from the dataset and convert the `SampleBatch` in the `PreLearner`
-    #     # and sample episodes.
-    #     batch = algo.offline_data.data.take_batch(10)
-    #     batch = unflatten_dict(oplr(batch))
-    #     # Ensure all transformations worked and we have a `MultiAgentBatch`.
-    #     self.assertIsInstance(batch, dict)
-    #     # Ensure that we have as many environment steps as the train batch size.
-    #     self.assertEqual(
-    #         batch[DEFAULT_POLICY_ID][Columns.REWARDS].shape[0],
-    #         self.config.train_batch_size_per_learner,
-    #     )
-    #     # Ensure all keys are available and the length of each value is the
-    #     # train batch size.
-    #     for key in self.EXPECTED_KEYS:
-    #         self.assertIn(key, batch[DEFAULT_POLICY_ID])
-    #         self.assertEqual(
-    #             len(batch[DEFAULT_POLICY_ID][key]),
-    #             self.config.train_batch_size_per_learner,
-    #         )
+        # Now sample from the dataset and convert the `SampleBatch` in the `PreLearner`
+        # and sample episodes.
+        batch = algo.offline_data.data.take_batch(10)
+        batch = unflatten_dict(oplr(batch))
+        # Ensure all transformations worked and we have a `MultiAgentBatch`.
+        self.assertIsInstance(batch, dict)
+        # Ensure that we have as many environment steps as the train batch size.
+        self.assertEqual(
+            batch[DEFAULT_POLICY_ID][Columns.REWARDS].shape[0],
+            self.config.train_batch_size_per_learner,
+        )
+        # Ensure all keys are available and the length of each value is the
+        # train batch size.
+        for key in self.EXPECTED_KEYS:
+            self.assertIn(key, batch[DEFAULT_POLICY_ID])
+            self.assertEqual(
+                len(batch[DEFAULT_POLICY_ID][key]),
+                self.config.train_batch_size_per_learner,
+            )
 
-    # def test_offline_prelearner_convert_to_episodes(self):
-    #     """Tests conversion from column data to episodes."""
+    def test_offline_prelearner_convert_to_episodes(self):
+        """Tests conversion from column data to episodes."""
 
-    #     # Create the dataset.
-    #     data = ray.data.read_parquet(self.data_path)
+        # Create the dataset.
+        data = ray.data.read_parquet(self.data_path)
 
-    #     # Now, take a small batch from the data and conert it to episodes.
-    #     batch = data.take_batch(batch_size=10)
-    #     episodes = OfflinePreLearner._map_to_episodes(False, batch)["episodes"]
+        # Now, take a small batch from the data and conert it to episodes.
+        batch = data.take_batch(batch_size=10)
+        episodes = OfflinePreLearner._map_to_episodes(False, batch)["episodes"]
 
-    #     self.assertTrue(len(episodes) == 10)
-    #     self.assertTrue(isinstance(episodes[0], SingleAgentEpisode))
+        self.assertTrue(len(episodes) == 10)
+        self.assertTrue(isinstance(episodes[0], SingleAgentEpisode))
 
-    # def test_offline_prelearner_ignore_final_observation(self):
-    #     # Create the dataset.
-    #     data = ray.data.read_parquet(self.data_path)
+    def test_offline_prelearner_ignore_final_observation(self):
+        # Create the dataset.
+        data = ray.data.read_parquet(self.data_path)
 
-    #     # Now, take a small batch from the data and conert it to episodes.
-    #     batch = data.take_batch(batch_size=10)
-    #     episodes = OfflinePreLearner._map_to_episodes(
-    #         False, batch, ignore_final_observation=True
-    #     )["episodes"]
+        # Now, take a small batch from the data and conert it to episodes.
+        batch = data.take_batch(batch_size=10)
+        episodes = OfflinePreLearner._map_to_episodes(
+            False, batch, ignore_final_observation=True
+        )["episodes"]
 
-    #     self.assertTrue(
-    #         all(all(eps.get_observations()[-1] == [0.0] * 4) for eps in episodes)
-    #     )
+        self.assertTrue(
+            all(all(eps.get_observations()[-1] == [0.0] * 4) for eps in episodes)
+        )
 
-    # def test_offline_prelearner_convert_from_old_sample_batch_to_episodes(self):
-    #     """Tests conversion from `SampleBatch` data to episodes."""
+    def test_offline_prelearner_convert_from_old_sample_batch_to_episodes(self):
+        """Tests conversion from `SampleBatch` data to episodes."""
 
-    #     # Use the old records storing `SampleBatch`es.
-    #     sample_batch_data_path = (
-    #         self.base_path / "offline/tests/data/cartpole/large.json"
-    #     )
+        # Use the old records storing `SampleBatch`es.
+        sample_batch_data_path = (
+            self.base_path / "offline/tests/data/cartpole/large.json"
+        )
 
-    #     # Create the dataset.
-    #     data = ray.data.read_json(sample_batch_data_path.as_posix())
+        # Create the dataset.
+        data = ray.data.read_json(sample_batch_data_path.as_posix())
 
-    #     # Sample a small batch from the raw data.
-    #     batch = data.take_batch(batch_size=10)
-    #     # Convert `SampleBatch` data to episode data.
-    #     episodes = OfflinePreLearner._map_sample_batch_to_episode(False, batch)[
-    #         "episodes"
-    #     ]
-    #     # Assert that we have sampled episodes.
-    #     self.assertTrue(len(episodes) == 10)
-    #     self.assertTrue(isinstance(episodes[0], SingleAgentEpisode))
+        # Sample a small batch from the raw data.
+        batch = data.take_batch(batch_size=10)
+        # Convert `SampleBatch` data to episode data.
+        episodes = OfflinePreLearner._map_sample_batch_to_episode(False, batch)[
+            "episodes"
+        ]
+        # Assert that we have sampled episodes.
+        self.assertTrue(len(episodes) == 10)
+        self.assertTrue(isinstance(episodes[0], SingleAgentEpisode))
 
-    # def test_offline_prelearner_sample_from_old_sample_batch_data(self):
-    #     """Tests sampling from a `SampleBatch` dataset."""
+    def test_offline_prelearner_sample_from_old_sample_batch_data(self):
+        """Tests sampling from a `SampleBatch` dataset."""
 
-    #     data_path = self.base_path / "offline/tests/data/cartpole/large.json"
+        data_path = self.base_path / "offline/tests/data/cartpole/large.json"
 
-    #     self.config.offline_data(
-    #         input_=["local://" + data_path.as_posix()],
-    #         # Note, the default is `read_parquet`.
-    #         input_read_method="read_json",
-    #         # Signal that we want to read in old `SampleBatch` data.
-    #         input_read_sample_batches=True,
-    #         # Use a different input batch size b/c each `SampleBatch`
-    #         # contains multiple timesteps.
-    #         input_read_batch_size=50,
-    #     )
+        self.config.offline_data(
+            input_=["local://" + data_path.as_posix()],
+            # Note, the default is `read_parquet`.
+            input_read_method="read_json",
+            # Signal that we want to read in old `SampleBatch` data.
+            input_read_sample_batches=True,
+            # Use a different input batch size b/c each `SampleBatch`
+            # contains multiple timesteps.
+            input_read_batch_size=50,
+        )
 
-    #     # Build the algorithm to get the learner.
-    #     algo = self.config.build()
-    #     # Get the module state from the `Learner`.
-    #     module_state = algo.offline_data.learner_handles[0].get_state(
-    #         component=COMPONENT_RL_MODULE,
-    #     )[COMPONENT_RL_MODULE]
-    #     # Set up an `OfflinePreLearner` instance.
-    #     oplr = OfflinePreLearner(
-    #         config=self.config,
-    #         module_spec=algo.offline_data.module_spec,
-    #         module_state=module_state,
-    #     )
-    #     # Now, pull a batch of defined size from the dataset.
-    #     batch = algo.offline_data.data.take_batch(
-    #         self.config.train_batch_size_per_learner
-    #     )
-    #     # Pass the batch through the `OfflinePreLearner`. Note, the batch is
-    #     # a batch of `SampleBatch`es and could potentially have more than the
-    #     # defined number of experiences to be used for learning.
-    #     # The `OfflinePreLearner`'s episode buffer should buffer all data
-    #     # and sample the exact size requested by the user, i.e.
-    #     # `train_batch_size_per_learner`
-    #     batch = unflatten_dict(oplr(batch))
+        # Build the algorithm to get the learner.
+        algo = self.config.build()
+        # Get the module state from the `Learner`.
+        module_state = algo.offline_data.learner_handles[0].get_state(
+            component=COMPONENT_RL_MODULE,
+        )[COMPONENT_RL_MODULE]
+        # Set up an `OfflinePreLearner` instance.
+        oplr = OfflinePreLearner(
+            config=self.config,
+            module_spec=algo.offline_data.module_spec,
+            module_state=module_state,
+        )
+        # Now, pull a batch of defined size from the dataset.
+        batch = algo.offline_data.data.take_batch(
+            self.config.train_batch_size_per_learner
+        )
+        # Pass the batch through the `OfflinePreLearner`. Note, the batch is
+        # a batch of `SampleBatch`es and could potentially have more than the
+        # defined number of experiences to be used for learning.
+        # The `OfflinePreLearner`'s episode buffer should buffer all data
+        # and sample the exact size requested by the user, i.e.
+        # `train_batch_size_per_learner`
+        batch = unflatten_dict(oplr(batch))
 
-    #     # Ensure all transformations worked and we have a `MultiAgentBatch`.
-    #     self.assertIsInstance(batch, dict)
-    #     # Ensure that we have as many environment steps as the train batch size.
-    #     self.assertEqual(
-    #         batch[DEFAULT_POLICY_ID][Columns.REWARDS].shape[0],
-    #         self.config.train_batch_size_per_learner,
-    #     )
-    #     # Ensure all keys are available and the length of each value is the
-    #     # train batch size.
-    #     for key in self.EXPECTED_KEYS:
-    #         self.assertIn(key, batch[DEFAULT_POLICY_ID])
-    #         self.assertEqual(
-    #             len(batch[DEFAULT_POLICY_ID][key]),
-    #             self.config.train_batch_size_per_learner,
-    #         )
+        # Ensure all transformations worked and we have a `MultiAgentBatch`.
+        self.assertIsInstance(batch, dict)
+        # Ensure that we have as many environment steps as the train batch size.
+        self.assertEqual(
+            batch[DEFAULT_POLICY_ID][Columns.REWARDS].shape[0],
+            self.config.train_batch_size_per_learner,
+        )
+        # Ensure all keys are available and the length of each value is the
+        # train batch size.
+        for key in self.EXPECTED_KEYS:
+            self.assertIn(key, batch[DEFAULT_POLICY_ID])
+            self.assertEqual(
+                len(batch[DEFAULT_POLICY_ID][key]),
+                self.config.train_batch_size_per_learner,
+            )
 
     def test_offline_prelearner_sample_from_episode_data(self):
 
