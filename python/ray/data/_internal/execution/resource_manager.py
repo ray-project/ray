dd26b3456638944914332a77281d78dd0825448c
Alexey Kudinkin <ak@anyscale.com>
2026-02-02 23:57:24 -0800

    Fixed conditional
    Signed-off-by: Alexey Kudinkin <ak@anyscale.com>

--- python/ray/data/_internal/execution/resource_manager.py
+++ python/ray/data/_internal/execution/resource_manager.py
@@ -868,7 +868,7 @@ class ReservationOpResourceAllocator(OpResourceAllocator):
             # Avoid scheduling if there's no more Object Store budget (for
             # task outputs)
             budget.object_store_memory
-            > (op.metrics.obj_store_mem_max_pending_output_per_task or 0)
+            >= (op.metrics.obj_store_mem_max_pending_output_per_task or 0)
         )
 
     def get_budget(self, op: PhysicalOperator) -> Optional[ExecutionResources]:
