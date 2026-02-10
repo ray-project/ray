46afcfed4525017f88dc114919f334ca1879b8d2
elliot-barn <elliot.barnwell@anyscale.com>
2026-02-10 18:58:48 +0000

    removing log
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/raydepsets/cli.py
+++ ci/raydepsets/cli.py
@@ -256,7 +256,6 @@ class DependencySetManager:
             _get_depset(self.config.depsets, single_depset_name)
             self.subgraph_dependency_nodes(single_depset_name)
         for node in topological_sort(self.build_graph):
-            click.echo(f"Executing node: {node}")
             node_type = self.build_graph.nodes[node]["node_type"]
             if node_type == "pre_hook":
                 pre_hook = self.build_graph.nodes[node]["pre_hook"]
