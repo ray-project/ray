6e9515bdae5eddd66e9edab74cfd45bae43bd791
harshit <harshit@anyscale.com>
2026-02-12 12:07:27 +0000

    test changes
    Signed-off-by: harshit <harshit@anyscale.com>

--- python/ray/serve/tests/test_taskiq_task_processor.py
+++ python/ray/serve/tests/test_taskiq_task_processor.py
@@ -11,14 +11,6 @@ from ray.serve.taskiq_task_processor import (
 class TestBrokerFactory:
     """Tests for broker creation helpers."""
 
-    def test_create_redis_broker(self):
-        broker = _create_broker(
-            broker_type="redis_stream",
-            queue_name="test_queue",
-            broker_kwargs={"url": "redis://localhost:6379"},
-        )
-        assert broker is not None
-
     def test_unsupported_broker_type_raises(self):
         with pytest.raises(ValueError, match="Unsupported broker_type"):
             _import_broker_class("unsupported_broker")
