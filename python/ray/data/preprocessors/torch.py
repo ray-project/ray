b9de8eedd06fe88cc13aed985ef0cad1d243490d
Sirui Huang <ray.huang@anyscale.com>
2026-02-17 11:17:05 -0800

    Add get/set serialiable fields to preprocessors
    Signed-off-by: Sirui Huang <ray.huang@anyscale.com>

--- python/ray/data/preprocessors/torch.py
+++ python/ray/data/preprocessors/torch.py
@@ -3,7 +3,8 @@ from typing import TYPE_CHECKING, Any, Callable, Dict, List, Mapping, Optional, 
 import numpy as np
 
 from ray.data._internal.tensor_extensions.utils import _create_possibly_ragged_ndarray
-from ray.data.preprocessor import Preprocessor
+from ray.data.preprocessor import SerializablePreprocessorBase
+from ray.data.preprocessors.version_support import SerializablePreprocessor
 from ray.data.util.data_batch_conversion import BatchFormat
 from ray.util.annotations import PublicAPI
 
@@ -11,8 +12,11 @@ if TYPE_CHECKING:
     import torch
 
 
+@SerializablePreprocessor(
+    version=1, identifier="io.ray.preprocessors.torchvision_preprocessor"
+)
 @PublicAPI(stability="alpha")
-class TorchVisionPreprocessor(Preprocessor):
+class TorchVisionPreprocessor(SerializablePreprocessorBase):
     """Apply a `TorchVision transform <https://pytorch.org/vision/stable/transforms.html>`_
     to image columns.
 
@@ -172,6 +176,21 @@ class TorchVisionPreprocessor(Preprocessor):
 
     def preferred_batch_format(cls) -> BatchFormat:
         return BatchFormat.NUMPY
+
+    def _get_serializable_fields(self) -> Dict[str, Any]:
+        return {
+            "columns": self._columns,
+            "output_columns": self._output_columns,
+            "torchvision_transform": self._torchvision_transform,
+            "batched": self._batched,
+        }
+
+    def _set_serializable_fields(self, fields: Dict[str, Any], version: int):
+        # required fields
+        self._columns = fields["columns"]
+        self._output_columns = fields["output_columns"]
+        self._torchvision_transform = fields["torchvision_transform"]
+        self._batched = fields["batched"]
 
     def __setstate__(self, state: Dict[str, Any]) -> None:
         super().__setstate__(state)
