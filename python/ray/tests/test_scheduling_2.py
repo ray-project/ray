7d6dddbf04db861a0c874b11d18417c76ad338a3
zac <zac@anyscale.com>
2026-02-18 23:18:42 +0000

    update test to use different metric for task count verification
    Signed-off-by: zac <zac@anyscale.com>

--- python/ray/tests/test_scheduling_2.py
+++ python/ray/tests/test_scheduling_2.py
@@ -788,12 +788,28 @@ def test_workload_placement_metrics(ray_start_regular):
     pg = placement_group(bundles=[{"CPU": 1}], strategy="SPREAD")
     ray.get(pg.ready())
 
+    # When in client mode, the placement group creation and the _get_bundle_cache
+    # function when checking whether the placement group is ready will be executed as
+    # a remote task. In other mode, the functions will be called directly as local
+    # functions. So the expected task workload metrics values are different between
+    # client and non-client mode.
     timeseries = PrometheusTimeseries()
     placement_metric_condition = get_metric_check_condition(
         [
-            MetricSamplePattern(name="ray_scheduler_placement_time_ms_p50"),
-            MetricSamplePattern(name="ray_scheduler_placement_time_ms_p99"),
-            MetricSamplePattern(name="ray_scheduler_placement_time_ms_max"),
+            MetricSamplePattern(
+                name="ray_scheduler_placement_time_ms_bucket",
+                value=1.0,
+                partial_label_match={"WorkloadType": "Actor"},
+            ),
+            MetricSamplePattern(
+                name="ray_tasks",
+                partial_label_match={"State": "FINISHED", "Name": "task"},
+            ),
+            MetricSamplePattern(
+                name="ray_scheduler_placement_time_ms_bucket",
+                value=1.0,
+                partial_label_match={"WorkloadType": "PlacementGroup"},
+            ),
         ],
         timeseries,
     )
