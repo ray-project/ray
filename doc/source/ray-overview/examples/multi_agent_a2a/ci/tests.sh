a3a4e51b0da091d943cde990d846643ae38655de
Aydin Abiar <aydin@anyscale.com>
2026-02-18 12:59:51 -0800

    add test script
    Signed-off-by: Aydin Abiar <aydin@anyscale.com>

--- doc/source/ray-overview/examples/multi_agent_a2a/ci/tests.sh
+++ doc/source/ray-overview/examples/multi_agent_a2a/ci/tests.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# TODO: Add test logic here
+# Generate README.sh from the notebook
+python ci/nb2sh.py content/README.ipynb content/README.sh
+
+# Run from the content directory so relative paths work
+cd content
+bash README.sh
