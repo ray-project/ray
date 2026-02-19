bb62e3b562ff0447721aa041c81be9b455092d7c
Sirui Huang <ray.huang@anyscale.com>
2026-02-18 16:59:25 -0800

    Change log level
    Signed-off-by: Sirui Huang <ray.huang@anyscale.com>

--- python/ray/data/_internal/execution/streaming_executor.py
+++ python/ray/data/_internal/execution/streaming_executor.py
@@ -185,8 +185,8 @@ class StreamingExecutor(Executor, threading.Thread):
                 )
 
             # Log the full DataContext for traceability
-            if logger.isEnabledFor(logging.INFO):
-                logger.info("Data Context:\n%s", pprint.pformat(self._data_context))
+            if logger.isEnabledFor(logging.DEBUG):
+                logger.debug("Data Context:\n%s", pprint.pformat(self._data_context))
 
         # Setup the streaming DAG topology and start the runner thread.
         self._topology = build_streaming_topology(dag, self._options)
