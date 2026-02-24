cfe244d1f5e9b2a29509ea3e24d95dd2319055fe
akyang-anyscale <alexyang@anyscale.com>
2026-02-24 08:41:54 +0000

    unit tests
    Signed-off-by: akyang-anyscale <alexyang@anyscale.com>

--- python/ray/serve/tests/unit/test_controller_haproxy.py
+++ python/ray/serve/tests/unit/test_controller_haproxy.py
@@ -1,3 +1,4 @@
+from audioop import add
 import pytest
 
 from ray.serve._private.common import (
@@ -109,7 +110,8 @@ def test_get_target_groups_haproxy(
 
     # Setup proxy state manager
     haproxy_controller.proxy_state_manager.add_proxy_details(
-        "proxy_node1", "instance1", "10.0.1.1", "proxy1")
+        "proxy_node1", "instance1", "10.0.1.1", "proxy1"
+    )
     haproxy_controller.proxy_state_manager.add_proxy_details(
         "proxy_node2", "instance2", "10.0.1.2", "proxy2"
     )
@@ -216,6 +218,116 @@ def test_get_target_groups_haproxy(
 
     assert target_groups == expected_target_groups
 
+
+def test_get_target_groups_haproxy_no_apps(
+    haproxy_controller: FakeHAProxyController,
+):
+    """Tests get_target_groups returns the appropriate target groups based on the
+    ha_proxy_enabled and from_proxy_manager parameters."""
+
+    haproxy_controller._ha_proxy_enabled = True
+    haproxy_controller.proxy_state_manager.add_fallback_proxy_details(
+        "fallback_node_id", "fallback_instance_id", "10.0.0.1", "fallback_proxy"
+    )
+
+    target_groups = haproxy_controller.get_target_groups(
+        from_proxy_manager=True,
+    )
+
+    assert target_groups == [
+        TargetGroup(
+            protocol=RequestProtocol.HTTP,
+            route_prefix="/",
+            app_name="",
+            targets=[],
+            fallback_target=Target(
+                ip="10.0.0.1",
+                port=8500,
+                instance_id="fallback_instance_id",
+                name="fallback_proxy",
+            ),
+        ),
+        TargetGroup(
+            protocol=RequestProtocol.GRPC,
+            route_prefix="/",
+            app_name="",
+            targets=[],
+            fallback_target=Target(
+                ip="10.0.0.1",
+                port=9500,
+                instance_id="fallback_instance_id",
+                name="fallback_proxy",
+            ),
+        ),
+    ]
+
+
+def test_get_target_groups_app_with_no_running_replicas(
+    haproxy_controller: FakeHAProxyController,
+):
+    """Tests get_target_groups returns the appropriate target groups when an app
+    has no running replicas."""
+
+    haproxy_controller._ha_proxy_enabled = True
+    haproxy_controller.proxy_state_manager.add_fallback_proxy_details(
+        "fallback_node_id", "fallback_instance_id", "10.0.0.1", "fallback_proxy"
+    )
+
+    # Setup test data with running applications
+    app_statuses = {"app1": {}}
+    route_prefixes = {"app1": "/app1"}
+    ingress_deployments = {"app1": "app1_ingress"}
+
+    deployment_id1 = DeploymentID(name="app1_ingress", app_name="app1")
+
+    running_replica_infos = {deployment_id1: []}
+
+    # Setup test application state manager
+    haproxy_controller.application_state_manager = FakeApplicationStateManager(
+        app_statuses=app_statuses,
+        route_prefixes=route_prefixes,
+        ingress_deployments=ingress_deployments,
+    )
+
+    # Setup test deployment state manager
+    haproxy_controller.deployment_state_manager = FakeDeploymentStateManager(
+        running_replica_infos=running_replica_infos,
+    )
+
+    haproxy_controller.proxy_state_manager.add_proxy_details(
+        "proxy_node1", "instance1", "10.0.0.1", "proxy1"
+    )
+
+    target_groups = haproxy_controller.get_target_groups(
+        from_proxy_manager=True,
+    )
+
+    assert target_groups == [
+        TargetGroup(
+            protocol=RequestProtocol.HTTP,
+            route_prefix="/app1",
+            app_name="app1",
+            targets=[],
+            fallback_target=Target(
+                ip="10.0.0.1",
+                port=8500,
+                instance_id="fallback_instance_id",
+                name="fallback_proxy",
+            ),
+        ),
+        TargetGroup(
+            protocol=RequestProtocol.GRPC,
+            route_prefix="/app1",
+            app_name="app1",
+            targets=[],
+            fallback_target=Target(
+                ip="10.0.0.1",
+                port=9500,
+                instance_id="fallback_instance_id",
+                name="fallback_proxy",
+            ),
+        ),
+    ]
 
 if __name__ == "__main__":
     pytest.main()
