07be9ebda5c42da67efcb63a427aa657f00568a1
Lehui Liu <lehui@anyscale.com>
2026-02-25 09:38:10 -0800

    revert change in dolly notebook
    Signed-off-by: Lehui Liu <lehui@anyscale.com>

--- python/ray/train/lightning/_lightning_utils.py
+++ python/ray/train/lightning/_lightning_utils.py
@@ -18,7 +18,7 @@ from ray.util import PublicAPI
 def import_lightning():  # noqa: F402
     try:
         import lightning.pytorch as pl
-    except ModuleNotFoundError:
+    except ImportError:
         import pytorch_lightning as pl
     return pl
 
@@ -32,7 +32,7 @@ _TORCH_FSDP_AVAILABLE = _TORCH_GREATER_EQUAL_1_12 and torch.distributed.is_avail
 
 try:
     from lightning.pytorch.plugins.environments import LightningEnvironment
-except ModuleNotFoundError:
+except ImportError:
     from pytorch_lightning.plugins.environments import LightningEnvironment
 
 if _LIGHTNING_GREATER_EQUAL_2_0:
