4a71ece514553a8954389587c936b9bca8f486ae
jeffreywang-anyscale <jeffreywang@anyscale.com>
2026-02-25 22:34:57 +0000

    Fix linter
    Signed-off-by: jeffreywang-anyscale <jeffreywang@anyscale.com>

--- python/ray/serve/_private/utils.py
+++ python/ray/serve/_private/utils.py
@@ -30,7 +30,6 @@ from ray.serve._private.constants import (
 )
 from ray.types import ObjectRef
 from ray.util.serialization import StandaloneSerializationContext
-from ray.util.state import list_actors
 
 try:
     import pandas as pd
@@ -531,12 +530,17 @@ def get_active_placement_group_ids() -> Set[str]:
     Returns:
         The set of placement group IDs referenced by alive Serve actors.
     """
+    # TODO (jeffreywang): Move the imports to the top of the file.
+    # https://github.com/ray-project/ray/issues/61330
+    from ray.util.state import list_actors
+    from ray.util.state.common import RAY_MAX_LIMIT_FROM_API_SERVER
+
     actors = list_actors(
         filters=[
             ("ray_namespace", "=", SERVE_NAMESPACE),
             ("state", "=", "ALIVE"),
         ],
-        limit=ray.util.state.common.RAY_MAX_LIMIT_FROM_API_SERVER,
+        limit=RAY_MAX_LIMIT_FROM_API_SERVER,
         detail=True,
         raise_on_missing_output=False,
     )
