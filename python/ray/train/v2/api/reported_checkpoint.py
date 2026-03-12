929a802e161b3477a3cb944024918a03eb208c63
Mark Towers <mark@anyscale.com>
2026-03-12 14:43:15 +0000

    [Train] Add status to ReportedCheckpoint
    Signed-off-by: Mark Towers <mark@anyscale.com>

--- python/ray/train/v2/api/reported_checkpoint.py
+++ python/ray/train/v2/api/reported_checkpoint.py
@@ -1,10 +1,24 @@
 from dataclasses import dataclass
+from enum import Enum
 from typing import TYPE_CHECKING, Any, Dict
 
 from ray.util.annotations import PublicAPI
 
 if TYPE_CHECKING:
     from ray.train import Checkpoint
+
+
+class ReportedCheckpointStatus(Enum):
+    """Reported Checkpoint status.
+
+    * COMMITTED: Saved without validation function
+    * PENDING_VALIDATION: Saved and pending validation function to finish
+    * VALIDATED: Saved and validation function is completed
+    """
+
+    COMMITTED = "COMMITTED"
+    PENDING_VALIDATION = "PENDING_VALIDATION"
+    VALIDATED = "VALIDATED"
 
 
 @dataclass
@@ -15,7 +29,9 @@ class ReportedCheckpoint:
     Attributes:
         checkpoint: The checkpoint reported by the user.
         metrics: The metrics associated with that checkpoint.
+        status: The status of the checkpoint.
     """
 
     checkpoint: "Checkpoint"
     metrics: Dict[str, Any]
+    status: ReportedCheckpointStatus
