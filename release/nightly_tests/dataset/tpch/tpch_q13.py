88360d64eda5864cf078fb44ce3de059395ab959
machichima <nary12321@gmail.com>
2026-03-18 20:08:17 +0800

    fix: use .filter
    Signed-off-by: machichima <nary12321@gmail.com>

--- release/nightly_tests/dataset/tpch/tpch_q13.py
+++ release/nightly_tests/dataset/tpch/tpch_q13.py
@@ -1,6 +1,6 @@
-import pyarrow.compute as pc
 import ray
 from ray.data.aggregate import Count
+from ray.data.expressions import col
 from common import parse_tpch_args, load_table, run_tpch_benchmark
 
 
@@ -33,13 +33,8 @@ def main(args):
 
         # Filter out orders whose comment matches '%<word1>%<word2>%' before the join
         # o_comment NOT LIKE '%special%requests%'
-        orders = orders.map_batches(
-            lambda batch: batch.filter(
-                pc.invert(
-                    pc.match_substring_regex(batch["o_comment"], f"{word1}.*{word2}")
-                )
-            ),
-            batch_format="pyarrow",
+        orders = orders.filter(
+            expr=~col("o_comment").str.match_regex(f"{word1}.*{word2}")
         )
 
         # Left outer join: customer LEFT OUTER JOIN orders ON c_custkey = o_custkey
