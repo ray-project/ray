a8dfd23ead717c7defc3e4acca185eec0257f564
Lonnie Liu <lonnie@anyscale.com>
2026-03-25 20:52:58 +0000

    fix sanity cpp-check
    Signed-off-by: Lonnie Liu <lonnie@anyscale.com>

--- release/util/sanity_check_cpp.sh
+++ release/util/sanity_check_cpp.sh
@@ -3,11 +3,15 @@
 # This script generate a ray C++ template and run example
 set -e
 rm -rf ray-template
+
+# Patch to make things work..
+echo "6.5.0" > .bazelversion
+
 ray cpp --generate-bazel-project-template-to ray-template
 (
     cd ray-template
 
     # Our generated CPP template does not work with bazel 7.x ,
     # so pin the bazel version to 6
-    USE_BAZEL_VERSION=6.5.0 bash run.sh
+    bash run.sh
 )
