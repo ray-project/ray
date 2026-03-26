ab60a06cee745b1ee49d741b9b4ba2fa320152d1
Mark Towers <mark@anyscale.com>
2026-03-26 16:53:56 +0000

    update wandb in byod
    Signed-off-by: Mark Towers <mark@anyscale.com>

--- release/ray_release/byod/byod_huggingface_trl.sh
+++ release/ray_release/byod/byod_huggingface_trl.sh
@@ -6,6 +6,6 @@ set -exo pipefail
 
 # Install TRL and math_verify
 pip3 install --no-cache-dir "trl[vllm]" math_verify
-pip3 install --no-cache-dir --force-reinstall numpy pandas tf_keras
+pip3 install --no-cache-dir --force-reinstall numpy pandas tf_keras wandb
 # vllm requires numpy>=2.x; upgrade tensorflow and matplotlib to a version compatible with numpy 2.x.
 pip3 install --no-cache-dir --upgrade tensorflow matplotlib
