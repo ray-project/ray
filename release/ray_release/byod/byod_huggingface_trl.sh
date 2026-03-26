610b35056d2e47702c13f6e322b978191465276e
Mark Towers <mark@anyscale.com>
2026-03-26 15:00:49 +0000

    Add tf-keras and matplotlib
    Signed-off-by: Mark Towers <mark@anyscale.com>

--- release/ray_release/byod/byod_huggingface_trl.sh
+++ release/ray_release/byod/byod_huggingface_trl.sh
@@ -6,6 +6,6 @@ set -exo pipefail
 
 # Install TRL and math_verify
 pip3 install --no-cache-dir "trl[vllm]" math_verify
-pip3 install --no-cache-dir --force-reinstall numpy pandas
-# vllm requires numpy>=2.x; upgrade tensorflow to a version compatible with numpy 2.x.
-pip3 install --no-cache-dir --upgrade tensorflow
+pip3 install --no-cache-dir --force-reinstall numpy pandas tf_keras
+# vllm requires numpy>=2.x; upgrade tensorflow and matplotlib to a version compatible with numpy 2.x.
+pip3 install --no-cache-dir --upgrade tensorflow matplotlib
