f8b1737f13db70e6eb1eea958794e44c077bbda8
Mark Towers <mark@anyscale.com>
2026-03-27 14:07:16 +0000

    cursor code review
    Signed-off-by: Mark Towers <mark@anyscale.com>

--- release/ray_release/byod/byod_huggingface_trl.sh
+++ release/ray_release/byod/byod_huggingface_trl.sh
@@ -10,4 +10,4 @@ pip3 install --no-cache-dir --force-reinstall numpy pandas tf_keras
 # vllm requires numpy>=2.x; upgrade tensorflow and matplotlib to a version compatible with numpy 2.x.
 pip3 install --no-cache-dir --upgrade tensorflow matplotlib
 # wandb and comet were installed but didn't have API keys which raised errors and warnings
-pip3 uninstall wandb comet
+pip3 uninstall -y wandb comet_ml
