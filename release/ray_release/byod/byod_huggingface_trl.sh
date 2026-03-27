a15164d5121ca8cb289411bf6c5fbf3309b87921
Mark Towers <mark@anyscale.com>
2026-03-27 12:22:54 +0000

    Uninstall wandb and comet
    Signed-off-by: Mark Towers <mark@anyscale.com>

--- release/ray_release/byod/byod_huggingface_trl.sh
+++ release/ray_release/byod/byod_huggingface_trl.sh
@@ -6,6 +6,8 @@ set -exo pipefail
 
 # Install TRL and math_verify
 pip3 install --no-cache-dir "trl[vllm]" math_verify
-pip3 install --no-cache-dir --force-reinstall numpy pandas tf_keras wandb
+pip3 install --no-cache-dir --force-reinstall numpy pandas tf_keras
 # vllm requires numpy>=2.x; upgrade tensorflow and matplotlib to a version compatible with numpy 2.x.
 pip3 install --no-cache-dir --upgrade tensorflow matplotlib
+# wandb and comet were installed but didn't have API keys which raised errors and warnings
+pip3 uninstall wandb comet
