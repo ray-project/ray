aba4c0b190ec837ceba75146456f66144a9ab177
iamjustinhsu <jhsu@anyscale.com>
2026-04-01 10:48:29 -0700

    add worker type too
    Signed-off-by: iamjustinhsu <jhsu@anyscale.com>

--- release/nightly_tests/dataset/worker_scaling_benchmark.py
+++ release/nightly_tests/dataset/worker_scaling_benchmark.py
@@ -9,24 +9,23 @@ import argparse
 import ray
 from benchmark import Benchmark, OperatorStatsTracker
 
-
-NUM_ACTORS = 1000
-NUM_ROWS = 1_000_000
+NUM_ROWS: int = 100_000_000
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--num-actors",
+        "--num-workers",
         type=int,
-        default=NUM_ACTORS,
-        help="Number of actors to use for map_batches.",
+        default=0,
+        help="Number of actors/tasks to use for map_batches.",
     )
     parser.add_argument(
-        "--num-rows",
-        type=int,
-        default=NUM_ROWS,
-        help="Number of rows in the input dataset.",
+        "--worker-type",
+        type=str,
+        choices=["actors", "tasks"],
+        default="actors",
+        help="Whether to use actors or regular tasks for map_batches.",
     )
     return parser.parse_args()
 
@@ -36,6 +35,10 @@ class NoOpUDF:
         return batch
 
 
+def no_op_udf(batch):
+    return batch
+
+
 def main(args: argparse.Namespace):
     benchmark = Benchmark()
 
@@ -43,19 +46,29 @@ def main(args: argparse.Namespace):
     ctx.custom_execution_callback_classes.append(OperatorStatsTracker)
 
     def benchmark_fn():
-        ds = ray.data.range(args.num_rows).map_batches(
-            NoOpUDF,
-            concurrency=args.num_actors,
-        )
+        override_num_blocks = 2 * args.num_workers
+        ds = ray.data.range(NUM_ROWS, override_num_blocks=override_num_blocks)
+
+        if args.worker_type == "actors":
+            ds = ds.map_batches(
+                NoOpUDF,
+                num_cpus=1,
+                compute=ray.data.ActorPoolStrategy(size=args.num_workers),
+            )
+        else:
+            ds = ds.map_batches(
+                no_op_udf,
+                num_cpus=1,
+                compute=ray.data.TaskPoolStrategy(size=args.num_workers),
+            )
 
         total_rows = 0
         for bundle in ds.iter_internal_ref_bundles():
             total_rows += bundle.num_rows()
-
         metrics = OperatorStatsTracker.collect()
         return metrics
 
-    benchmark.run_fn("actor_scaling", benchmark_fn)
+    benchmark.run_fn("worker_scaling", benchmark_fn)
     benchmark.write_result()
 
 
