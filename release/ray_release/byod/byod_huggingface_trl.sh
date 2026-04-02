462128c1a1e4ac965df549c57414ba125fd48fa5
Mark Towers <mark.m.towers@gmail.com>
2026-04-02 19:23:58 +0100

    [Train] Add HF TRL (Transformer Reinforcement Learning) example (#61627)
    Add an example of TRL for Ray-Train.
    
    Changes:
    1. Add `huggingface_transformer_reinforcement_learning.ipynb`, a
    notebook example that uses TRL's GRPO implementation and DeepMath-103k
    dataset (using https://github.com/anyscale/product/pull/35429 and
    https://huggingface.co/docs/trl/en/grpo_trainer as inspiration). This
    should outline what the DeepMath dataset is, how the GRPO algorithm
    works, then train a Qwen2.5 0.5B model for 2 epoches with 100 samples,
    saving a checkpoint for each epoch then printing the best result.
    Finally, a short section on how to scale the example to larger models,
    more GPUs, and fault tolerance.
    2. Modify `examples.yml` to add the jupyter notebook to ray.io website
    (it should also be added to the Anyscale workspace templates) and
    `release_tests.yml` to add testing that the example continues to work.
    
    
    Importantly, for the implementations, the math_verify `accuracy_reward`
    function uses functions that require `signal.alarm()` by default.
    Therefore, we need to copy the function and add `parsing_timeout=0` and
    `timeout_seconds=0` to disable this
    
    ---------
    
    Signed-off-by: Mark Towers <mark@anyscale.com>
    Co-authored-by: Mark Towers <mark@anyscale.com>

---
+++ release/ray_release/byod/byod_huggingface_trl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script is used to build an extra layer on top of the base anyscale/ray image
+# to run the Hugging Face TRL example release test.
+
+set -exo pipefail
+
+# Install TRL and math_verify
+pip3 install --no-cache-dir "trl[vllm]" math_verify
+pip3 install --no-cache-dir --force-reinstall numpy pandas tf_keras
+# vllm requires numpy>=2.x; upgrade tensorflow and matplotlib to a version compatible with numpy 2.x.
+pip3 install --no-cache-dir --upgrade tensorflow matplotlib
+# wandb and comet were installed but didn't have API keys which raised errors and warnings
+pip3 uninstall -y wandb comet_ml
