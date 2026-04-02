0cfebe144feaf2179d92fd0779fcee7dfe68621c
Limark Dcunha <83493294+limarkdcunha@users.noreply.github.com>
2026-04-01 20:44:07 -0400

    [Data] Move resource budget Prometheus gauges to ExecutionCallback (#62209)
    ### Description
    
    This PR moves out responsibility of tracking Prometheus metrics for
    resource budgets into a new class ResourceAllocatorPrometheusCallback.
    This is in accordance with single responsibility principle.
    
    Closes #60269
    
    ---------
    
    Signed-off-by: Limark Dcunha <limarkdcunha@gmail.com>

--- python/ray/data/context.py
+++ python/ray/data/context.py
@@ -984,11 +984,15 @@ class DataContext:
         from ray.data._internal.execution.callbacks.insert_issue_detectors import (
             IssueDetectionExecutionCallback,
         )
+        from ray.data._internal.execution.callbacks.resource_allocator_prometheus_callback import (
+            ResourceAllocatorPrometheusCallback,
+        )
         from ray.data._internal.execution.execution_callback import ExecutionCallback
 
         classes = [
             ExecutionIdxUpdateCallback,
             IssueDetectionExecutionCallback,
+            ResourceAllocatorPrometheusCallback,
         ]
 
         # Parse environment variable for custom callbacks
