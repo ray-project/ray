9baee35f408af8d9902e529f4b58a5da64b0084c
elliot-barn <elliot.barnwell@anyscale.com>
2026-04-10 00:59:23 +0000

    updating depsets
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- ci/ray_ci/macos/macos_ci.sh
+++ ci/ray_ci/macos/macos_ci.sh
@@ -135,7 +135,7 @@ _prelude() {
   ./ci/env/env_info.sh
 
   # Install locked dependencies to ensure consistent package versions
-  pip install -r python/deplocks/ci/macos_depset_py3.10.lock --no-deps
+  pip install -r python/deplocks/ci/macos_depset_py3.10.lock --no-deps --no-verify-hashes
 }
 
 _epilogue() {
