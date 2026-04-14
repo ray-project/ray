6e2cd52812c40c0ce71aa24c4384b61ad9e0e728
Yicheng-Lu-llll <51814063+Yicheng-Lu-llll@users.noreply.github.com>
2026-04-14 11:54:34 -0700

    [Core] Fix get_last_job() not actually sorting in test_task_events_2
    Signed-off-by: Yicheng-Lu-llll <51814063+Yicheng-Lu-llll@users.noreply.github.com>

--- python/ray/tests/test_task_events_2.py
+++ python/ray/tests/test_task_events_2.py
@@ -1020,8 +1020,7 @@ ray.get([f.options(name="f.{task_name}").remote() for _ in range(10)])
 
     def get_last_job() -> str:
         jobs = list_jobs()
-        sorted(jobs, key=lambda x: x["job_id"])
-        return jobs[-1].job_id
+        return max(jobs, key=lambda x: x["job_id"]).job_id
 
     async def verify_tasks(task_name: str):
         # Query with job directly.
