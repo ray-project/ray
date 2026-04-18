bc96cb09d6bf7e94f3e75dd69ca348cabd443086
Seiji Eicher <seiji@anyscale.com>
2026-04-17 19:02:31 -0700

    [Serve] Rename HAProxy backend target vars
    Signed-off-by: Seiji Eicher <seiji@anyscale.com>

--- python/ray/serve/_private/haproxy_templates.py
+++ python/ray/serve/_private/haproxy_templates.py
@@ -149,7 +149,7 @@ backend {{ backend.name or 'unknown' }}
     {{ hc.default_server_directive }}
     {%- if backend.custom_request_routing %}
     {%- for server in backend.servers %}
-    use-server {{ server.name }} if { var(txn.direct_ingress_target) -m str "{{ server.name }}" }
+    use-server {{ server.name }} if { var(txn.backend_http_target) -m str "{{ server.name }}" }
     {%- endfor %}
     {%- endif %}
     {%- for server in backend.servers %}
