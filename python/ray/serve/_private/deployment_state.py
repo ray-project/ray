3351ed3e4a4841845ad7d9840905752a51603215
Soham Rajpure <srajpure@outlook.com>
2026-04-18 17:24:23 -0500

    [Serve] Defer DEPLOYMENT_TARGETS broadcast while replicas are RECOVERING
    Signed-off-by: Soham Rajpure <srajpure@outlook.com>

--- python/ray/serve/_private/deployment_state.py
+++ python/ray/serve/_private/deployment_state.py
@@ -2905,11 +2905,6 @@ class DeploymentState:
             self._deployment_scheduler.on_replica_recovering(replica_id)
             logger.debug(f"RECOVERING {replica_id}.")
 
-        # TODO(jiaodong): this currently halts all traffic in the cluster
-        # briefly because we will broadcast a replica update with everything in
-        # RECOVERING. We should have a grace period where we recover the state
-        # of the replicas before doing this update.
-
     def _recover_deployment_actors(self):
         """Recover deployment-scoped actors that survived a controller restart.
 
@@ -3147,6 +3142,11 @@ class DeploymentState:
             not self._broadcasted_replicas_set_changed
             and not self._request_routing_info_updated
         ):
+            return
+
+        # Hold off on broadcasting while replicas are in RECOVERING state to avoid sending
+        # partial or empty routable set.
+        if self._replicas.count(states=[ReplicaState.RECOVERING]) > 0:
             return
 
         running_replica_infos = self.get_running_replica_infos()
--- python/ray/serve/tests/unit/test_deployment_state.py
+++ python/ray/serve/tests/unit/test_deployment_state.py
@@ -49,6 +49,7 @@ from ray.serve._private.deployment_state import (
     ReplicaStateContainer,
 )
 from ray.serve._private.exceptions import DeploymentIsBeingDeletedError
+from ray.serve._private.long_poll import LongPollNamespace
 from ray.serve._private.test_utils import (
     MockDeploymentActorWrapper,
     MockPlacementGroup,
@@ -7298,6 +7299,86 @@ def test_broadcasted_replicas_set_changed_flag_set_on_lightweight_broadcast_conf
     with patch.object(ds, "get_running_replica_infos") as mock_get_infos:
         ds.broadcast_running_replicas_if_changed()
         mock_get_infos.assert_not_called()
+
+
+def test_broadcast_deferred_while_replicas_recovering(mock_deployment_state_manager):
+    """Regression test: During controller recovery, broadcast_running_replicas_if_changed() must
+    be deferred until all RECOVERING replicas have transitioned, then fire once with
+    the complete set.
+
+    More Context: https://github.com/ray-project/ray/issues/62728
+    """
+
+    def targets_key_was_broadcast(mock_lph):
+        """Return True if notify_changed was called with a DEPLOYMENT_TARGETS key."""
+        for call in mock_lph.notify_changed.call_args_list:
+            keys_dict = call[0][0]
+            if any(
+                isinstance(k, tuple) and k[0] == LongPollNamespace.DEPLOYMENT_TARGETS
+                for k in keys_dict
+            ):
+                return True
+        return False
+
+    create_dsm, _, _, _ = mock_deployment_state_manager
+    dsm: DeploymentStateManager = create_dsm()
+
+    # Deploy 3 replicas and bring to steady state.
+    info_1, v1 = deployment_info(num_replicas=3, version="1")
+    dsm.deploy(TEST_DEPLOYMENT_ID, info_1)
+    dsm.save_checkpoint()
+    ds = dsm._deployment_states[TEST_DEPLOYMENT_ID]
+
+    dsm.update()
+    for r in ds._replicas.get():
+        r._actor.set_ready()
+    dsm.update()
+    check_counts(ds, total=3, by_state=[(ReplicaState.RUNNING, 3, v1)])
+    assert ds.curr_status_info.status == DeploymentStatus.HEALTHY
+
+    # Simulate controller restart
+    replica_ids = [r.replica_id.to_full_id_str() for r in ds._replicas.get()]
+    new_dsm: DeploymentStateManager = create_dsm(actor_names=replica_ids)
+    new_ds = new_dsm._deployment_states[TEST_DEPLOYMENT_ID]
+
+    # All 3 replicas should be RECOVERING.
+    check_counts(new_ds, total=3, by_state=[(ReplicaState.RECOVERING, 3, v1)])
+
+    # While replicas are RECOVERING, broadcast must not fire, even though the
+    # _broadcasted_replicas_set_changed=True.
+    assert new_ds._broadcasted_replicas_set_changed is True
+    new_ds._long_poll_host.notify_changed.reset_mock()
+    new_ds.broadcast_running_replicas_if_changed()
+    assert not targets_key_was_broadcast(new_ds._long_poll_host)
+
+    # Partially complete recovery: 2/3 replicas RUNNING.
+    # Broadcast still suppressed because 1 replica is RECOVERING.
+    new_ds._long_poll_host.notify_changed.reset_mock()
+    recovering = new_ds._replicas.get(states=[ReplicaState.RECOVERING])
+    for r in recovering[:2]:
+        r._actor.set_ready()
+    new_dsm.update()
+    check_counts(
+        new_ds,
+        total=3,
+        by_state=[(ReplicaState.RUNNING, 2, v1), (ReplicaState.RECOVERING, 1, v1)],
+    )
+    assert not targets_key_was_broadcast(new_ds._long_poll_host)
+
+    # All replicas finish recovery, so broadcast fires
+    new_ds._long_poll_host.notify_changed.reset_mock()
+    for r in new_ds._replicas.get(states=[ReplicaState.RECOVERING]):
+        r._actor.set_ready()
+    new_dsm.update()
+    check_counts(new_ds, total=3, by_state=[(ReplicaState.RUNNING, 3, v1)])
+    assert targets_key_was_broadcast(new_ds._long_poll_host)
+    # The payload must contain all 3 running replicas.
+    for call in new_ds._long_poll_host.notify_changed.call_args_list:
+        keys_dict = call[0][0]
+        key = (LongPollNamespace.DEPLOYMENT_TARGETS, TEST_DEPLOYMENT_ID)
+        if key in keys_dict:
+            assert len(keys_dict[key].running_replicas) == 3
+            break
 
 
 def test_in_transition_cleared_at_steady_state(mock_deployment_state_manager):
