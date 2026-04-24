de67506f4954b1b4e6ba9e0ef671bc90405ba4f1
You-Cheng Lin <106612301+owenowenisme@users.noreply.github.com>
2026-04-24 16:54:26 +0800

    [Data] Extract `create_checkpoint_filter_op` from read op only (#62908)
    ## Description
    Extract `create_checkpoint_filter_op` so other input op can use it
    
    ## Related issues
    > Link related issues: "Fixes #1234", "Closes #1234", or "Related to
    #1234".
    
    ## Additional information
    > Optional: Add implementation details, API changes, usage examples,
    screenshots, etc.
    
    Signed-off-by: You-Cheng Lin <mses010108@gmail.com>

--- python/ray/data/_internal/planner/checkpoint/__init__.py
+++ python/ray/data/_internal/planner/checkpoint/__init__.py
@@ -1,7 +1,11 @@
-from .plan_read_op import plan_read_op_with_checkpoint_filter
+from .plan_read_op import (
+    create_checkpoint_filter_op,
+    plan_read_op_with_checkpoint_filter,
+)
 from .plan_write_op import plan_write_op_with_checkpoint_writer
 
 __all__ = [
+    "create_checkpoint_filter_op",
     "plan_read_op_with_checkpoint_filter",
     "plan_write_op_with_checkpoint_writer",
 ]
