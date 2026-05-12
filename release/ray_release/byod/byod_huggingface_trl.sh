b4826fefa5c127f29b2f3ac13ba5e544adbce9d2
Mark Towers <mark@anyscale.com>
2026-05-12 10:53:55 +0100

    Remove extra vllm install
    Signed-off-by: Mark Towers <mark@anyscale.com>

--- release/ray_release/byod/byod_huggingface_trl.sh
+++ release/ray_release/byod/byod_huggingface_trl.sh
@@ -5,7 +5,7 @@
 set -exo pipefail
 
 # Install TRL and math_verify
-pip3 install --no-cache-dir "trl[vllm]==1.0.0" "vllm>=0.18.1" math_verify
+pip3 install --no-cache-dir "trl[vllm]==1.0.0" math_verify
 pip3 install --no-cache-dir --force-reinstall numpy pandas
 # vllm requires numpy>=2.x; upgrade matplotlib to a version compatible with numpy 2.x.
 pip3 install --no-cache-dir --upgrade matplotlib
