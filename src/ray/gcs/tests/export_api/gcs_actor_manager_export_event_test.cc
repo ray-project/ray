7af6000dd007cf974f68809a22d6e5f5c4e96de3
Edward Oakes <ed.nmi.oakes@gmail.com>
2026-05-13 15:24:01 -0500

    [core] Split out `periodical_runner_interface.h` build target (#63205)
    Follow-up from: https://github.com/ray-project/ray/pull/63042
    
    ---------
    
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>
    Signed-off-by: Neelansh Khare <kharen@uci.edu>

--- src/ray/gcs/tests/export_api/gcs_actor_manager_export_event_test.cc
+++ src/ray/gcs/tests/export_api/gcs_actor_manager_export_event_test.cc
@@ -25,6 +25,7 @@
 #include "mock/ray/gcs/gcs_kv_manager.h"
 #include "mock/ray/gcs/gcs_node_manager.h"
 #include "ray/asio/instrumented_io_context.h"
+#include "ray/asio/periodical_runner.h"
 #include "ray/common/runtime_env_manager.h"
 #include "ray/common/test_utils.h"
 #include "ray/core_worker_rpc_client/core_worker_client_pool.h"
