41bb948105d3ea6842465824b8c3ee4c8b728d5b
Edward Oakes <ed.nmi.oakes@gmail.com>
2026-05-21 16:57:34 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/autoscaler/__init__.py
+++ python/ray/autoscaler/__init__.py
@@ -2,10 +2,12 @@ import os
 from pathlib import Path
 
 
+# Lazily import sdk to avoid circular dependencies.
 def __getattr__(name):
     if name == "sdk":
         from ray.autoscaler import sdk
 
+        globals()[name] = sdk
         return sdk
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
 
