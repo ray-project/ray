19c17a140164a55b80605e0de6c4359b55102259
Edward Oakes <ed.nmi.oakes@gmail.com>
2026-05-21 17:01:15 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- python/ray/tests/tmp.py
+++
@@ -1,9 +0,0 @@
-job:
-    entrypoint: python main.py
-
-detached_job:
-    entrypoint: my_module.TrainingCoordinator
-
-class TrainingCoordinator(ray.DetachedJobDriver)
-    def step(self) -> Checkpoint:
-        pass
