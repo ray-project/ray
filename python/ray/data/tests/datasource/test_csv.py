0728bc4bb821dab33e96a4c9ebbf8090edcf0d9e
Ayush Kumar <ayushk7102@gmail.com>
2026-05-21 11:49:05 -0700

    Reapply "[Data] Preserve Arrow types through pandas roundtrip (#63017)"
    This reverts commit 55764931c0121a66480771519ea6826fce5870ee.

--- python/ray/data/tests/datasource/test_csv.py
+++ python/ray/data/tests/datasource/test_csv.py
@@ -29,7 +29,7 @@ def test_csv_read(
     df1.to_csv(path1, index=False)
     ds = ray.data.read_csv(path1, partitioning=None)
     dsdf = ds.to_pandas().sort_values(by=["one", "two"]).reset_index(drop=True)
-    assert df1.equals(dsdf)
+    pd.testing.assert_frame_equal(df1.astype(dsdf.dtypes.to_dict()), dsdf)
     # Test metadata ops.
     assert ds.count() == 3
     assert ds.input_files() == [_unwrap_protocol(path1)]
@@ -42,7 +42,7 @@ def test_csv_read(
     ds = ray.data.read_csv([path1, path2], override_num_blocks=2, partitioning=None)
     dsdf = ds.to_pandas().sort_values(by=["one", "two"]).reset_index(drop=True)
     df = pd.concat([df1, df2], ignore_index=True)
-    assert df.equals(dsdf)
+    pd.testing.assert_frame_equal(df.astype(dsdf.dtypes.to_dict()), dsdf)
     # Test metadata ops.
     for block, meta in ds._execute().blocks:
         BlockAccessor.for_block(ray.get(block)).size_bytes() == meta.size_bytes  # type: ignore[call-overload]
@@ -58,7 +58,7 @@ def test_csv_read(
     )
     df = pd.concat([df1, df2, df3], ignore_index=True)
     dsdf = ds.to_pandas().sort_values(by=["one", "two"]).reset_index(drop=True)
-    assert df.equals(dsdf)
+    pd.testing.assert_frame_equal(df.astype(dsdf.dtypes.to_dict()), dsdf)
 
 
 def test_csv_write(
@@ -129,7 +129,7 @@ def test_csv_read_no_header(ray_start_regular_shared, tmp_path):
         read_options=csv.ReadOptions(column_names=["one", "two"]),
     )
     out_df = ds.to_pandas()
-    assert df.equals(out_df)
+    pd.testing.assert_frame_equal(df.astype(out_df.dtypes.to_dict()), out_df)
 
 
 def test_csv_read_with_column_type_specified(ray_start_regular_shared, tmp_path):
@@ -157,7 +157,10 @@ def test_csv_read_with_column_type_specified(ray_start_regular_shared, tmp_path)
         ),
     )
     expected_df = pd.DataFrame({"one": [1.0, 2.0, 30.0], "two": ["a", "b", "c"]})
-    assert ds.to_pandas().equals(expected_df)
+    actual_df = ds.to_pandas()
+    pd.testing.assert_frame_equal(
+        expected_df.astype(actual_df.dtypes.to_dict()), actual_df
+    )
 
 
 @pytest.mark.skipif(
