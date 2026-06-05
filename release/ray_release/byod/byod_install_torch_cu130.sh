eaa87c3e62ae097d792154593cf1b88432d185fe
elliot-barn <elliot.barnwell@anyscale.com>
2026-06-05 02:58:04 +0000

    compiling lock files for ray cu130 images
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- release/ray_release/byod/byod_install_torch_cu130.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script is used to build an extra layer on top of the base llm image
-# to run the llm sglang release tests
-
-set -exo pipefail
-
-pip3 install torch==2.12.0 \
-  --extra-index-url https://download.pytorch.org/whl/cu130
