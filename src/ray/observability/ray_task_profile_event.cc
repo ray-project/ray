d245d5ad6567abe1d8f864667b9fdbabca047db5
Kartica Modi <karticamodi@gmail.com>
2026-06-05 18:52:41 -0700

    [core] Add task-event RayEventInterface wrappers
    Signed-off-by: Kartica Modi <karticamodi@gmail.com>

---
+++ src/ray/observability/ray_task_profile_event.cc
@@ -0,0 +1,51 @@
+// Copyright 2025 The Ray Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "ray/observability/ray_task_profile_event.h"
+
+#include <string>
+#include <utility>
+
+namespace ray {
+namespace observability {
+
+RayTaskProfileEvent::RayTaskProfileEvent(rpc::events::TaskProfileEvents data,
+                                         const std::string &session_name)
+    : RayEvent<rpc::events::TaskProfileEvents>(rpc::events::RayEvent::CORE_WORKER,
+                                               rpc::events::RayEvent::TASK_PROFILE_EVENT,
+                                               rpc::events::RayEvent::INFO,
+                                               "",
+                                               session_name) {
+  data_ = std::move(data);
+}
+
+std::string RayTaskProfileEvent::GetEntityId() const {
+  return data_.task_id() + std::to_string(data_.attempt_number());
+}
+
+void RayTaskProfileEvent::MergeData(RayEvent<rpc::events::TaskProfileEvents> &&other) {
+  auto &&other_event = static_cast<RayTaskProfileEvent &&>(other);
+  // Concatenate the profiling span entries; component/task identifiers are identical for
+  // the same attempt so MergeFrom's scalar overlay is a no-op for them.
+  data_.MergeFrom(other_event.data_);
+}
+
+ray::rpc::events::RayEvent RayTaskProfileEvent::SerializeData() && {
+  ray::rpc::events::RayEvent event;
+  event.mutable_task_profile_events()->Swap(&data_);
+  return event;
+}
+
+}  // namespace observability
+}  // namespace ray
---
+++ src/ray/observability/ray_task_profile_event.h
@@ -0,0 +1,43 @@
+// Copyright 2025 The Ray Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <string>
+
+#include "ray/observability/ray_event.h"
+#include "src/ray/protobuf/events_task_profile_events.pb.h"
+
+namespace ray {
+namespace observability {
+
+template class RayEvent<rpc::events::TaskProfileEvents>;
+
+// RayTaskProfileEvent wraps a rpc::events::TaskProfileEvents (timeline/profiling spans
+// for a task attempt) as a RayEventInterface for recording through RayEventRecorder.
+// Profile events for the same task attempt are merged by appending their span entries.
+class RayTaskProfileEvent : public RayEvent<rpc::events::TaskProfileEvents> {
+ public:
+  RayTaskProfileEvent(rpc::events::TaskProfileEvents data,
+                      const std::string &session_name);
+
+  std::string GetEntityId() const override;
+
+ protected:
+  void MergeData(RayEvent<rpc::events::TaskProfileEvents> &&other) override;
+  ray::rpc::events::RayEvent SerializeData() && override;
+};
+
+}  // namespace observability
+}  // namespace ray
