f486fe7d2e4bd54ba241ef95ca43dc869972db55
You-Cheng Lin <mses010108@gmail.com>
2026-06-08 19:03:17 +0800

    remove pread
    Signed-off-by: You-Cheng Lin <mses010108@gmail.com>

--- src/ray/object_manager/spilled_object_reader.h
+++ src/ray/object_manager/spilled_object_reader.h
@@ -16,7 +16,6 @@
 
 #include <gtest/gtest_prod.h>
 
-#include <memory>
 #include <string>
 
 #include "absl/types/optional.h"
@@ -119,7 +118,6 @@ class SpilledObjectReader : public IObjectReader {
   const uint64_t metadata_offset_;
   const uint64_t metadata_size_;
   const rpc::Address owner_address_;
-  std::shared_ptr<int> spill_file_fd_;
 };
 
 }  // namespace ray
