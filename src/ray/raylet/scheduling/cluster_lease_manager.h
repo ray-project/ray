769ff4bb84a5e609c6e3f4113f88b51c83a951a7
Edward Oakes <ed.nmi.oakes@gmail.com>
2026-06-10 11:22:30 -0500

    fix
    Signed-off-by: Edward Oakes <ed.nmi.oakes@gmail.com>

--- src/ray/raylet/scheduling/cluster_lease_manager.h
+++ src/ray/raylet/scheduling/cluster_lease_manager.h
@@ -47,12 +47,11 @@ class ClusterLeaseManager : public ClusterLeaseManagerInterface {
   /// \param announce_infeasible_lease: Callback that informs the user if a lease
   ///                                  is infeasible.
   /// \param local_lease_manager: Manages local leases.
-  ClusterLeaseManager(
-      const NodeID &self_node_id,
-      ClusterResourceScheduler &cluster_resource_scheduler,
-      internal::NodeInfoGetter get_node_info,
-      std::function<void(const RayLease &)> announce_infeasible_lease,
-      LocalLeaseManagerInterface &local_lease_manager);
+  ClusterLeaseManager(const NodeID &self_node_id,
+                      ClusterResourceScheduler &cluster_resource_scheduler,
+                      internal::NodeInfoGetter get_node_info,
+                      std::function<void(const RayLease &)> announce_infeasible_lease,
+                      LocalLeaseManagerInterface &local_lease_manager);
 
   /// Queue lease and schedule. This happens when processing the worker lease request.
   ///
