a0c18c0829fc5b9d08b3ee0bbae6c10a889f2e6e
Sirui Huang <ray.huang@anyscale.com>
2026-06-24 20:50:59 -0700

    Fix pyrefly again
    Signed-off-by: Sirui Huang <ray.huang@anyscale.com>

--- python/ray/data/tests/test_block_ref_counter.py
+++ python/ray/data/tests/test_block_ref_counter.py
@@ -119,8 +119,8 @@ class TestBlockRefCounterLifecycle:
         """
         counter = BlockRefCounter()
         ref = ray.put(
-            np.zeros(1 * MiB, dtype=np.uint8)
-        )  # pyrefly: ignore[bad-argument-type]
+            np.zeros(1 * MiB, dtype=np.uint8)  # pyrefly: ignore[bad-argument-type]
+        )
 
         counter.on_block_produced(ref, 1 * MiB, "op_task")
         assert counter.get_object_store_memory_usage("op_task") == 1 * MiB
