6e709a0fafa495c5fd6a915399a428704a924012
Saurav Gupta <91198524+Saurav-Gupta-13@users.noreply.github.com>
2026-07-01 12:34:56 +0530

    chore: revert accidental changes to loading-data.rst
    Signed-off-by: Saurav Gupta <91198524+Saurav-Gupta-13@users.noreply.github.com>

--- python/ray/_private/worker.py
+++ python/ray/_private/worker.py
@@ -2674,9 +2674,6 @@ def connect(
         ):
             script_directory = os.path.dirname(os.path.realpath(sys.argv[0]))
             # If driver's sys.path doesn't include the script directory
-            # (e.g driver is started via `python -m`,
-            # see https://peps.python.org/pep-0338/),
-            # then we shouldn't add it to the workers.
             #
             # Also skip when the job already specifies a runtime_env
             # working_dir: in that case the driver was launched from inside
