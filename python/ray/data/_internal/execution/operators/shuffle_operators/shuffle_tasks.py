94b5da131e974dd3e4c3d02ca4ad6c17ae0140a4
Ayush Kumar <ayushk7102@gmail.com>
2026-07-13 18:03:35 -0700

    [Data] max calls = 1 to try and prevent heap memory OOM in workers
    Signed-off-by: Ayush Kumar <ayushk7102@gmail.com>

--- python/ray/data/_internal/execution/operators/shuffle_operators/shuffle_tasks.py
+++ python/ray/data/_internal/execution/operators/shuffle_operators/shuffle_tasks.py
@@ -232,7 +232,7 @@ def _gather_input_shards(
     return tables
 
 
-@ray.remote
+@ray.remote(max_calls=1)
 def _shuffle_reduce_task(
     shard_refs_by_input: List[List[ObjectRef]],
     partition_id: int,
