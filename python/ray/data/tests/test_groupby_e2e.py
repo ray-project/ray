4dfd626f205d4062542347340aae060c6a838f8a
You-Cheng Lin <mses010108@gmail.com>
2026-07-23 14:27:46 +0800

    update
    Signed-off-by: You-Cheng Lin <mses010108@gmail.com>

--- python/ray/data/tests/test_groupby_e2e.py
+++ python/ray/data/tests/test_groupby_e2e.py
@@ -32,20 +32,13 @@ from ray.data.aggregate import (
     Unique,
 )
 from ray.data.block import BlockAccessor
-from ray.data.context import DataContext, ShuffleStrategy
+from ray.data.context import ShuffleStrategy
 from ray.data.expressions import col
 from ray.data.tests.conftest import *  # noqa
 from ray.data.tests.util import named_values
 from ray.tests.conftest import *  # noqa
 
 RANDOM_SEED = 123
-
-
-@pytest.fixture(autouse=True, params=[False, True], ids=["shufflev1", "shufflev2"])
-def hash_shuffle_version(request, restore_data_context):
-    """Run every groupby test on both v1 (old actor-based) & v2 shuffle."""
-    DataContext.get_current().use_hash_shuffle_v2 = request.param
-    return request.param
 
 
 def _sort_series_of_lists_elements(s: pd.Series):
@@ -592,7 +585,8 @@ def test_groupby_arrow_multi_agg(
         # NOTE: Hash-shuffle internally converts to pyarrow
         (
             ds_format == "pandas"
-            and configure_shuffle_method == ShuffleStrategy.HASH_SHUFFLE
+            and configure_shuffle_method
+            in (ShuffleStrategy.HASH_SHUFFLE, ShuffleStrategy.HASH_SHUFFLE_V2)
         )
     )
 
