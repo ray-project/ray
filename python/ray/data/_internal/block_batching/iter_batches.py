2b9bae85ff21edcd9e74dae3f67d6826e1a8cb4f
iamjustinhsu <140442892+iamjustinhsu@users.noreply.github.com>
2026-07-28 11:12:55 -0700

    [data] avoid epoch end for failued attempts (#65082)
    We don't want to call aftter_epoch_end for failed epochs because the
    function right now assumes successful epoch
    
    https://github.com/ray-project/ray/pull/64994 originally made this
    change, but the PR evolved and we didn't actually need it anymore. Also
    caused a semantic change because previously this was interpreted as
    "after epoch success." But with the try/finally, it also triggers on
    breaks and errors.
    
    ---------
    
    Signed-off-by: iamjustinhsu <jhsu@anyscale.com>

--- python/ray/data/_internal/block_batching/iter_batches.py
+++ python/ray/data/_internal/block_batching/iter_batches.py
@@ -284,20 +284,19 @@ class BatchIterator:
 
         self.before_epoch_start()
 
-        try:
-            while True:
-                with self.get_next_batch_context():
-                    blocked_start_s = time.perf_counter()
-                    try:
-                        batch = next(batch_iter)
-                    except StopIteration:
-                        break
-                    blocked_end_s = time.perf_counter()
-                self._attribute_blocked_time(batch, blocked_start_s, blocked_end_s)
-                with self.yield_batch_context(batch):
-                    yield batch.data
-        finally:
-            self.after_epoch_end()
+        while True:
+            with self.get_next_batch_context():
+                blocked_start_s = time.perf_counter()
+                try:
+                    batch = next(batch_iter)
+                except StopIteration:
+                    break
+                blocked_end_s = time.perf_counter()
+            self._attribute_blocked_time(batch, blocked_start_s, blocked_end_s)
+            with self.yield_batch_context(batch):
+                yield batch.data
+
+        self.after_epoch_end()
 
     def _attribute_blocked_time(
         self, batch: Batch, blocked_start_s: float, blocked_end_s: float
