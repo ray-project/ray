7d6ac8431e95f5895bbf011df0f645ea77879cf9
Andrew Sy Kim <andrewsy@google.com>
2026-08-16 17:56:02 +0000

    remove public API annotation for sandbox actor
    Signed-off-by: Andrew Sy Kim <andrewsy@google.com>

--- python/ray/experimental/sandbox/sandbox.py
+++ python/ray/experimental/sandbox/sandbox.py
@@ -8,10 +8,8 @@ from ray.experimental.sandbox.backend.base import (
 from ray.experimental.sandbox.config import SandboxConfig
 from ray.experimental.sandbox.exceptions import SandboxNotFoundError
 from ray.experimental.sandbox.runtime import SandboxRuntime
-from ray.util.annotations import PublicAPI
 
 
-@PublicAPI(stability="alpha")
 @ray.remote
 class Sandbox:
     """Ray actor interface for managing scheduling and lifecycle of an isolated sandbox.
