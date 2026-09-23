20beba5073ee7ff5c54ee873f592b11d2c1763dc
elliot-barn <elliot.barnwell@anyscale.com>
2026-09-23 07:04:01 +0000

    bumping ray version to 2.58.1
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- src/ray/common/constants.h
+++ src/ray/common/constants.h
@@ -74,7 +74,7 @@ constexpr char kDashboardAgentListenPortName[] = "dashboard_agent_listen_port";
 constexpr char kGcsServerPortName[] = "gcs_server_port";
 
 /// The version of Ray
-constexpr char kRayVersion[] = "2.58.0";
+constexpr char kRayVersion[] = "2.58.1";
 
 /*****************************/
 /* ENV labels for autoscaler */
